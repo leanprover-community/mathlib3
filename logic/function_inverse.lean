7e06124ce98784cf1c563141ae9b3811fa927ca0
Johannes Hölzl <johannes.hoelzl@gmx.de>
2017-09-10 20:33:16 -0400

    feat(logic): add small theory on inverse functions

---
+++ logic/function_inverse.lean
@@ -0,0 +1,76 @@
+/-
+Copyright (c) 2016 Johannes Hölzl. All rights reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
+Author: Johannes Hölzl
+
+Inverse of a function.
+-/
+import logic.basic
+noncomputable theory
+
+open classical function
+local attribute [instance] decidable_inhabited prop_decidable
+
+namespace set
+
+universes u v
+variables {α : Type u} {β : Type v} {f : α → β} {s : set α} {a : α} {b : β}
+
+section inv_fun
+variable [inhabited α]
+
+def inv_fun_on (f : α → β) (s : set α) (b : β) : α :=
+if h : ∃a, a ∈ s ∧ f a = b then some h else default α
+
+theorem inv_fun_on_pos (h : ∃a∈s, f a = b) : inv_fun_on f s b ∈ s ∧ f (inv_fun_on f s b) = b :=
+by rw [bex_def] at h; rw [inv_fun_on, dif_pos h]; exact some_spec h
+
+theorem inv_fun_on_mem (h : ∃a∈s, f a = b) : inv_fun_on f s b ∈ s := (inv_fun_on_pos h).left
+
+theorem inv_fun_on_eq (h : ∃a∈s, f a = b) : f (inv_fun_on f s b) = b := (inv_fun_on_pos h).right
+
+theorem inv_fun_on_neg (h : ¬ ∃a∈s, f a = b) : inv_fun_on f s b = default α :=
+by rw [bex_def] at h; rw [inv_fun_on, dif_neg h]
+
+def inv_fun (f : α → β) : β → α := inv_fun_on f univ
+
+theorem inv_fun_eq (h : ∃a, f a = b) : f (inv_fun f b) = b :=
+inv_fun_on_eq $ let ⟨a, ha⟩ := h in ⟨a, trivial, ha⟩
+
+theorem inv_fun_eq_of_injective_of_right_inverse {g : β → α}
+  (hf : injective f) (hg : right_inverse g f) : inv_fun f = g :=
+funext $ assume b,
+hf begin rw [hg b], exact inv_fun_eq ⟨g b, hg b⟩ end
+
+lemma right_inverse_inv_fun (hf : surjective f) : right_inverse (inv_fun f) f :=
+assume b, inv_fun_eq $ hf b
+
+lemma left_inverse_inv_fun (hf : injective f) : left_inverse (inv_fun f) f :=
+assume b,
+have f (inv_fun f (f b)) = f b,
+  from inv_fun_eq ⟨b, rfl⟩,
+hf this
+
+lemma has_left_inverse (hf : injective f) : has_left_inverse f :=
+⟨inv_fun f, left_inverse_inv_fun hf⟩
+
+end inv_fun
+
+section surj_inv
+
+def surj_inv {f : α → β} (h : surjective f) (b : β) : α := some (h b)
+
+lemma surj_inv_eq (h : surjective f) : f (surj_inv h b) = b := some_spec (h b)
+
+lemma right_inverse_surj_inv (hf : surjective f) : right_inverse (surj_inv hf) f :=
+assume b, surj_inv_eq hf
+
+lemma has_right_inverse (hf : surjective f) : has_right_inverse f :=
+⟨_, right_inverse_surj_inv hf⟩
+
+lemma injective_surj_inv (h : surjective f) : injective (surj_inv h) :=
+injective_of_has_left_inverse ⟨f, right_inverse_surj_inv h⟩
+
+end surj_inv
+
+end set

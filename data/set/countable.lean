28b46a2158b598678a1e41ae38eefbac2ba856e1
Johannes Hölzl <johannes.hoelzl@gmx.de>
2017-09-11 11:32:25 -0400

    fix(data/set/countable): finish proof countable_sUnion

--- data/set/countable.lean
+++ data/set/countable.lean
@@ -11,89 +11,12 @@ noncomputable theory
 
 open function set encodable
 
-namespace option
-
-@[simp] lemma bind_some {α β : Type*} {a : α} {f : α → option β} : some a >>= f = f a :=
-rfl
-
-end option
-
-namespace set
-
-section enumerate
-parameters {α : Type*} (sel : set α → option α)
-
-def enumerate : set α → ℕ → option α
-| s 0       := sel s
-| s (n + 1) := do a ← sel s, enumerate (s - {a}) n
-
-lemma enumerate_eq_none_of_sel {s : set α} (h : sel s = none) : ∀{n}, enumerate s n = none
-| 0       := by simp [h, enumerate]; refl
-| (n + 1) := by simp [h, enumerate]; refl
-
-lemma enumerate_eq_none : ∀{s n₁ n₂}, enumerate s n₁ = none → n₁ ≤ n₂ → enumerate s n₂ = none
-| s 0       m := assume : sel s = none, by simp [enumerate_eq_none_of_sel, this]
-| s (n + 1) m := assume h hm,
-  begin
-    cases hs : sel s,
-    { by simp [enumerate_eq_none_of_sel, hs] },
-    { cases m,
-      case nat.zero {
-        have : n + 1 = 0, from nat.eq_zero_of_le_zero hm,
-        contradiction },
-      case nat.succ m' {
-        simp [hs, enumerate] at h ⊢,
-        have hm : n ≤ m', from nat.le_of_succ_le_succ hm,
-        exact enumerate_eq_none h hm } }
-  end
-
-lemma enumerate_mem (h_sel : ∀s a, sel s = some a → a ∈ s) :
-  ∀{s n a}, enumerate s n = some a → a ∈ s
-| s 0 a     := h_sel s a
-| s (n+1) a :=
-  begin
-    cases h : sel s,
-    case option.none { simp [enumerate_eq_none_of_sel, h], contradiction },
-    case option.some a' {
-      simp [enumerate, h],
-      exact assume h' : enumerate _ (s - {a'}) n = some a,
-        have a ∈ s - {a'}, from enumerate_mem h',
-        this.left }
-  end
-
-lemma enumerate_inj {n₁ n₂ : ℕ} {a : α} {s : set α} (h_sel : ∀s a, sel s = some a → a ∈ s) :
-  enumerate s n₁ = some a → enumerate s n₂ = some a → n₁ = n₂ :=
-have ∀{n m s}, enumerate s n = some a → enumerate s (n + m) = some a → m = 0,
-begin
-  intros n m, induction n,
-  case nat.zero {
-    cases m,
-    case nat.zero { simp [enumerate] },
-    case nat.succ n {
-      simp [enumerate] {contextual := tt},
-      exact assume s _ h,
-        have a ∈ s \ {a}, from enumerate_mem _ h_sel h,
-        by simp [mem_diff] at this; assumption } },
-  case nat.succ n ih {
-    intro s,
-    cases h : sel s,
-    case option.none { simp [enumerate, h], contradiction },
-    case option.some a' {
-      simp at ih,
-      simp [enumerate, h, nat.add_succ] {contextual := tt},
-      exact ih } }
-end,
-match le_total n₁ n₂ with
-| or.inl h := let ⟨m, hm⟩ := nat.le.dest h in hm ▸ assume h₁ h₂, by simp [this h₁ h₂]
-| or.inr h := let ⟨m, hm⟩ := nat.le.dest h in hm ▸ assume h₁ h₂, by simp [this h₂ h₁]
-end
-
-end enumerate
-
 open classical (hiding some)
 local attribute [instance] decidable_inhabited prop_decidable
 universes u v w
 variables {α : Type u} {β : Type v} {γ : Type w}
+
+namespace set
 
 section encodable
 
@@ -156,10 +79,27 @@ let f' : {a // a ∈ s} → {b // b ∈ f '' s} := λ⟨a, ha⟩, ⟨f a, mem_im
 have hf' : surjective f', from assume ⟨b, a, ha, hab⟩, ⟨⟨a, ha⟩, subtype.eq hab⟩,
 countable_encodable' $ @encodable_of_inj _ _ hs.to_encodable (surj_inv hf') (injective_surj_inv hf')
 
-lemma countable_sUnion {s : set (set α)} (hs : countable s) (ht : ∀a∈s, countable a) :
+lemma countable_sUnion {s : set (set α)} (hs : countable s) (h : ∀a∈s, countable a) :
   countable (⋃₀ s) :=
-let f := classical.some hs in
-let g := λa ha, classical.some (ht a ha) in
-_
+by_cases
+  (assume : nonempty α, let ⟨a⟩ := this, inh : inhabited α := ⟨a⟩ in
+    let ⟨fs, hfs⟩ := countable_iff_exists_surjective.mp hs in
+    have ∀t, ∃ft:ℕ → α, t ∈ s → t ⊆ range ft,
+      from assume t,
+      by_cases
+        (assume : t ∈ s,
+          let ⟨ft, hft⟩ := (@countable_iff_exists_surjective α inh _).mp $ h t this in
+          ⟨ft, assume _, hft⟩)
+        (assume : t ∉ s, ⟨λ_, a, assume h, (this h).elim⟩),
+    have ∃ft:(∀t:set α, ℕ → α), ∀t∈s, t ⊆ range (ft t),
+      by simp [classical.skolem] at this; assumption,
+    let ⟨ft, hft⟩ := this in
+    (@countable_iff_exists_surjective α inh _).mpr
+      ⟨(λp:ℕ×ℕ, ft (fs p.1) p.2) ∘ nat.unpair,
+        by simp [subset_def];
+        from assume a t ha ht,
+        let ⟨i, hi⟩ := hfs ht, ⟨j, hj⟩ := hft t ht ha in
+        ⟨nat.mkpair i j, by simp [function.comp, nat.unpair_mkpair, hi, hj]⟩⟩)
+  (assume : ¬ nonempty α, ⟨λ_, 0, assume a, (this ⟨a⟩).elim⟩)
 
 end set

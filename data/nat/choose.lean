6f7f1e746377d3bec2788e837f992fead7a53398
Chris Hughes <33847686+dorhinj@users.noreply.github.com>
2018-02-15 14:27:26 +0000

    Update choose.lean

--- data/nat/choose.lean
+++ data/nat/choose.lean
@@ -5,60 +5,57 @@ Authors: Chris Hughes
 Mostly based on Jeremy Avigad's choose file in lean 2
 -/
 
-
 import data.nat.basic
 
 open nat
+
 def choose : ℕ → ℕ → ℕ
-| _              0  := 1
-| 0        (succ k) := 0
-| (succ n) (succ k) := choose n k + choose n (succ k)
+| _             0 := 1
+| 0       (k + 1) := 0
+| (n + 1) (k + 1) := choose n k + choose n (succ k)
 
-@[simp]
-lemma choose_zero_right (n : ℕ) : choose n 0 = 1 := by cases n; refl
+@[simp] lemma choose_zero_right (n : ℕ) : choose n 0 = 1 := by cases n; refl
 
-@[simp]
-lemma choose_zero_succ (k : ℕ) : choose 0 (succ k) = 0 := rfl
+@[simp] lemma choose_zero_succ (k : ℕ) : choose 0 (succ k) = 0 := rfl
 
 lemma choose_succ_succ (n k : ℕ) : choose (succ n) (succ k) = choose n k + choose n (succ k) := rfl
 
 lemma choose_eq_zero_of_lt : ∀ {n k}, n < k → choose n k = 0
-| _               0 hk := absurd hk dec_trivial
-| 0        (succ k) hk := choose_zero_succ _
-| (succ n) (succ k) hk := 
+| _             0 hk := absurd hk dec_trivial
+| 0       (k + 1) hk := choose_zero_succ _
+| (n + 1) (k + 1) hk := 
   have hnk : n < k, from lt_of_succ_lt_succ hk,
   have hnk1 : n < k + 1, from lt_of_succ_lt hk,
   by rw [choose_succ_succ, choose_eq_zero_of_lt hnk, choose_eq_zero_of_lt hnk1]
 
-@[simp]
-lemma choose_self (n : ℕ) : choose n n = 1 :=
+@[simp] lemma choose_self (n : ℕ) : choose n n = 1 :=
 by induction n; simp [*, choose, choose_eq_zero_of_lt (lt_succ_self _)]
 
-@[simp]
-lemma choose_succ_self (n : ℕ) : choose n (succ n) = 0 := choose_eq_zero_of_lt (lt_succ_self _)
+@[simp] lemma choose_succ_self (n : ℕ) : choose n (succ n) = 0 := 
+choose_eq_zero_of_lt (lt_succ_self _)
 
-@[simp]
-lemma choose_one_right (n : ℕ) : choose n 1 = n :=
+@[simp] lemma choose_one_right (n : ℕ) : choose n 1 = n :=
 by induction n; simp [*, choose]
 
 lemma choose_pos : ∀ {n k}, k ≤ n → 0 < choose n k
-| 0               _ hk := by rw [eq_zero_of_le_zero hk]; exact dec_trivial
-| (succ n)        0 hk := by simp; exact dec_trivial
-| (succ n) (succ k) hk := by rw choose_succ_succ;
+| 0             _ hk := by rw [eq_zero_of_le_zero hk]; exact dec_trivial
+| (n + 1)       0 hk := by simp; exact dec_trivial
+| (n + 1) (k + 1) hk := by rw choose_succ_succ;
     exact add_pos_of_pos_of_nonneg (choose_pos (le_of_succ_le_succ hk)) (zero_le _)
 
 
 lemma succ_mul_choose_eq : ∀ n k, succ n * choose n k = choose (succ n) (succ k) * succ k
-| 0               0 := dec_trivial
-| 0        (succ k) := by simp [choose]
-| (succ n)        0 := by simp
-| (succ n) (succ k) := by rw [choose_succ_succ (succ n) (succ k), add_mul, ←succ_mul_choose_eq, mul_succ,
-                              ←succ_mul_choose_eq, add_right_comm, ←mul_add, ←choose_succ_succ, ←succ_mul] 
+| 0             0 := dec_trivial
+| 0       (k + 1) := by simp [choose]
+| (n + 1)       0 := by simp
+| (n + 1) (k + 1) := 
+  by rw [choose_succ_succ (succ n) (succ k), add_mul, ←succ_mul_choose_eq, mul_succ,
+  ←succ_mul_choose_eq, add_right_comm, ←mul_add, ←choose_succ_succ, ←succ_mul] 
 
 lemma choose_mul_fact_mul_fact : ∀ {n k}, k ≤ n → choose n k * fact k * fact (n - k) = fact n
-| 0               _ hk := by simp [eq_zero_of_le_zero hk]
-| (succ n)        0 hk := by simp
-| (succ n) (succ k) hk := 
+| 0              _ hk := by simp [eq_zero_of_le_zero hk]
+| (n + 1)        0 hk := by simp
+| (n + 1) (succ k) hk := 
 begin 
   rw [choose_succ_succ, succ_sub_succ, add_mul, add_mul],
   have : choose n k * fact (succ k) * fact (n - k) = choose n k * fact k * fact (n - k) * succ k :=

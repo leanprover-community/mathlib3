1f1867939b132a0fbd00b4fae3ff8fb301454412
Scott Morrison <scott@tqft.net>
2018-02-16 11:34:41 +1100

    minor

--- categories/examples/groups/default.lean
+++ categories/examples/groups/default.lean
@@ -77,10 +77,10 @@ instance CategoryOfGroups : category Group := {
 open categories.functor
 open categories.types
 
-definition ForgetfulFunctor_Semigroups_to_Types : Functor Group (Type u₁) :=
+definition ForgetfulFunctor_Groups_to_Types : Functor Group (Type u₁) :=
 {
   onObjects     := λ s, s.1,
-  onMorphisms   := λ s t, λ f, ulift.up f.map,
+  onMorphisms   := λ s t, λ f, ulift.up f.map, -- we need this ulift because there's only one universe level in the category of types.
 }
 
 end categories.examples.groups

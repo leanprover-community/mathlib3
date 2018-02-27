df745d7fa8a96e01ce966694e36e6333fa0797ae
Simon Hudon <simon.hudon@gmail.com>
2018-02-26 19:36:39 -0500

    [WIP]  in example, added a specialized corecursor

--- data/coinductive/examples.lean
+++ data/coinductive/examples.lean
@@ -33,22 +33,24 @@ def branch (A : Type) : l_two_tree_node A → Type
 
 def l_two_tree_intl (A : Type) := cofix (branch A)
 
-inductive l_two_tree (A : Type) : Type
-| nil {}      : l_two_tree
-| cons        : A → l_two_tree_intl A → l_two_tree
-| cons₂       : A → l_two_tree_intl A → l_two_tree_intl A → l_two_tree
+inductive l_two_tree' (A X : Type) : Type
+| nil {}      : l_two_tree'
+| cons        : A → X → l_two_tree'
+| cons₂       : A → X → X → l_two_tree'
+
+def  l_two_tree (A : Type) := l_two_tree' A (l_two_tree_intl A)
 
 def to_intl {A : Type} : l_two_tree A → l_two_tree_intl A
-| l_two_tree.nil := cofix.mk nil empty.elim
-| (l_two_tree.cons x t) := cofix.mk (cons x) (λ _, t)
-| (l_two_tree.cons₂ x t₀ t₁) := cofix.mk (cons₂ x) (λ i : bool, if i then t₀ else t₁)
+| l_two_tree'.nil := cofix.mk nil empty.elim
+| (l_two_tree'.cons x t) := cofix.mk (cons x) (λ _, t)
+| (l_two_tree'.cons₂ x t₀ t₁) := cofix.mk (cons₂ x) (λ i : bool, if i then t₀ else t₁)
 
 def of_intl {A : Type} : l_two_tree_intl A → l_two_tree A :=
 cofix.cases $ λ i,
 match i with
-| nil := λ ch, l_two_tree.nil
-| (cons x) := λ ch, l_two_tree.cons x (ch ())
-| (cons₂ x) := λ ch, l_two_tree.cons₂ x (ch tt) (ch ff)
+| nil := λ ch, l_two_tree'.nil
+| (cons x) := λ ch, l_two_tree'.cons x (ch ())
+| (cons₂ x) := λ ch, l_two_tree'.cons₂ x (ch tt) (ch ff)
 end
 
 def l_two_equiv (A : Type) : l_two_tree A ≃ l_two_tree_intl A :=
@@ -60,39 +62,53 @@ def l_two_equiv (A : Type) : l_two_tree A ≃ l_two_tree_intl A :=
                     cases i ; simp [of_intl._match_1,to_intl] ;
                     congr ; funext x ; cases x ; simp } }
 
+-- corecursion
+def l_two_tree.corec {A} {X : Sort*}
+  (f : Π z, X → (X → z) → l_two_tree' A z)
+  (x₀ : X)
+: l_two_tree A :=
+of_intl $
+  cofix.corec (λ x,
+    match f X x id with
+     | l_two_tree'.nil := ⟨ nil, empty.elim ⟩
+     | (l_two_tree'.cons x t) := ⟨ cons x, λ _, t ⟩
+     | (l_two_tree'.cons₂ x t₀ t₁) := ⟨ cons₂ x, λ i : bool, if i then t₀ else t₁ ⟩
+    end)
+    x₀
+
 -- construct one infinite tree and print part of it
-def mk_tree : ℕ → l_two_tree_intl ℕ :=
-cofix.corec $ λ n,
-if n % 2 = 0 then
-⟨ cons₂ n , λ i : bool, if i then n+1 else n+2 ⟩
+def mk_tree : ℕ → l_two_tree ℕ :=
+l_two_tree.corec $ λ z n mk_tree,
+if n % 10 = 0 then
+  l_two_tree'.cons n (mk_tree $ n+1)
+else if n % 10 = 7 then
+  l_two_tree'.nil
 else
-⟨ cons n , λ _, n+1 ⟩
+  l_two_tree'.cons₂ n (mk_tree $ n+1) (mk_tree $ n+2)
 
 open nat
-def to_bin_tree : l_two_tree_intl ℕ → ℕ → bin_tree ℕ
+def to_bin_tree_aux : l_two_tree_intl ℕ → ℕ → bin_tree ℕ
  | t (succ n) :=
 match of_intl t with
- | l_two_tree.nil := bin_tree.empty
- | (l_two_tree.cons x t') := bin_tree.node (bin_tree.leaf x) (to_bin_tree t' n)
- | (l_two_tree.cons₂ x t₀ t₁) := bin_tree.node (bin_tree.leaf x)
-                                               (bin_tree.node (to_bin_tree t₀ n)
-                                                              (to_bin_tree t₁ n))
+ | l_two_tree'.nil := bin_tree.empty
+ | (l_two_tree'.cons x t') := bin_tree.node (bin_tree.leaf x) (to_bin_tree_aux t' n)
+ | (l_two_tree'.cons₂ x t₀ t₁) := bin_tree.node (bin_tree.leaf x)
+                                               (bin_tree.node (to_bin_tree_aux t₀ n)
+                                                              (to_bin_tree_aux t₁ n))
 end
  | _ 0 := bin_tree.empty
+
+def to_bin_tree (t : l_two_tree ℕ) : ℕ → bin_tree ℕ :=
+to_bin_tree_aux (to_intl t)
 
 #reduce to_bin_tree (mk_tree 0) 3
 /-
 bin_tree.node (bin_tree.leaf 0)
-  (bin_tree.node
-     (bin_tree.node (bin_tree.leaf 1) (bin_tree.node (bin_tree.leaf 2) (bin_tree.node bin_tree.empty bin_tree.empty)))
-     (bin_tree.node (bin_tree.leaf 2)
-        (bin_tree.node (bin_tree.node (bin_tree.leaf 3) bin_tree.empty)
-           (bin_tree.node (bin_tree.leaf 4) (bin_tree.node bin_tree.empty bin_tree.empty)))))
-
+  (bin_tree.node (bin_tree.leaf 1)
+     (bin_tree.node (bin_tree.node (bin_tree.leaf 2) (bin_tree.node bin_tree.empty bin_tree.empty))
+        (bin_tree.node (bin_tree.leaf 3) (bin_tree.node bin_tree.empty bin_tree.empty))))
 -/
-#eval bin_tree.to_list $ to_bin_tree (mk_tree 0) 3
--- [0, 1, 2, 2, 3, 4]
-
--- recursion
+#eval bin_tree.to_list $ to_bin_tree (mk_tree 0) 5
+-- [0, 1, 2, 3, 4, 5, 4, 5, 6, 3, 4, 5, 6, 5, 6]
 
 end examples

d80ca5926c00e25bb6a1381e5739f7e2dc064bd8
Mario Carneiro <di.gama@gmail.com>
2018-04-01 21:19:29 -0400

    feat(data/fin): add fz/fs recursor for fin

--- data/fin.lean
+++ data/fin.lean
@@ -16,3 +16,26 @@ variables {n : ℕ} {a b : fin n}
 
 protected theorem fin.succ.inj (p : fin.succ a = fin.succ b) : a = b :=
 by cases a; cases b; exact eq_of_veq (nat.succ.inj (veq_of_eq p))
+
+@[elab_as_eliminator] def fin.succ_rec
+  {C : ∀ n, fin n → Sort*}
+  (H0 : ∀ n, C (succ n) 0)
+  (Hs : ∀ n i, C n i → C (succ n) i.succ) : ∀ {n : ℕ} (i : fin n), C n i
+| 0        i           := i.elim0
+| (succ n) ⟨0, _⟩      := H0 _
+| (succ n) ⟨succ i, h⟩ := Hs _ _ (fin.succ_rec ⟨i, lt_of_succ_lt_succ h⟩)
+
+@[elab_as_eliminator] def fin.succ_rec_on {n : ℕ} (i : fin n)
+  {C : ∀ n, fin n → Sort*}
+  (H0 : ∀ n, C (succ n) 0)
+  (Hs : ∀ n i, C n i → C (succ n) i.succ) : C n i :=
+i.succ_rec H0 Hs
+
+@[simp] theorem fin.succ_rec_on_zero
+  {C : ∀ n, fin n → Sort*} {H0 Hs} (n) :
+  @fin.succ_rec_on (succ n) 0 C H0 Hs = H0 n := rfl
+
+@[simp] theorem fin.succ_rec_on_succ
+  {C : ∀ n, fin n → Sort*} {H0 Hs} {n} (i : fin n) :
+  @fin.succ_rec_on (succ n) i.succ C H0 Hs = Hs n i (fin.succ_rec_on i H0 Hs) :=
+by cases i; refl

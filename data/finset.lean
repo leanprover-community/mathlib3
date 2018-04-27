ed7275d6c0b1582ac1d4ad20643d5dac44249c8a
Chris Hughes <33847686+dorhinj@users.noreply.github.com>
2018-04-27 20:11:23 +0100

    Use global disjoint

--- data/finset.lean
+++ data/finset.lean
@@ -5,7 +5,7 @@ Author: Leonardo de Moura, Jeremy Avigad, Minchao Wu, Mario Carneiro
 
 Finite sets.
 -/
-import data.multiset order.boolean_algebra algebra.order_functions data.sigma.basic
+import data.multiset order.boolean_algebra algebra.order_functions data.sigma.basic data.set.lattice
 open multiset subtype nat lattice
 
 variables {α : Type*} {β : Type*} {γ : Type*}
@@ -992,61 +992,51 @@ list.to_finset_eq (sort_nodup r s) ▸ eq_of_veq (sort_eq r s)
 
 end sort
 
-def disjoint (s t : finset α) : Prop := ∀ ⦃a⦄, a ∈ s → a ∈ t → false
+variable [decidable_eq α]
 
-theorem disjoint.symm {s t : finset α} (d : disjoint s t) : disjoint t s
-| a i₂ i₁ := d i₁ i₂
+theorem disjoint_iff_inter_eq_empty [decidable_eq α] {s t : finset α} : disjoint s t ↔ s ∩ t = ∅ :=
+iff.rfl
 
-@[simp] theorem disjoint_comm {s t : finset α} : disjoint s t ↔ disjoint t s :=
-⟨disjoint.symm, disjoint.symm⟩
-
-theorem disjoint_left {s t : finset α} : disjoint s t ↔ ∀ {a}, a ∈ s → a ∉ t := iff.rfl
+theorem disjoint_left {s t : finset α} : disjoint s t ↔ ∀ {a}, a ∈ s → a ∉ t :=
+by simp [disjoint_iff_inter_eq_empty, ext, mem_inter]
 
 theorem disjoint_right {s t : finset α} : disjoint s t ↔ ∀ {a}, a ∈ t → a ∉ s :=
-disjoint_comm
+by rw [_root_.disjoint_comm, disjoint_left]
 
 theorem disjoint_iff_ne {s t : finset α} : disjoint s t ↔ ∀ a ∈ s, ∀ b ∈ t, a ≠ b :=
 by simp [disjoint_left, imp_not_comm]
 
-theorem disjoint_of_subset_left {s t u : finset α} (h : s ⊆ u) (d : disjoint u t) : disjoint s t
-| x m₁ := d (h m₁)
+theorem disjoint_of_subset_left {s t u : finset α} (h : s ⊆ u) (d : disjoint u t) : disjoint s t :=
+disjoint_left.2 (λ x m₁, (disjoint_left.1 d) (h m₁))
 
-theorem disjoint_of_subset_right {s t u : finset α} (h : t ⊆ u) (d : disjoint s u) : disjoint s t
-| x m m₁ := d m (h m₁)
+theorem disjoint_of_subset_right {s t u : finset α} (h : t ⊆ u) (d : disjoint s u) : disjoint s t :=
+disjoint_right.2 (λ x m₁, (disjoint_right.1 d) (h m₁))
 
-@[simp] theorem disjoint_empty_left (s : finset α) : disjoint ∅ s
-| a := (not_mem_empty a).elim
+@[simp] theorem disjoint_empty_left (s : finset α) : disjoint ∅ s := bot_inf_eq
 
-@[simp] theorem disjoint_empty_right (s : finset α) : disjoint s ∅ :=
-disjoint_comm.1 (disjoint_empty_left _)
+@[simp] theorem disjoint_empty_right (s : finset α) : disjoint s ∅ := inf_bot_eq
 
 @[simp] theorem singleton_disjoint {s : finset α} {a : α} : disjoint (singleton a) s ↔ a ∉ s :=
-by simp [disjoint]; refl
+by simp [disjoint_left]; refl
 
 @[simp] theorem disjoint_singleton {s : finset α} {a : α} : disjoint s (singleton a) ↔ a ∉ s :=
-by rw disjoint_comm; simp
+by rw _root_.disjoint_comm; simp
 
 @[simp] theorem disjoint_insert_left [decidable_eq α] {a : α} {s t : finset α} :
   disjoint (insert a s) t ↔ a ∉ t ∧ disjoint s t :=
-by simp [disjoint, or_imp_distrib, forall_and_distrib]; refl
+by simp [disjoint_left, or_imp_distrib, forall_and_distrib]; refl
 
 @[simp] theorem disjoint_insert_right [decidable_eq α] {a : α} {s t : finset α} :
   disjoint s (insert a t) ↔ a ∉ s ∧ disjoint s t :=
-disjoint_comm.trans $ by simp [disjoint_insert_left]
-
-theorem inter_eq_empty_iff_disjoint [decidable_eq α] {s t : finset α} : s ∩ t = ∅ ↔ disjoint s t :=
-by simp [ext, mem_inter]; refl
+_root_.disjoint_comm.trans $ by simp [disjoint_insert_left]
 
 @[simp] theorem disjoint_union_left [decidable_eq α] {s t u : finset α} :
   disjoint (s ∪ t) u ↔ disjoint s u ∧ disjoint t u :=
-by simp [disjoint, or_imp_distrib, forall_and_distrib]
+by simp [disjoint_left, or_imp_distrib, forall_and_distrib]
 
 @[simp] theorem disjoint_union_right [decidable_eq α] {s t u : finset α} :
   disjoint s (t ∪ u) ↔ disjoint s t ∧ disjoint s u :=
-by simp [disjoint, or_imp_distrib, forall_and_distrib]
-
-@[simp] theorem disjoint_val {s t : finset α} : multiset.disjoint s.1 t.1 ↔ disjoint s t :=
-by simp [disjoint, multiset.disjoint, mem_def]
+by simp [disjoint_right, or_imp_distrib, forall_and_distrib]
 
 @[simp] theorem card_disjoint_union [decidable_eq α] {s t : finset α} :
     disjoint s t → card (s ∪ t) = card s + card t :=

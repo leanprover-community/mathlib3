ca4d849cbce82ac0f15f11eb12d333d738ede0b9
Chris Hughes <chrishughes24@gmail.com>
2018-07-05 18:49:53 +0100

    feat(data/complex): use anonymous constructor and add mul_def and add_def.

--- data/complex/basic.lean
+++ data/complex/basic.lean
@@ -14,6 +14,8 @@ notation `ℂ` := complex
 
 namespace complex
 
+local attribute [pp_using_anonymous_constructor] complex
+
 @[simp] theorem eta : ∀ z : ℂ, complex.mk z.re z.im = z
 | ⟨a, b⟩ := rfl
 
@@ -56,6 +58,7 @@ def I : ℂ := ⟨0, 1⟩
 
 instance : has_add ℂ := ⟨λ z w, ⟨z.re + w.re, z.im + w.im⟩⟩
 
+@[simp] lemma add_def (a b c d : ℝ) : (⟨a, b⟩ : ℂ) + ⟨c, d⟩ = ⟨a + c, b + d⟩ := rfl
 @[simp] lemma add_re (z w : ℂ) : (z + w).re = z.re + w.re := rfl
 @[simp] lemma add_im (z w : ℂ) : (z + w).im = z.im + w.im := rfl
 @[simp] lemma of_real_add (r s : ℝ) : ((r + s : ℝ) : ℂ) = r + s := rfl
@@ -65,12 +68,14 @@ instance : has_add ℂ := ⟨λ z w, ⟨z.re + w.re, z.im + w.im⟩⟩
 
 instance : has_neg ℂ := ⟨λ z, ⟨-z.re, -z.im⟩⟩
 
+@[simp] lemma neg_def (a b : ℝ) : -(⟨a, b⟩ : ℂ) = ⟨-a, -b⟩ := rfl
 @[simp] lemma neg_re (z : ℂ) : (-z).re = -z.re := rfl
 @[simp] lemma neg_im (z : ℂ) : (-z).im = -z.im := rfl
 @[simp] lemma of_real_neg (r : ℝ) : ((-r : ℝ) : ℂ) = -r := ext_iff.2 $ by simp
 
 instance : has_mul ℂ := ⟨λ z w, ⟨z.re * w.re - z.im * w.im, z.re * w.im + z.im * w.re⟩⟩
 
+@[simp] lemma mul_def (a b c d : ℝ) : (⟨a, b⟩ : ℂ) * ⟨c, d⟩ = ⟨a * c - b * d, a * d + b * c⟩ := rfl
 @[simp] lemma mul_re (z w : ℂ) : (z * w).re = z.re * w.re - z.im * w.im := rfl
 @[simp] lemma mul_im (z w : ℂ) : (z * w).im = z.re * w.im + z.im * w.re := rfl
 @[simp] lemma of_real_mul (r s : ℝ) : ((r * s : ℝ) : ℂ) = r * s := ext_iff.2 $ by simp
@@ -83,6 +88,7 @@ ext_iff.2 $ by simp
 
 def conj (z : ℂ) : ℂ := ⟨z.re, -z.im⟩
 
+@[simp] lemma conj_def (a b : ℝ) : conj ⟨a, b⟩ = ⟨a, -b⟩ := rfl
 @[simp] lemma conj_re (z : ℂ) : (conj z).re = z.re := rfl
 @[simp] lemma conj_im (z : ℂ) : (conj z).im = -z.im := rfl
 
@@ -169,6 +175,7 @@ instance : comm_ring ℂ :=
 by refine { zero := 0, add := (+), neg := has_neg.neg, one := 1, mul := (*), ..};
    { intros, apply ext_iff.2; split; simp; ring }
 
+lemma sub_def (a b c d : ℝ) : (⟨a, b⟩ : ℂ) - ⟨c, d⟩ = ⟨a - c, b - d⟩ := rfl
 @[simp] lemma sub_re (z w : ℂ) : (z - w).re = z.re - w.re := rfl
 @[simp] lemma sub_im (z w : ℂ) : (z - w).im = z.im - w.im := rfl
 @[simp] lemma of_real_sub (r s : ℝ) : ((r - s : ℝ) : ℂ) = r - s := rfl
@@ -208,6 +215,9 @@ noncomputable instance : discrete_field ℂ :=
   inv_zero := inv_zero,
   has_decidable_eq := classical.dec_eq _,
   ..complex.comm_ring }
+
+@[simp] lemma of_real_pow (r : ℝ) (n : ℕ) : ((r ^ n : ℝ) : ℂ) = r ^ n :=
+by induction n; simp [pow_succ, *]
 
 @[simp] lemma of_real_div (r s : ℝ) : ((r / s : ℝ) : ℂ) = r / s :=
 by rw [division_def, of_real_mul, division_def, of_real_inv]

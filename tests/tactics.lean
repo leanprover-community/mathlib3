844c665b6e54685fe16a251e2f5972c37a8b36f3
Simon Hudon <simon.hudon@gmail.com>
2018-07-16 07:59:35 -0400

    feat(category/applicative): `id` and `comp` functors; proofs by `norm` (#184)

--- tests/tactics.lean
+++ tests/tactics.lean
@@ -3,7 +3,7 @@ Copyright (c) 2018 Simon Hudon. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Simon Hudon
 -/
-import tactic data.set.lattice
+import tactic data.set.lattice data.prod
 
 section solve_by_elim
 example {a b : Prop} (h₀ : a → b) (h₁ : a) : b :=

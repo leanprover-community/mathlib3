fed5274d3a820f0b828b24759a617c98bb67d544
Simon Hudon <simon.hudon@gmail.com>
2018-07-23 20:54:51 -0400

    feat(algebra/pi_instances): more automation

--- algebra/pi_instances.lean
+++ algebra/pi_instances.lean
@@ -6,35 +6,7 @@ Authors: Simon Hudon, Patrick Massot
 Pi instances for algebraic structures.
 -/
 
-import algebra.module order.basic
-
-namespace tactic
-
-open interactive interactive.types lean.parser
-open functor has_seq list nat tactic.interactive
-
-meta def derive_field : tactic unit :=
-do b ← target >>= is_prop,
-   if b then do
-     field ← get_current_field,
-     intros >> funext,
-     applyc field
-   else do
-     field ← get_current_field,
-     xs ← intros <* intro1,
-     applyc field,
-     xs.mmap' apply
-
-/-- `pi_instance [inst1,inst2]` constructs an instance of `my_class (Π i : I, f i)`
-    where we know `Π i, my_class (f i)` and where all non-propositional fields are
-    filled in by `inst1` and `inst2`
- -/
-meta def pi_instance : tactic unit :=
-refine_struct ``( { .. } ) ; try derive_field
-
-run_cmd add_interactive [`pi_instance]
-
-end tactic
+import algebra.module order.basic tactic.pi_instances
 
 namespace pi
 universes u v
@@ -80,25 +52,18 @@ instance vector_space (α) [field α] [∀ i, vector_space α $ f i] : vector_sp
 { ..pi.module }
 
 instance left_cancel_semigroup [∀ i, left_cancel_semigroup $ f i] : left_cancel_semigroup (Π i : I, f i) :=
-{ mul_left_cancel := λ a b c h, funext $ λ i, mul_left_cancel (congr_fun h i),
-  ..pi.semigroup }
+by pi_instance
 
 instance add_left_cancel_semigroup [∀ i, add_left_cancel_semigroup $ f i] : add_left_cancel_semigroup (Π i : I, f i) :=
-{ add_left_cancel := λ a b c h, funext $ λ i, add_left_cancel (congr_fun h i),
-  ..pi.add_semigroup }
+by pi_instance
 
 instance right_cancel_semigroup [∀ i, right_cancel_semigroup $ f i] : right_cancel_semigroup (Π i : I, f i) :=
-{ mul_right_cancel := λ a b c h, funext $ λ i, mul_right_cancel (congr_fun h i:_),
-  ..pi.semigroup }
+by pi_instance
 
 instance add_right_cancel_semigroup [∀ i, add_right_cancel_semigroup $ f i] : add_right_cancel_semigroup (Π i : I, f i) :=
-{ add_right_cancel := λ a b c h, funext $ λ i, add_right_cancel (congr_fun h i:_),
-  ..pi.add_semigroup }
+by pi_instance
 
 instance ordered_cancel_comm_monoid [∀ i, ordered_cancel_comm_monoid $ f i] : ordered_cancel_comm_monoid (Π i : I, f i) :=
-{ add_le_add_left := λ a b h c i, add_le_add_left' (h i),
-  le_of_add_le_add_left := λ a b c h i, le_of_add_le_add_left (h i),
-  ..pi.add_comm_monoid, ..pi.partial_order,
-  ..pi.add_left_cancel_semigroup, ..pi.add_right_cancel_semigroup }
+by pi_instance
 
 end pi

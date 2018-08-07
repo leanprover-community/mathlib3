8c9b38694a717299de4d0d638de5dc7672a45d2f
Scott Morrison <scott.morrison@anu.edu.au>
2018-08-07 16:25:53 +1000

    doc_strings

--- category_theory/products.lean
+++ category_theory/products.lean
@@ -8,11 +8,11 @@ namespace category_theory
 universes u₁ v₁ u₂ v₂ u₃ v₃ u₄ v₄
 
 section
-variable (C : Type u₁)
-variable [category.{u₁ v₁} C]
-variable (D : Type u₂)
-variable [category.{u₂ v₂} D]
+variables (C : Type u₁) [category.{u₁ v₁} C] (D : Type u₂) [category.{u₂ v₂} D]
 
+/--
+`product_category C D` gives the cartesian product of two categories.
+-/
 instance product_category : category.{(max u₁ u₂) (max v₁ v₂)} (C × D) := 
 { Hom     := λ X Y, ((X.1) ⟶ (Y.1)) × ((X.2) ⟶ (Y.2)),
   id      := λ X, ⟨ 𝟙 (X.1), 𝟙 (X.2) ⟩,
@@ -24,7 +24,7 @@ end
 
 namespace product_category
 
-section
+section -- rfl lemmas for product_category
 variables {C : Type u₁} [𝒞 : category.{u₁ v₁} C] {D : Type u₂} [𝒟 : category.{u₂ v₂} D]
 include 𝒞 𝒟
 
@@ -32,38 +32,45 @@ include 𝒞 𝒟
 @[simp,ematch] lemma comp {P Q R : C} {S T U : D} (f : (P, S) ⟶ (Q, T)) (g : (Q, T) ⟶ (R, U)) : f ≫ g = (f.1 ≫ g.1, f.2 ≫ g.2) := rfl
 end
 
-section -- Here we provide an addition instance when both factors have the same universe levels. This helps typeclass resolution.
+section 
 variables (C : Type u₁) [𝒞 : category.{u₁ v₁} C] (D : Type u₁) [𝒟 : category.{u₁ v₁} D]
 include 𝒞 𝒟
 
+/-- 
+`product_category.uniform C D` is an additional instance specialised so both factors have the same universe levels. This helps typeclass resolution.
+-/
 instance uniform : category.{u₁ v₁} (C × D) := category_theory.product_category C D
 end
 
 -- Next we define the natural functors into and out of product categories. For now this doesn't address the universal properties.
 
+/-- `right_injection_at C Z` is the functor `X ↦ (X, Z)`. -/
 definition right_injection_at (C : Type u₁) [category.{u₁ v₁} C] {D : Type u₁} [category.{u₁ v₁} D] (Z : D) : C ↝ (C × D) := 
-{ obj           := λ X, (X, Z),
-  map           := λ X Y f, (f, 𝟙 Z),
-  map_id        := begin /- `obviously'` says: -/ intros, refl end,
-  functoriality := begin /- `obviously'` says: -/ intros, dsimp, simp end }
+{ obj      := λ X, (X, Z),
+  map      := λ X Y f, (f, 𝟙 Z),
+  map_id   := begin /- `obviously'` says: -/ intros, refl end,
+  map_comp := begin /- `obviously'` says: -/ intros, dsimp, simp end }
 
+/-- `left_injection_at Z D` is the functor `X ↦ (Z, X)`. -/
 definition left_injection_at {C : Type u₁} [category.{u₁ v₁} C] (Z : C) (D : Type u₁) [category.{u₁ v₁} D] : D ↝ (C × D) := 
-{ obj           := λ X, (Z, X),
-  map           := λ X Y f, (𝟙 Z, f),
-  map_id        := begin /- `obviously'` says: -/ intros, refl end,
-  functoriality := begin /- `obviously'` says: -/ intros, dsimp, simp end }
+{ obj      := λ X, (Z, X),
+  map      := λ X Y f, (𝟙 Z, f),
+  map_id   := begin /- `obviously'` says: -/ intros, refl end,
+  map_comp := begin /- `obviously'` says: -/ intros, dsimp, simp end }
 
+/-- `left_projection` is the functor `(X, Y) ↦ X`. -/
 definition left_projection (C : Type u₁) [category.{u₁ v₁} C] (Z : C) (D : Type u₁) [category.{u₁ v₁} D] : (C × D) ↝ C := 
-{ obj           := λ X, X.1,
-  map           := λ X Y f, f.1,
-  map_id        := begin /- `obviously'` says: -/ intros, refl end,
-  functoriality := begin /- `obviously'` says: -/ intros, refl end }
+{ obj      := λ X, X.1,
+  map      := λ X Y f, f.1,
+  map_id   := begin /- `obviously'` says: -/ intros, refl end,
+  map_comp := begin /- `obviously'` says: -/ intros, refl end }
 
+/-- `right_projection` is the functor `(X, Y) ↦ Y`. -/
 definition right_projection (C : Type u₁) [category.{u₁ v₁} C] (Z : C) (D : Type u₁) [category.{u₁ v₁} D] : (C × D) ↝ D := 
-{ obj           := λ X, X.2,
-  map           := λ X Y f, f.2,
-  map_id        := begin /- `obviously'` says: -/ intros, refl end,
-  functoriality := begin /- `obviously'` says: -/ intros, refl end }
+{ obj      := λ X, X.2,
+  map      := λ X Y f, f.2,
+  map_id   := begin /- `obviously'` says: -/ intros, refl end,
+  map_comp := begin /- `obviously'` says: -/ intros, refl end }
 
 end product_category
 
@@ -71,11 +78,12 @@ variables {A : Type u₁} [𝒜 : category.{u₁ v₁} A] {B : Type u₂} [ℬ :
 include 𝒜 ℬ 𝒞 𝒟
 
 namespace functor
+/-- The cartesion product of two functors. -/
 definition product (F : A ↝ B) (G : C ↝ D) : (A × C) ↝ (B × D) :=
 { obj := λ X, (F X.1, G X.2),
   map := λ _ _ f, (F.map f.1, G.map f.2),
-  map_id    := begin /- `obviously'` says: -/ intros, cases X, dsimp, rw Functor.map_id_lemma, rw Functor.map_id_lemma end,
-  functoriality := begin /- `obviously'` says: -/ intros, cases Z, cases Y, cases X, cases f, cases g, dsimp at *, rw Functor.functoriality_lemma, rw Functor.functoriality_lemma end }
+  map_id   := begin /- `obviously'` says: -/ intros, cases X, dsimp, rw map_id_lemma, rw map_id_lemma end,
+  map_comp := begin /- `obviously'` says: -/ intros, cases Z, cases Y, cases X, cases f, cases g, dsimp at *, rw map_comp_lemma, rw map_comp_lemma end }
 
 notation F `×` G := product F G
 
@@ -85,16 +93,18 @@ namespace product
 end product
 end functor
 
-namespace natural_transformation
+namespace nat_trans
+
+/-- The cartesian product of two natural transformations. -/
 definition product {F G : A ↝ B} {H I : C ↝ D} (α : F ⟹ G) (β : H ⟹ I) : (F × H) ⟹ (G × I) :=
-{ components := λ X, (α X.1, β X.2),
-  naturality := begin /- `obviously'` says: -/ intros, cases f, cases Y, cases X, dsimp at *, simp, split, rw NaturalTransformation.naturality_lemma, rw NaturalTransformation.naturality_lemma end }
+{ app        := λ X, (α X.1, β X.2),
+  naturality := begin /- `obviously'` says: -/ intros, cases f, cases Y, cases X, dsimp at *, simp, split, rw naturality_lemma, rw naturality_lemma end }
 
 notation α `×` β := product α β
 
 namespace product
-@[simp,ematch] lemma components {F G : A ↝ B} {H I : C ↝ D} (α : F ⟹ G) (β : H ⟹ I) (a : A) (c : C) : (α × β) (a, c) = (α a, β c) := rfl
+@[simp,ematch] lemma app {F G : A ↝ B} {H I : C ↝ D} (α : F ⟹ G) (β : H ⟹ I) (a : A) (c : C) : (α × β) (a, c) = (α a, β c) := rfl
 end product
-end natural_transformation
+end nat_trans
 
 end category_theory

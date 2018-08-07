9b1be732e122d371100b0df479ca000c2a3f73b0
Scott Morrison <scott@tqft.net>
2018-08-07 20:43:17 +1000

    feat(category_theory): basic definitions (#152)
    * feat(category_theory): basic definitions
    
    * fixing formatting in documentation
    
    * corrections from review
    
    * removing second ematch attribute on associativity_lemma
    
    * being slightly more careful about variables
    
    (I don't think there were any actual errors,
    but I was sometimes using an argument
    when there was a variable of the same
    name available.)
    
    * fix(notation): transformation components
    
    Using `@>` per @rwbarton's suggestion.
    
    * fix(notation): more conventional notation for composition
    
    * adjusting namespaces, capitalisation, and headers
    
    * move functor/default.lean to functor.lean
    
    (Later PRs will add more files to the functor/ directory, but that can wait.)
    
    * oops
    
    * fixing indentation
    
    * namespace for instances
    
    * removing unnecessary `@>` notation for natural transformations
    
    * renaming, namespacing, and removing a spurious attribute
    
    * better naming, namespaces, and coercions
    
    * updating documentation
    
    * renaming id
    
    * reordering definitions
    
    * rfl lemmas for has_one
    
    * formatting
    
    * formatting
    
    * renaming: snake_case
    
    * renaming coe rfl lemmas
    
    * functoriality -> map_comp
    
    * rfl lemmas for identity C and identity F (reducing both to `1`)
    
    * renaming ext lemma to `ext`
    
    * rename `natural_transformation` to `nat_trans`
    
    * rename `make_lemma` to `restate_axiom`
    
    * renaming nat_trans.components to nat_trans.app
    
    * oops, fix import
    
    * adding doc_comments, and `protected`
    
    * formatting
    
    * removing `has_one` instances, per zulip chat, and adding a `vcomp.assoc` lemma
    
    * removing the attribute that `restate_axiom` used to add
    
    (it was causing a problem on travis, but not locally? anyway it's useless)
    
    * fixing names

---
+++ category_theory/category.lean
@@ -0,0 +1,66 @@
+/-
+Copyright (c) 2017 Scott Morrison. All rights reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
+Authors: Stephen Morgan, Scott Morrison
+
+Defines a category, as a typeclass parametrised by the type of objects.
+Introduces notations
+  `X ⟶ Y` for the morphism spaces,
+  `f ≫ g` for composition in the 'arrows' convention.
+
+Users may like to add `f ⊚ g` for composition in the standard convention, using
+```
+local notation f ` ⊚ `:80 g:80 := category.comp g f    -- type as \oo
+```
+-/
+
+import tactic.restate_axiom
+import tactic.interactive
+
+namespace category_theory
+
+universes u v
+
+meta def obviously := `[skip]  
+
+/- 
+The propositional fields of `category` are annotated with the auto_param `obviously`, which is just a synonym for `skip`.
+Actually, there is a tactic called `obviously` which is not part of this pull request, which should be used here. It successfully
+discharges a great many of these goals. For now, proofs which could be provided entirely by `obviously` (and hence omitted entirely
+and discharged by an auto_param), are all marked with a comment "-- obviously says:".
+-/
+
+/--
+The typeclass `category C` describes morphisms associated to objects of type `C`.
+The universe levels of the objects and morphisms are unconstrained, and will often need to be specified explicitly, as `category.{u v} C`. (See also `large_category` and `small_category`.)
+-/
+class category (Obj : Type u) : Type (max u (v+1)) :=
+(Hom     : Obj → Obj → Type v)
+(id      : Π X : Obj, Hom X X)
+(comp    : Π {X Y Z : Obj}, Hom X Y → Hom Y Z → Hom X Z)
+(id_comp : ∀ {X Y : Obj} (f : Hom X Y), comp (id X) f = f . obviously)
+(comp_id : ∀ {X Y : Obj} (f : Hom X Y), comp f (id Y) = f . obviously)
+(assoc   : ∀ {W X Y Z : Obj} (f : Hom W X) (g : Hom X Y) (h : Hom Y Z), comp (comp f g) h = comp f (comp g h) . obviously)
+
+notation `𝟙` := category.id -- type as \b1
+infixr ` ≫ `:80 := category.comp -- type as \gg
+infixr ` ⟶ `:10 := category.Hom -- type as \h
+
+-- restate_axiom is a command that creates a lemma from a structure field, discarding any auto_param wrappers from the type.
+restate_axiom category.id_comp
+restate_axiom category.comp_id
+restate_axiom category.assoc
+-- We tag some lemmas with the attribute `@[ematch]`, for later automation. (I'd be happy to change this to e.g. `@[search]`.)
+attribute [simp,ematch] category.id_comp_lemma category.comp_id_lemma category.assoc_lemma 
+
+/--
+A `large_category` has objects in one universe level higher than the universe level of the morphisms. 
+It is useful for examples such as the category of types, or the category of groups, etc.
+-/
+abbreviation large_category (C : Type (u+1)) : Type (u+1) := category.{u+1 u} C
+/--
+A `small_category` has objects and morphisms in the same universe level.
+-/
+abbreviation small_category (C : Type u)     : Type (u+1) := category.{u u} C
+
+end category_theory
---
+++ category_theory/natural_transformation.lean
@@ -0,0 +1,117 @@
+/-
+Copyright (c) 2017 Scott Morrison. All rights reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
+Authors: Tim Baumann, Stephen Morgan, Scott Morrison
+
+Defines natural transformations between functors.
+
+Introduces notations
+  `F ⟹ G` for the type of natural transformations between functors `F` and `G`,
+  `τ X` (a coercion) for the components of natural transformations,
+  `σ ⊟ τ` for vertical compositions, and
+  `σ ◫ τ` for horizontal compositions.
+-/
+
+import .functor
+
+namespace category_theory
+
+universes u₁ v₁ u₂ v₂ u₃ v₃ u₄ v₄
+
+variables {C : Type u₁} [𝒞 : category.{u₁ v₁} C] {D : Type u₂} [𝒟 : category.{u₂ v₂} D]
+include 𝒞 𝒟
+
+/--
+`nat_trans F G` represents a natural transformation between functors `F` and `G`.
+
+The field `app` provides the components of the natural transformation, and there is a
+coercion available so you can write `α X` for the component of a transformation `α` at an object `X`.
+
+Naturality is expressed by `α.naturality_lemma`.
+-/
+structure nat_trans (F G : C ↝ D) : Type (max u₁ v₂) :=
+(app : Π X : C, (F X) ⟶ (G X))
+(naturality : ∀ {X Y : C} (f : X ⟶ Y), (F.map f) ≫ (app Y) = (app X) ≫ (G.map f) . obviously)
+
+restate_axiom nat_trans.naturality
+attribute [ematch] nat_trans.naturality_lemma
+
+infixr ` ⟹ `:50  := nat_trans             -- type as \==> or ⟹
+
+namespace nat_trans
+
+instance {F G : C ↝ D} : has_coe_to_fun (F ⟹ G) :=
+{ F   := λ α, Π X : C, (F X) ⟶ (G X),
+  coe := λ α, α.app }
+
+@[simp] lemma coe_def {F G : C ↝ D} (α : F ⟹ G) (X : C) : α X = α.app X := rfl
+
+/-- `nat_trans.id F` is the identity natural transformation on a functor `F`. -/
+protected definition id (F : C ↝ D) : F ⟹ F := 
+{ app        := λ X, 𝟙 (F X),
+  naturality := begin /- `obviously'` says: -/ intros, dsimp, simp end }
+
+@[simp] lemma id_app (F : C ↝ D) (X : C) : (nat_trans.id F) X = 𝟙 (F X) := rfl
+
+open category
+open category_theory.functor
+
+section
+variables {F G H I : C ↝ D}
+
+-- We'll want to be able to prove that two natural transformations are equal if they are componentwise equal.
+@[extensionality] lemma ext (α β : F ⟹ G) (w : ∀ X : C, α X = β X) : α = β :=
+begin
+  induction α with α_components α_naturality,
+  induction β with β_components β_naturality,
+  have hc : α_components = β_components := funext w,
+  subst hc
+end
+
+/-- `vcomp α β` is the vertical compositions of natural transformations. -/
+definition vcomp (α : F ⟹ G) (β : G ⟹ H) : F ⟹ H := 
+{ app        := λ X, (α X) ≫ (β X),
+  naturality := begin /- `obviously'` says: -/ intros, simp, rw [←assoc_lemma, naturality_lemma, assoc_lemma, ←naturality_lemma], end }
+
+notation α `⊟` β:80 := vcomp α β    
+
+@[simp] lemma vcomp_app (α : F ⟹ G) (β : G ⟹ H) (X : C) : (α ⊟ β) X = (α X) ≫ (β X) := rfl
+@[ematch] lemma vcomp_assoc (α : F ⟹ G) (β : G ⟹ H) (γ : H ⟹ I) : (α ⊟ β) ⊟ γ = (α ⊟ (β ⊟ γ)) := begin ext, intros, dsimp, rw [assoc] end
+end
+
+variables {E : Type u₃} [ℰ : category.{u₃ v₃} E]
+include ℰ
+
+/-- `hcomp α β` is the horizontal composition of natural transformations. -/
+definition hcomp {F G : C ↝ D} {H I : D ↝ E} (α : F ⟹ G) (β : H ⟹ I) : (F ⋙ H) ⟹ (G ⋙ I) :=
+{ app        := λ X : C, (β (F X)) ≫ (I.map (α X)), 
+  naturality := begin 
+                  /- `obviously'` says: -/
+                  intros,
+                  dsimp,
+                  simp,
+                  -- Actually, obviously doesn't use exactly this sequence of rewrites, but achieves the same result
+                  rw [← assoc_lemma, naturality_lemma, assoc_lemma],
+                  conv { to_rhs, rw [← map_comp_lemma, ← α.naturality_lemma, map_comp_lemma] }
+                end }
+
+notation α `◫` β:80 := hcomp α β
+
+@[simp] lemma hcomp_app {F G : C ↝ D} {H I : D ↝ E} (α : F ⟹ G) (β : H ⟹ I) (X : C) : (α ◫ β) X = (β (F X)) ≫ (I.map (α X)) := rfl
+
+-- Note that we don't yet prove a `hcomp_assoc` lemma here: even stating it is painful, because we need to use associativity of functor composition
+
+@[ematch] lemma exchange {F G H : C ↝ D} {I J K : D ↝ E} (α : F ⟹ G) (β : G ⟹ H) (γ : I ⟹ J) (δ : J ⟹ K) : ((α ⊟ β) ◫ (γ ⊟ δ)) = ((α ◫ γ) ⊟ (β ◫ δ)) := 
+begin
+  -- `obviously'` says:
+  ext,
+  intros,
+  dsimp,
+  simp,
+  -- again, this isn't actually what obviously says, but it achieves the same effect.
+  conv { to_lhs, congr, skip, rw [←assoc_lemma, ←naturality_lemma, assoc_lemma] }
+end
+
+end nat_trans
+
+end category_theory

a995a3ad42d6a6e94a022005d4f4182c13dfb208
Mario Carneiro <di.gama@gmail.com>
2018-08-09 11:18:39 -0400

    perf(tactic/replacer): use if instead of match

--- tactic/replacer.lean
+++ tactic/replacer.lean
@@ -17,26 +17,24 @@ meta def replacer_attr (ntac : name) : user_attribute :=
   "can optionally have an argument of type `tactic unit` or " ++
   "`option (tactic unit)` which refers to the previous definition, if any.",
   after_set := some $ λ n _ _, do d ← get_decl n,
-    match d.type with
-    | `(tactic unit) := skip
-    | `(tactic unit → tactic unit) := skip
-    | `(option (tactic unit) → tactic unit) := skip
-    | _ := fail format!"incorrect type for @[{ntac}]"
-    end }
+    when (d.type ∉ [
+      (`(tactic unit) : expr),
+      `(tactic unit → tactic unit),
+      `(option (tactic unit) → tactic unit)]) $
+    fail format!"incorrect type for @[{ntac}]" }
 
 meta def replacer_core (ntac : name) : list name → tactic unit
 | [] := fail ("no implementation defined for " ++ to_string ntac)
-| (n::ns) := do d ← get_decl n,
-  match d.type with
-  | `(tactic unit) := monad.join (mk_const n >>= eval_expr (tactic unit))
-  | `(tactic unit → tactic unit) :=
+| (n::ns) := do d ← get_decl n, let t := d.type,
+  if t = `(tactic unit) then
+    monad.join (mk_const n >>= eval_expr (tactic unit))
+  else if t = `(tactic unit → tactic unit) then
     do tac ← mk_const n >>= eval_expr (tactic unit → tactic unit),
        tac (replacer_core ns)
-  | `(option (tactic unit) → tactic unit) :=
+  else if t = `(option (tactic unit) → tactic unit) then
     do tac ← mk_const n >>= eval_expr (option (tactic unit) → tactic unit),
        tac (guard (ns ≠ []) >> some (replacer_core ns))
-  | _ := failed
-  end
+  else failed
 
 meta def replacer (ntac : name) : tactic unit :=
 attribute.get_instances ntac >>= replacer_core ntac

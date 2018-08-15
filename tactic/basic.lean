1aae37cbd8791ce7a0af03308008b964f4e6641f
Mario Carneiro <di.gama@gmail.com>
2018-08-15 18:16:17 -0400

    refactor(tactic/basic): move meta.expr to tactic.basic, cleanup

--- tactic/basic.lean
+++ tactic/basic.lean
@@ -1,9 +1,8 @@
 /-
 Copyright (c) 2018 Mario Carneiro. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
-Authors: Mario Carneiro
+Authors: Mario Carneiro, Simon Hudon
 -/
-
 import data.dlist.basic category.basic
 
 namespace name
@@ -44,6 +43,28 @@ protected meta def of_int (α : expr) : ℤ → tactic expr
 | -[1+ n] := do
   e ← expr.of_nat α (n+1),
   tactic.mk_app ``has_neg.neg [e]
+
+meta def list_local_const (e : expr) : list expr :=
+e.fold [] (λ e' _ es, if is_local_constant e' then insert e' es else es)
+
+/- only traverses the direct descendents -/
+meta def {u} traverse {m : Type → Type u} [monad m]
+  {elab elab' : bool} (f : expr elab → m (expr elab')) :
+  expr elab → m (expr elab')
+ | (var v)  := pure $ var v
+ | (sort l) := pure $ sort l
+ | (const n ls) := pure $ const n ls
+ | (mvar n n' e) := mvar n n' <$> f e
+ | (local_const n n' bi e) := local_const n n' bi <$> f e
+ | (app e₀ e₁) := app <$> f e₀ <*> f e₁
+ | (lam n bi e₀ e₁) := lam n bi <$> f e₀ <*> f e₁
+ | (pi n bi e₀ e₁) := pi n bi <$> f e₀ <*> f e₁
+ | (elet n e₀ e₁ e₂) := elet n <$> f e₀ <*> f e₁ <*> f e₂
+ | (macro mac es) := macro mac <$> mmap f es
+
+meta def mfoldl {α : Type} {m} [monad m] (f : α → expr → m α) : α → expr → m α
+| x e := prod.snd <$> (state_t.run (e.traverse $ λ e',
+    (get >>= monad_lift ∘ flip f e' >>= put) $> e') x : m _)
 
 end expr
 
@@ -173,6 +194,41 @@ meta def set_binder : expr → list binder_info → expr
 | e [] := e
 | (expr.pi v _ d b) (bi :: bs) := expr.pi v bi d (set_binder b bs)
 | e _ := e
+
+meta def pis : list expr → expr → tactic expr
+| (e@(expr.local_const uniq pp info _) :: es) f := do
+  t ← infer_type e,
+  f' ← pis es f,
+  pure $ expr.pi pp info t (expr.abstract_local f' uniq)
+| _ f := pure f
+
+meta def last_explicit_arg : expr → tactic expr
+| (expr.app f e) :=
+do t ← infer_type f >>= whnf,
+   if t.binding_info = binder_info.default
+     then pure e
+     else last_explicit_arg f
+| e := pure e
+
+private meta def get_expl_pi_arity_aux : expr → tactic nat
+| (expr.pi n bi d b) :=
+  do m     ← mk_fresh_name,
+     let l := expr.local_const m n bi d,
+     new_b ← whnf (expr.instantiate_var b l),
+     r     ← get_expl_pi_arity_aux new_b,
+     if bi = binder_info.default then
+       return (r + 1)
+     else
+       return r
+| e := return 0
+
+/-- Compute the arity of explicit arguments of the given (Pi-)type -/
+meta def get_expl_pi_arity (type : expr) : tactic nat :=
+whnf type >>= get_expl_pi_arity_aux
+
+/-- Compute the arity of explicit arguments of the given function -/
+meta def get_expl_arity (fn : expr) : tactic nat :=
+infer_type fn >>= get_expl_pi_arity
 
 /-- variation on `assert` where a (possibly incomplete)
     proof of the assertion is provided as a parameter.

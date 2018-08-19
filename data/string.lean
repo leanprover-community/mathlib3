4b2aca4fa91e06a10fb0900ee300a4fd1104aa7e
Patrick Massot <patrickmassot@free.fr>
2018-08-19 20:54:19 +0200

    feat(data/string) adds split_on and map_tokens

--- data/string.lean
+++ data/string.lean
@@ -67,4 +67,10 @@ by refine_struct {
     decidable_eq := by apply_instance, .. };
   { simp [-not_le], introv, apply_field }
 
+def split_on (c : char) : string → list string :=
+split (= c)
+
+def map_tokens (c : char) (f : list string → list string) : string → string :=
+intercalate (singleton c) ∘ f ∘ split (= c)
+
 end string

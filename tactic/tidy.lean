d6d75d12f17d1a0a9cae769c731fd119e507b265
Scott Morrison <scott@tqft.net>
2018-08-28 20:06:53 +0200

    namespaces

--- tactic/tidy.lean
+++ tactic/tidy.lean
@@ -7,26 +7,32 @@ import tactic.auto_cases
 import tactic.chain
 import tactic.interactive
 
-open tactic
+namespace tactic
 
+namespace tidy
 meta def tidy_attribute : user_attribute := {
   name := `tidy,
   descr := "A tactic that should be called by `tidy`."
 }
 
-run_cmd attribute.register `tidy_attribute
+run_cmd attribute.register ``tidy_attribute
 
 meta def name_to_tactic (n : name) : tactic (tactic string) := 
-do e ← mk_const n,
-   (eval_expr (tactic string) e) <|> 
-   (eval_expr (tactic unit) e) >>= (λ t, pure (t >> pure n.to_string))
+do d ← get_decl n,
+   e ← mk_const n,
+   let t := d.type,
+   if (t =ₐ `(tactic unit)) then 
+     (eval_expr (tactic unit) e) >>= (λ t, pure (t >> pure n.to_string))
+   else if (t =ₐ `(tactic string)) then
+     (eval_expr (tactic string) e)
+   else fail "invalid type for @[tidy] tactic"
 
-meta def run_tidy_tactics : tactic string :=
+meta def run_tactics : tactic string :=
 do names ← attribute.get_instances `tidy,
    tactics ← names.mmap name_to_tactic,
    first tactics <|> fail "no @[tidy] tactics succeeded"
 
-meta def default_tidy_tactics : list (tactic string) :=
+meta def default_tactics : list (tactic string) :=
 [ reflexivity                                 >> pure "refl", 
   `[exact dec_trivial]                        >> pure "exact dec_trivial",
   propositional_goal >> assumption            >> pure "assumption",
@@ -40,28 +46,33 @@ meta def default_tidy_tactics : list (tactic string) :=
   injections_and_clear                        >> pure "injections_and_clear",
   propositional_goal >> (`[solve_by_elim])    >> pure "solve_by_elim",
   `[unfold_aux]                               >> pure "unfold_aux",
-  run_tidy_tactics ]
+  tidy.run_tactics ]
 
-meta structure tidy_cfg :=
+meta structure cfg :=
 (trace_result : bool            := ff)
-(trace_result_prefix : string   := "/- obviously says -/ ")
-(tactics : list (tactic string) := default_tidy_tactics)
+(trace_result_prefix : string   := "/- `tidy` says -/ ")
+(tactics : list (tactic string) := default_tactics)
 
 declare_trace tidy
 
-meta def tidy_core (cfg : tidy_cfg := {}) : tactic (list string) :=
+meta def core (cfg : cfg := {}) : tactic (list string) :=
 do
   results ← chain cfg.tactics,
-  if cfg.trace_result ∨ is_trace_enabled_for `tidy then
-    trace (cfg.trace_result_prefix ++ (", ".intercalate results))
-  else
-    tactic.skip,
+  when (cfg.trace_result ∨ is_trace_enabled_for `tidy) $
+    trace (cfg.trace_result_prefix ++ (", ".intercalate results)),
   return results
 
-meta def tidy (cfg : tidy_cfg := {}) := tidy_core cfg >> skip
+end tidy
+
+meta def tidy (cfg : tidy.cfg := {}) := tactic.tidy.core cfg >> skip
+
+namespace interactive
+meta def tidy (cfg : tidy.cfg := {}) := tactic.tidy
+end interactive
 
 @[hole_command] meta def tidy_hole_cmd : hole_command :=
 { name := "tidy", 
   descr := "Use `tidy` to complete the goal.",
-  action := λ _, do script ← tidy_core, return [("begin " ++ (", ".intercalate script) ++ " end", "by tidy")] }
+  action := λ _, do script ← tidy.core, return [("begin " ++ (", ".intercalate script) ++ " end", "by tidy")] }
 
+end tactic

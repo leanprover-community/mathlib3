73bb1766d2c864bf7baf3758ced5ff4d6274ac40
Patrick Massot <patrickmassot@free.fr>
2018-08-28 23:03:20 +0200

    feat(algebra/group): is_add_group_hom

--- algebra/group.lean
+++ algebra/group.lean
@@ -639,3 +639,30 @@ end is_group_anti_hom
 
 theorem inv_is_group_anti_hom [group α] : is_group_anti_hom (λ x : α, x⁻¹) :=
 ⟨mul_inv_rev⟩
+
+variables {γ : Type*} {δ : Type*} [add_group γ] [add_group δ]
+class is_add_group_hom  (f : γ → δ) : Prop :=
+(add : ∀ a b, f (a + b) = f a + f b)
+
+namespace is_add_group_hom
+
+variables (f : γ → δ) [is_add_group_hom f]
+
+theorem zero : f 0 = 0 :=
+add_self_iff_eq_zero.1 $ by simp [(add f 0 0).symm]
+
+
+lemma neg (a) : f (-a) = -(f a) :=
+eq.symm $ neg_eq_of_add_eq_zero $ by simp [(add f a (-a)).symm, zero f]
+
+lemma sub (a b) : f (a - b) = f a - f b :=
+calc f (a - b) = f (a + -b)   : rfl
+           ... = f a + f (-b) : add f _ _
+           ... = f a - f b    : by  simp[neg f]
+
+instance comp {ε} [add_group ε] (g : δ → ε) [is_add_group_hom g] :
+  is_add_group_hom (g ∘ f) :=
+⟨λ x y, calc
+  g (f (x + y)) = g (f x + f y)       : by rw add f
+  ...           = g (f x) + g (f y)   : by rw add g⟩
+end is_add_group_hom

19d22ced6134a5d75497cf384ce8b10d60ad874b
Rob Lewis <rob.y.lewis@gmail.com>
2018-08-29 19:08:07 +0200

    chore(tactic/linarith): add copyright

--- tactic/linarith.lean
+++ tactic/linarith.lean
@@ -1,3 +1,11 @@
+/-
+Copyright (c) 2018 Robert Y. Lewis. All rights reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
+Author: Robert Y. Lewis
+
+A tactic for discharging linear arithmetic goals.
+-/
+
 import tactic.ring data.nat.gcd data.list.basic meta.rb_map 
 
 open native 

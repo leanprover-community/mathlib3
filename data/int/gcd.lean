20b4143976dad48e664c4847b75a85237dca0a89
Johannes Hölzl <johannes.hoelzl@gmx.de>
2018-08-30 15:02:10 +0200

    feat(algebra): add normalization and GCD domain; setup for int

--- data/int/gcd.lean
+++ data/int/gcd.lean
@@ -1,13 +1,17 @@
 /-
 Copyright (c) 2018 Guy Leroy. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
-Authors: Sangwoo Jo (aka Jason), Guy Leroy
+Authors: Sangwoo Jo (aka Jason), Guy Leroy, Johannes Hölzl
 
 Greatest common divisor (gcd) and least common multiple (lcm) for integers.
--/
-import data.int.basic data.nat.prime
 
-/- extended euclidean algorithm -/
+This sets up ℤ to be a GCD domain, and introduces rules about `int.gcd`, as well as `int.lcm`.
+NOTE: If you add rules to this theory, check that the corresponding rules are available for
+`gcd_domain`.
+-/
+import data.int.basic data.nat.prime algebra.gcd_domain
+
+/- Extended Euclidean algorithm -/
 namespace nat
 
 def xgcd_aux : ℕ → ℤ → ℤ → ℕ → ℤ → ℤ → ℕ × ℤ × ℤ
@@ -98,48 +102,100 @@ dvd.elim H (λl H1, by rw mul_assoc at H1; exact ⟨_, eq_of_mul_eq_mul_left k_n
 theorem dvd_of_mul_dvd_mul_right {i j k : ℤ} (k_non_zero : k ≠ 0) (H : i * k ∣ j * k) : i ∣ j :=
 by rw [mul_comm i k, mul_comm j k] at H; exact dvd_of_mul_dvd_mul_left k_non_zero H
 
-/- gcd -/
+section normalization_domain
 
-@[simp] theorem gcd_self (i : ℤ) : gcd i i = nat_abs i :=
-by cases i; simp [gcd, mod_self]
+instance : normalization_domain ℤ :=
+{ norm_unit      := λa:ℤ, if 0 ≤ a then 1 else -1,
+  norm_unit_zero := if_pos (le_refl _),
+  norm_unit_mul  := assume a b hna hnb,
+  begin
+    by_cases ha : 0 ≤ a; by_cases hb : 0 ≤ b; simp [ha, hb],
+    exact if_pos (mul_nonneg ha hb),
+    exact if_neg (assume h, hb $ nonneg_of_mul_nonneg_left h $ lt_of_le_of_ne ha hna.symm),
+    exact if_neg (assume h, ha $ nonneg_of_mul_nonneg_right h $ lt_of_le_of_ne hb hnb.symm),
+    exact if_pos (mul_nonneg_of_nonpos_of_nonpos (le_of_not_ge ha) (le_of_not_ge hb))
+  end,
+  norm_unit_coe_units := assume u, (units_eq_one_or u).elim
+    (assume eq, eq.symm ▸ if_pos zero_le_one)
+    (assume eq, eq.symm ▸ if_neg (not_le_of_gt $ show (-1:ℤ) < 0, by simpa [@neg_lt ℤ _ 1 0])),
+  .. (infer_instance : integral_domain ℤ) }
+
+lemma norm_unit_of_nonneg {z : ℤ} (h : 0 ≤ z) : norm_unit z = 1 :=
+by unfold norm_unit; exact if_pos h
+
+lemma norm_unit_of_neg {z : ℤ} (h : z < 0) : norm_unit z = -1 :=
+by unfold norm_unit; exact if_neg (not_le_of_gt h)
+
+lemma norm_unit_nat_coe (n : ℕ) : norm_unit (n : ℤ) = 1 :=
+norm_unit_of_nonneg (coe_nat_le_coe_nat_of_le $ nat.zero_le n)
+
+theorem coe_nat_abs_eq_mul_norm_unit {z : ℤ} : (z.nat_abs : ℤ) = z * norm_unit z :=
+begin
+  by_cases 0 ≤ z,
+  { simp [nat_abs_of_nonneg h, norm_unit_of_nonneg h] },
+  { simp [of_nat_nat_abs_of_nonpos (le_of_not_ge h), norm_unit_of_neg (lt_of_not_ge h)] }
+end
+
+end normalization_domain
+
+/-- ℤ specific version of least common multiple. -/
+def lcm (i j : ℤ) : ℕ := nat.lcm (nat_abs i) (nat_abs j)
+
+theorem lcm_def (i j : ℤ) : lcm i j = nat.lcm (nat_abs i) (nat_abs j) := rfl
+
+section gcd_domain
+
+theorem gcd_dvd_left (i j : ℤ) : (gcd i j : ℤ) ∣ i :=
+dvd_nat_abs.mp $ coe_nat_dvd.mpr $ nat.gcd_dvd_left _ _
+
+theorem gcd_dvd_right (i j : ℤ) : (gcd i j : ℤ) ∣ j :=
+dvd_nat_abs.mp $ coe_nat_dvd.mpr $ nat.gcd_dvd_right _ _
+
+theorem dvd_gcd {i j k : ℤ} (h1 : k ∣ i) (h2 : k ∣ j) : k ∣ gcd i j :=
+nat_abs_dvd.1 $ coe_nat_dvd.2 $ nat.dvd_gcd (nat_abs_dvd_abs_iff.2 h1) (nat_abs_dvd_abs_iff.2 h2)
+
+theorem gcd_mul_lcm (i j : ℤ) : gcd i j * lcm i j = nat_abs (i * j) :=
+by rw [int.gcd, int.lcm, nat.gcd_mul_lcm, nat_abs_mul]
+
+instance : gcd_domain ℤ :=
+{ gcd            := λa b, int.gcd a b,
+  lcm            := λa b, int.lcm a b,
+  gcd_dvd_left   := assume a b, int.gcd_dvd_left _ _,
+  gcd_dvd_right  := assume a b, int.gcd_dvd_right _ _,
+  dvd_gcd        := assume a b c, dvd_gcd,
+  norm_unit_gcd  := assume a b, norm_unit_nat_coe _,
+  gcd_mul_lcm    := by intros; rw [← int.coe_nat_mul, gcd_mul_lcm, coe_nat_abs_eq_mul_norm_unit],
+  lcm_zero_left  := assume a, coe_nat_eq_zero.2 $ nat.lcm_zero_left _,
+  lcm_zero_right := assume a, coe_nat_eq_zero.2 $ nat.lcm_zero_right _,
+  .. int.normalization_domain }
+
+lemma coe_gcd (i j : ℤ) : ↑(int.gcd i j) = gcd_domain.gcd i j := rfl
+lemma coe_lcm (i j : ℤ) : ↑(int.lcm i j) = gcd_domain.lcm i j := rfl
+
+lemma nat_abs_gcd (i j : ℤ) : nat_abs (gcd_domain.gcd i j) = int.gcd i j := rfl
+lemma nat_abs_lcm (i j : ℤ) : nat_abs (gcd_domain.lcm i j) = int.lcm i j := rfl
+
+end gcd_domain
+
+theorem gcd_comm (i j : ℤ) : gcd i j = gcd j i := nat.gcd_comm _ _
+
+theorem gcd_assoc (i j k : ℤ) : gcd (gcd i j) k = gcd i (gcd j k) := nat.gcd_assoc _ _ _
+
+@[simp] theorem gcd_self (i : ℤ) : gcd i i = nat_abs i := by simp [gcd]
 
 @[simp] theorem gcd_zero_left (i : ℤ) : gcd 0 i = nat_abs i := by simp [gcd]
 
-@[simp] theorem gcd_zero_right (i : ℤ) : gcd i 0 = nat_abs i :=
-by cases i; simp [gcd]
-
-theorem gcd_dvd_left (i j : ℤ) : (gcd i j : ℤ) ∣ i :=
-dvd_nat_abs.mp (coe_nat_dvd.mpr (nat.gcd_dvd_left (nat_abs i) (nat_abs j)))
-
-theorem gcd_dvd_right (i j : ℤ) : (gcd i j : ℤ) ∣ j :=
-dvd_nat_abs.mp (coe_nat_dvd.mpr (nat.gcd_dvd_right (nat_abs i) (nat_abs j)))
-
-theorem gcd_dvd (i j : ℤ) : ((gcd i j : ℤ) ∣ i) ∧ ((gcd i j : ℤ) ∣ j) :=
-⟨gcd_dvd_left i j, gcd_dvd_right i j⟩
-
-theorem dvd_gcd {i j k : ℤ} : k ∣ i → k ∣ j → k ∣ gcd i j :=
-by intros H1 H2;
-exact nat_abs_dvd.mp (coe_nat_dvd.mpr (nat.dvd_gcd (nat_abs_dvd_abs_iff.mpr H1)
-                                                   (nat_abs_dvd_abs_iff.mpr H2)))
-
-theorem gcd_comm (i j : ℤ) : gcd i j = gcd j i :=
-nat.gcd_comm (nat_abs i) (nat_abs j)
-
-theorem gcd_assoc (i j k : ℤ) : gcd (gcd i j) k = gcd i (gcd j k) :=
-nat.gcd_assoc (nat_abs i) (nat_abs j) (nat_abs k)
+@[simp] theorem gcd_zero_right (i : ℤ) : gcd i 0 = nat_abs i := by simp [gcd]
 
 @[simp] theorem gcd_one_left (i : ℤ) : gcd 1 i = 1 := nat.gcd_one_left _
 
-@[simp] theorem gcd_one_right (i : ℤ) : gcd i 1 = 1 :=
-eq.trans (gcd_comm i 1) $ gcd_one_left i
+@[simp] theorem gcd_one_right (i : ℤ) : gcd i 1 = 1 := nat.gcd_one_right _
 
 theorem gcd_mul_left (i j k : ℤ) : gcd (i * j) (i * k) = nat_abs i * gcd j k :=
-by rw [gcd, nat_abs_mul, nat_abs_mul];
-exact nat.gcd_mul_left (nat_abs i) (nat_abs j) (nat_abs k)
+by simp [(int.coe_nat_eq_coe_nat_iff _ _).symm, coe_gcd, coe_nat_abs_eq_mul_norm_unit]
 
 theorem gcd_mul_right (i j k : ℤ) : gcd (i * j) (k * j) = gcd i k * nat_abs j :=
-by rw [gcd, nat_abs_mul, nat_abs_mul];
-exact nat.gcd_mul_right (nat_abs i) (nat_abs j) (nat_abs k)
+by simp [(int.coe_nat_eq_coe_nat_iff _ _).symm, coe_gcd, coe_nat_abs_eq_mul_norm_unit]
 
 theorem gcd_pos_of_non_zero_left {i : ℤ} (j : ℤ) (i_non_zero : i ≠ 0) : gcd i j > 0 :=
 nat.gcd_pos_of_pos_left (nat_abs j) (nat_abs_pos_of_ne_zero i_non_zero)
@@ -147,11 +203,8 @@ nat.gcd_pos_of_pos_left (nat_abs j) (nat_abs_pos_of_ne_zero i_non_zero)
 theorem gcd_pos_of_non_zero_right (i : ℤ) {j : ℤ} (j_non_zero : j ≠ 0) : gcd i j > 0 :=
 nat.gcd_pos_of_pos_right (nat_abs i) (nat_abs_pos_of_ne_zero j_non_zero)
 
-theorem eq_zero_of_gcd_eq_zero_left {i j : ℤ} (H : gcd i j = 0) : i = 0 :=
-eq_zero_of_nat_abs_eq_zero (nat.eq_zero_of_gcd_eq_zero_left H)
-
-theorem eq_zero_of_gcd_eq_zero_right {i j : ℤ} (H : gcd i j = 0) : j = 0 :=
-eq_zero_of_nat_abs_eq_zero (nat.eq_zero_of_gcd_eq_zero_right H)
+theorem gcd_eq_zero_iff {i j : ℤ} : gcd i j = 0 ↔ i = 0 ∧ j = 0 :=
+by rw [← int.coe_nat_eq_coe_nat_iff, int.coe_nat_zero, coe_gcd, gcd_eq_zero_iff]
 
 theorem gcd_div {i j k : ℤ} (H1 : k ∣ i) (H2 : k ∣ j) :
   gcd (i / k) (j / k) = gcd i j / nat_abs k :=
@@ -185,50 +238,34 @@ by rw [gcd_comm, gcd_eq_left H]
 
 /- lcm -/
 
-def lcm (i j : ℤ) : ℕ := nat_abs(i * j) / (gcd i j)
-
-theorem lcm_def (i j : ℤ) : lcm i j = nat.lcm (nat_abs i) (nat_abs j) :=
-by rw [lcm, nat.lcm, gcd, nat_abs_mul]
-
 theorem lcm_comm (i j : ℤ) : lcm i j = lcm j i :=
-by delta lcm; rw [mul_comm, gcd_comm]
-
-theorem lcm_zero_left (i : ℤ) : lcm 0 i = 0 :=
-by rw [lcm, zero_mul, gcd_zero_left]; by simp
-
-theorem lcm_zero_right (i : ℤ) : lcm i 0 = 0 := lcm_comm 0 i ▸ lcm_zero_left i
-
-theorem lcm_one_left (i : ℤ) : lcm 1 i = nat_abs i :=
-by rw [lcm, one_mul, gcd_one_left, nat.div_one]
-
-theorem lcm_one_right (i : ℤ) : lcm i 1 = nat_abs i :=
-by unfold lcm; simp
-
-theorem lcm_self (i : ℤ) : lcm i i = nat_abs i :=
-by rw [lcm, gcd_self, nat_abs_mul, nat.mul_div_assoc, mul_comm, nat.div_mul_cancel];
-simp; simp
-
-theorem dvd_lcm_left (i j : ℤ) : i ∣ lcm i j :=
-nat_abs_dvd.mp (coe_nat_dvd.mpr (eq.subst (eq.symm (lcm_def i j))
-                                          (nat.dvd_lcm_left (nat_abs i) (nat_abs j))))
-
-theorem dvd_lcm_right (i j : ℤ) : j ∣ lcm i j :=
-lcm_comm j i ▸ dvd_lcm_left j i
-
-theorem gcd_mul_lcm (i j : ℤ) : gcd i j * lcm i j = nat_abs (i * j) :=
-begin
-  rw [lcm, mul_comm, nat.div_mul_cancel],
-  exact eq.subst (eq.symm (nat_abs_mul i j))
-                 (dvd_mul_of_dvd_left (coe_nat_dvd.mp (dvd_nat_abs.mpr (gcd_dvd_left i j))) _),
-end
-
-theorem lcm_dvd {i j k : ℤ} (H1 : i ∣ k) (H2 : j ∣ k) : (lcm i j : ℤ) ∣ k :=
-dvd_nat_abs.mp (coe_nat_dvd.mpr (eq.subst (eq.symm (lcm_def i j))
-                                          (nat.lcm_dvd (nat_abs_dvd_abs_iff.mpr H1)
-                                                       (nat_abs_dvd_abs_iff.mpr H2))))
+by simp [(int.coe_nat_eq_coe_nat_iff _ _).symm, coe_lcm, lcm_comm]
 
 theorem lcm_assoc (i j k : ℤ) : lcm (lcm i j) k = lcm i (lcm j k) :=
-by rw [lcm_def, lcm_def, lcm_def, lcm_def];
-exact nat.lcm_assoc (nat_abs i) (nat_abs j) (nat_abs k)
+by simp [(int.coe_nat_eq_coe_nat_iff _ _).symm, coe_lcm, lcm_assoc]
+
+@[simp] theorem lcm_zero_left (i : ℤ) : lcm 0 i = 0 :=
+by simp [(int.coe_nat_eq_coe_nat_iff _ _).symm, coe_lcm]
+
+@[simp] theorem lcm_zero_right (i : ℤ) : lcm i 0 = 0 :=
+by simp [(int.coe_nat_eq_coe_nat_iff _ _).symm, coe_lcm]
+
+@[simp] theorem lcm_one_left (i : ℤ) : lcm 1 i = nat_abs i :=
+by simp [(int.coe_nat_eq_coe_nat_iff _ _).symm, coe_lcm, coe_nat_abs_eq_mul_norm_unit]
+
+@[simp] theorem lcm_one_right (i : ℤ) : lcm i 1 = nat_abs i :=
+by simp [(int.coe_nat_eq_coe_nat_iff _ _).symm, coe_lcm, coe_nat_abs_eq_mul_norm_unit]
+
+@[simp] theorem lcm_self (i : ℤ) : lcm i i = nat_abs i :=
+by simp [(int.coe_nat_eq_coe_nat_iff _ _).symm, coe_lcm, coe_nat_abs_eq_mul_norm_unit]
+
+theorem dvd_lcm_left (i j : ℤ) : i ∣ lcm i j :=
+by rw [coe_lcm]; exact dvd_lcm_left _ _
+
+theorem dvd_lcm_right (i j : ℤ) : j ∣ lcm i j :=
+by rw [coe_lcm]; exact dvd_lcm_right _ _
+
+theorem lcm_dvd {i j k : ℤ}  : i ∣ k → j ∣ k → (lcm i j : ℤ) ∣ k :=
+by rw [coe_lcm]; exact lcm_dvd
 
 end int

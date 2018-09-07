2b07d50f9dcf533c4a346f20d31419a1398d9ad4
Chris Hughes <33847686+dorhinj@users.noreply.github.com>
2018-09-07 21:26:21 +0100

    move prod.ring

--- analysis/normed_space.lean
+++ analysis/normed_space.lean
@@ -179,18 +179,6 @@ instance normed_ring.to_normed_group [β : normed_ring α] : normed_group α := 
 lemma norm_mul {α : Type*} [normed_ring α] (a b : α) : (∥a*b∥) ≤ (∥a∥) * (∥b∥) :=
 normed_ring.norm_mul _ _
 
-instance prod.ring [ring α] [ring β] : ring (α × β) :=
-{ left_distrib := assume x y z, calc
-    x*(y+z) = (x.1, x.2) * (y.1 + z.1, y.2 + z.2) : rfl
-    ... = (x.1*(y.1 + z.1), x.2*(y.2 + z.2)) : rfl
-    ... = (x.1*y.1 + x.1*z.1, x.2*y.2 + x.2*z.2) : by simp[left_distrib],
-  right_distrib := assume x y z, calc
-    (x+y)*z = (x.1 + y.1, x.2 + y.2)*(z.1, z.2) : rfl
-    ... = ((x.1 + y.1)*z.1, (x.2 + y.2)*z.2) : rfl
-    ... = (x.1*z.1 + y.1*z.1, x.2*z.2 + y.2*z.2) : by simp[right_distrib],
-  ..prod.monoid,
-  ..prod.add_comm_group}
-
 instance prod.normed_ring [normed_ring α] [normed_ring β] : normed_ring (α × β) :=
 { norm_mul := assume x y,
   calc

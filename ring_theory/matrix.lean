d978c56f2459431c027027ab0a29ba8fe52c9a4d
Mario Carneiro <di.gama@gmail.com>
2018-09-18 13:04:27 -0400

    fix author list

--- ring_theory/matrix.lean
+++ ring_theory/matrix.lean
@@ -1,7 +1,7 @@
 /-
-Copyright (c) 2018 Ellen Arlt, Blair Shi, Sean Leather. All rights reserved.
+Copyright (c) 2018 Ellen Arlt. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
-Authors: Ellen Arlt, Blair Shi, Sean Leather
+Authors: Ellen Arlt, Blair Shi, Sean Leather, Mario Carneiro
 
 Matrices
 -/

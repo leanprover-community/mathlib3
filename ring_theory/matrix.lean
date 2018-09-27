6f54f36f058bd837fe77074752295c836b970b12
Johan Commelin <johan@commelin.net>
2018-09-27 10:21:08 +0200

    feat(ring_theory/matrix): category of free modules; diagonal and scalar matrices

--- ring_theory/matrix.lean
+++ ring_theory/matrix.lean
@@ -6,7 +6,7 @@ Authors: Ellen Arlt, Blair Shi, Sean Leather
 Matrices
 -/
 
-import algebra.module data.fintype
+import algebra.module data.fintype category_theory.category
 
 universes u v
 
@@ -116,18 +116,18 @@ section semigroup
 variables [decidable_eq m] [decidable_eq n] [semiring α]
 
 protected theorem mul_assoc (L : matrix l m α) (M : matrix m n α) (N : matrix n o α) :
-  L.mul (M.mul N) = (L.mul M).mul N :=
+  (L.mul M).mul N = L.mul (M.mul N) :=
 funext $ λ i, funext $ λ k,
-  calc finset.univ.sum (λ (j₁ : m), L i j₁ * finset.univ.sum (λ (j₂ : n), M j₁ j₂ * N j₂ k))
-    = finset.univ.sum (λ (j₁ : m), finset.univ.sum (λ (j₂ : n), L i j₁ * M j₁ j₂ * N j₂ k)) :
-      by congr; funext; rw finset.mul_sum; congr; funext; rw mul_assoc
-    ... = finset.univ.sum (λ (j₂ : n), finset.univ.sum (λ (j₁ : m), L i j₁ * M j₁ j₂ * N j₂ k)) :
+  calc finset.univ.sum (λ (j₂ : n), finset.univ.sum (λ (j₁ : m), L i j₁ * M j₁ j₂) * N j₂ k)
+    = finset.univ.sum (λ (j₂ : n), finset.univ.sum (λ (j₁ : m), L i j₁ * M j₁ j₂ * N j₂ k)) :
+      by congr; funext; rw finset.sum_mul
+    ... = finset.univ.sum (λ (j₁ : m), finset.univ.sum (λ (j₂ : n), L i j₁ * M j₁ j₂ * N j₂ k)) :
       by rw finset.sum_comm
-    ... = finset.univ.sum (λ (j₂ : n), finset.univ.sum (λ (j₁ : m), L i j₁ * M j₁ j₂) * N j₂ k) :
-      by congr; funext; rw ←finset.sum_mul
+    ... = finset.univ.sum (λ (j₁ : m), L i j₁ * finset.univ.sum (λ (j₂ : n), M j₁ j₂ * N j₂ k)) :
+      by congr; funext; rw finset.mul_sum; congr; funext; rw mul_assoc
 
 instance : semigroup (matrix n n α) :=
-{ mul_assoc := λ L M N, (matrix.mul_assoc L M N).symm,
+{ mul_assoc := matrix.mul_assoc,
   ..matrix.has_mul }
 
 end semigroup
@@ -212,5 +212,102 @@ instance [ring α] : module α (matrix m n α) :=
   add_smul := λ a b M, ext' $ λ i j, add_mul a b (M i j),
   mul_smul := λ a b M, ext' $ λ i j, mul_assoc a b (M i j),
   one_smul := λ M, ext' $ λ i j, one_mul (M i j) }
+
+section free_module
+
+structure free_module (α : Type v) [semiring α] : Type (v+1) :=
+(carrier : Type v)
+[fin : fintype carrier]
+[deceq : decidable_eq carrier]
+
+instance (α) [semiring α] : has_coe_to_sort (free_module α) := ⟨_, free_module.carrier⟩
+instance {α} [semiring α] (n : free_module α) : fintype n := free_module.fin _
+instance {α} [semiring α] (n : free_module α) : decidable_eq n := free_module.deceq _
+
+variables [semiring α]
+
+open category_theory
+
+instance : large_category (free_module α) :=
+{ hom  := λ m n, matrix m n α,
+  id   := λ m, 1,
+  comp := λ _ _ _, matrix.mul,
+  comp_id' := λ _ _, matrix.mul_one,
+  id_comp' := λ _ _, matrix.one_mul,
+  assoc'   := λ _ _ _ _, matrix.mul_assoc }
+
+end free_module
+
+section diagonal
+variables [decidable_eq n]
+
+def diagonal [has_zero α] (d : n → α) : matrix n n α := λ i j, if i = j then d i else 0
+
+@[simp] theorem diagonal_val_eq  [has_zero α] {d : n → α} {i : n} : (diagonal d) i i = d i :=
+by simp [diagonal]
+
+@[simp] theorem diagonal_val_ne  [has_zero α] {d : n → α} {i j : n} (h : i ≠ j) :
+(diagonal d) i j = 0 := by simp [diagonal, h]
+
+@[simp] theorem diagonal_zero [has_zero α] : (diagonal (λ _, 0) : matrix n n α) = 0 :=
+by simp [diagonal]; refl
+
+@[simp] theorem diagonal_one [has_zero α] [has_one α] : (diagonal (λ _, 1) : matrix n n α) = 1 :=
+by simp [diagonal]; refl
+
+@[simp] lemma ite_mul [semiring α] (P Q : Prop) [decidable P] [decidable Q] (a b : α) :
+  (ite P a 0) * (ite Q b 0) = ite (P ∧ Q) (a * b) 0 :=
+by split_ifs; simp * at *
+
+@[simp] lemma eq_and_eq_symm {α : Type u} (a b : α) : (a = b ∧ b = a) ↔ a = b :=
+by simp [eq_comm]
+
+@[simp] theorem diagonal_mul [ring α] {d₁ d₂ : n → α} : 
+  (diagonal d₁) * (diagonal d₂) = (diagonal (λ i, d₁ i * d₂ i)) :=
+begin
+  funext i j,
+  dsimp [diagonal],
+  split_ifs,
+  { subst h,
+    transitivity finset.sum (finset.singleton i) _,
+    { symmetry,
+      apply finset.sum_subset,
+      { intros _ _, simp },
+      intros j h1 h2,
+      simp at h2,
+      simp [h2] },
+    simp },
+  { convert finset.sum_const_zero,
+    ext k,
+    split_ifs; cc <|> simp }
+end
+
+@[simp] theorem diagonal_add [ring α] {d₁ d₂ : n → α} : 
+  (diagonal d₁) + (diagonal d₂) = (diagonal (λ i, d₁ i + d₂ i)) :=
+begin
+  tidy,
+  dsimp [diagonal],
+  split_ifs,
+  tidy, -- TODO tidy should try split_ifs!
+end
+
+end diagonal
+
+section scalar
+variables [decidable_eq n] [has_zero α]
+
+def scalar (a : α) : matrix n n α := diagonal (λ _, a)
+
+@[simp] theorem scalar_val_eq {a : α} {i : n} : (scalar a) i i = a :=
+diagonal_val_eq
+
+@[simp] theorem scalar_val_ne {a : α} {i j : n} (h : i ≠ j) : (scalar a) i j = 0 :=
+diagonal_val_ne h
+
+@[simp] theorem scalar_zero : (scalar 0 : matrix n n α) = 0 := diagonal_zero
+
+@[simp] theorem scalar_one  [has_one α] : (scalar 1 : matrix n n α) = 1 := rfl
+
+end scalar
 
 end matrix

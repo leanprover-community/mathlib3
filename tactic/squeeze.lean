4572a3b42e83738e0535d577a3af80e77ff494dc
Simon Hudon <simon.hudon@gmail.com>
2018-10-06 17:54:19 -0400

    docs(tactic/squeeze): add header comments and documentation

--- tactic/squeeze.lean
+++ tactic/squeeze.lean
@@ -58,6 +58,49 @@ meta def parse_config : option pexpr → tactic (simp_config_ext × format)
      prod.mk <$> eval_expr simp_config_ext e
              <*> rec.to_tactic_format cfg
 
+/-- `squeeze_simp` behaves like `simp` (including all its arguments)
+and prints a `simp only` invokation to skip the search through the
+`simp` lemma list.
+
+For instance, the following is easily solved with `simp`:
+
+```
+example : 0 + 1 = 1 + 0 := by simp
+```
+
+To guide the proof search and speed it up, we may replace `simp`
+with `squeeze_simp`:
+
+```
+example : 0 + 1 = 1 + 0 := by squeeze_simp
+-- prints: simp only [add_zero, eq_self_iff_true, zero_add]
+```
+
+`squeeze_simp` suggests a replacement which we can use instead of
+`squeeze_simp`.
+
+```
+example : 0 + 1 = 1 + 0 := by simp only [add_zero, eq_self_iff_true, zero_add]
+```
+
+`squeeze_simp only` prints nothing as it already skips the `simp` list.
+
+This tactic is useful for speeding up the compilation of a complete file.
+Steps:
+
+   1. search and replace ` simp` with ` squeeze_simp` (the space helps avoid the
+      replacement of `simp` in `@[simp]`) throughout the file.
+   2. Starting at the beginning of the file, go to each printout in turn, copy
+      the suggestion in place of `squeeze_simp`.
+   3. after all the suggestions were applied, search and replace `squeeze_simp` with
+      `simp` to remove the occurrences of `squeeze_simp` that did not produce a suggestion.
+
+Known limitation(s):
+  * in cases where `squeeze_simp` is used after a `;` (e.g. `cases x; squeeze_simp`),
+    `squeeze_simp` will produce as many suggestions as the number of goals it is applied to.
+    It is likely that none of the suggestion is a good replacement but they can all be
+    combined by concatenating their list of lemmas.
+-/
 meta def squeeze_simp
   (use_iota_eqn : parse (tk "!")?) (no_dflt : parse only_flag) (hs : parse simp_arg_list)
   (attr_names : parse with_ident_list) (locat : parse location)
@@ -66,15 +109,20 @@ do g ← main_goal,
    (cfg',c) ← parse_config cfg,
    hs' ← hs.mmap arg.to_tactic_format,
    simp use_iota_eqn no_dflt hs attr_names locat cfg',
-   g ← instantiate_mvars g,
-   let vs := g.list_constant,
-   vs ← vs.mfilter (succeeds ∘ has_attribute `simp),
-   let use_iota_eqn := if use_iota_eqn.is_some then "!" else "",
-   let attrs := if attr_names.empty then "" else string.join (list.intersperse " " (" with" :: attr_names.map to_string)),
-   let loc := loc.to_string locat,
-   let args := hs' ++ vs.to_list.map to_fmt,
-   trace format!"simp{use_iota_eqn} only {args}{attrs}{loc}{c}"
+   when (¬ no_dflt) $ do
+     g ← instantiate_mvars g,
+     let vs := g.list_constant,
+     vs ← vs.mfilter (succeeds ∘ has_attribute `simp),
+     let use_iota_eqn := if use_iota_eqn.is_some then "!" else "",
+     let attrs := if attr_names.empty then "" else string.join (list.intersperse " " (" with" :: attr_names.map to_string)),
+     let loc := loc.to_string locat,
+     let args := hs' ++ vs.to_list.map to_fmt,
+     trace format!"simp{use_iota_eqn} only {args}{attrs}{loc}{c}"
 
+example : 0 + 1 = 1 + 0 :=
+by squeeze_simp
+
+/-- same as `squeeze_simp` for `simpa` -/
 meta def squeeze_simpa
   (use_iota_eqn : parse (tk "!")?) (no_dflt : parse only_flag) (hs : parse simp_arg_list)
   (attr_names : parse with_ident_list) (tgt : parse (tk "using" *> texpr)?)
@@ -84,15 +132,16 @@ do g ← main_goal,
    tgt' ← traverse (λ t, do t ← to_expr t >>= pp,
                             pure format!" using {t}") tgt,
    simpa use_iota_eqn no_dflt hs attr_names tgt cfg',
-   g ← instantiate_mvars g,
-   let vs := g.list_constant,
-   vs ← vs.mfilter (succeeds ∘ has_attribute `simp),
-   let use_iota_eqn := if use_iota_eqn.is_some then "!" else "",
-   let attrs := if attr_names.empty then "" else string.join (list.intersperse " " (" with" :: attr_names.map to_string)),
-   let tgt' := tgt'.get_or_else "",
-   hs ← hs.mmap arg.to_tactic_format,
-   let args := hs ++ vs.to_list.map to_fmt,
-   trace format!"simpa{use_iota_eqn} only {args}{attrs}{tgt'}{c}"
+   when (¬ no_dflt) $ do
+     g ← instantiate_mvars g,
+     let vs := g.list_constant,
+     vs ← vs.mfilter (succeeds ∘ has_attribute `simp),
+     let use_iota_eqn := if use_iota_eqn.is_some then "!" else "",
+     let attrs := if attr_names.empty then "" else string.join (list.intersperse " " (" with" :: attr_names.map to_string)),
+     let tgt' := tgt'.get_or_else "",
+     hs ← hs.mmap arg.to_tactic_format,
+     let args := hs ++ vs.to_list.map to_fmt,
+     trace format!"simpa{use_iota_eqn} only {args}{attrs}{tgt'}{c}"
 
 end interactive
 end tactic

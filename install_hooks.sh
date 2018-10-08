8e65c100069ea03acd34140d027913a1d53fda83
Scott Morrison <scott@tqft.net>
2018-10-08 11:11:05 +1100

    mkdir -p .git/hooks/

--- install_hooks.sh
+++ install_hooks.sh
@@ -1,3 +1,4 @@
 #! /bin/sh
 chmod +x post-commit pre-rebase post-checkout cache_olean.sh restore_olean.sh 
+mkdir -p .git/hooks/
 cp post-commit pre-rebase post-checkout cache_olean.sh restore_olean.sh .git/hooks/

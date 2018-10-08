0e40a9142a515bfd7bfc3309e73be4de89535ce2
Scott Morrison <scott@tqft.net>
2018-10-08 11:26:47 +1100

    use `git rev-parse --git-dir` to determine the correct path for the .git directory, for use with submodules

--- install_hooks.sh
+++ install_hooks.sh
@@ -1,4 +1,5 @@
 #! /bin/sh
 chmod +x post-commit pre-rebase post-checkout cache_olean.sh restore_olean.sh 
-mkdir -p .git/hooks/
-cp post-commit pre-rebase post-checkout cache_olean.sh restore_olean.sh .git/hooks/
+GITDIR=`git rev-parse --git-dir`
+mkdir -p $GITDIR/hooks/
+cp post-commit pre-rebase post-checkout cache_olean.sh restore_olean.sh $GITDIR/hooks/

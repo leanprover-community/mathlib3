5b0547d61e8b12c16ac4839ec86b65010c5be50d
Tobias Grosser <tobias@grosser.es>
2018-10-14 13:27:50 +0200

    data/fin: remove eq_of_lt_succ_of_not_lt
    This function does not belong in data/fin and has no user outside of
    data/fin and the last use of it in data/fin got removed in
    (refactor(data/equiv,encodable): refactor/simplify proofs, Tue Sep 19
    02:55:14 2017 -0400).

--- data/fin.lean
+++ data/fin.lean
@@ -39,10 +39,6 @@ instance {n : ℕ} : decidable_linear_order (fin n) :=
 
 end fin
 
-theorem eq_of_lt_succ_of_not_lt {a b : ℕ} (h1 : a < b + 1) (h2 : ¬ a < b) : a = b :=
-have h3 : a ≤ b, from le_of_lt_succ h1,
-or.elim (eq_or_lt_of_not_lt h2) (λ h, h) (λ h, absurd h (not_lt_of_ge h3))
-
 instance fin_to_nat (n : ℕ) : has_coe (fin n) nat := ⟨fin.val⟩
 instance fin_to_int (n : ℕ) : has_coe (fin n) int := ⟨λ k, ↑(fin.val k)⟩
 

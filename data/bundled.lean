ac02b3ffff780942efdb0d393f025b75bd38f00d
Sean Leather <sean.leather@gmail.com>
2018-10-17 08:48:27 +0200

    bundled: Sort -> Type

--- data/bundled.lean
+++ data/bundled.lean
@@ -10,44 +10,35 @@ universes u v
 
 /-- `bundled` is a type bundled with a type class instance for that type. Only
 the type class is exposed as a parameter. -/
-structure bundled (c : Sort u → Sort v) : Sort (max (u+1) v) :=
-mk' :: (α : Sort u) (inst : c α)
+structure bundled (c : Type u → Type v) : Type (max (u+1) v) :=
+(α : Type u)
+(inst : c α)
 
 /-
 Note on the definition of `bundled`:
 
 It is possible to define `bundled` with square brackets for the instance:
 
-  structure bundled (c : Sort u → Sort v) :=
-  (α : Sort u) [inst : c α]
+  structure bundled (c : Type u → Type v) :=
+  (α : Type u)
+  [inst : c α]
 
 The result is a constructor with this type:
 
-  mk : ∀ (c : Sort u → Sort v) (α : Sort u) [c α], bundled c
+  mk : ∀ (c : Type u → Type v) (α : Type u) [c α], bundled c
 
 However, that leads to needing `@mk` in practice and does not appear to provide
-any benefit. Therefore, we defined the constructor as above along with a wrapper
-below.
+any benefit. Therefore, we defined the constructor without square brackets.
 -/
 
 namespace bundled
-variables {c d : Sort u → Sort v} {α : Sort u}
-
-/-- A convenient wrapper around `bundled.mk'` with an implicit instance. -/
-def mk (α : Sort u) [h : c α] : bundled c :=
-mk' α h
+variables {c d : Type u → Type v} {α : Type u}
 
 instance : has_coe_to_sort (bundled c) :=
-{ S := Sort u, coe := bundled.α }
+{ S := Type u, coe := bundled.α }
 
 /-- Map over the bundled instance -/
 def map (f : ∀ {α}, c α → d α) (b : bundled c) : bundled d :=
-mk' b.α (f b.inst)
-
-/- See the following definitions for examples of using `bundled`. -/
-
-/-- Bundled `decidable_eq` -/
-@[reducible] def deceq : Sort (max (u+1) u 1) := bundled decidable_eq
-instance {d : deceq} : decidable_eq d := d.inst
+⟨b.α, f b.inst⟩
 
 end bundled

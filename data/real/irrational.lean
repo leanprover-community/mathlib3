13c65cadefaa46c022820edcd7f2e02676499885
jlpaca <jhjuhn.lok@gmail.com>
2018-10-25 16:36:17 +0100

    add author credit

--- data/real/irrational.lean
+++ data/real/irrational.lean
@@ -1,7 +1,7 @@
 /-
 Copyright (c) 2018 Mario Carneiro. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
-Authors: Mario Carneiro, Abhimanyu Pallavi Sudhir
+Authors: Mario Carneiro, Abhimanyu Pallavi Sudhir, Jean Lo
 
 Irrationality of real numbers.
 -/

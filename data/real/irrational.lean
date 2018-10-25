656eb54defbca834773376a32d6ad9ec1819d40c
jlpaca <jhjuhn.lok@gmail.com>
2018-10-25 16:26:51 +0100

    consistent indent

--- data/real/irrational.lean
+++ data/real/irrational.lean
@@ -31,16 +31,17 @@ theorem sqrt_two_irrational : irrational (sqrt 2)
 end
 
 theorem irr_of_irr_mul_rat (q : ℚ) (x : ℝ) : q ≠ 0 → irrational x → irrational (x * ↑q) :=
-    begin
-        intro Hqn0, intro Hix, intro Hqxrat, cases Hqxrat with r Hr,
-        rw [←div_eq_iff_mul_eq, rat.num_denom r, rat.num_denom q, rat.cast_mk, rat.cast_mk, div_div_div_div_eq] at Hr,
-        rw [←int.cast_mul, ←int.cast_mul, ←rat.cast_mk_of_ne_zero] at Hr,
-        unfold irrational at Hix, apply Hix, existsi (rat.mk (r.num * ↑(q.denom)) (↑(r.denom) * q.num)),
-        exact Hr.symm,
-        intro Hxd0, rw [int.cast_eq_zero, mul_eq_zero] at Hxd0, cases Hxd0,
-        rw int.coe_nat_eq_zero at Hxd0,
-        revert Hxd0,
-        apply rat.denom_ne_zero,
-        revert Hxd0, apply rat.num_ne_zero_of_ne_zero, exact Hqn0,
-        rw rat.cast_ne_zero, exact Hqn0,
-    end
+begin
+    intro Hqn0, intro Hix, intro Hqxrat, cases Hqxrat with r Hr,
+    rw [←div_eq_iff_mul_eq, rat.num_denom r, rat.num_denom q, rat.cast_mk, rat.cast_mk, div_div_div_div_eq] at Hr,
+    rw [←int.cast_mul, ←int.cast_mul, ←rat.cast_mk_of_ne_zero] at Hr,
+    unfold irrational at Hix, apply Hix, existsi (rat.mk (r.num * ↑(q.denom)) (↑(r.denom) * q.num)),
+    exact Hr.symm,
+    intro Hxd0, rw [int.cast_eq_zero, mul_eq_zero] at Hxd0, cases Hxd0,
+    rw int.coe_nat_eq_zero at Hxd0,
+    revert Hxd0,
+    apply rat.denom_ne_zero,
+    revert Hxd0, apply rat.num_ne_zero_of_ne_zero, exact Hqn0,
+    rw rat.cast_ne_zero, exact Hqn0,
+end
+

51e3ceab989a3458dd3be7581e4b256eaea10be7
Scott Morrison <scott.morrison@anu.edu.au>
2018-10-29 09:14:03 +1100

    fixes

--- tactic/fin_cases.lean
+++ tactic/fin_cases.lean
@@ -5,9 +5,7 @@ Author: Scott Morrison
 
 Case bashing on `fin n`, for explicit numerals `n`.
 -/
-import tactic.linarith
-
-def fin_val {m n : ℕ} (h : m < n) : (⟨m, h⟩ : fin n).val = m := rfl
+import data.fin
 
 namespace tactic
 open lean.parser
@@ -19,21 +17,35 @@ do `(fin %%n) ← infer_type e,
    [(_, [val, bd], _)] ← cases_core e [`val, `bd],
    -- We now call `cases val` n times, rotating the generated goals out of the way.
    iterate_at_most n (do val ← get_local `val, cases val, rotate 1),
-   -- We've got an absurd hypothesis, but it is messy: it looks like
+   -- We've got an absurd hypothesis `bd`, but it is messy: it looks like
    -- `nat.succ (... (nat.succ val)) < n`
-   -- So we rewrite it as `val + 1 + ... + 1 < n`, and call `linarith` to kill it.
+   -- So we rewrite it as `bd : val + 1 + ... + 1 < n`, and use `dec_trivial` to kill it.
    ss ← mk_const `nat.succ_eq_add_one,
    bd ← get_local `bd,
    (list.range n).mfoldl (λ bd _, do rewrite_hyp ss bd) bd,
-   exfalso >> `[linarith],
+   to_expr ``(absurd %%bd dec_trivial) >>= exact,
    -- We put the goals back in order, and clear the `bd` hypotheses.
    iterate_exactly n (do rotate_right 1,
-                         try `[rw [fin_val]],
+                         try `[rw [fin.mk_val]],
                          try (get_local `bd >>= clear))
 
 namespace interactive
 private meta def hyp := tk "*" *> return none <|> some <$> ident
 
+/--
+`fin_cases h` performs case analysis on a hypothesis `h : fin n`, where `n` is an explicit natural
+number. `fin_cases *` performs case analysis on all suitable hypotheses.
+
+As an example, in
+```
+example (f : ℕ → Prop) (p : fin 3) (h0 : f 0) (h1 : f 1) (h2 : f 2) : f p.val :=
+begin
+  fin_cases p,
+  all_goals { assumption }
+end
+```
+after `fin_cases p`, there are three goals, `f 0`, `f 1`, and `f 2`.
+-/
 meta def fin_cases : parse hyp → tactic unit
 | none := do ctx ← local_context,
              ctx.mfirst fin_cases_at <|> fail "No explicit `fin n` hypotheses."

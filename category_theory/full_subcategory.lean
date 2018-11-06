02a273b62b556bf1b751ebf7cb9ecaf6e2cf7256
Scott Morrison <scott@tqft.net>
2018-11-06 21:17:18 +1100

    minor

--- category_theory/full_subcategory.lean
+++ category_theory/full_subcategory.lean
@@ -20,8 +20,8 @@ def full_subcategory_inclusion (Z : C → Prop) : {X : C // Z X} ⥤ C :=
 { obj := λ X, X.1,
   map' := λ _ _ f, f }
 
-instance full_subcategory_full     (Z : C → Prop) : full     (full_subcategory_embedding Z) := by obviously
-instance full_subcategory_faithful (Z : C → Prop) : faithful (full_subcategory_embedding Z) := by obviously
+instance full_subcategory_full     (Z : C → Prop) : full     (full_subcategory_inclusion Z) := by obviously
+instance full_subcategory_faithful (Z : C → Prop) : faithful (full_subcategory_inclusion Z) := by obviously
 end
 
 end category_theory

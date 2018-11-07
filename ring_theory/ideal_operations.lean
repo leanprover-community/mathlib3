a6afe4193ce977d6ff4a5e46a0157858407b88cf
kckennylau <kc_kennylau@yahoo.com.hk>
2018-11-07 08:29:32 +0100

    feat(ring_theory/ideal_operations): define ideal operations

---
+++ ring_theory/ideal_operations.lean
@@ -0,0 +1,130 @@
+import ring_theory.ideals data.nat.choose
+
+universes u v w
+
+namespace submodule
+
+variables {R : Type u} {M : Type v}
+variables [comm_ring R] [add_comm_group M] [module R M]
+
+instance has_scalar' : has_scalar (ideal R) (submodule R M) :=
+⟨λ I N, ⨆ r : I, N.map (r.1 • linear_map.id)⟩
+
+variables {I : ideal R} {N : submodule R M}
+
+theorem smul_mem_smul {r} {n} (hr : r ∈ I) (hn : n ∈ N) : r • n ∈ I • N :=
+(lattice.le_supr _ ⟨r, hr⟩ : _ ≤ I • N) ⟨n, hn, rfl⟩
+
+theorem smul_le {P : submodule R M} : I • N ≤ P ↔ ∀ (r ∈ I) (n ∈ N), r • n ∈ P :=
+⟨λ H r hr n hn, H $ smul_mem_smul hr hn,
+λ H, lattice.supr_le $ λ r, map_le_iff_le_comap.2 $ λ n hn, H r.1 r.2 n hn⟩
+
+theorem smul_le_right : I • N ≤ N :=
+smul_le.2 $ λ r hr n, N.smul_mem r
+
+theorem smul_bot : I • (⊥ : submodule R M) = ⊥ :=
+lattice.eq_bot_iff.2 $ smul_le.2 $ λ r hri s hsb,
+submodule.mem_bot.2 $ (submodule.mem_bot.1 hsb).symm ▸ smul_zero r
+
+theorem bot_smul : ⊥ • I = ⊥ :=
+lattice.eq_bot_iff.2 $ smul_le.2 $ λ r hrb s hsi,
+submodule.mem_bot.2 $ (submodule.mem_bot.1 hrb).symm ▸ zero_smul s
+
+theorem top_smul : ⊤ • I = I :=
+le_antisymm smul_le_right $ λ r hri, one_smul r ▸ smul_mem_smul mem_top hri
+
+variables {S : set R} {T : set M}
+
+theorem smul_span : (ideal.span S) • (span T) =
+  span (⋃ (s ∈ S) (t ∈ T), {s • t}) :=
+le_antisymm (smul_le.2 $ λ r hrS n hnT, span_induction hrS
+  (λ r hrS, span_induction hnT
+    (λ n hnT, subset_span $ set.mem_bUnion hrS $
+      set.mem_bUnion hnT $ set.mem_singleton _)
+    ((smul_zero r : r • 0 = (0:M)).symm ▸ submodule.zero_mem _)
+    (λ x y, (smul_add r x y).symm ▸ submodule.add_mem _)
+    (λ c m, by rw [smul_smul, mul_comm, mul_smul]; exact submodule.smul_mem _ _))
+  ((zero_smul n).symm ▸ submodule.zero_mem _)
+  (λ r s, (add_smul r s n).symm ▸ submodule.add_mem _)
+  (λ c r, by rw [smul_eq_mul, mul_smul]; exact submodule.smul_mem _ _)) $
+span_le.2 $ set.bUnion_subset $ λ r hrS, set.bUnion_subset $ λ n hnT, set.singleton_subset_iff.2 $
+smul_mem_smul (subset_span hrS) (subset_span hnT)
+
+end submodule
+
+namespace ideal
+
+variables {R : Type u} [comm_ring R]
+
+instance : has_mul (ideal R) := ⟨(•)⟩
+
+variables {I J K : ideal R}
+
+theorem mul_mem_mul {r s} (hr : r ∈ I) (hs : s ∈ J) : r * s ∈ I * J :=
+submodule.smul_mem_smul hr hs
+
+theorem mul_mem_mul_rev {r s} (hr : r ∈ I) (hs : s ∈ J) : s * r ∈ I * J :=
+mul_comm r s ▸ mul_mem_mul hr hs
+
+theorem mul_le : I * J ≤ K ↔ ∀ (r ∈ I) (s ∈ J), r * s ∈ K :=
+submodule.smul_le
+
+variables (I J K)
+protected theorem mul_comm : I * J = J * I :=
+le_antisymm (mul_le.2 $ λ r hrI s hsJ, mul_mem_mul_rev hsJ hrI)
+(mul_le.2 $ λ r hrJ s hsI, mul_mem_mul_rev hsI hrJ)
+
+protected theorem mul_assoc : (I * J) * K = I * (J * K) :=
+le_antisymm (mul_le.2 $ λ rs hrsij t htk,
+  suffices I * J ≤ submodule.comap (t • linear_map.id) (I * (J * K)),
+  from (mul_comm t rs) ▸ this hrsij, mul_le.2 $ λ r hri s hsj,
+  show t * (r * s) ∈ I * (J * K), by rw [mul_comm, mul_assoc]; exact
+  mul_mem_mul hri (mul_mem_mul hsj htk))
+(mul_le.2 $ λ r hri st hstjk, suffices J * K ≤ submodule.comap (r • linear_map.id) ((I * J) * K), from this hstjk,
+mul_le.2 $ λ s hsj t htk, show r * (s * t) ∈ (I * J) * K, from mul_assoc r s t ▸ mul_mem_mul (mul_mem_mul hri hsj) htk)
+
+theorem mul_le_inf : I * J ≤ I ⊓ J :=
+mul_le.2 $ λ r hri s hsj, ⟨I.mul_mem_right hri, J.mul_mem_left hsj⟩
+variables {I J K}
+
+theorem mul_eq_inf_of_coprime (h : I ⊔ J = ⊤) : I * J = I ⊓ J :=
+le_antisymm (mul_le_inf _ _) $ λ r ⟨hri, hrj⟩,
+let ⟨s, hsi, t, htj, hst⟩ := submodule.mem_sup.1 ((eq_top_iff_one _).1 h) in
+mul_one r ▸ hst ▸ (mul_add r s t).symm ▸ ideal.add_mem (I * J) (mul_mem_mul_rev hsi hrj) (mul_mem_mul hri htj)
+
+theorem mul_bot : I * ⊥ = ⊥ :=
+submodule.smul_bot
+
+theorem bot_mul : ⊥ * I = ⊥ :=
+submodule.bot_smul
+
+theorem mul_top : I * ⊤ = I :=
+ideal.mul_comm ⊤ I ▸ submodule.top_smul
+
+theorem top_mul : ⊤ * I = I :=
+submodule.top_smul
+
+def radical (I : ideal R) : ideal R :=
+{ carrier := { r | ∃ n : ℕ, r ^ n ∈ I },
+  zero := ⟨1, (pow_one (0:R)).symm ▸ I.zero_mem⟩,
+  add := λ x y ⟨m, hxmi⟩ ⟨n, hyni⟩, ⟨m + n,
+    (add_pow x y (m + n)).symm ▸ I.sum_mem $
+    show ∀ c ∈ finset.range (nat.succ (m + n)), x ^ c * y ^ (m + n - c) * (choose (m + n) c) ∈ I,
+    from λ c hc, or.cases_on (le_total c m)
+      (λ hcm, I.mul_mem_right $ I.mul_mem_left $ nat.add_comm n m ▸ (nat.add_sub_assoc hcm n).symm ▸
+        (pow_add y n (m-c)).symm ▸ I.mul_mem_right hyni)
+      (λ hmc, I.mul_mem_right $ I.mul_mem_right $ nat.add_sub_cancel' hmc ▸
+        (pow_add x m (c-m)).symm ▸ I.mul_mem_right hxmi)⟩,
+  smul := λ r s ⟨n, hsni⟩, ⟨n, show (r * s)^n ∈ I,
+    from (mul_pow r s n).symm ▸ I.mul_mem_left hsni⟩ }
+
+theorem le_radical : I ≤ radical I :=
+λ r hri, ⟨1, (pow_one r).symm ▸ hri⟩
+
+theorem radical_top : (radical ⊤ : ideal R) = ⊤ :=
+(eq_top_iff_one _).2 ⟨0, submodule.mem_top⟩
+
+theorem radical_mono (H : I ≤ J) : radical I ≤ radical J :=
+λ r ⟨n, hrni⟩, ⟨n, H hrni⟩
+
+end ideal

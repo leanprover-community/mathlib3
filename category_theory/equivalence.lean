d381ff954c18e8ba6632329be9c34d9cb7fc2755
Scott Morrison <scott.morrison@anu.edu.au>
2018-11-16 14:13:42 +1100

    proof that a functor is an equivalence iff it is fully faithful and essentially surjective

--- category_theory/equivalence.lean
+++ category_theory/equivalence.lean
@@ -2,9 +2,10 @@
 -- Released under Apache 2.0 license as described in the file LICENSE.
 -- Authors: Tim Baumann, Stephen Morgan, Scott Morrison
 
-import category_theory.fully_faithful
+import category_theory.embedding
 import category_theory.functor_category
 import category_theory.natural_isomorphism
+import tactic.comp_slice
 import tactic.converter.interactive
 
 namespace category_theory
@@ -56,16 +57,14 @@ end
 variables {E : Type u₃} [ℰ : category.{u₃ v₃} E]
 include ℰ
 
-attribute [trans] category.comp
-
-@[simp] def effe_iso_id (e : C ≌ D) (f : D ≌ E) (X : C) :
+@[simp] private def effe_iso_id (e : C ≌ D) (f : D ≌ E) (X : C) :
   (e.inverse).obj ((f.inverse).obj ((f.functor).obj ((e.functor).obj X))) ≅ X :=
 calc
   (e.inverse).obj ((f.inverse).obj ((f.functor).obj ((e.functor).obj X)))
     ≅ (e.inverse).obj ((e.functor).obj X) : e.inverse.on_iso (nat_iso.app f.fun_inv_id _)
 ... ≅ X                                   : nat_iso.app e.fun_inv_id _
 
-@[simp] def feef_iso_id (e : C ≌ D) (f : D ≌ E) (X : E) :
+@[simp] private def feef_iso_id (e : C ≌ D) (f : D ≌ E) (X : E) :
   (f.functor).obj ((e.functor).obj ((e.inverse).obj ((f.inverse).obj X))) ≅ X :=
 calc
   (f.functor).obj ((e.functor).obj ((e.inverse).obj ((f.inverse).obj X)))
@@ -209,5 +208,78 @@ def obj_preimage (F : C ⥤ D) [ess_surj F] (d : D) : C := ess_surj.obj_preimage
 def fun_obj_preimage_iso (F : C ⥤ D) [ess_surj F] (d : D) : F.obj (F.obj_preimage d) ≅ d :=
 ess_surj.iso F d
 end functor
+
+namespace category_theory.equivalence
+
+def ess_surj_of_equivalence (F : C ⥤ D) [is_equivalence F] : ess_surj F :=
+⟨ λ Y : D, F.inv.obj Y, λ Y : D, (nat_iso.app F.inv_fun_id Y) ⟩
+
+instance faithful_of_equivalence (F : C ⥤ D) [is_equivalence F] : faithful F :=
+{ injectivity' := λ X Y f g w, begin
+                                have p := congr_arg (@category_theory.functor.map _ _ _ _ F.inv _ _) w,
+                                simp at *,
+                                assumption
+                              end }.
+
+instance full_of_equivalence (F : C ⥤ D) [is_equivalence F] : full F :=
+{ preimage := λ X Y f, (nat_iso.app F.fun_inv_id X).inv ≫ (F.inv.map f) ≫ (nat_iso.app F.fun_inv_id Y).hom,
+  witness' := λ X Y f,
+    begin
+      apply F.inv.injectivity,
+      /- obviously can finish from here... -/
+      dsimp, simp, dsimp,
+      conv { 
+        to_lhs,
+        comp_slice 4 6,
+        rw [←functor.map_comp, ←functor.map_comp],
+        rw [←is_equivalence.fun_inv_map],
+      },
+      conv {
+        to_lhs,
+        comp_slice 1 2,
+        simp,
+      },
+      dsimp, simp,
+      conv {
+        to_lhs,
+        comp_slice 2 4,
+        rw [←functor.map_comp, ←functor.map_comp],
+        erw [nat_iso.naturality_2],
+      },
+      erw [nat_iso.naturality_1],
+      refl,
+    end }.
+
+section
+
+@[simp] private def equivalence_inverse (F : C ⥤ D) [full F] [faithful F] [ess_surj F] : D ⥤ C :=
+{ obj  := λ X, F.obj_preimage X,
+  map := λ X Y f, F.preimage ((F.fun_obj_preimage_iso X).hom ≫ f ≫ (F.fun_obj_preimage_iso Y).inv),
+  map_id' := λ X, begin apply F.injectivity, tidy, end,
+  map_comp' := λ X Y Z f g, 
+  begin 
+    apply F.injectivity,
+    /- obviously can finish from here... -/
+    tidy,
+    conv {
+      to_rhs,
+      comp_slice 2 3,
+      erw [is_iso.hom_inv_id]
+    },
+    simp,
+  end }.
+
+def equivalence_of_fully_faithfully_ess_surj
+  (F : C ⥤ D) [full F] [faithful : faithful F] [ess_surj F] : is_equivalence F :=
+{ inverse := equivalence_inverse F,
+  fun_inv_id' := nat_iso.of_components
+    (λ X, preimage_iso (F.fun_obj_preimage_iso (F.obj X)))
+    (λ X Y f, begin apply F.injectivity, obviously, end),
+  inv_fun_id' := nat_iso.of_components
+    (λ Y, (F.fun_obj_preimage_iso Y))
+    (by obviously) }
+end
+
+end category_theory.equivalence
 
 end category_theory

739673cc0acf497999ebc0c1dae75129fb86c442
Chris Hughes <chrishughes24@gmail.com>
2018-11-26 02:08:24 +0000

    correct copyright

--- ring_theory/prime_count.lean
+++ ring_theory/prime_count.lean
@@ -1,7 +1,7 @@
 /-
-Copyright (c) 2018 Chris Hughes. All rights reserved.
+Copyright (c) 2018 Robert Y. Lewis. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
-Authors: Chris Hughes
+Authors: Robert Y. Lewis, Chris Hughes
 -/
 import data.nat.enat ring_theory.associated
 import tactic.converter.interactive

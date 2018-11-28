57ff341f51f4f916d9aee6137df68dc0dfe4461f
sgouezel <sebastien.gouezel@univ-rennes1.fr>
2018-11-28 12:24:49 +0100

    C is a proper metric space

--- analysis/complex.lean
+++ analysis/complex.lean
@@ -5,7 +5,7 @@ Author: Mario Carneiro
 
 Topology of the complex numbers.
 -/
-import data.complex.basic analysis.metric_space
+import data.complex.basic analysis.metric_space analysis.real
 
 noncomputable theory
 open filter
@@ -120,5 +120,38 @@ instance : topological_ring ℂ :=
 { continuous_mul := complex.continuous_mul, ..complex.topological_add_group }
 
 instance : topological_semiring ℂ := by apply_instance
+
+instance : proper_space ℂ :=
+⟨λx r, begin
+  have incl : closed_ball x r ⊆ (λp : ℝ × ℝ, p.1 + p.2 * I) '' (set.prod (closed_ball x.re r) (closed_ball x.im r)) :=
+  begin
+    assume y hy,
+    have : y = (λp : ℝ × ℝ, of_real (p.1) + of_real(p.2) * I) (y.re, y.im) :=
+      by simp,
+    rw this,
+    apply set.mem_image_of_mem _ _,
+    have A : y.re ∈ closed_ball x.re r := calc
+      dist (y.re) (x.re) =  _root_.abs((y-x).re) : by simp [real.dist_eq]
+      ... ≤ abs (y - x) : abs_re_le_abs _
+      ... ≤ r : hy,
+    have B : y.im ∈ closed_ball x.im r := calc
+      dist (y.im) (x.im) =  _root_.abs((y-x).im) : by simp [real.dist_eq]
+      ... ≤ abs (y - x) : abs_im_le_abs _
+      ... ≤ r : hy,
+    show (y.re, y.im) ∈ set.prod (closed_ball x.re r) (closed_ball x.im r), from ⟨A, B⟩,
+  end,
+  have cont : continuous (λp : ℝ × ℝ, of_real (p.1) + of_real (p.2) * I) := begin
+    apply continuous_add,
+    apply continuous.comp continuous_fst continuous_of_real,
+    refine continuous_mul _ continuous_const,
+    apply continuous.comp continuous_snd continuous_of_real,
+  end,
+  have : compact ((λp : ℝ × ℝ, of_real(p.1) + of_real(p.2) * I) '' (set.prod (closed_ball x.re r) (closed_ball x.im r))) :=
+  begin
+    apply compact_image _ cont,
+    exact compact_prod _ _ (proper_space.compact_ball _ _) (proper_space.compact_ball _ _)
+  end,
+  apply compact_of_is_closed_subset this is_closed_ball incl
+end⟩
 
 end complex

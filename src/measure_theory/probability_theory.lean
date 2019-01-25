4a49b24a287dfcf69e8ff7660bc75e75770b3905
Koundinya Vajjha <koundinya.vajjha@gmail.com>
2019-01-24 19:11:24 -0500

    fix(measure_theory/probability_theory): rearranging probability_theory and fixing the imports to reflect move to src

--- src/measure_theory/probability_theory.lean
+++ src/measure_theory/probability_theory.lean
@@ -3,16 +3,17 @@ Copyright (c) 2018 Koundinya Vajjha. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Koundinya Vajjha
 
-A formalization of Basic probability theory + Bayes theorem. 
+A formalization of Basic probability theory + Bayes theorem.
 -/
-import analysis.measure_theory.measure_space tactic.tidy 
--- import order.bounded_lattice
+import measure_theory.measure_space tactic.tidy
+
 open set measurable_space measure_theory ennreal lattice
 
 universe u
+
 /- The sample space -/
 variable {s : Type u}
-set_option pp.coercions true 
+
 section probability_measure
 
 structure probability_measure  (α : Type*) [measurable_space α] extends measure_theory.measure α :=
@@ -22,34 +23,32 @@ class probability_space (α : Type*) extends measurable_space α :=
 (μ {} : probability_measure α)
 
 instance prob.has_coe_to_fun {α} [probability_space α] : has_coe_to_fun (probability_measure α) :=
-⟨λ _, set α → ennreal, λ m, m.to_measure⟩ 
+⟨λ _, set α → ennreal, λ m, m.to_measure⟩
 
 /- The coercion of a ennreal probability measure to a nnreal probability measure -/
-def prob [probability_space s] (p : probability_measure s) (a : set s) 
-:= (p a).to_nnreal 
+def prob [probability_space s] (p : probability_measure s) (a : set s)
+:= (p a).to_nnreal
 
 variables [probability_space s] (p : probability_measure s) (a : set s)
 
-lemma prob_eq_coerc: 
-prob p a = ennreal.to_nnreal(p a) := rfl
- 
-lemma coe_def (a : set s): p a = p.to_measure a := rfl 
+lemma prob_eq_coerc: prob p a = ennreal.to_nnreal(p a) := rfl
 
-lemma prob_univ : p(univ) = 1 := 
-  p.is_one
+lemma coe_def (a : set s): p a = p.to_measure a := rfl
 
-lemma prob_univ_nnreal : prob p univ = 1 := 
-begin 
-rw prob, rw prob_univ, refl,
+lemma prob_univ : p(univ) = 1 := p.is_one
+
+lemma prob_univ_nnreal : prob p univ = 1 :=
+begin
+  rw [prob,prob_univ],refl,
 end
 
-lemma prob_le_1 : 
+lemma prob_le_1 :
 Π (a : set(s)), p(a) ≤ (1:ennreal) :=
 begin
   intros,
   rewrite ← prob_univ p,
   apply outer_measure.mono,
-  apply subset_univ, 
+  apply subset_univ,
 end
 
 lemma prob_ne_top :
@@ -57,68 +56,66 @@ lemma prob_ne_top :
 begin
   intros a,
   rw ← lattice.lt_top_iff_ne_top,
-  have h : (1 : ennreal) < ⊤, 
+  have h : (1 : ennreal) < ⊤,
     { rw lattice.lt_top_iff_ne_top,
     apply ennreal.one_ne_top, },
   apply lt_of_le_of_lt (prob_le_1 p a) h,
 end
 
-@[simp] lemma coe_prob (a : set s) :
-(prob p a : ennreal) = p a := coe_to_nnreal  (prob_ne_top _ _) 
+@[simp] lemma coe_prob (a : set s) : (prob p a : ennreal) = p a := coe_to_nnreal  (prob_ne_top _ _)
 
-lemma prob_le_1_nnreal : 
+lemma prob_le_1_nnreal :
 Π (a : set(s)), prob p a ≤ (1:nnreal) :=
 begin
   intros, rw ← ennreal.coe_le_coe,
   rw coe_prob,
   exact prob_le_1 p a,
-end 
+end
 
-lemma prob_empty : p(∅) = 0 := measure_empty 
+lemma prob_empty : p(∅) = 0 := measure_empty
 
-lemma prob_empty_nnreal : prob p ∅ = 0 := 
-begin 
+lemma prob_empty_nnreal : prob p ∅ = 0 :=
+begin
 rw prob, rw prob_empty, refl,
 end
 
-lemma prob_mono (a b : set s) (h1 : is_measurable a) 
+lemma prob_mono (a b : set s) (h1 : is_measurable a)
   (h2 : is_measurable b) : (a ⊆ b) → (p(a) ≤ p(b)) :=
-  measure_theory.measure_mono 
+  measure_theory.measure_mono
 
-lemma prob_mono_nnreal (a b : set s) (h1 : is_measurable a) 
+lemma prob_mono_nnreal (a b : set s) (h1 : is_measurable a)
   (h2 : is_measurable b) : (a ⊆ b) → (prob p a ≤ prob p b) :=
 begin
   intros h,
   rw ← ennreal.coe_le_coe,
-  rw [coe_prob,coe_prob], 
+  rw [coe_prob,coe_prob],
   apply prob_mono p a b h1 h2 h,
 end
 
-lemma prob_comp 
+lemma prob_comp
 (a : set s)
 (h: is_measurable a): p(-a) + p(a) = 1 :=
 begin
   intros, rewrite ← prob_univ p,
-  have h : ∀ x : set s, p x = p.to_measure x, {intros x, refl}, 
+  have h : ∀ x : set s, p x = p.to_measure x, {intros x, refl},
   repeat {rw h},
   rw [← compl_union_self a, measure_union _ _ _],
   apply disjoint_iff.2 (@compl_inter_self _ a),
   apply is_measurable.compl, repeat{assumption},
 end
 
-lemma prob_comp_nnreal 
+lemma prob_comp_nnreal
 (a : set s)
 (h: is_measurable a): prob p (-a) + prob p (a) = 1 :=
 begin
   rw ← ennreal.coe_eq_coe,
   simp, rw add_comm,
-  apply prob_comp p a h, 
+  apply prob_comp p a h,
 end
 
 
-lemma prob_union {s : Type u} [probability_space s] (s₁ s₂ : set s)
-(hd : disjoint s₁ s₂) (h₁ : is_measurable s₁) (h₂ : is_measurable s₂) (p : probability_measure s) :
-  p (s₁ ∪ s₂) = p s₁ + p s₂ := measure_theory.measure_union hd h₁ h₂ 
+lemma prob_union {s : Type u} [probability_space s] (s₁ s₂ : set s) (hd : disjoint s₁ s₂) (h₁ : is_measurable s₁) (h₂ : is_measurable s₂) (p : probability_measure s) :
+  p (s₁ ∪ s₂) = p s₁ + p s₂ := measure_theory.measure_union hd h₁ h₂
 
 lemma prob_union_nnreal {s : Type u} [probability_space s] (s₁ s₂ : set s)
 (hd : disjoint s₁ s₂) (h₁ : is_measurable s₁) (h₂ : is_measurable s₂) (p : probability_measure s) :
@@ -127,7 +124,7 @@ begin
   rw ← ennreal.coe_eq_coe, simp, apply prob_union s₁ s₂ hd h₁ h₂,
 end
 
-lemma prob_diff_inter {s : Type u} [probability_space s](p : probability_measure s) 
+lemma prob_diff_inter {s : Type u} [probability_space s](p : probability_measure s)
 (a b : set s) (h₁ : is_measurable a) (h₂ : is_measurable b) :
 p(b ∩ -a) + p(b ∩ a) = p(b) :=
 begin
@@ -143,19 +140,19 @@ begin
   apply is_measurable.inter h₂ h₁,
 end
 
-lemma prob_diff_inter_nnreal {s : Type u} [probability_space s](p : probability_measure s) 
+lemma prob_diff_inter_nnreal {s : Type u} [probability_space s](p : probability_measure s)
 (a b : set s) (h₁ : is_measurable a) (h₂ : is_measurable b) :
 prob p (b ∩ -a) + prob p (b ∩ a) = prob p (b) :=
 begin
   rw ← ennreal.coe_eq_coe,
   simp, rw add_comm,
-  apply prob_diff_inter p a b h₁ h₂, 
+  apply prob_diff_inter p a b h₁ h₂,
 end
 
-lemma prob_union_inter 
+lemma prob_union_inter
 (a b : set s) (g₁ : is_measurable a) (g₂ : is_measurable b) :
 p(a ∪ b) + p(a ∩ b) = p(a) + p(b) :=
-begin 
+begin
   have h₁ : a ∪ b = a ∪ (b ∩ -a),by
     rewrite [set.union_distrib_left, union_compl_self a,inter_univ],
   have h₂ : is_measurable(b ∩ -a),
@@ -171,17 +168,17 @@ begin
   apply disjoint_iff.2 h₄,
 end
 
-lemma prob_union_inter_nnreal 
+lemma prob_union_inter_nnreal
 (a b : set s) (g₁ : is_measurable a) (g₂ : is_measurable b) :
 prob p (a ∪ b) + prob p (a ∩ b) = prob p (a) + prob p (b) :=
 begin
-  rw ← ennreal.coe_eq_coe, 
+  rw ← ennreal.coe_eq_coe,
   simp only [coe_prob, ennreal.coe_add],
   apply prob_union_inter p a b g₁ g₂,
 end
 
 /-- The Bonnferroni inequality. --/
-lemma prob_add_le_inter_add_one 
+lemma prob_add_le_inter_add_one
 (a b : set s) (h_1 : is_measurable a) (h_2 : is_measurable b) :
 p(a) + p(b) ≤ p(a ∩ b) + 1 :=
 begin
@@ -191,136 +188,89 @@ begin
 end
 
 /-- The Bonnferroni inequality for a nnreal probability measure. --/
-lemma prob_add_le_inter_add_one_nnreal 
+lemma prob_add_le_inter_add_one_nnreal
 (a b : set s) (h_1 : is_measurable a) (h_2 : is_measurable b) :
 prob p a + prob p b ≤ prob p (a ∩ b) + 1:=
 begin
   rw ← ennreal.coe_le_coe, simp only [coe_prob, ennreal.coe_add, ennreal.coe_one],
-  apply prob_add_le_inter_add_one p a b h_1 h_2, 
+  apply prob_add_le_inter_add_one p a b h_1 h_2,
 end
 
--- def are_disj_ctble' (f : ℕ → set s) := ∀ i j : ℕ, (i ≠ j) → (f i) ∩ (f j) = ∅ 
- def are_disj_ctble' (f : ℕ → set s) := pairwise (disjoint on λ (i : ℕ), f i)
- 
-def is_partition' (f : ℕ  → set s) := 
-(are_disj_ctble' f) ∧ ((⋃i, f i) = univ) 
+def are_disj_ctble' (f : ℕ → set s) := pairwise (disjoint on λ (i : ℕ), f i)
+
+def is_partition' (f : ℕ  → set s) := (are_disj_ctble' f) ∧ ((⋃i, f i) = univ)
 
 
-lemma prob_partn 
+lemma prob_partn
 {f : ℕ → set s}
-{a : set s} 
-(h₁: is_partition' f) 
-(h₂ : is_measurable a) 
+{a : set s}
+(h₁: is_partition' f)
+(h₂ : is_measurable a)
 (h₃ : ∀ i : ℕ, is_measurable (f i))
 :
 p(a) = ∑i, p(a ∩ f i) :=
 begin
-  have g₁ : p(a) = p(a ∩ univ), by rw inter_univ,  
+  have g₁ : p(a) = p(a ∩ univ), by rw inter_univ,
   have g₂ : p(a) = p(⋃ i , (a ∩ f i)),by rw [g₁,← h₁.right, inter_Union_left],
   rw g₂,
-  apply p.m_Union, 
+  apply p.m_Union,
   {
-    intros j, 
-    apply is_measurable.inter, 
+    intros j,
+    apply is_measurable.inter,
     exact h₂, exact h₃ j,
   },
   intros m n h,
-  have g₃ : (λ (i : ℕ), a ∩ f i) m ∩ (λ (i : ℕ), a ∩ f i) n = ∅,
-  {
-    rw [set.inter_left_comm,set.inter_assoc,h₁.left m n],
-    simpa,
-  },
-  apply disjoint_iff.2 g₃,
+  refine disjoint_mono _ _ (h₁.left m n h),
+  repeat{apply inter_subset_right},
 end
 
-/- TODO : Prove that is_sum f (∑a, (f a : ennreal)).to_nnreal = f -/
-
-lemma prob_partn_ne_top {f : ℕ → set s} 
-(h₁: is_partition' f) 
-(h₂ : is_measurable a) 
+lemma prob_partn_ne_top {f : ℕ → set s}
+(h₁: is_partition' f)
+(h₂ : is_measurable a)
 (h₃ : ∀ i : ℕ, is_measurable (f i)):
 ((∑i, p (a ∩ f i)) ≠ ⊤) :=
 begin
 rw ← lt_top_iff_ne_top,
 have h : ∀ i , p(a ∩ f i) ≤ p (f i), {
-  intro j, apply prob_mono, by apply is_measurable.inter h₂ (h₃ j), by exact h₃ j, by finish,
+  intro j,
+  apply prob_mono,
+  by apply is_measurable.inter h₂ (h₃ j),
+  by exact h₃ j, by finish,
 },
-have k₁ : has_sum (λ (i : ℕ), p (f i)),
-{
-  have l₀ : tsum (λ (i : ℕ), p (f i)) = 1, 
-    {
-    rw [ ← prob_univ p,← h₁.right], 
-    symmetry,
-    apply p.m_Union h₃, 
-    exact h₁.left,
-    },
-existsi (1:ennreal), 
-rw dif_pos at l₀,
-unfold tsum at l₀, squeeze_simp at *, 
--- rw ← is_sum_iff_of_has_sum at l₀, 
--- exact l₀,
--- exact has_sum_spec l₀,
-},
+have k₁ : has_sum (λ (i : ℕ), p (f i)), by apply ennreal.has_sum,
 have k₂ : has_sum (λ (i : ℕ), p (a ∩ f i)),
+  by apply ennreal.has_sum,
 have k₃ : (∑i, p (a ∩ f i)) ≤ (∑i, p (f i)),
- {
-    apply tsum_le_tsum h _ _, 
-    apply has_sum_of_nonneg_of_le _ _ _,
- }
+{
+  apply tsum_le_tsum h k₂ k₁,
+},
+have l₀ : tsum (λ (i : ℕ), p (f i)) = 1,
+{
+  rw [ ← prob_univ p,← h₁.right],
+  symmetry,
+  apply p.m_Union h₃,
+  exact h₁.left,
+},
+rw l₀ at k₃,
+
+apply lt_of_le_of_lt k₃, rw lt_top_iff_ne_top,
+apply one_ne_top,
 end
 
-protected lemma tsum_coe' {α} {f : α → nnreal} (h : (∑a, (f a : ennreal)) ≠ ⊤) : (∑a, (f a : ennreal)) = ↑(tsum f) :=
-sorry
 
-lemma prob_partn_nnreal 
-{f : ℕ → set s} 
-(h₁: is_partition' f) 
+lemma prob_partn_nnreal
+{f : ℕ → set s}
+(h₁: is_partition' f)
 {a : set s}
-(h₂ : is_measurable a) 
+(h₂ : is_measurable a)
 (h₃ : ∀ i : ℕ, is_measurable (f i))
 :
 prob p a = ∑i, prob p (a ∩ f i) :=
 begin
-  have g₁ : prob p a = prob p (a ∩ univ), by rw inter_univ,  
-  have g₂ : prob p a = prob p (⋃ i , (a ∩ f i)),by rw [g₁,← h₁.right, inter_Union_left],
-  have h : p (⋃ (i : ℕ), a ∩ f i) = ∑ (i : ℕ), p (a ∩ f i),
-  {
-  apply p.m_Union, 
-  {
-    intros j, 
-    apply is_measurable.inter, 
-    exact h₂, exact h₃ j,
-  },
-  intros m n h,
-  have g₃ : (λ (i : ℕ), a ∩ f i) m ∩ (λ (i : ℕ), a ∩ f i) n = ∅,
-  {
-    rw [set.inter_left_comm,set.inter_assoc,h₁.left m n],
-    simpa,
-  },
-  apply disjoint_iff.2 g₃,
-  },
-  rw g₂,
-  rw [←ennreal.coe_eq_coe, coe_prob,h],
-  rw ← tsum_coe', simp, 
-  -- {
-  --   intros j, 
-  --   apply is_measurable.inter, 
-  --   exact h₂, exact h₃ j,
-  -- },
-  -- intros m n h,
-  -- have g₃ : (λ (i : ℕ), a ∩ f i) m ∩ (λ (i : ℕ), a ∩ f i) n = ∅,
-  -- {
-  --   rw [set.inter_left_comm,set.inter_assoc,h₁.left m n],
-  --   simpa,
-  -- },
-  -- apply disjoint_iff.2 g₃,
-  -- rw [←ennreal.coe_eq_coe, ←ennreal.tsum_coe],
-  -- simp, 
-  -- apply prob_partn p h₁ h₂ h₃,
-  
-  -- unfold has_sum,
-  -- existsi {prob p a},
-  -- apply has_sum_spec _ (∑i, prob p (a ∩ f i)),
+  rw [←ennreal.coe_eq_coe, ←ennreal.tsum_coe'], simp,
+  apply prob_partn p h₁ h₂ h₃,
+  simp, rw ← prob_partn p h₁ h₂ h₃,
+  apply prob_ne_top,
 end
 
 end probability_measure
@@ -339,13 +289,13 @@ notation `ℙ^`:95 p `[[`:95 a ` | `:95 b `]]`:0 := cond_prob p a b
 notation `ℙ^^`:95 p `[[`:95 a ` | `:95 b `]]`:0 := cond_prob_nnreal p a b
 
 
-lemma cond_prob_rw 
+lemma cond_prob_rw
 (a b : set s) (h₁ : p(b) ≠ 0):
 p(a ∩ b) = ℙ^p [[ a | b ]] * p(b) :=
 begin
   unfold cond_prob,
   rw [div_def,mul_assoc],
-  have g₁ : (1 : ennreal) < ⊤, 
+  have g₁ : (1 : ennreal) < ⊤,
   {
     rw lattice.lt_top_iff_ne_top,
     apply ennreal.one_ne_top,
@@ -355,19 +305,19 @@ begin
     intros a k,
     rw mul_comm,
     apply ennreal.mul_inv_cancel, exact k, rw ← lattice.lt_top_iff_ne_top,
-    apply lt_of_le_of_lt (prob_le_1 p a) g₁ 
+    apply lt_of_le_of_lt (prob_le_1 p a) g₁
   },
   rw g₂ b h₁, simp,
 end
 
 
-lemma cond_prob_rw_nnreal 
+lemma cond_prob_rw_nnreal
 (a b : set s) (h₁ : p(b) ≠ 0):
 prob p (a ∩ b) = ℙ^^p [[ a | b ]] * prob p b :=
 begin
   unfold cond_prob_nnreal,
   rw ← ennreal.coe_eq_coe, simp,
-  rw ← coe_div, simp, apply cond_prob_rw _ a b h₁,
+  rw coe_div, simp, apply cond_prob_rw _ a b h₁,
   {
     rw [ne.def, ← ennreal.coe_eq_coe, coe_prob],
     assumption,
@@ -375,7 +325,7 @@ begin
 end
 
 /- Bayes theorem for two sets -/
-@[simp] theorem cond_prob_swap 
+@[simp] theorem cond_prob_swap
 {a b : set s} (h₁ : p a ≠ 0) (h₂ : p b ≠ 0) :
 ℙ^p [[ b | a ]] * p(a) =  ℙ^p [[ a | b ]] * p(b) :=
 begin
@@ -391,20 +341,20 @@ begin
     intros a k,
     rw mul_comm,
     apply ennreal.mul_inv_cancel, exact k, rw ← lattice.lt_top_iff_ne_top,
-    apply lt_of_le_of_lt (prob_le_1 p a) g₁ 
+    apply lt_of_le_of_lt (prob_le_1 p a) g₁
   },
   rw [g₂ a,div_def,mul_assoc,g₂ b, mul_one],
   symmetry, rw [mul_one,set.inter_comm],
   assumption, assumption,
 end
 
-theorem cond_prob_swap_nnreal 
+theorem cond_prob_swap_nnreal
 {a b : set s} (h₁ : p a ≠ 0) (h₂ : p b ≠ 0) :
 ℙ^^p [[ b | a ]] * prob p a =  ℙ^^p [[ a | b ]] * prob p b :=
 begin
   unfold cond_prob_nnreal,
   rw ← ennreal.coe_eq_coe, simp,
-  repeat{rw ← coe_div}, simp,
+  repeat{rw coe_div}, simp,
   apply cond_prob_swap p h₁ h₂,
   repeat{
     rw [ne.def, ← ennreal.coe_eq_coe, coe_prob],
@@ -413,30 +363,30 @@ begin
 end
 
 /-- Law of Total Probability for an ennreal probability measure--/
-@[simp] lemma total_prob 
-{f : ℕ → set s} 
-(h₁: is_partition' f) 
+@[simp] lemma total_prob
+{f : ℕ → set s}
+(h₁: is_partition' f)
 {a : set s}
-(h₂ : is_measurable a) 
+(h₂ : is_measurable a)
 (h₃ : ∀ i : ℕ, is_measurable (f i))
 (h₄ : ∀ i : ℕ, p (f i) ≠ 0) :
 p(a) = ∑j, ℙ^p[[a | f j]] * p(f j)
 :=
-begin 
+begin
   rw prob_partn p h₁ h₂ h₃,
   have g : (λ (i : ℕ), p (a ∩ f i)) = (λ (i : ℕ),  ℙ^p [[ a | f i ]] * p(f i)),
   {
-    apply funext, intros x, apply cond_prob_rw, apply h₄ x,  
+    apply funext, intros x, apply cond_prob_rw, apply h₄ x,
   },
-  rw g, 
+  rw g,
 end
 
 /-- Law of Total Probability for a nnreal probability measure--/
 lemma total_prob_nnreal
-{f : ℕ → set s} 
-(h₁: is_partition' f) 
+{f : ℕ → set s}
+(h₁: is_partition' f)
 {a : set s}
-(h₂ : is_measurable a) 
+(h₂ : is_measurable a)
 (h₃ : ∀ i : ℕ, is_measurable (f i))
 (h₄ : ∀ i : ℕ, p (f i) ≠ 0) :
 prob p a = ∑j, ℙ^^p[[a | f j]] * prob p (f j)
@@ -445,48 +395,48 @@ begin
   rw prob_partn_nnreal p h₁ h₂ h₃,
    have g : (λ (i : ℕ),prob p (a ∩ f i)) = (λ (i : ℕ),  ℙ^^p [[ a | f i ]] * prob p(f i)),
   {
-    apply funext, intros x, apply cond_prob_rw_nnreal, apply h₄ x,  
+    apply funext, intros x, apply cond_prob_rw_nnreal, apply h₄ x,
   },
   rw g,
 end
 
 /-- Bayes theorem for an arbitrary partition of the sample space --/
-theorem cond_prob_partn_swap 
+theorem cond_prob_partn_swap
 (f : ℕ → set s) (b : set s)
-(h₁: is_partition' f) 
+(h₁: is_partition' f)
 (h₂ : p b ≠ 0)
 (h₃ : ∀ j : ℕ, is_measurable (f j))
 (h₄ : ∀ j : ℕ, p (f j) ≠ 0)
 (h₅ : is_measurable b)
-:  ∀ i : ℕ , 
-ℙ^p[[(f i) | b]] = (ℙ^p[[b | f i]])*(p(f i))/(∑j, ℙ^p[[b | f j]] * p(f j)):= 
+:  ∀ i : ℕ ,
+ℙ^p[[(f i) | b]] = (ℙ^p[[b | f i]])*(p(f i))/(∑j, ℙ^p[[b | f j]] * p(f j)):=
 begin
-    intros i,
-    rw ← total_prob p h₁ h₅ h₃ h₄,
-    rw cond_prob_swap p (h₄ i) h₂,
-    rw [div_def,mul_assoc],
-    rw ennreal.mul_inv_cancel h₂ (prob_ne_top p b),
-    simp,
+  intros i,
+  rw ← total_prob p h₁ h₅ h₃ h₄,
+  rw cond_prob_swap p (h₄ i) h₂,
+  rw [div_def,mul_assoc],
+  rw ennreal.mul_inv_cancel h₂ (prob_ne_top p b),
+  simp,
 end
 
 /-- Bayes theorem for an arbitrary partition of the sample space for an nnreal probability measure --/
 theorem cond_prob_partn_swap_nnreal
 (f : ℕ → set s) (b : set s)
-(h₁: is_partition' f) 
+(h₁: is_partition' f)
 (h₂ : p b ≠ 0)
 (h₃ : ∀ j : ℕ, is_measurable (f j))
 (h₄ : ∀ j : ℕ, p (f j) ≠ 0)
 (h₅ : is_measurable b)
-:  ∀ i : ℕ , 
-ℙ^^p[[(f i) | b]] = (ℙ^^p[[b | f i]])*(prob p(f i))/(∑j, ℙ^^p[[b | f j]] * prob p (f j)):= 
+:  ∀ i : ℕ ,
+ℙ^^p[[(f i) | b]] = (ℙ^^p[[b | f i]])*(prob p(f i))/(∑j, ℙ^^p[[b | f j]] * prob p (f j)):=
 begin
-    intros i,
-    rw ← total_prob_nnreal p h₁ h₅ h₃ h₄,
-    rw cond_prob_swap_nnreal p (h₄ i) h₂,
-    rw [nnreal.div_def,mul_assoc], 
-    rw nnreal.mul_inv_cancel,
-    simp, rw [ne.def, ← ennreal.coe_eq_coe, coe_prob],
-    assumption,
+  intros i,
+  rw ← total_prob_nnreal p h₁ h₅ h₃ h₄,
+  rw cond_prob_swap_nnreal p (h₄ i) h₂,
+  rw [nnreal.div_def,mul_assoc],
+  rw nnreal.mul_inv_cancel,
+  simp, rw [ne.def, ← ennreal.coe_eq_coe, coe_prob],
+  assumption,
 end
 
 end cond_prob

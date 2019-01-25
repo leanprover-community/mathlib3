06eba7f890f662fbc36d6653f36be26fbd970366
Rob Lewis <rob.y.lewis@gmail.com>
2019-01-25 16:10:58 +0100

    update authors on expr.lean (I don't know who's responsible for what)

--- src/meta/expr.lean
+++ src/meta/expr.lean
@@ -1,7 +1,7 @@
 /-
 Copyright (c) 2019 Robert Y. Lewis. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
-Author: Robert Y. Lewis
+Authors: Mario Carneiro, Simon Hudon, Scott Morrison, Keeley Hoek, Robert Y. Lewis
 
 Additional non-tactic operations on expr and related types
 -/

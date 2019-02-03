3109c4b354a3071bd8a2c239660e99bcb3da9b3a
Sean Leather <sean.leather@gmail.com>
2019-02-03 02:04:50 +0200

    chore(purge_olean.sh): a few small improvements (#661)
    * purge empty directories
    * Only print if an .olean is rm'd. This reduces the noise and reduces the
    script run time.
    * use git top-level dir to make the script relocatable
    * only affect src and test dirs
    * use bash instead of sed

--- purge_olean.sh
+++ purge_olean.sh
@@ -1,13 +1,25 @@
-#! /bin/sh
-# olean=./src/tactic/ring.olean
-# lean_file=`echo $$olean_file | sed "s/\\.olean/.lean/"`
+#!/bin/bash
 
-for olean_file in `find . -name "*.olean"`;
+# Top-level root directory of the Git repository
+root=`git rev-parse --show-toplevel`
+
+# A list of directories containing .olean files. We are being conservative to
+# avoid traversing irrelevant directories and affecting directories we do not
+# want changed (e.g. $root/.git).
+dirs="$root/src $root/test"
+
+# Delete every <path>.olean without a matching <path>.lean.
+
+for olean_file in `find $dirs -name "*.olean"`
 do
-    echo olean file: $olean_file
-    lean_file=`echo $olean_file | sed "s/\.olean/.lean/"`
+    lean_file=${olean_file/%.olean/.lean}
     if [ ! -e $lean_file ]; then
-        echo "absent " $lean_file;
+        echo "rm $olean_file"
         rm $olean_file
     fi
 done
+
+# Delete all empty directories. An empty directory may have been created if it
+# does not contain any .lean files and all of its .olean files were deleted.
+
+find $dirs -type d -empty -delete

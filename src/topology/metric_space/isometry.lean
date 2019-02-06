03909d3a03f28ee2595a32af5e770f48c7b97f47
sgouezel <sebastien.gouezel@univ-rennes1.fr>
2019-02-06 22:47:42 +0100

    update isometry.lean

--- src/topology/metric_space/isometry.lean
+++ src/topology/metric_space/isometry.lean
@@ -3,9 +3,9 @@ Copyright (c) 2018 Sébastien Gouëzel. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Isometries of emetric and metric spaces
 Authors: Sébastien Gouëzel
-We define emetric isometries, i.e., maps between emetric spaces that preserve
-the edistance, and metric isometries, i.e., maps between metric spaces that
-preserve the distance. We prove their basic properties.
+We define isometries, i.e., maps between emetric spaces that preserve
+the edistance (on metric spaces, these are exactly the maps that preserve distances),
+and prove their basic properties.
 -/
 
 import topology.metric_space.basic topology.instances.real
@@ -15,161 +15,198 @@ noncomputable theory
 universes u v w
 variables {α : Type u} {β : Type v} {γ : Type w}
 
-open function set metric
+open function set
 
-namespace emetric
-section isometry
+/-- An isometry (also known as isometric embedding) is a map preserving the edistance
+between emetric spaces, or equivalently the distance between metric space.  -/
+def isometry [emetric_space α] [emetric_space β] (f : α → β) : Prop :=
+∀x1 x2 : α, edist (f x1) (f x2) = edist x1 x2
+
+/-- On metric spaces, a map is an isometry if and only if it preserves distances. -/
+lemma isometry_emetric_iff_metric [metric_space α] [metric_space β] {f : α → β} :
+  isometry f ↔ (∀x y, dist (f x) (f y) = dist x y) :=
+⟨assume H x y, by simp [dist_edist, H x y],
+assume H x y, by simp [edist_dist, H x y]⟩
+
+/-- An isometry preserves edistances. -/
+theorem isometry.edist_eq [emetric_space α] [emetric_space β] {f : α → β} {x y : α} (hf : isometry f) :
+  edist (f x) (f y) = edist x y :=
+hf x y
+
+/-- An isometry preserves distances. -/
+theorem isometry.dist_eq [metric_space α] [metric_space β] {f : α → β} {x y : α} (hf : isometry f) :
+  dist (f x) (f y) = dist x y :=
+by rw [dist_edist, dist_edist, hf]
+
+section emetric_isometry
 
 variables [emetric_space α] [emetric_space β] [emetric_space γ]
 variables {f : α → β} {x y z : α}  {s : set α}
 
-/-- An isometry is a map preserving the edistance -/
-def isometry_on (f : α → β) (s : set α) : Prop :=
-∀⦃x1 x2 : α⦄, x1 ∈ s → x2 ∈ s → edist (f x1) (f x2) = edist x1 x2
-
 /-- An isometry is injective -/
-lemma isometry_on.inj_on (h : isometry_on f s) : inj_on f s :=
-λx y hx hy hxy, edist_eq_zero.1 $
-calc edist x y = edist (f x) (f y) : (h hx hy).symm
+lemma isometry.injective (h : isometry f) : injective f :=
+λx y hxy, edist_eq_zero.1 $
+calc edist x y = edist (f x) (f y) : (h x y).symm
          ...   = 0 : by rw [hxy]; simp
 
-/-- Any map on the empty set is an isometry -/
-theorem isometry_on_empty : isometry_on f ∅ :=
-λx y hx hy, false.elim hx
+/-- Any map on a subsingleton is an isometry -/
+theorem isometry_subsingleton [subsingleton α] : isometry f :=
+λx y, by rw subsingleton.elim x y; simp
 
-/-- Any map on a singleton is an isometry -/
-theorem isometry_on_singleton {a : α} : isometry_on f {a} :=
-assume x y hx hy,
-have A : x = y := by simp at hx; simp at hy; simp [hx, hy],
-by simp [A]
+/-- The identity is an isometry -/
+lemma isometry_id : isometry (id : α → α) :=
+λx y, rfl
 
 /-- The composition of isometries is an isometry -/
-theorem isometry_on_comp {g : β → γ} {f : α → β} {a : set α} {b : set β}
-  (h₁ : maps_to f a b) (h₂ : isometry_on g b) (h₃: isometry_on f a) :
-  isometry_on (g ∘ f) a :=
-assume x y hx hy, calc
-  edist ((g ∘ f) x) ((g ∘ f) y) = edist (f x) (f y) : h₂ (h₁ hx) (h₁ hy)
-                            ... = edist x y : h₃ hx hy
-
-open function
-
-/-- The inverse of an isometry is an isometry -/
-theorem isometry_on.inv_fun_on [inhabited α] (I : isometry_on f s) :
-  isometry_on (inv_fun_on f s) (f '' s) :=
-begin
-  rintros x y ⟨x', ⟨x's, hx'⟩⟩ ⟨y', ⟨y's, hy'⟩⟩,
-  have Ax : inv_fun_on f s (f x') = x' := inv_fun_on_eq' I.inj_on x's,
-  have Ay : inv_fun_on f s (f y') = y' := inv_fun_on_eq' I.inj_on y's,
-  simp [hx'.symm, hy'.symm, Ax, Ay, I x's y's]
-end
+theorem isometry.comp {g : β → γ} (hf : isometry f) (hg : isometry g) : isometry (g ∘ f) :=
+assume x y, calc
+  edist ((g ∘ f) x) ((g ∘ f) y) = edist (f x) (f y) : hg _ _
+                            ... = edist x y : hf _ _
 
 /-- An isometry is an embedding -/
-theorem isometry_on.uniform_embedding {f : α → β}
-  (hf : isometry_on f univ) : uniform_embedding f :=
+theorem isometry.uniform_embedding (hf : isometry f) : uniform_embedding f :=
 begin
-  have hf' : ∀x y, edist (f x) (f y) = edist x y := λx y, hf (mem_univ x) (mem_univ y),
-  refine uniform_embedding_iff.2 ⟨_, _, _⟩,
+  refine emetric.uniform_embedding_iff.2 ⟨_, _, _⟩,
   { assume x y hxy,
     have : edist (f x) (f y) = 0 := by simp [hxy],
     have : edist x y = 0 :=
-      begin have A := hf' x y, rwa this at A, exact eq.symm A end,
+      begin have A := hf x y, rwa this at A, exact eq.symm A end,
     by simpa using this },
-  { refine uniform_continuous_iff.2 (λε εpos, _),
+  { rw emetric.uniform_continuous_iff,
+    assume ε εpos,
     existsi [ε, εpos],
-    simp [hf'] },
+    simp [hf.edist_eq] },
   { assume δ δpos,
     existsi [δ, δpos],
-    simp [hf'] }
+    simp [hf.edist_eq] }
 end
 
-/-- An isometry is continuous -/
-lemma isometry_on.continuous {f : α → β}
-  (hf : isometry_on f univ) : continuous f :=
+/-- An isometry is continuous. -/
+lemma isometry.continuous (hf : isometry f) : continuous f :=
 hf.uniform_embedding.embedding.continuous
 
+/-- The inverse of an isometry is an isometry. -/
+lemma isometry.inv (e : α ≃ β) (h : isometry e.to_fun) : isometry e.inv_fun :=
+λx y, by rw [← h, e.right_inv _, e.right_inv _]
+
 /-- Isometries preserve the diameter -/
-lemma diam_image_of_isometry [emetric_space β] {f : α → β} (hf : isometry_on f s) :
-  diam (f '' s) = diam s :=
+lemma emetric.isometry.diam_image (hf : isometry f) {s : set α}:
+  emetric.diam (f '' s) = emetric.diam s :=
 begin
   refine le_antisymm _ _,
   { apply lattice.Sup_le _,
     simp only [and_imp, set.mem_image, set.mem_prod, exists_imp_distrib, prod.exists],
     assume b x x' z zs xz z' z's x'z' hb,
-    rw [← hb, ← xz, ← x'z', hf zs z's],
-    exact edist_le_diam_of_mem zs z's },
+    rw [← hb, ← xz, ← x'z', hf z z'],
+    exact emetric.edist_le_diam_of_mem zs z's },
   { apply lattice.Sup_le _,
     simp only [and_imp, set.mem_image, set.mem_prod, exists_imp_distrib, prod.exists],
     assume b x x' xs x's hb,
-    rw [← hb, ← hf xs x's],
-    exact edist_le_diam_of_mem (mem_image_of_mem _ xs) (mem_image_of_mem _ x's) }
+    rw [← hb, ← hf x x'],
+    exact emetric.edist_le_diam_of_mem (mem_image_of_mem _ xs) (mem_image_of_mem _ x's) }
 end
 
-end isometry
-end emetric
+end emetric_isometry --section
 
-namespace metric
-section isometry
+section metric_isometry
 
 variables [metric_space α] [metric_space β] [metric_space γ]
 variables {f : α → β} {x y z : α} {s : set α}
 
-def isometry_on (f : α → β) (s : set α) : Prop :=
-∀⦃x1 x2 : α⦄, x1 ∈ s → x2 ∈ s → dist (f x1) (f x2) = dist x1 x2
-
-lemma emetric_isometry_on_iff_isometry_on : emetric.isometry_on f s ↔ isometry_on f s :=
-⟨assume H x y hx hy, by simp [dist_edist, H hx hy],
-assume H x y hx hy, by simp [edist_dist, H hx hy]⟩
-
-/-- An isometry is injective -/
-lemma isometry_on.inj_on (h : isometry_on f s) : inj_on f s :=
-(emetric_isometry_on_iff_isometry_on.2 h).inj_on
-
-/-- Any map on the empty set is an isometry -/
-theorem isometry_on_empty : isometry_on f ∅ :=
-assume x y hx hy, false.elim hx
-
-/-- Any map on a singleton is an isometry -/
-theorem isometry_on_singleton {a : α} : isometry_on f {a} :=
-assume x y hx hy,
-have A : x = y := by simp at hx; simp at hy; simp [hx, hy],
-by simp [A]
-
-/-- The composition of isometries is an isometry -/
-theorem isometry_on_comp {g : β → γ} {f : α → β} {a : set α} {b : set β}
-  (h₁ : maps_to f a b) (h₂ : isometry_on g b) (h₃: isometry_on f a) :
-  isometry_on (g ∘ f) a :=
-assume x y hx hy, calc
-  dist ((g ∘ f) x) ((g ∘ f) y) = dist (f x) (f y) : h₂ (h₁ hx) (h₁ hy)
-                           ... = dist x y : h₃ hx hy
-
-open function
-/-- The inverse of an isometry is an isometry -/
-theorem isometry_on.inv_fun_on [inhabited α] (I : isometry_on f s) :
-  isometry_on (inv_fun_on f s) (f '' s) :=
-begin
-  rintros x y ⟨x', ⟨x's, hx'⟩⟩ ⟨y', ⟨y's, hy'⟩⟩,
-  have Ax : inv_fun_on f s (f x') = x' := inv_fun_on_eq' I.inj_on x's,
-  have Ay : inv_fun_on f s (f y') = y' := inv_fun_on_eq' I.inj_on y's,
-  simp [hx'.symm, hy'.symm, Ax, Ay, I x's y's]
-end
-
-/-- An isometry is a uniform embedding -/
-theorem isometry_on.uniform_embedding (h : isometry_on f univ) : uniform_embedding f :=
-(emetric_isometry_on_iff_isometry_on.2 h).uniform_embedding
-
-/-- An isometry is continuous -/
-lemma isometry_on.continuous
-  (hf : isometry_on f univ) : continuous f :=
-hf.uniform_embedding.embedding.continuous
-
 /-- The injection from a subtype is an isometry -/
-lemma isometry_on_subtype.val {s : set α} : isometry_on (subtype.val : s → α) univ :=
-λx y hx hy, rfl
+lemma isometry_subtype_val {s : set α} : isometry (subtype.val : s → α) :=
+λx y, rfl
 
 /-- An isometry preserves the diameter -/
-lemma diam_image_of_isometry [metric_space β] {f : α → β} (hf : isometry_on f s) :
-  diam (f '' s) = diam s :=
-by rw [diam, diam, emetric.diam_image_of_isometry (emetric_isometry_on_iff_isometry_on.2 hf)]
+lemma metric.isometry.diam_image (hf : isometry f) :
+  metric.diam (f '' s) = metric.diam s :=
+by rw [metric.diam, metric.diam, emetric.isometry.diam_image hf]
 
-end isometry --section
-end metric --namespace
+end metric_isometry --section
+
+/-- α and β are isometric, also called metric isomorphism -/
+structure isometric (α : Type*) (β : Type*) [emetric_space α] [emetric_space β]
+  extends α ≃ β :=
+(isometry_to_fun  : isometry to_fun)
+(isometry_inv_fun : isometry inv_fun)
+
+infix ` ≃ᵢ`:50 := isometric
+
+namespace isometric
+variables [emetric_space α] [emetric_space β] [emetric_space γ]
+
+instance : has_coe_to_fun (α ≃ᵢ β) := ⟨λ_, α → β, λe, e.to_equiv⟩
+
+lemma coe_eq_to_equiv (h : α ≃ᵢ β) (a : α) : h a = h.to_equiv a := rfl
+
+protected def to_homeomorph (h : α ≃ᵢ β) : α ≃ₜ β :=
+{ continuous_to_fun  := (isometry_to_fun h).continuous,
+  continuous_inv_fun := (isometry_inv_fun h).continuous,
+  .. h.to_equiv }
+
+lemma coe_eq_to_homeomorph (h : α ≃ᵢ β) (a : α) :
+  h a = h.to_homeomorph a := rfl
+
+lemma to_homeomorph_to_equiv (h : α ≃ᵢ β) :
+  h.to_homeomorph.to_equiv = h.to_equiv :=
+by ext; refl
+
+protected def refl (α : Type*) [emetric_space α] : α ≃ᵢ α :=
+{ isometry_to_fun := isometry_id, isometry_inv_fun := isometry_id, .. equiv.refl α }
+
+protected def trans (h₁ : α ≃ᵢ β) (h₂ : β ≃ᵢ γ) : α ≃ᵢ γ :=
+{ isometry_to_fun  := h₁.isometry_to_fun.comp h₂.isometry_to_fun,
+  isometry_inv_fun := h₂.isometry_inv_fun.comp h₁.isometry_inv_fun,
+  .. equiv.trans h₁.to_equiv h₂.to_equiv }
+
+protected def symm (h : α ≃ᵢ β) : β ≃ᵢ α :=
+{ isometry_to_fun  := h.isometry_inv_fun,
+  isometry_inv_fun := h.isometry_to_fun,
+  .. h.to_equiv.symm }
+
+protected lemma isometry (h : α ≃ᵢ β) : isometry h := h.isometry_to_fun
+
+lemma symm_comp_self (h : α ≃ᵢ β) : ⇑h.symm ∘ ⇑h = id :=
+funext $ assume a, h.to_equiv.left_inv a
+
+lemma self_comp_symm (h : α ≃ᵢ β) : ⇑h ∘ ⇑h.symm = id :=
+funext $ assume a, h.to_equiv.right_inv a
+
+lemma range_coe (h : α ≃ᵢ β) : range h = univ :=
+eq_univ_of_forall $ assume b, ⟨h.symm b, congr_fun h.self_comp_symm b⟩
+
+lemma image_symm (h : α ≃ᵢ β) : image h.symm = preimage h :=
+image_eq_preimage_of_inverse h.symm.to_equiv.left_inv h.symm.to_equiv.right_inv
+
+lemma preimage_symm (h : α ≃ᵢ β) : preimage h.symm = image h :=
+(image_eq_preimage_of_inverse h.to_equiv.left_inv h.to_equiv.right_inv).symm
+
+end isometric
+
+/-- An isometry induces an isometric isomorphism between the source space and the
+range of the isometry. -/
+lemma isometry.isometric_on_range [emetric_space α] [emetric_space β] {f : α → β} (h : isometry f) :
+  α ≃ᵢ range f :=
+{ isometry_to_fun := λx y,
+  begin
+    change edist ((equiv.set.range f _) x) ((equiv.set.range f _) y) = edist x y,
+    rw [equiv.set.range_apply f h.injective, equiv.set.range_apply f h.injective],
+    exact h x y
+  end,
+  isometry_inv_fun :=
+  begin
+    apply isometry.inv,
+    assume x y,
+    change edist ((equiv.set.range f _) x) ((equiv.set.range f _) y) = edist x y,
+    rw [equiv.set.range_apply f h.injective, equiv.set.range_apply f h.injective],
+    exact h x y
+  end,
+  .. equiv.set.range f h.injective }
+
+lemma isometry.isometric_on_range_apply [emetric_space α] [emetric_space β]
+  {f : α → β} (h : isometry f) (x : α) : h.isometric_on_range x = ⟨f x, mem_range_self _⟩ :=
+begin
+  dunfold isometry.isometric_on_range,
+  rw ← equiv.set.range_apply f h.injective x,
+  refl
+end

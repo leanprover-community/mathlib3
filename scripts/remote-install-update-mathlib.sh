503a4235959bd3ec26e1831dd26dd16ff402363e
Simon Hudon <simon.hudon@gmail.com>
2019-02-12 09:32:39 -0500

    feat(update-mathlib): improve setup and error messages

--- scripts/remote-install-update-mathlib.sh
+++ scripts/remote-install-update-mathlib.sh
@@ -5,3 +5,4 @@ chmod +x update-mathlib.py
 mkdir -p $HOME/.mathlib/bin || true
 cp update-mathlib.py $HOME/.mathlib/bin/update-mathlib
 echo "export PATH=\"\$HOME/.mathlib/bin:\$PATH\" " >> $HOME/.profile
+source $HOME/.profile

94571a746b048a17b414c3ee8308364af9f1ce43
Scott Morrison <scott@tqft.net>
2019-02-28 21:28:46 +1100

    looking pretty nice

--- src/tactic/fin_cases.lean
+++ src/tactic/fin_cases.lean
@@ -3,49 +3,80 @@ Copyright (c) 2018 Scott Morrison. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Author: Scott Morrison
 
-Case bashing on `fin n`, for explicit numerals `n`.
+Case bashing:
+* on `x ∈ A`, for `A : finset α` or `A : list α`, or
+* on `x : A`, with `[fintype A]`.
 -/
-import data.fin
-import data.finset
+import data.fintype
+import tactic.norm_num
 
 namespace tactic
 open lean.parser
 open interactive interactive.types expr
+open conv.interactive
 
-meta def finset_cases_at (e : expr) : tactic unit :=
-do `(%%x ∈ %%A) ← infer_type e,
-   `(finset %%α) ← infer_type A,
-   eval_expr (list ℕ) `(@finset.sort ℕ (<) _ _ _ _ %%A) >>= trace
+-- Maybe this is controversial.
+-- When we do `cases h`, for `h : x ∈ range n`, a wild `nat.add` appears.
+-- This simp lemma wraps it up again...
+@[simp] lemma nat.add_plus (a b : ℕ) : nat.add a b = a + b := rfl
 
-meta def fin_cases_at (e : expr) : tactic unit :=
-do `(fin %%n) ← infer_type e,
-   n ← eval_expr ℕ n,
-   [(_, [val, bd], _)] ← cases_core e [`val, `bd],
-   -- We now call `cases val` n times, rotating the generated goals out of the way.
-   iterate_at_most n (do val ← get_local `val, cases val, rotate 1),
-   -- We've got an absurd hypothesis `bd`, but it is messy: it looks like
-   -- `nat.succ (... (nat.succ val)) < n`
-   -- So we rewrite it as `bd : val + 1 + ... + 1 < n`, and use `dec_trivial` to kill it.
-   ss ← mk_const `nat.succ_eq_add_one,
-   bd ← get_local `bd,
-   (list.range n).mfoldl (λ bd _, do rewrite_hyp ss bd) bd,
-   to_expr ``(absurd %%bd dec_trivial) >>= exact,
-   -- We put the goals back in order, and clear the `bd` hypotheses.
-   iterate_exactly n (do rotate_right 1,
-                         try `[rw [fin.mk_val]],
-                         try (get_local `bd >>= clear))
+/-- Checks that the expression looks like `x ∈ A` for `A : finset α` or `A : list α`,
+    and returns the type α. -/
+meta def guard_mem_fin (e : expr) : tactic expr :=
+do t ← infer_type e,
+   match t with
+   | `(%%x ∈ %%A) :=
+      do t ← infer_type A,
+        match t with
+        | `(finset %%α) := return α
+        | `(list %%α)   := return α
+        | _ := failed
+        end
+   | `(@list.mem %%α %%x %%A) := return α
+   | _ := failed
+   end
+
+meta def fin_cases_at : expr → tactic unit
+| e :=
+focus1 $
+-- Deal with `x ∈ A` hypotheses first:
+(do ty ← guard_mem_fin e,
+   numeric ← option.is_some <$> try_core (unify ty `(ℕ) <|> unify ty `(ℤ) <|> unify ty `(ℚ)),
+   result ← cases_core e,
+   match result with
+   -- We have a goal with an equation `s`, and a second goal with a smaller `e : x ∈ _`.
+   | [(_, [s], _), (_, [e], _)] :=
+     do let sn := local_pp_name s,
+        if numeric then
+          -- tidy up with norm_num
+          tactic.interactive.conv (some sn) none (to_rhs >> `[try { norm_num }])
+        else skip,
+        s ← get_local sn,
+        try `[subst %%s],
+        rotate_left 1,
+        fin_cases_at e
+   -- No cases; we're done.
+   | [] := skip
+   | _ := failed
+   end) <|>
+-- Otherwise deal with `x : A`, where `[fintype A]` is available:
+(do
+   ty ← infer_type e,
+   i ← to_expr ``(fintype %%ty) >>= mk_instance,
+   t ← to_expr ``(%%e ∈ @fintype.elems %%ty %%i),
+   v ← to_expr ``(@fintype.complete %%ty %%i %%e),
+   h ← assertv `h t v,
+   fin_cases_at h)
 
 namespace interactive
 private meta def hyp := tk "*" *> return none <|> some <$> ident
 
-meta def finset_cases : parse hyp → tactic unit
-| none := do ctx ← local_context,
-             ctx.mfirst finset_cases_at <|> fail "No explicit `x ∈ A`, where `A : finset ℕ` hypotheses."
-| (some n) := do h ← get_local n, finset_cases_at h
-
 /--
-`fin_cases h` performs case analysis on a hypothesis `h : fin n`, where `n` is an explicit natural
-number. `fin_cases *` performs case analysis on all suitable hypotheses.
+`fin_cases h` performs case analysis on a hypothesis of the form
+1) `h : A`, where `[fintype A]` is available, or
+2) `h ∈ A`, where `A : finset X` or `A : list X`.
+
+`fin_cases *` performs case analysis on all suitable hypotheses.
 
 As an example, in
 ```
@@ -59,14 +90,9 @@ after `fin_cases p`, there are three goals, `f 0`, `f 1`, and `f 2`.
 -/
 meta def fin_cases : parse hyp → tactic unit
 | none := do ctx ← local_context,
-             ctx.mfirst fin_cases_at <|> fail "No explicit `fin n` hypotheses."
+             ctx.mfirst fin_cases_at <|> fail "No hypothesis of the forms `x ∈ A`, where `A : finset ℕ`, or `x : A`, with `[fintype A]`."
 | (some n) := do h ← get_local n, fin_cases_at h
+
 end interactive
 
 end tactic
-
-open finset
-example (x : ℕ) (h : x ∈ Ico 2 4) : x = 2 ∨ x = 3 :=
-begin
-  finset_cases h,
-end
--- test/fin_cases.lean
+++ test/fin_cases.lean
@@ -1,5 +1,16 @@
 import tactic.fin_cases
+import data.nat.prime
+import group_theory.perm
+import tactic.norm_num
 
+example (p : ℕ) (h : p ∈ list.range 2) : true :=
+begin
+  cases h,
+  trivial,
+  cases h,
+  trivial,
+  cases h,
+end
 
 example (f : ℕ → Prop) (p : fin 3) (h0 : f 0) (h1 : f 1) (h2 : f 2) : f p.val :=
 begin
@@ -14,4 +25,40 @@ begin
   success_if_fail { fin_cases * },
   success_if_fail { fin_cases y },
   all_goals { simp },
+end
+
+open finset
+example (x : ℕ) (h : x ∈ Ico 2 5) : x = 2 ∨ x = 3 ∨ x = 4 :=
+begin
+  fin_cases h; simp
+end
+
+open nat
+example (x : ℕ) (h : x ∈ [2,3,5,7]) : x = 2 ∨ x = 3 ∨ x = 5 ∨ x = 7 :=
+begin
+  fin_cases h; simp
+end
+
+instance (n : ℕ) : decidable (prime n) := decidable_prime_1 n
+example (x : ℕ) (h : x ∈ (range 10).filter prime) : x = 2 ∨ x = 3 ∨ x = 5 ∨ x = 7 :=
+begin
+  fin_cases h; exact dec_trivial
+end
+
+open equiv.perm
+example (x : (Σ (a : fin 4), fin 4)) (h : x ∈ fin_pairs_lt 4) : x.1.val < 4 :=
+begin
+  fin_cases h; simp,
+  any_goals { exact dec_trivial },
+end
+
+example (x : fin 3) : x.val < 5 :=
+begin
+  fin_cases x; exact dec_trivial
+end
+
+example (f : ℕ → Prop) (p : fin 3) (h0 : f 0) (h1 : f 1) (h2 : f 2) : f p.val :=
+begin
+  fin_cases *,
+  all_goals { assumption }
 end

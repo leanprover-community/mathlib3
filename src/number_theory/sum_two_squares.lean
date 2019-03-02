20a2af008cbc78f7ba08c0ad349f662a138fb41e
Chris Hughes <chrishughes24@gmail.com>
2019-03-02 21:03:23 +0000

    hopefully fixed for good this time

--- src/number_theory/sum_two_squares.lean
+++ src/number_theory/sum_two_squares.lean
@@ -62,7 +62,7 @@ have hnap : (norm a).nat_abs = p, from ((hp.mul_eq_prime_pow_two_iff
   by rw [← int.coe_nat_inj', int.coe_nat_pow, _root_.pow_two,
     ← @norm_nat_cast (-1), hpab];
     simp).1,
-⟨a.re.nat_abs, a.im.nat_abs, by simpa [nat_abs_norm_eq, _root_.pow_two] using hnap⟩
+⟨a.re.nat_abs, a.im.nat_abs, by simpa [nat_abs_norm_eq, pow_two] using hnap⟩
 
 end prime
 end nat

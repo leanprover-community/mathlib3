71a164c6c0589f6886c8e154bbc81dd9b9501cf3
Scott Morrison <scott@tqft.net>
2019-03-16 15:30:26 +1100

    add lex_decidable_linear_order

--- src/order/lexicographic.lean
+++ src/order/lexicographic.lean
@@ -3,55 +3,112 @@ Copyright (c) 2019 Scott Morrison. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Author: Scott Morrison
 
-Lexicographic preorder / partial_order / linear_order.
+Lexicographic preorder / partial_order / linear_order / decidable_linear_order,
+for pairs and dependent pairs.
 -/
 import order.basic
+import tactic.squeeze
 
 universes u v
 
 variables {α : Type u} {β : Type v}
 
-/-- Dictionary / lexicographic ordering on pairs. -/
-def lex_preorder [preorder α] [preorder β] : preorder (α × β) :=
-{ le := λ a b, a.1 < b.1 ∨ (a.1 = b.1 ∧ a.2 ≤ b.2),
-  le_refl := λ a, or.inr ⟨rfl, le_refl _⟩,
-  le_trans := λ a b c h₁ h₂,
-  begin
-    cases h₁,
-    { left, cases h₂,
-      { exact lt_trans h₁ h₂ },
-      { rwa ←h₂.left } },
-    { cases h₂,
-      { exact or.inl (by rwa h₁.left) },
-      { exact or.inr ⟨eq.trans h₁.1 h₂.1, le_trans h₁.2 h₂.2⟩ }
-    } end }
+/-- Dictionary / lexicographic ordering on pairs.
+    As the pointwise partial order is also useful,
+    neither is set as an instance by default.  -/
+def lex_has_le [preorder α] [preorder β] : has_le (α × β) :=
+{ le := λ a b, a.1 < b.1 ∨ (a.1 = b.1 ∧ a.2 ≤ b.2) }
 
+/-- Dictionary / lexicographic preorder for pairs. -/
+def lex_preorder [preorder α] [preorder β] : preorder (α × β) :=
+{ le_refl := λ a, or.inr ⟨rfl, le_refl _⟩,
+  le_trans :=
+  begin
+    rintros ⟨a₁, b₁⟩ ⟨a₂, b₂⟩ ⟨a₃, b₃⟩ (a₁₂_lt | ⟨a₁₂_eq, b₁₂_le⟩) (a₂₃_lt | ⟨a₂₃_eq, b₂₃_le⟩),
+    { exact or.inl (lt_trans a₁₂_lt a₂₃_lt) },
+    { left, rwa ←a₂₃_eq },
+    { left, rwa a₁₂_eq },
+    { exact or.inr ⟨eq.trans a₁₂_eq a₂₃_eq, le_trans b₁₂_le b₂₃_le⟩, }
+    end,
+  .. lex_has_le }
+
+/-- Dictionary / lexicographic partial for pairs. -/
 def lex_partial_order [partial_order α] [partial_order β] : partial_order (α × β) :=
 { le_antisymm :=
-begin
-  rintros ⟨a₁, b₁⟩ ⟨a₂, b₂⟩ (a₁₂_lt | ⟨a₁₂_eq, b₁₂_le⟩) (a₂₁_lt | ⟨a₂₁_eq, b₂₁_le⟩),
-  { exfalso, exact lt_irrefl a₁ (lt_trans a₁₂_lt a₂₁_lt) },
-  { exfalso, rw a₂₁_eq at a₁₂_lt, exact lt_irrefl a₁ a₁₂_lt },
-  { exfalso, rw a₁₂_eq at a₂₁_lt, exact lt_irrefl a₂ a₂₁_lt },
-  { dsimp at a₁₂_eq, rw a₁₂_eq, have h := le_antisymm b₁₂_le b₂₁_le, dsimp at h, rw h }
-end
+  begin
+    rintros ⟨a₁, b₁⟩ ⟨a₂, b₂⟩ (a₁₂_lt | ⟨a₁₂_eq, b₁₂_le⟩) (a₂₁_lt | ⟨a₂₁_eq, b₂₁_le⟩),
+    { exact false.elim (lt_irrefl a₁ (lt_trans a₁₂_lt a₂₁_lt)) },
+    { rw a₂₁_eq at a₁₂_lt, exact false.elim (lt_irrefl a₁ a₁₂_lt) },
+    { rw a₁₂_eq at a₂₁_lt, exact false.elim (lt_irrefl a₂ a₂₁_lt) },
+    { dsimp at a₁₂_eq, subst a₁₂_eq, have h := le_antisymm b₁₂_le b₂₁_le, dsimp at h, rw h }
+  end
   .. lex_preorder }
 
+/-- Dictionary / lexicographic linear_order for pairs. -/
 def lex_linear_order [linear_order α] [linear_order β] : linear_order (α × β) :=
 { le_total :=
-begin
-  rintros ⟨a₁, b₁⟩ ⟨a₂, b₂⟩,
-  -- Whee! An 8 part case bash.
-  rcases le_total a₁ a₂ with ha | ha;
-    rcases le_total b₁ b₂ with hb | hb;
-    cases lt_or_eq_of_le ha with a_lt a_eq,
-  { left,  left,  exact a_lt },
-  { left,  right, exact ⟨a_eq, hb⟩ },
-  { left,  left,  exact a_lt },
-  { right, right, exact ⟨a_eq.symm, hb⟩ },
-  { right, left,  exact a_lt },
-  { left,  right, exact ⟨a_eq.symm, hb⟩ },
-  { right, left,  exact a_lt },
-  { right, right, exact ⟨a_eq, hb⟩ }
-end
-  .. lex_partial_order }
+  begin
+    rintros ⟨a₁, b₁⟩ ⟨a₂, b₂⟩,
+    rcases le_total a₁ a₂ with ha | ha;
+      cases lt_or_eq_of_le ha with a_lt a_eq,
+    -- Deal with the two goals with a₁ ≠ a₂
+    { left, left, exact a_lt },
+    swap,
+    { right, left, exact a_lt },
+    -- Now deal with the two goals with a₁ = a₂
+    all_goals { subst a_eq,
+                rcases le_total b₁ b₂ with hb | hb },
+    { left,  right, exact ⟨rfl, hb⟩ },
+    { right, right, exact ⟨rfl, hb⟩ },
+    { left,  right, exact ⟨rfl, hb⟩ },
+    { right, right, exact ⟨rfl, hb⟩ }
+  end
+  .. lex_partial_order }.
+
+/-- Dictionary / lexicographic decidable_linear_order for pairs. -/
+def lex_decidable_linear_order [decidable_linear_order α] [decidable_linear_order β] :
+  decidable_linear_order (α × β) :=
+{ decidable_le :=
+  begin
+    rintros ⟨a₁, b₁⟩ ⟨a₂, b₂⟩,
+    rcases decidable_linear_order.decidable_le α a₁ a₂ with a₂₁_lt | a₁₂_le,
+    { -- a₂ < a₁
+      rw [not_le] at a₂₁_lt,
+      apply decidable.is_false,
+      rw [not_le],
+      split,
+      { exact or.inl a₂₁_lt },
+      { rintro (a₁₂_lt | ⟨a₁₂_eq, b₁₂_le⟩),
+        { exact lt_irrefl _ (lt_trans a₂₁_lt a₁₂_lt) },
+        { dsimp at a₁₂_eq,
+          rw a₁₂_eq at a₂₁_lt,
+          exact lt_irrefl _ a₂₁_lt } } },
+    { -- a₁ ≤ a₂
+      by_cases h : a₁ = a₂,
+      { subst h,
+        rcases decidable_linear_order.decidable_le _ b₁ b₂ with b₂₁_lt | b₁₂_le,
+        { -- b₂ < b₁
+          rw [not_le] at b₂₁_lt,
+          { apply decidable.is_false,
+            rw [not_le],
+            split,
+            { exact or.inr ⟨rfl, le_of_lt b₂₁_lt⟩ },
+            { rintro (b₁₂_lt | ⟨h, b₁₂_le⟩),
+              { exact lt_irrefl _ b₁₂_lt },
+              { exact (not_le_of_gt b₂₁_lt) b₁₂_le } } },
+        },
+        { -- b₁ ≤ b₂
+          apply decidable.is_true,
+          cases lt_or_eq_of_le a₁₂_le with a₁₂_lt a₁₂_eq,
+          { left, exact a₁₂_lt },
+          { right, exact ⟨a₁₂_eq, b₁₂_le⟩ } } },
+      { -- a₁ < a₂
+        apply decidable.is_true,
+        left,
+        cases lt_or_eq_of_le a₁₂_le with a₁₂_lt a₁₂_eq,
+        { exact a₁₂_lt },
+        { exact false.elim (h a₁₂_eq) } }
+    }
+  end,
+  .. lex_linear_order
+}

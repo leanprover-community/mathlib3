68c1aa6a0f8ad3045ab7dbd77be96e380a5c6f49
Kevin Buzzard <k.buzzard@imperial.ac.uk>
2019-03-16 09:18:29 +0000

    minor fixups

--- docs/tutorial/Zmod37.lean
+++ docs/tutorial/Zmod37.lean
@@ -19,7 +19,7 @@ theorem cong_mod_refl : reflexive (cong_mod37) :=
 begin
   intro x,
   -- to prove cong_mod37 x x we just observe that k = 0 will do.
-  use (0:ℤ), -- this is k
+  use (0 : ℤ), -- this is k
   simp,
 end
 
@@ -28,8 +28,8 @@ begin
   intros a b H,
   -- H : cond_mod37 a b
   cases H with l Hl,
-  -- Hk : l * 37 = (b - a)
-  -- Goal is to find an integer k with k * 37 = a - b  
+  -- Hl : l * 37 = (b - a)
+  -- Goal is to find an integer k with k * 37 = a - b
   use -l,
   simp [Hl],
 end
@@ -50,19 +50,18 @@ end
 theorem cong_mod_equiv : equivalence (cong_mod37) :=
 ⟨cong_mod_refl, cong_mod_symm, cong_mod_trans⟩
 
-namespace Zmod37
-
 -- Now let's put an equivalence relation on ℤ
-
-definition Z_setoid : setoid ℤ := { r := cong_mod37, iseqv := cong_mod_equiv }
+definition Zmod37.setoid : setoid ℤ := { r := cong_mod37, iseqv := cong_mod_equiv }
 
 -- Tell the type class inference system about this equivalence relation.
-local attribute [instance] Z_setoid
+local attribute [instance] Zmod37.setoid
 
 -- Now we can make the quotient.
-definition Zmod37 := quotient (Z_setoid)
+definition Zmod37 := quotient (Zmod37.setoid)
 
 -- now a little bit of basic interface
+
+namespace Zmod37
 
 -- Let's give a name to the reduction mod 37 map.
 definition reduce_mod37 : ℤ → Zmod37 := quot.mk (cong_mod37)
@@ -79,8 +78,8 @@ instance : has_zero (Zmod37) := ⟨reduce_mod37 0⟩
 instance : has_one (Zmod37) := ⟨reduce_mod37 1⟩
 
 -- Add basic facts about 0 and 1 to the set of simp facts
-@[simp] theorem of_int_zero : (0 : (Zmod37))  = reduce_mod37 0 := rfl 
-@[simp] theorem of_int_one : (1 : (Zmod37))  = reduce_mod37 1 := rfl 
+@[simp] theorem of_int_zero : (0 : (Zmod37))  = reduce_mod37 0 := rfl
+@[simp] theorem of_int_one : (1 : (Zmod37))  = reduce_mod37 1 := rfl
 
 -- now back to the maths
 
@@ -97,10 +96,10 @@ begin
   -- goal now a₁ + a₂ ≈ b₁ + b₂, and we know how to do these.
   use (m + n),
   simp [add_mul, Hm, Hn]
-end 
+end
 
 -- That lemma above is *exactly* what we need to make sure addition is
--- well-defined on Zmod37, so let's do this now, using quotient.lift 
+-- well-defined on Zmod37, so let's do this now, using quotient.lift
 
 -- note: stuff like "add" is used everywhere so it's best to protect.
 protected definition add : Zmod37 → Zmod37 → Zmod37 :=
@@ -148,7 +147,7 @@ instance : add_comm_group (Zmod37)  :=
   zero         := 0, -- because we already defined has_zero
   add          := (+), -- could also have written has_add.add or Zmod37.add
   neg          := has_neg.neg,
-  zero_add     := 
+  zero_add     :=
     λ abar, quotient.induction_on abar (begin
       -- goal is ∀ (a : ℤ), 0 + ⟦a⟧ = ⟦a⟧ -- that's what quotient.induction_on does for us
       intro a,
@@ -173,7 +172,7 @@ instance : add_comm_group (Zmod37)  :=
                   -- Note use of $ instead of the brackets
     λ abar, quotient.induction_on abar $ λ a, quotient.sound $ by rw add_zero,
                   -- that's it! Term mode proof.
-  add_left_neg := -- super-slow method not even using quotient.induction_on 
+  add_left_neg := -- super-slow method not even using quotient.induction_on
     begin
       intro abar,
       cases (quot.exists_rep abar) with a Ha,
@@ -191,7 +190,7 @@ instance : add_comm_group (Zmod37)  :=
 -- Now let's just nail this using all the tricks in the book. All ring axioms on the quotient
 -- follow from the corresponding axioms for Z.
 instance : comm_ring (Zmod37) :=
-{ 
+{
   mul := Zmod37.mul, -- could have written (*)
   -- Now look how the proof of mul_assoc is just the same structure as add_comm above
   -- but with three variables not two

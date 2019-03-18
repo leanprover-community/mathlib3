86df6603844c52226d62446b5e77929c68133437
Scott Morrison <scott@tqft.net>
2019-03-18 20:24:25 +1100

    about to define the core of a category

---
+++ src/category_theory/core.lean
@@ -0,0 +1,15 @@
+/-
+Copyright (c) 2019 Scott Morrison All rights reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
+Authors: Scott Morrison
+-/
+
+import category_theory.groupoid
+import category_theory.equivalence
+
+namespace category_theory
+
+universes v u -- declare the `v`'s first; see `category_theory.category` for an explanation
+
+
+end category_theory

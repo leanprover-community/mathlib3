759bcc8b27622524395ad8c998c5690a7b36121c
Scott Morrison <scott.morrison@anu.edu.au>
2019-03-19 10:05:41 +1100

    adding a comment about working in Type

--- src/category_theory/instances/rel.lean
+++ src/category_theory/instances/rel.lean
@@ -4,6 +4,8 @@ namespace category_theory
 
 universe u
 
+-- We must work in `Type u` rather than `Sort u`, because
+-- Lean would say `X → Y → Prop` is in `Sort (max u 1)`.
 def rel : large_category (Type u) :=
 { hom  := λ X Y, X → Y → Prop,
   id   := λ X, λ x y, x = y,

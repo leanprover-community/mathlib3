62f5f81d775aad2c34c5a11f8b2d4ba8310a6cdc
Patrick Massot <patrickmassot@free.fr>
2019-03-23 22:20:14 +0100

    fix(scripts): Make scripts more verbose, check for .profile prior edits

--- scripts/setup-dev-scripts.sh
+++ scripts/setup-dev-scripts.sh
@@ -1,5 +1,7 @@
 #! /bin/sh
-pip3 install toml PyGithub urllib3 certifi gitpython
+echo "Installing python dependencies (at user level)"
+pip3 install --user toml PyGithub urllib3 certifi gitpython
+echo "Installing mathlib scripts"
 BASEDIR=$(dirname "$0")
 cd $BASEDIR
 mkdir -p $HOME/.mathlib/bin || true
@@ -9,10 +11,11 @@ cp cache-olean.py $HOME/.mathlib/bin/cache-olean
 cp setup-lean-git-hooks.sh $HOME/.mathlib/bin/setup-lean-git-hooks
 cp post-commit $HOME/.mathlib/hooks/
 cp post-checkout $HOME/.mathlib/hooks/
-if [ "`which update-mathlib`" != "$HOME/.mathlib/bin/update-mathlib" ]; then
+if grep -q ".mathlib/bin" $HOME/.profile
+then
+    echo mathlib scripts are already added to \$PATH in .profile
+else
     echo adding "export PATH=\"\$HOME/.mathlib/bin:\$PATH\"" in \$HOME/.profile
     echo "export PATH=\"\$HOME/.mathlib/bin:\$PATH\" " >> $HOME/.profile
     echo Run: \"source \$HOME/.profile\"
-else
-    echo already in \$PATH
 fi

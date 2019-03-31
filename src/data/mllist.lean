633f7ec2837ae07f64ab76bf6838aad9c049b553
Scott Morrison <scott@tqft.net>
2019-03-31 22:55:14 +1100

    oops, fix header

--- src/data/mllist.lean
+++ src/data/mllist.lean
@@ -1,5 +1,5 @@
 /-
-Copyright (c) 2018 Microsoft Corporation. All rights reserved.
+Copyright (c) 2018 Scott Morrison. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Author: Mario Carnerio, Keeley Hoek, Scott Morrison
 

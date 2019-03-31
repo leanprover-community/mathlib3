88bbb39bf79ffaf3a51fdab9051769469735d3b8
Keeley Hoek <keeley@hoek.io>
2019-03-31 17:23:01 +1100

    Much better tests + new `local_cache.get` function

--- src/tactic/local_cache.lean
+++ src/tactic/local_cache.lean
@@ -9,15 +9,26 @@ variables {α : Type} [reflected α] [has_reflect α]
 
 meta def mk_full_namespace (ns : name) : name := `local_cache ++ ns
 
-meta def get_data_name (ns : name) : tactic name :=
+-- `mk_new` gives a way to generate a new name if no current one
+-- exists.
+meta def get_data_name_aux (ns : name) (mk_new : options → name → tactic name) : tactic name :=
 do o ← tactic.get_options,
    let opt := mk_full_namespace ns,
    match o.get_string opt "" with
-   | "" := do n ← mk_user_fresh_name,
-              tactic.set_options $ o.set_string opt n.to_string,
-              return n
+   | "" := mk_new o opt
    | s := return $ name.from_components $ s.split (λ c, c = '.')
    end
+
+meta def get_data_name (ns : name) : tactic name :=
+get_data_name_aux ns $ λ o opt,
+do n ← mk_user_fresh_name,
+   tactic.set_options $ o.set_string opt n.to_string,
+   return n
+
+-- Like `get_data_name`, but fail if `ns` does not have a cached
+-- decl name (we create a new one above).
+meta def try_get_data_name (ns : name) : tactic name :=
+get_data_name_aux ns $ λ o opt, fail format!"no cache for \"{ns}\""
 
 meta def save_data (dn : name) (a : α) [reflected a] : tactic unit :=
 tactic.add_decl $ mk_definition dn [] (reflect α) (reflect a)
@@ -42,15 +53,27 @@ meta def clear (ns : name) : tactic unit :=
 do o ← tactic.get_options,
    set_options $ o.set_string (mk_full_namespace ns) ""
 
+-- Gets the (optionally present) value-in-cache for `ns`
+meta def get (ns : name) (α : Type) [reflected α] [has_reflect α] : tactic (option α) :=
+do dn ← some <$> try_get_data_name ns <|> return none,
+   match dn with
+   | none := return none
+   | some dn := some <$> load_data dn
+   end
+-- Note: we can't just use `<|>` on `load_data` since it will fail
+-- when a cached value is not present *as well as* when the type of
+-- `α` is just wrong.
+
 end local_cache
 
 open local_cache
 
 -- Using the namespace `ns` as its key, when called for the first
 -- time `run_once ns t` runs `t`, then saves and returns the result.
--- Upon subsequent invocations in the same `environment` (usually
--- just in the same tactic block, with the scope of the caching being
--- inherited by child tactic blocks) we return the cached result directly.
+-- Upon subsequent invocations with the same `tactic.get_options` state
+-- (usually just in the same tactic block, with the scope of the caching
+-- being inherited by child tactic blocks) we return the cached result
+-- directly.
 --
 -- If `α` is just `unit`, this means we just run `t` once each tactic
 -- block.
--- test/local_cache.lean
+++ test/local_cache.lean
@@ -4,7 +4,8 @@ open tactic
 
 section example_tactic
 
-def TEST_NS : name := `my_tactic
+def TEST_NS_1 : name := `my_tactic
+def TEST_NS_2 : name := `my_other_tactic
 
 -- Example "expensive" function
 meta def generate_some_data : tactic (list ℕ) :=
@@ -12,9 +13,12 @@ do trace "cache regenerating",
    return [1, 2, 3, 4]
 
 meta def my_tactic : tactic unit :=
-do my_cached_data ← run_once TEST_NS generate_some_data,
+do my_cached_data ← run_once TEST_NS_1 generate_some_data,
    -- Do some stuff with `my_cached_data`
    skip
+
+meta def my_other_tactic : tactic unit :=
+run_once TEST_NS_2 (return [10, 20, 30, 40]) >> skip
 
 end example_tactic
 
@@ -40,28 +44,125 @@ end example_usage
 
 section test
 
-meta def fail_if_cache_miss : tactic unit :=
-do p ← local_cache.present TEST_NS,
+meta def fail_if_cache_miss (ns : name) : tactic unit :=
+do p ← local_cache.present ns,
    if p then skip else fail "cache miss"
 
-meta def clear_cache : tactic unit := local_cache.clear TEST_NS
+meta def fail_if_cache_miss_1 : tactic unit :=
+fail_if_cache_miss TEST_NS_1
 
-lemma my_test : true := begin
-    success_if_fail { fail_if_cache_miss },
-
-    my_tactic,
-
-    fail_if_cache_miss,
-    fail_if_cache_miss,
-
-    have h : true,
-    { fail_if_cache_miss,
-      trivial },
-
-    clear_cache,
-    success_if_fail { fail_if_cache_miss },
-
-    trivial
-end
+meta def fail_if_cache_miss_2 : tactic unit :=
+fail_if_cache_miss TEST_NS_2
 
 end test
+
+-- Test: the cache is reliably persistent, decends to sub-blocks,
+-- the api to inspect whether a cache entry is present works, and
+-- the cache can be manually cleared.
+section test_persistence
+
+lemma my_test_ps : true := begin
+  success_if_fail { fail_if_cache_miss_1 },
+
+  my_tactic,
+
+  fail_if_cache_miss_1,
+  fail_if_cache_miss_1,
+  success_if_fail { fail_if_cache_miss_2 },
+
+  have h : true,
+  { fail_if_cache_miss_1,
+    trivial },
+
+  -- Manually clear cache
+  local_cache.clear TEST_NS_1,
+  success_if_fail { fail_if_cache_miss_1 },
+
+  trivial
+end
+
+end test_persistence
+
+-- Test: caching under different namespaces doesn't share the
+-- cached state.
+section test_ns_collison
+
+lemma my_test_ns : true := begin
+  my_tactic,
+  fail_if_cache_miss_1,
+  success_if_fail { fail_if_cache_miss_2 },
+
+  my_other_tactic,
+  fail_if_cache_miss_1,
+  fail_if_cache_miss_2,
+
+  local_cache.clear TEST_NS_1,
+  success_if_fail { fail_if_cache_miss_1 },
+  fail_if_cache_miss_2,
+
+  my_other_tactic,
+  success_if_fail { fail_if_cache_miss_1 },
+  fail_if_cache_miss_2,
+
+  trivial
+end
+
+end test_ns_collison
+
+-- Test: cached results don't leak between `def`s or `lemma`s.
+section test_locality
+
+def my_def_1 : true := begin
+  success_if_fail { fail_if_cache_miss_1 },
+  my_tactic,
+  fail_if_cache_miss_1,
+
+  trivial
+end
+
+def my_def_2 : true := begin
+  success_if_fail { fail_if_cache_miss_1 },
+  my_tactic,
+  fail_if_cache_miss_1,
+
+  trivial
+end
+
+lemma my_lemma_1 : true := begin
+  success_if_fail { fail_if_cache_miss_1 },
+  my_tactic,
+  fail_if_cache_miss_1,
+
+  trivial
+end
+
+lemma my_lemma_2 : true := begin
+  success_if_fail { fail_if_cache_miss_1 },
+  my_tactic,
+  fail_if_cache_miss_1,
+
+  trivial
+end
+
+end test_locality
+
+-- Test: the `local_cache.get` function.
+section test_get
+
+meta def assert_equal {α : Type} [decidable_eq α] (a : α) (ta : tactic α) : tactic unit :=
+do a' ← ta,
+   if a = a' then skip
+             else fail "not equal!"
+
+lemma my_lemma_3 : true := begin
+  assert_equal none (local_cache.get TEST_NS_1 (list ℕ)),
+
+  my_tactic,
+  my_other_tactic,
+  assert_equal (some [1,2,3,4]) (local_cache.get TEST_NS_1 (list ℕ)),
+  assert_equal (some [10, 20, 30, 40]) (local_cache.get TEST_NS_2 (list ℕ)),
+
+  trivial
+end
+
+end test_get

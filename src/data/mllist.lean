c4497175982e1b9e2269df6ebadbe4d5ba4b96b2
Scott Morrison <scott.morrison@anu.edu.au>
2019-04-03 17:34:00 +1100

    update authors

--- src/data/mllist.lean
+++ src/data/mllist.lean
@@ -1,7 +1,7 @@
 /-
 Copyright (c) 2018 Scott Morrison. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
-Author: Mario Carnerio, Keeley Hoek, Scott Morrison
+Author: Mario Carnerio, Keeley Hoek, Simon Hudon, Scott Morrison
 
 Monadic lazy lists.
 

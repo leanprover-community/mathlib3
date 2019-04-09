f6a922ebdd8b380e81985a704ebcd837516d27e8
Johan Commelin <johan@commelin.net>
2019-04-09 21:53:36 +0200

    Clean up; add TODO

--- src/tactic/hom.lean
+++ src/tactic/hom.lean
@@ -4,40 +4,19 @@ import tactic.chain
 
 open tactic
 
+-- this instance can go once the hom-mul PR is merged
 instance is_mul_hom_of_is_monoid_hom {X Y : Type*} [monoid X] [monoid Y]
   (f : X → Y) [I : is_monoid_hom f] : is_mul_hom f :=
 {..I}
 
-meta def rw_map (ht : name) (f : expr) :=
-do cht ← mk_const ht,
-   to_expr ``(%%cht %%f) >>= rewrite_target
-
--- meta def map_mul (f : expr) : tactic unit :=
--- rw_map `is_mul_hom.map_mul f
--- meta def map_one (f : expr) : tactic unit :=
--- do to_expr ``(is_monoid_hom.map_one %%f) >>= rewrite_target
--- meta def map_pow (f : expr) : tactic unit :=
--- do to_expr ``(is_monoid_hom.map_pow %%f) >>= rewrite_target
--- meta def map_inv (f : expr) : tactic unit :=
--- do to_expr ``(is_group_hom.map_inv %%f) >>= rewrite_target
+meta def rw_map (map_lemma : name) (f : expr) :=
+do c ← mk_const map_lemma,
+   to_expr ``(%%c %%f) >>= rewrite_target
 
 meta def lookup_homs (n : name) : tactic (list expr) :=
 do cn ← mk_const n,
 do ctx ← local_context,
    ctx.mfilter (λ e, to_expr ``(%%cn %%e) >>= mk_instance >> pure true <|> pure false)
-
--- meta def mul_homs : tactic (list expr) :=
--- do lookup_homs `is_mul_hom
--- meta def monoid_homs : tactic (list expr) :=
--- do lookup_homs `is_monoid_hom
--- meta def group_homs : tactic (list expr) :=
--- do lookup_homs `is_group_hom
-
--- meta def push_monoid_hom (f : expr) : tactic unit :=
--- do mul ← to_expr ``(is_monoid_hom.map_mul %%f),
---    one ← to_expr ``(is_monoid_hom.map_one %%f),
---    chain [rewrite_target one, rewrite_target mul],
---    skip
 
 meta def instance_type : name → name
 | `has_mul   := `is_mul_hom
@@ -54,26 +33,13 @@ meta def map_types : name → (list name)
 meta def types : list name :=
 [`has_mul, `monoid, `group]
 
--- meta def hom : tactic unit :=
--- do mul_homs    ← mul_homs,
---     -- trace mul_homs,
---    let mul_tactics := mul_homs.map map_mul,
---    monoid_homs ← monoid_homs,
---     -- trace monoid_homs,
---    let one_tactics := monoid_homs.map map_one,
---    let pow_tactics := monoid_homs.map map_pow,
---    group_homs ← group_homs,
---     -- trace group_homs,
---    let inv_tactics := group_homs.map map_inv,
---    chain $ mul_tactics ++ one_tactics ++ pow_tactics ++ inv_tactics,
---    try reflexivity
-
 meta def tactics_of_homs (p : name × tactic (list expr)) : tactic (list (list (tactic unit))) :=
 do let map_lemmas := map_types p.1,
    fs ← p.2,
    return $ map_lemmas.map $ λ l, fs.map $ λ f, rw_map l f
 
-meta def hom' : tactic unit :=
+-- TODO: Enable `hom at hyp`.
+meta def hom : tactic unit :=
 do let homs := types.map $ λ t, (t, lookup_homs (instance_type t)),
    tactics ← homs.mmap tactics_of_homs,
    let flat_tactics := list.join $ list.join tactics,
@@ -83,5 +49,5 @@ do let homs := types.map $ λ t, (t, lookup_homs (instance_type t)),
 example (X Y : Type) [ring X] [ring Y] (f : X → Y) [is_monoid_hom f] (n : ℕ)
   (x y : X) : f (x^n * y) = (f x)^n * f y :=
 begin
-  hom',
+  hom,
 end

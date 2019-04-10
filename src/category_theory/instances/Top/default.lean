ac9b729f4e1f69497f514ad3329d05cadceaa640
Scott Morrison <scott@tqft.net>
2019-04-11 08:19:01 +1000

    oops, import from the future

--- src/category_theory/instances/Top/default.lean
+++ src/category_theory/instances/Top/default.lean
@@ -1,4 +1,3 @@
 import category_theory.instances.Top.basic
 import category_theory.instances.Top.limits
 import category_theory.instances.Top.adjunctions
-import category_theory.instances.Top.open_nhds

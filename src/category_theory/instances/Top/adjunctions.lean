85eb9bb2a1e8bb85cc6da37c44e1fd3996bac41e
Scott Morrison <scott.morrison@anu.edu.au>
2019-04-11 12:14:47 +1000

    restoring opens, adding headers

--- src/category_theory/instances/Top/adjunctions.lean
+++ src/category_theory/instances/Top/adjunctions.lean
@@ -1,3 +1,7 @@
+-- Copyright (c) 2017 Scott Morrison. All rights reserved.
+-- Released under Apache 2.0 license as described in the file LICENSE.
+-- Authors: Patrick Massot, Mario Carneiro
+
 import category_theory.instances.Top.basic
 import category_theory.adjunction
 

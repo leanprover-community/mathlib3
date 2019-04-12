fe39160e844595dc8740c9ddd561b54cc8f9d65a
Scott Morrison <scott@tqft.net>
2019-04-12 21:31:04 +1000

    working, now needs lots of cleanup

--- src/tactic/hom.lean
+++ src/tactic/hom.lean
@@ -49,47 +49,59 @@ meta def map_types : name → (list name)
 | `monoid      := [`is_monoid_hom.map_one, `is_monoid_hom.map_pow]
 | `add_monoid  := [`is_add_monoid_hom.map_zero, `is_add_monoid_hom.map_smul]
 | `group       := [`is_group_hom.map_inv, `is_group_hom.map_prod]
-| `add_group   := [`is_add_group_hom.map_neg, `is_add_group_hom.map_sum]
+| `add_group   := [`is_add_group_hom.map_neg, `is_add_group_hom.map_sub, `is_add_group_hom.map_gsmul, `is_add_group_hom.map_sum]
 | `field       := [`is_field_hom.map_div, `is_field_hom.map_inv]
 | _ := []
+
+#print is_add_group_hom.map_sub
+#print is_add_monoid_hom.map_smul
+#print is_add_group_hom.map_gsmul
 
 meta def algebraic_types : list name :=
 [`has_mul, `has_add, `monoid, `add_monoid, `group, `add_group, `field]
 
+#print is_monoid_hom.map_one
+#print is_monoid_hom.map_mul
+#print is_field_hom.map_inv
+
 meta def tactics_of_homs (p : name × list expr) : tactic (list (list expr)) :=
 let map_lemmas := map_types p.1 in
-map_lemmas.mmap $ λ l, p.2.mmap $ λ f, rw_map l f
+map_lemmas.mmap $ λ l, p.2.mmap $ λ f,
+  -- do c ← mk_const l, to_expr ``(%%c %%f)
+  do mk_mapp l [none, none, none, none, some f, none]
 
 -- TODO: Enable `hom at hyp`.
 meta def hom : tactic unit :=
 do homs ← algebraic_types.mmap $ λ t, (do h ← lookup_homs (instance_type t), return (t, h)),
+   num_goals >>= trace,
    trace homs,
    tactics ← homs.mmap tactics_of_homs,
    let flat_tactics := list.join $ list.join tactics,
+   num_goals >>= trace,
    trace flat_tactics,
    lemmas ← flat_tactics.mfoldl simp_lemmas.add simp_lemmas.mk,
    trace lemmas,
-   simp_target lemmas [] {} skip,
-   skip
-  --  try reflexivity
+   simp_target lemmas,
+   try reflexivity
 
 set_option profiler true
 
 example (X Y : Type) [ring X] [ring Y] (f : X → Y) [is_monoid_hom f] (n : ℕ)
-  (x y : X) : f (x^n * y) = (f x)^n * f y :=
-begin
-  hom,
-  set_goals [],
-end
+  (x y : X) : f (x * y) = f x * f y :=
+by hom
 
-example (X Y Z W : Type*) [add_group X] [discrete_field Y] [ring Z] [discrete_field W]
+example (X Y : Type) [add_group X] [add_group Y]
+  (f : X → Y) [is_add_group_hom f]
+  (a b : X) :
+  f (a + b) = f a + f b :=
+by hom
+
+set_option class.instance_max_depth 50
+
+example (X Y Z W : Type) [add_group X] [discrete_field Y] [ring Z] [discrete_field W]
   (f : X → Y) [is_add_group_hom f] (g : Y → W) [is_field_hom g]
   (h : X → Z) [is_add_group_hom h] (i : Z → W) [is_ring_hom i]
   (k l m : ℕ) (a b : X) (c d : Y) (e : Z) :
-  g (f (a + gsmul k b) * c⁻¹ / d) = i (e^l + (h (b - a))^m) :=
-begin
-  -- Of course this is not provable,
-  -- but the rewrites should still happen.
-  -- However, we currently get a timeout
-  hom,
-end
+  g (f (a + gsmul k b) * c⁻¹ / d) + i (e^l + ((h (b - a))^m)) =
+  (g (f a) + gsmul k (g (f b))) * (g c)⁻¹ / (g d) + ((i e)^l + (i (h b) - i (h a))^m) :=
+by hom

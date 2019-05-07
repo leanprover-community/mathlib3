cec179fb035a2295749f9e490b313d1833cd3f8c
Scott Morrison <scott@tqft.net>
2019-05-07 08:46:33 -0500

    various

--- src/category_theory/elements.lean
+++ src/category_theory/elements.lean
@@ -10,36 +10,43 @@ universes v u
 variables {C : Type u} [𝒞 : category.{v} C]
 include 𝒞
 
-def category_of_elements (F : C ⥤ Type u) : category (Σ c : C, F.obj c) :=
+def functor.elements (F : C ⥤ Type u) := (Σ c : C, F.obj c)
+
+instance category_of_elements (F : C ⥤ Type u) : category F.elements :=
 { hom := λ p q, { f : p.1 ⟶ q.1 // (F.map f) p.2 = q.2 },
   id := λ p, ⟨ 𝟙 p.1, by obviously ⟩,
   comp := λ p q r f g, ⟨ f.val ≫ g.val, by obviously ⟩ }
 
 namespace category_of_elements
-local attribute [instance] category_of_elements
 variable (F : C ⥤ Type u)
 
-def π : (Σ c : C, F.obj c) ⥤ C :=
+def π : F.elements ⥤ C :=
 { obj := λ X, X.1,
   map := λ X Y f, f.val }
 
-def to_comma : (Σ c : C, F.obj c) ⥤ comma (functor.of.obj punit) F :=
-{ obj := λ X, { left := punit.star, right := X.1, hom := λ _, X.2 },
-  map := λ X Y f, { right := f.val, } }
+@[simp] lemma π_obj (X : F.elements) : (π F).obj X = X.1 := rfl
+@[simp] lemma π_map {X Y : F.elements} (f : X ⟶ Y) : (π F).map f = f.val := rfl
 
-def from_comma : comma (functor.of.obj punit) F ⥤ (Σ c : C, F.obj c) :=
+def to_comma : F.elements ⥤ comma (functor.of.obj punit) F :=
+{ obj := λ X, { left := punit.star, right := X.1, hom := λ _, X.2 },
+  map := λ X Y f, { right := f.val } }
+
+@[simp] lemma to_comma_obj (X) :
+  (to_comma F).obj X = { left := punit.star, right := X.1, hom := λ _, X.2 } := rfl
+@[simp] lemma to_comma_map {X Y} (f : X ⟶ Y) :
+  (to_comma F).map f = { right := f.val } := rfl
+
+def from_comma : comma (functor.of.obj punit) F ⥤ F.elements :=
 { obj := λ X, ⟨X.right, X.hom (punit.star)⟩,
   map := λ X Y f, ⟨f.right, congr_fun f.w'.symm punit.star⟩ }
 
 section
-local attribute [simp] to_comma from_comma
-
-def comma_equivalence : (Σ c : C, F.obj c) ≌ comma (functor.of.obj punit) F :=
+def comma_equivalence : F.elements ≌ comma (functor.of.obj punit) F :=
 { functor := to_comma F,
   inverse := from_comma F,
   fun_inv_id' := nat_iso.of_components (λ X, eq_to_iso (by tidy)) (by tidy),
   inv_fun_id' := nat_iso.of_components
-    (λ X, { hom := begin tidy, exact 𝟙 _, simp, end, inv := begin tidy, exact 𝟙 _, simp, end })
+    (λ X, { hom := { right := 𝟙 _ }, inv := { right := 𝟙 _ } })
     (by tidy) }
 end
 
--- src/category_theory/punit.lean
+++ src/category_theory/punit.lean
@@ -28,10 +28,8 @@ namespace of
 end of
 
 def star : C ⥤ punit.{w+1} := (const C).obj punit.star
-namespace star
-@[simp] lemma obj (X : C) : star.obj X = punit.star := rfl
-@[simp] lemma map {X Y : C} (f : X ⟶ Y) : star.map f = 𝟙 _ := rfl
-end star
+@[simp] lemma star_obj (X : C) : star.obj X = punit.star := rfl
+@[simp] lemma star_map {X Y : C} (f : X ⟶ Y) : star.map f = 𝟙 _ := rfl
 
 end functor
 

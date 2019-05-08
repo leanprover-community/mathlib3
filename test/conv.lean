9d152f4e7c98413f986bf866743579a46e1956f1
Scott Morrison <scott@tqft.net>
2019-05-07 23:50:08 -0500

    fix copyright

--- test/conv.lean
+++ test/conv.lean
@@ -1,7 +1,7 @@
 /-
-Copyright (c) 2018 Simon Hudon. All rights reserved.
+Copyright (c) 2018 Keeley Hoek. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
-Authors: Simon Hudon
+Authors: Keeley Hoek
 -/
 import tactic.converter.interactive
 import tactic.ring

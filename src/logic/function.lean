8974923cff1314dcc899c05b3b57f3e8fdffa83a
Johan Commelin <johan@commelin.net>
2019-05-08 08:25:31 +0200

    Fix: we are already in the function namespace, duh

--- src/logic/function.lean
+++ src/logic/function.lean
@@ -225,12 +225,12 @@ theorem restrict_eq {α β} (f : α → β) (s : set α): function.restrict f s 
 section comp₂
 variables {α : Type*} {β : Type*} {γ : Type*} {δ : Type*}
 
-def function.comp₂ (f : α → β → γ) (g : γ → δ) : α → β → δ := λ x y, g (f x y)
+def comp₂ (f : α → β → γ) (g : γ → δ) : α → β → δ := λ x y, g (f x y)
 
-notation g `∘₂` f := function.comp₂ f g
+notation g `∘₂` f := comp₂ f g
 
-lemma function.uncurry_comp₂ (f : α → β → γ) (g : γ → δ) :
-  function.uncurry (g ∘₂ f) = (g ∘ function.uncurry f) :=
+lemma uncurry_comp₂ (f : α → β → γ) (g : γ → δ) :
+  uncurry (g ∘₂ f) = (g ∘ uncurry f) :=
 funext $ λ ⟨p, q⟩, rfl
 
 end comp₂

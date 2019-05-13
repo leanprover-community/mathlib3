7e422d878a4303552a8e15b9eb4f5291b1b08b8a
Scott Morrison <scott@tqft.net>
2019-05-13 19:50:19 +1000

    oof

--- src/category_theory/elements.lean
+++ src/category_theory/elements.lean
@@ -1,3 +1,6 @@
+-- Copyright (c) 2019 Scott Morrison. All rights reserved.
+-- Released under Apache 2.0 license as described in the file LICENSE.
+-- Authors: Scott Morrison
 import category_theory.types
 import category_theory.comma
 import category_theory.equivalence
--- src/category_theory/enriched_1.lean
+++ src/category_theory/enriched_1.lean
@@ -1,7 +1,6 @@
 -- Copyright (c) 2019 Scott Morrison. All rights reserved.
 -- Released under Apache 2.0 license as described in the file LICENSE.
 -- Authors: Scott Morrison
-import category_theory.concrete_category
 import category_theory.monoidal.types
 import category_theory.monoidal.functor
 
@@ -15,11 +14,11 @@ namespace category_theory
 open category_theory.monoidal.monoidal_category
 
 variables (C : Sort u) [𝒞 : category.{v+1} C]
-variables (c : Sort v → Sort v) [𝒱 : monoidal_category.{v} (bundled c)]
+variables (V : Sort v) [𝒱 : monoidal_category.{v} V]
 include 𝒞 𝒱
 
 set_option pp.universes true
-class enriched_over (F : is_lax_monoidal.{v v+1} (@bundled.α c)) :=
+class enriched_over (F : lax_monoidal_functor.{v v+1} V (Type v)) :=
 (e_hom   : C → C → V)
 (e_id    : Π X : C, tensor_unit V ⟶ e_hom X X)
 (e_comp  : Π X Y Z : C, e_hom X Y ⊗ e_hom Y Z ⟶ e_hom X Z)

fa9300d463d4997f38db965ccd385e64ba8a749e
Scott Morrison <scott@tqft.net>
2019-05-13 10:28:10 +1000

    rename hypotheses

--- src/category_theory/category.lean
+++ src/category_theory/category.lean
@@ -84,10 +84,10 @@ section
 variables {C : Sort u} [𝒞 : category.{v} C] {X Y Z : C}
 include 𝒞
 
-lemma eq_of_comp_left_eq {f g : X ⟶ Y} (h : ∀ {Z : C} (h : Y ⟶ Z), f ≫ h = g ≫ h) : f = g :=
-by { convert h (𝟙 Y), tidy }
-lemma eq_of_comp_right_eq {f g : Y ⟶ Z} (h : ∀ {X : C} (h : X ⟶ Y), h ≫ f = h ≫ g) : f = g :=
-by { convert h (𝟙 Y), tidy }
+lemma eq_of_comp_left_eq {f g : X ⟶ Y} (w : ∀ {Z : C} (h : Y ⟶ Z), f ≫ h = g ≫ h) : f = g :=
+by { convert w (𝟙 Y), tidy }
+lemma eq_of_comp_right_eq {f g : Y ⟶ Z} (w : ∀ {X : C} (h : X ⟶ Y), h ≫ f = h ≫ g) : f = g :=
+by { convert w (𝟙 Y), tidy }
 
 lemma eq_of_comp_left_eq' (f g : X ⟶ Y) (w : (λ {Z : C} (h : Y ⟶ Z), f ≫ h) = (λ {Z : C} (h : Y ⟶ Z), g ≫ h)) : f = g :=
 eq_of_comp_left_eq (λ Z h, begin
@@ -101,10 +101,10 @@ eq_of_comp_right_eq (λ X h, begin
  exact q
 end)
 
-lemma id_of_comp_left_id (f : X ⟶ X) (h : ∀ {Y : C} (g : X ⟶ Y), f ≫ g = g) : f = 𝟙 X :=
-by { convert h (𝟙 X), tidy }
-lemma id_of_comp_right_id (f : X ⟶ X) (h : ∀ {Y : C} (g : Y ⟶ X), g ≫ f = g) : f = 𝟙 X :=
-by { convert h (𝟙 X), tidy }
+lemma id_of_comp_left_id (f : X ⟶ X) (w : ∀ {Y : C} (g : X ⟶ Y), f ≫ g = g) : f = 𝟙 X :=
+by { convert w (𝟙 X), tidy }
+lemma id_of_comp_right_id (f : X ⟶ X) (w : ∀ {Y : C} (g : Y ⟶ X), g ≫ f = g) : f = 𝟙 X :=
+by { convert w (𝟙 X), tidy }
 
 class epi  (f : X ⟶ Y) : Prop :=
 (left_cancellation : Π {Z : C} (g h : Y ⟶ Z) (w : f ≫ g = f ≫ h), g = h)

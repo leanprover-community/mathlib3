ddca018166a1826d7feb89b5cec1eb162441e46d
Scott Morrison <scott@tqft.net>
2019-05-13 13:51:58 +1000

    more tests

--- test/back-2.lean
+++ test/back-2.lean
@@ -14,3 +14,10 @@ end
 lemma div_dvd_of_dvd {a b : ℕ} (h : b ∣ a) : (a / b) ∣ a :=
 -- The mathlib proof is: `⟨b, (nat.div_mul_cancel h).symm⟩`
 by back? [-nat.div_dvd_of_dvd] with _
+
+
+-- It would be great if `back` could prove all of the following, using `div_le_div_right`
+-- lemma div_le_succ_div {a b : ℕ} : a / b ≤ (a + 1) / b :=
+-- lemma div_le_add_right_div {a b c : ℕ} : a / b ≤ (a + c) / b :=
+-- lemma div_le_add_left_div {a b c : ℕ} : a / b ≤ (c + a) / b :=
+-- lemma pred_div_le {a b : ℕ} : (a - 1) / b ≤ a / b :=

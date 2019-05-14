04ed6e61f9bf55497636761f4748c878ee41f1b7
Reid Barton <rwbarton@gmail.com>
2019-05-14 08:38:44 -0400

    add file header

--- src/category_theory/instances/Top/epi_mono.lean
+++ src/category_theory/instances/Top/epi_mono.lean
@@ -1,3 +1,7 @@
+-- Copyright (c) 2019 Reid Barton. All rights reserved.
+-- Released under Apache 2.0 license as described in the file LICENSE.
+-- Authors: Reid Barton
+
 import category_theory.instances.Top.adjunctions
 import category_theory.epi_mono
 

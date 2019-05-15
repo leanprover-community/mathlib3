e30e318b4cfa22b6b5ab406ab4d344f47fea47d7
Scott Morrison <scott@tqft.net>
2019-05-15 10:08:36 +1000

    missing headers

--- src/algebraic_geometry/stalks.lean
+++ src/algebraic_geometry/stalks.lean
@@ -1,3 +1,6 @@
+-- Copyright (c) 2019 Scott Morrison. All rights reserved.
+-- Released under Apache 2.0 license as described in the file LICENSE.
+-- Authors: Scott Morrison
 import algebraic_geometry.presheafed_space
 import category_theory.instances.Top.stalks
 
--- src/algebraic_geometry/structured_stalks.lean
+++ src/algebraic_geometry/structured_stalks.lean
@@ -1,7 +1,6 @@
 -- Copyright (c) 2019 Scott Morrison. All rights reserved.
 -- Released under Apache 2.0 license as described in the file LICENSE.
 -- Authors: Scott Morrison, Mario Carneiro, Reid Barton
-
 import algebraic_geometry.stalks
 
 universes v₁ v₂ u₁ u₂

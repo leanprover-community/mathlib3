5e5172293e4967fbdf8ec92770a20b0d99283658
skbaek <seulkeebaek@gmail.com>
2019-05-19 19:00:42 -0400

    Add nat lemmas

--- src/tactic/spass/misc.lean
+++ src/tactic/spass/misc.lean
@@ -59,3 +59,29 @@ def digit_to_subs : char → char
 
 def nat.to_subs (n : nat) : string :=
 ⟨n.repr.data.map digit_to_subs⟩
+
+
+namespace nat
+
+  lemma succ_lt_succ_iff :
+    ∀ {a b : ℕ}, a.succ < b.succ ↔ a < b :=
+  begin
+    intros a b, apply iff.intro,
+    apply lt_of_succ_lt_succ,
+    apply succ_lt_succ
+  end
+
+  lemma succ_eq_succ_iff (k m : nat) :
+    k.succ = m.succ ↔ k = m :=
+  by { constructor; intro h0,
+       {cases h0, refl}, rw h0 }
+
+  lemma succ_ne_succ {k m : nat} :
+    k ≠ m → k.succ ≠ m.succ :=
+  by { intros h0 h1, apply h0,
+       rwa succ_eq_succ_iff at h1 }
+
+  lemma zero_ne_succ {k : nat} : 0 ≠ k.succ :=
+  λ h, by cases h
+
+end nat

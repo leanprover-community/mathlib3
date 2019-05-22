102f0e5271b9984d453317ab0a7f208a851abb9d
Michael Howes <u6079887@anu.edu.au>
2019-05-22 10:03:33 +1000

    Authorship

--- src/algebra/group.lean
+++ src/algebra/group.lean
@@ -1,7 +1,7 @@
 /-
 Copyright (c) 2014 Jeremy Avigad. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
-Authors: Jeremy Avigad, Leonardo de Moura, Michael
+Authors: Jeremy Avigad, Leonardo de Moura, Michael Howes
 
 Various multiplicative and additive structures.
 -/

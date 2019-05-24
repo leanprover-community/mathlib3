5f57e759e25e528920ca234a65bc1f65a8e1b5d1
Neil Strickland <N.P.Strickland@sheffield.ac.uk>
2019-05-24 12:45:42 +0100

    More on geometric sums

--- src/algebra/big_operators.lean
+++ src/algebra/big_operators.lean
@@ -552,29 +552,49 @@ end finset
 section geom_sum
 open finset
 
+def geom_series [semiring α] (x : α) (n : ℕ) :=
+(range n).sum (λ i, x ^ i)
+
+theorem geom_series_def [semiring α] (x : α) (n : ℕ) :
+ geom_series x n = (range n).sum (λ i, x ^ i) := rfl
+
+def geom_series₂ [semiring α] (x y : α) (n : ℕ) :=
+(range n).sum (λ i, x ^ i * (y ^ (n - 1 - i)))
+
+theorem geom_series₂_with_one [semiring α] (x : α) (n : ℕ) :
+ geom_series₂ x 1 n = geom_series x n :=
+sum_congr rfl (λ i _, by { rw [one_pow, mul_one] })
+
+theorem geom_series₂_zero [semiring α] (x y : α) :
+ geom_series₂ x y 0 = 0 :=
+by simp [geom_series₂]
+
+/-- Geometric progression formulae with one variable;
+  these work in noncommutative rings -/
+
 theorem geom_sum_mul_add [semiring α] (x : α) :
-  ∀ (n : ℕ), ((range n).sum (λ i, (x+1)^i)) * x + 1 = (x+1)^n
-| 0     := by simp
-| (n+1) := calc (range (n + 1)).sum (λi, (x + 1) ^ i) * x + 1 =
-        (x + 1)^n * x + (((range n).sum (λ i, (x+1)^i)) * x + 1) :
-    by simp [range_add_one, add_mul]
-  ... = (x + 1)^n * x + (x + 1)^n :
+  ∀ (n : ℕ), (geom_series (x + 1) n) * x + 1 = (x + 1) ^ n
+| 0     := by simp [geom_series]
+| (n+1) := calc (geom_series (x + 1) (n + 1)) * x + 1 =
+        (x + 1)^n * x + ((geom_series (x + 1) n) * x + 1) :
+    by simp [geom_series, range_add_one, add_mul]
+  ... = (x + 1) ^ n * x + (x + 1) ^ n :
     by rw geom_sum_mul_add n
   ... = (x + 1) ^ (n + 1) :
     by simp [pow_add, mul_add]
 
 theorem geom_sum_mul [ring α] (x : α) (n : ℕ) :
-  ((range n).sum (λ i, x^i)) * (x-1) = x^n-1 :=
-have _ := geom_sum_mul_add (x-1) n,
+  (geom_series x n) * (x - 1) = x ^ n - 1 :=
+have _ := geom_sum_mul_add (x - 1) n,
 by rw [sub_add_cancel] at this; rw [← this, add_sub_cancel]
 
 theorem geom_sum [division_ring α] {x : α} (h : x ≠ 1) (n : ℕ) :
-  (range n).sum (λ i, x^i) = (x^n-1)/(x-1) :=
+  (geom_series x n) = (x^n-1)/(x-1) :=
 have x - 1 ≠ 0, by simp [*, -sub_eq_add_neg, sub_eq_iff_eq_add] at *,
 by rw [← geom_sum_mul, mul_div_cancel _ this]
 
 lemma geom_sum_inv [division_ring α] {x : α} (hx1 : x ≠ 1) (hx0 : x ≠ 0) (n : ℕ) :
-  (range n).sum (λ m, x⁻¹ ^ m) = (x - 1)⁻¹ * (x - x⁻¹ ^ n * x) :=
+  (geom_series x⁻¹ n) = (x - 1)⁻¹ * (x - x⁻¹ ^ n * x) :=
 have h₁ : x⁻¹ ≠ 1, by rwa [inv_eq_one_div, ne.def, div_eq_iff_mul_eq hx0, one_mul],
 have h₂ : x⁻¹ - 1 ≠ 0, from mt sub_eq_zero.1 h₁,
 have h₃ : x - 1 ≠ 0, from mt sub_eq_zero.1 hx1,
@@ -585,6 +605,56 @@ have h₄ : x * x⁻¹ ^ n = x⁻¹ ^ n * x,
 by rw [geom_sum h₁, div_eq_iff_mul_eq h₂, ← domain.mul_left_inj h₃,
     ← mul_assoc, ← mul_assoc, mul_inv_cancel h₃];
   simp [mul_add, add_mul, mul_inv_cancel hx0, mul_assoc, h₄]
+
+theorem geom_sum_mul_neg [ring α] (x : α) (n : ℕ) :
+  (geom_series x n) * (1 - x) = 1 - x ^ n :=
+begin
+  let h : (geom_series x n) * (x - 1) = x ^ n - 1 := geom_sum_mul x n,
+  replace h := congr_arg has_neg.neg h,
+  rw [neg_sub, ← mul_neg_eq_neg_mul_symm, neg_sub] at h,
+  exact h
+end
+
+/-- Geometric progression formulae with two variables.
+  These are valid provided that x and y commute, but we
+  only prove them here under the assumption that the whole
+  semiring is commutative.  (This prevents us from treating
+  the one-variable version as a special case of the two-variable
+  version.) -/
+
+theorem geom_sum_mul_add₂ [comm_semiring α] (x y : α) (n : ℕ) :
+  x * (geom_series₂ (x + y) y n) + y ^ n = (x + y) ^ n :=
+begin
+  let f := λ (m i : ℕ), (x + y) ^ i * y ^ (m - 1 - i),
+  change x * ((range n).sum (f n)) + y ^ n = (x + y) ^ n,
+  induction n with n ih,
+  { rw [range_zero, sum_empty, mul_zero, zero_add, pow_zero, pow_zero] },
+  { have f_last : f n.succ n = (x + y) ^ n :=
+     by { dsimp [f],
+          rw [nat.sub_sub, nat.add_comm, nat.sub_self, pow_zero, mul_one] },
+    have f_succ : ∀ i, i ∈ range n → f n.succ i = y * f n i :=
+      λ i hi, by {
+        dsimp [f],
+        rw [← mul_assoc, mul_comm y, mul_assoc, ← pow_succ y (n - 1 - i)],
+        congr' 2,
+        rw [nat.succ_eq_add_one, nat.add_sub_cancel, nat.sub_sub, add_comm 1 i],
+        have := nat.add_sub_of_le (mem_range.mp hi),
+        rw [add_comm, nat.succ_eq_add_one] at this,
+        rw [← this, nat.add_sub_cancel, add_comm i 1, ← add_assoc,
+            nat.add_sub_cancel] },
+    rw [pow_succ (x + y), add_mul, sum_range_succ, f_last, mul_add, add_assoc],
+    congr' 1,
+    rw [← ih, mul_add, pow_succ, ← mul_assoc, mul_comm y x, mul_assoc],
+    congr' 2, rw[mul_sum], apply sum_congr rfl, exact f_succ }
+end
+
+theorem geom_sum_mul₂ [comm_ring α] (x y : α) (n : ℕ) :
+  (geom_series₂ x y n) * (x - y) = x ^ n - y ^ n :=
+begin
+  have := geom_sum_mul_add₂ (x - y) y n,
+  rw [sub_add_cancel, mul_comm] at this,
+  rw [← this, add_sub_cancel]
+end
 
 end geom_sum
 

6fe11b19a2bfb87a3d760cf47add763cb3f15726
Scott Morrison <scott@tqft.net>
2019-06-04 11:30:23 -0400

    rename common to basic

--- src/tactic/rewrite_all/congr.lean
+++ src/tactic/rewrite_all/congr.lean
@@ -1,7 +1,7 @@
 -- Copyright (c) 2018 Scott Morrison. All rights reserved.
 -- Released under Apache 2.0 license as described in the file LICENSE.
 -- Authors: Scott Morrison, Keeley Hoek
-import tactic.rewrite_all.common
+import tactic.rewrite_all.basic
 
 namespace tactic
 

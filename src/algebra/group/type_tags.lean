a7ca8ce3c11aace37a0633fbba893fbe8ed7c14b
Yury G. Kudryashov <urkud@urkud.name>
2019-06-08 09:19:22 -0400

    Fix compile

--- src/algebra/group/type_tags.lean
+++ src/algebra/group/type_tags.lean
@@ -1,12 +1,11 @@
-/--
+/-
 Copyright (c) 2018 Mario Carneiro. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Mario Carneiro
 
 Type tags `multiplicative` and `additive` that turn additive structures into multiplicative, and vice versa
 -/
-
-import algebra.group.hom
+import algebra.group.to_additive algebra.group.hom
 
 universes u v
 variables {α : Type u} {β : Type v}

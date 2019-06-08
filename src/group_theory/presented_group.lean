415cd5133a2562adf4c925c9a5dc6cee58ce1503
Michael Howes <u6079887@anu.edu.au>
2019-06-08 16:26:05 +1000

    Uniqueness of extension

--- src/group_theory/presented_group.lean
+++ src/group_theory/presented_group.lean
@@ -12,11 +12,18 @@ variables {α : Type}
 
 /-- Given a set of relations, rels, over a type α, presented_group constructs the group with
 generators α and relations rels as a quotient of free_group α.-/
-def presented_group (rels : set (list (α × bool))) : Type :=
-    quotient_group.quotient $ group.normal_closure (set.image free_group.mk rels)
+def presented_group (rels : set (free_group α)) : Type :=
+    quotient_group.quotient $ group.normal_closure rels
 
-instance presented_group.group (rels : set (list (α × bool))) : group (presented_group (rels)) :=
+namespace presented_group
+
+instance (rels : set (free_group α)) : group (presented_group (rels)) :=
 quotient_group.group _
+
+/-- `of x` is the canonical map from α to a presented group with generators α. The term x is
+mapped to the equivalence class of the image of x in free_group α. -/
+def of {rels : set (free_group α)} (x : α) : presented_group rels :=
+quotient_group.mk (free_group.of x)
 
 namespace to_group
 
@@ -26,29 +33,42 @@ images of f satisfy all the given relations, then f extends uniquely to a group 
 presented_group rels to β
 -/
 
-variables {β : Type} [group β] {f : α → β} {rels : set (list (α × bool))}
+variables {β : Type} [group β] {f : α → β} {rels : set (free_group α)}
 
-local notation `R` := set.image free_group.mk rels
 local notation `F` := free_group.to_group f
 
-lemma relations_in_kernel (h : ∀ r ∈ rels, free_group.to_group.aux f r = 1) : ∀ x ∈ R, F x = 1 :=
-λ x ⟨r, w₁, w₂⟩, by {rw ←w₂, exact (h r w₁)}
+variable (h : ∀ r ∈ rels, F r = 1)
 
-lemma closure_subset_kernel (h : ∀ r ∈ rels, free_group.to_group.aux f r = 1) :
-  group.normal_closure R ⊆ is_group_hom.ker F :=
-group.normal_closure_subset (λ x w, (is_group_hom.mem_ker F).2 (relations_in_kernel h x w))
+lemma closure_subset_kernel : group.normal_closure rels ⊆ is_group_hom.ker F :=
+group.normal_closure_subset (λ x w, (is_group_hom.mem_ker F).2 (h x w))
 
-lemma closure_mapsto_zero (h : ∀ r ∈ rels, free_group.to_group.aux f r = 1) :
-  ∀ x ∈ group.normal_closure R, F x = 1 :=
+lemma closure_mapsto_zero : ∀ x ∈ group.normal_closure rels, F x = 1 :=
 λ x w, (is_group_hom.mem_ker F).1  ((closure_subset_kernel h) w)
 
 /-- The extension of a map f : α → β that satisfies the given relations to a group homomorphism
 from presented_group rels → β. -/
-def to_group (h : ∀ r ∈ rels, free_group.to_group.aux f r = 1) : (presented_group rels) → β :=
-quotient_group.lift (group.normal_closure R) F (closure_mapsto_zero h)
+def to_group : presented_group rels → β :=
+quotient_group.lift (group.normal_closure rels) F (closure_mapsto_zero h)
 
-instance to_group.is_group_hom (h : ∀ r ∈ rels, free_group.to_group.aux f r = 1) :
-  is_group_hom (to_group h) :=
+instance to_group.is_group_hom : is_group_hom (to_group h) :=
 quotient_group.is_group_hom_quotient_lift _ _ _
 
+@[simp] lemma to_group.of {x : α} : to_group h (of x) = f x := free_group.to_group.of
+
+@[simp] lemma to_group.mul {x y} : to_group h (x * y) = to_group h x * to_group h y :=
+is_group_hom.map_mul _ _ _
+
+@[simp] lemma to_group.one : to_group h 1 = 1 :=
+is_group_hom.map_one _
+
+@[simp] lemma to_group.inv {x}: to_group h x⁻¹ = (to_group h x)⁻¹ :=
+is_group_hom.map_inv _ _
+
+theorem to_group.unique (g : presented_group rels → β) [is_group_hom g]
+  (hg : ∀ x : α, g (of x) = f x) : ∀ {x}, g x = to_group h x :=
+λ x, quotient_group.induction_on x
+    (λ _, free_group.to_group.unique
+      (λ (x : free_group α), g (quotient_group.mk x)) hg)
+
 end to_group
+end presented_group

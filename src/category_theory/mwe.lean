48125b0ba2a1144431ca91ded5635f710739e9a6
Michael Howes <u6079887@anu.edu.au>
2019-06-19 18:23:44 +1000

    map_comp in mwe nearly working

--- src/category_theory/mwe.lean
+++ src/category_theory/mwe.lean
@@ -26,26 +26,51 @@ begin
   {exact (fin.ext_iff _ _).2  (eq.trans rfl (eq.symm (nat.eq_of_lt_succ_of_not_lt a.is_lt h)))}
 end)
 
+
+lemma cast_succ_val_lt {n : ℕ} (i : fin n) : (fin.cast_succ i).val < n :=
+begin
+ rw [fin.cast_succ_val],
+ exact i.is_lt,
+end
+
+lemma cast_lt_cast_succ {n : ℕ} (i : fin n)  :
+  fin.cast_lt (fin.cast_succ i) (cast_succ_val_lt _) = i :=
+fin.eq_of_veq (by simp only [fin.cast_lt_val, fin.cast_succ_val])
+
+#print cast_lt_cast_succ
+#check
+
 lemma map_comp {l m n : ℕ} (f : hom l m) (g : hom m n) : map (g ∘ f) = (map g) ∘ (map f) :=
 begin
   ext,
   dsimp [map],
   split_ifs,
-   { split_ifs,
-    by_cases h2 : ((dite (x.val < l) (λ (h : x.val < l), fin.cast_succ (f (fin.cast_lt x h)))
-            (λ (h : ¬x.val < l), fin.last m)).val < m),
+   {  by_cases h2 : ((dite (x.val < l) (λ (h : x.val < l), fin.cast_succ (f (fin.cast_lt x h)))
+        (λ (h : ¬x.val < l), fin.last m)).val < m),
     {
       rw dif_pos h2,
       split_ifs,
       tidy,
-      sorry
+      simp [cast_lt_cast_succ],
     },
     {
       rw dif_neg h2,
+      split_ifs at h2,
       sorry
     }
   },
-  {
-    sorry
-  }-- succeeds, but does nothing
+  {by_cases h2 : ((dite (x.val < l) (λ (h : x.val < l), fin.cast_succ (f (fin.cast_lt x h)))
+            (λ (h : ¬x.val < l), fin.last m)).val < m),
+    {
+      rw dif_pos h2,
+      split_ifs at h2,
+      simp [fin.last] at h2,
+      exact (absurd h2 (irrefl m)),
+    },
+    {
+      rw dif_neg h2,
+    }
+  }
 end
+
+lemma foo (n : ℕ) : ¬(n < n) := by library_search

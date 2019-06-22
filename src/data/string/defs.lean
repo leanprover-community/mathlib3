9d6945168b05d9ecf33d33df17743c1d2ff2ad4f
Keeley Hoek <keeley@hoek.io>
2019-06-22 22:52:05 +1000

    type

--- src/data/string/defs.lean
+++ src/data/string/defs.lean
@@ -8,7 +8,7 @@ intercalate (singleton c) ∘ f ∘ split (= c)
 def over_list (f : list char → list char) : string → string :=
 list.as_string ∘ f ∘ string.to_list
 
-def split_on (c : char) (s : string) :=
+def split_on (c : char) (s : string) : list string :=
 (s.to_list.split_on c).map list.as_string
 
 end string

9d3156fe2258ca2d3fb95c004fb19a21c6d45bbd
Chris Hughes <33847686+ChrisHughes24@users.noreply.github.com>
2019-06-25 13:56:17 +0200

    Update matrix.lean

--- src/data/matrix.lean
+++ src/data/matrix.lean
@@ -185,10 +185,15 @@ section ring
 variables [ring α]
 
 @[simp] theorem neg_mul (M : matrix m n α) (N : matrix n o α) :
-  (-M) ⬝ N = -(M ⬝ N) := by ext; simp [matrix.mul]
+  (-M) ⬝ N = - M ⬝ N := rfl
 
 @[simp] theorem mul_neg (M : matrix m n α) (N : matrix n o α) :
-  M ⬝ (-N) = -(M ⬝ N) := by ext; simp [matrix.mul]
+  M ⬝ (-N) = - M ⬝ N :=
+begin
+  ext i j,
+  unfold matrix.mul,
+  simp,
+end
 
 end ring
 

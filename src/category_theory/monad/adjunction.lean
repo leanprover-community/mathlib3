0edcd8a772a2a2bc37b7e57e5edafe68bcbf4852
Scott Morrison <scott@tqft.net>
2019-07-03 10:22:33 -0500

    move file

--- src/category_theory/monad/adjunction.lean
+++ src/category_theory/monad/adjunction.lean
@@ -4,7 +4,7 @@ Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Scott Morrison
 -/
 import category_theory.monad.algebra
-import category_theory.reflective
+import category_theory.adjunction.fully_faithful
 
 namespace category_theory
 open category

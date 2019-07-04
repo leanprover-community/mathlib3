9164b92621f130f147bff46ba9f09704204e44a6
Scott Morrison <scott@tqft.net>
2019-07-04 06:20:04 -0500

    refactoring CommRing/adjunctions

--- src/algebra/CommRing/adjunctions.lean
+++ src/algebra/CommRing/adjunctions.lean
@@ -10,6 +10,8 @@ import algebra.CommRing.basic
 import category_theory.adjunction.basic
 import data.mv_polynomial
 
+noncomputable theory
+
 universe u
 
 open mv_polynomial
@@ -19,9 +21,11 @@ namespace CommRing
 
 local attribute [instance, priority 0] classical.prop_decidable
 
-noncomputable def polynomial_ring : Type u ⥤ CommRing.{u} :=
-{ obj := λ α, ⟨mv_polynomial α ℤ⟩,
-  map := λ α β f, ⟨rename f, by apply_instance⟩ }
+def CommRing.free (α : Type u) : CommRing.{u} := ⟨mv_polynomial α ℤ⟩
+
+def polynomial_ring : Type u ⥤ CommRing.{u} :=
+{ obj := CommRing.free,
+  map := λ α β f, ⟨rename f, by apply_instance⟩, }
 
 @[simp] lemma polynomial_ring_obj_α {α : Type u} :
   (polynomial_ring.obj α).α = mv_polynomial α ℤ := rfl
@@ -29,13 +33,15 @@ noncomputable def polynomial_ring : Type u ⥤ CommRing.{u} :=
 @[simp] lemma polynomial_ring_map_val {α β : Type u} {f : α → β} :
   (polynomial_ring.map f).val = rename f := rfl
 
-noncomputable def adj : polynomial_ring ⊣ (forget : CommRing ⥤ Type u) :=
+def hom_equiv (α : Type u) (R : CommRing.{u}) : (polynomial_ring.obj α ⟶ R) ≃ (α ⟶ forget.obj R) :=
+{ to_fun    := λ f, f ∘ X,
+  inv_fun   := λ f, ⟨eval₂ (λ n : ℤ, (n : R)) f, by { unfold_coes, apply_instance }⟩,
+  left_inv  := λ f, bundled.hom_ext (@eval₂_hom_X _ _ _ _ _ _ f.val _),
+  right_inv := λ x, by { ext1, unfold_coes, simp only [function.comp_app, eval₂_X] } }
+
+def adj : polynomial_ring ⊣ (forget : CommRing ⥤ Type u) :=
 adjunction.mk_of_hom_equiv
-{ hom_equiv := λ α R,
-  { to_fun    := λ f, f ∘ X,
-    inv_fun   := λ f, ⟨eval₂ (λ n : ℤ, (n : R)) f, by { unfold_coes, apply_instance }⟩,
-    left_inv  := λ f, bundled.hom_ext (@eval₂_hom_X _ _ _ _ _ _ f.val _),
-    right_inv := λ x, by { ext1, unfold_coes, simp only [function.comp_app, eval₂_X] } },
+{ hom_equiv := hom_equiv,
   hom_equiv_naturality_left_symm' :=
   λ X X' Y f g, by { ext1, dsimp, apply eval₂_cast_comp } }.
 

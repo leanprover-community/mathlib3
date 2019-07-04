25b6e7fe71beeefda899b9589f27f54b3afa8640
Scott Morrison <scott@tqft.net>
2019-07-04 16:37:26 -0500

    renaming

--- src/algebra/CommRing/adjunctions.lean
+++ src/algebra/CommRing/adjunctions.lean
@@ -21,25 +21,25 @@ namespace CommRing
 
 local attribute [instance, priority 0] classical.prop_decidable
 
-def CommRing.free (α : Type u) : CommRing.{u} := ⟨mv_polynomial α ℤ⟩
+private def free_obj (α : Type u) : CommRing.{u} := ⟨mv_polynomial α ℤ⟩
 
-def polynomial_ring : Type u ⥤ CommRing.{u} :=
-{ obj := CommRing.free,
+def free : Type u ⥤ CommRing.{u} :=
+{ obj := free_obj,
   map := λ α β f, ⟨rename f, by apply_instance⟩, }
 
-@[simp] lemma polynomial_ring_obj_α {α : Type u} :
-  (polynomial_ring.obj α).α = mv_polynomial α ℤ := rfl
+@[simp] lemma free_obj_α {α : Type u} :
+  (free.obj α).α = mv_polynomial α ℤ := rfl
 
-@[simp] lemma polynomial_ring_map_val {α β : Type u} {f : α → β} :
-  (polynomial_ring.map f).val = rename f := rfl
+@[simp] lemma free_map_val {α β : Type u} {f : α → β} :
+  (free.map f).val = rename f := rfl
 
-def hom_equiv (α : Type u) (R : CommRing.{u}) : (polynomial_ring.obj α ⟶ R) ≃ (α ⟶ forget.obj R) :=
+def hom_equiv (α : Type u) (R : CommRing.{u}) : (free.obj α ⟶ R) ≃ (α ⟶ forget.obj R) :=
 { to_fun    := λ f, f ∘ X,
   inv_fun   := λ f, ⟨eval₂ (λ n : ℤ, (n : R)) f, by { unfold_coes, apply_instance }⟩,
   left_inv  := λ f, bundled.hom_ext (@eval₂_hom_X _ _ _ _ _ _ f.val _),
   right_inv := λ x, by { ext1, unfold_coes, simp only [function.comp_app, eval₂_X] } }
 
-def adj : polynomial_ring ⊣ (forget : CommRing ⥤ Type u) :=
+def adj : free ⊣ (forget : CommRing ⥤ Type u) :=
 adjunction.mk_of_hom_equiv
 { hom_equiv := hom_equiv,
   hom_equiv_naturality_left_symm' :=

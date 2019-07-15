4639876333dd5bb17fa66a562c32198bf8fe9d05
Unknown <css_mn@yahoo.ie>
2019-07-15 17:24:08 +0200

    tidy up proof of integrally closed

--- src/field_theory/henselian.lean
+++ src/field_theory/henselian.lean
@@ -14,7 +14,7 @@ namespace valuation
 which is positive definite, multiplicative, and satisfies the (weak) triangle inequality. -/
 class valued_ring (α : Type u) [integral_domain α] :=
 (val : α → ℝ)
-(non_neg : ∀ a, val a ≥ 0)
+(nonneg : ∀ a, val a ≥ 0)
 (definite : ∀ a, val a = 0 ↔ a = 0)
 (val_mul : ∀ a b, val (a * b) = val a * val b)
 (val_add : ∀ a b, val (a + b) ≤ val a + val b)
@@ -34,7 +34,7 @@ variables {α : Type u} [integral_domain α] [valued_ring α]
 lemma val_ne_zero {x : α} (h : x ≠ 0) : val x ≠ 0 := (mt (definite x).mp) h
 
 /-- Shows that the valuation of a nonzero element is > 0. -/
-lemma val_pos {x : α} (h : x ≠ 0) : val x > 0 := lt_of_le_of_ne (non_neg x) (ne.symm $ val_ne_zero h)
+lemma val_pos {x : α} (h : x ≠ 0) : val x > 0 := lt_of_le_of_ne (nonneg x) (ne.symm $ val_ne_zero h)
 
 /-- Shows that the valuation of 0 equals 0. -/
 lemma val_zero : val (0 : α) = 0 := by rw [definite]
@@ -49,7 +49,7 @@ lemma val_neg_one : val (-1 : α) = 1 :=
 have val (-1 : α) * val (-1 : α) = 1, by rw[←val_mul, neg_one_mul, neg_neg, val_one],
 or.resolve_right
   ((mul_self_eq_one_iff _).mp this)
-  (ne_of_gt (lt_of_lt_of_le zero_gt_neg_one (non_neg (-1 : α))))
+  (ne_of_gt (lt_of_lt_of_le zero_gt_neg_one (nonneg (-1 : α))))
 
 /-- Shows that the valuation of -x equals x. -/
 lemma val_neg (x : α) : val (-x) = val x := by rw[←mul_neg_one, val_mul, val_neg_one, mul_one]
@@ -99,7 +99,7 @@ def add : valuation_ring α → valuation_ring α → valuation_ring α
 /-- The multiplication on a valuation ring. -/
 def mul : valuation_ring α → valuation_ring α → valuation_ring α
 | ⟨x, hx⟩ ⟨y, hy⟩ := ⟨x*y,
-    begin rw valued_ring.val_mul, apply mul_le_one; {assumption <|> apply valued_ring.non_neg} end⟩
+    begin rw valued_ring.val_mul, apply mul_le_one; {assumption <|> apply valued_ring.nonneg} end⟩
 
 /-- The subtraction on a valuation ring. -/
 def neg : valuation_ring α → valuation_ring α
@@ -189,7 +189,7 @@ instance : integral_domain (valuation_ring α) :=
 /-- The valuation ring is itself a valued ring -/
 instance : nonarch_valued_ring (valuation_ring α) :=
 { val := λ x, valued_ring.val (x : α),
-  non_neg := λ x, valued_ring.non_neg x,
+  nonneg := λ x, valued_ring.nonneg x,
   definite := λ x, by rw[subtype.ext]; simp; exact valued_ring.definite x,
   val_mul := λ x y, by rw[coe_mul]; exact valued_ring.val_mul x y,
   val_add := λ x y, by rw[coe_add]; exact valued_ring.val_add x y,
@@ -217,35 +217,39 @@ instance coe_is_ring_hom : @is_ring_hom _ _ (show ring (valuation_ring α), by a
 	map_mul := λ x y, by simp only [val_eq_coe, coe_mul, val_eq_coe],
 	map_add := λ x y, by simp only [val_eq_coe, coe_add, val_eq_coe] }
 
-open polynomial
+open polynomial valued_ring
 
 instance : algebra (valuation_ring α) α :=
 	algebra.of_ring_hom (subtype.val) valuation.coe_is_ring_hom
 
-lemma integrally_closed (x : α) (hi : is_integral (valuation_ring α) x) :
-	valued_ring.val x ≤ 1 :=
+/-- Shows that the valuation ring is integrally closed. -/
+lemma integrally_closed (x : α) (hi : is_integral (valuation_ring α) x) : val x ≤ 1 :=
 let ⟨p, hm, hp⟩ := hi in
 begin
 	by_contradiction hnx,
   change p.eval₂ subtype.val x = 0 at hp,
-  let X : polynomial (valuation_ring α):= X,
-  have h : valued_ring.val (x^p.nat_degree) =
-           valued_ring.val ((X^p.nat_degree + -p).eval₂ subtype.val x), from
-    congr_arg _ (by rw[eval₂_add, eval₂_neg, hp, neg_zero, add_zero, eval₂_X_pow]),
-  rw [val_pow] at h,
-  have hn0 : p ≠ 0, from ne_zero_of_monic hm,
-  have h1 : valued_ring.val x ^ p.nat_degree ≤ valued_ring.val x ^ (p.nat_degree - 1),
+  let X : polynomial (valuation_ring α) := X,
+  -- Shows that p and x are non-zero and therefore, val x > 0 as well
+  have hp0 : p ≠ 0, from ne_zero_of_monic hm,
+  have hx0 : x ≠ 0, from λ hx0, by rw [hx0, val_zero] at hnx; exact hnx zero_le_one,
+  have hvx : 0 < val x, from val_pos (λ hn, by rw [hn, val_zero] at hnx; exact hnx zero_le_one),
+  -- Show that x^n = x^n - ∑_i^n a_i * x^i where p(X) = ∑_i^{n-1} a_i * X^i
+  have h : val x^p.nat_degree = val ((X^p.nat_degree + -p).eval₂ subtype.val x),
+    by rw [←val_pow, eval₂_add, eval₂_neg, hp, neg_zero, add_zero, eval₂_X_pow],
+  -- Since a_n = 1 we get x^n = -∑_i^{n-1} a_i * x^i. Since val a_i ≤ 1 for i = 1..n, we may
+  -- conclude that val x^n ≤ val x^{n-1} using the strong triangle inequality
+  have h1 : val x^(p.nat_degree) ≤ val x^p.nat_degree * (val x)⁻¹,
     begin
-      rw [h],
+      conv_lhs { rw [h] },
       apply val_sum,
-      { cases nat.eq_zero_or_pos (nat_degree p - 1) with h0 h1,
-        { rw [h0, pow_zero], exact zero_le_one },
-        { rw [←zero_pow h1],
-          exact pow_le_pow_of_le_left (le_refl _) (valued_ring.non_neg x) _ } },
+      { cases nat.eq_zero_or_pos p.nat_degree with _ h1,
+        { rw [←val_inv x hx0, ←val_pow, ←val_mul], exact nonneg _ },
+        { rw [←zero_pow h1, zero_pow h1],
+          rw [←val_inv x hx0, ←val_pow, ←val_mul], exact nonneg _, } },
       { intros y hy,
         rw [multiset.mem_map] at hy,
         cases hy with a ha,
-        have hd : a ≤ p.nat_degree - 1,
+        have hd : a + 1 ≤ p.nat_degree,
           begin
             rw [←finset.mem_def] at ha,
             have ha1 : a ∈ (X ^ nat_degree p + -p).support, from ha.1,
@@ -258,56 +262,47 @@ begin
                 exact ha1 rfl,
                 refl,
               end,
-            have hm1 : a ∈ (X ^ nat_degree p).support ∪ (-p).support, from finset.mem_of_subset finsupp.support_add ha1,
-            have hm2 : (X ^ nat_degree p).support = finset.singleton (nat_degree p),
-              by rw [←one_mul (X^_), ←C_1, ←single_eq_C_mul_X, finsupp.support_single_ne_zero (one_ne_zero)]; refl,
-            have hm3 : (X ^ nat_degree p).support ⊆ (-p).support,
-            begin
-              rw [finsupp.support_neg, hm2, finset.subset_iff],
-              intros x hx,
-              rw [finset.mem_singleton] at hx,
-              rw [hx, finsupp.mem_support_iff],
-              change leading_coeff p ≠ 0,
-              rw [monic.def.mp hm],
-              exact one_ne_zero
-            end,
             have : a ∈ p.support,
             begin
               rw [←finsupp.support_neg],
-              exact finset.mem_of_subset (finset.union_subset hm3 (finset.subset.refl _)) hm1
+              refine finset.mem_of_subset
+                (finset.union_subset _ (finset.subset.refl _))
+                (finset.mem_of_subset finsupp.support_add ha1),
+              { rw [finsupp.support_neg, ←one_mul (X^_), ←C_1, ←single_eq_C_mul_X,
+                    finsupp.support_single_ne_zero (one_ne_zero), finset.subset_iff],
+                intros x h3,
+                change x ∈ finset.singleton (nat_degree p) at h3,
+                rw [finset.mem_singleton] at h3,
+                rw [h3, finsupp.mem_support_iff],
+                change leading_coeff p ≠ 0,
+                rw [monic.def.mp hm],
+                exact one_ne_zero }
             end,
             have : a ≤ p.nat_degree, from
             begin
-              rw [←with_bot.coe_le_coe, ←degree_eq_nat_degree hn0],
+              rw [←with_bot.coe_le_coe, ←degree_eq_nat_degree hp0],
               exact finset.le_sup this
             end,
-            exact nat.le_pred_of_lt (lt_of_le_of_ne this hna)
+            exact nat.succ_le_of_lt (lt_of_le_of_ne this hna)
           end,
-        rw [←ha.2, valued_ring.val_mul],
-        exact calc valued_ring.val (((X ^ nat_degree p - p).to_fun a).val) * valued_ring.val (x ^ a)
-              ≤ valued_ring.val (x ^ a) :
-                  mul_le_of_le_one_left (valued_ring.non_neg _) (((X ^ nat_degree p - p).to_fun a).property)
-          ... ≤ valued_ring.val x ^ (p.nat_degree - 1) :
-                  by rw [val_pow]; exact pow_le_pow (le_of_not_ge hnx) hd }
+        rw [←ha.2, val_mul, ←mul_le_mul_right hvx, mul_assoc, ←val_mul, ←pow_succ', mul_assoc,
+            inv_mul_cancel (val_ne_zero hx0), mul_one],
+        exact calc val (((X ^ nat_degree p - p).to_fun a).val) * val (x ^ (a + 1))
+              ≤ val (x ^ (a + 1)) :
+            mul_le_of_le_one_left (nonneg _) (((X ^ nat_degree p - p).to_fun a).property)
+          ... ≤ val x ^ p.nat_degree :
+            by rw [val_pow]; exact pow_le_pow (le_of_not_ge hnx) hd }
     end,
-  have h0 : 0 < nat_degree p, from
-    suffices ↑0 < degree p, by rwa [degree_eq_nat_degree hn0, with_bot.coe_lt_coe] at this,
-    (lt_of_not_ge $ λ hlt, begin
-      have := eq_C_of_degree_le_zero hlt,
-      rw [this, eval₂_C, ←zero_def] at hp,
-      exact hn0 (finsupp.ext (λ n, show coeff p n = 0, from
-        nat.cases_on n hp (λ _, coeff_eq_zero_of_degree_lt (lt_of_le_of_lt hlt
-          (with_bot.coe_lt_coe.2 (nat.succ_pos _))))))
+  rw [←mul_le_mul_right hvx, mul_assoc, inv_mul_cancel (val_ne_zero hx0), mul_one] at h1,
+  have : val x ^ nat_degree p > 0, from
+    lt_of_not_ge (λ hlt, begin
+      rw [←val_pow] at hlt,
+      have : val (x ^ nat_degree p) = 0, from le_antisymm hlt (nonneg _),
+      rw [definite] at this,
+      exact hx0 (pow_eq_zero this)
     end),
-  have hx : 0 < valued_ring.val x,
-    from val_pos (λ hn, by by_contradiction; rw [hn, val_zero] at hnx; exact hnx zero_le_one),
-  rw [←mul_le_mul_right hx, ←pow_succ' _ (_ - 1)] at h1,
-  change valued_ring.val x ^ p.nat_degree * valued_ring.val x ≤
-         valued_ring.val x ^ (nat.succ (nat.pred p.nat_degree)) at h1,
-  rw [nat.succ_pred_eq_of_pos h0] at h1,
-  have : valued_ring.val x ^ nat_degree p > 0,
-    by rw [←zero_pow h0]; exact pow_lt_pow_of_lt_left hx (le_refl 0) h0,
-  rw [mul_le_iff_le_one_right this] at h1,
+  -- Cancel (val x)^{n-1} on both sides to obtain val x ≤ 1
+  have : val x ≤ 1, from (mul_le_iff_le_one_right this).mp h1,
   contradiction
 end
 

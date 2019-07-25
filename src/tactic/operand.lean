9bade296eac23b3430cb41d11f22ed393b57a617
u6338192 <51683915+u6338192@users.noreply.github.com>
2019-07-26 09:44:11 +1000

    Add files via upload
    Added operand.lean file to the tactic folder.

---
+++ src/tactic/operand.lean
@@ -0,0 +1,38 @@
+import algebra.big_operators
+import tactic.interactive
+import tactic.converter.interactive
+
+open tactic
+open finset
+
+namespace conv.interactive
+
+meta def operand_core (t : conv.interactive.itactic) : conv unit :=
+do (r, lhs, _) ← target_lhs_rhs,
+   guard (r = `eq),
+   lam ← match lhs with
+     | `(finset.sum  %%S %%lam) := tactic.applyc `finset.sum_congr >> skip >> return lam
+     | `(finset.prod %%S %%lam) := tactic.applyc `finset.prod_congr >> skip >> return lam
+     | `(fold %%op %%b %%lam %%S)   := tactic.applyc `finset.fold_congr >> return lam
+     | _                        := tactic.fail "The goal expression is not in the correct form."
+   end,
+   (a::gs) ← get_goals,
+   set_goals [a],
+   (expr.lam n _ _ _) ← return lam,
+   intro n,
+   intro `s_mem,   
+   t
+
+--Note: for mathlib this will be in tactic.converter.interactive
+meta def zoom (t : conv.interactive.itactic) : conv unit :=
+do transitivity,
+   a :: rest ← get_goals,
+   set_goals [a],
+   t,
+   all_goals reflexivity,
+   set_goals rest
+   
+meta def operand (t : conv.interactive.itactic) : conv unit :=
+zoom (operand_core t)
+
+end conv.interactive

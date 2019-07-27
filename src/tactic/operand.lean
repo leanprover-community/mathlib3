1945af348353a0c6b6bacc8dd157c29f6d880d96
u6338192 <51683915+u6338192@users.noreply.github.com>
2019-07-27 11:37:23 +1000

    Update operand.lean
    Added header

--- src/tactic/operand.lean
+++ src/tactic/operand.lean
@@ -1,3 +1,9 @@
+/-
+Copyright (c) 2019 Lucas Allen. All rights reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
+Authors: Lucas Allen
+-/
+
 import algebra.big_operators
 import tactic.interactive
 import tactic.converter.interactive

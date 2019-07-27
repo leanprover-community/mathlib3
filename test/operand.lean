108627343101c139c3a6f1493866fd7242d2a42a
u6338192 <51683915+u6338192@users.noreply.github.com>
2019-07-27 11:38:00 +1000

    Update operand.lean
    Added header

--- test/operand.lean
+++ test/operand.lean
@@ -1,3 +1,9 @@
+/-
+Copyright (c) 2019 Lucas Allen. All rights reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
+Authors: Lucas Allen
+-/
+
 import tactic.operand
 import tactic.interactive
 

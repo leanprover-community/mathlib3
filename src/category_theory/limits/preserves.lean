9ba9e94f1a58900493ccee1e5033349f21eaa5ec
Scott Morrison <scott@tqft.net>
2019-07-28 12:50:12 +1000

    fix import

--- src/category_theory/limits/preserves.lean
+++ src/category_theory/limits/preserves.lean
@@ -3,11 +3,11 @@ Copyright (c) 2018 Scott Morrison. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Scott Morrison, Reid Barton
 -/
+import category_theory.limits.limits
+
 /-!
 # Preservation and reflection of (co)limits.
 -/
-
-import category_theory.limits.limits
 
 open category_theory
 

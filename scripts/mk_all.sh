3e79015f121bdfba48074391d453c0ea6e07cf8a
Floris van Doorn <fpvdoorn@gmail.com>
2019-07-29 17:48:21 -0400

    add scripts to import all mathlib files
    mk_all makes a file all.lean in each subdirectory of src/, importing all files in that directory, including subdirectories
    rm_all removes the files all.lean

---
+++ scripts/mk_all.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# Makes a file all.lean in all subfolders of src/ importing all files in that folder, including subfolders.
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+for d in $(find $DIR/../src/ -maxdepth 99 -type d)
+do
+  cd "$d"
+  find . -maxdepth 1 -mindepth 1 -name 'all.lean' -prune -o -name '*.lean' -print -o -type d -print |
+  sed 's/$/\.all/; s/\.lean\.all//; s/^\.\//       \./; 1s/^      /import/' > all.lean
+done

98cad17912744bd383597a683ef35660c7f071ea
Floris van Doorn <fpvdoorn@gmail.com>
2019-07-30 11:17:10 -0400

    also delete all.olean files

--- scripts/rm_all.sh
+++ scripts/rm_all.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # Removes all files named all.lean in the src/ directory
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-find $DIR/../src/ -name 'all.lean' -delete
+find $DIR/../src/ -name 'all.lean' -delete -o -name 'all.olean' -delete

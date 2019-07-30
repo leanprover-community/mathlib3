b40aa4666a8e5617035edcd8ebdfcf83dd5a741c
Rob Lewis <rob.y.lewis@gmail.com>
2019-07-30 14:53:08 +0200

    fix build

--- src/topology/instances/complex.lean
+++ src/topology/instances/complex.lean
@@ -79,7 +79,7 @@ lemma uniform_continuous_mul (s : set (ℂ × ℂ))
   (H : ∀ x ∈ s, abs (x : ℂ × ℂ).1 < r₁ ∧ abs x.2 < r₂) :
   uniform_continuous (λp:s, p.1.1 * p.1.2) :=
 metric.uniform_continuous_iff.2 $ λ ε ε0,
-let ⟨δ, δ0, Hδ⟩ := rat_mul_continuous_lemma abs ε0 r₁0 r₂0 in
+let ⟨δ, δ0, Hδ⟩ := rat_mul_continuous_lemma abs ε0 r₁0 in
 ⟨δ, δ0, λ a b h,
   let ⟨h₁, h₂⟩ := max_lt_iff.1 h in Hδ (H _ a.2).1 (H _ b.2).2 h₁ h₂⟩
 

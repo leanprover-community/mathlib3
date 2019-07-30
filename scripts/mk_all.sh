802ada0eda2eee563a11bd620159368901429dc7
Floris van Doorn <fpvdoorn@gmail.com>
2019-07-30 14:42:26 -0400

    remove unnecessary maxdepth

--- scripts/mk_all.sh
+++ scripts/mk_all.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # Makes a file all.lean in all subfolders of src/ importing all files in that folder, including subfolders.
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-for d in $(find $DIR/../src/ -maxdepth 99 -type d)
+for d in $(find $DIR/../src/ -type d)
 do
   cd "$d"
   find . -maxdepth 1 -mindepth 1 -name 'all.lean' -prune -o -name '*.lean' -print -o -type d -print |

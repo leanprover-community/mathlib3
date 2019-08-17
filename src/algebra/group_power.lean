3a1bacd1d7db105c47720a1c2b5ae6f77f7a578a
Your Name <you@example.com>
2019-08-17 20:26:16 +0100

    removing lemma

--- src/algebra/group_power.lean
+++ src/algebra/group_power.lean
@@ -443,10 +443,6 @@ theorem is_semiring_hom.map_pow {β} [semiring α] [semiring β]
 by induction n with n ih; [exact is_semiring_hom.map_one f,
   rw [pow_succ, pow_succ, is_semiring_hom.map_mul f, ih]]
 
-lemma map_pow {β} [semiring α] [semiring β] (f : α →+* β) (a : α) : ∀(n : ℕ), f (a ^ n) = (f a) ^ n
-| 0            := f.map_one
-| (nat.succ n) := by rw [pow_succ, semiring_hom.map_mul, map_pow n]; refl
-
 theorem neg_one_pow_eq_or {R} [ring R] : ∀ n : ℕ, (-1 : R)^n = 1 ∨ (-1 : R)^n = -1
 | 0     := or.inl rfl
 | (n+1) := (neg_one_pow_eq_or n).swap.imp

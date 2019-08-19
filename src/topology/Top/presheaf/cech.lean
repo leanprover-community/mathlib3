35852305b8c863eb6b3a613c63c67ec9293417ce
Scott Morrison <scott@tqft.net>
2019-08-20 07:39:40 +1000

    lemma

--- src/topology/Top/presheaf/cech.lean
+++ src/topology/Top/presheaf/cech.lean
@@ -25,18 +25,11 @@ def cech_zero_map (c d : (cover X)ᵒᵖ) (f : c ⟶ d) : cech_zero_obj F c ⟶ 
 limit.pre ((unop c).diagram ⋙ F) (intersections.map f.unop.s) ≫
   limits.lim.map (whisker_right (intersections.map_diagram f.unop) F)
 
-@[simp] lemma cech_zero_map_π (c d : (cover X)ᵒᵖ) (f : c ⟶ d) (j) :
+@[simp,reassoc] lemma cech_zero_map_π (c d : (cover X)ᵒᵖ) (f : c ⟶ d) (j) :
   cech_zero_map F c d f ≫ limit.π (diagram (unop d) ⋙ F) j =
     limit.π ((unop c).diagram ⋙ F) ((intersections.map (f.unop.s)).obj j) ≫
       F.map ((intersections.map_diagram f.unop).app j) :=
 by { dsimp [cech_zero_map], simp }
-
--- TODO Simon's attribute will handle this
-@[simp] lemma cech_zero_map_π_assoc (c d : (cover X)ᵒᵖ) (f : c ⟶ d) (j) (W) (g : _ ⟶ W):
-  cech_zero_map F c d f ≫ limit.π (diagram (unop d) ⋙ F) j ≫ g =
-    limit.π ((unop c).diagram ⋙ F) ((intersections.map (f.unop.s)).obj j) ≫
-      F.map ((intersections.map_diagram f.unop).app j) ≫ g:=
-by rw [←category.assoc, cech_zero_map_π, category.assoc]
 
 local attribute [tidy] cases_intersection
 def cech_zero : (cover X)ᵒᵖ ⥤ C :=

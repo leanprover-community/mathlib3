a36ccf37f66363aa7af94207165f5dabf8ab2a0e
Rob Lewis <rob.y.lewis@gmail.com>
2019-08-21 12:53:14 +0200

    feat(logic/basic): ne.symm_iff

--- src/logic/basic.lean
+++ src/logic/basic.lean
@@ -74,6 +74,8 @@ assume ⟨h⟩, h.elim
 -- missing [symm] attribute for ne in core.
 attribute [symm] ne.symm
 
+lemma ne.symm_iff {α} {a b : α} : a ≠ b ↔ b ≠ a := ⟨ne.symm, ne.symm⟩
+
 end miscellany
 
 /-

9b5d72d0ccb2108db797f0c18050a1cb3a3ba650
Scott Morrison <scott@tqft.net>
2019-09-01 19:53:44 +1000

    fix import

--- src/category_theory/yoneda.lean
+++ src/category_theory/yoneda.lean
@@ -4,6 +4,7 @@ Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Scott Morrison
 -/
 import category_theory.opposites
+import category_theory.hom_functor
 
 /-!
 # The Yoneda embedding

5e642b87678537e99c7e160211ea2b207201b6f1
Amelia Livingston <al3717@ic.ac.uk>
2019-09-03 20:05:35 +0100

    order iso and Galois insertion

--- src/data/setoid.lean
+++ src/data/setoid.lean
@@ -3,7 +3,7 @@ Copyright (c) 2019 Amelia Livingston. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Amelia Livingston
 -/
-import data.quot order.complete_lattice
+import data.quot order.complete_lattice data.equiv.basic order.order_iso order.galois_connection
 /-!
 # Setoids
 Some definitions relating to setoids, including the complete lattice of setoids on a type.
@@ -16,16 +16,16 @@ setoid, equivalence, iseqv, relation
 -/
 variables {α : Type*} {β : Type*}
 
+open lattice 
+
 namespace setoid
 
 def r' (r : setoid α) := @setoid.r _ r
 
 @[extensionality] lemma ext' {r s : setoid α} (H : ∀ a b, r.r' a b ↔ s.r' a b) : r = s := ext H
 
-def to_set (r : setoid α) : set (α × α) := {x : α × α | @setoid.r _ r x.1 x.2}
-
 instance : has_le (setoid α) := ⟨λ r s, ∀ x y, r.r' x y → s.r' x y⟩
-instance : has_mem (α × α) (setoid α) := ⟨λ x r, x ∈ r.to_set⟩
+instance : has_mem (α × α) (setoid α) := ⟨λ x r, r.r' x.1 x.2⟩
 
 @[refl] lemma refl' (r : setoid α) (x) : r.r' x x := r.2.1 x
 @[symm] lemma symm' (r : setoid α) : ∀ {x y}, r.r' x y → r.r' y x := λ _ _ h, r.2.2.1 h
@@ -35,9 +35,12 @@ instance : has_mem (α × α) (setoid α) := ⟨λ x r, x ∈ r.to_set⟩
 def ker (f : α → β) : setoid α :=
 ⟨λ x y, f x = f y, ⟨λ x, rfl, λ _ _ h, h.symm, λ _ _ _ h1 h2, h1.trans h2⟩⟩ 
 
+lemma ker_mk_eq (r : setoid α) : ker (@quotient.mk _ r) = r :=
+ext' $ λ x y, quotient.eq
+
 variables (α)
 
-def diag : setoid α :=
+def bot : setoid α :=
 ⟨(=), ⟨λ _, rfl, λ _ _ h, h.symm, λ _ _ _ h1 h2, h1.trans h2⟩⟩
 
 def top : setoid α := 
@@ -50,11 +53,6 @@ protected def prod (r : setoid α) (s : setoid β) : setoid (α × β) :=
   ⟨λ x, ⟨r.refl' x.1, s.refl' x.2⟩, λ _ _ h, ⟨r.symm' h.1, s.symm' h.2⟩, 
    λ _ _ _ h1 h2, ⟨r.trans' h1.1 h2.1, s.trans' h1.2 h2.2⟩⟩⟩
 
-def pi {ι : Type*} {f : ι → Type*} (C : Π i, setoid (f i)) : setoid (Π i, f i) :=
-{ r := λ x y, ∀ i, (C i).r' (x i) (y i),
-  iseqv := ⟨λ x i, (C i).refl' (x i), λ _ _ h i, (C i).symm' (h i),
-            λ _ _ _ h1 h2 i, (C i).trans' (h1 i) (h2 i)⟩}
-
 open lattice
 
 instance : partial_order (setoid α) :=
@@ -65,7 +63,7 @@ instance : partial_order (setoid α) :=
   lt_iff_le_not_le := λ _ _, iff.rfl,
   le_antisymm := λ r s h1 h2, setoid.ext' $ λ x y, ⟨h1 x y, h2 x y⟩ }
 
-instance : has_bot (setoid α) := ⟨diag α⟩
+instance : has_bot (setoid α) := ⟨bot α⟩
 
 instance : order_bot (setoid α) :=
 { bot := has_bot.bot (setoid α),
@@ -114,36 +112,103 @@ instance : complete_lattice (setoid α) :=
   ..setoid.partial_order,
   ..setoid.lattice.order_top, ..setoid.lattice.order_bot}
 
-inductive in_closure (r : α → α → Prop) : α → α → Prop
-| of_r : ∀ x y, r x y → in_closure x y
-| refl : ∀ x, in_closure x x
-| symm : ∀ {x y}, in_closure x y → in_closure y x
-| trans : ∀ {x y z}, in_closure x y → in_closure y z → in_closure x z 
-
-def closure (r : α → α → Prop) : setoid α :=
-⟨in_closure r, ⟨λ x, in_closure.refl r x, λ _ _, in_closure.symm, λ _ _ _, in_closure.trans⟩⟩ 
-
-theorem closure_eq (r : α → α → Prop) : closure r = Inf {s : setoid α | ∀ x y, r x y → s.r' x y} :=
+theorem eqv_gen_eq (r : α → α → Prop) : 
+  eqv_gen.setoid r = Inf {s : setoid α | ∀ x y, r x y → s.r' x y} :=
 setoid.ext' $ λ _ _, 
-  ⟨λ H, in_closure.rec (λ _ _ h _ hs, hs _ _ h) (setoid.refl' _) 
-    (λ _ _ _ h, setoid.symm' _ h) (λ _ _ _ _ _ h1 h2, setoid.trans' _ h1 h2) H, 
-  Inf_le _ _ (λ _ _, in_closure.of_r _ _) _ _⟩
+  ⟨λ H, eqv_gen.rec (λ _ _ h _ hs, hs _ _ h) (refl' _) 
+    (λ _ _ _ h, symm' _ h) (λ _ _ _ _ _ h1 h2, trans' _ h1 h2) H, 
+  Inf_le _ _ (λ _ _, eqv_gen.rel _ _) _ _⟩
 
-lemma sup_eq_closure (r s : setoid α) : r ⊔ s = closure (λ x y, r.r' x y ∨ s.r' x y) :=
-by rw closure_eq; apply congr_arg Inf; ext; exact 
+lemma sup_eq_eqv_gen (r s : setoid α) : r ⊔ s = eqv_gen.setoid (λ x y, r.r' x y ∨ s.r' x y) :=
+by rw eqv_gen_eq; apply congr_arg Inf; ext; exact 
   ⟨λ h _ _ H, or.elim H (λ hl, h.1 _ _ hl) $ λ hr, h.2 _ _ hr, 
    λ H, ⟨λ _ _ h, H _ _ $ or.inl h, λ _ _ h, H _ _ $ or.inr h⟩⟩
 
-lemma Sup_eq_closure (S : set (setoid α)) : 
-  Sup S = closure (λ x y, ∃ r : setoid α, r∈S ∧ r.r' x y) :=
-by rw closure_eq; apply congr_arg Inf; ext;
+lemma Sup_eq_eqv_gen (S : set (setoid α)) : 
+  Sup S = eqv_gen.setoid (λ x y, ∃ r : setoid α, r∈S ∧ r.r' x y) :=
+by rw eqv_gen_eq; apply congr_arg Inf; ext;
    exact ⟨λ h _ _ ⟨r, hr⟩, h r hr.1 _ _ hr.2, λ h r hS _ _ hr, h _ _ ⟨r, hS, hr⟩⟩
 
-theorem closure_le {r : α → α → Prop} {s : setoid α} (h : ∀ x y, r x y → s.r' x y) : 
-  closure r ≤ s :=
-by rw closure_eq; exact Inf_le _ _ h
+theorem eqv_gen_of_setoid (r : setoid α) : eqv_gen.setoid r.r = r :=
+le_antisymm (by rw eqv_gen_eq; exact Inf_le _ r (λ x y h, h)) eqv_gen.rel
 
-theorem closure_mono {r s : α → α → Prop} (h : ∀ x y, r x y → s x y) : closure r ≤ closure s :=
-closure_le $ λ x y hr, in_closure.of_r _ _ $ h x y hr
+theorem eqv_gen_le {r : α → α → Prop} {s : setoid α} (h : ∀ x y, r x y → s.r' x y) : 
+  eqv_gen.setoid r ≤ s :=
+by rw eqv_gen_eq; exact Inf_le _ _ h
+
+theorem eqv_gen_mono {r s : α → α → Prop} (h : ∀ x y, r x y → s x y) : 
+  eqv_gen.setoid r ≤ eqv_gen.setoid s :=
+eqv_gen_le $ λ x y hr, eqv_gen.rel _ _ $ h x y hr
+
+open function
+
+theorem injective_iff_ker_bot (f : α → β) :
+  injective f ↔ ker f = ⊥ := 
+⟨λ hf, setoid.ext' $ λ x y, ⟨λ h, hf h, λ h, h ▸ rfl⟩, 
+ λ hk x y h, show r' ⊥ x y, from hk ▸ (show (ker f).r' x y, from h)⟩
+
+lemma ker_apply_eq_preimage (f : α → β) (m) : (ker f).r' m = f ⁻¹' {f m} :=
+set.ext $ λ x,
+  ⟨λ h, set.mem_preimage.2 (set.mem_singleton_iff.2 h.symm),
+   λ h, (set.mem_singleton_iff.1 (set.mem_preimage.1 h)).symm⟩
+
+theorem lift_unique {r : setoid α} {f : α → β} (H : r ≤ ker f) (g : quotient r → β)
+  (Hg : f = g ∘ quotient.mk) : quotient.lift f H = g :=
+by ext; rcases x; erw [quotient.lift_beta f H, Hg]; refl
+
+lemma injective_ker_lift (f : α → β) : injective (@quotient.lift _ _ (ker f) f (λ _ _ h, h)) :=
+λ x y, by apply quotient.induction_on₂' x y; intros a b; show (ker f).r' a b → _; from quotient.sound'
+
+variables (r : setoid α)
+
+lemma ker_eq_of_equiv (h : quotient r ≃ β) (f : α → β) (H : ∀ x y, r.r' x y → f x = f y) 
+  (hh : quotient.lift f H = h) : ker f = r :=
+le_antisymm 
+ (λ x y hk, quotient.exact $ h.injective $ hh ▸ 
+    show quotient.lift f H ⟦x⟧ = quotient.lift f H ⟦y⟧, from hk) 
+ $ λ _ _ h, H _ _ h
+
+noncomputable def quotient_ker_equiv_range (f : α → β) : 
+  quotient (ker f) ≃ set.range f :=
+  @equiv.of_bijective _ (set.range f) (@quotient.lift _ (set.range f) (ker f) 
+              (λ x, ⟨f x, set.mem_range_self x⟩) $ λ a b h, subtype.eq' h)
+        ⟨λ x y h, injective_ker_lift f $ by rcases x; rcases y; injections, 
+         λ ⟨w, z, hz⟩, ⟨@quotient.mk _ (ker f) z, by rw quotient.lift_beta; exact subtype.ext.2 hz⟩⟩
+
+noncomputable def quotient_ker_equiv_of_surjective (f : α → β) (hf : surjective f) :
+  quotient (ker f) ≃ β :=
+@equiv.of_bijective _ _ (@quotient.lift _ _ (ker f) f (λ _ _ h, h)) 
+  ⟨injective_ker_lift f, λ y, exists.elim (hf y) $ λ w hw, ⟨quotient.mk' w, hw⟩⟩
+
+noncomputable def quotient_quotient_equiv_quotient (r s : setoid α) (h : r ≤ s) :
+  quotient (ker (quot.map_right h)) ≃ quotient s :=
+quotient_ker_equiv_of_surjective _ $ λ x, by rcases x; exact ⟨quotient.mk' x, rfl⟩
+
+def to_con (r : setoid α) (s : {s // r ≤ s}) : setoid (quotient r) :=
+{ r := λ x y, quotient.lift_on₂' x y s.1.r' $ λ _ _ _ _ ha hb, iff_iff_eq.1
+         ⟨λ h', s.1.trans' (s.1.symm' $ s.2 _ _ ha) $ s.1.trans' h' $ s.2 _ _ hb,
+          λ h', s.1.trans' (s.2 _ _ ha) $ s.1.trans' h' $ s.1.symm' $ s.2 _ _ hb⟩,
+  iseqv := ⟨λ x, quotient.induction_on' x $ λ y, s.1.refl' y,
+              λ x y, quotient.induction_on₂' x y $ λ _ _ h', s.1.symm' h',
+              λ x y z, quotient.induction_on₃' x y z $ λ _ _ _ h1 h2, s.1.trans' h1 h2⟩}
+
+def of_con (s : setoid (quotient r)) : setoid α :=
+⟨λ x y, s.r' ⟦x⟧ ⟦y⟧, ⟨λ _, s.refl' _, λ _ _ h, s.symm' h, λ _ _ _ h1 h2, s.trans' h1 h2⟩⟩
+
+def correspondence (r : setoid α) : ((≤) : {s // r ≤ s} → {s // r ≤ s} → Prop) ≃o
+((≤) : setoid (quotient r) → setoid (quotient r) → Prop) :=
+{ to_fun := λ s, r.to_con s,
+  inv_fun := λ s, subtype.mk (r.of_con s) $ 
+    λ x y h, show s.r' ⟦x⟧ ⟦y⟧, from quotient.sound h ▸ s.refl' ⟦x⟧,
+  left_inv := λ s, subtype.ext.2 $ show r.of_con (r.to_con s) = s.1, by ext; refl,
+  right_inv := λ s, by ext; rcases a; rcases b; refl,
+  ord := λ a b, ⟨λ hle x y, quotient.induction_on₂ x y $ λ w z h, hle w z h,
+                 λ H p q h, H ⟦p⟧ ⟦q⟧ h⟩ }
+
+def gi : @galois_insertion (α → α → Prop) (setoid α) _ _ eqv_gen.setoid r' :=
+{ choice := λ r h, eqv_gen.setoid r,
+  gc := λ r s, ⟨λ H _ _ h, H _ _ $ eqv_gen.rel _ _ h, λ H, (eqv_gen_eq r).symm ▸ Inf_le _ s H⟩,
+  le_l_u := λ x, (eqv_gen_of_setoid x).symm ▸ le_refl x,
+  choice_eq := λ _ _, rfl}
 
 end setoid

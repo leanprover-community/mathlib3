bbbe6bd2057bf21622071abab8ed64bb06ae2159
Scott Morrison <scott@tqft.net>
2019-09-04 20:25:34 +1000

    create /basic.lean files

--- src/category_theory/hom_functor.lean
+++ src/category_theory/hom_functor.lean
@@ -4,7 +4,7 @@ Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Reid Barton, Scott Morrison
 -/
 import category_theory.opposites
-import category_theory.products
+import category_theory.products.basic
 
 /-!
 The hom functor, sending `(X, Y)` to the type `X ⟶ Y`.
--- src/category_theory/sums/associator.lean
+++ src/category_theory/sums/associator.lean
@@ -3,7 +3,7 @@ Copyright (c) 2019 Scott Morrison. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Scott Morrison
 -/
-import category_theory.sums
+import category_theory.sums.basic
 
 /-#
 The associator functor `((C ⊕ D) ⊕ E) ⥤ (C ⊕ (D ⊕ E))` and its inverse form an equivalence.

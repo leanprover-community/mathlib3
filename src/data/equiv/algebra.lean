bb46e1054766d4332487279d1c078ac830edbe61
u6374247 <u6374247@anu.edu.au>
2019-09-09 20:08:29 +1000

    minor change

--- src/data/equiv/algebra.lean
+++ src/data/equiv/algebra.lean
@@ -1,7 +1,7 @@
 /-
 Copyright (c) 2018 Johannes Hölzl. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
-Authors: Johannes Hölzl
+Authors: Johannes Hölzl, Callum Sutton
 -/
 
 import data.equiv.basic algebra.field

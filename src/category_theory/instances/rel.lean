26c10497b75b14215ec137706b7677f7b69636b8
Yury G. Kudryashov <urkud@urkud.name>
2019-09-10 15:14:32 -0400

    Merge `category_theory/Rel` into the existing `instances/rel`

--- src/category_theory/instances/rel.lean
+++ src/category_theory/instances/rel.lean
@@ -1,9 +1,9 @@
 /-
 Copyright (c) 2019 Scott Morrison. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
-Authors: Scott Morrison
+Authors: Scott Morrison, Yury Kudryashov
 -/
-import category_theory.category
+import data.rel category_theory.types
 
 /-!
 The category of types with binary relations as morphisms.
@@ -20,9 +20,21 @@ def Rel := Type u
 /-- The category of types with binary relations as morphisms. -/
 -- We must work in `Type u` rather than `Sort u`, because
 -- `X → Y → Prop` is in `Sort (max u 1)`.
-instance rel : large_category Rel.{u} :=
-{ hom  := λ X Y, X → Y → Prop,
-  id   := λ X, λ x y, x = y,
-  comp := λ X Y Z f g x z, ∃ y, f x y ∧ g y z }
+instance Rel.category : large_category Rel.{u} :=
+{ hom := rel,
+  comp := @rel.comp,
+  id := @eq,
+  comp_id' := @rel.comp_right_id,
+  id_comp' := @rel.comp_left_id,
+  assoc' := @rel.comp_assoc }
+
+def graph'_functor : Type u ⥤ Rel.{u} :=
+{ obj := id,
+  map := @function.graph',
+  map_id' := @function.graph'_id,
+  map_comp' := @function.graph'_comp }
+
+instance graph'_functor_faithful : faithful graph'_functor :=
+⟨@function.graph'_inj⟩
 
 end category_theory

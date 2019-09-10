c8feb476969a124e65f3387e15316da4e1c22a40
Floris van Doorn <fpvdoorn@gmail.com>
2019-09-09 22:34:35 -0700

    import logic.basic in test

--- test/logic_inline.lean
+++ test/logic_inline.lean
@@ -1,3 +1,10 @@
+/-
+Copyright (c) 2019 Keeley Hoek. All rights reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
+Authors: Keeley Hoek, Floris van Doorn
+-/
+
+import logic.basic
 meta def error_msg (s : string) : bool := @undefined_core bool $ s ++ ".decidable not inlined"
 
 meta def examine (b : Prop) [decidable b] : bool := b

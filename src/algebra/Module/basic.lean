2509bc5b16b45a3cd0f3330d55d94faee04e9dc2
Robert Spencer <ras228@cam.ac.uk>
2019-09-11 06:53:04 +0100

    Minor indentation formatting

--- src/algebra/Module/basic.lean
+++ src/algebra/Module/basic.lean
@@ -15,16 +15,16 @@ variables (R : Type u) [ring R]
 
 /-- The category of R-modules and their morphisms. -/
 structure Module :=
-  (carrier : Type u)
-  [is_add_comm_group : add_comm_group carrier]
-  [is_module : module R carrier]
+(carrier : Type u)
+[is_add_comm_group : add_comm_group carrier]
+[is_module : module R carrier]
 
 attribute [instance] Module.is_add_comm_group Module.is_module
 
 namespace Module
 
 instance : has_coe_to_sort (Module R) :=
-  { S := Type u, coe := Module.carrier }
+{ S := Type u, coe := Module.carrier }
 
 def of (X : Type u) [add_comm_group X] [module R X] : Module R := ⟨R, X⟩
 
@@ -32,11 +32,10 @@ instance : has_zero (Module R) := ⟨ of R punit ⟩
 
 variables (M N U : Module R)
 
-instance : category (Module R) := {
-  hom := λ M N, M →ₗ[R] N,
-  id := λ M, 1,
-  comp := λ A B C f g, g.comp f
-}
+instance : category (Module R) :=
+{ hom  := λ M N, M →ₗ[R] N,
+  id   := λ M, 1,
+  comp := λ A B C f g, g.comp f }
 
 @[simp] lemma id_apply (m : M) : (𝟙 M : M → M) m = m := rfl
 
@@ -44,7 +43,7 @@ instance : category (Module R) := {
   ((f ≫ g) : M → U) = g ∘ f := rfl
 
 @[extensionality] lemma hom_ext  {f g : M ⟶ N} : (f : M → N) = g → f = g :=
-  λ w, linear_map.ext (function.funext_iff.1 w)
+λ w, linear_map.ext (function.funext_iff.1 w)
 
 instance hom_is_module_hom {M₁ M₂ : Module R} (f : M₁ ⟶ M₂) :
   is_linear_map R (f : M₁ → M₂) := linear_map.is_linear _

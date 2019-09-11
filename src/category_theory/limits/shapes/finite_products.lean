8c6e3ceaff474e8df6e0c09b21f69369b55a2429
Scott Morrison <scott@tqft.net>
2019-09-11 18:42:03 +1000

    fix

--- src/category_theory/limits/shapes/finite_products.lean
+++ src/category_theory/limits/shapes/finite_products.lean
@@ -29,8 +29,8 @@ instance has_finite_coproducts_of_has_coproducts [has_coproducts.{v} C] : has_fi
 { has_colimits_of_shape := λ J _, by apply_instance }
 
 instance has_finite_products_of_has_finite_limits [has_finite_limits.{v} C] : has_finite_products.{v} C :=
-{ has_limits_of_shape := λ J _, by { resetI, classical, apply_instance } }
+{ has_limits_of_shape := λ J _ _, by { resetI, apply_instance } }
 instance has_finite_coproducts_of_has_finite_colimits [has_finite_colimits.{v} C] : has_finite_coproducts.{v} C :=
-{ has_colimits_of_shape := λ J _, by { resetI, classical, apply_instance } }
+{ has_colimits_of_shape := λ J _ _, by { resetI, apply_instance } }
 
 end category_theory.limits

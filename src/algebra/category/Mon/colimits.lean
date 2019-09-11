1e567f98a36b589887fb68f6756aacd8201bbb3a
Scott Morrison <scott@tqft.net>
2019-09-11 19:11:54 +1000

    chore(*): renaming files constructing category instances

--- src/algebra/category/Mon/colimits.lean
+++ src/algebra/category/Mon/colimits.lean
@@ -3,7 +3,7 @@ Copyright (c) 2019 Scott Morrison. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Scott Morrison
 -/
-import algebra.Mon.basic
+import algebra.category.Mon.basic
 import category_theory.limits.limits
 
 /-!

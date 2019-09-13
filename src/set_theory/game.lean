8801aa819cbc9f2905e76b56847774a7aaebbc93
Scott Morrison <scott@tqft.net>
2019-09-13 12:58:04 +1000

    adding Reid to the author line

--- src/set_theory/game.lean
+++ src/set_theory/game.lean
@@ -1,7 +1,7 @@
 /-
 Copyright (c) 2019 Mario Carneiro. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
-Authors: Mario Carneiro, Isabel Longbottom, Scott Morrison
+Authors: Reid Barton, Mario Carneiro, Isabel Longbottom, Scott Morrison
 -/
 import data.equiv.basic logic.embedding
 import data.nat.cast

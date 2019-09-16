0f620f9eea3bcdf38af24faabac8557a83eedee1
Johan Commelin <johan@commelin.net>
2019-09-16 17:10:49 +0200

    State lemmas as iff

--- src/logic/basic.lean
+++ src/logic/basic.lean
@@ -81,11 +81,13 @@ attribute [symm] ne.symm
 
 lemma ne_comm {α} {a b : α} : a ≠ b ↔ b ≠ a := ⟨ne.symm, ne.symm⟩
 
-lemma eq_iff_eq_cancel_left (a b c : α) (h : b = c) :
-  a = b ↔ a = c := by rw h
+lemma eq_iff_eq_cancel_left {b c : α} :
+  (∀ {a}, a = b ↔ a = c) ↔ (b = c) :=
+⟨λ h, by rw [← h], λ h a, by rw h⟩
 
-lemma eq_iff_eq_cancel_right (a b c : α) (h : a = b) :
-  a = c ↔ b = c := by rw h
+lemma eq_iff_eq_cancel_right {a b : α} :
+  (∀ {c}, a = c ↔ b = c) ↔ (a = b) :=
+⟨λ h, by rw h, λ h a, by rw h⟩
 
 end miscellany
 

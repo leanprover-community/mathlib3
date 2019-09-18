b51978df49b72a5caa70a7525e95bc332162efee
Johan Commelin <johan@commelin.net>
2019-09-18 15:43:01 +0200

    chore(data/mllist): Typo in author name [ci skip] (#1458)

--- src/data/mllist.lean
+++ src/data/mllist.lean
@@ -1,7 +1,7 @@
 /-
 Copyright (c) 2018 Scott Morrison. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
-Author: Mario Carnerio, Keeley Hoek, Simon Hudon, Scott Morrison
+Author: Mario Carneiro, Keeley Hoek, Simon Hudon, Scott Morrison
 
 Monadic lazy lists.
 

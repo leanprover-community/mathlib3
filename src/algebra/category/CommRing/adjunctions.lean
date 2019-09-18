aaa8b1ee928a9fab20df08a5d81e009ed9c96678
Yury G. Kudryashov <urkud@urkud.name>
2019-09-17 23:48:38 -0400

    Fix compile: now `ring_hom.ext` takes fewer explicit args

--- src/algebra/category/CommRing/adjunctions.lean
+++ src/algebra/category/CommRing/adjunctions.lean
@@ -28,8 +28,8 @@ private def free_obj (α : Type u) : CommRing.{u} := ⟨mv_polynomial α ℤ⟩
 def free : Type u ⥤ CommRing.{u} :=
 { obj := free_obj,
   map := λ _ _ f, @ring_hom.of _ _ _ _ (rename f) (rename.is_semiring_hom f),
-  map_id' := λ X, ring_hom.ext _ _ $ funext $ rename_id,
-  map_comp' := λ X Y Z f g, ring_hom.ext _ _ $ funext $ λ p, (rename_rename f g p).symm }
+  map_id' := λ X, ring_hom.ext $ funext $ rename_id,
+  map_comp' := λ X Y Z f g, ring_hom.ext $ funext $ λ p, (rename_rename f g p).symm }
 
 @[simp] lemma free_obj_coe {α : Type u} :
   (free.obj α : Type u) = mv_polynomial α ℤ := rfl
@@ -37,7 +37,7 @@ def free : Type u ⥤ CommRing.{u} :=
 @[simp] lemma free_map_coe {α β : Type u} {f : α → β} :
   ⇑(free.map f) = rename f := rfl
 
-def adj : free ⊣ (forget CommRing) :=
+def adj : free ⊣ forget CommRing :=
 adjunction.mk_of_hom_equiv
 { hom_equiv := λ X R, hom_equiv,
   hom_equiv_naturality_left_symm' := by {intros, ext, dsimp, apply eval₂_cast_comp} }

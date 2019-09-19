b11f0f149dbb73a3c978293a4cdeb3f01227b98f
Yury G. Kudryashov <urkud@urkud.name>
2019-09-18 22:38:18 -0400

    refactor(category_theory): refactor concrete categories (#1380)
    * trying to use bundled homs
    
    * bundled monoids should use bundled homs
    
    * fixes
    
    * fixes
    
    * refactor(*): rewrite concrete categories
    
    * Add module documentation
    
    * local instance
    
    * Move files around; don't touch content yet
    
    * Move code around, fix some compile errors
    
    * Add `unbundled_hom.lean`
    
    * Turn `hom` into an argument of `(un)bundled_hom`
    
    For some reason, it helps Lean find coercions from `Ring` category
    morphisms to functions.
    
    * Define `unbundled_hom.mk_has_forget`; fix compile
    
    * Add some documentation
    
    * Fix compile
    
    * chore(category_theory): require morphisms live in Type
    
    * move back to Type
    
    * tweaks to doc-comments
    
    * fixing some formatting
    
    * Revert most of the changes to `data/mv_polynomials`
    
    * Drop unused argument, add a comment
    
    * Simplify universe levels in `concrete_category/basic`.
    
    Still fails to compile.
    
    * Simplify universe levels in `concrete_category/{un,}bundled_hom`
    
    * fixes
    
    * Fix an `import`
    
    * Doc cleanup
    
    * update post #1412
    
    * Drop `simp`
    
    * Rename variable
    
    Co-Authored-By: Johan Commelin <johan@commelin.net>
    
    * Rename variable
    
    Co-Authored-By: Johan Commelin <johan@commelin.net>
    
    * Fix more issues reported by @rwbarton
    
    * Rename variable
    
    Co-Authored-By: Reid Barton <rwbarton@gmail.com>
    
    * Use `subtype.eq` instead of `subtype.ext.2`
    
    * Cleanup
    
    * Fix compile: now `ring_hom.ext` takes fewer explicit args
    
    * Fix compile
    
    * Run `sanity_check` on all files modified in this branch
    
    * Fix most of the issues reported
    
    * Except for the old ones in `mv_polynomial` (postponed to a separate
      PR) and a false positive in `single_obj`
    
    * Review `∀` vs `Π`
    
    * Remove some unnecessary parentheses
    
    * add comment
    
    * punit_instances: no need to explicitly provide constants and operations
    
    * Rename `has_forget` to `has_forget₂`
    
    * add comment, simplify comm_ring punit
    
    * Drop `private def free_obj`
    
    * documentation
    
    * fix comment
    
    * tweaks
    
    * comments
    
    * documentation on touched files
    
    * many doc-strings

--- src/algebra/category/CommRing/limits.lean
+++ src/algebra/category/CommRing/limits.lean
@@ -9,6 +9,17 @@ import category_theory.limits.preserves
 import ring_theory.subring
 import algebra.pi_instances
 
+/-!
+# The category of commutative rings has all limits
+
+Further, these limits are preserves by the forgetful functor --- that is,
+the underlying types are just the limits in the category of types.
+
+## Further work
+A lot of this should be generalised / automated, as it's quite common for concrete
+categories that the forgetful functor preserves limits.
+-/
+
 open category_theory
 open category_theory.limits
 
@@ -18,22 +29,20 @@ namespace CommRing
 
 variables {J : Type u} [small_category J]
 
-instance comm_ring_obj (F : J ⥤ CommRing.{u}) (j) : comm_ring ((F ⋙ CommRing.forget).obj j) :=
-by { dsimp, apply_instance }
-instance is_ring_hom_map (F : J ⥤ CommRing.{u}) (j j') (f : j ⟶ j') : is_ring_hom ((F ⋙ CommRing.forget).map f) :=
+instance comm_ring_obj (F : J ⥤ CommRing.{u}) (j) :
+  comm_ring ((F ⋙ forget CommRing).obj j) :=
 by { dsimp, apply_instance }
 
-instance sections_submonoid (F : J ⥤ CommRing.{u}) : is_submonoid (F ⋙ forget).sections :=
+instance sections_submonoid (F : J ⥤ CommRing.{u}) :
+  is_submonoid (F ⋙ forget CommRing).sections :=
 { one_mem := λ j j' f,
   begin
-    simp only [functor.comp_map],
-    erw is_ring_hom.map_one (CommRing.forget.map (F.map f)),
-    refl,
+    erw [functor.comp_map, forget_map_eq_coe, (F.map f).map_one],
+    refl
   end,
   mul_mem := λ a b ah bh j j' f,
   begin
-    simp only [functor.comp_map],
-    erw is_ring_hom.map_mul (CommRing.forget.map (F.map f)),
+    erw [functor.comp_map, forget_map_eq_coe, (F.map f).map_mul],
     dsimp [functor.sections] at ah,
     rw ah f,
     dsimp [functor.sections] at bh,
@@ -41,17 +50,16 @@ instance sections_submonoid (F : J ⥤ CommRing.{u}) : is_submonoid (F ⋙ forge
     refl,
   end }
 
-instance sections_add_submonoid (F : J ⥤ CommRing.{u}) : is_add_submonoid (F ⋙ forget).sections :=
+instance sections_add_submonoid (F : J ⥤ CommRing.{u}) :
+  is_add_submonoid (F ⋙ forget CommRing).sections :=
 { zero_mem := λ j j' f,
   begin
-    simp only [functor.comp_map],
-    erw is_ring_hom.map_zero (CommRing.forget.map (F.map f)),
+    erw [functor.comp_map, forget_map_eq_coe, (F.map f).map_zero],
     refl,
   end,
   add_mem := λ a b ah bh j j' f,
   begin
-    simp only [functor.comp_map],
-    erw is_ring_hom.map_add (CommRing.forget.map (F.map f)),
+    erw [functor.comp_map, forget_map_eq_coe, (F.map f).map_add],
     dsimp [functor.sections] at ah,
     rw ah f,
     dsimp [functor.sections] at bh,
@@ -59,56 +67,72 @@ instance sections_add_submonoid (F : J ⥤ CommRing.{u}) : is_add_submonoid (F �
     refl,
   end }
 
-instance sections_add_subgroup (F : J ⥤ CommRing.{u}) : is_add_subgroup (F ⋙ forget).sections :=
+instance sections_add_subgroup (F : J ⥤ CommRing.{u}) :
+  is_add_subgroup (F ⋙ forget CommRing).sections :=
 { neg_mem := λ a ah j j' f,
   begin
-    simp only [functor.comp_map],
-    erw is_ring_hom.map_neg (CommRing.forget.map (F.map f)),
+    erw [functor.comp_map, forget_map_eq_coe, (F.map f).map_neg],
     dsimp [functor.sections] at ah,
     rw ah f,
     refl,
   end,
   ..(CommRing.sections_add_submonoid F) }
 
-instance sections_subring (F : J ⥤ CommRing.{u}) : is_subring (F ⋙ forget).sections :=
+instance sections_subring (F : J ⥤ CommRing.{u}) :
+  is_subring (F ⋙ forget CommRing).sections :=
 { ..(CommRing.sections_submonoid F),
   ..(CommRing.sections_add_subgroup F) }
 
-instance limit_comm_ring (F : J ⥤ CommRing.{u}) : comm_ring (limit (F ⋙ forget)) :=
-@subtype.comm_ring ((Π (j : J), (F ⋙ forget).obj j)) (by apply_instance) _
+instance limit_comm_ring (F : J ⥤ CommRing.{u}) :
+  comm_ring (limit (F ⋙ forget CommRing)) :=
+@subtype.comm_ring ((Π (j : J), (F ⋙ forget _).obj j)) (by apply_instance) _
   (by convert (CommRing.sections_subring F))
 
-instance limit_π_is_ring_hom (F : J ⥤ CommRing.{u}) (j) : is_ring_hom (limit.π (F ⋙ CommRing.forget) j) :=
+instance limit_π_is_ring_hom (F : J ⥤ CommRing.{u}) (j) :
+  is_ring_hom (limit.π (F ⋙ forget CommRing) j) :=
 { map_one := by { simp only [types.types_limit_π], refl },
   map_mul := λ x y, by { simp only [types.types_limit_π], refl },
   map_add := λ x y, by { simp only [types.types_limit_π], refl } }
 
-def limit (F : J ⥤ CommRing.{u}) : cone F :=
-{ X := ⟨limit (F ⋙ forget), by apply_instance⟩,
-  π :=
-  { app := λ j, ⟨limit.π (F ⋙ forget) j, by apply_instance⟩,
-    naturality' := λ j j' f, subtype.eq ((limit.cone (F ⋙ forget)).π.naturality f) } }
+-- The next two definitions are used in the construction of `has_limits CommRing`.
+-- After that, the limits should be constructed using the generic limits API,
+-- e.g. `limit F`, `limit.cone F`, and `limit.is_limit F`.
 
-def limit_is_limit (F : J ⥤ CommRing.{u}) : is_limit (limit F) :=
+private def limit (F : J ⥤ CommRing.{u}) : cone F :=
+{ X := ⟨limit (F ⋙ forget _), by apply_instance⟩,
+  π :=
+  { app := λ j, ring_hom.of $ limit.π (F ⋙ forget _) j,
+    naturality' := λ j j' f,
+      ring_hom.ext ((limit.cone (F ⋙ forget _)).π.naturality f) } }
+
+private def limit_is_limit (F : J ⥤ CommRing.{u}) : is_limit (limit F) :=
 begin
-  refine is_limit.of_faithful forget (limit.is_limit _) (λ s, ⟨_, _⟩) (λ s, rfl),
-  dsimp, split,
+  refine is_limit.of_faithful
+    (forget CommRing) (limit.is_limit _)
+    (λ s, ⟨_, _, _, _, _⟩) (λ s, rfl); dsimp,
   { apply subtype.eq, funext, dsimp,
-    erw is_ring_hom.map_one (CommRing.forget.map (s.π.app j)), refl },
+    erw (s.π.app j).map_one, refl },
   { intros x y, apply subtype.eq, funext, dsimp,
-    erw is_ring_hom.map_mul (CommRing.forget.map (s.π.app j)), refl },
+    erw (s.π.app j).map_mul, refl },
+  { apply subtype.eq, funext, dsimp,
+    erw (s.π.app j).map_zero, refl },
   { intros x y, apply subtype.eq, funext, dsimp,
-    erw is_ring_hom.map_add (CommRing.forget.map (s.π.app j)), refl },
+    erw (s.π.app j).map_add, refl }
 end
 
+/-- The category of commutative rings has all limits. -/
 instance CommRing_has_limits : has_limits.{u} CommRing.{u} :=
 { has_limits_of_shape := λ J 𝒥,
   { has_limit := λ F, by exactI { cone := limit F, is_limit := limit_is_limit F } } }
 
-instance forget_preserves_limits : preserves_limits (forget : CommRing.{u} ⥤ Type u) :=
+/--
+The forgetful functor from commutative rings to types preserves all limits. (That is, the underlying
+types could have been computed instead as limits in the category of types.)
+-/
+instance forget_preserves_limits : preserves_limits (forget CommRing.{u}) :=
 { preserves_limits_of_shape := λ J 𝒥,
   { preserves_limit := λ F,
     by exactI preserves_limit_of_preserves_limit_cone
-      (limit.is_limit F) (limit.is_limit (F ⋙ forget)) } }
+      (limit.is_limit F) (limit.is_limit (F ⋙ forget _)) } }
 
 end CommRing

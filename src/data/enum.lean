674912da36238e849ffd6105fb3835f19a7df8ed
Simon Hudon <simon.hudon@gmail.com>
2019-09-18 22:04:15 -0400

    Update enum.lean

--- src/data/enum.lean
+++ src/data/enum.lean
@@ -2,7 +2,7 @@
 Copyright (c) 2019 Simon Hudon. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Author(s): Simon Hudon
-/-
+-/
 import category.monad.basic
 import data.list.basic
 import data.equiv.basic

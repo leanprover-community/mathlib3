7db1830c8f7a6f65d1aa19b6c388cea821d8b156
Yury G. Kudryashov <urkud@urkud.name>
2019-09-23 08:55:30 -0400

    refactor(algebra/*): Make `monoid_hom.ext` etc use `∀ x, f x = g x` as an assumption

--- src/data/mv_polynomial.lean
+++ src/data/mv_polynomial.lean
@@ -780,7 +780,7 @@ functiosn out of the the type `σ`, -/
 def hom_equiv : (mv_polynomial σ ℤ →+* β) ≃ (σ → β) :=
 { to_fun := λ f, ⇑f ∘ X,
   inv_fun := λ f, ring_hom.of (eval₂ (λ n : ℤ, (n : β)) f),
-  left_inv := λ f, ring_hom.ext $ funext $ eval₂_hom_X _ _,
+  left_inv := λ f, ring_hom.ext  $ eval₂_hom_X _ _,
   right_inv := λ f, funext $ λ x, by simp only [ring_hom.coe_of, function.comp_app, eval₂_X] }
 
 end eval₂

718ee0f5c239f73769c7e7d90fcba3d45eaca39e
Floris van Doorn <fpvdoorn@gmail.com>
2019-09-24 18:52:22 -0400

    fix errors
    in the process also add some lemmas about bxor to simplify proofs

--- src/data/int/basic.lean
+++ src/data/int/basic.lean
@@ -795,22 +795,20 @@ lemma units_inv_eq_self (u : units ℤ) : u⁻¹ = u :=
 @[simp] lemma bodd_two : bodd 2 = ff := rfl
 
 @[simp] lemma bodd_sub_nat_nat (m n : ℕ) : bodd (sub_nat_nat m n) = bxor m.bodd n.bodd :=
-by apply sub_nat_nat_elim m n (λ m n i, bodd i = bxor m.bodd n.bodd);
-   intros i m; simp [bodd]; cases i.bodd; cases m.bodd; refl
+by apply sub_nat_nat_elim m n (λ m n i, bodd i = bxor m.bodd n.bodd); intros;
+  simp [bodd, -of_nat_eq_coe]
 
 @[simp] lemma bodd_neg_of_nat (n : ℕ) : bodd (neg_of_nat n) = n.bodd :=
 by cases n; simp; refl
 
 @[simp] lemma bodd_neg (n : ℤ) : bodd (-n) = bodd n :=
-by cases n; unfold has_neg.neg; simp [int.coe_nat_eq, int.neg, bodd]
+by cases n; simp [has_neg.neg, int.coe_nat_eq, int.neg, bodd, -of_nat_eq_coe]
 
 @[simp] lemma bodd_add (m n : ℤ) : bodd (m + n) = bxor (bodd m) (bodd n) :=
-by cases m with m m; cases n with n n; unfold has_add.add; simp [int.add, bodd];
-   cases m.bodd; cases n.bodd; refl
+by cases m with m m; cases n with n n; unfold has_add.add; simp [int.add, bodd, -of_nat_eq_coe]
 
 @[simp] lemma bodd_mul (m n : ℤ) : bodd (m * n) = bodd m && bodd n :=
-by cases m with m m; cases n with n n; unfold has_mul.mul; simp [int.mul, bodd];
-   cases m.bodd; cases n.bodd; refl
+by cases m with m m; cases n with n n; unfold has_mul.mul; simp [int.mul, bodd, -of_nat_eq_coe]
 
 theorem bodd_add_div2 : ∀ n, cond (bodd n) 1 0 + 2 * div2 n = n
 | (n : ℕ) :=

b5a94ede87a51c75d189955015ea8b344b459a5a
Yury G. Kudryashov <urkud@urkud.name>
2019-09-25 22:02:32 -0400

    fix(scripts/mk_all): ignore hidden files
    Emacs sometimes creates temporary files with names like `.#name.lean`.

--- scripts/mk_all.sh
+++ scripts/mk_all.sh
@@ -7,7 +7,7 @@ for d in $(find $DIR/../src/ -type d)
 # For every subfolder of src/ (including src/ itself)
 do
   cd "$d" # cd to that directory
-  find . -maxdepth 1 -mindepth 1 -name 'all.lean' -prune -o -name '*.lean' -print -o -type d -print |
+  find . -maxdepth 1 -mindepth 1 -name 'all.lean' -prune -o -name '.*' -prune -o -name '*.lean' -print -o -type d -print |
   # find all files with .lean extension (except all.lean) and all subdirectories
   sed 's/$/\.all/; s/\.lean\.all//; s/^\.\//       \./; 1s/^      /\/- automatically generated file importing all files in this directory and subdirectories. -\/\nimport/' > all.lean
   # Now modify this output so that Lean can parse it. Changes `./foo.lean` to `.foo` and `foodir` to `.foodir.all`. Also adds indentation, a comment and `import`. Write this to `all.lean`

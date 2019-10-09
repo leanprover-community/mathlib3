88a1944d87d92137f57a7eeaebc8d561bdeb58d3
Chris Hughes <33847686+ChrisHughes24@users.noreply.github.com>
2019-10-09 16:26:08 +0100

    Update src/ring_theory/algebra.lean
    Co-Authored-By: Johan Commelin <johan@commelin.net>

--- src/ring_theory/algebra.lean
+++ src/ring_theory/algebra.lean
@@ -484,7 +484,7 @@ instance id : algebra R R :=
 algebra.of_ring_hom id $ by apply_instance
 
 def of_id : R →ₐ A :=
-{ commutes' := λ _, rfl.. ring_hom.of (algebra_map A) }
+{ commutes' := λ _, rfl, .. ring_hom.of (algebra_map A) }
 variables {R}
 
 theorem of_id_apply (r) : of_id R A r = algebra_map A r := rfl

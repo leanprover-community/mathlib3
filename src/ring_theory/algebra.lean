24aad2ec3a84bc2765194b1452891d11fefe07f4
Chris Hughes <chrishughes24@gmail.com>
2019-10-11 00:57:13 +0100

    fix build

--- src/ring_theory/algebra.lean
+++ src/ring_theory/algebra.lean
@@ -387,8 +387,8 @@ variables (S : subalgebra R A)
 
 instance : is_subring (S : set A) := S.subring
 instance : ring S := @@subtype.ring _ S.is_subring
-instance (R : Type u) (A : Type v) [comm_ring R] [comm_ring A]
-  [algebra R A] (S : subalgebra R A) : comm_ring S := @@subtype.comm_ring _ S.is_subring
+instance (R : Type u) (A : Type v) {rR : comm_ring R} {rA : comm_ring A}
+  {aA : algebra R A} (S : subalgebra R A) : comm_ring S := @@subtype.comm_ring _ S.is_subring
 
 instance algebra : algebra R S :=
 { smul := λ (c:R) x, ⟨c • x.1,

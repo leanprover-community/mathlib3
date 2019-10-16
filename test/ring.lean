076db0c2a26a804cd470a5f0732d12962e9a346f
Mario Carneiro <di.gama@gmail.com>
2019-10-16 10:31:26 -0400

    add test

--- test/ring.lean
+++ test/ring.lean
@@ -8,6 +8,7 @@ example (x y : ℚ) : x / 2 + x / 2 = x := by ring
 example (x y : ℚ) : (x + y) ^ 3 = x ^ 3 + y ^ 3 + 3 * (x * y ^ 2 + x ^ 2 * y) := by ring
 example (x y : ℝ) : (x + y) ^ 3 = x ^ 3 + y ^ 3 + 3 * (x * y ^ 2 + x ^ 2 * y) := by ring
 example {α} [comm_semiring α] (x : α) : (x + 1) ^ 6 = (1 + x) ^ 6 := by try_for 15000 {ring}
+example (n : ℕ) : (n / 2) + (n / 2) = 2 * (n / 2) := by ring
 example (a n s: ℕ) : a * (n - s) = (n - s) * a := by ring
 
 example (x y z : ℚ) (hx : x ≠ 0) (hy : y ≠ 0) (hz : z ≠ 0) :

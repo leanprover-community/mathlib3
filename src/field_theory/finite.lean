3cfe90e1283d04070c146dfe13fffd558e552837
Johan Commelin <johan@commelin.net>
2019-10-17 13:10:30 +0200

    Finish proof of Chevalley-Warning

--- src/field_theory/finite.lean
+++ src/field_theory/finite.lean
@@ -167,9 +167,7 @@ variables [discrete_field α] [fintype α] {σ : Type*} [fintype σ] [decidable_
 lemma sum_mul_sum {ι₁ : Type*} {ι₂ : Type*} (s₁ : finset ι₁) (s₂ : finset ι₂)
   (f₁ : ι₁ → α) (f₂ : ι₂ → α) :
   s₁.sum f₁ * s₂.sum f₂ = (s₁.product s₂).sum (λ p, f₁ p.1 * f₂ p.2) :=
-begin
-  sorry
-end
+by { rw [sum_product, sum_mul, sum_congr rfl], intros, rw mul_sum }
 
 lemma sum_mv_polynomial_eq_zero (f : mv_polynomial σ α)
   (h : f.total_degree < (q - 1) * fintype.card σ) :
@@ -194,27 +192,64 @@ begin
     rw eq_univ_iff_forall,
     intro i, rw [finsupp.mem_support_iff, ne.def, ← nat.le_zero_iff],
     push_neg, exact lt_of_lt_of_le hq (h _), },
-  suffices claim : (univ.filter (λ (x : σ → α), ∀ j, j ≠ i → x j = 0)).sum (λ x, x i ^ d i) *
-    (univ.filter (λ (x : σ → α), x i = 0)).sum
-    (λ (x : σ → α), (univ \ finset.singleton i).prod (λ j, x j ^ d j)) = 0,
-  { rw sum_mul_sum at claim, rw ← claim, symmetry,
-    refine sum_bij (λ p _ j, if j = i then p.1 j else p.2 j) (λ _ _, mem_univ _) _ _ _,
-    { rintros ⟨x,y⟩ hxy, rw [mem_product, mem_filter, mem_filter] at hxy,
-      sorry -- is this even true??
-      -- simp at hxy,
-      -- simp,
-       },
-      all_goals { sorry } },
-  { rw mul_eq_zero, left,
-    conv_rhs {rw ← sum_pow_lt_card_sub_one (d i) hi},
-    refine sum_bij (λ x _, x i) (λ _ _, mem_univ _) (λ _ _, rfl) _ _,
-    { intros x y hx hy H, rw mem_filter at hx hy,
-      funext j, by_cases hj : j = i, {rwa hj},
-      rw [hx.2 _ hj, hy.2 _ hj], },
-    { intros a ha,
-      refine ⟨λ j, if j = i then a else 0, _, (if_pos rfl).symm⟩,
-      rw mem_filter,
-      exact ⟨mem_univ _, λ j hj, dif_neg hj⟩ } }
+  by_cases hd' : d.support = univ,
+  { suffices claim : (univ.filter (λ (x : σ → α), ∀ j, j ≠ i → x j = 0)).sum (λ x, x i ^ d i) *
+      (univ.filter (λ (x : σ → α), x i = 0)).sum
+      (λ (x : σ → α), (univ \ finset.singleton i).prod (λ j, x j ^ d j)) = 0,
+    { rw sum_mul_sum at claim,
+      rw [← claim, hd'], symmetry,
+      refine sum_bij (λ p _ j, if j = i then p.1 j else p.2 j) (λ _ _, mem_univ _) _ _ _,
+      { rintros ⟨x,y⟩ hxy, rw [mem_product, mem_filter, mem_filter] at hxy,
+        rw [← prod_sdiff (subset_univ (finset.singleton i)), prod_singleton, if_pos rfl, mul_comm],
+        congr' 1, apply prod_congr rfl, intros j hj, rw [mem_sdiff, mem_singleton] at hj,
+        rw if_neg hj.2, },
+      { rintros ⟨x₁, x₂⟩ ⟨y₁, y₂⟩ hx hy hxy, rw function.funext_iff at hxy, rw prod.mk.inj_iff,
+        rw [mem_product, mem_filter, mem_filter] at hx hy,
+        split; funext j; by_cases hj : j = i,
+        { specialize hxy i, rw [if_pos rfl, if_pos rfl] at hxy, rwa hj },
+        { simp only [true_and, mem_univ, ne.def] at hx hy, rw [hx.1 j hj, hy.1 j hj], },
+        { dsimp at hx hy, rw [hj, hx.2.2, hy.2.2], },
+        { specialize hxy j, rwa [if_neg hj, if_neg hj] at hxy }, },
+      { intros x hx, refine ⟨⟨λ j, if j = i then x j else 0, λ j, if j = i then 0 else x j⟩, _, _⟩,
+        { rw [mem_product, mem_filter, mem_filter],
+          refine ⟨⟨mem_univ _, λ j hj, if_neg hj⟩, ⟨mem_univ _, _⟩⟩,
+          simp only [if_true, if_pos rfl], },
+        { funext j, split_ifs with hj; refl }, } },
+    { rw mul_eq_zero, left,
+      conv_rhs {rw ← sum_pow_lt_card_sub_one (d i) hi},
+      refine sum_bij (λ x _, x i) (λ _ _, mem_univ _) (λ _ _, rfl) _ _,
+      { intros x y hx hy H, rw mem_filter at hx hy,
+        funext j, by_cases hj : j = i, {rwa hj},
+        rw [hx.2 _ hj, hy.2 _ hj], },
+      { intros a ha,
+        refine ⟨λ j, if j = i then a else 0, _, (if_pos rfl).symm⟩,
+        rw mem_filter,
+        exact ⟨mem_univ _, λ j hj, dif_neg hj⟩ } } },
+  { rw eq_univ_iff_forall at hd', push_neg at hd', rcases hd' with ⟨i₀, hi₀⟩,
+    let stratified : finset (σ → α) := univ.bind (λ a : α, univ.filter (λ x, x i₀ = a)),
+    suffices : stratified = univ,
+    { rw [← this, sum_bind],
+      { suffices aux : (λ (a : α), (univ.filter (λ (x : σ → α), x i₀ = a)).sum
+            (λ (x : σ → α), finset.prod (d.support) (λ (j : σ), x j ^ d j))) =
+          (λ (a : α), (univ.filter (λ (x : σ → α), x i₀ = 0)).sum
+            (λ (x : σ → α), finset.prod (d.support) (λ (j : σ), x j ^ d j))),
+        { simp [aux, add_monoid.smul_eq_mul, card_univ, cast_card_eq_zero], },
+        funext a,
+        refine sum_bij (λ x _ j, if j = i₀ then 0 else x j) _ _ _ _,
+        { intros x hx, rw mem_filter at hx ⊢, exact ⟨mem_univ _, if_pos rfl⟩ },
+        { intros x hx, apply prod_congr rfl, intros j hj, rw if_neg, rintro rfl, exact hi₀ hj },
+        { intros x y hx hy hxy, rw mem_filter at hx hy, rw function.funext_iff at hxy ⊢,
+          intros j, specialize hxy j,
+          by_cases hj : j = i₀, { rw [hj, hx.2, hy.2] }, { rwa [if_neg hj, if_neg hj] at hxy } },
+        { intros x hx, refine ⟨λ j, if j = i₀ then a else x j, _, _⟩,
+          { rw mem_filter at hx ⊢, exact ⟨mem_univ _, if_pos rfl⟩ },
+          { funext j, split_ifs with hj, { rw mem_filter at hx, rw [hj, hx.2] }, { refl } } } },
+      { intros x hx y hy hxy, rw disjoint_iff, contrapose! hxy,
+        rcases exists_mem_of_ne_empty hxy with ⟨z, hz⟩,
+        rw [inf_eq_inter, mem_inter, mem_filter, mem_filter] at hz,
+        rcases hz with ⟨⟨_, rfl⟩, ⟨_, rfl⟩⟩, refl } },
+    { rw eq_univ_iff_forall, intro x, rw mem_bind,
+      refine ⟨x i₀, mem_univ _, _⟩, rw mem_filter, exact ⟨mem_univ _, rfl⟩ } },
 end
 
 -- move this

f7baaf193203c795ac3ea5d8a171bbee84f1c51e
Scott Morrison <scott.morrison@anu.edu.au>
2019-10-17 14:44:40 +1100

    test properly

--- test/refine_list.lean
+++ test/refine_list.lean
@@ -5,7 +5,7 @@
 import tactic.refine_list
 
 /- Turn off trace messages so they don't pollute the test build: -/
---set_option trace.silence_refine_list true
+set_option trace.silence_refine_list true
 
 open tactic
 
@@ -18,51 +18,51 @@ end
 
 example (a : Prop) : a ∨ true :=
 begin
-  refine_list, exact or.inr trivial,
+  (do s ← refine_list, guard $ s.head = "exact or.inr trivial"), exact or.inr trivial,
 end
 
 example {a a' : ℕ} (h : a == a') : a = a' :=
 begin
-  refine_list, exact eq_of_heq h,
+  (do s ← refine_list, guard $ s.head = "exact eq_of_heq h"), exact eq_of_heq h,
 end
 example {a b c : ℤ} (h₁ : a = b) (h₂ : b = c) : a = c :=
 begin
-  refine_list, exact eq.trans h₁ h₂,
+  (do s ← refine_list, guard $ "exact eq.trans h₁ h₂" ∈ s), exact eq.trans h₁ h₂,
 end
 
 example (n : nat) : n + 0 = n :=
 begin
-  refine_list, exact rfl,
+  (do s ← refine_list, guard $ s.head = "exact rfl"), exact rfl,
 end
 
 example (n : nat) : n < n + 1 :=
 begin
-  refine_list,
+  (do s ← refine_list, guard $ s.head = "exact nat.lt.base n"),
   exact nat.lt.base n
 end
 example (n : nat) : n < n + 2 :=
 begin
-  refine_list,
+  (do s ← refine_list, guard $ "refine nat.lt.step _" ∈ s),
   refine nat.lt.step _, -- this wasn't the first result; humans still necessary :-(
-  refine_list,
+  (do s ← refine_list, guard $ s.head = "exact nat.lt.base n"),
   exact nat.lt.base n
 end
 
 example (a b : Prop) : (a ∨ true) ∧ (b ∨ true) :=
 begin
-  refine_list,
+  (do s ← refine_list, guard $ "refine ⟨_, _⟩" ∈ s),
   refine ⟨_, _⟩, -- wasn't the first result, because it created two new goals
-  refine_list,
+  (do s ← refine_list, guard $ s.head = "exact or.inr trivial"),
   exact or.inr trivial,
-  refine_list,
+  (do s ← refine_list, guard $ s.head = "exact or.inr trivial"),
   exact or.inr trivial,
 end
 
 example (A B : Prop) (a : A) (b : unit → B) : A ∧ B :=
 begin
-  refine_list,
+  (do s ← refine_list, guard $ "refine ⟨a, _⟩" ∈ s),
   refine ⟨a, _⟩,
   replace b := b (),
-  refine_list,
+  (do s ← refine_list, guard $ s.head = "exact b"),
   exact b,
 end

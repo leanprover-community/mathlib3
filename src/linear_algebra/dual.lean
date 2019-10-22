d1962961427abd691ea7e943071cc885e0a5009e
Rob Lewis <Rob.y.lewis@gmail.com>
2019-10-22 09:06:30 +0200

    undo removed whitespace

--- src/linear_algebra/dual.lean
+++ src/linear_algebra/dual.lean
@@ -3,6 +3,7 @@ Copyright (c) 2019 Johan Commelin. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Johan Commelin, Fabian Glöckle
 -/
+
 import linear_algebra.tensor_product
 import linear_algebra.finite_dimensional
 import tactic.apply_fun

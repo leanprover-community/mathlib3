be47ad323a1171c540c9a23889679694a7184adf
Scott Morrison <scott.morrison@anu.edu.au>
2019-10-23 19:13:18 +1100

    resuming work on nat_cases

--- test/nat_cases.lean
+++ test/nat_cases.lean
@@ -1,7 +1,7 @@
 /-
-Copyright (c) 2019 . All rights reserved.
+Copyright (c) 2019 Scott Morrison. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
-Authors:
+Authors: Scott Morrison
 -/
 
 import tactic.nat_cases

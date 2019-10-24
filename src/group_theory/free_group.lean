d7101ece78e6e792def96106c6a8df6be8ff0ad9
Rob Lewis <rob.y.lewis@gmail.com>
2019-10-24 13:25:51 +0200

    chore(group_theory/free_group): rename sum.sum

--- src/group_theory/free_group.lean
+++ src/group_theory/free_group.lean
@@ -561,7 +561,7 @@ instance sum.is_group_hom : is_group_hom (@sum α _) :=
 prod.is_group_hom
 
 -- TODO(lint): This should probably be renamed `sum.mul`
-@[simp, nolint] lemma sum.sum : sum (x * y) = sum x + sum y :=
+@[simp, nolint] lemma sum.mul : sum (x * y) = sum x + sum y :=
 prod.mul
 
 @[simp] lemma sum.one : sum (1:free_group α) = 0 :=

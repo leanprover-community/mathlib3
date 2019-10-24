4b1509e531063fd87da280936f1e4c7c467bfaee
Scott Morrison <scott@tqft.net>
2019-10-24 11:43:27 +1100

    finishing fintypes

--- src/data/set/intervals/fintype.lean
+++ src/data/set/intervals/fintype.lean
@@ -6,7 +6,7 @@ Authors: Scott Morrison
 import data.set.intervals
 import data.fintype
 
-open set
+namespace set
 
 local attribute [simp] list.mem_range
 
@@ -19,8 +19,24 @@ fintype.of_list
   (λ x, begin
           simp only [list.mem_map, list.mem_attach, true_and, subtype.exists],
           exact ⟨x, mem_Ico_iff_mem_list_Ico.mp x.2, subtype.ext.2 rfl⟩,
-        end )
+        end)
 
 instance Ico_ℤ_fintype (l u : ℤ) : fintype (Ico l u) :=
-let ls : list (Ico l u) := (list.range (u - l).to_nat).attach.map (λ n, ⟨(n : ℤ) + l, begin cases n, simp, simp at n_property, split, library_search,  end) in
-fintype.mk ls.to_finset sorry
+let ls : list (Ico l u) :=
+(list.range (u - l).to_nat).attach.map (λ n,
+  ⟨(n : ℤ) + l,
+   begin
+     cases n,
+     simp only [add_comm, le_add_iff_nonneg_right, mem_Ico],
+     simp only [list.mem_range, int.lt_to_nat] at n_property,
+     exact ⟨trivial, lt_sub_iff_add_lt'.mp n_property⟩,
+   end⟩) in
+fintype.mk ls.to_finset
+begin
+  rintro ⟨x,⟨hl,hu⟩⟩,
+  simp only [list.mem_map, list.mem_to_finset, list.mem_attach,
+    subtype.mk_eq_mk, subtype.exists, subtype.coe_mk, coe_coe],
+  exact ⟨(x - l).to_nat, by simpa [hl], by simp [hl]⟩,
+end
+
+end set

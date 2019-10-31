0d12b236b0201468891234fca13c907803b430cd
Rob Lewis <Rob.y.lewis@gmail.com>
2019-10-31 13:36:44 +0100

    add copyright header

--- src/tactic/elide.lean
+++ src/tactic/elide.lean
@@ -1,3 +1,9 @@
+/-
+Copyright (c) 2018 Mario Carneiro. All rights reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
+Authors: Mario Carneiro, Simon Hudon
+-/
+
 import logic.basic tactic.core
 
 namespace tactic

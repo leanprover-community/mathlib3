09db9d2d90c38317fef528ce290d7a6bfcdf3294
Joe <zhouhanz@andrew.cmu.edu>
2019-11-06 13:18:41 -0500

    Use characteristic properties of an extended function, instead of the explicit construction

--- src/topology/algebra/uniform_extension.lean
+++ src/topology/algebra/uniform_extension.lean
@@ -24,9 +24,14 @@ This theorem is proved in `topology.uniform_space.uniform_embedding.lean`. See t
 
 In this file we show that
 
-* If `f` is an `add_monoid_hom`, then `ψ` is an `add_monoid_hom`
-* If `f` is a `continuous_linear_map`, them `ψ` is a `continuous_linear_map`
-* If `∥f∥ ≤ M`, then `∥ψ∥ ≤ M`
+* If `f` is an `add_monoid_hom`, then `ψ` is also an `add_monoid_hom`.
+* If `f` is a `continuous_linear_map`, them `ψ` is also a `continuous_linear_map`.
+* If `∥f∥ ≤ M`, then `∥ψ∥ ≤ M`.
+
+## Main definitions
+
+* For `f : β →+ γ`, `f.extend _ _ _ _ : α →+ γ` is the extension of `f`.
+* Fof `f : α →L[K] γ`, `f.extend _ _ _ _ : α →L[K] γ` is the extension of `f`.
 
 ## Implementation notes
 
@@ -46,55 +51,28 @@ open_locale classical
 
 variables {α : Type*} {β : Type*} {γ : Type*}
 
-section uniformly_extend_zero
-
-variables [uniform_space α] [has_zero α]
-variables [uniform_space β] [has_zero β]
-variables [uniform_space γ] [has_zero γ] [separated γ]
-variables (e : β → α) (e_zero : e 0 = 0) (h_e : uniform_inducing e) (h_dense : dense_range e)
-variables {f : β → γ} (h_f : uniform_continuous f)
-
-include e_zero h_f
-
-local notation `ψ` := (h_e.dense_inducing h_dense).extend f
-
-lemma uniformly_extend_zero (h : f 0 = 0) : ψ 0 = 0 :=
-by { rw ← e_zero, rwa uniformly_extend_of_ind h_e h_dense h_f }
-
-end uniformly_extend_zero
-
 section uniformly_extend_add
 
-variables [uniform_space α] [add_monoid α] [topological_add_monoid α]
-variables [uniform_space β] [add_monoid β]
-variables [uniform_space γ] [add_monoid γ] [topological_add_monoid γ] [separated γ]
-          [complete_space γ]
-variables (e : β → α) (e_add : ∀b₁ b₂, e (b₁ + b₂) = e b₁ + e b₂) (h_e : uniform_inducing e)
-          (h_dense : dense_range e)
-variables {f : β → γ} (h_f : uniform_continuous f)
+variables [topological_space α] [add_monoid α] [topological_add_monoid α]
+variables [topological_space β] [has_add β]
+variables [topological_space γ] [add_monoid γ] [topological_add_monoid γ] [t2_space γ]
+variables (e : β → α) (e_add : ∀b₁ b₂, e (b₁ + b₂) = e b₁ + e b₂) (h_e : dense_inducing e)
+variables {f : β → γ} (f_add : ∀ b₁ b₂, f (b₁ + b₂) = f b₁ + f b₂)
+variables {ψ : α → γ} (ψ_cont : continuous ψ) (h_psi : ∀b, ψ (e b) = f b)
 
-include e_add h_f
+include e_add f_add h_psi
 
-local notation `ψ` := (h_e.dense_inducing h_dense).extend f
-
-lemma uniformly_extend_add (h : ∀ b₁ b₂, f (b₁ + b₂) = f b₁ + f b₂) :
-  ∀ a₁ a₂, ψ (a₁ + a₂) = ψ a₁ + ψ a₂ :=
-have h₁ : continuous ψ,
-begin
-  apply uniform_continuous.continuous,
-  apply uniform_continuous_uniformly_extend,
-  exact h_f
-end,
-have h₂ : ∀ b₁ b₂, ψ (e b₁ + e b₂) = ψ (e b₁) + ψ (e b₂),
+lemma uniformly_extend_add : ∀ a₁ a₂, ψ (a₁ + a₂) = ψ a₁ + ψ a₂ :=
+have h : ∀ b₁ b₂, ψ (e b₁ + e b₂) = ψ (e b₁) + ψ (e b₂),
 begin
   assume b₁ b₂, rw ← e_add,
-  simp only [uniformly_extend_of_ind h_e h_dense h_f],
-  apply h
+  simp only [h_psi],
+  apply f_add
 end,
-is_closed_property2 (h_e.dense_inducing h_dense)
-  (is_closed_eq (h₁.comp (continuous_add continuous_fst continuous_snd))
-          (continuous_add (h₁.comp continuous_fst) (h₁.comp continuous_snd)))
-  h₂
+is_closed_property2 h_e
+  (is_closed_eq (ψ_cont.comp (continuous_add continuous_fst continuous_snd))
+          (continuous_add (ψ_cont.comp continuous_fst) (ψ_cont.comp continuous_snd)))
+  h
 
 end uniformly_extend_add
 
@@ -104,19 +82,25 @@ variables [uniform_space α] [add_monoid α] [topological_add_monoid α]
 variables [uniform_space β] [add_monoid β]
 variables [uniform_space γ] [add_monoid γ] [topological_add_monoid γ] [separated γ]
           [complete_space γ]
-variables (e : add_monoid_hom β α) (h_e : uniform_inducing e)
-          (h_dense : dense_range e)
-variables {f : add_monoid_hom β γ} (h_f : uniform_continuous f)
+variables (f : β →+ γ) (h_f : uniform_continuous f)
+variables (e : β →+ α) (h_e : uniform_inducing e) (h_dense : dense_range e)
 
 include h_f
 
-/-- Extension of a uniform continuous `add_monoid_hom` -/
-def extend : add_monoid_hom α γ :=
+/-- If `f : β →+ γ` is a uniform continuous `add_monoid_hom` defined on a dense subset of `α`,
+    then `f.extend : α →+ γ` is a uniform continuous extension of `f` that is also an
+    `add_monoid_hom`. -/
+protected def extend : add_monoid_hom α γ :=
 { to_fun := (h_e.dense_inducing h_dense).extend f,
-  map_zero' := uniformly_extend_zero e e.map_zero h_e h_dense h_f f.map_zero ,
-  map_add' := uniformly_extend_add e e.map_add h_e h_dense h_f f.map_add }
+  map_zero' := by simp only [e.map_zero.symm, uniformly_extend_of_ind h_e h_dense h_f, f.map_zero],
+  map_add' := uniformly_extend_add e
+    e.map_add
+    (h_e.dense_inducing h_dense)
+    f.map_add
+    (uniform_continuous_uniformly_extend h_e h_dense h_f).continuous
+    (uniformly_extend_of_ind h_e h_dense h_f) }
 
-local notation `ψ` := extend e h_e h_dense h_f
+local notation `ψ` := f.extend h_f e h_e h_dense
 
 lemma uniform_continuous_extend : uniform_continuous ψ :=
 uniform_continuous_uniformly_extend h_e h_dense h_f
@@ -126,39 +110,73 @@ end add_monoid_hom
 section uniformly_extend_smul
 
 variables {K : Type*} [semiring K] [topological_space K]
-variables [uniform_space α] [add_comm_group α] [semimodule K α]
+variables [topological_space α] [add_comm_group α] [semimodule K α]
           [topological_semimodule K α]
-variables [uniform_space β] [add_comm_group β] [semimodule K β]
-variables [uniform_space γ] [add_comm_group γ] [semimodule K γ]
-          [topological_semimodule K γ] [separated γ] [complete_space γ]
-variables (e : β → α) (e_smul : ∀(k:K) b, e (k • b) = k • (e b)) (h_e : uniform_inducing e)
-          (h_dense : dense_range e)
-variables {f : β → γ} (h_f : uniform_continuous f)
+variables [topological_space β] [add_comm_group β] [semimodule K β]
+variables [topological_space γ] [add_comm_group γ] [semimodule K γ]
+          [topological_semimodule K γ] [t2_space γ]
+variables (e : β → α) (e_smul : ∀(k:K) b, e (k • b) = k • (e b)) (h_e : dense_inducing e)
+variables {f : β → γ} (f_smul : ∀ (k:K) b, f (k • b) = k • (f b))
+variables {ψ : α → γ} (ψ_cont : continuous ψ) (h_psi : ∀b, ψ (e b) = f b)
 
-include e_smul h_f
+include e_smul f_smul h_psi
 
-local notation `ψ` := (h_e.dense_inducing h_dense).extend f
-
-lemma uniformly_extend_smul (h : ∀ (k:K) b, f (k • b) = k • (f b)) :
+lemma uniformly_extend_smul  :
   ∀ (k:K) a, ψ (k • a) = k • (ψ a) :=
-have h₁ : continuous ψ,
+have h : ∀ (k:K) b, ψ (k • e b) = k • ψ (e b),
 begin
-  apply uniform_continuous.continuous,
-  apply uniform_continuous_uniformly_extend,
-  exact h_f
+  assume k b,
+  rw [← e_smul, h_psi, h_psi],
+  apply f_smul
 end,
-have h₂ : ∀ (k:K) b, ψ (k • e b) = k • ψ (e b),
-begin
-  assume k b, rw ← e_smul,
-  simp only [uniformly_extend_of_ind h_e h_dense h_f],
-  apply h
-end,
-λk, is_closed_property (h_e.dense_inducing h_dense).closure_range
-  (is_closed_eq (h₁.comp (continuous_smul (continuous_const) continuous_id))
-                (continuous.comp (continuous_smul (continuous_const) continuous_id) h₁))
-  (h₂ k)
+λk, is_closed_property h_e.closure_range
+  (is_closed_eq (ψ_cont.comp (continuous_smul (continuous_const) continuous_id))
+                (continuous.comp (continuous_smul (continuous_const) continuous_id) ψ_cont))
+  (h k)
 
 end uniformly_extend_smul
+
+section uniformly_extend_norm
+
+set_option class.instance_max_depth 70
+
+variables {K : Type*} [nondiscrete_normed_field K]
+variables [normed_group α] [normed_space K α]
+variables [normed_group β] [normed_space K β]
+variables [normed_group γ] [normed_space K γ]
+variables {e : β →L[K] α} (h_e : dense_inducing e)
+variables {f : β →L[K] γ}
+variables {ψ : α →L[K] γ} (h_psi : ∀b, ψ (e b) = f b)
+
+include h_e h_psi
+
+lemma uniformly_extend_norm' {M N : ℝ} (N0 : 0 ≤ N) (he : ∀b, ∥b∥ ≤ N * ∥e b∥) (hf : ∥f∥ ≤ M) :
+  ∥ψ∥ ≤ M * N :=
+have M0 : 0 ≤ M := le_trans (norm_nonneg _) hf,
+have MN0 : 0 ≤ M * N := mul_nonneg M0 N0,
+have h : ∀b, ∥ψ (e b)∥ ≤ M * N * ∥e b∥,
+begin
+  assume b,
+  have h : ∥f b∥ ≤ M * ∥b∥ :=
+    calc ∥f b∥ ≤ ∥f∥ * ∥b∥ : f.le_op_norm b
+      ... ≤ M * ∥b∥ : mul_le_mul hf (le_refl _) (norm_nonneg _) M0,
+  rw [h_psi, mul_assoc],
+  exact le_trans h (mul_le_mul (le_refl _) (he b) (norm_nonneg _) M0)
+end,
+ψ.op_norm_le_bound MN0
+  $ @is_closed_property β α _ e (λa, ∥ψ a∥ ≤ M * N * ∥a∥) h_e.closure_range
+    (is_closed_le (continuous_norm.comp ψ.cont) (continuous_mul continuous_const continuous_norm))
+    h
+
+lemma uniformly_extend_norm {M : ℝ} (he : ∀b, ∥b∥ = ∥e b∥) (hf : ∥f∥ ≤ M) :
+  ∥ψ∥ ≤ M :=
+begin
+  have := uniformly_extend_norm' h_e h_psi zero_le_one
+    (λb, by { rw one_mul, exact le_of_eq (he b) }) hf,
+  rwa mul_one at this,
+end
+
+end uniformly_extend_norm
 
 namespace continuous_linear_map
 
@@ -170,19 +188,31 @@ variables [uniform_space α] [add_comm_group α] [module K α] [topological_add_
 variables [uniform_space β] [add_comm_group β] [module K β]
 variables [uniform_space γ] [add_comm_group γ] [module K γ] [topological_add_group γ]
           [topological_semimodule K γ] [separated γ] [complete_space γ]
-variables (e : β →L[K] α) (h_e : uniform_inducing e) (h_dense : dense_range e)
 variables (f : β →L[K] γ) (h_f : uniform_continuous f)
+variables (e : β →L[K] α) (h_e : uniform_inducing e) (h_dense : dense_range e)
 
 include h_f
 
-/-- Extension of a uniform continuous `continuous_linear_map`. -/
+/-- If `f : β →L[K] γ` is a uniform continuous `continuous_linear_map` defined on a dense subset of
+    `α`, then `f.extend : α →L[K] γ` is a uniform continuous extension of `f` that is also a
+    `continuous_linear_map`. -/
 def extend : α →L[K] γ :=
 { to_fun := (h_e.dense_inducing h_dense).extend f,
-  add := uniformly_extend_add e e.map_add h_e h_dense h_f f.map_add,
-  smul := uniformly_extend_smul e (λk, map_smul k e) h_e h_dense h_f $ λ k b, map_smul _ _ _,
+  add := uniformly_extend_add e
+    e.map_add
+    (h_e.dense_inducing h_dense)
+    f.map_add
+    (uniform_continuous_uniformly_extend h_e h_dense h_f).continuous
+    (uniformly_extend_of_ind h_e h_dense h_f),
+  smul := uniformly_extend_smul e
+    (λk b, map_smul _ _ _)
+    (h_e.dense_inducing h_dense)
+    (λk b, map_smul _ _ _)
+    (uniform_continuous_uniformly_extend h_e h_dense h_f).continuous
+    (uniformly_extend_of_ind h_e h_dense h_f)  ,
   cont := uniform_continuous.continuous $ uniform_continuous_uniformly_extend _ _ h_f }
 
-local notation `ψ` := extend e h_e h_dense f h_f
+local notation `ψ` := f.extend h_f e h_e h_dense
 
 lemma uniform_continuous_extend : uniform_continuous ψ :=
 uniform_continuous_uniformly_extend h_e h_dense h_f
@@ -197,44 +227,18 @@ variables {K : Type*} [nondiscrete_normed_field K]
 variables [normed_group α] [normed_space K α]
 variables [normed_group β] [normed_space K β]
 variables [normed_group γ] [normed_space K γ] [complete_space γ]
-variables (e : β →L[K] α) (h_e : uniform_inducing e) (h_dense : dense_range e)
 variables (f : β →L[K] γ) (h_f : uniform_continuous f)
+variables (e : β →L[K] α) (h_e : uniform_inducing e) (h_dense : dense_range e)
 
-local notation `ψ` := extend e h_e h_dense f h_f
+local notation `ψ` := f.extend h_f e h_e h_dense
 
 lemma norm_uniformly_extend_le' {M N : ℝ} (N0 : 0 ≤ N) (he : ∀b, ∥b∥ ≤ N * ∥e b∥) (hf : ∥f∥ ≤ M) :
   ∥ψ∥ ≤ M * N :=
-have M0 : 0 ≤ M := le_trans (norm_nonneg _) hf,
-have MN0 : 0 ≤ M * N := mul_nonneg M0 N0,
-have h₁ : continuous ψ,
-begin
-  apply uniform_continuous.continuous,
-  apply uniform_continuous_uniformly_extend,
-  exact h_e, exact h_dense, exact h_f
-end,
-have h₂ : ∀b, ∥ψ (e b)∥ ≤ M * N * ∥e b∥,
-begin
-  assume b,
-  have h : ∥f b∥ ≤ M * ∥b∥ :=
-    calc ∥f b∥ ≤ ∥f∥ * ∥b∥ : le_op_norm f b
-      ... ≤ M * ∥b∥ : mul_le_mul hf (le_refl _) (norm_nonneg _) M0,
-  have : ψ (e b) = f b := uniformly_extend_of_ind h_e h_dense h_f b,
-  rw [this, mul_assoc],
-  exact le_trans h (mul_le_mul (le_refl _) (he b) (norm_nonneg _) M0)
-end,
-op_norm_le_bound _ MN0
-  $ @is_closed_property β α _ e (λa, ∥ψ a∥ ≤ M * N * ∥a∥) (h_e.dense_inducing h_dense).closure_range
-      (is_closed_le (continuous_norm.comp h₁) (continuous_mul continuous_const continuous_norm))
-      h₂
+uniformly_extend_norm' (h_e.dense_inducing h_dense) (uniformly_extend_of_ind h_e h_dense h_f)
+  N0 he hf
 
-/-- Special case when the map `e` is the inclusion of `β` into `α`. -/
-lemma norm_uniformly_extend_le {M : ℝ} (he : ∀b, ∥b∥ = ∥e b∥) (hf : ∥f∥ ≤ M) :
-  ∥ψ∥ ≤ M :=
-begin
-  have := norm_uniformly_extend_le' e h_e h_dense f h_f zero_le_one
-    (λb, by { rw one_mul, exact le_of_eq (he b) }) hf,
-  rwa mul_one at this,
-end
+lemma norm_uniformly_extend_le {M : ℝ} (he : ∀b, ∥b∥ = ∥e b∥) (hf : ∥f∥ ≤ M) : ∥ψ∥ ≤ M :=
+uniformly_extend_norm (h_e.dense_inducing h_dense) (uniformly_extend_of_ind h_e h_dense h_f) he hf
 
 end operator_norm
 

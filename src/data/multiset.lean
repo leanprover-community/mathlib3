5e57094d475ed1798f2762b35f2ea61efe772106
Chris Hughes <chrishughes24@gmail.com>
2019-11-12 21:33:28 +0000

    feat(data/multiset): dvd_sum

--- src/data/multiset.lean
+++ src/data/multiset.lean
@@ -812,6 +812,9 @@ lemma abs_sum_le_sum_abs [discrete_linear_ordered_field α] {s : multiset α} :
   abs s.sum ≤ (s.map abs).sum :=
 le_sum_of_subadditive _ abs_zero abs_add s
 
+theorem dvd_sum [comm_semiring α] {{a : α} {s : multiset α} (h : ∀ x ∈ s, a ∣ x) : a ∣ s.sum :=
+list.dvd_sum h
+
 /- join -/
 
 /-- `join S`, where `S` is a multiset of multisets, is the lift of the list join

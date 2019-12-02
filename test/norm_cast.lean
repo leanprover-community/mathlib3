e3c50b2b121817a6ff371dd716fa0abd976241a1
Paul-Nicolas Madelaine <paul-nicolas.madelaine@ens.fr>
2019-12-02 16:57:34 +0100

    update tests

--- test/norm_cast.lean
+++ test/norm_cast.lean
@@ -9,53 +9,52 @@ import data.zmod.basic
 constants (an bn cn dn : ℕ) (az bz cz dz : ℤ) (aq bq cq dq : ℚ)
 constants (ar br cr dr : ℝ) (ac bc cc dc : ℂ)
 
-example : (an : ℤ) = bn → an = bn := λ h, by exact_mod_cast h -- by simp
-example : an = bn → (an : ℤ) = bn := λ h, by exact_mod_cast h -- by simp
-example : az = bz ↔ (az : ℚ) = bz := by norm_cast -- by simp
+example : (an : ℤ) = bn → an = bn := λ h, by exact_mod_cast h
+example : an = bn → (an : ℤ) = bn := λ h, by exact_mod_cast h
+example : az = bz ↔ (az : ℚ) = bz := by norm_cast
 
 example : (aq : ℝ) = br ↔ (aq : ℂ) = br := by norm_cast
 example : (an : ℚ) = bz ↔ (an : ℂ) = bz := by norm_cast
 example : (((an : ℤ) : ℚ) : ℝ) = bq ↔ ((an : ℚ) : ℂ) = (bq : ℝ) :=
 by norm_cast
 
-example : (an : ℤ) < bn ↔ an < bn             := by norm_cast -- by simp
+example : (an : ℤ) < bn ↔ an < bn             := by norm_cast
 example : (an : ℚ) < bz ↔ (an : ℝ) < bz       := by norm_cast
 example : ((an : ℤ) : ℝ) < bq ↔ (an : ℚ) < bq := by norm_cast
-example : (an : ℤ) ≠ (bn : ℤ) ↔ an ≠ bn := by norm_cast -- by simp
+example : (an : ℤ) ≠ (bn : ℤ) ↔ an ≠ bn := by norm_cast
 
 -- zero and one cause special problems
-example : 0 < (bq : ℝ) ↔ 0 < bq := by norm_cast -- by simp
-example : az > (1 : ℕ) ↔ az > 1 := by norm_cast -- by simp
-example : az > (0 : ℕ) ↔ az > 0 := by norm_cast -- by simp
-example : (an : ℤ) ≠ 0 ↔ an ≠ 0 := by norm_cast -- by simp
+example : 0 < (bq : ℝ) ↔ 0 < bq := by norm_cast
+example : az > (1 : ℕ) ↔ az > 1 := by norm_cast
+example : az > (0 : ℕ) ↔ az > 0 := by norm_cast
+example : (an : ℤ) ≠ 0 ↔ an ≠ 0 := by norm_cast
 example : aq < (1 : ℕ) ↔ (aq : ℝ) < (1 : ℤ) := by norm_cast
 
-example : (an : ℤ) + bn = (an + bn : ℕ)   := by norm_cast -- by simp
-example : (an : ℂ) + bq = ((an + bq) : ℚ) := by norm_cast -- by simp
-example : (((an : ℤ) : ℚ) : ℝ) + bn = (an + (bn : ℤ)) := by norm_cast -- by simp
+example : (an : ℤ) + bn = (an + bn : ℕ)   := by norm_cast
+example : (an : ℂ) + bq = ((an + bq) : ℚ) := by norm_cast
+example : (((an : ℤ) : ℚ) : ℝ) + bn = (an + (bn : ℤ)) := by norm_cast
 
 example : (((((an : ℚ) : ℝ) * bq) + (cq : ℝ) ^ dn) : ℂ) = (an : ℂ) * (bq : ℝ) + cq ^ dn :=
-by norm_cast -- by simp
+by norm_cast
 example : ((an : ℤ) : ℝ) < bq ∧ (cr : ℂ) ^ 2 = dz ↔ (an : ℚ) < bq ∧ ((cr ^ 2) : ℂ) = dz :=
 by norm_cast
 
-example : (an : ℤ) = 1 → an = 1 := λ h, by exact_mod_cast h
-example : (an : ℤ) < 5 → an < 5 := λ h, by exact_mod_cast h
-example : an < 5 → (an : ℤ) < 5 := λ h, by exact_mod_cast h
-example : (an + 5) < 10 → (an : ℤ) + 5 < 10 := λ h, by exact_mod_cast h
-example : (an : ℤ) + 5 < 10 → (an + 5) < 10 := λ h, by exact_mod_cast h
-example : ((an + 5 : ℕ) : ℤ) < 10 → an + 5 < 10 := λ h, by exact_mod_cast h
-example : an + 5 < 10 → ((an + 5 : ℕ) : ℤ) < 10 := λ h, by exact_mod_cast h
-example : (az : ℚ) = 7 → az = 7 := λ h, by exact_mod_cast h
+--testing numerals
+example : ((42 : ℕ) : ℤ) = 42 := by norm_cast
+example : ((42 : ℕ) : ℂ) = 42 := by norm_cast
+example : ((42 : ℤ) : ℚ) = 42 := by norm_cast
+example : ((42 : ℚ) : ℝ) = 42 := by norm_cast
 
+example (h : (an : ℝ) = 0) : an = 0 := by exact_mod_cast h
+example (h : (an : ℝ) = 42) : an = 42 := by exact_mod_cast h
+example (h : (an + 42) ≠ 42) : (an : ℝ) + 42 ≠ 42 := by exact_mod_cast h
+
+-- testing the heuristic
 example (h : bn ≤ an) : an - bn = 1 ↔ (an - bn : ℤ) = 1 :=
 by norm_cast
 example (h : (cz : ℚ) = az / bz) : (cz : ℝ) = az / bz :=
 by assumption_mod_cast
 
-example : ((42 : ℕ) : ℤ) = 42 := by norm_cast
-example : ((42 : ℕ) : ℚ) = 42 := by norm_cast
-example : ((42 : ℕ) : ℝ) = 42 := by norm_cast
 
 namespace hidden
 
@@ -83,8 +82,7 @@ option.some_inj
 @[move_cast] lemma mul_coe {α : Type*} [has_mul α] (a b : α) :
   ((a * b : α) : with_zero α) = (a : with_zero α) * b := rfl
 
-example [has_mul α] [has_one α] (x y : α) (h : (x : with_zero α) * y = 1) :
-  x*y = 1 :=
+example [has_mul α] [has_one α] (x y : α) (h : (x : with_zero α) * y = 1) : x*y = 1 :=
 by exact_mod_cast h
 
 end hidden

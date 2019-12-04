17a6fdca2de1beca073cb848111049ce6d9b51f6
Jean Lo <19224446+jlpaca@users.noreply.github.com>
2019-12-04 21:29:24 +0000

    style(analysis/convex): place lemmas at reasonable locations

--- src/analysis/convex.lean
+++ src/analysis/convex.lean
@@ -22,6 +22,8 @@ variables {α : Type*} {β : Type*} {ι : Sort _}
   [add_comm_group α] [vector_space ℝ α] [add_comm_group β] [vector_space ℝ β]
   (A : set α) (B : set α) (x : α)
 
+local attribute [instance] set.pointwise_add
+
 /-- Convexity of sets -/
 def convex (A : set α) :=
 ∀ (x y : α) (a b : ℝ), x ∈ A → y ∈ A → 0 ≤ a → 0 ≤ b → a + b = 1 →
@@ -43,6 +45,15 @@ begin
   rw hb',
   exact h hx hy ha ha'
 end⟩
+
+/-- Alternative definition of set convexity, in terms of pointwise set operations. -/
+lemma convex_iff':
+  convex A ↔ ∀ {a b : ℝ}, 0 ≤ a → 0 ≤ b → a + b = 1 →
+    ((λ x, a • x) '' A) + ((λ x, b • x) '' A) ⊆ A :=
+⟨λ hA a b ha hb hab w ⟨au, ⟨u, hu, hau⟩, bv, ⟨v, hv, hbv⟩, hw⟩,
+  by { rw [←hau, ←hbv] at hw; rw hw; exact hA _ _ _ _ hu hv ha hb hab },
+ λ h x y a b hx hy ha hb hab,
+  (h ha hb hab) (set.add_mem_pointwise_add ⟨_, hx, rfl⟩ ⟨_, hy, rfl⟩)⟩
 
 /-- Another alternative definition of set convexity -/
 lemma convex_iff_div:
@@ -748,6 +759,43 @@ calc
   ... ≤ max (f x) (f y) : by rw [←add_mul, hab, one_mul]
 
 end vector_space
+
+section topological_vector_space
+
+variables {α : Type*} [add_comm_group α] [vector_space ℝ α]
+[topological_space α] [topological_add_group α] [topological_vector_space ℝ α]
+
+local attribute [instance] set.pointwise_add
+
+open set
+
+/-- In a topological vector space, the interior of a convex set is convex. -/
+lemma convex_interior {A : set α} (hA : convex A) : convex (interior A) :=
+(convex_iff' _).mpr $ λ a b ha hb hab,
+  have h : is_open ((λ x, a • x) '' interior A + (λ x, b • x) '' interior A), from
+  or.elim (classical.em (a = 0))
+  (λ heq,
+    have hne : b ≠ 0, from by { rw [heq, zero_add] at hab, rw hab, exact one_ne_zero },
+    is_open_pointwise_add_left ((is_open_map_smul_of_ne_zero hne _) is_open_interior))
+  (λ hne,
+    is_open_pointwise_add_right ((is_open_map_smul_of_ne_zero hne _) is_open_interior)),
+  (subset_interior_iff_subset_of_open h).mpr $ subset.trans
+    (by { apply pointwise_add_subset_add; exact image_subset _ interior_subset })
+    ((convex_iff' _).mp hA ha hb hab)
+
+/-- In a topological vector space, the closure of a convex set is convex. -/
+lemma convex_closure {A : set α} (hA : convex A) : convex (closure A) :=
+λ x y a b hx hy ha hb hab,
+let f : α → α → α := λ x' y', a • x' + b • y' in
+have hf : continuous ((λ p : α × α, p.fst + p.snd) ∘ (λ p : α × α, (a • p.fst, b • p.snd))), from
+  continuous.comp continuous_add (continuous.prod_mk
+    (continuous_smul continuous_const continuous_fst)
+    (continuous_smul continuous_const continuous_snd)),
+show f x y ∈ closure A, from
+  mem_closure_of_continuous2 hf hx hy (λ x' hx' y' hy', subset_closure
+  (hA _ _ _ _ hx' hy' ha hb hab))
+
+end topological_vector_space
 
 section normed_space
 variables {α : Type*} [normed_group α] [normed_space ℝ α]

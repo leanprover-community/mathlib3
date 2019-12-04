7594ac2c017d886174be4cb0aaa1c5b478497f16
Jean Lo <19224446+jlpaca@users.noreply.github.com>
2019-12-04 20:24:33 +0000

    feat(analysis/convex): extract lemma

--- src/analysis/convex_vs.lean
+++ src/analysis/convex_vs.lean
@@ -1,6 +1,25 @@
 import analysis.convex
 import topology.algebra.module
 
+section vector_space
+variables {α : Type*} {β : Type*} {ι : Sort _}
+  [add_comm_group α] [vector_space ℝ α] [add_comm_group β] [vector_space ℝ β]
+  (A : set α) (B : set α) (x : α)
+
+local attribute [instance] set.pointwise_add
+
+lemma convex_iff':
+convex A ↔ ∀ {a b : ℝ}, 0 ≤ a → 0 ≤ b → a + b = 1 →
+  ((λ x, a • x) '' A) + ((λ x, b • x) '' A) ⊆ A :=
+⟨λ hA a b ha hb hab w ⟨au, ⟨u, hu, hau⟩, bv, ⟨v, hv, hbv⟩, hw⟩,
+  by { rw [←hau, ←hbv] at hw; rw hw; exact hA _ _ _ _ hu hv ha hb hab },
+ λ h x y a b hx hy ha hb hab,
+  (h ha hb hab) (set.add_mem_pointwise_add ⟨_, hx, rfl⟩ ⟨_, hy, rfl⟩)⟩
+
+end vector_space
+
+
+
 section topological_vector_space
 
 variables {α : Type*} [add_comm_group α] [vector_space ℝ α]
@@ -8,27 +27,29 @@ variables {α : Type*} [add_comm_group α] [vector_space ℝ α]
 
 local attribute [instance] set.pointwise_add
 
+open set
+
 /-- In a topological vector space, the interior of a convex set is convex. -/
 lemma convex_interior {A : set α} (hA : convex A) : convex (interior A) :=
-λ x y a b hx hy ha hb hab,
-let aA := (λ x, a • x) '' (interior A) in
-let bA := (λ x, b • x) '' (interior A) in
-or.elim (eq_or_lt_of_le ha)
-(λ heq,
-  have hb₁: b = 1, by rwa [←heq, zero_add] at hab,
-  by rwa [←heq, zero_smul, zero_add, hb₁, one_smul])
-(λ hlt, 
-  have haA : is_open aA, from
-    (is_open_map_smul_of_ne_zero (ne_of_gt hlt) _) is_open_interior,
-  mem_interior.2 $ exists.intro (aA + bA)
-  ⟨λ w ⟨ax', ⟨x', hx', hax'⟩, by', ⟨y', hy', hby'⟩, hw⟩,
-  by { rw [←hax', ←hby'] at hw, rw hw, exact hA
-    _ _ _ _ (interior_subset hx') (interior_subset hy') ha hb hab },
-  is_open_pointwise_add_right haA,
-  ⟨_, ⟨_, hx, rfl⟩, _, ⟨_, hy, rfl⟩, rfl⟩⟩)
+(convex_iff' _).mpr $ λ a b ha hb hab,
+  have h : is_open ((λ x, a • x) '' interior A + (λ x, b • x) '' interior A), from
+  or.elim (classical.em (a = 0))
+  (λ heq,
+    have hne : b ≠ 0, from by { rw [heq, zero_add] at hab, rw hab, exact one_ne_zero },
+    is_open_pointwise_add_left ((is_open_map_smul_of_ne_zero hne _) is_open_interior))
+  (λ hne,
+    is_open_pointwise_add_right ((is_open_map_smul_of_ne_zero hne _) is_open_interior)),
+  (subset_interior_iff_subset_of_open h).mpr $ subset.trans
+    (by { apply pointwise_add_subset_add; exact image_subset _ interior_subset })
+    ((convex_iff' _).mp hA ha hb hab)
+
+#check mem_closure_of_continuous2
 
 /-- In a topological vector space, the closure of a convex set is convex. -/
 lemma convex_closure {A : set α} (hA : convex A) : convex (closure A) :=
-sorry
+λ x y a b hx hy ha hb hab,
+let f : α × α → α := λ p, a • p.fst + b • p.snd in
+_
+
 
 end topological_vector_space

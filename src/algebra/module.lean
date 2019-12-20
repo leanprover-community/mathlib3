3208cb27957e8834314946b117bd0c9b45e9c3c7
Kevin Buzzard <k.buzzard@imperial.ac.uk>
2019-12-20 17:11:03 +0000

    moving ' name to the weaker theorem

--- src/algebra/module.lean
+++ src/algebra/module.lean
@@ -55,7 +55,7 @@ lemma semimodule.eq_zero_of_zero_eq_one (zero_eq_one : (0 : α) = 1) : x = 0 :=
 by rw [←one_smul α x, ←zero_eq_one, zero_smul]
 
 /-- R-linearity of finite sums of elements of an R-semimodule. -/
-lemma finset.sum_smul' {α : Type*} {R : Type*} [semiring R] {M : Type*} [add_comm_monoid M]
+lemma finset.sum_smul {α : Type*} {R : Type*} [semiring R] {M : Type*} [add_comm_monoid M]
   [semimodule R M] (s : finset α) (r : R) (f : α → M) :
     finset.sum s (λ (x : α), (r • (f x))) = r • (finset.sum s f) :=
 by classical; exact

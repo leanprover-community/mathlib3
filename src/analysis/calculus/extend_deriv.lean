189d7de4941b7bc448d41875e00e5da238ee642d
sgouezel <sebastien.gouezel@univ-rennes1.fr>
2019-12-27 11:31:29 +0100

    Update src/analysis/calculus/extend_deriv.lean

--- src/analysis/calculus/extend_deriv.lean
+++ src/analysis/calculus/extend_deriv.lean
@@ -4,7 +4,7 @@ Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Sébastien Gouëzel
 -/
 
-import analysis.calculus.mean_value topology.sequences
+import analysis.calculus.mean_value
 import tactic.monotonicity
 
 /-!

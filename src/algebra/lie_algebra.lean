c8c57b7109af6ee06a213061f6af219964018787
Oliver Nash <oliver.nash@gmail.com>
2020-01-04 18:34:54 +0000

    Lie ideals are subalgebras

--- src/algebra/lie_algebra.lean
+++ src/algebra/lie_algebra.lean
@@ -329,6 +329,10 @@ lemma lie_mem_right (I : lie_ideal R L) (x y : L) (h : y ∈ I) : ⁅x, y⁆ ∈
 lemma lie_mem_left (I : lie_ideal R L) (x y : L) (h : x ∈ I) : ⁅x, y⁆ ∈ I := by {
   rw [←lie_skew, ←neg_lie], apply lie_mem_right, assumption, }
 
+def lie_ideal_subalgebra (I : lie_ideal R L) : lie_subalgebra R L := {
+  lie_mem := by { intros x y hx hy, apply lie_mem_right, exact hy, },
+  ..I.to_submodule, }
+
 end lie_module
 
 namespace lie_quotient

07f57a06d09b3eee21586cceb1ccde3b96e66c06
Tim Baanen <t.baanen@vu.nl>
2020-01-23 18:07:29 +0100

    Module documentation for the file `tactic/simp_rw.lean`

--- src/tactic/simp_rw.lean
+++ src/tactic/simp_rw.lean
@@ -7,6 +7,22 @@ The `simp_rw` tactic, a mix of `simp` and `rewrite`.
 -/
 import tactic.core
 
+/-!
+# The `simp_rw` tactic
+
+This module defines a tactic `simp_rw` which functions as a mix of `simp` and
+`rw`. Like `rw`, it applies each rewrite rule in the given order, but like
+`simp` it repeatedly applies these rules and also under binders like `∀ x, ...`,
+`∃ x, ...` and `λ x, ...`.
+
+## Implementation notes
+
+The tactic works by taking each rewrite rule in turn and applying `simp only` to
+it. It should be possible to support backwards rewriting in the tactic, i.e.
+`simp_rw [←lemma]`, but it will be more useful and not much more work to add
+support for this directly to `simp`.
+-/
+
 namespace tactic.interactive
 open interactive interactive.types tactic
 

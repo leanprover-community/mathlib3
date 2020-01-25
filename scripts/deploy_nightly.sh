946aa7e551f29d7f738b9c1c5cbe4a1946581a22
Bryan Gin-ge Chen <bryangingechen@gmail.com>
2020-01-25 10:59:24 -0500

    fix(scripts/deploy_nightly): fill in blank env vars

--- scripts/deploy_nightly.sh
+++ scripts/deploy_nightly.sh
@@ -28,11 +28,13 @@ fi
 
 # Try to update the lean-x.y.z branch on mathlib. This could fail if
 # a subsequent commit has already pushed an update.
+LEAN_VERSION="3.4.2"
+
 git push mathlib HEAD:refs/heads/$LEAN_VERSION || \
     echo "mathlib rejected push to branch $LEAN_VERSION; maybe it already has a later version?" >&2
 
 # Push the commits to a branch on nightly and push a tag.
-git push nightly HEAD:"mathlib-$TRAVIS_BRANCH" || true
+git push nightly HEAD:"mathlib-master" || true
 git tag $MATHLIB_VERSION_STRING
 git push nightly tag $MATHLIB_VERSION_STRING
 

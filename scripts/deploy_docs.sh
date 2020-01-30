0dd0213f0b9d2ec431b35ce84ed6146c07777692
Rob Lewis <rob.y.lewis@gmail.com>
2020-01-30 10:31:06 +0100

    rmeove _test

--- scripts/deploy_docs.sh
+++ scripts/deploy_docs.sh
@@ -14,12 +14,12 @@ cd doc-gen
 sed -i "s/rev = \"\S*\"/rev = \"$git_hash\"/" leanpkg.toml
 echo -e "builtin_path\npath ./src\npath ../src" > leanpkg.path
 git clone "https://$DEPLOY_NIGHTLY_GITHUB_USER:$DEPLOY_NIGHTLY_GITHUB_TOKEN@github.com/leanprover-community/mathlib_docs.git"
-rm -rf mathlib_docs/docs/*
+rm -rf mathlib_docs/docs/
 elan override set leanprover-community/lean:nightly
 python3 -m pip install --upgrade pip
 pip3 install markdown2 toml
-./gen_docs -w -r "../" -t "mathlib_docs_test/docs/"
-cd mathlib_docs_test/docs
+./gen_docs -w -r "../" -t "mathlib_docs/docs/"
+cd mathlib_docs/docs
 git add -A .
 git config user.name "leanprover-community-bot"
 git config user.email "leanprover.community@gmail.com"

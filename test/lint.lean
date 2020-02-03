b67e076ec253c7eb7d3472be87beafdb23fdd32f
Floris van Doorn <fpvdoorn@gmail.com>
2020-02-03 14:15:26 +0100

    update ge_or_gt test

--- test/lint.lean
+++ test/lint.lean
@@ -4,7 +4,8 @@ def foo1 (n m : ℕ) : ℕ := n + 1
 def foo2 (n m : ℕ) : m = m := by refl
 lemma foo3 (n m : ℕ) : ℕ := n - m
 lemma foo.foo (n m : ℕ) : n ≥ n := le_refl n
-instance bar.bar : has_add ℕ := by apply_instance -- we don't check the name of instances
+instance bar.bar : has_add ℕ := by apply_instance  -- we don't check the name of instances
+lemma foo.bar (ε > 0) : ε = ε := rfl -- >/≥ is allowed in binders (and in fact, in all hypotheses)
 -- section
 -- local attribute [instance, priority 1001] classical.prop_decidable
 -- lemma foo4 : (if 3 = 3 then 1 else 2) = 1 := if_pos (by refl)
@@ -18,11 +19,12 @@ run_cmd do
   l ← e.mfilter (λ d, return $
     e.in_current_file' d.to_name && ¬ d.to_name.is_internal && ¬ d.is_auto_generated e),
   l2 ← fold_over_with_cond l (return ∘ check_unused_arguments),
-  guard $ l2.length = 3,
+  guard $ l2.length = 4,
   let l2 : list t := l2.map $ λ x, ⟨x.1.to_name, x.2⟩,
   guard $ (⟨`foo1, [2]⟩ : t) ∈ l2,
   guard $ (⟨`foo2, [1]⟩ : t) ∈ l2,
   guard $ (⟨`foo.foo, [2]⟩ : t) ∈ l2,
+  guard $ (⟨`foo.bar, [2]⟩ : t) ∈ l2,
   l2 ← fold_over_with_cond l incorrect_def_lemma,
   guard $ l2.length = 2,
   let l2 : list (name × _) := l2.map $ λ x, ⟨x.1.to_name, x.2⟩,

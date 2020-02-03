760eb06eb138ea1ad95b1d8820633cf4decea848
Floris van Doorn <fpvdoorn@gmail.com>
2020-02-03 19:18:28 +0100

    update mk_all.sh

--- scripts/mk_all.sh
+++ scripts/mk_all.sh
@@ -39,5 +39,6 @@ cat <<EOT >> lint_mathlib.lean
 
 open nat -- need to do something before running a command
 
-#lint_mathlib- only unused_arguments dup_namespace doc_blame illegal_constants def_lemma instance_priority
+#lint_mathlib- only unused_arguments dup_namespace doc_blame ge_or_gt def_lemma instance_priority
+  has_inhabited_instance impossible_instance incorrect_type_class_argument dangerous_instance
 EOT

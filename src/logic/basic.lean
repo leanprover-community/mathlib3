8e664bbaf9cae41674d67456f0525990a071465b
Rob Lewis <rob.y.lewis@gmail.com>
2020-02-05 21:17:53 +0100

    fix library_note imports

--- src/logic/basic.lean
+++ src/logic/basic.lean
@@ -4,7 +4,7 @@ Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Jeremy Avigad, Leonardo de Moura
 -/
 
-import tactic.library_note
+import tactic.doc_commands
 
 /-!
 # Basic logic properties

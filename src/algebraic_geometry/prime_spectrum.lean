965e7ba692fb37120fc187c863de34ede7df9c6d
Johan Commelin <johan@commelin.net>
2020-02-06 19:00:50 +0100

    Update src/algebraic_geometry/prime_spectrum.lean
    Co-Authored-By: Chris Hughes <33847686+ChrisHughes24@users.noreply.github.com>

--- src/algebraic_geometry/prime_spectrum.lean
+++ src/algebraic_geometry/prime_spectrum.lean
@@ -136,7 +136,7 @@ lemma Inter_zero_locus {ι : Type*} (s : ι → set R) :
 is defined via the closed sets of the topology:
 they are exactly those sets that are the zero locus of a subset of the ring. -/
 instance zariski_topology : topological_space (prime_spectrum R) :=
-topological_space.of_closed {Z | ∃ s, Z = prime_spectrum.zero_locus s}
+topological_space.of_closed (set.range prime_spectrum.zero_locus)
 (⟨set.univ, by simp⟩)
 begin
   intros Zs h,

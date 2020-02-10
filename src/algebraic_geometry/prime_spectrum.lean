62942aaa90a8ed5f0fb010304e6cff142a944cfb
Johan Commelin <johan@commelin.net>
2020-02-10 05:42:02 +0100

    wip

--- src/algebraic_geometry/prime_spectrum.lean
+++ src/algebraic_geometry/prime_spectrum.lean
@@ -26,6 +26,9 @@ and Chris Hughes (on an earlier repository).
 
 -/
 
+noncomputable theory
+open_locale classical
+
 universe variables u v
 
 variables (R : Type u) [comm_ring R]
@@ -69,6 +72,35 @@ def zero_locus (s : set R) : set (prime_spectrum R) :=
 @[simp] lemma mem_zero_locus (x : prime_spectrum R) (s : set R) :
   x ∈ zero_locus s ↔ s ⊆ x.as_ideal := iff.rfl
 
+/-- The vanishing ideal of a set `s` of points
+of the prime spectrum of a commutative ring `R`
+is the largest ideal of the ring that is contained in all the prime ideals in the set `s`.
+
+An element `f` of `R` can be thought of as a dependent function
+on the prime spectrum of `R`.
+At a point `x` (a prime ideal)
+the function (i.e., element) `f` takes values in the quotient ring `R` modulo the prime ideal `x`.
+In this manner, `vanishing_ideal s` is exactly the ideal of `R`
+consisting of all "functions" that vanish on all of `s`.
+-/
+def vanishing_ideal (s : set (prime_spectrum R)) : ideal R :=
+⨅ (x : prime_spectrum R) (h : x ∈ s), x.as_ideal
+
+lemma coe_vanishing_ideal (s : set (prime_spectrum R)) :
+  (vanishing_ideal s : set R) = {f : R | ∀ x : prime_spectrum R, x ∈ s → f ∈ x.as_ideal} :=
+begin
+  ext f,
+  rw [vanishing_ideal, submodule.mem_coe, submodule.mem_infi],
+  apply forall_congr, intro x,
+  rw [submodule.mem_infi],
+end
+
+lemma mem_vanishing_ideal (s : set (prime_spectrum R)) (f : R) :
+  f ∈ vanishing_ideal s ↔ ∀ x : prime_spectrum R, x ∈ s → f ∈ x.as_ideal :=
+by rw [← submodule.mem_coe, coe_vanishing_ideal, set.mem_set_of_eq]
+
+def gc : galois_connection vanishing_ideal (λ I, zero_locus (I : set R))
+
 lemma zero_locus_empty_of_one_mem {s : set R} (h : (1:R) ∈ s) :
   zero_locus s = ∅ :=
 begin
@@ -78,6 +110,18 @@ begin
   have x_prime : x.as_ideal.is_prime := by apply_instance,
   have eq_top : x.as_ideal = ⊤, { rw ideal.eq_top_iff_one, exact hx h },
   apply x_prime.1 eq_top,
+end
+
+lemma zero_locus_empty_iff_eq_top {I : ideal R} :
+  zero_locus (I : set R) = ∅ ↔ I = ⊤ :=
+begin
+  split,
+  { contrapose!,
+    intro h,
+    apply set.ne_empty_iff_nonempty.mpr,
+    rcases ideal.exists_le_maximal I h with ⟨M, hM, hIM⟩,
+    exact ⟨⟨M, hM.is_prime⟩, hIM⟩ },
+  { rintro rfl, apply zero_locus_empty_of_one_mem, trivial }
 end
 
 @[simp] lemma zero_locus_univ :
@@ -131,6 +175,46 @@ by { ext x, simp only [mem_zero_locus, set.mem_Inter, set.Union_subset_iff] }
 lemma Inter_zero_locus {ι : Type*} (s : ι → set R) :
   (⋂ i, zero_locus (s i)) = zero_locus (⋃ i, s i) :=
 (zero_locus_Union s).symm
+
+lemma zero_locus_union (s t : set R) :
+  zero_locus (s ∪ t) = zero_locus s ∩ zero_locus t :=
+begin
+  ext x,
+  simp only [set.union_subset_iff, set.mem_inter_eq, iff_self, prime_spectrum.mem_zero_locus]
+end
+
+-- TODO: we actually get the radical ideal,
+-- but I think that isn't in mathlib yet.
+lemma subset_vanishing_ideal_zero_locus (s : set R) :
+  s ⊆ vanishing_ideal (zero_locus s) :=
+begin
+  intros f hf,
+  rw coe_vanishing_ideal,
+  intro x,
+  rw mem_zero_locus,
+  intro hsx,
+  exact hsx hf
+end
+
+lemma le_vanishing_ideal_zero_locus (I : ideal R) :
+  I ≤ vanishing_ideal (zero_locus I) :=
+subset_vanishing_ideal_zero_locus I
+
+lemma vanishing_ideal_union (s t : set (prime_spectrum R)) :
+  vanishing_ideal (s ∪ t) = vanishing_ideal s ⊓ vanishing_ideal t :=
+begin
+  ext f,
+  simp only [mem_vanishing_ideal, or_imp_distrib, forall_and_distrib,
+    submodule.mem_inf, iff_self, set.mem_union_eq]
+end
+
+lemma vanishing_ideal_Union {ι : Type*} (s : ι → set (prime_spectrum R)) :
+  vanishing_ideal (⋃ i, s i) = (⨅ i, vanishing_ideal (s i)) :=
+begin
+  ext f,
+  simp only [mem_vanishing_ideal, set.mem_Union, exists_imp_distrib, submodule.mem_infi],
+  rw forall_swap
+end
 
 /-- The Zariski topology on the prime spectrum of a commutative ring
 is defined via the closed sets of the topology:
@@ -198,5 +282,53 @@ begin
 end
 
 end comap
+
+lemma zero_locus_vanishing_ideal_eq_closure (s : set (prime_spectrum R)) :
+  zero_locus (vanishing_ideal s : set R) = closure s :=
+begin
+  apply set.subset.antisymm,
+  { intros x hx,
+    rw mem_zero_locus at hx,
+    sorry },
+end
+
+lemma submodule.exists_finset_of_mem_supr {R : Type u} {M : Type v}
+  [ring R] [add_comm_group M] [module R M] {ι : Sort*}
+  (p : ι → submodule R M) {m : M} (hm : m ∈ ⨆ (i : ι), p i) :
+  ∃ s : finset ι, m ∈ ⨆ i ∈ s, p i :=
+begin
+  sorry,
+end
+
+instance : compact_space (prime_spectrum R) :=
+{ compact_univ :=
+  begin
+    apply compact_of_finite_subcover,
+    intros U h_open h_cover,
+    rw set.univ_subset_iff at h_cover,
+    have H : -⋃₀ U = ∅, { simp [h_cover] },
+    rw [set.compl_sUnion, set.sInter_image, set.bInter_eq_Inter] at H,
+    change (⋂ (x : U), - x.val) = ∅ at H,
+    -- TODO: define vanishing_ideal and get rid of choice
+    let f : U → set R := λ i, classical.some (h_open _ i.2),
+    have hf : ∀ i : U, -i.1 = zero_locus (f i) := λ i, (classical.some_spec (h_open _ i.2)).symm,
+    simp only [hf] at H,
+    rw [Inter_zero_locus, ← zero_locus_span,
+        zero_locus_empty_iff_eq_top, ideal.eq_top_iff_one] at H,
+    have aux : ideal.span (⋃ i : U, f i) = ⨆ i : U, ideal.span (f i),
+    { exact submodule.span_Union _, },
+    rw aux at H, clear aux,
+    rcases submodule.exists_finset_of_mem_supr _ H with ⟨s, hs⟩,
+    have aux : ideal.span (⋃ (i : U) (H : i ∈ s), f i) = ⨆ (i : U) (H : i ∈ s), ideal.span (f i),
+    { sorry },
+    rw ← aux at hs, clear aux,
+    rw [←ideal.eq_top_iff_one, ←zero_locus_empty_iff_eq_top,
+        zero_locus_span, ←Inter_zero_locus] at hs,
+    refine ⟨subtype.val '' (↑s : set U), _, _, _⟩,
+    { rw set.image_subset_iff, intros u hu, exact u.2 },
+    { apply set.finite_image, exact finset.finite_to_set s },
+    rw set.univ_subset_iff,
+    rw [set.sUnion_image, set.bUnion_eq_Union],
+  end }
 
 end prime_spectrum

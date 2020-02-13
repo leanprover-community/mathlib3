349906ce2913c3b938a7f840add32a3b65f9ebf6
Rob Lewis <rob.y.lewis@gmail.com>
2020-02-13 21:09:53 +0100

    header override is already unset

--- scripts/update_nolints.sh
+++ scripts/update_nolints.sh
@@ -3,11 +3,6 @@ DEPLOY_NIGHTLY_GITHUB_USER=leanprover-community-bot
 set -e
 set -x
 
-# By default, github actions overrides the credentials used to access any
-# github url so that it uses the github-actions[bot] user.  We want to access
-# github using a different username.
-git config --unset http.https://github.com/.extraheader
-
 cd scripts
 ./mk_all.sh
 lean --run mk_nolint.lean

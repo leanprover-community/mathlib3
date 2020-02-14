938960ee6d6aef11647a38711c84d49dd1776731
Rob Lewis <Rob.y.lewis@gmail.com>
2020-02-14 11:30:14 +0100

    fix(scripts/deploy_docs): remove last trace of nightly builds (#1986)

--- scripts/deploy_docs.sh
+++ scripts/deploy_docs.sh
@@ -10,7 +10,7 @@ sed -i "s/rev = \"\S*\"/rev = \"$git_hash\"/" leanpkg.toml
 echo -e "builtin_path\npath ./src\npath ../src" > leanpkg.path
 git clone "https://$DEPLOY_NIGHTLY_GITHUB_USER:$DEPLOY_NIGHTLY_GITHUB_TOKEN@github.com/leanprover-community/mathlib_docs.git"
 rm -rf mathlib_docs/docs/
-elan override set leanprover-community/lean:nightly
+elan override set leanprover-community/lean:3.5.1
 python3 -m pip install --upgrade pip
 pip3 install markdown2 toml
 ./gen_docs -w -r "../" -t "mathlib_docs/docs/"

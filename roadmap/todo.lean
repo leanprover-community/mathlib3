8777d5e39d211b5b32dc6486b050f67f8b216a97
Scott Morrison <scott@tqft.net>
2020-02-17 10:21:15 -0800

    Update roadmap/todo.lean

--- roadmap/todo.lean
+++ roadmap/todo.lean
@@ -1,2 +1,4 @@
--- Axiom used to skip proofs in formal roadmaps.
+/-- Axiom used to skip proofs in formal roadmaps.
+(When working on a roadmap, you may prefer to prove new lemmas, rather than trying to solve an `exact todo` in-line. The tactic `extract_goal` is useful for this.)
+-/
 axiom todo {p : Prop} : p

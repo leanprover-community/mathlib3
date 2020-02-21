1ff273374970d4a4b3cb5f423386246414bb1746
Johan Commelin <johan@commelin.net>
2020-02-21 08:55:46 +0100

    Fix import

--- src/ring_theory/polynomial.lean
+++ src/ring_theory/polynomial.lean
@@ -8,7 +8,7 @@ Ring-theoretic supplement of data.polynomial.
 Main result: Hilbert basis theorem, that if a ring is noetherian then so is its polynomial ring.
 -/
 
-import data.polynomial data.mv_polynomial
+import data.equiv.fin data.polynomial data.mv_polynomial
 import ring_theory.subring
 import ring_theory.ideals ring_theory.noetherian
 

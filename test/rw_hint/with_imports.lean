77b8a9547cad3285b443baa8a9f43a3125d4c005
Scott Morrison <scott@tqft.net>
2020-02-25 09:46:49 -0800

    with_imports doesn't really work, sadly.

--- test/rw_hint/with_imports.lean
+++ test/rw_hint/with_imports.lean
@@ -1,5 +1,6 @@
 import tactic.rw_hint
--- Now import a bunch of stuff, to make sure `rw_hint` is still usable.
+-- Now import a bunch of stuff, to see if `rw_hint` is still usable.
+-- (Unfortunately, it's not!)
 import topology.uniform_space.uniform_embedding
 import algebraic_geometry.stalks
 import measure_theory.l1_space
@@ -10,6 +11,10 @@ open tactic
 example : 2 * (3 + 4) = 2 * 3 + 2 * 4 :=
 begin
   -- rw_hint,
-  tactic.try_for 50000 (do s ← tactic.rw_hint, guard $ "rw left_distrib" ∈ s),
+  -- Collecting all the rewrites (~20k) from the environment takes a few seconds,
+  -- but is still reasonable:
+  tactic.try_for 15000 (tactic.rewrite_search.discovery.find_all_rewrites),
+  -- Unfortunately trying them all becomes too slow:
+  -- tactic.try_for 50000 (do s ← tactic.rw_hint, guard $ "rw left_distrib" ∈ s),
   rw left_distrib,
 end

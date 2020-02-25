566464840390e5041eb4e584e620d67abfa138c1
Scott Morrison <scott@tqft.net>
2020-02-24 16:57:00 -0800

    chore(field_theory/minimal_polynomial): fix timeout

--- src/field_theory/minimal_polynomial.lean
+++ src/field_theory/minimal_polynomial.lean
@@ -190,18 +190,21 @@ lemma root {x : β} (hx : is_integral α x) {y : α}
 begin
   have ndeg_one : nat_degree (minimal_polynomial hx) = 1,
   { rw ← polynomial.degree_eq_iff_nat_degree_eq_of_pos (nat.zero_lt_one),
-    exact degree_eq_one_of_irreducible_of_root (irreducible hx) h },
+     exact degree_eq_one_of_irreducible_of_root (irreducible hx) h },
   have coeff_one : (minimal_polynomial hx).coeff 1 = 1,
-  { simpa [ndeg_one, leading_coeff] using (monic hx).leading_coeff },
+  { simpa only [ndeg_one, leading_coeff] using (monic hx).leading_coeff },
   have hy : y = - coeff (minimal_polynomial hx) 0,
   { rw (minimal_polynomial hx).as_sum at h,
     apply eq_neg_of_add_eq_zero,
-    simpa [ndeg_one, finset.sum_range_succ, coeff_one] using h },
+    simpa only [ndeg_one, coeff_one, C_1, eval_C, eval_X, eval_add, mul_one, one_mul, pow_zero, pow_one,
+      is_root.def, finset.sum_range_succ, finset.insert_empty_eq_singleton, finset.sum_singleton, finset.range_one] using h, },
   subst y,
   rw [algebra.map_neg, neg_eq_iff_add_eq_zero],
   have H := aeval hx,
   rw (minimal_polynomial hx).as_sum at H,
-  simpa [ndeg_one, finset.sum_range_succ, coeff_one, aeval_def] using H
+  simpa only [ndeg_one, coeff_one, aeval_def, C_1, eval₂_add, eval₂_C, eval₂_X,
+    mul_one, one_mul, pow_one, pow_zero, add_comm,
+    finset.sum_range_succ, finset.insert_empty_eq_singleton, finset.sum_singleton, finset.range_one] using H,
 end
 
 /--The constant coefficient of the minimal polynomial of x is 0

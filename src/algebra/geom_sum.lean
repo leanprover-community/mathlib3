748a257f2e9f96b8e57ce929e1f50ab3275971e7
Scott Morrison <scott@tqft.net>
2020-03-04 17:27:25 -0800

    fixing a proof

--- src/algebra/geom_sum.lean
+++ src/algebra/geom_sum.lean
@@ -146,6 +146,10 @@ have h₄ : x * x⁻¹ ^ n = x⁻¹ ^ n * x,
   from nat.cases_on n (by simp)
   (λ _, by conv { to_rhs, rw [pow_succ', mul_assoc, inv_mul_cancel hx0, mul_one] };
     rw [pow_succ, ← mul_assoc, mul_inv_cancel hx0, one_mul]),
-by rw [geom_sum h₁, div_eq_iff_mul_eq h₂, ← domain.mul_left_inj h₃,
-    ← mul_assoc, ← mul_assoc, mul_inv_cancel h₃];
-  simp [mul_add, add_mul, mul_inv_cancel hx0, mul_assoc, h₄, sub_eq_add_neg, add_comm, add_left_comm]
+-- TODO better to prove this directly, without h₄?
+have h₅ : x * (x ^ n)⁻¹ = (x ^ n)⁻¹ * x, by simpa using h₄,
+begin
+  rw [geom_sum h₁, div_eq_iff_mul_eq h₂, ← domain.mul_left_inj h₃,
+    ← mul_assoc, ← mul_assoc, mul_inv_cancel h₃],
+  simp [mul_add, add_mul, mul_inv_cancel hx0, mul_assoc, h₅, sub_eq_add_neg, add_comm, add_left_comm],
+end

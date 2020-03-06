d7483752a404078bd59e502fc844fbfc4fff283d
Rob Lewis <rob.y.lewis@gmail.com>
2020-03-06 17:52:14 +0100

    unnecessary to look up the exact git sha

--- scripts/fetch_olean_cache.sh
+++ scripts/fetch_olean_cache.sh
@@ -3,10 +3,6 @@ set -x
 
 archive_url="https://oleanstorage.azureedge.net/mathlib/"
 
-if [[ `wget -S --spider $archive_url$git_sha.tar.gz  2>&1 | grep 'HTTP/1.1 200 OK'` ]]; then
-  curl "$archive_url$git_sha.tar.gz" | tar xz --wildcards *.olean
-else
-  new_git_sha=$(python scripts/look_up_olean_hash.py $lean_file_hash) || exit 0
-  echo "new hash $new_git_sha"
-  curl "$archive_url$new_git_sha.tar.gz" | tar xz --wildcards *.olean
-fi
+new_git_sha=$(python scripts/look_up_olean_hash.py $lean_file_hash) || exit 0
+echo "equivalent Git sha: $new_git_sha"
+curl "$archive_url$new_git_sha.tar.gz" | tar xz --wildcards *.olean

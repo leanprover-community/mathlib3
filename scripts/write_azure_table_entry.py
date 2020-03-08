ff2cdcd2fcf96e26d1ab5edc30767e4f943a15e6
Rob Lewis <rob.y.lewis@gmail.com>
2020-03-08 19:43:52 +0100

    simplify

--- scripts/write_azure_table_entry.py
+++ scripts/write_azure_table_entry.py
@@ -5,12 +5,11 @@ file_hash = sys.argv[1]
 git_hash = sys.argv[2]
 token = sys.argv[3]
 
-def get_url():
-  return "https://oleanstorage.table.core.windows.net/oleanlookup?{0}".format(token)
+lookup_url = 'https://oleanstorage.table.core.windows.net/oleanlookup?' + token
 
 def add_entry(file_hash, git_hash):
   r = requests.post(
-    url = get_url(),
+    url = lookup_url,
     json={'PartitionKey':'0', 'RowKey':file_hash, 'git':git_hash},
     headers={'Accept':'application/json;odata=nometadata'}
   )

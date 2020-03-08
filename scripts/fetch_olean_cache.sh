304827a944b3f227acdadf45312a5562f7a71642
Rob Lewis <rob.y.lewis@gmail.com>
2020-03-08 19:26:25 +0100

    untar all and only src directory

--- scripts/fetch_olean_cache.sh
+++ scripts/fetch_olean_cache.sh
@@ -5,4 +5,4 @@ archive_url="https://oleanstorage.azureedge.net/mathlib/"
 
 new_git_sha=$(python scripts/look_up_olean_hash.py $lean_file_hash) || exit 0
 echo "equivalent Git sha: $new_git_sha"
-curl "$archive_url$new_git_sha.tar.gz" | tar xz --wildcards *.olean
+curl "$archive_url$new_git_sha.tar.gz" | tar xz src

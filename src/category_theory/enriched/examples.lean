dd55435333b9f630e6d964aa42ad8ae2ccd73e83
Scott Morrison <scott@tqft.net>
2020-03-12 07:07:28 -0700

    blech

--- src/category_theory/enriched/examples.lean
+++ src/category_theory/enriched/examples.lean
@@ -1,34 +1,23 @@
 import category_theory.enriched.enriched_over
-import algebra.category.Group.monoidal
+import algebra.category.Module.monoidal
 
 universes u
 
 open category_theory
 
-namespace AddCommGroup
+namespace Module
 
--- TODO generalise the next three, to anything with a forgetful functor preserving limits
-
--- def forget_unit : (forget AddCommGroup).obj (𝟙_ AddCommGroup) ≅ 𝟙_ (Type u) :=
--- (limits.preserves_limit_iso (functor.empty AddCommGroup) (forget AddCommGroup)).trans
---   (limits.lim.map_iso (functor.empty_comp (forget AddCommGroup)))
-
--- def forget_tensor (G H : AddCommGroup.{u}) :
---   (forget AddCommGroup).obj (G ⊗ H) ≅ (forget AddCommGroup).obj G ⊗ (forget AddCommGroup).obj H :=
--- (limits.preserves_limit_iso _ (forget AddCommGroup)).trans
---   (limits.lim.map_iso (limits.pair_comp G H (forget AddCommGroup)))
-
-instance : concrete_monoidal_category AddCommGroup.{u} :=
+instance : concrete_monoidal_category (Module ℤ) :=
 { lax_monoidal :=
-  { ε := forget_unit.inv,
-    μ := λ G H, (forget_tensor G H).inv,
-    μ_natural' := λ X Y X' Y' f g, begin dsimp, ext, dsimp, end,
-    associativity' := λ X Y Z, begin dsimp, ext, dsimp, end, }}
+  { ε := sorry,
+    μ := λ G H, sorry,
+    μ_natural' := λ X Y X' Y' f g, sorry,
+    associativity' := λ X Y Z, sorry, }}
 
-example : enriched_over AddCommGroup.{u} AddCommGroup.{u} :=
-{ e_hom := λ X Y, AddCommGroup.of (X ⟶ Y),
-  e_id := λ X, begin fsplit, exact λ x, 𝟙 X, end,
-  e_comp := λ X Y Z p, p.val (limits.walking_pair.left) ≫ p.val (limits.walking_pair.right), -- that was ugly...
+example : enriched_over (Module ℤ) (Module ℤ) :=
+{ e_hom := λ X Y, Module.of ℤ (X ⟶ Y),
+  e_id := λ X, sorry,
+  e_comp := λ X Y Z p, sorry,
   e_hom_forget := λ X Y, equiv.refl _ }
 
 -- TODO modules over a ring are enriched over themselves

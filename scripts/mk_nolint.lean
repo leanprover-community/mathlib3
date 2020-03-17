leanprover-community/mathlib3

commit e94fef0121317cce8afd4af2b1d94645012088f5

feat(lint): run some linters on automatically generated declarations (#2090)
* style(lint): remove double periods

remove spacing in the OK-results of #lint
add newline after ever failed result of #lint_mathlib (1 newline between two files, 2 newlines between two linters)

* run some linters on automatically generated declarations

* fix doc string

* fix mk_nolint

* fix test

* fix linter errors

also explain how to fix linter errors for automatically generated declarations

* fix linter errors

Co-authored-by: Rob Lewis <Rob.y.lewis@gmail.com>
Co-authored-by: Bryan Gin-ge Chen <bryangingechen@gmail.com>
Co-authored-by: mergify[bot] <37929162+mergify[bot]@users.noreply.github.com>

scripts/mk_nolint.lean
@@ -26,9 +26,11 @@ open native
 
 /-- Runs when called with `lean --run` -/
 meta def main : io unit := do
+e ← run_tactic get_env,
 decls ← run_tactic lint_mathlib_decls,
+let non_auto_decls := decls.filter (λ d, ¬ d.to_name.is_internal ∧ ¬ d.is_auto_generated e),
 linters ← run_tactic $ get_linters mathlib_linters,
-results ← run_tactic $ lint_core decls linters,
+results ← run_tactic $ lint_core decls non_auto_decls linters,
 env ← run_tactic tactic.get_env,
 mathlib_path_len ← string.length <$> run_tactic tactic.get_mathlib_dir,
 let failed_decls_by_file := rb_lmap.of_list (do

src/analysis/normed_space/basic.lean
@@ -771,11 +771,14 @@ end normed_space
 
 section normed_algebra
 
+section prio
+set_option default_priority 100 -- see Note [default priority]
 /-- A normed algebra `𝕜'` over `𝕜` is an algebra endowed with a norm for which the embedding of
 `𝕜` in `𝕜'` is an isometry. -/
 class normed_algebra (𝕜 : Type*) (𝕜' : Type*) [normed_field 𝕜] [normed_ring 𝕜']
   extends algebra 𝕜 𝕜' :=
 (norm_algebra_map_eq : ∀x:𝕜, ∥algebra_map 𝕜' x∥ = ∥x∥)
+end prio
 
 @[simp] lemma norm_algebra_map_eq {𝕜 : Type*} (𝕜' : Type*) [normed_field 𝕜] [normed_ring 𝕜']
   [h : normed_algebra 𝕜 𝕜'] (x : 𝕜) : ∥algebra_map 𝕜' x∥ = ∥x∥ :=

src/category_theory/limits/shapes/finite_limits.lean
@@ -35,7 +35,9 @@ class has_finite_limits :=
 class has_finite_colimits :=
 (has_colimits_of_shape : Π (J : Type v) [small_category J] [fin_category J], has_colimits_of_shape.{v} J C)
 
-attribute [instance] has_finite_limits.has_limits_of_shape has_finite_colimits.has_colimits_of_shape
+attribute [instance, priority 100] -- see Note [lower instance priority]
+  has_finite_limits.has_limits_of_shape
+  has_finite_colimits.has_colimits_of_shape
 
 @[priority 100] -- see Note [lower instance priority]
 instance [has_limits.{v} C] : has_finite_limits.{v} C :=

src/category_theory/limits/shapes/images.lean
@@ -175,7 +175,7 @@ end
 class has_images :=
 (has_image : Π {X Y : C} (f : X ⟶ Y), has_image.{v} f)
 
-attribute [instance] has_images.has_image
+attribute [instance, priority 100] has_images.has_image
 
 section
 variables (f) [has_image f]

src/data/fin_enum.lean
@@ -24,7 +24,7 @@ class fin_enum (α : Sort*) :=
 (equiv : α ≃ fin card)
 [dec_eq : decidable_eq α]
 
-attribute [instance] fin_enum.dec_eq
+attribute [instance, priority 100] fin_enum.dec_eq
 
 namespace fin_enum
 

src/ring_theory/principal_ideal_domain.lean
@@ -21,7 +21,8 @@ class principal_ideal_domain (α : Type*) extends integral_domain α :=
 (principal : ∀ (S : ideal α), S.is_principal)
 end prio
 
-attribute [instance] principal_ideal_domain.principal
+-- see Note [lower instance priority]
+attribute [instance, priority 500] principal_ideal_domain.principal
 namespace ideal.is_principal
 variable [comm_ring α]
 

src/tactic/lint.lean
@@ -102,12 +102,15 @@ signifies a passing test. Returning `some msg` reports a failing test with error
 when at least one test is positive.
 
 If `is_fast` is false, this test will be omitted from `#lint-`.
+
+If `auto_decls` is true, this test will also be executed on automatically generated declarations.
 -/
 meta structure linter :=
 (test : declaration → tactic (option string))
 (no_errors_found : string)
 (errors_found : string)
 (is_fast : bool := tt)
+(auto_decls : bool := ff)
 
 /-- Takes a list of names that resolve to declarations of type `linter`,
 and produces a list of linters. -/
@@ -259,7 +262,6 @@ such as `∀ ε > 0`. Such statements should be marked with the attribute `nolin
 failures."
 
 /-- checks whether an instance that always applies has priority ≥ 1000. -/
--- TODO: instance_priority should also be tested on automatically-generated declarations
 meta def instance_priority (d : declaration) : tactic (option string) := do
   let nm := d.to_name,
   b ← is_instance nm,
@@ -314,7 +316,11 @@ This is in the default linter set. -/
   no_errors_found := "All instance priorities are good",
   errors_found := "DANGEROUS INSTANCE PRIORITIES.
 The following instances always apply, and therefore should have a priority < 1000.
-If you don't know what priority to choose, use priority 100." }
+If you don't know what priority to choose, use priority 100.
+
+If this is an automatically generated instance (using the keywords `class` and `extends`),
+see note [lower instance priority] and see note [default priority] for instructions to change the priority",
+  auto_decls := tt }
 
 /-- Reports definitions and constants that are missing doc strings -/
 meta def doc_blame_report_defn : declaration → tactic (option string)
@@ -364,7 +370,7 @@ else
 @[linter, priority 1440]
 meta def linter.has_inhabited_instance : linter :=
 { test := has_inhabited_instance,
-  no_errors_found := "No types have missing inhabited instances.",
+  no_errors_found := "No types have missing inhabited instances",
   errors_found := "TYPES ARE MISSING INHABITED INSTANCES",
   is_fast := ff }
 
@@ -435,8 +441,11 @@ meta def dangerous_instance (d : declaration) : tactic (option string) := do
 @[linter, priority 1410] meta def linter.dangerous_instance : linter :=
 { test := dangerous_instance,
   no_errors_found := "No dangerous instances",
-  errors_found := "DANGEROUS INSTANCES FOUND
-These instances are recursive, and create a new type-class problem which will have metavariables. Currently this linter does not check whether the metavariables only occur in arguments marked with `out_param`, in which case this linter gives a false positive." }
+  errors_found := "DANGEROUS INSTANCES FOUND.\nThese instances are recursive, and create a new type-class problem which will have metavariables.
+  Possible solution: remove the instance attribute or make it a local instance instead.
+
+  Currently this linter does not check whether the metavariables only occur in arguments marked with `out_param`, in which case this linter gives a false positive.",
+  auto_decls := tt }
 
 /-- Applies expression `e` to local constants, but lifts all the arguments that are `Sort`-valued to
   `Type`-valued sorts. -/
@@ -643,7 +652,7 @@ and which hence never fire.
 @[linter, priority 1389] meta def linter.simp_var_head : linter :=
 { test := simp_var_head,
   no_errors_found :=
-    "No left-hand sides of a simp lemma has a variable as head symbol.",
+    "No left-hand sides of a simp lemma has a variable as head symbol",
   errors_found := "LEFT-HAND SIDE HAS VARIABLE AS HEAD SYMBOL.\n" ++
     "Some simp lemmas have a variable as head symbol of the left-hand side" }
 
@@ -664,7 +673,7 @@ pure $ "should not be marked simp"
 /-- A linter for commutativity lemmas that are marked simp. -/
 @[linter, priority 1385] meta def linter.simp_comm : linter :=
 { test := simp_comm,
-  no_errors_found := "No commutativity lemma is marked simp.",
+  no_errors_found := "No commutativity lemma is marked simp",
   errors_found := "COMMUTATIVITY LEMMA IS SIMP.\n" ++
     "Some commutativity lemmas are simp lemmas" }
 
@@ -688,18 +697,21 @@ e ← get_env,
 pure $ ¬ e.contains (mk_nolint_decl_name decl linter)
 
 /--
-`lint_core decls checks` applies the linters `checks` to the list of declarations `decls`.
+`lint_core all_decls non_auto_decls checks` applies the linters `checks` to the list of declarations.
+If `auto_decls` is false for a linter (default) the linter is applied to `non_auto_decls`.
+If `auto_decls` is true, then it is applied to `all_decls`.
 The resulting list has one element for each linter, containing the linter as
 well as a map from declaration name to warning.
 -/
-meta def lint_core (decls : list declaration) (checks : list (name × linter)) :
-  tactic (list (name × linter × rb_map name string)) :=
+meta def lint_core (all_decls non_auto_decls : list declaration) (checks : list (name × linter)) :
+  tactic (list (name × linter × rb_map name string)) := do
 checks.mmap $ λ ⟨linter_name, linter⟩, do
-results ← decls.mfoldl (λ (results : rb_map name string) decl, do
-  tt ← should_be_linted linter_name decl.to_name | pure results,
-  some linter_warning ← linter.test decl | pure results,
-  pure $ results.insert decl.to_name linter_warning) mk_rb_map,
-pure (linter_name, linter, results)
+  let test_decls := if linter.auto_decls then all_decls else non_auto_decls,
+  results ← test_decls.mfoldl (λ (results : rb_map name string) decl, do
+    tt ← should_be_linted linter_name decl.to_name | pure results,
+    some linter_warning ← linter.test decl | pure results,
+    pure $ results.insert decl.to_name linter_warning) mk_rb_map,
+  pure (linter_name, linter, results)
 
 /-- Sorts a map with declaration keys as names by line number. -/
 meta def sort_results {α} (results : rb_map name α) : tactic (list (name × α)) := do
@@ -730,9 +742,10 @@ let results := results.fold (rb_map.mk string (rb_map name string)) $
     let fn := (e.decl_olean decl_name).get_or_else "" in
     results.insert fn (((results.find fn).get_or_else mk_rb_map).insert
       decl_name linter_warning),
-format.intercalate "\n\n" <$> results.to_list.reverse.mmap (λ ⟨fn, results⟩, do
+l ← results.to_list.reverse.mmap (λ ⟨fn, results⟩, do
   formatted ← formatter results,
-  pure $ "-- " ++ fn.popn drop_fn_chars ++ "\n" ++ formatted)
+  pure ("-- " ++ fn.popn drop_fn_chars ++ "\n" ++ formatted : format)),
+return $ format.intercalate "\n\n" l ++ "\n"
 
 /-- The common denominator of `#lint[|mathlib|all]`.
 The different commands have different configurations for `l`,
@@ -743,10 +756,12 @@ By setting `checks` you can customize which checks are performed.
 
 Returns a `name_set` containing the names of all declarations that fail any check in `check`,
 and a `format` object describing the failures. -/
-meta def lint_aux (l : list declaration) (group_by_filename : option nat)
+meta def lint_aux (decls : list declaration) (group_by_filename : option nat)
     (where_desc : string) (slow verbose : bool) (checks : list (name × linter)) :
   tactic (name_set × format) := do
-results ← lint_core l checks,
+e ← get_env,
+let non_auto_decls := decls.filter (λ d, ¬ d.to_name.is_internal ∧ ¬ d.is_auto_generated e),
+results ← lint_core decls non_auto_decls checks,
 formatted_results ← results.mmap $ λ ⟨linter_name, linter, results⟩,
   let report_str : format := to_fmt "/- The `" ++ to_fmt linter_name ++ "` linter reports: -/\n" in
   if ¬ results.empty then do
@@ -754,12 +769,12 @@ formatted_results ← results.mmap $ λ ⟨linter_name, linter, results⟩,
       | none := print_warnings results
       | some dropped := grouped_by_filename results dropped print_warnings
       end,
-    pure $ report_str ++ "/- " ++ linter.errors_found ++ ": -/\n" ++ warnings
+      pure $ report_str ++ "/- " ++ linter.errors_found ++ ": -/\n" ++ warnings ++ "\n"
   else
     pure $ if verbose then "/- OK: " ++ linter.no_errors_found ++ ". -/" else format.nil,
-let s := format.intercalate "\n\n" (formatted_results.filter (λ f, ¬ f.is_nil)),
+let s := format.intercalate "\n" (formatted_results.filter (λ f, ¬ f.is_nil)),
 let s := if ¬ verbose then s else
-  "/- Checking " ++ l.length ++ " declarations " ++ where_desc ++ " -/\n\n" ++ s,
+  format!"/- Checking {non_auto_decls.length} declarations (plus {decls.length - non_auto_decls.length} automatically generated ones) {where_desc} -/\n\n" ++ s,
 let s := if slow then s else s ++ "/- (slow tests skipped) -/\n",
 let ns := name_set.of_list (do (_,_,rs) ← results, rs.keys),
 pure (ns, s)
@@ -769,16 +784,14 @@ meta def lint (slow : bool := tt) (verbose : bool := tt) (extra : list name := [
   (use_only : bool := ff) : tactic (name_set × format) := do
   checks ← get_checks slow extra use_only,
   e ← get_env,
-  let l := e.filter (λ d,
-    e.in_current_file' d.to_name ∧ ¬ d.to_name.is_internal ∧ ¬ d.is_auto_generated e),
+  let l := e.filter (λ d, e.in_current_file' d.to_name),
   lint_aux l none "in the current file" slow verbose checks
 
 /-- Returns the declarations considered by the mathlib linter. -/
 meta def lint_mathlib_decls : tactic (list declaration) := do
 e ← get_env,
 ml ← get_mathlib_dir,
-pure $ e.filter $ λ d,
-  e.is_prefix_of_file ml d.to_name ∧ ¬ d.to_name.is_internal ∧ ¬ d.is_auto_generated e
+pure $ e.filter $ λ d, e.is_prefix_of_file ml d.to_name
 
 /-- Return the message printed by `#lint_mathlib` and a `name_set` containing all declarations
 that fail. -/
@@ -795,7 +808,7 @@ meta def lint_all (slow : bool := tt) (verbose : bool := tt) (extra : list name 
   (use_only : bool := ff) : tactic (name_set × format) := do
   checks ← get_checks slow extra use_only,
   e ← get_env,
-  let l := e.filter (λ d, ¬ d.to_name.is_internal ∧ ¬ d.is_auto_generated e),
+  let l := e.get_decls,
   lint_aux l (some 0) "in all imported files (including this one)" slow verbose checks
 
 /-- Parses an optional `only`, followed by a sequence of zero or more identifiers.

src/topology/subset_properties.lean
@@ -655,12 +655,15 @@ closure_eq_iff_is_closed.1 $ eq_irreducible_component
 class preirreducible_space (α : Type u) [topological_space α] : Prop :=
 (is_preirreducible_univ : is_preirreducible (univ : set α))
 
+section prio
+set_option default_priority 100 -- see Note [default priority]
 /-- An irreducible space is one that is nonempty
 and where there is no non-trivial pair of disjoint opens. -/
 class irreducible_space (α : Type u) [topological_space α] extends preirreducible_space α : Prop :=
 (to_nonempty : nonempty α)
+end prio
 
-attribute [instance] irreducible_space.to_nonempty
+attribute [instance, priority 50] irreducible_space.to_nonempty -- see Note [lower instance priority]
 
 theorem nonempty_preirreducible_inter [preirreducible_space α] {s t : set α} :
   is_open s → is_open t → s.nonempty → t.nonempty → (s ∩ t).nonempty :=
@@ -991,11 +994,14 @@ subset_connected_component
 class preconnected_space (α : Type u) [topological_space α] : Prop :=
 (is_preconnected_univ : is_preconnected (univ : set α))
 
+section prio
+set_option default_priority 100 -- see Note [default priority]
 /-- A connected space is a nonempty one where there is no non-trivial open partition. -/
 class connected_space (α : Type u) [topological_space α] extends preconnected_space α : Prop :=
 (to_nonempty : nonempty α)
+end prio
 
-attribute [instance] connected_space.to_nonempty
+attribute [instance, priority 50] connected_space.to_nonempty -- see Note [lower instance priority]
 
 @[priority 100] -- see Note [lower instance priority]
 instance preirreducible_space.preconnected_space (α : Type u) [topological_space α]

commit 132bda1aeeceb9949c9e5cf2cf9be2f7a1707e30

fix(scripts/mk_nolint): fix error introduced by #2090

scripts/mk_nolint.lean
@@ -26,7 +26,7 @@ open native
 
 /-- Runs when called with `lean --run` -/
 meta def main : io unit := do
-e ← run_tactic get_env,
+e ← run_tactic tactic.get_env,
 decls ← run_tactic lint_mathlib_decls,
 let non_auto_decls := decls.filter (λ d, ¬ d.to_name.is_internal ∧ ¬ d.is_auto_generated e),
 linters ← run_tactic $ get_linters mathlib_linters,
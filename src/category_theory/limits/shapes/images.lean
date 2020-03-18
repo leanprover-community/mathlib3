dd5b9d61b9a24d6429ea2761aae4c32613039317
Scott Morrison <scott@tqft.net>
2020-03-18 11:20:39 -0700

    chore(category_theory/images): fix some minor problems

--- src/category_theory/limits/shapes/images.lean
+++ src/category_theory/limits/shapes/images.lean
@@ -126,13 +126,12 @@ def image : C := (image.mono_factorisation f).I
 def image.ι : image f ⟶ Y := (image.mono_factorisation f).m
 @[simp] lemma image.as_ι : (image.mono_factorisation f).m = image.ι f := rfl
 instance : mono (image.ι f) := (image.mono_factorisation f).m_mono
-/-- The 'corestriction' morphism from the source to the image. -/
-def image.c : X ⟶ image f := (image.mono_factorisation f).e
-@[simp] lemma image.as_c : (image.mono_factorisation f).e = image.c f := rfl
-@[simp] lemma image.c_ι : image.c f ≫ image.ι f = f := by erw (image.mono_factorisation f).fac
 
 /-- The map from the source to the image of a morphism. -/
 def factor_thru_image : X ⟶ image f := (image.mono_factorisation f).e
+@[simp]
+/-- Rewrite in terms of the `factor_thru_image` interface. -/
+lemma as_factor_thru_image : (image.mono_factorisation f).e = factor_thru_image f := rfl
 @[simp, reassoc]
 lemma image.fac : factor_thru_image f ≫ image.ι f = f := (image.mono_factorisation f).fac'
 
@@ -177,9 +176,9 @@ variables (C)
 /-- `has_images` represents a choice of image for every morphism -/
 class has_images :=
 (has_image : Π {X Y : C} (f : X ⟶ Y), has_image.{v} f)
+end
 
 attribute [instance, priority 100] has_images.has_image
-end
 
 section
 variables (f) [has_image f]

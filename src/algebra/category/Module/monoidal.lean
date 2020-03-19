f51e926b6cebb266d8c947ce3021931849020e6a
Scott Morrison <scott@tqft.net>
2020-03-19 13:05:06 -0700

    feat(algebra/category/Module/monoidal): simp lemmas

--- src/algebra/category/Module/monoidal.lean
+++ src/algebra/category/Module/monoidal.lean
@@ -67,7 +67,7 @@ end
 
 /-- (implementation) the left unitor for R-modules -/
 def left_unitor (M : Module R) : Module.of R (R ⊗[R] M) ≅ M :=
-(linear_equiv.to_Module_iso (tensor_product.lid R M) : of R (R ⊗ M) ≅ of R M).trans (of_self R M)
+(linear_equiv.to_Module_iso (tensor_product.lid R M) : of R (R ⊗ M) ≅ of R M).trans (of_self_iso M)
 
 lemma left_unitor_naturality {M N : Module R} (f : M ⟶ N) :
   tensor_hom (𝟙 (Module.of R R)) f ≫ (left_unitor N).hom = (left_unitor M).hom ≫ f :=
@@ -80,7 +80,7 @@ end
 
 /-- (implementation) the right unitor for R-modules -/
 def right_unitor (M : Module R) : Module.of R (M ⊗[R] R) ≅ M :=
-(linear_equiv.to_Module_iso (tensor_product.rid R M) : of R (M ⊗ R) ≅ of R M).trans (of_self R M)
+(linear_equiv.to_Module_iso (tensor_product.rid R M) : of R (M ⊗ R) ≅ of R M).trans (of_self_iso M)
 
 lemma right_unitor_naturality {M N : Module R} (f : M ⟶ N) :
   tensor_hom f (𝟙 (Module.of R R)) ≫ (right_unitor N).hom = (right_unitor M).hom ≫ f :=
@@ -107,7 +107,7 @@ end monoidal_category
 
 open monoidal_category
 
-instance : monoidal_category (Module.{u} R) :=
+instance Module.monoidal_category : monoidal_category (Module.{u} R) :=
 { -- data
   tensor_obj   := tensor_obj,
   tensor_hom   := @tensor_hom _ _,
@@ -123,5 +123,93 @@ instance : monoidal_category (Module.{u} R) :=
   right_unitor_naturality' := λ M N f, right_unitor_naturality f,
   pentagon'                := λ M N K L, pentagon M N K L,
   triangle'                := λ M N, triangle M N, }
+
+/-- Remind ourselves that the monoidal unit, being just `R`, is still a commutative ring. -/
+instance : comm_ring ((𝟙_ (Module R) : Module R) : Type u) := (by apply_instance : comm_ring R)
+
+namespace monoidal_category
+
+-- FIXME as far as I can see, the type ascription around the `(λ_ M).hom` in the statement
+-- of the next lemma is exactly its usual type. But without it, the coercion to a function fails?
+@[simp]
+lemma left_unitor_hom {M : Module.{u} R} (r : R) (m : M) : ((λ_ M).hom : 𝟙_ (Module R) ⊗ M ⟶ M) (r ⊗ₜ[R] m) = r • m :=
+begin
+  -- This is just weird and messed up.
+  dunfold Module.monoidal_category,
+  dsimp,
+  dunfold left_unitor,
+  erw [iso.trans_hom],
+  erw [Module.coe_comp],
+  -- dsimp, -- FIXME Inserting this dsimp causes a timeout. That's not good.
+  change ((of_self_iso M).hom) (((linear_equiv.to_Module_iso (tensor_product.lid.{u} R M)).hom) (r ⊗ₜ[R] m)) = _,
+  erw linear_equiv.to_Module_iso_hom,
+  erw tensor_product.lid_tmul,
+end
+@[simp]
+lemma right_unitor_hom {M : Module R} (r : R) (m : M) :
+  ((ρ_ M).hom : M ⊗ 𝟙_ (Module R) ⟶ M) (m ⊗ₜ r) = r • m :=
+begin
+  show (linear_equiv.to_linear_map (tensor_product.rid R ↥M)) (m ⊗ₜ[R] r) = r • m,
+  dunfold tensor_product.rid,
+  dunfold tensor_product.comm,
+  unfold_coes,
+  unfold linear_map.flip,
+  unfold_coes,
+  unfold linear_map.mk₂,
+  unfold tensor_product.mk,
+  dsimp,
+  unfold_coes,
+  unfold linear_equiv.of_linear,
+  unfold linear_equiv.trans,
+  dunfold linear_equiv.to_linear_map,
+  dsimp,
+  unfold tensor_product.lid,
+  unfold linear_equiv.of_linear,
+  dsimp,
+  unfold_coes,
+  unfold linear_map.lsmul,
+  unfold linear_map.mk₂,
+  dsimp,
+  squeeze_simp, -- tensor_product.lift.tmul used here
+  show (tensor_product.lift
+       {to_fun := λ (a : R), {to_fun := λ (m : ↥M), a • m, add := _, smul := _}, add := _, smul := _}).to_fun
+      (r ⊗ₜ[R] m) =
+    r • m,
+  squeeze_simp, -- tensor_product.lift.tmul'
+  refl,
+end
+
+-- alternative proof with added weird metavariable goals
+@[simp]
+lemma right_unitor_hom' {M : Module R} (r : R) (m : M) :
+  ((ρ_ M).hom : M ⊗ 𝟙_ (Module R) ⟶ M) (m ⊗ₜ r) = r • m :=
+begin
+  show (tensor_product.lift
+       {to_fun := λ (a : R), {to_fun := λ (m : ↥M), a • m, add := _, smul := _}, add := _, smul := _}).to_fun
+      ((tensor_product.lift
+          {to_fun := λ (m : ↥M), {to_fun := λ (m_1 : R), m_1 ⊗ₜ[R] m, add := _, smul := _},
+           add := _,
+           smul := _}).to_fun
+         (m ⊗ₜ[R] r)) =
+    r • m,
+  rw tensor_product.lift.tmul',
+  show (tensor_product.lift
+       {to_fun := λ (a : R), {to_fun := λ (m : ↥M), a • m, add := _, smul := _}, add := _, smul := _}).to_fun
+      (r ⊗ₜ[R] m) =
+    r • m,
+  rw tensor_product.lift.tmul',
+  refl, -- no goals!
+  recover, -- four goals!
+  { simp},
+  { intros, congr', intros, ext, squeeze_simp, apply tensor_product.tmul_add},
+  recover, -- two goals!
+  intros, rw tensor_product.smul_tmul c x m_1, simp, intros, apply tensor_product.add_tmul,
+end
+
+@[simp]
+lemma associator_hom {M N K : Module R} (m : M) (n : N) (k : K) :
+  ((α_ M N K).hom : (M ⊗ N) ⊗ K ⟶ M ⊗ (N ⊗ K)) ((m ⊗ₜ n) ⊗ₜ k) = (m ⊗ₜ (n ⊗ₜ k)) := rfl
+
+end monoidal_category
 
 end Module

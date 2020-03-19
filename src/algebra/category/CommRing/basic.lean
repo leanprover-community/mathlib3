0946e3224d988b42cdbd1a664c2c06ca63a07cc7
Scott Morrison <scott@tqft.net>
2020-03-19 15:28:52 -0700

    fixes

--- src/algebra/category/CommRing/basic.lean
+++ src/algebra/category/CommRing/basic.lean
@@ -49,6 +49,7 @@ instance (R : SemiRing) : semiring R := R.str
 instance bundled_hom : bundled_hom @ring_hom :=
 ⟨@ring_hom.to_fun, @ring_hom.id, @ring_hom.comp, @ring_hom.coe_inj⟩
 
+instance : category SemiRing := infer_instance -- short-circuit type class inference
 instance : concrete_category SemiRing := infer_instance -- short-circuit type class inference
 
 instance has_forget_to_Mon : has_forget₂ SemiRing Mon :=
@@ -77,6 +78,7 @@ instance : has_coe_to_sort Ring := infer_instance -- short-circuit type class in
 
 instance (R : Ring) : ring R := R.str
 
+instance : category Ring := infer_instance -- short-circuit type class inference
 instance : concrete_category Ring := infer_instance -- short-circuit type class inference
 
 instance has_forget_to_SemiRing : has_forget₂ Ring SemiRing := infer_instance  -- short-circuit type class inference
@@ -104,6 +106,7 @@ instance : has_coe_to_sort CommSemiRing := infer_instance -- short-circuit type 
 
 instance (R : CommSemiRing) : comm_semiring R := R.str
 
+instance : category CommSemiRing := infer_instance -- short-circuit type class inference
 instance : concrete_category CommSemiRing := infer_instance -- short-circuit type class inference
 
 instance has_forget_to_SemiRing : has_forget₂ CommSemiRing SemiRing := infer_instance -- short-circuit type class inference
@@ -132,6 +135,7 @@ instance : has_coe_to_sort CommRing := infer_instance -- short-circuit type clas
 
 instance (R : CommRing) : comm_ring R := R.str
 
+instance : category CommRing := infer_instance -- short-circuit type class inference
 instance : concrete_category CommRing := infer_instance -- short-circuit type class inference
 
 instance has_forget_to_Ring : has_forget₂ CommRing Ring := infer_instance -- short-circuit type class inference

546c854c0a237d14114f4e6a2237c3a04532db75
Gabriel Ebner <gebner@gebner.org>
2020-03-21 22:43:41 +0100

    Change suggested by Bryan.

--- src/tactic/doc_commands.lean
+++ src/tactic/doc_commands.lean
@@ -224,9 +224,11 @@ The argument to `add_tactic_doc` is a structure of type `tactic_doc_entry`.
 * The doc string is the body of the entry, it can be formatted with markdown.
   What you are reading now is the description of `add_tactic_doc`.
 
-If only one related declaration is listed in `decl_names` and it does not have a doc string,
-`description` will be automatically added as its doc string. If there are multiple declarations, you
-can select the one to be used by passing a name to the `inherit_description_from` field.
+If only one related declaration is listed in `decl_names` and if this
+invocation of `add_tactic_doc` does not have a doc string, the doc string of
+that declaration will become the body of the tactic doc entry. If there are
+multiple declarations, you can select the one to be used by passing a name to
+the `inherit_description_from` field.
 
 If you prefer a tactic to have a doc string that is different then the doc entry, then between
 the `/--` `-/` markers, write the desired doc string first, then `---` surrounded by new lines,

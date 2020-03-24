f574d364f9bf8b4473aa59f7ce458769e71f4ce3
Scott Morrison <scott@tqft.net>
2020-03-24 21:27:49 +1100

    clean up

--- src/tactic/show_term.lean
+++ src/tactic/show_term.lean
@@ -8,14 +8,9 @@ import tactic.tauto
 
 namespace tactic
 
-/-- Run a tactic, returning the goals as they were before running the tactic. -/
-meta def capture_terms {α : Type} (t : tactic α) : tactic (list expr × α) :=
-do
-  gs ← get_goals,
-  a ← t,
-  return (gs, a)
-
 /-- Run a tactic, and then return the pretty printed original goal. -/
+-- This could be inlined into the interactive `show_term`,
+-- but I've kept it separate so we can do silent testing.
 meta def pp_term {α : Type} (t : tactic α) : tactic string :=
 do
   g :: _ ← get_goals,
@@ -32,7 +27,9 @@ This is useful for understanding what tactics are doing,
 and how metavariables are handled.
 
 As an example, if the goal is `ℕ × ℕ`, `show_term { split, exact 0 }` will
-print `(0, ?m_1)`, indicating that the original goal has been partially filled in.
+print `(0, ?m_1)`, and afterwards there will be one remaining goal (of type `ℕ`).
+This indicates that `split, exact 0` partially filled in the original metavariable,
+but created a new metavariable for the resulting sub-goal.
 -/
 meta def show_term (t : itactic) : itactic :=
 pp_term t >>= trace

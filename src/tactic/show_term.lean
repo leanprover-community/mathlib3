1897671ea834875eb6ae62aeb7013ad902a5caf8
Scott Morrison <scott@tqft.net>
2020-03-24 11:19:19 +1100

    move tests

--- src/tactic/show_term.lean
+++ src/tactic/show_term.lean
@@ -1,3 +1,8 @@
+/-
+Copyright (c) 2020 Scott Morrison. All rights reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
+Authors: Scott Morrison
+-/
 import tactic.core
 import tactic.tauto
 
@@ -38,45 +43,3 @@ end interactive
 end tactic
 
 set_option trace.show_term true
-
--- Comment this out to observe the output below:
-set_option trace.show_term false
-
-example : ℕ → ℕ :=
-begin
-  show_term { intro n },
-  show_term { exact 0 },
-end
-
-example : ℕ × ℕ :=
-begin
-  show_term { split, },
-  exact 0, exact 1,
-end
-
-example : ℕ × ℕ :=
-begin
-  -- We test the output is as expected.
-  tactic.lock_tactic_state $ (do
-    s ← tactic.pp_term `[split, exact 0],
-    guard $ s = "(0, ?m_1)"),
-
-  show_term { split, exact 0, },
-  exact 1,
-end
-
-example (a b c : ℕ) : a + (b + c) = (a + c) + b :=
-begin
-  show_term { simp only [add_comm b c], },
-  show_term { simp only [add_assoc], },
-end
-
-example {P Q R : Prop} (h₁ : Q → P) (h₂ : R) (h₃ : R → Q) : P ∧ R :=
-begin
-  -- We test the output is as expected.
-  tactic.lock_tactic_state $ (do
-    s ← tactic.pp_term `[tauto],
-    guard $ s = "⟨h₁ (h₃ h₂), eq.mpr rfl h₂⟩"),
-
-  show_term { tauto },
-end

e1152541e3069faecf70ce36435d54301846a83f
Simon Hudon <simon.hudon@gmail.com>
2020-03-25 13:40:57 -0400

    fix tests

--- test/examples.lean
+++ test/examples.lean
@@ -258,6 +258,8 @@ by simp [finset.filter_or]
 
 end
 
+open tactic
+
 /--
 Demonstrate the packaged goals and how comparison of dependent goals
 works.
@@ -268,13 +270,13 @@ by do
   gs₀ ← retrieve $ tac >> get_goals,
   gs₁ ← retrieve $ tac >> get_goals,
   guard (gs₀ ≠ gs₁ : bool),
-  gs₀ ← get_proof_state_after $ tac <* all_goals (local_context >>= trace),
+  gs₀ ← get_proof_state_after tac,
   gs₁ ← get_proof_state_after tac,
   guard (gs₀ = gs₁),
-  gs₀ ← get_proof_state_after $ tac >> swap,
+  gs₀ ← get_proof_state_after $ tac >> tactic.swap,
   gs₁ ← get_proof_state_after tac,
   guard (gs₀ ≠ gs₁),
-  gs₀ ← get_proof_state_after $ tac >> swap,
-  gs₁ ← get_proof_state_after $ tac >> swap,
+  gs₀ ← get_proof_state_after $ tac >> tactic.swap,
+  gs₁ ← get_proof_state_after $ tac >> tactic.swap,
   guard (gs₀ = gs₁),
-  admit }
+  tactic.admit }

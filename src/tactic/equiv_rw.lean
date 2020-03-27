d35b2fc052129cee778dddde0fb6be55bf7d94e4
Scott Morrison <scott@tqft.net>
2020-03-27 22:56:32 +1100

    update

--- src/tactic/equiv_rw.lean
+++ src/tactic/equiv_rw.lean
@@ -159,7 +159,7 @@ do
     accept := λ goals, lock_tactic_state (do
       when_tracing `adapt_equiv (do
         goals.mmap pp >>= λ goals, trace format!"So far, we've built: {goals}"),
-      goals.any_of (eq.may_occur) <|>
+      goals.any_of (λ g, g.contains_expr_or_mvar eq) <|>
         (trace_if_enabled `adapt_equiv format!"Rejected, result does not contain {eq}" >> failed),
       done <|>
       when_tracing `adapt_equiv (do

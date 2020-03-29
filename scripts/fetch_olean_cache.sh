f7aa58ddedd25df85002a299f154fad485ca70f6
Bryan Gin-ge Chen <bryangingechen@gmail.com>
2020-03-29 00:49:47 -0400

    feat(ci): try fetching older branch oleans

--- scripts/fetch_olean_cache.sh
+++ scripts/fetch_olean_cache.sh
@@ -2,7 +2,21 @@ set -e
 set -x
 
 archive_url="https://oleanstorage.azureedge.net/mathlib/"
+MAX_REQUESTS=10
 
-new_git_sha=$(python scripts/look_up_olean_hash.py $lean_file_hash) || exit 0
-echo "equivalent Git sha: $new_git_sha"
+if new_git_sha=$(python scripts/look_up_olean_hash.py $lean_file_hash); then
+  echo "equivalent Git sha: $new_git_sha"
+else
+  for new_git_sha in $(git log --skip=1 -$MAX_REQUESTS --first-parent --pretty=format:%H)
+  do
+    if curl -fI "$archive_url$new_git_sha.tar.gz" ; then
+      echo "found ancestor Git sha: $new_git_sha"
+      break
+    fi
+    new_git_sha=""
+  done
+fi
+# exit if there were no successful requests
+[ "$new_git_sha" != "" ] || exit 0
+
 curl "$archive_url$new_git_sha.tar.gz" | tar xz src

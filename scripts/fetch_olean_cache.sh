063338ca10aaf5e36a1602de9407be8b1029ca3a
Bryan Gin-ge Chen <bryangingechen@gmail.com>
2020-03-29 01:57:49 -0400

    debug

--- scripts/fetch_olean_cache.sh
+++ scripts/fetch_olean_cache.sh
@@ -7,8 +7,10 @@ MAX_REQUESTS=10
 if new_git_sha=$(python scripts/look_up_olean_hash.py $lean_file_hash); then
   echo "equivalent Git sha: $new_git_sha"
 else
+  git log --skip=1 -$MAX_REQUESTS --first-parent --pretty=format:%H
   for new_git_sha in $(git log --skip=1 -$MAX_REQUESTS --first-parent --pretty=format:%H)
   do
+    echo "$new_git_sha"
     if curl -fI "$archive_url$new_git_sha.tar.gz" ; then
       echo "found ancestor Git sha: $new_git_sha"
       break

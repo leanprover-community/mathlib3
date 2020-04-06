c2a9afd971ef6ac861cdd52b10abbf32b2984e8b
Johan Commelin <johan@commelin.net>
2020-04-06 08:51:33 +0000

    Fooling around with definition of random graphs

--- src/graph_theory/erdos.lean
+++ src/graph_theory/erdos.lean
@@ -8,6 +8,7 @@ Authors: Johan Commelin
 
 import graph_theory.basic
 import measure_theory.probability_mass_function
+import .to_mathlib
 
 /-!
 # Graphs with high chromatic number and girth
@@ -20,10 +21,10 @@ universe variables v v₁ v₂ v₃ u u₁ u₂ u₃
 
 variables {V : Type u}
 
-def graph.weighted_edge_count [fintype V] (G : graph V) (p : nnreal) (hp : p ≤ 1) :
-  nnreal :=
-finset.prod finset.univ $ λ (xy : V × V),
-if xy.1 ~[G] xy.2 then p else (1 - p)
+-- def graph.weighted_edge_count [fintype V] (G : graph V) (p : nnreal) (hp : p ≤ 1) :
+--   nnreal :=
+-- finset.prod finset.univ $ λ (xy : V × V),
+-- if xy.1 ~[G] xy.2 then p else (1 - p)
 
 @[ext]
 lemma graph.ext (G₁ G₂ : graph V) (h : ∀ x y, (x ~[G₁] y) ↔ (x ~[G₂] y)) : G₁ = G₂ :=
@@ -33,19 +34,79 @@ begin
   cases this, congr, ext, refl,
 end
 
--- def graph.edges_as_sets (G : graph V) : set ({s : finset V // s.card = 2} ⊕ V)
--- | (sum.inl s) := ∃ (x y : V), x ∈ s.1 ∧ y ∈ s.1 ∧ x ≠ y ∧ (x ~[G] y)
--- | (sum.inr x) := x ~[G] x
+def graph_from_sets (E : set ({s : finset V // s.card = 1 ∨ s.card = 2})) : graph V :=
+{ edge := assume x y, ∃ e ∈ E, ({x,y} : finset V) = e,
+  symm :=
+  begin
+    rintro x y ⟨e, he, h⟩,
+    refine ⟨e, he, _⟩,
+    rw ← h,
+    finish [finset.ext],
+  end }
+.
 
--- def graph.foo (G : graph V) (x y : V) : {s : finset V // s.card = 2} ⊕ V :=
--- if h : x = y then sum.inr x else sum.inl ⟨{x,y}, by {  }⟩
+lemma graph_from_sets.aux {x y : V} : @finset.card V {x, y} = 1 ∨ @finset.card V {x, y} = 2 :=
+begin
+  classical,
+  by_cases h : x = y,
+  { simp only [h, finset.insert_singleton_self_eq', true_or, eq_self_iff_true,
+      finset.singleton_eq_singleton, finset.card_singleton], },
+  { right, rw finset.card_insert_of_not_mem, { refl },
+    simp only [ne.symm h, not_false_iff, finset.mem_singleton, finset.singleton_eq_singleton], }
+end
 
--- lemma graph.edges_as_sets_bijective : function.bijective (@graph.edges_as_sets V) :=
--- begin
---   split,
---   { assume G₁ G₂ h, ext, },
---   {  }
--- end
+lemma graph_from_sets_edge_iff (E : set ({s : finset V // s.card = 1 ∨ s.card = 2})) (x y : V) :
+  (x ~[graph_from_sets E] y) ↔
+  ((⟨{x, y}, graph_from_sets.aux⟩ : {s : finset V // s.card = 1 ∨ s.card = 2}) ∈ E) :=
+begin
+  split,
+  { rintro ⟨⟨s, hs⟩, hsE, h⟩, convert hsE },
+  { intro h, exact ⟨_, h, rfl⟩ }
+end
+
+lemma graph_from_sets.aux' (s : finset V) (hs : s.card = 1 ∨ s.card = 2) :
+  ∃ (x y : V), s = {x, y} :=
+begin
+  cases hs,
+  { rcases finset.card_eq_one.mp hs with ⟨x, hx⟩,
+    use [x, x], simp only [hx, finset.insert_singleton_self_eq', finset.singleton_eq_singleton] },
+  { rw finset.card_eq_succ at hs,
+    rcases hs with ⟨y, t, hy, hyt, ht⟩,
+    rcases finset.card_eq_one.mp ht with ⟨x, hx⟩,
+    use [x, y], finish, }
+end
+
+example (X : Type*) (a b c d : X) (h : ({a, b} : finset X) = {c, d}) :
+  (a = c ∧ b = d) ∨ (a = d ∧ b = c) := sorry
+
+lemma graph_from_sets_bijective : function.bijective (@graph_from_sets V) :=
+begin
+  split,
+  { assume E₁ E₂ h,
+    apply set.ext,
+    rintro ⟨s, hs⟩,
+    rcases graph_from_sets.aux' s hs with ⟨x, y, rfl⟩,
+    erw ← graph_from_sets_edge_iff E₁ x y,
+    erw ← graph_from_sets_edge_iff E₂ x y,
+    rw h, },
+  { assume G,
+    let E : set {s : finset V // s.card = 1 ∨ s.card = 2} :=
+      λ s, ∃ (x y : V), (x ~[G] y) ∧ s.1 = {x, y},
+    use E,
+    ext x y,
+    rw graph_from_sets_edge_iff,
+    split,
+    { rintro ⟨a, b, hab, H⟩,
+      suffices : (x = a ∧ y = b) ∨ (x = b ∧ y = a),
+      { rcases this with ⟨rfl, rfl⟩ | ⟨rfl, rfl⟩,
+        { exact hab },
+        { exact G.symm hab } },
+      rw classical.or_iff_not_imp_right,
+      simp at H,
+       },
+    { intro e, use [x, y, e] }
+  }
+end
 
 abbreviation random_graph (V : Type u) := pmf (graph V)
 
@@ -54,9 +115,7 @@ namespace random_graph
 /-- The Erdős–Rényi model for random graphs. -/
 def with_edge_prob (V : Type u) [fintype V] (p : nnreal) (hp : p ≤ 1) :
   random_graph V :=
-⟨λ G, G.weighted_edge_count p hp,
-begin
+pmf.map (graph_from_sets ∘ coe) $ pmf.binomial {s : finset V // s.card = 1 ∨ s.card = 2} p hp
 
-end⟩
 
 end random_graph
--- src/graph_theory/to_mathlib.lean
+++ src/graph_theory/to_mathlib.lean
@@ -30,4 +30,25 @@ begin
   exact finset.sum_le_sum hm,
 end
 
+@[simp]
+lemma finset.powerset_univ (X : Type*) [fintype X] :
+  (finset.univ : finset X).powerset = finset.univ :=
+by simp only [finset.eq_univ_iff_forall, finset.mem_powerset, finset.subset_univ, forall_const]
+
+section classical
+open_locale classical
+
+noncomputable def pmf.binomial (X : Type*) [fintype X] (p : nnreal) (hp : p ≤ 1) : pmf (finset X) :=
+⟨λ s, p ^ s.card * (1 - p) ^ (fintype.card X - s.card),
+begin
+  have p_add : p + (1 - p) = 1,
+  { rw add_comm, exact nnreal.sub_add_cancel_of_le hp },
+  have := finset.sum_pow_mul_eq_add_pow p (1 - p) (finset.univ : finset X),
+  rw [p_add, one_pow, finset.powerset_univ, finset.card_univ] at this,
+  conv { congr, skip, rw ← this },
+  apply has_sum_fintype,
+end⟩
+
+end classical
+
 end move_this

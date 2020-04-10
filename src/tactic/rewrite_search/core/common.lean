26ddf67bb7909c9d2bdfc2b2bff804fa26600bc1
Johan Commelin <johan@commelin.net>
2020-04-10 20:12:54 +0000

    Docstrings for 2nd half of core.common

--- src/tactic/rewrite_search/core/common.lean
+++ src/tactic/rewrite_search/core/common.lean
@@ -8,10 +8,12 @@ meta inductive how
 | rewrite (rule_index : ℕ) (location : ℕ) (addr : option (list side))
 | defeq
 | simp  -- TODO handle "explaining" me
+
 meta def how.to_string : how → format
 | (how.rewrite idx loc addr) := format!"rewrite {idx} {loc} {addr.iget.to_string}"
 | how.defeq := "(defeq)"
 | how.simp := "simp"
+
 meta instance how.has_to_format : has_to_format how := ⟨how.to_string⟩
 
 meta structure rewrite :=
@@ -22,13 +24,13 @@ meta structure rewrite :=
 namespace tactic.rewrite_search
 
 meta structure core_cfg :=
-(max_iterations  : ℕ := 500)
-(optimal         : bool := tt)
-(exhaustive      : bool := ff)
-(trace           : bool := ff)
-(trace_summary   : bool := ff)
-(trace_rules     : bool := ff)
-(explain         : bool := ff)
+(max_iterations     : ℕ := 500)
+(optimal            : bool := tt)
+(exhaustive         : bool := ff)
+(trace              : bool := ff)
+(trace_summary      : bool := ff)
+(trace_rules        : bool := ff)
+(explain            : bool := ff)
 (explain_using_conv : bool := tt)
 
 end tactic.rewrite_search
@@ -37,25 +39,32 @@ open tactic
 
 namespace rw_equation
 
+/-- Split an expression that is an equation or an iff into its rhs and lhs parts. -/
 meta def split : expr → tactic (expr × expr)
 | `(%%lhs = %%rhs) := return (lhs, rhs)
 | `(%%lhs ↔ %%rhs) := return (lhs, rhs)
 | _                := fail "target is not an equation or iff"
 
+/-- The left hand side of an expression (fails if the expression is not an equation or iff). -/
 meta def lhs (e : expr) : tactic expr := prod.fst <$> split e
 
+/-- The right hand side of an expression (fails if the expression is not an equation or iff). -/
 meta def rhs (e : expr) : tactic expr := prod.snd <$> split e
 
 end rw_equation
 
+/-- Returns true if expression is an equation or iff. -/
 meta def is_acceptable_rewrite : expr → bool
 | (expr.pi n bi d b) := is_acceptable_rewrite b
 | `(%%a = %%b)       := tt
 | `(%%a ↔ %%b)       := tt
 | _                  := ff
 
+/-- Returns true if the type of expression is an equation or iff. -/
 meta def is_acceptable_lemma (r : expr) : tactic bool :=
   is_acceptable_rewrite <$> (infer_type r >>= whnf)
 
+/-- Returns true if the type of expression is an equation or iff
+and does not contain metavariables. -/
 meta def is_acceptable_hyp (r : expr) : tactic bool :=
   do t ← infer_type r >>= whnf, return $ is_acceptable_rewrite t ∧ ¬t.has_meta_var

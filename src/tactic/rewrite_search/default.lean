05c96d32b57c997358965d15727cc8ac76d5a7bf
Johan Commelin <johan@commelin.net>
2020-04-10 20:37:04 +0000

    Little stubs of documentation

--- src/tactic/rewrite_search/default.lean
+++ src/tactic/rewrite_search/default.lean
@@ -5,3 +5,16 @@ import tactic.rewrite_search.interactive
 import tactic.rewrite_search.strategy
 import tactic.rewrite_search.metric
 import tactic.rewrite_search.tracer
+
+/-!
+# Searching for chains of rewrites
+
+`rewrite_search` is a tactic that attempts to rewrite
+the lhs and rhs of an equation or iff so that they become more similar.
+
+In this comment we will provide an overview of the code
+and pointers to more specific documentation.
+
+TODO: write this documentation.
+
+-/
--- src/tactic/rewrite_search/interactive.lean
+++ src/tactic/rewrite_search/interactive.lean
@@ -1,8 +1,14 @@
--- Copyright (c) 2018 Scott Morrison. All rights reserved.
--- Released under Apache 2.0 license as described in the file LICENSE.
--- Authors: Keeley Hoek, Scott Morrison
+/-
+Copyright (c) 2018 Scott Morrison. All rights reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
+Authors: Keeley Hoek, Scott Morrison
+-/
 
 import tactic.rewrite_search.tactic
+
+/-!
+# Interactive versions of rewrite tactics and documentation
+-/
 
 namespace tactic.interactive
 
@@ -11,22 +17,57 @@ open tactic.rewrite_search
 
 variables {α β γ δ : Type}
 
+/-- Search for a chain of rewrites to prove equations or iffs. -/
 meta def rewrite_search (try_harder : parse $ optional (tk "!"))
   (cfg : config α β γ δ . pick_default_config) : tactic string :=
   tactic.rewrite_search cfg try_harder.is_some
+
+add_tactic_doc
+{ name        := "rewrite_search",
+  category    := doc_category.tactic,
+  decl_names  := [`tactic.interactive.rewrite_search],
+  tags        := ["rewrite", "automation"] }
 
 meta def rewrite_search_with (try_harder : parse $ optional (tk "!")) (rs : parse rw_rules)
   (cfg : config α β γ δ . pick_default_config) : tactic string :=
   tactic.rewrite_search_with rs.rules cfg try_harder.is_some
 
+-- Uncomment this after adding a docstring.
+-- add_tactic_doc
+-- { name        := "rewrite_search_with",
+--   category    := doc_category.tactic,
+--   decl_names  := [`tactic.interactive.rewrite_search_with],
+--   tags        := ["rewrite", "automation"] }
+
 meta def rewrite_search_using (try_harder : parse $ optional (tk "!")) (as : list name)
   (cfg : config α β γ δ . pick_default_config) : tactic string :=
   tactic.rewrite_search_using as cfg try_harder.is_some
 
+-- Uncomment this after adding a docstring.
+-- add_tactic_doc
+-- { name        := "rewrite_search_using",
+--   category    := doc_category.tactic,
+--   decl_names  := [`tactic.interactive.rewrite_search_using],
+--   tags        := ["rewrite", "automation"] }
+
 meta def simp_search (cfg : collect_cfg := {}) : tactic unit :=
   tactic.simp_search cfg
 
+-- Uncomment this after adding a docstring.
+-- add_tactic_doc
+-- { name        := "simp_search",
+--   category    := doc_category.tactic,
+--   decl_names  := [`tactic.interactive.simp_search],
+--   tags        := ["simp", "automation"] }
+
 meta def simp_search_with (rs : parse rw_rules) (cfg : collect_cfg := {}) : tactic unit :=
   tactic.simp_search_with rs.rules cfg
+
+-- Uncomment this after adding a docstring.
+-- add_tactic_doc
+-- { name        := "simp_search_with",
+--   category    := doc_category.tactic,
+--   decl_names  := [`tactic.interactive.simp_search_with],
+--   tags        := ["simp", "automation"] }
 
 end tactic.interactive

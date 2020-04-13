38152f481c5799eab7eb3c9247ef789ff436f433
Gabriel Ebner <gebner@gebner.org>
2020-04-13 16:26:30 +0200

    Improve naming.

--- src/tactic/norm_cast.lean
+++ src/tactic/norm_cast.lean
@@ -36,10 +36,10 @@ This is a work around to the fact that in some cases
 mk_instance times out instead of failing,
 for example: `has_lift_t ℤ ℕ`
 
-`mk_instance'` is used when we assume the type class search
+`mk_instance_fast` is used when we assume the type class search
 should end instantly.
 -/
-meta def mk_instance' (e : expr) (timeout := 1000) : tactic expr :=
+meta def mk_instance_fast (e : expr) (timeout := 1000) : tactic expr :=
 try_for timeout (mk_instance e)
 
 end tactic
@@ -327,7 +327,7 @@ An expression of the shape: op (↑(x : α) : γ) (↑(y : β) : γ)
 is rewritten to:            op (↑(↑(x : α) : β) : γ) (↑(y : β) : γ)
 when (↑(↑(x : α) : β) : γ) = (↑(x : α) : γ) can be proven with a squash lemma
 -/
-meta def splitting_procedure : expr → tactic (unit × expr × expr)
+meta def splitting_procedure : expr → tactic (expr × expr)
 | (app (app op x) y) :=
 (do
   `(@coe %%α %%δ %%coe1 %%xx) ← return x,
@@ -336,74 +336,77 @@ meta def splitting_procedure : expr → tactic (unit × expr × expr)
   is_def_eq δ γ,
 
   (do
-    coe3 ← mk_app `has_lift_t [α, β] >>= mk_instance',
+    coe3 ← mk_app `has_lift_t [α, β] >>= mk_instance_fast,
     new_x ← to_expr ``(@coe %%β %%δ %%coe2 (@coe %%α %%β %%coe3 %%xx)),
     let new_e := app (app op new_x) y,
     eq_x ← aux_down x new_x,
     pr ← mk_congr_arg op eq_x,
     pr ← mk_congr_fun pr y,
-    return ((), new_e, pr)
+    return (new_e, pr)
   ) <|> (do
-    coe3 ← mk_app `has_lift_t [β, α] >>= mk_instance',
+    coe3 ← mk_app `has_lift_t [β, α] >>= mk_instance_fast,
     new_y ← to_expr ``(@coe %%α %%δ %%coe1 (@coe %%β %%α %%coe3 %%yy)),
     let new_e := app (app op x) new_y,
     eq_y ← aux_down y new_y,
     pr ← mk_congr_arg (app op x) eq_y,
-    return ((), new_e, pr)
+    return (new_e, pr)
   )
 ) <|> (do
   `(@coe %%α %%β %%coe1 %%xx) ← return x,
   `(@has_one.one %%β %%h1) ← return y,
-  h2 ← to_expr ``(has_one %%α) >>= mk_instance',
+  h2 ← to_expr ``(has_one %%α) >>= mk_instance_fast,
   new_y ← to_expr ``(@coe %%α %%β %%coe1 (@has_one.one %%α %%h2)),
   eq_y ← aux_down y new_y,
   let new_e := app (app op x) new_y,
   pr ← mk_congr_arg (app op x) eq_y,
-  return ((), new_e, pr)
+  return (new_e, pr)
  ) <|> (do
   `(@coe %%α %%β %%coe1 %%xx) ← return x,
   `(@has_zero.zero %%β %%h1) ← return y,
-  h2 ← to_expr ``(has_zero %%α) >>= mk_instance',
+  h2 ← to_expr ``(has_zero %%α) >>= mk_instance_fast,
   new_y ← to_expr ``(@coe %%α %%β %%coe1 (@has_zero.zero %%α %%h2)),
   eq_y ← aux_down y new_y,
   let new_e := app (app op x) new_y,
   pr ← mk_congr_arg (app op x) eq_y,
-  return ((), new_e, pr)
+  return (new_e, pr)
 ) <|> (do
   `(@has_one.one %%β %%h1) ← return x,
   `(@coe %%α %%β %%coe1 %%xx) ← return y,
-  h1 ← to_expr ``(has_one %%α) >>= mk_instance',
+  h1 ← to_expr ``(has_one %%α) >>= mk_instance_fast,
   new_x ← to_expr ``(@coe %%α %%β %%coe1 (@has_one.one %%α %%h1)),
   eq_x ← aux_down x new_x,
   let new_e := app (app op new_x) y,
   pr ← mk_congr_arg (lam `x binder_info.default β (app (app op (var 0)) y)) eq_x,
-  return ((), new_e, pr)
+  return (new_e, pr)
 ) <|> (do
   `(@has_zero.zero %%β %%h1) ← return x,
   `(@coe %%α %%β %%coe1 %%xx) ← return y,
-  h1 ← to_expr ``(has_zero %%α) >>= mk_instance',
+  h1 ← to_expr ``(has_zero %%α) >>= mk_instance_fast,
   new_x ← to_expr ``(@coe %%α %%β %%coe1 (@has_zero.zero %%α %%h1)),
   eq_x ← aux_down x new_x,
   let new_e := app (app op new_x) y,
   pr ← mk_congr_arg (lam `x binder_info.default β (app (app op (var 0)) y)) eq_x,
-  return ((), new_e, pr)
+  return (new_e, pr)
 )
 | _ := failed
 
 /--
-assumption is used to discharge proofs in step 2
+Discharging function used during simplification in the "squash" step.
+
 TODO: norm_cast takes a list of expressions to use as lemmas for the discharger
 TODO: a tactic to print the results the discharger fails to proove
 -/
-private meta def prove : tactic unit := assumption
+private meta def prove : tactic unit :=
+assumption
 
 /--
-This is an auxiliary function used in step 2.
+Core rewriting function used in the "squash" step, which moves casts upwards
+and eliminates them.
+
 It tries to rewrite an expression using the elim and move lemmas.
 On failure, it calls the splitting procedure heuristic.
 -/
-@[nolint unused_arguments] -- unused `unit` argument required by simplify
-meta def post (s : simp_lemmas) (_ : unit) (e : expr) : tactic (unit × expr × expr) :=
+meta def upward_and_elim (s : simp_lemmas) (e : expr) : tactic (expr × expr) :=
 (do
   r ← mcond (is_prop e) (return `iff) (return `eq),
   (new_e, pr) ← s.rewrite e prove r,
@@ -411,36 +414,40 @@ meta def post (s : simp_lemmas) (_ : unit) (e : expr) : tactic (unit × expr × 
   | `iff := mk_app `propext [pr]
   | _    := return pr
   end,
-  return ((), new_e, pr)
+  return (new_e, pr)
 ) <|> splitting_procedure e
 
 /-!
 The following auxiliary functions are used to handle numerals.
 -/
 
-/-- if possible, rewrite (n : α) to ((n : ℕ) : α) where n is a numeral and α ≠ ℕ -/
-@[nolint unused_arguments] -- unused `unit` argument required by simplify
-meta def aux_num_1 (_ : unit) (e : expr) : tactic (unit × expr × expr) :=
+/--
+If possible, rewrite `(n : α)` to `((n : ℕ) : α)` where `n` is a numeral and `α ≠ ℕ`.
+Returns a pair of the new expression and proof that they are equal.
+-/
+meta def numeral_to_coe (e : expr) : tactic (expr × expr) :=
 do
   α ← infer_type e,
   success_if_fail $ is_def_eq α `(ℕ),
   n ← e.to_nat,
-  h1 ← mk_app `has_lift_t [`(ℕ), α] >>= mk_instance',
-  new_e ← expr.of_nat `(ℕ) n,
+  h1 ← mk_app `has_lift_t [`(ℕ), α] >>= mk_instance_fast,
+  let new_e : expr := reflect n,
   new_e ← to_expr ``(@coe ℕ %%α %%h1 %%new_e),
   pr ← aux_down e new_e,
-  return ((), new_e, pr)
+  return (new_e, pr)
 
-/-- if possible, rewrite (↑n : α) to (n : α) where n is a numeral -/
-@[nolint unused_arguments] -- unused `unit` argument required by simplify
-meta def aux_num_2 (_ : unit) (e : expr) : tactic (unit × expr × expr) :=
+/--
+If possible, rewrite `((n : ℕ) : α)` to `(n : α)` where `n` is a numeral.
+Returns a pair of the new expression and proof that they are equal.
+-/
+meta def coe_to_numeral (e : expr) : tactic (expr × expr) :=
 do
   `(@coe ℕ %%α %%h1 %%e') ← return e,
   n ← e'.to_nat,
   new_e ← expr.of_nat α n,
   h ← to_expr ``(%%e = %%new_e),
   pr ← aux_down e new_e,
-  return ((), new_e, pr)
+  return (new_e, pr)
 
 /-- A local variant on `simplify_top_down`. -/
 private meta def simplify_top_down' {α} (a : α) (pre : α → expr → tactic (α × expr × expr))
@@ -471,18 +478,16 @@ do
   let e0 := e,
 
   -- step 1: pre-processing of numerals
-  ((), e1, pr1) ← simplify_top_down' () aux_num_1 e0 cfg,
+  ((), e1, pr1) ← simplify_top_down' () (λ _ e, prod.mk () <$> numeral_to_coe e) e0 cfg,
 
   -- step 2: casts are moved upwards and eliminated
-  let s2 := cache.up,
-  ((), e2, pr2) ← simplify_bottom_up () (post s2) e1 cfg,
+  ((), e2, pr2) ← simplify_bottom_up () (λ _ e, prod.mk () <$> upward_and_elim cache.up e) e1 cfg,
 
   -- step 3: casts are squashed
-  let s3 := cache.squash,
-  (e3, pr3) ← simplify s3 [] e2 cfg,
+  (e3, pr3) ← simplify cache.squash [] e2 cfg,
 
   -- step 4: post-processing of numerals
-  ((), e4, pr4) ← simplify_top_down' () aux_num_2 e3 cfg,
+  ((), e4, pr4) ← simplify_top_down' () (λ _ e, prod.mk () <$> coe_to_numeral e) e3 cfg,
 
   let new_e := e3,
   guard (¬ new_e =ₐ e),

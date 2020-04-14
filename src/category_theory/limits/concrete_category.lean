3698fe8c7a0f86927b6cc4ab0fc95320428f799c
Scott Morrison <scott.morrison@anu.edu.au>
2020-04-14 12:00:46 +1000

    minor

--- src/category_theory/limits/concrete_category.lean
+++ src/category_theory/limits/concrete_category.lean
@@ -19,15 +19,15 @@ namespace category_theory.limits
 -- We now prove a lemma about naturality of cones over functors into bundled categories.
 namespace cone
 
-variables {J' : Type u} [small_category J']
-variables {C' : Type (u+1)} [𝒞' : concrete_category C']
-include 𝒞'
+variables {J : Type u} [small_category J]
+variables {C : Type (u+1)} [large_category C] [𝒞 : concrete_category C]
+include 𝒞
 
 local attribute [instance] concrete_category.has_coe_to_sort
 local attribute [instance] concrete_category.has_coe_to_fun
 
 /-- Naturality of a cone over functors to a concrete category. -/
-@[simp] lemma naturality_concrete {G : J' ⥤ C'} (s : cone G) {j j' : J'} (f : j ⟶ j') (x : s.X) :
+@[simp] lemma naturality_concrete {G : J ⥤ C} (s : cone G) {j j' : J} (f : j ⟶ j') (x : s.X) :
    (G.map f) ((s.π.app j) x) = (s.π.app j') x :=
 begin
   convert congr_fun (congr_arg (λ k : s.X ⟶ G.obj j', (k : s.X → G.obj j')) (s.π.naturality f).symm) x;
@@ -38,15 +38,15 @@ end cone
 
 namespace cocone
 
-variables {J' : Type u} [small_category J']
-variables {C' : Type (u+1)} [𝒞' : concrete_category C']
-include 𝒞'
+variables {J : Type u} [small_category J]
+variables {C : Type (u+1)} [large_category C] [𝒞 : concrete_category C]
+include 𝒞
 
 local attribute [instance] concrete_category.has_coe_to_sort
 local attribute [instance] concrete_category.has_coe_to_fun
 
 /-- Naturality of a cocone over functors into a concrete category. -/
-@[simp] lemma naturality_concrete {G : J' ⥤ C'} (s : cocone G) {j j' : J'} (f : j ⟶ j') (x : G.obj j) :
+@[simp] lemma naturality_concrete {G : J ⥤ C} (s : cocone G) {j j' : J} (f : j ⟶ j') (x : G.obj j) :
   (s.ι.app j') ((G.map f) x) = (s.ι.app j) x :=
 begin
   convert congr_fun (congr_arg (λ k : G.obj j ⟶ s.X, (k : G.obj j → s.X)) (s.ι.naturality f)) x;

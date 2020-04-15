11833b744dff3fb342876bab81e60b2115cbd170
Scott Morrison <scott.morrison@anu.edu.au>
2020-04-15 20:54:02 +1000

    add author line, fix '

--- src/data/monoid_algebra.lean
+++ src/data/monoid_algebra.lean
@@ -1,7 +1,7 @@
 /-
 Copyright (c) 2017 Johannes Hölzl. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
-Authors: Johannes Hölzl, Scott Morrison
+Authors: Johannes Hölzl, Yury G. Kudryashov, Scott Morrison
 -/
 import data.finsupp ring_theory.algebra
 

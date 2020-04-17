40b0a6a977c27193058a2a5d3b864932d2419267
Bhavik Mehta <bhavikmehta8@gmail.com>
2020-04-17 17:35:20 +0100

    fixup

--- src/category_theory/limits/creates.lean
+++ src/category_theory/limits/creates.lean
@@ -4,7 +4,6 @@ Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Bhavik Mehta
 -/
 import category_theory.limits.limits
--- import category_theory.adjunction.limits
 import category_theory.reflect_isomorphisms
 
 open category_theory category_theory.limits

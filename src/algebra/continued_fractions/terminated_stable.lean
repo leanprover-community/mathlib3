cb87baaedf08d07deb26398c97e8d05ed9ed57e7
Kevin Kappelmann <accounts@kappelmann.me>
2020-04-19 09:59:58 +0200

    fix(continued_fractions) change relative to absolute import

--- src/algebra/continued_fractions/terminated_stable.lean
+++ src/algebra/continued_fractions/terminated_stable.lean
@@ -3,7 +3,7 @@ Copyright (c) 2020 Kevin Kappelmann. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Kevin Kappelmann
 -/
-import .translations
+import algebra.continued_fractions.translations
 /-!
 # Stabilisation of gcf Computations Under Termination
 

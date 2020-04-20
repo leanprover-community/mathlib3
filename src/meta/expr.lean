45e8be2ad61f448abd18aad2bfbac158b8296c13
Keeley Hoek <keeley@hoek.io>
2020-04-21 02:20:24 +1000

    Move rewrite-agnostic general machinery into the library

--- src/meta/expr.lean
+++ src/meta/expr.lean
@@ -618,6 +618,94 @@ meta def is_eta_expansion (val : expr) : tactic (option expr) := do
 
 end expr
 
+/-! ### Declarations about `expr_lens` -/
+
+/-- A "lens" for looking into the subterms of an expression, tracking where we've been, so that
+when we "zoom out" after making a change we know exactly which order of `congr_fun`s and
+`congr_arg`s we need to make things work.
+
+You're supposed to think of an `expr_lens` as a big set of nested applications with a single
+hole which needs to be filled, either in a function spot or argument spot. `expr_lens.fill` can
+fill this hole and turn your lens back into a real `expr`. -/
+meta inductive expr_lens
+| app_fun : expr_lens → expr → expr_lens
+| app_arg : expr_lens → expr → expr_lens
+| entire  : expr_lens
+
+namespace expr_lens
+
+open tactic
+
+/-- Sometimes `mk_congr_arg` fails, when the function is 'superficially dependent'.
+This hack `dsimp`s the function before building the `congr_arg` expression.
+
+Unfortunately it creates some dummy hypotheses that I can't work out how to dispose of cleanly. -/
+meta def mk_congr_arg_using_dsimp' (G W : expr) (u : list name) : tactic expr :=
+do s ← simp_lemmas.mk_default,
+   t ← infer_type G,
+   t' ← s.dsimplify u t {fail_if_unchanged := ff},
+   definev `_mk_congr_arg_aux t' G,
+   to_expr ```(congr_arg _mk_congr_arg_aux %%W)
+
+/-- Fill the function or argument hole in this lens with the given `expr`. -/
+meta def fill : expr_lens → expr → expr
+| entire        e := e
+| (app_fun l f) x := l.fill (expr.app f x)
+| (app_arg l x) f := l.fill (expr.app f x)
+
+private meta def trace_congr_error (f : expr) (x_eq : expr) : tactic unit :=
+do pp_f ← pp f,
+   pp_f_t ← (infer_type f >>= λ t, pp t),
+   pp_x_eq ← pp x_eq,
+   pp_x_eq_t ← (infer_type x_eq >>= λ t, pp t),
+   trace format!"expr_lens.congr failed on \n{pp_f} : {pp_f_t}\n{pp_x_eq} : {pp_x_eq_t}"
+
+/-- Turn an `e : expr_lens` and a proof that `a = b` into a series of `congr_arg` or `congr_fun`
+applications showing that the expressions obtained from `e.fill a` and `e.fill b` are equal. -/
+meta def congr : expr_lens → expr → tactic expr
+| entire e_eq        := pure e_eq
+| (app_fun l f) x_eq := do fx_eq ← try_core $ do {
+                             mk_congr_arg f x_eq
+                             <|> mk_congr_arg_using_dsimp' f x_eq [`has_coe_to_fun.F]
+                           },
+                           match fx_eq with
+                           | (some fx_eq) := l.congr fx_eq
+                           | none         := trace_congr_error f x_eq >> failed
+                           end
+| (app_arg l x) f_eq := mk_congr_fun f_eq x >>= l.congr
+
+/-- Pretty print a lens. -/
+meta def to_tactic_string : expr_lens → tactic string
+| entire := return "(entire)"
+| (app_fun l f) := do pp ← pp f,
+                      rest ← l.to_tactic_string,
+                      return sformat!"(fun \"{pp}\" {rest})"
+| (app_arg l x) := do pp ← pp x,
+                      rest ← l.to_tactic_string,
+                      return sformat!"(arg \"{pp}\" {rest})"
+
+end expr_lens
+
+/-- The private internal function used by `app_map`, which "does the work". -/
+private meta def app_map_aux {α} (F : expr_lens → expr → tactic (list α)) :
+  expr_lens → expr → tactic (list α)
+| l (expr.app f x) := list.join <$> monad.sequence [
+    F l (expr.app f x),
+    app_map_aux (expr_lens.app_arg l x) f,
+    app_map_aux (expr_lens.app_fun l f) x
+  ] <|> pure []
+| l e := F l e <|> pure []
+
+/-- Map a function `F` which understands `expr_lens`es over the given `e : expr` in the natural way;
+that is, make holes in `e` everywhere where that is possible (generating `expr_lens`es in the
+process), and at each stage call the function `F` passing both the `expr_lens` generated and the
+`expr` which was removed to make the hole.
+
+At each stage `F` returns a list of some type, and `app_map` collects these lists together and
+returns a concatenation of them all. -/
+meta def expr.app_map {α} (F : expr_lens → expr → tactic (list α)) : expr → tactic (list α) :=
+app_map_aux F expr_lens.entire
+
 /-! ### Declarations about `declaration` -/
 
 namespace declaration

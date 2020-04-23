7d9259fa903e2ff063ce9b6a30322ec3deaa9a1e
Vierkantor <vierkantor@vierkantor.com>
2020-04-23 14:58:35 +0000

    Make most `invertible` instances into `def`s

--- src/algebra/invertible.lean
+++ src/algebra/invertible.lean
@@ -19,9 +19,10 @@ like `ℤ[1/2]` where some inverses exist but there is no general `⁻¹` operat
 or to specify that a field has characteristic `≠ 2`.
 It is the `Type`-valued analogue to the `Prop`-valued `is_unit`.
 
-This file also includes some instances of `invertible`, notably for all elements
-of a group and for some specific numbers if the characteristic is zero.
-To find instances, `invertible_of_nonzero` is a useful definition.
+This file also includes some instances of `invertible` for specific numbers in
+characteristic zero. Some more cases are given as a `def`, to be included only
+when needed. To construct instances for concrete numbers,
+`invertible_of_nonzero` is a useful definition.
 
 ## Notation
 
@@ -71,40 +72,52 @@ by simp [mul_assoc]
 lemma left_inv_eq_right_inv [monoid α] {a b c : α} (hba : b * a = 1) (hac : a * c = 1) : b = c :=
 by rw [←one_mul c, ←hba, mul_assoc, hac, mul_one b]
 
+lemma inv_of_eq_right_inv [monoid α] {a b : α} [invertible a] (hac : a * b = 1) : ⅟a = b :=
+left_inv_eq_right_inv (inv_of_mul_self _) hac
+
 instance [monoid α] (a : α) : subsingleton (invertible a) :=
 ⟨ λ ⟨b, hba, hab⟩ ⟨c, hca, hac⟩, by { congr, exact left_inv_eq_right_inv hba hac } ⟩
 
 lemma is_unit_of_invertible [monoid α] (a : α) [invertible a] : is_unit a :=
 ⟨⟨a, ⅟a, mul_inv_of_self a, inv_of_mul_self a⟩, rfl⟩
 
-@[priority(100)]
-instance invertible_of_group [group α] (a : α) : invertible a :=
+/-- Each element of a group is invertible. -/
+def invertible_of_group [group α] (a : α) : invertible a :=
 ⟨a⁻¹, inv_mul_self a, mul_inv_self a⟩
 
-@[simp] lemma inv_of_eq_group_inv [group α] (a : α) : ⅟a = a⁻¹ :=
-rfl
+@[simp] lemma inv_of_eq_group_inv [group α] (a : α) [invertible a] : ⅟a = a⁻¹ :=
+inv_of_eq_right_inv (mul_inv_self a)
 
-instance invertible_one [monoid α] : invertible (1 : α) :=
+/-- `1` is the inverse of itself -/
+def invertible_one [monoid α] : invertible (1 : α) :=
 ⟨ 1, mul_one _, one_mul _ ⟩
 
-@[simp] lemma inv_of_one [monoid α] : ⅟(1 : α) = 1 := rfl
+@[simp] lemma inv_of_one [monoid α] [invertible (1 : α)] : ⅟(1 : α) = 1 :=
+inv_of_eq_right_inv (mul_one _)
 
-instance invertible_neg [ring α] (a : α) [invertible a] : invertible (-a) :=
+/-- `-⅟a` is the inverse of `-a` -/
+def invertible_neg [ring α] (a : α) [invertible a] : invertible (-a) :=
 ⟨ -⅟a, by simp, by simp ⟩
 
-@[simp] lemma inv_of_neg [ring α] (a : α) [invertible a] : ⅟(-a) = -⅟a := rfl
+@[simp] lemma inv_of_neg [ring α] (a : α) [invertible a] [invertible (-a)] : ⅟(-a) = -⅟a :=
+inv_of_eq_right_inv (by simp)
 
-instance invertible_inv_of [has_one α] [has_mul α] {a : α} [invertible a] : invertible (⅟a) :=
+/-- `a` is the inverse of `⅟a`. -/
+def invertible_inv_of [has_one α] [has_mul α] {a : α} [invertible a] : invertible (⅟a) :=
 ⟨ a, mul_inv_of_self a, inv_of_mul_self a ⟩
 
-@[simp] lemma inv_of_inv_of [has_one α] [has_mul α] {a : α} [invertible a] : ⅟(⅟a) = a :=
-rfl
+@[simp] lemma inv_of_inv_of [monoid α] {a : α} [invertible a] [invertible (⅟a)] :
+  ⅟(⅟a) = a :=
+inv_of_eq_right_inv (inv_of_mul_self _)
 
-instance invertible_mul [monoid α] (a b : α) [invertible a] [invertible b] : invertible (a * b) :=
+/-- `⅟b * ⅟a` is the inverse of `a * b` -/
+def invertible_mul [monoid α] (a b : α) [invertible a] [invertible b] : invertible (a * b) :=
 ⟨ ⅟b * ⅟a, by simp [←mul_assoc], by simp [←mul_assoc] ⟩
 
 @[simp]
-lemma inv_of_mul [monoid α] (a b : α) [invertible a] [invertible b] : ⅟(a * b) = ⅟b * ⅟a := rfl
+lemma inv_of_mul [monoid α] (a b : α) [invertible a] [invertible b] [invertible (a * b)] :
+  ⅟(a * b) = ⅟b * ⅟a :=
+inv_of_eq_right_inv (by simp [←mul_assoc])
 
 section group_with_zero
 
@@ -119,7 +132,7 @@ def invertible_of_nonzero {a : α} (h : a ≠ 0) : invertible a :=
 ⟨ a⁻¹, inv_mul_cancel' _ h, mul_inv_cancel' _ h ⟩
 
 @[simp] lemma inv_of_eq_inv (a : α) [invertible a] : ⅟a = a⁻¹ :=
-left_inv_eq_right_inv (inv_of_mul_self a) (mul_inv_cancel' _ (nonzero_of_invertible a))
+inv_of_eq_right_inv (mul_inv_cancel' _ (nonzero_of_invertible a))
 
 @[simp] lemma inv_mul_cancel_of_invertible (a : α) [invertible a] : a⁻¹ * a = 1 :=
 inv_mul_cancel' _ (nonzero_of_invertible a)
@@ -136,13 +149,16 @@ mul_div_cancel'' a (nonzero_of_invertible b)
 @[simp] lemma div_self_of_invertible (a : α) [invertible a] : a / a = 1 :=
 div_self' (nonzero_of_invertible a)
 
-instance invertible_div (a b : α) [invertible a] [invertible b] : invertible (a / b) :=
+/-- `b / a` is the inverse of `a / b` -/
+def invertible_div (a b : α) [invertible a] [invertible b] : invertible (a / b) :=
 ⟨ b / a, by simp [←mul_div_assoc''], by simp [←mul_div_assoc''] ⟩
 
-@[simp] lemma inv_of_div (a b : α) [invertible a] [invertible b] : ⅟(a / b) = b / a :=
-rfl
+@[simp] lemma inv_of_div (a b : α) [invertible a] [invertible b] [invertible (a / b)] :
+  ⅟(a / b) = b / a :=
+inv_of_eq_right_inv (by simp [←mul_div_assoc''])
 
-instance invertible_inv {a : α} [invertible a] : invertible (a⁻¹) :=
+/-- `a` is the inverse of `a⁻¹` -/
+def invertible_inv {a : α} [invertible a] : invertible (a⁻¹) :=
 ⟨ a, by simp, by simp ⟩
 
 end group_with_zero

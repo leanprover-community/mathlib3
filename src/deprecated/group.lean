c7327adfd5d979e65f1bdc214769af16177c9ccc
Johan Commelin <johan@commelin.net>
2020-04-23 20:59:26 +0200

    Update src/deprecated/group.lean

--- src/deprecated/group.lean
+++ src/deprecated/group.lean
@@ -2,8 +2,6 @@
 Copyright (c) 2019 Yury Kudryashov. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Author: Yury Kudryashov
-
-Supplementary theorems about the `string` type.
 -/
 import algebra.group.type_tags
 import algebra.group.is_unit

db09f5b4700353c540a6b36b2c4e7e1f1273d0ad
Scott Morrison <scott.morrison@gmail.com>
2020-04-25 06:52:43 +1000

    merge

--- src/tactic/nth_rewrite/congr.lean
+++ src/tactic/nth_rewrite/congr.lean
@@ -3,8 +3,6 @@ Copyright (c) 2018 Scott Morrison. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Keeley Hoek, Scott Morrison
 -/
-import data.mllist
-import tactic.core
 import tactic.nth_rewrite.basic
 
 namespace tactic

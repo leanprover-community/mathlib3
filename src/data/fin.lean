2798d129db7f79eb12aab91184bbf9028a22d7f1
Joseph Myers <jsm@polyomino.org.uk>
2020-04-25 18:41:15 +0000

    Use n + 1 in place of succ n.

--- src/data/fin.lean
+++ src/data/fin.lean
@@ -106,15 +106,15 @@ iff.rfl
 @[norm_cast] lemma coe_fin_le {n : ℕ} {a b : fin n} : (a : ℕ) ≤ (b : ℕ) ↔ a ≤ b :=
 iff.rfl
 
-/-- Converting an in-range number to `fin (succ n)` with `of_nat`
+/-- Converting an in-range number to `fin (n + 1)` with `of_nat`
 produces a result whose value is the original number.  -/
-lemma of_nat_val_of_lt {n : ℕ} {a : ℕ} (h : a < nat.succ n) :
-  ((of_nat a) : fin (nat.succ n)).val = a :=
+lemma of_nat_val_of_lt {n : ℕ} {a : ℕ} (h : a < n + 1) :
+  ((of_nat a) : fin (n + 1)).val = a :=
 nat.mod_eq_of_lt h
 
-/-- Converting the value of a `fin (succ n)` to `fin (succ n)` with `of_nat`
+/-- Converting the value of a `fin (n + 1)` to `fin (n + 1)` with `of_nat`
 results in the same value.  -/
-lemma of_nat_val_eq_self {n : ℕ} (a : fin (nat.succ n)) : of_nat a.val = a :=
+lemma of_nat_val_eq_self {n : ℕ} (a : fin (n + 1)) : of_nat a.val = a :=
 begin
   rw eq_iff_veq,
   exact of_nat_val_of_lt a.is_lt
@@ -122,17 +122,17 @@ end
 
 /-- `of_nat` of an in-range number, converted back to `ℕ`, is that
 number. -/
-lemma of_nat_coe_of_lt {n : ℕ} {a : ℕ} (h : a < nat.succ n) :
-  (((of_nat a) : fin (nat.succ n)) : ℕ) = a :=
+lemma of_nat_coe_of_lt {n : ℕ} {a : ℕ} (h : a < n + 1) :
+  (((of_nat a) : fin (n + 1)) : ℕ) = a :=
 nat.mod_eq_of_lt h
 
-/-- Converting a `fin (succ n)` to `ℕ` and back with `of_nat` results in
+/-- Converting a `fin (n + 1)` to `ℕ` and back with `of_nat` results in
 the same value. -/
-lemma of_nat_coe_eq_self {n : ℕ} (a : fin (nat.succ n)) : of_nat (a : ℕ) = a :=
+lemma of_nat_coe_eq_self {n : ℕ} (a : fin (n + 1)) : of_nat (a : ℕ) = a :=
 of_nat_val_eq_self a
 
 /-- `of_nat 0`, converted to `ℕ`, is 0. -/
-lemma of_nat_coe_zero {n : ℕ} : (((of_nat 0) : fin (nat.succ n)) : ℕ) = 0 :=
+lemma of_nat_coe_zero {n : ℕ} : (((of_nat 0) : fin (n + 1)) : ℕ) = 0 :=
 rfl
 
 /-- Assume `k = l`. If two functions defined on `fin k` and `fin l` are equal on each element,

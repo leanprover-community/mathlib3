15618ba3aac464442b0760d7f74ea243014daa4d
Chris Hughes <chrishughes24@gmail.com>
2020-04-27 14:41:41 +0100

    fix imports

--- src/ring_theory/algebra.lean
+++ src/ring_theory/algebra.lean
@@ -3,10 +3,8 @@ Copyright (c) 2018 Kenny Lau. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Kenny Lau, Yury Kudryashov
 -/
-
 import data.matrix.basic
 import linear_algebra.tensor_product
-import ring_theory.subring
 import algebra.commute
 
 /-!

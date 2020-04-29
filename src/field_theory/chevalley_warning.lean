8b2cf7d3796dc113003cb43e10b9c079fb373970
Johan Commelin <johan@commelin.net>
2020-04-29 13:14:00 +0000

    refactor proof

--- src/field_theory/chevalley_warning.lean
+++ src/field_theory/chevalley_warning.lean
@@ -7,6 +7,7 @@ Authors: Johan Commelin
 import data.mv_polynomial
 import algebra.pi_instances
 import field_theory.finite
+import tactic.nth_rewrite
 
 /-!
 # The Chevalley–Warning theorem
@@ -54,6 +55,77 @@ begin
   exact lt_of_lt_of_le hq (h _),
 end
 
+section
+
+variables {α : Type*} {β : Type*}
+
+@[simp] lemma equiv.sigma_preimage_equiv_apply (f : α → β) (x : Σ b, {x // f x = b}) :
+  (equiv.sigma_preimage_equiv f) x = x.2.1 := rfl
+
+def equiv.sum_compl (p : α → Prop) [decidable_pred p] :
+  {a // p a} ⊕ {a // ¬ p a} ≃ α :=
+{ to_fun := sum.elim coe coe,
+  inv_fun := λ a, if h : p a then sum.inl ⟨a, h⟩ else sum.inr ⟨a, h⟩,
+  left_inv := by { rintros (⟨x,hx⟩|⟨x,hx⟩); dsimp; [rw dif_pos, rw dif_neg], },
+  right_inv := λ a, by { dsimp, split_ifs; refl } }
+
+def equiv.foo (p : α → Prop) [decidable_pred p] (x₀ : {a // p a} → β) :
+  {x : α → β // x ∘ coe = x₀} ≃ ({a // ¬ p a} → β) :=
+{ to_fun := λ x, x.1 ∘ coe,
+  inv_fun := λ x, ⟨λ a, if h : p a then x₀ ⟨a, h⟩ else x ⟨a, h⟩,
+    funext $ λ ⟨a, h⟩, dif_pos h⟩,
+  left_inv := λ ⟨x, hx⟩, subtype.val_injective $ funext $ λ a,
+    (by { dsimp, split_ifs; [ rw ← hx, skip ]; refl }),
+  right_inv := λ x, funext $ λ ⟨a, h⟩, by { dsimp, rw [dif_neg h], } }
+
+variables (α)
+
+def equiv.bar [unique α] : (α → β) ≃ β :=
+{ to_fun := λ f, f (default α),
+  inv_fun := λ b a, b,
+  left_inv := λ f, funext $ λ a, congr_arg f $ subsingleton.elim _ _,
+  right_inv := λ b, rfl }
+
+end
+
+section
+
+variables {α : Type*} {γ : Type*} [fintype α] [comm_monoid γ]
+
+@[simp] lemma finsupp.prod_pow (f : α →₀ ℕ) (g : α → γ) :
+  f.prod (λ a b, g a ^ b) = univ.prod (λ a : α, g a ^ (f a)) :=
+begin
+  classical,
+  rw [finsupp.prod, ← fintype.prod_extend_by_one f.support, prod_congr rfl],
+  intros a ha,
+  split_ifs with h h,
+  { refl },
+  { rw [finsupp.not_mem_support_iff.mp h, pow_zero] }
+end
+
+end
+
+section
+
+variables {α : Type*} {α₁ : Type*} {α₂ : Type*} {β : Type*} [fintype α₁] [fintype α₂] [comm_monoid β]
+
+@[to_additive]
+lemma finset.prod_sum_univ (f : α₁ ⊕ α₂ → β) :
+  univ.prod f = univ.prod (f ∘ sum.inl) * univ.prod (f ∘ sum.inr) :=
+begin
+  classical,
+  let s : finset (α₁ ⊕ α₂) := univ.image sum.inr,
+  rw [← prod_sdiff (subset_univ s),
+      ← @prod_image (α₁ ⊕ α₂) _ _ _ _ _ _ sum.inl,
+      ← @prod_image (α₁ ⊕ α₂) _ _ _ _ _ _ sum.inr],
+  { congr, rw finset.ext, rintro (a|a);
+    { simp only [mem_image, exists_eq, mem_sdiff, mem_univ, exists_false,
+        exists_prop_of_true, not_false_iff, and_self, not_true, and_false], } },
+  all_goals { intros, solve_by_elim [sum.inl.inj, sum.inr.inj], }
+end
+
+end
+
 lemma sum_mv_polynomial_eq_zero [decidable_eq σ] (f : mv_polynomial σ K)
   (h : f.total_degree < (q - 1) * fintype.card σ) :
   univ.sum (λ x, f.eval x) = (0:K) :=
@@ -62,65 +134,45 @@ begin
   rw [sum_comm, sum_eq_zero],
   intros d hd,
   rw [← mul_sum, mul_eq_zero], right,
-  simp only [finsupp.prod],
   obtain ⟨i, hi⟩ : ∃ i, d i < q - 1, from exists_degree_lt_card_sub_one f h d hd,
+
   haveI : decidable_eq K := classical.dec_eq K,
-  let S : finset (σ → K) := univ.filter (λ x, x i = 0),
-  let stratified : finset (σ → K) := S.bind (λ x, univ.filter (λ y, ∀ j ≠ i, y j = x j)),
-  have : stratified = univ,
-  { simp only [eq_univ_iff_forall, mem_bind, true_and, exists_prop, mem_filter, mem_univ, ne.def],
-    intro x,
-    refine ⟨function.update x i 0, function.update_same _ _ _, _⟩,
-    intros j hj, exact (function.update_noteq hj _ _).symm },
-  rw [← this, sum_bind],
-  swap,
-  { rintros x hx y hy hxy z hz,
-    apply hxy,
-    funext j,
-    by_cases hj : j = i,
-    { subst j, rw mem_filter at hx hy, rw [hx.2, hy.2] },
-    { simp only [true_and, mem_filter, mem_univ, inf_eq_inter, ne.def, mem_inter] at hz,
-      cases hz with hzx hzy,
-      rw [← hzx j hj, ← hzy j hj] } },
-  rw sum_eq_zero,
-  intros x hx,
-  let T := d.support \ finset.singleton i,
-  let c := T.prod (λ j, x j ^ d j),
+  haveI : unique { j // ¬ j ≠ i } :=
+  { default := ⟨i, not_not.mpr rfl⟩,
+    uniq    := λ ⟨j, hj⟩, subtype.val_injective $ not_not.mp hj },
+
+  refine calc _ = univ.sum (λ (x₀ : {j // j ≠ i} → K),
+         univ.sum (λ (x : {x : σ → K // x ∘ coe = x₀}),
+              ((λ (x : σ → K), d.prod (λ (n : σ), pow (x n)))) x.1)) : _
+              ... = 0 : sum_eq_zero _,
+  { let e := equiv.sigma_preimage_equiv (λ x, x ∘ coe : (σ → K) → ({j // j ≠ i} → K)),
+    rw [← sum_equiv e, ← univ_sigma_univ, sum_sigma], refl, },
+
+  intros x₀ hx₀,
+  let c := univ.prod (λ j, x₀ j ^ d j),
   refine calc _ = univ.sum (λ a : K, c * a ^ d i) : _
             ... = c * univ.sum (λ a : K, a ^ d i) : by rw mul_sum
             ... = 0 : by rw [sum_pow_lt_card_sub_one _ hi, mul_zero],
-  symmetry,
-  apply sum_bij (λ a ha, function.update x i a),
-  { intros a ha, simp only [true_and, mem_filter, mem_univ, ne.def],
-    intros j hj, apply function.update_noteq hj },
-  { intros a ha,
-    by_cases hi : i ∈ d.support,
-    { rw ← singleton_subset_iff at hi,
-      show c * a ^ d i = d.support.prod (λ (j : σ), update x i a j ^ d j),
-      rw [← prod_sdiff hi, prod_singleton, function.update_same],
-      congr' 1,
-      apply prod_congr rfl,
-      simp only [and_imp, mem_sdiff, finsupp.mem_support_iff, ne.def, mem_singleton],
-      intros j hdj hj,
-      rw function.update_noteq hj, },
-    { have hT : T = d.support,
-      { apply sdiff_eq_self_of_disjoint, rwa ← disjoint_singleton at hi, },
-      rw finsupp.not_mem_support_iff at hi,
-      simp only [hi, mul_one, pow_zero, c, hT],
-      apply prod_congr rfl,
-      intros j hj,
-      rw function.update_noteq,
-      rintro rfl,
-      rw finsupp.mem_support_iff at hj,
-      contradiction } },
-  { intros a b ha hb H,
-    simpa only [function.update_same] using congr_fun H i, },
-  { intros y hy, refine ⟨y i, mem_univ _, _⟩,
-    funext j,
-    by_cases hj : j = i,
-    { subst j, exact (function.update_same i (y i) x).symm },
-    { simp only [true_and, mem_filter, mem_univ, ne.def] at hy,
-      simpa only [hy j hj] using (function.update_noteq hj (y i) x).symm, } },
+
+  let e' : {x // x ∘ coe = x₀} ≃ K := (equiv.foo _ x₀).trans (equiv.bar _),
+  rw [← sum_equiv e'.symm],
+
+  apply sum_congr rfl,
+  intros a ha,
+
+  let e'' : {j // j = i} ⊕ {j // j ≠ i} ≃ σ := equiv.sum_compl _,
+  dsimp,
+  rw [finsupp.prod_pow, ← prod_equiv e'', finset.prod_sum_univ, mul_comm],
+  congr' 1,
+  { apply prod_congr rfl,
+    rintros ⟨j, hj⟩ _,
+    dsimp [e'', equiv.sum_compl, equiv.foo, equiv.bar],
+    rw dif_pos hj, },
+  { letI : unique {j // j = i} :=
+    { default := ⟨i, rfl⟩, uniq := λ ⟨j, hj⟩, subtype.val_injective hj },
+    simp only [comp_app, prod_singleton, univ_unique, singleton_eq_singleton],
+    dsimp [e'', equiv.sum_compl, equiv.foo, equiv.bar],
+    rw dif_neg, { refl }, { rw not_not, refl } }
 end
 
 variables [decidable_eq K] [decidable_eq σ]

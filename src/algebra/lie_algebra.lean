907750ad497b04c16791884c207095c001f26f64
Oliver Nash <oliver.nash@gmail.com>
2020-04-29 22:54:16 +0100

    Golf proof of commutative_ring_iff_abelian_lie_ring
    I still feel like I'm fighting an idiom in this proof. Hoping for clarifiation in code review.

--- src/algebra/lie_algebra.lean
+++ src/algebra/lie_algebra.lean
@@ -168,15 +168,8 @@ lemma lie_ring.of_associative_ring_bracket (A : Type v) [ring A] (x y : A) :
 lemma commutative_ring_iff_abelian_lie_ring (A : Type v) [ring A] :
   is_commutative A ring.mul ↔ lie_algebra.is_abelian A :=
 begin
-  -- Proof is obviously ridiculous but we can come back to it.
-  have h₁ : is_commutative A ring.mul ↔ ∀ (a b : A), a * b = b * a := by {
-    split; intros h,
-    { exact h.comm, },
-    { exact ⟨h⟩, }, },
-  have h₂ : lie_algebra.is_abelian A ↔ ∀ (a b : A), ⁅a, b⁆ = 0 := by {
-    split; intros h,
-    { exact h.abelian, },
-    { exact ⟨h⟩, }, },
+  have h₁ : is_commutative A ring.mul ↔ ∀ (a b : A), a * b = b * a := ⟨λ h, h.1, λ h, ⟨h⟩⟩,
+  have h₂ : lie_algebra.is_abelian A ↔ ∀ (a b : A), ⁅a, b⁆ = 0 := ⟨λ h, h.1, λ h, ⟨h⟩⟩,
   simp only [h₁, h₂, lie_ring.of_associative_ring_bracket, sub_eq_zero],
 end
 

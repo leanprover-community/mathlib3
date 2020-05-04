db2d876ba540269ccf4611c76459ed6814a1b753
Johan Commelin <johan@commelin.net>
2020-05-04 14:32:54 +0000

    feat(ring_theory/ideals): the quotient map to the residue field as ring hom

--- src/ring_theory/ideals.lean
+++ src/ring_theory/ideals.lean
@@ -479,10 +479,14 @@ variables [local_ring α] [local_ring β]
 variable (α)
 def residue_field := (nonunits_ideal α).quotient
 
-namespace residue_field
-
-noncomputable instance : field (residue_field α) :=
+noncomputable instance residue_field.field : field (residue_field α) :=
 ideal.quotient.field (nonunits_ideal α)
+
+/-- The quotient map from a local ring to it's residue field. -/
+def residue : α →+* (residue_field α) :=
+ideal.quotient.mk_hom _
+
+namespace residue_field
 
 variables {α β}
 noncomputable def map (f : α →+* β) [is_local_ring_hom f] :

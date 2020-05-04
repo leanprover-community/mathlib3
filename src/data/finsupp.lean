ecba56dae2f1e6a73f718dd86d8fe8aeda0b3ee1
Johan Commelin <johan@commelin.net>
2020-05-04 20:50:50 +0000

    feat(*): lemmas on sums and products over fintypes

--- src/data/finsupp.lean
+++ src/data/finsupp.lean
@@ -39,7 +39,7 @@ This file defines `α →₀ β` as notation for `finsupp α β`.
 -/
 
 noncomputable theory
-open_locale classical
+open_locale classical big_operators
 
 open finset
 
@@ -462,6 +462,22 @@ by { dsimp [finsupp.prod], rw f.support.prod_ite_eq, }
 lemma prod_ite_eq' [has_zero β] [comm_monoid γ] (f : α →₀ β) (a : α) (b : α → β → γ) :
   f.prod (λ x v, ite (x = a) (b x v) 1) = ite (a ∈ f.support) (b a (f a)) 1 :=
 by { dsimp [finsupp.prod], rw f.support.prod_ite_eq', }
+
+@[simp] lemma prod_pow [fintype α] [comm_monoid γ] (f : α →₀ ℕ) (g : α → γ) :
+  f.prod (λ a b, g a ^ b) = ∏ a, g a ^ (f a) :=
+begin
+  classical,
+  calc f.prod (λ a b, g a ^ b) = ∏ a, (if a ∈ f.support then g a ^ f a else 1) : _
+    ... = _ : _,
+  { rw [finsupp.prod, ← finset.prod_filter, finset.prod_congr],
+    { ext, simp, },
+    { intros, refl } },
+  { rw [finset.prod_congr rfl],
+    intros a ha,
+    split_ifs with h h,
+    { refl },
+    { rw [finsupp.not_mem_support_iff.mp h, pow_zero] } }
+end
 
 section add_monoid
 variables [add_monoid β]

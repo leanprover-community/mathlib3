a826dae72a9e5f6c0b3c0c099462e1fe32f5e1c7
Johan Commelin <johan@commelin.net>
2020-05-16 04:53:22 +0000

    Move algebra from core to mathlib

---
+++ src/init/algebra/default.lean
@@ -0,0 +1,7 @@
+/-
+Copyright (c) 2016 Microsoft Corporation. All rights reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
+Authors: Leonardo de Moura
+-/
+import init.algebra.group init.algebra.ordered_group init.algebra.ring init.algebra.ordered_ring
+import init.algebra.field init.algebra.ordered_field init.algebra.norm_num init.algebra.functions

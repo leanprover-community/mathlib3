c3ff7d812c04d3e7bdce02f5517c9581d9d5c639
Smaug123 <patrick+github@patrickstevens.co.uk>
2020-05-16 20:52:25 +0100

    Generalise nat.dvd_choose

--- src/data/nat/choose.lean
+++ src/data/nat/choose.lean
@@ -9,12 +9,22 @@ open nat
 
 open_locale big_operators
 
-lemma nat.prime.dvd_choose {p k : ℕ} (hk : 0 < k) (hkp : k < p) (hp : prime p) : p ∣ choose p k :=
-have h₁ : p ∣ fact p, from hp.dvd_fact.2 (le_refl _),
-have h₂ : ¬p ∣ fact k, from mt hp.dvd_fact.1 (not_le_of_gt hkp),
-have h₃ : ¬p ∣ fact (p - k), from mt hp.dvd_fact.1 (not_le_of_gt (nat.sub_lt_self hp.pos hk)),
-by rw [← choose_mul_fact_mul_fact (le_of_lt hkp), mul_assoc, hp.dvd_mul, hp.dvd_mul] at h₁;
+lemma nat.prime.dvd_choose {p a b : ℕ} (hap : a < p) (hbp : b < p) (h : p ≤ a + b)
+  (hp : prime p) : p ∣ choose (a + b) a :=
+have h₁ : p ∣ fact (a + b), from hp.dvd_fact.2 h,
+have h₂ : ¬p ∣ fact a, from mt hp.dvd_fact.1 (not_le_of_gt hap),
+have h₃ : ¬p ∣ fact b, from mt hp.dvd_fact.1 (not_le_of_gt hbp),
+by
+  rw [← choose_mul_fact_mul_fact (le.intro rfl), mul_assoc, hp.dvd_mul, hp.dvd_mul,
+    norm_num.sub_nat_pos (a + b) a b rfl] at h₁;
   exact h₁.resolve_right (not_or_distrib.2 ⟨h₂, h₃⟩)
+
+lemma nat.prime.dvd_choose_self {p k : ℕ} (hk : 0 < k) (hkp : k < p) (hp : prime p) : p ∣ choose p k :=
+begin
+  have r : k + (p - k) = p, by omega,
+  have e : p ∣ choose (k + (p - k)) k, exact nat.prime.dvd_choose hkp (by omega) (by omega) hp,
+  {rw r at e, exact e}
+end
 
 /-- Show that choose is increasing for small values of the right argument. -/
 lemma choose_le_succ_of_lt_half_left {r n : ℕ} (h : r < n/2) :
@@ -100,8 +110,9 @@ theorem add_pow [comm_semiring α] (x y : α) (n : ℕ) :
 (commute.all x y).add_pow n
 
 /-- The sum of entries in a row of Pascal's triangle -/
-theorem sum_range_choose (n : ℕ) : 
+theorem sum_range_choose (n : ℕ) :
   ∑ m in range (n + 1), choose n m = 2 ^ n :=
 by simpa using (add_pow 1 1 n).symm
+
 
 end binomial

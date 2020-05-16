67c7f8a6052e883b5129d7734f051e297687d045
khanh93 <alexmaplegm@gmail.com>
2020-05-16 00:47:38 -0400

    indented begin-end block

--- archive/100-theorems-list/inverse-triangle-sum.lean
+++ archive/100-theorems-list/inverse-triangle-sum.lean
@@ -21,41 +21,41 @@ discrete_sum
 lemma inverse_triangle_sum (n : ℕ) :
 (finset.range n).sum (λ x, (2 : ℚ) / (x * (x + 1))) = if n ≤ 1 then 0 else 2 - (2 : ℚ) / n :=
 begin
-rw finset.sum_range_induction,
-  { rw if_pos, norm_num },
-intro n,
+  rw finset.sum_range_induction,
+    { rw if_pos, norm_num },
+  intro n,
 
-by_cases h0 : n = 0,
-  { rw [h0, if_pos, if_pos], ring,
-    norm_num, norm_num },
+  by_cases h0 : n = 0,
+    { rw [h0, if_pos, if_pos], ring,
+      norm_num, norm_num },
 
-by_cases h1 : n = 1,
-  { rw [h1, if_neg, if_pos], ring,
-    norm_num, norm_num },
+  by_cases h1 : n = 1,
+    { rw [h1, if_neg, if_pos], ring,
+      norm_num, norm_num },
 
--- we're going to do arithmetic where n-1, n, and n+1 all appear in denominators, so let's show that's okay
-have n0 : (n : ℚ) ≠ 0 := by {norm_cast, exact h0}, clear h0,
-have n1 : ((n + 1) : ℚ) ≠ 0 := by {norm_cast, omega},
-have nn1 : (n - 1 : ℚ) ≠ 0,
-  { intro h, revert h1, rw [imp_false, not_not],
-  apply_fun (λ x, x + 1) at h,
-  simp only [sub_add_cancel, zero_add] at h,
-  norm_cast at h, exact h },
+  -- we're going to do arithmetic where n-1, n, and n+1 all appear in denominators, so let's show that's okay
+  have n0 : (n : ℚ) ≠ 0 := by {norm_cast, exact h0}, clear h0,
+  have n1 : ((n + 1) : ℚ) ≠ 0 := by {norm_cast, omega},
+  have nn1 : (n - 1 : ℚ) ≠ 0,
+    { intro h, revert h1, rw [imp_false, not_not],
+    apply_fun (λ x, x + 1) at h,
+    simp only [sub_add_cancel, zero_add] at h,
+    norm_cast at h, exact h },
 
--- let's clear our if-then-else
-rw if_neg, swap,
-  { revert h1 n0, norm_cast, omega },
-rw if_neg, swap,
-  { revert n0, norm_cast, omega },
-clear h1,
+  -- let's clear our if-then-else
+  rw if_neg, swap,
+    { revert h1 n0, norm_cast, omega },
+  rw if_neg, swap,
+    { revert n0, norm_cast, omega },
+  clear h1,
 
--- let's leave ℕ now
-have coe_elim : ∃ x : ℚ, (n : ℚ) = x, simp only [exists_eq'],
-cases coe_elim with x coe_elim,
-simp only [one_div_eq_inv, add_sub_cancel, nat.cast_add, nat.cast_one],
-rw coe_elim at *,
-clear coe_elim n,
+  -- let's leave ℕ now
+  have coe_elim : ∃ x : ℚ, (n : ℚ) = x, simp only [exists_eq'],
+  cases coe_elim with x coe_elim,
+  simp only [one_div_eq_inv, add_sub_cancel, nat.cast_add, nat.cast_one],
+  rw coe_elim at *,
+  clear coe_elim n,
 
--- now that we're morally in (units ℚ), our tactics can do the arithmetic
-field_simp [nn1, n0, n1], ring,
+  -- now that we're morally in (units ℚ), our tactics can do the arithmetic
+  field_simp [nn1, n0, n1], ring,
 end

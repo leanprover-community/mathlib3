2f976028f7003aaa5d6f7ffae32a50f95ea5db3f
Vierkantor <vierkantor@vierkantor.com>
2020-05-19 15:58:24 +0000

    Show principal ideal domains are dedekind domains
    TODO: clean up

--- src/ring_theory/dedekind_domain.lean
+++ src/ring_theory/dedekind_domain.lean
@@ -1,18 +1,19 @@
 import ring_theory.fractional_ideal
 import ring_theory.principal_ideal_domain
 
-universes u
+universes u v
 
 open localization
 
 namespace ring
 
-/-- A dedekind domain is an integral domain that satisfies one of some equivalent conditions.
+variables {R : Type u} {K : Type v} [field K]
 
-  In this case we use that all nonzero fractional ideals are invertible. -/
-class dedekind_domain (R : Type u) extends integral_domain R :=
-(ideal_mul_inv_cancel : ∀ {I : fractional_ideal R (non_zero_divisors R)}, I ≠ 0 → I * I⁻¹ = 1)
+/-- Condition (DD3) of being a Dedekind domain: all nonzero fractional ideals are invertible. -/
+def DD3 [integral_domain R] (g : fraction_map R K) : Prop :=
+∀ {I : fractional_ideal g}, I ≠ 0 → I * I⁻¹ = 1
 
-variables {R : Type u} [integral_domain R] {I : fractional_ideal R (non_zero_divisors R)}
+lemma DD3_of_principal_ideal_domain [principal_ideal_domain R] (g : fraction_map R K) : DD3 g :=
+λ I hI, fractional_ideal.invertible_of_principal I hI
 
 end ring

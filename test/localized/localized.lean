421a752ccacb4a83ab8c87eca830772526a4c9ff
Rob Lewis <Rob.y.lewis@gmail.com>
2020-05-19 09:56:38 +0200

    Update localized.lean

--- test/localized/localized.lean
+++ test/localized/localized.lean
@@ -25,7 +25,8 @@ example : 2 ↓ 3 = 8 := rfl
 example : 2 ⊖ 3 = 8 := rfl
 example {n m : ℕ} (h : n < m) : n ≤ m := by { success_if_fail { simp [h] }, exact le_of_lt h }
 
-run_cmd success_if_fail $ get_localized [`int]
+-- test that `open_locale` will fail when given a nonexistent locale
+run_cmd success_if_fail $ get_localized [`ceci_nest_pas_une_locale]
 
 open_locale nat
 example : 2 ⊹ 3 = 5 := rfl

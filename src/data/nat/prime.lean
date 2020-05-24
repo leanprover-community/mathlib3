91cd5178ed36eb4b290c7cb93895f003a0e4fadf
Scott Morrison <scott@tqft.net>
2020-05-24 23:26:26 +1000

    Update src/data/nat/prime.lean
    Co-authored-by: Bryan Gin-ge Chen <bryangingechen@gmail.com>

--- src/data/nat/prime.lean
+++ src/data/nat/prime.lean
@@ -256,13 +256,11 @@ section min_fac
   The square of the smallest prime factor of a composite number n is at most n.
   -/
   lemma min_fac_sq_le_self (n : ℕ) (w : 0 < n) (h : ¬ prime n) : (min_fac n)^2 ≤ n :=
-  begin
-    have t : (min_fac n) ≤ (n/min_fac n) := min_fac_le_div w h,
-    calc
-    (min_fac n)^2 = (min_fac n) * (min_fac n)   : pow_two (min_fac n)
-              ... ≤ (n/min_fac n) * (min_fac n) : mul_le_mul_right (min_fac n) t
-              ... ≤ n                           : div_mul_le_self n (min_fac n)
-  end
+  have t : (min_fac n) ≤ (n/min_fac n) := min_fac_le_div w h,
+  calc
+  (min_fac n)^2 = (min_fac n) * (min_fac n)   : pow_two (min_fac n)
+            ... ≤ (n/min_fac n) * (min_fac n) : mul_le_mul_right (min_fac n) t
+            ... ≤ n                           : div_mul_le_self n (min_fac n)
 
   @[simp]
   lemma min_fac_eq_one_iff (n : ℕ) : min_fac n = 1 ↔ n = 1 :=

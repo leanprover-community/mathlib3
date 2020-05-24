bdb0dcee40b512e270813a4895963eb29aea54ee
kl-i <kl.i@icloud.com>
2020-05-24 17:44:34 +0100

    feat(ring_theory/ideals): prod_dvd_of_coprime

--- src/data/polynomial.lean
+++ src/data/polynomial.lean
@@ -2326,6 +2326,14 @@ prime_of_associated normalize_associated this
 lemma irreducible_of_degree_eq_one (hp1 : degree p = 1) : irreducible p :=
 irreducible_of_prime (prime_of_degree_eq_one hp1)
 
+theorem pairwise_coprime_X_sub {α : Type u} [field α] {I : Type v}
+  {s : I → α} (H : function.injective s) :
+  ideal.pairwise_coprime (λ i : I, polynomial.X - polynomial.C (s i)) :=
+λ i j hij, have h : s j - s i ≠ 0, from sub_ne_zero_of_ne $ function.injective.ne H hij.symm,
+ideal.is_coprime_def'.2  ⟨polynomial.C (s j - s i)⁻¹, -polynomial.C (s j - s i)⁻¹,
+by rw [neg_mul_eq_neg_mul_symm, ← sub_eq_add_neg, ← mul_sub, sub_sub_sub_cancel_left,
+    ← polynomial.C_sub, ← polynomial.C_mul, inv_mul_cancel h, polynomial.C_1]⟩
+
 end field
 
 section derivative

7b6d7389d1d96f4c80db9665815570096b2219e6
Reid Barton <rwbarton@gmail.com>
2020-05-25 21:37:29 +0000

    feat(tactic/with_local_reducibility): alter reducibility attributes locally

---
+++ src/tactic/with_local_reducibility.lean
@@ -0,0 +1,46 @@
+import tactic.core
+
+namespace tactic
+
+/-- Possible reducibility attributes for a declaration:
+reducible, semireducible (the default), irreducible. -/
+@[derive decidable_eq]
+inductive decl_reducibility
+| reducible
+| semireducible
+| irreducible
+
+/-- Get the reducibility attribute of a declaration.
+Fails if the name does not refer to an existing declaration. -/
+meta def get_decl_reducibility (n : name) : tactic decl_reducibility :=
+do is_irred ← has_attribute' `irreducible n,
+   if is_irred then pure decl_reducibility.irreducible else
+do is_red ← has_attribute' `reducible n,
+   if is_red then pure decl_reducibility.reducible else
+do e ← get_env,
+   if e.contains n then pure decl_reducibility.semireducible else
+   fail format!"get_decl_reducibility: no declaration {n}"
+
+/-- Return the attribute (as a `name`) corresponding to a reducibility level. -/
+def decl_reducibility.to_attribute : decl_reducibility → name
+| decl_reducibility.reducible := `reducible
+| decl_reducibility.semireducible := `semireducible
+| decl_reducibility.irreducible := `irreducible
+
+-- Note: even though semireducible definitions don't have the `semireducible` attribute
+-- (according to `has_attribute`), setting `semireducible` still has the intended effect
+-- of clearing `reducible`/`irreducible`.
+
+/-- Set the reducibility attribute of a declaration.
+If `persistent := ff`, this is scoped to the enclosing `section`, like `local attribute`. -/
+meta def set_decl_reducibility (n : name) (r : decl_reducibility) (persistent := ff)
+  : tactic unit :=
+set_basic_attribute r.to_attribute n persistent
+
+/-- Execute a tactic with a temporarily modified reducibility attribute for a declaration. -/
+meta def with_local_reducibility {α : Type*} (n : name) (r : decl_reducibility)
+  (body : tactic α) : tactic α :=
+do r' ← get_decl_reducibility n,
+   bracket (set_decl_reducibility n r) body (set_decl_reducibility n r')
+
+end tactic

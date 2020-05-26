92a5eed467081b6a167951359fd5e04a8c384887
Joseph Myers <jsm@polyomino.org.uk>
2020-05-26 00:36:41 +0000

    Add metric_space_of_normed_group_of_add_torsor.

--- src/analysis/normed_space/add_torsor.lean
+++ src/analysis/normed_space/add_torsor.lean
@@ -36,3 +36,34 @@ lemma norm_dist (V : Type*) {P : Type*} [normed_group V] [metric_space P] [norme
     (x y : P) :
   dist x y = ∥(x -ᵥ y : V)∥ :=
 normed_add_torsor.norm_dist' x y
+
+open add_torsor
+
+/-- The distance defines a metric space structure on the torsor. This
+is not an instance because it depends on `V` to define a `metric_space
+P`. -/
+def metric_space_of_normed_group_of_add_torsor (V : Type*) (P : Type*) [normed_group V]
+    [add_torsor V P] : metric_space P :=
+{ dist := λ x y, ∥(x -ᵥ y : V)∥,
+  dist_self := begin
+    intro p,
+    change ∥p -ᵥ p∥ = 0,
+    rw [vsub_self, norm_zero]
+  end,
+  eq_of_dist_eq_zero := begin
+    intros p1 p2 h,
+    rw norm_eq_zero at h,
+    exact eq_of_vsub_eq_zero V h
+  end,
+  dist_comm := begin
+    intros x y,
+    change ∥x -ᵥ y∥ = ∥y -ᵥ x∥,
+    convert norm_neg (y -ᵥ x),
+    exact (neg_vsub_eq_vsub_rev V y x).symm
+  end,
+  dist_triangle := begin
+    intros x y z,
+    change ∥x -ᵥ z∥ ≤ ∥x -ᵥ y∥ + ∥y -ᵥ z∥,
+    rw ←vsub_add_vsub_cancel,
+    apply norm_add_le
+  end }

a002958af31e616b743f1bb773816f7fffec93c5
Scott Morrison <scott@tqft.net>
2020-05-27 18:49:31 +1000

    fix

--- src/category_theory/abelian/schur.lean
+++ src/category_theory/abelian/schur.lean
@@ -4,7 +4,7 @@ Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Scott Morrison
 -/
 import category_theory.simple
-import category_theory.abelian.abelian
+import category_theory.abelian.basic
 
 /-!
 We prove Schur's Lemma,

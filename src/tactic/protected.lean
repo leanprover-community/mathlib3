5f671563d3c66029fb01c8442ac81a222a2b3b3a
Rob Lewis <rob.y.lewis@gmail.com>
2020-05-29 18:08:14 +0200

    doc changes

--- src/tactic/protected.lean
+++ src/tactic/protected.lean
@@ -39,6 +39,10 @@ namespace tactic
 Attribute to protect a declaration.
 If a declaration `foo.bar` is marked protected, then it must be referred to
 by its full name `foo.bar`, even when the `foo` namespace is open.
+
+Protectedness is a built in parser feature that is independent of this attribute.
+A declaration may be protected even if it does not have the `@[protected]` attribute.
+This provides a convenient way to protect many declarations at once.
 -/
 @[user_attribute] meta def protected_attr : user_attribute :=
 { name := "protected",
@@ -51,7 +55,7 @@ add_tactic_doc
 { name        := "protected",
   category    := doc_category.attr,
   decl_names  := [`tactic.protected_attr],
-  tags        := ["user attribute"] }
+  tags        := ["parsing", "environment"] }
 
 /-- Tactic that is executed when a structure is marked with the `protect_proj` attribute -/
 meta def protect_proj_tac (n : name) (l : list name) : tactic unit :=
@@ -62,12 +66,19 @@ match env.structure_fields_full n with
     when (l.all $ λ m, bnot $ m.is_suffix_of field) $ mk_protected field
 end
 
-/-- Attribute to protect the projections of a structure.
-    If a structure `foo` is marked with the `protect_proj` user attribute, then
-    all of the projections become protected, meaning they must always be referred to by
-    their full name `foo.bar`, even when the `foo` namespace is open.
+/--
+Attribute to protect the projections of a structure.
+If a structure `foo` is marked with the `protect_proj` user attribute, then
+all of the projections become protected, meaning they must always be referred to by
+their full name `foo.bar`, even when the `foo` namespace is open.
 
-    `protect_proj without bar baz` will protect all projections except for bar and baz -/
+`protect_proj without bar baz` will protect all projections except for `bar` and `baz`.
+
+```lean
+@[protect_proj without baz bar] structure foo : Type :=
+(bar : unit) (baz : unit) (qux : unit)
+```
+-/
 @[user_attribute] meta def protect_proj_attr : user_attribute unit (list name) :=
 { name := "protect_proj",
   descr := "Attribute to protect the projections of a structure.
@@ -84,6 +95,6 @@ add_tactic_doc
 { name        := "protect_proj",
   category    := doc_category.attr,
   decl_names  := [`tactic.protect_proj_attr],
-  tags        := ["user attribute", "structures"] }
+  tags        := ["parsing", "environment", "structures"] }
 
 end tactic

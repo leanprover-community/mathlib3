a2c954e40ccb965a55d97ce10154833fbcf08901
Chris Hughes <chrishughes24@gmail.com>
2020-05-29 12:00:51 +0100

    fix import of tactic.protected

--- src/tactic/protected.lean
+++ src/tactic/protected.lean
@@ -3,7 +3,7 @@ Copyright (c) 2020 Chris Hughes. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Chris Hughes
 -/
-import tactic.suggest tactic.find
+import tactic.core
 /-!
 ## `protected` and `protect_proj` user attributes
 

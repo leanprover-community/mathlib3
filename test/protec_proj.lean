69ef16a4f0219240e08e7ce2e40802678ff03bd9
Rob Lewis <rob.y.lewis@gmail.com>
2020-05-29 18:04:47 +0200

    strengthen tests

--- test/protec_proj.lean
+++ test/protec_proj.lean
@@ -6,8 +6,24 @@ Authors: Chris Hughes
 
 import tactic.protected
 
+open tactic
+
 @[protect_proj without baz bar] structure foo : Type :=
 (bar : unit) (baz : unit) (qux : unit)
 
+open foo
+
+#check bar
+#check foo.qux
+
+run_cmd success_if_fail $ resolve_name `qux
+
 @[protect_proj] structure X : Type :=
 (n : nat) (i : fin n)
+
+open X
+
+#check X.i
+#check X.n
+
+run_cmd success_if_fail $ resolve_name `n

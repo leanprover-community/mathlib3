613055ad3f89a699067282e697629041445650f1
Scott Morrison <scott.morrison@gmail.com>
2020-05-30 14:26:23 +1000

    add Markus

--- src/category_theory/schur.lean
+++ src/category_theory/schur.lean
@@ -1,7 +1,7 @@
 /-
 Copyright (c) 2020 Scott Morrison. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
-Authors: Scott Morrison
+Authors: Markus Himmel, Scott Morrison
 -/
 import category_theory.simple
 import category_theory.preadditive

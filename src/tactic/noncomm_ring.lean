974b44657f8452b1ea189b57007aed426247ee29
Scott Morrison <scott.morrison@gmail.com>
2020-05-31 06:38:04 +1000

    better handling of scalars

--- src/tactic/noncomm_ring.lean
+++ src/tactic/noncomm_ring.lean
@@ -1,10 +1,28 @@
 /-
 Copyright (c) 2020 Oliver Nash. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
-Authors: Oliver Nash
+Authors: Scott Morrison, Oliver Nash
 -/
 import tactic.doc_commands
 import tactic.abel
+
+-- TODO this should be moved to `algebra/group_power.lean`.
+section
+variables {R : Type*} [ring R]
+
+lemma bit0_mul {n r : R} : bit0 n * r = gsmul 2 (n * r) :=
+by { dsimp [bit0], rw [add_mul, add_gsmul, one_gsmul], }
+
+lemma mul_bit0 {n r : R} : r * bit0 n = gsmul 2 (r * n) :=
+by { dsimp [bit0], rw [mul_add, add_gsmul, one_gsmul], }
+
+lemma bit1_mul {n r : R} : bit1 n * r = gsmul 2 (n * r) + r :=
+by { dsimp [bit1], rw [add_mul, bit0_mul, one_mul], }
+
+lemma mul_bit1 {n r : R} : r * bit1 n = gsmul 2 (r * n) + r :=
+by { dsimp [bit1], rw [mul_add, mul_bit0, mul_one], }
+
+end
 
 namespace tactic
 namespace interactive
@@ -18,8 +36,15 @@ by noncomm_ring
 ```
 -/
 meta def noncomm_ring :=
-`[simp only [add_mul, mul_add, sub_eq_add_neg, pow_bit0, pow_bit1, pow_one, mul_assoc,
-             mul_two, two_mul,
+`[simp only [add_mul, mul_add, sub_eq_add_neg,
+             mul_assoc,
+             -- We expand powers to numerals.
+             pow_bit0, pow_bit1, pow_one,
+             -- We replace multiplication by numerals with `gsmul`.
+             bit0_mul, mul_bit0, bit1_mul, mul_bit1, one_mul, mul_one, zero_mul, mul_zero,
+             -- We pull `gsmul n` out the front so `abel` can see them.
+             ←mul_gsmul_assoc, ←mul_gsmul_left,
+             -- we pull out negations.
              neg_mul_eq_neg_mul_symm, mul_neg_eq_neg_mul_symm];
   abel]
 
--- test/noncomm_ring.lean
+++ test/noncomm_ring.lean
@@ -12,10 +12,13 @@ example : a - b = a + -b := by noncomm_ring
 example : a * b * c = a * (b * c) := by noncomm_ring
 example : a + a = a * 2 := by noncomm_ring
 example : a + a = 2 * a := by noncomm_ring
+example : -a = (-1) * a := by noncomm_ring
+example : a + a + a = 3 * a := by noncomm_ring
 example : a ^ 2 = a * a := by noncomm_ring
+example : a ^ 3 = a * a * a := by noncomm_ring
 example : (-a) * b = -(a * b) := by noncomm_ring
 example : a * (-b) = -(a * b) := by noncomm_ring
-example : a * (b + c + c - b) = 2*a*c := by noncomm_ring
+example : a * (b + c + b + c - 2*b) = 2*a*c := by noncomm_ring
 example : (a + b)^2 = a^2 + a*b + b*a + b^2 := by noncomm_ring
 example : (a - b)^2 = a^2 - a*b - b*a + b^2 := by noncomm_ring
 example : (a + b)^3 = a^3 + a^2*b + a*b*a + a*b^2 + b*a^2 + b*a*b + b^2*a + b^3 := by noncomm_ring
@@ -31,11 +34,12 @@ example : ⁅a, ⁅b, c⁆⁆ = ⁅⁅a, b⁆, c⁆ + ⁅b, ⁅a, c⁆⁆ := by 
 example : ⁅⁅a, b⁆, c⁆ = ⁅⁅a, c⁆, b⁆ + ⁅a, ⁅b, c⁆⁆ := by noncomm_ring
 example : ⁅a * b, c⁆ = a * ⁅b, c⁆ + ⁅a, c⁆ * b := by noncomm_ring
 example : ⁅a, b * c⁆ = ⁅a, b⁆ * c + b * ⁅a, c⁆ := by noncomm_ring
-example : ⁅2 * a, a⁆ = 0 := by noncomm_ring
-example : ⁅a * 2, a⁆ = 0 := by noncomm_ring
-example : ⁅a^2, a⁆ = 0 := by noncomm_ring
+example : ⁅3 * a, a⁆ = 0 := by noncomm_ring
+example : ⁅a * -5, a⁆ = 0 := by noncomm_ring
+example : ⁅a^3, a⁆ = 0 := by noncomm_ring
 
 example : a ⚬ a = 2*a^2 := by noncomm_ring
+example : (2 * a) ⚬ a = 4*a^2 := by noncomm_ring
 example : a ⚬ b = b ⚬ a := by noncomm_ring
 example : a ⚬ (b + c) = a ⚬ b + (a ⚬ c) := by noncomm_ring
 example : (a + b) ⚬ c = a ⚬ c + (b ⚬ c) := by noncomm_ring

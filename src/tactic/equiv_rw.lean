f9c0ea695613b5062eca19fef4144f37a9d6acb0
Scott Morrison <scott.morrison@gmail.com>
2020-05-31 17:49:48 +1000

    lost import

--- src/tactic/equiv_rw.lean
+++ src/tactic/equiv_rw.lean
@@ -4,6 +4,7 @@ Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Scott Morrison
 -/
 import control.equiv_functor
+import category_theory.concrete_category.basic
 
 /-!
 # The `equiv_rw` tactic transports goals or hypotheses along equivalences.

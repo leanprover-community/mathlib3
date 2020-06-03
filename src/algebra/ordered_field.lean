16c1862d3a8813e06550f9dc02b003d35db18cde
Yury G. Kudryashov <urkud@urkud.name>
2020-06-03 16:32:07 -0400

    chore(*): fix some `ge_or_gt` lint issues

--- src/algebra/ordered_field.lean
+++ src/algebra/ordered_field.lean
@@ -322,7 +322,7 @@ begin
   apply one_div_pos_of_pos he
 end
 
-lemma exists_add_lt_and_pos_of_lt {a b : α} (h : b < a) : ∃ c : α, b + c < a ∧ c > 0 :=
+lemma exists_add_lt_and_pos_of_lt {a b : α} (h : b < a) : ∃ c : α, b + c < a ∧ 0 < c :=
 begin
   apply exists.intro ((a - b) / (1 + 1)),
   split,
@@ -340,7 +340,7 @@ begin
   exact div_pos_of_pos_of_pos (sub_pos_of_lt h) two_pos
 end
 
-lemma ge_of_forall_ge_sub {a b : α} (h : ∀ ε : α, ε > 0 → a ≥ b - ε) : a ≥ b :=
+lemma le_of_forall_sub_le {a b : α} (h : ∀ ε > 0, b - ε ≤ a) : b ≤ a :=
 begin
   apply le_of_not_gt,
   intro hb,

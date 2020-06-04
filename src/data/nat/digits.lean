025c4a284b6cf5836ba7c010921ec56d32154802
Johan Commelin <johan@commelin.net>
2020-06-04 17:02:21 +0000

    WIP

--- src/data/nat/digits.lean
+++ src/data/nat/digits.lean
@@ -315,21 +315,34 @@ def alternating_sum {G : Type*} [add_group G] : list G → G
 | (g :: []) := g
 | (g :: h :: t) := g - h + alternating_sum t
 
+section
+variables {G : Type*} [add_group G]
+
+@[simp] lemma alternating_sum_nil :
+  alternating_sum ([] : list G) = 0 := rfl
+
+@[simp] lemma alternating_sum_singleton (g : G) :
+  alternating_sum [g] = g := rfl
+
+@[simp] lemma alternating_sum_cons_cons (g h : G) (l : list G) :
+  alternating_sum (g :: h :: l) = g - h + alternating_sum l := rfl
+
+end
 
 open_locale big_operators
-lemma alternating_sum_eq_finset_sum {R : Type*} [ring R] : ∀ (L : list R),
-  alternating_sum L = ∑ i : fin L.length, (-1 : R) ^ (i : ℕ) * L.nth_le i i.2
+lemma alternating_sum_eq_finset_sum {G : Type*} [add_comm_group G] :
+  ∀ (L : list G), alternating_sum L = ∑ i : fin L.length, (-1 : ℤ) ^ (i : ℕ) •ℤ L.nth_le i i.2
 | [] := by { rw [alternating_sum, finset.sum_eq_zero], rintro ⟨i, ⟨⟩⟩ }
 | (g :: []) :=
 begin
-  show g = ∑ i : fin 1, (-1 : R) ^ (i : ℕ) * [g].nth_le i i.2,
+  show g = ∑ i : fin 1, (-1 : ℤ) ^ (i : ℕ) •ℤ [g].nth_le i i.2,
   rw [fin.sum_univ_succ], simp,
 end
 | (g :: h :: L) :=
 calc g - h + L.alternating_sum
-    = g - h + ∑ i : fin L.length, (-1 : R) ^ (i : ℕ) * L.nth_le i i.2 :
+    = g - h + ∑ i : fin L.length, (-1 : ℤ) ^ (i : ℕ) •ℤ L.nth_le i i.2 :
       congr_arg _ (alternating_sum_eq_finset_sum _)
-... = ∑ i : fin (L.length + 2), (-1 : R) ^ (i : ℕ) * list.nth_le (g :: h :: L) i _ :
+... = ∑ i : fin (L.length + 2), (-1 : ℤ) ^ (i : ℕ) •ℤ list.nth_le (g :: h :: L) i _ :
 begin
   rw [fin.sum_univ_succ, fin.sum_univ_succ, sub_eq_add_neg, add_assoc],
   unfold_coes,

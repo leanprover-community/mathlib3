3382c565e4139c0711ff44238b51e1cc485b8ae4
Scott Morrison <scott@tqft.net>
2020-06-05 17:55:28 +1000

    copyright header

--- src/analysis/convex/caratheodory.lean
+++ src/analysis/convex/caratheodory.lean
@@ -1,3 +1,8 @@
+/-
+Copyright (c) 2020 Scott Morrison. All rights reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
+Authors: Johan Commelin, Scott Morrison
+-/
 import analysis.convex.basic
 import linear_algebra.finite_dimensional
 

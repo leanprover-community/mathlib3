3252d6188529f899135091cfc6f87d3806c0956c
Smaug123 <patrick+github@patrickstevens.co.uk>
2020-06-06 15:12:39 +0100

    Markups

--- src/data/padics/padic_norm.lean
+++ src/data/padics/padic_norm.lean
@@ -112,14 +112,18 @@ lemma padic_val_rat_of_int (z : ℤ) (hp : p ≠ 1) (hz : z ≠ 0) :
     (finite_int_iff.2 ⟨hp, hz⟩) :=
 by rw [padic_val_rat, dif_pos]; simp *; refl
 
+end padic_val_rat
+
+section padic_val_nat
+
 /--
 A convenience function for the case of `padic_val_rat` when both inputs are natural numbers.
 -/
 def padic_val_nat (p : nat) (n : nat) : ℕ :=
-  int.to_nat (padic_val_rat p n)
+int.to_nat (padic_val_rat p n)
 
 /--
-`padic_val_rat` is defined as an `int.to_nat` cast; this lemma ensures that the cast is well-behaved.
+`padic_val_nat` is defined as an `int.to_nat` cast; this lemma ensures that the cast is well-behaved.
 -/
 lemma zero_le_padic_val_rat_of_nat (p n : nat) : 0 ≤ padic_val_rat p n :=
 begin
@@ -132,7 +136,7 @@ end
 /--
 `padic_val_rat` coincides with `padic_val_nat`.
 -/
-lemma padic_val_rat_of_nat (p n : nat) : padic_val_rat p n = ↑(padic_val_nat p n) :=
+@[simp, norm_cast] lemma padic_val_rat_of_nat (p n : nat) : ↑(padic_val_nat p n) = padic_val_rat p n :=
 begin
   unfold padic_val_nat,
   rw int.to_nat_of_nonneg (zero_le_padic_val_rat_of_nat p n),
@@ -146,11 +150,11 @@ lemma padic_val_nat_def {p : ℕ} [hp : fact p.prime] {n : ℕ} (hn : n ≠ 0) :
   (multiplicity p n).get (multiplicity.finite_nat_iff.2 ⟨nat.prime.ne_one hp, bot_lt_iff_ne_bot.mpr hn⟩) :=
 begin
   have n_nonzero : (n : ℚ) ≠ 0, by simpa only [cast_eq_zero, ne.def],
-  simpa [int.coe_nat_multiplicity p n, rat.coe_nat_denom n, padic_val_rat_of_nat p n]
+  simpa [int.coe_nat_multiplicity p n, rat.coe_nat_denom n, eq.symm (padic_val_rat_of_nat p n)]
     using padic_val_rat_def p n_nonzero,
 end
 
-end padic_val_rat
+end padic_val_nat
 
 section padic_val_rat
 open multiplicity

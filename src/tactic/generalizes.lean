14e33868461faa5bc553a00bdafe9b727255fea1
Jannis Limperg <jannis@limperg.de>
2020-06-08 03:45:45 +0200

    Lets of generalize docs, better errors, better tests

--- src/tactic/generalizes.lean
+++ src/tactic/generalizes.lean
@@ -12,8 +12,20 @@ namespace tactic
 
 open expr
 
--- Input must be in reverse dependency order!
--- Output constants are also in reverse dependency order.
+/--
+`generalizes'_aux₁ md unify e [] args` iterates through the `args` from left to
+right. In each step of the iteration, any occurrences of the expression from
+`args` in `e` is replaced by a new local constant. The local constant's pretty
+name is the name given in `args`. The `args` must be given in reverse dependency
+order: `[fin n, n]` is okay, but `[n, fin n]` won't work.
+
+The result of `generalizes'_aux₁` is `e` with all the `args` replaced and the
+list of new local constants, one for each element of `args`. Note that the local
+constants are not added to the local context.
+
+`md` and `unify` control how subterms of `e` are matched against the expressions
+in `args`; see `kabstract`.
+-/
 private meta def generalizes'_aux₁ (md : transparency) (unify : bool)
   : expr → list expr → list (name × expr) → tactic (expr × list expr)
 | e cnsts [] := pure (e, cnsts.reverse)
@@ -24,7 +36,20 @@ private meta def generalizes'_aux₁ (md : transparency) (unify : bool)
   cnsts ← cnsts.mmap $ λ c', kreplace c' x c md unify,
   generalizes'_aux₁ e (c :: cnsts) xs
 
--- Input must be in reverse dependency order!
+/--
+`generalizes'_aux₂` takes as input the expression `e` produced by
+`generalizes'_aux₁` and a list containing, for each argument to be generalized:
+
+- a name for the generalisation equation;
+- the local constant produced by `generalizes'_aux₁`;
+- the argument itself.
+
+From this information, it generates a type of the form
+
+    ∀ (j₁ : T₁) (j₁_eq : j₁ == a₁) (j₂ : T₂) (j₂_eq : j₂ == a₂), e
+
+where the `aᵢ` are the arguments and the `jᵢ` correspond to the local constants.
+-/
 private meta def generalizes'_aux₂ : expr → list (name × expr × expr) → tactic expr
 | e [] := pure e
 | e ((eq_name, cnst, arg) :: cs) := do
@@ -37,8 +62,30 @@ private meta def generalizes'_aux₂ : expr → list (name × expr × expr) → 
     (e.abstract cnst).lift_vars 0 1,
   generalizes'_aux₂ e cs
 
+/-
+Generalizes the target over each of the expressions in `args`. Given
+`args = [(a₁, h₁, arg₁), ...]`, this changes the target to
+
+    ∀ (a₁ : T₁) (h₁ : a₁ == arg₁) ..., U
+
+where `U` is the current target with every occurrence of `argᵢ` replaced by
+`aᵢ`. A similar effect can be achieved by using `generalize` once for each of
+the `args`, but if there are dependencies between the `args`, this may fail to
+perform some generalizations.
+
+The replacement is performed using keyed matching/unification with transparency
+`md`. `unify` determines whether matching or unification is used. See
+`kabstract`.
+
+The `args` must be given in dependency order, so `[n, fin n]` is okay but
+`[fin n, n]` will result in an error.
+
+After generalizing the `args`, the target type may no longer type check.
+`generalizes'` will then raise an error.
+-/
 meta def generalizes' (args : list (name × name × expr))
-  (md := semireducible) (unify := tt) : tactic nat := focus1 $ do
+  (md := semireducible) (unify := tt) : tactic unit :=
+focus1 $ do
   tgt ← target,
   let args_rev := args.reverse,
   (tgt, cnsts) ← generalizes'_aux₁ md unify tgt []
@@ -47,7 +94,8 @@ meta def generalizes' (args : list (name × name × expr))
     @list.map₂ (name × name × expr) expr _
       (λ ⟨x_name, eq_name, x⟩ cnst, (eq_name, cnst, x)) args_rev cnsts,
   tgt ← generalizes'_aux₂ tgt args',
-  type_check tgt <|> fail! "TODO",
+  type_check tgt <|> fail!
+    "generalizes: unable to generalize the target because the generalized target type does not type check:\n{tgt}",
   n ← mk_fresh_name,
   h ← assert n tgt,
   swap,
@@ -56,8 +104,7 @@ meta def generalizes' (args : list (name × name × expr))
     sort u ← infer_type x_type,
     pure [x, (const `heq.refl [u]) x_type x]
   },
-  exact $ h.mk_app apps.join,
-  pure $ args.length * 2
+  exact $ h.mk_app apps.join
 
 
 namespace interactive
@@ -82,16 +129,40 @@ private meta def generalizes_args_parser
 with_desc "[id : expr = id, ...]" $
   tk "[" *> sep_by (tk ",") generalizes_arg_parser <* tk "]"
 
-meta def generalizes (args : parse generalizes_args_parser) : tactic unit := do
+/--
+Generalizes the target over multiple expressions. For example, given the goal
+
+    P : ∀ n, fin n → Prop
+    n : ℕ
+    f : fin n
+    ⊢ P (nat.succ n) (fin.succ f)
+
+you can use `generalizes [n'_eq : nat.succ n = n', f'_eq : fin.succ f = f']` to get
+
+    P : ∀ n, fin n → Prop
+    n : ℕ
+    f : fin n
+    n' : ℕ
+    n'_eq : n' == nat.succ n
+    f' : fin n'
+    f'_eq : f' == fin.succ f
+    ⊢ P n' f'
+
+The expressions must be given in dependency order, so
+`[f'_eq : fin.succ f = f', n'_eq : n + 1 = n']` would fail since the type of
+`fin.succ f` is `n + 1`.
+
+After generalization, the target type may no longer type check. `generalizes`
+will then raise an error.
+-/
+meta def generalizes (args : parse generalizes_args_parser) : tactic unit :=
+propagate_tags $ do
   args ← args.mmap $ λ ⟨arg_name, hyp_name, arg⟩, do {
     arg ← to_expr arg,
     pure (arg_name, hyp_name, arg)
   },
-  propagate_tags $ do {
-    n ← generalizes' args,
-    intron n,
-    pure ()
-  },
+  n ← generalizes' args,
+  intron $ args.length * 2,
   pure ()
 
 end interactive
--- test/generalizes.lean
+++ test/generalizes.lean
@@ -8,6 +8,15 @@ import tactic.generalizes
 
 universes u
 
+lemma example_from_docs (P : ∀ n, fin n → Prop) (n : ℕ) (f : fin n)
+  (p : ∀ n xs, P n xs) : P (nat.succ n) (fin.succ f) :=
+  -- TODO: `generalizes` fails if we write `n + 1` instead of `nat.succ n`,
+  -- even with transparency `all`. I don't understand why.
+begin
+  generalizes [n'_eq : (nat.succ n) = n', f'_eq : fin.succ f = f'],
+  exact p n' f'
+end
+
 inductive Vec (α : Sort u) : ℕ → Sort (max 1 u)
 | nil : Vec 0
 | cons {n} (x : α) (xs : Vec n) : Vec (n + 1)
@@ -21,6 +30,26 @@ inductive eq {α} : ∀ (n m : ℕ), Vec α n → Vec α m → Prop
   → eq n m xs ys
   → eq (n + 1) (m + 1) (cons x xs) (cons y ys)
 
+inductive fancy_unit {α} : ∀ n, Vec α n → Prop
+| intro (n xs) : fancy_unit n xs
+
+lemma test {α n} {x : α} {xs} : fancy_unit (n + 1) (cons x xs) :=
+begin
+  -- Successive `generalize` invocations don't give us the right goal:
+  -- The second generalisation fails due to the dependency between
+  -- `xs' : Vec α (n + 1)` and `n + 1`.
+  success_if_fail {
+    generalize eq_xs' : cons x xs = xs',
+    generalize eq_n' : n + 1 = n',
+    exact fancy_unit.intro n' xs'
+  },
+  -- `generalizes` gives us the expected result with everything generalised.
+  generalizes [eq_n' : n + 1 = n', eq_xs' : cons x xs = xs'],
+  exact fancy_unit.intro n' xs'
+end
+
+-- An example where `generalizes` enables a successful `induction` (though in
+-- this case, `cases` would suffice and already performs the generalisation).
 lemma eq_cons_inversion {α} {n m x y} {xs : Vec α n} {ys : Vec α m}
   : eq (n + 1) (m + 1) (cons x xs) (cons y ys)
   → eq n m xs ys :=

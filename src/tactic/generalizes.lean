ad63c5fd682194fdfd6bfe70e67cdca3058d28e1
Jannis Limperg <jannis@limperg.de>
2020-06-08 19:36:29 +0200

    Enable tactic.generalize' to omit generated equations

--- src/tactic/generalizes.lean
+++ src/tactic/generalizes.lean
@@ -73,9 +73,10 @@ private meta def generalizes'_aux₁ (md : transparency) (unify : bool)
 `generalizes'_aux₂ md []` takes as input the expression `e` produced by
 `generalizes'_aux₁` and a list containing, for each argument to be generalized:
 
-- a name for the generalisation equation;
-- the local constant produced by `generalizes'_aux₁`;
-- the argument itself.
+- A name for the generalisation equation. If this is `none`, no equation is
+  generated.
+- The local constant produced by `generalizes'_aux₁`.
+- The argument itself.
 
 From this information, it generates a type of the form
 
@@ -83,30 +84,41 @@ From this information, it generates a type of the form
 
 where the `aᵢ` are the arguments and the `jᵢ` correspond to the local constants.
 
-It also returns, for each argument, whether the equation associated with this
-argument is homogeneous (`tt`) or heterogeneous (`ff`).
+It also returns, for each argument, whether an equation was generated for the
+argument and if so, whether the equation is homogeneous (`tt`) or heterogeneous
+(`ff`).
 
 The transparency `md` is used when determining whether the types of an argument
 and its associated constant are definitionally equal (and thus whether to
 generate a homogeneous or a heterogeneous equation).
 -/
 private meta def generalizes'_aux₂ (md : transparency)
-  : list bool → expr → list (name × expr × expr) → tactic (expr × list bool)
-| acc e [] := pure (e, acc.reverse)
-| acc e ((eq_name, cnst, arg) :: cs) := do
+  : list (option bool) → expr → list (option name × expr × expr)
+  → tactic (expr × list (option bool))
+| eq_kinds e [] := pure (e, eq_kinds.reverse)
+| eq_kinds e ((eq_name, cnst, arg) :: cs) := do
   cnst_type ← infer_type cnst,
   arg_type ← infer_type arg,
   sort u ← infer_type arg_type,
-  homogeneous ← succeeds $ is_def_eq cnst_type arg_type,
-  let eq_type :=
-    if homogeneous
-      then ((const `eq [u]) cnst_type (var 0) arg)
-      else ((const `heq [u]) cnst_type (var 0) arg_type arg),
+  ⟨eq_binder, eq_kind⟩ ← do {
+    match eq_name with
+    | none := pure ((id : expr → expr), none)
+    | some eq_name := do
+        homogeneous ← succeeds $ is_def_eq cnst_type arg_type,
+        let eq_type :=
+          if homogeneous
+            then ((const `eq [u]) cnst_type (var 0) arg)
+            else ((const `heq [u]) cnst_type (var 0) arg_type arg),
+        let eq_binder : expr → expr := λ e,
+          pi eq_name binder_info.default eq_type (e.lift_vars 0 1),
+        pure (eq_binder, some homogeneous )
+    end
+  },
   let e :=
     pi cnst.local_pp_name binder_info.default cnst_type $
-    pi eq_name binder_info.default eq_type $
-    (e.abstract cnst).lift_vars 0 1,
-  generalizes'_aux₂ (homogeneous :: acc) e cs
+    eq_binder $
+    e.abstract cnst,
+  generalizes'_aux₂ (eq_kind :: eq_kinds) e cs
 
 /--
 Generalizes the target over each of the expressions in `args`. Given
@@ -129,7 +141,7 @@ The `args` must be given in dependency order, so `[n, fin n]` is okay but
 After generalizing the `args`, the target type may no longer type check.
 `generalizes'` will then raise an error.
 -/
-meta def generalizes' (args : list (name × name × expr))
+meta def generalizes' (args : list (name × option name × expr))
   (md := semireducible) (unify := tt) : tactic unit :=
 focus1 $ do
   tgt ← target,
@@ -137,28 +149,42 @@ focus1 $ do
   (tgt, cnsts) ← generalizes'_aux₁ md unify tgt []
     (args_rev.map (λ ⟨e_name, _, e⟩, (e_name, e))),
   let args' :=
-    @list.map₂ (name × name × expr) expr _
-      (λ ⟨x_name, eq_name, x⟩ cnst, (eq_name, cnst, x)) args_rev cnsts,
-  ⟨tgt, eq_types⟩ ← generalizes'_aux₂ md [] tgt args',
-  let eq_types := eq_types.reverse,
+    @list.map₂ (name × option name × expr) expr _
+      (λ ⟨_, eq_name, x⟩ cnst, (eq_name, cnst, x)) args_rev cnsts,
+  ⟨tgt, eq_kinds⟩ ← generalizes'_aux₂ md [] tgt args',
+  let eq_kinds := eq_kinds.reverse,
   type_check tgt <|> fail!
     "generalizes: unable to generalize the target because the generalized target type does not type check:\n{tgt}",
   n ← mk_fresh_name,
   h ← assert n tgt,
   swap,
   let args' :=
-    @list.map₂ (name × name × expr) bool _
-      (λ ⟨_, _, x⟩ homogeneous, (x, homogeneous)) args eq_types,
-  apps ← args'.mmap $ λ ⟨x, homogeneous⟩, do {
-    x_type ← infer_type x,
-    sort u ← infer_type x_type,
-    let eq_proof :=
-      if homogeneous
-        then (const `eq.refl [u]) x_type x
-        else (const `heq.refl [u]) x_type x,
-    pure [x, eq_proof]
+    @list.map₂ (name × option name × expr) (option bool) _
+      (λ ⟨_, _, x⟩ eq_kind, (x, eq_kind)) args eq_kinds,
+  apps ← args'.mmap $ λ ⟨x, eq_kind⟩, do {
+    match eq_kind with
+    | none := pure [x]
+    | some eq_is_homogeneous := do
+      x_type ← infer_type x,
+      sort u ← infer_type x_type,
+      let eq_proof :=
+        if eq_is_homogeneous
+          then (const `eq.refl [u]) x_type x
+          else (const `heq.refl [u]) x_type x,
+      pure [x, eq_proof]
+    end
   },
   exact $ h.mk_app apps.join
+
+/--
+Like `generalizes'`, but also introduces the generalized constants and their
+associated equations into the context.
+-/
+meta def generalizes_intro (args : list (name × option name × expr))
+  (md := semireducible) (unify := tt) : tactic (list expr) := do
+  generalizes' args md unify,
+  let binder_nos := args.map (λ ⟨_, hyp, _⟩, 1 + if hyp.is_some then 1 else 0),
+  intron' binder_nos.sum
 
 
 namespace interactive
@@ -166,10 +192,9 @@ namespace interactive
 open interactive
 open lean.parser
 
-private meta def generalizes_arg_parser : lean.parser (name × name × pexpr) :=
+private meta def generalizes_arg_parser : lean.parser (name × option name × pexpr) :=
 with_desc "id : expr = id" $ do
-  hyp_name ← ident,
-  tk ":",
+  hyp_name ← optional (ident <* tk ":"),
   arg ← lean.parser.pexpr 0,
   (arg, arg_name) ←
     match arg with
@@ -180,7 +205,7 @@ with_desc "id : expr = id" $ do
   pure $ (arg_name, hyp_name, arg)
 
 private meta def generalizes_args_parser
-  : lean.parser (list (name × name × pexpr)) :=
+  : lean.parser (list (name × option name × pexpr)) :=
 with_desc "[id : expr = id, ...]" $
   tk "[" *> sep_by (tk ",") generalizes_arg_parser <* tk "]"
 
@@ -208,6 +233,18 @@ The expressions must be given in dependency order, so
 `[f'_eq : fin.succ f == f', n'_eq : nat.succ n = n']` would fail since the type
 of `fin.succ f` is `nat.succ n`.
 
+You can choose to omit some or all of the generated equations. For the above
+example, `generalizes [(nat.succ n = n'), (fin.succ f == f')]` gets you
+
+    P : ∀ n, fin n → Prop
+    n : ℕ
+    f : fin n
+    n' : ℕ
+    f' : fin n'
+    ⊢ P n' f'
+
+Note the parentheses; these are necessary to avoid parsing issues.
+
 After generalization, the target type may no longer type check. `generalizes`
 will then raise an error.
 -/
@@ -217,8 +254,7 @@ propagate_tags $ do
     arg ← to_expr arg,
     pure (arg_name, hyp_name, arg)
   },
-  n ← generalizes' args,
-  intron $ args.length * 2,
+  generalizes_intro args,
   pure ()
 
 add_tactic_doc
--- test/generalizes.lean
+++ test/generalizes.lean
@@ -8,17 +8,29 @@ import tactic.generalizes
 
 universes u
 
-lemma example_from_docs (P : ∀ n, fin n → Prop) (n : ℕ) (f : fin n)
+lemma example_from_docs₁ (P : ∀ n, fin n → Prop) (n : ℕ) (f : fin n)
   (p : ∀ n xs, P n xs) : P (nat.succ n) (fin.succ f) :=
 begin
-  -- TODO: `generalizes` fails if we write `n + 1` instead of `nat.succ n`,
-  -- even though these are defeq at transparency semireducible.
-  (do n ← tactic.get_local `n,
-      h₁ ← tactic.to_expr ``(%%n + 1),
-      h₂ ← tactic.to_expr ``(nat.succ %%n),
-      tactic.is_def_eq h₁ h₂ semireducible ff
-  ),
-  generalizes [n'_eq : (nat.succ n) = n', f'_eq : fin.succ f == f'],
+  generalizes [n'_eq : nat.succ n = n', f'_eq : fin.succ f == f'],
+  guard_hyp n' := ℕ,
+  guard_hyp n'_eq := n' = nat.succ n,
+  guard_hyp f' := fin n',
+  guard_hyp f'_eq := f' == fin.succ f,
+  exact p n' f'
+  -- Note: `generalizes` fails if we write `n + 1` instead of `nat.succ n` in
+  -- the target because `kabstract` works with a notion of equality that is
+  -- weaker than definitional equality. This is annoying, but we can't do much
+  -- about it short of reimplementing `kabstract`.
+end
+
+lemma example_from_docs₂ (P : ∀ n, fin n → Prop) (n : ℕ) (f : fin n)
+  (p : ∀ n xs, P n xs) : P (nat.succ n) (fin.succ f) :=
+begin
+  generalizes [(nat.succ n = n'), (fin.succ f == f')],
+  guard_hyp n' := ℕ,
+  success_if_fail { guard_hyp n'_eq := n' = nat.succ n },
+  guard_hyp f' := fin n',
+  success_if_fail { guard_hyp f'_eq := f' == fin.succ f },
   exact p n' f'
 end
 
@@ -38,7 +50,9 @@ inductive eq {α} : ∀ (n m : ℕ), Vec α n → Vec α m → Prop
 inductive fancy_unit {α} : ∀ n, Vec α n → Prop
 | intro (n xs) : fancy_unit n xs
 
-lemma test {α n} {x : α} {xs} : fancy_unit (n + 1) (cons x xs) :=
+-- An example of the difference between multiple calls to `generalize` and
+-- one call to `generalizes`.
+lemma test₁ {α n} {x : α} {xs} : fancy_unit (n + 1) (cons x xs) :=
 begin
   -- Successive `generalize` invocations don't give us the right goal:
   -- The second generalisation fails due to the dependency between
@@ -50,6 +64,22 @@ begin
   },
   -- `generalizes` gives us the expected result with everything generalised.
   generalizes [eq_n' : n + 1 = n', eq_xs' : cons x xs = xs'],
+  guard_hyp n' := ℕ,
+  guard_hyp eq_n' := n' = n + 1,
+  guard_hyp xs' := Vec α n',
+  guard_hyp eq_xs' := xs' == cons x xs,
+  exact fancy_unit.intro n' xs'
+end
+
+-- We can also choose to introduce equations for only some of the generalised
+-- expressions.
+lemma test₂ {α n} {x : α} {xs} : fancy_unit (n + 1) (cons x xs) :=
+begin
+  generalizes [(n + 1 = n'), eq_xs' : cons x xs = xs'],
+  guard_hyp n' := ℕ,
+  success_if_fail { guard_hyp eq_n' := n' = n + 1 },
+  guard_hyp xs' := Vec α n',
+  guard_hyp eq_xs' := xs' == cons x xs,
   exact fancy_unit.intro n' xs'
 end
 

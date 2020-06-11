c64b1c46cb37d102f8c77600a416ecbde9f1235d
Yury G. Kudryashov <urkud@urkud.name>
2020-06-11 01:00:11 -0400

    refactor(analysis/mean_inequalities): review
    Also add several lemmas to other files

--- src/analysis/mean_inequalities.lean
+++ src/analysis/mean_inequalities.lean
@@ -5,6 +5,8 @@ Authors: Yury Kudryashov
 -/
 import analysis.convex.specific_functions
 import analysis.special_functions.pow
+import data.real.conjugate_exponents
+import tactic.nth_rewrite
 
 /-!
 # Mean value inequalities
@@ -29,15 +31,18 @@ and `n=3`.
 
 universes u v
 
-open real finset
+open finset
 open_locale classical nnreal big_operators
+noncomputable theory
 
 variables {ι : Type u} (s : finset ι)
 
+namespace real
+
 /-- Geometric mean is less than or equal to the arithmetic mean, weighted version
 for functions on `finset`s. -/
-theorem real.am_gm_weighted (w z : ι → ℝ)
-  (hw : ∀ i ∈ s, 0 ≤ w i) (hw' : ∑ i in s, w i = 1) (hz : ∀ i ∈ s, 0 ≤ z i) :
+theorem am_gm_weighted (w z : ι → ℝ) (hw : ∀ i ∈ s, 0 ≤ w i) (hw' : ∑ i in s, w i = 1)
+  (hz : ∀ i ∈ s, 0 ≤ z i) :
   (∏ i in s, (z i) ^ (w i)) ≤ ∑ i in s, w i * z i :=
 begin
   let s' := s.filter (λ i, w i ≠ 0),
@@ -68,33 +73,33 @@ begin
     { exact sum_congr rfl (λ i hi, congr_arg _ (exp_log $ hz i hi).symm) } }
 end
 
-theorem nnreal.am_gm_weighted (w z : ι → ℝ≥0) (hw' : ∑ i in s, w i = 1) :
+end real
+
+namespace nnreal
+
+theorem am_gm_weighted (w z : ι → ℝ≥0) (hw' : ∑ i in s, w i = 1) :
   (∏ i in s, (z i) ^ (w i:ℝ)) ≤ ∑ i in s, w i * z i :=
 begin
-  rw [← nnreal.coe_le_coe, nnreal.coe_prod, nnreal.coe_sum],
+  rw [← nnreal.coe_le_coe],
+  push_cast,
   refine real.am_gm_weighted _ _ _ (λ i _, (w i).coe_nonneg) _ (λ i _, (z i).coe_nonneg),
   assumption_mod_cast
 end
 
-theorem nnreal.am_gm2_weighted (w₁ w₂ p₁ p₂ : ℝ≥0) (hw : w₁ + w₂ = 1) :
+theorem am_gm2_weighted (w₁ w₂ p₁ p₂ : ℝ≥0) (hw : w₁ + w₂ = 1) :
   p₁ ^ (w₁:ℝ) * p₂ ^ (w₂:ℝ) ≤ w₁ * p₁ + w₂ * p₂ :=
 begin
-  have := nnreal.am_gm_weighted (univ : finset (fin 2)) (fin.cons w₁ $ fin.cons w₂ fin_zero_elim)
+  have := am_gm_weighted (univ : finset (fin 2)) (fin.cons w₁ $ fin.cons w₂ fin_zero_elim)
     (fin.cons p₁ $ fin.cons p₂ $ fin_zero_elim),
   simp only [fin.prod_univ_succ, fin.sum_univ_succ, fin.prod_univ_zero, fin.sum_univ_zero,
     fin.cons_succ, fin.cons_zero, add_zero, mul_one] at this,
   exact this hw
 end
 
-theorem real.am_gm2_weighted {w₁ w₂ p₁ p₂ : ℝ} (hw₁ : 0 ≤ w₁) (hw₂ : 0 ≤ w₂)
-  (hp₁ : 0 ≤ p₁) (hp₂ : 0 ≤ p₂) (hw : w₁ + w₂ = 1) :
-  p₁ ^ w₁ * p₂ ^ w₂ ≤ w₁ * p₁ + w₂ * p₂ :=
-nnreal.am_gm2_weighted ⟨w₁, hw₁⟩ ⟨w₂, hw₂⟩ ⟨p₁, hp₁⟩ ⟨p₂, hp₂⟩ $ nnreal.coe_eq.1 $ by assumption
-
-theorem nnreal.am_gm3_weighted (w₁ w₂ w₃ p₁ p₂ p₃ : ℝ≥0) (hw : w₁ + w₂ + w₃ = 1) :
+theorem am_gm3_weighted (w₁ w₂ w₃ p₁ p₂ p₃ : ℝ≥0) (hw : w₁ + w₂ + w₃ = 1) :
   p₁ ^ (w₁:ℝ) * p₂ ^ (w₂:ℝ) * p₃ ^ (w₃:ℝ) ≤ w₁ * p₁ + w₂ * p₂ + w₃ * p₃:=
 begin
-  have := nnreal.am_gm_weighted (univ : finset (fin 3))
+  have := am_gm_weighted (univ : finset (fin 3))
     (fin.cons w₁ $ fin.cons w₂ $ fin.cons w₃ fin_zero_elim)
     (fin.cons p₁ $ fin.cons p₂ $ fin.cons p₃ fin_zero_elim),
   simp only [fin.prod_univ_succ, fin.sum_univ_succ, fin.prod_univ_zero, fin.sum_univ_zero,
@@ -103,47 +108,76 @@ begin
   exact this hw
 end
 
-/-- Young's inequality, `ℝ≥0` version -/
-theorem nnreal.young_inequality (a b : ℝ≥0) {p q : ℝ≥0} (hp : 1 < p) (hq : 1 < q)
-  (hpq : 1/p + 1/q = 1) : a * b ≤ a^(p:ℝ) / p + b^(q:ℝ) / q :=
-begin
-  have := nnreal.am_gm2_weighted (1/p) (1/q) (a^(p:ℝ)) (b^(q:ℝ)) hpq,
-  simp only [← nnreal.rpow_mul, one_div_eq_inv, nnreal.coe_div, nnreal.coe_one] at this,
-  rw [mul_inv_cancel, mul_inv_cancel, nnreal.rpow_one, nnreal.rpow_one] at this,
-  { ring at ⊢ this,
-    convert this;
-    { rw [nnreal.div_def, nnreal.div_def], ring } },
-  { exact ne_of_gt (lt_trans zero_lt_one hq) },
-  { exact ne_of_gt (lt_trans zero_lt_one hp) }
-end
+end nnreal
 
-/-- Young's inequality, `ℝ` version -/
-theorem real.young_inequality {a b : ℝ} (ha : 0 ≤ a) (hb : 0 ≤ b)
-  {p q : ℝ} (hp : 1 < p) (hq : 1 < q) (hpq : 1/p + 1/q = 1) :
+namespace real
+
+theorem am_gm2_weighted {w₁ w₂ p₁ p₂ : ℝ} (hw₁ : 0 ≤ w₁) (hw₂ : 0 ≤ w₂)
+  (hp₁ : 0 ≤ p₁) (hp₂ : 0 ≤ p₂) (hw : w₁ + w₂ = 1) :
+  p₁ ^ w₁ * p₂ ^ w₂ ≤ w₁ * p₁ + w₂ * p₂ :=
+nnreal.am_gm2_weighted ⟨w₁, hw₁⟩ ⟨w₂, hw₂⟩ ⟨p₁, hp₁⟩ ⟨p₂, hp₂⟩ $ nnreal.coe_eq.1 $ by assumption
+
+/-- Young's inequality, a version for nonnegative real numbers. -/
+theorem young_inequality_of_nonneg {a b p q : ℝ} (ha : 0 ≤ a) (hb : 0 ≤ b)
+  (hpq : p.is_conjugate_exponent q) :
   a * b ≤ a^p / p + b^q / q :=
-@nnreal.young_inequality ⟨a, ha⟩ ⟨b, hb⟩ ⟨p, le_trans zero_le_one (le_of_lt hp)⟩
-  ⟨q, le_trans zero_le_one (le_of_lt hq)⟩ hp hq (nnreal.coe_eq.1 hpq)
+by simpa [← rpow_mul, ha, hb, hpq.ne_zero, hpq.symm.ne_zero, div_eq_inv_mul]
+  using real.am_gm2_weighted (le_of_lt hpq.one_div_pos) (le_of_lt hpq.symm.one_div_pos)
+    (rpow_nonneg_of_nonneg ha p) (rpow_nonneg_of_nonneg hb q) hpq.inv_add_inv_conj
 
-theorem real.pow_am_le_am_pow (w z : ι → ℝ) (hw : ∀ i ∈ s, 0 ≤ w i)
+/-- Young's inequality, a version for arbitrary real numbers. -/
+theorem young_inequality (a b : ℝ) {p q : ℝ} (hpq : p.is_conjugate_exponent q) :
+  a * b ≤ (abs a)^p / p + (abs b)^q / q :=
+calc a * b ≤ abs (a * b)                   : le_abs_self (a * b)
+       ... = abs a * abs b                 : abs_mul a b
+       ... ≤ (abs a)^p / p + (abs b)^q / q :
+  real.young_inequality_of_nonneg (abs_nonneg a) (abs_nonneg b) hpq
+
+theorem pow_am_le_am_pow (w z : ι → ℝ) (hw : ∀ i ∈ s, 0 ≤ w i)
   (hw' : ∑ i in s, w i = 1) (hz : ∀ i ∈ s, 0 ≤ z i) (n : ℕ) :
   (∑ i in s, w i * z i) ^ n ≤ ∑ i in s, (w i * z i ^ n) :=
 (convex_on_pow n).map_sum_le hw hw' hz
 
-theorem nnreal.pow_am_le_am_pow (w z : ι → ℝ≥0) (hw' : ∑ i in s, w i = 1) (n : ℕ) :
-  (∑ i in s, w i * z i) ^ n ≤ ∑ i in s, (w i * z i ^ n) :=
-begin
-  rw [← nnreal.coe_le_coe],
-  push_cast,
-  refine (convex_on_pow n).map_sum_le (λ i _, (w i).coe_nonneg) _ (λ i _, (z i).coe_nonneg),
-  assumption_mod_cast
-end
-
-theorem real.pow_am_le_am_pow_of_even (w z : ι → ℝ) (hw : ∀ i ∈ s, 0 ≤ w i)
+theorem pow_am_le_am_pow_of_even (w z : ι → ℝ) (hw : ∀ i ∈ s, 0 ≤ w i)
   (hw' : ∑ i in s, w i = 1) {n : ℕ} (hn : n.even) :
   (∑ i in s, w i * z i) ^ n ≤ ∑ i in s, (w i * z i ^ n) :=
 (convex_on_pow_of_even hn).map_sum_le hw hw' (λ _ _, trivial)
 
-theorem real.fpow_am_le_am_fpow (w z : ι → ℝ) (hw : ∀ i ∈ s, 0 ≤ w i)
+theorem fpow_am_le_am_fpow (w z : ι → ℝ) (hw : ∀ i ∈ s, 0 ≤ w i)
   (hw' : ∑ i in s, w i = 1) (hz : ∀ i ∈ s, 0 < z i) (m : ℤ) :
   (∑ i in s, w i * z i) ^ m ≤ ∑ i in s, (w i * z i ^ m) :=
 (convex_on_fpow m).map_sum_le hw hw' hz
+
+theorem rpow_am_le_am_rpow (w z : ι → ℝ) (hw : ∀ i ∈ s, 0 ≤ w i)
+  (hw' : ∑ i in s, w i = 1) (hz : ∀ i ∈ s, 0 ≤ z i) {p : ℝ} (hp : 1 ≤ p) :
+  (∑ i in s, w i * z i) ^ p ≤ ∑ i in s, (w i * z i ^ p) :=
+(convex_on_rpow hp).map_sum_le hw hw' hz
+
+end real
+
+namespace nnreal
+
+/-- Young's inequality, `ℝ≥0` version -/
+theorem young_inequality (a b : ℝ≥0) {p q : ℝ≥0} (hp : 1 < p) (hpq : 1 / p + 1 / q = 1) :
+  a * b ≤ a^(p:ℝ) / p + b^(q:ℝ) / q :=
+real.young_inequality_of_nonneg a.coe_nonneg b.coe_nonneg ⟨hp, nnreal.coe_eq.2 hpq⟩
+
+theorem pow_am_le_am_pow (w z : ι → ℝ≥0) (hw' : ∑ i in s, w i = 1) (n : ℕ) :
+  (∑ i in s, w i * z i) ^ n ≤ ∑ i in s, (w i * z i ^ n) :=
+begin
+  rw [← nnreal.coe_le_coe],
+  push_cast,
+  refine real.pow_am_le_am_pow s _ _ (λ i _, (w i).coe_nonneg) _ (λ i _, (z i).coe_nonneg) n,
+  assumption_mod_cast
+end
+
+theorem rpow_am_le_am_rpow (w z : ι → ℝ≥0) (hw' : ∑ i in s, w i = 1) {p : ℝ} (hp : 1 ≤ p) :
+  (∑ i in s, w i * z i) ^ p ≤ ∑ i in s, (w i * z i ^ p) :=
+begin
+  rw [← nnreal.coe_le_coe],
+  push_cast,
+  refine real.rpow_am_le_am_rpow s _ _ (λ i _, (w i).coe_nonneg) _ (λ i _, (z i).coe_nonneg) hp,
+  assumption_mod_cast
+end
+
+end nnreal

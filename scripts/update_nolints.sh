6300faf9fbd5281a0ec5814c9d0848e94090d56e
Bryan Gin-ge Chen <bryangingechen@gmail.com>
2020-06-12 20:23:31 -0400

    only update nolints once daily

--- scripts/update_nolints.sh
+++ scripts/update_nolints.sh
@@ -9,9 +9,9 @@ owner_name=leanprover-community
 git fetch $remote_name
 git rev-parse --verify --quiet refs/remotes/$remote_name/$branch_name && exit 0
 
-# Exit if there are no changes with respect to master
+# Exit if there are no changes relative to master
 git diff-index --quiet refs/remotes/$remote_name/master -- scripts/nolints.txt && exit 0
-# Exit if staging exists and there are no changes with respect to staging
+# Exit if staging exists and there are no changes relative to staging
 git rev-parse --verify --quiet refs/remotes/$remote_name/staging && git diff-index --quiet refs/remotes/$remote_name/staging -- scripts/nolints.txt && exit 0
 
 pr_title='chore(scripts): update nolints.txt'

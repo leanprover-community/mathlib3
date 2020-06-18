00d3b3fac4713295a67c1cb555653e3b5d072476
Rob Lewis <rob.y.lewis@gmail.com>
2020-06-18 13:44:24 +0200

    improve zify

---
+++ test/zify.lean
@@ -0,0 +1,24 @@
+import tactic.zify
+
+example (a b c x y z : ℕ) (h : ¬ x*y*z < 0) : a + 3*b > c :=
+begin
+  zify at h ⊢,
+  guard_hyp h := ¬↑x * ↑y * ↑z < (0 : ℤ),
+  guard_target ↑c < (↑a : ℤ) + 3 * ↑b,
+  admit
+end
+
+example (a b : ℕ) : a ≤ b :=
+begin
+  zify,
+  guard_target (a : ℤ) ≤ b,
+  admit
+end
+
+example (a b : ℕ) (h : a = b ∧ b < a) : false :=
+begin
+  zify at h,
+  cases h with ha hb,
+  apply ne_of_lt hb,
+  rw ha
+end

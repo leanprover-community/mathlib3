f82572d60c3d9931b1bdbf55496e68b9824cc00b
Kyle Miller <kmill31415@gmail.com>
2020-06-24 23:59:51 -0700

    put vertex and edge types outside structure.  this seems like a good idea.  getting the opposite of an edge from a vertex is still noncomputable, though

--- src/combinatorics/graphs/basic.lean
+++ src/combinatorics/graphs/basic.lean
@@ -11,101 +11,114 @@ import order.bounded_lattice
 import tactic
 open function (hiding graph)
 --open equiv
---open sym2
 
 namespace graph
 universes u v
---variables {V : Type u} {E : Type v}
-
 
 section links
 
-structure link (V : Type u) (E : Type v) :=
-(src : V) (via : E) (dest : V)
+-- A link with vertex type α and edge type β represents a directed
+-- path through a given edge.
+structure link (α : Type u) (β : Type v) :=
+(src : α) (via : β) (dest : α)
 
 namespace link
-variables {V : Type u} {E : Type v}
+variables {α : Type u} {β : Type v}
 
-def rev (x : link V E) : link V E := ⟨x.dest, x.via, x.src⟩
+def rev (x : link α β) : link α β := ⟨x.dest, x.via, x.src⟩
 
-lemma rev.involutive : involutive (@link.rev V E) :=
+lemma rev.involutive : involutive (@link.rev α β) :=
 begin intro x, cases x, simp [rev], end
 
-lemma rev_via (x : link V E) : x.rev.via = x.via := rfl
+@[simp] lemma rev_src (x : link α β) : x.rev.src = x.dest := rfl
+@[simp] lemma rev_via (x : link α β) : x.rev.via = x.via := rfl
+@[simp] lemma rev_dest (x : link α β) : x.rev.dest = x.src := rfl
+@[simp] lemma rev_rev (x : link α β) : x.rev.rev = x := by cases x; simp only [link.rev, eq_self_iff_true, and_self]
 
 -- Since we are modeling undirected graphs, two links are essentially
 -- the same if they are the same up to orientation reversal.
-def almost_equal : link V E → link V E → Prop :=
+def almost_equal : link α β → link α β → Prop :=
 λ x y, x = y ∨ x = y.rev
 
-instance : has_equiv (link V E) := ⟨almost_equal⟩
+instance : has_equiv (link α β) := ⟨almost_equal⟩
 
-def lift {V' : set V} {E' : set E} (x : link V' E') : link V E := ⟨x.src.val, x.via.val, x.dest.val⟩
+def lift {V : set α} {E : set β} (x : link V E) : link α β := ⟨x.src.val, x.via.val, x.dest.val⟩
 
 end link
 
 end links
 
 
--- This is the type for multigraphs with vertex and edge sets in a
--- particular universe.  The V type is inside the structure so that we
--- may define a coercion from subgraphs to multigraphs.  If you want
--- to deal with graphs all with the same vertex then you might want to
--- use subgraphs of a complete graph.
+-- This is the type for multigraphs with vertex and edge sets that are
+-- drawn from some given types.
+--
+-- If you want to deal with graphs all with the same vertex then you
+-- might want to use subgraphs of a complete graph.
 --
 -- The links field carries the edges and attaches them to the
 -- vertices.  For non-loop edges, they come in pairs and represent the
 -- two possible orientations of the edge.  For loop edges, there is
 -- only a single link.
-structure multigraph :=
-(V : Type u) (E : Type v) (links : set (link V E))
-(has_links : ∀ e : E, ∃ {x : link V E}, x ∈ links ∧ e = x.via)
-(reversible : ∀ {x : link V E}, x ∈ links → x.rev ∈ links)
-(efficient : ∀ {x y : link V E}, x ∈ links → y ∈ links → x.via = y.via → x ≈ y)
+--
+-- This definition is drawn from [Chou94].
+--
+-- [Chou94] Chou, Ching-Tsun. "A formal theory of undirected graphs in
+-- Higher-Order Logic." (1994)
+-- https://doi.org/10.1007/3-540-58450-1_40
+structure multigraph (α : Type u) (β : Type v) :=
+(V : set α) (E : set β) (links : set (link α β))
+(link_src : link.src '' links ⊆ V)
+(link_via : link.via '' links = E)
+(link_rev : link.rev '' links ⊆ links)
+(efficient : ∀ {x y : link α β}, x ∈ links → y ∈ links → x.via = y.via → x ≈ y)
 
---(edges : V → V → set E)
---(has_edges : ∀ e, ∃ x y, e ∈ edges x y)
---(reversible : ∀ x y, edges x y = edges y x)
---(has_ends : ∀ {x y x' y' e}, e ∈ edges x y → e ∈ edges x' y' → (x = x' ∧ y = y') ∨ (x = y' ∧ y = x'))
+def multigraph.link_dest {α : Type u} {β : Type v} (g : multigraph α β) : link.dest '' g.links ⊆ g.V :=
+begin
+  rintros v ⟨x, xel, xdest⟩,
+  rw ←xdest,
+  exact g.link_src ⟨x.rev, g.link_rev ⟨x, xel, rfl⟩, rfl⟩,
+end
 
--- The complete simple graph with vertex set V.
---def complete_graph (V : Type u) : multigraph
 
 --instance (g : multigraph V) [fintype g.links] : fintype V := sorry
 --instance (g : multigraph V) [fintype g.links] : fintype g.E := sorry
 
 namespace multigraph
+variables {α : Type u} {β : Type v} {v w : α}
 
-def links_between (g : multigraph) (v w : g.V) : set (link g.V g.E) := {x ∈ g.links | x.src = v ∧ x.dest = w}
 
-lemma links_between_subset {g : multigraph} {v w : g.V} : g.links_between v w ⊆ g.links :=
+def links_between (g : multigraph α β) (v w : α) : set (link α β) := {x ∈ g.links | x.src = v ∧ x.dest = w}
+
+lemma links_between_subset {g : multigraph α β} (v w : α) : g.links_between v w ⊆ g.links :=
 λ x h, h.1
 
-lemma links_between_rev_iff {g : multigraph} {v w : g.V} {x : link g.V g.E}
+lemma links_between_rev_iff {g : multigraph α β} {v w : α} {x : link α β}
 : x ∈ g.links_between v w ↔ x.rev ∈ g.links_between w v :=
 begin
   split,
   rintro ⟨xel, h⟩,
-  use g.reversible xel,
-  rw [←h.1, ←h.2], split; cases x; simp only [link.rev],
-  rintro ⟨xrevel, h⟩,
-  have xel := g.reversible xrevel,
-  rw link.rev.involutive at xel,
-  use xel,
-  rw [←h.1, ←h.2], split; cases x; simp only [link.rev],
+  use g.link_rev ⟨x, xel, rfl⟩,  rw [←h.1, ←h.2], simp,
+  rintro ⟨xrel, h⟩,
+  have xel := g.link_rev ⟨x.rev, xrel, rfl⟩,
+  rw link.rev_rev at xel,
+  use xel, rw [←h.1, ←h.2], simp only [link.rev_src, link.rev_dest, eq_self_iff_true, and_self],
 end
 
-def edges (g : multigraph) (v w : g.V) : set g.E := link.via '' g.links_between v w
+def edges (g : multigraph α β) (v w : α) : set β := link.via '' g.links_between v w
 
-lemma edges_reversible (g : multigraph) : ∀ v w, g.edges v w = g.edges w v :=
+lemma edges_subset_edge_set (g : multigraph α β) (v w : α) : g.edges v w ⊆ g.E :=
+begin
+  rintros e ⟨x, ⟨xel, h⟩, xvia⟩,
+  rw ←g.link_via,
+  use [x, xel, xvia],
+end
+
+lemma edges_reversible (g : multigraph α β) : ∀ v w, g.edges v w = g.edges w v :=
 begin
   have key : ∀ v w e, e ∈ g.edges v w → e ∈ g.edges w v, {
-    intros v w e,
-    intro h,
-    rcases h with ⟨x,el,xvia⟩,
+    rintros v w e ⟨x,el,xvia⟩,
     use x.rev,
-    rw links_between_rev_iff,
-    rw link.rev.involutive,
+    rw [links_between_rev_iff, link.rev.involutive],
     use el,
     rwa link.rev_via,
   },
@@ -114,81 +127,100 @@ begin
   exact ⟨key v w e, key w v e⟩,
 end
 
-lemma edges_have_ends {g : multigraph} {x y x' y' : g.V} {e : g.E} (h : e ∈ g.edges x y) (h' : e ∈ g.edges x' y')
+lemma edges_have_ends {g : multigraph α β} {x y x' y' : α} {e : β} (h : e ∈ g.edges x y) (h' : e ∈ g.edges x' y')
 : (x = x' ∧ y = y') ∨ (x = y' ∧ y = x') :=
 begin
   rcases h with ⟨lx, lxbt, lxvia⟩,
   rcases h' with ⟨lx', lxbt', lxvia'⟩,
   have p : lx.via = lx'.via, cc,
-  have q := g.efficient lxbt.1 lxbt'.1 p,
   rw [←lxbt.2.1, ←lxbt.2.2],
   rw [←lxbt'.2.1, ←lxbt'.2.2],
-  cases q; cases lx; cases lx'; simp only [link.rev] at p q ⊢,
-  left, cc, right, cc,
+  cases g.efficient lxbt.1 lxbt'.1 p with q q; cases lx; cases lx'; simp only [link.rev] at p q ⊢,
+  left, tauto, right, tauto,
 end
+
+lemma has_link (g : multigraph α β) {e : β} (h : e ∈ g.E) : ∃ (x : link α β), x ∈ g.links ∧ x.via = e :=
+(set.ext_iff.mp g.link_via e).mpr h
+
+def links_at (g : multigraph α β) (v : α) : set (link α β) := {x ∈ g.links | x.src = v}
+
+-- The set of edges incident to a given vertex.  You might want
+-- `links_at` if you are traversing a graph.
+def incident (g : multigraph α β) (v : α) : set β := link.via '' links_at g v
+
+-- TODO this does not appear to be computable in general
+noncomputable
+def opp {g : multigraph α β} (v : α) {e} (h : e ∈ incident g v) : α := begin
+  exact link.dest (classical.some h),
+end
+
+
+--∃ w, e ∈ g.edges v w := h
+
 
 end multigraph
 
 
 section subgraphs
+variables {α : Type u} {β : Type v}
 open multigraph
 
 -- `subgraph g` is the type of all subgraphs of a given multigraph.
 -- These are not multigraphs per se, but they can be converted to
 -- multigraphs using `subgraph.to_graph`.  There is an instance to
 -- coerce subgraphs to multigraphs.
-structure subgraph (g : multigraph) :=
-(V : set g.V) (E : set g.E)
-(has_verts : ∀ {e}, e ∈ E → ∀ {x : link g.V g.E}, x ∈ g.links → e = x.via → x.src ∈ V ∧ x.dest ∈ V)
+structure subgraph (g : multigraph α β) :=
+(V : set α) (E : set β)
+(vert_is_subset : V ⊆ g.V) (edge_is_subset : E ⊆ g.E)
+(has_verts : ∀ {e}, e ∈ E → ∀ {x : link α β}, x ∈ g.links → e = x.via → x.src ∈ V)
 
---∃ {v w}, v ∈ V' ∧ w ∈ V' ∧ e ∈ g.edges v w)
+-- The dest version of subgraph.has_verts
+lemma subgraph.has_verts_dest {g : multigraph α β} (h : subgraph g) : ∀ {e}, e ∈ h.E → ∀ {x : link α β}, x ∈ g.links → e = x.via → x.dest ∈ h.V :=
+begin
+  intros e eel x xel hvia,
+  rw ←link.rev_src,
+  apply h.has_verts eel (g.link_rev ⟨x, xel, rfl⟩),
+  simpa only,
+end
 
 -- Construct a multigraph from the given subgraph.
-def subgraph.to_graph {g : multigraph} (g' : subgraph g) : multigraph := {
-  V := ↥g'.V,
-  E := ↥g'.E,
-  links := {x | ∃ (y : link g.V g.E), y ∈ g.links ∧ x.src.val = y.src ∧ x.via.val = y.via ∧ x.dest.val = y.dest},
-  has_links := begin
-    rintros ⟨e, p⟩,
-    rcases g.has_links e with ⟨x, xel, xvia⟩,
-    rcases g'.has_verts p xel xvia with ⟨hs, hd⟩,
-    use link.mk ⟨x.src, hs⟩ ⟨e, p⟩ ⟨x.dest, hd⟩,
-    use [x, xel, xvia],
+def subgraph.to_graph {g : multigraph α β} (g' : subgraph g) : multigraph α β := {
+  V := g'.V,
+  E := g'.E,
+  links := {x : link α β | x ∈ g.links ∧ x.via ∈ g'.E},
+  link_src := begin
+    rintros a ⟨x, ⟨xel, h⟩, xsrc⟩,
+    have key := g'.has_verts h xel rfl,
+    rwa xsrc at key,
   end,
-  reversible := begin
-    rintros x' ⟨x, xel, h⟩,
-    use [x.rev, g.reversible xel],
-    cases x, cc,
+  link_via := begin
+    ext e, split,
+    rintros ⟨x, ⟨⟨xel, h⟩, rfl⟩⟩,
+    exact h,
+    intro eel,
+    rcases g.has_link (g'.edge_is_subset eel) with ⟨x, xel, rfl⟩,
+    use [x, ⟨xel, eel⟩],
+  end,
+  link_rev := begin
+    rintros x' ⟨x, ⟨xel, h⟩, rfl⟩,
+    use g.link_rev ⟨x, xel, rfl⟩,
+    simpa,
   end,
   efficient := begin
-    rintros x' y' ⟨x, xel, xh⟩ ⟨y, yel, yh⟩ h,
-    have key : x.via = y.via, {
-      have h' := congr_arg subtype.val h,
-      rwa [xh.2.1, yh.2.1] at h',
-    },
-    cases g.efficient xel yel key with key' key',
-    left,
-    cases x' with xs xv xd, cases y' with ys yv yd,
-    simp only at xh yh h, rw h,
-    congr; apply subtype.eq; cc,
-    right,
-    cases x with xs xv xd, cases y with ys yv yd,
-    simp only [link.rev] at key',
-    cases x' with xs xv xd, cases y' with ys yv yd,
-    simp only at xh yh h, rw h at xh ⊢,
-    congr; apply subtype.eq; cc,
+    rintros x y ⟨xel, hx⟩ ⟨yel, hy⟩ vias,
+    exact g.efficient xel yel vias,
   end
 }
 
-instance {g : multigraph} (x : subgraph g) : has_coe (subgraph g) multigraph := ⟨subgraph.to_graph⟩
+instance {g : multigraph α β} (x : subgraph g) : has_coe (subgraph g) (multigraph α β) := ⟨subgraph.to_graph⟩
 
 @[ext]
-def subgraph.ext {g : multigraph} (x y : subgraph g) : x.V = y.V → x.E = y.E → x = y :=
+def subgraph.ext {g : multigraph α β} (x y : subgraph g) : x.V = y.V → x.E = y.E → x = y :=
 begin intros hv he, cases x, cases y, congr, exact hv, exact he, end
 
 section subgraph_lattice
 
-variables {g : multigraph}
+variables {g : multigraph α β}
 
 def subgraph.is_subgraph (x y : subgraph g) := x.V ⊆ y.V ∧ x.E ⊆ y.E
 
@@ -197,34 +229,24 @@ def subgraph.is_ssubgraph (x y : subgraph g) := x.is_subgraph y ∧ (x.V ⊂ y.V
 def subgraph.union (x y : subgraph g) : subgraph g := {
   V := x.V ∪ y.V,
   E := x.E ∪ y.E,
+  vert_is_subset := set.union_subset x.vert_is_subset y.vert_is_subset,
+  edge_is_subset := set.union_subset x.edge_is_subset y.edge_is_subset,
   has_verts := begin
-    intros e h,
+    rintros e h l lel rfl,
     cases h with h h,
-    rcases x.has_verts h with ⟨v, w, vin, win, h⟩,
-    use [v, w, or.inl vin, or.inl win, h],
-    rcases y.has_verts h with ⟨v, w, vin, win, h⟩,
-    use [v, w, or.inr vin, or.inr win, h],
-  end}
+    exact or.inl (x.has_verts h lel rfl),
+    exact or.inr (y.has_verts h lel rfl),
+  end
+}
 
 def subgraph.inter (x y : subgraph g) : subgraph g := {
   V := x.V ∩ y.V,
   E := x.E ∩ y.E,
+  vert_is_subset := set.subset.trans (set.inter_subset_left _ _) x.vert_is_subset,
+  edge_is_subset := set.subset.trans (set.inter_subset_left _ _) x.edge_is_subset,
   has_verts := begin
-    intros e h,
-    rcases x.has_verts h.1 with ⟨xv, xw, xvin, xwin, xl, ⟨xlin, xls, xld⟩, xlvia⟩,
-    rcases y.has_verts h.2 with ⟨yv, yw, yvin, ywin, yl, ⟨ylin, yls, yld⟩, ylvia⟩,
-    have via_eq : xl.via = yl.via, cc,
-    cases g.efficient xlin ylin via_eq with eq eq;
-      rw eq at *; rw xls at *; rw yls at *; rw xld at *; rw yld at *,
-    use [yv, yw, ⟨xvin, yvin⟩, ⟨xwin, ywin⟩], rw ←xlvia,
-    
-    
-    -- cases g.has_ends xh yh with eqs eqs;
-    --   rw eqs.1 at xvin;
-    --   rw eqs.2 at xwin,
-    -- use [yv, yw, ⟨xvin, yvin⟩, ⟨xwin, ywin⟩, yh],
-    -- rw g.reversible at yh,
-    -- use [yw, yv, ⟨xvin, ywin⟩, ⟨xwin, yvin⟩, yh],
+    rintros e h l lel rfl,
+    exact ⟨x.has_verts h.1 lel rfl, y.has_verts h.2 lel rfl⟩,
   end
 }
 
@@ -234,18 +256,18 @@ instance : has_union (subgraph g) := ⟨subgraph.union⟩
 instance : has_inter (subgraph g) := ⟨subgraph.inter⟩
 
 def subgraph.top : subgraph g := {
-  V := ⊤,
-  E := ⊤,
-  has_verts := begin
-    intros e h,
-    rcases g.has_edges e with ⟨v, w, ein⟩,
-    refine ⟨v, w, trivial, trivial, ein⟩,
-  end
+  V := g.V,
+  E := g.E,
+  vert_is_subset := set.subset.refl _,
+  edge_is_subset := set.subset.refl _,
+  has_verts := by rintros e h l lel rfl; exact g.link_src ⟨l, lel, rfl⟩
 }
-def subgraph.bot : subgraph g :=  {
+def subgraph.bot : subgraph g := {
   V := ∅,
   E := ∅,
-  has_verts := begin intros e h, exfalso, rwa set.mem_empty_eq at h, end
+  vert_is_subset := set.empty_subset _,
+  edge_is_subset := set.empty_subset _,
+  has_verts := by rintros e h l lel rfl; tauto
 }
 
 instance : bounded_lattice (subgraph g) := {
@@ -262,7 +284,7 @@ instance : bounded_lattice (subgraph g) := {
     exact set.subset.antisymm hxy.1 hyx.1,
     exact set.subset.antisymm hxy.2 hyx.2,
   end,
-  le_top := by intro a; split; exact set.subset_univ _,
+  le_top := λ x, ⟨x.vert_is_subset, x.edge_is_subset⟩,
   bot_le := by intro a; split; exact set.empty_subset _,
   sup := subgraph.union,
   inf := subgraph.inter,
@@ -283,20 +305,6 @@ end subgraph_lattice
 
 
 end subgraphs
-
-section accessors
-variable {g : multigraph}
-
--- The set of edges incident to a given vertex.
-def incident (v : g.V) : set g.E := {e | ∃ w, e ∈ g.edges v w}
-
--- The vertex opposite an edge incident to a given vertex.  This
--- function is mainly here as a note that we cannot make a g.E-valued
--- function due to the way multigraphs are defined.
-def opp (v : g.V) {e} (h : e ∈ incident v) : ∃ w, e ∈ g.edges v w := h
-
-end accessors
-
 
 end graph
 

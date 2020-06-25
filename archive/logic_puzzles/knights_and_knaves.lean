d5a08b76c038ea4f16fc2aaf38e049c11c884a41
Dan Stanescu <stanescu@uwyo.edu>
2020-06-25 08:11:52 -0600

    Update knights_and_knaves.lean
    Improved puzzle descriptions, added links.

--- archive/logic_puzzles/knights_and_knaves.lean
+++ archive/logic_puzzles/knights_and_knaves.lean
@@ -9,25 +9,27 @@ import tactic
 /-!
 # Knights and knaves puzzles
 Two puzzles from "Knights and Knaves" by Raymond Smullyan.
+For an online description of these puzzles, see: http://mesosyn.com/mental1-6.html
+More information on R. Smullyan: https://en.wikipedia.org/wiki/Raymond_Smullyan
 
 There is an island where all inhabitants are either knights or knaves. 
 Knights always tell the truth. Knaves always lie.
-Jack and Bob are two inhabitants of this island.
+Joe and Bob are two inhabitants of this island.
 -/
 
-inductive person
+inductive islander
 | knight
 | knave
 
-notation `y` := person.knight
-notation `n` := person.knave
+notation `y` := islander.knight
+notation `n` := islander.knave
 
-structure Q := (p₁ p₂ : person)
+structure Q := (Joe Bob : islander)
 
 /--
-In this first puzzle, only Jack makes a statement.
-He affirms that both our heroes (i.e. both Jack and Bob) are knaves.
-What kind of islanders are they?
+In this first puzzle, only Joe makes a statement.
+He affirms that both protagonist (i.e. both Joe and Bob) are knaves.
+Question: what kind of islanders are they in fact?
 -/
 
 def Q1 := Q
@@ -36,8 +38,9 @@ namespace Q1
 
 variables (q : Q1)
 
+-- Joe's statement; Joe is the first member of q
 def S1 := q.1 = n ∧ q.2 = n
-
+-- Stating that Joe can be a knight or a knave
 def H := (q.1 = y ∧ q.S1) ∨ (q.1 = n ∧ ¬ q.S1)
 
 lemma answer : q.H → q.1 = n ∧ q.2 = y :=
@@ -50,10 +53,10 @@ end
 end Q1
 
 /--
-In the second puzzle, both Jack and Bob make a statement.
-Jack states that he's a knave if and only if Bob is a knave.
+In the second puzzle, both Joe and Bob make a statement.
+Joe states that he's a knave if and only if Bob is a knave.
 Bob only states that they are different kinds. 
-Again, what kind of islanders are our protagonists?
+Question: again, what kind of islanders are our protagonists?
 -/
 
 
@@ -63,9 +66,11 @@ namespace Q2
 
 variables (q : Q2)
 
+-- Joe's statement; Joe is the first member of q
 def S1 := q.1 = n ↔ q.2 = n
+-- Bob's statement; Bob is the second member of q
 def S2 := q.1 = y ∧ q.2 = n ∨ q.1 = n ∧ q.2 = y
-
+-- Stating that either one can be a knight or a knave
 def H1 := (q.1 = y ∧ q.S1) ∨ (q.1 = n ∧ ¬ q.S1)
 def H2 := (q.2 = y ∧ q.S2) ∨ (q.2 = n ∧ ¬ q.S2)
 def H := q.H1 ∧ q.H2

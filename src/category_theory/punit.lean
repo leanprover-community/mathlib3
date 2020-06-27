ffa5540f85fbbb4e36b4c069be9a4bf95e96586e
Bhavik Mehta <bhavikmehta8@gmail.com>
2020-06-27 14:26:31 +0100

    refactor punit

--- src/category_theory/punit.lean
+++ src/category_theory/punit.lean
@@ -1,26 +1,28 @@
 /-
 Copyright (c) 2018 Scott Morrison. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
-Authors: Scott Morrison
+Authors: Scott Morrison, Bhavik Mehta
 -/
 import category_theory.const
+import category_theory.discrete_category
 
-universes v w u -- declare the `v`'s first; see `category_theory.category` for an explanation
+universes v u -- declare the `v`'s first; see `category_theory.category` for an explanation
 
 namespace category_theory
 
-instance punit_category : small_category punit :=
-{ hom  := λ X Y, punit,
-  id   := λ _, punit.star,
-  comp := λ _ _ _ _ _, punit.star }
-
 namespace functor
-variables {C : Type u} [category.{v} C]
+variables (C : Type u) [category.{v} C]
 
-/-- The constant functor sending everything to `punit.start`. -/
-def star : C ⥤ punit.{w+1} := (const C).obj punit.star
-@[simp] lemma star_obj (X : C) : star.obj X = punit.star := rfl
-@[simp] lemma star_map {X Y : C} (f : X ⟶ Y) : star.map f = 𝟙 _ := rfl
+/-- The constant functor sending everything to `punit.star`. -/
+@[simps]
+def star : C ⥤ discrete punit :=
+{ obj := λ _, punit.star,
+  map := λ _ _ _, 𝟙 _ }
+
+variable {C}
+/-- Any two functors to `discrete punit` are isomorphic. -/
+def punit_ext (F G : C ⥤ discrete punit) : F ≅ G :=
+nat_iso.of_components (λ _, eq_to_iso dec_trivial) (λ _ _ _, dec_trivial)
 
 end functor
 

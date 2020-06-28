cfe24bf1aa084c7a26409832e00fec7782557440
Patrick Massot <patrickmassot@free.fr>
2020-06-28 23:44:36 +0200

    More proof comments and some reorganization

--- src/topology/uniform_space/compact_separated.lean
+++ src/topology/uniform_space/compact_separated.lean
@@ -75,9 +75,7 @@ begin
   haveI : @compact_space α u'.to_topological_space := by rw h' ; assumption,
   haveI : @separated_space α u := by rwa [separated_iff_t2, h],
   haveI : @separated_space α u' :=  by rwa [separated_iff_t2, h'],
-  rw compact_space_uniformity,
-  rw compact_space_uniformity,
-  rw [h, h'],
+  rw [compact_space_uniformity, compact_space_uniformity, h, h']
 end
 
 /-- The unique uniform structure inducing a given compact Hausdorff topological structure. -/
@@ -97,33 +95,44 @@ def uniform_space_of_compact_t2 {α : Type*} [topological_space α] [compact_spa
     erw [nhds_prod_eq, ← prod_comm],
   end,
   comp := begin
-    set 𝓝Δ := ⨆ x : α, 𝓝 (x, x),
-    set F := 𝓝Δ.lift' (λ (s : set (α × α)), s ○ s),
     classical,
-    haveI : normal_space α := normal_of_compact_t2,
+    /-
+    This is the difficult part of the proof. We need to prove that, for each neighborhood W
+    of the diagonal Δ, W ○ W is still a neighborhood of the diagonal.
+    -/
+    set 𝓝Δ := ⨆ x : α, 𝓝 (x, x), -- The filter of neighborhoods of Δ
+    set F := 𝓝Δ.lift' (λ (s : set (α × α)), s ○ s), -- Compositions of neighborhoods of Δ
+    -- If this weren't true, then there would be V ∈ 𝓝Δ such that F ⊓ 𝓟 (-V) ≠ ⊥
     rw le_iff_forall_inf_principal_compl,
     intros V V_in,
+    by_contra H,
+    -- Hence compactness would give us a cluster point (x, y) for F ⊓ 𝓟 (-V)
+    obtain ⟨⟨x, y⟩, hxy⟩ : ∃ (p : α × α), cluster_pt p (F ⊓ 𝓟 (-V)) := cluster_point_of_compact H,
+    -- In particular (x, y) is a cluster point of 𝓟 (-V), hence is not in the interior of V,
+    -- and a fortiori not in Δ, so x ≠ y
+    have clV : cluster_pt (x, y) (𝓟 $ -V) := hxy.of_inf_right,
+    have : (x, y) ∉ interior V,
+    { have : (x, y) ∈ closure (-V), by rwa mem_closure_iff_cluster_pt,
+      rwa closure_compl at this },
     have diag_subset : diagonal α ⊆ interior V,
     { rw subset_interior_iff_nhds,
       rintros ⟨x, x⟩ ⟨⟩,
       exact (mem_supr_sets.mp V_in : _) x },
-    by_contra H,
-    obtain ⟨⟨x, y⟩, hxy⟩ : ∃ (p : α × α), cluster_pt p (F ⊓ 𝓟 (-V)) := cluster_point_of_compact H,
-    have clF : cluster_pt (x, y) F := hxy.of_inf_left,
-    have clV : cluster_pt (x, y) (𝓟 $ -V) := hxy.of_inf_right,
-
-    have : (x, y) ∉ interior V,
-    { have : (x, y) ∈ closure (-V), by rwa mem_closure_iff_cluster_pt,
-      rwa closure_compl at this },
     have x_ne_y : x ≠ y,
     { intro h,
       apply this,
       apply diag_subset,
-      simp [h] }, clear this diag_subset,
+      simp [h] },
+    -- Since α is compact and Hausdorff, it is normal, hence regular.
+    haveI : normal_space α := normal_of_compact_t2,
+    -- So there are closed neighboords V₁ and V₂ of x and y contained in disjoint open neighborhoods
+    -- U₁ and U₂.
     obtain ⟨U₁, V₁, U₁_in, V₁_in, U₂, V₂, U₂_in₂, V₂_in, V₁_cl, V₂_cl, U₁_op, U₂_op, VU₁, VU₂, hU₁₂⟩ :
        ∃ (U₁ V₁ ∈ 𝓝 x) (U₂ V₂ ∈ 𝓝 y), is_closed V₁ ∧ is_closed V₂ ∧ is_open U₁ ∧ is_open U₂ ∧
                                        V₁ ⊆ U₁ ∧ V₂ ⊆ U₂ ∧ U₁ ∩ U₂ = ∅ :=
-    disjoint_nested_nhds x_ne_y,
+       disjoint_nested_nhds x_ne_y,
+    -- We set U₃ := -(V₁ ∪ V₂) so that W := (U₁.prod U₁) ∪ (U₂.prod U₂) ∪ (U₃.prod U₃) is an open
+    -- neighborhood of Δ.
     let U₃ := -(V₁ ∪ V₂),
     have U₃_op : is_open U₃ :=
       is_open_compl_iff.mpr (is_closed_union V₁_cl V₂_cl),
@@ -139,35 +148,46 @@ def uniform_space_of_compact_t2 {α : Type*} [topological_space α] [compact_spa
           rw mem_prod,
           tauto }, },
       all_goals { simp only [is_open_prod, *] } },
+    -- So W ○ W ∈ F by definition of F
+    have : W ○ W ∈ F,
+    { dsimp [F],-- Lean has weird elaboration trouble with this line
+      exact mem_lift' W_in },
+    -- And V₁.prod V₂ ∈ 𝓝 (x, y)
     have hV₁₂ : V₁.prod V₂ ∈ 𝓝 (x, y) := prod_mem_nhds_sets V₁_in V₂_in,
+    -- But (x, y) is also a cluster point of F so (V₁.prod V₂) ∩ (W ○ W) ≠ ∅
+    have clF : cluster_pt (x, y) F := hxy.of_inf_left,
+    obtain ⟨p, p_in⟩ : ∃ p, p ∈ (V₁.prod V₂) ∩ (W ○ W) :=
+      cluster_pt_iff.mp clF hV₁₂ this,
+    -- However the construction of W implies (V₁.prod V₂) ∩ (W ○ W) = ∅.
+    -- Indeed assume for contradiction there is some (u, v) in the intersection.
+    -- So u ∈ V₁, v ∈ V₂, and there exists some w such that (u, w) ∈ W and (w ,v) ∈ W.
+    -- Because u is in V₁ which is disjoint from U₂ and U₃, (u, w) ∈ W forces (u, w) ∈ U₁.prod U₁.
+    -- Similarly, because v ∈ V₂, (w ,v) ∈ W forces (w, v) ∈ U₂.prod U₂.
+    -- Hence w ∈ U₁ ∩ U₂ which is empty.
     have inter_empty : (V₁.prod V₂) ∩ (W ○ W) = ∅,
     { rw eq_empty_iff_forall_not_mem,
       rintros ⟨u, v⟩ ⟨⟨u_in, v_in⟩, w, huw, hwv⟩,
-      have uw_in : (u, w) ∈ U₁.prod U₁,
-        by finish [set.ext_iff, set.subset_def],
-      have wv_in : (w, v) ∈ U₂.prod U₂,
-        by finish [set.ext_iff, set.subset_def],
+      have uw_in : (u, w) ∈ U₁.prod U₁ :=
+        set.mem_prod.2 ((huw.resolve_right (λ h, (h.1 $ or.inl u_in))).resolve_right
+        (λ h, have u ∈ U₁ ∩ U₂, from ⟨VU₁ u_in, h.1⟩, by rwa hU₁₂ at this)),
+      have wv_in : (w, v) ∈ U₂.prod U₂ :=
+        set.mem_prod.2 ((hwv.resolve_right (λ h, (h.2 $ or.inr v_in))).resolve_left
+        (λ h, have v ∈ U₁ ∩ U₂, from ⟨h.2, VU₂ v_in⟩, by rwa hU₁₂ at this)),
       have : w ∈ U₁ ∩ U₂ := ⟨uw_in.2, wv_in.1⟩,
       rwa hU₁₂ at this },
-    have : W ○ W ∈ F,
-    { -- Lean has weird elaboration trouble here
-      dsimp [F],
-      exact mem_lift' W_in },
-    obtain ⟨p, p_in⟩ : ∃ p, p ∈ (V₁.prod V₂) ∩ (W ○ W) :=
-      cluster_pt_iff.mp clF hV₁₂ this,
+    -- So we have a contradiction
     rwa inter_empty at p_in,
   end,
   is_open_uniformity := begin
-    dsimp only,
-    intros s,
-    change is_open s ↔ _,
-    simp_rw [is_open_iff_mem_nhds, nhds_eq_comap_uniformity_aux, comap_supr, nhds_prod_eq],
-    apply forall_congr,
+    -- Here we need to prove the topology induced by the constructed uniformity is the
+    -- topology we started with.
+    suffices : ∀ x : α,  comap (prod.mk x) (⨆ y, 𝓝 (y ,y)) = 𝓝 x,
+    { intros s,
+      change is_open s ↔ _,
+      simp_rw [is_open_iff_mem_nhds, nhds_eq_comap_uniformity_aux, this] },
     intros x,
-    apply forall_congr,
-    intros x_in,
-    convert iff.rfl,
-    simp_rw [comap_prod, show prod.fst ∘ prod.mk x = λ y : α, x, by ext ; simp,
+    simp_rw [comap_supr, nhds_prod_eq, comap_prod,
+             show prod.fst ∘ prod.mk x = λ y : α, x, by ext ; simp,
              show prod.snd ∘ (prod.mk x) = (id : α → α), by ext ; refl, comap_id],
     rw [supr_split_single _ x, comap_const_of_mem (λ V, mem_of_nhds)],
     suffices : ∀ y ≠ x, comap (λ (y : α), x) (𝓝 y) ⊓ 𝓝 y ≤ 𝓝 x,

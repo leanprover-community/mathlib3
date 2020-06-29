d3006badc2af402a15537995b1bb97d009e0ded4
Yury G. Kudryashov <urkud@urkud.name>
2020-06-29 16:12:00 +0000

    chore(init_/): remove this directory (#3227)
    * remove `init_/algebra`;
    * move `init_/data/nat` to `data/nat/basic`;
    * move `init_/data/int` to `data/int/basic`.

--- test/library_search/ordered_ring.lean
+++ test/library_search/ordered_ring.lean
@@ -3,8 +3,7 @@ Copyright (c) 2018 Scott Morrison. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Scott Morrison
 -/
-import algebra.ordered_ring
-import init_.data.nat.lemmas
+import data.nat.basic
 
 /- Turn off trace messages so they don't pollute the test build: -/
 set_option trace.silence_library_search true

40a37ede14d6c64a8079dd676a2a9fd27e401377
Gabriel Ebner <gebner@gebner.org>
2020-06-29 21:17:20 +0200

    Add basic module doc string.

--- src/tactic/interactive_expr.lean
+++ src/tactic/interactive_expr.lean
@@ -5,6 +5,25 @@ Released under Apache 2.0 license as described in the file LICENSE.
 Author: E.W.Ayers
 -/
 
+/-!
+# Widgets used for tactic state and term-mode goal display
+
+The vscode extension supports the display of interactive widgets.
+Default implementation of these widgets are included in the core
+library.  We override them here using `vm_override` so that we can
+change them quickly without waiting for the next Lean release.
+
+The function `widget_override.interactive_expression.mk` renders a single
+expression as a widget component.  Each goal in a tactic state is rendered
+using the `widget_override.tactic_view_goal` function,
+a complete tactic state is rendered using
+`widget_override.tactic_view_component`.
+
+Lean itself calls the `widget_override.term_goal_widget` function to render
+term-mode goals and `widget_override.tactic_state_widget` to render the
+tactic state in a tactic proof.
+-/
+
 namespace widget_override
 open widget
 

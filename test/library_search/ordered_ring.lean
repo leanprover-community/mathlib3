061557bf3c4d52166f53f964422831d353d5fb80
Scott Morrison <scott.morrison@gmail.com>
2020-06-30 20:39:17 +1000

    fix tests

--- test/library_search/ordered_ring.lean
+++ test/library_search/ordered_ring.lean
@@ -3,12 +3,14 @@ Copyright (c) 2018 Scott Morrison. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Scott Morrison
 -/
-import tactic.suggest
+import tactic.basic
+import data.nat.basic
 import algebra.ordered_ring
-import init_.data.nat.lemmas
 
 /- Turn off trace messages so they don't pollute the test build: -/
 set_option trace.silence_library_search true
+
+set_option pp.all true
 
 example {a b : ℕ} (h : b > 0) (w : a ≥ 1) : b ≤ a * b :=
 by library_search -- exact (le_mul_iff_one_le_left h).mpr w

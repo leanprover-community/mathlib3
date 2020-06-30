3f5d327057d9537855a0a60ff6483b13e3ea7368
Vierkantor <vierkantor@vierkantor.com>
2020-06-30 10:48:28 +0000

    Update mathlbi

--- src/algebra/group_with_zero.lean
+++ src/algebra/group_with_zero.lean
@@ -4,6 +4,7 @@ Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Johan Commelin
 -/
 import algebra.group.commute
+import algebra.group.hom
 import tactic.alias
 import tactic.push_neg
 
--- src/data/polynomial.lean
+++ src/data/polynomial.lean
@@ -1713,7 +1713,7 @@ lemma is_root_of_aeval_algebra_map_eq_zero [algebra R S] {p : polynomial R}
   {r : R} (hr : aeval R S (algebra_map R S r) p = 0) : p.is_root r :=
 is_root_of_eval₂_map_eq_zero inj hr
 
-lemma dvd_term_of_dvd_eval_of_dvd_terms {z p : R} {f : polynomial R} (i : ℕ)
+lemma dvd_term_of_dvd_eval_of_dvd_terms {z p : A} {f : polynomial A} (i : ℕ)
   (dvd_eval : p ∣ f.eval z) (dvd_terms : ∀ (j ≠ i), p ∣ f.coeff j * z ^ j) :
   p ∣ f.coeff i * z ^ i :=
 begin
@@ -1730,7 +1730,7 @@ begin
     exact finsupp.not_mem_support_iff.mp hi }
 end
 
-lemma dvd_term_of_is_root_of_dvd_terms {r p : R} {f : polynomial R} (i : ℕ)
+lemma dvd_term_of_is_root_of_dvd_terms {r p : A} {f : polynomial A} (i : ℕ)
   (hr : f.is_root r) (h : ∀ (j ≠ i), p ∣ f.coeff j * r ^ j) : p ∣ f.coeff i * r ^ i :=
 dvd_term_of_dvd_eval_of_dvd_terms i (eq.symm hr ▸ dvd_zero p) h
 

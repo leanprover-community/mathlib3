7a43ab664a4866c6b11089ae570c14df9de281bb
Kyle Miller <kmill31415@gmail.com>
2020-07-01 17:46:45 -0700

    feat(data/sym2) Defining the symmetric square (unordered pairs)
    Incorporated suggestions from code review.

--- src/data/sym2.lean
+++ src/data/sym2.lean
@@ -14,6 +14,13 @@ open function
 This file defines symmetric squares, which is `α × α` modulo swapping.
 This is also known as the type of unordered pairs.
 
+From the point of view that an unordered pair is equivalent to a
+multiset of cardinality two, we have `sym2.mem` (the `has_mem`
+instance), which is a `Prop`-valued membership test.  Given `a ∈ z`
+for `z : sym2 α`, it does not appear to be possible, in general, to
+*computably* give the other element in the pair.  For this,
+`sym2.vmem a z` is a `Type`-valued predicate that contains this other
+element.
 
 ## Notation
 
@@ -26,100 +33,85 @@ symmetric square, unordered pairs
 -/
 
 namespace sym2
-universe u
-variables {α : Type u}
+variables {α : Type*}
 
 /--
 This is the relation capturing the notion of pairs equivalent up to permutations.
 -/
-inductive rel (α : Type u) : (α × α) → (α × α) → Prop
+inductive rel (α : Type*) : (α × α) → (α × α) → Prop
 | refl (x y : α) : rel (x, y) (x, y)
 | swap (x y : α) : rel (x, y) (y, x)
 
 attribute [refl] rel.refl
 
 @[symm] lemma rel.symm {x y : α × α} : rel α x y → rel α y x :=
-by { tidy, cases a, exact a, apply rel.swap }
+by { intro a, cases a, exact a, apply rel.swap }
 
 @[trans] lemma rel.trans {x y z : α × α} : rel α x y → rel α y z → rel α x z :=
-by { tidy, cases_matching* rel _ _ _; apply rel.refl <|> apply rel.swap }
+by { intros a b, cases_matching* rel _ _ _; apply rel.refl <|> apply rel.swap }
 
-instance rel.setoid (α : Type u) : setoid (α × α) :=
-by { use rel α, tidy, apply rel.trans a a_1 }
+instance rel.setoid (α : Type*) : setoid (α × α) :=
+by { use rel α,
+     split, { intros x, cases x, refl },
+     split, { apply rel.symm },
+     { intros x y z a b, apply rel.trans a b } }
+
+end sym2
 
 /--
 This is the type for the symmetric square (i.e., the type of unordered pairs).
 -/
 @[reducible]
-def sym2 (α : Type u) [h : setoid (α × α)] := quotient h
+def sym2 (α : Type*) := quotient (sym2.rel.setoid α)
+
+namespace sym2
+universe u
+variables {α : Type u}
 
 lemma eq_swap {a b : α} : ⟦(a, b)⟧ = ⟦(b, a)⟧ :=
 by { rw quotient.eq, apply rel.swap }
 
-lemma other_unique (a b c : α) : ⟦(a, b)⟧ = ⟦(a, c)⟧ ↔ b = c :=
+lemma congr_right (a b c : α) : ⟦(a, b)⟧ = ⟦(a, c)⟧ ↔ b = c :=
 begin
   split,
   intro h, rw quotient.eq at h, cases h; refl,
   intro h, apply congr_arg _ h,
 end
 
-/--
-A type `α` is naturally included in the diagonal of `α × α`, this function gives the image
-of this diagonal in `sym2 α`.
--/
-def incl (x : α) : sym2 α := ⟦(x, x)⟧
 
 /--
-Checks whether a given element of `sym2 α` is in the image of `incl`.
+The functor `sym2` is functorial, and this function constructs the induced maps.
 -/
-def is_diag (z : sym2 α) : Prop :=
-quotient.rec_on z (λ y, y.1 = y.2) (by { tidy, cases p; refl, cases p; refl })
-
-def image_incl_is_diag (z : sym2 α) (h : is_diag z) : ∃ x, z = incl x :=
-by tidy
-
-lemma diag_to_eq (x y : α) (h : is_diag ⟦(x, y)⟧) : x = y :=
-by tidy
-
-/--
-The functor `sym2` is functorial, and this constructs induced maps.
--/
-def induce {α β : Type u} (f : α → β) : sym2 α → sym2 β :=
-λ z, quotient.rec_on z (λ x, ⟦(f x.1, f x.2)⟧)
-begin
-  intros, rw [eq_rec_constant, quotient.eq],
-  cases p, refl, apply rel.swap,
+def map {α β : Type*} (f : α → β) : sym2 α → sym2 β :=
+quotient.map (prod.map f f) begin
+  rintros ⟨a₁, a₂⟩ ⟨b₁, b₂⟩ h,
+  cases h,
+  apply rel.refl,
+  apply rel.swap,
 end
 
 @[simp]
-def induce.id : sym2.induce (@id α) = id :=
+lemma map_id : sym2.map (@id α) = id :=
 by tidy
 
-def induce.functorial {α β γ: Type u} {g : β → γ} {f : α → β} : sym2.induce (g ∘ f) = sym2.induce g ∘ sym2.induce f :=
+lemma map_comp {α β γ: Type*} {g : β → γ} {f : α → β} : sym2.map (g ∘ f) = sym2.map g ∘ sym2.map f :=
 by tidy
-
-instance is_diag.decidable_pred (α : Type u) [decidable_eq α] : decidable_pred (@is_diag α) :=
-begin
-  intro x,
-  induction x,
-  solve_by_elim,
-  cases x_p,
-  simp only [],
-  apply subsingleton.elim,
-end
 
 section membership
 
 /-! ### Declarations about membership -/
 
 /--
-This is a predicate that determines whether a given term is a member of a term of the symmetric square.
-From this point of view, the symmetric square is the subtype of cardinality-two multisets on `α`.
+This is a predicate that determines whether a given term is a member of a term of the
+symmetric square.  From this point of view, the symmetric square is the subtype of
+cardinality-two multisets on `α`.
 -/
 def mem (x : α) (z : sym2 α) : Prop :=
 ∃ (y : α), z = ⟦(x, y)⟧
 
-def mk_has_mem (x y : α) : mem x ⟦(x, y)⟧ :=
+instance : has_mem α (sym2 α) := ⟨mem⟩
+
+lemma mk_has_mem (x y : α) : x ∈ ⟦(x, y)⟧ :=
 ⟨y, rfl⟩
 
 /--
@@ -131,8 +123,11 @@ def vmem (x : α) (z : sym2 α) : Type u :=
 {y : α // z = ⟦(x, y)⟧}
 
 instance (x : α) (z : sym2 α) : subsingleton {y : α // z = ⟦(x, y)⟧} :=
-⟨by { rintros ⟨a, ha⟩ ⟨b, hb⟩, rw ha at hb, rw other_unique at hb, tidy, }⟩
+⟨by { rintros ⟨a, ha⟩ ⟨b, hb⟩, rw ha at hb, rw congr_right at hb, tidy, }⟩
 
+/--
+The `vmem` version of `mk_has_mem`.
+-/
 def mk_has_vmem (x y : α) : vmem x ⟦(x, y)⟧ :=
 ⟨y, rfl⟩
 
@@ -141,41 +136,78 @@ instance {a : α} {z : sym2 α} : has_lift (vmem a z) (mem a z) := ⟨λ h, ⟨h
 /--
 Given an element of a term of the symmetric square (using `vmem`), retrieve the other element.
 -/
-lemma other {a : α} {p : sym2 α} (h : vmem a p) : α := h.val
+def vmem.other {a : α} {p : sym2 α} (h : vmem a p) : α := h.val
 
 /--
 The defining property of the other element is that it can be used to
 reconstruct the term of the symmetric square.
 -/
-lemma other_spec (a : α) (z : sym2 α) (h : vmem a z) : z = ⟦(a, other h)⟧ :=
-by { dunfold other, tidy, }
+lemma vmem_other_spec {a : α} {z : sym2 α} (h : vmem a z) : z = ⟦(a, h.other)⟧ :=
+by { dunfold vmem.other, tidy, }
 
 /--
 This is the `mem`-based version of `other`.
 -/
-noncomputable
-lemma mem_other {a : α} {z : sym2 α} (h : mem a z) : α :=
+noncomputable def mem.other {a : α} {z : sym2 α} (h : a ∈ z) : α :=
 classical.some h
 
-lemma mem_other_spec (a : α) (z : sym2 α) (h : mem a z) : ⟦(a, mem_other h)⟧ = z :=
+lemma mem_other_spec {a : α} {z : sym2 α} (h : a ∈ z) :
+  ⟦(a, h.other)⟧ = z :=
 begin
-  dunfold mem_other,
+  dunfold mem.other,
   exact (classical.some_spec h).symm,
 end
 
-lemma other_is_mem_other {a : α} {z : sym2 α} (h : vmem a z) (h' : mem a z) :
-  other h = mem_other h' :=
-by rw [←other_unique a, ←other_spec a z, mem_other_spec]
+lemma other_is_mem_other {a : α} {z : sym2 α} (h : vmem a z) (h' : a ∈ z) :
+  h.other = mem.other h' :=
+by rw [←congr_right a, ←vmem_other_spec h, mem_other_spec]
 
-lemma eq {x y z w : α} (h : ⟦(x, y)⟧ = ⟦(z, w)⟧):
-  (x = z ∧ y = w) ∨ (x = w ∧ y = z) :=
-by { rw quotient.eq at h, cases h; tidy }
+lemma eq_pairs {x y z w : α} :
+  ⟦(x, y)⟧ = ⟦(z, w)⟧ ↔ (x = z ∧ y = w) ∨ (x = w ∧ y = z) :=
+by { split; intro h,
+     { rw quotient.eq at h, cases h; tidy, },
+     { cases h; rw [h.1, h.2], rw eq_swap, } }
 
-lemma is_one_of {a b c : α} (h : mem a ⟦(b, c)⟧) : a = b ∨ a = c :=
-by { cases h, have p := eq h_h, tidy }
+lemma mem_iff {a b c : α} : a ∈ ⟦(b, c)⟧ ↔ a = b ∨ a = c :=
+begin
+  split,
+  { intro h, cases h, rw eq_pairs at h_h, tidy },
+  { intro h, cases h, rw h, apply mk_has_mem,
+    rw h, rw eq_swap, apply mk_has_mem, }
+end
 
 end membership
 
+/--
+A type `α` is naturally included in the diagonal of `α × α`, this function gives the image
+of this diagonal in `sym2 α`.
+-/
+def diag (x : α) : sym2 α := ⟦(x, x)⟧
+
+/--
+A predicate for testing whether an element of `sym2 α` is on the diagonal.
+-/
+def is_diag (z : sym2 α) : Prop :=
+z ∈ set.range (@diag α)
+
+lemma is_diag_iff_proj_eq (z : α × α) : is_diag ⟦z⟧ ↔ z.1 = z.2 :=
+begin
+  cases z with a b,
+  split,
+  { intro h, cases h with x h, dsimp [diag] at h,
+    cases eq_pairs.mp h with h h; rw [←h.1, ←h.2], },
+  { intro h, dsimp at h, rw h, use b, simp [diag], },
+end
+
+instance is_diag.decidable_pred (α : Type u) [decidable_eq α] : decidable_pred (@is_diag α) :=
+begin
+  intro z,
+  induction z,
+  change decidable (is_diag ⟦z⟧),
+  rw is_diag_iff_proj_eq,
+  apply_instance,
+  apply subsingleton.elim,
+end
 
 section relations
 
@@ -183,17 +215,20 @@ section relations
 
 variables {r : α → α → Prop}
 
-def in_rel (sym : symmetric r) (z : sym2 α) : Prop :=
-quotient.rec_on z (λ z, r z.1 z.2) (begin
+/--
+Symmetric relations define a set on `sym2 α` by taking all those elements that are related.
+-/
+def from_rel (sym : symmetric r) : set (sym2 α) :=
+(λ z, quotient.rec_on z (λ z, r z.1 z.2) (begin
   intros z w p,
   cases p,
   simp,
   simp,
   split; apply sym,
-end)
+end))
 
 @[simp]
-lemma in_rel_prop {sym : symmetric r} {a b : α} : in_rel sym ⟦(a, b)⟧ ↔ r a b :=
+lemma from_rel_prop {sym : symmetric r} {a b : α} : ⟦(a, b)⟧ ∈ from_rel sym  ↔ r a b :=
 by tidy
 
 end relations

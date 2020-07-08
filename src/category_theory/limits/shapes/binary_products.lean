c1eee45bf83ef8ba1d0e45df72815fde2186d112
Scott Morrison <scott.morrison@gmail.com>
2020-07-08 15:29:32 +1000

    fixes

--- src/category_theory/limits/shapes/binary_products.lean
+++ src/category_theory/limits/shapes/binary_products.lean
@@ -3,7 +3,6 @@ Copyright (c) 2019 Scott Morrison. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Scott Morrison, Bhavik Mehta
 -/
--- import category_theory.limits.shapes.finite_products
 import category_theory.limits.shapes.terminal
 
 /-!

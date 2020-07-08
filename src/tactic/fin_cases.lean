1b211c34fe27010a40b6d157888f01bbd2f65798
Gabriel Ebner <gebner@gebner.org>
2020-07-08 15:26:12 +0200

    Fix conv.interactive.norm_num

--- src/tactic/fin_cases.lean
+++ src/tactic/fin_cases.lean
@@ -47,8 +47,8 @@ meta def fin_cases_at_aux : Π (with_list : list expr) (e : expr), tactic unit
         -- Otherwise, call `norm_num`. We let `norm_num` unfold `max` and `min`
         -- because it's helpful for the `interval_cases` tactic.
         | _ := try $ tactic.interactive.conv (some sn) none $
-               to_rhs >> tactic.interactive.norm_num
-                 [simp_arg_type.expr ``(max), simp_arg_type.expr ``(min)] (loc.ns [some sn])
+               to_rhs >> conv.interactive.norm_num
+                 [simp_arg_type.expr ``(max), simp_arg_type.expr ``(min)]
         end,
         s ← get_local sn,
         try `[subst %%s],

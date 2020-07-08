0945c71334dee189ffbe3fa469fdc6815cdaf97a
Kevin Buzzard <k.buzzard@imperial.ac.uk>
2020-07-08 18:42:26 +0100

    add blank line

--- src/group_theory/coset.lean
+++ src/group_theory/coset.lean
@@ -4,6 +4,7 @@ Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Mitchell Rowett, Scott Morrison
 -/
 import group_theory.subgroup
+
 open set function
 
 variable {α : Type*}

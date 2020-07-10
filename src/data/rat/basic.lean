35d4b12de295fc392bc9a416fa12523c8a2f0236
Scott Morrison <scott.morrison@gmail.com>
2020-07-10 11:36:07 +1000

    linting?

--- src/data/rat/basic.lean
+++ src/data/rat/basic.lean
@@ -611,7 +611,10 @@ by rw [← int.cast_coe_nat, coe_int_num]
 @[simp, norm_cast] theorem coe_nat_denom (n : ℕ) : (n : ℚ).denom = 1 :=
 by rw [← int.cast_coe_nat, coe_int_denom]
 
-@[simp, norm_cast] lemma coe_int_inj (m n : ℤ) : (m : ℚ) = n ↔ m = n :=
+-- The linter complained that this was provable `by simp only [int.cast_inj]`,
+-- but that's not the case.
+@[nolint simp_nf, simp, norm_cast]
+lemma coe_int_inj (m n : ℤ) : (m : ℚ) = n ↔ m = n :=
 ⟨λ h, by simpa using congr_arg num h, congr_arg _⟩
 
 end casts

d597c23168980bb8f1adb4683577f28a70cc9c93
Gabriel Ebner <gebner@gebner.org>
2020-07-10 16:33:14 +0200

    Even more robust by_cases..

--- src/tactic/fix_by_cases.lean
+++ src/tactic/fix_by_cases.lean
@@ -22,9 +22,14 @@ Do not use this function directly, use `tactic.by_cases`.
 meta def by_cases' (e : expr) (h : name) : tactic unit := do
 dec_e ← mk_app ``decidable [e] <|> fail "by_cases tactic failed, type is not a proposition",
 inst ← mk_instance dec_e <|> pure `(classical.prop_decidable %%e),
-refine ``(@dite %%e %%inst _
-  %%(expr.lam h binder_info.default ``(%%e) ``(_))
-  %%(expr.lam h binder_info.default ``(¬ %%e) ``(_)))
+tgt ← target,
+expr.sort tgt_u ← infer_type tgt >>= whnf,
+g1 ← mk_meta_var (e.imp tgt),
+g2 ← mk_meta_var (`(¬ %%e).imp tgt),
+focus1 $ do
+  exact $ expr.const ``dite [tgt_u] e inst tgt g1 g2,
+  set_goals [g1, g2],
+  all_goals' $ intro h >> skip
 
 attribute [vm_override by_cases'] by_cases
 

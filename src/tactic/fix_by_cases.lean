5d5f41f794d72ec6b1c49a0bccae7de8dbf72bd5
Gabriel Ebner <gebner@gebner.org>
2020-07-10 14:46:56 +0200

    by_cases': produce dite-terms

--- src/tactic/fix_by_cases.lean
+++ src/tactic/fix_by_cases.lean
@@ -22,10 +22,9 @@ Do not use this function directly, use `tactic.by_cases`.
 meta def by_cases' (e : expr) (h : name) : tactic unit := do
 dec_e ← mk_app ``decidable [e] <|> fail "by_cases tactic failed, type is not a proposition",
 inst ← mk_instance dec_e <|> pure `(classical.prop_decidable %%e),
-let inst := `(id %%inst : decidable %%e),
-cases inst [h, h],
-tactic.swap,
-skip
+refine ``(@dite %%e %%inst _
+  %%(expr.lam h binder_info.default ``(%%e) ``(_))
+  %%(expr.lam h binder_info.default ``(¬ %%e) ``(_)))
 
 attribute [vm_override by_cases'] by_cases
 

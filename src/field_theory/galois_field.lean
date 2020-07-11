d5add5188b7f0ffcea39e7479b19710e9c46d087
Aaron Anderson <awainverse@gmail.com>
2020-07-10 22:47:20 -0700

    Some useful lemmas

--- src/field_theory/galois_field.lean
+++ src/field_theory/galois_field.lean
@@ -4,6 +4,7 @@ Authors: Johan Commelin
 -/
 
 import field_theory.finite
+import field_theory.separable
 import linear_algebra.finite_dimensional
 
 /-!
@@ -46,9 +47,36 @@ end
 
 open polynomial
 
+lemma galois_poly_separable {K : Type*} [field K] (p q : ℕ) [char_p K p]
+  [fact p.prime] (h : p ∣ q) :
+  separable (X ^ q - X : polynomial K) :=
+begin
+  unfold separable, unfold is_coprime, use 1, use (X ^ q - X - 1),
+  rw [← _inst_2.cast_eq_zero_iff, ← C_inj, C_eq_nat_cast] at h,
+  simp [derivative_pow, h],
+end
+
+section polynomial_facts
+
+open_locale big_operators
+open finset
+
+variables {R : Type*} [integral_domain R] [decidable_eq R]
+
+lemma roots_prod_linear (s : finset R) : (∏ i in s, (X - C i)).roots = s :=
+begin
+  apply s.induction_on, ext, simp,
+  intros a s' has' ih, rw [finset.prod_insert has', roots_mul, ih], ext, simp,
+  intro con, cases eq_zero_or_eq_zero_of_mul_eq_zero con, swap,
+  rw finset.prod_eq_zero_iff at h, cases h with x h, cases h with i j,
+  repeat { apply polynomial.ne_zero_of_monic (monic_X_sub_C _), assumption, apply_instance, },
+end
+
+end polynomial_facts
+
 section splitting_field_facts
 
-variables {K : Type*} [field K] {f : polynomial K}
+variables {K : Type*} {L : Type*} [field K] [field L] {f : polynomial K}
 /-- The canonical isomorphism between a field and the splitting field of a polynomial that splits-/
 def ring_equiv_splitting_field_of_splits (h : polynomial.splits (ring_hom.id K) f) :
   (K ≃+* f.splitting_field) :=
@@ -60,8 +88,41 @@ begin
   apply_instance,
 end
 
-instance is_splitting_field.finite_dimensional {L : Type*} [field L] [algebra K L]
-  [is_splitting_field K L f] : finite_dimensional K L :=
+-- Needs one more step, showing basically that separable implies squarefree
+lemma exists_finset_of_splits [decidable_eq L] (i : K →+* L) (sep : separable f) : splits i f →
+  ∃ (s : finset L), f.map i = C (i f.leading_coeff) *
+  (s.prod (λ a : L, (X : polynomial L) - C a)) :=
+begin
+  intro sp, cases exists_multiset_of_splits i sp with s h, existsi s.to_finset, rw h,
+  rw finset.prod_eq_multiset_prod, rw ← multiset.to_finset_eq,
+  rw multiset.nodup_iff_le, intro a, intro con, rw le_iff_exists_add at con,
+  cases con with c hc, rw multiset.cons_add at hc, rw multiset.cons_add at hc,
+  rw hc at h, simp at h, sorry,
+end
+
+open_locale big_operators
+variables {α : Type*} [decidable_eq α] (s : finset α) (g : α → polynomial K)
+-- I have a proof in another project, hope to PR soon - Aaron Anderson
+lemma nat_degree_prod_eq' (h : ∏ i in s, (g i).leading_coeff ≠ 0) :
+  (s.prod g).nat_degree = ∑ i in s, (g i).nat_degree :=
+begin
+  sorry
+end
+
+-- monic should be removed eventually
+lemma card_roots_eq_of_splits_monic (m : monic f) [decidable_eq K]
+  (sep: separable f) (sp : polynomial.splits (ring_hom.id K) f) :
+  f.roots.card = f.nat_degree :=
+begin
+  cases (exists_finset_of_splits (ring_hom.id K) sep sp) with s hs,
+  simp only [ring_hom.id_apply, map_id] at hs, rw hs,
+  rw [m.leading_coeff, C_1, one_mul] at *,
+  rw roots_prod_linear s, rw nat_degree_prod_eq', simp,
+  simp_rw (monic_X_sub_C _).leading_coeff, simp,
+end
+
+instance is_splitting_field.finite_dimensional  [algebra K L] [is_splitting_field K L f] :
+  finite_dimensional K L :=
 sorry
 
 end splitting_field_facts

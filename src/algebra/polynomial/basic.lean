8156a81091d550ef122e5b4149d4e301943bba38
jalex-stark <alexmaplegm@gmail.com>
2020-07-12 19:26:28 -0400

    break into two files

--- src/algebra/polynomial/basic.lean
+++ src/algebra/polynomial/basic.lean
@@ -4,6 +4,7 @@ Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Aaron Anderson, Jalex Stark.
 -/
 import data.polynomial
+import algebra.polynomial.big_operators
 open polynomial finset
 
 /-
@@ -11,7 +12,7 @@ open polynomial finset
 Here we have utility lemmas for polynomials.
 For results that are useful for programming, see data/polynomial.lean
 ## Main results
-- `nat_degree_prod_eq_of_monic`: needed for reasoning about the characteristic polynomial
+- `card_pred_coeff_prod_X_sub_C` : yields that the trace is a coefficient of the characteristic polynomial
 -/
 
 noncomputable theory
@@ -22,106 +23,6 @@ universes u w
 variables {R : Type u} {α : Type w}
 
 namespace polynomial
-
-section comm_semiring_homs
-
-variable [comm_semiring R]
-
-/-- `polynomial.eval` bundled as a ring_hom -/
-def eval_ring_hom : R → (polynomial R →+* R) := eval₂_ring_hom (ring_hom.id R)
-
-@[simp]
-lemma coe_eval_ring_hom (r : R) (p : polynomial R) : eval_ring_hom r p = eval r p := rfl
-
-/-- A ring hom returning the constant term -/
-def coeff_zero_ring_hom : polynomial R →+* R := eval_ring_hom 0
-
-@[simp]
-lemma coe_coeff_zero_ring_hom (p : polynomial R) : coeff_zero_ring_hom p = p.coeff 0 :=
-by { rw coeff_zero_eq_eval_zero p, refl }
-
-end comm_semiring_homs
-
-section integral_domain_homs
-
-variable [integral_domain R]
-
-/-- `leading_coeff` bundled as a monoid hom-/
-def leading_coeff_monoid_hom : polynomial R →* R :=
-{to_fun := leading_coeff, map_one' := by simp, map_mul' := leading_coeff_mul}
-
-@[simp] lemma coe_leading_coeff_monoid_hom (p : polynomial R) :
-  leading_coeff_monoid_hom p = leading_coeff p := rfl
-
-end integral_domain_homs
-
-section poly_big_ops
-
-variable (s : finset α)
-
-section comm_semiring
-variables [comm_semiring R] (f : α → polynomial R)
-
-lemma nat_degree_prod_le : (s.prod f).nat_degree ≤ ∑ i in s, (f i).nat_degree :=
-begin
-  classical,
-  apply s.induction_on, simp,intros a s anins ih,
-  rw [prod_insert anins, sum_insert anins],
-  transitivity (f a).nat_degree + (∏ (x : α) in s, (f x)).nat_degree,
-  apply polynomial.nat_degree_mul_le, apply add_le_add_left ih,
-end
-
-lemma leading_coeff_prod' (h : ∏ i in s, (f i).leading_coeff ≠ 0) :
-  (∏ i in s, f i).leading_coeff = ∏ i in s, (f i).leading_coeff :=
-begin
-  classical,
-  revert h, induction s using finset.induction with a s ha hs, { simp },
-  repeat { rw prod_insert ha },
-  intro h, rw polynomial.leading_coeff_mul'; { rwa hs, apply right_ne_zero_of_mul h },
-end
-
-lemma nat_degree_prod_eq' (h : ∏ i in s, (f i).leading_coeff ≠ 0) :
-  (s.prod f).nat_degree = ∑ i in s, (f i).nat_degree :=
-begin
-  classical,
-  revert h, induction s using finset.induction with a s ha hs, { simp },
-  rw [prod_insert ha, prod_insert ha, sum_insert ha],
-  intro h, rw polynomial.nat_degree_mul_eq', rw hs,
-  apply right_ne_zero_of_mul h,
-  rwa polynomial.leading_coeff_prod', apply right_ne_zero_of_mul h,
-end
-
-lemma monic_prod_monic :
-  (∀ a : α, a ∈ s → monic (f a)) → monic (s.prod f) :=
-by { apply prod_induction, apply monic_mul, apply monic_one }
-
-lemma nat_degree_prod_eq_of_monic [nontrivial R] (h : ∀ i : α, i ∈ s → (f i).monic) :
-  (s.prod f).nat_degree = ∑ i in s, (f i).nat_degree :=
-begin
-  apply nat_degree_prod_eq',
-  suffices : ∏ (i : α) in s, (f i).leading_coeff = 1, { rw this, simp },
-  rw prod_eq_one, intros, apply h, assumption,
-end
-
-end comm_semiring
-
-section integral_domain
-
-variables [integral_domain R] (f : α → polynomial R)
-
-lemma nat_degree_prod_eq (h : ∀ i ∈ s, f i ≠ 0) :
-  (s.prod f).nat_degree = ∑ i in s, (f i).nat_degree :=
-begin
-  apply nat_degree_prod_eq', rw prod_ne_zero_iff,
-  intros x hx, simp [h x hx],
-end
-
-lemma leading_coeff_prod :
-  (∏ i in s, f i).leading_coeff = ∏ i in s, (f i).leading_coeff :=
-by { rw ← coe_leading_coeff_monoid_hom, apply monoid_hom.map_prod }
-
-end integral_domain
-end poly_big_ops
 
 variables [comm_ring R]
 

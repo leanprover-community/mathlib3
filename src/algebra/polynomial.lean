439068cf16e6781a8b17b29ca2ea1d798d8899be
Aaron Anderson <awainverse@gmail.com>
2020-07-12 15:21:53 -0700

    Linting [decidable_eq] to classical in proof

--- src/algebra/polynomial.lean
+++ src/algebra/polynomial.lean
@@ -22,7 +22,7 @@ open_locale big_operators
 
 universes u w
 
-variables {R : Type u} {α : Type w} [decidable_eq α]
+variables {R : Type u} {α : Type w}
 
 namespace polynomial
 
@@ -67,6 +67,7 @@ variables [comm_semiring R] (f : α → polynomial R)
 
 lemma nat_degree_prod_le : (s.prod f).nat_degree ≤ ∑ i in s, (f i).nat_degree :=
 begin
+  classical,
   apply s.induction_on, simp,intros a s anins ih,
   rw [prod_insert anins, sum_insert anins],
   transitivity (f a).nat_degree + (∏ (x : α) in s, (f x)).nat_degree,
@@ -76,6 +77,7 @@ end
 lemma leading_coeff_prod' (h : ∏ i in s, (f i).leading_coeff ≠ 0) :
   (∏ i in s, f i).leading_coeff = ∏ i in s, (f i).leading_coeff :=
 begin
+  classical,
   revert h, induction s using finset.induction with a s ha hs, { simp },
   repeat { rw prod_insert ha },
   intro h, rw polynomial.leading_coeff_mul'; { rwa hs, apply right_ne_zero_of_mul h },
@@ -84,6 +86,7 @@ end
 lemma nat_degree_prod_eq' (h : ∏ i in s, (f i).leading_coeff ≠ 0) :
   (s.prod f).nat_degree = ∑ i in s, (f i).nat_degree :=
 begin
+  classical,
   revert h, induction s using finset.induction with a s ha hs, { simp },
   rw [prod_insert ha, prod_insert ha, sum_insert ha],
   intro h, rw polynomial.nat_degree_mul_eq', rw hs,

b476232b60453b667b1a7efd83af47e49e297111
Scott Morrison <scott.morrison@gmail.com>
2020-07-13 10:25:24 +1000

    use linarith

--- demo/demo.lean
+++ demo/demo.lean
@@ -1,4 +1,5 @@
 import data.nat.prime
+import tactic.linarith
 
 open nat
 
@@ -8,13 +9,13 @@ begin
   let M := fact N + 1,
   let p := min_fac M,
 
-  have pp : prime p := begin show_term { /- suggest -/ refine min_fac_prime _, apply ne_of_gt, apply succ_lt_succ, library_search, }, end,
+  have pp : prime p := begin show_term { /- suggest -/ refine min_fac_prime _, have : fact N > 0 := by library_search, linarith, }, end,
 
   use p,
   split,
   { by_contradiction,
-    have h₁ : p ∣ M := by library_search,
     have h₂ : p ∣ fact N := begin /- suggest -/ refine pp.dvd_fact.mpr _, library_search, end,
+    have h₁ : p ∣ M := by library_search,
     have h : p ∣ 1 := by library_search,
     by library_search, },
   { assumption, },

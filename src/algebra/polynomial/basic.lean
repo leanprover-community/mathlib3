48372a5877348440048b64ad45cbd5d9f4536c79
jalex-stark <alexmaplegm@gmail.com>
2020-07-12 20:04:26 -0400

    break basic.lean up by level of typeclass assumption

--- src/algebra/polynomial/basic.lean
+++ src/algebra/polynomial/basic.lean
@@ -10,11 +10,8 @@ open polynomial finset
 # Polynomials
 Eventually, much of data/polynomial.lean should land here.
 ## Main results
-- `card_pred_coeff_prod_X_sub_C` : yields that the trace is a coefficient of the characteristic polynomial
+- `next_coeff` :
 -/
-
-noncomputable theory
-open_locale big_operators
 
 universes u w
 
@@ -22,10 +19,11 @@ variables {R : Type u} {α : Type w}
 
 namespace polynomial
 
-variables [comm_ring R]
+section semiring
+variables [semiring R]
 
 /-- The second-highest coefficient, or 0 for constants -/
-def next_coeff (p : polynomial R) : R := ite (p.nat_degree = 0) 0 p.coeff (p.nat_degree - 1)
+def next_coeff [semiring R] (p : polynomial R) : R := ite (p.nat_degree = 0) 0 p.coeff (p.nat_degree - 1)
 
 @[simp]
 lemma next_coeff_C_eq_zero (c : R) :
@@ -34,15 +32,25 @@ next_coeff (C c) = 0 := by {rw next_coeff, simp}
 lemma next_coeff_of_pos_nat_degree (p : polynomial R) :
   0 < p.nat_degree → next_coeff p = p.coeff (p.nat_degree - 1) :=
 by { intro h, rw next_coeff, rw if_neg, intro contra, rw contra at h, apply lt_irrefl 0 h, }
+end semiring
+
+section ring
+variables [ring R]
 
 @[simp]
-lemma next_coeff_X_sub_C_eq [nontrivial R] (c : R) : next_coeff (X - C c) = - c :=
+lemma next_coeff_X_sub_C_eq [nontrivial R]  (c : R) : next_coeff (X - C c) = - c :=
 by { rw next_coeff_of_pos_nat_degree; simp [nat_degree_X_sub_C] }
+
+end ring
+
+section comm_semiring
+variables [comm_semiring R]
 
 lemma pow_comp (p q : polynomial R) (k : ℕ) : (p ^ k).comp q = (p.comp q) ^ k :=
 begin
   unfold comp, rw ← coe_eval₂_ring_hom, apply ring_hom.map_pow,
 end
 
+end comm_semiring
 
 end polynomial

c059aafe124327fd809c08d0021e6652ff8c542f
Scott Morrison <scott.morrison@gmail.com>
2020-07-14 17:54:39 +1000

    add notes about things to do

--- src/category_theory/monoidal/Mon_in.lean
+++ src/category_theory/monoidal/Mon_in.lean
@@ -56,9 +56,23 @@ instance : category.{v₁} (Mon_in C) :=
 --   { obj := λ M, ⟨M.X, { one := as_element M.ι, mul := M.μ, }⟩,
 --   }}
 
--- TODO `Mon_in (AddCommMon) ≌ SemiRing`
--- TODO `Mon_in (AddCommGroup) ≌ Ring`
--- TODO `Mon_in (Module R) ≌ Algebra R`
+/-
+TODO
+
+* Construct the category of module objects for a fixed monoid object.
+* Check that `Mon_in Type ≌ Mon`.
+* Check that `Mon_in Mon ≌ CommMon`, via the Eckmann-Hilton argument.
+  (You'll have to hook up the cartesian monoidal structure on `Mon` first.)
+* Check that `Mon_in AddCommGroup ≌ Ring`.
+  (You'll have to hook up the monoidal structure on `AddCommGroup`.
+  Currently we have the monoidal structure on `Module R`; perhaps one could specialize to `R = ℤ`
+  and transport the monoidal structure across an equivalence? This sounds like some work!)
+* Check that `Mon_in (Module R) ≌ Algebra R`.
+* Show that if `C` is braided (you'll have to define that first!)
+   then `Mon_in C` is naturally monoidal.
+* Can you transport this monoidal structure to `Ring` or `Algebra R`?
+  How does it compare to the "native" one?
+-/
 
 end Mon_in
 

07a77b7efe4a326e64008263d612029ad78a3add
Simon Hudon <simon.hudon@gmail.com>
2020-07-14 11:14:49 -0400

    add import in default.lean

--- src/data/qpf/indexed/constructions/default.lean
+++ src/data/qpf/indexed/constructions/default.lean
@@ -1,4 +1,6 @@
 
 import data.qpf.indexed.constructions.basic
 import data.qpf.indexed.constructions.fix
+import data.qpf.indexed.constructions.cofix
 import data.qpf.indexed.constructions.comp
+import data.qpf.indexed.constructions.prod

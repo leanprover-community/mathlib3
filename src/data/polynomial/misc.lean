ed31a02777d59b240ed575514b2369cb2a700f9d
jalex-stark <alexmaplegm@gmail.com>
2020-07-15 10:53:10 -0400

    fix ambiguous import

--- src/data/polynomial/misc.lean
+++ src/data/polynomial/misc.lean
@@ -3,7 +3,9 @@ Copyright (c) 2018 Chris Hughes. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Chris Hughes, Johannes Hölzl, Scott Morrison, Jens Wagemaker
 -/
-import data.polynomial.default
+import data.polynomial.algebra_map
+import data.polynomial.division
+import data.polynomial.derivative
 
 /-!
 # Theory of univariate polynomials

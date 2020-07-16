1cb93272b9050c68f08423927264ec3af82b4e52
Scott Morrison <scott.morrison@gmail.com>
2020-07-16 17:13:17 +1000

    linting

--- src/meta/expr.lean
+++ src/meta/expr.lean
@@ -370,6 +370,9 @@ e.fold mk_name_set $ λ e' _ l,
 meta def contains_constant (e : expr) (p : name → Prop) [decidable_pred p] : bool :=
 e.fold ff (λ e' _ b, if p (e'.const_name) then tt else b)
 
+/--
+Returns true if `e` contains a `sorry`.
+-/
 meta def contains_sorry (e : expr) : bool :=
 e.fold ff (λ e' _ b, if (is_sorry e').is_some then tt else b)
 

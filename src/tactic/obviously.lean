5de033bf703aaa2ecdcbb3a19ecf182ddd7c8a3f
Scott Morrison <scott.morrison@gmail.com>
2020-07-16 14:40:00 +1000

    linting

--- src/tactic/obviously.lean
+++ src/tactic/obviously.lean
@@ -3,6 +3,10 @@ import tactic.basic
 
 namespace tactic
 
+/--
+`sorry_if_contains_sorry` will solve any goal already containing `sorry` in its type with `sorry`,
+and fail otherwise.
+-/
 meta def sorry_if_contains_sorry : tactic unit :=
 do
   g ← target,
@@ -18,8 +22,17 @@ which is defined here as a
 [`replacer` tactic](https://leanprover-community.github.io/mathlib_docs/commands.html#def_replacer).
 We then immediately set up `obviously` to call `tidy`. Later, this can be replaced with more
 powerful tactics.
+
+(In fact, at present this mechanism is not actually used, and the implementation of
+`obviously` below stays in place throughout mathlib.)
 -/
 def_replacer obviously
+
+/--
+The default implementation of `obviously`
+discharges any goals which contain `sorry` in their type using `sorry`,
+and then calls `tidy`.
+-/
 @[obviously] meta def obviously' :=
 tactic.sorry_if_contains_sorry <|>
 tactic.tidy <|>

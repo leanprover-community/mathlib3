58f39ef4010d804772e14949c2db567d74d51965
jalex-stark <alexmaplegm@gmail.com>
2020-07-16 09:23:51 -0400

    fix broken import

--- src/data/polynomial/eval.lean
+++ src/data/polynomial/eval.lean
@@ -3,7 +3,7 @@ Copyright (c) 2018 Chris Hughes. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Chris Hughes, Johannes Hölzl, Scott Morrison, Jens Wagemaker
 -/
-import data.polynomial.degree_basic
+import data.polynomial.coeff
 import data.polynomial.induction
 
 /-!
--- src/data/polynomial/ring_division.lean
+++ src/data/polynomial/ring_division.lean
@@ -4,7 +4,7 @@ Copyright (c) 2018 Chris Hughes. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Chris Hughes, Johannes Hölzl, Scott Morrison, Jens Wagemaker
 -/
-import data.polynomial.semiring_division
+import data.polynomial.div
 
 
 /-!

a9bdde6e7c5cf51587567ee832558db14f225474
Nicolò Cavalleri <nico@cavalleri.net>
2020-07-16 16:10:29 +0200

    Added some documentation

--- src/topology/continuous_map.lean
+++ src/topology/continuous_map.lean
@@ -1,4 +1,17 @@
+/-
+Copyright © 2020 Nicolò Cavalleri. All rights reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
+Author: Nicolò Cavalleri.
+-/
+
 import topology.subset_properties
+
+/-!
+# Continuous bundled map
+
+In this file we define the type `continuous_map` of continuous bundled maps.
+
+-/
 
 @[protect_proj]
 structure continuous_map (α : Type*) (β : Type*)
@@ -15,6 +28,7 @@ variables {α : Type*} {β : Type*} [topological_space α] [topological_space β
 instance : has_coe_to_fun (C(α, β)) := ⟨_, continuous_map.to_fun⟩
 
 variables {α β} {f g : continuous_map α β}
+
 @[ext] theorem ext (H : ∀ x, f x = g x) : f = g :=
 by cases f; cases g; congr'; exact funext H
 

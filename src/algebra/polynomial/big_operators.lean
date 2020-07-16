a77bd29eba7f652b8763add19c0623826bdbe2ad
jalex-stark <alexmaplegm@gmail.com>
2020-07-16 19:59:01 -0400

    attempt to fix import

--- src/algebra/polynomial/big_operators.lean
+++ src/algebra/polynomial/big_operators.lean
@@ -4,7 +4,7 @@ Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Aaron Anderson, Jalex Stark.
 -/
 import data.polynomial.algebra_map
-import data.polynomial
+-- import data.polynomial
 open polynomial finset
 
 /-!

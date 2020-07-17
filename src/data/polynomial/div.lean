5fcf23793ae51f93c352ae4f617bc56447d53b28
Scott Morrison <scott.morrison@gmail.com>
2020-07-18 01:47:44 +1000

    remove

--- src/data/polynomial/div.lean
+++ src/data/polynomial/div.lean
@@ -28,20 +28,6 @@ variables {R : Type u} {S : Type v} {T : Type w} {A : Type z} {a b : R} {n : ℕ
 section semiring
 variables [semiring R] {p q : polynomial R}
 
-section
-/--
-The coercion turning a `polynomial` into the function which reports the coefficient of a given
-monomial `X^n`
--/
--- TODO we would like to completely remove this, but this requires fixing some proofs
-def coeff_coe_to_fun : has_coe_to_fun (polynomial R) :=
-finsupp.has_coe_to_fun
-
-local attribute [instance] coeff_coe_to_fun
-
-lemma apply_eq_coeff : p n = coeff p n := rfl
-end
-
 /-- `div_X p` return a polynomial `q` such that `q * X + C (p.coeff 0) = p`.
   It can be used in a semiring where the usual division algorithm is not possible -/
 def div_X (p : polynomial R) : polynomial R :=
@@ -57,7 +43,7 @@ def div_X (p : polynomial R) : polynomial R :=
   mem_support_to_fun := λ n,
     suffices (∃ (a : ℕ), (¬coeff p a = 0 ∧ a > 0) ∧ a - 1 = n) ↔
       ¬coeff p (n + 1) = 0,
-    by simpa [finset.mem_def.symm, apply_eq_coeff],
+    by simpa [finset.mem_def.symm],
     ⟨λ ⟨a, ha⟩, by rw [← ha.2, nat.sub_add_cancel ha.1.2]; exact ha.1.1,
       λ h, ⟨n + 1, ⟨h, nat.succ_pos _⟩, nat.succ_sub_one _⟩⟩ }
 

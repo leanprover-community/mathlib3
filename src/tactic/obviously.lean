f344e145b064dffafba7047cd67f0ac21e388c36
Scott Morrison <scott.morrison@gmail.com>
2020-07-18 14:39:44 +1000

    chore(*): add copyright header, cleanup imports

--- src/tactic/obviously.lean
+++ src/tactic/obviously.lean
@@ -1,5 +1,10 @@
+/-
+Copyright (c) 2017 Scott Morrison. All rights reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
+Authors: Scott Morrison
+-/
 import tactic.tidy
-import tactic.basic
+import tactic.replacer
 
 namespace tactic
 

06823d6dd94d25bce25ae8793ae9012c508df898
Scott Morrison <scott@tqft.net>
2020-07-18 06:56:08 +0000

    chore(*): add copyright header, cleanup imports (#3440)
    Fixes
    
    1. a missing copyright header
    2. moves `tactic.obviously` into the imports of `tactic.basic`, so everyone has `tidy` and `obviously` available.
    3. removes a few redundant imports
    
    
    
    Co-authored-by: Scott Morrison <scott.morrison@gmail.com>

--- src/algebra/group/basic.lean
+++ src/algebra/group/basic.lean
@@ -3,8 +3,6 @@ Copyright (c) 2014 Jeremy Avigad. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Jeremy Avigad, Leonardo de Moura, Simon Hudon, Mario Carneiro
 -/
-import tactic.basic
-import algebra.group.to_additive
 import algebra.group.defs
 import logic.function.basic
 

36ea9e8443591810a5c7bac5b4e7e375b1ccc496
Scott Morrison <scott@tqft.net>
2020-07-22 16:16:15 +0000

    chore(*): cleanup imports (#3511)
    A not-very-interesting cleanup of imports.
    
    I deleted 
    ```
    instance orbit_fintype (b : β) [fintype α] [decidable_eq β] :	
      fintype (orbit α b) := set.fintype_range _
    ```
    which wasn't being used, for the sake of not having to import everything about finiteness into `algebra.group_action`.
    
    
    
    Co-authored-by: Scott Morrison <scott.morrison@gmail.com>

--- src/data/polynomial/algebra_map.lean
+++ src/data/polynomial/algebra_map.lean
@@ -3,7 +3,6 @@ Copyright (c) 2018 Chris Hughes. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Chris Hughes, Johannes Hölzl, Scott Morrison, Jens Wagemaker
 -/
-import data.polynomial.induction
 import data.polynomial.degree
 
 /-!
--- src/ring_theory/ideals.lean
+++ src/ring_theory/ideals.lean
@@ -4,7 +4,6 @@ Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Kenny Lau, Chris Hughes, Mario Carneiro
 -/
 import algebra.associated
-import algebra.pointwise
 import linear_algebra.basic
 import order.zorn
 

82f50f826741b54083a6c2f9195dc0fdacdefcd4
Adam Topaz <github@adamtopaz.com>
2020-07-23 14:27:59 -0600

    Add header

--- src/linear_algebra/tensor_algebra.lean
+++ src/linear_algebra/tensor_algebra.lean
@@ -1,3 +1,9 @@
+/-
+Copyright (c) 2020 Adam Topaz. All rights reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
+Author: Adam Topaz.
+-/
+
 import ring_theory.algebra
 import linear_algebra
 

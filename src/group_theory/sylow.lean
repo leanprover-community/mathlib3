eb811d62f7cf77e8a72a18a97e7f19746d062872
Kevin Buzzard <k.buzzard@imperial.ac.uk>
2020-07-25 20:52:35 +0100

    a def and a lemma to go

--- src/group_theory/sylow.lean
+++ src/group_theory/sylow.lean
@@ -11,7 +11,7 @@ import data.fintype.card
 import data.list.rotate
 
 open equiv fintype finset mul_action function
-open equiv.perm is_subgroup list quotient_group
+open equiv.perm subgroup list quotient_group
 open_locale big_operators
 universes u v w
 variables {G : Type u} {α : Type v} {β : Type w} [group G]
@@ -170,20 +170,21 @@ let ⟨a, ha⟩ := this in
   (hp.2 _ (order_of_dvd_of_pow_eq_one this)).resolve_left
     (λ h, ha1 (order_of_eq_one_iff.1 h))⟩
 
-open is_subgroup is_submonoid is_group_hom mul_action
+open subgroup submonoid is_group_hom mul_action
 
-lemma mem_fixed_points_mul_left_cosets_iff_mem_normalizer {H : subgroup G} [fintype (H : set G)]
-  {x : G} : (x : quotient H) ∈ fixed_points H (quotient H) ↔ x ∈ normalizer (H : set G) :=
+lemma mem_fixed_points_mul_left_cosets_iff_mem_normalizer {H : subgroup G} [fintype ((H : set G) : Type u)]
+  {x : G} : (x : quotient H) ∈ fixed_points H (quotient H) ↔ x ∈ normalizer H :=
 ⟨λ hx, have ha : ∀ {y : quotient H}, y ∈ orbit H (x : quotient H) → y = x,
   from λ _, ((mem_fixed_points' _).1 hx _),
-  (inv_mem_iff _).1 (mem_normalizer_fintype (λ n hn,
-    have (n⁻¹ * x)⁻¹ * x ∈ H := quotient_group.eq.1 (ha (mem_orbit _ ⟨n⁻¹, inv_mem hn⟩)),
-    by simpa only [mul_inv_rev, inv_inv] using this)),
+  (inv_mem_iff _).1 (@mem_normalizer_fintype _ _ _ _inst_2 _ (λ n (hn : n ∈ H),
+    have (n⁻¹ * x)⁻¹ * x ∈ H := quotient_group.eq.1 (ha (mem_orbit _ ⟨n⁻¹, H.inv_mem hn⟩)),
+    show _ ∈ H, by {rw [mul_inv_rev, inv_inv] at this, convert this, rw inv_inv}
+    )),
 λ (hx : ∀ (n : G), n ∈ H ↔ x * n * x⁻¹ ∈ H),
 (mem_fixed_points' _).2 $ λ y, quotient.induction_on' y $ λ y hy, quotient_group.eq.2
   (let ⟨⟨b, hb₁⟩, hb₂⟩ := hy in
   have hb₂ : (b * x)⁻¹ * y ∈ H := quotient_group.eq.1 hb₂,
-  (inv_mem_iff H).1 $ (hx _).2 $ (mul_mem_cancel_left H (inv_mem hb₁)).1
+  (inv_mem_iff H).1 $ (hx _).2 $ (mul_mem_cancel_left H (H.inv_mem hb₁)).1
   $ by rw hx at hb₂;
     simpa [mul_inv_rev, mul_assoc] using hb₂)⟩
 

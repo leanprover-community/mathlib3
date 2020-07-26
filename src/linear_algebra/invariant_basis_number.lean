83addb7dac330c704bd4c4230e14fd53b5b82a4a
Markus Himmel <markus@himmel-villmar.de>
2020-07-26 11:28:29 +0200

    Cleanup

--- src/linear_algebra/invariant_basis_number.lean
+++ src/linear_algebra/invariant_basis_number.lean
@@ -103,31 +103,31 @@ end
 section
 variables {R : Type u} [comm_ring R] (I : ideal R) (ι : Type v)
 
-/-- I^n as an ideal of R^n. -/
-def all_in : ideal (ι → R) :=
+/-- `I^n` as an ideal of `R^n`. -/
+def ideal.pi : ideal (ι → R) :=
 { carrier := { x | ∀ i, x i ∈ I },
   zero_mem' := λ i, submodule.zero_mem _,
   add_mem' := λ a b ha hb i, submodule.add_mem _ (ha i) (hb i),
   smul_mem' := λ a b hb i, ideal.mul_mem_left _ (hb i) }
 
-lemma mem_all_in (x : ι → R) : x ∈ all_in I ι ↔ ∀ i, x i ∈ I := iff.rfl
+lemma mem_all_in (x : ι → R) : x ∈ I.pi ι ↔ ∀ i, x i ∈ I := iff.rfl
 
-/-- R^n/I^n is a R/I-module. -/
-instance module_all_in : module (I.quotient) (all_in I ι).quotient :=
+/-- `R^n/I^n` is a `R/I`-module. -/
+instance ideal.module_pi : module (I.quotient) (I.pi ι).quotient :=
 begin
   refine { smul := λ c m, quotient.lift_on₂' c m (λ r m, submodule.quotient.mk $ r • m) _, .. },
   { intros c₁ m₁ c₂ m₂ hc hm,
     change c₁ - c₂ ∈ I at hc,
-    change m₁ - m₂ ∈ all_in I ι at hm,
+    change m₁ - m₂ ∈ (I.pi ι) at hm,
     apply ideal.quotient.eq.2,
-    have : c₁ • (m₂ - m₁) ∈ all_in I ι,
+    have : c₁ • (m₂ - m₁) ∈ I.pi ι,
     { rw mem_all_in,
       intro i,
       simp only [algebra.id.smul_eq_mul, pi.smul_apply, pi.sub_apply],
       apply ideal.mul_mem_left,
       rw ←ideal.neg_mem_iff,
       simpa only [neg_sub] using hm i },
-    rw [←ideal.add_mem_iff_left (all_in I ι) this, sub_eq_add_neg, add_comm, ←add_assoc, ←smul_add,
+    rw [←ideal.add_mem_iff_left (I.pi ι) this, sub_eq_add_neg, add_comm, ←add_assoc, ←smul_add,
       sub_add_cancel, ←sub_eq_add_neg, ←sub_smul, mem_all_in],
     intro i,
     simp only [algebra.id.smul_eq_mul, pi.smul_apply],
@@ -138,13 +138,13 @@ begin
     congr, ext, simp [mul_assoc, mul_add, add_mul] }
 end
 
-/-- R^n/I^n is isomorphic to (R/I)^n as an R/I-module. -/
-def all_in_quot_equiv : (all_in I ι).quotient ≃ₗ[I.quotient] (ι → I.quotient) :=
+/-- `R^n/I^n` is isomorphic to `(R/I)^n` as an `R/I`-module. -/
+def ideal.pi_quot_equiv : (I.pi ι).quotient ≃ₗ[I.quotient] (ι → I.quotient) :=
 { to_fun := λ x, quotient.lift_on' x (λ f i, ideal.quotient.mk I (f i)) $
     λ a b hab, funext (λ i, ideal.quotient.eq.2 (hab i)),
   map_add' := by { rintros ⟨_⟩ ⟨_⟩, refl },
   map_smul' := by { rintros ⟨_⟩ ⟨_⟩, refl },
-  inv_fun := λ x, ideal.quotient.mk (all_in I ι) $ λ i, quotient.out' (x i),
+  inv_fun := λ x, ideal.quotient.mk (I.pi ι) $ λ i, quotient.out' (x i),
   left_inv :=
   begin
     rintro ⟨x⟩,
@@ -170,8 +170,7 @@ lemma mem_fin (I : ideal R) (x : ι → R) (hi : ∀ i, x i ∈ I)
   (f : (ι → R) →ₗ[R] (ι' → R)) (i : ι') : f x i ∈ I :=
 begin
   rw pi_eq_sum_univ x,
-  simp only [finset.sum_apply, algebra.id.smul_eq_mul, linear_map.map_sum,
-    pi.smul_apply, linear_map.map_smul],
+  simp only [finset.sum_apply, smul_eq_mul, linear_map.map_sum, pi.smul_apply, linear_map.map_smul],
   exact submodule.sum_mem _ (λ j hj, ideal.mul_mem_right _ (hi j))
 end
 
@@ -183,8 +182,8 @@ local attribute [instance] invariant_basis_number_field
 local attribute [instance, priority 1] ideal.quotient.field
 
 /-- An isomorphism of `R`-modules `R^n ≃ R^m` induces a function `R^n/I^n → R^m/I^n`. -/
-def induced_map {R : Type u} [comm_ring R] (I : ideal R) (n m : ℕ)
-  (e : (fin n → R) ≃ₗ[R] (fin m → R)) : (all_in I (fin n)).quotient → (all_in I (fin m)).quotient :=
+private def induced_map {R : Type u} [comm_ring R] (I : ideal R) (n m : ℕ)
+  (e : (fin n → R) ≃ₗ[R] (fin m → R)) : (I.pi (fin n)).quotient → (I.pi (fin m)).quotient :=
 λ x, quotient.lift_on' x (λ y, ideal.quotient.mk _ (e y))
 begin
   intros a b hab,
@@ -196,12 +195,13 @@ end
 
 /-- An isomorphism of `R`-modules `R^n ≃ R^m` induces an isomorphism `R/I`-modules
     `R^n/I^n ≃ R^m/I^m`. -/
-def induced {R : Type u} [comm_ring R] (I : ideal R) {n m : ℕ} (e : (fin n → R) ≃ₗ[R] (fin m → R)) :
-  (all_in I (fin n)).quotient ≃ₗ[I.quotient] (all_in I (fin m)).quotient :=
+private def induced_equiv {R : Type u} [comm_ring R] (I : ideal R) {n m : ℕ}
+  (e : (fin n → R) ≃ₗ[R] (fin m → R)) :
+  (I.pi (fin n)).quotient ≃ₗ[I.quotient] (I.pi (fin m)).quotient :=
 begin
   refine { to_fun := induced_map I n m e, inv_fun := induced_map I m n e.symm, .. },
   { rintro ⟨x⟩ ⟨y⟩,
-    change ideal.quotient.mk (all_in I (fin m)) (e (x + y)) =
+    change ideal.quotient.mk (I.pi (fin m)) (e (x + y)) =
       ideal.quotient.mk _ (e x) + ideal.quotient.mk _ (e y),
     simp only [ring_hom.map_add, linear_equiv.map_add] },
   all_goals { rintro ⟨a⟩ ⟨b⟩ <|> rintro ⟨a⟩,
@@ -218,7 +218,7 @@ instance invariant_basis_number_of_nontrivial_of_comm_ring {R : Type u} [comm_ri
   obtain ⟨I, ⟨hI, hI'⟩⟩ := ideal.exists_le_maximal (⊥ : ideal R) submodule.bot_ne_top,
   resetI,
   exact eq_of_fin_equiv I.quotient
-    ((all_in_quot_equiv _ _).symm.trans ((induced _ e).trans (all_in_quot_equiv _ _)))
+    ((ideal.pi_quot_equiv _ _).symm.trans ((induced_equiv _ e).trans (ideal.pi_quot_equiv _ _)))
 end⟩
 
 end

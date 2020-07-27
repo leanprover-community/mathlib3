49da3f69ddf18f916a3b6a6cc750d8760102d59e
Jannis Limperg <jannis@limperg.de>
2020-07-28 00:38:00 +0200

    Address review comments

--- src/data/ulift.lean
+++ src/data/ulift.lean
@@ -10,28 +10,28 @@ universes u v
 
 namespace plift
 
-variables {α β : Sort u}
+variables {α : Sort u} {β : Sort v}
 
 /-- Functorial action. -/
-@[simp] def map (f : α → β) : plift α → plift β
+@[simp] protected def map (f : α → β) : plift α → plift β
 | (up a) := up (f a)
 
 /-- Embedding of pure values. -/
-@[simp] def pure : α → plift α := up
+@[simp] protected def pure : α → plift α := up
 
 /-- Applicative sequencing. -/
-@[simp] def seq : plift (α → β) → plift α → plift β
+@[simp] protected def seq : plift (α → β) → plift α → plift β
 | (up f) (up a) := up (f a)
 
 /-- Monadic bind. -/
-@[simp] def bind : plift α → (α → plift β) → plift β
+@[simp] protected def bind : plift α → (α → plift β) → plift β
 | (up a) f := f a
 
 instance : monad plift :=
-{ map := @map,
-  pure := @pure,
-  seq := @seq,
-  bind := @bind }
+{ map := @plift.map,
+  pure := @plift.pure,
+  seq := @plift.seq,
+  bind := @plift.bind }
 
 instance : is_lawful_functor plift :=
 { id_map := λ α ⟨x⟩, rfl,
@@ -58,28 +58,29 @@ end plift
 
 namespace ulift
 
-variables {α β : Type u}
+variables {α : Type u} {β : Type v}
 
 /-- Functorial action. -/
-@[simp] def map (f : α → β) : ulift α → ulift β
+@[simp] protected def map (f : α → β) : ulift α → ulift β
 | (up a) := up (f a)
 
 /-- Embedding of pure values. -/
-@[simp] def pure : α → ulift α := up
+@[simp] protected def pure : α → ulift α := up
 
 /-- Applicative sequencing. -/
-@[simp] def seq : ulift (α → β) → ulift α → ulift β
+@[simp] protected def seq : ulift (α → β) → ulift α → ulift β
 | (up f) (up a) := up (f a)
 
 /-- Monadic bind. -/
-@[simp] def bind : ulift α → (α → ulift β) → ulift β
-| (up a) f := f a
+@[simp] protected def bind : ulift α → (α → ulift β) → ulift β
+| (up a) f := up (down (f a))
+-- The `up ∘ down` gives us more universe polymorphism than simply `f a`.
 
 instance : monad ulift :=
-{ map := @map,
-  pure := @pure,
-  seq := @seq,
-  bind := @bind }
+{ map := @ulift.map,
+  pure := @ulift.pure,
+  seq := @ulift.seq,
+  bind := @ulift.bind }
 
 instance : is_lawful_functor ulift :=
 { id_map := λ α ⟨x⟩, rfl,
@@ -94,8 +95,10 @@ instance : is_lawful_applicative ulift :=
 instance : is_lawful_monad ulift :=
 { bind_pure_comp_eq_map := λ α β f ⟨x⟩, rfl,
   bind_map_eq_seq := λ α β ⟨a⟩ ⟨b⟩, rfl,
-  pure_bind := λ α β x f, rfl,
-  bind_assoc := λ α β γ ⟨x⟩ f g, rfl }
+  pure_bind := λ α β x f,
+    by { dsimp only [bind, pure, ulift.pure, ulift.bind], cases (f x), refl },
+  bind_assoc := λ α β γ ⟨x⟩ f g,
+    by { dsimp only [bind, pure, ulift.pure, ulift.bind], cases (f x), refl } }
 
 @[simp] lemma rec.constant {α : Type u} {β : Sort v} (b : β) :
   @ulift.rec α (λ _, β) (λ _, b) = λ _, b :=

1229f297aae7571bc15f3d974123934ff23bfcad
Jannis Limperg <jannis@limperg.de>
2020-08-04 17:44:54 +0200

    Rename *_correct lemmas

--- src/data/list/indexes.lean
+++ src/data/list/indexes.lean
@@ -25,7 +25,8 @@ theorem foldr_with_index_aux_spec_cons (f : ℕ → α → β → β) (start b a
   f start a (foldr_with_index_aux_spec f (start + 1) b as) :=
 rfl
 
-theorem foldr_with_index_aux_correct (f : ℕ → α → β → β) (start b as) :
+theorem foldr_with_index_aux_eq_foldr_with_index_aux_spec (f : ℕ → α → β → β)
+  (start b as) :
   foldr_with_index_aux f start b as = foldr_with_index_aux_spec f start b as :=
 begin
   induction as generalizing start,
@@ -33,24 +34,26 @@ begin
   { simp only [foldr_with_index_aux, foldr_with_index_aux_spec_cons, *] }
 end
 
-theorem foldr_with_index_correct (f : ℕ → α → β → β) (b : β) (as : list α) :
+theorem foldr_with_index_eq_foldr_enum (f : ℕ → α → β → β) (b : β) (as : list α) :
   foldr_with_index f b as = foldr (uncurry f) b (enum as) :=
 by simp only
-    [ foldr_with_index, foldr_with_index_aux_spec, foldr_with_index_aux_correct,
-      enum ]
+    [ foldr_with_index, foldr_with_index_aux_spec,
+      foldr_with_index_aux_eq_foldr_with_index_aux_spec, enum ]
 
 end foldr_with_index
 
 
-theorem indexes_values_correct (p : α → Prop) [decidable_pred p] (as : list α) :
+theorem indexes_values_eq_filter_enum (p : α → Prop) [decidable_pred p]
+  (as : list α) :
   indexes_values p as = filter (p ∘ prod.snd) (enum as) :=
-by simp [indexes_values, foldr_with_index_correct, uncurry, filter_eq_foldr]
+by simp [indexes_values, foldr_with_index_eq_foldr_enum, uncurry, filter_eq_foldr]
 
-theorem find_indexes_correct (p : α → Prop) [decidable_pred p] (as : list α) :
+theorem find_indexes_eq_map_indexes_values (p : α → Prop) [decidable_pred p]
+  (as : list α) :
   find_indexes p as = map prod.fst (indexes_values p as) :=
 by simp only
-    [ indexes_values_correct, map_filter_eq_foldr, find_indexes,
-      foldr_with_index_correct, uncurry ]
+    [ indexes_values_eq_filter_enum, map_filter_eq_foldr, find_indexes,
+      foldr_with_index_eq_foldr_enum, uncurry ]
 
 
 section foldl_with_index
@@ -65,7 +68,8 @@ theorem foldl_with_index_aux_spec_cons (f : ℕ → α → β → α) (start a b
   foldl_with_index_aux_spec f (start + 1) (f start a b) bs :=
 rfl
 
-theorem foldl_with_index_aux_correct (f : ℕ → α → β → α) (start a bs) :
+theorem foldl_with_index_aux_eq_foldl_with_index_aux_spec (f : ℕ → α → β → α)
+  (start a bs) :
   foldl_with_index_aux f start a bs = foldl_with_index_aux_spec f start a bs :=
 begin
   induction bs generalizing start a,
@@ -73,12 +77,12 @@ begin
   { simp [foldl_with_index_aux, foldl_with_index_aux_spec_cons, *] }
 end
 
-theorem foldl_with_index_correct (f : ℕ → α → β → α) (a : α) (bs : list β) :
+theorem foldl_with_index_eq_foldl_enum (f : ℕ → α → β → α) (a : α) (bs : list β) :
   foldl_with_index f a bs =
   foldl (λ a (p : ℕ × β), f p.fst a p.snd) a (enum bs) :=
 by simp only
-    [ foldl_with_index, foldl_with_index_aux_spec, foldl_with_index_aux_correct,
-      enum ]
+    [ foldl_with_index, foldl_with_index_aux_spec,
+      foldl_with_index_aux_eq_foldl_with_index_aux_spec, enum ]
 
 end foldl_with_index
 
@@ -87,16 +91,16 @@ section mfold_with_index
 
 variables {m : Type u → Type v} [monad m]
 
-theorem mfoldr_with_index_correct {α β} (f : ℕ → α → β → m β) (b : β) (as : list α) :
+theorem mfoldr_with_index_eq_mfoldr_enum {α β} (f : ℕ → α → β → m β) (b : β) (as : list α) :
   mfoldr_with_index f b as = mfoldr (uncurry f) b (enum as) :=
 by simp only
-    [mfoldr_with_index, mfoldr_eq_foldr, foldr_with_index_correct, uncurry]
+    [mfoldr_with_index, mfoldr_eq_foldr, foldr_with_index_eq_foldr_enum, uncurry]
 
-theorem mfoldl_with_index_correct [is_lawful_monad m] {α β} (f : ℕ → β → α → m β)
-  (b : β) (as : list α) :
+theorem mfoldl_with_index_eq_mfoldl_enum [is_lawful_monad m] {α β}
+  (f : ℕ → β → α → m β) (b : β) (as : list α) :
   mfoldl_with_index f b as =
   mfoldl (λ b (p : ℕ × α), f p.fst b p.snd) b (enum as) :=
-by rw [mfoldl_with_index, mfoldl_eq_foldl, foldl_with_index_correct]
+by rw [mfoldl_with_index, mfoldl_eq_foldl, foldl_with_index_eq_foldl_enum]
 
 end mfold_with_index
 
@@ -122,7 +126,8 @@ rfl
 
 variables [is_lawful_monad m]
 
-theorem mmap_with_index_aux_correct {α β} (f : ℕ → α → m β) (start : ℕ) (as : list α) :
+theorem mmap_with_index_aux_eq_mmap_with_index_aux_spec {α β} (f : ℕ → α → m β)
+  (start : ℕ) (as : list α) :
   mmap_with_index_aux f start as = mmap_with_index_aux_spec f start as :=
 begin
   induction as generalizing start,
@@ -130,11 +135,11 @@ begin
   { simp [mmap_with_index_aux, mmap_with_index_aux_spec_cons, *] with monad_norm }
 end
 
-theorem mmap_with_index_correct {α β} (f : ℕ → α → m β) (as : list α) :
+theorem mmap_with_index_eq_mmap_enum {α β} (f : ℕ → α → m β) (as : list α) :
   mmap_with_index f as = mmap (uncurry f) (enum as) :=
 by simp only
-    [ mmap_with_index, mmap_with_index_aux_spec, mmap_with_index_aux_correct,
-      enum ]
+    [ mmap_with_index, mmap_with_index_aux_spec,
+      mmap_with_index_aux_eq_mmap_with_index_aux_spec, enum ]
 
 end mmap_with_index
 

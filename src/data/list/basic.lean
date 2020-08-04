247894dc97e108f7384eafa24ebcca76418724c3
Jannis Limperg <jannis@limperg.de>
2020-08-04 16:59:35 +0200

    reformatting

--- src/data/list/basic.lean
+++ src/data/list/basic.lean
@@ -1305,8 +1305,8 @@ end insert_nth
 
 @[simp] lemma map_nil (f : α → β) : map f [] = [] := rfl
 
-theorem map_eq_foldr (f : α → β) (l : list α)
-  : map f l = foldr (λ a bs, f a :: bs) [] l :=
+theorem map_eq_foldr (f : α → β) (l : list α) :
+  map f l = foldr (λ a bs, f a :: bs) [] l :=
 by induction l; simp *
 
 lemma map_congr {f g : α → β} : ∀ {l : list α}, (∀ x ∈ l, f x = g x) → map f l = map g l
@@ -1368,8 +1368,8 @@ begin
     cases x, simpa using hxy, simp at hxy, simp [y_ih hxy.2, h hxy.1] }
 end
 
-theorem map_filter_eq_foldr (f : α → β) (p : α → Prop) [decidable_pred p] (as : list α)
-  : map f (filter p as) = foldr (λ a bs, if p a then f a :: bs else bs) [] as :=
+theorem map_filter_eq_foldr (f : α → β) (p : α → Prop) [decidable_pred p] (as : list α) :
+  map f (filter p as) = foldr (λ a bs, if p a then f a :: bs else bs) [] as :=
 by induction as; [ refl, simp! [*, apply_ite (map f)] ]
 
 /-! ### map₂ -/
@@ -2541,8 +2541,8 @@ filter_map_eq_map f ▸ s.filter_map _
 section filter
 variables {p : α → Prop} [decidable_pred p]
 
-theorem filter_eq_foldr (p : α → Prop) [decidable_pred p] (l : list α)
-  : filter p l = foldr (λ a out, if p a then a :: out else out) [] l :=
+theorem filter_eq_foldr (p : α → Prop) [decidable_pred p] (l : list α) :
+  filter p l = foldr (λ a out, if p a then a :: out else out) [] l :=
 by induction l; simp [*, filter]
 
 lemma filter_congr {p q : α → Prop} [decidable_pred p] [decidable_pred q]
--- src/data/list/indexes.lean
+++ src/data/list/indexes.lean
@@ -20,20 +20,21 @@ def foldr_with_index_aux_spec (f : ℕ → α → β → β) (start : ℕ) (b : 
   (as : list α) : β :=
 foldr (uncurry f) b $ zip (range' start as.length) as
 
-theorem foldr_with_index_aux_spec_cons (f : ℕ → α → β → β) (start b a as)
-  : foldr_with_index_aux_spec f start b (a :: as)
-  = f start a (foldr_with_index_aux_spec f (start + 1) b as) := rfl
+theorem foldr_with_index_aux_spec_cons (f : ℕ → α → β → β) (start b a as) :
+  foldr_with_index_aux_spec f start b (a :: as) =
+  f start a (foldr_with_index_aux_spec f (start + 1) b as) :=
+rfl
 
-theorem foldr_with_index_aux_correct (f : ℕ → α → β → β) (start b as)
-  : foldr_with_index_aux f start b as = foldr_with_index_aux_spec f start b as :=
+theorem foldr_with_index_aux_correct (f : ℕ → α → β → β) (start b as) :
+  foldr_with_index_aux f start b as = foldr_with_index_aux_spec f start b as :=
 by induction as generalizing start;
     [refl, simp only [foldr_with_index_aux, foldr_with_index_aux_spec_cons, *]]
 
-theorem foldr_with_index_correct' (f : ℕ → α → β → β) (b : β) (as : list α)
-  : foldr_with_index f b as = foldr (uncurry f) b (zip (range as.length) as) :=
+theorem foldr_with_index_correct' (f : ℕ → α → β → β) (b : β) (as : list α) :
+  foldr_with_index f b as = foldr (uncurry f) b (zip (range as.length) as) :=
 by simp only
-    [ foldr_with_index, foldr_with_index_aux_spec, foldr_with_index_aux_correct
-    , ←range_eq_range' ]
+    [ foldr_with_index, foldr_with_index_aux_spec, foldr_with_index_aux_correct,
+      ←range_eq_range' ]
 
 end foldr_with_index
 
@@ -49,21 +50,21 @@ end
 theorem indexed_correct (as : list α) : indexed as = zip (indexes as) as :=
 by simp [indexed, foldr_with_index_correct', indexes_correct, uncurry]
 
-theorem foldr_with_index_correct (f : ℕ → α → β → β) (b : β) (as : list α)
-  : foldr_with_index f b as = foldr (uncurry f) b (indexed as) :=
+theorem foldr_with_index_correct (f : ℕ → α → β → β) (b : β) (as : list α) :
+  foldr_with_index f b as = foldr (uncurry f) b (indexed as) :=
 by rw [indexed_correct, indexes_correct, foldr_with_index_correct']
 
-theorem indexes_values_correct (p : α → Prop) [decidable_pred p] (as : list α)
-  : indexes_values p as = filter (p ∘ prod.snd) (indexed as) :=
+theorem indexes_values_correct (p : α → Prop) [decidable_pred p] (as : list α) :
+  indexes_values p as = filter (p ∘ prod.snd) (indexed as) :=
 by simp
-    [ indexes_values, foldr_with_index_correct, indexes_correct, uncurry
-    , filter_eq_foldr ]
+    [ indexes_values, foldr_with_index_correct, indexes_correct, uncurry,
+      filter_eq_foldr ]
 
-theorem find_indexes_correct (p : α → Prop) [decidable_pred p] (as : list α)
-  : find_indexes p as = map prod.fst (indexes_values p as) :=
+theorem find_indexes_correct (p : α → Prop) [decidable_pred p] (as : list α) :
+  find_indexes p as = map prod.fst (indexes_values p as) :=
 by simp only
-    [ indexes_values_correct, map_filter_eq_foldr, find_indexes
-    , foldr_with_index_correct, uncurry ]
+    [ indexes_values_correct, map_filter_eq_foldr, find_indexes,
+      foldr_with_index_correct, uncurry ]
 
 
 section foldl_with_index
@@ -73,21 +74,22 @@ def foldl_with_index_aux_spec (f : ℕ → α → β → α) (start : ℕ) (a : 
   (bs : list β) : α :=
 foldl (λ a (p : ℕ × β), f p.fst a p.snd) a $ zip (range' start bs.length) bs
 
-theorem foldl_with_index_aux_spec_cons (f : ℕ → α → β → α) (start a b bs)
-  : foldl_with_index_aux_spec f start a (b :: bs)
-  = foldl_with_index_aux_spec f (start + 1) (f start a b) bs := rfl
+theorem foldl_with_index_aux_spec_cons (f : ℕ → α → β → α) (start a b bs) :
+  foldl_with_index_aux_spec f start a (b :: bs) =
+  foldl_with_index_aux_spec f (start + 1) (f start a b) bs :=
+rfl
 
-theorem foldl_with_index_aux_correct (f : ℕ → α → β → α) (start a bs)
-  : foldl_with_index_aux f start a bs = foldl_with_index_aux_spec f start a bs :=
+theorem foldl_with_index_aux_correct (f : ℕ → α → β → α) (start a bs) :
+  foldl_with_index_aux f start a bs = foldl_with_index_aux_spec f start a bs :=
 by induction bs generalizing start a;
     [refl, simp [foldl_with_index_aux, foldl_with_index_aux_spec_cons, *]]
 
-theorem foldl_with_index_correct (f : ℕ → α → β → α) (a : α) (bs : list β)
-  : foldl_with_index f a bs
-  = foldl (λ a (p : ℕ × β), f p.fst a p.snd) a (indexed bs) :=
+theorem foldl_with_index_correct (f : ℕ → α → β → α) (a : α) (bs : list β) :
+  foldl_with_index f a bs =
+  foldl (λ a (p : ℕ × β), f p.fst a p.snd) a (indexed bs) :=
 by simp only
-    [ foldl_with_index, foldl_with_index_aux_spec, foldl_with_index_aux_correct
-    , indexed_correct, indexes_correct, ←range_eq_range' ]
+    [ foldl_with_index, foldl_with_index_aux_spec, foldl_with_index_aux_correct,
+      indexed_correct, indexes_correct, ←range_eq_range' ]
 
 end foldl_with_index
 
@@ -96,15 +98,15 @@ section mfold_with_index
 
 variables {m : Type u → Type v} [monad m]
 
-theorem mfoldr_with_index_correct {α β} (f : ℕ → α → β → m β) (b : β) (as : list α)
-  : mfoldr_with_index f b as = mfoldr (uncurry f) b (indexed as) :=
+theorem mfoldr_with_index_correct {α β} (f : ℕ → α → β → m β) (b : β) (as : list α) :
+  mfoldr_with_index f b as = mfoldr (uncurry f) b (indexed as) :=
 by simp only
     [mfoldr_with_index, mfoldr_eq_foldr, foldr_with_index_correct, uncurry]
 
 theorem mfoldl_with_index_correct [is_lawful_monad m] {α β} (f : ℕ → β → α → m β)
-  (b : β) (as : list α)
-  : mfoldl_with_index f b as
-  = mfoldl (λ b (p : ℕ × α), f p.fst b p.snd) b (indexed as) :=
+  (b : β) (as : list α) :
+  mfoldl_with_index f b as =
+  mfoldl (λ b (p : ℕ × α), f p.fst b p.snd) b (indexed as) :=
 by rw [mfoldl_with_index, mfoldl_eq_foldl, foldl_with_index_correct]
 
 end mfold_with_index
@@ -115,34 +117,34 @@ section mmap_with_index
 variables {m : Type u → Type v} [monad m]
 
 /-- Specification of `mmap_with_index_aux`. -/
-def mmap_with_index_aux_spec {α β} (f : ℕ → α → m β) (start : ℕ) (as : list α)
-  : m (list β) :=
+def mmap_with_index_aux_spec {α β} (f : ℕ → α → m β) (start : ℕ) (as : list α) :
+  m (list β) :=
 mmap (uncurry f) $ zip (range' start as.length) as
 
 theorem mmap_with_index_aux_spec_cons {α β} (f : ℕ → α → m β) (start : ℕ)
-  (a : α) (as : list α)
-  : mmap_with_index_aux_spec f start (a :: as)
-  = do {
-      b ← f start a,
-      bs ← mmap_with_index_aux_spec f (start + 1) as,
-      pure $ b :: bs
-    } :=
+  (a : α) (as : list α) :
+  mmap_with_index_aux_spec f start (a :: as) =
+  do {
+    b ← f start a,
+    bs ← mmap_with_index_aux_spec f (start + 1) as,
+    pure $ b :: bs
+  } :=
 rfl
 
 variables [is_lawful_monad m]
 
-theorem mmap_with_index_aux_correct {α β} (f : ℕ → α → m β) (start : ℕ) (as : list α)
-  : mmap_with_index_aux f start as = mmap_with_index_aux_spec f start as :=
+theorem mmap_with_index_aux_correct {α β} (f : ℕ → α → m β) (start : ℕ) (as : list α) :
+  mmap_with_index_aux f start as = mmap_with_index_aux_spec f start as :=
 by induction as generalizing start;
     [ refl
     , simp [mmap_with_index_aux, mmap_with_index_aux_spec_cons, *]
         with monad_norm ]
 
-theorem mmap_with_index_correct {α β} (f : ℕ → α → m β) (as : list α)
-  : mmap_with_index f as = mmap (uncurry f) (indexed as) :=
+theorem mmap_with_index_correct {α β} (f : ℕ → α → m β) (as : list α) :
+  mmap_with_index f as = mmap (uncurry f) (indexed as) :=
 by simp only
-    [ mmap_with_index, mmap_with_index_aux_spec, mmap_with_index_aux_correct
-    , indexed_correct, indexes_correct, range_eq_range' ]
+    [ mmap_with_index, mmap_with_index_aux_spec, mmap_with_index_aux_correct,
+      indexed_correct, indexes_correct, range_eq_range' ]
 
 end mmap_with_index
 
@@ -152,15 +154,15 @@ section mmap_with_index'
 variables {m : Type u → Type v} [applicative m] [is_lawful_applicative m]
 
 theorem mmap_with_index'_aux_eq_mmap_with_index_aux {α} (f : ℕ → α → m punit)
-  (start : ℕ) (as : list α)
-  : mmap_with_index'_aux f start as
-  = mmap_with_index_aux f start as *> pure punit.star :=
+  (start : ℕ) (as : list α) :
+  mmap_with_index'_aux f start as =
+  mmap_with_index_aux f start as *> pure punit.star :=
 by induction as generalizing start;
     simp [mmap_with_index'_aux, mmap_with_index_aux, *, seq_right_eq, const]
       with functor_norm
 
-theorem mmap_with_index'_eq_mmap_with_index {α} (f : ℕ → α → m punit) (as : list α)
-  : mmap_with_index' f as = mmap_with_index f as *> pure punit.star :=
+theorem mmap_with_index'_eq_mmap_with_index {α} (f : ℕ → α → m punit) (as : list α) :
+  mmap_with_index' f as = mmap_with_index f as *> pure punit.star :=
 by apply mmap_with_index'_aux_eq_mmap_with_index_aux
 
 end mmap_with_index'

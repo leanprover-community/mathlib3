ad0e39fbfb1be4e39b0a72c3f33781a866557cf5
Simon Hudon <simon.hudon@gmail.com>
2020-08-05 15:11:18 -0400

    add documentation

--- src/tactic/mk_opaque.lean
+++ src/tactic/mk_opaque.lean
@@ -1,11 +1,39 @@
+/-
+Copyright (c) 2020 Simon Hudon. All rights reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
+Authors: Simon Hudon
+-/
 import tactic.core
+
+/-!
+# mk_opaque
+
+Tactic mk_opaque turns local definitions into local constants
+-/
 
 namespace tactic
 
 setup_tactic_parser
 
 /-- `mk_opaque x y z`, with `x`, `y`, `z` local definitions, transforms them into
-normal local constants -/
+normal local constants
+
+In the goal:
+
+```lean
+x y : ℤ,
+z : ℤ := x + y
+⊢ P
+```
+
+`mk_opaque z` yields the following goal:
+
+```lean
+x y : ℤ,
+z : ℤ
+⊢ P
+```
+-/
 meta def interactive.mk_opaque (ns : parse ident*) : tactic unit :=
 ns.mmap' $ λ n,
 do h ← get_local n,

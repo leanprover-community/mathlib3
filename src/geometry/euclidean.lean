5899c8914c08342031918ed2b11f477256c5ff69
Joseph Myers <jsm@polyomino.org.uk>
2020-08-08 13:44:03 +0000

    Move linear_algebra/affine_space.lean to linear_algebra/affine_space/basic.lean.
    Preparation for splitting into smaller pieces.

--- src/geometry/euclidean.lean
+++ src/geometry/euclidean.lean
@@ -5,7 +5,7 @@ Author: Joseph Myers.
 -/
 import analysis.normed_space.real_inner_product
 import analysis.normed_space.add_torsor
-import linear_algebra.affine_space
+import linear_algebra.affine_space.basic
 import tactic.interval_cases
 
 noncomputable theory

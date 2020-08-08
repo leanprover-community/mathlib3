107d6662dbbd62d1a7024f52635531477a481470
Joseph Myers <jsm@polyomino.org.uk>
2020-08-08 13:55:21 +0000

    Split out affine_independent and related lemmas and definitions.

--- src/geometry/euclidean.lean
+++ src/geometry/euclidean.lean
@@ -5,7 +5,7 @@ Author: Joseph Myers.
 -/
 import analysis.normed_space.real_inner_product
 import analysis.normed_space.add_torsor
-import linear_algebra.affine_space.basic
+import linear_algebra.affine_space.independent
 import tactic.interval_cases
 
 noncomputable theory

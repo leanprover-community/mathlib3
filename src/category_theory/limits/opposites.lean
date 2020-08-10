4eec9de5b6877870411896c73ea3002e2c360492
Scott Morrison <scott.morrison@gmail.com>
2020-08-10 15:47:34 +1000

    add missing doc-strings to old file

--- src/category_theory/limits/opposites.lean
+++ src/category_theory/limits/opposites.lean
@@ -18,6 +18,9 @@ variables {C : Type u} [category.{v} C]
 variables {J : Type v} [small_category J]
 variable (F : J ⥤ Cᵒᵖ)
 
+/--
+If `F.left_op : Jᵒᵖ ⥤ C` has a chosen colimit, we can construct a chosen limit for `F : J ⥤ Cᵒᵖ`.
+-/
 def has_limit_of_has_colimit_left_op [has_colimit F.left_op] : has_limit F :=
 { cone := cone_of_cocone_left_op (colimit.cocone F.left_op),
   is_limit :=
@@ -41,15 +44,24 @@ def has_limit_of_has_colimit_left_op [has_colimit F.left_op] : has_limit F :=
       refl,
     end } }
 
+/--
+If `C` has chosen colimits of shape `Jᵒᵖ`, we can construct chosen limits in `Cᵒᵖ` of shape `J`.
+-/
 def has_limits_of_shape_op_of_has_colimits_of_shape [has_colimits_of_shape Jᵒᵖ C] :
   has_limits_of_shape J Cᵒᵖ :=
 { has_limit := λ F, has_limit_of_has_colimit_left_op F }
 
 local attribute [instance] has_limits_of_shape_op_of_has_colimits_of_shape
 
+/--
+If `C` has chosen colimits, we can construct chosen limits for `Cᵒᵖ`.
+-/
 def has_limits_op_of_has_colimits [has_colimits C] : has_limits Cᵒᵖ :=
 { has_limits_of_shape := λ J 𝒥, by { resetI, apply_instance } }
 
+/--
+If `F.left_op : Jᵒᵖ ⥤ C` has a chosen limit, we can construct a chosen colimit for `F : J ⥤ Cᵒᵖ`.
+-/
 def has_colimit_of_has_limit_left_op [has_limit F.left_op] : has_colimit F :=
 { cocone := cocone_of_cone_left_op (limit.cone F.left_op),
   is_colimit :=
@@ -70,16 +82,25 @@ def has_colimit_of_has_limit_left_op [has_limit F.left_op] : has_colimit F :=
       refl,
     end } }
 
+/--
+If `C` has chosen colimits of shape `Jᵒᵖ`, we can construct chosen limits in `Cᵒᵖ` of shape `J`.
+-/
 def has_colimits_of_shape_op_of_has_limits_of_shape [has_limits_of_shape Jᵒᵖ C] :
   has_colimits_of_shape J Cᵒᵖ :=
 { has_colimit := λ F, has_colimit_of_has_limit_left_op F }
 
 local attribute [instance] has_colimits_of_shape_op_of_has_limits_of_shape
 
+/--
+If `C` has chosen limits, we can construct chosen colimits for `Cᵒᵖ`.
+-/
 def has_colimits_op_of_has_limits [has_limits C] : has_colimits Cᵒᵖ :=
 { has_colimits_of_shape := λ J 𝒥, by { resetI, apply_instance } }
 
 variables (X : Type v)
+/--
+If `C` has products indexed by `X`, then `Cᵒᵖ` has coproducts indexed by `X`.
+-/
 def has_coproducts_opposite [has_products_of_shape X C] :
   has_coproducts_of_shape X Cᵒᵖ :=
 begin
@@ -88,6 +109,9 @@ begin
   apply_instance
 end
 
+/--
+If `C` has coproducts indexed by `X`, then `Cᵒᵖ` has products indexed by `X`.
+-/
 def has_products_opposite [has_coproducts_of_shape X C] :
   has_products_of_shape X Cᵒᵖ :=
 begin
@@ -95,6 +119,5 @@ begin
     has_colimits_of_shape_of_equivalence (discrete.opposite X).symm,
   apply_instance
 end
-
 
 end category_theory.limits

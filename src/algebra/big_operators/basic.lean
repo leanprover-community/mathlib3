df7b1bc3aef3bb2e15a959a31b26fc2a33da596c
Patrick Lutz <pglutz@berkeley.edu>
2020-08-10 21:50:18 -0700

    rewrite prod_subset_one_on_sdiff according to Scott's suggestion

--- src/algebra/big_operators/basic.lean
+++ src/algebra/big_operators/basic.lean
@@ -550,17 +550,13 @@ begin
 end
 
 @[to_additive]
-lemma prod_subset_one_on_sdiff (h : s₁ ⊆ s₂) (hg : ∀ x ∈ s₂, x ∉ s₁ → g x = 1)
+lemma prod_subset_one_on_sdiff [decidable_eq α] (h : s₁ ⊆ s₂) (hg : ∀ x ∈ (s₂ \ s₁), g x = 1)
   (hfg : ∀ x ∈ s₁, f x = g x) : ∏ i in s₁, f i = ∏ i in s₂, g i :=
 begin
-  haveI := classical.dec_eq α,
   rw ← prod_sdiff h,
   have : ∏ x in s₂ \ s₁, g x = 1,
   { apply prod_eq_one,
-    intros x hx,
-    simp only [mem_sdiff] at *,
-    cases hx,
-    solve_by_elim, },
+    exact hg, },
   rw [this, one_mul],
   apply prod_congr,
   { refl, },

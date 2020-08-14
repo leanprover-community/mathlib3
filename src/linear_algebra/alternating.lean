3f77cbe34f2e7d5c9338f84843d9a534eb0a8dfa
zhangir-azerbayev <zazerbayev@gmail.com>
2020-08-14 20:33:47 +0200

    style(linear_algebra/alternating): did reviewer suggestions for alternating.lean

--- src/linear_algebra/alternating.lean
+++ src/linear_algebra/alternating.lean
@@ -25,10 +25,10 @@ variables (M : Type*) [add_comm_group M] [module R M]
 variables (N : Type*) [add_comm_group N] [module R N]
 variables (ι : Type*) [decidable_eq ι]
 
-class alternating_map extends multilinear_map R (λ i : ι , M) N :=
+structure alternating_map extends multilinear_map R (λ i : ι , M) N :=
 (map_alternating {ν : ι → M} {i j : ι} (h : ν i = ν j) (hij : i ≠ j) : to_fun ν = 0)
 
-namespace alternating
+namespace alternating_map
 
 variable {f : alternating_map R M N ι}
 variable (ν : ι → M)
@@ -46,11 +46,10 @@ f.to_multilinear_map.map_add' ν i x y
   f (update ν i (r • x)) = r • f (update ν i x) :=
 f.to_multilinear_map.map_smul' ν i r x
 
-lemma eq_args {i j : ι} (h : ν i = ν j) (hij : i ≠ j) : f ν = 0 :=
-alternating_map.map_alternating h hij
+lemma eq_args {i j : ι} (h : ν i = ν j) (hij : i ≠ j) : f ν = 0 := f.map_alternating h hij
 
-lemma map_add_swap {i j : ι} (hij : i ≠ j):
-f ν + f (ν ∘ equiv.swap i j) = 0 :=
+lemma map_add_swap {i j : ι} (hij : i ≠ j) :
+  f ν + f (ν ∘ equiv.swap i j) = 0 :=
 begin
   have key : f (function.update (function.update ν i (ν i + ν j)) j (ν i + ν j)) = 0 :=
     by rw eq_args (function.update (function.update ν i (ν i + ν j)) j (ν i + ν j))
@@ -67,26 +66,26 @@ begin
   function.update_same]) hij at key,
   rw [add_zero, add_comm] at key,
   convert key,
-  simp,
-  ext x,
+  { simp,  },
+  { ext x,
     cases classical.em (x = i) with hx hx,
     --case x = i
-    rw hx,
-    simp only [equiv.swap_apply_left, function.comp_app],
-    rw function.update_same,
+    { rw hx,
+      simp only [equiv.swap_apply_left, function.comp_app],
+      rw function.update_same,  },
     --case x ≠ i
-    cases classical.em (x = j) with hx1 hx1,
-    rw hx1,
-    simp only [equiv.swap_apply_left, function.comp_app],
-    rw function.update_noteq (ne_comm.mp hij),
-    simp,
+    { cases classical.em (x = j) with hx1 hx1,
+      { rw hx1,
+        simp only [equiv.swap_apply_left, function.comp_app],
+        rw function.update_noteq (ne_comm.mp hij),
+        simp, },
     --case x ≠ i, x ≠ j,
-    simp only [hx, hx1, function.comp_app, function.update_noteq, ne.def, not_false_iff],
-    rw equiv.swap_apply_of_ne_of_ne hx hx1,
+      { simp only [hx, hx1, function.comp_app, function.update_noteq, ne.def, not_false_iff],
+        rw equiv.swap_apply_of_ne_of_ne hx hx1, }, }, },
 end
 
-lemma map_swap {i j : ι} (hij : i ≠ j):
-f (ν ∘ equiv.swap i j) = - f ν  :=
+lemma map_swap {i j : ι} (hij : i ≠ j) :
+  f (ν ∘ equiv.swap i j) = - f ν  :=
 begin
   apply eq_neg_of_add_eq_zero,
   rw add_comm,
@@ -96,20 +95,18 @@ end
 variable [fintype ι]
 
 lemma map_perm (σ : equiv.perm ι) :
-f ν = (equiv.perm.sign σ : ℤ) • f (ν ∘ σ) :=
+  f ν = (equiv.perm.sign σ : ℤ) • f (ν ∘ σ) :=
 begin
   apply equiv.perm.swap_induction_on' σ,
-  --Base case
-  rw equiv.perm.sign_one,
-  simp only [units.coe_one, one_smul, coe_fn_coe_base],
-  congr,
-  --Inductive step
-  intros s x y hxy hI,
-  have assoc : ν ∘ (s * equiv.swap x y : equiv.perm ι) = (ν ∘ s ∘ equiv.swap x y) := rfl,
-  rw [assoc, map_swap (ν ∘ s) hxy, ←neg_one_smul ℤ (f (ν ∘ s))],
-  have h1 : (-1 : ℤ) = equiv.perm.sign (equiv.swap x y) := by simp [hxy],
-  rw [h1, smul_smul, ←units.coe_mul, ←equiv.perm.sign_mul, mul_assoc, equiv.swap_mul_self, mul_one],
-  assumption,
+  { rw equiv.perm.sign_one,
+    simp only [units.coe_one, one_smul, coe_fn_coe_base],
+    congr,  },
+  { intros s x y hxy hI,
+    have assoc : ν ∘ (s * equiv.swap x y : equiv.perm ι) = (ν ∘ s ∘ equiv.swap x y) := rfl,
+    rw [assoc, map_swap (ν ∘ s) hxy, ←neg_one_smul ℤ (f (ν ∘ s))],
+    have h1 : (-1 : ℤ) = equiv.perm.sign (equiv.swap x y) := by simp [hxy],
+    rw [h1, smul_smul, ←units.coe_mul, ←equiv.perm.sign_mul, mul_assoc, equiv.swap_mul_self, mul_one],
+    assumption, },
 end
 
-end alternating
+end alternating_map

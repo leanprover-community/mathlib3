6d3864410c78699fc527b806db10619dc41515d3
Markus Himmel <markus@himmel-villmar.de>
2020-08-15 09:30:16 +0200

    Fix

--- src/category_theory/abelian/basic.lean
+++ src/category_theory/abelian/basic.lean
@@ -91,7 +91,7 @@ variables {C : Type u} [category.{v} C]
 variables (C)
 
 section prio
-set_option default_priority 200
+set_option default_priority 100
 
 /--
 A (preadditive) category `C` is called abelian if it has all finite products,

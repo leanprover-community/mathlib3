dee5af0e34e9cf2a07afa82c71f3074014c9013f
James Arthur <jamesarthur7@icloud.com>
2020-08-16 12:17:50 +0100

    Update src/data/complex/exponential.lean
    Renamed `sinh_def` to `sinh_eq`
    
    Co-authored-by: Johan Commelin <johan@commelin.net>

--- src/data/complex/exponential.lean
+++ src/data/complex/exponential.lean
@@ -504,7 +504,7 @@ lemma two_cosh : 2 * cosh x = exp x + exp (-x) :=
 mul_div_cancel' _ two_ne_zero'
 
 /-- The real definition of `sinh`-/
-lemma sinh_def (x : ℝ) : sinh x = (exp x - exp (-x)) / 2 :=
+lemma sinh_eq (x : ℝ) : sinh x = (exp x - exp (-x)) / 2 :=
 by simp only [sinh, complex.sinh, complex.div_re, complex.exp_of_real_re, complex.one_re,
   bit0_zero, add_zero, complex.sub_re, euclidean_domain.zero_div, complex.bit0_re,
   complex.one_im, complex.bit0_im, mul_zero, ← complex.of_real_neg, complex.norm_sq];

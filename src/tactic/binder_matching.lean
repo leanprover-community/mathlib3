dbb726148e36ed1ca2893c0e134d80ef2c14c676
Jannis Limperg <jannis@limperg.de>
2020-08-18 17:56:40 +0200

    doc updates

--- src/tactic/binder_matching.lean
+++ src/tactic/binder_matching.lean
@@ -48,6 +48,14 @@ Based on `mk_binder`, we define many variations of this recipe:
   ```
 - `tactic.get_pi_binders` and `tactic.get_pi_binders_nondep` are more
   specialised instances of `mk_binder`.
+
+The `mk_local_*` functions are commonly used like this:
+
+1. Open (some of) the binders of an expression `e`, producing local constants
+   `lcs` and the 'body' `e'` of `e`.
+2. Process `e'` in some way.
+3. Reconstruct the binders using `tactic.pis` or `tactic.lambdas`, which
+   Π/λ-bind the `lcs` in `e'`.
 -/
 
 namespace tactic
@@ -81,8 +89,8 @@ open expr
 3. Use `result` to construct a result.
 4. Recurse into the binder body.
 
-Returns the list of results and the rest of the expression (with previously
-bound variables instantiated with their replacements).
+`mk_binders` returns the list of results and the rest of the expression (with
+previously bound variables instantiated with their replacements).
 
 `binder_replacement` and `result` receive the binder information returned by
 `match_binder`. `match_binder` and `result` also receive the current recursion
@@ -121,41 +129,47 @@ of functions. It implements the "match exactly `max_depth` binders" logic.
     pure (some x)
 
 /--
-Like `tactic.mk_local_pis` but the Π binders of the input expression are
-instantiated with new metavariables instead of new local constants.
+`mk_meta_pis e` instantiates all leading Π binders of `e` with fresh
+metavariables. Returns the metavariables and the remainder of `e`. This is
+`tactic.mk_local_pis` but with metavariables instead of local constants.
 -/
 meta def mk_meta_pis : expr → tactic (list expr × expr) :=
 mk_binders' (λ _ e, pure e.match_pi) (λ _ _ t, mk_meta_var t)
 
 /--
-Like `tactic.mk_local_pis` but only instantiates the first `n` Π binders. Fails
-if `e` does not start with at least `n` Π binders.
+`mk_local_pisn e n` instantiates the first `n` Π binders of `e` with fresh local
+constants. Returns the local constants and the remainder of `e`. Fails if `e`
+does not start with at least `n` Π binders. This is `tactic.mk_local_pis` but
+limited to `n` binders.
 -/
 meta def mk_local_pisn (e : expr) (n : ℕ) : tactic (list expr × expr) :=
 mk_binders' (match_with_depth (pure ∘ match_pi) n) mk_local' e
 
 /--
-Like `mk_meta_pis` but only instantiates the first `n` Π binders. Fails if `e`
-does not start with at least `n` Π binders.
+`mk_meta_pisn e n` instantiates the first `n` Π binders of `e` with fresh
+metavariables. Returns the metavariables and the remainder of `e`. This is
+`mk_local_pisn` but with metavariables instead of local constants.
 -/
 meta def mk_meta_pisn (e : expr) (n : ℕ) : tactic (list expr × expr) :=
 mk_binders' (match_with_depth (pure ∘ match_pi) n) (λ _ _ t, mk_meta_var t) e
 
 /--
-Like `mk_local_pis` but the input expression is reduced to weak head normal form
-(with transparency `md`) each time before we check whether it is a Π type.
-`unfold_ginductive` controls whether constructors of generalised inductive types
-are unfolded.
+`mk_local_pis_whnf e md unfold_ginductive` instantiates all leading Π binders of
+`e` with fresh local constants. The leading Π binders of `e` are matched up to
+normalisation with transparency `md`. `unfold_ginductive` determines whether
+constructors of generalised inductive types are unfolded during normalisation.
+This is `tactic.mk_local_pis` up to normalisation.
 -/
 meta def mk_local_pis_whnf (e : expr) (md := semireducible)
   (unfold_ginductive := tt) : tactic (list expr × expr) :=
 mk_binders' (λ _ e, match_pi <$> whnf e md unfold_ginductive) mk_local' e
 
 /--
-Like `mk_meta_pis` but the input expression is reduced to weak head normal form
-(with transparency `md`) each time before we check whether it is a Π type.
-`unfold_ginductive` controls whether constructors of generalised inductive types
-are unfolded.
+`mk_meta_pis_whnf e md unfold_ginductive` instantiates all leading Π binders of
+`e` with fresh metavariables. The leading Π binders of `e` are matched up to
+normalisation with transparency `md`. `unfold_ginductive` determines whether
+constructors of generalised inductive types are unfolded during normalisation.
+This is `mk_meta_pis` up to normalisation.
 -/
 meta def mk_meta_pis_whnf (e : expr) (md := semireducible)
   (unfold_ginductive := tt) : tactic (list expr × expr) :=
@@ -163,10 +177,11 @@ mk_binders' (λ _ e, match_pi <$> whnf e md unfold_ginductive)
   (λ _ _ t, mk_meta_var t) e
 
 /--
-Like `mk_local_pisn` but the input expression is reduced to weak head normal
-form (with transparency `md`) each time before we check whether it is a Π type.
-`unfold_ginductive` controls whether constructors of generalised inductive types
-are unfolded.
+`mk_local_pisn_whnf e n md unfold_ginductive` instantiates the first `n` Π
+binders of `e` with fresh local constants. The leading Π binders of `e` are
+matched up to normalisation with transparency `md`. `unfold_ginductive`
+determines whether constructors of generalised inductive types are unfolded
+during normalisation. This is `mk_local_pis_whnf` but restricted to `n` binders.
 -/
 meta def mk_local_pisn_whnf (e : expr) (n : ℕ) (md := semireducible)
   (unfold_ginductive := tt) : tactic (list expr × expr) :=
@@ -174,10 +189,11 @@ mk_binders' (match_with_depth (λ e, match_pi <$> whnf e md unfold_ginductive) n
   mk_local' e
 
 /--
-Like `mk_meta_pisn` but the input expression is reduced to weak head normal
-form (with transparency `md`) each time before we check whether it is a Π type.
-`unfold_ginductive` controls whether constructors of generalised inductive types
-are unfolded.
+`mk_meta_pisn_whnf e n md unfold_ginductive` instantiates the first `n` Π
+binders of `e` with fresh metavariables. The leading Π binders of `e` are
+matched up to normalisation with transparency `md`. `unfold_ginductive`
+determines whether constructors of generalised inductive types are unfolded
+during normalisation. This is `mk_meta_pis_whnf` but restricted to `n` binders.
 -/
 meta def mk_meta_pisn_whnf (e : expr) (n : ℕ) (md := semireducible)
   (unfold_ginductive := tt) : tactic (list expr × expr) :=
@@ -185,17 +201,21 @@ mk_binders' (match_with_depth (λ e, match_pi <$> whnf e md unfold_ginductive) n
   (λ _ _ t, mk_meta_var t) e
 
 /--
-A variant of `tactic.mk_local_pis` which returns `binder`s instead of local
-constants. See also `expr.pi_binders` (which produces open terms).
+`get_pi_binders e` instantiates all leading Π binders of `e` with fresh local
+constants. Returns the remainder of `e` and information about the binders that
+were instantiated (but not the new local constants). See also `expr.pi_binders`
+(which produces open terms).
 -/
 meta def get_pi_binders : expr → tactic (list binder × expr) :=
 mk_binders (λ _ e, pure e.match_pi) mk_local'
   (λ _ name bi type _, pure $ some $ binder.mk name bi type)
 
 /--
-A variant of `get_pi_binders` that returns only nondependent binders (i.e.
-those binders that do not occur later in the expression). Also returns the
-index of each returned binder (starting at 0).
+`get_pi_binders_nondep e` instantiates all leading Π binders of `e` with fresh
+local constants. Returns the remainder of `e` and information about the
+*nondependent* binders that were instantiated (but not the new local constants).
+A nondependent binder is one that does not appear later in the expression.
+Also returns the index of each returned binder (starting at 0).
 -/
 meta def get_pi_binders_nondep : expr → tactic (list (ℕ × binder) × expr) :=
 mk_binders (λ _ e, pure e.match_pi) mk_local'
@@ -203,54 +223,56 @@ mk_binders (λ _ e, pure e.match_pi) mk_local'
     pure $ if body.has_var then none else some (depth, binder.mk name bi type))
 
 /--
-`mk_local_lambdas e` instantiates all leading lambda binders of `e` with fresh
+`mk_local_lambdas e` instantiates all leading λ binders of `e` with fresh
 local constants. Returns the new local constants and the remainder of `e`.
-Example:
-
-```
-mk_local_lambdas `(λ (x : X) (y : Y), f x y) =
-  ([_fresh.1, _fresh.2], `(f _fresh.1 _fresh.2))
-```
+This is `tactic.mk_local_pis` but for λ binders rather than Π binders.
 -/
-meta def mk_local_lambdas : expr → tactic (list expr × expr) :=
-mk_binders' (λ _ e, pure e.match_lam) mk_local'
+meta def mk_local_lambdas : expr → tactic (list expr × expr) := mk_binders' (λ _
+e, pure e.match_lam) mk_local'
 
 /--
-Like `mk_local_lambdas` but instantiates the bound variables with fresh
-metavariables instead of fresh local constants.
+`mk_meta_lambdas e` instantiates all leading λ binders of `e` with fresh
+metavariables. Returns the new metavariables and the remainder of `e`. This is
+`mk_local_lambdas` but with metavariables instead of local constants.
 -/
 meta def mk_meta_lambdas : expr → tactic (list expr × expr) :=
 mk_binders' (λ _ e, pure e.match_lam) (λ _ _ t, mk_meta_var t)
 
 /--
-Like `mk_local_lambdas` but only instantiates the first `n` lambda binders.
-Fails if `e` does not start with at least `n` lambda binders.
+`mk_local_lambdasn e n` instantiates the first `n` λ binders of `e` with fresh
+local constants. Returns the new local constants and the remainder of `e`. Fails
+if `e` does not start with at least `n` λ binders. This is `mk_local_lambdas`
+but restricted to the first `n` binders.
 -/
 meta def mk_local_lambdasn (e : expr) (n : ℕ) : tactic (list expr × expr) :=
 mk_binders' (match_with_depth (pure ∘ match_lam) n) mk_local' e
 
 /--
-Like `mk_meta_lambdas` but only instantiates the first `n` lambda binders.
-Fails if `e` does not start with at least `n` lambda binders.
+`mk_meta_lambdasn e n` instantiates the first `n` λ binders of `e` with fresh
+metavariables. Returns the new metavariables and the remainder of `e`. Fails
+if `e` does not start with at least `n` λ binders. This is `mk_meta_lambdas`
+but restricted to the first `n` binders.
 -/
 meta def mk_meta_lambdasn (e : expr) (n : ℕ) : tactic (list expr × expr) :=
 mk_binders' (match_with_depth (pure ∘ match_lam) n) (λ _ _ t, mk_meta_var t) e
 
 /--
-Like `mk_local_lambdas` but the input expression is reduced to weak head normal
-form (with transparency `md`) each time before we check whether it start with a
-lambda binder. `unfold_ginductive` controls whether constructors of generalised
-inductive types are unfolded.
+`mk_local_lambdas_whnf e md unfold_ginductive` instantiates all leading λ
+binders of `e` with fresh local constants. The leading λ binders of `e` are
+matched up to normalisation with transparency `md`. `unfold_ginductive`
+determines whether constructors of generalised inductive types are unfolded
+during normalisation. This is `mk_local_lambdas` up to normalisation.
 -/
 meta def mk_local_lambdas_whnf (e : expr) (md := semireducible)
   (unfold_ginductive := tt) : tactic (list expr × expr) :=
 mk_binders' (λ _ e, match_lam <$> whnf e md unfold_ginductive) mk_local' e
 
 /--
-Like `mk_meta_lambdas` but the input expression is reduced to weak head normal
-form (with transparency `md`) each time before we check whether it start with a
-lambda binder. `unfold_ginductive` controls whether constructors of generalised
-inductive types are unfolded.
+`mk_meta_lambdas_whnf e md unfold_ginductive` instantiates all leading λ binders
+of `e` with fresh metavariables. The leading λ binders of `e` are matched up to
+normalisation with transparency `md`. `unfold_ginductive` determines whether
+constructors of generalised inductive types are unfolded during normalisation.
+This is `mk_meta_lambdas` up to normalisation.
 -/
 meta def mk_meta_lambdas_whnf (e : expr) (md := semireducible)
   (unfold_ginductive := tt) : tactic (list expr × expr) :=
@@ -258,10 +280,12 @@ mk_binders' (λ _ e, match_lam <$> whnf e md unfold_ginductive)
   (λ _ _ t, mk_meta_var t) e
 
 /--
-Like `mk_local_lambdasn` but the input expression is reduced to weak head normal
-form (with transparency `md`) each time before we check whether it start with a
-lambda binder. `unfold_ginductive` controls whether constructors of generalised
-inductive types are unfolded.
+`mk_local_lambdasn_whnf e md unfold_ginductive` instantiates the first `n` λ
+binders of `e` with fresh local constants. The λ binders are matched up to
+normalisation with transparency `md`. `unfold_ginductive` determines whether
+constructors of generalised inductive types are unfolded during normalisation.
+Fails if `e` does not start with `n` λ binders (after normalisation). This is
+`mk_local_lambdasn` up to normalisation.
 -/
 meta def mk_local_lambdasn_whnf (e : expr) (n : ℕ) (md := semireducible)
   (unfold_ginductive := tt) : tactic (list expr × expr) :=
@@ -269,10 +293,12 @@ mk_binders' (match_with_depth (λ e, match_lam <$> whnf e md unfold_ginductive) 
   mk_local' e
 
 /--
-Like `mk_meta_lambdasn` but the input expression is reduced to weak head normal
-form (with transparency `md`) each time before we check whether it start with a
-lambda binder. `unfold_ginductive` controls whether constructors of generalised
-inductive types are unfolded.
+`mk_meta_lambdasn_whnf e md unfold_ginductive` instantiates the first `n` λ
+binders of `e` with fresh metavariables. The λ binders are matched up to
+normalisation with transparency `md`. `unfold_ginductive` determines whether
+constructors of generalised inductive types are unfolded during normalisation.
+Fails if `e` does not start with `n` λ binders (after normalisation). This is
+`mk_meta_lambdasn` up to normalisation.
 -/
 meta def mk_meta_lambdasn_whnf (e : expr) (n : ℕ) (md := semireducible)
   (unfold_ginductive := tt) : tactic (list expr × expr) :=

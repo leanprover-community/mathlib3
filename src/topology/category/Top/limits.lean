87b0600213ad203f169905bfcd54dec272cc3288
Scott Morrison <scott.morrison@gmail.com>
2020-08-20 17:48:15 +1000

    doc-strings

--- src/topology/category/Top/limits.lean
+++ src/topology/category/Top/limits.lean
@@ -19,12 +19,22 @@ variables {J : Type u} [small_category J]
 
 local notation `forget` := forget Top
 
+/--
+A choice of limit cone for a functor `F : J ⥤ Top`.
+Generally you should just use `limit.cone F`, unless you need the actual definition
+(which is in terms of `types.limit_cone`).
+-/
 def limit_cone (F : J ⥤ Top.{u}) : cone F :=
 { X := ⟨(types.limit_cone (F ⋙ forget)).X, ⨅j, (F.obj j).str.induced ((types.limit_cone (F ⋙ forget)).π.app j)⟩,
   π :=
   { app := λ j, ⟨(types.limit_cone (F ⋙ forget)).π.app j, continuous_iff_le_induced.mpr (infi_le _ _)⟩,
     naturality' := λ j j' f, continuous_map.coe_inj ((types.limit_cone (F ⋙ forget)).π.naturality f) } }
 
+/--
+The chosen cone `Top.limit_cone F` for a functor `F : J ⥤ Top` is a limit cone.
+Generally you should just use `limit.is_limit F`, unless you need the actual definition
+(which is in terms of `types.limit_cone_is_limit`).
+-/
 def limit_cone_is_limit (F : J ⥤ Top.{u}) : is_limit (limit_cone F) :=
 by { refine is_limit.of_faithful forget (types.limit_cone_is_limit _) (λ s, ⟨_, _⟩) (λ s, rfl),
      exact continuous_iff_coinduced_le.mpr (le_infi $ λ j,
@@ -40,12 +50,22 @@ instance forget_preserves_limits : preserves_limits (forget : Top.{u} ⥤ Type u
     by exactI preserves_limit_of_preserves_limit_cone
       (limit_cone_is_limit F) (types.limit_cone_is_limit (F ⋙ forget)) } }
 
+/--
+A choice of colimit cocone for a functor `F : J ⥤ Top`.
+Generally you should just use `colimit.coone F`, unless you need the actual definition
+(which is in terms of `types.colimit_cocone`).
+-/
 def colimit_cocone (F : J ⥤ Top.{u}) : cocone F :=
 { X := ⟨(types.colimit_cocone (F ⋙ forget)).X, ⨆ j, (F.obj j).str.coinduced ((types.colimit_cocone (F ⋙ forget)).ι.app j)⟩,
   ι :=
   { app := λ j, ⟨(types.colimit_cocone (F ⋙ forget)).ι.app j, continuous_iff_coinduced_le.mpr (le_supr _ j)⟩,
     naturality' := λ j j' f, continuous_map.coe_inj ((types.colimit_cocone (F ⋙ forget)).ι.naturality f) } }
 
+/--
+The chosen cocone `Top.colimit_cocone F` for a functor `F : J ⥤ Top` is a colimit cocone.
+Generally you should just use `colimit.is_colimit F`, unless you need the actual definition
+(which is in terms of `types.colimit_cocone_is_colimit`).
+-/
 def colimit_cocone_is_colimit (F : J ⥤ Top.{u}) : is_colimit (colimit_cocone F) :=
 by { refine is_colimit.of_faithful forget (types.colimit_cocone_is_colimit _) (λ s, ⟨_, _⟩) (λ s, rfl),
      exact continuous_iff_le_induced.mpr (supr_le $ λ j,

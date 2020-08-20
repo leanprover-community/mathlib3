c26723b8e5cdb7b7b93ec8f318f29b9372ab7001
Johan Commelin <johan@commelin.net>
2020-08-20 11:40:29 +0000

    Add ! variant

--- test/dec_trivial_tactic.lean
+++ test/dec_trivial_tactic.lean
@@ -1,13 +1,13 @@
 import data.nat.basic
 
 example (n : ℕ) (h : n < 2) : n = 0 ∨ n = 1 :=
-by dec_trivial
+by dec_trivial!
 
 example (n : ℕ) (h : n < 3) : true :=
 begin
   let k : ℕ := n - 1,
   have : ∀ i < k, i = 0 ∨ i = 1,
-  by dec_trivial,
+  by dec_trivial!,
   trivial
 end
 
@@ -15,6 +15,12 @@ example (b : bool) (h : b = tt) : true :=
 begin
   let b₁ : bool := b,
   have : ∀ b₂ : bool, b₂ ≠ b₁ → b₂ = ff,
-  by dec_trivial,
+  by dec_trivial!,
   trivial
+end
+
+example (n : ℕ) : (0 : fin (n+1)) + 0 = 0 :=
+begin
+  success_if_fail { dec_trivial! },
+  dec_trivial
 end

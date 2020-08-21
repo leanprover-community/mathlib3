401783d8a71ca26eecebac116e80dcdda8702d64
Bhavik Mehta <bhavikmehta8@gmail.com>
2020-08-21 20:17:05 +0100

    fixup

--- src/category_theory/adjunction/opposites.lean
+++ src/category_theory/adjunction/opposites.lean
@@ -64,4 +64,13 @@ adjunction.mk_of_hom_equiv
 { hom_equiv := λ X Y,
   (op_equiv _ Y).trans ((h.hom_equiv _ _).symm.trans (op_equiv X (opposite.op _)).symm) }
 
+def adjoint_unop (F : Cᵒᵖ ⥤ Dᵒᵖ) (G : D ⥤ C) (h : G ⊣ F.unop) : F ⊣ G.op :=
+(op_adjoint_of_adjoint F.unop _ h).of_nat_iso_left F.op_unop_iso
+
+def adjoint_unop' (F : C ⥤ D) (G : Dᵒᵖ ⥤ Cᵒᵖ) (h : G.unop ⊣ F) : F.op ⊣ G :=
+(op_adjoint_of_adjoint _ G.unop h).of_nat_iso_right G.op_unop_iso
+
+def adjoint_unop'' (F : Cᵒᵖ ⥤ Dᵒᵖ) (G : Dᵒᵖ ⥤ Cᵒᵖ) (h : G.unop ⊣ F.unop) : F ⊣ G :=
+(adjoint_unop _ _ h).of_nat_iso_right G.op_unop_iso
+
 end adjunction

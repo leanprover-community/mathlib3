8ad980f24a81f7f16e0c3b76f9dcf7ede0533ce0
Floris van Doorn <fpvdoorn@gmail.com>
2020-08-20 23:27:16 -0400

    undo part of fix

--- src/field_theory/chevalley_warning.lean
+++ src/field_theory/chevalley_warning.lean
@@ -99,7 +99,7 @@ theorem char_dvd_card_solutions_family (p : ℕ) [char_p K p]
   p ∣ fintype.card {x : σ → K // ∀ i ∈ s, eval x (f i) = 0} :=
 begin
   have hq : 0 < q - 1, { rw [← card_units, fintype.card_pos_iff], exact ⟨1⟩ },
-  let S : finset (σ → K) := { x ∈ univ | ∀ i ∈ s, mv_polynomial.eval x (f i) = 0 },
+  let S : finset (σ → K) := { x ∈ univ | ∀ i ∈ s, eval x (f i) = 0 },
   have hS : ∀ (x : σ → K), x ∈ S ↔ ∀ (i : ι), i ∈ s → eval x (f i) = 0,
   { intros x, simp only [S, true_and, sep_def, mem_filter, mem_univ], },
   /- The polynomial `F = ∏ i in s, (1 - (f i)^(q - 1))` has the nice property

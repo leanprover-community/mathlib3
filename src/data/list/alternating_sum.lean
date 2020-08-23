37f256199578cc5405742732c631e93a98c7a3d0
Chris Wong <lambda.fairy@gmail.com>
2020-08-23 15:13:41 +1000

    Add alternating_{prod,sum}_{append,reverse}

--- src/data/list/alternating_sum.lean
+++ src/data/list/alternating_sum.lean
@@ -3,7 +3,7 @@ Copyright (c) 2020 Scott Morrison. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Scott Morrison, Chris Wong
 -/
-import algebra.group.basic
+import algebra.group_power
 
 namespace list
 
@@ -47,5 +47,43 @@ attribute [simp] alternating_prod_cons
 @[simp]
 lemma alternating_sum_cons {G : Type*} [add_comm_group G] :
   ∀ (g : G) (l : list G), alternating_sum (g :: l) = g - alternating_sum l := alternating_sum_cons'
+
+lemma alternating_prod_append :
+  ∀ (l₁ l₂ : list G), alternating_prod (l₁ ++ l₂)
+    = alternating_prod l₁ * alternating_prod l₂ ^ (-1 : ℤ) ^ length l₁
+| [] l₂ := by simp
+| (g :: l₁) l₂ :=
+begin
+  have := alternating_prod_append l₁ l₂,
+  simp [-mul_inv_rev, mul_inv, mul_assoc, pow_succ, this]
+end
+
+lemma alternating_sum_append {G : Type*} [add_comm_group G] :
+  ∀ (l₁ l₂ : list G), alternating_sum (l₁ ++ l₂)
+    = alternating_sum l₁ + (-1) ^ length l₁ •ℤ alternating_sum l₂
+| [] l₂ := by simp
+| (g :: l₁) l₂ :=
+begin
+  have := alternating_sum_append l₁ l₂,
+  simp [pow_succ, sub_add, this]
+end
+
+lemma alternating_prod_reverse :
+  ∀ (l : list G), alternating_prod (reverse l) = alternating_prod l ^ (-1 : ℤ) ^ (length l + 1)
+| [] := by simp
+| (g :: l) :=
+begin
+  have := alternating_prod_reverse l,
+  simp [alternating_prod_append, inv_gpow, mul_gpow, pow_succ, this], ac_refl
+end
+
+lemma alternating_sum_reverse {G : Type*} [add_comm_group G] :
+  ∀ (l : list G), alternating_sum (reverse l) = (-1) ^ (length l + 1) •ℤ alternating_sum l
+| [] := by simp
+| (g :: l) :=
+begin
+  have := alternating_sum_reverse l,
+  simp [alternating_sum_append, gsmul_sub, neg_add_eq_sub, pow_succ, this]
+end
 
 end list

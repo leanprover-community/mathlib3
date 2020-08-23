a05f709c858790b647b08664381f9d10235f6611
Bhavik Mehta <bhavikmehta8@gmail.com>
2020-08-23 14:20:06 +0100

    unique image

--- src/category_theory/limits/shapes/images.lean
+++ src/category_theory/limits/shapes/images.lean
@@ -541,4 +541,28 @@ instance has_image_maps_of_has_strong_epi_images [has_strong_epi_images C] :
 
 end has_strong_epi_images
 
+variables [has_strong_epi_mono_factorisations.{v} C]
+variables {X Y : C} (f : X ⟶ Y)
+
+/--
+If `C` has strong epi mono factorisations, then the image is unique up to isomorphism, in that if
+`f` factors as a strong epi followed by a mono, this factorisation is essentially the image
+factorisation.
+-/
+@[simps]
+def unique_factorise (I' : C) (e : X ⟶ I') (m : I' ⟶ Y) (comm : e ≫ m = f) [strong_epi e] [mono m] :
+  I' ≅ image f :=
+{ hom := {strong_epi_mono_factorisation . I := I', m := m, e := e}.to_mono_is_image.lift _,
+  inv := image.lift {strong_epi_mono_factorisation . I := I', m := m, e := e}.to_mono_factorisation,
+  hom_inv_id' := by erw [← cancel_mono m, category.assoc, category.id_comp, image.lift_fac, is_image.lift_fac],
+  inv_hom_id' := by erw [← cancel_mono (image.ι f), category.id_comp, category.assoc, is_image.lift_fac, image.lift_fac] }
+
+lemma unique_factorise_hom_comp_image (I' : C) (e : X ⟶ I') (m : I' ⟶ Y) (comm : e ≫ m = f) [strong_epi e] [mono m] :
+  (unique_factorise f I' e m comm).hom ≫ image.ι f = m :=
+is_image.lift_fac _ _
+
+lemma unique_factorise_inv_comp_mono (I' : C) (e : X ⟶ I') (m : I' ⟶ Y) (comm : e ≫ m = f) [strong_epi e] [mono m] :
+  (unique_factorise f I' e m comm).inv ≫ m = image.ι f :=
+image.lift_fac _
+
 end category_theory.limits

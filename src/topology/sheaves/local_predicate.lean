10fc6773cf01a49bf094bb728acdbb99b206bc01
Scott Morrison <scott.morrison@gmail.com>
2020-08-24 09:45:11 +1000

    doc-string

--- src/topology/sheaves/local_predicate.lean
+++ src/topology/sheaves/local_predicate.lean
@@ -105,6 +105,10 @@ instance inhabited_local_predicate (T : Top.{v}) : inhabited (local_predicate _)
 
 variables {X T}
 
+/--
+Given a `P : prelocal_predicate`, we can always construct a `local_predicate`
+by asking that the condition from `P` holds locally near every point.
+-/
 def prelocal_predicate.sheafify {T : X → Type v} (P : prelocal_predicate T) : local_predicate T :=
 { pred := λ U f, ∀ x : U, ∃ (V : opens X) (m : x.1 ∈ V) (i : V ⟶ U), P.pred (λ x : V, f (i x : U)),
   res := λ V U i f w x,

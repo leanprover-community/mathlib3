8580d34cacbc3a4f6be7b479d15d68603b8bbbd0
Scott Morrison <scott.morrison@gmail.com>
2020-08-25 15:56:20 +1000

    cleanup

--- src/algebraic_geometry/Spec.lean
+++ src/algebraic_geometry/Spec.lean
@@ -8,16 +8,8 @@ namespace algebraic_geometry
 def Spec_as_LRS (R : CommRing) : LocallyRingedSpace :=
 { carrier := Top.of (prime_spectrum R),
   local_ring := λ x,
-  begin
-    -- TODO golf!
-    dsimp,
-    apply (local_ring.of_ring_equiv _).mp,
-    swap 4,
-    apply category_theory.iso.CommRing_iso_to_ring_equiv,
-    exact (stalk_iso R x).symm,
-    show local_ring (localization.at_prime _), -- Need simp lemmas for this!
-    apply_instance,
-  end,
+    (local_ring.of_ring_equiv (stalk_iso R x).symm.CommRing_iso_to_ring_equiv).mp
+      (by { dsimp, apply_instance, }),
   ..structure_sheaf R }
 
 end algebraic_geometry

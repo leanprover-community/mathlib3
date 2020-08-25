41c7de3d2a9407be8e47be21dc2b39af7da5b0f3
Scott Morrison <scott.morrison@gmail.com>
2020-08-25 13:10:00 +1000

    open_nhds_is_filtered

--- src/topology/category/Top/open_nhds.lean
+++ src/topology/category/Top/open_nhds.lean
@@ -4,6 +4,7 @@ Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Scott Morrison
 -/
 import topology.category.Top.opens
+import category_theory.filtered
 
 open category_theory
 open topological_space
@@ -19,16 +20,56 @@ def open_nhds (x : X) := { U : opens X // x ∈ U }
 
 namespace open_nhds
 
+instance (x : X) : partial_order (open_nhds x) :=
+{ le := λ U V, U.1 ≤ V.1,
+  le_refl := λ U, @le_refl _ _ U.1.1,
+  le_trans := λ U V W, @le_trans _ _ U.1.1 V.1.1 W.1.1,
+  le_antisymm := λ U V i j, subtype.eq (subtype.eq (@le_antisymm _ _ U.1.1 V.1.1 i j)), }
+
+instance (x : X) : lattice (open_nhds x) :=
+{ inf := λ U V, ⟨U.1 ⊓ V.1, ⟨U.2, V.2⟩⟩,
+  le_inf := λ U V W, @le_inf _ _ U.1.1 V.1.1 W.1.1,
+  inf_le_left := λ U V, @inf_le_left _ _ U.1.1 V.1.1,
+  inf_le_right := λ U V, @inf_le_right _ _ U.1.1 V.1.1,
+  sup := λ U V, ⟨U.1 ⊔ V.1, V.1.1.mem_union_left U.2⟩,
+  sup_le := λ U V W, @sup_le _ _ U.1.1 V.1.1 W.1.1,
+  le_sup_left := λ U V, @le_sup_left _ _ U.1.1 V.1.1,
+  le_sup_right := λ U V, @le_sup_right _ _ U.1.1 V.1.1,
+  ..open_nhds.partial_order x }
+
+instance (x : X) : order_top (open_nhds x) :=
+{ top := ⟨⊤, trivial⟩,
+  le_top := λ U, @le_top _ _ U.1.1,
+  ..open_nhds.partial_order x }
+
 instance open_nhds_category (x : X) : category.{u} (open_nhds x) := by {unfold open_nhds, apply_instance}
 
 instance opens_nhds_hom_has_coe_to_fun {x : X} {U V : open_nhds x} : has_coe_to_fun (U ⟶ V) :=
 { F := λ f, U.1 → V.1,
   coe := λ f x, ⟨x, (le_of_hom f) x.2⟩ }
 
+/--
+The inclusion `U ⊓ V ⟶ U` as a morphism in the category of open sets.
+-/
+def inf_le_left {x : X} (U V : open_nhds x) : U ⊓ V ⟶ U :=
+hom_of_le inf_le_left
+
+/--
+The inclusion `U ⊓ V ⟶ V` as a morphism in the category of open sets.
+-/
+def inf_le_right {x : X} (U V : open_nhds x) : U ⊓ V ⟶ V :=
+hom_of_le inf_le_right
+
 def inclusion (x : X) : open_nhds x ⥤ opens X :=
 full_subcategory_inclusion _
 
 @[simp] lemma inclusion_obj (x : X) (U) (p) : (inclusion x).obj ⟨U,p⟩ = U := rfl
+
+lemma open_nhds_is_filtered (x : X) : is_filtered (open_nhds x)ᵒᵖ :=
+{ nonempty := ⟨op ⊤⟩,
+  cocone_objs := λ U V, ⟨op (unop U ⊓ unop V),
+    (inf_le_left (unop U) (unop V)).op, (inf_le_right (unop U) (unop V)).op, trivial⟩ ,
+  cocone_maps := λ U V i j, begin end, }
 
 def map (x : X) : open_nhds (f x) ⥤ open_nhds x :=
 { obj := λ U, ⟨(opens.map f).obj U.1, by tidy⟩,

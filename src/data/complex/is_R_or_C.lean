f89c52e3b4ff9f6ed866971fe6eb22f4a2c62116
Frédéric Dupuis <dupuisf@iro.umontreal.ca>
2020-08-25 10:08:18 -0400

    char_zero: def -> lemma

--- src/data/complex/is_R_or_C.lean
+++ src/data/complex/is_R_or_C.lean
@@ -318,7 +318,7 @@ by rw [← of_real_rat_cast, of_real_im]
 
 Note: This is not registered as an instance to avoid having multiple instances on ℝ and ℂ.
 -/
-def char_zero_R_or_C : char_zero K :=
+lemma char_zero_R_or_C : char_zero K :=
 add_group.char_zero_of_inj_zero $ λ n h,
 by rwa [← of_real_nat_cast, of_real_eq_zero, nat.cast_eq_zero] at h
 

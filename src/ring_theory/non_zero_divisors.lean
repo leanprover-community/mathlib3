eae2f77ebaa84210251144e9d91362c2d49e8192
Devon Tuma <tumax040@umn.edu>
2020-08-28 01:23:52 -0500

    Generalize non_zero_divisors to monoid with zero

--- src/ring_theory/non_zero_divisors.lean
+++ src/ring_theory/non_zero_divisors.lean
@@ -1,24 +1,22 @@
 /-
-Copyright (c) 2018 Kenny Lau. All rights reserved.
+Copyright (c) 2020 Kenny Lau. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
-Authors: Kenny Lau
+Authors: Kenny Lau, Devon Tuma
 -/
 
 import group_theory.submonoid.basic
 
-variables {R A : Type*} [comm_ring R] [integral_domain A]
-
 section non_zero_divisors
 
 /-- The submonoid of non-zero-divisors of a `comm_ring` `R`. -/
-def non_zero_divisors (R : Type*) [comm_ring R] : submonoid R :=
+def non_zero_divisors (R : Type*) [monoid R] [has_zero R] : submonoid R :=
 { carrier := {x | ∀ z, z * x = 0 → z = 0},
   one_mem' := λ z hz, by rwa mul_one at hz,
   mul_mem' := λ x₁ x₂ hx₁ hx₂ z hz,
     have z * x₁ * x₂ = 0, by rwa mul_assoc,
     hx₁ z $ hx₂ (z * x₁) this }
 
-variables {R}
+variables {R A : Type*} [comm_ring R] [integral_domain A]
 
 lemma mul_mem_non_zero_divisors {a b : R} :
   a * b ∈ non_zero_divisors R ↔ a ∈ non_zero_divisors R ∧ b ∈ non_zero_divisors R :=
@@ -34,12 +32,10 @@ begin
     rw [mul_assoc, hx] },
 end
 
-lemma eq_zero_of_ne_zero_of_mul_eq_zero
-  {x y : A} (hnx : x ≠ 0) (hxy : y * x = 0) :
-  y = 0 := or.resolve_right (eq_zero_or_eq_zero_of_mul_eq_zero hxy) hnx
+lemma eq_zero_of_ne_zero_of_mul_eq_zero {x y : A} (hnx : x ≠ 0) (hxy : y * x = 0) : y = 0 :=
+or.resolve_right (eq_zero_or_eq_zero_of_mul_eq_zero hxy) hnx
 
-lemma mem_non_zero_divisors_iff_ne_zero {x : A} :
-  x ∈ non_zero_divisors A ↔ x ≠ 0 :=
+lemma mem_non_zero_divisors_iff_ne_zero {x : A} : x ∈ non_zero_divisors A ↔ x ≠ 0 :=
 ⟨λ hm hz, zero_ne_one (hm 1 $ by rw [hz, one_mul]).symm,
 λ hnx z, eq_zero_of_ne_zero_of_mul_eq_zero hnx⟩
 
@@ -52,9 +48,9 @@ lemma map_mem_non_zero_divisors {B : Type*} [integral_domain B] {g : A →+* B}
 λ z hz, eq_zero_of_ne_zero_of_mul_eq_zero
   (map_ne_zero_of_mem_non_zero_divisors hg) hz
 
-lemma le_non_zero_divisors_of_domain {K : Type*} [integral_domain K] {M : submonoid K}
-  (hM : ↑0 ∉ M) : M ≤ non_zero_divisors K :=
+lemma le_non_zero_divisors_of_domain {M : submonoid A}
+  (hM : ↑0 ∉ M) : M ≤ non_zero_divisors A :=
 λ x hx y hy, or.rec_on (eq_zero_or_eq_zero_of_mul_eq_zero hy)
-  (λ h, h) (λ h, absurd (h ▸ hx : (0 : K) ∈ M) hM)
+  (λ h, h) (λ h, absurd (h ▸ hx : (0 : A) ∈ M) hM)
 
 end non_zero_divisors
--- src/ring_theory/polynomial/scale_roots.lean
+++ src/ring_theory/polynomial/scale_roots.lean
@@ -1,7 +1,7 @@
 /-
 Copyright (c) 2020 Anne Baanen. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
-Authors: Anne Baanen
+Authors: Anne Baanen, Devon Tuma
 -/
 
 import ring_theory.polynomial.basic

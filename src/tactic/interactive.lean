a08fb2f74e1767bd6338504c4ee29a0fe1866a48
Floris van Doorn <fpvdoorn@gmail.com>
2020-08-28 10:25:09 +0000

    feat(tactic/congr): additions to the congr' tactic (#3936)
    This PR gives a way to apply `ext` after `congr'`.
    `congr' 3 with x y : 2` is a new notation for `congr' 3; ext x y : 2`.
    New tactic `rcongr` that recursively keeps applying `congr'` and `ext`.
    Move `congr'` and every tactic depending on it from `tactic/interactive` to a new file `tactic/congr`.
    The original `tactic.interactive.congr'` is now best called as `tactic.congr'`. 
    Other than the tactics `congr'` and `rcongr` no tactics have been (essentially) changed.

--- src/tactic/interactive.lean
+++ src/tactic/interactive.lean
@@ -154,36 +154,6 @@ add_tactic_doc
   decl_names := [`tactic.interactive.clear_],
   tags       := ["context management"] }
 
-meta def apply_iff_congr_core : tactic unit :=
-applyc ``iff_of_eq
-
-meta def congr_core' : tactic unit :=
-do tgt ← target,
-   apply_eq_congr_core tgt
-   <|> apply_heq_congr_core
-   <|> apply_iff_congr_core
-   <|> fail "congr tactic failed"
-
-/--
-Same as the `congr` tactic, but takes an optional argument which gives
-the depth of recursive applications. This is useful when `congr`
-is too aggressive in breaking down the goal. For example, given
-`⊢ f (g (x + y)) = f (g (y + x))`, `congr'` produces the goals `⊢ x = y`
-and `⊢ y = x`, while `congr' 2` produces the intended `⊢ x + y = y + x`.
-If, at any point, a subgoal matches a hypothesis then the subgoal will be closed. -/
-meta def congr' : parse (with_desc "n" small_nat)? → tactic unit
-| (some 0) := failed
-| o        := focus1 (assumption <|> (congr_core' >>
-  all_goals (reflexivity <|> `[apply proof_irrel_heq] <|>
-             `[apply proof_irrel] <|> try (congr' (nat.pred <$> o)))))
-
-add_tactic_doc
-{ name       := "congr'",
-  category   := doc_category.tactic,
-  decl_names := [`tactic.interactive.congr', `tactic.interactive.congr],
-  tags       := ["congruence"],
-  inherit_description_from := `tactic.interactive.congr' }
-
 /--
 Acts like `have`, but removes a hypothesis with the same name as
 this one. For example if the state is `h : p ⊢ goal` and `f : p → q`,
@@ -260,54 +230,6 @@ add_tactic_doc
   category   := doc_category.tactic,
   decl_names := [`tactic.interactive.generalize_hyp],
   tags       := ["context management"] }
-
-/--
-The `exact e` and `refine e` tactics require a term `e` whose type is
-definitionally equal to the goal. `convert e` is similar to `refine
-e`, but the type of `e` is not required to exactly match the
-goal. Instead, new goals are created for differences between the type
-of `e` and the goal. For example, in the proof state
-
-```lean
-n : ℕ,
-e : prime (2 * n + 1)
-⊢ prime (n + n + 1)
-```
-
-the tactic `convert e` will change the goal to
-
-```lean
-⊢ n + n = 2 * n
-```
-
-In this example, the new goal can be solved using `ring`.
-
-The syntax `convert ← e` will reverse the direction of the new goals
-(producing `⊢ 2 * n = n + n` in this example).
-
-Internally, `convert e` works by creating a new goal asserting that
-the goal equals the type of `e`, then simplifying it using
-`congr'`. The syntax `convert e using n` can be used to control the
-depth of matching (like `congr' n`). In the example, `convert e using
-1` would produce a new goal `⊢ n + n + 1 = 2 * n + 1`.
--/
-meta def convert (sym : parse (with_desc "←" (tk "<-")?)) (r : parse texpr)
-  (n : parse (tk "using" *> small_nat)?) : tactic unit :=
-do v ← mk_mvar,
-   if sym.is_some
-     then refine ``(eq.mp %%v %%r)
-     else refine ``(eq.mpr %%v %%r),
-   gs ← get_goals,
-   set_goals [v],
-   try (congr' n),
-   gs' ← get_goals,
-   set_goals $ gs' ++ gs
-
-add_tactic_doc
-{ name       := "convert",
-  category   := doc_category.tactic,
-  decl_names := [`tactic.interactive.convert],
-  tags       := ["congruence"] }
 
 meta def compact_decl_aux : list name → binder_info → expr → list expr →
   tactic (list (list name × binder_info × expr))
@@ -869,45 +791,6 @@ add_tactic_doc
   decl_names := [`tactic.interactive.change', `tactic.interactive.change],
   tags       := ["renaming"],
   inherit_description_from := `tactic.interactive.change' }
-
-meta def convert_to_core (r : pexpr) : tactic unit :=
-do tgt ← target,
-   h   ← to_expr ``(_ : %%tgt = %%r),
-   rewrite_target h,
-   swap
-
-/--
-`convert_to g using n` attempts to change the current goal to `g`, but unlike `change`,
-it will generate equality proof obligations using `congr' n` to resolve discrepancies.
-`convert_to g` defaults to using `congr' 1`.
-
-`ac_change` is `convert_to` followed by `ac_refl`. It is useful for rearranging/reassociating
-e.g. sums:
-```lean
-example (a b c d e f g N : ℕ) : (a + b) + (c + d) + (e + f) + g ≤ N :=
-begin
-  ac_change a + d + e + f + c + g + b ≤ _,
--- ⊢ a + d + e + f + c + g + b ≤ N
-end
-```
--/
-meta def convert_to (r : parse texpr) (n : parse (tk "using" *> small_nat)?) : tactic unit :=
-match n with
-  | none     := convert_to_core r >> `[congr' 1]
-  | (some 0) := convert_to_core r
-  | (some o) := convert_to_core r >> congr' o
-end
-
-/-- `ac_change g using n` is `convert_to g using n; try {ac_refl}`. -/
-meta def ac_change (r : parse texpr) (n : parse (tk "using" *> small_nat)?) : tactic unit :=
-convert_to r n; try ac_refl
-
-add_tactic_doc
-{ name       := "convert_to",
-  category   := doc_category.tactic,
-  decl_names := [`tactic.interactive.convert_to, `tactic.interactive.ac_change],
-  tags       := ["congruence"],
-  inherit_description_from := `tactic.interactive.convert_to }
 
 private meta def opt_dir_with : parser (option (bool × name)) :=
 (do tk "with",

2c321b436cc1a55398f4c3deeba9b57a6329a71e
tb65536 <tb65536@users.noreply.github.com>
2020-08-29 11:35:24 -0700

    Update src/field_theory/adjoin.lean
    Co-authored-by: Bryan Gin-ge Chen <bryangingechen@gmail.com>

--- src/field_theory/adjoin.lean
+++ src/field_theory/adjoin.lean
@@ -135,7 +135,7 @@ begin
   exact subset_adjoin F {α} (set.mem_singleton α),
 end
 
-/-- generator of F[(α)] -/
+/-- generator of `F[(α)]` -/
 def adjoin_simple.gen : F[(α)] := ⟨α, mem_adjoin_simple_self F α⟩
 
 lemma adjoin_simple.algebra_map_gen : algebra_map F[(α)] E (adjoin_simple.gen F α) = α := rfl

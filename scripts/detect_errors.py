0fa28735269cb52db980756492d67de4699d90c5
Bryan Gin-ge Chen <bryangingechen@gmail.com>
2020-08-30 17:13:40 -0400

    change detect_errors.py to exit at end of build if it was not silent

--- scripts/detect_errors.py
+++ scripts/detect_errors.py
@@ -1,9 +1,16 @@
 import itertools
 import sys
 
+was_silent = True
 for line in sys.stdin:
     sys.stdout.write(line)
     if 'error' in line:
         for line in itertools.islice(sys.stdin, 20):
             sys.stdout.write(line)
         sys.exit(1)
+    elif not line.startswith('configuring mathlib') and not line.startswith('> lean --make'):
+        print(line)
+        was_silent = False
+
+if not was_silent:
+    sys.exit(1)

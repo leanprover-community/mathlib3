39b11f1ce2cdd9f9ff2296e582d20f5b534aeea2
Scott Morrison <scott.morrison@gmail.com>
2020-08-30 10:48:51 +1000

    fix import

--- src/algebra/category/Group/limits.lean
+++ src/algebra/category/Group/limits.lean
@@ -8,6 +8,7 @@ import algebra.category.Group.preadditive
 import category_theory.over
 import category_theory.limits.types
 import category_theory.limits.preserves.basic
+import category_theory.limits.concrete_category
 import category_theory.limits.shapes.concrete_category
 import algebra.group.pi
 

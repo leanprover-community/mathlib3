4f86d76f1a6c924b1e68e829be04f20fdd6694c6
Rob Lewis <rob.y.lewis@gmail.com>
2020-09-01 13:59:09 +0200

    remove more changes

--- src/data/mv_polynomial.lean
+++ src/data/mv_polynomial.lean
@@ -1823,20 +1823,6 @@ by rw [pderivative_mul, pderivative_C, zero_mul, zero_add]
 
 end pderivative
 
-section invertible
-
-noncomputable instance invertible_C
-  (σ : Type*) {R : Type*} [comm_semiring R] (r : R) [invertible r] :
-  invertible (C r : mv_polynomial σ R) :=
-invertible.map ⟨C, C_1, λ x y, C_mul⟩ _
-
--- name??
-noncomputable def invertible_rat_coe_nat (σ : Type*) (p : ℕ) [invertible (p : ℚ)] :
-  invertible (p : mv_polynomial σ ℚ) :=
-(mv_polynomial.invertible_C σ (p:ℚ)).copy p $ (C_eq_coe_nat p).symm
-
-end invertible
-
 section counit
 
 variables [comm_semiring α] [comm_semiring β] [algebra α β]

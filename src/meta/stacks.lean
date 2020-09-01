6254b2c314c6a25623614e9c28a2de9fb7cee986
Scott Morrison <scott.morrison@gmail.com>
2020-09-01 19:09:45 +1000

    oops

---
+++ src/meta/stacks.lean
@@ -0,0 +1,29 @@
+import tactic.core
+
+namespace stacks
+
+@[derive has_reflect, derive inhabited]
+structure stacks_tag : Type := (tag : string) (comment : option string)
+
+open tactic
+
+/-- the parser for the arguments to `@[stacks]` -/
+meta def parser : lean.parser stacks_tag :=
+do
+  tag_e ← interactive.types.texpr,
+  tag ← ((to_expr tag_e >>= eval_expr string) : tactic string),
+  -- FIXME this doesn't actually work: Lean says "function expected" when you provide two strings.
+  doc_e ← optional interactive.types.texpr,
+  doc ← match doc_e with
+      | some e := some <$> ((to_expr e >>= eval_expr string) : tactic string)
+      | none := pure none
+      end,
+  return ⟨tag, doc⟩
+
+@[user_attribute]
+meta def stacks_attribute : user_attribute unit stacks_tag :=
+{ name := `stacks,
+  descr := "A declaration corresponding to a tag from the Stacks project.",
+  parser := parser }
+
+end stacks

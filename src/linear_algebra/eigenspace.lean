af0b3fac7b74189a87f6bfb25a29fab4a7a0dc8c
Alexander Bentkamp <a.bentkamp@vu.nl>
2020-09-03 21:01:15 +0200

    feat(linear_algebra): Add lemmas about powers of endomorphisms
    Add lemmas about powers of endomorphisms and the corollary that every generalized eigenvector is a generalized eigenvector for exponent `findim K V`.

--- src/linear_algebra/eigenspace.lean
+++ src/linear_algebra/eigenspace.lean
@@ -311,5 +311,12 @@ begin
   rwa [has_generalized_eigenvalue, generalized_eigenspace, pow_one]
 end
 
+/-- Every generalized eigenvector is a generalized eigenvector for exponent `findim K V`. -/
+lemma generalized_eigenspace_le_generalized_eigenspace_findim
+  [field K] [vector_space K V] [finite_dimensional K V]
+  (f : End K V) (μ : K) (k : ℕ) :
+  f.generalized_eigenspace μ k ≤ f.generalized_eigenspace μ (findim K V) :=
+ker_pow_le_ker_pow_findim _ _
+
 end End
 end module

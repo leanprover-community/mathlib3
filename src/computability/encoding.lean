cf222d2b898c2cd3e10e1fd1f734fa6c839547a2
Pim <spelier.pim@gmail.com>
2020-09-05 09:49:18 +0200

    Changed encodek to decode_encode and removed superfluous parentheses

--- src/computability/encoding.lean
+++ src/computability/encoding.lean
@@ -30,7 +30,7 @@ structure encoding (α : Type) :=
 (Γ : Type)
 (encode : α → list Γ)
 (decode : list Γ → option α)
-(encodek : ∀ x, decode (encode x) = some x)
+(decode_encode : ∀ x, decode (encode x) = some x)
 
 /-- An encoding plus a guarantee of finiteness of the alphabet. -/
 structure fin_encoding (α : Type) extends encoding α :=
@@ -94,7 +94,7 @@ def decode_nat : list Γ₀₁ → nat := λ l, decode_num l
 lemma encode_pos_num_nonempty (n : pos_num) : (encode_pos_num n) ≠ [] :=
 pos_num.cases_on n (list.cons_ne_nil _ _) (λ m, list.cons_ne_nil _ _) (λ m, list.cons_ne_nil _ _)
 
-lemma encodek_pos_num : ∀ n, (decode_pos_num(encode_pos_num n) ) = n :=
+lemma decode_encode_pos_num : ∀ n, decode_pos_num(encode_pos_num n) = n :=
 begin
   intros n,
   induction n with m hm m hm; unfold encode_pos_num decode_pos_num,
@@ -104,21 +104,21 @@ begin
   { exact congr_arg pos_num.bit0 hm }
 end
 
-lemma encodek_num : ∀ n, (decode_num(encode_num n) ) = n :=
+lemma decode_encode_num : ∀ n, decode_num(encode_num n) = n :=
 begin
   intros n,
   cases n; unfold encode_num decode_num,
   { refl },
-  rw encodek_pos_num n,
+  rw decode_encode_pos_num n,
   rw pos_num.cast_to_num,
   exact if_neg (encode_pos_num_nonempty n),
 end
 
-lemma encodek_nat : ∀ n, (decode_nat(encode_nat n) ) = n :=
+lemma decode_encode_nat : ∀ n, decode_nat(encode_nat n) = n :=
 begin
   intro n,
   conv_rhs {rw ← num.to_of_nat n},
-  exact congr_arg coe (encodek_num ↑n),
+  exact congr_arg coe (decode_encode_num ↑n),
 end
 
 /-- A binary encoding of ℕ in Γ₀₁. -/
@@ -126,7 +126,7 @@ def encoding_nat_Γ₀₁ : encoding ℕ :=
 { Γ := Γ₀₁,
   encode := encode_nat,
   decode := λ n, some (decode_nat n),
-  encodek := λ n, congr_arg _ (encodek_nat n) }
+  decode_encode := λ n, congr_arg _ (decode_encode_nat n) }
 
 /-- A binary fin_encoding of ℕ in Γ₀₁. -/
 def fin_encoding_nat_Γ₀₁ : fin_encoding ℕ := ⟨encoding_nat_Γ₀₁, Γ₀₁.fintype⟩
@@ -135,9 +135,9 @@ def fin_encoding_nat_Γ₀₁ : fin_encoding ℕ := ⟨encoding_nat_Γ₀₁, Γ
 def encoding_nat_Γ' : encoding ℕ :=
 { Γ := Γ',
   encode := λ x, list.map inclusion_Γ₀₁_Γ' (encode_nat x),
-  decode := λ x, option.some (decode_nat (list.map section_Γ'_Γ₀₁ x)),
-  encodek := λ x, congr_arg _ $
-    by rw [list.map_map, list.map_id' left_inverse_section_inclusion, encodek_nat] }
+  decode := λ x, some (decode_nat (list.map section_Γ'_Γ₀₁ x)),
+  decode_encode := λ x, congr_arg _ $
+    by rw [list.map_map, list.map_id' left_inverse_section_inclusion, decode_encode_nat] }
 
 /-- A binary fin_encoding of ℕ in Γ'. -/
 def fin_encoding_nat_Γ' : fin_encoding ℕ := ⟨encoding_nat_Γ', Γ'.fintype⟩
@@ -150,15 +150,15 @@ def unary_encode_nat : nat → list Γ₀₁
 /-- A unary decoding function from `list Γ₀₁` to ℕ. -/
 def unary_decode_nat : list Γ₀₁ → nat := list.length
 
-lemma unary_encodek_nat : ∀ n, unary_decode_nat (unary_encode_nat n) = n :=
-λ n, nat.rec rfl (λ (m : ℕ) (hm : unary_decode_nat (unary_encode_nat m) = m), (congr_arg nat.succ hm.symm).symm) n
+lemma unary_decode_encode_nat : ∀ n, unary_decode_nat (unary_encode_nat n) = n :=
+λ n, nat.rec rfl (λ (m : ℕ) hm, (congr_arg nat.succ hm.symm).symm) n
 
 /-- A unary fin_encoding of ℕ. -/
 def unary_fin_encoding_nat : fin_encoding ℕ :=
 { Γ := Γ₀₁,
   encode := unary_encode_nat,
   decode := λ n, some (unary_decode_nat n),
-  encodek := λ n, congr_arg _ (unary_encodek_nat n),
+  decode_encode := λ n, congr_arg _ (unary_decode_encode_nat n),
   Γ_fin := Γ₀₁.fintype}
 
 /-- An encoding function of bool in Γ₀₁. -/
@@ -172,14 +172,14 @@ def decode_bool : list Γ₀₁ → bool
 | [Γ₀₁.bit1] := tt
 | _ := arbitrary bool
 
-lemma encodek_bool : ∀ b, (decode_bool(encode_bool b)) = b := λ b, bool.cases_on b rfl rfl
+lemma decode_encode_bool : ∀ b, decode_bool(encode_bool b) = b := λ b, bool.cases_on b rfl rfl
 
 /-- A fin_encoding of bool in Γ₀₁. -/
 def fin_encoding_bool_Γ₀₁ : fin_encoding bool :=
 { Γ := Γ₀₁,
   encode := encode_bool,
-  decode := λ x, some(decode_bool x),
-  encodek := λ x, congr_arg _ (encodek_bool x),
+  decode := λ x, some (decode_bool x),
+  decode_encode := λ x, congr_arg _ (decode_encode_bool x),
   Γ_fin := Γ₀₁.fintype }
 
 instance inhabited_fin_encoding : inhabited (fin_encoding bool) := ⟨fin_encoding_bool_Γ₀₁⟩

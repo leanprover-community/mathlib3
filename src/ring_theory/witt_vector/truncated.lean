f2f6cb623ab3d4b8e013e93a27fe05e0e63ee56d
Johan Commelin <johan@commelin.net>
2020-09-07 13:31:46 +0000

    truncate_lift_fun

--- src/ring_theory/witt_vector/truncated.lean
+++ src/ring_theory/witt_vector/truncated.lean
@@ -43,20 +43,6 @@ def mk (x : fin n → R) : truncated_witt_vector p n R :=
 witt_vector.truncate p n $ witt_vector.mk p $
 λ i, if h : i < n then x ⟨i, h⟩ else 0
 
-section
-local attribute [semireducible] witt_vector
-
-lemma truncate_mk {m : ℕ} (f : ℕ → R) :
-  witt_vector.truncate p n (witt_vector.mk p f) = mk _ (λ k, f k) :=
-begin
-  simp only [witt_vector.truncate, mk, ideal.quotient.eq,
-    witt_vector.mem_ideal_iff, fin.coe_mk],
-  intros j hj,
-  sorry
-end
-
-end
-
 variable {p}
 
 def coeff (i : fin n) : truncated_witt_vector p n R → R :=
@@ -74,7 +60,101 @@ begin
   sorry
 end
 
-variables (p R)
+section mk_and_coeff
+
+variables (p)
+
+@[simp]
+lemma mk_coeff (x : truncated_witt_vector p n R) :
+  mk p (λ (i : fin n), coeff i x) = x :=
+begin
+  apply quot.induction_on x,
+  intros x',
+  show mk p (λ i : fin n, witt_vector.coeff i x') = _,
+  apply quot.sound,
+  show _ ∈ (witt_vector.ideal p R n),
+  sorry
+  -- intros i hi,
+end
+
+@[simp]
+lemma coeff_mk (i : fin n) (x : fin n → R) :
+  coeff i (mk p x) = x i :=
+begin
+  dsimp [coeff, mk],
+  convert quot.lift_beta _ (coeff._proof_1 _) _,
+  { dsimp, rw [dif_pos i.is_lt, fin.eta], },
+  { apply_instance, }
+end
+
+@[simp] lemma coeff_zero (i : fin n) : coeff i (0 : truncated_witt_vector p n R) = 0 :=
+sorry
+
+section
+-- move this
+local attribute [semireducible] witt_vector
+@[simp] lemma witt_vector.mk_zero : witt_vector.mk p (λ _, (0 : R)) = 0 :=
+by ext; simp [witt_vector.mk]; refl
+end
+
+variables (p n R)
+@[simp] lemma mk_zero : mk p (0 : fin n → R) = 0 :=
+begin
+  -- not sure if we need this
+  have : ∀ i n, dite (i < n) (λ (h : i < n), (0 : fin n → R) ⟨i, h⟩) (λ (h : ¬i < n), 0) = 0,
+  { intros, split_ifs; simp only [eq_self_iff_true, pi.zero_apply] },
+  simp only [mk, this, witt_vector.mk_zero, ring_hom.map_zero],
+end
+
+def equiv : truncated_witt_vector p n R ≃ (fin n → R) :=
+{ to_fun := λ x i, x.coeff i,
+  inv_fun := mk p,
+  left_inv := by { intros x, apply mk_coeff },
+  right_inv := by { intros x, ext i, apply coeff_mk } }
+
+variables {p n R}
+
+@[ext] lemma ext (x y : truncated_witt_vector p n R) (h : ∀ i : fin n, x.coeff i = y.coeff i) :
+  x = y :=
+begin
+  apply (equiv p n R).injective,
+  ext i,
+  exact h i
+end
+
+@[ext] lemma ext_iff (x y : truncated_witt_vector p n R) :
+  x = y ↔ (∀ i : fin n, x.coeff i = y.coeff i) :=
+⟨λ h i, congr_arg _ h, ext x y⟩
+
+end mk_and_coeff
+
+#where
+end truncated_witt_vector
+
+namespace witt_vector
+local attribute [semireducible] witt_vector
+
+variables {p n : ℕ} {R : Type*} [fact (nat.prime p)] [comm_ring R]
+
+@[simp]
+lemma coeff_truncate (x : witt_vector p R) (i : fin n) :
+  (truncate p n x).coeff i = x.coeff i := rfl
+
+@[simp]
+lemma truncate_mk (f : ℕ → R) :
+  truncate p n (mk p f) = truncated_witt_vector.mk _ (λ k, f k) :=
+begin
+  ext i,
+  rw [coeff_truncate, coeff_mk, truncated_witt_vector.coeff_mk],
+end
+
+end witt_vector
+
+namespace truncated_witt_vector
+
+variables (p : ℕ) (R : Type*) [fact (nat.prime p)] [comm_ring R] {n : ℕ}
+local notation `𝕎` := witt_vector p -- type as `\bbW`
+
 def truncate {m : ℕ} (hm : n ≤ m) : truncated_witt_vector p m R →+* truncated_witt_vector p n R :=
 ideal.quotient.lift _ (witt_vector.truncate p n)
 begin
@@ -106,57 +186,11 @@ lemma truncate_witt_vector_truncate {m : ℕ} (hm : n ≤ m) (x) :
   truncate p R hm (witt_vector.truncate p _ x) = witt_vector.truncate p _ x :=
 by rw ← truncate_comp_witt_vector_truncate p R hm; refl
 
-section mk_and_coeff
-
-variables (p)
-
-lemma mk_coeff (x : truncated_witt_vector p n R) :
-  mk p (λ (i : fin n), coeff i x) = x :=
+@[simp] lemma coeff_truncate {m : ℕ} (hm : n ≤ m) (i : fin n) (x : truncated_witt_vector p m R) :
+  (truncate p R hm x).coeff i = x.coeff (fin.cast_le hm i) :=
 begin
-  apply quot.induction_on x,
-  intros x',
-  show mk p (λ i : fin n, witt_vector.coeff i x') = _,
-  apply quot.sound,
-  show _ ∈ (witt_vector.ideal p R n),
   sorry
-  -- intros i hi,
 end
-
-lemma coeff_mk (i : fin n) (x : fin n → R) :
-  coeff i (mk p x) = x i :=
-begin
-  dsimp [coeff, mk],
-  convert quot.lift_beta _ (coeff._proof_1 _) _,
-  { dsimp, rw [dif_pos i.is_lt, fin.eta], },
-  { apply_instance, }
-end
-
-@[simp] lemma coeff_zero (i : fin n) : coeff i (0 : truncated_witt_vector p n R) = 0 :=
-sorry
-
-section
-local attribute [semireducible] witt_vector
-@[simp] lemma witt_vector.mk_zero : witt_vector.mk p (λ _, (0 : R)) = 0 :=
-by ext; simp [witt_vector.mk]; refl
-end
-
-variables (p n R)
-@[simp] lemma mk_zero : mk p (0 : fin n → R) = 0 :=
-begin
-  -- not sure if we need this
-  have : ∀ i n, dite (i < n) (λ (h : i < n), (0 : fin n → R) ⟨i, h⟩) (λ (h : ¬i < n), 0) = 0,
-  { intros, split_ifs; simp only [eq_self_iff_true, pi.zero_apply] },
-  simp only [mk, this, witt_vector.mk_zero, ring_hom.map_zero],
-end
-
-def equiv : truncated_witt_vector p n R ≃ (fin n → R) :=
-{ to_fun := λ x i, x.coeff i,
-  inv_fun := mk p,
-  left_inv := by { intros x, apply mk_coeff },
-  right_inv := by { intros x, ext i, apply coeff_mk } }
-
-
-end mk_and_coeff
 
 section fintype
 
@@ -180,8 +214,7 @@ begin
   exact hx _ _ (nat.lt_succ_self _)
 end
 
-#check submodule.mem_bot
-
+-- move this
 lemma ideal.mem_bot {x : R} : x ∈ (⊥ : ideal R) ↔ x = 0 :=
 submodule.mem_bot _
 
@@ -193,8 +226,23 @@ variables {S : Type*} [comm_ring S]
 variable (f : Π k : ℕ, S →+* truncated_witt_vector p k R)
 variable f_compat : ∀ (k₁ k₂ : ℕ) (hk : k₁ ≤ k₂), (truncate p R hk).comp (f k₂) = f k₁
 variables {p R}
+
 def lift_fun (s : S) : 𝕎 R :=
 witt_vector.mk p $ λ k, coeff (fin.last k) (f (k+1) s)
+
+include f_compat
+
+@[simp]
+private lemma truncate_lift_fun (s : S) :
+  witt_vector.truncate p n (lift_fun p f s) = f n s :=
+begin
+  ext i,
+  simp only [lift_fun, coeff_mk, witt_vector.truncate_mk],
+  rw [← f_compat (i+1) n i.is_lt, ring_hom.comp_apply, coeff_truncate],
+  -- this is a bit unfortunate
+  congr, ext,
+  simp only [fin.coe_last, fin.coe_cast_le],
+end
 
 lemma lift_fun_zero : lift_fun p f 0 = 0 :=
 by simp [lift_fun]
@@ -217,7 +265,7 @@ begin
   intro i,
   rw [← ideal.quotient.eq, ring_hom.map_add],
   show witt_vector.truncate _ _ _ = witt_vector.truncate _ _ _ + witt_vector.truncate _ _ _,
-  simp [lift_fun],
+  squeeze_simp [truncate_lift_fun, f_compat],
 end
 
 

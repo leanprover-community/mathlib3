7651cf2df9681abed00fa773057f30fabd955d30
Rob Lewis <rob.y.lewis@gmail.com>
2020-09-08 17:52:50 +0200

    update authorship

--- src/data/mv_polynomial/rename.lean
+++ src/data/mv_polynomial/rename.lean
@@ -1,7 +1,7 @@
 /-
 Copyright (c) 2017 Johannes Hölzl. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
-Authors: Johannes Hölzl, Johan Commelin, Mario Carneiro, Shing Tak Lam
+Authors: Johannes Hölzl, Johan Commelin, Mario Carneiro
 -/
 
 import data.mv_polynomial.variables

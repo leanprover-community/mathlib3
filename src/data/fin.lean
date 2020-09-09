a526a8823b6274dc33490d7c33a748c9cbaf134d
Yakov Pechersky <ffxen158@gmail.com>
2020-09-09 17:35:40 -0400

    feat(data/fin): iffs about succ_above ordering
    New lemmas:
    `succ_above_lt_iff`
    `succ_above_gt_iff`
    
    These help avoid needing to do case analysis when faced with
    inequalities about `succ_above`.

--- src/data/fin.lean
+++ src/data/fin.lean
@@ -488,6 +488,36 @@ lemma succ_above_lt_gt (p : fin (n + 1)) (i : fin n) : i.cast_succ < p ∨ p < i
 or.cases_on (succ_above_lt_ge p i)
   (λ h, or.inl h) (λ h, or.inr (lt_of_le_of_lt h (cast_succ_lt_succ i)))
 
+/-- Embedding `i : fin n` into `fin (n + 1)` using a pivot `p` that is greater
+results in a value that is less than `p`. -/
+lemma succ_above_lt_iff (p : fin (n + 1)) (i : fin n) : i.cast_succ < p ↔ p.succ_above i < p :=
+begin
+  refine iff.intro _ _,
+  { intro h,
+    rw succ_above_below _ _ h,
+    exact h },
+  { intro h,
+    cases succ_above_lt_ge p i with H H,
+    { exact H },
+    { rw succ_above_above _ _ H at h,
+      exact lt_trans (cast_succ_lt_succ i) h } }
+end
+
+/-- Embedding `i : fin n` into `fin (n + 1)` using a pivot `p` that is lesser
+results in a value that is greater than `p`. -/
+lemma succ_above_gt_iff (p : fin (n + 1)) (i : fin n) : p ≤ i.cast_succ ↔ p < p.succ_above i :=
+begin
+  refine iff.intro _ _,
+  { intro h,
+    rw succ_above_above _ _ h,
+    exact lt_of_le_of_lt h (cast_succ_lt_succ i) },
+  { intro h,
+    cases succ_above_lt_ge p i with H H,
+    { rw succ_above_below _ _ H at h,
+      exact le_of_lt h },
+    { exact H } }
+end
+
 /-- Embedding `i : fin n` into `fin (n + 1)` with a hole around `p : fin (n + 1)`
 never results in `p` itself -/
 theorem succ_above_ne (p : fin (n + 1)) (i : fin n) : p.succ_above i ≠ p :=

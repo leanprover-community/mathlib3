367db79d3dba6a5e9d2f72e13f28d3573657728e
Rob Lewis <rob.y.lewis@gmail.com>
2020-09-09 17:03:47 +0200

    factor out comap stuff

--- src/ring_theory/witt_vector/witt_vector_preps.lean
+++ src/ring_theory/witt_vector/witt_vector_preps.lean
@@ -84,82 +84,9 @@ end
 end mv_polynomial
 
 namespace mv_polynomial
-variables {σ : Type*} {τ : Type*} {υ : Type*} {R S : Type*} [comm_semiring R] [comm_semiring S]
-variables (f : R →+* S)
-variables (p q : mv_polynomial σ R)
-
-open function
-open_locale classical big_operators
-
-section vars
-
-end vars
-
-end mv_polynomial
-
-
-namespace mv_polynomial
 variables {σ : Type*} {τ : Type*} {υ : Type*} {R : Type*} [comm_semiring R]
 
-/-- This is an example of a map of “algebraic varieties for dummies” over `R`.
-(Not meant in a degrading way. Just that we don'y have any actual varieties in Lean yet.) -/
-noncomputable def comap (f : mv_polynomial σ R →ₐ[R] mv_polynomial τ R) :
-  (τ → R) → (σ → R) :=
-λ x i, aeval x (f (X i))
 
-@[simp] lemma comap_apply (f : mv_polynomial σ R →ₐ[R] mv_polynomial τ R) (x : τ → R) (i : σ) :
-  comap f x i = aeval x (f (X i)) := rfl
-
-@[simp] lemma comap_id_apply (x : σ → R) : comap (alg_hom.id R (mv_polynomial σ R)) x = x :=
-by { funext i, simp only [comap, alg_hom.id_apply, id.def, aeval_X], }
-
-variables (σ R)
-
-lemma comap_id : comap (alg_hom.id R (mv_polynomial σ R)) = id :=
-by { funext x, exact comap_id_apply x }
-
-variables {σ R}
-
-lemma comap_comp_apply (f : mv_polynomial σ R →ₐ[R] mv_polynomial τ R)
-  (g : mv_polynomial τ R →ₐ[R] mv_polynomial υ R) (x : υ → R) :
-  comap (g.comp f) x = (comap f) (comap g x) :=
-begin
-  funext i,
-  transitivity (aeval x (aeval (λ i, g (X i)) (f (X i)))),
-  { apply eval₂_hom_congr rfl rfl,
-    rw alg_hom.comp_apply,
-    suffices : g = aeval (λ i, g (X i)), { rw ← this, },
-    apply mv_polynomial.alg_hom_ext g,
-    intro, rw [aeval_X], },
-  { simp only [comap, aeval_eq_eval₂_hom, map_eval₂_hom, alg_hom.comp_apply],
-    refine eval₂_hom_congr _ rfl rfl,
-    ext r, apply aeval_C },
-end
-
-lemma comap_comp (f : mv_polynomial σ R →ₐ[R] mv_polynomial τ R)
-  (g : mv_polynomial τ R →ₐ[R] mv_polynomial υ R) (x : υ → R) :
-  comap (g.comp f) = (comap f) ∘ (comap g) :=
-by { funext x, exact comap_comp_apply _ _ _ }
-
-lemma comap_eq_id_of_eq_id (f : mv_polynomial σ R →ₐ[R] mv_polynomial σ R)
-  (hf : ∀ φ, f φ = φ) (x : σ → R) :
-  comap f x = x :=
-by { convert comap_id_apply x, ext1 φ, rw [hf, alg_hom.id_apply] }
-
-noncomputable def comap_equiv (f : mv_polynomial σ R ≃ₐ[R] mv_polynomial τ R) :
-  (τ → R) ≃ (σ → R) :=
-{ to_fun    := comap f,
-  inv_fun   := comap f.symm,
-  left_inv  := by { intro x, rw [← comap_comp_apply], apply comap_eq_id_of_eq_id, intro,
-    simp only [alg_hom.id_apply, alg_equiv.comp_symm], },
-  right_inv := by { intro x, rw [← comap_comp_apply], apply comap_eq_id_of_eq_id, intro,
-  simp only [alg_hom.id_apply, alg_equiv.symm_comp] }, }
-
-@[simp] lemma comap_equiv_coe (f : mv_polynomial σ R ≃ₐ[R] mv_polynomial τ R) :
-  (comap_equiv f : (τ → R) → (σ → R)) = comap f := rfl
-
-@[simp] lemma comap_equiv_symm_coe (f : mv_polynomial σ R ≃ₐ[R] mv_polynomial τ R) :
-  ((comap_equiv f).symm : (σ → R) → (τ → R)) = comap f.symm := rfl
 
 lemma equiv_of_family_aux (f : σ → mv_polynomial τ R) (g : τ → mv_polynomial σ R)
   (h : ∀ i, aeval g (f i) = X i) (φ : mv_polynomial σ R) :

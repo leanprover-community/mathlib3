e03a8518bbbc5708f0ed83ea4ec786b386f1beae
kckennylau <kc_kennylau@yahoo.com.hk>
2020-09-11 14:44:53 +0800

    update project

--- src/algebraic_geometry/Scheme.lean
+++ src/algebraic_geometry/Scheme.lean
@@ -37,20 +37,16 @@ structure Scheme extends X : LocallyRingedSpace :=
     Spec.PresheafedSpace R), true)
 
 -- PROJECT
--- In fact, we can construct `Spec.LocallyRingedSpace R`,
--- and the isomorphism `i` above is an isomorphism in `LocallyRingedSpace`.
+-- In fact, we can make the isomorphism `i` above an isomorphism in `LocallyRingedSpace`.
 -- However this is a consequence of the above definition, and not necessary for defining schemes.
--- We haven't done this yet because:
--- 1. We haven't proved that the stalk of the structure sheaf is isomorphic to the localisation
---    **as a ring**, only at the level of `Type`.
---    To do this, we need to know that `forget CommRing` preserves filtered colimits.
--- 2. We haven't shown that you can restrict a `LocallyRingedSpace` along an open embedding.
---    We can do this already for `SheafedSpace` (as above), but we need to know that
---    the stalks of the restriction are still local rings, which we follow if we knew that
---    the stalks didn't change.
---    This will follow if we define cofinal functors, and show precomposing with a cofinal functor
---    doesn't change colimits, because open neighbourhoods of `x` within `U` are cofinal in
---    all open neighbourhoods of `x`.
+-- We haven't done this yet because we haven't shown that you can restrict a `LocallyRingedSpace`
+-- along an open embedding.
+-- We can do this already for `SheafedSpace` (as above), but we need to know that
+-- the stalks of the restriction are still local rings, which we follow if we knew that
+-- the stalks didn't change.
+-- This will follow if we define cofinal functors, and show precomposing with a cofinal functor
+-- doesn't change colimits, because open neighbourhoods of `x` within `U` are cofinal in
+-- all open neighbourhoods of `x`.
 
 namespace Scheme
 

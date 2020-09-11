78f9b3b5155f029c3b2f302939bf01d4f38bc7d5
Alexander Bentkamp <a.bentkamp@vu.nl>
2020-09-11 15:40:16 +0200

    axler ref

--- src/linear_algebra/eigenspace.lean
+++ src/linear_algebra/eigenspace.lean
@@ -34,8 +34,7 @@ notation for it.
 
 ## References
 
-* [Sheldon Axler, *Linear Algebra Done Right*,
-  https://zhangyk8.github.io/teaching/file_spring2018/linear_algebra_done_right.pdf][axler2015]
+* Sheldon Axler, *Linear Algebra Done Right* [axler2015]
 * https://en.wikipedia.org/wiki/Eigenvalues_and_eigenvectors
 
 ## Tags

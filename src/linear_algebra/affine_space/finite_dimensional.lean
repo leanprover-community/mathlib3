685768b203be7209f259e38c9da8670f13500c83
Joseph Myers <jsm@polyomino.org.uk>
2020-09-12 23:29:16 +0000

    feat(linear_algebra/affine_space): more lemmas
    Add another batch of lemmas relating to affine spaces.  These include
    factoring out `vector_span_mono` as a combination of two other lemmas
    that's used several times, and additional variants of lemmas relating
    to finite-dimensional subspaces.

--- src/linear_algebra/affine_space/finite_dimensional.lean
+++ src/linear_algebra/affine_space/finite_dimensional.lean
@@ -32,6 +32,18 @@ lemma finite_dimensional_vector_span_of_finite {s : set P} (h : set.finite s) :
   finite_dimensional k (vector_span k s) :=
 span_of_finite k $ vsub_set_finite_of_finite h
 
+/-- The `vector_span` of a family indexed by a `fintype` is
+finite-dimensional. -/
+instance finite_dimensional_direction_vector_span_of_fintype [fintype ι] (p : ι → P) :
+  finite_dimensional k (vector_span k (set.range p)) :=
+finite_dimensional_vector_span_of_finite k (set.finite_range _)
+
+/-- The `vector_span` of a subset of a family indexed by a `fintype`
+is finite-dimensional. -/
+instance finite_dimensional_vector_span_image_of_fintype [fintype ι] (p : ι → P)
+  (s : set ι) : finite_dimensional k (vector_span k (p '' s)) :=
+finite_dimensional_vector_span_of_finite k ((set.finite.of_fintype _).image _)
+
 /-- The direction of the affine span of a finite set is
 finite-dimensional. -/
 lemma finite_dimensional_direction_affine_span_of_finite {s : set P} (h : set.finite s) :
@@ -52,35 +64,47 @@ finite_dimensional_direction_affine_span_of_finite k ((set.finite.of_fintype _).
 
 variables {k}
 
+/-- The `vector_span` of a finite subset of an affinely independent
+family has dimension one less than its cardinality. -/
+lemma findim_vector_span_image_finset_of_affine_independent {p : ι → P}
+  (hi : affine_independent k p) (s : finset ι) {n : ℕ} (hc : finset.card s = n + 1) :
+  findim k (vector_span k (p '' ↑s)) = n :=
+begin
+  have hi' := affine_independent_of_subset_affine_independent
+    (affine_independent_set_of_affine_independent hi) (set.image_subset_range p ↑s),
+  have hc' : fintype.card (p '' ↑s) = n + 1,
+  { rwa [set.card_image_of_injective ↑s (injective_of_affine_independent hi), fintype.card_coe] },
+  have hn : (p '' ↑s).nonempty,
+  { simp [hc, ←finset.card_pos] },
+  rcases hn with ⟨p₁, hp₁⟩,
+  rw affine_independent_set_iff_linear_independent_vsub k hp₁ at hi',
+  have hfr : (p '' ↑s \ {p₁}).finite := ((set.finite_mem_finset _).image _).subset
+    (set.diff_subset _ _),
+  haveI := hfr.fintype,
+  have hf : set.finite ((λ (p : P), p -ᵥ p₁) '' (p '' ↑s \ {p₁})) := hfr.image _,
+  haveI := hf.fintype,
+  have hc : hf.to_finset.card = n,
+  { rw [hf.card_to_finset,
+        set.card_image_of_injective (p '' ↑s \ {p₁}) (vsub_left_injective _)],
+    have hd : insert p₁ (p '' ↑s \ {p₁}) = p '' ↑s,
+    { rw [set.insert_diff_singleton, set.insert_eq_of_mem hp₁] },
+    have hc'' : fintype.card ↥(insert p₁ (p '' ↑s \ {p₁})) = n + 1,
+    { convert hc' },
+    rw set.card_insert (p '' ↑s \ {p₁}) (λ h, ((set.mem_diff p₁).2 h).2 rfl) at hc'',
+    simpa using hc'' },
+  rw [vector_span_eq_span_vsub_set_right_ne k hp₁, findim_span_set_eq_card _ hi', ←hc],
+  congr
+end
+
 /-- The `vector_span` of a finite affinely independent family has
 dimension one less than its cardinality. -/
 lemma findim_vector_span_of_affine_independent [fintype ι] {p : ι → P}
   (hi : affine_independent k p) {n : ℕ} (hc : fintype.card ι = n + 1) :
   findim k (vector_span k (set.range p)) = n :=
 begin
-  have hi' := affine_independent_set_of_affine_independent hi,
-  have hc' : fintype.card (set.range p) = n + 1,
-  by rwa set.card_range_of_injective (injective_of_affine_independent hi),
-  have hn : (set.range p).nonempty,
-  { refine set.range_nonempty_iff_nonempty.2 (fintype.card_pos_iff.1 _),
-    simp [hc] },
-  rcases hn with ⟨p₁, hp₁⟩,
-  rw affine_independent_set_iff_linear_independent_vsub k hp₁ at hi',
-  have hfr : (set.range p \ {p₁}).finite := (set.finite_range _).subset (set.diff_subset _ _),
-  haveI := hfr.fintype,
-  have hf : set.finite ((λ (p : P), p -ᵥ p₁) '' (set.range p \ {p₁})) := hfr.image _,
-  haveI := hf.fintype,
-  have hc : hf.to_finset.card = n,
-  { rw [hf.card_to_finset,
-        set.card_image_of_injective (set.range p \ {p₁}) (vsub_left_injective _)],
-    have hd : insert p₁ (set.range p \ {p₁}) = set.range p,
-    { rw [set.insert_diff_singleton, set.insert_eq_of_mem hp₁] },
-    have hc'' : fintype.card ↥(insert p₁ (set.range p \ {p₁})) = n + 1,
-    { convert hc' },
-    rw set.card_insert (set.range p \ {p₁}) (λ h, ((set.mem_diff p₁).2 h).2 rfl) at hc'',
-    simpa using hc'' },
-  rw [vector_span_eq_span_vsub_set_right_ne k hp₁, findim_span_set_eq_card _ hi', ←hc],
-  congr
+  rw ←finset.card_univ at hc,
+  rw [←set.image_univ, ←finset.coe_univ],
+  exact findim_vector_span_image_finset_of_affine_independent hi finset.univ hc
 end
 
 /-- The `vector_span` of a finite affinely independent family whose

c5d730481f57f716064ae911adfa36be747a33d9
Bhavik Mehta <bhavikmehta8@gmail.com>
2020-09-12 18:19:04 +0100

    shuffle limits in functor cats

--- src/category_theory/limits/functor_category.lean
+++ src/category_theory/limits/functor_category.lean
@@ -7,32 +7,80 @@ import category_theory.limits.preserves.basic
 
 open category_theory category_theory.category
 
-noncomputable theory
-
 namespace category_theory.limits
 
-universes v u -- declare the `v`'s first; see `category_theory.category` for an explanation
+universes v v₂ u -- declare the `v`'s first; see `category_theory.category` for an explanation
 
 variables {C : Type u} [category.{v} C]
 
-variables {J K : Type v} [small_category J] [small_category K]
+variables {J K : Type v} [small_category J] [category.{v₂} K]
 
-@[simp] lemma cone.functor_w {F : J ⥤ (K ⥤ C)} (c : cone F) {j j' : J} (f : j ⟶ j') (k : K) :
-  (c.π.app j).app k ≫ (F.map f).app k = (c.π.app j').app k :=
-by convert ←nat_trans.congr_app (c.π.naturality f).symm k; apply id_comp
+-- @[simp, reassoc] lemma cone.functor_w {F : J ⥤ (K ⥤ C)} (c : cone F) {j j' : J} (f : j ⟶ j') (k : K) :
+--   (c.π.app j).app k ≫ (F.map f).app k = (c.π.app j').app k :=
+-- by convert ←nat_trans.congr_app (c.π.naturality f).symm k; apply id_comp
 
-@[simp] lemma cocone.functor_w {F : J ⥤ (K ⥤ C)} (c : cocone F) {j j' : J} (f : j ⟶ j') (k : K) :
-  (F.map f).app k ≫ (c.ι.app j').app k = (c.ι.app j).app k :=
-by convert ←nat_trans.congr_app (c.ι.naturality f) k; apply comp_id
+-- @[simp, reassoc] lemma cocone.functor_w {F : J ⥤ (K ⥤ C)} (c : cocone F) {j j' : J} (f : j ⟶ j') (k : K) :
+--   (F.map f).app k ≫ (c.ι.app j').app k = (c.ι.app j).app k :=
+-- by convert ←nat_trans.congr_app (c.ι.naturality f) k; apply comp_id
+/--
+The evaluation functors jointly reflect limits: that is, to show a cone is a limit of `F`
+it suffices to show that each evaluation cone is a limit. In other words, to prove a cone is
+limiting you can show it's pointwise limiting.
+-/
+def eval_jointly_reflects {F : J ⥤ K ⥤ C} (c : cone F)
+  (t : Π (k : K), is_limit (((evaluation K C).obj k).map_cone c)) : is_limit c :=
+{ lift := λ s,
+  { app := λ k, (t k).lift ⟨s.X.obj k, whisker_right s.π ((evaluation K C).obj k)⟩,
+    naturality' :=
+    begin
+      intros,
+      apply (t Y).hom_ext,
+      intro j,
+      rw [assoc, (t Y).fac _ j],
+      simpa using ((t X).fac_assoc ⟨s.X.obj X, whisker_right s.π ((evaluation K C).obj X)⟩ j _).symm,
+    end },
+  fac' := λ s j, nat_trans.ext _ _ $ funext $ λ k, (t k).fac _ j,
+  uniq' := λ s m w, nat_trans.ext _ _ $ funext $ λ x, (t x).hom_ext $ λ j,
+  begin
+    rw (t x).fac,
+    simp [← w],
+  end }
 
-@[simps] def functor_category_limit_cone [has_limits_of_shape J C] (F : J ⥤ K ⥤ C) :
+/--
+Given a functor `F` and a collection of limit cones for each diagram `F (-) k`, we can stitch
+them together to give a cone for the diagram `F`.
+`combined_is_limit` shows that the new cone is limiting, and `eval_combined` shows it is
+(essentially) made up of the original cones.
+-/
+@[simps] def combine_cones (F : J ⥤ K ⥤ C) (c : Π (k : K), limit_cone (F.flip.obj k)) :
   cone F :=
-{ X := F.flip ⋙ lim,
+{ X :=
+  { obj := λ k, (c k).cone.X,
+    map := λ k₁ k₂ f, (c k₂).is_limit.lift ⟨_, (c k₁).cone.π ≫ F.flip.map f⟩,
+    map_id' := λ k, (c k).is_limit.hom_ext (λ j, by { dsimp, simp }),
+    map_comp' := λ k₁ k₂ k₃ f₁ f₂, (c k₃).is_limit.hom_ext (λ j, by simp) },
   π :=
-  { app := λ j,
-    { app := λ k, limit.π (F.flip.obj k) j },
-      naturality' := λ j j' f,
-        by ext k; convert (limit.w (F.flip.obj k) _).symm using 1; apply id_comp } }
+  { app := λ j, { app := λ k, (c k).cone.π.app j },
+    naturality' := λ j₁ j₂ g, nat_trans.ext _ _ $ funext $ λ k, (c k).cone.π.naturality g } }
+
+/-- The stitched together cones each project down to the original given cones (up to iso). -/
+def eval_combined (F : J ⥤ K ⥤ C) (c : Π (k : K), limit_cone (F.flip.obj k)) (k : K) :
+  ((evaluation K C).obj k).map_cone (combine_cones F c) ≅ (c k).cone :=
+cones.ext (iso.refl _) (by tidy)
+
+/-- Stitching together limiting cones gives a new limiting cone. -/
+def combined_is_limit (F : J ⥤ K ⥤ C) (c : Π (k : K), limit_cone (F.flip.obj k)) :
+  is_limit (combine_cones F c) :=
+eval_jointly_reflects _ (λ k, (c k).is_limit.of_iso_limit (eval_combined F c k).symm)
+
+noncomputable theory
+
+/--
+Construct a cone for `F` by stitching together the limiting cones for each `k` which we know
+exist from the typeclass.
+-/
+def functor_category_limit_cone [has_limits_of_shape J C] (F : J ⥤ K ⥤ C) : cone F :=
+combine_cones F (λ k, get_limit_cone _)
 
 @[simps] def functor_category_colimit_cocone [has_colimits_of_shape J C] (F : J ⥤ K ⥤ C) :
   cocone F :=
@@ -47,7 +95,7 @@ by convert ←nat_trans.congr_app (c.ι.naturality f) k; apply comp_id
   [has_limits_of_shape J C] (F : J ⥤ K ⥤ C) (k : K) :
   ((evaluation K C).obj k).map_cone (functor_category_limit_cone F) ≅
     limit.cone (F.flip.obj k) :=
-cones.ext (iso.refl _) (by tidy)
+eval_combined F _ k
 
 @[simp] def evaluate_functor_category_colimit_cocone
   [has_colimits_of_shape J C] (F : J ⥤ K ⥤ C) (k : K) :
@@ -57,14 +105,7 @@ cocones.ext (iso.refl _) (by tidy)
 
 def functor_category_is_limit_cone [has_limits_of_shape J C] (F : J ⥤ K ⥤ C) :
   is_limit (functor_category_limit_cone F) :=
-{ lift := λ s,
-  { app := λ k, limit.lift (F.flip.obj k) (((evaluation K C).obj k).map_cone s) },
-  uniq' := λ s m w,
-  begin
-    ext1, ext1 k,
-    exact is_limit.uniq _
-      (((evaluation K C).obj k).map_cone s) (m.app k) (λ j, nat_trans.congr_app (w j) k)
-  end }
+combined_is_limit _ _
 
 def functor_category_is_colimit_cocone [has_colimits_of_shape J C] (F : J ⥤ K ⥤ C) :
   is_colimit (functor_category_colimit_cocone F) :=

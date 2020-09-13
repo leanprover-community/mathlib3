c7bfb80a6f4dbd78078e62902f1ac723991b9969
= <shingtaklam1324@gmail.com>
2020-09-13 20:27:13 +0800

    add mem_support_iff_coeff_ne_zero

--- src/data/polynomial/derivative.lean
+++ src/data/polynomial/derivative.lean
@@ -188,7 +188,7 @@ variables [integral_domain R]
 lemma mem_support_derivative [char_zero R] (p : polynomial R) (n : ℕ) :
   n ∈ (derivative p).support ↔ n + 1 ∈ p.support :=
 suffices (¬(coeff p (n + 1) = 0 ∨ ((n + 1:ℕ) : R) = 0)) ↔ coeff p (n + 1) ≠ 0,
-  by rwa [mem_support_to_fun, ←coeff, coeff_derivative, ne.def, mul_eq_zero, mem_support_to_fun],
+  by simpa only [mem_support_iff_coeff_ne_zero, coeff_derivative, ne.def, mul_eq_zero],
 by rw [nat.cast_eq_zero]; simp only [nat.succ_ne_zero, or_false]
 
 @[simp] lemma degree_derivative_eq [char_zero R] (p : polynomial R) (hp : 0 < nat_degree p) :

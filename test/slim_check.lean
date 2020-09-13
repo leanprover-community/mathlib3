6ac3de51c4cc089e757e68a9ca129119ed314f45
Simon Hudon <simon.hudon@gmail.com>
2020-09-13 00:10:31 -0400

    move test cases

--- test/slim_check.lean
+++ test/slim_check.lean
@@ -12,6 +12,8 @@ Found problems!
 
 i := 0
 j := 1
+guard: 0 < 1 (by construction)
+issue: 1 < 0 does not hold
 -------------------
 ",
   admit,
@@ -27,7 +29,8 @@ begin
 ===================
 Found problems!
 
-x := 102
+x := 104
+issue: 104 < 100 does not hold
 -------------------
 ",
   admit,
@@ -43,9 +46,10 @@ begin
 ===================
 Found problems!
 
-xs := [0, 5]
+xs := [5, 5, 0, 1]
 x := 0
 y := 5
+issue: 5 < 5 does not hold
 -------------------
 ",
   admit,
@@ -61,7 +65,8 @@ begin
 ===================
 Found problems!
 
-x := 102
+x := 104
+issue: 104 < 100 does not hold
 -------------------
 ",
   admit,
@@ -80,7 +85,7 @@ Found problems!
 α := ℤ
 xs := [0]
 ys := [1]
-[0, 1] ≠ [1, 0]
+issue: [0, 1] = [1, 0] does not hold
 -------------------
 ",
   admit,
@@ -92,5 +97,241 @@ begin
   have : ∀ x ∈ [1,2,3], x < 4,
   slim_check { random_seed := some 257, quiet := tt },
     -- success
+  trivial,
+end
+
+open function slim_check
+
+example (f : ℤ → ℤ) (h : injective f) : true :=
+begin
+  have : monotone (f ∘ small.mk),
+  success_if_fail_with_msg
+  { slim_check { random_seed := some 257 } }
+"
+===================
+Found problems!
+
+f := [-1 ↦ 0, -2 ↦ -2, 0 ↦ -1, 1 ↦ 1, x ↦ x]
+x := -1
+y := 0
+guard: -1 ≤ 0 (by construction)
+issue: 0 ≤ -1 does not hold
+-------------------
+",
+  admit,
+  trivial
+end
+
+example (f : ℤ → ℤ) (h : injective f) : true :=
+begin
+  have : monotone f,
+  success_if_fail_with_msg
+  { slim_check { random_seed := some 257 } }
+"
+===================
+Found problems!
+
+f := [-1 ↦ 0, -2 ↦ -2, 0 ↦ -1, 1 ↦ 1, x ↦ x]
+x := -1
+y := 0
+guard: -1 ≤ 0 (by construction)
+issue: 0 ≤ -1 does not hold
+-------------------
+",
+  admit,
+  trivial
+end
+
+section
+
+open tactic
+@[interactive]
+meta def mk_slim_check_test : tactic unit := do
+tgt ← target,
+msg ← (λ s, match interactive.slim_check { random_seed := some 257 } s with
+      | result.success x _ := fail "expecting error" s
+      | result.exception msg _ _ := result.success (msg.iget ()).to_string s
+      end ),
+trace!"Try this:
+  have : {tgt},
+  success_if_fail_with_msg
+  {{ slim_check {{ random_seed := some 257 } }
+\"{msg}\",
+  admit,
+  trivial
+"
+
+end
+
+example (xs ys : list ℤ) (h : xs ~ ys) : true :=
+begin
+  have : list.qsort (λ x y, x ≠ y) xs = list.qsort (λ x y, x ≠ y) ys,
+  success_if_fail_with_msg
+  { slim_check { random_seed := some 257 } }
+"
+===================
+Found problems!
+
+xs := [0, 1]
+ys := [1, 0]
+guard: [0, 1] ~ [1, 0] (by construction)
+issue: [0, 1] = [1, 0] does not hold
+-------------------
+",
+  admit,
+  trivial
+end
+
+example (x y : ℕ) : true :=
+begin
+  have : y ≤ x → x + y < 100,
+  success_if_fail_with_msg
+  { slim_check { random_seed := some 257 } }
+"
+===================
+Found problems!
+
+x := 59
+y := 42
+guard: 42 ≤ 59 (by construction)
+issue: 101 < 100 does not hold
+-------------------
+",
+  admit,
+  trivial,
+end
+
+example (x : ℤ) : true :=
+begin
+  have : x ≤ 3 → 3 ≤ x,
+  success_if_fail_with_msg
+  { slim_check { random_seed := some 257 } }
+"
+===================
+Found problems!
+
+x := 2
+guard: 2 ≤ 3 (by construction)
+issue: 3 ≤ 2 does not hold
+-------------------
+",
+  admit,
+  trivial,
+end
+
+example (x y : ℤ) : true :=
+begin
+  have : y ≤ x → x + y < 100,
+  success_if_fail_with_msg
+  { slim_check { random_seed := some 257 } }
+"
+===================
+Found problems!
+
+x := 52
+y := 49
+guard: 49 ≤ 52 (by construction)
+issue: 101 < 100 does not hold
+-------------------
+",
+  admit,
+  trivial,
+end
+
+example (x y : Prop) : true :=
+begin
+  have : x ∨ y → y ∧ x,
+  success_if_fail_with_msg
+  { slim_check { random_seed := some 257 } }
+"
+===================
+Found problems!
+
+x := tt
+y := ff
+guard: (true ∨ false)
+issue: false does not hold
+-------------------
+",
+  admit,
+  trivial,
+end
+
+example (x y : Prop) : true :=
+begin
+  have : (¬x ↔ y) → y ∧ x,
+  success_if_fail_with_msg
+  { slim_check { random_seed := some 257 } }
+"
+===================
+Found problems!
+
+x := tt
+y := ff
+guard: (¬ true ↔ false)
+issue: false does not hold
+-------------------
+",
+  admit,
+  trivial,
+end
+
+example (x y : Prop) : true :=
+begin
+  -- deterministic
+  have : (x ↔ y) → y ∨ x,
+  success_if_fail_with_msg
+  { slim_check }
+"
+===================
+Found problems!
+
+x := ff
+y := ff
+guard: (false ↔ false)
+issue: false does not hold
+issue: false does not hold
+-------------------
+",
+  admit,
+  trivial,
+end
+
+example (x y : Prop) : true :=
+begin
+  -- deterministic
+  have : y ∨ x,
+  success_if_fail_with_msg
+  { slim_check }
+"
+===================
+Found problems!
+
+x := ff
+y := ff
+issue: false does not hold
+issue: false does not hold
+-------------------
+",
+  admit,
+  trivial,
+end
+
+example (x y : Prop) : true :=
+begin
+  have : x ↔ y,
+  success_if_fail_with_msg
+  { slim_check { random_seed := some 257 } }
+"
+===================
+Found problems!
+
+x := tt
+y := ff
+issue: false does not hold
+issue: ¬ true does not hold
+-------------------
+",
+  admit,
   trivial,
 end

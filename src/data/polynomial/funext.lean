1066ced51456fc3fd286f3e73681e8834c02257b
Rob Lewis <rob.y.lewis@gmail.com>
2020-09-14 14:33:22 +0200

    fix build?

--- src/data/polynomial/funext.lean
+++ src/data/polynomial/funext.lean
@@ -5,8 +5,8 @@ namespace polynomial
 variables {R : Type*} [integral_domain R]
 
 lemma zero_of_eval_zero [hr : infinite R] (p : polynomial R) (h : ∀ x, p.eval x = 0) : p = 0 :=
-classical.by_contradiction $ λ hp,
-  infinite.not_fintype $ ⟨p.roots, λ x, (mem_roots hp).mpr (h _)⟩
+by classical; by_contradiction hp; exact
+infinite.not_fintype ⟨p.roots.to_finset, λ x, multiset.mem_to_finset.mpr ((mem_roots hp).mpr (h _))⟩
 
 lemma funext [infinite R] {p q : polynomial R} (ext : ∀ r : R, p.eval r = q.eval r) : p = q :=
 begin

a492298671d4b8f1f5990bc23833f3e92f5d9664
Johan Commelin <johan@commelin.net>
2020-09-15 14:21:17 +0000

    lint witt_sub

--- src/ring_theory/witt_vector/witt_sub.lean
+++ src/ring_theory/witt_vector/witt_sub.lean
@@ -1,5 +1,22 @@
 import ring_theory.witt_vector.basic
 
+/-!
+# Subtraction of Witt vectors
+
+In Lean, subtraction in a ring is by definition equal to `x + -y`.
+For Witt vectors, this means that subtraction is not defined in terms of
+the polynomials `witt_sub p`.
+
+In this file we define a family of polynomials `poly_add_comp_neg`,
+which is the polynomial composition of `witt_add` and `witt_neg` (in the second entry).
+It is straightforward to show that the coefficients of `x - y` are
+obtained by evaluating `poly_add_comp_neg` on the coefficients of `x` and `y`.
+
+We then show by a computation that `poly_add_comp_neg p` is equal to `witt_sub p`
+to deduce in `witt_vector.sub_coeff` that evaluating `witt_sub p` on the coefficients of `x` and `y`
+gives the coefficients of `x - y`.
+-/
+
 namespace witt_vector
 
 variables {p : ℕ} {R S σ idx : Type*} [hp : fact p.prime] [comm_ring R] [comm_ring S]
@@ -21,19 +38,22 @@ lemma sub_def (x y : 𝕎 R) : x - y =
   eval (witt_add p) ![x, eval (witt_neg p) ![y]] :=
 rfl
 
-noncomputable def Sub : ℕ → mv_polynomial (fin 2 × ℕ) ℤ :=
+/-- The composition of `witt_add` and `witt_neg` (in the second entry).
+This gives a polynomial description of the coefficients of `x - y`,
+for Witt vectors `x` and `y`.
+
+In `poly_add_comp_neg_eq` we show that this polynomial is equal to `witt_sub`. -/
+noncomputable def poly_add_comp_neg : ℕ → mv_polynomial (fin 2 × ℕ) ℤ :=
 λ n, bind₁ (function.uncurry $
   ![λ k, X ((0 : fin 2), k),
     λ k, rename (prod.map fin.succ id) (witt_neg p k)])
   (witt_add p n)
 
 lemma sub_eq (x y : 𝕎 R) :
-  x - y = eval (Sub p) ![x, y] :=
+  x - y = eval (poly_add_comp_neg p) ![x, y] :=
 begin
   apply ext, intro n,
-  dsimp [Sub, sub_def],
-  -- show aeval (λ bn : bool × ℕ, cond bn.1 (x.coeff bn.2)
-  --   (aeval (λ m : unit × ℕ, (y.coeff m.2)) (witt_neg p bn.2))) (witt_add p n) = _,
+  dsimp [poly_add_comp_neg, sub_def],
   conv_rhs { rw [eval, coeff_mk, peval, aeval_bind₁] },
   apply eval₂_hom_congr (ring_hom.ext_int _ _) _ rfl,
   ext ⟨b, k⟩, fin_cases b; dsimp [function.uncurry],
@@ -45,11 +65,11 @@ begin
       matrix.cons_val_one, id.def, fin.succ_zero_eq_one, prod.map_mk], }
 end
 
-lemma Sub_eq : Sub p = witt_sub p :=
+lemma poly_add_comp_neg_eq : poly_add_comp_neg p = witt_sub p :=
 begin
   apply eq_witt_structure_int,
   intro n,
-  rw [Sub, ← bind₁_bind₁, witt_add, witt_structure_int_prop p _ n, bind₁_bind₁,
+  rw [poly_add_comp_neg, ← bind₁_bind₁, witt_add, witt_structure_int_prop p _ n, bind₁_bind₁,
       alg_hom.map_add, alg_hom.map_sub, sub_eq_add_neg, ← alg_hom.map_neg],
   apply congr₂,
   { rw [bind₁_X_right, bind₁_X_right, bind₁_rename],
@@ -65,7 +85,7 @@ end
 
 lemma sub_coeff (x y : 𝕎 R) (n : ℕ) :
   (x - y).coeff n = peval (witt_sub p n) ![x.coeff, y.coeff] :=
-by { rw [sub_eq, Sub_eq, eval, coeff_mk], refl }
+by { rw [sub_eq, poly_add_comp_neg_eq, eval, coeff_mk], refl }
 
 end sub_coeff
 

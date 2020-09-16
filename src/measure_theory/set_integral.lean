64cfc9ba550b0c0332721f6532b4355e350100c7
Patrick Massot <patrickmassot@free.fr>
2020-09-16 11:14:49 +0200

    feat(integration): integral commutes with continuous linear maps
    from the sphere eversion project.

--- src/measure_theory/set_integral.lean
+++ src/measure_theory/set_integral.lean
@@ -287,7 +287,7 @@ a simple function with a multiple of a characteristic function and that the inte
 of their images is a subset of `{0}`).
 -/
 @[elab_as_eliminator]
-lemma integrable.induction {P : (α → E) → Prop}
+lemma integrable.induction (P : (α → E) → Prop)
   (h_ind : ∀ (c : E) ⦃s⦄, is_measurable s → μ s < ⊤ → P (s.indicator (λ _, c)))
   (h_sum : ∀ ⦃f g : α → E⦄, set.univ ⊆ f ⁻¹' {0} ∪ g ⁻¹' {0} → measurable f → measurable g →
     integrable f μ → integrable g μ → P f → P g → P (f + g))
@@ -463,6 +463,135 @@ lemma continuous_at.integral_sub_linear_is_o_ae
   {f : α → E} (ha : continuous_at f a) (hfm : measurable f) :
   is_o (λ s, ∫ x in s, f x ∂μ - (μ s).to_real • f a) (λ s, (μ s).to_real) ((𝓝 a).lift' powerset) :=
 (ha.mono_left inf_le_left).integral_sub_linear_is_o_ae hfm (μ.finite_at_nhds a)
+
+section continuous_linear_map
+
+variables {μ : measure α} [normed_group E] [normed_space ℝ E] [normed_group F]  [normed_space ℝ F]
+
+namespace measure_theory
+
+lemma integrable.clm_apply {φ : α → E} (φ_int : integrable φ μ) (L : E →L[ℝ] F) :
+  integrable (λ (a : α), L (φ a)) μ :=
+((integrable.norm φ_int).const_mul ∥L∥).mono' (eventually_of_forall $ λ a, L.le_op_norm (φ a))
+
+variables [second_countable_topology E] [measurable_space E] [borel_space E]
+
+lemma l1.norm_clm_apply_le (φ : α →₁[μ] E) (L : E →L[ℝ] F) : ∀ᵐ a ∂μ, ∥L (φ a)∥ ≤ ∥L∥*∥φ a∥ :=
+eventually_of_forall (λ a, L.le_op_norm (φ a))
+
+section
+variables [measurable_space F] [borel_space F] [second_countable_topology F]
+
+/-- Composing `φ : α →₁[μ] E` with `L : E →L[ℝ] F`. -/
+def l1.clm_apply (φ : α →₁[μ] E) (L : E →L[ℝ] F) : α →₁[μ] F :=
+l1.of_fun (λ a, L (φ a)) (φ.measurable.clm_apply L) (φ.integrable.clm_apply L)
+
+lemma l1.clm_apply_apply (φ : α →₁[μ] E) (L : E →L[ℝ] F) : ∀ᵐ a ∂μ, (φ.clm_apply L) a = L (φ a) :=
+l1.to_fun_of_fun _ _ _
+
+lemma l1.measurable_clm_apply (L : E →L[ℝ] F) (φ : α →₁[μ] E) : measurable (φ.clm_apply L) :=
+(φ.clm_apply L).measurable
+
+lemma l1.integrable_clm_apply (L : E →L[ℝ] F) (φ : α →₁[μ] E) : integrable (φ.clm_apply L) μ :=
+(φ.clm_apply L).integrable
+
+end
+
+lemma l1.integrable_clm_apply' (L : E →L[ℝ] F) (φ : α →₁[μ] E) : integrable (λ a, L (φ a)) μ :=
+φ.integrable.clm_apply L
+
+variables [measurable_space F]
+
+lemma l1.measurable_clm_apply' [borel_space F] (L : E →L[ℝ] F) (φ : α →₁[μ] E) :
+  measurable (λ a, L (φ a)) := L.measurable.comp φ.measurable
+
+variables [borel_space F] [second_countable_topology F]
+
+lemma l1.integral_clm_apply [complete_space F] (φ : α →₁[μ] E) (L : E →L[ℝ] F):
+  ∫ a, (φ.clm_apply L) a ∂μ = ∫ a, L (φ a) ∂μ :=
+by simp [l1.clm_apply]
+
+/-- Composing `φ : α →₁[μ] E` with `L : E →L[ℝ] F`, seen as a `ℝ`-linear map on `α →₁[μ] E`. -/
+def l1.clm_applyₗ (L : E →L[ℝ] F) : (α →₁[μ] E) →ₗ[ℝ] (α →₁[μ] F) :=
+{ to_fun := λ φ, φ.clm_apply L,
+  map_add' := begin
+    intros f g,
+    dsimp [l1.clm_apply],
+    rw [← l1.of_fun_add, l1.of_fun_eq_of_fun],
+    apply (l1.add_to_fun f g).mono,
+    intros a ha,
+    simp only [ha, pi.add_apply, L.map_add]
+  end,
+  map_smul' := begin
+    intros c f,
+    dsimp [l1.clm_apply],
+    rw [← l1.of_fun_smul, l1.of_fun_eq_of_fun],
+    apply (l1.smul_to_fun c f).mono,
+    intros a ha,
+    simp only [ha, pi.smul_apply, continuous_linear_map.map_smul]
+  end }
+
+lemma l1.clm_apply_norm_le (φ : α →₁[μ] E) (L : E →L[ℝ] F) : ∥φ.clm_apply L∥ ≤ ∥L∥*∥φ∥ :=
+begin
+  erw l1.norm_of_fun_eq_integral_norm,
+  calc
+  ∫ a, ∥L (φ a)∥ ∂μ ≤ ∫ a, ∥L∥ *∥φ a∥ ∂μ : integral_mono (L.measurable.comp φ.measurable).norm
+                                (φ.integrable_clm_apply' L).norm (φ.measurable_norm.const_mul $ ∥L∥)
+                                (φ.integrable_norm.const_mul $ ∥L∥) (φ.norm_clm_apply_le L)
+  ... = ∥L∥ * ∥φ∥ : by rw [integral_mul_left, φ.norm_eq_integral_norm]
+end
+
+end measure_theory
+
+open measure_theory
+
+variables (μ)
+variables [measurable_space E] [borel_space E] [second_countable_topology E]
+variables [measurable_space F] [borel_space F] [second_countable_topology F]
+
+/-- Composing `φ : α →₁[μ] E` with `L : E →L[ℝ] F`, seen as a continuous `ℝ`-linear map on
+`α →₁[μ] E`. -/
+def continuous_linear_map.l1_apply (L : E →L[ℝ] F) : (α →₁[μ] E) →L[ℝ] (α →₁[μ] F) :=
+linear_map.mk_continuous (measure_theory.l1.clm_applyₗ L) (∥L∥) (λ φ, φ.clm_apply_norm_le L)
+
+variables [complete_space F]
+
+lemma continuous_linear_map.continuous_integral_apply (L : E →L[ℝ] F) :
+continuous (λ (φ : α →₁[μ] E), ∫ (a : α), L (φ a) ∂μ) :=
+begin
+  rw ← funext (λ φ : α →₁[μ] E, φ.integral_clm_apply L),
+  exact continuous_integral.comp (L.l1_apply μ).continuous
+end
+
+variables {μ} [complete_space E]
+
+lemma continuous_linear_map.integral_apply_comm {φ : α → E} (L : E →L[ℝ] F) (φ_meas : measurable φ)
+  (φ_int : integrable φ μ) : ∫ a, L (φ a) ∂μ = L (∫ a, φ a ∂μ) :=
+begin
+  apply integrable.induction (λ φ, ∫ a, L (φ a) ∂μ = L (∫ a, φ a ∂μ)),
+  { intros e s s_meas s_finite,
+    rw [integral_indicator_const e s_meas, continuous_linear_map.map_smul,
+        ← integral_indicator_const (L e) s_meas],
+    congr' 1,
+    ext a,
+    rw set.indicator_comp_of_zero L.map_zero },
+  { intros f g H f_meas g_meas f_int g_int hf hg,
+    simp [L.map_add, integral_add f_meas f_int g_meas g_int,
+      integral_add (f_meas.clm_apply L) (f_int.clm_apply L)
+      (g_meas.clm_apply L) (g_int.clm_apply L), hf, hg] },
+  { exact is_closed_eq (L.continuous_integral_apply μ)  (L.continuous.comp continuous_integral) },
+  { intros f g hfg f_meas g_meas f_int hf,
+    convert hf using 1 ; clear hf,
+    { exact integral_congr_ae (L.measurable.comp g_meas) (L.measurable.comp f_meas) (hfg.fun_comp L).symm },
+    { rw integral_congr_ae g_meas f_meas hfg.symm } },
+  all_goals { assumption }
+end
+
+lemma continuous_linear_map.l1_integral_apply_comm (L : E →L[ℝ] F) (φ : α →₁[μ] E) :
+  ∫ a, L (φ a) ∂μ = L (∫ a, φ a ∂μ) :=
+L.integral_apply_comm φ.measurable φ.integrable
+
+end continuous_linear_map
 
 /-
 namespace integrable

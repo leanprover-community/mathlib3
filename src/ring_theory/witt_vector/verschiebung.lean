a335ac24dae48503891d077e72b6ac6fefdb0732
Johan Commelin <johan@commelin.net>
2020-09-16 14:10:39 +0000

    use is_poly2 for verschiebung

--- src/ring_theory/witt_vector/verschiebung.lean
+++ src/ring_theory/witt_vector/verschiebung.lean
@@ -19,6 +19,8 @@ local notation `𝕎` := witt_vector p -- type as `\bbW`
 local attribute [semireducible] witt_vector
 local attribute [instance] mv_polynomial.invertible_rat_coe_nat
 
+noncomputable theory
+
 /--
 `verschiebung_fun x` shifts the coefficients of `x` up by one, by inserting 0 as the 0th coefficient.
 `x.coeff i` then becomes `(verchiebung_fun x).coeff (i + 1)`.
@@ -36,105 +38,8 @@ lemma verschiebung_fun_coeff_zero (x : 𝕎 R) :
   (verschiebung_fun x).coeff 0 = 0 :=
 by rw [verschiebung_fun_coeff, if_pos rfl]
 
-include hp
-
-/-- verschiebung is a natural transformation -/
-@[simp] lemma map_verschiebung_fun (f : R →+* S) (x : 𝕎 R) :
-  map f (verschiebung_fun x) = verschiebung_fun (map f x) :=
-by { ext ⟨-, -⟩, exact f.map_zero, refl }
-
-omit hp
-@[simp] lemma ghost_component_zero_verschiebung_fun (x : 𝕎 R) :
-  ghost_component 0 (verschiebung_fun x) = 0 :=
-by rw [ghost_component_apply, aeval_witt_polynomial, finset.range_one, finset.sum_singleton,
-       verschiebung_fun_coeff_zero, pow_zero, pow_zero, pow_one, one_mul]
-
-include hp
-@[simp] lemma ghost_component_verschiebung_fun (x : 𝕎 R) (n : ℕ) :
-  ghost_component (n + 1) (verschiebung_fun x) = p * ghost_component n x :=
-begin
-  simp only [ghost_component, aeval_witt_polynomial],
-  rw [finset.sum_range_succ', verschiebung_fun_coeff, if_pos rfl, zero_pow (nat.pow_pos hp.pos _),
-      mul_zero, add_zero, finset.mul_sum, finset.sum_congr rfl],
-  rintro i -,
-  simp only [pow_succ, mul_assoc, verschiebung_fun_coeff, if_neg (nat.succ_ne_zero i),
-    nat.succ_sub_succ, nat.sub_zero]
-end
-
-lemma verschiebung_add_aux₁ {R : Type*} (x y : 𝕎 (mv_polynomial R ℚ)) :
-  verschiebung_fun (x + y) = verschiebung_fun x + verschiebung_fun y :=
-begin
-  apply (ghost_map.bijective_of_invertible p (mv_polynomial R ℚ)).1,
-  ext1 n,
-  rw ring_hom.map_add,
-  simp only [pi.add_apply],
-  cases n,
-  { simp only [add_zero, ghost_component_zero_verschiebung_fun, ghost_map_apply], },
-  { simp only [ghost_map_apply, ghost_component_verschiebung_fun, ghost_component_add, mul_add], }
-end
-
-lemma verschiebung_add_aux₂ {R : Type*} (x y : 𝕎 (mv_polynomial R ℤ)) :
-  verschiebung_fun (x + y) = verschiebung_fun x + verschiebung_fun y :=
-begin
-  refine map_injective (mv_polynomial.map (int.cast_ring_hom ℚ))
-    (mv_polynomial.map_injective _ int.cast_injective) _,
-  simp only [verschiebung_add_aux₁, ring_hom.map_add, map_verschiebung_fun],
-end
-
-variables {R}
-
-/--
-`verschiebung x` shifts the coefficients of `x` up by one, by inserting 0 as the 0th coefficient.
-`x.coeff i` then becomes `(verchiebung x).coeff (i + 1)`.
-
-This is a additive monoid hom with underlying function `verschiebung_fun`.
--/
-noncomputable
-def verschiebung : 𝕎 R →+ 𝕎 R :=
-{ to_fun := verschiebung_fun,
-  map_zero' :=
-  by ext ⟨⟩; rw [verschiebung_fun_coeff]; simp only [if_true, eq_self_iff_true, zero_coeff, if_t_t],
-  map_add' :=
-  begin
-    intros x y,
-    rcases map_surjective _ (counit_surjective R) x with ⟨x, rfl⟩,
-    rcases map_surjective _ (counit_surjective R) y with ⟨y, rfl⟩,
-    rw [← ring_hom.map_add],
-    iterate 3 { rw [← map_verschiebung_fun] },
-    rw [verschiebung_add_aux₂, ring_hom.map_add],
-  end }
-
-@[simp] lemma verschiebung_coeff_zero (x : 𝕎 R) :
-  (verschiebung x).coeff 0 = 0 := rfl
-
--- simp_nf complains if this is simp
-lemma verschiebung_coeff_add_one (x : 𝕎 R) (n : ℕ) :
-  (verschiebung x).coeff (n + 1) = x.coeff n := rfl
-
-@[simp] lemma verschiebung_coeff_succ (x : 𝕎 R) (n : ℕ) :
-  (verschiebung x).coeff n.succ = x.coeff n := rfl
-
-/-- Verschiebung is a natural transformation. -/
-@[simp] lemma map_verschiebung (f : R →+* S) (x : 𝕎 R) :
-  map f (verschiebung x) = verschiebung (map f x) :=
-map_verschiebung_fun _ _
-
-@[simp] lemma ghost_component_zero_verschiebung (x : 𝕎 R) :
-  ghost_component 0 (verschiebung x) = 0 :=
-ghost_component_zero_verschiebung_fun _
-
-@[simp] lemma ghost_component_verschiebung (x : 𝕎 R) (n : ℕ) :
-  ghost_component (n + 1) (verschiebung x) = p * ghost_component n x :=
-ghost_component_verschiebung_fun _ _
-
-section
-open mv_polynomial
-
-noncomputable theory
-
-variables (p)
-
-omit hp
+@[simp] lemma verschiebung_fun_coeff_succ (x : 𝕎 R) (n : ℕ) :
+  (verschiebung_fun x).coeff n.succ = x.coeff n := rfl
 
 /--
 The 0th Verschiebung polynomial is 0. For `n > 0`, the `n`th Verschiebung polynomial is the
@@ -145,24 +50,6 @@ if n = 0 then 0 else X (n-1)
 
 @[simp] lemma verschiebung_poly_zero :
   verschiebung_poly 0 = 0 := rfl
-
-set_option pp.implicit true
-set_option pp.structure_projections false
-
-/--
-`witt_vector.verschiebung` has polynomial structure given by `witt_vector.verschiebung_poly`.
--/
-@[simps { fully_applied := ff }]
-def verschiebung_is_poly : is_poly p (λ R _Rcr, @verschiebung p R hp _Rcr) :=
-{ poly := verschiebung_poly,
-  coeff :=
-  begin
-    rintro R _Rcr x, resetI,
-    funext n, cases n,
-    { simp only [verschiebung_poly, verschiebung_coeff_zero, if_pos rfl, alg_hom.map_zero] },
-    { rw [verschiebung_poly, verschiebung_coeff_succ, if_neg (n.succ_ne_zero),
-          aeval_X, nat.succ_eq_add_one, nat.add_sub_cancel], }
-  end }
 
 include hp
 lemma bind₁_verschiebung_poly_witt_polynomial (n : ℕ) :
@@ -193,6 +80,96 @@ begin
     ring_exp, }
 end
 
+variable (p)
+omit hp
+
+/--
+`witt_vector.verschiebung` has polynomial structure given by `witt_vector.verschiebung_poly`.
+-/
+@[simps { fully_applied := ff }]
+def verschiebung_fun_is_poly : is_poly p (λ R _Rcr, @verschiebung_fun p R _Rcr) :=
+{ poly := verschiebung_poly,
+  coeff :=
+  begin
+    rintro R _Rcr x, resetI,
+    funext n, cases n,
+    { simp only [verschiebung_poly, verschiebung_fun_coeff_zero, if_pos rfl, alg_hom.map_zero] },
+    { rw [verschiebung_poly, verschiebung_fun_coeff_succ, if_neg (n.succ_ne_zero),
+          aeval_X, nat.succ_eq_add_one, nat.add_sub_cancel], }
+  end }
+
+variable {p}
+include hp
+
+/--
+`verschiebung x` shifts the coefficients of `x` up by one, by inserting 0 as the 0th coefficient.
+`x.coeff i` then becomes `(verchiebung x).coeff (i + 1)`.
+
+This is a additive monoid hom with underlying function `verschiebung_fun`.
+-/
+noncomputable
+def verschiebung : 𝕎 R →+ 𝕎 R :=
+{ to_fun := verschiebung_fun,
+  map_zero' :=
+  by ext ⟨⟩; rw [verschiebung_fun_coeff]; simp only [if_true, eq_self_iff_true, zero_coeff, if_t_t],
+  map_add' :=
+  begin
+    apply is_poly₂.ext'
+      ((verschiebung_fun_is_poly p).comp₂ (add_is_poly₂ p))
+      ((add_is_poly₂ p).comp (verschiebung_fun_is_poly p) (verschiebung_fun_is_poly p)),
+    intro n,
+    by_cases hn : n = 0;
+    simp only [hn, is_poly.comp₂_poly, verschiebung_fun_is_poly_poly, add_is_poly₂_poly, is_poly₂.comp_poly,
+      ← bind₁_bind₁, bind₁_verschiebung_poly_witt_polynomial, witt_add, witt_structure_int_prop,
+      alg_hom.map_add, bind₁_X_right, function.uncurry, bind₁_rename, function.comp,
+      matrix.head_cons, matrix.cons_val_one, matrix.cons_val_zero, ← rename_bind₁,
+      add_zero, if_true, ring_hom.map_zero, eq_self_iff_true, alg_hom.map_zero,
+      ring_hom.map_nat_cast, alg_hom.map_nat_cast, if_false, ring_hom.map_mul, alg_hom.map_mul, mul_add],
+  end }
+
+omit hp
+
+/--
+`witt_vector.verschiebung` has polynomial structure given by `witt_vector.verschiebung_poly`.
+-/
+@[simps { fully_applied := ff }]
+def verschiebung_is_poly : is_poly p (λ R _Rcr, @verschiebung p R hp _Rcr) :=
+{ poly := verschiebung_poly,
+  coeff := (verschiebung_fun_is_poly p).coeff }
+
+include hp
+
+/-- verschiebung is a natural transformation -/
+@[simp] lemma map_verschiebung (f : R →+* S) (x : 𝕎 R) :
+  map f (verschiebung x) = verschiebung (map f x) :=
+by { ext ⟨-, -⟩, exact f.map_zero, refl }
+
+omit hp
+@[simp] lemma ghost_component_zero_verschiebung (x : 𝕎 R) :
+  ghost_component 0 (verschiebung_fun x) = 0 :=
+by rw [ghost_component_apply, aeval_witt_polynomial, finset.range_one, finset.sum_singleton,
+       verschiebung_fun_coeff_zero, pow_zero, pow_zero, pow_one, one_mul]
+
+include hp
+@[simp] lemma ghost_component_verschiebung (x : 𝕎 R) (n : ℕ) :
+  ghost_component (n + 1) (verschiebung_fun x) = p * ghost_component n x :=
+begin
+  simp only [ghost_component, aeval_witt_polynomial],
+  rw [finset.sum_range_succ', verschiebung_fun_coeff, if_pos rfl, zero_pow (nat.pow_pos hp.pos _),
+      mul_zero, add_zero, finset.mul_sum, finset.sum_congr rfl],
+  rintro i -,
+  simp only [pow_succ, mul_assoc, verschiebung_fun_coeff, if_neg (nat.succ_ne_zero i),
+    nat.succ_sub_succ, nat.sub_zero]
 end
+
+@[simp] lemma verschiebung_coeff_zero (x : 𝕎 R) :
+  (verschiebung x).coeff 0 = 0 := rfl
+
+-- simp_nf complains if this is simp
+lemma verschiebung_coeff_add_one (x : 𝕎 R) (n : ℕ) :
+  (verschiebung x).coeff (n + 1) = x.coeff n := rfl
+
+@[simp] lemma verschiebung_coeff_succ (x : 𝕎 R) (n : ℕ) :
+  (verschiebung x).coeff n.succ = x.coeff n := rfl
 
 end witt_vector

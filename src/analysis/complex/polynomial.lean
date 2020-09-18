784fe5a74662d9c6e36fa45418cfe96b3ad6c0b4
Jujian Zhang <jujian.zhang1998@outlook.com>
2020-09-18 14:53:05 +0100

    Update src/analysis/complex/polynomial.lean
    Co-authored-by: Shing Tak Lam <shingtaklam1324@gmail.com>

--- src/analysis/complex/polynomial.lean
+++ src/analysis/complex/polynomial.lean
@@ -4,7 +4,6 @@ Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Chris Hughes
 -/
 import topology.algebra.polynomial
-import data.polynomial.basic
 import analysis.special_functions.pow
 
 /-!

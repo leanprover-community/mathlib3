e44163869b5f0a08e9c5a2dc69d22858505c70af
Heather Macbeth <25316162+hrmacbeth@users.noreply.github.com>
2020-09-19 19:09:50 -0400

    linter

--- src/geometry/manifold/sheaves.lean
+++ src/geometry/manifold/sheaves.lean
@@ -8,15 +8,17 @@ open set topological_space
 open structure_groupoid
 open structure_groupoid.local_invariant_prop
 
-variables {H : Type*} {M : Type} [topological_space H] [topological_space M] [charted_space H (Top.of M)]
-{H' : Type*} {M' : Type} [topological_space H'] [topological_space M'] [charted_space H' M']
-variables (G : structure_groupoid H) (G' : structure_groupoid H')
-variables (P : (H → H') → (set H) → H → Prop)
+variables {M : Type} {M' : Type}
 
 /-- Extension of a function defined on a subtype `U ⊆ M` to a function on `M`, by taking the junk
 value `default M'` outside `U`. Rather general-purpose; where in mathlib should this live? -/
 def extend [inhabited M'] (U : set M) (f : U → M') : M → M' :=
 λ x, if h : x ∈ U then f ⟨x, h⟩ else default M'
+
+variables {H : Type*} [topological_space H] [topological_space M] [charted_space H (Top.of M)]
+{H' : Type*} [topological_space H'] [topological_space M'] [charted_space H' M']
+variables (G : structure_groupoid H) (G' : structure_groupoid H')
+variables (P : (H → H') → (set H) → H → Prop)
 
 /-- Let `M`, `M'` be charted spaces with transition functions in the groupoids `G`, `G'`, and let
 `P` be a `local_invariant_prop` for functions between spaces with these groupoids.  Then there is an

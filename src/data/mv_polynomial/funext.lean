87ace162b6cffb2d9d469f0b11f7508871061bda
Johan Commelin <johan@commelin.net>
2020-09-21 10:05:22 +0000

    copy

--- src/data/mv_polynomial/funext.lean
+++ src/data/mv_polynomial/funext.lean
@@ -1,3 +1,8 @@
+/-
+Copyright (c) 2020 Johan Commelin. All rights reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
+Authors: Johan Commelin
+-/
 import data.polynomial.ring_division
 import data.mv_polynomial.rename
 import ring_theory.polynomial.basic

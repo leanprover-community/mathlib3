b1031983ae858b244bf8b52e774b903517aca029
Thomas Browning <tb65536@uw.edu>
2020-09-21 11:33:37 -0700

    removed line of documentation

--- src/field_theory/adjoin.lean
+++ src/field_theory/adjoin.lean
@@ -16,8 +16,6 @@ For example, `algebra.adjoin K {x}` might not include `x⁻¹`.
 
 ## Main results
 
-(This is just a start; we've got more to add, including a proof of the Primitive Element Theorem.)
-
 - `adjoin_adjoin_left`: adjoining S and then T is the same as adjoining S ∪ T.
 
 ## Notation

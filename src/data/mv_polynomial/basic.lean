7b42831e119ec8e3f0521fdee330acc5cdff8680
Johan Commelin <johan@commelin.net>
2020-09-22 05:00:23 +0000

    move to own file

--- src/data/mv_polynomial/basic.lean
+++ src/data/mv_polynomial/basic.lean
@@ -900,45 +900,5 @@ eval₂_hom_eq_zero _ _ _ h
 
 end aeval
 
-section counit
-open function
-
-variables (A B R : Type*) [comm_semiring A] [comm_semiring B] [comm_ring R] [algebra A B]
-
-/-- `mv_polynomial.acounit R A` is the natural surjective algebra homomorphism
-`mv_polynomial A R →ₐ[R] A` obtained by `X a ↦ a`.
-
-See `mv_polynomial.counit` for the “absolute” variant with `R = ℤ`,
-and `mv_polynomial.counit_nat` for the “absolute” variant with `R = ℕ`. -/
-noncomputable def acounit : mv_polynomial B A →ₐ[A] B :=
-aeval id
-
-lemma acounit_surjective : surjective (acounit A B) :=
-λ a, ⟨X a, eval₂_hom_X' _ _ _⟩
-
-/-- `mv_polynomial.counit R` is the natural surjective ring homomorphism
-`mv_polynomial R ℤ →+* R` obtained by `X r ↦ r`.
-
-See `mv_polynomial.acounit` for a “relative” variant for algebras over a base ring,
-and `mv_polynomial.counit_nat` for the “absolute” variant with `R = ℕ`. -/
-noncomputable def counit : mv_polynomial R ℤ →+* R :=
-acounit ℤ R
-
-lemma counit_surjective : surjective (counit R) :=
-acounit_surjective ℤ R
-
-/-- `mv_polynomial.counit_nat R` is the natural surjective ring homomorphism
-`mv_polynomial R ℕ →+* R` obtained by `X r ↦ r`.
-
-See `mv_polynomial.acounit` for a “relative” variant for algebras over a base ring
-and `mv_polynomial.counit` for the “absolute” variant with `R = ℤ`. -/
-noncomputable def counit_nat : mv_polynomial A ℕ →+* A :=
-acounit ℕ A
-
-lemma counit_nat_surjective : surjective (counit_nat A) :=
-acounit_surjective ℕ A
-
-end counit
-
 end comm_semiring
 end mv_polynomial

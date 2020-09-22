ca6b0b6950562b0f8bb2025332d17e7d9dfc1217
tb65536 <tb65536@users.noreply.github.com>
2020-09-22 09:11:36 -0700

    Update src/data/polynomial/field_division.lean
    Co-authored-by: Vierkantor <Vierkantor@users.noreply.github.com>

--- src/data/polynomial/field_division.lean
+++ src/data/polynomial/field_division.lean
@@ -223,7 +223,7 @@ by { cases euclidean_domain.gcd_dvd_right f g with p hp,
 
 lemma root_gcd_iff_root_left_right [comm_semiring k] (ϕ : R →+* k) (f g : polynomial R) (α : k) :
   (euclidean_domain.gcd f g).eval₂ ϕ α = 0 ↔ (f.eval₂ ϕ α = 0) ∧ (g.eval₂ ϕ α = 0) :=
-⟨λ h, ⟨root_left_of_root_gcd h,root_right_of_root_gcd h⟩, λ h, eval₂_gcd_eq_zero h.1 h.2⟩
+⟨λ h, ⟨root_left_of_root_gcd h, root_right_of_root_gcd h⟩, λ h, eval₂_gcd_eq_zero h.1 h.2⟩
 
 lemma is_root_gcd_iff_is_root_left_right (f g : polynomial R) (α : R) :
   (euclidean_domain.gcd f g).is_root α  ↔ f.is_root α ∧ g.is_root α :=

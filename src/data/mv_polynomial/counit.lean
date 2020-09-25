90c76e13ba79525ae5b400005089941f9b4e24dc
Johan Commelin <johan@commelin.net>
2020-09-25 11:10:08 +0000

    fix docstrings, add simp lemmas

--- src/data/mv_polynomial/counit.lean
+++ src/data/mv_polynomial/counit.lean
@@ -10,7 +10,7 @@ import data.mv_polynomial.basic
 ## Counit morphisms for multivariate polynomials
 
 One may consider the ring of multivariate polynomials `mv_polynomial A R` with coefficients in `R`
-and variables indexed by `A`. If `A` is not just a type, but an algebra over `A`,
+and variables indexed by `A`. If `A` is not just a type, but an algebra over `R`,
 then there is a natural surjective algebra homomorphism `mv_polynomial A R →ₐ[R] A`
 obtained by `X a ↦ a`.
 
@@ -28,16 +28,25 @@ open function
 
 variables (A B R : Type*) [comm_semiring A] [comm_semiring B] [comm_ring R] [algebra A B]
 
-/-- `mv_polynomial.acounit R A` is the natural surjective algebra homomorphism
-`mv_polynomial A R →ₐ[R] A` obtained by `X a ↦ a`.
+/-- `mv_polynomial.acounit A B` is the natural surjective algebra homomorphism
+`mv_polynomial B A →ₐ[A] B` obtained by `X a ↦ a`.
 
-See `mv_polynomial.counit` for the “absolute” variant with `R = ℤ`,
-and `mv_polynomial.counit_nat` for the “absolute” variant with `R = ℕ`. -/
+See `mv_polynomial.counit` for the “absolute” variant with `A = ℤ`,
+and `mv_polynomial.counit_nat` for the “absolute” variant with `A = ℕ`. -/
 noncomputable def acounit : mv_polynomial B A →ₐ[A] B :=
 aeval id
 
-lemma acounit_surjective : surjective (acounit A B) :=
-λ a, ⟨X a, eval₂_hom_X' _ _ _⟩
+variables {B}
+
+@[simp] lemma acounit_X (b : B) : acounit A B (X b) = b := aeval_X _ b
+
+variables {A} (B)
+
+@[simp] lemma acounit_C (a : A) : acounit A B (C a) = algebra_map A B a := aeval_C _ a
+
+variables (A)
+
+lemma acounit_surjective : surjective (acounit A B) := λ b, ⟨X b, acounit_X A b⟩
 
 /-- `mv_polynomial.counit R` is the natural surjective ring homomorphism
 `mv_polynomial R ℤ →+* R` obtained by `X r ↦ r`.
@@ -47,18 +56,24 @@ and `mv_polynomial.counit_nat` for the “absolute” variant with `R = ℕ`. -/
 noncomputable def counit : mv_polynomial R ℤ →+* R :=
 acounit ℤ R
 
-lemma counit_surjective : surjective (counit R) :=
-acounit_surjective ℤ R
-
-/-- `mv_polynomial.counit_nat R` is the natural surjective ring homomorphism
-`mv_polynomial R ℕ →+* R` obtained by `X r ↦ r`.
+/-- `mv_polynomial.counit_nat A` is the natural surjective ring homomorphism
+`mv_polynomial A ℕ →+* A` obtained by `X a ↦ a`.
 
 See `mv_polynomial.acounit` for a “relative” variant for algebras over a base ring
-and `mv_polynomial.counit` for the “absolute” variant with `R = ℤ`. -/
+and `mv_polynomial.counit` for the “absolute” variant with `A = ℤ`. -/
 noncomputable def counit_nat : mv_polynomial A ℕ →+* A :=
 acounit ℕ A
 
-lemma counit_nat_surjective : surjective (counit_nat A) :=
-acounit_surjective ℕ A
+
+lemma counit_surjective : surjective (counit R) := acounit_surjective ℤ R
+lemma counit_nat_surjective : surjective (counit_nat A) := acounit_surjective ℕ A
+
+@[simp] lemma counit_C (n : ℤ) : counit R (C n) = n := acounit_C _ _
+@[simp] lemma counit_nat_C (n : ℕ) : counit_nat A (C n) = n := acounit_C _ _
+
+variables {R A}
+
+@[simp] lemma counit_X (r : R) : counit R (X r) = r := acounit_X _ _
+@[simp] lemma counit_nat_X (a : A) : counit_nat A (X a) = a := acounit_X _ _
 
 end mv_polynomial

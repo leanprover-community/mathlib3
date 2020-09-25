7436d608c2605d18ec88f9834f7cf80eff704e9c
Scott Morrison <scott.morrison@gmail.com>
2020-09-25 14:26:00 +1000

    chore(category_theory/limits): facts about opposites of limit cones

--- src/category_theory/limits/cones.lean
+++ src/category_theory/limits/cones.lean
@@ -512,8 +512,7 @@ begin
   dsimp,
   rw [comp_id, H.map_comp, is_equivalence.fun_inv_map H, assoc, nat_iso.cancel_nat_iso_hom_left,
       assoc, is_equivalence.inv_fun_id_inv_comp],
-  apply comp_id,
-  -- annoyingly `dsimp, simp` leaves it as `c.π.app j ≫ 𝟙 _ = c.π.app j` instead of closing...
+  dsimp, simp,
 end
 
 /-- `map_cone` is the right inverse to `map_cone_inv`. -/
@@ -527,11 +526,47 @@ end category_theory
 
 namespace category_theory.limits
 
+section
+variables {F : J ⥤ C}
+
+/-- Change a `cocone F` into a `cone F.op`. -/
+@[simps] def cocone.op (c : cocone F) : cone F.op :=
+{ X := op c.X,
+  π :=
+  { app := λ j, (c.ι.app (unop j)).op,
+    naturality' := λ j j' f, has_hom.hom.unop_inj (by tidy) } }
+
+/-- Change a `cone F` into a `cocone F.op`. -/
+@[simps] def cone.op (c : cone F) : cocone F.op :=
+{ X := op c.X,
+  ι :=
+  { app := λ j, (c.π.app (unop j)).op,
+    naturality' := λ j j' f, has_hom.hom.unop_inj (by tidy) } }
+
+/-- Change a `cocone F.op` into a `cone F`. -/
+@[simps] def cocone.unop (c : cocone F.op) : cone F :=
+{ X := unop c.X,
+  π :=
+  { app := λ j, (c.ι.app (op j)).unop,
+    naturality' := λ j j' f, has_hom.hom.op_inj
+    begin dsimp, simp only [comp_id], exact (c.w f.op).symm, end } }
+
+/-- Change a `cone F.op` into a `cocone F`. -/
+@[simps] def cone.unop (c : cone F.op) : cocone F :=
+{ X := unop c.X,
+  ι :=
+  { app := λ j, (c.π.app (op j)).unop,
+    naturality' := λ j j' f, has_hom.hom.op_inj
+    begin dsimp, simp only [id_comp], exact (c.w f.op), end } }
+
+end
+
+section
 variables {F : J ⥤ Cᵒᵖ}
 
--- Here and below we only automatically generate the `@[simp]` lemma for the `X` field,
--- as we can be a simpler `rfl` lemma for the components of the natural transformation by hand.
 /-- Change a cocone on `F.left_op : Jᵒᵖ ⥤ C` to a cocone on `F : J ⥤ Cᵒᵖ`. -/
+-- Here and below we only automatically generate the `@[simp]` lemma for the `X` field,
+-- as we can write a simpler `rfl` lemma for the components of the natural transformation by hand.
 @[simps X] def cone_of_cocone_left_op (c : cocone F.left_op) : cone F :=
 { X := op c.X,
   π := nat_trans.right_op (c.ι ≫ (const.op_obj_unop (op c.X)).hom) }
@@ -566,4 +601,29 @@ by { dsimp [cocone_of_cone_left_op], simp }
 @[simp] lemma cone_left_op_of_cocone_π_app (c : cocone F) (j) :
   (cone_left_op_of_cocone c).π.app j = (c.ι.app (unop j)).unop :=
 by { dsimp [cone_left_op_of_cocone], simp }
+
+end
+
 end category_theory.limits
+
+namespace category_theory.functor
+
+open category_theory.limits
+
+variables {F : J ⥤ C}
+variables {D : Type u'} [category.{v} D]
+
+section
+variables (G : C ⥤ D)
+
+/-- The opposite cocone of the image of a cone is the image of the opposite cocone. -/
+def map_cone_op (t : cone F) : (G.map_cone t).op ≅ (G.op.map_cocone t.op) :=
+cocones.ext (iso.refl _) (by tidy)
+
+/-- The opposite cone of the image of a cocone is the image of the opposite cone. -/
+def map_cocone_op {t : cocone F} : (G.map_cocone t).op ≅ (G.op.map_cone t.op) :=
+cones.ext (iso.refl _) (by tidy)
+
+end
+
+end category_theory.functor

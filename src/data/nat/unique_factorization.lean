924a64c22c5299e62d88577cb9468d50a30e9f0b
Aaron Anderson <awainverse@gmail.com>
2020-09-26 09:45:10 -0700

    ahh fix the import

--- src/data/nat/unique_factorization.lean
+++ src/data/nat/unique_factorization.lean
@@ -4,7 +4,7 @@ Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Aaron Anderson
 -/
 
-import data.nat.associated
+import data.nat.prime
 import ring_theory.unique_factorization_domain
 
 /-#

3863794dbdffacbf8a7aa4923f7fd0838a1da6d0
MadPidgeon <daanvangent@gmail.com>
2020-09-26 17:18:40 +0200

    rewrite fsum for the purpose of configurations

--- src/computability/fsum.lean
+++ src/computability/fsum.lean
@@ -8,13 +8,21 @@ import tactic.basic
 import data.fintype.basic
 
 def quot.lift_on' {α : Type*} [s : setoid α]
-{t : α → Type*} {ht : ∀ a b : α, a ≈ b → t a = t b}
-(f : ∀ a : α, t a) (hf : ∀ a b : α, a ≈ b → f a == f b) : ∀ q : quotient s, quot.lift_on q t ht :=
-λ q, quotient.hrec_on q f hf
+{t : α → Type*} {C : Type* → Type*} {ht : ∀ a b : α, a ≈ b → t a = t b}
+(f : ∀ a : α, C (t a)) (hf : ∀ a b : α, a ≈ b → f a == f b) :
+∀ q : quotient s, C (quot.lift_on q t ht) := λ q, quotient.hrec_on q f hf
 
-def sum.elim' {α β: Type*} {tα : α → Type*} {tβ : β → Type*} (f : ∀ x, tα x) (g : ∀ x, tβ x) :
-∀ x : α ⊕ β, sum.elim tα tβ x := λ x, sum.cases_on x (λ y, f y) (λ y, g y)
+/-def sum.elim' {α β: Type*} {tα : α → Type*} {tβ : β → Type*} (f : ∀ x, tα x) (g : ∀ x, tβ x) :
+∀ x : α ⊕ β, sum.elim tα tβ x := λ x, sum.cases_on x f g-/
 
+def sum.elim' {α β: Type*} {tα : α → Type*} {tβ : β → Type*} {C : Type* → Type*}
+(f : ∀ x, C (tα x)) (g : ∀ x, C (tβ x)) :
+∀ x : α ⊕ β, C (sum.elim tα tβ x) := λ x, sum.cases_on x f g
+
+@[simp] lemma sum.elim_inl' {α β : Type*} {tα : α → Type*} {tβ : β → Type*} {C : Type* → Type*}
+(f : ∀ x, C (tα x)) (g : ∀ x, C (tβ x)) (x : α) : sum.elim' f g (sum.inl x) = f x := rfl
+@[simp] lemma sum.elim_inr' {α β : Type*} {tα : α → Type*} {tβ : β → Type*} {C : Type* → Type*}
+(f : ∀ x, C (tα x)) (g : ∀ x, C (tβ x)) (y : β) : sum.elim' f g (sum.inr y) = g y := rfl
 
 namespace fsum
 section
@@ -64,34 +72,154 @@ def elim_rel {γ : Type*} (f : α ⊕ β → γ) (h : f (sum.inl a) = f (sum.inr
 ∀ x y, fsum_rel x y → f x = f y := by tidy
 def elim_aux {γ : Type*} (f : α ⊕ β → γ) (h : f (sum.inl a) = f (sum.inr b)) : fsum → γ :=
 λ q, quot.lift_on q f (elim_rel f h)
-
 def elim {γ : Type*} (f : α → γ) (g : β → γ) (h : f a = g b) : fsum → γ :=
 elim_aux (sum.elim f g) h
+
+/--/
+def liftl' [hβ : decidable_eq β] (x : α ⊕ β) : α ⊕ β :=
+sum.cases_on x (λ x, sum.inl x) (λ y, ite (y = b) (sum.inl a) (sum.inr y))
+lemma liftl_nb' [hβ : decidable_eq β] (x : α ⊕ β) : liftl' x ≠ sum.inr b :=
+begin
+  cases x; unfold liftl',
+  apply sum.no_confusion,
+  intro h,
+  cases em (x = b) with e e, {
+    rw if_pos e at h,
+    exact sum.no_confusion h,
+  },
+  rw if_neg e at h,
+  apply e,
+  exact sum.inr.inj h,
+end
+
+def liftl [hβ : decidable_eq β] (q : fsum) : α ⊕ β :=
+elim_aux (λ x, liftl' x) begin
+  unfold liftl',
+  rw if_pos,
+  refl,
+end q
+
+lemma liftl_nb [hβ : decidable_eq β] (q : fsum) : liftl q ≠ sum.inr b :=
+begin
+  sorry,
+end-/
+
+def liftr [hα : decidable_eq α] (q : fsum) : α ⊕ β := begin
+  induction q,
+  cases q with x y h,
+  exact ite (x = a) (sum.inr b) (sum.inl x),
+  exact sum.inr y,
+  tidy,
+end
 
 @[simp] lemma elim_inl {γ : Type*} (f : α → γ) (g : β → γ)
 {h : f a = g b} (x : α) : elim f g h (inl x) = f x := rfl
 @[simp] lemma elim_inr {γ : Type*} (f : α → γ) (g : β → γ)
 {h : f a = g b} (y : β) : elim f g h (inr y) = g y := rfl
 
-def elim_rel' {t : α ⊕ β → Type*} {ht : t (sum.inl a) = t (sum.inr b)}
-(f : ∀ x : α ⊕ β, t x) (hf : f (sum.inl a) == f (sum.inr b)) : ∀ x y, fsum_rel x y → f x == f y := by tidy
-def elim_aux' {t : α ⊕ β → Type*} {ht : t (sum.inl a) = t (sum.inr b)}
-(f : ∀ x : α ⊕ β, t x) (hf : f (sum.inl a) == f (sum.inr b)) : ∀ x : fsum, elim_aux t ht x :=
-@quot.lift_on' _ fsum_setoid t (elim_rel t ht) f (@elim_rel' t ht f hf)
+def eliml [hβ : decidable_eq β] {γ : Type*} (f : α → γ) (g : β → γ) : fsum → γ :=
+elim_aux (sum.elim f (λ y, ite (y = b) (f a) (g y))) (by simp)
+def elimr [hα : decidable_eq α] {γ : Type*} (f : α → γ) (g : β → γ) : fsum → γ :=
+elim_aux (sum.elim (λ x, ite (x = a) (g b) (f x)) g) (by simp)
 
-def elim' {t₁ : α → Type*} {t₂ : β → Type*} {ht : t₁ a = t₂ b}
-(f : ∀ x : α, t₁ x) (g : ∀ x : β, t₂ x) (hf : f a == g b) : ∀ x : fsum, elim t₁ t₂ ht x :=
-@elim_aux' (sum.elim t₁ t₂) ht (sum.elim' f g) hf
+set_option pp.proofs true
+
+@[simp] lemma eliml_inl [hβ : decidable_eq β] {γ : Type*} (f : α → γ) (g : β → γ) (x : α):
+eliml f g (inl x) = f x := by refl
+@[simp] lemma elimr_inr [hα : decidable_eq α] {γ : Type*} (f : α → γ) (g : β → γ) (y : β):
+elimr f g (inr y) = g y := by refl
+
+lemma eliml_eq_elim [hβ : decidable_eq β] {γ : Type*} (f : α → γ) (g : β → γ) (h : f a = g b):
+eliml f g = elim f g h := begin
+  unfold eliml elim,
+  suffices h' : (λ (y : β), ite (y = b) (f a) (g y)) = g, {
+    simp [h'],
+    rw proof_irrel h,
+  },
+  ext y,
+  cases em (y = b) with h' h', {
+    rw [if_pos h',h'],
+    exact h,
+  },
+  rw if_neg h',
+end
+
+lemma elimr_eq_elim [hα : decidable_eq α] {γ : Type*} (f : α → γ) (g : β → γ) (h : f a = g b):
+elimr f g = elim f g h := begin
+  unfold elimr elim,
+  suffices h' : (λ (x : α), ite (x = a) (g b) (f x)) = f, {
+    simp [h'],
+    rw proof_irrel h,
+  },
+  ext x,
+  cases em (x = a) with h' h', {
+    rw [if_pos h',h'],
+    symmetry,
+    exact h,
+  },
+  rw if_neg h',
+end
+
+def elim_prod_rel {t : α ⊕ β → Type*} {C : Type* → Type*} {ht : t (sum.inl a) = t (sum.inr b)}
+(f : ∀ x : α ⊕ β, C (t x)) (hf : f (sum.inl a) == f (sum.inr b)) : ∀ x y, fsum_rel x y → f x == f y := by tidy
+def elim_prod_aux {t : α ⊕ β → Type*} {C : Type* → Type*} {ht : t (sum.inl a) = t (sum.inr b)}
+(f : ∀ x : α ⊕ β, C (t x)) (hf : f (sum.inl a) == f (sum.inr b)) : ∀ x : fsum, C (elim_aux t ht x) :=
+@quot.lift_on' _ fsum_setoid t C (elim_rel t ht) f (@elim_prod_rel t C ht f hf)
+def elim_prod {t₁ : α → Type*} {t₂ : β → Type*} {C : Type* → Type*} {ht : t₁ a = t₂ b}
+(f : ∀ x : α, C (t₁ x)) (g : ∀ x : β, C (t₂ x)) (hf : f a == g b) : ∀ x : fsum, C (elim t₁ t₂ ht x) :=
+@elim_prod_aux (sum.elim t₁ t₂) C ht (sum.elim' f g) hf
+
+
+def eliml_prod [hβ : decidable_eq β] {t₁ : α → Type*} {t₂ : β → Type*} {C : Type* → Type*}
+(f : ∀ x : α, C (t₁ x)) (g : ∀ y : β, C (t₂ y)) : ∀ q : fsum, C (eliml t₁ t₂ q) :=
+elim_prod_aux (sum.elim' f (λ y, dite (y = b)
+(λ h, by{ rw if_pos h, exact f a }) (λ h, by{ rw if_neg h, exact g y })))
+(by{ apply heq.symm, simp })
+
+def elimr_prod [hα : decidable_eq α] {t₁ : α → Type*} {t₂ : β → Type*} {C : Type* → Type*}
+(f : ∀ x : α, C (t₁ x)) (g : ∀ y : β, C (t₂ y)) : ∀ q : fsum, C (elimr t₁ t₂ q) :=
+elim_prod_aux (sum.elim' (λ x, dite (x = a)
+(λ h, by{ rw if_pos h, exact g b}) (λ h, by{ rw if_neg h, exact f x})) g)
+(by simp)
+
+
+def extendl_value {γ : Type*} [h : decidable_eq β] (f : α → γ) (c : γ) : fsum → γ :=
+elim f (λ y, ite (y = b) (f a) c) (by {rw if_pos, refl})
+def extendr_value {γ : Type*} [h : decidable_eq α] (g : β → γ) (c : γ) : fsum → γ :=
+elim (λ x, ite (x = a) (g b) c) g (by {rw if_pos, refl})
+
+def extendl {γ : Type*} (f : α → γ) : fsum → γ := elim f (λ y, f a) rfl
+def extendr {γ : Type*} (g : β → γ) : fsum → γ := elim (λ y, g b) g rfl
+
+def extendl_prod {γ : Type*} {t : α → Type*} {C : Type* → Type*} (f : ∀ x : α, C (t x))
+: ∀ q : fsum, C (extendl t q) := elim_prod f (λ y, f a) (congr_arg_heq f rfl)
+def extendr_prod {γ : Type*} {t : β → Type*} {C : Type* → Type*} (g : ∀ y : β, C (t y))
+: ∀ q : fsum, C (extendr t q) := elim_prod (λ x, g b) g (congr_arg_heq g rfl)
+
 
 instance inhabited_fsum : inhabited fsum := ⟨inl a⟩
 instance decidable_eq_fsum [dα : decidable_eq α] [dβ : decidable_eq β] : decidable_eq fsum :=
 @quotient.decidable_eq (α ⊕ β) fsum_setoid fsum_setoid.decidable_rel
-
-instance fintype [hα : fintype α] [hβ : fintype β] [hα' : decidable_eq α] [hβ' : decidable_eq β] :
+instance fintype_fsum [hα : fintype α] [hβ : fintype β] [hα' : decidable_eq α] [hβ' : decidable_eq β] :
 fintype fsum := @quotient.fintype _ (sum.fintype α β) fsum_setoid fsum_setoid.decidable_rel
 
 end
+
+def inl' {α β : Type*} {a : α} {b : β} (x : α) : fsum a b := inl a b x
+def inr' {α β : Type*} {a : α} {b : β} (y : β) : fsum a b := inr a b y
+def extendl' {α β γ : Type*} {a : α} {b : β} (f : α → γ) : fsum a b → γ := extendl a b f
+def extendr' {α β γ : Type*} {a : α} {b : β} (g : β → γ) : fsum a b → γ := extendr a b g
+
+lemma point_eq' {α β : Type*} {a : α} {b : β}: @inl' _ _ a b a = inr' b := point_eq a b
+
 end fsum
+
+def map_rec_codomain {α : Type*} {t₁ t₂ : α → Type*} (ft : ∀ a : α, t₁ a → t₂ a) (f : ∀ a : α, t₁ a)
+: ∀ a : α, t₂ a := λ a, ft a (f a)
+
+def map_rec_domain {α β : Type*} {t₁ : α → Type*} {t₂ : β → Type*} (ft : α → β)
+(h : ∀ a : α, t₁ a = t₂ (ft a)) (f : ∀ b : β, t₂ b) : ∀ a : α, t₁ a := λ a,
+begin rw h a, exact f (ft a), end
 
 
 /-
--- src/computability/tm_composition.lean
+++ src/computability/tm_composition.lean
@@ -6,80 +6,136 @@ Authors: Pim Spelier, Daan van Gent.
 
 import computability.encoding
 import computability.tm_computable
+import computability.fsum
+import data.list.basic
 
-namespace computability
+namespace turing
 
-private def fsum_setoid {α β : Type*} (a : α) (b : β) : setoid (α ⊕ β) := {
-  r := λ u v, sum.cases_on u
-    (λ x, sum.cases_on v
-      (λ y, x = y)
-      (λ y, (x = a) ∧ (y = b)))
-    (λ x, sum.cases_on v
-      (λ y, (x = b) ∧ (y = a))
-      (λ y, x = y)),
-  iseqv := by tidy }
+namespace composition
+noncomputable theory
 
-instance fsum_setoid.decidable_rel {α β : Type*} (a : α) (b : β) [dα : decidable_eq α] [dβ : decidable_eq β]:
-decidable_rel (fsum_setoid a b).r := begin
-  intros x y,
-  cases x; cases y,
-  exact dα x y,
-  exact and.decidable,
-  exact and.decidable,
-  exact dβ x y,
-end
+section computer
+open turing.TM2
+open turing.TM2.stmt
+open fsum
 
-def fsum {α β : Type*} (a : α) (b : β) : Type* := quotient (fsum_setoid a b)
+parameters (tm₂ tm₁ : fin_tm2) (hΓ : tm₂.Γ tm₂.k₀ = tm₁.Γ tm₁.k₁)
+instance dK₁ : _ := tm₁.K_decidable_eq
+instance dK₂ : _ := tm₂.K_decidable_eq
+instance dK : _ := fsum.decidable_eq_fsum tm₂.k₀ tm₁.k₁
+-- instance temp : fintype (fintype (fsum tm₂.k₀ tm₁.k₁)) := sorry
+-- instance temp' : fintype (fintype (tm₂.Λ ⊕ tm₁.Λ)) := sorry
 
-def fsum.inl {α β : Type*} {a : α} {b : β} (x : α) : fsum a b := quotient.mk' (sum.inl x)
-def fsum.inr {α β : Type*} {a : α} {b : β} (y : β) : fsum a b := quotient.mk' (sum.inr y)
+private def embed_tm₂_stmt : fin_tm2.stmt tm₂ →
+stmt (elim _ _ tm₂.Γ tm₁.Γ hΓ) (tm₂.Λ ⊕ tm₁.Λ) (fsum tm₂.initial_state tm₁.initial_state)
+| (push k f q) := push (inl' k) (extendl' f) (embed_tm₂_stmt q)
+| (peek k f q) := peek (inl' k) (extendl' (λ s g, inl' (f s g))) (embed_tm₂_stmt q)
+| (pop k f q) := pop (inl' k) (extendl' (λ s g, inl' (f s g))) (embed_tm₂_stmt q)
+| (load f q) := load (extendl' (inl' ∘ f)) (embed_tm₂_stmt q)
+| (branch f q r) := branch (extendl' f) (embed_tm₂_stmt q) (embed_tm₂_stmt r)
+| (goto f) := goto (extendl' (sum.inl ∘ f))
+| (halt) := halt
 
-def fsum.elim {α β γ : Type*} {a : α} {b : β} (f : α → γ) (g : β → γ) (h : f a = g b): fsum a b → γ := λ x,
-begin
-  apply quot.lift_on x (sum.elim f g) (λ u v h',
-  begin
-    cases u; cases v,
-    {
-      exact congr_arg (sum.elim f g) (congr_arg sum.inl h'),
-    }, {
-      suffices h'' : (u = a) ∧ (v = b),
-      simp only [h, h'', sum.elim_inl, sum.elim_inr],
-      exact h',
-    }, {
-      suffices h'' : (u = b) ∧ (v = a),
-      simp only [h, h'', sum.elim_inl, sum.elim_inr],
-      exact h',
-    }, {
-      exact congr_arg (sum.elim f g) (congr_arg sum.inr h'),
-    }
-  end),
-end
+private def embed_tm₁_stmt : fin_tm2.stmt tm₁ →
+stmt (elim _ _ tm₂.Γ tm₁.Γ hΓ) (tm₂.Λ ⊕ tm₁.Λ) (fsum tm₂.initial_state tm₁.initial_state)
+| (push k f q) := push (inr' k) (extendr' f) (embed_tm₁_stmt q)
+| (peek k f q) := peek (inr' k) (extendr' (λ s g, inr' (f s g))) (embed_tm₁_stmt q)
+| (pop k f q) := pop (inr' k) (extendr' (λ s g, inr' (f s g))) (embed_tm₁_stmt q)
+| (load f q) := load (extendr' (inr' ∘ f)) (embed_tm₁_stmt q)
+| (branch f q r) := branch (extendr' f) (embed_tm₁_stmt q) (embed_tm₁_stmt r)
+| (goto f) := goto (extendr' (sum.inr ∘ f))
+| (halt) := goto (λ s, sum.inl tm₂.main )
 
-instance fsum.inhabited {α β : Type*} (a : α) (b : β): inhabited (fsum a b) := ⟨fsum.inl a⟩
-instance fsum.decidable_eq {α β : Type*} [dα : decidable_eq α] [dβ : decidable_eq β] (a : α) (b : β): decidable_eq (fsum a b) :=
-@quotient.decidable_eq (α ⊕ β) (fsum_setoid a b) (fsum_setoid.decidable_rel a b)
-instance fsum.fintype {α β : Type*} (a : α) (b : β) [hα : fintype α] [hβ : fintype β]
-[hα' : decidable_eq α] [hβ' : decidable_eq β]: fintype (fsum a b) :=
-@quotient.fintype _ (sum.fintype α β) (fsum_setoid a b) (fsum_setoid.decidable_rel a b)
+-- class findeq (α : Type*) := ( fin : fintype α ) ( deq : decidable_eq α )
 
-noncomputable
-
-
-
-def fin_tm_comp (tm₂ tm₁ : fin_tm2) (hΓ : tm₂.Γ tm₂.k₀ = tm₁.Γ tm₁.k₁) : fin_tm2 := {
+def computer : fin_tm2 := {
   K := fsum tm₂.k₀ tm₁.k₁,
-  K_decidable_eq := @fsum.decidable_eq _ _ (tm₂.K_decidable_eq) (tm₁.K_decidable_eq) _ _,
-  k₀ := fsum.inr tm₁.k₀,
-  k₁ := fsum.inl tm₂.k₁,
-  Γ := fsum.elim tm₂.Γ tm₁.Γ hΓ,
+  K_decidable_eq := dK,
+  K_fin := @fsum.fintype_fsum _ _ _ _ tm₂.K_fin tm₁.K_fin tm₂.K_decidable_eq tm₁.K_decidable_eq,
+  k₀ := inr' tm₁.k₀,
+  k₁ := inl' tm₂.k₁,
+  Γ := elim _ _ tm₂.Γ tm₁.Γ hΓ,
   Λ := tm₂.Λ ⊕ tm₁.Λ,
   main := sum.inr tm₁.main,
+  Λ_fin := @sum.fintype tm₂.Λ tm₁.Λ tm₂.Λ_fin tm₁.Λ_fin,
   σ := fsum tm₂.initial_state tm₁.initial_state,
-  initial_state := fsum.inr tm₁.initial_state,
-  σ_fin := @fsum.fintype _ _ _ _ tm₂.σ_fin tm₁.σ_fin (classical.dec_eq tm₂.σ) (classical.dec_eq tm₁.σ), -- Horror!
+  initial_state := inr' tm₁.initial_state,
+  σ_fin := @fsum.fintype_fsum _ _ _ _ tm₂.σ_fin tm₁.σ_fin (classical.dec_eq tm₂.σ) (classical.dec_eq tm₁.σ), -- Horror!
   Γk₀_fin := tm₁.Γk₀_fin,
-  M := λ (l : tm₂.Λ ⊕ tm₁.Λ), sorry
+  M := sum.elim (embed_tm₂_stmt ∘ tm₂.M) (embed_tm₁_stmt ∘ tm₁.M)
 }
 
+def embed_tm₁_cfg (c : fin_tm2.cfg tm₁): fin_tm2.cfg computer := {
+  l := (option.map sum.inr) c.l,
+  var := inr' c.var,
+  stk := begin
+    let h := elimr_prod tm₂.k₀ tm₁.k₁ (λ k, @list.nil (tm₂.Γ k)) c.stk,
+    rw elimr_eq_elim tm₂.k₀ tm₁.k₁ tm₂.Γ tm₁.Γ hΓ at h,
+    exact h,
+  end
+}
 
-end computability
+def embed_tm₂_cfg (c : fin_tm2.cfg tm₂): fin_tm2.cfg computer := {
+  l := (option.map sum.inl) c.l,
+  var := inl' c.var,
+  stk := begin
+    let h := eliml_prod tm₂.k₀ tm₁.k₁ c.stk (λ k, @list.nil (tm₁.Γ k)),
+    rw eliml_eq_elim tm₂.k₀ tm₁.k₁ tm₂.Γ tm₁.Γ hΓ at h,
+    exact h,
+  end
+}
+
+end computer
+
+open computability
+
+lemma center_stack {α β γ : Type} (eα : fin_encoding α) (eβ : fin_encoding β) (eγ : fin_encoding γ)
+(g : β → γ) (f : α → β) (hg : tm2_computable eβ eγ g) (hf : tm2_computable eα eβ f) :
+hg.to_tm2_computable_aux.tm.Γ hg.to_tm2_computable_aux.tm.k₀ = hf.to_tm2_computable_aux.tm.Γ hf.to_tm2_computable_aux.tm.k₁ :=
+begin
+
+end
+
+theorem X {α β γ : Type} (eα : fin_encoding α) (eβ : fin_encoding β) (eγ : fin_encoding γ)
+(g : β → γ) (f : α → β) (hg : tm2_computable eβ eγ g) (hf : tm2_computable eα eβ f) :
+tm2_computable eα eγ (g ∘ f) := {
+  tm := computer hg.tm hf.tm begin end, -- !!!
+  input_alphabet := hf.input_alphabet,
+  output_alphabet := hg.output_alphabet,
+  outputs_fun := λ a, {
+    steps := (hg.outputs_fun (f a)).steps + (hf.outputs_fun a).steps,
+    evals_in_steps := begin
+      --suffices h : ((flip bind f)^[(hg.outputs_fun (f a)).steps] a) = some (f a)
+
+      let tm₁ := hf.tm,
+      let tm₂ := hg.tm,
+      have hΓ : tm₂.Γ tm₂.k₀ = tm₁.Γ tm₁.k₁ := sorry,
+      let tm := computer tm₂ tm₁ hΓ,
+      let alph1 := hf.input_alphabet,
+      let alph2 := hf.output_alphabet,
+      let alph3 := hg.output_alphabet,
+
+      /-have hΓ' : hf.to_computable_by_tm2_aux.tm.Γ hf.to_computable_by_tm2_aux.tm.k₁ = tm.Γ tm.k₁ :=
+      begin
+        change tm₁.Γ tm₁.k₁ = tm.Γ tm.k₁,
+        -- have fsum.elim tm₂.Γ tm₁.Γ hΓ (fsum.inl tm₂.k₁) = tm.Γ tm.k₁,
+
+        --change fsum.elim hg.tm.Γ hf.tm.Γ sorry
+        --exact fsum.elim_inl hf.tm.Γ _ _ tm.k₁,
+      end-/
+      let la := (list.map alph1.inv_fun (eα.encode a)),
+      let lb := (list.map alph2.inv_fun (eβ.encode (f a))),
+      let lc := (list.map alph3.inv_fun (eγ.encode ((g ∘ f) a))),
+
+      have h : evals_to tm.step (init_list tm la) ((option.map (halt_list tm)) (some lb)),
+      sorry,
+      --evals_to tm.step (init_list tm l) ((option.map (halt_list tm)) l')
+    end
+}}
+
+
+--lemma X (l : list (computer.Γ computer.k₀)) (l' : option (list (computer.Γ computer.k₁))) (o₂ : tm2_outputs tm₂ l l' )
+--(l : list (tm.Γ tm.k₀)) (l' : option (list (tm.Γ tm.k₁))) :=
+
+end composition
+end turing

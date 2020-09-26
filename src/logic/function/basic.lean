c2f896fec2dea503c961f87d82a92d6e1976a815
Floris van Doorn <fpvdoorn@gmail.com>
2020-09-26 07:58:50 +0000

    feat(data/set): add some lemmas (#4263)
    Some lemmas about sets, mostly involving disjointness
    I also sneaked in the lemma `(λ x : α, y) = const α y` which is useful to rewrite with.

--- src/logic/function/basic.lean
+++ src/logic/function/basic.lean
@@ -26,6 +26,8 @@ variables {α β γ : Sort*} {f : α → β}
 lemma comp_apply {α : Sort u} {β : Sort v} {φ : Sort w} (f : β → φ) (g : α → β) (a : α) :
   (f ∘ g) a = f (g a) := rfl
 
+lemma const_def {y : β} : (λ x : α, y) = const α y := rfl
+
 @[simp] lemma const_apply {y : β} {x : α} : const α y x = y := rfl
 
 @[simp] lemma const_comp {f : α → β} {c : γ} : const β c ∘ f = const α c := rfl

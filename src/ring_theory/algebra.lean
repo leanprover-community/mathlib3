d0b594766906c0e3ca9082228fffd8c9e0ff5435
Oliver Nash <oliver.nash@gmail.com>
2020-09-26 19:15:50 +0000

    feat(algebra/universal_enveloping_algebra): construction of universal enveloping algebra and its universal property (#4041)
    ## Main definitions
    
      * `universal_enveloping_algebra`
      * `universal_enveloping_algebra.algebra`
      * `universal_enveloping_algebra.lift`
      * `universal_enveloping_algebra.ι_comp_lift`
      * `universal_enveloping_algebra.lift_unique`
      * `universal_enveloping_algebra.hom_ext`
    
    Co-authored-by: Eric Wieser <wieser.eric@gmail.com>

--- src/ring_theory/algebra.lean
+++ src/ring_theory/algebra.lean
@@ -3,6 +3,7 @@ Copyright (c) 2018 Kenny Lau. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Kenny Lau, Yury Kudryashov
 -/
+import tactic.nth_rewrite
 import data.matrix.basic
 import linear_algebra.tensor_product
 import ring_theory.subring

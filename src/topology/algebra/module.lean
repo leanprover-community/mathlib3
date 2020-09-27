698cd3c85a2e31e4c3e0fd149e2a93113cee0071
Floris van Doorn <fpvdoorn@gmail.com>
2020-09-27 16:44:20 -0400

    Update src/topology/algebra/module.lean
    Co-authored-by: Yury G. Kudryashov <urkud@urkud.name>

--- src/topology/algebra/module.lean
+++ src/topology/algebra/module.lean
@@ -153,8 +153,8 @@ lemma is_open_map_smul_of_ne_zero (ha : a ≠ 0) : is_open_map (λ (x : M), a �
 
 /-- `smul` is a closed map in the second argument.
 
-The lemma that `smul` is a closed map in the first argument (for a complete normed space) is
-`is_closed_map_smul_left` in `analysis.normed_space.finite_dimension`. -/
+The lemma that `smul` is a closed map in the first argument (for a normed space over a complete
+normed field) is `is_closed_map_smul_left` in `analysis.normed_space.finite_dimension`. -/
 lemma is_closed_map_smul_of_ne_zero (ha : a ≠ 0) : is_closed_map (λ (x : M), a • x) :=
 (homeomorph.smul_of_ne_zero ha).is_closed_map
 

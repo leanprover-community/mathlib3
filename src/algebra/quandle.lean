aced97df0ab890f73c44725292749a489dfd2776
Kyle Miller <kmill31415@gmail.com>
2020-09-26 21:52:31 -0700

    actually, data.equiv.mul_add imports all that's needed

--- src/algebra/quandle.lean
+++ src/algebra/quandle.lean
@@ -3,7 +3,6 @@ Copyright (c) 2020 Kyle Miller. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Kyle Miller
 -/
-import algebra.group.basic
 import data.zmod.basic
 import data.equiv.mul_add
 import tactic.group

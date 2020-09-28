3e3dfff2be819c21c8b486a9f69fec09661bdb2d
Rob Lewis <rob.y.lewis@gmail.com>
2020-09-28 14:12:02 +0200

    wip

--- src/ring_theory/witt_vector/witt_vector_preps.lean
+++ src/ring_theory/witt_vector/witt_vector_preps.lean
@@ -13,8 +13,10 @@ import ring_theory.multiplicity
 import algebra.invertible
 import number_theory.basic
 import group_theory.order_of_element
-import field_theory.finite
+import field_theory.finite.basic
 import field_theory.mv_polynomial
+import data.mv_polynomial.expand
+import data.mv_polynomial.counit
 
 universes u v w u₁
 
@@ -29,39 +31,9 @@ variables (σ R A : Type*) [comm_semiring R] [comm_semiring A]
 
 open_locale big_operators
 
-lemma C_dvd_iff_dvd_coeff {σ : Type*} {R : Type*} [comm_ring R]
-  (r : R) (φ : mv_polynomial σ R) :
-  C r ∣ φ ↔ ∀ i, r ∣ (φ.coeff i) :=
-begin
-  split,
-  { rintros ⟨φ, rfl⟩ c, rw coeff_C_mul, apply dvd_mul_right },
-  { intro h,
-    choose c hc using h,
-    classical,
-    let c' : (σ →₀ ℕ) → R := λ i, if i ∈ φ.support then c i else 0,
-    let ψ : mv_polynomial σ R := ∑ i in φ.support, monomial i (c' i),
-    use ψ,
-    apply mv_polynomial.ext, intro i,
-    simp only [coeff_C_mul, coeff_sum, coeff_monomial],
-    rw [finset.sum_eq_single i, if_pos rfl],
-    { dsimp [c'], split_ifs with hi hi,
-      { rw hc },
-      { rw finsupp.not_mem_support_iff at hi, rwa [mul_zero] } },
-    { intros j hj hji, convert if_neg hji },
-    { intro hi, rw [if_pos rfl], exact if_neg hi } }
-end
 
 -- Johan: why the hack does ring_hom.ker not exist!!!
 -- Rob: it does now, why do you ask here?
-
-lemma C_dvd_iff_map_hom_eq_zero {σ : Type*} {R : Type*} {S : Type*} [comm_ring R] [comm_ring S]
-  (q : R →+* S) (r : R) (hr : ∀ r' : R, q r' = 0 ↔ r ∣ r')
-  (φ : mv_polynomial σ R) :
-  C r ∣ φ ↔ map q φ = 0 :=
-begin
-  rw [C_dvd_iff_dvd_coeff, mv_polynomial.ext_iff],
-  simp only [coeff_map, ring_hom.coe_of, coeff_zero, hr],
-end
 
 lemma C_dvd_iff_zmod {σ : Type*} (n : ℕ) (φ : mv_polynomial σ ℤ) :
   C (n:ℤ) ∣ φ ↔ map (int.cast_ring_hom (zmod n)) φ = 0 :=
@@ -114,143 +86,13 @@ end
 
 end monadic_stuff
 
-/-- Expand the polynomial by a factor of p, so `∑ aₙ xⁿ` becomes `∑ aₙ xⁿᵖ`. -/
--- this definition should also work for non-commutative `R`
-noncomputable def expand (p : ℕ) : mv_polynomial σ R →ₐ[R] mv_polynomial σ R :=
-{ commutes' := λ r, eval₂_hom_C _ _ _,
-  .. (eval₂_hom C (λ i, (X i) ^ p) : mv_polynomial σ R →+* mv_polynomial σ R) }
 
-@[simp] lemma expand_C (p : ℕ) (r : R) : expand p (C r : mv_polynomial σ R) = C r :=
-eval₂_hom_C _ _ _
-
-@[simp] lemma expand_X (p : ℕ) (i : σ) : expand p (X i : mv_polynomial σ R) = (X i) ^ p :=
-eval₂_hom_X' _ _ _
-
-@[simp] lemma expand_monomial (p : ℕ) (d : σ →₀ ℕ) (r : R) :
-  expand p (monomial d r) = C r * ∏ i in d.support, ((X i) ^ p) ^ (d i) :=
-bind₁_monomial _ _ _
-
-lemma expand_one_apply (f : mv_polynomial σ R) : expand 1 f = f :=
-by simp only [expand, bind₁_X_left, alg_hom.id_apply, ring_hom.to_fun_eq_coe,
-  eval₂_hom_C_left, alg_hom.coe_to_ring_hom, pow_one, alg_hom.coe_mk]
-
-@[simp] lemma expand_one : expand 1 = alg_hom.id R (mv_polynomial σ R) :=
-by { ext1 f, rw [expand_one_apply, alg_hom.id_apply] }
-
-lemma expand_comp_bind₁ (p : ℕ) (f : σ → mv_polynomial τ R) :
-  (expand p).comp (bind₁ f) = bind₁ (λ i, expand p (f i)) :=
-by { apply alg_hom_ext, intro i, simp only [alg_hom.comp_apply, bind₁_X_right], }
-
-lemma expand_bind₁ (p : ℕ) (f : σ → mv_polynomial τ R) (φ : mv_polynomial σ R) :
-  expand p (bind₁ f φ) = bind₁ (λ i, expand p (f i)) φ :=
-by rw [← alg_hom.comp_apply, expand_comp_bind₁]
-
-section
-variables {S : Type*} [comm_semiring S]
-
-@[simp]
-lemma map_expand (f : R →+* S) (p : ℕ) (φ : mv_polynomial σ R) :
-  map f (expand p φ) = expand p (map f φ) :=
-by simp [expand, map_bind₁]
-
-@[simp]
-lemma rename_expand (f : σ → τ) (p : ℕ) (φ : mv_polynomial σ R) :
-  rename f (expand p φ) = expand p (rename f φ) :=
-by simp [expand, bind₁_rename, rename_bind₁]
-
-@[simp] lemma rename_comp_expand (f : σ → τ) (p : ℕ) :
-  (rename f).comp (expand p) = (expand p).comp (rename f : mv_polynomial σ R →ₐ[R] mv_polynomial τ R) :=
-by { ext1 φ, simp only [rename_expand, alg_hom.comp_apply] }
-
-open_locale classical
-
-lemma vars_rename {τ} (f : σ → τ) (φ : mv_polynomial σ R) :
-  (rename f φ).vars ⊆ (φ.vars.image f) :=
-begin
-  -- I guess a higher level proof might be shorter
-  -- should we prove `degrees_rename` first?
-  intros i,
-  rw [mem_vars, finset.mem_image],
-  rintro ⟨d, hd, hi⟩,
-  simp only [exists_prop, mem_vars],
-  contrapose! hd,
-  rw [rename_eq],
-  rw [finsupp.not_mem_support_iff],
-  simp only [finsupp.map_domain, finsupp.sum_apply, finsupp.single_apply],
-  rw [finsupp.sum, finset.sum_eq_zero],
-  intros d' hd',
-  split_ifs with H, swap, refl,
-  subst H,
-  rw [finsupp.mem_support_iff, finsupp.sum_apply] at hi,
-  contrapose! hi,
-  rw [finsupp.sum, finset.sum_eq_zero],
-  intros j hj,
-  rw [finsupp.single_apply, if_neg],
-  apply hd,
-  exact ⟨d', hd', hj⟩
-end
-
-
-end
 
 section rename
 open function
 
-@[simp]
-lemma coeff_rename_map_domain (f : σ → τ) (hf : injective f) (φ : mv_polynomial σ R) (d : σ →₀ ℕ) :
-  (rename f φ).coeff (d.map_domain f) = φ.coeff d :=
-begin
-  apply induction_on' φ,
-  { intros u r,
-    rw [rename_monomial, coeff_monomial, coeff_monomial],
-    simp only [(finsupp.map_domain_injective hf).eq_iff],
-    split_ifs; refl, },
-  { intros, simp only [*, alg_hom.map_add, coeff_add], }
-end
-
-lemma coeff_rename_eq_zero (f : σ → τ) (φ : mv_polynomial σ R) (d : τ →₀ ℕ)
-  (h : ∀ u : σ →₀ ℕ, u.map_domain f ≠ d) :
-  (rename f φ).coeff d = 0 :=
-begin
-  apply induction_on' φ,
-  { intros u r,
-    rw [rename_monomial, coeff_monomial],
-    split_ifs,
-    { exact (h _ ‹_›).elim },
-    { refl } },
-  { intros,  simp only [*, alg_hom.map_add, coeff_add, add_zero], }
-end
-
-lemma coeff_rename_ne_zero (f : σ → τ) (φ : mv_polynomial σ R) (d : τ →₀ ℕ)
-  (h : (rename f φ).coeff d ≠ 0) :
-  ∃ u : σ →₀ ℕ, u.map_domain f = d :=
-by { contrapose! h, apply coeff_rename_eq_zero _ _ _ h }
 
 end rename
-
-section
-open_locale classical
-variables {S : Type*} [comm_semiring S]
-
-lemma eval₂_hom_congr' {f₁ f₂ : R →+* S} {g₁ g₂ : σ → S} {p₁ p₂ : mv_polynomial σ R} :
-  f₁ = f₂ → (∀ i, i ∈ p₁.vars → i ∈ p₂.vars → g₁ i = g₂ i) → p₁ = p₂ →
-   eval₂_hom f₁ g₁ p₁ = eval₂_hom f₂ g₂ p₂ :=
-begin
-  rintro rfl h rfl,
-  rename [p₁ p, f₁ f],
-  rw p.as_sum,
-  simp only [ring_hom.map_sum, eval₂_hom_monomial],
-  apply finset.sum_congr rfl,
-  intros d hd,
-  congr' 1,
-  simp only [finsupp.prod],
-  apply finset.prod_congr rfl,
-  intros i hi,
-  have : i ∈ p.vars, { rw mem_vars, exact ⟨d, hd, hi⟩ },
-  rw h i this this,
-end
-
-end
 
 section move_this
 
@@ -284,10 +126,6 @@ namespace finset
 
 variables {α : Type*} [fintype α]
 
-lemma eq_univ_of_card (s : finset α) (hs : s.card = fintype.card α) :
-  s = univ :=
-eq_of_subset_of_card_le (subset_univ _) $ by rw [hs, card_univ]
-
 end finset
 
 namespace function
@@ -310,37 +148,7 @@ namespace fintype
 variables {α β : Type*} [fintype α] [fintype β]
 open function finset
 
-lemma bijective_iff_injective_and_card (f : α → β) :
-  bijective f ↔ injective f ∧ card α = card β :=
-begin
-  split,
-  { intro h, exact ⟨h.1, fintype.card_congr (equiv.of_bijective f h)⟩, },
-  { rintro ⟨hf, h⟩,
-    refine ⟨hf, _⟩,
-    let s := finset.univ.map ⟨f, hf⟩,
-    have hs : s = univ := s.eq_univ_of_card (by rw [card_map, card_univ, h]),
-    intro b,
-    suffices : b ∈ s,
-    { rw mem_map at this, rcases this with ⟨a, -, ha⟩, exact ⟨a, ha⟩ },
-    rw [hs],
-    exact mem_univ _ }
-end
 
-lemma bijective_iff_surjective_and_card (f : α → β) :
-  bijective f ↔ surjective f ∧ card α = card β :=
-begin
-  split,
-  { intro h, exact ⟨h.2, fintype.card_congr (equiv.of_bijective f h)⟩, },
-  { rintro ⟨hf, h⟩,
-    refine ⟨_, hf⟩,
-    apply injective_of_inj_on,
-    rintro x - y - hxy,
-    apply inj_on_of_surj_on_of_card_le
-      (λ a _, f a)
-      (λ b _, mem_univ _) _ _ (mem_univ x) (mem_univ y) (by simpa),
-    { rintro b -, obtain ⟨a, rfl⟩ := hf b, exact ⟨a, mem_univ _, rfl⟩ },
-    { rw [card_univ, card_univ, h] } }
-end
 
 end fintype
 
@@ -381,12 +189,6 @@ begin
   intro h, ext, apply h,
 end
 
-/-- The unique ring isomorphism between `zmod n` and a ring `R`
-of characteristic `n` and cardinality `n`. -/
-noncomputable def zmod.ring_equiv [fintype R] [char_p R n] (hn : fintype.card R = n) :
-  zmod n ≃+* R :=
-ring_equiv.of_bijective _ (zmod.cast_hom_bij _  _ hn)
-
 instance zmod.ring_equiv_subsingleton : subsingleton (zmod n ≃+* R) :=
 ⟨λ f g, by { rw ring_equiv.coe_ring_hom_inj, apply ring_hom.ext_zmod _ _ }⟩
 
@@ -397,21 +199,6 @@ begin
   simpa only [mul_one, nsmul_eq_mul]
 end
 
-lemma char_p_of_ne_zero [fintype R] (hn : fintype.card R = n) (hR : ∀ i < n, (i : R) = 0 → i = 0) :
-  char_p R n :=
-{ cast_eq_zero_iff :=
-  begin
-    have H : (n : R) = 0, by { rw [← hn, cast_card_eq_zero] },
-    intros k,
-    split,
-    { intro h,
-      rw [← nat.mod_add_div k n, nat.cast_add, nat.cast_mul, H, zero_mul, add_zero] at h,
-      rw nat.dvd_iff_mod_eq_zero,
-      apply hR _ (nat.mod_lt _ _) h,
-      rw [← hn, gt, fintype.card_pos_iff],
-      exact ⟨0⟩, },
-    { rintro ⟨k, rfl⟩, rw [nat.cast_mul, H, zero_mul], }
-  end }
 
 lemma char_p_of_prime_pow_ne_zero [fintype R] (p : ℕ) [hp : fact p.prime] (n : ℕ) (hn : fintype.card R = p ^ n)
   (hR : ∀ i ≤ n, (p ^ i : R) = 0 → i = n) :
@@ -464,39 +251,6 @@ open function
 
 variables (A B R : Type*) [comm_semiring A] [comm_semiring B] [comm_ring R] [algebra A B]
 
-/-- `mv_polynomial.acounit R A` is the natural surjective algebra homomorphism
-`mv_polynomial A R →ₐ[R] A` obtained by `X a ↦ a`.
-
-See `mv_polynomial.counit` for the “absolute” variant with `R = ℤ`,
-and `mv_polynomial.counit_nat` for the “absolute” variant with `R = ℕ`. -/
-noncomputable def acounit : mv_polynomial B A →ₐ[A] B :=
-aeval id
-
-lemma acounit_surjective : surjective (acounit A B) :=
-λ a, ⟨X a, eval₂_hom_X' _ _ _⟩
-
-/-- `mv_polynomial.counit R` is the natural surjective ring homomorphism
-`mv_polynomial R ℤ →+* R` obtained by `X r ↦ r`.
-
-See `mv_polynomial.acounit` for a “relative” variant for algebras over a base ring,
-and `mv_polynomial.counit_nat` for the “absolute” variant with `R = ℕ`. -/
-noncomputable def counit : mv_polynomial R ℤ →+* R :=
-acounit ℤ R
-
-lemma counit_surjective : surjective (counit R) :=
-acounit_surjective ℤ R
-
-/-- `mv_polynomial.counit_nat R` is the natural surjective ring homomorphism
-`mv_polynomial R ℕ →+* R` obtained by `X r ↦ r`.
-
-See `mv_polynomial.acounit` for a “relative” variant for algebras over a base ring
-and `mv_polynomial.counit` for the “absolute” variant with `R = ℤ`. -/
-noncomputable def counit_nat : mv_polynomial A ℕ →+* A :=
-acounit ℕ A
-
-lemma counit_nat_surjective : surjective (counit_nat A) :=
-acounit_surjective ℕ A
-
 end
 end mv_polynomial
 
@@ -526,20 +280,5 @@ lemma mv_polynomial.expand_zmod [fact p.prime] (φ : mv_polynomial σ (zmod p)) 
   expand p φ = φ^p :=
 (mv_polynomial.frobenius_zmod _).symm
 
--- move this
-instance zmod.algebra (R : Type*) [comm_ring R] [char_p R p] : algebra (zmod p) R :=
-ring_hom.to_algebra (zmod.cast_hom (dvd_refl p) R)
-
 end zmod
-
-lemma rat.coe_nat_div (a b : ℕ) (h : b ∣ a) : ((a / b : ℕ) : ℚ) = (a / b : ℚ) :=
-begin
-  rcases h with ⟨c, rfl⟩,
-  simp only [dvd_mul_right, nat.cast_dvd_char_zero],
-end
-
-@[simp]
-lemma ideal.mem_bot {R : Type*} [comm_ring R] {x : R} : x ∈ (⊥ : ideal R) ↔ x = 0 :=
-submodule.mem_bot _
-
 -- ### end FOR_MATHLIB

3986e978fd3b370f0d280de6e0390d3ec0e051fc
Oliver Nash <oliver.nash@gmail.com>
2020-09-28 05:25:33 +0000

    chore(algebra/lie): group Lie algebra files together in their own directory (#4288)

--- src/algebra/lie/classical.lean
+++ src/algebra/lie/classical.lean
@@ -4,7 +4,7 @@ Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Oliver Nash
 -/
 import algebra.invertible
-import algebra.lie_algebra
+import algebra.lie.basic
 import linear_algebra.matrix
 
 /-!

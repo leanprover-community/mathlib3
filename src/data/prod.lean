155ccd9bd282fc4aa9e90b3c25f4e4b58cff1dfd
Rob Lewis <rob.y.lewis@gmail.com>
2020-09-29 12:12:12 +0200

    feat(data/prod): mk injective lemmas

--- src/data/prod.lean
+++ src/data/prod.lean
@@ -38,6 +38,14 @@ funext $ map_snd f g
 @[simp] theorem mk.inj_iff {a₁ a₂ : α} {b₁ b₂ : β} : (a₁, b₁) = (a₂, b₂) ↔ (a₁ = a₂ ∧ b₁ = b₂) :=
 ⟨prod.mk.inj, by cc⟩
 
+lemma mk.inj_left {α β : Type*} (a : α) :
+  function.injective (prod.mk a : β → α × β) :=
+by { intros b₁ b₂ h, simpa only [true_and, prod.mk.inj_iff, eq_self_iff_true] using h }
+
+lemma mk.inj_right {α β : Type*} (b : β) :
+  function.injective (λ a, prod.mk a b : α → α × β) :=
+by { intros b₁ b₂ h, by simpa only [and_true, eq_self_iff_true, mk.inj_iff] using h }
+
 lemma ext_iff {p q : α × β} : p = q ↔ p.1 = q.1 ∧ p.2 = q.2 :=
 by rw [← @mk.eta _ _ p, ← @mk.eta _ _ q, mk.inj_iff]
 

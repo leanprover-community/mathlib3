7b23ea4c4c7b5bb4ecdbe53324e0a37ae456113e
Rob Lewis <Rob.y.lewis@gmail.com>
2020-09-30 21:29:38 +0200

    Update archive/imo/imo1959_q1.lean

--- archive/imo/imo1959_q1.lean
+++ archive/imo/imo1959_q1.lean
@@ -20,7 +20,7 @@ as saying the numerator and denominator are relatively prime.
 lemma calculation (n k : ℕ) (h1 : k ∣ 21 * n + 4) (h2 : k ∣ 14 * n + 3) : k ∣ 1 :=
 have h3 : k ∣ 2 * (21 * n + 4), from dvd_mul_of_dvd_right h1 2,
 have h4 : k ∣ 3 * (14 * n + 3), from dvd_mul_of_dvd_right h2 3,
-have h5 : 3 * (14 * n + 3) = 2 * (21 * n + 4) + 1 , by ring,
+have h5 : 3 * (14 * n + 3) = 2 * (21 * n + 4) + 1, by ring,
 (nat.dvd_add_right h3).mp (h5 ▸ h4)
 
 theorem imo1959_q1 : ∀ n : ℕ, coprime (21 * n + 4) (14 * n + 3) :=

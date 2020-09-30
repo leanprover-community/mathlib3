054cd8cf3c38eebb2eaafe0f6b947ee0fc0e1de6
Kevin Lacker <lacker@gmail.com>
2020-09-30 12:15:55 -0700

    linarith -> ring

--- archive/imo/imo1959_q1.lean
+++ archive/imo/imo1959_q1.lean
@@ -4,7 +4,7 @@ Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Kevin Lacker
 -/
 
-import tactic.linarith
+import tactic.ring
 
 open nat
 
@@ -20,7 +20,7 @@ as saying the numerator and denominator are relatively prime.
 lemma calculation (k n : ℕ) (h1 : k ∣ 21 * n + 4) (h2 : k ∣ 14 * n + 3) : k ∣ 1 :=
 have h3 : k ∣ 2 * (21 * n + 4), from dvd_mul_of_dvd_right h1 2,
 have h4 : k ∣ 3 * (14 * n + 3), from dvd_mul_of_dvd_right h2 3,
-have h5 : 2 * (21 * n + 4) + 1 = 3 * (14 * n + 3), by linarith,
+have h5 : 2 * (21 * n + 4) + 1 = 3 * (14 * n + 3), by ring,
 (nat.dvd_add_right h3).mp (eq.subst h5.symm h4)
 
 theorem imo1959_q1 : ∀ n : ℕ, coprime (21 * n + 4) (14 * n + 3) :=

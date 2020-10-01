0a7555df69f84ab6bebda628299c0cffb6665a95
Johan Commelin <johan@commelin.net>
2020-10-01 11:55:41 +0000

    process review comments

--- src/data/mv_polynomial/invertible.lean
+++ src/data/mv_polynomial/invertible.lean
@@ -5,7 +5,7 @@ Authors: Johan Commelin and Robert Y. Lewis
 -/
 
 import data.mv_polynomial.basic
-import algebra.invertible
+import ring_theory.algebra_tower
 
 /-!
 # Invertible polynomials
@@ -14,14 +14,6 @@ This file is a stub containing some basic facts about invertible elements in the
 -/
 
 open mv_polynomial
-
-/-- A natural number that is invertible when coerced to `R` is also invertible
-when coerced to any `R`-algebra. -/
-def invertible_algebra_coe_nat (R S : Type*) (p : ℕ)
-  [comm_semiring R] [semiring S] [algebra R S] [invertible (p : R)] :
-  invertible (p : S) :=
-invertible.copy (invertible.map (algebra_map R S : R →* S) p) p
-  (by simp only [ring_hom.map_nat_cast, coe_monoid_hom])
 
 noncomputable instance mv_polynomial.invertible_C
   (σ : Type*) {R : Type*} [comm_semiring R] (r : R) [invertible r] :
@@ -35,4 +27,4 @@ Short-cut for typeclass resolution. -/
 noncomputable instance mv_polynomial.invertible_coe_nat
   (σ R : Type*) (p : ℕ) [comm_semiring R] [invertible (p : R)] :
   invertible (p : mv_polynomial σ R) :=
-invertible_algebra_coe_nat R _ _
+is_scalar_tower.invertible_algebra_coe_nat R _ _

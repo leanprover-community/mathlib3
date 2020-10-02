e513af3fe3a030c18c4844b4aa758f01f44dd265
Markus Himmel <markus@himmel-villmar.de>
2020-10-02 09:26:27 +0100

    WIP

--- src/algebra/homology/resolution.lean
+++ src/algebra/homology/resolution.lean
@@ -21,6 +21,12 @@ local attribute [instance] has_zero_object.has_zero
     Todo: Move -/
 def exact_at (C : homological_complex V b) (i : ℤ) := exact (C.d i) (C.d (i + b))
 
+lemma single_exact (n : ℤ) (M : V) (i : ℤ) : exact_at (single n M) i :=
+begin
+  unfold exact_at,
+  simp,
+end
+
 structure resolution (M : V) :=
 (C : chain_complex V)
 (exact : ∀ i, exact_at C i)
@@ -105,10 +111,72 @@ def cplx : chain_complex V :=
   ((single_at_eq_n 0 M _ (by omega)).hom ≫ (single_at_eq_n (-1) M _ (by omega)).inv)
   (by simp) (by simp)
 
+--                 0       -1
+-- 0 ----> 0 --1--> M --0-> M --(-1)--> 0 ----> 0
+
+lemma cplx_exact_one : exact_at (cplx M) 1 :=
+begin
+  unfold exact_at,
+  dsimp only [←sub_eq_add_neg, cplx],
+  rw [glue_d_n_sub_one, glue_d_n_sub_one_lt _ _ (sub_one_lt _)],
+  simp only [zero_comp, single_d, comp_zero, category.assoc],
+  rw ←abelian.mono_iff_exact_zero_left,
+  apply mono_comp _ _,
+  { apply_instance },
+  apply mono_comp _ _,
+  { apply_instance },
+  exact mono_comp _ _
+end
+
+lemma chain_complex.eq_to_hom {C : chain_complex V} {n m : ℤ} (h : n = m) :
+  C.d n = eq_to_hom (by rw h) ≫ C.d m ≫ eq_to_hom (by rw h) :=
+by { subst h, simp }
+
+lemma cplx_exact_zero : exact_at (cplx M) 0 :=
+begin
+  unfold exact_at,
+  dsimp only [←sub_eq_add_neg, cplx],
+  rw chain_complex.eq_to_hom (show (0 : ℤ) = 1 - 1, by omega),
+  rw [glue_d_n_sub_one],
+  rw glue_d_lt_n_sub_one _ _,
+  tactic.swap,
+  { omega },
+  simp only [zero_comp, single_d, comp_zero, category.id_comp, eq_to_hom_refl, category.comp_id,
+    category.assoc],
+  rw ←abelian.epi_iff_exact_zero_right,
+  apply epi_comp _ _,
+  { apply_instance },
+  apply epi_comp _ _,
+  { apply_instance },
+  exact epi_comp _ _
+end
+
+lemma exact_zero_zero_left {C : V} : exact (0 : 0 ⟶ 0) (0 : 0 ⟶ C) :=
+begin
+  rw ←abelian.epi_iff_exact_zero_right,
+  apply_instance,
+end
+
+lemma exact_zero_zero_right {A : V} : exact (0 : A ⟶ 0) (0 : 0 ⟶ 0) :=
+begin
+  rw ←abelian.mono_iff_exact_zero_left,
+  apply_instance
+end
+
+lemma cplx_exact_one_lt {i : ℤ} (h : 1 < i) : exact_at (cplx M) i :=
+begin
+  unfold exact_at,
+  dsimp only [←sub_eq_add_neg, cplx],
+  rw [glue_d_n_sub_one_lt, glue_d_n_sub_one_lt],
+  simp,
+
+end
+
 /-lemma cplx_exact (i) : exact_at (cplx M) i :=
 begin
   unfold exact_at,
-
+  by_cases h : i = 1,
+  {}
 end-/
 
 end projective

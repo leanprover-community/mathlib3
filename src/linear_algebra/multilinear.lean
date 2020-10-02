8e57dcb297dcd322409b6b414905f47b0dc9446d
Yury G. Kudryashov <urkud@urkud.name>
2020-10-02 16:03:10 -0400

    Fix import

--- src/linear_algebra/multilinear.lean
+++ src/linear_algebra/multilinear.lean
@@ -4,7 +4,7 @@ Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Sébastien Gouëzel
 -/
 import linear_algebra.basic
-import ring_theory.algebra
+import algebra.algebra.basic
 import tactic.omega
 import data.fintype.sort
 

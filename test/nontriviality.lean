bb080d1f68203129f60a5007eaf4792751a4c6a5
Scott Morrison <scott.morrison@gmail.com>
2020-10-03 12:07:46 +1000

    feat(logic/nontrivial): a tactic to summon nontrivial instances

---
+++ test/nontriviality.lean
@@ -0,0 +1,17 @@
+import logic.nontrivial
+import algebra.ordered_ring
+
+example {R : Type} [ordered_ring R] {a : R} (h : 0 < a) : 0 < a :=
+begin
+  nontriviality,
+  guard_hyp _inst : nontrivial R,
+  assumption,
+end
+
+example {R : Type} [ring R] (h : false) : 0 = (1 : R) :=
+begin
+  nontriviality,
+  guard_hyp _inst : nontrivial R,
+  exfalso,
+  assumption,
+end

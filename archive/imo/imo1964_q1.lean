551d6d987861f052601910f75c1e07937dc3595a
mushokunosora <knaka3435@gmail.com>
2020-10-02 23:33:30 -0500

    some changes

--- archive/imo/imo1964_q1.lean
+++ archive/imo/imo1964_q1.lean
@@ -19,35 +19,72 @@ begin
   norm_num,
 end
 
+/- TODO(?) : Refactor to only use `%` -/
+lemma modfact' (n : ℕ) : (2^n : ℕ) % 7 = 2^(n % 3) % 7 :=
+begin
+  have h1 : ∀(k : ℕ), k < 6 → ((2^k : ℕ) % 7) = 2^(k % 3) % 7 := by dec_trivial,
+  haveI : fact (nat.prime 7) := by { delta fact, norm_num },
+  have h2 : (2 % 7) ≠ 0 := dec_trivial,
+  rw ← nat.mod_add_div n 6,
+  have : 6 * (n / 6) % 3 = 0, sorry,
+  --simp similar to above
+  sorry,
+end
+
+
 /-!
 # IMO 1964 Q1
 
-* (a) Find all positive integers `n` for which `2^n-1` is divisble by `7`.
+* (a) Find all positive integers `n` for which `2^n-1` is divisible by `7`.
 * (b) Prove that there is no positive integer `n` for which `2^n+1` is divisible by `7`.
 -/
-example (n : ℕ) (gt_zero : 0 < n) : ((2^n -1) : zmod 7) = 0 ↔ (n : zmod 3) = 0 :=
+
+lemma nonneg (n : ℕ) (gt_zero : 0 < n) : 1 ≤ (2^n : ℤ) :=
 begin
+    { induction gt_zero with k h1 h2,
+      simp only [nat.succ_pos', pow_one, nat.one_le_bit0_iff],
+      rw [pow_succ,mul_comm],
+      suffices : 1 * (1 : ℤ) ≤ 2^k * 2, rwa mul_one (1 : ℤ) at this,
+      apply int.mul_le_mul h2;
+      linarith, }
+end
+
+lemma modcoe (n : ℕ) (gt_zero : 0 < n) : (↑(2^n-1) : zmod 7) = 2^n-1 :=
+begin
+  have nonne := nonneg n gt_zero,
+  suffices : ((2^n-1) : ℕ) = 2^n-1,
+  have h1 : 1 ≤ 2^n := by linarith,
+  sorry,
+  exact ⟨_⟩,
+end
+
+
+example (n : ℕ) (gt_zero : 0 < n) : 7 ∣ 2^(n : ℕ) -1 ↔ 3 ∣ n :=
+begin
+  repeat {rw [←zmod.nat_coe_zmod_eq_zero_iff_dvd]},
   split,
-  { rw modfact,
+  { intro hn,
+    rw [(modcoe n gt_zero), modfact] at hn,
     have h1 := zmod.val_lt (n : zmod 3),
-    intro hn,
     have h2 : (n : zmod 3).val = 0 ∨ (n : zmod 3).val = 1 ∨ (n : zmod 3).val = 2 := by omega,
     cases h2,
     exact fin.ext h2,
     cases h2;
     rw h2 at hn;
-    exfalso,
+    exfalso;
     norm_num at hn,
     contrapose! hn,
-    dec_trivial },
+    dec_trivial,},
   { intro hn,
-    rw [modfact, hn],
-    norm_num }
+    rw [(modcoe n gt_zero), modfact, hn],
+    norm_num,}
 end
 
-example (n : ℕ) (gt_zero : 0 < n) : ¬((2^n + 1) : zmod 7) = 0 :=
+example (n : ℕ) (gt_zero : 0 < n) : ¬(7 ∣ (2^n + 1)) :=
 begin
   intro hn,
+  rw [←zmod.nat_coe_zmod_eq_zero_iff_dvd] at hn,
+  simp only [nat.cast_bit0, nat.cast_add, nat.cast_one, nat.cast_pow] at hn,
   have h1 := zmod.val_lt (n : zmod 3),
   have h2 : (n : zmod 3).val = 0 ∨ (n : zmod 3).val = 1 ∨ (n : zmod 3).val = 2 := by omega,
   rw [modfact] at hn,

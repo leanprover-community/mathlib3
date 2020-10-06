393eaaa04b1225af6a338a4a298ada4a5265caf5
Patrick Massot <patrickmassot@free.fr>
2020-10-06 11:22:16 +0200

    Use notation for int and nat in parity files

--- src/data/nat/parity.lean
+++ src/data/nat/parity.lean
@@ -9,16 +9,16 @@ import data.nat.modeq
 
 namespace nat
 
-@[simp] theorem mod_two_ne_one {n : nat} : ¬ n % 2 = 1 ↔ n % 2 = 0 :=
+@[simp] theorem mod_two_ne_one {n : ℕ} : ¬ n % 2 = 1 ↔ n % 2 = 0 :=
 by cases mod_two_eq_zero_or_one n with h h; simp [h]
 
-@[simp] theorem mod_two_ne_zero {n : nat} : ¬ n % 2 = 0 ↔ n % 2 = 1 :=
+@[simp] theorem mod_two_ne_zero {n : ℕ} : ¬ n % 2 = 0 ↔ n % 2 = 1 :=
 by cases mod_two_eq_zero_or_one n with h h; simp [h]
 
-theorem even_iff {n : nat} : even n ↔ n % 2 = 0 :=
+theorem even_iff {n : ℕ} : even n ↔ n % 2 = 0 :=
 ⟨λ ⟨m, hm⟩, by simp [hm], λ h, ⟨n / 2, (mod_add_div n 2).symm.trans (by simp [h])⟩⟩
 
-theorem odd_iff {n : nat} : odd n ↔ n % 2 = 1 :=
+theorem odd_iff {n : ℕ} : odd n ↔ n % 2 = 1 :=
 ⟨λ ⟨m, hm⟩, by { rw [hm, add_mod], norm_num },
  λ h, ⟨n / 2, (mod_add_div n 2).symm.trans (by { rw h, abel })⟩⟩
 
@@ -41,10 +41,10 @@ mk_simp_attribute parity_simps "Simp attribute for lemmas about `even`"
 @[simp] theorem not_even_one : ¬ even 1 :=
 by rw even_iff; apply one_ne_zero
 
-@[simp] theorem even_bit0 (n : nat) : even (bit0 n) :=
+@[simp] theorem even_bit0 (n : ℕ) : even (bit0 n) :=
 ⟨n, by rw [bit0, two_mul]⟩
 
-@[parity_simps] theorem even_add {m n : nat} : even (m + n) ↔ (even m ↔ even n) :=
+@[parity_simps] theorem even_add {m n : ℕ} : even (m + n) ↔ (even m ↔ even n) :=
 begin
   cases mod_two_eq_zero_or_one m with h₁ h₁; cases mod_two_eq_zero_or_one n with h₂ h₂;
     simp [even_iff, h₁, h₂],
@@ -57,7 +57,7 @@ end
 theorem even.add {m n : ℕ} (hm : even m) (hn : even n) : even (m + n) :=
 even_add.2 $ by simp only [*]
 
-@[simp] theorem not_even_bit1 (n : nat) : ¬ even (bit1 n) :=
+@[simp] theorem not_even_bit1 (n : ℕ) : ¬ even (bit1 n) :=
 by simp [bit1] with parity_simps
 
 lemma two_not_dvd_two_mul_add_one (a : ℕ) : ¬(2 ∣ 2 * a + 1) :=
@@ -69,7 +69,7 @@ end
 lemma two_not_dvd_two_mul_sub_one : Π {a : ℕ} (w : 0 < a), ¬(2 ∣ 2 * a - 1)
 | (a+1) _ := two_not_dvd_two_mul_add_one a
 
-@[parity_simps] theorem even_sub {m n : nat} (h : n ≤ m) : even (m - n) ↔ (even m ↔ even n) :=
+@[parity_simps] theorem even_sub {m n : ℕ} (h : n ≤ m) : even (m - n) ↔ (even m ↔ even n) :=
 begin
   conv { to_rhs, rw [←nat.sub_add_cancel h, even_add] },
   by_cases h : even n; simp [h]
@@ -80,10 +80,10 @@ theorem even.sub {m n : ℕ} (hm : even m) (hn : even n) : even (m - n) :=
   (λ h, by simp only [even_sub h, *])
   (λ h, by simp only [sub_eq_zero_of_le h, even_zero])
 
-@[parity_simps] theorem even_succ {n : nat} : even (succ n) ↔ ¬ even n :=
+@[parity_simps] theorem even_succ {n : ℕ} : even (succ n) ↔ ¬ even n :=
 by rw [succ_eq_add_one, even_add]; simp [not_even_one]
 
-@[parity_simps] theorem even_mul {m n : nat} : even (m * n) ↔ even m ∨ even n :=
+@[parity_simps] theorem even_mul {m n : ℕ} : even (m * n) ↔ even m ∨ even n :=
 begin
   cases mod_two_eq_zero_or_one m with h₁ h₁; cases mod_two_eq_zero_or_one n with h₂ h₂;
     simp [even_iff, h₁, h₂],
@@ -95,7 +95,7 @@ end
 
 /-- If `m` and `n` are natural numbers, then the natural number `m^n` is even
 if and only if `m` is even and `n` is positive. -/
-@[parity_simps] theorem even_pow {m n : nat} : even (m^n) ↔ even m ∧ n ≠ 0 :=
+@[parity_simps] theorem even_pow {m n : ℕ} : even (m^n) ↔ even m ∧ n ≠ 0 :=
 by { induction n with n ih; simp [*, pow_succ', even_mul], tauto }
 
 lemma even_div {a b : ℕ} : even (a / b) ↔ a % (2 * b) / b = 0 :=
@@ -109,7 +109,7 @@ theorem neg_one_pow_eq_one_iff_even {α : Type*} [ring α] {n : ℕ} (h1 : (-1 :
 
 -- Here are examples of how `parity_simps` can be used with `nat`.
 
-example (m n : nat) (h : even m) : ¬ even (n + 3) ↔ even (m^2 + m + n) :=
+example (m n : ℕ) (h : even m) : ¬ even (n + 3) ↔ even (m^2 + m + n) :=
 by simp [*, (dec_trivial : ¬ 2 = 0)] with parity_simps
 
 example : ¬ even 25394535 :=

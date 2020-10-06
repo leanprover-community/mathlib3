608804662387417761aef444264f6a4718bf1430
Vierkantor <vierkantor@vierkantor.com>
2020-10-06 12:22:00 +0000

    Fix `data.dfinsupp`

--- src/data/dfinsupp.lean
+++ src/data/dfinsupp.lean
@@ -3,6 +3,7 @@ Copyright (c) 2018 Kenny Lau. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Johannes Hölzl, Kenny Lau
 -/
+import algebra.module.linear_map
 import algebra.module.pi
 import algebra.big_operators.basic
 import data.set.finite

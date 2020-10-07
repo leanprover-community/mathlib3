11b388b889b4aba882033c98e42fb9e57ba04bbe
Johan Commelin <johan@commelin.net>
2020-10-07 06:43:07 +0000

    chore(*): normalise copyright headers
    This diff makes sure that all files start with a copyright header
    of the following shape
    
    ```
    /-
    Copyright ...
    ... Apache ...
    Author...
    -/
    ```
    
    Some files used to have a short description of the contents
    in the same comment block.
    Such comments have *not* been turned into module docstrings,
    but simply moved to a regular comment block below the copyright header.
    Turning these comments into good module docstrings is an
    effort that should be undertaken in future PRs.

--- src/measure_theory/category/Meas.lean
+++ src/measure_theory/category/Meas.lean
@@ -1,4 +1,5 @@
-/- Copyright (c) 2018 Johannes Hölzl. All rights reserved.
+/-
+Copyright (c) 2018 Johannes Hölzl. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Johannes Hölzl
 -/

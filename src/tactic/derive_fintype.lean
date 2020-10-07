841d64d4306b3d63ef46181fa2ed97c7a172ff71
Johan Commelin <johan@commelin.net>
2020-10-07 07:56:32 +0000

    chore(*): normalise copyright headers (#4497)
    This diff makes sure that all files start with a copyright header
    of the following shape
    
    ```
    /-
    Copyright ...
    ... Apache ...
    Author...
    -/
    ```
    
    Some files used to have a short description of the contents
    in the same comment block.
    Such comments have *not* been turned into module docstrings,
    but simply moved to a regular comment block below the copyright header.
    Turning these comments into good module docstrings is an
    effort that should be undertaken in future PRs.

--- src/tactic/derive_fintype.lean
+++ src/tactic/derive_fintype.lean
@@ -1,6 +1,7 @@
 /-
 Copyright (c) 2020 Mario Carneiro. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
+Authors: Mario Carneiro
 -/
 import logic.basic
 import data.fintype.basic

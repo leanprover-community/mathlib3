03a07cf14f7a4a1425eecde3f56d1ca6f803bee1
Bhavik Mehta <bhavikmehta8@gmail.com>
2020-10-10 19:23:22 +0100

    tidy

--- src/category_theory/limits/presheaf.lean
+++ src/category_theory/limits/presheaf.lean
@@ -1,3 +1,8 @@
+/-
+Copyright (c) 2020 Bhavik Mehta. All rights reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
+Authors: Bhavik Mehta
+-/
 import category_theory.adjunction
 import category_theory.elements
 import category_theory.limits.functor_category
@@ -9,7 +14,7 @@ namespace category_theory
 noncomputable theory
 
 open category limits
-universes v₁ v₂ u₁ u₂
+universes u₁ u₂
 
 variables {C : Type u₁} [small_category C]
 variables {ℰ : Type u₂} [category.{u₁} ℰ]
@@ -17,26 +22,28 @@ variable (A : C ⥤ ℰ)
 
 namespace colimit_adj
 
-@[simps]
+/--
+The functor taking `(E : ℰ) (c : Cᵒᵖ)` to the homset `(A.obj C ⟶ E)`. It is shown in `L_adjunction`
+that this functor has a left adjoint (provided E has colimits) given by taking colimits over
+categories of elements.
+-/
+@[simps {rhs_md := semireducible}]
 def R : ℰ ⥤ (Cᵒᵖ ⥤ Type u₁) :=
-{ obj := λ E,
-  { obj := λ c, A.obj c.unop ⟶ E,
-    map := λ c c' f k, A.map f.unop ≫ k,
-    map_id' := λ X, by { ext, simp } },
-    map_comp' := λ X Y Z f g, by { ext, simp },
-  map := λ E E' k,
-  { app := λ c f, f ≫ k,
-    naturality' := λ X Y f, by { ext, apply assoc } },
-  map_id' := λ X, by { ext, simp },
-  map_comp' := λ X Y Z f g, by { ext, simp } }.
+yoneda ⋙ (whiskering_left _ _ (Type u₁)).obj (functor.op A)
 
+/--
+(Implementation). The equivalence of homsets which helps construct the left adjoint to
+`colimit_adj.R`.
+It is shown in `Le'_natural` that this is a natural bijection.
+-/
 def Le' (P : Cᵒᵖ ⥤ Type u₁) (E : ℰ) {c : cocone ((category_of_elements.π P).left_op ⋙ A)}
   (t : is_colimit c) : (c.X ⟶ E) ≃ (P ⟶ (R A).obj E) :=
 (t.hom_iso' E).to_equiv.trans
 { to_fun := λ k,
   { app := λ c p, k.1 (opposite.op ⟨_, p⟩),
     naturality' := λ c c' f, funext $ λ p,
-      (k.2 (has_hom.hom.op ⟨f, rfl⟩ : (opposite.op ⟨c', P.map f p⟩ : P.elementsᵒᵖ) ⟶ opposite.op ⟨c, p⟩)).symm },
+      (k.2 (has_hom.hom.op ⟨f, rfl⟩ :
+              (opposite.op ⟨c', P.map f p⟩ : P.elementsᵒᵖ) ⟶ opposite.op ⟨c, p⟩)).symm },
   inv_fun := λ τ,
   { val := λ p, τ.app p.unop.1 p.unop.2,
     property := λ p p' f,
@@ -58,6 +65,7 @@ def Le' (P : Cᵒᵖ ⥤ Type u₁) (E : ℰ) {c : cocone ((category_of_elements
     refl,
   end }
 
+/-- (Implementation). Show that the bijection in `Le'` is natural (on the right). -/
 lemma Le'_natural (P : Cᵒᵖ ⥤ Type u₁) (E₁ E₂ : ℰ) (g : E₁ ⟶ E₂)
   {c : cocone _} (t : is_colimit c) (k : c.X ⟶ E₁) :
 Le' A P E₂ t (k ≫ g) = Le' A P E₁ t k ≫ (R A).map g :=
@@ -67,11 +75,18 @@ begin
 end
 
 variables [has_colimits ℰ]
+
+/-- The left adjoint to the functor `R` which sends `R(E,C)  -/
 def L : (Cᵒᵖ ⥤ Type u₁) ⥤ ℰ :=
 adjunction.left_adjoint_of_equiv
   (λ P E, Le' A P E (colimit.is_colimit _))
   (λ P E E' g, Le'_natural A P E E' g _)
 
+@[simp]
+lemma L_obj (P : Cᵒᵖ ⥤ Type u₁) : (L A).obj P =
+colimit ((category_of_elements.π P).left_op ⋙ A) := rfl
+
+/-- Show `L` is left adjoint to `R`. -/
 def L_adjunction : L A ⊣ R A := adjunction.adjunction_of_equiv_left _ _
 
 /--
@@ -97,7 +112,11 @@ def is_term (A : C) : is_terminal (term_element A) :=
     simp,
   end }
 
-def extend : (yoneda : C ⥤ _) ⋙ L A ≅ A :=
+/--
+On the full subcategory of representables, `L A` is an extension of `A`.
+TODO: Among functors preserving colimits, `L` is unique with this property up to isomorphism.
+-/
+def extend : (yoneda : C ⥤ Cᵒᵖ ⥤ Type u₁) ⋙ L A ≅ A :=
 nat_iso.of_components
 (λ X, (colimit.is_colimit _).cocone_point_unique_up_to_iso (colimit_of_diagram_terminal (is_term X) _))
 begin
@@ -117,6 +136,7 @@ end colimit_adj
 
 open colimit_adj
 
+/-- The functor `R` is isomorphic to the identity functor if evaluated at the yoneda embedding. -/
 def right_is_id : R (yoneda : C ⥤ _) ≅ 𝟭 _ :=
 nat_iso.of_components
 (λ P, nat_iso.of_components (λ X, yoneda_sections_small X.unop _)
@@ -128,14 +148,29 @@ nat_iso.of_components
   end))
 (λ _ _ _, rfl)
 
+/--
+Since `L A` is adjoint to `R A`, if we use `A = yoneda` then `R A` is isomorphic to the
+identity, and so `L A` is as well.
+-/
 def left_is_id : L (yoneda : C ⥤ _) ≅ 𝟭 _ :=
 adjunction.left_adjoint_uniq (L_adjunction _) (adjunction.id.of_nat_iso_right right_is_id.symm)
 
--- This is a cocone with point `P`, for which the diagram consists solely of representables.
+/--
+This is a cocone with point `P`, for which the diagram consists solely of representables.
+It is shown in `is_a_limit P` that this cocone is a colimit: that is, we have exhibited an
+arbitrary presheaf `P` as a colimit of representables.
+-/
 def the_cocone (P : Cᵒᵖ ⥤ Type u₁) :
   cocone ((category_of_elements.π P).left_op ⋙ yoneda) :=
 cocone.extend (colimit.cocone _) (left_is_id.hom.app P)
 
+@[simp]
+lemma the_cocone_X (P : Cᵒᵖ ⥤ Type u₁) : (the_cocone P).X = P := rfl
+
+/--
+The cocone with point `P` given by `the_cocone` is a colimit: that is, we have exhibited an
+arbitrary presheaf `P` as a colimit of representables.
+-/
 def is_a_limit (P : Cᵒᵖ ⥤ Type u₁) : is_colimit (the_cocone P) :=
 begin
   apply is_colimit.of_point_iso (colimit.is_colimit ((category_of_elements.π P).left_op ⋙ yoneda)),
@@ -143,19 +178,5 @@ begin
   rw [colimit.desc_extend, colimit.desc_cocone],
   apply_instance,
 end
-
--- Probably needs preserves branch for simp to be nice
--- def unique_extension [has_colimits ℰ] (L' : (Cᵒᵖ ⥤ Type u₁) ⥤ ℰ) (hL : (yoneda : C ⥤ _) ⋙ L' ≅ A)
---   [preserves_colimits L'] :
---   L' ≅ L A :=
--- begin
---   apply nat_iso.of_components _ _,
---   intro P,
---   apply (preserves_colimit.preserves (is_a_limit P)).cocone_points_iso_of_nat_iso (colimit.is_colimit ((category_of_elements.π P).left_op ⋙ A)),
---   apply functor.associator _ _ _ ≪≫ iso_whisker_left _ hL,
---   apply_instance,
---   intros X Y f,
---   simp,
--- end
 
 end category_theory

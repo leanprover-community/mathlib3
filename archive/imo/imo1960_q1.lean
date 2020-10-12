57cadeafd3ff545c244f6f2f1543e62472afc0d0
Kevin Lacker <lacker@gmail.com>
2020-10-12 15:14:27 -0700

    use norm_digits method

--- archive/imo/imo1960_q1.lean
+++ archive/imo/imo1960_q1.lean
@@ -17,9 +17,7 @@ Since Lean doesn't have a way to directly express problem statements of the form
 other is equivalent to solving the problem. A human solver also has to discover the
 second predicate.
 
-In theory, we should be able to brute force this and check every number up to 1000.
-In practice, the Lean tactics that are capable of checking a single number, like `norm_num`,
-are not efficient enough to run a thousand times while compiling one file.
+The strategy here is roughly brute force, checking the possible multiples of 11.
 -/
 
 open nat
@@ -56,114 +54,50 @@ end,
 by linarith
 
 /-
-How to calculate the sum of square digits, so that we can quickly simplify a
-`sum_of_digit_squares` expression.
-These are basically "hints" to speed up `norm_num`.
+We do an exhaustive search to show that all results are covered by `solution_predicate`.
 -/
 
-def sum_of_digit_squares (n : ℕ) : ℕ := sum_of_squares (nat.digits 10 n)
+def search_up_to (c n : ℕ) : Prop :=
+n = c * 11 ∧ ∀ m : ℕ, m < n → problem_predicate m → solution_predicate m
 
-lemma sods_zero : sum_of_digit_squares 0 = 0 :=
-by norm_num [sum_of_digit_squares, sum_of_squares]
+lemma search_up_to_start : search_up_to 9 99 := ⟨rfl, λ n h p, by linarith [ge_100 p]⟩
 
-lemma calc_sods {n : ℕ} (h1 : 0 < n) :
-sum_of_digit_squares n = (n % 10) ^ 2 + (sum_of_digit_squares (n / 10)) :=
+lemma search_up_to_step {c n} (H : search_up_to c n)
+  {c' n'} (ec : c + 1 = c') (en : n + 11 = n')
+  {l} (el : nat.digits 10 n = l)
+  (H' : c = sum_of_squares l → c = 50 ∨ c = 73) :
+  search_up_to c' n' :=
 begin
-  unfold sum_of_digit_squares,
-  rw [nat.digits, nat.digits_aux_def _ _ _ h1],
-  simp [sum_of_squares, pow_two],
+  subst ec, subst en, subst el,
+  obtain ⟨rfl, H⟩ := H,
+  refine ⟨by ring, λ m l p, _⟩,
+  obtain ⟨h₁, ⟨m, rfl⟩, h₂⟩ := id p,
+  by_cases h : 11 * m < c * 11, { exact H _ h p },
+  have : m = c, {linarith}, subst m,
+  rw [nat.mul_div_cancel_left _ (by norm_num : 11 > 0), mul_comm] at h₂,
+  refine (H' h₂).imp _ _; {rintro rfl, norm_num}
+end
+
+lemma search_up_to_end {c} (H : search_up_to c 1001)
+  {n : ℕ} (ppn : problem_predicate n) : solution_predicate n :=
+H.2 _ (by linarith [lt_1000 ppn]) ppn
+
+lemma right_direction {n : ℕ} : problem_predicate n → solution_predicate n :=
+begin
+  have := search_up_to_start,
+  iterate 82 {
+    replace := search_up_to_step this (by norm_num1; refl) (by norm_num1; refl)
+      (by norm_digits; refl) dec_trivial },
+  exact search_up_to_end this
 end
 
 /-
-This part is doing an exhaustive search.
+Now we just need to prove the equivalence, for the precise problem statement.
 -/
-
-def fails_sum (c : ℕ) : Prop := c ≠ sum_of_digit_squares (c*11)
-
-lemma multiples_of_11 {n : ℕ} (h1 : problem_predicate n) :
-∃ c : ℕ, ¬ fails_sum c ∧ 9 < c ∧ c < 91 ∧ n = c * 11 :=
-begin
-  obtain ⟨c : ℕ, h2 : n = c * 11⟩ := exists_eq_mul_left_of_dvd h1.right.left,
-  refine ⟨c, _, _, _, _⟩,
-  { have h3 : c = (c * 11) / 11, by simp,
-    have h4 : c = n / 11, from h2.symm ▸ h3,
-    unfold fails_sum,
-    rw [h2.symm, h4],
-    simp only [not_not],
-    exact h1.right.right },
-  { have h5 : 100 ≤ n, from ge_100 h1,
-    linarith },
-  { have h6 : n < 1000, from lt_1000 h1,
-    linarith },
-  { exact h2 },
-end
-
-lemma iterative_step (c bound : ℕ) (h1 : fails_sum (bound + 1)) (h2 : fails_sum c ∨ bound < c) :
-fails_sum c ∨ bound + 1 < c :=
-begin
-  cases h2,
-  { left,
-    exact h2 },
-  obtain (h3 | h3) : bound + 1 = c ∨ bound + 1 < c, from eq_or_lt_of_le h2,
-  { left,
-    exact h3 ▸ h1 },
-  right,
-  exact h3,
-end
-
-lemma low_search {c : ℕ} (lower_bound : 9 < c) : fails_sum c ∨ 49 < c :=
-begin
-  iterate 40
-  { refine iterative_step _ _ _ _,
-    norm_num [fails_sum, calc_sods, sods_zero] },
-  right,
-  exact lower_bound,
-end
-
-lemma mid_search {c : ℕ} (lower_bound : 50 < c) : fails_sum c ∨ 72 < c :=
-begin
-  iterate 22
-  { refine iterative_step _ _ _ _,
-    norm_num [fails_sum, calc_sods, sods_zero] },
-  right,
-  exact lower_bound,
-end
-
-lemma high_search {c : ℕ} (lower_bound : 73 < c) : fails_sum c ∨ 90 < c :=
-begin
-  iterate 17
-  { refine iterative_step _ _ _ _,
-    norm_num [fails_sum, calc_sods, sods_zero] },
-  right,
-  exact lower_bound,
-end
-
-/-
-Now we just need to combine the results from the `search` lemmas.
--/
-
-lemma right_direction (n : ℕ) (ppn : problem_predicate n) : solution_predicate n :=
-begin
-  obtain ⟨c : ℕ, h1 : ¬ fails_sum c ∧ 9 < c ∧ c < 91 ∧ n = c * 11⟩ := multiples_of_11 ppn,
-  obtain (h2 | h2) : fails_sum c ∨ 49 < c, from low_search h1.right.left,
-  { exact absurd h2 h1.left },
-  have h3 : n = c * 11, from h1.right.right.right,
-  obtain (h4 | h4) : 50 = c ∨ 50 < c, from eq_or_lt_of_le h2,
-  { left,
-    linarith },
-  obtain (h5 | h5) : fails_sum c ∨ 72 < c, from mid_search h4,
-  { exact absurd h5 h1.left },
-  obtain (h6 | h6) : 73 = c ∨ 73 < c, from eq_or_lt_of_le h5,
-  { right,
-    linarith },
-  obtain (h7 | h7) : fails_sum c ∨ 90 < c, from high_search h6,
-  { exact absurd h7 h1.left },
-  linarith,
-end
 
 lemma left_direction (n : ℕ) (spn : solution_predicate n) : problem_predicate n :=
 by rcases spn with (rfl | rfl); norm_num [problem_predicate, sum_of_squares]
 
 theorem imo1960_q1 (n : ℕ) : problem_predicate n ↔ solution_predicate n :=
-⟨right_direction n, left_direction n⟩
+⟨right_direction, left_direction n⟩
 

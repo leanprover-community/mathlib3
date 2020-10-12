7d756f1115aebc767feb80cb03464c005c1c2499
Johan Commelin <johan@commelin.net>
2020-10-12 13:25:04 +0000

    more automation

--- archive/imo/imo2020_q2.lean
+++ archive/imo/imo2020_q2.lean
@@ -25,25 +25,22 @@ theorem imo2020_q2 (a b c d : ℝ) (hd0 : 0 < d) (hdc : d ≤ c) (hcb : c ≤ b)
   (h1 : a + b + c + d = 1) :
   (a + 2 * b + 3 * c + 4 * d) * a ^ a * b ^ b * c ^ c * d ^ d < 1 :=
 begin
-  have hc0 : 0 < c := hd0.trans_le hdc,
-  have hb0 : 0 < b := hc0.trans_le hcb,
-  have ha0 : 0 < a := hb0.trans_le hba,
-  have hp : a ^ a * b ^ b * c ^ c * d ^ d ≤ a * a + b * b + c * c + d * d :=
-    geom_mean_le_arith_mean4_weighted ha0.le hb0.le hc0.le hd0.le ha0.le hb0.le hc0.le hd0.le h1,
-  calc (a + 2 * b + 3 * c + 4 * d) * a ^ a * b ^ b * c ^ c * d ^ d
-    = (a + 2 * b + 3 * c + 4 * d) * (a ^ a * b ^ b * c ^ c * d ^ d) : by ac_refl
-    ... ≤ (a + 2 * b + 3 * c + 4 * d) * (a * a + b * b + c * c + d * d) :
-      mul_le_mul_of_nonneg_left hp (by linarith)
-    ... = (a + 2 * b + 3 * c + 4 * d) * (a * a) + (a + 2 * b + 3 * c + 4 * d) * (b * b) +
-          (a + 2 * b + 3 * c + 4 * d) * (c * c) + (a + 2 * b + 3 * c + 4 * d) * (d * d) :
-      by simp only [mul_add]
-    ... ≤ (a + 3 * b + 3 * c + 3 * d) * (a * a) + (3 * a + b + 3 * c + 3 * d) * (b * b) +
-          (3 * a + 3 * b + c + 3 * d) * (c * c) + (3 * a + 3 * b + 3 * c + d) * (d * d) :
-      by apply_rules [add_le_add]; refine mul_le_mul_of_nonneg_right _ (mul_self_nonneg _); linarith
-    ... < (a + 3 * b + 3 * c + 3 * d) * (a * a) + (3 * a + b + 3 * c + 3 * d) * (b * b) +
-          (3 * a + 3 * b + c + 3 * d) * (c * c) + (3 * a + 3 * b + 3 * c + d) * (d * d) +
-          (6 * a * b * c + 6 * a * b * d + 6 * a * c * d + 6 * b * c * d) :
-      lt_add_of_pos_right _ (by apply_rules [add_pos, mul_pos, zero_lt_one])
-    ... = (a + b + c + d) ^ 3 : by ring
-    ... = 1 : by simp [h1]
+  have hp : a ^ a * b ^ b * c ^ c * d ^ d ≤ a * a + b * b + c * c + d * d,
+  by refine geom_mean_le_arith_mean4_weighted _ _ _ _ _ _ _ _ h1; linarith,
+  calc  (a + 2 * b + 3 * c + 4 * d) * a ^ a * b ^ b * c ^ c * d ^ d
+      = (a + 2 * b + 3 * c + 4 * d) * (a ^ a * b ^ b * c ^ c * d ^ d) : by ac_refl
+  ... ≤ (a + 2 * b + 3 * c + 4 * d) * (a * a + b * b + c * c + d * d) :
+    mul_le_mul_of_nonneg_left hp (by linarith)
+  ... = (a + 2 * b + 3 * c + 4 * d) * (a * a) + (a + 2 * b + 3 * c + 4 * d) * (b * b) +
+        (a + 2 * b + 3 * c + 4 * d) * (c * c) + (a + 2 * b + 3 * c + 4 * d) * (d * d) :
+    by simp only [mul_add]
+  ... ≤ (a + 3 * b + 3 * c + 3 * d) * (a * a) + (3 * a + b + 3 * c + 3 * d) * (b * b) +
+        (3 * a + 3 * b + c + 3 * d) * (c * c) + (3 * a + 3 * b + 3 * c + d) * (d * d) :
+    by apply_rules [add_le_add]; refine mul_le_mul_of_nonneg_right _ (mul_self_nonneg _); linarith
+  ... < (a + 3 * b + 3 * c + 3 * d) * (a * a) + (3 * a + b + 3 * c + 3 * d) * (b * b) +
+        (3 * a + 3 * b + c + 3 * d) * (c * c) + (3 * a + 3 * b + 3 * c + d) * (d * d) +
+        (6 * a * b * c + 6 * a * b * d + 6 * a * c * d + 6 * b * c * d) :
+    lt_add_of_pos_right _ (by apply_rules [add_pos, mul_pos, zero_lt_one]; linarith)
+  ... = (a + b + c + d) ^ 3 : by ring
+  ... = 1 : by simp [h1]
 end

a313b1a40ac7ecd15f71f5b345553937333b191f
Kevin Lacker <lacker@gmail.com>
2020-10-12 16:04:37 -0700

    initial

---
+++ archive/imo/imo1981_q3.lean
@@ -0,0 +1,14 @@
+/-
+Copyright (c) 2020 Kevin Lacker. All rights reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
+Authors: Kevin Lacker
+-/
+
+import data.nat.basic
+
+/-!
+# IMO 1981 Q3
+
+Determine the maximum value of `m ^ 2 + n ^ 2`, where `m` and `n` are integers in
+`{1, 2, ..., 1981}` and `(n ^ 2 - m * n - m ^ 2) ^ 2 = 1`.
+-/

ebcd4884bed60fb81a5c04cc98d4b5fff4e984c7
Bryan Gin-ge Chen <bryangingechen@gmail.com>
2020-10-13 18:32:09 -0400

    stash

--- archive/imo/imo1969_q1.lean
+++ archive/imo/imo1969_q1.lean
@@ -6,7 +6,7 @@ Authors: Kevin Lacker
 import algebra.group_power.identities
 import tactic.linarith
 import tactic.norm_cast
-import data.fintype.basic
+import data.set.finite
 
 open int
 open nat
@@ -38,6 +38,10 @@ The factorization is over the integers, but we need the nonprimality over the na
 lemma int_large {a : ℤ} (h : 1 < a) : 1 < a.nat_abs :=
 by exact_mod_cast lt_of_lt_of_le h le_nat_abs
 
+/-
+Should this be in src? Probably, with assumptions 1 < a.nat_abs and 1 < b.nat_abs.
+-/
+
 lemma int_not_prime {a b : ℤ} {c : ℕ} (h1 : 1 < a) (h2 : 1 < b) (h3 : a*b = (c : ℤ)) : ¬ prime c :=
 have h4 : a.nat_abs * b.nat_abs = c, by rw [←nat_abs_mul, h3, nat_abs_of_nat],
 norm_num.not_prime_helper a.nat_abs b.nat_abs c h4 (int_large h1) (int_large h2)
@@ -55,20 +59,33 @@ infinitely many of them.
 $a = 4*(2+b)^4$ should do. So $m = 2+b$.
 -/
 
-/-- `good_nats` is an abbreviation for the subtype of natural numbers satisfying the
+/-- `good_nats` is the set of natural numbers satisfying the
 condition in the problem statement, namely the `a : ℕ` such that `n^4 + a` is not prime
 for any `n : ℕ`. -/
-abbreviation good_nats : Type := {a : ℕ // ∀ n : ℕ, ¬ prime (n^4 + a)}
+def good_nats : set ℕ := {a : ℕ | ∀ n : ℕ, ¬ prime (n^4 + a)}
 
 /--
-The function $a = 4*(2+b)^4$ as a function from `ℕ` to `good_nats`.
+The function $a = 4*(2+b)^4$.
 -/
-def a (b : ℕ) : good_nats :=
-⟨4*(2+b)^4, polynomial_not_prime (show 1 < 2+b, by linarith)⟩
+def a (b : ℕ) : ℕ := 4*(2+b)^4
 
-/-- For all `b : good_nats`, $a(b) > b$. (This is true for `b : ℕ`, but it's more convenient to
-state it this way.) -/
-lemma lt_a (b : good_nats) : b < a b :=
+/-
+You should be able to prove that this is strict_mono (hence, injective) using dot-syntax lemmas. If not, this is a good reason to add more of them.
+-/
+
+lemma a_good (b : ℕ) : a b ∈ good_nats :=
+polynomial_not_prime (show 1 < 2+b, by linarith)
+
+/-
+A good lemma for src/ (not_congr finite_image_iff): if f : α → β is inj_on s, then f '' s is an infinite set iff s is an infinite set. Related lemmas:
+
+    if f is inj_on s and s is infinite and maps_to f s t, then t is infinite (above + monotonicity);
+    if f is injective and [infinite α], then range f is an infinite set;
+    if f is injective, [infinite α] and ∀ x, f x ∈ s, then s is an infinite set (this is what's I'd use in this problem).
+-/
+
+/-- `a` is a monotone function -/
+lemma lt_a {b : ℕ} (hb : b ∈ good_nats) : b < a b :=
 begin
   simp only [←subtype.coe_lt_coe, subtype.coe_mk, a],
   nlinarith [show (b : ℕ) ≤ b^2, { rw [pow_two], exact le_mul_self _ }],
@@ -76,7 +93,7 @@ end
 
 /-- We conclude by using `a` to get a contradiction with the assumption that `good_nats` is
 a `fintype`, since the elements of a `fintype` must have a maximal element. -/
-theorem imo1969_q1 : infinite good_nats :=
+theorem imo1969_q1 : set.infinite good_nats :=
 ⟨begin
   intro h,
   obtain ⟨m, hm1, hm2⟩ := h.elems.exists_maximal ⟨a 0, by apply h.complete⟩,

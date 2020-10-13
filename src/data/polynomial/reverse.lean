8570d8bb25fddb5463f35e344a0690bbee4897d1
adomani <adomani@gmail.com>
2020-10-13 08:42:52 +0200

    define reverse, taking a polynomial to the polynomial with reversed coefficietns.  Prove that reverse is a multiplicative monoid homomorphism

--- src/data/polynomial/reverse.lean
+++ src/data/polynomial/reverse.lean
@@ -5,7 +5,7 @@ Authors: Damiano Testa
 -/
 
 import data.polynomial.degree.basic
---import data.polynomial.erase_lead
+import data.polynomial.erase_lead
 import data.polynomial.degree.trailing_degree
 
 /-!
@@ -34,10 +34,10 @@ def rev_at (N : ℕ) : ℕ → ℕ := λ i : ℕ, ite (i ≤ N) (N-i) i
 @[simp] lemma rev_at_invol {N n : ℕ} : rev_at N (rev_at N n) = n :=
 begin
   unfold rev_at,
-  split_ifs,
+  split_ifs with h j,
   { exact nat.sub_sub_self h, },
   { exfalso,
-    apply h_1,
+    apply j,
     exact nat.sub_le N n, },
   { refl, },
 end
@@ -59,8 +59,8 @@ def reflect : ℕ → polynomial R → polynomial R :=
   intro,
   rw mem_image,
   split,
-  { intro a_1,
-    rcases a_1 with ⟨ a, ha, rfl⟩,
+  { intro h,
+    rcases h with ⟨ a, ha, rfl⟩,
     rwa [rev_at_invol, ← mem_support_iff_coeff_ne_zero], },
   { intro,
     use (rev_at N a),
@@ -98,10 +98,10 @@ end
 @[simp] lemma reflect_C_mul_X_pow (N n : ℕ) {c : R} :
  reflect N (C c * X ^ n) = C c * X ^ (rev_at N n) :=
 begin
-  ext1,
+  ext1 m,
   unfold reflect,
   rw coeff_mk,
-  by_cases h : rev_at N n = n_1,
+  by_cases h : rev_at N n = m,
   { rw [h, coeff_C_mul, coeff_C_mul, coeff_X_pow_self, ← h, rev_at_invol, coeff_X_pow_self], },
   { rw not_mem_support_iff_coeff_zero.mp,
     { symmetry,
@@ -111,15 +111,13 @@ begin
       exact (mem_support_C_mul_X_pow a).symm, },
     { intro,
       apply h,
-      rw ← @rev_at_invol N n_1,
+      rw ← @rev_at_invol N m,
       apply congr_arg _,
       exact (mem_support_C_mul_X_pow a).symm, }, },
 end
 
 @[simp] lemma reflect_monomial (N n : ℕ) : reflect N ((X : polynomial R) ^ n) = X ^ (rev_at N n) :=
-begin
-  rw [← one_mul (X^n), ← one_mul (X^(rev_at N n)), ← C_1, reflect_C_mul_X_pow],
-end
+by rw [← one_mul (X^n), ← one_mul (X^(rev_at N n)), ← C_1, reflect_C_mul_X_pow]
 
 /-- The reverse of a polynomial f is the polynomial obtained by "reading f backwards".
 Even though this is not the actual definition, reverse f = f (1/X) * X ^ f.nat_degree. -/
@@ -144,7 +142,6 @@ begin
   { induction cg with cg hcg,
     -- second induction: base case
     { intros N O f g Cf Cg Nf Og,
-      rw C_mul_X_pow
       rw [C_mul_X_pow_of_card_support_le_one Cf, C_mul_X_pow_of_card_support_le_one Cg],
       rw [mul_assoc, X_pow_mul, mul_assoc, ← pow_add X],
       repeat {rw rp_smul},
@@ -165,19 +162,19 @@ begin
       by_cases g0 : g = 0,
       { rw [g0, mul_zero, rp_zero, rp_zero, mul_zero], },
       { rw [← erase_lead_add_C_mul_X_pow g, mul_add, rp_add, rp_add, mul_add],
-        rw hcg N O f _ Cf _ Nf (le_trans nat_degree_le Og),
+        rw hcg N O f _ Cf _ Nf (le_trans erase_lead_nat_degree_le Og),
         rw hcg N O f _ Cf (le_add_left card_support_C_mul_X_pow_le_one) Nf _,
         { exact (le_trans (nat_degree_C_mul_X_pow_le g.leading_coeff g.nat_degree) Og), },
-        { exact nat.lt_succ_iff.mp (gt_of_ge_of_gt Cg (support_card_lt g0)), }, }, }, },
+        { exact nat.lt_succ_iff.mp (gt_of_ge_of_gt Cg (erase_lead_support_card_lt g0)), }, }, }, },
   --first induction: induction step
   { intros N O f g Cf Cg Nf Og,
     by_cases f0 : f=0,
     { rw [f0, zero_mul, rp_zero, rp_zero, zero_mul], },
     { rw [← erase_lead_add_C_mul_X_pow f, add_mul, rp_add, rp_add, add_mul],
-      rw hcf N O _ g _ Cg (le_trans nat_degree_le Nf) Og,
+      rw hcf N O _ g _ Cg (le_trans erase_lead_nat_degree_le Nf) Og,
       rw hcf N O _ g (le_add_left card_support_C_mul_X_pow_le_one) Cg _ Og,
       { exact (le_trans (nat_degree_C_mul_X_pow_le f.leading_coeff f.nat_degree) Nf), },
-      { exact nat.lt_succ_iff.mp (gt_of_ge_of_gt Cf (support_card_lt f0)), }, }, },
+      { exact nat.lt_succ_iff.mp (gt_of_ge_of_gt Cf (erase_lead_support_card_lt f0)), }, }, },
 end
 
 lemma pol_ind_Rhom_prod {rp : ℕ → polynomial R → polynomial R}

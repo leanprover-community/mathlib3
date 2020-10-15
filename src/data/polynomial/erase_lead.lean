726a2770c5f15aa6c2bae1a07dbd2adaab3e8ac0
adomani <adomani@gmail.com>
2020-10-15 15:46:06 +0200

    erased further implied imports

--- src/data/polynomial/erase_lead.lean
+++ src/data/polynomial/erase_lead.lean
@@ -4,7 +4,6 @@ Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Damiano Testa
 -/
 import data.polynomial.degree.basic
-import data.polynomial.degree.trailing_degree
 
 /-!
 # Erase the leading term of a univariate polynomial

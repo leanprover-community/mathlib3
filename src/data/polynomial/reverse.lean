7fba2470ee7e65a5c2ad0367f12963cf67e2b046
adomani <adomani@gmail.com>
2020-10-16 08:43:44 +0200

    removed paragraphs before imports removed a "cyclic" import

--- src/data/polynomial/reverse.lean
+++ src/data/polynomial/reverse.lean
@@ -3,7 +3,6 @@ Copyright (c) 2020 Damiano Testa. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Damiano Testa
 -/
-
 import data.polynomial.erase_lead
 
 /-!

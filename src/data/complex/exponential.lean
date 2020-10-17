10005cb9449663338c0b86e7351fe42c719a451f
Johan Commelin <johan@commelin.net>
2020-10-17 05:15:31 +0000

    move bounds on exp to new file

--- src/data/complex/exponential.lean
+++ src/data/complex/exponential.lean
@@ -1135,44 +1135,6 @@ lemma exp_approx_start (x a b : ℝ)
   abs' (exp x - a) ≤ b :=
 by simpa using h
 
-lemma exp_one_near_10 : abs' (exp 1 - 2244083/825552) ≤ 1/10^10 :=
-begin
-  apply exp_approx_start,
-  iterate 13 { refine exp_1_approx_succ_eq (by norm_num1; refl) (by norm_cast; refl) _ },
-  norm_num1,
-  refine exp_approx_end' _ (by norm_num1; refl) _ (by norm_cast; refl) (by simp) _,
-  rw [_root_.abs_one, abs_of_pos]; norm_num1,
-end
-
-lemma exp_one_near_20 : abs' (exp 1 - 363916618873/133877442384) ≤ 1/10^20 :=
-begin
-  apply exp_approx_start,
-  iterate 21 { refine exp_1_approx_succ_eq (by norm_num1; refl) (by norm_cast; refl) _ },
-  norm_num1,
-  refine exp_approx_end' _ (by norm_num1; refl) _ (by norm_cast; refl) (by simp) _,
-  rw [_root_.abs_one, abs_of_pos]; norm_num1,
-end
-
-lemma exp_one_gt_271828182 : 2.71828182 < exp 1 :=
-lt_of_lt_of_le (by norm_num) (sub_le.1 (abs_sub_le_iff.1 exp_one_near_10).2)
-
-lemma exp_one_lt_271828183 : exp 1 < 2.71828183 :=
-lt_of_le_of_lt (sub_le_iff_le_add.1 (abs_sub_le_iff.1 exp_one_near_10).1) (by norm_num)
-
-lemma exp_neg_one_gt_0367879441 : 0.367879441 < exp (-1) :=
-begin
-  rw [exp_neg, lt_inv _ (exp_pos _)],
-  refine lt_of_le_of_lt (sub_le_iff_le_add.1 (abs_sub_le_iff.1 exp_one_near_10).1) _,
-  all_goals {norm_num},
-end
-
-lemma exp_neg_one_lt_0367879442 : exp (-1) < 0.367879442 :=
-begin
-  rw [exp_neg, inv_lt (exp_pos _)],
-  refine lt_of_lt_of_le _ (sub_le.1 (abs_sub_le_iff.1 exp_one_near_10).2),
-  all_goals {norm_num},
-end
-
 lemma cos_bound {x : ℝ} (hx : abs' x ≤ 1) :
   abs' (cos x - (1 - x ^ 2 / 2)) ≤ abs' x ^ 4 * (5 / 96) :=
 calc abs' (cos x - (1 - x ^ 2 / 2)) = abs (complex.cos x - (1 - x ^ 2 / 2)) :

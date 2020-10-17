e89dbd15095619c010fd3dd15cdb1cd7b68a09b1
faenuccio <filippo.nuccio@univ-st-etienne.fr>
2020-10-17 20:20:32 +0200

    Worked out integrality in 2nd part of proof of Samuel's Thm 1

--- src/ring_theory/dedekind_domain.lean
+++ src/ring_theory/dedekind_domain.lean
@@ -169,7 +169,10 @@ instance principal_ideal_ring.to_dedekind_domain [is_principal_ideal_ring A]
 namespace dedekind_domain
 
 variables {R S : Type*} [integral_domain R] [integral_domain S] [algebra R S]
-variables {L : Type*} [field K] [field L] {f : fraction_map R K}
+variables {L : Type*} [field L] {f : fraction_map R K}
+
+#check fraction_ring R
+#check Type u_3
 
 open finsupp polynomial
 
@@ -184,25 +187,18 @@ end
 
 variables {M : ideal R} [is_maximal M]
 
-lemma if_inv_then_int {I : ideal R} (x : f.codomain) (h_I : I ≠ 0) (h : (1 / ↑I : fractional_ideal f) * ↑I = ↑I) :
-x ∈ (1/↑I : fractional_ideal f).val → f.is_integer x :=
+lemma if_inv_then_int {I : ideal R} (x : f.codomain) (h_I : I ≠ 0) (h : ↑I * (1 / ↑I : fractional_ideal f) = ↑I) :
+x ∈ (1/↑I : fractional_ideal f).val → (f.to_map).is_integral_elem x :=
 begin
-  -- sorry,
 have h_powx_M : ∀ (n : ℕ), (ring.fractional_ideal.span_singleton x^n ) • (↑I : fractional_ideal f) ≤ (↑I : fractional_ideal f),
 intro, induction n with n hn,
   {have h_Id : ↑ I = ↑ I, triv,
-  simp, apply eq.le h_Id,
+   simp, apply eq.le h_Id,
   },
   {intros a ha,sorry,
   },
-intro x,
+intro x,sorry,
 end
-
-#check integrally_closed_iff_integral_implies_integer
-
-def gg: is_dedekind_domain R:=
-
-#check gg.4
 
 local attribute [instance] classical.prop_decidable
 
@@ -211,121 +207,80 @@ lemma maximal_ideal_inv_of_dedekind
   (hnz_M : M ≠ 0) -- this is now superfluous as by def'n DD are not fields
   : is_unit (M : fractional_ideal f) :=
 begin
-   have hnz_Mf : (↑ M : fractional_ideal f) ≠ (0 : fractional_ideal f),
-    apply fractional_ideal.coe_nonzero_of_nonzero.mp hnz_M,
-  have hnz_invMf : (1 : fractional_ideal f)/(↑ M : fractional_ideal f) ≠ (0 : fractional_ideal f),
-    apply fractional_ideal.nonzero_inv_of_nonzero hnz_Mf,
-  have hnz_invinvMf : (1 : fractional_ideal f)/((1 : fractional_ideal f)/(↑ M : fractional_ideal f)) ≠ (0 : fractional_ideal f),
-    apply fractional_ideal.nonzero_inv_of_nonzero hnz_invMf,
-  have h_MfinR : ↑M≤ (1:fractional_ideal f), apply fractional_ideal.coe_ideal_le_one,
-  have hM_inclMinv : (↑ M : fractional_ideal f) ≤ (↑ M : fractional_ideal f)*(1/↑ M : fractional_ideal f),
-  rw ← fractional_ideal.div_mul_inv,apply (fractional_ideal.le_div_iff_mul_le hnz_Mf).mpr (fractional_ideal.mul_self_le_self h_MfinR),exact hnz_Mf,
-  have h_self : (↑ M : fractional_ideal f)≤ (1: fractional_ideal f)*↑ M,
-  simp,exact le_refl ↑ M,
-  have hMMinv_inclR : ↑ M * (1/↑ M) ≤ (1 : fractional_ideal f),
-   rw ← fractional_ideal.inv_inv hnz_Mf at h_self {occs := occurrences.pos [2]},
+have hnz_Mf : (↑ M : fractional_ideal f) ≠ (0 : fractional_ideal f),
+ apply fractional_ideal.coe_nonzero_of_nonzero.mp hnz_M,
+have hnz_invMf : (1 : fractional_ideal f)/(↑ M : fractional_ideal f) ≠ (0 : fractional_ideal f),
+ apply fractional_ideal.nonzero_inv_of_nonzero hnz_Mf,
+have hnz_invinvMf : (1 : fractional_ideal f)/((1 : fractional_ideal f)/(↑ M : fractional_ideal f)) ≠ (0 : fractional_ideal f),
+ apply fractional_ideal.nonzero_inv_of_nonzero hnz_invMf,
+have h_MfinR : ↑M≤ (1:fractional_ideal f), apply fractional_ideal.coe_ideal_le_one,
+have hM_inclMinv : (↑ M : fractional_ideal f) ≤ (↑ M : fractional_ideal f)*(1/↑ M : fractional_ideal f),
+ rw ← fractional_ideal.div_mul_inv, apply (fractional_ideal.le_div_iff_mul_le hnz_Mf).mpr (fractional_ideal.mul_self_le_self h_MfinR),exact hnz_Mf,
+have h_self : (↑ M : fractional_ideal f)≤ (1: fractional_ideal f)*↑ M,
+ ring, exact le_refl ↑ M,
+have hMMinv_inclR : ↑ M * (1/↑ M) ≤ (1 : fractional_ideal f),
+  {rw ← fractional_ideal.inv_inv hnz_Mf at h_self {occs := occurrences.pos [2]},
    rw ← fractional_ideal.div_mul_inv at h_self,
    apply (@fractional_ideal.le_div_iff_mul_le _ _ _ _ _  ( ↑ M : fractional_ideal f) ( 1 : fractional_ideal f ) (( 1 : fractional_ideal f )/↑ M : fractional_ideal f) hnz_invMf).mp,
    exact h_self,  exact hnz_invMf,
-  suffices hprod : ↑M*((1: fractional_ideal f)/↑M)=(1: fractional_ideal f),
-  apply is_unit_of_mul_eq_one ↑M ((1: fractional_ideal f)/↑M) hprod,
-      --now comes the 'hard' part: showing that M*(1/M)≤ 1 implies M*(1/M)=1 since M is max'l.
-  have h_nonfrac : ∃ (I : ideal R), ↑ I=↑M*((1: fractional_ideal f)/↑M),
-    ring,apply (fractional_ideal.le_one_iff_exists_coe_ideal.mp hMMinv_inclR),
-    cases h_nonfrac with I hI,--could replace the above have and this cases by obtain?
-  have h_Iincl : M ≤ I,
-    {suffices h_Iincl_f : (↑M: fractional_ideal f) ≤ (↑I: fractional_ideal f),-- what follows is the proof that h_Iincl_f → h_Iincl
-      intros x hx,
-      let y := f.to_map x,
-      have defy: f.to_map x =y,refl,
-      have hy : y ∈  (↑ M : fractional_ideal f),simp,use x,exact ⟨hx,defy⟩,
-      have hyI : y ∈  (↑ I : fractional_ideal f),
-        apply fractional_ideal.le_iff.mp h_Iincl_f,exact hy,
-      have hxyI : ∃ (x' ∈ I), f.to_map x' = y,
-        apply fractional_ideal.mem_coe.mp hyI,
-      rcases hxyI with ⟨a, ⟨ha, hfa⟩⟩,
-      have hax : a=x,
-        suffices haxf : f.to_map a=f.to_map x,apply fraction_map.injective f haxf,rw hfa,
-      subst hax,exact ha,
-      rw hI, exact hM_inclMinv,
-    },
-    have h_top : I= ⊤,
-      {by_contradiction h_abs,
-      have h_IM : M=I, apply is_maximal.eq_of_le hM h_abs h_Iincl,
-      have h_inveqR : 1/↑ M = (1:fractional_ideal f),
-        {suffices h_invR_dbl : 1/↑ M≤ (1:fractional_ideal f) ∧ (1:fractional_ideal f) ≤ 1/↑ M,
-        apply (has_le.le.le_iff_eq h_invR_dbl.right).mp (h_invR_dbl.left),
-        split,
-          {intros x hx,--the proof that 1/M ≤ 1
-          have h_MMinvI : (1 / ↑M : fractional_ideal f) * ↑M = ↑M,sorry,
-          have h_intx : f.is_integer x,
-          apply if_inv_then_int _ _ hnz_M h_MMinvI hx,
-          apply (integrally_closed_iff_integral_implies_integer K).mp h.4,
-          },
-          {sorry,--the proof that 1≤ 1/M
-
-          },
-        },--end of the proof of h_inveqR
-
-      },--end of the proof of h_top
-    ----
-    ----everything from here...
-    ----
-    -- have h_neqM : M ≠ I,--what Samuel does in his 2nd paragraph of Thm2 "mm'=m" is impossible
-    -- {
-    --   suffices h_invM_neqA : (1/↑ M) ≠ (1:fractional_ideal f),simp at ⊢,--what Samuel prives in his 3rd paragraph of Thm2
-    --   by_contradiction ha,
-    --   have h_invM_inclR : ∀ x ∈ (1/↑ M:fractional_ideal f), x ∈ (1:fractional_ideal f),
-    --   intros x hx,
-    --    {have h_xn : submodule.is_principal.span_singleton (x) * ↑M ≤ ↑ M,
-
-    --    },
-    -- },
-    have h_Iincl_str : M < I, apply lt_of_le_of_ne h_Iincl h_neqM,
-    have h_Itop : I=⊤, apply hM.right I h_Iincl_str,
-  -- have h_Iincl_ss : ↑ M ⊆  I, apply submodule.span_le,
-  --
-  -- the following steps are
-  -- 1) one_fI: 1 ∈ ↑I
-  -- 2) one_I: 1 ∈ I
-  -- 3) h_Itop: from 2)I=R
-  --
-  have one_fI : (1 : K) ∈ (↑I:fractional_ideal f),
-    {have ex_nzM : ∃ y ∈ M, y ≠ (0 : R),sorry,
-    rcases ex_nzM with ⟨ y , h_My , h_nzy ⟩,
-    have h_invyinvM : (1/f.to_map y: K) ∈ (1:fractional_ideal f)/(↑ M:fractional_ideal f),sorry,
-      -- rw hI,
-
-
-    sorry},
-  have one_I : (1 : R)∈ I,
-   {have u1_I : ∃ (u ∈ (I: ideal R)), (f.to_map u) = (1: K),
-    apply (fractional_ideal.mem_coe ).mp one_fI,
-    rcases u1_I with ⟨v,hv,hfv⟩,
-    have h_v1 : v=1,
-    suffices h_v1f : f.to_map v=f.to_map 1, apply fraction_map.injective f h_v1f, rw hfv,
-    apply (ring_hom.map_one f.to_ring_hom).symm,rw h_v1 at hv,exact hv,
-   },
-  have h_Itop' : I=⊤, --this (and its proof) should be replaced by h_Itop once that is OK
-  apply (ideal.eq_top_iff_one I).mpr, exact one_I,
-  ----
-  ----up to here, must disappear...
-  ----
-  have h_okfI : ↑I=(1 : fractional_ideal f), apply fractional_ideal.ext_iff.mp,
-    intros x,split,
-      {intro hx,
-      have h_x' : ∃ (x' ∈  (I : ideal R)), f.to_map x' = x,
-      apply fractional_ideal.mem_coe.mp hx,
-      apply fractional_ideal.mem_one_iff.mpr,simp * at *,
-      },
-      {intro hx,
-      have h_x' : ∃ x' ∈ (1:ideal R),  f.to_map x' = x,
-      apply fractional_ideal.mem_coe.mp hx,
-      rw h_Itop',simp,
-      rcases h_x' with ⟨a, ⟨ha,hfa⟩ ⟩,
-            use a,exact hfa,
-      },
-    rw ← hI,exact h_okfI,
+  },
+suffices hprod : ↑M*((1: fractional_ideal f)/↑M)=(1: fractional_ideal f),
+ apply is_unit_of_mul_eq_one ↑M ((1: fractional_ideal f)/↑M) hprod,
+obtain ⟨ I, hI ⟩ : ∃ (I : ideal R), ↑ I=↑M*((1: fractional_ideal f)/↑M),
+ ring, apply (fractional_ideal.le_one_iff_exists_coe_ideal.mp hMMinv_inclR),
+have h_Iincl : M ≤ I,
+  {suffices h_Iincl_f : (↑M: fractional_ideal f) ≤ (↑I: fractional_ideal f),-- what follows is the proof that h_Iincl_f → h_Iincl
+   intros x hx,
+   let y := f.to_map x,
+   have defy: f.to_map x = y, refl,
+   have hy : y ∈  (↑ M : fractional_ideal f), simp, use x, exact ⟨hx,defy⟩,
+   have hyI : y ∈  (↑ I : fractional_ideal f),
+    apply fractional_ideal.le_iff.mp h_Iincl_f, exact hy,
+   obtain ⟨a, ⟨ ha, hfa ⟩ ⟩ : ∃ (x' ∈ I), f.to_map x' = y,
+    apply fractional_ideal.mem_coe.mp hyI,
+   have hax : a=x,
+   suffices haxf : f.to_map a=f.to_map x, apply fraction_map.injective f haxf,
+    rw hfa, subst hax, exact ha, rw hI, exact hM_inclMinv,
+  },
+have h_top : I= ⊤,
+  {by_contradiction h_abs,
+    have h_IM : I = M, apply (is_maximal.eq_of_le hM h_abs h_Iincl).symm,
+    have h_inveqR : 1/↑ M = (1:fractional_ideal f),
+      {suffices h_invR_dbl : 1/↑ M≤ (1:fractional_ideal f) ∧ (1:fractional_ideal f) ≤ 1/↑ M,
+       apply (has_le.le.le_iff_eq h_invR_dbl.right).mp (h_invR_dbl.left),
+       split,
+        {intros x hx,--the proof that 1/M ≤ 1
+         have h_MMinvI : ↑ M * (1 / ↑M : fractional_ideal f) = ↑M, rw ← hI, rw h_IM,
+         have h_integralfx : (f.to_map).is_integral_elem x, apply if_inv_then_int _ x hnz_M h_MMinvI hx,
+         have h_intxR : x ∈ (integral_closure R f.codomain), apply h_integralfx,
+         have h_xintegral : x ∈ (⊥  : subalgebra R f.codomain), rw ← ((is_dedekind_domain_iff _ _ f).mp h).right.right.right, exact h_intxR,
+         have h_coe : ((⊥  : subalgebra R f.codomain) : submodule R f.codomain) = localization_map.coe_submodule f ⊤,
+          --  have try : (⊥  : subalgebra R f.codomain) = (f.to_map).alg_hom.range, →  I believe that the localization map should be upbgraded to a algebra hom (see l. 91 of localization.lean)
+         sorry,
+         suffices h_final : x ∈ localization_map.coe_submodule f ⊤,
+         simpa, rw ← h_coe, exact h_xintegral,
+        },
+        {sorry,--the proof that 1≤ 1/M
+        --
+        },
+      },--end of the proof of h_inveqR (assuming h_abs)
+    sorry, -- this sorry (showing that h_inveqR is absurd) must be replaced by the 3rd part in Samuel's proof, using his Lemma 3 on prime ideals
+  },--end of the proof of h_top
+have h_okfI : ↑I=(1 : fractional_ideal f), apply fractional_ideal.ext_iff.mp,
+ intros x,split,
+  {intro hx,
+   have h_x' : ∃ (x' ∈  (I : ideal R)), f.to_map x' = x,
+   apply fractional_ideal.mem_coe.mp hx,
+   apply fractional_ideal.mem_one_iff.mpr, simp * at *,
+  },
+  {intro hx,
+   have h_x' : ∃ x' ∈ (1:ideal R),  f.to_map x' = x,
+   apply fractional_ideal.mem_coe.mp hx,
+   rw h_top,simp,
+   rcases h_x' with ⟨a, ⟨ha,hfa⟩ ⟩,
+   use a,exact hfa,
+  },
+rw ← hI, exact h_okfI,
 end
 
 

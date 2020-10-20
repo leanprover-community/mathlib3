95ac5c6ff92ee7fa99df04314f95f27e264bc640
Scott Morrison <scott.morrison@gmail.com>
2020-10-21 09:59:08 +1100

    use ordered_semimodule

--- src/algebra/algebra/ordered.lean
+++ src/algebra/algebra/ordered.lean
@@ -17,40 +17,23 @@ The prototypical example is 2x2 matrices over the reals or complexes (or indeed 
 where the ordering the one determined by the positive cone of positive operators,
 i.e. `A ≤ B` iff `B - A = star R * R` for some `R`.
 
+## Implementation
+
+Because the axioms for an ordered algebra are exactly the same as those for the underlying
+module being ordered, we don't actually introduce a new class, but just use the `ordered_semimodule`
+mixin.
+
 ## Tags
 
 ordered algebra
 -/
 
-
-/--
-An ordered algebra over an ordered commutative semiring is an algebra
-for which the scalar multiplication is compatible with the orders.
--/
-@[protect_proj, ancestor semimodule]
-class ordered_algebra (R A : Type*)
-  [ordered_comm_semiring R] [ordered_semiring A] extends algebra R A :=
-(smul_lt_smul_of_pos : ∀ {a b : A}, ∀ {r : R}, a < b → 0 < r → r • a < r • b)
-(lt_of_smul_lt_smul_of_nonneg : ∀ {a b : A}, ∀ {r : R}, r • a < r • b → 0 ≤ r → a < b)
-
-namespace ordered_algebra
+section ordered_algebra
 
 variables {R A : Type*} {a b : A} {r : R}
 
 section
-variables [ordered_comm_semiring R] [ordered_semiring A] [ordered_algebra R A]
-
-/--
-The underlying ordered module of an ordered algebra.
--/
-@[priority 100]
-instance to_ordered_semimodule : ordered_semimodule R A :=
-{ ..‹ordered_algebra R A› }
-
-end
-
-section
-variables [ordered_comm_ring R] [ordered_ring A] [ordered_algebra R A]
+variables [ordered_comm_ring R] [ordered_ring A] [algebra R A] [ordered_semimodule R A]
 
 lemma algebra_map_monotone : monotone (algebra_map R A) :=
 λ a b h,
@@ -63,13 +46,13 @@ end
 
 end
 
-end ordered_algebra
+section ordered_algebra
 
 section instances
 
 variables {R : Type*} [linear_ordered_comm_ring R]
 
-instance linear_ordered_comm_ring.to_ordered_algebra : ordered_algebra R R :=
+instance linear_ordered_comm_ring.to_ordered_semimodule : ordered_semimodule R R :=
 { smul_lt_smul_of_pos      := ordered_semiring.mul_lt_mul_of_pos_left,
   lt_of_smul_lt_smul_of_nonneg  := λ _ _ _, lt_of_mul_lt_mul_left }
 

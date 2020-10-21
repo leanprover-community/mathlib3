d810e89642ee8be7c76bcf9a213578dab3673f1e
Thomas Browning <tb65536@uw.edu>
2020-10-20 20:32:41 -0700

    fixed fields

--- src/field_theory/galois.lean
+++ src/field_theory/galois.lean
@@ -35,10 +35,10 @@ instance aut : group (E ≃ₐ[F] E) := {
     mul := λ ϕ ψ, ψ.trans ϕ,
     mul_assoc := λ ϕ ψ χ, rfl,
     one := 1,
-    one_mul := λ ϕ, by {ext,refl},
-    mul_one := λ ϕ, by {ext,refl},
+    one_mul := λ ϕ, by {ext, refl},
+    mul_one := λ ϕ, by {ext, refl},
     inv := alg_equiv.symm,
-    mul_left_inv := λ ϕ, by {ext,exact alg_equiv.symm_apply_apply ϕ a},
+    mul_left_inv := λ ϕ, by {ext, exact alg_equiv.symm_apply_apply ϕ a},
 }
 
 instance aut_action : faithful_mul_semiring_action (E ≃ₐ[F] E) E := {
@@ -52,46 +52,63 @@ instance aut_action : faithful_mul_semiring_action (E ≃ₐ[F] E) E := {
     eq_of_smul_eq_smul' := λ _ _, alg_equiv.ext,
 }
 
-end
-
-section
-
-variables {F : Type*} [field F] {E : Type*} [field E] [algebra F E]
-
-instance subgroup_action (H : subgroup (E ≃ₐ[F] E)) : faithful_mul_semiring_action H E :=
-  mul_action.submonoid_action E H
-
-def fixed_field (H : subgroup (E ≃ₐ[F] E)) : intermediate_field F E := {
-  carrier := mul_action.fixed_points H E,
+def aut_fixed_field : intermediate_field F E := {
+  carrier := mul_action.fixed_points (E ≃ₐ[F] E) E,
+  zero_mem' := smul_zero,
+  add_mem' := λ _ _ hx hy _, by rw [smul_add, hx, hy],
+  neg_mem' := λ _ hx _, by rw [smul_neg, hx],
+  one_mem' := smul_one,
+  mul_mem' := λ _ _ hx hy _, by rw [smul_mul', hx, hy],
+  inv_mem' := λ _ hx _, by rw [smul_inv, hx],
+  algebra_map_mem' := λ _ _, alg_equiv.commutes _ _,
 }
 
-end
-
-/-instance aut_action : faithful_mul_semiring_action (E ≃ₐ[F] E) E := {
-    smul := alg_equiv.to_fun,
-    smul_zero := alg_equiv.map_zero,
-    smul_one := alg_equiv.map_one,
-    one_smul := λ _, rfl,
-    smul_add := alg_equiv.map_add,
-    smul_mul := alg_equiv.map_mul,
-    mul_smul := λ _ _ _, rfl,
-    eq_of_smul_eq_smul' := λ _ _, alg_equiv.ext,
-}
-
-instance : is_scalar_tower F (E ≃ₐ[F] E) E := {
-
-}
+lemma aut_fixed_field_findim_eq_card [finite_dimensional F E] :
+  finite_dimensional.findim (aut_fixed_field F E) E = fintype.card (E ≃ₐ[F] E) :=
+fixed_points.findim_eq_card (E ≃ₐ[F] E) E
 
 lemma card_aut_le_dim [finite_dimensional F E] :
   fintype.card (E ≃ₐ[F] E) ≤ finite_dimensional.findim F E :=
 begin
-  haveI := fixed_points.finite_dimensional (E ≃ₐ[F] E) E,
-  have key := fixed_points.findim_eq_card (E ≃ₐ[F] E) E,
-  rw ← key,
+  rw ← aut_fixed_field_findim_eq_card F E,
+  rw ← finite_dimensional.findim_mul_findim F (aut_fixed_field F E) E,
+  exact nat.le_mul_of_pos_left finite_dimensional.findim_pos,
 end
 
+end
+
+section
+
+variables {F : Type*} [field F] {E : Type*} [field E] [algebra F E] (H : subgroup (E ≃ₐ[F] E))
+
+instance subgroup_action : faithful_mul_semiring_action H E := {
+  smul := λ h x, (↑h : (E ≃ₐ[F] E)) • x,
+  smul_zero := λ _, alg_equiv.map_zero _,
+  smul_add := λ _, alg_equiv.map_add _,
+  one_smul := λ _, rfl,
+  smul_one := λ _, alg_equiv.map_one _,
+  mul_smul := λ _ _ _, rfl,
+  smul_mul := λ _, alg_equiv.map_mul _,
+  eq_of_smul_eq_smul' := λ x y z, subtype.ext (alg_equiv.ext z),
+}
+
+def subgroup_fixed_field : intermediate_field F E := {
+  carrier := mul_action.fixed_points H E,
+  zero_mem' := smul_zero,
+  add_mem' := λ _ _ hx hy _, by rw [smul_add, hx, hy],
+  neg_mem' := λ _ hx _, by rw [smul_neg, hx],
+  one_mem' := smul_one,
+  mul_mem' := λ _ _ hx hy _, by rw [smul_mul', hx, hy],
+  inv_mem' := λ _ hx _, by rw [smul_inv, hx],
+  algebra_map_mem' := λ _ _, alg_equiv.commutes _ _,
+}
+
+lemma subgroup_fixed_field_findim_eq_card [finite_dimensional F E] :
+  finite_dimensional.findim (subgroup_fixed_field H) E = fintype.card H :=
+fixed_points.findim_eq_card H E
+
 lemma lem2 (h : ∃ p : polynomial F, p.separable ∧ p.is_splitting_field F E) :
-  (mul_action.fixed_points (E ≃ₐ[F] E) E) = (⊥ : intermediate_field F E) :=
+  aut_fixed_field F E = ⊥ :=
 begin
   cases h with p hp,
   haveI := hp.2,
@@ -100,5 +117,7 @@ begin
   haveI := fintype.of_injective (@alg_equiv.to_alg_hom F E E _ _ _ _ _)
     (λ _ _ h, alg_equiv.ext (alg_hom.ext_iff.mp h)),
   haveI := fixed_points.finite_dimensional (E ≃ₐ[F] E) E,
-  have key := fixed_points.findim_eq_card (E ≃ₐ[F] E) E,
-end -/
+  have key := aut_fixed_field_findim_eq_card F E,
+end
+
+end

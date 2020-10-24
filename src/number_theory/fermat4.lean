2dc3a3ae96b1c306432e03cc8ecb12fcda2fa4ae
pvanwamelen <pvanwamelen@air.org>
2020-10-24 11:51:58 -0400

    Use fermat_42 namespace and get rid of c ne 0 hypothesis

--- src/number_theory/fermat4.lean
+++ src/number_theory/fermat4.lean
@@ -16,13 +16,15 @@ open_locale classical
 /-- Shorthand for three non-zero integers `a`, `b`, and `c` satisfying `a ^ 4 + b ^ 4 = c ^ 2`.
 We will show that no integers satisfy this equation. Clearly Fermat's Last theorem for n = 4
 follows. -/
-def fermat_42 (a b c : ℤ) : Prop := a ≠ 0 ∧ b ≠ 0 ∧ c ≠ 0 ∧ a ^ 4 + b ^ 4 = c ^ 2
+def fermat_42 (a b c : ℤ) : Prop := a ≠ 0 ∧ b ≠ 0 ∧ a ^ 4 + b ^ 4 = c ^ 2
 
-lemma fermat_42_comm {a b c : ℤ} :
+namespace fermat_42
+
+lemma comm {a b c : ℤ} :
  (fermat_42 a b c) ↔ (fermat_42 b a c) :=
  by { delta fermat_42, rw add_comm, tauto }
 
-lemma fermat_42_mul {a b c k : ℤ} (hk0 : k ≠ 0) :
+lemma mul {a b c k : ℤ} (hk0 : k ≠ 0) :
   fermat_42 a b c ↔ fermat_42 (k * a) (k * b) (k ^ 2 * c) :=
 begin
   delta fermat_42,
@@ -30,30 +32,36 @@ begin
   { intro f42,
     split, { exact mul_ne_zero hk0 f42.1 },
     split, { exact mul_ne_zero hk0 f42.2.1 },
-    split, { apply mul_ne_zero, { rw pow_two, exact mul_ne_zero hk0 hk0 }, exact f42.2.2.1 },
     { calc (k * a) ^ 4 + (k * b) ^ 4
           = k ^ 4 * (a ^ 4 + b ^ 4) : by ring
-      ... = k ^ 4 * c ^ 2 : by rw f42.2.2.2
+      ... = k ^ 4 * c ^ 2 : by rw f42.2.2
       ... = (k ^ 2 * c) ^ 2 : by ring }},
   { intro f42,
     split, { exact right_ne_zero_of_mul f42.1 },
     split, { exact right_ne_zero_of_mul f42.2.1 },
-    split, { exact right_ne_zero_of_mul f42.2.2.1 },
     apply (mul_right_inj' (pow_ne_zero 4 hk0)).mp,
     { calc k ^ 4 * (a ^ 4 + b ^ 4)
           = (k * a) ^ 4 + (k * b) ^ 4 : by ring
-      ... = (k ^ 2 * c) ^ 2 : by rw f42.2.2.2
+      ... = (k ^ 2 * c) ^ 2 : by rw f42.2.2
       ... = k ^ 4 * c ^ 2 : by ring }}
+end
+
+lemma ne_zero {a b c : ℤ} (h : fermat_42 a b c) : c ≠ 0 :=
+begin
+  apply ne_zero_pow (dec_trivial : 2 ≠ 0), apply ne_of_gt,
+  rw [← h.2.2, (by ring : a ^ 4 + b ^ 4 = (a ^ 2) ^ 2 + (b ^ 2) ^ 2)],
+  exact add_pos (pow_two_pos_of_ne_zero _ (pow_ne_zero 2 h.1))
+      (pow_two_pos_of_ne_zero _ (pow_ne_zero 2 h.2.1))
 end
 
 /-- We say a solution to `a ^ 4 + b ^ 4 = c ^ 2` is minimal if there are no other solution with
 a smaller c (in absolute value). -/
-def minimal_fermat_42 (a b c : ℤ) : Prop :=
+def minimal (a b c : ℤ) : Prop :=
   (fermat_42 a b c) ∧ ∀ (a1 b1 c1 : ℤ), (fermat_42 a1 b1 c1) → int.nat_abs c ≤ int.nat_abs c1
 
 /-- if we have a solution to `a ^ 4 + b ^ 4 = c ^ 2` then there must be a minimal one. -/
-lemma exists_minimal_fermat_42 {a b c : ℤ} (h : fermat_42 a b c) :
-  ∃ (a0 b0 c0), (minimal_fermat_42 a0 b0 c0) :=
+lemma exists_minimal {a b c : ℤ} (h : fermat_42 a b c) :
+  ∃ (a0 b0 c0), (minimal a0 b0 c0) :=
 begin
   let all : set (ℤ × ℤ × ℤ) := {s | fermat_42 s.1 s.2.1 s.2.2},
   let S : set (ℕ) := { n | ∃ (s : ℤ × ℤ × ℤ), fermat_42 s.1 s.2.1 s.2.2 ∧ n = int.nat_abs s.2.2},
@@ -78,7 +86,7 @@ begin
 end
 
 /-- a minimal solution to `a ^ 4 + b ^ 4 = c ^ 2` must have `a` and `b` coprime. -/
-lemma coprime_of_minimal_fermat_42 {a b c : ℤ} (h : minimal_fermat_42 a b c) :
+lemma coprime_of_minimal {a b c : ℤ} (h : minimal a b c) :
   int.gcd a b = 1 :=
 begin
   by_contradiction hab,
@@ -87,72 +95,74 @@ begin
   obtain ⟨b1, hb1⟩ := exists_eq_mul_right_of_dvd (int.coe_nat_dvd_left.mpr hpb),
   have hpc : (p : ℤ) ^ 2 ∣ c,
   { apply (int.pow_dvd_pow_iff (dec_trivial : 0 < 2)).mp,
-    rw [← h.1.2.2.2, ha1, hb1],
+    rw [← h.1.2.2, ha1, hb1],
     apply dvd.intro (a1 ^ 4 + b1 ^ 4), ring },
   obtain ⟨c1, hc1⟩ := exists_eq_mul_right_of_dvd hpc,
   have hf : fermat_42 a1 b1 c1,
-  { apply (fermat_42_mul (int.coe_nat_ne_zero.mpr (nat.prime.ne_zero hp))).mpr,
+  { apply (fermat_42.mul (int.coe_nat_ne_zero.mpr (nat.prime.ne_zero hp))).mpr,
     rw [← ha1, ← hb1, ← hc1], exact h.1 },
   apply nat.le_lt_antisymm (h.2 _ _ _ hf),
   rw [hc1, int.nat_abs_mul],
   rw [← one_mul (int.nat_abs c1)] {occs := occurrences.pos [1]},
-  rw mul_lt_mul_right (nat.pos_of_ne_zero (int.nat_abs_ne_zero_of_ne_zero hf.right.right.left)),
+  rw mul_lt_mul_right (nat.pos_of_ne_zero (int.nat_abs_ne_zero_of_ne_zero (ne_zero hf))),
   rw [pow_two, int.nat_abs_mul, int.nat_abs_of_nat],
   refine nat.sqrt_lt.mp _, rw [← one_mul 1, nat.sqrt_eq], exact nat.prime.one_lt hp
 end
 
 /-- We can swap `a` and `b` in a minimal solution to `a ^ 4 + b ^ 4 = c ^ 2`. -/
-lemma minimal_fermat_42_comm {a b c : ℤ} :
- (minimal_fermat_42 a b c) → (minimal_fermat_42 b a c) :=
+lemma minimal_comm {a b c : ℤ} :
+ (minimal a b c) → (minimal b a c) :=
 begin
   rintros ⟨h1, h2⟩,
-  split, { exact fermat_42_comm.mp h1 },
+  split, { exact fermat_42.comm.mp h1 },
   exact h2
 end
 
 /-- We can assume that a minimal solution to `a ^ 4 + b ^ 4 = c ^ 2` has positive `c`. -/
-lemma neg_of_minimal_fermat_42 {a b c : ℤ} :
- (minimal_fermat_42 a b c) → (minimal_fermat_42 a b (-c)) :=
+lemma neg_of_minimal {a b c : ℤ} :
+ (minimal a b c) → (minimal a b (-c)) :=
 begin
-  rintros ⟨⟨ha, hb, hc, heq⟩, h2⟩,
+  rintros ⟨⟨ha, hb, heq⟩, h2⟩,
   split,
-  { apply and.intro ha (and.intro hb (and.intro (neg_ne_zero.mpr hc) _)),
+  { apply and.intro ha (and.intro hb _),
     rw heq, exact (neg_square c).symm },
   rw (int.nat_abs_neg c), exact h2
 end
 
 /-- We can assume that a minimal solution to `a ^ 4 + b ^ 4 = c ^ 2` has `a` odd. -/
-lemma exists_odd_minimal_fermat_42 {a b c : ℤ} (h : fermat_42 a b c) :
-  ∃ (a0 b0 c0), (minimal_fermat_42 a0 b0 c0) ∧ a0 % 2 = 1 :=
+lemma exists_odd_minimal {a b c : ℤ} (h : fermat_42 a b c) :
+  ∃ (a0 b0 c0), (minimal a0 b0 c0) ∧ a0 % 2 = 1 :=
 begin
-  obtain ⟨a0, b0, c0, hf⟩ := exists_minimal_fermat_42 h,
+  obtain ⟨a0, b0, c0, hf⟩ := exists_minimal h,
   cases int.mod_two_eq_zero_or_one a0 with hap hap,
   { cases int.mod_two_eq_zero_or_one b0 with hbp hbp,
     { exfalso,
       have h1 : 2 ∣ (int.gcd a0 b0 : ℤ),
       { exact int.dvd_gcd (int.dvd_of_mod_eq_zero hap) (int.dvd_of_mod_eq_zero hbp) },
-        rw (coprime_of_minimal_fermat_42 hf) at h1, revert h1, norm_num },
-      { exact ⟨b0, ⟨a0, ⟨c0, minimal_fermat_42_comm hf, hbp⟩⟩⟩ }},
+        rw (coprime_of_minimal hf) at h1, revert h1, norm_num },
+      { exact ⟨b0, ⟨a0, ⟨c0, minimal_comm hf, hbp⟩⟩⟩ }},
   exact ⟨a0, ⟨b0, ⟨c0 , hf, hap⟩⟩⟩,
 end
 
 /-- We can assume that a minimal solution to `a ^ 4 + b ^ 4 = c ^ 2` has
 `a` odd and `c` positive. -/
-lemma exists_pos_odd_minimal_fermat_42 {a b c : ℤ} (h : fermat_42 a b c) :
-  ∃ (a0 b0 c0), (minimal_fermat_42 a0 b0 c0) ∧ a0 % 2 = 1 ∧ 0 < c0  :=
+lemma exists_pos_odd_minimal {a b c : ℤ} (h : fermat_42 a b c) :
+  ∃ (a0 b0 c0), (minimal a0 b0 c0) ∧ a0 % 2 = 1 ∧ 0 < c0  :=
 begin
-  obtain ⟨a0, b0, c0, hf, hc⟩ := exists_odd_minimal_fermat_42 h,
+  obtain ⟨a0, b0, c0, hf, hc⟩ := exists_odd_minimal h,
   by_cases h0 : 0 < c0,
   { use [a0, b0, c0], tauto },
   { use [a0, b0, -c0],
-    apply and.intro (neg_of_minimal_fermat_42 hf),
+    apply and.intro (neg_of_minimal hf),
     apply and.intro hc,
     apply neg_pos.mpr, apply not_le.mp, intro h2,
-    have h3 : 0 < c0, { exact lt_of_le_of_ne h2 (ne.symm hf.1.2.2.1) },
+    have h3 : 0 < c0, { exact lt_of_le_of_ne h2 (ne.symm (ne_zero hf.1)) },
     revert h3, exact h0 }
 end
 
-lemma coprime_of_sqr_sum {m r s : ℤ} (h : m = r ^ 2 + s ^ 2) (h2 : int.gcd r s = 1) :
+end fermat_42
+
+lemma int.coprime_of_sqr_sum {m r s : ℤ} (h : m = r ^ 2 + s ^ 2) (h2 : int.gcd r s = 1) :
   int.gcd m r = 1 :=
 begin
   by_contradiction hg,
@@ -167,21 +177,23 @@ begin
   rw pow_two, exact dvd_mul_of_dvd_left (int.coe_nat_dvd_left.mpr hr) r
 end
 
-lemma coprime_of_sqr_sum' {m r s : ℤ} (h : m = r ^ 2 + s ^ 2) (h2 : int.gcd r s = 1) :
+lemma int.coprime_of_sqr_sum' {m r s : ℤ} (h : m = r ^ 2 + s ^ 2) (h2 : int.gcd r s = 1) :
   int.gcd m (r * s) = 1 :=
 begin
   dunfold int.gcd,
   rw int.nat_abs_mul,
-  apply nat.coprime.mul_right (coprime_of_sqr_sum h h2),
+  apply nat.coprime.mul_right (int.coprime_of_sqr_sum h h2),
   rw add_comm at h,
   rw [int.gcd_comm r s] at h2,
-  exact coprime_of_sqr_sum h h2
+  exact int.coprime_of_sqr_sum h h2
 end
+
+namespace fermat_42
 
 -- If we have a solution to a ^ 4 + b ^ 4 = c ^ 2, we can construct a smaller one. This
 -- implies there can't be a smallest solution.
-lemma not_minimal_fermat_42 {a b c : ℤ}
-  (h : minimal_fermat_42 a b c) (ha2 : a % 2 = 1) (hc : 0 < c) : false :=
+lemma not_minimal {a b c : ℤ}
+  (h : minimal a b c) (ha2 : a % 2 = 1) (hc : 0 < c) : false :=
 begin
   -- Use the fact that a ^ 2, b ^ 2, c form a pythagorean triple to obtain m and n such that
   -- a ^ 2 = m ^ 2 - n ^ 2, b ^ 2 = 2 * m * n and c = m ^ 2 + n ^ 2
@@ -189,7 +201,7 @@ begin
   have ht : pythagorean_triple (a ^ 2) (b ^ 2) c,
   { calc ((a ^ 2) * (a ^ 2)) + ((b ^ 2) * (b ^ 2))
         = a ^ 4 + b ^ 4 : by ring
-    ... = c ^ 2 : by rw h.1.2.2.2
+    ... = c ^ 2 : by rw h.1.2.2
     ... = c * c : by rw pow_two
   },
   -- coprime requirement:
@@ -198,7 +210,7 @@ begin
     rw [pow_two, int.nat_abs_mul],
     apply nat.coprime.mul;
     { rw [pow_two, int.nat_abs_mul], apply nat.coprime.mul_right;
-      apply (coprime_of_minimal_fermat_42 h) } },
+      apply (coprime_of_minimal h) } },
   -- in order to reduce the possibilities we get from the classification of pythagorean triples
   -- it helps if we know the parity of a ^ 2 (and the sign of c):
   have ha22 : a ^ 2 % 2 = 1, { rw [pow_two, int.mul_mod, ha2], norm_num },
@@ -233,7 +245,7 @@ begin
   -- Now use the fact that (b / 2) ^ 2 = m * r * s, and m, r and s are pairwise coprime to obtain
   -- i, j and k such that m = i ^ 2, r = j ^ 2 and s = k ^ 2.
   -- m and r * s are coprime because m = r ^ 2 + s ^ 2 and r and s are coprime.
-  have hcp : int.gcd m (r * s) = 1, exact coprime_of_sqr_sum' htt3 htt4,
+  have hcp : int.gcd m (r * s) = 1, exact int.coprime_of_sqr_sum' htt3 htt4,
   -- b is even because b ^ 2 = 2 * m * n.
   have hb2 : 2 ∣ b,
   { apply (or_self _).mp (int.prime.dvd_mul' (by norm_num : nat.prime 2) _),
@@ -259,8 +271,6 @@ begin
     rw ← h1 at hit,
     apply absurd h4 (not_lt.mpr hit) },
   replace hi : m = i ^ 2, { apply or.resolve_right hi hi' },
-  have hi0 : i ≠ 0,
-  { intro h0, rw [h0, zero_pow (dec_trivial : 0 < 2)] at hi, apply absurd hi (ne_of_gt h4) },
   rw mul_comm at hs,
   rw [int.gcd_comm] at hcp,
   -- obtain d such that r * s = d ^ 2
@@ -301,22 +311,19 @@ begin
     exact lt_add_of_pos_right (m ^ 2) (pow_two_pos_of_ne_zero n hn) },
   have hic' : int.nat_abs c ≤ int.nat_abs i,
   { apply (h.2 j k i),
-    exact and.intro hj0 (and.intro hk0 (and.intro hi0 hh.symm)) },
+    exact and.intro hj0 (and.intro hk0 hh.symm) },
   apply absurd (not_le_of_lt hic) (not_not.mpr hic')
 end
+
+end fermat_42
 
 lemma not_fermat_42 {a b c : ℤ} (ha : a ≠ 0) (hb : b ≠ 0) :
   a ^ 4 + b ^ 4 ≠ c ^ 2 :=
 begin
   intro h,
-  have hc : c ≠ 0,
-  { apply ne_zero_pow (dec_trivial : 2 ≠ 0), apply ne_of_gt,
-    rw [← h, (by ring : a ^ 4 + b ^ 4 = (a ^ 2) ^ 2 + (b ^ 2) ^ 2)],
-    exact add_pos (pow_two_pos_of_ne_zero _ (pow_ne_zero 2 ha))
-      (pow_two_pos_of_ne_zero _ (pow_ne_zero 2 hb)) },
   obtain ⟨a0, b0, c0, ⟨hf, h2, hp⟩⟩ :=
-    exists_pos_odd_minimal_fermat_42 (and.intro ha (and.intro hb (and.intro hc h))),
-  apply not_minimal_fermat_42 hf h2 hp
+    fermat_42.exists_pos_odd_minimal (and.intro ha (and.intro hb h)),
+  apply fermat_42.not_minimal hf h2 hp
 end
 
 theorem not_fermat_4 {a b c : ℤ} (ha : a ≠ 0) (hb : b ≠ 0) :

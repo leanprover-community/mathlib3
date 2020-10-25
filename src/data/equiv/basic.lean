125fbecad520436227df241f0cc0fd1e4061b2db
Yury G. Kudryashov <urkud@urkud.name>
2020-10-25 01:46:02 -0400

    Prove `semiconj₂` versions, add `equiv.semiconj_conj`

--- src/data/equiv/basic.lean
+++ src/data/equiv/basic.lean
@@ -372,30 +372,6 @@ rfl
   (arrow_congr e₁ e₂).symm = arrow_congr e₁.symm e₂.symm :=
 rfl
 
-section binary_op
-
-variables {α₁ β₁ : Type*} (f : α₁ → α₁ → α₁) (e : α₁ ≃ β₁)
-
-instance [is_associative α₁ f] :
-  is_associative β₁ (equiv.arrow_congr e (equiv.arrow_congr e e) f) :=
-⟨λ x y z, by simp [@is_associative.assoc _ f]⟩
-
-instance [is_idempotent α₁ f] :
-  is_idempotent β₁ (equiv.arrow_congr e (equiv.arrow_congr e e) f) :=
-⟨λ x, by simp [@is_idempotent.idempotent _ f]⟩
-
-instance [is_left_cancel α₁ f] :
-  is_left_cancel β₁ (equiv.arrow_congr e (equiv.arrow_congr e e) f) :=
-⟨λ x y z hx, equiv.injective e.symm $
- by { simp at hx, rw @is_left_cancel.left_cancel _ f _ _ _ _ hx }⟩
-
-instance [is_right_cancel α₁ f] :
-  is_right_cancel β₁ (equiv.arrow_congr e (equiv.arrow_congr e e) f) :=
-⟨λ x y z hx, equiv.injective e.symm $
- by { simp at hx, rw @is_right_cancel.right_cancel _ f _ _ _ _ hx }⟩
-
-end binary_op
-
 /--
 A version of `equiv.arrow_congr` in `Type`, rather than `Sort`.
 
@@ -444,6 +420,32 @@ rfl
 lemma conj_comp (e : α ≃ β) (f₁ f₂ : α → α) :
   e.conj (f₁ ∘ f₂) = (e.conj f₁) ∘ (e.conj f₂) :=
 by apply arrow_congr_comp
+
+section binary_op
+
+variables {α₁ β₁ : Type*} (e : α₁ ≃ β₁) (f : α₁ → α₁ → α₁)
+
+lemma semiconj_conj (f : α₁ → α₁) : semiconj e f (e.conj f) := λ x, by simp
+
+lemma semiconj₂_conj : semiconj₂ e f (e.arrow_congr e.conj f) := λ x y, by simp
+
+instance [is_associative α₁ f] :
+  is_associative β₁ (e.arrow_congr (e.arrow_congr e) f) :=
+(e.semiconj₂_conj f).is_associative_right e.surjective
+
+instance [is_idempotent α₁ f] :
+  is_idempotent β₁ (e.arrow_congr (e.arrow_congr e) f) :=
+(e.semiconj₂_conj f).is_idempotent_right e.surjective
+
+instance [is_left_cancel α₁ f] :
+  is_left_cancel β₁ (e.arrow_congr (e.arrow_congr e) f) :=
+⟨e.surjective.forall₃.2 $ λ x y z, by simpa using @is_left_cancel.left_cancel _ f _ x y z⟩
+
+instance [is_right_cancel α₁ f] :
+  is_right_cancel β₁ (e.arrow_congr (e.arrow_congr e) f) :=
+⟨e.surjective.forall₃.2 $ λ x y z, by simpa using @is_right_cancel.right_cancel _ f _ x y z⟩
+
+end binary_op
 
 /-- `punit` sorts in any two universes are equivalent. -/
 def punit_equiv_punit : punit.{v} ≃ punit.{w} :=

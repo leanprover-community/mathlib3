f07af0cb6743e3dc151d2e162a3ebddaada8f40c
Thomas Browning <tb65536@uw.edu>
2020-10-25 17:40:32 -0700

    copyright

--- src/field_theory/galois.lean
+++ src/field_theory/galois.lean
@@ -1,3 +1,9 @@
+/-
+Copyright (c) 2020 Thomas Browning and Patrick Lutz. All rights reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
+Authors: Thomas Browning and Patrick Lutz
+-/
+
 import field_theory.normal
 import field_theory.primitive_element
 import field_theory.fixed

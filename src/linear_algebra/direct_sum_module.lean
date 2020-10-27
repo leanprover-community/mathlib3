3c73e4858c825d1e6aec8d20a81ccb0818ec15f4
Eric Wieser <wieser.eric@gmail.com>
2020-10-27 16:33:39 +0000

    chore(data/dfinsupp): Provide dfinsupp with a semimodule instance
    finsupp already has one, it seems pointless to hide the one for dfinsupp behind a def.

--- src/linear_algebra/direct_sum_module.lean
+++ src/linear_algebra/direct_sum_module.lean
@@ -36,7 +36,7 @@ open_locale direct_sum
 
 variables {R ι M}
 
-instance : semimodule R (⨁ i, M i) := dfinsupp.to_semimodule
+instance : semimodule R (⨁ i, M i) := dfinsupp.semimodule
 
 include dec_ι
 

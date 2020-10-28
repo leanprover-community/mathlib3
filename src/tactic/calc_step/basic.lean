a940a031adb6e66df922a365559de9a51ef32656
Johan Commelin <johan@commelin.net>
2020-10-28 12:24:33 +0000

    wip

--- src/tactic/calc_step/basic.lean
+++ src/tactic/calc_step/basic.lean
@@ -10,19 +10,33 @@ namespace tactic
 
 open calc_step calc_step.side calc_step.op calc_step.sign
 
-meta def calc_step (e : pexpr) (s : option side) (op : op) (sgn : option sign) : tactic unit :=
+/-- The default side that the `calc_step` tactic acts on is on the left,
+but for `div` (division) the default is to divide on the right. -/
+meta def get_side : option side → op → sign → side
+| (some s) _   _ := s
+| none     div _ := R
+| _        _   _ := L
+
+meta def calc_step_aux (e : pexpr) (n : name) : tactic unit :=
+_
+
+meta def calc_step (e : pexpr) (s : option side) (op : op) (sgn : sign) : tactic unit :=
 focus1 $
 do ctx ← local_context,
-  n ← get_unused_name `nonzero,
-  to_expr ``(%%e ≠ 0) >>= assert n,
-  focus1 `[try { assumption <|> norm_num, done }],
-  swap,
-  h0 ← get_local n,
-  match s with
-  | (some side.R) := `[apply (mul_left_inj' %%h0).mp]
-  | _             := `[apply (mul_right_inj' %%h0).mp]
-  end,
-  clear h0
+  let sd := get_side s op sgn,
+  let lems := lookup.find (sd, op, sgn),
+  lems.mfirst (calc_step_aux e)
+  --
+  -- n ← get_unused_name `nonzero,
+  -- to_expr ``(%%e ≠ 0) >>= assert n,
+  -- focus1 `[try { assumption <|> norm_num, done }],
+  -- swap,
+  -- h0 ← get_local n,
+  -- match s with
+  -- | (some side.R) := `[apply (mul_left_inj' %%h0).mp]
+  -- | _             := `[apply (mul_right_inj' %%h0).mp]
+  -- end,
+  -- clear h0
 
 meta def add_by (e : pexpr) : option side → tactic unit
 | (some side.R) := do `[apply (add_right_inj %%e).mp]
@@ -55,13 +69,17 @@ tactic.add_by q s
 meta def mul_by (q : parse parser.pexpr) (s : parse side_p?) : tactic unit :=
 tactic.mul_by q s
 
+meta def dummy (q : parse parser.pexpr) (s : parse side_p?) : tactic unit :=
+tactic.calc_step q s mul none
+
 end interactive
 
 end tactic
 
 example (a b : ℕ) (ha : a ≠ 0) (h : 2 * a = 2 * b) : a = b :=
 begin
-  mul_by a L,
+  dummy a L,
+  -- mul_by a L,
 end
 
 example (a b : ℂ) (x : ℂ) (h : x * a = x * b) : a = b :=

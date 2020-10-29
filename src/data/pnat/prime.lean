ff560806cf1ebcc3501353b0a17a8ce1acf103d9
Mario Carneiro <di.gama@gmail.com>
2020-10-29 14:23:07 -0400

    docs

--- src/data/pnat/prime.lean
+++ src/data/pnat/prime.lean
@@ -1,3 +1,15 @@
+/-
+Copyright (c) 2017 Microsoft Corporation. All rights reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
+Author: Mario Carneiro, Neil Strickland
+-/
+
+/-!
+# Primality and GCD on pnat
+
+This file extends the theory of `ℕ+` with `gcd`, `lcm` and `prime` functions, analogous to those on
+`nat`.
+-/
 import data.nat.prime
 import data.pnat.basic
 

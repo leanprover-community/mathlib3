d4893456588f5331ce74ef84518fe3f7b271d235
Thomas Browning <tb65536@uw.edu>
2020-10-29 11:50:31 -0700

    remove redundant equalizer

--- src/algebra/algebra/subalgebra.lean
+++ src/algebra/algebra/subalgebra.lean
@@ -258,25 +258,6 @@ instance integral_domain {R A : Type*} [comm_ring R] [integral_domain A] [algebr
   (S : subalgebra R A) : integral_domain S :=
 @subring.domain A _ S _
 
-/-- The equalizer of two R-algebra homomorphisms -/
-def equalizer (ϕ ψ : A →ₐ[R] B) : subalgebra R A :=
-{ carrier := {a | ϕ a = ψ a},
-  zero_mem' := by { change ϕ 0 = ψ 0, rw [alg_hom.map_zero, alg_hom.map_zero] },
-  add_mem' := λ x y hx hy, by
-  { change ϕ x = ψ x at hx,
-    change ϕ y = ψ y at hy,
-    change ϕ (x + y) = ψ (x + y),
-    rw [alg_hom.map_add, alg_hom.map_add, hx, hy] },
-  one_mem' := by { change ϕ 1 = ψ 1, rw [alg_hom.map_one, alg_hom.map_one] },
-  mul_mem' := λ x y hx hy, by
-  { change ϕ x = ψ x at hx,
-    change ϕ y = ψ y at hy,
-    change ϕ (x * y) = ψ (x * y),
-    rw [alg_hom.map_mul, alg_hom.map_mul, hx, hy] },
-  algebra_map_mem' := λ x, by
-  { change ϕ (algebra_map R A x) = ψ (algebra_map R A x),
-    rw [alg_hom.commutes, alg_hom.commutes] } }
-
 end subalgebra
 
 namespace alg_hom

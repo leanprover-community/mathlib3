a8fa6ab6e11fc17dbb4aa69ef3c294fa8107550d
Yakov Pechersky <ffxen158@gmail.com>
2020-10-29 16:50:24 -0400

    Rename to map_comp_map, provide map_map

--- src/data/prod.lean
+++ src/data/prod.lean
@@ -39,9 +39,18 @@ funext $ map_snd f g
 Composing a `prod.map` with another `prod.map` is equal to
 a single `prod.map` of composed functions.
 -/
-lemma comp_map {ε ζ : Type*}
+lemma map_comp_map {ε ζ : Type*}
   (f : α → β) (f' : γ → δ) (g : β → ε) (g' : δ → ζ) :
   prod.map g g' ∘ prod.map f f' = prod.map (g ∘ f) (g' ∘ f') :=
+rfl
+
+/--
+Composing a `prod.map` with another `prod.map` is equal to
+a single `prod.map` of composed functions, fully applied.
+-/
+lemma map_map {ε ζ : Type*}
+  (f : α → β) (f' : γ → δ) (g : β → ε) (g' : δ → ζ) (x : α × γ) :
+  prod.map g g' (prod.map f f' x) = prod.map (g ∘ f) (g' ∘ f') x :=
 rfl
 
 @[simp] theorem mk.inj_iff {a₁ a₂ : α} {b₁ b₂ : β} : (a₁, b₁) = (a₂, b₂) ↔ (a₁ = a₂ ∧ b₁ = b₂) :=

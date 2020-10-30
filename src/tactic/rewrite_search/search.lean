ebe5db03d06ab1399e8b822ea2fbe0e2a809fec3
Kevin Lacker <lacker@gmail.com>
2020-10-30 09:07:36 -0700

    comments on search.lean

--- src/tactic/rewrite_search/search.lean
+++ src/tactic/rewrite_search/search.lean
@@ -9,13 +9,24 @@ import tactic.rewrite_search.discovery
 import tactic.rewrite_search.types
 
 /-!
-# The core algorithm underlying rewrite search.
+# The graph algorithm part of rewrite search
+
+`search.lean` contains the logic to do a graph search. The search algorithm
+starts with an equation to prove, treats the left hand side and right hand side as
+two vertices in a graph, and uses rewrite rules to find a path that connects the two sides.
 -/
 
 open tactic
 
 namespace tactic.rewrite_search
 
+/-
+An edge represents a proof that can get from one expression to another.
+It represents the fact that, starting from the vertex `fr`, the expression in `proof`
+can prove the vertex `to`.
+`how` contains information that the explainer will use to generate Lean code for the
+proof.
+-/
 meta structure edge :=
 (fr to : ℕ)
 (proof : tactic expr)
@@ -26,6 +37,16 @@ meta def edge.to_string : edge → format
 
 meta instance edge.has_to_format : has_to_format edge := ⟨edge.to_string⟩
 
+/-
+A vertex represents an expression that is equivalent to either the left or right side
+of our initial equation.
+`id` is a numerical id used to refer to this vertex in the context of a single graph.
+`exp` is the expression this vertex represents.
+`pp` is the string format of the expression; we store this in the vertex to avoid
+recalculating it.
+`side` is whether this vertex descends from the left or right side of the equation.
+`parent` is the edge that originally added this vertex to the graph.
+-/
 meta structure vertex :=
 (id     : ℕ)
 (exp    : expr)
@@ -33,6 +54,16 @@ meta structure vertex :=
 (side   : side)
 (parent : option edge)
 
+/-
+The graph represents two trees, one descending from each of the left and right sides
+of our initial equation.
+`conf` and `rules` determine what rewrites are used to generate new graph vertices.
+`vertices` maps vertex.id to vertex.
+`vmap` maps vertex.pp to vertex.id, so we can quickly find collisions.
+`solving_edge` represents a solution that will prove our target equation.
+It is an edge that would connect the two trees, so `solving_edge.fr` and `solving_edge.to`
+are vertices in different trees.
+-/
 meta structure graph :=
 (conf         : config)
 (rules        : list (expr × bool))
@@ -40,6 +71,9 @@ meta structure graph :=
 (vmap         : native.rb_map string ℕ)
 (solving_edge : option edge)
 
+/-
+Construct a graph to search for a proof of a given equation.
+-/
 meta def mk_graph (conf : config) (rules : list (expr × bool)) (eq : expr) : tactic graph :=
 do (lhs, rhs) ← split_equation eq,
   lhs_pp ← to_string <$> tactic.pp lhs,
@@ -53,11 +87,18 @@ variables (g : graph)
 
 namespace graph
 
+/-
+A convenience wrapper around `g.vertices` that fails if the index is out of bounds.
+-/
 meta def get_vertex (i : ℕ) : tactic vertex :=
 if h : i < g.vertices.size then return $ g.vertices.read (fin.mk i h)
 else fail "invalid vertex access"
 
--- note: the returned edges are backwards 
+/-
+Find a list of edges that connect the given edge to the root of its tree.
+The edges are returned in leaf-to-root order, while they are in root-to-leaf direction,
+so if you want them in the logical order you must reverse the returned list.
+-/
 private meta def walk_up_parents : option edge → tactic (list edge)
 | none     := return []
 | (some e) := do
@@ -65,8 +106,10 @@ private meta def walk_up_parents : option edge → tactic (list edge)
   edges ← walk_up_parents v.parent,
   return (e :: edges)
 
--- Returns two lists. The first is a path from LHS to some interior vertex,
--- the second is a path from the RHS to that interior vertex.
+/-
+Returns two lists that represent a solution. The first list is a path from LHS to some
+interior vertex, the second is a path from the RHS to that interior vertex.
+-/
 private meta def solution_paths : tactic (list edge × list edge) :=
 do e ← g.solving_edge,
   v ← g.get_vertex e.to,
@@ -77,6 +120,12 @@ do e ← g.solving_edge,
     | side.R := return (path1.reverse, path2.reverse)
   end
 
+/-
+Add the new vertex and edge to the graph, that can be proved in one step starting
+at a given vertex, with a given rewrite expression.
+For efficiency, it's important that this is the only way the graph is mutated,
+and it only appends to the end of the `vertices` buffer.
+-/
 private meta def add_rewrite (v : vertex) (rw : rewrite) : tactic graph :=
 do pp ← to_string <$> tactic.pp rw.exp,
 let maybe_id := g.vmap.find pp,
@@ -94,10 +143,16 @@ match maybe_id with
                     vmap := g.vmap.insert pp new_vertex_id }
 end
 
+/-
+Add all single-step rewrites starting at a particular vertex to the graph.
+-/
 private meta def expand_vertex (v : vertex) : tactic graph :=
 do rws ← get_rewrites g.rules v.exp g.conf,
 list.mfoldl (λ g rw, add_rewrite g v rw) g rws.to_list
 
+/-
+Repeatedly expand edges, starting at a given vertex id, until a solution is found.
+-/
 private meta def find_solving_edge : graph → ℕ → tactic graph
 | g vertex_id :=
 if vertex_id ≥ g.conf.max_iterations then fail "search failed: max iterations reached"
@@ -110,13 +165,19 @@ else if h : vertex_id < g.vertices.size then
   end
 else fail "search failed: all vertices explored"
 
--- Fails if the list is empty
+/-
+Use `mk_eq_trans` to combine a list of proof expressions into a single proof expression.
+-/
 private meta def combine_proofs (proofs : list expr) : tactic expr :=
 match proofs with
   | []              := fail "cannot combine empty proof list"
   | (proof :: rest) := list.mfoldl mk_eq_trans proof rest
 end
 
+/-
+Construct a proof unit, given a path through the graph.
+This reverses the direction of the proof on the right hand side, with `mk_eq_symm`.
+-/
 private meta def proof_for_edges : (side × list edge) → tactic (option proof_unit)
 | (s, []) := return none
 | (s, edges) := do
@@ -128,6 +189,10 @@ private meta def proof_for_edges : (side × list edge) → tactic (option proof_
   let hows := edges.map (λ e, e.how),
   return $ some ⟨proof, s, hows⟩
 
+/-
+Run the search to find a proof for the provided graph.
+Normally, this is the only external method needed to run the graph search.
+-/
 meta def find_proof : tactic (graph × expr × list proof_unit) :=
 do g ← find_solving_edge g 0,
   (left_edges, right_edges) ← solution_paths g,

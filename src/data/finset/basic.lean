0768037f4602ad1158d0b33960db922afa635052
Thomas Browning <tb65536@uw.edu>
2020-10-30 00:19:41 -0700

    to_finset_card_of_nodup

--- src/data/finset/basic.lean
+++ src/data/finset/basic.lean
@@ -2263,6 +2263,10 @@ congr_arg card $ (@multiset.erase_dup_eq_self α _ l).2 h
 end list
 
 namespace multiset
+variable [decidable_eq α]
+
+theorem to_finset_card_of_nodup {l : multiset α} (h : l.nodup) : l.to_finset.card = l.card :=
+congr_arg card $ (@multiset.erase_dup_eq_self α _ l).2 h
 
 lemma disjoint_to_finset [decidable_eq α] (m1 m2 : multiset α) :
   _root_.disjoint m1.to_finset m2.to_finset ↔ m1.disjoint m2 :=

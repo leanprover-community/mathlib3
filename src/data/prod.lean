a389ab5cc1682851a87be0d39b1dfa392f0973f0
Yakov Pechersky <pechersky@users.noreply.github.com>
2020-10-29 22:53:31 -0400

    Update src/data/prod.lean
    Co-authored-by: Bryan Gin-ge Chen <bryangingechen@gmail.com>

--- src/data/prod.lean
+++ src/data/prod.lean
@@ -35,7 +35,7 @@ funext $ map_fst f g
 lemma map_snd' (f : α → γ) (g : β → δ) : (prod.snd ∘ map f g) = g ∘ prod.snd :=
 funext $ map_snd f g
 
-/-
+/--
 Composing a `prod.map` with another `prod.map` is equal to
 a single `prod.map` of composed functions.
 -/

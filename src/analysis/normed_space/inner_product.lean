9a6778701667848a5fd5436efc32dfc70c0f1bc2
Frédéric Dupuis <31101893+dupuisf@users.noreply.github.com>
2020-10-29 21:45:08 -0400

    Update src/analysis/normed_space/inner_product.lean
    Co-authored-by: hrmacbeth <25316162+hrmacbeth@users.noreply.github.com>

--- src/analysis/normed_space/inner_product.lean
+++ src/analysis/normed_space/inner_product.lean
@@ -251,7 +251,7 @@ begin
       have : ⟪y, y⟫ / (⟪y, y⟫ * ⟪y, y⟫) = 1 / ⟪y, y⟫ :=
         by rw [div_mul_eq_div_mul_one_div, div_self hy', one_mul],
       rw [this, div_eq_mul_inv, one_mul, ←div_eq_mul_inv] },
-    have h₄ : ⟪y, y⟫ = (re ⟪y, y⟫) := by simp only [inner_self_re_to_K],
+    have h₄ : ⟪y, y⟫ = re ⟪y, y⟫ := by simp only [inner_self_re_to_K],
     have h₅ : re ⟪y, y⟫ > 0,
     { refine lt_of_le_of_ne inner_self_nonneg _,
       intro H,

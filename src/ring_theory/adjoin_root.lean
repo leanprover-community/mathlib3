7e40869c41b2daec19f7c53586bf70c348a94dca
tb65536 <tb65536@users.noreply.github.com>
2020-10-30 23:17:47 -0700

    Update src/ring_theory/adjoin_root.lean
    Co-authored-by: Johan Commelin <johan@commelin.net>

--- src/ring_theory/adjoin_root.lean
+++ src/ring_theory/adjoin_root.lean
@@ -136,7 +136,7 @@ def alg_hom (x : S) (hfx : aeval x f = 0) : adjoin_root f →ₐ[R] S :=
 @[simp] lemma coe_alg_hom (x : S) (hfx : aeval x f = 0) :
   (alg_hom f x hfx : adjoin_root f →+* S) = lift (algebra_map R S) x hfx := rfl
 
-lemma aeval_alg_hom_eq_zero (ϕ : adjoin_root f →ₐ[R] S) : aeval (ϕ (root f)) f = 0 :=
+@[simp] lemma aeval_alg_hom_eq_zero (ϕ : adjoin_root f →ₐ[R] S) : aeval (ϕ (root f)) f = 0 :=
 begin
   have h : ϕ.to_ring_hom.comp (of f) = algebra_map R S := ring_hom.ext_iff.mpr (ϕ.commutes),
   rw [aeval_def, ←h, ←ring_hom.map_zero ϕ.to_ring_hom, ←eval₂_root f, hom_eval₂],

515e55d27fd7ff338dff0d934f604c18031ff484
Mario Carneiro <di.gama@gmail.com>
2020-11-01 12:44:26 -0500

    modifications from review

--- src/tactic/simpa.lean
+++ src/tactic/simpa.lean
@@ -27,9 +27,10 @@ This is a "finishing" tactic modification of `simp`. It has two forms.
 meta def simpa (use_iota_eqn : parse $ (tk "!")?) (no_dflt : parse only_flag)
   (hs : parse simp_arg_list) (attr_names : parse with_ident_list)
   (tgt : parse (tk "using" *> texpr)?) (cfg : simp_config_ext := {}) : tactic unit :=
-let simp_at lc close_tac :=
-  simp use_iota_eqn no_dflt hs attr_names (loc.ns lc) {fail_if_unchanged := ff, ..cfg} >>
-  (close_tac <|> trivial) in
+let simp_at lc (close_tac : tactic unit) := focus1 $ do
+  simp use_iota_eqn no_dflt hs attr_names (loc.ns lc) {fail_if_unchanged := ff, ..cfg},
+  focus1 (close_tac <|> trivial <|> fail "simpa failed"),
+  all_goals (try apply_instance) in
 match tgt with
 | none := get_local `this >> simp_at [some `this, none] assumption <|> simp_at [none] assumption
 | some e := do

2d6db28c2f6dff569e5c14af2ea5bc9ad6086e5b
Bryan Gin-ge Chen <bryangingechen@gmail.com>
2020-11-01 12:46:00 -0500

    reviewer comments

--- src/tactic/mk_iff_of_inductive_prop.lean
+++ src/tactic/mk_iff_of_inductive_prop.lean
@@ -4,25 +4,29 @@ Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Johannes Hölzl
 -/
 import tactic.core
+import tactic.lint
 /-!
 # mk_iff_of_inductive_prop
 
-This file defines a tactic that generates `iff` rules for inductive `Prop`s, like for `list.chain`:
+This file defines a tactic `tactic.mk_iff_of_inductive_prop` that generates `iff` rules for
+inductive `Prop`s. For example, when applied to `list.chain`, it creates a declaration with
+the following type:
 
 ```lean
 ∀{α : Type*} (R : α → α → Prop) (a : α) (l : list α),
   chain R a l ↔ l = [] ∨ ∃{b : α} {l' : list α}, R a b ∧ chain R b l ∧ l = b :: l'
 ```
 
+This tactic can be called using either the `mk_iff_of_inductive_prop` user command or
+the `mk_iff` attribute.
 -/
-namespace tactic
 
 open tactic expr
+namespace mk_iff
 
-/-- Given two expressions `e₀` and `e₁`, return the expression `` `(%%e₀ ↔ %%e₁)``. -/
-meta def mk_iff (e₀ : expr) (e₁ : expr) : expr := `(%%e₀ ↔ %%e₁)
-
-private meta def select : ℕ → ℕ → tactic unit
+/-- `select m n` runs `tactic.right` `m` times, and then `tactic.left` `(n-m)` times.
+Fails if `n < m`. -/
+meta def select : ℕ → ℕ → tactic unit
 | 0 0             := skip
 | 0 (n + 1)       := left >> skip
 | (m + 1) (n + 1) := right >> select m n
@@ -35,24 +39,24 @@ R as := ∃ bs, Λ_i a_i = p_i[bs]
 This relation is user-visible, so we compact it by removing each `b_j` where a `p_i = b_j`, and
 hence `a_i = b_j`. We need to take care when there are `p_i` and `p_j` with `p_i = p_j = b_k`.
 
-TODO: this is copied from Lean's `coinductive_predicates.lean`, export it there.
+TODO: this is a variant of `compact_relation` in `coinductive_predicates.lean`, export it there.
 -/
-private meta def compact_relation :
+meta def compact_relation :
   list expr → list (expr × expr) → list (option expr) × list (expr × expr)
 | []        ps := ([], ps)
 | (b :: bs) ps :=
   match ps.span (λap:expr × expr, ¬ ap.2 =ₐ b) with
-    | (_, [])           := let (bs, ps) := compact_relation bs ps in (b::bs, ps)
-    | (ps₁, list.cons (a, _) ps₂) :=
-      let
-        i := a.instantiate_local b.local_uniq_name,
+    | (_, [])              := let (bs, ps) := compact_relation bs ps in (b::bs, ps)
+    | (ps₁, (a, _) :: ps₂) :=
+      let i := a.instantiate_local b.local_uniq_name,
         (bs, ps) := compact_relation (bs.map i) ((ps₁ ++ ps₂).map (λ⟨a, p⟩, (a, i p)))
       in (none :: bs, ps)
   end
 
+@[nolint doc_blame] -- TODO: document
 meta def constr_to_prop (univs : list level) (g : list expr) (idxs : list expr) (c : name) :
-  tactic ((list (option expr) × (expr ⊕ ℕ)) × expr) := do
-  e ← get_env,
+  tactic ((list (option expr) × (expr ⊕ ℕ)) × expr) :=
+do e ← get_env,
   decl ← get_decl c,
   some type' ← return $ decl.instantiate_type_univ_params univs,
   type ← drop_pis g type',
@@ -83,8 +87,9 @@ meta def constr_to_prop (univs : list level) (g : list expr) (idxs : list expr) 
   end,
   return ((bs, n), r)
 
-private meta def to_cases (s : list $ list (option expr) × (expr ⊕ ℕ)) : tactic unit := do
-  h ← intro1,
+@[nolint doc_blame] -- TODO: document
+meta def to_cases (s : list $ list (option expr) × (expr ⊕ ℕ)) : tactic unit :=
+do h ← intro1,
   i ← induction h,
   focus ((s.zip i).enum.map $ λ⟨p, (shape, t), _, vars, _⟩, do
     let si := (shape.zip vars).filter_map (λ⟨c, v⟩, c >>= λ _, some v),
@@ -101,13 +106,24 @@ private meta def to_cases (s : list $ list (option expr) × (expr ⊕ ℕ)) : ta
     done),
   done
 
-private def list_option_merge {α : Type*} {β : Type*} : list (option α) → list β → list (option β)
+/--
+Iterate over two lists, if the first element of the first list is `none`, insert `none` into the
+result and continue with the tail of first list. Otherwise, wrap the first element of the second
+list with `some` and continue with the tails of both lists. Return when either list is empty.
+
+Example:
+```
+list_option_merge [none, some (), none, some ()] [0, 1, 2, 3, 4] = [none, (some 0), none, (some 1)]
+```
+-/
+def list_option_merge {α : Type*} {β : Type*} : list (option α) → list β → list (option β)
 | [] _ := []
 | (none :: xs) ys := none :: list_option_merge xs ys
-| (some a :: xs) (y :: ys) := some y :: list_option_merge xs ys
-| (some a :: xs) [] := []
+| (some _ :: xs) (y :: ys) := some y :: list_option_merge xs ys
+| (some _ :: xs) [] := []
 
-private meta def to_inductive
+@[nolint doc_blame] -- TODO: document
+meta def to_inductive
   (cs : list name) (gs : list expr) (s : list (list (option expr) × (expr ⊕ ℕ))) (h : expr) :
   tactic unit :=
 match s.length with
@@ -142,6 +158,11 @@ match s.length with
   done
 end
 
+end mk_iff
+
+namespace tactic
+open mk_iff
+
 /--
 `mk_iff_of_inductive_prop i r` makes an `iff` rule for the inductively-defined proposition `i`.
 The new rule `r` has the shape `∀ps is, i as ↔ ⋁_j, ∃cs, is = cs`, where `ps` are the type
@@ -152,20 +173,23 @@ each constructor for each of the indices to the inductive type `i`.
 In each case, we remove constructor parameters (i.e. `cs`) when the corresponding equality would
 be just `c = i` for some index `i`.
 
-For example: `mk_iff_of_inductive_prop` on `list.chain` produces:
+For example, `mk_iff_of_inductive_prop` on `list.chain` produces:
 
 ```lean
 ∀ {α : Type*} (R : α → α → Prop) (a : α) (l : list α),
   chain R a l ↔ l = [] ∨ ∃{b : α} {l' : list α}, R a b ∧ chain R b l ∧ l = b :: l'
 ```
 -/
-meta def mk_iff_of_inductive_prop (i : name) (r : name) : tactic unit := do
-  e ← get_env,
+meta def mk_iff_of_inductive_prop (i : name) (r : name) : tactic unit :=
+do e ← get_env,
   guard (e.is_inductive i),
   let constrs := e.constructors_of i,
   let params := e.inductive_num_params i,
   let indices := e.inductive_num_indices i,
-  let rec := match e.recursor_of i with some rec := rec | none := i.append `rec end,
+  let rec := match e.recursor_of i with
+    | some rec := rec
+    | none := i.append `rec
+    end,
   decl ← get_decl i,
   let type := decl.type,
 
@@ -200,7 +224,7 @@ each constructor for each of the indices to the inductive type `i`.
 In each case, we remove constructor parameters (i.e. `cs`) when the corresponding equality would
 be just `c = i` for some index `i`.
 
-For example: `mk_iff_of_inductive_prop` on `list.chain` produces:
+For example, `mk_iff_of_inductive_prop` on `list.chain` produces:
 
 ```lean
 ∀ {α : Type*} (R : α → α → Prop) (a : α) (l : list α),
@@ -242,20 +266,32 @@ foo_iff : ∀ (m n : ℕ), foo m n ↔ m = n ∧ m + n = 2
 ```
 
 You can add an optional string after `mk_iff` to change the name of the generated lemma.
+For example, if we try the following:
+```lean
+@[mk_iff bar] structure foo (m n : ℕ) : Prop :=
+(equal : m = n)
+(sum_eq_two : m + n = 2)
+```
+
+Then `#check bar` returns:
+```lean
+bar : ∀ (m n : ℕ), foo m n ↔ m = n ∧ m + n = 2
+```
 
 See also the user command `mk_iff_of_inductive_prop`.
 -/
-@[user_attribute] meta def mk_iff_of_inductive_prop_attr : user_attribute unit (option name) :=
+@[user_attribute] meta def mk_iff_attr : user_attribute unit (option name) :=
 { name := `mk_iff,
   descr := "Generate an `iff` lemma for an inductive `Prop`.",
   parser := ident?,
   after_set := some $ λ n _ _, do
-    tgt ← mk_iff_of_inductive_prop_attr.get_param n,
+    tgt ← mk_iff_attr.get_param n,
     tactic.mk_iff_of_inductive_prop n (tgt.get_or_else (n.append_suffix "_iff")) }
 
 add_tactic_doc
 { name        := "mk_iff",
   category    := doc_category.attr,
-  decl_names  := [`mk_iff_of_inductive_prop_attr],
+  decl_names  := [`mk_iff_attr],
   tags        := ["logic", "environment"] }
+
 end

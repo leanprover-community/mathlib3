9da6f9febb605be1996d7cf05c04e39b0f9570ab
faenuccio <65080144+faenuccio@users.noreply.github.com>
2020-11-02 09:56:33 +0000

    Update operations.lean

--- src/algebra/algebra/operations.lean
+++ src/algebra/algebra/operations.lean
@@ -4,7 +4,6 @@ Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Kenny Lau
 -/
 import algebra.algebra.basic
-import linear_algebra.finsupp
 
 /-!
 # Multiplication and division of submodules of an algebra.

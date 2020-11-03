51cf92356e525519abec40e3bb9245a365662cfc
agusakov <39916842+agusakov@users.noreply.github.com>
2020-11-03 13:52:10 -0500

    grammar correction
    Co-authored-by: Bryan Gin-ge Chen <bryangingechen@gmail.com>

--- src/combinatorics/colex.lean
+++ src/combinatorics/colex.lean
@@ -16,7 +16,7 @@ lemmas and properties relating to it.
 The colex ordering likes to avoid large values - it can be thought of on
 `finset ℕ` as the "binary" ordering. That is, order A based on
 `∑_{i ∈ A} 2^i`.
-It's defined here a slightly more general way, requiring only `has_lt α` in
+It's defined here in a slightly more general way, requiring only `has_lt α` in
 the definition of colex on `finset α`. In the context of the Kruskal-Katona
 theorem, we are interested in particular on how colex behaves for sets of a
 fixed size. If the size is 3, colex on ℕ starts

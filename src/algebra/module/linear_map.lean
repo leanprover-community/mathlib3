3c619b91a997fb8e754f1e5bbd0cc64883c1f546
Eric Wieser <wieser.eric@gmail.com>
2020-11-03 19:36:03 +0000

    Update linear_map.lean

--- src/algebra/module/linear_map.lean
+++ src/algebra/module/linear_map.lean
@@ -57,6 +57,9 @@ end
 /-- The `add_hom` underlying a `linear_map`. -/
 add_decl_doc linear_map.to_add_hom
 
+/-- The `mul_action_hom` underlying a `linear_map`. -/
+add_decl_doc linear_map.to_mul_action_hom
+
 infixr ` →ₗ `:25 := linear_map _
 notation M ` →ₗ[`:25 R:25 `] `:0 M₂:0 := linear_map R M M₂
 

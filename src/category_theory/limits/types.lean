b37d4a3dc94fdd81dd211febbc8526cf7f61586b
Bhavik Mehta <bhavikmehta8@gmail.com>
2020-11-03 17:56:54 +0000

    feat(category_theory/limits/types): ext iff lemma (#4883)
    A little lemma which sometimes makes it easier to work with limits in type.

--- src/category_theory/limits/types.lean
+++ src/category_theory/limits/types.lean
@@ -104,6 +104,10 @@ begin
   simp [w j],
 end
 
+lemma limit_ext_iff (F : J ⥤ Type u) (x y : limit F) :
+  x = y ↔ (∀ j, limit.π F j x = limit.π F j y) :=
+⟨λ t _, t ▸ rfl, limit_ext _ _ _⟩
+
 -- TODO: are there other limits lemmas that should have `_apply` versions?
 -- Can we generate these like with `@[reassoc]`?
 -- PROJECT: prove these for any concrete category where the forgetful functor preserves limits?

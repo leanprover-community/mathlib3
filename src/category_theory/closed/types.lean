51f007124f1fd365a6d1f74de3e0a31affd60c97
Bhavik Mehta <bhavikmehta8@gmail.com>
2020-11-04 00:02:34 +0000

    feat(category_theory/closed/types): presheaf category is cartesian closed

--- src/category_theory/closed/types.lean
+++ src/category_theory/closed/types.lean
@@ -4,6 +4,8 @@ Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Bhavik Mehta
 -/
 import category_theory.limits.presheaf
+import category_theory.limits.functor_category
+import category_theory.limits.shapes.types
 import category_theory.closed.cartesian
 
 namespace category_theory
@@ -15,8 +17,6 @@ universes v₁ v₂ u₁ u₂
 
 variables {C : Type v₂} [category.{v₁} C]
 variables {D : Type u₂} [category.{v₂} D]
-
-open colimit_adj
 
 section cartesian_closed
 
@@ -39,6 +39,53 @@ def prod_preserves_colimits [has_finite_products D] [has_colimits D]
         { intros G G',
           apply prod_comparison_natural ((evaluation C D).obj k) (𝟙 F) },
       end } } }
+
+@[simps]
+def explicit_prod (X : Type v₁) : Type v₁ ⥤ Type v₁ :=
+{ obj := λ Y, (types.binary_product_limit_cone X Y).cone.X,
+  map := λ Y₁ Y₂ f,
+  begin
+    apply (types.binary_product_limit_cone X Y₂).is_limit.lift (binary_fan.mk _ _),
+    apply _root_.prod.fst,
+    exact ↾_root_.prod.snd ≫ f,
+  end }
+
+instance (X : Type v₁) : is_left_adjoint (explicit_prod X) :=
+{ right :=
+  { obj := λ Y, X ⟶ Y,
+    map := λ Y₁ Y₂ f g, g ≫ f },
+  adj := adjunction.mk_of_unit_counit
+  { unit := { app := λ Z (z : Z) x, ⟨x, z⟩ },
+    counit :=
+    { app := λ Z xf, xf.2 xf.1 } } }
+
+def same_prod (X : Type v₁) : explicit_prod X ≅ prod.functor.obj X :=
+begin
+  apply nat_iso.of_components _ _,
+  { intro Y,
+    exact ((limit.is_limit _).cone_point_unique_up_to_iso (types.binary_product_limit_cone X Y).is_limit).symm },
+  { tidy }
+end
+
+-- Why isn't this automatically inferred? I can't seem to make
+-- `has_finite_products_of_has_products` an instance, not sure why.
+instance : has_finite_products (Type v₁) := has_finite_products_of_has_products _
+
+instance : cartesian_closed (Type v₁) :=
+{ closed := λ X, { is_adj := adjunction.left_adjoint_of_nat_iso (same_prod X) } }
+
+-- As above
+instance {C : Type v₁} [small_category C] : has_finite_products (Cᵒᵖ ⥤ Type v₁) :=
+has_finite_products_of_has_products _
+
+instance {C : Type v₁} [small_category C] : cartesian_closed (Cᵒᵖ ⥤ Type v₁) :=
+{ closed := λ F,
+  { is_adj :=
+    begin
+      apply is_left_adjoint_of_preserves_colimits _,
+      apply_instance,
+      apply prod_preserves_colimits,
+    end } }
 
 end cartesian_closed
 

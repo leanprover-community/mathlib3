49d9cd8d3592d76304f00245ee9559d542753acd
Eric Wieser <wieser.eric@gmail.com>
2020-11-04 13:17:34 +0000

    Missed changes from the other branch

--- src/data/complex/basic.lean
+++ src/data/complex/basic.lean
@@ -186,7 +186,7 @@ eq_conj_iff_real.trans ⟨by rintro ⟨r, rfl⟩; simp, λ h, ⟨_, h.symm⟩⟩
 
 instance : star_ring ℂ :=
 { star := λ z, conj z,
-  star_involutive := by simp,
+  star_involutive := λ x, by simp,
   star_mul := λ r s, by { ext; simp [mul_comm], },
   star_zero := by simp,
   star_add := by simp, }

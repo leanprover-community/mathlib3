6a025ee98e39d496f7a90bad1d3dbf7053abaf43
Kevin Buzzard <k.buzzard@imperial.ac.uk>
2020-11-07 19:30:36 +0000

    better names for aux lemmas

--- archive/imo/imo1964_q1.lean
+++ archive/imo/imo1964_q1.lean
@@ -14,7 +14,7 @@ import data.nat.modeq
 
 (b) Prove that there is no positive integer $n$ for which $2^n+1$ is divisible by $7$.
 
-We define part (a) as a predicate, and prove that it is the set of positive
+We define a predicate for the solutions in (a), and prove that it is the set of positive
 integers which are a multiple of 3.
 -/
 
@@ -24,24 +24,24 @@ integers which are a multiple of 3.
 
 open nat.modeq
 
-lemma two_pow_mul_three_mod_seven (m : ℕ) : 2 ^ (3 * m) ≡ 1 [MOD 7] :=
+lemma two_pow_three_mul_mod_seven (m : ℕ) : 2 ^ (3 * m) ≡ 1 [MOD 7] :=
 begin
   rw pow_mul,
   have h : 8 ≡ 1 [MOD 7] := modeq_of_dvd (by {use -1, norm_num } ),
-  convert modeq_pow h,
+  convert modeq_pow _ h,
   simp,
 end
 
-lemma two_pow_mul_three_add_one_mod_seven (m : ℕ) : 2 ^ (3 * m + 1) ≡ 2 [MOD 7] :=
+lemma two_pow_three_mul_add_one_mod_seven (m : ℕ) : 2 ^ (3 * m + 1) ≡ 2 [MOD 7] :=
 begin
   rw pow_add,
-  exact modeq_mul (two_pow_mul_three_mod_seven m) (show 2 ^ 1 ≡ 2 [MOD 7], by refl),
+  exact modeq_mul (two_pow_three_mul_mod_seven m) (show 2 ^ 1 ≡ 2 [MOD 7], by refl),
 end
 
-lemma two_pow_mul_three_add_two_mod_seven (m : ℕ) : 2 ^ (3 * m + 2) ≡ 4 [MOD 7] :=
+lemma two_pow_three_mul_add_two_mod_seven (m : ℕ) : 2 ^ (3 * m + 2) ≡ 4 [MOD 7] :=
 begin
   rw pow_add,
-  exact modeq_mul (two_pow_mul_three_mod_seven m) (show 2 ^ 2 ≡ 4 [MOD 7], by refl),
+  exact modeq_mul (two_pow_three_mul_mod_seven m) (show 2 ^ 2 ≡ 4 [MOD 7], by refl),
 end
 
 /-!
@@ -68,15 +68,15 @@ begin
     interval_cases t with hr; rw hr at h,
     { exact nat.dvd_of_mod_eq_zero hr },
     { exfalso,
-      have nonsense := (two_pow_mul_three_add_one_mod_seven _).symm.trans h,
+      have nonsense := (two_pow_three_mul_add_one_mod_seven _).symm.trans h,
       rw modeq_iff_dvd at nonsense,
       norm_num at nonsense },
     { exfalso,
-      have nonsense := (two_pow_mul_three_add_two_mod_seven _).symm.trans h,
+      have nonsense := (two_pow_three_mul_add_two_mod_seven _).symm.trans h,
       rw modeq_iff_dvd at nonsense,
       norm_num at nonsense } },
   { rintro ⟨m, rfl⟩,
-    apply two_pow_mul_three_mod_seven }
+    apply two_pow_three_mul_mod_seven }
 end
 
 theorem imo1964_q1b (n : ℕ) : ¬ (7 ∣ 2 ^ n + 1) :=
@@ -87,17 +87,17 @@ begin
   interval_cases t with hr; rw hr,
   { rw zero_add,
     intro h,
-    have := h.symm.trans (modeq_add (nat.modeq.refl _) (two_pow_mul_three_mod_seven _)),
+    have := h.symm.trans (modeq_add (nat.modeq.refl _) (two_pow_three_mul_mod_seven _)),
     rw modeq_iff_dvd at this,
     norm_num at this },
   { rw add_comm _ (3 * _),
     intro h,
-    have := h.symm.trans (modeq_add (nat.modeq.refl _) (two_pow_mul_three_add_one_mod_seven _)),
+    have := h.symm.trans (modeq_add (nat.modeq.refl _) (two_pow_three_mul_add_one_mod_seven _)),
     rw modeq_iff_dvd at this,
     norm_num at this },
   { rw add_comm _ (3 * _),
     intro h,
-    have := h.symm.trans (modeq_add (nat.modeq.refl _) (two_pow_mul_three_add_two_mod_seven _)),
+    have := h.symm.trans (modeq_add (nat.modeq.refl _) (two_pow_three_mul_add_two_mod_seven _)),
     rw modeq_iff_dvd at this,
     norm_num at this },
 end

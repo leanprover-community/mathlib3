ea9f23168a7e0e9549ed0e078efb08e9873e558c
tb65536 <tb65536@users.noreply.github.com>
2020-11-12 09:38:01 -0800

    Update src/field_theory/adjoin.lean
    Co-authored-by: Anne Baanen <Vierkantor@users.noreply.github.com>

--- src/field_theory/adjoin.lean
+++ src/field_theory/adjoin.lean
@@ -437,7 +437,7 @@ fintype.of_equiv _ (alg_hom_adjoin_integral_equiv F h).symm
 lemma card_alg_hom_adjoin_integral (h : is_integral F α) (h_sep : (minimal_polynomial h).separable)
   (h_splits : (minimal_polynomial h).splits (algebra_map F K)) :
   @fintype.card (F⟮α⟯ →ₐ[F] K) (fintype_of_alg_hom_adjoin_integral F h) =
-  (minimal_polynomial h).nat_degree :=
+    (minimal_polynomial h).nat_degree :=
 begin
   let s := ((minimal_polynomial h).map (algebra_map F K)).roots.to_finset,
   have H := λ x, multiset.mem_to_finset,

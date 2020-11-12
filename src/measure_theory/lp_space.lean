a734fee1915f82f80fd19bafaf53d5f4bcc3e79d
RemyDegenne <Remydegenne@gmail.com>
2020-11-12 16:58:57 +0100

    feat(measure_theory/lp_space): show that Lp is add_comm_group

--- src/measure_theory/lp_space.lean
+++ src/measure_theory/lp_space.lean
@@ -5,6 +5,7 @@ Author: Rémy Degenne.
 -/
 import measure_theory.l1_space
 import analysis.special_functions.pow
+import analysis.convex.specific_functions
 
 /-!
 # ℒp space
@@ -120,5 +121,143 @@ begin
   refl,
   rw [one_div, inv_pos], exact hp0_lt,
 end
+
+section borel_space
+variables [borel_space β] [topological_space.second_countable_topology β]
+
+section measurability_lemmas
+
+lemma real.measurable_rpow {p : ℝ} (hp0 : 0 < p) : measurable (λ a : ℝ, a^ p) :=
+begin
+  have h : continuous (λ a : ℝ, a ^ p),
+  { change continuous (λ a : ℝ, (id a) ^ ((λ a : ℝ, p) a)),
+    refine real.continuous_rpow _ continuous_id continuous_const,
+    intro a, right, change 0 < p, exact hp0, },
+  exact continuous.measurable h,
+end
+
+lemma nnreal.measurable_rpow {p : ℝ} (hp0 : 0 < p) : measurable (λ a : nnreal, a ^ p) :=
+begin
+  have h_rw : (λ (a : nnreal), a ^ p) = (λ (a : nnreal), nnreal.of_real(↑a ^ p)),
+  { ext1 a, rw ←nnreal.coe_rpow, rw nnreal.of_real_coe, },
+  rw h_rw,
+  refine measurable.nnreal_of_real _,
+  change measurable ((λ a : ℝ, a ^ p) ∘ (λ a : nnreal, ↑a)),
+  exact measurable.comp (real.measurable_rpow hp0) (nnreal.measurable_coe),
+end
+
+lemma ennreal.measurable_rpow_coe {p : ℝ} (hp0 : 0 < p) :
+  measurable (λ a : nnreal, (a : ennreal) ^ p) :=
+begin
+  simp_rw ennreal.coe_rpow_of_nonneg _ (le_of_lt hp0),
+  exact measurable.ennreal_coe (nnreal.measurable_rpow hp0),
+end
+
+lemma measurable_nnnorm_rpow {f : α → β} {p : ℝ} (hp0 : 0 < p) (hf : measurable f) :
+  measurable (λ (a : α), (nnnorm (f a)) ^ p) :=
+measurable.comp (nnreal.measurable_rpow hp0) (measurable.comp measurable_nnnorm hf)
+
+lemma measurable_nnnorm_rpow_coe {f : α → β} {p : ℝ} (hp0 : 0 < p) (hf : measurable f) :
+  measurable (λ (a : α), (nnnorm (f a) : ennreal) ^ p) :=
+measurable.comp (ennreal.measurable_rpow_coe hp0) (measurable.comp measurable_nnnorm hf)
+
+end measurability_lemmas
+
+lemma add_in_ℒp {f g : α → β} :
+  in_ℒp hp1 μ f → in_ℒp hp1 μ g → in_ℒp hp1 μ (f+g) :=
+begin
+  have hp0 : 0 ≤ p, from le_trans zero_le_one hp1,
+  have hp0_lt : 0 < p, from lt_of_lt_of_le zero_lt_one hp1,
+  have hp0_sub1 : 0 ≤ p - 1, by { simp only [sub_nonneg], exact hp1, },
+  intros hf hg,
+  split,
+  exact measurable.add hf.1 hg.1,
+  simp_rw pi.add_apply,
+  refine lt_of_le_of_lt _ _,
+  exact ∫⁻ a, (2^(p-1) * (nnnorm (f a) : ennreal) ^ p + 2^(p-1) * (nnnorm (g a) : ennreal) ^ p) ∂ μ,
+  have h_zero_lt_half : (0 : nnreal) < 1 / 2,
+  by { simp only [one_div, nnreal.inv_pos, zero_lt_bit0], exact zero_lt_one, },
+  { refine lintegral_mono _,
+    intro a, simp only,
+    -- go from ennreal to nnreal
+    simp_rw [←@ennreal.coe_two, ennreal.coe_rpow_of_nonneg _ hp0,
+      ennreal.coe_rpow_of_nonneg _ hp0_sub1, ←ennreal.coe_mul, ←ennreal.coe_add, ennreal.coe_le_coe],
+    -- continue the proof with nnreal
+    have h_zero_lt : (0 : nnreal) < (1 / 2) ^ p,
+    { rw [←nnreal.zero_rpow (ne_of_lt hp0_lt).symm, nnreal.rpow_lt_rpow_iff hp0_lt],
+      exact h_zero_lt_half, },
+    rw [←mul_le_mul_left (h_zero_lt), mul_add],
+    repeat {rw ←mul_pow,}, repeat {rw ← mul_assoc},
+    have h_rw : (1 / 2) ^ p * (2:nnreal) ^ (p - 1) = 1 / 2,
+    { rw [nnreal.rpow_sub, nnreal.div_rpow], simp only [one_div, nnreal.rpow_one, nnreal.one_rpow],
+      rw [←mul_div_assoc, inv_mul_cancel, one_div],
+      simp only [not_and, nnreal.rpow_eq_zero_iff, not_not, nnreal.coe_eq_zero, ne.def],
+      intro h_absurd, exfalso, exact two_ne_zero h_absurd,
+      exact two_ne_zero, },
+    repeat {rw h_rw,},
+    have h_nnnorm_add_le : ((1/2) * (nnnorm (f a + g a))) ^ p
+      ≤ ((1/2) * (nnnorm (f a)) + (1/2) * (nnnorm (g a))) ^ p,
+    { refine nnreal.rpow_le_rpow _ hp0,
+      rw [← mul_add, mul_le_mul_left h_zero_lt_half],
+      exact nnnorm_add_le (f a) (g a), },
+    rw ←nnreal.mul_rpow,
+    refine le_trans h_nnnorm_add_le _,
+    repeat {rw ←smul_eq_mul},
+    -- go from nnreal to ℝ to use convexity (which is not defined for nnreal)
+    suffices h_R :
+      (((1 / (2:nnreal)) • nnnorm (f a) + (1 / (2:nnreal)) • nnnorm (g a)) ^ p).val
+      ≤ ((1 / (2:nnreal)) • nnnorm (f a) ^ p + (1 / (2:nnreal)) • nnnorm (g a) ^ p).val,
+    { exact h_R, },
+    have h_eq_left :
+      (((1 / (2:nnreal)) • nnnorm (f a) + (1 / (2:nnreal)) • nnnorm (g a)) ^ p).val
+      = ((1 / (2:real)) • norm (f a) + (1 / (2:real)) • norm (g a)) ^ p,
+    { simp only [one_div, algebra.id.smul_eq_mul, nnreal.val_eq_coe, nnreal.coe_inv, nnreal.coe_add,
+        coe_nnnorm, nnreal.coe_rpow, nnreal.coe_one, nnreal.coe_bit0, nnreal.coe_mul], },
+    have h_eq_right :
+      ((1 / (2:nnreal)) • nnnorm (f a) ^ p + (1 / (2:nnreal)) • nnnorm (g a) ^ p).val
+      = (1 / (2:real)) • norm (f a) ^ p + (1 / (2:real)) • norm (g a) ^ p,
+    { simp only [one_div, algebra.id.smul_eq_mul, nnreal.val_eq_coe, nnreal.coe_inv, nnreal.coe_add,
+        coe_nnnorm, nnreal.coe_rpow, nnreal.coe_one, nnreal.coe_bit0, nnreal.coe_mul], },
+    rw [h_eq_left, h_eq_right],
+    -- use convexity of rpow
+    refine (convex_on_rpow hp1).right _ _ _ _ _,
+    rw set.mem_Ici, exact norm_nonneg (f a),
+    rw set.mem_Ici, exact norm_nonneg (g a),
+    exact le_of_lt h_zero_lt_half, exact le_of_lt h_zero_lt_half,
+    ring, },
+  { rw lintegral_add, simp_rw mul_comm, repeat {rw lintegral_mul_const},
+    rw ennreal.add_lt_top,
+    split; rw ennreal.mul_lt_top_iff; left;
+    { split, try { exact hf.2, }, try { exact hg.2, },
+      refine ennreal.rpow_lt_top_of_nonneg hp0_sub1 ennreal.coe_ne_top, },
+    -- finish by proving the measurability of all functions involved
+    exact measurable_nnnorm_rpow_coe hp0_lt hg.left,
+    exact measurable_nnnorm_rpow_coe hp0_lt hf.left,
+    all_goals { simp_rw [←@ennreal.coe_two, ennreal.coe_rpow_of_nonneg _ hp0,
+      ennreal.coe_rpow_of_nonneg _ hp0_sub1, ←ennreal.coe_mul],
+      refine measurable.ennreal_coe _, },
+    refine measurable.const_mul (measurable_nnnorm_rpow hp0_lt hf.1) _,
+    refine measurable.const_mul (measurable_nnnorm_rpow hp0_lt hg.1) _, },
+end
+
+lemma neg_in_ℒp {f : α → β} :
+  in_ℒp hp1 μ f → in_ℒp hp1 μ (-f) :=
+begin
+  intro hf,
+  split,
+  exact measurable.neg hf.1,
+  simp only [hf.right, pi.neg_apply, nnnorm_neg],
+end
+
+lemma is_add_subgroup {μ : measure α}:
+  is_add_subgroup {f : α → β | in_ℒp hp1 μ f} :=
+{ zero_mem := @zero_in_ℒp α β _ _ _ p hp1 μ,
+  add_mem := λ _ _, @ℒp.add_in_ℒp α β _ _ _ p hp1 μ _ _ _ _,
+  neg_mem := λ _, @ℒp.neg_in_ℒp α β _ _ _ p hp1 μ _ _ _ }
+
+instance {p : ℝ} {hp1 : 1 ≤ p} {μ : measure α} : add_comm_group (ℒp α β hp1 μ) :=
+@subtype.add_comm_group _ _ {f : α → β | in_ℒp hp1 μ f} (is_add_subgroup)
+
+end borel_space
 
 end ℒp

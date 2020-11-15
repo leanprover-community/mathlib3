99c4575d1b4ccde35a753f371a4b2d8bbbf6fdf1
Bryan Gin-ge Chen <bryangingechen@gmail.com>
2020-11-14 21:31:52 -0500

    reviewer comments

--- archive/imo/imo1969_q1.lean
+++ archive/imo/imo1969_q1.lean
@@ -39,12 +39,12 @@ lemma right_factor_large {m : ℤ} (n : ℤ) (h : 1 < m) : 1 < ((n + m)^2 + m^2)
 /-!
 The factorization is over the integers, but we need the nonprimality over the natural numbers.
 -/
-lemma int_large {a : ℤ} (h : 1 < a) : 1 < a.nat_abs :=
+lemma int_large {m : ℤ} (h : 1 < m) : 1 < m.nat_abs :=
 by exact_mod_cast lt_of_lt_of_le h le_nat_abs
 
-lemma not_prime_of_int_mul' {a b : ℤ} {c : ℕ}
-  (ha : 1 < a) (hb : 1 < b) (hc : a*b = (c : ℤ)) : ¬ prime c :=
-not_prime_of_int_mul (int_large ha) (int_large hb) hc
+lemma not_prime_of_int_mul' {m n : ℤ} {c : ℕ}
+  (hm : 1 < m) (hn : 1 < n) (hc : m*n = (c : ℤ)) : ¬ prime c :=
+not_prime_of_int_mul (int_large hm) (int_large hn) hc
 
 /-- Every natural number of the form `n^4 + 4*m^4` is not prime. -/
 lemma polynomial_not_prime {m : ℕ} (h1 : 1 < m) (n : ℕ) : ¬ prime (n^4 + 4*m^4) :=
@@ -55,19 +55,19 @@ begin
 end
 
 /--
-We define $a(b) := 4*(2+b)^4$, so that we can take $m = 2+b$ in `polynomial_not_prime`.
+We define $a_{choice}(b) := 4*(2+b)^4$, so that we can take $m = 2+b$ in `polynomial_not_prime`.
 -/
-def a (b : ℕ) : ℕ := 4*(2+b)^4
+def a_choice (b : ℕ) : ℕ := 4*(2+b)^4
 
-lemma a_good (b : ℕ) : a b ∈ good_nats :=
+lemma a_choice_good (b : ℕ) : a_choice b ∈ good_nats :=
 polynomial_not_prime (show 1 < 2+b, by linarith)
 
-/-- `a` is a strictly monotone function; this is easily proven by chaining together lemmas
+/-- `a_choice` is a strictly monotone function; this is easily proven by chaining together lemmas
 in the `strict_mono` namespace. -/
-lemma a_strict_mono : strict_mono a :=
+lemma a_choice_strict_mono : strict_mono a_choice :=
 ((strict_mono_id.const_add 2).nat_pow (dec_trivial : 0 < 4)).const_mul (dec_trivial : 0 < 4)
 
-/-- We conclude by using the fact that `a` is an injective function from the natural numbers to
-the set `good_nats`. -/
-theorem imo1969_q1 : set.infinite good_nats :=
-set.infinite_of_injective_forall_mem a_strict_mono.injective a_good
+/-- We conclude by using the fact that `a_choice` is an injective function from the natural numbers
+to the set `good_nats`. -/
+theorem imo1969_q1 : set.infinite {a : ℕ | ∀ n : ℕ, ¬ prime (n^4 + a)} :=
+set.infinite_of_injective_forall_mem a_choice_strict_mono.injective a_choice_good

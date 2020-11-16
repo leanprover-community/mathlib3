197883abb93e654fc01ffc9c3a4f481dc89c9cc2
Alex J. Best <alex.j.best@gmail.com>
2020-11-16 02:09:14 -0500

    typo

--- src/analysis/specific_limits.lean
+++ src/analysis/specific_limits.lean
@@ -1,5 +1,5 @@
 /-
-Copyright (c) 2017 Johannes Hölzl. Ahas_sum_geometric_of_lt_1ll rights reserved.
+Copyright (c) 2017 Johannes Hölzl. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Johannes Hölzl
 -/

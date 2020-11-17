e63f915a1865b950909d4e5163ca9c74546c039f
Yakov Pechersky <ffxen158@gmail.com>
2020-11-17 05:08:50 -0500

    feat(data/list): map lemmas paralleling functor

--- src/data/list/basic.lean
+++ src/data/list/basic.lean
@@ -1390,6 +1390,22 @@ begin
     cases x, simpa using hxy, simp at hxy, simp [y_ih hxy.2, h hxy.1] }
 end
 
+/--
+A single `list.map` of a composition of functions is equal to
+composing a `list.map` with another `list.map`, fully applied.
+This is the reverse direction of `list.map_map`.
+-/
+lemma comp_map {α β γ : Type*} (l : list α) (g : α → β) (h : β → γ)  :
+  map (h ∘ g) l = map h (map g l) := (map_map _ _ _).symm
+
+/--
+Composing a `list.map` with another `list.map` is equal to
+a single `list.map` of composed functions.
+-/
+@[simp] lemma map_comp_map {α β γ : Type*} (f : α → β) (g : β → γ) :
+  map g ∘ map f = map (g ∘ f) :=
+by apply funext; intro; rw comp_map
+
 theorem map_filter_eq_foldr (f : α → β) (p : α → Prop) [decidable_pred p] (as : list α) :
   map f (filter p as) = foldr (λ a bs, if p a then f a :: bs else bs) [] as :=
 by { induction as, { refl }, { simp! [*, apply_ite (map f)] } }

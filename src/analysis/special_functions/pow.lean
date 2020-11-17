216cf2115190a616daada707d24ee0b824524892
RemyDegenne <Remydegenne@gmail.com>
2020-11-17 19:17:28 +0100

    add my name to the authors

--- src/analysis/special_functions/pow.lean
+++ src/analysis/special_functions/pow.lean
@@ -1,7 +1,8 @@
 /-
 Copyright (c) 2018 Chris Hughes. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
-Authors: Chris Hughes, Abhimanyu Pallavi Sudhir, Jean Lo, Calle Sönne, Sébastien Gouëzel
+Authors: Chris Hughes, Abhimanyu Pallavi Sudhir, Jean Lo, Calle Sönne, Sébastien Gouëzel,
+  Rémy Degenne
 -/
 import analysis.special_functions.trigonometric
 import analysis.calculus.extend_deriv

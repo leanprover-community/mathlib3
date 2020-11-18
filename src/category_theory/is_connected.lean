dab2ae389b986715a29d81294d884d654ddb80bf
Bhavik Mehta <bhavikmehta8@gmail.com>
2020-11-18 09:32:06 +0000

    feat(category_theory/is_connected): transfer across equivalence (#5021)
    Also renames some universes to match usual conventions

--- src/category_theory/is_connected.lean
+++ src/category_theory/is_connected.lean
@@ -50,8 +50,8 @@ namespace category_theory
 /--
 A possibly empty category for which every functor to a discrete category is constant.
 -/
-class is_preconnected (J : Type v₂) [category.{v₁} J] : Prop :=
-(iso_constant : Π {α : Type v₂} (F : J ⥤ discrete α) (j : J),
+class is_preconnected (J : Type u₁) [category.{v₁} J] : Prop :=
+(iso_constant : Π {α : Type u₁} (F : J ⥤ discrete α) (j : J),
   nonempty (F ≅ (functor.const J).obj (F.obj j)))
 
 /--
@@ -66,18 +66,19 @@ This allows us to show that the functor X ⨯ - preserves connected limits.
 
 See https://stacks.math.columbia.edu/tag/002S
 -/
-class is_connected (J : Type v₂) [category.{v₁} J] extends is_preconnected J : Prop :=
+class is_connected (J : Type u₁) [category.{v₁} J] extends is_preconnected J : Prop :=
 [is_nonempty : nonempty J]
 
 attribute [instance, priority 100] is_connected.is_nonempty
 
-variables {J : Type v₂} [category.{v₁} J]
+variables {J : Type u₁} [category.{v₁} J]
+variables {K : Type u₂} [category.{v₂} K]
 
 /--
 If `J` is connected, any functor `F : J ⥤ discrete α` is isomorphic to
 the constant functor with value `F.obj j` (for any choice of `j`).
 -/
-def iso_constant [is_preconnected J] {α : Type v₂} (F : J ⥤ discrete α) (j : J) :
+def iso_constant [is_preconnected J] {α : Type u₁} (F : J ⥤ discrete α) (j : J) :
   F ≅ (functor.const J).obj (F.obj j) :=
   (is_preconnected.iso_constant F j).some
 
@@ -86,7 +87,7 @@ If J is connected, any functor to a discrete category is constant on objects.
 The converse is given in `is_connected.of_any_functor_const_on_obj`.
 -/
 lemma any_functor_const_on_obj [is_preconnected J]
-  {α : Type v₂} (F : J ⥤ discrete α) (j j' : J) :
+  {α : Type u₁} (F : J ⥤ discrete α) (j j' : J) :
   F.obj j = F.obj j' :=
 ((iso_constant F j').hom.app j).down.1
 
@@ -95,7 +96,7 @@ If any functor to a discrete category is constant on objects, J is connected.
 The converse of `any_functor_const_on_obj`.
 -/
 lemma is_connected.of_any_functor_const_on_obj [nonempty J]
-  (h : ∀ {α : Type v₂} (F : J ⥤ discrete α), ∀ (j j' : J), F.obj j = F.obj j') :
+  (h : ∀ {α : Type u₁} (F : J ⥤ discrete α), ∀ (j j' : J), F.obj j = F.obj j') :
   is_connected J :=
 { iso_constant := λ α F j',
   ⟨nat_iso.of_components (λ j, eq_to_iso (h F j j')) (λ _ _ _, subsingleton.elim _ _)⟩ }
@@ -107,7 +108,7 @@ This can be thought of as a local-to-global property.
 
 The converse is shown in `is_connected.of_constant_of_preserves_morphisms`
 -/
-lemma constant_of_preserves_morphisms [is_preconnected J] {α : Type v₂} (F : J → α)
+lemma constant_of_preserves_morphisms [is_preconnected J] {α : Type u₁} (F : J → α)
   (h : ∀ (j₁ j₂ : J) (f : j₁ ⟶ j₂), F j₁ = F j₂) (j j' : J) :
   F j = F j' :=
 any_functor_const_on_obj { obj := F, map := λ _ _ f, eq_to_hom (h _ _ f) } j j'
@@ -120,7 +121,7 @@ This can be thought of as a local-to-global property.
 The converse of `constant_of_preserves_morphisms`.
 -/
 lemma is_connected.of_constant_of_preserves_morphisms [nonempty J]
-  (h : ∀ {α : Type v₂} (F : J → α), (∀ {j₁ j₂ : J} (f : j₁ ⟶ j₂), F j₁ = F j₂) → (∀ j j' : J, F j = F j')) :
+  (h : ∀ {α : Type u₁} (F : J → α), (∀ {j₁ j₂ : J} (f : j₁ ⟶ j₂), F j₁ = F j₂) → (∀ j j' : J, F j = F j')) :
   is_connected J :=
 is_connected.of_any_functor_const_on_obj (λ _ F, h F.obj (λ _ _ f, (F.map f).down.1))
 
@@ -169,6 +170,27 @@ lemma is_preconnected_induction [is_preconnected J] (Z : J → Sort*)
   {j₀ : J} (x : Z j₀) (j : J) : nonempty (Z j) :=
 (induct_on_objects {j | nonempty (Z j)} ⟨x⟩
   (λ j₁ j₂ f, ⟨by { rintro ⟨y⟩, exact ⟨h₁ f y⟩, }, by { rintro ⟨y⟩, exact ⟨h₂ f y⟩, }⟩) j : _)
+
+/-- If `J` and `K` are equivalent, then if `J` is preconnected then `K` is as well. -/
+lemma is_preconnected_of_equivalent {K : Type u₁} [category.{v₂} K] [is_preconnected J]
+  (e : J ≌ K) :
+  is_preconnected K :=
+{ iso_constant := λ α F k, ⟨
+  calc F ≅ e.inverse ⋙ e.functor ⋙ F : (e.inv_fun_id_assoc F).symm
+     ... ≅ e.inverse ⋙ (functor.const J).obj ((e.functor ⋙ F).obj (e.inverse.obj k)) :
+                       iso_whisker_left e.inverse (iso_constant (e.functor ⋙ F) (e.inverse.obj k))
+
+     ... ≅ e.inverse ⋙ (functor.const J).obj (F.obj k) :
+          iso_whisker_left _ ((F ⋙ functor.const J).map_iso (e.counit_iso.app k))
+     ... ≅ (functor.const K).obj (F.obj k) : nat_iso.of_components (λ X, iso.refl _) (by simp),
+  ⟩ }
+
+/-- If `J` and `K` are equivalent, then if `J` is connected then `K` is as well. -/
+lemma is_connected_of_equivalent {K : Type u₁} [category.{v₂} K]
+  (e : J ≌ K) [is_connected J] :
+  is_connected K :=
+{ is_nonempty := nonempty.map e.functor.obj (by apply_instance),
+  to_is_preconnected := is_preconnected_of_equivalent e }
 
 /-- j₁ and j₂ are related by `zag` if there is a morphism between them. -/
 @[reducible]
@@ -256,7 +278,7 @@ discrete.equiv_of_equivalence
     unit_iso := by { exact (iso_constant _ (classical.arbitrary _)), },
     counit_iso := functor.punit_ext _ _ }
 
-variables {C : Type u₂} [category.{v₂} C]
+variables {C : Type u₂} [category.{u₁} C]
 
 /--
 For objects `X Y : C`, any natural transformation `α : const X ⟶ const Y` from a connected

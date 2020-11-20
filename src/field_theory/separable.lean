b6ae6259caa3bbadf3f44ea77b5e09c86da679e7
Riccardo Brasca <riccardo.brasca@gmail.com>
2020-11-20 19:25:28 +0100

    Update src/field_theory/separable.lean
    Co-authored-by: Aaron Anderson <65780815+awainverse@users.noreply.github.com>

--- src/field_theory/separable.lean
+++ src/field_theory/separable.lean
@@ -485,7 +485,7 @@ begin
 end
 
 /--If `n ≠ 0` in `F`, then ` X ^ n - a` is squarefree for any `a ≠ 0`. -/
-lemma squarefree_of_X_pow_sub_C {n : ℕ} (a : F) (hn : ↑n ≠ (0 : F)) (hpos : 0 < n) (ha : a ≠ 0) :
+lemma squarefree_X_pow_sub_C {n : ℕ} (a : F) (hn : ↑n ≠ (0 : F)) (hpos : 0 < n) (ha : a ≠ 0) :
   squarefree (X ^ n - C a) :=
 separable.squarefree (separable_of_X_pow_sub_C a hn hpos ha)
 

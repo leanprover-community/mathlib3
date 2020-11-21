a4f395f2201eb8e82fd32e21bbf9b385ad8d65a4
Alena Gusakov <agusakov@udel.edu>
2020-11-21 16:42:44 -0500

    added forall_mem_union in finset

--- src/data/finset/basic.lean
+++ src/data/finset/basic.lean
@@ -428,6 +428,11 @@ mem_union.2 $ or.inl h
 theorem mem_union_right {a : α} {s₂ : finset α} (s₁ : finset α) (h : a ∈ s₂) : a ∈ s₁ ∪ s₂ :=
 mem_union.2 $ or.inr h
 
+theorem forall_mem_union [decidable_eq α] {s₁ s₂ : finset α} {p : α → Prop} :
+  (∀ a ∈ s₁, p a) ∧ (∀ b ∈ s₂, p b) ↔ ∀ ab ∈ (s₁ ∪ s₂), p ab :=
+⟨λ h ab hab, (mem_union.mp hab).elim (h.1 _) (h.2 _),
+ λ h, ⟨λ a, h a ∘ mem_union_left _, λ b, h b ∘ mem_union_right _⟩⟩
+
 theorem not_mem_union {a : α} {s₁ s₂ : finset α} : a ∉ s₁ ∪ s₂ ↔ a ∉ s₁ ∧ a ∉ s₂ :=
 by rw [mem_union, not_or_distrib]
 

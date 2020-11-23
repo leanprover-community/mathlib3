97bac6671ae44d3ff385dc6991c336f4af7aa702
Eric Wieser <wieser.eric@gmail.com>
2020-11-23 11:28:53 +0000

    chore(*): Fix linter error

--- src/linear_algebra/alternating.lean
+++ src/linear_algebra/alternating.lean
@@ -117,7 +117,8 @@ begin
     have assoc : ν ∘ (s * equiv.swap x y : equiv.perm ι) = (ν ∘ s ∘ equiv.swap x y) := rfl,
     rw [assoc, map_swap (ν ∘ s) hxy, ←neg_one_smul ℤ (g (ν ∘ s))],
     have h1 : (-1 : ℤ) = equiv.perm.sign (equiv.swap x y) := by simp [hxy],
-    rw [h1, smul_smul, ←units.coe_mul, ←equiv.perm.sign_mul, mul_assoc, equiv.swap_mul_self, mul_one],
+    rw [h1, smul_smul, ←units.coe_mul, ←equiv.perm.sign_mul, mul_assoc, equiv.swap_mul_self,
+      mul_one],
     assumption, },
 end
 

16fd4a4bce34bf844c95ee3aacbff4966260b99b
Eric Wieser <wieser.eric@gmail.com>
2020-11-23 19:02:33 +0000

    Update src/group_theory/perm/sign.lean

--- src/group_theory/perm/sign.lean
+++ src/group_theory/perm/sign.lean
@@ -261,7 +261,10 @@ begin
     exact hmul_swap _ _ _ hxy.1 (ih _ ⟨rfl, λ v hv, hl.2 _ (list.mem_cons_of_mem _ hv)⟩ h1 hmul_swap) }
 end
 
-/-- Like `swap_induction_on`, but with the composition on the right of `f` -/
+/-- Like `swap_induction_on`, but with the composition on the right of `f`
+
+An induction principle for permutations. If `P` holds for the identity permutation, and
+is preserved under composition with a non-trivial swap, then `P` holds for all permutations. -/
 @[elab_as_eliminator] lemma swap_induction_on' [fintype α] {P : perm α → Prop} (f : perm α) :
   P 1 → (∀ f x y, x ≠ y → P f → P (f * swap x y)) → P f :=
 λ h1 IH, inv_inv f ▸ swap_induction_on f⁻¹ h1 (λ f, IH f⁻¹)

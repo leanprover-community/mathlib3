9f910138b286435c232f9d4365151ae326dc73af
Alena Gusakov <agusakov@udel.edu>
2020-11-24 18:57:07 -0500

    satisfying linter

--- src/order/antichain.lean
+++ src/order/antichain.lean
@@ -13,7 +13,16 @@ Investigating the structure of finsets in a partial order.
 We define antichains.
 
 ## Main definitions
-* `antichain` is a family of sets where no set is a subset of another.
+* `antichain` is a finset of elements in a partial order where
+*   no element is less than another.
+
+* The `join` of two antichains is the set obtained by taking
+*   their union and removing all elements that are less than
+*   another element.
+
+* The `meet` of two antichains is the set obtained by taking
+*   their intersection and removing all elements that are less than
+*   another element.
 -/
 
 open partial_order
@@ -26,7 +35,7 @@ noncomputable theory
 variable {α : Type u}
 
 section
-variables [partial_order α] [decidable_eq α] [fintype α]
+variables [partial_order α]
 
 /--
 A set of elements of a partial order forms an antichain if no two elements
@@ -48,18 +57,29 @@ begin
   apply ha a (finset.mem_of_subset hb ha2) b (finset.mem_of_subset hb hb2) hab,
 end
 
-
+/--
+The join of two antichains `A` and `B` is the set obtained by taking
+the union of `A` and `B` and removing all elements `a` that are
+less than some element `b`.
+-/
 def antichain_join (A B : finset α) :
   set α := { a | a ∈ A ∪ B ∧ ∀ b ∈ A ∪ B, a ≤ b → a = b}
 
-variables (A B : finset α) [antichain A] [antichain B]
-
+/--
+The finset obtained by applying `antichain_join` to `A ∪ B` using
+`finset.filter`
+-/
 def antichain.join (A B : finset α):
   finset α := (A ∪ B).filter (λ a, a ∈ antichain_join A B)
 
+/--
+The meet of two antichains `A` and `B` is the set obtained by taking
+the intersection of `A` and `B` and removing all elements `a` that are
+less than some element `b`.
+-/
 def antichain.meet (A B : finset α) : finset α := A ∩ B
 
-theorem join_antichain (A B : finset α) (ha : antichain A) (hb : antichain B) :
+theorem join_antichain (A B : finset α) :
   antichain (antichain.join A B) :=
 begin
   intros a ha2 b hb2 hle,
@@ -83,7 +103,5 @@ begin
 end
 
 
-
-
-
 end
+#lint

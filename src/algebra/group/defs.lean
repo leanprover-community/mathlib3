544c27cd1df0d541157ae7348b43bbd7b26ca83f
Vierkantor <vierkantor@vierkantor.com>
2020-11-25 12:59:01 +0000

    chore(algebra): add `has_div`/`has_sub` to `(add_)group(_with_zero)`
    The goal of this change is to avoid diamonds when `has_div` and `has_inv` are
    defined separately, e.g. the diamond `field -> euclidean_domain -> has_div;
    field -> group_with_zero -> has_div`.

--- src/algebra/group/defs.lean
+++ src/algebra/group/defs.lean
@@ -290,14 +290,57 @@ class cancel_comm_monoid (M : Type u) extends left_cancel_comm_monoid M, right_c
 
 end cancel_monoid
 
-/-- A `group` is a `monoid` with an operation `⁻¹` satisfying `a⁻¹ * a = 1`. -/
-@[protect_proj, ancestor monoid has_inv]
-class group (α : Type u) extends monoid α, has_inv α :=
-(mul_left_inv : ∀ a : α, a⁻¹ * a = 1)
-/-- An `add_group` is an `add_monoid` with a unary `-` satisfying `-a + a = 0`. -/
-@[protect_proj, ancestor add_monoid has_neg]
-class add_group (α : Type u) extends add_monoid α, has_neg α :=
-(add_left_neg : ∀ a : α, -a + a = 0)
+/-- `div_sub_tac` proves `∀ a b, a / b = a * b⁻¹` or `∀ a b, a - b = a + -b`
+if it holds by definition. -/
+meta def div_sub_tac : tactic unit := `[intros; refl]
+
+/-- A `div_inv_monoid` is a `monoid` with operations `/` and `⁻¹` satisfying
+`div_eq_mul_inv : ∀ a b, a / b = a * b⁻¹`.
+
+This is the immediate common ancestor of `group` and `group_with_zero`,
+in order to deduplicate the name `div_eq_mul_inv`.
+The default for `div` is such that `a / b = a * b⁻¹` holds by definition.
+-/
+@[protect_proj, ancestor monoid has_inv has_div]
+class div_inv_monoid (G : Type u) extends monoid G, has_inv G, has_div G :=
+(div := λ a b, a * b⁻¹)
+(div_eq_mul_inv : ∀ a b : G, a / b = a * b⁻¹ . div_sub_tac)
+
+/-- A `sub_neg_monoid` is an `add_monoid` with unary `-` and binary `-` operations
+satisfying `sub_eq_add_neg : ∀ a b, a - b = a + -b`.
+
+The default for `sub` is such that `a - b = a + -b` holds by definition.
+-/
+@[protect_proj, ancestor add_monoid has_neg has_sub]
+class sub_neg_monoid (G : Type u) extends add_monoid G, has_neg G, has_sub G :=
+(sub := λ a b, a + -b)
+(sub_eq_add_neg : ∀ a b : G, a - b = a + -b . div_sub_tac)
+
+attribute [to_additive sub_neg_monoid] div_inv_monoid
+
+@[to_additive]
+lemma div_eq_mul_inv {G : Type u} [div_inv_monoid G] :
+  ∀ a b : G, a / b = a * b⁻¹ :=
+div_inv_monoid.div_eq_mul_inv
+
+/-- A `group` is a `monoid` with an operation `⁻¹` satisfying `a⁻¹ * a = 1`.
+
+There is also a division operation `/` such that `a / b = a * b⁻¹`,
+with a default so that `a / b = a * b⁻¹` holds by definition.
+-/
+@[protect_proj, ancestor div_inv_monoid]
+class group (G : Type u) extends div_inv_monoid G :=
+(mul_left_inv : ∀ a : G, a⁻¹ * a = 1)
+
+/-- An `add_group` is an `add_monoid` with a unary `-` satisfying `-a + a = 0`.
+
+There is also a binary operation `-` such that `a - b = a + -b`,
+with a default so that `a - b = a + -b` holds by definition.
+-/
+@[protect_proj, ancestor sub_neg_monoid]
+class add_group (A : Type u) extends sub_neg_monoid A :=
+(add_left_neg : ∀ a : A, -a + a = 0)
+
 attribute [to_additive] group
 
 section group
@@ -339,23 +382,6 @@ instance group.to_cancel_monoid : cancel_monoid G :=
   ..‹group G› }
 
 end group
-
-section add_group
-
-variables {G : Type u} [add_group G]
-
-/-- The subtraction operation on an `add_group` -/
-@[reducible] protected def algebra.sub (a b : G) : G :=
-a + -b
-
-@[priority 100]    -- see Note [lower instance priority]
-instance add_group_has_sub : has_sub G :=
-⟨algebra.sub⟩
-
-lemma sub_eq_add_neg (a b : G) : a - b = a + -b :=
-rfl
-
-end add_group
 
 /-- A commutative group is a group with commutative `(*)`. -/
 @[protect_proj, ancestor group comm_monoid]

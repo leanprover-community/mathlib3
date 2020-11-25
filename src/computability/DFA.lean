f7057622eeff8f1226437110efd73ddb661d5f15
foxthomson <foxthomson0@gmail.com>
2020-11-25 16:42:37 +0000

    Unbundle state for DFAs

--- src/computability/DFA.lean
+++ src/computability/DFA.lean
@@ -17,25 +17,20 @@ universes u v
 
 /-- A DFA is a set of states (`state`), a transition function from state to state labelled by the
   alphabet (`step`), a starting state (`start`) and a set of acceptance states (`accept`) -/
-structure DFA (alphabet : Type u) :=
-(state : Type v)
-[state_fintype : fintype state]
-[state_dec : decidable_eq state]
+structure DFA (alphabet : Type u) (state : Type v) :=
 (step : state → alphabet → state)
 (start : state)
 (accept : finset state)
 
 namespace DFA
 
-variables {α : Type u} (M : DFA α)
+variables {α : Type u} {σ σ₁ σ₂ σ₃ : Type v} (M : DFA α σ)
 
-instance DFA_inhabited : inhabited (DFA α) := ⟨ DFA.mk punit (λ _ _, punit.star) punit.star ∅ ⟩
-
-instance dec := M.state_dec
-instance fin := M.state_fintype
+instance DFA_inhabited [inhabited σ] : inhabited (DFA α σ) :=
+  ⟨ DFA.mk (λ _ _, default σ) (default σ) ∅ ⟩
 
 /-- `M.eval_from s x` evaluates  `M` with input `x` starting from the state `s` -/
-def eval_from (start : M.state) : list α → M.state :=
+def eval_from (start : σ) : list α → σ :=
 list.foldl M.step start
 
 /-- `M.eval x` evaluates  `M` with input `x` starting from the state `M.start` -/
@@ -46,16 +41,16 @@ def accepts (x : list α) : Prop :=
 M.eval x ∈ M.accept
 
 /-- Two DFA's are equivalent if the accept exactly the same strings -/
-def equiv (M N : DFA α) : Prop := ∀ x, M.accepts x ↔ N.accepts x
+def equiv (M : DFA α σ₁) (N : DFA α σ₂) : Prop := ∀ x, M.accepts x ↔ N.accepts x
 
 local infix ` ≈ ` := equiv
 
-@[refl] lemma equiv_refl (M : DFA α) : M ≈ M := λ x, by refl
-@[symm] lemma equiv_symm (M N : DFA α) : M ≈ N → N ≈ M := λ h x, (h x).symm
-@[trans] lemma equiv_trans (M N P : DFA α) : M ≈ N → N ≈ P → M ≈ P :=
+@[refl] lemma equiv_refl (M : DFA α σ) : M ≈ M := λ x, by refl
+@[symm] lemma equiv_symm (M : DFA α σ₁) (N : DFA α σ₂) : M ≈ N → N ≈ M := λ h x, (h x).symm
+@[trans] lemma equiv_trans (M : DFA α σ₁) (N : DFA α σ₂) (P : DFA α σ₃) : M ≈ N → N ≈ P → M ≈ P :=
   λ h₁ h₂ x, iff.trans (h₁ x) (h₂ x)
 
-@[simp] lemma equiv_def (M N : DFA α) : M ≈ N ↔ ∀ x, M.accepts x ↔ N.accepts x :=
+@[simp] lemma equiv_def (M : DFA α σ₁) (N : DFA α σ₂) : M ≈ N ↔ ∀ x, M.accepts x ↔ N.accepts x :=
   by refl
 
 end DFA

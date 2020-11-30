0387c411fbebd5d53e8bb0e1092aac07ca7734eb
Riccardo Brasca <riccardo.brasca@gmail.com>
2020-11-30 16:12:23 +0100

    long line

--- src/number_theory/divisors.lean
+++ src/number_theory/divisors.lean
@@ -127,7 +127,7 @@ begin
 end
 
 lemma divisors_subset_of_dvd {m : ℕ} (hzero : n ≠ 0) (h : m ∣ n) : divisors m ⊆ divisors n :=
-finset.subset_iff.mpr $ λ x hx, nat.mem_divisors.mpr (⟨dvd.trans (nat.mem_divisors.mp hx).1 h, hzero⟩)
+finset.subset_iff.2 $ λ x hx, nat.mem_divisors.mpr (⟨dvd.trans (nat.mem_divisors.mp hx).1 h, hzero⟩)
 
 lemma divisors_subset_proper_divisors {m : ℕ} (hzero : n ≠ 0) (h : m ∣ n) (hdiff : m ≠ n) :
   divisors m ⊆ proper_divisors n :=

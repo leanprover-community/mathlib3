96c84c29cac57e5e5d9591d36e979c191de0a472
Alena Gusakov <agusakov@udel.edu>
2020-11-30 14:19:47 -0500

    matching and perfect matching definitions done

--- src/combinatorics/simple_graph/matching.lean
+++ src/combinatorics/simple_graph/matching.lean
@@ -18,28 +18,46 @@ import combinatorics.simple_graph.basic
 * a `perfect_matching` on a simple graph is a matching in which every
    vertex belongs to an edge.
 
-## Implementation notes
+TODO:
+  - Lemma stating that the existence of a perfect matching on `G` implies that
+    the cardinality of `V` is even (assuming it's finite)
+  - Hall's Marriage Theorem
+  - Tutte's Theorem
 
-
-TODO: perfect matchings, Hall's Marriage Theorem
-
+TODO: Tutte and Hall require a definition of subgraphs.
 -/
 open finset
 universe u
 
 namespace simple_graph
-variables {V : Type u} (G : simple_graph V)
+variables {V : Type u} (G : simple_graph V) (M : set (sym2 V)) (h : M ⊆ edge_set G)
+
+instance : inhabited (set (sym2 V)) :=
+  { default := (complete_graph V).edge_set }
 
 /--
-A matching on `G` is a subset of its edges such that no two edges share an endpoint.
+A matching on `G` is a subset of its edges such that no two edges share a vertex.
 -/
-def matching (M : set (sym2 V)) (h : M ⊆ edge_set G) : Prop :=
-  ∀ x y ∈ M, ∀ v : V, v ∈ x ∧ v ∈ y → x = y
+def matching : Type* :=
+  { M : set (sym2 V) // M ⊆ edge_set G ∧ ∀ x ∈ M, ∀ y ∈ M, ∀ v : V, v ∈ x ∧ v ∈ y → x = y }
+
+instance : inhabited (matching G) := { default :=
+  ⟨(∅ : set (sym2 V)),
+    begin
+      split,
+      exact set.empty_subset G.edge_set,
+      intros x hx y hy v hv,
+      have h2 := set.mem_empty_eq x,
+      by_contra,
+      rw ← h2,
+      exact hx,
+    end ⟩ }
 
 /--
-A perfect matching on `G` is a matching such that every vertex is contained in an edge.
+A perfect matching on `G` with vertex set `V` is a matching such that
+  every vertex in `V` is contained in an edge of `G`.
 -/
-def perfect_matching (P : set (sym2 V)) (h1 : P ⊆ edge_set G) (h2 : matching G P h1) :
-  Prop := ∀ v : V, ∃ x ∈ P, v ∈ x
+def perfect_matching (M : matching G) :
+  Prop := ∀ v : V, ∃ x ∈ M.1, v ∈ x
 
 end simple_graph

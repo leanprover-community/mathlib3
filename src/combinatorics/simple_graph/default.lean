469f8e717096629e30b2fa76e53b771209190d7a
Alena Gusakov <agusakov@udel.edu>
2020-12-01 09:36:22 -0500

    deleted everything except basic

--- src/combinatorics/simple_graph/default.lean
+++
@@ -1,6 +0,0 @@
-import combinatorics.simple_graph.basic
-import combinatorics.simple_graph.hom
-import combinatorics.simple_graph.subgraph
-import combinatorics.simple_graph.simple_graph_on
-import combinatorics.simple_graph.walks
-import combinatorics.simple_graph.degree_sum

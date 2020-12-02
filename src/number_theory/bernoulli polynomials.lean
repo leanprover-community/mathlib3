861ddc8d28e009d82c5882e499904e8d2e01ce82
Ashvni <ashvni.n@gmail.com>
2020-12-02 12:13:02 +0000

    first defs

--- src/number_theory/bernoulli polynomials.lean
+++ src/number_theory/bernoulli polynomials.lean
@@ -1,1 +1,17 @@
 import number_theory.bernoulli
+import data.nat.basic
+import analysis.complex.roots_of_unity
+import data.complex.exponential
+
+noncomputable theory
+def bernoulli_polynomial (n : ℕ) : ℂ → ℂ := λ X, ∑' i : fin (n+1), (bernoulli n)*(nat.choose n i)*(X^(n-i))
+
+lemma one_sub_eq_neg : ∀ n : ℕ, ∀ X : ℂ, bernoulli_polynomial n (1 - X) = (-1)^n * bernoulli_polynomial n X :=
+begin
+  sorry
+end
+
+lemma exp_bernoulli : ∀ t : ℕ, ∀ X : ℕ, ∑' i : ℕ, (bernoulli_polynomial i X) * t^i / (nat.factorial i) = (t * real.exp (X * t))/(real.exp t - 1) :=
+begin
+  sorry
+end

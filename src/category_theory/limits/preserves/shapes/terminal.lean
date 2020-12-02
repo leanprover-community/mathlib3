ef7aac06aa12713456710e307a9ae7f8cafce08c
Bhavik Mehta <bhavikmehta8@gmail.com>
2020-12-02 17:38:27 +0000

    fixup

--- src/category_theory/limits/preserves/shapes/terminal.lean
+++ src/category_theory/limits/preserves/shapes/terminal.lean
@@ -20,7 +20,7 @@ universes v u₁ u₂
 
 noncomputable theory
 
-open category limits
+open category_theory category_theory.category category_theory.limits
 
 variables {C : Type u₁} [category.{v} C]
 variables {D : Type u₂} [category.{v} D]
@@ -31,22 +31,22 @@ namespace category_theory.limits
 variables (X : C)
 
 /--
-The map of an empty cone is a limit iff the empty cone consisting of the mapped object is a limit.
+The map of an empty cone is a limit iff the mapped object is terminal.
 -/
-def is_limit_map_cone_fork_equiv :
+def is_limit_map_cone_empty_cone_equiv :
   is_limit (G.map_cone (as_empty_cone X)) ≃ is_terminal (G.obj X) :=
 (is_limit.postcompose_hom_equiv (functor.empty_ext _ _) _).symm.trans
   (is_limit.equiv_iso_limit (cones.ext (iso.refl _) (by tidy)))
 
 /-- The property of preserving terminal objects expressed in terms of `is_terminal`. -/
-def map_is_limit_of_preserves_of_is_limit [preserves_limit (functor.empty C) G]
+def is_terminal_obj_of_is_terminal [preserves_limit (functor.empty C) G]
   (l : is_terminal X) : is_terminal (G.obj X) :=
-terminal_map_cone_limit G X (preserves_limit.preserves l)
+is_limit_map_cone_empty_cone_equiv G X (preserves_limit.preserves l)
 
 /-- The property of reflecting terminal objects expressed in terms of `is_terminal`. -/
-def is_limit_of_reflects_of_map_is_limit [reflects_limit (functor.empty C) G]
+def is_terminal_of_is_terminal_obj [reflects_limit (functor.empty C) G]
   (l : is_terminal (G.obj X)) : is_terminal X :=
-reflects_limit.reflects ((terminal_map_cone_limit G X).symm l)
+reflects_limit.reflects ((is_limit_map_cone_empty_cone_equiv G X).symm l)
 
 variables [has_terminal C]
 /--
@@ -55,7 +55,7 @@ object is terminal.
 -/
 def is_limit_of_has_terminal_of_preserves_limit [preserves_limit (functor.empty C) G] :
   is_terminal (G.obj (⊤_ C)) :=
-map_is_limit_of_preserves_of_is_limit G (⊤_ C) terminal_is_terminal
+is_terminal_obj_of_is_terminal G (⊤_ C) terminal_is_terminal
 
 /--
 If `C` has a terminal object and `G` preserves terminal objects, then `D` has a terminal object
@@ -72,39 +72,40 @@ begin
   apply has_limit_of_iso F.unique_from_empty.symm,
 end⟩
 
+variable [has_terminal D]
 /--
 If the terminal comparison map for `G` is an isomorphism, then `G` preserves terminal objects.
 -/
-def preserves_terminal_of_iso_comparison [has_terminal D]
+def preserves_terminal.of_iso_comparison
   [i : is_iso (terminal_comparison G)] : preserves_limit (functor.empty C) G :=
 begin
   apply preserves_limit_of_preserves_limit_cone terminal_is_terminal,
-  apply (terminal_map_cone_limit _ _).symm _,
+  apply (is_limit_map_cone_empty_cone_equiv _ _).symm _,
   apply is_limit.of_point_iso (limit.is_limit (functor.empty D)),
   apply i,
 end
 
 /-- If there is any isomorphism `G.obj ⊤ ⟶ ⊤`, then `G` preserves terminal objects. -/
-def preserves_terminal_of_is_iso [has_terminal D]
+def preserves_terminal_of_is_iso
   (f : G.obj (⊤_ C) ⟶ ⊤_ D) [i : is_iso f] : preserves_limit (functor.empty C) G :=
 begin
   rw subsingleton.elim f (terminal_comparison G) at i,
-  exactI preserves_terminal_of_iso_comparison G,
+  exactI preserves_terminal.of_iso_comparison G,
 end
 
 /-- If there is any isomorphism `G.obj ⊤ ≅ ⊤`, then `G` preserves terminal objects. -/
-def preserves_terminal_of_iso [has_terminal D]
+def preserves_terminal_of_iso
   (f : G.obj (⊤_ C) ≅ ⊤_ D) : preserves_limit (functor.empty C) G :=
 preserves_terminal_of_is_iso G f.hom
 
-variables [has_terminal D] [preserves_limit (functor.empty C) G]
+variables [preserves_limit (functor.empty C) G]
 
 /-- If `G` preserves terminal objects, then the terminal comparison map for `G` an isomorphism. -/
-def iso : G.obj (⊤_ C) ≅ ⊤_ D :=
+def preserves_terminal.iso : G.obj (⊤_ C) ≅ ⊤_ D :=
 (is_limit_of_has_terminal_of_preserves_limit G).cone_point_unique_up_to_iso (limit.is_limit _)
 
 @[simp]
-lemma iso_hom : (preserves_terminal.iso G).hom = terminal_comparison G :=
+lemma preserves_terminal.iso_hom : (preserves_terminal.iso G).hom = terminal_comparison G :=
 rfl
 
 instance : is_iso (terminal_comparison G) :=
@@ -114,5 +115,3 @@ begin
 end
 
 end category_theory.limits
-
-end category_theory

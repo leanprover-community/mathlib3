c755bfbaf5427ea548007f531298466f346f790e
= <shingtaklam1324@gmail.com>
2020-12-02 09:48:41 +0000

    change to fact 0 < n

--- src/group_theory/dihedral.lean
+++ src/group_theory/dihedral.lean
@@ -103,16 +103,16 @@ private def fintype_helper : (zmod n ⊕ zmod n) ≃ dihedral n :=
 /--
 If `0 < n`, then `dihedral n` is a finite group
 -/
-instance : fintype (dihedral n.succ) := fintype.of_equiv _ fintype_helper
+instance [fact (0 < n)] : fintype (dihedral n) := fintype.of_equiv _ fintype_helper
 
 /--
 If `0 < n`, then `dihedral n` has `2n` elements.
 -/
-lemma card : fintype.card (dihedral n.succ) = 2 * n.succ :=
+lemma card [fact (0 < n)] : fintype.card (dihedral n) = 2 * n :=
 begin
   rw ←fintype.card_eq.mpr ⟨fintype_helper⟩,
-  change fintype.card (fin n.succ ⊕ fin n.succ) = 2 * n.succ,
-  rw [fintype.card_sum, fintype.card_fin, two_mul]
+  change fintype.card (zmod n ⊕ zmod n) = 2 * n,
+  rw [fintype.card_sum, zmod.card, two_mul]
 end
 
 lemma r_one_pow (k : ℕ) : (r 1 : dihedral n) ^ k = r k :=
@@ -139,7 +139,7 @@ lemma sr_mul_self (i : zmod n) : sr i * sr i = 1 := by rw [sr_mul_sr, sub_self, 
 /--
 If `0 < n`, then `sr i` has order 2.
 -/
-lemma order_of_sr (i : zmod n.succ) : order_of (sr i) = 2 :=
+lemma order_of_sr [fact (0 < n)] (i : zmod n) : order_of (sr i) = 2 :=
 begin
   rw order_of_eq_prime _ _,
   { exact nat.prime_two },
@@ -150,16 +150,15 @@ end
 /--
 If `0 < n`, then `(r 1)` has order `n`.
 -/
-lemma order_of_r_one : order_of (r 1 : dihedral n.succ) = n.succ :=
+lemma order_of_r_one [hnpos : fact (0 < n)] : order_of (r 1 : dihedral n) = n :=
 begin
-  cases lt_or_eq_of_le (nat.le_of_dvd (nat.succ_pos _)
-        (order_of_dvd_of_pow_eq_one (@r_one_pow_n n.succ))) with h h,
-  { have h1 : (r 1 : dihedral n.succ)^(order_of (r 1)) = 1,
+  cases lt_or_eq_of_le (nat.le_of_dvd hnpos (order_of_dvd_of_pow_eq_one (@r_one_pow_n n))) with h h,
+  { have h1 : (r 1 : dihedral n)^(order_of (r 1)) = 1,
     { exact pow_order_of_eq_one _ },
     rw r_one_pow at h1,
     injection h1 with h2,
     rw [←zmod.val_eq_zero, zmod.val_cast_nat, nat.mod_eq_of_lt h] at h2,
-    have := order_of_pos (r 1 : dihedral n.succ),
+    have := order_of_pos (r 1 : dihedral n),
     rw h2 at this,
     cases this },
   { exact h }
@@ -168,7 +167,7 @@ end
 /--
 If `0 < n`, and `i : zmod n` has order `n / gcd n i`
 -/
-lemma order_of_r (i : zmod n.succ) : order_of (r i) = n.succ / nat.gcd n.succ i.val :=
+lemma order_of_r [fact (0 < n)] (i : zmod n) : order_of (r i) = n / nat.gcd n i.val :=
 begin
   conv_lhs { rw ←zmod.cast_val i },
   rw [←r_one_pow, order_of_pow, order_of_r_one]

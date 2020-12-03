491a9ca84d99f6b67b78e0ec2c0829fd0f23e5d8
Oliver Nash <oliver.nash@gmail.com>
2020-12-03 20:55:57 +0000

    Update src/algebra/lie/basic.lean
    Co-authored-by: Johan Commelin <johan@commelin.net>

--- src/algebra/lie/basic.lean
+++ src/algebra/lie/basic.lean
@@ -862,7 +862,7 @@ def hom_of_le : N →ₗ⁅R,L⁆ N' :=
 
 @[simp] lemma coe_hom_of_le (m : N) : (hom_of_le h m : M) = m := rfl
 
-lemma of_le_apply (m : N) : hom_of_le h m = ⟨m.1, h m.2⟩ := rfl
+lemma hom_of_le_apply (m : N) : hom_of_le h m = ⟨m.1, h m.2⟩ := rfl
 
 end inclusion_maps
 

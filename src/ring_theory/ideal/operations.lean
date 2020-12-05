426696b784261ffed20748134881a51edb1d259f
Devon Tuma <tumax040@umn.edu>
2020-12-05 17:53:26 -0600

    feat(ring_theory/*): various lemmas about quotients, localizations, and polynomials

--- src/ring_theory/ideal/operations.lean
+++ src/ring_theory/ideal/operations.lean
@@ -1145,6 +1145,10 @@ lemma quotient_map_mk {J : ideal R} {I : ideal S} {f : R →+* S} {H : J ≤ I.c
   {x : R} : quotient_map I f H (quotient.mk J x) = quotient.mk I (f x) :=
 quotient.lift_mk J _ _
 
+lemma quotient_map_comp_mk {J : ideal R} {I : ideal S} {f : R →+* S} {H : J ≤ I.comap f} :
+  (quotient_map I f H).comp (quotient.mk J) = (quotient.mk I).comp f :=
+ring_hom.ext (λ x, by simp only [function.comp_app, ring_hom.coe_comp, ideal.quotient_map_mk])
+
 /-- If we take `J = I.comap f` then `quotient_map` is injective -/
 lemma quotient_map_injective {I : ideal S} {f : R →+* S} :
   function.injective (quotient_map I f le_rfl) :=
@@ -1154,6 +1158,11 @@ begin
   rw quotient_map_mk at ha,
   rwa quotient.eq_zero_iff_mem at ha ⊢
 end
+
+lemma quotient_map_surjective {J : ideal R} {I : ideal S} {f : R →+* S} {H : J ≤ I.comap f}
+  (hf : function.surjective f) : function.surjective (quotient_map I f H) :=
+λ x, let ⟨x, hx⟩ := quotient.mk_surjective x in
+  let ⟨y, hy⟩ := hf x in ⟨(quotient.mk J) y, by simp [hx, hy]⟩
 
 /-- Commutativity of a square is preserved when taking quotients by an ideal -/
 lemma comp_quotient_map_eq_of_comp_eq {R' S' : Type*} [comm_ring R'] [comm_ring S']

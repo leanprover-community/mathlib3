d4a43345bd9bff18c35519569bdd603914e604d0
agusakov <39916842+agusakov@users.noreply.github.com>
2020-12-05 13:57:05 -0500

    Update src/combinatorics/simple_graph/basic.lean
    Co-authored-by: Rob Lewis <Rob.y.lewis@gmail.com>

--- src/combinatorics/simple_graph/basic.lean
+++ src/combinatorics/simple_graph/basic.lean
@@ -96,11 +96,8 @@ incident to `v`.
 lemma adj_iff_exists_edge {v w : V} :
   G.adj v w ↔ v ≠ w ∧ ∃ (e ∈ G.edge_set), v ∈ e ∧ w ∈ e :=
 begin
-  split,
-  { intro,
-    split,
-    { exact G.ne_of_adj ‹_› },
-    { use ⟦(v,w)⟧, simpa } },
+  refine ⟨λ _, ⟨G.ne_of_adj ‹_›, ⟦(v,w)⟧, _⟩, _⟩,
+  { simpa },
   { rintro ⟨hne, e, he, hv⟩,
     rw sym2.elems_iff_eq hne at hv,
     subst e,

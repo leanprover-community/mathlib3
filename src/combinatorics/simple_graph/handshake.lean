f8e7f435f2f9d5852c638252d435217b8c7f7630
Kyle Miller <kmill31415@gmail.com>
2020-12-06 17:42:19 -0800

    fixed copyright line (had copied from basic.lean)

--- src/combinatorics/simple_graph/handshake.lean
+++ src/combinatorics/simple_graph/handshake.lean
@@ -1,5 +1,5 @@
 /-
-Copyright (c) 2020 Aaron Anderson, Jalex Stark, Kyle Miller. All rights reserved.
+Copyright (c) 2020 Kyle Miller. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Author: Kyle Miller.
 -/

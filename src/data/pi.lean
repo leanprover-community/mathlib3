2916a9fb2fcb6ef044279c8eaf9ef20e4ff04a18
Eric Wieser <wieser.eric@gmail.com>
2020-12-08 11:31:19 +0000

    chore(algebra/group/pi): Split into multiple files
    This allows files that appear before `ordered_group` to still use `pi.monoid` etc.

--- src/data/pi.lean
+++ src/data/pi.lean
@@ -5,6 +5,7 @@ Authors: Simon Hudon, Patrick Massot, Eric Wieser
 -/
 import tactic.split_ifs
 import tactic.simpa
+import algebra.group.to_additive
 /-!
 # Theorems on pi types
 
@@ -17,6 +18,23 @@ variable {f : I → Type v} -- The family of types already equipped with instanc
 variables (x y : Π i, f i) (i : I)
 
 namespace pi
+
+/-- `1`, `0`, `+`, `*`, `-`, `⁻¹`, and `/` are defined pointwise. -/
+
+@[to_additive] instance has_one [∀ i, has_one $ f i] : has_one (Π i : I, f i) := ⟨λ _, 1⟩
+@[simp, to_additive] lemma one_apply [∀ i, has_one $ f i] : (1 : Π i, f i) i = 1 := rfl
+
+@[to_additive]
+instance has_mul [∀ i, has_mul $ f i] : has_mul (Π i : I, f i) := ⟨λ f g i, f i * g i⟩
+@[simp, to_additive] lemma mul_apply [∀ i, has_mul $ f i] : (x * y) i = x i * y i := rfl
+
+@[to_additive] instance has_inv [∀ i, has_inv $ f i] : has_inv (Π i : I, f i) := ⟨λ f i, (f i)⁻¹⟩
+@[simp, to_additive] lemma inv_apply [∀ i, has_inv $ f i] : x⁻¹ i = (x i)⁻¹ := rfl
+
+instance has_div [Π i, has_div $ f i] : has_div (Π i : I, f i) := ⟨λ f g i, f i / g i⟩
+@[simp] lemma div_apply [Π i, has_div $ f i] : (x / y) i = x i / y i := rfl
+
+section
 
 variables [decidable_eq I]
 variables [Π i, has_zero (f i)]
@@ -48,4 +66,5 @@ variables (f)
 lemma single_injective (i : I) : function.injective (single i : f i → Π i, f i) :=
 λ x y h, by simpa only [single, dif_pos] using congr_fun h i
 
+end
 end pi

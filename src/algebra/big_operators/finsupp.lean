9f3a816cc9bd346032ff80dc1ebae56ea61d709e
Johan Commelin <johan@commelin.net>
2020-12-10 13:49:26 +0000

    rename variables, and move to top of file

--- src/algebra/big_operators/finsupp.lean
+++ src/algebra/big_operators/finsupp.lean
@@ -13,24 +13,19 @@ import data.finsupp
 This file contains theorems relevant to big operators in finitely supported functions.
 -/
 
-universes u v w u₁ v₁ w₁
-
 open_locale big_operators
 
-variables {α : Type u} {ι : Type v} {β : Type w} [add_comm_monoid β]
-variables {s : finset α} {f : α → (ι →₀ β)} (i : ι)
+variables {α ι γ A B C : Type*} [add_comm_monoid A] [add_comm_monoid B] [add_comm_monoid C]
+variables {t : ι → A → C} (h0 : ∀ i, t i 0 = 0) (h1 : ∀ i x y, t i (x + y) = t i x + t i y)
+variables {s : finset α} {f : α → (ι →₀ A)} (i : ι)
+variables (g : ι →₀ A) (k : ι → A → γ → B) (x : γ)
 
 theorem finset.sum_apply' : (∑ k in s, f k) i = ∑ k in s, f k i :=
 (s.sum_hom $ finsupp.apply_add_hom i).symm
 
-variables {γ : Type u₁} {δ : Type v₁} [add_comm_monoid δ]
-variables (g : ι →₀ β) (k : ι → β → γ → δ) (x : γ)
-
 theorem finsupp.sum_apply' : g.sum k x = g.sum (λ i b, k i b x) :=
 finset.sum_apply _ _ _
 
-variables {ε : Type w₁} [add_comm_monoid ε]
-variables {t : ι → β → ε} (h0 : ∀ i, t i 0 = 0) (h1 : ∀ i x y, t i (x + y) = t i x + t i y)
 include h0 h1
 
 open_locale classical

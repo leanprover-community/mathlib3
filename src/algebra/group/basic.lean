3afdf41fbbe6eb77bce52ea733d1386184552aed
Yury G. Kudryashov <urkud@urkud.name>
2020-12-12 09:03:27 +0000

    chore(*): generalize some lemmas from `linear_ordered_semiring` to `ordered_semiring` (#5327)
    API changes:
    
    * Many lemmas now have weaker typeclass assumptions. Sometimes this means that `@myname _ _ _` needs one more `_`.
    * Drop `eq_one_of_mul_self_left_cancel` etc in favor of the new `mul_eq_left_iff` etc.
    * A few new lemmas that state `monotone` or `strict_mono_incr_on`.

--- src/algebra/group/basic.lean
+++ src/algebra/group/basic.lean
@@ -107,25 +107,27 @@ end comm_monoid
 
 section left_cancel_monoid
 
-variables {M : Type u} [left_cancel_monoid M]
+variables {M : Type u} [left_cancel_monoid M] {a b : M}
 
-@[to_additive] lemma eq_one_of_mul_self_left_cancel {a : M} (h : a * a = a) : a = 1 :=
-mul_left_cancel (show a * a = a * 1, by rwa mul_one)
+@[simp, to_additive] lemma mul_eq_left_iff : a * b = a ↔ b = 1 :=
+calc a * b = a ↔ a * b = a * 1 : by rw mul_one
+           ... ↔ b = 1         : mul_left_cancel_iff
 
-@[to_additive] lemma eq_one_of_left_cancel_mul_self {a : M} (h : a = a * a) : a = 1 :=
-mul_left_cancel (show a * a = a * 1, by rwa [mul_one, eq_comm])
+@[simp, to_additive] lemma left_eq_mul_iff : a = a * b ↔ b = 1 :=
+eq_comm.trans mul_eq_left_iff
 
 end left_cancel_monoid
 
 section right_cancel_monoid
 
-variables {M : Type u} [right_cancel_monoid M]
+variables {M : Type u} [right_cancel_monoid M] {a b : M}
 
-@[to_additive] lemma eq_one_of_mul_self_right_cancel {a : M} (h : a * a = a) : a = 1 :=
-mul_right_cancel (show a * a = 1 * a, by rwa one_mul)
+@[simp, to_additive] lemma mul_eq_right_iff : a * b = b ↔ a = 1 :=
+calc a * b = b ↔ a * b = 1 * b : by rw one_mul
+           ... ↔ a = 1         : mul_right_cancel_iff
 
-@[to_additive] lemma eq_one_of_right_cancel_mul_self {a : M} (h : a = a * a) : a = 1 :=
-mul_right_cancel (show a * a = 1 * a, by rwa [one_mul, eq_comm])
+@[simp, to_additive] lemma right_eq_mul_iff : b = a * b ↔ a = 1 :=
+eq_comm.trans mul_eq_right_iff
 
 end right_cancel_monoid
 

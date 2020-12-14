f8bf53c2738ffe786c4d1e34b5d2c4cf59558630
Yury G. Kudryashov <urkud@urkud.name>
2020-12-13 19:06:33 -0500

    Fix

--- src/data/real/irrational.lean
+++ src/data/real/irrational.lean
@@ -3,7 +3,7 @@ Copyright (c) 2018 Mario Carneiro. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Mario Carneiro, Abhimanyu Pallavi Sudhir, Jean Lo, Calle Sönne, Yury Kudryashov.
 -/
-import data.real.basic
+import data.real.sqrt
 import data.rat.sqrt
 import ring_theory.int.basic
 import data.polynomial.eval

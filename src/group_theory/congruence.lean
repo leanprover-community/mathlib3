a21f7a11e33c6ae85fd12045a1a687cae8833c36
Vierkantor <vierkantor@vierkantor.com>
2020-12-16 10:35:20 +0000

    Fix overlong line
    (I could have sworn the line was just short enough; I cannot count apparently)

--- src/group_theory/congruence.lean
+++ src/group_theory/congruence.lean
@@ -871,7 +871,8 @@ instance has_inv : has_inv c.quotient :=
 an `add_group`."]
 instance group : group c.quotient :=
 { inv := λ x, x⁻¹,
-  mul_left_inv := λ x, show x⁻¹ * x = 1, from quotient.induction_on' x $ λ _, congr_arg coe $ mul_left_inv _,
+  mul_left_inv := λ x, show x⁻¹ * x = 1,
+    from quotient.induction_on' x $ λ _, congr_arg coe $ mul_left_inv _,
   .. con.monoid c}
 
 end groups

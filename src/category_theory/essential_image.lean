2c461a23ef913f9ec341f4f013d26577d9be7197
Bhavik Mehta <bhavikmehta8@gmail.com>
2020-12-17 19:19:43 +0000

    coe

--- src/category_theory/essential_image.lean
+++ src/category_theory/essential_image.lean
@@ -65,13 +65,11 @@ set.ext $ λ A, ⟨ess_image.of_nat_iso h, ess_image.of_nat_iso h.symm⟩
 /-- An object in the image is in the essential image. -/
 lemma obj_mem_ess_image (F : D ⥤ C) (Y : D) : F.obj Y ∈ ess_image F := ⟨Y, ⟨iso.refl _⟩⟩
 
-/-- The essential image of the functor `F` as an explicit subcategory of `D`. -/
-@[derive category]
-def essential_image (F : D ⥤ C) := {D // D ∈ F.ess_image}
+instance : category F.ess_image := category_theory.full_subcategory _
 
 /-- The essential image as a subcategory has a fully faithful inclusion into the target category. -/
 @[derive [full, faithful], simps {rhs_md := semireducible}]
-def essential_image_inclusion (F : C ⥤ D) : F.essential_image ⥤ D :=
+def ess_image_inclusion (F : C ⥤ D) : F.ess_image ⥤ D :=
 full_subcategory_inclusion _
 
 /--
@@ -79,17 +77,17 @@ Given a functor `F : C ⥤ D`, we have an (essentially surjective) functor from 
 image of `F`.
 -/
 @[simps]
-def to_essential_image (F : C ⥤ D) : C ⥤ F.essential_image :=
+def to_ess_image (F : C ⥤ D) : C ⥤ F.ess_image :=
 { obj := λ X, ⟨_, obj_mem_ess_image _ X⟩,
-  map := λ X Y f, (essential_image_inclusion F).preimage (F.map f) }
+  map := λ X Y f, (ess_image_inclusion F).preimage (F.map f) }
 
 /--
 The functor `F` factorises through its essential image, where the first functor is essentially
 surjective and the second is fully faithful.
 -/
 @[simps {rhs_md := semireducible}]
-def to_essential_image_comp_essential_image_inclusion :
-  F.to_essential_image ⋙ F.essential_image_inclusion ≅ F :=
+def to_ess_image_comp_essential_image_inclusion :
+  F.to_ess_image ⋙ F.ess_image_inclusion ≅ F :=
 nat_iso.of_components (λ X, iso.refl _) (by tidy)
 
 end functor
@@ -103,7 +101,7 @@ See https://stacks.math.columbia.edu/tag/001C.
 class ess_surj (F : C ⥤ D) : Prop :=
 (mem_ess_image [] (Y : D) : Y ∈ F.ess_image)
 
-instance : ess_surj F.to_essential_image :=
+instance : ess_surj F.to_ess_image :=
 { mem_ess_image := λ ⟨Y, hY⟩, ⟨_, ⟨⟨_, _, hY.get_iso.hom_inv_id, hY.get_iso.inv_hom_id⟩⟩⟩ }
 
 variables (F) [ess_surj F]

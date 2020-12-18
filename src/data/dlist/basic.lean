e77c58a0fe0d1a6c090cb30c40536aab4ff666d3
Yakov Pechersky <ffxen158@gmail.com>
2020-12-18 17:58:16 -0500

    feat(data/dlist/basic): dlist_singleton lemma

--- src/data/dlist/basic.lean
+++ src/data/dlist/basic.lean
@@ -10,3 +10,6 @@ difference list.  Similar to `list.join`. -/
 def dlist.join {α : Type*} : list (dlist α) → dlist α
  | [] := dlist.empty
  | (x :: xs) := x ++ dlist.join xs
+
+@[simp] lemma dlist_singleton {α : Type*} {a : α} :
+  dlist.singleton a = dlist.lazy_of_list ([a]) := rfl

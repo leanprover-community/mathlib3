a417178a1d2db0eb2c668e63e675d369dd2694ef
Kevin Buzzard <k.buzzard@imperial.ac.uk>
2020-12-21 12:48:27 +0000

    add attributes which aren't being added

--- src/algebra/ordered_monoid.lean
+++ src/algebra/ordered_monoid.lean
@@ -212,9 +212,14 @@ preorder.lift (coe : units α → α)
 theorem coe_le_coe [monoid α] [preorder α] {a b : units α} :
   (a : α) ≤ b ↔ a ≤ b := iff.rfl
 
+-- should `to_additive` do this?
+attribute [norm_cast] add_units.coe_le_coe
+
 @[simp, norm_cast, to_additive]
 theorem coe_lt_coe [monoid α] [preorder α] {a b : units α} :
   (a : α) < b ↔ a < b := iff.rfl
+
+attribute [norm_cast] add_units.coe_lt_coe
 
 @[to_additive]
 instance [monoid α] [partial_order α] : partial_order (units α) :=
@@ -229,10 +234,14 @@ theorem max_coe [monoid α] [linear_order α] {a b : units α} :
   (↑(max a b) : α) = max a b :=
 by by_cases b ≤ a; simp [max, h]
 
+attribute [norm_cast] add_units.max_coe
+
 @[simp, norm_cast, to_additive]
 theorem min_coe [monoid α] [linear_order α] {a b : units α} :
   (↑(min a b) : α) = min a b :=
 by by_cases a ≤ b; simp [min, h]
+
+attribute [norm_cast] add_units.min_coe
 
 end units
 

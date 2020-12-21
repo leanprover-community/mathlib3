6fb3dee82e6064ef70e0413a4ecdfe0c47db66dd
Bhavik Mehta <bhavikmehta8@gmail.com>
2020-12-21 17:36:11 +0000

    still trying to show flat functors preserve finite limits

--- src/category_theory/flat.lean
+++ src/category_theory/flat.lean
@@ -9,12 +9,11 @@ import category_theory.filtered
 import category_theory.limits.shapes.finite_limits
 import category_theory.limits.shapes.types
 import category_theory.limits.presheaf
-import category_theory.limits.filtered_colimit_commutes_finite_limit2
+import category_theory.limits.filtered_colimit_commutes_finite_limit
 import category_theory.limits.preserves.limits
 import category_theory.limits.preserves.shapes.terminal
 import category_theory.limits.preserves.shapes.binary_products
 import category_theory.limits.preserves.shapes.equalizers
-import category_theory.hom_functor
 
 /-!
 # Flat functors
@@ -162,41 +161,35 @@ def alt_colimit (F : C ⥤ Type v₁) :
     simp,
   end }
 
-instance : has_colimit (my_functor F) :=
-begin
-end
+-- def preserves_finite_limits_of_set_flat {C : Type u₁} [category.{v₁} C]
+--   [has_finite_limits C] (F : C ⥤ Type v₁)
+-- [is_set_flat F] (J : Type v₁) [small_category J] [fin_category J] :
+-- preserves_limits_of_shape J F :=
+-- { preserves_limit := λ K,
+--   { preserves := λ c t,
+--     begin
+--       let Γ : F.elementsᵒᵖ ⥤ J ⥤ Type v₁ :=
+--         (category_of_elements.π F).op ⋙ coyoneda ⋙ (whiskering_left J C _).obj K,
+--       -- have := alt_colimit F,
+--       have := F.elementsᵒᵖ,
+--       have := @filtered_colimit_finite_limit_iso _ _ _ _ Γ,
+--       -- let Γ : F.elementsᵒᵖ × J ⥤ Type v₁ := (category_of_elements.π F).op.prod K ⋙ functor.hom C,
 
-set_option pp.universes true
+--       -- apply functor.prod _ _ ⋙ functor.hom C,
+--       -- have : has_colimits_of_shape (F.elementsᵒᵖ) (Type v₁),
 
-def preserves_finite_limits_of_set_flat {C : Type u₁} [category.{v₁} C]
-  [has_finite_limits C] (F : C ⥤ Type v₁)
-[is_set_flat F] (J : Type v₁) [small_category J] [fin_category J] :
-preserves_limits_of_shape J F :=
-{ preserves_limit := λ K,
-  { preserves := λ c t,
-    begin
-      let Γ : F.elementsᵒᵖ ⥤ J ⥤ Type v₁ :=
-        (category_of_elements.π F).op ⋙ coyoneda ⋙ (whiskering_left J C _).obj K,
-      -- have := alt_colimit F,
-      have := F.elementsᵒᵖ,
-      have := @filtered_colimit_finite_limit_iso _ _ _ _ Γ,
-      -- let Γ : F.elementsᵒᵖ × J ⥤ Type v₁ := (category_of_elements.π F).op.prod K ⋙ functor.hom C,
+--       -- have := alt_colimit F,
+--       -- have := colimit (my_functor F),
+--       -- have := category_theory.limits.functor_category_has_colimits_of_shape,
 
-      -- apply functor.prod _ _ ⋙ functor.hom C,
-      -- have : has_colimits_of_shape (F.elementsᵒᵖ) (Type v₁),
+--       -- let X := limit (K ⋙ F),
+--       -- let θ := functor.op (category_of_elements.π F),
+--       -- have := colimit_limit_to_limit_colimit,
 
-      -- have := alt_colimit F,
-      -- have := colimit (my_functor F),
-      -- have := category_theory.limits.functor_category_has_colimits_of_shape,
-
-      -- let X := limit (K ⋙ F),
-      -- let θ := functor.op (category_of_elements.π F),
-      -- have := colimit_limit_to_limit_colimit,
-
-      -- let F' := (op_op_equivalence C).functor ⋙ F,
-      -- have := colimit_of_representable F',
-      -- have := colimit_of_representable,
-    end } }
+--       -- let F' := (op_op_equivalence C).functor ⋙ F,
+--       -- have := colimit_of_representable F',
+--       -- have := colimit_of_representable,
+--     end } }
 
 -- def is_set_flat_of_is_flat (F : C ⥤ Type w) [is_flat F] : is_set_flat F :=
 -- begin

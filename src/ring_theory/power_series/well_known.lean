96273c716f28e1ac04fc27b95d3247855ca33215
Yury G. Kudryashov <urkud@urkud.name>
2020-12-21 23:13:02 -0500

    Rename files

--- src/ring_theory/power_series/well_known.lean
+++ src/ring_theory/power_series/well_known.lean
@@ -3,7 +3,7 @@ Copyright (c) 2020 Yury G. Kudryashov. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Author: Yury G. Kudryashov
 -/
-import ring_theory.power_series
+import ring_theory.power_series.basic
 import data.nat.parity
 
 /-!

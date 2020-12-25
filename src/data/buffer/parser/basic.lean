a0438dd5c7bc24c78cfc580d09bbc46efc71e9a6
Yakov Pechersky <ffxen158@gmail.com>
2020-12-25 13:39:53 -0500

    simplify proofs

--- src/data/buffer/parser/basic.lean
+++ src/data/buffer/parser/basic.lean
@@ -57,39 +57,21 @@ def valid : Prop :=
 lemma fail_iff :
   (∀ pos' result, p cb n ≠ done pos' result) ↔
     ∃ (pos' : ℕ) (err : dlist string), p cb n = fail pos' err :=
-begin
-  cases p cb n,
-  { simp only [not_not, exists_false, exists_eq_right', iff_false, exists_eq', not_forall] },
-  { simp only [forall_const, ne.def, not_false_iff, exists_eq_right', exists_eq', forall_true_iff] }
-end
+by cases p cb n; simp
 
 lemma success_iff :
   (∀ pos' err, p cb n ≠ fail pos' err) ↔ ∃ (pos' : ℕ) (result : α), p cb n = done pos' result :=
-begin
-  cases p cb n,
-  { simp only [exists_eq_right', exists_eq', forall_const, ne.def, not_false_iff,
-               forall_true_iff] },
-  { simp only [exists_eq_right', exists_eq', exists_prop, not_not, exists_false, iff_false,
-               not_forall] }
-end
+by cases p cb n; simp
 
 variables {p q cb n n' msgs msg}
 
 lemma decorate_errors_fail (h : p cb n = fail n' err) :
   @decorate_errors α msgs p cb n = fail n ((dlist.lazy_of_list (msgs ()))) :=
-begin
-  dunfold decorate_errors,
-  rw h,
-  refl
-end
+by simp [decorate_errors, h]
 
 lemma decorate_errors_success (h : p cb n = done n' a) :
   @decorate_errors α msgs p cb n = done n' a :=
-begin
-  dunfold decorate_errors,
-  rw h,
-  refl
-end
+by simp [decorate_errors, h]
 
 lemma decorate_error_fail (h : p cb n = fail n' err) :
   @decorate_error α msg p cb n = fail n ((dlist.lazy_of_list ([msg ()]))) :=
@@ -101,13 +83,7 @@ decorate_errors_success h
 
 @[simp] lemma decorate_errors_success_iff :
   @decorate_errors α msgs p cb n = done n' a ↔ p cb n = done n' a :=
-begin
-  dunfold decorate_errors,
-  cases p cb n,
-  { rw decorate_errors },
-  { rw decorate_errors,
-    simp only }
-end
+by cases h : p cb n; simp [decorate_errors, h]
 
 @[simp] lemma decorate_error_success_iff :
   @decorate_error α msg p cb n = done n' a ↔ p cb n = done n' a :=
@@ -116,14 +92,7 @@ decorate_errors_success_iff
 @[simp] lemma decorate_errors_failure_iff :
   @decorate_errors α msgs p cb n = fail n err ↔
     err = dlist.lazy_of_list (msgs ()) ∧ ∃ np err', p cb n = fail np err' :=
-begin
-  dunfold decorate_errors,
-  cases p cb n,
-  { rw decorate_errors,
-    simp only [exists_false, and_false] },
-  { rw decorate_errors,
-    simp only [true_and, and_true, eq_self_iff_true, exists_eq_right', exists_eq', eq_comm] }
-end
+by cases h : p cb n; simp [decorate_errors, h, eq_comm]
 
 @[simp] lemma decorate_error_failure_iff :
   @decorate_error α msg p cb n = fail n err ↔
@@ -136,57 +105,32 @@ decorate_errors_failure_iff
 
 section bind
 
-variable {f : α → parser β}
+variable (f : α → parser β)
 
 @[simp] lemma bind_eq_bind : p.bind f = p >>= f := rfl
+
+variable {f}
 
 @[simp] lemma bind_eq_done :
   (p >>= f) cb n = done n' b ↔
   ∃ (np : ℕ) (a : α), p cb n = done np a ∧ f a cb np = done n' b :=
-begin
-  split,
-  { intro h,
-    simp_rw [←bind_eq_bind, parser.bind] at h,
-    cases p cb n with pos r pos err,
-    { refine ⟨pos, r, rfl, _⟩,
-      rw ←h,
-      refl },
-    { simpa only using h } },
-  { rintro ⟨r, pos, h⟩,
-    simp only [h, ←bind_eq_bind, parser.bind, eq_self_iff_true, and_self] }
-end
+by cases hp : p cb n; simp [hp, ←bind_eq_bind, parser.bind, and_assoc]
 
 @[simp] lemma bind_eq_fail :
   (p >>= f) cb n = fail n' err ↔
-  (p cb n = fail n' err) ∨ (∃ (np : ℕ) (a : α), p cb n = done np a ∧ f a cb np = fail n' err)
-  :=
-begin
-  split,
-  { intro h,
-    simp_rw [←bind_eq_bind, parser.bind] at h,
-    cases p cb n with pos r pos err,
-    { rw ←h,
-      simp only [false_or, exists_false],
-      refine ⟨pos, r, by simp only [eq_self_iff_true, and_self], _⟩,
-      refl },
-    { simpa only [parser.bind, exists_false, or_false, false_and] using h } },
-  { rintro (h | ⟨r, n', h, h'⟩),
-    { simp only [←bind_eq_bind, parser.bind, h, eq_self_iff_true, and_self] },
-    { simp only [←h', ←bind_eq_bind, parser.bind, h] } }
-end
+  (p cb n = fail n' err) ∨ (∃ (np : ℕ) (a : α), p cb n = done np a ∧ f a cb np = fail n' err) :=
+by cases hp : p cb n; simp [hp, ←bind_eq_bind, parser.bind, and_assoc]
 
 @[simp] lemma and_then_eq_bind {α β : Type} {m : Type → Type} [monad m] (a : m α) (b : m β) :
   a >> b = a >>= (λ _, b) := rfl
 
 lemma and_then_fail :
   (p >> return ()) cb n = parse_result.fail n' err ↔ p cb n = fail n' err :=
-by simp only [and_then_eq_bind, return_eq_pure, exists_false, or_false, pure_eq_done,
-              bind_eq_fail, and_false]
+by simp
 
 lemma and_then_success :
   (p >> return ()) cb n = parse_result.done n' () ↔ ∃ a, p cb n = done n' a:=
-by simp only [pure_eq_done, and_then_eq_bind, and_true, bind_eq_done, exists_eq_right,
-              eq_iff_true_of_subsingleton, return_eq_pure, exists_and_distrib_right]
+by simp
 
 end bind
 
@@ -196,23 +140,14 @@ variable {f : α → β}
 
 @[simp] lemma map_eq_done : (f <$> p) cb n = done n' b ↔
   ∃ (a : α), p cb n = done n' a ∧ f a = b :=
-begin
-  rw ←is_lawful_monad.bind_pure_comp_eq_map,
-  simp only [bind_eq_done, function.comp_app, pure_eq_done],
-  split,
-  { rintro ⟨np, a, h, rfl, rfl⟩,
-    exact ⟨a, h, rfl⟩ },
-  { rintro ⟨a, h, rfl⟩,
-    exact ⟨n', a, h, rfl, rfl⟩ }
-end
+by cases hp : p cb n; simp [←is_lawful_monad.bind_pure_comp_eq_map, hp, and_assoc]
 
 @[simp] lemma map_eq_fail : (f <$> p) cb n = fail n' err ↔ p cb n = fail n' err :=
-by simp only [←bind_pure_comp_eq_map, exists_false, function.comp_app, or_false, pure_eq_done,
-              bind_eq_fail, and_false]
+by simp [←bind_pure_comp_eq_map]
 
 @[simp] lemma map_const_eq_done {b'} : (b <$ p) cb n = done n' b' ↔
   ∃ (a : α), p cb n = done n' a ∧ b = b' :=
-by simp only [map_const_eq, function.const_apply, iff_self, map_eq_done]
+by simp [map_const_eq]
 
 @[simp] lemma map_const_eq_fail : (b <$ p) cb n = fail n' err ↔ p cb n = fail n' err :=
 by simp only [map_const_eq, map_eq_fail]
@@ -231,29 +166,15 @@ end map
 @[simp] lemma orelse_eq_done : (p <|> q) cb n = done n' a ↔
   (p cb n = done n' a ∨ (q cb n = done n' a ∧ ∃ err, p cb n = fail n err)) :=
 begin
-  rw ←orelse_eq_orelse,
-  split,
-  { intro h,
-    simp_rw parser.orelse at h,
-    cases p cb n with posp resp posp errp,
-    { left,
-      simpa only [parser.orelse] using h },
-    { right,
-      simp only [parser.orelse, ite_not] at h,
-      split_ifs at h with H H,
-      { cases q cb n with posq resq posq errq,
-        { simp only [parser.orelse] at h,
-          simp only [true_and, exists_eq', H, eq_self_iff_true, h] },
-        { simp only [parser.orelse] at h,
-          split_ifs at h;
-          exact h.elim } },
-      { exact h.elim } } },
-  { rintro (h | ⟨h', err, h⟩),
-    { simp only [parser.orelse, h, eq_self_iff_true, and_self] },
-    { simp only [h, parser.orelse, eq_self_iff_true, not_true, if_false, ne.def],
-      cases q cb n with posq resq posq errq,
-      { simpa only [parser.orelse] using h' },
-      { simpa only using h' } } }
+  cases hp : p cb n with np resp np errp,
+  { simp [hp, ←orelse_eq_orelse, parser.orelse] },
+  { by_cases hn : np = n,
+    { cases hq : q cb n with nq resq nq errq,
+      { simp [hp, hn, hq, ←orelse_eq_orelse, parser.orelse] },
+      { rcases lt_trichotomy nq n with H|rfl|H;
+        simp [hp, hn, hq, H, not_lt_of_lt H, lt_irrefl, ←orelse_eq_orelse, parser.orelse] <|>
+          simp [hp, hn, hq, lt_irrefl, ←orelse_eq_orelse, parser.orelse] } },
+    { simp [hp, hn, ←orelse_eq_orelse, parser.orelse] } }
 end
 
 @[simp] lemma orelse_eq_fail_eq : (p <|> q) cb n = fail n err ↔
@@ -261,78 +182,50 @@ end
   (∃ (errp errq), p cb n = fail n errp ∧ q cb n = fail n errq ∧ errp ++ errq = err)
  :=
 begin
-  rw ←orelse_eq_orelse,
-  simp only [parser.orelse, exists_and_distrib_left],
-  cases p cb n with posp resp posp errp,
-  { simp only [parser.orelse, exists_false, false_and, or_self] },
-  by_cases h : posp = n,
-  { simp only [h, parser.orelse, true_and, eq_self_iff_true, not_true, if_false, ne.def,
-               exists_eq_left'],
-    cases q cb n with posq resq posq errq,
-    { simp only [parser.orelse, exists_false, and_false, false_and, or_self] },
-    { simp only [parser.orelse, exists_eq_right'],
-      split_ifs with H H',
-      { simp only [H, true_and, and_true, eq_self_iff_true, ne_of_gt H, exists_false, or_false,
-                   false_and] },
-      { have hn : posq ≠ n := ne_of_lt H',
-        have hl : posq ≤ n := le_of_not_lt H,
-        simp only [hn, hl, false_iff, not_and, not_lt, exists_false, or_false, false_and,
-                   forall_true_iff] },
-      { have : posq = n := le_antisymm (le_of_not_lt H) (le_of_not_lt H'),
-        simp only [this, lt_irrefl, true_and, false_or, eq_self_iff_true, exists_eq_left',
-                   and_false] } } },
-  { simp only [h, parser.orelse, if_true, exists_false, ne.def, not_false_iff, false_and,
-               or_self] },
+  cases hp : p cb n with np resp np errp,
+  { simp [hp, ←orelse_eq_orelse, parser.orelse] },
+  { by_cases hn : np = n,
+    { cases hq : q cb n with nq resq nq errq,
+      { simp [hp, hn, hq, ←orelse_eq_orelse, parser.orelse] },
+      { rcases lt_trichotomy nq n with H|rfl|H;
+        simp [hp, hq, hn, ←orelse_eq_orelse, parser.orelse, H,
+              ne_of_gt H, ne_of_lt H, not_lt_of_lt H] <|>
+          simp [hp, hq, hn, ←orelse_eq_orelse, parser.orelse, lt_irrefl] } },
+    { simp [hp, hn, ←orelse_eq_orelse, parser.orelse] } }
 end
 
 lemma orelse_eq_fail_invalid_lt (hn : n' < n) : (p <|> q) cb n = fail n' err ↔
   (p cb n = fail n' err) ∨
-  (q cb n = fail n' err ∧ (∃ (errp), p cb n = fail n errp))
- :=
+  (q cb n = fail n' err ∧ (∃ (errp), p cb n = fail n errp)) :=
 begin
-  rw ←orelse_eq_orelse,
-  simp only [parser.orelse, exists_and_distrib_left],
-  cases p cb n with posp resp posp errp,
-  { simp only [parser.orelse, exists_false, or_self, and_false] },
-  by_cases h : posp = n,
-  { simp only [h, parser.orelse, true_and, eq_self_iff_true, not_true, if_false, ne.def, and_true,
-               false_or, exists_eq', false_and, ne_of_gt hn],
-    cases q cb n with posq resq posq errq,
-    { simp only [parser.orelse, not_false_iff] },
-    { simp only [parser.orelse],
-      split_ifs with H H',
-      { have : posq ≠ n' := ne_of_gt (lt_trans hn H),
-        simp only [false_and, ne_of_gt hn, this] },
-      { simp only [iff_self] },
-      { have : posq = n := le_antisymm (le_of_not_lt H) (le_of_not_lt H'),
-        simp only [ne_of_gt hn, this, false_and] } } },
-  { simp only [h, parser.orelse, if_true, exists_false, ne.def, not_false_iff, false_and,
-               or_false, and_false] },
+  cases hp : p cb n with np resp np errp,
+  { simp [hp, ←orelse_eq_orelse, parser.orelse] },
+  { by_cases h : np = n,
+    { cases hq : q cb n with nq resq nq errq,
+      { simp [hp, h, hn, hq, ne_of_gt hn, ←orelse_eq_orelse, parser.orelse] },
+      { rcases lt_trichotomy nq n with H|H|H,
+        { simp [hp, hq, h, H, ne_of_gt hn, not_lt_of_lt H, ←orelse_eq_orelse, parser.orelse] },
+        { simp [hp, hq, h, H, ne_of_gt hn, lt_irrefl, ←orelse_eq_orelse, parser.orelse] },
+        { simp [hp, hq, h, H, ne_of_gt (hn.trans H), ←orelse_eq_orelse, parser.orelse] } } },
+    { simp [hp, h, ←orelse_eq_orelse, parser.orelse] } }
 end
 
-lemma orelse_eq_fail_of_valid_ne (hq : q.valid) (hn : n ≠ n') :
-  (p <|> q) cb n = fail n' err ↔ p cb n = fail n' err
- :=
+lemma orelse_eq_fail_of_valid_ne (hv : q.valid) (hn : n ≠ n') :
+  (p <|> q) cb n = fail n' err ↔ p cb n = fail n' err :=
 begin
-  rw ←orelse_eq_orelse,
-  simp only [parser.orelse, exists_and_distrib_left],
-  cases p cb n with posp resp posp errp,
-  { simp [parser.orelse, exists_false, or_self, and_false] },
-  by_cases h : posp = n,
-  { simp only [h, parser.orelse, true_and, eq_self_iff_true, not_true, if_false, ne.def,
-               hn, and_true, false_or, exists_eq', false_and],
-    specialize hq cb n,
-    cases q cb n with posq resq posq errq,
-    { simp [parser.orelse] },
-    { simp only [parser.orelse, if_false],
-      simp only [parse_result.pos] at hq,
-      rcases eq_or_lt_of_le hq.left with rfl|H,
-      { simp only [lt_irrefl, hn, if_false, false_and] },
-      { simp only [H, if_true, hn, false_and] } } },
-  { simp only [h, parser.orelse, if_true, ne.def, not_false_iff] },
+  cases hp : p cb n with np resp np errp,
+  { simp [hp, ←orelse_eq_orelse, parser.orelse] },
+  { by_cases h : np = n,
+    { cases hq : q cb n with nq resq nq errq,
+      { simp [hp, h, hn, hq, hn, ←orelse_eq_orelse, parser.orelse] },
+      { have : n ≤ nq := by { convert (hv cb n).left, simp [hq] },
+        rcases eq_or_lt_of_le this with rfl|H,
+        { simp [hp, hq, h, hn, lt_irrefl, ←orelse_eq_orelse, parser.orelse] },
+        { simp [hp, hq, h, hn, H, ←orelse_eq_orelse, parser.orelse] } } },
+    { simp [hp, h, ←orelse_eq_orelse, parser.orelse] } },
 end
 
-@[simp] lemma parser_eq_parser : @parser.failure α = failure := rfl
+@[simp] lemma failure_eq_failure : @parser.failure α = failure := rfl
 
 @[simp] lemma failure_def : (failure : parser α) cb n = fail n dlist.empty := rfl
 
@@ -349,27 +242,16 @@ by { intros cb n, simp only [pure_eq_done, parse_result.pos, imp_self, and_true]
   (p >>= f).valid :=
 begin
   intros cb n,
-  specialize hp cb n,
   cases hx : (p >>= f) cb n,
   { obtain ⟨n', a, h, h'⟩ := bind_eq_done.mp hx,
-    specialize hf a cb n',
-    simp only [h', parse_result.pos] at hf,
-    simp only [h, parse_result.pos] at hp,
-    simp only [parse_result.pos],
-    split,
-    { exact le_trans hp.left hf.left },
-    { intro hn,
-      simp [hn, hf, hp] } },
+    refine ⟨le_trans (hp cb n).left (h' ▸ _), λ hn, (hp cb n).right _⟩,
+    { simp [h, hf a cb n'] },
+    { simp [h, (hf a cb n').right (h'.symm ▸ hn)] } },
   { obtain h | ⟨n', a, h, h'⟩ := bind_eq_fail.mp hx,
-    { simpa only [h] using hp },
-    { specialize hf a cb n',
-      simp only [h', parse_result.pos] at hf,
-      simp only [h, parse_result.pos] at hp,
-      simp only [parse_result.pos],
-      split,
-      { exact le_trans hp.left hf.left },
-      { intro hn,
-        simp only [hn, hf, hp] } } },
+    { simpa only [h] using hp cb n },
+    { refine ⟨le_trans (hp cb n).left (h' ▸ _), λ hn, (hp cb n).right _⟩,
+      { simp [h, hf a cb n'] },
+      { simp [h, (hf a cb n').right (h'.symm ▸ hn)] } } }
 end
 
 lemma and_then {q : parser β} (hp : p.valid) (hq : q.valid) : (p >> q).valid :=
@@ -395,16 +277,11 @@ end
 begin
   induction l with hd tl hl generalizing h,
   { exact pure },
-  { refine and_then (h _ (list.mem_cons_self _ _))
-      (hl (λ _ ha, h _ (list.mem_cons_of_mem _ ha))) }
+  { exact and_then (h _ (list.mem_cons_self _ _)) (hl (λ _ ha, h _ (list.mem_cons_of_mem _ ha))) }
 end
 
 @[simp] lemma failure : @parser.valid α failure :=
-begin
-  simp only [failure_def, valid, imp_self, and_true, parse_result.pos],
-  rintro - _,
-  refl
-end
+by simp [valid, le_refl]
 
 @[simp] lemma guard {p : Prop} [decidable p] : valid (guard p) :=
 by simp only [guard, apply_ite valid, pure, failure, or_true, if_true_left_eq_or]
@@ -413,25 +290,21 @@ by simp only [guard, apply_ite valid, pure, failure, or_true, if_true_left_eq_or
 begin
   intros cb n,
   cases hx : (p <|> q) cb n with posx resx posx errx,
-  { obtain h | ⟨h', err, h⟩ := orelse_eq_done.mp hx,
-    { simpa only [h] using hp cb n },
-    { simpa only [h'] using hq cb n } },
+  { obtain h | ⟨h, -, -⟩ := orelse_eq_done.mp hx,
+    { simpa [h] using hp cb n },
+    { simpa [h] using hq cb n } },
   { by_cases h : n = posx,
-    { simp only [hx, h, imp_self, and_true, parse_result.pos] },
-    { rw orelse_eq_fail_of_valid_ne hq h at hx,
-      simpa only [parse_result.pos, hx] using hp cb n } }
+    { simp [hx, h] },
+    { simpa [(orelse_eq_fail_of_valid_ne hq h).mp hx] using hp cb n } }
 end
 
 @[simp] lemma decorate_errors (hp : p.valid) :
   (@decorate_errors α msgs p).valid :=
 begin
-  dunfold decorate_errors,
   intros cb n,
-  specialize hp cb n,
-  dsimp only,
-  cases p cb n,
-  { simpa only using hp },
-  { simp only [decorate_errors, imp_self, and_true, parse_result.pos] },
+  cases h : p cb n,
+  { simpa [decorate_errors, h] using hp cb n },
+  { simp [decorate_errors, h] }
 end
 
 @[simp] lemma decorate_error (hp : p.valid) : (@decorate_error α msg p).valid :=
@@ -440,11 +313,9 @@ decorate_errors hp
 @[simp] lemma any_char : valid any_char :=
 begin
   intros cb n,
-  simp only [any_char],
-  split_ifs,
-  { simpa only [true_and, zero_le, le_add_iff_nonneg_right,
-                parse_result.pos] using nat.le_of_succ_le},
-  { simp only [imp_self, and_true, parse_result.pos] }
+  by_cases h : n < cb.size,
+  { simpa [any_char, h] using nat.le_of_succ_le },
+  { simp [any_char, h] }
 end
 
 @[simp] lemma sat {p : char → Prop} [decidable_pred p] : valid (sat p) :=
@@ -452,10 +323,9 @@ begin
   intros cb n,
   simp only [sat],
   split_ifs,
-  { simpa only [true_and, zero_le, le_add_iff_nonneg_right,
-                parse_result.pos] using nat.le_of_succ_le },
-  { simp only [imp_self, and_true, parse_result.pos] },
-  { simp only [parse_result.pos, imp_self, and_true] }
+  { simpa using nat.le_of_succ_le },
+  { simp },
+  { simp }
 end
 
 @[simp] lemma eps : valid eps := pure

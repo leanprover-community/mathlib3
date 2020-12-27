11da47b2e114aa8142166cd7f1b543d5f67584bc
jalex-stark <alexmaplegm@gmail.com>
2020-12-27 16:36:39 -0500

    refactor proofs + add verbose comments

--- src/combinatorics/simple_graph/hall.lean
+++ src/combinatorics/simple_graph/hall.lean
@@ -28,34 +28,42 @@ open_locale classical
 theorem hall_easy (f : α → β) (hf₁ : function.injective f) (hf₂ : ∀ x, r x (f x)) (A : finset α) :
   A.card ≤ (image_rel r A).card :=
 begin
-  -- the image of f of A is a subset of image_rel
-  have h : (image f A) ⊆ (image_rel r A),
-  -- proof of h
-  { -- use the fact that A is a subset of B if and only if x ∈ A implies x ∈ B
-    rw subset_iff,
-    -- let x be of type β and let h2 be the statement that x is in the image of f(A)
-    intros x h2,
-    -- simplify at h2 to get that there exists some a of type α such that a ∈ A and f(a) = x
-    simp only [mem_image, exists_prop] at h2,
-    -- break up h2 into its respective parts; i.e.
-        -- a : α
-        -- ha : a ∈ A
-        -- hfa : f a = x
-    rcases h2 with ⟨a, ha, hfa⟩,
-    -- rewrite the definition of image_rel
-    unfold image_rel,
-    -- simplify
-    simp,
-    -- because a is of type α, by hf₂ we have that a is related to f(a) by r
-    specialize hf₂ a,
-    -- replace f a with x in hf₂
-    rw hfa at hf₂,
-    -- close the goal with a, ha, and hf₂
-    refine ⟨a, ha, hf₂⟩ },
-  -- the cardinality of the image of an injective function is the cardinality of its preimage
-  rw ← card_image_of_injective A hf₁,
-  -- the cardinality of a subset is less than or equal to its superset
-  apply card_le_of_subset h,
+  -- it's enough to show that the image of A under f is a subset of image_rel
+  suffices h : (image f A) ⊆ (image_rel r A),
+  {
+    -- the cardinality of the image of an injective function is the cardinality of its preimage
+    rw ← card_image_of_injective A hf₁,
+
+    -- the cardinality of a subset is less than or equal to its superset
+    apply card_le_of_subset h },
+
+  -- use the fact that A is a subset of B if and only if x ∈ A implies x ∈ B
+  rw subset_iff,
+
+  -- let x be of type β and let h2 be the statement that x is in the image of f(A)
+  intros x h2,
+
+  -- convert h2 into an existential statement
+  simp only [mem_image, exists_prop] at h2,
+
+  -- break up h2 into its respective parts; i.e.
+    -- a : α
+    -- ha : a ∈ A
+    -- hfa : f a = x
+  rcases h2 with ⟨a, ha, hfa⟩,
+
+  -- because a is of type α, by hf₂ we have that a is related to f(a) by r
+  specialize hf₂ a,
+
+  -- replace f a with x in hf₂
+  rw hfa at hf₂,
+
+  -- convert the goal into an existential statement
+  simp [image_rel],
+
+  -- close by supplying a as a witness and supplying ha and hf₂ as proofs that a works
+  use ⟨a, ha, hf₂⟩,
+
 end
 
 /- we prove the opposite direction using strong induction on
@@ -66,15 +74,20 @@ theorem hall_hard_inductive_zero (hn : fintype.card α ≤ 0)
   (hr : ∀ (A : finset α), A.card ≤ (image_rel r A).card) :
   ∃ (f : α → β), function.injective f ∧ ∀ x, r x (f x) :=
 begin
-  rw le_zero_iff_eq at hn,
-  rw fintype.card_eq_zero_iff at hn,
+  -- We know α is empty. `h` is the automation-friendly form of this fact
+  have h : α → false,
+  {
+    -- two standard lemmas show that `h` is the same as `hn`
+    rwa [← fintype.card_eq_zero_iff, ← le_zero_iff_eq],
+  },
+  -- split into two subgoals: give a function and then prove it has the desired properties
   refine ⟨_, _⟩,
-  intro a,
-  specialize hn a,
-  tauto,
-  split,
-  tauto,
-  tauto,
+
+  -- there is no nontrivial function out of α; `tauto` can deal with this for us
+  { tautology },
+
+  -- since our function is trivial, proving it has the properties is also trivial
+  { tautology },
 end
 
 /-- Base case 1: the cardinality of `α` is `1` -/
@@ -84,39 +97,58 @@ theorem hall_hard_inductive_one (hn : fintype.card α = 1)
 begin
   rw fintype.card_eq_one_iff at hn,
   cases hn with x hx,
+
+  suffices hh : ∃ b : β, r x b,
+  {
+    cases hh with b hb,
+    use (λ a, b),
+    split,
+    { intros a1 a2 _,
+      rw [hx a1, hx a2] },
+    { intro a,
+      rwa hx a }
+  },
+
   specialize hr {x},
   rw [card_singleton, le_iff_lt_or_eq] at hr,
-  cases hr with hlt heq,
-  rw one_lt_card_iff at hlt,
-  rcases hlt with ⟨b1, b2, hb1, hb2, hbne⟩,
-  unfold image_rel at hb1,
-  simp at hb1,
-  use (λ a, b1),
-  split,
-  unfold function.injective,
-  intros a1 a2 h,
-  rw [hx a1, hx a2],
-  intros x1,
-  change r x1 b1,
-  specialize hx x1,
-  rw hx,
-  exact hb1,
 
-  symmetry' at heq,
-  rw card_eq_one at heq,
-  cases heq with a ha,
-  use (λ x, a),
-  split,
-  unfold function.injective,
-  intros a1 a2 ha,
-  rw [hx a1, hx a2],
-  intros x2,
-  unfold image_rel at ha,
-  rw eq_singleton_iff_unique_mem at ha,
-  rcases ha with ⟨ha, ha2⟩,
-  simp at ha,
-  rw hx x2,
-  exact ha,
+  -- split into two cases. in each case we'll have something slightly different to do
+  -- in order to get our witness `b` and prove `r x b`.
+  cases hr with hlt heq,
+  work_on_goal 0 {
+    -- convert `hlt` into an existential statement
+    rw one_lt_card_iff at hlt,
+
+    -- pull out the witness `b` and proof `hb` from `hlt`
+    rcases hlt with ⟨b, _, hb, _, _⟩ },
+
+  work_on_goal 1 {
+    -- flip the equality to make it easier to match the form of a `rw` lemma
+    symmetry' at heq,
+
+    -- convert statement to an existential
+    rw card_eq_one at heq,
+
+    -- pull out the witness `b` and not-the-right-proof `hb'` from `heq`
+    cases heq with b hb',
+
+    -- convert `hb'` to a conjunction, of which we care about only the left part
+    rw eq_singleton_iff_unique_mem at hb',
+
+    -- pull out the left part `hb`
+    cases hb' with hb _ },
+
+  all_goals {
+    -- use the `b` witness for the goal
+    use b,
+
+    -- ask lean to unify our goal with the `hb` proof
+    convert hb,
+
+    -- now the simplifier can handle it, so long as we tell it to peek under the hood of
+    -- the `image_rel` definition
+    simp [image_rel] },
+
 end
 
 /-

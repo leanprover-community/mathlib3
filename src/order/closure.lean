31388fc6c695bc5951bbcc9d4066f4e40d3b431d
Bhavik Mehta <bhavikmehta8@gmail.com>
2020-12-29 19:36:17 +0000

    review suggestion

--- src/order/closure.lean
+++ src/order/closure.lean
@@ -24,8 +24,8 @@ is less than its closure) and idempotent.
 -/
 @[ext]
 structure closure_operator extends α →ₘ α :=
-(extensive' : ∀ x, x ≤ to_fun x)
-(idem' : ∀ x, to_fun (to_fun x) = to_fun x)
+(le_closure' : ∀ x, x ≤ to_fun x)
+(idempotent' : ∀ x, to_fun (to_fun x) = to_fun x)
 
 instance : has_coe_to_fun (closure_operator α) :=
 { F := _, coe := λ c, c.to_fun }
@@ -39,22 +39,26 @@ namespace closure_operator
 def id : closure_operator α :=
 { to_fun := λ x, x,
   monotone' := λ _ _ h, h,
-  extensive' := λ _, le_refl _,
-  idem' := λ _, rfl }
+  le_closure' := λ _, le_refl _,
+  idempotent' := λ _, rfl }
 
 instance : inhabited (closure_operator α) := ⟨id α⟩
 
 variables {α} (c : closure_operator α)
 
 @[mono] lemma monotone : monotone c := c.monotone'
-lemma extensive (x : α) : x ≤ c x := c.extensive' x
-@[simp] lemma idempotent (x : α) : c (c x) = c x := c.idem' x
+/--
+Every element is less than its closure. This property is sometimes referred to as extensivity or
+inflationary.
+-/
+lemma le_closure (x : α) : x ≤ c x := c.le_closure' x
+@[simp] lemma idempotent (x : α) : c (c x) = c x := c.idempotent' x
 
 lemma le_closure_iff (x y : α) : x ≤ c y ↔ c x ≤ c y :=
-⟨λ h, c.idempotent y ▸ c.monotone h, λ h, le_trans (c.extensive x) h⟩
+⟨λ h, c.idempotent y ▸ c.monotone h, λ h, le_trans (c.le_closure x) h⟩
 
 lemma closure_top {α : Type u} [order_top α] (c : closure_operator α) : c ⊤ = ⊤ :=
-le_antisymm le_top (c.extensive _)
+le_antisymm le_top (c.le_closure _)
 
 lemma closure_inter_le {α : Type u} [semilattice_inf α] (c : closure_operator α) (x y : α) :
   c (x ⊓ y) ≤ c x ⊓ c y :=

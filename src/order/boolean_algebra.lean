a533c0f2301578c42dae598202afe8d0a0892af5
Chris Upshaw (From Yukari) <chrisaupshaw@gmail.com>
2020-12-30 03:36:45 -0800

    Revert "Add my self as an author.." to follow convention.
    This reverts commit 2e519df1a4a0ec6467c7169cf3c52bcd30f2f7b7.

--- src/order/boolean_algebra.lean
+++ src/order/boolean_algebra.lean
@@ -1,8 +1,7 @@
 /-
 Copyright (c) 2017 Johannes Hölzl. All rights reserved.
-Copyright (c) 2020 Chris Upshaw. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
-Authors: Johannes Hölzl, Chris Upshaw
+Authors: Johannes Hölzl
 
 Type class hierarchy for Boolean algebras.
 -/

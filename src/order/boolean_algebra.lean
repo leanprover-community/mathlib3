2e519df1a4a0ec6467c7169cf3c52bcd30f2f7b7
Chris Upshaw (From Yukari) <chrisaupshaw@gmail.com>
2020-12-30 02:11:42 -0800

    Add my self as an author to boolean_algebra.lean

--- src/order/boolean_algebra.lean
+++ src/order/boolean_algebra.lean
@@ -1,7 +1,8 @@
 /-
 Copyright (c) 2017 Johannes Hölzl. All rights reserved.
+Copyright (c) 2020 Chris Upshaw. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
-Authors: Johannes Hölzl
+Authors: Johannes Hölzl, Chris Upshaw
 
 Type class hierarchy for Boolean algebras.
 -/

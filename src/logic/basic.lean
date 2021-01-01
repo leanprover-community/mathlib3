fcbaf625fbf4c139b99e5a16de2be0cfbc05b67b
Floris van Doorn <fpvdoorn@gmail.com>
2021-01-01 15:24:16 +0000

    doc(lint/type_classes): add has_coe_to_fun linter (#5546)

--- src/logic/basic.lean
+++ src/logic/basic.lean
@@ -85,7 +85,7 @@ theorem coe_sort_coe_trans
 Many structures such as bundled morphisms coerce to functions so that you can
 transparently apply them to arguments. For example, if `e : α ≃ β` and `a : α`
 then you can write `e a` and this is elaborated as `⇑e a`. This type of
-coercion is implemented using the `has_coe_to_fun`type class. There is one
+coercion is implemented using the `has_coe_to_fun` type class. There is one
 important consideration:
 
 If a type coerces to another type which in turn coerces to a function,

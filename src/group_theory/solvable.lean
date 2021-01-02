b292d16506633ade34a93a317dc2ef5d43e9ea96
pglutz <pglutz@berkeley.edu>
2021-01-02 08:09:18 -0800

    Update src/group_theory/solvable.lean
    Co-authored-by: Oliver Nash <oliver.nash@gmail.com>

--- src/group_theory/solvable.lean
+++ src/group_theory/solvable.lean
@@ -1,5 +1,5 @@
 /-
-Copyright (c) 2020 Jordan Brown, Thomas Browning and Patrick Lutz. All rights reserved.
+Copyright (c) 2021 Jordan Brown, Thomas Browning and Patrick Lutz. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Jordan Brown, Thomas Browning and Patrick Lutz
 -/

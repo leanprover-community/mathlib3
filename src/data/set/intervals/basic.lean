b601cc0a10e5e7c41e97a7bc7f46a4701d3aca56
RemyDegenne <Remydegenne@gmail.com>
2021-01-03 15:57:43 +0100

    add my name to the authors

--- src/data/set/intervals/basic.lean
+++ src/data/set/intervals/basic.lean
@@ -1,7 +1,7 @@
 /-
 Copyright (c) 2017 Johannes Hölzl. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
-Authors: Johannes Hölzl, Mario Carneiro, Patrick Massot, Yury Kudryashov
+Authors: Johannes Hölzl, Mario Carneiro, Patrick Massot, Yury Kudryashov, Rémy Degenne
 -/
 import algebra.ordered_group
 import data.set.basic

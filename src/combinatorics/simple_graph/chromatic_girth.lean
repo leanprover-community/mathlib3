f68d7df91dc57a8e94ce0150f275d55000361641
Bhavik Mehta <bhavikmehta8@gmail.com>
2021-01-03 17:02:06 +0000

    fix

--- src/combinatorics/simple_graph/chromatic_girth.lean
+++ src/combinatorics/simple_graph/chromatic_girth.lean
@@ -4,19 +4,34 @@ open_locale classical
 
 def simple_graph.girth {V : Type*} [fintype V] : simple_graph V → ℕ := sorry
 
+@[ext]
 structure Γ (g n d m : ℕ) :=
-(G : simple_graph (fin (n+1)))
-(edges_le : G.edge_finset.card = m)
-(degree_le : G.max_degree ≤ d * d)
+(G : simple_graph (fin n))
+(edges_eq : G.edge_finset.card = m)
+(edges_le : G.edge_finset.card ≤ n * d)
+(degree_le : ∀ v, G.degree v ≤ d * d)
 (girth_ge : g ≤ G.girth)
 
+@[ext]
 structure Ψ (k n d m : ℕ) :=
-(G : simple_graph (fin (n+1)))
-(edges_le : G.edge_finset.card = m)
-(degree_le : G.max_degree ≤ d * d)
+(G : simple_graph (fin n))
+(edges_eq : G.edge_finset.card = m)
+(edges_le : G.edge_finset.card ≤ n * d)
+(degree_le : ∀ v, G.degree v ≤ d * d)
 (colourable : G.chromatic_number ≤ k)
 
-def Ψ_ {k n d m : ℕ} (C : fin (n+1) → fin k) :=
-{G : Ψ k n d m // ∀ {v w : fin (n+1)}, G.G.adj v w → C v ≠ C w}
+noncomputable instance {k n d m : ℕ} : fintype (Ψ k n d m) :=
+begin
+  apply fintype.of_injective Ψ.G,
+  apply Ψ.ext,
+  sorry,
+end
 
-a
+@[derive fintype]
+def Ψ_ (k n d m : ℕ) (C : fin n → fin k) :=
+{G : Ψ k n d m // ∀ {v w : fin n}, G.G.adj v w → C v ≠ C w}
+
+lemma Ψ_C_bound {k n d m : ℕ} (C : fin n → fin k) :
+  fintype.card (Ψ_ k n d m C) ≤ sorry :=
+begin
+end

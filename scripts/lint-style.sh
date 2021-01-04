37744b549fa555824e2ebf9ea0e0e070d91c44f5
Johan Commelin <johan@commelin.net>
2021-01-04 06:34:11 +0000

    doc(scripts/lint-style.sh): a bit of docs on the style linter

--- scripts/lint-style.sh
+++ scripts/lint-style.sh
@@ -1,13 +1,30 @@
 #!/usr/bin/env bash
 
-# This script can be run locally from the root of mathlib using:
-# scripts/lint-style.sh
-# to check whether there are any new style issues, such as:
-# copyright header / module doc / line length issues
+# # Style linter
 
-# This is a little wrapper around the python script
-# lint-style.py
+# ## Usage
+
+# Run this script from the root of mathlib using:
+# ./scripts/lint-style.sh
+
+# ## Purpose
+
+# The style linter checks for new style issues,
+# and maintains a list of exceptions for legacy reasons.
+# Ideally, the length of the list of exceptions tends to 0.
+
+# Examples of issues that are checked for are:
+# * existence of copyright header
+# * existence of module docstrings (in the right place)
+# * line length <= 100 (unless URL)
+
+# ## Implementation details
+
+# A Python script `scripts/lint-style.py` that lints the contents of a Lean file.
+# This script is called below on all Lean files in the repository.
+# Exceptions are maintained in `scripts/style-exceptions.txt`.
 
 touch scripts/style-exceptions.txt
 
 find src archive -name '*.lean' | xargs ./scripts/lint-style.py
+

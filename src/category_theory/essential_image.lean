78abf5925af30c48055533ff0fe4b1621f3fa453
Bhavik Mehta <bhavikmehta8@gmail.com>
2021-01-05 20:48:57 +0000

    restore

--- src/category_theory/essential_image.lean
+++ src/category_theory/essential_image.lean
@@ -58,27 +58,51 @@ lemma ess_image.of_nat_iso {F' : C ⥤ D} (h : F ≅ F') {Y : D} (hY : Y ∈ ess
 hY.imp (λ X, nonempty.map (λ t, h.symm.app X ≪≫ t))
 
 /-- Isomorphic functors have equal essential images. -/
-lemma image_eq_of_nat_iso {F' : C ⥤ D} (h : F ≅ F') :
+lemma ess_image_eq_of_nat_iso {F' : C ⥤ D} (h : F ≅ F') :
   ess_image F = ess_image F' :=
-begin
-  ext A,
-  split,
-  { apply ess_image.of_nat_iso h },
-  { apply ess_image.of_nat_iso h.symm },
-end
+set.ext $ λ A, ⟨ess_image.of_nat_iso h, ess_image.of_nat_iso h.symm⟩
 
+/-- An object in the image is in the essential image. -/
 lemma obj_mem_ess_image (F : D ⥤ C) (Y : D) : F.obj Y ∈ ess_image F := ⟨Y, ⟨iso.refl _⟩⟩
+
+instance : category F.ess_image := category_theory.full_subcategory _
+
+/-- The essential image as a subcategory has a fully faithful inclusion into the target category. -/
+@[derive [full, faithful], simps]
+def ess_image_inclusion (F : C ⥤ D) : F.ess_image ⥤ D :=
+full_subcategory_inclusion _
+
+/--
+Given a functor `F : C ⥤ D`, we have an (essentially surjective) functor from `C` to the essential
+image of `F`.
+-/
+@[simps]
+def to_ess_image (F : C ⥤ D) : C ⥤ F.ess_image :=
+{ obj := λ X, ⟨_, obj_mem_ess_image _ X⟩,
+  map := λ X Y f, (ess_image_inclusion F).preimage (F.map f) }
+
+/--
+The functor `F` factorises through its essential image, where the first functor is essentially
+surjective and the second is fully faithful.
+-/
+@[simps]
+def to_ess_image_comp_essential_image_inclusion :
+  F.to_ess_image ⋙ F.ess_image_inclusion ≅ F :=
+nat_iso.of_components (λ X, iso.refl _) (by tidy)
 
 end functor
 
 /--
-A functor `F : C ⥤ D` is essentially surjective if for every `Y : D`, there is some `X : C`
-with `F.obj X ≅ Y`.
+A functor `F : C ⥤ D` is essentially surjective if every object of `D` is in the essential image
+of `F`. In other words, for every `Y : D`, there is some `X : C` with `F.obj X ≅ Y`.
 
 See https://stacks.math.columbia.edu/tag/001C.
 -/
 class ess_surj (F : C ⥤ D) : Prop :=
 (mem_ess_image [] (Y : D) : Y ∈ F.ess_image)
+
+instance : ess_surj F.to_ess_image :=
+{ mem_ess_image := λ ⟨Y, hY⟩, ⟨_, ⟨⟨_, _, hY.get_iso.hom_inv_id, hY.get_iso.inv_hom_id⟩⟩⟩ }
 
 variables (F) [ess_surj F]
 

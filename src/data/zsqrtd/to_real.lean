85d4f945c139a6cad104f92a6e0f2ae974ded5bf
Eric Wieser <wieser.eric@gmail.com>
2021-01-06 19:33:06 +0000

    Update src/data/zsqrtd/to_real.lean
    Co-authored-by: Bryan Gin-ge Chen <bryangingechen@gmail.com>

--- src/data/zsqrtd/to_real.lean
+++ src/data/zsqrtd/to_real.lean
@@ -1,7 +1,7 @@
 /-
 Copyright (c) 2021 Eric Wieser. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
-Authors: Mario Carneiro
+Authors: Eric Wieser
 -/
 import data.real.sqrt
 import data.zsqrtd.basic

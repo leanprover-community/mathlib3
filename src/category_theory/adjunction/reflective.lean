739ab084335bd80907c366edbc1ab8b1bbd38e29
Bhavik Mehta <bhavikmehta8@gmail.com>
2021-01-06 03:53:23 +0000

    more

--- src/category_theory/adjunction/reflective.lean
+++ src/category_theory/adjunction/reflective.lean
@@ -4,6 +4,7 @@ Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Bhavik Mehta
 -/
 import category_theory.limits.preserves.shapes.binary_products
+import category_theory.limits.preserves.shapes.terminal
 import category_theory.adjunction.fully_faithful
 
 universes v₁ v₂ u₁ u₂

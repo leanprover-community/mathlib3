e3c18dea9e15975394d74a72554806d0e1941722
Thomas Browning <tb65536@uw.edu>
2021-01-07 11:30:03 -0800

    hom

--- src/field_theory/splitting_field.lean
+++ src/field_theory/splitting_field.lean
@@ -812,4 +812,15 @@ lemma alg_equiv.restrict_is_splitting_field_commutes [is_splitting_field F E p] 
 algebra_map E K (χ.restrict_is_splitting_field p E x) = χ (algebra_map E K x) :=
 χ.to_alg_hom.restrict_is_splitting_field_commutes p E x
 
+lemma alg_equiv.restrict_is_splitting_field_comp [hp : is_splitting_field F E p] :
+(χ.restrict_is_splitting_field p E).trans (ω.restrict_is_splitting_field p E) =
+  (χ.trans ω).restrict_is_splitting_field p E :=
+alg_equiv.ext (λ _, (algebra_map E K).injective (by
+{ simp only [alg_equiv.trans_apply, alg_equiv.restrict_is_splitting_field_commutes] }))
+
+def alg_equiv.restict_is_splitting_field_hom [hp : is_splitting_field F E p] :
+(K ≃ₐ[F] K) →* (E ≃ₐ[F] E) :=
+monoid_hom.mk' (λ χ, χ.restrict_is_splitting_field p E)
+  (λ ω χ, (χ.restrict_is_splitting_field_comp ω p E).symm)
+
 end alg_equiv_restrict

13bc64a1038e0a852241631ec7cde94d20381e86
foxthomson <foxthomson0@gmail.com>
2021-01-07 20:02:20 +0000

    Started proof

--- src/computability/DFA.lean
+++ src/computability/DFA.lean
@@ -6,6 +6,7 @@ Authors: Fox Thomson
 
 import data.fintype.basic
 import computability.language
+import tactic.norm_num
 
 /-!
 # Deterministic Finite Automata
@@ -43,12 +44,55 @@ def eval := M.eval_from M.start
 def accepts : language α :=
 λ x, M.eval x ∈ M.accept
 
-variable [finset σ]
+def state_path : σ → list α → list σ
+| start [] := [start]
+| start (a :: x) := M.step start a :: state_path (M.step start a) x
 
-lemma pumping_lemma : ∃ p : ℕ, ∀ x ∈ M.accepts, list.length x ≥ p → ∃ a b c,
-  x = a ++ b ++ c ∧ ∀ n, a ++ (list.repeat b n).foldl (++) [] ++ c ∈ M.accepts :=
+@[simp] lemma length_state_path (start : σ) (x : list α) :
+  (M.state_path start x).length = x.length + 1 :=
 begin
-  sorry
+  induction x generalizing start,
+  refl,
+  tauto
+end
+
+variable [fintype σ]
+
+lemma pumping_lemma : ∃ p : ℕ, ∀ x ∈ M.accepts, p ≤ list.length x → ∃ a b c, x = a ++ b ++ c ∧
+  b ≠ [] ∧ (a ++ b).length ≤ p ∧ ∀ n, a ++ (list.repeat b n).foldl (++) [] ++ c ∈ M.accepts :=
+begin
+  -- Let p be the number of states in our DFA
+  use fintype.card σ,
+  intros x hMx hlen,
+
+  -- By pidgeon hole principal the DFA passes though the same state twice in the first p+1 states we
+  -- pass through. Let these be the `n`th and `m`th states with `n < m`
+  let state_map : fin (fintype.card σ + 1) → σ := λ n, (M.state_path M.start x).nth_le n begin cases n with n h, rw [length_state_path, fin.coe_mk], apply lt_of_lt_of_le, assumption, finish end,
+  have h := fintype.exists_ne_map_eq_of_card_lt state_map (by norm_num),
+  rcases h with ⟨ n, m, hneq, heq ⟩,
+  wlog hle : n ≤ m using [n m, m n],
+  { exact le_total n m },
+
+  -- Now let `a` be the first `n` characters of `x`, `b` be the next `m-n` characters and `c` the
+  -- rest
+  use [(x.take m).take n, (x.take m).drop n, x.drop m],
+  split,
+  { rw [list.take_append_drop, list.take_append_drop] },
+
+  split,
+  { -- We shall show that `b` is not nil by showing it's length is not zero
+    intro h,
+    have hlen := congr_arg list.length h,
+    simp only [list.length_drop, list.length, list.length_take] at hlen,
+    rw [min_eq_left_of_lt, nat.sub_eq_zero_iff_le, fin.coe_fin_le] at hlen,
+    { apply hneq,
+      finish },
+    cases m with m hb,
+    simp,
+    apply lt_of_lt_of_le hb,
+    assumption,
+  },
+  {}
 end
 
 end DFA

78e0db339aa61f57abae61d3509e7e13e2520dd1
Thomas Browning <tb65536@uw.edu>
2021-01-07 22:30:42 -0800

    delete import

--- src/group_theory/perm/cycles.lean
+++ src/group_theory/perm/cycles.lean
@@ -4,7 +4,6 @@ Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Chris Hughes
 -/
 import group_theory.perm.sign
-import group_theory.order_of_element
 
 namespace equiv.perm
 open equiv function finset

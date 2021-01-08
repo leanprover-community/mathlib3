1de0fa9ecf7ed0e2003dbac6b3960aa69979b1ad
Yakov Pechersky <ffxen158@gmail.com>
2021-01-08 13:31:37 -0500

    no need to use instance_cache to generate proofs

--- src/tactic/norm_swap.lean
+++ src/tactic/norm_swap.lean
@@ -19,16 +19,6 @@ Based on equality of these `nat`s, equality proofs are generated using either
 
 open equiv tactic expr
 
-/--
-Creates the application `n c.α p l`, where `p` is a type class instance found in the cache `c`,
-but works on `c.α : Sort*` instead of how `mk_app` uses `c.α : Type*`.
--/
-meta def tactic.instance_cache.mk_sorted_app (c : instance_cache) (n : name) (l : list expr) :
-  tactic (instance_cache × expr) :=
-do d ← get_decl n,
-   (c, l) ← instance_cache.append_typeclasses d.type.binding_body c l,
-   return (c, (const n [c.univ.succ]).mk_app (c.α :: l))
-
 open norm_num
 
 namespace norm_swap
@@ -45,23 +35,22 @@ example : equiv.swap 1 2 1 = 2 := by norm_num
   guard (fexpr.get_app_fn.const_name = ``equiv.swap) <|> fail "coe_fn not of equiv.swap",
   [α, deceq_inst, a, b] ← pure fexpr.get_app_args <|> fail "swap did not have exactly two args applied",
   unify α `(ℕ) <|> fail "currently, norm_swap supports only ℕ",
-  dic ← mk_instance_cache α,
   na ← a.to_nat,
   nb ← b.to_nat,
   nc ← c.to_nat,
   if ha : nc = na
     then do
-      (dic, p) ← dic.mk_sorted_app ``swap_apply_left [a, b],
+      let p : expr := `(@swap_apply_left.{0} %%α %%deceq_inst %%a %%b),
       pure (b, p)
   else if hb : nc = nb
     then do
-      (dic, p) ← dic.mk_sorted_app ``swap_apply_right [a, b],
+      let p : expr := `(@swap_apply_right.{0} %%α %%deceq_inst %%a %%b),
       pure (a, p)
   else do
     nic ← mk_instance_cache `(ℕ), -- our `na, nb, nc` are now in Nat
     (_, hca) ← (prove_ne nic c a nc na),
     (_, hcb) ← (prove_ne nic c b nc nb),
-    (dic, p) ← dic.mk_sorted_app ``swap_apply_of_ne_of_ne [a, b, c, hca, hcb],
+    let p : expr := `(@swap_apply_of_ne_of_ne.{0} %%α %%deceq_inst %%a %%b %%c %%hca %%hcb),
     pure (c, p)
 
 end norm_swap

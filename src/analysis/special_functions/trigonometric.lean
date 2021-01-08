c471a2171e869e0fca8730aa45b0eb4dbd3bc75d
Floris van Doorn <fpvdoorn@gmail.com>
2021-01-08 01:12:26 -0500

    fix

--- src/analysis/special_functions/trigonometric.lean
+++ src/analysis/special_functions/trigonometric.lean
@@ -1789,7 +1789,7 @@ lemma times_cont_diff_on_arcsin {n : with_top ℕ} :
 lemma times_cont_diff_at_arcsin_iff {x : ℝ} {n : with_top ℕ} :
   times_cont_diff_at ℝ n arcsin x ↔ n = 0 ∨ (x ≠ -1 ∧ x ≠ 1) :=
 ⟨λ h, or_iff_not_imp_left.2 $ λ hn, differentiable_at_arcsin.1 $ h.differentiable_at $
-  with_top.one_le_iff_pos.2 (zero_lt_iff_ne_zero.2 hn),
+  with_top.one_le_iff_pos.2 (pos_iff_ne_zero.2 hn),
   λ h, h.elim (λ hn, hn.symm ▸ (times_cont_diff_zero.2 continuous_arcsin).times_cont_diff_at) $
     λ hx, times_cont_diff_at_arcsin hx.1 hx.2⟩
 
@@ -2614,7 +2614,7 @@ end
 
 lemma surj_on_tan : surj_on tan (Ioo (-(π / 2)) (π / 2)) univ :=
 have _ := neg_lt_self pi_div_two_pos,
-continuous_on_tan_Ioo.surj_on_of_tendsto (nonempty_Ioo.2 this) 
+continuous_on_tan_Ioo.surj_on_of_tendsto (nonempty_Ioo.2 this)
   (by simp [tendsto_tan_neg_pi_div_two, this]) (by simp [tendsto_tan_pi_div_two, this])
 
 lemma tan_surjective : function.surjective tan :=

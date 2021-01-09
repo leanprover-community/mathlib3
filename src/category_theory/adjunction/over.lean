4b694ab84dd4bc4f3d11d5e46ef804c7bd13784d
Bhavik Mehta <bhavikmehta8@gmail.com>
2021-01-09 17:05:48 +0000

    more

--- src/category_theory/adjunction/over.lean
+++ src/category_theory/adjunction/over.lean
@@ -24,9 +24,20 @@ cofree _ ⋙ coalgebra_to_over X
 lemma forget_iso [has_binary_products C] : over_to_coalgebra X ⋙ forget _ = over.forget X :=
 rfl
 
+/--
+Note that the binary products assumption is necessary: the existence of a right adjoint to
+`over.forget X` is equivalent to the existence of each binary product `X ⨯ -`.
+-/
 def forget_adj_star [has_binary_products C] : over.forget X ⊣ star X :=
 (coalgebra_equiv_over X).symm.to_adjunction.comp _ _ (adj _)
 
+/--
+(Implementation). Define the "dependent sum" functor, which makes a functor `C / A ⥤ C / B` given
+a morphism `f : A ⟶ B`. This functor is naturally isomorphic to `over.map f`, but in this form it
+is easier to prove adjunction properties, as (if enough structure exists), `over.forget` is a left
+adjoint and the first functor is an equivalence.
+As far as possible, definitions should use `over.map` rather than this.
+-/
 def dependent_sum {A B : C} (f : A ⟶ B) : over A ⥤ over B :=
 (over.iterated_slice_equiv (over.mk f)).inverse ⋙ over.forget _
 
@@ -34,7 +45,7 @@ def dependent_sum {A B : C} (f : A ⟶ B) : over A ⥤ over B :=
 `over.map f` gives nice definitional equalities but `dependent_sum` makes it easy to prove
 adjunction properties
 -/
-def over_map_iso_dependent_sum {A B : C} (f : A ⟶ B) : dependent_sum f ≅ over.map f :=
+def dependent_sum_iso_over_map {A B : C} (f : A ⟶ B) : dependent_sum f ≅ over.map f :=
 nat_iso.of_components (λ X, over.iso_mk (iso.refl _)) (λ X Y g, by tidy)
 
 end category_theory

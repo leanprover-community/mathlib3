0812827cd0a6a2c486cdad8d7b3c16686bed57dd
Andrew Souther <asouther@fordham.edu>
2021-01-10 20:08:05 -0600

    adding basic work from 1st and 2nd steps

--- archive/100-theorems-list/9_area_of_unit_disk.lean
+++ archive/100-theorems-list/9_area_of_unit_disk.lean
@@ -1,7 +1,142 @@
 import measure_theory.interval_integral
+import data.real.sqrt
+import measure_theory.measure_space
+import topology.metric_space.basic
+
 open_locale real
 
-open set interval_integral
+open set interval_integral metric real
+
+--Def'n and alternate def'n of the unit disc
+def unit_disc := {point : ℝ × ℝ | (point.1)^2 + (point.2)^2 < 1 }
+
+def unit_disc_alt := {point : ℝ × ℝ | -1 * sqrt (1 - (point.1)^2) < point.2 ∧
+                                       point.2 < sqrt (1 - (point.1)^2)}
+
+
+--Andrew's work (unfinished), lemma helpful for first step
+lemma unit_disc_open : is_open unit_disc :=
+begin
+  unfold unit_disc,
+  rw is_open_iff,
+  intros p p_in,
+  let ε := (1/2) * (1 - sqrt ((p.1) ^ 2 + (p.2) ^ 2) ),
+  use ε, split,
+  { norm_num,
+    rw ← sqrt_one,
+    exact (sqrt_lt (add_nonneg (pow_two_nonneg p.1) (pow_two_nonneg p.2))).2 p_in, },
+
+  { intros q hq,
+    simp * at *,
+    have ε_def : ε = 1 / 2 * (1 - sqrt (p.fst ^ 2 + p.snd ^ 2)) := rfl,
+    rw ε_def at hq,
+    sorry,
+     },
+end
+
+
+
+
+--Andrew's work : lemma for set eq'ty in second step
+lemma lt_sqrt_of_sqr_lt  {a b : ℝ} : a^2 < b → a < sqrt b :=
+begin
+  intro h,
+  have hb : 0 ≤ b := by linarith [pow_two_nonneg a],
+  have := le_sqrt_of_sqr_le (le_of_lt h),
+  by_contradiction hyp,
+  push_neg at hyp,
+  have heq : a = sqrt b := by linarith,
+  rw heq at h,
+  rw sqr_sqrt hb at h,
+  linarith,
+end
+
+--Andrew's work : another lemma for set eq'ty in second step
+lemma opposite_sqrt_lt_of_sqr_lt {a b : ℝ} : a^2 < b → (-1) * sqrt b < a :=
+begin
+  intro h,
+  have hb : 0 ≤ b := by linarith [pow_two_nonneg a],
+  by_contradiction hyp,
+  push_neg at hyp,
+  by_cases ha : a = 0,
+  {by_cases hb' : b = 0,
+   {  rw [ha, hb'] at h, linarith },
+   {  rw ← ne.def at hb',
+      replace hb' := ne.symm hb',
+      replace hb' : 0 < b := lt_iff_le_and_ne.2 ⟨hb, hb'⟩,
+      have hsqrtb := sqrt_nonneg b,
+      have : (-1) * sqrt b ≤ 0 := by linarith,
+      rw ha at hyp,
+      replace hyp : (-1) * sqrt b = 0 := by linarith,
+      replace hyp :  sqrt b = 0 := by linarith,
+      replace hyp : b = 0 := (sqrt_eq_zero hb).1 hyp,
+      linarith,  }  },
+  {replace ha : a ≠ 0 := by assumption,
+   by_cases ha' : a < 0,
+    { let c := - a,
+      have hc : c = -a := rfl,
+      replace hc : a = -c := by linarith,
+      rw hc at hyp,
+      have hbc : sqrt b ≤ c := by linarith,
+      have := (sqrt_le_iff.1 hbc).2,
+      replace hc : c = -a := rfl,
+      rw hc at this,
+      have hsqrtb := sqrt_nonneg b,
+      rw neg_square a at this,
+      linarith,  },
+    { push_neg at ha',
+      replace ha : 0 ≠ a := ne.symm ha,
+      have a_pos : 0 < a := lt_iff_le_and_ne.2 ⟨ha', ha⟩,
+      have hsqrtb := sqrt_nonneg b,
+      have : (-1) * sqrt b ≤ 0 := by linarith,
+      linarith,  },  },
+end
+
+--Andrew's work : final lemma for set eq'ty in second step
+lemma lt_sqrt {x y : ℝ} (hx : 0 ≤ x) (hy : 0 < y) : x < sqrt y ↔ x ^ 2 < y :=
+by rw [mul_self_lt_mul_self_iff hx (le_of_lt (sqrt_pos.2 hy)), pow_two, mul_self_sqrt (le_of_lt hy)]
+
+
+
+--Andrew's work, set equality for the second step
+lemma second_step : unit_disc  = unit_disc_alt :=
+begin
+  unfold unit_disc, unfold unit_disc_alt,
+  apply set.ext,
+  intro point, split,
+
+  { intro hp,
+    have h₁ : (point.1)^2 + (point.2)^2 < 1 := hp,
+    have h₂ : (point.2)^2 < 1 - (point.1)^2 := by linarith,
+    exact ⟨opposite_sqrt_lt_of_sqr_lt h₂, lt_sqrt_of_sqr_lt h₂⟩, },
+
+  { intro hp,
+    cases hp with hp₁ hp₂,
+    have h₁ : 0 ≤ sqrt (1 - point.fst ^ 2) := sqrt_nonneg (1 - point.fst ^ 2),
+    have term_pos : (1 - point.fst ^ 2) > 0,
+      {by_contradiction hyp,
+      push_neg at hyp,
+      have := sqrt_eq_zero_of_nonpos hyp,
+      rw this at hp₁, rw this at hp₂,
+      simp at hp₁,
+      linarith,},
+    by_cases hyp : 0 ≤ point.snd,
+
+    { have h₁ := (lt_sqrt hyp (by linarith)).1 hp₂,
+      have h₂ : (point.1)^2 + (point.2)^2 < 1 := by linarith,
+      exact h₂, },
+
+    { push_neg at hyp,
+      have h₁ :  - point.snd < sqrt (1 - point.fst ^ 2) := by linarith,
+      have neg_point_pos : 0 < - point.snd := by linarith,
+      have h₂ := (lt_sqrt (le_of_lt neg_point_pos) (by linarith)).1 h₁,
+      rw neg_square point.snd at h₂,
+      have h₃ : (point.1)^2 + (point.2)^2 < 1 := by linarith,
+      exact h₃, }, },
+end
+
+
+
 
 variables {β E F : Type*} [measurable_space E] [normed_group E] {f : ℝ → E}
   {c ca cb : E} {a b z : ℝ} {u v ua ub va vb : β → ℝ} {f' : ℝ → E} [normed_space ℝ E]

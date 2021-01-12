cc113bc820a07068e86c7dc316f5f71636c61904
Heather Macbeth <25316162+hrmacbeth@users.noreply.github.com>
2021-01-11 21:36:47 -0500

    orthonormal basis

---
+++ src/analysis/normed_space/orthonormal.lean
@@ -0,0 +1,154 @@
+import analysis.normed_space.inner_product
+
+noncomputable theory
+
+open_locale big_operators classical
+open submodule finite_dimensional
+
+variables (𝕜 : Type*) {E : Type*} [is_R_or_C 𝕜] [inner_product_space 𝕜 E]
+variables {ι : Type*} [fintype ι] [decidable_eq ι]
+
+local notation `⟪`x`, `y`⟫` := @inner 𝕜 _ _ x y -- here work over 𝕜
+
+/-- Induced inner product on a submodule. -/
+instance submodule_inner_product_space {W : submodule 𝕜 E} : inner_product_space 𝕜 W :=
+{ inner             := λ x y, ⟪(x:E), ↑y⟫,
+  conj_sym          := λ _ _, inner_conj_sym _ _ ,
+  nonneg_im         := λ _, inner_self_nonneg_im,
+  norm_sq_eq_inner  := λ _, norm_sq_eq_inner _,
+  add_left          := λ _ _ _ , inner_add_left,
+  smul_left         := λ _ _ _, inner_smul_left,
+  ..submodule.normed_space W }
+
+/-- The inner product on submodules is the same as on the ambient space. -/
+@[simp] lemma coe_inner (W : submodule 𝕜 E) (x y: W) : ⟪x, y⟫ = ⟪(x:E), ↑y⟫ := rfl
+
+/-- An orthonormal set of vectors in an `inner_product_space` -/
+def orthonormal (v : ι → E) : Prop :=
+∀ i j, ⟪v i, v j⟫ = if i = j then (1:𝕜) else (0:𝕜)
+
+/-- Lemma for `finsupp`, leave as a sorry for now. -/
+lemma finsupp.sum_inner {ι : Type*} (l : ι →₀ 𝕜) (v : ι → E) (x : E) :
+  ⟪l.sum (λ (i : ι) (a : 𝕜), a • v i), x⟫ = l.sum (λ (i : ι) (a : 𝕜), a • ⟪v i, x⟫) :=
+sorry
+
+/-- An orthonormal set is linearly independent.  Clean this up later; maybe there's an existing
+theory of the Kronecker delta function to help.  -/
+lemma linear_independent_of_orthonormal (v : ι → E) (he : orthonormal 𝕜 v) :
+  linear_independent 𝕜 v :=
+begin
+  rw linear_independent_iff,
+  intros l hl,
+  ext i,
+  have key : ⟪finsupp.total ι E 𝕜 v l, v i⟫ = 0,
+  { rw hl, simp },
+  { rw finsupp.total_apply at key,
+    rw finsupp.sum_inner at key,
+    simp at key,
+    have : (λ j a, a * ⟪v j, v i⟫) = λ j a, a * (if j = i then (1:𝕜) else (0:𝕜)),
+    { ext j,
+      simp [he j i] },
+    rw this at key,
+    simp at key,
+    convert key }
+end
+
+lemma is_basis_of_orthonormal_of_card_eq_findim
+  [nonempty ι] [finite_dimensional 𝕜 E] (v : ι → E) (he : orthonormal 𝕜 v)
+  (card_eq : fintype.card ι = findim 𝕜 E) :
+  is_basis 𝕜 v :=
+is_basis_of_linear_independent_of_card_eq_findim
+(linear_independent_of_orthonormal 𝕜 v he) card_eq
+
+def is_basis.equiv_fun_euclidean [finite_dimensional 𝕜 E]
+  {v : ι → E} (h : is_basis 𝕜 v) :
+  E ≃ₗ[𝕜] (euclidean_space 𝕜 ι) :=
+h.equiv_fun
+
+lemma is_basis.isometry_equiv_fun_euclidean [finite_dimensional 𝕜 E]
+  {v : ι → E} (h : is_basis 𝕜 v) (he : orthonormal 𝕜 v) :
+  isometry (h.equiv_fun_euclidean 𝕜) :=
+sorry
+
+example (P Q : Prop) (a b : ℝ) (h : P ↔ Q) : ite P a b = ite Q a b := if_congr h rfl rfl
+
+/-- Finite dimensional `inner_product_space`s have nonzero orthonormal sets of maximal size. -/
+theorem exists_max_orthonormal [finite_dimensional 𝕜 E] :
+  ∃ (v : fin (findim 𝕜 E) → E), orthonormal 𝕜 v :=
+begin
+  tactic.unfreeze_local_instances,
+  induction hk : findim 𝕜 E with k IH generalizing E,
+  { use λ i, 0,
+    have h₀ : fin 0 → fin 0 → false := fin.elim0,
+    simpa [orthonormal] using h₀ },
+  have hE : 0 < findim 𝕜 E,
+  { rw hk,
+    exact k.succ_pos },
+  obtain ⟨x, hx⟩ := findim_pos_iff_exists_ne_zero.mp hE,
+  let e := (∥x∥⁻¹ : 𝕜) • x,
+  have he : ∥e∥ = 1,
+  { simp [e],
+    have : ∥(∥x∥ : 𝕜)∥ = ∥x∥,
+    { rw is_R_or_C.norm_eq_abs,
+      exact is_R_or_C.abs_of_nonneg (norm_nonneg _) },
+    simp [norm_smul, this],
+    rw inv_mul_cancel,
+    simp [hx] },
+  have he' : ⟪e, e⟫ = 1,
+  { rw ← inner_self_re_to_K,
+    rw ← norm_sq_eq_inner,
+    rw he,
+    simp },
+  have he'' : e ≠ 0,
+  { rw [← norm_pos_iff, he],
+    norm_num },
+  have dim_perp_to_line : findim 𝕜 (𝕜 ∙ e)ᗮ = k,
+  { simp [findim_orthogonal_span_singleton he'', hk] },
+  -- apply our inductive hyp to `(𝕜 ∙ e)ᗮ`
+  obtain ⟨w, hw⟩ := IH dim_perp_to_line,
+  let v : fin (k + 1) → E := λ i, if h : i ≠ 0 then coe (w (i.pred h)) else e,
+  -- refine ⟨v, _⟩,
+  have h_end : ∀ (j : fin (k.succ)), 0 ≠ j → ⟪v 0, v j⟫ = 0,
+  { intros j hj,
+    suffices : ⟪e, w (j.pred hj.symm)⟫ = 0,
+    { simpa [v, hj.symm] using this },
+    apply inner_right_of_mem_orthogonal_singleton,
+    exact (w (j.pred hj.symm)).2 },
+  use v,
+  intros i,
+  by_cases h : i = 0,
+  { rw h,
+    intros j,
+    by_cases h' : j = 0,
+    { simp [v, h', he'] },
+    { convert h_end _ (ne.symm h'),
+      simp [ne.symm h'], } },
+  { intros j,
+    by_cases h' : j = 0,
+    { rw h',
+      rw ← inner_conj_sym,
+      rw h_end i (ne.symm h),
+      simp [h] },
+    { convert hw (i.pred h) (j.pred h') using 1,
+      { simp [v, h, h'] },
+      { refine if_congr _ rfl rfl,
+        simp } } }
+end
+
+variables (E)
+
+def max_orthonormal [finite_dimensional 𝕜 E] : fin (findim 𝕜 E) → E :=
+classical.some (exists_max_orthonormal 𝕜)
+
+variables {E}
+
+lemma max_orthonormal_spec [finite_dimensional 𝕜 E] : orthonormal 𝕜 (max_orthonormal 𝕜 E) :=
+classical.some_spec (exists_max_orthonormal 𝕜)
+
+instance [nontrivial E] : nonempty (fin (findim 𝕜 E)) := sorry
+
+variables (E)
+
+lemma is_basis_max_orthonormal [nontrivial E] [finite_dimensional 𝕜 E] :
+  is_basis 𝕜 (max_orthonormal 𝕜 E) :=
+is_basis_of_orthonormal_of_card_eq_findim 𝕜 (max_orthonormal 𝕜 E) (max_orthonormal_spec 𝕜) (by simp)

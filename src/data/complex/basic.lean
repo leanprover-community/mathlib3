6d9d6defc592cf4d951ee3fd52ae586d4ed2a2ac
James Arthur <jamesarthur7@icloud.com>
2021-01-14 20:42:22 +0000

    removed non-terminal simps.

--- src/data/complex/basic.lean
+++ src/data/complex/basic.lean
@@ -364,11 +364,12 @@ char_zero_of_inj_zero $ λ n h,
 by rwa [← of_real_nat_cast, of_real_eq_zero, nat.cast_eq_zero] at h
 
 theorem re_eq_add_conj (z : ℂ) : (z.re : ℂ) = (z + conj z) / 2 :=
-by rw [add_conj]; simp; rw [mul_div_cancel_left (z.re:ℂ) two_ne_zero']
+by {rw add_conj, simp only [of_real_mul, of_real_one, of_real_bit0],
+  rw [mul_div_cancel_left (z.re:ℂ) two_ne_zero']}
 
 theorem im_eq_sub_conj (z : ℂ) : (z.im : ℂ) = (z - conj(z))/(2 * I) :=
-by rw sub_conj; simp; rw [mul_right_comm, mul_div_cancel_left _
-  (mul_ne_zero two_ne_zero' I_ne_zero : 2 * I ≠ 0)]
+by {rw sub_conj, simp only [of_real_mul, of_real_one, of_real_bit0],
+  rw [mul_right_comm, mul_div_cancel_left _ (mul_ne_zero two_ne_zero' I_ne_zero : 2 * I ≠ 0)]}
 
 /-! ### Absolute value -/
 

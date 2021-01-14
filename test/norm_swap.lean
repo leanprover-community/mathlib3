d89f43738e1a690dfc9984b5f32bb7e071b7601c
Yakov Pechersky <ffxen158@gmail.com>
2021-01-14 13:36:53 -0500

    fix int example, provide rat also

--- test/norm_swap.lean
+++ test/norm_swap.lean
@@ -1,5 +1,7 @@
 import data.equiv.basic
 import tactic.norm_swap
+import data.int.cast
+
 
 open equiv tactic
 
@@ -29,14 +31,23 @@ example : true := by do
   done
 
 example : true := by do
-  let l : list ℤ := [0, 1, 2, 3],
-  let l' : list ((ℤ × ℤ) × ℤ) := (do a ← l, b ← l, c ← l, pure ((a, b), c)),
-  (lhs : list expr) ← mmap (λ (tup : (ℤ × ℤ) × ℤ),
-    to_expr ``(equiv.swap %%tup.fst.fst %%tup.fst.snd %%tup.snd)) l',
-  (rhs : list expr) ← mmap (λ (tup : (ℤ × ℤ) × ℤ),
-    if tup.snd = tup.fst.fst then to_expr ``(%%tup.fst.snd)
-    else if tup.snd = tup.fst.snd then to_expr ``(%%tup.fst.fst)
-    else to_expr ``(%%tup.snd)) l',
+  let l : list ℤ := [0, 2, -1, 3, -4],
+  let l' := (do a ← l, b ← l, c ← l, pure ((a, b), c)),
+  ic ← mk_instance_cache `(ℤ),
+  -- we have to convert the provided `int`s into numeral form for the goals
+  -- to look like what they would usually: `⇑(swap -1 3) -4 = -4`, instead of
+-- `⇑(swap (int.neg_succ_of_nat 0) (int.of_nat 3)) (int.neg_succ_of_nat 3) = int.neg_succ_of_nat 3`
+  el ← mmap (λ (tup : (ℤ × ℤ) × ℤ), do
+    (_, a) ← ic.of_int tup.fst.fst,
+    (_, b) ← ic.of_int tup.fst.snd,
+    (_, c) ← ic.of_int tup.snd,
+    pure ((a, b), c)) l',
+  (lhs : list expr) ← mmap (λ (tup : (expr × expr) × expr),
+    to_expr ``((equiv.swap %%tup.fst.fst %%tup.fst.snd) %%tup.snd)) el,
+  let rhs : list expr := el.map (λ (tup : (expr × expr) × expr),
+    if tup.snd = tup.fst.fst then tup.fst.snd
+    else if tup.snd = tup.fst.snd then tup.fst.fst
+    else tup.snd),
   let eqs : list expr := list.zip_with (λ L R, `(@eq.{1} ℤ %%L %%R)) lhs rhs,
   g ← get_goals,
   gls ← mmap mk_meta_var eqs,
@@ -46,15 +57,23 @@ example : true := by do
   done
 
 example : true := by do
-  let l : list ℤ := [0, -1, 2, -3],
-  let l' : list ((ℤ × ℤ) × ℤ) := (do a ← l, b ← l, c ← l, pure ((a, b), c)),
-  (lhs : list expr) ← mmap (λ (tup : (ℤ × ℤ) × ℤ),
-    to_expr ``(equiv.swap %%tup.fst.fst %%tup.fst.snd %%tup.snd)) l',
-  (rhs : list expr) ← mmap (λ (tup : (ℤ × ℤ) × ℤ),
-    if tup.snd = tup.fst.fst then to_expr ``(%%tup.fst.snd)
-    else if tup.snd = tup.fst.snd then to_expr ``(%%tup.fst.fst)
-    else to_expr ``(%%tup.snd)) l',
-  let eqs : list expr := list.zip_with (λ L R, `(@eq.{1} ℤ %%L %%R)) lhs rhs,
+  let l : list ℚ := [0, (2 : ℚ) / -2, -1, 3 / 5, 4],
+  let l' := (do a ← l, b ← l, c ← l, pure ((a, b), c)),
+  ic ← mk_instance_cache `(ℚ),
+  -- we have to convert the provided `rat`s into numeral form for the goals
+  -- to look like what they would usually: `⇑(swap -1 3) -4 = -4`, like with ℤ above
+  el ← mmap (λ (tup : (ℚ × ℚ) × ℚ), do
+    (_, a) ← ic.of_rat tup.fst.fst,
+    (_, b) ← ic.of_rat tup.fst.snd,
+    (_, c) ← ic.of_rat tup.snd,
+    pure ((a, b), c)) l',
+  (lhs : list expr) ← mmap (λ (tup : (expr × expr) × expr),
+    to_expr ``((equiv.swap %%tup.fst.fst %%tup.fst.snd) %%tup.snd)) el,
+  let rhs : list expr := el.map (λ (tup : (expr × expr) × expr),
+    if tup.snd = tup.fst.fst then tup.fst.snd
+    else if tup.snd = tup.fst.snd then tup.fst.fst
+    else tup.snd),
+  let eqs : list expr := list.zip_with (λ L R, `(@eq.{1} ℚ %%L %%R)) lhs rhs,
   g ← get_goals,
   gls ← mmap mk_meta_var eqs,
   set_goals $ g ++ gls,
@@ -69,9 +88,6 @@ begin
   rw swap_apply_of_ne_of_ne;
   dec_trivial
 end
-
-example : swap (0 : ℤ) (0 : ℤ) (-1 : ℤ) = (-1 : ℤ) := by norm_num
-example : swap ((1 : ℚ) / 2) ((3 : ℚ) / -2) ((-6 : ℚ) / 4) = ((-4 : ℚ) / -8) := by norm_num
 
 -- norm_swap doesn't generate trace output on non-swap expressions
 example : (1 : ℤ) = (1 : ℕ) := by norm_num

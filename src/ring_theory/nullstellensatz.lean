9d712a20f40dda40196049d7abc438ee1c81b8aa
Devon Tuma <tumax040@umn.edu>
2021-01-15 15:22:55 -0600

    Improve naming and orginization

--- src/ring_theory/nullstellensatz.lean
+++ src/ring_theory/nullstellensatz.lean
@@ -9,8 +9,16 @@ import field_theory.mv_polynomial
 
 /-!
   # Nullstellensatz
-  This file establishes a version of Hilbert's classical Nullstellensatz for `mv_polynomial`.
+  This file establishes a version of Hilbert's classical Nullstellensatz for `mv_polynomial`s.
   The main statement of the theorem is `vanishing_ideal_zero_locus_eq_radical`.
+
+  The statement is in terms of new definitions `vanishing_ideal` and `zero_locus`.
+  Mathlib already has versions of these in terms of the prime spectrum of a ring,
+    but those are not well-suited for expressing this result.
+  Suggestions for better ways to state this theorem or organize things are welcome.
+
+  The machinery around `vanishing_ideal` and `zero_locus` is also minimal, I only added lemmas
+    directly needed in this proof, since I'm not sure if they are the right approach.
 -/
 
 open ideal
@@ -21,62 +29,63 @@ noncomputable theory
 variables {k : Type*} [field k] [is_alg_closed k]
 variables {σ : Type*} [fintype σ]
 
-/-- The statement of the theorem is in terms of
--/
-def vanishing_ideal (I : ideal (mv_polynomial σ k)) : set (σ → k) :=
+/-- Set of points that are zeroes of all polynomials in an ideal -/
+def zero_locus (I : ideal (mv_polynomial σ k)) : set (σ → k) :=
   {x : σ → k | ∀ p ∈ I, eval x p = 0}
 
-lemma vanishing_ideal_anti_mono (I J : ideal (mv_polynomial σ k)) (h : I ≤ J) :
-  vanishing_ideal J ≤ vanishing_ideal I :=
+lemma zero_locus_anti_mono (I J : ideal (mv_polynomial σ k)) (h : I ≤ J) :
+  zero_locus J ≤ zero_locus I :=
 λ x hx p hp, hx p $ h hp
 
-lemma vanishing_ideal_bot : vanishing_ideal (⊥ : ideal (mv_polynomial σ k)) = ⊤ :=
+lemma zero_locus_bot : zero_locus (⊥ : ideal (mv_polynomial σ k)) = ⊤ :=
 begin
   refine le_antisymm le_top (λ x hx p hp, _),
   rw mem_bot.1 hp,
   exact (eval x).map_zero,
 end
 
-def zero_locus (V : set (σ → k)) : ideal (mv_polynomial σ k) :=
+/-- Ideal of polynomials with common zeroes at all elements of a set -/
+def vanishing_ideal (V : set (σ → k)) : ideal (mv_polynomial σ k) :=
 { carrier := {p | ∀ x ∈ V, eval x p = 0},
   zero_mem' := λ x hx, ring_hom.map_zero _,
   add_mem' := λ p q hp hq x hx, by simp only [hq x hx, hp x hx, add_zero, ring_hom.map_add],
   smul_mem' := λ p q hq x hx,
     by simp only [hq x hx, algebra.id.smul_eq_mul, mul_zero, ring_hom.map_mul] }
 
-lemma zero_locus_anti_mono {A B : set (σ → k)} (h : A ≤ B) : zero_locus B ≤ zero_locus A :=
+lemma vanishing_ideal_anti_mono {A B : set (σ → k)} (h : A ≤ B) :
+  vanishing_ideal B ≤ vanishing_ideal A :=
 λ p hp x hx, hp x $ h hx
 
-lemma zero_locus_bot : zero_locus (∅ : set (σ → k)) = ⊤ :=
+lemma vanishing_ideal_bot : vanishing_ideal (∅ : set (σ → k)) = ⊤ :=
 le_antisymm le_top (λ p hp x hx, absurd hx (set.not_mem_empty x))
 
-lemma le_zero_locus_vanishing_ideal (I : ideal (mv_polynomial σ k)) :
-  I ≤ zero_locus (vanishing_ideal I) :=
+lemma le_vanishing_ideal_zero_locus (I : ideal (mv_polynomial σ k)) :
+  I ≤ vanishing_ideal (zero_locus I) :=
 λ p hp x hx, hx p hp
 
-lemma mem_zero_locus_singleton_iff (x : σ → k) (p : mv_polynomial σ k) :
-  p ∈ (zero_locus {x} : ideal (mv_polynomial σ k)) ↔ (eval x p = 0) :=
+lemma mem_vanishing_ideal_singleton_iff (x : σ → k) (p : mv_polynomial σ k) :
+  p ∈ (vanishing_ideal {x} : ideal (mv_polynomial σ k)) ↔ (eval x p = 0) :=
 ⟨λ h, h x rfl, λ hpx y hy, hy.symm ▸ hpx⟩
 
-lemma zero_locus_singleton_maximal (x : σ → k) :
-  (zero_locus {x} : ideal (mv_polynomial σ k)).is_maximal :=
+lemma vanishing_ideal_singleton_maximal (x : σ → k) :
+  (vanishing_ideal {x} : ideal (mv_polynomial σ k)).is_maximal :=
 begin
-  have : (zero_locus {x} : ideal (mv_polynomial σ k)).quotient ≃+* k := ring_equiv.of_bijective
-    (ideal.quotient.lift _ (eval x) (λ p h, (mem_zero_locus_singleton_iff x p).mp h))
+  have : (vanishing_ideal {x} : ideal (mv_polynomial σ k)).quotient ≃+* k := ring_equiv.of_bijective
+    (ideal.quotient.lift _ (eval x) (λ p h, (mem_vanishing_ideal_singleton_iff x p).mp h))
     begin
-      refine ⟨_, λ z, ⟨(ideal.quotient.mk (zero_locus {x})) (C z), by simp⟩⟩,
+      refine ⟨_, λ z, ⟨(ideal.quotient.mk (vanishing_ideal {x})) (C z), by simp⟩⟩,
       refine (ring_hom.injective_iff _).mpr (λ p hp, _),
       obtain ⟨q, rfl⟩ := quotient.mk_surjective p,
-      rwa [quotient.lift_mk, ← mem_zero_locus_singleton_iff, ← quotient.eq_zero_iff_mem] at hp,
+      rwa [quotient.lift_mk, ← mem_vanishing_ideal_singleton_iff, ← quotient.eq_zero_iff_mem] at hp,
     end,
   rw [← bot_quotient_is_maximal_iff, ring_equiv.bot_maximal_iff this],
   exact bot_is_maximal,
 end
 
-lemma is_maximal_iff_eq_zero_locus_singleton (I : ideal (mv_polynomial σ k)) :
-  I.is_maximal ↔ ∃ (x : σ → k), I = zero_locus {x} :=
+lemma is_maximal_iff_eq_vanishing_ideal_singleton (I : ideal (mv_polynomial σ k)) :
+  I.is_maximal ↔ ∃ (x : σ → k), I = vanishing_ideal {x} :=
 begin
-  refine ⟨_, λ h, let ⟨x, hx⟩ := h in hx.symm ▸ zero_locus_singleton_maximal x⟩,
+  refine ⟨_, λ h, let ⟨x, hx⟩ := h in hx.symm ▸ vanishing_ideal_singleton_maximal x⟩,
   introsI hI,
   let ϕ : k →+* I.quotient := (ideal.quotient.mk I).comp C,
   -- Sorry half is from other PR #5520, proven using facts about Jacobson rings
@@ -86,29 +95,29 @@ begin
   let x : σ → k := λ s, φ ((ideal.quotient.mk I) (X s)),
   have hx : ∀ s : σ, ϕ (x s) = (ideal.quotient.mk I) (X s) :=
     λ s, hφ ((ideal.quotient.mk I) (X s)),
-  have : zero_locus {x} ≤ I,
+  have : vanishing_ideal {x} ≤ I,
   { intros p hp,
     rw [← quotient.eq_zero_iff_mem, map_mv_polynomial_eq_eval₂ (ideal.quotient.mk I) p, eval₂_eq'],
-    rw [mem_zero_locus_singleton_iff, eval_eq'] at hp,
+    rw [mem_vanishing_ideal_singleton_iff, eval_eq'] at hp,
     convert (trans (congr_arg ϕ hp) ϕ.map_zero),
     simp only [ϕ.map_sum, ϕ.map_mul, ϕ.map_prod, ϕ.map_pow, hx] },
-  refine ⟨x, (is_maximal.eq_of_le (zero_locus_singleton_maximal x) hI.1 this).symm⟩,
+  refine ⟨x, (is_maximal.eq_of_le (vanishing_ideal_singleton_maximal x) hI.1 this).symm⟩,
 end
 
 /-- Main statement of the Nullstellensatz -/
 theorem vanishing_ideal_zero_locus_eq_radical (I : ideal (mv_polynomial σ k)) :
-  zero_locus (vanishing_ideal (I)) = I.radical :=
+  vanishing_ideal (zero_locus (I)) = I.radical :=
 begin
   rw (radical_eq_jacobson I),
   refine le_antisymm _ _,
   { refine le_Inf _,
     rintros J ⟨hJI, hJ⟩,
-    obtain ⟨x, hx⟩ := (is_maximal_iff_eq_zero_locus_singleton J).1 hJ,
-    refine hx.symm ▸ zero_locus_anti_mono (λ y hy p hp, _),
-    rw [← mem_zero_locus_singleton_iff, set.mem_singleton_iff.1 hy, ← hx],
+    obtain ⟨x, hx⟩ := (is_maximal_iff_eq_vanishing_ideal_singleton J).1 hJ,
+    refine hx.symm ▸ vanishing_ideal_anti_mono (λ y hy p hp, _),
+    rw [← mem_vanishing_ideal_singleton_iff, set.mem_singleton_iff.1 hy, ← hx],
     refine hJI hp },
   { refine λ p hp x hx, _,
-    rw ← mem_zero_locus_singleton_iff x p,
-    refine (mem_Inf.mp hp) ⟨le_trans (le_zero_locus_vanishing_ideal I)
-      (zero_locus_anti_mono (λ y hy, hy.symm ▸ hx)), zero_locus_singleton_maximal x⟩ },
+    rw ← mem_vanishing_ideal_singleton_iff x p,
+    refine (mem_Inf.mp hp) ⟨le_trans (le_vanishing_ideal_zero_locus I)
+      (vanishing_ideal_anti_mono (λ y hy, hy.symm ▸ hx)), vanishing_ideal_singleton_maximal x⟩ },
 end

609f03979f8308c5bc48d6c505511d3cc22cdc9c
Eric Wieser <wieser.eric@gmail.com>
2021-01-15 11:32:52 +0000

    chore(data/mv_polynomial): mark degrees_{zero,one} with simp

--- src/data/mv_polynomial/variables.lean
+++ src/data/mv_polynomial/variables.lean
@@ -101,10 +101,10 @@ multiset.le_zero.1 $ degrees_monomial _ _
 lemma degrees_X (n : σ) : degrees (X n : mv_polynomial σ R) ≤ {n} :=
 le_trans (degrees_monomial _ _) $ le_of_eq $ to_multiset_single _ _
 
-lemma degrees_zero : degrees (0 : mv_polynomial σ R) = 0 :=
+@[simp] lemma degrees_zero : degrees (0 : mv_polynomial σ R) = 0 :=
 by { rw ← C_0, exact degrees_C 0 }
 
-lemma degrees_one : degrees (1 : mv_polynomial σ R) = 0 := degrees_C 1
+[simp] lemma degrees_one : degrees (1 : mv_polynomial σ R) = 0 := degrees_C 1
 
 lemma degrees_add (p q : mv_polynomial σ R) : (p + q).degrees ≤ p.degrees ⊔ q.degrees :=
 begin

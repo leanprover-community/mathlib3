8977fbd53da729d3496e0023ad69a7b511106122
adomani <adomani@gmail.com>
2021-01-16 20:34:52 +0100

    introduce `denoms_clearable`

--- src/data/polynomial/denominators.lean
+++ src/data/polynomial/denominators.lean
@@ -15,21 +15,23 @@ the image of the homomorphism `i`.
 
 open polynomial finset
 
-section clearing_denominators
+section denoms_clearable
 
 variables {R K : Type*} [semiring R] [comm_semiring K] {i : R →+* K}
 variables {a b : R} {bi : K}
 -- TODO: use hypothesis (ub : is_unit (i b)) to work with localizations.
 
-lemma is_integer_eval_zero (N : ℕ) (a b : R) :
-  ∃ D : R, i D = i b ^ N * eval (i a * bi) (polynomial.map i 0) :=
-⟨0, by simp only [eval_zero, ring_hom.map_zero, mul_zero, map_zero] ⟩
+def denoms_clearable (a b : R) (N : ℕ) (f : polynomial R) (i : R →+* K) :=
+  ∃ D : R, ∃ bi : K, bi * i b = 1 ∧ i D = i b ^ N * eval (i a * bi) (polynomial.map i f)
 
-lemma is_integer_eval_C_mul_X_pow (N : ℕ) (a b : R) (bu : bi * i b = 1) :
-  ∀ (n : ℕ) (r : R), n ≤ N →
-    ∃ D : R,  i D = i b ^ N * eval (i a * bi) (polynomial.map i (C r * X ^ n)) :=
+lemma denoms_clearable_zero (N : ℕ) (a b : R) (bu : bi * i b = 1) :
+  denoms_clearable a b N 0 i :=
+⟨0, bi, bu, by simp only [eval_zero, ring_hom.map_zero, mul_zero, map_zero]⟩
+
+lemma denoms_clearable_C_mul_X_pow (N : ℕ) (a b : R) (bu : bi * i b = 1) :
+  ∀ (n : ℕ) (r : R), n ≤ N → denoms_clearable a b N (C r * X ^ n) i :=
 begin
-  refine λ n r nN, ⟨r * a ^ n * b ^ (N - n), _⟩,
+  refine λ n r nN, ⟨r * a ^ n * b ^ (N - n), bi, bu, _⟩,
   rw [C_mul_X_pow_eq_monomial, map_monomial, ← C_mul_X_pow_eq_monomial, eval_mul, eval_pow, eval_C],
   rw [ring_hom.map_mul, ring_hom.map_mul, ring_hom.map_pow, ring_hom.map_pow, eval_X, mul_comm],
   rw [← nat.sub_add_cancel nN] {occs := occurrences.pos [2]},
@@ -37,28 +39,33 @@ begin
   rw [bu, one_pow, mul_one],
 end
 
-lemma is_integer_eval_add (N : ℕ) (a b : R) :
+lemma denoms_clearable_add (N : ℕ) (a b : R) :
   ∀ (f g : polynomial R),
-    (∃ D : R, i D = (i b ^ N * eval (i a * bi) (polynomial.map i f))) →
-    (∃ D : R, i D = (i b ^ N * eval (i a * bi) (polynomial.map i g))) →
-    (∃ D : R, i D = (i b ^ N * eval (i a * bi) (polynomial.map i (f + g)))) :=
-λ f g ⟨Df, Hf⟩ ⟨Dg, Hg⟩,
-  ⟨Df + Dg, by rw [ring_hom.map_add, polynomial.map_add, eval_add, mul_add, Hf, Hg] ⟩
+    denoms_clearable a b N f i →
+    denoms_clearable a b N g i →
+    denoms_clearable a b N (f + g) i :=
+λ f g ⟨Df, bf, bfu, Hf⟩ ⟨Dg, bg, bgu, Hg⟩,
+  ⟨Df + Dg, bf, bfu,
+  begin
+    rw [ring_hom.map_add, polynomial.map_add, eval_add, mul_add, Hf, Hg],
+    congr,
+    refine @inv_unique K _ (i b) bg bf _ _;
+    rwa mul_comm,
+  end ⟩
 
-lemma exists_integer_eval_div (N : ℕ) (a b : R) {bi : K} (bu : bi * i b = 1) :
-  ∀ (f : polynomial R), f.nat_degree ≤ N →
-  (∃ D : R, i D = (i b ^ N * eval (i a * bi) (polynomial.map i f))) :=
+lemma denoms_clearable_of_nat_degree_le (N : ℕ) (a b : R) {bi : K} (bu : bi * i b = 1) :
+  ∀ (f : polynomial R), f.nat_degree ≤ N → denoms_clearable a b N f i :=
 induction_with_nat_degree_le N
-  (is_integer_eval_zero N a b)
-  (λ N_1 r r0, is_integer_eval_C_mul_X_pow N a b bu N_1 r)
-  (λ f g fN gN, is_integer_eval_add N a b f g)
+  (denoms_clearable_zero N a b bu)
+  (λ N_1 r r0, denoms_clearable_C_mul_X_pow N a b bu N_1 r)
+  (λ f g fN gN df dg, denoms_clearable_add N a b f g df dg)
 
 /-- If `i : R → K` is a ring homomorphism, `f` is a polynomial with coefficients in `R`,
 `a, b` are elements of `R`, with `i b` invertible, then there is a `D ∈ R` such that
 `b ^ f.nat_degree * f (a / b)` equals `i D`. -/
 theorem is_integer_pow_mul_eval
   (i : R →+* K) (f : polynomial R) (a b : R) (bi : K) (bu : bi * i b = 1) :
-  ∃ D : R, i D = (i b ^ f.nat_degree * eval (i a * bi) (polynomial.map i f)) :=
-exists_integer_eval_div (f.nat_degree) a b bu f le_rfl
+  denoms_clearable a b f.nat_degree f i :=
+denoms_clearable_of_nat_degree_le (f.nat_degree) a b bu f le_rfl
 
-end clearing_denominators
+end denoms_clearable

0a1f6c201937b04c00a1c2069118425fc50b9e2e
Yakov Pechersky <ffxen158@gmail.com>
2021-01-18 12:21:54 -0500

    add examples of in situ normalization

--- test/norm_fin.lean
+++ test/norm_fin.lean
@@ -13,3 +13,12 @@ example : fin.cast_succ (fin.cast_succ (4 : fin 5)) = 4 := by norm_num
 example : fin.cast_succ (4 : fin 6) = 11 := by norm_num
 example : fin.cast_succ (3 : fin 6) = 10 := by norm_num
 example : fin.cast_succ (fin.cast_succ (3 : fin 5)) = 10 := by norm_num
+
+example : equiv.swap (0 : fin 3) 1 (fin.succ 1) = 2 :=
+begin
+  success_if_fail {guard_target ((equiv.swap (0 : fin 3) 1) 2 = 2)},
+  norm_num,
+  guard_target ((equiv.swap (0 : fin 3) 1) 2 = 2),
+  exact equiv.swap_apply_of_ne_of_ne (by dec_trivial) (by dec_trivial)
+end
+example : equiv.swap (0 : fin 3) 1 (fin.cast_succ 1) = 0 := by norm_num

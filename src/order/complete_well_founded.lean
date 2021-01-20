5a3a36c11e36187bf2db8935fefc6ed7d45cf96c
Chase Meadors <c.ed.mead@gmail.com>
2021-01-20 16:58:52 -0700

    define compact elements; add "all elements are compact" to the characterizations of compact lattices

--- src/order/complete_well_founded.lean
+++ src/order/complete_well_founded.lean
@@ -49,6 +49,61 @@ same `Sup`. -/
 def is_Sup_finite_compact : Prop :=
 ∀ (s : set α), ∃ (t : finset α), ↑t ⊆ s ∧ Sup s = t.sup id
 
+/-- An element k of a complete lattice is said to be compact if any set that Sups
+above k has a finite subset that Sups above k -/
+def is_compact_element {α : Type*} [complete_lattice α] (k : α) :=
+∀ s : set α, k ≤ Sup s → ∃ t : finset α, ↑t ⊆ s ∧ k ≤ t.sup id
+
+/-- An equivalent characterization of a compact element k is that any directed
+that Sups above k must get above k at some point in the set -/
+theorem is_compact_element_iff_directed_Sup_finite (k : α) :
+  is_compact_element k ↔ ∀ s : set α, s.nonempty → directed_on (≤) s → k ≤ Sup s → ∃ x : α, x ∈ s ∧ k ≤ x :=
+begin
+  classical,
+  split,
+  { by_cases hbot : k = ⊥,
+    -- Any nonempty directed set certainly sups above ⊥
+    { rintros _ _ ⟨x, hx⟩ _ _, use x, by simp[hx, hbot], },
+    { intros hk s hne hdir hsup,
+      cases (hk s hsup) with t ht,
+      -- If t were empty, it would sup to ⊥, which is not above k ≠ ⊥.
+      have tne : t.nonempty,
+      { by_contradiction n,
+        rw [finset.nonempty_iff_ne_empty, not_not] at n,
+        simp only [n, true_and, set.empty_subset, finset.coe_empty, finset.sup_empty, le_bot_iff] at ht,
+        exact absurd ht hbot, },
+      -- certainly ↑t ⊆ s, so every element of t is below something in s, to apply finset_sup_of_directed_on
+      have t_below_s : ∀ x ∈ t, ∃ y ∈ s, x ≤ y, from λ x hxt, ⟨x, ht.left hxt, by refl⟩,
+      rcases (finset.sup_le_of_le_directed s hdir t tne t_below_s) with ⟨x, ⟨hxs, hsupx⟩⟩,
+      exact ⟨x, ⟨hxs, le_trans k (t.sup id) x ht.right hsupx⟩⟩, }, },
+  { intros hk s hsup,
+    -- Consider the set of finite joins of elements of the (plain) set s.
+    let S : set α := { x | ∃ t : finset α, ↑t ⊆ s ∧ x = t.sup id },
+    -- S is directed, nonempty, and still sups above k.
+    have dir_US : directed_on (≤) S,
+    { intros x hx y hy,
+      cases hx with c hc,
+      cases hy with d hd,
+      use x ⊔ y,
+      split,
+      { use c ∪ d,
+        split,
+        { simp only [hc.left, hd.left, set.union_subset_iff, finset.coe_union, and_self], },
+        { simp only [hc.right, hd.right, finset.sup_union], }, },
+      simp only [and_self, le_sup_left, le_sup_right], },
+    have sup_S : Sup s ≤ Sup S,
+    { apply Sup_le_Sup,
+      intros x hx, use {x}, simpa, },
+    have Sne : S.nonempty,
+    { suffices : ⊥ ∈ S, from set.nonempty_of_mem this,
+      use ∅,
+      simp only [set.empty_subset, finset.coe_empty, finset.sup_empty, eq_self_iff_true, and_self], },
+    -- Now apply the defn of compact and finish.
+    rcases (hk S Sne dir_US (le_trans k (Sup s) (Sup S) hsup sup_S)) with ⟨j, ⟨hjS, hjk⟩⟩,
+    rcases hjS with ⟨t, ⟨htS, htsup⟩⟩,
+    use t, exact ⟨htS, by rwa ←htsup⟩, },
+end
+
 lemma well_founded.is_Sup_finite_compact (h : well_founded ((>) : α → α → Prop)) :
   is_Sup_finite_compact α :=
 begin
@@ -88,12 +143,36 @@ begin
   { rintros x y ⟨m, hm⟩ ⟨n, hn⟩, use m ⊔ n, rw [← hm, ← hn], apply a.to_rel_hom.map_sup, },
 end
 
+lemma all_elements_compact.is_Sup_finite_compact (h : ∀ k : α, is_compact_element k) :
+  is_Sup_finite_compact α :=
+begin
+  intro s,
+  rcases (h (Sup s) s (by refl)) with ⟨t, ⟨hts, htsup⟩⟩,
+  have : Sup s = t.sup id,
+  { suffices : t.sup id ≤ Sup s, by { apply le_antisymm; assumption },
+    simp only [id.def, finset.sup_le_iff],
+    intros x hx,
+    apply le_Sup, exact hts hx, },
+  use [t, hts], assumption,
+end
+
+lemma is_Sup_finite_compact.all_elements_compact (h : is_Sup_finite_compact α) :
+  ∀ k : α, is_compact_element k :=
+begin
+  intros k s hs,
+  rcases (h s) with ⟨t, ⟨hts, htsup⟩⟩,
+  use [t, hts],
+  rwa ←htsup,
+end
+
 lemma well_founded_characterisations :
-  tfae [well_founded ((>) : α → α → Prop), is_Sup_finite_compact α, is_sup_closed_compact α] :=
+  tfae [well_founded ((>) : α → α → Prop), is_Sup_finite_compact α, is_sup_closed_compact α, ∀ k : α, is_compact_element k] :=
 begin
   tfae_have : 1 → 2, by { exact well_founded.is_Sup_finite_compact α, },
   tfae_have : 2 → 3, by { exact is_Sup_finite_compact.is_sup_closed_compact α, },
   tfae_have : 3 → 1, by { exact is_sup_closed_compact.well_founded α, },
+  tfae_have : 2 → 4, by { exact is_Sup_finite_compact.all_elements_compact α },
+  tfae_have : 4 → 2, by { exact all_elements_compact.is_Sup_finite_compact α },
   tfae_finish,
 end
 
@@ -109,9 +188,14 @@ lemma is_sup_closed_compact_iff_well_founded :
   is_sup_closed_compact α ↔ well_founded ((>) : α → α → Prop) :=
 (well_founded_characterisations α).out 2 0
 
+lemma is_Sup_finite_compact_iff_all_elements_compact :
+  is_Sup_finite_compact α ↔ ∀ k : α, is_compact_element k :=
+(well_founded_characterisations α).out 1 3
+
 alias well_founded_iff_is_Sup_finite_compact ↔ _ is_Sup_finite_compact.well_founded
 alias is_Sup_finite_compact_iff_is_sup_closed_compact ↔
       _ is_sup_closed_compact.is_Sup_finite_compact
 alias is_sup_closed_compact_iff_well_founded ↔ _ well_founded.is_sup_closed_compact
+alias is_Sup_finite_compact_iff_all_elements_compact ↔ _ all_elements_compact.is_Sup_finite_compact
 
 end complete_lattice

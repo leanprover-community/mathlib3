a6b82a6f951a0ea22ba8c410f561a837c9a35501
adomani <adomani@gmail.com>
2021-01-20 08:28:49 +0100

    doc_strings

--- src/algebraic_geometry/is_open_comap_C.lean
+++ src/algebraic_geometry/is_open_comap_C.lean
@@ -15,32 +15,32 @@ namespace algebraic_geometry
 
 namespace polynomial
 
-variables {R : Type*} [comm_ring R]
+variables {R : Type*} [comm_ring R] {f : polynomial R}
 
 /-- Given a polynomial `f ∈ R[x]`, `image_of_Df` is the subset of `Spec R` where at least one
 of the coefficients of `f` does not vanish.  Lemma `image_of_Df_eq_comap_C_compl_zero_locus`
 proves that `image_of_Df` is the image of `(zero_locus {f})ᶜ` under the morphism
 `comap C : Spec R[x] → Spec R`. -/
-def image_of_Df (f : polynomial R) : set (prime_spectrum R) :=
+def image_of_Df (f) : set (prime_spectrum R) :=
   {p : prime_spectrum R | ∃ i : ℕ , (coeff f i) ∉ p.as_ideal}
 
-lemma is_open_image_of_Df {f : polynomial R} : is_open (image_of_Df f) :=
+lemma is_open_image_of_Df : is_open (image_of_Df f) :=
 begin
   rw [image_of_Df, set_of_exists (λ i (x : prime_spectrum R), coeff f i ∉ x.val)],
   exact is_open_Union (λ i, is_open_basic_open),
 end
 
-/-- If a point of `Spec R[x]` is not contained in the vanishing set of `f`,
-then its image in `Spec R` is contained in the open where at least one of the coefficients
-of `f` is non-zero.  Such an open set is called `image_of_Df f` and actually coincides
-with the image of `basic_open f`.
+/-- If a point of `Spec R[x]` is not contained in the vanishing set of `f`, then its image in
+`Spec R` is contained in the open set where at least one of the coefficients of `f` is non-zero.
 This lemma is a reformulation of `exists_coeff_not_mem_C_inverse`. -/
-lemma comap_C_mem_image_of_Df {f : polynomial R} {I : prime_spectrum (polynomial R)}
+lemma comap_C_mem_image_of_Df {I : prime_spectrum (polynomial R)}
   (H : I ∈ (zero_locus {f} : set (prime_spectrum (polynomial R)))ᶜ ) :
   comap (polynomial.C : R →+* polynomial R) I ∈ image_of_Df f :=
 exists_coeff_not_mem_C_inverse (mem_compl_zero_locus_iff_not_mem.mp H)
 
-lemma image_of_Df_eq_comap_C_compl_zero_locus {f : polynomial R} :
+/-- The open set `image_of_Df f` coincides with the image of `basic_open f` under the
+morphism `C⁺ : Spec R[x] → Spec R`. -/
+lemma image_of_Df_eq_comap_C_compl_zero_locus :
   image_of_Df f = comap C '' (zero_locus {f})ᶜ :=
 begin
   refine ext (λ x, ⟨λ hx, ⟨⟨map C x.val, (is_prime_map_C_of_is_prime x.property)⟩, ⟨_, _⟩⟩, _⟩),

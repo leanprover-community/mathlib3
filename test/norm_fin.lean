8860c9abb22b5fa1f5b06e12e919828ede52b619
Yakov Pechersky <ffxen158@gmail.com>
2021-01-20 22:13:30 -0500

    le lt ge gt tests

--- test/norm_fin.lean
+++ test/norm_fin.lean
@@ -38,3 +38,25 @@ begin
   exact equiv.swap_apply_of_ne_of_ne dec_trivial dec_trivial
 end
 example : equiv.swap (0 : fin 3) 1 (fin.cast_succ 1) = 0 := by norm_fin
+
+example : (5 : fin 7) < fin.succ (fin.succ 9) := by norm_num
+example : (12 : fin 7) < 6 := by norm_num
+example : (11 : fin 7) > 3 := by norm_num
+example : (12 : fin 6) ≤ 0 := by norm_num
+example : (11 : fin 6) ≥ 5 := by norm_num
+example : fin.succ (4 : fin 6) < 13 := by norm_num
+example : fin.succ (1 : fin (1 + 2)) < 3 := by norm_num
+example : fin.succ (1 : fin (nat.succ (1 + 1))) < 3 := by norm_num
+example : (1 : fin (nat.succ (1 + 1))) < 2 := by norm_num
+example : (1 : fin (nat.succ (1 + 2))) < 7 := by norm_num
+example : (3 : fin (nat.succ (1 + 1 + 1))) > 4 := by norm_num
+example : (2 : fin (nat.succ (2 + 1 + 1))) < 8 := by norm_num
+example : (2 : fin (nat.succ (nat.succ (2 + 1 + 1)))) < 9 := by norm_num
+example : fin.cast_succ (1 : fin (nat.succ (1 + 1))) < 7 := by norm_num
+example : (1 : fin (nat.succ (1 + 1))) < fin.succ 3 := by norm_num
+example : fin.succ (3 : fin 6) < fin.cast_succ 11 := by norm_num
+example : fin.cast_succ (4 : fin 6) < fin.succ 4 := by norm_num
+example : fin.cast_succ (fin.cast_succ (4 : fin 5)) < fin.cast_succ (fin.succ 4) := by norm_num
+example : fin.cast_succ (4 : fin 6) < fin.succ 11 := by norm_num
+example : fin.cast_succ (3 : fin 6) < fin.cast_succ 10 := by norm_num
+example : fin.cast_succ (fin.cast_succ (3 : fin 5)) < fin.succ (fin.cast_succ 9) := by norm_num

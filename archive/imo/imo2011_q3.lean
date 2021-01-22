6a7a1aaa6b407580c803fd179a0d833c0698f133
David Renshaw <dwrenshaw@gmail.com>
2021-01-22 00:45:50 -0500

    improve formatting

--- archive/imo/imo2011_q3.lean
+++ archive/imo/imo2011_q3.lean
@@ -20,9 +20,8 @@ for all x and y. Prove that f(x) = 0 for all x ≤ 0.
 Direct translation of the solution found in https://www.imo-official.org/problems/IMO2011SL.pdf
 -/
 
-theorem imo2011_q3
-  (f : ℝ → ℝ) (hf : ∀ x y, f (x + y) ≤ y * f x + f (f x))
-  : ∀ x ≤ 0, f x = 0 :=
+theorem imo2011_q3 (f : ℝ → ℝ) (hf : ∀ x y, f (x + y) ≤ y * f x + f (f x)) :
+  ∀ x ≤ 0, f x = 0 :=
 begin
   -- reparameterize
   have hxt : ∀ x t, f t ≤ t * f x - x * f x + f (f x),

492fa74d747d20208cf1498c206ec12b6f3c102c
agusakov <39916842+agusakov@users.noreply.github.com>
2021-01-25 07:09:07 -0500

    Update src/combinatorics/hall.lean
    Co-authored-by: Bryan Gin-ge Chen <bryangingechen@gmail.com>

--- src/combinatorics/hall.lean
+++ src/combinatorics/hall.lean
@@ -102,7 +102,7 @@ end
 First case of the inductive step: assuming that
 `∀ (A : finset α), A.nonempty → A ≠ univ → A.card < (A.bind r).card`
 and that the statement of Hall's Marriage Theorem is true for all
-`α'` of cardinality ≤ `n`, then it is true for `α` of cardinality n.succ.
+`α'` of cardinality ≤ `n`, then it is true for `α` of cardinality `n + 1`.
 -/
 lemma hall_hard_inductive_step_A {n : ℕ} (hn : fintype.card α = n.succ)
   (hr : ∀ (A : finset α), A.card ≤ (A.bind r).card)

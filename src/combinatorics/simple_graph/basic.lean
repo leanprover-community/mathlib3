62b10930ba830ac60c635eefd74c700f2931a0bb
Alena Gusakov <agusakov@udel.edu>
2021-01-25 07:32:11 -0500

    changed to simp only

--- src/combinatorics/simple_graph/basic.lean
+++ src/combinatorics/simple_graph/basic.lean
@@ -465,19 +465,22 @@ begin
   tauto,
 end
 
+lemma finset.compl_to_finset (α : Type u) [decidable_eq α] [fintype α] (s : set α) :
+  sᶜ.to_finset = (s.to_finset)ᶜ:= by ext; simp only [mem_compl, set.mem_to_finset, set.mem_compl_eq]
+
 lemma neighbor_set_union_compl_neighbor_set_card [fintype V] (G : simple_graph V) (v : V) :
   (G.neighbor_set v ∪ Gᶜ.neighbor_set v).to_finset.card = fintype.card V - 1 :=
 begin
+  classical,
   rw neighbor_set_union_compl_neighbor_set_eq,
-  --rw finset.card_compl,
-  rw ← card_univ,
-  simp,
-
+  rw finset.compl_to_finset,
+  rw card_compl,
+  rw ← card_singleton v,
   sorry,
 end
 
-lemma compl_regular_is_regular [fintype V] (G : simple_graph V) [nonempty V] (k : ℕ) (h : G.is_regular_of_degree k) :
-  Gᶜ.is_regular_of_degree (fintype.card V - k - 1) :=
+lemma compl_regular_is_regular [fintype V] (G : simple_graph V) [nonempty V] (k : ℕ)
+  (h : G.is_regular_of_degree k) : Gᶜ.is_regular_of_degree (fintype.card V - k - 1) :=
 begin
   rw is_regular_of_degree,
   intros v,

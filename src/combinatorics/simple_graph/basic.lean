a0eb6858ce8a7fdacf81d09b553dd616cf6a9d95
Alena Gusakov <agusakov@udel.edu>
2021-01-25 09:25:34 -0500

    removed from fintype

--- src/combinatorics/simple_graph/basic.lean
+++ src/combinatorics/simple_graph/basic.lean
@@ -465,18 +465,18 @@ begin
   tauto,
 end
 
-lemma finset.compl_to_finset (α : Type u) [decidable_eq α] [fintype α] (s : set α) :
-  sᶜ.to_finset = (s.to_finset)ᶜ:= by ext; simp only [mem_compl, set.mem_to_finset, set.mem_compl_eq]
-
 lemma neighbor_set_union_compl_neighbor_set_card [fintype V] (G : simple_graph V) (v : V) :
   (G.neighbor_set v ∪ Gᶜ.neighbor_set v).to_finset.card = fintype.card V - 1 :=
 begin
+  -- i don't love the `fintype.subtype_of_fintype situation` i have here
   classical,
   rw neighbor_set_union_compl_neighbor_set_eq,
   rw finset.compl_to_finset,
   rw card_compl,
+  simp,
+  rw fintype.card_of_finset' {v},
   rw ← card_singleton v,
-  sorry,
+  simp,
 end
 
 lemma compl_regular_is_regular [fintype V] (G : simple_graph V) [nonempty V] (k : ℕ)
@@ -486,13 +486,13 @@ begin
   intros v,
   specialize h v,
   have h2 := degree_lt_card_verts G v,
-  have h3 := neighbor_set_union_compl_neighbor_set_card G v,
   rw nat.sub_sub,
   rw add_comm,
   rw ← nat.sub_sub,
-  rw ← h3,
+  rw ← neighbor_set_union_compl_neighbor_set_card G v,
   rw ← h,
-  --rw set.to_finset_union,
+  simp,
+  rw set.to_finset_union,
   sorry,
 end
 

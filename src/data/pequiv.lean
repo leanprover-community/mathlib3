517ad85d0221106dec356a8be57971130ba72df9
Jesse Michael Han <hyrodi@gmail.com>
2021-01-26 18:28:38 -0500

    `refl` -> rfl

--- src/data/pequiv.lean
+++ src/data/pequiv.lean
@@ -140,7 +140,7 @@ by dsimp [of_set]; split_ifs; split; finish
 @[simp] lemma of_set_symm : (of_set s).symm = of_set s := rfl
 
 @[simp] lemma of_set_univ : of_set set.univ = pequiv.refl α :=
-by refl
+rfl
 
 @[simp] lemma of_set_eq_refl {s : set α} [decidable_pred s] :
   of_set s = pequiv.refl α ↔ s = set.univ :=

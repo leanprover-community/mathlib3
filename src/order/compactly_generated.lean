63da64cd75d4a1c654bc45fd948f0a92c7664d6f
Chase Meadors <c.ed.mead@gmail.com>
2021-01-25 19:30:45 -0700

    Generalize and simplify finset_sup_compact_of_compact w/ new lemma

--- src/order/compactly_generated.lean
+++ src/order/compactly_generated.lean
@@ -120,16 +120,19 @@ begin
     use t, exact ⟨htS, by rwa ←htsup⟩, },
 end
 
-lemma finset_sup_compact_of_compact (s : finset α) (h : ∀ x ∈ s, is_compact_element x) :
-  is_compact_element (s.sup id) :=
+lemma finset_sup_compact_of_compact {α β : Type*} [complete_lattice α] {f : β → α}
+  (s : finset β) (h : ∀ x ∈ s, is_compact_element (f x)) : is_compact_element (s.sup f) :=
 begin
+  classical,
   rw is_compact_element_iff_le_of_directed_Sup_le,
   intros d hemp hdir hsup,
+  change f with id ∘ f, rw finset.sup_finset_image,
   apply finset.sup_le_of_le_directed d hemp hdir,
-  intros x hxs,
-  specialize h x hxs,
+  rintros x hx,
+  obtain ⟨p, ⟨hps, rfl⟩⟩ := finset.mem_image.mp hx,
+  specialize h p hps,
   rw is_compact_element_iff_le_of_directed_Sup_le at h,
-  specialize h d hemp hdir (_root_.le_trans (@finset.le_sup _ _ _ _ id _ hxs) hsup),
+  specialize h d hemp hdir (_root_.le_trans (finset.le_sup hps) hsup),
   simpa only [exists_prop],
 end
 

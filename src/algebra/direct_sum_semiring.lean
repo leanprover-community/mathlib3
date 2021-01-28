a137f6f2d1deb7c6b2155d1826ece11d53504e6d
Eric Wieser <wieser.eric@gmail.com>
2021-01-28 16:28:01 +0000

    Get everything working

--- src/algebra/direct_sum_semiring.lean
+++ src/algebra/direct_sum_semiring.lean
@@ -1,9 +1,110 @@
+/-
+Copyright (c) 2021 Eric Wieser. All rights reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
+Authors: Eric Wieser
+-/
 import algebra.direct_sum
 import ring_theory.subsemiring
+
+/-!
+# The semiring structure on `⨁ i, M i` when `M i : submonoid S`
+
+This module provides a typeclass `semiring_add_gradation M` that shows `M : ι → submonoid S` is an
+additive gradation of `S`, such that:
+
+* `1 ∈ M 0`
+* `x ∈ M i → y ∈ M j → (x + y) ∈ M (i + j)`
+
+When this typeclass is present, it imbues a `semiring` structure over `⨁ i, M i`.begin
+
+If the `M i` are disjoint, this is a graded semiring.
+
+-/
+section
+
+variables {ι : Type*} {β : ι → Type*} {R S : Type*}
+variables [decidable_eq ι]
+
+@[simp,to_additive]
+lemma monoid_hom.dfinsupp_prod_apply' [comm_monoid R] [comm_monoid S]
+  [Π (i : ι), has_zero (β i)]
+  { _ :Π (i : ι) (x : β i), decidable (x ≠ 0)}
+  (f : Π₀ i, β i) (g : Π i, β i → R →* S) (r : R):
+  (f.prod g) r = f.prod (λ a b, (g a b) r) := monoid_hom.finset_prod_apply _ _ r
+
+@[simp]
+lemma add_monoid_hom.map_dfinsupp_sum_add_hom
+  [add_comm_monoid R] [add_comm_monoid S]
+  [Π (i : ι), add_comm_monoid (β i)]
+   (h : R →+ S) (f : Π₀ i, β i) (g : Π i, β i →+ R) :
+  h (dfinsupp.sum_add_hom g f)
+    = dfinsupp.sum_add_hom (λ i, h.comp (g i)) f :=
+add_monoid_hom.congr_fun (dfinsupp.comp_lift_add_hom h g) f
+
+@[simp]
+lemma add_monoid_hom.dfinsupp_sum_add_hom_apply
+  [add_comm_monoid R] [add_comm_monoid S]
+  [Π (i : ι), add_comm_monoid (β i)]
+  (f : Π₀ i, β i) (g : Π i, β i →+ R →+ S) (r : R) :
+  (dfinsupp.sum_add_hom g f) r
+    = dfinsupp.sum_add_hom (λ i, (add_monoid_hom.eval r).comp (g i)) f :=
+add_monoid_hom.map_dfinsupp_sum_add_hom (add_monoid_hom.eval r) f g
+
+lemma add_monoid_hom.coe_dfinsupp_sum_add_hom
+  [add_comm_monoid R] [add_comm_monoid S]
+  [Π (i : ι), add_comm_monoid (β i)]
+  (f : Π₀ i, β i) (g : Π i, β i →+ R →+ S) :
+  ⇑(dfinsupp.sum_add_hom g f)
+    = dfinsupp.sum_add_hom (λ i, (add_monoid_hom.coe_fn R S).comp (g i)) f :=
+add_monoid_hom.map_dfinsupp_sum_add_hom (add_monoid_hom.coe_fn R S) f g
+
+@[simp]
+lemma dfinsupp.sum_add_hom_single_add_hom
+  [Π (i : ι), add_comm_monoid (β i)] :
+  dfinsupp.sum_add_hom (dfinsupp.single_add_hom β) = add_monoid_hom.id _ :=
+by { ext, simp }
+
+lemma {u₁ v₁} dfinsupp.sum_add_hom_comp_sum_add_hom {γ } {ι₁ : Type u₁} [decidable_eq ι₁] {β₁ : ι₁ → Type v₁}
+  [Π i, add_comm_monoid (β i)]
+  [Π i, add_comm_monoid (β₁ i)]
+  [add_comm_monoid γ]
+  {g : Π i₁, β₁ i₁ →+ Π₀ i, β i}
+  {h : Π i, β i →+ γ} :
+  (dfinsupp.sum_add_hom h).comp (dfinsupp.sum_add_hom g) = dfinsupp.sum_add_hom (λi, (dfinsupp.sum_add_hom h).comp (g i)) :=
+begin
+  ext,
+  simp,
+end
+
+@[simp]
+lemma dfinsupp.sum_add_hom_zero {γ} [Π (i : ι), add_monoid (β i)] [add_comm_monoid γ] :
+  dfinsupp.sum_add_hom (λ i, (0 : β i →+ γ)) = 0 :=
+begin
+  ext,
+  simp,
+end
+
+@[simp]
+lemma dfinsupp.sum_add_hom_add {γ} [Π (i : ι), add_monoid (β i)] [add_comm_monoid γ]
+  (g : Π i, β i →+ γ) (h : Π i, β i →+ γ) :
+  dfinsupp.sum_add_hom (λ i, g i + h i) = dfinsupp.sum_add_hom g + dfinsupp.sum_add_hom h :=
+begin
+  ext,
+  simp,
+end
+
+lemma dfinsupp.single_eq_of_sigma_eq
+  [Π (i : ι), has_zero (β i)]
+  {i j} {xi : β i} {xj : β j} (h : (⟨i, xi⟩ : sigma β) = ⟨j, xj⟩ ):
+  dfinsupp.single i xi = dfinsupp.single j xj :=
+by { cases h, refl }
+
+end
 
 variables
   {A : Type*} [semiring A]
   {ι : Type*} [add_comm_monoid ι] [decidable_eq ι]
+
 
 namespace direct_sum
 
@@ -13,12 +114,12 @@ class semiring_add_gradation (carriers : ι → add_submonoid A) :=
 
 variables (carriers : ι → add_submonoid A) [semiring_add_gradation carriers]
 
-
 open_locale direct_sum
 
-/-! First, define multiplication and 1 on the individual carriers, and show they form a
-heq-monoid-/
-def hmul {i j} : carriers i →+ carriers j →+ carriers (i + j) :=
+/-! The following lemmas begin with `h` to indicate that they are the heterogenous operators -/
+
+/-! Multiplication is defined on the underlying semiring. -/
+private def hmul {i j} : carriers i →+ carriers j →+ carriers (i + j) :=
 { to_fun := λ a,
   { to_fun := λ b, ⟨(a * b : A), semiring_add_gradation.mul_mem a b⟩,
     map_add' := λ _ _, subtype.ext (mul_add _ _ _),
@@ -26,105 +127,158 @@ def hmul {i j} : carriers i →+ carriers j →+ carriers (i + j) :=
   map_add' := λ _ _, add_monoid_hom.ext $ λ _, subtype.ext (add_mul _ _ _),
   map_zero' := add_monoid_hom.ext $ λ _, subtype.ext (zero_mul _) }
 
-def hone : carriers 0 := ⟨1, semiring_add_gradation.one_mem⟩
+/-! One is defined on the underlying semiring. -/
+private def hone : carriers 0 := ⟨1, semiring_add_gradation.one_mem⟩
 
-lemma hone_hmul {i} (a : carriers i) : hmul carriers (hone carriers) a == a :=
+/-! We write the equalities as equalities of sigma types, as these contain more information than
+`heq`; `(xi : carriers i) == (xj : carriers j)` does not tell us that `i = j`. -/
+
+private lemma hone_hmul {i} (a : carriers i) :
+  (⟨_, hmul carriers (hone carriers) a⟩ : Σ i, carriers i) = ⟨i, a⟩ :=
 begin
+  have h := zero_add i,
+  congr,
+  exact h,
   rw subtype.heq_iff_coe_eq,
   { exact one_mul _ },
-  { exact λ x, (zero_add i).symm ▸ iff.rfl, }
+  { exact λ x, h.symm ▸ iff.rfl, }
 end
 
-lemma hmul_hone {i} (a : carriers i) : hmul carriers a (hone carriers) == a :=
+private lemma hmul_hone {i} (a : carriers i) :
+  (⟨_, hmul carriers a (hone carriers)⟩ : Σ i, carriers i) = ⟨_, a⟩ :=
 begin
+  have h := add_zero i,
+  congr,
+  exact h,
   rw subtype.heq_iff_coe_eq,
   exact mul_one _,
-  exact λ x, (add_zero i).symm ▸ iff.rfl,
+  exact λ x, h.symm ▸ iff.rfl,
 end
 
-lemma hmul_assoc {i j k} (a : carriers i) (b : carriers j) (c : carriers k) :
-  hmul carriers (hmul carriers a b) c == hmul carriers a (hmul carriers b c) :=
+private lemma hmul_assoc {i j k} (a : carriers i) (b : carriers j) (c : carriers k) :
+  (⟨_, hmul carriers (hmul carriers a b) c⟩ : Σ i, carriers i) =
+  ⟨_, hmul carriers a (hmul carriers b c)⟩ :=
 begin
+  have h := add_assoc i j k,
+  congr,
+  exact h,
   rw subtype.heq_iff_coe_eq,
   exact mul_assoc _ _ _,
-  exact λ x, (add_assoc i j k).symm ▸ iff.rfl,
+  exact λ x, h.symm ▸ iff.rfl,
 end
 
-
-/-! Next, compose them with `direct_sum.of`. -/
-
-def mul_def' {i j} : carriers i →+ carriers j →+ ⨁ i, carriers i :=
-{ to_fun := λ a,
-    add_monoid_hom.comp_hom (direct_sum.of (λ i, carriers i) $ i + j) (hmul carriers a),
-  map_add' := λ a b, by simp only [add_monoid_hom.map_add],
-  map_zero' := by simp only [add_monoid_hom.map_zero] }
-
-def one_def' : ⨁ i, carriers i := direct_sum.of (λ i, carriers i) 0 (hone carriers)
+/-! Embed the heterogenous definitions above into `direct_sum`  -/
 
 instance : has_one (⨁ i, carriers i) :=
 { one := direct_sum.of (λ i, carriers i) 0 (hone carriers)}
 
 instance : has_mul (⨁ i, carriers i) :=
 { mul := λ a b, begin
+    -- the elaborator struggles here, so use tactics to assemble the expression
     refine direct_sum.to_add_monoid (λ j,
       direct_sum.to_add_monoid (λ i,
         _
       ) a
     ) b,
-    exact mul_def' carriers,
+    exact { to_fun := λ a,
+      add_monoid_hom.comp_hom (direct_sum.of (λ i, carriers i) $ i + j) (hmul carriers a),
+      map_add' := λ a b, by simp only [add_monoid_hom.map_add],
+      map_zero' := by simp only [add_monoid_hom.map_zero] },
   end }
 
-/-! Now the fun begins! -/
+/-! The various semiring fields are proved separately because the proofs are slow. -/
 
-lemma one_mul (x : ⨁ i, carriers i) : 1 * x = x :=
+private lemma one_mul (x : ⨁ i, carriers i) : 1 * x = x :=
 begin
-  unfold has_one.one has_mul.mul one_def' mul_def',
+  unfold has_one.one has_mul.mul,
   simp only [add_monoid_hom.coe_mk, to_add_monoid_of, add_monoid_hom.comp_hom_apply_apply],
-  rw [direct_sum.to_add_monoid, dfinsupp.lift_add_hom_apply],
-  haveI : Π (i : ι) (x : (λ i, ↥(carriers i)) i), decidable (x ≠ 0) := λ i x, classical.dec _,
-  rw @dfinsupp.sum_add_hom_apply ι (λ i, ↥(carriers i)) _ _ _ ‹_›,
-  simp [add_monoid_hom.coe_comp, function.comp, direct_sum.of],
-  convert dfinsupp.sum_single,
+  simp only [direct_sum.to_add_monoid, dfinsupp.lift_add_hom_apply, direct_sum.of],
+  convert add_monoid_hom.congr_fun (dfinsupp.sum_add_hom_single_add_hom) x,
   ext1 i, ext1 xi,
-  congr,
-  exact zero_add _,
-  exact hone_hmul _ xi,
-  assumption
+  exact dfinsupp.single_eq_of_sigma_eq (hone_hmul _ xi),
 end
 
-@[simp,to_additive]
-lemma monoid_hom.coe_dfinsupp_prod' {β : ι → Type*} {R S : Type*} [monoid R] [comm_monoid S]
-  [Π (i : ι), has_zero (β i)]
-  {_ : Π (i : ι) (x : β i), decidable (x ≠ 0)}
-  (f : Π₀ i, β i) (g : Π i, β i → R →* S) :
-  ⇑(f.prod g) = f.prod (λ a b, (g a b)) :=
-@monoid_hom.coe_dfinsupp_prod ι β _ _ _ _ _ _ _ f g
-
-lemma mul_one (x : ⨁ i, carriers i) : x * 1 = x :=
+private lemma mul_one (x : ⨁ i, carriers i) : x * 1 = x :=
 begin
-  unfold has_one.one has_mul.mul one_def' mul_def',
+  unfold has_one.one has_mul.mul,
   simp only [add_monoid_hom.coe_mk, to_add_monoid_of, add_monoid_hom.comp_hom_apply_apply],
-  rw [direct_sum.to_add_monoid, dfinsupp.lift_add_hom_apply],
-  haveI : Π (i : ι) (x : (λ i, ↥(carriers i)) i), decidable (x ≠ 0) := λ i x, classical.dec _,
-  -- haveI : Π (i : ι) (x : carriers i), decidable (x ≠ 0) := λ i x, classical.dec _,
-  rw @dfinsupp.sum_add_hom_apply ι (λ i, ↥(carriers i)) _ _ _ ‹_›,
-  simp [add_monoid_hom.coe_comp, function.comp, direct_sum.of, dfinsupp.single_add_hom,
-    add_monoid_hom.coe_dfinsupp_sum],
-  convert dfinsupp.sum_single,
+  simp only [direct_sum.to_add_monoid, dfinsupp.lift_add_hom_apply, direct_sum.of],
+  rw add_monoid_hom.dfinsupp_sum_add_hom_apply x _,
+  convert add_monoid_hom.congr_fun (dfinsupp.sum_add_hom_single_add_hom ) x,
   ext1 i, ext1 xi,
-  congr,
-  -- simp [add_monoid_hom.dfinsupp_sum_apply],
-  -- ext1 i, ext1 xi,
-  -- congr,
-  -- exact zero_add _,
-  -- have := hone_hmul _ xi,
-  -- exact this,
-  -- assumption
+  simp [dfinsupp.single_eq_of_sigma_eq (hmul_hone _ xi)],
 end
 
+private lemma mul_assoc (a b c : ⨁ i, carriers i) : a * b * c = a * (b * c) :=
+begin
+  unfold has_one.one has_mul.mul,
+  simp only [add_monoid_hom.coe_mk, to_add_monoid_of, add_monoid_hom.comp_hom_apply_apply],
+  simp only [direct_sum.to_add_monoid, dfinsupp.lift_add_hom_apply, direct_sum.of],
+  simp only [←add_monoid_hom.comp_apply],
+  simp only [dfinsupp.sum_add_hom_comp_sum_add_hom],
 
-#print direct_sum.has_mul
+  -- unpack c
+  refine add_monoid_hom.congr_fun _ c,
+  congr' 1, ext1 ci, ext1 cx,
 
-#check dfinsupp.sum
+  erw add_monoid_hom.dfinsupp_sum_add_hom_apply,
+  rw add_monoid_hom.comp_apply,
+  erw add_monoid_hom.dfinsupp_sum_add_hom_apply,
+  rw ←add_monoid_hom.comp_apply,
+  erw dfinsupp.sum_add_hom_comp_sum_add_hom,
+
+  -- unpack b
+  refine add_monoid_hom.congr_fun _ b,
+  congr' 1, ext1 bi, ext1 bx,
+  simp only [add_monoid_hom.comp_apply, add_monoid_hom.eval_apply, add_monoid_hom.coe_mk],
+  erw add_monoid_hom.dfinsupp_sum_add_hom_apply,
+  erw add_monoid_hom.dfinsupp_sum_add_hom_apply,
+
+  simp only [add_monoid_hom.map_dfinsupp_sum_add_hom, dfinsupp.single_add_hom_apply, dfinsupp.sum_add_hom_single],
+  erw add_monoid_hom.dfinsupp_sum_add_hom_apply,
+
+  -- unpack a
+  refine add_monoid_hom.congr_fun _ a,
+  congr' 1, ext1 ai, ext1 ax,
+
+  simp [dfinsupp.single_eq_of_sigma_eq (hmul_assoc _ ax bx cx)],
+end
+
+private lemma zero_mul (x : ⨁ i, carriers i) : 0 * x = 0 :=
+begin
+  unfold has_mul.mul,
+  simp [direct_sum.to_add_monoid, direct_sum.of],
+end
+
+private lemma mul_zero (x : ⨁ i, carriers i) : x * 0 = 0 :=
+begin
+  unfold has_mul.mul,
+  simp,
+end
+
+private lemma left_distrib (a b c : ⨁ i, carriers i) : a * (b + c) = a * b + a * c :=
+begin
+  unfold has_mul.mul,
+  simp,
+end
+
+private lemma right_distrib (a b c : ⨁ i, carriers i) : (a + b) * c = a * c + b * c :=
+begin
+  unfold has_mul.mul,
+  simp [direct_sum.to_add_monoid, direct_sum.of],
+end
+
+/-- The ring structure on `⨁ i, carriers i` in the presence of -/
+instance : semiring (⨁ i, carriers i) := {
+  one := 1,
+  mul := (*),
+  one_mul := one_mul carriers,
+  mul_one := mul_one carriers,
+  mul_assoc := mul_assoc carriers,
+  zero_mul := zero_mul carriers,
+  mul_zero := mul_zero carriers,
+  left_distrib := left_distrib carriers,
+  right_distrib := right_distrib carriers,
+  ..direct_sum.add_comm_monoid _ _, }
 
 end direct_sum

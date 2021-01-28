c00ff53a17ddfbdbf2901696f934d6d3a8b51a13
Ashvni <ashvni.n@gmail.com>
2021-01-28 15:46:51 +0000

    Added intro

--- src/algebraic_number_theory/number_field.lean
+++ src/algebraic_number_theory/number_field.lean
@@ -1,3 +1,9 @@
+/-
+Copyright (c) 2020 Ashvni Narayanan. All rights reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
+Author: Ashvni Narayanan
+-/
+
 import algebra.field
 import algebraic_number_theory.class_number
 import data.rat.basic
@@ -5,6 +11,41 @@ import ring_theory.algebraic
 import ring_theory.dedekind_domain
 import ring_theory.integral_closure
 
+/-!
+# Number fields
+
+This file defines a number field and the ring of integers corresponding to it.
+
+## Main definitions
+
+ - `is_number_field` defines a number field as a field which has characteristic zero and is finite
+    dimensional over ℚ.
+ - `ring_of_integers` defines the ring of integers (or number ring) corresponding to a number field
+    as the integral closure of ℤ in the number field.
+
+## Main results
+
+ - `is_dedekind_domain_of_ring_of_integers`: Shows that the ring of integers of a number field is a
+    Dedekind domain.
+
+## Implementation notes
+
+The definitions that involve a field of fractions choose a canonical field of fractions,
+but are independent of that choice.
+
+## References
+
+* [D. Marcus, *Number Fields*][marcus1977number]
+* [J.W.S. Cassels, A. Frölich, *Algebraic Number Theory*][cassels1967algebraic]
+* [P. Samuel, *Algebraic Theory of Numbers*][samuel1970algebraic]
+
+## Tags
+
+number field, ring of integers
+-/
+
+/-- A number field is a field which has characteristic zero and is finite
+dimensional over ℚ. -/
 class is_number_field (K : Type*) [field K] : Prop :=
 [cz : char_zero K]
 [fd : finite_dimensional ℚ K]
@@ -15,18 +56,18 @@ open_locale classical big_operators
 namespace number_field
 variables (K : Type*) [field K] [is_number_field K]
 
- instance is_field_of_number_field : field K := by apply_instance
+instance is_field_of_number_field : field K := by apply_instance
 
- instance char_zero_of_number_field : char_zero K := is_number_field.cz
+instance char_zero_of_number_field : char_zero K := is_number_field.cz
 
- lemma finite_dimensional_of_number_field : finite_dimensional ℚ K := is_number_field.fd
+lemma finite_dimensional_of_number_field : finite_dimensional ℚ K := is_number_field.fd
 
- instance algebra_of_number_field : algebra ℚ K := rat.algebra_rat
+instance algebra_of_number_field : algebra ℚ K := rat.algebra_rat
 
 lemma is_algebraic_of_number_field : algebra.is_algebraic ℚ K :=
   @algebra.is_algebraic_of_finite ℚ K _ _ _ (finite_dimensional_of_number_field K)
 
- instance infinite_of_number_field : infinite K := begin
+instance infinite_of_number_field : infinite K := begin
   let f : ℤ → K := (λ n, (n : ℚ) • (1 : K)),
   apply infinite.of_injective f,
   intros x y hxy,
@@ -42,6 +83,8 @@ lemma is_algebraic_of_number_field : algebra.is_algebraic ℚ K :=
   {exfalso, revert h2, simp}
 end
 
+/-- The ring of integers (or number ring) corresponding to a number field
+  is the integral closure of ℤ in the number field. -/
 def ring_of_integers := @integral_closure ℤ K _
 
 namespace ring_of_integers
@@ -74,7 +117,7 @@ example (K : Type) [field K] (x : K) (h : x ≠ 0): x * (field.inv x) = 1 := fie
 instance integral_closure_int.is_dedekind_domain : is_dedekind_domain (integral_closure ℤ K) :=
 is_dedekind_domain.integral_closure int.fraction_map (principal_ideal_ring.is_dedekind_domain _)
 
-instance : is_dedekind_domain (ring_of_integers K) :=
+instance is_dedekind_domain_of_ring_of_integers : is_dedekind_domain (ring_of_integers K) :=
 integral_closure_int.is_dedekind_domain K
 
 noncomputable instance : fintype (class_group (ring_of_integers.fraction_map K)) :=

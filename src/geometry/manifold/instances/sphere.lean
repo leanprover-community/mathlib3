a57e4f7ce966b59b7e81927e8cdde39c1ef4184b
Heather Macbeth <25316162+hrmacbeth@users.noreply.github.com>
2021-01-29 18:43:11 -0500

    unnecessary import

--- src/geometry/manifold/instances/sphere.lean
+++ src/geometry/manifold/instances/sphere.lean
@@ -4,7 +4,6 @@ Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Heather Macbeth
 -/
 import geometry.manifold.instances.real
-import analysis.normed_space.inner_product
 
 /-!
 # Manifold structure on the sphere

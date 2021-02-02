77a2fe60effe620531ba3c6ede7cb1497e921f74
Benjamin Davidson <68528197+benjamindavidson@users.noreply.github.com>
2021-02-02 01:53:19 -0500

    update measure theory (will break)

--- archive/100-theorems-list/9_area_of_a_circle.lean
+++ archive/100-theorems-list/9_area_of_a_circle.lean
@@ -12,16 +12,31 @@ import analysis.mean_inequalities
 
 The area of a disc with radius `r` is `π * r^2`.
 
-Skip to line 107.
+Skip to line 151.
 -/
 
-open set interval_integral real measure_theory
+open set interval_integral real measure_theory filter
 open_locale classical
 variables {α : Type*} [measurable_space α] {μ : measure α} [sigma_finite μ]
-  {f g : α → ℝ} {s : set α}
+  {f g : α → ℝ} {s t : set α}
+
+lemma measure.eq_restrict_of_measurable_subset (ht : is_measurable t) (t_subset : t ⊆ s) :
+  μ t = μ.restrict s t :=
+by rw [measure.restrict_apply ht, set.inter_eq_self_of_subset_left t_subset]
+
+lemma measure.restrict_apply' (hs : is_measurable s) : μ.restrict s t = μ (t ∩ s) :=
+by rw [← coe_to_outer_measure, measure.restrict_to_outer_measure_eq_to_outer_measure_restrict hs,
+      outer_measure.restrict_apply s t _, coe_to_outer_measure]
+
+lemma measure.eq_restrict_of_subset_of_measurable (hs : is_measurable s) (t_subset : t ⊆ s) :
+  μ t = μ.restrict s t :=
+by rwa [measure.restrict_apply', set.inter_eq_self_of_subset_left t_subset]
 
 def region_between (f g : α → ℝ) (s : set α) : set (α × ℝ) :=
 {p : α × ℝ | p.1 ∈ s ∧ p.2 ∈ Ioo (f p.1) (g p.1)}
+
+lemma region_between_subset (f g : α → ℝ) (s : set α) : region_between f g s ⊆ s.prod univ :=
+by simpa only [prod_univ, region_between, set.preimage, set_of_subset_set_of] using λ a, and.left
 
 lemma is_measurable_region_between (hf : measurable f) (hg: measurable g) (hs : is_measurable s) :
   is_measurable (region_between f g s) :=
@@ -33,7 +48,7 @@ begin
   simp only [and_true, mem_univ],
 end
 
-theorem volume_region_between_eq_lintegral
+theorem volume_region_between_eq_lintegral'
   (hf : measurable f) (hg : measurable g) (hs : is_measurable s) :
   μ.prod volume (region_between f g s) = ∫⁻ y in s, ennreal.of_real ((g - f) y) ∂μ :=
 begin
@@ -53,20 +68,49 @@ begin
   { exact is_measurable_region_between hf hg hs },
 end
 
+theorem volume_region_between_eq_lintegral
+  (hf : ae_measurable f (μ.restrict s)) (hg : ae_measurable g (μ.restrict s))
+  (hs : is_measurable s) :
+  μ.prod volume (region_between f g s) = ∫⁻ y in s, ennreal.of_real ((g - f) y) ∂μ :=
+begin
+  have h₁ : (λ y, ennreal.of_real ((g - f) y))
+          =ᵐ[μ.restrict s]
+              λ y, ennreal.of_real ((ae_measurable.mk g hg - ae_measurable.mk f hf) y) :=
+    eventually_eq.fun_comp (eventually_eq.sub hg.ae_eq_mk hf.ae_eq_mk) _,
+  have h₂ : (μ.restrict s).prod volume (region_between f g s) =
+    (μ.restrict s).prod volume (region_between (ae_measurable.mk f hf) (ae_measurable.mk g hg) s),
+  { apply measure_congr,
+    apply eventually_eq.inter, { refl },
+    exact eventually_eq.inter
+            (eventually_eq.comp₂ (ae_eq_comp' measurable_fst hf.ae_eq_mk
+              measure.prod_fst_absolutely_continuous) _ eventually_eq.rfl)
+            (eventually_eq.comp₂ eventually_eq.rfl _
+              (ae_eq_comp' measurable_fst hg.ae_eq_mk measure.prod_fst_absolutely_continuous)) },
+  rw [lintegral_congr_ae h₁,
+      ← volume_region_between_eq_lintegral' hf.measurable_mk hg.measurable_mk hs],
+  convert h₂ using 1,
+  { rw measure.restrict_prod_eq_prod_univ,
+    exacts [measure.eq_restrict_of_subset_of_measurable (hs.prod is_measurable.univ)
+      (region_between_subset f g s), hs] },
+  { rw measure.restrict_prod_eq_prod_univ,
+    exacts [measure.eq_restrict_of_subset_of_measurable (hs.prod is_measurable.univ)
+      (region_between_subset (ae_measurable.mk f hf) (ae_measurable.mk g hg) s), hs] },
+end
+
 theorem volume_region_between_eq_integral
-  (f_int : integrable_on f s μ) (g_int : integrable_on g s μ)
-  (f_meas : measurable f) (g_meas : measurable g) (hs : is_measurable s)
-  (hfg : ∀ x ∈ s, f x ≤ g x) :
+  (hf : integrable_on f s μ) (hg : integrable_on g s μ)
+  (hs : is_measurable s) (hfg : ∀ x ∈ s, f x ≤ g x) :
   μ.prod volume (region_between f g s) = ennreal.of_real (∫ y in s, (g - f) y ∂μ) :=
 begin
   have h : g - f =ᵐ[μ.restrict s] λ y, (λ x, nnreal.of_real (g x - f x)) y,
-  { rw filter.eventually_eq_iff_exists_mem,
+  { rw eventually_eq_iff_exists_mem,
     use s,
     simpa only [measure.ae, mem_set_of_eq, filter.mem_mk, measure.restrict_apply hs.compl,
                 measure_empty, compl_inter_self, eq_self_iff_true, true_and] using
       λ x hx, (nnreal.coe_of_real _ (sub_nonneg.mpr (hfg x hx))).symm },
-  rw [volume_region_between_eq_lintegral, integral_congr_ae h, lintegral_congr_ae,
-      lintegral_coe_eq_integral _ ((integrable_congr h).mp (g_int.sub f_int))];
+  rw [volume_region_between_eq_lintegral hf.ae_measurable hg.ae_measurable hs,
+      integral_congr_ae h, lintegral_congr_ae,
+      lintegral_coe_eq_integral _ ((integrable_congr h).mp (hg.sub hf))],
   simpa only,
 end
 

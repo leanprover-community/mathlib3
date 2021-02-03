e70bb3465f0ad460813b2d05820c2dba87498a65
adomani <adomani@gmail.com>
2021-02-03 17:58:15 +0100

    add two lemmas in data.nat.factorial

--- src/data/nat/factorial.lean
+++ src/data/nat/factorial.lean
@@ -96,4 +96,22 @@ lemma self_le_factorial : ∀ n : ℕ, n ≤ n!
 | 0 := zero_le_one
 | (k+1) := le_mul_of_one_le_right k.zero_lt_succ.le (nat.one_le_of_lt $ nat.factorial_pos _)
 
+lemma lt_factorial_self {n : ℕ} (hi : 3 ≤ n) : n < n! :=
+begin
+  rw [← succ_pred_eq_of_pos (lt_of_lt_of_le (zero_lt_two.trans (lt.base 2)) hi), factorial_succ],
+  exact lt_mul_of_one_lt_right ((pred n).succ_pos) (lt_of_lt_of_le (lt_of_lt_of_le one_lt_two
+    (le_pred_of_lt (succ_le_iff.mp hi))) (self_le_factorial _)),
+end
+
+lemma add_factorial_lt_factorial_add {i : ℕ} (n : ℕ) (hi : 2 ≤ i) :
+  i + n.succ! < (i + n.succ)! :=
+begin
+  rw [succ_eq_add_one, ← add_assoc, factorial_succ (i + _), succ_eq_add_one, add_mul, one_mul],
+  have : i ≤ i + n := le.intro rfl,
+  exact add_lt_add_of_lt_of_le (lt_of_le_of_lt this ((lt_mul_iff_one_lt_right (lt_of_lt_of_le
+    zero_lt_two (hi.trans this))).mpr (lt_iff_le_and_ne.mpr ⟨(i + n).factorial_pos, λ g,
+    nat.not_succ_le_self 1 ((hi.trans this).trans (factorial_eq_one.mp g.symm))⟩))) (factorial_le
+    ((le_of_eq (add_comm n 1)).trans ((add_le_add_iff_right n).mpr (one_le_two.trans hi)))),
+end
+
 end nat

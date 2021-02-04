08122697c4e8a627d606b91395b09a1428dda1dd
Floris van Doorn <fpvdoorn@gmail.com>
2021-02-04 00:51:58 -0500

    missing open_locale

--- src/analysis/normed_space/basic.lean
+++ src/analysis/normed_space/basic.lean
@@ -15,7 +15,7 @@ variables {α : Type*} {β : Type*} {γ : Type*} {ι : Type*}
 
 noncomputable theory
 open filter metric
-open_locale topological_space big_operators nnreal
+open_locale topological_space big_operators nnreal ennreal
 
 /-- Auxiliary class, endowing a type `α` with a function `norm : α → ℝ`. This class is designed to
 be extended in more interesting classes specifying the properties of the norm. -/

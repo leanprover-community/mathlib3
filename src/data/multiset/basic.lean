5966e89b983a8a96d912a1676e3dde567f6f8e9c
faenuccio <filippo.nuccio@univ-st-etienne.fr>
2021-02-05 13:40:39 +0100

    added classical to exists_minimal_subset

--- src/data/multiset/basic.lean
+++ src/data/multiset/basic.lean
@@ -1544,9 +1544,10 @@ lemma nat.well_ordered_of_exists {s : set ℕ} [decidable_pred (λ w, w ∈ s)] 
 ⟨nat.find ex, nat.find_spec ex, λ m, nat.find_min' ex⟩
 
 lemma exists_minimal_subset {p : multiset α → Prop} {s₀ : multiset α} (H : p s₀)
-  [decidable_pred (λ w, w ∈ {i | ∃ u, u ≤ s₀ ∧ p u ∧ u.card = i})] :
-  ∃ s ≤ s₀, p s ∧ (∀ s' ≤ s, p s' → s' = s) :=
+  --[decidable_pred (λ w, w ∈ {i | ∃ u, u ≤ s₀ ∧ p u ∧ u.card = i})] :
+  : ∃ s ≤ s₀, p s ∧ (∀ s' ≤ s, p s' → s' = s) :=
 begin
+  classical,
   obtain ⟨n, ⟨sm, sm0, ps, sc⟩, F⟩ := nat.well_ordered_of_exists
     (⟨_, _, le_rfl, H, rfl⟩ : ∃ w : ℕ, w ∈ { i | ∃ u, u ≤ s₀ ∧ p u ∧ u.card = i }),
   exact ⟨sm, sm0, ps, λ t tsm pt, eq_of_le_of_card_le tsm

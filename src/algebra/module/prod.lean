e64767cbc6cd07638af590f907d0ff99aebb1b95
Eric Wieser <wieser.eric@gmail.com>
2021-02-05 12:21:58 +0000

    chore(algebra/module/prod): add an smul_comm_class instance

--- src/algebra/module/prod.lean
+++ src/algebra/module/prod.lean
@@ -10,21 +10,21 @@ import algebra.module.basic
 This file defines instances for binary product of modules
 -/
 
-variables {α : Type*} {β : Type*} {γ : Type*} {δ : Type*} {p q : α × β}
+variables {R : Type*} {S : Type*} {M : Type*} {N : Type*}
 
 namespace prod
 
-instance [has_scalar α β] [has_scalar α γ] : has_scalar α (β × γ) := ⟨λa p, (a • p.1, a • p.2)⟩
+instance [has_scalar R M] [has_scalar R N] : has_scalar R (M × N) := ⟨λa p, (a • p.1, a • p.2)⟩
 
-@[simp] theorem smul_fst [has_scalar α β] [has_scalar α γ]
-  (a : α) (x : β × γ) : (a • x).1 = a • x.1 := rfl
-@[simp] theorem smul_snd [has_scalar α β] [has_scalar α γ]
-  (a : α) (x : β × γ) : (a • x).2 = a • x.2 := rfl
-@[simp] theorem smul_mk [has_scalar α β] [has_scalar α γ]
-  (a : α) (b : β) (c : γ) : a • (b, c) = (a • b, a • c) := rfl
+@[simp] theorem smul_fst [has_scalar R M] [has_scalar R N]
+  (a : R) (x : M × N) : (a • x).1 = a • x.1 := rfl
+@[simp] theorem smul_snd [has_scalar R M] [has_scalar R N]
+  (a : R) (x : M × N) : (a • x).2 = a • x.2 := rfl
+@[simp] theorem smul_mk [has_scalar R M] [has_scalar R N]
+  (a : R) (b : M) (c : N) : a • (b, c) = (a • b, a • c) := rfl
 
-instance {r : semiring α} [add_comm_monoid β] [add_comm_monoid γ]
-  [semimodule α β] [semimodule α γ] : semimodule α (β × γ) :=
+instance {r : semiring R} [add_comm_monoid M] [add_comm_monoid N]
+  [semimodule R M] [semimodule R N] : semimodule R (M × N) :=
 { smul_add  := assume a p₁ p₂, mk.inj_iff.mpr ⟨smul_add _ _ _, smul_add _ _ _⟩,
   add_smul  := assume a p₁ p₂, mk.inj_iff.mpr ⟨add_smul _ _ _, add_smul _ _ _⟩,
   mul_smul  := assume a₁ a₂ p, mk.inj_iff.mpr ⟨mul_smul _ _ _, mul_smul _ _ _⟩,
@@ -32,5 +32,11 @@ instance {r : semiring α} [add_comm_monoid β] [add_comm_monoid γ]
   zero_smul := assume ⟨b, c⟩, mk.inj_iff.mpr ⟨zero_smul _ _, zero_smul _ _⟩,
   smul_zero := assume a, mk.inj_iff.mpr ⟨smul_zero _, smul_zero _⟩,
   .. prod.has_scalar }
+
+instance {r : semiring R} {s : semiring S} [add_comm_monoid M] [add_comm_monoid N]
+  [semimodule R M] [semimodule R N] [semimodule S M] [semimodule S N]
+  [smul_comm_class R S M] [smul_comm_class R S N] :
+  smul_comm_class R S (M × N) :=
+{ smul_comm := λ r s x, mk.inj_iff.mpr ⟨smul_comm _ _ _, smul_comm _ _ _⟩ }
 
 end prod

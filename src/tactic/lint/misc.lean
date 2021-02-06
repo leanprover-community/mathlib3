a9f76c09606aab0a8695a46a46d5f3d632a5dfe1
Mario Carneiro <di.gama@gmail.com>
2021-02-06 15:26:15 -0500

    move linter to type_classes.lean

--- src/tactic/lint/misc.lean
+++ src/tactic/lint/misc.lean
@@ -15,7 +15,6 @@ This file defines several small linters:
   - `doc_blame` checks that every definition has a documentation string
   - `doc_blame_thm` checks that every theorem has a documentation string (not enabled by default)
   - `def_lemma` checks that a declaration is a lemma iff its type is a proposition.
-  - `class_structure` checks that every `class` is a structure, i.e. `@[class] def` is forbidden.
 -/
 
 open tactic expr
@@ -244,23 +243,3 @@ has been used. -/
   errors_found := "INCORRECT DEF/LEMMA" }
 
 attribute [nolint def_lemma] classical.dec classical.dec_pred classical.dec_rel classical.dec_eq
-
-
-
-/-!
-## Linter for `@[class] def`
--/
-
-/-- A linter for checking that all uses of the `@[class]` attribute apply to structures or
-inductive types. -/
-@[linter] meta def linter.class_structure : linter :=
-{ test := λ d, (do
-    is_class ← has_attribute' `class d.to_name,
-    if is_class then do
-      env ← get_env,
-      pure $ if env.is_inductive d.to_name then none else
-        "is a non-structure or inductive type marked @[class]"
-    else pure none),
-  auto_decls := tt,
-  no_errors_found := "All classes are structures",
-  errors_found := "USE OF @[class] def IS DISALLOWED" }

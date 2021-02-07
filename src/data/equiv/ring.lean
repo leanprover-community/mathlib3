3cf247461b990920c3e7d2051b970f358d77911f
Kevin Buzzard <k.buzzard@imperial.ac.uk>
2021-02-07 14:06:16 +0000

    add uniqueness proofs

--- src/data/equiv/ring.lean
+++ src/data/equiv/ring.lean
@@ -98,6 +98,17 @@ instance has_coe_to_add_equiv : has_coe (R ≃+* S) (R ≃+ S) := ⟨ring_equiv.
 @[norm_cast] lemma coe_add_equiv (f : R ≃+* S) (a : R) :
   (f : R ≃+ S) a = f a := rfl
 
+/-- The `ring_equiv` between two semirings with a unique element. -/
+def ring_equiv_of_unique_of_unique {M N}
+  [unique M] [unique N] [has_add M] [has_mul M] [has_add N] [has_mul N] : M ≃+* N :=
+{ ..add_equiv.add_equiv_of_unique_of_unique,
+  ..mul_equiv.mul_equiv_of_unique_of_unique}
+
+instance {M N} [unique M] [unique N] [has_add M] [has_mul M] [has_add N] [has_mul N] :
+  unique (M ≃+* N) :=
+{ default := ring_equiv_of_unique_of_unique,
+  uniq := λ _, ext $ λ x, subsingleton.elim _ _}
+
 variable (R)
 
 /-- The identity map is a ring isomorphism. -/
@@ -143,12 +154,6 @@ protected lemma surjective (e : R ≃+* S) : function.surjective e := e.to_equiv
 
 lemma image_eq_preimage (e : R ≃+* S) (s : set R) : e '' s = e.symm ⁻¹' s :=
 e.to_equiv.image_eq_preimage s
-
-/-- The `ring_equiv` between two semiring with a unique element. -/
-def ring_equiv_of_unique_of_unique {M N}
-  [unique M] [unique N] [has_add M] [has_mul M] [has_add N] [has_mul N] : M ≃+* N :=
-{ ..add_equiv.add_equiv_of_unique_of_unique,
-  ..mul_equiv.mul_equiv_of_unique_of_unique}
 
 end basic
 

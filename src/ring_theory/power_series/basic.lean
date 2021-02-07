8048e063e83f465b7bf13511d103aa4bbc99975d
laughinggas <58670661+laughinggas@users.noreply.github.com>
2021-02-07 21:44:22 +0000

    Update src/ring_theory/power_series/basic.lean
    Co-authored-by: Eric Wieser <wieser.eric@gmail.com>

--- src/ring_theory/power_series/basic.lean
+++ src/ring_theory/power_series/basic.lean
@@ -1020,10 +1020,13 @@ noncomputable def eval_mul_hom (a : R) : power_series R →+* power_series R :=
   map_one' := by { ext1, simp only [mul_boole, power_series.coeff_mk, power_series.coeff_one],
                 split_ifs, { rw [h, pow_zero], }, refl, },
   map_add' := by {intros, ext, exact mul_add _ _ _, },
-  map_mul' := by {intros, ext, rw [power_series.coeff_mul, power_series.coeff_mk,
-              power_series.coeff_mul, finset.mul_sum], apply sum_congr rfl,
-              simp only [coeff_mk, prod.forall, nat.mem_antidiagonal],
-              intros b c H, rw [<-H, pow_add, mul_mul_mul_comm] }, }
+  map_mul' := λ f g, by {
+    ext,
+    rw [power_series.coeff_mul, power_series.coeff_mk, power_series.coeff_mul, finset.mul_sum],
+    apply sum_congr rfl,
+    simp only [coeff_mk, prod.forall, nat.mem_antidiagonal],
+    intros b c H,
+    rw [<-H, pow_add, mul_mul_mul_comm] }, }
 
 @[simp] lemma coeff_eval_mul_hom (f : power_series R) (a : R) (n : ℕ) :
   coeff R n (eval_mul_hom a f) = a^n * coeff R n f := coeff_mk n _

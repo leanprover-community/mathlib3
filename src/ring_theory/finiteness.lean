246bb4f881bdad0e5bd9950faea77d9bfe4139d3
Riccardo Brasca <riccardo.brasca@gmail.com>
2021-02-08 18:46:57 +0100

    Update src/ring_theory/finiteness.lean
    Co-authored-by: Anne Baanen <Vierkantor@users.noreply.github.com>

--- src/ring_theory/finiteness.lean
+++ src/ring_theory/finiteness.lean
@@ -245,13 +245,9 @@ lemma quotient {I : ideal A} (h : submodule.fg I) (hfp : finitely_presented R A)
   finitely_presented R I.quotient :=
 begin
   obtain ⟨n, f, hf⟩ := hfp,
-  use [n, (ideal.quotient.mkₐ R I).comp f],
-  split,
+  refine ⟨n, (ideal.quotient.mkₐ R I).comp f, _, _⟩,
   { exact function.surjective.comp (ideal.quotient.mkₐ_surjective R I) hf.1 },
-  { have h : ((ideal.quotient.mkₐ R I).comp f).to_ring_hom =
-      (ideal.quotient.mkₐ R I).to_ring_hom.comp f.to_ring_hom := rfl,
-    rw h,
-    refine submodule.fg_of_ker_of_ring_hom_comp _ _ hf.2 _ hf.1,
+  { refine submodule.fg_of_ker_of_ring_hom_comp _ _ hf.2 _ hf.1,
     rwa ideal.quotient.mkₐ_ker R I }
 end
 

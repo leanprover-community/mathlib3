1373c9da65cbf7a9413d673db32f0f1b4d1acba1
Eric Wieser <wieser.eric@gmail.com>
2021-02-08 21:34:04 +0000

    fix import

--- src/linear_algebra/linear_pmap.lean
+++ src/linear_algebra/linear_pmap.lean
@@ -4,6 +4,7 @@ Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Yury Kudryashov
 -/
 import linear_algebra.basic
+import linear_algebra.prod
 
 /-!
 # Partially defined linear maps

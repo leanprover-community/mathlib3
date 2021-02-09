1332d0a58c25654aaa96d4da1319cb0880205d3e
Jesse Michael Han <39395247+jesse-michael-han@users.noreply.github.com>
2021-02-09 08:15:34 -0800

    Update src/data/set/intervals/basic.lean
    Co-authored-by: Bryan Gin-ge Chen <bryangingechen@gmail.com>

--- src/data/set/intervals/basic.lean
+++ src/data/set/intervals/basic.lean
@@ -801,11 +801,12 @@ subset.antisymm (λ x hx, hx.elim (λ hx, lt_of_le_of_lt hx h) and.right) Iio_su
 lemma Iio_union_Ioo' {c d : α} (h₁ : c < b) :
   Iio b ∪ Ioo c d = Iio (max b d) :=
 begin
-  ext x, cases lt_or_le x b with hba hba,
+  ext x,
+  cases lt_or_le x b with hba hba,
   { simp [hba, h₁] },
   { simp only [mem_Iio, mem_union_eq, mem_Ioo, lt_max_iff],
-    apply or_congr, refl, split, exact and.right,
-    exact λ h₂, ⟨lt_of_lt_of_le h₁ hba, h₂⟩}
+    refine or_congr iff.rfl ⟨and.right, _⟩,
+    exact λ h₂, ⟨h₁.trans_le hba, h₂⟩ },
 end
 
 lemma Iio_union_Ioo {c d : α} (h : min c d < b) :

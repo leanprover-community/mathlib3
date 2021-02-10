136352074c4fa565d4570717de8fcdcfce01e95d
Bhavik Mehta <bhavikmehta8@gmail.com>
2021-02-10 18:12:58 +0000

    fix

--- src/category_theory/limits/elements.lean
+++ src/category_theory/limits/elements.lean
@@ -3,25 +3,26 @@ Copyright (c) 2020 Bhavik Mehta. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Bhavik Mehta
 -/
-import category_theory.adjunction
 import category_theory.elements
 import category_theory.limits.functor_category
 import category_theory.limits.preserves.limits
 import category_theory.limits.shapes.terminal
-import category_theory.limits.types
 
 namespace category_theory
-open limits
+open limits opposite
 
-def elements.initial2 (A : C) : (coyoneda.obj (op A)).elements :=
+universes v u
+variables {C : Type u} [category.{v} C]
+
+def elements.initial (A : C) : (coyoneda.obj (op A)).elements :=
 ⟨A, 𝟙 _⟩
 
-def is_initial2 (A : C) : is_initial (elements.initial2 A) :=
+def is_initial (A : C) : is_initial (elements.initial A) :=
 { desc := λ s, ⟨s.X.2, category.id_comp _⟩,
   uniq' := λ s m w,
   begin
     simp_rw ← m.2,
-    dsimp [elements.initial2],
+    dsimp [elements.initial],
     simp,
   end }
 

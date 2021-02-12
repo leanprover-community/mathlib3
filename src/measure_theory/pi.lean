7fa8ab6dc65969a34bcfe275f123b29b4575a9b0
Floris van Doorn <fpvdoorn@gmail.com>
2021-02-12 16:52:28 -0500

    checkpoint

--- src/measure_theory/pi.lean
+++ src/measure_theory/pi.lean
@@ -44,12 +44,175 @@ finitary product measure
 -/
 
 noncomputable theory
-open function set measure_theory.outer_measure filter topological_space
+open function set measure_theory.outer_measure filter measurable_space
 open_locale classical big_operators topological_space ennreal
+
+variables {ι ι' : Type*} {α : ι → Type*}
+
+/-- Given one value over a unique, we get a dependent function. -/
+def unique_elim [unique ι] (x : α (default ι)) (i : ι) : α i :=
+by { rw [unique.eq_default i], exact x }
+
+@[simp] lemma unique_elim_default [unique ι] (x : α (default ι)) : unique_elim x (default ι) = x :=
+rfl
+
+lemma unique_elim_preimage [unique ι] (t : ∀ i, set (α i)) :
+  unique_elim ⁻¹'  pi univ t = t (default ι) :=
+by { ext, simp [unique.forall_iff] }
+
+lemma Pi_congr_left_symm_preimage (f : ι' ≃ ι) (s : set ι) (t : ∀ i, set (α i)) :
+  (f.Pi_congr_left α).symm ⁻¹' (f ⁻¹' s).pi (λ i', t $ f i') = s.pi t :=
+begin
+  ext, simp, sorry
+end
+
+lemma measurable_unique_elim [unique ι] [∀ i, measurable_space (α i)] :
+  measurable (unique_elim : α (default ι) → Π i, α i) :=
+by { simp_rw [measurable_pi_iff, unique.forall_iff, unique_elim_default], exact measurable_id }
+
+
+lemma measurable_set.univ_pi_fintype {δ} {π : δ → Type*} [∀ i, measurable_space (π i)] [fintype δ]
+  {t : Π i, set (π i)} (ht : ∀ i, measurable_set (t i)) : measurable_set (pi univ t) :=
+measurable_set.pi_fintype (λ i _, ht i)
+
+
+/- rename measurable_set.pi_univ -> measurable_set.univ_pi -/
+
+lemma function.surjective.Union_comp {α ι ι₂} {f : ι → ι₂}
+  (hf : function.surjective f) (g : ι₂ → set α) :
+  (⋃ x, g (f x)) = ⋃ y, g y :=
+hf.supr_comp g
+
+lemma function.surjective.Inter_comp {α ι ι₂} {f : ι → ι₂}
+  (hf : function.surjective f) (g : ι₂ → set α) :
+  (⋂ x, g (f x)) = ⋂ y, g y :=
+hf.infi_comp g
+
+lemma Union_congr {ι ι₂ α : Type*} {f : ι → set α} {g : ι₂ → set α} (h : ι → ι₂)
+  (h1 : function.surjective h) (h2 : ∀ x, g (h x) = f x) : (⋃ x, f x) = ⋃ y, g y :=
+supr_congr h h1 h2
+
+lemma Inter_congr {ι ι₂ α : Type*} {f : ι → set α} {g : ι₂ → set α} (h : ι → ι₂)
+  (h1 : function.surjective h) (h2 : ∀ x, g (h x) = f x) : (⋂ x, f x) = ⋂ y, g y :=
+infi_congr h h1 h2
+
+lemma Union_univ_pi {ι ι₂} {α : ι → Type*} (t : ∀ i, ι₂ → set (α i)) :
+  (⋃ (x : ι → ι₂), pi univ (λ i, t i (x i))) = pi univ (λ i, ⋃ (j : ι₂), t i j) :=
+by { ext, simp [classical.skolem] }
+
+/- rename Union_prod to Union_prod_const.
+Also prove Union_unpair using this or similar -/
+lemma Union_prod {ι α β} (s : ι → set α) (t : ι → set β) :
+  (⋃ (x : ι × ι), (s x.1).prod (t x.2)) = (⋃ (i : ι), s i).prod (⋃ (i : ι), t i) :=
+by { ext, simp }
+
+lemma surjective_decode_iget (α : Type*) [encodable α] [inhabited α] :
+  surjective (λ n, (encodable.decode α n).iget) :=
+λ x, ⟨encodable.encode x, by simp_rw [encodable.encodek]⟩
+
+lemma pred_update {α} {β : α → Type*} (P : ∀ ⦃a⦄, β a → Prop)
+  (f : Π a, β a) (a' : α) (v : β a') (a : α) :
+  P (update f a' v a) ↔ (a = a' ∧ P v) ∨ (a ≠ a' ∧ P (f a)) :=
+by { rw [update], split_ifs, { subst h, simp }, { rw [← ne.def] at h, simp [h] }}
+
+@[simp] lemma imp_and_neg_imp_iff (p q : Prop) [decidable p] : (p → q) ∧ (¬ p → q) ↔ q :=
+by { by_cases p; simp [h] }
+
+lemma eval_preimage {ι} {α : ι → Type*} {i : ι} {s : set (α i)} :
+  eval i ⁻¹' s = pi univ (update (λ i, univ) i s) :=
+by { ext x, simp [@forall_update_iff _ (λ i, set (α i)) _ _ _ _ (λ i' y, x i' ∈ y)] }
+
+lemma eval_preimage' {ι} {α : ι → Type*} {i : ι} {s : set (α i)} :
+  eval i ⁻¹' s = pi {i} (update (λ i, univ) i s) :=
+by { ext, simp }
+
+lemma mem_pi_univ {ι : Type*} {α : ι → Type*} (t : ∀ i, set (α i)) (x : ∀ i, α i) :
+  x ∈ pi univ t ↔ ∀ i, x i ∈ t i :=
+by simp
+
+lemma pi_univ_ite {ι} {α : ι → Type*} (s : set ι) (t : ∀ i, set (α i)) :
+  pi univ (λ i, if i ∈ s then t i else univ) = s.pi t :=
+by { ext, simp_rw [mem_pi], apply forall_congr, intro i, split_ifs; simp [h] }
+
+lemma pi_univ_eq_Inter {ι} {α : ι → Type*} (t : ∀ i, set (α i)) :
+  pi univ t = ⋂ i, eval i ⁻¹' t i :=
+by simp only [pi_def, Inter_pos, mem_univ]
+
+/-! We start with some measurability properties -/
+
+/-- Boxes formed by π-systems form a π-system. -/
+lemma is_pi_system.pi {C : ∀ i, set (set (α i))} (hC : ∀ i, is_pi_system (C i)) :
+  is_pi_system (pi univ '' pi univ C) :=
+begin
+  rintro _ _ ⟨s₁, hs₁, rfl⟩ ⟨s₂, hs₂, rfl⟩ hst,
+  rw [← pi_inter_distrib] at hst ⊢, rw [univ_pi_nonempty_iff] at hst,
+  exact mem_image_of_mem _ (λ i _, hC i _ _ (hs₁ i (mem_univ i)) (hs₂ i (mem_univ i)) (hst i))
+end
+
+variables [fintype ι] [fintype ι'] -- or encodable at the start
+/-- Boxes of countably spanning sets are countably spanning. -/
+lemma is_countably_spanning.pi {C : ∀ i, set (set (α i))}
+  (hC : ∀ i, is_countably_spanning (C i)) :
+  is_countably_spanning (pi univ '' pi univ C) :=
+begin
+  choose s h1s h2s using hC,
+  haveI := encodable.fintype.encodable ι,
+  let e : ℕ → (ι → ℕ) := λ n, (encodable.decode (ι → ℕ) n).iget,
+  refine ⟨λ n, pi univ (λ i, s i (e n i)), λ n, mem_image_of_mem _ (λ i _, h1s i _), _⟩,
+  simp_rw [(surjective_decode_iget (ι → ℕ)).Union_comp (λ x, pi univ (λ i, s i (x i))),
+      Union_univ_pi s, h2s, pi_univ]
+end
+
+/-- The product of generated σ-algebras is the one generated by boxes, if both generating sets
+  are countably spanning. -/
+lemma generate_from_pi_eq {C : ∀ i, set (set (α i))}
+  (hC : ∀ i, is_countably_spanning (C i)) :
+  @measurable_space.pi _ _ (λ i, generate_from (C i)) = generate_from (pi univ '' pi univ C) :=
+begin
+  haveI := encodable.fintype.encodable ι,
+  apply le_antisymm,
+  { refine supr_le _, intro i, rw [comap_generate_from],
+    apply generate_from_le, rintro _ ⟨s, hs, rfl⟩, dsimp,
+    choose t h1t h2t using hC,
+    simp_rw [eval_preimage, ← h2t],
+    rw [← @Union_const _ ℕ _ s],
+    have : (pi univ (update (λ (i' : ι), Union (t i')) i (⋃ (i' : ℕ), s))) =
+      (pi univ (λ k, ⋃ j : ℕ, @update ι (λ i', set (α i')) _ (λ i', t i' j) i s k)),
+    { ext, simp_rw [mem_univ_pi], apply forall_congr, intro i',
+      by_cases (i' = i), { subst h, simp }, { rw [← ne.def] at h, simp [h] }},
+    rw [this, ← Union_univ_pi],
+    apply measurable_set.Union,
+    intro n, apply measurable_set_generate_from,
+    apply mem_image_of_mem, intros j _, dsimp only,
+    by_cases h: j = i, subst h, rwa [update_same], rw [update_noteq h], apply h1t },
+  { apply generate_from_le, rintro _ ⟨s, hs, rfl⟩,
+    rw [pi_univ_eq_Inter], apply measurable_set.Inter, intro i, apply measurable_pi_apply,
+    exact measurable_set_generate_from (hs i (mem_univ i)) }
+end
+
+/-- If `C` and `D` generate the σ-algebras on `α` resp. `β`, then rectangles formed by `C` and `D`
+  generate the σ-algebra on `α × β`. -/
+lemma generate_from_eq_pi [h : ∀ i, measurable_space (α i)]
+  {C : ∀ i, set (set (α i))} (hC : ∀ i, generate_from (C i) = h i)
+  (h2C : ∀ i, is_countably_spanning (C i)) :
+    generate_from (pi univ '' pi univ C) = measurable_space.pi :=
+by rw [← funext hC, generate_from_pi_eq h2C]
+
+/-- The product σ-algebra is generated from boxes, i.e. `s.prod t` for sets `s : set α` and
+  `t : set β`. -/
+lemma generate_from_pi [∀ i, measurable_space (α i)] :
+  generate_from (pi univ '' pi univ (λ i, { s : set (α i) | measurable_set s})) =
+  measurable_space.pi :=
+generate_from_eq_pi (λ i, generate_from_measurable_set) (λ i, is_countably_spanning_measurable_set)
+
+/-- Boxes form a π-system. -/
+lemma is_pi_system_pi [∀ i, measurable_space (α i)] :
+  is_pi_system (pi univ '' pi univ (λ i, { s : set (α i) | measurable_set s})) :=
+is_pi_system.pi (λ i, is_pi_system_measurable_set)
 
 namespace measure_theory
 
-variables {ι ι' : Type*} [fintype ι] [fintype ι'] {α : ι → Type*} {m : Π i, outer_measure (α i)}
+variables {m : Π i, outer_measure (α i)}
 
 /-- An upper bound for the measure in a finite product space.
   It is defined to by taking the image of the set under all projections, and taking the product
@@ -212,9 +375,7 @@ end
 @[irreducible] protected def pi : measure (Π i, α i) :=
 to_measure (outer_measure.pi (λ i, (μ i).to_outer_measure)) (pi_caratheodory μ)
 
-variables [∀ i, sigma_finite (μ i)]
-
-lemma pi_pi (s : Π i, set (α i)) (hs : ∀ i, measurable_set (s i)) :
+lemma pi_pi [∀ i, sigma_finite (μ i)] (s : Π i, set (α i)) (hs : ∀ i, measurable_set (s i)) :
   measure.pi μ (pi univ s) = ∏ i, μ i (s i) :=
 begin
   refine le_antisymm _ _,
@@ -231,6 +392,107 @@ begin
     intros s hs,
     simp_rw [to_outer_measure_apply],
     exact pi'_pi_le μ }
+end
+
+variable {μ}
+
+/-- `μ.prod ν` has finite spanning sets in rectangles of finite spanning sets. -/
+def finite_spanning_sets_in.pi {C : ∀ i, set (set (α i))}
+  (hμ : ∀ i, (μ i).finite_spanning_sets_in (C i)) (hC : ∀ i (s ∈ C i), measurable_set s) :
+  (measure.pi μ).finite_spanning_sets_in (pi univ '' pi univ C) :=
+begin
+  haveI := λ i, (hμ i).sigma_finite (hC i),
+  haveI := encodable.fintype.encodable ι,
+  let e : ℕ → (ι → ℕ) := λ n, (encodable.decode (ι → ℕ) n).iget,
+  refine ⟨λ n, pi univ (λ i, (hμ i).set (e n i)), λ n, _, λ n, _, _⟩,
+  { refine mem_image_of_mem _ (λ i _, (hμ i).set_mem _) },
+  { simp_rw [pi_pi μ (λ i, (hμ i).set (e n i)) (λ i, hC i _ ((hμ i).set_mem _))],
+    exact ennreal.prod_lt_top (λ i _, (hμ i).finite _) },
+  { simp_rw [(surjective_decode_iget (ι → ℕ)).Union_comp (λ x, pi univ (λ i, (hμ i).set (x i))),
+      Union_univ_pi (λ i, (hμ i).set), (hμ _).spanning, pi_univ] }
+end
+
+/-- Measures on a finite product space are equal the product measure if they are equal on rectangles
+  with as sides sets that generate the corresponding σ-algebras. -/
+lemma pi_eq_generate_from {C : ∀ i, set (set (α i))}
+  (hC : ∀ i, generate_from (C i) = _inst_3 i)
+  (h2C : ∀ i, is_pi_system (C i))
+  (h3C : ∀ i, (μ i).finite_spanning_sets_in (C i))
+  {μν : measure (Π i, α i)}
+  (h₁ : ∀ s : ∀ i, set (α i), (∀ i, s i ∈ C i) → μν (pi univ s) = ∏ i, μ i (s i)) :
+    measure.pi μ = μν :=
+begin
+  have h4C : ∀ i (s : set (α i)), s ∈ C i → measurable_set s,
+  { intros i s hs, rw [← hC], exact measurable_set_generate_from hs },
+  refine (finite_spanning_sets_in.pi h3C h4C).ext
+    (generate_from_eq_pi hC (λ i, (h3C i).is_countably_spanning)).symm
+    (is_pi_system.pi h2C) _,
+  rintro _ ⟨s, hs, rfl⟩,
+  rw [mem_pi_univ] at hs,
+  haveI := λ i, (h3C i).sigma_finite (h4C i),
+  simp_rw [h₁ s hs, pi_pi μ s (λ i, h4C i _ (hs i))]
+end
+
+variables [∀ i, sigma_finite (μ i)]
+
+/-- Measures on a product space are equal to the product measure if they are equal on rectangles. -/
+lemma pi_eq {μ' : measure (Π i, α i)}
+  (h : ∀ s : ∀ i, set (α i), (∀ i, measurable_set (s i)) → μ' (pi univ s) = ∏ i, μ i (s i)) :
+  measure.pi μ = μ' :=
+pi_eq_generate_from (λ i, generate_from_measurable_set)
+  (λ i, is_pi_system_measurable_set)
+  (λ i, (μ i).to_finite_spanning_sets_in) h
+
+variable (μ)
+
+instance pi.sigma_finite : sigma_finite (measure.pi μ) :=
+⟨⟨(finite_spanning_sets_in.pi (λ i, (μ i).to_finite_spanning_sets_in) (λ _ _, id)).mono $
+  by { rintro _ ⟨s, hs, rfl⟩, exact measurable_set.pi_fintype hs }⟩⟩
+
+
+lemma pi_empty_left (h : ι → false) : measure.pi μ = dirac (λ i, (h i).elim) :=
+begin
+  apply pi_eq,
+  intros s hs,
+  rw [dirac_apply' _ (measurable_set.univ_pi_fintype hs)], rw [indicator_of_mem],
+  { convert finset.prod_empty.symm, ext i, exact (h i).elim },
+  { intro i, exact (h i).elim }
+end
+
+lemma pi_unique_left [unique ι] : measure.pi μ = map unique_elim (μ (default ι)) :=
+begin
+  apply pi_eq, intros s hs,
+  rw [map_apply measurable_unique_elim (measurable_set.univ_pi_fintype hs), unique_elim_preimage],
+  symmetry, convert finset.prod_singleton, rw [finset.ext_iff, unique.forall_iff], simp
+end
+
+open sum
+/--  The type of dependent functions on a sum type `ι ⊕ ι'` is equivalent to the type of pairs of
+  functions on `ι` and on `ι'`. This is a dependent version of `equiv.sum_arrow_equiv_prod_arrow`. -/
+def equiv.Pi_sum (π : ι ⊕ ι' → Type*) : ((Π i, π (inl i)) × (Π i', π (inr i'))) ≃ Π i, π i :=
+{ to_fun := λ f, sum.rec f.1 f.2,
+  inv_fun := λ g, ⟨λ i, g (inl i), λ i', g (inr i')⟩,
+  left_inv := λ f, prod.ext rfl rfl,
+  right_inv := λ g, by { ext (i|i); refl } }
+
+def equiv.Pi_sum' (π : ι → Type*) (π' : ι' → Type*) :
+  ((Π i, π i) × (Π i', π' i')) ≃ Π i, sum.elim π π' i :=
+equiv.Pi_sum (sum.elim π π')
+
+lemma pi_map_left (f : ι ≃ ι') :
+  map (f.Pi_congr_left' α).symm (measure.pi (λ i', μ (f.symm i'))) = measure.pi μ :=
+begin
+  refine (pi_eq _).symm, intros s hs,
+  rw [map_apply],
+  all_goals {sorry}
+end
+
+lemma pi_sum {π : ι ⊕ ι' → Type*} [∀ i, measurable_space (π i)] (μ : ∀ i, measure (π i)) :
+  map (equiv.Pi_sum π) ((measure.pi (λ i, μ (sum.inl i))).prod (measure.pi (λ i, μ (sum.inr i)))) = measure.pi μ :=
+begin
+  refine (pi_eq _).symm, intros s hs,
+  rw [map_apply],
+  all_goals {sorry}
 end
 
 lemma pi_eval_preimage_null {i : ι} {s : set (α i)} (hs : μ i s = 0) :
@@ -257,22 +519,6 @@ lemma ae_eval_ne (i : ι) [has_no_atoms (μ i)] (x : α i) :
   ∀ᵐ y : Π i, α i ∂measure.pi μ, y i ≠ x :=
 compl_mem_ae_iff.2 (pi_hyperplane μ i x)
 
-lemma pi_empty_left (h : ι → false) : measure.pi μ = dirac (λ i, (h i).elim) :=
-sorry
-
-/-- Given one value over a subsingleton, we get a dependent function. -/
-def subsingleton_const [subsingleton ι] {i : ι} (x : α i) (j : ι) : α j :=
-by { rw [subsingleton.elim j i], exact x }
-
-@[simp] lemma subsingleton_const_self [subsingleton ι] {i : ι} (x : α i) :
-  subsingleton_const x i = x := rfl
-
-lemma pi_subsingleton_left [subsingleton ι] (i : ι) : measure.pi μ = map subsingleton_const (μ i) :=
-sorry
-
-lemma pi_map_left (f : ι ≃ ι') :
-  map (f.Pi_congr_left' α).symm (measure.pi (λ i', μ (f.symm i'))) = measure.pi μ :=
-sorry
 
 variable {μ}
 
@@ -383,74 +629,89 @@ lemma volume_pi_pi [Π i, measure_space (α i)] [∀ i, sigma_finite (volume : m
   volume (pi univ s) = ∏ i, volume (s i) :=
 measure.pi_pi (λ i, volume) s hs
 
-section integrate_away
+section marginal
 
-open finset
+open finset topological_space
 variables {δ : Type*} {π : δ → Type*} [∀ x, measurable_space (π x)]
 variables {μ : ∀ i, measure (π i)} [∀ i, sigma_finite (μ i)]
 variables {E : Type*} [normed_group E] [second_countable_topology E]
   [normed_space ℝ E] [complete_space E] [measurable_space E] [borel_space E]
 
 /-- Integrate `f(x₁,…,xₙ)` over all variables `xᵢ` where `i ∈ s`. Return a function in the
-  remaining variables (it will be constant in the `xᵢ` for `i ∈ s`) -/
+  remaining variables (it will be constant in the `xᵢ` for `i ∈ s`).
+  This is the marginal distribution of all variables not in `s`. -/
 -- give better name?
-def integrate_away (μ : ∀ i, measure (π i)) (s : finset δ) (f : (Π i, π i) → E) (x : Π i, π i) :
+def marginal (μ : ∀ i, measure (π i)) (s : finset δ) (f : (Π i, π i) → E) (x : Π i, π i) :
   E :=
-∫ y : Π i : {i // i ∈ s}, π i, f (λ i, if hi : i ∈ s then y ⟨i, hi⟩ else x i)
-  ∂(measure.pi (λ i : {i // i ∈ s}, μ i))
+∫ y : Π i : (s : set δ), π i, f (λ i, if hi : i ∈ s then y ⟨i, hi⟩ else x i)
+  ∂(measure.pi (λ i : (s : set δ), μ i))
+
+/-- The integrand of `marginal _ _ f` is measurable if `f` is. -/
+lemma measurable.marginal_aux {f : (Π i, π i) → E} (hf : measurable f) {s : finset δ} {x : Π i, π i} :
+  measurable (λ (y : Π i : (s : set δ), π i), f (λ i, if hi : i ∈ s then y ⟨i, hi⟩ else x i)) :=
+begin
+  refine hf.comp _,
+  rw measurable_pi_iff, intro i,
+  by_cases h : i ∈ s,
+  { simp [h, measurable_pi_apply] },
+  { simp [h] }
+end
+alias measurable.marginal_aux ← measurable.marginal_aux
 
 /- Note: this notation is not a binder. This is more convenient since it returns a function. -/
-notation `∫⋯∫_` s `, ` f ` ∂` μ:70 := integrate_away μ s f
-notation `∫⋯∫_` s `, ` f := integrate_away volume s f
+notation `∫⋯∫_` s `, ` f ` ∂` μ:70 := marginal μ s f
+notation `∫⋯∫_` s `, ` f := marginal volume s f
 
-lemma integrate_away_empty (f : (Π i, π i) → E) : ∫⋯∫_ ∅, f ∂μ = f :=
+lemma marginal_empty (f : (Π i, π i) → E) : ∫⋯∫_ ∅, f ∂μ = f :=
 begin
-  haveI : subsingleton (Π (i : { i // i ∈ (∅ : finset δ)}), π i) := ⟨λ x y, funext $ λ i, i.2.elim⟩,
+  haveI : subsingleton (Π (i : ((∅ : finset δ) : set δ)), π i) := ⟨λ x y, funext $ λ i, i.2.elim⟩,
   ext x,
-  simp_rw [integrate_away, measure.pi_empty_left _ (λ i : { i // i ∈ (∅ : finset δ)}, i.prop)],
-  refine (integral_dirac _ _ subsingleton.measurable).trans (by simp)
+  simp_rw [marginal, measure.pi_empty_left _ (λ i : ((∅ : finset δ) : set δ), i.prop)],
+  refine (integral_dirac' _ _ subsingleton.measurable).trans (by simp)
 end
 
-lemma integrate_away_eq {s : finset δ} {x y : Π i, π i} (f : (Π i, π i) → E)
+lemma marginal_eq {s : finset δ} {x y : Π i, π i} (f : (Π i, π i) → E)
   (h : ∀ i ∉ s, x i = y i) : (∫⋯∫_ s, f ∂μ) x = (∫⋯∫_ s, f ∂μ) y :=
-by { dsimp [integrate_away], rcongr, exact h _ ‹_› }
+by { dsimp [marginal], rcongr, exact h _ ‹_› }
 
-lemma integrate_away_union (f : (Π i, π i) → E) (s t : finset δ) (hst : disjoint s t) :
+lemma marginal_union (f : (Π i, π i) → E) (s t : finset δ) (hst : disjoint s t) :
   ∫⋯∫_ s ∪ t, f ∂μ = ∫⋯∫_ t, ∫⋯∫_ s, f ∂μ ∂μ :=
 begin
-  let e : { j // j ∈ s ∪ t} ≃ { j // j ∈ s} × { j // j ∈ t} :=
-  equiv.subtype_univ_equiv finset.mem_univ,
+  have : (s : set δ) ∩ (t : set δ) ⊆ ∅, { exact_mod_cast hst },
+  let e : (((s ∪ t : finset δ) : set δ) ≃ (s : set δ) ⊕ (t : set δ) : Type*),
+  { exact_mod_cast equiv.set.union this },
   ext x,
-  simp_rw [integrate_away, ← measure.pi_map_left μ e.symm],
-  rw [integral_map], congr' with y, congr' with i, simp [e], dsimp [e], refl,
-  sorry, sorry
+  simp_rw [marginal, ← measure.pi_map_left _ e],
+  rw [integral_map],
+  -- congr' with y, congr' with i, simp [e], dsimp [e], refl,
+  -- sorry, sorry
 end
 
-
-lemma integrate_away_singleton (f : (Π i, π i) → E) (i : δ) :
+lemma marginal_singleton (f : (Π i, π i) → E) (hf : measurable f) (i : δ) :
   ∫⋯∫_ {i}, f ∂μ = λ x, ∫ xᵢ, f (function.update x i xᵢ) ∂(μ i) :=
 begin
-  haveI : subsingleton ({ j // j ∈ ({i} : finset δ)}) :=
-  ⟨λ j₁ j₂, subtype.ext $ (finset.mem_singleton.mp j₁.2).trans (finset.mem_singleton.mp j₂.2).symm⟩,
+  letI : unique (({i} : finset δ) : set δ) :=
+  ⟨⟨⟨i, finset.mem_singleton_self i⟩⟩, λ j, subtype.ext $ finset.mem_singleton.mp j.2⟩,
   ext x,
-  simp_rw [integrate_away,
-    measure.pi_subsingleton_left _ (⟨i, finset.mem_singleton_self i⟩ : { j // j ∈ ({i} : finset δ)})],
-  rw [integral_map], congr' with y, congr' with j,
-  by_cases hij : j = i, { cases hij.symm, simp },
-  simp [hij],
-  sorry, sorry
+  simp_rw [marginal, measure.pi_unique_left _],
+  rw [integral_map_of_measurable measurable_unique_elim hf.marginal_aux],
+  congr' with y, dsimp only, congr' with j,
+  by_cases hj : j = i,
+  { cases hj.symm, simp only [dif_pos, finset.mem_singleton, update_same],
+    exact @unique_elim_default _ (λ i : (({i} : finset δ) : set δ), π i) _ y },
+  { simp [hj] }
 end
 
-lemma integrate_away_univ [fintype δ] (f : (Π i, π i) → E) :
+lemma marginal_univ [fintype δ] (f : (Π i, π i) → E) :
   ∫⋯∫_ finset.univ, f ∂μ = λ _, ∫ x, f x ∂(measure.pi μ) :=
 begin
   let e : { j // j ∈ finset.univ} ≃ δ := equiv.subtype_univ_equiv finset.mem_univ,
   ext x,
-  simp_rw [integrate_away, ← measure.pi_map_left μ e.symm],
+  simp_rw [marginal, ← measure.pi_map_left μ e.symm],
   rw [integral_map], congr' with y, congr' with i, simp [e], dsimp [e], refl,
   sorry, sorry
 end
 
-end integrate_away
+end marginal
 
 end measure_theory

c05415d4a89aff3b5bcbd95d8794e85e99cd15ee
callesonne <calle.sonne@gmail.com>
2021-02-13 10:17:34 +0000

    more docs

--- src/topology/category/Profinite.lean
+++ src/topology/category/Profinite.lean
@@ -1,7 +1,7 @@
 /-
 Copyright (c) 2020 Kevin Buzzard. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
-Authors: Kevin Buzzard
+Authors: Kevin Buzzard, Calle Sönne
 -/
 
 import topology.category.CompHaus

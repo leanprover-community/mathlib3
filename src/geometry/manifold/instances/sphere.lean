7a2e9ef0290fc87794dd592b497428c869173a4b
Yury G. Kudryashov <urkud@urkud.name>
2021-02-14 02:16:40 -0500

    Fix

--- src/geometry/manifold/instances/sphere.lean
+++ src/geometry/manifold/instances/sphere.lean
@@ -275,7 +275,7 @@ def stereographic' (n : ℕ) [fact (findim ℝ E = n + 1)] (v : sphere (0:E) 1) 
   local_homeomorph (sphere (0:E) 1) (euclidean_space ℝ (fin n)) :=
 (stereographic (norm_eq_of_mem_sphere v)).trans
 (linear_isometry_equiv.from_orthogonal_span_singleton
-  (nonzero_of_mem_unit_sphere v)).to_continuous_linear_equiv.to_homeomorph.to_local_homeomorph
+  (nonzero_of_mem_unit_sphere v)).to_isometric.to_homeomorph.to_local_homeomorph
 
 @[simp] lemma stereographic'_source {n : ℕ} [fact (findim ℝ E = n + 1)] (v : sphere (0:E) 1) :
   (stereographic' n v).source = {v}ᶜ :=

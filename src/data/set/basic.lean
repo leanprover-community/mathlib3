13bef6805143618bff6cb0edd7eb17c73430d034
Benjamin Davidson <68528197+benjamindavidson@users.noreply.github.com>
2021-02-15 02:28:36 -0500

    golf

--- src/data/set/basic.lean
+++ src/data/set/basic.lean
@@ -221,10 +221,7 @@ theorem eq_of_subset_of_subset {a b : set α} : a ⊆ b → b ⊆ a → a = b :=
 theorem mem_of_subset_of_mem {s₁ s₂ : set α} {a : α} (h : s₁ ⊆ s₂) : a ∈ s₁ → a ∈ s₂ := @h _
 
 theorem not_mem_subset (hsubset : s ⊆ t) (ht : a ∉ t) : a ∉ s :=
-begin
-  by_contra hs,
-  exact absurd (mem_of_subset_of_mem hsubset hs) ht,
-end
+λ hs, ht (mem_of_subset_of_mem hsubset hs)
 
 theorem not_subset : (¬ s ⊆ t) ↔ ∃a ∈ s, a ∉ t := by simp only [subset_def, not_forall]
 

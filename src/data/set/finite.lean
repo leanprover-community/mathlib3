c4eb79536ac61626d04414d170f12f6c4612da35
Kevin Buzzard <k.buzzard@imperial.ac.uk>
2021-02-16 17:15:46 +0000

    fix(data/set/finite): add decidable assumptions

--- src/data/set/finite.lean
+++ src/data/set/finite.lean
@@ -584,32 +584,32 @@ lemma finite.card_to_finset {s : set α} [fintype s] (h : s.finite) :
 by { rw [← finset.card_attach, finset.attach_eq_univ, ← fintype.card], congr' 2, funext,
      rw set.finite.mem_to_finset }
 
-section
+section decidable_eq
 
-local attribute [instance, priority 1] classical.prop_decidable
-
-lemma to_finset_compl {α : Type*} [fintype α] (s : set α) :
+lemma to_finset_compl {α : Type*} [fintype α] [decidable_eq α] (s : set α) [decidable_pred (∈ s)] :
   sᶜ.to_finset = (s.to_finset)ᶜ :=
 by ext; simp
 
-lemma to_finset_inter {α : Type*} [fintype α] (s t : set α) :
+lemma to_finset_inter {α : Type*} [fintype α] [decidable_eq α] (s t : set α)
+  [decidable_pred s] [decidable_pred t] :
   (s ∩ t).to_finset = s.to_finset ∩ t.to_finset :=
 by ext; simp
 
-lemma to_finset_union {α : Type*} [fintype α] (s t : set α) :
+lemma to_finset_union {α : Type*} [fintype α] [decidable_eq α] (s t : set α)
+  [decidable_pred s] [decidable_pred t] :
   (s ∪ t).to_finset = s.to_finset ∪ t.to_finset :=
 by ext; simp
 
-lemma to_finset_ne_eq_erase {α : Type*} [fintype α] (a : α) :
+lemma to_finset_ne_eq_erase {α : Type*} [fintype α] [decidable_eq α] (a : α) :
   {x : α | x ≠ a}.to_finset = finset.univ.erase a :=
 by ext; simp
 
-lemma card_ne_eq [fintype α] (a : α) :
+lemma card_ne_eq [fintype α] [decidable_eq α] (a : α) :
   fintype.card {x : α | x ≠ a} = fintype.card α - 1 :=
 by rw [←to_finset_card, to_finset_ne_eq_erase, finset.card_erase_of_mem (finset.mem_univ _),
        finset.card_univ, nat.pred_eq_sub_one]
 
-end
+end decidable_eq
 
 section
 

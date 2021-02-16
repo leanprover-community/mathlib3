893e7087b49f58e3144b80a26d4ccd3c4c9eeaa2
Scott Morrison <scott.morrison@gmail.com>
2021-02-17 10:06:20 +1100

    term mode

--- src/algebra/category/Module/adjunctions.lean
+++ src/algebra/category/Module/adjunctions.lean
@@ -41,10 +41,8 @@ The free-forgetful adjunction for R-modules.
 def adj : free R ⊣ forget (Module.{u} R) :=
 adjunction.mk_of_hom_equiv
 { hom_equiv := λ X M, (finsupp.lift M R X).to_equiv.symm,
-  hom_equiv_naturality_left_symm' :=
-  begin
-    intros _ _ M f g, ext x,
-    exact (finsupp.sum_map_domain_index_add_monoid_hom (λ y, (smul_add_hom R M).flip (g y))).symm,
-  end }
+  hom_equiv_naturality_left_symm' := λ _ _ M f g,
+  finsupp.lhom_ext' (λ x, linear_map.ext_ring
+    (finsupp.sum_map_domain_index_add_monoid_hom (λ y, ((smul_add_hom R ↥M).flip) (g y))).symm) }
 
 end Module

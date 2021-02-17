f2fbb02c23cbc2d663d47d60689355c62591d825
Scott Morrison <scott.morrison@gmail.com>
2021-02-17 23:29:45 +1100

    .

--- src/topology/simplicial/Moore_complex.lean
+++ src/topology/simplicial/Moore_complex.lean
@@ -5,6 +5,7 @@ Authors: Scott Morrison
 -/
 import category_theory.simplicial_object
 import category_theory.abelian.basic
+import category_theory.subobject
 import algebra.homology.connective_chain_complex
 
 universes v u
@@ -14,19 +15,26 @@ noncomputable theory
 open category_theory category_theory.limits
 open opposite
 
-variables (C : Type*) [category C] [abelian C]
+def fin.rest (n : ℕ) : finset (fin (n+1)) := finset.univ.image (fin.succ : fin n → fin (n+1))
+
+variables {C : Type*} [category C] [abelian C]
+local attribute [instance] abelian.has_pullbacks
 
 /-! The definitions in this namespace are all auxilliary definitions for `normalized_Moore_complex`
 and should usually only be accessed via that. -/
 namespace normalized_Moore_complex
 
+def obj_X (X : simplicial_object C) : Π n : ℕ, subobject (X.obj (op n))
+| 0 := ⊤
+| (n+1) := finset.univ.inf (λ k : fin (n+1), kernel_subobject (X.δ k.succ))
+
 def obj (X : simplicial_object C) : connective_chain_complex C :=
-{ X := obj_X_apply_ℤ X,
+{ X := λ n, obj_X X n,
   d := sorry, }
 
 end normalized_Moore_complex
 
-variables (R)
+variables (C)
 
 def normalized_Moore_complex : (simplicial_object C) ⥤ connective_chain_complex C :=
 { obj := λ X, sorry,

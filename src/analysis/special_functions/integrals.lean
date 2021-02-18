c1d52b3ec55ab8d3193d952f7bc127d711be3c2a
Benjamin Davidson <68528197+benjamindavidson@users.noreply.github.com>
2021-02-18 03:57:18 -0500

    style

--- src/analysis/special_functions/integrals.lean
+++ src/analysis/special_functions/integrals.lean
@@ -8,13 +8,13 @@ import measure_theory.interval_integral
 /-!
 # Integration of specific interval integrals
 
-This file contains the computation of the integrals of various simple functions, including `pow`,
-`exp`, `inv`/`one_div`, `sin`, `cos`, and `λ x, 1 / (1 + x^2)`.
+This file contains proofs of the integrals of various simple functions, including `pow`, `exp`,
+`inv`/`one_div`, `sin`, `cos`, and `λ x, 1 / (1 + x^2)`.
 
 With these lemmas, many simple integrals can be computed by `simp` or `norm_num`. Scroll to the
 bottom of the file for examples.
 
-This file is far from complete; we are working on adding more integrals.
+This file is incomplete; we are working on expanding it.
 -/
 
 open real set interval_integral
@@ -40,7 +40,7 @@ lemma integral_one : ∫ x in a..b, (1:ℝ) = b - a :=
 by simpa using integral_pow 0
 
 @[simp]
-lemma integral_exp {a b : ℝ}: ∫ x in a..b, exp x = exp b - exp a :=
+lemma integral_exp : ∫ x in a..b, exp x = exp b - exp a :=
 by rw integral_deriv_eq_sub'; norm_num [continuous_exp.continuous_on]
 
 @[simp]
@@ -54,11 +54,11 @@ end
 
 @[simp]
 lemma integral_inv_of_pos (ha : 0 < a) (hb : 0 < b) : ∫ x : ℝ in a..b, x⁻¹ = log (b / a) :=
-integral_inv (not_mem_interval_of_lt ha hb)
+integral_inv $ not_mem_interval_of_lt ha hb
 
 @[simp]
 lemma integral_inv_of_neg (ha : a < 0) (hb : b < 0) : ∫ x : ℝ in a..b, x⁻¹ = log (b / a) :=
-integral_inv (not_mem_interval_of_gt ha hb)
+integral_inv $ not_mem_interval_of_gt ha hb
 
 lemma integral_one_div (h : (0:ℝ) ∉ interval a b) : ∫ x : ℝ in a..b, 1/x = log (b / a) :=
 by simp only [one_div, integral_inv h]
@@ -70,25 +70,24 @@ lemma integral_one_div_of_neg (ha : a < 0) (hb : b < 0) : ∫ x : ℝ in a..b, 1
 by simp only [one_div, integral_inv_of_neg ha hb]
 
 @[simp]
-lemma integral_sin {a b : ℝ}: ∫ x in a..b, sin x = cos a - cos b :=
+lemma integral_sin : ∫ x in a..b, sin x = cos a - cos b :=
 by rw integral_deriv_eq_sub' (λ x, -cos x); norm_num [continuous_on_sin]
 
 @[simp]
-lemma integral_cos {a b : ℝ}: ∫ x in a..b, cos x = sin b - sin a :=
+lemma integral_cos : ∫ x in a..b, cos x = sin b - sin a :=
 by rw integral_deriv_eq_sub'; norm_num [continuous_on_cos]
 
 @[simp]
-lemma integral_inv_one_add_sq {a b : ℝ} : ∫ x : ℝ in a..b, (1 + x^2)⁻¹ = arctan b - arctan a :=
+lemma integral_inv_one_add_sq : ∫ x : ℝ in a..b, (1 + x^2)⁻¹ = arctan b - arctan a :=
 begin
   simp only [← one_div],
   refine integral_deriv_eq_sub' _ _ _ (continuous_const.div _ (λ x, _)).continuous_on;
   norm_num,
-  continuity,
-  nlinarith,
+  { continuity },
+  { nlinarith },
 end
 
-lemma integral_one_div_one_add_sq {a b : ℝ} :
-  ∫ x : ℝ in a..b, 1 / (1 + x^2) = arctan b - arctan a :=
+lemma integral_one_div_one_add_sq : ∫ x : ℝ in a..b, 1 / (1 + x^2) = arctan b - arctan a :=
 by simp
 
 
@@ -97,8 +96,8 @@ open_locale real
 example : ∫ x in 0..π, sin x = 2 := by norm_num
 example : ∫ x in 0..π/4, cos x = sqrt 2 / 2 := by simp
 example : ∫ x:ℝ in 2..4, x^(3:ℕ) = 60 := by norm_num
-example : ∫ x in 0..2, exp x = exp 2 - 1 := by simp
-example : ∫ x:ℝ in (-1)..2, x = 3/2 := by norm_num
-example : ∫ x:ℝ in 8..11, (-1:ℝ) = -3 := by norm_num
+example : ∫ x in 0..2, -exp x = 1 - exp 2 := by simp
+example : ∫ x:ℝ in (-1)..4, x = 15/2 := by norm_num
+example : ∫ x:ℝ in 8..11, (1:ℝ) = 3 := by norm_num
 example : ∫ x:ℝ in 2..3, x⁻¹ = log (3/2) := by norm_num
 example : ∫ x:ℝ in 0..1, 1 / (1 + x^2) = π/4 := by simp

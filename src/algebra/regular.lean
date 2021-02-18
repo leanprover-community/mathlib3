1d0e1bf8558178f76dcfa4ce040ad945d7212f3c
adomani <adomani@gmail.com>
2021-02-18 11:09:45 +0100

    use more variables

--- src/algebra/regular.lean
+++ src/algebra/regular.lean
@@ -16,7 +16,7 @@ is regular.
 
 The final goal is to develop part of the API to prove, eventually, results about non-zero-divisors.
 -/
-variables {R : Type*}
+variables {R : Type*} {a b : R}
 
 /-- A left-regular element is an element `c` such that multiplication on the left by `c`
 is injective on the left. -/
@@ -39,32 +39,30 @@ section semigroup
 variable [semigroup R]
 
 /-- In a semigroup, then the product of left-regular elements is left-regular. -/
-lemma is_left_regular.mul {a b : R}
-  (lra : is_left_regular a) (lrb : is_left_regular b) :
+lemma is_left_regular.mul (lra : is_left_regular a) (lrb : is_left_regular b) :
   is_left_regular (a * b) :=
 show function.injective ((*) (a * b)), from (comp_mul_left a b) ▸ lra.comp lrb
 
 /-- In a semigroup, then the product of right-regular elements is right-regular. -/
-lemma is_right_regular.mul {a b : R}
-  (rra : is_right_regular a) (rrb : is_right_regular b) :
+lemma is_right_regular.mul (rra : is_right_regular a) (rrb : is_right_regular b) :
   is_right_regular (a * b) :=
 show function.injective (* (a * b)), from (comp_mul_right b a) ▸ rrb.comp rra
 
 /--  If an element `b` becomes left-regular after multiplying it on the left by a left-regular
 element, then `b` is left-regular. -/
-lemma is_left_regular_saturated {a b : R} (ab : is_left_regular (a * b)) :
+lemma is_left_regular_saturated (ab : is_left_regular (a * b)) :
   is_left_regular b :=
 function.injective.of_comp (by rwa comp_mul_left a b)
 
 /--  An element is left-regular if and only if multiplying it on the left by a left-regular element
 is left-regular. -/
-@[simp] lemma mul_is_left_regular_iff {a : R} (b : R) (ha : is_left_regular a) :
+@[simp] lemma mul_is_left_regular_iff (b : R) (ha : is_left_regular a) :
   is_left_regular (a * b) ↔ is_left_regular b :=
 ⟨λ ab, is_left_regular_saturated ab, λ ab, is_left_regular.mul ha ab⟩
 
 /--  If an element `b` becomes right-regular after multiplying it on the right by a right-regular
 element, then `b` is right-regular. -/
-lemma is_right_regular_saturated {a b : R} (ab : is_right_regular (b * a)) :
+lemma is_right_regular_saturated (ab : is_right_regular (b * a)) :
   is_right_regular b :=
 begin
   refine λ x y xy, ab (_ : x * (b * a) = y * (b * a)),
@@ -74,14 +72,13 @@ end
 
 /--  An element is right-regular if and only if multiplying it on the right with a right-regular
 element is right-regular. -/
-@[simp] lemma mul_is_right_regular_iff {a : R} (b : R) (ha : is_right_regular a) :
+@[simp] lemma mul_is_right_regular_iff (b : R) (ha : is_right_regular a) :
   is_right_regular (b * a) ↔ is_right_regular b :=
 ⟨λ ab, is_right_regular_saturated ab, λ ab, is_right_regular.mul ab ha⟩
 
 /--  Two elements `a` and `b` are regular if and only if both products `a * b` and `b * a`
 are regular. -/
-lemma mul_and_mul_iff {a b : R} :
-  is_regular (a * b) ∧ is_regular (b * a) ↔ is_regular a ∧ is_regular b :=
+lemma mul_and_mul_iff : is_regular (a * b) ∧ is_regular (b * a) ↔ is_regular a ∧ is_regular b :=
 begin
   refine ⟨_, _⟩,
   { rintros ⟨ab, ba⟩,
@@ -95,7 +92,7 @@ begin
 end
 
 /--  The "most used" implication of `mul_iff`, with split hypotheses, instead of `∧`. -/
-lemma mul_of_mul_and_mul {a b : R} (ab : is_regular (a * b)) (ba : is_regular (b * a)) :
+lemma mul_of_mul_and_mul (ab : is_regular (a * b)) (ba : is_regular (b * a)) :
   is_regular a ∧ is_regular b :=
 mul_and_mul_iff.mp ⟨ab, ba⟩
 
@@ -106,7 +103,7 @@ section comm_semigroup
 variable [comm_semigroup R]
 
 /--  A product is regular if and only if the factors are. -/
-lemma mul_iff {a b : R} :
+lemma mul_iff :
   is_regular (a * b) ↔ is_regular a ∧ is_regular b :=
 begin
   refine iff.trans _ mul_and_mul_iff,
@@ -125,19 +122,19 @@ lemma one : is_regular (1 : R) :=
   λ a b ab, (mul_one a).symm.trans (eq.trans ab (mul_one b))⟩
 
 /-- An element admitting a left inverse is left-regular. -/
-lemma left_of_mul_eq_one {a ai : R} (h : ai * a = 1) : is_left_regular a :=
-@is_left_regular_saturated R _ ai _ (by { rw h, exact one.left })
+lemma left_of_mul_eq_one (h : b * a = 1) : is_left_regular a :=
+@is_left_regular_saturated R _ a _ (by { rw h, exact one.left })
 
 /-- An element admitting a right inverse is right-regular. -/
-lemma right_of_mul_eq_one {a ai : R} (h : a * ai = 1) : is_right_regular a :=
-@is_right_regular_saturated R _ ai _ (by { rw h, exact one.right })
+lemma right_of_mul_eq_one (h : a * b = 1) : is_right_regular a :=
+@is_right_regular_saturated R _ a _ (by { rw h, exact one.right })
 
 /-- If `R` is a monoid, an element in `units R` is regular. -/
 lemma units.is_regular (a : units R) : is_regular (a : R) :=
 ⟨left_of_mul_eq_one a.inv_mul,  right_of_mul_eq_one a.mul_inv⟩
 
 /-- A unit in a monoid is regular. -/
-lemma is_unit.is_regular {a : R} (ua : is_unit a) : is_regular a :=
+lemma is_unit.is_regular (ua : is_unit a) : is_regular a :=
 begin
   rcases ua with ⟨a, rfl⟩,
   exact units.is_regular a,
@@ -174,9 +171,7 @@ section cancel_monoid_with_zero
 variables  [cancel_monoid_with_zero R]
 
 /--  Non-zero elements of an integral domain are regular. -/
-lemma is_regular_of_cancel_monoid_with_zero {a : R}
- (a0 : a ≠ 0) :
-  is_regular a :=
+lemma is_regular_of_cancel_monoid_with_zero (a0 : a ≠ 0) : is_regular a :=
 ⟨λ b c, (mul_right_inj' a0).mp, λ b c, (mul_left_inj' a0).mp⟩
 
 end cancel_monoid_with_zero

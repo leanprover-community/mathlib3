024b4e3767c14228e0ad62ef84605d17b7a28eb0
Adam Topaz <github@adamtopaz.com>
2021-02-22 16:19:41 -0700

    Fix

--- src/category_theory/abelian/additive_functor.lean
+++ src/category_theory/abelian/additive_functor.lean
@@ -36,26 +36,18 @@ namespace category_theory
 /-- A functor `F` is additive provided `F.map` is an additive homomorphism. -/
 class functor.additive {C D : Type*} [category C] [category D]
   [preadditive C] [preadditive D] (F : C ⥤ D) : Prop :=
-(exists_hom [] : ∀ (X Y : C), ∃ f : (X ⟶ Y) →+ (F.obj X ⟶ F.obj Y),
-  ∀ g : X ⟶ Y, F.map g = f g)
+(map_zero' : Π {X Y : C}, F.map (0 : X ⟶ Y) = 0 . obviously)
+(map_add' : Π {X Y : C} {f g : X ⟶ Y}, F.map (f + g) = F.map f + F.map g . obviously)
+
+restate_axiom functor.additive.map_zero'
+restate_axiom functor.additive.map_add'
+
+attribute [simp] functor.additive.map_zero functor.additive.map_add
 
 section preadditive
+
 variables {C D : Type*} [category C] [category D] [preadditive C]
   [preadditive D] (F : C ⥤ D) [functor.additive F]
-
-namespace functor.additive
-
-/--
-Construct an additive instance for `G` from proofs that `G.map` sends `0` to `0`
-and is compatible with addition of morphisms.
--/
-lemma of_is_hom (G : C ⥤ D)
-  (map_zero : ∀ X Y : C, G.map (0 : X ⟶ Y) = 0)
-  (map_add : ∀ (X Y : C) (f g : X ⟶ Y), G.map (f + g) = G.map f + G.map g) :
-  functor.additive G := functor.additive.mk $ λ X Y,
-⟨⟨λ f, G.map f, map_zero _ _, map_add _ _⟩, λ g, rfl⟩
-
-end functor.additive
 
 namespace functor
 
@@ -63,37 +55,20 @@ namespace functor
 @[simps]
 def add_map {X Y : C} : (X ⟶ Y) →+ (F.obj X ⟶ F.obj Y) :=
 { to_fun := λ f, F.map f,
-  map_zero' := begin
-    rcases functor.additive.exists_hom F X Y with ⟨f, hf⟩,
-    simp [hf],
-  end,
-  map_add' := begin
-    rcases functor.additive.exists_hom F X Y with ⟨f, hf⟩,
-    simp [hf],
-  end }
+  map_zero' := additive.map_zero,
+  map_add' := λ _ _, additive.map_add }
 
 lemma add_map_spec {X Y : C} {f : X ⟶ Y} : F.add_map f = F.map f := rfl
 
 lemma add_map_spec' {X Y : C} : ⇑(F.add_map : (X ⟶ Y) →+ _) = @map C _ D _ F X Y := rfl
 
 @[simp]
-lemma map_zero {X Y : C} : F.map (0 : X ⟶ Y) = 0 := F.add_map.map_zero
-
-@[simp]
-lemma map_add {X Y : C} {f g : X ⟶ Y} : F.map (f + g) = F.map f + F.map g :=
-F.add_map.map_add _ _
-
-@[simp]
-lemma map_neg {X Y : C} {f : X ⟶ Y} : F.map (-f) = - F.map f :=
+lemma additive.map_neg {X Y : C} {f : X ⟶ Y} : F.map (-f) = - F.map f :=
 F.add_map.map_neg _
 
 @[simp]
-lemma map_sub {X Y : C} {f g : X ⟶ Y} : F.map (f - g) = F.map f - F.map g :=
+lemma additive.map_sub {X Y : C} {f g : X ⟶ Y} : F.map (f - g) = F.map f - F.map g :=
 F.add_map.map_sub _ _
-
--- sanity check
-example (G : C ⥤ D) [functor.additive G] {X Y : C} {f g : X ⟶ Y} :
-  G.map (f - f + 0 - g) = - G.map g := by simp
 
 end functor
 end preadditive

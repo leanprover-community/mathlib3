ae28c03252bf8b84d859f1fd4585696acf636d37
Ruben Van de Velde <65514131+Ruben-VandeVelde@users.noreply.github.com>
2021-02-22 19:26:57 +0100

    fixup! feat(data/fintype/intervals): Add set.finite lemmas for integer intervals

--- src/data/fintype/intervals.lean
+++ src/data/fintype/intervals.lean
@@ -39,15 +39,15 @@ fintype.of_finset (finset.Ico_ℤ l u) $
 calc fintype.card (Ico l u) = (finset.Ico_ℤ l u).card : fintype.card_of_finset _ _
                         ... = (u - l).to_nat          : finset.Ico_ℤ.card l u
 
-lemma Ico_finite (l u : ℤ) : set.finite (Ico l u) := ⟨set.Ico_ℤ_fintype l u⟩
-lemma Ioo_finite (l u : ℤ) : set.finite (Ioo l u) := Ico_finite (l + 1) u
-lemma Icc_finite (l u : ℤ) : set.finite (Icc l u) :=
+lemma Ico_ℤ_finite (l u : ℤ) : set.finite (Ico l u) := ⟨set.Ico_ℤ_fintype l u⟩
+lemma Ioo_ℤ_finite (l u : ℤ) : set.finite (Ioo l u) := Ico_ℤ_finite (l + 1) u
+lemma Icc_ℤ_finite (l u : ℤ) : set.finite (Icc l u) :=
 begin
-  convert Ico_finite l (u + 1),
+  convert Ico_ℤ_finite l (u + 1),
   ext,
   simp only [int.lt_add_one_iff, iff_self, mem_Ico, mem_Icc],
 end
-lemma Ioc_finite (l u : ℤ) : set.finite (Ioc l u) := Icc_finite (l + 1) u
+lemma Ioc_ℤ_finite (l u : ℤ) : set.finite (Ioc l u) := Icc_ℤ_finite (l + 1) u
 
 -- TODO other useful instances: fin n, zmod?
 

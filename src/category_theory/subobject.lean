fe372acbcda7ddcc3e6f77c8b61bdca56faca6e6
Scott Morrison <scott.morrison@gmail.com>
2021-02-25 18:50:31 +1100

    coercion

--- src/category_theory/subobject.lean
+++ src/category_theory/subobject.lean
@@ -76,10 +76,8 @@ This isn't skeletal, so it's not a partial order.
 
 Later we define `subobject X` as the quotient of this by isomorphisms.
 -/
-@[derive [category, λ t, has_coe t (over X)]]
+@[derive [category]]
 def mono_over (X : C) := {f : over X // mono f.hom}
-
-attribute [priority 100] mono_over.has_coe
 
 namespace mono_over
 
@@ -90,8 +88,11 @@ def mk' {X A : C} (f : A ⟶ X) [hf : mono f] : mono_over X := { val := over.mk 
 /-- The inclusion from monomorphisms over X to morphisms over X. -/
 def forget (X : C) : mono_over X ⥤ over X := full_subcategory_inclusion _
 
+instance : has_coe (mono_over X) C :=
+{ coe := λ Y, Y.val.left, }
+
 @[simp]
-lemma forget_obj_left {f} : ((forget X).obj f).left = f.val.left := rfl
+lemma forget_obj_left {f} : ((forget X).obj f).left = f := rfl
 
 /-- Convenience notation for the underlying arrow of a monomorphism over X. -/
 abbreviation arrow (f : mono_over X) : _ ⟶ X := ((forget X).obj f).hom
@@ -200,11 +201,11 @@ def pullback_id : pullback (𝟙 X) ≅ 𝟭 _ :=
 lift_iso _ _ over.pullback_id ≪≫ lift_id
 
 @[simp] lemma pullback_obj_left (f : X ⟶ Y) (g : mono_over Y) :
-(↑((pullback f).obj g) : over X).left = limits.pullback g.arrow f :=
+  (((pullback f).obj g) : C) = limits.pullback g.arrow f :=
 rfl
 
 @[simp] lemma pullback_obj_arrow (f : X ⟶ Y) (g : mono_over Y) :
-((pullback f).obj g).arrow = pullback.snd :=
+  ((pullback f).obj g).arrow = pullback.snd :=
 rfl
 
 end pullback
@@ -231,12 +232,13 @@ def map_id : map (𝟙 X) ≅ 𝟭 _ :=
 lift_iso _ _ over.map_id ≪≫ lift_id
 
 @[simp] lemma map_obj_left (f : X ⟶ Y) [mono f] (g : mono_over X) :
-(↑((map f).obj g) : over Y).left = g.val.left :=
+  (((map f).obj g) : C) = g.val.left :=
 rfl
 
 @[simp]
 lemma map_obj_arrow (f : X ⟶ Y) [mono f] (g : mono_over X) :
-((map f).obj g).arrow = g.arrow ≫ f := rfl
+  ((map f).obj g).arrow = g.arrow ≫ f :=
+rfl
 
 instance full_map (f : X ⟶ Y) [mono f] : full (map f) :=
 { preimage := λ g h e,
@@ -390,7 +392,7 @@ instance {X : C} : inhabited (mono_over X) := ⟨⊤⟩
 def le_top (f : mono_over X) : f ⟶ ⊤ :=
 hom_mk f.arrow (comp_id _)
 
-@[simp] lemma top_left (X : C) : ((⊤ : mono_over X) : over X).left = X := rfl
+@[simp] lemma top_left (X : C) : ((⊤ : mono_over X) : C) = X := rfl
 @[simp] lemma top_arrow (X : C) : (⊤ : mono_over X).arrow = 𝟙 X := rfl
 
 /-- `map f` sends `⊤ : mono_over X` to `⟨X, f⟩ : mono_over Y`. -/
@@ -427,7 +429,7 @@ local attribute [instance] has_zero_object.has_zero
 instance {X : C} : has_bot (mono_over X) :=
 { bot := mk' (0 : 0 ⟶ X) }
 
-@[simp] lemma bot_left (X : C) : ((⊥ : mono_over X) : over X).left = 0 := rfl
+@[simp] lemma bot_left (X : C) : ((⊥ : mono_over X) : C) = 0 := rfl
 @[simp] lemma bot_arrow {X : C} : (⊥ : mono_over X).arrow = 0 :=
 by ext
 

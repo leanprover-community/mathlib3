49d2191b66bac1cbe9f20076ae5228502a162c5a
tb65536 <tb65536@uw.edu>
2021-02-26 00:42:50 +0000

    feat(data/polynomial/basic): monomial_neg (#6422)
    The monomial of a negation is the negation of the monomial.
    
    (this PR is part of the irreducibility saga)

--- src/data/polynomial/basic.lean
+++ src/data/polynomial/basic.lean
@@ -177,6 +177,9 @@ instance : ring (polynomial R) := add_monoid_algebra.ring
 @[simp]
 lemma coeff_sub (p q : polynomial R) (n : ℕ) : coeff (p - q) n = coeff p n - coeff q n := rfl
 
+@[simp] lemma monomial_neg (n : ℕ) (a : R) : monomial n (-a) = -(monomial n a) :=
+by rw [eq_neg_iff_add_eq_zero, ←monomial_add, neg_add_self, monomial_zero_right]
+
 end ring
 
 instance [comm_ring R] : comm_ring (polynomial R) := add_monoid_algebra.comm_ring

eb21f06c914762a3510a4d4bb215da3e2b9026f0
Bryan Gin-ge Chen <bryangingechen@gmail.com>
2021-02-27 18:03:01 -0500

    copyright header

--- src/algebra/ring/boolean_ring.lean
+++ src/algebra/ring/boolean_ring.lean
@@ -1,3 +1,9 @@
+/-
+Copyright (c) 2021 Bryan Gin-ge Chen. All rights reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
+Authors: Bryan Gin-ge Chen
+-/
+
 import algebra.ring.basic
 import tactic.ring
 import tactic.abel

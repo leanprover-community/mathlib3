42ac73f2d5ff3825bccc57c60835f8f2ab7f1395
RemyDegenne <Remydegenne@gmail.com>
2021-02-28 16:27:43 +0100

    fix snorm_sum_le

--- src/measure_theory/lp_space.lean
+++ src/measure_theory/lp_space.lean
@@ -592,14 +592,14 @@ lemma snorm'_sum_le [second_countable_topology E] {ι} {f : ι → α → E} {s 
   (hfs : ∀ i, i ∈ s → ae_measurable (f i) μ) (hq1 : 1 ≤ q) :
   snorm' (∑ i in s, f i) q μ ≤ ∑ i in s, snorm' (f i) q μ :=
 @finset.le_sum_of_subadditive_on_pred (α → E) ℝ≥0∞ ι _ _ (λ f, snorm' f q μ)
-  (snorm'_zero (zero_lt_one.trans_le hq1)) (λ f, ae_measurable f μ)
+  (λ f, ae_measurable f μ) (snorm'_zero (zero_lt_one.trans_le hq1))
   (λ f g hf hg, snorm'_add_le hf hg hq1) (λ x y, ae_measurable.add) _ _ hfs
 
 lemma snorm_sum_le [second_countable_topology E] {ι} {f : ι → α → E} {s : finset ι}
   (hfs : ∀ i, i ∈ s → ae_measurable (f i) μ) (hp1 : 1 ≤ p) :
   snorm (∑ i in s, f i) p μ ≤ ∑ i in s, snorm (f i) p μ :=
 @finset.le_sum_of_subadditive_on_pred (α → E) ℝ≥0∞ ι _ _ (λ f, snorm f p μ)
-  snorm_zero (λ f, ae_measurable f μ) (λ f g hf hg, snorm_add_le hf hg hp1)
+  (λ f, ae_measurable f μ) snorm_zero (λ f g hf hg, snorm_add_le hf hg hp1)
   (λ x y, ae_measurable.add) _ _ hfs
 
 lemma snorm_add_lt_top_of_one_le {f g : α → E} (hf : mem_ℒp f p μ) (hg : mem_ℒp g p μ)

d023d5c23908c61fbc294819e98a631badd6cd27
Luke Kershaw <35707277+l-kershaw@users.noreply.github.com>
2021-03-04 20:05:56 +0000

    Add definition of triangle

---
+++ src/category_theory/additive/basic.lean
@@ -0,0 +1,39 @@
+/-
+Copyright (c) 2021 Luke Kershaw. All rights reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
+Authors: Luke Kershaw
+-/
+import category_theory.limits.constructions.pullbacks
+import category_theory.limits.shapes.biproducts
+import category_theory.limits.shapes.images
+import category_theory.abelian.non_preadditive
+import category_theory.shift
+
+/-!
+# Additive Categories
+
+This file contains the definition of additive categories.
+
+-/
+
+noncomputable theory
+
+open category_theory
+open category_theory.preadditive
+open category_theory.limits
+
+universes v v₀ v₁ v₂ u u₀ u₁ u₂
+
+namespace category_theory
+
+variables {C : Type u} [category C]
+variables (C)
+
+/--
+A preadditive category `C` is called additive if it has all finite biproducts.
+See https://stacks.math.columbia.edu/tag/0104.
+-/
+class additive_category extends preadditive C, has_finite_biproducts C
+
+end category_theory
+#lint
---
+++ src/category_theory/triangulated/basic.lean
@@ -0,0 +1,105 @@
+/-
+Copyright (c) 2021 Luke Kershaw. All rights reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
+Authors: Luke Kershaw
+-/
+import category_theory.limits.constructions.pullbacks
+import category_theory.limits.shapes.biproducts
+import category_theory.limits.shapes.images
+import category_theory.abelian.non_preadditive
+import category_theory.additive.basic
+import category_theory.shift
+
+/-!
+# Triangulated Categories
+
+This file contains the definition of triangulated categories.
+
+-/
+
+noncomputable theory
+
+open category_theory
+open category_theory.preadditive
+open category_theory.limits
+
+universes v v₀ v₁ v₂ u u₀ u₁ u₂
+
+namespace category_theory.triangulated
+
+/--
+We work in an additive category C equipped with an additive shift.
+-/
+variables {C : Type u} [category.{v} C] [has_shift C] [additive_category C]
+variables (C)
+
+
+/--
+A triangle in C is a sextuple (X,Y,Z,f,g,h) where X,Y,Z are objects of C,
+and f: X → Y, g: Y → Z, h: Z → ΣX are morphisms in C.
+See https://stacks.math.columbia.edu/tag/0144.
+-/
+@[nolint has_inhabited_instance]
+structure triangle :=
+(obj1 : C)
+(obj2 : C)
+(obj3 : C)
+(mor1 : obj1 ⟶ obj2)
+(mor2 : obj2 ⟶ obj3)
+(mor3 : obj3 ⟶ obj1⟦1⟧)
+
+variable {C}
+
+/--
+A morphism of triangles (X,Y,Z,f,g,h)→(X',Y',Z',f',g',h') in C is a triple of morphisms
+a: X → X', b: Y → Y', c: Z → Z' such that b ≫ f = f' ≫ a, c ≫ g = g' ≫ b,
+and Σa ≫ h = h' ≫ c.
+In other words, we have a commutative diagram:
+     f      g      h
+  X  --> Y  --> Z  --> ΣX
+  |      |      |       |
+  |a     |b     |c      |Σa
+  V      V      V       V
+  X' --> Y' --> Z' --> ΣX'
+     f'     g'     h'
+
+See https://stacks.math.columbia.edu/tag/0144.
+-/
+@[nolint has_inhabited_instance]
+structure triangle_morphism (T₁ : triangle C) (T₂ : triangle C):=
+(trimor1 : T₁.obj1 ⟶ T₂.obj1)
+(trimor2 : T₁.obj2 ⟶ T₂.obj2)
+(trimor3 : T₁.obj3 ⟶ T₂.obj3)
+(comm1: T₁.mor1 ≫ trimor2 = trimor1 ≫ T₂.mor1)
+(comm2: T₁.mor2 ≫ trimor3 = trimor2 ≫  T₂.mor2)
+(comm3: T₁.mor3 ≫ trimor1⟦1⟧' = trimor3 ≫ T₂.mor3)
+
+/--
+The identity triangle morphism.
+-/
+def triangle_morphism_id (T : triangle C) : triangle_morphism T T :=
+{ trimor1 := 𝟙 T.obj1,
+  trimor2 := 𝟙 T.obj2,
+  trimor3 := 𝟙 T.obj3,
+  comm1 := by rw [category.id_comp, category.comp_id],
+  comm2 := by rw [category.id_comp, category.comp_id],
+  comm3 := by { dsimp, simp } }
+
+
+variable {T₁ : triangle C}
+variable {T₂ : triangle C}
+variable {T₃ : triangle C}
+/--
+Composition of triangle morphisms gives a triangle morphism.
+-/
+def triangle_morphism.comp (f : triangle_morphism T₁ T₂) (g : triangle_morphism T₂ T₃) : triangle_morphism T₁ T₃ :=
+{ trimor1 := f.trimor1 ≫ g.trimor1,
+  trimor2 := f.trimor2 ≫ g.trimor2,
+  trimor3 := f.trimor3 ≫ g.trimor3,
+  comm1 := by rw [← category.assoc, f.comm1, category.assoc, g.comm1, category.assoc],
+  comm2 := by rw [← category.assoc, f.comm2, category.assoc, g.comm2, category.assoc],
+  comm3 := by rw [functor.map_comp, ← category.assoc, f.comm3, category.assoc, g.comm3, category.assoc], }
+
+
+end category_theory.triangulated
+#lint

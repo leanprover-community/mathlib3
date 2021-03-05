6c00943b5190a00e71637becb90d4c8c9c20018e
tb65536 <tb65536@users.noreply.github.com>
2021-03-05 09:55:16 -0800

    Update src/data/polynomial/mirror.lean
    Co-authored-by: Bryan Gin-ge Chen <bryangingechen@gmail.com>

--- src/data/polynomial/mirror.lean
+++ src/data/polynomial/mirror.lean
@@ -8,23 +8,23 @@ import data.polynomial.ring_division
 import algebra.big_operators.nat_antidiagonal
 
 /-!
-# Reverse of a univariate polynomial
+# "Mirror" of a univariate polynomial
 
-In this file we define `mirror`, a variant of `reverse`. The difference between `reverse` and
-`mirror` is that `reverse` will decrease the degree if the polynomial is divisible by `X`.
-We also define `norm2`, which is the sum of the squares of the coefficients of a polynomial.
-It is also a coefficient of `p * p.mirror`.
+In this file we define `polynomial.mirror`, a variant of `polynomial.reverse`. The difference 
+between `reverse` and `mirror` is that `reverse` will decrease the degree if the polynomial is 
+divisible by `X`. We also define `polynomial.norm2`, which is the sum of the squares of the 
+coefficients of a polynomial. It is also a coefficient of `p * p.mirror`.
 
 ## Main definitions
 
-- `p.mirror`
-- `p.norm2`
+- `polynomial.mirror`
+- `polynomial.norm2`
 
 ## Main results
 
-- `mirror_mul_of_domain`: `mirror` preserves multiplication.
-- `irreducible_of_mirror`: an irreducibility criterion involving `mirror`
-- `norm2_eq_mul_reverse_coeff`: `norm2` is a coefficient of `p * p.mirror`
+- `polynomial.mirror_mul_of_domain`: `mirror` preserves multiplication.
+- `polynomial.irreducible_of_mirror`: an irreducibility criterion involving `mirror`
+- `polynomial.norm2_eq_mul_reverse_coeff`: `norm2` is a coefficient of `p * p.mirror`
 
 -/
 

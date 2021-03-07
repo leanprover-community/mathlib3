4ac48e4c7319a53cfdaab5cbd6a0634870ef2291
Moritz Firsching <firsching@google.com>
2021-03-07 22:36:39 +0100

    fix sum_bernoulli_poly

--- src/number_theory/bernoulli_polynomials.lean
+++ src/number_theory/bernoulli_polynomials.lean
@@ -109,11 +109,11 @@ begin
   simp only [add_right_eq_self, cast_succ, mul_one, cast_one, cast_add, nat.add_sub_cancel_left,
     choose_succ_self_right, one_smul, bernoulli_zero, sum_singleton, zero_add,
     linear_map.map_add, range_one],
-  have f : ∀ x ∈ range n, 2 ≤ n + 1 - x,
-  { rintros x H, rw [mem_range] at H,
-    exact nat.le_sub_left_of_add_le (succ_le_succ H) },
   apply sum_eq_zero (λ x hx, _),
-  rw [sum_bernoulli _ (f x hx), zero_smul],
+  rw [sum_bernoulli],
+  have f : (ite (n + 1 - x = 1) (1 : ℚ) 0) = 0,
+    { simp only [ite_eq_right_iff, one_ne_zero], },
+  rw [f, zero_smul],
 end
 
 open power_series

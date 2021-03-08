c25ceb43d5c8a4b4d672b56fd6cf0183cc7d63df
Eric Wieser <wieser.eric@gmail.com>
2021-03-08 17:49:42 +0000

    fix

--- src/ring_theory/finiteness.lean
+++ src/ring_theory/finiteness.lean
@@ -183,9 +183,9 @@ begin
   { rw iff_quotient_mv_polynomial',
     rintro ⟨ι, hfintype, ⟨f, hsur⟩⟩,
     obtain ⟨n, equiv⟩ := @fintype.exists_equiv_fin ι hfintype,
-    replace equiv := mv_polynomial.rename_equiv (nonempty.some equiv),
-    use [n, alg_hom.comp f equiv.symm, function.surjective.comp hsur
-      (alg_equiv.symm equiv).surjective] },
+    replace equiv : _ ≃ₐ[R] _ := mv_polynomial.rename_equiv (nonempty.some equiv),
+    exact ⟨n, alg_hom.comp f equiv.symm, function.surjective.comp hsur
+      (alg_equiv.symm equiv).surjective⟩ },
   { rintro ⟨n, ⟨f, hsur⟩⟩,
     exact finite_type.of_surjective (finite_type.mv_polynomial R (fin n)) f hsur }
 end
@@ -239,7 +239,7 @@ variable (R)
 lemma mv_polynomial (ι : Type u_2) [fintype ι] : finite_presentation R (mv_polynomial ι R) :=
 begin
   obtain ⟨n, equiv⟩ := @fintype.exists_equiv_fin ι _,
-  replace equiv := mv_polynomial.rename_equiv (nonempty.some equiv),
+  replace equiv : _ ≃ₐ[R] _  := mv_polynomial.rename_equiv (nonempty.some equiv),
   use [n, alg_equiv.to_alg_hom equiv.symm],
   split,
   { exact (alg_equiv.symm equiv).surjective },

fab38e95e0760f2c992136a7c75a0b2e3f5023b2
adomani <adomani@gmail.com>
2021-03-09 21:10:06 +0100

    update example can_ordered

--- src/counterexamples/canonically_ordered_comm_semiring_two_mul.lean
+++ src/counterexamples/canonically_ordered_comm_semiring_two_mul.lean
@@ -5,6 +5,7 @@ Authors: Damiano Testa
 -/
 import data.zmod.basic
 import ring_theory.subsemiring
+import algebra.ordered_monoid
 /-!
 
 A `canonically_ordered_comm_semiring` with two different elements `a` and `b` such that
@@ -40,12 +41,9 @@ end from_Bhavik
 
 lemma mem_zmod_2 (a : zmod 2) : a = 0 ∨ a = 1 :=
 begin
-  cases a with a1 h1,
-  cases h1 with h1 h1,
-  { exact or.inr (by simpa only) },
-  { refine or.inl _,
-    congr,
-    rw nat.le_zero_iff.mp (nat.lt_succ_iff.mp (nat.succ_le_iff.mp h1)) }
+  rcases a with ⟨_ | _ | _ | _ | a_val, _ | ⟨_, _ | ⟨_, ⟨⟩⟩⟩⟩,
+  { exact or.inl rfl },
+  { exact or.inr rfl },
 end
 
 lemma add_self_zmod_2 (a : zmod 2) : a + a = 0 :=
@@ -85,6 +83,11 @@ instance preN2 : partial_order (ℕ × zmod 2) :=
 
 instance csrN2 : comm_semiring (ℕ × zmod 2) := by apply_instance
 
+instance csrN2_1 : add_cancel_comm_monoid (ℕ × zmod 2) :=
+{ add_left_cancel := λ a b c h, (add_right_inj a).mp h,
+  add_right_cancel := λ a b c h, (add_left_inj b).mp h,
+  ..Nxzmod_2.csrN2 }
+
 /-- A strict inequality forces the first components to be different. -/
 @[simp] lemma lt_def : a < b ↔ a.1 < b.1 :=
 begin
@@ -123,30 +126,237 @@ lemma mul_lt_mul_of_pos_right : ∀ (a b c : ℕ × zmod 2), a < b → 0 < c →
 λ a b c ab c0, lt_def.mpr ((mul_lt_mul_right (lt_def.mp c0)).mpr (lt_def.mp ab))
 
 instance ocsN2 : ordered_comm_semiring (ℕ × zmod 2) :=
-{ add_left_cancel := λ a b c h, (add_right_inj a).mp h,
-  add_right_cancel := λ a b c h, (add_left_inj b).mp h,
-  add_le_add_left := add_le_add_left,
+{ add_le_add_left := add_le_add_left,
   le_of_add_le_add_left := le_of_add_le_add_left,
   zero_le_one := zero_le_one,
   mul_lt_mul_of_pos_left := mul_lt_mul_of_pos_left,
   mul_lt_mul_of_pos_right := mul_lt_mul_of_pos_right,
+  ..Nxzmod_2.csrN2_1,
   ..(infer_instance : partial_order (ℕ × zmod 2)),
   ..(infer_instance : comm_semiring (ℕ × zmod 2)) }
 
-lemma mul_one_val {a b : ℕ × zmod 2}
-  (ha : (a = (1, 0) ∨ a = (1, 1))) (hb : (b = (1, 0) ∨ b = (1, 1))) :
-  a * b = (1, 0) ∨ a * b = (1, 1) :=
-begin
-  rcases ha with rfl | rfl;
-  { rcases hb with rfl | rfl;
-    simp }
-end
-
 end Nxzmod_2
+
+/-- Pullback an `ordered_comm_monoid` under an injective map. -/
+@[to_additive function.injective.ordered_add_comm_monoid
+"Pullback an `ordered_add_comm_monoid` under an injective map."]
+def function.injective.ordered_comm_monoid {α β : Type*} [ordered_comm_monoid α]
+  [has_one β] [has_mul β]
+  (f : β → α) (hf : function.injective f) (one : f 1 = 1)
+  (mul : ∀ x y, f (x * y) = f x * f y) :
+  ordered_comm_monoid β :=
+{ mul_le_mul_left := λ a b ab c,
+    show f (c * a) ≤ f (c * b), by simp [mul, mul_le_mul_left' ab],
+  lt_of_mul_lt_mul_left :=
+    λ a b c bc, @lt_of_mul_lt_mul_left' _ _ (f a) _ _ (by rwa [← mul, ← mul]),
+  ..partial_order.lift f hf,
+  ..hf.comm_monoid f one mul }
+
+/-- Pullback an `ordered_cancel_comm_monoid` under an injective map. -/
+@[to_additive function.injective.ordered_cancel_add_comm_monoid
+"Pullback an `ordered_cancel_add_comm_monoid` under an injective map."]
+def function.injective.ordered_cancel_comm_monoid {α β : Type*} [ordered_cancel_comm_monoid α]
+  [has_one β] [has_mul β]
+  (f : β → α) (hf : function.injective f) (one : f 1 = 1)
+  (mul : ∀ x y, f (x * y) = f x * f y) :
+  ordered_cancel_comm_monoid β :=
+{ le_of_mul_le_mul_left := λ x y z (xy : f (x * y) ≤ f (x * z)),
+    (by { rw [mul, mul] at xy, exact le_of_mul_le_mul_left' xy }),
+  ..hf.left_cancel_semigroup f mul,
+  ..hf.right_cancel_semigroup f mul,
+  ..hf.ordered_comm_monoid f one mul }
+
+/-- Pullback an `ordered_semiring` under an injective map. -/
+def function.injective.ordered_semiring {α β : Type*} [ordered_semiring α]
+  [has_zero β] [has_one β] [has_add β] [has_mul β]
+  (f : β → α) (hf : function.injective f) (zero : f 0 = 0) (one : f 1 = 1)
+  (add : ∀ x y, f (x + y) = f x + f y) (mul : ∀ x y, f (x * y) = f x * f y) :
+  ordered_semiring β :=
+{ zero_le_one := show f 0 ≤ f 1, by  simp only [zero, one, zero_le_one],
+  mul_lt_mul_of_pos_left := λ  a b c ab c0, show f (c * a) < f (c * b),
+    begin
+      rw [mul, mul],
+      refine mul_lt_mul_of_pos_left ab _,
+      rwa ← zero,
+    end,
+  mul_lt_mul_of_pos_right := λ a b c ab c0, show f (a * c) < f (b * c),
+    begin
+      rw [mul, mul],
+      refine mul_lt_mul_of_pos_right ab _,
+      rwa ← zero,
+    end,
+  ..hf.ordered_cancel_add_comm_monoid f zero add,
+  ..hf.semiring f zero one add mul }
+
+/-- Pullback an `ordered_comm_semiring` under an injective map. -/
+def function.injective.ordered_comm_semiring {α β : Type*} [ordered_comm_semiring α]
+  [has_zero β] [has_one β] [has_add β] [has_mul β]
+  (f : β → α) (hf : function.injective f) (zero : f 0 = 0) (one : f 1 = 1)
+  (add : ∀ x y, f (x + y) = f x + f y) (mul : ∀ x y, f (x * y) = f x * f y) :
+  ordered_comm_semiring β :=
+{ ..hf.comm_semiring f zero one add mul,
+  ..hf.ordered_semiring f zero one add mul }
 
 namespace ex_L
 
 open Nxzmod_2 subtype
+
+def L_1 : Type := { l : (ℕ × zmod 2) // l ≠ (0, 1) }
+
+instance zero : has_zero L_1 := ⟨⟨(0, 0), dec_trivial⟩⟩
+
+instance one : has_one L_1 := ⟨⟨(1, 1), dec_trivial⟩⟩
+
+instance : inhabited L_1 := ⟨1⟩
+
+lemma add_L_1 {a b : ℕ × zmod 2} (ha : a ≠ (0, 1)) (hb : b ≠ (0, 1)) :
+  a + b ≠ (0, 1) :=
+begin
+  rcases a with ⟨a, a2⟩,
+  rcases b with ⟨b, b2⟩,
+  cases b,
+  { rcases mem_zmod_2 b2 with rfl | rfl,
+    { simp [ha] },
+    { simpa only } },
+  { simp [(a + b).succ_ne_zero] }
+end
+
+lemma mul_L_1 {a b : ℕ × zmod 2} (ha : a ≠ (0, 1)) (hb : b ≠ (0, 1)) :
+  a * b ≠ (0, 1) :=
+begin
+  rcases a with ⟨a, a2⟩,
+  rcases b with ⟨b, b2⟩,
+  cases b,
+  { rcases mem_zmod_2 b2 with rfl | rfl;
+    rcases mem_zmod_2 a2 with rfl | rfl;
+    -- while this looks like a non-terminal `simp`, it (almost) isn't: there is only one goal where
+    -- it does not finish the proof and on that goal it asks to prove `false`
+    simp,
+    exact hb rfl },
+  cases a,
+  { rcases mem_zmod_2 b2 with rfl | rfl;
+    rcases mem_zmod_2 a2 with rfl | rfl;
+    -- while this looks like a non-terminal `simp`, it (almost) isn't: there is only one goal where
+    -- it does not finish the proof and on that goal it asks to prove `false`
+    simp,
+    exact ha rfl },
+  { simp [mul_ne_zero _ _, nat.succ_ne_zero _] }
+end
+
+instance has_add_L_1 : has_add L_1 :=
+{ add := λ ⟨a, ha⟩ ⟨b, hb⟩, ⟨a + b, add_L_1 ha hb⟩ }
+
+instance : has_mul L_1 :=
+{ mul := λ ⟨a, ha⟩ ⟨b, hb⟩, ⟨a * b, mul_L_1 ha hb⟩ }
+
+instance : ordered_comm_semiring L_1 :=
+begin
+  refine function.injective.ordered_comm_semiring _ subtype.coe_injective rfl rfl _ _;
+  { refine λ x y, _,
+    cases x,
+    cases y,
+    refl }
+end
+
+lemma bot_le : ∀ (a : L_1), 0 ≤ a :=
+begin
+  rintros ⟨⟨an, a2⟩, ha⟩,
+  cases an,
+  { rcases mem_zmod_2 a2 with (rfl | rfl),
+    { refl, },
+    { exact (ha rfl).elim } },
+  { refine or.inr _,
+    exact nat.succ_pos _ }
+end
+
+instance order_bot : order_bot L_1 :=
+{ bot := 0,
+  bot_le := bot_le,
+  ..(infer_instance : partial_order L_1) }
+
+lemma lt_of_add_lt_add_left : ∀ (a b c : L_1), a + b < a + c → b < c :=
+λ (a : L_1) {b c : L_1}, (add_lt_add_iff_left a).mp
+
+lemma le_iff_exists_add : ∀ (a b : L_1), a ≤ b ↔ ∃ (c : L_1), b = a + c :=
+begin
+  rintros ⟨⟨an, a2⟩, ha⟩ ⟨⟨bn, b2⟩, hb⟩,
+  rw subtype.mk_le_mk,
+  refine ⟨λ h, _, λ h, _⟩,
+  { rcases h with ⟨rfl, rfl⟩ | h,
+    { exact ⟨(0 : L_1), (add_zero _).symm⟩ },
+    { refine ⟨⟨⟨bn - an, b2 + a2⟩, _⟩, _⟩,
+      { rw [ne.def, prod.mk.inj_iff, not_and_distrib],
+        exact or.inl (ne_of_gt (nat.sub_pos_of_lt h)) },
+      { congr,
+        { exact (nat.add_sub_cancel' h.le).symm },
+        { change b2 = a2 + (b2 + a2),
+          rw [add_comm b2, ← add_assoc, add_self_zmod_2, zero_add] } } } },
+  { rcases h with ⟨⟨⟨c, c2⟩, hc⟩, abc⟩,
+    injection abc with abc,
+    rw [prod.mk_add_mk, prod.mk.inj_iff] at abc,
+    rcases abc with ⟨rfl, rfl⟩,
+    cases c,
+    { refine or.inl _,
+      rw [ne.def, prod.mk.inj_iff, eq_self_iff_true, true_and] at hc,
+      rcases mem_zmod_2 c2 with rfl | rfl,
+      { rw [add_zero, add_zero] },
+      { exact (hc rfl).elim } },
+    { refine or.inr _,
+      exact (lt_add_iff_pos_right _).mpr c.succ_pos } }
+end
+
+lemma eq_zero_or_eq_zero_of_mul_eq_zero : ∀ (a b : L_1), a * b = 0 → a = 0 ∨ b = 0 :=
+begin
+  rintros ⟨⟨a, a2⟩, ha⟩ ⟨⟨b, b2⟩, hb⟩ ab1,
+  injection ab1 with ab,
+  injection ab with abn ab2,
+  rw mul_eq_zero at abn,
+  rcases abn with ⟨rfl, rfl⟩ | ⟨rfl, rfl⟩,
+  { refine or.inl _,
+    rcases mem_zmod_2 a2 with rfl | rfl,
+    { refl },
+    { exact (ha rfl).elim } },
+  { refine or.inr _,
+    rcases mem_zmod_2 b2 with rfl | rfl,
+    { refl },
+    { exact (hb rfl).elim } }
+end
+
+instance can : canonically_ordered_comm_semiring L_1 :=
+{ lt_of_add_lt_add_left := lt_of_add_lt_add_left,
+  le_iff_exists_add := le_iff_exists_add,
+  eq_zero_or_eq_zero_of_mul_eq_zero := eq_zero_or_eq_zero_of_mul_eq_zero,
+  ..(infer_instance : order_bot L_1),
+  ..(infer_instance : ordered_comm_semiring L_1) }
+
+lemma one_eq : (1 : ℕ × zmod 2).2 = 1 := rfl
+
+lemma one_eq_L_1 : (1 : L_1).1.2 = 1 := rfl
+
+/--
+The elements `(1,0)` and `(1,1)` of `L_1` are different, but their doubles coincide.
+-/
+example : ∃ a b : L_1, a ≠ b ∧ 2 * a = 2 * b :=
+begin
+  refine ⟨⟨(1,0), by simp⟩, 1, λ (h : (⟨(1, 0), _⟩ : L_1) = ⟨⟨1, 1⟩, _⟩), _, rfl⟩,
+  obtain (F : (0 : zmod 2) = 1) := congr_arg (λ j : L_1, j.1.2) h,
+  cases F,
+end
+
+#exit
+
+lemma mul_one_val {a b : ℕ × zmod 2}
+  (ha : (a = (1, 0) ∨ a = (1, 1))) (hb : (b = (1, 0) ∨ b = (1, 1))) :
+  a * b = (1, 0) ∨ a * b = (1, 1) :=
+by rcases ha with rfl | rfl; { rcases hb with rfl | rfl; simp }
+
+instance can : canonically_ordered_comm_semiring L_1 :=
+{ lt_of_add_lt_add_left := lt_of_add_lt_add_left,
+  le_iff_exists_add := le_iff_exists_add,
+  eq_zero_or_eq_zero_of_mul_eq_zero := eq_zero_or_eq_zero_of_mul_eq_zero,
+  ..(infer_instance : order_bot L),
+  ..(infer_instance : ordered_comm_semiring L_1) }
+
 
 /-- A slightly different example. -/
 @[derive [comm_semiring]]

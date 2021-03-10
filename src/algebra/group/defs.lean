96b74307b5d4f07f817fb416d90f47d91436b418
Oliver Nash <github@olivernash.org>
2021-03-10 16:35:39 +0000

    Fix build failure 3 / ?
    If this doesn't work, I think I'll give up on inserting `mul_one_class` /
    `add_zero_class` into the `monoid` / `add_monoid` definitions.

--- src/algebra/group/defs.lean
+++ src/algebra/group/defs.lean
@@ -189,12 +189,12 @@ class add_zero_class (M : Type u) extends has_zero M, has_add M :=
 (add_zero : ∀ (a : M), a + 0 = a)
 
 /-- A `monoid` is a `semigroup` with an element `1` such that `1 * a = a * 1 = a`. -/
-@[ancestor semigroup mul_one_class]
-class monoid (M : Type u) extends semigroup M, mul_one_class M
+@[ancestor semigroup has_one mul_one_class]
+class monoid (M : Type u) extends semigroup M, has_one M, mul_one_class M
 
 /-- An `add_monoid` is an `add_semigroup` with an element `0` such that `0 + a = a + 0 = a`. -/
-@[ancestor add_semigroup add_zero_class]
-class add_monoid (M : Type u) extends add_semigroup M, add_zero_class M
+@[ancestor add_semigroup has_zero add_zero_class]
+class add_monoid (M : Type u) extends add_semigroup M, has_zero M, add_zero_class M
 
 attribute [to_additive] monoid
 attribute [to_additive] mul_one_class

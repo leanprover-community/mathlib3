6b38cd964a530f65ff891f233f89e34389e41d23
adomani <adomani@gmail.com>
2021-03-11 11:47:11 +0100

    add copyright

--- src/algebra/smul_with_zero.lean
+++ src/algebra/smul_with_zero.lean
@@ -1,3 +1,8 @@
+/-
+Copyright (c) 2021 Damiano Testa. All rights reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
+Authors: Damiano Testa
+-/
 import group_theory.group_action.defs
 
 variables (R M : Type*) [has_zero M]

177e48421636cf8ce7c43384fd68fb0b2bfc6a42
Benjamin Davidson <68528197+benjamindavidson@users.noreply.github.com>
2021-03-11 04:21:57 -0500

    lint

--- src/data/nat/choose/sum.lean
+++ src/data/nat/choose/sum.lean
@@ -30,33 +30,33 @@ begin
   let t : ℕ → ℕ → α := λ n m, x ^ m * (y ^ (n - m)) * (choose n m),
   change (x + y) ^ n = ∑ m in range (n + 1), t n m,
   have h_first : ∀ n, t n 0 = y ^ n :=
-    λ n, by { dsimp [t], rw[choose_zero_right, nat.cast_one, mul_one, one_mul] },
+    λ n, by { dsimp [t], rw [choose_zero_right, nat.cast_one, mul_one, one_mul] },
   have h_last : ∀ n, t n n.succ = 0 :=
     λ n, by { dsimp [t], rw [choose_succ_self, nat.cast_zero, mul_zero] },
-  have h_middle : ∀ (n i : ℕ), (i ∈ finset.range n.succ) →
+  have h_middle : ∀ (n i : ℕ), (i ∈ range n.succ) →
    ((t n.succ) ∘ nat.succ) i = x * (t n i) + y * (t n i.succ) :=
   begin
     intros n i h_mem,
-    have h_le : i ≤ n := nat.le_of_lt_succ (finset.mem_range.mp h_mem),
+    have h_le : i ≤ n := nat.le_of_lt_succ (mem_range.mp h_mem),
     dsimp [t],
     rw [choose_succ_succ, nat.cast_add, mul_add],
     congr' 1,
-    { rw[pow_succ x, succ_sub_succ, mul_assoc, mul_assoc, mul_assoc] },
-    { rw[← mul_assoc y, ← mul_assoc y, (h.symm.pow_right i.succ).eq],
+    { rw [pow_succ x, succ_sub_succ, mul_assoc, mul_assoc, mul_assoc] },
+    { rw [← mul_assoc y, ← mul_assoc y, (h.symm.pow_right i.succ).eq],
       by_cases h_eq : i = n,
       { rw [h_eq, choose_succ_self, nat.cast_zero, mul_zero, mul_zero] },
-      { rw[succ_sub (lt_of_le_of_ne h_le h_eq)],
-        rw[pow_succ y, mul_assoc, mul_assoc, mul_assoc, mul_assoc] } }
+      { rw [succ_sub (lt_of_le_of_ne h_le h_eq)],
+        rw [pow_succ y, mul_assoc, mul_assoc, mul_assoc, mul_assoc] } }
   end,
   induction n with n ih,
-  { rw [pow_zero, sum_range_succ, range_zero, sum_empty, add_zero],
+  { rw [pow_zero, sum_range_succ, range_zero, sum_empty, zero_add],
     dsimp [t], rw [choose_self, nat.cast_one, mul_one, mul_one] },
-  { rw[sum_range_succ', h_first],
-    rw[finset.sum_congr rfl (h_middle n), finset.sum_add_distrib, add_assoc],
-    rw[pow_succ (x + y), ih, add_mul, finset.mul_sum, finset.mul_sum],
+  { rw [sum_range_succ', h_first],
+    rw [sum_congr rfl (h_middle n), sum_add_distrib, add_assoc],
+    rw [pow_succ (x + y), ih, add_mul, mul_sum, mul_sum],
     congr' 1,
-    rw[finset.sum_range_succ', finset.sum_range_succ, h_first, h_last,
-       mul_zero, zero_add, pow_succ] }
+    rw [sum_range_succ', sum_range_succ, h_first, h_last,
+       mul_zero, add_zero, pow_succ] }
 end
 
 /-- The binomial theorem -/

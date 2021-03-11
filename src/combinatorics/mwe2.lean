b0d90e30ff486e6fc3ad1f53c27e6419674bab98
Bhavik Mehta <bhavikmehta8@gmail.com>
2021-03-11 18:41:24 +0000

    wip

--- src/combinatorics/mwe2.lean
+++ src/combinatorics/mwe2.lean
@@ -1,6 +1,25 @@
 import analysis.convex.basic
 import linear_algebra.affine_space.independent
 
+open_locale classical
+
+lemma disjoint_convex_hulls {m : ℕ} {X : set (fin m → ℝ)}
+  (hX : affine_independent ℝ (λ p, p : X → fin m → ℝ))
+  (Y₁ Y₂ : set (fin m → ℝ)) (hY₁ : Y₁ ⊆ X) (hY₂ : Y₂ ⊆ X) :
+  convex_hull Y₁ ∩ convex_hull Y₂ ⊆ convex_hull (Y₁ ∩ Y₂) :=
+begin
+end
+
+lemma kenny (M : Type*) [add_comm_group M] [vector_space ℝ M] (s : affine_subspace ℝ M) :
+  convex (s : set M) :=
+λ x y hxs hys a b ha hb hab,
+calc a • x + b • y = b • (y - x) + x : convex.combo_to_vadd hab
+               ... ∈ s : s.2 _ hys hxs hxs
+
+example {m : ℕ} (X : set (fin m → ℝ)) :
+  convex (affine_span ℝ X : set (fin m → ℝ)) :=
+kenny _ _
+
 example {m : ℕ}
   (points : fin (m + 1) → fin (m + 1) → ℝ) -- We have (m+1) points in R^(m+1)
   (independent : affine_independent ℝ points) -- which are affinely independent

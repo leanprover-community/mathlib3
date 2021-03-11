2f732734d778ed186bc29a3c19d667483c6e91cf
damiano <adomani@gmail.com>
2021-03-11 18:13:57 +0100

    Update src/algebra/module_regular.lean
    Co-authored-by: Eric Wieser <wieser.eric@gmail.com>

--- src/algebra/module_regular.lean
+++ src/algebra/module_regular.lean
@@ -72,7 +72,7 @@ end
 /--  The "most used" implication of `mul_and_mul_iff`, with split hypotheses, instead of `∧`. -/
 lemma is_regular.and_of_mul_of_mul (ab : is_regular M (a * b)) (ba : is_regular M (b * a)) :
   is_regular M a ∧ is_regular M b :=
-is_regular_mul_and_mul_iff.mp ⟨ab, ba⟩
+⟨ba.of_smul b, ab.of_smul a⟩
 
 @[simp] lemma is_regular_one : is_regular M (1 : R) :=
 begin

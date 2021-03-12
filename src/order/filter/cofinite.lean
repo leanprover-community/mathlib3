11b9e18a36500f165842e864a1bf2db554be8c82
AlexKontorovich <58564076+AlexKontorovich@users.noreply.github.com>
2021-03-12 17:19:16 -0500

    version 2: [not_top_order \beta] removed

--- src/order/filter/cofinite.lean
+++ src/order/filter/cofinite.lean
@@ -91,27 +91,33 @@ lemma nat.frequently_at_top_iff_infinite {p : ℕ → Prop} :
   (∃ᶠ n in at_top, p n) ↔ set.infinite {n | p n} :=
 by simp only [← nat.cofinite_eq_at_top, frequently_cofinite_iff_infinite]
 
-lemma filter.tendsto.exists_forall_le {α β : Type*} [nonempty α] [linear_order β] [no_top_order β]
+lemma filter.tendsto.exists_forall_le {α β : Type*} [nonempty α] [linear_order β]
   {f : α → β} (hf : tendsto f cofinite at_top) :
   ∃ a₀, ∀ a, f a₀ ≤ f a :=
 begin
-  -- take the inverse image, `small_vals`, of some bounded nonempty set; it's finite, so has a min
   inhabit α,
-  haveI : inhabited β := ⟨f (default α)⟩,
-  let small_vals : finset α := (filter.eventually_cofinite.mp
-    ((at_top_basis_Ioi.tendsto_right_iff).1 hf (f $ default α) trivial)).to_finset,
-  have default_in : default α ∈ small_vals := by simp,
-  obtain ⟨a₀, -, others_bigger⟩ := small_vals.exists_min_image f ⟨default α, default_in⟩,
-  use a₀,
-  intros a,
-  by_cases h : a ∈ small_vals,
-  { exact others_bigger a h },
-  have inDef : f(a₀) ≤ f (default α) := others_bigger (default α) default_in,
-  refine le_trans inDef (le_of_lt _),
-  simpa using h,
+  by_cases not_all_top : ∃ y, ∃ x, f y < x,
+  { -- take the inverse image, `small_vals`, of some bounded nonempty set; it's finite so has a min
+    haveI : inhabited β := ⟨f (default α)⟩,
+    obtain ⟨y, x, hx⟩ := not_all_top,
+    let small_vals : finset α := (filter.eventually_cofinite.mp
+      ((at_top_basis.tendsto_right_iff).1 hf x trivial)).to_finset,
+    have y_in : y ∈ small_vals := by simp [hx],
+    obtain ⟨a₀, -, others_bigger⟩ := small_vals.exists_min_image f ⟨y, y_in⟩,
+    use a₀,
+    intros a,
+    by_cases h : a ∈ small_vals,
+    { exact others_bigger a h },
+    refine le_trans (others_bigger y y_in) (le_trans hx.le _),
+    simpa using h },
+  { -- in this case, f is constant because all values are at top
+    push_neg at not_all_top,
+    use default α,
+    intros a,
+    exact not_all_top a (f $ default α) }
 end
 
-lemma filter.tendsto.exists_forall_ge {α β : Type*} [nonempty α] [linear_order β] [no_bot_order β]
+lemma filter.tendsto.exists_forall_ge {α β : Type*} [nonempty α] [linear_order β]
   {f : α → β} (hf : tendsto f cofinite at_bot) :
   ∃ a₀, ∀ a, f a ≤ f a₀ :=
-@filter.tendsto.exists_forall_le _ (order_dual β) _ _ _ _ hf
+@filter.tendsto.exists_forall_le _ _ _ _ _ hf

d3a673937616979113a3d9bd7b99114a0eac0c4c
Yakov Pechersky <ffxen158@gmail.com>
2021-03-15 11:55:57 -0400

    only to nsmul by suggestion

--- src/data/finsupp/basic.lean
+++ src/data/finsupp/basic.lean
@@ -1515,9 +1515,9 @@ begin
 end
 
 lemma to_multiset_sum_single {ι : Type*} (s : finset ι) (n : ℕ) :
-  finsupp.to_multiset (∑ i in s, single i n) = n • s.val :=
+  finsupp.to_multiset (∑ i in s, single i n) = n •ℕ s.val :=
 by simp_rw [to_multiset_sum, finsupp.to_multiset_single, multiset.singleton_eq_singleton,
-            sum_nsmul, sum_multiset_singleton, nsmul_eq_smul]
+            sum_nsmul, sum_multiset_singleton]
 
 lemma card_to_multiset (f : α →₀ ℕ) : f.to_multiset.card = f.sum (λa, id) :=
 by simp [to_multiset_apply, add_monoid_hom.map_finsupp_sum, function.id_def]

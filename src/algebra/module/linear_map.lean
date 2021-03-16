cf57fa781861b3d317b90a54e6f627fa3eef725f
Ruben-VandeVelde <65514131+Ruben-VandeVelde@users.noreply.github.com>
2021-03-16 20:24:32 +0100

    Update src/algebra/module/linear_map.lean

--- src/algebra/module/linear_map.lean
+++ src/algebra/module/linear_map.lean
@@ -6,7 +6,6 @@ Authors: Nathaniel Thomas, Jeremy Avigad, Johannes Hölzl, Mario Carneiro, Anne 
 import algebra.group.hom
 import algebra.module.basic
 import algebra.group_action_hom
-import logic.function.iterate
 
 /-!
 # Linear maps and linear equivalences

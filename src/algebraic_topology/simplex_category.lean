145e63d610e3ebba18018502e3956cc9d132c83b
Scott Morrison <scott.morrison@gmail.com>
2021-03-17 09:46:18 +1100

    use derive

--- src/algebraic_topology/simplex_category.lean
+++ src/algebraic_topology/simplex_category.lean
@@ -30,7 +30,7 @@ def simplex_category := ℕ
 
 namespace simplex_category
 
-instance : small_category simplex_category :=
+instance small_category : small_category simplex_category :=
 { hom := λ n m, preorder_hom (fin (n+1)) (fin (m+1)),
   id := λ m, preorder_hom.id,
   comp := λ _ _ _ f g, preorder_hom.comp g f, }
@@ -293,14 +293,12 @@ def is_skeleton_of : is_skeleton_of NonemptyFinLinOrd.{u} simplex_category skele
   eqv := skeletal_functor.is_equivalence }
 
 /-- The truncated simplex category. -/
+@[derive small_category]
 def truncated (n : ℕ) := {a : simplex_category // a.len ≤ n}
 
 namespace truncated
 
 instance {n} : inhabited (truncated n) := ⟨⟨[0],by simp⟩⟩
-
--- Derive didn't work for some reason...
-instance {n} : small_category (truncated n) := by {unfold truncated, apply_instance}
 
 /--
 The fully faithful inclusion of the truncated simplex category into the usual

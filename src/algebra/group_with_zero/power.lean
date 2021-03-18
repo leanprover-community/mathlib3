ae40e96fcdf43dea0208e7d0e4213439bd086b70
Yakov Pechersky <ffxen158@gmail.com>
2021-03-18 18:00:43 -0400

    remove unnecessary import change

--- src/algebra/group_with_zero/power.lean
+++ src/algebra/group_with_zero/power.lean
@@ -3,7 +3,7 @@ Copyright (c) 2020 Johan Commelin. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Johan Commelin
 -/
-import algebra.group_power.lemmas
+import algebra.group_power
 
 /-!
 # Powers of elements of groups with an adjoined zero element

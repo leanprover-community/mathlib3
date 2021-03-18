432ad856dc59d9a2ec8b8581459d972c05c97fa0
thejohncrafter <thejohncrafter@gmail.com>
2021-03-18 10:49:08 +0100

    Performed requested changes

--- src/ring_theory/int/basic.lean
+++ src/ring_theory/int/basic.lean
@@ -342,15 +342,14 @@ instance decidable_int : decidable_rel (λ a b : ℤ, (multiplicity a b).dom) :=
 
 end multiplicity
 
-lemma induction_on_primes {P : nat → Prop} (h₁ : P 0) (h₂ : P 1)
-  (h : ∀ p a : ℕ, nat.prime p → P a → P (p * a)) : ∀ n: ℕ, P n :=
+lemma induction_on_primes {P : nat → Prop} (h₀ : P 0) (h₁ : P 1)
+  (h : ∀ p a : ℕ, nat.prime p → P a → P (p * a)) (n : ℕ) : P n :=
 begin
-  intro n,
   apply unique_factorization_monoid.induction_on_prime,
-  exact h₁,
+  exact h₀,
   { intros n h,
     rw nat.is_unit_iff.1 h,
-    exact h₂, },
+    exact h₁, },
   { intros a p _ hp ha,
     exact h p a (nat.prime_iff_prime.2 hp) ha, },
 end
@@ -388,7 +387,7 @@ begin
   exact associated_iff_eq,
 end
 
-lemma int.associated_iff (a : ℤ) (b : ℤ) : associated a b ↔ (a = b ∨ a = -b) :=
+lemma int.associated_iff {a b : ℤ} : associated a b ↔ (a = b ∨ a = -b) :=
 begin
   rw int.associated_iff_nat_abs,
   exact int.nat_abs_eq_nat_abs_iff,

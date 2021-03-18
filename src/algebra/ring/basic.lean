9f651ad20b7bc5e11245d43a8148a58f61b03f62
Oliver Nash <github@olivernash.org>
2021-03-18 09:22:21 +0000

    Drop spurious priority settings

--- src/algebra/ring/basic.lean
+++ src/algebra/ring/basic.lean
@@ -116,15 +116,12 @@ class semiring (α : Type u) extends add_comm_monoid α, monoid_with_zero α, di
 section semiring
 variables [semiring α]
 
-@[priority 200] -- see Note [lower instance priority]
 instance semiring.to_non_unital_non_assoc_semiring : non_unital_non_assoc_semiring α :=
 { ..‹semiring α› }
 
-@[priority 200] -- see Note [lower instance priority]
 instance semiring.non_unital_semiring : non_unital_semiring α :=
 { ..‹semiring α› }
 
-@[priority 200] -- see Note [lower instance priority]
 instance semiring.non_assoc_semiring : non_assoc_semiring α :=
 { ..‹semiring α› }
 

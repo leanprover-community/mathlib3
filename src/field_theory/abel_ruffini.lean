3d816eefb39a68834a442dcc60c2d424234fa2eb
tb65536 <tb65536@users.noreply.github.com>
2021-03-17 20:52:55 -0700

    Update src/field_theory/abel_ruffini.lean
    Co-authored-by: Scott Morrison <scott.morrison@gmail.com>

--- src/field_theory/abel_ruffini.lean
+++ src/field_theory/abel_ruffini.lean
@@ -1,5 +1,5 @@
 /-
-Copyright (c) 2020 Thomas Brownng and Patrick Lutz. All rights reserved.
+Copyright (c) 2020 Thomas Browning and Patrick Lutz. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Thomas Browning and Patrick Lutz
 -/

172b959be7afee52eea0ae62b9b53be3a6b70646
Jakob Scholbach <jakob.scholbach@uni-muenster.de>
2021-03-19 10:16:34 +0100

    author format

--- src/algebra/char_p/exp_char.lean
+++ src/algebra/char_p/exp_char.lean
@@ -1,7 +1,7 @@
 /-
 Copyright (c) 2021 Jakob Scholbach. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
-Author: Jakob Scholbach.
+Authors: Jakob Scholbach
 -/
 import algebra.char_p.basic
 import algebra.char_zero

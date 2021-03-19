77d9391c48eb12fcc7997eff8a51c35b19eff254
Yakov Pechersky <ffxen158@gmail.com>
2021-03-19 00:12:01 -0400

    fix digits

--- src/data/nat/digits.lean
+++ src/data/nat/digits.lean
@@ -127,7 +127,7 @@ begin
         { simp [nat.add_mod, nat.mod_eq_of_lt w], },
         { simp [mul_comm (b+2), nat.add_mul_div_right, nat.div_eq_of_lt w], }
       },
-      { apply nat.succ_pos, }, }, },
+      { rw mul_succ, apply nat.succ_pos, }, }, },
 end
 
 /--

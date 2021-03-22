13c1266294ba175c378db2f578c7b95e863c550d
Adam Topaz <github@adamtopaz.com>
2021-03-22 08:06:03 -0600

    Add comments + linting

--- src/category_theory/limits/kan_extension.lean
+++ src/category_theory/limits/kan_extension.lean
@@ -1,6 +1,32 @@
+/-
+Copyright (c) 2021 Adam Topaz. All rights reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
+Authors: Bhavik Mehta, Adam Topaz
+-/
 import category_theory.punit
 import category_theory.comma
 import category_theory.limits.functor_category
+
+/-!
+
+# Kan extensions
+
+This file defines the right and left Kan extensions of a functor.
+They exist under the assumption that the target category has enough limits
+resp. colimits.
+
+The main definitions are `Ran ι` and `Lan ι`, where `ι : S ⥤ L` is a functor.
+Namely, `Ran ι` is the right Kan extension, while `Lan ι` is the left Kan extension,
+both as functors `(S ⥤ D) ⥤ (L ⥤ D)`.
+
+To access the right resp. left adjunction associated to these, use `Ran.adjunction`
+resp. `Lan.adjunction`.
+
+# Projects
+
+A lot of boilerplate could be generalized by defining and working with pseudofunctors.
+
+-/
 
 noncomputable theory
 
@@ -18,18 +44,22 @@ namespace Ran
 
 local attribute [simp] comma.snd comma.map_left
 
-@[simp, derive category]
+/-- The index category of limits used to define `Ran`. -/
+@[simp, derive category, nolint has_inhabited_instance]
 def index (l : L) := comma (functor.from_punit l) ι
 
 variable {ι}
 
+/-- Make a term of type `Ran.index ι x`. -/
 @[simp]
 def index.mk {x : L} {y : S} (f : x ⟶ ι.obj y) : index ι x := ⟨⟨⟩, y, f⟩
 
+/-- The functor `Ran.index ι y ⥤ Ran.index ι x` associated to a morphism `x ⟶ y`. -/
 @[simp]
-def index_map {x y : L} (f : x ⟶ y) : index ι y ⥤ index ι x :=
+def index.map {x y : L} (f : x ⟶ y) : index ι y ⥤ index ι x :=
 comma.map_left _ ((functor.const _).map f)
 
+/-- Make a morphism in `Ran.index ι x`. -/
 @[simps]
 def index.mk_hom {x : L} {y z : S} (f : x ⟶ ι.obj y) (g : y ⟶ z) :
   index.mk f ⟶ index.mk (f ≫ ι.map g) :=
@@ -37,25 +67,25 @@ def index.mk_hom {x : L} {y z : S} (f : x ⟶ ι.obj y) (g : y ⟶ z) :
   right := g,
   w' := by simpa }
 
-@[simp]
-lemma index_map_mk {x y : L} {z : S} (f : x ⟶ ι.obj z) (g : y ⟶ x) :
-  (index_map g).obj (index.mk f) = index.mk (g ≫ f) := rfl
+lemma index.map_mk {x y : L} {z : S} (f : x ⟶ ι.obj z) (g : y ⟶ x) :
+  (index.map g).obj (index.mk f) = index.mk (g ≫ f) := rfl
 
-@[simp]
-lemma index_map_id {x : L} {j : index ι x} :
-  (index_map (𝟙 x)).obj j = j := by {cases j, tidy}
+lemma index.map_id {x : L} {j : index ι x} :
+  (index.map (𝟙 x)).obj j = j := by {cases j, tidy}
 
-@[simp]
-lemma index_map_comp {x y z : L} (f : z ⟶ y) (g : y ⟶ x) (j : index ι x) :
-  (index_map (f ≫ g)).obj j = (index_map f).obj ((index_map g).obj j) :=
+lemma index.map_comp {x y z : L} (f : z ⟶ y) (g : y ⟶ x) (j : index ι x) :
+  (index.map (f ≫ g)).obj j = (index.map f).obj ((index.map g).obj j) :=
 by {cases j, tidy}
 
 variable (ι)
+
+/-- The diagram indexed by `Ran.index ι x` used to define `Ran`. -/
 @[simp]
 def diagram (F : S ⥤ D) (x : L) : index ι x ⥤ D :=
   comma.snd (functor.from_punit x) ι ⋙ F
 variable {ι}
 
+/-- A cone over `Ran.diagram ι F x` used to define `Ran`. -/
 @[simp]
 def cone {F : S ⥤ D} {G : L ⥤ D} (x : L) (f : ι ⋙ G ⟶ F) :
   cone (diagram ι F x) :=
@@ -73,16 +103,17 @@ def cone {F : S ⥤ D} {G : L ⥤ D} (x : L) (f : ι ⋙ G ⟶ F) :
 
 variable (ι)
 
+/-- An auxiliary definition used to define `Ran`. -/
 @[simps]
 def obj_aux (F : S ⥤ D) [∀ x, has_limits_of_shape (index ι x) D] : L ⥤ D :=
 { obj := λ x, limit (diagram ι F x),
-  map := λ x y f, limit.pre (diagram _ _ _) (index_map f),
+  map := λ x y f, limit.pre (diagram _ _ _) (index.map f),
   map_id' := begin
     intro l,
     ext j,
     simp only [category.id_comp, limit.pre_π],
     congr' 1,
-    rw [index_map_id],
+    rw [index.map_id],
   end,
   map_comp' := begin
     intros x y z f g,
@@ -90,8 +121,9 @@ def obj_aux (F : S ⥤ D) [∀ x, has_limits_of_shape (index ι x) D] : L ⥤ D 
     erw [limit.pre_pre, limit.pre_π, limit.pre_π],
     congr' 1,
     tidy,
-  end }.
+  end }
 
+/-- An auxiliary definition used to define `Ran` and `Ran.adjunction`. -/
 @[simps]
 def equiv [∀ x, has_limits_of_shape (index ι x) D] (F : S ⥤ D) (G : L ⥤ D) :
   (G ⟶ obj_aux ι F) ≃ (ι ⋙ G ⟶ F) :=
@@ -111,7 +143,7 @@ def equiv [∀ x, has_limits_of_shape (index ι x) D] (F : S ⥤ D) (G : L ⥤ D
       intros x y ff,
       ext j,
       erw [limit.lift_pre, limit.lift_π, category.assoc, limit.lift_π (cone _ f) j],
-      delta cone index_map,
+      delta cone index.map,
       tidy,
     end },
   left_inv := begin
@@ -126,14 +158,16 @@ def equiv [∀ x, has_limits_of_shape (index ι x) D] (F : S ⥤ D) (G : L ⥤ D
     cases j,
     tidy,
   end,
-  right_inv := by tidy }.
+  right_inv := by tidy }
 
+/-- A variant of `Ran.equiv` with `whiskering_left` instead of functor composition. -/
 @[simps]
 def equiv' [∀ x, has_limits_of_shape (index ι x) D] (F : S ⥤ D) (G : L ⥤ D) :
   (G ⟶ obj_aux ι F) ≃ (((whiskering_left _ _ _).obj ι).obj G ⟶ F) := equiv _ _ _
 
 end Ran
 
+/-- The right Kan extension of a functor. -/
 @[simps]
 def Ran [∀ X, has_limits_of_shape (Ran.index ι X) D] : (S ⥤ D) ⥤ L ⥤ D :=
 adjunction.right_adjoint_of_equiv (λ F G, (Ran.equiv' ι G F).symm) (by tidy)
@@ -142,7 +176,7 @@ namespace Ran
 
 variable (D)
 
-@[simps]
+/-- The adjunction associated to `Ran`. -/
 def adjunction [∀ X, has_limits_of_shape (Ran.index ι X) D] :
   (whiskering_left _ _ D).obj ι ⊣ Ran ι :=
 adjunction.adjunction_of_equiv_right _ _
@@ -153,18 +187,22 @@ namespace Lan
 
 local attribute [simp] comma.fst comma.map_right
 
-@[simp, derive category]
+/-- The index category of limits used to define `Lan`. -/
+@[simp, derive category, nolint has_inhabited_instance]
 def index (l : L) := comma ι (functor.from_punit l)
 
 variable {ι}
 
+/-- Make a term of type `Lan.index ι x`. -/
 @[simp]
 def index.mk {x : L} {y : S} (f : ι.obj y ⟶ x) : index ι x := ⟨y, ⟨⟩, f⟩
 
+/-- The functor `Lan.index ι x ⥤ Lan.index ι y` associated to a morphism `x ⟶ y`. -/
 @[simp]
-def index_map {x y : L} (f : x ⟶ y) : index ι x ⥤ index ι y :=
+def index.map {x y : L} (f : x ⟶ y) : index ι x ⥤ index ι y :=
 comma.map_right _ ((functor.const _).map f)
 
+/-- Make a morphism in `Lan.index ι x`. -/
 @[simps]
 def index.mk_hom {x : L} {y z : S} (f : ι.obj z ⟶ x) (g : y ⟶ z) :
   index.mk (ι.map g ≫ f) ⟶ index.mk f :=
@@ -172,25 +210,24 @@ def index.mk_hom {x : L} {y z : S} (f : ι.obj z ⟶ x) (g : y ⟶ z) :
   right := 𝟙 _,
   w' := by simpa }
 
-@[simp]
-lemma index_map_mk {x y : L} {z : S} (f : ι.obj z ⟶ x) (g : x ⟶ y) :
-  (index_map g).obj (index.mk f) = index.mk (f ≫ g) := rfl
+lemma index.map_mk {x y : L} {z : S} (f : ι.obj z ⟶ x) (g : x ⟶ y) :
+  (index.map g).obj (index.mk f) = index.mk (f ≫ g) := rfl
 
-@[simp]
-lemma index_map_id {x : L} {j : index ι x} :
-  (index_map (𝟙 x)).obj j = j := by {cases j, tidy}
+lemma index.map_id {x : L} {j : index ι x} :
+  (index.map (𝟙 x)).obj j = j := by {cases j, tidy}
 
-@[simp]
-lemma index_map_comp {x y z : L} (f : x ⟶ y) (g : y ⟶ z) (j : index ι x) :
-  (index_map (f ≫ g)).obj j = (index_map g).obj ((index_map f).obj j) :=
+lemma index.map_comp {x y z : L} (f : x ⟶ y) (g : y ⟶ z) (j : index ι x) :
+  (index.map (f ≫ g)).obj j = (index.map g).obj ((index.map f).obj j) :=
 by {cases j, tidy}
 variable (ι)
 
+/-- The diagram indexed by `Ran.index ι x` used to define `Ran`. -/
 @[simp]
 def diagram (F : S ⥤ D) (x : L) : index ι x ⥤ D :=
   comma.fst ι (functor.from_punit x) ⋙ F
 variable {ι}
 
+/-- A cocone over `Lan.diagram ι F x` used to define `Lan`. -/
 @[simp]
 def cocone {F : S ⥤ D} {G : L ⥤ D} (x : L) (f : F ⟶ ι ⋙ G) :
   cocone (diagram ι F x) :=
@@ -207,31 +244,33 @@ def cocone {F : S ⥤ D} {G : L ⥤ D} (x : L) (f : F ⟶ ι ⋙ G) :
 
 variable (ι)
 
+/-- An auxiliary definition used to define `Lan`. -/
 @[simps]
 def obj_aux (F : S ⥤ D) [∀ x, has_colimits_of_shape (index ι x) D] : L ⥤ D :=
 { obj := λ x, colimit (diagram ι F x),
-  map := λ x y f, colimit.pre (diagram _ _ _) (index_map f),
+  map := λ x y f, colimit.pre (diagram _ _ _) (index.map f),
   map_id' := begin
     intro l,
     ext j,
     erw [colimit.ι_pre, category.comp_id],
     congr' 1,
-    rw index_map_id,
+    rw index.map_id,
   end,
   map_comp' := begin
     intros x y z f g,
     ext j,
-    have := colimit.pre_pre (diagram ι F z) (index_map g) (index_map f),
+    have := colimit.pre_pre (diagram ι F z) (index.map g) (index.map f),
     change _ = _ ≫
-      colimit.pre (index_map g ⋙ diagram ι F z) (index_map f) ≫
-      colimit.pre (diagram ι F z) (index_map g),
+      colimit.pre (index.map g ⋙ diagram ι F z) (index.map f) ≫
+      colimit.pre (diagram ι F z) (index.map g),
     rw this,
-    change _ = colimit.ι ((index_map f ⋙ index_map g) ⋙ diagram ι F z) j ≫ _,
+    change _ = colimit.ι ((index.map f ⋙ index.map g) ⋙ diagram ι F z) j ≫ _,
     rw [colimit.ι_pre, colimit.ι_pre],
     congr' 1,
-    simp only [index_map_comp, functor.comp_obj],
-  end }.
+    simp only [index.map_comp, functor.comp_obj],
+  end }
 
+/-- An auxiliary definition used to define `Lan` and `Lan.adjunction`. -/
 @[simps]
 def equiv [∀ x, has_colimits_of_shape (index ι x) D] (F : S ⥤ D) (G : L ⥤ D) :
   (obj_aux ι F ⟶ G) ≃ (F ⟶ ι ⋙ G ) :=
@@ -243,12 +282,12 @@ def equiv [∀ x, has_colimits_of_shape (index ι x) D] (F : S ⥤ D) (G : L ⥤
     erw [← f.naturality (ι.map ff)],
     delta obj_aux,
     erw [← category.assoc, ← category.assoc],
-    erw colimit.ι_pre (diagram ι F (ι.obj y)) (index_map (ι.map ff)) (index.mk (𝟙 _)),
+    erw colimit.ι_pre (diagram ι F (ι.obj y)) (index.map (ι.map ff)) (index.mk (𝟙 _)),
     congr' 1,
     have := colimit.w (diagram ι F (ι.obj y)) (index.mk_hom (𝟙 _) ff),
     convert this,
-    rw index_map_mk,
-    simp [index_map_mk],
+    rw index.map_mk,
+    simp [index.map_mk],
   end },
   inv_fun := λ f,
   { app := λ x, colimit.desc (diagram ι F x) (cocone _ f),
@@ -265,21 +304,23 @@ def equiv [∀ x, has_colimits_of_shape (index ι x) D] (F : S ⥤ D) (G : L ⥤
     dsimp only [cocone],
     rw [category.assoc, ← x.naturality j.hom, ← category.assoc],
     congr' 1,
-    dsimp only [obj_aux, index_map],
-    change colimit.ι _ _ ≫ colimit.pre (diagram ι F k) (index_map _) = _,
+    dsimp only [obj_aux, index.map],
+    change colimit.ι _ _ ≫ colimit.pre (diagram ι F k) (index.map _) = _,
     rw colimit.ι_pre,
     congr,
     cases j,
     tidy,
   end,
-  right_inv := by tidy }.
+  right_inv := by tidy }
 
+/-- A variant of `Lan.equiv` with `whiskering_left` instead of functor composition. -/
 @[simps]
 def equiv' [∀ x, has_colimits_of_shape (index ι x) D] (F : S ⥤ D) (G : L ⥤ D) :
   (obj_aux ι F ⟶ G) ≃ (F ⟶ ((whiskering_left _ _ _).obj ι).obj G) := equiv _ _ _
 
 end Lan
 
+/-- The left Kan extension of a functor. -/
 @[simps]
 def Lan [∀ X, has_colimits_of_shape (Lan.index ι X) D] : (S ⥤ D) ⥤ L ⥤ D :=
 adjunction.left_adjoint_of_equiv (Lan.equiv' ι) (by tidy)
@@ -288,7 +329,7 @@ namespace Lan
 
 variable (D)
 
-@[simps]
+/-- The adjunction associated to `Lan`. -/
 def adjunction [∀ X, has_colimits_of_shape (Lan.index ι X) D] :
   Lan ι ⊣ (whiskering_left _ _ D).obj ι :=
 adjunction.adjunction_of_equiv_left _ _

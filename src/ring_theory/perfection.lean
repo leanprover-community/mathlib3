0e0866aac12ed91ed790cead89d98880f1d7d8fb
adomani <adomani@gmail.com>
2021-03-23 15:08:49 +0100

    ring_theory/perfection.lean add lots of underscores

--- src/ring_theory/perfection.lean
+++ src/ring_theory/perfection.lean
@@ -284,7 +284,8 @@ lemma map_map {π : P →+* R} (m : perfection_map p π) {σ : Q →+* S} (n : p
 ring_hom.ext_iff.1 (comp_map p m n φ) x
 
 -- Why is this slow?
-lemma map_eq_map (φ : R →+* S) : map p (of p R) (of p S) φ = perfection.map p φ :=
+lemma map_eq_map (φ : R →+* S) : @map p _ R _ _ _ _ _ _ _ _ _ _ _ _ _ _ (of p R) _ (of p S) φ =
+  perfection.map p φ :=
 hom_ext _ (of p S) $ λ f, by rw [map_map, perfection.coeff_map]
 
 end perfection_map

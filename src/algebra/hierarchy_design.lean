f8b817fdbd1f44fdb46dc6eb6072102abb79ebe2
Scott Morrison <scott.morrison@gmail.com>
2021-03-25 10:41:33 +1100

    work on algebraic hierarchy note

--- src/algebra/hierarchy_design.lean
+++ src/algebra/hierarchy_design.lean
@@ -3,13 +3,41 @@ import tactic.doc_commands
 /--
 # The algebraic hierarchy
 
-[[ Summarize the design principles of the algebraic hierarchy,
-without necessarily giving a tour. ]]
+In any theorem proving environment,
+there are difficult decisions surrounding the design of the "algebraic hierarchy".
 
-## Adding new typeclasses
+There is a danger of exponential explosion in the number of gadgets,
+especially once interactions between algebraic and order/topological/etc structures are considered.
 
-When adding new typeclasses `Z` to the algebraic hierarchy
-(either new leaves, or new intermediate classes)
+In mathlib, we try to avoid this by only introducing new algebraic typeclasses either
+1. when there is "real mathematics" to be done with them, or
+2. when there is a meaninful gain in simplicity by factoring out a common substructure.
+
+(As examples, at this point we don't have `loop`, or `unital_magma`,
+but we do have `lie_submodule` and `topological_field`!)
+
+Generally in mathlib we use the extension mechanism (so `comm_ring` extends `ring`)
+rather than mixins (e.g. with separate `ring` and `comm_mul` classes),
+in part because of the potential blow-up in term sizes described at
+https://www.ralfj.de/blog/2019/05/15/typeclasses-exponential-blowup.html
+However there is tension here, as it results in considerable duplication in the API,
+particularly in the interaction with order structures.
+
+This library note is not intended as a design document
+justifying and explaining the history of mathlib's algebraic hierarchy!
+Instead it is intended as a developer's guide, for contributors wanting to extend
+(either new leaves, or new intermediate classes) the algebraic hierarchy as it exists.
+
+(Ideally we would have both a tour guide to the existing hierarchy,
+and an account of the design choices.
+See https://arxiv.org/abs/1910.09336 for an overview of mathlib as a whole,
+with some attention to the algebraic hierarchy and
+https://leanprover-community.github.io/mathlib-overview.html
+for a summary of what is in mathlib today.)
+
+## Instances
+
+When adding a new typeclass `Z` to the algebraic hierarchy
 one should attempt to add the following constructions and results,
 when applicable:
 
@@ -23,15 +51,16 @@ when applicable:
   ```
   instance pi.Z [∀ i, Z $ f i] : Z (Π i : I, f i) := ...`
   ```
-* Definitions for transferring the proof fields of instances along injective and surjective functions that agree
-  on the data fields, like `function.injective.monoid` and `function.surjective.monoid`.
-  See ``algebra.group.inj_surj` for more examples
+* Definitions for transferring the proof fields of instances along
+  injective or surjective functions that agree on the data fields,
+  like `function.injective.monoid` and `function.surjective.monoid`.
+  See ``algebra.group.inj_surj` for more examples.
   ```
   def function.injective.Z [Z M₂] (f : M₁ → M₂) (hf : injective f)
     (one : f 1 = 1) (mul : ∀ x y, f (x * y) = f x * f y) : Z M₁ := ...
 
   def function.surjective.Z [Z M₁] (f : M₁ → M₂) (hf : surjective f)
-    (one : f 1 = 1) (mul : ∀ x y, f (x * y) = f x * f y) : Z M₂ :=
+    (one : f 1 = 1) (mul : ∀ x y, f (x * y) = f x * f y) : Z M₂ := ...
   ```
 * Instances tranferred elementwise to `finsupp`s, like `finsupp.semigroup`.
   See `data.finsupp.pointwise` for more examples.
@@ -44,33 +73,72 @@ when applicable:
   instance set.Z [Z α] : Z (set α) := ...
   ```
 * Definitions for transferring the entire structure across an equivalence, like `equiv.monoid`.
-  See `data.equiv.transfer_instance` for more examples.
+  See `data.equiv.transfer_instance` for more examples. See also the `transport` tactic.
   ```
-  def equiv.Z (e : α ≃ β) [Z β] : Z α := ...` and
-  /- when there is a new notion of `Z`-equiv -/
+  def equiv.Z (e : α ≃ β) [Z β] : Z α := ...
+  /- When there is a new notion of `Z`-equiv: -/
   def equiv.Z_equiv (e : α ≃ β) [Z β] : by { letI := equiv.Z e, exact α ≃Z β } := ...
   ```
-* When `Z` adds only new proof fields to an existing structure `Y`, instances transferring
-  `Z α` to `Z (sub_Y α)`, like `submonoid.to_comm_monoid`.
-  Typically this is done using the `function.injective.Z` definition mentioned above.
-  ```
-  instance sub_Y.to_Z [Z α] : Z (sub_Y α) :=
-  coe_injective.Z coe ...
-  ```
-* When `Z` adds new data fields too, a new `sub_Z` `structure` with a `carrier` field.
-  If `Z` extends `Y`, then `sub_Z` should usually extend `sub_Y`.
 
-[[ Talk about morphisms? ]]
-[[ Talk about equivalences? ]]
-[[ Talk about subobjects? ]]
+## Subobjects
 
-### Interactions with topology
+When a new typeclass `Z` adds new data fields,
+you should also create a new `sub_Z` `structure` with a `carrier` field.
+
+This can be a lot of work; for now try to closely follow the existing examples
+(e.g. `submonoid`, `subring`, `subalgebra`).
+We would very much like to provide some automation here, but a prerequisite will be making
+all the existing APIs more uniform.
+
+If `Z` extends `Y`, then `sub_Z` should usually extend `sub_Y`.
+
+When `Z` adds only new proof fields to an existing structure `Y`,
+you should provide instances transferring
+`Z α` to `Z (sub_Y α)`, like `submonoid.to_comm_monoid`.
+Typically this is done using the `function.injective.Z` definition mentioned above.
+```
+instance sub_Y.to_Z [Z α] : Z (sub_Y α) :=
+coe_injective.Z coe ...
+```
+
+## Morphisms and equivalences
+
+## Category theory
+
+For many algebraic structures, particularly ones used in representation theory, algebraic geometry,
+etc., we also define "bundled" versions, which carry `category` instances.
+
+These bundled versions are usually named in camel case,
+so for example we have `AddCommGroup` as a bundled `add_comm_group`,
+and `TopCommRing` (which bundles together `comm_ring`, `topological_space`, and `topological_ring`).
+
+These bundled versions have many appealing features:
+* a uniform notation for morphisms `X ⟶ Y`
+* a uniform notation (and definition) for isomorphisms `X ≅ Y`
+* a uniform API for subobjects, via the partial order `subobject X`
+* interoperability with unbundled structures, via coercions to `Type`
+  (so if `G : AddCommGroup`, you can treat `G` as a type,
+  and it automatically has an `add_comm_group` instance)
+  and lifting maps `AddCommGroup.of G`, when `G` is a type with an `add_comm_group` instance.
+
+If, for example you do the work of proving that a typeclass `Z` has a good notion of tensor product,
+you are strongly encouraged to provide the corresponding `monoidal_category` instance
+on a bundled version.
+This ensures that the API for tensor products is complete, and enables use of general machinery.
+Similarly if you prove universal properties, or adjunctions, you are encouraged to state these
+using categorical language!
+
+One disadvantage of the bundled approach is that we can only speak of morphisms between
+objects living in the same type-theoretic universe.
+In practice this is rarely a problem.
+
+## Interactions with topology
 
 [[ Mention here all the `topological_Z` infrastructure you might want. ]]
 
-### Interactions with order
+## Interactions with order
 
-[[ Complicated! ]]
+[[ ... ]]
 
 -/
 library_note "the algebraic hierarchy"

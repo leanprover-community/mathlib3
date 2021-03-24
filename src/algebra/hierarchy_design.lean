71f9ec632383755ddefeef13e9d1e3708db1b6f9
Scott Morrison <scott.morrison@gmail.com>
2021-03-25 10:47:47 +1100

    copyright header

--- src/algebra/hierarchy_design.lean
+++ src/algebra/hierarchy_design.lean
@@ -1,3 +1,8 @@
+/-
+Copyright (c) 2021 Scott Morrison. All rights reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
+Authors: Scott Morrison, Eric Weiser
+-/
 import tactic.doc_commands
 
 /--

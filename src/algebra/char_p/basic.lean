de5afd656b5f8393ad2fc784bf9256dd703caeab
adomani <adomani@gmail.com>
2021-03-25 09:23:35 +0100

    resolve conflicts?

--- src/algebra/char_p/basic.lean
+++ src/algebra/char_p/basic.lean
@@ -119,13 +119,14 @@ theorem add_pow_char_of_commute [semiring R] {p : ℕ} [fact p.prime]
 begin
   rw [commute.add_pow h, finset.sum_range_succ, nat.sub_self, pow_zero, nat.choose_self],
   rw [nat.cast_one, mul_one, mul_one], congr' 1,
-  convert finset.sum_eq_single 0 _ _, { simp },
-  swap, { intro h1, contrapose! h1, rw finset.mem_range, apply nat.prime.pos, assumption },
-  intros b h1 h2,
-  suffices : (p.choose b : R) = 0, { rw this, simp },
-  rw char_p.cast_eq_zero_iff R p,
-  refine nat.prime.dvd_choose_self (pos_iff_ne_zero.mpr h2) _ (by assumption),
-  rwa ← finset.mem_range
+  convert finset.sum_eq_single 0 _ _,
+  { simp only [mul_one, one_mul, nat.choose_zero_right, nat.sub_zero, nat.cast_one, pow_zero] },
+  { intros b h1 h2,
+    suffices : (p.choose b : R) = 0, { rw this, simp },
+    rw char_p.cast_eq_zero_iff R p,
+    refine nat.prime.dvd_choose_self (pos_iff_ne_zero.mpr h2) _ (by assumption),
+    rwa ← finset.mem_range },
+  { intro h1, contrapose! h1, rw finset.mem_range, apply nat.prime.pos, assumption },
 end
 
 theorem add_pow_char_pow_of_commute [semiring R] {p : ℕ} [fact p.prime]

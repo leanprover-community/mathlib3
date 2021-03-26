e0a501cc95c7a8d5db3d69e3de2aef37be089766
Eric Wieser <wieser.eric@gmail.com>
2021-03-26 15:39:44 +0000

    Tidy variables

--- src/algebra/smul_with_zero.lean
+++ src/algebra/smul_with_zero.lean
@@ -40,23 +40,19 @@ class smul_with_zero extends has_scalar R M :=
 (smul_zero : ∀ r : R, r • (0 : M) = 0)
 (zero_smul : ∀ m : M, (0 : R) • m = 0)
 
-variables (R) {M}
-@[simp] lemma zero_smul [smul_with_zero R M] (m : M) :
-  (0 : R) • m = 0 :=
-smul_with_zero.zero_smul m
+variables (R) {M} [smul_with_zero R M]
+
+@[simp] lemma zero_smul (m : M) : (0 : R) • m = 0 := smul_with_zero.zero_smul m
 
 variables {R} (M)
-/-- Note that this lemma has different typeclass assumptions to `smul_zero`-/
-@[simp] lemma smul_zero' [smul_with_zero R M] (r : R) :
-  r • (0 : M) = 0 :=
-smul_with_zero.smul_zero r
+/-- Note that this lemma has different typeclass assumptions to `smul_zero`. -/
+@[simp] lemma smul_zero' (r : R) : r • (0 : M) = 0 := smul_with_zero.smul_zero r
 
-variables {R M}
+variables {R M} [has_zero M'] [has_scalar R M']
+
 /-- Pullback a `smul_zero_class` structure along an injective zero-preserving homomorphism. -/
 protected def function.injective.smul_zero_class
-  [has_zero M'] [has_scalar R M'] [smul_with_zero R M]
-  (f : zero_hom M' M) (hf : function.injective f)
-  (smul : ∀ (a : R) b, f (a • b) = a • f b) :
+  (f : zero_hom M' M) (hf : function.injective f) (smul : ∀ (a : R) b, f (a • b) = a • f b) :
   smul_with_zero R M' :=
 { smul := (•),
   zero_smul := λ a, hf $ by simp [smul],
@@ -64,9 +60,7 @@ protected def function.injective.smul_zero_class
 
 /-- Pushforward a `smul_zero_class` structure along a surjective zero-preserving homomorphism. -/
 protected def function.surjective.smul_zero_class
-  [has_zero M'] [has_scalar R M'] [smul_with_zero R M]
-  (f : zero_hom M M') (hf : function.surjective f)
-  (smul : ∀ (a : R) b, f (a • b) = a • f b) :
+  (f : zero_hom M M') (hf : function.surjective f) (smul : ∀ (a : R) b, f (a • b) = a • f b) :
   smul_with_zero R M' :=
 { smul := (•),
   zero_smul := λ m, by { rcases hf m with ⟨x, rfl⟩, simp [←smul] },
@@ -97,21 +91,18 @@ instance monoid_with_zero.to_mul_action_with_zero : mul_action_with_zero R R :=
   zero_smul := zero_mul,
   ..monoid.to_mul_action R }
 
-variables {R M}
+variables {R M} [mul_action_with_zero R M]
+
 /-- Pullback a `mul_action_with_zero` structure along an injective zero-preserving homomorphism. -/
-protected def function.injective.mul_action_with_zero
-  [has_zero M'] [has_scalar R M'] [mul_action_with_zero R M]
-  (f : zero_hom M' M) (hf : function.injective f)
-  (smul : ∀ (a : R) b, f (a • b) = a • f b) :
+protected def function.injective.mul_action_with_zero [has_scalar R M'] [has_zero M']
+  (f : zero_hom M' M) (hf : function.injective f) (smul : ∀ (a : R) b, f (a • b) = a • f b) :
   mul_action_with_zero R M' :=
 { ..hf.mul_action f smul, ..hf.smul_zero_class f smul }
 
 /-- Pushforward a `mul_action_with_zero` structure along a surjective zero-preserving homomorphism.
 -/
-protected def function.surjective.mul_action_with_zero
-  [has_zero M'] [has_scalar R M'] [mul_action_with_zero R M]
-  (f : zero_hom M M') (hf : function.surjective f)
-  (smul : ∀ (a : R) b, f (a • b) = a • f b) :
+protected def function.surjective.mul_action_with_zero [has_zero M'] [has_scalar R M']
+  (f : zero_hom M M') (hf : function.surjective f) (smul : ∀ (a : R) b, f (a • b) = a • f b) :
   mul_action_with_zero R M' :=
 { ..hf.mul_action f smul, ..hf.smul_zero_class f smul }
 

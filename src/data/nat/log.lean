b6a3c35d71a82335aa5d4e2dcc5e7a90aebc545d
hallow-world <65730278+hallow-world@users.noreply.github.com>
2021-03-26 19:31:06 +0000

    simplify proofs and add monotonicity

--- src/data/nat/log.lean
+++ src/data/nat/log.lean
@@ -25,6 +25,34 @@ such that `b^k ≤ n`, so if `b^k = n`, it returns exactly `k`. -/
     log (n / b) + 1
   else 0
 
+lemma log_eq_zero {b n : ℕ} (hnb : n < b ∨ b ≤ 1) : log b n = 0 :=
+begin
+  rw [or_iff_not_and_not, not_lt, not_le] at hnb,
+  rw [log, ←ite_not, if_pos hnb],
+end
+
+lemma log_eq_zero_of_lt {b n : ℕ} (hn : n < b) : log b n = 0 :=
+log_eq_zero $ or.inl hn
+
+lemma log_eq_zero_of_lt' {b n : ℕ} (hb : b ≤ 1) : log b n = 0 :=
+log_eq_zero $ or.inr hb
+
+lemma log_zero_eq_zero {b : ℕ} : log b 0 = 0 :=
+by cases b; exact rfl; exact rfl
+
+lemma log_one_eq_zero {b : ℕ} : log b 1 = 0 :=
+begin
+  by_cases b ≤ 1,
+  exact log_eq_zero_of_lt' h,
+  exact log_eq_zero_of_lt (not_le.mp h),
+end
+
+lemma log_b_zero_eq_zero {n : ℕ} : log 0 n = 0 :=
+log_eq_zero_of_lt' zero_le_one
+
+lemma log_b_one_eq_zero {n : ℕ} : log 1 n = 0 :=
+log_eq_zero_of_lt' rfl.ge
+
 lemma pow_le_iff_le_log (x y : ℕ) {b} (hb : 1 < b) (hy : 1 ≤ y) :
   b^x ≤ y ↔ x ≤ log b y :=
 begin
@@ -65,51 +93,19 @@ end
 lemma pow_log_le_self (b x : ℕ) (hb : 1 < b) (hx : 1 ≤ x) : b ^ log b x ≤ x :=
 by rw [pow_le_iff_le_log _ _ hb hx]
 
-lemma log_eq_zero {b n : ℕ} (hnb : n < b ∨ b < 2) : log b n = 0 :=
-begin
-  rw log,
-  simp only [or_iff_not_and_not, not_lt, not_and, not_le] at hnb,
-  simp only [and_imp, ite_eq_right_iff, add_eq_zero_iff, one_ne_zero, and_false],
-  intros,
-  exact lt_le_antisymm (hnb ᾰ) ᾰ_1,
-end
-
-lemma log_eq_zero_of_lt {b n : ℕ} (hn : n < b) : log b n = 0 :=
-by exact log_eq_zero (or.inl hn)
-
-lemma log_eq_zero_of_lt' {b n : ℕ} (hb : b < 2) : log b n = 0 :=
-by exact log_eq_zero (or.inr hb)
-
-lemma log_zero_eq_zero {b : ℕ} : log b 0 = 0 :=
-begin
-  cases b,
-  exact log_eq_zero_of_lt' zero_lt_two,
-  exact rfl,
-end
-
-lemma log_one_eq_zero {b : ℕ} : log b 1 = 0 :=
-begin
-  by_cases b < 2,
-  exact log_eq_zero_of_lt' h,
-  exact log_eq_zero_of_lt (not_lt.mp h),
-end
-
-lemma log_b_zero_eq_zero {n : ℕ} : log 0 n = 0 :=
-by exact log_eq_zero_of_lt' zero_lt_two
-
-lemma log_b_one_eq_zero {n : ℕ} : log 1 n = 0 :=
-by exact log_eq_zero_of_lt' one_lt_two
-
 lemma log_le_log_of_le {b n m : ℕ} (h : n ≤ m) : log b n ≤ log b m :=
 begin
-  by_cases hb : b < 2, by simp only [log_eq_zero_of_lt', hb],
-  by_cases hn : n = 0, by simp only [hn, log_zero_eq_zero, zero_le],
+  by_cases hb : b ≤ 1, by rw log_eq_zero_of_lt' hb; exact zero_le',
+  by_cases hn : n = 0, by rw [hn, log_zero_eq_zero]; exact zero_le',
 
-  rw ←pow_le_iff_le_log _ _ (not_lt.mp hb) (lt_of_lt_of_le (zero_lt_iff.mpr hn) h),
-  exact (pow_log_le_self b n (not_lt.mp hb) (zero_lt_iff.mpr hn)).trans h,
+  rw ←pow_le_iff_le_log _ _ (not_le.mp hb) (lt_of_lt_of_le (zero_lt_iff.mpr hn) h),
+  exact (pow_log_le_self b n (not_le.mp hb) (zero_lt_iff.mpr hn)).trans h,
 end
 
 lemma log_le_log_succ {b n : ℕ} : log b n ≤ log b n.succ :=
-by exact log_le_log_of_le (le_succ n)
+log_le_log_of_le $ le_succ n
+
+lemma log_mono {b : ℕ} : monotone (λ n : ℕ, log b n) :=
+monotone_of_monotone_nat $ λ n, log_le_log_succ
 
 end nat

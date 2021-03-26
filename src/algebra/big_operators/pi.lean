f752dd5150bf4f1e7cd58ee0c3bda3bf75319ba6
Scott Morrison <scott.morrison@gmail.com>
2021-03-26 14:06:39 +1100

    Update src/algebra/big_operators/pi.lean

--- src/algebra/big_operators/pi.lean
+++ src/algebra/big_operators/pi.lean
@@ -36,7 +36,7 @@ lemma finset.prod_apply {α : Type*} {β : α → Type*} {γ} [∀a, comm_monoid
 (monoid_hom.apply β a).map_prod _ _
 
 /-- An 'unapplied' analogue of `finset.prod_apply`. -/
-@[to_additive "An 'unapplied analogue of `finset.sum_apply`."]
+@[to_additive "An 'unapplied' analogue of `finset.sum_apply`."]
 lemma finset.prod_fn {α : Type*} {β : α → Type*} {γ} [∀a, comm_monoid (β a)]
   (s : finset γ) (g : γ → Πa, β a) : (∏ c in s, g c) = (λ a, ∏ c in s, g c a) :=
 funext (λ a, finset.prod_apply _ _ _)

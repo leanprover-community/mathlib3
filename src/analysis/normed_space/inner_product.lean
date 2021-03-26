c39a22fa3f9ada9b3b228b333994c65bc1ba6bff
Heather Macbeth <25316162+hrmacbeth@users.noreply.github.com>
2021-03-25 21:06:26 -0400

    isometry of C with R^2

--- src/analysis/normed_space/inner_product.lean
+++ src/analysis/normed_space/inner_product.lean
@@ -1814,6 +1814,15 @@ begin
   { rw [← h.equiv_fun.symm_apply_apply y, h.equiv_fun_symm_apply] }
 end
 
+def complex.isometry_euclidean : ℂ ≃ₗᵢ[ℝ] (euclidean_space ℝ (fin 2)) :=
+complex.is_basis_one_I.isometry_euclidean_of_orthonormal
+begin
+  rw orthonormal_iff_ite,
+  intros i, fin_cases i;
+  intros j; fin_cases j;
+  simp [real_inner_eq_re_inner]
+end
+
 end pi_Lp
 
 

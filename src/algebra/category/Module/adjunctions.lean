372207bd43643a280246eeaa9dbd41d6e99bcc6e
Scott Morrison <scott.morrison@gmail.com>
2021-03-27 15:16:36 +1100

    feat(algebra/Module): free module functor is lax monoidal

--- src/algebra/category/Module/adjunctions.lean
+++ src/algebra/category/Module/adjunctions.lean
@@ -3,8 +3,10 @@ Copyright (c) 2021 Scott Morrison. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Scott Morrison, Johan Commelin
 -/
-import algebra.category.Module.basic
-import linear_algebra.finsupp
+import algebra.category.Module.monoidal
+import category_theory.monoidal.functorial
+import category_theory.monoidal.types
+import linear_algebra.direct_sum.finsupp
 
 /-!
 The functor of forming finitely supported functions on a type with values in a `[ring R]`
@@ -22,7 +24,10 @@ namespace Module
 
 open_locale classical
 
-variables (R : Type u) [ring R]
+variables (R : Type u)
+
+section
+variables [ring R]
 
 /--
 The free functor `Type u ⥤ Module R` sending a type `X` to the
@@ -44,5 +49,24 @@ adjunction.mk_of_hom_equiv
   hom_equiv_naturality_left_symm' := λ _ _ M f g,
   finsupp.lhom_ext' (λ x, linear_map.ext_ring
     (finsupp.sum_map_domain_index_add_monoid_hom (λ y, ((smul_add_hom R ↥M).flip) (g y))).symm) }
+
+end
+
+section lax_monoidal
+variables [comm_ring R]
+
+/-- The free R-module functor is lax monoidal. -/
+-- In fact, it's strong monoidal, but we don't yet have a typeclass for that.
+instance : lax_monoidal.{u} (free R).obj :=
+{ -- Send `R` to `punit →₀ R`
+  ε := finsupp.lsingle punit.star,
+  -- Send `(α →₀ R) ⊗ (β →₀ R)` to `α × β →₀ R`
+  μ := λ α β, (finsupp_tensor_finsupp' R α β).to_linear_map,
+  μ_natural' := by { intros, ext x x' ⟨y, y'⟩, simp, },
+  left_unitality' := by { intros, ext, simp, },
+  right_unitality' := by { intros, ext, simp, },
+  associativity' := by { intros, ext, simp, }, }
+
+end lax_monoidal
 
 end Module

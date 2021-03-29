f8a22847be63d164d969dc0fda100b67121a1917
Alain Verberkmoes <verberkm@xs4all.nl>
2021-03-29 23:39:33 +0200

    comments incorporated + cosmetic changes

--- archive/imo/imo2011_q5.lean
+++ archive/imo/imo2011_q5.lean
@@ -4,8 +4,7 @@ Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Alain Verberkmoes
 -/
 
-import tactic
-open int
+import data.int.basic
 
 /-!
 # IMO 2011 Q5
@@ -16,6 +15,8 @@ of positive integers.  Suppose that, for any two integers
 `f(m − n)`.  Prove that, for all integers `m` and `n` with
 `f(m) ≤ f(n)`, the number `f(n)` is divisible by `f(m)`.
 -/
+
+open int
 
 theorem imo2011_q5 (f : ℤ → ℤ) (hpos : ∀ n : ℤ, 0 < f n)
   (hdvd : ∀ m n : ℤ, f (m - n) ∣ f m - f n) :
@@ -29,30 +30,26 @@ begin
     { rw ←sub_sub_self m n,
       exact hdvd m (m - n) },
     have h_d_lt_fn : d < f n,
-    { refine lt_trans _ h_fm_lt_fn,
-      exact sub_lt_self _ (hpos (m - n)) },
+    { calc d < f m : sub_lt_self _ (hpos (m - n))
+        ... < f n : h_fm_lt_fn },
     have h_neg_d_lt_fn : -d < f n,
-    { rw neg_sub,
-      refine lt_trans (sub_lt_self _ (hpos m)) _,
-      refine lt_of_le_of_lt _ (sub_lt_self _ (hpos m)),
-      refine le_of_dvd (sub_pos.mpr h_fm_lt_fn) _,
-      rw [←neg_sub (f m), dvd_neg],
-      exact hdvd m n },
+    { calc -d = f (m - n) - f m : neg_sub _ _
+        ... < f (m - n) : sub_lt_self _ (hpos m)
+        ... ≤ f n - f m : le_of_dvd (sub_pos.mpr h_fm_lt_fn) _
+        ... < f n : sub_lt_self _ (hpos m),
+      -- ⊢ f (m - n) ∣ f n - f m
+      rw [←dvd_neg, neg_sub], exact hdvd m n },
     have h_d_eq_zero : d = 0,
-    { have hd : d > 0 ∨ d = 0 ∨ d < 0, from trichotomous d 0,
-      rcases hd with (hd | hd | hd),
-      { -- hd : d > 0
+    { obtain (hd | hd | hd) : d > 0 ∨ d = 0 ∨ d < 0 := trichotomous d 0,
+      { -- d > 0
         have h₁ : f n ≤ d, from le_of_dvd hd h_fn_dvd_d,
         have h₂ : ¬ f n ≤ d, from not_le.mpr h_d_lt_fn,
         contradiction },
-      { -- hd : d = 0
+      { -- d = 0
         exact hd },
-      { -- hd : d < 0
-        have h₁ : f n ≤ -d,
-        { refine le_of_dvd _ _,
-          { exact neg_pos.mpr hd },
-          { exact (dvd_neg _ _).mpr h_fn_dvd_d } },
-        have h₂ : ¬ f n ≤ -d := not_le.mpr h_neg_d_lt_fn,
+      { -- d < 0
+        have h₁ : f n ≤ -d, from le_of_dvd (neg_pos.mpr hd) ((dvd_neg _ _).mpr h_fn_dvd_d),
+        have h₂ : ¬ f n ≤ -d, from not_le.mpr h_neg_d_lt_fn,
         contradiction } },
     have h₁ : f m = f (m - n), from sub_eq_zero.mp h_d_eq_zero,
     have h₂ : f (m - n) ∣ f m - f n, from hdvd m n,

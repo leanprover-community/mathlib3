9167322b0330b0a8c8954783b602cbd453a895ce
Adrián Doña Mateo <drnaia100@gmail.com>
2021-03-31 18:02:21 +0100

    Make arguments explicity

--- src/group_theory/subgroup.lean
+++ src/group_theory/subgroup.lean
@@ -1200,28 +1200,26 @@ namespace subgroup
 
 open monoid_hom
 
-variables {H : Type*} [group H] {f : G →* H}
+variables {H : Type*} [group H]
 
 @[to_additive]
-lemma map_le_range {K : subgroup G} : map f K ≤ f.range :=
+lemma map_le_range (f : G →* H) (K : subgroup G) : map f K ≤ f.range :=
 (range_eq_map f).symm ▸ map_mono le_top
 
 @[to_additive]
-lemma ker_le_comap {K : subgroup H} : f.ker ≤ comap f K :=
+lemma ker_le_comap (f : G →* H) (K : subgroup H) : f.ker ≤ comap f K :=
 comap_mono bot_le
 
-variable (f)
-
 @[to_additive]
-lemma map_comap_le (K : subgroup H) : map f (comap f K) ≤ K :=
+lemma map_comap_le (f : G →* H) (K : subgroup H) : map f (comap f K) ≤ K :=
 (gc_map_comap f).l_u_le _
 
 @[to_additive]
-lemma le_comap_map (K : subgroup G) : K ≤ comap f (map f K) :=
+lemma le_comap_map (f : G →* H) (K : subgroup G) : K ≤ comap f (map f K) :=
 (gc_map_comap f).le_u_l _
 
 @[to_additive]
-lemma map_comap_eq (K : subgroup H) :
+lemma map_comap_eq (f : G →* H) (K : subgroup H) :
   map f (comap f K) = f.range ⊓ K :=
 set_like.ext' begin
   convert set.image_preimage_eq_inter_range,
@@ -1229,10 +1227,10 @@ set_like.ext' begin
 end
 
 @[to_additive]
-lemma comap_map_eq (K : subgroup G) :
+lemma comap_map_eq (f : G →* H) (K : subgroup G) :
   comap f (map f K) = K ⊔ f.ker :=
 begin
-  refine le_antisymm _ (sup_le (le_comap_map _ _) ker_le_comap),
+  refine le_antisymm _ (sup_le (le_comap_map _ _) (ker_le_comap _ _)),
   intros x hx, simp only [exists_prop, mem_map, mem_comap] at hx,
   rcases hx with ⟨y, hy, hy'⟩,
   have : y⁻¹ * x ∈ f.ker, { rw mem_ker, simp [hy'] },
@@ -1240,38 +1238,36 @@ begin
   simp,
 end
 
-variable {f}
-
 @[to_additive]
-lemma map_comap_eq_self {K : subgroup H} (h : K ≤ f.range) :
+lemma map_comap_eq_self {f : G →* H} {K : subgroup H} (h : K ≤ f.range) :
   map f (comap f K) = K :=
 by rwa [map_comap_eq, inf_eq_right]
 
 @[to_additive]
-lemma map_comap_eq_self_of_surjective (h : function.surjective f) (K : subgroup H) :
+lemma map_comap_eq_self_of_surjective {f : G →* H} (h : function.surjective f) (K : subgroup H) :
   map f (comap f K) = K :=
 map_comap_eq_self ((range_top_of_surjective _ h).symm ▸ le_top)
 
 @[to_additive]
-lemma comap_injective (h : function.surjective f) : function.injective (comap f) :=
+lemma comap_injective {f : G →* H} (h : function.surjective f) : function.injective (comap f) :=
 λ K L hKL, by { apply_fun map f at hKL, simpa [map_comap_eq_self_of_surjective h] using hKL }
 
 @[to_additive]
-lemma comap_map_eq_self {K : subgroup G} (h : f.ker ≤ K) :
+lemma comap_map_eq_self {f : G →* H} {K : subgroup G} (h : f.ker ≤ K) :
   comap f (map f K) = K :=
 by rwa [comap_map_eq, sup_eq_left]
 
 @[to_additive]
-lemma comap_map_eq_self_of_injective (h : function.injective f) (K : subgroup G) :
+lemma comap_map_eq_self_of_injective {f : G →* H} (h : function.injective f) (K : subgroup G) :
   comap f (map f K) = K :=
 comap_map_eq_self (((ker_eq_bot_iff _).mpr h).symm ▸ bot_le)
 
 @[to_additive]
-lemma map_injective (h : function.injective f) : function.injective (map f) :=
+lemma map_injective {f : G →* H} (h : function.injective f) : function.injective (map f) :=
 λ K L hKL, by { apply_fun comap f at hKL, simpa [comap_map_eq_self_of_injective h] using hKL }
 
 @[to_additive]
-lemma map_eq_comap_of_inverse {g : H →* G} (hl : function.left_inverse g f)
+lemma map_eq_comap_of_inverse {f : G →* H} {g : H →* G} (hl : function.left_inverse g f)
   (hr : function.right_inverse g f) (K : subgroup G) : map f K = comap g K :=
 set_like.ext' $ by rw [coe_map, coe_comap, set.image_eq_preimage_of_inverse hl hr]
 

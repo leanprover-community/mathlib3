91c0c7e95ffdd11dacc1da98978aa267e6d21d6e
Floris van Doorn <fpvdoorn@gmail.com>
2021-03-31 13:28:46 -0400

    remove simp

--- src/order/bounded_lattice.lean
+++ src/order/bounded_lattice.lean
@@ -565,11 +565,11 @@ by rw [← sup_eq_max, lattice_eq_DLO]
 theorem inf_eq_min [linear_order α] (x y : with_bot α) : x ⊓ y = min x y :=
 by rw [← inf_eq_min, lattice_eq_DLO]
 
-@[simp, norm_cast, nolint simp_nf] -- the with_top lemmas leak through when applying simp
+@[norm_cast] -- mark this as simp if the corresponding with_top lemmas don't apply
 lemma coe_min [linear_order α] (x y : α) : ((min x y : α) : with_bot α) = min x y :=
 by simp [min, ite_cast]
 
-@[simp, norm_cast, nolint simp_nf] -- the with_top lemmas leak through when applying simp
+@[norm_cast] -- mark this as simp if the corresponding with_top lemmas don't apply
 lemma coe_max [linear_order α] (x y : α) : ((max x y : α) : with_bot α) = max x y :=
 by simp [max, ite_cast]
 

9019f342cd6a2a1d726c8a92f57888e52ce9fafd
Manuel Candales <manuelcandales@gmail.com>
2021-04-01 14:01:36 -0400

    review suggestions + clean up

--- archive/imo/imo2008_q2.lean
+++ archive/imo/imo2008_q2.lean
@@ -25,13 +25,13 @@ using c, m and n. We factor LHS - 1 as a square, which finishes the proof.
 set of rational solutions to the equation, and that W is infinite.
 -/
 
-lemma subst_abc (x y z : ℝ) (h : x*y*z = 1) :
+lemma subst_abc {x y z : ℝ} (h : x*y*z = 1) :
   ∃ a b c : ℝ, a ≠ 0 ∧ b ≠ 0 ∧ c ≠ 0 ∧ x = a/b ∧ y = b/c ∧ z = c /a :=
 begin
-  use x, use 1, use 1/y,
-  have h₁ : x ≠ 0,              { intro p, rw p at h, simp at h, exact h },
+  use [x, 1, 1/y],
+  have h₁ : x ≠ 0,              exact left_ne_zero_of_mul (left_ne_zero_of_mul_eq_one h),
   have h₂ : (1 : ℝ) ≠ (0 : ℝ),  exact one_ne_zero,
-  have hy_ne_zero : y ≠ 0,      { intro p, rw p at h, simp at h, exact h },
+  have hy_ne_zero : y ≠ 0,      exact right_ne_zero_of_mul (left_ne_zero_of_mul_eq_one h),
   have h₃ : 1/y ≠ 0,            exact one_div_ne_zero hy_ne_zero,
   have h₄ : x = x / 1,          exact (div_one x).symm,
   have h₅ : y = 1 / (1 / y),    exact (one_div_one_div y).symm,
@@ -42,13 +42,10 @@ end
 theorem imo2008_q2a (x y z : ℝ) (h : x*y*z = 1) (hx : x ≠ 1) (hy : y ≠ 1) (hz : z ≠ 1) :
   x^2 / (x-1)^2 + y^2 / (y-1)^2 + z^2 / (z-1)^2 ≥ 1 :=
 begin
-  rcases (subst_abc _ _ _ h) with ⟨a, b, c, ha, hb, hc, hx₂, hy₂, hz₂⟩,
+  obtain ⟨a, b, c, ha, hb, hc, hx₂, hy₂, hz₂⟩ := subst_abc h,
 
-  let m := c-b,
-  let n := b-a,
-
-  have hm_abc : m = c - b,          exact rfl,
-  have hn_abc : n = b - a,          exact rfl,
+  set m := c-b with hm_abc,
+  set n := b-a with hn_abc,
   have ha_cmn : a = c - m - n,      linarith,
   have hb_cmn : b = c - m,          linarith,
   have hab_mn : (a-b)^2 = n^2,      { rw [ha_cmn, hb_cmn], ring },
@@ -93,72 +90,67 @@ begin
   { intros s hs_in_W,
     rw rational_solutions,
     simp at hs_in_W ⊢,
-    rcases hs_in_W with ⟨x, y, z, hs_xyz, ht⟩,
+    rcases hs_in_W with ⟨x, y, z, h₁, ht⟩,
     rcases ht with ⟨t, ht_gt_zero, hx_t, hy_t, hz_t⟩,
-    use x, use y, use z,
+    use [x, y, z],
 
-    have ht_ne_zero : t ≠ 0, exact ne_of_gt ht_gt_zero,
-    have ht1_ne_zero : t+1 ≠ 0, linarith[ht_gt_zero],
-
+    have ht_ne_zero  : t ≠ 0,           exact ne_of_gt ht_gt_zero,
+    have ht1_ne_zero : t+1 ≠ 0,         linarith[ht_gt_zero],
     have key_gt_zero : t^2 + t + 1 > 0, linarith[pow_pos ht_gt_zero 2, ht_gt_zero],
     have key_ne_zero : t^2 + t + 1 ≠ 0, exact ne_of_gt key_gt_zero,
 
-    have h₂ : x ≠ 1, { rw hx_t, field_simp, linarith[key_gt_zero] },
-    have h₃ : y ≠ 1, { rw hy_t, field_simp, linarith[key_gt_zero] },
-    have h₄ : z ≠ 1, { rw hz_t, linarith[key_gt_zero] },
-
-    have h₅ : x*y*z = 1, { rw [hx_t, hy_t, hz_t], field_simp, ring },
+    have h₂ : x ≠ 1,      { rw hx_t, field_simp, linarith[key_gt_zero] },
+    have h₃ : y ≠ 1,      { rw hy_t, field_simp, linarith[key_gt_zero] },
+    have h₄ : z ≠ 1,      { rw hz_t, linarith[key_gt_zero] },
+    have h₅ : x*y*z = 1,  { rw [hx_t, hy_t, hz_t], field_simp, ring },
 
     have h₆ : x^2/(x-1)^2 + y^2/(y-1)^2 + z^2/(z-1)^2 = 1,
-    { have hx1 : (x - 1)^2 = (t^2 + t + 1)^2/t^4,
-      { field_simp, rw hx_t, field_simp, ring },
-      have hy1 : (y - 1)^2 = (t^2 + t + 1)^2/(t+1)^4,
-      { field_simp, rw hy_t, field_simp, ring },
-      have hz1 : (z - 1)^2 = (t^2 + t + 1)^2,
-      { rw hz_t, ring },
+    { have hx1 : (x - 1)^2 = (t^2 + t + 1)^2/t^4,     { field_simp, rw hx_t, field_simp, ring },
+      have hy1 : (y - 1)^2 = (t^2 + t + 1)^2/(t+1)^4, { field_simp, rw hy_t, field_simp, ring },
+      have hz1 : (z - 1)^2 = (t^2 + t + 1)^2,         { rw hz_t, ring },
       calc  x^2/(x-1)^2 + y^2/(y-1)^2 + z^2/(z-1)^2
           = (x^2*t^4 + y^2*(t+1)^4 + z^2)/(t^2 + t + 1)^2 :
             by { rw [hx1, hy1, hz1], field_simp }
       ... = 1 :
             by { rw [hx_t, hy_t, hz_t], field_simp, ring } },
 
-    exact ⟨hs_xyz, h₂, h₃, h₄, h₅, h₆⟩,
-  },
+    exact ⟨h₁, h₂, h₃, h₄, h₅, h₆⟩ },
 
   have hW_inf : set.infinite W,
-  { let g : ℚ×ℚ×ℚ → ℚ := (λs, -s.2.2),
-    let Z := g '' W,
+  { set g : ℚ×ℚ×ℚ → ℚ := (λs, -s.2.2) with hg_eval,
+    let K := g '' W,
 
-    have hZ_not_bdd : ¬bdd_above Z,
+    have hK_not_bdd : ¬bdd_above K,
     { rw not_bdd_above_iff,
       intro q,
       let t : ℚ := max (q+1) 1,
       use t*(t+1),
-      let x : ℚ := (-1 + -t)/t^2,
-      let y : ℚ := t/(t+1)^2,
-      let z : ℚ := -t*(t+1),
-      have hz_def : z = -t*(t+1), exact rfl,
 
-      split,
-      { simp, use x, use y, use z, split,
-        { use x, use y, use z, split,
-          { split, refl, split, refl, refl },
+      have h₁ : t * (t + 1) ∈ K,
+      { let x : ℚ := (-1 + -t)/t^2,
+        let y : ℚ := t/(t+1)^2,
+        set z : ℚ := -t*(t+1) with hz_def,
+
+        simp, use [x, y, z], split,
+        { use [x, y, z], split,
+          exact ⟨refl x, refl y, refl z⟩,
           { use t, split,
             { simp, right, exact zero_lt_one },
-            { split, refl, split, refl, linarith[hz_def] } } },
-        { have hg_eval : g(x, y, z) = -z, exact rfl,
-          rw hg_eval, linarith[hz_def] } },
-      { calc q < q + 1    : by { linarith }
-           ... ≤ t        : by { exact le_max_left (q + 1) 1 }
-           ... ≤ t+t^2    : by { linarith [pow_two_nonneg t] }
-           ... = t*(t+1)  : by { ring } },
-    },
+            exact ⟨rfl, rfl, (by linarith[hz_def])⟩ } },
+        simp [hg_eval, hz_def] },
 
-    have hZ_inf : set.infinite Z,
-    { intro h, apply hZ_not_bdd, exact set.finite.bdd_above h },
+      have h₂ : q < t * (t + 1),
+      { calc q < q + 1    : by linarith
+           ... ≤ t        : le_max_left (q + 1) 1
+           ... ≤ t+t^2    : by linarith [pow_two_nonneg t]
+           ... = t*(t+1)  : by ring },
 
-    exact set.infinite_of_infinite_image g hZ_inf,
-  },
+      exact ⟨h₁, h₂⟩ },
+
+    have hK_inf : set.infinite K,
+    { intro h, apply hK_not_bdd, exact set.finite.bdd_above h },
+
+    exact set.infinite_of_infinite_image g hK_inf },
 
   exact set.infinite_mono hW_sub_S hW_inf,
 end

6a042a4b9c3d31aa83e6e0981490eb3558f35436
Jakob Scholbach <jakob.scholbach@uni-muenster.de>
2021-04-01 20:18:53 +0200

    simplifications

--- src/field_theory/separable_degree.lean
+++ src/field_theory/separable_degree.lean
@@ -6,7 +6,7 @@ Authors: Jakob Scholbach.
 import field_theory.separable
 import algebra.algebra.basic
 import data.polynomial.degree
-import exp_char
+import algebra.char_p.exp_char
 /-!
 # Separable degree
 
@@ -19,7 +19,7 @@ This file contains basics about the separable degree of a polynomial.
 - `separable_degree_dvd_degree'`: the separable degree divides the degree, in function of the
   exponential characteristic of the field
 - `separable_degree_dvd_degree` and `separable_degree_eq_degree` specialize the statement of
-`separable_degree_dvd_degree`
+  `separable_degree_dvd_degree`
 
 ## Tags
 separable degree, degree, polynomial
@@ -31,52 +31,46 @@ open_locale classical
 variables {F : Type} [comm_semiring F]
   (q : ℕ) [exp_char F q]
 
+/-- A separable contraction of a polynomial `f` is a separable polynomial `g` such that 
+`g(x^(q^m)) = f(x)` for some `m : ℕ`.-/
 class has_separable_contraction (f : polynomial F) : Prop :=
 (has_separable_contraction' : ∃ g : polynomial F,
   g.separable ∧ ∃ m : ℕ, polynomial.expand F (q^m) g = f)
 
+/-- A choice of a separable contraction. -/
 def separable_contraction (f : polynomial F) [hf : has_separable_contraction q f] :
-  polynomial F :=
-classical.some hf.has_separable_contraction'
+  polynomial F := classical.some hf.has_separable_contraction'
 
-/- The separable degree of a polynomial is the degree of a given separable contraction. -/
+/-- The separable degree of a polynomial is the degree of a given separable contraction. -/
 def separable_degree (f : polynomial F) [has_separable_contraction q f] : ℕ :=
 polynomial.nat_degree (separable_contraction q f)
 
-/- Every irreducible polynomial can be contracted to a separable polynomial.
+/-- Every irreducible polynomial can be contracted to a separable polynomial.
 https://stacks.math.columbia.edu/tag/09H0 -/
-instance irreducible_has_separable_contraction {F : Type} [field F] (q : ℕ) [hF : exp_char F q]
+lemma irreducible_has_separable_contraction {F : Type} [field F] (q : ℕ) [hF : exp_char F q]
   (f : polynomial F) (irred : irreducible f) (fn : f ≠ 0) : has_separable_contraction q f :=
 begin
-  cases hF.exp_char_def with q_one q_pos,
+  casesI hF,
   { use f,
-    split,
-    { haveI := q_one.2, apply irreducible.separable irred },
-    { use 0, simp, } },
-  { haveI qp : fact (nat.prime q) := q_pos.1,
-    haveI := q_pos.2,
-    cases polynomial.exists_separable_of_irreducible q irred fn with n hn,
-    cases hn with g hg,
+    exact ⟨irreducible.separable irred, ⟨0, by rw [one_pow, polynomial.expand_one]⟩⟩ },
+  { haveI qp : fact (nat.prime q) := fact_iff.mpr hF_hprime,
+    rcases polynomial.exists_separable_of_irreducible q irred fn with ⟨n, g, hg⟩,
     use g,
-    split,
-    { exact hg.1, },
-    { use n,
-      exact hg.2, } }
+    exact ⟨hg.1, ⟨n, hg.2⟩⟩, }
 end
 
-/- The separable degree divides the degree, in function of the exponential characteristic of F. -/
+/-- The separable degree divides the degree, in function of the exponential characteristic of F. -/
 lemma separable_degree_dvd_degree' (f : polynomial F) [hf : has_separable_contraction q f] :
   ∃ m : ℕ, (separable_degree q f) * (q ^ m) = f.nat_degree :=
 begin
-  let g := classical.some hf.has_separable_contraction',
-  have deg_g : g.nat_degree = separable_degree q f, { refl, },
   cases (classical.some_spec hf.has_separable_contraction').2 with m hm,
   use m,
-  rw [←deg_g, ←hm],
-  rw [polynomial.nat_degree_expand (q^m) g, mul_comm],
+  let g := classical.some hf.has_separable_contraction',
+  have deg_g : g.nat_degree = separable_degree q f, by refl,
+  rw [←deg_g, ←hm, polynomial.nat_degree_expand (q^m) g, mul_comm],
 end
 
-/- The separable degree divides the degree. -/
+/-- The separable degree divides the degree. -/
 lemma separable_degree_dvd_degree (f : polynomial F) [hf : has_separable_contraction q f] :
   (separable_degree q f) ∣ f.nat_degree :=
 begin
@@ -85,7 +79,7 @@ begin
   rw hm,
 end
 
-/- In exponential characteristic one, the separable degree equals the degree. -/
+/-- In exponential characteristic one, the separable degree equals the degree. -/
 lemma separable_degree_eq_degree (f : polynomial F) [exp_char F 1]
   [hf : has_separable_contraction 1 f] : (separable_degree 1 f) = f.nat_degree :=
 begin

79613f51356c6e844c8c2922ce8c9cfb308f2b44
Jakob Scholbach <jakob.scholbach@uni-muenster.de>
2021-04-01 20:22:01 +0200

    dot

--- src/field_theory/separable_degree.lean
+++ src/field_theory/separable_degree.lean
@@ -1,7 +1,7 @@
 /-
 Copyright (c) 2021 Jakob Scholbach. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
-Authors: Jakob Scholbach.
+Authors: Jakob Scholbach
 -/
 import field_theory.separable
 import algebra.algebra.basic

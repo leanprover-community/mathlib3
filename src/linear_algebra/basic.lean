8473218ed1b791cd2fb324a1bc41a5e5614f8293
Kexing <kexing.ying19@imperial.ac.uk>
2021-04-02 12:50:22 +0200

    add trans lemma

--- src/linear_algebra/basic.lean
+++ src/linear_algebra/basic.lean
@@ -1861,6 +1861,12 @@ lemma of_submodule'_symm_apply [semimodule R M] [semimodule R M₂]
   (f : M ≃ₗ[R] M₂) (U : submodule R M₂) (x : U) :
 ((f.of_submodule' U).symm x : M) = f.symm (x : M₂) := rfl
 
+@[simp]
+lemma of_submodule'_trans_of_submodule_apply [semimodule R M] [semimodule R M₂]
+  (f : M ≃ₗ[R] M₂) (U : submodule R M₂) (x : U.comap (f : M →ₗ[R] M₂)) :
+  ((f.of_submodule' U).trans (f.symm.of_submodule U) x : M) = x :=
+by simp only [of_submodule_apply, symm_apply_apply, trans_apply, of_submodule'_apply]
+
 variable (p)
 
 /-- The top submodule of `M` is linearly equivalent to `M`. -/

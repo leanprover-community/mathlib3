b476f7aa4e151bebcd7d97b5a21c99631bc919a1
Heather Macbeth <25316162+hrmacbeth@users.noreply.github.com>
2021-04-04 17:35:48 -0400

    documentation

--- src/geometry/manifold/instances/units_of_normed_algebra.lean
+++ src/geometry/manifold/instances/units_of_normed_algebra.lean
@@ -4,51 +4,62 @@ Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Nicolò Cavalleri, Heather Macbeth
 -/
 
-import geometry.manifold.algebra.lie_group
+import geometry.manifold.smooth_manifold_with_corners
 import analysis.normed_space.units
 
 /-!
-# Units of normed algebra
+# Units of a normed algebra
 
-In this file we aim to prove that the units of a complete normed algebra are a Lie group.
+This file is a stub, containing a construction of the charted space structure on the group of units
+of a complete normed ring `R`, and of the smooth manifold structure on the group of units of a
+complete normed `𝕜`-algebra `R`.
+
+This manifold is actually a Lie group, which eventually should be the main result of this file.
+
+An important special case of this construction is the general linear group.  For a normed space `V`
+over a field `𝕜`, the general linear group GL(`𝕜`, `V`) is a normed `𝕜`-algebra (see
+`continuous_linear_map.to_normed_algebra `), so this construction provides a Lie group structure on
+GL(`𝕜`, `V`).
 
 ## TODO
 
-We are still missing the main result, i.e. the instance
+The Lie group instance requires the following fields:
+```
+instance : lie_group 𝓘(𝕜, R) (units R) :=
+{ smooth_mul := sorry,
+  smooth_inv := sorry,
+  ..units.smooth_manifold_with_corners }
+```
 
-```
-instance units_of_normed_algebra.lie_group
-  {𝕜 : Type*} [nondiscrete_normed_field 𝕜]
-  {R : Type*} [normed_ring R] [normed_algebra 𝕜 R] [complete_space R] :
-  lie_group (model_with_corners_self 𝕜 R) (units R) :=
-{ smooth_mul := begin
-    sorry,
-  end,
-  smooth_inv := begin
-    sorry,
-  end,
-  ..units.smooth_manifold_with_corners,
-  /- Why does it not find the instance alone? -/ }
-```
+The ingredients needed for the construction are
+* smoothness of multiplication and inversion in the charts, i.e. as functions on the normed
+  `𝕜`-space `R`:  see `times_cont_diff_at_ring_inverse` for the inversion result, and
+  `times_cont_diff_mul` (needs to be generalized from field to algebra) for the multiplication
+  result
+* for an open embedding `f`, whose domain is equipped with the induced manifold structure
+  `f.singleton_smooth_manifold_with_corners`, characterization of smoothness of functions to/from
+  this manifold in terms of smoothness in the target space.  See the pair of lemmas
+  `times_cont_mdiff_coe_sphere` and `times_cont_mdiff.cod_restrict_sphere` for a model.
+None of this should be particularly difficult.
+
 -/
 
 noncomputable theory
 
 open_locale manifold
 
-instance {R : Type*} [normed_ring R] [complete_space R] : charted_space R (units R) :=
-units.open_embedding_coe.singleton_charted_space
-
 namespace units
 
 variables {R : Type*} [normed_ring R] [complete_space R]
 
+instance : charted_space R (units R) := open_embedding_coe.singleton_charted_space
+
 @[simp, mfld_simps] lemma chart_at_apply {a : units R} {b : units R} : chart_at R a b = b := rfl
 @[simp, mfld_simps] lemma chart_at_source {a : units R} : (chart_at R a).source = set.univ := rfl
 
-end units
+variables {𝕜 : Type*} [nondiscrete_normed_field 𝕜] [normed_algebra 𝕜 R]
 
-instance {𝕜 : Type*} [nondiscrete_normed_field 𝕜]
-  {R : Type*} [normed_ring R] [normed_algebra 𝕜 R] [complete_space R] :
-  smooth_manifold_with_corners 𝓘(𝕜, R) (units R) :=
-units.open_embedding_coe.singleton_smooth_manifold_with_corners 𝓘(𝕜, R)
+instance : smooth_manifold_with_corners 𝓘(𝕜, R) (units R) :=
+open_embedding_coe.singleton_smooth_manifold_with_corners 𝓘(𝕜, R)
+
+end units

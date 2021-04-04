c33fdaa587f523d5ea27b2cf3ecf2ea9f6dfdfa8
Mario Carneiro <di.gama@gmail.com>
2021-04-04 00:26:45 -0400

    golf

--- src/logic/girard.lean
+++ src/logic/girard.lean
@@ -14,43 +14,32 @@ so instead we axiomatize the behavior of the Pi type and application if the typi
 `(Type → Type) → Type` instead of `(Type → Type) → Type 1`.
 
 Furthermore, we don't actually want false axioms in mathlib, so rather than introducing the axioms
-using `axiom` or `constant` declarations, we declare a `girard` class, which bundles up the
-hypotheses in Girard's paradox, such that an instance of `girard.{u}` asserts that universe `u`
-satisfies the assumptions, i.e. `Type u : Type u`, give or take. Then the paradox amounts to a
-proof that the `girard` class is empty.
+using `axiom` or `constant` declarations, we take them as assumptions to the `girard` theorem.
 
 Based on Watkins' LF implementation of Hurkens' simplification of Girard's paradox:
 <http://www.cs.cmu.edu/~kw/research/hurkens95tlca.elf>.
 
 ## Main statements
 
-* `girard.paradox : girard → false`: there are no Girard universes.
+* `girard`: there are no Girard universes.
 -/
 
-universe u
-
-/-- A "Girard universe", which is a universe `u` such that `Pi : (Type u → Type u) → Type u`.
-Since we can't actually change the type of lean's `Π` operator, we define `pi`, `lam`, `app` and
-the `beta` rule instead. -/
-class girard :=
-(pi : (Type u → Type u) → Type u)
-(lam {A : Type u → Type u} : (∀ x, A x) → pi A)
-(app {A} : pi A → ∀ x, A x)
-(beta {A : Type u → Type u} (f : ∀ x, A x) (x) : app (lam f) x = f x)
-
-namespace girard
-
-/-- Girard's paradox: there are no Girard universes. -/
-theorem paradox (G : girard.{u}) : false :=
-let univ := pi (λ X, (set (set X) → X) → set (set X)) in
-let τ (T : set (set univ)) : univ :=
-  lam $ λ X f, {p | {x : univ | f (app x _ f) ∈ p} ∈ T} in
-let σ (S : univ) : set (set univ) := app S _ τ in
-have τσ : ∀ s S, s ∈ σ (τ S) ↔ {x | τ (σ x) ∈ s} ∈ S := by simp [σ, τ, beta],
-let ω := {p | ∀ x, p ∈ σ x → x ∈ p}, δ (S) := ∀ p, p ∈ S → τ S ∈ p in
-have δ ω, from λ p d, d _ (by simpa [τσ] using λ x h, d (τ (σ x)) (by simpa)),
-this {y | ¬ δ (σ y)}
-  (λ x e f, f _ e (λ p h, f (τ ∘ σ ⁻¹' p) (by rwa [τσ] at h)))
-  (λ p h, this (τ ∘ σ ⁻¹' p) (by simpa [τσ] using h))
-
-end girard
+/-- Girard's paradox: there are no universes `u` such that `Type u : Type u`.
+Since we can't actually change the type of lean's `Π` operator, we assume the existence of
+`pi`, `lam`, `app` and the `beta` rule equivalent to the `Π` and `app` constructors of type theory.
+-/
+theorem {u} girard
+  (pi : (Type u → Type u) → Type u)
+  (lam : ∀ {A : Type u → Type u}, (∀ x, A x) → pi A)
+  (app : ∀ {A}, pi A → ∀ x, A x)
+  (beta : ∀ {A : Type u → Type u} (f : ∀ x, A x) (x), app (lam f) x = f x) : false :=
+let F (X) := (set (set X) → X) → set (set X), U := pi F in
+let G (T : set (set U)) (X) : F X := λ f, {p | {x : U | f (app x X f) ∈ p} ∈ T} in
+let τ (T : set (set U)) : U := lam (G T) in
+let σ (S : U) : set (set U) := app S U τ in
+have στ : ∀ {s S}, s ∈ σ (τ S) ↔ {x | τ (σ x) ∈ s} ∈ S := λ s S,
+  iff_of_eq (congr_arg (λ f : F U, s ∈ f τ) (beta (G S) U) : _),
+let ω : set (set U) := {p | ∀ x, p ∈ σ x → x ∈ p} in
+let δ (S : set (set U)) := ∀ p, p ∈ S → τ S ∈ p in
+have δ ω := λ p d, d (τ ω) $ στ.2 $ λ x h, d (τ (σ x)) (στ.2 h),
+this {y | ¬ δ (σ y)} (λ x e f, f _ e (λ p h, f _ (στ.1 h))) (λ p h, this _ (στ.1 h))

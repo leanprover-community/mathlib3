39394812efe17c5900f61d6f325c91391da25dfa
Mario Carneiro <di.gama@gmail.com>
2021-04-04 01:26:15 -0400

    simpler imports

--- src/logic/girard.lean
+++ src/logic/girard.lean
@@ -3,7 +3,7 @@ Copyright (c) 2021 Mario Carneiro. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Mario Carneiro
 -/
-import data.set.basic
+import logic.basic
 
 /-!
 # Girard's paradox

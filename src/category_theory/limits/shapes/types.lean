88f4c1a54570ee9a4b3bccbe307017f4240e625c
Greg Price <gnprice@gmail.com>
2021-04-05 20:22:06 -0700

    tweak formatting

--- src/category_theory/limits/shapes/types.lean
+++ src/category_theory/limits/shapes/types.lean
@@ -281,12 +281,12 @@ explicit pullback object given by `pullback_limit_obj`.
 noncomputable def pullback_iso_pullback : pullback f g ≅ pullback_obj f g :=
 (cones.forget _).map_iso $ pullback_cone_iso_pullback f g
 
-@[simp] lemma pullback_fst'
-  : (pullback_iso_pullback f g).hom ≫ (λ p, (p : X × Y).fst) = pullback.fst :=
+@[simp] lemma pullback_fst' :
+  (pullback_iso_pullback f g).hom ≫ (λ p, (p : X × Y).fst) = pullback.fst :=
 (pullback_cone_iso_pullback f g).hom.w left
 
-@[simp] lemma pullback_snd'
-  : (pullback_iso_pullback f g).hom ≫ (λ p, (p : X × Y).snd) = pullback.snd :=
+@[simp] lemma pullback_snd' :
+  (pullback_iso_pullback f g).hom ≫ (λ p, (p : X × Y).snd) = pullback.snd :=
 (pullback_cone_iso_pullback f g).hom.w right
 
 end pullback

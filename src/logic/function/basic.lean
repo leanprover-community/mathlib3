85408199a2d4b36dfec86cf6160c28f8f9eebba1
adomani <adomani@gmail.com>
2021-04-06 15:19:54 +0200

    add surjective in logic/function/basic

--- src/logic/function/basic.lean
+++ src/logic/function/basic.lean
@@ -350,14 +350,10 @@ lemma bijective_iff_has_inverse : bijective f ↔ ∃ g, left_inverse g f ∧ ri
 lemma injective_surj_inv (h : surjective f) : injective (surj_inv h) :=
 (right_inverse_surj_inv h).injective
 
-lemma function.surjective.to_subsingleton {α β : Type*} (f : α → β) [nonempty α] [subsingleton β] :
+lemma function.surjective.to_subsingleton {α β : Type*} [na : nonempty α] [subsingleton β]
+  (f : α → β) :
   function.surjective f :=
-begin
-  refine function.surjective_iff_has_right_inverse.mpr ⟨function.inv_fun f, _⟩,
-
---  refine ⟨function.inv_fun f, _⟩,
---  ⟨function.inv_fun f, by finish⟩
-end
+λ y, ⟨nonempty.some na, (eq_iff_true_of_subsingleton _ _).mpr trivial⟩
 
 end surj_inv
 

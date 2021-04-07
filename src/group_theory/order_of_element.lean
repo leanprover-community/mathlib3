fe8c7c0ab23812efa4821e1b4233f3d242970965
Julian <julian.kuelshammer@math.uu.se>
2021-04-07 18:25:58 +0200

    minor

--- src/group_theory/order_of_element.lean
+++ src/group_theory/order_of_element.lean
@@ -171,9 +171,7 @@ end
 
 @[to_additive add_order_of_eq_zero]
 lemma order_of_eq_zero {a : α} (h : ¬ is_of_fin_order a) : order_of a = 0 :=
-begin
-rwa [order_of, minimal_period, dif_neg]
-end
+by rwa [order_of, minimal_period, dif_neg]
 
 lemma add_order_of_le_of_nsmul_eq_zero' {m : ℕ} (h : m < add_order_of x) : ¬ (0 < m ∧ m •ℕ x = 0) :=
 begin

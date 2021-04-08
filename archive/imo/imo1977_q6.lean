5cc5bf95362c21913e25debe757f213d1b85c745
Tian Chen <27919714+peakpoint@users.noreply.github.com>
2021-04-08 19:12:31 -0400

    format

--- archive/imo/imo1977_q6.lean
+++ archive/imo/imo1977_q6.lean
@@ -25,10 +25,12 @@ begin
     { intros n hk,
       apply nat.succ_le_of_lt,
       calc k ≤ f (f (n - 1)) : h_ind _ (h_ind (n - 1) (nat.le_sub_right_of_add_le hk))
-      ... < f n : nat.sub_add_cancel (le_trans (nat.succ_le_succ (nat.zero_le _)) hk) ▸ h _ } },
-  have hf := λ n, h' n n rfl.le,
+         ... < f n           : nat.sub_add_cancel
+        (le_trans (nat.succ_le_succ (nat.zero_le _)) hk) ▸ h _ } },
+  have hf : ∀ n, n ≤ f n := λ n, h' n n rfl.le,
   have hf_mono : strict_mono f := strict_mono.nat (λ _, lt_of_le_of_lt (hf _) (h _)),
-  intro, exact nat.eq_of_le_of_lt_succ (hf _) (hf_mono.lt_iff_lt.mp (h _))
+  intro,
+  exact nat.eq_of_le_of_lt_succ (hf _) (hf_mono.lt_iff_lt.mp (h _))
 end
 
 theorem imo1977_q6 (f : ℕ+ → ℕ+) (h : ∀ n, f (f n) < f (n + 1)) :

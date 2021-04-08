68a644498a5bb78662b24320c871df7e2c451ce2
Eric <ericrboidi@gmail.com>
2021-04-08 23:16:06 +0100

    fix typo in data/set_like.lean

--- src/data/set_like.lean
+++ src/data/set_like.lean
@@ -8,7 +8,7 @@ import data.set.basic
 /-!
 # Typeclass for a type `A` with an injective map to `set B`
 
-This typecalss is primary for use by subobjects like `submonoid` and `submodule`.
+This typeclass is primarily for use by subobjects like `submonoid` and `submodule`.
 
 A typical subobject should be declared as:
 ```

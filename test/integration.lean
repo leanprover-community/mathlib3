505ffa9c00ac61544ddb110b9f57ad01834f088b
Benjamin Davidson <68528197+benjamindavidson@users.noreply.github.com>
2021-04-09 02:29:47 +0000

    feat(analysis/special_functions/integrals): integral of `cos x ^ 2 - sin x ^ 2` (#7012)
    An example of a direct application of integration by parts.

--- test/integration.lean
+++ test/integration.lean
@@ -28,6 +28,7 @@ example : ∫ x in 0..π/4, cos x = sqrt 2 / 2 := by simp
 example : ∫ x in 0..π, 2 * sin x = 4 := by norm_num
 example : ∫ x in 0..π/2, cos x / 2 = 1 / 2 := by simp
 example : ∫ x : ℝ in 0..1, 1 / (1 + x ^ 2) = π/4 := by simp
+example : ∫ x in 0..π, cos x ^ 2 - sin x ^ 2 = 0 := by simp [integral_cos_sq_sub_sin_sq]
 
 /-- the exponential function -/
 example : ∫ x in 0..2, -exp x = 1 - exp 2 := by simp

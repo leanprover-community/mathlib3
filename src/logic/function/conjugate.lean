5e4051bff605a1249fd5ff249d4a1c15b5a6bdb5
adomani <adomani@gmail.com>
2021-04-09 08:41:38 +0200

    by popular demand, remove commute.apply!

--- src/logic/function/conjugate.lean
+++ src/logic/function/conjugate.lean
@@ -55,10 +55,6 @@ end semiconj
 Given `h : function.commute f g` and `a : α`, the term `h a` has Type `h a : f (g a) = g (f a)`. -/
 def commute (f g : α → α) : Prop := semiconj f g g
 
-lemma commute.apply {f g : α → α} (h : function.commute f g) (x : α) :
-  f (g x) = g (f x) :=
-h _
-
 lemma semiconj.commute {f g : α → α} (h : semiconj f g g) : commute f g := h
 
 namespace commute

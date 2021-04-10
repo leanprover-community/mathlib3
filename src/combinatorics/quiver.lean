f382b5167760f1661e7859d11192c3321597b90c
Scott Morrison <scott.morrison@gmail.com>
2021-04-10 16:36:28 +1000

    minimise imports

--- src/combinatorics/quiver.lean
+++ src/combinatorics/quiver.lean
@@ -3,7 +3,10 @@ Copyright (c) 2021 David Wärn. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: David Wärn
 -/
-import tactic
+import data.equiv.basic
+import order.well_founded
+import data.nat.basic
+
 /-!
 # Quivers
 

ec96e299c6d06596303700f006b61f93a42b1e22
Bhavik Mehta <bhavikmehta8@gmail.com>
2021-04-11 05:15:49 +0100

    review suggestions

--- src/data/complex/exponential_bounds.lean
+++ src/data/complex/exponential_bounds.lean
@@ -6,6 +6,7 @@ Authors: Mario Carneiro, Joseph Myers
 
 import data.complex.exponential
 import analysis.special_functions.exp_log
+import algebra.continued_fractions.computation.approximation_corollaries
 
 /-!
 # Bounds on specific values of the exponential
@@ -33,20 +34,20 @@ begin
   rw [_root_.abs_one, abs_of_pos]; norm_num1,
 end
 
-lemma exp_one_gt_271828182 : 2.7182818283 < exp 1 :=
+lemma exp_one_gt_d10 : 2.7182818283 < exp 1 :=
 lt_of_lt_of_le (by norm_num) (sub_le.1 (abs_sub_le_iff.1 exp_one_near_10).2)
 
-lemma exp_one_lt_271828183 : exp 1 < 2.7182818286 :=
+lemma exp_one_lt_d10 : exp 1 < 2.7182818286 :=
 lt_of_le_of_lt (sub_le_iff_le_add.1 (abs_sub_le_iff.1 exp_one_near_10).1) (by norm_num)
 
-lemma exp_neg_one_gt_0367879441 : 0.36787944116 < exp (-1) :=
+lemma exp_neg_one_gt_d10 : 0.36787944116 < exp (-1) :=
 begin
   rw [exp_neg, lt_inv _ (exp_pos _)],
   refine lt_of_le_of_lt (sub_le_iff_le_add.1 (abs_sub_le_iff.1 exp_one_near_10).1) _,
   all_goals {norm_num},
 end
 
-lemma exp_neg_one_lt_0367879442 : exp (-1) < 0.36787944120 :=
+lemma exp_neg_one_lt_d10 : exp (-1) < 0.36787944120 :=
 begin
   rw [exp_neg, inv_lt (exp_pos _)],
   refine lt_of_lt_of_le _ (sub_le.1 (abs_sub_le_iff.1 exp_one_near_10).2),
@@ -71,10 +72,10 @@ begin
   norm_num
 end
 
-lemma log_two_gt_6931471803 : 0.6931471803 < real.log 2 :=
+lemma log_two_gt_d10 : 0.6931471803 < log 2 :=
 lt_of_lt_of_le (by norm_num1) (sub_le.1 (abs_sub_le_iff.1 log_two_near_10).2)
 
-lemma log_two_lt_6931471808 : real.log 2 < 0.6931471808 :=
+lemma log_two_lt_d10 : log 2 < 0.6931471808 :=
 lt_of_le_of_lt (sub_le_iff_le_add.1 (abs_sub_le_iff.1 log_two_near_10).1) (by norm_num)
 
 end real

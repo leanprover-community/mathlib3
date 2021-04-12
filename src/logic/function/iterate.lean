e86e64d4127151b19a6d9f6f2d9e5249ff2f68a7
Yury G. Kudryashov <urkud@urkud.name>
2021-04-12 05:50:55 -0400

    Expand docs, use explicit args

--- src/logic/function/iterate.lean
+++ src/logic/function/iterate.lean
@@ -149,10 +149,10 @@ theorem right_inverse.iterate {g : α → α} (hg : right_inverse g f) (n : ℕ)
   right_inverse (g^[n]) (f^[n]) :=
 hg.iterate n
 
-lemma iterate_comm {m n : ℕ} : f^[n]^[m] = (f^[m]^[n] : α → α) :=
+lemma iterate_comm (f : α → α) (m n : ℕ) : f^[n]^[m] = (f^[m]^[n]) :=
 (iterate_mul _ _ _).symm.trans (eq.trans (by rw nat.mul_comm) (iterate_mul _ _ _))
 
 lemma iterate_commute (m n : ℕ) : commute (λ f : α → α, f^[m]) (λ f, f^[n]) :=
-λ f, iterate_comm
+λ f, iterate_comm f m n
 
 end function

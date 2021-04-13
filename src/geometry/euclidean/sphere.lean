d4268495c025a519444b7a68c83804cfa670588f
Manuel Candales <manuelcandales@gmail.com>
2021-04-13 19:53:03 -0400

    more detailed names

--- src/geometry/euclidean/sphere.lean
+++ src/geometry/euclidean/sphere.lean
@@ -26,7 +26,7 @@ This section develops some results on spheres in real inner product spaces,
 which are used to deduce corresponding results for Euclidean affine spaces.
 -/
 
-lemma mul_norm_eq_sub_norm_pow_two {x y z : V}
+lemma mul_norm_eq_abs_sub_sq_norm_of_smul_and_norm_eq_norm {x y z : V}
   (h₁ : ∃ k : ℝ, k ≠ 1 ∧ x + y = k • (x - y)) (h₂ : ∥z - y∥ = ∥z + y∥) :
   ∥x - y∥ * ∥x + y∥ = abs (∥z + y∥ ^ 2 - ∥z - x∥ ^ 2) :=
 begin
@@ -78,7 +78,7 @@ include V
 
 /-- If `P` is a point on the line `AB` and `Q` is equidistant from `A` and `B`, then
 `AP * BP = abs (BQ ^ 2 - PQ ^ 2)`. -/
-lemma mul_dist_eq_sub_dist_pow_two {a b p q : P}
+lemma mul_dist_eq_abs_sub_sq_dist_of_smul_and_dist_eq_dist {a b p q : P}
   (hp : ∃ k : ℝ, k ≠ 1 ∧ b -ᵥ p = k • (a -ᵥ p)) (hq : dist a q = dist b q) :
   dist a p * dist b p = abs (dist b q ^ 2 - dist p q ^ 2) :=
 begin
@@ -90,12 +90,13 @@ begin
   rw [← h1, ← h2, h, h],
   rw [← h1, h] at hp,
   rw [dist_eq_norm_vsub V a q, dist_eq_norm_vsub V b q, ← h3, h] at hq,
-  exact mul_norm_eq_sub_norm_pow_two hp hq,
+  exact mul_norm_eq_abs_sub_sq_norm_of_smul_and_norm_eq_norm hp hq,
 end
 
 /-- If `A`, `B`, `C`, `D` are cospherical and `P` is on both lines `AB` and `CD`, then
 `AP * BP = CP * DP`. -/
-lemma mul_dist_eq_mul_dist_of_cospherical {a b c d p : P} (h : cospherical ({a, b, c, d} : set P))
+lemma mul_dist_eq_mul_dist_of_cospherical_and_smul {a b c d p : P}
+  (h : cospherical ({a, b, c, d} : set P))
   (hapb : ∃ k₁ : ℝ, k₁ ≠ 1 ∧ b -ᵥ p = k₁ • (a -ᵥ p))
   (hcpd : ∃ k₂ : ℝ, k₂ ≠ 1 ∧ d -ᵥ p = k₂ • (c -ᵥ p)) :
   dist a p * dist b p = dist c p * dist d p :=
@@ -103,31 +104,33 @@ begin
   obtain ⟨q, r, h'⟩ := (cospherical_def {a, b, c, d}).mp h,
   obtain ⟨ha, hb, hc, hd⟩ := ⟨h' a _, h' b _, h' c _, h' d _⟩,
   { rw ← hd at hc,
-    rw ← hb at ha hd,
-    rw [mul_dist_eq_sub_dist_pow_two hapb ha, mul_dist_eq_sub_dist_pow_two hcpd hc, hd] },
+    rw ← hb at ha,
+    rw [mul_dist_eq_abs_sub_sq_dist_of_smul_and_dist_eq_dist hapb ha, hb,
+        mul_dist_eq_abs_sub_sq_dist_of_smul_and_dist_eq_dist hcpd hc, hd] },
   all_goals { simp },
 end
 
 /-- Intersecting Chords Theorem. -/
-theorem mul_dist_eq_mul_dist_of_cospherical_and_angles_eq_pi {a b c d p : P}
+theorem mul_dist_eq_mul_dist_of_cospherical_and_angle_eq_pi {a b c d p : P}
   (h : cospherical ({a, b, c, d} : set P))
   (hapb : ∠ a p b = π ) (hcpd : ∠ c p d = π ) :
   dist a p * dist b p = dist c p * dist d p :=
 begin
   obtain ⟨-, k₁, _, hab⟩ := angle_eq_pi_iff.mp hapb,
   obtain ⟨-, k₂, _, hcd⟩ := angle_eq_pi_iff.mp hcpd,
-  exact mul_dist_eq_mul_dist_of_cospherical h ⟨k₁, (by linarith), hab⟩ ⟨k₂, (by linarith), hcd⟩,
+  exact mul_dist_eq_mul_dist_of_cospherical_and_smul
+          h ⟨k₁, (by linarith), hab⟩ ⟨k₂, (by linarith), hcd⟩,
 end
 
 /-- Intersecting Secants Theorem. -/
-theorem mul_dist_eq_mul_dist_of_cospherical_and_angles_eq_zero {a b c d p : P}
+theorem mul_dist_eq_mul_dist_of_cospherical_and_angle_eq_zero {a b c d p : P}
   (h : cospherical ({a, b, c, d} : set P))
   (hab : a ≠ b) (hcd : c ≠ d) (hapb : ∠ a p b = 0 ) (hcpd : ∠ c p d = 0 ) :
   dist a p * dist b p = dist c p * dist d p :=
 begin
   obtain ⟨-, k₁, -, hab₁⟩ := angle_eq_zero_iff.mp hapb,
   obtain ⟨-, k₂, -, hcd₁⟩ := angle_eq_zero_iff.mp hcpd,
-  refine mul_dist_eq_mul_dist_of_cospherical h ⟨k₁, _, hab₁⟩ ⟨k₂, _, hcd₁⟩;
+  refine mul_dist_eq_mul_dist_of_cospherical_and_smul h ⟨k₁, _, hab₁⟩ ⟨k₂, _, hcd₁⟩;
   by_contra hnot;
   simp only [not_not, *, one_smul] at *,
   exacts [hab (vsub_left_cancel hab₁).symm, hcd (vsub_left_cancel hcd₁).symm],

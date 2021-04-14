b4656414a3c437a5e7222e080e818706137ca9ec
Scott Morrison <scott.morrison@gmail.com>
2021-04-14 16:17:09 +1000

    golf

--- src/algebra/category/Module/epi_mono.lean
+++ src/algebra/category/Module/epi_mono.lean
@@ -24,11 +24,8 @@ variables {R : Type u} [ring R]
 
 lemma mono_iff_injective {X Y : Module.{u} R} (f : X ⟶ Y) : mono f ↔ function.injective f :=
 begin
-  suffices : mono f ↔ mono ((forget (Module.{u} R)).map f),
-  { rw [this, category_theory.mono_iff_injective], refl },
-  split,
-  { apply right_adjoint_preserves_mono (adj R) },
-  { apply faithful_reflects_mono }
+  rw ←category_theory.mono_iff_injective,
+  exact ⟨right_adjoint_preserves_mono (adj R), faithful_reflects_mono (forget (Module.{u} R))⟩,
 end
 
 end Module

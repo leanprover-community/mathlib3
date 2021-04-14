61ef3d3f703a7b03fcb635c7f7067f2331cbf0d9
Scott Morrison <scott.morrison@gmail.com>
2021-04-14 21:46:35 +1000

    stuff

--- src/algebra/homology2/homology.lean
+++ src/algebra/homology2/homology.lean
@@ -4,6 +4,7 @@ Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Scott Morrison
 -/
 import algebra.homology2.image_to_kernel
+import category_theory.subobject.lattice
 import category_theory.graded_object
 
 universes v u

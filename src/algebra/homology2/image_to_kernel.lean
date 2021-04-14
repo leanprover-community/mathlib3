88e83c8dc0a72ed37478dc238511f10c856ac67c
Scott Morrison <scott.morrison@gmail.com>
2021-04-14 21:56:52 +1000

    yay

--- src/algebra/homology2/image_to_kernel.lean
+++ src/algebra/homology2/image_to_kernel.lean
@@ -4,7 +4,6 @@ Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Scott Morrison
 -/
 import algebra.homology2.homological_complex
-import category_theory.subobject.specific_subobjects
 
 universes v u
 

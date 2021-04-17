da6dc58791829d1c60641561c08486ea10e1be0d
Bhavik Mehta <bhavikmehta8@gmail.com>
2021-04-17 21:08:46 +0100

    Floris' suggestions'

--- src/data/complex/exponential_bounds.lean
+++ src/data/complex/exponential_bounds.lean
@@ -34,20 +34,20 @@ begin
   rw [_root_.abs_one, abs_of_pos]; norm_num1,
 end
 
-lemma exp_one_gt_d10 : 2.7182818283 < exp 1 :=
+lemma exp_one_gt_d9 : 2.7182818283 < exp 1 :=
 lt_of_lt_of_le (by norm_num) (sub_le.1 (abs_sub_le_iff.1 exp_one_near_10).2)
 
-lemma exp_one_lt_d10 : exp 1 < 2.7182818286 :=
+lemma exp_one_lt_d9 : exp 1 < 2.7182818286 :=
 lt_of_le_of_lt (sub_le_iff_le_add.1 (abs_sub_le_iff.1 exp_one_near_10).1) (by norm_num)
 
-lemma exp_neg_one_gt_d10 : 0.36787944116 < exp (-1) :=
+lemma exp_neg_one_gt_d9 : 0.36787944116 < exp (-1) :=
 begin
   rw [exp_neg, lt_inv _ (exp_pos _)],
   refine lt_of_le_of_lt (sub_le_iff_le_add.1 (abs_sub_le_iff.1 exp_one_near_10).1) _,
   all_goals {norm_num},
 end
 
-lemma exp_neg_one_lt_d10 : exp (-1) < 0.36787944120 :=
+lemma exp_neg_one_lt_d9 : exp (-1) < 0.36787944120 :=
 begin
   rw [exp_neg, inv_lt (exp_pos _)],
   refine lt_of_lt_of_le _ (sub_le.1 (abs_sub_le_iff.1 exp_one_near_10).2),
@@ -66,16 +66,16 @@ begin
   norm_num1 at z,
   rw [one_div (2:ℝ), log_inv, ←sub_eq_add_neg, _root_.abs_sub] at z,
   apply le_trans (_root_.abs_sub_le _ _ _) (add_le_add z _),
-  iterate 33 { rw sum_range_succ },
+  simp_rw [sum_range_succ],
   norm_num,
   rw abs_of_pos;
   norm_num
 end
 
-lemma log_two_gt_d10 : 0.6931471803 < log 2 :=
+lemma log_two_gt_d9 : 0.6931471803 < log 2 :=
 lt_of_lt_of_le (by norm_num1) (sub_le.1 (abs_sub_le_iff.1 log_two_near_10).2)
 
-lemma log_two_lt_d10 : log 2 < 0.6931471808 :=
+lemma log_two_lt_d9 : log 2 < 0.6931471808 :=
 lt_of_le_of_lt (sub_le_iff_le_add.1 (abs_sub_le_iff.1 log_two_near_10).1) (by norm_num)
 
 end real

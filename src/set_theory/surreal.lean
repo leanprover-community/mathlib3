3953378b97735788c0df773447b6b8ec85560914
Apurva Nakade <apurvnakade@gmail.com>
2021-04-18 14:42:48 +0000

    feat(set_theory/surreal): add add_monoid instance (#7228)
    This PR is a part of a project for putting ordered abelian group structure on surreal numbers.
    
    [Zulip](https://leanprover.zulipchat.com/#narrow/stream/116395-maths/topic/Surreal.20numbers/near/234694758)
    
    We construct add_monoid instance for surreal numbers.
    
    The "term_type" proofs suggested on the above Zulip thread are not working nicely as Lean is unable to infer the type of the setoid.

--- src/set_theory/surreal.lean
+++ src/set_theory/surreal.lean
@@ -348,14 +348,23 @@ begin
   exact add_assoc_equiv,
 end
 
-instance : add_semigroup surreal :=
-{ add_assoc := add_assoc,
-  ..(by apply_instance : has_add surreal) }
+theorem zero_add : ∀ (x : surreal), 0 + x = x :=
+by { rintro ⟨x, ox⟩, exact quotient.sound (pgame.zero_add_equiv _) }
+
+theorem add_zero : ∀ (x : surreal), x + 0 = x :=
+by { rintro ⟨x, ox⟩, exact quotient.sound (pgame.add_zero_equiv _) }
+
+instance : add_monoid surreal :=
+{ add       := surreal.add,
+  add_assoc := surreal.add_assoc,
+  zero      := 0,
+  zero_add  := surreal.zero_add,
+  add_zero  := surreal.add_zero }
 
 -- We conclude with some ideas for further work on surreals; these would make fun projects.
 
--- TODO construct the remaining instances:
---   add_monoid, add_group, add_comm_semigroup, add_comm_group, ordered_add_comm_group,
+-- TODO replace the `add_monoid` instance above with a stronger instance:
+--   add_group, add_comm_semigroup, add_comm_group, ordered_add_comm_group,
 -- as per the instances for `game`
 
 -- TODO define the inclusion of groups `surreal → game`

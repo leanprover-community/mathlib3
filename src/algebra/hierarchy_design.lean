a640cbc0e71d0eed957be62cc6cfa99a86cb117f
Scott Morrison <scott.morrison@gmail.com>
2021-04-19 10:00:50 +1000

    Apply suggestions from code review

--- src/algebra/hierarchy_design.lean
+++ src/algebra/hierarchy_design.lean
@@ -167,7 +167,7 @@ With so many moving parts, how do you actually go about changing the algebraic h
 
 We're still evolving how to handle this, but the current suggestion is:
 
-* If you adding a new "leaf" class, the requirements are lower,
+* If you're adding a new "leaf" class, the requirements are lower,
   and an initial PR can just add whatever is immediately needed.
 * A new "intermediate" class, especially low down in the hierarchy,
   needs to be careful about leaving gaps.
@@ -176,7 +176,7 @@ In a perfect world, there would be a group of simultaneous PRs that basically co
 (Or at least an expectation that PRs may not be merged immediately while waiting on other
 PRs that fill out the API.)
 
-However perfect is the enemy of good, and it would also be completely reasonable
+However "perfect is the enemy of good", and it would also be completely reasonable
 to add a TODO list in the main module doc-string for the new class,
 briefly listing the parts of the API which still need to be provided.
 Hopefully this document makes it easy to assemble this list.

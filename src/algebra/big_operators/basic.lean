beffd22df8850ae09a936dc84f71239386b6eda1
Eric <ericrboidi@gmail.com>
2021-04-21 00:31:40 +0100

    fix lint

--- src/algebra/big_operators/basic.lean
+++ src/algebra/big_operators/basic.lean
@@ -571,12 +571,12 @@ by { rw prod_ite, simp [filter_false_of_mem h, filter_true_of_mem h] }
 by { simp_rw ←(ite_not (p _)), apply prod_ite_of_false, simpa }
 
 @[to_additive] lemma prod_apply_ite_of_false {p : α → Prop} {hp : decidable_pred p} (f g : α → γ)
-  (k : γ → β) (h : ∀ x ∈ s, ¬p x) : (∏ x in s, k (if p x then f x else g x)) = (∏ x in s, k (g x)) :=
-by { simp_rw apply_ite k, apply prod_ite_of_false, exact h }
+  (k : γ → β) (h : ∀ x ∈ s, ¬p x) : (∏ x in s, k (if p x then f x else g x)) = (∏ x in s, k (g x))
+  := by { simp_rw apply_ite k, apply prod_ite_of_false, exact h }
 
 @[to_additive] lemma prod_apply_ite_of_true {p : α → Prop} {hp : decidable_pred p} (f g : α → γ)
-  (k : γ → β) (h : ∀ x ∈ s, p x) : (∏ x in s, k (if p x then f x else g x)) = (∏ x in s, k (f x)) :=
-by { simp_rw apply_ite k, apply prod_ite_of_true, exact h }
+  (k : γ → β) (h : ∀ x ∈ s, p x) : (∏ x in s, k (if p x then f x else g x)) = (∏ x in s, k (f x))
+  := by { simp_rw apply_ite k, apply prod_ite_of_true, exact h }
 
 @[to_additive]
 lemma prod_extend_by_one [decidable_eq α] (s : finset α) (f : α → β) :

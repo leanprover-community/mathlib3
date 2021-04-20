4ba169a95c7159ec1163e7079efb6fc038bc286e
Yakov Pechersky <ffxen158@gmail.com>
2021-04-20 15:19:24 -0400

    feat(data/list/zip): map_zip_with

--- src/data/list/zip.lean
+++ src/data/list/zip.lean
@@ -116,6 +116,16 @@ theorem zip_map' (f : α → β) (g : α → γ) : ∀ (l : list α),
 | []     := rfl
 | (a::l) := by simp only [map, zip_cons_cons, zip_map' l]; split; refl
 
+lemma map_zip_with {δ : Type*} (f : α → β) (g : γ → δ → α) (l : list γ) (l' : list δ) :
+  map f (zip_with g l l') = zip_with (λ x y, f (g x y)) l l' :=
+begin
+  induction l with hd tl hl generalizing l',
+  { simp },
+  { cases l',
+    { simp },
+    { simp [hl] } }
+end
+
 theorem mem_zip {a b} : ∀ {l₁ : list α} {l₂ : list β},
    (a, b) ∈ zip l₁ l₂ → a ∈ l₁ ∧ b ∈ l₂
 | (_::l₁) (_::l₂) (or.inl rfl) := ⟨or.inl rfl, or.inl rfl⟩

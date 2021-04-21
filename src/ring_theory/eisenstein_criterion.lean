69f4bddca96d227faffbd5fa3291b2f305211583
Thomas Browning <tb65536@uw.edu>
2021-04-21 12:32:26 -0700

    clean up imports

--- src/ring_theory/eisenstein_criterion.lean
+++ src/ring_theory/eisenstein_criterion.lean
@@ -3,9 +3,6 @@ Copyright (c) 2020 Chris Hughes. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Chris Hughes
 -/
-import ring_theory.ideal.operations
-import data.polynomial.ring_division
-import tactic.apply_fun
 import ring_theory.prime
 import ring_theory.polynomial.content
 /-!

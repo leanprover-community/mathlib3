80c94aa882271a7cea857950507ce279bc3261e2
Riccardo Brasca <riccardo.brasca@gmail.com>
2021-04-22 15:16:17 +0200

    Update src/algebra/monoid_algebra.lean
    Co-authored-by: Eric Wieser <wieser.eric@gmail.com>

--- src/algebra/monoid_algebra.lean
+++ src/algebra/monoid_algebra.lean
@@ -605,8 +605,8 @@ section span
 
 variables [semiring k] [mul_one_class G]
 
-lemma mem.span_support (f : (monoid_algebra k G)) :
-  f ∈ submodule.span k ((monoid_algebra.of k G)'' (f.support : set G)) :=
+lemma mem_span_support (f : monoid_algebra k G) :
+  f ∈ submodule.span k (monoid_algebra.of k G '' (f.support : set G)) :=
 by rw [monoid_algebra.of, monoid_hom.coe_mk, ← finsupp.supported_eq_span_single,
   finsupp.mem_supported]
 

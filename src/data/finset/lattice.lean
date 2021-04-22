8b7014b981c9f7879f96ec2d65cc1cfc028e442a
Alistair Tucker <agjftucker@gmail.com>
2021-04-22 10:34:43 +0000

    fix(data/finset/lattice): sup'/inf' docstring (#7281)
    Made proper reference to `f` in the doc string.

--- src/data/finset/lattice.lean
+++ src/data/finset/lattice.lean
@@ -258,9 +258,9 @@ lemma sup_of_mem {s : finset β} (f : β → α) {b : β} (h : b ∈ s) :
   ∃ (a : α), s.sup (coe ∘ f : β → with_bot α) = ↑a :=
 Exists.imp (λ a, Exists.fst) (@le_sup (with_bot α) _ _ _ _ _ h (f b) rfl)
 
-/-- Given nonempty finset `s` in (possibly unbounded) join-semilattice `α` then `s.sup' H` is its
-supremum in `α` where `H` is a proof of nonemptiness. If `α` has a bottom element you may instead
-use `s.sup` which does not require `s` nonempty. -/
+/-- Given nonempty finset `s` then `s.sup' H f` is the supremum of its image under `f` in (possibly
+unbounded) join-semilattice `α`, where `H` is a proof of nonemptiness. If `α` has a bottom element
+you may instead use `finset.sup` which does not require `s` nonempty. -/
 def sup' (s : finset β) (H : s.nonempty) (f : β → α) : α :=
 option.get $ let ⟨b, hb⟩ := H in option.is_some_iff_exists.2 (sup_of_mem f hb)
 
@@ -363,9 +363,9 @@ lemma inf_of_mem {s : finset β} (f : β → α) {b : β} (h : b ∈ s) :
   ∃ (a : α), s.inf (coe ∘ f : β → with_top α) = ↑a :=
 @sup_of_mem (order_dual α) _ _ _ f _ h
 
-/-- Given nonempty finset `s` in (possibly unbounded) meet-semilattice `α` then `s.inf' H` is its
-infimum in `α` where `H` is a proof of nonemptiness. If `α` has a top element you may instead use
-`s.inf` which does not require `s` nonempty. -/
+/-- Given nonempty finset `s` then `s.inf' H f` is the infimum of its image under `f` in (possibly
+unbounded) meet-semilattice `α`, where `H` is a proof of nonemptiness. If `α` has a top element you
+may instead use `finset.inf` which does not require `s` nonempty. -/
 def inf' (s : finset β) (H : s.nonempty) (f : β → α) : α :=
 @sup' (order_dual α) _ _ s H f
 

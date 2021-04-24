63d24f7cde5c4b33c8c051e7541869c1d3d44f1d
Riccardo Brasca <riccardo.brasca@gmail.com>
2021-04-24 19:37:41 +0200

    Update src/group_theory/finiteness.lean
    Co-authored-by: Patrick Massot <patrickmassot@free.fr>

--- src/group_theory/finiteness.lean
+++ src/group_theory/finiteness.lean
@@ -48,29 +48,12 @@ lemma monoid.fg_def : monoid.fg M ↔
 ⟨λ ⟨S, hS⟩, ⟨S, hS, finset.finite_to_set S⟩, λ ⟨S, hS, hf⟩, ⟨set.finite.to_finset hf, by simp [hS]⟩⟩
 
 lemma monoid_fg_iff_add_fg : monoid.fg M ↔ add_monoid.fg (additive M) :=
-begin
-  split,
-  { intro h,
-    obtain ⟨S, hS, hf⟩ := (monoid.fg_def _).1 h,
-    rw [add_monoid.fg_def],
-    exact ⟨additive.to_mul ⁻¹' S, by simpa [← submonoid.to_add_submonoid_closure, hS], hf⟩ },
-  { intro h,
-    obtain ⟨T, hT, hf⟩ := (add_monoid.fg_def _).1 h,
-    rw [monoid.fg_def],
-    exact ⟨multiplicative.of_add ⁻¹' T, by simpa [← add_submonoid.to_submonoid'_closure, hT], hf⟩ }
-end
+⟨λ h, let ⟨S, hS, hf⟩ := (monoid.fg_def _).1 h in (add_monoid.fg_def _).mpr
+  ⟨additive.to_mul ⁻¹' S, by simpa [← submonoid.to_add_submonoid_closure, hS], hf⟩,
+ λ h, let ⟨T, hT, hf⟩ := (add_monoid.fg_def _).1 h in (monoid.fg_def _).mpr
+  ⟨multiplicative.of_add ⁻¹' T, by simpa [← add_submonoid.to_submonoid'_closure, hT], hf⟩⟩
 
 lemma add_monoid_fg_iff_mul_fg : add_monoid.fg N ↔ monoid.fg (multiplicative N) :=
-begin
-  split,
-  { intro h,
-    obtain ⟨S, hS, hf⟩ := (add_monoid.fg_def _).1 h,
-    rw [monoid.fg_def],
-    exact ⟨multiplicative.to_add ⁻¹' S, by simpa [← add_submonoid.to_submonoid_closure, hS], hf⟩, },
-  { intro h,
-    obtain ⟨T, hT, hf⟩ := (monoid.fg_def _).1 h,
-    rw [add_monoid.fg_def],
-    exact ⟨additive.of_mul ⁻¹' T, by simpa [← submonoid.to_add_submonoid'_closure, hT], hf⟩ }
-end
+(monoid_fg_iff_add_fg $ multiplicative N).symm
 
 end monoid

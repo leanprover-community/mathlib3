21085c8ded1ac3752d18476e007319d761cd4516
Eric <ericrboidi@gmail.com>
2021-04-24 21:04:48 +0100

    this will work, I'm 99% sure of it

--- archive/100-theorems-list/93_birthday_problem.lean
+++ archive/100-theorems-list/93_birthday_problem.lean
@@ -45,18 +45,6 @@ fintype.of_equiv {f : α → β // injective f} (embedding_of_subtype α β).sym
 lemma less_injs {α β} [fintype α] [fintype β] : ‖α ↪ β‖ ≤ ‖α → β‖ :=
 by {rw fintype.of_equiv_card, exact fintype.card_subtype_le injective}
 
-lemma lt_of_ne_last {n : ℕ} {i : fin n.succ} (h : i ≠ fin.last n) : i.1 < n :=
-begin
-  rw fin.ne_iff_vne at h,
-  simp only [fin.val_eq_coe, fin.coe_last] at h ⊢,
-  exact lt_of_le_of_ne (lt_succ_iff.mp i.property) h
-end
-
-def fin.cast_ne {n : ℕ} (i : fin n.succ) (h : i ≠ fin.last n) : fin n := i.cast_lt $ lt_of_ne_last h
-
-@[simp] lemma fin.cast_succ_cast_ne {n : ℕ} (i : fin n.succ) (h : i ≠ fin.last n)
-  : fin.cast_succ (i.cast_ne h) = i := fin.cast_succ_cast_lt i _
-
 lemma card_inj' (n : ℕ) (β) [fintype β] (h : n ≤ ‖β‖) : ‖fin n ↪ β‖ = desc_fac (‖β‖ - n) n :=
 begin
   induction n with n hn,
@@ -64,58 +52,59 @@ begin
     obtain ⟨f, g, ne⟩ := exists_pair_ne (fin 0 ↪ β),
     exfalso, apply ne, ext x, exact fin.elim0 x,
 
+  let extend : (fin n → β) → β → fin n.succ → β :=
+    λ f : fin n → β, λ b : β, fin.cons b f,
+
   let equiv_classes : (fin n ↪ β) → finset (fin n.succ ↪ β) :=
-    λ f : fin n ↪ β, univ.filter (λ g : fin n.succ ↪ β, ∀ k : fin n, f k = g k.succ),
+    λ f : fin n ↪ β, univ.filter (λ g : fin n.succ ↪ β, ∃ k : β, extend f k = g),
 
-  --let add_one : (fin n → β) → β → fin n.succ → β:= λ f : fin n → β, λ b : β, fin.cons _ f b,
-
-  --let equiv_classes' : (fin n ↪ β) → finset (fin n.succ ↪ β) :=
-    --λ f : fin n ↪ β, univ.filter (λ g : fin n.succ ↪ β, ∃ k : β, fin.cons _ f k = g),
-
-  have all_injf_covered : univ = univ.bUnion equiv_classes,
+  have all_injf_covered : univ = univ.bUnion equiv_classes, sorry, /-
     apply subset.antisymm,
     { rintros f -, rw mem_bUnion,
-      exact ⟨⟨λ x, f x.succ, λ _ _ h, fin.succ_inj.mp $ f.injective h⟩, by simp⟩ },
-    { exact subset_univ _ },
+      refine ⟨⟨fin.tail f, λ _ _ h, fin.succ_inj.mp $ f.injective h⟩, _⟩,
+      suffices : ∃ (a : β), extend (fin.tail ⇑f) a = ⇑f, by simpa,
+      use f 0, simp only [extend], exact fin.cons_self_tail _ },
+    { exact subset_univ _ }, -/
 
   have equiv_class_size : ∀ f : fin n ↪ β, |equiv_classes f| = ‖β‖ - n,
   {
     intro f, let poss_vals := univ \ finset.map ⟨f, f.inj'⟩ univ,
     have num_poss_vals : |poss_vals| = ‖β‖ - n, by simp [poss_vals, card_univ, card_sdiff],
-    apply le_antisymm,
-    { by_contra h, push_neg at h, let first := λ g : fin n.succ ↪ β, g 0,
+    apply le_antisymm, sorry, /-
+    { by_contra card_too_big, let first := λ g : fin n.succ ↪ β, g 0,
+      -- (if I just write down x 0 = y 0, it won't work! need this shim)
       suffices : ∃ x ∈ equiv_classes f, ∃ y ∈ equiv_classes f, x ≠ y ∧ first x = first y,
-      {
-        obtain ⟨x, x_equiv, y, y_equiv, x_ne_y, x_y_agree⟩ := this,
-        apply x_ne_y,
+      { obtain ⟨x, x_equiv, y, y_equiv, x_ne_y, x_y_agree⟩ := this,
         simp only [true_and, mem_filter, mem_univ, fin.coe_eq_cast_succ] at x_equiv y_equiv,
+        obtain ⟨x_zero, x_equiv⟩ := x_equiv, obtain ⟨y_zero, y_equiv⟩ := y_equiv,
+        simp only [extend, first] at x_y_agree x_equiv y_equiv,
 
-        ext t,
-        by_cases h : t = 0,
-        { subst h, exact x_y_agree },
-        { specialize x_equiv (t.cast_ne h), specialize y_equiv (t.cast_ne h),
-          rw fin.cast_succ_cast_ne at x_equiv y_equiv,
-          rw [←x_equiv, ←y_equiv] },
-      },
+        apply x_ne_y, ext t, revert t,
+        refine fin.induction _ _,
+          exact x_y_agree,
+        rintros t -, rw [←x_equiv, ←y_equiv, fin.cons_succ, fin.cons_succ] },
       apply finset.exists_ne_map_eq_of_card_lt_of_maps_to,
-      rw ←num_poss_vals at h, exact h,
+      push_neg at card_too_big, rw ←num_poss_vals at card_too_big, exact card_too_big,
 
-      intros g g_equiv, simp [last] at g_equiv ⊢, by_contra not_inj, push_neg at not_inj,
-      -- change simps here
-      obtain ⟨a, not_inj⟩ := not_inj, rw g_equiv at not_inj,
-      have := g.injective not_inj,
-      suffices : fin.cast_succ a ≠ fin.last n, by contradiction,
-      exact (fin.cast_succ_lt_last a).ne },
-    sorry, /-
+      intros g g_equiv, simp [first, extend] at g_equiv ⊢, obtain ⟨k, g_equiv⟩ := g_equiv,
+      have : g 0 = k, by rw [←g_equiv, fin.cons_zero],
+      intros x eq, have : g x.succ = g 0, by rw [←eq, ←g_equiv, fin.cons_succ],
+      apply fin.succ_ne_zero x, exact g.injective this }, -/
     {
-      let extend : β → fin n.succ → β := λ b, fin.snoc f b, -- if I don't λ, typechecker &$@!?
-      let extender : {x // x ∈ poss_vals} ↪ (fin n.succ ↪ β)
-        := ⟨λ b, ⟨extend b, _⟩, _⟩, rotate,
-      { intros a₁ a₂ f_eqs, sorry,
-      },
-      { sorry },
-      sorry
-    } -/
+      let extended : finset (fin n.succ ↪ β) :=
+        finset.map ⟨λ x : {x // x ∈ poss_vals}, ⟨extend f x, _⟩, _⟩ poss_vals.attach,
+      rotate,
+      { intros a₁ a₂ eq, simp only [extend] at eq, sorry },
+      { intros a₁ a₂ eq, simp only [extend] at eq,
+        ext, rw funext_iff at eq,
+        specialize eq 0, rwa [fin.cons_zero, fin.cons_zero] at eq },
+      have : |extended| = ‖β‖ - n, by simp [extended, poss_vals, card_sdiff, card_univ],
+      rw ←this, apply card_le_of_subset, rintros g g_extended,
+      simp only [extend, true_and, mem_filter, mem_univ],
+      use g 0, ext t, revert t,
+      refine fin.induction (by rw fin.cons_zero) _,
+      rintros i -, sorry
+    }
   },
   rw [←card_univ, all_injf_covered, card_bUnion], simp [equiv_class_size], unfold desc_fac,
   -- use hn and stuff

f78134db6cce456c3a5cbc7d56ae3d5c5d859b4d
Riccardo Brasca <riccardo.brasca@gmail.com>
2021-04-25 13:36:11 +0200

    generalize to submonoid

--- src/group_theory/finiteness.lean
+++ src/group_theory/finiteness.lean
@@ -40,20 +40,38 @@ def monoid.fg : Prop := (⊤ : submonoid M).fg
 itself. -/
 add_decl_doc add_monoid.fg
 
+variables {M N}
+
+/-- An equivalent expression of `submonoid.fg` in terms of `set.finite` instead of `finset`. -/
+@[to_additive "An equivalent expression of `add_submonoid.fg` in terms of `set.finite` instead of
+`finset`."]
+lemma submonoid.fg_def (P : submonoid M) : submonoid.fg P ↔
+  ∃ S : set M, submonoid.closure S = P ∧ S.finite :=
+⟨λ ⟨S, hS⟩, ⟨S, hS, finset.finite_to_set S⟩, λ ⟨S, hS, hf⟩, ⟨set.finite.to_finset hf, by simp [hS]⟩⟩
+
 /-- An equivalent expression of `monoid.fg` in terms of `set.finite` instead of `finset`. -/
 @[to_additive "An equivalent expression of `add_monoid.fg` in terms of `set.finite` instead of
 `finset`."]
 lemma monoid.fg_def : monoid.fg M ↔
   ∃ S : set M, submonoid.closure S = (⊤ : submonoid M) ∧ S.finite :=
-⟨λ ⟨S, hS⟩, ⟨S, hS, finset.finite_to_set S⟩, λ ⟨S, hS, hf⟩, ⟨set.finite.to_finset hf, by simp [hS]⟩⟩
+submonoid.fg_def ⊤
+
+lemma submonoid.fg_iff_add_fg (P : submonoid M) : P.fg ↔ P.to_add_submonoid.fg :=
+⟨λ h, let ⟨S, hS, hf⟩ := (submonoid.fg_def _).1 h in (add_submonoid.fg_def _).mpr
+  ⟨additive.to_mul ⁻¹' S, by simp [← submonoid.to_add_submonoid_closure, hS], hf⟩,
+ λ h, let ⟨T, hT, hf⟩ := (add_submonoid.fg_def _).1 h in (submonoid.fg_def _).mpr
+  ⟨multiplicative.of_add ⁻¹' T, by simp [← add_submonoid.to_submonoid'_closure, hT], hf⟩⟩
 
 lemma monoid_fg_iff_add_fg : monoid.fg M ↔ add_monoid.fg (additive M) :=
-⟨λ h, let ⟨S, hS, hf⟩ := (monoid.fg_def _).1 h in (add_monoid.fg_def _).mpr
-  ⟨additive.to_mul ⁻¹' S, by simpa [← submonoid.to_add_submonoid_closure, hS], hf⟩,
- λ h, let ⟨T, hT, hf⟩ := (add_monoid.fg_def _).1 h in (monoid.fg_def _).mpr
-  ⟨multiplicative.of_add ⁻¹' T, by simpa [← add_submonoid.to_submonoid'_closure, hT], hf⟩⟩
+submonoid.fg_iff_add_fg ⊤
+
+lemma add_submonoid_fg_iff_mul_fg (P : add_submonoid N) : P.fg ↔ P.to_submonoid.fg :=
+begin
+  convert (submonoid.fg_iff_add_fg P.to_submonoid).symm,
+  exact set_like.ext' rfl
+end
 
 lemma add_monoid_fg_iff_mul_fg : add_monoid.fg N ↔ monoid.fg (multiplicative N) :=
-(monoid_fg_iff_add_fg $ multiplicative N).symm
+add_submonoid_fg_iff_mul_fg ⊤
 
 end monoid

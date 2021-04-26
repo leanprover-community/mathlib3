1cac09e6a1f152449a4b635d93e94edff344e5b5
Riccardo Brasca <riccardo.brasca@gmail.com>
2021-04-26 11:45:08 +0200

    reorganize section

--- src/group_theory/finiteness.lean
+++ src/group_theory/finiteness.lean
@@ -19,9 +19,9 @@ We define finitely generated monoids. See also `module.finite` for finitely-gene
 
 -/
 
-section monoid
-
 variables {M N : Type*} [monoid M] [add_monoid N]
+
+section submonoid
 
 /-- A submonoid of `M` is finitely generated if it is the closure of a finite subset of `M`. -/
 @[to_additive]
@@ -31,6 +31,29 @@ def submonoid.fg (P : submonoid M) : Prop := ∃ S : finset M, submonoid.closure
 `M`. -/
 add_decl_doc add_submonoid.fg
 
+/-- An equivalent expression of `submonoid.fg` in terms of `set.finite` instead of `finset`. -/
+@[to_additive "An equivalent expression of `add_submonoid.fg` in terms of `set.finite` instead of
+`finset`."]
+lemma submonoid.fg_iff (P : submonoid M) : submonoid.fg P ↔
+  ∃ S : set M, submonoid.closure S = P ∧ S.finite :=
+⟨λ ⟨S, hS⟩, ⟨S, hS, finset.finite_to_set S⟩, λ ⟨S, hS, hf⟩, ⟨set.finite.to_finset hf, by simp [hS]⟩⟩
+
+lemma submonoid.fg_iff_add_fg (P : submonoid M) : P.fg ↔ P.to_add_submonoid.fg :=
+⟨λ h, let ⟨S, hS, hf⟩ := (submonoid.fg_iff _).1 h in (add_submonoid.fg_iff _).mpr
+  ⟨additive.to_mul ⁻¹' S, by simp [← submonoid.to_add_submonoid_closure, hS], hf⟩,
+ λ h, let ⟨T, hT, hf⟩ := (add_submonoid.fg_iff _).1 h in (submonoid.fg_iff _).mpr
+  ⟨multiplicative.of_add ⁻¹' T, by simp [← add_submonoid.to_submonoid'_closure, hT], hf⟩⟩
+
+lemma add_submonoid.fg_iff_mul_fg (P : add_submonoid N) : P.fg ↔ P.to_submonoid.fg :=
+begin
+  convert (submonoid.fg_iff_add_fg P.to_submonoid).symm,
+  exact set_like.ext' rfl
+end
+
+end submonoid
+
+section monoid
+
 variables (M N)
 
 /-- A monoid is finitely generated if it is finitely generated as a submonoid of itself. -/
@@ -38,7 +61,6 @@ class monoid.fg : Prop := (out : (⊤ : submonoid M).fg)
 
 /-- An additive monoid is finitely generated if it is finitely generated as an additive submonoid of
 itself. -/
-
 class add_monoid.fg : Prop := (out : (⊤ : add_submonoid N).fg)
 
 attribute [to_additive] monoid.fg
@@ -49,13 +71,6 @@ lemma monoid.fg_def : monoid.fg M ↔ (⊤ : submonoid M).fg := ⟨λ h, h.1, λ
 
 lemma add_monoid.fg_def : add_monoid.fg N ↔ (⊤ : add_submonoid N).fg := ⟨λ h, h.1, λ h, ⟨h⟩⟩
 
-/-- An equivalent expression of `submonoid.fg` in terms of `set.finite` instead of `finset`. -/
-@[to_additive "An equivalent expression of `add_submonoid.fg` in terms of `set.finite` instead of
-`finset`."]
-lemma submonoid.fg_iff (P : submonoid M) : submonoid.fg P ↔
-  ∃ S : set M, submonoid.closure S = P ∧ S.finite :=
-⟨λ ⟨S, hS⟩, ⟨S, hS, finset.finite_to_set S⟩, λ ⟨S, hS, hf⟩, ⟨set.finite.to_finset hf, by simp [hS]⟩⟩
-
 /-- An equivalent expression of `monoid.fg` in terms of `set.finite` instead of `finset`. -/
 @[to_additive "An equivalent expression of `add_monoid.fg` in terms of `set.finite` instead of
 `finset`."]
@@ -63,20 +78,8 @@ lemma monoid.fg_iff : monoid.fg M ↔
   ∃ S : set M, submonoid.closure S = (⊤ : submonoid M) ∧ S.finite :=
 ⟨λ h, (submonoid.fg_iff ⊤).1 h.out, λ h, ⟨(submonoid.fg_iff ⊤).2 h⟩⟩
 
-lemma submonoid.fg_iff_add_fg (P : submonoid M) : P.fg ↔ P.to_add_submonoid.fg :=
-⟨λ h, let ⟨S, hS, hf⟩ := (submonoid.fg_iff _).1 h in (add_submonoid.fg_iff _).mpr
-  ⟨additive.to_mul ⁻¹' S, by simp [← submonoid.to_add_submonoid_closure, hS], hf⟩,
- λ h, let ⟨T, hT, hf⟩ := (add_submonoid.fg_iff _).1 h in (submonoid.fg_iff _).mpr
-  ⟨multiplicative.of_add ⁻¹' T, by simp [← add_submonoid.to_submonoid'_closure, hT], hf⟩⟩
-
 lemma monoid_fg_iff_add_fg : monoid.fg M ↔ add_monoid.fg (additive M) :=
 ⟨λ h, ⟨(submonoid.fg_iff_add_fg ⊤).1 h.out⟩, λ h, ⟨(submonoid.fg_iff_add_fg ⊤).2 h.out⟩⟩
-
-lemma add_submonoid.fg_iff_mul_fg (P : add_submonoid N) : P.fg ↔ P.to_submonoid.fg :=
-begin
-  convert (submonoid.fg_iff_add_fg P.to_submonoid).symm,
-  exact set_like.ext' rfl
-end
 
 lemma add_monoid_fg_iff_mul_fg : add_monoid.fg N ↔ monoid.fg (multiplicative N) :=
 ⟨λ h, ⟨(add_submonoid.fg_iff_mul_fg ⊤).1 h.out⟩, λ h, ⟨(add_submonoid.fg_iff_mul_fg ⊤).2 h.out⟩⟩

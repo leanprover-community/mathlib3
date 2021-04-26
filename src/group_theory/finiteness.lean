61b67b703a5e0cd11e11696855303e6a4f4dd731
Riccardo Brasca <riccardo.brasca@gmail.com>
2021-04-26 22:07:04 +0200

    Update src/group_theory/finiteness.lean
    Co-authored-by: Eric Wieser <wieser.eric@gmail.com>

--- src/group_theory/finiteness.lean
+++ src/group_theory/finiteness.lean
@@ -17,7 +17,7 @@ finitely-generated modules.
 ## Main definition
 
 * `submonoid.fg S`, `add_submonoid.fg S` : A submonoid `S` is finitely generated.
-* `monoid.fg M`, `add_submonoid.fg M` : A typeclass indicating a type `M` is finitely generated as a
+* `monoid.fg M`, `add_monoid.fg M` : A typeclass indicating a type `M` is finitely generated as a
 monoid.
 * `subgroup.fg S`, `add_subgroup.fg S` : A subgroup `S` is finitely generated.
 * `group.fg M`, `add_group.fg M` : A typeclass indicating a type `M` is finitely generated as a

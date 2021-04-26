6694971e467f86b00acedb92aa2ee3ff84237f28
Eric <37984851+ericrbg@users.noreply.github.com>
2021-04-27 00:48:30 +0100

    Apply suggestions from code review
    Co-authored-by: Eric Wieser <wieser.eric@gmail.com>

--- archive/100-theorems-list/93_birthday_problem.lean
+++ archive/100-theorems-list/93_birthday_problem.lean
@@ -11,11 +11,12 @@ import data.fintype.card_inj
 This file proves Theorem 93 from the [100 Theorems List](https://www.cs.ru.nl/~freek/100/).
 
 As of writing, probability is not implemented in Lean, so we instead state the birthday problem
-in terms of injective functions. We instead prove a general result about ‖α ↪ β‖.
+in terms of injective functions. The general result about `fintype.card (α ↪ β)` which this proof
+uses is `fintype.card_inj`.
 -/
 
 local notation `‖` x `‖` := fintype.card x
 
-theorem birthday : 2 * ‖fin 23 ↪ fin 365‖ < ‖fin 23 → fin 365‖ := by norm_num [nat.desc_fac]
-
-lemma birthday' : 2 * ‖fin 22 ↪ fin 365‖ > ‖fin 22 → fin 365‖ := by norm_num [nat.desc_fac]
+theorem birthday :
+  2 * ‖fin 23 ↪ fin 365‖ < ‖fin 23 → fin 365‖ ∧ 2 * ‖fin 22 ↪ fin 365‖ > ‖fin 22 → fin 365‖ :=
+by split; norm_num [nat.desc_fac]

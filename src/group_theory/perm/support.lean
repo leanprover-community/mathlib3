9d71d2d029e231fd0654fba6eff889f118dd5b7e
Yakov Pechersky <ffxen158@gmail.com>
2021-04-27 01:12:37 -0400

    simplify imports

--- src/group_theory/perm/support.lean
+++ src/group_theory/perm/support.lean
@@ -4,9 +4,7 @@ Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Chris Hughes, Aaron Anderson, Yakov Pechersky
 -/
 import data.set.finite
-import data.fintype.basic
 import group_theory.perm.basic
-import data.list.perm
 import dynamics.fixed_points.basic
 
 /-!

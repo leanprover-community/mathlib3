794d4dab56a1369e194e4ec59d75afd717fb403d
Yakov Pechersky <ffxen158@gmail.com>
2021-04-27 20:59:26 -0400

    author

--- src/data/list/rotate.lean
+++ src/data/list/rotate.lean
@@ -1,7 +1,7 @@
 /-
 Copyright (c) 2019 Chris Hughes. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
-Authors: Chris Hughes
+Authors: Chris Hughes, Yakov Pechersky
 -/
 import data.list.perm
 import data.list.range

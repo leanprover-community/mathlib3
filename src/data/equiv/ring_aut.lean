6de6b382618075d6770b9dc69f2af0c4878f01ca
Eric <ericrboidi@gmail.com>
2021-04-28 10:42:21 +0100

    Merge branch 'master' into birthday

--- src/data/equiv/ring_aut.lean
+++ src/data/equiv/ring_aut.lean
@@ -42,7 +42,8 @@ by refine_struct
   one := ring_equiv.refl R,
   inv := ring_equiv.symm,
   div := _,
-  npow :=  @npow_rec _ ⟨ring_equiv.refl R⟩ ⟨λ g h, ring_equiv.trans h g⟩ };
+  npow := @npow_rec _ ⟨ring_equiv.refl R⟩ ⟨λ g h, ring_equiv.trans h g⟩,
+  gpow := @gpow_rec _ ⟨ring_equiv.refl R⟩ ⟨λ g h, ring_equiv.trans h g⟩ ⟨ring_equiv.symm⟩ };
 intros; ext; try { refl }; apply equiv.left_inv
 
 instance : inhabited (ring_aut R) := ⟨1⟩

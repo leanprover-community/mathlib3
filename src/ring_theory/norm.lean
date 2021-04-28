4d7eafc94a49be07e3a55017b2427ec9408a632a
Vierkantor <vierkantor@vierkantor.com>
2021-04-28 10:03:44 +0000

    Define the ring norm as the determinant of `lmul R S`

---
+++ src/ring_theory/norm.lean
@@ -0,0 +1,104 @@
+/-
+Copyright (c) 2021 Anne Baanen. All rights reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
+Authors: Anne Baanen
+-/
+
+import linear_algebra.matrix
+import ring_theory.power_basis
+
+/-!
+# Norm for (finite) ring extensions.
+
+Suppose we have an `R`-algebra `S` with a finite basis. For each `s : S`,
+the determinant of the linear map given by multiplying by `s` gives information
+about the roots of the minimal polynomial of `s` over `R`.
+
+## Implementation notes
+
+Typically, the norm is defined specifically for finite field extensions.
+The definition is as general as possible and the assumption that we have
+fields or that the extension is finite is added to the lemmas as needed.
+
+We only define the norm for left multiplication (`algebra.left_mul_matrix`,
+i.e. `algebra.lmul_left`).
+For now, the definitions assume `S` is commutative, so the choice doesn't matter anyway.
+
+See also `algebra.norm`, which is defined similarly as the norm of
+`algebra.left_mul_matrix`.
+
+## References
+
+ * https://en.wikipedia.org/wiki/Field_norm
+
+-/
+
+universes u v w
+
+variables {R S T : Type*} [comm_ring R] [comm_ring S] [comm_ring T]
+variables [algebra R S] [algebra R T]
+variables {K L : Type*} [field K] [field L] [algebra K L]
+variables {ι : Type w} [fintype ι]
+
+open finite_dimensional
+open linear_map
+open matrix
+
+open_locale big_operators
+open_locale matrix
+
+namespace algebra
+
+variables {b : ι → S} (hb : is_basis R b)
+
+variables (R S)
+
+/-- The norm of an element `s` of an `R`-algebra is the determinant of `(*) s`. -/
+noncomputable def norm : S →* R :=
+linear_map.det.comp (lmul R S).to_ring_hom.to_monoid_hom
+
+@[simp] lemma norm_apply (x : S) : norm R S x = linear_map.det (lmul R S x) := rfl
+
+variables {S}
+
+lemma norm_eq_one_of_not_exists_basis
+  (h : ¬ ∃ (s : set S) (b : is_basis R (coe : s → S)), s.finite) (x) : norm R S x = 1 :=
+by { rw [norm_apply, linear_map.det], split_ifs with h, refl }
+
+include hb
+
+variables {R}
+
+-- Can't be a `simp` lemma because it depends on a choice of basis
+lemma norm_eq_matrix_det [decidable_eq ι] (hb : is_basis R b) (s : S) :
+  norm R S s = matrix.det (algebra.left_mul_matrix hb s) :=
+by rw [norm_apply, linear_map.det_eq_det_to_matrix hb, to_matrix_lmul_eq]
+
+/-- If `x` is in the base field `K`, then the norm is `x ^ [L : K]`. -/
+lemma norm_algebra_map_of_basis (x : R) :
+  norm R S (algebra_map R S x) = x ^ fintype.card ι :=
+begin
+  haveI := classical.dec_eq ι,
+  rw [norm_apply, linear_map.det_eq_det_to_matrix hb, lmul_algebra_map],
+  convert @det_diagonal _ _ _ _ _ (λ (i : ι), x),
+  { ext i j, rw [to_matrix_lsmul, matrix.diagonal] },
+  { rw [finset.prod_const, finset.card_univ] }
+end
+omit hb
+
+/-- If `x` is in the base field `K`, then the norm is `x ^ [L : K]`.
+
+(If `L` is not finite-dimensional over `K`, then `norm = 1 = x ^ 0 = x ^ (finrank L K)`.)
+-/
+@[simp]
+lemma norm_algebra_map (x : K) : norm K L (algebra_map K L x) = x ^ finrank K L :=
+begin
+  by_cases H : ∃ (s : set L) (b : is_basis K (coe : s → L)), s.finite,
+  { haveI : fintype H.some := H.some_spec.some_spec.some,
+    rw [norm_algebra_map_of_basis H.some_spec.some, finrank_eq_card_basis H.some_spec.some] },
+  { rw [norm_eq_one_of_not_exists_basis K H, finrank_eq_zero_of_not_exists_basis, pow_zero],
+    rintros ⟨s, b⟩,
+    exact H ⟨↑s, b, s.finite_to_set⟩ },
+end
+
+end algebra

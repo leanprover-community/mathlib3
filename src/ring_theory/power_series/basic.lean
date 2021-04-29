12bfc3f8065e0539544ed10674eb35a4d775bd97
sgouezel <sebastien.gouezel@univ-rennes1.fr>
2021-04-29 22:54:22 +0200

    further fixes

--- src/ring_theory/power_series/basic.lean
+++ src/ring_theory/power_series/basic.lean
@@ -1649,7 +1649,8 @@ congr_arg (coeff φ) (finsupp.single_eq_same)
   (monomial n a : power_series R) = power_series.monomial R n a :=
 by { ext, simp [coeff_coe, power_series.coeff_monomial, polynomial.coeff_monomial, eq_comm] }
 
-@[simp, norm_cast] lemma coe_zero : ((0 : polynomial R) : power_series R) = 0 := rfl
+@[simp, norm_cast] lemma coe_zero : ((0 : polynomial R) : power_series R) = 0 :=
+by { ext, simp }
 
 @[simp, norm_cast] lemma coe_one : ((1 : polynomial R) : power_series R) = 1 :=
 begin
@@ -1658,7 +1659,8 @@ begin
 end
 
 @[simp, norm_cast] lemma coe_add (φ ψ : polynomial R) :
-  ((φ + ψ : polynomial R) : power_series R) = φ + ψ := rfl
+  ((φ + ψ : polynomial R) : power_series R) = φ + ψ :=
+by { ext, simp }
 
 @[simp, norm_cast] lemma coe_mul (φ ψ : polynomial R) :
   ((φ * ψ : polynomial R) : power_series R) = φ * ψ :=

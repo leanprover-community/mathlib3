83b93fe8cdad3d061136de9609121650fba99a9f
Adam Topaz <github@adamtopaz.com>
2021-04-30 11:12:44 -0600

    Add author

--- src/topology/discrete_quotient.lean
+++ src/topology/discrete_quotient.lean
@@ -1,7 +1,7 @@
 /-
 Copyright (c) 2021 Adam Topaz. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
-Authors: Adam Topaz
+Authors: Calle Sönne, Adam Topaz
 -/
 import topology.separation
 import topology.subset_properties

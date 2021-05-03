4ddad84c3db47e42802b0f7509f6e7ccbeb9e3bf
Scott Morrison <scott.morrison@gmail.com>
2021-05-03 22:18:10 +1000

    comments

--- src/category_theory/derived.lean
+++ src/category_theory/derived.lean
@@ -5,6 +5,39 @@ Authors: Scott Morrison
 -/
 import category_theory.preadditive.projective_resolution
 
+/-!
+# Left-derived functors
+
+We define the left-derived functors `F.left_derived n : C ⥤ D` for any additive functor `F`
+out of a category with projective resolutions.
+
+The definition is
+```
+projective_resolutions C ⋙ F.map_homotopy_category _ ⋙ homotopy_category.homology_functor D _ n
+```
+that is, we pick a projective resolution thought of as an object of the homotopy category,
+we apply `F` objectwise, and compute `n`-th homology.
+
+We show that these left-derived functors can be calculated
+on objects using any choice of projective resolution,
+and on morphisms by any choice of lift to a chain map between chosen projective resolutions.
+
+Similarly we define natural transformations between left-derived functors coming from
+natural transformations between the original additive functors,
+and show how to compute the components.
+
+## Implementation
+
+We don't assume the categories involved are abelian
+(just preadditive, and have equalizers, cokernels, and image maps),
+or that the functors are right exact.
+None of these assumptions are needed yet.
+
+It is often convenient, of course, to work with `[abelian C] [enough_projectives C] [abelian D]`
+which (assuming the results from `category_theory.abelian.projective`) are enough to
+provide all the typeclass hypotheses assumed here.
+-/
+
 noncomputable theory
 
 open category_theory
@@ -15,6 +48,7 @@ universes v u
 namespace category_theory
 variables {C : Type u} [category.{v} C] {D : Type*} [category D]
 
+-- Importing `category_theory.abelian.projective` and assuming
 -- `[abelian C] [enough_projectives C] [abelian D]` suffices to acquire all the following:
 variables [preadditive C] [has_zero_object C] [has_equalizers C] [has_cokernels C]
   [has_images C] [has_image_maps C] [has_projective_resolutions C]
@@ -22,12 +56,12 @@ variables [preadditive D] [has_zero_object D] [has_equalizers D] [has_cokernels 
   [has_images D] [has_image_maps D]
 
 /-- The left derived functors of an additive functor. -/
-def functor.left_derived (n : ℕ) (F : C ⥤ D) [F.additive] : C ⥤ D :=
+def functor.left_derived (F : C ⥤ D) [F.additive] (n : ℕ) : C ⥤ D :=
 projective_resolutions C ⋙ F.map_homotopy_category _ ⋙ homotopy_category.homology_functor D _ n
 
 /-- We can compute a left derived functor using a chosen projective resolution. -/
-def functor.left_derived_obj_iso (n : ℕ) (F : C ⥤ D) [F.additive]
-  (X : C) (P : ProjectiveResolution X) :
+def functor.left_derived_obj_iso (F : C ⥤ D) [F.additive] (n : ℕ)
+  {X : C} (P : ProjectiveResolution X) :
   (F.left_derived n).obj X ≅
     (homology_functor D _ n).obj ((F.map_homological_complex _).obj P.complex) :=
 (homotopy_category.homology_functor D _ n).map_iso
@@ -39,13 +73,13 @@ def functor.left_derived_obj_iso (n : ℕ) (F : C ⥤ D) [F.additive]
 We can compute a left derived functor on a morphism using a lift of that morphism
 to a chain map between chosen projective resolutions.
 -/
-lemma functor.left_derived_map_eq (n : ℕ) (F : C ⥤ D) [F.additive] {X Y : C} (f : X ⟶ Y)
-  (P : ProjectiveResolution X) (Q : ProjectiveResolution Y) (g : P.complex ⟶ Q.complex)
+lemma functor.left_derived_map_eq (F : C ⥤ D) [F.additive] (n : ℕ) {X Y : C} (f : X ⟶ Y)
+  {P : ProjectiveResolution X} {Q : ProjectiveResolution Y} (g : P.complex ⟶ Q.complex)
   (w : g ≫ Q.π = P.π ≫ (chain_complex.of C).map f) :
   (F.left_derived n).map f =
-  (F.left_derived_obj_iso n X P).hom ≫
+  (F.left_derived_obj_iso n P).hom ≫
     (homology_functor D _ n).map ((F.map_homological_complex _).map g) ≫
-    (F.left_derived_obj_iso n Y Q).inv :=
+    (F.left_derived_obj_iso n Q).inv :=
 begin
   dsimp only [functor.left_derived, functor.left_derived_obj_iso],
   dsimp, simp only [category.comp_id, category.id_comp],
@@ -62,7 +96,7 @@ begin
 end
 
 /-- The natural transformation between left-derived functors induced by a natural transformation. -/
-def nat_trans.left_derived (n : ℕ) {F G : C ⥤ D} [F.additive] [G.additive] (α : F ⟶ G) :
+def nat_trans.left_derived {F G : C ⥤ D} [F.additive] [G.additive] (α : F ⟶ G) (n : ℕ) :
   F.left_derived n ⟶ G.left_derived n :=
 whisker_left (projective_resolutions C)
   (whisker_right (nat_trans.map_homotopy_category α _)
@@ -72,12 +106,12 @@ whisker_left (projective_resolutions C)
 A component of the natural transformation between left-derived functors can be computed
 using a chosen projective resolution.
 -/
-lemma nat_trans.left_derived_eq (n : ℕ) {F G : C ⥤ D} [F.additive] [G.additive] (α : F ⟶ G)
-  (X : C) (P : ProjectiveResolution X) :
-  (nat_trans.left_derived n α).app X =
-    (F.left_derived_obj_iso n X P).hom ≫
+lemma nat_trans.left_derived_eq {F G : C ⥤ D} [F.additive] [G.additive] (α : F ⟶ G) (n : ℕ)
+  {X : C} (P : ProjectiveResolution X) :
+  (nat_trans.left_derived α n).app X =
+    (F.left_derived_obj_iso n P).hom ≫
       (homology_functor D _ n).map ((nat_trans.map_homological_complex α _).app P.complex) ≫
-        (G.left_derived_obj_iso n X P).inv :=
+        (G.left_derived_obj_iso n P).inv :=
 begin
   symmetry,
   dsimp [nat_trans.left_derived, functor.left_derived_obj_iso],

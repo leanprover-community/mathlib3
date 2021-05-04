1ef8168726b09dea5c52fa0742eacca311a218e1
Johan Commelin <johan@commelin.net>
2021-05-04 06:57:04 +0000

    remove duplicate namespace

--- src/category_theory/opposites.lean
+++ src/category_theory/opposites.lean
@@ -272,23 +272,23 @@ protected def op (α : X ≅ Y) : op Y ≅ op X :=
   inv_hom_id' := quiver.hom.unop_inj α.hom_inv_id }
 
 /-- The isomorphism obtained from an isomorphism in the opposite category. -/
-@[simps] def iso.unop {X Y : Cᵒᵖ} (f : X ≅ Y) : (Y.unop ≅ X.unop) :=
+@[simps] def unop {X Y : Cᵒᵖ} (f : X ≅ Y) : (Y.unop ≅ X.unop) :=
 { hom := f.hom.unop,
   inv := f.inv.unop,
   hom_inv_id' := by simp only [← unop_comp, f.inv_hom_id, unop_id],
   inv_hom_id' := by simp only [← unop_comp, f.hom_inv_id, unop_id] }
 
 /-- The isomorphism obtained from an isomorphism in the opposite category. -/
-@[simps] def iso.remove_op {X Y : C} (f : (op X) ≅ (op Y)) : Y ≅ X :=
+@[simps] def remove_op {X Y : C} (f : (op X) ≅ (op Y)) : Y ≅ X :=
 { hom := f.hom.unop,
   inv := f.inv.unop,
   hom_inv_id' := by simp only [← unop_comp, f.inv_hom_id, unop_id_op],
   inv_hom_id' := by simp only [← unop_comp, f.hom_inv_id, unop_id_op] }
 
-@[simp] lemma iso.unop_op {X Y : Cᵒᵖ} (f : X ≅ Y) : f.unop.op = f :=
+@[simp] lemma unop_op {X Y : Cᵒᵖ} (f : X ≅ Y) : f.unop.op = f :=
 by ext; refl
 
-@[simp] lemma iso.op_unop {X Y : C} (f : X ≅ Y) : f.op.unop = f :=
+@[simp] lemma op_unop {X Y : C} (f : X ≅ Y) : f.op.unop = f :=
 by ext; refl
 
 end iso

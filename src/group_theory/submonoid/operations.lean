cde417ac061d09320286dbcd81e381c8cc0ae30a
Riccardo Brasca <riccardo.brasca@gmail.com>
2021-05-04 12:06:15 +0200

    Update src/group_theory/submonoid/operations.lean
    Co-authored-by: Eric Wieser <wieser.eric@gmail.com>

--- src/group_theory/submonoid/operations.lean
+++ src/group_theory/submonoid/operations.lean
@@ -191,7 +191,7 @@ lemma mem_map {f : M →* N} {S : submonoid M} {y : N} :
 mem_image_iff_bex
 
 @[to_additive]
-lemma mem_map_of_mem {f : M →* N} {S : submonoid M} {x : M} (hx : x ∈ S) : f x ∈ S.map f :=
+lemma mem_map_of_mem {S : submonoid M} (f : M →* N) {x : M} (hx : x ∈ S) : f x ∈ S.map f :=
 mem_image_of_mem f hx
 
 @[to_additive]

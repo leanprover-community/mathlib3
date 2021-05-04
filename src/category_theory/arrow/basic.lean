f993b47567027cf91d3126c3dafd7dcdf676f951
Johan Commelin <johan@commelin.net>
2021-05-04 07:43:00 +0000

    feat(category_theory/arrow): is_iso instance for arrow categories

--- src/category_theory/arrow/basic.lean
+++ src/category_theory/arrow/basic.lean
@@ -80,6 +80,12 @@ def hom_mk' {X Y : T} {f : X ⟶ Y} {P Q : T} {g : P ⟶ Q} {u : X ⟶ P} {v : Y
   sq.left ≫ g = f.hom ≫ sq.right :=
 sq.w
 
+instance {f g : arrow T} (ff : f ⟶ g) [is_iso ff.left] [is_iso ff.right] :
+  is_iso ff :=
+{ out := ⟨⟨inv ff.left, inv ff.right⟩,
+          by { ext; dsimp; simp only [is_iso.hom_inv_id] },
+          by { ext; dsimp; simp only [is_iso.inv_hom_id] }⟩ }
+
 /-- Given a square from an arrow `i` to an isomorphism `p`, express the source part of `sq`
 in terms of the inverse of `p`. -/
 @[simp] lemma square_to_iso_invert (i : arrow T) {X Y : T} (p : X ≅ Y) (sq : i ⟶ arrow.mk p.hom) :

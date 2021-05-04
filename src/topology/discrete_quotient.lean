0459e937f32906150ec3588521bd6b0b1e8ec579
Adam Topaz <github@adamtopaz.com>
2021-05-04 14:19:46 -0600

    Rename le_rel -> le_comap

--- src/topology/discrete_quotient.lean
+++ src/topology/discrete_quotient.lean
@@ -31,9 +31,9 @@ The partial ordering `A ≤ B` mathematically means that `B.proj` factors throug
 The top element `⊤` is the trivial quotient, meaning that every element of `X` is collapsed
 to a point. Given `h : A ≤ B`, the map `A → B` is `discrete_quotient.of_le h`.
 
-Given `f : X → Y` and `h : continuous f`, we define a predicate `le_rel h A B` for
+Given `f : X → Y` and `h : continuous f`, we define a predicate `le_comap h A B` for
 `A : discrete_quotient X` and `B : discrete_quotient Y`, asserting that `f` descends to `A → B`.
-If `cond : le_rel h A B`, the function `A → B` is obtained by `discrete_quotient.map cond`.
+If `cond : le_comap h A B`, the function `A → B` is obtained by `discrete_quotient.map cond`.
 
 ## Theorems
 The two main results proved in this file are:
@@ -204,56 +204,56 @@ variables {Y : Type*} [topological_space Y] {f : Y → X}
   (cont : continuous f) (A : discrete_quotient Y) (B : discrete_quotient X)
 
 /-- 
-Given `cont : continuous f`, `le_rel cont A B` is defined as `A ≤ B.comap f`.
+Given `cont : continuous f`, `le_comap cont A B` is defined as `A ≤ B.comap f`.
 Mathematically this means that `f` descends to a morphism `A → B`.
 -/
-def le_rel : Prop := A ≤ B.comap cont
+def le_comap : Prop := A ≤ B.comap cont
 
 variables {cont A B}
 
-lemma le_rel_id (A : discrete_quotient X) : le_rel continuous_id A A := by tauto
+lemma le_comap_id (A : discrete_quotient X) : le_comap continuous_id A A := by tauto
 
-lemma le_rel_comp {Z : Type*} [topological_space Z] {g : Z → Y} {cont' : continuous g}
-  {C : discrete_quotient Z} : le_rel cont' C A → le_rel cont A B →
-  le_rel (continuous.comp cont cont') C B := by tauto
+lemma le_comap_comp {Z : Type*} [topological_space Z] {g : Z → Y} {cont' : continuous g}
+  {C : discrete_quotient Z} : le_comap cont' C A → le_comap cont A B →
+  le_comap (continuous.comp cont cont') C B := by tauto
 
-lemma le_rel_trans {C : discrete_quotient X} :
-  le_rel cont A B → B ≤ C → le_rel cont A C := λ h1 h2, le_trans h1 $ comap_mono _ h2
+lemma le_comap_trans {C : discrete_quotient X} :
+  le_comap cont A B → B ≤ C → le_comap cont A C := λ h1 h2, le_trans h1 $ comap_mono _ h2
 
 /-- Map a discrete quotient along a continuous map. -/
-def map (cond : le_rel cont A B) : A → B := quotient.map' f cond
+def map (cond : le_comap cont A B) : A → B := quotient.map' f cond
 
-lemma map_continuous (cond : le_rel cont A B) : continuous (map cond) :=
+lemma map_continuous (cond : le_comap cont A B) : continuous (map cond) :=
 continuous_of_discrete_topology
 
 @[simp]
-lemma map_proj (cond : le_rel cont A B) : map cond ∘ A.proj = B.proj ∘ f := rfl
+lemma map_proj (cond : le_comap cont A B) : map cond ∘ A.proj = B.proj ∘ f := rfl
 
 @[simp]
-lemma map_proj_apply (cond : le_rel cont A B) (y : Y) : map cond (A.proj y) = B.proj (f y) := rfl
+lemma map_proj_apply (cond : le_comap cont A B) (y : Y) : map cond (A.proj y) = B.proj (f y) := rfl
 
 @[simp]
-lemma map_id : map (le_rel_id A) = id := by { ext ⟨⟩, refl }
+lemma map_id : map (le_comap_id A) = id := by { ext ⟨⟩, refl }
 
 @[simp]
 lemma map_comp {Z : Type*} [topological_space Z] {g : Z → Y} {cont' : continuous g}
-  {C : discrete_quotient Z} (h1 : le_rel cont' C A) (h2 : le_rel cont A B) :
-  map (le_rel_comp h1 h2) = map h2 ∘ map h1 := by { ext ⟨⟩, refl }
+  {C : discrete_quotient Z} (h1 : le_comap cont' C A) (h2 : le_comap cont A B) :
+  map (le_comap_comp h1 h2) = map h2 ∘ map h1 := by { ext ⟨⟩, refl }
 
 @[simp]
-lemma of_le_map {C : discrete_quotient X} (cond : le_rel cont A B) (h : B ≤ C) :
-   map (le_rel_trans cond h) = of_le h ∘ map cond := by { ext ⟨⟩, refl }
+lemma of_le_map {C : discrete_quotient X} (cond : le_comap cont A B) (h : B ≤ C) :
+   map (le_comap_trans cond h) = of_le h ∘ map cond := by { ext ⟨⟩, refl }
 
 @[simp]
-lemma of_le_map_apply {C : discrete_quotient X} (cond : le_rel cont A B) (h : B ≤ C) (a : A) :
-  map (le_rel_trans cond h) a = of_le h (map cond a) := by { rcases a, refl }
+lemma of_le_map_apply {C : discrete_quotient X} (cond : le_comap cont A B) (h : B ≤ C) (a : A) :
+  map (le_comap_trans cond h) a = of_le h (map cond a) := by { rcases a, refl }
 
 @[simp]
-lemma map_of_le {C : discrete_quotient Y} (cond : le_rel cont A B) (h : C ≤ A) :
+lemma map_of_le {C : discrete_quotient Y} (cond : le_comap cont A B) (h : C ≤ A) :
    map (le_trans h cond) = map cond ∘ of_le h := by { ext ⟨⟩, refl }
 
 @[simp]
-lemma map_of_le_apply {C : discrete_quotient Y} (cond : le_rel cont A B) (h : C ≤ A) (c : C) :
+lemma map_of_le_apply {C : discrete_quotient Y} (cond : le_comap cont A B) (h : C ≤ A) (c : C) :
   map (le_trans h cond) c = map cond (of_le h c) := by { rcases c, refl }
 
 end map

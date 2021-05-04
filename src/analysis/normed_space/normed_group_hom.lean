01ac364d7380ed3d2795fdb35b6f5ab1e92f2da0
Riccardo Brasca <riccardo.brasca@gmail.com>
2021-05-04 14:05:10 +0200

    Update src/analysis/normed_space/normed_group_hom.lean
    Co-authored-by: Eric Wieser <wieser.eric@gmail.com>

--- src/analysis/normed_space/normed_group_hom.lean
+++ src/analysis/normed_space/normed_group_hom.lean
@@ -438,7 +438,7 @@ by { ext, refl }
 lemma ker_zero : (0 : normed_group_hom V₁ V₂).ker = ⊤ :=
 by { ext, simp [normed_group_hom.mem_ker] }
 
-lemma ker_eq_preimage : (f.ker : set V₁) = (f : V₁ → V₂) ⁻¹' {0} :=
+lemma coe_ker : (f.ker : set V₁) = (f : V₁ → V₂) ⁻¹' {0} :=
 by { ext, erw f.mem_ker }
 
 lemma is_closed_ker {V₂ : Type*} [normed_group V₂] (f : normed_group_hom V₁ V₂) :

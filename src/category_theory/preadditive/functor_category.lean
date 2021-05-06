8aabeba5b2469be39996c0e5f05fa05dfc70704f
Johan Commelin <johan@commelin.net>
2021-05-06 14:06:04 +0000

    addy copy righty

--- src/category_theory/preadditive/functor_category.lean
+++ src/category_theory/preadditive/functor_category.lean
@@ -1,3 +1,9 @@
+/-
+Copyright (c) 2021 Johan Commelin. All rights reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
+Authors: Johan Commelin
+-/
+
 import category_theory.preadditive.default
 
 /-!

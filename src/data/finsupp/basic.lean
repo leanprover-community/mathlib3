deb329d9e9a1e802f7211d39963b32f91c0209f3
Bolton Bailey <bolton.bailey@gmail.com>
2021-05-07 22:38:40 -0400

    refactor(data/finsupp/basic): Rename single_sub to single_sub_nat

--- src/data/finsupp/basic.lean
+++ src/data/finsupp/basic.lean
@@ -909,7 +909,7 @@ instance nat_sub : has_sub (α →₀ ℕ) := ⟨zip_with (λ m n, m - n) (nat.s
 @[simp] lemma coe_nat_sub (g₁ g₂ : α →₀ ℕ) : ⇑(g₁ - g₂) = g₁ - g₂ := rfl
 lemma nat_sub_apply (g₁ g₂ : α →₀ ℕ) (a : α) : (g₁ - g₂) a = g₁ a - g₂ a := rfl
 
-@[simp] lemma single_sub {a : α} {n₁ n₂ : ℕ} : single a (n₁ - n₂) = single a n₁ - single a n₂ :=
+@[simp] lemma single_sub_nat {a : α} {n₁ n₂ : ℕ} : single a (n₁ - n₂) = single a n₁ - single a n₂ :=
 begin
   ext f,
   by_cases h : (a = f),

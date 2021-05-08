bentoner/mathlib

Update `std_basis`

src/linear_algebra/std_basis.lean
@@ -187,60 +187,165 @@ begin
     exact (disjoint_std_basis_std_basis _ _ _ _ h₃).mono h₁ h₂ }
 end
 
+@[to_additive add_equiv.Pi_congr_right]
+def mul_equiv.Pi_congr_right
+  {Ms Ns : η → Type*} [Π j, monoid (Ms j)] [Π j, monoid (Ns j)]
+  (es : ∀ j, Ms j ≃* Ns j) : (Π j, Ms j) ≃* (Π j, Ns j) :=
+{ map_mul' := λ x y,
+    by { ext j, simp_rw [equiv.Pi_congr_right, pi.mul_apply, mul_equiv.coe_to_equiv, mul_equiv.map_mul] },
+  .. equiv.Pi_congr_right (λ j, (es j).to_equiv) }
+
+@[simp, to_additive add_equiv.Pi_congr_right_apply]
+lemma mul_equiv.Pi_congr_right_apply
+  {Ms Ns : η → Type*} [Π j, monoid (Ms j)] [Π j, monoid (Ns j)]
+  (es : ∀ j, Ms j ≃* Ns j) (x j) :
+  mul_equiv.Pi_congr_right es x j = es j (x j) := rfl
+
+def linear_equiv.Pi_congr_right {Ms Ns : η → Type*}
+  [Π j, add_comm_monoid (Ms j)] [Π j, semimodule R (Ms j)]
+  [Π j, add_comm_monoid (Ns j)] [Π j, semimodule R (Ns j)]
+  (es : ∀ j, Ms j ≃ₗ[R] Ns j) : (Π j, Ms j) ≃ₗ[R] (Π j, Ns j) :=
+{ map_smul' := λ m x, by { ext j, simp },
+  .. add_equiv.Pi_congr_right (λ j, (es j).to_add_equiv) }
+
+@[simp]
+lemma linear_equiv.Pi_congr_right_apply
+  {Ms Ns : η → Type*}
+  [Π j, add_comm_monoid (Ms j)] [Π j, semimodule R (Ms j)]
+  [Π j, add_comm_monoid (Ns j)] [Π j, semimodule R (Ns j)]
+  (es : ∀ j, Ms j ≃ₗ[R] Ns j) (x j) :
+  linear_equiv.Pi_congr_right es x j = es j (x j) := rfl
+
 variable [fintype η]
 
-lemma is_basis_std_basis [decidable_eq η] (s : Πj, ιs j → (Ms j)) (hs : ∀j, is_basis R (s j)) :
-  is_basis R (λ (ji : Σ j, ιs j), std_basis R Ms ji.1 (s ji.1 ji.2)) :=
+noncomputable def finsupp.sigma_finsupp_equiv_pi_finsupp
+  {α : Type*} {ιs : η → Type*} [has_zero α] :
+  ((Σ j, ιs j) →₀ α) ≃ Π j, (ιs j →₀ α) :=
+{ to_fun := λ f j, f.split j,
+  inv_fun := λ f, finsupp.on_finset
+    (finset.sigma finset.univ (λ j, (f j).support))
+    (λ ji, f ji.1 ji.2)
+    (λ g hg, finset.mem_sigma.mpr ⟨finset.mem_univ _, finsupp.mem_support_iff.mpr hg⟩),
+  left_inv := λ f, by { ext, simp [finsupp.split] },
+  right_inv := λ f, by { ext, simp [finsupp.split] } }
+
+@[simp] lemma finsupp.sigma_finsupp_equiv_pi_finsupp_apply
+  {α : Type*} {ιs : η → Type*} [has_zero α] (f : (Σ j, ιs j) →₀ α) (j i) :
+  finsupp.sigma_finsupp_equiv_pi_finsupp f j i = f ⟨j, i⟩ := rfl
+
+noncomputable def finsupp.sigma_finsupp_add_equiv_pi_finsupp
+  {α : Type*} {ιs : η → Type*} [add_monoid α] :
+  ((Σ j, ιs j) →₀ α) ≃+ Π j, (ιs j →₀ α) :=
+{ map_add' := λ f g, by { ext, simp },
+  .. finsupp.sigma_finsupp_equiv_pi_finsupp }
+
+@[simp] lemma finsupp.sigma_finsupp_add_equiv_pi_finsupp_apply
+{α : Type*} {ιs : η → Type*} [add_monoid α] (f : (Σ j, ιs j) →₀ α) (j i) :
+  finsupp.sigma_finsupp_add_equiv_pi_finsupp f j i = f ⟨j, i⟩ := rfl
+
+section
+
+variables (R)
+
+noncomputable def finsupp.sigma_finsupp_lequiv_pi_finsupp
+  {M : Type*} {ιs : η → Type*} [add_comm_monoid M] [semimodule R M] :
+  ((Σ j, ιs j) →₀ M) ≃ₗ[R] Π j, (ιs j →₀ M) :=
+{ map_smul' := λ c f, by { ext, simp },
+  .. finsupp.sigma_finsupp_add_equiv_pi_finsupp }
+
+@[simp] lemma finsupp.sigma_finsupp_lequiv_pi_finsupp_apply
+  {M : Type*} {ιs : η → Type*} [add_comm_monoid M] [semimodule R M]
+  (f : (Σ j, ιs j) →₀ M) (j i) :
+  finsupp.sigma_finsupp_lequiv_pi_finsupp R f j i = f ⟨j, i⟩ := rfl
+
+@[simp] lemma finsupp.sigma_finsupp_lequiv_pi_finsupp_symm_apply
+  {M : Type*} {ιs : η → Type*} [add_comm_monoid M] [semimodule R M]
+  (f : Π j, (ιs j →₀ M)) (ji) :
+  (finsupp.sigma_finsupp_lequiv_pi_finsupp R).symm f ji = f ji.1 ji.2 := rfl
+
+end
+
+section
+
+-- FIXME: we have to do some hacks to make the typeclass inference work
+local attribute [-instance] mul_zero_class.to_has_zero
+/-- `pi.basis (s : ∀ j, basis (ιs j) R (Ms j))` is the `Σ j, ιs j`-indexed basis on `Π j, Ms j`
+given by `s j` on each component. -/
+protected noncomputable def basis (s : ∀ j, basis (ιs j) R (Ms j)) :
+  basis (Σ j, ιs j) R (Π j, Ms j) :=
+let e₁ := ((linear_equiv.Pi_congr_right (λ j, (s j).repr))) in
+basis.of_repr (@linear_equiv.trans _ _ (Π i, ιs i →₀ R) _ _ _ _ _ _ _ _ e₁
+  (finsupp.sigma_finsupp_lequiv_pi_finsupp R).symm)
+
+@[simp] lemma basis_repr_std_basis [decidable_eq η] (s : ∀ j, basis (ιs j) R (Ms j)) (j i) :
+  (pi.basis s).repr (std_basis R _ j (s j i)) = finsupp.single ⟨j, i⟩ 1 :=
 begin
-  split,
-  { apply linear_independent_std_basis _ (assume i, (hs i).1) },
-  have h₁ : Union (λ j, set.range (std_basis R Ms j ∘ s j))
-    ⊆ range (λ (ji : Σ (j : η), ιs j), (std_basis R Ms (ji.fst)) (s (ji.fst) (ji.snd))),
-  { apply Union_subset, intro i,
-    apply range_comp_subset_range (λ x : ιs i, (⟨i, x⟩ : Σ (j : η), ιs j))
-        (λ (ji : Σ (j : η), ιs j), std_basis R Ms (ji.fst) (s (ji.fst) (ji.snd))) },
-  have h₂ : ∀ i, span R (range (std_basis R Ms i ∘ s i)) = range (std_basis R Ms i),
-  { intro i,
-    rw [set.range_comp, submodule.span_image, (assume i, (hs i).2), submodule.map_top] },
-  apply eq_top_mono,
-  apply span_mono h₁,
-  rw span_Union,
-  simp only [h₂],
-  apply supr_range_std_basis
+  ext ⟨j', i'⟩,
+  by_cases hj : j = j',
+  { subst hj,
+    simp only [pi.basis, linear_equiv.trans_apply, basis.repr_self, std_basis_same,
+        linear_equiv.Pi_congr_right_apply, finsupp.sigma_finsupp_lequiv_pi_finsupp_symm_apply],
+    symmetry,
+    exact basis.finsupp.single_apply_left
+      (λ i i' (h : (⟨j, i⟩ : Σ j, ιs j) = ⟨j, i'⟩), eq_of_heq (sigma.mk.inj h).2) _ _ _ },
+  simp only [pi.basis, linear_equiv.trans_apply, finsupp.sigma_finsupp_lequiv_pi_finsupp_symm_apply,
+      linear_equiv.Pi_congr_right_apply],
+  dsimp,
+  rw [std_basis_ne _ _ _ _ (ne.symm hj), linear_equiv.map_zero, finsupp.zero_apply,
+      finsupp.single_eq_of_ne],
+  rintros ⟨⟩,
+  contradiction
+end
+
+@[simp] lemma basis_apply [decidable_eq η] (s : ∀ j, basis (ιs j) R (Ms j)) (ji) :
+  pi.basis s ji = std_basis R _ ji.1 (s ji.1 ji.2) :=
+basis.apply_eq_iff.mpr (by simp)
+
+@[simp] lemma basis_repr (s : ∀ j, basis (ιs j) R (Ms j)) (x) (ji) :
+  (pi.basis s).repr x ji = (s ji.1).repr (x ji.1) ji.2 :=
+rfl
+
 end
 
 section
 variables (R η)
 
-lemma is_basis_fun₀ [decidable_eq η] : is_basis R
-    (λ (ji : Σ (j : η), unit),
-       (std_basis R (λ (i : η), R) (ji.fst)) 1) :=
-@is_basis_std_basis R η (λi:η, unit) (λi:η, R) _ _ _ _ _ (λ _ _, (1 : R))
-  (assume i, @is_basis_singleton_one _ _ _ _)
+noncomputable def basis_fun₀ : basis (Σ (j : η), unit) R (Π (j : η), R) :=
+pi.basis (λ j, basis.singleton unit R)
 
-lemma is_basis_fun [decidable_eq η] : is_basis R (λ i, std_basis R (λi:η, R) i 1) :=
-begin
-  apply (is_basis_fun₀ R η).comp (λ i, ⟨i, punit.star⟩),
-  apply bijective_iff_has_inverse.2,
-  use sigma.fst,
-  simp [function.left_inverse, function.right_inverse]
-end
+@[simp] lemma basis_fun₀_apply [decidable_eq η] (ji) :
+  basis_fun₀ R η ji = std_basis R (λ (i : η), R) ji.fst 1 :=
+by rw [basis_fun₀, pi.basis_apply, basis.singleton_apply]
 
-@[simp] lemma is_basis_fun_repr [decidable_eq η] (x : η → R) (i : η) :
-  (pi.is_basis_fun R η).repr x i = x i :=
-begin
-  conv_rhs { rw ← (pi.is_basis_fun R η).total_repr x },
-  rw [finsupp.total_apply, finsupp.sum_fintype],
-  show (pi.is_basis_fun R η).repr x i =
-    (∑ j, λ i, (pi.is_basis_fun R η).repr x j • std_basis R (λ _, R) j 1 i) i,
-  rw [finset.sum_apply, finset.sum_eq_single i],
-  { simp only [pi.smul_apply, smul_eq_mul, std_basis_same, mul_one] },
-  { rintros b - hb, simp only [std_basis_ne _ _ _ _ hb.symm, smul_zero] },
-  { intro,
-    have := finset.mem_univ i,
-    contradiction },
-  { intros, apply zero_smul },
-end
+@[simp] lemma basis_fun₀_repr (x i) :
+  (basis_fun₀ R η).repr x i = x i.1 :=
+by { rcases i with ⟨j, ⟨⟩⟩, simp [basis_fun₀] }
+
+noncomputable def finsupp.add_equiv_fun_on_fintype (α M : Type*) [fintype α] [add_monoid M] :
+  (α →₀ M) ≃+ (α → M) :=
+{ map_add' := λ x y, by simp [finsupp.equiv_fun_on_fintype],
+  .. finsupp.equiv_fun_on_fintype }
+
+noncomputable def finsupp.lequiv_fun_on_fintype (α M : Type*) [fintype α]
+  [add_comm_monoid M] [semimodule R M] :
+  (α →₀ M) ≃ₗ[R] (α → M) :=
+{ map_smul' := λ x y, by simp [finsupp.add_equiv_fun_on_fintype, finsupp.equiv_fun_on_fintype],
+  .. finsupp.add_equiv_fun_on_fintype α M }
+
+noncomputable def basis_fun : basis η R (Π (j : η), R) :=
+(basis_fun₀ R η).reindex
+  { to_fun := sigma.fst,
+    inv_fun := λ i, ⟨i, punit.star⟩,
+    left_inv := λ ⟨j, i⟩, by simp,
+    right_inv := λ j, rfl }
+
+@[simp] lemma basis_fun_apply [decidable_eq η] (i) :
+  basis_fun R η i = std_basis R (λ (i : η), R) i 1 :=
+by simp [basis_fun]
+
+@[simp] lemma basis_fun_repr (x : η → R) (i : η) :
+  (pi.basis_fun R η).repr x i = x i :=
+by simp [basis_fun]
 
 end
 

leanprover-community/mathlib3

src/linear_algebra/std_basis.lean
@@ -187,60 +187,165 @@ begin
     exact (disjoint_std_basis_std_basis _ _ _ _ h₃).mono h₁ h₂ }
 end
 
+@[to_additive add_equiv.Pi_congr_right]
+def mul_equiv.Pi_congr_right
+  {Ms Ns : η → Type*} [Π j, monoid (Ms j)] [Π j, monoid (Ns j)]
+  (es : ∀ j, Ms j ≃* Ns j) : (Π j, Ms j) ≃* (Π j, Ns j) :=
+{ map_mul' := λ x y,
+    by { ext j, simp_rw [equiv.Pi_congr_right, pi.mul_apply, mul_equiv.coe_to_equiv, mul_equiv.map_mul] },
+  .. equiv.Pi_congr_right (λ j, (es j).to_equiv) }
+
+@[simp, to_additive add_equiv.Pi_congr_right_apply]
+lemma mul_equiv.Pi_congr_right_apply
+  {Ms Ns : η → Type*} [Π j, monoid (Ms j)] [Π j, monoid (Ns j)]
+  (es : ∀ j, Ms j ≃* Ns j) (x j) :
+  mul_equiv.Pi_congr_right es x j = es j (x j) := rfl
+
+def linear_equiv.Pi_congr_right {Ms Ns : η → Type*}
+  [Π j, add_comm_monoid (Ms j)] [Π j, module R (Ms j)]
+  [Π j, add_comm_monoid (Ns j)] [Π j, module R (Ns j)]
+  (es : ∀ j, Ms j ≃ₗ[R] Ns j) : (Π j, Ms j) ≃ₗ[R] (Π j, Ns j) :=
+{ map_smul' := λ m x, by { ext j, simp },
+  .. add_equiv.Pi_congr_right (λ j, (es j).to_add_equiv) }
+
+@[simp]
+lemma linear_equiv.Pi_congr_right_apply
+  {Ms Ns : η → Type*}
+  [Π j, add_comm_monoid (Ms j)] [Π j, module R (Ms j)]
+  [Π j, add_comm_monoid (Ns j)] [Π j, module R (Ns j)]
+  (es : ∀ j, Ms j ≃ₗ[R] Ns j) (x j) :
+  linear_equiv.Pi_congr_right es x j = es j (x j) := rfl
+
 variable [fintype η]
 
-lemma is_basis_std_basis [decidable_eq η] (s : Πj, ιs j → (Ms j)) (hs : ∀j, is_basis R (s j)) :
-  is_basis R (λ (ji : Σ j, ιs j), std_basis R Ms ji.1 (s ji.1 ji.2)) :=
+noncomputable def finsupp.sigma_finsupp_equiv_pi_finsupp
+  {α : Type*} {ιs : η → Type*} [has_zero α] :
+  ((Σ j, ιs j) →₀ α) ≃ Π j, (ιs j →₀ α) :=
+{ to_fun := λ f j, f.split j,
+  inv_fun := λ f, finsupp.on_finset
+    (finset.sigma finset.univ (λ j, (f j).support))
+    (λ ji, f ji.1 ji.2)
+    (λ g hg, finset.mem_sigma.mpr ⟨finset.mem_univ _, finsupp.mem_support_iff.mpr hg⟩),
+  left_inv := λ f, by { ext, simp [finsupp.split] },
+  right_inv := λ f, by { ext, simp [finsupp.split] } }
+
+@[simp] lemma finsupp.sigma_finsupp_equiv_pi_finsupp_apply
+  {α : Type*} {ιs : η → Type*} [has_zero α] (f : (Σ j, ιs j) →₀ α) (j i) :
+  finsupp.sigma_finsupp_equiv_pi_finsupp f j i = f ⟨j, i⟩ := rfl
+
+noncomputable def finsupp.sigma_finsupp_add_equiv_pi_finsupp
+  {α : Type*} {ιs : η → Type*} [add_monoid α] :
+  ((Σ j, ιs j) →₀ α) ≃+ Π j, (ιs j →₀ α) :=
+{ map_add' := λ f g, by { ext, simp },
+  .. finsupp.sigma_finsupp_equiv_pi_finsupp }
+
+@[simp] lemma finsupp.sigma_finsupp_add_equiv_pi_finsupp_apply
+{α : Type*} {ιs : η → Type*} [add_monoid α] (f : (Σ j, ιs j) →₀ α) (j i) :
+  finsupp.sigma_finsupp_add_equiv_pi_finsupp f j i = f ⟨j, i⟩ := rfl
+
+section
+
+variables (R)
+
+noncomputable def finsupp.sigma_finsupp_lequiv_pi_finsupp
+  {M : Type*} {ιs : η → Type*} [add_comm_monoid M] [module R M] :
+  ((Σ j, ιs j) →₀ M) ≃ₗ[R] Π j, (ιs j →₀ M) :=
+{ map_smul' := λ c f, by { ext, simp },
+  .. finsupp.sigma_finsupp_add_equiv_pi_finsupp }
+
+@[simp] lemma finsupp.sigma_finsupp_lequiv_pi_finsupp_apply
+  {M : Type*} {ιs : η → Type*} [add_comm_monoid M] [module R M]
+  (f : (Σ j, ιs j) →₀ M) (j i) :
+  finsupp.sigma_finsupp_lequiv_pi_finsupp R f j i = f ⟨j, i⟩ := rfl
+
+@[simp] lemma finsupp.sigma_finsupp_lequiv_pi_finsupp_symm_apply
+  {M : Type*} {ιs : η → Type*} [add_comm_monoid M] [module R M]
+  (f : Π j, (ιs j →₀ M)) (ji) :
+  (finsupp.sigma_finsupp_lequiv_pi_finsupp R).symm f ji = f ji.1 ji.2 := rfl
+
+end
+
+section
+
+-- FIXME: we have to do some hacks to make the typeclass inference work
+local attribute [-instance] mul_zero_class.to_has_zero
+/-- `pi.basis (s : ∀ j, basis (ιs j) R (Ms j))` is the `Σ j, ιs j`-indexed basis on `Π j, Ms j`
+given by `s j` on each component. -/
+protected noncomputable def basis (s : ∀ j, basis (ιs j) R (Ms j)) :
+  basis (Σ j, ιs j) R (Π j, Ms j) :=
+let e₁ := ((linear_equiv.Pi_congr_right (λ j, (s j).repr))) in
+basis.of_repr (@linear_equiv.trans _ _ (Π i, ιs i →₀ R) _ _ _ _ _ _ _ _ e₁
+  (finsupp.sigma_finsupp_lequiv_pi_finsupp R).symm)
+
+@[simp] lemma basis_repr_std_basis [decidable_eq η] (s : ∀ j, basis (ιs j) R (Ms j)) (j i) :
+  (pi.basis s).repr (std_basis R _ j (s j i)) = finsupp.single ⟨j, i⟩ 1 :=
 begin
-  split,
-  { apply linear_independent_std_basis _ (assume i, (hs i).1) },
-  have h₁ : Union (λ j, set.range (std_basis R Ms j ∘ s j))
-    ⊆ range (λ (ji : Σ (j : η), ιs j), (std_basis R Ms (ji.fst)) (s (ji.fst) (ji.snd))),
-  { apply Union_subset, intro i,
-    apply range_comp_subset_range (λ x : ιs i, (⟨i, x⟩ : Σ (j : η), ιs j))
-        (λ (ji : Σ (j : η), ιs j), std_basis R Ms (ji.fst) (s (ji.fst) (ji.snd))) },
-  have h₂ : ∀ i, span R (range (std_basis R Ms i ∘ s i)) = range (std_basis R Ms i),
-  { intro i,
-    rw [set.range_comp, submodule.span_image, (assume i, (hs i).2), submodule.map_top] },
-  apply eq_top_mono,
-  apply span_mono h₁,
-  rw span_Union,
-  simp only [h₂],
-  apply supr_range_std_basis
+  ext ⟨j', i'⟩,
+  by_cases hj : j = j',
+  { subst hj,
+    simp only [pi.basis, linear_equiv.trans_apply, basis.repr_self, std_basis_same,
+        linear_equiv.Pi_congr_right_apply, finsupp.sigma_finsupp_lequiv_pi_finsupp_symm_apply],
+    symmetry,
+    exact basis.finsupp.single_apply_left
+      (λ i i' (h : (⟨j, i⟩ : Σ j, ιs j) = ⟨j, i'⟩), eq_of_heq (sigma.mk.inj h).2) _ _ _ },
+  simp only [pi.basis, linear_equiv.trans_apply, finsupp.sigma_finsupp_lequiv_pi_finsupp_symm_apply,
+      linear_equiv.Pi_congr_right_apply],
+  dsimp,
+  rw [std_basis_ne _ _ _ _ (ne.symm hj), linear_equiv.map_zero, finsupp.zero_apply,
+      finsupp.single_eq_of_ne],
+  rintros ⟨⟩,
+  contradiction
+end
+
+@[simp] lemma basis_apply [decidable_eq η] (s : ∀ j, basis (ιs j) R (Ms j)) (ji) :
+  pi.basis s ji = std_basis R _ ji.1 (s ji.1 ji.2) :=
+basis.apply_eq_iff.mpr (by simp)
+
+@[simp] lemma basis_repr (s : ∀ j, basis (ιs j) R (Ms j)) (x) (ji) :
+  (pi.basis s).repr x ji = (s ji.1).repr (x ji.1) ji.2 :=
+rfl
+
 end
 
 section
 variables (R η)
 
-lemma is_basis_fun₀ [decidable_eq η] : is_basis R
-    (λ (ji : Σ (j : η), unit),
-       (std_basis R (λ (i : η), R) (ji.fst)) 1) :=
-@is_basis_std_basis R η (λi:η, unit) (λi:η, R) _ _ _ _ _ (λ _ _, (1 : R))
-  (assume i, @is_basis_singleton_one _ _ _ _)
+noncomputable def basis_fun₀ : basis (Σ (j : η), unit) R (Π (j : η), R) :=
+pi.basis (λ j, basis.singleton unit R)
 
-lemma is_basis_fun [decidable_eq η] : is_basis R (λ i, std_basis R (λi:η, R) i 1) :=
-begin
-  apply (is_basis_fun₀ R η).comp (λ i, ⟨i, punit.star⟩),
-  apply bijective_iff_has_inverse.2,
-  use sigma.fst,
-  simp [function.left_inverse, function.right_inverse]
-end
+@[simp] lemma basis_fun₀_apply [decidable_eq η] (ji) :
+  basis_fun₀ R η ji = std_basis R (λ (i : η), R) ji.fst 1 :=
+by rw [basis_fun₀, pi.basis_apply, basis.singleton_apply]
 
-@[simp] lemma is_basis_fun_repr [decidable_eq η] (x : η → R) (i : η) :
-  (pi.is_basis_fun R η).repr x i = x i :=
-begin
-  conv_rhs { rw ← (pi.is_basis_fun R η).total_repr x },
-  rw [finsupp.total_apply, finsupp.sum_fintype],
-  show (pi.is_basis_fun R η).repr x i =
-    (∑ j, λ i, (pi.is_basis_fun R η).repr x j • std_basis R (λ _, R) j 1 i) i,
-  rw [finset.sum_apply, finset.sum_eq_single i],
-  { simp only [pi.smul_apply, smul_eq_mul, std_basis_same, mul_one] },
-  { rintros b - hb, simp only [std_basis_ne _ _ _ _ hb.symm, smul_zero] },
-  { intro,
-    have := finset.mem_univ i,
-    contradiction },
-  { intros, apply zero_smul },
-end
+@[simp] lemma basis_fun₀_repr (x i) :
+  (basis_fun₀ R η).repr x i = x i.1 :=
+by { rcases i with ⟨j, ⟨⟩⟩, simp [basis_fun₀] }
+
+noncomputable def finsupp.add_equiv_fun_on_fintype (α M : Type*) [fintype α] [add_monoid M] :
+  (α →₀ M) ≃+ (α → M) :=
+{ map_add' := λ x y, by simp [finsupp.equiv_fun_on_fintype],
+  .. finsupp.equiv_fun_on_fintype }
+
+noncomputable def finsupp.lequiv_fun_on_fintype (α M : Type*) [fintype α]
+  [add_comm_monoid M] [module R M] :
+  (α →₀ M) ≃ₗ[R] (α → M) :=
+{ map_smul' := λ x y, by simp [finsupp.add_equiv_fun_on_fintype, finsupp.equiv_fun_on_fintype],
+  .. finsupp.add_equiv_fun_on_fintype α M }
+
+noncomputable def basis_fun : basis η R (Π (j : η), R) :=
+(basis_fun₀ R η).reindex
+  { to_fun := sigma.fst,
+    inv_fun := λ i, ⟨i, punit.star⟩,
+    left_inv := λ ⟨j, i⟩, by simp,
+    right_inv := λ j, rfl }
+
+@[simp] lemma basis_fun_apply [decidable_eq η] (i) :
+  basis_fun R η i = std_basis R (λ (i : η), R) i 1 :=
+by simp [basis_fun]
+
+@[simp] lemma basis_fun_repr (x : η → R) (i : η) :
+  (pi.basis_fun R η).repr x i = x i :=
+by simp [basis_fun]
 
 end
 
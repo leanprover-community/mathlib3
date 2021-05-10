b768fb74cf7cbf240e405a4c254ef06e91382f4d
adomani <adomani@gmail.com>
2021-05-10 08:38:21 +0200

    fix algebra/group_power/lemmas

--- src/algebra/group_power/lemmas.lean
+++ src/algebra/group_power/lemmas.lean
@@ -523,8 +523,10 @@ end linear_ordered_ring
 theorem nat.cast_le_pow_sub_div_sub {K : Type*} [linear_ordered_field K] {a : K} (H : 1 < a)
   (n : ℕ) :
   (n : K) ≤ (a ^ n - 1) / (a - 1) :=
-(le_div_iff (sub_pos.2 H)).2 $ le_sub_left_of_add_le $
-  one_add_mul_sub_le_pow ((neg_le_self $ @zero_le_one K _).trans H.le) _
+(le_div_iff (sub_pos.2 H)).2 $ le_sub_left_of_add_le begin
+  rw add_comm,
+  exact one_add_mul_sub_le_pow ((neg_le_self $ @zero_le_one K _).trans H.le) _
+end
 
 /-- For any `a > 1` and a natural `n` we have `n ≤ a ^ n / (a - 1)`. See also
 `nat.cast_le_pow_sub_div_sub` for a stronger inequality with `a ^ n - 1` in the numerator. -/

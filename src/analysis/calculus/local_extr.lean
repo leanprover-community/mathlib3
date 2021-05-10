01240dadb50678604ae289357ef429c3e06cce8f
Thomas Browning <tb65536@uw.edu>
2021-05-10 00:10:01 -0700

    import

--- src/analysis/calculus/local_extr.lean
+++ src/analysis/calculus/local_extr.lean
@@ -4,7 +4,6 @@ Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Yury Kudryashov
 -/
 import analysis.calculus.deriv
-import data.finset.sort
 import topology.algebra.polynomial
 import topology.local_extr
 

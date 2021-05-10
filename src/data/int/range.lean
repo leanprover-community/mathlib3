cbb3774ae9393a209d727c3df8d47fe97766f129
adomani <adomani@gmail.com>
2021-05-10 09:23:53 +0200

    fix data/set/intervals/basic

--- src/data/int/range.lean
+++ src/data/int/range.lean
@@ -17,13 +17,15 @@ def range (m n : ℤ) : list ℤ :=
 theorem mem_range_iff {m n r : ℤ} : r ∈ range m n ↔ m ≤ r ∧ r < n :=
 ⟨λ H, let ⟨s, h1, h2⟩ := list.mem_map.1 H in h2 ▸
   ⟨le_add_of_nonneg_right trivial,
-  add_lt_of_lt_sub_left $ match n-m, h1 with
+  lt_sub_iff_add_lt'.mp
+  --add_lt_of_lt_sub_left
+  $ match n-m, h1 with
     | (k:ℕ), h1 := by rwa [list.mem_range, to_nat_coe_nat, ← coe_nat_lt] at h1
     end⟩,
 λ ⟨h1, h2⟩, list.mem_map.2 ⟨to_nat (r-m),
   list.mem_range.2 $ by rw [← coe_nat_lt, to_nat_of_nonneg (sub_nonneg_of_le h1),
       to_nat_of_nonneg (sub_nonneg_of_le (le_of_lt (lt_of_le_of_lt h1 h2)))];
-    exact sub_lt_sub_right h2 _,
+    exact sub_lt_sub_right'' h2 _,
   show m + _ = _, by rw [to_nat_of_nonneg (sub_nonneg_of_le h1), add_sub_cancel'_right]⟩⟩
 
 instance decidable_le_lt (P : int → Prop) [decidable_pred P] (m n : ℤ) :

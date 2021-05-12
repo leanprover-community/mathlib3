a89e72cd670a5948e2e0ffa80f79405dc16274e2
YaelDillies <yael.dillies@gmail.com>
2021-05-12 07:13:02 +0100

    bundled hypergraph

--- src/combinatorics/szemeredi/regularity.lean
+++ src/combinatorics/szemeredi/regularity.lean
@@ -1,7 +1,15 @@
 import data.finset.basic
 import data.real.basic
 
-variables {V : Type*} {G : finset V → Prop} {k n : ℕ}
+variables {V : Type*} {G : finset V → Prop} {k n r : ℕ}
+
+structure partite_hypergraph (r : ℕ) :=
+(G : finset V → Prop)
+(ι : Type*)
+(ι_fin : fintype ι)
+(ι_card : fintype.card ι = r)
+(X : ι → finset (finset V))
+(partite : ∀ x₁ x₂, G x₁ → G x₂ → ∀ i₁ i₂, x₁ ∈ X i₁ → x₂ ∈ X i₂ → i₁ = i₂)
 
 def is_uniform_hypergraph (G : finset V → Prop) (k : ℕ) : Prop :=
 ∀ x : finset V, G x → x.card = k
@@ -10,6 +18,8 @@ def is_partite (G : finset V → Prop) (k : ℕ) : Prop :=
 ∃ (ι : Type*) (X : ι → finset (finset V)), ∀ x₁ x₂, G x₁ → G x₂ →
   ∀ i₁ i₂, x₁ ∈ X i₁ → x₂ ∈ X i₂ → i₁ = i₂
 
+def is_down_closed (G : finset V → Prop) : Prop :=
+∀ x y : finset V, x ≤ y → G y → G x
 --def is_partite_function
 
 --def is_quasi_random :

9aa12a9b464daaa6a694b99233b901575693ecab
YaelDillies <yael.dillies@gmail.com>
2021-05-12 07:47:06 +0100

    transferred to complete_lattice.lean

--- src/order/complete_lattice.lean
+++ src/order/complete_lattice.lean
@@ -1072,6 +1072,72 @@ instance pi.complete_lattice {α : Type*} {β : α → Type*} [∀ i, complete_l
   le_Inf := λ s f hf i, le_infi $ λ g, hf g g.2 i,
   .. pi.bounded_lattice }
 
+/- The Knaster-Tarski theorem -/
+instance complete_lattice_fixed_points {α : Type*} [complete_lattice α] {f : α → α} [monotone f] :
+complete_lattice {x // x = f x} :=
+{ le := (≤),
+  top := ⟨(⨆ (x : α) (hx : x ≤ f x), x), begin
+    let u := ⨆ (x : α) (hx : x ≤ f x), x,
+    have h : u ≤ f u := bsupr_le (λ x hx, hx.trans (‹monotone f› (le_bsupr x hx))),
+    refine h.antisymm _,
+    apply le_bsupr_of_le _,--exact le_bsupr (f u) (‹monotone f› h), weirdly doesn't work
+    exact ‹monotone f› h,
+    exact le_refl _,
+  end⟩,
+  le_top := λ ⟨x, hx⟩, begin
+    apply le_bsupr_of_le,
+    exact hx.le,
+    exact le_refl _,
+  end,
+  bot := ⟨(⨅ (x : α) (hx : f x ≤ x), x), begin
+    let u := ⨅ (x : α) (hx : f x ≤ x), x,
+    have h : f u ≤ u := le_binfi (λ x hx, le_trans (‹monotone f› (binfi_le x hx)) hx),
+    refine (h.antisymm _).symm,
+    exact binfi_le (f u) (‹monotone f› h),--and this works!!!
+  end⟩,
+  bot_le := λ ⟨x, hx⟩, begin
+    refine binfi_le x _,
+    exact hx.ge,
+  end,
+  .. @complete_lattice_of_complete_semilattice_Sup {x // x = f x}
+  { le := (≤),
+    le_refl := le_refl,
+    le_trans := λ x y z, le_trans,
+    le_antisymm := λ x y, le_antisymm,
+    Sup := λ s, ⟨(⨅ (x : α) (hx : f x ≤ x) (hxs : ∀ y ∈ s, ↑y ≤ x), x), begin
+      let u := (⨅ (x : α) (hx : f x ≤ x) (hxs : ∀ y ∈ s, ↑y ≤ x), x),
+      have h : f u ≤ u,
+      refine le_binfi (λ x hx, le_infi (λ hxs, le_trans (‹monotone f› _) hx)),
+      apply binfi_le_of_le,
+      exact hx,
+      exact infi_le _ hxs,
+      refine (h.antisymm _).symm,
+      apply binfi_le_of_le (f u) _ _,
+      exact (‹monotone f› h),
+      apply infi_le,
+      rintro ⟨y, hy⟩ hys,
+      refine hy.le.trans (‹monotone f› _),
+      apply le_binfi,
+      rintro z hz,
+      apply le_infi,
+      rintro hzs,
+      exact hzs ⟨y, hy⟩ hys,
+    end⟩,
+    le_Sup := λ s x hxs, begin
+      apply le_binfi,
+      rintro y hy,
+      apply le_infi,
+      rintro hys,
+      exact hys x hxs,
+    end,
+    Sup_le := λ s ⟨x, hx⟩ hxs, begin
+      apply binfi_le_of_le,
+      exact hx.ge,
+      apply infi_le,
+      rintro y hy,
+      exact hxs y hy,
+    end }}
+
 lemma Inf_apply {α : Type*} {β : α → Type*} [Π i, has_Inf (β i)]
   {s : set (Πa, β a)} {a : α} :
   (Inf s) a = (⨅ f : s, (f : Πa, β a) a) :=

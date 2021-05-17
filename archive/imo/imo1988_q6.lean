d9dd252582d30af7f4ef846e9728e1a21e71b8e4
Ruben Van de Velde <65514131+Ruben-VandeVelde@users.noreply.github.com>
2021-05-17 13:52:36 +0200

    Apply suggestions from code review
    Co-authored-by: Benjamin Davidson <68528197+benjamindavidson@users.noreply.github.com>

--- archive/imo/imo1988_q6.lean
+++ archive/imo/imo1988_q6.lean
@@ -291,8 +291,6 @@ begin
     { simp only [mul_one, one_mul, add_comm, zero_add] at h,
       have y_dvd : y ∣ y * k := dvd_mul_right y k,
       rw [← h, ← add_assoc, nat.dvd_add_left (dvd_mul_left y y)] at y_dvd,
-      obtain rfl|rfl : y = 1 ∨ y = 2 := (nat.dvd_prime nat.prime_two).mp y_dvd;
-        norm_num at h;
-        [skip, apply nat.eq_of_mul_eq_mul_left zero_lt_two];
-        exact h.symm } }
+      obtain rfl|rfl := (nat.dvd_prime nat.prime_two).mp y_dvd; apply nat.eq_of_mul_eq_mul_left, 
+      exacts [zero_lt_one, h.symm, zero_lt_two, h.symm]
 end

fb9cb4a97966d9ff72900b99aac5ec61ed04829b
Riccardo Brasca <riccardo.brasca@gmail.com>
2021-05-19 12:13:41 +0200

    Update counterexamples/cyclotomic_105.lean
    Co-authored-by: Eric Wieser <wieser.eric@gmail.com>

--- counterexamples/cyclotomic_105.lean
+++ counterexamples/cyclotomic_105.lean
@@ -91,7 +91,7 @@ end
 lemma cyclotomic_105 : cyclotomic 105 ℤ =
   1 + X + X ^ 2 - X ^ 5 - X ^ 6 - 2 * X ^ 7 - X ^ 8 - X ^ 9 + X ^ 12 + X ^ 13 + X ^ 14 + X ^ 15
   + X ^ 16 + X ^ 17 - X ^ 20 - X ^ 22 - X ^ 24 - X ^ 26 - X ^ 28 + X ^ 31 + X ^ 32 + X ^ 33 +
-  X ^ 34 + X ^ 35 + X ^ 36 - X ^ 39 - X ^ 40 - 2 • X ^ 41 - X ^ 42 - X ^ 43 + X ^ 46 + X ^ 47 +
+  X ^ 34 + X ^ 35 + X ^ 36 - X ^ 39 - X ^ 40 - 2 * X ^ 41 - X ^ 42 - X ^ 43 + X ^ 46 + X ^ 47 +
   X ^ 48 :=
 begin
   refine ((eq_cyclotomic_iff (show 0 < 105, by norm_num) _).2 _).symm,

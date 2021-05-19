f91ba28b8169e668a6aa3b449912421f4fdfdf0e
Bhavik Mehta <bhavikmehta8@gmail.com>
2021-05-19 16:11:20 +0100

    finish proofs

--- src/category_theory/limits/preserves/shapes/terminal.lean
+++ src/category_theory/limits/preserves/shapes/terminal.lean
@@ -29,24 +29,42 @@ variables (G : C ⥤ D)
 
 namespace category_theory.limits
 
+/-- `G` preserves terminal objects if it preserves limits of every empty diagram. -/
 abbreviation preserves_terminal := preserves_limits_of_shape (discrete pempty) G
+/-- `G` reflects terminal objects if it reflects limits of every empty diagram. -/
 abbreviation reflects_terminal := reflects_limits_of_shape (discrete pempty) G
+/-- `G` creates terminal objects if it creates limits of every empty diagram. -/
 abbreviation creates_terminal := creates_limits_of_shape (discrete pempty) G
 
-lemma preserves_terminal_of_preserves_limit_empty [preserves_limit (functor.empty C) G] :
+def preserves_terminal_of_preserves_limit_empty [preserves_limit (functor.empty C) G] :
   preserves_terminal G :=
 { preserves_limit := λ K, preserves_limit_of_iso_diagram _ K.unique_from_empty.symm }
 
-lemma reflects_terminal_of_reflects_limit_empty [reflects_limit (functor.empty C) G] :
+def reflects_terminal_of_reflects_limit_empty [reflects_limit (functor.empty C) G] :
   reflects_terminal G :=
 { reflects_limit := λ K, reflects_limit_of_iso_diagram _ K.unique_from_empty.symm }
 
--- { has_limit := λ F, has_limit_of_iso (diagram_iso_parallel_pair F).symm }
+def creates_terminal_of_creates_limit_empty [creates_limit (functor.empty C) G] :
+  creates_terminal G :=
+{ creates_limit := λ K, creates_limit_of_iso_diagram _ K.unique_from_empty.symm }
 
 abbreviation preserves_initial := preserves_colimits_of_shape (discrete pempty) G
 abbreviation reflects_initial := reflects_colimits_of_shape (discrete pempty) G
 abbreviation creates_initial := creates_colimits_of_shape (discrete pempty) G
 
+def preserves_initial_of_preserves_colimit_empty [preserves_colimit (functor.empty C) G] :
+  preserves_initial G :=
+{ preserves_colimit := λ K, preserves_colimit_of_iso_diagram _ K.unique_from_empty.symm }
+
+def reflects_initial_of_reflects_colimit_empty [reflects_colimit (functor.empty C) G] :
+  reflects_initial G :=
+{ reflects_colimit := λ K, reflects_colimit_of_iso_diagram _ K.unique_from_empty.symm }
+
+def creates_initial_of_creates_colimit_empty [creates_colimit (functor.empty C) G] :
+  creates_initial G :=
+{ creates_colimit := λ K, creates_colimit_of_iso_diagram _ K.unique_from_empty.symm }
+
+#lint
 
 variables (X : C)
 
@@ -133,22 +151,5 @@ begin
   rw ← preserves_terminal.iso_hom,
   apply_instance,
 end
-
-
--- /-- `has_equalizers` represents a choice of equalizer for every pair of morphisms -/
--- abbreviation has_equalizers := has_limits_of_shape walking_parallel_pair C
-
--- /-- `has_coequalizers` represents a choice of coequalizer for every pair of morphisms -/
--- abbreviation has_coequalizers := has_colimits_of_shape walking_parallel_pair C
-
--- /-- If `C` has all limits of diagrams `parallel_pair f g`, then it has all equalizers -/
--- lemma has_equalizers_of_has_limit_parallel_pair
---   [Π {X Y : C} {f g : X ⟶ Y}, has_limit (parallel_pair f g)] : has_equalizers C :=
--- { has_limit := λ F, has_limit_of_iso (diagram_iso_parallel_pair F).symm }
-
--- /-- If `C` has all colimits of diagrams `parallel_pair f g`, then it has all coequalizers -/
--- lemma has_coequalizers_of_has_colimit_parallel_pair
---   [Π {X Y : C} {f g : X ⟶ Y}, has_colimit (parallel_pair f g)] : has_coequalizers C :=
--- { has_colimit := λ F, has_colimit_of_iso (diagram_iso_parallel_pair F) }
 
 end category_theory.limits

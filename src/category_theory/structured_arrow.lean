57e1500a67601d383de014b9d85b692bed671685
Bhavik Mehta <bhavikmehta8@gmail.com>
2021-05-19 03:23:52 +0100

    unabbreviate more

--- src/category_theory/structured_arrow.lean
+++ src/category_theory/structured_arrow.lean
@@ -34,7 +34,7 @@ def structured_arrow (S : D) (T : C ⥤ D) := comma (functor.from_punit S) T
 namespace structured_arrow
 
 /-- The obvious projection functor from structured arrows. -/
-abbreviation proj (S : D) (T : C ⥤ D) : structured_arrow S T ⥤ C := comma.snd _ _
+def proj (S : D) (T : C ⥤ D) : structured_arrow S T ⥤ C := comma.snd _ _
 
 variables {S S' S'' : D} {Y Y' : C} {T : C ⥤ D}
 
@@ -125,7 +125,7 @@ def costructured_arrow (S : C ⥤ D) (T : D) := comma S (functor.from_punit T)
 namespace costructured_arrow
 
 /-- The obvious projection functor from costructured arrows. -/
-abbreviation proj (S : C ⥤ D) (T : D) : costructured_arrow S T ⥤ C := comma.fst _ _
+def proj (S : C ⥤ D) (T : D) : costructured_arrow S T ⥤ C := comma.fst _ _
 
 variables {T T' T'' : D} {Y Y' : C} {S : C ⥤ D}
 

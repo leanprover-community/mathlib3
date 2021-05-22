44244943e2a9c3b3d4a1b30d7636d5aa48514979
YaelDillies <yael.dillies@gmail.com>
2021-05-22 21:33:36 +0100

    some more

--- src/combinatorics/szemeredi/hyperregularity.lean
+++ src/combinatorics/szemeredi/hyperregularity.lean
@@ -17,3 +17,9 @@ def is_uniform_hypergraph (G : finset V → Prop) (k : ℕ) : Prop :=
 def is_partite (G : finset V → Prop) (k : ℕ) : Prop :=
 ∃ (ι : Type*) (X : ι → finset (finset V)), ∀ x₁ x₂, G x₁ → G x₂ →
   ∀ i₁ i₂, x₁ ∈ X i₁ → x₂ ∈ X i₂ → i₁ = i₂
+
+--def is_partite_function
+
+--def is_quasi_random :
+
+--lemma szemeredi_regularity {G : finset V → Prop} (hG : is_partite G 2) {ε : ℝ} (ε_pos : 0 < ε) :s
--- src/combinatorics/szemeredi/regularity.lean
+++ src/combinatorics/szemeredi/regularity.lean
@@ -1,53 +1,52 @@
 import data.finset.basic
 import data.real.basic
 
-variables {V : Type*} {G : finset V → Prop} {k n r : ℕ}
-
-structure partite_hypergraph (r : ℕ) :=
-(G : finset V → Prop)
-(ι : Type*)
-(ι_fin : fintype ι)
-(ι_card : fintype.card ι = r)
-(X : ι → finset (finset V))
-(partite : ∀ x₁ x₂, G x₁ → G x₂ → ∀ i₁ i₂, x₁ ∈ X i₁ → x₂ ∈ X i₂ → i₁ = i₂)
-
-def is_uniform_hypergraph (G : finset V → Prop) (k : ℕ) : Prop :=
-∀ x : finset V, G x → x.card = k
-
-def is_partite (G : finset V → Prop) (k : ℕ) : Prop :=
-∃ (ι : Type*) (X : ι → finset (finset V)), ∀ x₁ x₂, G x₁ → G x₂ →
-  ∀ i₁ i₂, x₁ ∈ X i₁ → x₂ ∈ X i₂ → i₁ = i₂
+open_locale classical
+variables {V : Type*} {k n r : ℕ}
 
 def is_down_closed (G : finset V → Prop) : Prop :=
 ∀ x y : finset V, x ≤ y → G y → G x
---def is_partite_function
-
---def is_quasi_random :
-
---lemma szemeredi_regularity {G : finset V → Prop} (hG : is_partite G 2) {ε : ℝ} (ε_pos : 0 < ε) :
 
 structure hypergraph (V : Type*) :=
 (edges : set (finset V))
 
-variable (G : hypergraph V)
+namespace hypergraph
+variables (G : hypergraph V) {A B A' B' : finset V}
 
-def edges_pair (A B : finset V) : finset (V × V) :=
+noncomputable def edges_pair (A B : finset V) : finset (V × V) :=
 (finset.product A B).filter (λ e, e.1 ≠ e.2 ∧ {e.1, e.2} ∈ G.edges)
 
-def density_pair (A B : finset V) : ℚ := (edges_pair A B : ℚ)/(A.card * B.card)
-
-def density_diff (A B A' B' : finset V) : ℚ := abs (density_pair A B - density_pair A' B')
-
-def uniform_pair (A B : finset V) (ε : ℝ): ∀ A' ⊆ A, ε * A.card ≤ A'.card → ∀ B' ⊆ B,
-  ε * B.card ≤ B'.card → density_diff A B A' B' < ε
-
-def uniform_partition (P : finset (finset V))
-
-variables {A B A' B' : finset V}
-
-lemma LemmaB {δ : ℝ} (hAA' : A' ⊆ A) (hA' : ε * A.card ≤ A'.card) (hAnemp : A.nonempty)
-  (hBB' : B' ⊆ B) (hB' : (1 - δ) * B.card ≤ B'.card) (hBnemp : B.nonempty) :
-  density_diff A B A' B' ≤ 2 * δ :=
+lemma edges_pair_mono_left (hA : A' ⊆ A) (B : finset V) : G.edges_pair A' B ⊆ G.edges_pair A B :=
 begin
+  rintro e he,
+  unfold edges_pair at ⊢ he,
+  rw finset.mem_filter at ⊢ he,
+  refine ⟨⟩
+  have := finset.m
+end
+
+noncomputable def density_pair (A B : finset V) : ℝ := (G.edges_pair A B).card/(A.card * B.card)
+
+noncomputable def density_diff (A B A' B' : finset V) : ℝ :=
+abs (G.density_pair A B - G.density_pair A' B')
+
+def uniform_pair (A B : finset V) (ε : ℝ) : Prop :=
+∀ A' ⊆ A, ε * A.card ≤ A'.card → ∀ B' ⊆ B, ε * B.card ≤ B'.card →
+  G.density_diff A B A' B' < ε
+
+--def uniform_partition (P : finset (finset V))
+
+end hypergraph
+
+open hypergraph
+variables {A B A' B' : finset V} (G : hypergraph V)
+
+lemma LemmaB {δ : ℝ} (hAA' : A' ⊆ A) (hA' : (1 - δ) * A.card ≤ A'.card) (hAnemp : A.nonempty)
+  (hBB' : B' ⊆ B) (hB' : (1 - δ) * B.card ≤ B'.card) (hBnemp : B.nonempty) :
+  (G.density_diff A B A' B' : ℝ) ≤ 2 * δ :=
+begin
+  unfold density_diff density_pair,
+  rw abs_le,
+  simp,
   sorry
 end

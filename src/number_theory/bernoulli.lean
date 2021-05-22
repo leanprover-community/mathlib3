feba6e27955f5a43843bc6365e545508a91b1557
Floris van Doorn <fpvdoorn@gmail.com>
2021-05-22 16:30:40 -0400

    change name

--- src/number_theory/bernoulli.lean
+++ src/number_theory/bernoulli.lean
@@ -321,7 +321,7 @@ end
 /-- Alternate form of Faulhaber's theorem, relating the sum of p-th powers to the Bernoulli numbers:
 $$\sum_{k=1}^{n} k^p = \sum_{i=0}^p (-1)^iB_i\binom{p+1}{i}\frac{n^{p+1-i}}{p+1}.$$
 Deduced from `sum_range_pow`. -/
-theorem sum_range_pow' (n p : ℕ) :
+theorem sum_Ico_pow (n p : ℕ) :
   ∑ k in Ico 1 (n + 1), (k : ℚ) ^ p =
     ∑ i in range (p + 1), bernoulli' i * (p + 1).choose i * n ^ (p + 1 - i) / (p + 1) :=
 begin

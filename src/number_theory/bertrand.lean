f1ec0b22a1b9daa728e15981138b47a70f6e68e5
Patrick Stevens <patrick+github@patrickstevens.co.uk>
2021-05-23 18:19:05 +0000

    Add licence to top of file

--- src/number_theory/bertrand.lean
+++ src/number_theory/bertrand.lean
@@ -1,3 +1,9 @@
+/-
+Copyright (c) 2020 Patrick Stevens. All rights reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
+Authors: Patrick Stevens, Bolton Bailey
+-/
+
 import data.nat.prime
 import data.finset.intervals
 import data.nat.multiplicity

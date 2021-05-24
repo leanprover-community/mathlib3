803403a13deaf842be376c83704c201424adc979
Jon Eugster <j.eugster@sms.ed.ac.uk>
2021-05-24 20:41:18 +0100

    renamed file.

--- src/algebra/char_p/default.lean
+++ src/algebra/char_p/default.lean
@@ -1,5 +1,5 @@
+import algebra.char_p.algebra
 import algebra.char_p.basic
-import algebra.char_p.localization
 import algebra.char_p.pi
 import algebra.char_p.quotient
 import algebra.char_p.subring

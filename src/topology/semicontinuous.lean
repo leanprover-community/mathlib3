a86f960f644dad8b8585520adcecd2c3635aa89d
sgouezel <sebastien.gouezel@univ-rennes1.fr>
2021-05-25 07:50:24 +0200

    header

--- src/topology/semicontinuous.lean
+++ src/topology/semicontinuous.lean
@@ -1,5 +1,5 @@
 /-
-Copyright (c) 2019 Sébastien Gouëzel. All rights reserved.
+Copyright (c) 2021 Sébastien Gouëzel. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Sébastien Gouëzel
 -/

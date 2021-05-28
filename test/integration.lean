f39773eb4663a9c84623b233ef5cc54d619f57a1
Benjamin Davidson <68528197+benjamindavidson@users.noreply.github.com>
2021-05-27 23:41:15 -0400

    integral_log

--- test/integration.lean
+++ test/integration.lean
@@ -37,6 +37,9 @@ example : ∫ x in 0..π, cos x ^ 2 - sin x ^ 2 = 0 := by simp [integral_cos_sq_
 /- the exponential function -/
 example : ∫ x in 0..2, -exp x = 1 - exp 2 := by simp
 
+/- the logarithmic function -/
+example : ∫ x in 1..2, log x = 2 * log 2 - 1 := by norm_num [sub_add]
+
 /- linear combinations (e.g. polynomials) -/
 example : ∫ x : ℝ in 0..2, 6*x^5 + 3*x^4 + x^3 - 2*x^2 + x - 7 = 1048 / 15 := by norm_num
 example : ∫ x : ℝ in 0..1, exp x + 9 * x^8 + x^3 - x/2 + (1 + x^2)⁻¹ = exp 1 + π / 4 := by norm_num

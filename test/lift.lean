f849d53c3b07ceb0ec01ab7a78bc453f2e823828
Floris van Doorn <fpvdoorn@gmail.com>
2021-05-28 13:33:20 -0400

    feat(tactic/lift): elaborate proof with the expected type
    also slightly refactor the corresponding function a bit

--- test/lift.lean
+++ test/lift.lean
@@ -17,3 +17,19 @@ by { lift x to {x // P x} using hx with y, trivial }
 /-! Test that `lift` elaborates `s` as a type, not as a set. -/
 example {R : Type*} {s : set R} (x : R) (hx : x ∈ s) : true :=
 by { lift x to s using hx with y, trivial }
+
+example (n : ℤ) (hn : 0 ≤ n) : true :=
+by { lift n to ℕ, trivial, exact hn }
+
+example (n : ℤ) (hn : 0 ≤ n) : true :=
+by { lift n to ℕ using hn, trivial }
+
+example (n : ℤ) (hn : n ≥ 0) : true :=
+by { lift n to ℕ using ge.le _, trivial, guard_target (n ≥ 0), exact hn }
+
+example (n : ℤ) (hn : 0 ≤ 1 * n) : true :=
+begin
+  lift n to ℕ using by { simpa [int.one_mul] using hn } with k, -- the braces are optional
+  guard_hyp hn : 0 ≤ 1 * ((k : ℕ) : ℤ),
+  trivial
+end

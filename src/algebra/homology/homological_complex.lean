9690f20e54032ca581a56c1e87f3cc267ab05146
Scott Morrison <scott.morrison@gmail.com>
2021-05-29 12:34:58 +1000

    feat(algebra/homology): construct isomorphisms of complexes

--- src/algebra/homology/homological_complex.lean
+++ src/algebra/homology/homological_complex.lean
@@ -499,6 +499,31 @@ arrow.hom_mk (f.comm_to j)
 @[simp] lemma sq_to_left (f : hom C₁ C₂) (j : ι) : (f.sq_to j).left = f.prev j := rfl
 @[simp] lemma sq_to_right (f : hom C₁ C₂) (j : ι) : (f.sq_to j).right = f.f j := rfl
 
+/-- The `i`-th component of an isomorphism of chain complexes. -/
+@[simps]
+def iso_app (f : C₁ ≅ C₂) (i : ι) : C₁.X i ≅ C₂.X i :=
+{ hom := f.hom.f i,
+  inv := f.inv.f i,
+  hom_inv_id' := by { erw [← comp_f, f.hom_inv_id, id_f] },
+  inv_hom_id' := by { erw [← comp_f, f.inv_hom_id, id_f] } }
+
+/-- Construct an isomorphism of chain complexes from isomorphism of the objects
+which commute with the differentials. -/
+@[simps]
+def iso_of_components (f : Π i, C₁.X i ≅ C₂.X i)
+  (hf : ∀ i j, c.rel i j → (f i).hom ≫ C₂.d i j = C₁.d i j ≫ (f j).hom) :
+  C₁ ≅ C₂ :=
+{ hom := { f := λ i, (f i).hom, comm' := hf },
+  inv :=
+  { f := λ i, (f i).inv,
+    comm' := λ i j hij,
+    calc (f i).inv ≫ C₁.d i j
+        = (f i).inv ≫ (C₁.d i j ≫ (f j).hom) ≫ (f j).inv : by simp
+    ... = (f i).inv ≫ ((f i).hom ≫ C₂.d i j) ≫ (f j).inv : by rw hf i j hij
+    ... =  C₂.d i j ≫ (f j).inv : by simp },
+  hom_inv_id' := by { ext i, exact (f i).hom_inv_id },
+  inv_hom_id' := by { ext i, exact (f i).inv_hom_id } }
+
 end hom
 
 end homological_complex

6982cf4bdf7eb784e3c3e191ac31b9c1d268f296
RemyDegenne <Remydegenne@gmail.com>
2021-05-30 12:35:44 +0200

    add copyright to test/measurability.lean

--- test/measurability.lean
+++ test/measurability.lean
@@ -1,3 +1,8 @@
+/-
+Copyright (c) 2021 Rémy Degenne. All rights reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
+Authors: Rémy Degenne
+-/
 import measure_theory.tactic
 import measure_theory.borel_space
 

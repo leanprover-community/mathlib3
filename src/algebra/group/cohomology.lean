ce56c4f6fa5913563abfc09b85a06e4962846e0e
Kevin Buzzard <k.buzzard@imperial.ac.uk>
2021-05-30 04:18:54 +0100

    proof that d^2=0

--- src/algebra/group/cohomology.lean
+++ src/algebra/group/cohomology.lean
@@ -4,6 +4,7 @@ Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Kevin Buzzard
 -/
 import algebra.group_action_hom
+import data.fin_simplicial_complex
 
 /-!
 # Group cohomology
@@ -63,11 +64,16 @@ instance : has_coe_to_fun (cochain_succ G M n) :=
 { F := _,
   coe := to_fun }
 
+@[simp] lemma seems_useful (c : (fin n → G) → M) (hc : ∀ (s : G) (g : fin n → G), s • c g = c (λ i, s * g i))
+  (g : fin n → G) : (⟨c, hc⟩ : cochain_succ G M n) g = c g := rfl
+
+@[simp] lemma also_seems_useful (c : cochain_succ G M n) (g : fin n → G) : c.to_fun g = c g := rfl
+
 @[ext] theorem ext' (c₁ c₂ : cochain_succ G M n) (h : ∀ g : fin n → G, c₁ g = c₂ g) : c₁ = c₂ :=
 ext c₁ c₂ (funext (λ (x : fin n → G), h x))
 
 def zero : cochain_succ G M n :=
-{ to_fun := λ _, 0,
+{ to_fun := 0,
   smul_apply' := λ s g, smul_zero s }
 
 instance : has_zero (cochain_succ G M n) := ⟨zero G M n⟩
@@ -96,6 +102,11 @@ instance : has_add (cochain_succ G M n) := ⟨add⟩
 @[simp] lemma add_apply (c₁ c₂ : cochain_succ G M n) (g : fin n → G) : (c₁ + c₂) g = c₁ g + c₂ g :=
 rfl
 
+
+--lemma gsmul_univ {A : Type uA} [add_group A] (F : ℤ → A → A) (F_zero : ∀ a, F 0 a = 0)
+--  (F_succ : ∀ (n : ℕ) (a : A), F (n.succ : ℕ) a = a + F n a)
+--  (F_neg : ∀ (n : ℕ) (a : A), F -[1+ n] a = -F (n.succ : ℕ) a) : F = gsmul := sorry
+
 instance : add_comm_group.add_comm_group_aux (cochain_succ G M n) :=
 { add_assoc := by { intros, ext, simp [add_assoc] },
   zero_add := by {intros, ext, simp },
@@ -103,14 +114,123 @@ instance : add_comm_group.add_comm_group_aux (cochain_succ G M n) :=
   add_comm := by {intros, ext, simp [add_comm] },
 }
 
-end cochain_succ
+@[simp] lemma sub_apply (c₁ c₂ : cochain_succ G M n) (g : fin n → G) :
+  (c₁ - c₂) g = c₁ g - c₂ g :=
+begin
+  simp [sub_eq_add_neg],
+end
 
-def d {i j : ℕ} (hij : j = i + 1) : cochain_succ G M i →+ cochain_succ G M j :=
+lemma pred_smul {A : Type uA} [add_group A] (a : A) (n : ℤ) :
+  (n - 1) • a = n • a - a :=
+int.induction_on n
+  (by simp)
+  (λ _ _, by simp [add_gsmul, one_gsmul])
+  (λ _, by simp [sub_gsmul])
+
+lemma smul_gsmul {G : Type uG} [group G] {M : Type uM} [add_comm_group M] [distrib_mul_action G M]
+  (g : G) (n : ℤ) (m : M) : g • n • m = n • g • m :=
+int.induction_on n
+  ( by simp)
+  ( λ i h, by { simp only [add_smul, smul_add, add_left_inj, one_gsmul, h] })
+  ( λ i h, by { simp only [pred_smul, smul_sub, smul_neg, neg_inj, sub_left_inj, h] } )
+
+
+@[simp] lemma int_smul_apply (c : cochain_succ G M n) (z : ℤ) (g : fin n → G) :
+  (z • c) g = z • (c g) :=
+begin
+--  cases c with c hc,
+  apply int.induction_on z,
+  { simp },
+  { intros i this,
+    simpa [add_gsmul] },
+  { intros i this,
+    rw [pred_smul, pred_smul, sub_apply, this] },
+end
+
+
+def d {i j : ℕ} (hj : j = i + 1) : cochain_succ G M i →+ cochain_succ G M j :=
 { to_fun := λ c,
-  { to_fun := λ g, (finset.range j).sum (λ k, if hk : k < j then (-1 : ℤ)^k • c sorry else 0),
-    smul_apply' := sorry },
-  map_zero' := sorry,
-  map_add' := sorry }
+  { to_fun := λ g, (finset.range j).sum (λ p, (-1 : ℤ)^p • c $ λ t, g (fin.delta hj p t)),
+    smul_apply' := λ s g, begin
+      rw [finset.smul_sum],
+      simp,
+      congr,
+      ext,
+      rw ← c.smul_apply,
+      generalize : (c : (fin i → G) → M) _ = C,
+      simp [],
+      rw smul_gsmul,
+    end },
+  map_zero' := begin ext, simp end,
+  map_add' := λ x y, by {ext, simp [finset.sum_add_distrib]} }
+
+lemma d_eval {i j : ℕ} (hj : j = i + 1) (c : cochain_succ G M i) (g : fin j → G) :
+  d hj c g = (finset.range j).sum (λ p, (-1 : ℤ)^p • c $ λ t, g $ fin.delta hj p t) := rfl
+
+theorem d_squared {i j k : ℕ} (hj : j = i + 1) (hk : k = j + 1) (c : cochain_succ G M i) :
+  (d hk (d hj c)) = 0 :=
+begin
+  ext g, change _ = (0 : M),
+  simp only [d_eval],
+  -- how do I avoid `conv` if I want to apply `d_eval` again?
+  conv begin
+    congr,
+    congr, skip,
+    funext,
+    rw int_smul_apply,
+    rw d_eval,
+  end,
+  simp_rw finset.smul_sum,
+  rw ← finset.sum_product',
+  apply finset.sum_involution (λ (pq : ℕ × ℕ) (hpq),
+    if pq.fst ≤ pq.2 then (pq.2.succ, pq.1) else (pq.2, pq.1.pred)),
+  { intros,
+    simp,
+    split_ifs,
+    { simp [fin.delta_comm_apply hj hk h, pow_succ, smul_smul, mul_comm ((-1 : ℤ) ^ a.fst)] },
+    { -- kill the pred.
+      cases a with p q,
+      -- pred 0 can't happen
+      cases p, { push_neg at h, cases h },
+      -- rewrite now succeeds
+      simp [nat.pred_succ, pow_succ],
+      push_neg at h,
+      have hqp : q ≤ p := nat.lt_succ_iff.mp h,
+      have := fin.delta_comm_apply.symm hj hk hqp,
+      simp_rw this,
+      simp [smul_comm ((-1 : ℤ) ^ p)] } },
+  { rintros ⟨p, q⟩ h _ hfalse,
+    rw prod.ext_iff at hfalse,
+    rcases hfalse with ⟨h1, h2⟩,
+    dsimp at *,
+    split_ifs at *,
+    { subst h1,revert h_1,
+      apply nat.not_succ_le_self },
+    { exact h_1 (h1 ▸ le_refl _) } },
+  { rintro ⟨p, q⟩ hpqrange,
+    simp [nat.succ_eq_add_one],
+    split_ifs,
+      exfalso, linarith,
+      refl,
+      cases p, {exfalso, exact h (zero_le _)}, refl,
+      exfalso, cases p, exact h (zero_le _), rw nat.pred_succ at h_1,
+        rw nat.succ_eq_add_one at h,linarith },
+  { rintros ⟨p, q⟩ hpqbounds,
+    rw finset.mem_product at hpqbounds,
+    rcases hpqbounds with ⟨hpk : p ∈ _, hqj : q ∈ _⟩,
+    rw finset.mem_range at hpk hqj,
+    simp,
+    split_ifs,
+    { rw nat.succ_eq_add_one,
+      split; linarith },
+    { push_neg at h,
+      cases p, cases h,
+      rw nat.pred_succ,
+      rw nat.succ_eq_add_one at *,
+      split; linarith } },
+end
+
+end cochain_succ
 
 --def H (n : ℕ) (G : Type uG) [group G] (M : Type uM) [add_comm_group M]
 --  [distrib_mul_action G M] := sorry
---
+++ src/data/fin_simplicial_complex.lean
@@ -0,0 +1,43 @@
+-- Do we have this stuff?
+import data.fin -- yes all 1800 lines of it
+import tactic
+
+namespace fin
+
+/-- The function dsending enoted `δᵖ` by Riehl-Verity, sending`t` to `t` if `t<p` and
+  `t.succ` otherwise.`-/
+def delta {i j : ℕ} (hj : j = i + 1) (p : ℕ) (t : fin i) : fin j :=
+-- fin.succ_above expects p : fin i.succ for some reason
+⟨if (t : ℕ) < p then t else (t : ℕ).succ, begin
+  subst hj,
+  cases t with t ht,
+  split_ifs,
+  { exact ht.trans (nat.lt_succ_self _) },
+  { exact nat.succ_lt_succ ht },
+end⟩
+
+lemma delta_eval {i j : ℕ} (hj : j = i + 1) (p : ℕ) (t : fin i) :
+  (delta hj p t : ℕ) = if (t : ℕ) < p then t else (t : ℕ).succ := rfl
+
+theorem delta_comm_apply  {i j k : ℕ} (hj : j = i.succ) (hk : k = j.succ) {p q : ℕ}
+  (hpq : p ≤ q) (t : fin i) : delta hk q.succ (delta hj p t) = delta hk p (delta hj q t) :=
+begin
+  subst hk,
+  subst hj,
+  ext,
+  cases t with t ht,
+  simp only [delta_eval],
+  dsimp,
+  split_ifs;
+  try {rw nat.succ_eq_add_one at *};
+  try {push_neg at *};
+  try {rw nat.succ_eq_add_one at *};
+  try {linarith},
+end
+
+theorem delta_comm_apply.symm {i j k : ℕ} (hj : j = i.succ) (hk : k = j.succ) {p q_succ : ℕ}
+  (hpq : q_succ ≤ p) (t : fin i) :
+  delta hk p.succ (delta hj q_succ t) = delta hk q_succ (delta hj p t) :=
+delta_comm_apply hj hk hpq t
+
+end fin

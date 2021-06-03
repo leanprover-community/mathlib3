5b03c9181459c729f428e673329b98a42c3927b0
Eric <ericrboidi@gmail.com>
2021-06-03 22:55:49 +0100

    fixup card_embedding

--- src/data/fintype/card_embedding.lean
+++ src/data/fintype/card_embedding.lean
@@ -27,17 +27,18 @@ lemma card_embedding_of_unique
 ‖α ↪ β‖ = ‖β‖ := card_congr equiv.unique_embedding_equiv_result
 
 private lemma card_embedding_aux (n : ℕ) (β) [fintype β] [decidable_eq β] (h : n ≤ ‖β‖) :
-  ‖fin n ↪ β‖ = nat.asc_fact (‖β‖ - n) n :=
+  ‖fin n ↪ β‖ = (nat.desc_fact ‖β‖ n) :=
 begin
   induction n with n hn,
   { nontriviality (fin 0 ↪ β),
-    rw [nat.asc_fact_zero, fintype.card_eq_one_iff],
+    rw [nat.desc_fact_zero, fintype.card_eq_one_iff],
     refine ⟨nonempty.some nontrivial.to_nonempty, λ x, function.embedding.ext fin.elim0⟩ },
 
   rw [nat.succ_eq_add_one, ←card_congr (equiv.embedding_congr fin_sum_fin_equiv (equiv.refl β))],
   rw card_congr equiv.sum_embedding_equiv_sigma_embedding_restricted,
   -- these `rw`s create goals for instances, which it doesn't infer for some reason
-  all_goals { try { apply_instance } }, -- however, this needs to be done here instead of at the end
+  all_goals { try { apply_instance } },
+  -- however, this needs to be done here instead of at the end
   -- else, a later `simp`, which depends on the `fintype` instance, won't work.
 
   have : ∀ (f : fin n ↪ β), ‖fin 1 ↪ ↥((set.range f)ᶜ)‖ = ‖β‖ - n,
@@ -51,33 +52,29 @@ begin
   rw card_sigma,
   simp only [this, finset.sum_const, finset.card_univ, nsmul_eq_mul, nat.cast_id],
 
-  replace h := nat.lt_of_succ_le h,
-  rw [hn h.le, mul_comm, nat.asc_fact_of_sub h]
+  replace h := (nat.lt_of_succ_le h).le,
+  rw [nat.desc_fact_succ, hn h, mul_comm]
 end
 
-variables {α β : Type*} [fintype α] [fintype β] [decidable_eq α] [decidable_eq β]
+variables
 
-/- Establishes the cardinality of the type of all injections, if any exist.  -/
-@[simp] theorem card_embedding (h : ‖α‖ ≤ ‖β‖) : ‖α ↪ β‖ = (nat.asc_fact (‖β‖ - ‖α‖) ‖α‖) :=
+/- Establishes the cardinality of the type of all injections between two finite types.  -/
+@[simp] theorem card_embedding {α β} [fintype α] [fintype β][decidable_eq α] [decidable_eq β] :
+‖α ↪ β‖ = (nat.desc_fact ‖β‖ ‖α‖) :=
 begin
-  trunc_cases fintype.trunc_equiv_fin α with eq,
-  rw fintype.card_congr (equiv.embedding_congr eq (equiv.refl β)),
-  exact card_embedding_aux _ _ h,
+  by_cases h : ‖α‖ ≤ ‖β‖,
+  { trunc_cases fintype.trunc_equiv_fin α with eq,
+    rw fintype.card_congr (equiv.embedding_congr eq (equiv.refl β)),
+    exact card_embedding_aux _ _ h },
+  { push_neg at h,
+    rw card_eq_zero_iff.mpr (function.embedding.is_empty_of_card_lt h),
+    symmetry,
+    exact nat.desc_fact_eq_zero_iff_lt.mpr h }
 end
 
-/-- If `‖β‖ < ‖α‖` there are no embeddings `α ↪ β`.
-This is a formulation of the pigeonhole principle. -/
-@[simp] theorem card_embedding_eq_zero (h : ‖β‖ < ‖α‖) : ‖α ↪ β‖ = 0 :=
-card_eq_zero_iff.mpr $ function.embedding.is_empty_of_card_lt h
-
-theorem card_embedding_eq_if : ‖α ↪ β‖ = if ‖α‖ ≤ ‖β‖ then nat.asc_fact (‖β‖ - ‖α‖) ‖α‖ else 0 :=
-begin
-  split_ifs with h,
-  { exact card_embedding h },
-  { exact card_embedding_eq_zero (not_le.mp h) }
-end
-
-lemma card_embedding_eq_infinite {α β} [infinite α] [fintype β] : ‖α ↪ β‖ = 0 :=
-by rw card_eq_zero_iff; apply_instance
+/- The cardinality of embeddings from an infinite type to a finite type is zero.
+This is a re-statement of the pigeonhole principle. -/
+@[simp] lemma card_embedding_eq_infinite {α β} [infinite α] [fintype β] : ‖α ↪ β‖ = 0 :=
+card_eq_zero_iff.mpr function.embedding.is_empty
 
 end fintype

6a00169e0a251091c8d0df7f656b385d8a9df5a4
Anne Baanen <Vierkantor@users.noreply.github.com>
2021-06-04 09:04:09 +0000

    Change variables to reuse assumptions
    Co-authored-by: Eric Wieser <wieser.eric@gmail.com>

--- src/linear_algebra/basis.lean
+++ src/linear_algebra/basis.lean
@@ -398,7 +398,7 @@ by rw [b.constr_def S f, linear_map.range_comp, linear_map.range_comp, linear_eq
        ← finsupp.span_eq_map_total, set.image_id]
 
 @[simp]
-lemma constr_comp [module S M] [smul_comm_class R S M] (f : M →ₗ[R] M) (v : ι → M) :
+lemma constr_comp (f : M' →ₗ[R] M') (v : ι → M') :
   b.constr S (f ∘ v) = f.comp (b.constr S v) :=
 b.ext (λ i, by simp only [basis.constr_basis, linear_map.comp_apply])
 

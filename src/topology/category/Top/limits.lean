535fe64c2a0f09dcbb072721cf9eceeaa2d0c203
Kyle Miller <kmill31415@gmail.com>
2021-06-05 17:18:26 -0700

    moved import back

--- src/topology/category/Top/limits.lean
+++ src/topology/category/Top/limits.lean
@@ -3,9 +3,9 @@ Copyright (c) 2017 Scott Morrison. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Patrick Massot, Scott Morrison, Mario Carneiro
 -/
+import topology.category.Top.basic
 import category_theory.limits.types
 import category_theory.limits.preserves.basic
-import topology.category.Top.basic
 
 /-!
 # The category of topological spaces has all limits and colimits

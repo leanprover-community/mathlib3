73aaec45446d3ef1ad0e70bf13a8107fb456c484
Riccardo Brasca <riccardo.brasca@gmail.com>
2021-06-07 10:47:46 +0200

    lint

--- src/linear_algebra/free_module.lean
+++ src/linear_algebra/free_module.lean
@@ -102,6 +102,7 @@ of_basis $ (choose_basis R M).prod (choose_basis R N)
 
 instance of_self : module.free R R := of_basis $ basis.singleton unit R
 
+@[priority 100]
 instance of_zero [subsingleton N] : module.free R N :=
 of_basis (basis.empty N : basis pempty R N)
 

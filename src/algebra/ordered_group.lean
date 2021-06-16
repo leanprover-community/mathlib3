c989e876c1647f99d2ab41057f59c5730623ec2e
adomani <adomani@gmail.com>
2021-06-16 10:08:58 +0200

    reintroduce alias

--- src/algebra/ordered_group.lean
+++ src/algebra/ordered_group.lean
@@ -773,6 +773,8 @@ end
   max a 1 / max a⁻¹ 1 = a :=
 by { rcases le_total a 1 with h|h; simp [h] }
 
+alias max_zero_sub_max_neg_zero_eq_self ← max_zero_sub_eq_self
+
 end variable_names
 
 section densely_ordered

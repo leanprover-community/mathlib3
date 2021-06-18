9f74ec28fa0c16f411cf21d8b6e0140c8d57d5f6
Oliver Nash <github@olivernash.org>
2021-06-18 14:55:38 +0100

    Improve module doc string, add examples

--- src/algebra/non_unital_alg_hom.lean
+++ src/algebra/non_unital_alg_hom.lean
@@ -8,11 +8,26 @@ import algebra.algebra.basic
 /-!
 # Morphisms of non-unital algebras
 
-This file defines morphisms between two types, each of which carries an addition, additive zero,
-multiplication, and scalar action. The multiplications are not assumed to be associative or
-unital, or even to be compatible with the scalar multiplications. In a typical application, the
-operations will satisfy compatibility conditions making them into algebras (albeit possibly
-non-associative and/or non-unital) but such conditions are not required to make this definition.
+This file defines morphisms between two types, each of which carries:
+ * an addition,
+ * an additive zero,
+ * a multiplication,
+ * a scalar action.
+
+The multiplications are not assumed to be associative or unital, or even to be compatible with the
+scalar actions. In a typical application, the operations will satisfy compatibility conditions
+making them into algebras (albeit possibly non-associative and/or non-unital) but such conditions
+are not required to make this definition.
+
+This notion of morphism should be useful for any category of non-unital algebras. The motivating
+application at the time it was introduced was to be able to state the adjunction property for
+magma algebras. These are non-unital, non-associative algebras obtained by applying the
+group-algebra construction except where we take a type carrying just `has_mul` instead of `group`.
+
+For a plausible future application, one could take the non-unital algebra of compactly-supported
+functions on a non-compact topological space. A proper map between a pair of such spaces
+(contravariantly) induces a morphism between their algebras of compactly-supported functions which
+will be a `non_unital_alg_hom`.
 
 ## Main definitions
 

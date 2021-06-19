76238e80a0ed2e81c5d4fec532ee463aacc963a2
adomani <adomani@gmail.com>
2021-06-19 18:58:34 +0200

    shorten PR

--- src/analysis/liouville/liouville_constant.lean
+++ src/analysis/liouville/liouville_constant.lean
@@ -18,19 +18,6 @@ noncomputable theory
 open_locale nat big_operators
 open set real finset
 
--- Should this lemma go somewhere else?
-lemma summable_inv_pow_ge {m : ℝ} {f : ℕ → ℕ} (hm : 1 < m) (fi : ∀ i, i ≤ f i) :
-  summable (λ i, 1 / m ^ f i) :=
-begin
-  refine summable_of_nonneg_of_le
-    (λ a, one_div_nonneg.mpr (pow_nonneg (zero_le_one.trans hm.le) _)) (λ a, _)
-    (summable_geometric_of_lt_1 (one_div_nonneg.mpr (zero_le_one.trans hm.le))
-      ((one_div_lt (zero_lt_one.trans hm) zero_lt_one).mpr (one_div_one.le.trans_lt hm))),
-  rw [div_pow, one_pow],
-  refine (one_div_le_one_div _ _).mpr (pow_le_pow hm.le (fi a));
-  exact pow_pos (zero_lt_one.trans hm) _
-end
-
 section m_is_real
 
 variable {m : ℝ}
@@ -63,30 +50,6 @@ $$
 $$
 -/
 def liouville_number_terms_after_k (m : ℝ) (k : ℕ) :=  ∑' i, 1 / m ^ (i + (k+1))!
-
-lemma liouville_number_terms_after_pos (hm : 1 < m) (k : ℕ) :
-  0 < liouville_number_terms_after_k m k :=
--- replace `0` with the constantly zero series `∑ i : ℕ, 0`
-calc  (0 : ℝ) = ∑' i : ℕ, 0 : tsum_zero.symm
-          ... < liouville_number_terms_after_k m k :
-  -- to show that a series with non-negative terms has strictly positive sum it suffices
-  -- to prove that
-  tsum_lt_tsum_of_nonneg
-    -- 1. the terms of the zero series are indeed non-negative
-    (λ _, rfl.le)
-    -- 2. the terms of our series are non-negative
-    (λ i, one_div_nonneg.mpr (pow_nonneg (zero_le_one.trans hm.le) _))
-    -- 3. one term of our series is strictly positive -- they all are, we use the first term
-    (one_div_pos.mpr (pow_pos (zero_lt_one.trans hm) (0 + (k + 1))!)) $
-    -- 4. our series converges -- it does since it is the tail of a converging series, though
-    -- this is not the argument here.
-    summable_inv_pow_ge hm (λ i, trans le_self_add (nat.self_le_factorial _))
-
-/--  Split the sum definining a Liouville number into the first `k` term and the rest. -/
-lemma liouville_number_eq_first_k_terms_add_rest (hm : 1 < m) (k : ℕ) :
-  liouville_number m = liouville_number_first_k_terms m k +
-  liouville_number_terms_after_k m k :=
-(sum_add_tsum_nat_add _ (summable_inv_pow_ge hm (λ i, i.self_le_factorial))).symm
 
 end liouville
 

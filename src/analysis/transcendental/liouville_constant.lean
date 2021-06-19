8718a26a9941deb780e25d39f688ce763950be4a
adomani <adomani@gmail.com>
2021-06-19 07:45:02 +0200

    Merge branch 'adomani_add_one_le_two_mul' into liouville_theoerm/lemmas

--- src/analysis/transcendental/liouville_constant.lean
+++ src/analysis/transcendental/liouville_constant.lean
@@ -257,9 +257,9 @@ begin
     unfold liouville_number_first_k_terms at h_k ⊢,
     rw [sum_range_succ, h_k, div_add_div, div_eq_div_iff, add_mul],
     { norm_cast,
-      rw [add_mul, one_mul, nat.factorial_succ, show k.succ * k! - k! = (k.succ - 1) * k!,
-        by rw [nat.mul_sub_right_distrib, one_mul], nat.succ_sub_one, nat.succ_eq_add_one, add_mul,
-          one_mul, pow_add],
+      rw [add_mul, one_mul, nat.factorial_succ,
+        show k.succ * k! - k! = (k.succ - 1) * k!, by rw [nat.mul_sub_right_distrib, one_mul],
+        nat.succ_sub_one, nat.succ_eq_add_one, add_mul, one_mul, pow_add],
       simp [mul_assoc] },
     refine mul_ne_zero_iff.mpr ⟨_, _⟩,
     all_goals { exact pow_ne_zero _ (nat.cast_ne_zero.mpr hm.ne.symm) } }

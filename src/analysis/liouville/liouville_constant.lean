07c5f8313240bc843a0924f93ee331ffdd35dea4
adomani <adomani@gmail.com>
2021-06-19 12:13:45 +0200

    move to correct folder

--- src/analysis/liouville/liouville_constant.lean
+++ src/analysis/liouville/liouville_constant.lean
@@ -3,7 +3,7 @@ Copyright (c) 2020 Jujian Zhang. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Damiano Testa, Jujian Zhang
 -/
-import data.real.liouville
+import analysis.liouville.liouville
 /-!
 # Liouville constants
 

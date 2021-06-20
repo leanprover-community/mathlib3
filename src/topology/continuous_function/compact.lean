03e919bb6ccaa653d6355e6735d07aa58ae693ae
Heather Macbeth <25316162+hrmacbeth@users.noreply.github.com>
2021-06-20 03:07:11 -0400

    long line

--- src/topology/continuous_function/compact.lean
+++ src/topology/continuous_function/compact.lean
@@ -271,7 +271,8 @@ end uniform_continuity
 end continuous_map
 
 section comp_left
-variables (X : Type*) {𝕜 β γ : Type*} [topological_space X] [compact_space X] [nondiscrete_normed_field 𝕜]
+variables (X : Type*) {𝕜 β γ : Type*} [topological_space X] [compact_space X]
+  [nondiscrete_normed_field 𝕜]
 variables [normed_group β] [normed_space 𝕜 β] [normed_group γ] [normed_space 𝕜 γ]
 
 open continuous_map

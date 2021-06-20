12afdcee52d3e1933e9ab683388cd194e0165907
Heather Macbeth <25316162+hrmacbeth@users.noreply.github.com>
2021-06-20 03:09:38 -0400

    vanity

--- src/topology/continuous_function/stone_weierstrass.lean
+++ src/topology/continuous_function/stone_weierstrass.lean
@@ -1,7 +1,7 @@
 /-
 Copyright (c) 2021 Scott Morrison. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
-Authors: Scott Morrison
+Authors: Scott Morrison, Heather Macbeth
 -/
 import topology.continuous_function.weierstrass
 import analysis.complex.basic

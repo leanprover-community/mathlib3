e046cacbed2d488c11283047f3eec0ee367ea420
Heather Macbeth <25316162+hrmacbeth@users.noreply.github.com>
2021-06-22 03:57:08 -0400

    all works!

--- src/topology/continuous_function/stone_weierstrass.lean
+++ src/topology/continuous_function/stone_weierstrass.lean
@@ -366,21 +366,35 @@ variables {X : Type*} [topological_space X]
 
 namespace continuous_map
 
-/-- A real subalgebra of `C(X, ℂ)` is `conj_invariant`, if its image under conjugation is itself.
--/
+/-- A real subalgebra of `C(X, ℂ)` is `conj_invariant`, if it contains all its conjugates. -/
 def conj_invariant_subalgebra (A : subalgebra ℝ C(X, ℂ)) : Prop :=
-A.map (conj_ae.to_alg_hom.comp_left_continuous ℝ conj_cle.continuous) = A
+A.map (conj_ae.to_alg_hom.comp_left_continuous ℝ conj_cle.continuous) ≤ A
 
 lemma mem_conj_invariant {A : subalgebra ℝ C(X, ℂ)} (hA : conj_invariant_subalgebra A)
   {f : C(X, ℂ)} (hf : f ∈ A) :
   (conj_ae.to_alg_hom.comp_left_continuous ℝ conj_cle.continuous) f ∈ A :=
-begin
-  suffices : _ ∈ A.map (conj_ae.to_alg_hom.comp_left_continuous ℝ conj_cle.continuous),
-  { convert this,
-    exact hA.symm },
-  rw subalgebra.mem_map,
-  exact ⟨f, hf, rfl⟩,
-end
+hA ⟨f, hf, rfl⟩
+
+@[simp] lemma to_ring_hom_apply {R A B : Type*} [comm_ring R] [ring A] [algebra R A]
+   [ring B] [algebra R B] (f : A →ₐ[R] B) (x : A) :
+  f.to_ring_hom x = f x :=
+rfl
+
+-- lemma conj_invariant_of_forall {A : subalgebra ℝ C(X, ℂ)}
+--   (hA : ∀ f ∈ A, (conj_ae.to_alg_hom.comp_left_continuous ℝ conj_cle.continuous) f ∈ A) :
+--   conj_invariant_subalgebra A:=
+-- begin
+--   rintros _ ⟨f, hf, rfl⟩,
+--   exact hA f hf,
+-- end
+-- lemma mem_of_mem_conj_invariant {A : subalgebra ℝ C(X, ℂ)} (hA : conj_invariant_subalgebra A)
+--   {f : C(X, ℂ)} (hf : (conj_ae.to_alg_hom.comp_left_continuous ℝ conj_cle.continuous) f ∈ A) :
+--   f ∈ A :=
+-- begin
+--   convert mem_conj_invariant hA hf,
+--   ext1 x,
+--   simp,
+-- end
 
 end continuous_map
 

1f3ab9c61217a67d3cb2b6b2d8fa7d3bc93eeacc
Riccardo Brasca <riccardo.brasca@gmail.com>
2021-06-22 13:24:27 +0200

    Update src/linear_algebra/dfinsupp.lean
    Co-authored-by: Anne Baanen <Vierkantor@users.noreply.github.com>

--- src/linear_algebra/dfinsupp.lean
+++ src/linear_algebra/dfinsupp.lean
@@ -3,7 +3,7 @@ Copyright (c) 2018 Kenny Lau. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Johannes Hölzl, Kenny Lau
 -/
-import data.dfinsupp
+import data.finsupp.to_dfinsupp
 import linear_algebra.basis
 
 /-!

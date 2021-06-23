8fb1740f5a873ddf6c3bc713d36b0584d84f9ff9
Vierkantor <vierkantor@vierkantor.com>
2021-06-23 10:18:26 +0000

    feat(field_theory): state primitive element theorem using `power_basis`

--- src/field_theory/primitive_element.lean
+++ src/field_theory/primitive_element.lean
@@ -184,4 +184,17 @@ begin
     exact induction_on_adjoin P base ih ⊤ },
 end
 
+/-- Alternative phrasing of primitive element theorem:
+a finite separable field extension has a basis `1, α, α^2, ..., α^n`.
+
+See also `exists_primitive_element`. -/
+noncomputable def power_basis_of_finite_of_separable
+  [finite_dimensional F E] (F_sep : is_separable F E) :
+  power_basis F E :=
+let α := (exists_primitive_element F_sep).some in
+have e : F⟮α⟯ = ⊤ := (exists_primitive_element F_sep).some_spec,
+-- TODO: times out if you get rid of the `@` and write `adjoin.power_basis (F_sep.is_integral α)`
+power_basis.map (@adjoin.power_basis _ _ _ _ _ _ (F_sep.is_integral α))
+  ((intermediate_field.equiv_of_eq e).trans intermediate_field.top_equiv)
+
 end field

b8d76e557652dc554d25f6e885ac6055021e1955
Bhavik Mehta <bhavikmehta8@gmail.com>
2021-06-27 05:26:24 +0100

    strict initials

--- src/category_theory/limits/shapes/strict_initial.lean
+++ src/category_theory/limits/shapes/strict_initial.lean
@@ -5,8 +5,37 @@ Authors: Bhavik Mehta
 -/
 
 import category_theory.limits.shapes.terminal
+import category_theory.limits.shapes.binary_products
 import category_theory.epi_mono
-import tactic.tfae
+
+/-!
+# Strict initial objects
+
+This file sets up the basic theory of strict initial objects: initial objects where every morphism
+to it is an isomorphism. This generalises a property of the empty set in the category of sets:
+namely that the only function to the empty set is from itself.
+
+We say `C` has strict initial objects if every initial object is strict, ie given any morphism
+`f : A ⟶ I` where `I` is initial, then `f` is an isomorphism.
+Strictly speaking, this says that *any* initial object must be strict, rather than that strict
+initial objects exist, which turns out to be a more useful notion to formalise.
+
+If the binary product of `X` with a strict initial object exists, it is also initial.
+
+To show a category `C` with an initial object has strict initial objects, the most convenient way
+is to show any morphism to the (chosen) initial object is an isomorphism and use
+`has_strict_initial_object_of_initial_is_strict`.
+
+The dual notion (strict terminal objects) occurs much less frequently in practice so is ignored.
+
+## TODO
+
+Construct examples of this: `Type*`, `Top`, `Groupoid`, simplicial types, posets.
+
+## References
+* https://ncatlab.org/nlab/show/strict+initial+object
+-/
+
 
 universes v u
 
@@ -26,8 +55,10 @@ initial objects exist.
 class has_strict_initial_object : Prop :=
 (out : ∀ {I A : C} (f : A ⟶ I), is_initial I → is_iso f)
 
-variables {C} [has_strict_initial_object C]
-variables {I : C}
+variables {C}
+
+section
+variables [has_strict_initial_object C] {I : C}
 
 lemma is_initial.is_iso_to (hI : is_initial I) {A : C} (f : A ⟶ I) :
   is_iso f :=
@@ -48,6 +79,81 @@ lemma is_initial.mono_from (hI : is_initial I) {A : C} (f : I ⟶ A) :
 lemma is_initial.to_mono (hI : is_initial I) {A : C} :
   mono (hI.to A) :=
 hI.mono_from _
+
+/-- If `I` is initial, then `X ⨯ I` is isomorphic to it. -/
+@[simps hom]
+noncomputable def mul_is_initial (X : C) [has_binary_product X I] (hI : is_initial I) :
+  X ⨯ I ≅ I :=
+@@as_iso _ prod.snd (hI.is_iso_to _)
+
+@[simp] lemma mul_is_initial_inv (X : C) [has_binary_product X I] (hI : is_initial I) :
+  (mul_is_initial X hI).inv = hI.to _ :=
+hI.hom_ext _ _
+
+/-- If `I` is initial, then `I ⨯ X` is isomorphic to it. -/
+@[simps hom]
+noncomputable def is_initial_mul (X : C) [has_binary_product I X] (hI : is_initial I) :
+  I ⨯ X ≅ I :=
+@@as_iso _ prod.fst (hI.is_iso_to _)
+
+@[simp] lemma is_initial_mul_inv (X : C) [has_binary_product I X] (hI : is_initial I) :
+  (is_initial_mul X hI).inv = hI.to _ :=
+hI.hom_ext _ _
+
+variable [has_initial C]
+
+instance initial_is_iso_to {A : C} (f : A ⟶ ⊥_C) : is_iso f :=
+initial_is_initial.is_iso_to _
+
+lemma initial_subsingleton_to {A : C} (f g : A ⟶ ⊥_C) :
+  f = g :=
+initial_is_initial.subsingleton_to _ _
+
+instance initial_mono_from {A : C} (f : ⊥_C ⟶ A) : mono f :=
+initial_is_initial.mono_from _
+
+instance initial_to_mono {A : C} : mono (initial.to A) :=
+initial_is_initial.to_mono
+
+/--
+The product of `X` with an initial object in a category with strict initial objects is itself
+initial.
+This is the generalisation of the fact that `X × empty ≃ empty` for types (or `n * 0 = 0`).
+-/
+@[simps hom]
+noncomputable def mul_initial (X : C) [has_binary_product X ⊥_C] :
+  X ⨯ ⊥_C ≅ ⊥_C :=
+mul_is_initial _ initial_is_initial
+
+@[simp] lemma mul_initial_inv (X : C) [has_binary_product X ⊥_C] :
+  (mul_initial X).inv = initial.to _ :=
+subsingleton.elim _ _
+
+/--
+The product of `X` with an initial object in a category with strict initial objects is itself
+initial.
+This is the generalisation of the fact that `empty × X ≃ empty` for types (or `0 * n = 0`).
+-/
+@[simps hom]
+noncomputable def initial_mul (X : C) [has_binary_product (⊥_C) X] :
+  ⊥_C ⨯ X ≅ ⊥_C :=
+is_initial_mul _ initial_is_initial
+
+@[simp] lemma initial_mul_inv (X : C) [has_binary_product (⊥_C) X] :
+  (initial_mul X).inv = initial.to _ :=
+subsingleton.elim _ _
+end
+
+/-- If `C` has an initial object such that every morphism *to* it is an isomorphism, then `C`
+has strict initial objects. -/
+lemma has_strict_initial_object_of_initial_is_strict [has_initial C]
+  (h : ∀ A (f : A ⟶ ⊥_C), is_iso f) :
+  has_strict_initial_object C :=
+{ out := λ I A f hI,
+  begin
+    haveI := h A (f ≫ hI.to _),
+    exact ⟨⟨hI.to _ ≫ inv (f ≫ hI.to ⊥_C), by rw [←assoc, is_iso.hom_inv_id], hI.hom_ext _ _⟩⟩,
+  end }
 
 end limits
 end category_theory

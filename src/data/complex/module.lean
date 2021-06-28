3abc7443d83ec3ad3a67d3d39e2dd734c4595857
Eric Wieser <wieser.eric@gmail.com>
2021-06-28 14:55:28 +0100

    tidy?

--- src/data/complex/module.lean
+++ src/data/complex/module.lean
@@ -238,8 +238,7 @@ variables {A : Type*} [ring A] [algebra ℝ A]
 /-- There is an alg_hom from `ℂ` to any `ℝ`-algebra with an element that squares to `-1`.
 
 See `complex.lift` for this as an equiv. -/
-def lift_aux (I' : A) (hf : I' * I' = -1) :
-  ℂ →ₐ[ℝ] A :=
+def lift_aux (I' : A) (hf : I' * I' = -1) : ℂ →ₐ[ℝ] A :=
 alg_hom.mk'
   { to_fun := ⇑((algebra.of_id ℝ A).to_linear_map.comp re_lm +
                 (linear_map.to_span_singleton _ _ I').comp im_lm),
@@ -248,22 +247,21 @@ alg_hom.mk'
     map_mul' := λ ⟨x₁, y₁⟩ ⟨x₂, y₂⟩, by {
       change algebra_map ℝ A (x₁ * x₂ - y₁ * y₂) + (x₁ * y₂ + y₁ * x₂) • I' =
              (algebra_map ℝ A x₁ + y₁ • I') * (algebra_map ℝ A x₂ + y₂ • I'),
-      rw [mul_add, add_mul, add_mul],
-      rw [smul_mul_smul, hf, smul_neg, ←algebra.algebra_map_eq_smul_one, ←sub_eq_add_neg],
-      simp only [←algebra.commutes x₂, ←ring_hom.map_mul],
-      simp only [←algebra.smul_def],
-      simp only [add_smul, smul_smul, ring_hom.map_sub, mul_comm y₁ x₂],
-      abel, },
+      rw [add_mul, mul_add, mul_add, smul_mul_smul, hf, smul_neg, ←algebra.algebra_map_eq_smul_one,
+        ←sub_eq_add_neg],
+      simp only [algebra.smul_def],
+      rw [←algebra.right_comm _ x₂, ←mul_assoc, ←ring_hom.map_mul, ←ring_hom.map_mul,
+        ←ring_hom.map_mul, add_sub, add_assoc, ←add_mul, ←ring_hom.map_add,
+        ←sub_add_eq_add_sub, ring_hom.map_sub], },
     map_one' := show algebra_map ℝ A 1 + (0 : ℝ) • I' = 1,
       by rw [ring_hom.map_one, zero_smul, add_zero], }
   (linear_map.map_smul _)
 
 @[simp]
 lemma lift_aux_apply (I' : A) (hI') (z : ℂ) :
-  lift_aux I' hI' z = algebra_map ℝ A z.re + z.im • I' := rfl
+ lift_aux I' hI' z = algebra_map ℝ A z.re + z.im • I' := rfl
 
-lemma lift_aux_apply_I (I' : A) (hI') :
-  lift_aux I' hI' I = I' := by simp
+lemma lift_aux_apply_I (I' : A) (hI') : lift_aux I' hI' I = I' := by simp
 
 /-- A universal property of the complex numbers, providing a unique `ℂ →ₐ[ℝ] A` for every element
 of `A` which squares to `-1`.
@@ -272,8 +270,7 @@ This can be used to embed the complex numbers in the `quaternion`s.
 
 This isomorphism is named to match the very similar `zsqrtd.lift`. -/
 @[simps {simp_rhs := tt}]
-noncomputable def lift  :
-  {I' : A // I' * I' = -1} ≃ (ℂ →ₐ[ℝ] A) :=
+noncomputable def lift : {I' : A // I' * I' = -1} ≃ (ℂ →ₐ[ℝ] A) :=
 { to_fun := λ I', lift_aux I' I'.prop,
   inv_fun := λ F, ⟨F I, by rw [←F.map_mul, I_mul_I, alg_hom.map_neg, alg_hom.map_one]⟩,
   left_inv := λ I', subtype.ext $ lift_aux_apply_I I' I'.prop,

a9131d22cb2b422d616925c7a162d10409b9ab67
Johan Commelin <johan@commelin.net>
2021-06-29 15:44:47 +0000

    add namespace

--- src/data/real/nnreal.lean
+++ src/data/real/nnreal.lean
@@ -790,4 +790,4 @@ end
 
 lemma cast_nat_abs_eq_nnabs_cast (n : ℤ) :
   (n.nat_abs : ℝ≥0) = real.nnabs n :=
-by { ext, rw [nnreal.coe_nat_cast, cast_nat_abs, nnreal.coe_nnabs] }
+by { ext, rw [nnreal.coe_nat_cast, int.cast_nat_abs, nnreal.coe_nnabs] }

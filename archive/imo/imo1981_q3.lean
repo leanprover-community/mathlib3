7f10fd63cad42115d9f63170c0f1ba758221089e
YaelDillies <yael.dillies@gmail.com>
2021-06-29 10:17:16 +0100

    some stuff

--- archive/imo/imo1981_q3.lean
+++ archive/imo/imo1981_q3.lean
@@ -45,11 +45,15 @@ variable {N}
 
 lemma m_le_n {m n : ℤ} (h1 : problem_predicate N m n) : m ≤ n :=
 begin
-  by_contradiction h2,
+  refine le_of_not_lt (λ h2, _),
   have h3 : 1 = (n * (n - m) - m ^ 2) ^ 2,
   { calc 1 = (n ^ 2 - m * n - m ^ 2) ^ 2 : h1.eq_one.symm
        ... = (n * (n - m) - m ^ 2) ^ 2   : by ring },
-  have h4 : n * (n - m) - m ^ 2 < -1, by nlinarith [h1.n_range.left],
+  have h4 : n * (n - m) - m ^ 2 < -1,
+  {
+    rw int.lt_iff_add_one_le at ⊢ h2,
+    nlinarith [h1.n_range.left],
+  },
   have h5 : 1 < (n * (n - m) - m ^ 2) ^ 2, by nlinarith,
   exact h5.ne h3
 end

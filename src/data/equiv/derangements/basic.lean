4d65f222039fa3113582f60dc213ae3b4563780a
Henry Swanson <henryswanson94@gmail.com>
2021-07-01 22:24:02 -0700

    Remove unnecessary imports

--- src/data/equiv/derangements/basic.lean
+++ src/data/equiv/derangements/basic.lean
@@ -3,8 +3,6 @@ Copyright (c) 2021 Henry Swanson. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Henry Swanson
 -/
-import data.equiv.basic
-import data.equiv.option
 import group_theory.perm.option
 
 import tactic.equiv_rw

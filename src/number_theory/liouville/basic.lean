8a55d1172422d6f54855302f39e2eae17651e68d
Hunter Monroe <github.com@huntermonroe.com>
2021-07-03 16:41:29 -0400

    Update src/number_theory/liouville/basic.lean
    Co-authored-by: hrmacbeth <25316162+hrmacbeth@users.noreply.github.com>

--- src/number_theory/liouville/basic.lean
+++ src/number_theory/liouville/basic.lean
@@ -166,7 +166,7 @@ begin
     exact (mem_roots fR0).mpr (is_root.def.mpr hy) }
 end
 
-/-- **Louisville's Theorem** -/
+/-- **Liouville's Theorem** -/
 theorem transcendental {x : ℝ} (lx : liouville x) :
   transcendental ℤ x :=
 begin

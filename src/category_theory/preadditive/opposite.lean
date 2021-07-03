97c7b28d6822e1b93ef72e42d9fc5e200931d58f
Adam Topaz <github@adamtopaz.com>
2021-07-03 13:02:26 -0600

    Op additive functors

--- src/category_theory/preadditive/opposite.lean
+++ src/category_theory/preadditive/opposite.lean
@@ -1,9 +1,10 @@
 /-
 Copyright (c) 2021 Scott Morrison. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
-Authors: Scott Morrison
+Authors: Scott Morrison, Adam Topaz
 -/
 import category_theory.preadditive
+import category_theory.preadditive.additive_functor
 import data.equiv.transfer_instance
 
 /-!
@@ -26,5 +27,35 @@ instance : preadditive Cᵒᵖ :=
 
 @[simp] lemma unop_zero (X Y : Cᵒᵖ) : (0 : X ⟶ Y).unop = 0 := rfl
 @[simp] lemma unop_add {X Y : Cᵒᵖ} (f g : X ⟶ Y) : (f + g).unop = f.unop + g.unop := rfl
+
+variables {C} {D : Type*} [category D] [preadditive D]
+
+instance functor.op_additive (F : C ⥤ D) [F.additive] : F.op.additive := {}
+
+instance functor.right_op_additive (F : Cᵒᵖ ⥤ D) [F.additive] : F.right_op.additive :=
+{ map_zero' := λ X Y, begin
+    change (F.map 0).op = 0,
+    rw functor.map_zero,
+    refl,
+  end,
+  map_add' := λ X Y f g, begin
+    change (F.map (f.op + g.op)).op = _,
+    rw functor.map_add,
+    refl,
+  end }
+
+instance functor.left_op_additive (F : C ⥤ Dᵒᵖ) [F.additive] : F.left_op.additive := {}
+
+instance functor.unop_additive (F : Cᵒᵖ ⥤ Dᵒᵖ) [F.additive] : F.unop.additive :=
+{ map_zero' := λ X Y, begin
+    change (F.map 0).unop = 0,
+    rw functor.map_zero,
+    refl,
+  end,
+  map_add' := λ X Y f g, begin
+    change (F.map (f.op + g.op)).unop = _,
+    rw F.map_add,
+    refl,
+  end }
 
 end category_theory

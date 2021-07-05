b6ac3a9f0441c76909d5f51b73e46bba45d35826
Hunter Monroe <github.com@huntermonroe.com>
2021-07-05 12:49:33 -0400

    Update src/ring_theory/noetherian.lean
    Co-authored-by: Bryan Gin-ge Chen <bryangingechen@gmail.com>

--- src/ring_theory/noetherian.lean
+++ src/ring_theory/noetherian.lean
@@ -97,7 +97,8 @@ begin
     refine ⟨range s, finite_range s, hs⟩ },
 end
 
-/-- **Nakayama's Lemma**. Atiyah-Macdonald 2.5, Eisenbud 4.7, Matsumura 2.2, Stacks 00DV -/
+/-- **Nakayama's Lemma**. Atiyah-Macdonald 2.5, Eisenbud 4.7, Matsumura 2.2,
+[Stacks 00DV](https://stacks.math.columbia.edu/tag/00VL) -/
 theorem exists_sub_one_mem_and_smul_eq_zero_of_fg_of_le_smul {R : Type*} [comm_ring R]
   {M : Type*} [add_comm_group M] [module R M]
   (I : ideal R) (N : submodule R M) (hn : N.fg) (hin : N ≤ I • N) :

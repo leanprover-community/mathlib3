134518d9ee710b2f483e2ab0b9f9cf6a98d1571d
Adam Topaz <github@adamtopaz.com>
2021-07-05 12:21:04 -0600

    Add projective succ iso

--- src/category_theory/abelian/ext.lean
+++ src/category_theory/abelian/ext.lean
@@ -17,9 +17,16 @@ We define `Ext R C n : Cᵒᵖ ⥤ C ⥤ Module R` for any `R`-linear abelian ca
 by deriving in the first argument of the bifunctor `(X, Y) ↦ Module.of R (unop X ⟶ Y)`.
 
 ## Implementation
+
 It's not actually necessary here to assume `C` is abelian,
 but the hypotheses, involving both `C` and `Cᵒᵖ`, are quite lengthy,
 and in practice the abelian case is hopefully enough.
+
+PROJECT we don't yet have injective resolutions and right derived functors
+(although this is only a copy-and-paste dualisation)
+so we can't even state the alternative definition
+in terms of right-deriving in the first argument,
+let alone start the harder project of showing they agree.
 -/
 
 noncomputable theory
@@ -50,10 +57,21 @@ functor.flip
     intros X Y Z f g,
     rw [(linear_yoneda R C).map_comp, nat_trans.right_op_comp, nat_trans.left_derived_comp],
     refl,
-  end }
+  end }.
 
--- PROJECT we don't yet have injective resolutions and right derived functors
--- (although this is only a copy-and-paste dualisation)
--- so we can't even state the alternative definition
--- in terms of right-deriving in the first argument,
--- let alone start the harder project of showing they agree.
+open_locale zero_object
+
+def ext_projective_succ (X Y : C) [projective X] (n : ℕ) :
+  ((Ext R C (n+1)).obj (opposite.op X)).obj Y ≅ 0 :=
+let E := (((linear_yoneda R C).obj Y).right_op.left_derived_obj_projective_succ n X).unop.symm in
+E ≪≫
+{ hom := 0,
+  inv := 0,
+  hom_inv_id' := begin
+    let Z : (Module R)ᵒᵖ := 0,
+    rw [← (0 : 0 ⟶ Z.unop).unop_op, ← (0 : Z.unop ⟶ 0).unop_op,
+      ← unop_id, ← unop_comp],
+    congr' 1,
+    dsimp,
+    dec_trivial,
+  end }

bbbcbcffe8c8f71ca8fe45f8abf50f8dabfb6b69
Adam Topaz <github@adamtopaz.com>
2021-07-05 07:28:47 -0600

    Add linear yoneda

---
+++ src/category_theory/linear/yoneda.lean
@@ -0,0 +1,41 @@
+/-
+Copyright (c) 2021 Scott Morrison. All rights reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
+Authors: Scott Morrison
+-/
+import algebra.category.Module.basic
+import category_theory.linear
+import category_theory.preadditive.additive_functor
+import category_theory.preadditive.opposite
+
+/-!
+# The Yoneda embedding for `R`-linear categories
+
+The Yoneda embedding for `R`-linear categories `C`,
+sends an object `X : C` to the `Module R`-valued presheaf on `C`,
+with value on `Y : Cᵒᵖ` given by `Module.of R (unop Y ⟶ X)`.
+-/
+
+namespace category_theory
+
+open opposite
+
+variables (R : Type*) [ring R] (C : Type*) [category C] [preadditive C] [linear R C]
+
+/-- The Yoneda embedding for `R`-linear categories `C`,
+sending an object `X : C` to the `Module R`-valued presheaf on `C`,
+with value on `Y : Cᵒᵖ` given by `Module.of R (unop Y ⟶ X)`. -/
+@[simps]
+def linear_yoneda : C ⥤ Cᵒᵖ ⥤ Module R :=
+{ obj := λ X,
+  { obj := λ Y, Module.of R (unop Y ⟶ X),
+    map := λ Y Y' f, linear.left_comp R _ f.unop,
+    map_comp' := λ _ _ _ f g, begin ext, dsimp, erw [category.assoc] end,
+    map_id' := λ Y, begin ext, dsimp, erw [category.id_comp] end },
+  map := λ X X' f, { app := λ Y, linear.right_comp R _ f } }.
+
+instance linear_yoneda_obj_additive (X : C) : ((linear_yoneda R C).obj X).additive := {}
+-- TODO, in fact, it's `R`-linear as well
+-- TODO, in fact, `linear_yoneda` itself is additive and `R`-linear.
+
+end category_theory

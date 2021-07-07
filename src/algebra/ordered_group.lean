cf8eb59d2703a23efd8edd8b5680d6d1b9741d41
adomani <adomani@gmail.com>
2021-07-07 12:35:55 +0200

    unsimp

--- src/algebra/ordered_group.lean
+++ src/algebra/ordered_group.lean
@@ -409,7 +409,7 @@ variables [comm_group α]
 section has_le
 variables [has_le α] [covariant_class α α (*) (≤)] {a b c d : α}
 
-@[simp, to_additive]
+@[to_additive]
 lemma inv_mul_le_iff_le_mul' : c⁻¹ * a ≤ b ↔ a ≤ b * c :=
 by rw [inv_mul_le_iff_le_mul, mul_comm]
 

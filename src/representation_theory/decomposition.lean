187a71958486fc0ae4a55e46326cb1ec3cb420d3
Hanting Zhang <hantingzhang03@gmail.com>
2021-07-07 19:28:45 -0700

    define decompositions

--- src/representation_theory/decomposition.lean
+++ src/representation_theory/decomposition.lean
@@ -16,43 +16,50 @@ This file talks about decompositions of modules.
 universes u v w
 
 open_locale direct_sum classical big_operators
-open classical linear_map
+open classical linear_map dfinsupp
 noncomputable theory
 
-variables (ι : Type u) (R : Type v) (M : Type w)
-variables [semiring R] [add_comm_monoid M] [module R M]
+namespace submodule
 
-/-- A `decomposition ι R M` for a module `M` is the type of `ι`-indexed decompositions of `M`.
+/--
+A `decomposition ι R M` for a module `M` is the type of `ι`-indexed decompositions of `M`.
 The factors are available as `factors : ι → submodule R M`.
 To create a decomposition of `M`, one furthermore must provide data that the
 factors satisfy `∀ i : ι, disjoint (factors i) (⨆ (j ≠ i), factors j)` and
 that their union is the whole module `M`.
+
 Note: Why not say `set (submodule R M)`? Then one could get rid of explicitly
 identifying `ι`. But I think it's better to follow the `basis` design,
-which was phased out of exactly the `set` design because of problems with it.
+which phased out of the `set` design because of problems with it.
 -/
 @[nolint has_inhabited_instance]
-structure decomposition : Type (max u v w) :=
+structure decomposition (ι : Type u) (R : Type v) (M : Type w)
+  [semiring R] [add_comm_monoid M] [module R M] : Type (max u v w) :=
 (factors : ι → submodule R M)
 (factors_ind : complete_lattice.independent factors)
 (factors_supr : supr factors = ⊤)
 
-/-
-variables {ι R M}
+
+namespace decomposition
+variables {ι : Type u} {R : Type v} {M : Type w}
+
+
+/- Stuff in here doesn't require `add_comm_group M`. -/
+section monoid
+variables [semiring R] [add_comm_monoid M] [module R M]
 
 /--
-bruh
-
+Given an `ι`-indexed collection of submodules, build the map of their direct sum into `M`.
 -/
-def terrible_map (p : ι → submodule R M) :
+def to_direct_sum_map (p : ι → submodule R M) :
   (⨁ i, p i) →ₗ[R] (supr p : submodule R M) :=
 (direct_sum.to_module R ι _ (λ i, submodule.of_le (le_supr p i : p i ≤ supr p)))
 
-@[simp] lemma terrible_map_apply (p : ι → submodule R M) (x : ⨁ (i : ι), (p i)) :
-  terrible_map p x = ∑ i in x.support, ⟨(x i).1, (le_supr p i : p i ≤ supr p) (x i).2⟩ :=
+@[simp] lemma to_direct_sum_map_apply (p : ι → submodule R M) (x : ⨁ (i : ι), (p i)) :
+  to_direct_sum_map p x = ∑ i in x.support, ⟨(x i).1, (le_supr p i : p i ≤ supr p) (x i).2⟩ :=
 begin
-  simp only [terrible_map, direct_sum.to_module, dfinsupp.sum_add_hom_apply, coe_mk,
-    to_add_monoid_hom_coe, dfinsupp.lsum_apply, subtype.val_eq_coe],
+  simp only [to_direct_sum_map, direct_sum.to_module, sum_add_hom_apply, to_add_monoid_hom_coe,
+    linear_map.coe_mk, dfinsupp.lsum_apply, subtype.val_eq_coe],
   refine finset.sum_congr rfl (λ i hi, _),
   congr,
 end
@@ -78,17 +85,19 @@ begin
   rwa finsupp.mem_support_iff at hi,
 end
 
-end setup
+end monoid
 
-variables {ι : Type u} {R : Type v} {M : Type w}
+
+section group
 variables [ring R] [add_comm_group M] [module R M]
-lemma terrible_map_ker
-  (p : ι → submodule R M) (h : complete_lattice.independent p) : (terrible_map p).ker = ⊥ :=
+
+lemma to_direct_sum_map_ker
+  (p : ι → submodule R M) (h : complete_lattice.independent p) : (to_direct_sum_map p).ker = ⊥ :=
 begin
   rw ker_eq_bot',
   intros x hx,
   ext i,
-  simp only [terrible_map_apply, ← set_like.coe_eq_coe, coe_sum, submodule.coe_mk] at hx,
+  simp only [to_direct_sum_map_apply, ← set_like.coe_eq_coe, submodule.coe_sum, submodule.coe_mk] at hx,
   revert i,
   by_contra,
   push_neg at h,
@@ -114,21 +123,21 @@ begin
   contradiction,
 end
 
-lemma terrible_map_range
-  (p : ι → submodule R M) (h : complete_lattice.independent p) : (terrible_map p).range = ⊤ :=
+lemma to_direct_sum_map_range
+  (p : ι → submodule R M) (h : complete_lattice.independent p) : (to_direct_sum_map p).range = ⊤ :=
 begin
   ext,
   cases x with x hx,
-  simp only [coe_fn_sum, submodule.mem_top, mem_range,
-    lsum_apply, coe_proj, function.comp_app, function.eval_apply,
+  simp only [linear_map.coe_fn_sum, submodule.mem_top, mem_range,
+    linear_map.lsum_apply, coe_proj, function.comp_app, function.eval_apply,
     iff_true, coe_comp, subtype.val_eq_coe],
   rw submodule.mem_supr' at hx,
   rcases hx with ⟨v, hv, hvs⟩,
   use direct_sum.mk (λ i, p i) v.support (λ i, ⟨v i, hv i⟩),
-  simp only [terrible_map_apply, subtype.val_eq_coe],
+  simp only [to_direct_sum_map_apply, subtype.val_eq_coe],
   rw support_mk_support,
   rw [← set_like.coe_eq_coe, set_like.coe_mk, ← hvs],
-  rw @coe_sum ι R M _ _ _ (supr p : submodule R M) _ v.support,
+  rw submodule.coe_sum,
   refine finset.sum_congr rfl (λ i hi, _),
   simp [direct_sum.mk],
   split_ifs,
@@ -137,53 +146,53 @@ begin
 end
 
 /--
-blah
-
+Given an indexed collection of submodules `p i : submodule R M` and
+a proof that they are independent, build the obvious equivalence between
+the direct sum of the `p i`'s and `supr p : submodule R M`.
 -/
-def submodule.direct_sum_equiv_of_independent
+def direct_sum_equiv_of_independent
   (p : ι → submodule R M) (h : complete_lattice.independent p) :
   (⨁ i, p i) ≃ₗ[R] (supr p : submodule R M) :=
 begin
   letI : add_comm_group (⨁ (i : ι), (p i)) := direct_sum.add_comm_group (λ i, p i),
   convert @linear_equiv.of_bijective R (⨁ i, p i) (supr p : submodule R M) _ _ _ _ _ _ _ _,
-  convert terrible_map p,
+  convert to_direct_sum_map p,
   dsimp,
-  convert terrible_map_ker p h,
+  convert to_direct_sum_map_ker p h,
   dsimp,
-  convert terrible_map_range p h,
+  convert to_direct_sum_map_range p h,
 end
 
-@[simp] lemma submodule.direct_sum_equiv_of_independent_apply
+@[simp] lemma direct_sum_equiv_of_independent_apply
   (p : ι → submodule R M) (h : complete_lattice.independent p) (x : ⨁ i, p i) :
-  submodule.direct_sum_equiv_of_independent p h x = terrible_map p x :=
+  direct_sum_equiv_of_independent p h x = to_direct_sum_map p x :=
 rfl
 
 variables {R M}
 
 /--
-blah
-
+Given an decomposition `D`, build an equivalence between the direct sum of `D.factors` and `M`.
 -/
-def is_decomposition.equiv (D : decomposition ι R M) : (⨁ i, D.factors i) ≃ₗ[R] M :=
-((submodule.direct_sum_equiv_of_independent D.factors D.factors_ind).trans
+def to_equiv (D : decomposition ι R M) : (⨁ i, D.factors i) ≃ₗ[R] M :=
+((direct_sum_equiv_of_independent D.factors D.factors_ind).trans
   (linear_equiv.of_eq _ _ D.factors_supr)).trans (submodule.top_equiv_self)
 
-lemma is_decomposition.equiv_apply (D : decomposition ι R M) (x : ⨁ i, D.factors i) :
-  (is_decomposition.equiv D) x = ∑ i in x.support, x i :=
+lemma to_equiv_apply (D : decomposition ι R M) (x : ⨁ i, D.factors i) :
+  D.to_equiv x = ∑ i in x.support, x i :=
 begin
-  rw [is_decomposition.equiv],
+  rw [to_equiv],
   rw linear_equiv.trans_apply,
   rw linear_equiv.trans_apply,
   simp [submodule.top_equiv_self],
 end
 
-lemma is_decomposition.equiv_symm_single_apply
+lemma to_equiv_symm_single_apply
   (D : decomposition ι R M) (i : ι) (x : M) (hx : x ∈ D.factors i) :
-  (is_decomposition.equiv D).symm x = dfinsupp.single i ⟨x, hx⟩ :=
+  D.to_equiv.symm x = dfinsupp.single i ⟨x, hx⟩ :=
 begin
-  apply_fun (is_decomposition.equiv D),
+  apply_fun D.to_equiv,
   rw [linear_equiv.apply_symm_apply],
-  rw is_decomposition.equiv_apply,
+  rw to_equiv_apply,
   by_cases h : x = 0,
   { rw dfinsupp.support_single_eq_zero,
     rw finset.sum_empty,
@@ -198,4 +207,9 @@ begin
   exact_mod_cast h,
   exact linear_equiv.injective _,
 end
--/
+
+end group
+
+end decomposition
+
+end submodule

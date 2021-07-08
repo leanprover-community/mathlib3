ba20fa3ae6bb96c5b2e8af90b7365d82711d2bbc
Hunter Monroe <github.com@huntermonroe.com>
2021-07-08 09:19:04 -0400

    Update src/combinatorics/simple_graph/subgraph.lean
    Co-authored-by: Kyle Miller <kmill31415@gmail.com>

--- src/combinatorics/simple_graph/subgraph.lean
+++ src/combinatorics/simple_graph/subgraph.lean
@@ -75,7 +75,7 @@ begin
   exact G'.edge_vert (G'.sym' he),
 end
 
-lemma adj_symm' (G' : subgraph G) ⦃v w : V⦄ : G'.adj' v w ↔ G'.adj' w v :=
+lemma sym' (G' : subgraph G) : symmetric G'.adj :=
 by { split; apply G'.sym' }
 
 /--

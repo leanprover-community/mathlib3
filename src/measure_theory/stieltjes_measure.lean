438ad85f892a2277a5c04bf9522080fc0ccc7ac8
sgouezel <sebastien.gouezel@univ-rennes1.fr>
2021-07-11 16:14:41 +0200

    complete construction of stieltjes measures

--- src/measure_theory/stieltjes_measure.lean
+++ src/measure_theory/stieltjes_measure.lean
@@ -1,18 +1,33 @@
 /-
-Copyright (c) 2017 Johannes Hölzl. All rights reserved.
+Copyright (c) 2021 Sébastien Gouëzel. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
-Authors: Johannes Hölzl, Yury Kudryashov
+Authors: Johannes Hölzl, Yury Kudryashov, Sébastien Gouëzel
 -/
-import measure_theory.pi
+import measure_theory.borel_space
 
 /-!
-# stieltjes measure on the real line and on `ℝⁿ`
+# Stieltjes measures on the real line
+
+Consider a function `f : ℝ → ℝ` which is monotone and right-continuous. Then one can define a
+corrresponding measure, giving mass `f b - f a` to the interval `(a, b]`.
+
+## Main definitions
+
+* `stieltjes_function` is a structure containing a function from `ℝ → ℝ`, together with the
+assertions that it is monotone and right-continuous. To `f : stieltjes_function`, one associates
+a Borel measure `f.measure`.
+* `f.left_lim x` is the limit of `f` to the left of `x`.
+* `f.measure_Ioc` asserts that `f.measure (Ioc a b) = of_real (f b - f a)`
+* `f.measure_Ioo` asserts that `f.measure (Ioo a b) = of_real (f.left_lim b - f a)`.
+* `f.measure_Icc` and `f.measure_Ico` are analogous.
 -/
 
 noncomputable theory
 open classical set filter
 open ennreal (of_real)
 open_locale big_operators ennreal nnreal topological_space
+
+/-! ### Basic properties of Stieltjes functions -/
 
 structure stieltjes_function :=
 (to_fun : ℝ → ℝ)
@@ -28,10 +43,6 @@ variable (f : stieltjes_function)
 lemma mono : monotone f := f.mono'
 
 lemma right_continuous (x : ℝ) : continuous_within_at f (Ici x) x := f.right_continuous' x
-
-/-!
-### Preliminary definitions
--/
 
 /-- The limit of a Stieltjes function to the left of `x` (it exists by monotonicity). The fact that
 it is indeed a left limit is asserted in `tendsto_left_lim` -/
@@ -55,6 +66,15 @@ begin
   assume z hz,
   exact f.mono hz.1.le,
 end
+
+lemma left_lim_le_left_lim {x y : ℝ} (h : x ≤ y) : f.left_lim x ≤ f.left_lim y :=
+begin
+  rcases eq_or_lt_of_le h with rfl|hxy,
+  { exact le_rfl },
+  { exact (f.left_lim_le le_rfl).trans (f.le_left_lim hxy) }
+end
+
+/-! ### The outer measure associated to a Stieltjes function -/
 
 /-- Length of an interval. This is the largest monotonic function which correctly
   measures all intervals. -/
@@ -237,328 +257,83 @@ begin
   simp [f.measurable_set_Ioi] { contextual := tt }
 end
 
+/-! ### The measure associated to a Stieltjes function -/
+
 /-- The measure associated to a Stieltjes function, giving mass `f b - f a` to the
 interval `(a, b]`. -/
-protected def measure : measure ℝ :=
+@[irreducible] protected def measure : measure ℝ :=
 { to_outer_measure := f.outer,
   m_Union := λ s hs, f.outer.Union_eq_of_caratheodory $
     λ i, f.borel_le_measurable _ (hs i),
   trimmed := f.outer_trim }
 
 @[simp] lemma measure_Ioc (a b : ℝ) : f.measure (Ioc a b) = of_real (f b - f a) :=
-f.outer_Ioc a b
+by { rw stieltjes_function.measure, exact f.outer_Ioc a b }
 
 @[simp] lemma measure_singleton (a : ℝ) : f.measure {a} = of_real (f a - f.left_lim a) :=
 begin
-  have T := exists_mono
-  have Z := tendsto_measure_Inter,
+  obtain ⟨u, u_mono, u_lt_a, u_lim⟩ : ∃ (u : ℕ → ℝ), strict_mono u ∧ (∀ (n : ℕ), u n < a)
+    ∧ tendsto u at_top (𝓝 a) := exists_seq_strict_mono_tendsto a,
+  have A : {a} = ⋂ n, Ioc (u n) a,
+  { refine subset.antisymm (λ x hx, by simp [mem_singleton_iff.1 hx, u_lt_a]) (λ x hx, _),
+    simp at hx,
+    have : a ≤ x := le_of_tendsto' u_lim (λ n, (hx n).1.le),
+    simp [le_antisymm this (hx 0).2] },
+  have L1 : tendsto (λ n, f.measure (Ioc (u n) a)) at_top (𝓝 (f.measure {a})),
+  { rw A,
+    refine tendsto_measure_Inter (λ n, measurable_set_Ioc) (λ m n hmn, _) _,
+    { exact Ioc_subset_Ioc (u_mono.monotone hmn) le_rfl },
+    { exact ⟨0, by simp only [measure_Ioc, ennreal.of_real_lt_top]⟩ } },
+  have L2 : tendsto (λ n, f.measure (Ioc (u n) a)) at_top (𝓝 (of_real (f a - f.left_lim a))),
+  { simp only [measure_Ioc],
+    have : tendsto (λ n, f (u n)) at_top (𝓝 (f.left_lim a)),
+    { apply (f.tendsto_left_lim a).comp,
+      exact tendsto_nhds_within_of_tendsto_nhds_of_eventually_within _ u_lim
+        (eventually_of_forall (λ n, u_lt_a n)) },
+    exact ennreal.continuous_of_real.continuous_at.tendsto.comp (tendsto_const_nhds.sub this) },
+  exact tendsto_nhds_unique L1 L2
 end
 
-#exit
-
-
-/-!
-### Definition of the stieltjes measure and lengths of intervals
--/
-
-/-- stieltjes measure on the Borel sets
-
-The outer stieltjes measure is the completion of this measure. (TODO: proof this)
--/
-instance real.measure_space : measure_space ℝ :=
-⟨{outer_measure := stieltjes_outer,
-  m_Union := λ f hf, stieltjes_outer.Union_eq_of_caratheodory $
-    λ i, borel_le_stieltjes_measurable _ (hf i),
-  trimmed := stieltjes_outer_trim }⟩
-
-@[simp] theorem stieltjes_outer_measure :
-  (volume : measure ℝ).outer_measure = stieltjes_outer := rfl
-
-end measure_theory
-
-open measure_theory
-
-namespace real
-
-variables {ι : Type*} [fintype ι]
-
-open_locale topological_space
-
-theorem volume_val (s) : volume s = stieltjes_outer s := rfl
-
-instance has_no_atoms_volume : has_no_atoms (volume : measure ℝ) :=
-⟨stieltjes_outer_singleton⟩
-
-@[simp] lemma volume_Ico {a b : ℝ} : volume (Ico a b) = of_real (b - a) := stieltjes_outer_Ico a b
-
-@[simp] lemma volume_Icc {a b : ℝ} : volume (Icc a b) = of_real (b - a) := stieltjes_outer_Icc a b
-
-@[simp] lemma volume_Ioo {a b : ℝ} : volume (Ioo a b) = of_real (b - a) := stieltjes_outer_Ioo a b
-
-@[simp] lemma volume_Ioc {a b : ℝ} : volume (Ioc a b) = of_real (b - a) := stieltjes_outer_Ioc a b
-
-@[simp] lemma volume_singleton {a : ℝ} : volume ({a} : set ℝ) = 0 := stieltjes_outer_singleton a
-
-@[simp] lemma volume_interval {a b : ℝ} : volume (interval a b) = of_real (abs (b - a)) :=
-by rw [interval, volume_Icc, max_sub_min_eq_abs]
-
-@[simp] lemma volume_Ioi {a : ℝ} : volume (Ioi a) = ∞ :=
-top_unique $ le_of_tendsto' ennreal.tendsto_nat_nhds_top $ λ n,
-calc (n : ℝ≥0∞) = volume (Ioo a (a + n)) : by simp
-... ≤ volume (Ioi a) : measure_mono Ioo_subset_Ioi_self
-
-@[simp] lemma volume_Ici {a : ℝ} : volume (Ici a) = ∞ :=
-by simp [← measure_congr Ioi_ae_eq_Ici]
-
-@[simp] lemma volume_Iio {a : ℝ} : volume (Iio a) = ∞ :=
-top_unique $ le_of_tendsto' ennreal.tendsto_nat_nhds_top $ λ n,
-calc (n : ℝ≥0∞) = volume (Ioo (a - n) a) : by simp
-... ≤ volume (Iio a) : measure_mono Ioo_subset_Iio_self
-
-@[simp] lemma volume_Iic {a : ℝ} : volume (Iic a) = ∞ :=
-by simp [← measure_congr Iio_ae_eq_Iic]
-
-instance locally_finite_volume : locally_finite_measure (volume : measure ℝ) :=
-⟨λ x, ⟨Ioo (x - 1) (x + 1),
-  is_open.mem_nhds is_open_Ioo ⟨sub_lt_self _ zero_lt_one, lt_add_of_pos_right _ zero_lt_one⟩,
-  by simp only [real.volume_Ioo, ennreal.of_real_lt_top]⟩⟩
-
-instance finite_measure_restrict_Icc (x y : ℝ) : finite_measure (volume.restrict (Icc x y)) :=
-⟨by simp⟩
-
-instance finite_measure_restrict_Ico (x y : ℝ) : finite_measure (volume.restrict (Ico x y)) :=
-⟨by simp⟩
-
-instance finite_measure_restrict_Ioc (x y : ℝ) : finite_measure (volume.restrict (Ioc x y)) :=
- ⟨by simp⟩
-
-instance finite_measure_restrict_Ioo (x y : ℝ) : finite_measure (volume.restrict (Ioo x y)) :=
-⟨by simp⟩
-
-/-!
-### Volume of a box in `ℝⁿ`
--/
-
-lemma volume_Icc_pi {a b : ι → ℝ} : volume (Icc a b) = ∏ i, ennreal.of_real (b i - a i) :=
+@[simp] lemma measure_Icc (a b : ℝ) : f.measure (Icc a b) = of_real (f b - f.left_lim a) :=
 begin
-  rw [← pi_univ_Icc, volume_pi_pi],
-  { simp only [real.volume_Icc] },
-  { exact λ i, measurable_set_Icc }
+  rcases le_or_lt a b with hab|hab,
+  { have A : disjoint {a} (Ioc a b), by simp,
+    simp [← Icc_union_Ioc_eq_Icc le_rfl hab, -singleton_union, ← ennreal.of_real_add, f.left_lim_le,
+      measure_union A (measurable_set_singleton a) measurable_set_Ioc, f.mono hab] },
+  { simp only [hab, measure_empty, Icc_eq_empty],
+    symmetry,
+    simp [ennreal.of_real_eq_zero, f.le_left_lim hab] }
 end
 
-@[simp] lemma volume_Icc_pi_to_real {a b : ι → ℝ} (h : a ≤ b) :
-  (volume (Icc a b)).to_real = ∏ i, (b i - a i) :=
-by simp only [volume_Icc_pi, ennreal.to_real_prod, ennreal.to_real_of_real (sub_nonneg.2 (h _))]
-
-lemma volume_pi_Ioo {a b : ι → ℝ} :
-  volume (pi univ (λ i, Ioo (a i) (b i))) = ∏ i, ennreal.of_real (b i - a i) :=
-(measure_congr measure.univ_pi_Ioo_ae_eq_Icc).trans volume_Icc_pi
-
-@[simp] lemma volume_pi_Ioo_to_real {a b : ι → ℝ} (h : a ≤ b) :
-  (volume (pi univ (λ i, Ioo (a i) (b i)))).to_real = ∏ i, (b i - a i) :=
-by simp only [volume_pi_Ioo, ennreal.to_real_prod, ennreal.to_real_of_real (sub_nonneg.2 (h _))]
-
-lemma volume_pi_Ioc {a b : ι → ℝ} :
-  volume (pi univ (λ i, Ioc (a i) (b i))) = ∏ i, ennreal.of_real (b i - a i) :=
-(measure_congr measure.univ_pi_Ioc_ae_eq_Icc).trans volume_Icc_pi
-
-@[simp] lemma volume_pi_Ioc_to_real {a b : ι → ℝ} (h : a ≤ b) :
-  (volume (pi univ (λ i, Ioc (a i) (b i)))).to_real = ∏ i, (b i - a i) :=
-by simp only [volume_pi_Ioc, ennreal.to_real_prod, ennreal.to_real_of_real (sub_nonneg.2 (h _))]
-
-lemma volume_pi_Ico {a b : ι → ℝ} :
-  volume (pi univ (λ i, Ico (a i) (b i))) = ∏ i, ennreal.of_real (b i - a i) :=
-(measure_congr measure.univ_pi_Ico_ae_eq_Icc).trans volume_Icc_pi
-
-@[simp] lemma volume_pi_Ico_to_real {a b : ι → ℝ} (h : a ≤ b) :
-  (volume (pi univ (λ i, Ico (a i) (b i)))).to_real = ∏ i, (b i - a i) :=
-by simp only [volume_pi_Ico, ennreal.to_real_prod, ennreal.to_real_of_real (sub_nonneg.2 (h _))]
-
-/-!
-### Images of the stieltjes measure under translation/multiplication/...
--/
-
-lemma map_volume_add_left (a : ℝ) : measure.map ((+) a) volume = volume :=
-eq.symm $ real.measure_ext_Ioo_rat $ λ p q,
-  by simp [measure.map_apply (measurable_const_add a) measurable_set_Ioo, sub_sub_sub_cancel_right]
-
-lemma map_volume_add_right (a : ℝ) : measure.map (+ a) volume = volume :=
-by simpa only [add_comm] using real.map_volume_add_left a
-
-lemma smul_map_volume_mul_left {a : ℝ} (h : a ≠ 0) :
-  ennreal.of_real (abs a) • measure.map ((*) a) volume = volume :=
+@[simp] lemma measure_Ioo {a b : ℝ} (h : a ≠ b) :
+  f.measure (Ioo a b) = of_real (f.left_lim b - f a) :=
 begin
-  refine (real.measure_ext_Ioo_rat $ λ p q, _).symm,
-  cases lt_or_gt_of_ne h with h h,
-  { simp only [real.volume_Ioo, measure.smul_apply, ← ennreal.of_real_mul (le_of_lt $ neg_pos.2 h),
-      measure.map_apply (measurable_const_mul a) measurable_set_Ioo, neg_sub_neg,
-      ← neg_mul_eq_neg_mul, preimage_const_mul_Ioo_of_neg _ _ h, abs_of_neg h, mul_sub,
-      mul_div_cancel' _ (ne_of_lt h)] },
-  { simp only [real.volume_Ioo, measure.smul_apply, ← ennreal.of_real_mul (le_of_lt h),
-      measure.map_apply (measurable_const_mul a) measurable_set_Ioo, preimage_const_mul_Ioo _ _ h,
-      abs_of_pos h, mul_sub, mul_div_cancel' _ (ne_of_gt h)] }
+  rcases lt_trichotomy a b with hab|rfl|hab,
+  { have A : disjoint (Ioo a b) {b}, by simp,
+    have D : f b - f a = (f b - f.left_lim b) + (f.left_lim b - f a), by abel,
+    have := f.measure_Ioc a b,
+    simp only [←Ioo_union_Icc_eq_Ioc hab le_rfl, measure_singleton,
+      measure_union A measurable_set_Ioo (measurable_set_singleton b), Icc_self] at this,
+    rw [D, ennreal.of_real_add, add_comm] at this,
+    { simpa only [ennreal.add_right_inj, ennreal.of_real_lt_top] },
+    { simp only [f.left_lim_le, sub_nonneg] },
+    { simp only [f.le_left_lim hab, sub_nonneg] } },
+  { exact (h rfl).elim },
+  { simp only [hab.le, measure_empty, Ioo_eq_empty],
+    symmetry,
+    simp [ennreal.of_real_eq_zero, f.left_lim_le hab.le] }
 end
 
-lemma map_volume_mul_left {a : ℝ} (h : a ≠ 0) :
-  measure.map ((*) a) volume = ennreal.of_real (abs a⁻¹) • volume :=
-by conv_rhs { rw [← real.smul_map_volume_mul_left h, smul_smul,
-  ← ennreal.of_real_mul (abs_nonneg _), ← abs_mul, inv_mul_cancel h, abs_one, ennreal.of_real_one,
-  one_smul] }
-
-lemma smul_map_volume_mul_right {a : ℝ} (h : a ≠ 0) :
-  ennreal.of_real (abs a) • measure.map (* a) volume = volume :=
-by simpa only [mul_comm] using real.smul_map_volume_mul_left h
-
-lemma map_volume_mul_right {a : ℝ} (h : a ≠ 0) :
-  measure.map (* a) volume = ennreal.of_real (abs a⁻¹) • volume :=
-by simpa only [mul_comm] using real.map_volume_mul_left h
-
-@[simp] lemma map_volume_neg : measure.map has_neg.neg (volume : measure ℝ) = volume :=
-eq.symm $ real.measure_ext_Ioo_rat $ λ p q,
-  by simp [show measure.map has_neg.neg volume (Ioo (p : ℝ) q) = _,
-    from measure.map_apply measurable_neg measurable_set_Ioo]
-
-end real
-
-open_locale topological_space
-
-lemma filter.eventually.volume_pos_of_nhds_real {p : ℝ → Prop} {a : ℝ} (h : ∀ᶠ x in 𝓝 a, p x) :
-  (0 : ℝ≥0∞) < volume {x | p x} :=
+@[simp] lemma measure_Ico (a b : ℝ) : f.measure (Ico a b) = of_real (f.left_lim b - f.left_lim a) :=
 begin
-  rcases h.exists_Ioo_subset with ⟨l, u, hx, hs⟩,
-  refine lt_of_lt_of_le _ (measure_mono hs),
-  simpa [-mem_Ioo] using hx.1.trans hx.2
+  rcases le_or_lt b a with hab|hab,
+  { simp only [hab, measure_empty, Ico_eq_empty],
+    symmetry,
+    simp [ennreal.of_real_eq_zero, f.left_lim_le_left_lim hab] },
+  { have A : disjoint {a} (Ioo a b) := by simp,
+    simp [← Icc_union_Ioo_eq_Ico le_rfl hab, -singleton_union, hab.ne, f.left_lim_le,
+      measure_union A (measurable_set_singleton a) measurable_set_Ioo, f.le_left_lim hab,
+      ← ennreal.of_real_add] }
 end
 
-section region_between
-
-open_locale classical
-
-variable {α : Type*}
-
-/-- The region between two real-valued functions on an arbitrary set. -/
-def region_between (f g : α → ℝ) (s : set α) : set (α × ℝ) :=
-{p : α × ℝ | p.1 ∈ s ∧ p.2 ∈ Ioo (f p.1) (g p.1)}
-
-lemma region_between_subset (f g : α → ℝ) (s : set α) : region_between f g s ⊆ s.prod univ :=
-by simpa only [prod_univ, region_between, set.preimage, set_of_subset_set_of] using λ a, and.left
-
-variables [measurable_space α] {μ : measure α} {f g : α → ℝ} {s : set α}
-
-/-- The region between two measurable functions on a measurable set is measurable. -/
-lemma measurable_set_region_between
-  (hf : measurable f) (hg : measurable g) (hs : measurable_set s) :
-  measurable_set (region_between f g s) :=
-begin
-  dsimp only [region_between, Ioo, mem_set_of_eq, set_of_and],
-  refine measurable_set.inter _ ((measurable_set_lt (hf.comp measurable_fst) measurable_snd).inter
-    (measurable_set_lt measurable_snd (hg.comp measurable_fst))),
-  convert hs.prod measurable_set.univ,
-  simp only [and_true, mem_univ],
-end
-
-theorem volume_region_between_eq_lintegral'
-  (hf : measurable f) (hg : measurable g) (hs : measurable_set s) :
-  μ.prod volume (region_between f g s) = ∫⁻ y in s, ennreal.of_real ((g - f) y) ∂μ :=
-begin
-  rw measure.prod_apply,
-  { have h : (λ x, volume {a | x ∈ s ∧ a ∈ Ioo (f x) (g x)})
-            = s.indicator (λ x, ennreal.of_real (g x - f x)),
-    { funext x,
-      rw indicator_apply,
-      split_ifs,
-      { have hx : {a | x ∈ s ∧ a ∈ Ioo (f x) (g x)} = Ioo (f x) (g x) := by simp [h, Ioo],
-        simp only [hx, real.volume_Ioo, sub_zero] },
-      { have hx : {a | x ∈ s ∧ a ∈ Ioo (f x) (g x)} = ∅ := by simp [h],
-        simp only [hx, measure_empty] } },
-    dsimp only [region_between, preimage_set_of_eq],
-    rw [h, lintegral_indicator];
-    simp only [hs, pi.sub_apply] },
-  { exact measurable_set_region_between hf hg hs },
-end
-
-/-- The volume of the region between two almost everywhere measurable functions on a measurable set
-    can be represented as a stieltjes integral. -/
-theorem volume_region_between_eq_lintegral [sigma_finite μ]
-  (hf : ae_measurable f (μ.restrict s)) (hg : ae_measurable g (μ.restrict s))
-  (hs : measurable_set s) :
-  μ.prod volume (region_between f g s) = ∫⁻ y in s, ennreal.of_real ((g - f) y) ∂μ :=
-begin
-  have h₁ : (λ y, ennreal.of_real ((g - f) y))
-          =ᵐ[μ.restrict s]
-              λ y, ennreal.of_real ((ae_measurable.mk g hg - ae_measurable.mk f hf) y) :=
-    eventually_eq.fun_comp (eventually_eq.sub hg.ae_eq_mk hf.ae_eq_mk) _,
-  have h₂ : (μ.restrict s).prod volume (region_between f g s) =
-    (μ.restrict s).prod volume (region_between (ae_measurable.mk f hf) (ae_measurable.mk g hg) s),
-  { apply measure_congr,
-    apply eventually_eq.inter, { refl },
-    exact eventually_eq.inter
-            (eventually_eq.comp₂ (ae_eq_comp' measurable_fst hf.ae_eq_mk
-              measure.prod_fst_absolutely_continuous) _ eventually_eq.rfl)
-            (eventually_eq.comp₂ eventually_eq.rfl _
-              (ae_eq_comp' measurable_fst hg.ae_eq_mk measure.prod_fst_absolutely_continuous)) },
-  rw [lintegral_congr_ae h₁,
-      ← volume_region_between_eq_lintegral' hf.measurable_mk hg.measurable_mk hs],
-  convert h₂ using 1,
-  { rw measure.restrict_prod_eq_prod_univ,
-    exacts [ (measure.restrict_eq_self_of_subset_of_measurable (hs.prod measurable_set.univ)
-      (region_between_subset f g s)).symm, hs], },
-  { rw measure.restrict_prod_eq_prod_univ,
-    exacts [(measure.restrict_eq_self_of_subset_of_measurable (hs.prod measurable_set.univ)
-      (region_between_subset (ae_measurable.mk f hf) (ae_measurable.mk g hg) s)).symm, hs] },
-end
-
-
-theorem volume_region_between_eq_integral' [sigma_finite μ]
-  (f_int : integrable_on f s μ) (g_int : integrable_on g s μ)
-  (hs : measurable_set s) (hfg : f ≤ᵐ[μ.restrict s] g ) :
-  μ.prod volume (region_between f g s) = ennreal.of_real (∫ y in s, (g - f) y ∂μ) :=
-begin
-  have h : g - f =ᵐ[μ.restrict s] (λ x, real.to_nnreal (g x - f x)),
-  { apply hfg.mono,
-    simp only [real.to_nnreal, max, sub_nonneg, pi.sub_apply],
-    intros x hx,
-    split_ifs,
-    refl },
-  rw [volume_region_between_eq_lintegral f_int.ae_measurable g_int.ae_measurable hs,
-    integral_congr_ae h, lintegral_congr_ae,
-    lintegral_coe_eq_integral _ ((integrable_congr h).mp (g_int.sub f_int))],
-  simpa only,
-end
-
-/-- If two functions are integrable on a measurable set, and one function is less than
-    or equal to the other on that set, then the volume of the region
-    between the two functions can be represented as an integral. -/
-theorem volume_region_between_eq_integral [sigma_finite μ]
-  (f_int : integrable_on f s μ) (g_int : integrable_on g s μ)
-  (hs : measurable_set s) (hfg : ∀ x ∈ s, f x ≤ g x) :
-  μ.prod volume (region_between f g s) = ennreal.of_real (∫ y in s, (g - f) y ∂μ) :=
-volume_region_between_eq_integral' f_int g_int hs
-  ((ae_restrict_iff' hs).mpr (eventually_of_forall hfg))
-
-end region_between
-
-/-
-section vitali
-
-def vitali_aux_h (x : ℝ) (h : x ∈ Icc (0:ℝ) 1) :
-  ∃ y ∈ Icc (0:ℝ) 1, ∃ q:ℚ, ↑q = x - y :=
-⟨x, h, 0, by simp⟩
-
-def vitali_aux (x : ℝ) (h : x ∈ Icc (0:ℝ) 1) : ℝ :=
-classical.some (vitali_aux_h x h)
-
-theorem vitali_aux_mem (x : ℝ) (h : x ∈ Icc (0:ℝ) 1) : vitali_aux x h ∈ Icc (0:ℝ) 1 :=
-Exists.fst (classical.some_spec (vitali_aux_h x h):_)
-
-theorem vitali_aux_rel (x : ℝ) (h : x ∈ Icc (0:ℝ) 1) :
- ∃ q:ℚ, ↑q = x - vitali_aux x h :=
-Exists.snd (classical.some_spec (vitali_aux_h x h):_)
-
-def vitali : set ℝ := {x | ∃ h, x = vitali_aux x h}
-
-theorem vitali_nonmeasurable : ¬ null_measurable_set measure_space.μ vitali :=
-sorry
-
-end vitali
--/
+end stieltjes_function

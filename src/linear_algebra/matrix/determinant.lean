7508d310f30a814e5546a29b8a575d814d68211b
Kevin Buzzard <k.buzzard@imperial.ac.uk>
2021-07-11 22:49:43 +0100

    fix linear_algebra.matrix.determinant

--- src/linear_algebra/matrix/determinant.lean
+++ src/linear_algebra/matrix/determinant.lean
@@ -162,7 +162,7 @@ calc det (M ⬝ N) = ∑ p : n → n, ∑ σ : perm n, ε σ * ∏ i, (M (σ i) 
       by { simp_rw [equiv.coe_mul_right, h], simp only [this] }))
 ... = det M * det N : by simp only [det_apply', finset.mul_sum, mul_comm, mul_left_comm]
 
-instance : is_monoid_hom (det : matrix n n R → R) :=
+lemma det.is_monoid_hom : is_monoid_hom (det : matrix n n R → R) :=
 { map_one := det_one,
   map_mul := det_mul }
 

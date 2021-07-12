750e7e3049a7ae59b2527d6e861dadb74253e06b
Heather Macbeth <25316162+hrmacbeth@users.noreply.github.com>
2021-07-11 23:10:07 -0400

    new Lp induction lemmas

--- src/measure_theory/continuous_map_dense.lean
+++ src/measure_theory/continuous_map_dense.lean
@@ -1,5 +1,5 @@
 import measure_theory.regular
-import measure_theory.bochner_integration
+import measure_theory.simple_func_dense
 import topology.urysohns_lemma
 
 open measure_theory topological_space continuous_map
@@ -39,6 +39,64 @@ begin
 end
 
 variables [normal_space α] [normed_space ℝ E]
+
+/-- A simple function in `Lp` can be approximated in `Lp` by continuous functions. -/
+lemma measure_theory.simple_func.mem_closure_continuous' [_i : fact (1 ≤ p)] [μ.weakly_regular]
+  (hp' : p ≠ ⊤) :
+  ∀ f : Lp E p μ, f ∈ (continuous_and_Lp E p μ).topological_closure :=
+begin
+  refine Lp.induction hp' _ _ _ _,
+  { -- The characteristic function of a measurable set can be approximated by continuous functions
+    intros c s hs hsμ,
+    refine mem_closure_iff_frequently.mpr _,
+    rw metric.nhds_basis_ball.frequently_iff,
+    intros ε' hε',
+    obtain ⟨η, hη_pos, hη_lt⟩ : ∃ η, (0:nnreal) < η ∧ (norm c) * (2 * η) ^ (1 / p.to_real) < ε',
+    { sorry },
+    have hη_pos' : (0 : ℝ≥0∞) < ↑η := by exact_mod_cast hη_pos,
+    -- use the regularity of the measure to approximate `s` by an open superset and a closed subset
+    obtain ⟨u, u_open, su, μu⟩ : ∃ u, is_open u ∧ s ⊆ u ∧ μ u < μ s + ↑η,
+    { refine hs.exists_is_open_lt_of_lt _ _,
+      simpa using (ennreal.add_lt_add_iff_left hsμ).2 hη_pos' },
+    obtain ⟨F, F_closed, Fs, μF⟩ : ∃ F, is_closed F ∧ F ⊆ s ∧ μ s < μ F + ↑η :=
+      hs.exists_lt_is_closed_of_lt_top_of_pos hsμ hη_pos',
+    have : disjoint uᶜ F,
+    { rw [set.disjoint_iff_inter_eq_empty, set.inter_comm, ← set.subset_compl_iff_disjoint],
+      simpa using Fs.trans su },
+    -- apply Urysohn's lemma to get a continuous approximation to the characteristic function of
+    -- the set `s`
+    obtain ⟨g, hg_cont, hgu, hgF, hg_range⟩ :=
+      exists_continuous_zero_one_of_closed u_open.is_closed_compl F_closed this,
+    -- multiply this by `c` to get a continuous approximation to the function `f`
+    have hgc_cont : continuous (λ x, g x • c),
+    { exact continuous_smul.comp (hg_cont.prod_mk continuous_const) },
+    have hgc_ℒp : mem_ℒp (λ x, g x • c) p μ,
+    { have hu_ℒp : mem_ℒp (set.indicator u (λ x, c)) p μ,
+      { refine mem_ℒp_indicator_const p u_open.measurable_set c (or.inr _),
+        sorry },
+      refine hu_ℒp.of_le (hgc_cont.ae_measurable μ) (filter.eventually_of_forall _),
+      intros a,
+      by_cases ha : a ∈ uᶜ,
+      { have : g a = 0 := by simpa using hgu ha,
+        simp [this] },
+      { have : ∥g a∥ ≤ 1,
+        { rw [real.norm_eq_abs, abs_of_nonneg (hg_range a).1],
+          exact (hg_range a).2 },
+        have ha' : a ∈ u := by simpa using ha,
+        simp [ha'],
+        sorry } },
+    refine ⟨mem_ℒp.to_Lp (λ x, g x • c) hgc_ℒp, _, _⟩,
+    { rw mem_ball_iff_norm,
+      -- rw ← mem_ℒp.to_Lp_sub,
+      -- rw Lp.norm_to_Lp,
+      sorry },
+    { rw [set_like.mem_coe, mem_continuous_and_Lp_iff],
+      exact ⟨⟨_, hgc_cont⟩, rfl⟩ } },
+  { -- If two functions in `Lp` with disjoint support can be approximated by continuous functions,
+    -- then so can their sum
+    exact λ f g hf hg hfg', add_subgroup.add_mem _ },
+  { exact add_subgroup.is_closed_topological_closure }
+end
 
 /-- A simple function in `Lp` can be approximated in `Lp` by continuous functions. -/
 lemma measure_theory.simple_func.mem_closure_continuous [_i : fact (1 ≤ p)] [μ.weakly_regular]

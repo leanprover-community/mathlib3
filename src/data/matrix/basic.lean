427ad5eb8a78f7cf0de6d33341a64e86711bee2d
l534zhan <luming.zhang@merton.ox.ac.uk>
2021-07-13 18:27:20 +0100

    change

--- src/data/matrix/basic.lean
+++ src/data/matrix/basic.lean
@@ -62,7 +62,7 @@ def transpose (M : matrix m n α) : matrix n m α
 
 localized "postfix `ᵀ`:1500 := matrix.transpose" in matrix
 
-/-- The conjugate transpose of a matrix. -/
+/-- The conjugate transpose of a matrix defined in term of `star`. -/
 def conj_transpose [has_star α] (M : matrix m n α) : matrix n m α :=
 M.transpose.map star
 
@@ -929,7 +929,7 @@ end conj_transpose
 section star
 
 /-- When `α` has a star operation, square matrices `matrix n n α` have a star
-operation given by taking the transpose, and the star of each entry. -/
+operation equal to `matrix.conj_transpose`. -/
 instance [has_star α] : has_star (matrix n n α) := {star := conj_transpose}
 
 lemma star_eq_conj_transpose [has_star α] (M : matrix m m α) : star M = Mᴴ := rfl
@@ -938,13 +938,9 @@ lemma star_eq_conj_transpose [has_star α] (M : matrix m m α) : star M = Mᴴ :
   (star M) i j = star (M j i) := rfl
 
 instance [has_involutive_star α] : has_involutive_star (matrix n n α) :=
-{ star := λ M, M.transpose.map star,
-  star_involutive := conj_transpose_conj_transpose, }
+{ star_involutive := conj_transpose_conj_transpose }
 
-/--
-When `α` is a `*`-(semi)ring, `matrix n n α` becomes a `*`-(semi)ring with
-the star operation given by taking the conjugate, and the star of each entry.
--/
+/-- When `α` is a `*`-(semi)ring, `matrix.has_star` is also a `*`-(semi)ring. -/
 instance [decidable_eq n] [semiring α] [star_ring α] : star_ring (matrix n n α) :=
 { star_add := conj_transpose_add,
   star_mul := conj_transpose_mul, }

08d1be4595cb851abd90ad8607b5ed6e07b372f2
Apurva Nakade <apurvnakade@gmail.com>
2021-07-13 08:26:38 -0400

    Apply suggestions from code review
    Co-authored-by: Eric Wieser <wieser.eric@gmail.com>

--- src/algebra/group_power/lemmas.lean
+++ src/algebra/group_power/lemmas.lean
@@ -541,32 +541,30 @@ lemma le_self_sq (b : ℤ) : b ≤ b ^ 2 := le_trans (le_nat_abs) (abs_le_self_s
 
 alias int.le_self_sq ← int.le_self_pow_two
 
-lemma pow_right_injective {x : ℤ} (h : 2 ≤ x.nat_abs) : function.injective (λ (n : ℕ), x ^ n) :=
+lemma pow_right_injective {x : ℤ} (h : 2 ≤ x.nat_abs) : function.injective ((^) x : ℕ → ℤ) :=
 λ n m hnm, begin
   obtain pos | neg := nat_abs_eq x,
   { lift x to ℕ using (pos.symm ▸ coe_nat_nonneg _),
-  norm_cast at h hnm,
-  exact nat.pow_right_injective h hnm },
-  dsimp only at hnm,
-  rw [←neg_neg x, neg_pow, neg_pow (-x)] at hnm,
-  replace neg := neg_eq_iff_neg_eq.mp neg.symm,
-  generalize' hy : -x = y,
-  rw [←neg_neg x, nat_abs_neg] at h,
-  rw [hy] at *,
-  lift y to ℕ using (neg.symm ▸ coe_nat_nonneg _),
-  obtain h₁ | h₁ := neg_one_pow_eq_or ℤ n;
-  obtain h₂ | h₂ := neg_one_pow_eq_or ℤ m;
-  all_goals { simp only [h₁, h₂, neg_mul_eq_neg_mul_symm, one_mul, neg_inj] at hnm,
-              norm_cast at h hnm,
-              exact nat.pow_right_injective h hnm <|> exfalso },
-  swap,
-  replace hnm := hnm.symm,
-  all_goals { apply int.ne_neg_of_pos _ _ hnm;
-              norm_cast;
-              apply nat.pos_of_ne_zero;
-              apply pow_ne_zero;
-              apply ne_bot_of_gt;
-              exact nat.succ_le_iff.mp h }
+    norm_cast at h hnm,
+    exact nat.pow_right_injective h hnm },
+  rw [neg, neg_pow _ n, neg_pow _ m, ←coe_nat_pow, ←coe_nat_pow] at hnm,
+  with_cases {
+    cases neg_one_pow_eq_or ℤ n with hn hn;
+    cases neg_one_pow_eq_or ℤ m with hm hm;
+    simp only [hn, hm, neg_mul_eq_neg_mul_symm, neg_inj, one_mul, coe_nat_inj'] at hnm },
+  case [or.inl or.inl, or.inr or.inr] {
+    all_goals { exact nat.pow_right_injective h hnm } },
+  case [or.inr or.inl, or.inl or.inr] {
+    replace hnm := hnm.symm,
+    all_goals {
+      exfalso,
+      apply int.ne_neg_of_pos _ _ hnm,
+      all_goals {
+        norm_cast,
+        apply nat.pos_of_ne_zero,
+        apply pow_ne_zero,
+        apply ne_bot_of_gt,
+        exact nat.succ_le_iff.mp h }, } }
 end
 
 end int

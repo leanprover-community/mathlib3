16e2c2162a137aac1552f4f433ec9c0835a85568
l534zhan <luming.zhang@merton.ox.ac.uk>
2021-07-13 15:48:12 +0100

    change

--- src/data/matrix/basic.lean
+++ src/data/matrix/basic.lean
@@ -1,7 +1,7 @@
 /-
 Copyright (c) 2018 Ellen Arlt. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
-Authors: Ellen Arlt, Blair Shi, Sean Leather, Mario Carneiro, Johan Commelin
+Authors: Ellen Arlt, Blair Shi, Sean Leather, Mario Carneiro, Johan Commelin, Lu-Ming Zhang
 -/
 import algebra.big_operators.pi
 import algebra.module.pi

b751290719d88ff065433f51094d12e0fa1f4038
RemyDegenne <Remydegenne@gmail.com>
2021-07-13 08:58:41 +0200

    docstrings

--- src/measure_theory/l2_space.lean
+++ src/measure_theory/l2_space.lean
@@ -139,6 +139,9 @@ section indicator_const_Lp
 variables [measurable_space 𝕜] [borel_space 𝕜] {s : set α}
 
 variables (𝕜)
+
+/-- The inner product in `L2` of the indicator of a set `indicator_const_Lp 2 hs hμs c` and `f` is
+equal to the integral of the inner product over `s`: `∫ x in s, ⟪c, f x⟫ ∂μ`. -/
 lemma inner_indicator_const_Lp_eq_set_integral_inner (f : Lp E 2 μ) (hs : measurable_set s) (c : E)
   (hμs : μ s ≠ ∞) :
   inner (indicator_const_Lp 2 hs hμs c) f = ∫ x in s, ⟪c, f x⟫ ∂μ :=
@@ -168,13 +171,18 @@ begin
   rw [h_left, h_right, add_zero],
 end
 
+/-- The inner product in `L2` of the indicator of a set `indicator_const_Lp 2 hs hμs c` and `f` is
+equal to the inner product of the constant `c` and the integral of `f` over `s`. -/
 lemma inner_indicator_const_Lp_eq_inner_set_integral [complete_space E] [normed_space ℝ E]
   [is_scalar_tower ℝ 𝕜 E] (hs : measurable_set s) (hμs : μ s ≠ ∞) (c : E) (f : Lp E 2 μ) :
   inner (indicator_const_Lp 2 hs hμs c) f = ⟪c, ∫ x in s, f x ∂μ⟫ :=
 by rw [← integral_inner (integrable_on_Lp_of_measure_ne_top f fact_one_le_two_ennreal.elim hμs),
     L2.inner_indicator_const_Lp_eq_set_integral_inner]
+
 variables {𝕜}
 
+/-- The inner product in `L2` of the indicator of a set `indicator_const_Lp 2 hs hμs (1 : ℝ)` and
+a real function `f` is equal to the integral of `f` over `s`. -/
 lemma inner_indicator_const_Lp_one (hs : measurable_set s) (hμs : μ s ≠ ∞) (f : Lp ℝ 2 μ) :
   inner (indicator_const_Lp 2 hs hμs (1 : ℝ)) f = ∫ x in s, f x ∂μ :=
 by { rw L2.inner_indicator_const_Lp_eq_inner_set_integral ℝ hs hμs (1 : ℝ) f, simp, }

367967902404512188241e6b0d57e9d085799cc7
YaelDillies <yael.dillies@gmail.com>
2021-07-13 23:48:11 +0200

    ./calculus/

--- src/analysis/calculus/extend_deriv.lean
+++ src/analysis/calculus/extend_deriv.lean
@@ -4,7 +4,6 @@ Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Sébastien Gouëzel
 -/
 import analysis.calculus.mean_value
-import analysis.normed_space.bounded_linear_maps
 
 /-!
 # Extending differentiability to the boundary

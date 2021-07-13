8c1bbb2d1b478e63371e6dc5a995acb4d03199d1
Anne Baanen <Vierkantor@users.noreply.github.com>
2021-07-13 15:02:46 +0000

    Golfing
    Co-authored-by: Eric Wieser <wieser.eric@gmail.com>

--- src/algebra/pointwise.lean
+++ src/algebra/pointwise.lean
@@ -413,46 +413,25 @@ instance smul_comm_class {γ : Type*}
   [has_scalar α γ] [has_scalar β γ] [smul_comm_class α β γ] :
   smul_comm_class (set α) (set β) (set γ) :=
 { smul_comm := λ T T' T'', begin
-    ext c,
-    simp only [mem_smul],
-    split,
-    { rintro ⟨a, _, ha, ⟨b, c, hb, hc, rfl⟩, rfl⟩,
-      exact ⟨b, _, hb, ⟨a, c, ha, hc, rfl⟩, (smul_comm a b c).symm⟩ },
-    { rintro ⟨b, _, hb, ⟨a, c, ha, hc, rfl⟩, rfl⟩,
-      exact ⟨a, _, ha, ⟨b, c, hb, hc, rfl⟩, smul_comm a b c⟩ },
+    simp only [←image2_smul, image2_swap _ T],
+    exact image2_assoc (λ b c a, smul_comm a b c),
   end }
 
 instance is_scalar_tower {γ : Type*}
   [has_scalar α β] [has_scalar α γ] [has_scalar β γ] [is_scalar_tower α β γ] :
   is_scalar_tower α β (set γ) :=
-{ smul_assoc := λ a b T,
-    by { ext c, simp only [mem_smul_set, smul_assoc, exists_exists_and_eq_and] } }
+{ smul_assoc := λ a b T, by simp only [←image_smul, image_image, smul_assoc] }
 
 instance is_scalar_tower' {γ : Type*}
   [has_scalar α β] [has_scalar α γ] [has_scalar β γ] [is_scalar_tower α β γ] :
   is_scalar_tower α (set β) (set γ) :=
-{ smul_assoc := λ a T T', begin
-    ext c,
-    simp only [mem_smul_set, mem_smul],
-    split,
-    { rintros ⟨_, c, ⟨b, hb, rfl⟩, hc, rfl⟩,
-      exact ⟨_, ⟨b, c, hb, hc, rfl⟩, (smul_assoc _ _ _).symm⟩ },
-    { rintros ⟨_, ⟨b, c, hb, hc, rfl⟩, rfl⟩,
-      exact ⟨_, c, ⟨b, hb, rfl⟩, hc, smul_assoc _ _ _⟩ }
-  end }
+{ smul_assoc := λ a T T',
+    by simp only [←image_smul, ←image2_smul, image_image2, image2_image_left, smul_assoc] }
 
 instance is_scalar_tower'' {γ : Type*}
   [has_scalar α β] [has_scalar α γ] [has_scalar β γ] [is_scalar_tower α β γ] :
   is_scalar_tower (set α) (set β) (set γ) :=
-{ smul_assoc := λ T T' T'', begin
-    ext c,
-    simp only [mem_smul],
-    split,
-    { rintros ⟨_, c, ⟨a, b, ha, hb, rfl⟩, hc, rfl⟩,
-      exact ⟨a, _, ha, ⟨b, c, hb, hc, rfl⟩, (smul_assoc a b c).symm⟩ },
-    { rintros ⟨a, _, ha, ⟨b, c, hb, hc, rfl⟩, rfl⟩,
-      exact ⟨_, c, ⟨a, b, ha, hb, rfl⟩, hc, smul_assoc a b c⟩ }
-  end }
+{ smul_assoc := λ T T' T'', image2_assoc smul_assoc }
 
 section monoid
 

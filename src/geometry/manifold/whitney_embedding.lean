5cbd03ce2dc0830b1239a952532ac6af2b2f114d
Yury G. Kudryashov <urkud@urkud.name>
2021-07-14 05:16:52 -0400

    Squash

--- src/geometry/manifold/whitney_embedding.lean
+++ src/geometry/manifold/whitney_embedding.lean
@@ -1,30 +1,29 @@
 /-
-Copyright (c) 2021 Yury G. Kudryashov. All rights reserved.
+Copyright (c) 2021 Yury Kudryashov. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
-Authors: Yury G. Kudryashov
+Authors: Yury Kudryashov
 -/
 import geometry.manifold.partition_of_unity
 
 /-!
 # Whitney embedding theorem
 
-In this file we prove a version of the Whitney embedding theorem: for any compact real manifold `M`,
-for sufficiently large `n` there exists a smooth embedding `M → ℝ^n`.
+In this file we prove a “baby” version of the Whitney weak embedding theorem: for any compact real
+manifold `M`, for sufficiently large `n` there exists a smooth embedding `M → ℝ^n`. The actual
+theorem does not assume that `M` is a compact space and provides an explicit `n`.
 
 ## TODO
 
-* Prove the weak Whitney embedding theorem: any `σ`-compact smooth `m`-dimensional manifold can be
-  embedded into `ℝ^(2m+1)`. This requires a version of Sard's theorem: for a locally Lipschitz
-  continuous map `f : ℝ^m → ℝ^n`, `m < n`, the range has Hausdorff dimension at most `m`, hence it
-  has measure zero.
+* Drop unneeded assumptions.
+* Formalize the strong embedding theorem.
 
 ## Tags
 
-partition of unity, smooth bump function, whitney theorem
+manifold, embedding, immersion
 -/
 
-universes uE uF uH uM
-variables
+universes uι uE uH uM
+variables {ι : Type uι}
 {E : Type uE} [normed_group E] [normed_space ℝ E] [finite_dimensional ℝ E]
 {H : Type uH} [topological_space H] {I : model_with_corners ℝ E H}
 {M : Type uM} [topological_space M] [charted_space H M] [smooth_manifold_with_corners I M]
@@ -36,72 +35,75 @@ noncomputable theory
 
 namespace smooth_bump_covering
 
-variables {s : set M} {U : M → set M} (fs : smooth_bump_covering I s)
+/-!
+### Whitney embedding theorem
 
-variables [t2_space M] [fintype fs.ι] (f : smooth_bump_covering I (univ : set M))
-  [fintype f.ι]
+In this section we prove a version of the Whitney embedding theorem: for any compact real manifold
+`M`, for sufficiently large `n` there exists a smooth embedding `M → ℝ^n`.
+-/
+
+variables [t2_space M] {s : set M} (f : smooth_bump_covering I M s) [fintype f.ι]
 
 /-- Smooth embedding of `M` into `(E × ℝ) ^ f.ι`. -/
-def embedding_pi_tangent : C^∞⟮I, M; 𝓘(ℝ, fs.ι → (E × ℝ)), fs.ι → (E × ℝ)⟯ :=
-{ to_fun := λ x i, (fs i x • ext_chart_at I (fs.c i) x, fs i x),
+def embedding_pi_tangent : C^∞⟮I, M; 𝓘(ℝ, f.ι → (E × ℝ)), f.ι → (E × ℝ)⟯ :=
+{ to_fun := λ x i, (f i x • ext_chart_at I (f.c i) x, f i x),
   times_cont_mdiff_to_fun := times_cont_mdiff_pi_space.2 $ λ i,
-    ((fs i).smooth_smul times_cont_mdiff_on_ext_chart_at).prod_mk_space ((fs i).smooth) }
+    ((f i).smooth_smul times_cont_mdiff_on_ext_chart_at).prod_mk_space ((f i).smooth) }
 
 local attribute [simp] lemma embedding_pi_tangent_coe :
-  ⇑fs.embedding_pi_tangent = λ x i, (fs i x • ext_chart_at I (fs.c i) x, fs i x) :=
+  ⇑f.embedding_pi_tangent = λ x i, (f i x • ext_chart_at I (f.c i) x, f i x) :=
 rfl
 
-lemma embedding_pi_tangent_inj_on : inj_on fs.embedding_pi_tangent s :=
+lemma embedding_pi_tangent_inj_on : inj_on f.embedding_pi_tangent s :=
 begin
   intros x hx y hy h,
   simp only [embedding_pi_tangent_coe, funext_iff] at h,
-  obtain ⟨h₁, h₂⟩ := prod.mk.inj_iff.1 (h (fs.ind x hx)),
-  rw [fs.apply_ind x hx] at h₂,
-  rw [← h₂, fs.apply_ind x hx, one_smul, one_smul] at h₁,
-  have := fs.mem_ext_chart_at_source_of_eq_one h₂.symm,
-  exact (ext_chart_at I (fs.c _)).inj_on (fs.mem_ext_chart_at_ind_source x hx) this h₁
+  obtain ⟨h₁, h₂⟩ := prod.mk.inj_iff.1 (h (f.ind x hx)),
+  rw [f.apply_ind x hx] at h₂,
+  rw [← h₂, f.apply_ind x hx, one_smul, one_smul] at h₁,
+  have := f.mem_ext_chart_at_source_of_eq_one h₂.symm,
+  exact (ext_chart_at I (f.c _)).inj_on (f.mem_ext_chart_at_ind_source x hx) this h₁
 end
 
-lemma embedding_pi_tangent_injective :
+lemma embedding_pi_tangent_injective (f : smooth_bump_covering I M) [fintype f.ι] :
   injective f.embedding_pi_tangent :=
 injective_iff_inj_on_univ.2 f.embedding_pi_tangent_inj_on
 
 lemma comp_embedding_pi_tangent_mfderiv (x : M) (hx : x ∈ s) :
   ((continuous_linear_map.fst ℝ E ℝ).comp
-    (@continuous_linear_map.proj ℝ _ fs.ι (λ _, E × ℝ) _ _
-      (λ _, infer_instance) (fs.ind x hx))).comp
-      (mfderiv I 𝓘(ℝ, fs.ι → (E × ℝ)) fs.embedding_pi_tangent x) =
-  mfderiv I I (chart_at H (fs.c (fs.ind x hx))) x :=
+    (@continuous_linear_map.proj ℝ _ f.ι (λ _, E × ℝ) _ _
+      (λ _, infer_instance) (f.ind x hx))).comp
+      (mfderiv I 𝓘(ℝ, f.ι → (E × ℝ)) f.embedding_pi_tangent x) =
+  mfderiv I I (chart_at H (f.c (f.ind x hx))) x :=
 begin
   set L := ((continuous_linear_map.fst ℝ E ℝ).comp
-    (@continuous_linear_map.proj ℝ _ fs.ι (λ _, E × ℝ) _ _ (λ _, infer_instance) (fs.ind x hx))),
-  have := (L.has_mfderiv_at.comp x (fs.embedding_pi_tangent.mdifferentiable_at.has_mfderiv_at)),
+    (@continuous_linear_map.proj ℝ _ f.ι (λ _, E × ℝ) _ _ (λ _, infer_instance) (f.ind x hx))),
+  have := L.has_mfderiv_at.comp x f.embedding_pi_tangent.mdifferentiable_at.has_mfderiv_at,
   convert has_mfderiv_at_unique this _,
-  refine (has_mfderiv_at_ext_chart_at I (fs.mem_chart_at_ind_source x hx)).congr_of_eventually_eq _,
-  refine (fs.eventually_eq_one x hx).mono (λ y hy, _),
+  refine (has_mfderiv_at_ext_chart_at I (f.mem_chart_at_ind_source x hx)).congr_of_eventually_eq _,
+  refine (f.eventually_eq_one x hx).mono (λ y hy, _),
   simp only [embedding_pi_tangent_coe, continuous_linear_map.coe_comp', (∘),
     continuous_linear_map.coe_fst', continuous_linear_map.proj_apply],
   rw [hy, pi.one_apply, one_smul]
 end
 
 lemma embedding_pi_tangent_ker_mfderiv (x : M) (hx : x ∈ s) :
-  (mfderiv I 𝓘(ℝ, fs.ι → (E × ℝ)) fs.embedding_pi_tangent x).ker = ⊥ :=
+  (mfderiv I 𝓘(ℝ, f.ι → (E × ℝ)) f.embedding_pi_tangent x).ker = ⊥ :=
 begin
   apply bot_unique,
-  rw [← (mdifferentiable_chart I (fs.c (fs.ind x hx))).ker_mfderiv_eq_bot
-    (fs.mem_chart_at_ind_source x hx), ← comp_embedding_pi_tangent_mfderiv],
+  rw [← (mdifferentiable_chart I (f.c (f.ind x hx))).ker_mfderiv_eq_bot
+    (f.mem_chart_at_ind_source x hx), ← comp_embedding_pi_tangent_mfderiv],
   exact linear_map.ker_le_ker_comp _ _
 end
 
 lemma embedding_pi_tangent_injective_mfderiv (x : M) (hx : x ∈ s) :
-  injective (mfderiv I 𝓘(ℝ, fs.ι → (E × ℝ)) fs.embedding_pi_tangent x) :=
-linear_map.ker_eq_bot.1 (fs.embedding_pi_tangent_ker_mfderiv x hx)
+  injective (mfderiv I 𝓘(ℝ, f.ι → (E × ℝ)) f.embedding_pi_tangent x) :=
+linear_map.ker_eq_bot.1 (f.embedding_pi_tangent_ker_mfderiv x hx)
 
 /-- Baby version of the Whitney weak embedding theorem: if `M` admits a finite covering by
 supports of bump functions, then for some `n` it can be immersed into the `n`-dimensional
 Euclidean space. -/
-lemma exists_immersion_finrank (f : smooth_bump_covering I (univ : set M))
-  [fintype f.ι] :
+lemma exists_immersion_euclidean (f : smooth_bump_covering I M) [fintype f.ι] :
   ∃ (n : ℕ) (e : M → euclidean_space ℝ (fin n)), smooth I (𝓡 n) e ∧
     injective e ∧ ∀ x : M, injective (mfderiv I (𝓡 n) e x) :=
 begin
@@ -122,11 +124,11 @@ end smooth_bump_covering
 /-- Baby version of the Whitney weak embedding theorem: if `M` admits a finite covering by
 supports of bump functions, then for some `n` it can be embedded into the `n`-dimensional
 Euclidean space. -/
-lemma exists_embedding_finrank_of_compact [t2_space M] [compact_space M] :
+lemma exists_embedding_euclidean_of_compact [t2_space M] [compact_space M] :
   ∃ (n : ℕ) (e : M → euclidean_space ℝ (fin n)), smooth I (𝓡 n) e ∧
     closed_embedding e ∧ ∀ x : M, injective (mfderiv I (𝓡 n) e x) :=
 begin
-  rcases (smooth_bump_covering.choice I M).exists_immersion_finrank
+  rcases (smooth_bump_covering.choice I M).exists_immersion_euclidean
     with ⟨n, e, hsmooth, hinj, hinj_mfderiv⟩,
   exact ⟨n, e, hsmooth, hsmooth.continuous.closed_embedding hinj, hinj_mfderiv⟩
 end

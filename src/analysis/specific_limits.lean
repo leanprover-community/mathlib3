862934c61589654c2a2a94804e73bcc067dec6ed
YaelDillies <yael.dillies@gmail.com>
2021-07-17 00:31:52 +0200

    fix trigonometric

--- src/analysis/specific_limits.lean
+++ src/analysis/specific_limits.lean
@@ -3,8 +3,8 @@ Copyright (c) 2017 Johannes Hölzl. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Johannes Hölzl
 -/
-import analysis.asymptotics.asymptotics
 import algebra.geom_sum
+import analysis.asymptotics.asymptotics
 import order.filter.archimedean
 import order.iterate
 import topology.instances.ennreal

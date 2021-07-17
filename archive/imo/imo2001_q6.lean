c2e186d76c5b5f6e34f901c804a4d7413ce295bf
saradiazr11 <78075706+saradiazr11@users.noreply.github.com>
2021-07-17 09:19:13 +0200

    Update archive/imo/imo2001_q6.lean
    Co-authored-by: Bryan Gin-ge Chen <bryangingechen@gmail.com>

--- archive/imo/imo2001_q6.lean
+++ archive/imo/imo2001_q6.lean
@@ -38,8 +38,8 @@ variables {a b c d : ℤ}
 -/
 
 lemma equivalent_sums
-  (h : a*c + b*d = (a + b - c + d) * (-a + b + c + d))
-  : b^2 + b*d + d^2 = a^2 - a*c + c^2 :=
+  (h : a*c + b*d = (a + b - c + d) * (-a + b + c + d)) :
+  b^2 + b*d + d^2 = a^2 - a*c + c^2 :=
 begin
   have h1: (a + b - c + d) * (-a + b + c + d) =
            -a^2  + b^2  + a*c - c^2 + b*d + d^2 + a*c + b*d,

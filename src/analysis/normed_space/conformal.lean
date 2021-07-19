3c6b6531d4482fa92df32d925bd827671e8dfb39
Yourong "DZR" Zang <yourongzang@outlook.com>
2021-07-19 12:40:34 +0000

    put an 's' after 'Author'

--- src/analysis/normed_space/conformal.lean
+++ src/analysis/normed_space/conformal.lean
@@ -1,7 +1,7 @@
 /-
 Copyright (c) 2021 Yourong Zang. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
-Author: Yourong Zang
+Authors: Yourong Zang
 -/
 import geometry.euclidean.basic
 import geometry.manifold.charted_space

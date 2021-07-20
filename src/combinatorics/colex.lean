06591905c8e1d4302450d53e089511f6dbe4f549
Bhavik Mehta <bhavikmehta8@gmail.com>
2021-07-20 15:36:17 +0100

    fix

--- src/combinatorics/colex.lean
+++ src/combinatorics/colex.lean
@@ -328,8 +328,9 @@ begin
   apply empty_to_colex_le
 end
 
+/-- The function from finsets to finsets with the colex order is a relation homomorphism. -/
 @[simps]
-def to_colex_order_emb [linear_order α] :
+def to_colex_rel_hom [linear_order α] :
   ((⊆) : finset α → finset α → Prop) →r ((≤) : finset.colex α → finset.colex α → Prop) :=
 { to_fun := finset.to_colex,
   map_rel' := λ A B, colex_le_of_subset }
@@ -346,6 +347,7 @@ noncomputable instance [linear_order α] : semilattice_inf_bot (finset.colex α)
 noncomputable instance [linear_order α] : semilattice_sup_bot (finset.colex α) :=
 { ..finset.colex.order_bot,
   ..(by apply_instance : semilattice_sup (finset.colex α)) }
+
 /-- For subsets of ℕ, we can show that colex is equivalent to binary. -/
 lemma sum_two_pow_lt_iff_lt (A B : finset ℕ) :
   ∑ i in A, 2^i < ∑ i in B, 2^i ↔ A.to_colex < B.to_colex :=

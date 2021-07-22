96f685722db3aebd1d08e7f6c61aab1bfe77ced3
Anatole Dedecker <anatolededecker@gmail.com>
2021-07-22 16:27:10 +0200

    A monotone function is interval_integrable

--- src/measure_theory/interval_integral.lean
+++ src/measure_theory/interval_integral.lean
@@ -337,6 +337,22 @@ begin
   exact (integrable_on_compact_of_monotone_on is_compact_interval hu).mono_set Ioc_subset_Icc_self,
 end
 
+lemma interval_integrable_of_antimono_on {u : ι → E} {a b : ι}
+  (hu : ∀ ⦃x y⦄, x ∈ interval a b → y ∈ interval a b → x ≤ y → u y ≤ u x) :
+  interval_integrable u μ a b :=
+@interval_integrable_of_monotone_on (order_dual E) _ ‹_› ι _ _ _ _ _ _ _ _ _ ‹_› ‹_› u a b hu
+
+lemma interval_integrable_of_monotone {u : ι → E} {a b : ι} (hu : monotone u) :
+  interval_integrable u μ a b :=
+interval_integrable_of_monotone_on (λ x y _ _ hxy, hu hxy)
+
+alias interval_integrable_of_monotone ← monotone.interval_integrable
+
+lemma interval_integrable_of_antimono {u : ι → E} {a b : ι}
+  (hu : ∀ ⦃x y⦄, x ≤ y → u y ≤ u x) :
+  interval_integrable u μ a b :=
+@interval_integrable_of_monotone (order_dual E) _ ‹_› ι _ _ _ _ _ _ _ _ _ ‹_› ‹_› u a b hu
+
 end
 
 /-- Let `l'` be a measurably generated filter; let `l` be a of filter such that each `s ∈ l'`

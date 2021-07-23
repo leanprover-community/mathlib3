1bcaa9d039a69f18ca40dba76d336bd68eef72e8
Nicholas Tan <nicholas_tan@apple.com>
2021-07-22 18:37:47 -0700

    using count partition

--- src/group_theory/class_equation.lean
+++ src/group_theory/class_equation.lean
@@ -3,12 +3,14 @@ Copyright (c) 2021 Nicholas Tan. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Nicholas Tan
 -/
+
+import set_theory.fincard
 import group_theory.group_action
-import group_theory.quotient_group
-import group_theory.order_of_element
-import data.zmod.basic
-import data.fintype.card
-import data.list.rotate
+import data.setoid.partition
+import data.setoid.count_partition
+import data.fintype.basic
+import algebra.big_operators
+
 
 /-!
 # Class Equation
@@ -25,7 +27,11 @@ of a group and `[G : H]` is the index of `H` in `G`
 
 * prove/find/import that orbits are partition of a set
       because they define an equivalence relation `orbit_rel`
-* prove that orbit is either fixed point or not
+* conclude that summing size of each orbit will give size of set
+
+* prove that orbit is either fixed point or not fixed
+* fixed points have orbit of size 1
+* sum of all orbits is number of fixed points + sum of card of nonfixed orbits
 
 * use group action of conjugation on group
 
@@ -33,28 +39,24 @@ of a group and `[G : H]` is the index of `H` in `G`
 
 open equiv fintype finset mul_action function
 open equiv.perm subgroup list quotient_group
-open_locale big_operators
+open_locale classical big_operators
 universes u v w
-variables {G : Type u} {α : Type v} {β : Type w} [group G]
+variables {G : Type u} {α : Type v} {β : Type w} [group G] [fintype α] (S : setoid α)
 
 local attribute [instance, priority 10] subtype.fintype set_fintype classical.prop_decidable
 
-namespace mul_action
 variables [mul_action G α]
 
--- lemma orbits_partition
---   [fintype α] [fintype G] :
+def orbits [fintype α] [fintype G] : setoid α := orbit_rel G α
 
--- begin
---   sorry,
--- end
+namespace mul_action
 
--- lemma card_set_is_card_orbits
---   [fintype α] [fintype G] :
---   card α = ∑ :=
--- begin
---   sorry,
--- end
+lemma card_set_is_sum_card_orbits
+  [fintype α] [fintype G] :
+  card α = ∑ orb in (orbit_rel G α).classes.to_finset, (orb : set α).to_finset.card :=
+begin
+  exact card_set_is_sum_card_classes (orbit_rel G α),
+end
 
 /-- **Class Equation for a Group Action** -/
 theorem card_set_is_card_fixed_add_card_orbits
@@ -65,7 +67,7 @@ begin
 end
 
 /-- **Class equation for a Group** -/
-theorem card_group_is_card_center_add_conj [fintype G] :
+theorem card_group_is_card_center_add_card_conj [fintype G] :
   card G = card (center G) :=
 begin
   sorry,

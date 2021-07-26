0a49036dc27a7ae3eb6d516495f216015637cfbe
Mantas Baksys <baksysmantas@gmail.com>
2021-07-26 23:17:13 +0100

    proof golf and explanations

--- archive/imo/imo2021_q1.lean
+++ archive/imo/imo2021_q1.lean
@@ -24,37 +24,33 @@ begin
   linarith,
 end
 
-lemma upper_bound (n : ℕ) (l : ℤ): l ≥ 0 → (l : ℝ) ≤ sqrt (1 + n) - 1 → 2 * l * l + 4 * l ≤ 2 * n :=
+lemma upper_bound (n : ℕ) (l : ℤ) (hl₁ : l ≥ 0) (hl₂ : (l : ℝ) ≤ sqrt (1 + n) - 1):
+2 * l * l + 4 * l ≤ 2 * n :=
 begin
-  intros hl₁ hl₂,
   have h₁ : (l : ℝ) + 1 ≤ sqrt (1 + n) := by linarith,
-  rw mul_self_le_mul_self_iff at h₁,
-  rw ← sqrt_mul at h₁,
-  rw sqrt_mul_self at h₁,
+  rw le_sqrt at h₁,
   ring_exp at h₁,
-  norm_num at h₁,
-  have : 2 * ((l : ℝ) * 2 + l ^ 2) ≤ 2 * n := by linarith,
-  norm_cast at h₁,
+  norm_cast at h₁, push_cast at h₁,
   linarith,
   repeat {norm_cast, linarith},
-  apply sqrt_nonneg,
 end
 
 
-lemma radical_inequality {n : ℕ} (h: n ≥ 107) : sqrt (4 + 2 * n) ≤ 2 * (sqrt (1 + n) - 3) :=
+lemma radical_inequality {n : ℕ} (h : n ≥ 107) : sqrt (4 + 2 * n) ≤ 2 * (sqrt (1 + n) - 3) :=
 begin
-  rw mul_self_le_mul_self_iff,
+  rw sqrt_le_iff,
+  split,
+  { norm_num,
+    rw le_sqrt,
+    repeat {norm_cast, linarith}},
   ring_exp,
-  simp only [pow_two, mul_neg_eq_neg_mul_symm],
-  repeat {rw ← sqrt_mul, rw sqrt_mul_self},
-  ring_nf,
+  rw [pow_two, ← sqrt_mul, sqrt_mul_self],
   suffices : 24 * sqrt (1 + n) ≤ 2 * n + 36,
   linarith,
   rw mul_self_le_mul_self_iff,
   ring_exp,
-  rw pow_two,
-  repeat {rw ← sqrt_mul, rw sqrt_mul_self},
-  ring_exp,
+  rw [pow_two, ← sqrt_mul, sqrt_mul_self],
+  --Not splitting into cases lead to a deterministic timeout on my machine
   by_cases p: n ≥ 108,
   norm_cast,
   nlinarith,
@@ -63,19 +59,14 @@ begin
   norm_num,
   repeat {norm_cast, linarith},
   repeat {norm_num, apply sqrt_nonneg},
-  suffices : 3 ≤ sqrt (1 + n),
-  linarith,
-  rw mul_self_le_mul_self_iff,
-  repeat {rw ← sqrt_mul, rw sqrt_mul_self},
-  repeat {norm_cast, linarith},
-  apply sqrt_nonneg,
 end
 
-
-lemma exists_numbers_in_interval (n : ℕ): n ≥ 107 → ∃ (l : ℤ), l ≥ 0 ∧
+/-We will later make use of the fact that there exists (l : ℕ) such that
+n ≤ 2 * l * l - 4 * l and 2 * l * l + 4 * l ≤ 2 * n for n ≥ 107.
+-/
+lemma exists_numbers_in_interval (n : ℕ) (hn : n ≥ 107): ∃ (l : ℤ), l ≥ 0 ∧
 ((n : ℤ) + 4 * l ≤ 2 * l * l ∧ 2 * l * l + 4 * l ≤ 2 * n) :=
 begin
-  intros hn,
   suffices : ∃ (l : ℤ), l ≥ 0 ∧ 2 * (l : ℝ) ≥ 2 + sqrt (4 + 2 * n) ∧ (l : ℝ) ≤ sqrt (1 + n) - 1,
   cases this with l t,
   use l,
@@ -85,14 +76,10 @@ begin
   { simp only [ge_iff_le],
     rw floor_nonneg,
     norm_num,
-    rw mul_self_le_mul_self_iff,
-    repeat {rw ← sqrt_mul, rw sqrt_mul_self},
-    repeat {norm_cast, linarith},
-    apply sqrt_nonneg },
+    rw le_sqrt,
+    repeat {norm_cast, linarith}},
   refine ⟨_, floor_le (sqrt (1 + n) - 1)⟩,
   { suffices : 2 * (sqrt (1 + n) - 2) ≥ 2 + sqrt (4 + 2 * n),
-    simp only [ge_iff_le] at this,
-    simp only [ge_iff_le],
     apply le_trans this _,
     simp only [mul_le_mul_left, zero_lt_bit0, zero_lt_one],
     suffices : sqrt (1 + n) - 1 ≤ ↑⌊sqrt (1 + n) - 1⌋ + 1,
@@ -102,7 +89,6 @@ begin
     exact ceil_le_floor_add_one _,
     apply le_trans _ t,
     exact le_ceil (sqrt (1 + ↑n) - 1),
-    simp only [ge_iff_le],
     suffices : sqrt (4 + 2 * n) ≤ 2 * (sqrt (1 + n) - 3),
     linarith,
     exact radical_inequality hn },
@@ -126,10 +112,12 @@ Therefore, it is enough to show that there exists a natural number l such that
 n ≤ 2 * l * l - 4 * l and 2 * l * l + 4 * l ≤ 2 * n for n ≥ 100.
 -/
 
-lemma exists_triplet_summing_to_squares (n : ℕ): n ≥ 100 → (∃ (a b c : ℤ), (n : ℤ) ≤ a ∧ a < b ∧ b < c ∧ c ≤ 2 * n
-∧ (∃ (k : ℤ), a + b = k * k) ∧ (∃ (l : ℤ), a + c = l * l) ∧ (∃ (m : ℤ), b + c = m * m)) :=
+lemma exists_triplet_summing_to_squares (n : ℕ) (hn : n ≥ 100): (∃ (a b c : ℤ),
+(n : ℤ) ≤ a ∧ a < b ∧ b < c ∧ c ≤ 2 * n ∧  (∃ (k : ℤ), a + b = k * k) ∧
+(∃ (l : ℤ), a + c = l * l) ∧ (∃ (m : ℤ), b + c = m * m)) :=
 begin
-  intros hn,
+  --If n ≥ 107, we do not explicitly construct the triplet but use an existence
+  --argument from lemma above.
   by_cases p : n ≥ 107,
   have h := exists_numbers_in_interval n p,
   cases h with l hl,
@@ -148,6 +136,9 @@ begin
     ring_exp_eq },
   { use(2 * l + 1),
     ring_exp_eq },
+    --Otherwise, if 100 ≤ n < 107, then it suffices to consider explicit
+    --construction of a triplet {a, b, c}, which is constructed by setting l=9
+    --in the argument above this lemma.
   { use 126,
     use 163,
     use 198,
@@ -161,46 +152,36 @@ begin
     linarith },
 end
 
-lemma mem_compl_iff_nmem_subset {A : finset ℕ} {a b c n : ℕ} (t : A ⊆ finset.range(2 * n + 1)\finset.range n) (h₁ : (n : ℤ) ≤ a)
-(h₂ : (a : ℤ) < b) (h₃ : (b : ℤ) < c) (h₄ : (c : ℤ) ≤ 2 * n) : (a ∉ A ↔ a ∈ (finset.range (2 * n + 1)\finset.range n)\A)
-∧ (b ∉ A ↔ b ∈ (finset.range (2 * n + 1)\finset.range n)\A) ∧ (c ∉ A ↔ c ∈ (finset.range (2 * n + 1)\finset.range n)\A) :=
+lemma mem_compl_iff_nmem_subset {A : finset ℕ} {a b c n : ℕ}
+(t : A ⊆ finset.range(2 * n + 1)\finset.range n) (h₁ : (n : ℤ) ≤ a)
+(h₂ : (a : ℤ) < b) (h₃ : (b : ℤ) < c) (h₄ : (c : ℤ) ≤ 2 * n) :
+(a ∉ A ↔ a ∈ (finset.range (2 * n + 1)\finset.range n)\A)
+∧ (b ∉ A ↔ b ∈ (finset.range (2 * n + 1)\finset.range n)\A) ∧
+(c ∉ A ↔ c ∈ (finset.range (2 * n + 1)\finset.range n)\A) :=
 begin
   norm_cast at h₁,
   norm_cast at h₂,
   norm_cast at h₃,
   norm_cast at h₄,
-  split,
-  split,
+  refine ⟨⟨_,_⟩, ⟨⟨_,_⟩,⟨_,_⟩⟩⟩,
   { intro ha,
     simp only [finset.mem_sdiff, finset.mem_range],
-    split,
-    split,
-    linarith,
-    linarith,
-    exact ha },
+    refine ⟨⟨_,_⟩,ha⟩,
+    repeat {linarith}},
   { intro ha,
     simp only [finset.mem_sdiff, finset.mem_range] at ha,
     exact ha.2 },
-  split,
-  split,
   { intro hb,
     simp only [finset.mem_sdiff, finset.mem_range],
-    split,
-    split,
-    linarith,
-    linarith,
-    exact hb },
+    refine ⟨⟨_,_⟩,hb⟩,
+    repeat {linarith}},
   { intro hb,
     simp only [finset.mem_sdiff, finset.mem_range] at hb,
     exact hb.2 },
-  split,
    {intro hc,
     simp only [finset.mem_sdiff, finset.mem_range],
-    split,
-    split,
-    linarith,
-    linarith,
-    exact hc },
+    refine ⟨⟨_,_⟩,hc⟩,
+    repeat {linarith}},
   { intro hc,
     simp only [finset.mem_sdiff, finset.mem_range] at hc,
     exact hc.2 },
@@ -212,7 +193,8 @@ theorem IMO_2021_Q1 : ∀ (n : ℕ), n ≥ 100 → ∀ (A ⊆ finset.range(2 * n
 (∃ (a b: (finset.range (2 * n + 1)\finset.range n)\A), a ≠ b ∧ ∃(k : ℤ), (a : ℤ) + b=k * k) :=
 begin
   intros n hn A hA,
-  -- There exists a pairwise unequal triplet a, b, c ∈ [n, 2n] such that all pairwise sums are perfect squares
+  -- There exists a pairwise unequal triplet a, b, c ∈ [n, 2n]
+  --such that all pairwise sums are perfect squares.
   have p := exists_triplet_summing_to_squares n hn,
   rcases p with ⟨a, b, c, ⟨han, hba, hcb, hnc, p⟩⟩,
   lift a to ℕ,

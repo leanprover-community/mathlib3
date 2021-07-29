c2a59fc5887d8083f73e372eef43fc62b37636af
Chris Birkbeck <c.birkbeck@ucl.ac.uk>
2021-07-29 17:26:40 +0100

    typo

--- src/linear_algebra/general_linear_group.lean
+++ src/linear_algebra/general_linear_group.lean
@@ -3,7 +3,7 @@ Copyright (c) 2020 Anne Baanen. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Chris Birkbeck
 
-The Genera Linear group $GL(n, R)$
+The General Linear group $GL(n, R)$
 -/
 import linear_algebra.matrix
 import linear_algebra.matrix.nonsingular_inverse

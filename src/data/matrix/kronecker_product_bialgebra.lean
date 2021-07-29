1acb7cdbde5ac8fcc5da3f7fef2a87a26e8e6a24
Eric Wieser <wieser.eric@gmail.com>
2021-07-29 13:47:40 +0100

    Suggestions for PR 8152 (#8462)
    * wip
    
    * Finish
    
    * Finish

--- src/data/matrix/kronecker_product_bialgebra.lean
+++ src/data/matrix/kronecker_product_bialgebra.lean
@@ -28,33 +28,89 @@ namespace matrix_bialgebra
 open algebra matrix function
 open_locale matrix big_operators
 
-variables (α : Type*) [comm_semiring α]
-variables (R : Type*) [comm_semiring R]
-variables (S : Type*) [comm_semiring S]
-variables (β : Type*) [comm_semiring β]
+section
+
+variables {R α β γ : Type*} {l m n p : Type*} [fintype l] [fintype m] [fintype n] [fintype p]
+
+/-- Produce a matrix with `f` applied to every pair of elements from `A` and `B` -/
+@[simp] def kronecker_map (f : α → β → γ) (A : matrix l m α) (B : matrix n p β) :
+  matrix (l × n) (m × p) γ
+| i j := f (A i.1 j.1) (B i.2 j.2)
+
+lemma kronecker_map_zero_left [has_zero α] [has_zero γ] (f : α → β → γ) (hf : ∀ b, f 0 b = 0)
+  (B : matrix n p β) :
+  kronecker_map f (0 : matrix l m α) B = 0:=
+ext $ λ i j,hf _
+
+lemma kronecker_map_zero_right [has_zero β] [has_zero γ] (f : α → β → γ) (hf : ∀ a, f a 0 = 0)
+  (A : matrix l m α) :
+  kronecker_map f A (0 : matrix n p β) = 0 :=
+ext $ λ i j, hf _
+
+lemma kronecker_map_add_left [has_add α] [has_add γ] (f : α → β → γ)
+  (hf : ∀ a₁ a₂ b, f (a₁ + a₂) b = f a₁ b + f a₂ b)
+  (A₁ A₂ : matrix l m α) (B : matrix n p β) :
+  kronecker_map f (A₁ + A₂) B = kronecker_map f A₁ B + kronecker_map f A₂ B :=
+ext $ λ i j, hf _ _ _
+
+lemma kronecker_map_add_right [has_add β] [has_add γ] (f : α → β → γ)
+  (hf : ∀ a b₁ b₂, f a (b₁ + b₂) = f a b₁ + f a b₂)
+  (A : matrix l m α) (B₁ B₂ : matrix n p β) :
+  kronecker_map f A (B₁ + B₂) = kronecker_map f A B₁ + kronecker_map f A B₂ :=
+ext $ λ i j, hf _ _ _
+
+lemma kronecker_map_smul_left [has_scalar R α] [has_scalar R γ] (f : α → β → γ)
+  (hf : ∀ (r : R) a b, f (r • a) b = r • f a b) (r : R)
+  (A : matrix l m α) (B : matrix n p β) :
+  kronecker_map f (r • A) B = r • kronecker_map f A B :=
+ext $ λ i j, hf _ _ _
+
+lemma kronecker_map_smul_right [has_scalar R β] [has_scalar R γ] (f : α → β → γ)
+  (hf : ∀ (r : R) a b, f a (r • b) = r • f a b) (r : R)
+  (A : matrix l m α) (B : matrix n p β) :
+  kronecker_map f A (r • B) = r • kronecker_map f A B :=
+ext $ λ i j, hf _ _ _
+
+@[simps]
+def kronecker_map_linear [comm_semiring R]
+  [add_comm_monoid α] [add_comm_monoid β] [add_comm_monoid γ]
+  [module R α] [module R β] [module R γ]
+  (f : α →ₗ[R] β →ₗ[R] γ) :
+  matrix l m α →ₗ[R] matrix n p β →ₗ[R] matrix (l × n) (m × p) γ :=
+linear_map.mk₂ R
+  (kronecker_map (λ r s, f r s))
+  (kronecker_map_add_left _ $ f.map_add₂)
+  (kronecker_map_smul_left _ $ f.map_smul₂)
+  (kronecker_map_add_right _ $ λ a, (f a).map_add)
+  (kronecker_map_smul_right _ $ λ r a, (f a).map_smul r)
+
+end
+
+
+variables {α R S β : Type*} [comm_semiring α] [comm_semiring R] [comm_semiring S] [comm_semiring β]
 variables [algebra α R] [algebra α S] [algebra α β] [algebra R β] [algebra S β]
 variables {l m n p l' m' n' p' : Type*}
 variables [fintype l] [fintype m] [fintype n] [fintype p]
 variables [fintype l'] [fintype m'] [fintype n'] [fintype p']
 
+variables (α β)
+
+-- TODO: move this
+def algebra.biprod [is_scalar_tower α R β] [is_scalar_tower α S β] :
+  R →ₗ[α] S →ₗ[α] β :=
+((algebra.lmul α β).to_linear_map.compl₂ ((algebra.linear_map S β).restrict_scalars α)).comp
+  ((algebra.linear_map R β).restrict_scalars α)
+
+@[simp]
+lemma algebra.biprod_apply [is_scalar_tower α R β] [is_scalar_tower α S β] (r : R) (s : S) :
+  algebra.biprod α β r s = algebra_map R β r * algebra_map S β s := rfl
+
+variables (R S β)
+
+@[simps?]
 def kronecker_biprod [is_scalar_tower α R β] [is_scalar_tower α S β] :
   (matrix l m R) →ₗ[α] (matrix n p S) →ₗ[α] matrix (l × n) (m × p) β :=
-{ to_fun :=
-  begin
-    intro A,
-    use λ B, λ i j, (algebra_map R β (A i.1 j.1)) * (algebra_map S β (B i.2 j.2)),
-    all_goals {intros x y, ext},
-    { simp only [pi.add_apply, mul_add, ring_hom.map_add, dmatrix.add_apply] },
-    { simp only [pi.smul_apply],
-      rw [← is_scalar_tower.algebra_map_smul S x, id.smul_eq_mul, ring_hom.map_mul,
-        smul_def, (is_scalar_tower.algebra_map_apply α S β x).symm],
-      ring,
-      all_goals {exact is_scalar_tower.right} },
-  end,
-  map_add' := λ _ _, by {simp only [add_mul, ring_hom.map_add, dmatrix.add_apply], refl},
-  map_smul' := λ _ _, by {simp_rw [pi.smul_apply, ← smul_def, is_scalar_tower.smul_assoc],
-    refl},
-  }
+kronecker_map_linear $ algebra.biprod α β
 
 lemma kronecker_biprod_reindex_left (eₗ : l ≃ l') (eₘ : m ≃ m') (A : matrix l m R)
   (B : matrix n p S) [is_scalar_tower α R β] [is_scalar_tower α S β] :
@@ -80,8 +136,10 @@ theorem kronecker_biprod_mul (A : matrix l m R) (B : matrix m n R) (A' : matrix 
     (kronecker_biprod α R S β A A') ⬝ (kronecker_biprod α R S β B B') :=
 begin
   ext ⟨i, i'⟩ ⟨j, j'⟩,
-  simp only [mul_apply, kronecker_biprod, algebra_map_eq_smul_one, mul_one, algebra.mul_smul_comm,
-    linear_map.coe_mk, algebra.smul_mul_assoc, ← finset.univ_product_univ, finset.sum_product],
+  simp only [mul_apply, kronecker_biprod_apply_apply, algebra_map_eq_smul_one, mul_one,
+    algebra.mul_smul_comm,
+    linear_map.coe_mk, algebra.smul_mul_assoc, ← finset.univ_product_univ, finset.sum_product,
+    kronecker_map, algebra.biprod_apply],
   simp_rw [finset.sum_smul, finset.smul_sum, ← smul_eq_mul],
   repeat {apply finset.sum_congr, refl, intros _ _,},
   rw [is_scalar_tower.smul_assoc, id.smul_eq_mul (A' i' x_1) (B' x_1 j')],
@@ -97,11 +155,12 @@ theorem kronecker_biprod_assoc {T : Type*} [comm_semiring T] [algebra α T] [alg
     kronecker_biprod α R β β A (kronecker_biprod α S T β B C)
     :=
 begin
-  simp only [matrix.linear_equiv_index_assoc, kronecker_biprod, linear_map.coe_mk, id.map_eq_self,
+  simp only [matrix.linear_equiv_index_assoc, kronecker_biprod_apply_apply, linear_map.coe_mk,
+    id.map_eq_self,
     reindex_apply, linear_equiv.coe_mk],
   ext i j,
   simp only [equiv.prod_assoc_symm_apply, reindex_linear_equiv_apply, minor_apply, reindex_apply,
-    mul_assoc],
+    mul_assoc, kronecker_map, algebra.biprod_apply, id.map_eq_self],
 end
 
 end matrix_bialgebra

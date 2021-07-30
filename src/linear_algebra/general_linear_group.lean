1b2ca37345d182157a458f8f9de61ae3ea9a3fd5
Chris Birkbeck <c.birkbeck@ucl.ac.uk>
2021-07-30 11:36:47 +0100

    style fixes round 2

--- src/linear_algebra/general_linear_group.lean
+++ src/linear_algebra/general_linear_group.lean
@@ -49,26 +49,26 @@ variables (n : Type u) [decidable_eq n] [fintype n] (R : Type v) [comm_ring R]
 /-- `GL n R` is the group of `n` by `n` `R`-matrices with unit determinant.
 Defined as a subtype of matrices
 -/
-abbreviation general_linear_group (R : Type*) [comm_ring R] :  Type* := units (matrix n n R )
+abbreviation general_linear_group (R : Type*) [comm_ring R] : Type* := units (matrix n n R)
 
-notation `GL`:= general_linear_group
+notation `GL` := general_linear_group
 
-lemma GL.is_unit_det  (A : GL n R) : is_unit ( (A : matrix n n R).det) :=
+lemma GL.is_unit_det  (A : GL n R) : is_unit ((A : matrix n n R).det) :=
 begin
   rw ←matrix.is_unit_iff_is_unit_det,
   exact units.is_unit _
 end
 
 /--The `GL n R` and `general_linear_group R n` are multiplicatively equivalent-/
-def GL_mul_equiv_general_linear_group : (GL n R) ≃* (linear_map.general_linear_group R ( n → R)) :=
+def GL_mul_equiv_general_linear_group : (GL n R) ≃* (linear_map.general_linear_group R (n → R)) :=
 units.map_equiv to_lin_alg_equiv'.to_mul_equiv
 
 /--Given a matrix with invertible determinant we get an element of `GL n R`-/
- def GL.mk' (A : matrix n n R) (h : invertible (det A)): GL n R:=
+ def GL.mk' (A : matrix n n R) (h : invertible (det A)) : GL n R :=
 unit_of_det_invertible A
 
 /--Given a matrix with unit determinant we get an element of `GL n R`-/
-noncomputable def GL.mk'' (A : matrix n n R) (h: is_unit (det A)): GL n R:=
+noncomputable def GL.mk'' (A : matrix n n R) (h : is_unit (det A)) : GL n R :=
 nonsing_inv_unit A h
 
 end
@@ -122,25 +122,25 @@ begin
   have:=nonsing_inv_coe_eq_coe_inv A, unfold_coes at *, rw ← this, refl,
 end
 
-@[simp] lemma to_lin'_mul :to_lin' (A * B) = (to_lin' A).comp (to_lin' B) :=
+@[simp] lemma to_lin'_mul : to_lin' (A * B) = (to_lin' A).comp (to_lin' B) :=
 matrix.to_lin'_mul A B
 
-@[simp] lemma to_lin'_one :to_lin' (1 : GL n R) = linear_map.id :=
+@[simp] lemma to_lin'_one : to_lin' (1 : GL n R) = linear_map.id :=
 matrix.to_lin'_one
 
 end coe_lemmas
 
-instance SL_to_GL : has_coe (special_linear_group n R) (GL n R):=
+instance SL_to_GL : has_coe (special_linear_group n R) (GL n R) :=
 ⟨λ A, ⟨↑A, ↑(A⁻¹), congr_arg coe (mul_right_inv A), congr_arg coe (mul_left_inv A)⟩⟩
 
-@[simp] lemma GL_vals (A : GL n R): ∀ i j, A i j = A.1 i j:=
+@[simp] lemma GL_vals (A : GL n R) : ∀ i j, A i j = A.1 i j :=
 begin
 unfold_coes, intros i j,refl,
 end
 
-lemma det_not_zero [nontrivial R] (A : GL n R): det A ≠ 0:=
+lemma det_not_zero [nontrivial R] (A : GL n R) : det A ≠ 0 :=
 begin
-  have:=GL.is_unit_det _ _ A, simp, by_contradiction, unfold_coes at *,
+  have := GL.is_unit_det _ _ A, simp, by_contradiction, unfold_coes at *,
   rw h at this, simp at *, exact this,
 end
 
@@ -150,50 +150,49 @@ namespace GL_plus
 
 variables  (n : Type u) [decidable_eq n] [fintype n] (R : Type v) [linear_ordered_comm_ring R ]
 
-lemma one_in_GL_pos : 0 < det (1 : GL n R ) :=
+lemma one_in_GL_pos : 0 < det (1 : GL n R) :=
 begin
 simp only [det_one, gt_iff_lt, general_linear_group.one_apply,zero_lt_one],
 end
 
-lemma mul_in_GL_pos (A B : GL n R ) (h1: 0 <  det A  ) (h2: 0 <  det B ): 0 < det (A*B):=
-
+lemma mul_in_GL_pos (A B : GL n R) (h1 : 0 <  det A) (h2 : 0 <  det B) : 0 < det (A*B) :=
 begin
-simp only [gt_iff_lt, det_mul, mul_eq_mul], apply mul_pos h1 h2,
+  simp only [gt_iff_lt, det_mul, mul_eq_mul], apply mul_pos h1 h2,
 end
 
-lemma inv_det_pos  (A : GL n R ) (h: 0 < det A ):  0 < det (A⁻¹).1 :=
+lemma inv_det_pos  (A : GL n R) (h : 0 < det A) :  0 < det (A⁻¹).1 :=
 
 begin
-  have h0:=(GL.is_unit_det _ _ A),
-  have h1:=is_unit_nonsing_inv_det A h0,
-  have h2:= nonsing_inv_det A h0,
-  have h3: 0 < det ⇑A*  det (⇑A)⁻¹ , by {rw mul_comm, rw h2, linarith}, unfold_coes at *,
+  have h0 := (GL.is_unit_det _ _ A),
+  have h1 := is_unit_nonsing_inv_det A h0,
+  have h2 := nonsing_inv_det A h0,
+  have h3 : 0 < det ⇑A*  det (⇑A)⁻¹ , by {rw mul_comm, rw h2, linarith}, unfold_coes at *,
   convert (zero_lt_mul_left h).mp h3,
-  have:= general_linear_group.nonsing_inv_coe_eq_coe_inv A,  unfold_coes  at this, simp_rw this,
+  have := general_linear_group.nonsing_inv_coe_eq_coe_inv A,  unfold_coes  at this, simp_rw this,
 end
 
 /-- This is the subgroup of `nxn` matrices with  entries over a
 linear ordered ring and positive determinant-/
 
  def GL_pos : subgroup (GL n R) :=
-{ carrier:={M  | 0 < det M},
-  one_mem':=one_in_GL_pos _ _ ,
-  mul_mem':=λ A B h1 h2, mul_in_GL_pos _ _ A B h1 h2,
-  inv_mem':=λ A h1, by {have:= inv_det_pos _ _ A h1, simp only [set.mem_set_of_eq] at *,
+{ carrier := {M  | 0 < det M},
+  one_mem' := one_in_GL_pos _ _ ,
+  mul_mem' := λ A B h1 h2, mul_in_GL_pos _ _ A B h1 h2,
+  inv_mem' := λ A h1, by {have:= inv_det_pos _ _ A h1, simp only [set.mem_set_of_eq] at *,
   unfold_coes at *, convert this, }
 }
 
-@[simp] lemma mem_GL_pos (A : GL n R ) : A  ∈ (GL_pos n R)  ↔ 0 < A.1.det := iff.rfl
+@[simp] lemma mem_GL_pos (A : GL n R) : A  ∈ (GL_pos n R)  ↔ 0 < A.1.det := iff.rfl
 
 instance GL_pos_to_GL : has_coe (GL_pos n R)  (GL n R) := ⟨λ A, A.val⟩
 
-lemma SL_det_pos' (A : special_linear_group n R) : 0 < (A).1.det:=
+lemma SL_det_pos' (A : special_linear_group n R) : 0 < (A).1.det :=
 begin
-  have:=A.2, simp only [gt_iff_lt, subtype.val_eq_coe],
+  have := A.2, simp only [gt_iff_lt, subtype.val_eq_coe],
   simp only [subtype.val_eq_coe] at this,  rw this,linarith,
 end
 
-instance SL_to_GL_pos: has_coe (special_linear_group n R) (GL_pos n R):=
+instance SL_to_GL_pos: has_coe (special_linear_group n R) (GL_pos n R) :=
 ⟨λ A, ⟨(A : GL n R), by {simp, apply SL_det_pos' _ _ A}, ⟩⟩
 
 end GL_plus

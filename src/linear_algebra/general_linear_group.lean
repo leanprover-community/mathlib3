aa674e951a0e8186a9960044a81058437ce5999f
Chris Birkbeck <c.birkbeck@ucl.ac.uk>
2021-07-30 13:06:08 +0100

    typo

--- src/linear_algebra/general_linear_group.lean
+++ src/linear_algebra/general_linear_group.lean
@@ -1,5 +1,5 @@
 /-
-Copyright (c) 2020 Anne Baanen. All rights reserved.
+Copyright (c) 2020 Chris Birkbeck. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Chris Birkbeck
 

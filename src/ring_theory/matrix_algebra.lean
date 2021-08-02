7e333d0b91605ad509a34dce6a987685c5b1446d
Eric Wieser <wieser.eric@gmail.com>
2021-08-02 10:58:43 +0100

    Sort imports
    Co-authored-by: Anne Baanen <Vierkantor@users.noreply.github.com>

--- src/ring_theory/matrix_algebra.lean
+++ src/ring_theory/matrix_algebra.lean
@@ -3,8 +3,8 @@ Copyright (c) 2020 Scott Morrison. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Scott Morrison
 -/
-import ring_theory.tensor_product
 import data.matrix.basic
+import ring_theory.tensor_product
 
 /-!
 We show `matrix n n A ≃ₐ[R] (A ⊗[R] matrix n n R)`.

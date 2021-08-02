a32c6b6178c1aff2b815794fe03af42fe9e82b6f
Yakov Pechersky <ffxen158@gmail.com>
2021-08-02 13:29:53 -0400

    fix name

--- src/data/fintype/basic.lean
+++ src/data/fintype/basic.lean
@@ -1205,7 +1205,7 @@ lemma fintype.card_subtype_or (p q : α → Prop) [decidable_pred p]
   [fintype {x // p x}] [fintype {x // q x}] [fintype {x // p x ∨ q x}] :
   fintype.card {x // p x ∨ q x} ≤ fintype.card {x // p x} + fintype.card {x // q x} :=
 begin
-  convert fintype.card_le_of_embedding (subtype_or_embedding p q),
+  convert fintype.card_le_of_embedding (subtype_or_left_embedding p q),
   rw fintype.card_sum
 end
 

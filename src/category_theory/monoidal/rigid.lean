ccf8116cb48c8ec5141ee6ef9083402472241e04
Jakob von Raumer <jakob@von-raumer.de>
2021-08-03 17:39:15 +0200

    define adjoint mates

--- src/category_theory/monoidal/rigid.lean
+++ src/category_theory/monoidal/rigid.lean
@@ -34,9 +34,9 @@ open has_right_dual
 restate_axiom coevaluation_evaluation'
 restate_axiom evaluation_coevaluation'
 
-notation X ` ^*`:120 := dual X
-notation `η_` := coevaluation
-notation `ε_` := evaluation
+notation X `^*`:1200 := dual X
+notation `η_`:100 X := @coevaluation _ _ _ X _
+notation `ε_`:100 X := @has_right_dual.evaluation _ _ _ X _
 
 def has_right_dual_unit : has_right_dual (𝟙_ C) :=
 { dual := 𝟙_C,
@@ -51,6 +51,27 @@ def has_right_dual_unit : has_right_dual (𝟙_ C) :=
       monoidal_category.unitors_inv_equal,
       monoidal_category.unitors_equal], simp } }
 
+def adjoint_mate {X Y : C} [has_right_dual X] [has_right_dual Y] (f : X ⟶ Y) : Y^* ⟶ X^* :=
+(ρ_ _).inv ≫ (𝟙 Y^* ⊗ η_ X)
+  ≫ (𝟙 (Y^*) ⊗ (f ⊗ 𝟙 X^*))
+  ≫ (α_ _ _ _).inv
+  ≫ ((ε_ _) ⊗ 𝟙 _)
+  ≫ (λ_ _).hom
+
+theorem adjoint_mate_id {X : C} [has_right_dual X] : adjoint_mate (𝟙 X) = 𝟙 X^* :=
+begin
+  unfold adjoint_mate,
+  simp only [monoidal_category.tensor_id, category.id_comp],
+  sorry
+end
+
+def has_right_dual_dual_tensor (X Y : C) [hX : has_right_dual X] [hY : has_right_dual Y]
+: has_right_dual (X ⊗ Y) :=
+{ dual := Y ^* ⊗ X ^*,
+  coevaluation := (η_ _) ≫ (𝟙 _ ⊗ ((λ_ _).inv ≫ ((η_ _) ⊗ 𝟙 _) ≫ (α_ _ _ _).hom)) ≫ (α_ _ _ _).inv,
+  evaluation := (α_ _ _ _).hom ≫ (𝟙 _ ⊗ ((α_ _ _ _).inv ≫ ((ε_ _) ⊗ 𝟙 Y) ≫ (λ_ Y).hom)) ≫ ε_ _,
+  evaluation_coevaluation' := by { sorry },
+  coevaluation_evaluation' := by sorry }
 
 /- A right rigid monoidal category is one in which every object has a right dual. -/
 class right_rigid_category (C : Type u) [category.{v} C] [monoidal_category.{v} C] :=

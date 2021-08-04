579819b45092e909f2c2987cb747e7163d1fadea
Eric Wieser <wieser.eric@gmail.com>
2021-08-04 12:11:06 +0100

    protected

--- src/linear_algebra/clifford_algebra/equivs.lean
+++ src/linear_algebra/clifford_algebra/equivs.lean
@@ -30,25 +30,24 @@ namespace clifford_algebra_ring
 variables {R : Type*} [comm_ring R]
 
 @[simp]
-lemma ι_eq_0 : ι (0 : quadratic_form R unit) = 0 :=
+lemma ι_eq_zero : ι (0 : quadratic_form R unit) = 0 :=
 subsingleton.elim _ _
 
 /-- The clifford algebra over a 0-dimensional vector space is isomorphic to its scalars. -/
-def equiv : clifford_algebra (0 : quadratic_form R unit) ≃ₐ[R] R :=
+protected def equiv : clifford_algebra (0 : quadratic_form R unit) ≃ₐ[R] R :=
 alg_equiv.of_alg_hom
   (clifford_algebra.lift (0 : quadratic_form R unit) $
     ⟨0, λ m : unit, (zero_mul (0 : R)).trans (algebra_map R _).map_zero.symm⟩)
   (algebra.of_id R _)
   (by { ext x, exact (alg_hom.commutes _ x), })
-  (by { ext : 1, rw [ι_eq_0, linear_map.comp_zero, linear_map.comp_zero], })
+  (by { ext : 1, rw [ι_eq_zero, linear_map.comp_zero, linear_map.comp_zero], })
 
 end clifford_algebra_ring
 
 namespace clifford_algebra_complex
 
 /-- The quadratic form sending elements to the negation of their square. -/
-def Q : quadratic_form ℝ ℝ :=
--quadratic_form.lin_mul_lin linear_map.id linear_map.id
+def Q : quadratic_form ℝ ℝ := -quadratic_form.lin_mul_lin linear_map.id linear_map.id
 
 @[simp]
 lemma Q_apply (r : ℝ) : Q r = - (r * r) := rfl
@@ -69,7 +68,7 @@ clifford_algebra.lift_ι_apply _ _ r
 /-- The clifford algebras over `clifford_algebra_complex.Q` is isomorphic as an `ℝ`-algebra to
 `ℂ`. -/
 @[simps]
-def equiv : clifford_algebra Q ≃ₐ[ℝ] ℂ :=
+protected def equiv : clifford_algebra Q ≃ₐ[ℝ] ℂ :=
 alg_equiv.of_alg_hom to_complex
   (complex.lift ⟨clifford_algebra.ι Q 1, begin
     rw [clifford_algebra.ι_sq_scalar, Q_apply, one_mul, ring_hom.map_neg, ring_hom.map_one],
@@ -85,5 +84,8 @@ alg_equiv.of_alg_hom to_complex
       alg_hom.to_linear_map_apply, alg_hom.comp_apply],
     rw [to_complex_ι, one_smul, complex.lift_aux_apply_I],
   end)
+
+-- this name is too short for us to want it visible after `open clifford_algebra_complex`
+attribute [protected] Q
 
 end clifford_algebra_complex

58e09cf156768d8d1fe3200259c0d19eeae7d501
Yourong "DZR" Zang <yourongzang@outlook.com>
2021-08-06 10:17:15 +0000

    removed all `antiholomorph` lemmas; completed one modified proof.

--- src/analysis/complex/real_deriv.lean
+++ src/analysis/complex/real_deriv.lean
@@ -63,60 +63,18 @@ times_cont_diff_iff_times_cont_diff_at.2 $ λ x,
 
 end real_deriv_of_complex
 
-section complex_fderiv_properties
-/-! ### Antiholomorphy and conformality of complex functions -/
+section conformality
+/-! ### Conformality of real-differentiable complex maps -/
 open complex continuous_linear_map
 
-variables {E : Type*} [normed_group E] [normed_space ℝ E]
-  {z : ℂ} {f : ℂ → E}
-
-lemma has_fderiv_at_conj (z : ℂ) : has_fderiv_at conj (conj_cle : ℂ →L[ℝ] ℂ) z :=
-conj_cle.has_fderiv_at
-
-lemma fderiv_conj_eq_conj_fderiv {z : ℂ} (h : differentiable_at ℝ f z) :
-  (fderiv ℝ f z).comp ↑conj_cle = fderiv ℝ (f ∘ conj) (conj z) :=
-begin
-  rw ← conj_conj z at h,
-  convert (fderiv.comp (conj z) h (has_fderiv_at_conj $ conj z).differentiable_at).symm,
-  rw conj_conj,
-  rw (has_fderiv_at_conj $ conj z).fderiv,
-end
-
-/-- A (real-differentiable) complex function `f` is antiholomorphic if and only if there exists some
-    complex linear map `g'` that equals to the composition of `f`'s differential and the conjugate
-    function -/
-lemma antiholomorph_at_iff_exists_complex_linear_conj
-  [normed_space ℂ E] [is_scalar_tower ℝ ℂ E]
-  (hf : differentiable_at ℝ f z) : differentiable_at ℂ (f ∘ conj) (conj z) ↔
-  ∃ (g' : ℂ →L[ℂ] E), g'.restrict_scalars ℝ =
-  (fderiv ℝ f z).comp ↑conj_cle :=
-begin
-  split,
-  { intros h,
-    rw ← conj_conj z at hf,
-    rcases (differentiable_at_iff_exists_linear_map ℝ $
-      hf.comp (conj z) (has_fderiv_at_conj $ conj z).differentiable_at).mp h with ⟨f', hf'⟩,
-    rw conj_conj at hf,
-    rw ← fderiv_conj_eq_conj_fderiv hf at hf',
-    exact ⟨f', hf'⟩, },
-  { rintros ⟨g', hg'⟩,
-    rw ← conj_conj z at hf hg',
-    exact ⟨g', has_fderiv_at_of_eq ℝ
-      (hf.has_fderiv_at.comp (conj z) $ has_fderiv_at_conj $ conj z) hg'⟩, },
-end
-
-end complex_fderiv_properties
-
-open complex continuous_linear_map
-
-section conformal_complex_normed_space
-
-variables {E : Type*} [normed_group E] [normed_space ℝ E] [normed_space ℂ E]
-  [is_scalar_tower ℝ ℂ E] {z : ℂ} {f : ℂ → E}
+variables
 
 /-- A real differentiable function of the complex plane into some complex normed space `E` is
     conformal at a point `z` if it is holomorphic at that point -/
-lemma conformal_at_of_holomorph (hf' : fderiv ℝ f z ≠ 0) (h : differentiable_at ℂ f z) :
+lemma conformal_at_of_holomorph {E : Type*}
+  [normed_group E] [normed_space ℝ E] [normed_space ℂ E]
+  [is_scalar_tower ℝ ℂ E] {z : ℂ} {f : ℂ → E}
+  (hf' : fderiv ℝ f z ≠ 0) (h : differentiable_at ℂ f z) :
   conformal_at f z :=
 begin
   rw conformal_at_iff_is_conformal_map_fderiv,
@@ -126,47 +84,9 @@ begin
   simp [w]
 end
 
-/-- A real differentiable function of the complex plane into some complex normed space `E` is
-    conformal at a point `z` if it is antiholomorphic at that point -/
-lemma conformal_at_of_antiholomorph (hf' : fderiv ℝ f z ≠ 0)
-  (h : differentiable_at ℂ (f ∘ conj) (conj z)) : conformal_at f z :=
-begin
-  have minor₁ : f ∘ conj ∘ conj = f := by ext1; simp only [function.comp_app, conj_conj],
-  have minor₂ : differentiable_at ℝ f z :=
-    minor₁ ▸ (h.restrict_scalars ℝ).comp z (has_fderiv_at_conj z).differentiable_at,
-  rw [conformal_at_iff_is_conformal_map_fderiv],
-  rw [antiholomorph_at_iff_exists_complex_linear_conj minor₂] at h,
-  rcases h with ⟨map, hmap⟩,
-  have minor₃ : fderiv ℝ f z = (map.restrict_scalars ℝ).comp ↑conj_cle,
-  { ext1,
-    rw hmap,
-    simp only [coe_comp', function.comp_app, conj_cle.coe_coe, conj_cle_apply, conj_conj], },
-  rw minor₃,
-  refine is_conformal_map_complex_linear_conj _,
-  contrapose! hf' with w,
-  rw minor₃,
-  ext1,
-  simp only [coe_comp', function.comp_app, conj_cle.coe_coe,
-             conj_cle_apply, coe_restrict_scalars', w, continuous_linear_map.zero_apply],
-end
-
-end conformal_complex_normed_space
-
-section into_the_complex_plane
-
-variables {f : ℂ → ℂ} {z : ℂ}
-
-lemma conformal_at_iff_holomorphic_or_antiholomorph_at_aux (hf : differentiable_at ℝ f z) :
-  conformal_at f z ↔
-  (differentiable_at ℂ f z ∨ differentiable_at ℂ (f ∘ conj) (conj z)) ∧ fderiv ℝ f z ≠ 0 :=
-by rw [conformal_at_iff_is_conformal_map_fderiv,
-       is_complex_or_conj_complex_linear_iff_is_conformal_map,
-       differentiable_at_iff_exists_linear_map ℝ hf,
-       antiholomorph_at_iff_exists_complex_linear_conj hf]
-
 /-- A complex function is conformal if and only if the function is holomorphic or antiholomorphic
     with a nonvanishing differential. -/
-lemma conformal_at_iff_holomorphic_or_antiholomorphic_at :
+lemma conformal_at_iff_holomorphic_or_antiholomorphic_at {f : ℂ → ℂ} {z : ℂ} :
   conformal_at f z ↔
   (differentiable_at ℂ f z ∨ differentiable_at ℂ (f ∘ conj) (conj z)) ∧ fderiv ℝ f z ≠ 0 :=
 begin
@@ -183,4 +103,5 @@ begin
   have : fderiv ℝ conj (conj z) = _ := conj_cle.fderiv,
   simp [fderiv.comp _ h_diff conj_cle.differentiable_at, this, conj_conj],
 end
-end into_the_complex_plane
+
+end conformality

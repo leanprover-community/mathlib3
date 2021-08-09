d27189e34d200d2a5884e77d6f3da4770ec12ff6
Johan Commelin <johan@commelin.net>
2021-08-09 10:19:53 +0000

    a couple more tweaks

--- archive/imo/imo2001_q6.lean
+++ archive/imo/imo2001_q6.lean
@@ -4,9 +4,9 @@ Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Sara Díaz Real
 -/
 import data.int.basic
+import algebra.associated
 import tactic.linarith
 import tactic.ring
-import algebra.associated
 
 /-!
 # IMO 2001 Q6
@@ -25,9 +25,9 @@ theorem imo2001_q6 (hd : 0 < d) (hdc : d < c) (hcb : c < b) (hba : b < a)
   ¬ prime (a*b + c*d) :=
 begin
   assume h0 : prime (a*b + c*d),
-  have ha : 0 < a, by linarith,
-  have hb : 0 < b, by linarith,
-  have hc : 0 < c, by linarith,
+  have ha : 0 < a, { linarith },
+  have hb : 0 < b, { linarith },
+  have hc : 0 < c, { linarith },
   -- the key step is to show that `a*c + b*d` divides the product `(a*b + c*d) * (a*d + b*c)`
   have dvd_mul : a*c + b*d ∣ (a*b + c*d) * (a*d + b*c),
   { use b^2 + b*d + d^2,
@@ -41,12 +41,12 @@ begin
   obtain (h1 : a*b + c*d ∣ a*c + b*d) | (h2 : a*c + b*d ∣ a*d + b*c) :=
     left_dvd_or_dvd_right_of_dvd_prime_mul h0 dvd_mul,
   -- in both cases, we derive a contradiction
-  { have aux : 0 < a*c + b*d,         by nlinarith only [ha, hb, hc, hd],
-    have : a*b + c*d ≤ a*c + b*d,     from int.le_of_dvd aux h1,
-    have : ¬ (a*b + c*d ≤ a*c + b*d), by nlinarith only [hba, hcb, hdc, h],
+  { have aux : 0 < a*c + b*d,         { nlinarith only [ha, hb, hc, hd] },
+    have : a*b + c*d ≤ a*c + b*d,     { from int.le_of_dvd aux h1 },
+    have : ¬ (a*b + c*d ≤ a*c + b*d), { nlinarith only [hba, hcb, hdc, h] },
     contradiction, },
-  { have aux : 0 < a*d + b*c,         by nlinarith only [ha, hb, hc, hd],
-    have : a*c + b*d ≤ a*d + b*c,     from int.le_of_dvd aux h2,
-    have : ¬ (a*c + b*d ≤ a*d + b*c), by nlinarith only [hba, hdc, h],
+  { have aux : 0 < a*d + b*c,         { nlinarith only [ha, hb, hc, hd] },
+    have : a*c + b*d ≤ a*d + b*c,     { from int.le_of_dvd aux h2 },
+    have : ¬ (a*c + b*d ≤ a*d + b*c), { nlinarith only [hba, hdc, h] },
     contradiction, },
 end

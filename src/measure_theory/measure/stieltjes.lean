52ad4aff597de451efb40c34e9efa143129cdef8
sgouezel <sebastien.gouezel@univ-rennes1.fr>
2021-08-11 12:03:53 +0200

    fix import

--- src/measure_theory/measure/stieltjes.lean
+++ src/measure_theory/measure/stieltjes.lean
@@ -3,7 +3,7 @@ Copyright (c) 2021 Sébastien Gouëzel. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Johannes Hölzl, Yury Kudryashov, Sébastien Gouëzel
 -/
-import measure_theory.borel_space
+import measure_theory.constructions.borel_space
 
 /-!
 # Stieltjes measures on the real line

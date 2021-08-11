057064c4a48f3f2df823de9c14a9796dd4d125e8
Chris Hughes <chrishughes24@gmail.com>
2021-08-11 11:24:58 +0100

    add nolint instance

--- src/data/set_like/fintype.lean
+++ src/data/set_like/fintype.lean
@@ -16,6 +16,7 @@ namespace set_like
 
 /-- TODO: It should be possible to obtain a computable version of this for most
 set_like objects. If we add those instances, we should remove this one. -/
+@[nolint dangerous_instance]
 noncomputable instance {A B : Type*} [fintype B] [set_like A B] : fintype A :=
 fintype.of_injective coe set_like.coe_injective
 

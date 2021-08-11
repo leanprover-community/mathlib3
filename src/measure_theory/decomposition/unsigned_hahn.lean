9c8602b04715b959a7eae703d8506acd5281c44d
Floris van Doorn <fpvdoorn@gmail.com>
2021-08-11 06:39:10 +0000

    refactor(measure_theory): add subfolder (#8594)
    * This PR adds the subfolders `constructions` `function` `group` `integral` and `measure` to `measure_theory`
    * File renamings:
    ```
    group -> group.basic
    prod_group -> group.prod
    bochner_integration -> integral.bochner
    integration -> integral.lebesgue
    haar_measure -> measure.haar
    lebesgue_measure -> measure.lebesgue
    hausdorff_measure -> measure.hausdorff
    ```

--- src/measure_theory/decomposition/unsigned_hahn.lean
+++ src/measure_theory/decomposition/unsigned_hahn.lean
@@ -3,7 +3,7 @@ Copyright (c) 2019 Johannes Hölzl. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Johannes Hölzl
 -/
-import measure_theory.measure_space
+import measure_theory.measure.measure_space
 
 /-!
 # Unsigned Hahn decomposition theorem

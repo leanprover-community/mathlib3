f9153236730df1f7941ce71f3c8be4b61f420c4a
Chris Hughes <chrishughes24@gmail.com>
2021-08-12 12:01:00 +0100

    linter - remove unused group argument from conj

--- src/group_theory/group_action/conj.lean
+++ src/group_theory/group_action/conj.lean
@@ -13,20 +13,30 @@ A type alias `conj G` is introduced for a group `G`. The group `conj G` acts on 
 
 -/
 
-variables (G : Type*) [group G]
+variables (G : Type*)
 
 /-- A type alias for a group `G`. `conj G` acts on `G` by conjugation -/
-@[derive group] def conj : Type* := G
+def conj : Type* := G
 
 namespace conj
 open mul_action subgroup
 
 variable {G}
 
-def to_conj : G → conj G := id
+instance : Π [group G], group (conj G) := id
 
-def of_conj : conj G → G := id
+variable [group G]
 
+instance : inhabited (conj G) := ⟨1⟩
+
+/-- Reinterpret `g : conj G` as an element of `G`. -/
+def of_conj : conj G ≃* G := ⟨id, id, λ _, rfl, λ _, rfl, λ _ _, rfl⟩
+
+/-- Reinterpret `g : G` as an element of `conj G`. -/
+def to_conj : G ≃* conj G := of_conj.symm
+
+@[simp] lemma of_mul_symm_eq : (@of_conj G _).symm = to_conj := rfl
+@[simp] lemma to_mul_symm_eq : (@to_conj G _).symm = of_conj := rfl
 @[simp] lemma to_conj_of_conj (x : conj G) : to_conj (of_conj x) = x := rfl
 @[simp] lemma of_conj_to_conj (x : G) : of_conj (to_conj x) = x := rfl
 @[simp] lemma of_conj_one : of_conj (1 : conj G) = 1 := rfl
@@ -43,20 +53,14 @@ instance : mul_action (conj G) G :=
 
 lemma smul_def (g : conj G) (h : G) : g • h = of_conj g * h * (of_conj g)⁻¹ := rfl
 
-/-- `G` is isomorphic as a group to `conj G` -/
-@[simps] def mul_equiv : conj G ≃* G :=
-{ to_fun := of_conj,
-  inv_fun := to_conj,
-  left_inv := λ _, rfl,
-  right_inv := λ _, rfl,
-  map_mul' := λ _ _, rfl }
-
 instance : Π [fintype G], fintype (conj G) := id
 
 @[simp] lemma card [fintype G] : fintype.card (conj G) = fintype.card G := rfl
 
 @[simp] lemma «forall» (p : conj G → Prop) :
   (∀ (x : conj G), p x) ↔ ∀ x : G, p (to_conj x) := iff.rfl
+
+lemma smul_eq_conj (g : conj G) (h : G) : g • h = mul_aut.conj g h := rfl
 
 /-- The set of fixed points of the conjugation action of `G` on itself is the center of `G`. -/
 lemma fixed_points_eq_center : fixed_points (conj G) G = center G :=

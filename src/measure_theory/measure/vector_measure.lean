501863b14fad71425d80b5358f4d49c5e3dec55b
Kexing Ying <kexing.ying19@imperial.ac.uk>
2021-08-12 16:42:36 +0200

    Apply suggestions from code review
    Co-authored-by: sgouezel <sebastien.gouezel@univ-rennes1.fr>

--- src/measure_theory/measure/vector_measure.lean
+++ src/measure_theory/measure/vector_measure.lean
@@ -875,9 +875,9 @@ measure.of_measurable (s.to_measure_of_zero_le' i hi₂)
     simp only [to_measure_of_zero_le', s.restrict_apply hi₁ (measurable_set.Union hf₁),
                set.inter_comm, set.inter_Union, s.of_disjoint_Union_nat h₁ h₂,
                ennreal.some_eq_coe, id.def],
-    have h : ∀ n, 0 ≤ s (i ∩ f n),
-    { exact λ n, s.nonneg_of_zero_le_restrict
-          (s.zero_le_restrict_subset hi₁ (inter_subset_left _ _) hi₂) },
+    have h : ∀ n, 0 ≤ s (i ∩ f n) :=
+      λ n, s.nonneg_of_zero_le_restrict
+          (s.zero_le_restrict_subset hi₁ (inter_subset_left _ _) hi₂),
     rw [nnreal.coe_tsum_of_nonneg h, ennreal.coe_tsum],
     { refine tsum_congr (λ n, _),
       simp_rw [s.restrict_apply hi₁ (hf₁ n), set.inter_comm] },
@@ -891,8 +891,8 @@ lemma to_measure_of_zero_le_apply (hi : 0 ≤[i] s)
   s.to_measure_of_zero_le i hi₁ hi j =
   @coe ℝ≥0 ℝ≥0∞ _ ⟨s (i ∩ j), nonneg_of_zero_le_restrict s
     (zero_le_restrict_subset s hi₁ (set.inter_subset_left _ _) hi)⟩ :=
-by { simp_rw [to_measure_of_zero_le, measure.of_measurable_apply _ hj₁, to_measure_of_zero_le',
-              s.restrict_apply hi₁ hj₁, set.inter_comm] }
+by simp_rw [to_measure_of_zero_le, measure.of_measurable_apply _ hj₁, to_measure_of_zero_le',
+              s.restrict_apply hi₁ hj₁, set.inter_comm]
 
 /-- Given a signed measure `s` and a negative measurable set `i`, `to_measure_of_le_zero`
 provides the measure, mapping measurable sets `j` to `-s (i ∩ j)`. -/

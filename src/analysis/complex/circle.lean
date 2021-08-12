3654e3cbafc98922ccab68c6d1cfd01b94b57533
Shadman Sakib <ssakib4550@townsendharris.org>
2021-08-12 09:38:55 -0400

    Fix Lint

--- src/analysis/complex/circle.lean
+++ src/analysis/complex/circle.lean
@@ -28,13 +28,10 @@ for example, the circle is not defeq to `{z : ℂ | abs z = 1}`, which is the ke
 considered as a homomorphism from `ℂ` to `ℝ`, nor is it defeq to `{z : ℂ | norm_sq z = 1}`, which
 is the kernel of the homomorphism `complex.norm_sq` from `ℂ` to `ℝ`.
 
-Due to import hierarchy reasons, lemmas `exp_map_circle_surjective`, `exp_map_circle_eq_one_iff`,
-and `exp_map_circle_hom_ker` have been moved to `analysis.special_functions.trigonometric`.
-`exp_map_circle_surjective` proves the function `exp_map_circle` is surjective.
-`exp_map_circle_eq_one_iff` proves when `exp_map_circle` of some `x : ℝ = 1`.
-`exp_map_circle_hom_ker` characterizes `exp_map_circle_hom.ker` as
-`add_subgroup.gmultiples (2 * π)`. Moreover, material about the equivalence `angle_to_circle` between
-`real.angle` and `circle` is in  `analysis.special_functions.trigonometric`.
+Due to import hierarchy reasons, lemmas about the surjectivity and the kernel of
+`exp_map_circle_hom` are in `analysis.special_functions.trigonometric`.
+Moreover, material about the equivalence `angle_to_circle` between `real.angle` and `circle` is in
+`analysis.special_functions.trigonometric`.
 -/
 
 noncomputable theory

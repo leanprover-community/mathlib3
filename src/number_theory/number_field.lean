82227073ffdb59242486e98df785a7ddffda2554
Anne Baanen <Vierkantor@users.noreply.github.com>
2021-08-16 14:42:33 +0000

    Style fix Authors: line

--- src/number_theory/number_field.lean
+++ src/number_theory/number_field.lean
@@ -1,7 +1,7 @@
 /-
 Copyright (c) 2021 Ashvni Narayanan. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
-Author: Ashvni Narayanan, Anne Baanen
+Authors: Ashvni Narayanan, Anne Baanen
 -/
 
 import algebra.field

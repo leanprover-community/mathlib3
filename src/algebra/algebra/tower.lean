951c4d758d1d1b60bc9386d7c0dcd35302d2b215
Anne Baanen <Vierkantor@users.noreply.github.com>
2021-08-18 18:17:48 +0000

    Deverbosify
    Co-authored-by: Eric Wieser <wieser.eric@gmail.com>

--- src/algebra/algebra/tower.lean
+++ src/algebra/algebra/tower.lean
@@ -185,9 +185,8 @@ lemma restrict_scalars_apply (f : A →ₐ[S] B) (x : A) : f.restrict_scalars R 
 @[simp] lemma coe_restrict_scalars' (f : A →ₐ[S] B) : (restrict_scalars R f : A → B) = f := rfl
 
 lemma restrict_scalars_injective :
-  function.injective (restrict_scalars R : ((A →ₐ[S] B) → (A →ₐ[R] B))) :=
-λ f g h, alg_hom.ext (λ x,
-show f.restrict_scalars R x = g.restrict_scalars R x, from alg_hom.congr_fun h x)
+  function.injective (restrict_scalars R : (A →ₐ[S] B) → (A →ₐ[R] B)) :=
+λ f g h, alg_hom.ext (alg_hom.congr_fun h : _)
 
 end alg_hom
 
@@ -206,9 +205,8 @@ lemma restrict_scalars_apply (f : A ≃ₐ[S] B) (x : A) : f.restrict_scalars R 
 @[simp] lemma coe_restrict_scalars' (f : A ≃ₐ[S] B) : (restrict_scalars R f : A → B) = f := rfl
 
 lemma restrict_scalars_injective :
-  function.injective (restrict_scalars R : ((A ≃ₐ[S] B) → (A ≃ₐ[R] B))) :=
-λ f g h, alg_equiv.ext (λ x,
-show f.restrict_scalars R x = g.restrict_scalars R x, from alg_equiv.congr_fun h x)
+  function.injective (restrict_scalars R : (A ≃ₐ[S] B) → (A ≃ₐ[R] B)) :=
+λ f g h, alg_equiv.ext (alg_equiv.congr_fun h : _)
 
 end alg_equiv
 

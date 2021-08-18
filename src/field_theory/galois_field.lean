6d8156939e7c0588f875ca06578aec689499e8c0
Alex J. Best <alex.j.best@gmail.com>
2021-08-18 17:26:20 +0200

    fix names

--- src/field_theory/galois_field.lean
+++ src/field_theory/galois_field.lean
@@ -1,7 +1,7 @@
 /-
 Copyright (c) 2021 Johan Commelin. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
-Authors: Aaron Anderson, Alex J. Best, Johan Commelin, Eric Rodriguez and Ruben Van de Velde
+Authors: Aaron Anderson, Alex J. Best, Johan Commelin, Eric Rodriguez, Ruben Van de Velde
 -/
 
 import field_theory.finite.basic

f4ac086ad9c1a3ac952363eed9645b8177cb1316
Paul-Lez <72892199+Paul-Lez@users.noreply.github.com>
2021-08-18 14:53:08 +0200

    Added lemma `lift_mk_of_le`

--- src/ring_theory/ideal/basic.lean
+++ src/ring_theory/ideal/basic.lean
@@ -590,6 +590,9 @@ def lift (S : ideal α) (f : α →+* β) (H : ∀ (a : α), a ∈ S → f a = 0
 @[simp] lemma lift_mk (S : ideal α) (f : α →+* β) (H : ∀ (a : α), a ∈ S → f a = 0) :
   lift S f H (mk S a) = f a := rfl
 
+def lift_mk_of_le (S T : ideal α) (H : S ≤ T) : S.quotient →+* T.quotient :=
+  ideal.quotient.lift S (T^.quotient.mk) (λ x hx, eq_zero_iff_mem.2 (H hx))
+
 end quotient
 
 /-- Quotienting by equal ideals gives equivalent rings.

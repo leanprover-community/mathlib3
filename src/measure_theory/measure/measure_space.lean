c2a3059dd372b4f7bec5599b36206c9aafcb25cc
Kexing <kexing.ying19@imperial.ac.uk>
2021-08-19 16:38:36 +0200

    more reviews

--- src/measure_theory/measure/measure_space.lean
+++ src/measure_theory/measure/measure_space.lean
@@ -1831,21 +1831,21 @@ variable [measurable_space α]
 /-- Given `S : μ.finite_spanning_sets_in {s | measurable_set s}`,
 `disjointed_finite_spanning_sets_in` provides a `finite_spanning_sets_in {s | measurable_set s}`
 such that its underlying sets are pairwise disjoint. -/
-def finite_spanning_sets_in.disjointed {μ : measure α}
+protected def finite_spanning_sets_in.disjointed {μ : measure α}
   (S : μ.finite_spanning_sets_in {s | measurable_set s}) :
    μ.finite_spanning_sets_in {s | measurable_set s} :=
 ⟨disjointed S.set, measurable_set.disjointed S.set_mem,
   λ n, lt_of_le_of_lt (measure_mono (disjointed_subset S.set n)) (S.finite _),
   S.spanning ▸ Union_disjointed⟩
 
-lemma disjoint_disjointed_finite_spanning_sets_in {μ : measure α}
+lemma finite_spanning_sets_in.disjoint_disjointed {μ : measure α}
   (S : μ.finite_spanning_sets_in {s | measurable_set s}) :
   pairwise (disjoint on S.disjointed.set) :=
 disjoint_disjointed _
 
 /-- Given two σ-finite measures `μ` and `ν`, `finite_spanning_sets.in_add μ ν` is a sequence of
 pairwise disjoint, finite spanning sets in `{s | measurable_set s}` with respect to `μ + ν`. -/
-def finite_spanning_sets_in.add {μ ν : measure α}
+protected def finite_spanning_sets_in.add {μ ν : measure α}
   (S : μ.finite_spanning_sets_in {s | measurable_set s})
   (T : ν.finite_spanning_sets_in {s | measurable_set s}) :
   (μ + ν).finite_spanning_sets_in {s | measurable_set s} :=
@@ -1877,7 +1877,7 @@ lemma exists_eq_disjoint_finite_spanning_sets_in
     S.set = T.set ∧ pairwise (disjoint on S.set) :=
 let S := (μ.to_finite_spanning_sets_in.add ν.to_finite_spanning_sets_in).disjointed in
 ⟨S.of_le (measure.le_add_right (le_refl _)), S.of_le (measure.le_add_left (le_refl _)),
- rfl, disjoint_disjointed_finite_spanning_sets_in _⟩
+ rfl, finite_spanning_sets_in.disjoint_disjointed _⟩
 
 end disjointed
 

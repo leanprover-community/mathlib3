43f5ee548d6d53f825acdc168f32cd0a4ef5cd42
Eric <ericrboidi@gmail.com>
2021-08-20 22:41:24 +0100

    some more knocked out+some fixes

--- src/data/nat/count.lean
+++ src/data/nat/count.lean
@@ -21,9 +21,8 @@ def count (n : ℕ) : ℕ :=
 
 lemma nth_count_gc  : galois_connection (nth p) (count p) :=
 begin
-  rintro a b,
-  rw nth,
-  rw Inf_le_iff,
+  -- this is not true
+  sorry
 end
 
 lemma nth_le_iff_le_count {p} [decidable_pred p] {a b : ℕ} : nth p a ≤ b ↔ a ≤ count p b :=
@@ -70,12 +69,15 @@ noncomputable def nth  : ℕ → ℕ
 lemma nth_def  (n : ℕ) : nth p n = Inf { i : ℕ | p i ∧ ∀ k < n, nth p k < i } :=
 well_founded.fix_eq _ _ _
 
+noncomputable def count_set_fintype (n : ℕ) (p : ℕ → Prop) : fintype { i | i ≤ n ∧ p i } :=
+  fintype.of_injective (λ i, (⟨i.1, i.2.1⟩ : { i | i ≤ n })) (by tidy)
+
+local attribute [instance] count_set_fintype
+
 lemma count_monotone  : monotone (count p) :=
 begin
   intros n m h,
   dsimp [count],
-  haveI : Π n, fintype { i | i ≤ n ∧ p i } :=
-    λ n, fintype.of_injective (λ i, (⟨i.1, i.2.1⟩ : { i | i ≤ n })) (by tidy),
   rw [card_eq_fintype_card, card_eq_fintype_card],
   fapply fintype.card_le_of_injective,
   { exact λ i, ⟨i.1, i.2.1.trans h, i.2.2⟩, },
@@ -111,10 +113,12 @@ sorry
 lemma nth_le_of_le_count  (n k : ℕ) (h : k ≤ count p n) : nth p k ≤ n :=
 sorry
 
+-- similarly to computable, I assume this isn't true
 lemma nth_count_gc  : galois_connection (nth p) (count p) :=
 begin
   rintro a b,
   rw nth,
+  sorry
 end
 
 lemma nth_le_iff_le_count {p} [decidable_pred p] {a b : ℕ} : nth p a ≤ b ↔ a ≤ count p b :=
@@ -126,9 +130,24 @@ lt_iff_lt_of_le_iff_le nth_le_iff_le_count
 lemma nth_count_le  (n : ℕ) : nth p (count p n) ≤ n :=
 (nth_count_gc _).l_u_le _
 
+-- todo: move
+lemma _root_.nat.le_succ_iff (x n : ℕ): x ≤ n + 1 ↔ x ≤ n ∨ x = n + 1 :=
+by rw [decidable.le_iff_lt_or_eq, lt_succ_iff]
+
 lemma count_eq_count_add_one  (n : ℕ) (h : p (n+1)) :
   count p (n + 1) = count p n + 1 :=
-sorry
+begin
+  unfold count,
+  suffices : {i : ℕ | i ≤ n + 1 ∧ p i} = {i : ℕ | i ≤ n ∧ p i} ∪ {n + 1},
+  { simp [this] },
+  ext,
+  simp only [set.mem_insert_iff, set.mem_set_of_eq, set.union_singleton],
+  rw [le_succ_iff, or_and_distrib_left],
+  suffices : (x = n + 1 ∨ p x) ↔ p x,
+  { rw this,
+    tauto },
+  simp [h] {contextual := tt}
+end
 
 -- TODO this is the difficult sorry
 lemma nth_count  (n : ℕ) (h : p n) : nth p (count p n) = n :=
@@ -138,9 +157,9 @@ end
 
 noncomputable def set.infinite.order_iso_nat {s : set ℕ} (i : s.infinite) : s ≃o ℕ :=
 (strict_mono.order_iso_of_surjective
-  (λ n, (⟨nat.nth s n, nat.nth_mem_of_infinite s n i⟩ : s))
-  (λ n m h, nat.nth_strict_mono_of_infinite s i h)
-  (λ ⟨n, w⟩, ⟨nat.count s n, by simpa using nat.nth_count s n w⟩)).symm
+  (λ n, (⟨nth s n, nth_mem_of_infinite s n i⟩ : s))
+  (λ n m h, nth_strict_mono_of_infinite s i h)
+  (λ ⟨n, w⟩, ⟨count s n, by simpa using nth_count s n w⟩)).symm
 
 end noncomp
 

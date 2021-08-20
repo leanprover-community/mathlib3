787b69347193e6939e05fe468561f578e346eb82
Eric Wieser <wieser.eric@gmail.com>
2021-08-20 09:45:17 +0100

    chore(algebra/direct_sum): Move all the algebraic structure on `direct_sum` into a single directory

--- src/algebra/direct_sum/ring.lean
+++ src/algebra/direct_sum/ring.lean
@@ -3,9 +3,9 @@ Copyright (c) 2021 Eric Wieser. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Eric Wieser
 -/
-import algebra.direct_sum
 import algebra.algebra.basic
 import algebra.algebra.operations
+import algebra.direct_sum.basic
 import group_theory.subgroup
 
 /-!
--- src/linear_algebra/direct_sum/finsupp.lean
+++ src/linear_algebra/direct_sum/finsupp.lean
@@ -3,17 +3,15 @@ Copyright (c) 2019 Johannes Hölzl. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Johannes Hölzl
 -/
+import algebra.direct_sum.finsupp
 import linear_algebra.finsupp
 import linear_algebra.direct_sum.tensor_product
 import data.finsupp.to_dfinsupp
 
 /-!
-# Results on direct sums and finitely supported functions.
+# Results on finitely supported functions.
 
-1. The linear equivalence between finitely supported functions `ι →₀ M` and
-the direct sum of copies of `M` indexed by `ι`.
-
-2. The tensor product of ι →₀ M and κ →₀ N is linearly equivalent to (ι × κ) →₀ (M ⊗ N).
+The tensor product of ι →₀ M and κ →₀ N is linearly equivalent to (ι × κ) →₀ (M ⊗ N).
 -/
 
 universes u v w
@@ -24,28 +22,6 @@ open_locale direct_sum
 open set linear_map submodule
 variables {R : Type u} {M : Type v} {N : Type w} [ring R] [add_comm_group M] [module R M]
   [add_comm_group N] [module R N]
-
-section finsupp_lequiv_direct_sum
-
-variables (R M) (ι : Type*) [decidable_eq ι]
-
-/-- The finitely supported functions `ι →₀ M` are in linear equivalence with the direct sum of
-copies of M indexed by ι. -/
-def finsupp_lequiv_direct_sum : (ι →₀ M) ≃ₗ[R] ⨁ i : ι, M :=
-by haveI : Π m : M, decidable (m ≠ 0) := classical.dec_pred _; exact finsupp_lequiv_dfinsupp R
-
-@[simp] theorem finsupp_lequiv_direct_sum_single (i : ι) (m : M) :
-  finsupp_lequiv_direct_sum R M ι (finsupp.single i m) = direct_sum.lof R ι _ i m :=
-finsupp.to_dfinsupp_single i m
-
-@[simp] theorem finsupp_lequiv_direct_sum_symm_lof (i : ι) (m : M) :
-  (finsupp_lequiv_direct_sum R M ι).symm (direct_sum.lof R ι _ i m) = finsupp.single i m :=
-begin
-  letI : Π m : M, decidable (m ≠ 0) := classical.dec_pred _,
-  exact (dfinsupp.to_finsupp_single i m),
-end
-
-end finsupp_lequiv_direct_sum
 
 section tensor_product
 

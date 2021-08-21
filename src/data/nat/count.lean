7b8420932dce3ea41e0b5eb3aa0d7ff22a3bbe37
Kyle Miller <kmill31415@gmail.com>
2021-08-21 03:29:04 -0700

    nth_mem_of_infinite_aux

--- src/data/nat/count.lean
+++ src/data/nat/count.lean
@@ -57,12 +57,29 @@ lemma nth_mem_of_le_card (n : ℕ) (w : (n : cardinal) ≤ cardinal.mk { i | p i
   p (nth p n) :=
 sorry
 
-lemma nth_mem_of_infinite_aux (n : ℕ) (i : set.infinite p) :
+lemma infinite_of_infinite_sdiff_finite {α : Type*} {s t : set α}
+  (hs : s.infinite) (ht : t.finite) : (s \ t).infinite :=
+begin
+  intro hd,
+  have := set.finite.union hd (set.finite.inter_of_right ht s),
+  rw set.diff_union_inter at this,
+  exact hs this,
+end
+
+lemma nth_mem_of_infinite_aux (n : ℕ) (i : set.infinite (set_of p)) :
   nth p n ∈ { i : ℕ | p i ∧ ∀ k < n, nth p k < i } :=
 begin
-  -- This sorry should be relatively easy:
-  -- it's the intersection of an infinite set with finitely many cofinite sets.
-  have ne : set.nonempty { i : ℕ | p i ∧ ∀ k < n, nth p k < i } := sorry,
+  have ne : set.nonempty { i : ℕ | p i ∧ ∀ k < n, nth p k < i },
+  { let s : set ℕ := ⋃ (k < n), { i : ℕ | nth p k ≥ i },
+    convert_to ((set_of p) \ s).nonempty,
+    { ext i, simp, },
+    have : s.finite,
+    { apply set.finite.bUnion,
+      apply set.finite_lt_nat,
+      intros k h,
+      apply set.finite_le_nat, },
+    apply set.infinite.nonempty,
+    apply infinite_of_infinite_sdiff_finite i this, },
   rw nth_def,
   exact Inf_mem ne,
 end

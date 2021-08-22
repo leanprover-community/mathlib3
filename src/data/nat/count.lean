66bf3b2b789d13522277a452ba8d6826fb899bd9
Scott Morrison <scott@tqft.net>
2021-08-22 21:24:53 +1000

    commenting out unneeded lemmas

--- src/data/nat/count.lean
+++ src/data/nat/count.lean
@@ -169,22 +169,22 @@ end
 lemma nth_zero_of_exists (h : ∃ n, p n) : nth p 0 = nat.find h :=
 by { rw [nth_zero], convert nat.Inf_def h, }
 
-lemma nth_eq_zero_iff (n : ℕ) : nth p n = 0 ↔ n = 0 ∧ p 0 ∨ set_of p = ∅ :=
-begin
-  rw nth,
-  split,
-  { simp only [nat.not_lt_zero, set.mem_set_of_eq, Inf_eq_zero],
-    rintro (⟨hp0, hn⟩ | rhs),
-    { rw eq_bot_of_minimal hn,
-      exact or.inl ⟨rfl, hp0⟩ },
-    { sorry }, },
-  { rintro (⟨rfl, hp0⟩ | hnone),
-    { simp [hp0] },
-    { rw nat.Inf_eq_zero,
-      right,
-      rw set.set_of_and,
-      convert set.empty_inter _ } }
-end
+-- lemma nth_eq_zero_iff (n : ℕ) : nth p n = 0 ↔ n = 0 ∧ p 0 ∨ set_of p = ∅ :=
+-- begin
+--   rw nth,
+--   split,
+--   { simp only [nat.not_lt_zero, set.mem_set_of_eq, Inf_eq_zero],
+--     rintro (⟨hp0, hn⟩ | rhs),
+--     { rw eq_bot_of_minimal hn,
+--       exact or.inl ⟨rfl, hp0⟩ },
+--     { sorry }, },
+--   { rintro (⟨rfl, hp0⟩ | hnone),
+--     { simp [hp0] },
+--     { rw nat.Inf_eq_zero,
+--       right,
+--       rw set.set_of_and,
+--       convert set.empty_inter _ } }
+-- end
 
 lemma count_monotone : monotone (count p) :=
 begin
@@ -220,54 +220,54 @@ lemma nth_mem_of_infinite (i : set.infinite (set_of p)) (n : ℕ) : p (nth p n) 
 lemma nth_strict_mono_of_infinite (i : set.infinite (set_of p)) : strict_mono (nth p) :=
 λ n m h, (nth_mem_of_infinite_aux p i m).2 _ h
 
-lemma count_nth_of_lt_card (n : ℕ) (w : (n : cardinal) < cardinal.mk { i | p i }) :
-  count p (nth p n) = n :=
-begin
-  casesI fintype_or_infinite {i | p i},
-  { sorry },
-  { sorry }
-end
+-- lemma count_nth_of_lt_card (n : ℕ) (w : (n : cardinal) < cardinal.mk { i | p i }) :
+--   count p (nth p n) = n :=
+-- begin
+--   casesI fintype_or_infinite {i | p i},
+--   { sorry },
+--   { sorry }
+-- end
 
-lemma nth_mem_of_lt_card (n : ℕ) (w : (n : cardinal) < cardinal.mk { i | p i }) :
-  p (nth p n) :=
-begin
-  casesI fintype_or_infinite {i | p i},
-  { rw [cardinal.fintype_card, cardinal.nat_cast_lt, ←nat.card_eq_fintype_card] at w,
-    sorry, },
-  { apply nth_mem_of_infinite,
-    rwa ←set.infinite_coe_iff, },
-end
+-- lemma nth_mem_of_lt_card (n : ℕ) (w : (n : cardinal) < cardinal.mk { i | p i }) :
+--   p (nth p n) :=
+-- begin
+--   casesI fintype_or_infinite {i | p i},
+--   { rw [cardinal.fintype_card, cardinal.nat_cast_lt, ←nat.card_eq_fintype_card] at w,
+--     sorry, },
+--   { apply nth_mem_of_infinite,
+--     rwa ←set.infinite_coe_iff, },
+-- end
 
-lemma count_nth_of_infinite (i : set.infinite p) (n : ℕ) : count p (nth p n) = n :=
-sorry
+-- lemma count_nth_of_infinite (i : set.infinite p) (n : ℕ) : count p (nth p n) = n :=
+-- sorry
 
-lemma count_nth_gc (i : set.infinite p) : galois_connection (count p) (nth p) :=
-begin
-  rintro x y,
-  rw [nth, le_cInf_iff (⟨0, λ _ _, zero_le _⟩ : bdd_below _)],
-  dsimp,
-  sorry,
-  sorry,
-end
+-- lemma count_nth_gc (i : set.infinite p) : galois_connection (count p) (nth p) :=
+-- begin
+--   rintro x y,
+--   rw [nth, le_cInf_iff (⟨0, λ _ _, zero_le _⟩ : bdd_below _)],
+--   dsimp,
+--   sorry,
+--   sorry,
+-- end
 
-lemma count_le_iff_le_nth {p} [decidable_pred p] (i : set.infinite p) {a b : ℕ} :
-  count p a ≤ b ↔ a ≤ nth p b :=
-count_nth_gc p i _ _
+-- lemma count_le_iff_le_nth {p} [decidable_pred p] (i : set.infinite p) {a b : ℕ} :
+--   count p a ≤ b ↔ a ≤ nth p b :=
+-- count_nth_gc p i _ _
 
-lemma lt_nth_iff_count_lt {p} [decidable_pred p] (i : set.infinite p) {a b : ℕ} :
-  a < count p b ↔ nth p a < b :=
-lt_iff_lt_of_le_iff_le $ count_le_iff_le_nth i
+-- lemma lt_nth_iff_count_lt {p} [decidable_pred p] (i : set.infinite p) {a b : ℕ} :
+--   a < count p b ↔ nth p a < b :=
+-- lt_iff_lt_of_le_iff_le $ count_le_iff_le_nth i
 
-lemma nth_count_le (i : set.infinite p) (n : ℕ) : count p (nth p n) ≤ n :=
-(count_nth_gc _ i).l_u_le _
+-- lemma nth_count_le (i : set.infinite p) (n : ℕ) : count p (nth p n) ≤ n :=
+-- (count_nth_gc _ i).l_u_le _
 
-lemma nth_le_of_le_count (a b : ℕ) (h : a ≤ count p b) : nth p a ≤ b :=
-begin
-  sorry
-end
+-- lemma nth_le_of_le_count (a b : ℕ) (h : a ≤ count p b) : nth p a ≤ b :=
+-- begin
+--   sorry
+-- end
 
-lemma nth_lt_of_lt_count (n k : ℕ) (h : k < count p n) : nth p k < n :=
-sorry
+-- lemma nth_lt_of_lt_count (n k : ℕ) (h : k < count p n) : nth p k < n :=
+-- sorry
 
 lemma nth_of_not_zero (h : ¬ p 0) (k : ℕ) : nth p k = nth (λ i, p (i+1)) k + 1 :=
 begin

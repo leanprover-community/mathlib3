944b39cdbc7b4e1ece02bcce1f296ee93579afb7
RemyDegenne <remydegenne@gmail.com>
2021-08-22 13:44:13 +0200

    doc

--- src/measure_theory/function/conditional_expectation.lean
+++ src/measure_theory/function/conditional_expectation.lean
@@ -373,7 +373,7 @@ begin
     exact hf_zero s hs hμs, },
 end
 
-/-- **Unicity of the conditional expectation** of `Lp` functions. -/
+/-- **Unicity of the conditional expectation** -/
 lemma Lp.ae_eq_of_forall_set_integral_eq'
   (hm : m ≤ m0) (f g : Lp E' p μ) (hp_ne_zero : p ≠ 0) (hp_ne_top : p ≠ ∞)
   (hf_int_finite : ∀ s, measurable_set[m] s → μ s < ∞ → integrable_on f s μ)

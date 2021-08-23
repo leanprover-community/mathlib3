b1d70e6b482404b7cdf2b76a00a5c861b5683864
Bolton Bailey <bolton.bailey@gmail.com>
2021-08-23 04:06:17 -0500

    work on main theorem

---
+++ src/number_theory/lucas_primality.lean
@@ -0,0 +1,116 @@
+/-
+Copyright (c) 2020 Bolton Bailey. All rights reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
+Authors: Bolton Bailey
+-/
+
+import data.zmod.basic
+import data.fintype.basic
+import field_theory.finite.basic
+import group_theory.order_of_element
+
+/-!
+# The Lucas test for primes.
+
+This file implements the Lucas test for primes (not to be confused with the Lucas-Lehmer test for
+Mersenne primes). A number a witnesses that n is prime if a has order n-1 in the multiplicative
+group of integers mod n. This is checked by verifying that a^(n-1) = 1 (mod n) and a^d ≠ 1 (mod n)
+for any divisor d | n. This test is the basis of the Pratt primality certificate.
+
+## TODO
+
+- Implement the test
+- Bonus: Show the reverse implication i.e. if a number is prime then it has a Lucas witness. See [this stackexchange](https://math.stackexchange.com/a/59911/165144) answer for proof.
+- Write a tactic that uses this theorem to generate Pratt primality certificates [see](https://leanprover.zulipchat.com/#narrow/stream/113488-general/topic/norm_num.20for.20real.20exponentiation/near/249615218)
+
+-/
+
+
+-- TODO Mario says this can be made computable
+noncomputable instance units_zmod.fintype : Π n, fintype (units (zmod n))
+| 0     := units_int.fintype
+| (n+1) := units.fintype
+
+
+lemma prime_iff_card_units (p : ℕ) :
+  p.prime ↔ fintype.card (units (zmod p)) = p - 1
+:=
+begin
+  split,
+    { intro p_prime,
+      haveI := fact.mk p_prime,
+      convert zmod.card_units p, },
+    {
+      by_cases 0 < p,
+        { haveI := fact.mk h,
+          rw zmod.card_units_eq_totient p,
+        },
+    }
+end
+
+
+lemma foobar (a b c : ℕ) (a_dvd_b : a ∣ b) : c ∣ b / a ↔ c * a ∣ b :=
+begin
+  sorry,
+end
+
+lemma foosbar (a b c : ℕ) (hc : 0 < c) : (a ∣ b) ↔ (c * a ∣ c * b) :=
+begin
+  exact (nat.mul_dvd_mul_iff_left hc).symm,
+end
+
+theorem lucas_primality (p : ℕ) (a : zmod p)
+  (hp : 1 < p)
+  (ha : a^(p-1) = 1)
+  -- (hd : (∀ d : ℕ, d ∈ (p-1).factors -> a^((p-1) / d) ≠ 1)
+  (hd : (∀ d : ℕ, (nat.prime d) -> (d ∣ (p-1)) -> a^((p-1)/d) ≠ 1))
+  : p.prime :=
+begin
+  -- TODO Use field structure on zmod p
+  -- show the multiplicative group has order p-1
+  have b_dvd_factor : ∃ (b : ℕ), p-1 = (order_of a) * b,
+    exact exists_eq_mul_right_of_dvd (order_of_dvd_of_pow_eq_one ha),
+  cases b_dvd_factor with b hb,
+
+  have b_min_fac_dvd_p_sub_one : b.min_fac ∣ p - 1,
+      have c_dvd_factor : ∃ (c : ℕ), b = c * b.min_fac ,
+        refine exists_eq_mul_left_of_dvd _,
+        exact nat.min_fac_dvd b,
+      cases c_dvd_factor with c hc,
+      rw [hc, <-mul_assoc] at hb,
+      exact dvd.intro_left (order_of a * c) (eq.symm hb),
+
+  have order_of_a : order_of a = p-1,
+    { by_contra,
+      -- Let f be the minimum prime factor of p-1 / order_of a
+      -- f is prime
+      -- f divides p-1
+      -- order_of a divides p-1 / f
+      apply hd (nat.min_fac b),
+      apply @nat.min_fac_prime b,
+      intro hb',
+      rw hb' at hb,
+      rw hb at h,
+      simp at h,
+      exact h,
+      exact b_min_fac_dvd_p_sub_one,
+
+      have hfoo : order_of a ∣ (p-1) / (b.min_fac),
+        rw foobar,
+        rw hb,
+        rw nat.mul_dvd_mul_iff_left,
+        exact nat.min_fac_dvd b,
+        apply order_of_pos',
+        rw is_of_fin_order_iff_pow_eq_one,
+        use (p-1),
+        split,
+        exact sub_pos_iff_lt.mpr hp,
+        exact ha,
+        exact b_min_fac_dvd_p_sub_one,
+        rw <-order_of_dvd_iff_pow_eq_one,
+        exact hfoo, },
+
+
+
+  sorry,
+end

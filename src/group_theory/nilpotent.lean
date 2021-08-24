5289bcd5e66e3da64858ea7b211240930625b1f7
Ines W <ineswright7@gmail.com>
2021-08-24 20:31:19 +0100

    added my name

--- src/group_theory/nilpotent.lean
+++ src/group_theory/nilpotent.lean
@@ -1,7 +1,7 @@
 /-
 Copyright (c) 2021 Kevin Buzzard. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
-Authors: Kevin Buzzard
+Authors: Kevin Buzzard, Ines Wright
 -/
 
 import group_theory.general_commutator

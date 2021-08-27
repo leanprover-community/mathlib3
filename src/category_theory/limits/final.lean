ac300e54591d248bbaf8385c1bb08f6888a46209
Justus Springer <justusspringer@gmx.de>
2021-08-27 15:08:52 +0200

    remove import

--- src/category_theory/limits/final.lean
+++ src/category_theory/limits/final.lean
@@ -3,7 +3,6 @@ Copyright (c) 2020 Scott Morrison. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Scott Morrison
 -/
-import category_theory.adjunction.opposites
 import category_theory.punit
 import category_theory.structured_arrow
 import category_theory.is_connected

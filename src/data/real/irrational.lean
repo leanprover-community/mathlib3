09ca9277339b857b129128f7232e189c0983bc07
Ender Doe <ender@shpazzle.com>
2021-08-27 14:23:34 -0700

    Updated doc string and added type annotation

--- src/data/real/irrational.lean
+++ src/data/real/irrational.lean
@@ -257,10 +257,7 @@ open irrational
 @[simp] theorem irrational_inv_iff : irrational x⁻¹ ↔ irrational x :=
 ⟨of_inv, irrational.inv⟩
 
-/-!
-### Theorem about Irrationals between Reals
--/
-
+/-- There is an irrational number `r` between any two reals `x < r < y`. -/
 theorem exists_irrational_btwn {x y : ℝ} (h : x < y) :
   ∃ r, irrational r ∧ x < r ∧ r < y :=
 let ⟨q, ⟨hq1, hq2⟩⟩ := (exists_rat_btwn ((sub_lt_sub_iff_right (real.sqrt 2)).mpr h)) in

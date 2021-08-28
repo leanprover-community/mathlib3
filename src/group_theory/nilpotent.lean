a743dd35d9b252f91cd96bb8d772eced0cbc7dbb
Ines W <ineswright7@gmail.com>
2021-08-28 21:50:27 +0100

    slight golf

--- src/group_theory/nilpotent.lean
+++ src/group_theory/nilpotent.lean
@@ -287,6 +287,22 @@ begin
     (normal.conj_mem (lower_central_series.subgroup.normal n) z⁻¹ (inv_mem _ hz) a),
 end
 
+lemma lower_central_series_antimono' (m n : ℕ) (h : n < m) :
+  lower_central_series G m ≤ lower_central_series G n :=
+begin
+
+  sorry,
+  -- intros x hx,
+  -- simp only [mem_lower_central_series_succ_iff, exists_prop, mem_top, exists_true_left, true_and]
+  --   at hx,
+  -- refine closure_induction hx _ (subgroup.one_mem _) (@subgroup.mul_mem _ _ _)
+  --   (@subgroup.inv_mem _ _ _),
+  -- rintros y ⟨z, hz, a, ha⟩,
+  -- rw [← ha, mul_assoc, mul_assoc, ← mul_assoc a z⁻¹ a⁻¹],
+  -- exact mul_mem (lower_central_series G n) hz
+    -- (normal.conj_mem (lower_central_series.subgroup.normal n) z⁻¹ (inv_mem _ hz) a),
+end
+
 /-- The lower central series of a group is a descending central series. -/
 theorem lower_central_series_is_descending_central_series :
   is_descending_central_series (lower_central_series G) :=
@@ -342,15 +358,13 @@ begin
   induction n with d hd,
   { simp [nat.nat_zero_eq_zero] },
   { rintros a ⟨x, hx : x ∈ lower_central_series G d.succ, rfl⟩,
-    refine closure_induction hx _ (by simp [f.map_one, subgroup.one_mem _]) _ _,
+    refine closure_induction hx _ (by simp [f.map_one, subgroup.one_mem _])
+      (λ y z hy hz, by simp [monoid_hom.map_mul, subgroup.mul_mem _ hy hz])
+      (λ y hy, by simp [f.map_inv, subgroup.inv_mem _ hy]),
     { rintros a ⟨y, hy, z, ⟨-, rfl⟩⟩,
       apply mem_closure.mpr,
       simp only [exists_prop, mem_top, exists_true_left, true_and, monoid_hom.map_mul,
         monoid_hom.map_mul_inv, set.mem_set_of_eq],
       intros K hK,
-      exact hK ⟨f y, hd (mem_map_of_mem f hy), by use f z⟩ },
-    { intros y z hy hz,
-      simp [monoid_hom.map_mul, subgroup.mul_mem _ hy hz] },
-    { intros y hy,
-      simp [f.map_inv, subgroup.inv_mem _ hy] } }
+      exact hK ⟨f y, hd (mem_map_of_mem f hy), by use f z⟩ } }
 end

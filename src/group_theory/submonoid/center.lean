9e2f86a3a2f737a8752bf8f669ca5255300e5e9d
Eric Wieser <wieser.eric@gmail.com>
2021-08-30 13:02:16 +0100

    lemmas

--- src/group_theory/submonoid/center.lean
+++ src/group_theory/submonoid/center.lean
@@ -41,10 +41,19 @@ lemma inv_mem_center [group M] {a : M}
   (ha : a ∈ set.center M) : a⁻¹ ∈ set.center M :=
 λ g, by by rw [← inv_inj, mul_inv_rev, inv_inv, ← ha, mul_inv_rev, inv_inv]
 
+variables (M)
+
+@[simp, to_additive add_center_eq_univ]
+lemma center_eq_univ [comm_semigroup M] : center M = set.univ :=
+begin
+  ext x,
+  simp [mem_center_iff, mul_comm],
+end
+
 end set
 
-
 namespace submonoid
+section
 variables (M) [monoid M]
 
 /-- The center of a monoid `M` is the set of elements that commute with everything in `M` -/
@@ -55,8 +64,17 @@ def center : submonoid M :=
   one_mem' := set.one_mem_center M,
   mul_mem' := λ a b, set.mul_mem_center }
 
-variable {M}
+variables {M}
 
 @[to_additive] lemma mem_center_iff {z : M} : z ∈ center M ↔ ∀ g, g * z = z * g := iff.rfl
+
+end
+
+section
+variables (M) [comm_monoid M]
+
+@[simp] lemma center_eq_top : center M = ⊤ :=
+set_like.coe_injective (set.center_eq_univ M)
+end
 
 end submonoid

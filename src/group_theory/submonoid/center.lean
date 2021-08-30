96d7dd61cd3c90a4dab27d0e2f3d9cadacd8bbbf
Eric Wieser <wieser.eric@gmail.com>
2021-08-30 13:25:34 +0100

    More lemmas

--- src/group_theory/submonoid/center.lean
+++ src/group_theory/submonoid/center.lean
@@ -4,14 +4,18 @@ Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Eric Wieser
 -/
 import group_theory.submonoid.basic
+
 /-!
 # Centers of magmas and monoids
 
 ## Main definitions
 
-* `set.center`
-* `submonoid.center`
+* `set.center`: the center of a magma
+* `submonoid.center`: the center of a monoid
+* `set.add_center`: the center of an additive magma
+* `add_submonoid.center`: the center of an additive monoid
 
+We show `subgrup.center` and `subsemiring.center` and `subring.center` in other files.
 -/
 
 variables {M : Type*}
@@ -20,7 +24,8 @@ namespace set
 
 variables (M)
 
-@[to_additive add_center]
+/-- The center of a magma. -/
+@[to_additive add_center /-" The center of an additive magma. "-/]
 def center [has_mul M] : set M := {z | ∀ m, m * z = z * m}
 
 @[to_additive mem_add_center]
@@ -28,6 +33,9 @@ lemma mem_center_iff [has_mul M] {z : M} : z ∈ center M ↔ ∀ g, g * z = z *
 
 @[simp, to_additive zero_mem_add_center]
 lemma one_mem_center [mul_one_class M] : (1 : M) ∈ set.center M := by simp [mem_center_iff]
+
+@[simp]
+lemma zero_mem_center [mul_zero_class M] : (0 : M) ∈ set.center M := by simp [mem_center_iff]
 
 variables {M}
 
@@ -41,14 +49,20 @@ lemma inv_mem_center [group M] {a : M}
   (ha : a ∈ set.center M) : a⁻¹ ∈ set.center M :=
 λ g, by by rw [← inv_inj, mul_inv_rev, inv_inv, ← ha, mul_inv_rev, inv_inv]
 
+@[simp]
+lemma add_mem_center [distrib M] {a b : M}
+  (ha : a ∈ set.center M) (hb : b ∈ set.center M) : a + b ∈ set.center M :=
+λ c, by rw [add_mul, mul_add, ha c, hb c]
+
+@[simp]
+lemma neg_mem_center [ring M] {a : M} (ha : a ∈ set.center M) : -a ∈ set.center M :=
+λ c, by rw [←neg_mul_comm, ha (-c), neg_mul_comm]
+
 variables (M)
 
 @[simp, to_additive add_center_eq_univ]
 lemma center_eq_univ [comm_semigroup M] : center M = set.univ :=
-begin
-  ext x,
-  simp [mem_center_iff, mul_comm],
-end
+subset.antisymm (subset_univ _) $ λ x _ y, mul_comm y x
 
 end set
 
@@ -75,6 +89,7 @@ variables (M) [comm_monoid M]
 
 @[simp] lemma center_eq_top : center M = ⊤ :=
 set_like.coe_injective (set.center_eq_univ M)
+
 end
 
 end submonoid

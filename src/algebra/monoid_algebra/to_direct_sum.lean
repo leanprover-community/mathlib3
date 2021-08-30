1e89df22bbce32e6f6fea2c95d6aa52dfadef012
Eric Wieser <wieser.eric@gmail.com>
2021-08-30 14:54:37 +0000

    chore(algebra/monoid_algebra): convert a filename prefix into a folder (#8925)
    This moves:
    * `algebra.monoid_algebra` to `algebra.monoid_algebra.basic`
    * `algebra.monoid_algebra_to_direct_sum` to `algebra.monoid_algebra.to_direct_sum`

--- src/algebra/monoid_algebra/to_direct_sum.lean
+++ src/algebra/monoid_algebra/to_direct_sum.lean
@@ -4,7 +4,7 @@ Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Eric Wieser
 -/
 import algebra.direct_sum.algebra
-import algebra.monoid_algebra
+import algebra.monoid_algebra.basic
 import data.finsupp.to_dfinsupp
 
 /-!

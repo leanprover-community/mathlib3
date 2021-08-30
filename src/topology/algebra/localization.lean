2941147d253f431cb6f3ad88648939777122b7f1
Maria Ines de Frutos Fernandez <mariaines.dff@gmail.com>
2021-08-30 20:08:53 +0100

    Fixed localization.lean

--- src/topology/algebra/localization.lean
+++ src/topology/algebra/localization.lean
@@ -23,9 +23,17 @@ to the equivalence class of `(x, 1)` in the localization of `R` at a `M`.
 
 -/
 
+/- instance {R : Type*}
+  [comm_ring R][t: topological_space R]{M: submonoid R} : topological_space (localization M) :=
+topological_space.coinduced (localization.monoid_of M).to_fun t -/
+
+def localization.ring_topology {R : Type*}
+  [comm_ring R][topological_space R]{M: submonoid R} : ring_topology (localization M) :=
+ring_topology.coinduced (localization.monoid_of M).to_fun
+
 instance {R : Type*}
-  [comm_ring R][t: topological_space R]{M: submonoid R} : ring_topology (localization M) :=
-ring_topology.coinduced (localization.monoid_of M).to_fun t
+  [comm_ring R][topological_space R]{M: submonoid R} : topological_space (localization M) :=
+localization.ring_topology.to_topological_space
 
 instance {R : Type*}
   [comm_ring R][topological_space R]{M: submonoid R} : topological_ring (localization M) :=

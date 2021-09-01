387cbd910f0159ccc9a37c23efa9948f03ad4363
Eric Wieser <wieser.eric@gmail.com>
2021-09-01 10:21:19 +0100

    feat(group_theory/sub{monoid,group}): pointwise actions on `add_sub{monoid,group}`s

--- src/group_theory/subgroup.lean
+++ src/group_theory/subgroup.lean
@@ -2260,6 +2260,39 @@ end subgroup
 
 end actions
 
+/-! ### Pointwise instances on `subgroup`s -/
+
+section
+variables {α : Type*}
+
+namespace add_subgroup
+
+variables [monoid α] [distrib_mul_action α A]
+
+/-- The action on a additive subgroup corresponding to applying the action to every element.
+
+This is available as an instance in the `pointwise` locale. -/
+protected def pointwise_mul_action : mul_action α (add_subgroup A) :=
+{ smul := λ a S, S.map (distrib_mul_action.hom_add_monoid_hom _ _ a),
+  one_smul := λ S, (congr_arg (λ f, S.map f) (monoid_hom.map_one _)).trans S.map_id,
+  mul_smul := λ a₁ a₂ S,
+    (congr_arg (λ f, S.map f) (monoid_hom.map_mul _ _ _)).trans (S.map_map _ _).symm,}
+
+localized "attribute [instance] add_subgroup.pointwise_mul_action" in pointwise
+open_locale pointwise
+
+@[simp] lemma coe_pointwise_smul (a : α) (S : add_subgroup A) : ↑(a • S) = a • (S : set A) := rfl
+
+@[simp] lemma pointwise_smul_to_submonoid (a : α) (S : add_subgroup A) :
+  (a • S).to_submonoid = a • S.to_submonoid := rfl
+
+lemma smul_mem_pointwise_smul (m : A) (a : α) (S : add_subgroup A) : m ∈ S → a • m ∈ a • S :=
+(set.smul_mem_smul_set : _ → _ ∈ a • (S : set A))
+
+end add_subgroup
+
+end
+
 /-! ### Saturated subgroups -/
 
 section saturated
--- src/group_theory/submonoid/operations.lean
+++ src/group_theory/submonoid/operations.lean
@@ -9,6 +9,7 @@ import group_theory.submonoid.basic
 import data.equiv.mul_add
 import algebra.group.prod
 import algebra.group.inj_surj
+import algebra.pointwise
 
 /-!
 # Operations on `submonoid`s
@@ -775,6 +776,7 @@ def of_left_inverse' (f : M →* N) {g : N → M} (h : function.left_inverse g f
 
 end mul_equiv
 
+section actions
 /-! ### Actions by `submonoid`s
 
 These instances tranfer the action by an element `m : M` of a monoid `M` written as `m • a` onto the
@@ -783,7 +785,6 @@ action by an element `s : S` of a submonoid `S : submonoid M` such that `s • a
 These instances work particularly well in conjunction with `monoid.to_mul_action`, enabling
 `s • m` as an alias for `↑s * m`.
 -/
-section actions
 
 namespace submonoid
 
@@ -827,5 +828,35 @@ instance [mul_action M' α] [has_faithful_scalar M' α] (S : submonoid M') :
 { eq_of_smul_eq_smul := λ x y h, subtype.ext (eq_of_smul_eq_smul h) }
 
 end submonoid
+
+/-! ### Pointwise instances on `submonoid`s -/
+
+section
+variables {M' : Type*} {α β : Type*}
+
+namespace add_submonoid
+
+variables [monoid α] [add_monoid M'] [distrib_mul_action α M']
+
+/-- The action on a additive submonoid corresponding to applying the action to every element.
+
+This is available as an instance in the `pointwise` locale. -/
+protected def pointwise_mul_action : mul_action α (add_submonoid M') :=
+{ smul := λ a S, S.map (distrib_mul_action.hom_add_monoid_hom _ _ a),
+  one_smul := λ S, (congr_arg (λ f, S.map f) (monoid_hom.map_one _)).trans S.map_id,
+  mul_smul := λ a₁ a₂ S,
+    (congr_arg (λ f, S.map f) (monoid_hom.map_mul _ _ _)).trans (S.map_map _ _).symm,}
+
+localized "attribute [instance] add_submonoid.pointwise_mul_action" in pointwise
+open_locale pointwise
+
+@[simp] lemma coe_pointwise_smul (a : α) (S : add_submonoid M') : ↑(a • S) = a • (S : set M') := rfl
+
+lemma smul_mem_pointwise_smul (m : M') (a : α) (S : add_submonoid M') : m ∈ S → a • m ∈ a • S :=
+(set.smul_mem_smul_set : _ → _ ∈ a • (S : set M'))
+
+end add_submonoid
+
+end
 
 end actions

5b2ad8072cf9448900e51513d368144f38e4bef5
l534zhan <luming.zhang@merton.ox.ac.uk>
2021-09-01 20:42:18 +0100

    change

--- src/linear_algebra/matrix/symmetric.lean
+++ src/linear_algebra/matrix/symmetric.lean
@@ -12,46 +12,46 @@ This file contains the definition and basic results about symmetric matrices.
 
 ## Main definition
 
- * `matrix.is_sym `: a matrix `A : matrix I I α` is "symmetric" if `Aᵀ = A`.
+ * `matrix.is_sym `: a matrix `A : matrix n n α` is "symmetric" if `Aᵀ = A`.
 
 ## Tags
 
 sym, symmetric, matrix
 -/
 
-variables {α I J R : Type*} [fintype I] [fintype J]
+variables {α n m R : Type*} [fintype n] [fintype m]
 
 namespace matrix
 
 open_locale matrix
 
-/-- A matrix `A : matrix I I α` is "symmetric" if `Aᵀ = A`. -/
-def is_sym (A : matrix I I α) : Prop := Aᵀ = A
+/-- A matrix `A : matrix n n α` is "symmetric" if `Aᵀ = A`. -/
+def is_sym (A : matrix n n α) : Prop := Aᵀ = A
 
-lemma is_sym.eq {A : matrix I I α} (h : A.is_sym) : Aᵀ = A := h
+lemma is_sym.eq {A : matrix n n α} (h : A.is_sym) : Aᵀ = A := h
 
-lemma is_sym.ext_iff {A : matrix I I α} : A.is_sym ↔ ∀ i j, Aᵀ i j = A i j :=
+lemma is_sym.ext_iff {A : matrix n n α} : A.is_sym ↔ ∀ i j, Aᵀ i j = A i j :=
 by rw [is_sym, matrix.ext_iff]
 
-lemma is_sym.ext_iff' {A : matrix I I α} : A.is_sym ↔ ∀ i j, A j i = A i j :=
+lemma is_sym.ext_iff' {A : matrix n n α} : A.is_sym ↔ ∀ i j, A j i = A i j :=
 is_sym.ext_iff
 
-lemma is_sym.apply {A : matrix I I α} (h : A.is_sym) (i j : I) : Aᵀ i j = A i j :=
+lemma is_sym.apply {A : matrix n n α} (h : A.is_sym) (i j : n) : Aᵀ i j = A i j :=
 is_sym.ext_iff.1 h i j
 
-lemma is_sym.apply' {A : matrix I I α} (h : A.is_sym) (i j : I) : A j i = A i j :=
+lemma is_sym.apply' {A : matrix n n α} (h : A.is_sym) (i j : n) : A j i = A i j :=
 is_sym.apply h i j
 
-lemma is_sym.ext {A : matrix I I α} : (∀ i j, Aᵀ i j = A i j) → A.is_sym :=
+lemma is_sym.ext {A : matrix n n α} : (∀ i j, Aᵀ i j = A i j) → A.is_sym :=
 is_sym.ext_iff.2
 
-lemma is_sym.ext' {A : matrix I I α} : (∀ i j, A j i = A i j) → A.is_sym :=
+lemma is_sym.ext' {A : matrix n n α} : (∀ i j, A j i = A i j) → A.is_sym :=
 is_sym.ext
 
 /-- A block matrix `A.from_blocks B C D` is symmetric,
     if `A` and `D` are symmetric and `Bᵀ = C`. -/
 lemma is_sym_from_blocks
-  {A : matrix I I α} {B : matrix I J α} {C : matrix J I α} {D : matrix J J α}
+  {A : matrix m m α} {B : matrix m n α} {C : matrix n m α} {D : matrix n n α}
   (h1 : A.is_sym) (h2 : D.is_sym) (h3 : Bᵀ = C) :
   (A.from_blocks B C D).is_sym :=
 begin
@@ -63,27 +63,41 @@ begin
 end
 
 /-- `A ⬝ Aᵀ` is symmertric. -/
-lemma is_sym_mul_transpose_self [comm_semiring α] (A : matrix I I α) :
+lemma is_sym_mul_transpose_self [comm_semiring α] (A : matrix n n α) :
   (A ⬝ Aᵀ).is_sym :=
 by simp [matrix.is_sym, transpose_mul]
 
 /-- The identity matrix is symmetric. -/
-@[simp] lemma is_sym_one [decidable_eq I] [has_zero α] [has_one α] :
-  (1 : matrix I I α).is_sym := by {ext, simp}
+@[simp] lemma is_sym_one [decidable_eq n] [has_zero α] [has_one α] :
+  (1 : matrix n n α).is_sym := by {ext, simp}
 
 /-- The negtive identity matrix is symmetric. -/
-@[simp] lemma is_sym_neg_one [decidable_eq I] [has_zero α] [has_one α] [has_neg α] :
-  (-1 : matrix I I α).is_sym := by {ext, simp}
+@[simp] lemma is_sym_neg_one [decidable_eq n] [has_zero α] [has_one α] [has_neg α] :
+  (-1 : matrix n n α).is_sym := by {ext, simp}
+
+
+@[simp] lemma is_sym.smul
+[has_scalar R α] {A : matrix n n α} (h : A.is_sym) (k : R) :
+(k • A).is_sym :=
+by ext; simp [h.apply']
 
 /-- The identity matrix multiplied by any scalar `k` is symmetric. -/
 @[simp] lemma is_sym_of_smul_one
-[decidable_eq I] [monoid R] [add_monoid α] [has_one α] [distrib_mul_action R α] (k : R) :
-(k • (1 : matrix I I α)).is_sym :=
-by { ext, simp [is_sym_of_one.apply'] }
+[decidable_eq n] [has_zero α] [has_one α] [has_scalar R α] (k : R) :
+(k • (1 : matrix n n α)).is_sym :=
+is_sym_one.smul k
+
+@[simp] lemma is_sym_diagonal [decidable_eq n] [has_zero α] (v : n → α) :
+(diagonal v).is_sym :=
+begin
+  ext,
+  by_cases i = j; simp [*, diagonal],
+  intro g, exfalso, exact h g.symm
+end
 
 /-- If a block matrix `A.from_blocks B C D` is symmetric,-/
 lemma block_conditions_of_is_sym
-{A : matrix I I α} {B : matrix I J α} {C : matrix J I α} {D : matrix J J α} :
+{A : matrix m m α} {B : matrix m n α} {C : matrix n m α} {D : matrix n n α} :
 (A.from_blocks B C D).is_sym → (A.is_sym) ∧ (D.is_sym) ∧ (Cᵀ = B) ∧ (Bᵀ = C) :=
 begin
   rintros h,

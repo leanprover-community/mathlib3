2da2c8c77f418b0ee77ba750531cfb6010201f6f
Anatole Dedecker <anatolededecker@gmail.com>
2021-09-02 16:55:55 +0200

    ++

--- src/analysis/integral_comparison.lean
+++ src/analysis/integral_comparison.lean
@@ -1,4 +1,13 @@
+/-
+Copyright (c) 2021 Anatole Dedecker. All rights reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
+Authors: Anatole Dedecker
+-/
 import measure_theory.integral_eq_improper
+
+/-!
+TODO
+-/
 
 open measure_theory set filter finset
 

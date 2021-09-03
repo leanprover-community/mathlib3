a1f2d0620262acdff1f84c36dc8548d6e11cda41
Kalle-t30200-lr228 <kalle.kytola@aalto.fi>
2021-09-04 01:24:47 +0300

    Edits mainly based on the review.

--- src/measure_theory/measure/finite_measure_weak_convergence.lean
+++ src/measure_theory/measure/finite_measure_weak_convergence.lean
@@ -44,6 +44,21 @@ No new notation is introduced.
 The topology of weak convergence of finite Borel measures will be defined using a mapping from
 `finite_measure α` to `weak_dual ℝ≥0 (α →ᵇ ℝ≥0)`, inheriting the topology from there.
 
+The current first implementation of `finite_measure α` and `probability_measure α` is directly
+as subtypes of `measure α`, and the coercion to a function is the composition `ennreal.to_nnreal`
+and the coercion to function of `measure α`. Another alternative would be to use a bijection
+with `vector_measure α ℝ≥0` as an intermediate step. This can be changed if
+
+Potential advantages of using the `nnreal`-valued vector measure alternative:
+ * The coercion to function would avoid need to compose with `ennreal.to_nnreal`, the
+   `nnreal`-valued API could be more directly available.
+Potential drawbacks of the vector measure alternative:
+ * The coercion to function would lose monotonicity, as non-measurable sets would be defined to
+   have measure 0.
+ * No integration theory directly. E.g., the topology definition requires `lintegral` w.r.t.
+   a coercion to `measure α` in any case.
+
+
 ## References
 
 * [Billingsley, *Convergence of probability measures*][billingsley1999]
@@ -62,14 +77,6 @@ open_locale topological_space ennreal nnreal
 
 namespace measure_theory
 
-section finite_measure
-/-!
-### Finite measures
-In this section, we define the `Type` of finite measures on a given measurable space. This type
-will be equipped with the topology of weak convergence of measures when the measurable space is
-a topological space equipped with its Borel sigma-algebra.
--/
-
 variables {α : Type*} [measurable_space α]
 
 /-- Finite measures are defined as the subtype of measures that have the property of being finite
@@ -80,18 +87,9 @@ def finite_measure (α : Type*) [measurable_space α] : Type* :=
 namespace finite_measure
 
 instance has_zero : has_zero (finite_measure α) :=
-{ zero := ⟨0, is_finite_measure_zero⟩ }
+{ zero := ⟨0, measure_theory.is_finite_measure_zero⟩ }
 
-instance : inhabited (finite_measure α) := { default := 0 }
-
--- TODO: A coercion `finite_measure α → measure α` is crucial. Now it is the subtype coercion.
--- Another alternative is to use a bijection with `vector_measure α ℝ≥0` as an intermediate step.
--- Potential advantages of the vector measure alternative:
---  * Structures of additive commutative monoid and ℝ≥0 module on `finite_measure α` could be
---    "inherited".
--- Potential drawbacks of the vector measure alternative:
---  * No integration theory directly.
---  * Monotonicity lost as non-measurable sets defined to have measure 0.
+instance : inhabited (finite_measure α) := ⟨0⟩
 
 /-- A finite measure can be interpreted as a measure. -/
 instance : has_coe (finite_measure α) (measure_theory.measure α) := coe_subtype
@@ -131,18 +129,25 @@ lemma coe_zero : (coe : finite_measure α → measure α) 0 = 0 := rfl
 
 @[simp] lemma coe_smul (c : ℝ≥0) (μ : finite_measure α) : ↑(c • μ) = (c • ↑μ : measure α) := rfl
 
+@[simp] lemma coe_fn_zero :
+  (⇑(0 : finite_measure α) : set α → ℝ≥0) = (0 : set α → ℝ≥0) :=
+by { funext, refl, }
+
+@[simp] lemma coe_fn_add (μ ν : finite_measure α) :
+  (⇑(μ + ν) : set α → ℝ≥0) = (⇑μ + ⇑ν : set α → ℝ≥0) :=
+by { funext, simp [← ennreal.coe_eq_coe], }
+
+@[simp] lemma coe_fn_smul (c : ℝ≥0) (μ : finite_measure α) :
+  (⇑(c • μ) : set α → ℝ≥0) = c • (⇑μ : set α → ℝ≥0) :=
+by { funext, simp [← ennreal.coe_eq_coe], refl, }
+
 /-- The (total) mass of a finite measure `μ` is `μ univ`, i.e., the cast to `nnreal` of
 `(μ : measure α) univ`. -/
 def mass (μ : finite_measure α) : ℝ≥0 := μ univ
 
 @[simp] lemma mass_ennreal {μ : finite_measure α} :
-  (μ.mass : ℝ≥0∞) = (μ : measure α) univ :=
-begin
-  apply ennreal.coe_to_nnreal,
-  exact ennreal.lt_top_iff_ne_top.mp (μ.prop).measure_univ_lt_top,
-end
+  (μ.mass : ℝ≥0∞) = (μ : measure α) univ := to_fun_coe_eq_to_measure μ set.univ
 
--- TODO: Another alternative would use a bijection with `vector_measure α ℝ≥0`.
 instance : add_comm_monoid (finite_measure α) :=
 (finite_measure.coe_injective).add_comm_monoid
   (coe : finite_measure α → measure α) finite_measure.coe_zero finite_measure.coe_add
@@ -152,25 +157,10 @@ instance : add_comm_monoid (finite_measure α) :=
 def coe_add_monoid_hom : finite_measure α →+ measure α :=
 { to_fun := coe, map_zero' := coe_zero, map_add' := coe_add }
 
--- TODO: Another alternative would use a bijection with `vector_measure α ℝ≥0`.
 instance {α : Type*} [measurable_space α] : module ℝ≥0 (finite_measure α) :=
 function.injective.module _ coe_add_monoid_hom finite_measure.coe_injective coe_smul
 
 end finite_measure -- end namespace
-
-end finite_measure -- end section
-
-section probability_measure
-/-!
-### Probability measures
-In this section, we define the `Type` of probability measures on a given measurable space. This
-type will be equipped with the topology of weak convergence of measures when the measurable space
-is a topological space equipped with its Borel sigma-algebra.
-
-There is a coercion (embedding) to finite measures on the same space.
--/
-
-variables {α : Type} [measurable_space α]
 
 /-- Probability measures are defined as the subtype of measures that have the property of being
 probability measures (i.e., their total mass is one). -/
@@ -209,9 +199,6 @@ def to_finite_measure (μ : probability_measure α) : (finite_measure α) := ⟨
 lemma to_finite_measure_coe_eq_coe (ν : probability_measure α) :
   (ν.to_finite_measure : measure α) = (ν : measure α) := rfl
 
-lemma to_finite_measure_coe_eq_val (ν : probability_measure α) :
-  (ν.to_finite_measure : measure α) = ν.val := rfl
-
 @[simp]
 lemma to_finite_measure_to_fun_eq_to_fun (ν : probability_measure α) :
   (ν.to_finite_measure : set α → ℝ≥0) = (ν : set α → ℝ≥0) := rfl
@@ -229,7 +216,5 @@ lemma to_finite_measure_mass (μ : probability_measure α) :
 μ.to_fun_univ
 
 end probability_measure -- end namespace
-
-end probability_measure -- end section
 
 end measure_theory

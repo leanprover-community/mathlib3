c3e87ef24a167d915047328ba3df8ee8aa060285
Thomas Browning <tb65536@uw.edu>
2021-09-03 10:56:46 -0700

    fix imports

--- src/group_theory/group_action/basic.lean
+++ src/group_theory/group_action/basic.lean
@@ -3,8 +3,10 @@ Copyright (c) 2018 Chris Hughes. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Chris Hughes
 -/
+import data.setoid.basic
 import group_theory.group_action.defs
 import group_theory.group_action.group
+import data.fintype.card
 import group_theory.quotient_group
 
 /-!

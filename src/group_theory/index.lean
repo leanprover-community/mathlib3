bcbb6c4d8cee96490897781b307889efc5c747c5
Thomas Browning <tb65536@uw.edu>
2021-09-02 20:17:26 -0700

    computable!

--- src/group_theory/index.lean
+++ src/group_theory/index.lean
@@ -55,26 +55,33 @@ end
 variables {H} {K : subgroup G}
 
 /-- If `H ≤ K` then `G/H ≃ G/K × K/H` -/
+def quotient_equiv_prod_of_le' (h_le : H ≤ K) (f : quotient_group.quotient K → G)
+  (hf : function.right_inverse f quotient_group.mk) :
+  quotient_group.quotient H ≃
+  quotient_group.quotient K × quotient_group.quotient (H.subgroup_of K) :=
+{ to_fun := λ a, ⟨a.map' id (by exact λ b c h, h_le h),
+    a.map' (λ g : G, ⟨(f (quotient.mk' g))⁻¹ * g, quotient.exact' (hf g)⟩) (λ b c h, by
+    { change ((f b)⁻¹ * b)⁻¹ * ((f c)⁻¹ * c) ∈ H,
+      have key : f b = f c := congr_arg f (quotient.sound' (h_le h)),
+      rwa [key, mul_inv_rev, inv_inv, mul_assoc, mul_inv_cancel_left] })⟩,
+  inv_fun := λ a, a.2.map' (λ b, f a.1 * b) (λ b c h, by
+  { change (f a.1 * b)⁻¹ * (f a.1 * c) ∈ H,
+    rwa [mul_inv_rev, mul_assoc, inv_mul_cancel_left] }),
+  left_inv := by
+  { refine quotient.ind' (λ a, _),
+    simp_rw [quotient.map'_mk', id.def, K.coe_mk, mul_inv_cancel_left] },
+  right_inv := by
+  { refine prod.rec _,
+    refine quotient.ind' (λ a, _),
+    refine quotient.ind' (λ b, _),
+    have key : quotient.mk' (f (quotient.mk' a) * b) = quotient.mk' a :=
+    (quotient_group.mk_mul_of_mem (f a) ↑b b.2).trans (hf a),
+    simp_rw [quotient.map'_mk', id.def, key, inv_mul_cancel_left, subtype.coe_eta] } }
+
+/-- If `H ≤ K` then `G/H ≃ G/K × K/H` -/
 noncomputable def quotient_equiv_prod_of_le (h_le : H ≤ K) : quotient_group.quotient H ≃
   quotient_group.quotient K × quotient_group.quotient (H.subgroup_of K) :=
-{ to_fun := λ a, ⟨quotient_group.mk a.out', quotient_group.mk
-    ⟨(quotient_group.mk a.out' : quotient_group.quotient K).out'⁻¹ * a.out',
-      by rw [←quotient_group.eq', quotient_group.out_eq']⟩⟩,
-  inv_fun := λ a, quotient_group.mk (a.1.out' * a.2.out'),
-  left_inv := λ a, by
-  { obtain ⟨x₁, h₁⟩ := quotient_group.mk_out'_eq_mul K a.out',
-    obtain ⟨x₂, h₂⟩ := quotient_group.mk_out'_eq_mul (H.subgroup_of K) x₁⁻¹,
-    simp_rw [h₁, mul_inv_rev, inv_mul_cancel_right, ←K.coe_inv, subtype.coe_eta],
-    simp_rw [h₂, K.coe_mul, K.coe_inv, mul_assoc, mul_inv_cancel_left],
-    exact (quotient_group.mk_mul_of_mem a.out' x₂ x₂.2).trans a.out_eq' },
-  right_inv := λ a, by
-  { obtain ⟨x, h⟩ := quotient_group.mk_out'_eq_mul H (a.1.out' * a.2.out'),
-    simp_rw [h, mul_assoc, quotient_group.mk_mul_of_mem a.1.out' (a.2.out' * x)
-      (K.mul_mem a.2.out'.2 (h_le x.2)), quotient_group.out_eq', inv_mul_cancel_left],
-    change (a.1, quotient_group.mk (⟨(a.2.out' * ⟨x, h_le x.2⟩ : K), _⟩ : K)) = a,
-    simp_rw subtype.coe_eta,
-    exact prod.ext rfl ((quotient_group.mk_mul_of_mem a.2.out' ⟨x, h_le x.2⟩ (by exact x.2)).trans
-      a.2.out_eq') } }
+quotient_equiv_prod_of_le' h_le quotient.out' quotient.out_eq'
 
 lemma index_eq_mul_of_le (h_le : H ≤ K) : H.index = K.index * (H.subgroup_of K).index :=
 (congr_arg cardinal.to_nat (by exact cardinal.eq_congr (quotient_equiv_prod_of_le h_le))).trans

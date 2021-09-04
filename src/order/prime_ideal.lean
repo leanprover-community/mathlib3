a1cb44dd247b28a7a154de9aa93e91f40b2e0108
Noam Atar <atarnoam@gmail.com>
2021-09-04 16:11:22 +0300

    - Added is_prime_iff_mem_or_compl_mem - Changed signature of is_prime_of_mem_or_compl_mem

--- src/order/prime_ideal.lean
+++ src/order/prime_ideal.lean
@@ -163,20 +163,23 @@ end
 lemma is_prime.mem_compl_of_not_mem (hI : is_prime I) (hxnI : x ∉ I) : xᶜ ∈ I :=
 hI.mem_or_compl_mem.resolve_left hxnI
 
-lemma is_prime_of_mem_or_compl_mem [is_proper I] (h : ∀ (x : P), (x ∈ I) ∨ (xᶜ ∈ I)) : is_prime I :=
+lemma is_prime_of_mem_or_compl_mem [is_proper I] (h : ∀ {x : P}, (x ∈ I) ∨ (xᶜ ∈ I)) : is_prime I :=
 begin
   rw is_prime_iff_mem_or_mem,
   intros x y hxy,
   rw or_iff_not_imp_left,
   intro hxI,
   have hxcI : xᶜ ∈ I,
-  exact (h x).resolve_left hxI,
+  exact h.resolve_left hxI,
   suffices ass : (y ⊓ x) ⊔ (y ⊓ xᶜ) ∈ I,
   rwa sup_inf_inf_compl at ass,
   apply sup_mem,
   rwa inf_comm,
   exact mem_of_le I inf_le_right hxcI,
 end
+
+lemma is_prime_iff_mem_or_compl_mem [is_proper I] : is_prime I ↔ ∀ {x : P}, (x ∈ I) ∨ (xᶜ ∈ I) :=
+⟨λ h _, h.mem_or_compl_mem, is_prime_of_mem_or_compl_mem⟩
 
 end boolean_algebra
 

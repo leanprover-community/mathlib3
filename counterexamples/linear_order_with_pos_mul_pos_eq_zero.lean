1a69f54b183af2eb42ea71da4dfc59dad41959ad
Scott Morrison <scott@tqft.net>
2021-09-06 17:14:23 +1000

    merge

--- counterexamples/linear_order_with_pos_mul_pos_eq_zero.lean
+++ counterexamples/linear_order_with_pos_mul_pos_eq_zero.lean
@@ -1,8 +1,8 @@
 /-
-Copyright (c) 2021 Johan Commelin (inspired by Kevin Buzzard, copied by Damiano Testa).
+Copyright (c) 2021 Johan Commelin.
 All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
-Authors: Johan Commelin (inspired by Kevin Buzzard, copied by Damiano Testa)
+Authors: Johan Commelin, Damiano Testa, Kevin Buzzard
 -/
 import algebra.ordered_monoid
 

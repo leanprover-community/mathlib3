b415f95d19ca940e6ab5ebde534cc43d5f3f6e16
Bolton Bailey <bolton.bailey@gmail.com>
2021-09-06 18:01:13 -0500

    Update src/data/nat/totient.lean
    Co-authored-by: Johan Commelin <johan@commelin.net>

--- src/data/nat/totient.lean
+++ src/data/nat/totient.lean
@@ -205,12 +205,9 @@ begin
   rwa [succ_le_iff, pos_iff_ne_zero],
 end
 
-lemma card_units_zmod_lt_sub_one {p : ℕ} (hp : 1 < p) [fact (0 < p)]
-  : fintype.card (units (zmod p)) ≤ p - 1 :=
-begin
-  rw zmod.card_units_eq_totient p,
-  exact nat.le_pred_of_lt (nat.totient_lt p hp),
-end
+lemma card_units_zmod_lt_sub_one {p : ℕ} (hp : 1 < p) [fact (0 < p)] :
+  fintype.card (units (zmod p)) ≤ p - 1 :=
+by { rw zmod.card_units_eq_totient p, exact nat.le_pred_of_lt (nat.totient_lt p hp), }
 
 lemma prime_iff_card_units (p : ℕ) [fact (0 < p)] :
   p.prime ↔ fintype.card (units (zmod p)) = p - 1 :=

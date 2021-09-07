4f156ee13bdcfa176afeb8a957acff67643d5ade
Riccardo Brasca <riccardo.brasca@gmail.com>
2021-09-07 17:24:50 +0200

    feat(linear_algebra/free_module): add instances

--- src/linear_algebra/free_module.lean
+++ src/linear_algebra/free_module.lean
@@ -7,6 +7,10 @@ Authors: Riccardo Brasca
 import linear_algebra.direct_sum.finsupp
 import linear_algebra.std_basis
 import logic.small
+import ring_theory.finiteness
+import linear_algebra.matrix.to_lin
+
+open_locale big_operators
 
 /-!
 
@@ -131,16 +135,88 @@ instance pi {ι : Type*} [fintype ι] {M : ι → Type*} [Π (i : ι), add_comm_
 [Π (i : ι), module R (M i)] [Π (i : ι), module.free R (M i)] : module.free R (Π i, M i) :=
 of_basis $ pi.basis $ λ i, choose_basis R (M i)
 
+instance matrix {ι₁ : Type*} [fintype ι₁] {ι₂ : Type*} [fintype ι₂] :
+  module.free R (matrix ι₁ ι₂ R) :=
+of_basis $ pi.basis $ λ i, pi.basis_fun _ _
+
 end semiring
+
+section ring
+
+variables [ring R] [add_comm_group M] [module R M] [module.free R M]
+
+/-- If a free module is finite, then any basis is finite. -/
+noncomputable
+instance [nontrivial R] [module.finite R M] :
+  fintype (module.free.choose_basis_index R M) :=
+begin
+  classical,
+  unfreezingI { obtain ⟨h⟩ := ‹module.finite R M› },
+  choose s hs using h,
+  let b := module.free.choose_basis R M,
+  let t := (s.image b.repr).bUnion finsupp.support,
+  refine { elems := t, complete := _ },
+  intro i,
+  by_contra hi,
+  refine linear_dependent_iff.mpr _ b.linear_independent,
+  rw [submodule.eq_top_iff'] at hs,
+  specialize hs (b i),
+  rw [finsupp.mem_span_iff_total] at hs,
+  obtain ⟨c, hc⟩ := hs,
+  let g := (∑ m : {x // x ∈ s}, c m • b.repr m) - finsupp.single i 1,
+  have hgi : g i = -1,
+  { rw [finsupp.sub_apply, finsupp.single_apply, if_pos rfl, finset.sum_apply', finset.sum_eq_zero,
+      zero_sub],
+    rintro j -,
+    rw [finsupp.smul_apply],
+    suffices : b.repr j i = 0, { rw [this, smul_zero] },
+    rw [← finsupp.not_mem_support_iff],
+    contrapose! hi,
+    simp only [finset.mem_bUnion, exists_prop, finset.mem_image],
+    exact ⟨_, ⟨j, j.2, rfl⟩, hi⟩, },
+  refine ⟨insert i t, g, _, ⟨i, finset.mem_insert_self _ _, _⟩⟩,
+  { rw [finset.sum_insert hi, hgi, neg_one_smul, neg_add_eq_zero],
+    dsimp only [g],
+    simp only [finsupp.sub_apply, sub_smul, finset.sum_sub_distrib],
+    rw [eq_comm, sub_eq_iff_eq_add, eq_comm, finset.sum_eq_zero, add_zero],
+    { simp only [finset.sum_apply', finset.sum_smul],
+      rw [finset.sum_comm, ← hc, finsupp.total_apply, finsupp.sum_fintype],
+      refine fintype.sum_congr _ _ _,
+      { intro m,
+        simp only [finsupp.smul_apply, smul_assoc, ← finset.smul_sum],
+        congr' 1,
+        convert (b.total_repr m).symm using 1, symmetry,
+        apply finset.sum_subset,
+        { intros x hx, rw finset.mem_bUnion,
+          simp only [finset.mem_bUnion, exists_prop, finset.mem_image],
+          exact ⟨_, ⟨_, m.2, rfl⟩, hx⟩, },
+        { intros x hx1 hx2,
+          rw [finsupp.not_mem_support_iff] at hx2,
+          simp only [linear_map.smul_right_apply, linear_map.id_apply, hx2, zero_smul], } },
+      { intro, exact zero_smul _ _ } },
+    { intros j hj,
+      rw [finsupp.single_apply, if_neg, zero_smul],
+      rintro rfl, exact hi hj, } },
+  { rw [hgi, ne.def, neg_eq_zero], exact one_ne_zero }
+end
+
+end ring
 
 section comm_ring
 
-variables [comm_ring R] [add_comm_group M] [module R M]
-variables [add_comm_group N] [module R N]
+variables [comm_ring R] [add_comm_group M] [module R M] [module.free R M]
+variables [add_comm_group N] [module R N] [module.free R N]
 
-instance tensor [module.free R M] [module.free R N] : module.free R (M ⊗[R] N) :=
+instance tensor : module.free R (M ⊗[R] N) :=
 of_equiv' (of_equiv' (finsupp.free R) (finsupp_tensor_finsupp' R _ _).symm)
   (tensor_product.congr (choose_basis R M).repr (choose_basis R N).repr).symm
+
+instance [nontrivial R] [module.finite R M] [module.finite R N] : module.free R (M →ₗ[R] N) :=
+begin
+  classical,
+  exact of_equiv
+    (linear_map.to_matrix (module.free.choose_basis R M) (module.free.choose_basis R N)).symm,
+end
 
 end comm_ring
 

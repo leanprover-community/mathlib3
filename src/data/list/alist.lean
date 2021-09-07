660a8756152fe1a85e196f4bf9e1a637962655f9
Bolton Bailey <bolton.bailey@gmail.com>
2021-09-06 23:34:49 -0500

    Update src/data/list/alist.lean
    Co-authored-by: Scott Morrison <scott.morrison@gmail.com>

--- src/data/list/alist.lean
+++ src/data/list/alist.lean
@@ -8,7 +8,7 @@ import data.list.sigma
 /-!
 # Association Lists
 
-This file makes use of the contents of the `list.sigma` file to define association lists. These
+This file makes use of the contents of the `data.list.sigma` file to define association lists. These
 association lists are represented by the `alist` structure, which comprises a list of sigmas as a
 key-value store, as well as a proof that this has no duplicate keys.
 

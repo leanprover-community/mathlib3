34e2ad6be0982270e1d5cf886cf563572abaf4ac
Johan Commelin <johan@commelin.net>
2021-09-08 08:39:44 +0000

    golf

--- src/ring_theory/discrete_valuation_ring.lean
+++ src/ring_theory/discrete_valuation_ring.lean
@@ -91,6 +91,10 @@ begin
     use a * b, exact hh.symm }
 end⟩
 
+lemma _root_.irreducible.maximal_ideal_eq {ϖ : R} (h : irreducible ϖ) :
+  maximal_ideal R = ideal.span {ϖ} :=
+(irreducible_iff_uniformizer _).mp h
+
 variable (R)
 
 /-- Uniformisers exist in a DVR -/
@@ -425,6 +429,10 @@ by simpa only [one_mul, eq_self_iff_true, units.coe_one, pow_one, forall_true_le
 lemma add_val_pow (a : R) (n : ℕ) : add_val R (a ^ n) = n • add_val R a :=
 (add_val R).map_pow _ _
 
+lemma _root_.irreducible.add_val_pow {ϖ : R} (h : irreducible ϖ) (n : ℕ) :
+  add_val R (ϖ ^ n) = n :=
+by rw [add_val_pow, add_val_uniformizer h, nsmul_one]
+
 lemma add_val_eq_top_iff {a : R} : add_val R a = ⊤ ↔ a = 0 :=
 begin
   have hi := (classical.some_spec (exists_prime R)).irreducible,
@@ -461,33 +469,15 @@ lemma add_val_add {a b : R} :
 
 end
 
--- TODO: fix this
-lemma enat.coe_eq_coe (n : ℕ) :
-  @eq enat
-    (@coe nat enat (@coe_to_lift nat enat (@nat.cast_coe enat (@add_zero_class.to_has_zero enat
-      (@add_monoid.to_add_zero_class enat
-        (@add_comm_monoid.to_add_monoid enat enat.add_comm_monoid)))
-        enat.has_one (@add_zero_class.to_has_add enat (@add_monoid.to_add_zero_class enat
-          (@add_comm_monoid.to_add_monoid enat enat.add_comm_monoid))))) n)
-    (@coe nat enat (@coe_to_lift nat enat (@coe_base nat enat enat.has_coe)) n) :=
-begin
-  induction n with n ih, { refl },
-  simp only [nat.succ_eq_add_one, enat.coe_one, enat.coe_add, nat.cast_add, nat.cast_one, ih],
-end
-
 instance (R : Type*) [integral_domain R] [discrete_valuation_ring R] :
   is_Hausdorff (maximal_ideal R) R :=
 { haus' := λ x hx,
   begin
-    simp only [← ideal.one_eq_top, smul_eq_mul, mul_one, smodeq.zero] at hx,
-    rw [← add_val_eq_top_iff, enat.eq_top_iff_forall_le],
-    intro n,
     obtain ⟨ϖ, hϖ⟩ := exists_irreducible R,
-    have : add_val R (ϖ ^ n) = n,
-    { rw [add_val_pow, add_val_uniformizer hϖ, nsmul_one, enat.coe_eq_coe], },
-    rw irreducible_iff_uniformizer at hϖ,
-    rw [← this, add_val_le_iff_dvd, ← ideal.mem_span_singleton, ← ideal.span_singleton_pow, ← hϖ],
-    exact hx n,
+    simp only [← ideal.one_eq_top, smul_eq_mul, mul_one, smodeq.zero,
+      hϖ.maximal_ideal_eq, ideal.span_singleton_pow, ideal.mem_span_singleton,
+      ← add_val_le_iff_dvd, hϖ.add_val_pow] at hx,
+    rwa [← add_val_eq_top_iff, enat.eq_top_iff_forall_le],
   end }
 
 end discrete_valuation_ring

994254b6a06fa7d24a5679a7f49eeec5aa0f7814
RemyDegenne <Remydegenne@gmail.com>
2021-09-08 17:06:07 +0200

    add authors

--- src/measure_theory/integral/set_to_l1.lean
+++ src/measure_theory/integral/set_to_l1.lean
@@ -1,7 +1,7 @@
 /-
 Copyright (c) 2021 Rémy Degenne. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
-Authors: Rémy Degenne
+Authors: Zhouhang Zhou, Yury Kudryashov, Sébastien Gouëzel, Rémy Degenne
 -/
 import measure_theory.function.simple_func_dense
 

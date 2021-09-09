a84cbf8930c79678e5a15717700f74882bf73ccf
Justus Springer <justusspringer@gmx.de>
2021-09-09 20:51:53 +0200

    to_additive everywhere

--- src/algebra/category/Mon/filtered_colimits.lean
+++ src/algebra/category/Mon/filtered_colimits.lean
@@ -40,14 +40,18 @@ parameters {J : Type v} [small_category J] (F : J ⥤ Mon.{v})
 The colimit of `F ⋙ forget Mon` in the category of types.
 The task of this file is to construct a monoid structure on `M`.
 -/
+@[to_additive]
 abbreviation M : Type v := types.quot (F ⋙ forget Mon)
 
 /-- The canonical projection to the colimit. -/
+@[to_additive]
 abbreviation M.mk : (Σ j, F.obj j) → M := quot.mk (types.quot.rel (F ⋙ forget Mon))
 
+@[to_additive]
 instance monoid_obj (j) : monoid ((F ⋙ forget Mon).obj j) :=
 by { change monoid (F.obj j), apply_instance }
 
+@[to_additive]
 lemma M.mk_eq (x y : Σ j, F.obj j)
   (h : ∃ (k : J) (f : x.1 ⟶ k) (g : y.1 ⟶ k), F.map f x.2 = F.map g y.2) :
   M.mk x = M.mk y :=
@@ -59,12 +63,14 @@ variables [is_filtered J]
 As `J` is nonempty, we can pick an arbitrary object `j₀ : J`. We use this object to define the
 "one" in the colimit as the equivalence class of `⟨j₀, 1 : F.obj j₀⟩`.
 -/
+@[to_additive]
 def colimit_one : M := M.mk ⟨is_filtered.nonempty.some, 1⟩
 
 /--
 The definition of `colimit_one` is independent of the chosen object of `J`. In particular, this
 lemma allows us to "unfold" the definition of `colimit_one` at a custom chosen object `j`.
 -/
+@[to_additive]
 lemma colimit_one_eq' (j : J) : colimit_one = M.mk ⟨j, 1⟩ :=
 begin
   apply M.mk_eq,
@@ -77,12 +83,14 @@ The "unlifted" version of multiplication in the colimit. To multiply two depende
 `⟨j₁, x⟩` and `⟨j₂, y⟩`, we pass to a common successor of `j₁` and `j₂` (given by `is_filtered.max`)
 and multiply them there.
 -/
+@[to_additive]
 def colimit_mul_aux (x y : Σ j, F.obj j) : M :=
 M.mk ⟨max' x.1 y.1, F.map (left_to_max x.1 y.1) x.2 * F.map (right_to_max x.1 y.1) y.2⟩
 
 /--
 Multiplication in the colimit is well-defined in the left argument.
 -/
+@[to_additive]
 lemma colimit_mul_aux_eq_of_rel_left {x x' y : Σ j, F.obj j}
   (hxx' : types.filtered_colimit.rel (F ⋙ forget Mon) x x') :
   colimit_mul_aux x y = colimit_mul_aux x' y :=
@@ -101,6 +109,7 @@ end
 /--
 Multiplication in the colimit is well-defined in the right argument.
 -/
+@[to_additive]
 lemma colimit_mul_aux_eq_of_rel_right {x y y' : Σ j, F.obj j}
   (hyy' : types.filtered_colimit.rel (F ⋙ forget Mon) y y') :
   colimit_mul_aux x y = colimit_mul_aux x y' :=
@@ -119,6 +128,7 @@ end
 /--
 Multiplication in the colimit. See also `colimit_mul_aux`.
 -/
+@[to_additive]
 def colimit_mul (x y : M) : M :=
 begin
   refine quot.lift₂ (colimit_mul_aux F) _ _ x y,
@@ -137,6 +147,7 @@ Multiplication in the colimit is independent of the chosen "maximum" in the filt
 In particular, this lemma allows us to "unfold" the definition of the multiplication of `x` and `y`,
 using a custom object `k` and morphisms `f : x.1 ⟶ k` and `g : y.1 ⟶ k`.
 -/
+@[to_additive]
 lemma colimit_mul_eq' (x y : Σ j, F.obj j) (k : J) (f : x.1 ⟶ k) (g : y.1 ⟶ k) :
   colimit_mul (quot.mk _ x) (quot.mk _ y) = quot.mk _ ⟨k, F.map f x.2 * F.map g y.2⟩ :=
 begin
@@ -148,6 +159,7 @@ begin
   simp_rw [monoid_hom.map_mul, ← comp_apply, ← F.map_comp, h₁, h₂],
 end
 
+@[to_additive]
 lemma colimit_one_mul (x : M) : colimit_mul colimit_one x = x :=
 begin
   apply quot.induction_on x, clear x, intro x,
@@ -156,6 +168,7 @@ begin
     monoid_hom.map_one, one_mul, F.map_id, id_apply],
 end
 
+@[to_additive]
 lemma colimit_mul_one (x : types.quot (F ⋙ forget Mon)) :
   colimit_mul x colimit_one = x :=
 begin
@@ -165,6 +178,7 @@ begin
     monoid_hom.map_one, mul_one, F.map_id, id_apply],
 end
 
+@[to_additive]
 lemma colimit_mul_assoc (x y z : M) :
   colimit_mul (colimit_mul x y) z = colimit_mul x (colimit_mul y z) :=
 begin
@@ -177,6 +191,7 @@ begin
   simp only [F.map_id, id_apply, mul_assoc],
 end
 
+@[to_additive]
 instance colimit_monoid : monoid M :=
 { one := colimit_one,
   mul := colimit_mul,
@@ -185,12 +200,14 @@ instance colimit_monoid : monoid M :=
   mul_assoc := colimit_mul_assoc }
 
 /-- The bundled monoid giving the filtered colimit of a diagram. -/
+@[to_additive]
 def colimit : Mon := ⟨M, by apply_instance⟩
 
 /--
 The definition of `colimit_one` is independent of the chosen object of `J`. In particular, this
 lemma allows us to "unfold" the definition of `colimit_one` at a custom chosen object `j`.
 -/
+@[to_additive]
 lemma colimit_one_eq (j : J) : (1 : M) = M.mk ⟨j, 1⟩ :=
 colimit_one_eq' j
 
@@ -199,11 +216,13 @@ Multiplication in the colimit is independent of the chosen "maximum" in the filt
 In particular, this lemma allows us to "unfold" the definition of the multiplication of `x` and `y`,
 using a custom object `k` and morphisms `f : x.1 ⟶ k` and `g : y.1 ⟶ k`.
 -/
+@[to_additive]
 lemma colimit_mul_eq (x y : Σ j, F.obj j) (k : J) (f : x.1 ⟶ k) (g : y.1 ⟶ k) :
   M.mk x * M.mk y = M.mk ⟨k, F.map f x.2 * F.map g y.2⟩ :=
 colimit_mul_eq' x y k f g
 
 /-- The monoid homomorphism from a given monoid in the diagram to the colimit monoid. -/
+@[to_additive]
 def cocone_morphism (j : J) : F.obj j ⟶ colimit :=
 { to_fun := (types.colimit_cocone (F ⋙ forget Mon)).ι.app j,
   map_one' := (colimit_one_eq j).symm,
@@ -212,11 +231,13 @@ def cocone_morphism (j : J) : F.obj j ⟶ colimit :=
     rw [F.map_id, id_apply, id_apply], refl,
   end }
 
-@[simp] lemma cocone_naturality {j j' : J} (f : j ⟶ j') :
+@[to_additive, simp]
+lemma cocone_naturality {j j' : J} (f : j ⟶ j') :
   F.map f ≫ (cocone_morphism j') = cocone_morphism j :=
 monoid_hom.coe_inj ((types.colimit_cocone (F ⋙ forget Mon)).ι.naturality f)
 
 /-- The cocone over the proposed colimit monoid. -/
+@[to_additive]
 def colimit_cocone : cocone F :=
 { X := colimit,
   ι := { app := cocone_morphism } }.
@@ -226,6 +247,7 @@ Given a cocone `t` of `F`, the induced monoid homomorphism from the colimit to t
 As a function, this is simply given by the induced map of the corresponding cocone in `Type`.
 The only thing left to see is that it is a monoid homomorphism.
 -/
+@[to_additive]
 def colimit_desc (t : cocone F) : colimit ⟶ t.X :=
 { to_fun := (types.colimit_cocone_is_colimit (F ⋙ forget Mon)).desc ((forget Mon).map_cocone t),
   map_one' := begin
@@ -241,6 +263,7 @@ def colimit_desc (t : cocone F) : colimit ⟶ t.X :=
   end }
 
 /-- The proposed colimit cocone is a colimit in `Mon`. -/
+@[to_additive]
 def colimit_cocone_is_colimit : is_colimit colimit_cocone :=
 { desc := colimit_desc,
   fac' := λ t j, monoid_hom.coe_inj
@@ -253,6 +276,7 @@ def colimit_cocone_is_colimit : is_colimit colimit_cocone :=
     exact monoid_hom.congr_fun (h j) x,
   end }
 
+@[to_additive]
 instance forget_preserves_filtered_colimits : preserves_filtered_colimits (forget Mon) :=
 { preserves_filtered_colimits := λ J _ _, by exactI
   { preserves_colimit := λ F, preserves_colimit_of_preserves_colimit_cocone

b30dd3e0b8b613657291707d3b3465eaa6a507ef
Chris Hughes <chrishughes24@gmail.com>
2021-09-11 19:32:57 +0100

    Docstrings

--- src/ring_theory/nakayama.lean
+++ src/ring_theory/nakayama.lean
@@ -1,29 +1,42 @@
+/-
+Copyright (c) 2021 Chris Hughes. All rights reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
+Authors: Chris Hughes
+-/
 import ring_theory.noetherian
 import ring_theory.jacobson
+/-
+# Nakayama's lemma
 
+This file contains some alternative statements of Nakayama's Lemma as found in
+[Stacks: Nakayama's Lemma](https://stacks.math.columbia.edu/tag/00DV).
+
+## Main statements
+
+* `eq_smul_of_le_smul_of_le_jacobson` - A version of (2) in
+  [Stacks: Nakayama's Lemma](https://stacks.math.columbia.edu/tag/00DV).,
+  generalising to the Jacobson of any ideal.
+* `eq_bot_of_le_smul_of_le_jacobson_bot` - Statement (2) in
+  [Stacks: Nakayama's Lemma](https://stacks.math.columbia.edu/tag/00DV).
+
+* `smul_sup_eq_smul_sup_of_le_smul_of_le_jacobson` - A version of (4) in
+  [Stacks: Nakayama's Lemma](https://stacks.math.columbia.edu/tag/00DV).,
+  generalising to the Jacobson of any ideal.
+* `smul_sup_eq_of_le_smul_of_le_jacobson_bot` - Statement (4) in
+  [Stacks: Nakayama's Lemma](https://stacks.math.columbia.edu/tag/00DV).
+
+## References
+* [Stacks: Nakayama's Lemma](https://stacks.math.columbia.edu/tag/00DV)
+-/
 variables {R M : Type*} [comm_ring R] [add_comm_group M] [module R M]
 
 open ideal
 
-lemma exists_mul_sub_mem_of_sub_one_mem_jacobson {I : ideal R} (r : R)
-  (h : r - 1 ∈ jacobson I) : ∃ s, r * s - 1 ∈ I :=
-begin
-  cases mem_jacobson_iff.1 h 1 with s hs,
-  use s,
-  simpa [sub_mul] using hs
-end
-
-lemma is_unit_of_sub_one_mem_jacobson_bot (r : R)
-  (h : r - 1 ∈ jacobson (⊥ : ideal R)) : is_unit r :=
-begin
-  cases exists_mul_sub_mem_of_sub_one_mem_jacobson r h with s hs,
-  rw [mem_bot, sub_eq_zero] at hs,
-  exact is_unit_of_mul_eq_one _ _ hs
-end
-
 namespace submodule
 
--- Number 2 on stacks
+/-- *Nakayama's Lemma** - A slightly more general version of (2) in
+[Stacks 00DV](https://stacks.math.columbia.edu/tag/00DV).
+See also `eq_bot_of_le_smul_of_le_jacobson_bot` for the special case when `J = ⊥`.  -/
 lemma eq_smul_of_le_smul_of_le_jacobson {I J : ideal R} {N : submodule R M}
   (hN : N.fg) (hIN : N ≤ I • N) (hIjac : I ≤ jacobson J) : N = J • N :=
 begin
@@ -37,10 +50,17 @@ begin
   exact submodule.smul_mem_smul (submodule.neg_mem _ hs) hn
 end
 
-lemma eq_bot_of_le_smul_of_le_jacobson (I : ideal R) (N : submodule R M)
+/-- *Nakayama's Lemma** - Statement (2) in
+[Stacks 00DV](https://stacks.math.columbia.edu/tag/00DV).
+See also `eq_smul_of_le_smul_of_le_jacobson` for a generalisation
+to the `jacobson` of any ideal -/
+lemma eq_bot_of_le_smul_of_le_jacobson_bot (I : ideal R) (N : submodule R M)
   (hN : N.fg) (hIN : N ≤ I • N) (hIjac : I ≤ jacobson ⊥) : N = ⊥ :=
 by rw [eq_smul_of_le_smul_of_le_jacobson hN hIN hIjac, submodule.bot_smul]
 
+/-- *Nakayama's Lemma** - A slightly more general version of (4) in
+[Stacks 00DV](https://stacks.math.columbia.edu/tag/00DV).
+See also `smul_sup_eq_of_le_smul_of_le_jacobson_bot` for the special case when `J = ⊥`.  -/
 lemma smul_sup_eq_smul_sup_of_le_smul_of_le_jacobson {I J : ideal R}
   {N N' : submodule R M} (hN' : N'.fg) (hIJ : I ≤ jacobson J)
   (hNN : N ⊔ N' ≤ N ⊔ I • N') : N ⊔ I • N' = N ⊔ J • N' :=
@@ -63,7 +83,11 @@ begin
   rw [this, sup_comm]
 end
 
-lemma smul_sup_eq_of_le_smul_of_le_jacobson {I : ideal R}
+/-- *Nakayama's Lemma** - Statement (4) in
+[Stacks 00DV](https://stacks.math.columbia.edu/tag/00DV).
+See also `smul_sup_eq_smul_sup_of_le_smul_of_le_jacobson` for a generalisation
+to the `jacobson` of any ideal -/
+lemma smul_sup_eq_of_le_smul_of_le_jacobson_bot {I : ideal R}
   {N N' : submodule R M} (hN' : N'.fg) (hIJ : I ≤ jacobson ⊥)
   (hNN : N ⊔ N' ≤ N ⊔ I • N') : N ⊔ I • N' = N :=
 by rw [smul_sup_eq_smul_sup_of_le_smul_of_le_jacobson hN' hIJ hNN, bot_smul, sup_bot_eq]

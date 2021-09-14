f9616cd2f9c11c3a900d9d88899c9abdaf7f1921
Riccardo Brasca <riccardo.brasca@gmail.com>
2021-09-14 10:23:31 +0200

    typo

--- src/linear_algebra/pi.lean
+++ src/linear_algebra/pi.lean
@@ -346,7 +346,7 @@ noncomputable def pi.extend_by_zero.linear_map  : (ι → R) →ₗ[R] (η → R
     { obtain ⟨i, hi⟩ := hx,
       simp [← hi, hs] },
     { simp [pi.extend_by_zero_apply' R s f x hx,
-        pi.extend_by_zero_apply' R s (r • f) x hx] }
+      pi.extend_by_zero_apply' R s (r • f) x hx] }
   end,
   ..pi.extend_by_zero.add_hom R hs }
 

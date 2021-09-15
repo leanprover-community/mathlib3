ad69679c5e06a9ec5def09615ac91b19ecd46562
Riccardo Brasca <riccardo.brasca@gmail.com>
2021-09-15 13:46:18 +0200

    Update src/linear_algebra/pi.lean
    Co-authored-by: Eric Wieser <wieser.eric@gmail.com>

--- src/linear_algebra/pi.lean
+++ src/linear_algebra/pi.lean
@@ -337,7 +337,8 @@ section extend
 variables (R) {η : Type x} [semiring R] (s : ι → η)
 
 /-- `function.extend s f 0` as a bundled linear map. -/
-noncomputable def pi.extend_by_zero.linear_map  : (ι → R) →ₗ[R] (η → R) :=
+@[simps]
+noncomputable def pi.extend_by_zero.linear_map : (ι → R) →ₗ[R] (η → R) :=
 { to_fun := λ f, function.extend s f 0,
   map_smul' := λ r f, by { simpa using function.extend_smul r s f 0 },
   ..function.extend_by_zero.add_hom R s }

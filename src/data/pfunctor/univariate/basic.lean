bcae36ba4cc8f3263d5b587a8071def1a57c794f
Chris Hughes <chrishughes24@gmail.com>
2021-09-15 07:44:56 +0100

    fix imports

--- src/data/pfunctor/univariate/basic.lean
+++ src/data/pfunctor/univariate/basic.lean
@@ -3,7 +3,7 @@ Copyright (c) 2018 Jeremy Avigad. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Jeremy Avigad
 -/
-import data.W
+import data.W.basic
 
 /-!
 # Polynomial functors

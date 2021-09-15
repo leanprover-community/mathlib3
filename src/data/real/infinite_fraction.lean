f66298d1257c79d664fbfa40d6b77ddfbf672c2e
Yury G. Kudryashov <urkud@urkud.name>
2021-09-14 22:45:30 -0400

    Add licence

--- src/data/real/infinite_fraction.lean
+++ src/data/real/infinite_fraction.lean
@@ -1,3 +1,8 @@
+/-
+Copyright (c) 2021 Yury Kudryashov. All rights reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
+Authors: Yury Kudryashov
+-/
 import set_theory.cardinal_ordinal
 import data.stream
 import data.setoid.basic

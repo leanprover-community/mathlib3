4882e9156f8c6044f54cb61bb12af6738ca6b5c3
Chris Hughes <chrishughes24@gmail.com>
2021-09-17 17:20:27 +0100

    changes based on comments

--- src/algebra/algebra/tower.lean
+++ src/algebra/algebra/tower.lean
@@ -123,13 +123,15 @@ ring_hom.ext $ λ _, rfl
 @[simp] lemma coe_to_alg_hom' : (to_alg_hom R S A : S → A) = algebra_map S A :=
 rfl
 
+variables {R S A B}
+
 @[simp] lemma _root_.alg_hom.commutes_of_tower (f : A →ₐ[S] B) (r : R) :
   f (algebra_map R A r) = algebra_map R B r :=
 by rw [algebra_map_apply R S A r, f.commutes, ← algebra_map_apply R S B]
 
 @[simp] lemma _root_.alg_hom.comp_algebra_map_of_tower (f : A →ₐ[S] B) :
   (f : A →+* B).comp (algebra_map R A) = algebra_map R B :=
-by ext; simp
+ring_hom.ext f.commutes_of_tower
 
 variables (R) {S A B}
 

7eedf41a269fdb7416fa4035c67999b20eadd225
Riccardo Brasca <riccardo.brasca@gmail.com>
2021-09-18 12:22:08 +0200

    move lemma

--- src/linear_algebra/invariant_basis_number.lean
+++ src/linear_algebra/invariant_basis_number.lean
@@ -79,6 +79,20 @@ lemma le_of_fin_injective [strong_rank_condition R] {n m : ℕ} (f : (fin n → 
   injective f → n ≤ m :=
 strong_rank_condition.le_of_fin_injective f
 
+/-- A ring satisfies the strong rank condition if and only if, for all `n : ℕ`, any linear map
+`(fin (n + 1) → R) →ₗ[R] (fin n → R)` is not injective. -/
+lemma strong_rank_condition_iff_succ : strong_rank_condition R ↔
+  ∀ (n : ℕ) (f : (fin (n + 1) → R) →ₗ[R] (fin n → R)), ¬function.injective f :=
+begin
+  refine ⟨λ h n, _, λ h, ⟨λ n m f hf, _⟩⟩,
+  { rintros f hf,
+    letI : strong_rank_condition R := h,
+    exact nat.not_succ_le_self n (le_of_fin_injective R f hf) },
+  { by_contra H,
+    exact h m (f.comp (function.extend_by_zero.linear_map R (fin.cast_le (not_le.1 H))))
+      (hf.comp (function.extend_injective (rel_embedding.injective _) 0)) }
+end
+
 lemma card_le_of_injective [strong_rank_condition R] {α β : Type*} [fintype α] [fintype β]
   (f : (α → R) →ₗ[R] (β → R)) (i : injective f) : fintype.card α ≤ fintype.card β :=
 begin
@@ -95,20 +109,6 @@ begin
   let Q := (finsupp.linear_equiv_fun_on_fintype R R α).symm,
   exact card_le_of_injective R ((P.to_linear_map.comp f).comp Q.to_linear_map)
     ((P.injective.comp i).comp Q.injective)
-end
-
-/-- A ring satisfies the strong rank condition if and only if, for all `n : ℕ`, any linear map
-`(fin (n + 1) → R) →ₗ[R] (fin n → R)` is not injective. -/
-lemma strong_rank_condition_iff_succ : strong_rank_condition R ↔
-  ∀ (n : ℕ) (f : (fin (n + 1) → R) →ₗ[R] (fin n → R)), ¬function.injective f :=
-begin
-  refine ⟨λ h n, _, λ h, ⟨λ n m f hf, _⟩⟩,
-  { rintros f hf,
-    letI : strong_rank_condition R := h,
-    exact nat.not_succ_le_self n (le_of_fin_injective R f hf) },
-  { by_contra H,
-    exact h m (f.comp (function.extend_by_zero.linear_map R (fin.cast_le (not_le.1 H))))
-      (hf.comp (function.extend_injective (rel_embedding.injective _) 0)) }
 end
 
 /-- We say that `R` satisfies the rank condition if `(fin n → R) →ₗ[R] (fin m → R)` surjective

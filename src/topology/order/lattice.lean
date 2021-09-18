90e08bc8231170c8a76d68be7e393dfebc544234
Christopher Hoskin <christopher.hoskin@gmail.com>
2021-09-18 20:59:41 +0100

    Document topological lattices

--- src/topology/order/lattice.lean
+++ src/topology/order/lattice.lean
@@ -1,16 +1,48 @@
--- Topological lattices
--- Modeled on topology.algebra.monoid
-
+/-
+Copyright (c) 2021 Christopher Hoskin. All rights reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
+Authors: Christopher Hoskin
+-/
 import topology.basic
 import topology.constructions
 import topology.algebra.ordered.basic
 
+/-!
+# Topological lattices
+
+In this file we define mixin classes `has_continuous_inf` and `has_continuous_sup`. We define the
+class `topological_lattice` as a topological space and lattice `L` extending `has_continuous_inf`
+and `has_continuous_sup`.
+
+## References
+
+* [Gierz et al, A Compendium of Continuous Lattices][GierzEtAl1980]
+
+## Tags
+
+topological, lattice
+-/
+
+/--
+Let `L` be a topological space and let `L×L` be equipped with the product topology and let
+`⊓:L×L → L` be an infimum. Then `L` is said to have *(jointly) continuous infimum* if the map
+`⊓:L×L → L` is continuous.
+-/
 class has_continuous_inf (L : Type*) [topological_space L] [has_inf L] : Prop :=
 (continuous_inf : continuous (λ p : L × L, p.1 ⊓ p.2))
 
+/--
+Let `L` be a topological space and let `L×L` be equipped with the product topology and let
+`⊓:L×L → L` be a supremum. Then `L` is said to have *(jointly) continuous supremum* if the map
+`⊓:L×L → L` is continuous.
+-/
 class has_continuous_sup (L : Type*) [topological_space L] [has_sup L] : Prop :=
 (continuous_sup : continuous (λ p : L × L, p.1 ⊔ p.2))
 
+/--
+Let `L` be a topological space with a supremum. If the order dual has a continuous infimum then the
+supremum is continuous.
+-/
 instance has_continuous_inf_dual_has_continuous_sup
 (L : Type*) [topological_space L] [has_sup L] [h: has_continuous_inf (order_dual L)] :
   has_continuous_sup  L :=
@@ -19,6 +51,9 @@ instance has_continuous_inf_dual_has_continuous_sup
     @has_continuous_inf.continuous_inf  (order_dual L) _ _ h
 }
 
--- The equivalent definition of a topological_monoid doesn't appear to exist
+/--
+Let `L` be a lattice equipped with a topology such that `L` has continuous infimum and supremum.
+Then `L` is said to be a *topological lattice*.
+-/
 class topological_lattice (L : Type*) [topological_space L] [lattice L]
   extends has_continuous_inf L, has_continuous_sup L

424645813e65650f407df545694a0b061aeb9693
Yury G. Kudryashov <urkud@urkud.name>
2021-09-17 20:58:09 -0400

    chore(order/preorder_hom): more homs, golf a few proofs
    * define more `preorder_hom`s;
    * use `order_dual` to get some proofs;
    * redefine `has_Inf.Inf` and `has_Sup.Sup` using `infi`/`supr`.

--- src/order/preorder_hom.lean
+++ src/order/preorder_hom.lean
@@ -25,40 +25,62 @@ infixr ` →ₘ `:25 := preorder_hom
 namespace preorder_hom
 variables {α : Type*} {β : Type*} {γ : Type*} [preorder α] [preorder β] [preorder γ]
 
-instance : has_coe_to_fun (preorder_hom α β) :=
+instance : has_coe_to_fun (α →ₘ β) :=
 { F := λ f, α → β,
   coe := preorder_hom.to_fun }
 
 initialize_simps_projections preorder_hom (to_fun → coe)
 
 @[mono]
-lemma monotone (f : α →ₘ β) : monotone f :=
+protected lemma mono (f : α →ₘ β) : monotone f :=
 preorder_hom.monotone' f
 
 @[simp] lemma to_fun_eq_coe {f : α →ₘ β} : f.to_fun = f := rfl
 @[simp] lemma coe_fun_mk {f : α → β} (hf : _root_.monotone f) : (mk f hf : α → β) = f := rfl
 
 @[ext] -- See library note [partially-applied ext lemmas]
-lemma ext (f g : preorder_hom α β) (h : (f : α → β) = g) : f = g :=
+lemma ext (f g : α →ₘ β) (h : (f : α → β) = g) : f = g :=
 by { cases f, cases g, congr, exact h }
 
 /-- The identity function as bundled monotone function. -/
 @[simps {fully_applied := ff}]
-def id : preorder_hom α α :=
-⟨id, monotone_id⟩
+def id : α →ₘ α := ⟨id, monotone_id⟩
 
-instance : inhabited (preorder_hom α α) := ⟨id⟩
+instance : inhabited (α →ₘ α) := ⟨id⟩
 
 /-- The composition of two bundled monotone functions. -/
 @[simps {fully_applied := ff}]
-def comp (g : preorder_hom β γ) (f : preorder_hom α β) : preorder_hom α γ :=
-⟨g ∘ f, g.monotone.comp f.monotone⟩
+def comp (g : β →ₘ γ) (f : α →ₘ β) : α →ₘ γ := ⟨g ∘ f, g.mono.comp f.mono⟩
 
-@[simp] lemma comp_id (f : preorder_hom α β) : f.comp id = f :=
+@[simp] lemma comp_id (f : α →ₘ β) : f.comp id = f :=
 by { ext, refl }
 
-@[simp] lemma id_comp (f : preorder_hom α β) : id.comp f = f :=
+@[simp] lemma id_comp (f : α →ₘ β) : id.comp f = f :=
 by { ext, refl }
+
+/-- Constant function bundled as a `preorder_hom`. -/
+@[simps {fully_applied := ff}]
+def const (α : Type*) [preorder α] {β : Type*} [preorder β] (b : β) : α →ₘ β :=
+⟨function.const α b, λ _ _ _, le_rfl⟩
+
+@[simp] lemma const_comp (f : α →ₘ β) (c : γ) : (const β c).comp f = const α c := rfl
+
+@[simp] lemma comp_const (γ : Type*) [preorder γ] (f : α →ₘ β) (c : α) :
+  f.comp (const γ c) = const γ (f c) := rfl
+
+/-- Given two bundled monotone maps `f`, `g`, `f.prod g` is the map `x ↦ (f x, g x)` bundled as a
+`preorder_hom`. -/
+@[simps] def prod (f : α →ₘ β) (g : α →ₘ γ) : α →ₘ (β × γ) :=
+⟨λ x, (f x, g x), λ x y h, ⟨f.mono h, g.mono h⟩⟩
+
+/-- Diagonal embedding of `α` into `α × α` as a `preorder_hom`. -/
+@[simps] def diag : α →ₘ α × α := id.prod id
+
+/-- Evaluation of an unbundled function at a point as a `preorder_hom`. -/
+@[simps {fully_applied := ff}]
+def eval {ι : Type*} {α : ι → Type*} [Π i, preorder (α i)] (i : ι) :
+  (Π j, α j) →ₘ α i :=
+⟨function.eval i, function.monotone_eval i⟩
 
 /-- `subtype.val` as a bundled monotone function.  -/
 @[simps {fully_applied := ff}]
@@ -76,14 +98,31 @@ subsingleton.elim _ _
 
 /-- The preorder structure of `α →ₘ β` is pointwise inequality: `f ≤ g ↔ ∀ a, f a ≤ g a`. -/
 instance : preorder (α →ₘ β) :=
-preorder.lift preorder_hom.to_fun
+@preorder.lift (α →ₘ β) (α → β) _ coe_fn
 
 instance {β : Type*} [partial_order β] : partial_order (α →ₘ β) :=
-partial_order.lift preorder_hom.to_fun $ by rintro ⟨⟩ ⟨⟩ h; congr; exact h
+@partial_order.lift (α →ₘ β) (α → β) _ coe_fn ext
+
+lemma le_def {f g : α →ₘ β} : f ≤ g ↔ ∀ x, f x ≤ g x := iff.rfl
+
+@[simp] lemma mk_le_mk {f g : α → β} {hf hg} : mk f hf ≤ mk g hg ↔ f ≤ g := iff.rfl
+
+/-- Reinterpret a bundled monotone function as a monotone function between dual orders. -/
+@[simps] protected def dual : (α →ₘ β) ≃ (order_dual α →ₘ order_dual β) :=
+{ to_fun := λ f, ⟨order_dual.to_dual ∘ f ∘ order_dual.of_dual, f.mono.order_dual⟩,
+  inv_fun := λ f, ⟨order_dual.of_dual ∘ f ∘ order_dual.to_dual, f.mono.order_dual⟩,
+  left_inv := λ f, ext _ _ rfl,
+  right_inv := λ f, ext _ _ rfl }
+
+/-- `preorder_hom.dual` as an order isomorphism. -/
+def dual_iso (α β : Type*) [preorder α] [preorder β] :
+  (α →ₘ β) ≃o order_dual (order_dual α →ₘ order_dual β) :=
+{ to_equiv := preorder_hom.dual.trans order_dual.to_dual,
+  map_rel_iff' := λ f g, iff.rfl }
 
 @[simps]
 instance {β : Type*} [semilattice_sup β] : has_sup (α →ₘ β) :=
-{ sup := λ f g, ⟨λ a, f a ⊔ g a, λ x y h, sup_le_sup (f.monotone h) (g.monotone h)⟩ }
+{ sup := λ f g, ⟨λ a, f a ⊔ g a, f.mono.sup g.mono⟩ }
 
 instance {β : Type*} [semilattice_sup β] : semilattice_sup (α →ₘ β) :=
 { sup := has_sup.sup,
@@ -94,14 +133,12 @@ instance {β : Type*} [semilattice_sup β] : semilattice_sup (α →ₘ β) :=
 
 @[simps]
 instance {β : Type*} [semilattice_inf β] : has_inf (α →ₘ β) :=
-{ inf := λ f g, ⟨λ a, f a ⊓ g a, λ x y h, inf_le_inf (f.monotone h) (g.monotone h)⟩ }
+{ inf := λ f g, ⟨λ a, f a ⊓ g a, f.mono.inf g.mono⟩ }
 
 instance {β : Type*} [semilattice_inf β] : semilattice_inf (α →ₘ β) :=
 { inf := (⊓),
-  inf_le_left := λ a b x, inf_le_left,
-  inf_le_right := λ a b x, inf_le_right,
-  le_inf := λ a b c h₀ h₁ x, le_inf (h₀ x) (h₁ x),
-  .. (_ : partial_order (α →ₘ β)) }
+  .. (_ : partial_order (α →ₘ β)),
+  .. (dual_iso α β).symm.to_galois_insertion.lift_semilattice_inf }
 
 instance {β : Type*} [lattice β] : lattice (α →ₘ β) :=
 { .. (_ : semilattice_sup (α →ₘ β)),
@@ -109,7 +146,7 @@ instance {β : Type*} [lattice β] : lattice (α →ₘ β) :=
 
 @[simps]
 instance {β : Type*} [order_bot β] : has_bot (α →ₘ β) :=
-{ bot := ⟨λ a, ⊥, λ a b h, le_refl _⟩ }
+{ bot := const α ⊥ }
 
 instance {β : Type*} [order_bot β] : order_bot (α →ₘ β) :=
 { bot := ⊥,
@@ -118,43 +155,45 @@ instance {β : Type*} [order_bot β] : order_bot (α →ₘ β) :=
 
 @[simps]
 instance {β : Type*} [order_top β] : has_top (α →ₘ β) :=
-{ top := ⟨λ a, ⊤, λ a b h, le_refl _⟩ }
+{ top := const α ⊤ }
 
 instance {β : Type*} [order_top β] : order_top (α →ₘ β) :=
 { top := ⊤,
   le_top := λ a x, le_top,
   .. (_ : partial_order (α →ₘ β)) }
 
+/-- Curry/uncurry as an order isomorphism between `α × β →ₘ γ` and `α →ₘ β →ₘ γ`. -/
+def curry : (α × β →ₘ γ) ≃o (α →ₘ β →ₘ γ) :=
+{ to_fun := λ f, ⟨λ x, ⟨function.curry f x, λ y₁ y₂ h, f.mono ⟨le_rfl, h⟩⟩,
+    λ x₁ x₂ h y, f.mono ⟨h, le_rfl⟩⟩,
+  inv_fun := λ f, ⟨function.uncurry (λ x, f x), λ x y h, (f.mono h.1 x.2).trans $ (f y.1).mono h.2⟩,
+  left_inv := λ f, by { ext ⟨x, y⟩, refl },
+  right_inv := λ f, by { ext x y, refl },
+  map_rel_iff' := λ f g, by simp [le_def] }
+
+@[simp] lemma curry_apply (f : α × β →ₘ γ) (x : α) (y : β) : curry f x y = f (x, y) := rfl
+
+@[simp] lemma curry_symm_apply (f : α →ₘ β →ₘ γ) (x : α × β) : curry.symm f x = f x.1 x.2 := rfl
+
+/-- Restriction of `f : α →ₘ α →ₘ β` to the diagonal. -/
+@[simps {simp_rhs := tt}] def on_diag (f : α →ₘ α →ₘ β) : α →ₘ β := (curry.symm f).comp diag
+
 @[simps]
 instance {β : Type*} [complete_lattice β] : has_Inf (α →ₘ β) :=
-{ Inf := λ s, ⟨ λ x, Inf ((λ f : _ →ₘ _, f x) '' s), λ x y h,
-    Inf_le_Inf_of_forall_exists_le begin
-      simp only [and_imp, exists_prop, set.mem_image, exists_exists_and_eq_and, exists_imp_distrib],
-      intros,
-      subst_vars,
-      refine ⟨_,by assumption, monotone _ h⟩
-    end ⟩ }
+{ Inf := λ s, ⟨λ x, ⨅ f ∈ s, (f : _) x, λ x y h, binfi_le_binfi (λ f _, f.mono h)⟩ }
 
 @[simps]
 instance {β : Type*} [complete_lattice β] : has_Sup (α →ₘ β) :=
-{ Sup := λ s, ⟨ λ x, Sup ((λ f : _ →ₘ _, f x) '' s), λ x y h,
-    Sup_le_Sup_of_forall_exists_le begin
-      simp only [and_imp, exists_prop, set.mem_image, exists_exists_and_eq_and, exists_imp_distrib],
-      intros,
-      subst_vars,
-      refine ⟨_,by assumption, monotone _ h⟩
-    end ⟩ }
+{ Sup := λ s, ⟨λ x, ⨆ f ∈ s, (f : _) x, λ x y h, bsupr_le_bsupr (λ f _, f.mono h)⟩ }
 
 @[simps Sup Inf]
 instance {β : Type*} [complete_lattice β] : complete_lattice (α →ₘ β) :=
-{ Sup := has_Sup.Sup,
-  le_Sup := λ s f hf x, @le_Sup β _ ((λ f : _ →ₘ _, f x) '' s) (f x) ⟨f, hf, rfl⟩,
-  Sup_le := λ s f hf x, @Sup_le β _ _ _ $ λ b (h : b ∈ (λ (f : α →ₘ β), f x) '' s),
-              by rcases h with ⟨g, h, ⟨ ⟩⟩; apply hf _ h,
-  Inf := has_Inf.Inf,
-  le_Inf := λ s f hf x, @le_Inf β _ _ _ $ λ b (h : b ∈ (λ (f : α →ₘ β), f x) '' s),
-              by rcases h with ⟨g, h, ⟨ ⟩⟩; apply hf _ h,
-  Inf_le := λ s f hf x, @Inf_le β _ ((λ f : _ →ₘ _, f x) '' s) (f x) ⟨f, hf, rfl⟩,
+{ Sup := Sup,
+  le_Sup := λ s f hf x, le_supr_of_le f (le_supr _ hf),
+  Sup_le := λ s f hf x, bsupr_le (λ g hg, hf g hg x),
+  Inf := Inf,
+  le_Inf := λ s f hf x, le_binfi (λ g hg, hf g hg x),
+  Inf_le := λ s f hf x, infi_le_of_le f (infi_le _ hf),
   .. (_ : lattice (α →ₘ β)),
   .. (_ : order_top (α →ₘ β)),
   .. (_ : order_bot (α →ₘ β)) }
@@ -169,12 +208,12 @@ begin
     { intros n, induction n with n ih; intros a₁ a₂,
       { refl, },
       { calc f^[n + 1] (a₁ ⊔ a₂) = (f^[n] (f (a₁ ⊔ a₂))) : function.iterate_succ_apply f n _
-                             ... ≤ (f^[n] ((f a₁) ⊔ a₂)) : f.monotone.iterate n (h a₁ a₂)
+                             ... ≤ (f^[n] ((f a₁) ⊔ a₂)) : f.mono.iterate n (h a₁ a₂)
                              ... ≤ (f^[n] (f a₁)) ⊔ a₂ : ih _ _
                              ... = (f^[n + 1] a₁) ⊔ a₂ : by rw ← function.iterate_succ_apply, }, },
     calc f^[n₁ + n₂] (a₁ ⊔ a₂) = (f^[n₁] (f^[n₂] (a₁ ⊔ a₂))) : function.iterate_add_apply f n₁ n₂ _
                            ... = (f^[n₁] (f^[n₂] (a₂ ⊔ a₁))) : by rw sup_comm
-                           ... ≤ (f^[n₁] ((f^[n₂] a₂) ⊔ a₁)) : f.monotone.iterate n₁ (h' n₂ _ _)
+                           ... ≤ (f^[n₁] ((f^[n₂] a₂) ⊔ a₁)) : f.mono.iterate n₁ (h' n₂ _ _)
                            ... = (f^[n₁] (a₁ ⊔ (f^[n₂] a₂))) : by rw sup_comm
                            ... ≤ (f^[n₁] a₁) ⊔ (f^[n₂] a₂) : h' n₁ a₁ _, },
 end

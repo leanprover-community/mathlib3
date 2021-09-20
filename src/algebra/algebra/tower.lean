af967127c5ad6b6f103c948119652b39763ab7c0
Chris Hughes <33847686+ChrisHughes24@users.noreply.github.com>
2021-09-20 16:33:38 +0100

    Update tower.lean

--- src/algebra/algebra/tower.lean
+++ src/algebra/algebra/tower.lean
@@ -46,7 +46,7 @@ def lsmul : A →ₐ[R] module.End R M :=
 
 @[simp] lemma lsmul_coe (a : A) : (lsmul R M a : M → M) = (•) a := rfl
 
-lemma lmul_algebra_map (x : R) :
+@[simp] lemma lmul_algebra_map (x : R) :
   lmul R A (algebra_map R A x) = algebra.lsmul R A x :=
 eq.symm $ linear_map.ext $ smul_def'' x
 

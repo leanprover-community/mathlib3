3c9ec2640fa31c6c48753146c058698a03c0d431
Scott Morrison <scott@tqft.net>
2021-09-20 17:32:08 +1000

    chore(analysis/convex): trivial generalizations of ℝ

--- src/analysis/convex/extreme.lean
+++ src/analysis/convex/extreme.lean
@@ -4,6 +4,7 @@ Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Yaël Dillies, Bhavik Mehta
 -/
 import analysis.convex.basic
+import data.real.basic
 
 /-!
 # Extreme sets

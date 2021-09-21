64bcb542d3a16513de09bf15b3f17b2fa6dec2c9
Kexing <kexing.ying19@imperial.ac.uk>
2021-09-21 16:08:10 +0100

    add copyright header

--- src/probability_theory/density.lean
+++ src/probability_theory/density.lean
@@ -1,3 +1,8 @@
+/-
+Copyright (c) 2021 Kexing Ying. All rights reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
+Authors: Kexing Ying
+-/
 import measure_theory.decomposition.radon_nikodym
 import measure_theory.measure.lebesgue
 

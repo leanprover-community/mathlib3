7c29870eb357f9fccc877ad1e277dfce6a24b002
sgouezel <sebastien.gouezel@univ-rennes1.fr>
2021-09-23 23:16:57 +0200

    add author name

--- src/measure_theory/measure/haar_lebesgue.lean
+++ src/measure_theory/measure/haar_lebesgue.lean
@@ -1,7 +1,7 @@
 /-
 Copyright (c) 2021 Floris van Doorn. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
-Authors: Floris van Doorn
+Authors: Floris van Doorn, Sébastien Gouëzel
 -/
 import measure_theory.measure.lebesgue
 import measure_theory.measure.haar

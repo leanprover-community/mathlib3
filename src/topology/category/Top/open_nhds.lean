e9008663fd1b09b415608c962252058877702c22
Scott Morrison <scott@tqft.net>
2021-09-23 15:20:58 +1000

    chore(*): linting

--- src/topology/category/Top/open_nhds.lean
+++ src/topology/category/Top/open_nhds.lean
@@ -16,6 +16,7 @@ variables {X Y : Top.{u}} (f : X ⟶ Y)
 
 namespace topological_space
 
+/-- The type of open neighbourhoods of a point `x` in a (bundled) topological space. -/
 def open_nhds (x : X) := { U : opens X // x ∈ U }
 
 namespace open_nhds
@@ -61,6 +62,8 @@ The inclusion `U ⊓ V ⟶ V` as a morphism in the category of open sets.
 def inf_le_right {x : X} (U V : open_nhds x) : U ⊓ V ⟶ V :=
 hom_of_le inf_le_right
 
+/-- The inclusion functor from open neighbourhoods of `x`
+to open sets in the ambient topological space. -/
 def inclusion (x : X) : open_nhds x ⥤ opens X :=
 full_subcategory_inclusion _
 
@@ -75,6 +78,8 @@ instance open_nhds_is_filtered (x : X) : is_filtered (open_nhds x)ᵒᵖ :=
     (inf_le_left (unop U) (unop V)).op, (inf_le_right (unop U) (unop V)).op, trivial⟩ ,
   cocone_maps := λ U V i j, ⟨V, 𝟙 V, rfl⟩, }
 
+/-- A continuous map provides a functor from open neighbourhoods of `f x`
+to open neighbourhoods of `x`, by taking the preimage. -/
 def map (x : X) : open_nhds (f x) ⥤ open_nhds x :=
 { obj := λ U, ⟨(opens.map f).obj U.1, by tidy⟩,
   map := λ U V i, (opens.map f).map i }
@@ -91,6 +96,8 @@ by simp
 @[simp] lemma op_map_id_obj (x : X) (U : (open_nhds x)ᵒᵖ) : (map (𝟙 X) x).op.obj U = U :=
 by simp
 
+/-- `opens.map f` and `open_nhds.map f` form a commuting square (up to natural isomorphism)
+with the inclusion functors into `opens X`. -/
 def inclusion_map_iso (x : X) : inclusion (f x) ⋙ opens.map f ≅ map f x ⋙ inclusion x :=
 nat_iso.of_components
   (λ U, begin split, exact 𝟙 _, exact 𝟙 _ end)

7c01b947d135f710bd93b925f5edb364cdeb65ad
Eric <ericrboidi@gmail.com>
2021-09-23 20:04:06 +0100

    add copyright header
    Vlad finshed the "longest" proofs, that's the reason
    I put his name there (very arbitrary, I know!)
    Otherwise, alphabetical by surname.

--- src/data/nat/count.lean
+++ src/data/nat/count.lean
@@ -1,3 +1,8 @@
+/-
+Copyright (c) 2021 Vladimir Goryachev. All rights reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
+Authors: Yaël Dillies, Vladimir Goryachev, Kyle Miller, Scott Morrison, Eric Rodriguez
+-/
 import data.list.basic
 import data.nat.prime
 import order.order_iso_nat

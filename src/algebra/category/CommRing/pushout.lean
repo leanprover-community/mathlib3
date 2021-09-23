7e376d3dab9ff529d03adc1bce6ab4628f19cb48
erd1 <the.erd.one@gmail.com>
2021-09-23 09:49:47 +0800

    Added copyright header

--- src/algebra/category/CommRing/pushout.lean
+++ src/algebra/category/CommRing/pushout.lean
@@ -1,3 +1,8 @@
+/-
+Copyright (c) 2020 Andrew Yang. All rights reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
+Authors: Andrew Yang
+-/
 import category_theory.limits.shapes.pullbacks
 import ring_theory.tensor_product
 import algebra.category.CommRing.basic

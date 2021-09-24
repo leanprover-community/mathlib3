8f2b18641ddc9abefc75e4f21cdefbc9158d6e4b
Chris Hughes <chrishughes24@gmail.com>
2021-09-24 15:41:37 +0100

    fix build

--- src/group_theory/group_action/conj_act.lean
+++ src/group_theory/group_action/conj_act.lean
@@ -12,13 +12,14 @@ the definition of conjugation as a homomorphism into the automorphism group.
 ## Main definitions
 A type alias `conj_act G` is introduced for a group `G`. The group `conj_act G` acts on `G`
 by conjugation.
--/
 
 ## Implementation Notes
-The scalar action in defined in this file can also be written using `mul_aut.conj g • h`. This 
+The scalar action in defined in this file can also be written using `mul_aut.conj g • h`. This
 has the advantage of not using the type alias `conj_act`, but the downside of this approach
-is that some theorems about the group actions will not apply when since this 
+is that some theorems about the group actions will not apply when since this
 `mul_aut.conj g • h` describes an action of `mul_aut G` on `G`, and not an action of `G`.
+
+-/
 
 variables (G : Type*)
 

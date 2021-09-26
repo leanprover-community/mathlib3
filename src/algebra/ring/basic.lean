83470aff743bfa2f4d524a496842a14f14f38239
Ruben Van de Velde <65514131+Ruben-VandeVelde@users.noreply.github.com>
2021-09-26 08:35:17 +0000

    feat(algebra/order/ring): add odd_neg, odd_abs, generalize dvd/abs lemmas (#9362)

--- src/algebra/ring/basic.lean
+++ src/algebra/ring/basic.lean
@@ -841,6 +841,17 @@ end
 @[simp] theorem even_neg (a : α) : even (-a) ↔ even a :=
 dvd_neg _ _
 
+lemma odd.neg {a : α} (hp : odd a) : odd (-a) :=
+begin
+  obtain ⟨k, hk⟩ := hp,
+  use -(k + 1),
+  rw [mul_neg_eq_neg_mul_symm, mul_add, neg_add, add_assoc, two_mul (1 : α), neg_add,
+    neg_add_cancel_right, ←neg_add, hk],
+end
+
+@[simp] lemma odd_neg (a : α) : odd (-a) ↔ odd a :=
+⟨λ h, neg_neg a ▸ h.neg, odd.neg⟩
+
 end ring
 
 section comm_ring

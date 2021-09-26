c96bd48e7b4b0bf39f1d65bb42ea4bc7b5f43a23
YaelDillies <yael.dillies@gmail.com>
2021-09-26 13:33:57 +0200

    fix imports

--- src/data/nat/factorial/cast.lean
+++ src/data/nat/factorial/cast.lean
@@ -3,7 +3,6 @@ Copyright (c) 2021 Yaël Dillies. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Yaël Dillies
 -/
-import data.nat.choose.basic
 import ring_theory.polynomial.pochhammer
 
 /-!

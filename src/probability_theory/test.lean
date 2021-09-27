f0b0f7edac385c3226ab858f1ae6d215a1374360
Kexing <kexing.ying19@imperial.ac.uk>
2021-09-27 22:16:01 +0100

    fix copyright

--- src/probability_theory/test.lean
+++ src/probability_theory/test.lean
@@ -1,3 +1,8 @@
+/-
+Copyright (c) 2021 Kexing Ying. All rights reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
+Authors: Kexing Ying
+-/
 import probability_theory.density
 
 /-

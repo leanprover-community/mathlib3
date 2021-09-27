7f9c5e7715333558c9e58bdfcb1c984e6a90c219
Eric Wieser <wieser.eric@gmail.com>
2021-09-27 12:44:39 +0100

    chore(ring_theory/coprime): split out imports into a new file so that `is_coprime` can be used earlier

--- src/ring_theory/int/basic.lean
+++ src/ring_theory/int/basic.lean
@@ -3,7 +3,7 @@ Copyright (c) 2018 Johannes Hölzl. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Johannes Hölzl, Jens Wagemaker, Aaron Anderson
 -/
-import ring_theory.coprime
+import ring_theory.coprime.basic
 import ring_theory.unique_factorization_domain
 
 /-!

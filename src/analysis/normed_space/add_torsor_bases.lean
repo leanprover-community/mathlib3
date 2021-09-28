d008f8d33f1e730bfd8d107ba07cb7af84e2ddfb
Oliver Nash <github@olivernash.org>
2021-09-28 12:03:34 +0100

    Minor tweaks

--- src/analysis/normed_space/add_torsor_bases.lean
+++ src/analysis/normed_space/add_torsor_bases.lean
@@ -23,7 +23,7 @@ open affine_map
 
 /-- Given a set `s` of affine-independent points belonging to an open set `u`, we may extend `s` to
 an affine basis, all of whose elements belong to `u`. -/
-lemma exists_subset_affine_independent_affine_span_eq_top_of_open {s u : set P} (hu : is_open u)
+lemma exists_subset_affine_independent_span_eq_top_of_open {s u : set P} (hu : is_open u)
   (hsu : s ⊆ u) (hne : s.nonempty) (h : affine_independent ℝ (coe : s → P)) :
   ∃ t : set P, s ⊆ t ∧ t ⊆ u ∧ affine_independent ℝ (coe : t → P) ∧ affine_span ℝ t = ⊤ :=
 begin
@@ -36,11 +36,11 @@ begin
     apply hεu,
     simp only [metric.mem_ball, f, line_map_apply, dist_vadd_left, norm_smul, real.norm_eq_abs,
       dist_eq_norm_vsub V y q],
-    cases eq_or_ne (∥y -ᵥ q∥) 0 with hyp hyp, { rwa [hyp, mul_zero], },
-    rw [← norm_pos_iff, norm_norm] at hyp,
+    cases eq_or_ne (∥y -ᵥ q∥) 0 with hyq hyq, { rwa [hyq, mul_zero], },
+    rw [← norm_pos_iff, norm_norm] at hyq,
     calc abs (ε / 2 / ∥y -ᵥ q∥) * ∥y -ᵥ q∥
-          = ε / 2 / ∥y -ᵥ q∥ * ∥y -ᵥ q∥ : by rw [abs_div, abs_of_pos (half_pos hε), abs_of_pos hyp]
-      ... = ε / 2 : div_mul_cancel _ (ne_of_gt hyp)
+          = ε / 2 / ∥y -ᵥ q∥ * ∥y -ᵥ q∥ : by rw [abs_div, abs_of_pos (half_pos hε), abs_of_pos hyq]
+      ... = ε / 2 : div_mul_cancel _ (ne_of_gt hyq)
       ... < ε : half_lt_self hε, },
   have hεyq : ∀ (y ∉ s), ε / 2 / dist y q ≠ 0,
   { simp only [ne.def, div_eq_zero_iff, or_false, dist_eq_zero, bit0_eq_zero, one_ne_zero,
@@ -52,11 +52,11 @@ begin
   { intros p hp, use ⟨p, ht₁ hp⟩, simp [w, hp], },
   { intros y hy,
     simp only [set.mem_range, set_coe.exists, subtype.coe_mk] at hy,
-    obtain ⟨p, hp, hyp⟩ := hy,
+    obtain ⟨p, hp, hyq⟩ := hy,
     by_cases hps : p ∈ s;
     simp only [w, hps, line_map_apply_one, units.coe_mk0, dif_neg, dif_pos, not_false_iff,
-      units.coe_one, subtype.coe_mk] at hyp;
-    rw ← hyp;
+      units.coe_one, subtype.coe_mk] at hyq;
+    rw ← hyq;
     [exact hsu hps, exact hf p], },
   { exact (ht₂.units_line_map ⟨q, ht₁ hq⟩ w).range, },
   { rw [affine_span_eq_affine_span_line_map_units (ht₁ hq) w, ht₃], },

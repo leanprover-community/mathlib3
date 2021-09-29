a69c6d55abc16f0ac626817728f26b785a439651
CBirkbeck <c.birkbeck@ucl.ac.uk>
2021-09-29 18:12:20 +0100

    double coset defn

--- src/number_theory/mod_forms/double_coset.lean
+++ src/number_theory/mod_forms/double_coset.lean
@@ -8,10 +8,41 @@ def double_coset_rel (H K : subgroup G) : G → G → Prop :=
 
 lemma double_coset_rel_is_equiv  (H K : subgroup G) : equivalence (double_coset_rel H K) :=
 begin
-rw equivalence,
-sorry,
+  rw double_coset_rel,
+  rw equivalence,
+  split,
+  simp only [exists_prop],
+  intro x,
+  use 1,
+  simp [H.one_mem, K.one_mem],
+  split,
+  intros x y,
+  simp only [and_imp, exists_prop, forall_exists_index],
+  intros a ha b hb hx,
+  use a⁻¹,
+  have haa:= subgroup.inv_mem H ha,
+  simp only [haa, true_and],
+  use b⁻¹,
+  have hbb:= subgroup.inv_mem K hb,
+  simp only [hbb, true_and],
+  rw ← hx,
+  simp_rw ← mul_assoc,
+  simp,
+  intros x y z,
+  simp only [and_imp, exists_prop, forall_exists_index],
+  intros a ha b hb hxy c hc d hd hyz,
+  use c*a,
+  have hac:= H.mul_mem hc ha,
+  simp only [hac, true_and],
+  use b*d,
+  have hdb:=  (K.mul_mem hb hd),
+  simp only [hdb, true_and],
+  rw ← hyz,
+  rw ← hxy,
+  simp_rw ← mul_assoc,
 end
 
-
-def left_rel (H K : subgroup G) : setoid G :=
+def double_coset_setoid (H K : subgroup G) : setoid G :=
 ⟨double_coset_rel H K, double_coset_rel_is_equiv H K⟩
+
+def double_coset_quotient (H K : subgroup G) : Type* := quotient (double_coset_setoid H K)

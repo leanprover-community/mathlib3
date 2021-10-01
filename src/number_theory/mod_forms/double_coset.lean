2e69645cbac8ee3596f84c49afed271c388524e7
CBirkbeck <c.birkbeck@ucl.ac.uk>
2021-10-01 13:52:06 +0100

    proof in prog

--- src/number_theory/mod_forms/double_coset.lean
+++ src/number_theory/mod_forms/double_coset.lean
@@ -1,7 +1,7 @@
 import data.setoid.basic
 import group_theory.subgroup.basic
 
-variables {G : Type*} [group G]
+variables {G : Type*} [group G] {α : Type*} [has_mul α]
 
 def double_coset_rel (H K : subgroup G) : G → G → Prop :=
 λ x y, (∃ (a ∈  H) (b ∈  K), a*x*b=y)
@@ -46,3 +46,52 @@ def double_coset_setoid (H K : subgroup G) : setoid G :=
 ⟨double_coset_rel H K, double_coset_rel_is_equiv H K⟩
 
 def double_coset_quotient (H K : subgroup G) : Type* := quotient (double_coset_setoid H K)
+
+def doset  (s t : set α) (a : α) : set α :={ b : α | ∃ (x ∈ s) (y ∈ t), b=x*a*y }
+
+@[simp]
+lemma doset_mem (s t : set α) (a b : α) : b ∈ (doset s t a) ↔ ∃ (x ∈ s) (y ∈ t), b=x*a*y :=iff.rfl
+
+lemma sub_doset  (H K : subgroup G) (a b : G) : b ∈ (doset H.1 K a) → (doset H.1 K b) ⊆  (doset H K a) :=
+begin
+  intro hb,
+  intro x,
+  simp only [and_imp, exists_prop, forall_exists_index, doset_mem, subgroup.mem_carrier,
+    set_like.mem_coe] at *,
+  let L:=classical.some_spec hb,
+  have hL:=L.2,
+  let R:=(classical.some_spec hL),
+  intros g hg h hh hx,
+  rw R.2 at hx,
+  use g*(classical.some hb),
+  simp only [H.mul_mem hg L.1, true_and],
+  use (classical.some hL)*h,
+  simp only [K.mul_mem R.1 hh, true_and],
+  simp_rw ← mul_assoc at *,
+  exact hx,
+end
+
+lemma disjoint_doset  (H K : subgroup G) (a b : G) : ¬ disjoint (doset H.1 K a  ) (doset H K b)
+  →  (doset H.1 K a  ) = (doset H K b) :=
+
+begin
+intro h,
+rw set.not_disjoint_iff at h,
+have hb :  b ∈ (doset H.1 K a), by {
+  simp at *,
+  let x:=classical.some_spec h,
+  have hx:= x.1,
+  have xh:= x.2,
+  let n:=classical.some_spec hx,
+  let m:=classical.some_spec n.2,
+  let nn:=classical.some_spec xh,
+  let mm:=classical.some_spec nn.2,
+  have hm:=m.2,
+  have hmm:=mm.2,
+
+
+
+sorry,
+},
+sorry,
+end

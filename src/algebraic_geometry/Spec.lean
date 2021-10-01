1e38a752efe7d5c45cd3d5ad0ff6770803e081d7
erd1 <the.erd.one@gmail.com>
2021-10-01 14:34:21 +0800

    added some docs about current progress

--- src/algebraic_geometry/Spec.lean
+++ src/algebraic_geometry/Spec.lean
@@ -23,9 +23,14 @@ We define $Spec$ in three consecutive steps, each with more structure than the l
 Additionally, we provide `Spec.to_PresheafedSpace` as a composition of `Spec.to_SheafedSpace` with
 a forgetful functor.
 
-## Future work
+## In progress
 
-Adjunction between `Γ` and `Spec`
+Adjunction between `Γ` and `Spec`: Currently, the counit of the adjunction is proven to be a
+natural transformation in `Spec_Γ_naturality`, and realized as a natural isomorphism in
+`Spec_Γ_identity`.
+
+TODO: provide the unit, and prove the triangle identities.
+
 
 -/
 

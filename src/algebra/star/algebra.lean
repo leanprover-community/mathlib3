905aaea00e22fd4d57f8e0e9d01078801ea327ba
Eric Wieser <wieser.eric@gmail.com>
2021-10-01 12:16:48 +0100

    chore(algebra/star/algebra): do not actually require an algebra

--- src/algebra/star/algebra.lean
+++ src/algebra/star/algebra.lean
@@ -19,15 +19,18 @@ universes u v w
 A star algebra `A` over a star ring `R` is an algebra which is a star ring,
 and the two star structures are compatible in the sense
 `star (r • a) = star r • star a`.
+
+Note that it is up to the user of this typeclass to enforce
+`[star_ring R] [star_ring A] [algebra R A]`, and that
+the statement only requires `[has_star R] [has_star A] [has_scalar R A]`.
 -/
--- Note that we take `star_ring A` as a typeclass argument, rather than extending it,
+-- Note that we take `has_star A` as a typeclass argument, rather than extending it,
 -- to avoid having multiple definitions of the star operation.
-class star_algebra (R : Type u) (A : Type v)
-  [comm_semiring R] [star_ring R] [semiring A] [star_ring A] [algebra R A] :=
-(star_smul : ∀ (r : R) (a : A), star (r • a) = (@has_star.star R _ r) • star a)
+class star_algebra (R : Type u) (A : Type v) [has_star R] [has_star A] [has_scalar R A] :=
+(star_smul : ∀ (r : R) (a : A), star (r • a) = star r • star a)
 
 @[simp] lemma star_smul {R : Type u} {A : Type v}
-  [comm_semiring R] [star_ring R] [semiring A] [star_ring A] [algebra R A] [star_algebra R A]
+  [has_star R] [has_star A] [has_scalar R A] [star_algebra R A]
   (r : R) (a : A) :
   star (r • a) = star r • star a :=
 star_algebra.star_smul r a
@@ -38,8 +41,7 @@ variable {I : Type u}     -- The indexing type
 variable {f : I → Type v} -- The family of types already equipped with instances
 
 instance {R : Type w}
-  [comm_semiring R] [Π i, semiring (f i)] [Π i, algebra R (f i)]
-  [star_ring R] [Π i, star_ring (f i)] [Π i, star_algebra R (f i)] :
+  [Π i, has_scalar R (f i)] [has_star R] [Π i, has_star (f i)] [Π i, star_algebra R (f i)] :
   star_algebra R (Π i, f i) :=
 { star_smul := λ r x, funext $ λ _, star_smul _ _ }
 

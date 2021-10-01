596c00960a6c31da59f1834544124cc2b190e6b8
Shing Tak Lam <shingtaklam1324@gmail.com>
2021-10-01 10:10:57 +0100

    add symm and trans, add simps

--- src/topology/homotopy/basic.lean
+++ src/topology/homotopy/basic.lean
@@ -18,10 +18,10 @@ HOL-Analysis. With this, we get `homotopy` as a special case of `homotopy_with`.
 
 ## Definitions
 
-* `homotopy_with f₀ f₁ P` is the type of homotoies between `f₀` and `f₁`, where the intermediate
+* `homotopy_with f₀ f₁ P` is the type of homotopies between `f₀` and `f₁`, where the intermediate
   maps satisfy the predicate `P : C(X, Y) → Prop`.
 * `homotopy_with.refl` is the homotopy between `f₀` and `f₀`.
-* `homotopy_with.symm F` is a defined by reversing the homotopy `F`
+* `homotopy_with.symm F` is defined by reversing the homotopy `F`
 * `homotopy_with.trans F G`, where `F : homotopy f₀ f₁`, `G : homotopy f₁ f₂` is a `homotopy f₀ f₂`
   defined by putting the first homotopy on `[0, 1/2]` and the second on `[1/2, 1]`.
 * `homotopy f₀ f₁` is defined to be `homotopy_with f₀ f₁ (λ f, true)`.
@@ -73,6 +73,12 @@ end
 @[ext]
 lemma ext {F G : homotopy_with f₀ f₁ P} (h : ∀ x, F x = G x) : F = G :=
 coe_fn_injective $ funext h
+
+/-- See Note [custom simps projection]. We need to specify this projection explicitly in this case,
+because it is a composition of multiple projections. -/
+def simps.apply (F : homotopy_with f₀ f₁ P) : I × X → Y := F
+
+initialize_simps_projections homotopy_with (to_continuous_map_to_fun -> apply, -to_continuous_map)
 
 @[continuity]
 protected lemma continuous (F : homotopy_with f₀ f₁ P) : continuous F := F.continuous_to_fun
@@ -147,6 +153,7 @@ variable {P : C(X, Y) → Prop}
 Given a continuous function `f` and a proof `hP : P f`, we can define a `homotopy_with f f` by
 `F (t, x) = f x`
 -/
+@[simps]
 def refl (f : C(X, Y)) (hP : P f) : homotopy_with f f P :=
 { to_fun := λ x, f x.2,
   continuous_to_fun := by continuity,
@@ -157,25 +164,19 @@ def refl (f : C(X, Y)) (hP : P f) : homotopy_with f f P :=
     ext, refl,
   end }
 
-@[simp]
-lemma refl_apply {f : C(X, Y)} (hP : P f) (x : I × X) : refl f hP x = f x.2 := rfl
-
 instance : inhabited (homotopy_with (continuous_map.id : C(X, X)) continuous_map.id (λ f, true)) :=
 ⟨homotopy_with.refl continuous_map.id trivial⟩
 
 /--
 Given a `homotopy_with f₀ f₁ P`, we can define a `homotopy_with f₁ f₀ P` by reversing the homotopy.
 -/
+@[simps]
 def symm {f₀ f₁ : C(X, Y)} (F : homotopy_with f₀ f₁ P) : homotopy_with f₁ f₀ P :=
 { to_fun := λ x, F (σ x.1, x.2),
   continuous_to_fun := by continuity,
   to_fun_zero := by norm_num,
   to_fun_one := by norm_num,
   prop' := λ t, by simpa using F.prop' (σ t) }
-
-@[simp]
-lemma symm_apply {f₀ f₁ : C(X, Y)} (F : homotopy_with f₀ f₁ P) (x : I × X) :
-  F.symm x = F (σ x.1, x.2) := rfl
 
 @[simp]
 lemma symm_symm {f₀ f₁ : C(X, Y)} (F : homotopy_with f₀ f₁ P) : F.symm.symm = F :=
@@ -189,21 +190,11 @@ def trans {f₀ f₁ f₂ : C(X, Y)} (F : homotopy_with f₀ f₁ P) (G : homoto
   homotopy_with f₀ f₂ P :=
 { to_fun := λ x, if (x.1 : ℝ) ≤ 1/2 then F.extend (2 * x.1) x.2 else G.extend (2 * x.1 - 1) x.2,
   continuous_to_fun := begin
-    refine continuous_if_le _ _ (continuous.continuous_on _) (continuous.continuous_on _) _,
-    swap 5,
-    { rintros x hx,
-      norm_num [hx] },
-    exact continuous_induced_dom.comp continuous_fst,
-    exact continuous_const,
-    -- TODO: Work out why `continuity` doesn't work here.
-    exact F.continuous.comp
-      ((continuous_proj_Icc.comp (continuous_const.mul
-        (continuous_induced_dom.comp continuous_fst))).prod_mk continuous_snd),
-    exact G.continuous.comp
-      ((continuous_proj_Icc.comp
-      ((continuous_const.mul
-        (continuous_induced_dom.comp continuous_fst)).sub continuous_const)).prod_mk
-          continuous_snd),
+    refine continuous_if_le (continuous_induced_dom.comp continuous_fst) continuous_const
+      (F.continuous.comp (by continuity)).continuous_on
+      (G.continuous.comp (by continuity)).continuous_on _,
+    rintros x hx,
+    norm_num [hx],
   end,
   to_fun_zero := λ x, by norm_num,
   to_fun_one := λ x, by norm_num,
@@ -260,7 +251,37 @@ namespace homotopy
 Given a map `f : C(X, Y)`, we can define a `homotopy f f` by setting `F (t, x) = f x` for all `t`.
 This is defined using `homotopy_with.refl`, but with the proof filled in.
 -/
+@[simps]
 def refl (f : C(X, Y)) : homotopy f f := homotopy_with.refl f trivial
+
+/--
+Given a `homotopy f₀ f₁`, we can define a `homotopy f₁ f₀` by reversing the homotopy.
+-/
+@[simps]
+def symm {f₀ f₁ : C(X, Y)} (F : homotopy f₀ f₁) : homotopy f₁ f₀ := homotopy_with.symm F
+
+@[simp]
+lemma symm_symm {f₀ f₁ : C(X, Y)} (F : homotopy f₀ f₁) : F.symm.symm = F :=
+homotopy_with.symm_symm F
+
+/--
+Given `homotopy f₀ f₁` and `homotopy f₁ f₂`, we can define a `homotopy f₀ f₂`
+by putting the first homotopy on `[0, 1/2]` and the second on `[1/2, 1]`.
+-/
+def trans {f₀ f₁ f₂ : C(X, Y)} (F : homotopy f₀ f₁) (G : homotopy f₁ f₂) : homotopy f₀ f₂ :=
+homotopy_with.trans F G
+
+lemma trans_apply {f₀ f₁ f₂ : C(X, Y)} (F : homotopy f₀ f₁) (G : homotopy f₁ f₂)
+  (x : I × X) : (F.trans G) x =
+  if h : (x.1 : ℝ) ≤ 1/2 then
+    F (⟨2 * x.1, (unit_interval.mul_pos_mem_iff zero_lt_two).2 ⟨x.1.2.1, h⟩⟩, x.2)
+  else
+    G (⟨2 * x.1 - 1, unit_interval.two_mul_sub_one_mem_iff.2 ⟨(not_le.1 h).le, x.1.2.2⟩⟩, x.2) :=
+homotopy_with.trans_apply F G x
+
+lemma symm_trans {f₀ f₁ f₂ : C(X, Y)} (F : homotopy f₀ f₁) (G : homotopy f₁ f₂) :
+  (F.trans G).symm = G.symm.trans F.symm :=
+homotopy_with.symm_trans F G
 
 end homotopy
 
@@ -284,16 +305,92 @@ lemma eq_snd (F : homotopy_rel f₀ f₁ S) (t : I) {x : X} (hx : x ∈ S) :
 
 lemma fst_eq_snd (F : homotopy_rel f₀ f₁ S) {x : X} (hx : x ∈ S) :
   f₀ x = f₁ x := F.eq_fst 0 hx ▸ F.eq_snd 0 hx
+
 end
 
-end homotopy_rel
+variables {f₀ f₁ f₂ : C(X, Y)} {S : set X}
 
 /--
 Given a map `f : C(X, Y)` and a set `S`, we can define a `homotopy_rel f f S` by setting
 `F (t, x) = f x` for all `t`. This is defined using `homotopy_with.refl`, but with the proof
 filled in.
 -/
+@[simps]
 def refl (f : C(X, Y)) (S : set X) : homotopy_rel f f S :=
 homotopy_with.refl f (λ x hx, ⟨rfl, rfl⟩)
 
+/--
+Given a `homotopy_rel f₀ f₁ S`, we can define a `homotopy_rel f₁ f₀ S` by reversing the homotopy.
+-/
+@[simps]
+def symm (F : homotopy_rel f₀ f₁ S) : homotopy_rel f₁ f₀ S :=
+{ prop' := λ t x hx, by simp [F.eq_snd _ hx, F.fst_eq_snd hx],
+  ..homotopy_with.symm F }
+
+@[simp]
+lemma symm_symm (F : homotopy_rel f₀ f₁ S) : F.symm.symm = F :=
+homotopy_with.symm_symm F
+
+/--
+Given `homotopy_rel f₀ f₁ S` and `homotopy_rel f₁ f₂ S`, we can define a `homotopy_rel f₀ f₂ S`
+by putting the first homotopy on `[0, 1/2]` and the second on `[1/2, 1]`.
+-/
+def trans (F : homotopy_rel f₀ f₁ S) (G : homotopy_rel f₁ f₂ S) : homotopy_rel f₀ f₂ S :=
+{ to_fun := λ x, if (x.1 : ℝ) ≤ 1/2 then F.extend (2 * x.1) x.2 else G.extend (2 * x.1 - 1) x.2,
+  continuous_to_fun := begin
+    refine continuous_if_le (continuous_induced_dom.comp continuous_fst) continuous_const
+      (F.continuous.comp (by continuity)).continuous_on
+      (G.continuous.comp (by continuity)).continuous_on _,
+    rintros x hx,
+    norm_num [hx],
+  end,
+  to_fun_zero := λ x, by norm_num,
+  to_fun_one := λ x, by norm_num,
+  prop' := λ t, begin
+    simp only,
+    split_ifs,
+    { intros x hx,
+      simp [(homotopy_with.extend_prop F (2 * t) x hx).1, F.fst_eq_snd hx, G.fst_eq_snd hx] },
+    { intros x hx,
+      simp [(homotopy_with.extend_prop G (2 * t - 1) x hx).1, F.fst_eq_snd hx, G.fst_eq_snd hx] },
+  end }
+
+lemma trans_apply (F : homotopy_rel f₀ f₁ S) (G : homotopy_rel f₁ f₂ S)
+  (x : I × X) : (F.trans G) x =
+  if h : (x.1 : ℝ) ≤ 1/2 then
+    F (⟨2 * x.1, (unit_interval.mul_pos_mem_iff zero_lt_two).2 ⟨x.1.2.1, h⟩⟩, x.2)
+  else
+    G (⟨2 * x.1 - 1, unit_interval.two_mul_sub_one_mem_iff.2 ⟨(not_le.1 h).le, x.1.2.2⟩⟩, x.2) :=
+show ite _ _ _ = _,
+by split_ifs;
+  { rw [homotopy_with.extend, continuous_map.coe_Icc_extend, set.Icc_extend_of_mem], refl }
+
+lemma symm_trans (F : homotopy_rel f₀ f₁ S) (G : homotopy_rel f₁ f₂ S) :
+  (F.trans G).symm = G.symm.trans F.symm :=
+begin
+  ext x,
+  simp only [symm_apply, trans_apply],
+  split_ifs with h₁ h₂,
+  { change (x.1 : ℝ) ≤ _ at h₂,
+    change (1 : ℝ) - x.1 ≤ _ at h₁,
+    have ht : (x.1 : ℝ) = 1/2,
+    { linarith },
+    norm_num [ht] },
+  { congr' 2,
+    apply subtype.ext,
+    simp only [unit_interval.coe_symm_eq, subtype.coe_mk],
+    linarith },
+  { congr' 2,
+    apply subtype.ext,
+    simp only [unit_interval.coe_symm_eq, subtype.coe_mk],
+    linarith },
+  { change ¬ (x.1 : ℝ) ≤ _ at h,
+    change ¬ (1 : ℝ) - x.1 ≤ _ at h₁,
+    exfalso, linarith }
+end
+
+end homotopy_rel
+
 end continuous_map
+
+#lint

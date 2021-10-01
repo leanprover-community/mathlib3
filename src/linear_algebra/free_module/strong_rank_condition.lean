ce9f02de85d3f61fa114ae0d8fb030b6054cbce9
Riccardo Brasca <riccardo.brasca@gmail.com>
2021-10-01 14:28:17 +0200

    fix build

--- src/linear_algebra/free_module/strong_rank_condition.lean
+++ src/linear_algebra/free_module/strong_rank_condition.lean
@@ -4,7 +4,7 @@ Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Riccardo Brasca
 -/
 
-import linear_algebra.charpoly
+import linear_algebra.charpoly.basic
 import linear_algebra.invariant_basis_number
 
 /-!

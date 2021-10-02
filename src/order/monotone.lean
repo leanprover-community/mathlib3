154e0492abc57db345b686ce8279e256c1136596
YaelDillies <yael.dillies@gmail.com>
2021-10-02 09:47:07 +0200

    fix copyright header

--- src/order/monotone.lean
+++ src/order/monotone.lean
@@ -1,7 +1,7 @@
 /-
 Copyright (c) 2014 Jeremy Avigad. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
-Author: Jeremy Avigad, Mario Carneiro, Yaël Dillies
+Authors: Jeremy Avigad, Mario Carneiro, Yaël Dillies
 -/
 import order.basic
 

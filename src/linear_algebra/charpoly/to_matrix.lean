e5879c6d4e961cb41eb3a48ed030f187ed294e7d
Riccardo Brasca <riccardo.brasca@gmail.com>
2021-10-02 09:19:01 +0200

    useless import

--- src/linear_algebra/charpoly/to_matrix.lean
+++ src/linear_algebra/charpoly/to_matrix.lean
@@ -4,7 +4,6 @@ Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Riccardo Brasca
 -/
 
-import linear_algebra.charpoly.basic
 import linear_algebra.matrix.basis
 import linear_algebra.free_module.strong_rank_condition
 

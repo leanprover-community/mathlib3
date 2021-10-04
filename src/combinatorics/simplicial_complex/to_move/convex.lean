fdb3d1a0396932e5b80874ec6fd5e81a32542dac
YaelDillies <yael.dillies@gmail.com>
2021-10-04 10:04:16 +0200

    fix convex

--- src/combinatorics/simplicial_complex/to_move/convex.lean
+++ src/combinatorics/simplicial_complex/to_move/convex.lean
@@ -3,33 +3,33 @@ Copyright (c) 2021 Yaël Dillies, Bhavik Mehta. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Yaël Dillies, Bhavik Mehta
 -/
-import analysis.convex.basic
+import analysis.convex.hull
 
 /-!
 # To move
 -/
 
-variables {E : Type*} [add_comm_group E] [module ℝ E] {s X Y : set E}
-
 open set
+
+variables {𝕜 E ι : Type*} [ordered_semiring 𝕜] [add_comm_monoid E] [module 𝕜 E] {s X Y : set E}
 
 -- can be proven from the stuff about closure operators
 lemma convex_hull_convex_hull_union :
-  convex_hull (convex_hull X ∪ Y) = convex_hull (X ∪ Y) :=
+  convex_hull 𝕜 (convex_hull 𝕜 X ∪ Y) = convex_hull 𝕜 (X ∪ Y) :=
 subset.antisymm (convex_hull_min (union_subset (convex_hull_mono (subset_union_left X Y))
-  (subset.trans (subset_convex_hull Y) (convex_hull_mono (subset_union_right X Y))))
-  (convex_convex_hull _)) (convex_hull_mono (union_subset_union_left _ (subset_convex_hull _)))
+  (subset.trans (subset_convex_hull 𝕜 Y) (convex_hull_mono (subset_union_right X Y))))
+  (convex_convex_hull 𝕜 _)) (convex_hull_mono (union_subset_union_left _ (subset_convex_hull 𝕜 _)))
 
 -- can be proven from the stuff about closure operators
 lemma convex_hull_self_union_convex_hull :
-  convex_hull (X ∪ convex_hull Y) = convex_hull (X ∪ Y) :=
+  convex_hull 𝕜 (X ∪ convex_hull 𝕜 Y) = convex_hull 𝕜 (X ∪ Y) :=
 begin
   rw [union_comm, union_comm X Y],
   exact convex_hull_convex_hull_union,
 end
 
-lemma eq_left_or_right_or_mem_open_segment_of_mem_segment {x y z : E} (hz : z ∈ segment ℝ x y) :
-  z = x ∨ z = y ∨ z ∈ open_segment ℝ x y :=
+lemma eq_left_or_right_or_mem_open_segment_of_mem_segment {x y z : E} (hz : z ∈ segment 𝕜 x y) :
+  z = x ∨ z = y ∨ z ∈ open_segment 𝕜 x y :=
 begin
    obtain ⟨a, b, ha, hb, hab, hz⟩ := hz,
   by_cases ha' : a = 0,
--- src/combinatorics/simplicial_complex/to_move/hahn_banach.lean
+++ src/combinatorics/simplicial_complex/to_move/hahn_banach.lean
@@ -19,24 +19,24 @@ variables {E : Type*} [normed_group E] [normed_space ℝ E] {x : E} {A B C : set
   {X : finset E}
 
 theorem geometric_hahn_banach_compact_closed {A B : set E}
-  (hA₁ : convex A) (hA₂ : is_compact A)
-  (hB₁ : convex B) (hB₂ : is_closed B)
+  (hA₁ : convex ℝ A) (hA₂ : is_compact A)
+  (hB₁ : convex ℝ B) (hB₂ : is_closed B)
   (disj : disjoint A B) :
   ∃ (f : E →L[ℝ] ℝ) (s t : ℝ), s < t ∧ (∀ b ∈ B, f b < s) ∧ (∀ a ∈ A, t < f a) :=
 sorry
 
 theorem geometric_hahn_banach_closed_point {A : set E} {x : E}
-  (hA₁ : convex A) (hA₂ : is_closed A)
+  (hA₁ : convex ℝ A) (hA₂ : is_closed A)
   (disj : x ∉ A) :
   ∃ (f : E →L[ℝ] ℝ) (s : ℝ), (∀ a ∈ A, f a < s) ∧ s < f x := sorry
 
 theorem geometric_hahn_banach_open_point {A : set E} {x : E}
-  (hA₁ : convex A) (hA₂ : is_open A)
+  (hA₁ : convex ℝ A) (hA₂ : is_open A)
   (disj : x ∉ A) :
   ∃ (f : E →L[ℝ] ℝ), (∀ a ∈ A, f a < f x) := sorry
 
 theorem geometric_hahn_banach_point_open {x : E} {B : set E}
-  (hB₁ : convex B) (hB₂ : is_open B)
+  (hB₁ : convex ℝ B) (hB₂ : is_open B)
   (disj : x ∉ B) :
   ∃ (f : E →L[ℝ] ℝ), (∀ b ∈ B, f x < f b) :=
 let ⟨f, hf⟩ := geometric_hahn_banach_open_point hB₁ hB₂ disj in ⟨-f, by simpa⟩

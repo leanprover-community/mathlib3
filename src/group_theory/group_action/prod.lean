3e6ca3516a3800acb790ce70643d73ced5f1870f
Eric Wieser <wieser.eric@gmail.com>
2021-10-05 12:07:27 +0100

    Update src/group_theory/group_action/prod.lean

--- src/group_theory/group_action/prod.lean
+++ src/group_theory/group_action/prod.lean
@@ -24,7 +24,7 @@ variables [has_scalar M α] [has_scalar M β] [has_scalar N α] [has_scalar N β
 @[simp, to_additive] theorem smul_fst : (a • x).1 = a • x.1 := rfl
 @[simp, to_additive] theorem smul_snd : (a • x).2 = a • x.2 := rfl
 @[simp, to_additive] theorem smul_mk (a : M) (b : α) (c : β) : a • (b, c) = (a • b, a • c) := rfl
-theorem smul_def (a : M) (x : α × β) : a • x = (a • x.1, a • x.2) := rfl
+@[to_additive] theorem smul_def (a : M) (x : α × β) : a • x = (a • x.1, a • x.2) := rfl
 
 instance [has_scalar M N] [is_scalar_tower M N α] [is_scalar_tower M N β] :
   is_scalar_tower M N (α × β) :=

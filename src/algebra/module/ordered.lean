0f3a91f90c406c3a5aac5aa7e93eb95be0ecfc98
YaelDillies <yael.dillies@gmail.com>
2021-10-05 13:28:01 +0200

    move to algebra.module.ordered

--- src/algebra/module/ordered.lean
+++ src/algebra/module/ordered.lean
@@ -5,9 +5,9 @@ Authors: Frédéric Dupuis, Yaël Dillies
 -/
 
 import algebra.module.pi
-import algebra.order.pi
 import algebra.module.prod
 import algebra.order.field
+import algebra.order.pi
 import algebra.order.smul
 
 /-!
@@ -150,4 +150,146 @@ instance [semiring k] [ordered_add_comm_monoid M] [module k M] : module k (order
 { add_smul := λ r s x, order_dual.rec (add_smul _ _) x,
   zero_smul := λ m, order_dual.rec (zero_smul _) m }
 
-end order_dual
+end
+
+/-! ### Upper/lower bounds -/
+
+section ordered_semiring
+variables {α β : Type*} [ordered_semiring α] [ordered_add_comm_monoid β] [smul_with_zero α β]
+  [ordered_smul α β] {s : set β} {a : α}
+
+lemma smul_lower_bounds_subset_lower_bounds_smul (ha : 0 ≤ a) :
+  a • lower_bounds s ⊆ lower_bounds (a • s) :=
+begin
+  rintro _ ⟨b, hb, rfl⟩ _ ⟨x, hx, rfl⟩,
+  exact smul_le_smul_of_nonneg (hb hx) ha,
+end
+
+lemma smul_upper_bounds_subset_upper_bounds_smul (ha : 0 ≤ a) :
+  a • upper_bounds s ⊆ upper_bounds (a • s) :=
+begin
+  rintro _ ⟨b, hb, rfl⟩ _ ⟨x, hx, rfl⟩,
+  exact smul_le_smul_of_nonneg (hb hx) ha,
+end
+
+lemma bdd_below.smul_of_nonneg (ha : 0 ≤ a) (hs : bdd_below s) :
+  bdd_below (a • s) :=
+begin
+  obtain ⟨b, hb⟩ := hs,
+  exact ⟨a • b, smul_lower_bounds_subset_lower_bounds_smul ha (smul_mem_smul_set hb)⟩,
+end
+
+lemma bdd_above.smul_of_nonneg (ha : 0 ≤ a) (hs : bdd_above s) :
+  bdd_above (a • s) :=
+begin
+  obtain ⟨b, hb⟩ := hs,
+  exact ⟨a • b, smul_upper_bounds_subset_upper_bounds_smul ha (smul_mem_smul_set hb)⟩,
+end
+
+end ordered_semiring
+
+section ordered_ring
+variables {α β : Type*} [ordered_ring α] [ordered_add_comm_group β] [module α β]
+  [ordered_smul α β] {s : set β} {a : α}
+
+lemma smul_lower_bounds_subset_upper_bounds_smul (ha : a ≤ 0) :
+  a • lower_bounds s ⊆ upper_bounds (a • s) :=
+begin
+  rintro _ ⟨b, hb, rfl⟩ _ ⟨x, hx, rfl⟩,
+  exact smul_le_smul_of_nonpos (hb hx) ha,
+end
+
+lemma smul_upper_bounds_subset_lower_bounds_smul (ha : a ≤ 0) :
+  a • upper_bounds s ⊆ lower_bounds (a • s) :=
+begin
+  rintro _ ⟨b, hb, rfl⟩ _ ⟨x, hx, rfl⟩,
+  exact smul_le_smul_of_nonpos (hb hx) ha,
+end
+
+lemma bdd_below.smul_of_nonpos (ha : a ≤ 0) (hs : bdd_below s) :
+  bdd_above (a • s) :=
+begin
+  obtain ⟨b, hb⟩ := hs,
+  exact ⟨a • b, smul_lower_bounds_subset_upper_bounds_smul ha (smul_mem_smul_set hb)⟩,
+end
+
+lemma bdd_above.smul_of_nonpos (ha : a ≤ 0) (hs : bdd_above s) :
+  bdd_below (a • s) :=
+begin
+  obtain ⟨b, hb⟩ := hs,
+  exact ⟨a • b, smul_upper_bounds_subset_lower_bounds_smul ha (smul_mem_smul_set hb)⟩,
+end
+
+end ordered_ring
+
+section linear_ordered_field
+variables {α β : Type*} [linear_ordered_field α] [ordered_add_comm_group β]
+
+section mul_action_with_zero
+variables [mul_action_with_zero α β] [ordered_smul α β] {s t : set β} {a : α}
+
+/-- Left scalar multiplication as an order isomorphism. -/
+@[simps] def order_iso.smul_left {c : k} (hc : 0 < c) : M ≃o M :=
+{ to_fun := λ b, c • b,
+  inv_fun := λ b, c⁻¹ • b,
+  left_inv := inv_smul_smul₀ hc.ne',
+  right_inv := smul_inv_smul₀ hc.ne',
+  map_rel_iff' := λ b₁ b₂, smul_le_smul_iff_of_pos hc }
+
+@[simp] lemma lower_bounds_smul_of_pos (ha : 0 < a) :
+  lower_bounds (a • s) = a • lower_bounds s :=
+(order_iso.smul_left ha).lower_bounds_image
+
+@[simp] lemma upper_bounds_smul_of_pos (ha : 0 < a) :
+  upper_bounds (a • s) = a • upper_bounds s :=
+(order_iso.smul_left ha).upper_bounds_image
+
+@[simp] lemma bdd_below_smul_iff_of_pos (ha : 0 < a) :
+  bdd_below (a • s) ↔ bdd_below s :=
+(order_iso.smul_left ha).bdd_below_image
+
+@[simp] lemma bdd_above_smul_iff_of_pos (ha : 0 < a) :
+  bdd_above (a • s) ↔ bdd_above s :=
+(order_iso.smul_left ha).bdd_above_image
+
+end mul_action_with_zero
+
+section module
+variables [module α β] [ordered_smul α β] {s t : set β} {a : α}
+
+@[simp] lemma lower_bounds_smul_of_neg (ha : a < 0) :
+  lower_bounds (a • s) = a • upper_bounds s :=
+begin
+  refine subset.antisymm _ (smul_upper_bounds_subset_lower_bounds_smul ha.le),
+  have h : a⁻¹ • lower_bounds (a • s) ⊆ upper_bounds (a⁻¹ • a • s) :=
+    smul_lower_bounds_subset_upper_bounds_smul (inv_nonpos.2 ha.le),
+  rwa [←subset_smul_iff' ha.ne, inv_smul_smul' ha.ne] at h,
+end
+
+@[simp] lemma upper_bounds_smul_of_neg (ha : a < 0) :
+  upper_bounds (a • s) = a • lower_bounds s :=
+begin
+  refine subset.antisymm _ (smul_lower_bounds_subset_upper_bounds_smul ha.le),
+  have h : a⁻¹ • upper_bounds (a • s) ⊆ lower_bounds (a⁻¹ • a • s) :=
+    smul_upper_bounds_subset_lower_bounds_smul (inv_nonpos.2 ha.le),
+  rwa [←subset_smul_iff' ha.ne, inv_smul_smul' ha.ne] at h,
+end
+
+@[simp] lemma bdd_below_smul_iff_of_neg (ha : a < 0) :
+  bdd_below (a • s) ↔ bdd_above s :=
+begin
+  refine ⟨λ h, _, bdd_above.smul_of_nonpos ha.le⟩,
+  rw ←inv_smul_smul' ha.ne s,
+  exact h.smul_of_nonpos (inv_nonpos.2 ha.le),
+end
+
+@[simp] lemma bdd_above_smul_iff_of_neg (ha : a < 0) :
+  bdd_above (a • s) ↔ bdd_below s :=
+begin
+  refine ⟨λ h, _, bdd_below.smul_of_nonpos ha.le⟩,
+  rw ←inv_smul_smul' ha.ne s,
+  exact h.smul_of_nonpos (inv_nonpos.2 ha.le),
+end
+
+end module
+end linear_ordered_field

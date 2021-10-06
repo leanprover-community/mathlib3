e86c00d78e967b7d38185af2bcc0d6fc56fbfa6c
Heather Macbeth <25316162+hrmacbeth@users.noreply.github.com>
2021-10-06 06:25:00 -0400

    fix

--- src/analysis/normed_space/linear_isometry.lean
+++ src/analysis/normed_space/linear_isometry.lean
@@ -387,9 +387,9 @@ e.isometry.comp_continuous_on_iff
 e.isometry.comp_continuous_iff
 
 /-- Construct a linear isometry equiv from a surjective linear isometry. -/
-noncomputable def of_surjective (f : E →ₛₗᵢ[σ₁₂] E₂)
+noncomputable def of_surjective (f : F →ₛₗᵢ[σ₁₂] E₂)
   (hfr : function.surjective f) :
-  E ≃ₛₗᵢ[σ₁₂] E₂ :=
+  F ≃ₛₗᵢ[σ₁₂] E₂ :=
 { norm_map' := f.norm_map,
   .. linear_equiv.of_bijective f.to_linear_map f.injective hfr }
 

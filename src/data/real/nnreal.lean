beac493303dec194e954443338045245d602e508
Floris van Doorn <fpvdoorn@gmail.com>
2021-10-07 18:40:56 +0200

    remove superfluous import

--- src/data/real/nnreal.lean
+++ src/data/real/nnreal.lean
@@ -3,7 +3,6 @@ Copyright (c) 2018 Johan Commelin. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Johan Commelin
 -/
-import algebra.order.with_zero
 import algebra.big_operators.ring
 import data.real.basic
 import algebra.indicator_function

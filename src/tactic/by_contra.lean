8b7a5407e38da15a11ef50ec1150760efaf52d61
Johan Commelin <johan@commelin.net>
2021-10-08 16:46:17 +0000

    process review comments

--- src/tactic/by_contra.lean
+++ src/tactic/by_contra.lean
@@ -26,18 +26,21 @@ This tactic will attempt to normalize negations in `¬ p` using `push_neg`.
 
 This tactic uses classical reasoning.
 
-A variant on the tactic `by_contra`.
+A variant on the tactic `by_contra` (`tactic.interactive.by_contra`).
 -/
 meta def by_contra' (h : parse ident?) (t : parse (tk ":" *> texpr)?) : tactic unit := do
   let h := h.get_or_else `this,
-  applyc ``classical.by_contradiction,
+  tgt ← target,
+  mk_mapp `classical.by_contradiction [some tgt] >>= tactic.eapply >> skip,
   h₁ ← tactic.intro h,
   t' ← infer_type h₁,
+  -- negation-normalize `t'` to the expression `e'` and get a proof `pr'` of `t' = e'`
   (e', pr') ← push_neg.normalize_negations t' <|> refl_conv t',
   match t with
   | none := () <$ replace_hyp h₁ e' pr'
   | some t := do
     t ← to_expr ``(%%t : Prop),
+    -- negation-normalize `t` to the expression `e` and get a proof `pr` of `t = e`
     (e, pr) ← push_neg.normalize_negations t <|> refl_conv t,
     unify e e',
     () <$ (mk_eq_symm pr >>= mk_eq_trans pr' >>= replace_hyp h₁ t)

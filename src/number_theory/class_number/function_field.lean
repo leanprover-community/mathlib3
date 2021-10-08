4f45ef2bdc833e80d6f04bd64897782f1cfeb0ed
Vierkantor <vierkantor@vierkantor.com>
2021-10-08 09:22:04 +0000

    Update to `ratfunc`

--- src/number_theory/class_number/function_field.lean
+++ src/number_theory/class_number/function_field.lean
@@ -22,9 +22,9 @@ cardinality of the class group of its ring of integers
 namespace function_field
 
 variables (Fq F : Type) [field Fq] [fintype Fq] [field F]
-variables [algebra (polynomial Fq) F] [algebra (fraction_ring (polynomial Fq)) F]
-variables [is_scalar_tower (polynomial Fq) (fraction_ring (polynomial Fq)) F]
-variables [function_field Fq F] [is_separable (fraction_ring (polynomial Fq)) F]
+variables [algebra (polynomial Fq) F] [algebra (ratfunc Fq) F]
+variables [is_scalar_tower (polynomial Fq) (ratfunc Fq) F]
+variables [function_field Fq F] [is_separable (ratfunc Fq) F]
 
 open_locale classical
 
@@ -33,7 +33,7 @@ namespace ring_of_integers
 open function_field
 
 noncomputable instance  : fintype (class_group (ring_of_integers Fq F) F) :=
-class_group.fintype_of_admissible_of_finite (fraction_ring (polynomial Fq)) F
+class_group.fintype_of_admissible_of_finite (ratfunc Fq) F
   (polynomial.card_pow_degree_is_admissible : absolute_value.is_admissible
     (polynomial.card_pow_degree : absolute_value (polynomial Fq) ℤ))
 

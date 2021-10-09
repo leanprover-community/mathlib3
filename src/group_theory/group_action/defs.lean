b37cc805f29c829513d1f71740fd5cfd2bb20f51
Yury G. Kudryashov <urkud@urkud.name>
2021-10-09 14:59:32 -0400

    Add a missing `to_additive`

--- src/group_theory/group_action/defs.lean
+++ src/group_theory/group_action/defs.lean
@@ -338,7 +338,7 @@ lemma is_scalar_tower.of_smul_one_mul {M N} [monoid N] [has_scalar M N]
   is_scalar_tower M N N :=
 ⟨λ x y z, by rw [← h, smul_eq_mul, mul_assoc, h, smul_eq_mul]⟩
 
-lemma smul_comm_class.of_mul_smul_one {M N} [monoid N] [has_scalar M N]
+@[to_additive] lemma smul_comm_class.of_mul_smul_one {M N} [monoid N] [has_scalar M N]
   (H : ∀ (x : M) (y : N), y * (x • (1 : N)) = x • y) : smul_comm_class M N N :=
 ⟨λ x y z, by rw [← H x z, smul_eq_mul, ← H, smul_eq_mul, mul_assoc]⟩
 

823ea3752dbf0f6d7e9a3a8a9314e611e6892d4e
Frédéric Dupuis <dupuisf@iro.umontreal.ca>
2021-10-09 21:24:05 -0400

    docstring for module.lean

--- src/algebra/star/module.lean
+++ src/algebra/star/module.lean
@@ -3,9 +3,14 @@ Copyright (c) 2021 Eric Wieser. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Eric Wieser, Frédéric Dupuis
 -/
-
 import algebra.star.basic
 import data.equiv.module
+
+/-!
+# The star operation, bundled as a star-linear equiv
+
+We define `star_linear_equiv`, which is the star operation bundled as a star-linear map.
+-/
 
 /-- If `A` is a left- and right- module over a commutative `R` with compatible actions,
 then `star` is a semilinear equivalence. -/

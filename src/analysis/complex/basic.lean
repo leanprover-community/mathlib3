fe06b8a8230a1a5c2aab1830a413b1f06ec09f60
Kexing <kexing.ying19@imperial.ac.uk>
2021-10-10 21:59:17 +0100

    reviews

--- src/analysis/complex/basic.lean
+++ src/analysis/complex/basic.lean
@@ -208,27 +208,18 @@ def equiv_real_prod_add_hom_lm : ℂ ≃ₗ[ℝ] ℝ × ℝ :=
 
 /-- The natural `continuous_linear_equiv` from `ℂ` to `ℝ × ℝ`. -/
 def equiv_real_prodₗ : ℂ ≃L[ℝ] ℝ × ℝ :=
-{ map_smul' := by simp [equiv_real_prod_add_hom],
-  continuous_to_fun := continuous_re.prod_mk continuous_im,
-  continuous_inv_fun :=
-  begin
-    refine linear_map.continuous_of_bound (equiv_real_prod_add_hom_lm.symm.to_linear_map) 2 _,
-    rintro ⟨a, b⟩,
-    rw two_mul,
-    exact le_trans (abs_le_abs_re_add_abs_im _) (add_le_add (le_max_left _ _) (le_max_right _ _)),
-  end, .. equiv_real_prod_add_hom }
+equiv_real_prod_add_hom_lm.to_continuous_linear_equiv
 
 end
 
 lemma has_sum_iff {α} (f : α → ℂ) (c : ℂ) :
   has_sum f c ↔ has_sum (λ x, (f x).re) c.re ∧ has_sum (λ x, (f x).im) c.im :=
 begin
-  split,
-  { exact λ h, ⟨h.mapL re_clm, h.mapL im_clm⟩ },
-  { rintro ⟨h₁, h₂⟩,
-    convert (h₁.prod_mk h₂).mapL equiv_real_prodₗ.symm.to_continuous_linear_map,
-    { ext x; refl },
-    { cases c, refl } }
+  refine ⟨λ h, ⟨h.mapL re_clm, h.mapL im_clm⟩, _⟩,
+  rintro ⟨h₁, h₂⟩,
+  convert (h₁.prod_mk h₂).mapL equiv_real_prodₗ.symm.to_continuous_linear_map,
+  { ext x; refl },
+  { cases c, refl }
 end
 
 end complex

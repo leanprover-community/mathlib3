d003a4a4988be98bcd4fcecbf9abf8866b34804b
Vierkantor <vierkantor@vierkantor.com>
2021-10-11 15:38:55 +0000

    Update to newer `polynomial-div-gcd-lemmas`

--- src/field_theory/ratfunc.lean
+++ src/field_theory/ratfunc.lean
@@ -4,7 +4,7 @@ Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Anne Baanen
 -/
 
-import algebra.gcd_monoid.euclidean_domain
+import ring_theory.euclidean_domain
 import ring_theory.localization
 
 /-!

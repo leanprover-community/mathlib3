ac27d85b8fb1ea4235296720a7ec08900eeb58fc
Riccardo Brasca <riccardo.brasca@gmail.com>
2021-10-11 19:56:26 +0200

    add neg_iff

--- src/analysis/normed_space/normed_group_hom.lean
+++ src/analysis/normed_space/normed_group_hom.lean
@@ -548,8 +548,8 @@ lemma comp {g : normed_group_hom V₂ V₃} {f : normed_group_hom V₁ V₂}
   (g.comp f).norm_noninc :=
 λ v, (hg (f v)).trans (hf v)
 
-lemma neg {f : normed_group_hom V₁ V₂} (hf : f.norm_noninc) : (-f).norm_noninc :=
-λ x, (norm_neg (f x)).le.trans (hf x)
+@[simp] lemma neg_iff {f : normed_group_hom V₁ V₂} : (-f).norm_noninc ↔ f.norm_noninc :=
+⟨λ h x, by { simpa using h x }, λ h x, (norm_neg (f x)).le.trans (h x)⟩
 
 end norm_noninc
 

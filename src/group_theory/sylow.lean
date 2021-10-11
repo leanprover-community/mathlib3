fe34fcc682be71141bfa8f79fa63b6342333a7ca
Thomas Browning <tb65536@uw.edu>
2021-10-11 09:32:31 -0700

    lemma

--- src/group_theory/sylow.lean
+++ src/group_theory/sylow.lean
@@ -4,6 +4,7 @@ Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Chris Hughes, Thomas Browning
 -/
 
+import group_theory.group_action.conj_act
 import group_theory.p_group
 
 /-!
@@ -197,6 +198,28 @@ lemma card_sylow_eq_index_normalizer [fact p.prime] [fintype (sylow p G)] (P : s
 lemma card_sylow_dvd_index [fact p.prime] [fintype (sylow p G)] (P : sylow p G) :
   card (sylow p G) ∣ P.1.index :=
 ((congr_arg _ (card_sylow_eq_index_normalizer P)).mp dvd_rfl).trans (index_dvd_of_le le_normalizer)
+
+/-- Frattini's Argument -/
+lemma sylow.normalizer_sup_eq_top {p : ℕ} [fact p.prime] {N : subgroup G} [N.normal]
+  [fintype (sylow p N)] (P : sylow p N) : (P.1.map N.subtype).normalizer ⊔ N = ⊤ :=
+begin
+  let ψ : G →* mul_aut N := mul_aut.conj_normal,
+  letI : mul_action G (sylow p N) := comp_hom (sylow p N) ψ,
+  refine top_le_iff.mp (λ g hg, _),
+  obtain ⟨n, hn⟩ := exists_smul_eq N (g • P) P,
+  replace hn := (mul_smul _ _ P).trans hn,
+  rw [show mul_aut.conj n = ψ n, by ext; refl, ←ψ.map_mul] at hn,
+  suffices : ↑n * g ∈ (P.1.map N.subtype).normalizer,
+  { rw ← inv_mul_cancel_left ↑n g,
+    exact mul_mem _ (inv_mem _ (mem_sup_right n.2)) (mem_sup_left this) },
+  refine λ x, iff.trans _ (set_like.ext_iff.mp (congr_arg _ (sylow.ext_iff.mp hn)) _),
+  change _ ↔ _ ∈ (P.1.map _).map _,
+  rw map_map,
+  change _ ↔ _ ∈ P.1.map ((mul_aut.conj (↑n * g)).to_monoid_hom.comp N.subtype),
+  rw ←map_map,
+  refine (mem_map_iff_mem _).symm,
+  exact (mul_aut.conj (↑n * g)).injective,
+end
 
 end infinite_sylow
 

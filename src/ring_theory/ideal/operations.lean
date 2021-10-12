abdd67271070b084d057a4e0652b45bf88d1aead
Paul-Lez <72892199+Paul-Lez@users.noreply.github.com>
2021-10-12 09:57:40 +0100

    Update src/ring_theory/ideal/operations.lean
    Co-authored-by: Eric Wieser <wieser.eric@gmail.com>

--- src/ring_theory/ideal/operations.lean
+++ src/ring_theory/ideal/operations.lean
@@ -1767,8 +1767,9 @@ def quot_quot_equiv_comm : (J.map I^.quotient.mk).quotient ≃+* (I.map J^.quoti
     (quot_quot_equiv_quot_sup J I).symm
 
 @[simp]
-lemma quot_quot_equiv_comm_quot_quot_mk : ring_hom.comp ↑(quot_quot_equiv_comm I J)
-  (quot_quot_mk I J) = quot_quot_mk J I :=
+lemma quot_quot_equiv_comm_comp_quot_quot_mk :
+  ring_hom.comp ↑(quot_quot_equiv_comm I J) (quot_quot_mk I J) = quot_quot_mk J I :=
+ring_hom.ext $ quot_quot_equiv_comm_quot_quot_mk I J
 by ext x ; simp only [ring_hom.comp_apply, quot_quot_equiv_comm, ring_equiv.trans_apply,
   quot_quot_equiv_quot_sup_quot_quot_mk, quot_equiv_of_eq_mk,
   quot_quot_equiv_quot_sup_symm_quot_quot_mk, ring_equiv.coe_to_ring_hom]

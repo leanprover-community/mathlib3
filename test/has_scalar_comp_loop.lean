6d95731bd1898329353c3bddc7f14ae9979da72f
Vierkantor <vierkantor@vierkantor.com>
2021-10-12 08:33:42 +0000

    Replace `sorry`s, so the test doesn't fail

--- test/has_scalar_comp_loop.lean
+++ test/has_scalar_comp_loop.lean
@@ -9,9 +9,11 @@ def foo [has_scalar R M] : Type* := ℕ
 variables [has_scalar R M] [has_scalar S R] [has_scalar S M]
 
 /-- This instance is incompatible with `has_scalar.comp.is_scalar_tower`.
-However, all its parameters are (instance) implicits or irreducible defs, so it should not be dangerous. -/
+However, all its parameters are (instance) implicits or irreducible defs, so it
+should not be dangerous. -/
 @[nolint unused_arguments]
-instance foo.has_scalar [is_scalar_tower S R M] : has_scalar S (foo R M) := sorry
+instance foo.has_scalar [is_scalar_tower S R M] : has_scalar S (foo R M) :=
+⟨λ _ _, by { unfold foo, exact 37 }⟩
 
 -- If there is no `is_scalar_tower S R M` parameter, this should fail quickly,
 -- not loop forever.
@@ -20,7 +22,8 @@ begin
   tactic.success_if_fail_with_msg tactic.interactive.apply_instance
     "tactic.mk_instance failed to generate instance for
   has_scalar S (foo R M)",
-  sorry
+  unfold foo,
+  exact ⟨λ _ _, 37⟩
 end
 
 /-
@@ -30,6 +33,7 @@ example : has_scalar S (foo R M) :=
 begin
   tactic.success_if_fail_with_msg tactic.interactive.apply_instance
     "maximum class-instance resolution depth has been reached (the limit can be increased by setting option 'class.instance_max_depth') (the class-instance resolution trace can be visualized by setting option 'trace.class_instances')",
-  sorry
+  unfold foo,
+  exact ⟨λ _ _, 37⟩
 end
 -/

edd632baa2ddabc1c00b3e28f23d98f9d50aebf9
Shing Tak Lam <shingtaklam1324@gmail.com>
2021-10-13 21:15:11 +0100

    add copyright

--- src/topology/category/hTop.lean
+++ src/topology/category/hTop.lean
@@ -1,3 +1,8 @@
+/-
+Copyright (c) 2021 Shing Tak Lam. All rights reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
+Authors: Shing Tak Lam
+-/
 import topology.homotopy.basic
 import category_theory.quotient
 import topology.category.Top

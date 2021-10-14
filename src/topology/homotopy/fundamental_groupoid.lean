bda8bd4e9fd8b22eae33f72cd20b702e8010e6f4
Shing Tak Lam <shingtaklam1324@gmail.com>
2021-10-14 17:58:42 +0100

    generalise inhabited instance

--- src/topology/homotopy/fundamental_groupoid.lean
+++ src/topology/homotopy/fundamental_groupoid.lean
@@ -258,7 +258,7 @@ The fundamental groupoid of a space `X` is defined to be a type synonym for `X`,
 -/
 def fundamental_groupoid (X : Type u) := X
 
-instance : inhabited (fundamental_groupoid unit) := ⟨()⟩
+instance {X : Type u} [h : inhabited X] : inhabited (fundamental_groupoid X) := h
 
 local attribute [reducible] fundamental_groupoid
 local attribute [instance] path.homotopic.setoid

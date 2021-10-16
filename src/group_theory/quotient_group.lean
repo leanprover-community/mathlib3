be77533274e7f9ffb66285da89773f8a25c0ff1b
Thomas Browning <tb65536@uw.edu>
2021-10-16 16:41:41 -0700

    add to_additive

--- src/group_theory/quotient_group.lean
+++ src/group_theory/quotient_group.lean
@@ -416,11 +416,12 @@ end third_iso_thm
 
 section trivial
 
-lemma subsingleton_quotient_top : subsingleton (quotient_group.quotient (⊤ : subgroup G)) :=
+@[to_additive] lemma subsingleton_quotient_top :
+  subsingleton (quotient_group.quotient (⊤ : subgroup G)) :=
 trunc.subsingleton
 
 /-- If the quotient by a subgroup gives a singleton then the subgroup is the whole group. -/
-lemma subgroup_eq_top_of_subsingleton (H : subgroup G)
+@[to_additive] lemma subgroup_eq_top_of_subsingleton (H : subgroup G)
   (h : subsingleton (quotient_group.quotient H)) : H = ⊤ :=
 top_unique $ λ x _,
   have this : 1⁻¹ * x ∈ H := quotient_group.eq.1 (subsingleton.elim _ _),

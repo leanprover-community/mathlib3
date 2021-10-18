27974fc8c171cdb586eee140cf42c0330ff2b7f9
Riccardo Brasca <riccardo.brasca@gmail.com>
2021-10-18 13:43:32 +0200

    fix style

--- src/analysis/normed/group/SemiNormedGroup/completion.lean
+++ src/analysis/normed/group/SemiNormedGroup/completion.lean
@@ -1,7 +1,7 @@
 /-
 Copyright (c) 2021 Riccardo Brasca. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
-Authors: Riccardo Brasca, Johan Commelin.
+Authors: Riccardo Brasca, Johan Commelin
 -/
 
 import analysis.normed.group.SemiNormedGroup

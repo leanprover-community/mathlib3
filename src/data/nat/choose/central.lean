24e9e02220d8f8cb6a37cbeb772c880135f494ce
Patrick Stevens <patrick+github@patrickstevens.co.uk>
2021-10-19 19:35:37 +0100

    Apply suggestions from code review
    Co-authored-by: tb65536 <tb65536@users.noreply.github.com>

--- src/data/nat/choose/central.lean
+++ src/data/nat/choose/central.lean
@@ -21,41 +21,34 @@ lemma central_binom_ne_zero (n : ℕ) : (2 * n).choose n ≠ 0 :=
 (choose_pos (nat.le_mul_of_pos_left zero_lt_two)).ne'
 
 lemma choose_le_central_binom (r n : ℕ) : choose (2 * n) r ≤ choose (2 * n) n :=
-begin
-  calc (2 * n).choose r ≤ (2 * n).choose (2 * n / 2) : choose_le_middle r (2 * n)
-  ... = (2 * n).choose n : by rw nat.mul_div_cancel_left n zero_lt_two
-end
+calc (2 * n).choose r ≤ (2 * n).choose (2 * n / 2) : choose_le_middle r (2 * n)
+... = (2 * n).choose n : by rw nat.mul_div_cancel_left n zero_lt_two
 
 lemma two_le_central_binom (n : ℕ) (n_pos : 0 < n) : 2 ≤ (2 * n).choose n :=
-begin
-  calc 2 ≤ (2 * n).choose 1 :
-    begin
-      simp only [choose_one_right],
-      exact le_mul_of_pos_right n_pos,
-    end
-   ... ≤ (2 * n).choose n : choose_le_central_binom 1 n
-end
+calc 2 ≤ 2 * n : le_mul_of_pos_right n_pos
+... = (2 * n).choose 1 : (choose_one_right (2 * n)).symm
+... ≤ (2 * n).choose n : choose_le_central_binom 1 n
 
 lemma central_binom_induction (n : ℕ) :
   (n + 1) * ((2 * (n + 1)).choose (n + 1)) = (2 * (2 * n + 1)) * (2 * n).choose n :=
-calc (n + 1) * (2 * (n + 1)).choose (n + 1) = (2 * n + 2).choose (n + 1) * (n + 1) : by ring
+calc (n + 1) * (2 * (n + 1)).choose (n + 1) = (2 * n + 2).choose (n + 1) * (n + 1) : mul_comm _ _
 ... = (2 * n + 1).choose n * (2 * n + 2) : by rw [choose_succ_right_eq, choose_mul_succ_eq]
 ... = 2 * ((2 * n + 1).choose n * (n + 1)) : by ring
 ... = 2 * ((2 * n + 1).choose n * ((2 * n + 1) - n)) :
   by rw [two_mul n, add_assoc, nat.add_sub_cancel_left]
 ... = 2 * ((2 * n).choose n * (2 * n + 1)) : by rw choose_mul_succ_eq
-... = (2 * (2 * n + 1)) * (2 * n).choose n : by ring
+... = (2 * (2 * n + 1)) * (2 * n).choose n : by rw [mul_assoc, mul_comm (2 * n + 1)]
 
 -- This bound is of interest because it appears in Tochiori's refinement of Erdős's proof
 -- of Bertrand's postulate
 -- (https://en.wikipedia.org/w/index.php?title=Proof_of_Bertrand%27s_postulate&oldid=859165151#Proof_by_Shigenori_Tochiori)
 lemma four_pow_n_lt_n_mul_central_binom : ∀ (n : ℕ) (n_big : 4 ≤ n), 4 ^ n < n * (2 * n).choose n
-| 0 pr := by linarith
-| 1 pr := by linarith
-| 2 pr := by linarith
-| 3 pr := by linarith
-| 4 _ := by { norm_num, unfold nat.choose, norm_num }
-| (m + 5) _ :=
+| 0 n_big := by norm_num at n_big
+| 1 n_big := by norm_num at n_big
+| 2 n_big := by norm_num at n_big
+| 3 n_big := by norm_num at n_big
+| 4 n_big := by { norm_num, unfold nat.choose, norm_num }
+| (m + 5) n_big :=
 calc 4 ^ (m + 5) < 4 * ((m + 4) * (2 * (m + 4)).choose (m + 4)) :
   (mul_lt_mul_left zero_lt_four).mpr (four_pow_n_lt_n_mul_central_binom (m + 4) le_add_self)
 ... = (4 * (m + 4)) * (2 * (m + 4)).choose (m + 4) : (mul_assoc _ _ _).symm

a800fae7d3b0b049cf4eef6d44b8ab44aa67a42f
Yakov Pechersky <ffxen158@gmail.com>
2021-10-19 00:32:35 -0400

    fix instance naming

--- src/algebra/order/monoid.lean
+++ src/algebra/order/monoid.lean
@@ -627,7 +627,7 @@ section canonically_linear_ordered_monoid
 variables [canonically_linear_ordered_monoid α]
 
 @[priority 100, to_additive]  -- see Note [lower instance priority]
-instance canonically_linear_ordered_monoid.semilattice_sup_bot : semilattice_sup α :=
+instance canonically_linear_ordered_monoid.semilattice_sup : semilattice_sup α :=
 { ..lattice_of_linear_order }
 
 instance with_top.canonically_linear_ordered_add_monoid

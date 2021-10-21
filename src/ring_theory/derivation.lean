c376df62b2d30e2d73110c015d3c348f9fc32733
Yury G. Kudryashov <urkud@urkud.name>
2021-10-21 10:28:24 -0400

    Update src/ring_theory/derivation.lean

--- src/ring_theory/derivation.lean
+++ src/ring_theory/derivation.lean
@@ -69,9 +69,6 @@ lemma coe_injective : @function.injective (derivation R A M) (A → M) coe_fn :=
 @[ext] theorem ext (H : ∀ a, D1 a = D2 a) : D1 = D2 :=
 coe_injective $ funext H
 
-lemma to_linear_map_injective (h : (D1 : A →ₗ[R] M) = D2) : D1 = D2 :=
-ext $ linear_map.congr_fun h
-
 lemma congr_fun (h : D1 = D2) (a : A) : D1 a = D2 a := congr_fun (congr_arg coe_fn h) a
 
 @[simp] lemma map_add : D (a + b) = D a + D b := linear_map.map_add D a b

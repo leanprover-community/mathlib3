e2c09c48324f21c68d06e18004031debcc96182d
Riccardo Brasca <riccardo.brasca@gmail.com>
2021-10-21 21:45:05 +0200

    fix build

--- src/linear_algebra/dual.lean
+++ src/linear_algebra/dual.lean
@@ -285,10 +285,16 @@ by { classical, exact (basis.of_vector_space K V).eval_ker }
 -- TODO(jmc): generalize to rings, once `module.rank` is generalized
 theorem dual_dim_eq [finite_dimensional K V] :
   cardinal.lift (module.rank K V) = module.rank K (dual K V) :=
-(basis.of_vector_space K V).dual_dim_eq
+begin
+  letI : is_noetherian K V := is_noetherian.iff_fg.2 infer_instance,
+  exact (basis.of_vector_space K V).dual_dim_eq,
+end
 
 lemma erange_coe [finite_dimensional K V] : (eval K V).range = ⊤ :=
-by { classical, exact (basis.of_vector_space K V).eval_range }
+begin
+  letI : is_noetherian K V := is_noetherian.iff_fg.2 infer_instance,
+  exact (basis.of_vector_space K V).eval_range
+end
 
 variables (K V)
 
@@ -537,13 +543,19 @@ open finite_dimensional
 variables {V₁ : Type*} [add_comm_group V₁] [module K V₁]
 
 instance [H : finite_dimensional K V] : finite_dimensional K (module.dual K V) :=
-linear_equiv.finite_dimensional (basis.of_vector_space K V).to_dual_equiv
+begin
+  letI : is_noetherian K V := is_noetherian.iff_fg.2 infer_instance,
+  exact linear_equiv.finite_dimensional (basis.of_vector_space K V).to_dual_equiv
+end
 
 variables [finite_dimensional K V] [finite_dimensional K V₁]
 
 @[simp] lemma dual_finrank_eq :
   finrank K (module.dual K V) = finrank K V :=
-linear_equiv.finrank_eq (basis.of_vector_space K V).to_dual_equiv.symm
+begin
+  letI : is_noetherian K V := is_noetherian.iff_fg.2 infer_instance,
+  exact linear_equiv.finrank_eq (basis.of_vector_space K V).to_dual_equiv.symm
+end
 
 /-- The quotient by the dual is isomorphic to its dual annihilator.  -/
 noncomputable def quot_dual_equiv_annihilator (W : subspace K V) :
@@ -555,6 +567,7 @@ linear_equiv.quot_equiv_of_quot_equiv $
 noncomputable def quot_equiv_annihilator (W : subspace K V) :
   W.quotient ≃ₗ[K] W.dual_annihilator :=
 begin
+  letI : is_noetherian K V := is_noetherian.iff_fg.2 infer_instance,
   refine _ ≪≫ₗ W.quot_dual_equiv_annihilator,
   refine linear_equiv.quot_equiv_of_equiv _ (basis.of_vector_space K V).to_dual_equiv,
   exact (basis.of_vector_space K W).to_dual_equiv.trans W.dual_equiv_dual

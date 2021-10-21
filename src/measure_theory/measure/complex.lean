8bf5c1b8e3ab17e3be4c35f4053d17c28b7bdd23
Kexing <kexing.ying19@imperial.ac.uk>
2021-10-21 20:46:38 +0100

    remove merged code

--- src/measure_theory/measure/complex.lean
+++ src/measure_theory/measure/complex.lean
@@ -31,49 +31,6 @@ Complex measure
 noncomputable theory
 open_locale classical measure_theory ennreal nnreal
 
-section
-
-variables {α β γ : Type*}
-  [add_comm_monoid α] [topological_space α]
-  [add_comm_monoid γ] [topological_space γ]
-
-lemma has_sum.prod_mk {f : β → α} {g : β → γ} {a : α} {b : γ}
-  (hf : has_sum f a) (hg : has_sum g b) :
-  has_sum (λ x, (⟨f x, g x⟩ : α × γ)) ⟨a, b⟩ :=
-by simp [has_sum, ← prod_mk_sum, filter.tendsto.prod_mk_nhds hf hg]
-
-def complex.equiv_real_prod_add_hom : ℂ ≃+ ℝ × ℝ :=
-{ map_add' := by simp, .. complex.equiv_real_prod }
-
-def complex.equiv_real_prod_add_hom_lm : ℂ ≃ₗ[ℝ] ℝ × ℝ :=
-{ map_smul' := by simp [complex.equiv_real_prod_add_hom], .. complex.equiv_real_prod_add_hom }
-
-def complex.equiv_real_prodₗ : ℂ ≃L[ℝ] ℝ × ℝ :=
-{ map_smul' := by simp [complex.equiv_real_prod_add_hom],
-  continuous_to_fun := complex.continuous_re.prod_mk complex.continuous_im,
-  continuous_inv_fun :=
-  begin
-    refine linear_map.continuous_of_bound
-      (complex.equiv_real_prod_add_hom_lm.symm.to_linear_map) 2 _,
-    rintro ⟨a, b⟩,
-    rw two_mul,
-    exact le_trans (complex.abs_le_abs_re_add_abs_im _)
-      (add_le_add (le_max_left _ _) (le_max_right _ _)),
-  end, .. complex.equiv_real_prod_add_hom }
-
-lemma complex.has_sum_iff (f : α → ℂ) (c : ℂ) :
-  has_sum f c ↔ has_sum (λ x, (f x).re) c.re ∧ has_sum (λ x, (f x).im) c.im :=
-begin
-  split,
-  { exact λ h, ⟨h.mapL complex.re_clm, h.mapL complex.im_clm⟩ },
-  { rintro ⟨h₁, h₂⟩,
-    convert (has_sum.prod_mk h₁ h₂).mapL complex.equiv_real_prodₗ.symm.to_continuous_linear_map,
-    { ext x; refl },
-    { cases c, refl } }
-end
-
-end
-
 variables {α β : Type*} {m : measurable_space α}
 
 namespace measure_theory

b449f5c96c81123e71c16e22a06b2d06e01ff547
Eric Wieser <wieser.eric@gmail.com>
2021-10-21 12:20:29 +0100

    fix and generalize

--- src/ring_theory/integral_domain.lean
+++ src/ring_theory/integral_domain.lean
@@ -53,8 +53,9 @@ begin
   convert (le_trans (card_nth_roots_subgroup_units f hf hn 1) (card_nth_roots n (f 1)))
 end
 
-/-- The unit group of a finite integral domain is cyclic. -/
-instance [fintype R] : is_cyclic (units R) :=
+/-- The unit group of a finite integral domain is cyclic. To support `units ℤ`, this requires
+`fintype (units R)` rather than deducing it from `fintype R`. -/
+instance [fintype (units R)] : is_cyclic (units R) :=
 is_cyclic_of_subgroup_integral_domain (units.coe_hom R) $ units.ext
 
 /-- Every finite integral domain is a field. -/

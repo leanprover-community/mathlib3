a696d0d1b5781e33d018b1fad7b400b51009ba82
YaelDillies <yael.dillies@gmail.com>
2021-10-23 17:23:22 +0100

    import swap

--- src/data/dlist/instances.lean
+++ src/data/dlist/instances.lean
@@ -3,9 +3,9 @@ Copyright (c) 2018 Simon Hudon. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Simon Hudon
 -/
-import data.dlist
 import control.traversable.equiv
 import control.traversable.instances
+import data.dlist
 
 /-!
 # Traversable instance for dlists

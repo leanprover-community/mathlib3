48985ac5a81c932b40be7a2b2cebb36681438853
dtumad <tumax040@umn.edu>
2021-10-23 15:28:24 -0500

    Add zip_with to lemma names

--- src/data/list/zip.lean
+++ src/data/list/zip.lean
@@ -386,20 +386,20 @@ section comm_monoid
 variables [comm_monoid α]
 
 @[to_additive]
-lemma prod_mul_prod : ∀ (L L' : list α), L.prod * L'.prod =
+lemma prod_mul_prod_eq_prod_zip_with_mul_prod_drop : ∀ (L L' : list α), L.prod * L'.prod =
   (zip_with (*) L L').prod * (L.drop L'.length).prod * (L'.drop L.length).prod
 | [] ys := by simp
 | xs [] := by simp
 | (x :: xs) (y :: ys) := begin
   simp only [drop, length, zip_with_cons_cons, prod_cons],
   rw [mul_assoc x, mul_comm xs.prod, mul_assoc y, mul_comm ys.prod,
-    prod_mul_prod xs ys, mul_assoc, mul_assoc, mul_assoc, mul_assoc]
+    prod_mul_prod_eq_prod_zip_with_mul_prod_drop xs ys, mul_assoc, mul_assoc, mul_assoc, mul_assoc]
 end
 
 @[to_additive]
-lemma prod_mul_prod_of_length_eq (L L' : list α) (h : L.length = L'.length) :
+lemma prod_mul_prod_eq_prod_zip_with_of_length_eq (L L' : list α) (h : L.length = L'.length) :
   L.prod * L'.prod = (zip_with (*) L L').prod :=
-(prod_mul_prod L L').trans (by simp [h])
+(prod_mul_prod_eq_prod_zip_with_mul_prod_drop L L').trans (by simp [h])
 
 end comm_monoid
 

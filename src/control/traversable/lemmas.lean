59d27db1a22436d32d9b9f816cd2ef2c6c784e6a
YaelDillies <yael.dillies@gmail.com>
2021-10-24 13:05:34 +0100

    imports

--- src/control/traversable/lemmas.lean
+++ src/control/traversable/lemmas.lean
@@ -3,8 +3,8 @@ Copyright (c) 2018 Simon Hudon. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Simon Hudon
 -/
-import control.traversable.basic
 import control.applicative
+import control.traversable.basic
 
 /-!
 # Traversing collections

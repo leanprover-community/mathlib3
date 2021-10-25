1eaa2ccde1b50754b0f7e8d215a12654d7b11e1c
Riccardo Brasca <riccardo.brasca@gmail.com>
2021-10-25 20:35:39 +0200

    this is now useless

--- src/linear_algebra/free_module/pid.lean
+++ src/linear_algebra/free_module/pid.lean
@@ -3,7 +3,7 @@ Copyright (c) 2020 Anne Baanen. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Anne Baanen
 -/
-import linear_algebra.free_module.strong_rank_condition
+
 import linear_algebra.finsupp_vector_space
 import ring_theory.principal_ideal_domain
 import ring_theory.finiteness

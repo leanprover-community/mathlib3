45078f3a093def5c9688b125f0bdacf0d5c837ce
Yury G. Kudryashov <urkud@urkud.name>
2021-10-24 20:31:41 -0400

    Fix

--- src/group_theory/subgroup/basic.lean
+++ src/group_theory/subgroup/basic.lean
@@ -3,10 +3,11 @@ Copyright (c) 2020 Kexing Ying. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Kexing Ying
 -/
-import group_theory.submonoid
+import group_theory.submonoid.pointwise
 import group_theory.submonoid.center
 import algebra.group.conj
 import order.atoms
+import algebra.pointwise
 
 /-!
 # Subgroups

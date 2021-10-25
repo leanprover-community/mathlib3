baf3f8384171b625c45bf0f6c368bb98e3c66e04
Riccardo Brasca <riccardo.brasca@gmail.com>
2021-10-25 18:43:32 +0200

    use finrank instead of rank

--- src/linear_algebra/free_module/finite/rank.lean
+++ src/linear_algebra/free_module/finite/rank.lean
@@ -21,7 +21,7 @@ variables (R : Type u) (M : Type v) (N : Type w)
 
 open_locale tensor_product direct_sum big_operators cardinal
 
-open cardinal
+open cardinal finite_dimensional
 
 namespace module.free
 
@@ -31,10 +31,10 @@ variables [ring R] [strong_rank_condition R]
 variables [add_comm_group M] [module R M] [module.free R M] [module.finite R M]
 
 /-- If `M` is finite and free, `finrank M = rank M`. -/
-lemma finrank_eq_rank : ↑(finite_dimensional.finrank R M) = module.rank R M :=
+lemma finrank_eq_rank : ↑(finrank R M) = module.rank R M :=
 begin
   letI := nontrivial_of_invariant_basis_number R,
-  simp [finite_dimensional.finrank, rank_eq_card_choose_basis_index, fintype_card],
+  simp [finrank, rank_eq_card_choose_basis_index, fintype_card],
 end
 
 end ring
@@ -45,24 +45,17 @@ variables [comm_ring R] [strong_rank_condition R]
 variables [add_comm_group M] [module R M] [module.free R M] [module.finite R M]
 variables [add_comm_group N] [module R N] [module.free R N] [module.finite R N]
 
-/-- The rank of `M →ₗ[R] N` is `(module.rank R M).lift * (module.rank R N).lift`. -/
-@[simp] lemma rank_linear_hom : module.rank R (M →ₗ[R] N) = lift.{w v} (module.rank R M) *
-  lift.{v w} (module.rank R N) :=
+/-- The finrank of `M →ₗ[R] N` is `(finrank R M) * (finrank R N)`. -/
+@[simp] lemma finrank_linear_hom : finrank R (M →ₗ[R] N) = (finrank R M) * (finrank R N) :=
 begin
   classical,
   letI := nontrivial_of_invariant_basis_number R,
-  have h := (linear_map.to_matrix (choose_basis R M) (choose_basis R N)).lift_dim_eq,
-  simp only [rank_matrix, lift_lift, lift_mul] at h,
-  rw [← lift_lift.{(max w v) (max (max w v u) v w) w},
-    ← lift_lift.{(max v w) (max (max w v u) v w) v}, ← lift_mul, lift_inj] at h,
-  simpa [rank_eq_card_choose_basis_index, lift_umax, mul_comm] using h
+  have h := (linear_map.to_matrix (choose_basis R M) (choose_basis R N)),
+  let b := (matrix.std_basis _ _ _).map h.symm,
+  rw [finrank, dim_eq_card_basis b, ← fintype_card, mk_to_nat_eq_card, finrank, finrank,
+    rank_eq_card_choose_basis_index, rank_eq_card_choose_basis_index, mk_to_nat_eq_card,
+    mk_to_nat_eq_card, fintype.card_prod, mul_comm]
 end
-
-/-- If `M` and `N` lie in the same universe, the rank of `M →ₗ[R] N` is
-  `(module.rank R M) * (module.rank R N)`. -/
-@[simp] lemma rank_linear_hom' (N : Type v) [add_comm_group N] [module R N] [module.free R N]
-  [module.finite R N] : module.rank R (M →ₗ[R] N) = (module.rank R M) * (module.rank R N) :=
-by simp
 
 end comm_ring
 

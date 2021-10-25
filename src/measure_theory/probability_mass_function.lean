4aab92b67b443d7be532eaf4e9bdc952bf43a064
dtumad <tumax040@umn.edu>
2021-10-25 11:13:51 -0500

    Define of_finset and change definitions to be in terms of that

--- src/measure_theory/probability_mass_function.lean
+++ src/measure_theory/probability_mass_function.lean
@@ -260,7 +260,31 @@ end map
 /-- The monadic sequencing operation for `pmf`. -/
 def seq (f : pmf (α → β)) (p : pmf α) : pmf β := f.bind (λ m, p.bind $ λ a, pure (m a))
 
-section uniform
+section of_finite
+
+/-- Given a finset `s` and a function `f : α → ℝ≥0` with sum `1` on `s`,
+  such that `f x = 0` for `x ∉ s`, we get a `pmf` -/
+def of_finset (f : α → ℝ≥0) (s : finset α) (h : ∑ x in s, f x = 1)
+  (h' : ∀ x ∉ s, f x = 0) : pmf α :=
+⟨f, h ▸ has_sum_sum_of_ne_finset_zero h'⟩
+
+@[simp]
+lemma of_finset_apply (f : α → ℝ≥0) (s : finset α) (h : ∑ x in s, f x = 1)
+  (h' : ∀ x ∉ s, f x = 0) (a : α) : of_finset f s h h' a = f a :=
+rfl
+
+lemma of_finset_apply_of_not_mem (f : α → ℝ≥0) (s : finset α) (h : ∑ x in s, f x = 1)
+  (h' : ∀ x ∉ s, f x = 0) (a : α) (ha : a ∉ s) : of_finset f s h h' a = 0 :=
+h' a ha
+
+/-- Given a finite type `α` and a function `f : α → ℝ≥0` with sum 1, we get a `pmf`. -/
+def of_fintype [fintype α] (f : α → ℝ≥0) (h : ∑ x, f x = 1) : pmf α :=
+of_finset f finset.univ h (λ x hx, absurd (finset.mem_univ x) hx)
+
+@[simp]
+lemma of_fintype_apply [fintype α] (f : α → ℝ≥0) (h : ∑ x, f x = 1)
+  (a : α) : of_fintype f h a = f a :=
+rfl
 
 /-- Given a non-empty multiset `s` we construct the `pmf` which sends `a` to the fraction of
   elements in `s` that are `a`. -/
@@ -276,42 +300,50 @@ def of_multiset (s : multiset α) (hs : s ≠ 0) : pmf α :=
     simp {contextual := tt},
   end⟩
 
-/-- Uniform distribution taking the same non-zero probability on the nonempty finset `s` -/
-def of_finset (s : finset α) (hs : s.nonempty) : pmf α :=
-⟨λ a, if a ∈ s then (s.card : ℝ≥0)⁻¹ else 0,
-  begin
-    obtain ⟨x, hx⟩ := hs,
-    have : ∀ (b : α), b ∉ s → ite (b ∈ s) (s.card : ℝ≥0)⁻¹ 0 = 0 := λ b hb, by simp [hb],
-    convert has_sum_sum_of_ne_finset_zero this,
-    refine symm _,
-    calc ∑ (a : α) in s, ite (a ∈ s) (s.card : ℝ≥0)⁻¹ 0
-      = ∑ (a : α) in s, (s.card : ℝ≥0)⁻¹ : finset.sum_congr rfl (λ x hx, by simp [hx])
-      ... = s.card • (s.card : ℝ≥0)⁻¹ : finset.sum_const _
-      ... = (s.card : ℝ≥0) * (s.card : ℝ≥0)⁻¹ : by rw nsmul_eq_mul
-      ... = 1 : div_self (nat.cast_ne_zero.2 $ finset.card_ne_zero_of_mem hx)
-  end⟩
-
 @[simp]
-lemma of_finset_apply (s : finset α) (hs : s.nonempty) (a : α) :
-  of_finset s hs a = if a ∈ s then (s.card : ℝ≥0)⁻¹ else 0 :=
+lemma of_multiset_apply (s : multiset α) (hs : s ≠ 0) (a : α) :
+  of_multiset s hs a = s.count a / s.card :=
 rfl
 
-lemma of_finset_apply_of_mem {s : finset α} (hs : s.nonempty) (a : α) (ha : a ∈ s) :
-  of_finset s hs a = (s.card)⁻¹ :=
+lemma of_multiset_apply_of_not_mem (s : multiset α) (hs : s ≠ 0)
+  (a : α) (ha : a ∉ s) : of_multiset s hs a = 0 :=
+div_eq_zero_iff.2 (or.inl $ nat.cast_eq_zero.2 $ multiset.count_eq_zero_of_not_mem ha)
+
+end of_finite
+
+section uniform
+
+/-- Uniform distribution taking the same non-zero probability on the nonempty finset `s` -/
+def uniform_of_finset (s : finset α) (hs : s.nonempty) : pmf α :=
+of_finset (λ a, if a ∈ s then (s.card : ℝ≥0)⁻¹ else 0) s (Exists.rec_on hs (λ x hx,
+  calc ∑ (a : α) in s, ite (a ∈ s) (s.card : ℝ≥0)⁻¹ 0
+    = ∑ (a : α) in s, (s.card : ℝ≥0)⁻¹ : finset.sum_congr rfl (λ x hx, by simp [hx])
+    ... = s.card • (s.card : ℝ≥0)⁻¹ : finset.sum_const _
+    ... = (s.card : ℝ≥0) * (s.card : ℝ≥0)⁻¹ : by rw nsmul_eq_mul
+    ... = 1 : div_self (nat.cast_ne_zero.2 $ finset.card_ne_zero_of_mem hx)
+  )) (λ x hx, by simp only [hx, if_false])
+
+@[simp]
+lemma uniform_of_finset_apply (s : finset α) (hs : s.nonempty) (a : α) :
+  uniform_of_finset s hs a = if a ∈ s then (s.card : ℝ≥0)⁻¹ else 0 :=
+rfl
+
+lemma uniform_of_finset_apply_of_mem {s : finset α} (hs : s.nonempty) (a : α) (ha : a ∈ s) :
+  uniform_of_finset s hs a = (s.card)⁻¹ :=
 by simp [ha]
 
-lemma of_finset_apply_of_not_mem {s : finset α} (hs : s.nonempty) (a : α) (ha : a ∉ s) :
-  of_finset s hs a = 0 :=
+lemma uniform_of_finset_apply_of_not_mem {s : finset α} (hs : s.nonempty) (a : α) (ha : a ∉ s) :
+  uniform_of_finset s hs a = 0 :=
 by simp [ha]
 
 /-- The uniform pmf taking the same uniform value on all of the fintype `α` -/
-def uniform (α : Type*) [fintype α] [inhabited α] : pmf α :=
-  of_finset (finset.univ) (finset.univ_nonempty)
+def uniform_of_fintype (α : Type*) [fintype α] [nonempty α] : pmf α :=
+  uniform_of_finset (finset.univ) (finset.univ_nonempty)
 
 @[simp]
-lemma uniform_apply [fintype α] [inhabited α] (a : α) :
-  uniform α a = (fintype.card α)⁻¹ :=
-by simpa only [uniform, finset.mem_univ, if_true, of_finset_apply]
+lemma uniform_of_fintype_apply [fintype α] [nonempty α] (a : α) :
+  uniform_of_fintype α a = (fintype.card α)⁻¹ :=
+by simpa only [uniform_of_fintype, finset.mem_univ, if_true, uniform_of_finset_apply]
 
 end uniform
 
@@ -357,10 +389,6 @@ by rw [← not_iff, filter_apply_eq_zero_iff, not_iff, not_not]
 end filter
 
 section bernoulli
-
-/-- Given a finite type `α` and a function `f : α → ℝ≥0` with sum 1, we get a `pmf`. -/
-def of_fintype [fintype α] (f : α → ℝ≥0) (h : ∑ x, f x = 1) : pmf α :=
-⟨f, h ▸ has_sum_sum_of_ne_finset_zero (by simp)⟩
 
 /-- A `pmf` which assigns probability `p` to `tt` and `1 - p` to `ff`. -/
 def bernoulli (p : ℝ≥0) (h : p ≤ 1) : pmf bool :=

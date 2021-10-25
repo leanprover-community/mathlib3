2026a5f7b63f51fe50c032dbb2811fcb502c9503
Yury G. Kudryashov <urkud@urkud.name>
2021-10-25 17:43:00 +0000

    feat(measure_theory/measure): better `measure.restrict_singleton` (#9936)
    With new `restrict_singleton`, `simp` can simplify `∫ x in {a}, f x ∂μ`
    to `(μ {a}).to_real • f a`.

--- src/measure_theory/measure/measure_space.lean
+++ src/measure_theory/measure/measure_space.lean
@@ -1117,6 +1117,16 @@ lemma map_dirac {f : α → β} (hf : measurable f) (a : α) :
   map f (dirac a) = dirac (f a) :=
 ext $ assume s hs, by simp [hs, map_apply hf hs, hf hs, indicator_apply]
 
+@[simp] lemma restrict_singleton (μ : measure α) (a : α) : μ.restrict {a} = μ {a} • dirac a :=
+begin
+  ext1 s hs,
+  by_cases ha : a ∈ s,
+  { have : s ∩ {a} = {a}, by simpa,
+    simp * },
+  { have : s ∩ {a} = ∅, from inter_singleton_eq_empty.2 ha,
+    simp * }
+end
+
 end dirac
 
 section sum
@@ -1761,7 +1771,7 @@ lemma _root_.set.subsingleton.measure_zero {α : Type*} {m : measurable_space α
   μ s = 0 :=
 hs.induction_on measure_empty measure_singleton
 
-@[simp] lemma measure.restrict_singleton' {a : α} :
+lemma measure.restrict_singleton' {a : α} :
   μ.restrict {a} = 0 :=
 by simp only [measure_singleton, measure.restrict_eq_zero]
 

ed5db6022ba5d7b0842d47b4a03b03adde5d8028
YaelDillies <yael.dillies@gmail.com>
2021-10-26 17:38:37 +0100

    fix zorn

--- src/order/zorn.lean
+++ src/order/zorn.lean
@@ -3,7 +3,7 @@ Copyright (c) 2017 Johannes Hölzl. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Johannes Hölzl
 -/
-import data.set.lattice
+import data.set.pairwise
 
 /-!
 # Chains and Zorn's lemmas

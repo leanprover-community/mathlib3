4dbddbe3584f766debedbdf9de61afb9e325a7f4
Chris Hughes <chrishughes24@gmail.com>
2021-10-26 14:06:53 +0200

    undo conj_act changes

--- src/group_theory/group_action/conj_act.lean
+++ src/group_theory/group_action/conj_act.lean
@@ -13,6 +13,10 @@ the definition of conjugation as a homomorphism into the automorphism group.
 
 ## Main definitions
 
+A type alias `conj_act G` is introduced for a group `G`. The group `conj_act G` acts on `G`
+by conjugation. The group `conj_act G` also acts on any normal subgroup of `G` by conjugation.
+
+## Implementation Notes
 
 The scalar action in defined in this file can also be written using `mul_aut.conj g • h`. This
 has the advantage of not using the type alias `conj_act`, but the downside of this approach
@@ -23,6 +27,7 @@ is that some theorems about the group actions will not apply when since this
 
 variables (G : Type*)
 
+/-- A type alias for a group `G`. `conj_act G` acts on `G` by conjugation -/
 def conj_act : Type* := G
 
 namespace conj_act
@@ -56,20 +61,24 @@ protected def rec {C : conj_act G → Sort*} (h : Π g, C (to_conj_act g)) : Π 
 @[simp] lemma to_mul_symm_eq : (@to_conj_act G _).symm = of_conj_act := rfl
 @[simp] lemma to_conj_act_of_conj_act (x : conj_act G) : to_conj_act (of_conj_act x) = x := rfl
 @[simp] lemma of_conj_act_to_conj_act (x : G) : of_conj_act (to_conj_act x) = x := rfl
+@[simp] lemma of_conj_act_one : of_conj_act (1 : conj_act G) = 1 := rfl
+@[simp] lemma to_conj_act_one : to_conj_act (1 : G) = 1 := rfl
 @[simp] lemma of_conj_act_inv (x : conj_act G) : of_conj_act (x⁻¹) = (of_conj_act x)⁻¹ := rfl
 @[simp] lemma to_conj_act_inv (x : G) : to_conj_act (x⁻¹) = (to_conj_act x)⁻¹ := rfl
 @[simp] lemma of_conj_act_mul (x y : conj_act G) :
+  of_conj_act (x * y) = of_conj_act x * of_conj_act y := rfl
 @[simp] lemma to_conj_act_mul (x y : G) : to_conj_act (x * y) =
   to_conj_act x * to_conj_act y := rfl
 
-instance : mul_distrib_mul_action (conj_act G) G :=
-{ smul := λ g h, of_conj_act g * h * (of_conj_act g)⁻¹,
-  smul_mul := by simp [mul_assoc],
-  smul_one := by simp,
-  one_smul := by simp,
-  mul_smul := by simp [mul_assoc] }
+instance : has_scalar (conj_act G) G :=
+{ smul := λ g h, of_conj_act g * h * (of_conj_act g)⁻¹ }
 
 lemma smul_def (g : conj_act G) (h : G) : g • h = of_conj_act g * h * (of_conj_act g)⁻¹ := rfl
+
+@[simp] lemma «forall» (p : conj_act G → Prop) :
+  (∀ (x : conj_act G), p x) ↔ ∀ x : G, p (to_conj_act x) := iff.rfl
+
+end div_inv_monoid
 
 section group_with_zero
 
@@ -102,5 +111,37 @@ begin
   ext x,
   simp [mem_center_iff, smul_def, mul_inv_eq_iff_eq_mul]
 end
+
+/-- As normal subgroups are closed under conjugation, they inherit the conjugation action
+  of the underlying group. -/
+instance subgroup.conj_action {H : subgroup G} [hH : H.normal] :
+  has_scalar (conj_act G) H :=
+⟨λ g h, ⟨g • h, hH.conj_mem h.1 h.2 (of_conj_act g)⟩⟩
+
+lemma subgroup.coe_conj_smul {H : subgroup G} [hH : H.normal] (g : conj_act G) (h : H) :
+  ↑(g • h) = g • (h : G) := rfl
+
+instance subgroup.conj_mul_distrib_mul_action {H : subgroup G} [hH : H.normal] :
+  mul_distrib_mul_action (conj_act G) H :=
+(subtype.coe_injective).mul_distrib_mul_action H.subtype subgroup.coe_conj_smul
+
+/-- Group conjugation on a normal subgroup. Analogous to `mul_aut.conj`. -/
+def _root_.mul_aut.conj_normal {H : subgroup G} [hH : H.normal] : G →* mul_aut H :=
+(mul_distrib_mul_action.to_mul_aut (conj_act G) H).comp to_conj_act.to_monoid_hom
+
+@[simp] lemma _root_.mul_aut.conj_normal_apply {H : subgroup G} [H.normal] (g : G) (h : H) :
+  ↑(mul_aut.conj_normal g h) = g * h * g⁻¹ := rfl
+
+@[simp] lemma _root_.mul_aut.conj_normal_symm_apply {H : subgroup G} [H.normal] (g : G) (h : H) :
+  ↑((mul_aut.conj_normal g).symm h) = g⁻¹ * h * g :=
+by { change _ * (_)⁻¹⁻¹ = _, rw inv_inv, refl }
+
+@[simp] lemma _root_.mul_aut.conj_normal_inv_apply {H : subgroup G} [H.normal] (g : G) (h : H) :
+  ↑((mul_aut.conj_normal g)⁻¹ h) = g⁻¹ * h * g :=
+mul_aut.conj_normal_symm_apply g h
+
+lemma _root_.mul_aut.conj_normal_coe {H : subgroup G} [H.normal] {h : H} :
+  mul_aut.conj_normal ↑h = mul_aut.conj h :=
+mul_equiv.ext (λ x, rfl)
 
 end conj_act

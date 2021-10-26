ce164e28f262acbb3de6281b3b03660a9f744e3c
Yaël Dillies <yael.dillies@gmail.com>
2021-10-26 17:54:26 +0000

    chore(data/{finset,multiset}/locally_finite): rename from `.interval` (#9980)
    The pattern is `data.stuff.interval` for files about `locally_finite_order stuff` and... `finset α` and `multiset α` are locally finite orders. This thus makes space for this theory.

--- src/data/multiset/locally_finite.lean
+++ src/data/multiset/locally_finite.lean
@@ -3,7 +3,7 @@ Copyright (c) 2021 Yaël Dillies. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Yaël Dillies
 -/
-import data.finset.interval
+import data.finset.locally_finite
 
 /-!
 # Intervals as multisets

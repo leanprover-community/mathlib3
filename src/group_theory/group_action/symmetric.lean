1ded83d1789cfb17260ae3ebaee972a5df97ddf3
Jakob von Raumer <jakob@von-raumer.de>
2021-10-31 18:25:18 +0100

    oops, change author

--- src/group_theory/group_action/symmetric.lean
+++ src/group_theory/group_action/symmetric.lean
@@ -1,5 +1,5 @@
 /-
-Copyright (c) 2018 Chris Hughes. All rights reserved.
+Copyright (c) 2021 Jakob von Raumer. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Jakob von Raumer, Eric Wieser
 -/

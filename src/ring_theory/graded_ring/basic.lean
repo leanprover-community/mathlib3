8c5546e7c21f5387cd5b63afba10af6e88d32661
Jujian Zhang <jujian.zhang1998@outlook.com>
2021-10-31 23:59:20 +0000

    Reducible?

--- src/ring_theory/graded_ring/basic.lean
+++ src/ring_theory/graded_ring/basic.lean
@@ -300,6 +300,7 @@ instance homogeneous_element.has_mul [add_comm_monoid ι] [decidable_eq ι] [gra
   use (i + j), exact graded_ring.mul_respect_grading hi hj,
 end ⟩ }
 
+@[reducible]
 instance homogenous_element_set.has_mul [add_comm_monoid ι] [decidable_eq ι] [graded_ring R A] :
   has_mul (set (homogeneous_element R A)) := by apply_instance
 

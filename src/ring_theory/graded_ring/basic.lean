3c14352a3d29a4e5bb1d9b32adce8bc84542df5e
jjaassoonn <jujian.zhang1998@out.com>
2021-11-02 15:47:45 +0000

    add `direct_sum.subgroup_coe_ring_hom`

--- src/ring_theory/graded_ring/basic.lean
+++ src/ring_theory/graded_ring/basic.lean
@@ -38,20 +38,24 @@ class graded_ring.core :=
 ( one_degree_zero : (1 : R) ∈ A 0 )
 ( mul_respect_grading : ∀ {i j : ι} {a b : R}, a ∈ A i → b ∈ A j → a * b ∈ A (i + j))
 
+/-- See above-/
 class graded_ring extends graded_ring.core R A :=
 ( decompose : R → ⨁ i, A i)
 ( left_inv : function.left_inverse decompose (direct_sum.add_subgroup_coe A) )
 ( right_inv : function.right_inverse decompose (direct_sum.add_subgroup_coe A) )
 
-variable [graded_ring R A]
-
-lemma graded_ring.is_internal : direct_sum.add_subgroup_is_internal A :=
+lemma graded_ring.is_internal [graded_ring R A] : direct_sum.add_subgroup_is_internal A :=
 ⟨graded_ring.left_inv.injective, graded_ring.right_inv.surjective⟩
+
+
+variable [graded_ring.core R A]
 
 instance gsemiring.of_ring_is_internally_graded :
   direct_sum.gsemiring (λ i, A i) :=
 direct_sum.gsemiring.of_add_subgroups A
   (graded_ring.core.one_degree_zero) (λ i j ai aj, graded_ring.core.mul_respect_grading ai.2 aj.2)
+
+variable [graded_ring R A]
 
 /--If `R` is graded by `ι` with degree `i` component `A i`, then `(⨁ i, A i ≃+* R)`-/
 def graded_ring.recompose : (⨁ i, A i) ≃+* R :=
@@ -71,3 +75,5 @@ let f : (⨁ i, A i) →+* R :=
   graded_ring.recompose R A (direct_sum.of _ i x) = x := dfinsupp.lift_add_hom_apply_single _ _ _
 
 end graded_ring
+
+#lint

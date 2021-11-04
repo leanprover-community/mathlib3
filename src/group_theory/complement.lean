8645dfd32c2eb634a68468301447009485ad8a45
Thomas Browning <tb65536@uw.edu>
2021-11-03 17:28:36 -0700

    results

--- src/group_theory/complement.lean
+++ src/group_theory/complement.lean
@@ -76,15 +76,74 @@ end
 @[to_additive] lemma is_complement'_comm : is_complement' H K ↔ is_complement' K H :=
 ⟨is_complement'.symm, is_complement'.symm⟩
 
-@[to_additive] lemma is_complement_top_singleton {g : G} :
-  is_complement (⊤ : set G) {g} :=
+@[to_additive] lemma is_complement_top_singleton {g : G} : is_complement (⊤ : set G) {g} :=
 ⟨λ ⟨x, _, rfl⟩ ⟨y, _, rfl⟩ h, prod.ext (subtype.ext (mul_right_cancel h)) rfl,
   λ x, ⟨⟨⟨x * g⁻¹, ⟨⟩⟩, g, rfl⟩, inv_mul_cancel_right x g⟩⟩
 
-@[to_additive] lemma is_complement_singleton_top {g : G} :
-  is_complement ({g} : set G) (⊤ : set G) :=
+@[to_additive] lemma is_complement_singleton_top {g : G} : is_complement ({g} : set G) ⊤ :=
 ⟨λ ⟨⟨_, rfl⟩, x⟩ ⟨⟨_, rfl⟩, y⟩ h, prod.ext rfl (subtype.ext (mul_left_cancel h)),
   λ x, ⟨⟨⟨g, rfl⟩, g⁻¹ * x, ⟨⟩⟩, mul_inv_cancel_left g x⟩⟩
+
+@[to_additive] lemma is_complement_singleton_left {g : G} : is_complement {g} S ↔ S = ⊤ :=
+begin
+  refine ⟨λ h, top_le_iff.mp (λ x hx, _), λ h, (congr_arg _ h).mpr is_complement_singleton_top⟩,
+  obtain ⟨y, hy⟩ := h.2 (g * x),
+  conv_rhs at hy { rw ← (show y.1.1 = g, from y.1.2) },
+  rw ← mul_left_cancel hy,
+  exact y.2.2,
+end
+
+@[to_additive] lemma is_complement_singleton_right {g : G} : is_complement S {g} ↔ S = ⊤ :=
+begin
+  refine ⟨λ h, top_le_iff.mp (λ x hx, _), λ h, (congr_arg _ h).mpr is_complement_top_singleton⟩,
+  obtain ⟨y, hy⟩ := h.2 (x * g),
+  conv_rhs at hy { rw ← (show y.2.1 = g, from y.2.2) },
+  rw ← mul_right_cancel hy,
+  exact y.1.2,
+end
+
+@[to_additive] lemma is_complement_top_left : is_complement ⊤ S ↔ ∃ g : G, S = {g} :=
+begin
+  refine ⟨λ h, set.exists_eq_singleton.mpr ⟨_, λ a b ha hb, _⟩, _⟩,
+  { obtain ⟨a, ha⟩ := h.2 1,
+    exact ⟨a.2.1, a.2.2⟩ },
+  { have : (⟨⟨_, mem_top a⁻¹⟩, ⟨a, ha⟩⟩ : (⊤ : set G) × S) = ⟨⟨_, mem_top b⁻¹⟩, ⟨b, hb⟩⟩ :=
+    h.1 ((inv_mul_self a).trans (inv_mul_self b).symm),
+    exact subtype.ext_iff.mp ((prod.ext_iff.mp this).2) },
+  { rintro ⟨g, rfl⟩,
+    exact is_complement_top_singleton },
+end
+
+@[to_additive] lemma is_complement_top_right :
+  is_complement S ⊤ ↔ ∃ g : G, S = {g} :=
+begin
+  refine ⟨λ h, set.exists_eq_singleton.mpr ⟨_, λ a b ha hb, _⟩, _⟩,
+  { obtain ⟨a, ha⟩ := h.2 1,
+    exact ⟨a.1.1, a.1.2⟩ },
+  { have : (⟨⟨a, ha⟩, ⟨_, mem_top a⁻¹⟩⟩ : S × (⊤ : set G)) = ⟨⟨b, hb⟩, ⟨_, mem_top b⁻¹⟩⟩ :=
+    h.1 ((mul_inv_self a).trans (mul_inv_self b).symm),
+    exact subtype.ext_iff.mp ((prod.ext_iff.mp this).1) },
+  { rintro ⟨g, rfl⟩,
+    exact is_complement_singleton_top },
+end
+
+@[to_additive] lemma is_complement'_top_bot : is_complement' (⊤ : subgroup G) ⊥ :=
+is_complement_top_singleton
+
+@[to_additive] lemma is_complement'_bot_top : is_complement' (⊥ : subgroup G) ⊤ :=
+is_complement_singleton_top
+
+@[to_additive] lemma is_complement'_bot_left : is_complement' ⊥ H ↔ H = ⊤ :=
+is_complement_singleton_left.trans coe_eq_top
+
+@[to_additive] lemma is_complement'_bot_right : is_complement' H ⊥ ↔ H = ⊤ :=
+is_complement_singleton_right.trans coe_eq_top
+
+@[to_additive] lemma is_complement'_top_left : is_complement' ⊤ H ↔ H = ⊥ :=
+is_complement_top_left.trans coe_eq_singleton
+
+@[to_additive] lemma is_complement'_top_right : is_complement' H ⊤ ↔ H = ⊥ :=
+is_complement_top_right.trans coe_eq_singleton
 
 @[to_additive] lemma mem_left_transversals_iff_exists_unique_inv_mul_mem :
   S ∈ left_transversals T ↔ ∀ g : G, ∃! s : S, (s : G)⁻¹ * g ∈ T :=

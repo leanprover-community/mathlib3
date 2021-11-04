1539003721f0bc676c829b1293e00caa4d579b28
erd1 <the.erd.one@gmail.com>
2021-11-04 13:55:48 +0800

    added author

--- src/topology/sheaves/sheafify.lean
+++ src/topology/sheaves/sheafify.lean
@@ -1,7 +1,7 @@
 /-
 Copyright (c) 2020 Scott Morrison. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
-Authors: Scott Morrison
+Authors: Scott Morrison, Andrew Yang
 -/
 import topology.sheaves.local_predicate
 import topology.sheaves.stalks

988a5505a25c8a2a38b4071cd6fc7629f2d132cc
YaelDillies <yael.dillies@gmail.com>
2021-11-05 17:42:11 +0000

    add le_log_iff_exp_le

--- src/analysis/special_functions/log.lean
+++ src/analysis/special_functions/log.lean
@@ -101,7 +101,9 @@ by { intro h, rwa [← exp_lt_exp, exp_log hx, exp_log (lt_trans hx h)] }
 lemma log_lt_log_iff (hx : 0 < x) (hy : 0 < y) : log x < log y ↔ x < y :=
 by { rw [← exp_lt_exp, exp_log hx, exp_log hy] }
 
-lemma log_le_iff_le_exp (hx : 0 < x) : log x ≤ y ↔ x ≤ exp y := by rw [←exp_le_exp, exp_log ha]
+lemma log_le_iff_le_exp (hx : 0 < x) : log x ≤ y ↔ x ≤ exp y := by rw [←exp_le_exp, exp_log hx]
+
+lemma le_log_iff_exp_le (hy : 0 < y) : x ≤ log y ↔ exp x ≤ y := by rw [←exp_le_exp, exp_log hy]
 
 lemma log_pos_iff (hx : 0 < x) : 0 < log x ↔ 1 < x :=
 by { rw ← log_one, exact log_lt_log_iff zero_lt_one hx }

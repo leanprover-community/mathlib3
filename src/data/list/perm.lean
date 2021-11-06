8b0eae6bd4b8f110910d644294093f0bb932c712
YaelDillies <yael.dillies@gmail.com>
2021-11-06 09:16:03 +0000

    fix data.list.perm

--- src/data/list/perm.lean
+++ src/data/list/perm.lean
@@ -3,6 +3,7 @@ Copyright (c) 2015 Microsoft Corporation. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Leonardo de Moura, Jeremy Avigad, Mario Carneiro
 -/
+import data.list.count
 import data.list.erase_dup
 import data.list.lattice
 import data.list.permutation

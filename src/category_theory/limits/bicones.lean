944c5f67aab80da2cb86f35892faeabf3ed61d17
erdOne <the.erd.one@gmail.com>
2021-11-06 16:31:24 +0000

    Moved bicones.lean

--- src/category_theory/limits/bicones.lean
+++ src/category_theory/limits/bicones.lean
@@ -1,5 +1,5 @@
 /-
-Copyright (c) 2017 Andrew Yang. All rights reserved.
+Copyright (c) 2021 Andrew Yang. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Andrew Yang
 -/

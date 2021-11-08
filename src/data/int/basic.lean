9888ff55d2e90131ddf66146c7db1a39eab52478
cocohearts <41932135+cocohearts@users.noreply.github.com>
2021-11-08 14:32:07 -0800

    Removing simp

--- src/data/int/basic.lean
+++ src/data/int/basic.lean
@@ -140,7 +140,7 @@ not_congr coe_nat_eq_zero
 
 @[simp] lemma coe_nat_nonneg (n : ℕ) : 0 ≤ (n : ℤ) := coe_nat_le.2 (nat.zero_le _)
 
-@[simp] lemma le_coe_nat_sub (m n : ℕ) :
+lemma le_coe_nat_sub (m n : ℕ) :
   (m - n : ℤ) ≤ ↑(m - n : ℕ) :=
 begin
   by_cases h: m ≥ n,

f4e00c3187ff9d2a7eb8e9c2165d47543e254527
YaelDillies <yael.dillies@gmail.com>
2021-11-09 13:02:19 +0000

    fix imports

--- src/analysis/normed_space/dual.lean
+++ src/analysis/normed_space/dual.lean
@@ -3,7 +3,7 @@ Copyright (c) 2020 Heather Macbeth. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Heather Macbeth
 -/
-import analysis.normed_space.hahn_banach
+import analysis.normed_space.hahn_banach.extension
 
 /-!
 # The topological dual of a normed space

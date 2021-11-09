a3035f441018f6e42f7bf679bddd272d3807d972
YaelDillies <yael.dillies@gmail.com>
2021-11-09 08:46:45 +0000

    namespacing, rename variables, mem_shadow_iff

--- src/combinatorics/set_family/shadow.lean
+++ src/combinatorics/set_family/shadow.lean
@@ -21,7 +21,7 @@ The `shadow` of a set family is everything we can get by removing an element fro
 
 `∂ 𝒜` is notation for `shadow 𝒜`. It is situated in locale `finset_family`.
 
-We also maintain the convention that `a, b : α` are elements of the ground type, `A, B : finset α`
+We also maintain the convention that `a, b : α` are elements of the ground type, `s, t : finset α`
 are finsets, and `𝒜, ℬ : finset (finset α)` are finset families.
 
 ## References
@@ -38,24 +38,15 @@ open finset nat
 
 variables {α : Type*}
 
-/-!
-### Shadows
+namespace finset
+variables [decidable_eq α] {𝒜 : finset (finset α)} {s t : finset α} {a : α} {k : ℕ}
 
-The shadow of a set family is everything we can get by removing an element
-from each set.
+/-- The shadow of a set family `𝒜` is all sets we can get by removing one element from any set in
+`𝒜`, and the (`k` times) iterated shadow (`shadow^[k]`) is all sets we can get by removing `k`
+elements from any set in `𝒜`. -/
+def shadow (𝒜 : finset (finset α)) : finset (finset α) := 𝒜.sup (λ s, s.image (erase s))
 
-This section develops the introductory theory of shadows, with some lemmas on
-iterated shadows as well.
--/
-
-variables [decidable_eq α] {𝒜 : finset (finset α)} {A B : finset α} {a : α}
-
-/-- The shadow of a set family `𝒜` is all sets we can get by removing one element
-from any set in `𝒜`, and the (`k` times) iterated shadow is all sets we can get
-by removing k elements from any set in `𝒜`. -/
-def shadow (𝒜 : finset (finset α)) : finset (finset α) := 𝒜.sup (λ A, A.image (erase A))
-
-localized "notation `∂ `:90 := shadow" in finset_family
+localized "notation `∂ `:90 := finset.shadow" in finset_family
 
 /-- The shadow of the empty set is empty. -/
 lemma shadow_empty : ∂ (∅ : finset (finset α)) = ∅ := rfl
@@ -64,68 +55,70 @@ lemma shadow_empty : ∂ (∅ : finset (finset α)) = ∅ := rfl
 lemma shadow_monotone : monotone (shadow : finset (finset α) → finset (finset α)) :=
 λ 𝒜 ℬ, sup_mono
 
-/-- `A` is in the shadow of `𝒜` iff there is an `B ∈ 𝒜` from which we can remove one element to
-get `A`. -/
-lemma mem_shadow_iff_eq_erase : A ∈ ∂ 𝒜 ↔ ∃ B ∈ 𝒜, ∃ a ∈ B, erase B a = A :=
+/-- `s` is in the shadow of `𝒜` iff there is an `t ∈ 𝒜` from which we can remove one element to
+get `s`. -/
+lemma mem_shadow_iff : s ∈ ∂ 𝒜 ↔ ∃ t ∈ 𝒜, ∃ a ∈ t, erase t a = s :=
 by simp only [shadow, mem_sup, mem_image]
 
-lemma erase_mem_shadow (hA : A ∈ 𝒜) (ha : a ∈ A) : erase A a ∈ ∂ 𝒜 :=
-mem_shadow_iff_eq_erase.2 ⟨A, hA, a, ha, rfl⟩
+lemma erase_mem_shadow (hs : s ∈ 𝒜) (ha : a ∈ s) : erase s a ∈ ∂ 𝒜 :=
+mem_shadow_iff.2 ⟨s, hs, a, ha, rfl⟩
 
-/-- `B` is in the shadow of `𝒜` iff we can an element to it so that the resulting finset is in `𝒜`.
+/-- `t` is in the shadow of `𝒜` iff we can an element to it so that the resulting finset is in `𝒜`.
 -/
-lemma mem_shadow_iff_insert_mem : A ∈ ∂ 𝒜 ↔ ∃ a ∉ A, insert a A ∈ 𝒜 :=
+lemma mem_shadow_iff_insert_mem : s ∈ ∂ 𝒜 ↔ ∃ a ∉ s, insert a s ∈ 𝒜 :=
 begin
-  refine mem_shadow_iff_eq_erase.trans ⟨_, _⟩,
-  { rintro ⟨A, hA, a, ha, rfl⟩,
-    refine ⟨a, not_mem_erase a A, _⟩,
+  refine mem_shadow_iff.trans ⟨_, _⟩,
+  { rintro ⟨s, hs, a, ha, rfl⟩,
+    refine ⟨a, not_mem_erase a s, _⟩,
     rwa insert_erase ha },
-  { rintro ⟨a, ha, hA⟩,
-    exact ⟨insert a A, hA, a, mem_insert_self _ _, erase_insert ha⟩ }
+  { rintro ⟨a, ha, hs⟩,
+    exact ⟨insert a s, hs, a, mem_insert_self _ _, erase_insert ha⟩ }
 end
 
-/-- `A ∈ ∂𝒜` iff `A` is exactly one element less than something from `𝒜` -/
+/-- `s ∈ ∂ 𝒜` iff `s` is exactly one element less than something from `𝒜` -/
 lemma mem_shadow_iff_exists_mem_card_add_one :
-  A ∈ ∂ 𝒜 ↔ ∃ B ∈ 𝒜, A ⊆ B ∧ B.card = A.card + 1 :=
+  s ∈ ∂ 𝒜 ↔ ∃ t ∈ 𝒜, s ⊆ t ∧ t.card = s.card + 1 :=
 begin
   refine mem_shadow_iff_insert_mem.trans ⟨_, _⟩,
-  { rintro ⟨a, ha, hA⟩,
-    exact ⟨insert a A, hA, subset_insert _ _, card_insert_of_not_mem ha⟩ },
-  { rintro ⟨B, hB, hAB, h⟩,
-    obtain ⟨a, ha⟩ : ∃ a, B \ A = {a} :=
-      card_eq_one.1 (by rw [card_sdiff hAB, h, add_tsub_cancel_left]),
-    exact ⟨a, λ haB,
-      not_mem_sdiff_of_mem_right haB ((ha.ge : _ ⊆ _) $ mem_singleton_self a),
-      by rwa [insert_eq a A, ←ha, sdiff_union_of_subset hAB]⟩ }
+  { rintro ⟨a, ha, hs⟩,
+    exact ⟨insert a s, hs, subset_insert _ _, card_insert_of_not_mem ha⟩ },
+  { rintro ⟨t, ht, hst, h⟩,
+    obtain ⟨a, ha⟩ : ∃ a, t \ s = {a} :=
+      card_eq_one.1 (by rw [card_sdiff hst, h, add_tsub_cancel_left]),
+    exact ⟨a, λ hat,
+      not_mem_sdiff_of_mem_right hat ((ha.ge : _ ⊆ _) $ mem_singleton_self a),
+      by rwa [insert_eq a s, ←ha, sdiff_union_of_subset hst]⟩ }
 end
 
-/-- Being in the shadow of `𝒜` means we have a superset in `𝒜`. -/
-lemma exists_subset_of_mem_shadow (hA : A ∈ ∂𝒜) : ∃ B ∈ 𝒜, A ⊆ B :=
-let ⟨B, hB, hAB⟩ :=  mem_shadow_iff_exists_mem_card_add_one.1 hA in ⟨B, hB, hAB.1⟩
+/-- teing in the shadow of `𝒜` means we have a superset in `𝒜`. -/
+lemma exists_subset_of_mem_shadow (hs : s ∈ ∂ 𝒜) : ∃ t ∈ 𝒜, s ⊆ t :=
+let ⟨t, ht, hst⟩ := mem_shadow_iff_exists_mem_card_add_one.1 hs in ⟨t, ht, hst.1⟩
 
-/-- `B ∈ ∂^k 𝒜` iff `B` is exactly `k` elements less than something in `𝒜`. -/
-lemma mem_shadow_iff_exists_mem_card_add {k : ℕ} :
-  A ∈ (shadow^[k] 𝒜) ↔ ∃ B ∈ 𝒜, A ⊆ B ∧ B.card = A.card + k :=
+/-- `t ∈ ∂^k 𝒜` iff `t` is exactly `k` elements less than something in `𝒜`. -/
+lemma mem_shadow_iff_exists_mem_card_add :
+  s ∈ (∂^[k]) 𝒜 ↔ ∃ t ∈ 𝒜, s ⊆ t ∧ t.card = s.card + k :=
 begin
-  induction k with k ih generalizing 𝒜 A,
-  { refine ⟨λ hA, ⟨A, hA, subset.refl _, rfl⟩, _⟩,
-    rintro ⟨B, hB, hAB, hcard⟩,
-    rwa eq_of_subset_of_card_le hAB hcard.le },
+  induction k with k ih generalizing 𝒜 s,
+  { refine ⟨λ hs, ⟨s, hs, subset.refl _, rfl⟩, _⟩,
+    rintro ⟨t, ht, hst, hcard⟩,
+    rwa eq_of_subset_of_card_le hst hcard.le },
   simp only [exists_prop, function.comp_app, function.iterate_succ],
   refine ih.trans _,
   clear ih,
   split,
-  { rintro ⟨B, hB, hAB, hcardAB⟩,
-    obtain ⟨C, hC, hBC, hcardBC⟩ := mem_shadow_iff_exists_mem_card_add_one.1 hB,
-    refine ⟨C, hC, hAB.trans hBC, _⟩,
-    rw [hcardBC, hcardAB],
+  { rintro ⟨t, ht, hst, hcardst⟩,
+    obtain ⟨u, hu, htu, hcardtu⟩ := mem_shadow_iff_exists_mem_card_add_one.1 ht,
+    refine ⟨u, hu, hst.trans htu, _⟩,
+    rw [hcardtu, hcardst],
     refl },
-  { rintro ⟨B, hB, hAB, hcard⟩,
-    obtain ⟨C, hAC, hCB, hC⟩ := finset.exists_intermediate_set k _ hAB,
-    rw add_comm at hC,
-    refine ⟨C, mem_shadow_iff_exists_mem_card_add_one.2 ⟨B, hB, hCB, _⟩, hAC, hC⟩,
-    rw [hcard, hC],
+  { rintro ⟨t, ht, hst, hcard⟩,
+    obtain ⟨u, hsu, hut, hu⟩ := finset.exists_intermediate_set k _ hst,
+    rw add_comm at hu,
+    refine ⟨u, mem_shadow_iff_exists_mem_card_add_one.2 ⟨t, ht, hut, _⟩, hsu, hu⟩,
+    rw [hcard, hu],
     refl,
     { rw [add_comm, hcard],
       exact le_succ _ } }
 end
+
+end finset

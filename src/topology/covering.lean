953aa86d62a32dd2a0d72c4a692568032534b520
Thomas Browning <tb65536@uw.edu>
2021-11-11 10:49:18 -0800

    cleaner approach to comp

--- src/topology/covering.lean
+++ src/topology/covering.lean
@@ -29,7 +29,7 @@ universes u
 variables {E E' X : Type u} [topological_space E] [topological_space E'] [topological_space X]
 
 def evenly_covered (f : E → X) (U : set X) : Prop :=
-∃ (α : Type u) (ι : (Σ (a : α), U) → E), embedding ι ∧ set.range ι = f ⁻¹' U ∧ ∀ s, f (ι s) = s.2
+∃ (α : Type u) (ι : (Σ a : α, U) → E), embedding ι ∧ set.range ι = f ⁻¹' U ∧ ∀ s, f (ι s) = s.2
 
 -- not sure if we'll end up needing this lemma
 lemma evenly_covered.comp {f : E' → X} {g : E → E'} {U : set X}
@@ -37,21 +37,18 @@ lemma evenly_covered.comp {f : E' → X} {g : E → E'} {U : set X}
 begin
   obtain ⟨α, ι, hι1, hι2, hι3⟩ := hf,
   obtain ⟨β, κ, hκ1, hκ2, hκ3⟩ := hg,
-  let γ := α × β,
-  let μ : (Σ (c : γ), U) → E :=
-  λ s, κ ⟨s.1.2, ⟨ι ⟨s.1.1, s.2⟩, (congr_arg (∈ U) (hι3 ⟨s.1.1, s.2⟩)).mpr s.2.2⟩⟩,
-  have h3 : ∀ s, f (g (μ s)) = s.2 := λ s, by rw [hκ3, subtype.coe_mk, hι3],
-  have h2 : set.range μ = (f ∘ g) ⁻¹' U,
-  { refine set.subset.antisymm _ _,
-    { rintros _ ⟨s, rfl⟩,
-      exact (congr_arg (∈ U) (h3 s)).mpr s.2.2 },
-    { intros e he,
-      obtain ⟨s, hs⟩ := hι2.symm.subset he,
-      obtain ⟨t, rfl⟩ := hκ2.symm.subset he,
-      exact ⟨⟨⟨s.1, t.1⟩, s.2⟩, by simp_rw [μ, sigma.eta, hs, hκ3, subtype.coe_eta, sigma.eta]⟩ } },
-  have h1 : embedding μ,
-  { sorry },
-  exact ⟨γ, μ, h1, h2, h3⟩,
+  --let ϕ : (Σ a : α, U) ≃ₜ set.range ι := homeomorph.of_embedding ι hι1,
+  let ψ : (Σ c : α × β, U) ≃ₜ (Σ b : β, f ⁻¹' U) :=
+  { to_fun := λ s, ⟨s.1.2, ⟨ι ⟨s.1.1, s.2⟩, (congr_arg (∈ U) (hι3 ⟨s.1.1, s.2⟩)).mpr s.2.2⟩⟩,
+    inv_fun := sorry,
+    left_inv := sorry,
+    right_inv := sorry,
+    continuous_to_fun := sorry,
+    continuous_inv_fun := sorry },
+  refine ⟨α × β, κ ∘ ψ, embedding.comp hκ1 ψ.embedding,
+    (function.surjective.range_comp ψ.surjective κ).trans hκ2, λ s, _⟩,
+  rw [function.comp_apply, hκ3],
+  apply hι3,
 end
 
 variables (E E' X)

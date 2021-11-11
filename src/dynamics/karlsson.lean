03fdffca7b17a8ad701cc4c43164d1b4f777b540
sgouezel <sebastien.gouezel@univ-rennes1.fr>
2021-11-11 21:17:31 +0100

    replace double file

--- src/dynamics/karlsson.lean
+++ src/dynamics/karlsson.lean
@@ -3,7 +3,7 @@ Copyright (c) 2021 Sébastien Gouëzel. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Sébastien Gouëzel
 -/
-import analysis.fekete
+import analysis.subadditive
 import analysis.inner_product_space.dual
 import tactic.by_contra
 import analysis.normed_space.dual

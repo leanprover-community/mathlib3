bcc366fcae205405354f1e6c6e4379bee46db7a5
Alex J. Best <alex.j.best@gmail.com>
2021-11-11 19:55:21 +0100

    fix

--- src/order/lexicographic.lean
+++ src/order/lexicographic.lean
@@ -3,7 +3,8 @@ Copyright (c) 2019 Scott Morrison. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Scott Morrison, Minchao Wu
 -/
-import logic.basic
+import tactic.rcases
+import tactic.interactive
 
 /-!
 # Lexicographic order

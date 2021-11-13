3472c3eed7d5262f4deec20c15e6fd36567e74d1
Alex J. Best <alex.j.best@gmail.com>
2021-11-13 22:51:50 +0100

    fix tests

--- src/tactic/zify.lean
+++ src/tactic/zify.lean
@@ -4,7 +4,7 @@ Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Robert Y. Lewis
 -/
 
-import data.int.basic
+import data.int.cast
 
 /-!
 # A tactic to shift `ℕ` goals to `ℤ`

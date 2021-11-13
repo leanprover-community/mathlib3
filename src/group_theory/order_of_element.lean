4d2cb30ab27b834bd53d990d775c1d0d2b41d696
Alex J. Best <alex.j.best@gmail.com>
2021-11-13 16:13:33 +0100

    couple more fixes

--- src/group_theory/order_of_element.lean
+++ src/group_theory/order_of_element.lean
@@ -4,6 +4,7 @@ Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Johannes Hölzl, Julian Kuelshammer
 -/
 import algebra.iterate_hom
+import data.int.gcd
 import dynamics.periodic_pts
 import group_theory.coset
 
--- src/group_theory/subgroup/basic.lean
+++ src/group_theory/subgroup/basic.lean
@@ -3,6 +3,7 @@ Copyright (c) 2020 Kexing Ying. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Kexing Ying
 -/
+import algebra.module.basic
 import group_theory.submonoid.pointwise
 import group_theory.submonoid.membership
 import group_theory.submonoid.center

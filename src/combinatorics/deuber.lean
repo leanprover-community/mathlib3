8fc63fdd68406504dff95d6e434f3a6a6d4c847d
David Wärn <codwarn@gmail.com>
2021-11-15 15:07:51 +0100

    finish composition

--- src/combinatorics/deuber.lean
+++ src/combinatorics/deuber.lean
@@ -24,33 +24,7 @@ lemma ends_at_with.self {α M} [has_zero M] [linear_order α] {f : α → M} {i 
   (h : ends_at_with f i x) : f i = x :=
 by rw [h _ (le_refl i), if_pos rfl]
 
-/-
-def starts_at_with {α M} [has_zero M] [linear_order α] (f : α → M) (i : α) (x : M) : Prop :=
-  ∀ j, j ≤ i → f j = if i = j then x else 0
-
-lemma starts_at_with.lt {α M} [has_zero M] [linear_order α] {f : α → M} {i j : α} {x : M}
-  (h : starts_at_with f i x) (ji : j < i) : f j = 0 :=
-by rw [h _ (le_of_lt ji), if_neg (ne_of_lt ji).symm]
-
-lemma starts_at_with.self {α M} [has_zero M] [linear_order α] {f : α → M} {i : α} {x : M}
-  (h : starts_at_with f i x) : f i = x :=
-by rw [h _ (le_refl i), if_pos rfl]
--/
-
 variables {R : Type} [semiring R] (A : submonoid R)
-
-/-
-lemma end_scalar_start {α} [fintype α] [linear_order α] {f v : α → R} {i j : α} {x y : R}
-  (ij : i ≤ j) (fe : ends_at_with f i x) (vs : starts_at_with v j y) :
-    ∑ a, f a * v a = if i = j then x * y else 0 :=
-begin
-  rw finset.sum_eq_single_of_mem i (finset.mem_univ i),
-  { rw [fe.self, vs _ ij, mul_ite, mul_zero, @eq_comm _ i j], },
-  intros k junk ki,
-  cases lt_or_gt_of_ne ki with h,
-  { rw [vs.lt (lt_of_lt_of_le h ij), mul_zero], },
-  { rw [fe.lt h, zero_mul], },
-end-/
 
 structure MPC :=
 (m : Type)
@@ -89,22 +63,12 @@ structure MPC.inclusion : Type :=
 structure MPC.row (o : option S.m) :=
 (d : S.m → R)
 (d_mem : ∀ i, d i ∈ S.p)
-(good : ∀ i ∈ o, ends_at_with d i S.C)
+(ends : ∀ i ∈ o, ends_at_with d i S.C)
 
 variables {S T}
 
-/-
-lemma MPC.inclusion.starts (inc : S.inclusion T) (i : S.m) :
-  starts_at_with (inc.matᵀ (inc.to_fun i)) i inc.scale :=
-begin
-  intros j ji,
-  rw [matrix.transpose_apply, inc.ends],
-  { congr' 1, ext, rw [order_embedding.eq_iff_eq, eq_comm], },
-  { rw [order_embedding.le_iff_le], exact ji, },
-end-/
-
 lemma MPC.inclusion.sum_eq (inc : S.inclusion T) {i : S.m} {j : T.m} {x : R}
-  (d : S.m → R) (ve : ends_at_with d i x) (ij : inc.to_fun i ≤ j) :
+  {d : S.m → R} (ve : ends_at_with d i x) (ij : inc.to_fun i ≤ j) :
   ∑ k : S.m, d k * inc.mat k j = if inc.to_fun i = j then d i * inc.scale else 0 :=
 begin
   rw [finset.sum_eq_single_of_mem i (finset.mem_univ i), inc.ends _ _ ij, mul_ite, mul_zero],
@@ -115,59 +79,64 @@ begin
   { rw [ve.lt, zero_mul], assumption, },
 end
 
-def MPC.inclusion.comp {R : MPC A} (f : S.inclusion T) (g : T.inclusion R) : S.inclusion R :=
+def MPC.inclusion.comp {U : MPC A} (f : S.inclusion T) (g : T.inclusion U) : S.inclusion U :=
 { to_fun := f.to_fun.trans g.to_fun,
   mat := f.mat ⬝ g.mat,
   scale := f.scale * g.scale,
   scale_C := by rw [←mul_assoc, f.scale_C, g.scale_C],
   ends :=
+    by { intros i j ij, rw [matrix.mul_apply, g.sum_eq (f.ends _) ij, (f.ends _).self], refl },
+  foo := λ x, g.foo x >>= f.foo,
+  disj :=
     begin
-      intros i j ij,
-      rw [matrix.mul_apply],
-      simp_rw ←matrix.transpose_apply g.mat,
-      rw [end_scalar_start _ (f.ends _) (g.starts _)],
+      intros i j h,
+      rw matrix.mul_apply,
+      apply finset.sum_eq_zero,
+      intros k junk,
+      by_cases hh : g.foo j = some k,
+      { rw [hh, option.some_bind] at h, rw [f.disj _ _ h, zero_mul], },
+      { rw [g.disj _ _ hh, mul_zero], }
     end,
-  foo := _,
-  disj := _,
-  mem := _ }
+  mem :=
+    begin
+      intros i k r hr,
+      rw matrix.mul_apply,
+      set o := g.foo k with ho,
+      clear_value o,
+      cases o with j,
+      { convert U.zero_mem, rw [finset.sum_eq_zero, mul_zero],
+        intros j junk, rw [g.disj, mul_zero], rw ←ho, rintro ⟨⟩, },
+      { rw [finset.sum_eq_single_of_mem j (finset.mem_univ j), ←mul_assoc],
+        { apply g.mem, apply f.mem, exact hr, },
+        { intros b junk bj, rw [g.disj, mul_zero],
+          rw ←ho, exact (option.some_injective _).ne bj.symm, }, }
+    end }
 
 def MPC.inclusion.map_row (inc : S.inclusion T) (o : option S.m) (v : S.row o) :
   T.row (o.map inc.to_fun) :=
-{ d := λ j, ∑ i : S.m, v.d i * inc.mat i j,
+{ d := inc.mat.vec_mul v.d,
   d_mem :=
     begin
       intro j,
-      set i := inc.foo j with hi,
-      clear_value i,
-      cases i,
-      { convert T.zero_mem,
-        apply finset.sum_eq_zero,
-        intros i junk,
-        rw [inc.disj, mul_zero],
-        rw ←hi, rintro ⟨⟩, },
+      change ∑ i : S.m, v.d i * inc.mat i j ∈ _,
+      set o := inc.foo j with hi,
+      clear_value o,
+      cases o with i,
+      { convert T.zero_mem, apply finset.sum_eq_zero,
+        intros i junk, rw [inc.disj, mul_zero], rw ←hi, rintro ⟨⟩, },
       { rw finset.sum_eq_single_of_mem i (finset.mem_univ i),
         { apply inc.mem, apply v.d_mem, },
-        { intros i' junk ine,
-          rw [inc.disj, mul_zero],
+        { intros i' junk ine, rw [inc.disj, mul_zero],
           rw ←hi, apply (option.some_injective _).ne, exact ine.symm, }, },
     end,
-  good :=
+  ends :=
     begin
       cases o with i,
       { simp only [forall_false_left, implies_true_iff, option.map_none', option.not_mem_none], },
       { simp only [option.map_some', option.mem_def, forall_eq'],
-        split,
-        { convert inc.scale_C,
-          dsimp only,
-          rw [finset.sum_eq_single_of_mem i (finset.mem_univ i), (v.good _ rfl).1, (inc.ends i).1],
-          { intros i' junk ine,
-            cases lt_or_gt_of_ne ine,
-            { rw [(inc.ends i').2, mul_zero], rw order_embedding.lt_iff_lt, assumption, },
-            { rw [(v.good _ rfl).2, zero_mul], assumption, }, } },
-        { intros j hj, apply finset.sum_eq_zero, intros i' junk, cases le_or_gt i' i,
-          { rw [(inc.ends i').2, mul_zero], refine lt_of_le_of_lt _ hj,
-            rw [order_embedding.le_iff_le], assumption, },
-          { rw [(v.good _ rfl).2, zero_mul], assumption, }, }, },
+        intros j ij,
+        convert inc.sum_eq (v.ends _ rfl) ij,
+        rw [(v.ends _ rfl).self, inc.scale_C], },
     end }
 
 variables (S T) (κ : Type)

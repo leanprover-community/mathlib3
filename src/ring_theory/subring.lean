235dc4bab8228f96be573fcd7c602c3906058b8e
Eric Wieser <wieser.eric@gmail.com>
2021-11-15 16:35:06 +0000

    fix

--- src/ring_theory/subring.lean
+++ src/ring_theory/subring.lean
@@ -1006,5 +1006,5 @@ def units.pos_subgroup (R : Type*) [linear_ordered_semiring R] :
   inv_mem' := λ x (hx : (0 : R) < x), (zero_lt_mul_left hx).mp $ x.mul_inv.symm ▸ zero_lt_one,
   ..(pos_submonoid R).comap (units.coe_hom R)}
 
-@[simp] lemma units.mem_pos_subgroup {R : Type*} [linear_ordered_comm_ring R] [nontrivial R]
+@[simp] lemma units.mem_pos_subgroup {R : Type*} [linear_ordered_semiring R]
   (u : units R) : u ∈ units.pos_subgroup R ↔ (0 : R) < u := iff.rfl

9b4b7f23f1ddda285077997aa2cc5ac0ea056ced
Stuart Presnell <stuart.presnell@gmail.com>
2021-11-16 15:11:10 +0000

    Moving material from `number_theory/divisors` to `data/nat/prime`

--- src/data/nat/prime.lean
+++ src/data/nat/prime.lean
@@ -633,6 +633,14 @@ lemma mem_factors_iff_dvd {n p : ℕ} (hn : 0 < n) (hp : prime p) : p ∈ factor
 ⟨λ h, prod_factors hn ▸ list.dvd_prod h,
  λ h, mem_list_primes_of_dvd_prod hp (@prime_of_mem_factors n) ((prod_factors hn).symm ▸ h)⟩
 
+lemma dvd_of_mem_factors {n p : ℕ} (h : p ∈ n.factors) : p ∣ n
+  :=
+begin
+  by_cases hn : n = 0,
+  { convert dvd_zero p },
+  { rwa ←mem_factors_iff_dvd (pos_iff_ne_zero.mpr hn) (prime_of_mem_factors h) }
+end
+
 lemma mem_factors {n p} (hn : 0 < n) : p ∈ factors n ↔ prime p ∧ p ∣ n :=
 ⟨λ h, ⟨prime_of_mem_factors h, (mem_factors_iff_dvd hn $ prime_of_mem_factors h).mp h⟩,
  λ ⟨hprime, hdvd⟩, (mem_factors_iff_dvd hn hprime).mpr hdvd⟩
@@ -974,5 +982,54 @@ end tactic
 namespace nat
 
 theorem prime_three : prime 3 := by norm_num
+
+end nat
+
+
+namespace nat
+
+/-- The only prime divisor of positive prime power `p^k` is `p` itself -/
+lemma prime_pow_prime_divisor {p k : ℕ} (hk : 0 < k) (hp: prime p) :
+  (p^k).factors.to_finset = {p}
+  :=
+begin
+  rw hp.factors_pow, exact list.repeat_to_finset_pos hk,
+end
+
+lemma factors_mul_of_pos {a b : ℕ} (ha : 0 < a) (hb : 0 < b) (p : ℕ) :
+  p ∈ (a * b).factors ↔ p ∈ a.factors ∨ p ∈ b.factors
+  :=
+begin
+  rw [mem_factors (mul_pos ha hb), mem_factors ha, mem_factors hb, ←and_or_distrib_left],
+  simp only [and.congr_right_iff], exact prime.dvd_mul,
+end
+
+/-- If `a`,`b` are positive the prime divisors of `(a * b)` are the union of those of `a` and `b` -/
+lemma prime_divisors_mul_of_pos {a b : ℕ} (ha : 0 < a) (hb : 0 < b) :
+  (a * b).factors.to_finset = a.factors.to_finset ∪ b.factors.to_finset
+  :=
+begin
+  ext p, rw finset.mem_union, repeat {rw list.mem_to_finset}, exact factors_mul_of_pos ha hb p,
+end
+
+/-- The sets of factors of coprime `a` and `b` are disjoint -/
+lemma coprime_factors_disjoint {a b : ℕ} (hab: a.coprime b) : list.disjoint a.factors b.factors
+  :=
+begin
+  intros q hqa hqb,
+  apply not_prime_one,
+  rw ←(eq_one_of_dvd_coprimes hab (dvd_of_mem_factors hqa) (dvd_of_mem_factors hqb)),
+  exact prime_of_mem_factors hqa,
+end
+
+lemma factors_mul_of_coprime {a b : ℕ} (hab : coprime a b) (p:ℕ):
+  p ∈ (a * b).factors ↔ p ∈ a.factors ∪ b.factors
+  :=
+begin
+  by_cases ha : a = 0, { rw ha at *, simp at hab, rw hab, simp },
+  by_cases hb : b = 0, { rw hb at *, simp at hab, rw hab, simp },
+  rw factors_mul_of_pos (pos_iff_ne_zero.mpr ha) (pos_iff_ne_zero.mpr hb) p,
+  rw list.mem_union,
+end
 
 end nat

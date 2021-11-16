3ec3d8a12d8e5daa94d246513d0b551c20b15ac0
Stuart Presnell <stuart.presnell@gmail.com>
2021-11-16 14:02:26 +0000

    Rename variables

--- src/data/finset/basic.lean
+++ src/data/finset/basic.lean
@@ -1823,12 +1823,12 @@ end
   to_finset l.reverse = l.to_finset :=
 to_finset_eq_of_perm _ _ (reverse_perm l)
 
-lemma to_finset_union (L1 L2 : list α) :
-(L1 ∪ L2).to_finset = L1.to_finset ∪ L2.to_finset :=
+lemma to_finset_union (l l' : list α) :
+(l ∪ l').to_finset = l.to_finset ∪ l'.to_finset :=
 by {ext, simp}
 
-lemma to_finset_inter (L1 L2 : list α) :
-(L1 ∩ L2).to_finset = L1.to_finset ∩ L2.to_finset :=
+lemma to_finset_inter (l l' : list α) :
+(l ∩ l').to_finset = l.to_finset ∩ l'.to_finset :=
 by {ext, simp}
 
 end list

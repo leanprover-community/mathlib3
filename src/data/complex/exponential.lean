34df5d80ef131e768556e1457c177befddb95dbc
Iván Sadofschi Costa <isadofschi@dm.uba.ar>
2021-11-17 16:05:33 -0300

    tidy more following suggestions from code review

--- src/data/complex/exponential.lean
+++ src/data/complex/exponential.lean
@@ -1360,9 +1360,10 @@ begin
   have h4 : |x| ≤ 1,
   { rw abs_le,
     exact ⟨by linarith, h2⟩ },
-  let h' := real.exp_bound h4 hn, rw h3 at h',
-  let h'' := (abs_sub_le_iff.1 h').1,
-  let t := sub_le_iff_le_add'.1 h'',
+  have h' := real.exp_bound h4 hn,
+  rw h3 at h',
+  have h'' := (abs_sub_le_iff.1 h').1,
+  have t := sub_le_iff_le_add'.1 h'',
   simpa [mul_div_assoc] using t
 end
 
@@ -1536,9 +1537,9 @@ begin
       neg_add_eq_sub, sub_half, sq, pow_succ, sq],
   have i1 : x * 4 / 18 ≤ 1 / 2 := by linarith,
   have i2 : 0 ≤ x * 4 / 18 := by linarith,
-  let i3 := mul_le_mul h1 h1 (le_refl (0 : ℝ)) h1,
+  have i3 := mul_le_mul h1 h1 (le_refl (0 : ℝ)) h1,
   rw zero_mul at i3,
-  let t := mul_le_mul (le_refl (x * x)) i1 i2 i3,
+  have t := mul_le_mul (le_refl (x * x)) i1 i2 i3,
   rw ← mul_assoc,
   rwa [mul_one_div, ← mul_div_assoc, ← mul_assoc] at t,
 end
@@ -1547,36 +1548,43 @@ lemma exp_bound_div_one_sub_of_interval {x : ℝ} (h1 : 0 ≤ x) (h2 : x < 1) :
   real.exp x ≤ 1 / (1 - x) :=
 begin
   have h : ∑ j in (finset.range 3), x^j ≤ 1/(1-x),
-    repeat {rw finset.sum}, norm_num,
-    have h1x : 0 < 1 - x, simp, exact h2,
-    apply (le_div_iff h1x).2, rw ← add_assoc, rw mul_sub_left_distrib,
-    rw mul_one, repeat {rw add_mul}, repeat {rw sub_add_eq_sub_sub},
-    rw ← pow_succ' x 2, norm_num,
-    have hx3 : 0 ≤ x ^ 3, norm_num, exact h1,
-    linarith,
-  let h03 : 0 < 3, linarith,
-  exact le_trans (exp_bound' h1 (le_of_lt h2) h03)
-   (le_trans (exp_bound_div_one_sub_of_interval_approx h1 (le_of_lt h2)) h),
+  { norm_num [finset.sum],
+    have h1x : 0 < 1 - x := by simpa,
+    apply (le_div_iff h1x).2,
+    norm_num [← add_assoc, mul_sub_left_distrib, mul_one, add_mul,
+      sub_add_eq_sub_sub, pow_succ' x 2],
+    have hx3 : 0 ≤ x ^ 3,
+    { norm_num,
+      exact h1 },
+    linarith },
+  exact le_trans (exp_bound' h1 h2.le (by linarith))
+   (le_trans (exp_bound_div_one_sub_of_interval_approx h1 h2.le) h),
 end
 
 lemma one_sub_le_exp_minus_of_pos {y : ℝ} (h : 0 ≤ y) : 1 - y ≤ real.exp (-y) :=
 begin
   rw real.exp_neg,
   have r1 : (1 - y) * (real.exp y) ≤ 1,
-    by_cases hy1 : y ≥ 1,
-      have h' : 1 - y ≤ 0, linarith,
-      have h'' : (1 - y) * real.exp y ≤ 0,
-        apply mul_nonpos_iff.2, right, apply and.intro, exact h', exact le_of_lt (real.exp_pos y),
-      linarith,
-    have hy0 : 0 < 1 - y, linarith,
-    apply (le_div_iff' hy0).1, simp at hy1, exact exp_bound_div_one_sub_of_interval h hy1,
-  rw inv_eq_one_div, apply (le_div_iff' (real.exp_pos y)).2, rw mul_comm, exact r1,
+  { cases le_or_lt (1-y) 0, {
+    have h'' : (1 - y) * real.exp y ≤ 0,
+    { apply mul_nonpos_iff.2,
+      right,
+      apply and.intro,
+      exact h_1,
+      exact (real.exp_pos y).le },
+    linarith },
+    have hy1 : y < 1 := by linarith,
+    apply (le_div_iff' h_1).1,
+    exact exp_bound_div_one_sub_of_interval h hy1 },
+  rw inv_eq_one_div,
+  apply (le_div_iff' (real.exp_pos y)).2,
+  rwa mul_comm at r1,
 end
 
 lemma add_one_le_exp_of_nonpos {x : ℝ} (h : x ≤ 0) : x + 1 ≤ real.exp x :=
 begin
   rw add_comm,
-  have h1 : 0 ≤ -x, linarith,
+  have h1 : 0 ≤ -x := by linarith,
   simpa using one_sub_le_exp_minus_of_pos h1
 end
 

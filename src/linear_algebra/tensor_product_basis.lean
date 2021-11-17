8451a8b8ab78d3b9f4821ccfe6887f67c926a7f6
antoinelab01 <66086247+antoinelab01@users.noreply.github.com>
2021-11-17 16:04:48 -0500

    Update src/linear_algebra/tensor_product_basis.lean
    Co-authored-by: Oliver Nash <github@olivernash.org>

--- src/linear_algebra/tensor_product_basis.lean
+++ src/linear_algebra/tensor_product_basis.lean
@@ -29,8 +29,8 @@ finsupp.basis_single_one.map
 
 @[simp]
 lemma basis.tensor_product_apply (b : basis ι R M) (c : basis κ R N) (i : ι) (j : κ) :
-  (basis.tensor_product b c) (i, j) = b i ⊗ₜ c j :=
-by { dunfold basis.tensor_product, simp }
+  basis.tensor_product b c (i, j) = b i ⊗ₜ c j :=
+by simp [basis.tensor_product]
 
 lemma basis.tensor_product_apply' (b : basis ι R M) (c : basis κ R N) (i : ι × κ) :
   (basis.tensor_product b c) i = b i.1 ⊗ₜ c i.2 :=

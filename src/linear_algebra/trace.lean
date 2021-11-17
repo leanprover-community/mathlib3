eadcf9cbeecdc9ced52fb726068ab517282ff6e0
Antoine Labelle <antoinelab01@gmail.com>
2021-11-17 13:59:19 -0500

    fixed typo

--- src/linear_algebra/trace.lean
+++ src/linear_algebra/trace.lean
@@ -1,7 +1,7 @@
 /-
 Copyright (c) 2019 Johannes Hölzl. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
-Authors: Johannes Hölzl, Patrick Massot, Casper Putz, Anne Baanen, ANtoine Labelle
+Authors: Johannes Hölzl, Patrick Massot, Casper Putz, Anne Baanen, Antoine Labelle
 -/
 import linear_algebra.matrix.to_lin
 import linear_algebra.matrix.trace

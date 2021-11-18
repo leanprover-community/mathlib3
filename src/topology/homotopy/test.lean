f2cdfe63b0bbcb5e957c888c2d43ce9603ffe6f6
Shing Tak Lam <shingtaklam1324@gmail.com>
2021-11-18 14:45:59 +0000

    init

---
+++ src/topology/homotopy/test.lean
@@ -0,0 +1,129 @@
+import topology.homotopy.basic
+
+universes u v
+
+variables (ι : Type u)
+
+noncomputable theory
+
+open_locale unit_interval classical
+/-
+(ι → I) → X : surface?
+-/
+
+def baz : C(ι → ℝ, ι → I) :=
+{ to_fun := λ x i, continuous_map.id.Icc_extend (@zero_le_one ℝ _) (x i) }
+
+lemma baz_apply_of_mem (x : ι → ℝ) (h : x ∈ (set.univ : set ι).pi (λ i, I)) :
+  baz ι x = (λ i, ⟨x i, set.mem_univ_pi.1 h _⟩) :=
+begin
+  ext i,
+  rw [baz, continuous_map.coe_mk, continuous_map.coe_Icc_extend, continuous_map.id_coe],
+  apply congr_arg,
+  rw set.Icc_extend_of_mem _ _ (set.mem_univ_pi.1 h _),
+  refl,
+end
+
+structure foo {X : Type v} [topological_space X] (x : X) extends C(ι → I, X) :=
+(boundary : ∀ p : ι → I, (∃ i, p i = 0 ∨ p i = 1) → to_fun p = x)
+
+namespace foo
+
+variable {ι}
+variables {X : Type v} [topological_space X] {x₀ : X}
+
+instance : has_coe_to_fun (foo ι x₀) (λ _, (ι → I) → X) := ⟨λ f, f.to_fun⟩
+
+def extend (f : foo ι x₀) : C(ι → ℝ, X) := f.to_continuous_map.comp (baz ι)
+
+lemma extend_apply_of_mem {f : foo ι x₀} {p : ι → ℝ} (h : p ∈ (set.univ : set ι).pi (λ i, I)) :
+  f.extend p = f (λ i, ⟨p i, set.mem_univ_pi.1 h _⟩) :=
+begin
+  rw [extend, continuous_map.comp_apply, baz_apply_of_mem _ _ h],
+  refl,
+end
+
+lemma apply_of_exists_eq_zero_or_eq_one {f : foo ι x₀} (p : ι → I) (hx : ∃ i, p i = 0 ∨ p i = 1) :
+  f p = x₀ := f.boundary _ hx
+
+example (f g : foo ι x₀) (i : ι) : foo ι x₀ :=
+{ to_fun := λ t,
+  if (t i : ℝ) ≤ 1/2 then
+    f.extend (λ j, if i = j then 2 * t j else t j)
+  else
+    g.extend (λ j, if i = j then 2 * t j - 1 else t j),
+  continuous_to_fun := begin
+    apply continuous_if_le,
+    { continuity },
+    { continuity },
+    { apply continuous.continuous_on,
+      apply continuous.comp,
+      { continuity },
+      { rw continuous_pi_iff,
+        intro j,
+        split_ifs;
+        continuity } },
+    { apply continuous.continuous_on,
+      apply continuous.comp,
+      { continuity },
+      { rw continuous_pi_iff,
+        intro j,
+        split_ifs;
+        continuity } },
+    { intros x hx,
+      rw [extend_apply_of_mem, extend_apply_of_mem],
+      { rw [apply_of_exists_eq_zero_or_eq_one, apply_of_exists_eq_zero_or_eq_one];
+        { use i,
+          norm_num [hx] } },
+      { rw set.mem_pi,
+        intros j hj,
+        split_ifs,
+        { rw [←h, hx],
+          norm_num },
+        { exact subtype.mem (x j) } },
+      { rw set.mem_pi,
+        intros j hj,
+        split_ifs,
+        { rw [←h, hx],
+          norm_num },
+        { exact subtype.mem (x j) } } }
+  end,
+  boundary := begin
+    rintros p ⟨j, hj⟩,
+    dsimp only,
+    split_ifs,
+    { rw [extend_apply_of_mem, apply_of_exists_eq_zero_or_eq_one],
+      { use j,
+        cases hj,
+        { left,
+          norm_num [hj] },
+        { right,
+          apply subtype.ext,
+          rw [subtype.coe_mk, if_neg, hj],
+          intro hij,
+          norm_num [hij, hj] at h } },
+      { rw set.mem_pi,
+        intros k hk,
+        split_ifs with hik hik,
+        { rw [unit_interval.mul_pos_mem_iff zero_lt_two, ←hik],
+          exact ⟨unit_interval.nonneg _, h⟩ },
+        { exact subtype.mem _ } } },
+    {  rw [extend_apply_of_mem, apply_of_exists_eq_zero_or_eq_one],
+      { use j,
+        cases hj,
+        { left,
+          apply subtype.ext,
+          rw [subtype.coe_mk, if_neg, hj],
+          intro hij,
+          norm_num [hij, hj] at h },
+        { right,
+          norm_num [hj] } },
+      { rw set.mem_pi,
+        intros k hk,
+        split_ifs with hik hik,
+        { rw [unit_interval.two_mul_sub_one_mem_iff, ←hik],
+          exact ⟨le_of_not_le h, unit_interval.le_one _⟩, },
+        { exact subtype.mem _ } } }
+  end }
+
+end foo

39923e0e03e7ee9ccc51cbf8a6faace86e6efb6c
Bolton Bailey <bolton.bailey@gmail.com>
2021-11-19 15:13:52 -0600

    golf

--- src/data/nat/interval.lean
+++ src/data/nat/interval.lean
@@ -168,9 +168,8 @@ end
 
 lemma Ico_succ_left_eq_erase_Ico : Ico a.succ b = erase (Ico a b) a :=
 begin
-  rw Ico_succ_left,
   ext x,
-  rw [mem_erase, mem_Ico, mem_Ioo, ←and_assoc, ne_comm, and_comm (a ≠ x), lt_iff_le_and_ne],
+  rw [Ico_succ_left, mem_erase, mem_Ico, mem_Ioo, ←and_assoc, ne_comm, and_comm (a ≠ x), lt_iff_le_and_ne],
 end
 
 lemma range_image_pred_top_sub (n : ℕ) : (finset.range n).image (λ j, n - 1 - j) = finset.range n :=

c662ede17bac8f4c73ab6b73421024ace0ef563b
Yury G. Kudryashov <urkud@urkud.name>
2021-11-20 10:24:37 -0500

    Fix import

--- src/topology/subset_properties.lean
+++ src/topology/subset_properties.lean
@@ -3,6 +3,7 @@ Copyright (c) 2017 Johannes Hölzl. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Johannes Hölzl, Mario Carneiro, Yury Kudryashov
 -/
+import order.filter.pi
 import topology.bases
 import data.finset.order
 import data.set.accumulate

312f4d29b413be3fc0c3e11fd25ee3770a746a24
Andrew Yang <36414270+erdOne@users.noreply.github.com>
2021-11-21 13:52:09 +0800

    Update functor_category.lean

--- src/category_theory/limits/preserves/functor_category.lean
+++ src/category_theory/limits/preserves/functor_category.lean
@@ -6,6 +6,7 @@ Authors: Bhavik Mehta
 import category_theory.limits.functor_category
 import category_theory.limits.preserves.shapes.binary_products
 import category_theory.limits.yoneda
+import category_theory.limits.kan_extension
 
 /-!
 # Preservation of (co)limits in the functor category

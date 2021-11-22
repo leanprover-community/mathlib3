34da4e715f1ebc806cf030feb518cd60dd4affe6
Jireh Loreaux <loreaujy@gmail.com>
2021-11-22 08:19:59 -0600

    address naming and namespace issues
    Keep the two is_unit lemmas out of the spectrum
    namespace, put the `spectrum` and `resolvent`
    definitions in the root namespace. Change names
    of declarations to not repeat spectrum (e.g., use
    `mem_iff` instead of `mem_iff_spectrum` since it
    is already in the `spectrum` namespace).

--- src/algebra/algebra/spectrum.lean
+++ src/algebra/algebra/spectrum.lean
@@ -35,7 +35,6 @@ This theory will serve as the foundation for spectral theory in Banach algebras.
 
 universes u v
 
-namespace spectrum
 
 section defs
 
@@ -64,25 +63,6 @@ end defs
 variables {R : Type u} {A : Type v}
 variables [comm_ring R] [ring A] [algebra R A]
 
-local notation `σ` := spectrum R
-
-lemma mem_spectrum_iff {r : R} {a : A} :
-  r ∈ σ a ↔ ¬ is_unit (r • 1 - a) :=
-iff.rfl
-
-lemma not_mem_spectrum_iff {r : R} {a : A} :
-  r ∉ σ a ↔ is_unit (r • 1 - a) :=
-by { apply not_iff_not.mp, simp [set.not_not_mem, mem_spectrum_iff] }
-
-lemma mem_resolvent_of_left_right_inverse {r : R} {a b c : A}
-  (h₁ : (r • 1 - a) * b = 1) (h₂ : c * (r • 1 - a) = 1) :
-  r ∈ resolvent R a :=
-units.is_unit ⟨r•1 - a, b, h₁, by rwa ←left_inv_eq_right_inv h₂ h₁⟩
-
-lemma mem_resolvent_iff {r : R} {a : A} :
-  r ∈ resolvent R a ↔ is_unit (r•1 - a) :=
-iff.rfl
-
 -- products of scalar units and algebra units
 
 lemma is_unit.smul_iff {G : Type u} [group G] [mul_action G A]
@@ -107,7 +87,29 @@ begin
   rw [←smul_sub,is_unit.smul_iff],
 end
 
-lemma add_mem_spectrum {a : A} {r s : R} :
+namespace spectrum
+
+
+local notation `σ` := spectrum R
+
+lemma mem_iff {r : R} {a : A} :
+  r ∈ σ a ↔ ¬ is_unit (r • 1 - a) :=
+iff.rfl
+
+lemma not_mem_iff {r : R} {a : A} :
+  r ∉ σ a ↔ is_unit (r • 1 - a) :=
+by { apply not_iff_not.mp, simp [set.not_not_mem, mem_iff] }
+
+lemma mem_resolvent_of_left_right_inverse {r : R} {a b c : A}
+  (h₁ : (r • 1 - a) * b = 1) (h₂ : c * (r • 1 - a) = 1) :
+  r ∈ resolvent R a :=
+units.is_unit ⟨r•1 - a, b, h₁, by rwa ←left_inv_eq_right_inv h₂ h₁⟩
+
+lemma mem_resolvent_iff {r : R} {a : A} :
+  r ∈ resolvent R a ↔ is_unit (r•1 - a) :=
+iff.rfl
+
+lemma add_mem_iff {a : A} {r s : R} :
   r ∈ σ a ↔ r + s ∈ σ (s • 1 + a) :=
 begin
   apply not_iff_not.mpr,
@@ -117,7 +119,7 @@ begin
   simp [h_eq],
 end
 
-lemma smul_mem_spectrum_smul {a : A} {s : R} {r : units R} :
+lemma smul_mem_smul_iff {a : A} {s : R} {r : units R} :
   r • s ∈ σ (r • a) ↔ s ∈ σ a :=
 begin
   apply not_iff_not.mpr,
@@ -128,25 +130,25 @@ end
 
 open_locale pointwise
 
-theorem spectrum_smul_eq_smul_spectrum (a : A) (r : units R) :
+theorem smul_eq_smul (a : A) (r : units R) :
   σ (r • a) = r • σ a :=
 begin
   ext,
   have x_eq : x = r•r⁻¹•x, by simp,
   nth_rewrite 0 x_eq,
-  rw smul_mem_spectrum_smul,
+  rw smul_mem_smul_iff,
   split,
     { exact λ h, ⟨r⁻¹•x,⟨h,by simp⟩⟩},
     { rintros ⟨_,_,x'_eq⟩, simpa [←x'_eq],}
 end
 
-theorem left_add_coset_spectrum (a : A) (r : R) :
+theorem left_add_coset_eq (a : A) (r : R) :
   left_add_coset r (σ a) = σ (r • 1 + a) :=
-by { ext, rw [mem_left_add_coset_iff, neg_add_eq_sub, add_mem_spectrum],
+by { ext, rw [mem_left_add_coset_iff, neg_add_eq_sub, add_mem_iff],
      nth_rewrite 1 ←sub_add_cancel x r, }
 
 -- `r ∈ σ(a*b) ↔ r ∈ σ(b*a)` for any `r : units R`
-theorem unit_mem_spectrum_mul_iff_swap_mul {a b : A} {r : units R} :
+theorem unit_mem_mul_iff_mem_swap_mul {a b : A} {r : units R} :
   ↑r ∈ σ (a * b) ↔ ↑r ∈ σ (b * a) :=
 begin
   apply not_iff_not.mpr,
@@ -173,8 +175,8 @@ begin
   rw [←smul_mul_assoc, ←mul_smul_comm r⁻¹ b a, is_unit_one_sub_mul_iff_swap],
 end
 
-theorem units_spectrum_mul_iff_swap_mul {a b : A} :
+theorem preimage_units_mul_eq_swap_mul {a b : A} :
   (coe : units R → R) ⁻¹' σ (a * b) = coe ⁻¹'  σ (b * a) :=
-by { ext, exact unit_mem_spectrum_mul_iff_swap_mul, }
+by { ext, exact unit_mem_mul_iff_mem_swap_mul, }
 
 end spectrum

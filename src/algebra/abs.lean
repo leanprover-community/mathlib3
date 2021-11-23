2997766dcf757334b8c885458fe777a0f08d580c
Christopher Hoskin <christopher.hoskin@gmail.com>
2021-11-23 23:02:32 +0000

    Update documentation

--- src/algebra/abs.lean
+++ src/algebra/abs.lean
@@ -10,9 +10,19 @@ Authors: Christopher Hoskin
 This file defines a notational class `has_abs` which adds the unary operator `abs` and the notation
 `|.|`. The concept of an absolute value occurs in lattice ordered groups and in GL and GM spaces.
 
+Mathematical structures possessing an absolute value often also possess a unique decomposition of
+elements into "positive" and "negative" parts which are in some sense "disjoint" (e.g. the Jordan
+decomposition of a measure). This file also defines `has_pos_part` and `has_neg_part` classes
+which add unary operators `pos` and `neg`, representing the maps taking an element to its positive
+and negative part respectively along with the notation ⁺ and ⁻.
+
 ## Notations
 
-The notation `|.|` is introduced for the absolute value.
+The following notation is introduced:
+
+* `|.|` for the absolute value;
+* `.⁺` for the positive part;
+* `.⁻` for the negative part.
 
 ## Tags
 

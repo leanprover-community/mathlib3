14db34b285bea269a0437be9842963f00c5ed0fb
Riccardo Brasca <riccardo.brasca@gmail.com>
2021-11-23 17:52:05 +0100

    fix build

--- src/ring_theory/adjoin/basic.lean
+++ src/ring_theory/adjoin/basic.lean
@@ -79,7 +79,7 @@ end
 
 lemma adjoin_adjoin_coe_preimage {s : set A} : adjoin R ((coe : adjoin R s → A) ⁻¹' s) = ⊤ :=
 begin
-  refine eq_top_iff.2 (λ x hx, adjoin_induction' (λ a ha, _) (λ r, _) (λ _ _, _) (λ _ _, _) x),
+  refine eq_top_iff.2 (λ x, adjoin_induction' (λ a ha, _) (λ r, _) (λ _ _, _) (λ _ _, _) x),
   { exact subset_adjoin ha },
   { exact subalgebra.algebra_map_mem _ r },
   { exact subalgebra.add_mem _ },

a481e2d60bb44b33545ed99fc3ea129621e6ee88
Eric <ericrboidi@gmail.com>
2021-11-24 19:27:49 +0000

    feat(algebra/char_p/two): couple more lemmas

--- src/algebra/char_p/two.lean
+++ src/algebra/char_p/two.lean
@@ -4,6 +4,7 @@ Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Eric Wieser
 -/
 import algebra.char_p.basic
+import tactic.interval_cases
 
 /-!
 # Lemmas about rings of characteristic two
@@ -23,6 +24,34 @@ by rw [bit0, ←two_smul R x, two_eq_zero, zero_smul]
 
 lemma neg_eq [ring R] [char_p R 2] (x : R) : -x = x :=
 by rw [neg_eq_iff_add_eq_zero, ←two_smul R x, two_eq_zero, zero_smul]
+
+lemma one_eq_neg_iff {R} [ring R] [nontrivial R] : ring_char R = 2 ↔ (-1 : R) = 1 :=
+begin
+  refine ⟨λ h, @@neg_eq _ (ring_char.of_eq h) 1, λ h, _⟩,
+  rw [eq_comm, ←sub_eq_zero, sub_neg_eq_add, show (1 + 1 : R) = (1 + 1 : ℕ), by norm_cast] at h,
+  have := nat.le_of_dvd zero_lt_two (ring_char.dvd h),
+  interval_cases (ring_char R) with hrc,
+  { haveI := ring_char.eq_iff.mp hrc,
+    haveI := char_p.char_p_to_char_zero R,
+    apply false.elim (@two_ne_zero' R _ _ _ _),
+    exact_mod_cast h },
+  { have := @char_p.ring_char_ne_one R _ _, contradiction },
+  { assumption }
+end
+
+lemma nat.fact_prime_two : fact (nat.prime 2) := ⟨nat.prime_two⟩
+
+local attribute [instance] nat.fact_prime_two
+
+@[simp] lemma order_of_neg_one {R} [ring R] [nontrivial R] :
+  order_of (-1 : R) = if ring_char R = 2 then 1 else 2 :=
+begin
+  split_ifs,
+  { simpa [one_eq_neg_iff] using h },
+  apply order_of_eq_prime,
+  { simp },
+  simpa [one_eq_neg_iff] using h
+end
 
 lemma sub_eq_add [ring R] [char_p R 2] (x y : R) : x - y = x + y :=
 by rw [sub_eq_add_neg, neg_eq]

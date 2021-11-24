98d6ab551abb0783545976553de1ce81e5a3d7d5
Oliver Nash <github@olivernash.org>
2021-11-24 12:30:06 +0000

    Fix long line

--- src/linear_algebra/affine_space/basis.lean
+++ src/linear_algebra/affine_space/basis.lean
@@ -194,7 +194,7 @@ noncomputable def coords : P →ᵃ[k] ι → k :=
   linear    :=
     { to_fun    := λ v i, -(b.basis_of i).sum_coords v,
       map_add'  := λ v w, by { ext i, simp only [linear_map.map_add, pi.add_apply, neg_add], },
-      map_smul' := λ t v, by { ext i, simpa only [linear_map.map_smul, pi.smul_apply, smul_neg], }, },
+      map_smul' := λ t v, by { ext i, simpa only [linear_map.map_smul, pi.smul_apply, smul_neg] } },
   map_vadd' := λ p v, by
     { ext i,
       simp only [add_left_inj, vadd_eq_add, pi.vadd_apply', pi.smul_apply, pi.neg_apply,

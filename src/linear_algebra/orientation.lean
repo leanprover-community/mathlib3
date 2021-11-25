c6a11013baa60122c484db5ad73834aa9c2dff79
Joseph Myers <jsm@polyomino.org.uk>
2021-11-25 19:50:47 +0000

    Remove unused arguments to same_ray_setoid_eq_orbit_rel as per linter.

--- src/linear_algebra/orientation.lean
+++ src/linear_algebra/orientation.lean
@@ -393,7 +393,7 @@ begin
 end
 
 /-- `same_ray_setoid` equals `mul_action.orbit_rel` for the `units.pos_subgroup`. -/
-lemma same_ray_setoid_eq_orbit_rel (v₁ v₂ : M) :
+lemma same_ray_setoid_eq_orbit_rel :
   same_ray_setoid R M = mul_action.orbit_rel (units.pos_subgroup R) M :=
 setoid.ext' $ same_ray_iff_orbit_rel R
 

4f9e8affee3ebb008d21c0a4ad7f6003bd38f158
Vierkantor <vierkantor@vierkantor.com>
2021-11-25 17:34:55 +0000

    Delete `set_like` dependency
    This requires redefining `has_quotient.quotient` to make `A` explicit,
    presumably because `A` doesn't actually appear anywhere in the type of
    `quotient'` (or is it due to `A` being an `out_param`?)

--- src/algebra/quotient.lean
+++ src/algebra/quotient.lean
@@ -32,7 +32,22 @@ ideal quotient, quotient ring
 
 universes u v
 
-class has_quotient (A : Type u) (B : Type v) [set_like B A] :=
-(quotient : B → Type (max u v))
+/-- `has_quotient A B` is a notation typeclass that allows us to write `A /// b` for `b : B`.
+This allows the usual notation for quotients of algebraic structures,
+such as groups, modules and rings.
+
+`A` is a parameter, despite being unused in the definition below, so it appears in the notation.
+-/
+class has_quotient (A : out_param $ Type u) (B : Type v) :=
+(quotient' : B → Type (max u v))
+
+/-- `has_quotient.quotient A b` (with notation `A /// b`) is the quotient of the type `A` by `b`.
+
+`A` is a parameter, despite being unused in the definition below, so it appears in the notation.
+-/
+@[nolint has_inhabited_instance] -- Will be provided by e.g. `ideal.quotient.inhabited`
+def has_quotient.quotient (A : out_param $ Type u) {B : Type v} [has_quotient A B] (b : B) :
+  Type (max u v) :=
+has_quotient.quotient' b
 
 notation G ` /// `:35 H:34 := has_quotient.quotient G H

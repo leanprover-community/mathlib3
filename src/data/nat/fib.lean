9762d40bf63bb0cc28e2f9c9a4fe94ac693ad64d
Yury G. Kudryashov <urkud@urkud.name>
2021-11-25 02:42:29 -0500

    Snapshot

--- src/data/nat/fib.lean
+++ src/data/nat/fib.lean
@@ -4,7 +4,6 @@ Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Kevin Kappelmann
 -/
 import data.nat.gcd
-import data.stream.init
 import tactic.ring
 
 /-!
@@ -34,12 +33,6 @@ fib, fibonacci
 
 namespace nat
 
-/-- Auxiliary function used in the definition of `fib_aux_stream`. -/
-private def fib_aux_step : (ℕ × ℕ) → (ℕ × ℕ) := λ p, ⟨p.snd, p.fst + p.snd⟩
-
-/-- Auxiliary stream creating Fibonacci pairs `⟨Fₙ, Fₙ₊₁⟩`. -/
-private def fib_aux_stream : stream (ℕ × ℕ) := stream.iterate fib_aux_step ⟨0, 1⟩
-
 /--
 Implementation of the fibonacci sequence satisfying
 `fib 0 = 0, fib 1 = 1, fib (n + 2) = fib n + fib (n + 1)`.
@@ -48,70 +41,59 @@ Implementation of the fibonacci sequence satisfying
 implementation.
 -/
 @[pp_nodot]
-def fib (n : ℕ) : ℕ := (fib_aux_stream n).fst
+def fib (n : ℕ) : ℕ := ((λ p : ℕ × ℕ, (p.snd, p.fst + p.snd))^[n] (0, 1)).fst
 
 @[simp] lemma fib_zero : fib 0 = 0 := rfl
 @[simp] lemma fib_one : fib 1 = 1 := rfl
 @[simp] lemma fib_two : fib 2 = 1 := rfl
 
-private lemma fib_aux_stream_succ {n : ℕ} :
-    fib_aux_stream (n + 1) = fib_aux_step (fib_aux_stream n) :=
-begin
-  change (stream.nth (n + 1) $ stream.iterate fib_aux_step ⟨0, 1⟩) =
-      fib_aux_step (stream.nth n $ stream.iterate fib_aux_step ⟨0, 1⟩),
-  rw [stream.nth_succ_iterate, stream.map_iterate, stream.nth_map]
-end
-
 /-- Shows that `fib` indeed satisfies the Fibonacci recurrence `Fₙ₊₂ = Fₙ + Fₙ₊₁.` -/
-lemma fib_succ_succ {n : ℕ} : fib (n + 2) = fib n + fib (n + 1) :=
-by simp only [fib, fib_aux_stream_succ, fib_aux_step]
+lemma fib_add_two {n : ℕ} : fib (n + 2) = fib n + fib (n + 1) :=
+by simp only [fib, function.iterate_succ']
 
-lemma fib_pos {n : ℕ} (n_pos : 0 < n) : 0 < fib n :=
-begin
-  induction n with n IH,
-  case nat.zero { norm_num at n_pos },
-  case nat.succ
-  { cases n,
-    case nat.zero { simp [fib_succ_succ, zero_lt_one] },
-    case nat.succ
-    { have : 0 ≤ fib n, by simp,
-      exact (lt_add_of_nonneg_of_lt this $ IH n.succ_pos) }}
-end
-
-lemma fib_le_fib_succ {n : ℕ} : fib n ≤ fib (n + 1) := by { cases n; simp [fib_succ_succ] }
+lemma fib_le_fib_succ {n : ℕ} : fib n ≤ fib (n + 1) := by { cases n; simp [fib_add_two] }
 
 @[mono] lemma fib_mono : monotone fib :=
 monotone_nat_of_le_succ $ λ _, fib_le_fib_succ
 
+lemma fib_pos {n : ℕ} (n_pos : 0 < n) : 0 < fib n :=
+calc 0 < fib 1 : dec_trivial
+   ... ≤ fib n : fib_mono n_pos
+
+lemma fib_lt_fib_succ {n : ℕ} (hn : 2 ≤ n) : fib n < fib (n + 1) :=
+begin
+  rcases le_iff_exists_add.1 hn with ⟨n, rfl⟩,
+  simp only [add_comm 2, fib_add_two], rw add_comm,
+  exact lt_add_of_pos_left _ (fib_pos succ_pos')
+end
+
 /-- `fib (n + 2)` is strictly monotone. -/
 lemma fib_add_two_strict_mono : strict_mono (λ n, fib (n + 2)) :=
-strict_mono_nat_of_lt_succ $ λ n, lt_add_of_pos_left _ $ fib_pos succ_pos'
+begin
+  refine strict_mono_nat_of_lt_succ (λ n, _),
+  rw add_right_comm,
+  exact fib_lt_fib_succ (self_le_add_left _ _)
+end
 
 lemma le_fib_self {n : ℕ} (five_le_n : 5 ≤ n) : n ≤ fib n :=
 begin
   induction five_le_n with n five_le_n IH,
-  { have : 5 = fib 5, by refl,  -- 5 ≤ fib 5
-    exact le_of_eq this },
+  { -- 5 ≤ fib 5
+    refl },
   { -- n + 1 ≤ fib (n + 1) for 5 ≤ n
-    cases n with n', -- rewrite n = succ n' to use fib.succ_succ
-    { have : 5 = 0, from nat.le_zero_iff.elim_left five_le_n, contradiction },
-    rw fib_succ_succ,
-    suffices : 1 + (n' + 1) ≤ fib n' + fib (n' + 1), by rwa [nat.succ_eq_add_one, add_comm],
-    have : n' ≠ 0, by { intro h, have : 5 ≤ 1, by rwa h at five_le_n, norm_num at this },
-    have : 1 ≤ fib n', from nat.succ_le_of_lt (fib_pos $ pos_iff_ne_zero.mpr this),
-    mono }
+    rw succ_le_iff,
+    calc n ≤ fib n       : IH
+       ... < fib (n + 1) : fib_lt_fib_succ (le_trans dec_trivial five_le_n) }
 end
 
 /-- Subsequent Fibonacci numbers are coprime,
   see https://proofwiki.org/wiki/Consecutive_Fibonacci_Numbers_are_Coprime -/
 lemma fib_coprime_fib_succ (n : ℕ) : nat.coprime (fib n) (fib (n + 1)) :=
 begin
-  unfold coprime,
   induction n with n ih,
   { simp },
-  { convert ih using 1,
-    rw [fib_succ_succ, succ_eq_add_one, gcd_rec, add_mod_right, gcd_comm (fib n),
-      gcd_rec (fib (n + 1))], }
+  { rw [fib_add_two, coprime_add_self_right],
+    exact ih.symm }
 end
 
 /-- See https://proofwiki.org/wiki/Fibonacci_Number_in_terms_of_Smaller_Fibonacci_Numbers -/
@@ -123,7 +105,7 @@ begin
   { intros,
     specialize ih (m + 1),
     rw [add_assoc m 1 n, add_comm 1 n] at ih,
-    simp only [fib_succ_succ, ← ih],
+    simp only [fib_add_two, ← ih],
     ring, }
 end
 

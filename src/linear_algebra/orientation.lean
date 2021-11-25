264cde31fe33e9b55690a84abe347f7a651926ae
Joseph Myers <jsm@polyomino.org.uk>
2021-11-25 13:29:54 +0000

    Linter says module.ray.ind should be a lemma not a def.

--- src/linear_algebra/orientation.lean
+++ src/linear_algebra/orientation.lean
@@ -132,7 +132,7 @@ variables {M}
 protected def ray_of_ne_zero [nontrivial R] (v : M) (h : v ≠ 0) : module.ray R M :=
 ⟦⟨v, h⟩⟧
 
-def module.ray.ind [nontrivial R] {C : module.ray R M → Prop}
+lemma module.ray.ind [nontrivial R] {C : module.ray R M → Prop}
   (h : Π v (hv : v ≠ 0), C (ray_of_ne_zero R v hv)) (x : module.ray R M) : C x :=
 quotient.ind (subtype.rec $ by exact h) x
 

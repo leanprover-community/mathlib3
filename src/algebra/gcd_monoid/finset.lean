cd3ae5843ebdbaaacdaf3719e5c87f4c4ea2b5ab
Eric <ericrboidi@gmail.com>
2021-11-26 22:45:10 +0000

    feat(algebra/gcd_monoid/*): assorted lemmas

--- src/algebra/gcd_monoid/finset.lean
+++ src/algebra/gcd_monoid/finset.lean
@@ -147,6 +147,8 @@ dvd_gcd (λ b hb, (gcd_dvd hb).trans (h b hb))
 lemma gcd_mono (h : s₁ ⊆ s₂) : s₂.gcd f ∣ s₁.gcd f :=
 dvd_gcd $ assume b hb, gcd_dvd (h hb)
 
+theorem gcd_eq_gcd_image [decidable_eq α] [is_idempotent α gcd_monoid.gcd] :
+  s.gcd f = (s.image f).gcd id := by simp [gcd, fold_image_idem]
 
 theorem gcd_eq_zero_iff : s.gcd f = 0 ↔ ∀ (x : β), x ∈ s → f x = 0 :=
 begin

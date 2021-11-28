0f43b9ffdb1b1778d9ed3ab8e49d11cd96b99a32
Heather Macbeth <25316162+hrmacbeth@users.noreply.github.com>
2021-11-28 00:32:52 -0500

    weird swap

--- src/analysis/normed_space/is_R_or_C.lean
+++ src/analysis/normed_space/is_R_or_C.lean
@@ -3,8 +3,8 @@ Copyright (c) 2021 Kalle Kytölä. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Kalle Kytölä
 -/
-import analysis.normed_space.operator_norm
 import data.complex.is_R_or_C
+import analysis.normed_space.operator_norm
 
 /-!
 # Normed spaces over R or C

eb3c0077402daafd38eed03e5e6c184514e54b0a
Stuart Presnell <stuart.presnell@gmail.com>
2021-11-30 10:57:37 +0000

    Removed 0 < k from `prime_factorization_prime_pos_pow`

--- src/data/nat/prime_factorization.lean
+++ src/data/nat/prime_factorization.lean
@@ -53,10 +53,11 @@ by simp only [support_prime_factorization, list.mem_to_finset]
 lemma prime_factorization_eq_nil_iff (n : ℕ) : n.prime_factorization = 0 ↔ n = 0 ∨ n = 1 :=
 by simp [prime_factorization, add_equiv.map_eq_zero_iff, multiset.coe_eq_zero]
 
-/-- For prime `p` and `k > 0`, the only prime factor of `p^k` is `p` with multiplicity `k` -/
-@[simp] lemma prime_factorization_prime_pos_pow {p k : ℕ} (hp : prime p) (hk : 0 < k) :
+/-- For prime `p` the only prime factor of `p^k` is `p` with multiplicity `k` -/
+@[simp] lemma prime_factorization_prime_pos_pow {p k : ℕ} (hp : prime p) :
   prime_factorization (p^k) = single p k :=
 begin
+  rcases k.eq_zero_or_pos with rfl | hk, { simp },
   rw eq_single_iff,
   split,
   { simp only [support_prime_factorization, prime_pow_prime_divisor hk hp, finset.subset.refl] },
@@ -66,6 +67,6 @@ end
 /-- The only prime factor of prime `p` is `p` itself, with multiplicity 1 -/
 @[simp] lemma prime_factorization_prime {p : ℕ} (hp : prime p) :
   p.prime_factorization = single p 1 :=
-by simp only [←prime_factorization_prime_pos_pow hp one_pos, pow_one]
+by simp only [←prime_factorization_prime_pos_pow hp, pow_one]
 
 end nat

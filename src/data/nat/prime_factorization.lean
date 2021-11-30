61571099443e5692916b9e1e726cdb2dcee13046
Stuart Presnell <stuart.presnell@gmail.com>
2021-11-30 20:28:38 +0000

    Formatting edits

--- src/data/nat/prime_factorization.lean
+++ src/data/nat/prime_factorization.lean
@@ -101,28 +101,33 @@ lemma prime_factorization_mul_add_of_coprime {a b : ℕ} (hab : coprime a b) :
   (a * b).prime_factorization = a.prime_factorization + b.prime_factorization :=
 begin
   ext q,
-  simp only [finsupp.coe_add, pi.add_apply, prime_factorization_count, count_factors_mul_of_coprime hab],
+  simp only [finsupp.coe_add, pi.add_apply, prime_factorization_count],
+  simp only [count_factors_mul_of_coprime hab],
 end
 
--- -- /-- For positive `a` and `b`, the power of `p` in `a * b` is the sum of the powers in `a` and `b` -/
--- -- lemma prime_factorization_mul_add_of_pos {a b : ℕ}  (ha : 0 < a) (hb : 0 < b) :
--- --   (a * b).prime_factorization = a.prime_factorization + b.prime_factorization :=
--- -- begin
--- --   ext q,
--- --   simp only [finsupp.coe_add, pi.add_apply, prime_factorization_count, count_factors_mul_of_pos ha hb],
--- -- end
+/-- For positive `a` and `b`, the power of `p` in `a * b` is the sum of the powers in `a` and `b` -/
+lemma prime_factorization_mul_add_of_pos {a b : ℕ}  (ha : 0 < a) (hb : 0 < b) :
+  (a * b).prime_factorization = a.prime_factorization + b.prime_factorization :=
+begin
+  ext q,
+  simp only [finsupp.coe_add, pi.add_apply, prime_factorization_count],
+  -- library_search,
+  -- simp only [count_factors_mul_of_pos ha hb],
+  sorry,
+end
 
--- /-- For coprime `a` and `b` the prime factorization `a * b` is the union of those of `a` and `b` -/
--- lemma prime_factorization_union_of_coprime {a b : ℕ} (hab : coprime a b) :
---   (a * b).prime_factorization.support = a.prime_factorization.support ∪ b.prime_factorization.support
---   :=
--- begin
---   rw prime_factorization_mul_add_of_coprime hab,
---   exact support_add_eq (prime_factorization_disjoint_of_coprime hab),
--- end
+/-- For coprime `a` and `b` the prime factorization `a * b` is the union of those of `a` and `b` -/
+lemma prime_factorization_union_of_coprime {a b : ℕ} (hab : coprime a b) :
+  (a * b).prime_factorization.support =
+    a.prime_factorization.support ∪ b.prime_factorization.support
+  :=
+begin
+  rw prime_factorization_mul_add_of_coprime hab,
+  exact support_add_eq (prime_factorization_disjoint_of_coprime hab),
+end
 
--- ---------------------------------------------------------------------------------------------------
--- ---------------------------------------------------------------------------------------------------
+---------------------------------------------------------------------------------------------------
+---------------------------------------------------------------------------------------------------
 
 /-- If a product over `n.prime_factorization` doesn't use the multiplicities of the prime factors
 then it's equal to the corresponding product over `n.factors.to_finset` -/
@@ -130,15 +135,14 @@ lemma rebase_prod_prime_factorization {n : ℕ} {β : Type*} [comm_monoid β] (f
   n.prime_factorization.prod (λ p k, f p) = ∏ p in n.factors.to_finset, (f p) :=
 by { apply prod_congr support_prime_factorization, simp }
 
--- ---------------------------------------------------------------------------------------------------
--- ---------------------------------------------------------------------------------------------------
+---------------------------------------------------------------------------------------------------
+---------------------------------------------------------------------------------------------------
 
-/-- For disjoint `f1` and `f2`, and function `g` with `∀ a, g a 0 = 1`,
+/-- For disjoint `f1` and `f2`, and function `g`
 the product of `g x (f1 x + f2 x))` over `f1.support` equals the product of `g` over `f1.support` -/
 lemma disjoint_prod_add_aux {f1 f2 : ℕ →₀ ℕ} (hd : disjoint f1.support f2.support)
-  {β : Type*} [comm_monoid β] {g : ℕ → ℕ → β} : --(hg_zero : ∀ (a : ℕ), g a 0 = 1) :
-(∏ (x : ℕ) in f1.support, g x (f1 x + f2 x)) = f1.prod g
-:=
+  {β : Type*} [comm_monoid β] {g : ℕ → ℕ → β} :
+(∏ (x : ℕ) in f1.support, g x (f1 x + f2 x)) = f1.prod g :=
 begin
   unfold finsupp.prod,
   rw prod_congr rfl,
@@ -147,24 +151,23 @@ begin
 end
 
 lemma disjoint_prod_add {f1 f2 : ℕ →₀ ℕ} (hd : disjoint f1.support f2.support)
-  {β : Type*} [comm_monoid β] {g : ℕ → ℕ → β} : -- (hg_zero : ∀ (a : ℕ), g a 0 = 1) :
-  f1.prod g * f2.prod g = (f1 + f2).prod g
-:=
+  {β : Type*} [comm_monoid β] {g : ℕ → ℕ → β} :
+  f1.prod g * f2.prod g = (f1 + f2).prod g :=
 begin
   rw [←disjoint_prod_add_aux hd, ←disjoint_prod_add_aux (disjoint.comm.mp hd)],
   simp only [add_comm, finsupp.prod, support_add_eq hd, prod_union hd, add_apply],
 end
 
--- ---------------------------------------------------------------------------------------------------
--- ---------------------------------------------------------------------------------------------------
+---------------------------------------------------------------------------------------------------
+---------------------------------------------------------------------------------------------------
 
--- /-- For any multiplicative function `f` with `f 1 = 1` and any `n > 0`, we can evaluate `f n` by evaluating `f` at `p ^ k` over the prime factorization of `n` -/
+/-- For any multiplicative function `f` with `f 1 = 1` and any `n > 0`,
+we can evaluate `f n` by evaluating `f` at `p ^ k` over the prime factorization of `n` -/
 lemma multiplicative_factorization {n : ℕ} {β : Type*} [comm_monoid β] {f : ℕ → β}
   (hn : 0 < n)
   (h_mult : ∀ x y : ℕ, coprime x y → f(x * y) = f x * f y)
   (hf : f 1 = 1) :
-f n = n.prime_factorization.prod (λ p k, f(p ^ k))
-:=
+f n = n.prime_factorization.prod (λ p k, f(p ^ k)) :=
 begin
   apply @nat.rec_on_pos_prime_coprime
     (λ n, (0 < n) → (f n = n.prime_factorization.prod (λ p k, f(p ^ k)))),
@@ -179,7 +182,7 @@ begin
   { simp },
 
   -- Case for 1
-    { rintros -, rw [prime_factorization_one, hf], simp },
+  { rintros -, rw [prime_factorization_one, hf], simp },
 
   -- Case for coprime a and b
   { intros a b hab ha hb hab_pos,
@@ -187,26 +190,24 @@ begin
     rw ha (pos_of_mul_pos_right hab_pos (b.zero_le)),
     rw hb (pos_of_mul_pos_left hab_pos (a.zero_le)),
     rw prime_factorization_mul_add_of_coprime hab,
-    apply disjoint_prod_add (prime_factorization_disjoint_of_coprime hab),
-    -- simpa using hf,
-  },
+    apply disjoint_prod_add (prime_factorization_disjoint_of_coprime hab) },
 
   exact hn,
 end
 
 ---------------------------------------------------------------------------------------------------
 
--- /-- For `n > 0`, the product of `p_i ^ k_i` over the prime factorization of `n` equals `n` -/
+/-- For `n > 0`, the product of `p_i ^ k_i` over the prime factorization of `n` equals `n` -/
 lemma prime_factorization_prod_pow {n : ℕ} (hn : 0 < n) :
   n = n.prime_factorization.prod pow :=
 by { refine (multiplicative_factorization hn _ _), simp }
 
 ---------------------------------------------------------------------------------------------------
 
--- /-- If the prime_factorization of `n` contains just one prime `p` then `n` is a power of `p` -/
--- lemma prime_pow_of_prime_factorization_single {n p k : ℕ} (hn : 0 < n)
---   (h : n.prime_factorization = single p k) : n = p ^ k :=
--- by { rw [prime_factorization_prod_pow hn, h], simp }
+/-- If the prime_factorization of `n` contains just one prime `p` then `n` is a power of `p` -/
+lemma prime_pow_of_prime_factorization_single {n p k : ℕ} (hn : 0 < n)
+  (h : n.prime_factorization = single p k) : n = p ^ k :=
+by { rw [prime_factorization_prod_pow hn, h], simp }
 
 ---------------------------------------------------------------------------------------------------
 

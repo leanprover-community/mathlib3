e3a41ac3c4d605b7a59e06c6ab8947cebcfc9cbd
Riccardo Brasca <riccardo.brasca@gmail.com>
2021-11-30 10:59:38 +0100

    rename discriminant to discr

--- src/ring_theory/discriminant.lean
+++ src/ring_theory/discriminant.lean
@@ -16,25 +16,25 @@ Given an `A`-algebra `B` and `b`, an `ι`-indexed family of elements of `B`, we 
 `b i * b j`.
 
 ## Main definition
-* `discriminant A b` : the discriminant of `b : ι → B`.
+* `discr A b` : the discriminant of `b : ι → B`.
 
 ## Main results
-* `zero_of_not_linear_independent` : if `b` is not linear independent, then
-  `discriminant A b = 0`.
-* `of_matrix_vec_mul` and `of_matrix_mul_vec` : formulas relating `discriminant A ι b` with
-  `discriminant A ((P.map (algebra_map A B)).vec_mul b)` and
-  `discriminant A ((P.map (algebra_map A B)).mul_vec b)`.
-* `not_zero_of_linear_independent` : over a field, if b` is linear independent, then
-  `discriminant K b ≠ 0`.
-* `eq_det_embeddings_matrix_reindex_pow_two` if `L/K` is a fields extension and `b : ι → L`, then
-  `discriminant K b` is the square of the determinant of the matrix whose `(i, j)` coefficient is
+* `discr_zero_of_not_linear_independent` : if `b` is not linear independent, then
+  `discr A b = 0`.
+* `discr_of_matrix_vec_mul` and `discr_of_matrix_mul_vec` : formulas relating `discr A ι b` with
+  `discr A ((P.map (algebra_map A B)).vec_mul b)` and
+  `discr A ((P.map (algebra_map A B)).mul_vec b)`.
+* `discr_not_zero_of_linear_independent` : over a field, if b` is linear independent, then
+  `discr K b ≠ 0`.
+* `discr_eq_det_embeddings_matrix_reindex_pow_two` if `L/K` is a fields extension and `b : ι → L`,
+  then `discr K b` is the square of the determinant of the matrix whose `(i, j)` coefficient is
   `σⱼ (b i)`, where `σⱼ : L →ₐ[K] E` is the embedding in an algebraically closed field `E`
   corresponding to `j : ι` via a bijection `e : ι ≃ (L →ₐ[K] E)`.
-* `of_power_basis_eq_prod` : the discriminant of a power basis.
+* `discr_of_power_basis_eq_prod` : the discriminant of a power basis.
 
 ## Implementation details
 Our definition works for any `A`-algebra `B`, but note that if `B` is not free as an `A`-module,
-then `trace A B = 0` by definition, so `discriminant A b = 0` for any `b`.
+then `trace A B = 0` by definition, so `discr A b = 0` for any `b`.
 -/
 
 universes u v w z
@@ -118,23 +118,23 @@ by { ext i j, simp [embeddings_matrix_reindex, embeddings_matrix] }
 
 end matrix
 
-section discriminant
+section discr
 
 /-- Given an `A`-algebra `B` and `b`, an `ι`-indexed family of elements of `B`, we define
-`discriminant A ι b` as the determinant of `trace_matrix A ι b`. -/
+`discr A ι b` as the determinant of `trace_matrix A ι b`. -/
 noncomputable
-def discriminant (A : Type u) {B : Type v} [comm_ring A] [comm_ring B] [algebra A B] [fintype ι]
+def discr (A : Type u) {B : Type v} [comm_ring A] [comm_ring B] [algebra A B] [fintype ι]
   (b : ι → B) := by { classical, exact (trace_matrix A b).det }
 
-lemma discriminant_def [decidable_eq ι] [fintype ι] (b : ι → B) :
-  discriminant A b = (trace_matrix A b).det := by convert rfl
+lemma discr_def [decidable_eq ι] [fintype ι] (b : ι → B) :
+  discr A b = (trace_matrix A b).det := by convert rfl
 
 variable [fintype ι]
 
 section basic
 
 lemma discr_zero_of_not_linear_independent [is_domain A] {b : ι → B}
-  (hli : ¬linear_independent A b) : discriminant A b = 0 :=
+  (hli : ¬linear_independent A b) : discr A b = 0 :=
 begin
   classical,
   obtain ⟨g, hg, i, hi⟩ := fintype.not_linear_independent_iff.1 hli,
@@ -145,22 +145,22 @@ begin
     simp only [mul_vec, dot_product, trace_matrix, pi.zero_apply, trace_form_apply,
       λ j, this j, ← linear_map.map_sum, ← sum_mul, hg, zero_mul, linear_map.map_zero] },
   by_contra h,
-  rw discriminant_def at h,
+  rw discr_def at h,
   simpa [matrix.eq_zero_of_mul_vec_eq_zero h this] using hi,
 end
 
 variable {A}
 
 lemma discr_of_matrix_vec_mul [decidable_eq ι] (b : ι → B) (P : matrix ι ι A) :
-  discriminant A ((P.map (algebra_map A B)).vec_mul b) = P.det ^ 2 * discriminant A b :=
-by rw [discriminant_def, trace_matrix_of_matrix_vec_mul, det_mul, det_mul, det_transpose, mul_comm,
-    ← mul_assoc, discriminant_def, pow_two]
+  discr A ((P.map (algebra_map A B)).vec_mul b) = P.det ^ 2 * discr A b :=
+by rw [discr_def, trace_matrix_of_matrix_vec_mul, det_mul, det_mul, det_transpose, mul_comm,
+    ← mul_assoc, discr_def, pow_two]
 
 
 lemma discr_of_matrix_mul_vec [decidable_eq ι] (b : ι → B) (P : matrix ι ι A) :
-  discriminant A ((P.map (algebra_map A B)).mul_vec b) = P.det ^ 2 * discriminant A b :=
-by rw [discriminant_def, trace_matrix_of_matrix_mul_vec, det_mul, det_mul, det_transpose,
-  mul_comm, ← mul_assoc, discriminant_def, pow_two]
+  discr A ((P.map (algebra_map A B)).mul_vec b) = P.det ^ 2 * discr A b :=
+by rw [discr_def, trace_matrix_of_matrix_mul_vec, det_mul, det_mul, det_transpose,
+  mul_comm, ← mul_assoc, discr_def, pow_two]
 
 end basic
 
@@ -172,11 +172,11 @@ variables [module.finite K L] [is_separable K L] [is_alg_closed E]
 variables (b : ι → L) (pb : power_basis K L)
 
 lemma discr_not_zero_of_linear_independent [nonempty ι]
-  (hcard : fintype.card ι = finrank K L) (hli : linear_independent K b) : discriminant K b ≠ 0 :=
+  (hcard : fintype.card ι = finrank K L) (hli : linear_independent K b) : discr K b ≠ 0 :=
 begin
   classical,
   have := span_eq_top_of_linear_independent_of_card_eq_finrank hli hcard,
-  rw [discriminant_def, trace_matrix_def],
+  rw [discr_def, trace_matrix_def],
   simp_rw [← basis.mk_apply hli this],
   rw [← trace_matrix_def, trace_matrix_of_basis, ← bilin_form.nondegenerate_iff_det_ne_zero],
   exact trace_form_nondegenerate _ _
@@ -195,13 +195,13 @@ by rw [trace_matrix_eq_embeddings_matrix_mul_trans, embeddings_matrix_reindex, r
   mul_transpose_eq_minor_mul_minor_transpose, ← equiv.coe_refl, equiv.refl_symm]
 
 lemma discr_eq_det_embeddings_matrix_reindex_pow_two [decidable_eq ι]
-  (e : ι ≃ (L →ₐ[K] E)) : algebra_map K E (discriminant K b) =
+  (e : ι ≃ (L →ₐ[K] E)) : algebra_map K E (discr K b) =
   (embeddings_matrix_reindex K E b e).det ^ 2 :=
-by rw [discriminant_def, ring_hom.map_det, ring_hom.map_matrix_apply,
+by rw [discr_def, ring_hom.map_det, ring_hom.map_matrix_apply,
     trace_matrix_eq_embeddings_matrix_reindex_mul_trans, det_mul, det_transpose, pow_two]
 
 lemma discr_of_power_basis_eq_prod (e : fin pb.dim ≃ (L →ₐ[K] E)) :
-  algebra_map K E (discriminant K pb.basis) =
+  algebra_map K E (discr K pb.basis) =
   ∏ i : fin pb.dim, ∏ j in finset.univ.filter (λ j, i < j), (e j pb.gen- (e i pb.gen)) ^ 2 :=
 begin
   rw [discr_eq_det_embeddings_matrix_reindex_pow_two K E pb.basis e,
@@ -212,6 +212,6 @@ end
 
 end field
 
-end discriminant
+end discr
 
 end algebra

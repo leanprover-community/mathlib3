2d83b2577e8c51d8d1ee1e4b910e1d265638e9c2
Anne Baanen <Vierkantor@users.noreply.github.com>
2021-12-02 14:53:25 +0000

    Fix overlong line

--- src/algebra/quotient.lean
+++ src/algebra/quotient.lean
@@ -24,7 +24,8 @@ The following notation is introduced:
 * `G /// H` stands for the quotient of the type `G` by some term `H` 
   (for example, `H` can be a normal subgroup of `G`).
   To implement this notation for other quotients, you should provide a `has_quotient` instance.
-  Note that since `G` can usually be inferred from `H`, `_ /// H` can also be used, but this is less readable.
+  Note that since `G` can usually be inferred from `H`, `_ /// H` can also be used,
+  but this is less readable.
 
 ## Tags
 

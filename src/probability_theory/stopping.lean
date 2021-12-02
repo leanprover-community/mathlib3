cb392076469bfb0c2a95340f687a0191e65083ca
Kexing <kexing.ying19@imperial.ac.uk>
2021-12-02 11:09:15 +0000

    remove import

--- src/probability_theory/stopping.lean
+++ src/probability_theory/stopping.lean
@@ -3,7 +3,6 @@ Copyright (c) 2021 Kexing Ying. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Kexing Ying
 -/
-import measure_theory.function.conditional_expectation
 import probability_theory.notation
 
 /-!

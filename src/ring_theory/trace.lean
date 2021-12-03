a773b3febf090b9801d740160ff3454635ba304c
Vierkantor <vierkantor@vierkantor.com>
2021-12-03 15:38:43 +0000

    Add docstrings

--- src/ring_theory/trace.lean
+++ src/ring_theory/trace.lean
@@ -20,6 +20,27 @@ Suppose we have an `R`-algebra `S` with a finite basis. For each `s : S`,
 the trace of the linear map given by multiplying by `s` gives information about
 the roots of the minimal polynomial of `s` over `R`.
 
+## Main definitions
+
+ * `algebra.trace R S x`: the trace of an element `s` of an `R`-algebra `S`
+ * `algebra.trace_form R S`: bilinear form sending `x`, `y` to the trace of `x * y`
+ * `algebra.trace_matrix R b`: the matrix whose `(i j)`-th element is the trace of `b i * b j`.
+ * `algebra.embeddings_matrix A C b : matrix κ (B →ₐ[A] C) C` is the matrix whose
+   `(i, σ)` coefficient is `σ (b i)`.
+ * `algebra.embeddings_matrix_reindex A C b e : matrix κ κ C` is the matrix whose `(i, j)`
+   coefficient is `σⱼ (b i)`, where `σⱼ : B →ₐ[A] C` is the embedding corresponding to `j : κ`
+   given by a bijection `e : κ ≃ (B →ₐ[A] C)`.
+
+## Main results
+
+ * `trace_algebra_map_of_basis`, `trace_algebra_map`: if `x : K`, then `Tr_{L/K} x = [L : K] x`
+ * `trace_trace_of_basis`, `trace_trace`: `Tr_{L/K} (Tr_{F/L} x) = Tr_{F/K} x`
+ * `trace_eq_sum_roots`: the trace of `x : K(x)` is the sum of all conjugate roots of `x`
+ * `trace_eq_sum_embeddings`: the trace of `x : K(x)` is the sum of all embeddings of `x` into an
+   algebraically closed field
+ * `trace_form_nondegenerate`: the trace form over a separable extension is a nondegenerate
+   bilinear form
+
 ## Implementation notes
 
 Typically, the trace is defined specifically for finite field extensions.
@@ -349,7 +370,7 @@ variables [comm_ring A] [comm_ring B] [algebra A B] [comm_ring C] [algebra A C]
 open finset
 
 /-- Given an `A`-algebra `B` and `b`, an `κ`-indexed family of elements of `B`, we define
-`trace_matrix A κ b` as the matrix whose `(i j)`-th element is the trace of `b i * b j`. -/
+`trace_matrix A b` as the matrix whose `(i j)`-th element is the trace of `b i * b j`. -/
 @[simp] noncomputable
 def trace_matrix (b : κ → B) : matrix κ κ A
 | i j := trace_form A B (b i) (b j)

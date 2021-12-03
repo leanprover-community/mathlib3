6094f3cd32d12924c1f0affbd7e2609e13ccaf8e
jjaassoonn <jujian.zhang1998@outlook.com>
2021-12-03 19:57:48 +0000

    added missing lemma

--- src/group_theory/subgroup/basic.lean
+++ src/group_theory/subgroup/basic.lean
@@ -2496,3 +2496,17 @@ end
 end add_subgroup
 
 end saturated
+
+section big_operators
+
+open_locale big_operators
+
+variables [comm_group A]
+variables (H : subgroup A)
+variables {ι : Type*} (p : ι → H)
+
+@[to_additive]
+lemma coe_finset_prod {s : finset ι} : ↑(∏ i in s, p i) = ∏ i in s, (p i : A) :=
+ H.subtype.map_prod p s
+
+end big_operators

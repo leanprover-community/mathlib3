c924625ddb4af89e5759db1366c4b0f347949031
Stuart Presnell <stuart.presnell@gmail.com>
2021-12-04 15:48:09 +0000

    Add third variant for `gcd_monoid`

--- src/algebra/gcd_monoid/basic.lean
+++ src/algebra/gcd_monoid/basic.lean
@@ -534,6 +534,14 @@ begin
   exact gcd_eq_normalize h (gcd_monoid.dvd_gcd hda hdb),
 end
 
+lemma gcd_greatest'' {α : Type*} [comm_cancel_monoid_with_zero α] [gcd_monoid α]
+  {a b d : α} (hda : d ∣ a) (hdb : d ∣ b)
+  (he : ∀ e : α, e ∣ a → e ∣ b → e ∣ d) : associated d (gcd_monoid.gcd a b) :=
+begin
+  have h := he _ (gcd_monoid.gcd_dvd_left a b) (gcd_monoid.gcd_dvd_right a b),
+  exact associated_of_dvd_dvd (gcd_monoid.dvd_gcd hda hdb) h,
+end
+
 end gcd
 
 section lcm

8b822096e4c8beebed02085ccdfe12e5e683abe6
Iván Sadofschi Costa <isadofschi@dm.uba.ar>
2021-12-04 11:06:27 -0300

    fix long line

--- src/data/multiset/basic.lean
+++ src/data/multiset/basic.lean
@@ -2043,7 +2043,8 @@ theorem count_ne_zero {a : α} {s : multiset α} : count a s ≠ 0 ↔ a ∈ s :
 by simp [ne.def, count_eq_zero]
 
 theorem one_le_count_iff_mem {x : α} {a : multiset α} : 1 ≤ a.count x ↔ x ∈ a :=
-⟨count_ne_zero.1 ∘ one_le_iff_ne_zero.1 , λ h, count_singleton_self x ▸ count_le_of_le x $ singleton_le.2 h⟩
+⟨count_ne_zero.1 ∘ one_le_iff_ne_zero.1 ,
+  λ h, count_singleton_self x ▸ count_le_of_le x $ singleton_le.2 h⟩
 
 @[simp] theorem count_repeat_self (a : α) (n : ℕ) : count a (repeat a n) = n :=
 by simp [repeat]

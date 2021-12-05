f8ede8b3f15470887f6b0149b8959cdec8cf5132
Eric Wieser <wieser.eric@gmail.com>
2021-12-05 14:14:02 +0000

    Add the missing multiset_prod and list_prod lemmas too
    Also should fix a lint error

--- src/group_theory/subgroup/basic.lean
+++ src/group_theory/subgroup/basic.lean
@@ -376,6 +376,19 @@ coe_subtype H ▸ monoid_hom.map_pow _ _ _
 @[simp, norm_cast, to_additive] lemma coe_zpow (x : H) (n : ℤ) : ((x ^ n : H) : G) = x ^ n :=
 coe_subtype H ▸ monoid_hom.map_zpow _ _ _
 
+@[simp, norm_cast, to_additive] theorem coe_list_prod (l : list H) :
+  (l.prod : G) = (l.map coe).prod :=
+H.to_submonoid.coe_list_prod l
+
+@[simp, norm_cast, to_additive] theorem coe_multiset_prod {G} [comm_group G] (S : subgroup G)
+  (m : multiset S) : (m.prod : G) = (m.map coe).prod :=
+S.to_submonoid.coe_multiset_prod m
+
+@[simp, norm_cast, to_additive] theorem coe_finset_prod {ι G} [comm_group G] (S : subgroup G)
+  (f : ι → S) (s : finset ι) :
+  ↑(∏ i in s, f i) = (∏ i in s, f i : G) :=
+S.to_submonoid.coe_finset_prod f s
+
 /-- The inclusion homomorphism from a subgroup `H` contained in `K` to `K`. -/
 @[to_additive "The inclusion homomorphism from a additive subgroup `H` contained in `K` to `K`."]
 def inclusion {H K : subgroup G} (h : H ≤ K) : H →* K :=
@@ -2496,17 +2509,3 @@ end
 end add_subgroup
 
 end saturated
-
-section big_operators
-
-open_locale big_operators
-
-variables [comm_group A]
-variables (H : subgroup A)
-variables {ι : Type*} (p : ι → H)
-
-@[to_additive]
-lemma subgroup.coe_finset_prod (s : finset ι) : ↑(∏ i in s, p i) = ∏ i in s, (p i : A) :=
-H.subtype.map_prod p s
-
-end big_operators

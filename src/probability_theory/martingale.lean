41055b58aec66740baf2311938c81723112b0763
RemyDegenne <Remydegenne@gmail.com>
2021-12-07 08:44:26 +0100

    change def

--- src/probability_theory/martingale.lean
+++ src/probability_theory/martingale.lean
@@ -17,12 +17,13 @@ The definitions of filtration and adapted can be found in `probability_theory.st
 
 ### Definitions
 
-* `is_martingale f ℱ μ`: `f` is a martingale with respect to filtration `ℱ` and measure `μ`.
+* `measure_theory.is_martingale f ℱ μ`: `f` is a martingale with respect to filtration `ℱ` and
+  measure `μ`.
 
 ### Results
 
-* `is_martingale_condexp f ℱ μ`: the sequence `λ i, μ[f | ℱ i, ℱ.le i])` is a martingale with
-  respect to `ℱ` and `μ`.
+* `measure_theory.is_martingale_condexp f ℱ μ`: the sequence `λ i, μ[f | ℱ i, ℱ.le i])` is a
+  martingale with respect to `ℱ` and `μ`.
 
 -/
 
@@ -37,39 +38,41 @@ variables {α E ι : Type*} [preorder ι] [measurable_space E]
   [second_countable_topology E]
   {f g : ι → α → E} {ℱ : filtration ι m0} [sigma_finite_filtration μ ℱ]
 
-/-- A family of functions `f : ι → α → E` is a martingale with respect to a filtration `ℱ` if every
-`f i` is integrable, `f` is adapted with respect to `ℱ` and for all `i ≤ j`,
-`μ[f j | ℱ.le i] =ᵐ[μ] f i`. -/
+/-- A family of functions `f : ι → α → E` is a martingale with respect to a filtration `ℱ` if `f`
+is adapted with respect to `ℱ` and for all `i ≤ j`, `μ[f j | ℱ.le i] =ᵐ[μ] f i`. -/
 def is_martingale (f : ι → α → E) (ℱ : filtration ι m0) (μ : measure α)
   [sigma_finite_filtration μ ℱ] : Prop :=
-(∀ i, integrable (f i) μ) ∧ (adapted ℱ f) ∧ ∀ i j, i ≤ j → μ[f j | ℱ i, ℱ.le i] =ᵐ[μ] f i
+adapted ℱ f ∧ ∀ i j, i ≤ j → μ[f j | ℱ i, ℱ.le i] =ᵐ[μ] f i
 
 variables (E)
 lemma is_martingale_zero (ℱ : filtration ι m0) (μ : measure α) [sigma_finite_filtration μ ℱ] :
   is_martingale (0 : ι → α → E) ℱ μ :=
-⟨λ i, integrable_zero _ _ _, adapted_zero E ℱ,
-  λ i j hij, by { rw [pi.zero_apply, condexp_zero], simp, }⟩
+⟨adapted_zero E ℱ, λ i j hij, by { rw [pi.zero_apply, condexp_zero], simp, }⟩
 variables {E}
 
 namespace is_martingale
 
-lemma integrable (hf : is_martingale f ℱ μ) : ∀ i, integrable (f i) μ := hf.1
+lemma adapted (hf : is_martingale f ℱ μ) : adapted ℱ f := hf.1
 
-lemma adapted (hf : is_martingale f ℱ μ) : adapted ℱ f := hf.2.1
+lemma measurable (hf : is_martingale f ℱ μ) (i : ι) : measurable[ℱ i] (f i) := hf.adapted i
+
+lemma condexp_ae_eq (hf : is_martingale f ℱ μ) {i j : ι} (hij : i ≤ j) :
+  μ[f j | ℱ i, ℱ.le i] =ᵐ[μ] f i :=
+hf.2 i j hij
+
+lemma integrable (hf : is_martingale f ℱ μ) (i : ι) : integrable (f i) μ :=
+integrable_condexp.congr (hf.condexp_ae_eq (le_refl i))
 
 lemma add (hf : is_martingale f ℱ μ) (hg : is_martingale g ℱ μ) :
   is_martingale (f + g) ℱ μ :=
 begin
-  refine ⟨λ i, (hf.integrable i).add (hg.integrable i), hf.adapted.add hg.adapted, λ i j hij, _⟩,
+  refine ⟨hf.adapted.add hg.adapted, λ i j hij, _⟩,
   exact (condexp_add (hf.integrable j) (hg.integrable j)).trans
-    ((hf.2.2 i j hij).add (hg.2.2 i j hij)),
+    ((hf.2 i j hij).add (hg.2 i j hij)),
 end
 
 lemma neg (hf : is_martingale f ℱ μ) : is_martingale (-f) ℱ μ :=
-begin
-  refine ⟨λ i, (hf.integrable i).neg, hf.adapted.neg, λ i j hij, _⟩,
-  exact (condexp_neg (f j)).trans ((hf.2.2 i j hij).neg),
-end
+⟨hf.adapted.neg, λ i j hij, (condexp_neg (f j)).trans ((hf.2 i j hij).neg)⟩
 
 lemma sub (hf : is_martingale f ℱ μ) (hg : is_martingale g ℱ μ) :
   is_martingale (f - g) ℱ μ :=
@@ -77,8 +80,8 @@ by { rw sub_eq_add_neg, exact hf.add hg.neg, }
 
 lemma smul (c : ℝ) (hf : is_martingale f ℱ μ) : is_martingale (c • f) ℱ μ :=
 begin
-  refine ⟨λ i, (hf.integrable i).smul c, hf.adapted.smul c, λ i j hij, _⟩,
-  refine (condexp_smul c (f j)).trans ((hf.2.2 i j hij).mono (λ x hx, _)),
+  refine ⟨hf.adapted.smul c, λ i j hij, _⟩,
+  refine (condexp_smul c (f j)).trans ((hf.2 i j hij).mono (λ x hx, _)),
   rw [pi.smul_apply, hx, pi.smul_apply, pi.smul_apply],
 end
 
@@ -87,6 +90,6 @@ end is_martingale
 lemma is_martingale_condexp (f : α → E) (ℱ : filtration ι m0) (μ : measure α)
   [sigma_finite_filtration μ ℱ] :
   is_martingale (λ i, μ[f | ℱ i, ℱ.le i]) ℱ μ :=
-⟨λ i, integrable_condexp, λ i, measurable_condexp, λ i j hij, condexp_condexp_of_le (ℱ.mono hij) _⟩
+⟨λ i, measurable_condexp, λ i j hij, condexp_condexp_of_le (ℱ.mono hij) _⟩
 
 end measure_theory

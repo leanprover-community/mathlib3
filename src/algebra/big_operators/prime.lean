1c33d79e0b3c50c222586348deb0187c40c978e5
Stuart Presnell <stuart.presnell@gmail.com>
2021-12-09 11:12:02 +0000

    Generalise the types of `f` and `g`

--- src/algebra/big_operators/prime.lean
+++ src/algebra/big_operators/prime.lean
@@ -20,11 +20,12 @@ prime numbers.
 
 open nat finset finsupp
 
-lemma nat.prime.dvd_finset_prod_iff {p : ℕ} {S : finset ℕ} (pp : prime p) (g : ℕ → ℕ) :
-  p ∣ S.prod g ↔ ∃ a ∈ S, p ∣ g a :=
+lemma nat.prime.dvd_finset_prod_iff {α : Type*} [decidable_eq α] {S : finset α} {p : ℕ}
+  (pp : prime p) (g : α → ℕ) :
+p ∣ S.prod g ↔ ∃ a ∈ S, p ∣ g a :=
 begin
   split,
-  { apply @finset.induction_on ℕ (λ S, p ∣ S.prod g → (∃ (a : ℕ) (H : a ∈ S), p ∣ g a)),
+  { apply @finset.induction_on α (λ S, p ∣ S.prod g → (∃ (a : α) (H : a ∈ S), p ∣ g a)),
     { intros hp,
       refine absurd _ not_prime_one,
       simp only [nat.dvd_one, finset.prod_empty] at hp,
@@ -37,11 +38,7 @@ begin
   { exact λ ⟨a, ha1, ha2⟩, dvd_trans ha2 (dvd_prod_of_mem g ha1) },
 end
 
-lemma nat.prime.dvd_finsupp_prod_iff {p : ℕ} {f: ℕ →₀ ℕ} (pp : prime p) :
-  p ∣ f.prod pow ↔ ∃ a ∈ f.support, p ∣ a ^ (f a) :=
-nat.prime.dvd_finset_prod_iff pp (λ x, x ^ (f x))
-
--- TODO: Generalise the types of `f` and `g`
-example {p : ℕ} {f: ℕ →₀ ℕ} {g : ℕ → ℕ → ℕ} (pp : prime p) :
-  p ∣ f.prod g ↔ ∃ a ∈ f.support, p ∣ g a (f a) :=
-nat.prime.dvd_finset_prod_iff pp (λ x, g x (f x))
+lemma nat.prime.dvd_finsupp_prod_iff {α M : Type*} [decidable_eq α] [has_zero M] {f: α →₀ M}
+  {g : α → M → ℕ} {p : ℕ} (pp : prime p) :
+p ∣ f.prod g ↔ ∃ a ∈ f.support, p ∣ g a (f a) :=
+nat.prime.dvd_finset_prod_iff pp _

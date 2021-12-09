319ca2e0910349679cb4db71c420c0fcd6d08a1f
Stuart Presnell <stuart.presnell@gmail.com>
2021-12-09 14:13:09 +0000

    Change `nat.prime p` to `p.prime`

--- src/algebra/big_operators/prime.lean
+++ src/algebra/big_operators/prime.lean
@@ -49,10 +49,10 @@ end comm_monoid_with_zero
 open nat
 
 lemma nat.prime.dvd_finset_prod_iff {α : Type*} [decidable_eq α] {S : finset α} {p : ℕ}
-  (pp : nat.prime p) (g : α → ℕ) : p ∣ S.prod g ↔ ∃ a ∈ S, p ∣ g a :=
+  (pp : p.prime) (g : α → ℕ) : p ∣ S.prod g ↔ ∃ a ∈ S, p ∣ g a :=
 prime.dvd_finset_prod_iff (prime_iff.mp pp) _
 
 lemma nat.prime.dvd_finsupp_prod_iff {α M : Type*} [decidable_eq α] [has_zero M] {f: α →₀ M}
-  {g : α → M → ℕ} {p : ℕ} (pp : nat.prime p) :
+  {g : α → M → ℕ} {p : ℕ} (pp : p.prime) :
 p ∣ f.prod g ↔ ∃ a ∈ f.support, p ∣ g a (f a) :=
 nat.prime.dvd_finset_prod_iff pp _

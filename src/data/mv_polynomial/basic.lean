7b405c33d2f0ba1f92a91ed8f449ef0dae7a910c
Iván Sadofschi Costa <isadofschi@dm.uba.ar>
2021-12-12 13:04:33 +0000

    remove unused argument m

--- src/data/mv_polynomial/basic.lean
+++ src/data/mv_polynomial/basic.lean
@@ -378,7 +378,7 @@ by rw [X, support_monomial, if_neg]; exact one_ne_zero
 
 @[simp] lemma support_zero : (0 : mv_polynomial σ R).support = ∅ := rfl
 
-lemma support_sum {α : Type*} {s : finset α} {f : α → mv_polynomial σ R} {m : σ →₀ ℕ} :
+lemma support_sum {α : Type*} {s : finset α} {f : α → mv_polynomial σ R} :
   (∑ x in s, f x).support ⊆ s.sup (λ x, (f x).support) :=
 begin
   induction s using finset.cons_induction_on with a s ha ih,

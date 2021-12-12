427bc233f3ab242a0fef755d575a1695f06f7297
YaelDillies <yael.dillies@gmail.com>
2021-12-12 14:11:06 +0100

    fix data.part

--- src/data/part.lean
+++ src/data/part.lean
@@ -4,6 +4,7 @@ Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Mario Carneiro, Jeremy Avigad, Simon Hudon
 -/
 import data.equiv.basic
+import data.set.basic
 
 /-!
 # Partial values of a type

9dc8f7db3c5cb36c33cd3bb043581a0f0f3980da
YaelDillies <yael.dillies@gmail.com>
2021-12-12 01:30:27 +0100

    fix data.finset.pairwise

--- src/order/sup_indep.lean
+++ src/order/sup_indep.lean
@@ -4,7 +4,6 @@ Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Yaël Dillies
 -/
 import data.finset.lattice
-import data.set.pairwise
 
 /-!
 # Finite supremum independence

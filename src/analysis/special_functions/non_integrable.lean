cbf32c4f3a6102e5b826934115e70e0b9099a5eb
Yury G. Kudryashov <urkud@urkud.name>
2021-12-13 02:55:12 -0500

    ...

--- src/analysis/special_functions/non_integrable.lean
+++ src/analysis/special_functions/non_integrable.lean
@@ -1,6 +1,12 @@
+/-
+Copyright (c) 2021 Yury Kudryashov. All rights reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
+Authors: Yury Kudryashov
+-/
 import analysis.special_functions.integrals
 
 /-!
+# 
 -/
 
 open_locale measure_theory topological_space interval

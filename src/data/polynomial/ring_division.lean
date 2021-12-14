9aa07481389e8c040227a6c1e74b90ea0307d4eb
Alex J. Best <alex.j.best@gmail.com>
2021-12-14 14:12:05 +0100

    lintfix

--- src/data/polynomial/ring_division.lean
+++ src/data/polynomial/ring_division.lean
@@ -426,9 +426,8 @@ calc ((roots ((X : polynomial R) ^ n - C a)).card : with_bot ℕ)
       ≤ degree ((X : polynomial R) ^ n - C a) : card_roots (X_pow_sub_C_ne_zero hn a)
   ... = n : degree_X_pow_sub_C hn a
 
-lemma le_root_multiplicity_map {K L : Type*} [comm_ring K] [is_domain K]
-  [comm_ring L] [is_domain L] {p : polynomial K} {f : K →+* L} (hf : function.injective f)
-  (hroots : p.roots.card = p.nat_degree) (a : K) :
+lemma le_root_multiplicity_map {K L : Type*} [comm_ring K]
+  [comm_ring L] {p : polynomial K} {f : K →+* L} (hf : function.injective f) (a : K) :
   root_multiplicity a p ≤ root_multiplicity (f a) (map f p) :=
 begin
   by_cases hp0 : p = 0, { simp only [hp0, root_multiplicity_zero, map_zero], },
@@ -441,14 +440,14 @@ begin
 end
 
 lemma count_map_roots {K L : Type*} [comm_ring K] [is_domain K]
-  [comm_ring L] [is_domain L] {p : polynomial K} {f : K →+* L} (hf : function.injective f)
-  (hroots : p.roots.card = p.nat_degree) (a : L) :
+  [comm_ring L] {p : polynomial K} {f : K →+* L} (hf : function.injective f)
+  (a : L) :
   count a (multiset.map f p.roots) ≤ root_multiplicity a (map f p) :=
 begin
   by_cases h : ∃ t, f t = a,
   { rcases h with ⟨h_w, rfl⟩,
     rw [multiset.count_map_eq_count' f _ hf, count_roots],
-    exact le_root_multiplicity_map hf hroots h_w },
+    exact le_root_multiplicity_map hf h_w },
   { suffices : multiset.count a (multiset.map f p.roots) = 0,
     { rw this, exact zero_le _, },
     rw [multiset.count_map, multiset.card_eq_zero, multiset.filter_eq_nil],
@@ -464,7 +463,7 @@ begin
   by_cases hp0 : p = 0, { simp only [hp0, roots_zero, multiset.map_zero, map_zero], },
   have hmap : map f p ≠ 0, { simpa only [map_zero] using (map_injective f hf).ne hp0, },
   apply multiset.eq_of_le_of_card_le,
-  { simpa only [multiset.le_iff_count, count_roots] using count_map_roots hf hroots },
+  { simpa only [multiset.le_iff_count, count_roots] using count_map_roots hf },
   { simpa only [multiset.card_map, hroots] using (card_roots' hmap).trans (nat_degree_map_le f p) },
 end
 

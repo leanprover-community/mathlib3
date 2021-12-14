464808515b70f6bc43dc21082d9de5b978cb3c20
Stuart Presnell <stuart.presnell@gmail.com>
2021-12-14 16:09:51 +0000

    Change `'prime_factorization` to `factorization` throughout

--- src/data/nat/prime_factorization.lean
+++ src/data/nat/prime_factorization.lean
@@ -9,11 +9,11 @@ import data.nat.mul_ind
 /-!
 # Prime factorizations
 
- `n.prime_factorization` is the finitely supported function `ℕ →₀ ℕ`
+ `n.factorization` is the finitely supported function `ℕ →₀ ℕ`
  mapping each prime factor of `n` to its multiplicity in `n`.  For example, since 2000 = 2^4 * 5^3,
-  * `prime_factorization 2000 2` is 4
-  * `prime_factorization 2000 5` is 3
-  * `prime_factorization 2000 k` is 0 for all other `k : ℕ`.
+  * `factorization 2000 2` is 4
+  * `factorization 2000 5` is 3
+  * `factorization 2000 k` is 0 for all other `k : ℕ`.
 -/
 
 open nat finset list finsupp
@@ -21,52 +21,52 @@ open_locale big_operators
 
 namespace nat
 
-/-- `n.prime_factorization` is the finitely supported function `ℕ →₀ ℕ`
+/-- `n.factorization` is the finitely supported function `ℕ →₀ ℕ`
  mapping each prime factor of `n` to its multiplicity in `n`. -/
-noncomputable def prime_factorization (n : ℕ) : ℕ →₀ ℕ := (n.factors : multiset ℕ).to_finsupp
+noncomputable def factorization (n : ℕ) : ℕ →₀ ℕ := (n.factors : multiset ℕ).to_finsupp
 
-lemma prime_factorization_eq_count {n p : ℕ} : n.prime_factorization p = list.count p n.factors :=
-by simp [prime_factorization]
+lemma factorization_eq_count {n p : ℕ} : n.factorization p = list.count p n.factors :=
+by simp [factorization]
 
 /-- Every positive natural number has a unique prime factorization -/
-lemma prime_factorization_eq_iff {a b : ℕ} (ha : 0 < a) (hb : 0 < b) :
-  a.prime_factorization = b.prime_factorization ↔ a = b :=
-⟨λ h, eq_of_count_factors_eq ha hb (λ p, by simp [←prime_factorization_eq_count, h]), λ h, by rw h⟩
+lemma factorization_eq_iff {a b : ℕ} (ha : 0 < a) (hb : 0 < b) :
+  a.factorization = b.factorization ↔ a = b :=
+⟨λ h, eq_of_count_factors_eq ha hb (λ p, by simp [←factorization_eq_count, h]), λ h, by rw h⟩
 
-@[simp] lemma prime_factorization_zero : prime_factorization 0 = 0  :=
-by simp [prime_factorization]
+@[simp] lemma factorization_zero : factorization 0 = 0  :=
+by simp [factorization]
 
-@[simp] lemma prime_factorization_one : prime_factorization 1 = 0 :=
-by simp [prime_factorization]
+@[simp] lemma factorization_one : factorization 1 = 0 :=
+by simp [factorization]
 
-/-- The support of `n.prime_factorization` is exactly `n.factors.to_finset` -/
-@[simp] lemma support_prime_factorization {n : ℕ} :
-  n.prime_factorization.support = n.factors.to_finset :=
-by simpa [prime_factorization, multiset.to_finsupp_support]
+/-- The support of `n.factorization` is exactly `n.factors.to_finset` -/
+@[simp] lemma support_factorization {n : ℕ} :
+  n.factorization.support = n.factors.to_finset :=
+by simpa [factorization, multiset.to_finsupp_support]
 
 lemma factor_iff_mem_factorization {n p : ℕ} :
-  (p ∈ n.prime_factorization.support) ↔ (p ∈ n.factors) :=
-by simp only [support_prime_factorization, list.mem_to_finset]
+  (p ∈ n.factorization.support) ↔ (p ∈ n.factors) :=
+by simp only [support_factorization, list.mem_to_finset]
 
 /-- The only numbers with empty prime factorization are `0` and `1` -/
-lemma prime_factorization_eq_nil_iff (n : ℕ) : n.prime_factorization = 0 ↔ n = 0 ∨ n = 1 :=
-by simp [prime_factorization, add_equiv.map_eq_zero_iff, multiset.coe_eq_zero]
+lemma factorization_eq_nil_iff (n : ℕ) : n.factorization = 0 ↔ n = 0 ∨ n = 1 :=
+by simp [factorization, add_equiv.map_eq_zero_iff, multiset.coe_eq_zero]
 
 /-- For any `p`, the power of `p` in `n^k` is `k` times the power in `n` -/
-lemma prime_factorization_pow {n k : ℕ} :
-  prime_factorization (n^k) = k • n.prime_factorization :=
+lemma factorization_pow {n k : ℕ} :
+  factorization (n^k) = k • n.factorization :=
 begin
   ext p,
   simp only [algebra.id.smul_eq_mul, finsupp.coe_smul, pi.smul_apply],
-  simp only [prime_factorization_eq_count, factors_count_pow],
+  simp only [factorization_eq_count, factors_count_pow],
 end
 
 /-- The only prime factor of prime `p` is `p` itself, with multiplicity `1` -/
-@[simp] lemma prime.prime_factorization {p : ℕ} (hp : prime p) :
-  p.prime_factorization = single p 1 :=
+@[simp] lemma prime.factorization {p : ℕ} (hp : prime p) :
+  p.factorization = single p 1 :=
 begin
   ext q,
-  rw [prime_factorization_eq_count, factors_prime hp],
+  rw [factorization_eq_count, factors_prime hp],
   by_cases hqp : q = p,
   { rw hqp, simp },
   { rw finsupp.single_eq_of_ne (ne.symm hqp),
@@ -74,8 +74,8 @@ begin
 end
 
 /-- For prime `p` the only prime factor of `p^k` is `p` with multiplicity `k` -/
-@[simp] lemma prime.prime_factorization_pow {p k : ℕ} (hp : prime p) :
-  prime_factorization (p^k) = single p k :=
-by simp [prime_factorization_pow, prime.prime_factorization hp]
+@[simp] lemma prime.factorization_pow {p k : ℕ} (hp : prime p) :
+  factorization (p^k) = single p k :=
+by simp [factorization_pow, prime.factorization hp]
 
 end nat

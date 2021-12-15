28175000eab24a718171829726df530a5e0cc4e8
Bolton Bailey <bolton.bailey@gmail.com>
2021-12-15 13:10:40 -0800

    Update src/data/nat/count.lean
    Co-authored-by: Floris van Doorn <fpvdoorn@gmail.com>

--- src/data/nat/count.lean
+++ src/data/nat/count.lean
@@ -62,13 +62,13 @@ by split_ifs; simp [count, list.range_succ, h]
 
 lemma count_add (a b : ℕ) : count p (a + b) = count p a + count (λ k, p (a + k)) b :=
 begin
-  rw [count_eq_card_filter_range, count_eq_card_filter_range, count_eq_card_filter_range,
-    range_add, filter_union, card_disjoint_union, map_filter, add_left_embedding, card_map,
-    function.embedding.coe_fn_mk],
-  intros x hx,
-  simp_rw [inf_eq_inter, mem_inter, mem_filter, mem_map, mem_range] at hx,
-  obtain ⟨⟨hx, _⟩, ⟨c, _, rfl⟩, _⟩ := hx,
-  exact (self_le_add_right _ _).not_lt hx,
+  have : disjoint ((range a).filter p) (((range b).map $ add_left_embedding a).filter p), 
+  { intros x hx,
+    simp_rw [inf_eq_inter, mem_inter, mem_filter, mem_map, mem_range] at hx,
+    obtain ⟨⟨hx, _⟩, ⟨c, _, rfl⟩, _⟩ := hx,
+    exact (self_le_add_right _ _).not_lt hx },
+  simp_rw [count_eq_card_filter_range, range_add, filter_union, card_disjoint_union this, 
+    map_filter, add_left_embedding, card_map, function.embedding.coe_fn_mk]
 end
 
 lemma count_add' (a b : ℕ) : count p (a + b) = count (λ k, p (k + b)) a + count p b :=

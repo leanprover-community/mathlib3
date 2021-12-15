b33c612241c97cebf6c6e6879c770dd037642e85
Yakov Pechersky <ffxen158@gmail.com>
2021-12-15 15:41:29 -0500

    linter helped generalize

--- src/field_theory/ratfunc.lean
+++ src/field_theory/ratfunc.lean
@@ -273,6 +273,7 @@ by simpa only [← of_fraction_ring_inv, ← of_fraction_ring_mul, ← of_fracti
   using _root_.mul_inv_cancel this
 
 section has_scalar
+omit hdomain
 
 variables {R : Type*} [monoid R]
 
@@ -293,6 +294,7 @@ lemma to_fraction_ring_smul [has_scalar R (fraction_ring (polynomial K))]
   to_fraction_ring (c • p) = c • to_fraction_ring p :=
 by { cases p, rw ←of_fraction_ring_smul }
 
+include hdomain
 variables [distrib_mul_action R (polynomial K)]
 variables [htower : is_scalar_tower R (polynomial K) (polynomial K)]
 include htower

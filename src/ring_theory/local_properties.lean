af01c4e9169fb23b633259f779b4198c9d6be6dc
erd1 <the.erd.one@gmail.com>
2021-12-16 17:37:10 +0800

    address comments

--- src/ring_theory/local_properties.lean
+++ src/ring_theory/local_properties.lean
@@ -24,8 +24,9 @@ In this file, we provide the proofs of various local properties.
 
 The following properties are covered:
 
-* The triviality of an ideal or an element.
-* `is_reduced`
+* The triviality of an ideal or an element:
+  `ideal_eq_zero_of_localization`, `eq_zero_of_localization`
+* `is_reduced` : `localization_is_reduced`, `is_reduced_of_localization_maximal`.
 
 -/
 
@@ -36,6 +37,30 @@ universe u
 variables {R S : Type u} [comm_ring R] [comm_ring S] (M : submonoid R)
 variables (N : submonoid S) (R' S' : Type u) [comm_ring R'] [comm_ring S'] (f : R →+* S)
 variables [algebra R R'] [algebra S S']
+
+section properties
+
+section comm_ring
+variable (P : ∀ (R : Type u) [comm_ring R], Prop)
+
+include P
+
+/-- A property `P` of comm rings is said to be preserved by localization
+  if `P` holds for `M⁻¹R` whenever `P` holds for `R`. -/
+def localization_preserves : Prop :=
+  ∀ {R : Type u} [hR : comm_ring R] (M : by exactI submonoid R) (S : Type u) [hS : comm_ring S]
+    [by exactI algebra R S] [by exactI is_localization M S], @P R hR → @P S hS
+
+/-- A property `P` of comm rings satisfies `of_localization_maximal` if
+  if `P` holds for `R` whenever `P` holds for `Rₘ` for all maximal ideal `m`. -/
+def of_localization_maximal : Prop :=
+  ∀ (R : Type u) [comm_ring R],
+    by exactI (∀ (J : ideal R) (hJ : J.is_maximal), by exactI P (localization.at_prime J)) → P R
+
+end comm_ring
+
+
+end properties
 
 section ideal
 
@@ -88,8 +113,10 @@ end ideal
 
 section reduced
 
-lemma localization_is_reduced [is_localization M R'] [hR : is_reduced R] : is_reduced R' :=
+lemma localization_is_reduced : localization_preserves (λ R hR, by exactI is_reduced R) :=
 begin
+  introv R _ _,
+  resetI,
   constructor,
   rintro x ⟨(_|n), e⟩,
   { simpa using congr_arg (*x) e },
@@ -97,22 +124,23 @@ begin
   dsimp only at hx,
   let hx' := congr_arg (^ n.succ) hx,
   simp only [mul_pow, e, zero_mul, ← ring_hom.map_pow] at hx',
-  rw [← (algebra_map R R').map_zero] at hx',
-  obtain ⟨m', hm'⟩ := (is_localization.eq_iff_exists M R').mp hx',
+  rw [← (algebra_map R S).map_zero] at hx',
+  obtain ⟨m', hm'⟩ := (is_localization.eq_iff_exists M S).mp hx',
   apply_fun (*m'^n) at hm',
   simp only [mul_assoc, zero_mul] at hm',
   rw [mul_comm, ← pow_succ, ← mul_pow] at hm',
   replace hm' := is_nilpotent.eq_zero ⟨_, hm'.symm⟩,
-  rw [← (is_localization.map_units R' m).mul_left_inj, hx, zero_mul,
+  rw [← (is_localization.map_units S m).mul_left_inj, hx, zero_mul,
     is_localization.map_eq_zero_iff M],
   exact ⟨m', by rw [← hm', mul_comm]⟩
 end
 
-instance [is_reduced R] : is_reduced (localization M) := localization_is_reduced M _
+instance [is_reduced R] : is_reduced (localization M) := localization_is_reduced M _ infer_instance
 
-lemma is_reduced_of_localization (h : ∀ (J : ideal R) (hJ : J.is_maximal),
-  by exactI is_reduced (localization.at_prime J)) : is_reduced R :=
+lemma is_reduced_of_localization_maximal :
+  of_localization_maximal (λ R hR, by exactI is_reduced R) :=
 begin
+  introv R h,
   constructor,
   intros x hx,
   apply eq_zero_of_localization,

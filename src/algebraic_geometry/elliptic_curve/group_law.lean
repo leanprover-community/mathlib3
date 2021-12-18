fcce427c145753984c2a34e41eec4301935eab88
Multramate <dka316@ic.ac.uk>
2021-12-18 15:41:21 +0000

    Refactor point structure with definition

--- src/algebraic_geometry/elliptic_curve/group_law.lean
+++ src/algebraic_geometry/elliptic_curve/group_law.lean
@@ -1,50 +1,45 @@
 import algebra.algebra.basic
 import algebraic_geometry.elliptic_curve.EllipticCurve
 
+local attribute [semireducible] with_zero
+
 namespace EllipticCurve
 
--- let `K` be a field
 variables {K : Type*} [field K]
--- let `E` be an elliptic curve over `K`
 variables (E : EllipticCurve K)
--- let `L` be a field extension of `K`
 variables (L : Type*) [decidable_eq L] [field L] [algebra K L]
 
--- algebra map from `K` to `L`
 notation K↑L := algebra_map K L
 
 /-- The group of `L`-rational points `E(L)` on an elliptic curve `E` over `K`,
-consisting of the point at infinity and the affine points satisfying the Weierstrass equation. -/
-inductive point
-  | Infinity
-  | Affine (x y : L) (w : y ^ 2 + (K↑L)E.a1 * x * y + (K↑L)E.a3 * y
-                        = x ^ 3 + (K↑L)E.a2 * x ^ 2 + (K↑L)E.a4 * x + (K↑L)E.a6)
+    consisting of the point at infinity and the affine points satisfying a Weierstrass equation. -/
+def point := with_zero {P : L × L // P.2 ^ 2 + (K↑L)E.a1 * P.1 * P.2 + (K↑L)E.a3 * P.2
+                                   = P.1 ^ 3 + (K↑L)E.a2 * P.1 ^ 2 + (K↑L)E.a4 * P.1 + (K↑L)E.a6}
+
 notation E/L := point E L
 
-open point
-
 /-- Zero in `E(L)`. -/
-def zero : E/L := Infinity
-instance has_zero : has_zero (E/L) := ⟨ zero E L ⟩
+instance has_zero : has_zero (E/L) := with_zero.has_zero
+
+def neg : E/L → E/L
+  | 0        := 0
+  | (some P) := let ⟨⟨x, y⟩, w⟩ := P in
+    some ⟨⟨x, -y - (K↑L)E.a1 * x - (K↑L)E.a3⟩, by rw [← w]; ring_nf⟩
 
 /-- Negation in `E(L)`. -/
-def neg : E/L → E/L
-  | 0              := 0
-  | (Affine x y w) := Affine x (-y - (K↑L)E.a1 * x - (K↑L)E.a3) $ by rw [← w]; ring
-instance has_neg : has_neg (E/L) := ⟨ neg E L ⟩
+instance has_neg : has_neg (E/L) := ⟨neg E L⟩
 
-/-- Addition in `E(L)`. -/
 def add : E/L → E/L → E/L
-  | P                 0                 := P
-  | 0                 Q                 := Q
-  | (Affine x₁ y₁ w₁) (Affine x₂ y₂ w₂) :=
+  | P         0       := P
+  | 0         Q       := Q
+  | (some P) (some Q) := let (⟨⟨x₁, y₁⟩, w₁⟩, ⟨⟨x₂, y₂⟩, w₂⟩) := (P, Q) in
     -- add points with different x-coordinates
     if xNe : x₁ - x₂ ≠ 0 then
       let
         l  := (y₁ - y₂) * (x₁ - x₂)⁻¹,
         x₃ := l ^ 2 + (K↑L)E.a1 * l - (K↑L)E.a2 - x₁ - x₂,
         y₃ := -l * x₃ - (K↑L)E.a1 * x₃ - y₁ + l * x₁ - (K↑L)E.a3
-      in Affine x₃ y₃ $ begin
+      in some ⟨⟨x₃, y₃⟩, begin
         -- rewrite Weierstrass equations as w₁(x, y) = 0 and w₂(x, y) = 0
         rw [← sub_eq_zero] at w₁ w₂,
         -- substitute y
@@ -134,7 +129,7 @@ def add : E/L → E/L → E/L
                 nth_rewrite_lhs 0 [← w₁]; nth_rewrite_rhs 1 [← w₂]; ring,
         -- rewrite Weierstrass equation as w₃(x, y) = 0 and sequence steps
         rw [← sub_eq_zero, ySimp, xSimp, lSimp1, lSimp2, lSimp3, lSimp4]
-      end
+      end⟩
     -- add points with the same x-coordinate and y-coordinate
     else if yNe : y₁ + (y₂ + (K↑L)E.a1 * x₂ + (K↑L)E.a3) ≠ 0 then
       let
@@ -142,7 +137,7 @@ def add : E/L → E/L → E/L
               * (y₁ + (y₁ + (K↑L)E.a1 * x₁ + (K↑L)E.a3))⁻¹,
         x₃ := l ^ 2 + (K↑L)E.a1 * l - (K↑L)E.a2 - 2 * x₁,
         y₃ := -l * x₃ - (K↑L)E.a1 * x₃ - y₁ + l * x₁ - (K↑L)E.a3
-      in Affine x₃ y₃ $ begin
+      in some ⟨⟨x₃, y₃⟩, begin
         -- show x-coordinates are the same
         have xEq : x₁ = x₂
           := by simp at xNe; rw [← sub_eq_zero]; exact xNe,
@@ -214,46 +209,54 @@ def add : E/L → E/L → E/L
           := by rw [← mul_assoc l, inv_mul_cancel_right₀ yNe, ← w₁]; ring,
         -- rewrite Weierstrass equation as w₃(x, y) = 0 and sequence steps
         rw [← sub_eq_zero, ySimp, xSimp, lSimp1, lSimp2, lSimp3]
-      end
+      end⟩
     -- add points with different y-coordinates
     else
       0
-instance has_add : has_add (E/L) := ⟨ add E L ⟩
+
+/-- Addition in `E(L)`. -/
+instance has_add : has_add (E/L) := ⟨add E L⟩
 
 /-- Left identity in `E(L)`. -/
 @[simp]
-lemma zero_add : ∀ P : E/L, 0 + P = P
-  | P := by cases P; refl
+lemma zero_add : ∀ P : E/L, 0 + P = P := λ P, by cases P; refl
 
 /-- Right identity in `E(L)`. -/
 @[simp]
-lemma add_zero : ∀ P : E/L, P + 0 = P
-  | P := by cases P; refl
+lemma add_zero : ∀ P : E/L, P + 0 = P := λ P, by cases P; refl
 
 /-- Left negation in `E(L)`. -/
 @[simp]
 lemma add_left_neg : ∀ P : E/L, -P + P = 0
-  | 0              := by refl
-  | (Affine x y w) := by dsimp [has_neg.neg]; dsimp [neg]; dsimp [has_add.add]; simp [add]
+  | 0        := by refl
+  | (some P) := let ⟨⟨_, _⟩, _⟩ := P in
+    by dsimp [has_neg.neg]; dsimp [neg]; dsimp [has_add.add]; simp [add]
+
+/-- Commutativity in `E(L)`. -/
+lemma add_comm : ∀ (P Q : E/L), P + Q = Q + P
+  | _         0       := by simp
+  | 0         _       := by simp
+  | (some P) (some Q) := let (⟨⟨x₁, y₁⟩, w₁⟩, ⟨⟨x₂, y₂⟩, w₂⟩) := (P, Q) in
+    sorry
 
 /-- Associativity in `E(L)`. -/
 lemma add_assoc : ∀ P Q R : E/L, (P + Q) + R = P + (Q + R)
-  | 0                 Q                 R                 := by simp
-  | P                 0                 R                 := by simp
-  | P                 Q                 0                 := by simp
-  | (Affine x₁ y₁ w₁) (Affine x₂ y₂ w₂) (Affine x₃ y₃ w₃) := sorry
+  | _        _        0        := by simp
+  | _        0        _        := by simp
+  | 0        _        _        := by simp
+  | (some P) (some Q) (some R) := let (⟨⟨x₁, y₁⟩, w₁⟩, ⟨⟨x₂, y₂⟩, w₂⟩, ⟨⟨x₃, y₃⟩, w₃⟩) := (P, Q, R) in
+    sorry
 
-/-- `E(L)` is an additive group. -/
-instance add_group : add_group (E/L) :=
-  { zero         := zero E L
+/-- `E(L)` is an additive commutative group. -/
+instance add_comm_group : add_comm_group (E/L) :=
+  { zero         := 0
   , neg          := neg E L
   , add          := add E L
   , zero_add     := zero_add E L
   , add_zero     := add_zero E L
   , add_left_neg := add_left_neg E L
+  , add_comm     := add_comm E L
   , add_assoc    := add_assoc E L
   }
-
--- TODO: `E(L)` is an additive commutative group
 
 end EllipticCurve

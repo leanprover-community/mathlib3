a3edf19c42086e1f69c58412b1bfe32a40aafe12
Apurva Nakade <apurvnakade@gmail.com>
2021-12-18 18:30:24 -0600

    feat(group_theory/membership): upgrade pow to monoid morphism

--- src/group_theory/submonoid/membership.lean
+++ src/group_theory/submonoid/membership.lean
@@ -220,7 +220,7 @@ lemma powers_subset {n : M} {P : submonoid M} (h : n ∈ P) : powers n ≤ P :=
 λ x hx, match x, hx with _, ⟨i, rfl⟩ := P.pow_mem h i end
 
 /-- Exponentiation map from natural numbers to powers. -/
-def pow (n : M) (m : ℕ) : powers n := ⟨n ^ m, m, rfl⟩
+def pow (n : M) (m : ℕ) : powers n := (powers_hom M n).mrange_restrict m
 
 /-- Logarithms from powers to natural numbers. -/
 def log [decidable_eq M] {n : M} (p : powers n) : ℕ :=
@@ -233,12 +233,17 @@ lemma pow_right_injective_iff_pow_injective {n : M} :
   function.injective (λ m : ℕ, n ^ m) ↔ function.injective (pow n) :=
 subtype.coe_injective.of_comp_iff (pow n)
 
-theorem log_pow_eq_self [decidable_eq M] {n : M} (h : function.injective (λ m : ℕ, n ^ m)) (m : ℕ) :
-  log (pow n m) = m :=
-pow_right_injective_iff_pow_injective.mp h $ pow_log_eq_self _
+/-- The exponentiation map is an isomorphism from the additive monoid on natural numbers to powers
+when it is injective. The inverse is given by the logarithms. -/
+def pow_log_equiv [decidable_eq M] {n : M} (h : function.injective (λ m : ℕ, n ^ m)) (m : ℕ) : multiplicative ℕ ≃* powers n := {
+  to_fun := pow n,
+  inv_fun := log,
+  left_inv := λ _, pow_right_injective_iff_pow_injective.mp h $ pow_log_eq_self _,
+  right_inv := pow_log_eq_self,
+  map_mul' := λ x y, by { simp only [pow, map_mul] } }
 
 theorem log_pow_int_eq_self {x : ℤ} (h : 1 < x.nat_abs) (m : ℕ) : log (pow x m) = m :=
-log_pow_eq_self (int.pow_right_injective h) _
+(pow_log_equiv (int.pow_right_injective h) m).left_inv _
 
 @[simp] lemma map_powers {N : Type*} [monoid N] (f : M →* N) (m : M) :
   (powers m).map f = powers (f m) :=

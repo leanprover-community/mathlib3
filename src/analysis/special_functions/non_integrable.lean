26bd79a13d2c335ddb3960e48bdd4a6fc6fd98d8
Yury G. Kudryashov <urkud@urkud.name>
2021-12-19 16:28:29 -0500

    Use `obtain`

--- src/analysis/special_functions/non_integrable.lean
+++ src/analysis/special_functions/non_integrable.lean
@@ -50,22 +50,22 @@ is the derivative of `f`, then `g` is not integrable on any interval `a..b` such
 lemma not_interval_integrable_of_tendsto_norm_at_top_of_deriv_is_O_filter {f : ℝ → E} {g : ℝ → F}
   {a b : ℝ} (l : filter ℝ) [ne_bot l] [tendsto_Ixx_class Icc l l] (hl : [a, b] ∈ l)
   (hd : ∀ᶠ x in l, differentiable_at ℝ f x) (hf : tendsto (λ x, ∥f x∥) l at_top)
-  (hg : is_O (deriv f) g l) :
+  (hfg : is_O (deriv f) g l) :
   ¬interval_integrable g volume a b :=
 begin
   intro hgi,
-  have : ∃ (C : ℝ) (hC₀ : 0 ≤ C) (s ∈ l), ∀ (x ∈ s) (y ∈ s), [x, y] ⊆ [a, b] ∧
-    ∀ z ∈ [x, y], differentiable_at ℝ f z ∧ ∥deriv f z∥ ≤ C * ∥g z∥,
-  { rcases hg.exists_nonneg with ⟨C, C₀, hC⟩,
+  obtain ⟨C, hC₀, s, hsl, hsub, hfd, hg⟩ : ∃ (C : ℝ) (hC₀ : 0 ≤ C) (s ∈ l),
+    (∀ (x ∈ s) (y ∈ s), [x, y] ⊆ [a, b]) ∧
+    (∀ (x ∈ s) (y ∈ s) (z ∈ [x, y]), differentiable_at ℝ f z) ∧
+    (∀ (x ∈ s) (y ∈ s) (z ∈ [x, y]), ∥deriv f z∥ ≤ C * ∥g z∥),
+  { rcases hfg.exists_nonneg with ⟨C, C₀, hC⟩,
     have h : ∀ᶠ x : ℝ × ℝ in l.prod l, ∀ y ∈ [x.1, x.2], (differentiable_at ℝ f y ∧
       ∥deriv f y∥ ≤ C * ∥g y∥) ∧ y ∈ [a, b],
       from (tendsto_fst.interval tendsto_snd).eventually ((hd.and hC.bound).and hl).lift'_powerset,
     rcases mem_prod_self_iff.1 h with ⟨s, hsl, hs⟩,
     simp only [prod_subset_iff, mem_set_of_eq] at hs,
-    exact ⟨C, C₀, s, hsl, λ x hx y hy, ⟨λ z hz, (hs x hx y hy z hz).2,
-      λ z hz, (hs x hx y hy z hz).1⟩⟩ },
-  clear hl hd hg,
-  choose C hC₀ s hsl hsub hfd hg,
+    exact ⟨C, C₀, s, hsl, λ x hx y hy z hz, (hs x hx y hy z hz).2,
+      λ x hx y hy z hz, (hs x hx y hy z hz).1.1, λ x hx y hy z hz, (hs x hx y hy z hz).1.2⟩ },
   replace hgi : interval_integrable (λ x, C * ∥g x∥) volume a b, by convert hgi.norm.smul C,
   obtain ⟨c, hc, d, hd, hlt⟩ : ∃ (c ∈ s) (d ∈ s), ∥f c∥ + ∫ y in Ι a b, C * ∥g y∥ < ∥f d∥,
   { rcases filter.nonempty_of_mem hsl with ⟨c, hc⟩,

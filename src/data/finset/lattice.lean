3c9898dff778fad55b1bb63532dfe587f824177c
Mantas Bakšys <39908973+MantasBaksys@users.noreply.github.com>
2021-12-19 12:48:02 +0200

    Update src/data/finset/lattice.lean
    Co-authored-by: Alex J Best <alex.j.best@gmail.com>

--- src/data/finset/lattice.lean
+++ src/data/finset/lattice.lean
@@ -8,7 +8,6 @@ import data.finset.option
 import data.finset.prod
 import data.multiset.lattice
 import order.complete_lattice
-import order.order_dual
 import order.lexicographic
 /-!
 # Lattice operations on finsets

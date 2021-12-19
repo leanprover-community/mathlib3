510b6f78ecfbe41f0a84f49697e664eaf8a2d0fd
Stuart Presnell <stuart.presnell@gmail.com>
2021-12-19 22:07:55 +0000

    Eliminate `finish` from `finprod_mem_finset_product'`

--- src/algebra/big_operators/finprod.lean
+++ src/algebra/big_operators/finprod.lean
@@ -876,13 +876,21 @@ iterating this lemma, e.g., if we have `f : α × β × γ → M`. -/
 begin
   have : ∀ a, ∏ (i : β) in (s.filter (λ ab, prod.fst ab = a)).image prod.snd, f (a, i) =
     (finset.filter (λ ab, prod.fst ab = a) s).prod f,
-  { intros a, apply finset.prod_bij (λ b _, (a, b)); finish, },
+  { intros a, apply finset.prod_bij (λ b _, (a, b));
+  simp,
+  intros a' b p ha, rw ←ha,
+  use p,
+  -- finish,
+  },
   rw finprod_mem_finset_eq_prod,
   simp_rw [finprod_mem_finset_eq_prod, this],
   rw [finprod_eq_prod_of_mul_support_subset _
     (s.mul_support_of_fiberwise_prod_subset_image f prod.fst),
     ← finset.prod_fiberwise_of_maps_to _ f],
-  finish,
+  simp,
+  intros a b p,
+  use [b,p],
+  -- finish,
 end
 
 /-- See also `finprod_mem_finset_product'`. -/

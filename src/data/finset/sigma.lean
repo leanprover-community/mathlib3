2c4fdb429ff5f0f543dcb372ccf8ef3f54328a32
YaelDillies <yael.dillies@gmail.com>
2021-12-21 18:38:30 +0100

    credit Bhavik

--- src/data/finset/sigma.lean
+++ src/data/finset/sigma.lean
@@ -1,7 +1,7 @@
 /-
 Copyright (c) 2017 Mario Carneiro. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
-Authors: Mario Carneiro, Yaël Dillies
+Authors: Mario Carneiro, Yaël Dillies, Bhavik Mehta
 -/
 import data.finset.card
 

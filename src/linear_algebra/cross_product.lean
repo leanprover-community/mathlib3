52c550eb285b7de138f72c48f944975fc12738c9
madvorak <dvorakmartinbridge@seznam.cz>
2021-12-21 16:39:13 +0100

    major reworking, mostly by Kyle Miller

--- src/linear_algebra/cross_product.lean
+++ src/linear_algebra/cross_product.lean
@@ -6,99 +6,125 @@ Authors: Martin Dvorak, Kyle Miller, Eric Wieser, Andrew Yang
 
 import data.matrix.basic
 import data.matrix.notation
+import linear_algebra.bilinear_map
+import linear_algebra.matrix.determinant
 import tactic.fin_cases
 
+/-!
+# Cross products
 
-namespace cross_product
+This module defines the cross product of vectors in $R^3$ for $R$ a commutive ring.
+
+## Main definitions
+
+* `cross_product` is the cross product of pairs of vectors in $R^3$.
+
+## Main results
+
+* `triple_product_eq_det`
+
+## Notation
+
+The locale `vectors` gives the following notations:
+
+* `×₃` for the cross product
+* `⬝` for dot products
+
+## Tags
+
+cross product
+-/
+
 
 variables {R : Type*} [comm_ring R]
 
+private lemma vec3_eq {a b : fin 3 → R}
+  (h0 : a 0 = b 0) (h1 : a 1 = b 1) (h2 : a 2 = b 2) :
+  a = b :=
+by { ext x, fin_cases x; assumption }
+
+private lemma vec3_eq' {a0 a1 a2 b0 b1 b2 : R}
+  (h0 : a0 = b0) (h1 : a1 = b1) (h2 : a2 = b2) :
+  ![a0, a1, a2] = ![b0, b1, b2] :=
+by rw [h0, h1, h2]
+
+private lemma vec3_add {a b : fin 3 → R} :
+  a + b = ![a 0 + b 0, a 1 + b 1, a 2 + b 2] :=
+by { apply vec3_eq; refl }
+
+private lemma vec3_add' {a0 a1 a2 b0 b1 b2 : R} :
+  ![a0, a1, a2] + ![b0, b1, b2] = ![a0 + b0, a1 + b1, a2 + b2] :=
+by { rw vec3_add, refl }
 
 
-/-- cross product of two 3D vectors -/
+
+/-- The cross product of two vectors in $R^3$ for $R$ a commutative ring. -/
 def cross_product : (fin 3 → R) →ₗ[R] (fin 3 → R) →ₗ[R] (fin 3 → R) :=
-{
-  to_fun := λ (a : fin 3 → R),
+begin
+  apply linear_map.mk₂ R (λ (a b : fin 3 → R),
+    ![ (a 1)*(b 2) - (a 2)*(b 1) ,
+       (a 2)*(b 0) - (a 0)*(b 2) ,
+       (a 0)*(b 1) - (a 1)*(b 0) ]);
+  intros;
+  simp only [vec3_add', pi.add_apply, algebra.id.smul_eq_mul, matrix.smul_cons,
+    matrix.smul_empty, pi.smul_apply];
+  apply vec3_eq';
+  ring
+end
 
-  {
-    to_fun := λ (b : fin 3 → R),
+localized "infixl ` ×₃ `: 68 := cross_product" in vectors
+localized "infix ` ⬝ ` : 67 := matrix.dot_product" in vectors
 
-      ![ (a 1)*(b 2) - (a 2)*(b 1) ,
-         (a 2)*(b 0) - (a 0)*(b 2) ,
-         (a 0)*(b 1) - (a 1)*(b 0) ],
-
-    map_add' := λ (b₁ b₂ : fin 3 → R),
-    begin
-      ext x,
-      fin_cases x; simp [mul_add]; ring,
-    end,
-
-    map_smul' := λ (c : R) (b : fin 3 → R),
-      by simp [mul_sub, mul_left_comm]
-  },
-
-  map_add' := λ (a₁ a₂ : fin 3 → R),
-  begin
-    ext b,
-    fin_cases x; simp [add_mul]; ring,
-  end,
-
-  map_smul' := λ (c : R) (a : fin 3 → R),
-  begin
-    ext b,
-    fin_cases x; simp [mul_sub, mul_assoc],
-  end
-}
-
-
-local infix ` ×₃ `: 68 := cross_product
-local infix ` ⬝ ` : 67 := matrix.dot_product
+lemma cross_product_def (a b : fin 3 → R) :
+  a ×₃ b = ![ (a 1)*(b 2) - (a 2)*(b 1) ,
+              (a 2)*(b 0) - (a 0)*(b 2) ,
+              (a 0)*(b 1) - (a 1)*(b 0) ] := rfl
 
 
 
-/-- cross product is anti-commutative -/
 lemma cross_product_anticomm (v w : fin 3 → R) :
   v ×₃ w = - (w ×₃ v) :=
-by simp [cross_product, mul_comm]
+by simp [cross_product_def, mul_comm]
 
-/-- vector sum of cross product with flipped cross product yields the zero vector -/
 lemma cross_product_anticomm' (v w : fin 3 → R) :
   v ×₃ w + w ×₃ v = 0 :=
 by rw [add_eq_zero_iff_eq_neg, cross_product_anticomm]
 
-/-- cross product of a vector with itself yields the zero vector -/
 lemma cross_product_self_eq_zero_vector (v : fin 3 → R) :
   v ×₃ v = 0 :=
-begin
-  unfold cross_product,
-  simp,
-  split; ring_nf; tauto,
-end
+by simp [cross_product_def, mul_comm]
 
 
-private lemma dot_product_unfold (v w : fin 3 → R) :
+private lemma vec3_dot_product (v w : fin 3 → R) :
   v ⬝ w = v 0 * w 0 + v 1 * w 1 + v 2 * w 2 :=
 by simp [matrix.dot_product, add_assoc, fin.sum_univ_succ]
 
-/-- cross product of two vectors is perpendicular to the first vector -/
-lemma cross_product_perpendicular_first_arg (v w : fin 3 → R) :
+lemma dot_self_cross_product_eq_zero (v w : fin 3 → R) :
   v ⬝ (v ×₃ w) = 0 :=
-by simp [cross_product, dot_product_unfold, mul_sub, mul_assoc, mul_left_comm]
+by simp [cross_product_def, vec3_dot_product, mul_sub, mul_assoc, mul_left_comm]
 
-/-- cross product of two vectors is perpendicular to the second vector -/
-lemma cross_product_perpendicular_second_arg (v w : fin 3 → R) :
+lemma dot_cross_product_self_eq_zero (v w : fin 3 → R) :
   w ⬝ (v ×₃ w) = 0 :=
-by rw [cross_product_anticomm, matrix.dot_product_neg, cross_product_perpendicular_first_arg, neg_zero]
+by rw [cross_product_anticomm, matrix.dot_product_neg, dot_self_cross_product_eq_zero, neg_zero]
 
-/-- triple-product permutation lemma (combination of dot product with cross product);
-    both terms express the determinant of [u|v|w] basically -/
+
+/-- Cyclic permutations preserve triple product.
+See also `triple_product_eq_det`. -/
 lemma triple_product_permutation (u v w : fin 3 → R) :
   u ⬝ (v ×₃ w) = v ⬝ (w ×₃ u) :=
 begin
-  simp [cross_product, dot_product_unfold, mul_sub, mul_assoc, mul_left_comm, mul_comm],
+  simp only [cross_product_def, vec3_dot_product,
+    matrix.head_cons, matrix.cons_vec_bit0_eq_alt0, matrix.empty_append, matrix.cons_val_one,
+    matrix.cons_vec_alt0, matrix.cons_append, matrix.cons_val_zero],
   ring,
 end
 
-
-
-end cross_product
+theorem triple_product_eq_det (u v w : fin 3 → R) :
+  u ⬝ (v ×₃ w) = matrix.det ![ u, v, w ] :=
+begin
+  simp only [vec3_dot_product, cross_product_def, matrix.det_fin_three,
+    matrix.head_cons, matrix.cons_vec_bit0_eq_alt0, matrix.empty_vec_alt0, matrix.cons_vec_alt0,
+    matrix.vec_head_vec_alt0, fin.fin_append_apply_zero, matrix.empty_append, matrix.cons_append,
+    matrix.cons_val', matrix.cons_val_one, matrix.cons_val_zero],
+  ring,
+end

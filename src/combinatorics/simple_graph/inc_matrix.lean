138a43ccc22b8302bbf15db08d358e20962d9c5e
YaelDillies <yael.dillies@gmail.com>
2021-12-21 01:00:50 +0100

    switch to set.indicator

--- src/combinatorics/simple_graph/inc_matrix.lean
+++ src/combinatorics/simple_graph/inc_matrix.lean
@@ -17,12 +17,8 @@ This file defines the unoriented incidence matrix of a simple graph.
 
 ## Main results
 
-* `inc_matrix_element_power_id`: Every element from `M` is idempotent.
-* `degree_equals_sum_of_incidence_row`: The sum of elements from row `i` of `G.inc_matrix R`
-is equal to the `degree` of vertex `i`.
-* `simple_graph.inc_matrix_mul_transpose`: Gives a complete description of the product of
-`G.inc_matrix R` and its transpose; the diagonal is the degrees of each vertex, and the
-off-diagonals are 1 or 0 depending on whether or not the vertices are adjacent.
+* `simple_graph.inc_matrix_mul_transpose_diag`: The sum of the `a`-th row of `G.inc_matrix R`
+  equals the degree of vertex `a`.
 
 ## Implementation notes
 
@@ -35,8 +31,17 @@ incidence matrix for each `simple_graph α` has the same type.
 
 * Define the oriented incidence matrices for oriented graphs.
 * Define the graph Laplacian of a simple graph using the oriented incidence matrix from an
-arbitrary orientation of a simple graph.
+  arbitrary orientation of a simple graph.
 -/
+namespace finset
+variables {α : Type*} [decidable_eq α] {x y : α}
+
+lemma card_doubleton (h : x ≠ y) : ({x, y} : finset α).card = 2 :=
+by rw [card_insert_of_not_mem (not_mem_singleton.2 h), card_singleton]
+
+end finset
+
+noncomputable theory
 
 open_locale big_operators matrix
 open finset matrix simple_graph sym2
@@ -49,33 +54,42 @@ by simp [←ite_and]
 variables {R α : Type*}
 
 namespace simple_graph
-variables [decidable_eq α] (G : simple_graph α) [decidable_rel G.adj] (R)
+variables [decidable_eq α] (G : simple_graph α) (R)
+ [decidable_rel G.adj]
 
 /-- `G.inc_matrix R` is the `α × sym2 α` matrix whose `(a, e)`-entry is `1` if `e` is incident to
 `a` and `0` otherwise. -/
 def inc_matrix [has_zero R] [has_one R] : matrix α (sym2 α) R :=
-λ a e, if e ∈ G.incidence_set a then 1 else 0
+λ a, (G.incidence_set a).indicator 1
 
 variables {R}
 
 @[simp]
 lemma inc_matrix_apply {a : α} {e : sym2 α} [has_zero R] [has_one R] :
-  G.inc_matrix R a e = if e ∈ G.incidence_set a then 1 else 0 := rfl
+  G.inc_matrix R a e = (G.incidence_set a).indicator 1 e := rfl
 
 section mul_zero_one_class
 variables [mul_zero_one_class R] {a b : α} {e : sym2 α}
 
 lemma inc_matrix_apply_mul_inc_matrix_apply :
   G.inc_matrix R a e * G.inc_matrix R b e =
-    if (e ∈ G.incidence_set a ∧ e ∈ G.incidence_set b) then 1 else 0 :=
+    (G.incidence_set a ∩ G.incidence_set b).indicator 1 e :=
 by rw [inc_matrix, ite_zero_mul_ite_zero_right _ _, mul_one]
+-- by { convert set.indicator_mul_indicator _ _ _, }
 
 lemma inc_matrix_apply_mul_inc_matrix_apply_of_not_adj (hab : a ≠ b) (h : ¬ G.adj a b) :
   G.inc_matrix R a e * G.inc_matrix R b e = 0 :=
 begin
-  rw [inc_matrix_apply_mul_inc_matrix_apply, if_neg],
+  rw [inc_matrix_apply_mul_inc_matrix_apply, set.indicator_of_not_mem],
   exact λ h', h (G.adj_of_mem_incidence_set hab h'.1 h'.2),
 end
+
+lemma inc_matrix_of_not_mem_incidence_set (h : e ∉ G.incidence_set a) :
+  G.inc_matrix R a e = 0 :=
+if_neg sorry
+
+lemma inc_matrix_of_mem_incidence_set (h : e ∈ G.incidence_set a) : G.inc_matrix R a e = 1 :=
+if_pos sorry
 
 variables [nontrivial R]
 
@@ -103,6 +117,39 @@ begin
 end
 
 lemma inc_matrix_mul_transpose_diag : (G.inc_matrix R ⬝ (G.inc_matrix R)ᵀ) a a = G.degree a :=
+begin
+  rw ←sum_inc_matrix_apply,
+  simp only [matrix.mul_apply, matrix.transpose_apply, ite_zero_mul_ite_zero_right,
+    inc_matrix_apply, and_self, mul_one],
+  congr,
+  ext,
+  congr,
+end
+
+/-- The sum of elements from column `e` of the incidence matrix is equal to `2` if `e` is an edge.
+-/
+lemma sum_inc_matrix_apply_of_mem_edge_set : e ∈ G.edge_set → ∑ a, G.inc_matrix R a e = 2 :=
+begin
+  refine e.ind _,
+  intros a b h,
+  rw mem_edge_set at h,
+  rw [←nat.cast_two, ←card_doubleton h.ne],
+  simp only [inc_matrix_apply, sum_boole],
+  apply congr_arg,
+  rw ←card_incidence_finset_eq_degree,
+  congr' 1,
+  ext e,
+  simp,
+end
+
+/-- The sum of elements from column `e` of the incidence matrix is equal to `0` if `e` is not an
+edge.
+-/
+lemma sum_inc_matrix_apply_of_not_mem_edge_set (h : e ∉ G.edge_set) : ∑ a, G.inc_matrix R a e = 0 :=
+sum_eq_zero $ λ a _, G.inc_matrix_of_not_mem_incidence_set $ λ he, h he.1
+
+lemma inc_matrix_transpose_mul_diag :
+  ((G.inc_matrix R)ᵀ ⬝ G.inc_matrix R) e e = if e ∈ G.edge_set then 2 else 0 :=
 begin
   rw ←sum_inc_matrix_apply,
   simp only [matrix.mul_apply, matrix.transpose_apply, ite_zero_mul_ite_zero_right,

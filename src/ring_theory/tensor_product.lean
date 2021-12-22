636992966fea81cd9cda23604a1df9c6a0053104
Thomas Browning <tb65536@uw.edu>
2021-12-22 10:52:18 -0800

    reorder imports

--- src/ring_theory/tensor_product.lean
+++ src/ring_theory/tensor_product.lean
@@ -4,8 +4,8 @@ Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Scott Morrison, Johan Commelin
 -/
 
-import ring_theory.adjoin.basic
 import linear_algebra.tensor_product_basis
+import ring_theory.adjoin.basic
 
 /-!
 # The tensor product of R-algebras

fd6f775d2607102b365ca8527921f17b9e056f4f
Alex J. Best <alex.j.best@gmail.com>
2021-12-23 12:51:02 +0100

    checkpoint

---
+++ src/ring_theory/power_series/noetherian.lean
@@ -0,0 +1,197 @@
+import ring_theory.power_series.basic
+import ring_theory.polynomial.basic
+import topology.algebra.ordered.basic
+
+variables {R : Type*} [comm_ring R]
+#check ideal.leading_coeff_nth
+
+#check power_series.le_order
+
+#check ideal.degree_le
+open power_series
+def of_power_series (I : ideal (power_series R)) : submodule R (power_series R) :=
+{ carrier := I.carrier,
+  zero_mem' := I.zero_mem,
+  add_mem' := λ _ _, I.add_mem,
+  smul_mem' := λ c x H, by { simp [set_like.mem_coe, submodule.mem_carrier],
+  exact submodule.smul_of_tower_mem I c H, } }
+
+noncomputable theory
+/-- The `R`-submodule of `R[[X]]` consisting of power series of order ≥ `n`. -/
+def submodule_le_order (R : Type*) [comm_ring R] (n : with_bot ℕ) : submodule R (power_series R) :=
+⨅ k : ℕ, ⨅ h : ↑k < n, (coeff R k).ker
+
+open_locale classical
+def order_coeff (p : power_series R) : R := coeff R ((order p).get_or_else 0) p
+lemma order_coeff_mul_X_pow (p : power_series R) (n : ℕ) :
+  order_coeff (p * X ^ n) = order_coeff p := by sorry; simp [order_coeff]
+namespace ideal
+
+variables (I : ideal (power_series R))
+/-- Given an ideal `I` of `R[[X]]`, make the `R`-submodule of `I`
+consisting of power series of order ≥ `n`. -/
+def le_order (n : with_bot ℕ) :
+  submodule R (power_series R) :=
+submodule_le_order R n ⊓ of_power_series I
+
+/-- Given an ideal `I` of `R[[X]]`, make the ideal in `R` of
+leading coefficients of polynomials in `I` with order ≥ `n`. -/
+def order_coeff_nth (n : ℕ) : ideal R :=
+(I.le_order n).map $ coeff R n
+
+theorem mem_le_order {n : with_bot ℕ} {f : power_series R} :
+  f ∈ submodule_le_order R n ↔ ↑(n.get_or_else 0) ≤ order f :=
+begin
+  cases n,
+  { simp [submodule_le_order, with_bot.not_lt_none], },
+  simp [submodule_le_order, nat_le_order],
+  split; intro h,
+  apply nat_le_order,
+  intros,
+  apply h,
+  exact with_bot.coe_lt_coe.mpr H,
+  intros m hm,
+  -- erw with_bot.coe_lt_coe at hm,
+  apply coeff_of_lt_order,
+  apply lt_of_lt_of_le _ h,
+  erw with_bot.coe_lt_coe at hm,
+  rw enat.coe_lt_coe,
+  exact hm,
+end
+
+theorem mem_order_coeff_nth (n : ℕ) (x) :
+  x ∈ I.order_coeff_nth n ↔ ∃ p ∈ I, ↑n ≤ order p ∧ order_coeff p = x :=
+begin
+  simp [order_coeff_nth, le_order, submodule.mem_map, submodule.mem_inf,
+    mem_le_order, order_zero],
+  split,
+  { rintro ⟨p, ⟨hpdeg, hpI⟩, rfl⟩,
+    cases lt_or_eq_of_le hpdeg with hpdeg hpdeg,
+    sorry;{ refine ⟨0, I.zero_mem, bot_le, _⟩,
+      rw [leading_coeff_zero, eq_comm],
+      exact coeff_eq_zero_of_degree_lt hpdeg },
+    { refine ⟨p, hpI, le_of_eq hpdeg, _⟩,
+      rw [order_coeff], simp [hpdeg], congr, rw ← hpdeg, sorry, } },
+      sorry,
+  -- { rintro ⟨p, hpI, hpdeg, rfl⟩,
+    -- have : order p + (↑n - order p) = n,
+    -- { exact add_tsub_cancel_of_le (nat_degree_le_of_degree_le hpdeg) },
+    -- refine ⟨p * X ^ (n - nat_degree p), ⟨_, I.mul_mem_right _ hpI⟩, _⟩,
+    -- { apply le_trans (degree_mul_le _ _) _,
+    --   apply le_trans (add_le_add (degree_le_nat_degree) (degree_X_pow_le _)) _,
+    --   rw [← with_bot.coe_add, this],
+    --   exact le_refl _ },
+    -- { rw [leading_coeff, ← coeff_mul_X_pow p (n - nat_degree p), this] } }
+end
+
+theorem order_coeff_nth_mono {m n : ℕ} (H : m ≤ n) :
+  I.order_coeff_nth m ≤ I.order_coeff_nth n :=
+begin
+  nontriviality R,
+  intros r hr,
+  simp only [set_like.mem_coe, mem_order_coeff_nth] at hr ⊢,
+  rcases hr with ⟨p, hpI, hpdeg, rfl⟩,
+  refine ⟨p * X ^ (n - m), I.mul_mem_right _ hpI, _, order_coeff_mul_X_pow _ _⟩,
+  refine le_trans _ (order_mul_ge _ _),
+  rw order_X_pow,
+  norm_cast,
+  sorry,
+  -- rw order_mul_ge,
+  -- refine le_trans (order_mul _ _) _,
+  -- refine le_trans (add_le_add hpdeg (degree_X_pow_le _)) _,
+  -- rw [← with_bot.coe_add, add_tsub_cancel_of_le H],
+  -- exact le_refl _
+end
+
+end ideal
+
+namespace power_series
+open_locale big_operators
+/-- Hilbert basis theoremish: a power series ring over a noetherian ring is a noetherian ring. -/
+protected theorem power_series.is_noetherian_ring [is_noetherian_ring R] :
+  is_noetherian_ring (power_series R) :=
+is_noetherian_ring_iff.2 ⟨assume I : ideal (power_series R),
+let M := well_founded.min (is_noetherian_iff_well_founded.1 (by apply_instance))
+  (set.range I.order_coeff_nth) ⟨_, ⟨0, rfl⟩⟩ in
+have hm : M ∈ set.range I.order_coeff_nth := well_founded.min_mem _ _ _,
+let ⟨N, HN⟩ := hm in
+-- λ k, or.cases_on (le_or_lt k N)
+-- or.cases_on (le_or_lt k N)
+--   (λ h, HN ▸ I.leading_coeff_nth_mono h)
+--   (λ h x hx, classical.by_contradiction $ λ hxm,
+--     have ¬M < I.leading_coeff_nth k, by refine well_founded.not_lt_min
+--       (well_founded_submodule_gt _ _) _ _ _; exact ⟨k, rfl⟩,
+--     this ⟨HN ▸ I.leading_coeff_nth_mono (le_of_lt h), λ H, hxm (H hx)⟩),
+-- have hs2 : ∀ {x}, x ∈ I.degree_le N → x ∈ ideal.span (↑s : set (polynomial R)),
+-- from hs ▸ λ x hx, submodule.span_induction hx (λ _ hx, ideal.subset_span hx) (ideal.zero_mem _)
+--   (λ _ _, ideal.add_mem _) (λ c f hf, f.C_mul' c ▸ ideal.mul_mem_left _ _ hf),
+-- ⟨s, le_antisymm
+--   (ideal.span_le.2 $ λ x hx, have x ∈ I.degree_le N, from hs ▸ submodule.subset_span hx, this.2) $
+begin
+  have : ∀ t, (I.order_coeff_nth t).fg :=
+    λ t, (is_noetherian_ring_iff_ideal_fg R).mp ‹_› (I.order_coeff_nth t),
+  let s' : ℕ → finset R := λ d, classical.some (this d),
+  have : ∀ (d) (a : s' d), ∃ fa : power_series R, fa ∈ I.le_order ↑d ∧ coeff R d fa = a,
+  { rintros d ⟨a, ha⟩,
+    simp only [s'] at ha,
+    generalize_proofs hh at ha,
+    have : a ∈ I.order_coeff_nth d,
+    { rw ← classical.some_spec hh,
+      exact ideal.subset_span (finset.mem_coe.mpr ha), },
+    rw [ideal.order_coeff_nth, submodule.mem_map] at this,
+    simpa, },
+  let s'' : Π d (a : s' d), power_series R := λ d a, classical.some (this d a),
+  let s := (finset.range N).bUnion (λ d, (s' d).attach.image (s'' d)),
+  refine ⟨s, le_antisymm _ _⟩,
+  { erw ideal.span_le,
+    simp only [s'', set.image_subset_iff, finset.coe_bUnion, set.Union_subset_iff,
+      finset.mem_range, finset.mem_coe, finset.coe_image],
+    intros i hi x hx,
+    simp only [set.mem_preimage, set_like.mem_coe],
+    generalize_proofs hh,
+    have := classical.some_spec hh,
+    simp only [ideal.le_order, of_power_series, submodule.mem_inf, set_like.mem_coe,
+      submodule.mem_mk, submodule.mem_carrier] at this,
+    convert this.1.2, },
+  have : submodule.span (power_series R) ↑s = ideal.span ↑s, by refl,
+  rw this,
+  intros p hp,
+  let f₀ : power_series R := sorry,
+  let f : ℕ → power_series R := λ n, @nat.rec_on (λ _, power_series R) n f₀ (λ d fd, _),
+  by_contra hf,
+  let T := {n | ∃ f ∈ ideal.span (↑s : set (power_series R)), p - f ∈ I ∧ order (p - f) = n},
+  have : T.nonempty,
+  { simp only [T, set.nonempty, exists_prop, finset.coe_bUnion,
+      set.mem_set_of_eq, finset.mem_range, finset.mem_coe, finset.coe_image],
+    refine ⟨_, 0, by simp, by simp [hp], rfl⟩, },
+
+  have Sup_mem : Sup T ∈ T,
+  { letI : topological_space enat := preorder.topology _,
+    -- haveI : discrete_topology enat := ⟨rfl⟩,
+    haveI : order_topology enat := ⟨rfl⟩,
+    have := cSup_mem_closure this ⟨⊤, λ a ha, le_top⟩,
+    rwa closure_eq_iff_is_closed.mpr _ at this,
+    haveI : discrete_topology enat := singletons_open_iff_discrete.mp (λ a, _),
+    -- TODO I-adic topology as a metric?
+     },
+  have sup_top : Sup T = ⊤,
+  { apply (eq_top_or_lt_top (Sup T)).resolve_right,
+    intro htt,
+    have := enat.ne_top_of_lt htt,
+    rw enat.ne_top_iff at this,
+    rcases this with ⟨this_w, this_h⟩,
+    simp only [exists_prop, finset.coe_bUnion, set.mem_set_of_eq,
+      finset.mem_range, finset.mem_coe, finset.coe_image],
+    intros b hb,
+    rw [lt_top_iff_ne_top, enat.ne_top_iff] at hb,
+    rcases hb with ⟨n, rfl⟩,
+    sorry, },
+  rw [sup_top, set.mem_def] at this,
+  rcases this with ⟨f, hps, hpf, opf⟩,
+  simp only [sub_eq_zero, order_eq_top] at opf,
+  rw opf at hf,
+  contradiction,
+end⟩
+
+attribute [instance] power_series.is_noetherian_ring
+end power_series

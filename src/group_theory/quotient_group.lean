ffbab0dc69635c8a0839fa00e5958d529d52b5d4
David Kurniadi Angdinata <dka31@cantab.ac.uk>
2021-12-24 13:57:18 +0000

    chore(group_theory/quotient_group): change injective_ker_lift to ker_lift_injective for naming regularisation (#11027)
    Minor change for naming regularisation.
    
    
    
    Co-authored-by: Multramate <dka316@ic.ac.uk>

--- src/group_theory/quotient_group.lean
+++ src/group_theory/quotient_group.lean
@@ -192,7 +192,7 @@ lift_mk _ _ _
 lemma ker_lift_mk' (g : G) : (ker_lift φ) (mk g) = φ g :=
 lift_mk' _ _ _
 
-@[to_additive quotient_add_group.injective_ker_lift]
+@[to_additive quotient_add_group.ker_lift_injective]
 lemma ker_lift_injective : injective (ker_lift φ) :=
 assume a b, quotient.induction_on₂' a b $
   assume a b (h : φ a = φ b), quotient.sound' $

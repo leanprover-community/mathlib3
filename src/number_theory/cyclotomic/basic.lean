2007b61c4cbbb6275043a9fa5d31b5b2a88c34cf
Riccardo Brasca <riccardo.brasca@gmail.com>
2021-12-25 19:50:17 +0100

    Update src/number_theory/cyclotomic/basic.lean
    Co-authored-by: Johan Commelin <johan@commelin.net>

--- src/number_theory/cyclotomic/basic.lean
+++ src/number_theory/cyclotomic/basic.lean
@@ -60,7 +60,7 @@ noncomputable theory
 that `cyclotomic a A` has a root in `B` for all `a ∈ S` and that `B` is generated over `A` by the
 roots of `X ^ n - 1`. -/
 @[mk_iff] class is_cyclotomic_extension : Prop :=
-(ex_root {a : ℕ+} (ha : a ∈ S) : ∃ r : B, aeval r (cyclotomic a A) = 0)
+(exists_root {a : ℕ+} (ha : a ∈ S) : ∃ r : B, aeval r (cyclotomic a A) = 0)
 (adjoint_roots : ∀ (x : B), x ∈ adjoin A { b : B | ∃ a : ℕ+, a ∈ S ∧ b ^ (a : ℕ) = 1 })
 
 namespace is_cyclotomic_extension

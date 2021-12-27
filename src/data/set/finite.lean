58910b5322662c083ae696a8917c52ca836d17ea
Patrick Massot <patrickmassot@free.fr>
2021-12-27 15:24:35 +0100

    feat(topology): missing lemmas for Kyle

--- src/data/set/finite.lean
+++ src/data/set/finite.lean
@@ -322,6 +322,10 @@ h.inter_of_left t
 theorem finite.inf_of_right {s : set α} (h : finite s) (t : set α) : finite (t ⊓ s) :=
 h.inter_of_right t
 
+lemma finite.sInter {α : Type*} {s : set (set α)} {t : set α} (ht : t ∈ s)
+  (hf : t.finite) : (⋂₀s).finite :=
+hf.subset (sInter_subset_of_mem ht)
+
 protected theorem infinite.mono {s t : set α} (h : s ⊆ t) : infinite s → infinite t :=
 mt (λ ht, ht.subset h)
 

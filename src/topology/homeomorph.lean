b83415d83d7b059f788151b57b96d19e2d143f5f
Kyle Miller <kmill31415@gmail.com>
2021-12-27 22:35:36 -0500

    counterexample works

--- src/topology/homeomorph.lean
+++ src/topology/homeomorph.lean
@@ -449,4 +449,34 @@ def homeo_of_equiv_compact_to_t2 [compact_space α] [t2_space β]
   continuous_inv_fun := hf.continuous_symm_of_equiv_compact_to_t2,
   ..f }
 
+lemma homeo_of_equiv_compact_to_t2.t1_counterexample :
+  ∃ (α β : Type) (Iα : topological_space α) (Iβ : topological_space β), by exactI
+  compact_space α ∧ t1_space β ∧ ∃ f : α ≃ β, continuous f ∧ ¬ continuous f.symm :=
+begin
+  let topα : topological_space ℕ := nhds_adjoint 0 filter.cofinite,
+  let topβ : topological_space ℕ := cofinite_topology ℕ,
+  refine ⟨ℕ, ℕ, topα, topβ, _, t1_space_cofinite, equiv.refl ℕ, _, _⟩,
+  { fsplit,
+    rw is_compact_iff_ultrafilter_le_nhds,
+    intros f,
+    suffices : ∃ a, ↑f ≤ @nhds _ topα a, by simpa,
+    by_cases hf : ↑f ≤ @nhds _ topα 0,
+    { exact ⟨0, hf⟩ },
+    { obtain ⟨U, h0U, hU_fin, hUf⟩ : ∃ U : set ℕ, 0 ∈ U ∧ Uᶜ.finite ∧ Uᶜ ∈ f,
+      { rw [nhds_adjoint_nhds, filter.le_def] at hf,
+        push_neg at hf,
+        simpa [and_assoc, ← ultrafilter.compl_mem_iff_not_mem] using hf },
+      obtain ⟨n, hn', hn⟩ := ultrafilter.eq_principal_of_finite_mem hU_fin hUf,
+      rw hn,
+      exact ⟨n, @mem_of_mem_nhds _ topα n⟩ } },
+  { rw continuous_iff_coinduced_le,
+    change topα ≤ topβ,
+    rw gc_nhds,
+    simp [nhds_cofinite] },
+  { intros h,
+    replace h : topβ ≤ topα, by simpa [continuous_iff_coinduced_le, coinduced_id] using h,
+    rw le_nhds_adjoint_iff at h,
+    exact (finite_singleton 1).infinite_compl (h.2 1 one_ne_zero ⟨1, mem_singleton 1⟩) }
+end
+
 end continuous

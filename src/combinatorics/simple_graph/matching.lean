914dfed24cf185849f534eb107f15d572dbdeb3d
Arthur Paulino <arthurleonardo.ap@gmail.com>
2021-12-28 11:01:17 -0300

    fixing lint

--- src/combinatorics/simple_graph/matching.lean
+++ src/combinatorics/simple_graph/matching.lean
@@ -93,8 +93,7 @@ lemma is_matching_iff_forall_degree {M : subgraph G} [Π (v : V), fintype (M.nei
   M.is_matching ↔ ∀ (v : V), v ∈ M.verts → M.degree v = 1 :=
 by simpa [degree_eq_one_iff_unique_adj]
 
-lemma is_matching.even_card {M : subgraph G} [hM : fintype M.verts] [decidable_eq V]
-  [decidable_rel G.adj] (h : M.is_matching) :
+lemma is_matching.even_card {M : subgraph G} [fintype M.verts] (h : M.is_matching) :
   even (M.verts.to_finset.card) :=
 begin
   classical,
@@ -118,9 +117,8 @@ lemma is_perfect_matching_iff_forall_degree {M : subgraph G} [Π v, fintype (M.n
   M.is_perfect_matching ↔ ∀ v, M.degree v = 1 :=
 by simp [degree_eq_one_iff_unique_adj, is_perfect_matching_iff]
 
-lemma is_perfect_matching.even_card {M : subgraph G}
-  [fintype V] [decidable_eq V] [decidable_rel G.adj]
-  (h : M.is_perfect_matching) : even (fintype.card V) :=
+lemma is_perfect_matching.even_card {M : subgraph G} [fintype V] (h : M.is_perfect_matching) :
+  even (fintype.card V) :=
 begin
   classical,
   rw is_perfect_matching_iff_forall_degree at h,

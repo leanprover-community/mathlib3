9767ee021fdf06a29d4441a39fdfbd1628917780
Joël Riou <joel.riou@universite-paris-saclay.fr>
2021-12-29 13:25:27 +0100

    generalized to homological_complex, following suggestion by @jcommelin

--- src/algebra/homology/null_homotopic.lean
+++ src/algebra/homology/null_homotopic.lean
@@ -7,56 +7,62 @@ Authors: Joël Riou
 import algebra.homology.homotopy
 
 /-!
-# Null homotopic chain complex maps
-
+# Null homotopic maps
 -/
 
 open category_theory
+open category_theory.limits
 open category_theory.category
+open category_theory.preadditive
 
-namespace chain_complex
+noncomputable theory
+open_locale classical
 
-variables {V : Type*} [category V] [preadditive V]
+namespace homological_complex
 
-/-- Given a sequence `h` of morphisms `C_n ⟶ D_{n+1}`, this is the chain
-complex morphism C ⟶ D given by the formula `dh+hd`. -/
+universes v u
+variables {ι : Type*}
+variables {V : Type u} [category.{v} V] [preadditive V]
+variables {c : complex_shape ι} {C D : homological_complex V c}
+
 @[simps]
-def null_homotopic_map {C D : chain_complex V ℕ}
-  (h : Π (n : ℕ), C.X n ⟶ D.X (n+1)) : C ⟶ D :=
-{ f := (λ n, by
-  { cases n,
-    exact h 0 ≫ D.d 1 0,
-    exact h (n+1) ≫ D.d (n+2) (n+1) + C.d (n+1) n ≫ h n, }),
-  comm' := λ i j, by
-  { rw complex_shape.down_rel,
-    intro hij,
-    cases j; rw ← hij,
-    { simp only [limits.comp_zero, nat.rec_add_one,
-      homological_complex.d_comp_d, preadditive.add_comp, zero_add, assoc], },
-    { simp only [limits.comp_zero, nat.rec_add_one, add_zero, limits.zero_comp,
-        homological_complex.d_comp_d, preadditive.add_comp,
-        preadditive.comp_add, zero_add, homological_complex.d_comp_d_assoc,
-        assoc], }, }, }
-
-/-- Morphisms constructed by `null_homotopic_map` are homotopic to the zero map. -/
-@[simps]
-def null_homotopy {C D : chain_complex V ℕ}
-  (h : Π (n : ℕ), C.X n ⟶ D.X (n+1)) :
-  homotopy (null_homotopic_map h) 0 :=
-{ hom := λ i j, dite (i+1=j)
-    (λ hij, h i ≫ (eq_to_hom (by { congr, assumption, }) : D.X (i+1) ⟶ D.X j))
-    (λ _, 0),
-  zero' := λ i j hij, begin
-    rw [dite_eq_right_iff],
-    intro hij',
-    exfalso,
-    exact hij hij',
-  end,
-  comm := λ n, begin
-    cases n,
-    { tidy, },
-    { tidy, apply add_comm, }
+def null_homotopic_map (hom : Π i j, C.X i ⟶ D.X j) : C ⟶ D :=
+{ f      := λ i, d_next i hom + prev_d i hom,
+  comm'  := λ i j hij,
+  begin
+    have eq1 :prev_d i hom ≫ D.d i j = 0,
+    { rcases h : c.prev i with _|⟨i',w⟩,
+      { dsimp [prev_d], rw h, erw zero_comp, },
+      { rw [prev_d_eq hom w, assoc, D.d_comp_d' i' i j w hij, comp_zero], }, },
+    have eq2 : C.d i j ≫ d_next j hom = 0,
+    { rcases h : c.next j with _|⟨j',w⟩,
+      { dsimp [d_next], rw h, erw comp_zero, },
+      { rw [d_next_eq hom w, ← assoc, C.d_comp_d' i j j' hij w, zero_comp], }, },
+    rw [d_next_eq hom hij, prev_d_eq hom hij, comp_add, add_comp,
+      eq1, eq2, add_zero, zero_add, assoc], 
   end }
 
-end chain_complex
+@[simp]
+def null_homotopic_map' (h : Π i j, c.rel j i → (C.X i ⟶ D.X j)) : C ⟶ D :=
+null_homotopic_map (λ i j, dite (c.rel j i) (h i j) (λ _, 0))
+
+@[simps]
+def null_homotopy (hom : Π i j, C.X i ⟶ D.X j) (zero' : ∀ i j, ¬ c.rel j i → hom i j = 0) :
+  homotopy (null_homotopic_map hom) 0 :=
+{ hom := hom,
+  zero' := zero',
+  comm := by { intro i, rw [homological_complex.zero_f_apply, add_zero], refl, }, }
+
+@[simps]
+def null_homotopy' (h : Π i j, c.rel j i → (C.X i ⟶ D.X j)) :
+  homotopy (null_homotopic_map' h) 0 := by
+{ apply null_homotopy (λ i j, dite (c.rel j i) (h i j) (λ _, 0)),
+  intros i j hij,
+  dsimp,
+  rw [dite_eq_right_iff],
+  intro hij',
+  exfalso,
+  exact hij hij', }
+
+end homological_complex
 

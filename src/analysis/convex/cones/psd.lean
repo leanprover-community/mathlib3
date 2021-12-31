a8cbd6a0399521ad1378677716f567417e540bc0
Ramon Fernandez Mir <rfernandezmir@gmail.com>
2021-12-31 17:51:10 +0100

    Playing around with spectral theorem

--- src/analysis/convex/cones/psd.lean
+++ src/analysis/convex/cones/psd.lean
@@ -1,15 +1,60 @@
 import data.matrix.basic
 import linear_algebra.matrix.symmetric
 import analysis.convex.cone
+import analysis.inner_product_space.spectrum
+
 
 section psd_cone
 
 variables (n : ℕ)
 
+local notation `ℳₙ` := matrix (fin n) (fin n) ℝ
+
+section definitions
+
 open_locale matrix
 open matrix
 
-/--
+noncomputable instance : inner_product_space ℝ (fin n → ℝ) :=
+inner_product_space.of_core $ {
+  inner := matrix.dot_product,
+  conj_sym := sorry,
+  nonneg_re := sorry,
+  definite := sorry,
+  add_left := sorry,
+  smul_left := sorry,
+}
+
+lemma inner_def (v w : fin n → ℝ) : inner v w = matrix.dot_product v w := rfl
+
+end definitions
+
+section spectral_theorem
+
+lemma is_self_adjoint_of_is_symm (M : ℳₙ) (hM : M.is_symm)
+  : is_self_adjoint (matrix.mul_vec_lin M) :=
+begin
+  intros x y,
+  simp [inner_def, matrix.dot_product_mul_vec, ←matrix.mul_vec_transpose],
+  congr,
+  exact hM.symm,
+end
+
+lemma spectral_theorem
+  (M : ℳₙ) (hM : M.is_symm)
+  (μ : module.End.eigenvalues (matrix.mul_vec_lin M))
+  (v : fin n → ℝ) :
+  (is_self_adjoint_of_is_symm n M hM).diagonalization (M.mul_vec_lin v) μ
+  = (μ : ℝ) • (is_self_adjoint_of_is_symm n M hM).diagonalization v μ :=
+begin
+  sorry,
+end
+
+#check is_self_adjoint.diagonalization_apply_self_apply
+
+end spectral_theorem
+
+/-
 instance : has_inner ℝ (matrix (fin n) (fin n) ℝ) :=
 { inner := λ A B, trace (fin n) ℝ ℝ (Aᵀ ⬝ B) }
 
@@ -18,7 +63,6 @@ matrix.normed_group
 
 noncomputable instance matrix.normed_space.inst : normed_space ℝ (matrix (fin n) (fin n) ℝ) :=
 matrix.normed_space
---/
 
 lemma matrix.dot_product_self_nonneg
   {γ R} [fintype γ] [linear_ordered_ring R] (v : γ → R)
@@ -62,5 +106,7 @@ def psd_cone_set : set (matrix (fin n) (fin n) ℝ) :=
 
 noncomputable def psd_cone_dual : convex_cone ℝ (matrix (fin n) (fin n) ℝ) :=
 set.inner_dual_cone (psd_cone_set n)
+
+-/
 
 end psd_cone

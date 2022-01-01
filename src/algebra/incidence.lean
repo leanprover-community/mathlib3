8ef81cb8f09da2d18ceec15b959b78b9a47f30fa
YaelDillies <yael.dillies@gmail.com>
2022-01-01 19:48:06 +0100

    fun_like instance, additive structure, Euler characteristic

--- src/algebra/incidence.lean
+++ src/algebra/incidence.lean
@@ -16,15 +16,90 @@ open_locale big_operators
 variables (𝕜 α : Type*)
 
 /-- The `𝕜`-incidence algebra over `α`. -/
-def incidence_algebra := α → α → 𝕜
-
-instance [inhabited 𝕜] : inhabited (incidence_algebra 𝕜 α) := ⟨λ _ _, default 𝕜⟩
+structure incidence_algebra [has_zero 𝕜] [has_le α] :=
+(to_fun : α → α → 𝕜)
+(eq_zero_of_not_le' {a b : α} : ¬ a ≤ b → to_fun a b = 0)
 
 namespace incidence_algebra
-section one
-variables [decidable_eq α] [has_zero 𝕜] [has_one 𝕜]
+section zero
+variables [has_zero 𝕜] [has_le α]
 
-instance : has_one (incidence_algebra 𝕜 α) := ⟨λ a b, if a = b then 1 else 0⟩
+instance fun_like : fun_like (incidence_algebra 𝕜 α) α (λ _, α → 𝕜) :=
+⟨to_fun, λ f g h, by { cases f, cases g, congr' }⟩
+
+variables {𝕜 α}
+
+lemma eq_zero_of_not_le {f : incidence_algebra 𝕜 α} {a b : α} : ¬ a ≤ b → f a b = 0 :=
+eq_zero_of_not_le' _
+
+lemma le_of_ne_zero {f : incidence_algebra 𝕜 α} {a b : α} : f a b ≠ 0 → a ≤ b :=
+not_imp_comm.1 eq_zero_of_not_le
+
+-- completely uninteresting lemmas about coercion to function, that all homs need
+section coes
+
+-- Fallback `has_coe_to_fun` instance to help the elaborator
+instance : has_coe_to_fun (incidence_algebra 𝕜 α) (λ _, α → α → 𝕜) := ⟨to_fun⟩
+
+-- this must come after the coe_to_fun definitions
+initialize_simps_projections incidence_algebra (to_fun → apply)
+
+@[simp] lemma to_fun_eq_coe (f : incidence_algebra 𝕜 α) : f.to_fun = f := rfl
+
+@[simp] lemma coe_mk (f : α → α → 𝕜) (h) : (mk f h : α → α → 𝕜) = f := rfl
+
+protected lemma congr_fun {f g : incidence_algebra 𝕜 α} (h : f = g) (a b : α) : f a b = g a b :=
+congr_arg (λ f : incidence_algebra 𝕜 α, f a b) h
+
+protected lemma congr_arg (f : incidence_algebra 𝕜 α) {a₁ a₂ b₁ b₂ : α} (ha : a₁ = a₂)
+  (hb : b₁ = b₂) :
+  f a₁ b₁ = f a₂ b₂ :=
+congr_arg2 f ha hb
+
+lemma coe_inj ⦃f g : incidence_algebra 𝕜 α⦄ (h : (f : α → α → 𝕜) = g) : f = g :=
+by { cases f, cases g, cases h, refl }
+
+@[ext] lemma ext ⦃f g : incidence_algebra 𝕜 α⦄ (h : ∀ a b, f a b = g a b) : f = g :=
+coe_inj $ funext $ λ a, funext $ h a
+
+lemma ext_iff {f g : incidence_algebra 𝕜 α} : f = g ↔ ∀ a b, f a b = g a b :=
+⟨incidence_algebra.congr_fun, λ h, ext h⟩
+
+@[simp] lemma mk_coe (f : incidence_algebra 𝕜 α) (h) : mk f h = f := ext $ λ _ _, rfl
+
+end coes
+
+variables {𝕜 α}
+
+instance : has_zero (incidence_algebra 𝕜 α) := ⟨⟨λ _ _, 0, λ _ _ _, rfl⟩⟩
+
+@[simp] lemma zero_apply (a b : α) : (0 : incidence_algebra 𝕜 α) a b = 0 := rfl
+
+end zero
+
+section add
+variables [add_zero_class 𝕜] [has_le α]
+
+instance : has_add (incidence_algebra 𝕜 α) :=
+⟨λ f g, ⟨f + g, λ a b h, by simp_rw [pi.add_apply, eq_zero_of_not_le h, zero_add]⟩⟩
+
+@[simp] lemma add_apply (f g : incidence_algebra 𝕜 α) (a b : α) :
+  (f + g) a b = f a b + g a b := rfl
+
+end add
+
+instance [add_monoid 𝕜] [has_le α] : add_monoid (incidence_algebra 𝕜 α) :=
+{ add := (+),
+  add_assoc := λ f g h, by { ext, exact add_assoc _ _ _ },
+  zero := 0,
+  zero_add := λ f, by { ext, exact zero_add _ },
+  add_zero := λ f, by { ext, exact add_zero _ } }
+
+section one
+variables [preorder α] [decidable_eq α] [has_zero 𝕜] [has_one 𝕜]
+
+instance : has_one (incidence_algebra 𝕜 α) :=
+⟨⟨λ a b, if a = b then 1 else 0, λ a b h, ite_eq_right_iff.2 $ λ H, (h H.le).elim⟩⟩
 
 @[simp] lemma one_apply (a b : α) : (1 : incidence_algebra 𝕜 α) a b = if a = b then 1 else 0 := rfl
 
@@ -33,14 +108,15 @@ end one
 section mul
 variables [preorder α] [locally_finite_order α] [add_comm_monoid 𝕜] [has_mul 𝕜]
 
-instance : has_mul (incidence_algebra 𝕜 α) := ⟨λ f g a b, ∑ x in Icc a b, f a x * g x b⟩
+instance : has_mul (incidence_algebra 𝕜 α) :=
+⟨λ f g, ⟨λ a b, ∑ x in Icc a b, f a x * g x b, λ a b h, by rw [Icc_eq_empty h, sum_empty]⟩⟩
 
 @[simp] lemma mul_apply (f g : incidence_algebra 𝕜 α) (a b : α) :
   (f * g) a b = ∑ x in Icc a b, f a x * g x b := rfl
 
 end mul
 
-instance [decidable_eq α] [preorder α] [locally_finite_order α] [non_assoc_semiring 𝕜] :
+instance [non_assoc_semiring 𝕜] [decidable_eq α] [preorder α] [locally_finite_order α] :
   monoid (incidence_algebra 𝕜 α) :=
 { mul := (*),
   mul_assoc := λ f g h, begin
@@ -52,20 +128,19 @@ instance [decidable_eq α] [preorder α] [locally_finite_order α] [non_assoc_se
   one_mul := λ f, begin
     ext a b,
     simp_rw [mul_apply, one_apply, sum_boole_mul],
-    refine if_pos (left_mem_Icc.2 _),
-    sorry
+    exact ite_eq_left_iff.2 (not_imp_comm.1 $ λ h, left_mem_Icc.2 $ le_of_ne_zero $ ne.symm h),
   end,
   mul_one := λ f, begin
     ext a b,
     simp_rw [mul_apply, one_apply, eq_comm, sum_mul_boole],
-    convert (if_pos $ right_mem_Icc.2 _).symm,
-    sorry
+    convert (ite_eq_left_iff.2 $ not_imp_comm.1 $
+      λ h, right_mem_Icc.2 $ le_of_ne_zero $ ne.symm h).symm,
   end }
 
 section zeta
-variables [has_le α] [@decidable_rel α (≤)] [has_zero 𝕜] [has_one 𝕜]
+variables [has_zero 𝕜] [has_one 𝕜] [has_le α] [@decidable_rel α (≤)]
 
-def zeta : incidence_algebra 𝕜 α := λ a b, if a ≤ b then 1 else 0
+def zeta : incidence_algebra 𝕜 α := ⟨λ a b, if a ≤ b then 1 else 0, λ a b h, if_neg h⟩
 
 variables {𝕜 α}
 
@@ -75,8 +150,8 @@ lemma zeta_of_le {a b : α} (h : a ≤ b) : zeta 𝕜 α a b = 1 := if_pos h
 
 end zeta
 
-lemma zeta_mul_zeta [preorder α] [locally_finite_order α] [@decidable_rel α (≤)] [add_comm_monoid 𝕜]
-  [mul_one_class 𝕜] (a b : α) :
+lemma zeta_mul_zeta [add_comm_monoid 𝕜] [mul_one_class 𝕜] [preorder α] [locally_finite_order α]
+  [@decidable_rel α (≤)] (a b : α) :
   (zeta 𝕜 α * zeta 𝕜 α) a b = (Icc a b).card :=
 begin
   rw [mul_apply, card_eq_sum_ones, nat.cast_sum, nat.cast_one],
@@ -86,7 +161,7 @@ begin
 end
 
 section mu
-variables [preorder α] [locally_finite_order α] [decidable_eq α] [add_comm_monoid 𝕜] [has_one 𝕜]
+variables [add_comm_monoid 𝕜] [has_one 𝕜] [preorder α] [locally_finite_order α] [decidable_eq α]
 
 def mu_aux (a : α) : α → 𝕜
 | b := if a = b then 1 else begin
@@ -97,7 +172,9 @@ def mu_aux (a : α) : α → 𝕜
 end
 using_well_founded { rel_tac := λ _ _, `[exact ⟨_, measure_wf (λ b, (Icc a b).card)⟩] }
 
-def mu : incidence_algebra 𝕜 α := mu_aux 𝕜 α
+def mu : incidence_algebra 𝕜 α := ⟨mu_aux 𝕜 α, λ a b, not_imp_comm.1 $ λ h, begin
+  sorry
+end⟩
 
 variables {𝕜 α}
 
@@ -109,8 +186,8 @@ end
 end mu
 
 section mu_zeta
-variables [preorder α] [locally_finite_order α] [decidable_eq α] [@decidable_rel α (≤)]
-  [add_comm_monoid 𝕜] [mul_one_class 𝕜]
+variables [add_comm_monoid 𝕜] [mul_one_class 𝕜] [preorder α] [locally_finite_order α]
+  [decidable_eq α] [@decidable_rel α (≤)]
 
 lemma mu_mul_zeta : mu 𝕜 α * zeta 𝕜 α = 1 :=
 begin
@@ -127,4 +204,13 @@ begin
 end
 
 end mu_zeta
+
+section euler
+variables [add_comm_monoid 𝕜] [has_one 𝕜] [preorder α] [bounded_order α] [locally_finite_order α]
+  [decidable_eq α]
+
+/-- The Euler characteristic of a finite bounded order. -/
+def euler_char : 𝕜 := mu 𝕜 α ⊥ ⊤
+
+end euler
 end incidence_algebra

1626185f980a85aa4d2ed34dbf93fdfe203bb2f4
YaelDillies <yael.dillies@gmail.com>
2022-01-01 11:15:44 +0100

    fix set_theory.ordinal

--- src/set_theory/ordinal.lean
+++ src/set_theory/ordinal.lean
@@ -3,8 +3,9 @@ Copyright (c) 2017 Johannes Hölzl. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Mario Carneiro, Floris van Doorn
 -/
-import set_theory.cardinal
+import data.sum.order
 import order.conditionally_complete_lattice
+import set_theory.cardinal
 
 /-!
 # Ordinals

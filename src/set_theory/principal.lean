22cebba2ce73a4b5dfa5ef601d8fc5194f672d08
Violeta Hernández <vi.hdz.p@gmail.com>
2022-01-01 21:37:47 -0600

    Tweaks

--- src/set_theory/principal.lean
+++ src/set_theory/principal.lean
@@ -3,9 +3,11 @@ Copyright (c) 2022 Violeta Hernández Palacios. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Violeta Hernández Palacios
 -/
-import .ordinal_arithmetic
+import set_theory.ordinal_arithmetic
 
 universe u
+
+namespace ordinal
 
 /-- An ordinal `o` is said to be principal or indecomposable under an operation when the set of
 ordinals less than it is closed under that operation. In standard mathematical usage, this term is
@@ -15,15 +17,17 @@ For simplicity, we break usual convention and regard 0 as principal. -/
 def principal (op : ordinal.{u} → ordinal.{u} → ordinal.{u}) (o : ordinal.{u}) : Prop :=
 ∀ {a b}, a < o → b < o → op a b < o
 
-theorem fp_iff_principal {op : ordinal.{u} → ordinal.{u} → ordinal.{u}}
-  (hop : ∀ a < o, is_normal (op a)) (o : ordinal.{u}) : principal op o ↔ ∀ a < o, op a o = o :=
+theorem fp_iff_principal {op : ordinal.{u} → ordinal.{u} → ordinal.{u}} (o : ordinal.{u})
+  (hop : ∀ a < o, is_normal (op a)) : principal op o ↔ ∀ a < o, op a o = o :=
 sorry
 
 theorem zero_principal (op : ordinal.{u} → ordinal.{u} → ordinal.{u}) : principal op 0 :=
 λ a _ h, (not_lt_of_le (ordinal.zero_le a) h).elim
 
+/-! ### Additive principal ordinals -/
+
 theorem omega_additive_principal : principal (+) ordinal.omega.{u} :=
-sorry
+@add_lt_omega
 
 theorem mul_omega_additive_principal (o : ordinal.{u}) : principal (+) (o * ordinal.omega.{u}) :=
 begin
@@ -35,9 +39,16 @@ end
 
 theorem omega_pow_additive_principal (o : ordinal.{u}) : principal (+) (ordinal.omega.{u} ^ o) :=
 begin
+  -- If o is zero, this is trivial.
   sorry
 end
 
 theorem additive_principal_iff_zero_or_omega_pow (o : ordinal.{u}) :
   principal (+) o ↔ o = 0 ∨ ∃ a : ordinal.{u}, o = ordinal.omega.{u} ^ a :=
 sorry
+
+/-! ### Multiplicative principal ordinals -/
+theorem omega_multiplicative_principal : principal (*) ordinal.omega.{u} :=
+@mul_lt_omega
+
+end ordinal

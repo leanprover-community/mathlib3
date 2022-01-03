43ab7631eb7b17e0b529d7af218098d70044d767
Riccardo Brasca <riccardo.brasca@gmail.com>
2022-01-03 14:24:54 +0100

    fix build

--- src/ring_theory/discriminant.lean
+++ src/ring_theory/discriminant.lean
@@ -208,7 +208,7 @@ begin
   rw [ring_hom.map_mul, ring_hom.map_pow, ring_hom.map_neg, ring_hom.map_one,
     of_power_basis_eq_prod'' _ _ _ e],
   congr,
-  rw [norm_eq_prod_embeddings, fin.prod_filter_gt_mul_neg_eq_prod_off_diag],
+  rw [norm_eq_prod_embeddings, fin.prod_filter_lt_mul_neg_eq_prod_off_diag],
   conv_rhs { congr, skip, funext,
     rw [← aeval_alg_hom_apply, aeval_root_derivative_of_splits (minpoly.monic
       (is_separable.is_integral K pb.gen)) (is_alg_closed.splits_codomain _) (hroots σ),

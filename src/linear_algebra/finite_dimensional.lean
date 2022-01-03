9bdeb734b5da51e0d1ea10be4f8b0152b3dc47bb
Heather Macbeth <25316162+hrmacbeth@users.noreply.github.com>
2022-01-03 13:09:30 -0500

    fix

--- src/linear_algebra/finite_dimensional.lean
+++ src/linear_algebra/finite_dimensional.lean
@@ -348,15 +348,15 @@ begin
   set b := basis.extend this with b_eq,
   letI : fintype (this.extend _) :=
     (finite_of_linear_independent (by simpa using b.linear_independent)).fintype,
-  letI : fintype (subtype.val '' basis.of_vector_space_index K S) :=
+  letI : fintype (coe '' basis.of_vector_space_index K S) :=
     (finite_of_linear_independent this).fintype,
   letI : fintype (basis.of_vector_space_index K S) :=
     (finite_of_linear_independent (by simpa using bS.linear_independent)).fintype,
-  have : subtype.val '' (basis.of_vector_space_index K S) = this.extend (set.subset_univ _),
+  have : coe '' (basis.of_vector_space_index K S) = this.extend (set.subset_univ _),
   from set.eq_of_subset_of_card_le (this.subset_extend _)
-    (by rw [set.card_image_of_injective _ subtype.val_injective, ← finrank_eq_card_basis bS,
+    (by rw [set.card_image_of_injective _ subtype.coe_injective, ← finrank_eq_card_basis bS,
          ← finrank_eq_card_basis b, h]; apply_instance),
-  rw [← b.span_eq, b_eq, basis.coe_extend, subtype.range_coe, ← this, ← subtype_eq_val, span_image],
+  rw [← b.span_eq, b_eq, basis.coe_extend, subtype.range_coe, ← this, ← subtype_eq_coe, span_image],
   have := bS.span_eq,
   rw [bS_eq, basis.coe_of_vector_space, subtype.range_coe] at this,
   rw [this, map_top (submodule.subtype S), range_subtype],

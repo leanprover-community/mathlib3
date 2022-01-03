68c2af215db9b91bc7ac2b11f4810b271c5c4c45
Eric Wieser <wieser.eric@gmail.com>
2022-01-03 17:13:25 +0000

    fix

--- src/ring_theory/graded_algebra/basic.lean
+++ src/ring_theory/graded_algebra/basic.lean
@@ -84,8 +84,8 @@ def graded_algebra.decompose : A ≃ₐ[R] ⨁ i, 𝒜 i := alg_equiv.symm
   (graded_algebra.decompose 𝒜).symm (direct_sum.of _ i x) = x :=
 direct_sum.submodule_coe_alg_hom_of 𝒜 _ _
 
-lemma is_homogeneous_decompose {i : ι} (x : 𝒜 i) :
-  set_like.is_homogeneous 𝒜 (graded_algebra.decompose 𝒜 x i) :=
+lemma is_homogeneous_decompose {i : ι} (x : A) :
+  set_like.is_homogeneous 𝒜 (graded_algebra.decompose 𝒜 x i : A) :=
 ⟨i, (graded_algebra.decompose 𝒜 x i).prop⟩
 
 /-- The projection maps of graded algebra-/

fffe19c9c25195ff222e9908c628c06af640a01b
Antoine Labelle <antoinelab01@gmail.com>
2022-01-03 12:12:58 -0500

    copyright headers

--- src/representation_theory/fixed_space.lean
+++ src/representation_theory/fixed_space.lean
@@ -1,6 +1,9 @@
+/-
+Copyright (c) 2022 Antoine Labelle. All rights reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
+Authors: Antoine Labelle.
+-/
 import representation_theory.basic
-
---noncomputable theory
 
 open_locale big_operators
 open monoid_algebra

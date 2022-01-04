b7e26698c55a612fb033bf626afb330a1c6dff74
Johan Commelin <johan@commelin.net>
2022-01-04 07:44:42 +0100

    Update src/field_theory/intermediate_field.lean

--- src/field_theory/intermediate_field.lean
+++ src/field_theory/intermediate_field.lean
@@ -248,10 +248,10 @@ def map (f : L →ₐ[K] L') : intermediate_field K L' :=
   neg_mem' := λ x hx, (S.to_subalgebra.map f).neg_mem hx,
   .. S.to_subalgebra.map f}
 
-lemma map_map {K L1 L2 L3 : Type*} [field K] [field L1] [algebra K L1]
-[field L2] [algebra K L2] [field L3] [algebra K L3]
-(E : intermediate_field K L1) (f : L1 →ₐ[K] L2) (g : L2 →ₐ[K] L3) :
-(E.map f).map g = E.map (g.comp f) :=
+lemma map_map {K L₁ L₂ L₃ : Type*} [field K] [field L₁] [algebra K L₁]
+  [field L₂] [algebra K L₂] [field L₃] [algebra K L₃]
+  (E : intermediate_field K L₁) (f : L₁ →ₐ[K] L₂) (g : L₂ →ₐ[K] L₃) :
+  (E.map f).map g = E.map (g.comp f) :=
 set_like.coe_injective $ set.image_image _ _ _
 
 /-- The embedding from an intermediate field of `L / K` to `L`. -/

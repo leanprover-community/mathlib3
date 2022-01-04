37596f048ab9127e78a4f1abafe685ae1c72f014
Stuart Presnell <stuart.presnell@gmail.com>
2022-01-04 05:13:32 +0000

    Create prime_reciprocals.lean

---
+++ src/data/nat/prime_reciprocals.lean
@@ -0,0 +1,15 @@
+/-
+Copyright (c) 2022 Stuart Presnell. All rights reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
+Author: Stuart Presnell
+-/
+
+import data.nat.prime
+import topology.algebra.infinite_sum
+
+/-- Implementing James Clarkson's (1966) proof that the sum of the prime reciprocals diverges.
+
+Clarkson, James A. (1966) On the series of prime reciprocals. Proc. Amer. Math.
+Soc., 17: 541; MR 32, #5573.
+
+ -/

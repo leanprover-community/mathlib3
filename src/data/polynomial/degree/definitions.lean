f57eec3e42d308ea51c39fe13dbcbee7a2a589eb
Eric Wieser <wieser.eric@gmail.com>
2022-01-04 15:21:17 +0000

    chore(data/polynomial, data/finset/lattice): basic lemmas
    Co-authored-by: Iván Sadofschi Costa <isadofschi@users.noreply.github.com>

--- src/data/polynomial/degree/definitions.lean
+++ src/data/polynomial/degree/definitions.lean
@@ -128,6 +128,9 @@ lemma le_nat_degree_of_mem_supp (a : ℕ) :
   a ∈ p.support → a ≤ nat_degree p:=
 le_nat_degree_of_ne_zero ∘ mem_support_iff.mp
 
+lemma degree_mono [semiring S] {f : polynomial R} {g : polynomial S}
+  (h : f.support ⊆ g.support) : f.degree ≤ g.degree := finset.sup_mono h
+
 lemma supp_subset_range (h : nat_degree p < m) : p.support ⊆ finset.range m :=
 λ n hn, mem_range.2 $ (le_nat_degree_of_mem_supp _ hn).trans_lt h
 

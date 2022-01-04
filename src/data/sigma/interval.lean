d9b82866c6cdb01f5b5baf9289539bcc3a71e956
YaelDillies <yael.dillies@gmail.com>
2022-01-04 16:53:28 +0100

    bump

--- src/data/sigma/interval.lean
+++ src/data/sigma/interval.lean
@@ -17,85 +17,6 @@ calculates the cardinality of its finite intervals.
 Do the same for the lexicographical order
 -/
 
-open function
-
-section ite
-variables {α β γ : Sort*} {σ : α → Sort*} (f : α → β) {P Q : Prop} [decidable P] [decidable Q]
-  {a b c : α} {A : P → α} {B : ¬ P → α}
-
-@[simp] lemma dite_eq_right_iff : dite P A (λ _, b) = b ↔ ∀ h, A h = b := sorry
-lemma dite_ne_right_iff : dite P A (λ _, b) ≠ b ↔ ∃ h, A h ≠ b := sorry
-
-end ite
-
-namespace finset
-variables {α β : Type*} {f : α ↪ β} {s : finset α}
-
-lemma map_nonempty : (s.map f).nonempty ↔ s.nonempty :=
-by rw [nonempty_iff_ne_empty, nonempty_iff_ne_empty, ne.def, map_eq_empty]
-
-end finset
-
-namespace finset
-variables {ι : Type*} {α : ι → Type*} [decidable_eq ι]
-
-/-- Lifts maps `α i → α i → finset (α i)` to a map `Σ i, α i → Σ i, α i → finset (Σ i, α i)`. -/
-def sigma_lift (f : Π ⦃i⦄, α i → α i → finset (α i)) (a b : Σ i, α i) : finset (Σ i, α i) :=
-dite (a.1 = b.1) (λ h, (f (h.rec a.2) b.2).map $ embedding.sigma_mk _) (λ _, ∅)
-
-lemma mem_sigma_lift (f : Π ⦃i⦄, α i → α i → finset (α i)) (a b x : Σ i, α i) :
-  x ∈ sigma_lift f a b ↔ ∃ (ha : a.1 = x.1) (hb : b.1 = x.1), x.2 ∈ f (ha.rec a.2) (hb.rec b.2) :=
-begin
-  obtain ⟨⟨i, a⟩, j, b⟩ := ⟨a, b⟩,
-  obtain rfl | h := decidable.eq_or_ne i j,
-  { split,
-    { simp_rw [sigma_lift, dif_pos rfl, mem_map, embedding.sigma_mk_apply],
-      rintro ⟨x, hx, rfl⟩,
-      exact ⟨rfl, rfl, hx⟩ },
-    { rintro ⟨⟨⟩, ⟨⟩, hx⟩,
-      rw [sigma_lift, dif_pos rfl, mem_map],
-      exact ⟨_, hx, by simp [sigma.ext_iff]⟩ } },
-  { rw [sigma_lift, dif_neg h],
-    refine iff_of_false (not_mem_empty _) _,
-    rintro ⟨⟨⟩, ⟨⟩, _⟩,
-    exact h rfl }
-end
-
-variables {f g : Π ⦃i⦄, α i → α i → finset (α i)} {a b : Σ i, α i}
-
-lemma sigma_lift_nonempty :
-  (sigma_lift f a b).nonempty ↔ ∃ h : a.1 = b.1, (f (h.rec a.2) b.2).nonempty :=
-begin
-  simp_rw nonempty_iff_ne_empty,
-  convert dite_ne_right_iff,
-  ext h,
-  simp_rw [←nonempty_iff_ne_empty],
-  exact map_nonempty.symm,
-end
-
-lemma sigma_lift_eq_empty : (sigma_lift f a b) = ∅ ↔ ∀ h : a.1 = b.1, (f (h.rec a.2) b.2) = ∅ :=
-begin
-  convert dite_eq_right_iff,
-  exact forall_congr_eq (λ h, propext map_eq_empty.symm),
-end
-
-lemma sigma_lift_mono (h : ∀ ⦃i⦄ ⦃a b : α i⦄, f a b ⊆ g a b) (a b : Σ i, α i) :
-  sigma_lift f a b ⊆ sigma_lift g a b :=
-begin
-  rintro x hx,
-  rw mem_sigma_lift at ⊢ hx,
-  obtain ⟨ha, hb, hx⟩ := hx,
-  exact ⟨ha, hb, h hx⟩,
-end
-
-variables (f a b)
-
-lemma card_sigma_lift :
-  (sigma_lift f a b).card = dite (a.1 = b.1) (λ h, (f (h.rec a.2) b.2).card) (λ _, 0) :=
-by { convert apply_dite _ _ _ _, ext h, exact (card_map _).symm }
-
-end finset
-
 open finset function
 
 namespace sigma
@@ -135,20 +56,20 @@ instance : locally_finite_order (Σ i, α i) :=
 section
 variables (a b : Σ i, α i)
 
-@[simp] lemma card_Icc : (Icc a b).card = dite (a.1 = b.1)
-    (λ h, (Icc (h.rec a.2) b.2).card) (λ _, 0) :=
+@[simp] lemma card_Icc :
+  (Icc a b).card = dite (a.1 = b.1) (λ h, (Icc (h.rec a.2) b.2).card) (λ _, 0) :=
 card_sigma_lift _ _ _
 
-@[simp] lemma card_Ico : (Ico a b).card = dite (a.1 = b.1)
-    (λ h, (Ico (h.rec a.2) b.2).card) (λ _, 0) :=
+@[simp] lemma card_Ico :
+  (Ico a b).card = dite (a.1 = b.1) (λ h, (Ico (h.rec a.2) b.2).card) (λ _, 0) :=
 card_sigma_lift _ _ _
 
-@[simp] lemma card_Ioc : (Ioc a b).card = dite (a.1 = b.1)
-    (λ h, (Ioc (h.rec a.2) b.2).card) (λ _, 0) :=
+@[simp] lemma card_Ioc :
+  (Ioc a b).card = dite (a.1 = b.1) (λ h, (Ioc (h.rec a.2) b.2).card) (λ _, 0) :=
 card_sigma_lift _ _ _
 
-@[simp] lemma card_Ioo : (Ioo a b).card = dite (a.1 = b.1)
-    (λ h, (Ioo (h.rec a.2) b.2).card) (λ _, 0) :=
+@[simp] lemma card_Ioo :
+  (Ioo a b).card = dite (a.1 = b.1) (λ h, (Ioo (h.rec a.2) b.2).card) (λ _, 0) :=
 card_sigma_lift _ _ _
 
 end

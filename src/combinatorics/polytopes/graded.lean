24f30f1039a279cb8d0992a73f1cb4cc15a61550
ocornoc <ocornoc@protonmail.com>
2022-01-05 16:51:40 -0800

    remove unnecessary import

--- src/combinatorics/polytopes/graded.lean
+++ src/combinatorics/polytopes/graded.lean
@@ -5,7 +5,6 @@ Authors: Grayson Burton, Yaël Dillies, Violeta Hernández Palacios
 -/
 import category_theory.category.basic
 import data.finsupp.order
-import data.finsupp.basic
 import data.nat.interval
 import data.set.intervals.ord_connected
 import data.sigma.order

71ed06d6e7ad6a8e685dce12bcea3153d1f07a91
Eric Wieser <wieser.eric@gmail.com>
2022-01-06 19:24:21 +0000

    golf

--- src/algebra/free_non_unital_non_assoc_algebra.lean
+++ src/algebra/free_non_unital_non_assoc_algebra.lean
@@ -41,9 +41,7 @@ noncomputable theory
 
 variables (R : Type u) (X : Type v) [semiring R]
 
-/-- The free non-unital, non-associative algebra on the type `X` with coefficients in `R`. -/
-@[derive [inhabited, non_unital_non_assoc_semiring]]
-def free_non_unital_non_assoc_algebra := monoid_algebra R (free_magma X)
+abbreviation free_non_unital_non_assoc_algebra := monoid_algebra R (free_magma X)
 
 namespace free_non_unital_non_assoc_algebra
 
@@ -52,41 +50,6 @@ variables {X}
 /-- The embedding of `X` into the free algebra with coefficients in `R`. -/
 def of : X → free_non_unital_non_assoc_algebra R X :=
 (monoid_algebra.of_magma R _) ∘ free_magma.of
-
-instance {S : Type*} [monoid S] [distrib_mul_action S R] :
-  distrib_mul_action S (free_non_unital_non_assoc_algebra R X) :=
-monoid_algebra.distrib_mul_action
-
-instance {S₁ S₂ : Type*} [monoid S₁] [monoid S₂] [distrib_mul_action S₁ R] [distrib_mul_action S₂ R]
-  [has_scalar S₁ S₂] [is_scalar_tower S₁ S₂ R] :
-  is_scalar_tower S₁ S₂ (free_non_unital_non_assoc_algebra R X) :=
-monoid_algebra.is_scalar_tower
-
-instance {S₁ S₂ : Type*} [monoid S₁] [monoid S₂] [distrib_mul_action S₁ R] [distrib_mul_action S₂ R]
-  [smul_comm_class S₁ S₂ R] :
-  smul_comm_class S₁ S₂ (free_non_unital_non_assoc_algebra R X) :=
-monoid_algebra.smul_comm_class
-
-instance {S : Type*} [monoid S] [distrib_mul_action S R] [distrib_mul_action Sᵐᵒᵖ R]
-  [is_central_scalar S R] : is_central_scalar S (free_non_unital_non_assoc_algebra R X) :=
-monoid_algebra.is_central_scalar
-
-instance is_scalar_tower_self {S : Type*} [monoid S] [distrib_mul_action S R]
-  [is_scalar_tower S R R] : is_scalar_tower S
-    (free_non_unital_non_assoc_algebra R X) (free_non_unital_non_assoc_algebra R X) :=
-monoid_algebra.is_scalar_tower_self R
-
-instance smul_comm_class_self {S : Type*} [monoid S] [distrib_mul_action S R]
-  [smul_comm_class S R R] : smul_comm_class S
-    (free_non_unital_non_assoc_algebra R X) (free_non_unital_non_assoc_algebra R X) :=
-monoid_algebra.smul_comm_class_self R
-
-instance {S : Type*} [semiring S] [module S R] : module S (free_non_unital_non_assoc_algebra R X) :=
-monoid_algebra.module
-
-instance (R : Type u) [ring R] :
-  non_unital_non_assoc_ring (free_non_unital_non_assoc_algebra R X) :=
-monoid_algebra.non_unital_non_assoc_ring
 
 variables {A : Type w} [non_unital_non_assoc_semiring A]
 variables [module R A] [is_scalar_tower R A A] [smul_comm_class R A A]
@@ -109,18 +72,15 @@ rfl
   F ∘ (of R) = f ↔ F = lift R f :=
 (lift R).symm_apply_eq
 
-attribute [irreducible] of lift
-
 @[simp] lemma lift_of_apply (f : X → A) (x) : lift R f (of R x) = f x :=
-by rw [← function.comp_app (lift R f) (of R) x, of_comp_lift]
+congr_fun (of_comp_lift _ f) x
 
 @[simp] lemma lift_comp_of (F : non_unital_alg_hom R (free_non_unital_non_assoc_algebra R X) A) :
   lift R (F ∘ (of R)) = F :=
-by rw [← lift_symm_apply, equiv.apply_symm_apply]
+(lift R).apply_symm_apply F
 
 @[ext] lemma hom_ext {F₁ F₂ : non_unital_alg_hom R (free_non_unital_non_assoc_algebra R X) A}
   (h : ∀ x, F₁ (of R x) = F₂ (of R x)) : F₁ = F₂ :=
-have h' : (lift R).symm F₁ = (lift R).symm F₂, { ext, simp [h], },
-(lift R).symm.injective h'
+(lift R).symm.injective $ funext h
 
 end free_non_unital_non_assoc_algebra

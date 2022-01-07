8091da1ab6019a8e6e74bf45b7ee5385e2787687
Christopher Hoskin <christopher.hoskin@gmail.com>
2022-01-07 19:24:55 +0000

    Remove commented out lines

--- src/algebra/jordan/special.lean
+++ src/algebra/jordan/special.lean
@@ -4,8 +4,6 @@ Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Christopher Hoskin
 -/
 import algebra.jordan.basic
---import data.equiv.basic
---import logic.nontrivial
 
 /-!
 # Special Jordan algebras

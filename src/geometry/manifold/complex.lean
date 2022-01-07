4d7e9309e9e1b3a246d80238acfa93363ed0f407
Heather Macbeth <25316162+hrmacbeth@users.noreply.github.com>
2022-01-07 17:08:43 -0500

    experiment

---
+++ src/geometry/manifold/complex.lean
@@ -0,0 +1,92 @@
+import geometry.manifold.times_cont_mdiff
+import analysis.complex.cauchy_integral
+import topology.locally_constant.basic
+
+open_locale manifold topological_space
+open complex
+
+variables {M : Type*} [topological_space M] [charted_space ℂ M]
+  [smooth_manifold_with_corners 𝓘(ℂ) M]
+
+/-
+F = f + i g
+0 = d(f ^2 + g ^2 )
+  = 2fdf + 2gdg
+
+df, dg are linearly dependent
+-/
+
+-- example
+
+example {E : Type*} [normed_group E] [normed_space ℂ E] (f : ℂ → E) (s : set ℂ) (hs : is_open s)
+  (c : ℝ) (hf : ∀ x ∈ s, ∥f x∥ = c)
+  (f' : ℂ → E) (hf' : ∀ x ∈ s, has_strict_deriv_at f (f' x) x) (x : ℂ) (hx : x ∈ s) :
+  f' x = 0 :=
+begin
+  by_contradiction,
+  have H₁ := has_strict_deriv_at.map_nhds_eq (hf' x hx) h,
+  have H₂ : s ∈ 𝓝 x := hs.mem_nhds hx,
+  have H₃ : f '' s ∈ filter.map f (𝓝 x) := filter.image_mem_map H₂,
+  rw H₁ at H₃,
+  rw metric.mem_nhds_iff at H₃,
+  obtain ⟨ε, hε, H₄⟩ := H₃,
+  by_cases hfx : f x = 0,
+  { sorry },
+  let η : ℝ := sorry, -- ε / (2 * ∥f x∥)
+  have hη : 0 < η := sorry,
+  have hη' : η * abs (f x) < ε := sorry,
+  have H₅ : (1 + η) • f x ∈ metric.ball (f x) ε,
+  { simp [dist_eq_norm, norm_eq_abs],
+    calc abs ((1 + ↑η) * f x - f x)
+        = abs ((η : ℂ) * f x) : by congr; ring
+    ... < ε : _,
+    simp,
+    rw _root_.abs_of_nonneg hη.le,
+    exact hη' },
+  obtain ⟨y, hys, hy⟩ := H₄ H₅,
+  have H₆ := congr_arg norm hy,
+  simp only [of_real_add, normed_field.norm_mul, real_smul, of_real_one] at H₆,
+  rw (hf y hys).trans (hf x hx).symm at H₆,
+  simp only [norm_eq_abs] at H₆,
+  norm_cast at H₆,
+  have H₇ : (1 - |1 + η|) * abs (f x) = 0,
+  { linarith, },
+  rw _root_.abs_of_nonneg (by linarith : 0 ≤ 1 + η) at H₇,
+  have H₈ : abs (f x) ≠ 0 := by simpa using hfx,
+  have H₉ : 1 - (1 + η) ≠ 0 := by linarith,
+  have := mul_ne_zero H₉ H₈,
+  contradiction,
+end
+
+example {f : M → ℂ} (hf : times_cont_mdiff 𝓘(ℂ) 𝓘(ℂ) 1 f) :
+  is_locally_constant f :=
+begin
+  rw (is_locally_constant.tfae f).out 0 4,
+  intros p,
+  refine ⟨(chart_at ℂ p).source, (chart_at ℂ p).open_source, mem_chart_source ℂ p, _⟩,
+  intros x hx,
+  rw times_cont_mdiff_iff at hf,
+  have := hf.2 p,
+  simp at this,
+  have := this.differentiable_on rfl.le,
+  have := is_open_set_of_mem_nhds_and_is_max_on_norm this,
+  simp at this,
+end
+
+-- example {f : M → ℂ} (hf : times_cont_mdiff 𝓘(ℂ) 𝓘(ℂ) 1 f) :
+--   is_open {p | is_max_on (norm ∘ f) set.univ p}  :=
+-- begin
+--   refine is_open_iff_mem_nhds.2 (λ p hp, _),
+--   rw ← (chart_at ℂ p).symm_map_nhds_eq (mem_chart_source ℂ p),
+--   rw filter.mem_map,
+--   let s := (chart_at ℂ p).symm ⁻¹' {z | is_max_on (norm ∘ f) set.univ z},
+--   have hs : s ⊆ (chart_at ℂ p).target := sorry,
+--   rw times_cont_mdiff_iff at hf,
+--   have := hf.2 p,
+--   simp at this,
+--   have := (this.differentiable_on rfl.le).mono hs,
+--   convert (is_open_set_of_mem_nhds_and_is_max_on_norm this).mem_nhds _,
+--   { ext x,
+--     simp,
+--   },
+-- end

00d56ab46d1f5e430d31eadee0d16aeca3090c03
YaelDillies <yael.dillies@gmail.com>
2022-01-07 17:58:02 +0100

    fix data.set.finite

--- src/data/set/finite.lean
+++ src/data/set/finite.lean
@@ -4,6 +4,7 @@ Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Johannes Hölzl, Mario Carneiro
 -/
 import data.finset.sort
+import data.set.functor
 
 /-!
 # Finite sets

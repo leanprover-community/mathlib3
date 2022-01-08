c7fa66eac25cfdb52638a590cfcf372dd2c18415
Bhavik Mehta <bhavikmehta8@gmail.com>
2022-01-08 15:04:33 +0000

    feat(data/nat/prime): power to factor count divides natural (#11226)

--- src/data/nat/prime.lean
+++ src/data/nat/prime.lean
@@ -821,6 +821,18 @@ begin
       nat.prod_factors hb]
 end
 
+lemma pow_factors_count_dvd (n p : ℕ) :
+  p ^ n.factors.count p ∣ n :=
+begin
+  by_cases hp : p.prime,
+  { apply dvd_of_factors_subperm (pow_ne_zero _ hp.ne_zero),
+    rw [hp.factors_pow, list.subperm_ext_iff],
+    intros q hq,
+    simp [list.eq_of_mem_repeat hq] },
+  { rw count_eq_zero_of_not_mem (mt prime_of_mem_factors hp),
+    simp },
+end
+
 end
 
 lemma succ_dvd_or_succ_dvd_of_succ_sum_dvd_mul {p : ℕ} (p_prime : prime p) {m n k l : ℕ}

e9ff3b78348d986f87ed3f4bbd2dbb719ac0d2e4
Alex J. Best <alex.j.best@gmail.com>
2022-01-10 23:57:29 +0100

    no fact

--- src/algebra/char_p/basic.lean
+++ src/algebra/char_p/basic.lean
@@ -117,10 +117,10 @@ end ring_char
 
 variable {R}
 
-theorem add_pow_prime_eq_pow_add_pow_add_prime_mul_of_commute [semiring R] (p : ℕ)
-  [fact p.prime] (x y : R) (h : commute x y) : ∃ r : R, (x + y) ^ p = x ^ p + y ^ p + p * r :=
+theorem add_pow_prime_eq_pow_add_pow_add_prime_mul_of_commute [semiring R] {p : ℕ}
+  (hp : p.prime) (x y : R) (h : commute x y) : ∃ r : R, (x + y) ^ p = x ^ p + y ^ p + p * r :=
 begin
-  have : p = p - 1 + 1 := (nat.succ_pred_prime (fact.out _)).symm,
+  have : p = p - 1 + 1 := (nat.succ_pred_prime hp).symm,
   rw [commute.add_pow h, finset.sum_range_succ_comm, tsub_self, pow_zero, nat.choose_self,
     nat.cast_one, mul_one, mul_one, this, finset.sum_range_succ'],
   simp only [this.symm, tsub_zero, mul_one, one_mul, nat.choose_zero_right, nat.cast_one, pow_zero],
@@ -137,20 +137,20 @@ begin
   congr,
   norm_cast,
   rw nat.div_mul_cancel,
-  exact nat.prime.dvd_choose_self (nat.succ_pos _) (lt_tsub_iff_right.mp hi) (fact.out _),
+  exact nat.prime.dvd_choose_self (nat.succ_pos _) (lt_tsub_iff_right.mp hi) hp,
 end
 
-theorem add_pow_prime_eq_pow_add_pow_add_prime_mul [comm_semiring R] (p : ℕ)
-  [fact p.prime] (x y : R) : ∃ r : R, (x + y) ^ p = x ^ p + y ^ p + p * r :=
-add_pow_prime_eq_pow_add_pow_add_prime_mul_of_commute _ _ _ (commute.all _ _)
+theorem add_pow_prime_eq_pow_add_pow_add_prime_mul [comm_semiring R] {p : ℕ}
+  (hp : p.prime) (x y : R) : ∃ r : R, (x + y) ^ p = x ^ p + y ^ p + p * r :=
+add_pow_prime_eq_pow_add_pow_add_prime_mul_of_commute hp _ _ (commute.all _ _)
 
 variable (R)
 
-theorem add_pow_char_of_commute [semiring R] {p : ℕ} [fact p.prime]
+theorem add_pow_char_of_commute [semiring R] {p : ℕ} [hp : fact p.prime]
   [char_p R p] (x y : R) (h : commute x y) :
   (x + y)^p = x^p + y^p :=
 begin
-  rcases add_pow_prime_eq_pow_add_pow_add_prime_mul_of_commute p x y h with ⟨r, hr⟩,
+  rcases add_pow_prime_eq_pow_add_pow_add_prime_mul_of_commute hp.out x y h with ⟨r, hr⟩,
   simp [hr],
 end
 

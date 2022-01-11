395a839b3616930dcdd7b2363f686c1495c764a2
Violeta Hernández <vi.hdz.p@gmail.com>
2022-01-10 23:29:49 -0600

    Added myself as author

--- src/set_theory/cardinal_ordinal.lean
+++ src/set_theory/cardinal_ordinal.lean
@@ -1,7 +1,7 @@
 /-
 Copyright (c) 2017 Johannes Hölzl. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
-Authors: Johannes Hölzl, Mario Carneiro, Floris van Doorn
+Authors: Johannes Hölzl, Mario Carneiro, Floris van Doorn, Violeta Hernández Palacios
 -/
 
 import logic.small

3a34531745cbb26f3e5d78fd688c746e0877beb0
Christopher Hoskin <mans0954@users.noreply.github.com>
2022-01-11 06:26:12 +0000

    Update src/algebra/jordan/basic.lean
    Co-authored-by: Alex J Best <alex.j.best@gmail.com>

--- src/algebra/jordan/basic.lean
+++ src/algebra/jordan/basic.lean
@@ -45,7 +45,7 @@ variables {A : Type*} [non_unital_non_assoc_ring A] [module ℝ A] [smul_comm_cl
 
 ## Main results
 
-- lin_jordan : Linearisation of the commutative Jordan axiom
+- `lin_jordan` : Linearisation of the commutative Jordan axiom
 
 ## Implementation notes
 

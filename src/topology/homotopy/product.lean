ae56ffe34c77b29099eba0eaa9af2b46cb204c8a
prakol16 <prakol16@users.noreply.github.com>
2022-01-11 11:38:00 -0500

    Update src/topology/homotopy/product.lean
    Fix copyright year
    
    Co-authored-by: Johan Commelin <johan@commelin.net>

--- src/topology/homotopy/product.lean
+++ src/topology/homotopy/product.lean
@@ -1,5 +1,5 @@
 /-
-Copyright (c) 2022 Praneeth Kolichala. All rights reserved.
+Copyright (c) 2021 Praneeth Kolichala. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Praneeth Kolichala
 -/

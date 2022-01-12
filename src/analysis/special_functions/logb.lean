b0cb95b42e04747dd62297df63e14fdf1e65a0cf
Bolton Bailey <bolton.bailey@gmail.com>
2022-01-12 14:32:12 -0800

    sectioning

--- src/analysis/special_functions/logb.lean
+++ src/analysis/special_functions/logb.lean
@@ -35,10 +35,24 @@ be `logb b |x|` for `x < 0`, and `0` for `x = 0`.-/
 
 lemma log_div_log : log x / log b = logb b x := rfl
 
--- lemma log_of_ne_zero (hx : x ≠ 0) : log x = exp_order_iso.symm ⟨|x|, abs_pos.2 hx⟩ := dif_neg hx
+@[simp] lemma logb_zero : logb b 0 = 0 := by simp [logb]
 
--- lemma log_of_pos (hx : 0 < x) : log x = exp_order_iso.symm ⟨x, hx⟩ :=
--- by { rw [log_of_ne_zero hx.ne'], congr, exact abs_of_pos hx }
+@[simp] lemma logb_one : logb b 1 = 0 := by simp [logb]
+
+@[simp] lemma logb_abs (x : ℝ) : logb b (|x|) = logb b x := by rw [logb, logb, log_abs]
+
+@[simp] lemma logb_neg_eq_logb (x : ℝ) : logb b (-x) = logb b x :=
+by rw [← logb_abs x, ← logb_abs (-x), abs_neg]
+
+lemma logb_mul (hx : x ≠ 0) (hy : y ≠ 0) : logb b (x * y) = logb b x + logb b y :=
+by simp_rw [logb, log_mul hx hy, add_div]
+
+lemma logb_div (hx : x ≠ 0) (hy : y ≠ 0) : logb b (x / y) = logb b x - logb b y :=
+by simp_rw [logb, log_div hx hy, sub_div]
+
+@[simp] lemma logb_inv (x : ℝ) : logb b (x⁻¹) = -logb b x := by simp [logb, neg_div]
+
+section b_pos_and_ne_one
 
 @[simp] lemma logb_rpow (b_pos : 0 < b) (b_ne_one : b ≠ 1) :
   logb b (b ^ x) = x :=
@@ -79,15 +93,6 @@ lemma logb_surjective (b_pos : 0 < b) (b_ne_one : b ≠ 1) : surjective (logb b)
 @[simp] lemma range_logb (b_pos : 0 < b) (b_ne_one : b ≠ 1) : range (logb b) = univ :=
 (logb_surjective b_pos b_ne_one).range_eq
 
-@[simp] lemma logb_zero : logb b 0 = 0 := by simp [logb]
-
-@[simp] lemma logb_one : logb b 1 = 0 := by simp [logb]
-
-@[simp] lemma logb_abs (x : ℝ) : logb b (|x|) = logb b x := by rw [logb, logb, log_abs]
-
-@[simp] lemma logb_neg_eq_logb (x : ℝ) : logb b (-x) = logb b x :=
-by rw [← logb_abs x, ← logb_abs (-x), abs_neg]
-
 lemma surj_on_logb' (b_pos : 0 < b) (b_ne_one : b ≠ 1) : surj_on (logb b) (Iio 0) univ :=
 begin
   intros x x_in_univ,
@@ -97,24 +102,22 @@ begin
   { rw [logb_neg_eq_logb, logb_rpow b_pos b_ne_one], },
 end
 
-lemma logb_mul (hx : x ≠ 0) (hy : y ≠ 0) : logb b (x * y) = logb b x + logb b y :=
-by simp_rw [logb, log_mul hx hy, add_div]
+end b_pos_and_ne_one
 
-lemma logb_div (hx : x ≠ 0) (hy : y ≠ 0) : logb b (x / y) = logb b x - logb b y :=
-by simp_rw [logb, log_div hx hy, sub_div]
+section one_lt_b
 
-@[simp] lemma logb_inv (x : ℝ) : logb b (x⁻¹) = -logb b x := by simp [logb, neg_div]
+variable {one_lt_b : 1 < b}
 
-@[simp] lemma logb_le_logb (one_lt_b : 1 < b) (h : 0 < x) (h₁ : 0 < y) :
+@[simp] lemma logb_le_logb {one_lt_b} (h : 0 < x) (h₁ : 0 < y) :
   logb b x ≤ logb b y ↔ x ≤ y :=
-by {rw [logb, logb, div_le_div_right (log_pos one_lt_b), log_le_log h h₁], }
+by { rw [logb, logb, div_le_div_right (log_pos one_lt_b), log_le_log h h₁], }
 
 lemma logb_lt_logb (one_lt_b : 1 < b) (hx : 0 < x) (hxy : x < y) : logb b x < logb b y :=
-by {rw [logb, logb, div_lt_div_right (log_pos one_lt_b)], exact log_lt_log hx hxy, }
+by { rw [logb, logb, div_lt_div_right (log_pos one_lt_b)], exact log_lt_log hx hxy, }
 
 @[simp] lemma logb_lt_logb_iff (one_lt_b : 1 < b) (hx : 0 < x) (hy : 0 < y) :
   logb b x < logb b y ↔ x < y :=
-by {rw [logb, logb, div_lt_div_right (log_pos one_lt_b)], exact log_lt_log_iff hx hy, }
+by { rw [logb, logb, div_lt_div_right (log_pos one_lt_b)], exact log_lt_log_iff hx hy, }
 
 lemma logb_le_iff_le_rpow (one_lt_b : 1 < b) (hx : 0 < x) : logb b x ≤ y ↔ x ≤ b ^ y :=
 by { rw [←rpow_le_rpow_left_iff one_lt_b, rpow_logb], linarith, linarith, exact hx, }
@@ -181,6 +184,8 @@ logb_inj_on_pos one_lt_b (set.mem_Ioi.2 h₁) (set.mem_Ioi.2 zero_lt_one)
 lemma logb_ne_zero_of_pos_of_ne_one (one_lt_b : 1 < b) (hx_pos : 0 < x) (hx : x ≠ 1) :
   logb b x ≠ 0 :=
 mt (eq_one_of_pos_of_logb_eq_zero one_lt_b hx_pos) hx
+
+end one_lt_b
 
 @[simp] lemma logb_eq_zero :
   logb b x = 0 ↔ b = 0 ∨ b = 1 ∨ b = -1 ∨ x = 0 ∨ x = 1 ∨ x = -1 :=

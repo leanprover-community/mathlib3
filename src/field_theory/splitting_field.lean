afdf6518649bd8431bef956ca520dde596bba12a
Riccardo Brasca <riccardo.brasca@gmail.com>
2022-01-13 11:13:05 +0100

    add prod_roots_eq_coeff_zero_of_monic_of_split

--- src/field_theory/splitting_field.lean
+++ src/field_theory/splitting_field.lean
@@ -505,6 +505,19 @@ begin
   rw [eval_multiset_prod_X_sub_C_derivative hr]
 end
 
+/-- If `P` is a monic polynomial that splits, than `coeff P 0` equals the product of the roots. -/
+lemma prod_roots_eq_coeff_zero_of_monic_of_split {P : polynomial K} (hmo : P.monic)
+  (hP : P.splits (ring_hom.id K)) : coeff P 0 = (-1) ^ P.nat_degree * P.roots.prod :=
+begin
+  nth_rewrite 0 [eq_prod_roots_of_monic_of_splits_id hmo hP],
+  rw [coeff_zero_eq_eval_zero, eval_multiset_prod, multiset.map_map],
+  simp_rw [function.comp_app, eval_sub, eval_X, zero_sub, eval_C],
+  conv_lhs { congr, congr, funext,
+    rw [neg_eq_neg_one_mul] },
+  rw [multiset.prod_map_mul, multiset.map_const, multiset.prod_repeat, multiset.map_id',
+    splits_iff_card_roots.1 hP]
+end
+
 end splits
 
 end polynomial

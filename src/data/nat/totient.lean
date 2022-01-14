2e917771515bd72afb2ce696bfde13f66ed2d0e1
Johan Commelin <johan@commelin.net>
2022-01-14 06:34:46 +0000

    golf2

--- src/data/nat/totient.lean
+++ src/data/nat/totient.lean
@@ -82,10 +82,9 @@ end
 lemma totient_even {n : ℕ} (hn : 2 < n) : even n.totient :=
 begin
   haveI : fact (1 < n) := ⟨one_lt_two.trans hn⟩,
-  have : 2 = order_of (-1 : (zmod n)ˣ), by rw [←order_of_units, units.coe_neg_one, order_of_neg_one,
-                                       ← @ring_char.eq (zmod n) _ n (zmod.char_p n), if_neg hn.ne'],
-  rw [← zmod.card_units_eq_totient, even_iff_two_dvd, this],
-  exact order_of_dvd_card_univ
+  suffices : 2 = order_of (-1 : (zmod n)ˣ),
+  { rw [← zmod.card_units_eq_totient, even_iff_two_dvd, this], exact order_of_dvd_card_univ },
+  rw [←order_of_units, units.coe_neg_one, order_of_neg_one, ring_char.eq (zmod n) n, if_neg hn.ne'],
 end
 
 lemma totient_mul {m n : ℕ} (h : m.coprime n) : φ (m * n) = φ m * φ n :=

135ad5d42d4cdb20961f5046aa494fc16f69fb4c
Heather Macbeth <25316162+hrmacbeth@users.noreply.github.com>
2022-01-14 02:59:42 -0500

    Update src/analysis/inner_product_space/l2_space.lean
    Co-authored-by: Rémy Degenne <remydegenne@gmail.com>

--- src/analysis/inner_product_space/l2_space.lean
+++ src/analysis/inner_product_space/l2_space.lean
@@ -1,5 +1,5 @@
 /-
-Copyright (c) 2021 Heather Macbeth. All rights reserved.
+Copyright (c) 2022 Heather Macbeth. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Heather Macbeth
 -/

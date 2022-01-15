65b87521d1b665447f5acfaacd2be1c18b624dea
Joachim Breitner <mail@joachim-breitner.de>
2022-01-15 12:58:06 +0100

    Update src/data/set/basic.lean
    Co-authored-by: Bryan Gin-ge Chen <bryangingechen@gmail.com>

--- src/data/set/basic.lean
+++ src/data/set/basic.lean
@@ -716,8 +716,8 @@ theorem eq_of_mem_singleton {x y : α} (h : x ∈ ({y} : set α)) : x = y := h
 @[simp] theorem singleton_eq_singleton_iff {x y : α} : {x} = ({y} : set α) ↔ x = y :=
 ext_iff.trans eq_iff_eq_cancel_left
 
-lemma singleton_injective : injective (singleton : α → set α)
-  := λ _ _ h, singleton_eq_singleton_iff.mp h
+lemma singleton_injective : injective (singleton : α → set α) :=
+λ _ _ h, singleton_eq_singleton_iff.mp h
 
 theorem mem_singleton_of_eq {x y : α} (H : x = y) : x ∈ ({y} : set α) := H
 

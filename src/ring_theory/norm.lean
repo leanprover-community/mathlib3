44a16fd289ced23ac3013371094c23d9beac67ee
Riccardo Brasca <riccardo.brasca@gmail.com>
2022-01-16 14:00:01 +0100

    add is_integral_norm

--- src/ring_theory/norm.lean
+++ src/ring_theory/norm.lean
@@ -8,7 +8,7 @@ import field_theory.primitive_element
 import linear_algebra.determinant
 import linear_algebra.matrix.charpoly.coeff
 import linear_algebra.matrix.to_linear_equiv
-import ring_theory.power_basis
+import field_theory.is_alg_closed.algebraic_closure
 
 /-!
 # Norm for (finite) ring extensions
@@ -186,9 +186,43 @@ begin
   rw [← power_basis.finrank, adjoin_simple.algebra_map_gen K x]
 end
 
-section eq_prod_embeddings
-
 variable {K}
+
+section intermediate_field
+
+lemma _root_.intermediate_field.adjoin_simple.norm_gen_eq_one  {x : L}
+  (hx : ¬_root_.is_integral K x) : norm K (adjoin_simple.gen K x) = 1 :=
+begin
+  rw [norm_eq_one_of_not_exists_basis],
+  contrapose! hx,
+  obtain ⟨s, ⟨b⟩⟩ := hx,
+  refine is_integral_of_mem_of_fg (K⟮x⟯).to_subalgebra _ x _,
+  { exact (submodule.fg_iff_finite_dimensional _).mpr (of_finset_basis b) },
+  { exact intermediate_field.subset_adjoin K _ (set.mem_singleton x) }
+end
+
+lemma _root_.intermediate_field.adjoin_simple.norm_gen_eq_prod_roots [algebra K L] [algebra K F]
+  (x : L) (hf : (minpoly K x).splits (algebra_map K F)) :
+  (algebra_map K F) (norm K (intermediate_field.adjoin_simple.gen K x)) =
+  ((minpoly K x).map (algebra_map K F)).roots.prod :=
+begin
+  have injKKx : function.injective (algebra_map K K⟮x⟯) := ring_hom.injective _,
+  have injKxL : function.injective (algebra_map K⟮x⟯ L) := ring_hom.injective _,
+  by_cases hx : _root_.is_integral K x, swap,
+  { simp [minpoly.eq_zero hx, intermediate_field.adjoin_simple.norm_gen_eq_one hx] },
+  have hx' : _root_.is_integral K (adjoin_simple.gen K x),
+  { rwa [← is_integral_algebra_map_iff injKxL, adjoin_simple.algebra_map_gen],
+    apply_instance },
+  rw [← adjoin.power_basis_gen hx],
+  rw [power_basis.norm_gen_eq_prod_roots];
+  rw [adjoin.power_basis_gen hx, minpoly.eq_of_algebra_map_eq injKxL hx'];
+  try { simp only [adjoin_simple.algebra_map_gen _ _] },
+  exact hf
+end
+
+end intermediate_field
+
+section eq_prod_embeddings
 
 open intermediate_field intermediate_field.adjoin_simple polynomial
 
@@ -207,6 +241,29 @@ begin
   { exact nodup_roots ((separable_map _).mpr hfx) },
   { intro x, refl },
   { intro σ, rw [power_basis.lift_equiv'_apply_coe, id.def] }
+end
+
+lemma norm_eq_prod_roots [algebra K L] [algebra K F] [is_separable K L] [finite_dimensional K L]
+  {x : L} (hF : splits (algebra_map K F) (minpoly K x)) :
+  (algebra_map K F) (norm K x) =
+  ((minpoly K x).map (algebra_map K F)).roots.prod ^ finrank (K⟮x⟯) L :=
+by rw [norm_eq_norm_adjoin K x, ring_hom.map_pow,
+  intermediate_field.adjoin_simple.norm_gen_eq_prod_roots _ hF]
+
+lemma is_integral_norm [algebra R L] [algebra R K] [algebra K L] [is_scalar_tower R K L]
+  [is_separable K L] [finite_dimensional K L] {x : L} (hx : _root_.is_integral R x) :
+  _root_.is_integral R (norm K x) :=
+begin
+  have hx' : _root_.is_integral K x := is_integral_of_is_scalar_tower _ hx,
+  rw [← is_integral_algebra_map_iff (algebra_map K (algebraic_closure L)).injective,
+      norm_eq_prod_roots],
+  { refine (is_integral.multiset_prod (λ y hy, _)).pow _,
+    rw mem_roots_map (minpoly.ne_zero hx') at hy,
+    use [minpoly R x, minpoly.monic hx],
+    rw ← aeval_def at ⊢ hy,
+    exact minpoly.aeval_of_is_scalar_tower R x y hy },
+  { apply is_alg_closed.splits_codomain },
+  { apply_instance }
 end
 
 variable (F)

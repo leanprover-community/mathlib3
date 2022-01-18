5062ad32124d5cc476dcac20218cb6787a98da9f
Eric Wieser <wieser.eric@gmail.com>
2022-01-18 15:10:52 +0000

    exterior too

--- src/linear_algebra/exterior_algebra/generators.lean
+++ src/linear_algebra/exterior_algebra/generators.lean
@@ -5,6 +5,7 @@ Authors: Eric Wieser
 -/
 import linear_algebra.exterior_algebra.basic
 import linear_algebra.direct_sum.basic
+import ring_theory.graded_algebra.basic
 
 /-!
 # Results about the generators and grading structure of the exterior algebra
@@ -14,35 +15,49 @@ Many of these results are copied with minimal modification from the tensor algeb
 
 namespace exterior_algebra
 variables {R M : Type*} [comm_ring R] [add_comm_monoid M] [module R M]
+variables (R M)
 
-lemma supr_ι_range_eq_top : (⨆ i : ℕ, (ι R : M →ₗ[_] _).range ^ i) = ⊤ :=
+open_locale direct_sum
+
+/-- A version of `exterior_algebra.ι` that maps directly into the graded structure. This is
+primarily an auxiliary construction used to provide `exterior_algebra.graded_algebra`. -/
+def graded_algebra.ι : M →ₗ[R] ⨁ i : ℕ, ↥((ι R : M →ₗ[_] _).range ^ i) :=
+direct_sum.lof R ℕ (λ i, ↥((ι R : M →ₗ[_] _).range ^ i)) 1
+  ∘ₗ (ι R).cod_restrict _ (λ m, by simpa only [pow_one] using linear_map.mem_range_self _ m)
+
+lemma graded_algebra.ι_apply (m : M) :
+  graded_algebra.ι R M m =
+    direct_sum.of (λ i, ↥((ι R : M →ₗ[_] _).range ^ i)) 1
+      (⟨ι R m, by simpa only [pow_one] using linear_map.mem_range_self _ m⟩) := rfl
+
+lemma graded_algebra.ι_sq_zero (m : M) : graded_algebra.ι R M m * graded_algebra.ι R M m = 0 :=
 begin
-  rw [submodule.supr_pow_eq_top_iff, subsemiring.eq_top_iff'],
-  intro x,
-  rw ring_hom.mem_srange,
-  induction x using exterior_algebra.induction,
-  case h_grade0 : r {
-    refine ⟨direct_sum.of _ 0 _, _⟩,
-    refine ⟨algebra_map _ _ r, _⟩,
-    { simp only [pow_zero, submodule.mem_one, exists_apply_eq_apply], },
-    rw [direct_sum.to_semiring_of],
-    refl },
-  case h_grade1 : m {
-    refine ⟨direct_sum.of _ 1 _, _⟩,
-    refine ⟨ι R m, _⟩,
-    { simp only [pow_one, linear_map.mem_range, exists_apply_eq_apply], },
-    rw [direct_sum.to_semiring_of],
-    refl },
-  case h_add : x y hx hy {
-    obtain ⟨fx, rfl⟩ := hx,
-    obtain ⟨fy, rfl⟩ := hy,
-    rw ←ring_hom.map_add,
-    exact ⟨_, rfl⟩, },
-  case h_mul : x y hx hy {
-    obtain ⟨fx, rfl⟩ := hx,
-    obtain ⟨fy, rfl⟩ := hy,
-    rw ←ring_hom.map_mul,
-    exact ⟨_, rfl⟩, },
+  rw [graded_algebra.ι_apply, graded_algebra.ι_apply, of_mul_of]
 end
+
+variables {R M}
+
+/-- The exterior algebra is graded by the powers of the submodule `(exterior_algebra.ι R).range`. -/
+instance graded_algebra :
+  graded_algebra ((^) (ι R : M →ₗ[R] exterior_algebra R M).range : ℕ → submodule R _) :=
+graded_algebra.of_alg_hom _
+  (lift _ $ ⟨graded_algebra.ι R M, graded_algebra.ι_sq_zero R M⟩)
+  (begin
+    ext m,
+    dsimp only [linear_map.comp_apply, alg_hom.to_linear_map_apply, alg_hom.comp_apply,
+      alg_hom.id_apply],
+    rw [lift_ι_apply, graded_algebra.ι_apply, direct_sum.submodule_coe_alg_hom_of, subtype.coe_mk],
+  end)
+  (λ i x, begin
+    cases x with x hx,
+    dsimp only [subtype.coe_mk, direct_sum.lof_eq_of],
+    apply submodule.pow_induction_on' _
+      (λ r, _) (λ x y i hx hy ihx ihy, _) (λ m hm i x hx ih, _) hx,
+    { rw [alg_hom.commutes, direct_sum.algebra_map_apply], refl },
+    { rw [alg_hom.map_add, ihx, ihy, ←map_add], refl },
+    { obtain ⟨_, rfl⟩ := hm,
+      rw [alg_hom.map_mul, ih, lift_ι_apply, graded_algebra.ι_apply, direct_sum.of_mul_of],
+      exact direct_sum.of_eq_of_graded_monoid_eq (sigma.subtype_ext (add_comm _ _) rfl) }
+  end)
 
 end exterior_algebra
--- src/linear_algebra/tensor_algebra/generators.lean
+++ src/linear_algebra/tensor_algebra/generators.lean
@@ -9,104 +9,52 @@ import ring_theory.graded_algebra.basic
 
 /-!
 # Results about the generators and grading structure of the tensor algebra
+
+The main result is `tensor_algebra.graded_algebra`, which says that the tensor algebra is a
+ℕ-graded algebra.
 -/
 
 namespace tensor_algebra
-variables {R M : Type*} [comm_ring R] [add_comm_monoid M] [module R M]
+variables {R M : Type*} [comm_semiring R] [add_comm_monoid M] [module R M]
 
 open_locale direct_sum
 
-def to_direct_sum_ι_range : tensor_algebra R M →ₐ[R] (⨁ i : ℕ, ↥((ι R : M →ₗ[_] _).range ^ i)) :=
-lift _ $ (direct_sum.lof R ℕ (λ i, ↥((ι R : M →ₗ[_] _).range ^ i)) 1) ∘ₗ (ι R).cod_restrict _
-  (λ m, by simpa only [pow_one] using linear_map.mem_range_self _ m)
+variables (R M)
 
-@[simp] lemma to_direct_sum_ι_range_ι (m : M) :
-  to_direct_sum_ι_range (ι R m) =
+/-- A version of `tensor_algebra.ι` that maps directly into the graded structure. This is
+primarily an auxiliary construction used to provide `tensor_algebra.graded_algebra`. -/
+def graded_algebra.ι : M →ₗ[R] ⨁ i : ℕ, ↥((ι R : M →ₗ[_] _).range ^ i) :=
+direct_sum.lof R ℕ (λ i, ↥((ι R : M →ₗ[_] _).range ^ i)) 1
+  ∘ₗ (ι R).cod_restrict _ (λ m, by simpa only [pow_one] using linear_map.mem_range_self _ m)
+
+lemma graded_algebra.ι_apply (m : M) :
+  graded_algebra.ι R M m =
     direct_sum.of (λ i, ↥((ι R : M →ₗ[_] _).range ^ i)) 1
-      (⟨ι R m, by simpa only [pow_one] using linear_map.mem_range_self _ m⟩) :=
-lift_ι_apply _ _
+      (⟨ι R m, by simpa only [pow_one] using linear_map.mem_range_self _ m⟩) := rfl
 
-lemma to_direct_sum_ι_range_coe (i : ℕ) (x : ↥((ι R : M →ₗ[_] _).range ^ i)) :
-  to_direct_sum_ι_range (x : tensor_algebra R M) =
-    direct_sum.of (λ (i : ℕ), ↥((ι R).range ^ i)) i x :=
-begin
-  cases x with x hx,
-  dsimp only [subtype.coe_mk],
-  apply submodule.pow_induction_on' _ (λ r, _) (λ x y i hx hy ihx ihy, _) (λ m hm i x hx ih, _) hx,
-  { rw alg_hom.commutes, refl },
-  { rw [alg_hom.map_add, ihx, ihy, ←map_add], refl },
-  obtain ⟨_, rfl⟩ := hm,
-  rw [alg_hom.map_mul, ih, to_direct_sum_ι_range_ι, direct_sum.of_mul_of],
-  apply direct_sum.of_eq_of_graded_monoid_eq,
-  sorry
-end
+variables {R M}
 
-lemma submodule_coe_alg_hom_comp_to_direct_sum_ι_range :
-  (direct_sum.submodule_coe_alg_hom ((^) (ι R : M →ₗ[R] _).range : ℕ → submodule R _)).comp
-    to_direct_sum_ι_range = alg_hom.id R (tensor_algebra R M) :=
-begin
-  ext m,
-  dsimp,
-  rw [to_direct_sum_ι_range_ι, direct_sum.submodule_coe_alg_hom_of, subtype.coe_mk],
-end
-
-instance graded_algebra : graded_algebra ((^) (ι R : M →ₗ[R] _).range : ℕ → submodule R _) :=
-begin
-  refine graded_algebra.of_alg_hom _ (show tensor_algebra R M →ₐ[R] _, from to_direct_sum_ι_range) _ _,
-  swap,
-  dsimp,
-  -- convert submodule_coe_alg_hom_comp_to_direct_sum_ι_range using 1,
-  -- change _ = ι R m,
-  -- dsimp,
-  -- rw [to_direct_sum_ι_range_ι],
-  -- exact (to_direct_sum_ι_range : tensor_algebra R M →ₐ[R] (⨁ i : ℕ, ↥((ι R : M →ₗ[_] _).range ^ i)))
-  -- { sorry },
-  -- { sorry }
-end
--- graded_algebra.of_alg_hom _
---   (to_direct_sum_ι_range : tensor_algebra R M →ₐ[R] _)
---   (begin
---     rintros i ⟨x, hx⟩,
---     -- apply submodule.pow_induction' _ _ _ hx,
---     -- induction i,
---     -- { rw pow_zero at hx,
---     --   obtain ⟨r, rfl : algebra_map _ _ r = x⟩ := hx,
---     --   rw alg_hom.commutes,
---     --   refl, },
---     -- { rw pow_succ at hx,
---     --   refine submodule.mul_induction_on hx _ _ _ _,
---     --   rw submodule.mem_mul },
---   end)
---   _
-
-#exit
-
-lemma supr_ι_range_eq_top : (⨆ i : ℕ, (ι R : M →ₗ[_] _).range ^ i) = ⊤ :=
-begin
-  rw [submodule.supr_pow_eq_range, algebra.to_submodule_eq_top, algebra.eq_top_iff],
-  intro x,
-  simp_rw [alg_hom.mem_range, direct_sum.to_algebra_apply],
-  induction x using tensor_algebra.induction,
-  case h_grade0 : r {
-    refine ⟨direct_sum.of _ 0 _, _⟩,
-    refine ⟨algebra_map _ _ r, _⟩,
-    { simp only [pow_zero, submodule.algebra_map_mem], },
-    rw [direct_sum.to_semiring_of],
-    refl },
-  case h_grade1 : m {
-    refine ⟨direct_sum.of _ 1 _, _⟩,
-    refine ⟨ι R m, _⟩,
-    { simp only [pow_one, linear_map.mem_range_self], },
-    rw [direct_sum.to_semiring_of],
-    refl },
-  case h_add : x y hx hy {
-    obtain ⟨fx, rfl⟩ := hx,
-    obtain ⟨fy, rfl⟩ := hy,
-    exact ⟨_, ring_hom.map_add _ _ _⟩, },
-  case h_mul : x y hx hy {
-    obtain ⟨fx, rfl⟩ := hx,
-    obtain ⟨fy, rfl⟩ := hy,
-    exact ⟨_, ring_hom.map_mul _ _ _⟩, },
-end
+/-- The tensor algebra is graded by the powers of the submodule `(tensor_algebra.ι R).range`. -/
+instance graded_algebra :
+  graded_algebra ((^) (ι R : M →ₗ[R] tensor_algebra R M).range : ℕ → submodule R _) :=
+graded_algebra.of_alg_hom _
+  (lift _ $ graded_algebra.ι R M)
+  (begin
+    ext m,
+    dsimp only [linear_map.comp_apply, alg_hom.to_linear_map_apply, alg_hom.comp_apply,
+      alg_hom.id_apply],
+    rw [lift_ι_apply, graded_algebra.ι_apply, direct_sum.submodule_coe_alg_hom_of, subtype.coe_mk],
+  end)
+  (λ i x, begin
+    cases x with x hx,
+    dsimp only [subtype.coe_mk, direct_sum.lof_eq_of],
+    apply submodule.pow_induction_on' _
+      (λ r, _) (λ x y i hx hy ihx ihy, _) (λ m hm i x hx ih, _) hx,
+    { rw [alg_hom.commutes, direct_sum.algebra_map_apply], refl },
+    { rw [alg_hom.map_add, ihx, ihy, ←map_add], refl },
+    { obtain ⟨_, rfl⟩ := hm,
+      rw [alg_hom.map_mul, ih, lift_ι_apply, graded_algebra.ι_apply, direct_sum.of_mul_of],
+      exact direct_sum.of_eq_of_graded_monoid_eq (sigma.subtype_ext (add_comm _ _) rfl) }
+  end)
 
 end tensor_algebra

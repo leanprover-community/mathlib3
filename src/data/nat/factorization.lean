b267fc0469ddae62631da70a948e67fafd2d8aa8
Stuart Presnell <stuart.presnell@gmail.com>
2022-01-18 15:33:53 +0000

    Golf proof of `factorization_prod_pow_inv`

--- src/data/nat/factorization.lean
+++ src/data/nat/factorization.lean
@@ -124,10 +124,8 @@ the product `∏ (a : ℕ) in f.support, a ^ f a`. -/
 lemma factorization_prod_pow_inv {f : ℕ →₀ ℕ} (hf : ∀ (p : ℕ), p ∈ f.support → prime p) :
   (f.prod pow).factorization = f :=
 begin
-  unfold finsupp.prod,
-  have h1 : ∀ (x : ℕ), x ∈ f.support → x ^ f x ≠ 0,
-  { exact λ p hp, pow_ne_zero _ (prime.ne_zero (hf p hp)) },
-  rw factorization_prod h1,
+  have h : ∀ x : ℕ, x ∈ f.support → x ^ f x ≠ 0 := λ p hp, pow_ne_zero _ (prime.ne_zero (hf p hp)),
+  simp only [finsupp.prod, factorization_prod h],
   nth_rewrite_rhs 0 (sum_single f).symm,
   exact sum_congr rfl (λ p hp, prime.factorization_pow (hf p hp)),
 end

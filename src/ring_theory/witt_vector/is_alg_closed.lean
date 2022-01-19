522431954330f03b9b386e319ad547f4c37898ba
Heather Macbeth <25316162+hrmacbeth@users.noreply.github.com>
2022-01-19 00:19:56 -0500

    sketch the ideas

---
+++ src/ring_theory/witt_vector/is_alg_closed.lean
@@ -0,0 +1,66 @@
+import field_theory.is_alg_closed.basic
+import field_theory.perfect_closure
+import ring_theory.witt_vector.domain
+
+noncomputable theory
+
+variables (p : ℕ) [fact p.prime]
+variables (k : Type*) [field k] [char_p k p] [is_alg_closed k]
+
+-- an algebraically closed field is perfect, many google hits, maybe somewhere in mathlib?
+instance is_alg_closed.perfect_ring : perfect_ring k p := sorry
+
+local notation `𝕎` := witt_vector p
+local notation `K` := fraction_ring (𝕎 k)
+
+lemma witt_vector.frobenius_bijective (R : Type*) [comm_ring R] [char_p R p] [perfect_ring R p] :
+  function.bijective (@witt_vector.frobenius p R _ _) :=
+begin
+  rw witt_vector.frobenius_eq_map_frobenius,
+  exact ⟨witt_vector.map_injective _ (frobenius_equiv R p).injective,
+    witt_vector.map_surjective _ (frobenius_equiv R p).surjective⟩,
+end
+
+local notation `φ` := is_fraction_ring.field_equiv_of_ring_equiv
+  (ring_equiv.of_bijective _ (witt_vector.frobenius_bijective p k))
+
+lemma split (a : 𝕎 k) (ha : a ≠ 0) :
+  ∃ (m : ℕ) (b : 𝕎 k), b.coeff 0 ≠ 0 ∧ a = p ^ m * b :=
+begin
+  obtain ⟨m, c, hc, hcm⟩ := witt_vector.verschiebung_nonzero ha,
+  obtain ⟨b, rfl⟩ := (witt_vector.frobenius_bijective p k).surjective.iterate m c,
+  rw witt_vector.iterate_frobenius_coeff at hc,
+  -- have := ((frobenius_equiv k p).injective.iterate m).ne,
+  have := congr_fun (witt_vector.verschiebung_frobenius_comm.comp_iterate m) b,
+  simp at this,
+  rw ← this at hcm,
+  refine ⟨m, b, _, _⟩,
+  { sorry },
+  { sorry },
+end
+
+lemma important_aux {a₁ a₂ : 𝕎 k} (ha₁ : a₁.coeff 0 ≠ 0) (ha₂ : a₂.coeff 0 ≠ 0) :
+  ∃ (b : 𝕎 k) (hb : b ≠ 0) (m : ℕ), witt_vector.frobenius b * a₁ = b * a₂ :=
+sorry
+
+lemma important {a : fraction_ring (𝕎 k)} (ha : a ≠ 0) :
+  ∃ (b : fraction_ring (𝕎 k)) (hb : b ≠ 0) (m : ℤ), φ b * a = p ^ m * b :=
+begin
+  refine localization.induction_on a _,
+  rintros ⟨r, q, hq⟩,
+  rw mem_non_zero_divisors_iff_ne_zero at hq,
+  have : r ≠ 0 := sorry,
+  obtain ⟨m, r', hr', rfl⟩ := split p k r this,
+  obtain ⟨n, q', hq', rfl⟩ := split p k q hq,
+  obtain ⟨b, hb, l, hl⟩ := important_aux p k hr' hq',
+  refine ⟨algebra_map (𝕎 k) _ b, _, m - n, _⟩,
+  { sorry },
+  simp [is_fraction_ring.field_equiv_of_ring_equiv],
+  suffices :
+  witt_vector.frobenius b * p ^ m * r' * p ^ n = p ^ m * b * (p ^ n * q') ,
+  { -- apply `algebra_map` to both sides and divide
+    sorry },
+  have H := congr_arg (λ x : 𝕎 k, x * p ^ m * p ^ n) hl,
+  dsimp at H,
+  refine (eq.trans _ H).trans _; ring
+end

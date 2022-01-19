3ecaf4017c7f1698baaa5a35f2f27116df93a25e
Oliver Nash <github@olivernash.org>
2022-01-19 18:51:34 +0000

    Remove diamond cleverly spotted by Eric

--- src/algebra/lie/of_associative.lean
+++ src/algebra/lie/of_associative.lean
@@ -66,17 +66,30 @@ lemma of_associative_ring_bracket (x y : A) : ⁅x, y⁆ = x*y - y*x := rfl
 
 end lie_ring
 
-instance lie_ring_module.of_associative_module {M : Type w} [add_comm_group M] [module A M] :
-  lie_ring_module A M :=
+section associative_module
+
+variables {M : Type w} [add_comm_group M] [module A M]
+
+/-- We can regard a module over an associative ring `A` as a Lie ring module over `A` with Lie
+bracket equal to its ring commutator.
+
+Note that this cannot be a global instance because it would create a diamond when `M = A`,
+specifically we can build two mathematically-different `has_bracket A A`s:
+ 1. `@ring.has_bracket A _` which says `⁅a, b⁆ = a * b - b * a`
+ 2. `(@lie_ring_module.of_associative_module A _ A _ _).to_has_bracket` which says `⁅a, b⁆ = a • b`
+    (and thus `⁅a, b⁆ = a * b`) -/
+def lie_ring_module.of_associative_module : lie_ring_module A M :=
 { bracket     := (•),
   add_lie     := add_smul,
   lie_add     := smul_add,
   leibniz_lie :=
     by simp [lie_ring.of_associative_ring_bracket, sub_smul, mul_smul, sub_add_cancel], }
 
-lemma lie_ring_module.of_associative_module_apply {M : Type w} [add_comm_group M] [module A M]
-  (a : A) (m : M) : ⁅a, m⁆ = a • m :=
-rfl
+local attribute [instance] lie_ring_module.of_associative_module
+
+lemma lie_eq_smul (a : A) (m : M) : ⁅a, m⁆ = a • m := rfl
+
+end associative_module
 
 section lie_algebra
 
@@ -90,13 +103,28 @@ instance lie_algebra.of_associative_algebra : lie_algebra R A :=
     by rw [lie_ring.of_associative_ring_bracket, lie_ring.of_associative_ring_bracket,
            algebra.mul_smul_comm, algebra.smul_mul_assoc, smul_sub], }
 
+local attribute [instance] lie_ring_module.of_associative_module
+
+section associative_representation
+
+variables {M : Type w} [add_comm_group M] [module R M] [module A M] [is_scalar_tower R A M]
+
 /-- A representation of an associative algebra `A` is also a representation of `A`, regarded as a
-Lie algebra via the ring commutator. -/
-instance lie_module.of_associative_module
-  {M : Type w} [add_comm_group M] [module R M] [module A M] [is_scalar_tower R A M] :
-  lie_module R A M :=
+Lie algebra via the ring commutator.
+
+See the comment at `lie_ring_module.of_associative_module` for why the possibility `M = A` means
+this cannot be a global instance. -/
+def lie_module.of_associative_module : lie_module R A M :=
 { smul_lie := smul_assoc,
   lie_smul := smul_algebra_smul_comm }
+
+instance module.End.lie_ring_module : lie_ring_module (module.End R M) M :=
+lie_ring_module.of_associative_module
+
+instance module.End.lie_module : lie_module R (module.End R M) M :=
+lie_module.of_associative_module
+
+end associative_representation
 
 namespace alg_hom
 
@@ -157,7 +185,7 @@ def lie_algebra.ad : L →ₗ⁅R⁆ module.End R L := lie_module.to_endomorphis
 
 @[simp] lemma lie_module.to_endomorphism_module_End :
   lie_module.to_endomorphism R (module.End R M) M = lie_hom.id :=
-by { ext g m, simp [lie_ring_module.of_associative_module_apply], }
+by { ext g m, simp [lie_eq_smul], }
 
 open lie_algebra
 

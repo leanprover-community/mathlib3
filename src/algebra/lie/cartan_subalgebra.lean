453ddfaae5c8ac8fedbe6747f947bf4d23c8314c
Oliver Nash <github@olivernash.org>
2022-01-21 22:23:36 +0000

    feat(algebra/lie/cartan_subalgebra): add self-normalizing characterisation for Lie subalgebra

--- src/algebra/lie/cartan_subalgebra.lean
+++ src/algebra/lie/cartan_subalgebra.lean
@@ -42,6 +42,9 @@ def normalizer : lie_subalgebra R L :=
 
 lemma mem_normalizer_iff (x : L) : x ∈ H.normalizer ↔ ∀ (y : L), (y ∈ H) → ⁅x, y⁆ ∈ H := iff.rfl
 
+lemma mem_normalizer_iff' (x : L) : x ∈ H.normalizer ↔ ∀ (y : L), (y ∈ H) → ⁅y, x⁆ ∈ H :=
+forall_congr (λ y, forall_congr (λ hy, by rw [← lie_skew, H.neg_mem_iff]))
+
 lemma le_normalizer : H ≤ H.normalizer :=
 λ x hx, show ∀ (y : L), y ∈ H → ⁅x,y⁆ ∈ H, from λ y, H.lie_mem hx
 
@@ -54,6 +57,36 @@ ideal. -/
 lemma le_normalizer_of_ideal {N : lie_subalgebra R L}
   (h : ∀ (x y : L), x ∈ N → y ∈ H → ⁅x,y⁆ ∈ H) : N ≤ H.normalizer :=
 λ x hx y, h x y hx
+
+/-- See also `lie_subalgebra.lt_normalizer_iff_nontrivial`. -/
+lemma normalizer_eq_self_iff_subsingleton :
+  H.normalizer = H ↔ subsingleton (lie_module.max_triv_submodule R H $ L ⧸ H.to_lie_submodule) :=
+begin
+  rw subsingleton_iff_forall_eq (0 : lie_module.max_triv_submodule R H (L ⧸ H.to_lie_submodule)),
+  refine ⟨λ h, _, λ h, le_antisymm (λ x hx, _) H.le_normalizer⟩,
+  { rintros ⟨⟨x⟩, hx⟩,
+    suffices : x ∈ H, by simpa,
+    rw [← h, H.mem_normalizer_iff'],
+    intros y hy,
+    replace hx : ⁅_, lie_submodule.quotient.mk' _ x⁆ = 0 := hx ⟨y, hy⟩,
+    rwa [← lie_module_hom.map_lie, lie_submodule.quotient.mk_eq_zero] at hx, },
+  { let y := lie_submodule.quotient.mk' H.to_lie_submodule x,
+    have hy : y ∈ lie_module.max_triv_submodule R H (L ⧸ H.to_lie_submodule),
+    { rintros ⟨z, hz⟩,
+      rw [← lie_module_hom.map_lie, lie_submodule.quotient.mk_eq_zero, coe_bracket_of_module,
+        submodule.coe_mk, mem_to_lie_submodule],
+      exact (H.mem_normalizer_iff' x).mp hx z hz, },
+    simpa using h ⟨y, hy⟩, },
+end
+
+/-- An alternate form of `lie_subalgebra.normalizer_eq_self_iff_subsingleton` that is sometimes
+more convenient. -/
+lemma lt_normalizer_iff_nontrivial :
+  H < H.normalizer ↔ nontrivial (lie_module.max_triv_submodule R H $ L ⧸ H.to_lie_submodule) :=
+begin
+  rw [← not_iff_not, not_nontrivial_iff_subsingleton, ← normalizer_eq_self_iff_subsingleton],
+  exact ⟨λ h, (eq_of_le_of_not_lt H.le_normalizer h).symm, λ h, by simp [h]⟩,
+end
 
 /-- A Cartan subalgebra is a nilpotent, self-normalizing subalgebra. -/
 class is_cartan_subalgebra : Prop :=

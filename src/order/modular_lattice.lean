fa3ef14bf6ddf907cfa5d62f8c883ca47e77a959
YaelDillies <yael.dillies@gmail.com>
2022-01-22 14:23:42 +0100

    initial commit

--- src/order/modular_lattice.lean
+++ src/order/modular_lattice.lean
@@ -1,40 +1,151 @@
 /-
 Copyright (c) 2020 Aaron Anderson. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
-Authors: Aaron Anderson
+Authors: Aaron Anderson, Yaël Dillies
 -/
-import order.rel_iso
+import order.cover
 import order.lattice_intervals
-import order.galois_connection
 
 /-!
 # Modular Lattices
-This file defines Modular Lattices, a kind of lattice useful in algebra.
+
+This file defines (semi)modular lattices, a kind of lattice useful in algebra.
 For examples, look to the subobject lattices of abelian groups, submodules, and ideals, or consider
 any distributive lattice.
 
+## Typeclasses
+
+We define (semi)modularity typeclasses as Prop-valued mixins.
+
+* `is_weak_up_modular_lattice`: Weakly upper modular lattices. Lattice where `a ⊔ b` covers `a` and
+  `b` if `a` and `b` both cover `a ⊓ b`.
+* `is_weak_low_modular_lattice`: Weakly lower modular lattices. Lattice where `a` and `b` cover
+  `a ⊓ b` if `a ⊔ b` covers both `a` and `b`
+* `is_up_modular_lattice`: Upper modular lattices. Lattices where `a ⊔ b` covers `a` if `b` covers
+  `a ⊓ b`.
+* `is_low_modular_lattice`: Lower modular lattices. Lattices where `a` covers `a ⊓ b` if `a ⊔ b`
+  covers `b`.
+- `is_modular_lattice`: Modular lattices. Lattices where `a ≤ c → (a ⊔ b) ⊓ c = a ⊔ (b ⊓ c)`. We
+  only require an inequality because the other direction holds in all lattices.
+
 ## Main Definitions
-- `is_modular_lattice` defines a modular lattice to be one such that
-  `x ≤ z → (x ⊔ y) ⊓ z ≤ x ⊔ (y ⊓ z)`
+
 - `inf_Icc_order_iso_Icc_sup` gives an order isomorphism between the intervals
   `[a ⊓ b, a]` and `[b, a ⊔ b]`.
   This corresponds to the diamond (or second) isomorphism theorems of algebra.
 
 ## Main Results
+
 - `is_modular_lattice_iff_inf_sup_inf_assoc`:
   Modularity is equivalent to the `inf_sup_inf_assoc`: `(x ⊓ z) ⊔ (y ⊓ z) = ((x ⊓ z) ⊔ y) ⊓ z`
 - `distrib_lattice.is_modular_lattice`: Distributive lattices are modular.
 
 ## To do
+
 - Relate atoms and coatoms in modular lattices
+- Prove that a modular lattice is both upper and lower modular.
 
 -/
 
 variable {α : Type*}
 
+/-- A weakly upper modular lattice is a lattice where `a ⊔ b` covers `a` and `b` if `a` and `b` both
+cover `a ⊓ b`. -/
+class is_weak_up_modular_lattice (α : Type*) [lattice α] : Prop :=
+(covers_sup_of_inf_covers_covers {a b : α} : a ⊓ b ⋖ a → a ⊓ b ⋖ b → a ⋖ a ⊔ b)
+
+/-- A weakly lower modular lattice is a lattice where `a` and `b` cover `a ⊓ b` if `a ⊔ b` covers
+both `a` and `b`. -/
+class is_weak_low_modular_lattice (α : Type*) [lattice α] : Prop :=
+(inf_covers_of_covers_covers_sup {a b : α} : a ⋖ a ⊔ b → b ⋖ a ⊔ b → a ⊓ b ⋖ a)
+
+/-- An upper modular lattice, aka semimodular lattice, is a lattice where `a ⊔ b` covers `a` and `b`
+if either `a` or `b` covers `a ⊓ b`. -/
+class is_up_modular_lattice (α : Type*) [lattice α] : Prop :=
+(covers_sup_of_inf_covers {a b : α} : a ⊓ b ⋖ a → b ⋖ a ⊔ b)
+
+/-- A lower modular lattice is a lattice where `a` and `b` both cover `a ⊓ b` if `a ⊔ b` covers
+either `a` or `b`. -/
+class is_low_modular_lattice (α : Type*) [lattice α] : Prop :=
+(inf_covers_of_covers_sup {a b : α} : a ⋖ a ⊔ b → a ⊓ b ⋖ b)
+
 /-- A modular lattice is one with a limited associativity between `⊓` and `⊔`. -/
-class is_modular_lattice α [lattice α] : Prop :=
+class is_modular_lattice (α : Type*) [lattice α] : Prop :=
 (sup_inf_le_assoc_of_le : ∀ {x : α} (y : α) {z : α}, x ≤ z → (x ⊔ y) ⊓ z ≤ x ⊔ (y ⊓ z))
+
+section weak_up_modular
+variables [lattice α] [is_weak_up_modular_lattice α] {a b : α}
+
+lemma covers_sup_of_inf_covers_of_inf_covers_left : a ⊓ b ⋖ a → a ⊓ b ⋖ b → a ⋖ a ⊔ b :=
+is_weak_up_modular_lattice.covers_sup_of_inf_covers_covers
+
+lemma covers_sup_of_inf_covers_of_inf_covers_right : a ⊓ b ⋖ a → a ⊓ b ⋖ b → b ⋖ a ⊔ b :=
+by { rw [inf_comm, sup_comm], exact λ ha hb, covers_sup_of_inf_covers_of_inf_covers_left hb ha }
+
+alias covers_sup_of_inf_covers_of_inf_covers_left ← covers.sup_of_inf_of_inf_left
+alias covers_sup_of_inf_covers_of_inf_covers_right ← covers.sup_of_inf_of_inf_right
+
+instance : is_weak_low_modular_lattice (order_dual α) :=
+⟨λ a b ha hb, (ha.of_dual.sup_of_inf_of_inf_left hb.of_dual).to_dual⟩
+
+end weak_up_modular
+
+section weak_low_modular
+variables [lattice α] [is_weak_low_modular_lattice α] {a b : α}
+
+lemma inf_covers_of_covers_sup_of_covers_sup_left : a ⋖ a ⊔ b → b ⋖ a ⊔ b → a ⊓ b ⋖ a :=
+is_weak_low_modular_lattice.inf_covers_of_covers_covers_sup
+
+lemma inf_covers_of_covers_sup_of_covers_sup_right : a ⋖ a ⊔ b → b ⋖ a ⊔ b → a ⊓ b ⋖ b :=
+by { rw [sup_comm, inf_comm], exact λ ha hb, inf_covers_of_covers_sup_of_covers_sup_left hb ha }
+
+alias inf_covers_of_covers_sup_of_covers_sup_left ← covers.inf_of_sup_of_sup_left
+alias inf_covers_of_covers_sup_of_covers_sup_right ← covers.inf_of_sup_of_sup_right
+
+instance : is_weak_up_modular_lattice (order_dual α) :=
+⟨λ a b ha hb, (ha.of_dual.inf_of_sup_of_sup_left hb.of_dual).to_dual⟩
+
+end weak_low_modular
+
+section up_modular
+variables [lattice α] [is_up_modular_lattice α] {a b : α}
+
+lemma covers_sup_of_inf_covers_left : a ⊓ b ⋖ a → b ⋖ a ⊔ b :=
+is_up_modular_lattice.covers_sup_of_inf_covers
+
+lemma covers_sup_of_inf_covers_right : a ⊓ b ⋖ b → a ⋖ a ⊔ b :=
+by { rw [sup_comm, inf_comm], exact covers_sup_of_inf_covers_left }
+
+alias covers_sup_of_inf_covers_left ← covers.sup_of_inf_left
+alias covers_sup_of_inf_covers_right ← covers.sup_of_inf_right
+
+@[priority 100] -- See note [lower instance priority]
+instance is_up_modular_lattice.to_is_weak_up_modular_lattice : is_weak_up_modular_lattice α :=
+⟨λ a b _, covers.sup_of_inf_right⟩
+
+instance : is_low_modular_lattice (order_dual α) := ⟨λ a b h, h.of_dual.sup_of_inf_left.to_dual⟩
+
+end up_modular
+
+section low_modular
+variables [lattice α] [is_low_modular_lattice α] {a b : α}
+
+lemma inf_covers_of_covers_sup_left : a ⋖ a ⊔ b → a ⊓ b ⋖ b :=
+is_low_modular_lattice.inf_covers_of_covers_sup
+
+lemma inf_covers_of_covers_sup_right : b ⋖ a ⊔ b → a ⊓ b ⋖ a :=
+by { rw [inf_comm, sup_comm], exact inf_covers_of_covers_sup_left }
+
+alias inf_covers_of_covers_sup_left ← covers.inf_of_sup_left
+alias inf_covers_of_covers_sup_right ← covers.inf_of_sup_right
+
+@[priority 100] -- See note [lower instance priority]
+instance is_low_modular_lattice.to_is_weak_low_modular_lattice : is_weak_low_modular_lattice α :=
+⟨λ a b _, covers.inf_of_sup_right⟩
+
+instance : is_up_modular_lattice (order_dual α) := ⟨λ a b h, h.of_dual.inf_of_sup_left.to_dual⟩
+
+end low_modular
 
 section is_modular_lattice
 variables [lattice α] [is_modular_lattice α]

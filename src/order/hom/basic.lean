a3df32d4fa7b1bc51bc7916eb2ecf790c5301da1
Markus Himmel <markus@himmel-villmar.de>
2022-01-23 22:15:10 +0100

    feat(order/hom): taking complements is strictly antitone

--- src/order/hom/basic.lean
+++ src/order/hom/basic.lean
@@ -58,7 +58,7 @@ because the more bundled version usually does not work with dot notation.
 We also define two functions to convert other bundled maps to `α →o β`:
 
 * `order_embedding.to_order_hom`: convert `α ↪o β` to `α →o β`;
-* `rel_hom.to_order_hom`: conver a `rel_hom` between strict orders to a `order_hom`.
+* `rel_hom.to_order_hom`: convert a `rel_hom` between strict orders to a `order_hom`.
 
 ## Tags
 
@@ -683,3 +683,22 @@ theorem order_iso.is_complemented_iff :
 
 end bounded_order
 end lattice_isos
+
+section boolean_algebra
+variables (α) [boolean_algebra.core α]
+
+/-- Taking complements as an order isomorphism to the order dual. -/
+def order_iso.compl : α ≃o order_dual α :=
+{ to_fun := compl,
+  inv_fun := (compl : α → α),
+  left_inv := compl_compl,
+  right_inv := compl_compl,
+  map_rel_iff' := λ x y, compl_le_compl_iff_le }
+
+theorem compl_strict_anti : strict_anti (compl : α → α) :=
+(order_iso.compl α).strict_mono
+
+theorem compl_antitone : antitone (compl : α → α) :=
+(order_iso.compl α).monotone
+
+end boolean_algebra

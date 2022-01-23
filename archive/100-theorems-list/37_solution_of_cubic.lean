c19eb642b4223a24340056b73a8dc7d25abfc4d4
null_lambda <jeoffrey0522@naver.com>
2022-01-23 15:23:54 +0000

    reviewer suggestions
    split assumptions in thms & fix indentations

--- archive/100-theorems-list/37_solution_of_cubic.lean
+++ archive/100-theorems-list/37_solution_of_cubic.lean
@@ -39,7 +39,7 @@ lemma omega_cube_eq_one : ω ^ 3 = 1 :=
 begin
   unfold ω,
   have h3 : real.sqrt 3 * real.sqrt 3 = 3,
-   { rw [← pow_two (real.sqrt 3), @real.sq_sqrt 3 (by linarith)], },
+  { rw [← pow_two (real.sqrt 3), @real.sq_sqrt 3 (by linarith)], },
   rw [pow_succ, pow_succ, pow_one],
   ext,
   iterate 2 {
@@ -52,7 +52,7 @@ end
 lemma omega_sum : 1 + ω + ω^2 = 0 :=
 begin
   have : 1 - ω ≠ 0,
-    { unfold ω, intro h, rw complex.ext_iff at h, norm_num at h },
+  { unfold ω, intro h, rw complex.ext_iff at h, norm_num at h },
   rw [← mul_right_inj' this, mul_zero],
   calc (1 - ω) * (1 + ω + ω^2)
         = 1 - ω^3 : by ring
@@ -61,10 +61,10 @@ end
 
 /- The solution of the monic cubic equation, with 2nd coefficient set to zero. -/
 lemma cubic_basic_eq_zero_iff
-  (h_eq : r^2 = q^2 + p^3 ∧
-          s^3 = (if p = 0 then 2 * q else q + r)∧
-          t * s = p ∧
-          (p = 0 → t = 0))
+  (hr : r^2 = q^2 + p^3)
+  (hs3 : s^3 = (if p = 0 then 2 * q else q + r))
+  (ht : t * s = p)
+  (hpt: p = 0 → t = 0)
   (x : ℂ) :
    x^3 + 3 * p * x - 2 * q = 0 ↔
       x = s       - t        ∨
@@ -79,39 +79,35 @@ begin
   rw h₁,
   suffices : x ^ 3 + 3 * p * x - 2 * q
     = (x - (s - t)) * (x - (s * ω - t * ω^2)) * (x - (s * ω^2 - t * ω)),
-    { rw this, },
-  obtain ⟨hr, hs3, ht, hsp⟩ := h_eq,
+  { rw this, },
   have hc : s^3 - t^3 = 2 * q,
-    { by_cases hp : p = 0,
-
-      { simp [hp] at hs3, rw [hsp hp, hs3], norm_num },
-      { simp [hp] at hs3, have : s ≠ 0 := λ h, by { apply hp, rw [h, mul_zero] at ht, exact ht.symm },
-        have h_nonzero: q + r ≠ 0 := by { rw ← hs3, exact pow_ne_zero _ this },
-        have hp3 : p^3 = r^2 - q^2 := by rw [hr]; ring,
-        calc    s^3 - t^3
-              = s^3 - p^3/s^3 : by { rw [← ht], field_simp, ring }
-          ... = (q+r) - (r^2-q^2)/(q+r) : by rw [hs3, hp3]
-          ... = (q+r) + (q-r) * ((q+r) / (q+r)): by ring
-          ... = 2 * q : by rw div_self h_nonzero; ring } },
+  { by_cases hp : p = 0,
+    { simp [hp] at hs3, rw [hpt hp, hs3], norm_num },
+    { simp [hp] at hs3, have : s ≠ 0 := λ h, by { apply hp, rw [h, mul_zero] at ht, exact ht.symm },
+      have h_nonzero: q + r ≠ 0 := by { rw ← hs3, exact pow_ne_zero _ this },
+      have hp3 : p^3 = r^2 - q^2 := by rw [hr]; ring,
+      calc    s^3 - t^3
+            = s^3 - p^3/s^3 : by { rw [← ht], field_simp, ring }
+        ... = (q+r) - (r^2-q^2)/(q+r) : by rw [hs3, hp3]
+        ... = (q+r) + (q-r) * ((q+r) / (q+r)): by ring
+        ... = 2 * q : by rw div_self h_nonzero; ring } },
   symmetry,
   calc (x - (s - t)) * (x - (s * ω - t * ω^2)) * (x - (s * ω^2 - t * ω))
       = x^3 - (s-t) * (1+ω+ω^2) * x^2
         + ((s^2+t^2)*ω*(1+ω+ω^2) - s*t*(-3 + 3*(1+ω+ω^2) + ω*(ω^3-1))) * x
-        - (s^3-t^3)*ω^3 + s*t*(s-t)*ω^2*(1+ω+ω^2)
-        : by ring
-  ... = x^3 + 3*(t*s)*x - (s^3-t^3)
-        : by { rw [omega_sum, omega_cube_eq_one], ring }
+        - (s^3-t^3)*ω^3 + s*t*(s-t)*ω^2*(1+ω+ω^2) : by ring
+  ... = x^3 + 3*(t*s)*x - (s^3-t^3) : by { rw [omega_sum, omega_cube_eq_one], ring }
   ... = x^3 + 3*p*x - 2*q : by rw [ht, hc]
 end
 
 /-- The solution of the monic cubic equation. -/
 lemma cubic_monic_eq_zero_iff
-  (h_eq : p = (3 * c - b^2) / 9 ∧
-          q = (9 * b * c - 2 * b^3 - 27 * d) / 54 ∧
-          r^2 = q^2 + p^3 ∧
-          s^3 = (if p = 0 then 2 * q else q + r) ∧
-          t * s = p ∧
-          (p = 0 → t = 0))
+  (hp : p = (3 * c - b^2) / 9)
+  (hq :  q = (9 * b * c - 2 * b^3 - 27 * d) / 54)
+  (hr : r^2 = q^2 + p^3)
+  (hs3 : s^3 = (if p = 0 then 2 * q else q + r))
+  (ht : t * s = p)
+  (hpt: p = 0 → t = 0)
   (x : ℂ) :
       x^3 + b * x^2 + c * x + d = 0 ↔
         x = s       - t       - b / 3 ∨
@@ -121,28 +117,27 @@ begin
   let y := x + b / 3,
   have h₁ : x^3 + b * x^2 + c * x + d = y^3 + 3 * p * y - 2 * q,
   { dsimp [y],
-    rcases h_eq with ⟨hp, hq, -⟩,
     rw [hp, hq],
     field_simp, ring, },
-  have h₂ := @cubic_basic_eq_zero_iff p q r s t _ (by simp [h_eq] at h_eq ⊢; cc) y,
+  have h₂ := @cubic_basic_eq_zero_iff p q r s t _ hr hs3 ht hpt y,
   rw [h₁, h₂],
   dsimp [y],
   have h₃ : ∀ s t : ℂ, x + b / 3 = s - t ↔ x = s - t - b / 3,
-    { intros s t, split; intros h,
-      { calc x = x + b / 3 - b / 3 : by ring
-         ... = s - t - b / 3 : by rw h },
-      { rw h, ring } },
-  repeat { rw h₃ },
+  { intros s t, split; intros h,
+    { calc x = x + b / 3 - b / 3 : by ring
+       ... = s - t - b / 3 : by rw h },
+    { rw h, ring } },
+  repeat { rw [h₃] } ,
 end
 
 /-- **The Solution of Cubic** -/
 theorem cubic_eq_zero_iff (ha : a ≠ 0)
-  (h_eq : p = (3 * a * c - b^2) / (9 * a^2) ∧
-          q = (9 * a * b * c - 2 * b^3 - 27 * a^2 * d) / (54 * a^3) ∧
-          r^2 = q^2 + p^3 ∧
-          s^3 = (if p = 0 then 2 * q else q + r) ∧
-          t * s = p ∧
-          (p = 0 → t = 0))
+  (hp : p = (3 * a * c - b^2) / (9 * a^2))
+  (hq : q = (9 * a * b * c - 2 * b^3 - 27 * a^2 * d) / (54 * a^3))
+  (hr : r^2 = q^2 + p^3)
+  (hs3 : s^3 = (if p = 0 then 2 * q else q + r))
+  (ht : t * s = p)
+  (hpt : p = 0 → t = 0)
   (x : ℂ) :
       a * x^3 + b * x^2 + c * x + d = 0 ↔
         x = s       - t       - b / (3 * a) ∨
@@ -150,25 +145,17 @@ theorem cubic_eq_zero_iff (ha : a ≠ 0)
         x = s * ω^2 - t * ω   - b / (3 * a) :=
 begin
   have h₁ : a * x^3 + b * x^2 + c * x + d = a * (x^3 + b/a * x^2 + c/a * x + d/a),
-    { field_simp, ring },
+  { field_simp, ring },
   have h₂ : ∀ x, a * x = 0 ↔ x = 0, { intros x, simp [ha], },
-  have h₃ := @cubic_monic_eq_zero_iff (b/a) (c/a) (d/a) p q r s t _ (by {
-    have : decidable (p = 0) := by assumption,
-    have hsp : p ≠ 0 → s ≠ 0, {
-      have : p = t * s := by simp [h_eq],
-      apply mt,
-      intro h,
-      rwa[h, mul_zero] at this },
-    simp [h_eq, ha] at h_eq hsp ⊢,
-    split,
-    repeat { field_simp, ring_nf, },
-    assumption'
-  }) x,
+  have hp' : p = (3 * (c/a) - (b/a) ^ 2) / 9, { field_simp [hp], ring_nf },
+  have hq' : q = (9 * (b/a) * (c/a) - 2 * (b/a) ^ 3 - 27 * (d/a)) / 54,
+  { field_simp [hq], ring_nf },
+  have h₃ := @cubic_monic_eq_zero_iff (b/a) (c/a) (d/a) p q r s t _ hp' hq' hr hs3 ht hpt x,
   rw [h₁, h₂, h₃],
   have h₄ :=
     calc b / a / 3 = b / (a * 3) : by { field_simp [ha] }
                ... = b / (3 * a) : by rw mul_comm,
-  simp [h_eq, h₄],
+  simp [h₄],
 end
 
 end

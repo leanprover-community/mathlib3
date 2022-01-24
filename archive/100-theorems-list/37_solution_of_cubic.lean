72bb3e68a3ce5ab0d28bb026e97f60d3fc5d7327
null_lambda <jeoffrey0522@naver.com>
2022-01-24 22:51:19 +0900

    docs - add author
    Co-authored-by: Alex J Best <alex.j.best@gmail.com>

--- archive/100-theorems-list/37_solution_of_cubic.lean
+++ archive/100-theorems-list/37_solution_of_cubic.lean
@@ -1,7 +1,7 @@
 /-
 Copyright (c) 2021 Joeff Lee. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
-Authors: -
+Authors: Jeoff Lee
 -/
 import tactic.basic
 import data.complex.basic

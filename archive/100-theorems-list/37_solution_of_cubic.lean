5bc5d981d1a7a45f4db20cd5d052398778b9c4eb
null_lambda <jeoffrey0522@naver.com>
2022-01-24 07:07:58 +0000

    fix import

--- archive/100-theorems-list/37_solution_of_cubic.lean
+++ archive/100-theorems-list/37_solution_of_cubic.lean
@@ -3,7 +3,7 @@ Copyright (c) 2021 Joeff Lee. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: -
 -/
-import tactic
+import tactic.basic
 import data.complex.basic
 import algebra.group.defs
 

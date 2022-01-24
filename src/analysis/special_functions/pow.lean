8378d1f1ed1f464e0717f1050f7d91c2ed723da6
Ruben Van de Velde <65514131+Ruben-VandeVelde@users.noreply.github.com>
2022-01-24 11:37:53 +0100

    Update src/analysis/special_functions/pow.lean
    Co-authored-by: Johan Commelin <johan@commelin.net>

--- src/analysis/special_functions/pow.lean
+++ src/analysis/special_functions/pow.lean
@@ -963,19 +963,17 @@ lemma rpow_le_rpow_of_exponent_ge {x : ℝ≥0} {y z : ℝ} (hx0 : 0 < x) (hx1 :
   x^y ≤ x^z :=
 real.rpow_le_rpow_of_exponent_ge hx0 hx1 hyz
 
-lemma rpow_pos_of_nonneg {p : ℝ} {x : ℝ≥0} (hx_pos : 0 < x) (hp_nonneg : 0 ≤ p) : 0 < x^p :=
-begin
-  obtain rfl|hp_pos := hp_nonneg.eq_or_lt,
-  { simp only [zero_lt_one, rpow_zero], },
-  { rw ←zero_rpow hp_pos.ne', exact rpow_lt_rpow hx_pos hp_pos, },
-end
-
 lemma rpow_pos {p : ℝ} {x : ℝ≥0} (hx_pos : 0 < x) : 0 < x^p :=
 begin
+  have rpow_pos_of_nonneg : ∀ {p : ℝ}, 0 ≤ p → 0 < x^p,
+  { intros p hp_nonneg,
+    obtain rfl|hp_pos := hp_nonneg.eq_or_lt,
+    { simp only [zero_lt_one, rpow_zero], },
+    { rw ←zero_rpow hp_pos.ne', exact rpow_lt_rpow hx_pos hp_pos, } },
   cases lt_or_le 0 p with hp_pos hp_nonpos,
-  { exact rpow_pos_of_nonneg hx_pos hp_pos.le },
+  { exact rpow_pos_of_nonneg hp_pos.le },
   { rw [←neg_neg p, rpow_neg, inv_pos],
-    exact rpow_pos_of_nonneg hx_pos (neg_nonneg.mpr hp_nonpos) },
+    exact rpow_pos_of_nonneg (neg_nonneg.mpr hp_nonpos) },
 end
 
 lemma rpow_lt_one {x : ℝ≥0} {z : ℝ} (hx : 0 ≤ x) (hx1 : x < 1) (hz : 0 < z) : x^z < 1 :=

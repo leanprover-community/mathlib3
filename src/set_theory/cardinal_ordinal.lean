009cec03ecf8014a144e0aa88926d8c7de383933
Violeta Hernández <vi.hdz.p@gmail.com>
2022-01-25 19:51:28 +0000

    feat(set_theory/cardinal_ordinal): Aleph is positive (#11657)

--- src/set_theory/cardinal_ordinal.lean
+++ src/set_theory/cardinal_ordinal.lean
@@ -207,6 +207,12 @@ by rw [← aleph'_omega, aleph'_le]
 theorem omega_le_aleph (o : ordinal) : ω ≤ aleph o :=
 by rw [aleph, omega_le_aleph']; apply ordinal.le_add_right
 
+theorem aleph'_pos {o : ordinal} (ho : 0 < o) : 0 < aleph' o :=
+by rwa [←aleph'_zero, aleph'_lt]
+
+theorem aleph_pos (o : ordinal) : 0 < aleph o :=
+omega_pos.trans_le (omega_le_aleph o)
+
 theorem ord_aleph_is_limit (o : ordinal) : is_limit (aleph o).ord :=
 ord_is_limit $ omega_le_aleph _
 

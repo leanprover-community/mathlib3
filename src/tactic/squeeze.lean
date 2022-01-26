800ec66442e17c57fa46543c1788d1622bb98d70
Arthur Paulino <arthurleonardo.ap@gmail.com>
2022-01-26 20:48:02 -0300

    using 'to_string' instead of 'pp'

--- src/tactic/squeeze.lean
+++ src/tactic/squeeze.lean
@@ -77,10 +77,13 @@ def squeeze_loc_attr_carrier := ()
 run_cmd squeeze_loc_attr.set ``squeeze_loc_attr_carrier none tt
 
 /-- Format a list of arguments for use with `simp` and friends. This omits the
-list entirely if it is empty. -/
+list entirely if it is empty.
+
+Patch: `pp` was changed to `to_string` because it was getting rid of prefixes
+that would be necessary for some disambiguations. -/
 meta def render_simp_arg_list : list simp_arg_type → tactic format
 | [] := pure ""
-| args := (++) " " <$> to_line_wrap_format <$> args.mmap pp
+| args := (++) " " <$> to_line_wrap_format <$> pure (args.map to_string)
 
 /-- Emit a suggestion to the user. If inside a `squeeze_scope` block,
 the suggestions emitted through `mk_suggestion` will be aggregated so that

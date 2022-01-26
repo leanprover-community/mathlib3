5d7cfa9033e79ece5ccec2f3482648a93d735753
Eric Rodriguez <ericrboidi@gmail.com>
2022-01-26 02:48:39 +0000

    the main change that I forgot to commit

--- src/tactic/squeeze.lean
+++ src/tactic/squeeze.lean
@@ -197,7 +197,7 @@ do v ← target >>= mk_meta_var,
      instantiate_mvars g },
    let vs := g.list_constant,
    vs ← vs.mfilter is_simp_lemma,
-   vs ← vs.mmap strip_prefix,
+   vs ← vs.mmap prepend_root_if_needed,
    vs ← vs.to_list.mmap name.to_simp_args,
    with_local_goals' [v] (filter_simp_set tac args vs)
      >>= mk_suggestion,

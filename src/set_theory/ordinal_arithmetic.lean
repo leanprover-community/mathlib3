b3b60366cca86fe6facc05e9cf849769fe24628d
Violeta Hernández <vi.hdz.p@gmail.com>
2022-01-27 16:00:32 -0600

    Move to new file

--- src/set_theory/ordinal_arithmetic.lean
+++ src/set_theory/ordinal_arithmetic.lean
@@ -1805,45 +1805,6 @@ by simp only [type_fin, lift_nat_cast]
 theorem type_fintype (r : α → α → Prop) [is_well_order α r] [fintype α] : type r = fintype.card α :=
 by rw [← card_eq_nat, card_type, mk_fintype]
 
-/-! ### Principal ordinals -/
-
-/-- An ordinal `o` is said to be principal or indecomposable under an operation when the set of
-ordinals less than it is closed under that operation. In standard mathematical usage, this term is
-almost exclusively used for additive and multiplicative principal ordinals.
-
-For simplicity, we break usual convention and regard 0 as principal. -/
-def principal (op : ordinal → ordinal → ordinal) (o : ordinal) : Prop :=
-∀ a b, a < o → b < o → op a b < o
-
-theorem principal_zero {op : ordinal → ordinal → ordinal} : principal op 0 :=
-λ a _ h, (ordinal.not_lt_zero a h).elim
-
-theorem principal_one_iff {op : ordinal → ordinal → ordinal} :
-  principal op 1 ↔ op 0 0 = 0 :=
-begin
-  refine ⟨λ h, _, λ h a b ha hb, _⟩,
-  { rwa ←lt_one_iff_zero,
-    exact h 0 0 zero_lt_one zero_lt_one },
-  { rwa [lt_one_iff_zero, ha, hb] at * }
-end
-
-theorem iterate_lt_of_principal {op : ordinal → ordinal → ordinal}
-  {a o : ordinal} (hao : a < o) (ho : principal op o) (n : ℕ) : (op a)^[n] a < o :=
-begin
-  induction n with n hn,
-  { rwa function.iterate_zero },
-  { have := ho a _ hao hn,
-    rwa function.iterate_succ' }
-end
-
-theorem op_eq_self_of_principal {op : ordinal → ordinal → ordinal} {a o : ordinal.{u}}
-  (hao : a < o) (H : is_normal (op a)) (ho : principal op o) (ho' : is_limit o) : op a o = o :=
-begin
-  refine le_antisymm _ (H.le_self _),
-  rw [←is_normal.bsup_eq.{u u} H ho', bsup_le],
-  exact λ b hbo, le_of_lt (ho a b hao hbo)
-end
-
 end ordinal
 
 /-! ### Properties of `omega` -/

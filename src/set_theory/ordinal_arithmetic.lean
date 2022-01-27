da7c6ef1a025acd8fa5af654be7039d5fd6202c4
Violeta Hernández <vi.hdz.p@gmail.com>
2022-01-26 21:15:04 -0600

    Moved much earlier in the file

--- src/set_theory/ordinal_arithmetic.lean
+++ src/set_theory/ordinal_arithmetic.lean
@@ -1805,6 +1805,45 @@ by simp only [type_fin, lift_nat_cast]
 theorem type_fintype (r : α → α → Prop) [is_well_order α r] [fintype α] : type r = fintype.card α :=
 by rw [← card_eq_nat, card_type, mk_fintype]
 
+/-! ### Principal ordinals -/
+
+/-- An ordinal `o` is said to be principal or indecomposable under an operation when the set of
+ordinals less than it is closed under that operation. In standard mathematical usage, this term is
+almost exclusively used for additive and multiplicative principal ordinals.
+
+For simplicity, we break usual convention and regard 0 as principal. -/
+def principal (op : ordinal → ordinal → ordinal) (o : ordinal) : Prop :=
+∀ a b, a < o → b < o → op a b < o
+
+theorem zero_principal {op : ordinal → ordinal → ordinal} : principal op 0 :=
+λ a _ h, (ordinal.not_lt_zero a h).elim
+
+theorem one_principal_iff {op : ordinal → ordinal → ordinal} :
+  principal op 1 ↔ op 0 0 = 0 :=
+begin
+  refine ⟨λ h, _, λ h a b ha hb, _⟩,
+  { rwa ←lt_one_iff_zero,
+    exact h 0 0 zero_lt_one zero_lt_one },
+  { rwa [lt_one_iff_zero, ha, hb] at * }
+end
+
+theorem iterate_lt_of_principal {op : ordinal → ordinal → ordinal}
+  {a o : ordinal} (hao : a < o) (ho : principal op o) (n : ℕ) : (op a)^[n] a < o :=
+begin
+  induction n with n hn,
+  { rwa function.iterate_zero },
+  { have := ho a _ hao hn,
+    rwa function.iterate_succ' }
+end
+
+theorem op_eq_self_of_principal {op : ordinal → ordinal → ordinal} {a o : ordinal.{u}}
+  (hao : a < o) (H : is_normal (op a)) (ho : principal op o) (ho' : is_limit o) : op a o = o :=
+begin
+  refine le_antisymm _ (H.le_self _),
+  rw [←is_normal.bsup_eq.{u u} H ho', bsup_le],
+  exact λ b hbo, le_of_lt (ho a b hao hbo)
+end
+
 end ordinal
 
 /-! ### Properties of `omega` -/
@@ -2081,6 +2120,10 @@ theorem is_normal.le_nfp {f} (H : is_normal f) {a b} :
 theorem nfp_eq_self {f : ordinal → ordinal} {a} (h : f a = a) : nfp f a = a :=
 le_antisymm (sup_le.mpr $ λ i, by rw [iterate_fixed h]) (le_nfp_self f a)
 
+theorem nfp_le_of_principal {op : ordinal → ordinal → ordinal}
+  {a o : ordinal} (hao : a < o) (ho : principal op o) : nfp (op a) a ≤ o :=
+nfp_le.2 $ λ n, le_of_lt (iterate_lt_of_principal hao ho n)
+
 /-- The derivative of a normal function `f` is
   the sequence of fixed points of `f`. -/
 def deriv (f : ordinal → ordinal) (o : ordinal) : ordinal :=
@@ -2132,48 +2175,5 @@ theorem is_normal.fp_iff_deriv {f} (H : is_normal f)
     rw [deriv_limit _ l, ← not_le, bsup_le, not_ball] at h,
     exact let ⟨o', h, hl⟩ := h in IH o' h (le_of_not_le hl) }
 end, λ ⟨o, e⟩, e.symm ▸ le_of_eq (H.deriv_fp _)⟩
-
-/-! ### Principal ordinals -/
-
-/-- An ordinal `o` is said to be principal or indecomposable under an operation when the set of
-ordinals less than it is closed under that operation. In standard mathematical usage, this term is
-almost exclusively used for additive and multiplicative principal ordinals.
-
-For simplicity, we break usual convention and regard 0 as principal. -/
-def principal (op : ordinal → ordinal → ordinal) (o : ordinal) : Prop :=
-∀ a b, a < o → b < o → op a b < o
-
-theorem zero_principal {op : ordinal → ordinal → ordinal} : principal op 0 :=
-λ a _ h, (ordinal.not_lt_zero a h).elim
-
-theorem one_principal_iff {op : ordinal → ordinal → ordinal} :
-  principal op 1 ↔ op 0 0 = 0 :=
-begin
-  refine ⟨λ h, _, λ h a b ha hb, _⟩,
-  { rwa ←lt_one_iff_zero,
-    exact h 0 0 zero_lt_one zero_lt_one },
-  { rwa [lt_one_iff_zero, ha, hb] at * }
-end
-
-theorem iterate_lt_of_principal {op : ordinal → ordinal → ordinal}
-  {a o : ordinal} (hao : a < o) (ho : principal op o) (n : ℕ) : (op a)^[n] a < o :=
-begin
-  induction n with n hn,
-  { rwa function.iterate_zero },
-  { have := ho a _ hao hn,
-    rwa function.iterate_succ' }
-end
-
-theorem op_eq_self_of_principal {op : ordinal → ordinal → ordinal} {a o : ordinal.{u}}
-  (hao : a < o) (H : is_normal (op a)) (ho : principal op o) (ho' : is_limit o) : op a o = o :=
-begin
-  refine le_antisymm _ (H.le_self _),
-  rw [←is_normal.bsup_eq.{u u} H ho', bsup_le],
-  exact λ b hbo, le_of_lt (ho a b hao hbo)
-end
-
-theorem nfp_le_of_principal {op : ordinal → ordinal → ordinal}
-  {a o : ordinal} (hao : a < o) (ho : principal op o) : nfp (op a) a ≤ o :=
-nfp_le.2 $ λ n, le_of_lt (iterate_lt_of_principal hao ho n)
 
 end ordinal

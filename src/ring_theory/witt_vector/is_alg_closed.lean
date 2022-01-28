a54fd9e71305eb78f9b41672e43d0d6a153a108b
Rob Lewis <rob.y.lewis@gmail.com>
2022-01-27 20:51:51 -0500

    got it

--- src/ring_theory/witt_vector/is_alg_closed.lean
+++ src/ring_theory/witt_vector/is_alg_closed.lean
@@ -76,38 +76,34 @@ open_locale big_operators
 
 lemma nth_mul_coeff_aux1 (n : ℕ) (x y : 𝕎 k) :
   ∑ i in range (n+1), ((x * y).coeff i)^(p^(n-i)) * p^i =
-  (∑ i in range (n+1), (x.coeff i)^(p^n-i) * p^i)*(∑ i in range (n+1), (y.coeff i)^(p^n-i) * p^i) :=
+  (∑ i in range (n+1), (x.coeff i)^(p^(n-i)) * p^i)*(∑ i in range (n+1), (y.coeff i)^(p^(n-i)) * p^i) :=
 begin
   have := witt_structure_prop p ((mv_polynomial.X 0 * mv_polynomial.X 1) : mv_polynomial (fin 2) ℤ) n,
   replace this := congr_arg (λ z, witt_vector.peval z ![λ i, x.coeff i, λ i, y.coeff i]) this,
-  have mvp : (p : mv_polynomial ℕ ℤ) = mv_polynomial.C ↑p := by rw [ring_hom.eq_int_cast, int.cast_coe_nat ],
-
-  simp only [int.cast_coe_nat,
- ring_hom.eq_int_cast,
- mv_polynomial.eval₂_mul,
- witt_vector.peval,
- ring_hom.to_fun_eq_coe,
- mv_polynomial.coe_eval₂_hom,
- mv_polynomial.C_pow,
- mv_polynomial.aeval,
- mv_polynomial.eval₂_map,
- witt_polynomial_eq_sum_C_mul_X_pow,
- int.nat_cast_eq_coe_nat,
- alg_hom.coe_mk,
- mv_polynomial.eval₂_sum,
- mv_polynomial.eval₂_X,
- finset.sum_congr,
- mv_polynomial.eval₂_pow] at this,
-rw [mvp, mv_polynomial.eval₂_C, ring_hom.eq_int_cast, int.cast_coe_nat] at this,
-have mvp : (p : mv_polynomial (fin 2 × ℕ) ℤ) = mv_polynomial.C ↑p := by rw [ring_hom.eq_int_cast, int.cast_coe_nat ],
-rw [mvp, mv_polynomial.eval₂_C, ring_hom.eq_int_cast, int.cast_coe_nat] at this,
-simp [witt_vector.mul_coeff],
-convert this using 2,
-ext z,
-rw mul_comm,
-congr' 1,
-simp [peval, mv_polynomial.aeval],
-sorry
+  have mvpz : (p : mv_polynomial ℕ ℤ) = mv_polynomial.C ↑p := by rw [ring_hom.eq_int_cast, int.cast_coe_nat ],
+  simp only [int.cast_coe_nat, ring_hom.eq_int_cast, mv_polynomial.eval₂_mul, witt_vector.peval,
+    ring_hom.to_fun_eq_coe, mv_polynomial.coe_eval₂_hom, mv_polynomial.C_pow, mv_polynomial.aeval,
+    mv_polynomial.eval₂_map, witt_polynomial_eq_sum_C_mul_X_pow, int.nat_cast_eq_coe_nat,
+    alg_hom.coe_mk, mv_polynomial.eval₂_sum, mv_polynomial.eval₂_X, finset.sum_congr,
+    mv_polynomial.eval₂_pow] at this,
+  rw [mvpz, mv_polynomial.eval₂_C, ring_hom.eq_int_cast, int.cast_coe_nat] at this,
+  have mvp : (p : mv_polynomial (fin 2 × ℕ) ℤ) = mv_polynomial.C ↑p := by rw [ring_hom.eq_int_cast, int.cast_coe_nat ],
+  rw [mvp, mv_polynomial.eval₂_C, ring_hom.eq_int_cast, int.cast_coe_nat] at this,
+  simp only [mul_coeff],
+  convert this using 2;
+  clear this,
+  { ext z,
+    rw mul_comm,
+    congr' 1,
+    simp only [peval, mv_polynomial.aeval, ring_hom.to_fun_eq_coe, mv_polynomial.coe_eval₂_hom, alg_hom.coe_mk],
+    congr' 2, },
+  all_goals
+  { simp only [mv_polynomial.eval₂_rename, int.cast_coe_nat, ring_hom.eq_int_cast, mv_polynomial.eval₂_mul,
+    function.uncurry_apply_pair, function.comp_app, mv_polynomial.eval₂_sum, mv_polynomial.eval₂_X,
+    matrix.cons_val_zero, mv_polynomial.eval₂_pow],
+    congr' 1 with z,
+    rw [mvpz, mv_polynomial.eval₂_C, mul_comm],
+    refl },
 end
 
 -- this is the version we think is true in char p

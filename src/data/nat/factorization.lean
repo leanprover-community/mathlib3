7f83adca4ede8b9a8a1ee0e7fc1e25f498cfb8ae
Stuart Presnell <stuart.presnell@gmail.com>
2022-01-29 16:55:25 +0000

    Simpler proof that mysteriously fails

--- src/data/nat/factorization.lean
+++ src/data/nat/factorization.lean
@@ -202,10 +202,8 @@ end
 lemma prod_prime_factors_dvd (n : ℕ) : (∏ (p : ℕ) in n.factors.to_finset, p) ∣ n :=
 begin
   rcases (decidable.eq_or_ne n 0) with rfl | hn0, { simp },
-  nth_rewrite_rhs 0 ←factorization_prod_pow_eq_self hn0,
-  rw [finsupp.prod, support_factorization],
-  refine finset.prod_dvd_prod _ _ (λ p hp, (dvd_pow (dvd_refl p) _)),
-  rwa [←finsupp.mem_support_iff, support_factorization],
+  convert multiset.to_finset_prod_dvd_prod,
+  simp [prod_factors hn0.bot_lt],
 end
 
 lemma prime_pow_dvd_iff_le_factorization (p k n : ℕ) (pp : prime p) (hn : n ≠ 0) :

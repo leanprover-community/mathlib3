ecda6a017d485b8e3c5e16703c1bcdeec132e69d
Anatole Dedecker <anatolededecker@gmail.com>
2022-01-30 17:30:56 +0100

    ++

--- src/topology/algebra/module/locally_convex.lean
+++ src/topology/algebra/module/locally_convex.lean
@@ -1,3 +1,8 @@
+/-
+Copyright (c) 2022 Anatole Dedecker. All rights reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
+Authors: Anatole Dedecker
+-/
 import analysis.convex.topology
 import analysis.normed_space.basic
 /-! TODO -/

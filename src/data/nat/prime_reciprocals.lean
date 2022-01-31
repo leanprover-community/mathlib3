18ad9b9777eb6d90a0c9ef1c4db99d02143f1e9a
Stuart Presnell <stuart.presnell@gmail.com>
2022-01-31 20:17:34 +0000

    Fix Authors line

--- src/data/nat/prime_reciprocals.lean
+++ src/data/nat/prime_reciprocals.lean
@@ -1,7 +1,7 @@
 /-
 Copyright (c) 2022 Stuart Presnell. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
-Author: Stuart Presnell
+Authors: Stuart Presnell
 -/
 import data.nat.prime
 import data.set.intervals.basic

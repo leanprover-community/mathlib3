f46e84b8b844917f23c8b4293c7a6465d09ca6a5
YaelDillies <yael.dillies@gmail.com>
2022-01-31 15:14:35 +0100

    revert

--- src/data/fintype/order.lean
+++ src/data/fintype/order.lean
@@ -138,7 +138,6 @@ end fintype
 
 /-! ### Concrete instances -/
 
-@[priority 900] -- to avoid noncomputability poisoning
 noncomputable instance {n : ℕ} : complete_linear_order (fin (n + 1)) :=
 fintype.to_complete_linear_order _
 

c823ee57421bdaed629df1bb9725d3de754a5e57
Praneeth Kolichala <praneeth.kolichala@gmail.com>
2022-01-31 16:19:06 -0800

    Change name of lemma

--- src/category_theory/discrete_category.lean
+++ src/category_theory/discrete_category.lean
@@ -133,7 +133,7 @@ def nat_iso_functor {I : Type u₁} {F : discrete I ⥤ C} : F ≅ discrete.func
 nat_iso $ λ i, iso.refl _
 
 /-- Composing `discrete.functor F` with another functor `G` amounts to composing `F` with `G.obj` -/
-def discrete_comp_iso_discrete {I : Type u₁} {D : Type u₂} [category.{v₂} D]
+def comp_nat_iso_discrete {I : Type u₁} {D : Type u₂} [category.{v₂} D]
  (F : I → C) (G : C ⥤ D) : discrete.functor F ⋙ G ≅ discrete.functor (G.obj ∘ F) :=
 nat_iso $ λ i, iso.refl _
 

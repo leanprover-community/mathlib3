a819c84a029ab4cbecbdfda44e846079b01d3f36
CBirkbeck <c.birkbeck@ucl.ac.uk>
2022-02-02 17:53:00 +0000

    lint fixes v2

--- src/group_theory/commensurable.lean
+++ src/group_theory/commensurable.lean
@@ -31,7 +31,7 @@ namespace commensurable
 open_locale pointwise
 
 @[refl] protected lemma refl (H : subgroup G) : commensurable H H := by
- simp only [commensurable, nat.one_ne_zero, ne.def, not_false_iff, and_self, subgroup.relindex_self]
+simp [commensurable]
 
 lemma comm {H K : subgroup G} : commensurable H K ↔ commensurable K H := and.comm
 
@@ -39,15 +39,14 @@ lemma comm {H K : subgroup G} : commensurable H K ↔ commensurable K H := and.c
 
 @[trans] lemma trans {H K L : subgroup G} (hhk : commensurable H K ) (hkl : commensurable K L) :
   commensurable H L :=
-⟨subgroup.relindex_ne_zero_trans hhk.1 hkl.1,
-  subgroup.relindex_ne_zero_trans hkl.2 hhk.2⟩
+⟨subgroup.relindex_ne_zero_trans hhk.1 hkl.1, subgroup.relindex_ne_zero_trans hkl.2 hhk.2⟩
 
 lemma equivalence : equivalence (@commensurable G _) :=
 ⟨commensurable.refl, λ _ _, commensurable.symm, λ _ _ _, commensurable.trans⟩
 
 /--Equivalence of `K/H ⊓ K` with `gKg⁻¹/gHg⁻¹ ⊓ gKg⁻¹`-/
 def  quot_conj_equiv (H K : subgroup G) (g : conj_act G) :
-  K ⧸  (H.subgroup_of K) ≃ (g • K).1 ⧸  ((g • H).subgroup_of (g • K)) :=
+  K ⧸ (H.subgroup_of K) ≃ (g • K).1 ⧸ ((g • H).subgroup_of (g • K)) :=
 quotient.congr (K.equiv_smul g).to_equiv (λ a b, by rw [←quotient.eq', ←quotient.eq',
   quotient_group.eq', quotient_group.eq', subgroup.mem_subgroup_of, subgroup.mem_subgroup_of,
   mul_equiv.coe_to_equiv, ←mul_equiv.map_inv, ←mul_equiv.map_mul,
@@ -60,11 +59,10 @@ and_congr (not_iff_not.mpr (eq.congr_left (cardinal.to_nat_congr (quot_conj_equi
 
 lemma commensurable_inv (H : subgroup G) (g : conj_act G) :
   commensurable (g • H) H ↔ commensurable H (g⁻¹ • H) :=
- by rw [commensurable_conj, inv_smul_smul]
+by rw [commensurable_conj, inv_smul_smul]
 
 /-- For `H` a subgroup of `G`, this is the subgroup of all elements `g : conj_aut G`
 such that `commensurable ( g • H) H`   -/
-
 def commensurator' (H : subgroup G) : subgroup (conj_act G) :=
 { carrier := {g : conj_act G | commensurable (g • H) H},
   one_mem' := by rw [set.mem_set_of_eq, one_smul],
@@ -75,7 +73,6 @@ def commensurator' (H : subgroup G) : subgroup (conj_act G) :=
 
 /-- For `H` a subgroup of `G`, this is the subgroup of all elements `g : G`
 such that `commensurable ( g H g⁻¹) H`   -/
-
 def commensurator (H : subgroup G) : subgroup G :=
 (commensurator' H).comap (conj_act.to_conj_act.to_monoid_hom)
 

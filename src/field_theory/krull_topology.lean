82551bc8546461ffb613426cb70ceec5af9026d5
Sebastian-Monnet <sebastian.monnet@gmail.com>
2022-02-02 16:01:28 +0000

    made authors plural

--- src/field_theory/krull_topology.lean
+++ src/field_theory/krull_topology.lean
@@ -1,7 +1,7 @@
 /-
 Copyright (c) 2022 Sebastian Monnet. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
-Author: Sebstian Monnet
+Authors: Sebstian Monnet
 -/
 
 import field_theory.galois

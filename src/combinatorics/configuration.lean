a18680a6122e880ef6f7e285ba846d8c523efed6
Heather Macbeth <25316162+hrmacbeth@users.noreply.github.com>
2022-02-02 07:21:04 +0000

    chore(topology/continuous_function/ordered): split from `continuous_function/basic` (#11761)
    Split material about orders out from `continuous_function/basic`, to move that file lower down the import hierarchy.

--- src/combinatorics/configuration.lean
+++ src/combinatorics/configuration.lean
@@ -3,6 +3,7 @@ Copyright (c) 2021 Thomas Browning. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Thomas Browning
 -/
+import algebra.big_operators.order
 import combinatorics.hall.basic
 import data.fintype.card
 import set_theory.fincard

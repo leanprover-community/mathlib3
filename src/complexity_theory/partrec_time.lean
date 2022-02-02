07e343833e6e747757ec05715a884d96aede1e1a
Bolton Bailey <bolton.bailey@gmail.com>
2022-02-02 00:33:08 -0600

    remove unneded code

--- src/complexity_theory/partrec_time.lean
+++ src/complexity_theory/partrec_time.lean
@@ -13,18 +13,6 @@ import data.nat.log
 # TODO
 -/
 
-/-- μ-recursive algorithms for functions from ℕ^k to ℕ. Similar to `nat.partrec.code`, but with
-arity. -/
-@[derive inhabited]
-inductive μ_recursive : ℕ -> Type
-| const {k : ℕ} {n : ℕ} : μ_recursive k
-| succ : μ_recursive 1
-| proj {i k : ℕ} (h : i < k) : μ_recursive k
-| comp {m k : ℕ} (h : μ_recursive m) (g : fin m -> μ_recursive k) : μ_recursive k
-| ρ {k : ℕ} (g : μ_recursive k) (h : μ_recursive (k + 2)) : μ_recursive (k + 1)
-| μ {k : ℕ} (f : μ_recursive (k + 1)) : μ_recursive k
-
-
 -- Note that we use the code type defined in turing.to_partrec, since this is over lists.
 -- A type of codes that works over naturals will not let us compute without exponential slowdowns.
 open turing.to_partrec

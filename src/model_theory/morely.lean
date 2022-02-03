ad4357397dda1bc2b68b059b0a1913ebb8a638b0
Ramon Fernandez Mir <rfernandezmir@gmail.com>
2022-02-03 14:52:42 +0000

    Lemma about the exchange principle

--- src/model_theory/morely.lean
+++ src/model_theory/morely.lean
@@ -19,6 +19,8 @@ import data.set.finite
 
 universes u
 
+section pregeometry
+
 instance {α : Type u} {X : set α} : has_mem X (𝒫 X) :=
   ⟨λ x A, x.1 ∈ A.1⟩
 
@@ -34,10 +36,39 @@ instance {α : Type u} {X : set α} : has_singleton X (𝒫 X) :=
 instance {α : Type u} {X : set α} : has_sdiff (𝒫 X) :=
   ⟨λ A B, ⟨A.val \ B.val, subset_trans (set.diff_subset A.1 B.1) A.2⟩⟩
 
-class pregeometry (α : Type u) :=
-(X : set α)
-(cl : 𝒫 X → 𝒫 X)
-(monotone_dominating : ∀ {A B}, A ⊆ B → A ⊆ cl A ∧ cl A ⊆ cl B)
-(idempotent : ∀ A, cl (cl A) = cl A)
-(finite_character : ∀ {A}, ∀ a ∈ cl A, ∃ F ⊆ A, set.finite F.1 ∧ a ∈ cl F)
-(exchange_principle : ∀ a b C, b ∈ cl (C ∪ {a}) \ cl C → a ∈ cl (C ∪ {b}))
+instance {α : Type u} {X : set α} : is_refl (𝒫 X) has_subset.subset :=
+  ⟨λ a x hx, hx⟩
+
+class pregeometry {α : Type u} (X : set α) (cl : 𝒫 X → 𝒫 X) :=
+  (monotone_dominating : ∀ {A B}, A ⊆ B → A ⊆ cl A ∧ cl A ⊆ cl B)
+  (idempotent : ∀ A, cl (cl A) = cl A)
+  (finite_character : ∀ {A}, ∀ a ∈ cl A, ∃ F ⊆ A, set.finite F.1 ∧ a ∈ cl F)
+  (exchange_principle : ∀ {a b C}, b ∈ cl (C ∪ {a}) \ cl C → a ∈ cl (C ∪ {b}))
+
+lemma exchange_principle_extended {α : Type} (X : set α) (cl : 𝒫 X → 𝒫 X) [pregeometry X cl] :
+  ∀ a b C, b ∈ cl (C ∪ {a}) \ cl C → a ∈ cl (C ∪ {b}) \ cl C :=
+  begin
+    intros a b C hb,
+    have ha := pregeometry.exchange_principle hb,
+    suffices hanclC : a ∉ cl C,
+    { exact ⟨ha, hanclC⟩, },
+    intros haC,
+    apply hb.2,
+    suffices hclCaclC : cl (C ∪ {a}) ⊆ cl C,
+    { exact (set.mem_of_subset_of_mem hclCaclC hb.1), },
+    suffices hCaclC : (C ∪ {a}) ⊆ cl C,
+    { have hclclC : cl C = cl (cl C) := (pregeometry.idempotent C).symm,
+      rw [hclclC],
+      exact (pregeometry.monotone_dominating hCaclC).2, },
+    have hCcl : C ⊆ cl C := (pregeometry.monotone_dominating (subset_refl C)).1,
+    have haC := (@set.singleton_subset_iff _ a.1 (cl C).1).2 haC,
+    exact set.union_subset hCcl haC,
+  end
+
+end pregeometry
+
+section strongly_minimal
+
+def strongly_minimal : Prop := sorry
+
+end strongly_minimal

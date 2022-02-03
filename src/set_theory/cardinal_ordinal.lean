6d28e0290bee1ea98c0cad595f44809406ff06f2
Violeta Hernández <vi.hdz.p@gmail.com>
2022-02-03 12:15:03 -0600

    Removed unnecessary import

--- src/set_theory/cardinal_ordinal.lean
+++ src/set_theory/cardinal_ordinal.lean
@@ -4,7 +4,6 @@ Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Johannes Hölzl, Mario Carneiro, Floris van Doorn
 -/
 
-import logic.small
 import order.bounded
 import set_theory.principal
 import tactic.linarith

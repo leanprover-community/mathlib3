012ed87e4d54f847460fc815679c481f3e7cd3ef
Sebastian-Monnet <54352341+Sebastian-Monnet@users.noreply.github.com>
2022-02-03 10:39:37 +0000

    Update src/field_theory/krull_topology.lean
    Co-authored-by: Johan Commelin <johan@commelin.net>

--- src/field_theory/krull_topology.lean
+++ src/field_theory/krull_topology.lean
@@ -183,8 +183,7 @@ def krull_topology (K L : Type*) [field K] [field L] [algebra K L] :
   topological_space (L ≃ₐ[K] L) :=
 group_filter_basis.topology (gal_group_basis K L)
 
-/-- For a field extension `L/K`, `krull_topological_group K L` is the topological group consisting
-of `L ≃ₐ[K] L`, together with the Krull topology `krull_topology K L` -/
-lemma krull_topological_group (K L : Type*) [field K] [field L] [algebra K L] :
+/-- For a field extension `L/K`,  the Krull topology on `L ≃ₐ[K] L`, makes it a topological group. -/
+instance (K L : Type*) [field K] [field L] [algebra K L] :
   topological_group (L ≃ₐ[K] L) :=
 group_filter_basis.is_topological_group (gal_group_basis K L)

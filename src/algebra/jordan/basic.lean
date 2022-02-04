27d19dc0b9432ee8c53e42a3391bbabbfb6f5807
Christopher Hoskin <christopher.hoskin@gmail.com>
2022-02-04 21:13:50 +0000

    Clarify that the second multiplication is the symmetrised one

--- src/algebra/jordan/basic.lean
+++ src/algebra/jordan/basic.lean
@@ -19,10 +19,10 @@ in `A`,
 (a * b) * a^2 = a * (b * a^2)
 ```
 i.e. the operators of multiplication by `a` and `a^2` commute. Every associative algebra can be
-equipped with a second  multiplication making it into a commutative Jordan algebra.
-Jordan algebras arising this way are said to be special. There are also exceptional Jordan algebras
-which can be shown not to be the symmetrization of any associative algebra. The 3x3
-matrices of octonions is the canonical example.
+equipped with a symmetrized multiplication making it into a commutative Jordan algebra. Jordan
+algebras arising this way are said to be special. There are also exceptional Jordan algebras which
+can be shown not to be the symmetrization of any associative algebra. The 3x3 matrices of octonions
+is the canonical example.
 
 Every Jordan algebra `A` has a triple product defined, for `a` `b` and `c` in `A` by
 ```

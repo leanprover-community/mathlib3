6aad374fa1de0797882a32fea9884dc57d65f1d2
Jujian Zhang <jujian.zhang1998@outlook.com>
2022-02-06 21:08:47 +0000

    missed a file

--- src/category_theory/preadditive/scratch2.lean
+++ src/category_theory/preadditive/scratch2.lean
@@ -11,10 +11,10 @@ universes v u
 namespace category_theory
 variables {C : Type u} [category.{v} C]
 
-instance [has_equalizers C] : has_coequalizers Cᵒᵖ := sorry
-instance [has_coequalizers C] : has_equalizers Cᵒᵖ := sorry
--- instance [has_images C] : has_images Cᵒᵖ := sorry
-instance [has_zero_morphisms C] [has_cokernels C] : has_kernels Cᵒᵖ := sorry
+-- instance [has_equalizers C] : has_coequalizers Cᵒᵖ := sorry
+-- instance [has_coequalizers C] : has_equalizers Cᵒᵖ := sorry
+-- instance [has_images C] : has_images Cᵒᵖ := sorry -- this is probably wrong
+-- instance [has_zero_morphisms C] [has_cokernels C] : has_kernels Cᵒᵖ := sorry
 
 instance exact.op [has_zero_morphisms C] [has_equalizers C] [has_images C]
   [has_equalizers Cᵒᵖ] [has_images Cᵒᵖ] {X Y Z : C} {f : X ⟶ Y} {g : Y ⟶ Z} [exact f g] :

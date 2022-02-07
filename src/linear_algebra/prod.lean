513b14b4f18716483d71f6b5f48f9ef1803ae323
Joachim Breitner <mail@joachim-breitner.de>
2022-02-07 15:23:53 +0100

    module prod_eq_bot

--- src/linear_algebra/prod.lean
+++ src/linear_algebra/prod.lean
@@ -466,6 +466,14 @@ begin
     simpa using add_mem _ h1' h2', }
 end
 
+lemma prod_eq_bot_iff {p₁ : submodule R M} {p₂ : submodule R M₂} :
+  p₁.prod p₂ = ⊥ ↔ p₁ = ⊥ ∧ p₂ = ⊥ :=
+by simp only [eq_bot_iff, prod_le_iff, (gc_map_comap _).le_iff_le, comap_bot, ker_inl, ker_inr]
+
+lemma prod_eq_top_iff {p₁ : submodule R M} {p₂ : submodule R M₂} :
+  p₁.prod p₂ = ⊤  ↔ p₁ = ⊤ ∧ p₂ = ⊤ :=
+by simp only [eq_top_iff, le_prod_iff, ← (gc_map_comap _).le_iff_le, map_top, range_fst, range_snd],
+
 end submodule
 
 namespace linear_equiv

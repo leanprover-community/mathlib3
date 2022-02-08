6f6745333828746467ae5ca266f4f5da77a47cfa
Dylan MacKenzie <ecstaticmorse@gmail.com>
2022-02-07 23:37:09 -0800

    Apply suggestions from code review
    Co-authored-by: Yury G. Kudryashov <urkud@urkud.name>

--- src/topology/algebra/uniform_group.lean
+++ src/topology/algebra/uniform_group.lean
@@ -154,20 +154,20 @@ uniform_continuous_of_tendsto_one $
   (hu : cauchy_seq u) (hv : cauchy_seq v) : cauchy_seq (u * v) :=
 uniform_continuous_mul.comp_cauchy_seq (hu.prod hv)
 
-@[to_additive] lemma cauchy_seq.mul_const {ι : Type*} [semilattice_sup ι] [nonempty ι]
+@[to_additive] lemma cauchy_seq.mul_const {ι : Type*} [semilattice_sup ι]
   {u : ι → α} {x : α} (hu : cauchy_seq u) : cauchy_seq (λ n, u n * x) :=
-uniform_continuous_mul.comp_cauchy_seq (hu.prod $ cauchy_seq_const x)
+(uniform_continuous_id.mul uniform_continuous_const).comp_cauchy_seq hu
 
-@[to_additive] lemma cauchy_seq.const_mul {ι : Type*} [semilattice_sup ι] [nonempty ι]
+@[to_additive] lemma cauchy_seq.const_mul {ι : Type*} [semilattice_sup ι]
   {u : ι → α} {x : α} (hu : cauchy_seq u) : cauchy_seq (λ n, x * u n) :=
-uniform_continuous_mul.comp_cauchy_seq ((cauchy_seq_const x).prod hu)
+(uniform_continuous_const.mul uniform_continuous_id).comp_cauchy_seq hu
 
 end uniform_group
 
 section uniform_add_group
 variables {α : Type*} [uniform_space α] [add_group α] [uniform_add_group α]
 
-lemma cauchy_seq.neg {ι : Type*} [semilattice_sup ι]
+@[to_additive] lemma cauchy_seq.inv {ι : Type*} [semilattice_sup ι]
   {u : ι → α} (h : cauchy_seq u) : cauchy_seq (-u) :=
 uniform_continuous_neg.comp_cauchy_seq h
 

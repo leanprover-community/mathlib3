4cb244a8b90a9bf7aad23beeb46bf0a2eca24585
Dylan MacKenzie <ecstaticmorse@gmail.com>
2022-02-07 19:42:43 -0800

    feat(topology/algebra/uniform_group): add `cauchy_seq_neg`

--- src/topology/algebra/uniform_group.lean
+++ src/topology/algebra/uniform_group.lean
@@ -154,6 +154,10 @@ uniform_continuous_of_tendsto_one $
   (hu : cauchy_seq u) (hv : cauchy_seq v) : cauchy_seq (u * v) :=
 uniform_continuous_mul.comp_cauchy_seq (hu.prod hv)
 
+lemma cauchy_seq.neg {ι : Type*} [add_group α] [uniform_add_group α] [semilattice_sup ι]
+  {u : ι → α} (h : cauchy_seq u) : cauchy_seq (-u) :=
+uniform_continuous_neg.comp_cauchy_seq h
+
 end uniform_group
 
 section topological_comm_group

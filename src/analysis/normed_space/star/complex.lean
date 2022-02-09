a34d4b001ec2163698c9f1e4fc63d1cd54cd6cb2
Frédéric Dupuis <dupuisf@iro.umontreal.ca>
2022-02-09 15:01:28 -0500

    suggestions + extra golf

--- src/analysis/normed_space/star/complex.lean
+++ src/analysis/normed_space/star/complex.lean
@@ -34,16 +34,8 @@ variables [add_comm_group E] [star_add_monoid E] [module ℂ E] [star_module ℂ
 { to_fun := λ x, ⟨(2⁻¹ : ℂ) • (x + star x), by simp only [self_adjoint.mem_iff, star_smul, add_comm,
                                         star_add_monoid.star_add, star_inv', star_bit0,
                                         star_one, star_star]⟩,
-  map_add' := λ x y, by { refine subtype.ext_val _, simp, abel },
-  map_smul' := λ r x,
-    begin
-      refine subtype.ext_val _,
-      simp only [star_smul, is_R_or_C.star_def, is_R_or_C.conj_to_real, smul_add, ring_hom.id_apply,
-                 subtype.val_eq_coe, self_adjoint.coe_smul, add_subgroup.coe_mk],
-      rw [smul_comm],
-      refine congr_arg _ _,
-      rw [smul_comm]
-    end }
+  map_add' := λ x y, by { ext, simp [add_add_add_comm] },
+  map_smul' := λ r x, by { ext, simp [smul_comm (2⁻¹ : ℂ) r] } }
 
 /-- The imaginary part of an element of a star module, as a real-linear map. -/
 @[simps] noncomputable def im : E →ₗ[ℝ] self_adjoint E :=
@@ -57,21 +49,19 @@ variables [add_comm_group E] [star_add_monoid E] [module ℂ E] [star_module ℂ
     end⟩,
   map_add' := λ x y,
     begin
-      refine subtype.ext_val _,
-      simp only [neg_mul_eq_neg_mul_symm, neg_smul, star_add_monoid.star_add, subtype.val_eq_coe,
-                 add_subgroup.coe_add, add_subgroup.coe_mk],
-      conv { to_rhs, rw [←neg_smul, ←neg_smul] },
-      rw [←neg_smul, ←smul_add],
+      ext,
+      simp only [neg_mul_eq_neg_mul_symm, ←neg_smul, star_add, add_subgroup.coe_mk,
+                 add_subgroup.coe_add, ←smul_add],
       refine congr_arg _ _,
-      abel
+      abel,
     end,
   map_smul' := λ r x,
     begin
-      refine subtype.ext_val _,
+      ext,
       simp only [neg_mul_eq_neg_mul_symm, neg_smul, star_smul, is_R_or_C.star_def,
                  is_R_or_C.conj_to_real, ring_hom.id_apply, subtype.val_eq_coe,
-                 self_adjoint.coe_smul, add_subgroup.coe_mk, smul_neg, neg_inj, ←smul_sub],
-      rw [smul_comm],
+                 self_adjoint.coe_smul, add_subgroup.coe_mk, smul_neg, neg_inj, ←smul_sub,
+                 smul_comm r],
     end }
 
 /-- An element of a complex star module can be decomposed into self-adjoint "real" and "imaginary"

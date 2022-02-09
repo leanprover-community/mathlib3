1c53d894b382632b2c4a6353c5fd86547b0604c1
Yakov Pechersky <ffxen158@gmail.com>
2022-02-09 02:01:11 -0500

    initial attempt

---
+++ src/nakada.lean
@@ -0,0 +1,199 @@
+/-
+Copyright (c) 2022 Yakov Pechersky. All rights reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
+Authors: Yakov Pechersky
+-/
+
+import algebra.order.group
+import tactic.tfae
+
+/-!
+# Formalization of linear extensions to partially ordered semigroups
+
+Following [PARTIALLY ORDERED ABELIAN SEMIGROUPS]
+(https://eprints.lib.hokudai.ac.jp/dspace/bitstream/2115/55970/1/JFSHIU_11_N4_181-189.pdf)
+-/
+
+open function (swap)
+
+section page181
+
+variables (S : Type*) [has_mul S] [has_le S]
+
+-- Definition 1
+@[to_additive add_nakada_po]
+abbreviation nakada_po : Prop :=
+covariant_class S S (swap (*)) (≤)
+
+variables {S}
+
+@[to_additive add_homogeneity]
+lemma homogeneity [nakada_po S] {a b : S} (h : a ≤ b) (c : S) :
+  a * c ≤ b * c :=
+mul_le_mul_right' h c
+
+-- extra API
+@[to_additive nsmul_le_nsmul_of_le]
+lemma pow_le_pow_of_le {S : Type*} [comm_monoid S] [preorder S] [nakada_po S] {a b : S}
+  (h : a ≤ b) (n : ℕ) : a ^ n ≤ b ^ n :=
+begin
+  induction n with n IH generalizing a b,
+  { simp },
+  { rw [pow_succ', pow_succ'],
+    refine (homogeneity (IH h) a).trans _,
+    rw [mul_comm _ a, mul_comm _ b],
+    exact homogeneity h _ }
+end
+
+variables (S)
+
+-- Definition 2
+@[to_additive add_nakada_strong]
+abbreviation nakada_strong : Prop :=
+contravariant_class S S (swap (*)) (≤)
+
+variables {S}
+
+@[to_additive add_strong]
+lemma strong [nakada_strong S] {a b c : S} (h : a * c ≤ b * c) : a ≤ b :=
+le_of_mul_le_mul_right' h
+
+-- Theorem 1
+@[priority 10, to_additive add_group.add_nakada_strong]
+instance group.nakada_strong {G : Type*} [group G] [has_le G] [nakada_po G] :
+  nakada_strong G :=
+⟨λ c a b h, by simpa using homogeneity h c⁻¹⟩
+
+end page181
+
+section page182
+
+variables {S S' : Type*} [has_mul S] [partial_order S] [has_mul S] [partial_order S]
+
+-- Theorem 2.1
+@[to_additive]
+lemma nakada_strong.cancel_right [nakada_strong S] {a b c : S} (h : a * c = b * c) :
+  a = b :=
+le_antisymm (strong h.le) (strong h.ge)
+
+-- Theorem 2.2
+@[to_additive]
+instance nakada_strong.contravariant_mul_lt [nakada_strong S] :
+  contravariant_class S S (swap (*)) (<) :=
+⟨λ c a b (h : a * c < b * c), begin
+  by_cases H : a = b,
+  { simpa [H] using h },
+  { simpa [lt_iff_le_and_ne, H] using strong h.le }
+end⟩
+
+-- Theorem 2.3
+@[to_additive add_nakada_po.covariant_add_lt_of_add_nakada_strong]
+instance nakada_po.covariant_mul_lt_of_nakada_strong [nakada_po S] [nakada_strong S] :
+  covariant_class S S (swap (*)) (<) :=
+⟨λ c a b h, begin
+  by_cases H : a * c = b * c,
+  { rw [nakada_strong.cancel_right H] at h,
+    exact absurd h (lt_irrefl _) },
+  { simpa [lt_iff_le_and_ne, H] using homogeneity h.le c }
+end⟩
+
+-- Theorem 3.1
+@[to_additive add_nakada_lo.contravariant_add_lt]
+instance nakada_lo.contravariant_mul_lt {S : Type*} [has_mul S] [linear_order S] [nakada_po S] :
+  contravariant_class S S (swap (*)) (<) :=
+⟨λ c a b h, begin
+  refine (le_or_lt _ _).resolve_left (λ H, _),
+  exact absurd (homogeneity H c) h.not_le
+end⟩
+
+-- Theorem 3.2
+@[to_additive nsmul_lt_nsmul_cancel]
+lemma pow_lt_pow_cancel {S : Type*} [comm_monoid S] [linear_order S] [nakada_po S] {a b : S} {n : ℕ}
+  (h : a ^ n < b ^ n) : a < b :=
+begin
+  refine (le_or_lt _ _).resolve_left (λ H, _),
+  exact absurd (pow_le_pow_of_le H _) h.not_le,
+end
+
+-- Theorem 4
+lemma nakada_lo.tfae {S : Type*} [has_mul S] [linear_order S] [nakada_po S] :
+  tfae [∀ {a b c : S}, a * c ≤ b * c → a ≤ b,
+        ∀ {a b c : S}, a * c = b * c → a = b,
+        ∀ {a b : S} (h : a < b) (c : S), a * c < b * c] :=
+begin
+  tfae_have : 1 → 2,
+  { intros H a b c h,
+    haveI : nakada_strong S := ⟨λ _ _ _, H⟩,
+    exact nakada_strong.cancel_right h },
+  tfae_have : 2 → 3,
+  { intros H a b h c,
+    refine lt_of_le_of_ne (homogeneity h.le c) (λ he, _),
+    simpa [H he] using h },
+  tfae_have : 3 → 1,
+  { intros H a b c,
+    contrapose!,
+    exact λ h, H h c },
+  tfae_finish
+end
+
+-- Definition 3
+@[ext]
+structure add_order_iso (S S' : Type*) [has_add S] [has_le S] [has_add S'] [has_le S'] extends
+  S ≃+ S' :=
+(map_rel_iff' : ∀ {a b : S}, (to_fun a) ≤ (to_fun b) ↔ a ≤ b)
+
+@[ext, to_additive]
+structure mul_order_iso (S S' : Type*) [has_mul S] [has_le S] [has_mul S'] [has_le S'] extends
+  S ≃* S' :=
+(map_rel_iff' : ∀ {a b : S}, (to_fun a) ≤ (to_fun b) ↔ a ≤ b)
+
+namespace mul_order_iso
+
+variables {M N : Type*} [has_mul M] [has_le M] [has_mul N] [has_le N]
+
+infix ` ≃*o `:25 := mul_order_iso
+infix ` ≃+o `:25 := add_order_iso
+
+@[to_additive]
+instance : has_coe_to_fun (M ≃*o N) (λ _, M → N) := ⟨λ e, e.to_mul_equiv⟩
+
+@[to_additive]
+def to_order_iso (e : M ≃*o N) : M ≃o N := { ..e }
+
+@[refl, to_additive]
+def refl (M : Type*) [has_mul M] [has_le M] : M ≃*o M :=
+{ ..mul_equiv.refl _, ..order_iso.refl _ }
+
+@[to_additive]
+instance : inhabited (M ≃*o M) := ⟨refl M⟩
+
+@[symm, to_additive]
+def symm (e : M ≃*o N) : N ≃*o M :=
+{ ..e.to_mul_equiv.symm, ..e.to_order_iso.symm }
+
+@[simp, to_additive]
+lemma inv_fun_eq_symm (e : M ≃*o N) : e.inv_fun = e.symm := rfl
+
+@[simp, to_additive]
+lemma coe_to_mul_equiv (e : M ≃*o N) : (e.to_mul_equiv : M → N) = e := rfl
+
+@[to_additive]
+instance : mul_equiv_class (M ≃*o N) M N :=
+{ coe := λ e, e, inv := λ e, e.symm,
+  left_inv := λ e, e.to_mul_equiv.left_inv,
+  right_inv := λ e, e.to_mul_equiv.right_inv,
+  coe_injective' := λ f g h₁ h₂, by { ext x, exact congr_fun h₁ x },
+  map_mul := λ e, e.map_mul' }
+
+@[to_additive]
+instance {M N : Type*} [has_mul M] [preorder M] [has_mul N] [preorder N] :
+  order_hom_class (M ≃*o N) M N :=
+{ coe := λ e, e,
+  coe_injective' := λ f g h, by { ext x, exact congr_fun h x },
+  map_rel := λ e a b, (@mul_order_iso.map_rel_iff' _ _ _ _ _ _ _ _ _).mpr }
+
+end mul_order_iso
+
+-- Theorem 5
+
+end page182

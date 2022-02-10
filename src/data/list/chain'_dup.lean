7f96468f6abbcd21bb8047f18f732cad1947f33c
Eric <ericrboidi@gmail.com>
2022-02-10 11:59:34 +0000

    I sort of now get why `chain` is preferred

--- src/data/list/chain'_dup.lean
+++ src/data/list/chain'_dup.lean
@@ -3,29 +3,25 @@ Copyright (c) 2022 Eric Rodriguez. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Eric Rodriguez
 -/
-import data.list.basic
+import data.list.chain
 
 /-!
-# Pairwise Deduplication of Lists
+# Chain Deduplication of Lists
 
-This file defines `list.no_pairwise_dup`, which removes all duplicates that are adjacent in a list,
-e.g. `[2, 2, 3, 3, 2].no_pairwise_dup = [2, 3, 2]`.
+This file defines `list.chain'_dedup`, which removes all duplicates that are adjacent in a list,
+e.g. `[2, 2, 3, 3, 2].chain'_dedup = [2, 3, 2]`.
 
 ## Main definitions
 
-* `list.no_pairwise_dup`: removes all adjacent duplicates in a list.
+* `list.chain'_dedup`: removes all adjacent duplicates in a list.
 
 ## Main statements
 
-* `list.no_pairwise_dup_sublist`: `l.no_pairwise_dup` is a sublist of `l`.
-
-## Implementation details
-
-We use `list.no_pairwise_dup_aux` as a way to implement `list.no_pairwise_dup`.
+* `list.chain'_dedup_sublist`: `l.chain'_dedup` is a sublist of `l`.
 
 ## Tags
 
-adjacent, pairwise, duplicates, remove, list
+adjacent, chain, duplicates, remove, list
 -/
 
 
@@ -33,51 +29,77 @@ variables {α : Type*} [decidable_eq α] (a b : α) (l : list α)
 
 namespace list
 
-/-- Implements `list.no_pairwise_dup`. -/
-def no_pairwise_dup_aux : α → list α → list α
+/-- Removes all adjacent duplicates in `a :: l`. -/
+def chain_dedup : α → list α → list α
 | a [] := []
-| a (h :: l) := if a = h then no_pairwise_dup_aux h l else h :: no_pairwise_dup_aux h l
+| a (h :: l) := if a = h then chain_dedup h l else h :: chain_dedup h l
 
-@[simp] lemma no_pairwise_dup_aux_nil : no_pairwise_dup_aux a [] = [] := rfl
+@[simp] lemma chain_dedup_nil : chain_dedup a [] = [] := rfl
 
 /-- Removes all adjacent duplicates in a list. -/
-def no_pairwise_dup : list α → list α
-| (h :: l) := h :: no_pairwise_dup_aux h l
+def chain'_dedup : list α → list α
+| (h :: l) := h :: chain_dedup h l
 | [] := []
 
-@[simp] lemma no_pairwise_dup_nil : ([] : list α).no_pairwise_dup = [] := rfl
+@[simp] lemma chain'_dedup_nil : ([] : list α).chain'_dedup = [] := rfl
 
-lemma no_pairwise_dup_cons' : (a :: l).no_pairwise_dup = a :: no_pairwise_dup_aux a l := rfl
+lemma chain'_dedup_cons' : (a :: l).chain'_dedup = a :: chain_dedup a l := rfl
 
-lemma no_pairwise_dup_cons_cons' : (a :: b :: l).no_pairwise_dup =
-  a :: if a = b then no_pairwise_dup_aux b l else b :: no_pairwise_dup_aux b l := rfl
+lemma chain'_dedup_cons_cons : (a :: b :: l).chain'_dedup =
+  a :: if a = b then chain_dedup b l else b :: chain_dedup b l := rfl
 
-@[simp] lemma no_pairwise_dup_singleton : no_pairwise_dup [a] = [a] := rfl
+@[simp] lemma chain'_dedup_singleton : chain'_dedup [a] = [a] := rfl
 
-@[simp] lemma no_pairwise_dup_pair :
-  no_pairwise_dup [a, b] = if a = b then [a] else [a, b] :=
-by split_ifs; simp [no_pairwise_dup_cons_cons', h]
+@[simp] lemma chain'_dedup_pair :
+  chain'_dedup [a, b] = if a = b then [a] else [a, b] :=
+by split_ifs; simp [chain'_dedup_cons_cons, h]
 
 @[simp]
-lemma no_pairwise_dup_cons_cons : (a :: a :: l).no_pairwise_dup = (a :: l).no_pairwise_dup :=
-by { rw [no_pairwise_dup_cons_cons', if_pos rfl], refl }
+lemma chain'_dedup_cons_cons' : (a :: a :: l).chain'_dedup = (a :: l).chain'_dedup :=
+by { rw [chain'_dedup_cons_cons, if_pos rfl], refl }
 
-lemma no_pairwise_dup_eq_or_eq : (a :: l).no_pairwise_dup = l.no_pairwise_dup ∨
-  (a :: l).no_pairwise_dup = a :: l.no_pairwise_dup :=
+lemma chain'_dedup_eq_or_eq : (a :: l).chain'_dedup = l.chain'_dedup ∨
+  (a :: l).chain'_dedup = a :: l.chain'_dedup :=
 begin
   induction l with h l hl,
   { simp },
-  rw [no_pairwise_dup_cons_cons'],
-  split_ifs with h; simp [h, no_pairwise_dup]
+  rw [chain'_dedup_cons_cons],
+  split_ifs with h; simp [h, chain'_dedup]
 end
 
-lemma no_pairwise_dup_sublist : l.no_pairwise_dup <+ l :=
+lemma chain'_dedup_sublist : l.chain'_dedup <+ l :=
 begin
   induction l with h l hl,
   { simp },
-  cases no_pairwise_dup_eq_or_eq h l with h h; rw h,
+  cases chain'_dedup_eq_or_eq h l with h h; rw h,
   { exact hl.cons _ _ _ },
   { exact hl.cons2 _ _ _ }
 end
+
+lemma chain'_dedup_is_chain : l.chain'_dedup.chain' ne :=
+begin
+  induction l with h l hl,
+  { simp },
+  cases l with i l,
+  { simp },
+  rw chain'_dedup_cons_cons,
+  split_ifs with hih,
+  { rwa hih },
+  { exact chain_cons.mpr ⟨hih, hl⟩ }
+end
+
+lemma chain'_dedup_of_chain (h : l.chain' ne) : l.chain'_dedup = l :=
+begin
+  induction l with a l ha,
+  { simp },
+  cases l with b l,
+  { simp },
+  have := chain'_cons.mp h,
+  specialize ha this.2,
+  rw [chain'_dedup_cons_cons, if_neg this.1, ←chain'_dedup, ha]
+end
+
+lemma chain_dedup_idem : l.chain'_dedup.chain'_dedup = l.chain'_dedup :=
+chain'_dedup_of_chain _ $ l.chain'_dedup_is_chain
 
 end list

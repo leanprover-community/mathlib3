fbdfe38e076a66ca7894f5866bddc0b7ecfcaa1a
Eric Wieser <wieser.eric@gmail.com>
2022-02-10 17:03:46 +0000

    authorship

--- src/data/list/chain_dup.lean
+++ src/data/list/chain_dup.lean
@@ -1,7 +1,7 @@
 /-
 Copyright (c) 2022 Eric Rodriguez. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
-Authors: Eric Rodriguez
+Authors: Eric Rodriguez, Eric Wieser
 -/
 import data.list.chain
 

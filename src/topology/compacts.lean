bed7dc35342bb3425738c48cb4996eafb0c2502b
YaelDillies <yael.dillies@gmail.com>
2022-02-10 22:29:29 +0000

    initial commit

--- src/topology/compacts.lean
+++ src/topology/compacts.lean
@@ -1,13 +1,12 @@
 /-
 Copyright (c) 2020 Floris van Doorn. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
-Authors: Floris van Doorn
+Authors: Floris van Doorn, Yaël Dillies
 -/
-import topology.homeomorph
+import topology.opens
+
 /-!
 # Compact sets
-
-## Summary
 
 We define the subtype of compact sets in a topological space.
 
@@ -17,49 +16,30 @@ We define the subtype of compact sets in a topological space.
 - `compacts α` is the type of compact subsets of a topological space `α`.
 - `nonempty_compacts α` is the type of non-empty compact subsets.
 - `positive_compacts α` is the type of compact subsets with non-empty interior.
+- `open_compacts α`: The type of open compact sets of a topological space `α`.
 -/
+
 open set
 
-
 variables (α : Type*) {β : Type*} [topological_space α] [topological_space β]
+
 namespace topological_space
 
+/-! ### Closed sets -/
+
 /-- The type of closed subsets of a topological space. -/
-def closeds := {s : set α // is_closed s}
+def closeds : Type* := {s : set α // is_closed s}
 
 /-- The type of closed subsets is inhabited, with default element the empty set. -/
 instance : inhabited (closeds α) := ⟨⟨∅, is_closed_empty ⟩⟩
 
+/-! ### Compact sets -/
+
 /-- The compact sets of a topological space. See also `nonempty_compacts`. -/
 def compacts : Type* := { s : set α // is_compact s }
 
-/-- The type of non-empty compact subsets of a topological space. The
-non-emptiness will be useful in metric spaces, as we will be able to put
-a distance (and not merely an edistance) on this space. -/
-def nonempty_compacts := {s : set α // s.nonempty ∧ is_compact s}
-
-/-- In an inhabited space, the type of nonempty compact subsets is also inhabited, with
-default element the singleton set containing the default element. -/
-instance nonempty_compacts_inhabited [inhabited α] : inhabited (nonempty_compacts α) :=
-⟨⟨{default}, singleton_nonempty default, is_compact_singleton ⟩⟩
-
-/-- The compact sets with nonempty interior of a topological space. See also `compacts` and
-  `nonempty_compacts`. -/
-@[nolint has_inhabited_instance]
-def positive_compacts: Type* := { s : set α // is_compact s ∧ (interior s).nonempty }
-
-/-- In a nonempty compact space, `set.univ` is a member of `positive_compacts`, the compact sets
-with nonempty interior. -/
-def positive_compacts_univ {α : Type*} [topological_space α] [compact_space α] [nonempty α] :
-  positive_compacts α :=
-⟨set.univ, compact_univ, by simp⟩
-
-@[simp] lemma positive_compacts_univ_val (α : Type*) [topological_space α] [compact_space α]
-  [nonempty α] : (positive_compacts_univ : positive_compacts α).val = univ := rfl
-
-variables {α}
-
 namespace compacts
+variables {α}
 
 instance : semilattice_sup (compacts α) :=
 subtype.semilattice_sup (λ K₁ K₂, is_compact.union)
@@ -67,7 +47,7 @@ subtype.semilattice_sup (λ K₁ K₂, is_compact.union)
 instance : order_bot (compacts α) :=
 subtype.order_bot is_compact_empty
 
-instance [t2_space α]: semilattice_inf (compacts α) :=
+instance [t2_space α] : semilattice_inf (compacts α) :=
 subtype.semilattice_inf (λ K₁ K₂, is_compact.inter)
 
 instance [t2_space α] : lattice (compacts α) :=
@@ -108,30 +88,121 @@ congr_fun (image_eq_preimage_of_inverse f.left_inv f.right_inv) K.1
 
 end compacts
 
-section nonempty_compacts
-open topological_space set
-variable {α}
+/-! ### Nonempty compact sets -/
 
-instance nonempty_compacts.to_compact_space {p : nonempty_compacts α} : compact_space p.val :=
-⟨is_compact_iff_is_compact_univ.1 p.property.2⟩
+/-- The type of non-empty compact subsets of a topological space. The
+non-emptiness will be useful in metric spaces, as we will be able to put
+a distance (and not merely an edistance) on this space. -/
+def nonempty_compacts : Type* := {s : set α // s.nonempty ∧ is_compact s}
 
-instance nonempty_compacts.to_nonempty {p : nonempty_compacts α} : nonempty p.val :=
-p.property.1.to_subtype
+namespace nonempty_compacts
+variables {α}
+
+/-- Turns a nonempty compact set into the corresponding compact set. -/
+def to_compacts : nonempty_compacts α → compacts α := inclusion $ λ s hs, hs.2
 
 /-- Associate to a nonempty compact subset the corresponding closed subset -/
-def nonempty_compacts.to_closeds [t2_space α] : nonempty_compacts α → closeds α :=
-set.inclusion $ λ s hs, hs.2.is_closed
+def to_closeds [t2_space α] : nonempty_compacts α → closeds α := inclusion $ λ s hs, hs.2.is_closed
+
+@[ext] protected lemma ext {s t : nonempty_compacts α} (h : s.1 = t.1) : s = t := subtype.eq h
+
+instance : semilattice_sup (nonempty_compacts α) :=
+subtype.semilattice_sup $ λ s t hs ht, ⟨hs.1.mono $ subset_union_left _ _, hs.2.union ht.2⟩
+
+instance [compact_space α] [nonempty α] : order_top (nonempty_compacts α) :=
+subtype.order_top ⟨univ_nonempty, compact_univ⟩
+
+/-- In an inhabited space, the type of nonempty compact subsets is also inhabited, with
+default element the singleton set containing the default element. -/
+instance [inhabited α] : inhabited (nonempty_compacts α) :=
+⟨⟨{default}, singleton_nonempty default, is_compact_singleton ⟩⟩
+
+instance to_compact_space {p : nonempty_compacts α} : compact_space p.val :=
+⟨is_compact_iff_is_compact_univ.1 p.property.2⟩
+
+instance to_nonempty {p : nonempty_compacts α} : nonempty p.val := p.property.1.to_subtype
 
 end nonempty_compacts
 
-section positive_compacts
+/-! ### Positive compact sets -/
 
-variable (α)
+/-- The compact sets with nonempty interior of a topological space. See also `compacts` and
+`nonempty_compacts`. -/
+def positive_compacts : Type* := {s : set α // is_compact s ∧ (interior s).nonempty}
+
+namespace positive_compacts
+variables {α}
+
+/-- Turns an open compact set into the corresponding compact set. -/
+def to_opens : positive_compacts α → compacts α := inclusion $ λ s hs, hs.1
+
+@[ext] protected lemma ext {s t : positive_compacts α} (h : s.1 = t.1) : s = t := subtype.eq h
+
+instance : semilattice_sup (positive_compacts α) :=
+subtype.semilattice_sup $ λ s t hs ht,
+  ⟨hs.1.union ht.1, hs.2.mono $ interior_mono $ subset_union_left _ _⟩
+
+instance [compact_space α] [nonempty α] : order_top (positive_compacts α) :=
+subtype.order_top ⟨compact_univ, by { convert univ_nonempty, exact interior_univ, apply_instance }⟩
+
+instance [compact_space α] [nonempty α] : inhabited (positive_compacts α) := ⟨⊤⟩
+
 /-- In a nonempty locally compact space, there exists a compact set with nonempty interior. -/
-instance nonempty_positive_compacts [locally_compact_space α] [h : nonempty α] :
-  nonempty (positive_compacts α) :=
-let ⟨K, hK⟩ := exists_compact_subset is_open_univ $ mem_univ h.some in ⟨⟨K, hK.1, ⟨_, hK.2.1⟩⟩⟩
+instance [locally_compact_space α] [nonempty α] : nonempty (positive_compacts α) :=
+let ⟨K, hK⟩ := exists_compact_subset is_open_univ $ mem_univ (classical.arbitrary α) in
+  ⟨⟨K, hK.1, _, hK.2.1⟩⟩
+
+@[simp] lemma val_sup (s t : positive_compacts α) : (s ⊔ t).1 = s.1 ∪ t.1 := rfl
+@[simp] lemma val_top [compact_space α] [nonempty α] : (⊤ : positive_compacts α).1 = univ := rfl
 
 end positive_compacts
 
+/-! ### Open compact sets -/
+
+/-- The open compact sets of a topological space. See also `compacts` and `opens`. -/
+def open_compacts : Type* := {s : set α // is_open s ∧ is_compact s}
+
+namespace open_compacts
+variables {α}
+
+/-- Turns an open compact set into the corresponding open set. -/
+def to_opens : open_compacts α → opens α := inclusion $ λ s hs, hs.1
+
+/-- Turns an open compact set into the corresponding compact set. -/
+def to_compacts : open_compacts α → compacts α := inclusion $ λ s hs, hs.2
+
+lemma «open» (s : open_compacts α) : is_open s.1 := s.2.1
+lemma compact (s : open_compacts α) : is_compact s.1 := s.2.2
+
+@[ext] protected lemma ext {s t : open_compacts α} (h : s.1 = t.1) : s = t := subtype.eq h
+
+instance : semilattice_sup (open_compacts α) :=
+subtype.semilattice_sup $ λ s t hs ht, ⟨hs.1.union ht.1, hs.2.union ht.2⟩
+
+instance : order_bot (open_compacts α) := subtype.order_bot ⟨is_open_empty, is_compact_empty⟩
+instance [compact_space α] : order_top (open_compacts α) :=
+subtype.order_top ⟨is_open_univ, compact_univ⟩
+
+instance : inhabited (open_compacts α) := ⟨⊥⟩
+
+instance [t2_space α] : semilattice_inf (open_compacts α) :=
+subtype.semilattice_inf $ λ s t hs ht, ⟨hs.1.inter ht.1, hs.2.inter ht.2⟩
+
+instance [t2_space α] : lattice (open_compacts α) :=
+{ ..open_compacts.semilattice_sup, ..open_compacts.semilattice_inf }
+
+@[simp] lemma val_sup (s t : open_compacts α) : (s ⊔ t).1 = s.1 ∪ t.1 := rfl
+@[simp] lemma val_inf [t2_space α] (s t : open_compacts α) : (s ⊓ t).1 = s.1 ∩ t.1 := rfl
+@[simp] lemma val_bot : (⊥ : open_compacts α).1 = ∅ := rfl
+@[simp] lemma val_top [compact_space α] : (⊤ : open_compacts α).1 = univ := rfl
+
+/-- The image of an open compact under a continuous open map. -/
+def map (f : α → β) (hf : continuous f) (hf' : is_open_map f) (s : open_compacts α) :
+  open_compacts β :=
+⟨f '' s.1, hf' _ s.open, s.compact.image hf⟩
+
+@[simp] lemma val_map {f : α → β} (hf : continuous f) (hf' : is_open_map f) (s : open_compacts α) :
+  (s.map f hf hf').1 = f '' s.1 := rfl
+
+end open_compacts
 end topological_space

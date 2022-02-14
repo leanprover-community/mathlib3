d70d52a115f0947da1f0da1790088b3d87785101
Alex J. Best <alex.j.best@gmail.com>
2022-02-14 18:54:50 +0100

    more homog version

--- src/order/extension.lean
+++ src/order/extension.lean
@@ -9,6 +9,7 @@ import tactic.by_contra
 import algebra.group_power.basic
 import algebra.smul_with_zero
 import algebra.module
+import tactic.group
 
 /-!
 # Extend a partial order to a linear order
@@ -121,34 +122,38 @@ theorem extend_ordered_group {α : Type u} [o : ordered_comm_group α]
     (@linear_ordered_comm_group.to_ordered_comm_group α l) :=
 begin
   let S := { s | is_partial_order α s ∧
-                (∀ a b : α, s a b → ∀ c : α, s (c * a) (c * b)) ∧
+                (∀ a b c : α, s a b ↔ s (c * a) (c * b)) ∧
                 ∀ x y, (∃ (n : ℕ) (hn : n ≠ 0), s (y ^ n) (x ^ n)) → s y x },
-  have hS : ∀ c, c ⊆ S → zorn.chain (≤) c → ∀ y ∈ c, ∃ ub ∈ S, ∀ z ∈ c, z ≤ ub,
-  { rintro c hc₁ hc₂ s hs,
-    haveI := (hc₁ hs).1.1,
-    refine ⟨Sup c, _, λ z hz, le_Sup hz⟩,
+  have hS : ∀ C, C ⊆ S → zorn.chain (≤) C → ∀ y ∈ C, ∃ ub ∈ S, ∀ z ∈ C, z ≤ ub,
+  { rintro C hC₁ hC₂ s hs,
+    haveI := (hC₁ hs).1.1,
+    refine ⟨Sup C, _, λ z hz, le_Sup hz⟩,
     refine ⟨{ refl := _, trans := _, antisymm := _ }, _, _⟩,
     any_goals{ simp_rw binary_relation_Sup_iff},
     { intro x,
       exact ⟨s, hs, refl x⟩ },
     { rintro x y z ⟨s₁, h₁s₁, h₂s₁⟩ ⟨s₂, h₁s₂, h₂s₂⟩,
-      haveI : is_partial_order _ _ := (hc₁ h₁s₁).1,
-      haveI : is_partial_order _ _ := (hc₁ h₁s₂).1,
-      cases hc₂.total_of_refl h₁s₁ h₁s₂,
+      haveI : is_partial_order _ _ := (hC₁ h₁s₁).1,
+      haveI : is_partial_order _ _ := (hC₁ h₁s₂).1,
+      cases hC₂.total_of_refl h₁s₁ h₁s₂,
       { exact ⟨s₂, h₁s₂, trans (h _ _ h₂s₁) h₂s₂⟩ },
       { exact ⟨s₁, h₁s₁, trans h₂s₁ (h _ _ h₂s₂)⟩ } },
     { rintro x y ⟨s₁, h₁s₁, h₂s₁⟩ ⟨s₂, h₁s₂, h₂s₂⟩,
-      haveI : is_partial_order _ _ := (hc₁ h₁s₁).1,
-      haveI : is_partial_order _ _ := (hc₁ h₁s₂).1,
-      cases hc₂.total_of_refl h₁s₁ h₁s₂,
+      haveI : is_partial_order _ _ := (hC₁ h₁s₁).1,
+      haveI : is_partial_order _ _ := (hC₁ h₁s₂).1,
+      cases hC₂.total_of_refl h₁s₁ h₁s₂,
       { exact antisymm (h _ _ h₂s₁) h₂s₂ },
       { apply antisymm h₂s₁ (h _ _ h₂s₂) } },
-    { rintro x y ⟨s₁, h₁s₁, h₂s₁⟩ z, -- TODO is this junk removable?
-      use [s₁, h₁s₁, (hc₁ h₁s₁).2.1 _ _ h₂s₁ z], },
+    { intros x y z,
+      split;
+      rintros ⟨s₁, h₁s₁, h₂s₁⟩; -- TODO is this junk removable?
+      use [s₁, h₁s₁],
+      rwa ← (hC₁ h₁s₁).2.1,
+      rwa (hC₁ h₁s₁).2.1, },
     { rintro x y ⟨n, hn, r, hr₁, hr₂⟩,
-      use [r, hr₁, (hc₁ hr₁).2.2 _ _ ⟨n, hn, hr₂⟩], }, },
+      use [r, hr₁, (hC₁ hr₁).2.2 _ _ ⟨n, hn, hr₂⟩], }, },
   obtain ⟨s, ⟨hs₁, hs₁a, hs₁b⟩, rs, hs₂⟩ := zorn.zorn_nonempty_partial_order₀ S hS (≤)
-    ⟨is_partial_order.mk, o.mul_le_mul_left, h_norm⟩,
+    ⟨is_partial_order.mk, λ a b c, by rw mul_le_mul_iff_left, h_norm⟩,
   resetI,
   have inst_refl : is_refl α s := by apply_instance, -- probably dont need to be haveI's
   have inst_trans : is_trans α s := by apply_instance,
@@ -160,36 +165,33 @@ begin
       le_antisymm := inst_antisymm.antisymm,
       le_total := _,
       decidable_le := by apply_instance,
-      mul_le_mul_left := hs₁a,
+      mul_le_mul_left := λ a b hab c, (hs₁a a _ _).mp hab,
       ..(@ordered_comm_group.to_comm_group α o)},
   refine ⟨t, rs⟩,
   intros x y,
   change s x y ∨ s y x,
   by_contra h,
-  let s' := λ x' y', ∃ p q (hpq : p ≠ 0 ∨ q ≠ 0), s ((y / x) ^ q) ((y' / x') ^ p), -- s x' y' ∨ s x' x ∧ s y y',
+  let s' := λ x' y', ∃ p q (hpq : p ≠ 0 ∨ q ≠ 0), s (y ^ q * x' ^ p) (y' ^ p * x ^ q),
   have hfine : s ≤ s',
   { intros a b h, --finer
     use [1, 0],
-    simp only [ne.def, nat.one_ne_zero, not_false_iff, eq_self_iff_true, not_true,
-      or_false, zero_smul, one_nsmul, true_and],
-    convert hs₁a _ _ h a⁻¹,
-    { simp only [pow_zero, mul_left_inv], },
-    { simp only [pow_one, div_eq_inv_mul' b a], }, },
-  have hp : ∀ (x') (y') {p q} (hpq : p ≠ 0 ∨ q ≠ 0) (hs' : s ((y / x) ^ q) ((y' / x') ^ p)), p ≠ 0,
+    simpa, },
+  have hp : ∀ x' y' {p q} (hpq : p ≠ 0 ∨ q ≠ 0) (hs' : s (y ^ q * x' ^ p) (y' ^ p * x ^ q)), p ≠ 0,
   { rintro x' y' p q (hpq | hpq) hs',
     { assumption, },
     intro hp,
     apply h,
     right,
-    rw [hp, pow_zero, ← one_pow q] at hs',
-    simpa using hs₁a _ _ (hs₁b _ _ ⟨q, hpq, hs'⟩) x, },
+    simp only [hp, mul_one, pow_zero, one_mul] at hs',
+    simpa using hs₁b _ _ ⟨q, hpq, hs'⟩, },
   rw ← hs₂ s' _ hfine at h,
   { exact h (or.inl ⟨1, 1, by simp, refl _⟩) }, -- case α ????    v) in Nakada
   { have key : ∀ (a b c : α) (pab qab : ℕ) (habn : pab ≠ 0 ∨ qab ≠ 0)
-      (hab : s ((y / x) ^ qab) ((b / a) ^ pab)) (pbc qbc : ℕ) (hbcn : pbc ≠ 0 ∨ qbc ≠ 0)
-      (hbc : s ((y / x) ^ qbc) ((c / b) ^ pbc)),
+      (hab : s (y ^ qab * a ^ pab) (b ^ pab * x ^ qab)) (pbc qbc : ℕ) (hbcn : pbc ≠ 0 ∨ qbc ≠ 0)
+      (hbc : s (y ^ qbc * b ^ pbc) (c ^ pbc * x ^ qbc)),
       (pab * pbc ≠ 0 ∨ qab * pbc + qbc * pab ≠ 0) ∧
-      s ((y / x) ^ (qab * pbc + qbc * pab)) ((c / a) ^ (pab * pbc)),
+      s (y ^ (qab * pbc + qbc * pab) * a ^ (pab * pbc))
+        (c ^ (pab * pbc) * x ^ (qab * pbc + qbc * pab)),
     { intros a b c pab qab habn hab pbc qbc hbcn hbc,
       have habp : pab ≠ 0 := hp _ _ habn hab,
       have hbcp : pbc ≠ 0 := hp _ _ hbcn hbc,
@@ -198,7 +200,7 @@ begin
         intro hprod,
         rw [nat.mul_eq_zero] at hprod,
         tauto, },
-      rw [← div_mul_div_cancel'' b a c, pow_add, mul_pow],
+      rw [pow_add],
       have : ∀ (l k : α) (n : ℕ), s k l → s (k ^ n) (l ^ n),
       { intros l k n hlk,
         induction n,
@@ -206,22 +208,19 @@ begin
           exact refl _, },
         { simp only [nat.succ_eq_add_one, pow_add, pow_one],
           apply trans (_ : s (k ^ n_n * k) (l ^ n_n * k)),
-          apply hs₁a,
-          assumption,
-          rw [mul_comm _ k, mul_comm _ k],
-          apply hs₁a,
-          assumption, }, },
-      apply trans (_ : s ((y / x) ^ (qab * pbc) * (y / x) ^ (qbc * pab))
-                         ((y / x) ^ (qab * pbc) * (c / b) ^ (pab * pbc))),
-      { rw [mul_comm _ ((c / b) ^ (pab * pbc)), mul_comm _ ((c / b) ^ (pab * pbc))],
-        apply hs₁a,
+          rwa ← hs₁a,
+          rwa [mul_comm _ k, mul_comm _ k, ← hs₁a], }, },
+      apply trans _ (_ : s (y ^ (qbc * pab) * b ^ (pab * pbc) * x ^ (qab * pbc))
+                         (c ^ (pab * pbc) * x ^ (qab * pbc + qbc * pab))),
+      { rw [mul_comm (y ^ (qab * pbc)) (y ^ (qbc * pab)), mul_assoc, mul_assoc, ← hs₁a],
         convert this _ _ pbc hab using 1,
-        { rw [pow_mul], },
-        { rw [pow_mul], }, },
-      { apply hs₁a,
+        { simp [mul_pow, pow_mul], },
+        { simp [mul_pow, pow_mul, mul_comm], }, },
+      { rw [add_comm, pow_add, ← mul_assoc,
+          mul_comm _ (x ^ (qab * _)), mul_comm _ (x ^ (qab * _)), ← hs₁a],
         convert this _ _ pab hbc using 1,
-        { rw [pow_mul], },
-        { rw [pow_mul'], }, }, },
+        { simp [mul_pow, pow_mul' b, pow_mul y], },
+        { simp [mul_pow, pow_mul' c, pow_mul x], }, }, },
     have trans : ∀ (a b c : α), s' a b → s' b c → s' a c,
     { rintro a b c ⟨pab, qab, habn, hab⟩ ⟨pbc, qbc, hbcn, hbc⟩,
       use [pab * pbc, qab * pbc + qbc * pab],
@@ -235,43 +234,38 @@ begin
       have hpabn := hp _ _ habn hab,
       have hqabn := hp _ _ hban hba,
       obtain ⟨keyl, keyr⟩ := key a b a pab qab habn hab pba qba hban hba,
-      simp only [div_self', one_pow] at keyr,
-      have : s 1 ((x / y) ^ (qab * pba + qba * pab)),
-      { simpa [← mul_pow] using hs₁a _ _ keyr ((x / y) ^ (qab * pba + qba * pab)), },
+      have : s (y ^ (qab * pba + qba * pab)) (x ^ (qab * pba + qba * pab)),
+      -- this requires cancellation (strongness)
+      { rwa [mul_comm, ← hs₁a] at keyr, },
       by_cases hqs : qab = 0 ∧ qba = 0,
-      { simp only [hqs, pow_zero] at hab hba,
+      { simp only [hqs, pow_zero, one_mul, mul_one] at hab hba,
         apply antisymm (_ : s a b),
-        { rw ← one_pow pba at hba,
-          have := hs₁b _ _ ⟨_, _, hba⟩,
-          simpa using hs₁a _ _ this b,
+        { refine hs₁b _ _ ⟨_, _, hba⟩,
           tauto, },
-        { rw ← one_pow pab at hab,
-          have := hs₁b _ _ ⟨_, _, hab⟩,
-          simpa using hs₁a _ _ this a,
+        { refine hs₁b _ _ ⟨_, _, hab⟩,
           tauto, }, },
       exfalso,
-      rw ← one_pow (qab * pba + qba * pab) at this,
-      have : s 1 (x / y) := hs₁b _ _ ⟨_, _, this⟩,
-      have := hs₁a _ _ this y,
-      simp only [mul_one, mul_div_cancel'_right] at this,
+      have : s y x := hs₁b _ _ ⟨_, _, this⟩,
       tauto,
       intro hz,
       simp only [add_eq_zero_iff, nat.mul_eq_zero] at hz,
       tauto, },
-    { rintros a b ⟨pab, qab, habn, hab⟩ c, -- homog
-      use [pab, qab, habn],
-      simpa, },
+    { rintros a b c, -- homog
+      refine exists₂_congr _,
+      simp only [exists_prop, and.congr_right_iff],
+      intros pab qab habn,
+      rw [mul_pow, mul_pow, ← mul_assoc, mul_comm _ (c ^ _), mul_assoc, mul_assoc, ← hs₁a], },
     { rintros a b ⟨n, hnn, ⟨pn, qn, han, ha⟩⟩, -- normal
       use [pn * n, qn],
       split,
       { left,
         simp only [hnn, ne.def, nat.mul_eq_zero, or_false],
         exact hp _ _ han ha, },
-      have : (a / b) ^ n = a ^ n / b ^ n,
-      sorry,
-      simpa [pow_mul', this] using ha, }, },
+      simpa [pow_mul'] using ha, }, },
 end
 
+-- TODO this lemma missing for groups?
+-- have : (a / b) ^ n = a ^ n / b ^ n,
 
 def linear_group_extension (α : Type u) : Type u := α
 

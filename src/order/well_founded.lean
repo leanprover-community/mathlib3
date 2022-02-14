3aa22206ee7def004a8bb2fbffb1de5f8b93425b
Violeta Hernández <vi.hdz.p@gmail.com>
2022-02-13 19:39:22 -0600

    fix?

--- src/order/well_founded.lean
+++ src/order/well_founded.lean
@@ -142,7 +142,6 @@ theorem eq_strict_mono_iff_eq_range {f g : β → γ} (hf : strict_mono f)
     (eq_strict_mono_iff_eq_range_aux hg hf hfg.symm (λ a hab, (H a hab).symm))
 end, congr_arg _⟩
 
-include h
 theorem self_le_of_strict_mono {φ : β → β} (hφ : strict_mono φ) : ∀ n, n ≤ φ n :=
 by { by_contra' h₁, have h₂ := h.min_mem _ h₁, exact h.not_lt_min _ h₁ (hφ h₂) h₂ }
 

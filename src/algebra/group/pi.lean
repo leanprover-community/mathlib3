9951eb1ba7fa00fd8fb4e300a9a96d1d7392c267
Alex J Best <alex.j.best@gmail.com>
2022-02-18 16:31:25 +0000

    fixup

--- src/algebra/group/pi.lean
+++ src/algebra/group/pi.lean
@@ -42,7 +42,8 @@ instance monoid [∀ i, monoid $ f i] : monoid (Π i : I, f i) :=
 by refine_struct { one := (1 : Π i, f i), mul := (*), npow := λ n x i, (x i) ^ n };
 tactic.pi_instance_derive_field
 
-@[simp, to_additive pi.nsmul_apply]
+-- the attributes are intentionally out of order. `smul_apply` proves `nsmul_apply`.
+@[to_additive nsmul_apply, simp]
 lemma pow_apply [∀ i, monoid $ f i] (n : ℕ) : (x^n) i = (x i)^n := rfl
 
 @[to_additive]

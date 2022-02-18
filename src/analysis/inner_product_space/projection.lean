b6dc6560280a2434b02b7bd13deb0629606201ba
Hans Parshall <hparshall@users.noreply.github.com>
2022-02-17 19:59:36 -0800

    Update src/analysis/inner_product_space/projection.lean
    Co-authored-by: Frédéric Dupuis <31101893+dupuisf@users.noreply.github.com>

--- src/analysis/inner_product_space/projection.lean
+++ src/analysis/inner_product_space/projection.lean
@@ -827,30 +827,16 @@ lemma norm_sq_eq_add_norm_sq_projection
   (x : E) (S : submodule 𝕜 E) [complete_space E] [complete_space S] :
   ∥x∥^2 = ∥orthogonal_projection S x∥^2 + ∥orthogonal_projection Sᗮ x∥^2 :=
 begin
-  let p1 := (orthogonal_projection S),
-  let p2 := (orthogonal_projection Sᗮ),
-  have x_decomp : x = (p1 x) + (p2 x) :=
+  let p1 := orthogonal_projection S,
+  let p2 := orthogonal_projection Sᗮ,
+  have x_decomp : x = p1 x + p2 x :=
     eq_sum_orthogonal_projection_self_orthogonal_complement S x,
   have x_orth : ⟪ p1 x, p2 x ⟫ = 0 :=
-  begin
-    have p1x : ↑(p1 x) ∈ S := set_like.coe_mem (p1 x),
-    have p2x : ↑(p2 x) ∈ Sᗮ := set_like.coe_mem (p2 x),
-    apply submodule.inner_right_of_mem_orthogonal p1x p2x,
-  end,
-  conv
-  begin
-    to_lhs,
-    rw x_decomp,
-  end,
-  simp only [sq],
-  rw norm_add_sq_eq_norm_sq_add_norm_sq_of_inner_eq_zero ((p1 x) : E) (p2 x) x_orth,
-  simp only [add_left_inj,
-    mul_eq_mul_left_iff,
-    norm_eq_zero,
-    true_or,
-    eq_self_iff_true,
-    submodule.coe_norm,
-    submodule.coe_eq_zero],
+    submodule.inner_right_of_mem_orthogonal (set_like.coe_mem (p1 x)) (set_like.coe_mem (p2 x)),
+  nth_rewrite 0 [x_decomp],
+  simp only [sq, norm_add_sq_eq_norm_sq_add_norm_sq_of_inner_eq_zero ((p1 x) : E) (p2 x) x_orth,
+             add_left_inj, mul_eq_mul_left_iff, norm_eq_zero, true_or, eq_self_iff_true,
+             submodule.coe_norm, submodule.coe_eq_zero]
 end
 
 /-- In a complete space `E`, the projection maps onto a complete subspace `K` and its orthogonal

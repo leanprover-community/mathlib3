e42b7d075a00117edfaf9c9d4272e3aabbdd47a9
Violeta Hernández <vi.hdz.p@gmail.com>
2022-02-18 23:56:03 -0600

    Tweak `sup_eq_Sup`

--- src/set_theory/ordinal_arithmetic.lean
+++ src/set_theory/ordinal_arithmetic.lean
@@ -1012,10 +1012,10 @@ theorem bdd_above_iff_small {s : set ordinal.{u}} : bdd_above s ↔ small.{u} s 
 ⟨λ ⟨a, h⟩, @small_subset _ _ _ (by exact λ b hb, lt_succ.2 (h hb)) (small_Iio a.succ),
 λ h, ⟨sup.{u u} (λ x, ((@equiv_shrink s h).symm x).val), le_sup_shrink_equiv h⟩⟩
 
-@[simp] theorem Sup_eq_sup {s : set ordinal.{u}} (hs : small.{u} s) (hne : s.nonempty) :
-  Sup s = sup.{u u} (λ x, ((@equiv_shrink s hs).symm x).val) :=
+@[simp] theorem sup_eq_Sup {s : set ordinal.{u}} (hs : small.{u} s) :
+  sup.{u u} (λ x, ((@equiv_shrink s hs).symm x).val) = Sup s :=
 let hs' := bdd_above_iff_small.2 hs in
-  ((cSup_le_iff hs' hne).2 (le_sup_shrink_equiv hs)).antisymm
+  ((cSup_le_iff' hs').2 (le_sup_shrink_equiv hs)).antisymm'
   (sup_le.2 (λ x, le_cSup hs' (subtype.mem _)))
 
 private theorem sup_le_sup {ι ι' : Type u} (r : ι → ι → Prop) (r' : ι' → ι' → Prop)

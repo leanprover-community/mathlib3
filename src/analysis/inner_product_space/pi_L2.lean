dd0814a5bf2bb8ed3e80f5bfe1deaefce869a91d
Hans Parshall <hparshall@gmail.com>
2022-02-21 13:44:36 -0800

    too much indent

--- src/analysis/inner_product_space/pi_L2.lean
+++ src/analysis/inner_product_space/pi_L2.lean
@@ -395,55 +395,55 @@ open finite_dimensional
 /-- Let `S` be a subspace of a finite-dimensional complex inner product space `V`.  A linear
 isometry mapping `S` into `V` can be extended to a full isometry of `V`. -/
 noncomputable def linear_isometry.extend (L : S →ₗᵢ[ℂ] V): V →ₗᵢ[ℂ] V :=
-  begin
-    -- Build an isometry from Sᗮ to L(S)ᗮ through euclidean_space
-    let d := finrank ℂ Sᗮ,
-    have dim_S : finrank ℂ Sᗮ = d := rfl,
-    let LS := L.to_linear_map.range,
-    have dim_LS_perp : finrank ℂ LSᗮ = d,
-    calc  finrank ℂ LSᗮ = finrank ℂ V - finrank ℂ LS : by simp only
-        [← LS.finrank_add_finrank_orthogonal, add_tsub_cancel_left]
-      ...               = finrank ℂ V - finrank ℂ S : by simp only
-        [linear_map.finrank_range_of_inj L.injective]
-      ...               = finrank ℂ Sᗮ : by simp only
-        [← S.finrank_add_finrank_orthogonal, add_tsub_cancel_left]
-      ...               = d : dim_S,
-    let L1 := ((fin_std_orthonormal_basis dim_S).to_orthonormal_basis
-      (fin_std_orthonormal_basis_orthonormal dim_S)).repr.to_linear_isometry,
-    let L2 := ((fin_std_orthonormal_basis dim_LS_perp).to_orthonormal_basis
-      (fin_std_orthonormal_basis_orthonormal dim_LS_perp)).repr.symm.to_linear_isometry,
-    let L3 := (LS)ᗮ.subtypeₗᵢ.comp (L2.comp L1),
-    -- Project onto S and Sᗮ
-    let p1 := (orthogonal_projection S).to_linear_map,
-    let p2 := (orthogonal_projection Sᗮ).to_linear_map,
-    -- Build a linear map from the isometries on S and Sᗮ
-    let M := L.to_linear_map.comp p1 + L3.to_linear_map.comp p2,
-    -- Prove that M is an isometry
-    have M_norm_map : ∀ (x : V), ∥M x∥ = ∥x∥,
-    { intro x,
-      -- Apply M to the orthogonal decomposition of x
-      have Mx_decomp : M x = L (p1 x) + L3 (p2 x),
-      { simp only [linear_map.add_apply, linear_map.comp_apply, linear_map.comp_apply,
-        linear_isometry.coe_to_linear_map]},
-      -- Mx_decomp is the orthogonal decomposition of M x
-      have Mx_orth : ⟪ L (p1 x), L3 (p2 x) ⟫_ℂ = 0,
-      { have Lp1x : L (p1 x) ∈ L.to_linear_map.range := L.to_linear_map.mem_range_self (p1 x),
-        have Lp2x : L3 (p2 x) ∈ (L.to_linear_map.range)ᗮ,
-        { simp only [L3, linear_isometry.coe_comp, function.comp_app, submodule.coe_subtypeₗᵢ,
-            ← submodule.range_subtype (LSᗮ)],
-          apply linear_map.mem_range_self},
-        apply submodule.inner_right_of_mem_orthogonal Lp1x Lp2x},
-      -- Apply the Pythagorean theorem and simplify
-      rw ← sq_eq_sq (norm_nonneg _) (norm_nonneg _),
-      rw norm_sq_eq_add_norm_sq_projection x S,
-      simp only [sq, Mx_decomp],
-      rw norm_add_sq_eq_norm_sq_add_norm_sq_of_inner_eq_zero (L (p1 x)) (L3 (p2 x)) Mx_orth,
-      simp only [linear_isometry.norm_map, p1, p2, continuous_linear_map.to_linear_map_eq_coe,
-        add_left_inj, mul_eq_mul_left_iff, norm_eq_zero, true_or, eq_self_iff_true,
-        continuous_linear_map.coe_coe, submodule.coe_norm, submodule.coe_eq_zero]},
-    exact {to_linear_map := M,
-      norm_map' := M_norm_map},
-  end
+begin
+  -- Build an isometry from Sᗮ to L(S)ᗮ through euclidean_space
+  let d := finrank ℂ Sᗮ,
+  have dim_S : finrank ℂ Sᗮ = d := rfl,
+  let LS := L.to_linear_map.range,
+  have dim_LS_perp : finrank ℂ LSᗮ = d,
+  calc  finrank ℂ LSᗮ = finrank ℂ V - finrank ℂ LS : by simp only
+      [← LS.finrank_add_finrank_orthogonal, add_tsub_cancel_left]
+    ...               = finrank ℂ V - finrank ℂ S : by simp only
+      [linear_map.finrank_range_of_inj L.injective]
+    ...               = finrank ℂ Sᗮ : by simp only
+      [← S.finrank_add_finrank_orthogonal, add_tsub_cancel_left]
+    ...               = d : dim_S,
+  let L1 := ((fin_std_orthonormal_basis dim_S).to_orthonormal_basis
+    (fin_std_orthonormal_basis_orthonormal dim_S)).repr.to_linear_isometry,
+  let L2 := ((fin_std_orthonormal_basis dim_LS_perp).to_orthonormal_basis
+    (fin_std_orthonormal_basis_orthonormal dim_LS_perp)).repr.symm.to_linear_isometry,
+  let L3 := (LS)ᗮ.subtypeₗᵢ.comp (L2.comp L1),
+  -- Project onto S and Sᗮ
+  let p1 := (orthogonal_projection S).to_linear_map,
+  let p2 := (orthogonal_projection Sᗮ).to_linear_map,
+  -- Build a linear map from the isometries on S and Sᗮ
+  let M := L.to_linear_map.comp p1 + L3.to_linear_map.comp p2,
+  -- Prove that M is an isometry
+  have M_norm_map : ∀ (x : V), ∥M x∥ = ∥x∥,
+  { intro x,
+    -- Apply M to the orthogonal decomposition of x
+    have Mx_decomp : M x = L (p1 x) + L3 (p2 x),
+    { simp only [linear_map.add_apply, linear_map.comp_apply, linear_map.comp_apply,
+      linear_isometry.coe_to_linear_map]},
+    -- Mx_decomp is the orthogonal decomposition of M x
+    have Mx_orth : ⟪ L (p1 x), L3 (p2 x) ⟫_ℂ = 0,
+    { have Lp1x : L (p1 x) ∈ L.to_linear_map.range := L.to_linear_map.mem_range_self (p1 x),
+      have Lp2x : L3 (p2 x) ∈ (L.to_linear_map.range)ᗮ,
+      { simp only [L3, linear_isometry.coe_comp, function.comp_app, submodule.coe_subtypeₗᵢ,
+          ← submodule.range_subtype (LSᗮ)],
+        apply linear_map.mem_range_self},
+      apply submodule.inner_right_of_mem_orthogonal Lp1x Lp2x},
+    -- Apply the Pythagorean theorem and simplify
+    rw ← sq_eq_sq (norm_nonneg _) (norm_nonneg _),
+    rw norm_sq_eq_add_norm_sq_projection x S,
+    simp only [sq, Mx_decomp],
+    rw norm_add_sq_eq_norm_sq_add_norm_sq_of_inner_eq_zero (L (p1 x)) (L3 (p2 x)) Mx_orth,
+    simp only [linear_isometry.norm_map, p1, p2, continuous_linear_map.to_linear_map_eq_coe,
+      add_left_inj, mul_eq_mul_left_iff, norm_eq_zero, true_or, eq_self_iff_true,
+      continuous_linear_map.coe_coe, submodule.coe_norm, submodule.coe_eq_zero]},
+  exact {to_linear_map := M,
+    norm_map' := M_norm_map},
+end
 
 lemma linear_isometry.extend_apply (L : S →ₗᵢ[ℂ] V) (s : S):
   L.extend s = L s :=

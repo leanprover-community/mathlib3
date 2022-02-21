f85d77664b48087cee26a1bb2d86e54a4b09bb34
Eric Wieser <wieser.eric@gmail.com>
2022-02-21 18:05:34 +0000

    fix(topology/continuous_function/bounded): is_central_scalar

--- src/topology/continuous_function/bounded.lean
+++ src/topology/continuous_function/bounded.lean
@@ -815,6 +815,9 @@ instance : has_scalar 𝕜 (α →ᵇ β) :=
 @[simp] lemma coe_smul (c : 𝕜) (f : α →ᵇ β) : ⇑(c • f) = λ x, c • (f x) := rfl
 lemma smul_apply (c : 𝕜) (f : α →ᵇ β) (x : α) : (c • f) x = c • f x := rfl
 
+instance [has_scalar 𝕜ᵐᵒᵖ β] [is_central_scalar 𝕜 β]: is_central_scalar 𝕜 (α →ᵇ β) :=
+{ op_smul_eq_smul := λ _ _, ext $ λ _, op_smul_eq_smul _ _ }
+
 instance : has_bounded_smul 𝕜 (α →ᵇ β) :=
 { dist_smul_pair' := λ c f₁ f₂, begin
     rw dist_le (mul_nonneg dist_nonneg dist_nonneg),

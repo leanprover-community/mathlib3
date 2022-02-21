d75a34ea02931a098fd38f17eeee58530b579105
Rob Lewis <rob.y.lewis@gmail.com>
2022-02-21 12:09:22 -0500

    localized notation and renaming

--- src/ring_theory/witt_vector/isocrystal.lean
+++ src/ring_theory/witt_vector/isocrystal.lean
@@ -16,6 +16,10 @@ https://www.math.ias.edu/~lurie/205notes/Lecture26-Isocrystals.pdf
 This construction is described in Dupuis, Lewis, and Macbeth,
 [Formalized functional analysis via semilinear maps][dupuis-lewis-macbeth2022].
 
+## Notation
+
+This file introduces notation in the locale `isocrystal`.
+
 -/
 
 noncomputable theory
@@ -27,9 +31,11 @@ open finite_dimensional
   linear_equiv.smul_of_ne_zero K M a ha x = a • x :=
 rfl
 
+namespace witt_vector
+
 variables (p : ℕ) [fact p.prime]
 variables (k : Type*) [comm_ring k]
-notation `K(` p`,` k`)` := fraction_ring (witt_vector p k)
+localized "notation `K(` p`,` k`)` := fraction_ring (witt_vector p k)" in isocrystal
 
 section perfect_ring
 variables [is_domain k] [char_p k p] [perfect_ring k p]
@@ -37,22 +43,23 @@ variables [is_domain k] [char_p k p] [perfect_ring k p]
 /-! ### Frobenius-linear maps -/
 
 /-- The Frobenius automorphism of `k` induces an automorphism of `K`. -/
-def foo₀ : K(p, k) ≃+* K(p, k) := is_fraction_ring.field_equiv_of_ring_equiv
+def fraction_ring.frobenius : K(p, k) ≃+* K(p, k) := is_fraction_ring.field_equiv_of_ring_equiv
   (ring_equiv.of_bijective _ (witt_vector.frobenius_bijective p k))
 
 /-- The Frobenius automorphism of `k` induces an endomorphism of `K`. For notation purposes. -/
-def foo : K(p, k) →+* K(p, k) := foo₀ p k
+def fraction_ring.frobenius_ring_hom : K(p, k) →+* K(p, k) := fraction_ring.frobenius p k
 
-notation `φ(` p`,` k`)` := foo₀ p k
+localized "notation `φ(` p`,` k`)` := witt_vector.fraction_ring.frobenius_ring_hom p k" in isocrystal
 
-instance inv_pair₁ : ring_hom_inv_pair (foo p k) _ :=
-ring_hom_inv_pair.of_ring_equiv (foo₀ p k)
+instance inv_pair₁ : ring_hom_inv_pair (φ(p, k)) _ :=
+ring_hom_inv_pair.of_ring_equiv (fraction_ring.frobenius p k)
 
-instance inv_pair₂ : ring_hom_inv_pair ((foo₀ p k).symm : K(p, k) →+* K(p, k)) _ :=
-ring_hom_inv_pair.of_ring_equiv (foo₀ p k).symm
+instance inv_pair₂ :
+  ring_hom_inv_pair ((fraction_ring.frobenius p k).symm : K(p, k) →+* K(p, k)) _ :=
+ring_hom_inv_pair.of_ring_equiv (fraction_ring.frobenius p k).symm
 
-notation M ` →ᶠˡ[`:50 p `,` k `] ` M₂ := linear_map (foo p k) M M₂
-notation M ` ≃ᶠˡ[`:50 p `,` k `] ` M₂ := linear_equiv (foo p k) M M₂
+localized "notation M ` →ᶠˡ[`:50 p `,` k `] ` M₂ := linear_map (witt_vector.fraction_ring.frobenius_ring_hom p k) M M₂" in isocrystal
+localized "notation M ` ≃ᶠˡ[`:50 p `,` k `] ` M₂ := linear_equiv (witt_vector.fraction_ring.frobenius_ring_hom p k) M M₂" in isocrystal
 
 /-! ### Isocrystals -/
 
@@ -71,10 +78,10 @@ variables {V}
 /--
 Project the Frobenius automorphism from an isocrystal. Denoted by `Φ(p, k)` when V can be inferred.
 -/
-def isocrystal_frob' : V ≃ᶠˡ[p, k] V := @isocrystal.frob p _ k _ _ _ _ _ _ _
+def isocrystal.frobenius : V ≃ᶠˡ[p, k] V := @isocrystal.frob p _ k _ _ _ _ _ _ _
 variables (V)
 
-notation `Φ(` p`,` k`)` := isocrystal_frob' p k
+localized "notation `Φ(` p`,` k`)` := isocrystal.frobenius p k" in isocrystal
 
 /-- A homomorphism between isocrystals respects the Frobenius map. -/
 @[nolint has_inhabited_instance]
@@ -86,17 +93,19 @@ structure isocrystal_hom extends V →ₗ[K(p, k)] V₂ :=
 structure isocrystal_equiv extends V ≃ₗ[K(p, k)] V₂ :=
 ( frob_equivariant : ∀ x : V, Φ(p, k) (to_linear_equiv x) = to_linear_equiv (Φ(p, k) x) )
 
-notation M ` →ᶠⁱ[`:50 p `,` k `] ` M₂ := isocrystal_hom p k M M₂
-notation M ` ≃ᶠⁱ[`:50 p `,` k `] ` M₂ := isocrystal_equiv p k M M₂
+localized "notation M ` →ᶠⁱ[`:50 p `,` k `] ` M₂ := isocrystal_hom p k M M₂" in isocrystal
+localized "notation M ` ≃ᶠⁱ[`:50 p `,` k `] ` M₂ := isocrystal_equiv p k M M₂" in isocrystal
 
 
 end perfect_ring
+
+open_locale isocrystal
 
 /-! ### Classification of isocrystals in dimension 1 -/
 
 /-- A helper instance for type class inference. -/
 local attribute [instance]
-def foo₀_module : module K(p, k) K(p, k) := semiring.to_module
+def fraction_ring.module : module K(p, k) K(p, k) := semiring.to_module
 
 /--
 Type synonym for `K(p, k)` to carry the standard `m`-indexed 1-dimensional isocrystal structure.
@@ -110,17 +119,17 @@ variables [is_domain k] [char_p k p] [perfect_ring k p]
 
 /-- The standard one-dimensional isocrystal is an isocrystal. -/
 instance (m : ℤ) : isocrystal p k (standard_one_dim_isocrystal p k m) :=
-{ frob := (foo₀ p k).to_semilinear_equiv.trans
+{ frob := (fraction_ring.frobenius p k).to_semilinear_equiv.trans
    (linear_equiv.smul_of_ne_zero _ _ _ (zpow_ne_zero m (witt_vector.fraction_ring.p_nonzero p k))) }
 
-@[simp] lemma frobenius_standard_one_dim_isocrystal_apply (m : ℤ)
+@[simp] lemma standard_one_dim_isocrystal.frobenius_apply (m : ℤ)
   (x : standard_one_dim_isocrystal p k m) :
   Φ(p, k) x = (p:K(p, k)) ^ m • φ(p, k) x :=
 rfl
 
 /-- A one-dimensional isocrystal admits an isomorphism to one of the standard (indexed by `m : ℤ`)
 one-dimensional isocrystals. -/
-lemma classification
+theorem isocrystal_classification
   (k : Type*) [field k] [is_alg_closed k] [char_p k p]
   (V : Type*) [add_comm_group V] [isocrystal p k V]
   (h_dim : finrank K(p, k) V = 1) :
@@ -150,15 +159,17 @@ begin
       rw linear_map.span_singleton_eq_range } },
   refine ⟨⟨(linear_equiv.smul_of_ne_zero K(p, k) _ _ hb).trans F, _⟩⟩,
   intros c,
-  simp only [foo, hax, ←mul_smul, linear_equiv.trans_apply, linear_equiv.smul_of_ne_zero_apply,
+  simp only [hax, ←mul_smul, linear_equiv.trans_apply, linear_equiv.smul_of_ne_zero_apply,
     algebra.id.smul_eq_mul, linear_equiv.of_bijective_apply, linear_map.to_span_singleton_apply,
-    linear_equiv.map_smulₛₗ, ring_equiv.coe_to_ring_hom, ring_equiv.map_mul,
-    frobenius_standard_one_dim_isocrystal_apply],
+    linear_equiv.map_smulₛₗ, ring_equiv.coe_to_ring_hom, ring_equiv.map_mul, ring_hom.map_mul,
+    standard_one_dim_isocrystal.frobenius_apply],
   congr' 1,
   transitivity (φ(p,k) b * a) * φ(p,k) c,
-  { ring },
+  { ring, },
   { rw hmb,
     ring },
 end
 
 end perfect_ring
+
+end witt_vector

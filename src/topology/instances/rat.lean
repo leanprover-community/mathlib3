d4dd40aca3e480f8496b47b14d5d6cc0b74755cd
Eric Wieser <wieser.eric@gmail.com>
2022-02-22 12:14:15 +0000

    fix

--- src/topology/instances/rat.lean
+++ src/topology/instances/rat.lean
@@ -1,32 +1,19 @@
 /-
-Copyright (c) 2022 Yury Kudryashov. All rights reserved.
+Copyright (c) 2017 Johannes Hölzl. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
-Authors: Yury Kudryashov
+Authors: Johannes Hölzl, Mario Carneiro
 -/
-import topology.instances.irrational
-import topology.alexandroff
-
+import topology.metric_space.basic
+import topology.instances.int
+import topology.instances.nat
+import topology.instances.real
 /-!
-# Topology on rational numbers
+# Topology on the ratonal numbers
 
-The structure of a metric space on `ℚ` is introduced in this file, induced from `ℝ`. We then
-prove some properties of this topological space and its one-point compactification.
-
-## Main statements
-
-- `rat.totally_disconnected_space`: `ℚ` is a totally disconnected space;
-
-- `rat.not_countably_generated_nhds_infty_alexandroff`: the filter of neighbourhoods of infinity in
-  `alexandroff ℚ` is not countably generated.
-
-## Notation
-
-- `ℚ∞` is used as a local notation for `alexandroff ℚ`
+The structure of a metric space on `ℚ` is introduced in this file, induced from `ℝ`.
 -/
-
-open set metric filter topological_space
-open_locale topological_space alexandroff
-local notation `ℚ∞` := alexandroff ℚ
+noncomputable theory
+open metric set filter
 
 namespace rat
 
@@ -65,7 +52,6 @@ uniform_embedding_bot_of_pairwise_le_dist zero_lt_one $ by simpa using nat.pairw
 lemma nat.closed_embedding_coe_rat : closed_embedding (coe : ℕ → ℚ) :=
 closed_embedding_of_pairwise_le_dist zero_lt_one $ by simpa using nat.pairwise_one_le_dist
 
-
 @[norm_cast, simp] theorem int.dist_cast_rat (x y : ℤ) : dist (x : ℚ) y = dist x y :=
 by rw [← int.dist_cast_real, ← rat.dist_cast]; congr' 1; norm_cast
 
@@ -76,7 +62,6 @@ lemma int.closed_embedding_coe_rat : closed_embedding (coe : ℤ → ℚ) :=
 closed_embedding_of_pairwise_le_dist zero_lt_one $ by simpa using int.pairwise_one_le_dist
 
 instance : noncompact_space ℚ := int.closed_embedding_coe_rat.noncompact_space
-
 
 -- TODO(Mario): Find a way to use rat_add_continuous_lemma
 theorem rat.uniform_continuous_add : uniform_continuous (λp : ℚ × ℚ, p.1 + p.2) :=
@@ -113,62 +98,3 @@ begin
   have := totally_bounded_preimage rat.uniform_embedding_coe_real (totally_bounded_Icc a b),
   rwa (set.ext (λ q, _) : Icc _ _ = _), simp
 end
-
-namespace rat
-
-variables {p q : ℚ} {s t : set ℚ}
-
-lemma interior_compact_eq_empty (hs : is_compact s) :
-  interior s = ∅ :=
-dense_embedding_coe_real.to_dense_inducing.interior_compact_eq_empty dense_irrational hs
-
-lemma dense_compl_compact (hs : is_compact s) : dense sᶜ :=
-interior_eq_empty_iff_dense_compl.1 (interior_compact_eq_empty hs)
-
-instance cocompact_inf_nhds_ne_bot : ne_bot (cocompact ℚ ⊓ 𝓝 p) :=
-begin
-  refine (has_basis_cocompact.inf (nhds_basis_opens _)).ne_bot_iff.2 _,
-  rintro ⟨s, o⟩ ⟨hs, hpo, ho⟩, rw inter_comm,
-  exact (dense_compl_compact hs).inter_open_nonempty _ ho ⟨p, hpo⟩
-end
-
-lemma not_countably_generated_cocompact : ¬is_countably_generated (cocompact ℚ) :=
-begin
-  introI H,
-  rcases exists_seq_tendsto (cocompact ℚ ⊓ 𝓝 0) with ⟨x, hx⟩,
-  rw tendsto_inf at hx, rcases hx with ⟨hxc, hx0⟩,
-  obtain ⟨n, hn⟩ : ∃ n : ℕ, x n ∉ insert (0 : ℚ) (range x),
-    from (hxc.eventually hx0.is_compact_insert_range.compl_mem_cocompact).exists,
-  exact hn (or.inr ⟨n, rfl⟩)
-end
-
-lemma not_countably_generated_nhds_infty_alexandroff :
-  ¬is_countably_generated (𝓝 (∞ : ℚ∞)) :=
-begin
-  introI,
-  have : is_countably_generated (comap (coe : ℚ → ℚ∞) (𝓝 ∞)), by apply_instance,
-  rw [alexandroff.comap_coe_nhds_infty, coclosed_compact_eq_cocompact] at this,
-  exact not_countably_generated_cocompact this
-end
-
-lemma not_first_countable_topology_alexandroff :
-  ¬first_countable_topology ℚ∞ :=
-by { introI, exact not_countably_generated_nhds_infty_alexandroff infer_instance }
-
-lemma not_second_countable_topology_alexandroff :
-  ¬second_countable_topology ℚ∞ :=
-by { introI, exact not_first_countable_topology_alexandroff infer_instance }
-
-instance : totally_disconnected_space ℚ :=
-begin
-  refine ⟨λ s hsu hs x hx y hy, _⟩, clear hsu,
-  by_contra' H : x ≠ y,
-  wlog hlt : x < y := H.lt_or_lt using [x y, y x],
-  rcases exists_irrational_btwn (rat.cast_lt.2 hlt) with ⟨z, hz, hxz, hzy⟩,
-  have := hs.image coe continuous_coe_real.continuous_on,
-  rw [is_preconnected_iff_ord_connected] at this,
-  have : z ∈ coe '' s := this.out (mem_image_of_mem _ hx) (mem_image_of_mem _ hy) ⟨hxz.le, hzy.le⟩,
-  exact hz (image_subset_range _ _ this)
-end
-
-end rat

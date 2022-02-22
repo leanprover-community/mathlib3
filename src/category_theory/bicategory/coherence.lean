249126d8bc62947c8d0e5a17a790ccca73aa912e
Junyan Xu <junyanxumath@gmail.com>
2022-02-22 00:12:31 -0500

    minimalistic proof

--- src/category_theory/bicategory/coherence.lean
+++ src/category_theory/bicategory/coherence.lean
@@ -111,6 +111,7 @@ begin
   all_goals { funext, refl }
 end
 
+/- should be moved to eq_to_hom -/
 lemma family_congr {ι C : Type*} [category C] {o₁ o₂ : ι → C} (m : ∀ i, o₁ i ⟶ o₂ i)
   {i j : ι} (h : i = j) : m i = eq_to_hom (by rw h) ≫ m j ≫ eq_to_hom (by rw h) :=
 by { subst h, apply eq_conj_eq_to_hom }
@@ -123,11 +124,11 @@ begin
   case id : { simp },
   case vcomp : _ _ _ _ _ _ _ ihf ihg
   { rw [mk_vcomp, bicategory.whisker_left_comp],
-    slice_lhs 2 3 {rw ihg}, slice_lhs 1 2 {rw ihf}, simp },
+    slice_lhs 2 3 { rw ihg }, slice_lhs 1 2 { rw ihf }, simp },
   case whisker_left : _ _ _ _ _ _ _ ih
   { dsimp, slice_lhs 1 2 { rw associator_inv_naturality_right },
     slice_lhs 2 3 { rw whisker_exchange },
-    slice_lhs 3 4 { erw ih }, simpa only [assoc] },
+    slice_lhs 3 4 { erw ih }, /- p ≠ nil required! -/ simpa only [assoc] },
   case whisker_right : _ _ _ _ _ h η ih
   { dsimp, slice_lhs 1 2 { rw associator_inv_naturality_middle },
     slice_lhs 2 3 { erw [←bicategory.whisker_right_comp, ih, bicategory.whisker_right_comp] },
@@ -143,11 +144,10 @@ begin
     slice_rhs 2 3 { erw associator_inv_naturality_left },
     slice_rhs 1 2 { erw ←pentagon_inv },
     simpa only [assoc, bicategory.whisker_right_comp] },
-  case left_unitor
-  { erw comp_id, dsimp, slice_rhs 1 2 { erw triangle_assoc_comp_right } },
+  case left_unitor { erw comp_id, symmetry, apply triangle_assoc_comp_right_assoc },
   case left_unitor_inv
   { dsimp, slice_lhs 1 2 { erw triangle_assoc_comp_left_inv },
-    erw [inv_hom_whisker_right, id_comp, comp_id] },
+    rw [inv_hom_whisker_right, id_comp, comp_id] },
   case right_unitor
   { erw [comp_id, whisker_left_right_unitor, assoc, ←right_unitor_naturality], refl },
   case right_unitor_inv
@@ -177,139 +177,12 @@ def normalize_unit_iso_aux {a b : free_bicategory B} (f : a ⟶ b) :
   f ≅ ((full_normalize B).map_functor a b ⋙ inclusion_path a b).obj f :=
 (λ_ _).symm ≪≫ normalize_iso nil f
 
-def normalize_unit_iso {a b : free_bicategory B} :
-  𝟭 (a ⟶ b) ≅ (full_normalize B).map_functor a b ⋙ inclusion_path a b :=
-nat_iso.of_components normalize_unit_iso_aux
-begin
-  rintros f g ⟨η⟩, dsimp, induction η,
-  case id : { simp },
-  case vcomp : _ _ _ _ _ _ _ ihf ihg
-  { rw mk_vcomp, slice_lhs 2 3 {rw ihg}, slice_lhs 1 2 {rw ihf}, simp },
-
-  case right_unitor
-  { dsimp [normalize_unit_iso_aux], erw [comp_id, whisker_left_right_unitor, assoc, ←right_unitor_naturality],
-    refl },
-  case whisker_right : a b c f g h η ih
-  { dsimp [normalize_unit_iso_aux],
-  },
-    case whisker_left : a b c f g h η ih
-  { dsimp [normalize_unit_iso_aux] ,
-  },
-end
-
-  --f ≅ ((full_normalize B).map_functor a b ⋙ inclusion_path a b).obj f :=
-
-
-/--
-Given a 1-morphism `f : hom b c` in the free bicategory and a path `p : path a b`, taking the
-composition of `p` and `f` in the free bicategory is functorial in both `f` and `p`.
--/
-def whisker_path (a b c : B) : hom b c ⥤ discrete (path.{v+1} a b) ⥤ hom a c :=
-{ obj := λ f, discrete.functor (λ p, (preinclusion B).map p ≫ f),
-  map := λ f g η, discrete.nat_trans (λ p, (preinclusion B).map p ◁ η) }
-
-lemma whisker_path_obj_map
-  (a : B) {b c : B} (f : hom b c) {p p' : discrete (path.{v+1} a b)} (η : p ⟶ p') :
-  ((whisker_path a b c).obj f).map η = (inclusion_path a b).map η ▷ f :=
-by tidy
-
-/--
-Auxiliary definition for `normalize_iso`. Here we construct the isomorphism between
-`inclusion_path_aux p ≫ f` and `inclusion_path_aux (normalize_hom f p)`.
--/
-@[simp]
-def normalize_iso_app {a : B} : Π {b c : B} (f : hom b c) (p : path a b),
-  ((whisker_path a b c).obj f).obj p ≅ ((normalize' a b c).obj f).obj p
-| _ _ (hom.of f) p := iso.refl _
-| _ _ (hom.id a) p := ρ_ ((preinclusion B).map p)
-| _ _ (hom.comp f g) p :=
-    (α_ _ _ _).symm ≪≫ whisker_right_iso (normalize_iso_app f p) g ≪≫
-      normalize_iso_app g (((normalize a _ _).obj f).obj p)
-
-/-- Auxiliary definition for `normalize_iso`. -/
-@[simp]
-def normalize_iso_aux (a : B) {b c : B} (f : hom b c) :
-  (whisker_path a b c).obj f ≅ (normalize' a b c).obj f :=
-nat_iso.of_components (normalize_iso_app f) (by tidy)
-
-/--
-The isomorphism between `inclusion_path_aux p ≫ f` and `inclusion_path_aux (normalize_hom f p)`
-is natural (in both `p` and `f`, but naturality in `p` is trivial and was "proved" in
-`normalize_iso_aux`). This is the real heart of our proof of the coherence theorem.
--/
-def normalize_iso (a b c : B) : whisker_path a b c ≅ normalize' a b c :=
-nat_iso.of_components (normalize_iso_aux a)
-begin
-  rintros f g ⟨η⟩,
-  ext p,
-  dsimp only [whisker_path, normalize_iso_aux, nat_trans.comp_app, discrete.nat_trans_app,
-    discrete.functor, nat_iso.of_components.hom_app],
-  induction η,
-  case id { erw [comp_id, bicategory.whisker_left_id, id_comp] },
-  case vcomp : _ _ _ _ _ _ _ ihf ihg
-  { simp only [mk_vcomp, bicategory.whisker_left_comp],
-    slice_lhs 2 3 { rw ihg },
-    slice_lhs 1 2 { rw ihf },
-    rw (normalize' _ _ _).map_comp,
-    simpa only [assoc] },
-  case whisker_left : _ _ _ _ _ _ _ ih
-  { dsimp only [mk_whisker_left, normalize_iso_app, iso.trans_hom],
-    slice_lhs 1 2 { erw associator_inv_naturality_right },
-    slice_lhs 2 3 { erw whisker_exchange },
-    slice_lhs 3 4 { erw ih },
-    simpa only [assoc] },
-  case whisker_right  : _ _ _ _ _ _ _ ih
-  { dsimp only [mk_whisker_right, normalize_iso_app, iso.trans_hom],
-    slice_lhs 1 2 { erw associator_inv_naturality_middle },
-    slice_lhs 2 3 { erw [←bicategory.whisker_right_comp, ih, bicategory.whisker_right_comp,
-      ←whisker_path_obj_map] },
-    slice_lhs 3 4 { erw (normalize_iso_aux _ _).hom.naturality ((normalize_map_aux _).app p) },
-    simpa only [assoc] },
-  case associator
-  { dsimp only [mk_associator_hom, normalize_iso_app, iso.trans_hom, whisker_right_iso_hom],
-    erw comp_id,
-    slice_lhs 3 4 { erw associator_inv_naturality_left },
-    slice_lhs 1 3 { erw pentagon_hom_inv_inv_inv_inv },
-    simpa only [assoc, bicategory.whisker_right_comp] },
-  case associator_inv
-  { dsimp only [mk_associator_inv, normalize_iso_app, iso.trans_hom, whisker_right_iso_hom],
-    erw comp_id,
-    slice_rhs 2 3 { erw associator_inv_naturality_left },
-    slice_rhs 1 2 { erw ←pentagon_inv },
-    simpa only [assoc, bicategory.whisker_right_comp] },
-  case left_unitor
-  { dsimp only [normalize_iso_app, mk_left_unitor_hom, iso.trans_hom, whisker_right_iso_hom],
-    slice_rhs 1 2 { erw triangle_assoc_comp_right },
-    erw comp_id,
-    refl },
-  case left_unitor_inv
-  { dsimp only [normalize_iso_app, mk_left_unitor_inv, iso.trans_hom, whisker_right_iso_hom],
-    slice_lhs 1 2 { erw triangle_assoc_comp_left_inv },
-    erw [inv_hom_whisker_right, id_comp, comp_id],
-    refl },
-  case right_unitor
-  { erw [comp_id, whisker_left_right_unitor, assoc, ←right_unitor_naturality],
-    refl },
-  case right_unitor_inv
-  { erw [comp_id, whisker_left_right_unitor_inv, assoc, iso.hom_inv_id_assoc,
-      right_unitor_conjugation] }
-end
-
-/-- Auxiliary definition for `normalize_unit_iso`. -/
-@[simp]
-def normalize_unit_iso_app_aux {a b : free_bicategory B} (f : a ⟶ b) :
-  (preinclusion B).map (𝟙 a) ≫ f ≅
-    (preinclusion B).map (((full_normalize B).map_functor a b).obj f) :=
-((normalize_iso _ _ _).app f).app nil
-
-/-- Auxiliary definition for `normalize_equiv`. -/
 def normalize_unit_iso (a b : free_bicategory B) :
-  𝟭 (a ⟶ b) ≅ (full_normalize B).map_functor _ _ ⋙ inclusion_path _ _ :=
-nat_iso.of_components (λ f, (λ_ f).symm ≪≫ normalize_unit_iso_app_aux f)
+  𝟭 (a ⟶ b) ≅ (full_normalize B).map_functor a b ⋙ inclusion_path a b :=
+nat_iso.of_components (λ f, (λ_ _).symm ≪≫ normalize_iso nil f)
 begin
-  intros f g η,
-  erw [left_unitor_inv_naturality_assoc, assoc, iso.cancel_iso_inv_left],
-  apply congr_arg (λ β, nat_trans.app β nil) ((normalize_iso _ _ _).hom.naturality η)
+  rintros f g η, erw left_unitor_inv_naturality_assoc,
+  simp only [iso.trans_hom, assoc], congr' 1, apply normalize_naturality nil,
 end
 
 /-- The normalization as an equivalence of categories. -/

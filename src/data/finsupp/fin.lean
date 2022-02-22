b7b3abc1739314c2f85a9ad59f3c79c804165054
Iván Sadofschi Costa <isadofschi@dm.uba.ar>
2022-02-22 00:54:48 +0000

    changes in finsupp.fin

--- src/data/finsupp/fin.lean
+++ src/data/finsupp/fin.lean
@@ -31,17 +31,18 @@ by simp [cons, finsupp.equiv_fun_on_fintype]
 
 @[simp] lemma cons_succ {n : ℕ} (i : fin n) (y : ℕ) (s : fin n →₀ ℕ) : cons y s i.succ = s i :=
 begin
-  simp only [cons, fin.cons, finsupp.equiv_fun_on_fintype, fin.cases_succ, finsupp.coe_mk],
-  rw [coe_fn, finsupp.has_coe_to_fun],
+
+  simp only [finsupp.cons, fin.cons, finsupp.equiv_fun_on_fintype, fin.cases_succ, finsupp.coe_mk],
+  refl,
 end
 
 @[simp] lemma tail_cons {n : ℕ} (y : ℕ) (s : fin n →₀ ℕ) : tail (cons y s) = s :=
 begin
-  simp only [cons, fin.cons, tail, fin.tail],
+  simp only [finsupp.cons, fin.cons, finsupp.tail, fin.tail],
   ext,
-  simp only [equiv_fun_on_fintype_symm_apply_to_fun, equiv.inv_fun_as_coe, finsupp.coe_mk,
-             fin.cases_succ, equiv_fun_on_fintype],
-  rw [coe_fn, finsupp.has_coe_to_fun],
+  simp only [equiv_fun_on_fintype_symm_apply_to_fun, equiv.inv_fun_as_coe,
+  finsupp.coe_mk, fin.cases_succ, equiv_fun_on_fintype],
+  refl,
 end
 
 @[simp] lemma cons_tail {n : ℕ} (s : fin (n + 1) →₀ ℕ) : cons (s 0) (tail s) = s :=

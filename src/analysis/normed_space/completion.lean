536b525c9f54b4a2fb298b081a1bfe032fbda298
Yury G. Kudryashov <urkud@urkud.name>
2022-02-23 16:34:47 -0500

    Add `norm_to_complL`

--- src/analysis/normed_space/completion.lean
+++ src/analysis/normed_space/completion.lean
@@ -4,8 +4,7 @@ Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Yury G. Kudryashov
 -/
 import analysis.normed.group.completion
-import analysis.normed_space.basic
-import analysis.normed_space.linear_isometry
+import analysis.normed_space.operator_norm
 import topology.algebra.uniform_mul_action
 
 /-!
@@ -51,6 +50,10 @@ def to_complL : E →L[𝕜] completion E :=
 to_complₗᵢ.to_continuous_linear_map
 
 @[simp] lemma coe_to_complL : ⇑(to_complL : E →L[𝕜] completion E) = coe := rfl
+
+@[simp] lemma norm_to_complL {𝕜 E : Type*} [nondiscrete_normed_field 𝕜]
+  [normed_group E] [normed_space 𝕜 E] [nontrivial E] : ∥(to_complL : E →L[𝕜] completion E)∥ = 1 :=
+(to_complₗᵢ : E →ₗᵢ[𝕜] completion E).norm_to_continuous_linear_map
 
 end completion
 end uniform_space

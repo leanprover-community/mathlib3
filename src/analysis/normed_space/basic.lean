35325533e9321e997647c882116aac2511a40799
Yury G. Kudryashov <urkud@urkud.name>
2022-02-23 11:38:23 -0500

    Fix

--- src/analysis/normed_space/basic.lean
+++ src/analysis/normed_space/basic.lean
@@ -580,8 +580,8 @@ end
 
 lemma inv_norm_smul_mem_closed_unit_ball [normed_space ℝ β] (x : β) :
   ∥x∥⁻¹ • x ∈ closed_ball (0 : β) 1 :=
-by simp only [mem_closed_ball_zero_iff, norm_smul, normed_field.norm_inv, norm_norm,
-  ← div_eq_inv_mul, div_self_le_one]
+by simp only [mem_closed_ball_zero_iff, norm_smul, norm_inv, norm_norm, ← div_eq_inv_mul,
+  div_self_le_one]
 
 lemma dist_smul [normed_space α β] (s : α) (x y : β) : dist (s • x) (s • y) = ∥s∥ * dist x y :=
 by simp only [dist_eq_norm, (norm_smul _ _).symm, smul_sub]

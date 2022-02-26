717f09dcdaacddccd34b7f1e6065ff96a467098b
Thomas Browning <tb65536@uw.edu>
2022-02-26 07:00:56 -0800

    refactor

--- src/group_theory/solvable.lean
+++ src/group_theory/solvable.lean
@@ -46,7 +46,7 @@ lemma derived_series_normal (n : ℕ) : (derived_series G n).normal :=
 begin
   induction n with n ih,
   { exact (⊤ : subgroup G).normal_of_characteristic },
-  { exactI general_commutator_normal (derived_series G n) (derived_series G n) }
+  { exactI subgroup.commutator_normal (derived_series G n) (derived_series G n) }
 end
 
 @[simp] lemma derived_series_one : derived_series G 1 = commutator G :=
@@ -59,7 +59,7 @@ section commutator_map
 lemma map_commutator_eq_commutator_map (H₁ H₂ : subgroup G) :
   ⁅H₁, H₂⁆.map f = ⁅H₁.map f, H₂.map f⁆ :=
 begin
-  rw [general_commutator, general_commutator, monoid_hom.map_closure],
+  rw [subgroup.commutator, subgroup.commutator, monoid_hom.map_closure],
   apply le_antisymm; apply closure_mono,
   { rintros _ ⟨x, ⟨p, hp, q, hq, rfl⟩, rfl⟩,
     refine ⟨f p, mem_map.mpr ⟨p, hp, rfl⟩, f q, mem_map.mpr ⟨q, hq, rfl⟩, by simp *⟩, },
@@ -69,7 +69,7 @@ end
 
 lemma commutator_le_map_commutator {H₁ H₂ : subgroup G} {K₁ K₂ : subgroup G'} (h₁ : K₁ ≤ H₁.map f)
   (h₂ : K₂ ≤ H₂.map f) : ⁅K₁, K₂⁆ ≤ ⁅H₁, H₂⁆.map f :=
-by { rw map_commutator_eq_commutator_map, exact general_commutator_mono h₁ h₂ }
+by { rw map_commutator_eq_commutator_map, exact subgroup.commutator_mono h₁ h₂ }
 
 section derived_series_map
 
@@ -80,7 +80,7 @@ lemma map_derived_series_le_derived_series (n : ℕ) :
 begin
   induction n with n ih,
   { simp only [derived_series_zero, le_top], },
-  { simp only [derived_series_succ, map_commutator_eq_commutator_map, general_commutator_mono, *], }
+  { simp only [derived_series_succ, map_commutator_eq_commutator_map, commutator_mono, ih] }
 end
 
 variables {f}
@@ -206,8 +206,8 @@ begin
   { exact derived_series_one G },
   rw [derived_series_succ, ih],
   cases (commutator.normal G).eq_bot_or_eq_top with h h,
-  { rw [h, general_commutator_bot] },
-  { rwa [h, ←commutator_def] },
+  { rw [h, commutator_bot] },
+  { rwa h },
 end
 
 lemma is_simple_group.comm_iff_is_solvable :
@@ -247,7 +247,7 @@ begin
   { exact mem_top x },
   { rw key,
     exact (derived_series_normal _ _).conj_mem _
-      (general_commutator_containment _ _ ih ((derived_series_normal _ _).conj_mem _ ih _)) _ },
+      (commutator_containment _ _ ih ((derived_series_normal _ _).conj_mem _ ih _)) _ },
 end
 
 lemma equiv.perm.not_solvable (X : Type*) (hX : 5 ≤ cardinal.mk X) :

e6bd39a0bf6343dbcf0b1cd7a7203abe33620d0b
yuma-mizuno <mizuno.y.aj@gmail.com>
2022-02-26 13:31:46 +0900

    add author

--- src/category_theory/bicategory/coherence.lean
+++ src/category_theory/bicategory/coherence.lean
@@ -1,7 +1,7 @@
 /-
 Copyright (c) 2022 Yuma Mizuno. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
-Authors: Yuma Mizuno
+Authors: Yuma Mizuno, Junyan Xu
 -/
 import category_theory.path_category
 import category_theory.fully_faithful

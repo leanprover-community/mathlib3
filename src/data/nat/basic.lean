659b2f3dd6bfdd4e7e66713ee3be1569f625087c
Riccardo Brasca <riccardo.brasca@gmail.com>
2022-02-26 11:47:57 +0100

    golf

--- src/data/nat/basic.lean
+++ src/data/nat/basic.lean
@@ -609,10 +609,8 @@ le_antisymm_iff.trans (le_antisymm_iff.trans
 
 lemma le_add_pred_of_pos (n : ℕ) {i : ℕ} (hi : i ≠ 0) : n ≤ i + (n - 1) :=
 begin
-  cases n,
-  { simp },
-  { rw [succ_sub_one, succ_eq_add_one, add_comm, add_le_add_iff_right],
-    exact nat.one_le_iff_ne_zero.2 hi }
+  refine le_trans _ (add_tsub_le_assoc),
+  simp [add_comm, nat.add_sub_assoc, one_le_iff_ne_zero.2 hi]
 end
 
 /-!

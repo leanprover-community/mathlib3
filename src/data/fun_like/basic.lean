4dde6ee02af3b062e03b2aaf49d77bf64be38e36
Frédéric Dupuis <dupuisf@iro.umontreal.ca>
2022-02-26 14:16:13 -0500

    subtype fun_like

--- src/data/fun_like/basic.lean
+++ src/data/fun_like/basic.lean
@@ -7,6 +7,7 @@ Authors: Anne Baanen
 import logic.function.basic
 import tactic.lint
 import tactic.norm_cast
+import data.subtype
 
 /-!
 # Typeclass for a type `F` with an injective map to `A → B`
@@ -127,6 +128,11 @@ such as `zero_hom_class`, `mul_hom_class`, `monoid_hom_class`, ....
 class fun_like (F : Sort*) (α : out_param Sort*) (β : out_param $ α → Sort*) :=
 (coe : F → Π a : α, β a)
 (coe_injective' : function.injective coe)
+
+instance subtype.fun_like (F : Sort*) (α : out_param Sort*) (β : out_param $ α → Sort*)
+  (p : F → Prop) [fun_like F α β] : fun_like (subtype p) α β :=
+{ coe := fun_like.coe ∘ coe_subtype.coe,
+  coe_injective' := function.injective.comp fun_like.coe_injective' subtype.coe_injective }
 
 section dependent
 

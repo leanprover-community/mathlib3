243d4cf692868b7f01f83e71778791e2af85f2d4
Hans Parshall <hparshall@gmail.com>
2022-02-26 11:54:02 -0800

    feat(analysis/inner_product_space/adjoint): `op_norm_of_unitary`
    We prove that `U` in `unitary (E →L[k] E)` is an isometry, and we prove
    that its operator norm is equal to 1.

--- src/analysis/inner_product_space/adjoint.lean
+++ src/analysis/inner_product_space/adjoint.lean
@@ -186,6 +186,37 @@ lemma is_adjoint_pair (A : E' →L[ℝ] F') :
 
 end real
 
+lemma norm_map_of_unitary {U : (E →L[𝕜] E)} (hU : U ∈ unitary (E →L[𝕜] E)) (x : E) :
+  ∥U x∥ = ∥x∥ :=
+begin
+  rw [← sq_eq_sq (norm_nonneg (U x)) (norm_nonneg x), norm_sq_eq_inner,
+    ← continuous_linear_map.adjoint_inner_left, ← continuous_linear_map.mul_apply],
+  rw unitary.mem_iff at hU,
+  rw [← continuous_linear_map.star_eq_adjoint, hU.1, continuous_linear_map.one_apply,
+    inner_self_eq_norm_sq],
+end
+
+lemma op_norm_of_unitary [nontrivial E] {U : (E →L[𝕜] E)} (hU : U ∈ unitary (E →L[𝕜] E)) :
+  ∥U∥ = 1 :=
+begin
+  have h_above : ∀ (x : E), ∥U x∥ ≤ 1 * ∥x∥,
+  { intro x,
+    rw norm_map_of_unitary hU,
+    norm_num },
+  have h_below : ∀ (N : ℝ), N ≥ 0 → (∀ (x : E), ∥U x∥ ≤ N * ∥x∥) → 1 ≤ N,
+  { intros N hN_zero,
+    contrapose!,
+    intro hN_one,
+    have hy_nonzero : ∃ (y : E), y ≠ 0 := exists_ne 0,
+    cases hy_nonzero with y hy,
+    use y,
+    rw norm_map_of_unitary hU,
+    have hy_pos : 0 < ∥y∥ := by {rw norm_pos_iff, exact hy},
+    rw mul_lt_iff_lt_one_left hy_pos,
+    exact hN_one },
+  apply continuous_linear_map.op_norm_eq_of_bounds zero_le_one h_above h_below,
+end
+
 end continuous_linear_map
 
 namespace linear_map

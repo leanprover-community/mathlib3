f380ea33c58121ec50554866abd6688785af2f50
Markus Himmel <markus@himmel-villmar.de>
2022-02-27 09:19:02 +0100

    Introduce class reflects_exact_sequences

--- src/category_theory/abelian/exact.lean
+++ src/category_theory/abelian/exact.lean
@@ -194,17 +194,18 @@ end abelian
 
 namespace functor
 variables {D : Type u₂} [category.{v₂} D] [abelian D]
-variables (F : C ⥤ D) {X Y Z : C} (f : X ⟶ Y) (g : Y ⟶ Z)
 
-lemma reflects_exact_of_preserves_zero_morphisms_of_faithful [preserves_zero_morphisms F]
-  [faithful F] (hfg : exact (F.map f) (F.map g)) : exact f g :=
-begin
-  rw [abelian.exact_iff, ← F.map_comp, F.map_eq_zero_iff] at hfg,
-  refine (abelian.exact_iff _ _).2 ⟨hfg.1, F.zero_of_map_zero _ _⟩,
-  obtain ⟨k, hk⟩ := kernel.lift' (F.map g) (F.map (kernel.ι g)) (by simp [← F.map_comp]),
-  obtain ⟨l, hl⟩ := cokernel.desc' (F.map f) (F.map (cokernel.π f)) (by simp [← F.map_comp]),
-  rw [F.map_comp, ← hk, ← hl, category.assoc, reassoc_of hfg.2, zero_comp, comp_zero]
-end
+@[priority 100]
+instance reflects_exact_sequences_of_preserves_zero_morphisms_of_faithful (F : C ⥤ D)
+  [preserves_zero_morphisms F] [faithful F] : reflects_exact_sequences F :=
+{ reflects := λ X Y Z f g hfg,
+  begin
+    rw [abelian.exact_iff, ← F.map_comp, F.map_eq_zero_iff] at hfg,
+    refine (abelian.exact_iff _ _).2 ⟨hfg.1, F.zero_of_map_zero _ _⟩,
+    obtain ⟨k, hk⟩ := kernel.lift' (F.map g) (F.map (kernel.ι g)) (by simp [← F.map_comp]),
+    obtain ⟨l, hl⟩ := cokernel.desc' (F.map f) (F.map (cokernel.π f)) (by simp [← F.map_comp]),
+    rw [F.map_comp, ← hk, ← hl, category.assoc, reassoc_of hfg.2, zero_comp, comp_zero]
+  end }
 
 end functor
 

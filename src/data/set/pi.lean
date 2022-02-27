019fdb23f41b701113676521b278389b26d8b4d6
Rishikesh Vaishnav <rishhvaishnav@gmail.com>
2022-02-27 18:31:12 +0000

    more docs

--- src/data/set/pi.lean
+++ src/data/set/pi.lean
@@ -16,14 +16,17 @@ namespace set
 
 /-- Restrict domain of a pi type `f` to a set `s`. Same as `subtype.restrict` but this
 takes the restricting set before the pi type. Generalization of `set.restrict` to pi types. -/
-def pi_restrict {α : Type*} {β : α → Type*} (mv : set α) := λ (g : Π i, β i) (i : mv), g i
+def pi_restrict {α : Type*} {β : α → Type*} (s : set α) (g : Π i, β i) :
+  Π i : s, β i := λ (i : s), g i
 
+/-- Take the image of a set of pi types on restriction to a subtype of inputs. -/
 @[reducible]
-def pi_restrict_image {α : Type*} {β : α → Type*} (mv : set α) :=
-  λ (g : set (Π i, β i)), pi_restrict mv '' g
+def pi_restrict_image {α : Type*} {β : α → Type*} (s : set α) (g : set (Π i, β i)) :
+  set (Π i : s, β i) := pi_restrict s '' g
 
+/-- Take the pre-image of a set of pi types restricted to a subtype of inputs. -/
 @[reducible]
-def pi_restrict_preimage {α : Type*} {β : α → Type*} (mv : set α) :=
-  λ (g : set (Π i : mv, β i)), pi_restrict mv ⁻¹' g
+def pi_restrict_preimage {α : Type*} {β : α → Type*} (s : set α) (g : set (Π i : s, β i)) :
+  set (Π i : α, β i) := pi_restrict s ⁻¹' g
 
 end set

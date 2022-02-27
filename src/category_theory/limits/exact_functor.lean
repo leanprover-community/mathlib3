37400819ee853a5eb5eb9e5c37f1c07232eebfc8
Markus Himmel <markus@himmel-villmar.de>
2022-02-27 14:16:17 +0100

    feat(category_theory/limits): bundled exact functors

--- src/category_theory/limits/exact_functor.lean
+++ src/category_theory/limits/exact_functor.lean
@@ -11,7 +11,7 @@ import category_theory.limits.preserves.finite
 We say that a functor `F` is left exact if it preserves finite limits, it is right exact if it
 preserves finite colimits, and it is exact if it is both left exact and right exact.
 
-In this file, we define the categories of bundled left exact, right exact and exact functors.#check
+In this file, we define the categories of bundled left exact, right exact and exact functors.
 
 ## Implementation notes
 
@@ -30,38 +30,105 @@ variables {C : Type u₁} [category.{v} C] {D : Type u₂} [category.{v} D]
 section
 variables (C) (D)
 
-/-- Bundled left-exact functors -/
-@[derive category]
+/-- Bundled left-exact functors. -/
+@[derive category, nolint has_inhabited_instance]
 def LeftExactFunctor :=
 { F : C ⥤ D // nonempty (preserves_finite_limits F) }
 
 infixr ` ⥤ₗ `:26 := LeftExactFunctor
 
-/-- Bundled right-exact functors -/
-@[derive category]
+/-- A left exact functor is in particular a functor. -/
+@[derive full, derive faithful]
+def LeftExactFunctor.forget : (C ⥤ₗ D) ⥤ (C ⥤ D) :=
+full_subcategory_inclusion _
+
+/-- Bundled right-exact functors. -/
+@[derive category, nolint has_inhabited_instance]
 def RightExactFunctor :=
 { F : C ⥤ D // nonempty (preserves_finite_colimits F) }
 
 infixr ` ⥤ᵣ `:26 := RightExactFunctor
 
-/-- Bundled exact functors -/
-@[derive category]
+/-- A right exact functor is in particular a functor. -/
+@[derive full, derive faithful]
+def RightExactFunctor.forget : (C ⥤ᵣ D) ⥤ (C ⥤ D) :=
+full_subcategory_inclusion _
+
+/-- Bundled exact functors. -/
+@[derive category, nolint has_inhabited_instance]
 def ExactFunctor :=
 { F : C ⥤ D // nonempty (preserves_finite_limits F) ∧ nonempty (preserves_finite_colimits F) }
 
 infixr ` ⥤ₑ `:26 := ExactFunctor
 
-@[simps]
-def LeftExact.of_exact : (C ⥤ₑ D) ⥤ (C ⥤ₗ D) :=
-{ obj := λ F, ⟨F.1, F.2.1⟩,
-  map := λ X Y f, f }
+/-- An exact functor is in particular a functor. -/
+@[derive full, derive faithful]
+def ExactFunctor.forget : (C ⥤ₑ D) ⥤ (C ⥤ D) :=
+full_subcategory_inclusion _
 
-@[simps]
-def RightExact.of_exact : (C ⥤ₑ D) ⥤ (C ⥤ᵣ D) :=
-{ obj := λ F, ⟨F.1, F.2.2⟩,
-  map := λ X Y f, f }
+/-- Turn an exact functor into a left exact functor. -/
+@[derive full, derive faithful]
+def LeftExactFunctor.of_exact : (C ⥤ₑ D) ⥤ (C ⥤ₗ D) :=
+full_subcategory.map (λ X, and.left)
 
+/-- Turn an exact functor into a left exact functor. -/
+@[derive full, derive faithful]
+def RightExactFunctor.of_exact : (C ⥤ₑ D) ⥤ (C ⥤ᵣ D) :=
+full_subcategory.map (λ X, and.right)
 
+variables {C D}
+
+@[simp] lemma LeftExactFunctor.of_exact_obj (F : C ⥤ₑ D) :
+  (LeftExactFunctor.of_exact C D).obj F = ⟨F.1, F.2.1⟩ := rfl
+@[simp] lemma RightExactFunctor.of_exact_obj (F : C ⥤ₑ D) :
+  (RightExactFunctor.of_exact C D).obj F = ⟨F.1, F.2.2⟩ := rfl
+
+@[simp] lemma LeftExactFunctor.of_exact_map {F G : C ⥤ₑ D} (α : F ⟶ G) :
+  (LeftExactFunctor.of_exact C D).map α = α := rfl
+@[simp] lemma RightExactFunctor.of_exact_map {F G : C ⥤ₑ D} (α : F ⟶ G) :
+  (RightExactFunctor.of_exact C D).map α = α := rfl
+
+@[simp] lemma LeftExactFunctor.forget_obj (F : C ⥤ₗ D) :
+  (LeftExactFunctor.forget C D).obj F = F.1 := rfl
+@[simp] lemma RightExactFunctor.forget_obj (F : C ⥤ᵣ D) :
+  (RightExactFunctor.forget C D).obj F = F.1 := rfl
+@[simp] lemma ExactFunctor.forget_obj (F : C ⥤ₑ D) :
+  (ExactFunctor.forget C D).obj F = F.1 := rfl
+
+@[simp] lemma LeftExactFunctor.forget_map {F G : C ⥤ₗ D} (α : F ⟶ G) :
+  (LeftExactFunctor.forget C D).map α = α := rfl
+@[simp] lemma RightExactFunctor.forget_map {F G : C ⥤ᵣ D} (α : F ⟶ G) :
+  (RightExactFunctor.forget C D).map α = α := rfl
+@[simp] lemma ExactFunctor.forget_map {F G : C ⥤ₑ D} (α : F ⟶ G) :
+  (ExactFunctor.forget C D).map α = α := rfl
+
+/-- Turn a left exact functor into an object of the category `LeftExactFunctor C D`. -/
+def LeftExactFunctor.of (F : C ⥤ D) [preserves_finite_limits F] : C ⥤ₗ D := ⟨F, ⟨infer_instance⟩⟩
+/-- Turn a right exact functor into an object of the category `RightExactFunctor C D`. -/
+def RightExactFunctor.of (F : C ⥤ D) [preserves_finite_colimits F] : C ⥤ᵣ D :=
+⟨F, ⟨infer_instance⟩⟩
+/-- Turn an exact functor into an object of the category `ExactFunctor C D`. -/
+def ExactFunctor.of (F : C ⥤ D) [preserves_finite_limits F] [preserves_finite_colimits F] :
+  C ⥤ₑ D := ⟨F, ⟨⟨infer_instance⟩, ⟨infer_instance⟩⟩⟩
+
+@[simp] lemma LeftExactFunctor.of_fst (F : C ⥤ D) [preserves_finite_limits F] :
+  (LeftExactFunctor.of F).1 = F := rfl
+@[simp] lemma RightExactFunctor.of_fst (F : C ⥤ D) [preserves_finite_colimits F] :
+  (RightExactFunctor.of F).1 = F := rfl
+@[simp] lemma ExactFunctor.of_fst (F : C ⥤ D) [preserves_finite_limits F]
+  [preserves_finite_colimits F] : (ExactFunctor.of F).1 = F := rfl
+
+lemma LeftExactFunctor.forget_obj_of (F : C ⥤ D) [preserves_finite_limits F] :
+  (LeftExactFunctor.forget C D).obj (LeftExactFunctor.of F) = F := rfl
+lemma RightExactFunctor.forget_obj_of (F : C ⥤ D) [preserves_finite_colimits F] :
+  (RightExactFunctor.forget C D).obj (RightExactFunctor.of F) = F := rfl
+lemma ExactFunctor.forget_obj_of (F : C ⥤ D) [preserves_finite_limits F]
+  [preserves_finite_colimits F] : (ExactFunctor.forget C D).obj (ExactFunctor.of F) = F := rfl
+
+noncomputable instance (F : C ⥤ₗ D) : preserves_finite_limits F.1 := classical.choice F.2
+noncomputable instance (F : C ⥤ᵣ D) : preserves_finite_colimits F.1 := classical.choice F.2
+noncomputable instance (F : C ⥤ₑ D) : preserves_finite_limits F.1 := classical.choice F.2.1
+noncomputable instance (F : C ⥤ₑ D) : preserves_finite_colimits F.1 := classical.choice F.2.2
 
 end
 

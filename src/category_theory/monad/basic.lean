d418ec74179036dbe2bfcc4605dad966e0c2fc2d
Markus Himmel <markus@himmel-villmar.de>
2022-02-27 20:50:42 +0100

    Get started

--- src/category_theory/monad/basic.lean
+++ src/category_theory/monad/basic.lean
@@ -3,7 +3,7 @@ Copyright (c) 2019 Scott Morrison. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Scott Morrison, Bhavik Mehta, Adam Topaz
 -/
-import category_theory.functor_category
+import category_theory.functor.category
 import category_theory.fully_faithful
 import category_theory.reflects_isomorphisms
 
--- src/category_theory/monoidal/functor_category.lean
+++ src/category_theory/monoidal/functor_category.lean
@@ -4,7 +4,7 @@ Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Scott Morrison
 -/
 import category_theory.monoidal.braided
-import category_theory.functor_category
+import category_theory.functor.category
 import category_theory.const
 
 /-!

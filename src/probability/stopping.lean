9cdb488d319fc3e6743f25a3abf005e10d92edcd
RemyDegenne <Remydegenne@gmail.com>
2022-02-28 14:19:15 +0100

    factor lemma out of adapted.prog_measurable_of_continuous

--- src/probability/stopping.lean
+++ src/probability/stopping.lean
@@ -335,43 +335,50 @@ lemma prog_measurable_of_tendsto [measurable_space ι] [metric_space β] [borel_
   prog_measurable f u :=
 prog_measurable_of_tendsto' at_top h h_tendsto
 
+lemma measurable_prod_of_continuous_of_measurable (ι) [emetric_space ι] [mι : measurable_space ι]
+  [second_countable_topology ι] [opens_measurable_space ι] [metric_space β] [borel_space β]
+  {m : measurable_space α} {u : ι → α → β}
+  (hu_cont : ∀ x, continuous (λ i, u i x)) (h : ∀ i, measurable[m] (u i)) :
+  measurable[prod.measurable_space' mι m] (λ (p : ι × α), u p.fst p.snd) :=
+begin
+  obtain ⟨t_sf, ht_sf⟩ : ∃ t : ℕ → @simple_func ι _ ι, ∀ j x,
+    tendsto (λ n, u (t n j) x) at_top (𝓝 $ u j x),
+  { have h_str_meas : strongly_measurable (id : ι → ι), from strongly_measurable_id,
+    refine ⟨h_str_meas.approx, λ j x, _⟩,
+    have h_tendsto : tendsto (λ n, h_str_meas.approx n j) at_top (𝓝 (id j)),
+      from h_str_meas.tendsto_approx j,
+    exact ((hu_cont x).tendsto j).comp h_tendsto, },
+  let U := λ (n : ℕ) (p : ι × α), u (t_sf n p.fst) p.snd,
+  have h_tendsto : tendsto U at_top (𝓝 (λ p, u p.fst p.snd)),
+  { rw tendsto_pi_nhds,
+    exact λ p, ht_sf p.fst p.snd, },
+  refine @measurable_of_tendsto_metric (ι × α) β
+    (prod.measurable_space' mι m) _ _ _ _ _ (λ n, _) h_tendsto,
+  have h_meas : measurable[prod.measurable_space' subtype.measurable_space m]
+    (λ (p : (t_sf n).range × α), u ↑p.fst p.snd),
+  { have : (λ (p : ↥((t_sf n).range) × α), u ↑(p.fst) p.snd)
+        = (λ (p : α × ((t_sf n).range)), u ↑(p.snd) p.fst) ∘ prod.swap,
+      by refl,
+    rw [this, @measurable_swap_iff α ↥((t_sf n).range) β m],
+    haveI : encodable (t_sf n).range, from fintype.encodable ↥(t_sf n).range,
+    exact measurable_from_prod_encodable (λ j, h j), },
+  have : (λ p : ι × α, u (t_sf n p.fst) p.snd)
+    = (λ p : ↥(t_sf n).range × α, u p.fst p.snd)
+      ∘ (λ p : ι × α, (⟨t_sf n p.fst, simple_func.mem_range_self _ _⟩, p.snd)),
+  { refl, },
+  simp_rw [U, this],
+  refine h_meas.comp (measurable.prod_mk _ (@measurable_snd _ _ _ m)),
+  exact ((t_sf n).measurable.comp (@measurable_fst _ _ _ m)).subtype_mk,
+end
+
 /-- A continuous and adapted process is progressively measurable. -/
 theorem adapted.prog_measurable_of_continuous [emetric_space ι] [measurable_space ι]
   [second_countable_topology ι] [opens_measurable_space ι] [metric_space β] [borel_space β]
   (h : adapted f u) (hu_cont : ∀ x, continuous (λ i, u i x)) :
   prog_measurable f u :=
-begin
-  intro i,
-  obtain ⟨t_sf, ht_sf⟩ : ∃ t : ℕ → @simple_func (set.Iic i) _ (set.Iic i), ∀ j x,
-    tendsto (λ n, u (t n j) x) at_top (𝓝 $ u j x),
-  { have h_str_meas : strongly_measurable (id : set.Iic i → set.Iic i),
-      from @strongly_measurable_id (set.Iic i) subtype.measurable_space _ _ _,
-    refine ⟨h_str_meas.approx, λ j x, _⟩,
-    have h_tendsto : tendsto (λ n, h_str_meas.approx n j) at_top (𝓝 (id j)),
-      from h_str_meas.tendsto_approx j,
-    exact ((hu_cont x).tendsto j).comp ((continuous_induced_dom.tendsto j).comp h_tendsto), },
-  let U := λ (n : ℕ) (p : set.Iic i × α), u (t_sf n p.fst) p.snd,
-  have h_tendsto : tendsto U at_top (𝓝 (λ p, u p.fst p.snd)),
-  { rw tendsto_pi_nhds,
-    exact λ p, ht_sf p.fst p.snd, },
-  refine @measurable_of_tendsto_metric (set.Iic i × α) β
-    (prod.measurable_space' subtype.measurable_space (f i)) _ _ _ _ _ (λ n, _) h_tendsto,
-  have h_meas : measurable[prod.measurable_space' subtype.measurable_space (f i)]
-    (λ (p : (t_sf n).range × α), u ↑p.fst p.snd),
-  { have : (λ (p : ↥((t_sf n).range) × α), u ↑(p.fst) p.snd)
-        = (λ (p : α × ((t_sf n).range)), u ↑(p.snd) p.fst) ∘ prod.swap,
-      by refl,
-    rw [this,  @measurable_swap_iff α ↥((t_sf n).range) β (f i)],
-    haveI : encodable (t_sf n).range, from fintype.encodable ↥(t_sf n).range,
-    exact measurable_from_prod_encodable (λ j, (h j).mono (f.mono (j : set.Iic i).prop) le_rfl), },
-  have : (λ p : set.Iic i × α, u (t_sf n p.fst) p.snd)
-    = (λ p : ↥(t_sf n).range × α, u p.fst p.snd)
-      ∘ (λ p : set.Iic i × α, (⟨t_sf n p.fst, simple_func.mem_range_self _ _⟩, p.snd)),
-  { refl, },
-  simp_rw [U, this],
-  refine h_meas.comp (measurable.prod_mk _ (@measurable_snd _ _ _ (f i))),
-  exact ((t_sf n).measurable.comp (@measurable_fst _ _ _ (f i))).subtype_mk,
-end
+λ i, measurable_prod_of_continuous_of_measurable (set.Iic i)
+  (λ x, (hu_cont x).comp continuous_induced_dom)
+  (λ j, (h j).mono (f.mono (j : set.Iic i).prop) le_rfl)
 
 end adapted_process
 

d84c4efeae1cb50224c7bc19e23ffc66d92a960e
pbazin <pierre-alexandre.bazin@ens.psl.eu>
2022-03-02 13:23:45 +0100

    ignor this commit

--- src/algebra/module/torsion_pid.lean
+++ src/algebra/module/torsion_pid.lean
@@ -4,15 +4,22 @@ import ring_theory.finiteness
 import algebra.module.torsion
 
 universes u v
+open_locale big_operators
 
-section coprime
-end coprime
+namespace finset
+lemma prod_finset_coe' {I R : Type*} [comm_monoid R] (f : I → R) (s : finset I) :
+  ∏ i : s, f i = ∏ i in s, f i :=
+by convert prod_finset_coe f s
+end finset
+
+namespace multiset
+lemma le_bind {α β : Type*} {f : α → multiset β} (S : multiset α) {x : α} (hx : x ∈ S) :
+  f x ≤ S.bind f := sorry
+end multiset
 
 section pid
 open_locale direct_sum
-open_locale big_operators
-open submodule
-open dfinsupp
+open submodule dfinsupp
 
 variables {R : Type u} [comm_ring R] [is_domain R] [is_principal_ideal_ring R]
   {M : Type v} [add_comm_group M] [module R M]
@@ -26,7 +33,6 @@ lemma coprime_of_irreducible_pow {ι : Type*} [fintype ι] (p : ι → R)
 λ i j h, ((irred i).coprime_iff_not_dvd.mpr
   (λ h', h (assoc _ _ ((irred i).associated_of_dvd (irred j) h')))).pow_left.pow_right
 
-
 theorem prime_power_torsion_generators [module.finite R M] (hM : torsion' R M = ⊤) :
   ∃ (ι : Type u) [fintype ι] [decidable_eq ι] (p : ι → R) [∀ i, irreducible (p i)] (e : ι → ℕ),
   by exactI direct_sum.submodule_is_internal (λ i, torsion R M ((p i) ^ e i)) :=
@@ -36,49 +42,32 @@ begin
   let P : multiset (associates R) :=
     S.val.bind (λ s, multiset.map associates.mk $
       principal_ideal_ring.factors ↑(classical.some (all_torsion s))),
-  let ι : Type u := P.to_finset,
-  haveI : fintype ι := fintype.of_finset P.to_finset (λ x, iff.rfl),
-  let p : ι → R := λ i, classical.some $ associates.mk_surjective i,
-  have hp : ∀ i, associates.mk (p i) = i := λ i, classical.some_spec $ associates.mk_surjective ↑i,
-  have irred : ∀ i, irreducible (p i) := λ i, begin
+  let ι := P.to_finset,
+  haveI : fintype ι := fintype.of_finset ι (λ x, iff.rfl),
+  let p : _ → R := λ i, classical.some $ associates.mk_surjective i,
+  have hp : ∀ i, associates.mk (p i) = i := λ i, classical.some_spec $ associates.mk_surjective i,
+  have irred : ∀ i : ι, irreducible (p i) := λ i, begin
     have hi := i.prop, rw [multiset.mem_to_finset, multiset.mem_bind] at hi,
     obtain ⟨s, hs, hi⟩ := hi, rw multiset.mem_map at hi, obtain ⟨q, hq, hi⟩ := hi,
     rw [← associates.irreducible_mk, hp i, ← hi, associates.irreducible_mk],
     apply (principal_ideal_ring.factors_spec _ _).left _ hq,
     exact non_zero_divisors.coe_ne_zero _
   end,
-  refine ⟨ι, by apply_instance, by {classical, apply_instance}, p, irred, λ i, P.count i, _⟩,
-  have coprime : pairwise (is_coprime on λ i, p i ^ P.count i) :=
+  refine ⟨ι, by apply_instance, by {classical, apply_instance}, λ i, p i, irred, λ i, P.count i, _⟩,
+  have coprime : pairwise (is_coprime on λ i : ι, p i ^ P.count i) :=
     coprime_of_irreducible_pow _ irred (λ i j assoc, subtype.coe_injective
-      (by { rw [← hp, ← hp, associates.mk_eq_mk_iff_associated], exact assoc })) _,
+      (by { rw [← hp ↑i, ← hp ↑j, associates.mk_eq_mk_iff_associated], exact assoc })) _,
   apply torsion_is_internal coprime,
   rw [eq_top_iff, ← h, span_le], intros s hs, rw set_like.mem_coe,
-  refine torsion_le_torsion_of_dvd ↑_ _ _ (classical.some_spec (all_torsion s)), sorry
+  refine torsion_le_torsion_of_dvd ↑_ _ _ (classical.some_spec (all_torsion s)),
+  --change _ ∣ ∏ (i : ↥ι), (λ i, p i ^ P.count i) ↑i,
+  rw [← (principal_ideal_ring.factors_spec (_ : R)
+    (non_zero_divisors.coe_ne_zero _)).right.dvd_iff_dvd_left,
+  ← associates.mk_dvd_mk, ← associates.prod_mk],
+  convert multiset.prod_dvd_prod_of_le (S.val.le_bind hs),
+  change _ = P.prod, rw finset.prod_multiset_count,
+  --have := finset.prod_finset_coe' (λ i, p i ^ P.count i) ι, rw this,
+  sorry, --prod_multiset_map_count
 end
 
 end pid
-
-/-  have fg : (torsion' R M).fg :=
-    is_noetherian_def.mp (is_noetherian_of_fg_of_noetherian' (module.finite_def.mp _)) _,
-  { cases fg with S h,
-    classical,
-    let P := S.val.bind (λ s, factors $ begin
-      by_cases hs : s ∈ torsion' R M,
-      { exact classical.some hs },
-      { exact 0 } --irrelevant value
-    end),
-    use P.to_finset,
-    split, split,
-    { intro p,
-      cases p with p hp,
-      rw [mem_to_finset, mem_bind] at hp,
-      cases hp with s hp, cases hp with hs hp,
-      apply (factors_spec _ _).left _ hp,
-      split_ifs with h1,
-      { exact (classical.some_spec h1).right },
-      { exfalso, apply h1,
-        rw ← h, exact submodule.subset_span hs } },
-    { use λ p, count (p:R) P,
-      sorry },
-    { apply_instance } },
-  apply_instance -/
--- src/ring_theory/coprime/pairwise.lean
+++ src/ring_theory/coprime/pairwise.lean
@@ -1,12 +1,20 @@
 import ring_theory.coprime.lemmas
 open_locale big_operators
 
-variables {R : Type*} {I : Type*} [comm_semiring R] [fintype I] {p : I → R} [decidable_eq I]
+variables {R : Type*} {I : Type*} [comm_semiring R] {p : I → R} [decidable_eq I] {s : finset I}
 
-lemma exists_sum_eq_one_iff_pairwise_coprime :
-  (∃ μ : I → R, ∑ (i : I), μ i * ∏ j in {i}ᶜ, p j = 1) ↔ pairwise (is_coprime on p) := sorry
+lemma exists_sum_eq_one_iff_pairwise_coprime' :
+  (∃ μ : I → R, ∑ i in s, μ i * ∏ j in s \ {i}, p j = 1) ↔ pairwise (is_coprime on λ i : s, p i) :=
+sorry
 
-lemma pairwise_coprime_iff_coprime_prod :
+lemma exists_sum_eq_one_iff_pairwise_coprime [fintype I] :
+  (∃ μ : I → R, ∑ (i : I), μ i * ∏ j in {i}ᶜ, p j = 1) ↔ pairwise (is_coprime on p) := begin
+convert exists_sum_eq_one_iff_pairwise_coprime' using 1, ext, split,
+{ rintro hp ⟨i, _⟩ ⟨j, _⟩ h, apply hp, intro h', apply h, ext, exact h' },
+{ intros hp i j h, apply hp ⟨i, finset.mem_univ i⟩ ⟨j, finset.mem_univ j⟩,
+  rintro ⟨h'⟩, exact h rfl } end
+
+lemma pairwise_coprime_iff_coprime_prod [fintype I] :
   pairwise (is_coprime on p) ↔ ∀ i, is_coprime (p i) (∏ j in {i}ᶜ, (p j)) :=
 begin
   split; intro hp,

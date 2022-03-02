816b0a2b5f28a7a92681ecc34b8abb5d6c30d0b8
Eric Rodriguez <ericrboidi@gmail.com>
2022-03-02 12:57:58 +0000

    tidy

--- src/ring_theory/localization/cardinality.lean
+++ src/ring_theory/localization/cardinality.lean
@@ -11,20 +11,15 @@ import ring_theory.integral_domain
 # Cardinality of localizations
 
 In this file, we establish the cardinality of localizations. In most cases, a localization has
-cardinality equal to the base ring (even in the finite case!); however, this can be muddled by
-zero-divisors.
+cardinality equal to the base ring. If there is zero-divisors, however, this is no longer true -
+for example, `zmod 6` localized at `{2, 4}` is equal to `zmod 3`, and if you have zero in your
+submonoid, then your localization becomes the trivial ring.
 
 ## Main statements
 
-* `is_localization.cardinal_mk`: All infinite localizations of rings at submonoids smaller than the
-  non-zero divisors have the same cardinality as the base ring.
-* `is_localization.card`: All localizations of finite integral domains at submonoids not containing
-  zero have the same cardinality as the base domain.
-
-## Implementation details
-
-Note that in `is_localization.card`, the second `fintype` is not an instance, and is instead
-provided by `is_localization.fintype' S L`.
+* `is_localization.card_le`: A localization has cardinality no larger than the base ring.
+* `is_localization.card`: If you don't localize at zero-divisors, the localization of a ring has
+  cardinality equal to its base ring,
 
 -/
 
@@ -36,49 +31,39 @@ universes u v
 namespace is_localization
 
 variables {R : Type u} [comm_ring R] (S : submonoid R) {L : Type u} [comm_ring L]
-  [algebra R L] [is_localization S L]
+          [algebra R L] [is_localization S L]
 include S
 
-lemma algebra_map_surj [fintype R] : function.surjective (algebra_map R L) :=
+/-- Localizing a finite ring can only reduce the amount of elements. -/
+lemma algebra_map_surjective_of_fintype [fintype R] : function.surjective (algebra_map R L) :=
 begin
-  classical, haveI : fintype L := is_localization.fintype' S L,
-  intro x, rcases is_localization.surj S x with ⟨⟨r,s⟩,h⟩,
-  rcases (is_of_fin_order_iff_pow_eq_one _).1
-    (exists_pow_eq_one (is_localization.map_units L s).unit) with ⟨n,hn,hp⟩,
-  rw [units.ext_iff, units.coe_pow] at hp, change algebra_map R L s ^ n = 1 at hp,
-  rw [← nat.succ_pred_eq_of_pos hn, pow_succ] at hp,
-  use r * s ^ (n-1), erw [map_mul, map_pow, ← h, mul_assoc, hp, mul_one],
+  classical,
+  haveI : fintype L := is_localization.fintype' S L,
+  intro x,
+  obtain ⟨⟨r, s⟩, h : x * (algebra_map R L) ↑s = (algebra_map R L) r⟩ := is_localization.surj S x,
+  obtain ⟨n, hn, hp⟩ :=
+    (is_of_fin_order_iff_pow_eq_one _).1 (exists_pow_eq_one (is_localization.map_units L s).unit),
+  rw [units.ext_iff, units.coe_pow, is_unit.unit_spec, ←nat.succ_pred_eq_of_pos hn, pow_succ] at hp,
+  exact ⟨r * s ^ (n - 1), by erw [map_mul, map_pow, ←h, mul_assoc, hp, mul_one]⟩
 end
 
+/-- A localization always has cardinality less than or equal to the base ring. -/
 lemma card_le : #L ≤ #R :=
 begin
-  classical, by_cases infinite R, resetI,
-  { erw [←cardinal.mul_eq_self $ cardinal.omega_le_mk R],
-    let f : R × R → L := λ aa, is_localization.mk' _ aa.1 (if h : aa.2 ∈ S then ⟨aa.2, h⟩ else 1),
-    refine @cardinal.mk_le_of_surjective _ _ f (λ a, _),
-    obtain ⟨x, y, h⟩ := is_localization.mk'_surjective S a,
-    use (x, y),
-    dsimp [f],
-    rwa [dif_pos $ show ↑y ∈ S, from y.2, set_like.eta] },
-  haveI : fintype R := fintype_of_not_infinite h,
-  exact cardinal.mk_le_of_surjective (algebra_map_surj S),
+  classical,
+  casesI fintype_or_infinite R,
+  { exact cardinal.mk_le_of_surjective (algebra_map_surjective_of_fintype S) },
+  erw [←cardinal.mul_eq_self $ cardinal.omega_le_mk R],
+  set f : R × R → L := λ aa, is_localization.mk' _ aa.1 (if h : aa.2 ∈ S then ⟨aa.2, h⟩ else 1),
+  refine @cardinal.mk_le_of_surjective _ _ f (λ a, _),
+  obtain ⟨x, y, h⟩ := is_localization.mk'_surjective S a,
+  use (x, y),
+  dsimp [f],
+  rwa [dif_pos $ show ↑y ∈ S, from y.2, set_like.eta]
 end
 
-lemma card_ge (hS : S ≤ R⁰) : #R = #L :=
+/-- If you do not localize at any zero-divisors, localization preserves cardinality. -/
+lemma card (hS : S ≤ R⁰) : #R = #L :=
 (cardinal.mk_le_of_injective (is_localization.injective L hS)).antisymm (card_le S)
-
-/-- All finite localizations of integral domains at submonoids not containing zero have the same
-cardinality as the base domain. -/
-lemma card {R : Type u} [comm_ring R] {S : submonoid R} {L : Type v} [comm_ring L] [algebra R L]
-  [fintype R] [is_domain R] [is_localization S L] (hS : ↑0 ∉ S) :
-  fintype.card R = @fintype.card L (fintype' S L) :=
-begin
-  casesI subsingleton_or_nontrivial R,
-  { haveI := unique R L S,
-    haveI := unique_of_subsingleton (0 : R),
-    simp only [fintype.card_unique] },
-  letI := fintype' S L,
-  exact fintype.card_of_bijective ((fintype.is_field_of_domain R).localization_map_bijective hS),
-end
 
 end is_localization

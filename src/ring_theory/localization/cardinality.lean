655448cbd115aefbe9f829f753914efa4432a133
Junyan Xu <junyanxumath@gmail.com>
2022-03-02 01:12:17 -0500

    clean up

--- src/ring_theory/localization/cardinality.lean
+++ src/ring_theory/localization/cardinality.lean
@@ -36,16 +36,15 @@ universes u v
 namespace is_localization
 
 variables {R : Type u} [comm_ring R] (S : submonoid R) {L : Type u} [comm_ring L]
-  [algebra R L] [hl : is_localization S L]
-include hl
+  [algebra R L] [is_localization S L]
+include S
 
 lemma algebra_map_surj [fintype R] : function.surjective (algebra_map R L) :=
 begin
-  classical,
-  haveI : fintype L := is_localization.fintype' S L,
-  intro x, have := hl.surj, rcases this x with ⟨⟨r,s⟩,h⟩,
-  have hu := hl.map_units, have := exists_pow_eq_one (hu s).unit,
-  rcases (is_of_fin_order_iff_pow_eq_one _).1 this with ⟨n,hn,hp⟩,
+  classical, haveI : fintype L := is_localization.fintype' S L,
+  intro x, rcases is_localization.surj S x with ⟨⟨r,s⟩,h⟩,
+  rcases (is_of_fin_order_iff_pow_eq_one _).1
+    (exists_pow_eq_one (is_localization.map_units L s).unit) with ⟨n,hn,hp⟩,
   rw [units.ext_iff, units.coe_pow] at hp, change algebra_map R L s ^ n = 1 at hp,
   rw [← nat.succ_pred_eq_of_pos hn, pow_succ] at hp,
   use r * s ^ (n-1), erw [map_mul, map_pow, ← h, mul_assoc, hp, mul_one],
@@ -65,21 +64,8 @@ begin
   exact cardinal.mk_le_of_surjective (algebra_map_surj S),
 end
 
-/-- All infinite localizations of rings at submonoids smaller than the non-zero divisors have the
-same cardinality as the base ring. -/
-lemma cardinal_mk {R : Type u} [comm_ring R] {S : submonoid R} (L : Type u) [comm_ring L]
-  [algebra R L] [infinite R] [is_localization S L] (hS : S ≤ R⁰) : #R = #L :=
-begin
-  classical,
-  apply (cardinal.mk_le_of_injective (is_localization.injective L hS)).antisymm,
-  erw [←cardinal.mul_eq_self $ cardinal.omega_le_mk R],
-  let f : R × R → L := λ aa, is_localization.mk' _ aa.1 (if h : aa.2 ∈ S then ⟨aa.2, h⟩ else 1),
-  refine @cardinal.mk_le_of_surjective _ _ f (λ a, _),
-  obtain ⟨x, y, h⟩ := is_localization.mk'_surjective S a,
-  use (x, y),
-  dsimp [f],
-  rwa [dif_pos $ show ↑y ∈ S, from y.2, set_like.eta]
-end
+lemma card_ge (hS : S ≤ R⁰) : #R = #L :=
+(cardinal.mk_le_of_injective (is_localization.injective L hS)).antisymm (card_le S)
 
 /-- All finite localizations of integral domains at submonoids not containing zero have the same
 cardinality as the base domain. -/

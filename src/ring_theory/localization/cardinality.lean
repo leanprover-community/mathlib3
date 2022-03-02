70c717a571521733146442148d4d055c498d2d09
Junyan Xu <junyanxumath@gmail.com>
2022-03-02 00:53:30 -0500

    localizations are surjective for a finite ring

--- src/ring_theory/localization/cardinality.lean
+++ src/ring_theory/localization/cardinality.lean
@@ -35,6 +35,36 @@ universes u v
 
 namespace is_localization
 
+variables {R : Type u} [comm_ring R] (S : submonoid R) {L : Type u} [comm_ring L]
+  [algebra R L] [hl : is_localization S L]
+include hl
+
+lemma algebra_map_surj [fintype R] : function.surjective (algebra_map R L) :=
+begin
+  classical,
+  haveI : fintype L := is_localization.fintype' S L,
+  intro x, have := hl.surj, rcases this x with ⟨⟨r,s⟩,h⟩,
+  have hu := hl.map_units, have := exists_pow_eq_one (hu s).unit,
+  rcases (is_of_fin_order_iff_pow_eq_one _).1 this with ⟨n,hn,hp⟩,
+  rw [units.ext_iff, units.coe_pow] at hp, change algebra_map R L s ^ n = 1 at hp,
+  rw [← nat.succ_pred_eq_of_pos hn, pow_succ] at hp,
+  use r * s ^ (n-1), erw [map_mul, map_pow, ← h, mul_assoc, hp, mul_one],
+end
+
+lemma card_le : #L ≤ #R :=
+begin
+  classical, by_cases infinite R, resetI,
+  { erw [←cardinal.mul_eq_self $ cardinal.omega_le_mk R],
+    let f : R × R → L := λ aa, is_localization.mk' _ aa.1 (if h : aa.2 ∈ S then ⟨aa.2, h⟩ else 1),
+    refine @cardinal.mk_le_of_surjective _ _ f (λ a, _),
+    obtain ⟨x, y, h⟩ := is_localization.mk'_surjective S a,
+    use (x, y),
+    dsimp [f],
+    rwa [dif_pos $ show ↑y ∈ S, from y.2, set_like.eta] },
+  haveI : fintype R := fintype_of_not_infinite h,
+  exact cardinal.mk_le_of_surjective (algebra_map_surj S),
+end
+
 /-- All infinite localizations of rings at submonoids smaller than the non-zero divisors have the
 same cardinality as the base ring. -/
 lemma cardinal_mk {R : Type u} [comm_ring R] {S : submonoid R} (L : Type u) [comm_ring L]

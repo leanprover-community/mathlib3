69e8437a90928adccd279c5671929e3aa2975e0e
mcdoll <moritz.doll@googlemail.com>
2022-03-03 21:42:51 +0100

    Update src/linear_algebra/matrix/bilinear_form.lean
    Co-authored-by: Eric Wieser <wieser.eric@gmail.com>

--- src/linear_algebra/matrix/bilinear_form.lean
+++ src/linear_algebra/matrix/bilinear_form.lean
@@ -1,7 +1,7 @@
 /-
-Copyright (c) 2018 Andreas Swerdlow. All rights reserved.
+Copyright (c) 2020 Anne Baanen. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
-Authors: Andreas Swerdlow, Kexing Ying
+Authors: Anne Baanen, Kexing Ying
 -/
 
 import linear_algebra.matrix.basis

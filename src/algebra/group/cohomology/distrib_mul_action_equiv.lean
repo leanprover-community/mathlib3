02fafa25036f56e7c67f7540156661ed8e7fb323
101damnations <101damnations@github.com>
2022-03-03 15:24:15 +0000

    adding some old API

---
+++ src/algebra/group/cohomology/distrib_mul_action_equiv.lean
@@ -0,0 +1,217 @@
+import algebra.group_action_hom
+import algebra.module.ulift
+
+variables (M : Type*) [monoid M]
+variables (A : Type*) [add_monoid A] [distrib_mul_action M A]
+variables (A' : Type*) [add_group A'] [distrib_mul_action M A']
+variables (B : Type*) [add_monoid B] [distrib_mul_action M B]
+variables (B' : Type*) [add_group B'] [distrib_mul_action M B']
+variables (C : Type*) [add_monoid C] [distrib_mul_action M C]
+variables (R : Type*) [semiring R] [mul_semiring_action M R]
+variables (R' : Type*) [ring R'] [mul_semiring_action M R']
+variables (S : Type*) [semiring S] [mul_semiring_action M S]
+variables (S' : Type*) [ring S'] [mul_semiring_action M S']
+variables (T : Type*) [semiring T] [mul_semiring_action M T]
+variables (G : Type*) [group G] (H : subgroup G)
+
+set_option old_structure_cmd true
+
+@[ancestor add_equiv distrib_mul_action_hom]
+structure distrib_mul_action_equiv extends equiv A B, distrib_mul_action_hom M A B
+
+notation A ` ≃+[`:25 M:25 `] `:0 B:0 := distrib_mul_action_equiv M A B
+
+namespace distrib_mul_action_equiv
+
+instance : has_coe_to_fun (A ≃+[M] B) _ :=
+⟨distrib_mul_action_equiv.to_fun⟩
+
+variables {M A B}
+
+def to_add_equiv (f : A ≃+[M] B) : A ≃+ B :=
+{ map_add' := f.map_add',
+  ..f.to_equiv }
+
+@[simp]
+lemma to_fun_eq_coe {f : A ≃+[M] B} : f.to_fun = f := rfl
+
+@[simp]
+lemma coe_to_equiv {f : A ≃+[M] B} : ⇑f.to_equiv = f := rfl
+
+@[simp]
+lemma coe_to_distrib_mul_action_hom {f : A ≃+[M] B} : ⇑f.to_distrib_mul_action_hom = f := rfl
+
+@[simp]
+lemma coe_to_add_equiv {f : A ≃+[M] B} :  ⇑f.to_add_equiv = f := rfl
+
+@[simp]
+lemma map_add (f : A ≃+[M] B) : ∀ x y, f (x + y) = f x + f y := f.map_add'
+
+@[simp]
+lemma map_smul (f : A ≃+[M] B) : ∀ (r : M) x, f (r • x) = r • f x := f.map_smul'
+
+@[simp]
+lemma map_zero (h : A ≃+[M] B) : h 0 = 0 := h.map_zero'
+
+@[simp]
+lemma map_eq_zero_iff (h : A ≃+[M] B) {x : A} :
+  h x = 0 ↔ x = 0 :=
+h.map_zero ▸ h.to_equiv.apply_eq_iff_eq
+
+def mk' (f : A ≃ B) (hsmul : ∀ (r : M) x, f (r • x) = r • f x)
+  (hadd : ∀ x y, f (x + y) = f x + f y) : A ≃+[M] B :=
+⟨f.1, f.2, f.3, f.4, hsmul,
+begin
+  rw [←add_zero (f.1 0), ←f.apply_symm_apply 0],
+  show f 0 + _ = _,
+  rw [←hadd, zero_add],
+end,
+  hadd⟩
+
+protected lemma bijective (e : A ≃+[M] B) : function.bijective e := e.to_equiv.bijective
+
+protected lemma injective (e : A ≃+[M] B) : function.injective e := e.to_equiv.injective
+
+protected lemma surjective (e : A ≃+[M] B) : function.surjective e := e.to_equiv.surjective
+
+variables (M A)
+@[refl]
+def refl : A ≃+[M] A :=
+{ map_smul' := λ _ _, rfl,
+  map_zero' := rfl,
+  map_add' := λ _ _, rfl,
+  ..equiv.refl _ }
+
+instance : inhabited (A ≃+[M] A) := ⟨refl M A⟩
+
+variables {M A}
+
+@[symm]
+def symm (h : A ≃+[M] B) : B ≃+[M] A :=
+{ map_smul' := (h.to_distrib_mul_action_hom.inverse h.to_equiv.symm h.left_inv h.right_inv).map_smul,
+  map_zero' := (h.to_distrib_mul_action_hom.inverse h.to_equiv.symm h.left_inv h.right_inv).map_zero,
+  map_add' := (h.to_distrib_mul_action_hom.inverse h.to_equiv.symm h.left_inv h.right_inv).map_add,
+  .. h.to_equiv.symm }
+
+--/-- See Note [custom simps projection] -/
+def simps.symm_apply (e : A ≃+[M] B) : B → A := e.symm
+
+initialize_simps_projections distrib_mul_action_equiv (to_fun → apply, inv_fun → symm_apply)
+
+@[simp]
+theorem to_equiv_symm (f : A ≃+[M] B) : f.symm.to_equiv = f.to_equiv.symm := rfl
+
+@[simp]
+theorem coe_mk (f : A → B) (g h₁ h₂) (h₃ : ∀ (r : M) x, f (r • x) = r • f x) (h₄ h₅) :
+  ⇑(distrib_mul_action_equiv.mk f g h₁ h₂ h₃ h₄ h₅) = f := rfl
+
+@[simp]
+lemma symm_symm : ∀ (f : A ≃+[M] B), f.symm.symm = f
+| ⟨f, g, h₁, h₂, h₃, h₄, h₅⟩ := rfl
+
+lemma symm_bijective : function.bijective (symm : (A ≃+[M] B) → (B ≃+[M] A)) :=
+equiv.bijective ⟨symm, symm, symm_symm, symm_symm⟩
+
+@[simp]
+theorem symm_mk (f : A → B) (g h₁ h₂) (h₃ : ∀ (r : M) x, f (r • x) = r • f x) (h₄ h₅) :
+  (distrib_mul_action_equiv.mk f g h₁ h₂ h₃ h₄ h₅).symm =
+  { to_fun := g, inv_fun := f, ..(distrib_mul_action_equiv.mk f g h₁ h₂ h₃ h₄ h₅).symm} := rfl
+
+variables {C}
+@[trans]
+def trans (h1 : A ≃+[M] B) (h2 : B ≃+[M] C) : (A ≃+[M] C) :=
+{ map_smul' := λ r x, show h2 (h1 (r • x)) = r • h2 (h1 x),
+    by rw [h1.map_smul, h2.map_smul],
+  map_zero' := show h2 (h1 0) = 0, by rw [h1.map_zero, h2.map_zero],
+  map_add' := λ x y, show h2 (h1 (x + y)) = h2 (h1 x) + h2 (h1 y),
+    by rw [h1.map_add, h2.map_add],
+  ..h1.to_equiv.trans h2.to_equiv }
+
+@[simp]
+lemma apply_symm_apply (e : A ≃+[M] B) : ∀ y, e (e.symm y) = y :=
+e.to_equiv.apply_symm_apply
+
+@[simp]
+lemma symm_apply_apply (e : A ≃+[M] B) : ∀ x, e.symm (e x) = x :=
+e.to_equiv.symm_apply_apply
+
+@[simp]
+theorem symm_comp_self (e : A ≃+[M] B) : e.symm ∘ e = id := funext e.symm_apply_apply
+
+@[simp]
+theorem self_comp_symm (e : A ≃+[M] B) : e ∘ e.symm = id := funext e.apply_symm_apply
+
+@[simp]
+theorem coe_refl : ⇑(refl M A) = id := rfl
+
+
+theorem refl_apply (m : A) : refl M A m = m := rfl
+
+@[simp]
+theorem coe_trans (e₁ : A ≃+[M] B) (e₂ : B ≃+[M] C) : ⇑(e₁.trans e₂) = e₂ ∘ e₁ := rfl
+
+
+theorem trans_apply (e₁ : A ≃+[M] B) (e₂ : B ≃+[M] C) (m : A) : e₁.trans e₂ m = e₂ (e₁ m) := rfl
+
+@[simp] theorem apply_eq_iff_eq (e : A ≃+[M] B) {x y : A} : e x = e y ↔ x = y :=
+e.injective.eq_iff
+
+lemma apply_eq_iff_symm_apply (e : A ≃+[M] B) {x : A} {y : B} : e x = y ↔ x = e.symm y :=
+e.to_equiv.apply_eq_iff_eq_symm_apply
+
+lemma symm_apply_eq (e : A ≃+[M] B) {x y} : e.symm x = y ↔ x = e y :=
+e.to_equiv.symm_apply_eq
+
+lemma eq_symm_apply (e : A ≃+[M] B) {x y} : y = e.symm x ↔ e y = x :=
+e.to_equiv.eq_symm_apply
+
+@[ext]
+lemma ext {f g : A ≃+[M] B} (h : ∀ x, f x = g x) : f = g :=
+begin
+  have h₁ : f.to_equiv = g.to_equiv := equiv.ext h,
+  cases f, cases g, congr,
+  { exact (funext h) },
+  { exact congr_arg equiv.inv_fun h₁ }
+end
+
+lemma ext_iff {f g : A ≃+[M] B} : f = g ↔ ∀ x, f x = g x :=
+⟨λ h x, h ▸ rfl, ext⟩
+
+@[simp] lemma mk_coe (e : A ≃+[M] B) (e' h₁ h₂ h₃ h₄ h₅) :
+  (⟨e, e', h₁, h₂, h₃, h₄, h₅⟩ : A ≃+[M] B) = e := ext $ λ _, rfl
+
+@[simp] lemma mk_coe' (e : A ≃+[M] B) (f h₁ h₂ h₃ h₄ h₅) :
+  (distrib_mul_action_equiv.mk f ⇑e h₁ h₂ h₃ h₄ h₅ : B ≃+[M] A) = e.symm :=
+symm_bijective.injective $ ext $ λ x, rfl
+
+protected lemma congr_arg {f : A ≃+[M] B} : Π {x x' : A}, x = x' → f x = f x'
+| _ _ rfl := rfl
+
+protected lemma congr_fun {f g : A ≃+[M] B} (h : f = g) (x : A) : f x = g x := h ▸ rfl
+
+-- not sure I should have all these instance variables
+def distrib_mul_action_equiv_of_unique_of_unique
+  [unique A] [unique B] : A ≃+[M] B :=
+{ map_smul' := λ _ _ , subsingleton.elim _ _,
+  map_zero' := subsingleton.elim _ _,
+  map_add' := λ _ _, subsingleton.elim _ _,
+  ..equiv_of_unique_of_unique }
+
+instance [unique A] [unique B] : unique (A ≃+[M] B) :=
+{ default := distrib_mul_action_equiv_of_unique_of_unique ,
+  uniq := λ _, ext $ λ x, subsingleton.elim _ _}
+
+end distrib_mul_action_equiv
+namespace ulift
+
+def distrib_mul_action_equiv : ulift A ≃+[M] A :=
+{ to_fun := ulift.down,
+  inv_fun := ulift.up,
+  map_zero' := rfl,
+  map_add' := λ x y, rfl,
+  map_smul' := λ m x, rfl,
+  left_inv := ulift.up_down,
+  right_inv := ulift.down_up }
+
+
+end ulift
---
+++ src/algebra/group/cohomology/sub_distrib_mul_action.lean
@@ -0,0 +1,285 @@
+import tactic
+import algebra.group_action_hom
+import group_theory.group_action.sub_mul_action
+import algebra.homology.exact
+import algebra.module.pi
+import algebra.group.cohomology.distrib_mul_action_equiv
+import group_theory.quotient_group
+
+universes v u w u₁ u₂ u₃
+
+set_option old_structure_cmd true
+
+structure sub_distrib_mul_action (G : Type u)
+  (M : Type v)
+  [monoid G] [add_comm_group M]
+  [distrib_mul_action G M]
+extends sub_mul_action G M, add_subgroup M
+
+namespace sub_distrib_mul_action
+variables {G : Type u} [monoid G]
+variables {M : Type v} [add_comm_group M] [distrib_mul_action G M]
+variables {N : Type w} [add_comm_group N] [distrib_mul_action G N]
+
+instance : set_like (sub_distrib_mul_action G M) M :=
+⟨carrier, λ p q h, by cases p; cases q; congr'⟩
+
+@[ext] lemma ext {A B : sub_distrib_mul_action G M}
+  (h : ∀ m : M, m ∈ A ↔ m ∈ B) : A = B := set_like.ext h
+
+theorem ext_iff {A B : sub_distrib_mul_action G M} :
+  A = B ↔ ∀ m : M, m ∈ A ↔ m ∈ B :=
+set_like.ext_iff
+
+variables (A : sub_distrib_mul_action G M)
+
+lemma zero_mem : (0 : M) ∈ A :=
+A.zero_mem'
+
+lemma add_mem {a b : M} (ha : a ∈ A) (hb : b ∈ A) :
+  a + b ∈ A :=
+A.add_mem' ha hb
+
+lemma smul_mem (g : G) {a : M} (ha : a ∈ A) :
+  g • a ∈ A :=
+A.smul_mem' g ha
+
+instance fuckoff : has_coe A M :=
+by apply_instance
+
+lemma neg_mem {a : M} (ha : a ∈ A) :
+  -a ∈ A :=
+A.neg_mem' ha
+
+instance to_add_comm_group (N : sub_distrib_mul_action G M) :
+  add_comm_group N :=
+add_subgroup.to_add_comm_group N.to_add_subgroup
+
+instance to_distrib_mul_action (N : sub_distrib_mul_action G M) :
+  distrib_mul_action G N :=
+{ smul := λ g n, ⟨g • (n : M), N.smul_mem g n.2⟩,
+  one_smul := λ n, by ext; exact one_smul G n,
+  mul_smul := λ g h n, by ext; exact mul_smul _ _ _,
+  smul_add := λ g m n, by ext; exact smul_add _ _ _,
+  smul_zero := λ g, by ext; exact smul_zero _ }
+
+variables {A}
+
+@[simp] lemma coe_add (a b : A) :
+  ((a + b : A) : M) = a + b := rfl
+
+@[simp] lemma coe_zero : ((0 : A) : M) = 0 := rfl
+
+@[simp] lemma coe_neg (a : A) :
+  ((-a : A) : M) = -a := rfl
+
+@[simp] lemma coe_sub (a b : A) :
+  ((a - b : A) : M) = a - b := rfl
+
+@[simp] lemma coe_smul (g : G) (a : A) :
+  ((g • a : A) : M) = g • (a : M) := rfl
+
+variables (A)
+def subtype : A →+[G] M :=
+{ to_fun := coe,
+  map_smul' := coe_smul,
+  map_zero' := coe_zero,
+  map_add' := coe_add }
+
+def quotient := M ⧸ A.to_add_subgroup
+
+instance : add_comm_group A.quotient :=
+by unfold quotient; apply_instance
+
+instance : distrib_mul_action G A.quotient :=
+{ smul := λ g m, quotient.lift_on' m (λ x, quotient_add_group.mk' A.to_add_subgroup (g • x))
+  (λ x y (h : _ + _ ∈ _), quotient.eq'.2 $ show _ + _ ∈ _, by
+    { rw neg_add_eq_sub at *,
+      rw ←smul_sub,
+      exact A.smul_mem _ h }),
+  one_smul := λ m, quotient.induction_on' m $ λ x, show quotient.mk' _ = _, by rw one_smul,
+  mul_smul := λ g h m, quotient.induction_on' m $ λ x,
+    show quotient.mk' _ = quotient.mk' _, by rw mul_smul,
+  smul_add := λ g m, quotient.induction_on' m $ λ x n, quotient.induction_on' n $ λ y,
+    show quotient_add_group.mk' _ _ = _, by rw [smul_add, add_monoid_hom.map_add]; refl,
+  smul_zero := λ g, by
+  { rw ←(quotient_add_group.mk' A.to_add_subgroup).map_zero,
+    show quotient_add_group.mk' A.to_add_subgroup (g • 0) = 0,
+    rw [smul_zero, add_monoid_hom.map_zero] }}
+
+def mkq : M →+[G] A.quotient :=
+{ map_smul' := λ g x, rfl,
+  .. quotient_add_group.mk' A.to_add_subgroup }
+
+lemma mkq_eq_zero_iff {x : M} : A.mkq x = 0 ↔ x ∈ A :=
+quotient_add_group.eq_zero_iff x
+
+instance : has_top (sub_distrib_mul_action G M) :=
+{ top := by refine
+  { carrier := set.univ,
+    smul_mem' := _,
+    zero_mem' := _,
+    add_mem' := _,
+    neg_mem' := _ }; { intros, trivial }}
+
+instance : has_bot (sub_distrib_mul_action G M) :=
+{ bot :=
+  { carrier := {0},
+    smul_mem' := λ g x (hx : x = 0), show _ = _, by rw [hx, smul_zero],
+    zero_mem' := rfl,
+    add_mem' := λ x y (hx : x = 0) (hy : y = 0), show _ = _, by rw [hx, hy, zero_add],
+    neg_mem' := λ x (hx : x = 0), show _ = _, by rw [hx, neg_zero] }}
+
+end sub_distrib_mul_action
+
+namespace distrib_mul_action
+
+variables {G : Type u} [monoid G] {M : Type v} [add_comm_group M] [distrib_mul_action G M]
+
+-- ugh can't find these
+lemma nsmul_comm (n : ℕ) (g : G) (m : M) :
+  n • g • m = g • n • m :=
+begin
+  induction n with n hn,
+  { simp only [zero_smul, smul_zero] },
+  { simp only [succ_nsmul, hn, smul_add], }
+end
+
+lemma gsmul_comm (n : ℤ) (g : G) (m : M) :
+  n • g • m = g • n • m :=
+begin
+  induction n with n n,
+  { simp only [nsmul_comm, int.of_nat_eq_coe, coe_nat_zsmul]},
+  { simp only [zsmul_neg_succ_of_nat, nsmul_comm, smul_neg] }
+end
+
+end distrib_mul_action
+
+variables {G : Type u} [monoid G] {α : Type u₁}
+
+def pi.eval_distrib_mul_action_hom (M : α → Type*) [Π i, add_comm_group (M i)]
+  [Π i, distrib_mul_action G (M i)] (i : α) : (Π i, M i) →+[G] M i :=
+{ map_smul' := λ g m, by simp only [pi.eval_add_monoid_hom_apply, add_monoid_hom.to_fun_eq_coe, pi.smul_apply],
+  .. pi.eval_add_monoid_hom M i }
+
+namespace distrib_mul_action_hom
+
+variables {M : Type v} [add_comm_group M] [distrib_mul_action G M]
+variables {N : Type w} [add_comm_group N] [distrib_mul_action G N]
+
+lemma map_nsmul (f : M →+[G] N) (m : M) (n : ℕ) :
+  f (n • m) = n • f m :=
+f.to_add_monoid_hom.map_nsmul m n
+
+lemma map_gsmul (f : M →+[G] N) (m : M) (n : ℤ) :
+  f (n • m) = n • f m :=
+f.to_add_monoid_hom.map_zsmul m n
+
+lemma map_sum (g : M →+[G] N) (f : α → M) (s : finset α) :
+  g (s.sum f) = s.sum (λ x, g (f x)) :=
+g.to_add_monoid_hom.map_sum f s
+
+-- can't find the add_monoid_hom version of this?
+def pi (M : α → Type*) [Π i, add_comm_group (M i)] [Π i, distrib_mul_action G (M i)]
+  (F : Π i, N →+[G] M i) : N →+[G] (Π i, M i) :=
+{ to_fun := λ x i, F i x,
+  map_smul' := by { intros, ext, rw map_smul, refl },
+  map_zero' := by { intros, ext, rw map_zero, refl },
+  map_add' := by { intros, ext, rw map_add, refl }}
+
+def ker (φ : M →+[G] N) :
+  sub_distrib_mul_action G M :=
+by { refine_struct {carrier := {m : M | φ m = 0}},
+  all_goals { intros, simp only [*, set.mem_set_of_eq] at * },
+  { simp only [*, smul_zero, map_smul] },
+  { exact map_zero _ },
+  { simp only [*, map_add, add_zero] },
+  { simp only [*, map_neg, neg_zero] }}
+
+def cod_restrict (A : sub_distrib_mul_action G N) (f : M →+[G] N) (h : ∀ m, f m ∈ A) :
+  M →+[G] A :=
+{ to_fun := λ m, ⟨f m, h m⟩,
+  map_smul' := by { intros, congr, rw map_smul, refl },
+  map_zero' := by { congr, rw map_zero },
+  map_add' := by { intros, congr, rw map_add }}
+
+@[simp] lemma cod_restrict_apply  (A : sub_distrib_mul_action G N) {f : M →+[G] N}
+  {h : ∀ m, f m ∈ A} {x : M} :
+  (f.cod_restrict A h x : N) = f x :=
+rfl
+
+variable (φ : M →+[G] N)
+
+@[simp] lemma mem_ker {m : M} : m ∈ φ.ker ↔ φ m = 0 := iff.rfl
+
+lemma ker_zero : ker (0 : M →+[G] N) = ⊤ :=
+sub_distrib_mul_action.ext $ λ x, ⟨λ h, trivial, λ h, rfl⟩
+
+def range (φ : M →+[G] N) :
+  sub_distrib_mul_action G N :=
+by { refine_struct {carrier := set.range φ},
+  { rintros c x ⟨y, hy⟩,
+    exact ⟨c • y, by rw [map_smul, hy]⟩ },
+  { exact ⟨0, map_zero _⟩ },
+  { rintros a b ⟨x, hx⟩ ⟨y, hy⟩,
+    exact ⟨x + y, by rw [map_add, hx, hy]⟩ },
+  { rintros x ⟨y, hy⟩,
+    exact ⟨-y, by rw [map_neg, hy]⟩ }}
+
+lemma range_coe (n : N) : n ∈ (set.range (⇑φ)) ↔ n ∈ φ.range := iff.rfl
+@[simp] lemma mem_range (n : N) : n ∈ (set.range (⇑φ)) ↔ ∃ m : M, φ m = n := iff.rfl
+@[simp] lemma mem_range_self {m : M} : φ m ∈ φ.range := ⟨m, rfl⟩
+
+variables {P : Type u₂} [add_comm_group P] [distrib_mul_action G P]
+lemma range_le_ker_iff {g : N →+[G] P} : φ.range ≤ g.ker ↔ g.comp φ = 0 :=
+⟨λ h, ext $ λ x, h φ.mem_range_self, λ h x ⟨y, hy⟩, by rw ←hy; exact ext_iff.1 h y⟩
+
+lemma range_eq_top_iff : φ.range = ⊤ ↔ function.surjective φ :=
+⟨λ h y, show y ∈ φ.range, by rw h; trivial, λ h, sub_distrib_mul_action.ext $ λ x,
+  ⟨λ h, trivial, λ hx, h x⟩⟩
+
+lemma range_mkq_comp : φ.range.mkq.comp φ = 0 :=
+ext $ λ x, φ.range.mkq_eq_zero_iff.2 φ.mem_range_self
+
+noncomputable def equiv_of_bijective (h : function.bijective φ) : M ≃+[G] N :=
+{ inv_fun := λ n, classical.some (h.2 n),
+  left_inv := sorry,
+  right_inv := sorry, .. φ }
+
+
+end distrib_mul_action_hom
+
+namespace sub_distrib_mul_action
+variables {M : Type v} {N : Type w} [add_comm_group M] [add_comm_group N] [distrib_mul_action G M]
+  [distrib_mul_action G N] (A : sub_distrib_mul_action G M)
+
+def lift (f : M →+[G] N) (h : A ≤ f.ker) : A.quotient →+[G] N :=
+{ map_smul' := λ g x, quotient.induction_on' x $ λ m, show _ = g • f m, by
+  { rw ←f.map_smul, refl },
+  .. quotient_add_group.lift A.to_add_subgroup f.to_add_monoid_hom h }
+
+lemma lift_mkq (f : M →+[G] N) (h : A ≤ f.ker) {m : M} :
+  A.lift f h (A.mkq m) = f m := rfl
+
+lemma ker_mkq : A.mkq.ker = A :=
+ext $ λ x, A.mkq_eq_zero_iff
+
+lemma range_mkq : A.mkq.range = ⊤ :=
+A.mkq.range_eq_top_iff.2 $ λ y, quotient.induction_on' y $ λ x, ⟨x, rfl⟩
+
+end sub_distrib_mul_action
+
+namespace distrib_mul_action_hom
+
+variables {M : Type v} {N : Type w} [add_comm_group M] [add_comm_group N] [distrib_mul_action G M]
+  [distrib_mul_action G N]
+
+noncomputable def quot_ker_equiv_range (f : M →+[G] N) : f.ker.quotient ≃+[G] f.range :=
+(f.ker.lift (f.cod_restrict f.range $ λ _, f.mem_range_self) $
+  λ x hx, by ext; exact hx).equiv_of_bijective sorry
+
+noncomputable def quot_ker_equiv_of_surjective (f : M →+[G] N) (hf : function.surjective f) :
+  f.ker.quotient ≃+[G] N :=
+(f.ker.lift f (λ x, id)).equiv_of_bijective sorry
+
+end distrib_mul_action_hom

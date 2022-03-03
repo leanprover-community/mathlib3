73d661ba003d176d0dddf5ab2f4c97a33d650e23
mcdoll <moritz.doll@googlemail.com>
2022-03-03 20:42:18 +0100

    Update src/linear_algebra/bilinear_form.lean
    Co-authored-by: Eric Wieser <wieser.eric@gmail.com>

--- src/linear_algebra/bilinear_form.lean
+++ src/linear_algebra/bilinear_form.lean
@@ -5,7 +5,7 @@ Authors: Andreas Swerdlow, Kexing Ying
 -/
 
 import linear_algebra.dual
-import linear_algebra.matrix.basis
+import linear_algebra.matrix.to_lin
 import linear_algebra.tensor_product
 
 /-!

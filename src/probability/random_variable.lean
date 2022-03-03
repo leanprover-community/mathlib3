00e3c309915f14d29fbda8aef6b988d6f1944cd0
Rishikesh Vaishnav <rishhvaishnav@gmail.com>
2022-03-03 06:03:50 +0000

    doc

--- src/probability/random_variable.lean
+++ src/probability/random_variable.lean
@@ -12,16 +12,17 @@ import data.set.pi
 This file defines joint and marginal distributions on random variables.
 
 Given an event space `α`, an arbitrary (not necessarily countable) index type `ι`, 
-an index of types `β : ι → Type*` each with their own measurable space, and an index
-of random variables `f : Π (i : ι), α → (β i)`, we define the joint distribution of
-`μ : measure α` as the distribution on `Π (i : ι), β i` induced by `f` via `μ`.
+an index of types `β : ι → Type*` each with their own measurable space, an index
+of random variables `f : Π (i : ι), α → (β i)` and a measure `μ : measure α`,
+we define the joint distribution `joint μ f` as the distribution on `Π (i : ι), β i`
+induced by `f` via `μ`.
 
 For a set of "marginalizing variable" indices `mv : set ι`, we define the
-marginal distribution on `s` as the distribution on `Π (i : mv), β i` induced by
-the restriction of `f` to `mv`.
+marginal distribution `marginal μ f mv` as the distribution on `Π (i : mv), β i` induced by
+the restriction of `f` to `mv` via `μ`.
 
-We also define a generic marginalization on a measure on `Π (i : ι), β i` to an
-index subset `mv`.
+We also define a generic marginalization on a measure `μ : measure (Π (i : ι), β i)` to
+an index subset `mv` (yielding a `measure (Π (i : mv), β i)` as `marginalize μ mv`.
 
 ## Notations
 
@@ -37,8 +38,8 @@ In both cases `S` can be omitted from the brackets to be inferred.
 
 * `marginal_eq_marginalize_joint`: the marginal distribution is the marginalized joint distribution.
 * `marginal_apply`: the marginal distribution's measure of a set `s : Π (i : mv), β i`
-is the joint distribution's measure of that same set, extended to allow the
-unmarginalized variables to take any value.
+is the joint distribution's measure of that same set, extended to allow the unmarginalized variables
+to take any value.
 
 ## Tags
 

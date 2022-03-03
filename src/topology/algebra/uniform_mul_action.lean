451a6257bf0d0258ba6637aaa428c2657a1b8f25
Eric Wieser <wieser.eric@gmail.com>
2022-03-03 17:09:23 +0000

    chore(topology/algebra/uniform_mul_action): add `has_uniform_continuous_const_smul Mᵐᵒᵖ X` instance
    This matches `has_continuous_const_smul.op`

--- src/topology/algebra/uniform_mul_action.lean
+++ src/topology/algebra/uniform_mul_action.lean
@@ -47,6 +47,17 @@ lemma uniform_continuous.const_smul [has_uniform_continuous_const_smul M X]
   uniform_continuous (c • f) :=
 (uniform_continuous_const_smul c).comp hf
 
+/-- If a scalar is central, then its right action is uniform continuous when its left action is. -/
+@[priority 100]
+instance has_uniform_continuous_const_smul.op [has_scalar Mᵐᵒᵖ X] [is_central_scalar M X]
+  [has_uniform_continuous_const_smul M X] :
+  has_uniform_continuous_const_smul Mᵐᵒᵖ X :=
+⟨mul_opposite.rec $ λ c, begin
+  change uniform_continuous (λ m, mul_opposite.op c • m),
+  simp_rw op_smul_eq_smul,
+  exact uniform_continuous_const_smul c,
+end⟩
+
 end has_scalar
 
 @[to_additive] instance uniform_group.to_has_uniform_continuous_const_smul

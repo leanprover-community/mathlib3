30cb480a7959c2bf70ae02c32e9e03c312d60992
Bolton Bailey <bolton.bailey@gmail.com>
2022-03-03 21:18:21 -0600

    binpow things

--- src/number_theory/miller_rabin.lean
+++ src/number_theory/miller_rabin.lean
@@ -31,9 +31,30 @@ a^(odd_part (n-1)) = 1 ∨ (∃ r : ℕ, r < padic_val_nat 2 (n-1) ∧ a^(2^r * 
 
 instance {n : ℕ} {a : zmod n} : decidable (strong_probable_prime n a) := or.decidable
 
--- TODO(Bolton): Find a way of making modular exponentiation faster
+def binpow {M} [has_one M] [has_mul M] (m : M) : ℕ → M :=
+nat.binary_rec 1 (λ b _ ih, let ih2 := ih * ih in cond b (m * ih2) ih2)
 
-#eval to_bool (strong_probable_prime 1000007 4)
+def fast_strong_probable_prime (n : nat) (a : zmod n) : Prop :=
+binpow a (odd_part (n-1)) = 1
+∨ (∃ r : ℕ, r < padic_val_nat 2 (n-1) ∧ binpow a (2^r * odd_part(n-1)) = -1)
+
+instance {n : ℕ} {a : zmod n} : decidable (fast_strong_probable_prime n a) := or.decidable
+
+lemma strong_probable_prime_iff_fast_strong_probable_prime (n : nat) (a : zmod n) :
+  strong_probable_prime n a ↔ fast_strong_probable_prime n a :=
+begin
+  unfold strong_probable_prime,
+  unfold fast_strong_probable_prime,
+  sorry,
+end
+
+-- TODO(Bolton): Find a way of making modular exponentiation faster
+-- set_option profiler true
+-- #eval to_bool (fast_strong_probable_prime 100003 2)
+-- #eval to_bool (fast_strong_probable_prime 1000003 2)
+-- #eval to_bool (nat.prime 1000003)
+-- #eval to_bool (fast_strong_probable_prime 99999997 4)
+-- #eval to_bool (nat.prime 104)
 
 def fermat_pseudoprime (n : nat) (a : zmod n) : Prop :=
 a^(n-1) = 1

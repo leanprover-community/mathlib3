cfad91405b725ae764fcb4c8078354e15a47659a
Violeta Hernández <vi.hdz.p@gmail.com>
2022-03-04 10:38:49 -0600

    Added surname

--- src/measure_theory/card_measurable_space.lean
+++ src/measure_theory/card_measurable_space.lean
@@ -1,7 +1,7 @@
 /-
 Copyright (c) 2022 Sébastien Gouëzel. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
-Authors: Sébastien Gouëzel, Violeta Hernández
+Authors: Sébastien Gouëzel, Violeta Hernández Palacios
 -/
 import measure_theory.measurable_space_def
 import set_theory.continuum

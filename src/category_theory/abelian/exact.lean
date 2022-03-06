c4e2d18364297a627ec252d30b2035e5c7501828
Jujian Zhang <jujian.zhang1998@outlook.com>
2022-03-06 22:26:18 +0000

    Update exact.lean

--- src/category_theory/abelian/exact.lean
+++ src/category_theory/abelian/exact.lean
@@ -1,7 +1,7 @@
 /-
 Copyright (c) 2020 Markus Himmel. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
-Authors: Markus Himmel
+Authors: Markus Himmel, Adam Topaz
 -/
 import category_theory.abelian.opposite
 import algebra.homology.exact

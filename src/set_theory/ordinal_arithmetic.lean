37783539844ceab9902b11a2fd95a298e9688eac
Violeta Hernández <vi.hdz.p@gmail.com>
2022-03-07 10:15:41 +0000

    feat(set_theory/ordinal_arithmetic): `enum_ord univ = id` (#12391)
    Co-authored-by: Johan Commelin <johan@commelin.net>

--- src/set_theory/ordinal_arithmetic.lean
+++ src/set_theory/ordinal_arithmetic.lean
@@ -1466,6 +1466,24 @@ lemma enum_ord_def_nonempty (hS : unbounded (<) S) {o} :
   {x | x ∈ S ∧ ∀ c, c < o → enum_ord S c < x}.nonempty :=
 (⟨_, enum_ord_mem hS o, λ _ b, enum_ord.strict_mono hS b⟩)
 
+@[simp] theorem enum_ord_range {f : ordinal → ordinal} (hf : strict_mono f) :
+  enum_ord (range f) = f :=
+funext (λ o, begin
+  apply wf.induction o,
+  intros a H,
+  rw enum_ord_def a,
+  have Hfa : f a ∈ range f ∩ {b | ∀ c, c < a → enum_ord (range f) c < b} :=
+    ⟨mem_range_self a, λ b hb, (by {rw H b hb, exact hf hb})⟩,
+  refine (cInf_le' Hfa).antisymm ((le_cInf_iff'' ⟨_, Hfa⟩).2 _),
+  rintros _ ⟨⟨c, rfl⟩, hc : ∀ b < a, enum_ord (range f) b < f c⟩,
+  rw hf.le_iff_le,
+  contrapose! hc,
+  exact ⟨c, hc, (H c hc).ge⟩,
+end)
+
+@[simp] theorem enum_ord_univ : enum_ord set.univ = id :=
+by { rw ←range_id, exact enum_ord_range strict_mono_id }
+
 @[simp] theorem enum_ord_zero : enum_ord S 0 = Inf S :=
 by { rw enum_ord_def, simp [ordinal.not_lt_zero] }
 
@@ -1498,7 +1516,7 @@ def enum_ord.order_iso (hS : unbounded (<) S) : ordinal ≃o S :=
 strict_mono.order_iso_of_surjective (λ o, ⟨_, enum_ord_mem hS o⟩) (enum_ord.strict_mono hS)
   (λ s, let ⟨a, ha⟩ := enum_ord.surjective hS s s.prop in ⟨a, subtype.eq ha⟩)
 
-theorem enum_ord_range (hS : unbounded (<) S) : range (enum_ord S) = S :=
+theorem range_enum_ord (hS : unbounded (<) S) : range (enum_ord S) = S :=
 by { rw range_eq_iff, exact ⟨enum_ord_mem hS, enum_ord.surjective hS⟩ }
 
 /-- A characterization of `enum_ord`: it is the unique strict monotonic function with range `S`. -/
@@ -1507,9 +1525,9 @@ theorem eq_enum_ord (f : ordinal → ordinal) (hS : unbounded (<) S) :
 begin
   split,
   { rintro ⟨h₁, h₂⟩,
-    rwa [←wf.eq_strict_mono_iff_eq_range h₁ (enum_ord.strict_mono hS), enum_ord_range hS] },
+    rwa [←wf.eq_strict_mono_iff_eq_range h₁ (enum_ord.strict_mono hS), range_enum_ord hS] },
   { rintro rfl,
-    exact ⟨enum_ord.strict_mono hS, enum_ord_range hS⟩ }
+    exact ⟨enum_ord.strict_mono hS, range_enum_ord hS⟩ }
 end
 
 end

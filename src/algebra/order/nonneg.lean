521ebb68ec6cb08853139181d17937ffe8bc9714
Eric Wieser <wieser.eric@gmail.com>
2022-03-07 11:48:24 +0000

    protected

--- src/algebra/order/nonneg.lean
+++ src/algebra/order/nonneg.lean
@@ -193,7 +193,7 @@ def coe_ring_hom [ordered_semiring α] : {x : α // 0 ≤ x} →+* α :=
 ⟨coe, nonneg.coe_one, nonneg.coe_mul, nonneg.coe_zero, nonneg.coe_add⟩
 
 @[simp, norm_cast]
-lemma coe_nat_cast [ordered_semiring α] (n : ℕ) : ((↑n : {x : α // 0 ≤ x}) : α) = n :=
+protected lemma coe_nat_cast [ordered_semiring α] (n : ℕ) : ((↑n : {x : α // 0 ≤ x}) : α) = n :=
 map_nat_cast (coe_ring_hom : {x : α // 0 ≤ x} →+* α) n
 
 instance has_inv [linear_ordered_field α] : has_inv {x : α // 0 ≤ x} :=

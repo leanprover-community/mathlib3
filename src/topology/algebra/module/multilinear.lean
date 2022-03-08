07028ccbc98b2af1bfdfe934c3a42bd8603ee535
Eric Wieser <wieser.eric@gmail.com>
2022-03-08 00:11:04 +0000

    fix bad merge

--- src/topology/algebra/module/multilinear.lean
+++ src/topology/algebra/module/multilinear.lean
@@ -136,33 +136,6 @@ function.injective.mul_action to_multilinear_map to_multilinear_map_inj (λ _ _,
 
 end has_scalar
 
-section has_scalar
-
-variables {R' R'' A : Type*} [monoid R'] [monoid R''] [semiring A]
-  [Π i, module A (M₁ i)] [module A M₂]
-  [distrib_mul_action R' M₂] [has_continuous_const_smul R' M₂] [smul_comm_class A R' M₂]
-  [distrib_mul_action R'' M₂] [has_continuous_const_smul R'' M₂] [smul_comm_class A R'' M₂]
-
-instance : has_scalar R' (continuous_multilinear_map A M₁ M₂) :=
-⟨λ c f, { cont := f.cont.const_smul c, .. c • f.to_multilinear_map }⟩
-
-@[simp] lemma smul_apply (f : continuous_multilinear_map A M₁ M₂) (c : R') (m : Πi, M₁ i) :
-  (c • f) m = c • f m := rfl
-
-@[simp] lemma to_multilinear_map_smul (c : R') (f : continuous_multilinear_map A M₁ M₂) :
-  (c • f).to_multilinear_map = c • f.to_multilinear_map :=
-rfl
-
-instance [smul_comm_class R' R'' M₂] :
-  smul_comm_class R' R'' (continuous_multilinear_map A M₁ M₂) :=
-⟨λ c₁ c₂ f, ext $ λ x, smul_comm _ _ _⟩
-
-instance [has_scalar R' R''] [is_scalar_tower R' R'' M₂] :
-  is_scalar_tower R' R'' (continuous_multilinear_map A M₁ M₂) :=
-⟨λ c₁ c₂ f, ext $ λ x, smul_assoc _ _ _⟩
-
-end has_scalar
-
 section has_continuous_add
 variable [has_continuous_add M₂]
 

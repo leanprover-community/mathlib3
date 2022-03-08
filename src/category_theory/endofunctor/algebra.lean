fd85e33e3b8470a558d10be711d3bc428079354a
jlh <48520973+Jlh18@users.noreply.github.com>
2022-03-08 20:22:11 +0000

    change to 2022 my name

--- src/category_theory/endofunctor/algebra.lean
+++ src/category_theory/endofunctor/algebra.lean
@@ -1,5 +1,5 @@
 /-
-Copyright (c) 2019 Scott Morrison. All rights reserved.
+Copyright (c) 2022 Joseph Hua. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Scott Morrison, Bhavik Mehta, Johan Commelin, Reid Barton, Rob Lewis, Joseph Hua
 -/

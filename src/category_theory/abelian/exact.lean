210e046937b90134118a700e643881869273e9d4
Jujian Zhang <jujian.zhang1998@outlook.com>
2022-03-08 21:19:11 +0000

    skeleton

--- src/category_theory/abelian/exact.lean
+++ src/category_theory/abelian/exact.lean
@@ -112,8 +112,20 @@ lemma is_equivalence.exact_iff {D : Type u₁} [category.{v₁} D] [abelian D]
   (F : C ⥤ D) [is_equivalence F] :
   exact (F.map f) (F.map g) ↔ exact f g :=
 begin
-  haveI : preserves_limit (parallel_pair g 0) F := sorry,
-  haveI : preserves_colimit (parallel_pair f 0) F := sorry,
+  haveI : preserves_limit (parallel_pair g 0) F := ⟨λ c hc,
+    { lift := λ s, (functor.map_cone_map_cone_inv F s).inv.hom ≫
+        F.map (hc.lift (functor.map_cone_inv F s)),
+      uniq' :=  begin
+        sorry
+      end,
+      fac' := λ s j, begin
+        sorry,
+      end }⟩,
+  haveI : preserves_colimit (parallel_pair f 0) F := ⟨λ c hc,
+    { desc := λ s, F.map (hc.desc (functor.map_cocone_inv F s)) ≫
+        (functor.map_cocone_map_cocone_inv F s).hom.hom,
+      uniq' := sorry,
+      fac' := sorry }⟩,
   simp only [exact_iff, ← F.map_eq_zero_iff, F.map_comp, category.assoc,
     ← kernel_comparison_comp_π g F, ← ι_comp_cokernel_comparison f F],
   rw [is_iso.comp_left_eq_zero (kernel_comparison g F), ← category.assoc,

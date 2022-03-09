fe73ae8f3eba4605fb5cb95030e676c0e0e1bb74
Scott Morrison <scott.morrison@gmail.com>
2022-03-09 17:23:50 +1100

    progress?

--- src/analysis/normed_space/star/cdga.lean
+++ src/analysis/normed_space/star/cdga.lean
@@ -13,20 +13,44 @@ import algebra.category.Module.abelian
 
 universes u v
 
-def cast_hom (A : ℕ → Type*) [Π n, add_comm_monoid (A n)] {i j : ℕ} (h : i = j) : A i →+ A j :=
+section
+variables {ι : Type*} (A : ι → Type*) [Π n, comm_monoid (A n)] {i j k : ι}
+
+@[to_additive]
+def cast_monoid_hom (h : i = j) : A i →* A j :=
 { to_fun := cast (congr_arg A h),
-  map_zero' := by { cases h, refl, },
-  map_add' := λ x y, by { cases h, refl, }}
+  map_one' := by { cases h, refl, },
+  map_mul' := λ x y, by { cases h, refl, }}
+
+@[simp, to_additive]
+lemma cast_monoid_hom_cast_monoid_hom (h₁ : i = j) (h₂ : j = k) (x):
+  cast_monoid_hom A h₂ (cast_monoid_hom A h₁ x) = cast_monoid_hom A (h₁.trans h₂) x :=
+by { cases h₁, cases h₂, refl, }
+
+@[simp, to_additive]
+lemma cast_monoid_hom_refl {i} (x : A i) : cast_monoid_hom A (eq.refl _) x = x :=
+rfl
+
+end
+
+section
+variables (R : Type*) [semiring R]
+variables {ι : Type*} (A : ι → Type*) [Π n, add_comm_monoid (A n)] [Π n, module R (A n)] {i j k : ι}
+
+def cast_linear_map (h : i = j) : A i →ₗ[R] A j :=
+{ map_smul' := λ r x, by { cases h, refl, },
+  ..cast_add_monoid_hom A h }
 
 @[simp]
-lemma cast_hom_cast_hom (A : ℕ → Type*) [Π n, add_comm_monoid (A n)] {i j k : ℕ} (h₁ : i = j) (h₂ : j = k) (x):
-  cast_hom A h₂ (cast_hom A h₁ x) = cast_hom A (h₁.trans h₂) x :=
+lemma cast_linear_map_cast_linear_map (h₁ : i = j) (h₂ : j = k) (x):
+  cast_linear_map R A h₂ (cast_linear_map R A h₁ x) = cast_linear_map R A (h₁.trans h₂) x :=
 by { cases h₁, cases h₂, refl, }
 
 @[simp]
-lemma cast_hom_refl (A : ℕ → Type*) [Π n, add_comm_monoid (A n)] {i} (x : A i):
-  cast_hom A (eq.refl _) x = x :=
+lemma cast_linear_map_refl (x : A i) : cast_linear_map R A (eq.refl _) x = x :=
 rfl
+
+end
 
 class graded_monoid (A : ℕ → Type*) :=
 (one : A 0)
@@ -61,7 +85,7 @@ class graded_semiring (A : ℕ → Type*) [Π n, add_comm_monoid (A n)] extends 
 
 class graded_comm_ring (A : ℕ → Type*) [Π n, add_comm_group (A n)] extends graded_semiring A :=
 (mul_comm : ∀ {i j} {x : A i} {y : A j},
-  gmul x y = cast_hom A (add_comm _ _) ((-1 : ℤ)^(i * j) • gmul y x))
+  gmul x y = cast_add_monoid_hom A (add_comm _ _) ((-1 : ℤ)^(i * j) • gmul y x))
 
 class differential_graded (A : ℕ → Type*) [Π n, add_comm_monoid (A n)] :=
 (d : Π {i}, A i →+ A (i+1))
@@ -77,15 +101,15 @@ lemma d_squared {A : ℕ → Type*} [Π n, add_comm_group (A n)] [differential_g
 differential_graded.d_squared
 
 @[simp]
-lemma cast_hom_d {A : ℕ → Type*} [Π n, add_comm_group (A n)] [differential_graded A] {i j : ℕ} {x : A i}
+lemma cast_add_monoid_hom_d {A : ℕ → Type*} [Π n, add_comm_group (A n)] [differential_graded A] {i j : ℕ} {x : A i}
   (h : i + 1 = j + 1):
-  cast_hom A h (d x) = d (cast_hom A (nat.succ.inj h) x) :=
+  cast_add_monoid_hom A h (d x) = d (cast_add_monoid_hom A (nat.succ.inj h) x) :=
 by { cases h, refl, }
 
 class differential_graded_ring (A : ℕ → Type*) [Π n, add_comm_group (A n)]
   extends differential_graded A, graded_semiring A :=
 (d_mul : ∀ {i j} {x : A i} {y : A j},
-  d (gmul x y) = cast_hom A (by abel) (gmul (d x) y) + (-1 : ℤ)^i • (gmul x (d y) : A (i + (j+1))))
+  d (gmul x y) = cast_add_monoid_hom A (by abel) (gmul (d x) y) + (-1 : ℤ)^i • (gmul x (d y) : A (i + (j+1))))
 
 set_option old_structure_cmd true
 
@@ -97,7 +121,7 @@ section
 variables {A : ℕ → Type*} [Π n, add_comm_group (A n)] [differential_graded_ring A]
 
 lemma d_mul {i j} (x : A i) (y : A j) :
-  d (gmul x y) = cast_hom A (by abel) (gmul (d x) y) + (-1 : ℤ)^i • (gmul x (d y) : A (i + (j+1))) :=
+  d (gmul x y) = cast_add_monoid_hom A (by abel) (gmul (d x) y) + (-1 : ℤ)^i • (gmul x (d y) : A (i + (j+1))) :=
 differential_graded_ring.d_mul
 
 @[simp]
@@ -123,10 +147,10 @@ begin
 end
 
 lemma mul_boundary_of_boundary_cycle {i j} (x : A i) (y : A j) (dy : d y = 0) :
-  gmul (d x) y = cast_hom A (by abel) (d (gmul x y)) :=
+  gmul (d x) y = cast_add_monoid_hom A (by abel) (d (gmul x y)) :=
 begin
   rw [d_mul, dy, graded_semiring.mul_zero x, smul_zero, add_zero],
-  rw [cast_hom_cast_hom, cast_hom_refl],
+  rw [cast_add_monoid_hom_cast_add_monoid_hom, cast_add_monoid_hom_refl],
 end
 
 end
@@ -143,7 +167,20 @@ def d_linear_map {A : ℕ → Type*} [Π n, add_comm_group (A n)] [Π n, module 
   ..d }
 
 class differential_graded_algebra (A : ℕ → Type*) [Π n, add_comm_group (A n)] [Π n, module R (A n)]
-  extends differential_graded_module R A, differential_graded_ring A.
+  extends differential_graded_ring A :=
+(d_smul : ∀ {i} (c : R) (x : A i), d (c • x) = c • d x)
+
+/-
+Unfortunately we can't just use
+`differential_graded_algebra extends differential_graded_module`,
+as this creates two non-defeq paths to `differential_graded`.
+-/
+instance differential_graded_algebra.to_differential_graded_module (A : ℕ → Type*)
+  [Π n, add_comm_group (A n)] [Π n, module R (A n)] [differential_graded_algebra R A] :
+  differential_graded_module R A :=
+{ d := λ i, d,
+  d_squared := λ i x, d_squared,
+  d_smul := λ i, differential_graded_algebra.d_smul, }
 
 def mul_bilinear (A : ℕ → Type u) [Π n, add_comm_group (A n)] [Π n, module R (A n)] [differential_graded_algebra R A] (i j : ℕ) :
   tensor_product R (A i) (A j) →ₗ[R] A (i + j) :=
@@ -163,19 +200,9 @@ rfl
   Module.of_hom f x = f x :=
 rfl
 
-def cast_linear_map (A : ℕ → Type*) [Π n, add_comm_monoid (A n)] [Π n, module R (A n)] {i j : ℕ} (h : i = j) : A i →ₗ[R] A j :=
-{ map_smul' := λ r x, by { cases h, refl, },
-  ..cast_hom A h }
 
-@[simp]
-lemma cast_linear_map_cast_linear_map (A : ℕ → Type*) [Π n, add_comm_monoid (A n)] [Π n, module R (A n)] {i j k : ℕ} (h₁ : i = j) (h₂ : j = k) (x):
-  cast_linear_map R A h₂ (cast_linear_map R A h₁ x) = cast_linear_map R A (h₁.trans h₂) x :=
-by { cases h₁, cases h₂, refl, }
-
-@[simp]
-lemma cast_linear_map_refl (A : ℕ → Type*) [Π n, add_comm_monoid (A n)] [Π n, module R (A n)] {i} (x : A i):
-  cast_linear_map R A (eq.refl _) x = x :=
-rfl
+section
+variables {R}
 
 @[simp]
 lemma d_linear_map_squared {A : ℕ → Type*} [Π n, add_comm_group (A n)] [Π n, module R (A n)] [differential_graded_module R A] {i} {x : A i} :
@@ -185,17 +212,25 @@ differential_graded.d_squared
 @[simp]
 lemma d_linear_map_one {A : ℕ → Type*} [Π n, add_comm_group (A n)] [Π n, module R (A n)] [differential_graded_algebra R A] :
   d_linear_map R (1 : A 0) = 0 :=
-begin
-  change d (1 : A 0) = 0,
-  convert d_one A,
-  sorry,
-end
+d_one A
+
+-- lemma d_linear_map_gmul {A : ℕ → Type*} [Π n, add_comm_group (A n)] [Π n, module R (A n)] [differential_graded_algebra R A] {i j : ℕ} (x : A i) (y : A j) :
+--   d_linear_map R (gmul x y) = gmul (d_linear_map R x) y + gmul x (d_linear_map R y) :=
+-- d_one A
+
+
+lemma mul_cycle_of_cycle_cycle' {A : ℕ → Type*} [Π n, add_comm_group (A n)] [Π n, module R (A n)] [differential_graded_algebra R A]
+  {i j} (x : A i) (y : A j) (dx : d x = 0) (dy : d y = 0) :
+  d_linear_map R (gmul x y) = 0 :=
+mul_cycle_of_cycle_cycle x y dx dy
 
 @[simp]
 lemma cast_linear_map_d {A : ℕ → Type*} [Π n, add_comm_group (A n)] [Π n, module R (A n)] [differential_graded_module R A] {i j : ℕ} {x : A i}
   (h : i + 1 = j + 1):
   cast_linear_map R A h (d_linear_map R x) = d_linear_map R (cast_linear_map R A (nat.succ.inj h) x) :=
 by { cases h, refl, }
+
+end
 
 @[simps]
 def to_cochain_complex (A : ℕ → Type v) [Π n, add_comm_group (A n)] [Π n, module R (A n)] [differential_graded_module R A] :
@@ -263,6 +298,9 @@ end
 def mm (A : ℕ → Type u) [Π n, add_comm_group (A n)] [Π n, module R (A n)] [differential_graded_algebra R A] (i j : ℕ) :
   graded.homology R A j →ₗ[R] (graded.homology R A i →ₗ[R] graded.homology R A (i + j)) :=
 monoidal_closed.curry (nn R A i j).
+
+
+
 
 namespace Module
 
@@ -337,52 +375,108 @@ end homology
 
 end Module
 
+
+
 variables (A : ℕ → Type u) [Π n, add_comm_group (A n)] [Π n, module R (A n)] [differential_graded_algebra R A]
 
-lemma w {i : ℕ} {x : A i} (dx : d x = 0) : (homological_complex.d_from (to_cochain_complex R A) i) x = 0 :=
+lemma d_from_nat {V : Type*} [category V] [has_zero_morphisms V] [has_zero_object V] (C : cochain_complex V ℕ) (i : ℕ) :
+  homological_complex.d_from C i = C.d i (i+1) ≫ (C.X_next_iso rfl).inv :=
+by rw homological_complex.d_from_eq
+
+lemma d_to_nat {V : Type*} [category V] [has_zero_morphisms V] [has_zero_object V] (C : chain_complex V ℕ) (i : ℕ) :
+  homological_complex.d_to C i = (C.X_prev_iso rfl).hom ≫ C.d (i+1) i :=
+by rw homological_complex.d_to_eq
+
+lemma d_from_eq_zero {i : ℕ} {x : A i} (dx : d x = 0) :
+  (homological_complex.d_from (to_cochain_complex R A) i) x = 0 :=
 begin
-rw homological_complex.d_from_eq,
-  swap 3, exact i+1,swap 2,
-  simp only [complex_shape.up_rel],
+  rw d_from_nat,
   simp [to_cochain_complex],
-  erw dx,
-  rw linear_map.map_zero,
+  erw [dx, linear_map.map_zero],
+end
+
+@[simp]
+lemma iso_hom_apply_eq_zero {X Y : Module R} (f : X ≅ Y) (x : X) : f.hom x = 0 ↔ x = 0 := sorry
+@[simp]
+lemma iso_inv_apply_eq_zero {X Y : Module R} (f : X ≅ Y) (y : Y) : f.inv y = 0 ↔ y = 0 := sorry
+
+@[simp]
+lemma ker_d_from_nat (C : cochain_complex (Module R) ℕ) (i : ℕ) :
+  linear_map.ker (homological_complex.d_from C i) = linear_map.ker (C.d i (i+1)) :=
+begin
+  rw d_from_nat,
+  ext, split,
+  { intro h, simpa using h, },
+  { intro h, simpa using h, },
+end
+
+section
+open Module.homology
+
+@[simp] lemma d_out (A : ℕ → Type u) [Π n, add_comm_group (A n)] [Π n, module R (A n)] [differential_graded_algebra R A] (i : ℕ)
+  (x : graded.homology R A i) : (d : A i →+ A (i+1)) (@coe _ ((to_cochain_complex R A).X i) (@coe_to_lift _ _ (@coe_base _ _ coe_subtype)) (out x)) = 0 :=
+by simpa using (out x).2
+
+def mm' (A : ℕ → Type u) [Π n, add_comm_group (A n)] [Π n, module R (A n)] [differential_graded_algebra R A] (i j : ℕ) :
+  graded.homology R A j →ₗ[R] (graded.homology R A i →ₗ[R] graded.homology R A (i + j)) :=
+{ to_fun := λ x,
+  { to_fun := λ y, mk _ _ _ begin fsplit, exact (gmul (out y).1 (out x).1 : A (i + j)), simp, rw mul_cycle_of_cycle_cycle', simp, simp, end,
+    map_add' := sorry,
+    map_smul' := sorry, },
+  map_add' := sorry,
+  map_smul' := sorry, }
+
 end
 
 def mk {i : ℕ} (x : A i) (dx : d x = 0) : (to_cochain_complex R A).homology i :=
-Module.homology.mk _ _ _ begin fsplit, exact x, exact w R A dx, end
+Module.homology.mk _ _ _ begin fsplit, exact x, exact d_from_eq_zero R A dx, end
+
+-- def mk' {i : ℕ} (x : add_monoid_hom.ker (d : A i →+ A (i+1))) : (to_cochain_complex R A).homology i :=
+-- Module.homology.mk _ _ _ begin fsplit, exact x.1, exact d_from_eq_zero R A x.2, end
+
+lemma mk_out {i : ℕ} (x : (to_cochain_complex R A).homology i) :
+  mk R A (Module.homology.out x).1 sorry = x :=
+sorry
+
+lemma mk_cast (A : ℕ → Type u) [Π n, add_comm_group (A n)] [Π n, module R (A n)] [differential_graded_algebra R A]
+  {i j : ℕ} (h : i = j) (x : A i) (dx : d (cast (congr_arg A h) x) = 0) :
+  mk R A (cast (congr_arg A h) x) dx =
+    cast (congr_arg (graded.homology R A) h : graded.homology R A i = graded.homology R A j)
+    (mk R A x (by { cases h, simpa using dx })) :=
+begin
+  cases h,
+  refl,
+end
 
 lemma nn_mk_mk (A : ℕ → Type*) [Π n, add_comm_group (A n)] [Π n, module R (A n)] [differential_graded_algebra R A]
   {i j} (x : A i) (dx : d x = 0) (y : A j) (dy : d y = 0) :
   nn R A i j (mk R A x dx ⊗ₜ[R] mk R A y dy) =
   mk R A (gmul x y) (mul_cycle_of_cycle_cycle x y dx dy) :=
-sorry
+begin
+  dsimp [nn, foo],
+  erw inv_cokernel_comparison_desc_apply,
+end
 
 def one : graded.homology R A 0 :=
 mk R A (1 : A 0) (d_one A)
--- begin
---   refine Module.homology.mk _ _ _ _,
---   refine ⟨(1 : A 0), _⟩,
---   rw homological_complex.d_from_eq,
---   swap 3, exact 1,swap 2, exact zero_add _,
---   simp,
---   dsimp [to_cochain_complex],
---   simp,
--- end.
 
 lemma one_mul'
   {i : ℕ} (x : graded.homology R A i) :
   (nn R A 0 i) (one R A ⊗ₜ[R] x) = cast sorry x :=
 begin
-  dsimp [nn, one],
-  dsimp only [foo],
+  rw ←mk_out R A x,
+  rw one,
+  rw nn_mk_mk,
+  simp only [one_gmul],
+  rw mk_cast,
+  refl,
 end
 
 instance :
   graded_monoid (graded.homology R A) :=
 { one := one R A,
   mul := λ i j x y, mm R A i j y x,
-  one_mul := λ i x, begin dsimp [mm], end,
+  one_mul := λ i x, one_mul' R A x,
   mul_one := sorry,
   mul_assoc := sorry, }
 

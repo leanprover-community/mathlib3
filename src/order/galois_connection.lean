25ec0bd1d8cc2e5f74e149e58cb9aa8ab43a183a
Yaël Dillies <yael.dillies@gmail.com>
2022-03-10 16:33:35 +0100

    revert import change
    Co-authored-by: Eric Wieser <wieser.eric@gmail.com>

--- src/order/galois_connection.lean
+++ src/order/galois_connection.lean
@@ -3,7 +3,7 @@ Copyright (c) 2017 Johannes Hölzl. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Johannes Hölzl
 -/
-import order.complete_boolean_algebra
+import order.complete_lattice
 import order.order_dual
 /-!
 # Galois connections, insertions and coinsertions

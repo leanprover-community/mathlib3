c146a67e1e0c2f88721713f0000b53393e4342a5
Ashvni <ashvni.n@gmail.com>
2022-03-10 11:59:44 +0000

    mistake

--- src/topology/locally_constant/basic.lean
+++ src/topology/locally_constant/basic.lean
@@ -1,7 +1,7 @@
 /-
 Copyright (c) 2021 Johan Commelin. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
-Authors: Johan Commelin, Ashvni Narayanan
+Authors: Johan Commelin
 -/
 import topology.subset_properties
 import topology.connected

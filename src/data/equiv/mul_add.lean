f59dcffba0224c47bfd709b4b02f4dd460341098
Damiano Testa <adomani@gmail.com>
2022-03-10 12:55:05 +0000

    auto names

--- src/data/equiv/mul_add.lean
+++ src/data/equiv/mul_add.lean
@@ -476,7 +476,7 @@ def monoid_hom.to_mul_equiv [mul_one_class M] [mul_one_class N] (f : M →* N) (
   map_mul' := f.map_mul }
 
 /-- A group is isomorphic to its group of units. -/
-@[to_additive to_add_units "An additive group is isomorphic to its group of additive units"]
+@[to_additive "An additive group is isomorphic to its group of additive units"]
 def to_units [group G] : G ≃* Gˣ :=
 { to_fun := λ x, ⟨x, x⁻¹, mul_inv_self _, inv_mul_self _⟩,
   inv_fun := coe,
@@ -484,7 +484,7 @@ def to_units [group G] : G ≃* Gˣ :=
   right_inv := λ u, units.ext rfl,
   map_mul' := λ x y, units.ext rfl }
 
-@[simp, to_additive coe_to_add_units] lemma coe_to_units [group G] (g : G) :
+@[simp, to_additive] lemma coe_to_units [group G] (g : G) :
   (to_units g : G) = g := rfl
 
 protected lemma group.is_unit {G} [group G] (x : G) : is_unit x := (to_units x).is_unit

5715108733bd0c28f1ff43809b4e842bdded5a22
Damiano Testa <adomani@gmail.com>
2022-03-11 08:48:39 +0000

    remove duplicated namespace

--- src/data/int/parity.lean
+++ src/data/int/parity.lean
@@ -182,12 +182,12 @@ theorem even.sub_odd (hm : even m) (hn : odd n) : odd (m - n) :=
 odd_sub'.2 $ iff_of_true hn hm
 
 @[simp]
-lemma int.odd_add_one_iff_even (z : ℤ) : odd (z + 1) ↔ even z :=
-int.odd_add'.trans (by simp)
+lemma odd_add_one_iff_even (z : ℤ) : odd (z + 1) ↔ even z :=
+odd_add'.trans (by simp)
 
 @[simp]
-lemma int.odd_sub_one_iff_even (z : ℤ) : odd (z - 1) ↔ even z :=
-int.odd_sub'.trans (by simp)
+lemma odd_sub_one_iff_even (z : ℤ) : odd (z - 1) ↔ even z :=
+odd_sub'.trans (by simp)
 
 lemma even_mul_succ_self (n : ℤ) : even (n * (n + 1)) :=
 begin

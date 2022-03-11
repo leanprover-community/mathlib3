9fed094089338fb44a34bd5fee3de8eec25767e9
sgouezel <sebastien.gouezel@univ-rennes1.fr>
2022-03-11 12:11:45 +0100

    fix

--- src/topology/algebra/module/basic.lean
+++ src/topology/algebra/module/basic.lean
@@ -1162,11 +1162,6 @@ write `A.det` instead of `(A : M →ₗ[R] M).det`. -/
   {M : Type*} [topological_space M] [add_comm_group M] [module R M] (A : M →L[R] M) : R :=
 linear_map.det (A : M →ₗ[R] M)
 
-@[simp] _root_.lemma continuous_linear_equiv.det_coe_symm {R : Type*} [field R]
-  {M : Type*} [topological_space M] [add_comm_group M] [module R M] (A : M ≃L[R] M) :
-  (A.symm : M →L[R] M).det = (A : M →L[R] M).det ⁻¹ :=
-linear_equiv.det_coe_symm A.to_linear_equiv
-
 variables
 {R : Type*} [comm_ring R]
 {M : Type*} [topological_space M] [add_comm_group M]
@@ -1758,6 +1753,11 @@ def fin_two_arrow : (fin 2 → M) ≃L[R] M × M :=
 { to_linear_equiv := linear_equiv.fin_two_arrow R M, .. pi_fin_two R (λ _, M) }
 
 end
+
+@[simp] lemma det_coe_symm {R : Type*} [field R]
+  {M : Type*} [topological_space M] [add_comm_group M] [module R M] (A : M ≃L[R] M) :
+  (A.symm : M →L[R] M).det = (A : M →L[R] M).det ⁻¹ :=
+linear_equiv.det_coe_symm A.to_linear_equiv
 
 end continuous_linear_equiv
 

52450867f97e74d2f55c1dcfb73996b854ae59a1
Bolton Bailey <bolton.bailey@gmail.com>
2022-03-11 18:40:36 -0600

    fix line too long

--- src/number_theory/padics/padic_norm.lean
+++ src/number_theory/padics/padic_norm.lean
@@ -197,7 +197,8 @@ by rw [of_int, padic_val_int.of_ne_one_ne_zero hp hz]
 lemma multiplicity_sub_multiplicity {q : ℚ} (hp : p ≠ 1) (hq : q ≠ 0) :
   padic_val_rat p q =
   (multiplicity (p : ℤ) q.num).get (finite_int_iff.2 ⟨hp, rat.num_ne_zero_of_ne_zero hq⟩) -
-  (multiplicity p q.denom).get (by {rw <-finite_iff_dom, rw finite_nat_iff, refine (and_iff_left _).mpr hp, exact q.pos,}) :=
+  (multiplicity p q.denom).get
+    (by {rw [←finite_iff_dom, finite_nat_iff, and_iff_right hp], exact q.pos,}) :=
 begin
   rw [padic_val_rat.of_ne_zero hq, padic_val_int.of_ne_one_ne_zero hp, padic_val_nat, dif_pos],
   refl,

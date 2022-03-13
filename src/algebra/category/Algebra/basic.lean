522810f28f88989f71ec792ee7c375aa25403ef2
Scott Morrison <scott.morrison@gmail.com>
2022-03-13 19:15:30 +1100

    fix

--- src/algebra/category/Algebra/basic.lean
+++ src/algebra/category/Algebra/basic.lean
@@ -64,7 +64,7 @@ def of_hom {R : Type u} [comm_ring R] {X Y : Type v} [ring X] [algebra R X] [rin
   (f : X →ₐ[R] Y) : of R X ⟶ of R Y := f
 
 @[simp] lemma of_hom_apply {R : Type u} [comm_ring R]
-  {X Y : Type v} [ring X] [algebra R X] [ring Y] [algebra R Y] (f : X →ₐ[R] Y)(x : X) :
+  {X Y : Type v} [ring X] [algebra R X] [ring Y] [algebra R Y] (f : X →ₐ[R] Y) (x : X) :
   of_hom f x = f x := rfl
 
 instance : inhabited (Algebra R) := ⟨of R R⟩

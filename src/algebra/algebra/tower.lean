059fabc6cf4c1e1445ac0c8144353f3c9fde3d98
Scott Morrison <scott.morrison@gmail.com>
2022-03-13 14:56:54 +1100

    split subalgebra/pointwise

--- src/algebra/algebra/tower.lean
+++ src/algebra/algebra/tower.lean
@@ -4,7 +4,7 @@ Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Kenny Lau
 -/
 
-import algebra.algebra.subalgebra
+import algebra.algebra.subalgebra.basic
 
 /-!
 # Towers of algebras

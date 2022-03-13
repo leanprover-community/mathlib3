1bd8414beb43e9353555a0642dbea3646d42568f
Hans Parshall <hparshall@gmail.com>
2022-03-12 18:37:57 -0800

    normed_star_monoid -> normed_star_group

--- src/analysis/normed_space/star/basic.lean
+++ src/analysis/normed_space/star/basic.lean
@@ -226,17 +226,17 @@ local attribute [instance] matrix.normed_group
 open_locale matrix
 
 @[simp] lemma matrix.entrywise_sup_norm_star_eq_norm {n : Type*} [normed_ring E] [star_add_monoid E]
-  [normed_star_monoid E] [fintype n] (M : (matrix n n E)) : ∥star M∥ = ∥M∥ :=
+  [normed_star_group E] [fintype n] (M : (matrix n n E)) : ∥star M∥ = ∥M∥ :=
 begin
   refine le_antisymm (by simp [norm_matrix_le_iff, M.norm_entry_le_entrywise_sup_norm]) _,
   refine ((norm_matrix_le_iff (norm_nonneg _)).mpr (λ i j, _)).trans
     (congr_arg _ M.star_eq_conj_transpose).ge,
-  exact (normed_star_monoid.norm_star).symm.le.trans Mᴴ.norm_entry_le_entrywise_sup_norm
+  exact (normed_star_group.norm_star).symm.le.trans Mᴴ.norm_entry_le_entrywise_sup_norm
 end
 
 @[priority 100] -- see Note [lower instance priority]
-noncomputable instance matrix.to_normed_star_monoid {n : Type*} [normed_ring E] [star_add_monoid E]
-  [normed_star_monoid E] [fintype n] : normed_star_monoid (matrix n n E) :=
+instance matrix.to_normed_star_group {n : Type*} [normed_ring E] [star_add_monoid E]
+  [normed_star_group E] [fintype n] : normed_star_group (matrix n n E) :=
 ⟨matrix.entrywise_sup_norm_star_eq_norm⟩
 
 end matrix

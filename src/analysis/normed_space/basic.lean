daa257ff3a9c62e08f9a9edcadf9da5c27a98004
Hans Parshall <hparshall@gmail.com>
2022-03-13 07:59:44 +0000

    feat(analysis/normed_space/star/basic): `matrix.entrywise_sup_norm_star_eq_norm` (#12201)
    This is precisely the statement needed for a `normed_star_monoid`
    instance on matrices using the entrywise sup norm.
    
    
    
    
    
    Co-authored-by: Hans Parshall <hparshall@users.noreply.github.com>
    Co-authored-by: Johan Commelin <johan@commelin.net>

--- src/analysis/normed_space/basic.lean
+++ src/analysis/normed_space/basic.lean
@@ -288,7 +288,7 @@ def matrix.normed_space {α : Type*} [normed_field α] {n m : Type*} [fintype n]
   normed_space α (matrix n m α) :=
 pi.normed_space
 
-lemma matrix.norm_entry_le_entrywise_sup_norm {α : Type*} [normed_field α] {n m : Type*} [fintype n]
+lemma matrix.norm_entry_le_entrywise_sup_norm {α : Type*} [normed_ring α] {n m : Type*} [fintype n]
   [fintype m] (M : (matrix n m α)) {i : n} {j : m} :
   ∥M i j∥ ≤ ∥M∥ :=
 (norm_le_pi_norm (M i) j).trans (norm_le_pi_norm M i)

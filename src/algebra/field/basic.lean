8030b291c1c3f7f3ca79d63404f30faf6cdee605
Gabriel Ebner <gebner@gebner.org>
2022-03-14 09:38:11 +0100

    fix

--- src/algebra/field/basic.lean
+++ src/algebra/field/basic.lean
@@ -330,7 +330,7 @@ See note [reducible non-instances]. -/
 @[reducible]
 protected def function.injective.division_ring [division_ring K] {K'}
   [add_monoid_with_one K'] [has_mul K'] [has_neg K'] [has_sub K'] [has_inv K'] [has_div K']
-  [has_div K'] [has_scalar ℤ K'] [has_pow K' ℕ] [has_pow K' ℤ]
+  [has_scalar ℤ K'] [has_pow K' ℕ] [has_pow K' ℤ]
   (f : K' → K) (hf : function.injective f) (zero : f 0 = 0) (one : f 1 = 1)
   (add : ∀ x y, f (x + y) = f x + f y) (mul : ∀ x y, f (x * y) = f x * f y)
   (neg : ∀ x, f (-x) = -f x) (sub : ∀ x y, f (x - y) = f x - f y)
@@ -346,7 +346,7 @@ See note [reducible non-instances]. -/
 @[reducible]
 protected def function.injective.field [field K] {K'}
   [add_monoid_with_one K'] [has_mul K'] [has_neg K'] [has_sub K'] [has_inv K'] [has_div K']
-  [has_div K'] [has_scalar ℤ K'] [has_pow K' ℕ] [has_pow K' ℤ]
+  [has_scalar ℤ K'] [has_pow K' ℕ] [has_pow K' ℤ]
   (f : K' → K) (hf : function.injective f) (zero : f 0 = 0) (one : f 1 = 1)
   (add : ∀ x y, f (x + y) = f x + f y) (mul : ∀ x y, f (x * y) = f x * f y)
   (neg : ∀ x, f (-x) = -f x) (sub : ∀ x y, f (x - y) = f x - f y)

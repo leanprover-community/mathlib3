361ce9b834a81818e7c5a2ebb8bb8f88e01361e3
YaelDillies <yael.dillies@gmail.com>
2022-03-14 15:53:03 +0000

    move to algebra.hom.group_instances

--- src/algebra/group/pi.lean
+++ src/algebra/group/pi.lean
@@ -3,7 +3,7 @@ Copyright (c) 2018 Simon Hudon. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Simon Hudon, Patrick Massot
 -/
-import algebra.hom.instances
+import algebra.hom.group_instances
 import data.pi
 import data.set.function
 import tactic.pi_instances

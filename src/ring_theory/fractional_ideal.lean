757b9fc28590590880406b70add14a918d6d62fa
David Kurniadi Angdinata <dka31@cantab.ac.uk>
2022-03-14 23:46:16 +0000

    Remove non-zero assumptions

--- src/ring_theory/fractional_ideal.lean
+++ src/ring_theory/fractional_ideal.lean
@@ -1048,27 +1048,6 @@ lemma mem_span_singleton_self (x : P) :
   x ∈ span_singleton S x :=
 (mem_span_singleton S).mpr ⟨1, one_smul _ _⟩
 
-lemma span_singleton_eq_span_singleton [no_zero_smul_divisors R P] {x y : P} (hx : x ≠ 0)
-  (hy : y ≠ 0) : span_singleton S x = span_singleton S y ↔ ∃ z : Rˣ, z • x = y :=
-begin
-  split,
-  { intro hxy,
-    cases (mem_span_singleton S).mp (by { rw [hxy], apply mem_span_singleton_self }) with v hv,
-    cases (mem_span_singleton S).mp (by { rw [← hxy], apply mem_span_singleton_self }) with i hi,
-    have vi : v * i = 1 :=
-    by { rw [← one_smul R y, ← hi, smul_smul, ← sub_eq_zero, ← sub_smul, smul_eq_zero] at hv,
-         exact sub_eq_zero.mp (or.resolve_right hv hy) },
-    have iv : i * v = 1 :=
-    by { rw [← one_smul R x, ← hv, smul_smul, ← sub_eq_zero, ← sub_smul, smul_eq_zero] at hi,
-         exact sub_eq_zero.mp (or.resolve_right hi hx) },
-    exact ⟨⟨v, i, vi, iv⟩, hv⟩ },
-  { rintro ⟨⟨v, i, _, iv⟩, hxy : v • x = y⟩,
-    ext,
-    rw [mem_span_singleton, mem_span_singleton],
-    exact ⟨λ ⟨z, hz⟩, ⟨z * i, by rw [← smul_smul, ← hxy, smul_smul i v, iv, one_smul, ← hz]⟩,
-           λ ⟨z, hz⟩, ⟨z * v, by rw [← smul_smul, ← hz, ← hxy]⟩⟩ }
-end
-
 variables {S}
 
 lemma eq_span_singleton_of_principal (I : fractional_ideal S P)
@@ -1097,6 +1076,32 @@ begin
   refine (mem_span_singleton S).trans ((exists_congr _).trans (mem_one_iff S).symm),
   intro x',
   rw [algebra.smul_def, mul_one]
+end
+
+lemma span_singleton_eq_span_singleton [no_zero_smul_divisors R P] {x y : P} :
+  span_singleton S x = span_singleton S y ↔ ∃ z : Rˣ, z • x = y :=
+begin
+  by_cases hy : y = 0,
+  { rw [hy, span_singleton_zero, span_singleton_eq_zero_iff],
+    exact ⟨λ hx, ⟨1, by rw [hx, smul_zero]⟩, λ ⟨z, hz⟩, (smul_eq_zero_iff_eq z).mp hz⟩ },
+  by_cases hx : x = 0,
+  { rw [hx, span_singleton_zero, eq_comm],
+    exact ⟨λ hy, ⟨1, by rw [smul_zero, span_singleton_eq_zero_iff.mp hy]⟩,
+           λ ⟨_, hz⟩, by simpa only [smul_zero] using hz.symm⟩ },
+  split,
+  { intro hxy,
+    cases (mem_span_singleton S).mp (by { rw [hxy], apply mem_span_singleton_self }) with v hv,
+    cases (mem_span_singleton S).mp (by { rw [← hxy], apply mem_span_singleton_self }) with i hi,
+    have vi : v * i = 1 :=
+    by { rw [← one_smul R y, ← hi, smul_smul] at hv, exact smul_left_injective R hy hv },
+    have iv : i * v = 1 :=
+    by { rw [← one_smul R x, ← hv, smul_smul] at hi, exact smul_left_injective R hx hi },
+    exact ⟨⟨v, i, vi, iv⟩, hv⟩ },
+  { rintro ⟨⟨v, i, _, iv⟩, hxy : v • x = y⟩,
+    ext,
+    rw [mem_span_singleton, mem_span_singleton],
+    exact ⟨λ ⟨z, hz⟩, ⟨z * i, by rw [← smul_smul, ← hxy, smul_smul i v, iv, one_smul, ← hz]⟩,
+           λ ⟨z, hz⟩, ⟨z * v, by rw [← smul_smul, ← hz, ← hxy]⟩⟩ }
 end
 
 @[simp]

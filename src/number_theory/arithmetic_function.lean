66ea5396b390302be834d5b2ef7ea0ed31b77c24
Kevin Wilson <khwilson@gmail.com>
2022-03-14 21:21:21 +0000

    feat(number_theory/arithmetic_function): add equality of multiplicative functions
    To show that two multiplicative functions are equal, it suffices to show
    that they are equal on prime powers. This is a commonly used strategy
    when two functions are known to be multiplicative (e.g., they're both
    Dirichlet convolutions of simpler multiplicative functions).
    
    This will be used in several ongoing commits to prove asymptotics for
    squarefree numbers.

--- src/number_theory/arithmetic_function.lean
+++ src/number_theory/arithmetic_function.lean
@@ -564,6 +564,35 @@ lemma multiplicative_factorization [comm_monoid_with_zero R] (f : arithmetic_fun
   ∀ {n : ℕ}, n ≠ 0 → f n = n.factorization.prod (λ p k, f (p ^ k)) :=
 λ n hn, multiplicative_factorization f hf.2 hf.1 hn
 
+/-- Two multiplicative functions `f` and `g` are equal if and only if
+they agree on prime powers -/
+lemma eq_iff_eq_on_prime_powers [comm_monoid_with_zero R]
+  (f : arithmetic_function R) (hf : f.is_multiplicative)
+  (g : arithmetic_function R) (hg : g.is_multiplicative) :
+  f = g ↔ ∀ (p i : ℕ), nat.prime p → f(p ^ i) = g(p ^ i) :=
+begin
+  split,
+  intros h p i _,
+  simp only [h],
+  intros h,
+  ext n,
+  by_cases hn : n = 0,
+  rw [hn, arithmetic_function.map_zero, arithmetic_function.map_zero],
+  rw nat.multiplicative_factorization f (λ x y, (λ hxy, hf.map_mul_of_coprime hxy)) hf.map_one hn,
+  rw nat.multiplicative_factorization g (λ x y, (λ hxy, hg.map_mul_of_coprime hxy)) hg.map_one hn,
+
+  have : n.factorization.support ⊆ n.factors.to_finset, simp,
+  rw finsupp.prod_of_support_subset n.factorization this,
+  rw finsupp.prod_of_support_subset n.factorization this,
+  apply prod_congr rfl,
+  intros p hp,
+  rw list.mem_to_finset at hp,
+  exact h p (n.factorization p) (nat.prime_of_mem_factors hp),
+
+  intros i hi, rw pow_zero, exact hg.map_one,
+  intros i hi, rw pow_zero, exact hf.map_one,
+end
+
 end is_multiplicative
 
 section special_functions

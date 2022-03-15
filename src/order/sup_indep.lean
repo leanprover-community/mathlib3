8b933f77889c5e60e0db8b831a18a76cd72f7a49
Eric Wieser <wieser.eric@gmail.com>
2022-03-15 11:18:03 +0000

    adjust authors

--- src/order/sup_indep.lean
+++ src/order/sup_indep.lean
@@ -1,7 +1,7 @@
 /-
-Copyright (c) 2021 Yaël Dillies. All rights reserved.
+Copyright (c) 2021 Aaron Anderson, Yaël Dillies. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
-Authors: Yaël Dillies
+Authors: Aaron Anderson, Kevin Buzzard, Yaël Dillies, Eric Wieser
 -/
 import data.finset.pairwise
 import data.set.finite

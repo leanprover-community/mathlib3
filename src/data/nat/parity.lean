a0ddf4553f5c4185fcac203c5491be9140f3c090
Damiano Testa <adomani@gmail.com>
2022-03-15 19:15:29 +0000

    remove unnecessary import

--- src/data/nat/parity.lean
+++ src/data/nat/parity.lean
@@ -4,7 +4,6 @@ Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Jeremy Avigad, Benjamin Davidson
 -/
 import data.nat.modeq
-import algebra.parity
 
 /-!
 # Parity of natural numbers

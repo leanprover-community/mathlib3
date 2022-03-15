0ddd351c2d795249aef8cd30b80824b584bd30f3
Eric Wieser <wieser.eric@gmail.com>
2022-03-15 10:04:02 +0000

    Update src/algebra/algebra/basic.lean

--- src/algebra/algebra/basic.lean
+++ src/algebra/algebra/basic.lean
@@ -670,7 +670,7 @@ by { ext, refl }
   of_linear_map linear_map.id map_one map_mul = alg_hom.id R A :=
 ext $ λ _, rfl
 
-@[simp] lemma map_smul_of_tower {R'} [has_scalar R' A] [has_scalar R' B]
+lemma map_smul_of_tower {R'} [has_scalar R' A] [has_scalar R' B]
   [linear_map.compatible_smul A B R' R] (r : R') (x : A) : φ (r • x) = r • φ x :=
 φ.to_linear_map.map_smul_of_tower r x
 

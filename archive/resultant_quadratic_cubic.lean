4ccabe27539d25124af2da3c046dc2561ec3cb8b
Heather Macbeth <25316162+hrmacbeth@users.noreply.github.com>
2022-03-16 18:35:46 -0400

    cleanup

--- archive/resultant_quadratic_cubic.lean
+++ archive/resultant_quadratic_cubic.lean
@@ -20,7 +20,7 @@ the general result (for polynomials of general degree).
 
 open matrix mv_polynomial
 
-open_locale classical
+open_locale classical matrix
 
 noncomputable theory
 
@@ -71,18 +71,23 @@ def mat : matrix (fin 5) (fin 5) R :=
   ![b₀, b₁, b₂, b₃,  0],
   ![ 0, b₀, b₁, b₂, b₃]]
 
-lemma sylvester_mul_rational_normal (a₀ a₁ a₂ b₀ b₁ b₂ b₃ U V : R) (k : fin 5 → R) :
-  dot_product
-    (vec_mul k (mat a₀ a₁ a₂ b₀ b₁ b₂ b₃))
-    (![U ^ 4, U ^ 3 * V, U ^ 2 * V ^ 2, U * V ^ 3, V ^ 4])
-  = (k 0 * U ^ 2 + k 1 * U * V + k 2 * V ^ 2) * (a₀ * U ^ 2 + a₁ * U * V + a₂ * V ^ 2)
-    + (k 3 * U + k 4 * V) * (b₀ * U ^ 3 + b₁ * U ^ 2 * V + b₂ * U * V ^ 2 + b₃ * V ^ 3) :=
+lemma sylvester_mul_vec_rational_normal (a₀ a₁ a₂ b₀ b₁ b₂ b₃ U V : R) :
+  mul_vec (mat a₀ a₁ a₂ b₀ b₁ b₂ b₃)
+    ![U ^ 4, U ^ 3 * V, U ^ 2 * V ^ 2, U * V ^ 3, V ^ 4]
+  = ![U ^ 2 * eval ![U, V] (q a₀ a₁ a₂),
+      U * V * eval ![U, V] (q a₀ a₁ a₂),
+      V ^ 2 * eval ![U, V] (q a₀ a₁ a₂),
+      U * eval ![U, V] (c b₀ b₁ b₂ b₃),
+      V * eval ![U, V] (c b₀ b₁ b₂ b₃)] :=
 begin
-  have H : (2 : fin (1+2)).succ = 3 := rfl,
-  have H' : (2 : fin (nat.succ (1+2))).succ = 3 := rfl,
-  have H'' : (3 : fin 4).succ = 4 := rfl,
-  simp [mat, vec_mul, dot_product, fin.sum_univ_succ, q, c, H, H', H''],
-  ring
+  ext i;
+  fin_cases i;
+  simp only [mat, q, c, cons_append, cons_val_fin_one, cons_val_one, cons_val_succ,
+    cons_val_zero, cons_vec_alt0, cons_vec_alt1, cons_vec_bit0_eq_alt0, cons_vec_bit1_eq_alt1,
+    dot_product, empty_append, eval_X, fin.sum_univ_succ, finset.card_singleton, finset.sum_const,
+    fintype.univ_of_subsingleton, head_cons, _root_.map_add, _root_.map_mul, map_pow, mul_vec,
+    nat.cast_one, nsmul_eq_mul, pi.zero_apply, smul_eval];
+  ring,
 end
 
 variables [is_domain R]
@@ -93,8 +98,6 @@ lemma resultant_eq_zero_of_common_root (hUV : ![U, V] ≠ 0) (hq : eval ![U, V] 
   (hc : eval ![U, V] (c b₀ b₁ b₂ b₃) = 0) :
   det (mat a₀ a₁ a₂ b₀ b₁ b₂ b₃) = 0 :=
 begin
-  simp only [q, c, algebra.smul_mul_assoc, _root_.map_add, smul_eval, map_pow, eval_X,
-    cons_val_zero, _root_.map_mul, cons_val_one, head_cons] at hq hc,
   rw ← matrix.exists_mul_vec_eq_zero_iff,
   refine ⟨![U ^ 4, U ^ 3 * V, U ^ 2 * V ^ 2, U * V ^ 3, V ^ 4], _, _⟩,
   { contrapose! hUV,
@@ -104,18 +107,9 @@ begin
       exact pow_eq_zero this },
     { have : V ^ 4 = 0 := congr_fun hUV 4,
       exact pow_eq_zero this } },
-  ext i,
-  fin_cases i;
-  simp only [mat, cons_append, cons_val_fin_one, cons_val_one, cons_val_succ, cons_val_zero,
-    cons_vec_alt0, cons_vec_alt1, cons_vec_bit0_eq_alt0, cons_vec_bit1_eq_alt1, dot_product,
-    empty_append, fin.sum_univ_succ, finset.card_singleton, finset.sum_const,
-    fintype.univ_of_subsingleton, head_cons, mul_vec, nat.cast_one, nsmul_eq_mul, pi.zero_apply],
-  { linear_combination (hq, U^2) },
-  { linear_combination (hq, U * V) },
-  { linear_combination (hq, V^2) },
-  { linear_combination (hc, U) },
-  { linear_combination (hc, V) },
-end
+  { simp [sylvester_mul_vec_rational_normal, hc, hq] },
+  end
+
 end ring
 
 variables {K : Type*} [field K] [is_alg_closed K]
@@ -126,34 +120,67 @@ have a common root. -/
 lemma common_root_of_resultant_eq_zero (h : det (mat a₀ a₁ a₂ b₀ b₁ b₂ b₃) = 0) :
   ∃ v : fin 2 → K, v ≠ 0 ∧ eval v (q a₀ a₁ a₂) = 0 ∧ eval v (c b₀ b₁ b₂ b₃) = 0 :=
 begin
+  have hq : q a₀ a₁ a₂ = eval X (q (C a₀) (C a₁) (C a₂)),
+  { simp only [q, smul_eq_C_mul, _root_.map_add, _root_.map_mul, eval_C, map_pow, eval_X] },
+  have hc : c b₀ b₁ b₂ b₃ = eval X (c (C b₀) (C b₁) (C b₂) (C b₃)),
+  { simp only [c, smul_eq_C_mul, _root_.map_add, _root_.map_mul, eval_C, map_pow, eval_X] },
   rw ← matrix.exists_vec_mul_eq_zero_iff at h,
   obtain ⟨k, hk, habk⟩ := h,
-  rw mat at habk,
   let A : mv_polynomial (fin 2) K := C (k 3) * X 0 + C (k 4) * X 1,
   let B : mv_polynomial (fin 2) K := C (k 0) * X 0 ^ 2 + C (k 1) * X 0 * X 1 + C (k 2) * X 1 ^ 2,
   have : B * (q a₀ a₁ a₂) + A * (c b₀ b₁ b₂ b₃) = 0,
-  { simp only [B, q, A, c, smul_eq_C_mul],
-    set U : mv_polynomial (fin 2) K := X 0,
+  { set U : mv_polynomial (fin 2) K := X 0,
     set V : mv_polynomial (fin 2) K := X 1,
-    have : vec_mul (C ∘ k) (mat (C a₀) (C a₁) (C a₂) (C b₀) (C b₁) (C b₂) (C b₃))
-      = (0 : fin 5 → mv_polynomial (fin 2) K),
-    calc vec_mul (C ∘ k) (mat (C a₀) (C a₁) (C a₂) (C b₀) (C b₁) (C b₂) (C b₃))
-        = vec_mul (C ∘ k) ((mat a₀ a₁ a₂ b₀ b₁ b₂ b₃).map C) : _
-    ... = C ∘ (vec_mul k (mat a₀ a₁ a₂ b₀ b₁ b₂ b₃)) : _
-    ... = 0 : _,
+    have H : ![U ^ 2 * q a₀ a₁ a₂,
+            U * V * q a₀ a₁ a₂,
+            V ^ 2 * q a₀ a₁ a₂,
+            U * c b₀ b₁ b₂ b₃,
+            V * c b₀ b₁ b₂ b₃]
+            = mul_vec ((mat a₀ a₁ a₂ b₀ b₁ b₂ b₃).map C)
+              ![U ^ 4, U ^ 3 * V, U ^ 2 * V ^ 2, U * V ^ 3, V ^ 4],
+    { calc ![U ^ 2 * q a₀ a₁ a₂,
+            U * V * q a₀ a₁ a₂,
+            V ^ 2 * q a₀ a₁ a₂,
+            U * c b₀ b₁ b₂ b₃,
+            V * c b₀ b₁ b₂ b₃]
+          = ![U ^ 2 * eval X (q (C a₀) (C a₁) (C a₂)),
+              U * V * eval X (q (C a₀) (C a₁) (C a₂)),
+              V ^ 2 * eval X (q (C a₀) (C a₁) (C a₂)),
+              U * eval X (c (C b₀) (C b₁) (C b₂) (C b₃)),
+              V * eval X (c (C b₀) (C b₁) (C b₂) (C b₃))] : by simp only [hq, hc]
+      ... = mul_vec (mat (C a₀) (C a₁) (C a₂) (C b₀) (C b₁) (C b₂) (C b₃))
+              ![U ^ 4, U ^ 3 * V, U ^ 2 * V ^ 2, U * V ^ 3, V ^ 4] : _
+      ... = mul_vec ((mat a₀ a₁ a₂ b₀ b₁ b₂ b₃).map C)
+              ![U ^ 4, U ^ 3 * V, U ^ 2 * V ^ 2, U * V ^ 3, V ^ 4] : _,
+      { have : ![U, V] = X,
+        { ext1 i; fin_cases i; refl },
+        simp only [sylvester_mul_vec_rational_normal, this] },
+      { congr,
+        ext1 i j; fin_cases i; fin_cases j;
+        simp only [matrix.map_apply, mat, C.map_zero, cons_val_zero, cons_vec_bit1_eq_alt1,
+          cons_vec_bit0_eq_alt0, cons_append, empty_append, cons_vec_alt1, cons_vec_alt0,
+          cons_val_one, head_cons] } },
+    calc B * q a₀ a₁ a₂ + A * c b₀ b₁ b₂ b₃
+        = (C ∘ k) ⬝ᵥ ![U ^ 2 * q a₀ a₁ a₂,
+                      U * V * q a₀ a₁ a₂,
+                      V ^ 2 * q a₀ a₁ a₂,
+                      U * c b₀ b₁ b₂ b₃,
+                      V * c b₀ b₁ b₂ b₃] : _
+    ... = (vec_mul (C ∘ k) ((mat a₀ a₁ a₂ b₀ b₁ b₂ b₃).map C))
+          ⬝ᵥ ![U ^ 4, U ^ 3 * V, U ^ 2 * V ^ 2, U * V ^ 3, V ^ 4] : by rw [H, dot_product_mul_vec]
+    ... = (C ∘ (vec_mul k (mat a₀ a₁ a₂ b₀ b₁ b₂ b₃)))
+          ⬝ᵥ ![U ^ 4, U ^ 3 * V, U ^ 2 * V ^ 2, U * V ^ 3, V ^ 4] : _
+    ... = 0 : by simp [habk],
+    { have : k = ![k 0, k 1, k 2, k 3, k 4],
+      { ext1 i; fin_cases i; refl },
+      conv_rhs { rw this },
+      simp only [A, B, U, V, dot_product, fin.sum_univ_succ, function.comp_app, cons_val_zero,
+        cons_val_succ, fintype.univ_of_subsingleton, cons_val_fin_one, finset.sum_const,
+        finset.card_singleton, nsmul_eq_mul, nat.cast_one, one_mul],
+      ring },
     { congr,
-      ext1 i j;
-      fin_cases i;
-      fin_cases j;
-      simp only [matrix.map_apply, mat, C.map_zero, cons_val_zero, cons_vec_bit1_eq_alt1,
-        cons_vec_bit0_eq_alt0, cons_append, empty_append, cons_vec_alt1, cons_vec_alt0,
-        cons_val_one, head_cons], },
-    { ext1 i,
-      rw ← C.map_vec_mul (mat a₀ a₁ a₂ b₀ b₁ b₂ b₃) k i },
-    { simp [mat, habk] },
-    rw [← sylvester_mul_rational_normal (C a₀) (C a₁) (C a₂) (C b₀) (C b₁) (C b₂) (C b₃) U V
-      (C ∘ k), this],
-    simp },
+      ext i,
+      rw ← C.map_vec_mul } },
   obtain ⟨x, hx, hx', hxq, hxc⟩ :=
     match_roots_and_pigeonhole (-A) B (q a₀ a₁ a₂) (c b₀ b₁ b₂ b₃) _ _ _ _ _,
   obtain ⟨v, hv, hvx⟩ := linear_has_root hx',

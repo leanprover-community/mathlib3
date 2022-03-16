c63dbcedbc785a7633f78496756f99da6d5515be
Jujian Zhang <jujian.zhang1998@outlook.com>
2022-03-16 22:37:26 +0000

    golf

--- archive/imo/imo1984_q1.lean
+++ archive/imo/imo1984_q1.lean
@@ -25,9 +25,8 @@ So we split into two cases according to whether `0 ≤ 1 - 2z` or `1 - 2z < 0`:
 
 namespace imo1984_q1
 
-variables (x y z : ℝ)
-variables (add_eq : x + y + z = 1)
-variables (hx : 0 ≤ x) (hy : 0 ≤ y) (hz : 0 ≤ z)
+open real
+variables (x y z : ℝ) (add_eq : x + y + z = 1) (hx : 0 ≤ x) (hy : 0 ≤ y) (hz : 0 ≤ z)
 variables (hxy : x ≤ y) (hyz : y ≤ z)
 include add_eq hx hy hz
 
@@ -58,24 +57,21 @@ calc (1 - 2 * x) * (1 - 2 * y) * (1 - 2 * z)
 ... = -1 + 4 * (y * z + x * z + x * y) - 8 * x * y * z : by norm_num,
 have EQ : x * y + y * z + x * z - 2 * x * y * z =
   1/4 * (1 - 2 * x) * (1 - 2 * y) * (1 - 2 * z) + 1/4, by linarith,
+have x_ineq : 0 ≤ 1 - 2 * x, by linarith,
+have y_ineq : 0 ≤ 1 - 2 * y, by linarith,
+have xy_ineq : _, from mul_nonneg x_ineq y_ineq,
+have ineq' : (0 : ℝ) ≤ 1/3, by linarith,
+have ineq'' : (0 : ℝ) ≤ 1/4, by linarith,
+have ineq''' : (0 : ℝ) < 3, by linarith,
+have eq' : (1 : ℝ) / 3 + 1 / 3 + 1 / 3 = 1, by norm_num,
 begin
-  have x_ineq : 0 ≤ 1 - 2 * x := by linarith,
-  have y_ineq : 0 ≤ 1 - 2 * y := by linarith,
-  have xy_ineq := mul_nonneg x_ineq y_ineq,
-  have ineq' : (0 : ℝ) ≤ 1/3 := by linarith,
-  have ineq'' : (0 : ℝ) ≤ 1/4 := by linarith,
-  have eq' : (1 : ℝ) / 3 + 1 / 3 + 1 / 3 = 1 := by norm_num,
-
   by_cases z_ineq : 0 ≤ 1 - 2*z,
   { have xyz_ineq := mul_nonneg xy_ineq z_ineq,
-    have ineq1 := real.geom_mean_le_arith_mean3_weighted ineq' ineq' ineq' x_ineq y_ineq z_ineq eq',
+    have ineq1 := geom_mean_le_arith_mean3_weighted ineq' ineq' ineq' x_ineq y_ineq z_ineq eq',
     simp only [← mul_add] at ineq1,
-    rw calc 1 - 2 * x + (1 - 2 * y) + (1 - 2 * z) = 3 - 2 * (x + y + z) : by ring at ineq1,
-    rw add_eq at ineq1,
+    rw [calc 1 - 2 * x + (1 - 2 * y) + (1 - 2 * z) = 3 - 2 * (x + y + z) : by ring, add_eq] at ineq1,
     norm_num at ineq1,
-    rw [← real.mul_rpow, ← real.mul_rpow] at ineq1,
-    rw ← real.rpow_le_rpow_iff _ _ (show (0 : ℝ) < 3, by norm_num) at ineq1,
-    rw ← real.rpow_mul at ineq1,
+    rw [←mul_rpow, ←mul_rpow, ←rpow_le_rpow_iff _ _ (_ : (0 : ℝ) < 3), ←rpow_mul] at ineq1,
     norm_num at ineq1,
     have ineq2 := add_le_add (mul_le_mul (le_refl (1/4 : ℝ)) ineq1 _ _) (le_refl (1/4 : ℝ)),
     rw [← mul_assoc, ← mul_assoc, ← EQ] at ineq2,

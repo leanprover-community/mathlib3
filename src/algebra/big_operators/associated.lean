2c7681fa9f34ce99d9104bc97cca91674b683af9
Ruben Van de Velde <65514131+Ruben-VandeVelde@users.noreply.github.com>
2022-03-16 20:39:15 +0100

    Add multiset variant

--- src/algebra/big_operators/associated.lean
+++ src/algebra/big_operators/associated.lean
@@ -64,25 +64,50 @@ multiset.induction_on s (by simp [mt is_unit_iff_dvd_one.2 hp.not_unit])
       exact ⟨q, multiset.mem_cons.2 (or.inr hq₁), hq₂⟩ }
   end)
 
-lemma prod_primes_dvd
+lemma multiset.prod_primes_dvd
+  [cancel_comm_monoid_with_zero α] [Π a : α, decidable_pred (associated a)]
+  {s : multiset α} (n : α) (h : ∀ a ∈ s, prime a) (div : ∀ a ∈ s, a ∣ n)
+  (uniq : ∀ a, s.countp (associated a) ≤ 1) :
+  s.prod ∣ n :=
+begin
+  induction s using multiset.induction_on with a s induct n primes divs generalizing n,
+  { simp only [multiset.prod_zero, one_dvd] },
+  { rw multiset.prod_cons,
+    obtain ⟨k, rfl⟩ : a ∣ n := div a (multiset.mem_cons_self a s),
+    apply mul_dvd_mul_left a,
+    refine induct
+      (λ a ha, h a (multiset.mem_cons_of_mem ha))
+      (λ a, (multiset.countp_le_of_le _ (multiset.le_cons_self _ _)).trans (uniq a))
+      k (λ b b_in_s, _),
+    { have b_div_n := div b (multiset.mem_cons_of_mem b_in_s),
+      have a_prime := h a (multiset.mem_cons_self a s),
+      have b_prime := h b (multiset.mem_cons_of_mem b_in_s),
+      refine (b_prime.dvd_or_dvd b_div_n).resolve_left (λ b_div_a, _),
+      have assoc := b_prime.associated_of_dvd a_prime b_div_a,
+      have := uniq a,
+      rw [multiset.countp_cons_of_pos _ (associated.refl _), nat.succ_le_succ_iff, ←not_lt,
+        multiset.countp_pos] at this,
+      exact this ⟨b, b_in_s, assoc.symm⟩ } }
+end
+
+lemma finset.prod_primes_dvd
   [cancel_comm_monoid_with_zero α] [unique αˣ]
   {s : finset α} (n : α) (h : ∀ a ∈ s, prime a) (div : ∀ a ∈ s, a ∣ n) :
   (∏ p in s, p) ∣ n :=
 begin
   classical,
-  induction s using finset.induction_on with a s a_not_in_s induct n primes divs generalizing n,
-  { simp only [finset.prod_empty, one_dvd] },
-  { rw finset.prod_insert a_not_in_s,
-    obtain ⟨k, rfl⟩ : a ∣ n := div a (finset.mem_insert_self a s),
-    apply mul_dvd_mul_left a,
-    apply induct (λ b b_in_s, h b (finset.mem_insert_of_mem b_in_s)) k (λ b b_in_s, _),
-    have b_div_n := div b (finset.mem_insert_of_mem b_in_s),
-    have a_prime := h a (finset.mem_insert_self a s),
-    have b_prime := h b (finset.mem_insert_of_mem b_in_s),
-    refine (b_prime.dvd_or_dvd b_div_n).resolve_left (λ b_div_a, _),
-    refine ne_of_mem_of_not_mem b_in_s a_not_in_s _,
-    rw ←associated_iff_eq,
-    exact b_prime.associated_of_dvd a_prime b_div_a }
+  rw finset.prod_eq_multiset_prod,
+  refine multiset.prod_primes_dvd _ _ _ _,
+  { intros a ha,
+    simp only [multiset.map_id', ←finset.mem_def] at ha,
+    exact h a ha },
+  { intros a ha,
+    simp only [multiset.map_id', ←finset.mem_def] at ha,
+    exact div a ha },
+  { intros a,
+    rw [multiset.map_id', associated_eq_eq, multiset.countp_eq_card_filter,
+      ←multiset.count_eq_card_filter_eq],
+    exact multiset.nodup_iff_count_le_one.mp s.nodup a }
 end
 
 namespace associates

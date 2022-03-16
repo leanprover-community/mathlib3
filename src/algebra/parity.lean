c2c573f4877f83ed7dd17f5131e5491af11f2a0b
Damiano Testa <adomani@gmail.com>
2022-03-16 07:51:08 +0000

    rename theorems to lemmas and remove paragraph

--- src/algebra/parity.lean
+++ src/algebra/parity.lean
@@ -13,28 +13,24 @@ variables {α : Type*}
 section semiring
 variable [semiring α]
 
-theorem even.add_even {m n : α} (hm : even m) (hn : even n) :
-  even (m + n) :=
+lemma even.add_even {m n : α} (hm : even m) (hn : even n) : even (m + n) :=
 begin
   rcases hm with ⟨m, rfl⟩,
   rcases hn with ⟨n, rfl⟩,
   exact ⟨m + n, (mul_add _ _ _).symm⟩
 end
 
-theorem even.add_odd {m n : α} (hm : even m) (hn : odd n) :
-  odd (m + n) :=
+lemma even.add_odd {m n : α} (hm : even m) (hn : odd n) : odd (m + n) :=
 begin
   rcases hm with ⟨m, rfl⟩,
   rcases hn with ⟨n, rfl⟩,
   exact ⟨m + n, by rw [mul_add, add_assoc]⟩
 end
 
-theorem odd.add_even {m n : α} (hm : odd m) (hn : even n) :
-  odd (m + n) :=
+lemma odd.add_even {m n : α} (hm : odd m) (hn : even n) : odd (m + n) :=
 by { rw add_comm, exact hn.add_odd hm }
 
-theorem odd.add_odd {m n : α} (hm : odd m) (hn : odd n) :
-  even (m + n) :=
+lemma odd.add_odd {m n : α} (hm : odd m) (hn : odd n) : even (m + n) :=
 begin
   rcases hm with ⟨m, rfl⟩,
   rcases hn with ⟨n, rfl⟩,

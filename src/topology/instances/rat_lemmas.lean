2ccbcc7d706e3531f26beda129775666163a8f6b
Heather Macbeth <25316162+hrmacbeth@users.noreply.github.com>
2022-03-16 22:12:46 -0400

    revert splitting of hausdorff_distance

--- src/topology/instances/rat_lemmas.lean
+++ src/topology/instances/rat_lemmas.lean
@@ -4,7 +4,6 @@ Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Yury Kudryashov
 -/
 import topology.instances.irrational
-import topology.instances.rat
 import topology.alexandroff
 
 /-!
--- src/topology/metric_space/hausdorff_distance.lean
+++ src/topology/metric_space/hausdorff_distance.lean
@@ -3,9 +3,10 @@ Copyright (c) 2019 Sébastien Gouëzel. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Sébastien Gouëzel
 -/
+import analysis.normed_space.basic
+import analysis.specific_limits.basic
 import topology.metric_space.isometry
 import topology.instances.ennreal
-import analysis.specific_limits.basic
 
 /-!
 # Hausdorff distance
@@ -538,6 +539,31 @@ end
 lemma exists_mem_closure_inf_dist_eq_dist [proper_space α] (hne : s.nonempty) (x : α) :
   ∃ y ∈ closure s, inf_dist x s = dist x y :=
 by simpa only [inf_dist_eq_closure] using is_closed_closure.exists_inf_dist_eq_dist hne.closure x
+
+lemma closed_ball_inf_dist_compl_subset_closure' {E : Type*} [semi_normed_group E]
+  [normed_space ℝ E] {x : E} {s : set E} (hx : s ∈ 𝓝 x) (hs : s ≠ univ) :
+  closed_ball x (inf_dist x sᶜ) ⊆ closure s :=
+begin
+  have hne : sᶜ.nonempty, from nonempty_compl.2 hs,
+  have hpos : 0 < inf_dist x sᶜ,
+  { rwa [← inf_dist_eq_closure, ← is_closed_closure.not_mem_iff_inf_dist_pos hne.closure,
+      closure_compl, mem_compl_iff, not_not, mem_interior_iff_mem_nhds] },
+  rw ← closure_ball x hpos,
+  apply closure_mono,
+  rw [← le_eq_subset, ← is_compl_compl.disjoint_right_iff],
+  exact disjoint_ball_inf_dist
+end
+
+lemma closed_ball_inf_dist_compl_subset_closure {E : Type*} [normed_group E] [normed_space ℝ E]
+  {x : E} {s : set E} (hx : x ∈ s) (hs : s ≠ univ) :
+  closed_ball x (inf_dist x sᶜ) ⊆ closure s :=
+begin
+  by_cases hx' : x ∈ closure sᶜ,
+  { rw [mem_closure_iff_inf_dist_zero (nonempty_compl.2 hs)] at hx',
+    simpa [hx'] using subset_closure hx },
+  { rw [closure_compl, mem_compl_iff, not_not, mem_interior_iff_mem_nhds] at hx',
+    exact closed_ball_inf_dist_compl_subset_closure' hx' hs }
+end
 
 /-! ### Distance of a point to a set as a function into `ℝ≥0`. -/
 

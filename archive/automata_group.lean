5373127734c5662208958bc318be7c2406997764
Yury G. Kudryashov <urkud@urkud.name>
2022-03-18 12:29:19 -0400

    Snapshot

--- archive/automata_group.lean
+++ archive/automata_group.lean
@@ -15,6 +15,21 @@ variables {S T : Type} {X : Type} [decidable_eq X] [fintype X]
 --!! does the following structuring sound reasonable to you?
 -- notation machine := S×X → X×S
 -- structure transducer := ⟨machine : S×X→X×S,s : S⟩
+structure machine (S X : Type*) :=
+(out : S → X → X)
+(next_state : S → X → S)
+
+structure invertible_machine (S X : Type*) :=
+(out : S → equiv.perm X)
+(next_state : S → X → S)
+
+namespace machine
+
+variables (m : machine S X)
+
+def is_invertible : Prop := ∀ s, bijective (m.out s)
+
+end machine
 
 -- we could implement in lean the composition of transducers -- it's again given by a transducer, with stateset S the product of the states of the factors.
 -- more precisely, ⟨machine1,s1⟩∘⟨machine2,s2⟩ := ⟨machine1∘machine2,(s1,s2)⟩, as below
@@ -52,16 +67,18 @@ namespace grigorchuk_example
 
 open automata_group
 
+
 notation `X` := fin 2
 notation `S` := fin 5
 
-def grigorchuk_machine : S×X → X×S := λ p,
--- perhaps would be nicer with a notation like | (0,0) := (1,4) etc.
-  if p=(0,0) then (1,4) else if p=(1,0) then (0,4) else
-  if p=(0,1) then (0,0) else if p=(1,1) then (1,2) else
-  if p=(0,2) then (0,0) else if p=(1,2) then (1,3) else
-  if p=(0,3) then (0,4) else if p=(1,3) then (1,1) else
-  if p=(0,4) then (0,4) else                 (1,4)
+def grigorchuk_machine : S → X → X × S :=
+![![_, _], _, _, _, _]
+-- -- perhaps would be nicer with a notation like | (0,0) := (1,4) etc.
+--   if p=(0,0) then (1,4) else if p=(1,0) then (0,4) else
+--   if p=(0,1) then (0,0) else if p=(1,1) then (1,2) else
+--   if p=(0,2) then (0,0) else if p=(1,2) then (1,3) else
+--   if p=(0,3) then (0,4) else if p=(1,3) then (1,1) else
+--   if p=(0,4) then (0,4) else                 (1,4)
 
 #check isinvertible_machine grigorchuk_machine
 -- this fails, though it should be easily decidable

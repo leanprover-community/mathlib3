5736df81baea6efc4cfcf80d36d9fc5c98057ae2
Violeta Hernández <vi.hdz.p@gmail.com>
2022-03-19 01:29:56 -0600

    progress

--- src/algebra/algebraic_card.lean
+++ src/algebra/algebraic_card.lean
@@ -4,11 +4,18 @@ Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Violeta Hernández Palacios
 -/
 
-import data.real.basic
+import data.real.cardinality
 import data.polynomial.cardinal
 import data.rat.denumerable
 import ring_theory.algebraic
 import set_theory.cardinal_ordinal
+
+/-!
+### Cardinality of algebraic numbers
+
+In this file, we prove that the set of the real algebraic numbers over `ℚ` has cardinality `ω`. We
+do this by constructing a surjection from `polynomial ℚ × ℚ × ℚ` into this set.
+-/
 
 open cardinal polynomial
 open_locale cardinal
@@ -35,7 +42,7 @@ begin
     intro x,
     rcases x.2 with ⟨p, hp, he⟩,
     -- A finite set of reals is disconnected.
-    suffices : ∃ s t : ℝ, s < x.1 ∧ x.1 < t ∧ ∀ y, (y ∈ set.Ioo s t ∧ aeval y p = 0) → y = x.1,
+    suffices : ∃ s t : ℝ, s < x.1 ∧ x.1 < t ∧ ∀ y, y ∈ set.Ioo s t ∧ aeval y p = 0 → y = x.1,
     { rcases this with ⟨s, t, hs, ht, Hy⟩,
       -- `exists_rat_near` is close to this.
       suffices H : ∀ s t : ℝ, s < t → ∃ q : ℚ, ↑q ∈ set.Ioo s t,
@@ -43,17 +50,26 @@ begin
         cases H x t ht with r hr,
         use [p, q, r],
         change dite _ _ _ = _,
-        have : p ≠ 0 ∧ ∃ x : ℝ, x ∈ set.Ioo ↑q ↑r ∧ aeval x p = 0 := ⟨hp, sorry⟩,
-        rw dif_pos this,
+        have Hss := set.Ioo_subset_Ioo hq.1.le hr.2.le,
+        have : p ≠ 0 ∧ ∃ x : ℝ, x ∈ set.Ioo (q : ℝ) ↑r ∧ aeval x p = 0 :=
+          ⟨hp, x.val, ⟨hq.2, hr.1⟩, he⟩,
         have hc := classical.some_spec this.2,
-        exact subtype.val_inj.1 (Hy _ ⟨(set.Ioo_subset_Ioo hq.1.le hr.2.le) hc.1, hc.2⟩) },
+        rw dif_pos this,
+        exact subtype.val_inj.1 (Hy _ ⟨Hss hc.1, hc.2⟩) },
       sorry },
     sorry },
   { let g : ulift ℕ → {x : ℝ | is_algebraic ℚ x} := λ n, ⟨_, nat_is_algebraic n.down⟩,
-    have hg : function.injective g := λ m n hmn, begin
-      have := nat.cast_inj.1 (subtype.mk.inj hmn),
-      apply_fun @ulift.up ℕ at this,
-      rwa [ulift.up_down, ulift.up_down] at this
-    end,
-    exact mk_le_of_injective hg }
+    apply @mk_le_of_injective _ _ g (λ m n hmn, _),
+    have := nat.cast_inj.1 (subtype.mk.inj hmn),
+    apply_fun @ulift.up ℕ at this,
+    rwa [ulift.up_down, ulift.up_down] at this }
+end
+
+/-- There exists a transcendental number. -/
+theorem exists_transcendental : ∃ x : ℝ, transcendental ℚ x := begin
+  show ∃ x : ℝ, ¬ is_algebraic ℚ x,
+  by_contra' H : ∀ x : ℝ, x ∈ {x : ℝ | is_algebraic ℚ x},
+  have := algebraic_card,
+  rw [set.eq_univ_of_forall H, mk_univ, mk_real] at this,
+  exact omega_lt_continuum.ne' this
 end

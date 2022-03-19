b45e3551d9458437f6e7c010cdb408f41d165a55
Bolton Bailey <bolton.bailey@gmail.com>
2022-03-19 17:13:50 -0500

    undo weird change

--- src/number_theory/padics/padic_norm.lean
+++ src/number_theory/padics/padic_norm.lean
@@ -475,13 +475,13 @@ end
 lemma pow_succ_padic_val_nat_not_dvd {p n : ℕ} [hp : fact (nat.prime p)] (hn : 0 < n) :
   ¬ p ^ (padic_val_nat p n + 1) ∣ n :=
 begin
-  { rw multiplicity.pow_dvd_iff_le_multiplicity,
-    rw padic_val_nat_def hn,
-    { rw [nat.cast_add, enat.coe_get],
-      simp only [nat.cast_one, not_le],
-      exact enat.lt_add_one (ne_top_iff_finite.mpr
-        (finite_nat_iff.mpr ⟨(fact.elim hp).ne_one, hn⟩)), },
-    { apply_instance } }
+  rw multiplicity.pow_dvd_iff_le_multiplicity,
+  rw padic_val_nat_def hn,
+  { rw [nat.cast_add, enat.coe_get],
+    simp only [nat.cast_one, not_le],
+    exact enat.lt_add_one (ne_top_iff_finite.mpr
+      (finite_nat_iff.mpr ⟨(fact.elim hp).ne_one, hn⟩)), },
+  { apply_instance }
 end
 
 lemma padic_val_nat_primes {p q : ℕ} [p_prime : fact p.prime] [q_prime : fact q.prime]

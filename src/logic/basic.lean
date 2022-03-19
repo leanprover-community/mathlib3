f1fac426d7d365dffd3d7ff01121937a683bb740
Violeta Hernández <vi.hdz.p@gmail.com>
2022-03-19 16:31:37 -0600

    Restate

--- src/logic/basic.lean
+++ src/logic/basic.lean
@@ -155,10 +155,10 @@ assume ⟨h⟩, h.elim
 lemma congr_arg_heq {α} {β : α → Sort*} (f : ∀ a, β a) : ∀ {a₁ a₂ : α}, a₁ = a₂ → f a₁ == f a₂
 | a _ rfl := heq.rfl
 
-lemma ulift.down_inj {α : Sort*} : ∀ {a b : ulift α}, a.down = b.down → a = b
+lemma ulift.down_injective {α : Sort*} : function.injective (@ulift.down α)
 | ⟨a⟩ ⟨b⟩ rfl := rfl
 
-lemma plift.down_inj {α : Sort*} : ∀ {a b : plift α}, a.down = b.down → a = b
+lemma plift.down_injective {α : Sort*} : function.injective (@plift.down α)
 | ⟨a⟩ ⟨b⟩ rfl := rfl
 
 -- missing [symm] attribute for ne in core.

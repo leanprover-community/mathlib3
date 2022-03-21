4c189483ea496b677ced447eeead7b1dcb719105
Kevin Wilson <khwilson@gmail.com>
2022-03-21 12:55:50 -0500

    Clean up some proofs

--- src/data/real/sqrt.lean
+++ src/data/real/sqrt.lean
@@ -308,17 +308,18 @@ theorem neg_sqrt_lt_of_sq_lt (h : x^2 < y) : -sqrt y < x := (sq_lt.mp h).1
 
 theorem lt_sqrt_of_sq_lt (h : x^2 < y) : x < sqrt y := (sq_lt.mp h).2
 
-/- The natural square root is at most the real square root -/
-lemma nat_sqrt_le_real_sqrt {a : ℕ} : ↑(nat.sqrt a) ≤ real.sqrt ↑a :=
+/-- The natural square root is at most the real square root -/
+lemma nat_sqrt_le_real_sqrt
+{a : ℕ} : ↑(nat.sqrt a) ≤ real.sqrt ↑a :=
 begin
-  rw real.le_sqrt,
+  rw real.le_sqrt (nat.cast_nonneg _) (nat.cast_nonneg _),
   norm_cast,
   exact nat.sqrt_le' a,
-  simp, simp,
 end
 
-/- The real square root is at most the natural square root plus one -/
-lemma real_sqrt_le_nat_sqrt_succ {a : ℕ} : real.sqrt ↑a ≤ nat.sqrt a + 1 :=
+/-- The real square root is at most the natural square root plus one -/
+lemma real_sqrt_le_nat_sqrt_succ {a : ℕ} : real.sqrt ↑a ≤ nat.sqrt a + 1
+:=
 begin
   rw real.sqrt_le_iff,
   split,

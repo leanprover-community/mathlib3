2a1ca7a3f508cc1609b72a83db8869c983dd23b6
Mantas Baksys <baksys.mantas@gmail.com>
2022-03-22 12:01:47 +0000

    avoid p^2

--- src/number_theory/multiplicity.lean
+++ src/number_theory/multiplicity.lean
@@ -23,120 +23,131 @@ This file contains results in number theory relating to multiplicity.
   (https://en.wikipedia.org/wiki/Lifting-the-exponent_lemma)
 -/
 
-variables {α R : Type*}
+variables {R : Type*} {n : ℕ}
 
 open ideal ideal.quotient
-
-section ring
-variables [ring α]
-
-lemma dvd_sub_comm {a b c : α} : a ∣ b - c ↔ a ∣ c - b := by rw [←dvd_neg, neg_sub]
-
-end ring
-
-section comm_ring
-variables [comm_ring R]
-
-lemma dvd_geom_sum₂ {n : ℕ} {x y : R} (h : ↑n ∣ x - y) : ↑n ∣ geom_sum₂ x y n :=
-begin
-  rw ←mem_span_singleton at ⊢ h,
-  rw ←ideal.quotient.eq at h,
-  rw [←eq_zero_iff_mem, ring_hom.map_geom_sum₂, h, geom_sum₂_self],
-  apply mul_eq_zero_of_left,
-  rw [←map_nat_cast (mk $ span ({n} : set R)) n, eq_zero_iff_mem, mem_span_singleton],
-end
-
-lemma multiplicity.geom_sum₂_eq_zero [@decidable_rel R (∣)] {p : R} (hp : prime p)
-  {x y : R} (hxy : p ∣ x - y) (hx : ¬p ∣ x) {n : ℕ} (hn : ¬p ∣ n) :
-  multiplicity p (geom_sum₂ x y n) = 0 :=
-begin
-  apply multiplicity.multiplicity_eq_zero_of_not_dvd,
-  rw ←mem_span_singleton at *,
-  rw ←ideal.quotient.eq at hxy,
-  rw ←eq_zero_iff_mem at *,
-  haveI := (span_singleton_prime hp.1).mpr hp,
-  rw [ring_hom.map_geom_sum₂, ←hxy, geom_sum₂_self, mul_eq_zero],
-  exact not_or (by rwa map_nat_cast at hn) (λ h, hx $ pow_eq_zero h),
-end
-
-section helpers
-
 open_locale big_operators
 
-variables {a b : R} {p n : ℕ} (hp2 : (p : R) ^ 2 = 0)
-include hp2
+section comm_ring
+variables [comm_ring R] {a b x y : R}
 
-private lemma add_mul_pow_eq : (a + p * b) ^ n = a ^ (n - 1) * (p * b) * n + a ^ n :=
+lemma dvd_geom_sum₂ {x y : R} (h : ↑n ∣ x - y) : ↑n ∣ geom_sum₂ x y n :=
 begin
-  cases n,
-  { rw [pow_zero, pow_zero, nat.cast_zero, mul_zero, zero_add] },
-  suffices : ∀ i ∈ finset.range n, a ^ i * (p * b) ^ (n + 1 - i) * (n + 1).choose i = 0,
-  { rw [nat.succ_sub_one, ←nat.add_one, add_pow, finset.sum_range_succ, finset.sum_range_succ,
-      finset.sum_eq_zero this, nat.choose_self, nat.cast_one, nat.choose_succ_self_right,
-      nat.sub_self, nat.add_sub_cancel_left],
-    ring },
-  intros i hi,
-  have h : (0 : R) ∣ p ^ (n + 1 - i) := hp2 ▸ pow_dvd_pow _
-    (le_tsub_of_add_le_left $ nat.succ_le_succ $ finset.mem_range.mp hi),
-  rw [mul_pow, eq_zero_of_zero_dvd h, zero_mul, mul_zero, zero_mul],
+  rw ← mem_span_singleton at ⊢ h,
+  rw ← ideal.quotient.eq at h,
+  rw [← eq_zero_iff_mem, ring_hom.map_geom_sum₂, h, geom_sum₂_self],
+  apply mul_eq_zero_of_left,
+  rw [← map_nat_cast (mk $ span ({n} : set R)) n, eq_zero_iff_mem, mem_span_singleton]
 end
 
-alias even_iff_two_dvd ↔ even.two_dvd _
-
-private lemma geom_sum₂_add_mul (hp : odd p) : geom_sum₂ (a + p * b) a p = p * a ^ (p - 1) :=
-have h : ∀ i ∈ finset.range p, a ^ ((i - 1) + (p - 1 - i)) * i = a ^ (p - 2) * i,
+lemma sq_dvd_add_mul_pow_sub (p x y : R) (n : ℕ) :
+  p ^ 2 ∣ (x + p * y) ^ n - (x ^ (n - 1) * (p * y) * n + x ^ n) :=
 begin
-  intros i hi,
-  cases i,
-  { rw [nat.cast_zero, mul_zero, mul_zero] },
-  { congr' 2,
-    rw ←nat.add_sub_assoc (nat.le_pred_of_lt (finset.mem_range.mp hi)),
-    exact congr_arg nat.pred (nat.add_sub_cancel_left _ _) }
-end,
-calc  ∑ i in finset.range p, (a + p * b) ^ i * a ^ (p - 1 - i)
-    = ∑ i in finset.range p,
-        (a ^ i * a ^ (p - 1 - i) + b * p * (a ^ ((i - 1) + (p - 1 - i)) * i)) :
-  finset.sum_congr rfl $ λ i hi, by rw add_mul_pow_eq hp2; ring_exp
-... = p * a ^ (p - 1) + b * a ^ (p - 2) * ↑((p - 1) / 2) * ↑p ^ 2 :
-  by rw [finset.sum_add_distrib, ←geom_sum₂, geom_sum₂_self, ←finset.mul_sum,
-    finset.sum_congr rfl h, ←finset.mul_sum, ←nat.cast_sum, finset.sum_range_id,
-    nat.mul_div_assoc _ (hp.sub_odd odd_one).two_dvd, nat.cast_mul]; ring
-... = p * a ^ (p - 1) :
-  by rw [hp2, mul_zero, add_zero]
+  cases n,
+  { simp only [pow_zero, nat.cast_zero, mul_zero, sub_self, dvd_zero, zero_add] },
+  { simp only [nat.succ_sub_succ_eq_sub, tsub_zero, nat.cast_succ, add_pow,
+    finset.sum_range_succ, nat.choose_self, nat.succ_sub _, tsub_self, pow_one,
+    nat.choose_succ_self_right, pow_zero, mul_one, nat.cast_zero, zero_add, nat.succ_eq_add_one],
+    abel,
+    apply finset.dvd_sum,
+    intros x hx,
+    apply dvd_mul_of_dvd_left,
+    apply dvd_mul_of_dvd_right,
+    rw mul_pow,
+    apply dvd_mul_of_dvd_left,
+    apply pow_dvd_pow,
+    apply le_tsub_of_add_le_left,
+    linarith [finset.mem_range.mp hx]  },
+end
 
-end helpers
+lemma not_dvd_geom_sum₂ {p : R} (hp : prime p)
+  (hxy : p ∣ x - y) (hx : ¬p ∣ x) (hn : ¬p ∣ n) :
+  ¬p ∣ geom_sum₂ x y n :=
+begin
+  rw ← mem_span_singleton at *,
+  rw ← ideal.quotient.eq at hxy,
+  rw ← eq_zero_iff_mem at *,
+  haveI := (span_singleton_prime hp.1).mpr hp,
+  rw [ring_hom.map_geom_sum₂, ←hxy, geom_sum₂_self, mul_eq_zero],
+  exact not_or (by rwa map_nat_cast at hn) (λ h, hx $ pow_eq_zero h)
+end
 
-end comm_ring
+variables {p : ℕ}
+
+lemma odd_sq_dvd_geom_sum₂_sub (hp : odd p) :
+  ↑p ^ 2 ∣ geom_sum₂ (a + p * b) a p - p * a ^ (p - 1) :=
+begin
+  have h1 := λ i, sq_dvd_add_mul_pow_sub ↑p a b i,
+  simp_rw [← mem_span_singleton, ← ideal.quotient.eq] at *,
+  simp_rw [ring_hom.map_geom_sum₂, geom_sum₂, ← map_pow, h1, ← ring_hom.map_mul],
+  ring_exp,
+  simp only [← pow_add, ring_hom.map_add, finset.sum_add_distrib, ← ring_hom.map_sum],
+  have h2 : (∑ (x : ℕ) in finset.range p, a ^ (x + (p - 1 - x))) =
+    ∑ (x : ℕ) in finset.range p, a ^ (p - 1),
+  { apply finset.sum_congr rfl,
+    intros x hx,
+    congr,
+    rw finset.mem_range at hx,
+    have hxp : x ≤ p - 1, { exact nat.le_pred_of_lt hx },
+    zify [hxp],
+    simp only [add_sub_cancel'_right] },
+  simp only [finset.sum_const, finset.card_range, nsmul_eq_mul] at h2,
+  simp only [h2, add_left_eq_self, ← mul_assoc, ← pow_add, mul_comm b _, mul_comm ↑p _,
+    ← finset.sum_mul],
+  have h3 : ∑ (x : ℕ) in finset.range p, a ^ (x - 1 + (p - 1 - x)) * ↑x =
+    ∑ (x : ℕ) in finset.range p, a ^ (p - 2) * x,
+  { apply finset.sum_congr rfl _,
+    intros i hi,
+    cases i,
+    { rw [nat.cast_zero, mul_zero, mul_zero] },
+    { congr' 2,
+      rw ←nat.add_sub_assoc (nat.le_pred_of_lt (finset.mem_range.mp hi)),
+      exact congr_arg nat.pred (nat.add_sub_cancel_left _ _) }},
+  rw [h3, ← finset.mul_sum, ← nat.cast_sum, finset.sum_range_id,
+    nat.mul_div_assoc _ (even_iff_two_dvd.mp (nat.odd.sub_odd hp odd_one)), nat.cast_mul],
+  ring_exp,
+  simp only [ring_hom.map_mul, mul_eq_zero_of_right, mul_eq_zero_of_left,
+    ideal.quotient.eq_zero_iff_mem, mem_span_singleton],
+end
+
 
 namespace multiplicity
 
 section integral_domain
-variables [comm_ring R] [is_domain R] [@decidable_rel R (∣)]
+variables [is_domain R] [@decidable_rel R (∣)]
 
 lemma pow_sub_pow' {p : R} (hp : prime p) {x y : R} (hxy : p ∣ x - y) (hx : ¬p ∣ x)
   {n : ℕ} (hn : ¬p ∣ n) :
   multiplicity p (x ^ n - y ^ n) = multiplicity p (x - y) :=
-by rw [←geom_sum₂_mul, multiplicity.mul hp, geom_sum₂_eq_zero hp hxy hx hn, zero_add]
+by rw [←geom_sum₂_mul, multiplicity.mul hp,
+  multiplicity_eq_zero_of_not_dvd (not_dvd_geom_sum₂ hp hxy hx hn), zero_add]
 
-variables {p : ℕ} (hp : prime (p : R)) (hp1 : odd p) {x y : R} (hxy : ↑p ∣ x - y) (hx : ¬↑p ∣ x)
+variables (hp : prime (p : R)) (hp1 : odd p) (hxy : ↑p ∣ x - y) (hx : ¬↑p ∣ x)
 include hp hp1 hxy hx
 
 lemma geom_sum₂_eq_one : multiplicity ↑p (geom_sum₂ x y p) = 1 :=
 begin
-  rw ←nat.cast_one,
+  rw ← nat.cast_one,
   refine multiplicity.eq_coe_iff.2 ⟨_, _⟩,
   { rw pow_one,
     exact dvd_geom_sum₂ hxy },
-  let I : ideal R := span {p ^ 2},
-  have hp' : (p : R ⧸ I) ^ 2 = 0,
-  { rw [←map_nat_cast (mk I), ←ring_hom.map_pow, eq_zero_iff_mem, mem_span_singleton] },
-  rw dvd_iff_dvd_of_dvd_sub hxy at hx,
-  cases hxy with k hk,
-  rw [←mem_span_singleton, ←eq_zero_iff_mem, ring_hom.map_geom_sum₂, sub_eq_iff_eq_add'.mp hk,
-    ring_hom.map_add, ring_hom.map_mul, map_nat_cast, geom_sum₂_add_mul hp' hp1,
-    ←map_nat_cast (mk I), ←ring_hom.map_pow, ←ring_hom.map_mul, eq_zero_iff_mem,
-    mem_span_singleton, pow_two, mul_dvd_mul_iff_left hp.1],
-  exact λ h, hx (hp.dvd_of_dvd_pow h)
+  { have hy : ¬↑p ∣ y,
+    { intro y,
+      apply hx,
+      exact (dvd_iff_dvd_of_dvd_sub hxy).mpr y },
+    cases hxy with k hk,
+    replace hk : x = y + p * k, { exact eq_add_of_sub_eq' hk },
+    have h1 := @odd_sq_dvd_geom_sum₂_sub _ _ y k _ hp1,
+    intro hp1,
+    rw [one_add_one_eq_two, hk] at hp1,
+    replace h1 : ↑p ^ 2 ∣ ↑p * y ^ (p - 1), { exact (dvd_iff_dvd_of_dvd_sub h1).mp hp1 },
+    rw [pow_two, mul_dvd_mul_iff_left] at h1,
+    { replace hy : ¬↑p ∣ y ^ (p - 1),
+      { intro hyp,
+        apply hy,
+        exact prime.dvd_of_dvd_pow hp h1},
+      apply hy h1 },
+    { exact prime.ne_zero hp } }
 end
 
 lemma pow_prime_sub_pow_prime : multiplicity ↑p (x ^ p - y ^ p) = multiplicity ↑p (x - y) + 1 :=
@@ -157,7 +168,7 @@ end
 end integral_domain
 
 section lifting_the_exponent
-variables {p : ℕ} (hp : p.prime) (hp1 : odd p)
+variables (hp : nat.prime p) (hp1 : odd p)
 include hp hp1
 
 /-- **Lifting the exponent lemma** for odd primes. -/
@@ -212,9 +223,9 @@ end
 
 end lifting_the_exponent
 end multiplicity
+end comm_ring
 
 namespace padic_val_nat
-section
 
 variables {p : ℕ} [hp : fact p.prime] (hp1 : odd p) {x y : ℕ}
 include hp hp1
@@ -244,5 +255,4 @@ begin
   { exact (nat.lt_add_left _ _ _ (pow_pos y.succ_pos _)).ne' }
 end
 
-end
 end padic_val_nat

e23ff2e011337dadbd2133c5246791414b8b223b
adomani <adomani@gmail.com>
2022-03-22 18:19:20 +0100

    even first, odd later

--- src/algebra/parity.lean
+++ src/algebra/parity.lean
@@ -28,6 +28,37 @@ by { ext x, simp [even, eq_comm] }
 @[simp] lemma even_bit0 (a : α) : even (bit0 a) :=
 ⟨a, by rw [bit0, two_mul]⟩
 
+lemma even.add_even (hm : even m) (hn : even n) : even (m + n) :=
+begin
+  rcases hm with ⟨m, rfl⟩,
+  rcases hn with ⟨n, rfl⟩,
+  exact ⟨m + n, (mul_add _ _ _).symm⟩
+end
+
+@[simp] lemma even_zero : even (0 : α) := ⟨0, (mul_zero _).symm⟩
+
+@[simp] lemma even_two : even (2 : α) := ⟨1, (mul_one _).symm⟩
+
+lemma even_two_mul (m : α) : even (2 * m) := ⟨m, rfl⟩
+
+lemma add_monoid_hom.even (f : α →+ β) (hm : even m) : even (f m) :=
+begin
+  rcases hm with ⟨m, rfl⟩,
+  exact ⟨f m, by simp [two_mul]⟩
+end
+
+@[simp] lemma even.mul_right (hm : even m) (n) : even (m * n) :=
+(add_monoid_hom.mul_right n).even hm
+
+@[simp] lemma even.mul_left (hm : even m) (n) : even (n * m) :=
+(add_monoid_hom.mul_left n).even hm
+
+lemma even.pow_of_ne_zero (hm : even m) : ∀ {a : ℕ}, a ≠ 0 → even (m ^ a)
+| 0       a0 := (a0 rfl).elim
+| (a + 1) _  := by { rw pow_succ, exact hm.mul_right _ }
+
+section with_odd
+
 /-- An element `a` of a semiring is odd if there exists `k` such `a = 2*k + 1`. -/
 def odd (a : α) : Prop := ∃ k, a = 2*k + 1
 
@@ -37,13 +68,6 @@ def odd (a : α) : Prop := ∃ k, a = 2*k + 1
 @[simp] lemma range_two_mul_add_one (α : Type*) [semiring α] :
   set.range (λ x : α, 2 * x + 1) = {a | odd a} :=
 by { ext x, simp [odd, eq_comm] }
-
-lemma even.add_even (hm : even m) (hn : even n) : even (m + n) :=
-begin
-  rcases hm with ⟨m, rfl⟩,
-  rcases hn with ⟨n, rfl⟩,
-  exact ⟨m + n, (mul_add _ _ _).symm⟩
-end
 
 lemma even.add_odd (hm : even m) (hn : odd n) : odd (m + n) :=
 begin
@@ -64,34 +88,16 @@ begin
   refl
 end
 
-@[simp] lemma even_zero : even (0 : α) := ⟨0, (mul_zero _).symm⟩
-
 @[simp] lemma odd_one : odd (1 : α) :=
 ⟨0, (zero_add _).symm.trans (congr_arg (+ (1 : α)) (mul_zero _).symm)⟩
 
-@[simp] lemma even_two : even (2 : α) := ⟨1, (mul_one _).symm⟩
-
-lemma even_two_mul (m : α) : even (2 * m) := ⟨m, rfl⟩
-
 @[simp] lemma odd_two_mul_add_one (m : α) : odd (2 * m + 1) := ⟨m, rfl⟩
-
-lemma add_monoid_hom.even (f : α →+ β) (hm : even m) : even (f m) :=
-begin
-  rcases hm with ⟨m, rfl⟩,
-  exact ⟨f m, by simp [two_mul]⟩
-end
 
 lemma ring_hom.odd (f : α →+* β) (hm : odd m) : odd (f m) :=
 begin
   rcases hm with ⟨m, rfl⟩,
   exact ⟨f m, by simp [two_mul]⟩
 end
-
-@[simp] lemma even.mul_right (hm : even m) (n) : even (m * n) :=
-(add_monoid_hom.mul_right n).even hm
-
-@[simp] lemma even.mul_left (hm : even m) (n) : even (n * m) :=
-(add_monoid_hom.mul_left n).even hm
 
 @[simp] lemma odd.mul_odd (hm : odd m) (hn : odd n) : odd (m * n) :=
 begin
@@ -102,13 +108,11 @@ begin
     ← nat.cast_two, ← nat.cast_comm],
 end
 
-lemma even.pow_of_ne_zero (hm : even m) : ∀ {a : ℕ}, a ≠ 0 → even (m ^ a)
-| 0       a0 := (a0 rfl).elim
-| (a + 1) _  := by { rw pow_succ, exact hm.mul_right _ }
-
 lemma odd.pow (hm : odd m) : ∀ {a : ℕ}, odd (m ^ a)
 | 0       := by { rw pow_zero, exact odd_one }
 | (a + 1) := by { rw pow_succ, exact hm.mul_odd odd.pow }
+
+end with_odd
 
 end semiring
 
@@ -117,6 +121,16 @@ variables [ring α] {m n : α}
 
 @[simp] theorem even_neg (a : α) : even (-a) ↔ even a :=
 dvd_neg _ _
+
+@[simp] lemma even_neg_two : even (- 2 : α) := by simp
+
+lemma even.sub_even (hm : even m) (hn : even n) : even (m - n) :=
+by { rw sub_eq_add_neg, exact hm.add_even ((even_neg n).mpr hn) }
+
+-- from src/algebra/order/ring.lean
+variables [linear_order α]
+lemma even_abs {a : α} : even (|a|) ↔ even a :=
+dvd_abs _ _
 
 lemma odd.neg {a : α} (hp : odd a) : odd (-a) :=
 begin
@@ -131,11 +145,6 @@ end
 
 @[simp] lemma odd_neg_one : odd (- 1 : α) := by simp
 
-@[simp] lemma even_neg_two : even (- 2 : α) := by simp
-
-lemma even.sub_even (hm : even m) (hn : even n) : even (m - n) :=
-by { rw sub_eq_add_neg, exact hm.add_even ((even_neg n).mpr hn) }
-
 theorem odd.sub_even (hm : odd m) (hn : even n) : odd (m - n) :=
 by { rw sub_eq_add_neg, exact hm.add_even ((even_neg n).mpr hn) }
 
@@ -146,11 +155,6 @@ lemma odd.sub_odd (hm : odd m) (hn : odd n) : even (m - n) :=
 by { rw sub_eq_add_neg, exact hm.add_odd ((odd_neg n).mpr hn) }
 
 -- from src/algebra/order/ring.lean
-variables [linear_order α]
-lemma even_abs {a : α} : even (|a|) ↔ even a :=
-dvd_abs _ _
-
--- from src/algebra/order/ring.lean
 lemma odd_abs {a : α} : odd (abs a) ↔ odd a :=
 by { cases abs_choice a with h h; simp only [h, odd_neg] }
 
@@ -159,9 +163,7 @@ end ring
 
 -- from src/algebra/group_power/lemmas.lean
 section powers
---open nat int
-variables --{M : Type u} {N : Type v} {G : Type w} {H : Type x} {A : Type y} {B : Type z}
- {R : Type*} --{S : Type u₂}
+variables {R : Type*}
   {a : R} {n : ℕ} [linear_ordered_ring R]
 
 lemma even.pow_nonneg (hn : even n) (a : R) : 0 ≤ a ^ n :=
@@ -212,15 +214,16 @@ lemma fintype.card_fin_even {k : ℕ} : fact (even (fintype.card (fin (bit0 k)))
 
 -- from src/algebra/field_power.lean
 section field_power
+variable {K : Type*}
 
-lemma even.zpow_neg {K : Type*} [division_ring K] {n : ℤ} (h : even n) (a : K) :
+lemma even.zpow_neg [division_ring K] {n : ℤ} (h : even n) (a : K) :
   (-a) ^ n = a ^ n :=
 begin
   obtain ⟨k, rfl⟩ := h,
   rw [←bit0_eq_two_mul, zpow_bit0_neg],
 end
 
-variables {K : Type*} [linear_ordered_field K] {n : ℤ} {a : K}
+variables [linear_ordered_field K] {n : ℤ} {a : K}
 
 lemma even.zpow_nonneg (hn : even n) (a : K) :
   0 ≤ a ^ n :=

e8a805dd174b98a30619f58d8a8d140e134b4c2c
Jujian Zhang <jujian.zhang1998@outlook.com>
2022-03-23 08:49:23 +0000

    Update exact.lean

--- src/category_theory/abelian/exact.lean
+++ src/category_theory/abelian/exact.lean
@@ -114,8 +114,8 @@ lemma is_equivalence.exact_iff {D : Type u₁} [category.{v₁} D] [abelian D]
   (F : C ⥤ D) [is_equivalence F] :
   exact (F.map f) (F.map g) ↔ exact f g :=
 begin
-  rw [is_iso.comp_left_eq_zero (kernel_comparison g F), ← category.assoc,
-    is_iso.comp_right_eq_zero _ (cokernel_comparison f F)],
+  simp only [exact_iff, ← F.map_eq_zero_iff, F.map_comp, category.assoc,
+     ← kernel_comparison_comp_ι g F, ← π_comp_cokernel_comparison f F],
 end
 
 /-- If `(f, g)` is exact, then `images.image.ι f` is a kernel of `g`. -/

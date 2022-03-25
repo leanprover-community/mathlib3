156ae6f9d9f5490fedcf672bdcc43c18a26564ca
pbazin <pierre-alexandre.bazin@ens.psl.eu>
2022-03-25 11:46:09 +0100

    finished pairwise

--- src/algebra/module/torsion_pid.lean
+++ src/algebra/module/torsion_pid.lean
@@ -83,15 +83,17 @@ end
 end multiset
 
 namespace dfinsupp
-instance {ι : Type*} [is_empty ι] {β : ι → Type*} [Π i, has_zero (β i)] :
-  subsingleton (Π₀ i, β i) :=
-  ⟨λ a b, by { ext, exact is_empty_elim i }⟩
+variables {ι : Type*} {α β : ι → Type*} [Π i, has_zero (β i)]
+instance [is_empty ι] : subsingleton (Π₀ i, β i) := ⟨λ a b, by { ext, exact is_empty_elim i }⟩
+lemma dfinsupp_sigma {β : (Σ i, α i) → Type*} [Π i, has_zero (β i)] :
+  (Π₀ i, β i) ≃ Π₀ i j, β ⟨i, j⟩ := sorry
+
 end dfinsupp
 
 namespace direct_sum
 open_locale direct_sum
-instance {ι : Type*} [is_empty ι] {β : ι → Type*} [Π i, add_comm_monoid (β i)] :
-  subsingleton (⨁ i, β i) := dfinsupp.subsingleton
+variables {ι : Type*} {β : ι → Type*} [Π i, add_comm_monoid (β i)]
+instance [is_empty ι] : subsingleton (⨁ i, β i) := dfinsupp.subsingleton
 end direct_sum
 
 namespace set
@@ -110,6 +112,33 @@ begin
 end
 end set
 
+namespace module
+open linear_map
+variables  {R A M B : Type*} [semiring R] [add_comm_monoid A] [module R A]
+  [add_comm_group B] [module R B] [add_comm_group M] [module R M]
+
+noncomputable lemma equiv_prod_of_split_exact (j : A →ₗ[R] M) (g : M →ₗ[R] B) (f : B →ₗ[R] M)
+  (hj : function.injective j) (exac : j.range = g.ker) (h : g.comp f = linear_map.id) :
+  (A × B) ≃ₗ[R] M :=
+begin
+  have : ∀ x, ∃ a, j a = x - f (g x) := λ x, by
+    rw [← mem_range, exac, mem_ker, map_sub, ← comp_apply g f, h, id_apply, sub_eq_zero],
+  refine equiv.to_linear_equiv ⟨_, pi.prod (λ x, (this x).some) g, λ x, _, λ x, _⟩
+    (j.coprod f).is_linear,
+  { obtain ⟨a, b⟩ := x, simp only [pi.prod, prod.mk.inj_iff],
+    have gj : g (j a) = 0 := by { rw [← mem_ker, ← exac, mem_range], exact ⟨a, rfl⟩ },
+    have gf : g (f b) = b := by rw [← comp_apply, h, id_apply],
+    split,
+    { apply hj, rw (this _).some_spec,
+      simp only [coe_comp, pi.add_apply, function.comp_app, fst_apply, snd_apply, map_add],
+      rw [gj, gf, map_zero, zero_add, add_sub_cancel] },
+    { simp only [coe_comp, pi.add_apply, function.comp_app, fst_apply, snd_apply, map_add],
+      rw [gj, gf, zero_add] } },
+  { simp only [pi.prod, coe_comp, pi.add_apply, function.comp_app, fst_apply, snd_apply],
+    rw [(this x).some_spec, sub_add_cancel] }
+end
+end module
+
 namespace submodule
 variables {R M R₂ M₂ : Type*} [ring R] [add_comm_group M] [module R M]
   [ring R₂] [add_comm_group M₂] [module R₂ M₂] {τ₁₂ : R →+* R₂}
@@ -117,7 +146,7 @@ def liftq_span_singleton (x : M) (f : M →ₛₗ[τ₁₂] M₂) (h : f x = 0) 
 (R ∙ x).liftq f $ by rw [span_singleton_le_iff_mem, linear_map.mem_ker, h]
 end submodule
 
-section pid
+/-section pid
 open_locale direct_sum
 open submodule
 
@@ -215,7 +244,7 @@ open finset multiset submodule.quotient
 
 theorem torsion_by_prime_power_decomposition' (d : ℕ) (s : fin d → M)
   (hs : span R (set.range s) = ⊤) :
-  ∃ (k : fin d → ℕ), nonempty $ (⨁ (i : fin d), R ⧸ R ∙ (p ^ (k i : ℕ))) ≃ₗ[R] M :=
+  ∃ (k : fin d → ℕ), nonempty $ M ≃ₗ[R] ⨁ (i : fin d), R ⧸ R ∙ (p ^ (k i : ℕ)) :=
 begin
   unfreezingI { induction d with d IH generalizing M },
   { use λ i, fin_zero_elim i,
@@ -229,20 +258,20 @@ begin
     let s' : fin d → M ⧸ R ∙ s j := mk ∘ s ∘ j.succ_above,
     haveI : decidable_eq (M ⧸ R ∙ s j) := by { classical, apply_instance },
     obtain ⟨k, ⟨f⟩⟩ := IH _ s' _,
-    /-{ have : ∀ i : fin d, ∃ x : M, p ^ k i • x = 0 ∧ mk x = f (direct_sum.lof R _ _ i 1),
+    { have : ∀ i : fin d, ∃ x : M, p ^ k i • x = 0 ∧ f (mk x) = direct_sum.lof R _ _ i 1,
       { intro i,
-        let fi := f.to_linear_map.comp (direct_sum.lof _ _ _ i),
+        let fi := f.symm.to_linear_map.comp (direct_sum.lof _ _ _ i),
         have fi1 := mk_surjective (R ∙ s j) (fi 1),
-        have : p ^ k i • classical.some fi1 ∈ R ∙ s j,
-        { rw [← quotient.mk_eq_zero, mk_smul, classical.some_spec fi1, ← fi.map_smul],
+        have : p ^ k i • fi1.some ∈ R ∙ s j,
+        { rw [← quotient.mk_eq_zero, mk_smul, fi1.some_spec, ← fi.map_smul],
           convert fi.map_zero, change _ • submodule.quotient.mk _ = _,
           rw [← mk_smul, quotient.mk_eq_zero, algebra.id.smul_eq_mul, mul_one],
           exact mem_span_singleton_self _ },
         obtain ⟨a, ha⟩ := p_pow_smul_lift hM sorry this,
-        refine ⟨classical.some fi1 - a • s j, by rw [smul_sub, sub_eq_zero, ha], _⟩,
+        refine ⟨fi1.some - a • s j, by rw [smul_sub, sub_eq_zero, ha], _⟩,
         rw [mk_sub, mk_smul, (quotient.mk_eq_zero _).mpr $ mem_span_singleton_self _,
-          smul_zero, sub_zero],
-        exact classical.some_spec fi1 },
+          smul_zero, sub_zero, fi1.some_spec],
+        simp only [linear_map.coe_comp, f.symm.coe_to_linear_map, f.apply_symm_apply] },
       use fin.cons (nat.find $ hM $ s j) k,
       refine ⟨linear_equiv.of_linear
         (direct_sum.to_module R (fin d.succ) M $ fin.cons
@@ -255,15 +284,16 @@ begin
         _ _⟩,
       { rw fin.cons_succ, exact (classical.some_spec $ this i).left },
       { sorry },
-      { sorry } }-/sorry,
+      { sorry } },
     { rintro ⟨x⟩, obtain ⟨N, hN⟩ := hM x, use N,
       rw [quotient.quot_mk_eq_mk, ← quotient.mk_smul, hN, quotient.mk_zero] },
     { have hs' := congr_arg (submodule.map $ mkq $ R ∙ s j) hs,
       rw [submodule.map_span, submodule.map_top, range_mkq] at hs', simp only [mkq_apply] at hs',
-      simp only [s'], rw [set.range_comp (mk ∘ s), fin.range_succ_above],
-      simp only at hs', rw [← set.range_comp, ← set.insert_image_compl_eq_range _ j] at hs',
-      sorry } }
+      simp only [s'], rw [set.range_comp (mk ∘ s), fin.range_succ_above], simp only at hs',
+      rw [← set.range_comp, ← set.insert_image_compl_eq_range _ j, function.comp_apply,
+        (quotient.mk_eq_zero _).mpr (mem_span_singleton_self _), span_insert_zero] at hs',
+      exact hs' } }
 end
 end p_torsion
 
-end pid
+end pid-/
--- src/ring_theory/coprime/pairwise.lean
+++ src/ring_theory/coprime/pairwise.lean
@@ -1,34 +1,39 @@
 import ring_theory.coprime.lemmas
+import algebra.big_operators.ring
 open_locale big_operators
 
-section subsingleton
+namespace finset
+variables {α : Type*} {s : finset α}
 
-variables {R : Type*} [comm_semiring R] [subsingleton R] {x y : R}
-
-@[nontriviality, simp] lemma is_coprime_subsingleton : is_coprime = λ x y : R, true :=
-by { funext, simp }
-
-end subsingleton
-
-section finset
-
-lemma pairwise_insert {α} {s : finset α} {a : α} (h : a ∉ s) (r : α → α → Prop) :
-  pairwise (r on λ a : s.cons a h, a) ↔ pairwise (r on λ a : s, a) ∧ ∀ b ∈ s, r a b ∧ r b a :=
-sorry
+lemma pairwise_cons {a : α} (ha : a ∉ s) (r : α → α → Prop) :
+  pairwise (r on λ a : s.cons a ha, a) ↔ pairwise (r on λ a : s, a) ∧ ∀ b ∈ s, r a b ∧ r b a :=
+begin
+  have : ∀ {s : finset α} {b c : α} {hb : b ∈ s} {hc : c ∈ s}, (⟨b, hb⟩ : s) = ⟨c, hc⟩ → b = c :=
+    λ s b c hb hc eq, congr_arg coe eq,
+  refine ⟨λ h, ⟨λ ⟨b, hb⟩ ⟨c, hc⟩ ne, h ⟨b, subset_cons ha hb⟩ ⟨c, subset_cons ha hc⟩
+    (λ eq, ne $ subtype.ext $ this eq),
+    λ b hb, ⟨h ⟨a, mem_cons_self _ _⟩ ⟨b, subset_cons ha hb⟩ (λ eq, ha $ (this eq).symm.cases_on hb),
+      h ⟨b, subset_cons ha hb⟩ ⟨a, mem_cons_self _ _⟩ (λ eq, ha $ (this eq).cases_on hb)⟩⟩,
+  λ h b c ne, _⟩,
+  obtain ⟨b, hb⟩ := b, obtain ⟨c, hc⟩ := c, change r b c, rw mem_cons at hb hc,
+  cases hb with hb hb; cases hc with hc hc,
+  { exfalso, apply ne, ext, change b = c, rw [hb, hc] },
+  { rw hb, exact (h.right c hc).left },
+  { rw hc, exact (h.right b hb).right },
+  { exact h.left ⟨b, hb⟩ ⟨c, hc⟩ (λ eq, ne $ subtype.ext $ this eq) }
+end
 
 end finset
 
 variables {R : Type*} {I : Type*} [comm_semiring R] {p : I → R} [decidable_eq I] {s : finset I}
+open finset
 
 lemma exists_sum_eq_one_iff_pairwise_coprime (h : s.nonempty) :
   (∃ μ : I → R, ∑ i in s, μ i * ∏ j in s \ {i}, p j = 1) ↔ pairwise (is_coprime on λ i : s, p i) :=
 begin
-  casesI subsingleton_or_nontrivial R,
-  { simp only [eq_iff_true_of_subsingleton, exists_const, is_coprime_subsingleton, true_iff],
-    tauto },
   refine h.cons_induction _ _; clear' s h,
-  { simp only [pairwise, finset.sum_singleton, finset.sdiff_self, finset.prod_empty, mul_one,
-               exists_apply_eq_apply, ne.def, true_iff],
+  { simp only [pairwise, sum_singleton, finset.sdiff_self, prod_empty, mul_one,
+      exists_apply_eq_apply, ne.def, true_iff],
     rintro a ⟨i, hi⟩ ⟨j, hj⟩ h,
     rw finset.mem_singleton at hi hj,
     simpa [hi, hj] using h },
@@ -36,10 +41,39 @@ begin
   have : ∀ s : finset I,
         (is_coprime on λ i : s, p i) = ((λ x y, is_coprime (p x) (p y)) on λ i : s, i),
   { simp [function.on_fun] },
-  rw [this, pairwise_insert, ←this],
-  split; intro h,
-  { sorry },
-  { sorry }
+  rw [this, pairwise_cons, ←this],
+  have mem : ∀ x ∈ s, a ∈ insert a s \ {x} := λ x hx, by
+    { rw [mem_sdiff, mem_singleton], exact ⟨mem_insert_self _ _, λ ha, has (ha.symm.cases_on hx)⟩ },
+  split,
+  { rintro ⟨μ, hμ⟩,
+    rw [sum_cons, cons_eq_insert, sdiff_singleton_eq_erase, erase_insert has] at hμ,
+    refine ⟨ih.mp ⟨pi.single h.some (μ a * p h.some) + μ * λ _, p a, _⟩, λ b hb, _⟩,
+    { rw [prod_eq_mul_prod_diff_singleton h.some_spec, ← mul_assoc,
+        ← @if_pos _ _ h.some_spec R (_ * _) 0, ← sum_pi_single', ← sum_add_distrib] at hμ,
+      rw [← hμ, sum_congr rfl], intros x hx, convert add_mul _ _ _ using 2,
+      { by_cases hx : x = h.some,
+        { rw [hx, pi.single_eq_same, pi.single_eq_same] },
+        { rw [pi.single_eq_of_ne hx, pi.single_eq_of_ne hx, zero_mul] } },
+      { convert (mul_assoc _ _ _).symm,
+        convert prod_eq_mul_prod_diff_singleton (mem x hx) _ using 3,
+        convert sdiff_sdiff_comm, rw [sdiff_singleton_eq_erase, erase_insert has] } },
+    { have : is_coprime (p b) (p a) :=
+        ⟨μ a * ∏ i in s \ {b}, p i, ∑ i in s, μ i * ∏ j in s \ {i}, p j, _⟩,
+      { exact ⟨this.symm, this⟩ },
+      rw [mul_assoc, ← prod_eq_prod_diff_singleton_mul hb, sum_mul, ← hμ, sum_congr rfl],
+      intros x hx, convert mul_assoc _ _ _,
+      convert prod_eq_prod_diff_singleton_mul (mem x hx) _ using 3,
+      convert sdiff_sdiff_comm, rw [sdiff_singleton_eq_erase, erase_insert has] } },
+  { rintro ⟨hp, Hb⟩, obtain ⟨μ, hμ⟩ := ih.mpr hp,
+    obtain ⟨u, v, huv⟩ := is_coprime.prod_left (λ b hb, (Hb b hb).right),
+    use λ i, if i = a then u else v * μ i,
+    have hμ' : ∑ i in s, v * ((μ i * ∏ j in s \ {i}, p j) * p a) = v * p a := by
+      rw [← mul_sum, ← sum_mul, hμ, one_mul],
+    rw [sum_cons, cons_eq_insert, sdiff_singleton_eq_erase, erase_insert has, if_pos rfl,
+      ← huv, ← hμ', sum_congr rfl], intros x hx,
+    rw [mul_assoc, if_neg (λ ha : x = a, has (ha.cases_on hx))],
+    convert mul_assoc _ _ _, convert (prod_eq_prod_diff_singleton_mul (mem x hx) _).symm using 3,
+    convert sdiff_sdiff_comm, rw [sdiff_singleton_eq_erase, erase_insert has] }
 end
 
 lemma exists_sum_eq_one_iff_pairwise_coprime' [fintype I] [nonempty I] :

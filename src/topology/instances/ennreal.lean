1e61af50faa4443d42b7dff753d883f473312583
Yury G. Kudryashov <urkud@urkud.name>
2022-03-24 23:40:12 -0400

    Golf

--- src/topology/instances/ennreal.lean
+++ src/topology/instances/ennreal.lean
@@ -873,9 +873,8 @@ lemma has_sum_to_real {f : α → ℝ≥0∞} (hsum : ∑' x, f x ≠ ∞) :
   has_sum (λ x, (f x).to_real) (∑' x, (f x).to_real) :=
 begin
   lift f to α → ℝ≥0 using ennreal.ne_top_of_tsum_ne_top hsum,
-  have : summable f, by rwa ennreal.tsum_coe_ne_top_iff_summable at hsum,
-  simpa only [ennreal.coe_to_real, ← nnreal.coe_tsum, nnreal.has_sum_coe]
-    using this.has_sum
+  simp only [coe_to_real, ← nnreal.coe_tsum, nnreal.has_sum_coe],
+  exact (tsum_coe_ne_top_iff_summable.1 hsum).has_sum
 end
 
 lemma summable_to_real {f : α → ℝ≥0∞} (hsum : ∑' x, f x ≠ ∞) :

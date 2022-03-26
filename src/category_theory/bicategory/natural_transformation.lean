265b11dd6a00cc31ce59f67643d968ae099ab639
yuma-mizuno <mizuno.y.aj@gmail.com>
2022-03-26 17:25:32 +0900

    modify import

--- src/category_theory/bicategory/natural_transformation.lean
+++ src/category_theory/bicategory/natural_transformation.lean
@@ -3,7 +3,7 @@ Copyright (c) 2022 Yuma Mizuno. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Yuma Mizuno
 -/
-import tactic.coherence
+import category_theory.bicategory.functor
 
 /-!
 # Oplax natural transformations

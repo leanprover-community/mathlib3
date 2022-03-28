a07e00f01124ac617ebad69fcf7fa3a7f1627aad
Anatole Dedecker <anatolededecker@gmail.com>
2022-03-28 11:27:50 +0200

    Clean a lot

--- src/topology/uniform_space/uniform_convergence_topology.lean
+++ src/topology/uniform_space/uniform_convergence_topology.lean
@@ -50,45 +50,6 @@ protected lemma has_basis_uniformity :
 protected def topological_space : topological_space (α → β) :=
 (uniform_convergence.uniform_core α β).to_topological_space
 
-lemma foo1 (h : s ⊆ s') : @uniform_continuous (s' → β) (s → β)
-  (uniform_convergence.uniform_space s' β) (uniform_convergence.uniform_space s β)
-  (λ f x, f ⟨x.1, h x.2⟩) :=
-((uniform_convergence.has_basis_uniformity s' β).tendsto_iff
-  (uniform_convergence.has_basis_uniformity s β)).mpr (λ U hU,
-    ⟨U, hU, λ uv huv ⟨x, hx⟩, huv ⟨x, h hx⟩⟩)
-
-lemma bar1 (h : s ⊆ s') {f : α → β} : (s.restrict : (α → β) → (s → β)) =
-  (λ (f : s' → β) (x : s), f (⟨x.1, h x.2⟩ : s')) ∘ s'.restrict :=
-rfl
-
-lemma foo2 (s s' : set α) : @uniform_continuous ((s → β) × (s' → β)) (s ∪ s' → β)
-  (@prod.uniform_space _ _
-    (uniform_convergence.uniform_space s β) (uniform_convergence.uniform_space s' β))
-  (uniform_convergence.uniform_space (s ∪ s' : set α) β)
-    (λ uv x, x.2.by_cases (λ hx, uv.1 ⟨x.1, hx⟩) (λ hx, uv.2 ⟨x.1, hx⟩)) :=
-begin
-  rw [uniform_continuous, uniformity_prod],
-  refine ((((uniform_convergence.has_basis_uniformity s β).comap _).inf
-    ((uniform_convergence.has_basis_uniformity s' β).comap _)).tendsto_iff
-    ((uniform_convergence.has_basis_uniformity (s ∪ s' : set α) β))).mpr
-    (λ U hU, ⟨⟨U, U⟩, ⟨hU, hU⟩, λ u₁v₁u₂v₂ h x, _⟩),
-  simp_rw or.by_cases,
-  --split_ifs with h₁ h₂,
-  sorry
-end
-
-lemma bar2 (s s' : set α) : ((s ∪ s' : set α).restrict : (α → β) → ((s ∪ s') → β)) =
-  (λ (uv : (s → β) × (s' → β)) x, x.2.by_cases (λ hx, uv.1 ⟨x.1, hx⟩) (λ hx, uv.2 ⟨x.1, hx⟩)) ∘
-    (λ f, ⟨λ x, f x.1, λ x, f x.1⟩) :=
-begin
-  ext f x,
-  dsimp [or.by_cases],
-  split_ifs with h₁ h₂,
-  { refl },
-  { refl },
-  { exact (x.2.elim h₁ h₂).elim }
-end
-
 variable {α}
 
 end uniform_convergence
@@ -118,51 +79,46 @@ protected lemma uniform_space_antitone : antitone (uniform_convergence_on.unifor
 
 variables {α}
 
-lemma uniform_space_eq_uniform_space_generated :
-  uniform_convergence_on.uniform_space α β 𝔖 =
-  uniform_convergence_on.uniform_space α β {s | @is_bounded _ (bornology.generate 𝔖) s} :=
-begin
-  rw [uniform_convergence_on.uniform_space, uniform_convergence_on.uniform_space],
-  refine le_antisymm (le_infi $ λ s, le_infi $ λ hs, _)
-    (infi_le_infi_of_subset $ λ s, is_bounded_generate),
-  sorry
-  --refine infi_le_infi (λ s, _),
-end
-
-def foo (u : uniform_space (α → β)) : bornology α :=
+def convergence_sets (u : uniform_space (α → β)) : bornology α :=
 bornology.of_bounded'
   {S | @uniform_continuous _ _ u (uniform_convergence.uniform_space _ _) (restrict S)}
   (@uniform_continuous_of_const _ _ u (uniform_convergence.uniform_space (∅ : set α) _)
     ((∅ : set α).restrict) (λ a b, funext $ λ x, x.2.elim))
-  (λ s₁ h₁ s₂ h₂₁, @uniform_continuous.comp _ _ _ u (uniform_convergence.uniform_space s₁ _)
-    (uniform_convergence.uniform_space s₂ _) _ _ (uniform_convergence.foo1 α β h₂₁) h₁)
-  (λ s₁ h₁ s₂ h₂,
-    begin
-      letI : uniform_space (s₁ → β) := uniform_convergence.uniform_space s₁ _,
-      letI : uniform_space (s₂ → β) := uniform_convergence.uniform_space s₂ _,
-      letI : uniform_space ((s₁ ∪ s₂) → β) := uniform_convergence.uniform_space (s₁ ∪ s₂ : set α) _,
-      change uniform_continuous ((s₁ ∪ s₂).restrict : (α → β) → ((s₁ ∪ s₂) → β)),
-      rw uniform_convergence.bar2 α β s₁ s₂,
-      exact (uniform_convergence.foo2 α β s₁ s₂).comp (uniform_continuous.prod_mk h₁ h₂)
-    end)
+  begin
+    intros s₁ h₁ s₂ h₂₁,
+    change tendsto _ _ _,
+    change tendsto _ _ _ at h₁,
+    rw (uniform_convergence.has_basis_uniformity _ β).tendsto_right_iff at ⊢ h₁,
+    intros U hU,
+    filter_upwards [h₁ U hU] using λ uv huv ⟨x, hx⟩, huv ⟨x, h₂₁ hx⟩
+  end
+  begin
+    intros s₁ h₁ s₂ h₂,
+    change tendsto _ _ _,
+    change tendsto _ _ _ at h₁,
+    change tendsto _ _ _ at h₂,
+    rw (uniform_convergence.has_basis_uniformity _ β).tendsto_right_iff at ⊢ h₁ h₂,
+    intros U hU,
+    filter_upwards [h₁ U hU, h₂ U hU],
+    rintros uv huv₁ huv₂ ⟨x, (hx|hx)⟩,
+    { exact huv₁ ⟨x, hx⟩ },
+    { exact huv₂ ⟨x, hx⟩ }
+  end
   (λ x, sorry)
 
-lemma uniform_convergence_on_foo : uniform_convergence_on.uniform_space α β 𝔖 =
+lemma uniform_convergence_on_convergence_sets : uniform_convergence_on.uniform_space α β 𝔖 =
   uniform_convergence_on.uniform_space α β
-    {s | @is_bounded _ (foo β $ uniform_convergence_on.uniform_space α β 𝔖) s} :=
+    {s | @is_bounded _ (convergence_sets β $ uniform_convergence_on.uniform_space α β 𝔖) s} :=
 begin
-  ext : 1,
-  refine le_antisymm _ _,
-  sorry,
-  sorry
+  refine le_antisymm _ _, sorry, sorry
 end
 
-lemma uniform_space_eq_uniform_space_generated' :
+lemma uniform_space_eq_uniform_space_generated :
   uniform_convergence_on.uniform_space α β 𝔖 =
   uniform_convergence_on.uniform_space α β {s | @is_bounded _ (bornology.generate 𝔖) s} :=
 begin
   refine le_antisymm _ (infi_le_infi_of_subset $ λ s, is_bounded_generate),
-  rw uniform_convergence_on_foo β 𝔖,
+  rw uniform_convergence_on_convergence_sets β 𝔖,
   refine uniform_convergence_on.uniform_space_antitone _ _ _,
   change ∀ s, @is_bounded _ _ s → @is_bounded _ _ s,
   rw [← bornology.le_iff],

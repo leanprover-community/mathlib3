74cd9b6d22081e2d217f0ee1069bc854a0747b8e
Kevin Wilson <khwilson@gmail.com>
2022-03-28 13:46:38 -0500

    Further PR requests

--- src/algebra/square.lean
+++ src/algebra/square.lean
@@ -3,8 +3,10 @@ Copyright (c) 2022 Kevin H. Wilson. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Kevin H. Wilson
 -/
+import data.int.sqrt
 import ring_theory.int.basic
 import algebra.parity
+import data.rat.sqrt
 
 /-!
 # Square elements of monoids
@@ -21,7 +23,7 @@ An element `x` of a monoid is a square when there is another element `y` such th
 ## TODO
 
 There are many more facts about squares that generalize to arbitrary `unique_factorization_monoid`s.
-The should eventually be added.
+However, exactly how units should be handled requires some care.
 
 ## Tags
 square, squarefree, multiplicity
@@ -142,12 +144,45 @@ begin
     rw [hk, pow_mul, pow_two, mul_pow], },
 end
 
+end nat
+end factorization
+
+section sqrt0
+
+/-! #### Square root of squares
+
+Throughout mathlib, there are several types `α` for which a notion of `sqrt` has been defined.
+For instance, `nat.sqrt`, `int.sqrt`, `nnreal.sqrt`, and `real.sqrt`. In most cases, `sqrt x` is
+defined as the greatest `y ∈ α` such that `y * y ≤ x` or `0` if no such `y` exists.
+
+But this definition has some limits. For example, it's not true for `rat.sqrt` where
+`rat.sqrt ((1 : ℚ) / (3 : ℚ)) = 1`.
+
+The following definition offers a different take on how to handle the "undefined" aspect of
+`sqrt`. Specifically, in `ℕ`, if `¬square x`, then we'll define `sqrt0 x = 0`. Otherwise, we'll
+define `sqrt0 x` as the (unique) `y` such that `y * y = x`.
+
+`TODO`: For now, we only define `sqrt0` for the natural numbers. This could be generalized to a
+`unique_factorization_monoid`, but there is a choice to make about units. Consider `ℤ` where we
+would like `sqrt0 1 = 1` but `(-1 : ℤ) * (-1) = 1` as well. In number theory, there are ways to
+consistently make this choice in totally real fields, but it gets tricky in any other instance.
+As such, before having a specific need to address, we leave this for future considerations.
+-/
+
+namespace nat
+
 /-- If `x` is a square, return its square root. Else, return 0. -/
 def sqrt0 (x : ℕ) := ite (square x) (sqrt x) 0
 
 lemma sqrt0_zero : sqrt0 0 = 0 := by simp only [sqrt0, square_zero, if_true, sqrt_zero]
 
 lemma sqrt0_one : sqrt0 1 = 1 := by simp only [sqrt0, square_one, if_true, sqrt_one]
+
+lemma sqrt0_square {x : ℕ} (hsquare : square x) : sqrt0 x = sqrt x :=
+by simp only [sqrt0, hsquare, if_true]
+
+lemma sqrt0_not_square {x : ℕ} (hsquare : ¬square x) : sqrt0 x = 0 :=
+by simp only [sqrt0, hsquare, if_false]
 
 lemma sqrt0_prime {p : ℕ} (hp : nat.prime p) : sqrt0 p = 0 :=
 begin
@@ -183,4 +218,4 @@ end
 
 end nat
 
-end factorization
+end sqrt0

803aea1c00085b1ff21edb196f2bb0b06589e90f
YaelDillies <yael.dillies@gmail.com>
2022-03-30 00:02:47 +0200

    rename

--- src/topology/sets/order.lean
+++ src/topology/sets/order.lean
@@ -16,49 +16,46 @@ open set
 
 variables {α β : Type*} [topological_space α] [has_le α] [topological_space β] [has_le β]
 
-namespace topological_space
-
 /-! ### Compact open sets -/
 
 /-- The type of clopen upper sets of a topological space. -/
-structure clopen_upset (α : Type*) [topological_space α] [has_le α] extends clopens α :=
+structure clopen_upper_set (α : Type*) [topological_space α] [has_le α] extends clopens α :=
 (upper' : is_upper_set carrier)
 
-namespace clopen_upset
+namespace clopen_upper_set
 
-instance : set_like (clopen_upset α) α :=
+instance : set_like (clopen_upper_set α) α :=
 { coe := λ s, s.carrier,
   coe_injective' := λ s t h, by { obtain ⟨⟨_, _⟩, _⟩ := s, obtain ⟨⟨_, _⟩, _⟩ := t, congr' } }
 
-lemma upper (s : clopen_upset α) : is_upper_set (s : set α) := s.upper'
-lemma clopen (s : clopen_upset α) : is_clopen (s : set α) := s.clopen'
+lemma upper (s : clopen_upper_set α) : is_upper_set (s : set α) := s.upper'
+lemma clopen (s : clopen_upper_set α) : is_clopen (s : set α) := s.clopen'
 
 /-- Reinterpret a upper clopen as an upper set. -/
-@[simps] def to_upper_set (s : clopen_upset α) : upper_set α := ⟨s, s.upper⟩
+@[simps] def to_upper_set (s : clopen_upper_set α) : upper_set α := ⟨s, s.upper⟩
 
-@[ext] protected lemma ext {s t : clopen_upset α} (h : (s : set α) = t) : s = t := set_like.ext' h
+@[ext] protected lemma ext {s t : clopen_upper_set α} (h : (s : set α) = t) : s = t := set_like.ext' h
 
 @[simp] lemma coe_mk (s : clopens α) (h) : (mk s h : set α) = s := rfl
 
-instance : has_sup (clopen_upset α) :=
+instance : has_sup (clopen_upper_set α) :=
 ⟨λ s t, ⟨s.to_clopens ⊔ t.to_clopens, s.upper.union t.upper⟩⟩
-instance : has_inf (clopen_upset α) :=
+instance : has_inf (clopen_upper_set α) :=
 ⟨λ s t, ⟨s.to_clopens ⊓ t.to_clopens, s.upper.inter t.upper⟩⟩
-instance : has_top (clopen_upset α) := ⟨⟨⊤, is_upper_set_univ⟩⟩
-instance : has_bot (clopen_upset α) := ⟨⟨⊥, is_upper_set_empty⟩⟩
+instance : has_top (clopen_upper_set α) := ⟨⟨⊤, is_upper_set_univ⟩⟩
+instance : has_bot (clopen_upper_set α) := ⟨⟨⊥, is_upper_set_empty⟩⟩
 
-instance : lattice (clopen_upset α) :=
+instance : lattice (clopen_upper_set α) :=
 set_like.coe_injective.lattice _ (λ _ _, rfl) (λ _ _, rfl)
 
-instance : bounded_order (clopen_upset α) :=
+instance : bounded_order (clopen_upper_set α) :=
 bounded_order.lift (coe : _ → set α) (λ _ _, id) rfl rfl
 
-@[simp] lemma coe_sup (s t : clopen_upset α) : (↑(s ⊔ t) : set α) = s ∪ t := rfl
-@[simp] lemma coe_inf (s t : clopen_upset α) : (↑(s ⊓ t) : set α) = s ∩ t := rfl
-@[simp] lemma coe_top : (↑(⊤ : clopen_upset α) : set α) = univ := rfl
-@[simp] lemma coe_bot : (↑(⊥ : clopen_upset α) : set α) = ∅ := rfl
+@[simp] lemma coe_sup (s t : clopen_upper_set α) : (↑(s ⊔ t) : set α) = s ∪ t := rfl
+@[simp] lemma coe_inf (s t : clopen_upper_set α) : (↑(s ⊓ t) : set α) = s ∩ t := rfl
+@[simp] lemma coe_top : (↑(⊤ : clopen_upper_set α) : set α) = univ := rfl
+@[simp] lemma coe_bot : (↑(⊥ : clopen_upper_set α) : set α) = ∅ := rfl
 
-instance : inhabited (clopen_upset α) := ⟨⊥⟩
+instance : inhabited (clopen_upper_set α) := ⟨⊥⟩
 
-end clopen_upset
-end topological_space
+end clopen_upper_set

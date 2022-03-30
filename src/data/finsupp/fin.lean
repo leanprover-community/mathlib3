af1ff7e9e2cff55a0af091f957580abbac8da27c
Iván Sadofschi Costa <isadofschi@dm.uba.ar>
2022-03-30 00:05:51 +0000

    generalize to [has_zero M]

--- src/data/finsupp/fin.lean
+++ src/data/finsupp/fin.lean
@@ -12,33 +12,33 @@ noncomputable theory
 
 namespace finsupp
 
-variable {n : ℕ}
+variables {M : Type*} [has_zero M] {n : ℕ}
 
 /-- `tail` for maps `fin (n+1) →₀ M`. See `fin.tail` for more details. -/
-def tail (s : fin (n+1) →₀ ℕ) : fin n →₀ ℕ :=
+def tail (s : fin (n+1) →₀ M) : fin n →₀ M :=
 finsupp.equiv_fun_on_fintype.inv_fun (fin.tail s.to_fun)
 
 /-- `cons` for maps `fin n →₀ M`. See `fin.cons` for more details. -/
-def cons (y : ℕ) (s : fin n →₀ ℕ) : fin (n+1) →₀ ℕ :=
+def cons (y : M) (s : fin n →₀ M) : fin (n+1) →₀ M :=
 finsupp.equiv_fun_on_fintype.inv_fun (fin.cons y s.to_fun)
 
-lemma tail_apply (s : fin (n+1) →₀ ℕ) (i : fin n) : tail s i = s i.succ :=
+lemma tail_apply (s : fin (n+1) →₀ M) (i : fin n) : tail s i = s i.succ :=
 begin
   simp only [tail, equiv_fun_on_fintype_symm_apply_to_fun, equiv.inv_fun_as_coe],
   congr,
 end
 
-@[simp] lemma cons_zero (y : ℕ) (s : fin n →₀ ℕ) : cons y s 0 = y :=
+@[simp] lemma cons_zero (y : M) (s : fin n →₀ M) : cons y s 0 = y :=
 by simp [cons, finsupp.equiv_fun_on_fintype]
 
-@[simp] lemma cons_succ (i : fin n) (y : ℕ) (s : fin n →₀ ℕ) : cons y s i.succ = s i :=
+@[simp] lemma cons_succ (i : fin n) (y : M) (s : fin n →₀ M) : cons y s i.succ = s i :=
 begin
 
   simp only [finsupp.cons, fin.cons, finsupp.equiv_fun_on_fintype, fin.cases_succ, finsupp.coe_mk],
   refl,
 end
 
-@[simp] lemma tail_cons (y : ℕ) (s : fin n →₀ ℕ) : tail (cons y s) = s :=
+@[simp] lemma tail_cons (y : M) (s : fin n →₀ M) : tail (cons y s) = s :=
 begin
   simp only [finsupp.cons, fin.cons, finsupp.tail, fin.tail],
   ext,
@@ -47,7 +47,7 @@ begin
   refl,
 end
 
-@[simp] lemma cons_tail (s : fin (n + 1) →₀ ℕ) : cons (s 0) (tail s) = s :=
+@[simp] lemma cons_tail (s : fin (n + 1) →₀ M) : cons (s 0) (tail s) = s :=
 begin
   ext,
   by_cases c_a : a = 0,
@@ -55,18 +55,17 @@ begin
   { rw [←fin.succ_pred a c_a, cons_succ, ←tail_apply] },
 end
 
-@[simp] lemma cons_zero_zero : cons 0 (0 : fin n →₀ ℕ) = 0 :=
+@[simp] lemma cons_zero_zero : cons 0 (0 : fin n →₀ M) = 0 :=
 begin
   ext,
   by_cases c : a ≠ 0,
   { rw [←fin.succ_pred a c, cons_succ],
     simp },
   { simp only [not_not] at c,
-    rw [c, cons_zero 0 0],
-    simp },
+    simp [c] },
 end
 
-lemma cons_ne_zero_of_left {y : ℕ} {m : fin n →₀ ℕ} (h : y ≠ 0) : cons y m ≠ 0 :=
+lemma cons_ne_zero_of_left {y : M} {m : fin n →₀ M} (h : y ≠ 0) : cons y m ≠ 0 :=
 begin
   by_contradiction c,
   have h1 : cons y m 0 = 0 := by simp [c],
@@ -74,7 +73,7 @@ begin
   cc,
 end
 
-lemma cons_ne_zero_of_right {y : ℕ} {m: fin n →₀ ℕ} (h : m ≠ 0) : cons y m ≠ 0 :=
+lemma cons_ne_zero_of_right {y : M} {m: fin n →₀ M} (h : m ≠ 0) : cons y m ≠ 0 :=
 begin
   by_contradiction c,
   have h' : m = 0,
@@ -83,13 +82,16 @@ begin
   cc,
 end
 
-lemma cons_ne_zero_iff {y : ℕ} {m: fin n →₀ ℕ} : cons y m ≠ 0 ↔ y ≠ 0 ∨ m ≠ 0 :=
+lemma cons_ne_zero_iff {y : M} {m: fin n →₀ M} : cons y m ≠ 0 ↔ y ≠ 0 ∨ m ≠ 0 :=
 begin
   apply iff.intro,
   { intro h,
+    apply or_iff_not_imp_left.2,
+    intro h',
+    simp only [not_not] at h',
     by_contra c,
-    simp only [not_or_distrib, ne.def, not_not] at c,
-    simpa [c.1, c.2, cons_zero_zero] using h },
+    rw [h', c] at h,
+    simpa using h },
   { intro h,
     cases h,
     { exact cons_ne_zero_of_left h },

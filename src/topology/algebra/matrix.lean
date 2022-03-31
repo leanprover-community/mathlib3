ea10d4270bad8eac639f4dfa73608013c75a172a
Eric Wieser <wieser.eric@gmail.com>
2022-03-31 10:29:17 +0000

    dot notation

--- src/topology/algebra/matrix.lean
+++ src/topology/algebra/matrix.lean
@@ -14,13 +14,14 @@ This file is a place to collect topological results about matrices.
 ## Main definitions:
 
  * `matrix.topological_ring`: square matrices form a topological ring
- * `matrix.continuous_det`: the determinant is continuous over a topological ring.
- * `matrix.continuous_adjugate`: the adjugate is continuous over a topological ring.
+ * `continuous.matrix_det`: the determinant is continuous over a topological ring.
+ * `continuous.matrix_adjugate`: the adjugate is continuous over a topological ring.
 -/
 
 open matrix
+open_locale matrix
 
-variables {α l m n p S R : Type*} [topological_space R]
+variables {X α l m n p S R : Type*} [topological_space X] [topological_space R]
 
 instance : topological_space (matrix m n R) := Pi.topological_space
 
@@ -39,140 +40,153 @@ lemma continuous_matrix [topological_space α] {f : α → matrix m n R}
   (h : ∀ i j, continuous (λ a, f a i j)) : continuous f :=
 continuous_pi $ λ _, continuous_pi $ λ j, h _ _
 
-lemma continuous_matrix_elem (i : m) (j : n) : continuous (λ A : matrix m n R, A i j) :=
-continuous_apply_apply i j
+lemma continuous.matrix_elem {A : X → matrix m n R} (hA : continuous A) (i : m) (j : n) :
+  continuous (λ x, A x i j) :=
+(continuous_apply_apply i j).comp hA
 
-namespace matrix
+@[continuity]
+lemma continuous.matrix_map [topological_space S] {A : X → matrix n m S} {f : S → R}
+   (hA : continuous A) (hf : continuous f) :
+  continuous (λ x, (A x).map f) :=
+continuous_matrix $ λ i j, hf.comp $ hA.matrix_elem _ _
 
-lemma continuous_map [topological_space S] (f : S → R) (hf : continuous f) :
-  continuous (λ A : matrix m n S, A.map f) :=
-continuous_matrix $ λ i j, hf.comp (continuous_matrix_elem _ _)
-
-lemma continuous_transpose :
-  continuous (transpose : matrix m n R → matrix n m R) :=
-continuous_matrix $ λ i j, continuous_matrix_elem _ _
+@[continuity]
+lemma continuous.matrix_transpose {A : X → matrix n m R} (hA : continuous A) :
+  continuous (λ x, (A x)ᵀ) :=
+continuous_matrix $ λ i j, hA.matrix_elem j i
 
 /-! TODO: add a `has_continuous_star` typeclass so we can write
 ```
-lemma continuous_conj_transpose [has_star R] :
-  continuous (conj_transpose : matrix m n R → matrix n m R) :=
-continuous_matrix $ λ i j, continuous.comp sorry (continuous_matrix_elem _ _)
+lemma continuous.matrix.conj_transpose [has_star R] {A : X → matrix n m R} (hA : continuous A) :
+  continuous (λ x, (A x)ᴴ) :=
+hA.matrix_transpose.matrix_map _
 ```
 -/
 
-lemma continuous_col : continuous (col : (n → R) → matrix n unit R) :=
-continuous_matrix $ λ i j, continuous_apply _
+@[continuity]
+lemma continuous.matrix_col {A : X → n → R} (hA : continuous A) : continuous (λ x, col (A x)) :=
+continuous_matrix $ λ i j, (continuous_apply _).comp hA
 
-lemma continuous_row : continuous (row : (n → R) → matrix unit n R) :=
-continuous_matrix $ λ i j, continuous_apply _
+@[continuity]
+lemma continuous.matrix_row {A : X → n → R} (hA : continuous A) : continuous (λ x, row (A x)) :=
+continuous_matrix $ λ i j, (continuous_apply _).comp hA
 
-lemma continuous_diagonal [has_zero R] [decidable_eq n] :
-  continuous (diagonal : (n → R) → matrix n n R) :=
+@[continuity]
+lemma continuous_matrix.diagonal [has_zero R] [decidable_eq n] {A : X → n → R} (hA : continuous A) :
+  continuous (λ x, diagonal (A x)) :=
 continuous_matrix $ λ i j, begin
   obtain rfl | hij := decidable.eq_or_ne i j,
   { simp_rw diagonal_apply_eq,
-    exact continuous_apply _ },
+    exact (continuous_apply _).comp hA },
   { simp_rw diagonal_apply_ne hij,
     exact continuous_zero },
 end
 
-lemma continuous_dot_product [fintype n] [has_mul R] [add_comm_monoid R] [has_continuous_add R]
-  [has_continuous_mul R] :
-  continuous (λ A : (n → R) × (n → R), dot_product A.1 A.2) :=
-continuous_finset_sum _ $ λ i _,
-  ((continuous_apply i).comp continuous_fst).mul ((continuous_apply i).comp continuous_snd)
+@[continuity]
+lemma continuous.matrix_dot_product [fintype n] [has_mul R] [add_comm_monoid R]
+  [has_continuous_add R] [has_continuous_mul R]
+  {A : X → n → R} {B : X → n → R} (hA : continuous A) (hB : continuous B) :
+  continuous (λ x, dot_product (A x) (B x)) :=
+continuous_finset_sum _ $ λ i _, ((continuous_apply i).comp hA).mul ((continuous_apply i).comp hB)
 
-/-- Note this refers to `matrix.mul`, for square matrices the usual `continuous_mul` can be used. -/
-lemma continuous_mul [fintype n] [has_mul R] [add_comm_monoid R] [has_continuous_add R]
-  [has_continuous_mul R] :
-  continuous (λ A : matrix m n R × matrix n p R, A.1.mul A.2) :=
+/-- For square matrices the usual `continuous_mul` can be used. -/
+@[continuity]
+lemma continuous.matrix_mul [fintype n] [has_mul R] [add_comm_monoid R] [has_continuous_add R]
+  [has_continuous_mul R]
+  {A : X → matrix m n R} {B : X → matrix n p R} (hA : continuous A) (hB : continuous B) :
+  continuous (λ x, (A x).mul (B x)) :=
 continuous_matrix $ λ i j, continuous_finset_sum _ $ λ k _,
-  ((continuous_matrix_elem _ _).comp continuous_fst).mul
-    ((continuous_matrix_elem _ _).comp continuous_snd)
+  (hA.matrix_elem _ _).mul (hB.matrix_elem _ _)
 
 instance [fintype n] [has_mul R] [add_comm_monoid R] [has_continuous_add R]
   [has_continuous_mul R] : has_continuous_mul (matrix n n R) :=
-⟨continuous_mul⟩
+⟨continuous_fst.matrix_mul continuous_snd⟩
 
 instance [fintype n] [decidable_eq n] [semiring R] [topological_ring R] :
   topological_ring (matrix n n R) :=
 { ..pi.has_continuous_add }
 
-lemma continuous_vec_mul_vec [has_mul R] [has_continuous_mul R] :
-  continuous (λ x : (m → R) × (n → R), vec_mul_vec x.1 x.2) :=
-continuous_matrix $ λ i j,
-  ((continuous_apply _).comp continuous_fst).mul ((continuous_apply _).comp continuous_snd)
+@[continuity]
+lemma continuous.matrix_vec_mul_vec [has_mul R] [has_continuous_mul R]
+  {A : X → m → R} {B : X → n → R} (hA : continuous A) (hB : continuous B) :
+  continuous (λ x, vec_mul_vec (A x) (B x)) :=
+continuous_matrix $ λ i j, ((continuous_apply _).comp hA).mul ((continuous_apply _).comp hB)
 
-lemma continuous_mul_vec [non_unital_non_assoc_semiring R] [has_continuous_add R]
-  [has_continuous_mul R] [fintype n] :
-  continuous (λ x : matrix m n R × (n → R), x.1.mul_vec x.2) :=
-continuous_pi $ λ i, show continuous ((λ x : (n → R) × (n → R), dot_product x.1 x.2) ∘
-                                      (λ x : matrix m n R × (n → R), (x.1 i, x.2))),
-  from continuous_dot_product.comp ((continuous_apply i).prod_map continuous_id)
+@[continuity]
+lemma continuous.matrix_mul_vec [non_unital_non_assoc_semiring R] [has_continuous_add R]
+  [has_continuous_mul R] [fintype n]
+  {A : X → matrix m n R} {B : X → n → R} (hA : continuous A) (hB : continuous B) :
+  continuous (λ x, (A x).mul_vec (B x)) :=
+continuous_pi $ λ i, ((continuous_apply i).comp hA).matrix_dot_product  hB
 
-lemma continuous_vec_mul [non_unital_non_assoc_semiring R] [has_continuous_add R]
-  [has_continuous_mul R] [fintype m] :
-  continuous (λ x : (m → R) × matrix m n R , matrix.vec_mul x.1 x.2) :=
-continuous_pi $ λ i, show continuous ((λ x : (m → R) × (m → R), dot_product x.1 x.2) ∘
-                                      (λ x : (m → R) × matrix m n R, (x.1, λ j, x.2 j i))),
-  from continuous_dot_product.comp $
-    continuous_fst.prod_mk (continuous_pi (λ j, (continuous_matrix_elem _ _).comp continuous_snd))
+@[continuity]
+lemma continuous.matrix_vec_mul [non_unital_non_assoc_semiring R] [has_continuous_add R]
+  [has_continuous_mul R] [fintype m]
+  {A : X → m → R} {B : X → matrix m n R} (hA : continuous A) (hB : continuous B) :
+  continuous (λ x, vec_mul (A x) (B x)) :=
+continuous_pi $ λ i, hA.matrix_dot_product $ continuous_pi $ λ j, (hB.matrix_elem _ _)
 
-lemma continuous_minor (e₁ : m → l) (e₂ : p → n) :
-  continuous (λ A : matrix l n R, A.minor e₁ e₂) :=
-continuous_matrix $ λ i j, continuous_matrix_elem _ _
+@[continuity]
+lemma continuous.matrix_minor {A : X → matrix l n R} (hA : continuous A) (e₁ : m → l) (e₂ : p → n) :
+  continuous (λ x, (A x).minor e₁ e₂) :=
+continuous_matrix $ λ i j, hA.matrix_elem _ _
 
-lemma continuous_reindex (e₁ : l ≃ m) (e₂ : n ≃ p) :
-  continuous (reindex e₁ e₂ : matrix l n R → matrix m p R) :=
-continuous_minor _ _
+@[continuity]
+lemma continuous.matrix_reindex {A : X → matrix l n R}
+  (hA : continuous A) (e₁ : l ≃ m) (e₂ : n ≃ p) :
+  continuous (λ x, reindex e₁ e₂ (A x)) :=
+hA.matrix_minor _ _
 
-lemma continuous_diag [semiring S] [add_comm_monoid R] [module S R] :
-  continuous (matrix.diag n S R) :=
-continuous_pi (λ _, continuous_matrix_elem _ _)
+@[continuity]
+lemma continuous.matrix_diag [semiring S] [add_comm_monoid R] [module S R]
+  {A : X → matrix n n R} (hA : continuous A) :
+  continuous (λ x, matrix.diag n S R (A x)) :=
+continuous_pi $ λ _, hA.matrix_elem _ _
 
-lemma continuous_trace [fintype n] [semiring S] [add_comm_monoid R] [has_continuous_add R]
-  [module S R] :
-  continuous (trace n S R) :=
-continuous_finset_sum _ $ λ i hi, continuous_matrix_elem _ _
+@[continuity]
+lemma continuous.matrix_trace [fintype n] [semiring S] [add_comm_monoid R] [has_continuous_add R]
+  [module S R] {A : X → matrix n n R} (hA : continuous A) :
+  continuous (λ x, trace n S R (A x)) :=
+continuous_finset_sum _ $ λ i hi, hA.matrix_elem _ _
 
-lemma continuous_det [fintype n] [decidable_eq n] [comm_ring R] [topological_ring R] :
-  continuous (det : matrix n n R → R) :=
+@[continuity]
+lemma continuous.matrix_det [fintype n] [decidable_eq n] [comm_ring R] [topological_ring R]
+  {A : X → matrix n n R} (hA : continuous A) :
+  continuous (λ x, (A x).det) :=
 begin
-  show continuous (λ A : matrix n n R, A.det),
   simp_rw matrix.det_apply,
   refine continuous_finset_sum _ (λ l _, continuous.const_smul _ _),
-  refine continuous_finset_prod _ (λ l _, continuous_matrix_elem _ _),
+  refine continuous_finset_prod _ (λ l _, hA.matrix_elem _ _),
 end
 
-lemma continuous_update_column [decidable_eq n] (i : n) :
-  continuous (λ x : matrix m n R × (m → R), x.1.update_column i x.2) :=
-continuous_matrix $ λ j k, (continuous_apply k).comp $ continuous.update
-  ((continuous_apply _).comp continuous_fst) i ((continuous_apply _).comp continuous_snd)
+@[continuity]
+lemma continuous.matrix_update_column [decidable_eq n] (i : n)
+  {A : X → matrix m n R} {B : X → m → R} (hA : continuous A) (hB : continuous B) :
+  continuous (λ x, (A x).update_column i (B x)) :=
+continuous_matrix $ λ j k, (continuous_apply k).comp $
+  ((continuous_apply _).comp hA).update i ((continuous_apply _).comp hB)
 
-lemma continuous_update_row [decidable_eq m] (i : m) :
-  continuous (λ x : matrix m n R × (n → R), x.1.update_row i x.2) :=
-continuous_update i
+@[continuity]
+lemma continuous.matrix_update_row [decidable_eq m] (i : m)
+  {A : X → matrix m n R} {B : X → n → R} (hA : continuous A) (hB : continuous B) :
+  continuous (λ x, (A x).update_row i (B x)) :=
+hA.update i hB
 
-lemma continuous_cramer [fintype n] [decidable_eq n] [comm_ring R] [topological_ring R] :
-  continuous (λ A : matrix n n R × (n → R), A.1.cramer A.2) :=
-continuous_pi $ λ i, continuous_det.comp $ continuous_update_column _
+@[continuity]
+lemma continuous.matrix_cramer [fintype n] [decidable_eq n] [comm_ring R] [topological_ring R]
+  {A : X → matrix n n R} {B : X → n → R} (hA : continuous A) (hB : continuous B) :
+  continuous (λ x, (A x).cramer (B x)) :=
+continuous_pi $ λ i, (hA.matrix_update_column _ hB).matrix_det
 
-lemma continuous_adjugate [fintype n] [decidable_eq n] [comm_ring R] [topological_ring R] :
-  continuous (adjugate : matrix n n R → matrix n n R) :=
-continuous_matrix $ λ j k, continuous_det.comp $ begin
-  show continuous (
-    (λ (a : matrix n n R × (n → R)), a.1.update_column k a.2) ∘
-    (λ a, (a, pi.single j 1)) ∘
-    transpose),
-  exact (continuous_update_column k).comp
-    (continuous.comp (continuous_id.prod_mk continuous_const) continuous_transpose),
-end
+@[continuity]
+lemma continuous.matrix_adjugate [fintype n] [decidable_eq n] [comm_ring R] [topological_ring R]
+  {A : X → matrix n n R} (hA : continuous A) :
+  continuous (λ x, (A x).adjugate) :=
+continuous_matrix $ λ j k, (hA.matrix_transpose.matrix_update_column k continuous_const).matrix_det
 
 /-- When `ring.inverse` is continuous at the determinant (such as in a `normed_ring`, or a
 `topological_field`), so is `matrix.has_inv`. -/
-lemma continuous_at_inv [fintype n] [decidable_eq n] [comm_ring R] [topological_ring R]
+lemma continuous_at_matrix_inv [fintype n] [decidable_eq n] [comm_ring R] [topological_ring R]
   (A : matrix n n R) (h : continuous_at ring.inverse A.det) :
   continuous_at has_inv.inv A :=
-(h.comp continuous_det.continuous_at).smul continuous_adjugate.continuous_at
-
-end matrix
+(h.comp continuous_id.matrix_det.continuous_at).smul continuous_id.matrix_adjugate.continuous_at

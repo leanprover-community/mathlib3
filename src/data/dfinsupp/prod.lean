9530992271a2e6d75c2cd14eb91cec12746d2c59
Eric Wieser <wieser.eric@gmail.com>
2022-03-31 17:21:15 +0000

    rename and golf

--- src/data/dfinsupp/prod.lean
+++ src/data/dfinsupp/prod.lean
@@ -1,52 +1,48 @@
-import data.dfinsupp
+import data.dfinsupp.basic
 import tactic
 
 universes u v w
 
 namespace dfinsupp.prod
 
-variables {ii : Type u} {jj : Type v} [decidable_eq ii] [decidable_eq jj]
-variables {β : ii → jj → Type w} [Π i j, has_zero (β i j)]
+variables {ι₁ : Type u} {ι₂ : Type v} [decidable_eq ι₁] [decidable_eq ι₂]
+variables {β : ι₁ → ι₂ → Type w} [Π i j, has_zero (β i j)]
 
 -- this definition is complete, and looks fairly readable - although there may be a shorter way
-def prod_to_nested (f : Π₀ (ij : ii × jj), β ij.1 ij.2) : Π₀ i, Π₀ j, β i j := begin
-  fapply quotient.lift_on f,
-  exact λ fij, ⟦{
+def curry (f : Π₀ (ij : ι₁ × ι₂), β ij.1 ij.2) : Π₀ i, Π₀ j, β i j :=
+begin
+  refine quotient.lift_on f (λ fij, _) (λ a b H, _),
+  exact ⟦{
     to_fun := λ i, ⟦ {
       to_fun := λ j, fij.to_fun (i, j),
       pre_support := fij.pre_support.map prod.snd,
       zero := λ j, begin
-        obtain hin | h0 := fij.zero (i, j),
-        {apply or.inl, rw multiset.mem_map, use (i, j), simp only [hin, eq_self_iff_true, and_self]},
-        {exact or.inr h0},
+        rw multiset.mem_map,
+        refine (fij.zero (i, j)).imp_left _,
+        intro hin,
+        exact ⟨(i, j), hin, rfl⟩
       end
     } ⟧,
     pre_support := fij.pre_support.map prod.fst,
     zero := λ i, begin
-      unfold has_zero.zero,
-      rw quotient.eq,
-      unfold has_equiv.equiv setoid.r dfinsupp.pre.to_fun,
-      rw ← forall_or_distrib_left,
-      exact λ j, begin
-        obtain hin | h0 := fij.zero (i, j),
-        {apply or.inl, rw multiset.mem_map, use (i, j), simp only [hin, eq_self_iff_true, and_self]},
-        {exact or.inr h0},
-      end
+      rw multiset.mem_map,
+      refine or.imp id quotient.sound _,
+      refine forall_or_distrib_left.mp (λ j, _),
+      refine (fij.zero (i, j)).imp_left _,
+      intro hin,
+      exact ⟨(i, j), hin, rfl⟩
     end
   }⟧,
-  exact λ a b H, quotient.sound $ λ i, by {
-    simp [H (i, _)],
-    exact λ j, by simp [H (i, j)]
-  },
+  exact (quotient.sound $ λ i, quotient.sound $ λ j, H (i, j)),
 end
 
 -- this was my first attempt, which requires me to prove untrue statements
-def nested_to_prod (f : Π₀ i j, β i j) : Π₀ (ij : ii × jj), β ij.1 ij.2 := begin
+def uncurry (f : Π₀ i j, β i j) : Π₀ (ij : ι₁ × ι₂), β ij.1 ij.2 := begin
   fapply quotient.lift_on f,
   exact λ fi, ⟦{
     to_fun := λ ij, fi.to_fun ij.fst ij.snd,
     pre_support := fi.pre_support.bind (λ i, begin
-      let js : multiset jj := quotient.lift_on (fi.to_fun i)
+      let js : multiset ι₂ := quotient.lift_on (fi.to_fun i)
         dfinsupp.pre.pre_support
         (λ a b H, begin
           /- This is unprovable, I think lift_on is the wrong approach, or I need to return a clever quotient -/
@@ -66,11 +62,11 @@ def nested_to_prod (f : Π₀ i j, β i j) : Π₀ (ij : ii × jj), β ij.1 ij.2
 end
 
 -- second attempt, by rewriting the inner function into a product function first.
-def nested_to_prod2 (f : Π₀ i j, β i j) : Π₀ (ij : ii × jj), β ij.1 ij.2 := begin
+def uncurry2 (f : Π₀ i j, β i j) : Π₀ (ij : ι₁ × ι₂), β ij.1 ij.2 := begin
   fapply quotient.lift_on f,
   exact λ fi, by {
     -- build a set of functions, each non-zero for a single i
-    let fij : multiset (Π₀ (ij : ii × jj), β ij.fst ij.snd) := fi.pre_support.map (λ i, begin
+    let fij : multiset (Π₀ (ij : ι₁ × ι₂), β ij.fst ij.snd) := fi.pre_support.map (λ i, begin
       fapply quotient.lift_on (fi.to_fun i),
       exact λ fij, begin
         exact ⟦{
@@ -115,24 +111,24 @@ def nested_to_prod2 (f : Π₀ i j, β i j) : Π₀ (ij : ii × jj), β ij.1 ij.
   exact λ a b H, sorry,
 end
 
-lemma is_right_inverse  (f : Π₀ i j, β i j) : prod_to_nested(nested_to_prod(f)) = f := begin
-  unfold prod_to_nested nested_to_prod,
+lemma is_right_inverse  (f : Π₀ i j, β i j) : curry(uncurry(f)) = f := begin
+  unfold curry uncurry,
   simp,
-  -- irrelevant until `nested_to_prod` is fixed
+  -- irrelevant until `uncurry` is fixed
   sorry
 end
 
-lemma is_left_inverse (f : Π₀ (ij : ii × jj), β ij.1 ij.2) : nested_to_prod(prod_to_nested(f)) = f := begin
-  unfold prod_to_nested nested_to_prod,
+lemma is_left_inverse (f : Π₀ (ij : ι₁ × ι₂), β ij.1 ij.2) : uncurry(curry(f)) = f := begin
+  unfold curry uncurry,
   simp,
-  -- irrelevant until `nested_to_prod` is fixed
+  -- irrelevant until `uncurry` is fixed
   sorry
 end
 
 -- the sole purpose of this file
-def eqv : (Π₀ (ij : ii × jj), β ij.1 ij.2) ≃ (Π₀ i j, β i j) := {
-  to_fun := prod_to_nested,
-  inv_fun := nested_to_prod,
+def eqv : (Π₀ (ij : ι₁ × ι₂), β ij.1 ij.2) ≃ (Π₀ i j, β i j) := {
+  to_fun := curry,
+  inv_fun := uncurry,
   left_inv := is_left_inverse,
   right_inv := is_right_inverse }
 

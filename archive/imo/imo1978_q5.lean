e26a6ddc0e41026645b9c5ba7544aff80b567eaf
MantasBaksys <baksys.mantas@gmail.com>
2022-03-31 17:01:59 +0100

    update slightly

--- archive/imo/imo1978_q5.lean
+++ archive/imo/imo1978_q5.lean
@@ -10,16 +10,6 @@ import data.real.basic
 
 open_locale big_operators
 
-
-lemma to_name {ι α : Type*} [preorder ι] [linear_order α] {f : ι → α} (s : finset ι) :
-  ∃ σ : equiv.perm ι, {x | σ x ≠ x} ⊆ s ∧ monotone_on (f ∘ σ) s :=
-begin
-  classical,
-  apply s.induction_on,
-  { },
-  { }
-end
-
 variables (n : ℕ) (a : ℕ ↪ ℕ) (ha0 : a 0 = 0)
 include ha0
 

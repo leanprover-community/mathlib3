fd38bb5c0c5af7c1f5162fb649ce5bd14b57d93b
Joël Riou <joel.riou@universite-paris-saclay.fr>
2022-03-31 20:12:29 +0200

    fixed author line

--- src/algebraic_topology/dold_kan/homotopies.lean
+++ src/algebraic_topology/dold_kan/homotopies.lean
@@ -1,7 +1,7 @@
 /-
 Copyright (c) 2022 Joël Riou. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
-Author: Joël Riou
+Authors: Joël Riou
 -/
 
 import algebra.homology.homotopy

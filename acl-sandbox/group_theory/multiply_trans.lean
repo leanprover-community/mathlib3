7345e0f57ac6dec2f5c30e13d4272fa03cbaff11
Antoine Chambert-Loir <antoine.chambert-loir@math.univ-paris-diderot.fr>
2022-04-02 00:43:02 +0200

    multiple transitivity of symmetric and alternating

--- acl-sandbox/group_theory/multiply_trans.lean
+++ acl-sandbox/group_theory/multiply_trans.lean
@@ -664,8 +664,6 @@ open_locale classical
 
 variables (α : Type*) [fintype α]
 
-#check equiv.perm.mul_def
-
 lemma unnamed [fintype α] :
   mul_action.is_multiply_pretransitive (equiv.perm α) α (fintype.card α):=
 begin
@@ -687,11 +685,87 @@ end
 
 lemma unnamed' [fintype α] :
   mul_action.is_multiply_pretransitive (alternating_group α) α (fintype.card α - 2) :=
-sorry
+begin
+  cases lt_or_ge (fintype.card α) 2 with h2 h2,
+  { rw nat.sub_eq_zero_of_le (le_of_lt h2),
+    apply is_zero_pretransitive },
+  -- fintype.card α ≥ 2
+  obtain ⟨n,hn⟩ := nat.le.dest h2,
+  have hn' : fintype.card α - 2 = n := norm_num.sub_nat_pos (fintype.card α) 2 n hn,
+  rw add_comm at hn,
+  have hn_le : n ≤ fintype.card α, { rw ← hn, exact le_self_add },
 
+  apply is_pretransitive.mk,
+  rw hn',
+  intros x y,
+
+  obtain ⟨x', hx'⟩ :=
+    may_extend hn_le (le_of_eq (cardinal.mk_fintype α).symm) x,
+  obtain ⟨y', hy'⟩ :=
+    may_extend hn_le (le_of_eq (cardinal.mk_fintype α).symm) y,
+  let heq := (unnamed α).exists_smul_eq,
+  obtain ⟨g, hg⟩ := heq x' y',
+  cases int.units_eq_one_or g.sign with h h,
+  { use ⟨g, equiv.perm.mem_alternating_group.mpr h⟩,
+    ext i,
+    simp only [function.embedding.smul_apply],
+    rw [← hx', ← hy', ← hg],
+    refl },
+  { have hg'1 : n + 1 < fintype.card α,
+    { rw ← hn, exact nat.lt.base (n + 1) },
+    have hg'2 : n < fintype.card α,
+    { apply lt_trans _ hg'1, exact lt_add_one n },
+
+    let g' := equiv.swap (y'.to_fun ⟨n+1, hg'1⟩) (y'.to_fun ⟨n, hg'2⟩),
+
+    have hg' : g'.sign = -1,
+    { rw equiv.perm.is_swap.sign_eq,
+      unfold equiv.perm.is_swap,
+      use (y'.to_fun ⟨n+1, hg'1⟩), use (y'.to_fun ⟨n, hg'2⟩),
+      split,
+      { intro h,
+        let h' := function.embedding.injective y' h,
+        simp only [subtype.mk_eq_mk] at h',
+        exact nat.succ_ne_self _ h' },
+      refl },
+
+    use g' * g,
+    { rw equiv.perm.mem_alternating_group,
+      simp only [equiv.perm.sign_mul, h, hg'],
+      norm_num },
+    ext i, simp only [function.embedding.smul_apply],
+    rw [← hx', ← hy', ← hg],
+
+      simp only [function.embedding.trans_apply, rel_embedding.coe_fn_to_embedding,
+        function.embedding.smul_apply, equiv.perm.smul_def],
+
+    change (g' * g) • _ = _,
+    rw ← smul_smul,
+    simp,
+    change (equiv.swap (y'.to_fun ⟨n+1, hg'1⟩) (y'.to_fun ⟨n, hg'2⟩))  _ = _,
+
+    refine equiv.swap_apply_of_ne_of_ne _ _,
+    { rw ← hg,
+      intro h,
+      simp only [function.embedding.to_fun_eq_coe, function.embedding.smul_apply,
+        equiv.perm.smul_def, embedding_like.apply_eq_iff_eq] at h,
+      let h' := fin.veq_of_eq h,
+      simp only [fin.val_eq_coe, fin.coe_cast_le] at h',
+      let hi := i.prop,
+      rw h' at hi,
+      simpa only [add_lt_iff_neg_left, not_lt_zero'] using hi } ,
+    { rw ← hg,
+      intro h,
+      simp only [function.embedding.to_fun_eq_coe, function.embedding.smul_apply,
+        equiv.perm.smul_def, embedding_like.apply_eq_iff_eq] at h,
+      let h' := fin.veq_of_eq h,
+      simp only [fin.val_eq_coe, fin.coe_cast_le] at h',
+      let hi := i.prop,
+      rw h' at hi,
+      simpa only [lt_self_iff_false] using hi} }
+end
 
 end finite_groups
-
 
 #exit
 

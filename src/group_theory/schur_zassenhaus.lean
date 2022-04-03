7d7bf19c8d123db2d4debeeb06e4248925fa72e0
Thomas Browning <tb65536@uw.edu>
2022-04-03 09:29:40 -0700

    .trans

--- src/group_theory/schur_zassenhaus.lean
+++ src/group_theory/schur_zassenhaus.lean
@@ -47,7 +47,7 @@ lemma smul_symm_apply_eq_mul_symm_apply_inv_smul
   (g : G) (α : left_transversals (H : set G)) (q : G ⧸ H) :
   ↑(to_equiv (g • α).2 q) = g * (to_equiv α.2 (g⁻¹ • q : G ⧸ H)) :=
 begin
-  refine eq.trans (subtype.ext_iff.mp ((to_equiv (g • α).2).apply_eq_iff_eq_symm_apply.mpr _))
+  refine (subtype.ext_iff.mp ((to_equiv (g • α).2).apply_eq_iff_eq_symm_apply.mpr _)).trans
     (subtype.coe_mk _ (mem_left_coset g (subtype.mem _))),
   change q = g • ((to_equiv α.2).symm (to_equiv α.2 (g⁻¹ • q : G ⧸ H))),
   rw [equiv.symm_apply_apply, ←mul_smul, mul_inv_self, one_smul],

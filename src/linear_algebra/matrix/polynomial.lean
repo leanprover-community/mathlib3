80109033ac2a2d90071e8e13c5f2cf08ee1541ac
adomani <adomani@gmail.com>
2022-04-04 13:56:20 +0200

    partial fix

--- src/linear_algebra/matrix/polynomial.lean
+++ src/linear_algebra/matrix/polynomial.lean
@@ -88,7 +88,8 @@ begin
     simp [coeff_C] },
   { intros p hp,
     refine (nat_degree_add_le _ _).trans _,
-    simpa using (nat_degree_mul_C_le _ _).trans nat_degree_X_le }
+    simp,
+    exact (nat_degree_C_mul_le _ _).trans nat_degree_X_le }
 end
 
 lemma leading_coeff_det_X_one_add_C (A : matrix n n α) :

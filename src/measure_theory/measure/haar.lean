a44448cffad54e85686284b4b11d9c1dcbdc6075
YaelDillies <yael.dillies@gmail.com>
2022-04-04 13:27:26 +0200

    fix measure_theory.measure.haar

--- src/measure_theory/measure/haar.lean
+++ src/measure_theory/measure/haar.lean
@@ -495,8 +495,8 @@ lemma haar_content_outer_measure_self_pos {K₀ : positive_compacts G} :
 begin
   apply ennreal.zero_lt_one.trans_le,
   rw [content.outer_measure_eq_infi],
-  refine le_infi₂ (λ U hU, le_infi _),
-  exact λ h2U, haar_content_self.ge.trans (le_supr₂ K₀.to_compacts h2U),
+  refine le_infi₂ (λ U hU, le_infi $ λ hK₀, le_trans _ $ le_supr₂ K₀.to_compacts hK₀),
+  exact haar_content_self.ge,
 end
 
 end haar

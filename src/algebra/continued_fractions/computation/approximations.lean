4b48d386a25a4eadec632328a071bd70d51b9505
Mario Carneiro <di.gama@gmail.com>
2022-04-05 05:30:46 -0400

    fix

--- src/algebra/continued_fractions/computation/approximations.lean
+++ src/algebra/continued_fractions/computation/approximations.lean
@@ -515,7 +515,8 @@ begin
       simp only [stream_nth_fr_ne_zero, conts_eq.symm, pred_conts_eq.symm] at tmp,
       rw tmp,
       simp only [denom'],
-      ring_nf },
+      ring_nf,
+      ac_refl },
     rwa this },
   -- derive some tedious inequalities that we need to rewrite our goal
   have nextConts_b_ineq : (fib (n + 2) : K) ≤ (pred_conts.b + gp.b * conts.b), by

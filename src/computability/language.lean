0bc458256c75ef5eceeb78d5693f2d94b1b63c50
YaelDillies <yael.dillies@gmail.com>
2022-04-06 12:08:31 +0200

    initial commit

--- src/computability/language.lean
+++ src/computability/language.lean
@@ -17,6 +17,8 @@ over the languages.
 
 universes u v
 
+open list set
+
 variables {α : Type u}
 
 /-- A language is a set of strings over an alphabet. -/
@@ -24,6 +26,7 @@ variables {α : Type u}
 def language (α) := set (list α)
 
 namespace language
+variables {l m : language α} {a b x : list α}
 
 local attribute [reducible] language
 
@@ -52,14 +55,15 @@ def star (l : language α) : language α :=
 lemma star_def (l : language α) :
   l.star = { x | ∃ S : list (list α), x = S.join ∧ ∀ y ∈ S, y ∈ l} := rfl
 
+lemma not_mem_zero (x : list α) : x ∉ (0 : language α) := id
 @[simp] lemma mem_one (x : list α) : x ∈ (1 : language α) ↔ x = [] := by refl
-@[simp] lemma mem_add (l m : language α) (x : list α) : x ∈ l + m ↔ x ∈ l ∨ x ∈ m :=
-by simp [add_def]
-lemma mem_mul (l m : language α) (x : list α) : x ∈ l * m ↔ ∃ a b, a ∈ l ∧ b ∈ m ∧ a ++ b = x :=
-by simp [mul_def]
-lemma mem_star (l : language α) (x : list α) :
-  x ∈ l.star ↔ ∃ S : list (list α), x = S.join ∧ ∀ y ∈ S, y ∈ l :=
-iff.rfl
+lemma nil_mem_one : [] ∈ (1 : language α) := set.mem_singleton _
+@[simp] lemma mem_add (l m : language α) (x : list α) : x ∈ l + m ↔ x ∈ l ∨ x ∈ m := iff.rfl
+lemma mem_mul : x ∈ l * m ↔ ∃ a b, a ∈ l ∧ b ∈ m ∧ a ++ b = x := mem_image2
+lemma append_mem_mul : a ∈ l → b ∈ m → a ++ b ∈ l * m := mem_image2_of_mem
+lemma mem_star : x ∈ l.star ↔ ∃ S : list (list α), x = S.join ∧ ∀ y ∈ S, y ∈ l := iff.rfl
+lemma join_mem_star {S : list (list α)} (h : ∀ y ∈ S, y ∈ l) : S.join ∈ l.star := ⟨S, rfl, h⟩
+lemma nil_mem_star (l : language α) : [] ∈ l.star := ⟨[], rfl, λ _, false.elim⟩
 
 instance : semiring (language α) :=
 { add := (+),

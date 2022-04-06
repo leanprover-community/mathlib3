af5f1bc2667e1b1c5301b404553bcc5bb89f82e7
YaelDillies <yael.dillies@gmail.com>
2022-04-06 16:20:50 +0200

    fix lint

--- src/data/set/intervals/with_bot_top.lean
+++ src/data/set/intervals/with_bot_top.lean
@@ -1,5 +1,5 @@
 /-
-Copyright (c) 2021 Yury G. Kudryashov. All rights reserved.
+Copyright (c) 2022 Yury G. Kudryashov. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Yury G. Kudryashov
 -/
@@ -20,9 +20,12 @@ variables {α : Type*}
 
 namespace with_top
 
+@[simp] lemma preimage_coe_top : (coe : α → with_top α) ⁻¹' {⊤} = (∅ : set α) :=
+eq_empty_of_subset_empty $ λ a, coe_ne_top
+
 variables [partial_order α] {a b : α}
 
-@[simp] lemma range_coe : range (coe : α → with_top α) = Iio ⊤ :=
+lemma range_coe : range (coe : α → with_top α) = Iio ⊤ :=
 begin
   ext x,
   rw [mem_Iio, lt_top_iff_ne_top, mem_range, ← none_eq_top, option.ne_none_iff_exists],
@@ -30,11 +33,8 @@ begin
 end
 
 @[simp] lemma preimage_coe_Ioi : (coe : α → with_top α) ⁻¹' (Ioi a) = Ioi a := ext $ λ x, coe_lt_coe
-
 @[simp] lemma preimage_coe_Ici : (coe : α → with_top α) ⁻¹' (Ici a) = Ici a := ext $ λ x, coe_le_coe
-
 @[simp] lemma preimage_coe_Iio : (coe : α → with_top α) ⁻¹' (Iio a) = Iio a := ext $ λ x, coe_lt_coe
-
 @[simp] lemma preimage_coe_Iic : (coe : α → with_top α) ⁻¹' (Iic a) = Iic a := ext $ λ x, coe_le_coe
 
 @[simp] lemma preimage_coe_Icc : (coe : α → with_top α) ⁻¹' (Icc a b) = Icc a b :=
@@ -49,9 +49,6 @@ by simp [← Ioi_inter_Iic]
 @[simp] lemma preimage_coe_Ioo : (coe : α → with_top α) ⁻¹' (Ioo a b) = Ioo a b :=
 by simp [← Ioi_inter_Iio]
 
-@[simp] lemma preimage_coe_top : (coe : α → with_top α) ⁻¹' {⊤} = (∅ : set α) :=
-eq_empty_of_subset_empty $ λ a, coe_ne_top
-
 @[simp] lemma preimage_coe_Iio_top : (coe : α → with_top α) ⁻¹' (Iio ⊤) = univ :=
 by rw [← range_coe, preimage_range]
 
@@ -61,33 +58,33 @@ by simp [← Ici_inter_Iio]
 @[simp] lemma preimage_coe_Ioo_top : (coe : α → with_top α) ⁻¹' (Ioo a ⊤) = Ioi a :=
 by simp [← Ioi_inter_Iio]
 
-@[simp] lemma image_coe_Ioi : (coe : α → with_top α) '' (Ioi a) = Ioo a ⊤ :=
+lemma image_coe_Ioi : (coe : α → with_top α) '' (Ioi a) = Ioo a ⊤ :=
 by rw [← preimage_coe_Ioi, image_preimage_eq_inter_range, range_coe, Ioi_inter_Iio]
 
-@[simp] lemma image_coe_Ici : (coe : α → with_top α) '' (Ici a) = Ico a ⊤ :=
+lemma image_coe_Ici : (coe : α → with_top α) '' (Ici a) = Ico a ⊤ :=
 by rw [← preimage_coe_Ici, image_preimage_eq_inter_range, range_coe, Ici_inter_Iio]
 
-@[simp] lemma image_coe_Iio : (coe : α → with_top α) '' (Iio a) = Iio a :=
+lemma image_coe_Iio : (coe : α → with_top α) '' (Iio a) = Iio a :=
 by rw [← preimage_coe_Iio, image_preimage_eq_inter_range, range_coe,
   inter_eq_self_of_subset_left (Iio_subset_Iio le_top)]
 
-@[simp] lemma image_coe_Iic : (coe : α → with_top α) '' (Iic a) = Iic a :=
+lemma image_coe_Iic : (coe : α → with_top α) '' (Iic a) = Iic a :=
 by rw [← preimage_coe_Iic, image_preimage_eq_inter_range, range_coe,
   inter_eq_self_of_subset_left (Iic_subset_Iio.2 $ coe_lt_top a)]
 
-@[simp] lemma image_coe_Icc : (coe : α → with_top α) '' (Icc a b) = Icc a b :=
+lemma image_coe_Icc : (coe : α → with_top α) '' (Icc a b) = Icc a b :=
 by rw [← preimage_coe_Icc, image_preimage_eq_inter_range, range_coe,
   inter_eq_self_of_subset_left (subset.trans Icc_subset_Iic_self $ Iic_subset_Iio.2 $ coe_lt_top b)]
 
-@[simp] lemma image_coe_Ico : (coe : α → with_top α) '' (Ico a b) = Ico a b :=
+lemma image_coe_Ico : (coe : α → with_top α) '' (Ico a b) = Ico a b :=
 by rw [← preimage_coe_Ico, image_preimage_eq_inter_range, range_coe,
   inter_eq_self_of_subset_left (subset.trans Ico_subset_Iio_self $ Iio_subset_Iio le_top)]
 
-@[simp] lemma image_coe_Ioc : (coe : α → with_top α) '' (Ioc a b) = Ioc a b :=
+lemma image_coe_Ioc : (coe : α → with_top α) '' (Ioc a b) = Ioc a b :=
 by rw [← preimage_coe_Ioc, image_preimage_eq_inter_range, range_coe,
   inter_eq_self_of_subset_left (subset.trans Ioc_subset_Iic_self $ Iic_subset_Iio.2 $ coe_lt_top b)]
 
-@[simp] lemma image_coe_Ioo : (coe : α → with_top α) '' (Ioo a b) = Ioo a b :=
+lemma image_coe_Ioo : (coe : α → with_top α) '' (Ioo a b) = Ioo a b :=
 by rw [← preimage_coe_Ioo, image_preimage_eq_inter_range, range_coe,
   inter_eq_self_of_subset_left (subset.trans Ioo_subset_Iio_self $ Iio_subset_Iio le_top)]
 
@@ -97,17 +94,16 @@ end with_top
 
 namespace with_bot
 
+@[simp] lemma preimage_coe_bot : (coe : α → with_bot α) ⁻¹' {⊥} = (∅ : set α) :=
+@with_top.preimage_coe_top (order_dual α)
+
 variables [partial_order α] {a b : α}
 
-@[simp] lemma range_coe : range (coe : α → with_bot α) = Ioi ⊥ :=
-@with_top.range_coe (order_dual α) _
+lemma range_coe : range (coe : α → with_bot α) = Ioi ⊥ := @with_top.range_coe (order_dual α) _
 
 @[simp] lemma preimage_coe_Ioi : (coe : α → with_bot α) ⁻¹' (Ioi a) = Ioi a := ext $ λ x, coe_lt_coe
-
 @[simp] lemma preimage_coe_Ici : (coe : α → with_bot α) ⁻¹' (Ici a) = Ici a := ext $ λ x, coe_le_coe
-
 @[simp] lemma preimage_coe_Iio : (coe : α → with_bot α) ⁻¹' (Iio a) = Iio a := ext $ λ x, coe_lt_coe
-
 @[simp] lemma preimage_coe_Iic : (coe : α → with_bot α) ⁻¹' (Iic a) = Iic a := ext $ λ x, coe_le_coe
 
 @[simp] lemma preimage_coe_Icc : (coe : α → with_bot α) ⁻¹' (Icc a b) = Icc a b :=
@@ -122,9 +118,6 @@ by simp [← Ioi_inter_Iic]
 @[simp] lemma preimage_coe_Ioo : (coe : α → with_bot α) ⁻¹' (Ioo a b) = Ioo a b :=
 by simp [← Ioi_inter_Iio]
 
-@[simp] lemma preimage_coe_bot : (coe : α → with_bot α) ⁻¹' {⊥} = (∅ : set α) :=
-@with_top.preimage_coe_top (order_dual α) _
-
 @[simp] lemma preimage_coe_Ioi_bot : (coe : α → with_bot α) ⁻¹' (Ioi ⊥) = univ :=
 by rw [← range_coe, preimage_range]
 
@@ -134,33 +127,33 @@ by simp [← Ioi_inter_Iic]
 @[simp] lemma preimage_coe_Ioo_bot : (coe : α → with_bot α) ⁻¹' (Ioo ⊥ a) = Iio a :=
 by simp [← Ioi_inter_Iio]
 
-@[simp] lemma image_coe_Iio : (coe : α → with_bot α) '' (Iio a) = Ioo ⊥ a :=
+lemma image_coe_Iio : (coe : α → with_bot α) '' (Iio a) = Ioo ⊥ a :=
 by rw [← preimage_coe_Iio, image_preimage_eq_inter_range, range_coe, inter_comm, Ioi_inter_Iio]
 
-@[simp] lemma image_coe_Iic : (coe : α → with_bot α) '' (Iic a) = Ioc ⊥ a :=
+lemma image_coe_Iic : (coe : α → with_bot α) '' (Iic a) = Ioc ⊥ a :=
 by rw [← preimage_coe_Iic, image_preimage_eq_inter_range, range_coe, inter_comm, Ioi_inter_Iic]
 
-@[simp] lemma image_coe_Ioi : (coe : α → with_bot α) '' (Ioi a) = Ioi a :=
+lemma image_coe_Ioi : (coe : α → with_bot α) '' (Ioi a) = Ioi a :=
 by rw [← preimage_coe_Ioi, image_preimage_eq_inter_range, range_coe,
   inter_eq_self_of_subset_left (Ioi_subset_Ioi bot_le)]
 
-@[simp] lemma image_coe_Ici : (coe : α → with_bot α) '' (Ici a) = Ici a :=
+lemma image_coe_Ici : (coe : α → with_bot α) '' (Ici a) = Ici a :=
 by rw [← preimage_coe_Ici, image_preimage_eq_inter_range, range_coe,
   inter_eq_self_of_subset_left (Ici_subset_Ioi.2 $ bot_lt_coe a)]
 
-@[simp] lemma image_coe_Icc : (coe : α → with_bot α) '' (Icc a b) = Icc a b :=
+lemma image_coe_Icc : (coe : α → with_bot α) '' (Icc a b) = Icc a b :=
 by rw [← preimage_coe_Icc, image_preimage_eq_inter_range, range_coe,
   inter_eq_self_of_subset_left (subset.trans Icc_subset_Ici_self $ Ici_subset_Ioi.2 $ bot_lt_coe a)]
 
-@[simp] lemma image_coe_Ioc : (coe : α → with_bot α) '' (Ioc a b) = Ioc a b :=
+lemma image_coe_Ioc : (coe : α → with_bot α) '' (Ioc a b) = Ioc a b :=
 by rw [← preimage_coe_Ioc, image_preimage_eq_inter_range, range_coe,
   inter_eq_self_of_subset_left (subset.trans Ioc_subset_Ioi_self $ Ioi_subset_Ioi bot_le)]
 
-@[simp] lemma image_coe_Ico : (coe : α → with_bot α) '' (Ico a b) = Ico a b :=
+lemma image_coe_Ico : (coe : α → with_bot α) '' (Ico a b) = Ico a b :=
 by rw [← preimage_coe_Ico, image_preimage_eq_inter_range, range_coe,
   inter_eq_self_of_subset_left (subset.trans Ico_subset_Ici_self $ Ici_subset_Ioi.2 $ bot_lt_coe a)]
 
-@[simp] lemma image_coe_Ioo : (coe : α → with_bot α) '' (Ioo a b) = Ioo a b :=
+lemma image_coe_Ioo : (coe : α → with_bot α) '' (Ioo a b) = Ioo a b :=
 by rw [← preimage_coe_Ioo, image_preimage_eq_inter_range, range_coe,
   inter_eq_self_of_subset_left (subset.trans Ioo_subset_Ioi_self $ Ioi_subset_Ioi bot_le)]
 

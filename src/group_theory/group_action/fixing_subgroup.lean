ab627d878ce78f8d171d2d003a3bcccf9962087a
Antoine Chambert-Loir <antoine.chambert-loir@math.univ-paris-diderot.fr>
2022-04-07 11:18:49 +0200

    alignment in the doc

--- src/group_theory/group_action/fixing_subgroup.lean
+++ src/group_theory/group_action/fixing_subgroup.lean
@@ -19,37 +19,37 @@ and relates it to the set of fixed points via a Galois connection.
 ## Main definitions
 
 * `fixing_submonoid M s` :
-in the presence of `mul_action M α` (with `monoid M`)
-it is the `submonoid M` consisting of elements which fix `s : set α` pointwise.
+  in the presence of `mul_action M α` (with `monoid M`)
+  it is the `submonoid M` consisting of elements which fix `s : set α` pointwise.
 
 * `fixing_submonoid_fixed_points_connection M α` is the `galois_connection`
-that relates `fixing_submonoid` with `fixed_points`.
+  that relates `fixing_submonoid` with `fixed_points`.
 
 * `fixing_submonoid_of_union` and `fixing_submonoid_of_Union` are consequences
-of the Galois connection, as well as `fixed_points_of_sup` and `fixed_points_of_supr`.
+  of the Galois connection, as well as `fixed_points_of_sup` and `fixed_points_of_supr`.
 
 * `fixing_subgroup M s` :
-in the presence of `mul_action M α` (with `group M`)
-it is the `subgroup M` consisting of elements which fix `s : set α` pointwise.
+  in the presence of `mul_action M α` (with `group M`)
+  it is the `subgroup M` consisting of elements which fix `s : set α` pointwise.
 
 * `fixing_subgroup_fixed_points_connection M α` is the `galois_connection`
-that relates `fixing_subgroup` with `fixed_points`.
+  that relates `fixing_subgroup` with `fixed_points`.
 
 * `fixing_subgroup_of_union` and `fixing_subgroup_of_Union` are consequences
-of the Galois connection,
-as well as `fixed_points_of_group_of_sup` and `fixed_points_of_group_of_supr`.
+  of the Galois connection,
+  as well as `fixed_points_of_group_of_sup` and `fixed_points_of_group_of_supr`.
 
 * The file starts with some lemmas that allow to rewrite `antitone` into `monotone`
-in various cases.
-Apparently the `monotone.dual` (and analogues) were not sufficient, so I wrote `monotone.dual_iff`
-that works in both directions.
+  in various cases.
+  Apparently the `monotone.dual` (and analogues) were not sufficient, so I wrote `monotone.dual_iff`
+  that works in both directions.
 
 TODO :
 
 * Adjust names as needed
 
 * Decide what needs to be done with the antitonicity section. In particular, the
-proofs need two directions, but the proof term is exactly the same (up to types).
+  proofs need two directions, but the proof term is exactly the same (up to types).
 
 * Maybe other lemmas are useful
 
@@ -137,12 +137,12 @@ galois_connection.l_supr (fixing_submonoid_fixed_points_connection M α)
 /-- Fixed points of sup of submonoids is intersection -/
 lemma fixed_points_of_submonoid_sup {P Q : submonoid M} :
   fixed_points (P ⊔ Q : submonoid M) α = fixed_points P α ∩ fixed_points Q α :=
-  galois_connection.u_inf (fixing_submonoid_fixed_points_connection M α)
+galois_connection.u_inf (fixing_submonoid_fixed_points_connection M α)
 
 /-- Fixed points of supr of submonoids is intersection -/
 lemma fixed_points_of_submonoid_supr {ι : Sort*} {P : ι → submonoid M} :
   fixed_points (supr P : submonoid M) α = ⋂ i, fixed_points (P i) α :=
-  galois_connection.u_infi (fixing_submonoid_fixed_points_connection M α)
+galois_connection.u_infi (fixing_submonoid_fixed_points_connection M α)
 
 end monoid
 

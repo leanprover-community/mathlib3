6a3550ae272fcaecda636ed56ab8e7852276f1d4
Kevin Wilson <khwilson@gmail.com>
2022-04-07 10:14:13 -0500

    Now with restatement (but WIP)

--- src/analysis/sum_integral_comparisons.lean
+++ src/analysis/sum_integral_comparisons.lean
@@ -31,6 +31,7 @@ API to directly address these issues forthwith.
 analysis, comparison, asymptotics
 -/
 
+open set
 open_locale big_operators
 
 lemma convert_finite_sum_to_interval_integral {m n : ℕ} {f : ℝ → ℝ} (hmn : m ≤ n) :
@@ -45,48 +46,51 @@ begin
     intros x hx,
     rw ← @integral_const_on_unit_interval x (f ↑x),
     apply interval_integral.integral_congr_ae,
-    rw [set.interval_oc_of_le (calc (x : ℝ) ≤ x + 1 : by simp), measure_theory.ae_iff],
+    rw [interval_oc_of_le (calc (x : ℝ) ≤ x + 1 : by simp), measure_theory.ae_iff],
     refine real.measure_space.volume.to_outer_measure.mono_null _
       (@real.volume_singleton ((x : ℝ) + 1)),
-    simp only [set.mem_Ioc, and_imp, not_forall, exists_prop, set.subset_singleton_iff,
-      set.mem_set_of_eq],
+    simp only [mem_Ioc, and_imp, not_forall, exists_prop, subset_singleton_iff,
+      mem_set_of_eq],
     intros y hy hy' hf,
     cases lt_or_eq_of_le hy',
     { exfalso, rw nat.floor_eq_on_Ico x y ⟨le_of_lt hy, h⟩ at hf, exact hf rfl, },
     { exact h, }, },
   { intros k hk,
-    rw [interval_integrable_iff, set.interval_oc_of_le (calc (k : ℝ) ≤ ↑(k + 1) : by simp)],
-    have : measure_theory.integrable_on (λ x : ℝ, f ↑k) (set.Ioc (k : ℝ) (↑k + 1))
+    rw [interval_integrable_iff, interval_oc_of_le (calc (k : ℝ) ≤ ↑(k + 1) : by simp)],
+    have : measure_theory.integrable_on (λ x : ℝ, f ↑k) (Ioc (k : ℝ) (↑k + 1))
       real.measure_space.volume,
     { simp, },
     apply this.congr_fun',
     rw [filter.eventually_eq, measure_theory.ae_iff],
     simp only [measure_theory.measure.restrict_apply', measurable_set_Ioc],
     refine measure_theory.outer_measure.mono_null _ _ (@real.volume_singleton ((k : ℝ) + 1)),
-    simp only [set.subset_singleton_iff, set.mem_inter_eq, set.mem_set_of_eq, set.mem_Ioc, and_imp],
+    simp only [subset_singleton_iff, mem_inter_eq, mem_set_of_eq, mem_Ioc, and_imp],
     intros y hf hy hy',
     cases lt_or_eq_of_le hy',
     { exfalso, rw k.floor_eq_on_Ico y ⟨le_of_lt hy, h⟩ at hf, exact hf rfl, },
     { exact h, }, },
 end
 
-lemma antitone_on.integral_le_sum {a b : ℕ} {f : ℝ → ℝ} (hab : a ≤ b)
-  (hf : antitone_on f (set.Icc a b)) : ∫ x in a..b, f x ≤ ∑ x in finset.Ico a b, f x :=
+lemma antitone_on.integral_le_sum' {x₀ : ℝ} {a : ℕ} {f : ℝ → ℝ}
+  (hf : antitone_on f (Icc x₀ (x₀ + a))) :
+  ∫ x in x₀..(x₀ + a), f x ≤ ∑ i in finset.range a, f (x₀ + i) :=
 begin
-  have : ∀ (x : ℝ), x ∈ set.Icc (a : ℝ) ↑b → f x ≤ f ⌊x⌋₊,
+  have : ∀ (x : ℝ), x ∈ Icc x₀ (x₀ + a) → f x ≤ f x₀,
   { intros x hx,
-    apply hf (nat.floor_mem_Icc_of_mem_Icc hx) hx,
-    exact nat.floor_le (calc (0 : ℝ) ≤ ↑a : by simp ... ≤ x : hx.left), },
+    apply hf,
+    simp,
+    exact hx,
+    simp [hx.left], },
   transitivity,
-  refine interval_integral.integral_mono_on (nat.cast_le.mpr hab) _ _ this,
+  refine interval_integral.integral_mono_on (by simp) _ _ this,
   apply antitone_on.interval_integrable,
-  rwa set.interval_of_le (calc (a : ℝ) ≤ b : nat.cast_le.mpr hab),
+  rwa interval_of_le (calc x₀ ≤ x₀ + a : by simp),
   apply antitone_on.interval_integrable,
-  rwa set.interval_of_le (calc (a : ℝ) ≤ b : nat.cast_le.mpr hab),
-  intros c hc d hd hcd,
-  exact hf (nat.floor_mem_Icc_of_mem_Icc hc) (nat.floor_mem_Icc_of_mem_Icc hd)
-    (nat.cast_le.mpr $ nat.floor_mono hcd),
+  simp [antitone_const, @antitone.antitone_on],
   conv
-  { to_rhs, congr, skip, funext, rw ← @integral_const_on_unit_interval x (f ↑x), },
-  rw convert_finite_sum_to_interval_integral hab,
+  { to_rhs, congr, skip, funext, rw ← @integral_const_on_unit_interval (x₀ + i) (f (x₀ + i)), },
+  have hint : ∀ (k : ℕ), k < a → interval_integrable (λ (x : ℝ), f (x₀ + k)) real.measure_space.volume ((λ i : ℕ, x₀ + i) k) ((λ i : ℕ, x₀ + i) (k + 1)),
+  { intros k hk,
+    exact interval_integrable_const, },
+  sorry,
 end

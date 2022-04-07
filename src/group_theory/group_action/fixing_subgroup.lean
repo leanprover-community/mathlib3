691885dd160d6fa5d683fd06ba333e9ca72962c6
Antoine Chambert-Loir <antoine.chambert-loir@u-paris.fr>
2022-04-07 08:09:53 +0200

    Update src/group_theory/group_action/fixing_subgroup.lean
    Co-authored-by: tb65536 <tb65536@users.noreply.github.com>

--- src/group_theory/group_action/fixing_subgroup.lean
+++ src/group_theory/group_action/fixing_subgroup.lean
@@ -202,8 +202,7 @@ galois_connection.u_inf (fixing_subgroup_fixed_points_connection M α)
 
 /-- Fixed points of supr of subgroups is intersection -/
 lemma fixed_points_of_group_of_supr {ι : Type*} {P : ι → subgroup M} :
-  fixed_points (supr P : subgroup M) α =
-    infi (λ i, (fixed_points (P i) α)) :=
-  galois_connection.u_infi (fixing_subgroup_fixed_points_connection M α)
+  fixed_points (supr P : subgroup M) α = infi (λ i, (fixed_points (P i) α)) :=
+galois_connection.u_infi (fixing_subgroup_fixed_points_connection M α)
 
 end group

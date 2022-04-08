270163a0b5c33e5e52b0826e130cc36ee85bf5d9
MantasBaksys <baksys.mantas@gmail.com>
2022-04-09 00:08:38 +0100

    golfs

--- src/order/bounds.lean
+++ src/order/bounds.lean
@@ -857,10 +857,7 @@ ball_image_of_ball (assume x H,  Hf Hat (Hst H) (Has H))
 
 lemma image_upper_bounds_subset_upper_bounds_image :
   f '' (upper_bounds s ∩ t) ⊆ upper_bounds (f '' s) :=
-begin
-  rintro _ ⟨a, ha, rfl⟩,
-  exact Hf.mem_upper_bounds_image Hst (mem_of_mem_inter_left ha) (mem_of_mem_inter_right ha)
-end
+by { rintro _ ⟨a, ha, rfl⟩, exact Hf.mem_upper_bounds_image Hst ha.1 ha.2 }
 
 lemma image_lower_bounds_subset_lower_bounds_image :
   f '' (lower_bounds s ∩ t) ⊆ lower_bounds (f '' s) :=
@@ -869,10 +866,7 @@ Hf.dual.image_upper_bounds_subset_upper_bounds_image Hst
 /-- The image under a monotone function on a set `t` of a subset which has an upper bound in `t`
   is bounded above. -/
 lemma map_bdd_above : (upper_bounds s ∩ t).nonempty → bdd_above (f '' s) :=
-begin
-  rintro ⟨C, hC⟩,
-  exact ⟨f C, Hf.mem_upper_bounds_image Hst (mem_of_mem_inter_left hC) (mem_of_mem_inter_right hC)⟩
-end
+λ ⟨C, hs, ht⟩, ⟨f C, Hf.mem_upper_bounds_image Hst hs ht⟩
 
 /-- The image under a monotone function on a set `t` of a subset which has a lower bound in `t`
   is bounded below. -/

1f398aff56db664f73040523443cc1c11352c574
YaelDillies <yael.dillies@gmail.com>
2022-04-09 00:33:12 +0200

    fix import

--- src/order/monovary.lean
+++ src/order/monovary.lean
@@ -3,8 +3,8 @@ Copyright (c) 2021 Yaël Dillies. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Yaël Dillies
 -/
-import order.monotone
 import data.set.basic
+
 /-!
 # Monovariance of functions
 

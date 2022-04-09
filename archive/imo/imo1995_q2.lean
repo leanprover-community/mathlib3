7be50d942f3dc0094f96177bdc6e402e3cbe084f
MantasBaksys <baksys.mantas@gmail.com>
2022-04-09 01:22:08 +0100

    almost done

--- archive/imo/imo1995_q2.lean
+++ archive/imo/imo1995_q2.lean
@@ -12,9 +12,13 @@ import data.fintype.card
 import tactic.fin_cases
 import tactic.norm_fin
 import tactic.ring_exp
+import analysis.mean_inequalities
+import tactic.linarith
 
-variables {a b c : ℝ} (ha : 0 < a) (hb : 0 < b) (hc : 0 < c)
-include ha hb hc
+open real
+
+variables {a b c : ℝ} (ha : 0 < a) (hb : 0 < b) (hc : 0 < c) (habc : a * b * c = 1)
+include ha hb hc habc
 
 lemma preparation : a ≤ b → b ≤ c → 3 / 2 ≤ a ^ 2 / (b + c) + b ^ 2 / (c + a) + c ^ 2 / (b + a) :=
 begin
@@ -44,14 +48,33 @@ begin
   iterate 2 { rw [finset.sum_insert _, finset.sum_insert, finset.sum_singleton] },
   { norm_fin,
     field_simp [hf, hg],
-    calc 3 = 3 * a ^ (1 / 3) * b ^ (1 / 3) * c ^ (1 / 3) : by sorry
-    ...  ≤ 3 * (a / 3 + b / 3 + c / 3) : by sorry
-    ... = a + b + c : by sorry
-    ... = (a + b) ^ 2 / (a + b) + (b + c) ^ 2 / (b + c) + (c + a) ^ 2 / (c + a)
     suffices : 3 ≤ (c ^ 2 + a ^ 2) / (c + a) +
       (b ^ 2 + a ^ 2) / (b + a) + (b ^ 2 + c ^ 2) / (b + c),
     { convert this using 1,
       ring_exp_eq },
-    },
+    calc 3 = 3 * (a ^ (1 / 3 : ℝ) * b ^ (1 / 3 : ℝ) * c ^ (1 / 3 : ℝ)) :
+      by { rw [← real.mul_rpow, ← real.mul_rpow, habc, one_rpow]; linarith [mul_pos ha hb] }
+    ...  ≤ 3 * (1/ 3 * a + 1 / 3 * b + 1 / 3 * c) :
+      by { rw [(mul_le_mul_left (show (0 : ℝ) < 3, by norm_num)), div_eq_mul_inv],
+           convert (geom_mean_le_arith_mean3_weighted _ _ _ _ _ _ _) using 1,
+           swap 7, iterate 4 { norm_num },
+           all_goals { linarith }}
+    ... = a + b + c : by field_simp [mul_comm]
+    ... = 1 / 2 * ((a + b) ^ 2 / (a + b) + (b + c) ^ 2 / (b + c) + (c + a) ^ 2 / (c + a)) :
+      by { field_simp [pow_two, mul_two], abel }
+    ... ≤ (a ^ 2 + b ^ 2) / (a + b) + (b ^ 2 + c ^ 2) / (b + c) + (c ^ 2 + a ^ 2) / (c + a) :
+      by { simp only [mul_add, ← mul_div_assoc],
+           apply add_le_add (add_le_add _ _),
+           { rw [div_le_div_right, one_div, inv_mul_le_iff, add_sq, mul_add];
+             linarith [two_mul_le_add_sq c a] },
+           swap 5,
+           { rw [div_le_div_right, one_div, inv_mul_le_iff, add_sq, mul_add];
+             linarith [two_mul_le_add_sq a b] },
+           swap 5,
+           { rw [div_le_div_right, one_div, inv_mul_le_iff, add_sq, mul_add];
+             linarith [two_mul_le_add_sq b c] },
+           all_goals { apply_instance } }
+    ... = (c ^ 2 + a ^ 2) / (c + a) + (b ^ 2 + a ^ 2) / (b + a) + (b ^ 2 + c ^ 2) / (b + c) :
+      by { rw [add_comm a b], ring_exp_eq } },
   all_goals { simp, norm_num }
 end

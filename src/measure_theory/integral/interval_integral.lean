d6fedc10f8f9cb3415daa07d7a8ffa15a79527d0
Eric Rodriguez <ericrboidi@gmail.com>
2022-04-09 10:31:15 +0100

    fix

--- src/measure_theory/integral/interval_integral.lean
+++ src/measure_theory/integral/interval_integral.lean
@@ -233,7 +233,7 @@ h.symm
 @[refl] lemma refl : interval_integrable f μ a a :=
 by split; simp
 
-@[trans] lemma trans {a b c : α} (hab : interval_integrable f μ a b)
+@[trans] lemma trans {a b c : ℝ} (hab : interval_integrable f μ a b)
   (hbc : interval_integrable f μ b c) : interval_integrable f μ a c :=
 ⟨(hab.1.union hbc.1).mono_set Ioc_subset_Ioc_union_Ioc,
   (hbc.2.union hab.2).mono_set Ioc_subset_Ioc_union_Ioc⟩

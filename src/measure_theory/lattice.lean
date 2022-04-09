a98a26b2d0fae2a7c2f284fe177dedfcf7fc24e8
Yury G. Kudryashov <urkud@urkud.name>
2022-04-09 06:37:15 +0000

    chore(measure_theory): move lemmas about `ae_measurable` to a new file (#13246)

--- src/measure_theory/lattice.lean
+++ src/measure_theory/lattice.lean
@@ -4,7 +4,7 @@ Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Rémy Degenne
 -/
 
-import measure_theory.measure.measure_space
+import measure_theory.measure.ae_measurable
 
 /-!
 # Typeclasses for measurability of lattice operations

6c96d72cb4f53bba06baa9e7b5df54b344201742
Joël Riou <joel.riou@universite-paris-saclay.fr>
2022-04-10 10:29:57 +0200

    feat(algebra/big_operators): split products and sums over fin (a+b)

--- src/algebra/big_operators/fin.lean
+++ src/algebra/big_operators/fin.lean
@@ -5,6 +5,7 @@ Authors: Anne Baanen
 -/
 import algebra.big_operators.basic
 import data.fintype.fin
+import data.fintype.card
 /-!
 # Big operators and `fin`
 
@@ -27,5 +28,49 @@ lemma prod_filter_succ_lt {M : Type*} [comm_monoid M] {n : ℕ} (j : fin n) (v :
   ∏ i in univ.filter (λ i, j.succ < i), v i =
     ∏ j in univ.filter (λ i, j < i), v j.succ :=
 by rw [univ_filter_succ_lt, finset.prod_map, rel_embedding.coe_fn_to_embedding, coe_succ_embedding]
+
+@[to_additive]
+lemma prod_congr {M : Type*} [comm_monoid M] {a b : ℕ} (f : fin b → M) (h : a = b) :
+  ∏ (i : fin a), f (fin.cast h i) = ∏ (i : fin b), f i :=
+by { subst h, congr, ext, congr, ext, rw fin.coe_cast, }
+
+@[to_additive]
+lemma prod_split {M : Type*} [comm_monoid M] {a b : ℕ} (f : fin (a+b) → M) :
+  ∏ (i : fin (a+b)), f i =
+  (∏ (i : fin a), f (fin.cast_le le_self_add i)) * ∏ (i : fin b), f (fin.nat_add a i) :=
+begin
+  revert f a,
+  induction b with b hb,
+  { intros a f,
+    simp only [prod_univ_zero, mul_one],
+    congr,
+    ext i,
+    congr,
+    ext,
+    rw fin.coe_cast_le, },
+  { intros a f,
+    have eq : (a+1)+b=(a+b).succ := by simpa only [add_assoc, add_comm 1],
+    rw [fin.prod_univ_succ, ← mul_assoc, ← fin.prod_congr f eq,
+      hb (λ (i : fin((a+1)+b)), f (fin.cast eq i))],
+    congr,
+    { rw fin.prod_univ_cast_succ,
+      congr, },
+    { ext,
+      simp only,
+      congr,
+      ext,
+      simp only [fin.coe_cast, fin.coe_nat_add, fin.coe_succ, add_assoc, add_comm 1], }, }
+end
+
+@[to_additive]
+lemma prod_trunc {M : Type*} [comm_monoid M] {n a b : ℕ}
+  (h : n=a+b) (f : fin (n) → M)
+  (hf : ∀ (j : fin b), f (fin.cast h.symm (fin.nat_add a j)) = 1) :
+  ∏ (i : fin (n)), f i =
+  ∏ (i : fin (a)), f (fin.cast_le (nat.le.intro (h.symm)) i) :=
+begin
+  rw [← fin.prod_congr f h.symm, fin.prod_split, fintype.prod_eq_one _ hf, mul_one],
+  congr,
+end
 
 end fin

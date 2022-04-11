034d4ab44e07099d0d26b80fc54fb0a171cb5950
Violeta Hernández <vi.hdz.p@gmail.com>
2022-04-10 20:41:56 -0500

    Basic fundamental sequences

--- src/set_theory/cofinality.lean
+++ src/set_theory/cofinality.lean
@@ -423,6 +423,23 @@ hf.2.1
 theorem is_fundamental_sequence.blsub_eq : blsub.{u u} o f = a :=
 hf.2.2
 
+theorem is_fundamental_sequence_id_of_cof_eq (h : o.cof.ord = o) :
+  is_fundamental_sequence o o (λ a _, a) :=
+⟨h.ge, λ i j _ _ h, h, blsub_id o⟩
+
+theorem is_fundamental_sequence_zero {f : Π b < (0 : ordinal), ordinal} :
+  is_fundamental_sequence 0 0 f :=
+⟨by rw [cof_zero, ord_zero], λ i j hi, (ordinal.not_lt_zero i hi).elim, blsub_zero rfl f⟩
+
+theorem is_fundamental_sequence_succ : is_fundamental_sequence o.succ 1 (λ _ _, o) :=
+begin
+  refine ⟨_, λ i j hi hj h, _, blsub_const ordinal.one_ne_zero o⟩,
+  { rw [cof_succ, ord_one] },
+  { rw lt_one_iff_zero at hi hj,
+    rw [hi, hj] at h,
+    exact h.false.elim }
+end
+
 include hf
 theorem is_fundamental_sequence.monotone {i j : ordinal} (hi : i < o) (hj : j < o) (hij : i ≤ j) :
   f i hi ≤ f j hj :=

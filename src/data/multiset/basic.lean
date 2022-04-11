7a40aff167cb4f6e84f41747ba6947eac63242d8
Vierkantor <vierkantor@vierkantor.com>
2022-04-11 13:39:42 +0000

    feat(data/multiset): lemmas on intersecting a multiset with `repeat x n`

--- src/data/multiset/basic.lean
+++ src/data/multiset/basic.lean
@@ -1776,6 +1776,23 @@ end
 lemma filter_eq (s : multiset α) (b : α) : s.filter (eq b) = repeat b (count b s) :=
 by simp_rw [←filter_eq', eq_comm]
 
+@[simp] lemma repeat_inter [decidable_eq α] (x : α) (n : ℕ) (s : multiset α) :
+  multiset.repeat x n ∩ s = multiset.repeat x (min n (s.count x)) :=
+begin
+  refine le_antisymm _ _,
+  { simp only [multiset.le_iff_count, multiset.count_inter, multiset.count_repeat],
+    intro a,
+    split_ifs with h,
+    { rw h },
+    { rw [nat.zero_min] } },
+  simp only [multiset.le_inter_iff, ← multiset.le_count_iff_repeat_le, multiset.count_inter,
+    multiset.count_repeat_self],
+end
+
+@[simp] lemma inter_repeat [decidable_eq α] (s : multiset α) (x : α) (n : ℕ) :
+  s ∩ multiset.repeat x n = multiset.repeat x (min (s.count x) n) :=
+by rw [multiset.inter_comm, multiset.repeat_inter, min_comm]
+
 end
 
 section embedding

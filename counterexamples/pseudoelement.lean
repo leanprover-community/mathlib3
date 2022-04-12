2db2134ee6511a5e9ed126a4a7ffbf47a54ff64b
Riccardo Brasca <riccardo.brasca@gmail.com>
2022-04-12 13:46:50 +0200

    typo

--- counterexamples/pseudoelement.lean
+++ counterexamples/pseudoelement.lean
@@ -1,5 +1,5 @@
 /-
-Copyright (c) 2021 Riccardo Brasca. All rights reserved.
+Copyright (c) 2022 Riccardo Brasca. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Riccardo Brasca
 -/

f9cdcfc548b1142a23b3953e6482903fff1be458
Scott Morrison <scott.morrison@gmail.com>
2022-04-13 20:16:45 +1000

    fix

--- src/category_theory/graded_object.lean
+++ src/category_theory/graded_object.lean
@@ -4,6 +4,7 @@ Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Scott Morrison
 -/
 import data.int.basic
+import algebra.group.basic
 import category_theory.pi.basic
 import category_theory.shift
 import category_theory.concrete_category.basic

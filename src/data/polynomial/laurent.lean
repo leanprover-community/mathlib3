5a5d65605a27bb1fafc9c34e6080fe272cff6b88
adomani <adomani@gmail.com>
2022-04-13 03:28:09 +0200

    move

--- src/data/polynomial/laurent.lean
+++ src/data/polynomial/laurent.lean
@@ -29,7 +29,7 @@ def polynomial.to_laurent_polynomial {R : Type*} [semiring R] :
   R[X] →+* R[T;T⁻¹] :=
 begin
   refine ring_hom.comp _ (to_finsupp_iso R).to_ring_hom,
-  exact (add_monoid_algebra.add_monoid_ring_hom_lift R (nat.cast_add_monoid_hom ℤ)),
+  exact (add_monoid_algebra.add_monoid_ring_hom_map R (nat.cast_add_monoid_hom ℤ)),
 end
 
 /--  The `R`-algebra map, taking a polynomial with coefficients in `R` to a Laurent polynomial
@@ -38,7 +38,7 @@ def polynomial.to_laurent_polynomial_alg {R : Type*} [comm_semiring R] :
   R[X] →ₐ[R] R[T;T⁻¹] :=
 begin
   refine alg_hom.comp _ (to_finsupp_iso_alg R).to_alg_hom,
-  exact (add_monoid_algebra.add_monoid_alg_hom_lift R (nat.cast_add_monoid_hom ℤ)),
+  exact (add_monoid_algebra.add_monoid_alg_hom_map R (nat.cast_add_monoid_hom ℤ)),
 end
 
 namespace laurent_polynomial
@@ -55,6 +55,51 @@ def C : R →+* R[T;T⁻¹] :=
   map_zero' := finsupp.single_zero,
   map_add'  := λ x y, finsupp.single_add }
 
+@[elab_as_eliminator] protected lemma induction_on {M : R[T;T⁻¹] → Prop} (p : R[T;T⁻¹])
+  (h_C : ∀a, M (C a))
+  (h_add : ∀{p q}, M p → M q → M (p + q))
+  (h_single : ∀(n : ℕ) (a : R), M (finsupp.single n a) → M (finsupp.single (n+1) a))
+  (h_single_Z : ∀(n : ℕ) (a : R), M (finsupp.single (- n) a) → M (finsupp.single (-n-1) a)) :
+  M p :=
+begin
+  have A : ∀(n:ℤ) (a), M (finsupp.single n a),
+  { assume n a,
+    apply n.induction_on,
+    { exact h_C a },
+    { exact λ m, h_single m a },
+    { exact λ (m : ℕ), h_single_Z m a } },
+  have B : ∀ (s : finset ℤ), M (s.sum (λ (n : ℤ), C (p.to_fun n) * (finsupp.single n 1))),
+  { apply finset.induction,
+    { convert h_C 0, simp only [finset.sum_empty, _root_.map_zero] },
+    { assume n s ns ih, rw finset.sum_insert ns,
+    { apply h_add  _ ih,
+      convert A n (p.to_fun n),
+      rw [C, ring_hom.coe_mk],
+      simp_rw [add_monoid_algebra.single_mul_single, zero_add, mul_one] } } },
+     convert B p.support,
+     rw [C, ring_hom.coe_mk],
+     simp_rw [add_monoid_algebra.single_mul_single, zero_add, mul_one],
+     ext a,
+     suffices : p a = ite (a ∈ p.support) (p.to_fun a) 0,
+     { simpa only [finset.sum_apply', finsupp.single_apply, finset.sum_ite_eq'] },
+     split_ifs with h h,
+     { refl },
+     { exact finsupp.not_mem_support_iff.mp h }
+end
+
+/--  To prove something about Laurent polynomials, it suffices to show that
+* the condition is closed under taking sums, and
+* it holds for monomials.
+-/
+@[elab_as_eliminator] protected lemma induction_on' {M : R[T;T⁻¹] → Prop} (p : R[T;T⁻¹])
+  (h_add : ∀p q, M p → M q → M (p + q))
+  (single : ∀(n : ℤ) (a : R), M (finsupp.single n a)) :
+  M p :=
+begin
+  refine p.induction_on (single 0) h_add _ _;
+  exact λ n f Mf, single _ f
+end
+
 /--  The variable of the ring of Laurent polynomials. -/
 def T' : R[T;T⁻¹] := finsupp.single 1 1
 
@@ -65,7 +110,7 @@ def T (n : ℤ) : R[T;T⁻¹] := finsupp.single n 1
 lemma T_zero : (T 0 : R[T;T⁻¹]) = 1 := rfl
 
 lemma T_add (m n : ℤ) : (T (m + n) : R[T;T⁻¹]) = T m * T n :=
-by {convert add_monoid_algebra.single_mul_single.symm, simp [T] }
+by {convert single_mul_single.symm, simp [T] }
 
 /--  `T_add_rev` is the `simp`-normal form of `T_add`. -/
 @[simp]
@@ -79,15 +124,20 @@ lemma is_regular_T (n : ℤ) : is_regular (T n : R[T;T⁻¹]) :=
 ⟨is_left_regular_of_mul_eq_one (by simp : T (-n) * T n = 1),
   is_right_regular_of_mul_eq_one (by simp : T n * T (-n) = 1)⟩
 
-lemma mul_T_injective (n : ℤ) : function.injective ((*) (T n) : R[T;T⁻¹] → R[T;T⁻¹]) :=
-(is_regular_T n).left
-
-lemma T_mul_injective (n : ℤ) : function.injective (λ f : R[T;T⁻¹], f * (T n)) :=
-(is_regular_T n).right
-
 @[simp]
 lemma T_pow (m : ℤ) (n : ℕ) : (T m ^ n : R[T;T⁻¹]) = T (n * m) :=
-by { convert add_monoid_algebra.single_pow n, simp [T] }
+by { convert single_pow n, simp [T] }
+
+@[simp]
+lemma mul_T_T (m n : ℤ) : (T m * T n : R[T;T⁻¹]) = T (m + n) :=
+(T_add _ _).symm
+
+
+@[simp] lemma add_comp_of_hom_apply (k G : Type*) [semiring k]
+  {H : Type*} [add_zero_class G] [add_zero_class H]
+  (f : G →+ H) (n : G) :
+  (of k H).comp f.to_multiplicative n = finsupp.single (f n) 1 :=
+rfl
 
 -- `by simp [polynomial.to_laurent_polynomial]` proves the lemma.
 -- This lemma locks in the right changes and is what Lean proved directly.
@@ -95,10 +145,11 @@ by { convert add_monoid_algebra.single_pow n, simp [T] }
 @[simp]
 lemma full_lift_monomial (n : ℕ) (r : R) :
   (monomial n r : R[X]).to_laurent_polynomial = finsupp.single n r :=
-by simp only [polynomial.to_laurent_polynomial, alg_equiv.to_alg_hom_eq_coe, alg_hom.coe_comp,
+by {simp only [polynomial.to_laurent_polynomial, alg_equiv.to_alg_hom_eq_coe, alg_hom.coe_comp,
     alg_equiv.coe_alg_hom, function.comp_app, to_finsupp_iso_alg_apply, ring_equiv.to_fun_eq_coe,
-    to_finsupp_iso_apply, to_finsupp_monomial, add_monoid_alg_hom_lift_single,
-    nat.coe_cast_add_monoid_hom, int.nat_cast_eq_coe_nat]
+    to_finsupp_iso_apply, to_finsupp_monomial, add_monoid_alg_hom_map_single,
+    nat.coe_cast_add_monoid_hom, int.nat_cast_eq_coe_nat],
+    sorry }
 
 @[simp]
 lemma full_lift_one_zero : (finsupp.single 0 1 : R[T;T⁻¹]) = (1 : R[T;T⁻¹]) := rfl
@@ -132,11 +183,35 @@ lemma single_eq_C_mul_T (r : R) (n : ℤ) :
   (finsupp.single n r : R[T;T⁻¹]) = (C r * T n : R[T;T⁻¹]) :=
 by convert add_monoid_algebra.single_mul_single.symm; simp
 
-lemma commute_T (n : ℤ) (f : R[T;T⁻¹]) : commute (T n) f :=
+lemma commute_T_single (n m : ℤ) (r : R) : commute (T n) (finsupp.single m r) :=
 begin
-  exact commute.all (T n) f,
+  show T n * _ = _,
+  unfold T,
+  rw [single_mul_single, single_mul_single],
+  simp [add_comm],
 end
 
+/-
+lemma commute_T (n : ℤ) (f : R[T;T⁻¹]) : commute (T n) f :=
+begin
+  generalize' hf : f.support = fs,
+  revert f,
+  apply fs.induction_on,
+  { simp },
+  intros a s as hs f fas,
+
+  rw erase_add
+  simp,
+  apply f.support.induction_on,
+  show T n * f = f * T n,
+
+
+  unfold T,
+
+  convert monoid_algebra.single_one_comm n (_ : monoid_algebra R ℤ),
+
+--  exact commute.all (T n) f,
+end
 
 @[simp]
 lemma T_neg_one_mul (f : R[T;T⁻¹]) : T (- 1) * f = f * T (- 1) :=
@@ -156,10 +231,7 @@ begin
 
   squeeze_simp {single_pass := tt},
 end
-
-@[simp]
-lemma mul_T_T (m n : ℤ) : (T m * T n : R[T;T⁻¹]) = T (m + n) :=
-(T_add _ _).symm
+-/
 
 
 
@@ -172,52 +244,6 @@ instance : module R[X] R[T;T⁻¹] :=
   add_smul := λ f g x, by simp only [add_mul, _root_.map_add],
   zero_smul := λ f, by simp only [_root_.map_zero, zero_mul] }
 
-@[elab_as_eliminator] protected lemma induction_on {M : R[T;T⁻¹] → Prop} (p : R[T;T⁻¹])
-  (h_C : ∀a, M (C a))
-  (h_add : ∀{p q}, M p → M q → M (p + q))
-  (h_monomial : ∀(n : ℕ) (a : R), M (finsupp.single n a) → M (finsupp.single (n+1) a))
-  (h_monomial_Z : ∀(n : ℕ) (a : R), M (finsupp.single (- n) a) → M (finsupp.single (-n-1) a)) :
-  M p :=
-begin
-  have A : ∀(n:ℤ) (a), M (finsupp.single n a),
-  { assume n a,
-    apply n.induction_on,
-    { exact h_C a },
-    { exact λ m, h_monomial m a },
-    { exact λ (m : ℕ), h_monomial_Z m a } },
-  have B : ∀ (s : finset ℤ), M (s.sum (λ (n : ℤ), C (p.to_fun n) * (finsupp.single n 1))),
-  { apply finset.induction,
-    { convert h_C 0, simp only [finset.sum_empty, _root_.map_zero] },
-    { assume n s ns ih, rw finset.sum_insert ns,
-    { apply h_add  _ ih,
-      convert A n (p.to_fun n),
-      rw [C, ring_hom.coe_mk],
-      simp_rw [add_monoid_algebra.single_mul_single, zero_add, mul_one] } } },
-     convert B p.support,
-     rw [C, ring_hom.coe_mk],
-     simp_rw [add_monoid_algebra.single_mul_single, zero_add, mul_one],
-     ext a,
-     suffices : p a = ite (a ∈ p.support) (p.to_fun a) 0,
-     { simpa only [finset.sum_apply', finsupp.single_apply, finset.sum_ite_eq'] },
-     split_ifs with h h,
-     { refl },
-     { exact finsupp.not_mem_support_iff.mp h }
-end
-
-
-/--
-To prove something about Laurent polynomials,
-it suffices to show the condition is closed under taking sums,
-and it holds for monomials.
--/
-@[elab_as_eliminator] protected lemma induction_on' {M : R[T;T⁻¹] → Prop} (p : R[T;T⁻¹])
-  (h_add : ∀p q, M p → M q → M (p + q))
-  (h_monomial : ∀(n : ℤ) (a : R), M (finsupp.single n a)) :
-  M p :=
-begin
-  refine p.induction_on (h_monomial 0) h_add _ _;
-  exact λ n f Mf, h_monomial _ f
-end
 
 instance : algebra R[X] R[T;T⁻¹] :=
 { commutes' := λ f l, by simp [mul_comm],

0dec8fe6d30e1cec35e9fe1ba2a3861c3cbed927
Junyan Xu <junyanxumath@gmail.com>
2022-04-13 23:45:32 -0400

    golfs

--- src/ring_theory/valuation/valuation_subring.lean
+++ src/ring_theory/valuation/valuation_subring.lean
@@ -30,11 +30,7 @@ variables {K} (A : valuation_subring K)
 
 instance : set_like (valuation_subring K) K :=
 { coe := λ A, A.to_subring,
-  coe_injective' := begin
-    intros A B h,
-    cases A, cases B, congr, apply set_like.ext,
-    exact set.ext_iff.mp h,
-  end }
+  coe_injective' := λ A B h, by { obtain ⟨⟨⟩⟩ := A, obtain ⟨⟨⟩⟩ := B, congr, exact h } }
 
 @[simp] lemma mem_carrier (x : K) : x ∈ A.carrier ↔ x ∈ A := iff.refl _
 @[simp] lemma mem_to_subring (x : K) : x ∈ A.to_subring ↔ x ∈ A := iff.refl _
@@ -70,12 +66,12 @@ instance : valuation_ring A :=
 begin
   constructor,
   intros a b,
-  by_cases (b : K) = 0, { use 0, left, rw mul_zero, exact_mod_cast h.symm },
-  by_cases (a : K) = 0, { use 0, right, rw mul_zero, exact_mod_cast h.symm },
+  by_cases (b : K) = 0, { use 0, left, ext, simp [h] },
+  by_cases (a : K) = 0, { use 0, right, ext, simp [h] },
   cases A.mem_or_inv_mem (a/b) with hh hh,
-  { use ⟨a/b,hh⟩, right, apply subtype.ext, field_simp, ring },
-  { rw (show ((a : K)/b)⁻¹ = b/a, by field_simp) at hh,
-    use ⟨b/a,hh⟩, left, apply subtype.ext, field_simp, ring }
+  { use ⟨a/b,hh⟩, right, ext, field_simp, ring },
+  { rw (show (a/b : K)⁻¹ = b/a, by field_simp) at hh,
+    use ⟨b/a,hh⟩, left, ext, field_simp, ring },
 end
 
 instance : algebra A K :=
@@ -86,30 +82,16 @@ lemma algebra_map_apply (a : A) : algebra_map A K a = a := rfl
 
 instance : is_fraction_ring A K :=
 { map_units := λ ⟨y,hy⟩,
-    (units.mk0 (y : K)
-      (λ c, non_zero_divisors.ne_zero hy (by exact_mod_cast c))).is_unit,
-  surj := begin
-    intros z,
+    (units.mk0 (y : K) (λ c, non_zero_divisors.ne_zero hy $ subtype.ext c)).is_unit,
+  surj := λ z, begin
     by_cases z = 0, { use (0,1), simp [h] },
     cases A.mem_or_inv_mem z with hh hh,
     { use (⟨z,hh⟩,1), simp },
-    { refine ⟨⟨1,⟨⟨_,hh⟩,_⟩⟩,_⟩,
-      { rw mem_non_zero_divisors_iff_ne_zero,
-        intro c, apply h,
-        exact inv_eq_zero.mp (congr_arg coe c) },
-      { dsimp, exact mul_inv_cancel h } }
+    { refine ⟨⟨1,⟨⟨_,hh⟩,_⟩⟩, mul_inv_cancel h⟩,
+      exact mem_non_zero_divisors_iff_ne_zero.2 (λ c, h (inv_eq_zero.mp (congr_arg coe c))) },
   end,
-  eq_iff_exists := begin
-    intros a b,
-    dsimp,
-    split,
-    { intro h, use 1, simp only [submonoid.coe_one, mul_one], exact_mod_cast h },
-    { rintro ⟨c,h⟩,
-      simp only [mul_eq_mul_right_iff] at h,
-      cases h,
-      { exact_mod_cast h },
-      { exact false.elim (non_zero_divisors.ne_zero c.2 h) } },
-  end }
+  eq_iff_exists := λ a b, ⟨ λ h, ⟨1, by { ext, simpa using h }⟩, λ ⟨c,h⟩,
+    congr_arg coe ((mul_eq_mul_right_iff.1 h).resolve_right (non_zero_divisors.ne_zero c.2)) ⟩ }
 
 /-- The value group of the valuation associated to `A`. -/
 @[derive linear_ordered_comm_group_with_zero]
@@ -119,29 +101,16 @@ def value_group := valuation_ring.value_group A K
 def valuation : valuation K A.value_group := valuation_ring.valuation A K
 
 lemma valuation_le_one (a : A) : A.valuation a ≤ 1 :=
-begin
-  change (a : K) ∈ A.valuation.integer,
-  erw valuation_ring.mem_integer_iff,
-  use a, refl,
-end
+(valuation_ring.mem_integer_iff A K _).2 ⟨a,rfl⟩
 
-lemma mem_of_valuation_le_one (x : K) : A.valuation x ≤ 1 → x ∈ A :=
-begin
-  rintro (h : x ∈ A.valuation.integer),
-  erw valuation_ring.mem_integer_iff at h,
-  obtain ⟨a,ha⟩ := h,
-  rw ← ha, exact a.2,
-end
+lemma mem_of_valuation_le_one (x : K) (h : A.valuation x ≤ 1) : x ∈ A :=
+let ⟨a,ha⟩ := (valuation_ring.mem_integer_iff A K x).1 h in ha ▸ a.2
 
 lemma valuation_le_one_iff (x : K) : A.valuation x ≤ 1 ↔ x ∈ A :=
 ⟨mem_of_valuation_le_one _ _, λ ha, A.valuation_le_one ⟨x,ha⟩⟩
 
 lemma valuation_eq_iff (x y : K) : A.valuation x = A.valuation y ↔
-  ∃ a : Aˣ, (a : K) * y = x :=
-begin
-  change quotient.mk' _ = quotient.mk' _ ↔ _,
-  rw quotient.eq', apply iff.refl,
-end
+  ∃ a : Aˣ, (a : K) * y = x := quotient.eq'
 
 lemma valuation_le_iff (x y : K) : A.valuation x ≤ A.valuation y ↔
   ∃ a : A, (a : K) * y = x := iff.rfl
@@ -149,25 +118,17 @@ lemma valuation_le_iff (x y : K) : A.valuation x ≤ A.valuation y ↔
 lemma valuation_surjective : function.surjective A.valuation := surjective_quot_mk _
 
 lemma valuation_unit (a : Aˣ) : A.valuation a = 1 :=
-begin
-  rw [← A.valuation.map_one, valuation_eq_iff],
-  use a, simp,
-end
+by { rw [← A.valuation.map_one, valuation_eq_iff], use a, simp }
 
 lemma valuation_eq_one_iff (a : A) : is_unit a ↔ A.valuation a = 1 :=
-begin
-  split,
-  { intros h, exact A.valuation_unit h.unit, },
-  { intros h,
+⟨ λ h, A.valuation_unit h.unit,
+  λ h, begin
     have ha : (a : K) ≠ 0,
     { intro c, rw [c, A.valuation.map_zero] at h, exact zero_ne_one h },
     have ha' : (a : K)⁻¹ ∈ A,
     { rw [← valuation_le_one_iff, A.valuation.map_inv, h, inv_one] },
-    use [a, a⁻¹, ha'],
-    { ext, field_simp },
-    { ext, field_simp },
-    { ext, refl } }
-end
+    exact is_unit_of_mul_eq_one a ⟨a⁻¹, ha'⟩ (by { ext, field_simp }),
+  end ⟩
 
 lemma valuation_lt_one_or_eq_one (a : A) : A.valuation a < 1 ∨ A.valuation a = 1 :=
 lt_or_eq_of_le (A.valuation_le_one a)
@@ -176,11 +137,7 @@ lemma valuation_lt_one_iff (a : A) : a ∈ local_ring.maximal_ideal A ↔ A.valu
 begin
   rw local_ring.mem_maximal_ideal,
   dsimp [nonunits], rw valuation_eq_one_iff,
-  cases A.valuation_lt_one_or_eq_one a; split,
-  { intro _, exact h },
-  { intro _, exact ne_of_lt h },
-  { contradiction },
-  { intros hh c, exact ne_of_lt hh c }
+  exact (A.valuation_le_one a).lt_iff_ne.symm,
 end
 
 /-- A subring `R` of `K` such that for all `x : K` either `x ∈ R` or `x⁻¹ ∈ R` is
@@ -194,11 +151,7 @@ lemma mem_of_subring (R : subring K) (hR : ∀ x : K, x ∈ R ∨ x⁻¹ ∈ R) 
 
 def of_le (R : valuation_subring K) (S : subring K) (h : R.to_subring ≤ S) :
   valuation_subring K :=
-{ mem_or_inv_mem' := begin
-    intros x, cases R.mem_or_inv_mem x with hx hx,
-    { left, exact h hx, },
-    { right, exact h hx, },
-  end, ..S}
+{ mem_or_inv_mem' := λ x, (R.mem_or_inv_mem x).imp (@h x) (@h _), ..S}
 
 section order
 
@@ -218,11 +171,7 @@ subring.subtype R.to_subring
 
 def map_of_le (R S : valuation_subring K) (h : R ≤ S) :
   R.value_group →*₀ S.value_group :=
-{ to_fun := quotient.map' id begin
-    rintros x y ⟨u,hu : u • _ = _⟩,
-    use units.map (R.inclusion S h).to_monoid_hom u,
-    exact hu
-  end,
+{ to_fun := quotient.map' id $ λ x y ⟨u,hu⟩, ⟨units.map (R.inclusion S h).to_monoid_hom u, hu⟩,
   map_zero' := rfl,
   map_one' := rfl,
   map_mul' := by { rintro ⟨⟩ ⟨⟩, refl } }
@@ -230,11 +179,7 @@ def map_of_le (R S : valuation_subring K) (h : R ≤ S) :
 @[mono]
 lemma monotone_map_of_le (R S : valuation_subring K) (h : R ≤ S) :
   monotone (R.map_of_le S h) :=
-begin
-  rintros ⟨x⟩ ⟨y⟩ ⟨a,ha⟩,
-  use [R.inclusion S h a, ha],
-  all_goals { refl }
-end
+by { rintros ⟨x⟩ ⟨y⟩ ⟨a,ha⟩, exact ⟨R.inclusion S h a, ha⟩ }
 
 @[simp]
 lemma map_of_le_comp_valuation (R S : valuation_subring K) (h : R ≤ S) :
@@ -246,13 +191,8 @@ lemma map_of_le_valuation_apply (R S : valuation_subring K) (h : R ≤ S) (x : K
 
 def ideal_of_le (R S : valuation_subring K) (h : R ≤ S) : ideal R :=
 { carrier := { r | S.valuation r < 1 },
-  add_mem' := begin
-    rintros a b (ha : S.valuation _ < _) (hb : S.valuation _ < _ ),
-    have hu := S.valuation.map_add a b,
-    refine lt_of_le_of_lt hu _,
-    exact max_lt ha hb,
-  end,
-  zero_mem' := by { dsimp, rw S.valuation.map_zero, exact zero_lt_one₀ },
+  add_mem' := λ a b ha hb, lt_of_le_of_lt (S.valuation.map_add a b) (max_lt ha hb),
+  zero_mem' := zero_lt_one₀,
   smul_mem' := begin
     rintros c a (ha : S.valuation _ < _), rw smul_eq_mul,
     let t : S := ⟨c, h c.2⟩,
@@ -281,44 +221,38 @@ begin
     { rwa [this, one_mul] at hh, } }
 end
 
-def of_prime (A : valuation_subring K) (P : ideal A) [hP : P.is_prime] :
+def of_prime (A : valuation_subring K) (P : ideal A) [P.is_prime] :
   valuation_subring K :=
 of_le A (localization.subring K P.prime_compl $
-  le_non_zero_divisors_of_no_zero_divisors $ not_not_intro P.zero_mem)
-begin
-  rintros a ha,
-  refine ⟨⟨a, ha⟩, 1, P.prime_compl.one_mem, by simp⟩,
-end
+  le_non_zero_divisors_of_no_zero_divisors $ not_not_intro P.zero_mem) $
+λ a ha, ⟨⟨a, ha⟩, 1, P.prime_compl.one_mem, by simp⟩
 
-instance of_prime_algebra (A : valuation_subring K) (P : ideal A) [hP : P.is_prime] :
+instance of_prime_algebra (A : valuation_subring K) (P : ideal A) [P.is_prime] :
   algebra A (A.of_prime P) :=
 show algebra A (localization.subring _ P.prime_compl
   (le_non_zero_divisors_of_no_zero_divisors $ not_not_intro P.zero_mem)),
 by apply_instance
 
-instance of_prime_scalar_tower (A : valuation_subring K) (P : ideal A) [hP : P.is_prime] :
+instance of_prime_scalar_tower (A : valuation_subring K) (P : ideal A) [P.is_prime] :
   is_scalar_tower A (A.of_prime P) K :=
 show is_scalar_tower A (localization.subring _ P.prime_compl
   (le_non_zero_divisors_of_no_zero_divisors $ not_not_intro P.zero_mem)) K,
 by apply_instance
 
-instance of_prime_localization (A : valuation_subring K) (P : ideal A) [hP : P.is_prime] :
+instance of_prime_localization (A : valuation_subring K) (P : ideal A) [P.is_prime] :
   is_localization.at_prime (A.of_prime P) P :=
 show is_localization P.prime_compl
   (localization.subring _ P.prime_compl
   (le_non_zero_divisors_of_no_zero_divisors $ not_not_intro P.zero_mem)),
   by apply_instance
 
-lemma le_of_prime (A : valuation_subring K) (P : ideal A) [hP : P.is_prime] :
+lemma le_of_prime (A : valuation_subring K) (P : ideal A) [P.is_prime] :
   A ≤ of_prime A P :=
-begin
-  intros a ha,
-  refine ⟨⟨a, ha⟩, 1, P.prime_compl.one_mem, by simp⟩,
-end
+λ a ha, ⟨⟨a, ha⟩, 1, P.prime_compl.one_mem, by simp⟩
 
 lemma of_prime_valuation_eq_one_iff_mem_prime_compl
   (A : valuation_subring K)
-  (P : ideal A) [hP : P.is_prime] (x : A) :
+  (P : ideal A) [P.is_prime] (x : A) :
   (of_prime A P).valuation x = 1 ↔ x ∈ P.prime_compl :=
 begin
   rw ← is_localization.at_prime.is_unit_to_map_iff (A.of_prime P) P x,
@@ -328,7 +262,7 @@ begin
 end
 
 @[simp]
-lemma ideal_of_le_of_prime (A : valuation_subring K) (P : ideal A) [hP : P.is_prime] :
+lemma ideal_of_le_of_prime (A : valuation_subring K) (P : ideal A) [P.is_prime] :
   ideal_of_le A (of_prime A P) (le_of_prime A P) = P :=
 begin
   ext a,
@@ -356,8 +290,7 @@ lemma of_prime_ideal_of_le (R S : valuation_subring K) (h : R ≤ S) :
 begin
   ext x,
   split,
-  { intro h,
-    obtain ⟨a,r,hr,rfl⟩ := h,
+  { rintro ⟨a,r,hr,rfl⟩,
     change _ ∉ _ at hr, dsimp [ideal_of_le] at hr,
     let s : S := R.inclusion S h r,
     change ¬ S.valuation s < 1 at hr,
@@ -394,7 +327,7 @@ lemma of_prime_le_of_le (P Q : ideal A) [P.is_prime] [Q.is_prime]
   (h : P ≤ Q) : of_prime A Q ≤ of_prime A P :=
 begin
   rintros x ⟨a,s,hs,rfl⟩,
-  refine ⟨a, s, λ c, hs (h c), rfl⟩,
+  exact ⟨a, s, λ c, hs (h c), rfl⟩,
 end
 
 lemma ideal_of_le_le_of_le (R S : valuation_subring K)

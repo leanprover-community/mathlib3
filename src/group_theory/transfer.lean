2d9c1d49d52f072f8df23f78a7c85f867c60b09a
Thomas Browning <tb65536@uw.edu>
2022-04-14 07:19:31 -0700

    lint

--- src/group_theory/transfer.lean
+++ src/group_theory/transfer.lean
@@ -27,7 +27,7 @@ namespace subgroup
 
 namespace left_transversals
 
-open mul_action
+open finset mul_action
 
 open_locale pointwise
 
@@ -40,8 +40,8 @@ let α := mem_left_transversals.to_equiv S.2, β := mem_left_transversals.to_equ
 ∏ q, ϕ ⟨(α q)⁻¹ * β q, quotient.exact' ((α.symm_apply_apply q).trans (β.symm_apply_apply q).symm)⟩
 
 @[to_additive] lemma diff_mul_diff : diff ϕ R S * diff ϕ S T = diff ϕ R T :=
-finset.prod_mul_distrib.symm.trans (finset.prod_congr rfl (λ q hq, (ϕ.map_mul _ _).symm.trans
-  (congr_arg ϕ (subtype.ext (by simp_rw [coe_mul, coe_mk, mul_assoc, mul_inv_cancel_left])))))
+prod_mul_distrib.symm.trans (prod_congr rfl (λ q hq, (ϕ.map_mul _ _).symm.trans
+  (congr_arg ϕ (by simp_rw [subtype.ext_iff, coe_mul, coe_mk, mul_assoc, mul_inv_cancel_left]))))
 
 @[to_additive] lemma diff_self : diff ϕ T T = 1 :=
 mul_right_eq_self.mp (diff_mul_diff ϕ T T T)
@@ -50,9 +50,9 @@ mul_right_eq_self.mp (diff_mul_diff ϕ T T T)
 inv_eq_of_mul_eq_one ((diff_mul_diff ϕ S T S).trans (diff_self ϕ S))
 
 @[to_additive] lemma smul_diff_smul (g : G) : diff ϕ (g • S) (g • T) = diff ϕ S T :=
-finset.prod_bij' (λ q _, g⁻¹ • q) (λ _ _, finset.mem_univ _) (λ _ _, congr_arg ϕ $ subtype.ext $ by
-  simp_rw [coe_mk, smul_apply_eq_smul_apply_inv_smul, smul_eq_mul, mul_inv_rev, mul_assoc, inv_mul_cancel_left])
-  (λ q _, g • q) (λ _ _, finset.mem_univ _) (λ q _, smul_inv_smul g q) (λ q _, inv_smul_smul g q)
+prod_bij' (λ q _, g⁻¹ • q) (λ _ _, mem_univ _) (λ _ _, congr_arg ϕ (by simp_rw [coe_mk,
+  smul_apply_eq_smul_apply_inv_smul, smul_eq_mul, mul_inv_rev, mul_assoc, inv_mul_cancel_left]))
+  (λ q _, g • q) (λ _ _, mem_univ _) (λ q _, smul_inv_smul g q) (λ q _, inv_smul_smul g q)
 
 end left_transversals
 

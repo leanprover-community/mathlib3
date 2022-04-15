f563be071bc6ec1969b4644a8cf973e5d10b3acf
yuma-mizuno <mizuno.y.aj@gmail.com>
2022-04-15 18:17:33 +0900

    update docs for `is_local_ring_hom`

--- src/ring_theory/ideal/local_ring.lean
+++ src/ring_theory/ideal/local_ring.lean
@@ -161,9 +161,9 @@ end local_ring
 
 end comm_ring
 
-/-- A local ring homomorphism is a homomorphism between local rings
-  such that the image of the maximal ideal of the source is contained within
-  the maximal ideal of the target. -/
+/-- A local ring homomorphism is a homomorphism `f` between local rings such that `a` in the domain
+  is a unit if `f a` is a unit for any `a`. See `local_ring.local_hom_tfae` for other equivalent
+  definitions. -/
 class is_local_ring_hom [semiring R] [semiring S] (f : R →+* S) : Prop :=
 (map_nonunit : ∀ a, is_unit (f a) → is_unit a)
 
@@ -246,6 +246,9 @@ section
 open local_ring
 variables [comm_semiring R] [local_ring R] [comm_semiring S] [local_ring S]
 
+/--
+The image of the maximal ideal of the source is contained within the maximal ideal of the target.
+-/
 lemma map_nonunit (f : R →+* S) [is_local_ring_hom f] (a : R) (h : a ∈ maximal_ideal R) :
   f a ∈ maximal_ideal S :=
 λ H, h $ is_unit_of_map_unit f a H

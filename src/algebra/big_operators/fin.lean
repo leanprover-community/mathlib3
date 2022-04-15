e269fc7e45ca76266243b7e57cd67dbcfe3ab672
Joël Riou <joel.riou@universite-paris-saclay.fr>
2022-04-15 10:16:44 +0200

    updated copyright year

--- src/algebra/big_operators/fin.lean
+++ src/algebra/big_operators/fin.lean
@@ -1,5 +1,5 @@
 /-
-Copyright (c) 2021 Mario Carneiro, Anne Baanen. All rights reserved.
+Copyright (c) 2022 Mario Carneiro, Anne Baanen. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Mario Carneiro, Anne Baanen
 -/

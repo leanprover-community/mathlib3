4ad7c5bf8fd99566b537025104709774158fb57e
Floris van Doorn <fpvdoorn@gmail.com>
2022-04-15 16:55:14 +0200

    author

--- src/analysis/calculus/specific_functions.lean
+++ src/analysis/calculus/specific_functions.lean
@@ -1,7 +1,7 @@
 /-
 Copyright (c) 2020 Sébastien Gouëzel. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
-Authors: Sébastien Gouëzel
+Authors: Sébastien Gouëzel, Floris van Doorn
 -/
 import analysis.calculus.iterated_deriv
 import analysis.inner_product_space.euclidean_dist

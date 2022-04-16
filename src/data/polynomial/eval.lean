fd3af2c2d0fbdedc85ebbe9325e6021a664b2548
negiizhao <egresf@gmail.com>
2022-04-16 22:12:54 +0800

    Update eval.lean

--- src/data/polynomial/eval.lean
+++ src/data/polynomial/eval.lean
@@ -307,7 +307,7 @@ eval₂_monomial _ _
 @[simp] lemma eval_bit1 : (bit1 p).eval x = bit1 (p.eval x) := eval₂_bit1 _ _
 
 @[simp] lemma eval_smul [monoid S] [distrib_mul_action S R] [is_scalar_tower S R R]
-  (p : R[X]) (x : R) {s : S} :
+  (s : S) (p : R[X]) (x : R) :
   (s • p).eval x = s • p.eval x :=
 by rw [← smul_one_smul R s p, eval, eval₂_smul, ring_hom.id_apply, smul_one_mul]
 
@@ -471,7 +471,7 @@ by simp only [bit0, add_comp]
 by simp only [bit1, add_comp, bit0_comp, one_comp]
 
 @[simp] lemma smul_comp [monoid S] [distrib_mul_action S R] [is_scalar_tower S R R]
-  (s : S) : (s • p).comp q = s • p.comp q :=
+  (s : S) (p q : R[X]) : (s • p).comp q = s • p.comp q :=
 by rw [← smul_one_smul R s p, comp, comp, eval₂_smul, ← smul_eq_C_mul, smul_assoc, one_smul]
 
 lemma comp_assoc {R : Type*} [comm_semiring R] (φ ψ χ : R[X]) :

65164245c9b353da8e85d62aeddd347dd4981e22
negiizhao <egresf@gmail.com>
2022-04-16 22:26:36 +0800

    Update taylor.lean

--- src/data/polynomial/taylor.lean
+++ src/data/polynomial/taylor.lean
@@ -4,8 +4,8 @@ Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Johan Commelin
 -/
 
-import data.polynomial.hasse_deriv
 import data.polynomial.algebra_map
+import data.polynomial.hasse_deriv
 
 /-!
 # Taylor expansions of polynomials

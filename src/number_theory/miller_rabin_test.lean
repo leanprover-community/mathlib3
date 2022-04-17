65534e3d35b803fb8d4b2ffa74e9bec0453e9a65
Bolton Bailey <bolton.bailey@gmail.com>
2022-04-16 20:29:56 -0500

    changes

--- src/number_theory/miller_rabin_test.lean
+++ src/number_theory/miller_rabin_test.lean
@@ -10,7 +10,16 @@ def fast_strong_probable_prime (n : nat) (a : zmod n) : Prop :=
 binpow a (odd_part (n-1)) = 1
 ∨ (∃ r : ℕ, r < fast_two_multiplicity (n-1) ∧ binpow a (2^r * odd_part(n-1)) = -1)
 
+def ast_strong_probable_prime (n : nat) (a : zmod n) : Prop :=
+binpow a ( (n-1)) = 1
+
+
 instance {n : ℕ} {a : zmod n} : decidable (fast_strong_probable_prime n a) := or.decidable
+
+instance {n : ℕ} {a : zmod n} : decidable (ast_strong_probable_prime n a) :=
+begin
+  library_search
+end
 
 lemma strong_probable_prime_iff_fast_strong_probable_prime (n : nat) (a : zmod n) :
   strong_probable_prime n a ↔ fast_strong_probable_prime n a :=
@@ -20,7 +29,7 @@ begin
   sorry,
 end
 
-#eval fast_two_multiplicity 10000
+-- #eval fast_two_multiplicity 10000
 
 lemma fast_two_multiplicity_eq_padic_val_nat_two (n : ℕ) :
   padic_val_nat 2 n = fast_two_multiplicity n :=
@@ -30,14 +39,14 @@ end
 
 --TODO(Bolton): Find a way of making modular exponentiation faster
 set_option profiler true
---#eval to_bool (fast_strong_probable_prime 10000019 2)
---#eval to_bool (fast_strong_probable_prime 1000003 2)
+-- #eval to_bool (fast_strong_probable_prime 10000019 2)
+#eval to_bool (fast_strong_probable_prime 1000003 2)
 --#eval to_bool (nat.prime 1000003)
 --#eval to_bool (fast_strong_probable_prime 99999997 4)
 --#eval (multiplicity 2 99999997)
 --#eval to_bool (nat.prime 100123456789)
 
-example : nat.prime 219365121653 :=
-begin
-  norm_num,
-end
+-- example : nat.prime 219365121653 :=
+-- begin
+--   norm_num,
+-- end

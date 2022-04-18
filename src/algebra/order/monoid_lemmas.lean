4d7a8bce5a50cab20cdc88e274926f3eb61553e0
negiizhao <egresf@gmail.com>
2022-04-18 17:00:43 +0800

    fix

--- src/algebra/order/monoid_lemmas.lean
+++ src/algebra/order/monoid_lemmas.lean
@@ -683,9 +683,9 @@ alias left.add_neg'                 ← add_neg'
 
 alias left.one_le_mul               ← one_le_mul
 alias left.add_nonneg               ← add_nonneg
-alias left.one_lt_mul_of_le_of_lt   ← one_lt_mul_of_le_of_lt
+alias left.one_lt_mul_of_le_of_lt   ← one_lt_mul_of_le_of_lt'
 alias left.add_pos_of_nonneg_of_pos ← add_pos_of_nonneg_of_pos
-alias left.one_lt_mul_of_lt_of_le   ← one_lt_mul_of_lt_of_le
+alias left.one_lt_mul_of_lt_of_le   ← one_lt_mul_of_lt_of_le'
 alias left.add_pos_of_pos_of_nonneg ← add_pos_of_pos_of_nonneg
 alias left.one_lt_mul               ← one_lt_mul'
 alias left.add_pos                  ← add_pos

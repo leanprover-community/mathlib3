869004e44009096738537c53f2ec66ca15929e5d
Adam Topaz <github@adamtopaz.com>
2022-04-18 19:25:52 -0600

    Add author

--- src/ring_theory/localization/as_subring.lean
+++ src/ring_theory/localization/as_subring.lean
@@ -1,7 +1,7 @@
 /-
 Copyright (c) 2022 Adam Topaz. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
-Authors: Adam Topaz
+Authors: Adam Topaz, Junyan Xu
 -/
 import ring_theory.localization.localization_localization
 import tactic.field_simp

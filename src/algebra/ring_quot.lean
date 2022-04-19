844f23d8ea659043bfb1b216f75467e1fbd964d0
Jireh Loreaux <loreaujy@gmail.com>
2022-04-19 16:57:38 -0500

    fix with ugly proof

--- src/algebra/ring_quot.lean
+++ src/algebra/ring_quot.lean
@@ -261,7 +261,18 @@ The ring equivalence between `ring_quot r` and `(ideal.of_rel r).quotient`
 def ring_quot_equiv_ideal_quotient (r : B → B → Prop) :
   ring_quot r ≃+* B ⧸ ideal.of_rel r :=
 ring_equiv.of_hom_inv (ring_quot_to_ideal_quotient r) (ideal_quotient_to_ring_quot r)
-  (by { ext, refl, }) (by { ext, refl, })
+  (begin
+    have : (ideal_quotient_to_ring_quot r).comp (ring_quot_to_ideal_quotient r) = ring_hom.id _,
+    { ext, refl },
+    ext,
+    exact fun_like.congr_fun this x,
+  end)
+  (begin
+    have : (ring_quot_to_ideal_quotient r).comp (ideal_quotient_to_ring_quot r) = ring_hom.id _,
+    { ext, refl },
+    ext,
+    exact fun_like.congr_fun this x,
+  end)
 
 end comm_ring
 

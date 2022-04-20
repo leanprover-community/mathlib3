7379c73d56f06a1097cd0060b6bf326818e0d8e3
Eric Wieser <wieser.eric@gmail.com>
2022-04-20 09:30:34 +0000

    feat(analysis/normed): more lemmas about the sup norm on pi types and matrices

--- src/analysis/matrix.lean
+++ src/analysis/matrix.lean
@@ -20,7 +20,7 @@ of a matrix.
 
 noncomputable theory
 
-open_locale nnreal
+open_locale nnreal matrix
 
 namespace matrix
 
@@ -29,7 +29,7 @@ variables {R m n α : Type*} [fintype m] [fintype n]
 section semi_normed_group
 variables [semi_normed_group α]
 
-/-- Seminormed group instance (using sup norm of sup norm) for matrices over a seminormed ring. Not
+/-- Seminormed group instance (using sup norm of sup norm) for matrices over a seminormed group. Not
 declared as an instance because there are several natural choices for defining the norm of a
 matrix. -/
 protected def semi_normed_group : semi_normed_group (matrix m n α) :=
@@ -61,9 +61,40 @@ lemma nnnorm_entry_le_entrywise_sup_nnorm (A : matrix m n α) {i : m} {j : n} :
   ∥A i j∥₊ ≤ ∥A∥₊ :=
 (nnnorm_le_pi_nnnorm (A i) j).trans (nnnorm_le_pi_nnnorm A i)
 
+@[simp] lemma nnnorm_transpose (A : matrix m n α) : ∥Aᵀ∥₊ = ∥A∥₊ :=
+by { simp_rw [pi.nnnorm_def], exact finset.sup_comm _ _ _ }
+@[simp] lemma norm_transpose (A : matrix m n α) : ∥Aᵀ∥ = ∥A∥ := congr_arg coe $ nnnorm_transpose A
+
+@[simp] lemma nnnorm_col (v : m → α) : ∥col v∥₊ = ∥v∥₊ := by simp [pi.nnnorm_def]
+@[simp] lemma norm_col (v : m → α) : ∥col v∥ = ∥v∥ := congr_arg coe $ nnnorm_col v
+
+@[simp] lemma nnnorm_row (v : n → α) : ∥row v∥₊ = ∥v∥₊ := by simp [pi.nnnorm_def]
+@[simp] lemma norm_row (v : n → α) : ∥row v∥ = ∥v∥ := congr_arg coe $ nnnorm_row v
+
+@[simp] lemma nnnorm_diagonal [decidable_eq n] (v : n → α) : ∥diagonal v∥₊ = ∥v∥₊ :=
+begin
+  simp_rw pi.nnnorm_def,
+  congr' 1 with i : 1,
+  refine le_antisymm (finset.sup_le $ λ j hj, _) _,
+  { obtain rfl | hij := eq_or_ne i j,
+    { rw diagonal_apply_eq },
+    { rw [diagonal_apply_ne hij, nnnorm_zero],
+      exact zero_le _ }, },
+  { refine eq.trans_le _ (finset.le_sup (finset.mem_univ i)),
+    rw diagonal_apply_eq }
+end
+
+@[simp] lemma norm_diagonal [decidable_eq n] (v : n → α) : ∥diagonal v∥ = ∥v∥ :=
+congr_arg coe $ nnnorm_diagonal v
+
+/-- Note this is safe as an instance as it carries no data. -/
+instance [nonempty n] [decidable_eq n] [has_one α] [norm_one_class α] :
+  norm_one_class (matrix n n α) :=
+⟨(norm_diagonal _).trans $ norm_one⟩
+
 end semi_normed_group
 
-/-- Normed group instance (using sup norm of sup norm) for matrices over a normed ring.  Not
+/-- Normed group instance (using sup norm of sup norm) for matrices over a normed group.  Not
 declared as an instance because there are several natural choices for defining the norm of a
 matrix. -/
 protected def normed_group [normed_group α] : normed_group (matrix m n α) :=
@@ -74,7 +105,7 @@ local attribute [instance] matrix.semi_normed_group
 
 variables [normed_field R] [semi_normed_group α] [normed_space R α]
 
-/-- Normed space instance (using sup norm of sup norm) for matrices over a normed field.  Not
+/-- Normed space instance (using sup norm of sup norm) for matrices over a normed space.  Not
 declared as an instance because there are several natural choices for defining the norm of a
 matrix. -/
 protected def normed_space : normed_space R (matrix m n α) :=

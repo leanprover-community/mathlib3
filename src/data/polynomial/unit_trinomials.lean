3b8e1bffcf8d5b236e53e8e6cbba31b45df0100b
Thomas Browning <tb65536@uw.edu>
2022-04-19 18:44:23 -0700

    more outline

--- src/data/polynomial/unit_trinomials.lean
+++ src/data/polynomial/unit_trinomials.lean
@@ -120,125 +120,60 @@ begin
         unit_trinomial_nat_trailing_degree hkm hmn hu hv hw,
         unit_trinomial_mirror hkm hmn hu hv hw],
     simp_rw [mul_add, add_mul, coeff_add, mul_assoc, coeff_C_mul, mul_comm, ←mul_assoc, mul_comm,
-      coeff_C_mul, ←pow_add, coeff_X_pow, add_left_inj, add_right_inj],
+      coeff_mul_C, ←pow_add, coeff_X_pow, add_left_inj, add_right_inj],
+    rw [if_neg (hkm.trans hmn).ne, if_neg (hkm.trans hmn).ne'],
     sorry },
   { sorry },
 end
 
-lemma is_unit_trinomial.lem1 (hp : is_unit_trinomial p) (q : ℤ[X])
-  (h : p * mirror p = q * mirror q) :
-  q = p ∨ q = -p ∨ q = mirror p ∨ q = -mirror p :=
+lemma is_unit_trinomial.irreducible1 (hp : is_unit_trinomial p)
+  (h : ∀ q : ℤ[X], q ∣ p → q ∣ mirror p → is_unit q) :
+  irreducible p :=
 begin
-  have hq : is_unit_trinomial q := by rwa [is_unit_trinomial_iff, ←h, ←is_unit_trinomial_iff],
-  sorry,
+  refine irreducible_of_mirror hp.not_is_unit (λ q hq, _) h,
+  -- this is a big result!
+  sorry
 end
 
-lemma is_unit_trinomial.lem2 (hp : is_unit_trinomial p)
-  (q : ℤ[X]) (hq1 : q ∣ p) (hq2 : q ∣ mirror p) : is_unit q :=
-begin
-end
-
-lemma is_unit_trinomial.irreducible_of_coprime (hp : is_unit_trinomial p)
+lemma is_unit_trinomial.irreducible2 (hp : is_unit_trinomial p)
   (h : ∀ z : ℂ, ¬ (aeval z p = 0 ∧ aeval z (mirror p) = 0)) : irreducible p :=
 begin
-  apply irreducible_of_mirror hp.not_is_unit hp.lem1,
-  intros g hg1 hg2,
-  suffices : ¬ (0 < g.nat_degree),
+  refine hp.irreducible1 (λ q hq hq', _),
+  suffices : ¬ (0 < q.nat_degree),
   { rw [not_lt, nat.le_zero_iff] at this,
     rw [eq_C_of_nat_degree_eq_zero this, is_unit_C, ←this],
-    cases hg1 with h fgh,
-    apply @is_unit_of_mul_is_unit_left _ _ g.leading_coeff h.leading_coeff,
+    cases hq with h fgh,
+    apply @is_unit_of_mul_is_unit_left _ _ q.leading_coeff h.leading_coeff,
     rw [←leading_coeff_mul, ←fgh],
-    exact is_unit_of_pow_eq_one _ _ (f.coeff_sq_eq_one_of_norm2_le_three
-      (le_of_eq h1) f.nat_degree (mt leading_coeff_eq_zero.mp hf)) zero_lt_two },
-  intro h,
-  have inj : function.injective (algebra_map ℤ ℂ) := int.cast_injective,
-  rw [nat_degree_pos_iff_degree_pos, ←degree_map' inj] at h,
-  cases complex.exists_root h with z hz,
-  apply h2 z,
+    sorry },
+  intro hq'',
+  have inj : function.injective (algebra_map ℤ ℂ) := (algebra_map ℤ ℂ).injective_int,
+  rw [nat_degree_pos_iff_degree_pos, ←degree_map_eq_of_injective inj] at hq'',
+  cases complex.exists_root hq'' with z hz,
+  apply h z,
   rw [is_root, eval_map, ←aeval_def] at hz,
   split,
-  { cases hg1 with g' hg',
+  { cases hq with g' hg',
     rw [hg', aeval_mul, hz, zero_mul] },
-  { cases hg2 with g' hg',
+  { cases hq' with g' hg',
     rw [hg', aeval_mul, hz, zero_mul] },
 end
 
-lemma is_unit_trinomial.irreducible_of_not_dvd (hp : is_unit_trinomial p)
+lemma is_unit_trinomial.irreducible3 (hp : is_unit_trinomial p)
   (hp1 : ¬ X ∣ p) (hp2 : ¬ X ^ 2 + X + 1 ∣ p) (hp3 : ¬ X ^ 2 - X + 1 ∣ p) : irreducible p :=
 begin
-  refine hp.irreducible_of_coprime (λ z hz, _),
-end
-
-
-namespace unit_trinomials
-
-variables {m n : ℕ} (hm : 0 < m) (hmn : m < n)
-variables {u v w : ℤ} (hu : is_unit u) (hv : is_unit v) (hw : is_unit w)
-
-include hm hmn hu hv hw
-
-lemma nat_degree_eq : nat_degree (u * X ^ n + v * X ^ m + w : ℤ[X]) = n :=
-begin
-  simp only [←ring_hom.eq_int_cast C],
-  rw [add_assoc, nat_degree_add_eq_left_of_nat_degree_lt, nat_degree_C_mul_X_pow n u hu.ne_zero],
-  rw [nat_degree_C_mul_X_pow n u hu.ne_zero],
+  refine hp.irreducible2 (λ z hz, _),
   sorry,
 end
-
-lemma nat_trailing_degree_eq : nat_trailing_degree (u * X ^ n + v * X ^ m + w : ℤ[X]) = 0 :=
-begin
-  simp only [←ring_hom.eq_int_cast C],
-  rw [←le_zero_iff],
-  apply nat_trailing_degree_le_of_ne_zero,
-  sorry,
-end
-
-lemma mirror_eq : mirror (u * X ^ n + v * X ^ m + w : ℤ[X]) = u * X ^ n + v * X ^ (n - m) + w :=
-begin
-  rw [mirror, nat_trailing_degree_eq hm hmn hu hv hw, pow_zero, mul_one, reverse,
-      nat_degree_eq hm hmn hu hv hw, reflect_add, reflect_add],
-  sorry,
-end
-
-lemma not_is_unit : ¬ is_unit (u * X ^ n + v * X ^ m + w : ℤ[X]) :=
-λ h, (hm.trans hmn).ne'
-  (nat_degree_eq hm hmn hu hv hw ▸ nat_degree_eq_of_degree_eq_some (degree_eq_zero_of_is_unit h))
-
-lemma is_irreducible_aux1 (f : ℤ[X])
-  (hf : (u * X ^ n + v * X ^ m + w : ℤ[X]) * mirror (u * X ^ n + v * X ^ m + w) = f * mirror f) :
-  f = u * X ^ n + v * X ^ m + w ∨ f = -(u * X ^ n + v * X ^ m + w) ∨
-  f = mirror (u * X ^ n + v * X ^ m + w) ∨ f = -mirror (u * X ^ n + v * X ^ m + w) :=
-begin
-  sorry
-end
-
-lemma is_irreducible_aux2 (f : ℤ[X]) (hf1 : f ∣ u * X ^ n + v * X ^ m + w)
-  (hf2 : f ∣ mirror (u * X ^ n + v * X ^ m + w)) : is_unit f :=
-begin
-  sorry
-end
-
-lemma is_irreducible : irreducible (u * X ^ n + v * X ^ m + w : ℤ[X]) :=
-begin
-  apply irreducible_of_mirror,
-  { exact not_is_unit hm hmn hu hv hw },
-  { exact is_irreducible_aux1 hm hmn hu hv hw },
-  { exact is_irreducible_aux2 hm hmn hu hv hw },
-end
-
-end unit_trinomials
 
 lemma selmer_irreducible {n : ℕ} (hn : 1 < n) : irreducible (X ^ n - X - 1 : ℤ[X]) :=
 begin
-  --simpa using trinomial_irreducible zero_lt_one hn is_unit_one is_unit_one.neg is_unit_one.neg,
+  let p : ℤ[X] := X ^ n - X - 1,
+  have hp : is_unit_trinomial p,
+  { refine ⟨0, 1, n, zero_lt_one, hn, 1, -1, -1, is_unit_one, is_unit_one.neg, is_unit_one.neg, _⟩,
+    rw [C_neg, neg_mul, neg_mul, C_1, one_mul, one_mul, one_mul, pow_zero, pow_one],
+    sorry },
+  refine hp.irreducible3 _ _ _,
 end
-
-/-
-|
-|
-|
-|
--/
 
 end polynomial

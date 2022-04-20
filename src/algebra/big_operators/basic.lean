e63bf4ae127e66c16ada2d7c1678d0d9d263a8c0
CBirkbeck <c.birkbeck@ucl.ac.uk>
2022-04-20 14:33:47 +0100

    feat(algebra/big_operators/basic): add prod_ite_extract and prod_update_eq

--- src/algebra/big_operators/basic.lean
+++ src/algebra/big_operators/basic.lean
@@ -1269,6 +1269,27 @@ lemma prod_pow_boole [decidable_eq α] (s : finset α) (f : α → β) (a : α) 
   (∏ x in s, (f x)^(ite (a = x) 1 0)) = ite (a ∈ s) (f a) 1 :=
 by simp
 
+@[to_additive]
+lemma prod_ite_eq_extract  [decidable_eq α]
+ (s : finset α) {b : α} (hb : b ∈ s) (f : α → β) :
+  ∏ n in s, f n = f b * ∏ n in s, ite (n = b) 1 (f n) :=
+begin
+  simp only [finset.prod_ite, finset.filter_ne', finset.prod_const_one, one_mul],
+  rw mul_comm,
+  exact (prod_erase_mul _ _ hb).symm,
+end
+
+@[to_additive]
+lemma prod_update_eq [decidable_eq α]
+ (s : finset α) {a : α} (b : β) (ha : a ∈ s)  (f : α → β) :
+  f a * (∏ n in s, (function.update f a b) n)  = b * ∏ n in s, (f n):=
+begin
+  simp only [function.update_apply, finset.prod_ite, finset.filter_ne', finset.filter_eq', ha,
+  if_true, finset.prod_const,finset.card_singleton, pow_one],
+  rw [ mul_comm, mul_assoc, (finset.prod_erase_mul s f ha)],
+end
+
+
 lemma prod_dvd_prod_of_dvd {S : finset α} (g1 g2 : α → β) (h : ∀ a ∈ S, g1 a ∣ g2 a) :
   S.prod g1 ∣ S.prod g2 :=
 begin

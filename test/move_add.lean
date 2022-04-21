e02d685ffc652a484b03abb3987507d9db02330a
adomani <adomani@gmail.com>
2022-04-21 16:09:44 +0200

    adapt tests

--- test/move_add.lean
+++ test/move_add.lean
@@ -11,7 +11,7 @@ example (s0 : s ≠ 0) (m n : ℕ) (mn : m < n) (m1 : 1 ≤ m) :
 begin
   -- move `C s * X ^ n` to the right, since it is the term with highest degree
   -- we also move `m * X` and `37 * X` to the left, to collect them in a single summand
-  move_all [C s * X ^ n, ← ↑m * (X : R[X]), ← 37 * (X : R[X])],
+  move_add [C s * X ^ n, ← ↑m * (X : R[X]), ← 37 * (X : R[X])],
   rw [← add_mul, nat_degree_add_eq_right_of_nat_degree_lt, nat_degree_C_mul_X_pow _ _ s0],
   rw [nat_degree_C_mul_X_pow _ _ s0],
   -- what is left has degree at most `m < n`, so let's inform Lean
@@ -26,36 +26,12 @@ begin
   { exact nat_degree_C_mul_X_pow_le _ _ }
 end
 
-example
-  (hp : (monomial 1) u + (g + (monomial 5) 1) + 5 * X + ((monomial 0) s + (monomial 2) t + f) +
-  (monomial 8) 1 = (3 * X + (monomial 8) 1 + (monomial 6) (t + 1)) + f + h + ((monomial 0) s +
-  (monomial 1) u) + (monomial 5) 1) :
-  (monomial 1) u + 5 * X + (g + (monomial 5) 1) + ((monomial 0) s + (monomial 2) t + f) +
-  (monomial 8) 1 = (3 * X + (monomial 8) 1 + (monomial 6) (t + 1)) + f + h + ((monomial 0) s +
-  (monomial 1) u) + (monomial 5) 1 :=
-begin
---  convert hp using 1, ac_refl, -- works and takes 8s
---  move_all at ⊢ hp, assumption, --takes under 600ms
-  move_all [g, (5 * X : R[X]), g, 3, f] at ⊢ hp,
-  move_all [(5 * X : R[X]), monomial 2 t, monomial 0 s],
-  move_all at ⊢ hp,
-  assumption,
-end
-
 example (hp : f = g) :
   7 + f + (C r * X ^ 3 + 42) + X ^ 5 * h = C r * X ^ 3 + h * X ^ 5 + g + 7 + 42 :=
 begin
   -- we move `f, g` to the right of their respective sides, so `congr` helps us remove the
   -- repeated term
-  move_all [f, g],
-  congr' 2, -- takes care of using assumption `hp`
-  exact X_pow_mul,
-end
-
-example (hp : f = g) :
-  7 + f + (monomial 3 r + 42) + X ^ 5 * h = monomial 3 r + h * X ^ 5 + g + 7 + 42 :=
-begin
-  move_all [f, g],
-  congr' 3, -- takes care of using assumption `hp`
+  move_add [← C r * X ^ 3, (7 : R[X]), (42 : R[X]), f, g],
+  congr' 4, -- takes care of using assumption `hp`
   exact X_pow_mul,
 end

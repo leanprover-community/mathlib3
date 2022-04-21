f89b8b9d927d234dba4ede96218110aad64515a6
Antoine Labelle <antoinelab01@gmail.com>
2022-04-21 11:17:57 -0400

    feat(representation_theory/basic): representation theory without scalar actions

--- src/representation_theory/invariants.lean
+++ src/representation_theory/invariants.lean
@@ -8,21 +8,20 @@ import representation_theory.basic
 /-!
 # Subspace of invariants a group representation
 
-This file introduce the subspace of invariants of a group representation
-and proves basic result about it.
+This file introduces the subspace of invariants of a group representation
+and proves basic results about it.
 The main tool used is the average of all elements of the group, seen as an element of
-`monoid_algebra k G`. Scalar multiplication by this special element gives a projection onto the
+`monoid_algebra k G`. The action of this special element gives a projection onto the
 subspace of invariants.
 In order for the definition of the average element to make sense, we need to assume for most of the
 results that the order of `G` is invertible in `k` (e. g. `k` has characteristic `0`).
 -/
 
 open_locale big_operators
-open monoid_algebra finset finite_dimensional linear_map representation
+open monoid_algebra
+open representation
 
-namespace representation
-
-section average
+namespace group_algebra
 
 variables (k G : Type*) [comm_semiring k] [group G]
 variables [fintype G] [invertible (fintype.card G : k)]
@@ -63,61 +62,52 @@ begin
   rw function.bijective.sum_comp (group.mul_right_bijective g) _,
 end
 
-end average
+end group_algebra
 
-section invariants
+namespace representation
 
-variables (k G V : Type*) [comm_semiring k] [group G] [add_comm_group V]
-variables [module k V] [distrib_mul_action G V] [smul_comm_class G k V]
+open group_algebra
+
+variables {k G V : Type*} [comm_semiring k] [group G] [add_comm_monoid V] [module k V]
+variables (ρ : representation k G V)
 
 /--
 The subspace of invariants, consisting of the vectors fixed by all elements of `G`.
 -/
 def invariants : submodule k V :=
-{ carrier := set_of (λ v, ∀ (g : G), g • v = v),
-  zero_mem' := by simp only [forall_const, set.mem_set_of_eq, smul_zero],
-  add_mem' := λ v w hv hw g, by simp only [smul_add, add_left_inj, eq_self_iff_true, hv g, hw g],
-  smul_mem' := λ r v hv g, by simp only [eq_self_iff_true, smul_comm, hv g]}
+{ carrier := set_of (λ v, ∀ (g : G), ρ g v = v),
+  zero_mem' := λ g, by simp only [map_zero],
+  add_mem' := λ v w hv hw g, by simp only [hv g, hw g, map_add],
+  smul_mem' := λ r v hv g, by simp only [hv g, linear_map.map_smulₛₗ, ring_hom.id_apply]}
 
 @[simp]
-lemma mem_invariants (v : V) : v ∈ (invariants k G V) ↔ ∀ (g: G), g • v = v := by refl
+lemma mem_invariants (v : V) : v ∈ invariants ρ ↔ ∀ (g: G), ρ g v = v := by refl
 
 lemma invariants_eq_inter :
-  (invariants k G V).carrier = ⋂ g : G, function.fixed_points (has_scalar.smul g) :=
-by { ext, simp [function.is_fixed_pt] }
-
-/--
-The subspace of invariants, as a submodule over `monoid_algebra k G`.
--/
-noncomputable def invariants' : submodule (monoid_algebra k G) V :=
-  submodule_of_smul_mem (invariants k G V) (λ g v hv, by {rw [of_smul, hv g], exact hv})
-
-@[simp] lemma invariants'_carrier :
-  (invariants' k G V).carrier = (invariants k G V).carrier := rfl
+  (invariants ρ).carrier = ⋂ g : G, function.fixed_points (ρ g) :=
+by {ext, simp [function.is_fixed_pt]}
 
 variables [fintype G] [invertible (fintype.card G : k)]
 
 /--
-Scalar multiplication by `average k G` sends elements of `V` to the subspace of invariants.
+The action of `average k G` gives a projection map onto the subspace of invariants.
 -/
-theorem smul_average_invariant (v : V) : (average k G) • v ∈ invariants k G V :=
-λ g, by rw [←of_smul k, smul_smul, of_apply, mul_average_left]
+@[simp]
+noncomputable def average_map : V →ₗ[k] V := as_algebra_hom ρ (average k G)
 
 /--
-`average k G` acts as the identity on the subspace of invariants.
+The `average_map` sends elements of `V` to the subspace of invariants.
 -/
-theorem smul_average_id (v : V) (H : v ∈ invariants k G V) : (average k G) • v = v :=
+theorem average_map_invariant (v : V) : average_map ρ v ∈ invariants ρ :=
+λ g, by rw [average_map, ←as_algebra_hom_single, ←linear_map.mul_apply, ←map_mul (as_algebra_hom ρ), mul_average_left]
+
+/--
+The `average_map` acts as the identity on the subspace of invariants.
+-/
+theorem average_map_id (v : V) (hv : v ∈ invariants ρ) : average_map ρ v = v :=
 begin
-  rw [representation.mem_invariants] at H,
-  simp_rw [average_def, smul_assoc, finset.sum_smul, representation.of_smul, H, finset.sum_const,
-    finset.card_univ, nsmul_eq_smul_cast k _ v, smul_smul, inv_of_mul_self, one_smul],
+  rw mem_invariants at hv,
+  simp [average_def, map_sum, hv, finset.card_univ, nsmul_eq_smul_cast k _ v, smul_smul],
 end
-
-/--
-Scalar multiplication by `average k G` gives a projection map onto the subspace of invariants.
--/
-noncomputable def average_map : V →ₗ[k] V := (as_algebra_hom k G V) (average k G)
-
-end invariants
 
 end representation

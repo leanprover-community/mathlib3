8235324a71cc7c1f7b387a115edad7ecda3df3eb
Eric Wieser <wieser.eric@gmail.com>
2022-04-22 22:20:07 +0100

    Update src/topology/uniform_space/matrix.lean
    Co-authored-by: Oliver Nash <github@olivernash.org>

--- src/topology/uniform_space/matrix.lean
+++ src/topology/uniform_space/matrix.lean
@@ -1,5 +1,5 @@
 /-
-Copyright (c) 2019 Eric Wieser. All rights reserved.
+Copyright (c) 2022 Eric Wieser. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Eric Wieser, Heather Macbeth
 -/

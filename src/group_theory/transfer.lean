9b977233834d537c16a6a630b7c431e7bf0b2f51
Thomas Browning <tb65536@uw.edu>
2022-04-23 09:52:57 -0700

    a couple approaches

--- src/group_theory/transfer.lean
+++ src/group_theory/transfer.lean
@@ -4,6 +4,7 @@ Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Thomas Browning
 -/
 
+import data.zmod.basic
 import group_theory.complement
 import group_theory.group_action.basic
 import group_theory.index
@@ -18,6 +19,37 @@ In this file we construct the transfer homomorphism.
 - `diff ϕ S T` : The difference of two left transversals `S` and `T` under the homomorphism `ϕ`.
 - `transfer ϕ` : The transfer homomorphism induced by `ϕ`.
 -/
+
+section temp
+
+noncomputable def orbit_zpowers_equiv
+  {α β : Type*} [group α] (a : α) [mul_action α β] (b : β) :
+  mul_action.orbit (subgroup.zpowers a) b ≃ zmod (function.minimal_period ((•) a) b) :=
+equiv.symm (equiv.of_bijective (λ k, (⟨a, subgroup.mem_zpowers a⟩ : subgroup.zpowers a) ^ k.val •
+  ⟨b, mul_action.mem_orbit_self b⟩) begin
+  let a' : subgroup.zpowers a := ⟨a, subgroup.mem_zpowers a⟩,
+  let b' : mul_action.orbit α b := ⟨b, mul_action.mem_orbit_self b⟩,
+  have key : function.minimal_period ((•) a) b = function.minimal_period ((•) a') b',
+  { sorry },
+  -- ^ this stuff might not be necessary ^ --
+  split,
+  { intros k k' h,
+    replace h : a ^ k.val • b = a ^ k'.val • b := subtype.ext_iff.mp h,
+    sorry },
+  { rintros ⟨-, ⟨-, k, rfl⟩, rfl⟩,
+    refine ⟨k, subtype.ext _⟩,
+    change a ^ (k : zmod _).val • b = a ^ k • b,
+    sorry },
+end)
+
+lemma orbit_zpowers_equiv_symm_apply {α β : Type*} [group α] (a : α) [mul_action α β] (b : β)
+  (k : zmod (function.minimal_period ((•) a) b)) :
+  (orbit_zpowers_equiv a b).symm k =
+  (⟨a, subgroup.mem_zpowers a⟩ : subgroup.zpowers a) ^ k.val • ⟨b, mul_action.mem_orbit_self b⟩ :=
+rfl
+
+
+end temp
 
 open_locale big_operators
 
@@ -82,6 +114,86 @@ by rw [transfer, ←diff_mul_diff, ←smul_diff_smul, mul_comm, diff_mul_diff]; 
 section explicit_computation
 
 variables (H)
+
+open_locale classical
+
+noncomputable def key_equiv (g : G) :
+  G ⧸ H ≃ Σ (q : quotient (mul_action.orbit_rel (zpowers g) (G ⧸ H))),
+  fin (fintype.card (mul_action.orbit (zpowers g) q.out')) :=
+(mul_action.self_equiv_sigma_orbits (zpowers g) (G ⧸ H)).trans
+  (equiv.sigma_congr_right (λ q, equiv.symm (equiv.of_bijective
+  (λ k, ((⟨g, mem_zpowers g⟩ : zpowers g) ^ k.1) • ⟨q.out', mul_action.mem_orbit_self q.out'⟩)
+begin
+  split,
+  { intros k k' h,
+    replace h := subtype.ext_iff.mp h,
+    simp only at h,
+    sorry },
+  { sorry }
+end)))
+
+lemma key_equiv_symm_apply (g : G) (q : quotient (mul_action.orbit_rel (zpowers g) (G ⧸ H)))
+  (k : fin (fintype.card (mul_action.orbit (zpowers g) q.out'))) :
+  (key_equiv H g).symm ⟨q, k⟩ = g ^ k.1 • q.out' :=
+rfl
+
+def key_transversal (g : G) : left_transversals (H : set G) :=
+⟨set.range (λ q, g ^ (key_equiv H g q).2.1 * (key_equiv H g q).1.out'.out'),
+  range_mem_left_transversals (λ q, begin
+  refine eq.trans _ ((key_equiv H g).symm_apply_apply q),
+  exact _root_.congr_arg ((•) (g ^ (key_equiv H g q).2.1))
+    (quotient.out_eq' (key_equiv H g q).1.out'),
+end)⟩
+
+lemma key_transversal_apply (g : G) (q : quotient (mul_action.orbit_rel (zpowers g) (G ⧸ H)))
+  (k : fin (fintype.card (mul_action.orbit (zpowers g) q.out'))) :
+  ↑(mem_left_transversals.to_equiv (key_transversal H g).2 ((key_equiv H g).symm ⟨q, k⟩)) =
+    g ^ k.1 * q.out'.out' :=
+begin
+  change _ = ↑(⟨g ^ k.1 * q.out'.out', begin
+    refine ⟨(key_equiv H g).symm ⟨q, k⟩, _⟩,
+    have key := (key_equiv H g).apply_symm_apply ⟨q, k⟩,
+    simp only [(key_equiv H g).apply_symm_apply ⟨q, k⟩],
+    congr,
+    simp only [(key_equiv H g).apply_symm_apply ⟨q, k⟩],
+    sorry,
+  end⟩ :
+    set.range (λ q, g ^ (key_equiv H g q).2.1 * (key_equiv H g q).1.out'.out')),
+  refine subtype.ext_iff.mp _,
+  rw equiv.apply_eq_iff_eq_symm_apply,
+  sorry
+end
+
+lemma key_transversal_apply' (g : G) (q : quotient (mul_action.orbit_rel (zpowers g) (G ⧸ H)))
+  (k : fin (fintype.card (mul_action.orbit (zpowers g) q.out'))) :
+  ↑(mem_left_transversals.to_equiv (g • key_transversal H g).2 ((key_equiv H g).symm ⟨q, k⟩)) =
+    g ^ k.1 * q.out'.out' :=
+begin
+  sorry
+end
+
+lemma transfer_computation (g : G) : transfer ϕ g =
+  ∏ (q : quotient (mul_action.orbit_rel (zpowers g) (G ⧸ H))), 1 :=
+begin
+  calc transfer ϕ g = ∏ (q : G ⧸ H), _ : transfer_def ϕ (key_transversal H g) g
+  ... = _ : ((key_equiv H g).symm.prod_comp _).symm
+  ... = _ : finset.prod_sigma _ _ _
+  ... = _ : fintype.prod_congr _ _ (λ q, _),
+  simp only [key_transversal_apply, key_transversal_apply'],
+end
+
+end explicit_computation
+
+section explicit_computation
+
+lemma transfer_eq_pow (hH : H ≤ center G) (g : G) : transfer ϕ g = ϕ ⟨g ^ H.index, sorry⟩ :=
+begin
+
+  sorry,
+end
+
+
+
 
 lemma orbit_powers_eq_orbit_zpowers {α : Type*} [group α] (a : α) {β : Type*} [fintype β]
   [mul_action α β] (b : β) : mul_action.orbit (submonoid.powers a) b = mul_action.orbit (zpowers a) b :=

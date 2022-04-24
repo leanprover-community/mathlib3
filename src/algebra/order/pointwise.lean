10fc087e13c4cc1046c75c59c4c0888308d8b9f6
YaelDillies <yael.dillies@gmail.com>
2022-04-24 14:34:03 +0200

    complete lattice versions

--- src/algebra/order/pointwise.lean
+++ src/algebra/order/pointwise.lean
@@ -22,15 +22,28 @@ open_locale pointwise
 variables {α : Type*}
 
 section has_one
-variables [conditionally_complete_lattice α] [has_one α]
+variables [has_one α]
+
+section conditionally_complete_lattice
+variables [conditionally_complete_lattice α]
 
 @[simp, to_additive] lemma cSup_one : Sup (1 : set α) = 1 := cSup_singleton _
 @[simp, to_additive] lemma cInf_one : Inf (1 : set α) = 1 := cInf_singleton _
 
+end conditionally_complete_lattice
+
+variables [complete_lattice α]
+
+@[simp, to_additive] lemma Sup_one : Sup (1 : set α) = 1 := Sup_singleton
+@[simp, to_additive] lemma Inf_one : Inf (1 : set α) = 1 := Inf_singleton
+
 end has_one
 
 section group
-variables [conditionally_complete_lattice α] [group α] [covariant_class α α (*) (≤)]
+variables [group α]
+
+section conditionally_complete_lattice
+variables [conditionally_complete_lattice α] [covariant_class α α (*) (≤)]
   [covariant_class α α (swap (*)) (≤)] {s : set α}
 
 @[to_additive] lemma cSup_inv (hs₀ : s.nonempty) (hs₁ : bdd_below s) : Sup s⁻¹ = (Inf s)⁻¹ :=
@@ -39,11 +52,23 @@ by { rw ←image_inv, exact ((order_iso.inv α).map_cInf' hs₀ hs₁).symm }
 @[to_additive] lemma cInf_inv (hs₀ : s.nonempty) (hs₁ : bdd_above s) : Inf s⁻¹ = (Sup s)⁻¹ :=
 by { rw ←image_inv, exact ((order_iso.inv α).map_cSup' hs₀ hs₁).symm }
 
+end conditionally_complete_lattice
+
+variables [complete_lattice α] [covariant_class α α (*) (≤)] [covariant_class α α (swap (*)) (≤)]
+
+@[to_additive] lemma Sup_inv (s : set α) : Sup s⁻¹ = (Inf s)⁻¹ :=
+by { rw [←image_inv, Sup_image], exact ((order_iso.inv α).map_Inf _).symm }
+
+@[to_additive] lemma Inf_inv (s : set α) : Inf s⁻¹ = (Sup s)⁻¹ :=
+by { rw [←image_inv, Inf_image], exact ((order_iso.inv α).map_Sup _).symm }
+
 end group
 
 section comm_group
-variables [conditionally_complete_linear_order α] [comm_group α] [covariant_class α α (*) (≤)]
-  {s t : set α}
+variables [comm_group α]
+
+section conditionally_complete_linear_order
+variables [conditionally_complete_linear_order α] [covariant_class α α (*) (≤)] {s t : set α}
 
 @[to_additive] lemma cSup_mul (hs₀ : s.nonempty) (hs₁ : bdd_above s) (ht₀ : t.nonempty)
   (ht₁ : bdd_above t) :
@@ -78,6 +103,36 @@ by rw [div_eq_mul_inv, cSup_mul hs₀ hs₁ ht₀.inv ht₁.inv, cSup_inv ht₀ 
   (ht₁ : bdd_above t) :
   Inf (s / t) = Inf s / Sup t :=
 by rw [div_eq_mul_inv, cInf_mul hs₀ hs₁ ht₀.inv ht₁.inv, cInf_inv ht₀ ht₁, div_eq_mul_inv]
+
+end conditionally_complete_linear_order
+
+variables [complete_linear_order α] [covariant_class α α (*) (≤)]
+
+@[to_additive] lemma Sup_mul (s t : set α) : Sup (s * t) = Sup s * Sup t :=
+begin
+  refine Sup_eq_of_forall_le_of_forall_lt_exists_gt _ (λ a ha, _),
+  { rintro f ⟨a, b, ha, hb, rfl⟩,
+    exact mul_le_mul' (le_Sup ha) (le_Sup hb) },
+  { obtain ⟨b, hb, hab⟩ := lt_Sup_iff.1 (div_lt_iff_lt_mul.2 ha),
+    obtain ⟨c, hc, hbc⟩ := lt_Sup_iff.1 (div_lt''.1 hab),
+    exact ⟨b * c, mul_mem_mul hb hc, div_lt_iff_lt_mul'.1 hbc⟩ }
+end
+
+@[to_additive] lemma Inf_mul (s t : set α) : Inf (s * t) = Inf s * Inf t :=
+begin
+  refine Inf_eq_of_forall_ge_of_forall_gt_exists_lt _ (λ a ha, _),
+  { rintro f ⟨a, b, ha, hb, rfl⟩,
+    exact mul_le_mul' (Inf_le ha) (Inf_le hb) },
+  { obtain ⟨b, hb, hab⟩ := Inf_lt_iff.1 (lt_div_iff_mul_lt.2 ha),
+    obtain ⟨c, hc, hbc⟩ := Inf_lt_iff.1 (lt_div''.1 hab),
+    exact ⟨b * c, mul_mem_mul hb hc, lt_div_iff_mul_lt'.1 hbc⟩ }
+end
+
+@[to_additive] lemma Sup_div (s t : set α) : Sup (s / t) = Sup s / Inf t :=
+by simp_rw [div_eq_mul_inv, Sup_mul, Sup_inv]
+
+@[to_additive] lemma Inf_div (s t : set α) : Inf (s / t) = Inf s / Sup t :=
+by simp_rw [div_eq_mul_inv, Inf_mul, Inf_inv]
 
 end comm_group
 

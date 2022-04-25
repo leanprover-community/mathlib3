32a60f4fee2aefef5a278563e4f1cad6c549f045
adomani <adomani@gmail.com>
2022-04-25 14:05:41 +0200

    todo

--- test/move_add.lean
+++ test/move_add.lean
@@ -4,16 +4,63 @@ import data.polynomial.degree
 open polynomial tactic interactive
 open_locale polynomial
 
-example {a b c : ℕ} : a + b + a + b + c = b + a + b + a + c :=
+example {R : Type*} [add_comm_semigroup R] {a b c d : R} (h : a + b + c = d) : b + (a + c) = d :=
 begin
-  move_add [a, ← a],
+  move_add,
+  move_add at *,
+  move_add [a] at *,
+  move_add at a,
+  move_add at h ⊢,
+  move_add at h ⊢,
+  move_add [b] at h,
 end
 
-example {a b c d : ℕ} (h : c = d) : c + b + a = b + a + d :=
+
+example {a b c d : ℕ} (h : a + b + a + b + c = b + a + b + a + c) : 2 + 1 = 0 :=
 begin
-  move_add [← a, b],  -- Goal: `a + c + b = a + d + b`
+  move_add [1, 2],
+  move_add [d] at h,
+  move_add [a, a] at h,
+  move_add [d, a, a] at h,
+  move_add [a, ← a, d] at *,
+end
+
+example {a b c d e : ℕ} (h : c = d) : c + b + a = b + a + d :=
+begin
+  move_add [← a, b, ← a, ← a, c],  -- Goal: `a + c + b = a + d + b`
+  move_add [_ * d, a, c, e] at *,  -- Goal: `a + c + b = a + d + b`
+  move_add [d, a, c, e] at *,  -- Goal: `a + c + b = a + d + b`
+  move_add [← d] at *,  -- Goal: `a + c + b = a + d + b`
+  move_add [d, d],  -- Goal: `a + c + b = a + d + b`
+  move_add [← a],  -- Goal: `a + c + b = a + d + b`
+
   congr,
   exact h
+end
+
+example {R : Type*} [add_comm_semigroup R] {a b : R} (h : a + b = b) : true :=
+begin
+  move_add [← b] at h,
+  move_add [a, ← b] at h,
+end
+
+example {a b c d : ℕ} (h : c = d) (k : c + b * c + a * c = a * d + d + b * d) :
+  c + b * c + a * c = a * d + d + b * d + 1 :=
+begin
+  move_add [← c, _ * _] at k,
+  move_add [c] at k,
+  move_add [c] at k,
+  move_add [c] at k h ⊢,
+  move_add [← c] at k h ⊢,
+  move_add ← 1 at *,
+  -- the first `_ * c` unifies with `b * c` and moves it to the right
+  -- the second `_ * c` unifies with `a * c` and moves it to the left
+  move_add [_ * c, ← _ * c], -- Goal: `a * c + c + b * c = a * d + d + b * d`
+  --  creates two side-goals of `ℕ`, one for each `_`, even though they have been unified
+  --  correctly and disappear with the `assumption`s.
+  --  It is possible to not have them at all?
+  congr,
+  repeat { assumption }
 end
 
 example {a b c d : ℕ} (h : c = d) : c + b * c + a * c = a * d + d + b * d :=

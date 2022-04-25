8f3053ab87c507cb5739579f4a732e11ebc7dda5
Violeta Hernández <vi.hdz.p@gmail.com>
2022-04-25 17:37:00 -0500

    rename

--- src/set_theory/game/pgame.lean
+++ src/set_theory/game/pgame.lean
@@ -155,8 +155,8 @@ x.rec_on $ λ yl yr yL yR, IH (mk yl yr yL yR)
 
 /-- `is_option x y` means that `x` is either a left or right option for `y`. -/
 @[mk_iff] inductive is_option : pgame → pgame → Prop
-| left {x : pgame} {i} : is_option (x.move_left i) x
-| right {x : pgame} {i} : is_option (x.move_right i) x
+| move_left {x : pgame} {i} : is_option (x.move_left i) x
+| move_right {x : pgame} {i} : is_option (x.move_right i) x
 
 theorem wf_is_option : well_founded is_option :=
 ⟨λ x, x.move_rec_on begin

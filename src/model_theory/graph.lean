dae22e98f8c422dfa94e9afeb004fe1f7a624fac
Aaron Anderson <awainverse@gmail.com>
2022-04-26 08:07:55 -0700

    import

--- src/model_theory/graph.lean
+++ src/model_theory/graph.lean
@@ -4,7 +4,7 @@ Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Aaron Anderson
 -/
 import model_theory.satisfiability
-import combinatorics.simple_graph.coloring
+import combinatorics.simple_graph.basic
 
 /-!
 # First-Ordered Structures in Graph Theory

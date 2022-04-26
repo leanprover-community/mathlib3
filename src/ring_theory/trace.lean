42b9479830e6844216370e10422aa52eabd885fe
Eric Wieser <wieser.eric@gmail.com>
2022-04-26 12:29:51 +0000

    fix

--- src/ring_theory/trace.lean
+++ src/ring_theory/trace.lean
@@ -106,7 +106,7 @@ lemma trace_algebra_map_of_basis (x : R) :
   trace R S (algebra_map R S x) = fintype.card ι • x :=
 begin
   haveI := classical.dec_eq ι,
-  rw [trace_apply, linear_map.trace_eq_matrix_trace R b, trace_diag],
+  rw [trace_apply, linear_map.trace_eq_matrix_trace R b, matrix.trace],
   convert finset.sum_const _,
   ext i,
   simp,
@@ -133,10 +133,10 @@ begin
   haveI := classical.dec_eq ι,
   haveI := classical.dec_eq κ,
   rw [trace_eq_matrix_trace (b.smul c), trace_eq_matrix_trace b, trace_eq_matrix_trace c,
-      matrix.trace_apply, matrix.trace_apply, matrix.trace_apply,
+      matrix.trace, matrix.trace, matrix.trace,
       ← finset.univ_product_univ, finset.sum_product],
   refine finset.sum_congr rfl (λ i _, _),
-  simp only [alg_hom.map_sum, smul_left_mul_matrix, finset.sum_apply,
+  simp only [alg_hom.map_sum, smul_left_mul_matrix, finset.sum_apply, matrix.diag_sum,
       -- The unifier is not smart enough to apply this one by itself:
       finset.sum_apply i _ (λ y, left_mul_matrix b (left_mul_matrix c x y y))]
 end

3efb6a328669265fab795e7be9b399be4650eaca
Joachim Breitner <mail@joachim-breitner.de>
2022-04-27 12:14:13 +0100

    Name tweak again

--- src/group_theory/is_free_group.lean
+++ src/group_theory/is_free_group.lean
@@ -70,7 +70,7 @@ free_group.lift.trans
 @[simp] lemma lift_of (f : generators G → H) (a : generators G) : lift f (of a) = f a :=
 congr_fun (lift.symm_apply_apply f) a
 
-@[simp] lemma lift_symm_of (f : G →* H) (a : generators G) : (lift.symm f) a = f (of a) :=
+@[simp] lemma lift_symm_apply (f : G →* H) (a : generators G) : (lift.symm f) a = f (of a) :=
 rfl
 
 @[ext] lemma ext_hom ⦃f g : G →* H⦄ (h : ∀ (a : generators G), f (of a) = g (of a)) : f = g :=

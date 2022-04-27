975a40413111ffd4681c6e9153485d87d550f953
Antoine Labelle <antoinelab01@gmail.com>
2022-04-26 20:03:53 -0400

    feat(linear_algebra/trace): trace of tensor_products and hom_tensor_hom is an equivalence

--- src/linear_algebra/contraction.lean
+++ src/linear_algebra/contraction.lean
@@ -25,11 +25,14 @@ section contraction
 open tensor_product linear_map matrix
 open_locale tensor_product big_operators
 
-variables (R M N : Type*) [add_comm_group M] [add_comm_group N]
+local attribute [ext] tensor_product.ext
+
+variables (R M N P Q : Type*) [add_comm_group M]
+variables [add_comm_group N] [add_comm_group P] [add_comm_group Q]
 
 section comm_ring
 
-variables [comm_ring R] [module R M] [module R N]
+variables [comm_ring R] [module R M] [module R N] [module R P] [module R Q]
 variables {ι : Type*} [decidable_eq ι] [fintype ι] (b : basis ι R M)
 
 /-- The natural left-handed pairing between a module and its dual. -/
@@ -44,7 +47,7 @@ def dual_tensor_hom : (module.dual R M) ⊗ N →ₗ[R] M →ₗ[R] N :=
   let M' := module.dual R M in
   (uncurry R M' N (M →ₗ[R] N) : _ → M' ⊗ N →ₗ[R] M →ₗ[R] N) linear_map.smul_rightₗ
 
-variables {R M N}
+variables {R M N P Q}
 
 @[simp] lemma contract_left_apply (f : module.dual R M) (m : M) :
   contract_left R M (f ⊗ₜ m) = f m := by apply uncurry_apply
@@ -55,6 +58,14 @@ variables {R M N}
 @[simp] lemma dual_tensor_hom_apply (f : module.dual R M) (m : M) (n : N) :
   dual_tensor_hom R M N (f ⊗ₜ n) m = (f m) • n :=
 by { dunfold dual_tensor_hom, rw uncurry_apply, refl, }
+
+lemma map_dual_tensor_hom (f : module.dual R M) (p : P) (g : module.dual R N) (q : Q) :
+  tensor_product.map (dual_tensor_hom R M P (f ⊗ₜ[R] p)) (dual_tensor_hom R N Q (g ⊗ₜ[R] q)) =
+  dual_tensor_hom R (M ⊗[R] N) (P ⊗[R] Q) (dual_tensor_dual_map (f ⊗ₜ g) ⊗ₜ[R] (p ⊗ₜ[R] q)) :=
+begin
+  ext m n, simp only [compr₂_apply, mk_apply, map_tmul, dual_tensor_hom_apply,
+  dual_tensor_dual_map_apply, mul_smul_tmul],
+end
 
 /-- As a matrix, `dual_tensor_hom` evaluated on a basis element of `M* ⊗ N` is a matrix with a
 single one and zeros elsewhere -/
@@ -69,13 +80,10 @@ begin
   rw [and_iff_not_or_not, not_not] at hij, cases hij; simp [hij],
 end
 
-local attribute [ext] tensor_product.ext
-
 /-- If `M` is free, the natural linear map $M^* ⊗ N → Hom(M, N)$ is an equivalence. This function
 provides this equivalence in return for a basis of `M`. -/
 @[simps]
-noncomputable def dual_tensor_hom_equiv_of_basis
-  {ι : Type*} [decidable_eq ι] [fintype ι] (b : basis ι R M) :
+noncomputable def dual_tensor_hom_equiv_of_basis :
   (module.dual R M) ⊗[R] N ≃ₗ[R] M →ₗ[R] N :=
 linear_equiv.of_linear
   (dual_tensor_hom R M N)
@@ -98,6 +106,8 @@ end)
   dual_tensor_hom R M N :=
 rfl
 
+variables (R M N P Q)
+
 variables [module.free R M] [module.finite R M] [nontrivial R]
 
 open_locale classical
@@ -106,6 +116,52 @@ open_locale classical
 equivalence. -/
 @[simp] noncomputable def dual_tensor_hom_equiv : (module.dual R M) ⊗[R] N ≃ₗ[R] M →ₗ[R] N :=
 dual_tensor_hom_equiv_of_basis (module.free.choose_basis R M)
+
+variables [module.free R N] [module.finite R N]
+
+open module
+
+variables [module.finite R (M ⊗[R] N)] --Temporary until this is made an instance by #13705
+
+/--
+When `M` and `N` are free `R` modules, the map `hom_tensor_hom_map` is an equivalence. Note that
+`hom_tensor_hom_equiv` is not defined directly in terms of `hom_tensor_hom_map`, but the equivalence
+between the two is given by `hom_tensor_hom_equiv_to_linear_map` and `hom_tensor_hom_equiv_apply`.
+-/
+noncomputable
+def hom_tensor_hom_equiv : (M →ₗ[R] P) ⊗[R] (N →ₗ[R] Q) ≃ₗ[R] (M ⊗[R] N →ₗ[R] P ⊗[R] Q) :=
+  congr (dual_tensor_hom_equiv R M P).symm (dual_tensor_hom_equiv R N Q).symm ≪≫ₗ
+  tensor_tensor_tensor_comm R (dual R M) P (dual R N) Q ≪≫ₗ
+  tensor_product.congr dual_tensor_dual_equiv (linear_equiv.refl R (P ⊗ Q)) ≪≫ₗ
+  dual_tensor_hom_equiv R (M ⊗ N) (P ⊗ Q)
+
+@[simp]
+lemma hom_tensor_hom_equiv_to_linear_map :
+  (hom_tensor_hom_equiv R M N P Q).to_linear_map = hom_tensor_hom_map :=
+begin
+  refine (linear_map.cancel_right
+  (congr (dual_tensor_hom_equiv R M P) (dual_tensor_hom_equiv R N Q)).surjective).1 _,
+  show (hom_tensor_hom_equiv R M N P Q).to_linear_map ∘ₗ
+    (congr (dual_tensor_hom_equiv R M P) (dual_tensor_hom_equiv R N Q)).to_linear_map =
+    hom_tensor_hom_map ∘ₗ
+    (congr (dual_tensor_hom_equiv R M P) (dual_tensor_hom_equiv R N Q)).to_linear_map,
+  ext f p g q m n,
+  simp only [dual_tensor_hom_equiv, compr₂_apply, mk_apply, coe_comp,
+  linear_equiv.coe_to_linear_map, function.comp_app, congr_tmul, hom_tensor_hom_map_apply,
+  map_tmul, dual_tensor_hom_apply, tmul_smul, hom_tensor_hom_equiv, dual_tensor_hom_equiv,
+  dual_tensor_dual_equiv, linear_equiv.trans_apply, congr_tmul, linear_equiv.symm_apply_apply,
+  tensor_tensor_tensor_comm_tmul, linear_equiv.refl_apply],
+  simp only [dual_tensor_hom_equiv_of_basis_apply, dual_tensor_hom_apply,
+  dual_tensor_dual_equiv_of_basis_apply_apply, hom_tensor_hom_map_apply, map_tmul, lid_tmul,
+  algebra.id.smul_eq_mul, mul_smul_tmul],
+end
+
+variables {R M N P Q}
+
+@[simp]
+lemma hom_tensor_hom_equiv_apply (x : (M →ₗ[R] P) ⊗[R] (N →ₗ[R] Q)) :
+  hom_tensor_hom_equiv R M N P Q x = hom_tensor_hom_map x :=
+by rw [←linear_equiv.coe_to_linear_map, hom_tensor_hom_equiv_to_linear_map]
 
 end comm_ring
 
--- src/linear_algebra/trace.lean
+++ src/linear_algebra/trace.lean
@@ -32,6 +32,8 @@ open_locale big_operators
 open_locale matrix
 open finite_dimensional
 
+open_locale tensor_product
+
 section
 variables (R : Type u) [comm_ring R] {M : Type v} [add_comm_group M] [module R M]
 variables {ι : Type w} [decidable_eq ι] [fintype ι]
@@ -113,8 +115,9 @@ end
 
 section
 
-variables (R : Type u) [comm_ring R] {M : Type v} [add_comm_group M] [module R M]
-variables {ι : Type w} [fintype ι]
+variables {R : Type*} [comm_ring R] {M : Type*} [add_comm_group M] [module R M]
+variables (N : Type*) [add_comm_group N] [module R N]
+variables {ι : Type*} [fintype ι]
 
 /-- The trace of a linear map correspond to the contraction pairing under the isomorphism
  `End(M) ≃ M* ⊗ M`-/
@@ -137,15 +140,20 @@ end
 lemma trace_eq_contract_of_basis' [decidable_eq ι] (b : basis ι R M) :
   (linear_map.trace R M) =
   (contract_left R M) ∘ₗ (dual_tensor_hom_equiv_of_basis b).symm.to_linear_map :=
-by simp [linear_equiv.eq_comp_to_linear_map_symm, trace_eq_contract_of_basis R b]
+by simp [linear_equiv.eq_comp_to_linear_map_symm, trace_eq_contract_of_basis b]
 
-variables [module.free R M] [module.finite R M] [nontrivial R]
+variables (R M)
+variables [module.free R M] [module.finite R M] [module.free R N] [module.finite R N] [nontrivial R]
 
 /-- When `M` is finite free, the trace of a linear map correspond to the contraction pairing under
 the isomorphism `End(M) ≃ M* ⊗ M`-/
 @[simp] theorem trace_eq_contract :
   (linear_map.trace R M) ∘ₗ (dual_tensor_hom R M M) = contract_left R M :=
-trace_eq_contract_of_basis R (module.free.choose_basis R M)
+trace_eq_contract_of_basis (module.free.choose_basis R M)
+
+@[simp] theorem trace_eq_contract_apply (x : module.dual R M ⊗[R] M) :
+  (linear_map.trace R M) ((dual_tensor_hom R M M) x) = contract_left R M x :=
+by rw [←comp_apply, trace_eq_contract]
 
 open_locale classical
 
@@ -153,8 +161,8 @@ open_locale classical
 the isomorphism `End(M) ≃ M* ⊗ M`-/
 theorem trace_eq_contract' :
   (linear_map.trace R M) =
-  (contract_left R M) ∘ₗ (dual_tensor_hom_equiv).symm.to_linear_map :=
-trace_eq_contract_of_basis' R (module.free.choose_basis R M)
+  (contract_left R M) ∘ₗ (dual_tensor_hom_equiv R M M).symm.to_linear_map :=
+trace_eq_contract_of_basis' (module.free.choose_basis R M)
 
 /-- The trace of the identity endomorphism is the dimension of the free module -/
 @[simp] theorem trace_one : trace R M 1 = (finrank R M : R) :=
@@ -162,6 +170,33 @@ begin
   have b := module.free.choose_basis R M,
   rw [trace_eq_matrix_trace R b, to_matrix_one, module.free.finrank_eq_card_choose_basis_index],
   simp,
+end
+
+open tensor_product
+
+variable [module.finite R (M ⊗[R] N)] --Temporary until this is made an instance by #13705
+
+theorem trace_tensor_product :
+  compr₂ map_bilinear (trace R (M ⊗ N)) =
+  compl₁₂ (lsmul R R : R →ₗ[R] R →ₗ[R] R) (trace R M) (trace R N) :=
+begin
+  apply (compl₁₂_inj
+    (dual_tensor_hom_equiv R M M).surjective (dual_tensor_hom_equiv R N N).surjective).1,
+  ext f m g n,
+  simp only [algebra_tensor_module.curry_apply, to_fun_eq_coe, curry_apply,
+  coe_restrict_scalars_eq_coe, compl₁₂_apply, compr₂_apply, map_bilinear_apply,
+  trace_eq_contract_apply, contract_left_apply, lsmul_apply, algebra.id.smul_eq_mul,
+  map_dual_tensor_hom, dual_tensor_dual_map_apply],
+end
+
+variables {R M N}
+
+theorem trace_tensor_product' (f : M →ₗ[R] M) (g : N →ₗ[R] N) :
+  trace R (M ⊗ N) (map f g) = trace R M f * trace R N g :=
+begin
+  have h := ext_iff.1 (ext_iff.1 (trace_tensor_product R M N) f) g,
+  simp only [compr₂_apply, map_bilinear_apply, compl₁₂_apply, lsmul_apply,
+    algebra.id.smul_eq_mul] at h, exact h,
 end
 
 end

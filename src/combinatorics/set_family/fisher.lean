87b2e4aef55ec59148a8be0959435acf950deacd
Bhavik Mehta <bm489@cam.ac.uk>
2022-04-29 14:49:16 +0000

    shorter proof

--- src/combinatorics/set_family/fisher.lean
+++ src/combinatorics/set_family/fisher.lean
@@ -1,13 +1,13 @@
 /-
 Copyright (c) 2022 Bhavik Mehta. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
-Authors:Bhavik Mehta
+Authors: Bhavik Mehta
 -/
 
 import data.real.basic
 import algebra.big_operators.ring
 import linear_algebra.finite_dimensional
-import data.matrix.rank
+import data.zmod.parity
 
 /-!
 # Fisher's inequality
@@ -35,74 +35,49 @@ lemma sum_sq {α R : Type*} [decidable_eq α] [comm_ring R] (s : finset α) (f :
 by simp_rw [sq, sum_mul, mul_sum, ←sum_product', ←diag_union_off_diag,
     sum_union (disjoint_diag_off_diag _), sum_diag s (λ i j, f i * f j)]
 
-lemma exists_sol {ι α : Type*} [fintype ι] [decidable_eq α]
-  (E : ι → finset α) (h : (univ.bUnion E).card < fintype.card ι) :
-  ∃ x : ι → ℝ, (∃ i, x i ≠ 0) ∧ ∀ j ∈ univ.bUnion E, ∑ i, ite (j ∈ E i) (x i) 0 = 0 :=
-begin
-  haveI : decidable_eq ι := classical.dec_eq _,
-  let M : matrix (univ.bUnion E) ι ℝ := λ j i, ite (↑j ∈ E i) 1 0,
-  have : 0 < finrank ℝ M.to_lin'.ker,
-  { rw [←add_lt_add_iff_left (finrank ℝ M.to_lin'.range), M.to_lin'.finrank_range_add_finrank_ker,
-      finrank_fintype_fun_eq_card, add_zero],
-    apply (M.rank_le_card_height.trans_eq _).trans_lt h,
-    rw fintype.card_coe },
-  rw finrank_pos_iff_exists_ne_zero at this,
-  obtain ⟨⟨x, hMx⟩, hx₀⟩ := this,
-  simp only [ne.def, submodule.mk_eq_zero, function.funext_iff, not_forall] at hx₀,
-  refine ⟨x, hx₀, λ j hj, _⟩,
-  simpa [M, matrix.mul_vec, matrix.dot_product] using congr_fun hMx ⟨j, hj⟩,
-end
-
-lemma fisher_fintype {ι α : Type*} [fintype ι] [decidable_eq α] (t : ℕ)
+lemma fisher {ι α : Type*} [fintype ι] [fintype α] [decidable_eq α] (t : ℕ)
   (E : ι → finset α) (hE : ∀ i, t < (E i).card) (hE' : pairwise (λ i j, (E i ∩ E j).card = t)) :
-  fintype.card ι ≤ (univ.bUnion E).card :=
+  fintype.card ι ≤ fintype.card α :=
 begin
   haveI : decidable_eq ι := classical.dec_eq _,
-  rcases eq_or_ne t 0 with rfl | ht,
-  { simp only [card_eq_zero, ←disjoint_iff_inter_eq_empty] at hE',
-    simp only [finset.card_pos] at hE,
-    refine card_univ.symm.trans_le (card_le_card_bUnion _ (λ i _, hE i)),
-    rwa [coe_univ, set.pairwise_disjoint, set.pairwise_univ] },
-  have ht' : (t : ℝ) ≠ 0 := nat.cast_ne_zero.2 ht,
-  by_contra' h,
-  obtain ⟨x, ⟨i, hi⟩, hx₂⟩ := exists_sol E h,
-  have h : (∑ i, x i) ^ 2 = ∑ i, x i ^ 2 +
-    t⁻¹ * ∑ k in univ.bUnion E, ((∑ i, ite (k ∈ E i) (x i) 0) ^ 2 - ∑ i, ite (k ∈ E i) (x i) 0 ^ 2),
-  { simp_rw [sum_sq, add_tsub_cancel_left, ite_mul, zero_mul, mul_ite, mul_zero, add_right_inj,
-      ←ite_and, ←mem_inter, @sum_comm _ _ α, ←sum_filter, filter_mem_eq_inter, sum_const, mul_sum],
+  let ind : ι → α → ℝ := λ i a, ite (a ∈ E i) 1 0,
+  suffices : linear_independent ℝ ind,
+  { simpa using fintype_card_le_finrank_of_linear_independent this },
+  rw [fintype.linear_independent_iff],
+  intros g hg,
+  have h : ∑ i, ((E i).card - t : ℝ) * g i ^ 2 + t * (∑ i, g i) ^ 2 =
+    matrix.dot_product (∑ (i : ι), g i • ind i) (∑ (i : ι), g i • ind i),
+  { simp_rw [sub_mul, sum_sub_distrib, matrix.dot_product, fintype.sum_apply, pi.smul_apply,
+      algebra.id.smul_eq_mul, ←sq, sum_sq, mul_add, mul_sum, ←add_assoc, sub_add_cancel, mul_pow,
+      mul_boole, ←ite_and_mul_zero, ite_pow, one_pow, zero_pow' _ two_ne_zero, mul_boole,
+      ←mem_inter, sum_add_distrib, @sum_comm _ _ α, ←sum_filter, filter_mem_eq_inter, univ_inter,
+      sum_const, nsmul_eq_mul, add_right_inj],
     refine sum_congr rfl (λ i hi, _),
     simp only [mem_off_diag, mem_univ, true_and] at hi,
-    rw [(inter_eq_right_iff_subset _ _).2 _, hE' _ _ hi, nsmul_eq_mul, inv_mul_cancel_left₀],
-    { simpa },
-    exact (inter_subset_left _ _).trans (subset_bUnion_of_mem _ (mem_univ _)) },
-  have h' : (∑ i, x i) ^ 2 = t⁻¹ * ∑ i, (t - (E i).card) * x i ^ 2,
-  { simp_rw [h, sum_sub_distrib, mul_sum, ←mul_assoc, mul_sub, inv_mul_cancel ht', sub_mul, one_mul,
-      sum_sub_distrib, sub_eq_add_neg, add_right_inj, @sum_comm _ ι, ite_pow, zero_pow zero_lt_two,
-      ←sum_filter, sum_const, filter_mem_eq_inter, mul_assoc, ←mul_sum, sum_filter, nsmul_eq_mul,
-      (inter_eq_right_iff_subset _ _).2 (subset_bUnion_of_mem _ (mem_univ _)), add_left_eq_self],
-    rw [sum_congr rfl (λ j hj, _), sum_const_zero, mul_zero],
-    rw [hx₂ _ hj, sq, mul_zero] },
-  refine ((sq_nonneg (univ.sum x)).trans_eq h').not_lt (mul_neg_of_pos_of_neg _ _),
-  { rwa [inv_pos, nat.cast_pos, pos_iff_ne_zero] },
-  refine (finset.sum_lt_sum (λ i _, _) ⟨i, mem_univ _, _⟩).trans_eq sum_const_zero,
-  { refine mul_nonpos_of_nonpos_of_nonneg _ (sq_nonneg _),
-    simpa using (hE _).le },
-  exact mul_neg_of_neg_of_pos (by simpa using hE i) (by rwa sq_pos_iff),
+    rw hE' _ _ hi },
+  have : ∀ i ∈ (univ : finset ι), 0 ≤ ((E i).card - t : ℝ) * g i ^ 2 :=
+    λ i _, mul_nonneg (by simpa using (hE i).le) (sq_nonneg _),
+  rw [hg, matrix.zero_dot_product, add_eq_zero_iff' (sum_nonneg this)
+    (mul_nonneg (nat.cast_nonneg _) (sq_nonneg _)), sum_eq_zero_iff_of_nonneg this] at h,
+  intro i,
+  simpa [(hE i).ne', sub_eq_zero] using h.1 i (mem_univ _),
 end
 
-lemma fisher {ι α : Type*} [decidable_eq α] (t : ℕ)
-  (s : finset ι) (E : ι → finset α)
-  (hE : ∀ i ∈ s, t < (E i).card) (hE' : (s : set ι).pairwise (λ i j, (E i ∩ E j).card = t)) :
-  s.card ≤ (s.bUnion E).card :=
+lemma oddtown {ι α : Type*} [fintype ι] [fintype α] [decidable_eq α] (t : ℕ)
+  (E : ι → finset α) (hE : ∀ i, odd (E i).card) (hE' : pairwise (λ i j, even (E i ∩ E j).card)) :
+  fintype.card ι ≤ fintype.card α :=
 begin
-  convert fisher_fintype t (λ i : {i // i ∈ s}, E i) (λ i, hE _ i.2) (λ i j t, _) using 1,
-  { exact (fintype.card_coe _).symm },
-  { rw [univ_eq_attach, ←sup_eq_bUnion s.attach, sup_attach, sup_eq_bUnion] },
-  { exact hE' i.2 j.2 (mt subtype.eq t) },
+  let ind : ι → α → zmod 2 := λ i a, ite (a ∈ E i) 1 0,
+  suffices : linear_independent (zmod 2) ind,
+  { simpa using fintype_card_le_finrank_of_linear_independent this },
+  rw [fintype.linear_independent_iff],
+  intros g hg j,
+  suffices : matrix.dot_product (∑ i, g i • ind i) (ind j) = g j,
+  { rw [←this, hg, matrix.zero_dot_product] },
+  simp_rw [matrix.dot_product, fintype.sum_apply, sum_mul, @sum_comm _ _ α, pi.smul_apply,
+    algebra.id.smul_eq_mul, mul_assoc, ←ite_and_mul_zero, ←mem_inter, one_mul, mul_boole,
+    ←sum_filter, filter_mem_eq_inter, univ_inter, sum_const, nsmul_eq_mul],
+  refine (sum_eq_single j (λ i _ h, _) (by simp)).trans _,
+  { rw [zmod.eq_zero_iff_even.2 (hE' _ _ h), zero_mul] },
+  rw [inter_self, zmod.eq_one_iff_odd.2 (hE _), one_mul],
 end
-
-lemma fisher_set_family {α : Type*} [decidable_eq α] (t : ℕ) (E : finset (finset α))
-  (hE : ∀ i ∈ E, t < finset.card i)
-  (hE' : (E : set (finset α)).pairwise (λ i j, (i ∩ j).card = t)) :
-  E.card ≤ (E.bUnion id).card :=
-fisher t E id hE hE'

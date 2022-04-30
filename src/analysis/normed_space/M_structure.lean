cc34d8edf4b2e46455858bebed7a53d87b949dcc
Christopher Hoskin <christopher.hoskin@gmail.com>
2022-04-30 13:29:18 +0100

    Define projections under algebra/ring

--- src/analysis/normed_space/M_structure.lean
+++ src/analysis/normed_space/M_structure.lean
@@ -4,6 +4,7 @@ Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Christopher Hoskin
 -/
 import analysis.normed_space.basic
+import algebra.ring.projections
 import tactic.noncomm_ring
 
 /-!
@@ -50,45 +51,6 @@ The approach to showing that the L-projections form a Boolean algebra is inspire
 M-summand, M-projection, L-summand, L-projection, M-ideal, M-structure
 
 -/
-
-variables {M : Type*} [has_mul M]
-
-/--
-A continuous linear map `P` on a normed space `X` is said to be a projection if it is idempotent.
--/
-def is_projection (x : M) : Prop := x*x = x
-
-lemma projection_def {P : M} (h : is_projection P) : P*P = P := by exact h
-
-namespace is_projection
-
-variables {S : Type*} [semigroup S]
-
-lemma mul_of_commute {P Q : S} (h : commute P Q) (h₁ : is_projection P) (h₂ : is_projection Q) :
-  is_projection (P * Q)  :=
-begin
-  rw is_projection at h₁,
-  rw is_projection at h₂,
-  rw [commute, semiconj_by] at h,
-  rw [is_projection, mul_assoc, ← mul_assoc Q, ←h, mul_assoc P, h₂, ← mul_assoc, h₁],
-end
-
-variables {R : Type*} [non_assoc_ring R]
-
-lemma complement {P : R} (h : is_projection P) : is_projection (1 - P) :=
-begin
-  rw is_projection at h,
-  rw [is_projection, mul_sub_left_distrib, mul_one, sub_mul, one_mul, h, sub_self, sub_zero],
-end
-
-
-lemma complement_iff {P : R} : is_projection P ↔ is_projection (1 - P) :=
-⟨ is_projection.complement , λ h, sub_sub_cancel 1 P ▸ h.complement⟩
-
-instance : has_compl (subtype (is_projection  : R → Prop)) :=
-⟨λ P, ⟨1 - P, P.prop.complement⟩⟩
-
-end is_projection
 
 variables {X : Type*} [normed_group X]
 

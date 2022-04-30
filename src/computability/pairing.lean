141a8c7e2385528e4a22e112a537022cacd0543b
prakol16 <prakol16@users.noreply.github.com>
2022-04-29 22:01:14 -0700

    Update pairing.lean

--- src/computability/pairing.lean
+++ src/computability/pairing.lean
@@ -3,7 +3,7 @@ Copyright (c) 2022 Praneeth Kolichala. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Praneeth Kolichala
 -/
-import data.num.encode_list_bool
+import data.num.to_bits
 import data.list.basic
 
 /-!
@@ -60,18 +60,18 @@ by { simp [mkpair'], ring, }
 This function encodes (a, b) as a unary encoding of the number of bits in `a.length`, followed
 by `a.length`, followed by `a ++ b`. -/
 def mkpair (a b : list bool) : list bool :=
-mkpair' (num.encode_num a.length) (a ++ b)
+mkpair' (num.to_bits a.length) (a ++ b)
 
 /-- Left inverse to `list.mkpair` -/
 def unpair (ls : list bool) : list bool × list bool :=
-(unpair' ls).2.split_at (num.decode_num (unpair' ls).1)
+(unpair' ls).2.split_at (num.of_bits (unpair' ls).1)
 
 @[simp] lemma unpair_mkpair (a b : list bool) : unpair (mkpair a b) = (a, b) :=
 by simp [unpair, mkpair]
 
 lemma mkpair_length (a b : list bool) : (mkpair a b).length =
   if a.length = 0 then b.length + 1 else 2*(nat.log 2 a.length) + a.length + b.length + 3 :=
-by { split_ifs with h; simp [mkpair, num.encode_num_len, h], ring, }
+by { split_ifs with h; simp [mkpair, num.to_bits_len, h], ring, }
 
 /-- Size bound for `mkpair'`: `|(a, b)| = |a| + |b| + O(log |a|)` -/
 lemma mkpair_length_le (a b : list bool) :
@@ -81,10 +81,10 @@ by { rw mkpair_length, split_ifs; linarith, }
 /-- A nice coincidence in our choice of pairing function: if `a` and `b` have most significant
 bit `1`, then so does `mkpair' a b`. This allows for easy encoding of nat's in the standard way -/
 lemma mkpair_mem_num_encode_range {a b : list bool}
-  (h₁ : b ∈ set.range num.encode_num) (h₂ : b = [] → a ∈ set.range num.encode_num) :
-  a.mkpair b ∈ set.range num.encode_num :=
+  (h₁ : b ∈ set.range num.to_bits) (h₂ : b = [] → a ∈ set.range num.to_bits) :
+  a.mkpair b ∈ set.range num.to_bits :=
 begin
-  simp only [mkpair, mkpair', num.mem_encode_range_iff] at ⊢ h₁ h₂,
+  simp only [mkpair, mkpair', num.mem_to_bits_range_iff] at ⊢ h₁ h₂,
   induction b using list.reverse_cases_on with st lt, swap,
   { simpa [last'_eq_head'_reverse] using h₁, },
   induction a using list.reverse_cases_on with st lt, swap,
@@ -111,20 +111,20 @@ end
 end list
 
 namespace nat
-open num (encode_num decode_num)
+open num (to_bits of_bits)
 
 /-- Efficient pairing function on ℕ, where result has size log(a) + log(b) + O(log log a) bits -/
 def mkpair' (a b : ℕ) : ℕ :=
-decode_num (list.mkpair (encode_num a) (encode_num b))
+of_bits (list.mkpair (to_bits a) (to_bits b))
 
 /-- Unpairing function for `mkpair'` -/
 def unpair' (n : ℕ) : ℕ × ℕ :=
-(decode_num (list.unpair (encode_num n)).1, decode_num (list.unpair (encode_num n)).2)
+(of_bits (list.unpair (to_bits n)).1, of_bits (list.unpair (to_bits n)).2)
 
 @[simp] lemma unpair'_mkpair' (a b : ℕ) : unpair' (mkpair' a b) = (a, b) :=
 begin
   simp only [unpair', mkpair', num.of_to_nat],
-  rw num.encode_decode_list_bool_valid, { simp, },
+  rw num.to_bits_of_bits_valid, { simp, },
   exact list.mkpair_mem_num_encode_range (set.mem_range_self _) (λ _, set.mem_range_self _),
 end
 
@@ -132,39 +132,39 @@ lemma mkpair'_zero (a : ℕ) : mkpair' 0 a = bit1 a :=
 by simp [mkpair', list.mkpair, list.mkpair']
 
 lemma mkpair'_one (a : ℕ) : mkpair' 1 a = bit0 (bit1 (bit1 (bit1 a))) :=
-by { simp [mkpair', list.mkpair, list.mkpair', encode_num, (show 1 = num.pos 1, from rfl)], }
+by { simp [mkpair', list.mkpair, list.mkpair', to_bits, (show 1 = num.pos 1, from rfl)], }
 
 lemma mkpair'_ne_zero (a b : ℕ) : mkpair' a b ≠ 0 :=
 by { simp only [mkpair', list.mkpair, list.mkpair'], norm_cast, simp, }
 
 lemma unpair'_fst_lt (n : ℕ) : (unpair' n.succ).1 < n.succ :=
 begin
-  convert_to (unpair' n.succ).1 < decode_num (encode_num n.succ), { simp, },
-  apply num.decode_num_strict_mono,
+  convert_to (unpair' n.succ).1 < of_bits (to_bits n.succ), { simp, },
+  apply num.of_bits_strict_mono,
   apply list.unpair_fst_len_lt, simp, norm_cast, simp,
 end
 
 lemma unpair'_snd_lt (n : ℕ) : (unpair' n.succ).2 < n.succ :=
 begin
-  convert_to (unpair' n.succ).2 < decode_num (encode_num n.succ), { simp, },
-  apply num.decode_num_strict_mono,
+  convert_to (unpair' n.succ).2 < of_bits (to_bits n.succ), { simp, },
+  apply num.of_bits_strict_mono,
   apply list.unpair_snd_len_lt, simp, norm_cast, simp,
 end
 
 /-- Size bound for `mkpair'`: `log₂ (mkpair' a b) = log₂ a + log₂ b + O(log log a) `-/
 lemma mkpair'_le (a b : ℕ) : log 2 (mkpair' a b) ≤
   log 2 a + log 2 b + 2 * log 2 (log 2 a + 1) + 5 :=
-calc log 2 (decode_num (list.mkpair (encode_num a) (encode_num b)))
-    ≤ log 2 (2^((list.mkpair (encode_num a) (encode_num b)).length) - 1) :
-      by { apply log_monotone, apply le_pred_of_lt, apply num.decode_num_lt, }
-... ≤ (list.mkpair (encode_num a) (encode_num b)).length :
+calc log 2 (of_bits (list.mkpair (to_bits a) (to_bits b)))
+    ≤ log 2 (2^((list.mkpair (to_bits a) (to_bits b)).length) - 1) :
+      by { apply log_monotone, apply le_pred_of_lt, apply num.of_bits_lt, }
+... ≤ (list.mkpair (to_bits a) (to_bits b)).length :
       by { conv_rhs { rw ← log_pow (show 1 < 2, by norm_num) (list.mkpair _ _).length },
            apply log_monotone, exact tsub_le_self, }
-... ≤ 2*(log 2 (encode_num a).length) + (encode_num a).length + (encode_num b).length + 3 :
+... ≤ 2*(log 2 (to_bits a).length) + (to_bits a).length + (to_bits b).length + 3 :
       list.mkpair_length_le _ _
 ... ≤ 2*(log 2 (log 2 a + 1)) + (log 2 a + 1) + (log 2 b + 1) + 3 :
       by { mono*, all_goals { try { apply log_monotone }, try { exact zero_le _, }, },
-           all_goals { convert num.encode_num_len_le _, simp, }, }
+           all_goals { convert num.to_bits_len_le _, simp, }, }
 ... = _ : by ring_nf
 
 end nat

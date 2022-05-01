1de5736d0a4767c8364b9ced42478e585c7a799b
haruhisa-enomoto <73296681+haruhisa-enomoto@users.noreply.github.com>
2022-05-02 00:33:55 +0900

    fix style

--- src/ring_theory/ideal/nc_jacobson_radical.lean
+++ src/ring_theory/ideal/nc_jacobson_radical.lean
@@ -1,7 +1,7 @@
 /-
 Copyright (c) 2022 Haruhisa Enomoto. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
-Authors: Haruhisa Enomoto.
+Authors: Haruhisa Enomoto
 -/
 import ring_theory.ideal.basic
 import tactic.noncomm_ring

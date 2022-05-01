9934d3e7d0483aba31bbe8d0e845f7a1f5463a0e
Christopher Hoskin <christopher.hoskin@gmail.com>
2022-05-01 14:25:28 +0100

    Shorten a few proofs

--- src/algebra/ring/idempotents.lean
+++ src/algebra/ring/idempotents.lean
@@ -20,28 +20,23 @@ projection, idempotent
 -/
 
 variables {M N S M₀ M₁ R : Type*}
-variables [has_mul M] [monoid N] [semigroup S] [mul_zero_class M₀] [mul_one_class M₁] [non_assoc_ring R]
+variables [has_mul M] [monoid N] [semigroup S] [mul_zero_class M₀] [mul_one_class M₁]
+  [non_assoc_ring R]
 
 /--
 An element `p` is said to be idempotent if `p * p = p`
 -/
 def is_idempotent_elem (p : M) : Prop := p * p = p
 
-lemma all [is_idempotent M (*)] (a : M) : is_idempotent_elem a :=
-begin
-  unfold is_idempotent_elem,
-  exact is_idempotent.idempotent a,
-end
+lemma all [is_idempotent M (*)] (a : M) : is_idempotent_elem a := is_idempotent.idempotent a
 
 namespace is_idempotent_elem
 
 lemma eq {p : M} (h : is_idempotent_elem p) : p * p = p := h
 
 lemma mul_of_commute {p q : S} (h : commute p q) (h₁ : is_idempotent_elem p)
-  (h₂ : is_idempotent_elem q) : is_idempotent_elem (p * q)  :=
-begin
-  rw [is_idempotent_elem, mul_assoc, ← mul_assoc q, ←h.eq, mul_assoc p, h₂.eq, ← mul_assoc, h₁.eq],
-end
+  (h₂ : is_idempotent_elem q) : is_idempotent_elem (p * q)  := by rw [is_idempotent_elem, mul_assoc,
+    ← mul_assoc q, ←h.eq, mul_assoc p, h₂.eq, ← mul_assoc, h₁.eq]
 
 lemma zero : is_idempotent_elem (0 : M₀) := mul_zero _
 
@@ -68,19 +63,15 @@ section instances
 
 instance : has_zero { p : M₀ // is_idempotent_elem p } := { zero := ⟨ 0, zero ⟩ }
 
-@[simp] lemma coe_zero : ↑(0 : {p : M₀ // is_idempotent_elem p}) = (0 : M₀) :=
-rfl
+@[simp] lemma coe_zero : ↑(0 : {p : M₀ // is_idempotent_elem p}) = (0 : M₀) := rfl
 
 instance : has_one { p : M₁ // is_idempotent_elem p } := { one := ⟨ 1, one ⟩ }
 
-@[simp] lemma coe_one : ↑(1 : { p : M₁ // is_idempotent_elem p }) = (1 : M₁) :=
-rfl
+@[simp] lemma coe_one : ↑(1 : { p : M₁ // is_idempotent_elem p }) = (1 : M₁) := rfl
 
-instance : has_compl { p : R // is_idempotent_elem p } :=
-⟨λ p, ⟨1 - p, p.prop.one_sub⟩⟩
+instance : has_compl { p : R // is_idempotent_elem p } := ⟨λ p, ⟨1 - p, p.prop.one_sub⟩⟩
 
-@[simp] lemma coe_compl (p : { p : R // is_idempotent_elem p }) :
-  ↑(pᶜ) = (1 : R) - ↑p := rfl
+@[simp] lemma coe_compl (p : { p : R // is_idempotent_elem p }) : ↑(pᶜ) = (1 : R) - ↑p := rfl
 
 @[simp] lemma compl_compl (p : {p : R // is_idempotent_elem p}) : pᶜᶜ = p :=
 subtype.ext $ sub_sub_cancel _ _

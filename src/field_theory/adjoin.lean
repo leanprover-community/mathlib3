8751032f8953fc59953b21d7ebd0704395bc70b9
YaelDillies <yael.dillies@gmail.com>
2022-05-01 21:13:30 +0100

    fix field_theory.adjoin

--- src/field_theory/adjoin.lean
+++ src/field_theory/adjoin.lean
@@ -368,7 +368,7 @@ begin
   haveI := minpoly.irreducible hα,
   suffices : ϕ ⟨x, hx⟩ * (ϕ ⟨x, hx⟩)⁻¹ = 1,
   { convert subtype.mem (ϕ.symm (ϕ ⟨x, hx⟩)⁻¹),
-    refine inv_eq_of_mul_right_eq_one _,
+    refine inv_eq_of_mul_eq_one_LEFT _,
     apply_fun ϕ.symm at this,
     rw [alg_equiv.map_one, alg_equiv.map_mul, alg_equiv.symm_apply_apply] at this,
     rw [←subsemiring.coe_one, ←this, subsemiring.coe_mul, subtype.coe_mk] },

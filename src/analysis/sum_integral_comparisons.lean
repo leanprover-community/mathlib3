903c7caf1d956216ec617c9296c4870c0bee01cc
sgouezel <sebastien.gouezel@univ-rennes1.fr>
2022-05-02 10:26:11 +0200

    golf and clarify

--- src/analysis/sum_integral_comparisons.lean
+++ src/analysis/sum_integral_comparisons.lean
@@ -13,7 +13,7 @@ import analysis.special_functions.integrals
 
 ## Summary
 
-It is often the case that error terms in analysis can be computed by converting the
+It is often the case that error terms in analysis can be computed by comparing
 an infinite sum to the improper integral of an antitone function. This file will eventually enable
 that.
 
@@ -41,56 +41,32 @@ lemma antitone_on.integral_le_sum {x₀ : ℝ} {a : ℕ} {f : ℝ → ℝ}
   (hf : antitone_on f (Icc x₀ (x₀ + a))) :
   ∫ x in x₀..(x₀ + a), f x ≤ ∑ i in finset.range a, f (x₀ + i) :=
 begin
-  have : ∫ x in x₀..(x₀ + a), f x = ∫ x in 0..↑a, f (x₀ + x),
-  { simp only [interval_integral.integral_comp_add_left, add_zero], },
-  rw this,
-  conv
-  { to_rhs, congr, skip, funext, rw ←@integral_const_on_unit_interval (x₀ + i) (f (x₀ + i)), },
-  have hint : ∀ (k : ℕ), k < a → interval_integrable (λ x, f (x₀ + x))
-    real.measure_space.volume k (k + 1),
-  { intros k hk,
-    apply antitone_on.interval_integrable,
+  have hint : ∀ (k : ℕ), k < a → interval_integrable f volume (x₀+k) (x₀ + (k + 1 : ℕ)),
+  { assume k hk,
+    refine (hf.mono _).interval_integrable,
     rw interval_of_le,
-    { intros x hx y hy hxy,
-      refine hf _ _ (by simp [hxy]),
-      { split,
-        { rw le_add_iff_nonneg_right, calc (0 : ℝ) ≤ k : by simp ... ≤ x : hx.left, },
-        { rw add_le_add_iff_left,
-          refine le_trans hx.right _,
-          norm_cast,
-          exact nat.succ_le_of_lt hk, }, },
-      { split,
-        { rw le_add_iff_nonneg_right, calc (0 : ℝ) ≤ k : by simp ... ≤ y : hy.left,},
-        { rw add_le_add_iff_left,
-          refine le_trans hy.right _,
-          norm_cast,
-          exact nat.succ_le_of_lt hk, }, }, },
-    { simp, }, },
-  rw [←nat.cast_zero, ←interval_integral.sum_integral_adjacent_intervals hint],
-  refine finset.sum_le_sum (λ i hi, _),
-  rw finset.mem_range at hi,
-  simp only [nat.cast_add, nat.cast_one, interval_integral.integral_comp_add_left,
-    interval_integral.integral_const, add_tsub_cancel_left, algebra.id.smul_eq_mul, one_mul],
-  rw [←@integral_const_on_unit_interval (x₀ + i) (f (x₀ + i)), add_assoc],
-  refine interval_integral.integral_mono_on (by simp) _ (by simp) _,
-  { apply antitone_on.interval_integrable,
-    rw interval_of_le,
-    { intros x hx y hy hxy,
-      refine hf ⟨le_trans (by simp) hx.left, le_trans hx.right _⟩
-        ⟨le_trans (by simp) hy.left, le_trans hy.right _⟩ hxy,
-      { rw add_le_add_iff_left,
-        norm_cast,
-        exact nat.succ_le_of_lt hi,},
-      { rw add_le_add_iff_left,
-        norm_cast,
-        exact nat.succ_le_of_lt hi, }, },
-    { linarith, }, },
-  { refine (λ x hx, hf _ _ hx.left),
-    exact ⟨by simp, by simp [hi.le]⟩,
-    refine ⟨le_trans (by simp) hx.left, le_trans hx.right _⟩,
-    rw add_le_add_iff_left,
-    norm_cast,
-    exact nat.succ_le_of_lt hi, },
+    { apply Icc_subset_Icc,
+      { simp only [le_add_iff_nonneg_right, nat.cast_nonneg] },
+      { simp only [add_le_add_iff_left, nat.cast_le, nat.succ_le_of_lt hk] } },
+    { simp only [add_le_add_iff_left, nat.cast_le, nat.le_succ] } },
+  calc
+  ∫ x in x₀..(x₀ + a), f x = ∑ i in finset.range a, ∫ x in (x₀+i)..(x₀+(i+1 : ℕ)), f x :
+    begin
+      convert (interval_integral.sum_integral_adjacent_intervals hint).symm,
+      simp only [nat.cast_zero, add_zero],
+    end
+  ... ≤ ∑ i in finset.range a, ∫ x in (x₀+i)..(x₀+(i+1)), f (x₀ + i) :
+    begin
+      apply finset.sum_le_sum (λ i hi, _),
+      have ia : i < a := finset.mem_range.1 hi,
+      refine interval_integral.integral_mono_on (by simp) (hint _ ia) (by simp) (λ x hx, _),
+      apply hf _ _ hx.1,
+      { simp only [ia.le, mem_Icc, le_add_iff_nonneg_right, nat.cast_nonneg, add_le_add_iff_left,
+          nat.cast_le, and_self] },
+      { refine mem_Icc.2 ⟨le_trans (by simp) hx.1, le_trans hx.2 _⟩,
+        simp only [add_le_add_iff_left, nat.cast_le, nat.succ_le_of_lt ia] },
+    end
+  ... = ∑ i in finset.range a, f (x₀ + i) : by simp
 end
 
 lemma antitone_on.integral_le_sum_Ico {a b : ℕ} {f : ℝ → ℝ} (hab : a ≤ b)

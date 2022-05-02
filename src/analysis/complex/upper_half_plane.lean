44eca18268d8628b7ce6570e05638307da53f3d8
CBirkbeck <c.birkbeck@ucl.ac.uk>
2022-05-02 13:09:28 +0100

    re-add some subgroup lems

--- src/analysis/complex/upper_half_plane.lean
+++ src/analysis/complex/upper_half_plane.lean
@@ -218,9 +218,11 @@ end
 
 section SL_modular_action
 
-variables (g : SL(2, ℤ)) (z : ℍ)
+variables (g : SL(2, ℤ)) (z : ℍ) (Γ : subgroup SL(2,ℤ))
 
 @[simp]lemma sl_moeb (A: SL(2,ℤ)) (z : ℍ) : A • z = (A : (GL(2, ℝ)⁺)) • z := rfl
+lemma subgroup_moeb (A: Γ) (z : ℍ) : A • z = (A : (GL(2, ℝ)⁺)) • z := rfl
+@[simp]lemma subgroup_to_sl_moeb (A : Γ) (z : ℍ) : A • z = (A : SL(2,ℤ)) • z := rfl
 
 @[simp] lemma SL_neg_smul (g : SL(2,ℤ)) (z : ℍ) : -g • z = g • z :=
 begin

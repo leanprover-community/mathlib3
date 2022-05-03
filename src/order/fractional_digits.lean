a262e9c0b9e46a47bb8b202545f0a426135d98e0
Eric Wieser <wieser.eric@gmail.com>
2022-05-03 07:07:11 +0000

    wip

--- src/order/fractional_digits.lean
+++ src/order/fractional_digits.lean
@@ -9,7 +9,7 @@ import algebra.order.floor
 import data.fin.vec_notation
 
 /-!
-# Fractional Digits of a linearly ordered field
+# Fractional Digits of elements of linearly ordered fields
 
 ## Main definitions
 
@@ -25,15 +25,15 @@ import data.fin.vec_notation
 
 variables {R : Type*} [linear_ordered_field R] [floor_ring R]
 
-/-- The least power of `n` which is strictly greater than `r` -/
-def zlog (n : ℕ) (r : R) : ℤ :=
+/-- The greatest power of `b` such that `b ^ zlog b r ≤ r`. -/
+def zlog (b : ℕ) (r : R) : ℤ :=
 if 1 ≤ r then
-  nat.log n (nat.floor r)
+  nat.log b (nat.floor r)
 else
-  -nat.clog n (nat.ceil r⁻¹)
+  -nat.clog b (nat.ceil r⁻¹)
 
-lemma le_pow_zlog (n : ℕ) (r : R) (hn : 1 < n) (hr : 0 < r) :
-  (n : R) ^ zlog n r ≤ r :=
+lemma le_pow_zlog (b : ℕ) (r : R) (hn : 1 < b) (hr : 0 < r) :
+  (b : R) ^ zlog b r ≤ r :=
 begin
   by_cases hr1 : 1 ≤ r,
   { rw [zlog, if_pos hr1, zpow_coe_nat],
@@ -50,8 +50,8 @@ begin
     exact_mod_cast nat.le_pow_clog hn _, },
 end
 
-lemma lt_pow_succ_zlog (n : ℕ) (r : R) (hn : 1 < n) (hr : 0 < r) :
-  r < (n : R) ^ (zlog n r + 1) :=
+lemma lt_pow_succ_zlog (b : ℕ) (r : R) (hn : 1 < b) (hr : 0 < r) :
+  r < (b : R) ^ (zlog b r + 1) :=
 begin
   by_cases hr1 : 1 ≤ r,
   { rw [zlog, if_pos hr1, int.coe_nat_add_one_out, zpow_coe_nat],
@@ -61,7 +61,7 @@ begin
     { rw not_le at hr1,
       replace hr1 := one_lt_inv hr hr1,
       exact_mod_cast hr1.trans_le (nat.le_ceil _), },
-    have : 1 ≤ nat.clog n ⌈r⁻¹⌉₊ := nat.succ_le_of_lt (nat.clog_pos hn $ nat.succ_le_of_lt hcri),
+    have : 1 ≤ nat.clog b ⌈r⁻¹⌉₊ := nat.succ_le_of_lt (nat.clog_pos hn $ nat.succ_le_of_lt hcri),
     rw [zlog, if_neg hr1, neg_add_eq_sub, ←neg_sub, ←int.coe_nat_one, ← int.coe_nat_sub this,
       zpow_neg₀, zpow_coe_nat, lt_inv hr (pow_pos (nat.cast_pos.mpr $ zero_lt_one.trans hn) _)],
     have := nat.pow_pred_clog_lt_self hn hcri,
@@ -69,24 +69,38 @@ begin
     exact_mod_cast this }
 end
 
-def fractional_digits_aux (n : ℕ) (hn : 1 < n) : ℕ → Π r : R, 0 ≤ r → r < 1 → fin n
-| 0 := λ r hr0 hr1, ⟨⌊n • r⌋₊, (nat.floor_lt (nsmul_nonneg hr0 _)).mpr $
+#check @nat.pow_le_iff_le_log
+
+lemma zpow_le_iff_le_log {b : ℕ} (hb : 1 < b) {z : ℤ} {r : R} (hy : 0 < r) :
+  (b : R) ^ z ≤ r ↔ z ≤ zlog b r :=
+begin
+  by_cases hr1 : 1 ≤ r,
+  obtain ⟨n, rfl | rfl⟩ := int.eq_coe_or_neg z,
+  { rw zpow_coe_nat, },
+  induction z using int.,
+  rw [zlog, if_pos hr1],
+  rw int.le_coe
+
+end
+
+def fractional_digits_aux (b : ℕ) (hn : 1 < b) : ℕ → Π r : R, 0 ≤ r → r < 1 → fin b
+| 0 := λ r hr0 hr1, ⟨⌊b • r⌋₊, (nat.floor_lt (nsmul_nonneg hr0 _)).mpr $
   begin
     rw nsmul_eq_mul,
     apply mul_lt_of_lt_one_right (zero_lt_one.trans $ _) hr1,
     exact_mod_cast hn,
   end⟩
-| (k + 1) := λ r hr0 hr1, fractional_digits_aux k (n • r - ⌊n • r⌋₊)
+| (k + 1) := λ r hr0 hr1, fractional_digits_aux k (b • r - ⌊n • r⌋₊)
   (sub_nonneg_of_le $ nat.floor_le $ nsmul_nonneg hr0 _) (sub_lt_iff_lt_add.mpr $
     (nat.lt_succ_floor _).trans_eq $ (nat.cast_succ _).trans (add_comm _ _))
 
-/-- Compute the `k`th digit of `r` in base `n`.
+/-- Compute the `k`th digit of `r` in base `b`.
 
 Note: the VM gets stuck in an infinte loop without the `true →` in `hr`! -/
-def fractional_digits (n : ℕ) (hn : 1 < n) (r : R) (hr : true → 0 ≤ r) (k : ℕ) : fin n :=
-fractional_digits_aux n hn k (r / (n : R) ^ (zlog n r + 1))
-  (div_nonneg (hr trivial) $ zpow_nonneg (nat.cast_nonneg _) _) $ begin
-    obtain rfl | hr := eq_or_lt_of_le (hr trivial),
+def fractional_digits (b : ℕ) (hn : 1 < b) (r : R) (hr : 0 ≤ r) (k : ℕ) : fin b :=
+fractional_digits_aux b hn k (r / (b : R) ^ (zlog b r + 1))
+  (div_nonneg (hr) $ zpow_nonneg (nat.cast_nonneg _) _) $ begin
+    obtain rfl | hr := eq_or_lt_of_le (hr),
     { rw zero_div, exact zero_lt_one },
     have := lt_pow_succ_zlog _ r hn hr,
     rw div_lt_one,
--- test/fractional_digits.lean
+++ test/fractional_digits.lean
@@ -2,4 +2,6 @@ import order.fractional_digits
 import data.fin.vec_notation
 
 -- ![1, 2, 3, 4, 5, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
-#eval fractional_digits 10 (by norm_num) (0.12345 : ℚ) (by norm_num) ∘ (coe : fin 1 → ℕ)
+#eval fractional_digits 10 (by norm_num) (0.12345 : ℚ) (by norm_num) ∘ (coe : fin 15 → ℕ)
+
+#eval (list.range 10).map (fractional_digits 10 (by norm_num) (1/7 : ℚ) (by norm_num))

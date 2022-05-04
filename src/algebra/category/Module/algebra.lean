319de4a5b12caada6e686bcd81946538e576b809
Scott Morrison <scott.morrison@gmail.com>
2022-05-04 16:24:04 +1000

    another instance

--- src/algebra/category/Module/algebra.lean
+++ src/algebra/category/Module/algebra.lean
@@ -3,8 +3,9 @@ Copyright (c) 2022 Scott Morrison. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Scott Morrison
 -/
-import algebra.category.Module.basic
+import algebra.category.Module.monoidal
 import algebra.algebra.restrict_scalars
+import category_theory.linear
 
 /-!
 # Additional typeclass for modules over an algebra
@@ -12,6 +13,8 @@ import algebra.algebra.restrict_scalars
 For an object in `M : Module A`, where `A` is a `k`-algebra,
 we provide additional typeclasses on the underlying type `M`,
 namely `module k M` and `is_scalar_tower k A M`.
+
+We provide the `linear k (Module A)` instance.
 
 ## Note
 
@@ -24,6 +27,8 @@ that carries these typeclasses, this seems hard to achieve.
 (An alternative would be to always require these typeclasses,
 requiring users to write `Module' ℤ A` when `A` is merely a ring.)
 -/
+
+open category_theory
 
 namespace Module
 
@@ -38,5 +43,8 @@ by { change is_scalar_tower k A (restrict_scalars k A M), apply_instance, }
 
 -- We verify that the morphism spaces become `k`-modules.
 example (M N : Module A) : module k (M ⟶ N) := by apply_instance
+
+instance linear_over_field : linear k (Module A) :=
+{ hom_module := λ M N, by apply_instance, }
 
 end Module

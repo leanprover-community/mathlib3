590c8245ff7b5db0a3e18d1fc41487e8aa791380
Antoine Labelle <antoinelab01@gmail.com>
2022-05-04 10:23:03 -0400

    change names

--- src/linear_algebra/dual.lean
+++ src/linear_algebra/dual.lean
@@ -754,7 +754,7 @@ end field
 
 end linear_map
 
-section tensor_product
+namespace tensor_product
 
 variables (R) (M : Type*) (N : Type*)
 variables [add_comm_group M] [add_comm_group N]
@@ -778,7 +778,7 @@ The canonical linear map from `dual M ⊗ dual N` to `dual (M ⊗ N)`,
 sending `f ⊗ g` to the composition of `tensor_product.map f g` with
 the natural isomorphism `R ⊗ R ≃ R`.
 -/
-def dual_tensor_dual_map : (dual R M) ⊗[R] (dual R N) →ₗ[R] dual R (M ⊗[R] N) :=
+def dual_distrib : (dual R M) ⊗[R] (dual R N) →ₗ[R] dual R (M ⊗[R] N) :=
 (comp_right ↑(tensor_product.lid R R)) ∘ₗ hom_tensor_hom_map R M N R R
 
 variables {R M N}
@@ -787,22 +787,22 @@ variables {R M N}
 An inverse to `dual_tensor_dual_map` given bases.
 -/
 noncomputable
-def dual_tensor_dual_inv_of_basis (b : basis ι R M) (c : basis κ R N) :
+def dual_distrib_inv_of_basis (b : basis ι R M) (c : basis κ R N) :
   dual R (M ⊗[R] N) →ₗ[R] (dual R M) ⊗[R] (dual R N) :=
 ∑ i j, (ring_lmap_equiv_self R ℕ _).symm (b.dual_basis i ⊗ₜ c.dual_basis j)
     ∘ₗ applyₗ (c j) ∘ₗ applyₗ (b i) ∘ₗ (lcurry R M N R)
 
 @[simp]
-lemma dual_tensor_dual_map_apply (f : dual R M) (g : dual R N) (m : M) (n : N) :
-  dual_tensor_dual_map R M N (f ⊗ₜ g) (m ⊗ₜ n) = f m * g n :=
-by simp only [dual_tensor_dual_map, coe_comp, function.comp_app, hom_tensor_hom_map_apply,
+lemma dual_distrib_apply (f : dual R M) (g : dual R N) (m : M) (n : N) :
+  dual_distrib R M N (f ⊗ₜ g) (m ⊗ₜ n) = f m * g n :=
+by simp only [dual_distrib, coe_comp, function.comp_app, hom_tensor_hom_map_apply,
   comp_right_apply, linear_equiv.coe_coe, map_tmul, lid_tmul, algebra.id.smul_eq_mul]
 
 @[simp]
-lemma dual_tensor_dual_inv_of_basis_apply (b : basis ι R M) (c : basis κ R N)
-  (f : dual R (M ⊗[R] N)) : dual_tensor_dual_inv_of_basis b c f =
+lemma dual_distrib_inv_of_basis_apply (b : basis ι R M) (c : basis κ R N)
+  (f : dual R (M ⊗[R] N)) : dual_distrib_inv_of_basis b c f =
   ∑ i j, (f (b i ⊗ₜ c j)) • (b.dual_basis i ⊗ₜ c.dual_basis j) :=
-by simp [dual_tensor_dual_inv_of_basis]
+by simp [dual_distrib_inv_of_basis]
 
 /--
 A linear equivalence between `dual M ⊗ dual N` and `dual (M ⊗ N)` given bases for `M` and `N`.
@@ -810,20 +810,20 @@ It sends `f ⊗ g` to the composition of `tensor_product.map f g` with the natur
 isomorphism `R ⊗ R ≃ R`.
 -/
 @[simps]
-noncomputable def dual_tensor_dual_equiv_of_basis (b : basis ι R M) (c : basis κ R N) :
+noncomputable def dual_distrib_equiv_of_basis (b : basis ι R M) (c : basis κ R N) :
   (dual R M) ⊗[R] (dual R N) ≃ₗ[R] dual R (M ⊗[R] N) :=
 begin
   refine linear_equiv.of_linear
-    (dual_tensor_dual_map R M N) (dual_tensor_dual_inv_of_basis b c) _ _,
+    (dual_distrib R M N) (dual_distrib_inv_of_basis b c) _ _,
   { ext f m n,
     have h : ∀ (r s : R), r • s = s • r := is_commutative.comm,
-    simp only [compr₂_apply, mk_apply, comp_apply, id_apply, dual_tensor_dual_inv_of_basis_apply,
-      linear_map.map_sum, map_smul, sum_apply, smul_apply, dual_tensor_dual_map_apply, h (f _) _,
+    simp only [compr₂_apply, mk_apply, comp_apply, id_apply, dual_distrib_inv_of_basis_apply,
+      linear_map.map_sum, map_smul, sum_apply, smul_apply, dual_distrib_apply, h (f _) _,
       ← f.map_smul, ←f.map_sum, ←smul_tmul_smul, ←tmul_sum, ←sum_tmul, basis.coe_dual_basis,
       basis.coord_apply, basis.sum_repr] },
   { ext f g,
-    simp only [compr₂_apply, mk_apply, comp_apply, id_apply, dual_tensor_dual_inv_of_basis_apply,
-      dual_tensor_dual_map_apply, ←smul_tmul_smul, ←tmul_sum, ←sum_tmul, basis.coe_dual_basis,
+    simp only [compr₂_apply, mk_apply, comp_apply, id_apply, dual_distrib_inv_of_basis_apply,
+      dual_distrib_apply, ←smul_tmul_smul, ←tmul_sum, ←sum_tmul, basis.coe_dual_basis,
       basis.sum_dual_apply_smul_coord] }
 end
 
@@ -840,7 +840,7 @@ isomorphism `R ⊗ R ≃ R`.
 -/
 @[simp]
 noncomputable
-def dual_tensor_dual_equiv : (dual R M) ⊗[R] (dual R N) ≃ₗ[R] dual R (M ⊗[R] N) :=
-dual_tensor_dual_equiv_of_basis (module.free.choose_basis R M) (module.free.choose_basis R N)
+def dual_distrib_equiv : (dual R M) ⊗[R] (dual R N) ≃ₗ[R] dual R (M ⊗[R] N) :=
+dual_distrib_equiv_of_basis (module.free.choose_basis R M) (module.free.choose_basis R N)
 
 end tensor_product

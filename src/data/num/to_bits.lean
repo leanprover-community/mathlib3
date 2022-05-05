e52d2e4768c76915dbb417cd8393a07a1ebee15a
Praneeth Kolichala <praneeth.kolichala@gmail.com>
2022-05-04 22:09:23 -0700

    Redid definition of `of_bits` when trailing ff's

--- src/data/num/to_bits.lean
+++ src/data/num/to_bits.lean
@@ -11,9 +11,7 @@ import data.nat.log
 # Conversion of `num` to and from `list bool`
 
 This file provides functions to encode `num` as a `list bool` and decode `list bool`'s into `num`'s.
-For convenience, the decoding function always returns a value. If the most significant bit isn't
-`1`, it implicitly changes it to `1` when decoding. We then prove several lemmas about this
-encoding.
+For convenience, the decoding function always returns a value, essentially ignoring trailing `ff`'s.
 -/
 
 namespace pos_num
@@ -49,18 +47,6 @@ begin
   { simp [cast_bit0, nat.bit0_val b, ih, nat.one_add], }
 end
 
-lemma equiv_list_bool_symm_lt (ls : list bool) : (equiv_list_bool.symm ls : ℕ) < 2^(ls.length+1) :=
-begin
-  induction ls with hd tl ih, { simp [equiv_list_bool], },
-  cases hd,
-  { simp only [equiv_list_bool_symm_cons_ff, pos_num.cast_bit0],
-    rw [nat.bit0_val, mul_comm, pow_add], simpa, },
-  { simp only [equiv_list_bool_symm_cons_tt, pos_num.cast_bit1],
-    rw [nat.bit1_val, add_comm, pow_add],
-    apply @nat.lt_of_div_lt_div _ _ 2,
-    simpa [nat.add_mul_div_left, nat.div_eq_of_lt], },
-end
-
 end pos_num
 
 namespace num
@@ -79,16 +65,19 @@ by { cases n, { contradiction, }, simp [to_bits, num.bit0], }
 by { cases n, { refl, }, simp [to_bits, num.bit1], }
 
 /-- A decoding function from `list bool` to `num` -/
-def of_bits (L : list bool) : num := if L = [] then 0 else pos_num.equiv_list_bool.symm L.init
+def of_bits : list bool → num
+| [] := 0
+| (tt :: xs) := num.bit1 (of_bits xs)
+| (ff :: xs) := num.bit0 (of_bits xs)
 
 @[simp] lemma of_bits_nil : of_bits [] = 0 := rfl
-@[simp] lemma of_bits_cons_ff (l : list bool) (hl : l ≠ []) :
-  of_bits (ff :: l) = num.bit0 (of_bits l) := by simp [of_bits, hl, num.bit0]
-@[simp] lemma of_bits_cons_tt (l : list bool) : of_bits (tt :: l) = num.bit1 (of_bits l) :=
-by { cases l, { refl, }, simp [of_bits, num.bit1], }
+@[simp] lemma of_bits_cons_ff (l : list bool) : of_bits (ff :: l) = num.bit0 (of_bits l) := rfl
+@[simp] lemma of_bits_cons_tt (l : list bool) : of_bits (tt :: l) = num.bit1 (of_bits l) := rfl
+lemma of_bits_append_tt (l : list bool) : of_bits (l ++ [tt]) = pos_num.equiv_list_bool.symm l :=
+by { induction l with hd tl ih, { refl, }, cases hd; simp [ih]; refl, }
 
 @[simp] lemma of_bits_to_bits (n : num) : of_bits (to_bits n) = n :=
-by { cases n, { refl, }, simp [of_bits, to_bits], }
+by { cases n, { refl, }, simp [of_bits_append_tt, to_bits], }
 
 lemma of_bits_to_bits_left_inv : function.left_inverse of_bits to_bits := of_bits_to_bits
 
@@ -105,8 +94,8 @@ end
 @[simp] lemma to_bits_of_bits_valid {l : list bool} (h : l ∈ set.range to_bits) :
   to_bits (of_bits l) = l := of_bits_to_bits_left_inv.right_inv_on_range h
 
-@[simp] lemma of_bits_invalid (l : list bool) :
-  of_bits (l ++ [ff]) = of_bits (l ++ [tt]) := by simp [of_bits]
+@[simp] lemma of_bits_invalid (l : list bool) : of_bits (l ++ [ff]) = of_bits l :=
+by { induction l with hd tl ih, { refl, }, cases hd; simp [ih], }
 
 lemma to_bits_len (n : num) : (to_bits n).length = if n = 0 then 0 else nat.log 2 n + 1 :=
 begin
@@ -117,34 +106,34 @@ end
 lemma to_bits_len_le (n : num) : (to_bits n).length ≤ nat.log 2 n + 1 :=
 by { rw to_bits_len, split_ifs with h; simp [h],  }
 
-lemma of_bits_lt (l : list bool) : (of_bits l : ℕ) < 2^l.length :=
+lemma of_bits_lt {l : list bool} (hl : l ∈ set.range to_bits) : (of_bits l : ℕ) < 2^l.length :=
 begin
-  apply list.reverse_cases_on l, { simp [of_bits], },
-  intros st tl, cases tl; simpa [of_bits] using pos_num.equiv_list_bool_symm_lt _,
+  rcases hl with ⟨n, rfl⟩,
+  by_cases h : n = 0, { subst h, simp, },
+  convert nat.lt_pow_succ_log_self (show 1 < 2, by simp) n;
+  simp [h, to_bits_len],
 end
 
-lemma le_of_bits (l : list bool) (hl : l ≠ []) : 2^(l.length - 1) ≤ (of_bits l : ℕ) :=
+lemma le_of_bits (l : list bool) : 2^l.length ≤ (of_bits (l ++ [tt]) : ℕ) :=
 begin
-  induction l with hd tl ih, { contradiction, },
-  cases tl with h t, { cases hd; simp [of_bits], },
-  specialize ih (by trivial),
+  rw of_bits_append_tt, simp only [num.cast_pos, pos_num.cast_to_num],
+  induction l with hd tl ih, { simp, },
   cases hd,
-  { simpa [pow_add, nat.bit0_val (of_bits (h :: t)), mul_comm] using ih, },
-  simp only [of_bits_cons_tt, nat.bit1_val (of_bits (h :: t)), num.cast_bit1],
-  refine trans _ (nat.le_succ _),
-  simpa [pow_add, mul_comm] using ih,
+  { simpa [nat.bit0_val (pos_num.equiv_list_bool.symm tl), pow_add, mul_comm], },
+  refine calc _ = 2 * 2^tl.length : by { simp [pow_add, mul_comm], }
+            ... ≤ 2 * 2^tl.length + 1 : nat.le_succ _
+            ... ≤ _ : _,
+  simpa [nat.bit1_val (pos_num.equiv_list_bool.symm tl)],
 end
 
-lemma of_bits_strict_mono (l₁ l₂ : list bool) (h : l₁.length < l₂.length) :
-  (of_bits l₁ : ℕ) < of_bits l₂ :=
-calc (of_bits l₁ : ℕ) < 2^l₁.length : of_bits_lt l₁
-                 ...  ≤ 2^(l₂.length - 1) : pow_mono (show 1 ≤ 2, by simp) (nat.le_pred_of_lt h)
-                 ...  ≤ of_bits l₂ : le_of_bits l₂ (λ H, by { subst H, simpa using h, })
+lemma of_bits_strict_mono {l₁ l₂ : list bool} (h : l₁.length < l₂.length) :
+  (of_bits (l₁ ++ [tt]) : ℕ) < of_bits (l₂ ++ [tt]) :=
+calc (of_bits (l₁ ++ [tt]) : ℕ)
+      < 2^(l₁.length + 1) : by { convert of_bits_lt _; simp [mem_to_bits_range_iff], }
+ ...  ≤ 2^l₂.length: pow_mono (show 1 ≤ 2, by simp) (nat.succ_le_iff.mpr h)
+ ...  ≤ of_bits (l₂ ++ [tt]) : le_of_bits l₂
 
 @[simp] lemma to_bits_nil_iff (n : num) : to_bits n = [] ↔ n = 0 :=
 ⟨λ h, by simpa using congr_arg of_bits h, λ h, by { rw h, refl, }⟩
-
-@[simp] lemma of_bits_zero_iff (l : list bool) : of_bits l = 0 ↔ l = [] :=
-⟨λ h, by { by_contra H, simp [of_bits, H] at h, contradiction, }, λ h, by { rw h, refl, }⟩
 
 end num

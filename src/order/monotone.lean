b4063967ffd23f2e74336b8260e2450f79df45f0
sgouezel <sebastien.gouezel@univ-rennes1.fr>
2022-05-05 16:40:45 +0200

    weaker typeclasses

--- src/order/monotone.lean
+++ src/order/monotone.lean
@@ -316,14 +316,14 @@ end subsingleton
 
 /-! ### Miscellaneous monotonicity results -/
 
-lemma monotone_id [preorder α] : monotone (id : α → α) := λ a b, id
+lemma monotone_id [has_le α] : monotone (id : α → α) := λ a b, id
 
-lemma strict_mono_id [preorder α] : strict_mono (id : α → α) := λ a b, id
+lemma strict_mono_id [has_lt α] : strict_mono (id : α → α) := λ a b, id
 
-theorem monotone_const [preorder α] [preorder β] {c : β} : monotone (λ (a : α), c) :=
+theorem monotone_const [has_le α] [preorder β] {c : β} : monotone (λ (a : α), c) :=
 λ a b _, le_refl c
 
-theorem antitone_const [preorder α] [preorder β] {c : β} : antitone (λ (a : α), c) :=
+theorem antitone_const [has_le α] [preorder β] {c : β} : antitone (λ (a : α), c) :=
 λ a b _, le_refl c
 
 lemma strict_mono_of_le_iff_le [preorder α] [preorder β] {f : α → β}
@@ -393,7 +393,7 @@ end preorder
 /-! ### Monotonicity under composition -/
 
 section composition
-variables [preorder α] [preorder β] [preorder γ] {g : β → γ} {f : α → β} {s : set α}
+variables [has_le α] [has_le β] [has_le γ] {g : β → γ} {f : α → β} {s : set α}
 
 protected lemma monotone.comp (hg : monotone g) (hf : monotone f) :
   monotone (g ∘ f) :=
@@ -429,6 +429,11 @@ protected lemma antitone.comp_antitone_on (hg : antitone g) (hf : antitone_on f 
 lemma antitone.comp_monotone_on (hg : antitone g) (hf : monotone_on f s) :
   antitone_on (g ∘ f) s :=
 λ a ha b hb h, hg (hf ha hb h)
+
+end composition
+
+section composition_lt
+variables [has_lt α] [has_lt β] [has_lt γ] {g : β → γ} {f : α → β} {s : set α}
 
 protected lemma strict_mono.comp (hg : strict_mono g) (hf : strict_mono f) :
   strict_mono (g ∘ f) :=
@@ -466,7 +471,7 @@ lemma strict_anti.comp_strict_mono_on (hg : strict_anti g) (hf : strict_mono_on 
   strict_anti_on (g ∘ f) s :=
 λ a ha b hb h, hg (hf ha hb h)
 
-end composition
+end composition_lt
 
 namespace list
 

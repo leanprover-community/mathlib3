d462d4fdfbd45d6ce770b27cbb2e96abcfd7fd9b
Eric Rodriguez <37984851+ericrbg@users.noreply.github.com>
2022-05-05 03:09:30 -0700

    oopsie

--- src/number_theory/cyclotomic/primitive_roots.lean
+++ src/number_theory/cyclotomic/primitive_roots.lean
@@ -507,8 +507,7 @@ if `p ^ (k - s + 1) ≠ 2`. -/
 lemma prime_ne_two_pow_norm_zeta_pow_sub_one [ne_zero ((p : ℕ) : K)] {k : ℕ}
   [hpri : fact (p : ℕ).prime]
   [is_cyclotomic_extension {p ^ (k + 1)} K L]
-  (hirr : irreducible (cyclotomic (↑(p ^ (k + 1)) : ℕ) K))  {s : ℕ}
-  (hirr₁ : irreducible (cyclotomic (↑(p ^ (k - s + 1)) : ℕ) K)) (hs : s ≤ k)
+  (hirr : irreducible (cyclotomic (↑(p ^ (k + 1)) : ℕ) K))  {s : ℕ} (hs : s ≤ k)
   (htwo : p ^ (k - s + 1) ≠ 2) :
   norm K ((zeta (p ^ (k + 1)) K L) ^ ((p : ℕ) ^ s) - 1) = p ^ ((p : ℕ) ^ s) :=
 begin

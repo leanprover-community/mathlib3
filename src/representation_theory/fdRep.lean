6c4df11597e759bb54b9fabf02143964f97af8ee
Scott Morrison <scott.morrison@gmail.com>
2022-05-06 12:02:33 +1000

    more

--- src/representation_theory/fdRep.lean
+++ src/representation_theory/fdRep.lean
@@ -23,10 +23,11 @@ We verify that `fdRep k G` is a `k`-linear monoidal category, and rigid when `G`
 
 ## TODO
 * `fdRep k G ≌ { V : Rep k G // finite_dimensional k V }`
-* Upgrade the right rigid structure to a rigid structure (this just needs to be done for `FinVect`).
+* Upgrade the right rigid structure to a rigid structure
+  (this just needs to be done for `fgModule`).
 * `fdRep k G` has all finite colimits.
 * `fdRep k G` is abelian.
-* `fdRep k G ≌ FinVect (monoid_algebra k G)` (this will require generalising `FinVect` first).
+* `fdRep k G ≌ fgModule (monoid_algebra k G)`.
 -/
 
 universes u
@@ -37,7 +38,7 @@ open category_theory.limits
 /-- The category of finite dimensional `k`-linear representations of a monoid `G`. -/
 @[derive [large_category, concrete_category, preadditive, has_finite_limits]]
 abbreviation fdRep (k G : Type u) [field k] [monoid G] :=
-Action (FinVect.{u} k) (Mon.of G)
+Action (fgModule.{u} k) (Mon.of G)
 
 namespace fdRep
 
@@ -48,18 +49,18 @@ instance : linear k (fdRep k G) := by apply_instance
 instance : has_coe_to_sort (fdRep k G) (Type u) := concrete_category.has_coe_to_sort _
 
 instance (V : fdRep k G) : add_comm_group V :=
-by { change add_comm_group ((forget₂ (fdRep k G) (FinVect k)).obj V), apply_instance, }
+by { change add_comm_group ((forget₂ (fdRep k G) (fgModule k)).obj V), apply_instance, }
 
 instance (V : fdRep k G) : module k V :=
-by { change module k ((forget₂ (fdRep k G) (FinVect k)).obj V), apply_instance, }
+by { change module k ((forget₂ (fdRep k G) (fgModule k)).obj V), apply_instance, }
 
 instance (V : fdRep k G) : finite_dimensional k V :=
-by { change finite_dimensional k ((forget₂ (fdRep k G) (FinVect k)).obj V), apply_instance, }
+by { change finite_dimensional k ((forget₂ (fdRep k G) (fgModule k)).obj V), apply_instance, }
 
 /-- All hom spaces are finite dimensional. -/
 instance (V W : fdRep k G) : finite_dimensional k (V ⟶ W) :=
 finite_dimensional.of_injective
-  ((forget₂ (fdRep k G) (FinVect k)).map_linear_map k) (functor.map_injective _)
+  ((forget₂ (fdRep k G) (fgModule k)).map_linear_map k) (functor.map_injective _)
 
 -- This works well with the new design for representations:
 example (V : fdRep k G) : G →* (V →ₗ[k] V) := V.ρ
@@ -68,10 +69,10 @@ example (V : fdRep k G) : G →* (V →ₗ[k] V) := V.ρ
 @[simps ρ]
 def of {V : Type u} [add_comm_group V] [module k V] [finite_dimensional k V]
   (ρ : G →* (V →ₗ[k] V)) : Rep k G :=
-⟨FinVect.of k V, ρ⟩
+⟨fgModule.of k V, ρ⟩
 
 instance : has_forget₂ (fdRep k G) (Rep k G) :=
-{ forget₂ := (forget₂ (FinVect k) (Module k)).map_Action (Mon.of G), }
+{ forget₂ := (forget₂ (fgModule k) (Module k)).map_Action (Mon.of G), }
 
 -- Verify that the monoidal structure is available.
 example : monoidal_category (fdRep k G) := by apply_instance
@@ -93,6 +94,6 @@ variables {k G : Type u} [field k] [group G]
 
 -- Verify that the rigid structure is available when the monoid is a group.
 noncomputable instance : right_rigid_category (fdRep k G) :=
-by { change right_rigid_category (Action (FinVect k) (Group.of G)), apply_instance, }
+by { change right_rigid_category (Action (fgModule k) (Group.of G)), apply_instance, }
 
 end fdRep

759da38674e32925b3c3ec9ccf41d76e036d9ea9
Maria Ines de Frutos Fernandez <mariaines.dff@gmail.com>
2022-05-06 12:42:24 +0100

    incorporate #13610

--- src/ring_theory/dedekind_domain/adic_valuation.lean
+++ src/ring_theory/dedekind_domain/adic_valuation.lean
@@ -5,6 +5,7 @@ Authors: María Inés de Frutos-Fernández
 -/
 import ring_theory.dedekind_domain.ideal
 import ring_theory.valuation.integers
+import ring_theory.valuation.extend_to_localization
 import topology.algebra.valued_field
 
 /-!
@@ -242,119 +243,47 @@ end
 /-! ### Adic valuations on the field of fractions `K` -/
 /-- The `v`-adic valuation of `x ∈ K` is the valuation of `r` divided by the valuation of `s`,
 where `r` and `s` are chosen so that `x = r/s`. -/
-def valuation_def (x : K) : (with_zero (multiplicative ℤ)) :=
-let s := classical.some (classical.some_spec (is_localization.mk'_surjective
-  (non_zero_divisors R) x)) in (v.int_valuation_def (classical.some
-    (is_localization.mk'_surjective (non_zero_divisors R) x)))/(v.int_valuation_def s)
+def valuation  (v : height_one_spectrum R) : valuation K (with_zero (multiplicative ℤ)) :=
+valuation.extend_to_localization v.int_valuation
+  (λ r hr, set.mem_compl (v.int_valuation_ne_zero' ⟨r, hr⟩)) K
 
-variable (K)
-/-- The valuation of `k ∈ K` is independent on how we express `k` as a fraction. -/
-lemma int_valuation_div_eq {r r' : R} {s s' : non_zero_divisors R}
-  (h_mk : is_localization.mk' K r s = is_localization.mk' K r' s') :
-  (v.int_valuation_def r)/(v.int_valuation_def s) =
-  (v.int_valuation_def r')/(v.int_valuation_def s') :=
-begin
-  rw [div_eq_div_iff (int_valuation_ne_zero' v s) (int_valuation_ne_zero' v s'),
-    ← int_valuation.map_mul', ← int_valuation.map_mul',
-    is_fraction_ring.injective R K (is_localization.mk'_eq_iff_eq.mp h_mk)],
-end
+lemma valuation_def (x : K) : v.valuation x = valuation.extend_to_localization v.int_valuation
+  (λ r hr, set.mem_compl (v.int_valuation_ne_zero' ⟨r, hr⟩)) K  x :=
+rfl
 
 /-- The `v`-adic valuation of `r/s ∈ K` is the valuation of `r` divided by the valuation of `s`. -/
 lemma valuation_of_mk' {r : R} {s : non_zero_divisors R} :
-  v.valuation_def (is_localization.mk' K r s) = (v.int_valuation_def r)/(v.int_valuation_def s) :=
+  v.valuation (is_localization.mk' K r s) = (v.int_valuation_def r)/(v.int_valuation_def s) :=
 begin
-  rw valuation_def,
-  exact int_valuation_div_eq K v
-    (classical.some_spec (classical.some_spec (is_localization.mk'_surjective (non_zero_divisors R)
-    (is_localization.mk' K r s)))),
+  erw [valuation_def, (is_localization.to_localization_map (non_zero_divisors R) K).lift_mk',
+    div_eq_mul_inv, mul_eq_mul_left_iff],
+  left,
+  rw [units.coe_inv', inv_inj],
+  refl,
 end
 
-variable {K}
 /-- The `v`-adic valuation on `K` extends the `v`-adic valuation on `R`. -/
 lemma valuation_of_algebra_map (r : R) :
-  v.valuation_def (algebra_map R K r) = v.int_valuation_def r :=
-by rw [← is_localization.mk'_one K r, valuation_of_mk', submonoid.coe_one,
-    int_valuation.map_one', div_one _]
+  v.valuation (algebra_map R K r) = v.int_valuation_def r :=
+by { rw [valuation_def, valuation.extend_to_localization_apply_map_apply], refl }
 
 /-- The `v`-adic valuation on `R` is bounded above by 1. -/
-lemma valuation_le_one (r : R) : v.valuation_def (algebra_map R K r) ≤ 1 :=
+lemma valuation_le_one (r : R) : v.valuation (algebra_map R K r) ≤ 1 :=
 by { rw valuation_of_algebra_map, exact v.int_valuation_le_one r }
 
 /-- The `v`-adic valuation of `r ∈ R` is less than 1 if and only if `v` divides the ideal `(r)`. -/
 lemma valuation_lt_one_iff_dvd (r : R) :
-  v.valuation_def (algebra_map R K r) < 1 ↔ v.as_ideal ∣ ideal.span {r} :=
+  v.valuation (algebra_map R K r) < 1 ↔ v.as_ideal ∣ ideal.span {r} :=
 by { rw valuation_of_algebra_map, exact v.int_valuation_lt_one_iff_dvd r }
-
-/-- The `v`-adic valuation of `0 : K` equals 0. -/
-lemma valuation.map_zero' : v.valuation_def (0 : K) = 0 :=
-begin
-  rw [← (algebra_map R K).map_zero, valuation_of_algebra_map v],
-  exact v.int_valuation.map_zero',
-end
-
-/-- The `v`-adic valuation of `1 : K` equals 1. -/
-lemma valuation.map_one' : v.valuation_def (1 : K) = 1 :=
-begin
-  rw [← (algebra_map R K).map_one, valuation_of_algebra_map v],
-  exact v.int_valuation.map_one',
-end
-
-/-- The `v`-adic valuation of a product is the product of the valuations. -/
-lemma valuation.map_mul' (x y : K) :
-  v.valuation_def (x * y) = v.valuation_def x * v.valuation_def y :=
-begin
-  rw [valuation_def, valuation_def, valuation_def, div_mul_div_comm₀, ← int_valuation.map_mul',
-    ← int_valuation.map_mul', ← submonoid.coe_mul],
-  apply int_valuation_div_eq K v,
-  rw [(classical.some_spec (valuation_def._proof_2 (x * y))), is_fraction_ring.mk'_eq_div,
-    (algebra_map R K).map_mul, submonoid.coe_mul, (algebra_map R K).map_mul, ← div_mul_div_comm₀,
-    ← is_fraction_ring.mk'_eq_div, ← is_fraction_ring.mk'_eq_div,
-    (classical.some_spec (valuation_def._proof_2 x)),
-    (classical.some_spec (valuation_def._proof_2 y))],
-end
-
-/-- The `v`-adic valuation of a sum is bounded above by the maximum of the valuations. -/
-lemma valuation.map_add_le_max' (x y : K) :
-  v.valuation_def (x + y) ≤ max (v.valuation_def x) (v.valuation_def y) :=
-begin
-  obtain ⟨rx, sx, hx⟩ := is_localization.mk'_surjective (non_zero_divisors R) x,
-  obtain ⟨rxy, sxy, hxy⟩ := is_localization.mk'_surjective (non_zero_divisors R) (x + y),
-  obtain ⟨ry, sy, hy⟩ := is_localization.mk'_surjective (non_zero_divisors R) y,
-  rw [← hxy, ← hx, ← hy, valuation_of_mk', valuation_of_mk', valuation_of_mk'],
-  have h_frac_xy : is_localization.mk' K rxy sxy =
-    is_localization.mk' K (rx*(sy : R) + ry*(sx : R)) (sx*sy),
-  { rw [is_localization.mk'_add, hx, hy, hxy], },
-  have h_frac_x : is_localization.mk' K rx sx = is_localization.mk' K (rx*(sy : R)) (sx*sy),
-  { rw [is_localization.mk'_eq_iff_eq, submonoid.coe_mul, mul_assoc, mul_comm (sy : R) _], },
-  have h_frac_y : is_localization.mk' K ry sy = is_localization.mk' K (ry*(sx : R)) (sx*sy),
-  { rw [is_localization.mk'_eq_iff_eq, submonoid.coe_mul, mul_assoc], },
-  have h_denom : 0 < v.int_valuation_def ↑(sx * sy),
-  { rw [int_valuation_def, if_neg _],
-    { exact with_zero.zero_lt_coe _ },
-    { exact non_zero_divisors.ne_zero
-        (submonoid.mul_mem (non_zero_divisors R) sx.property sy.property), }},
-  rw [int_valuation_div_eq K v h_frac_x, int_valuation_div_eq K v h_frac_y,
-    int_valuation_div_eq K v h_frac_xy, le_max_iff, div_le_div_right₀ (ne_of_gt h_denom),
-    div_le_div_right₀ (ne_of_gt h_denom), ← le_max_iff],
-  exact v.int_valuation.map_add_le_max' _ _,
-end
-
-/-- The `v`-adic valuation on `K`. -/
-def valuation  : valuation K (with_zero (multiplicative ℤ)) :=
-{ to_fun    := v.valuation_def,
-  map_zero' := valuation.map_zero' v,
-  map_one'  := valuation.map_one' v,
-  map_mul'  := valuation.map_mul' v,
-  map_add_le_max'  := valuation.map_add_le_max' v }
 
 variable (K)
 /-- There exists `π ∈ K` with `v`-adic valuation `multiplicative.of_add (-1)`. -/
 lemma valuation_exists_uniformizer :
-  ∃ (π : K), v.valuation_def π = multiplicative.of_add (-1 : ℤ) :=
+  ∃ (π : K), v.valuation π = multiplicative.of_add (-1 : ℤ) :=
 begin
   obtain ⟨r, hr⟩ := v.int_valuation_exists_uniformizer,
   use algebra_map R K r,
-  rw valuation_of_algebra_map v,
+  rw [valuation_def, valuation.extend_to_localization_apply_map_apply],
   exact hr,
 end
 
@@ -363,8 +292,6 @@ lemma valuation_uniformizer_ne_zero :
   (classical.some (v.valuation_exists_uniformizer K)) ≠ 0 :=
 begin
   have hu := (classical.some_spec (v.valuation_exists_uniformizer K)),
-  have h : v.valuation_def (classical.some _) = valuation v (classical.some _) := rfl,
-  rw h at hu,
   exact (valuation.ne_zero_iff _).mp (ne_of_eq_of_ne hu with_zero.coe_ne_zero),
 end
 
@@ -378,8 +305,7 @@ variable {K}
 /-- `K` as a valued field with the `v`-adic valuation. -/
 def adic_valued : valued K (with_zero (multiplicative ℤ)) := valued.mk' v.valuation
 
-lemma adic_valued_def {x : K} : (v.adic_valued.v : _) x = v.valuation_def x := rfl
-
+lemma adic_valued_def {x : K} : (v.adic_valued.v : _) x = v.valuation x := rfl
 
 /-- The uniform space structure on `K` corresponding to the `v`-adic valuation.
 This cannot be made an instance since it depends on the choice of `v`. -/

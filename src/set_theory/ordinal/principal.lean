7f0487bc61cdd49e1e427d5f8c054110f3e5dfa4
negiizhao <egresf@gmail.com>
2022-05-07 22:02:15 +0800

    Update principal.lean

--- src/set_theory/ordinal/principal.lean
+++ src/set_theory/ordinal/principal.lean
@@ -252,7 +252,7 @@ begin
         rcases hd with ⟨y, hy, hy'⟩,
         use [x + y, hb hx hy],
         rw mul_add,
-        exact left.add_lt_add hx' hy' },
+        exact add_lt_add hx' hy' },
       assumption' } }
 end
 

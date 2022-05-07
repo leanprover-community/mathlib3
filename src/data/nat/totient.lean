b116c1db67c4a288e37809dbbae40b976135717a
Stuart Presnell <stuart.presnell@gmail.com>
2022-05-07 09:01:59 +0100

    Patch to prove original statement

--- src/data/nat/totient.lean
+++ src/data/nat/totient.lean
@@ -166,51 +166,14 @@ begin
 end
 
 lemma sum_totient (n : ℕ) : ∑ m in (range n.succ).filter (∣ n), φ m = n :=
-if hn0 : n = 0 then by simp [hn0]
-else
-calc ∑ m in (range n.succ).filter (∣ n), φ m
-    = ∑ d in (range n.succ).filter (∣ n), ((range (n / d)).filter (λ m, gcd (n / d) m = 1)).card :
-  eq.symm $ sum_bij (λ d _, n / d)
-    (λ d hd, mem_filter.2 ⟨mem_range.2 $ lt_succ_of_le $ nat.div_le_self _ _,
-      by conv {to_rhs, rw ← nat.mul_div_cancel' (mem_filter.1 hd).2}; simp⟩)
-    (λ _ _, rfl)
-    (λ a b ha hb h,
-      have ha : a * (n / a) = n, from nat.mul_div_cancel' (mem_filter.1 ha).2,
-      have 0 < (n / a), from nat.pos_of_ne_zero (λ h, by simp [*, lt_irrefl] at *),
-      by rw [← nat.mul_left_inj this, ha, h, nat.mul_div_cancel' (mem_filter.1 hb).2])
-    (λ b hb,
-      have hb : b < n.succ ∧ b ∣ n, by simpa [-range_succ] using hb,
-      have hbn : (n / b) ∣ n, from ⟨b, by rw nat.div_mul_cancel hb.2⟩,
-      have hnb0 : (n / b) ≠ 0, from λ h, by simpa [h, ne.symm hn0] using nat.div_mul_cancel hbn,
-      ⟨n / b, mem_filter.2 ⟨mem_range.2 $ lt_succ_of_le $ nat.div_le_self _ _, hbn⟩,
-        by rw [← nat.mul_left_inj (nat.pos_of_ne_zero hnb0),
-          nat.mul_div_cancel' hb.2, nat.div_mul_cancel hbn]⟩)
-... = ∑ d in (range n.succ).filter (∣ n), ((range n).filter (λ m, gcd n m = d)).card :
-  sum_congr rfl (λ d hd,
-    have hd : d ∣ n, from (mem_filter.1 hd).2,
-    have hd0 : 0 < d, from nat.pos_of_ne_zero (λ h, hn0 (eq_zero_of_zero_dvd $ h ▸ hd)),
-    card_congr (λ m hm, d * m)
-      (λ m hm, have hm : m < n / d ∧ gcd (n / d) m = 1, by simpa using hm,
-        mem_filter.2 ⟨mem_range.2 $ nat.mul_div_cancel' hd ▸
-          (mul_lt_mul_left hd0).2 hm.1,
-          by rw [← nat.mul_div_cancel' hd, gcd_mul_left, hm.2, mul_one]⟩)
-      (λ a b ha hb h, (nat.mul_right_inj hd0).1 h)
-      (λ b hb, have hb : b < n ∧ gcd n b = d, by simpa using hb,
-        ⟨b / d, mem_filter.2 ⟨mem_range.2
-            ((mul_lt_mul_left (show 0 < d, from hb.2 ▸ hb.2.symm ▸ hd0)).1
-              (by rw [← hb.2, nat.mul_div_cancel' (gcd_dvd_left _ _),
-                nat.mul_div_cancel' (gcd_dvd_right _ _)]; exact hb.1)),
-            hb.2 ▸ coprime_div_gcd_div_gcd (hb.2.symm ▸ hd0)⟩,
-          hb.2 ▸ nat.mul_div_cancel' (gcd_dvd_right _ _)⟩))
-... = ((filter (∣ n) (range n.succ)).bUnion (λ d, (range n).filter (λ m, gcd n m = d))).card :
-  (card_bUnion (by intros; apply disjoint_filter.2; cc)).symm
-... = (range n).card :
-  congr_arg card (finset.ext (λ m, ⟨by simp,
-    λ hm, have h : m < n, from mem_range.1 hm,
-      mem_bUnion.2 ⟨gcd n m, mem_filter.2
-        ⟨mem_range.2 (lt_succ_of_le (le_of_dvd (lt_of_le_of_lt (zero_le _) h)
-          (gcd_dvd_left _ _))), gcd_dvd_left _ _⟩, mem_filter.2 ⟨hm, rfl⟩⟩⟩))
-... = n : card_range _
+begin
+  rcases n.eq_zero_or_pos with rfl | hn0, { simp },
+  nth_rewrite_rhs 0 ←sum_totient' n,
+  apply sum_congr _ (λ x hx, rfl),
+  ext,
+  simp only [divisors, mem_filter, mem_range, mem_Ico, and.congr_left_iff, iff_and_self],
+  exact λ ha1 _, succ_le_iff.mpr (pos_of_dvd_of_pos ha1 hn0),
+end
 
 /-- When `p` is prime, then the totient of `p ^ (n + 1)` is `p ^ n * (p - 1)` -/
 lemma totient_prime_pow_succ {p : ℕ} (hp : p.prime) (n : ℕ) :

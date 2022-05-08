563ab3334a90cc1998d2ee7071c97e23cd3c4d7c
YaelDillies <yael.dillies@gmail.com>
2022-05-08 23:54:05 +0100

    move around

--- src/linear_algebra/matrix/block.lean
+++ src/linear_algebra/matrix/block.lean
@@ -30,62 +30,46 @@ matrix, diagonal, det, block triangular
 
 -/
 
-open finset
-open_locale big_operators
+open finset function order_dual
+open_locale big_operators matrix
 
 universes v
 
-variables {α m n o : Type*} [decidable_eq n] [fintype n] [decidable_eq m] [fintype m]
-variables {R : Type v} [comm_ring R]
+variables {α m n : Type*}
+variables {R : Type v} [comm_ring R] {M : matrix m m R} {b : m → α}
 
 namespace matrix
 
-lemma det_to_block (M : matrix m m R) (p : m → Prop) [decidable_pred p] :
-  M.det = (matrix.from_blocks (to_block M p p) (to_block M p (λ j, ¬p j))
-    (to_block M (λ j, ¬p j) p) (to_block M (λ j, ¬p j) (λ j, ¬p j))).det :=
-begin
-  rw ← matrix.det_reindex_self (equiv.sum_compl p).symm M,
-  rw [det_apply', det_apply'],
-  congr, ext σ, congr, ext,
-  generalize hy : σ x = y,
-  cases x; cases y;
-  simp only [matrix.reindex_apply, to_block_apply, equiv.symm_symm,
-    equiv.sum_compl_apply_inr, equiv.sum_compl_apply_inl,
-    from_blocks_apply₁₁, from_blocks_apply₁₂, from_blocks_apply₂₁, from_blocks_apply₂₂,
-    matrix.minor_apply],
-end
-
-lemma two_block_triangular_det (M : matrix m m R) (p : m → Prop) [decidable_pred p]
-  (h : ∀ i (h1 : ¬p i) j (h2 : p j), M i j = 0) :
-  M.det = (to_square_block_prop M p).det * (to_square_block_prop M (λ i, ¬p i)).det :=
-begin
-  rw det_to_block M p,
-  convert det_from_blocks_zero₂₁ (to_block M p p) (to_block M p (λ j, ¬p j))
-    (to_block M (λ j, ¬p j) (λ j, ¬p j)),
-  ext,
-  exact h ↑i i.2 ↑j j.2
-end
-
-lemma equiv_block_det (M : matrix m m R) {p q : m → Prop} [decidable_pred p] [decidable_pred q]
-  (e : ∀x, q x ↔ p x) : (to_square_block_prop M p).det = (to_square_block_prop M q).det :=
-by convert matrix.det_reindex_self (equiv.subtype_equiv_right e) (to_square_block_prop M q)
-
-lemma to_square_block_det'' (M : matrix m m R) {n : nat} (b : m → fin n) (k : fin n) :
-  (to_square_block M b k).det = (to_square_block' M (λ i, ↑(b i)) ↑k).det :=
-begin
-  simp_rw [to_square_block_def],
-  apply equiv_block_det,
-  intro x,
-  apply (fin.ext_iff _ _).symm
-end
+section has_lt
+variables [has_lt α]
 
 /-- Let `b` map rows and columns of a square matrix `M` to blocks indexed by `ℕ`s. Then
 `block_triangular M n b` says the matrix is block triangular. -/
-def block_triangular [has_lt α] (M : matrix o o R) (b : o → α) : Prop :=
-∀ ⦃i j⦄, b j < b i → M i j = 0
+def block_triangular (M : matrix m m R) (b : m → α) : Prop := ∀ ⦃i j⦄, b j < b i → M i j = 0
 
-lemma upper_two_block_triangular' {m n : Type*}
-  (A : matrix m m R) (B : matrix m n R) (D : matrix n n R) :
+protected lemma block_triangular.minor {f : n → m} (h : M.block_triangular b) :
+  (M.minor f f).block_triangular (b ∘ f) :=
+λ i j hij, h hij
+
+@[simp] lemma block_triangular_reindex_iff {b : n → α} {e : m ≃ n} :
+  (reindex e e M).block_triangular b ↔ M.block_triangular (b ∘ e) :=
+begin
+  refine ⟨λ h, _, λ h, _⟩,
+  { convert h.minor,
+    simp only [reindex_apply, minor_minor, minor_id_id, equiv.symm_comp_self] },
+  { convert h.minor,
+    simp only [comp.assoc b e e.symm, equiv.self_comp_symm, comp.right_id] }
+end
+
+protected lemma block_triangular.transpose :
+  M.block_triangular b → Mᵀ.block_triangular (to_dual ∘ b) := swap
+
+@[simp] protected lemma block_triangular_transpose_iff {b : m → αᵒᵈ} :
+  Mᵀ.block_triangular b ↔ M.block_triangular (of_dual ∘ b) := forall_swap
+
+end has_lt
+
+lemma upper_two_block_triangular' (A : matrix m m R) (B : matrix m n R) (D : matrix n n R) :
   block_triangular (from_blocks A B 0 D) (sum.elim (λ i, (0 : fin 2)) (λ j, 1)) :=
 begin
   intros k1 k2 hk12,
@@ -106,8 +90,7 @@ begin
   { exact absurd hk12 (irrefl 1) }
 end
 
-lemma upper_two_block_triangular {m n : Type*}
-  (A : matrix m m R) (B : matrix m n R) (D : matrix n n R) :
+lemma upper_two_block_triangular (A : matrix m m R) (B : matrix m n R) (D : matrix n n R) :
   block_triangular (from_blocks A B 0 D) (sum.elim (λ i, 0) (λ j, 1)) :=
 begin
   intros k1 k2 hk12,
@@ -124,52 +107,114 @@ begin
   { exact absurd hk12 (irrefl 1) }
 end
 
+/-! ### Determinant -/
+
+variables [decidable_eq m] [fintype m] [decidable_eq n] [fintype n]
+
+lemma equiv_block_det (M : matrix m m R) {p q : m → Prop} [decidable_pred p] [decidable_pred q]
+  (e : ∀ x, q x ↔ p x) : (to_square_block_prop M p).det = (to_square_block_prop M q).det :=
+by convert matrix.det_reindex_self (equiv.subtype_equiv_right e) (to_square_block_prop M q)
+
+@[simp] lemma det_to_square_block_id [decidable_eq R] (M : matrix m m R) (i : m) :
+  (M.to_square_block id i).det = M i i :=
+begin
+  letI : unique {a // id a = i} := ⟨⟨⟨i, rfl⟩⟩, λ j, subtype.ext j.property⟩,
+  exact (det_unique _).trans rfl,
+end
+
+lemma det_to_block (M : matrix m m R) (p : m → Prop) [decidable_pred p] :
+  M.det = (from_blocks (to_block M p p) (to_block M p $ λ j, ¬p j)
+    (to_block M (λ j, ¬p j) p) $ to_block M (λ j, ¬p j) $ λ j, ¬p j).det :=
+begin
+  rw ←matrix.det_reindex_self (equiv.sum_compl p).symm M,
+  rw [det_apply', det_apply'],
+  congr, ext σ, congr, ext,
+  generalize hy : σ x = y,
+  cases x; cases y;
+  simp only [matrix.reindex_apply, to_block_apply, equiv.symm_symm,
+    equiv.sum_compl_apply_inr, equiv.sum_compl_apply_inl,
+    from_blocks_apply₁₁, from_blocks_apply₁₂, from_blocks_apply₂₁, from_blocks_apply₂₂,
+    matrix.minor_apply],
+end
+
+lemma two_block_triangular_det (M : matrix m m R) (p : m → Prop) [decidable_pred p]
+  (h : ∀ i, ¬ p i → ∀ j, p j → M i j = 0) :
+  M.det = (to_square_block_prop M p).det * (to_square_block_prop M (λ i, ¬p i)).det :=
+begin
+  rw det_to_block M p,
+  convert det_from_blocks_zero₂₁ (to_block M p p) (to_block M p (λ j, ¬p j))
+    (to_block M (λ j, ¬p j) (λ j, ¬p j)),
+  ext,
+  exact h ↑i i.2 ↑j j.2
+end
+
+lemma two_block_triangular_det' (M : matrix m m R) (p : m → Prop) [decidable_pred p]
+  (h : ∀ i, p i → ∀ j, ¬ p j → M i j = 0) :
+  M.det = (to_square_block_prop M p).det * (to_square_block_prop M (λ i, ¬p i)).det :=
+begin
+  rw [M.two_block_triangular_det (λ i, ¬ p i), mul_comm],
+  simp_rw not_not,
+  congr' 1,
+  exact equiv_block_det _ (λ _, not_not.symm),
+  simpa only [not_not] using h,
+end
+
+lemma to_square_block_det'' (M : matrix m m R) {n : nat} (b : m → fin n) (k : fin n) :
+  (M.to_square_block b k).det = (M.to_square_block (λ i, (b i : ℕ)) k).det :=
+equiv_block_det _ $ λ _, (fin.ext_iff _ _).symm
+
 lemma det_of_block_triangular (M : matrix m m R) (b : m → ℕ) (h : M.block_triangular b) :
-  ∀ (n : ℕ) (hn : ∀ i, b i < n), M.det = ∏ k in range n, (to_square_block' M b k).det :=
+  ∀ (n : ℕ) (hn : ∀ i, b i < n), M.det = ∏ k in range n, (M.to_square_block b k).det :=
 begin
   intros n hn,
   unfreezingI { induction n with n hi generalizing m M b },
-  { rw prod_range_zero,
-    apply det_eq_one_of_card_eq_zero,
-    apply fintype.card_eq_zero_iff.mpr,
-    exact ⟨λ i, nat.not_lt_zero (b i) (hn i)⟩ },
+  { haveI : is_empty m := ⟨λ i, not_lt_bot $ hn i⟩,
+    exact det_is_empty },
   rw [prod_range_succ_comm],
   have h2 : (M.to_square_block_prop (λ (i : m), b i = n.succ)).det =
-    (M.to_square_block' b n.succ).det,
-  { dunfold to_square_block', dunfold to_square_block_prop, refl },
-  rw two_block_triangular_det M (λ i, ¬(b i = n)),
-  { rw mul_comm,
-    apply congr (congr_arg has_mul.mul _),
-    { let m' := {a // ¬b a = n },
-      let b' := (λ (i : m'), b ↑i),
-      have h' :
-        block_triangular (M.to_square_block_prop (λ (i : m), ¬b i = n)) b',
-      { intros i j, apply h },
-      have hni : ∀ (i : {a // ¬b a = n}), b' i < n,
-      { exact λ i, (ne.le_iff_lt i.property).mp (nat.lt_succ_iff.mp (hn ↑i)) },
-      have h1 := hi (M.to_square_block_prop (λ (i : m), ¬b i = n)) b' h' hni,
-      rw ←fin.prod_univ_eq_prod_range at h1 ⊢,
-      convert h1,
-      ext k,
-      simp only [to_square_block_def],
-      let he : {a // b' a = ↑k} ≃ {a // b a = ↑k},
-      { have hc : ∀ (i : m), (λ a, b a = ↑k) i → (λ a, ¬b a = n) i,
-        { intros i hbi, rw hbi, exact ne_of_lt (fin.is_lt k) },
-        exact equiv.subtype_subtype_equiv_subtype hc },
-      exact matrix.det_reindex_self he (λ (i j : {a // b' a = ↑k}), M ↑i ↑j) },
-    { rw det_to_square_block' M b n,
-      have hh : ∀ a, b a = n ↔ ¬(λ (i : m), ¬b i = n) a,
-      { intro i, simp only [not_not] },
-      exact equiv_block_det M hh }},
-  { intros i hi j hj,
-    apply h, simp only [not_not] at hi,
-    rw hi,
-    exact hj.lt_of_le (nat.lt_succ_iff.mp $ hn j) }
+    (M.to_square_block b n.succ).det,
+  { refl },
+  rw two_block_triangular_det' M (λ i, b i = n),
+  { refine congr_arg _ _,
+    let b' := λ i : {a // b a ≠ n}, b ↑i,
+    have h' :  block_triangular (M.to_square_block_prop (λ (i : m), b i ≠ n)) b',
+    { intros i j, apply h },
+    have hni : ∀ (i : {a // b a ≠ n}), b' i < n,
+    { exact λ i, (ne.le_iff_lt i.property).mp (nat.lt_succ_iff.mp (hn ↑i)) },
+    have h1 := hi (M.to_square_block_prop (λ (i : m), b i ≠ n)) b' h' hni,
+    rw ←fin.prod_univ_eq_prod_range at h1 ⊢,
+    convert h1,
+    ext k,
+    simp only [to_square_block_def],
+    let he : {a // b' a = ↑k} ≃ {a // b a = ↑k},
+    { have hc : ∀ (i : m), (λ a, b a = ↑k) i → (λ a, b a ≠ n) i,
+      { intros i hbi, rw hbi, exact ne_of_lt (fin.is_lt k) },
+      exact equiv.subtype_subtype_equiv_subtype hc },
+    exact matrix.det_reindex_self he (λ (i j : {a // b' a = ↑k}), M ↑i ↑j) },
+  { rintro i rfl j hj,
+    exact h ((nat.lt_succ_iff.mp $ hn _).lt_of_ne hj) }
 end
 
-lemma det_of_block_triangular'' (M : matrix m m R) (b : m → ℕ) (h : block_triangular M b) :
-  M.det = ∏ k in univ.image b, (to_square_block' M b k).det :=
+protected lemma block_triangular.det [decidable_eq α] [preorder α] (hM : block_triangular M b) :
+  M.det = ∏ a in univ.image b, (M.to_square_block b a).det :=
 begin
+  unfreezingI { induction ‹fintype m› using fintype.induction_empty_option'
+    with α₁ α₂ h₂ e ih m h ih },
+  { haveI : fintype α₁ := fintype.of_equiv _ e.symm,
+    haveI : decidable_eq α₁ := classical.dec_eq _,
+    rw ←det_reindex_self e.symm,
+    convert (ih hM.minor).trans _,
+    simp_rw e.symm_symm,
+    refine prod_congr _ (λ a ha, _),
+    { rw ←image_image,
+      sorry },
+    unfold to_square_block to_square_block_prop to_block,
+    rw minor_minor,
+    sorry
+  },
+  sorry,
+  { simp only [coe_det_is_empty, prod_const_one] },
+  rw two_block_triangular_det M (λ i, i ≠ none),
   let n : ℕ := (Sup (univ.image b : set ℕ)).succ,
   have hn : ∀ i, b i < n,
   { intro i,
@@ -189,35 +234,23 @@ begin
   exact det_is_empty }
 end
 
-lemma det_of_block_triangular' (M : matrix m m R) {n : ℕ} (b : m → fin n)
+lemma block_triangular.det_fintype [decidable_eq α] [fintype α] [preorder α]
   (h : block_triangular M b) :
-  M.det = ∏ (k : fin n), (to_square_block M b k).det :=
+  M.det = ∏ k : α, (M.to_square_block b k).det :=
 begin
-  let b2 : m → ℕ := λ i, ↑(b i),
-  simp_rw to_square_block_det'',
-  rw fin.prod_univ_eq_prod_range (λ (k : ℕ), (M.to_square_block' b2 k).det) n,
-  apply det_of_block_triangular,
-  { intros i j hij, exact h (fin.coe_fin_lt.mp hij) },
-  { intro i, exact fin.is_lt (b i) }
+  refine h.det.trans (prod_subset (subset_univ _) $ λ a _ ha, _),
+  have : is_empty {i // b i = a} := ⟨λ i, ha $ mem_image.2 ⟨i, mem_univ _, i.2⟩⟩,
+  exactI det_is_empty,
 end
 
-lemma det_of_upper_triangular {n : ℕ} (M : matrix (fin n) (fin n) R) (h : M.block_triangular id) :
-  M.det = ∏ i : (fin n), M i i :=
+lemma det_of_upper_triangular [preorder m] (h : M.block_triangular id) : M.det = ∏ i : m, M i i :=
 begin
-  rw det_of_block_triangular M coe h _ _,
-  ext i,
-  have h2 : ∀ (j : {a // id a = i}), j = ⟨i, rfl⟩ :=
-    λ (j : {a // id a = i}), subtype.ext j.property,
-  haveI : unique {a // id a = i} := ⟨⟨⟨i, rfl⟩⟩, h2⟩,
-  simp [h2 default]
+  haveI : decidable_eq R := classical.dec_eq _,
+  simp_rw [h.det, image_id, det_to_square_block_id],
 end
 
-lemma det_of_lower_triangular {n : ℕ} (M : matrix (fin n) (fin n) R)
-  (h : ∀ (i j : fin n), i < j → M i j = 0) :
-  M.det = ∏ i : (fin n), M i i :=
-begin
-  rw ← det_transpose,
-  exact det_of_upper_triangular _ (λ (i j : fin n) (hji : j < i), h j i hji)
-end
+lemma det_of_lower_triangular [preorder m] (M : matrix m m R) (h : M.block_triangular to_dual) :
+  M.det = ∏ i : m, M i i :=
+by { rw ←det_transpose, exact det_of_upper_triangular h.transpose }
 
 end matrix

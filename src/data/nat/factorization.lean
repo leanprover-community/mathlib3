d69e55a6146c3ec17654610d78b194bf429972d8
Stuart Presnell <stuart.presnell@gmail.com>
2022-05-08 18:26:46 +0100

    Remove brackets

--- src/data/nat/factorization.lean
+++ src/data/nat/factorization.lean
@@ -217,11 +217,11 @@ begin
   simp [list.eq_of_mem_repeat hq],
 end
 
-lemma pow_factorization_le {n : ℕ} (p : ℕ) (hn : 0 < n) : p ^ (n.factorization) p ≤ n :=
+lemma pow_factorization_le {n : ℕ} (p : ℕ) (hn : 0 < n) : p ^ n.factorization p ≤ n :=
 le_of_dvd hn (nat.pow_factorization_dvd n p)
 
 lemma div_pow_factorization_ne_zero {n : ℕ} (p : ℕ) (hn : 0 < n) :
-  n / p ^ (n.factorization) p ≠ 0 :=
+  n / p ^ n.factorization p ≠ 0 :=
 begin
   by_cases pp : nat.prime p,
   { apply mt (nat.div_eq_zero_iff (pow_pos (prime.pos pp) _)).1,
@@ -296,7 +296,7 @@ begin
 end
 
 lemma not_dvd_div_pow_factorization {n p : ℕ} (hp : prime p) (h: p ∣ n) (hn : 0 < n) :
-  ¬p ∣ n / p ^ (n.factorization) p :=
+  ¬p ∣ n / p ^ n.factorization p :=
 begin
   rw [nat.prime.dvd_iff_one_le_factorization hp (div_pow_factorization_ne_zero p hn),
     nat.factorization_div (nat.pow_factorization_dvd n p)],

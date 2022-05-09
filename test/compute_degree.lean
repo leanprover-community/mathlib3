05b357bf1762d188fa381e63c5ca90728e64afbd
adomani <adomani@gmail.com>
2022-05-09 13:47:40 +0200

    by's

--- test/compute_degree.lean
+++ test/compute_degree.lean
@@ -8,16 +8,12 @@ variables {R : Type*} [semiring R] [nontrivial R] {f g h : R[X]} {a b c d e : R}
 example {h : C d ≠ 0} (f10 : f.nat_degree ≤ 10) :
   nat_degree (monomial 5 c * monomial 1 c + monomial 7 d + monomial 9 1 +
     C a * X ^ 0 + C b * X ^ 5 + C c * X ^ 2 + X ^ 10 + C e * X) = 10 :=
-begin
-  compute_degree,
-end
+by compute_degree
 
 example {h : C d ≠ 0} (f10 : f.nat_degree ≤ 10) :
   nat_degree (monomial 1 c + monomial 5 c * monomial 1 c + monomial 7 d + monomial 9 1 +
     C a * X ^ 0 + C b * X ^ 5 + C c * X ^ 2 + X ^ 10 + C e * X) = 10 :=
-begin
-  compute_degree,
-end
+by compute_degree
 
 /--  Michael's example. -/
 example {F} [field F] : nat_degree (X ^ 4 + C 1 : F[X]) = 4 :=
@@ -31,25 +27,19 @@ by compute_degree
 --  prevent a noisy file
 /--  `Try this: exact nat_degree_X_pow _` -/
 example {F} [division_ring F] : nat_degree (X ^ 4 : F[X]) = 4 :=
-begin
-  compute_degree,
-end
+by compute_degree
 
 --  This test produces a suggestion.  I would like to uncomment it, but do not know how to
 --  prevent a noisy file
 /--  `Try this: exact nat_degree_C _` -/
 example {F} [field F] : nat_degree (C 4 : F[X]) = 0 :=
-begin
-  compute_degree,
-end
+by compute_degree
 
 --  This test produces a suggestion.  I would like to uncomment it, but do not know how to
 --  prevent a noisy file
 /--  `Try this: exact nat_degree_X` -/
 example {F} [field F] : nat_degree (X : F[X]) = 1 :=
-begin
-  compute_degree,
-end
+by compute_degree
 --/
 
 --  The first `compute_degree` fails since there are non-closed `ℕ`-expressions.
@@ -74,7 +64,7 @@ by compute_degree_le
 example : nat_degree (monomial 5 c * monomial 1 c + monomial 7 d +
   C a * X ^ 0 + C b * X ^ 5 + C c * X ^ 2 + X ^ 10 + C e * X : R[X]) ≤ 9 :=
 by compute_degree_le
--/
+--/
 
 example : nat_degree (monomial 5 c * monomial 1 c + monomial 7 d +
   C a * X ^ 0 + C b * X ^ 5 + C c * X ^ 2 + X ^ 10 + C e * X : R[X]) ≤ 11 :=

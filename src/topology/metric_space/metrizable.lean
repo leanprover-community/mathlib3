f8f6c8fcc4b3492846d0b48fa1c68cdb71daf544
Yury G. Kudryashov <urkud@urkud.name>
2022-05-09 15:09:44 -0400

    Update

--- src/topology/metric_space/metrizable.lean
+++ src/topology/metric_space/metrizable.lean
@@ -66,57 +66,60 @@ instance pseudo_metrizable_space.subtype [pseudo_metrizable_space X]
   (s : set X) : pseudo_metrizable_space s :=
 inducing_coe.pseudo_metrizable_space
 
+instance pseudo_metrizable_space_pi [Π i, pseudo_metrizable_space (π i)] :
+  pseudo_metrizable_space (Π i, π i) :=
+by { letI := λ i, pseudo_metrizable_space_pseudo_metric (π i), apply_instance }
+
 /-- A topological space is metrizable if there exists a metric space structure compatible with the
 topology. To endow such a space with a compatible distance, use
-`letI : metric_space α := topological_space.metrizable_space_metric α` -/
-class metrizable_space (α : Type*) [t : topological_space α] : Prop :=
-(exists_metric : ∃ (m : metric_space α), m.to_uniform_space.to_topological_space = t)
+`letI : metric_space X := topological_space.metrizable_space_metric X` -/
+class metrizable_space (X : Type*) [t : topological_space X] : Prop :=
+(exists_metric : ∃ (m : metric_space X), m.to_uniform_space.to_topological_space = t)
 
 @[priority 100]
-instance _root_.metric_space.to_metrizable_space {α : Type*} [m : metric_space α] :
-  metrizable_space α :=
+instance _root_.metric_space.to_metrizable_space {X : Type*} [m : metric_space X] :
+  metrizable_space X :=
 ⟨⟨m, rfl⟩⟩
 
 @[priority 100]
-instance metrizable_space.to_pseudo_metrizable_space {α : Type*} [topological_space α]
-  [h : metrizable_space α] : pseudo_metrizable_space α :=
+instance metrizable_space.to_pseudo_metrizable_space [h : metrizable_space X] :
+  pseudo_metrizable_space X :=
 ⟨let ⟨m, hm⟩ := h.1 in ⟨m.to_pseudo_metric_space, hm⟩⟩
 
 /-- Construct on a metrizable space a metric compatible with the topology. -/
-noncomputable def metrizable_space_metric
-  (α : Type*) [topological_space α] [h : metrizable_space α] :
-  metric_space α :=
+noncomputable def metrizable_space_metric (X : Type*) [topological_space X]
+  [h : metrizable_space X] :
+  metric_space X :=
 h.exists_metric.some.replace_topology h.exists_metric.some_spec.symm
 
 @[priority 100]
-instance t2_space_of_metrizable_space
-  (α : Type*) [topological_space α] [metrizable_space α] : t2_space α :=
-by { letI : metric_space α := metrizable_space_metric α, apply_instance }
+instance t2_space_of_metrizable_space [metrizable_space X] : t2_space X :=
+by { letI : metric_space X := metrizable_space_metric X, apply_instance }
 
-instance metrizable_space_prod (α : Type*) [topological_space α] [metrizable_space α]
-  (β : Type*) [topological_space β] [metrizable_space β] :
-  metrizable_space (α × β) :=
+instance metrizable_space_prod [metrizable_space X] [metrizable_space Y] :
+  metrizable_space (X × Y) :=
 begin
-  letI : metric_space α := metrizable_space_metric α,
-  letI : metric_space β := metrizable_space_metric β,
+  letI : metric_space X := metrizable_space_metric X,
+  letI : metric_space Y := metrizable_space_metric Y,
   apply_instance
 end
 
-instance metrizable_space.subtype {α : Type*} [topological_space α] [metrizable_space α]
-  (s : set α) : metrizable_space s :=
-by { letI := metrizable_space_metric α, apply_instance }
-
 /-- Given an embedding of a topological space into a metrizable space, the source space is also
 metrizable. -/
-lemma _root_.embedding.metrizable_space {α β : Type*} [topological_space α] [topological_space β]
-  [metrizable_space β] {f : α → β} (hf : embedding f) :
-  metrizable_space α :=
+lemma _root_.embedding.metrizable_space [metrizable_space Y] {f : X → Y} (hf : embedding f) :
+  metrizable_space X :=
 begin
-  letI : metric_space β := metrizable_space_metric β,
+  letI : metric_space Y := metrizable_space_metric Y,
   exact ⟨⟨hf.comap_metric_space f, rfl⟩⟩
 end
 
-variables (X : Type*) [topological_space X] [normal_space X] [second_countable_topology X]
+instance metrizable_space.subtype [metrizable_space X] (s : set X) : metrizable_space s :=
+embedding_subtype_coe.metrizable_space
+
+instance metrizable_space_pi [Π i, metrizable_space (π i)] : metrizable_space (Π i, π i) :=
+by { letI := λ i, metrizable_space_metric (π i), apply_instance }
+
+variables (X) [normal_space X] [second_countable_topology X]
 
 /-- A normal topological space with second countable topology can be embedded into `l^∞ = ℕ →ᵇ ℝ`.
 -/

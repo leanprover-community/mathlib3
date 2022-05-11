fe97caed5d3f80bb143f94107530b75eb8efe8fe
Violeta Hernández <vi.hdz.p@gmail.com>
2022-05-11 11:56:59 -0500

    fix

--- src/set_theory/game/nim.lean
+++ src/set_theory/game/nim.lean
@@ -107,11 +107,11 @@ lemma move_right_nim_heq (O : ordinal) : (nim O).move_right == λ i : O.out.α, 
 by { rw nim_def, refl }
 
 /-- Turns an ordinal less than `O` into a left move for `nim O` and viceversa. -/
-noncomputable def to_left_moves_nim {O : ordinal} : {O' // O' < O} ≃ (nim O).left_moves :=
+noncomputable def to_left_moves_nim {O : ordinal} : set.Iio O ≃ (nim O).left_moves :=
 (Iio_order_iso_out O).to_equiv.trans (equiv.cast (left_moves_nim O).symm)
 
 /-- Turns an ordinal less than `O` into a right move for `nim O` and viceversa. -/
-noncomputable def to_right_moves_nim {O : ordinal} : {O' // O' < O} ≃ (nim O).right_moves :=
+noncomputable def to_right_moves_nim {O : ordinal} : set.Iio O ≃ (nim O).right_moves :=
 (Iio_order_iso_out O).to_equiv.trans (equiv.cast (right_moves_nim O).symm)
 
 @[simp] theorem to_left_moves_nim_symm_lt {O : ordinal} (i : (nim O).left_moves) :

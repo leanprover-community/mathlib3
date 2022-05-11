820504d7df91d3939cea402b0f22423cc185dd12
Kyle Miller <kmill31415@gmail.com>
2022-05-11 08:07:59 -0700

    a little refinement and renaming

--- src/combinatorics/simple_graph/eulerian.lean
+++ src/combinatorics/simple_graph/eulerian.lean
@@ -59,7 +59,7 @@ begin
   change f.card = _,
   congr',
   ext e,
-  simp,
+  simp only [finset.mem_mk, multiset.mem_coe, mem_edge_finset],
   split,
   apply edges_subset_edge_set p,
   intro he,
@@ -80,7 +80,7 @@ begin
   have : f ⊆ G.edge_finset,
   { intros e he,
     rw mem_edge_finset,
-    simp at he,
+    simp only [finset.mem_mk, multiset.mem_coe] at he,
     exact edges_subset_edge_set p he, },
   have : f = G.edge_finset,
   { apply finset.eq_of_subset_of_card_le this,
@@ -105,9 +105,10 @@ theorem list.countp_cons {α : Type*} (p : α → Prop) [decidable_pred p] (a : 
   list.countp p (a :: l) = list.countp p l + ite (p a) 1 0 :=
 by { by_cases h : p a; simp [h] }
 
-lemma is_trail.even_countp_edges {u v : V} {p : G.walk u v} (ht : p.is_trail) (x : V) :
-  (u ≠ v → x ≠ u ∧ x ≠ v) ↔ even (p.edges.countp (λ e, x ∈ e)) :=
+lemma is_trail.even_countp_edges_iff {u v : V} {p : G.walk u v} (ht : p.is_trail) (x : V) :
+  even (p.edges.countp (λ e, x ∈ e)) ↔ (u ≠ v → x ≠ u ∧ x ≠ v) :=
 begin
+  symmetry, -- I'd proved it the other way first. TODO: fix this
   induction p with u u v w huv p ih,
   { simp, },
   { rw [cons_is_trail_iff] at ht,
@@ -143,14 +144,12 @@ begin
   simp [mk_mem_incidence_set_iff],
 end
 
-lemma is_eulerian.even_degree {x u v : V} {p : G.walk u v} (ht : p.is_eulerian)
+lemma is_eulerian.even_degree_iff {x u v : V} {p : G.walk u v} (ht : p.is_eulerian)
   [fintype V] [decidable_rel G.adj] :
-  (u ≠ v → x ≠ u ∧ x ≠ v) ↔ even (G.degree x) :=
+  even (G.degree x) ↔ (u ≠ v → x ≠ u ∧ x ≠ v) :=
 begin
-  convert ht.is_trail.even_countp_edges x,
-  have := ht.is_trail.edges_nodup,
-  rw ← multiset.coe_nodup at this,
-  let edges : finset (sym2 V) := finset.mk ↑p.edges this,
+  convert ht.is_trail.even_countp_edges_iff x,
+  let edges : finset (sym2 V) := finset.mk ↑p.edges ht.is_trail.edges_nodup,
   rw [← multiset.coe_countp, multiset.countp_eq_card_filter, ← card_incidence_finset_eq_degree],
   change multiset.card _ = _,
   congr' 1,
@@ -169,12 +168,12 @@ begin
   exact (finset.eq_of_subset_of_card_le h1 h2.ge).symm,
 end
 
-lemma is_eulerian.degrees [fintype V] [decidable_rel G.adj]
+lemma is_eulerian.card_odd_degree [fintype V] [decidable_rel G.adj]
   {u v : V} {p : G.walk u v} (ht : p.is_eulerian) :
   ((finset.univ : finset V).filter (λ v, odd (G.degree v))).card ∈ ({0, 2} : set ℕ) :=
 begin
   simp only [nat.odd_iff_not_even, set.mem_insert_iff, finset.card_eq_zero, set.mem_singleton_iff],
-  simp only [←ht.even_degree, ne.def, not_forall, not_and, not_not, exists_prop],
+  simp only [ht.even_degree_iff, ne.def, not_forall, not_and, not_not, exists_prop],
   obtain (rfl | hn) := eq_or_ne u v,
   { left,
     simp, },
@@ -234,8 +233,7 @@ def edges : list (verts × verts) :=
 [ (V1, B1), (V1, B2), (V1, B3), (V1, B4), (V1, B5),
   (B1, V2), (B2, V2), (B3, V4), (B4, V3), (B5, V3),
   (V2, B6), (B6, V4),
-  (V3, B7), (B7, V4)
-]
+  (V3, B7), (B7, V4) ]
 
 def adj (v w : verts) : bool := ((v, w) ∈ edges) || ((w, v) ∈ edges)
 
@@ -244,17 +242,36 @@ def graph : simple_graph verts :=
 { adj := λ v w, adj v w,
   symm := begin
     dsimp [symmetric, adj],
-    exact dec_trivial,
+    dec_trivial,
   end,
   loopless := begin
     dsimp [irreflexive, adj],
-    exact dec_trivial
+    dec_trivial
   end }
-.
 
 instance : decidable_rel graph.adj :=
-λ a b,
-if h : adj a b then decidable.is_true h else decidable.is_false h
+λ a b, if h : adj a b then decidable.is_true h else decidable.is_false h
+
+def degrees : verts → ℕ
+| V1 := 5 | V2 := 3 | V3 := 3 | V4 := 3
+| B1 := 2 | B2 := 2 | B3 := 2 | B4 := 2 | B5 := 2 | B6 := 2 | B7 := 2
+
+lemma degrees_ok (v : verts) : graph.degree v = degrees v :=
+begin
+  cases v; refl,
+end
+
+theorem no_euler_trail {u v : verts} (p : graph.walk u v) (h : p.is_eulerian) : false :=
+begin
+  have : finset.filter (λ (v : verts), odd (graph.degree v)) finset.univ = {verts.V1, verts.V2, verts.V3, verts.V4},
+  { ext w,
+    simp,
+    cases w; simp [degrees_ok, degrees], },
+  have h := h.card_odd_degree,
+  rw [this] at h,
+  simp at h,
+  norm_num at h,
+end
 
 /- Was trying to make a dec_trivial proof, but it didn't work.
 
@@ -278,62 +295,6 @@ begin
 end
 -/
 -/
-
-def neighbor_sets : verts → finset verts
-| V1 := {B1, B2, B3, B4, B5}
-| V2 := {B1, B2, B6}
-| V3 := {B4, B5, B7}
-| V4 := {B3, B6, B7}
-| B1 := {V1, V2}
-| B2 := {V1, V2}
-| B3 := {V1, V4}
-| B4 := {V1, V3}
-| B5 := {V1, V3}
-| B6 := {V2, V4}
-| B7 := {V3, V4}
-
-lemma neighbor_sets_ok (v : verts) : graph.neighbor_finset v = neighbor_sets v :=
-begin
-  cases v; refl,
-/-  apply finset.ext;
-  intro w;
-  simp only [neighbor_sets, adj, mem_neighbor_finset, graph_adj, bor_coe_iff, bool.of_to_bool_iff, finset.mem_insert,
-    finset.mem_singleton];
-  simp only [edges, list.mem_cons_iff, prod.mk.inj_iff, false_and, eq_self_iff_true, true_and, list.not_mem_nil, or_false,
-    false_or, and_false, and_true];
-  cases w; simp,-/
-end
-.
-
-def degrees : verts → ℕ
-| V1 := 5
-| V2 := 3
-| V3 := 3
-| V4 := 3
-| B1 := 2
-| B2 := 2
-| B3 := 2
-| B4 := 2
-| B5 := 2
-| B6 := 2
-| B7 := 2
-
-lemma degrees_ok (v : verts) : graph.degree v = degrees v :=
-begin
-  cases v; refl,
-end
-
-theorem no_euler_trail {u v : verts} (p : graph.walk u v) (h : p.is_eulerian) : false :=
-begin
-  have : finset.filter (λ (v : verts), odd (graph.degree v)) finset.univ = {verts.V1, verts.V2, verts.V3, verts.V4},
-  { ext w,
-    simp,
-    cases w; simp [degrees_ok, degrees], },
-  have h := h.degrees,
-  rw [this] at h,
-  simp at h,
-  norm_num at h,
-end
 
 end konigsberg
 

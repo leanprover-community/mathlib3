b02b40fd58d5a0c3158b81a65f98b7900fefcc14
YaelDillies <yael.dillies@gmail.com>
2022-05-11 14:14:27 +0100

    has_distrib_neg instances

--- src/algebra/ring/basic.lean
+++ src/algebra/ring/basic.lean
@@ -927,6 +927,37 @@ lemma neg_mul_eq_mul_neg (a b : α) : -(a * b) = a * -b :=
 lemma neg_mul_comm (a b : α) : -a * b = a * -b :=
 by simp
 
+/-- A type endowed with `-` and `*` has distributive negation, if it admits an injective map that
+preserves `-` and `*` to a type which has distributive negation. -/
+protected def function.injective.has_distrib_neg [has_neg β] [has_mul β] (f : β → α)
+  (hf : injective f) (neg :  ∀ a, f (-a) = -f a) (mul : ∀ a b, f (a * b) = f a * f b) :
+  has_distrib_neg β :=
+{ neg_mul := λ x y, hf $ by erw [neg, mul, neg, neg_mul, mul],
+  mul_neg := λ x y, hf $ by erw [neg, mul, neg, mul_neg, mul],
+  ..hf.has_involutive_neg _ neg, ..‹has_mul β› }
+
+/-- A type endowed with `-` and `*` has distributive negation, if it admits a surjective map that
+preserves `-` and `*` from a type which has distributive negation. -/
+protected def function.surjective.has_distrib_neg [has_neg β] [has_mul β] (f : α → β)
+  (hf : surjective f) (neg :  ∀ a, f (-a) = -f a) (mul : ∀ a b, f (a * b) = f a * f b) :
+  has_distrib_neg β :=
+{ neg_mul := hf.forall₂.2 $ λ x y, by { erw [←neg, ← mul, neg_mul, neg, mul], refl },
+  mul_neg := hf.forall₂.2 $ λ x y, by { erw [←neg, ← mul, mul_neg, neg, mul], refl },
+  ..hf.has_involutive_neg _ neg, ..‹has_mul β› }
+
+namespace add_opposite
+
+instance : has_distrib_neg αᵃᵒᵖ := unop_injective.has_distrib_neg _ unop_neg unop_mul
+
+end add_opposite
+
+open mul_opposite
+
+instance : has_distrib_neg αᵐᵒᵖ :=
+{ neg_mul := λ _ _, unop_injective $ mul_neg _ _,
+  mul_neg := λ _ _, unop_injective $ neg_mul _ _,
+  ..mul_opposite.has_involutive_neg _ }
+
 end has_mul
 
 section mul_one_class

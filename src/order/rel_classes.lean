ab0f70883c4bf0775b095feed3434f3a363698fa
Violeta Hernández <vi.hdz.p@gmail.com>
2022-05-12 00:50:59 -0500

    Delete reduntant thms, use dot notation

--- src/order/rel_classes.lean
+++ src/order/rel_classes.lean
@@ -76,22 +76,9 @@ protected theorem is_asymm.is_irrefl [is_asymm α r] : is_irrefl α r :=
 protected theorem is_total.is_trichotomous (r) [is_total α r] : is_trichotomous α r :=
 ⟨λ a b, or.left_comm.1 (or.inr $ total_of r a b)⟩
 
-/-- Note this cannot be an instance as it would loop with itself. -/
-theorem is_refl_swap {α : Type u} (r : α → α → Prop) [is_refl α r] :
-  is_refl α (λ x y, r y x) :=
-⟨@refl α r _⟩
-
-/-- Note this cannot be an instance as it would loop with itself. -/
-theorem is_irrefl_swap {α : Type u} (r : α → α → Prop) [is_irrefl α r] :
-  is_irrefl α (λ x y, r y x) :=
+protected theorem is_irrefl.is_refl (r) [is_irrefl α r] : is_refl α (λ x y, ¬ r x y) :=
 ⟨@irrefl α r _⟩
-
-theorem is_refl_of_irrefl {α : Type u} (r : α → α → Prop) [is_irrefl α r] :
-  is_refl α (λ x y, ¬ r x y) :=
-⟨@irrefl α r _⟩
-
-theorem is_irrefl_of_refl {α : Type u} (r : α → α → Prop) [is_refl α r] :
-  is_irrefl α (λ x y, ¬ r x y) :=
+protected theorem is_refl.is_irrefl (r) [is_refl α r] : is_irrefl α (λ x y, ¬ r x y) :=
 ⟨λ a, not_not_intro (is_refl.refl a)⟩
 
 @[priority 100]  -- see Note [lower instance priority]

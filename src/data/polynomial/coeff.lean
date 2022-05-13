37dd6bc7cf22d32b6d36fdd999c3b93b25350e1d
Thomas Browning <tb65536@uw.edu>
2022-05-12 22:19:14 -0700

    delete

--- src/data/polynomial/coeff.lean
+++ src/data/polynomial/coeff.lean
@@ -189,9 +189,6 @@ pow_one (X : R[X]) ▸ mul_X_pow_injective 1
 lemma C_mul_X_pow_eq_monomial (c : R) (n : ℕ) : C c * X^n = monomial n c :=
 by { ext1, rw [monomial_eq_smul_X, coeff_smul, coeff_C_mul, smul_eq_mul] }
 
-lemma support_mul_X_pow (c : R) (n : ℕ) (H : c ≠ 0) : (C c * X^n).support = singleton n :=
-by rw [C_mul_X_pow_eq_monomial, support_monomial n c H]
-
 lemma support_C_mul_X_pow' {c : R} {n : ℕ} : (C c * X^n).support ⊆ singleton n :=
 by { rw [C_mul_X_pow_eq_monomial], exact support_monomial' n c }
 

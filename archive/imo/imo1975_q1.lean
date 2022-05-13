f1aea2e7b6f0740e6e437ec94b1c14cdc0d5e073
MantasBaksys <baksys.mantas@gmail.com>
2022-05-13 14:22:54 +0100

    style updates

--- archive/imo/imo1975_q1.lean
+++ archive/imo/imo1975_q1.lean
@@ -13,23 +13,22 @@ import algebra.big_operators.ring
 
 Let `x₁, x₂, ... , xₙ` and `y₁, y₂, ... , yₙ` be two sequences of real numbers, such that
 `x₁ ≥ x₂ ≥ ... ≥ xₙ` and `y₁ ≥ y₂ ≥ ... ≥ yₙ`. Prove that if `z₁, z₂, ... , zₙ` is any permutation
-of `y₁, y₂, ... , yₙ`, then
-`∑ i in finset.Icc 1 n, (x i - y i) ^ 2 ≤ ∑ i in finset.Icc 1 n, (x i - z i) ^ 2`
+of `y₁, y₂, ... , yₙ`, then `∑ (xᵢ - yᵢ)^2 ≤ ∑ (xᵢ - zᵢ)^2`
 
 # Solution
 
 Firstly, we expand the squares withing both sums and distribute into separate finite sums. Then,
-noting that `∑ i in finset.Icc 1 n, (y i) ^ 2 = ∑ i in finset.Icc 1 n, (z i) ^ 2`, it remains to
-prove that `∑ i in finset.Icc 1 n, x i * z i ≤ ∑ i in finset.Icc 1 n, x i * y i`, which is true by
-the Rearrangement Inequality
+noting that `∑ yᵢ ^ 2 = ∑ zᵢ ^ 2`, it remains to prove that `∑ xᵢ * zᵢ ≤ ∑ xᵢ * yᵢ`, which is true
+by the Rearrangement Inequality
 -/
 
 open_locale big_operators
 
 /-- Let `n` be a natural number, `x` and `y` be as in the problem statement and `σ` be the
 permutation of natural numbers such that `z = y ∘ σ` -/
-variables (n : ℕ) (x y : ℕ → ℝ) (σ : equiv.perm ℕ) (hx : antitone_on x (finset.Icc 1 n))
-  (hy : antitone_on y (finset.Icc 1 n)) (hσ : {x | σ x ≠ x} ⊆ finset.Icc 1 n)
+variables (n : ℕ) (σ : equiv.perm ℕ) (hσ : {x | σ x ≠ x} ⊆ finset.Icc 1 n) (x y : ℕ → ℝ)
+variables (hx : antitone_on x (finset.Icc 1 n))
+variables (hy : antitone_on y (finset.Icc 1 n))
 include hx hy hσ
 
 theorem IMO_1975_Q1 :

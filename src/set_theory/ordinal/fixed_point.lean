edc260b3cb455d2e785c9eb09fd9008c22ce128c
Violeta Hernández <vi.hdz.p@gmail.com>
2022-05-13 15:10:03 -0500

    fix

--- src/set_theory/ordinal/fixed_point.lean
+++ src/set_theory/ordinal/fixed_point.lean
@@ -561,7 +561,7 @@ end
 theorem mul_eq_right_iff_opow_omega_dvd {a b : ordinal} : a * b = b ↔ a ^ omega ∣ b :=
 begin
   cases eq_zero_or_pos a with ha ha,
-  { rw [ha, zero_mul, zero_opow omega_ne_zero, zero_dvd],
+  { rw [ha, zero_mul, zero_opow omega_ne_zero, zero_dvd_iff],
     exact eq_comm },
   refine ⟨λ hab, _, λ h, _⟩,
   { rw dvd_iff_mod_eq_zero,
@@ -569,7 +569,7 @@ begin
       add_left_cancel] at hab,
     cases eq_zero_or_opow_omega_le_of_mul_eq_right hab with hab hab,
     { exact hab },
-    refine (not_lt_of_le hab (mod_lt b (opow_ne_zero omega _))).elim,
+    refine (hab.not_lt (mod_lt b (opow_ne_zero omega _))).elim,
     rwa ←ordinal.pos_iff_ne_zero },
   cases h with c hc,
   rw [hc, ←mul_assoc, ←opow_one_add, one_add_omega]

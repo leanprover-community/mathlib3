e94931bf6632921c0695224df300f7086aca15df
Violeta Hernández <vi.hdz.p@gmail.com>
2022-05-14 14:17:33 -0500

    add todo

--- src/set_theory/game/impartial.lean
+++ src/set_theory/game/impartial.lean
@@ -164,6 +164,7 @@ by rw [←zero_le_neg_iff, le_congr_right (neg_equiv_self G)]
 lemma lf_zero_iff {G : pgame} [G.impartial] : G ⧏ 0 ↔ 0 ⧏ G :=
 by rw [←zero_lf_neg_iff, lf_congr_right (neg_equiv_self G)]
 
+-- TODO: remove, this is just `false ↔ false`.
 lemma lt_zero_iff {G : pgame} [G.impartial] : G < 0 ↔ 0 < G :=
 by rw [←zero_lt_neg_iff, lt_congr_right (neg_equiv_self G)]
 

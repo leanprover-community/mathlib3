e78bb148d11177d73b948b99edc270f2e255bdf0
Yury G. Kudryashov <urkud@urkud.name>
2022-05-15 16:30:31 -0400

    Snapshot

--- src/algebra/order/monoid.lean
+++ src/algebra/order/monoid.lean
@@ -213,9 +213,11 @@ instance [partial_order α] : partial_order (with_zero α) := with_bot.partial_o
 
 instance [preorder α] : order_bot (with_zero α) := with_bot.order_bot
 
-lemma zero_le [partial_order α] (a : with_zero α) : 0 ≤ a := order_bot.bot_le a
+lemma zero_le [preorder α] (a : with_zero α) : 0 ≤ a := bot_le
 
 lemma zero_lt_coe [preorder α] (a : α) : (0 : with_zero α) < a := with_bot.bot_lt_coe a
+
+lemma zero_eq_bot [preorder α] : (0 : with_zero α) = ⊥ := rfl
 
 @[simp, norm_cast] lemma coe_lt_coe [preorder α] {a b : α} : (a : with_zero α) < b ↔ a < b :=
 with_bot.coe_lt_coe
@@ -227,30 +229,28 @@ instance [lattice α] : lattice (with_zero α) := with_bot.lattice
 
 instance [linear_order α] : linear_order (with_zero α) := with_bot.linear_order
 
-lemma mul_le_mul_left {α : Type u} [has_mul α] [preorder α]
+instance covariant_class_mul_le {α : Type u} [has_mul α] [preorder α]
   [covariant_class α α (*) (≤)] :
-  ∀ (a b : with_zero α),
-    a ≤ b → ∀ (c : with_zero α), c * a ≤ c * b :=
+  covariant_class (with_zero α) (with_zero α) (*) (≤) :=
 begin
-  rintro (_ | a) (_ | b) h (_ | c);
-  try { exact λ f hf, option.no_confusion hf },
-  { exact false.elim (not_lt_of_le h (with_zero.zero_lt_coe a))},
-  { simp_rw [some_eq_coe] at h ⊢,
-    norm_cast at h ⊢,
-    exact covariant_class.elim _ h }
+  refine ⟨λ a b c hbc, _⟩,
+  induction a using with_zero.rec_zero_coe, { exact zero_le _ },
+  induction b using with_zero.rec_zero_coe, { exact zero_le _ },
+  rcases with_bot.coe_le_iff.1 hbc with ⟨c, rfl, hbc'⟩,
+  rw [← coe_mul, ← coe_mul, coe_le_coe],
+  exact mul_le_mul_left' hbc' a
 end
 
-lemma lt_of_mul_lt_mul_left {α : Type u} [has_mul α] [partial_order α]
+instance contravariant_class_mul_lt {α : Type u} [has_mul α] [partial_order α]
   [contravariant_class α α (*) (<)] :
-  ∀ (a b c : with_zero α), a * b < a * c → b < c :=
+  contravariant_class (with_zero α) (with_zero α) (*) (<) :=
 begin
-  rintro (_ | a) (_ | b) (_ | c) h;
-  try { exact false.elim (lt_irrefl none h) },
-  { exact with_zero.zero_lt_coe c },
-  { exact false.elim (not_le_of_lt h (with_zero.zero_le _)) },
-  { simp_rw [some_eq_coe] at h ⊢,
-    norm_cast at h ⊢,
-    apply lt_of_mul_lt_mul_left' h }
+  refine ⟨λ a b c h, _⟩,
+  have := ((zero_le _).trans_lt h).ne',
+  lift a to α using left_ne_zero_of_mul this,
+  lift c to α using right_ne_zero_of_mul this,
+  induction b using with_zero.rec_zero_coe,
+  exacts [zero_lt_coe _, coe_lt_coe.mpr (lt_of_mul_lt_mul_left' h)]
 end
 
 @[simp] lemma le_max_iff [linear_order α] {a b c : α} :
@@ -262,9 +262,27 @@ by simp only [with_zero.coe_le_coe, le_max_iff]
 by simp only [with_zero.coe_le_coe, min_le_iff]
 
 instance [ordered_comm_monoid α] : ordered_comm_monoid (with_zero α) :=
-{ mul_le_mul_left := with_zero.mul_le_mul_left,
+{ mul_le_mul_left := λ _ _, mul_le_mul_left',
   ..with_zero.comm_monoid_with_zero,
   ..with_zero.partial_order }
+
+protected lemma covariant_class_add_le [add_zero_class α] [preorder α]
+  [covariant_class α α (+) (≤)] (h : ∀ a : α, 0 ≤ a) :
+  covariant_class (with_zero α) (with_zero α) (+) (≤) :=
+begin
+  refine ⟨λ a b c hbc, _⟩,
+  induction a using with_zero.rec_zero_coe,
+  { rwa [zero_add, zero_add] },
+  induction b using with_zero.rec_zero_coe,
+  { rw [add_zero],
+    induction c using with_zero.rec_zero_coe,
+    { rw [add_zero] },
+    { rw [← coe_add, coe_le_coe],
+      exact le_add_of_nonneg_right (h _) } },
+  { rcases with_bot.coe_le_iff.1 hbc with ⟨c, rfl, hbc'⟩,
+    rw [← coe_add, ← coe_add, coe_le_coe],
+    exact add_le_add_left hbc' a }
+end
 
 /-
 Note 1 : the below is not an instance because it requires `zero_le`. It seems
@@ -280,25 +298,9 @@ See note [reducible non-instances].
 -/
 @[reducible] protected def ordered_add_comm_monoid [ordered_add_comm_monoid α]
   (zero_le : ∀ a : α, 0 ≤ a) : ordered_add_comm_monoid (with_zero α) :=
-begin
-  suffices, refine
-  { add_le_add_left := this,
-    ..with_zero.partial_order,
-    ..with_zero.add_comm_monoid, .. },
-  { intros a b h c ca h₂,
-    cases b with b,
-    { rw le_antisymm h bot_le at h₂,
-      exact ⟨_, h₂, le_rfl⟩ },
-    cases a with a,
-    { change c + 0 = some ca at h₂,
-      simp at h₂, simp [h₂],
-      exact ⟨_, rfl, by simpa using add_le_add_left (zero_le b) _⟩ },
-    { simp at h,
-      cases c with c; change some _ = _ at h₂;
-        simp [-add_comm] at h₂; subst ca; refine ⟨_, rfl, _⟩,
-      { exact h },
-      { exact add_le_add_left h _ } } }
-end
+{ add_le_add_left := @add_le_add_left _ _ _ (with_zero.covariant_class_add_le zero_le),
+  ..with_zero.partial_order,
+  ..with_zero.add_comm_monoid, .. }
 
 end with_zero
 
@@ -372,7 +374,7 @@ iff.intro ne_of_gt $ assume hne, lt_of_le_of_ne (one_le _) hne.symm
 @[to_additive] lemma eq_one_or_one_lt : a = 1 ∨ 1 < a :=
 (one_le a).eq_or_lt.imp_left eq.symm
 
-@[to_additive] lemma exists_pos_mul_of_lt (h : a < b) : ∃ c > 1, a * c = b :=
+@[to_additive] lemma exists_one_lt_mul_of_lt (h : a < b) : ∃ c (hc : 1 < c), a * c = b :=
 begin
   obtain ⟨c, hc⟩ := le_iff_exists_mul.1 h.le,
   refine ⟨c, one_lt_iff_ne_one.2 _, hc.symm⟩,

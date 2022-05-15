c1091050947ce7ae78ed9af018fd1853a53e5654
Eric Rodriguez <ericrboidi@gmail.com>
2022-05-15 14:04:27 +0000

    chore(logic/equiv): golf equiv.subtype_equiv (#14125)
    The naming is a bit all over the place, but I will fix this in a later PR.
    
    
    
    Co-authored-by: Eric Rodriguez <37984851+ericrbg@users.noreply.github.com>

--- src/logic/equiv/basic.lean
+++ src/logic/equiv/basic.lean
@@ -1319,10 +1319,10 @@ at corresponding points, then `{a // p a}` is equivalent to `{b // q b}`.
 For the statement where `α = β`, that is, `e : perm α`, see `perm.subtype_perm`. -/
 def subtype_equiv {p : α → Prop} {q : β → Prop}
   (e : α ≃ β) (h : ∀ a, p a ↔ q (e a)) : {a : α // p a} ≃ {b : β // q b} :=
-⟨λ x, ⟨e x, (h _).1 x.2⟩,
- λ y, ⟨e.symm y, (h _).2 (by { simp, exact y.2 })⟩,
- λ ⟨x, h⟩, subtype.ext_val $ by simp,
- λ ⟨y, h⟩, subtype.ext_val $ by simp⟩
+{ to_fun    := λ a, ⟨e a, (h _).mp a.prop⟩,
+  inv_fun   := λ b, ⟨e.symm b, (h _).mpr ((e.apply_symm_apply b).symm ▸ b.prop)⟩,
+  left_inv  := λ a, subtype.ext $ by simp,
+  right_inv := λ b, subtype.ext $ by simp }
 
 @[simp] lemma subtype_equiv_refl {p : α → Prop}
   (h : ∀ a, p a ↔ p (equiv.refl _ a) := λ a, iff.rfl) :

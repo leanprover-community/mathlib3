97fa93aa9e6b8525c3449eb08f66b06ef7eccdc1
Stuart Presnell <stuart.presnell@gmail.com>
2022-05-16 13:46:21 +0100

    Add `has_pow [0,1] ℕ`

--- src/data/set/intervals/instances.lean
+++ src/data/set/intervals/instances.lean
@@ -6,3 +6,5 @@ open set
 
 def prob_pow_Icc {α : Type*} [ordered_semiring α] (p : Icc (0:α) 1) (n : ℕ) : (Icc (0:α) 1) :=
 ⟨p^n, ⟨pow_nonneg p.2.1 n, pow_le_one n p.2.1 p.2.2⟩⟩
+
+instance {α : Type*} [ordered_semiring α] : has_pow (Icc (0:α) 1) ℕ := ⟨prob_pow_Icc⟩

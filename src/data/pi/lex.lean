3b839cb3a53d6eee8a3d062fab9b8bd3f1cd8376
YaelDillies <yael.dillies@gmail.com>
2022-05-16 14:20:12 +0100

    partially give up on notation

--- src/data/pi/lex.lean
+++ src/data/pi/lex.lean
@@ -39,15 +39,17 @@ instance {α : Type*} : Π [inhabited α], inhabited (lex α) := id
 protected def lex (x y : Π i, β i) : Prop :=
 ∃ i, (∀ j, r j i → x j = y j) ∧ s (x i) (y i)
 
+/- This unfortunately results in a type that isn't delta-reduced, so we keep the notation out of the
+basic API, just in case -/
 notation `Πₗ` binders `, ` r:(scoped p, lex (Π i, p i)) := r
 
 @[simp] lemma to_lex_apply (x : Π i, β i) (i : ι) : to_lex x i = x i := rfl
-@[simp] lemma of_lex_apply (x : Πₗ i, β i) (i : ι) : of_lex x i = x i := rfl
+@[simp] lemma of_lex_apply (x : lex (Π i, β i)) (i : ι) : of_lex x i = x i := rfl
 
-instance [has_lt ι] [∀ a, has_lt (β a)] : has_lt (Πₗ i, β i) := ⟨pi.lex (<) (λ _, (<))⟩
+instance [has_lt ι] [Π a, has_lt (β a)] : has_lt (lex (Π i, β i)) := ⟨pi.lex (<) (λ _, (<))⟩
 
 instance lex.is_strict_order [linear_order ι] [∀ a, partial_order (β a)] :
-  is_strict_order (Πₗ i, β i) (<) :=
+  is_strict_order (lex (Π i, β i)) (<) :=
 { irrefl := λ a ⟨k, hk₁, hk₂⟩, lt_irrefl (a k) hk₂,
   trans :=
     begin
@@ -58,12 +60,12 @@ instance lex.is_strict_order [linear_order ι] [∀ a, partial_order (β a)] :
         ⟨N₂, λ j hj, (lt_N₁ _ (hj.trans H)).trans (lt_N₂ _ hj), (lt_N₁ _ H).symm ▸ b_lt_c⟩]
     end }
 
-instance [linear_order ι] [∀ a, partial_order (β a)] : partial_order (Πₗ i, β i) :=
+instance [linear_order ι] [Π a, partial_order (β a)] : partial_order (lex (Π i, β i)) :=
 partial_order_of_SO (<)
 
 protected lemma lex.is_strict_total_order' [linear_order ι]
   (wf : well_founded ((<) : ι → ι → Prop)) [∀ a, linear_order (β a)] :
-  is_strict_total_order' (Πₗ i, β i) (<) :=
+  is_strict_total_order' (lex (Π i, β i)) (<) :=
 { trichotomous := λ a b,
     begin
       by_cases h : ∃ i, a i ≠ b i,
@@ -81,11 +83,11 @@ protected lemma lex.is_strict_total_order' [linear_order ι]
 This cannot be an instance, since it depends on the well-foundedness of `<`. -/
 protected noncomputable def lex.linear_order [linear_order ι]
   (wf : well_founded ((<) : ι → ι → Prop)) [∀ a, linear_order (β a)] :
-  linear_order (Πₗ i, β i) :=
+  linear_order (lex (Π i, β i)) :=
 @linear_order_of_STO' (Πₗ i, β i) (<) (pi.lex.is_strict_total_order' wf) (classical.dec_rel _)
 
 lemma lex.le_of_forall_le [linear_order ι]
-  (wf : well_founded ((<) : ι → ι → Prop)) [∀ a, linear_order (β a)] {a b : Πₗ i, β i}
+  (wf : well_founded ((<) : ι → ι → Prop)) [Π a, linear_order (β a)] {a b : lex (Π i, β i)}
   (h : ∀ i, a i ≤ b i) : a ≤ b :=
 begin
   letI : linear_order (Πₗ i, β i) := lex.linear_order wf,
@@ -95,7 +97,7 @@ end
 --we might want the analog of `pi.ordered_cancel_comm_monoid` as well in the future
 @[to_additive]
 instance lex.ordered_comm_group [linear_order ι] [∀ a, ordered_comm_group (β a)] :
-  ordered_comm_group (Πₗ i, β i) :=
+  ordered_comm_group (lex (Π i, β i)) :=
 { mul_le_mul_left := λ x y hxy z,
     hxy.elim
       (λ hxyz, hxyz ▸ le_rfl)

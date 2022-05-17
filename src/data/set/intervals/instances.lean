b794e9f0811b64ebd36e9b1e6e7bc60e88438a53
Stuart Presnell <stuart.presnell@gmail.com>
2022-05-17 16:50:31 +0100

    Draft: extend to `comm_monoid_with_zero`

--- src/data/set/intervals/instances.lean
+++ src/data/set/intervals/instances.lean
@@ -4,56 +4,56 @@ import algebra.group_power.order
 
 open set
 
-def prob_pow_Icc {α : Type*} [ordered_semiring α] (p : Icc (0:α) 1) (n : ℕ) : (Icc (0:α) 1) :=
-⟨p^n, ⟨pow_nonneg p.2.1 n, pow_le_one n p.2.1 p.2.2⟩⟩
-
-instance {α : Type*} [ordered_semiring α] : has_pow (Icc (0:α) 1) ℕ := ⟨prob_pow_Icc⟩
-
-
 instance {α : Type*} [ordered_semiring α] : monoid (Icc (0:α) 1) := {
   mul := λ p q, ⟨p*q, ⟨mul_nonneg p.2.1 q.2.1, mul_le_one p.2.2 q.2.1 q.2.2⟩⟩,
   mul_assoc := λ p q r, subtype.mk_eq_mk.2 (mul_assoc p q r),
   one := ⟨1, right_mem_Icc.2 zero_le_one⟩,
   one_mul := λ p, (by simp),
   mul_one := λ p, (by simp),
-  npow := λ n p, ⟨p.1^n, ⟨pow_nonneg p.2.1 n, pow_le_one n p.2.1 p.2.2⟩⟩,
+  npow := λ n p, ⟨p.1 ^ n, ⟨pow_nonneg p.2.1 n, pow_le_one n p.2.1 p.2.2⟩⟩,
   npow_zero' := λ p, (by simp [has_one.one]),
   npow_succ' :=
   begin
     refine λ n p, _,
     simp only [pow_succ p.val n],
     refl,
-  end
+  end,
 }
 
 
 
 
+instance {α : Type*} [ordered_comm_semiring α] : comm_monoid_with_zero (Icc (0:α) 1) :=
+{
+  mul_comm :=
+begin
+  refine λ p q, _,
+  apply subtype.mk_eq_mk.2,
+  apply mul_comm,
+end ,
 
--- instance {α : Type*} [ordered_semiring α] : comm_monoid_with_zero (Icc (0:α) 1) :=
--- {
---   mul_comm :=
--- begin
---   refine λ p q, _,
---   apply subtype.mk_eq_mk.2,
---   have := @mul_comm,
+  zero := ⟨0,left_mem_Icc.2 zero_le_one⟩,
 
---   sorry,
--- end ,
+  zero_mul :=
+begin
+  refine λ p, _,
+  rcases p with ⟨p1, ⟨p21,p22⟩⟩,
+  suffices : (⟨0 * p1, _⟩ : (Icc (0:α) 1)) = ⟨0, _⟩ * ⟨p1, _⟩,
+  { rw ←this, simp only [zero_mul] },
+  refl,
+end ,
 
---   zero := ⟨0,left_mem_Icc.2 zero_le_one⟩,
+  mul_zero :=
+begin
+  refine λ p, _,
+  rcases p with ⟨p1, ⟨p21,p22⟩⟩,
+  suffices : (⟨p1 * 0, _⟩ : (Icc (0:α) 1)) = ⟨p1, _⟩ * ⟨0, _⟩,
+  {rw ←this, simp only [mul_zero] },
+  refl,
+end ,
 
---   zero_mul :=
--- begin
---   refine λ p, _,
---   simp only [subtype.coe_mk, zero_mul],
--- end ,
-
---   mul_zero :=
--- begin
---   refine λ p, _,
---   simp only [subtype.coe_mk, mul_zero],
--- end  }
+  ..(show monoid (Icc (0:α) 1), by apply_instance)
+}
 
 
 

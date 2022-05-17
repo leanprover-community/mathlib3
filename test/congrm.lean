4a5e97fa5fca1198771baf4654a509e353bb8f65
adomani <adomani@gmail.com>
2022-05-17 06:14:52 +0200

    comment out example

--- test/congrm.lean
+++ test/congrm.lean
@@ -6,6 +6,17 @@ def j₁ : A → A | _ := w
 def j₂ : A → A → A | _ _ := w
 def w : A := w
 
+/-
+example (H : true → a = b) (H' : true → c + (f a) = c + (f d)) (H'' : true → f d = f b) :
+  f a + f a = f b + f b :=
+begin
+  congrm f _ + f _,
+  { exact H trivial },
+  { exact H'' trivial },
+  { exact H' trivial },
+end
+-/
+
 example (H : true → a = b) (H' : true → c + (f a) = c + (f d)) (H'' : true → f d = f b) :
   f (f a) * (f d + (c + f a)) = f (f b) * (f b + (c + f d)) :=
 begin
@@ -22,3 +33,9 @@ begin
   { exact H'' trivial },
   { exact H' trivial },
 end
+
+example (h1 : 5 = 3) (h2 : 7 = 1) : nat.succ 5 + nat.pred 7 = nat.pred 3 * nat.succ 1 :=
+begin
+  congrm_1 j₂ (j₁ _) (j₁ _); -- the main goal becomes
+  assumption,                -- `nat.succ 1 + nat.pred 3 = nat.succ 1 * nat.pred 3` and `refl`
+end                          -- closes it!

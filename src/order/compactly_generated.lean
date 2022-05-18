402dbe3c9c10471f8c3514423d6e60c11892baef
Oliver Nash <github@olivernash.org>
2022-05-18 10:36:14 +0100

    feat(order/compactly_generated): an independent subset of a well-founded complete lattice is finite

--- src/order/compactly_generated.lean
+++ src/order/compactly_generated.lean
@@ -228,6 +228,25 @@ alias is_Sup_finite_compact_iff_is_sup_closed_compact ↔
       _ is_sup_closed_compact.is_Sup_finite_compact
 alias is_sup_closed_compact_iff_well_founded ↔ _ well_founded.is_sup_closed_compact
 
+lemma well_founded.finite_of_set_independent (h : well_founded ((>) : α → α → Prop))
+  {s : set α} (hs : set_independent s) : s.finite :=
+begin
+  classical,
+  refine set.not_infinite.mp (λ contra, _),
+  obtain ⟨t, ht₁, ht₂⟩ := well_founded.is_Sup_finite_compact α h s,
+  replace contra : ∃ (x : α), x ∈ s ∧ x ≠ ⊥ ∧ x ∉ t,
+  { have : (s \ (insert ⊥ t : finset α)).infinite := contra.diff (finset.finite_to_set _),
+    obtain ⟨x, hx₁, hx₂⟩ := this.nonempty,
+    exact ⟨x, hx₁, by simpa [not_or_distrib] using hx₂⟩, },
+  obtain ⟨x, hx₀, hx₁, hx₂⟩ := contra,
+  replace hs : x ⊓ Sup s = ⊥,
+  { have := hs.mono (by simp [ht₁, hx₀, -set.union_singleton] : ↑t ∪ {x} ≤ s) (by simp : x ∈ _),
+    simpa [disjoint, hx₂, ← t.sup_id_eq_Sup, ← ht₂] using this, },
+  apply hx₁,
+  rw [← hs, eq_comm, inf_eq_left],
+  exact le_Sup hx₀,
+end
+
 end complete_lattice
 
 /-- A complete lattice is said to be compactly generated if any

4778f5927bb5939baf5b0c3e6a0f7558c2df97a3
Hanting Zhang <hantingz@usc.edu>
2022-05-18 21:03:11 -0700

    Add lemmas about `v + s`, better placement

--- src/linear_algebra/affine_space/affine_subspace.lean
+++ src/linear_algebra/affine_space/affine_subspace.lean
@@ -189,51 +189,6 @@ iff.rfl
 
 variables {k P}
 
-/-- Two affine subspaces are equal if they have the same points. -/
-@[ext] lemma coe_injective  : function.injective (coe : affine_subspace k P → set P) :=
-λ s1 s2 h, begin
-  cases s1,
-  cases s2,
-  congr,
-  exact h
-end
-
-@[simp] lemma ext_iff (s₁ s₂ : affine_subspace k P) :
-  (s₁ : set P) = s₂ ↔ s₁ = s₂ :=
-⟨λ h, coe_injective h, by tidy⟩
-
-instance : has_vadd V (affine_subspace k P) :=
-{ vadd := λ x S,
-{ carrier := x +ᵥ S,
-  smul_vsub_vadd_mem := λ v _ _ _ ha hb hc,
-  begin
-    rcases ha with ⟨a, haS, rfl⟩,
-    rcases hb with ⟨b, hbS, rfl⟩,
-    rcases hc with ⟨c, hcS, rfl⟩,
-    simp only [affine_subspace.mem_coe, vadd_vsub_vadd_cancel_left] at *,
-    refine ⟨v • (a -ᵥ b) +ᵥ c, S.smul_vsub_vadd_mem v haS hbS hcS, _⟩,
-    rw [vadd_vadd, vadd_vadd, add_comm],
-  end } }
-
-instance : add_action V (affine_subspace k P) :=
-{ vadd := (+ᵥ),
-  zero_vadd := λ p, by { unfold has_vadd.vadd, simp only [zero_vadd, set.image_id'], ext, refl, },
-  add_vadd := λ x y p,
-  begin
-    unfold has_vadd.vadd,
-    simp only [set.image_vadd],
-    ext z,
-    rw [set.mem_vadd_set, set.mem_vadd_set],
-    refine ⟨λ h, _, λ h, _⟩,
-    { rcases h with ⟨a, ha, rfl⟩,
-      refine ⟨y +ᵥ a, ⟨a, ha, rfl⟩, vadd_vadd x y a⟩, },
-    rcases h with ⟨a, ha, rfl⟩,
-    refine ⟨-y +ᵥ a, _, _⟩,
-    { rcases ha with ⟨b, hb, rfl⟩,
-      rwa [vadd_vadd, add_left_neg, zero_vadd], },
-    rw [vadd_vadd, add_neg_cancel_right],
-  end }
-
 /-- The direction of an affine subspace is the submodule spanned by
 the pairwise differences of points.  (Except in the case of an empty
 affine subspace, where the direction is the zero submodule, every
@@ -387,6 +342,19 @@ begin
   simp
 end
 
+/-- Two affine subspaces are equal if they have the same points. -/
+@[ext] lemma coe_injective : function.injective (coe : affine_subspace k P → set P) :=
+λ s1 s2 h, begin
+  cases s1,
+  cases s2,
+  congr,
+  exact h
+end
+
+@[simp] lemma ext_iff (s₁ s₂ : affine_subspace k P) :
+  (s₁ : set P) = s₂ ↔ s₁ = s₂ :=
+⟨λ h, coe_injective h, by tidy⟩
+
 /-- Two affine subspaces with the same direction and nonempty
 intersection are equal. -/
 lemma ext_of_direction_eq {s1 s2 : affine_subspace k P} (hd : s1.direction = s2.direction)
@@ -424,6 +392,74 @@ rfl
 @[simp, norm_cast] lemma coe_vadd (s : affine_subspace k P) [nonempty s] (a : s.direction) (b : s) :
   ↑(a +ᵥ b) = (a:V) +ᵥ (b:P) :=
 rfl
+
+instance : has_vadd V (affine_subspace k P) :=
+{ vadd := λ x S,
+{ carrier := x +ᵥ S,
+  smul_vsub_vadd_mem := λ v _ _ _ ha hb hc,
+  begin
+    rcases ha with ⟨a, haS, rfl⟩,
+    rcases hb with ⟨b, hbS, rfl⟩,
+    rcases hc with ⟨c, hcS, rfl⟩,
+    simp only [affine_subspace.mem_coe, vadd_vsub_vadd_cancel_left] at *,
+    refine ⟨v • (a -ᵥ b) +ᵥ c, S.smul_vsub_vadd_mem v haS hbS hcS, _⟩,
+    rw [vadd_vadd, vadd_vadd, add_comm],
+  end } }
+
+instance : add_action V (affine_subspace k P) :=
+{ vadd := (+ᵥ),
+  zero_vadd := λ p, by { unfold has_vadd.vadd, simp only [zero_vadd, set.image_id'], ext, refl, },
+  add_vadd := λ x y p,
+  begin
+    unfold has_vadd.vadd,
+    simp only [set.image_vadd],
+    ext z,
+    rw [set.mem_vadd_set, set.mem_vadd_set],
+    refine ⟨λ h, _, λ h, _⟩,
+    { rcases h with ⟨a, ha, rfl⟩,
+      refine ⟨y +ᵥ a, ⟨a, ha, rfl⟩, vadd_vadd x y a⟩, },
+    rcases h with ⟨a, ha, rfl⟩,
+    refine ⟨-y +ᵥ a, _, _⟩,
+    { rcases ha with ⟨b, hb, rfl⟩,
+      rwa [vadd_vadd, add_left_neg, zero_vadd], },
+    rw [vadd_vadd, add_neg_cancel_right],
+  end }
+
+@[simp] lemma coe_const_vadd (v : V) (s : affine_subspace k P) :
+  ((v +ᵥ s : affine_subspace k P) : set P) = v +ᵥ s := rfl
+
+lemma mem_const_vadd_iff (v : V) {s : affine_subspace k P} {p : P} :
+  v +ᵥ p ∈ v +ᵥ s ↔ p ∈ s :=
+vadd_mem_vadd_set_iff
+
+lemma const_vadd_empty {v : V} {s : affine_subspace k P} (hs : (s : set P) = ∅) :
+  v +ᵥ s = s :=
+by { ext, simp [hs] }
+
+lemma mem_const_vadd_direction (v x : V) {s : affine_subspace k P} :
+  x ∈ s.direction → x ∈ (v +ᵥ s).direction :=
+begin
+  by_cases hem : set.nonempty (s : set P),
+  { rw mem_direction_iff_eq_vsub,
+    rintro ⟨p, hp, q, hq, rfl⟩,
+    rw ← vadd_vsub_vadd_cancel_left v _ _,
+    rw ← mem_const_vadd_iff v at hp,
+    rw ← mem_const_vadd_iff v at hq,
+    exact vsub_mem_direction hp hq,
+    exact hem, },
+  rw not_nonempty_iff_eq_empty at hem,
+  rw const_vadd_empty hem,
+  exact id,
+end
+
+lemma const_vadd_direction (v : V) (s : affine_subspace k P) :
+  (v +ᵥ s).direction = s.direction :=
+begin
+  ext, refine ⟨λ h, _, λ h, mem_const_vadd_direction v x h⟩,
+  have hx := mem_const_vadd_direction (-v) x h,
+  rwa neg_vadd_vadd at hx,
+end
+
 
 /-- Two affine subspaces with nonempty intersection are equal if and
 only if their directions are equal. -/
@@ -1289,6 +1325,23 @@ omit V₃
 @[simp] lemma map_direction (s : affine_subspace k P₁) :
   (s.map f).direction = s.direction.map f.linear :=
 by simp [direction_eq_vector_span]
+
+lemma map_const_vadd (v : V₁) (s : affine_subspace k P₁) :
+  (v +ᵥ s).map f = f.linear v +ᵥ s.map f :=
+begin
+  ext,
+  simp only [coe_map, coe_const_vadd, mem_image],
+  refine ⟨λ h, _, λ h, _⟩,
+  { rcases h with ⟨_, ⟨y, hy, rfl⟩, rfl⟩,
+    rw [affine_map.map_vadd, vadd_mem_vadd_set_iff],
+    exact mem_image_of_mem _ hy, },
+  rcases h with ⟨_, ⟨y, hy, rfl⟩, rfl⟩,
+  use v +ᵥ y,
+  rw vadd_mem_vadd_set_iff,
+  refine ⟨hy, _⟩,
+  rw affine_map.map_vadd,
+end
+
 
 lemma map_span (s : set P₁) :
   (affine_span k s).map f = affine_span k (f '' s) :=

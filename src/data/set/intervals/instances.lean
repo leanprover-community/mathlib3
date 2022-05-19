24f43c35a47925d1a44d92a945d4df2d3d080509
Stuart Presnell <stuart.presnell@gmail.com>
2022-05-19 16:03:11 +0100

    Golf

--- src/data/set/intervals/instances.lean
+++ src/data/set/intervals/instances.lean
@@ -10,21 +10,19 @@ instance {α : Type*} [ordered_semiring α] : monoid (Icc (0:α) 1) := {
   mul := λ p q, ⟨p*q, ⟨mul_nonneg p.2.1 q.2.1, mul_le_one p.2.2 q.2.1 q.2.2⟩⟩,
   mul_assoc := λ p q r, subtype.mk_eq_mk.2 (mul_assoc p q r),
   one := ⟨1, right_mem_Icc.2 zero_le_one⟩,
-  one_mul := λ p, (by simp only [subtype.coe_mk, one_mul, subtype.coe_eta]),
-  mul_one := λ p, (by simp only [subtype.coe_mk, mul_one, subtype.coe_eta]),
+  one_mul := (by simp),
+  mul_one := (by simp),
   npow := λ n p, ⟨p.1 ^ n, ⟨pow_nonneg p.2.1 n, pow_le_one n p.2.1 p.2.2⟩⟩,
-  npow_zero' := λ p, (by simp [has_one.one]),
+  npow_zero' := (by simp [has_one.one]),
   npow_succ' := λ n p, (by {simp only [pow_succ p.val n], refl}) }
 
 instance {α : Type*} [ordered_comm_semiring α] : comm_monoid_with_zero (Icc (0:α) 1) :=
-{ mul_comm :=
-  begin
-    exact λ p q, subtype.mk_eq_mk.2 (mul_comm _ _),
-  end,
+{ mul_comm := λ p q, (by exact subtype.mk_eq_mk.2 (mul_comm _ _)),
   zero := ⟨0, left_mem_Icc.2 zero_le_one⟩,
   zero_mul :=
   begin
     rintros ⟨p1, ⟨_,_⟩⟩,
+    -- have := zero_mul p1,
     have : (⟨0 * p1, _⟩ : (Icc (0:α) 1)) = ⟨0, _⟩ * ⟨p1, _⟩, { refl },
     rw ←this,
     simp only [zero_mul],
@@ -42,29 +40,16 @@ instance {α : Type*} [ordered_comm_semiring α] : comm_monoid_with_zero (Icc (0
 /-- Instances for `(Ico 0 1)` -/
 
 instance {α : Type*} [ordered_semiring α] : semigroup (Ico (0:α) 1) := {
-  mul :=
-  begin
-    refine λ p q, ⟨p*q, ⟨mul_nonneg p.2.1 q.2.1, _⟩⟩,
-    rcases p with ⟨p1, ⟨_,_⟩⟩,
-    rcases q with ⟨q1, ⟨_,_⟩⟩,
-    apply mul_lt_one_of_nonneg_of_lt_one_right,
-    exact le_of_lt p_property_right,
-    repeat {assumption},
-  end,
+  mul := λ p q, ⟨p*q, ⟨mul_nonneg p.2.1 q.2.1,
+      mul_lt_one_of_nonneg_of_lt_one_right (le_of_lt p.2.2) q.2.1 q.2.2⟩⟩,
+  -- TODO: golf this when `mul_lt_one` is merged
 
   mul_assoc := λ p q r, (by exact subtype.mk_eq_mk.2 (mul_assoc p q r)),
 }
 
 instance {α : Type*} [ordered_comm_semiring α] : comm_semigroup (Ico (0:α) 1) := {
-  mul_comm :=
-  begin
-    refine λ p q, _,
-    rcases p with ⟨p1, ⟨_,_⟩⟩,
-    rcases q with ⟨q1, ⟨_,_⟩⟩,
-    apply subtype.mk_eq_mk.2,
-    simp only [subtype.coe_mk],
-    exact mul_comm p1 q1,
-  end,
+  mul_comm := λ ⟨p1, _⟩ ⟨q1, _⟩,
+    (by {apply subtype.mk_eq_mk.2, simp [subtype.coe_mk, mul_comm p1 q1]}),
   ..(show semigroup (Ico (0:α) 1), by apply_instance)
 }
 
@@ -73,50 +58,14 @@ instance {α : Type*} [ordered_comm_semiring α] : comm_semigroup (Ico (0:α) 1)
 
 instance {α : Type*} [ordered_comm_semiring α] [nontrivial α] : comm_monoid (Ioc (0:α) 1) := {
   mul := λ p q, ⟨p.1 * q.1, ⟨mul_pos p.2.1 q.2.1, mul_le_one p.2.2 (le_of_lt q.2.1) q.2.2⟩⟩,
-  mul_assoc :=
-begin
-  refine λ p q r, _,
-  rcases p with ⟨p1, ⟨p21,p22⟩⟩,
-  rcases q with ⟨q1, ⟨q21,q22⟩⟩,
-  rcases r with ⟨r1, ⟨r21,r22⟩⟩,
-  simp,
-  refine mul_assoc p1 q1 r1,
-end ,
+  mul_assoc := λ p q r, (by simp only [subtype.mk_eq_mk, mul_assoc]),
   one := ⟨1, ⟨zero_lt_one, le_refl 1⟩⟩,
-  one_mul :=
-begin
-  refine λ p, _,
-  rcases p with ⟨p1, ⟨p21,p22⟩⟩,
-  simp only [one_mul, subtype.mk_eq_mk],
-end ,
-  mul_one :=
-begin
-  refine λ p, _,
-  rcases p with ⟨p1, ⟨p21,p22⟩⟩,
-  simp only [mul_one, subtype.mk_eq_mk],
-end ,
-  npow :=
-begin
-  refine λ n p, _,
-  rcases p with ⟨p1, ⟨p21,p22⟩⟩,
-  exact ⟨p1 ^ n, ⟨pow_pos p21 n, pow_le_one n (le_of_lt p21) p22⟩⟩,
-end ,
-  npow_zero' :=
-begin
-  refine λ p, _,
-  rcases p with ⟨p1, ⟨p21,p22⟩⟩,
-  simp only [pow_zero],
-  refl,
-end ,
-  npow_succ' := λ n ⟨p1, ⟨_,_⟩⟩, (by {simp only [pow_succ p1 n], refl }),
-  mul_comm :=
-begin
-  refine λ p q, _,
-  rcases p with ⟨p1, ⟨_,_⟩⟩,
-  rcases q with ⟨q1, ⟨_,_⟩⟩,
-  ext1,
-  exact mul_comm p1 q1,
-end
+  one_mul := (by simp),
+  mul_one := (by simp),
+  npow := λ n p, ⟨p.1 ^ n, ⟨pow_pos p.2.1 n, pow_le_one n (le_of_lt p.2.1) p.2.2⟩⟩,
+  npow_zero' := λ p, (by {simp only [pow_zero], refl}),
+  npow_succ' := λ n ⟨p1, _⟩, (by {simp only [pow_succ p1 n], refl }),
+  mul_comm := λ p q, (by {ext1, exact mul_comm p.1 q.1}),
 }
 
 #exit

4a278cd42f2e3b2567c936799fbf2537159f7a19
Damiano Testa <adomani@gmail.com>
2022-05-21 14:17:52 +0100

    unneeded import

--- src/data/nat/choose/basic.lean
+++ src/data/nat/choose/basic.lean
@@ -4,7 +4,6 @@ Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Chris Hughes, Bhavik Mehta
 -/
 import data.nat.factorial.basic
-import tactic.move_add
 
 /-!
 # Binomial coefficients

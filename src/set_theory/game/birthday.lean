327680b16d886fc636f661dc36f2486f7cc65e9b
Violeta Hernández <vi.hdz.p@gmail.com>
2022-05-21 14:11:27 -0500

    simp only

--- src/set_theory/game/birthday.lean
+++ src/set_theory/game/birthday.lean
@@ -110,7 +110,11 @@ by { rw birthday_def, simp }
 by { rw birthday_def, simp }
 
 @[simp] theorem birthday_half : birthday half = 2 :=
-by { rw birthday_def, simp, exact max_eq_right (lt_succ_self 1).le }
+begin
+  rw birthday_def,
+  simp only [half_move_left, birthday_zero, lsub_unique, succ_zero, half_move_right, birthday_one],
+  exact max_eq_right (lt_succ_self 1).le
+end
 
 @[simp] theorem neg_birthday : ∀ x : pgame, (-x).birthday = x.birthday
 | ⟨xl, xr, xL, xR⟩ := begin

46408ef8cf8bc0a0aac4f528e2a20033aecbd3dd
Stuart Presnell <stuart.presnell@gmail.com>
2022-05-21 20:11:16 +0100

    Format braces

--- src/data/set/intervals/instances.lean
+++ src/data/set/intervals/instances.lean
@@ -6,8 +6,8 @@ open set
 
 /-- Instances for `(Icc 0 1)` -/
 
-instance {α : Type*} [ordered_semiring α] : monoid (Icc (0:α) 1) := {
-  mul := λ p q, ⟨p*q, ⟨mul_nonneg p.2.1 q.2.1, mul_le_one p.2.2 q.2.1 q.2.2⟩⟩,
+instance {α : Type*} [ordered_semiring α] : monoid (Icc (0:α) 1) :=
+{ mul := λ p q, ⟨p*q, ⟨mul_nonneg p.2.1 q.2.1, mul_le_one p.2.2 q.2.1 q.2.2⟩⟩,
   mul_assoc := λ p q r, subtype.mk_eq_mk.2 (mul_assoc p q r),
   one := ⟨1, right_mem_Icc.2 zero_le_one⟩,
   one_mul := (by simp),
@@ -37,20 +37,20 @@ instance {α : Type*} [ordered_comm_semiring α] : comm_monoid_with_zero (Icc (0
 
 /-- Instances for `(Ico 0 1)` -/
 
-instance {α : Type*} [ordered_semiring α] : semigroup (Ico (0:α) 1) := {
-  mul := λ p q, ⟨p*q, ⟨mul_nonneg p.2.1 q.2.1,
+instance {α : Type*} [ordered_semiring α] : semigroup (Ico (0:α) 1) :=
+{ mul := λ p q, ⟨p*q, ⟨mul_nonneg p.2.1 q.2.1,
     mul_lt_one_of_nonneg_of_lt_one_right p.2.2.le q.2.1 q.2.2⟩⟩,
   mul_assoc := λ p q r, (by exact subtype.mk_eq_mk.2 (mul_assoc p q r)) }
 
-instance {α : Type*} [ordered_comm_semiring α] : comm_semigroup (Ico (0:α) 1) := {
-  mul_comm := λ ⟨p1, _⟩ ⟨q1, _⟩,
+instance {α : Type*} [ordered_comm_semiring α] : comm_semigroup (Ico (0:α) 1) :=
+{ mul_comm := λ ⟨p1, _⟩ ⟨q1, _⟩,
     (by {apply subtype.mk_eq_mk.2, simp [subtype.coe_mk, mul_comm p1 q1]}),
   ..(show semigroup (Ico (0:α) 1), by apply_instance) }
 
 /-- Instances for `(Ioc 0 1)` -/
 
-instance {α : Type*} [ordered_semiring α] [nontrivial α] : monoid (Ioc (0:α) 1) := {
-  mul := λ p q, ⟨p.1 * q.1, ⟨mul_pos p.2.1 q.2.1, mul_le_one p.2.2 (le_of_lt q.2.1) q.2.2⟩⟩,
+instance {α : Type*} [ordered_semiring α] [nontrivial α] : monoid (Ioc (0:α) 1) :=
+{ mul := λ p q, ⟨p.1 * q.1, ⟨mul_pos p.2.1 q.2.1, mul_le_one p.2.2 (le_of_lt q.2.1) q.2.2⟩⟩,
   mul_assoc := λ p q r, (by simp only [subtype.mk_eq_mk, mul_assoc]),
   one := ⟨1, ⟨zero_lt_one, le_refl 1⟩⟩,
   one_mul := (by simp),
@@ -59,18 +59,18 @@ instance {α : Type*} [ordered_semiring α] [nontrivial α] : monoid (Ioc (0:α)
   npow_zero' := λ p, (by {simp only [pow_zero], refl}),
   npow_succ' := λ n ⟨p1, _⟩, (by {simp only [pow_succ p1 n], refl }) }
 
-instance {α : Type*} [ordered_comm_semiring α] [nontrivial α] : comm_monoid (Ioc (0:α) 1) := {
-  mul_comm := λ p q, (by {ext1, exact mul_comm p.1 q.1}),
+instance {α : Type*} [ordered_comm_semiring α] [nontrivial α] : comm_monoid (Ioc (0:α) 1) :=
+{ mul_comm := λ p q, (by {ext1, exact mul_comm p.1 q.1}),
   ..(show monoid (Ioc (0:α) 1), by apply_instance) }
 
 /-- Instances for `(Ioo 0 1)` -/
 
-instance {α : Type*} [ordered_semiring α] : semigroup (Ioo (0:α) 1) := {
-  mul := λ p q, ⟨p.1 * q.1, ⟨mul_pos p.2.1 q.2.1,
+instance {α : Type*} [ordered_semiring α] : semigroup (Ioo (0:α) 1) :=
+{ mul := λ p q, ⟨p.1 * q.1, ⟨mul_pos p.2.1 q.2.1,
     mul_lt_one_of_nonneg_of_lt_one_right p.2.2.le q.2.1.le q.2.2⟩⟩,
   mul_assoc := λ p q r, (by simp only [subtype.mk_eq_mk, mul_assoc]) }
 
-instance {α : Type*} [ordered_comm_semiring α] : comm_semigroup (Ioo (0:α) 1) := {
-  mul_comm := λ ⟨p1, _⟩ ⟨q1, _⟩,
+instance {α : Type*} [ordered_comm_semiring α] : comm_semigroup (Ioo (0:α) 1) :=
+{ mul_comm := λ ⟨p1, _⟩ ⟨q1, _⟩,
     (by {apply subtype.mk_eq_mk.2, simp [subtype.coe_mk, mul_comm p1 q1]}),
   ..(show semigroup (Ioo (0:α) 1), by apply_instance) }

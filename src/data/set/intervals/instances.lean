7a1382a3a93b56f7c1fff11f202c9073974f74f8
Stuart Presnell <stuart.presnell@gmail.com>
2022-05-21 20:11:54 +0100

    Copyright header

--- src/data/set/intervals/instances.lean
+++ src/data/set/intervals/instances.lean
@@ -1,3 +1,8 @@
+/-
+Copyright (c) 2022 Stuart Presnell. All rights reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
+Authors: Stuart Presnell
+-/
 import data.set.intervals.basic
 import algebra.order.ring
 import algebra.group_power.order

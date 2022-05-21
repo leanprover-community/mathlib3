b89a738961c023bf6f6606076b0a4fdd4ebbe6c2
Stuart Presnell <stuart.presnell@gmail.com>
2022-05-21 22:19:23 +0100

    Break out instances; `comm_monoid_with_zero Icc`

--- src/data/set/intervals/instances.lean
+++ src/data/set/intervals/instances.lean
@@ -18,6 +18,18 @@ open set
 
 /-- Instances for `(Icc 0 1)` -/
 
+instance {α : Type*} [ordered_semiring α] : has_zero (Icc (0:α) 1) :=
+  { zero := ⟨0, left_mem_Icc.2 zero_le_one⟩ }
+
+instance {α : Type*} [ordered_semiring α] : has_one (Icc (0:α) 1) :=
+  { one := ⟨1, right_mem_Icc.2 zero_le_one⟩ }
+
+instance {α : Type*} [ordered_semiring α] : has_mul (Icc (0:α) 1) :=
+  { mul := λ p q, ⟨p*q, ⟨mul_nonneg p.2.1 q.2.1, mul_le_one p.2.2 q.2.1 q.2.2⟩⟩ }
+
+instance {α : Type*} [ordered_semiring α] : has_pow (Icc (0:α) 1) ℕ :=
+  { pow := λ p n, ⟨p.1 ^ n, ⟨pow_nonneg p.2.1 n, pow_le_one n p.2.1 p.2.2⟩⟩ }
+
 instance {α : Type*} [ordered_semiring α] : monoid (Icc (0:α) 1) :=
 { mul := λ p q, ⟨p*q, ⟨mul_nonneg p.2.1 q.2.1, mul_le_one p.2.2 q.2.1 q.2.2⟩⟩,
   mul_assoc := λ p q r, subtype.mk_eq_mk.2 (mul_assoc p q r),
@@ -29,23 +41,13 @@ instance {α : Type*} [ordered_semiring α] : monoid (Icc (0:α) 1) :=
   npow_succ' := λ n p, (by {simp only [pow_succ p.val n], refl}) }
 
 instance {α : Type*} [ordered_comm_semiring α] : comm_monoid_with_zero (Icc (0:α) 1) :=
-{ mul_comm := λ p q, (by exact subtype.mk_eq_mk.2 (mul_comm _ _)),
-  zero := ⟨0, left_mem_Icc.2 zero_le_one⟩,
-  zero_mul :=
-  begin
-    rintros ⟨p1, ⟨p21,p22⟩⟩,
-    have : (⟨0 * p1, _⟩ : (Icc (0:α) 1)) = ⟨0, _⟩ * ⟨p1, _⟩, { refl },
-    rw ←this,
-    simp only [zero_mul],
-  end,
-  mul_zero :=
-  begin
-    rintros ⟨p1, ⟨p21,p22⟩⟩,
-    have : (⟨p1 * 0, _⟩ : (Icc (0:α) 1)) = ⟨p1, _⟩ * ⟨0, _⟩, { refl },
-    rw ←this,
-    simp only [mul_zero],
-  end,
-  ..(show monoid (Icc (0:α) 1), by apply_instance) }
+begin
+  apply function.injective.comm_monoid_with_zero (λ (p:(Icc (0:α) 1)), p.1) subtype.coe_injective,
+  { simp [has_zero.zero] },
+  { simp [has_one.one] },
+  { simp [has_mul.mul] },
+  { simp only [has_pow.pow, subtype.coe_mk, eq_self_iff_true, forall_const] },
+end
 
 /-- Instances for `(Ico 0 1)` -/
 

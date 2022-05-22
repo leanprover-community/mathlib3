e355d923e62fe9b978e05477fcf27707a9d154ac
Stuart Presnell <stuart.presnell@gmail.com>
2022-05-22 15:18:22 +0100

    name instances

--- src/data/set/intervals/instances.lean
+++ src/data/set/intervals/instances.lean
@@ -30,14 +30,14 @@ open set
 
 namespace set.Icc
 
-instance : has_zero (Icc (0:α) 1) := { zero := ⟨0, left_mem_Icc.2 zero_le_one⟩ }
+instance has_zero : has_zero (Icc (0:α) 1) := { zero := ⟨0, left_mem_Icc.2 zero_le_one⟩ }
 
-instance : has_one (Icc (0:α) 1) := { one := ⟨1, right_mem_Icc.2 zero_le_one⟩ }
+instance has_one : has_one (Icc (0:α) 1) := { one := ⟨1, right_mem_Icc.2 zero_le_one⟩ }
 
-instance : has_mul (Icc (0:α) 1) :=
+instance has_mul : has_mul (Icc (0:α) 1) :=
 { mul := λ p q, ⟨p*q, ⟨mul_nonneg p.2.1 q.2.1, mul_le_one p.2.2 q.2.1 q.2.2⟩⟩ }
 
-instance : has_pow (Icc (0:α) 1) ℕ :=
+instance has_pow : has_pow (Icc (0:α) 1) ℕ :=
 { pow := λ p n, ⟨p.1 ^ n, ⟨pow_nonneg p.2.1 n, pow_le_one n p.2.1 p.2.2⟩⟩ }
 
 @[simp, norm_cast] lemma coe_zero : ↑(0 : Icc (0:α) 1) = (0 : α) := rfl
@@ -45,10 +45,11 @@ instance : has_pow (Icc (0:α) 1) ℕ :=
 @[simp, norm_cast] lemma coe_mul (x y : Icc (0:α) 1) : ↑(x * y) = (x * y : α) := rfl
 @[simp, norm_cast] lemma coe_pow (x : Icc (0:α) 1) (n : ℕ) : ↑(x ^ n) = (x ^ n : α) := rfl
 
-instance : monoid_with_zero (Icc (0:α) 1) :=
+instance monoid_with_zero : monoid_with_zero (Icc (0:α) 1) :=
 subtype.coe_injective.monoid_with_zero _ coe_zero coe_one coe_mul coe_pow
 
-instance {α : Type*} [ordered_comm_semiring α] : comm_monoid_with_zero (Icc (0:α) 1) :=
+instance comm_monoid_with_zero {α : Type*} [ordered_comm_semiring α] :
+    comm_monoid_with_zero (Icc (0:α) 1) :=
 subtype.coe_injective.comm_monoid_with_zero _ coe_zero coe_one coe_mul coe_pow
 
 end set.Icc
@@ -57,16 +58,16 @@ end set.Icc
 
 namespace set.Ico
 
-instance : has_mul (Ico (0:α) 1) :=
+instance has_mul : has_mul (Ico (0:α) 1) :=
 { mul := λ p q, ⟨p*q, ⟨mul_nonneg p.2.1 q.2.1,
     mul_lt_one_of_nonneg_of_lt_one_right p.2.2.le q.2.1 q.2.2⟩⟩ }
 
 @[simp, norm_cast] lemma coe_mul (x y : Ico (0:α) 1) : ↑(x * y) = (x * y : α) := rfl
 
-instance : semigroup (Ico (0:α) 1) :=
+instance semigroup : semigroup (Ico (0:α) 1) :=
 subtype.coe_injective.semigroup _ coe_mul
 
-instance {α : Type*} [ordered_comm_semiring α] : comm_semigroup (Ico (0:α) 1) :=
+instance comm_semigroup {α : Type*} [ordered_comm_semiring α] : comm_semigroup (Ico (0:α) 1) :=
 subtype.coe_injective.comm_semigroup _ coe_mul
 
 end set.Ico
@@ -75,28 +76,29 @@ end set.Ico
 
 namespace set.Ioc
 
-instance [nontrivial α] : has_one (Ioc (0:α) 1) := { one := ⟨1, ⟨zero_lt_one, le_refl 1⟩⟩ }
+instance has_one [nontrivial α] : has_one (Ioc (0:α) 1) := { one := ⟨1, ⟨zero_lt_one, le_refl 1⟩⟩ }
 
-instance : has_mul (Ioc (0:α) 1) :=
+instance has_mul : has_mul (Ioc (0:α) 1) :=
 { mul := λ p q, ⟨p.1 * q.1, ⟨mul_pos p.2.1 q.2.1, mul_le_one p.2.2 (le_of_lt q.2.1) q.2.2⟩⟩ }
 
-instance Ioc_pow : has_pow (Ioc (0:α) 1) ℕ :=
+instance has_pow : has_pow (Ioc (0:α) 1) ℕ :=
 { pow := λ p n, ⟨p.1 ^ n, ⟨pow_pos p.2.1 n, pow_le_one n (le_of_lt p.2.1) p.2.2⟩⟩ }
 
 @[simp, norm_cast] lemma coe_one [nontrivial α] : ↑(1 : Ioc (0:α) 1) = (1 : α) := rfl
 @[simp, norm_cast] lemma coe_mul (x y : Ioc (0:α) 1) : ↑(x * y) = (x * y : α) := rfl
 @[simp, norm_cast] lemma coe_pow (x : Ioc (0:α) 1) (n : ℕ) : ↑(x ^ n) = (x ^ n : α) := rfl
 
-instance : semigroup (Ioc (0:α) 1) :=
+instance semigroup : semigroup (Ioc (0:α) 1) :=
 subtype.coe_injective.semigroup _ coe_mul
 
-instance [nontrivial α] : monoid (Ioc (0:α) 1) :=
+instance monoid [nontrivial α] : monoid (Ioc (0:α) 1) :=
 subtype.coe_injective.monoid _ coe_one coe_mul coe_pow
 
-instance {α : Type*} [ordered_comm_semiring α] : comm_semigroup (Ioc (0:α) 1) :=
+instance comm_semigroup {α : Type*} [ordered_comm_semiring α] : comm_semigroup (Ioc (0:α) 1) :=
 subtype.coe_injective.comm_semigroup _ coe_mul
 
-instance {α : Type*} [ordered_comm_semiring α] [nontrivial α] : comm_monoid (Ioc (0:α) 1) :=
+instance comm_monoid {α : Type*} [ordered_comm_semiring α] [nontrivial α] :
+    comm_monoid (Ioc (0:α) 1) :=
 subtype.coe_injective.comm_monoid _ coe_one coe_mul coe_pow
 
 end set.Ioc
@@ -105,15 +107,15 @@ end set.Ioc
 
 namespace set.Ioo
 
-instance : has_mul (Ioo (0:α) 1) := { mul := λ p q, ⟨p.1 * q.1, ⟨mul_pos p.2.1 q.2.1,
+instance has_mul : has_mul (Ioo (0:α) 1) := { mul := λ p q, ⟨p.1 * q.1, ⟨mul_pos p.2.1 q.2.1,
     mul_lt_one_of_nonneg_of_lt_one_right p.2.2.le q.2.1.le q.2.2⟩⟩ }
 
 @[simp, norm_cast] lemma coe_mul (x y : Ioo (0:α) 1) : ↑(x * y) = (x * y : α) := rfl
 
-instance : semigroup (Ioo (0:α) 1) :=
+instance semigroup : semigroup (Ioo (0:α) 1) :=
 subtype.coe_injective.semigroup _ coe_mul
 
-instance {α : Type*} [ordered_comm_semiring α] : comm_semigroup (Ioo (0:α) 1) :=
+instance comm_semigroup {α : Type*} [ordered_comm_semiring α] : comm_semigroup (Ioo (0:α) 1) :=
 subtype.coe_injective.comm_semigroup _ coe_mul
 
 end set.Ioo

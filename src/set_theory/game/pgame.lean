5ce98997cae1fef5ee5e73eb469e00ba372aea1e
Violeta Hernández <vi.hdz.p@gmail.com>
2022-05-22 03:35:46 -0500

    Update src/set_theory/game/pgame.lean
    Co-authored-by: Eric Wieser <wieser.eric@gmail.com>

--- src/set_theory/game/pgame.lean
+++ src/set_theory/game/pgame.lean
@@ -943,7 +943,7 @@ end⟩
 
 @[simp] theorem nat_one : ((1 : ℕ) : pgame) = 0 + 1 := rfl
 
-instance is_empty_add_left_moves (x y : pgame.{u})
+instance is_empty_left_moves_add (x y : pgame.{u})
   [is_empty x.left_moves] [is_empty y.left_moves] : is_empty (x + y).left_moves :=
 begin
   unfreezingI { cases x, cases y },

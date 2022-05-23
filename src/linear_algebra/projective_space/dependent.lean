5abcaf06528999857326027669853a348135293a
Michael14071407 <39394803+Michael14071407@users.noreply.github.com>
2022-05-23 11:44:15 -0600

    fix me

--- src/linear_algebra/projective_space/dependent.lean
+++ src/linear_algebra/projective_space/dependent.lean
@@ -1,10 +1,287 @@
 import linear_algebra.projective_space.basic
-
+import tactic
 variables {K V : Type*} [field K] [add_comm_group V] [module K V]
 
 namespace projectivization
 
-def independent {α : Type*} (ι : α → ℙ K V) : Prop := sorry
-def dependent {α : Type*} (ι : α → ℙ K V) : Prop := sorry
+/- Composition of a function and a finite indexing of size two or three commute -/
+lemma fin_comp_commutes₂ {α β : Type*} {a b : α} {f: α → β} : (f ∘ (![a, b])) = (![f a, f b]) :=
+by { ext, fin_cases x; refl }
+
+lemma fin_comp_commutes₃ {α β : Type*} {a b c : α} {f: α → β} :
+  (f ∘ (![a, b, c])) = (![f a, f b, f c]) :=
+by { ext, fin_cases x; refl }
+
+/- A finite indexing of size two or three is nonzero iff all but one element being
+mapped to 0 implies the other element is not mapped to zero. -/
+@[simp]
+lemma fin_nonzero_iff₂ {a b : K} : ![a, b] ≠ 0 ↔ (a = 0 → b ≠ 0) :=
+by { simp }
+
+@[simp]
+lemma fin_nonzero_iff₃ {a b c : K} : ![a, b, c] ≠ 0 ↔ (a = 0 → b = 0 → c ≠ 0) :=
+by { simp }
+
+/- If an indexing of size 3 is nonzero and the first term is zero, then
+  the indexing obtained from the last two elements is nonzero. -/
+@[simp] lemma nontrivial_and_zero₁ {a b c : K} (ht : ![a, b, c] ≠ 0)
+  (ha : a = 0) : ![b, c] ≠ 0 :=
+  by { simp_rw [fin_nonzero_iff₂, fin_nonzero_iff₃] at *, exact ht ha }
+
+/- If an indexing of size 3 is nonzero and the second term is zero, then
+  the indexing obtained from the first and last elements is nonzero. -/
+@[simp] lemma nontrivial_and_zero₂ {a b c : K} (ht : ![a, b, c] ≠ 0)
+  (hb : b = 0) : ![a, c] ≠ 0 :=
+  by { simp_rw [fin_nonzero_iff₂, fin_nonzero_iff₃] at *, intro ha, exact ht ha hb  }
+
+/- If an indexing of size 3 is nonzero and the third term is zero, then
+  the indexing obtained from the first and second elements is nonzero. -/
+@[simp] lemma nontrivial_and_zero₃ {a b c : K} (ht : ![a, b, c] ≠ 0)
+  (hc : c = 0) : ![a, b] ≠ 0 :=
+  by { simp_rw [fin_nonzero_iff₂, fin_nonzero_iff₃] at *, intros ha hb, exact ht ha hb hc }
+
+variable (K)
+
+/-Two nonzero vectors go to the same point in projective space iff one is in
+the span of the other. -/
+lemma mk_eq_mk_iff' (v w: V) (hv : v ≠ 0) (hw : w ≠ 0) : mk K v hv = mk K w hw ↔
+  ∃ a : K, a • w = v :=
+begin
+  rw mk_eq_mk_iff K v w hv hw,
+  split,
+  { rintro ⟨a, ha⟩, exact ⟨a, ha⟩ },
+  { rintro ⟨a, ha⟩, refine ⟨units.mk0 a (λ c, hv.symm _), ha⟩, rwa [c, zero_smul] at ha }
+end
+
+/- A nonzero vector is a scalar multiple of the representative of its equivalence class in
+projective space. -/
+lemma exists_smul_mk_rep_eq (v : V) (hv : v ≠ 0) : ∃ (a : Kˣ), v = a • ((mk K v hv).rep) :=
+begin
+  cases (exists_smul_eq_mk_rep K v hv) with a ha,
+  refine ⟨a⁻¹, _⟩,
+  apply_fun (λ x, (a)⁻¹ • x) at ha,
+  rwa [inv_smul_smul] at ha,
+end
+
+variable {K}
+
+/- A linearly independent familty of nonzero vectors gives an independent family of points
+in the projective space. -/
+inductive independent {ι : Type*} : (ι → ℙ K V) → Prop
+| mk (f : ι → V) (hf : ∀ i : ι, f i ≠ 0) (hl : linear_independent K f) :
+    independent (λ i, mk K (f i) (hf i))
+
+/- A family of points in a projective space are independent iff their
+representatives are linearly independent. -/
+lemma independent_iff (ι : Type*) (f : ι → (ℙ K V)) : independent f ↔
+  linear_independent K (projectivization.rep ∘ f) :=
+begin
+  split,
+  { rintro h, induction h with ff hff hh,
+    choose a ha using λ (i : ι), exists_smul_eq_mk_rep K (ff i) (hff i),
+    convert hh.units_smul a,
+    ext i, exact (ha i).symm },
+  { intro h,
+    convert independent.mk _ _ h,
+    { ext, simp only [mk_rep] },
+    { intro i, apply rep_nonzero } }
+end
+
+/- A linearly dependent family of nonzero vectors gives a dependent family of points
+in the projective space. -/
+inductive dependent {ι : Type*} : (ι → ℙ K V) → Prop
+| mk (f : ι → V) (hf : ∀ i : ι, f i ≠ 0) (h : ¬linear_independent K f) :
+    dependent (λ i, mk K (f i) (hf i))
+
+/- A family of points in a projective space is dependent iff their
+representatives are linearly dependent. -/
+lemma dependent_iff (ι : Type*) (f : ι → (ℙ K V)) : dependent f ↔
+  ¬ linear_independent K (projectivization.rep ∘ f) :=
+begin
+  split,
+  { rw ← independent_iff,
+    intros h1, induction h1 with ff hff hh1,
+    contrapose! hh1, rw independent_iff at hh1,
+    choose a ha using λ (i : ι), exists_smul_eq_mk_rep K (ff i) (hff i),
+    convert hh1.units_smul a⁻¹,
+    ext i, simp only [← ha, inv_smul_smul, pi.smul_apply', pi.inv_apply, function.comp_app] },
+  { intro h,
+    convert dependent.mk _ _ h,
+    { ext, simp only [mk_rep] },
+    { intro i, apply rep_nonzero } }
+end
+
+/- Dependence is the negation of independence. -/
+lemma dependent_iff_not_independent {ι : Type*} (f : ι → ℙ K V) :
+  dependent f ↔ ¬ independent f :=
+by { rw [dependent_iff, independent_iff] }
+
+/- Independence is the negation of dependence. -/
+lemma independent_iff_not_dependent {ι : Type*} (f : ι → ℙ K V) :
+  independent f ↔ ¬ dependent f :=
+by { rw [dependent_iff, independent_iff, not_not] }
+
+
+/- Two points in a projective space are dependent iff they are equal. -/
+@[simp] lemma pair_dependent_iff_eq (u v : ℙ K V) : dependent ![u, v] ↔ u = v :=
+begin
+  simp_rw [dependent_iff_not_independent, independent_iff, linear_independent_fin2,
+    function.comp_app, matrix.cons_val_one, matrix.head_cons,
+    ne.def, matrix.cons_val_zero, not_and, not_forall, not_not,
+    ← mk_eq_mk_iff' K _ _ (rep_nonzero u) (rep_nonzero v), mk_rep,
+    imp_iff_right_iff],
+  exact or.inl (rep_nonzero v),
+end
+
+/- Two points in a projective space are independent iff the points are not equal. -/
+@[simp] lemma pair_independent_iff_neq (u v : ℙ K V) : (independent ![u, v]) ↔ u ≠ v :=
+by { rw [independent_iff_not_dependent, pair_dependent_iff_eq u v] }
+
+open_locale big_operators
+/- Two vectors are linearly dependent iff 0 is a nontrivial linear combination of them. -/
+lemma linear_dependent_iff₂ (u v : V) : ¬ linear_independent K ![u, v] ↔
+  (∃ (a b : K) (hnt : ![a, b] ≠ 0), a • u + b • v = 0) :=
+begin
+  rw fintype.linear_independent_iff,
+  simp only [not_forall, exists_prop, fin.sum_univ_succ, function.comp_app, matrix.cons_val_zero,
+  matrix.cons_val_succ, fin.succ_zero_eq_one, fintype.univ_of_subsingleton, fin.mk_eq_subtype_mk,
+  fin.mk_zero, finset.sum_singleton, function.ne_iff],
+  split,
+  { rintros ⟨g, h, ⟨i, hi⟩⟩,
+    exact ⟨g 0, g 1, ⟨⟨i, by { fin_cases i; exact hi}⟩, h⟩⟩ },
+  { rintros ⟨a, b, ⟨h, hs⟩⟩,
+    exact ⟨![a, b], ⟨hs, h⟩⟩ }
+end
+
+/- Three vectors are linearly dependent iff 0 is a nontrivial linear combination of them. -/
+lemma linear_dependent_iff₃ (u v w : V) : ¬ linear_independent K ![u, v, w] ↔
+  (∃ (a b c: K) (hnt : ![a, b, c] ≠ 0), a • u + b • v + c • w = 0) :=
+begin
+  rw fintype.linear_independent_iff,
+  simp only [not_forall, exists_prop, fin.sum_univ_succ, function.comp_app, matrix.cons_val_zero,
+  matrix.cons_val_succ, fin.succ_zero_eq_one, fintype.univ_of_subsingleton, fin.mk_eq_subtype_mk,
+  fin.mk_zero, finset.sum_singleton, add_assoc, function.ne_iff],
+  split,
+  { rintros ⟨g, h, ⟨i, hi⟩⟩,
+    exact ⟨g 0, g 1, g 2, ⟨⟨i, by {fin_cases i; exact hi}⟩, h⟩⟩ },
+  { rintros ⟨a, b, c, ⟨h, hs⟩⟩,
+    exact ⟨![a, b, c], ⟨hs, h⟩⟩ }
+end
+
+/- Two points are dependent iff 0 is a nontrivial linear combination of their representatives. -/
+lemma dependent_iff_reps_dependent₂ (u v : ℙ K V) : dependent ![u, v] ↔
+  ∃ (a b : K) (hnt : ![a, b] ≠ 0), a • u.rep + b • v.rep = 0 :=
+  by { rw [dependent_iff, fin_comp_commutes₂], exact linear_dependent_iff₂ u.rep v.rep }
+
+
+/- Three points are dependent iff 0 is a nontrivial linear combination of their representatives. -/
+lemma dependent_iff_reps_dependent₃ (u v w : ℙ K V) : dependent ![u, v, w] ↔
+  ∃ (a b c : K) (hnt : ![a, b, c] ≠ 0), a • u.rep + b • v.rep + c • w.rep = 0 :=
+  by { rw [dependent_iff, fin_comp_commutes₃], exact linear_dependent_iff₃ u.rep v.rep w.rep }
+
+
+/- An indexed family of nonzero vectors is linearly independent iff
+  the family of representatives of the points they determine in projective space
+  is linearly independent. -/
+lemma linear_independent_iff_mk_rep {ι : Type*} (f : ι → V) (hf : ∀ i : ι, f i ≠ 0) :
+  linear_independent K f ↔ linear_independent K (λ i, (mk K (f i) (hf i)).rep) :=
+begin
+  choose a ha using λ (i : ι), exists_smul_eq_mk_rep K (f i) (hf i),
+  split,
+  { intro hf,
+    convert linear_independent.units_smul hf a,
+    ext,
+    rwa [pi.smul_apply', ha x] },
+  { intro hf,
+    convert linear_independent.units_smul hf (λ i, (a i)⁻¹),
+    ext,
+    specialize ha x,
+    apply_fun (λ (v : V), (a x)⁻¹ • v) at ha,
+    rwa [inv_smul_smul] at ha },
+end
+
+/- The points two nonzero vectors represent in projective space are dependent iff
+  zero is a nontrivial linear combination of the vectors. -/
+lemma mk_dependent_iff_dependent₂ (u v : V) (hu : u ≠ 0) (hv : v ≠ 0) :
+  dependent ![mk K u hu, mk K v hv] ↔ ∃ (a b: K) (hnt : ![a, b] ≠ 0), a • u + b • v = 0 :=
+begin
+  rw [← linear_dependent_iff₂, dependent_iff, not_iff_not, iff.comm],
+  convert linear_independent_iff_mk_rep ![u, v] (by { intro i, fin_cases i; assumption }),
+  ext, fin_cases x; simp
+end
+
+/- The points three nonzero vectors represent in projective space are dependent iff
+  zero is a nontrivial linear combination of the vectors. -/
+lemma mk_dependent_iff_dependent₃ (u v w : V) (hu : u ≠ 0) (hv : v ≠ 0) (hw : w ≠ 0) :
+  dependent ![mk K u hu, mk K v hv, mk K w hw] ↔
+  ∃ (a b c : K) (hnt : ![a, b, c] ≠ 0), a • u + b • v + c • w = 0 :=
+begin
+  rw [← linear_dependent_iff₃, dependent_iff, not_iff_not, iff.comm],
+  convert linear_independent_iff_mk_rep ![u, v, w] (by { intro i, fin_cases i; assumption }),
+  ext, fin_cases x; simp
+end
+
+
+/-If three points in a projective geometry are such that zero is a linear combination of their
+representatives, and the scalar associated to one of the representative of the is zero,
+then the other two points are dependent. -/
+
+lemma dependent_and_zero_gives_dependent_pair₁ {u v w : ℙ K V} {a b c : K} (hnz : ![a, b, c] ≠ 0)
+  (ha : a = 0) (hsz : a • u.rep + b • v.rep + c • w.rep = 0) : dependent ![v, w] :=
+begin
+  simp_rw [ha, zero_smul, zero_add, dependent_iff_reps_dependent₂] at ⊢ hsz,
+  exact ⟨b, c, ⟨nontrivial_and_zero₁ hnz ha, hsz⟩⟩,
+end
+
+lemma dependent_and_zero_gives_dependent_pair₂ {u v w : ℙ K V} {a b c : K} (hnz : ![a, b, c] ≠ 0)
+  (hb : b = 0) (hsz : a • u.rep + b • v.rep + c • w.rep = 0) : dependent ![u, w] :=
+begin
+  simp_rw [hb, zero_smul, add_zero, dependent_iff_reps_dependent₂] at ⊢ hsz,
+  exact ⟨a, c, ⟨nontrivial_and_zero₂ hnz hb, hsz⟩⟩,
+end
+
+lemma dependent_and_zero_gives_dependent_pair₃ {u v w : ℙ K V} {a b c : K} (hnz : ![a, b, c] ≠ 0)
+  (hc : c = 0) (hsz : a • u.rep + b • v.rep + c • w.rep = 0) : dependent ![u, v] :=
+begin
+  simp_rw [hc, zero_smul, add_zero, dependent_iff_reps_dependent₂] at ⊢ hsz,
+  exact ⟨a, b, ⟨nontrivial_and_zero₃ hnz hc, hsz⟩⟩,
+end
+
+/-If three points in a projective geometry are such that 0 is a nontrivial linear combination of
+their representatives, and two of the points are distinct, the scalar corresponding to
+the remaining point is not zero. -/
+lemma neq_scalar_neq_zero₁ {u v w : ℙ K V} {a b c : K} (hneq : v ≠ w)
+  (hnt : ![a, b, c] ≠ 0) (hsz : a • u.rep + b • v.rep + c • w.rep = 0) : a ≠ 0 :=
+begin
+  intro h,
+  rw fin_nonzero_iff₃ at hnt,
+  refine hneq _,
+  rw [← pair_dependent_iff_eq, dependent_iff_reps_dependent₂],
+  exact ⟨b, c, by { rw fin_nonzero_iff₂, exact hnt h}, by { rwa [h, zero_smul, zero_add] at hsz }⟩,
+end
+
+/-If three points of a projective space are dependent, and two of them are not equal, the
+representative of the remaining point is in the span of the other two representatives. -/
+lemma dependent_triple_and_neq_pair {u v w : ℙ K V} (huv : u ≠ v)
+  (huvw : dependent ![u, v, w]) : ∃ (a b : K) (hxy : ![a, b] ≠ 0), w.rep = a • u.rep + b • v.rep :=
+begin
+  by_cases hvw : v = w,
+  { exact ⟨0, 1, by { simp }, by { rw [zero_smul, zero_add, one_smul, hvw] }⟩ },
+  { rw dependent_iff_reps_dependent₃ at huvw,
+    rcases huvw with ⟨a, b, c, ⟨hnt, hsz⟩⟩,
+    have hc : c ≠ 0, by
+      { intro h, rw [← pair_independent_iff_neq, independent_iff_not_dependent] at huv,
+        exact huv (dependent_and_zero_gives_dependent_pair₃ hnt h hsz) },
+    refine ⟨-c⁻¹*a, -c⁻¹*b, _, _⟩,
+      { intro h,
+        simp only [matrix.cons_eq_zero_iff, neg_eq_zero, mul_eq_zero, inv_eq_zero,
+        eq_iff_true_of_subsingleton, and_true] at h,
+        cases h with ha hb,
+        exact (neq_scalar_neq_zero₁ hvw hnt hsz) (or.resolve_left ha hc) },
+      { apply_fun (λ x, c⁻¹ • x) at hsz,
+        simp_rw [smul_zero, smul_add, add_eq_zero_iff_neg_eq, ← mul_smul, inv_mul_cancel hc,
+        one_smul, neg_add, ← neg_smul, ← neg_mul] at hsz,
+        exact hsz.symm } }
+end
 
 end projectivization
--- src/linear_algebra/projective_space/subspace.lean
+++ src/linear_algebra/projective_space/subspace.lean
@@ -1,13 +1,20 @@
 import linear_algebra.projective_space.dependent
+import linear_algebra
+import linear_algebra.finsupp
+import linear_algebra.finite_dimensional
+import algebra.module.submodule.basic
+import tactic
 
 variables (K V : Type*) [field K] [add_comm_group V] [module K V]
 
 namespace projectivization
 
-structure subspace :=
+
+
+@[ext] structure subspace :=
 (carrier : set (ℙ K V))
-(mem_of_dependent' (x y z : ℙ K V) (h : x ≠ y) (hdep : dependent ![x,y,z]) :
-  x ∈ carrier → y ∈ carrier → z ∈ carrier)
+(mem_of_add' (v w : V) (hv : v ≠ 0) (hw : w ≠ 0) (hvw : v + w ≠ 0) :
+  mk K v hv ∈ carrier → mk K w hw ∈ carrier → mk K (v + w) (hvw) ∈ carrier)
 
 namespace subspace
 
@@ -17,22 +24,45 @@ instance : set_like (subspace K V) (ℙ K V) :=
 { coe := carrier,
   coe_injective' := λ A B, by { cases A, cases B, simp } }
 
+lemma mem_add (T : subspace K V) (v w : V) (hv : v ≠ 0) (hw : w ≠ 0) (hvw : v + w ≠ 0) :
+  projectivization.mk K v hv ∈ T → projectivization.mk K w hw ∈ T →
+  projectivization.mk K (v + w) (hvw) ∈ T :=
+  T.mem_of_add' v w hv hw hvw
+
+
+open_locale big_operators
+lemma mem_sum (T : subspace K V) {ι : Type*} [fintype ι] {f : ι → V} (hf : ∀ i : ι, f i ≠ 0)
+  (hs : (∑ i : ι, f i) ≠ 0) (h : ∀ i : ι, projectivization.mk K (f i) (hf i) ∈ T) :
+  projectivization.mk K (∑ i : ι, f i) (hs) ∈ T := sorry
+
 lemma mem_of_dependent (T : subspace K V) (x y z : ℙ K V)
   (h : x ≠ y) (hdep : dependent ![x,y,z]) (hx : x ∈ T) (hy : y ∈ T) :
-  z ∈ T := T.mem_of_dependent' x y _ h hdep hx hy
+  z ∈ T := sorry
+
+
 
 inductive span_carrier (S : set (ℙ K V)) : set (ℙ K V)
 | of (x : ℙ K V) (hx : x ∈ S) : span_carrier x
-| mem_of_dependent (x y z : ℙ K V) (h : x ≠ y)
-    (hdep : dependent ![x,y,z]) : span_carrier x → span_carrier y → span_carrier z
+| mem_of_add (v w : V) (hv : v ≠ 0) (hw : w ≠ 0) (hvw : v + w ≠ 0) : span_carrier (projectivization.mk K v hv)  →
+  span_carrier (projectivization.mk K w hw) → span_carrier (projectivization.mk K (v + w) (hvw))
+
 
 def span (S : set (ℙ K V)) : subspace K V :=
 { carrier := span_carrier S,
-  mem_of_dependent' := λ x y z h hdep hx hy,
-    span_carrier.mem_of_dependent x y z h hdep hx hy }
+  mem_of_add' := λ v w hv hw hvw,
+    span_carrier.mem_of_add v w hv hw hvw }
 
 lemma subset_span (S : set (ℙ K V)) : S ⊆ span S :=
 λ x hx, span_carrier.of _ hx
+
+@[simp] lemma span_subspace_self (W : subspace K V) : span ↑W = W :=
+begin
+  ext, split; intro hx,
+  { induction hx,
+    exact hx_hx,
+    refine W.mem_of_dependent hx_x hx_y hx_z hx_h _ _ _, assumption' },
+  { exact subset_span W hx  },
+end
 
 def gi : galois_insertion (span : set (ℙ K V) → subspace K V) coe :=
 { choice := λ S hS, span S,
@@ -48,16 +78,283 @@ def gi : galois_insertion (span : set (ℙ K V) → subspace K V) coe :=
 instance : complete_lattice (subspace K V) :=
 gi.lift_complete_lattice
 
+/- The intersection of two subspaces is a subspace-/
+instance : has_inter (subspace K V) :=
+{ inter := λ W T,
+  { carrier := W.carrier ∩ T.carrier,
+    mem_of_dependent' := λ x y z h hdep hx hy,
+      ⟨W.mem_of_dependent x y z h hdep hx.1 hy.1, T.mem_of_dependent x y z h hdep hx.2 hy.2⟩ } }
+
 def of_submodule (H : submodule K V) : subspace K V :=
 { carrier := { x | x.submodule ≤ H },
-  mem_of_dependent' := sorry }
+  mem_of_add' :=
+  begin
+    intros v w hv hw hvw h1 h2,
+    simp only [set.mem_set_of_eq, submodule_mk, submodule.span_singleton_le_iff_mem] at h1 h2 ⊢,
+    exact H.add_mem h1 h2,
+  end }
 
+open_locale big_operators classical
 def equiv : subspace K V ≃o submodule K V :=
 { to_fun := λ S, ⨆ (x : ℙ K V) (hx : x ∈ S), x.submodule,
   inv_fun := of_submodule,
-  left_inv := sorry,
-  right_inv := sorry,
-  map_rel_iff' := sorry }
+  left_inv :=
+  begin
+    intros W,
+    dsimp,
+    ext,
+    unfold of_submodule,
+    split,
+
+    { rw ← mk_rep x,
+      intros h,
+      dsimp at h,
+      simp at h,
+      rw submodule.mem_bsupr_iff_exists_dfinsupp at h,
+      obtain ⟨f, hf ⟩ := h,
+      dsimp at hf,
+      erw dfinsupp.sum_add_hom_apply at hf,
+      dsimp [dfinsupp.sum] at hf,
+      rw finset.sum_subtype at hf,
+      rotate,
+      exact f.support,
+      rw ← hf,
+
+
+
+      /-
+      have heq : (⨆ (x : ℙ K V) (hx : x ∈ W), x.submodule) = (⨆ (x : ℙ K V) (hx : x ∈ W),
+      submodule.span K {x.rep}), by {congr, ext, rw [← submodule_mk x_1.rep (rep_nonzero x_1), mk_rep],},
+      rw heq at hx,
+      have hxr : _, by { exact (hx (submodule.mem_span_singleton_self x.rep))},
+      suffices hmemw : x.rep = 0 ∨ (∃ hx : x.rep ≠ 0, (projectivization.mk K x.rep hx) ∈ W.carrier), by {sorry},
+      apply @submodule.supr_induction K V _ _ _ _ _ (λ y, y = 0 ∨ (∃ hy : y ≠ 0, (projectivization.mk K y hy) ∈ W.carrier)) x.rep hxr,
+
+      { intros y v hv,
+        simp at hv,
+        have hvy: v ∈ submodule.span K {y.rep}, by { sorry },
+        rw submodule.mem_span_singleton at hvy,
+        by_cases hvz : v = 0,
+        { exact or.inl hvz,},
+        { right,
+          use hvz,
+          rw ← projectivization.mk_eq_mk_iff' K v y.rep hvz (rep_nonzero y) at hvy,
+          rw hvy,
+          sorry },},
+      { simp,},
+      { intros v u hv hu,
+        cases hv,
+        { cases hu,
+          { rw [hv, hu, add_zero], exact or.inl (rfl),},
+          { simp_rw [hv, zero_add], exact or.inr hu},},
+        { cases hu,
+          { simp_rw [hu, add_zero], exact or.inr hv},
+          { cases hv with hv hvw,
+            cases hu with hu huw,
+            sorry},},},
+
+            -/
+            },
+    { intro hx,
+      rw set.mem_set_of_eq,
+      refine le_Sup _,
+      rw set.mem_range,
+      exact ⟨x, supr_pos hx⟩ },
+  end,
+  right_inv :=
+  begin
+    intros W, dsimp, ext, split,
+    { intro hx,
+      have hxw : (⨆ (x : ℙ K V) (hx : x ∈ of_submodule W), x.submodule) ≤ W, by
+      { refine supr_le _, simp only [supr_le_iff], intros i hi, exact hi },
+      exact hxw hx },
+    { intro hx,
+      by_cases hxt : x = 0,
+      { rw hxt, exact submodule.zero_mem _ },
+      { have hxw : (projectivization.mk K x hxt) ∈ (of_submodule W), by
+        { exact (submodule.span_singleton_le_iff_mem x W).mpr hx },
+        have : (projectivization.mk K x hxt).submodule ≤
+        ⨆ (x : ℙ K V) (hx : x ∈ of_submodule W), x.submodule, by
+        { refine le_Sup _, rw [submodule_mk, set.mem_range],
+          exact ⟨(projectivization.mk K x hxt), supr_pos hxw⟩ },
+        exact this (by { rw submodule_mk, exact submodule.mem_span_singleton_self x }) } },
+  end,
+  map_rel_iff' :=
+  begin
+    intros W S,
+    rw [equiv.coe_fn_mk, supr₂_le_iff],
+    split,
+    {
+      intros hw v hv,
+      specialize hw v hv,
+      rw [← mk_rep v, submodule_mk, submodule.span_singleton_le_iff_mem,
+      submodule.supr_eq_span] at hw,
+      rcases (submodule.mem_span_finite_of_mem_span hw) with ⟨s, ⟨hs, hvs⟩⟩,
+      rw mem_span_finset at hvs,
+      cases hvs with f hf,
+      sorry},
+
+    { intros hws w hw u hu,
+      specialize hws hw,
+      have : w.submodule ≤ ⨆ (x : ℙ K V) (hx : x ∈ S), x.submodule,
+      by { refine le_Sup _, rw set.mem_range, exact ⟨w, supr_pos hws⟩},
+      exact this hu},
+  end, }
+
+
+
+/- A point in projective space determines a subspace consisting of the point. -/
+instance : has_singleton (ℙ K V) (subspace K V):=
+{ singleton := λ a,
+{ carrier := {a},
+  mem_of_dependent' :=
+  by {intros x y z h hdep hx hy, simp at *, rw [hx, hy] at h, contradiction} }}
+
+/- A point belongs to a singleton subspace iff the point is equal to the singleton determining
+  the subspace-/
+lemma mem_singleton_iff (a b : ℙ K V) : b ∈ ({a} : subspace K V) ↔ b = a :=
+begin
+  exact ⟨by { intro hb, induction hb, refl }, by { intro h, rw h, split }⟩,
+end
+
+/- The submodule corresponding to the intersection of two subspaces is the intersection of the
+  two submodules corresponding to the respective subspaces. -/
+lemma inter_equiv_eq_equiv_inter (W S : subspace K V) :
+  subspace.equiv (W ∩ S) = (subspace.equiv W) ∩ (subspace.equiv S) :=
+begin
+  sorry
+end
+
+/- The line determined by two points in projective space is their span. -/
+def line (a b : ℙ K V) : subspace K V := span {a, b}
+
+/- A point in projective space is a member of a subspace iff the subspace determined by that
+  point is in the subspace. -/
+lemma singleton_mem_iff_leq (a : ℙ K V) (W : subspace K V) : a ∈ W ↔ {a} ≤ W :=
+  by { change a ∈ W ↔ ({a} : set (ℙ K V)) ⊆ W, rw [set.singleton_subset_iff, set_like.mem_coe] }
+
+/- The submodule corresponding to a one point subspace in projective space is the same as the
+  submodule determined by the point. -/
+lemma of_submodule_singleton (a : ℙ K V) : subspace.equiv {a} = a.submodule :=
+begin
+  unfold equiv,
+  simp_rw [rel_iso.coe_fn_mk, equiv.coe_fn_mk, mem_singleton_iff, supr_supr_eq_left],
+end
+
+/- A point in projective space is a member of a subspace iff the representative of the point is
+  a member of the corresponding submodule. -/
+lemma mem_of_subspace_iff (a : ℙ K V) (W : subspace K V) : a ∈ W ↔ a.rep ∈ subspace.equiv W :=
+begin
+  rw [← submodule.span_singleton_le_iff_mem, singleton_mem_iff_leq a W,
+   ← subspace.equiv.map_rel_iff'],
+  change (subspace.equiv) {a} ≤ (subspace.equiv) W ↔ submodule.span K {a.rep} ≤ equiv W,
+  rw [of_submodule_singleton, ← submodule_mk a.rep (rep_nonzero a), mk_rep],
+end
+
+/- If a point is contained in the span of a set of points, then the point's representative is
+  contained in the span of the set's representatives. -/
+lemma in_span_in_rep_span (a : ℙ K V) (S : set (ℙ K V)) (h : a ∈ span S) :
+  a.rep ∈ submodule.span K {v : V | ∃ s : ℙ K V, s ∈ S ∧  v = s.rep} :=
+begin
+  change a ∈ span_carrier S at h,
+  induction h with a b c d e f g h i j k,
+  { exact submodule.subset_span ⟨a, by { simpa }⟩ },
+  { rcases  (dependent_triple_and_neq_pair f g) with ⟨p, q, hpq, hz ⟩,
+    rw hz,
+    exact submodule.add_mem _ (submodule.smul_mem _ p j) (submodule.smul_mem _ q k) },
+end
+
+/- The supremum of the submodules determined by the points in the span of set S is a submodule of
+  the span of the representatives of the points in S. -/
+lemma supr_span_submodule_leq_span (S : set (ℙ K V)) :
+  (⨆ (x : ℙ K V) (hx : x ∈ span S), x.submodule) ≤
+  submodule.span K {v : V | ∃ s : ℙ K V, s ∈ S ∧ v = s.rep} :=
+begin
+  intros x hx,
+  apply submodule.supr_induction _ hx,
+  { intros v y hy,
+    dsimp at hy,
+    sorry},
+  { exact submodule.zero_mem _ },
+  { intros y z hy hz, exact submodule.add_mem _ hy hz },
+end
+
+/- The submodule corresponding to the span of a set of points in projective space is
+  the submodule spanned by the representatives of the points. -/
+lemma of_submodule_eq_span_mem_reps (S : set (ℙ K V)) :
+submodule.span K {v : V | ∃ s : ℙ K V, s ∈ S ∧  v = s.rep} = subspace.equiv (span S) :=
+begin
+  unfold equiv,
+  rw [rel_iso.coe_fn_mk, equiv.coe_fn_mk],
+  ext,
+  split,
+  { intro hx,
+    apply submodule.span_induction hx,
+    { intros y hy,
+      refine submodule.mem_supr_of_mem (projectivization.mk K y _) _,
+      rcases hy with ⟨s, ⟨hs, hsy⟩⟩,
+      { rw hsy, exact rep_nonzero s },
+      { rcases hy with ⟨z, ⟨hs, hy⟩⟩,
+        simp_rw [submodule_mk, hy, mk_rep z],
+        apply submodule.mem_supr_of_mem,
+        exact (subset_span S hs),
+        exact submodule.mem_span_singleton_self (z.rep) } },
+    { simp },
+    { intros y z hy hz, exact submodule.add_mem _ hy hz },
+    { intros a y hy, exact submodule.smul_mem _ a hy } },
+  { intro hxx, exact ((supr_span_submodule_leq_span S) hxx) },
+end
+
+/- The submodule corresponding to a line is the span of the
+  representatives of the two points which determine the line. -/
+lemma equiv_line_eq_span_reps (a b : ℙ K V) : submodule.span K {a.rep, b.rep} =
+  subspace.equiv (line a b) :=
+begin
+  convert of_submodule_eq_span_mem_reps _,
+  ext,
+  simp only [set.mem_insert_iff, set.mem_singleton_iff, set.mem_set_of_eq],
+  split,
+  { rintro ⟨ha, hb⟩,
+    { use a, simpa },
+    { use b, simpa } },
+  { rintro ⟨y, hy⟩,
+    rcases hy with ⟨⟨hab, hab⟩, hr⟩, exact or.inl hr, rw hy_left at hr, exact or.inr hr }
+end
+
+/- The line determined by a single point is the one-point subspace containing that point. -/
+lemma P1 (a : ℙ K V) : (line a a) = {a} :=
+by { unfold line, rw set.pair_eq_singleton, exact span_subspace_self {a} }
+
+/- A point in projective space is a member of any line it determines. -/
+
+lemma P2 (a b : ℙ K V) : a ∈ line b a :=
+begin
+  refine subset_span {b, a} _,
+  simp only [set.mem_insert_iff, set.mem_singleton, or_true],
+end
+
+lemma P2_ref (a b : ℙ K V) : a ∈ line a b :=
+begin
+  refine subset_span {a, b} _,
+  simp only [set.mem_insert_iff, eq_self_iff_true, true_or],
+end
+
+/- If -/
+lemma P3 (a b p c d : ℙ K V) (h1: a ≠ c) (h2: a ∈ line b p) (h3: p ∈ line c d) :
+(∃ q : ℙ K V, q ∈ line a c ∩ line b d) :=
+begin
+  by_cases hbp : b = p,
+  { refine ⟨b, _, P2_ref b d ⟩,
+    rw [hbp, P1, mem_singleton_iff] at h2, rw [hbp, h2], exact P2_ref p c },
+  { by_cases hcd : c = d,
+    { use d,
+      rw hcd, exact ⟨P2 d a, P2 d b⟩ },
+    { simp_rw [mem_of_subspace_iff, ← equiv_line_eq_span_reps] at *,
+      change b ≠ p at hbp,
+      change c ≠ d at hcd,
+      rw [← pair_independent_iff_neq, independent_iff, fin_comp_commutes₂] at hbp hcd,
+      sorry } },
+end
 
 end subspace
 

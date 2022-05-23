36ec6cf09f23f5e9be317ca213af9c54e9c65191
Maria Ines de Frutos Fernandez <mariaines.dff@gmail.com>
2022-05-23 17:39:03 +0100

    fix gauge_seminorm

--- src/analysis/convex/gauge.lean
+++ src/analysis/convex/gauge.lean
@@ -369,9 +369,11 @@ end
 /-- `gauge s` as a seminorm when `s` is symmetric, convex and absorbent. -/
 @[simps] def gauge_seminorm (hs₀ : ∀ x ∈ s, -x ∈ s) (hs₁ : convex ℝ s) (hs₂ : absorbent ℝ s) :
   seminorm ℝ E :=
-{ to_fun := gauge s,
-  smul' := λ r x, by rw [gauge_smul hs₀, real.norm_eq_abs, smul_eq_mul]; apply_instance,
-  triangle' := gauge_add_le hs₁ hs₂ }
+{ to_fun    := gauge s,
+  map_zero' := gauge_zero,
+  nonneg    := gauge_nonneg,
+  smul'     := λ r x, by simp only [gauge_smul hs₀, real.norm_eq_abs, smul_eq_mul]; apply_instance,
+  add       := gauge_add_le hs₁ hs₂ }
 
 section gauge_seminorm
 variables {hs₀ : ∀ x ∈ s, -x ∈ s} {hs₁ : convex ℝ s} {hs₂ : absorbent ℝ s}

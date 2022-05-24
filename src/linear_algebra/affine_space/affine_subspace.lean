cf5079bd40a6cc5e04e5d77d57b6edb06ba76b2f
Hanting Zhang <hantingz@usc.edu>
2022-05-23 20:37:34 -0700

    move lemmas

--- src/linear_algebra/affine_space/affine_subspace.lean
+++ src/linear_algebra/affine_space/affine_subspace.lean
@@ -1285,7 +1285,9 @@ by rw [← affine_subspace.map_span, h, map_top_of_surjective f hf]
 
 end affine_map
 
-lemma affine_equiv.span_eq_top_iff {s : set P₁} (e : P₁ ≃ᵃ[k] P₂) :
+namespace affine_equiv
+
+lemma span_eq_top_iff {s : set P₁} (e : P₁ ≃ᵃ[k] P₂) :
   affine_span k s = ⊤ ↔ affine_span k (e '' s) = ⊤ :=
 begin
   refine ⟨(e : P₁ →ᵃ[k] P₂).span_eq_top_of_surjective e.surjective, _⟩,
@@ -1294,6 +1296,12 @@ begin
   rw this,
   exact (e.symm : P₂ →ᵃ[k] P₁).span_eq_top_of_surjective e.symm.surjective h,
 end
+
+omit V₂
+@[simp] lemma map_refl (s : affine_subspace k P₁) : s.map ↑(refl k P₁) = s :=
+by { ext, simp, }
+
+end affine_equiv
 
 end
 
--- src/linear_algebra/affine_space/pointwise.lean
+++ src/linear_algebra/affine_space/pointwise.lean
@@ -40,25 +40,26 @@ variables {k : Type*} {V : Type*} {P : Type*} [ring k] [add_comm_group V] [modul
 include S
 
 instance : has_vadd V (affine_subspace k P) :=
-{ vadd := λ x S, S.map (equiv.const_vadd_hom P) }
+{ vadd := λ x S, S.map (affine_equiv.const_vadd k P x) }
 
 instance : add_action V (affine_subspace k P) :=
 { vadd := (+ᵥ),
-  zero_vadd := λ p, by { unfold has_vadd.vadd, simp only [zero_vadd, set.image_id'], ext, refl, },
+  zero_vadd := λ p, by { unfold has_vadd.vadd, simp, },
   add_vadd := λ x y p,
   begin
     unfold has_vadd.vadd,
-    simp only [set.image_vadd],
     ext z,
-    rw [set.mem_vadd_set, set.mem_vadd_set],
+    simp only [affine_equiv.const_vadd_add, coe_map, affine_equiv.coe_coe,
+      affine_equiv.trans_apply, affine_equiv.const_vadd_apply, mem_image, mem_coe,
+      image_vadd, set.mem_vadd_set],
     refine ⟨λ h, _, λ h, _⟩,
     { rcases h with ⟨a, ha, rfl⟩,
-      refine ⟨y +ᵥ a, ⟨a, ha, rfl⟩, vadd_vadd x y a⟩, },
+      refine ⟨y +ᵥ a, ⟨a, ha, rfl⟩, rfl⟩, },
     rcases h with ⟨a, ha, rfl⟩,
     refine ⟨-y +ᵥ a, _, _⟩,
     { rcases ha with ⟨b, hb, rfl⟩,
       rwa [vadd_vadd, add_left_neg, zero_vadd], },
-    rw [vadd_vadd, add_neg_cancel_right],
+    rw [vadd_vadd, vadd_vadd, add_neg_cancel_right],
   end }
 
 @[simp] lemma coe_const_vadd (v : V) (s : affine_subspace k P) :
@@ -88,7 +89,7 @@ begin
   exact id,
 end
 
-lemma const_vadd_direction (v : V) (s : affine_subspace k P) :
+lemma const_vadd_direction (v : V) {s : affine_subspace k P} :
   (v +ᵥ s).direction = s.direction :=
 begin
   ext, refine ⟨λ h, _, λ h, mem_const_vadd_direction v x h⟩,
@@ -96,7 +97,13 @@ begin
   rwa neg_vadd_vadd at hx,
 end
 
-lemma map_const_vadd (v : V₁) (s : affine_subspace k P₁) :
+variables {V₁ P₁ V₂ P₂ V₃ P₃ : Type*}
+variables [add_comm_group V₁] [module k V₁] [add_torsor V₁ P₁]
+variables [add_comm_group V₂] [module k V₂] [add_torsor V₂ P₂]
+variables [add_comm_group V₃] [module k V₃] [add_torsor V₃ P₃]
+include V₁ V₂
+
+lemma map_const_vadd {f : P₁ →ᵃ[k] P₂} (v : V₁) (s : affine_subspace k P₁) :
   (v +ᵥ s).map f = f.linear v +ᵥ s.map f :=
 begin
   ext,
@@ -111,6 +118,5 @@ begin
   refine ⟨hy, _⟩,
   rw affine_map.map_vadd,
 end
-
 
 end affine_subspace

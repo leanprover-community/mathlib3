2b686cd4c5475b3fb88964417585fdb74bc27369
Justin <justmas@gmail.com>
2022-05-28 23:02:28 -0500

    Apply suggestions from Vierkantor

--- src/linear_algebra/annihilating_polynomial.lean
+++ src/linear_algebra/annihilating_polynomial.lean
@@ -3,14 +3,9 @@ Copyright (c) 2022 Justin Thomas. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Justin Thomas
 -/
-import data.polynomial
-import data.polynomial.ring_division
-import data.polynomial.field_division
-import ring_theory.principal_ideal_domain
-import algebra.module.linear_map
+import data.set.basic
 import field_theory.minpoly
-import linear_algebra
-import ring_theory.ideal.operations
+import ring_theory.principal_ideal_domain
 import ring_theory.polynomial_algebra
 
 /-!
@@ -33,7 +28,7 @@ as defined in `field_theory.minpoly`.
 ## Special case: endomorphism algebra
 
 Given an `R`-module `M` (`[add_comm_group M] [module R M]`)
-there are some common specialiazations which may be more familiar.
+there are some common specializations which may be more familiar.
 * Example 1: `A = M →ₗ[R] M`, the endomorphism algebra of an `R`-module M.
 * Example 2: `A = n × n` matrices with entries in `R`.
 -/
@@ -48,7 +43,8 @@ variables {R A : Type*} [comm_semiring R] [semiring A] [algebra R A]
 
 variables (R)
 
-/--
+/-- `ann_ideal R a` is the *annihilating ideal* of all `p : R[X]` such that `p(a) = 0`.
+
 The informal notation `p(a)` stand for `polynomial.aeval a p`.
 Again informally, the annihilating ideal of `a` is
 `{ p ∈ R[X] | p(a) = 0 }`. This is an ideal in `R[X]`.
@@ -63,14 +59,6 @@ variables {R}
 lemma mem_ann_ideal_iff_aeval_eq_zero (a : A) (p : R[X]) :
   p ∈ ann_ideal R a ↔ aeval a p = 0 :=
 iff.rfl
-
-/-- `p ∈ ann_ideal R a` stated using `eval₂`. -/
-lemma mem_ann_ideal_of_eval₂_algebra_map_eq_zero (a : A) (p : R[X])
-  (h : eval₂ (algebra_map R A) a p = 0) : p ∈ ann_ideal R a :=
-begin
-  apply (mem_ann_ideal_iff_aeval_eq_zero a p).2,
-  rwa aeval_def,
-end
 
 end semiring
 
@@ -88,21 +76,28 @@ noncomputable def ann_ideal_generator (a : A) : 𝕜[X] :=
 let g := is_principal.generator $ ann_ideal 𝕜 a
   in (C g.leading_coeff⁻¹) * g
 
+section
+
+variables {𝕜}
+
+@[simp] lemma ann_ideal_generator_eq_zero_iff {a : A} :
+  ann_ideal_generator 𝕜 a = 0 ↔ ann_ideal 𝕜 a = ⊥ :=
+by simp only [ann_ideal_generator, mul_eq_zero, is_principal.eq_bot_iff_generator_eq_zero,
+  polynomial.C_eq_zero, inv_eq_zero, polynomial.leading_coeff_eq_zero, or_self]
+end
+
 /-- `ann_ideal_generator 𝕜 a` is indeed a generator. -/
-lemma span_singleton_ann_ideal_generator (a : A) :
-  span 𝕜[X] {ann_ideal_generator 𝕜 a} = ann_ideal 𝕜 a :=
+@[simp] lemma span_singleton_ann_ideal_generator (a : A) :
+  ideal.span {ann_ideal_generator 𝕜 a} = ann_ideal 𝕜 a :=
 begin
-  by_cases (is_principal.generator $ ann_ideal 𝕜 a) = 0,
-  { rw ← is_principal.eq_bot_iff_generator_eq_zero at h,
-   simp only [ann_ideal_generator, h], simp, rw ← is_principal.eq_bot_iff_generator_eq_zero, },
-  { simp only [ann_ideal_generator, ann_ideal, alg_hom.to_ring_hom_eq_coe, ideal.submodule_span_eq],
-    rw ideal.span_singleton_mul_left_unit,
-    { exact is_principal.span_singleton_generator _ },
-    { rw [is_unit_C, is_unit_iff_ne_zero],
-      apply inv_ne_zero,
-      rw [ne.def, leading_coeff_eq_zero_iff_deg_eq_bot, degree_eq_bot],
-      rw [ann_ideal, alg_hom.to_ring_hom_eq_coe] at h,
-      apply h, }, },
+  by_cases h : ann_ideal_generator 𝕜 a = 0,
+  { rw [h, ann_ideal_generator_eq_zero_iff.mp h, set.singleton_zero, ideal.span_zero] },
+  { rw [ann_ideal_generator, ideal.span_singleton_mul_left_unit, ideal.span_singleton_generator],
+    apply polynomial.is_unit_C.mpr,
+    apply is_unit.mk0,
+    apply inv_eq_zero.not.mpr,
+    apply polynomial.leading_coeff_eq_zero.not.mpr,
+    apply (mul_ne_zero_iff.mp h).2, }
 end
 
 /-- The annihilating ideal generator is a member of the annihilating ideal,
@@ -115,7 +110,8 @@ let I := ann_ideal 𝕜 a,
 /-- sourced from submodule.mem_iff_eq_smul_generator -/
 lemma mem_iff_eq_smul_ann_ideal_generator {p : 𝕜[X]} (a : A) :
   p ∈ ann_ideal 𝕜 a ↔ ∃ s : 𝕜[X], p = s • ann_ideal_generator 𝕜 a :=
-by simp_rw [@eq_comm _ p, ← mem_span_singleton, span_singleton_ann_ideal_generator]
+by simp_rw [@eq_comm _ p, ← mem_span_singleton, ← span_singleton_ann_ideal_generator 𝕜 a,
+ ideal.span]
 
 /-- sourced from submodule.eq_bot_iff_generator_eq_zero -/
 lemma eq_bot_iff_ann_ideal_generator_eq_zero (a : A) :
@@ -126,8 +122,8 @@ begin
 end
 
 /-- The generator we chose for the annihilating ideal is monic when the ideal is non-zero. -/
-lemma monic_of_ann_ideal_generator (a : A) (hg : (ann_ideal_generator 𝕜 a : 𝕜[X]) ≠ 0) :
-  monic (ann_ideal_generator 𝕜 a : 𝕜[X]) :=
+lemma monic_ann_ideal_generator (a : A) (hg : ann_ideal_generator 𝕜 a ≠ 0) :
+  monic (ann_ideal_generator 𝕜 a) :=
 begin
   dunfold ann_ideal_generator,
   dsimp *,
@@ -143,7 +139,7 @@ in the field case is the minimal polynomial. We are going to use a uniqueness
 theorem of the minimal polynomial. This is the first condition: it must annihilate
 the original element `a : A`. -/
 lemma ann_ideal_generator_aeval_eq_zero (a : A) :
-  aeval a (ann_ideal_generator 𝕜 a : 𝕜[X]) = 0 :=
+  aeval a (ann_ideal_generator 𝕜 a) = 0 :=
 begin
   have hg : aeval a (is_principal.generator (ann_ideal 𝕜 a)) = 0,
   { have gen_member := submodule.is_principal.generator_mem (ann_ideal 𝕜 a),
@@ -161,35 +157,41 @@ lemma mem_iff_ann_ideal_generator_dvd (a : A) {x : 𝕜[X]} :
  the non-zero members of the annihilating ideal -/
 lemma degree_ann_ideal_generator_le_of_mem (a : A) (p : 𝕜[X])
   (hp : p ∈ ann_ideal 𝕜 a) (hpn0 : p ≠ 0) :
-  degree (ann_ideal_generator 𝕜 a : 𝕜[X]) ≤ degree p :=
+  degree (ann_ideal_generator 𝕜 a) ≤ degree p :=
 degree_le_of_dvd ((mem_iff_ann_ideal_generator_dvd 𝕜 a).1 hp) hpn0
 
-/-- This is what we have been building to:
-The monic generator of the annihilating ideal is the minimal polynomial. -/
-lemma minpoly_eq_monic_ann_ideal_generator (a : A) :
+variables (𝕜)
+
+/-- The generator of the annihilating ideal is the minimal polynomial. -/
+lemma ann_ideal_generator_eq_minpoly (a : A) :
   ann_ideal_generator 𝕜 a = minpoly 𝕜 a :=
 begin
-  by_cases (ann_ideal_generator 𝕜 a) = 0,
-  { /- case: generator is zero -/
-    rw h, apply eq.symm, apply minpoly.eq_zero, unfold is_integral,
-    by_contra hi, cases hi with p hp,
-    have hpnz : p ≠ 0, { apply monic.ne_zero hp.left, },
-    have hmem : p ∈ ann_ideal 𝕜 a,
-    { exact mem_ann_ideal_of_eval₂_algebra_map_eq_zero a p hp.right },
-    rw [mem_iff_ann_ideal_generator_dvd 𝕜 a, h] at hmem,
-    exact hpnz (eq_zero_of_zero_dvd hmem), },
-  { /- case: generator is not zero -/
-    /- 3 conditions for a poly being the minpoly -/
-    apply minpoly.unique,
-  /- 1st condition: the poly is monic -/
-  { exact monic_of_ann_ideal_generator 𝕜 a h, },
-  /- 2nd condition: the poly annihilates a -/
-  { apply ann_ideal_generator_aeval_eq_zero, },
-  /- 3rd condition: the poly has minimal degree among annihilators of a -/
-  { intros q hqm heval,
-    apply degree_ann_ideal_generator_le_of_mem 𝕜 a q _ _,
-    exact (mem_ann_ideal_iff_aeval_eq_zero a q).2 heval,
-    exact monic.ne_zero hqm, } }
+  by_cases h : ann_ideal_generator 𝕜 a = 0,
+  { rw [h, minpoly.eq_zero],
+    rintro ⟨p, p_monic, (hp : aeval a p = 0)⟩,
+    refine p_monic.ne_zero (ideal.mem_bot.mp _),
+    simpa only [ann_ideal_generator_eq_zero_iff.mp h]
+      using (mem_ann_ideal_iff_aeval_eq_zero a _).mpr hp },
+  { exact minpoly.unique _ _
+      (monic_ann_ideal_generator _ _ h)
+      (ann_ideal_generator_aeval_eq_zero _ _)
+      (λ q q_monic hq, (degree_ann_ideal_generator_le_of_mem _ a q
+        ((mem_ann_ideal_iff_aeval_eq_zero a q).mpr hq)
+        q_monic.ne_zero)) }
+end
+
+/-- If a monic generates the annihilating ideal, it must match our choice
+ of the annihilating ideal generator. -/
+lemma monic_generator_eq_minpoly (a : A) (p : 𝕜[X])
+  (p_monic : p.monic) (p_gen : ideal.span {p} = ann_ideal 𝕜 a) :
+  ann_ideal_generator 𝕜 a = p :=
+begin
+  by_cases h : p = 0,
+  { rwa [h, ann_ideal_generator_eq_zero_iff, ← p_gen, ideal.span_singleton_eq_bot.mpr], },
+  { rw [← span_singleton_ann_ideal_generator, ideal.span_singleton_eq_span_singleton] at p_gen,
+    rw eq_comm,
+    apply eq_of_monic_of_associated p_monic _ p_gen,
+    { apply monic_ann_ideal_generator _ _ ((associated.ne_zero_iff p_gen).mp h), }, },
 end
 
 end field

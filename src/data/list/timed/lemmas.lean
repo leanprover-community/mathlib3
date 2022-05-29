bdca0923b81ad96f1b85f847011adc5298eed7aa
Tomaz Gomes <tomgm1502@gmail.com>
2022-05-29 20:23:50 -0300

    Replace: Author -> Authors

--- src/data/list/timed/lemmas.lean
+++ src/data/list/timed/lemmas.lean
@@ -1,7 +1,7 @@
 /-
 Copyright (c) 2022 Tomaz Gomes. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
-Author: Tomaz Gomes.
+Authors: Tomaz Gomes
 -/
 import tactic
 import data.nat.log

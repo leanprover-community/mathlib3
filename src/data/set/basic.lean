57a25421aa0364eb592928703e4df287af70a1ec
Violeta Hernández <vi.hdz.p@gmail.com>
2022-05-29 14:11:20 -0500

    change namespace

--- src/data/set/basic.lean
+++ src/data/set/basic.lean
@@ -1622,7 +1622,7 @@ begin
 end
 
 /-- The subtype of subsets of `t` is equivalent to `set ↥t`. -/
-def subset_equiv_set_coe {α : Type*} (t : set α) : {s : set α // s ⊆ t} ≃ set t :=
+def _root_.equiv.subset_equiv_set_coe {α : Type*} (t : set α) : {s : set α // s ⊆ t} ≃ set t :=
 { to_fun := λ s, {x | ↑x ∈ s.1},
   inv_fun := λ s, ⟨s.image subtype.val, image_subset_iff.2 $ λ x _, x.2⟩,
   left_inv := λ s, by { ext, simpa using (λ hx, s.2 hx) },

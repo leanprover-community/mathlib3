03391a489cad8256c1f63c8d8f4ceaa29bf9d253
Yury G. Kudryashov <urkud@urkud.name>
2022-05-31 00:18:22 -0400

    Add `to_lex_monotone`

--- src/data/pi/lex.lean
+++ src/data/pi/lex.lean
@@ -90,15 +90,19 @@ lemma lex.le_of_forall_le [linear_order ι] [is_well_order ι (<)] [Π a, linear
   {a b : lex (Π i, β i)} (h : ∀ i, a i ≤ b i) : a ≤ b :=
 le_of_not_lt (λ ⟨i, hi⟩, (h i).not_lt hi.2)
 
+lemma to_lex_monotone [linear_order ι] [is_well_order ι (<)] [Π a, linear_order (β a)] :
+  monotone (@to_lex (Π i, β i)) :=
+λ _ _, lex.le_of_forall_le
+
 instance [linear_order ι] [is_well_order ι (<)] [Π a, linear_order (β a)]
   [Π a, order_bot (β a)] : order_bot (lex (Π a, β a)) :=
 { bot := to_lex ⊥,
-  bot_le := λ f, lex.le_of_forall_le $ λ a, bot_le }
+  bot_le := λ f, to_lex_monotone bot_le }
 
 instance [linear_order ι] [is_well_order ι (<)] [Π a, linear_order (β a)]
   [Π a, order_top (β a)] : order_top (lex (Π a, β a)) :=
 { top := to_lex ⊤,
-  le_top := λ f, lex.le_of_forall_le $ λ a, le_top }
+  le_top := λ f, to_lex_monotone le_top }
 
 instance [linear_order ι] [is_well_order ι (<)] [Π a, linear_order (β a)]
   [Π a, bounded_order (β a)] : bounded_order (lex (Π a, β a)) :=

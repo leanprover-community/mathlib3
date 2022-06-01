567a44ae69e6fe7e53ad5184490135459be332cb
Alex J Best <alex.j.best@gmail.com>
2022-06-01 20:18:23 +0000

    rename

--- src/data/nat/parity.lean
+++ src/data/nat/parity.lean
@@ -72,7 +72,7 @@ begin
               one_ne_zero, and_self] },
 end
 
-lemma zero_lt_of_odd (h : odd n) : 0 < n :=
+lemma pos_of_odd (h : odd n) : 0 < n :=
 by { obtain ⟨k, rfl⟩ := h, exact succ_pos' }
 
 @[simp] theorem two_dvd_ne_zero : ¬ 2 ∣ n ↔ n % 2 = 1 :=

fbc659983f33ee3988e557f08060f473f27395fc
Bolton Bailey <bolton.bailey@gmail.com>
2022-06-01 10:19:51 -0400

    delete lemma

--- src/data/nat/choose/factorization.lean
+++ src/data/nat/choose/factorization.lean
@@ -47,9 +47,6 @@ begin
   refine (finset.card_filter_le _ _).trans (le_of_eq (nat.card_Ico _ _)),
 end
 
-lemma factorization_central_binom_le : (central_binom n).factorization p ≤ log p (2 * n) :=
-factorization_choose_le
-
 /--
 A `pow` form of `nat.factorization_central_binom_le`
 -/

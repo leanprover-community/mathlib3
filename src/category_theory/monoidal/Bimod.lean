a1b110107ce4c98b933b8d00df2d90b9ac5ba1f8
Oleksandr Manzyuk <manzyuk@gmail.com>
2022-06-01 08:23:41 +0100

    Add myself as an author

--- src/category_theory/monoidal/Bimod.lean
+++ src/category_theory/monoidal/Bimod.lean
@@ -1,7 +1,7 @@
 /-
 Copyright (c) 2022 Scott Morrison. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
-Authors: Scott Morrison
+Authors: Scott Morrison, Oleksandr Manzyuk
 -/
 import category_theory.bicategory.basic
 import category_theory.monoidal.Mon_

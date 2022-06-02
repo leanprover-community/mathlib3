06c4cfdd207e19364d71b3eff01e3778587685cb
Adam Topaz <github@adamtopaz.com>
2022-06-02 11:24:10 -0400

    add lemma about sheafiification compatibility

--- src/category_theory/sites/cover_lifting.lean
+++ src/category_theory/sites/cover_lifting.lean
@@ -295,7 +295,7 @@ variables
   [∀ (X : D), has_colimits_of_shape (K.cover X)ᵒᵖ A]
 
 /-- The isomorphism exhibiting compatibility between pullback and sheafification. -/
-def whiskering_left_comp_presheaf_to_Sheaf_iso_presheaf_to_Sheaf_comp_pullback
+def pullback_sheafification_compatibility
   {G : C ⥤ D} (Hp : cover_preserving J K G)
   (Hl : cover_lifting J K G) (Hc : compatible_preserving K G) :
   (whiskering_left _ _ A).obj G.op ⋙ presheaf_to_Sheaf J A ≅
@@ -304,16 +304,49 @@ let A1 : (whiskering_left _ _ A).obj G.op ⊣ _ := Ran.adjunction _ _,
     A2 : presheaf_to_Sheaf J A ⊣ _ := sheafification_adjunction _ _,
     B1 : presheaf_to_Sheaf K A ⊣ _ := sheafification_adjunction _ _,
     B2 : sites.pullback A Hc Hp ⊣ _ := sites.pullback_copullback_adjunction _ _ Hl _,
-    A := A1.comp _ _ A2,
-    B := B1.comp _ _ B2 in
-A.left_adjoint_uniq B
+    A12 := A1.comp _ _ A2,
+    B12 := B1.comp _ _ B2 in
+A12.left_adjoint_uniq B12
 
-lemma whiskering_left_comp_presheaf_to_Sheaf_iso_presheaf_to_Sheaf_comp_pullback_hom_apply_val
+lemma to_sheafify_pullback_sheafification_compatibility
   {G : C ⥤ D} (Hp : cover_preserving J K G)
   (Hl : cover_lifting J K G) (Hc : compatible_preserving K G) (F) :
-((sites.whiskering_left_comp_presheaf_to_Sheaf_iso_presheaf_to_Sheaf_comp_pullback.{w v u}
-  A Hp Hl Hc).hom.app F).val = J.sheafify_lift
-  (whisker_left _ (K.to_sheafify _)) (Sheaf.cond _) := sorry
+  J.to_sheafify (G.op ⋙ F) ≫
+  ((pullback_sheafification_compatibility.{w v u} A Hp Hl Hc).hom.app F).val =
+  whisker_left _ (K.to_sheafify _) :=
+begin
+  dsimp [pullback_sheafification_compatibility, adjunction.left_adjoint_uniq],
+  apply quiver.hom.op_inj,
+  apply coyoneda.map_injective, swap, apply_instance,
+  ext E f : 2,
+  dsimp [functor.preimage, full.preimage, coyoneda, adjunction.left_adjoints_coyoneda_equiv],
+  simp only [adjunction.hom_equiv_unit, functor.comp_map, Sheaf_to_presheaf_map,
+    adjunction.hom_equiv_naturality_left_symm, whiskering_left_obj_map,
+    adjunction.hom_equiv_counit, Sheaf.category_theory.category_comp_val,
+    presheaf_to_Sheaf_map_val],
+  dsimp [adjunction.comp],
+  simp only [sheafification_adjunction_unit_app, category.comp_id, functor.map_id,
+    whisker_left_id', category.assoc, grothendieck_topology.sheafify_map_sheafify_lift,
+    category.id_comp, grothendieck_topology.to_sheafify_sheafify_lift],
+  ext t,
+  dsimp, simp only [category.assoc], congr' 1, simp only [← category.assoc],
+  convert category.id_comp _,
+  dsimp [pullback_sheaf],
+  have := (Ran.adjunction A G.op).left_triangle,
+  apply_fun (λ e, (e.app (K.sheafify F)).app x) at this,
+  exact this,
+end
+
+@[simp]
+lemma pullback_sheafification_compatibility_hom_apply_val
+  {G : C ⥤ D} (Hp : cover_preserving J K G)
+  (Hl : cover_lifting J K G) (Hc : compatible_preserving K G) (F) :
+((pullback_sheafification_compatibility.{w v u} A Hp Hl Hc).hom.app F).val =
+  J.sheafify_lift (whisker_left _ $ K.to_sheafify _) (Sheaf.cond _) :=
+begin
+  apply J.sheafify_lift_unique,
+  apply to_sheafify_pullback_sheafification_compatibility,
+end
 
 end sites
 

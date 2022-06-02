73fa7a0dd534c1e7705c7f2f6fdb80ecf573d284
Violeta Hernández <vi.hdz.p@gmail.com>
2022-06-02 00:57:23 -0500

    turn lemmas to instances

--- src/set_theory/ordinal/arithmetic.lean
+++ src/set_theory/ordinal/arithmetic.lean
@@ -172,8 +172,8 @@ instance : nontrivial ordinal.{u} :=
 @[simp] theorem zero_lt_one : (0 : ordinal) < 1 :=
 lt_iff_le_and_ne.2 ⟨ordinal.zero_le _, ne.symm $ ordinal.one_ne_zero⟩
 
-@[simp] theorem zero_le_one : (0 : ordinal) ≤ 1 :=
-zero_lt_one.le
+instance : zero_le_one_class ordinal :=
+⟨zero_lt_one.le⟩ 
 
 instance unique_out_one : unique (1 : ordinal).out.α :=
 { default := enum (<) 0 (by simp),

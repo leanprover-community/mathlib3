a1f0ba7b0095e991db167244f3fbbcea232ec6ee
Violeta Hernández <vi.hdz.p@gmail.com>
2022-06-03 17:40:44 -0500

    Update src/algebra/group_power/lemmas.lean
    Co-authored-by: damiano <adomani@gmail.com>

--- src/algebra/group_power/lemmas.lean
+++ src/algebra/group_power/lemmas.lean
@@ -524,7 +524,7 @@ theorem nat.cast_le_pow_sub_div_sub {K : Type*} [linear_ordered_field K] {a : K}
   (n : ℕ) :
   (n : K) ≤ (a ^ n - 1) / (a - 1) :=
 (le_div_iff (sub_pos.2 H)).2 $ le_sub_left_of_add_le $
-  one_add_mul_sub_le_pow ((neg_le_self $ @zero_le_one K _ _ _ _).trans H.le) _
+  one_add_mul_sub_le_pow ((neg_le_self zero_le_one).trans H.le) _
 
 /-- For any `a > 1` and a natural `n` we have `n ≤ a ^ n / (a - 1)`. See also
 `nat.cast_le_pow_sub_div_sub` for a stronger inequality with `a ^ n - 1` in the numerator. -/

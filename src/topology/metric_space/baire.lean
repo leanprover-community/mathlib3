1853ffbad5bcbbeba78fdf8742e55daeced86547
Yury G. Kudryashov <urkud@urkud.name>
2022-06-04 20:12:11 -0400

    Reorder to make the diff smaller

--- src/topology/metric_space/baire.lean
+++ src/topology/metric_space/baire.lean
@@ -34,26 +34,12 @@ section Baire_theorem
 
 open emetric ennreal
 
-
 /-- The property `baire_space α` means that the topological space `α` has the Baire property:
 any countable intersection of open dense subsets is dense.
 Formulated here when the source space is ℕ (and subsumed below by `dense_Inter_of_open` working
 with any encodable source space).-/
 class baire_space (α : Type*) [topological_space α] : Prop :=
-  (baire_property : ∀ {f : ℕ → set α} (ho : ∀n, is_open (f n)) (hd : ∀n, dense (f n)),
-  dense (⋂n, f n))
-
-
-/-- Definition of a Baire space. -/
-theorem dense_Inter_of_open_nat {α : Type*} [topological_space α] [baire_space α] {f : ℕ → set α}
-  (ho : ∀ n, is_open (f n)) (hd : ∀ n, dense (f n)) :
-  dense (⋂ n, f n) :=
-begin
-  apply _inst_2.baire_property,
-  { exact ho },
-  { exact hd },
-end
-
+(baire_property : ∀ f : ℕ → set α, (∀ n, is_open (f n)) → (∀ n, dense (f n)) → dense (⋂n, f n))
 
 /-- Baire theorems asserts that various topological spaces have the Baire property.
 Two versions of these theorems are given.
@@ -61,97 +47,94 @@ The first states that complete pseudo_emetric spaces are Baire. -/
 @[priority 100]
 instance baire_category_theorem_emetric_complete [pseudo_emetric_space α] [complete_space α] :
   baire_space α :=
-{ baire_property :=
-  begin
-    intros f ho hd,
-    let B : ℕ → ℝ≥0∞ := λn, 1/2^n,
-    have Bpos : ∀n, 0 < B n,
-    { intro n,
-      simp only [B, one_div, one_mul, ennreal.inv_pos],
-      exact pow_ne_top two_ne_top },
-    /- Translate the density assumption into two functions `center` and `radius` associating
-    to any n, x, δ, δpos a center and a positive radius such that
-    `closed_ball center radius` is included both in `f n` and in `closed_ball x δ`.
-    We can also require `radius ≤ (1/2)^(n+1)`, to ensure we get a Cauchy sequence later. -/
-    have : ∀n x δ, δ ≠ 0 → ∃y r, 0 < r ∧ r ≤ B (n+1) ∧ closed_ball y r ⊆ (closed_ball x δ) ∩ f n,
-    { assume n x δ δpos,
-      have : x ∈ closure (f n) := hd n x,
-      rcases emetric.mem_closure_iff.1 this (δ/2) (ennreal.half_pos δpos) with ⟨y, ys, xy⟩,
-      rw edist_comm at xy,
-      obtain ⟨r, rpos, hr⟩ : ∃ r > 0, closed_ball y r ⊆ f n :=
-        nhds_basis_closed_eball.mem_iff.1 (is_open_iff_mem_nhds.1 (ho n) y ys),
-      refine ⟨y, min (min (δ/2) r) (B (n+1)), _, _, λz hz, ⟨_, _⟩⟩,
-      show 0 < min (min (δ / 2) r) (B (n+1)),
-        from lt_min (lt_min (ennreal.half_pos δpos) rpos) (Bpos (n+1)),
-      show min (min (δ / 2) r) (B (n+1)) ≤ B (n+1), from min_le_right _ _,
-      show z ∈ closed_ball x δ, from calc
-        edist z x ≤ edist z y + edist y x : edist_triangle _ _ _
-        ... ≤ (min (min (δ / 2) r) (B (n+1))) + (δ/2) : add_le_add hz (le_of_lt xy)
-        ... ≤ δ/2 + δ/2 : add_le_add (le_trans (min_le_left _ _) (min_le_left _ _)) le_rfl
-        ... = δ : ennreal.add_halves δ,
-      show z ∈ f n, from hr (calc
-        edist z y ≤ min (min (δ / 2) r) (B (n+1)) : hz
-        ... ≤ r : le_trans (min_le_left _ _) (min_le_right _ _)) },
-    choose! center radius Hpos HB Hball using this,
-    refine λ x, (mem_closure_iff_nhds_basis nhds_basis_closed_eball).2 (λ ε εpos, _),
-    /- `ε` is positive. We have to find a point in the ball of radius `ε` around `x` belonging
-    to all `f n`. For this, we construct inductively a sequence `F n = (c n, r n)` such that the
-    closed ball `closed_ball (c n) (r n)` is included in the previous ball and in `f n`,
-    and such that `r n` is small enough to ensure that `c n` is a Cauchy sequence.
-    Then `c n` converges to a limit which belongs to all the `f n`. -/
-    let F : ℕ → (α × ℝ≥0∞) := λn, nat.rec_on n (prod.mk x (min ε (B 0)))
-                                (λn p, prod.mk (center n p.1 p.2) (radius n p.1 p.2)),
-    let c : ℕ → α := λn, (F n).1,
-    let r : ℕ → ℝ≥0∞ := λn, (F n).2,
-    have rpos : ∀ n, 0 < r n,
-    { assume n,
-      induction n with n hn,
-      exact lt_min εpos (Bpos 0),
-      exact Hpos n (c n) (r n) hn.ne' },
-    have r0 : ∀ n, r n ≠ 0 := λ n, (rpos n).ne',
-    have rB : ∀n, r n ≤ B n,
-    { assume n,
-      induction n with n hn,
-      exact min_le_right _ _,
-      exact HB n (c n) (r n) (r0 n) },
-    have incl : ∀n, closed_ball (c (n+1)) (r (n+1)) ⊆ (closed_ball (c n) (r n)) ∩ (f n) :=
-      λ n, Hball n (c n) (r n) (r0 n),
-    have cdist : ∀n, edist (c n) (c (n+1)) ≤ B n,
-    { assume n,
-      rw edist_comm,
-      have A : c (n+1) ∈ closed_ball (c (n+1)) (r (n+1)) := mem_closed_ball_self,
-      have I := calc
-        closed_ball (c (n+1)) (r (n+1)) ⊆ closed_ball (c n) (r n) :
-          subset.trans (incl n) (inter_subset_left _ _)
-        ... ⊆ closed_ball (c n) (B n) : closed_ball_subset_closed_ball (rB n),
-      exact I A },
-    have : cauchy_seq c :=
-      cauchy_seq_of_edist_le_geometric_two _ one_ne_top cdist,
-    -- as the sequence `c n` is Cauchy in a complete space, it converges to a limit `y`.
-    rcases cauchy_seq_tendsto_of_complete this with ⟨y, ylim⟩,
-    -- this point `y` will be the desired point. We will check that it belongs to all
-    -- `f n` and to `ball x ε`.
-    use y,
-    simp only [exists_prop, set.mem_Inter],
-    have I : ∀n, ∀m ≥ n, closed_ball (c m) (r m) ⊆ closed_ball (c n) (r n),
-    { assume n,
-      refine nat.le_induction _ (λm hnm h, _),
-      { exact subset.refl _ },
-      { exact subset.trans (incl m) (subset.trans (inter_subset_left _ _) h) }},
-    have yball : ∀n, y ∈ closed_ball (c n) (r n),
-    { assume n,
-      refine is_closed_ball.mem_of_tendsto ylim _,
-      refine (filter.eventually_ge_at_top n).mono (λ m hm, _),
-      exact I n m hm mem_closed_ball_self },
-    split,
-    show ∀n, y ∈ f n,
-    { assume n,
-      have : closed_ball (c (n+1)) (r (n+1)) ⊆ f n :=
-        subset.trans (incl n) (inter_subset_right _ _),
-      exact this (yball (n+1)) },
-    show edist y x ≤ ε, from le_trans (yball 0) (min_le_left _ _),
-  end }
-
+begin
+  refine ⟨λ f ho hd, _⟩,
+  let B : ℕ → ℝ≥0∞ := λn, 1/2^n,
+  have Bpos : ∀n, 0 < B n,
+  { intro n,
+    simp only [B, one_div, one_mul, ennreal.inv_pos],
+    exact pow_ne_top two_ne_top },
+  /- Translate the density assumption into two functions `center` and `radius` associating
+  to any n, x, δ, δpos a center and a positive radius such that
+  `closed_ball center radius` is included both in `f n` and in `closed_ball x δ`.
+  We can also require `radius ≤ (1/2)^(n+1)`, to ensure we get a Cauchy sequence later. -/
+  have : ∀n x δ, δ ≠ 0 → ∃y r, 0 < r ∧ r ≤ B (n+1) ∧ closed_ball y r ⊆ (closed_ball x δ) ∩ f n,
+  { assume n x δ δpos,
+    have : x ∈ closure (f n) := hd n x,
+    rcases emetric.mem_closure_iff.1 this (δ/2) (ennreal.half_pos δpos) with ⟨y, ys, xy⟩,
+    rw edist_comm at xy,
+    obtain ⟨r, rpos, hr⟩ : ∃ r > 0, closed_ball y r ⊆ f n :=
+      nhds_basis_closed_eball.mem_iff.1 (is_open_iff_mem_nhds.1 (ho n) y ys),
+    refine ⟨y, min (min (δ/2) r) (B (n+1)), _, _, λz hz, ⟨_, _⟩⟩,
+    show 0 < min (min (δ / 2) r) (B (n+1)),
+      from lt_min (lt_min (ennreal.half_pos δpos) rpos) (Bpos (n+1)),
+    show min (min (δ / 2) r) (B (n+1)) ≤ B (n+1), from min_le_right _ _,
+    show z ∈ closed_ball x δ, from calc
+      edist z x ≤ edist z y + edist y x : edist_triangle _ _ _
+      ... ≤ (min (min (δ / 2) r) (B (n+1))) + (δ/2) : add_le_add hz (le_of_lt xy)
+      ... ≤ δ/2 + δ/2 : add_le_add (le_trans (min_le_left _ _) (min_le_left _ _)) le_rfl
+      ... = δ : ennreal.add_halves δ,
+    show z ∈ f n, from hr (calc
+      edist z y ≤ min (min (δ / 2) r) (B (n+1)) : hz
+      ... ≤ r : le_trans (min_le_left _ _) (min_le_right _ _)) },
+  choose! center radius Hpos HB Hball using this,
+  refine λ x, (mem_closure_iff_nhds_basis nhds_basis_closed_eball).2 (λ ε εpos, _),
+  /- `ε` is positive. We have to find a point in the ball of radius `ε` around `x` belonging to all
+  `f n`. For this, we construct inductively a sequence `F n = (c n, r n)` such that the closed ball
+  `closed_ball (c n) (r n)` is included in the previous ball and in `f n`, and such that
+  `r n` is small enough to ensure that `c n` is a Cauchy sequence. Then `c n` converges to a
+  limit which belongs to all the `f n`. -/
+  let F : ℕ → (α × ℝ≥0∞) := λn, nat.rec_on n (prod.mk x (min ε (B 0)))
+                              (λn p, prod.mk (center n p.1 p.2) (radius n p.1 p.2)),
+  let c : ℕ → α := λn, (F n).1,
+  let r : ℕ → ℝ≥0∞ := λn, (F n).2,
+  have rpos : ∀ n, 0 < r n,
+  { assume n,
+    induction n with n hn,
+    exact lt_min εpos (Bpos 0),
+    exact Hpos n (c n) (r n) hn.ne' },
+  have r0 : ∀ n, r n ≠ 0 := λ n, (rpos n).ne',
+  have rB : ∀n, r n ≤ B n,
+  { assume n,
+    induction n with n hn,
+    exact min_le_right _ _,
+    exact HB n (c n) (r n) (r0 n) },
+  have incl : ∀n, closed_ball (c (n+1)) (r (n+1)) ⊆ (closed_ball (c n) (r n)) ∩ (f n) :=
+    λ n, Hball n (c n) (r n) (r0 n),
+  have cdist : ∀n, edist (c n) (c (n+1)) ≤ B n,
+  { assume n,
+    rw edist_comm,
+    have A : c (n+1) ∈ closed_ball (c (n+1)) (r (n+1)) := mem_closed_ball_self,
+    have I := calc
+      closed_ball (c (n+1)) (r (n+1)) ⊆ closed_ball (c n) (r n) :
+        subset.trans (incl n) (inter_subset_left _ _)
+      ... ⊆ closed_ball (c n) (B n) : closed_ball_subset_closed_ball (rB n),
+    exact I A },
+  have : cauchy_seq c :=
+    cauchy_seq_of_edist_le_geometric_two _ one_ne_top cdist,
+  -- as the sequence `c n` is Cauchy in a complete space, it converges to a limit `y`.
+  rcases cauchy_seq_tendsto_of_complete this with ⟨y, ylim⟩,
+  -- this point `y` will be the desired point. We will check that it belongs to all
+  -- `f n` and to `ball x ε`.
+  use y,
+  simp only [exists_prop, set.mem_Inter],
+  have I : ∀n, ∀m ≥ n, closed_ball (c m) (r m) ⊆ closed_ball (c n) (r n),
+  { assume n,
+    refine nat.le_induction _ (λm hnm h, _),
+    { exact subset.refl _ },
+    { exact subset.trans (incl m) (subset.trans (inter_subset_left _ _) h) }},
+  have yball : ∀n, y ∈ closed_ball (c n) (r n),
+  { assume n,
+    refine is_closed_ball.mem_of_tendsto ylim _,
+    refine (filter.eventually_ge_at_top n).mono (λ m hm, _),
+    exact I n m hm mem_closed_ball_self },
+  split,
+  show ∀n, y ∈ f n,
+  { assume n,
+    have : closed_ball (c (n+1)) (r (n+1)) ⊆ f n := subset.trans (incl n) (inter_subset_right _ _),
+    exact this (yball (n+1)) },
+  show edist y x ≤ ε, from le_trans (yball 0) (min_le_left _ _),
+end
 
 /-- The second theorem states that locally compact spaces are Baire. -/
 @[priority 100]
@@ -229,9 +212,12 @@ instance baire_category_theorem_locally_compact [topological_space α] [t2_space
         exact is_compact.is_closed (compact_seq_is_compact_nonempty n).1 } },
   end }
 
-
 variables [topological_space α] [baire_space α]
 
+/-- Definition of a Baire space. -/
+theorem dense_Inter_of_open_nat {f : ℕ → set α} (ho : ∀ n, is_open (f n)) (hd : ∀ n, dense (f n)) :
+  dense (⋂ n, f n) :=
+baire_space.baire_property f ho hd
 
 /-- Baire theorem: a countable intersection of dense open sets is dense. Formulated here with ⋂₀. -/
 theorem dense_sInter_of_open {S : set (set α)} (ho : ∀s∈S, is_open s) (hS : countable S)

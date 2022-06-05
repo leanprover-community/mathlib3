bedde51fc18f9151f538ee3e25cdfb51102c1858
Yury G. Kudryashov <urkud@urkud.name>
2022-06-05 17:52:07 -0400

    Snapshot

--- src/topology/continuous_function/basic.lean
+++ src/topology/continuous_function/basic.lean
@@ -105,8 +105,7 @@ lemma coe_injective : @function.injective (C(α, β)) (α → β) coe_fn :=
 @[simp] lemma coe_mk (f : α → β) (h : continuous f) :
   ⇑(⟨f, h⟩ : C(α, β)) = f := rfl
 
-lemma map_specialization {x y : α} (h : x ⤳ y) (f : C(α, β)) : f x ⤳ f y :=
-h.map f.2
+lemma map_specializes (f : C(α, β)) {x y : α} (h : x ⤳ y) : f x ⤳ f y := h.map f.2
 
 section
 variables (α β)

d0f0e10ef056e8e87cd22eb76234b1f3eaeed595
Hanting Zhang <hantingz@usc.edu>
2022-06-04 17:52:47 -0700

    add suggestions

--- src/linear_algebra/affine_space/pointwise.lean
+++ src/linear_algebra/affine_space/pointwise.lean
@@ -20,8 +20,14 @@ open set
 
 
 namespace affine_subspace
-variables {k : Type*} {V : Type*} {P : Type*} [ring k] [add_comm_group V] [module k V]
-          [affine_space V P]
+
+variables {k : Type*} [ring k]
+variables {V P V₁ P₁ V₂ P₂: Type*}
+
+variables [add_comm_group V] [module k V] [affine_space V P]
+variables [add_comm_group V₁] [module k V₁] [add_torsor V₁ P₁]
+variables [add_comm_group V₂] [module k V₂] [add_torsor V₂ P₂]
+
 include V
 
 /-- The additive action on an affine subspace corresponding to applying the action to every element.
@@ -40,14 +46,14 @@ open_locale pointwise
 @[simp] lemma coe_pointwise_vadd (v : V) (s : affine_subspace k P) :
   ((v +ᵥ s : affine_subspace k P) : set P) = v +ᵥ s := rfl
 
-lemma vadd_mem_pointwise_vadd_iff (v : V) {s : affine_subspace k P} {p : P} :
+lemma vadd_mem_pointwise_vadd_iff {v : V} {s : affine_subspace k P} {p : P} :
   v +ᵥ p ∈ v +ᵥ s ↔ p ∈ s :=
 vadd_mem_vadd_set_iff
 
-lemma pointwise_vadd_bot {v : V} : v +ᵥ (⊥ : affine_subspace k P) = ⊥ :=
+lemma pointwise_vadd_bot (v : V) : v +ᵥ (⊥ : affine_subspace k P) = ⊥ :=
 by { ext, simp, }
 
-lemma pointwise_vadd_direction (v : V) {s : affine_subspace k P} :
+lemma pointwise_vadd_direction (v : V) (s : affine_subspace k P) :
   (v +ᵥ s).direction = s.direction :=
 begin
   unfold has_vadd.vadd,
@@ -55,18 +61,14 @@ begin
   exact submodule.map_id _,
 end
 
-lemma pointwise_vadd_span (v : V) {s : set P} :
+lemma pointwise_vadd_span (v : V) (s : set P) :
   v +ᵥ affine_span k s = affine_span k (v +ᵥ s) :=
 map_span _ s
 
-variables {V₁ P₁ V₂ P₂ V₃ P₃ : Type*}
-variables [add_comm_group V₁] [module k V₁] [add_torsor V₁ P₁]
-variables [add_comm_group V₂] [module k V₂] [add_torsor V₂ P₂]
-variables [add_comm_group V₃] [module k V₃] [add_torsor V₃ P₃]
-include V₁ V₂
 omit V
+include V₁ V₂
 
-lemma map_pointwise_vadd {f : P₁ →ᵃ[k] P₂} (v : V₁) (s : affine_subspace k P₁) :
+lemma map_pointwise_vadd (f : P₁ →ᵃ[k] P₂) (v : V₁) (s : affine_subspace k P₁) :
   (v +ᵥ s).map f = f.linear v +ᵥ s.map f :=
 begin
   unfold has_vadd.vadd,

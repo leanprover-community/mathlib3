6ad1a55f4f6be5d96c342fd35ca0104540b80f29
Violeta Hernández <vi.hdz.p@gmail.com>
2022-06-06 22:44:27 +0000

    feat(set_theory/game/pgame): induction on left/right moves of add/mul (#14345)

--- src/set_theory/game/basic.lean
+++ src/set_theory/game/basic.lean
@@ -213,24 +213,24 @@ rfl
    = x.move_right i * y + x * y.move_left j - x.move_right i * y.move_left j :=
 by { cases x, cases y, refl }
 
-lemma left_moves_mul_cases {x y : pgame} (k : (x * y).left_moves) :
-  (∃ ix iy, k = to_left_moves_mul (sum.inl ⟨ix, iy⟩)) ∨
-  ∃ jx jy, k = to_left_moves_mul (sum.inr ⟨jx, jy⟩) :=
+lemma left_moves_mul_cases {x y : pgame} (k) {P : (x * y).left_moves → Prop}
+  (hl : ∀ ix iy, P $ to_left_moves_mul (sum.inl ⟨ix, iy⟩))
+  (hr : ∀ jx jy, P $ to_left_moves_mul (sum.inr ⟨jx, jy⟩)) : P k :=
 begin
   rw ←to_left_moves_mul.apply_symm_apply k,
   rcases to_left_moves_mul.symm k with ⟨ix, iy⟩ | ⟨jx, jy⟩,
-  { exact or.inl ⟨ix, iy, rfl⟩ },
-  { exact or.inr ⟨jx, jy, rfl⟩ }
+  { apply hl },
+  { apply hr }
 end
 
-lemma right_moves_mul_cases {x y : pgame} (k : (x * y).right_moves) :
-  (∃ ix jy, k = to_right_moves_mul (sum.inl ⟨ix, jy⟩)) ∨
-  ∃ jx iy, k = to_right_moves_mul (sum.inr ⟨jx, iy⟩) :=
+lemma right_moves_mul_cases {x y : pgame} (k) {P : (x * y).right_moves → Prop}
+  (hl : ∀ ix jy, P $ to_right_moves_mul (sum.inl ⟨ix, jy⟩))
+  (hr : ∀ jx iy, P $ to_right_moves_mul (sum.inr ⟨jx, iy⟩)) : P k :=
 begin
   rw ←to_right_moves_mul.apply_symm_apply k,
-  rcases to_right_moves_mul.symm k with ⟨ix, jy⟩ | ⟨jx, iy⟩,
-  { exact or.inl ⟨ix, jy, rfl⟩ },
-  { exact or.inr ⟨jx, iy, rfl⟩ }
+  rcases to_right_moves_mul.symm k with ⟨ix, iy⟩ | ⟨jx, jy⟩,
+  { apply hl },
+  { apply hr }
 end
 
 theorem quot_mul_comm : Π (x y : pgame.{u}), ⟦x * y⟧ = ⟦y * x⟧

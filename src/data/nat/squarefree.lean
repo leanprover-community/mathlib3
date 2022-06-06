1a1302dd465fb611cbc6f485082c67c0a5bc174c
Stuart Presnell <stuart.presnell@gmail.com>
2022-06-06 22:18:08 +0100

    Remove imports

--- src/data/nat/squarefree.lean
+++ src/data/nat/squarefree.lean
@@ -3,9 +3,7 @@ Copyright (c) 2020 Aaron Anderson. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Aaron Anderson
 -/
-import ring_theory.unique_factorization_domain
 import ring_theory.int.basic
-import number_theory.divisors
 import algebra.is_prime_pow
 import algebra.squarefree
 

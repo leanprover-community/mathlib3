d38a674650ab3a702d636f9469f1cf5364028052
Violeta Hernández <vi.hdz.p@gmail.com>
2022-06-07 18:28:18 -0500

    golf

--- src/logic/equiv/basic.lean
+++ src/logic/equiv/basic.lean
@@ -363,7 +363,7 @@ def prop_equiv_punit {p : Prop} (h : p) : p ≃ punit :=
 
 /-- The `Sort` of proofs of a false proposition is equivalent to `pempty`. -/
 def prop_equiv_pempty {p : Prop} (h : ¬p) : p ≃ pempty :=
-⟨λ x, absurd x h, λ x, by cases x, λ x, absurd x h, λ x, by cases x⟩
+@equiv_pempty p $ is_empty.prop_iff.2 h
 
 /-- `true` is equivalent to `punit`. -/
 def true_equiv_punit : true ≃ punit := prop_equiv_punit trivial

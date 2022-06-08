8cd340ac4eed65f3042477486fd69a0616f12c36
Violeta Hernández <vi.hdz.p@gmail.com>
2022-06-08 09:22:46 -0500

    apply suggestions

--- src/set_theory/ordinal/basic.lean
+++ src/set_theory/ordinal/basic.lean
@@ -504,8 +504,8 @@ theorem type_eq {α β} {r : α → α → Prop} {s : β → β → Prop}
   [is_well_order α r] [is_well_order β s] :
   type r = type s ↔ nonempty (r ≃r s) := quotient.eq
 
-theorem _root_.rel_iso.type_eq {α β} {r : α → α → Prop} {s : β → β → Prop}
- [is_well_order α r] [is_well_order β s] (h : r ≃r s) :
+theorem _root_.rel_iso.ordinal_type_eq {α β} {r : α → α → Prop} {s : β → β → Prop}
+  [is_well_order α r] [is_well_order β s] (h : r ≃r s) :
   type r = type s := type_eq.2 ⟨h⟩
 
 @[simp] theorem type_lt (o : ordinal) : type ((<) : o.out.α → o.out.α → Prop) = o :=

4de18627eba4ebd0d4a1e9ba9fadf0c09e00fcf6
jjaassoonn <jujian.zhang1998@outlook.com>
2022-06-09 14:32:57 +0100

    changed to new spelling

--- src/algebra/module/graded_module.lean
+++ src/algebra/module/graded_module.lean
@@ -5,6 +5,7 @@ Authors: Jujian Zhang
 -/
 
 import ring_theory.graded_algebra.basic
+import algebra.direct_sum.decomposition
 
 /-!
 # Graded Module
@@ -39,57 +40,40 @@ Given a graded `R`-algebra `A` graded by `𝓐 : ι → submodule R A` and a dec
 `M` into `𝓜 : ι → add_submonoid M`, we say that `M` is graded by `𝓜` if and only if the
 decomposition map `M → ⨁ i, 𝓜 i` is inverse to the canonical map `⨁ i, 𝓜 i → M`.
 -/
-class graded_module :=
-(decompose' : M → ⨁ i, 𝓜 i)
-(left_inv : function.left_inverse decompose' (direct_sum.coe_add_monoid_hom 𝓜))
-(right_inv : function.right_inverse decompose' (direct_sum.coe_add_monoid_hom 𝓜))
+class graded_module extends direct_sum.decomposition 𝓜 :=
 (smul_mem : ∀ ⦃i j : ι⦄ {a : A} {m : M} (hi : a ∈ 𝓐 i) (hj : m ∈ 𝓜 j), a • m ∈ 𝓜 (i + j))
 
 namespace graded_module
 
-variables [graded_module 𝓐 𝓜]
 
-protected lemma is_internal : direct_sum.is_internal 𝓜 :=
-{ left := (@graded_module.left_inv ι R A _ _ _ _ _ 𝓐 _ M _ _ 𝓜 _).injective,
-  right := (@graded_module.right_inv ι R A _ _ _ _ _ 𝓐 _ M _ _ 𝓜 _).surjective }
+protected lemma is_internal [graded_module 𝓐 𝓜] : direct_sum.is_internal 𝓜 :=
+direct_sum.decomposition.is_internal _
 
 /--
 If `M` is graded by `𝓜`, then `M` is isomorphic to `⨁ i, 𝓜 i` as `add_comm_monoid`.
 -/
-def decompose : M ≃+ ⨁ i, 𝓜 i := add_equiv.symm
+def decompose [graded_module 𝓐 𝓜] : M ≃+ ⨁ i, 𝓜 i := add_equiv.symm
 { to_fun := direct_sum.coe_add_monoid_hom 𝓜,
-  inv_fun := graded_module.decompose' 𝓐,
-  left_inv := graded_module.left_inv,
-  right_inv := graded_module.right_inv,
+  inv_fun := direct_sum.decompose 𝓜,
+  left_inv := direct_sum.decomposition.right_inv,
+  right_inv := direct_sum.decomposition.left_inv,
   map_add' := λ x y, by rw map_add }
 
-@[simp] lemma decompose_symm_of {i : ι} (x : 𝓜 i) :
+@[simp] lemma decompose_symm_of [graded_module 𝓐 𝓜] {i : ι} (x : 𝓜 i) :
   (graded_module.decompose 𝓐 𝓜).symm (direct_sum.of _ i x) = x :=
 direct_sum.coe_add_monoid_hom_of _ _ _
 
 instance self : graded_module 𝓐 (λ i, (𝓐 i).to_add_submonoid) :=
-{ decompose' := graded_algebra.decompose 𝓐,
-  left_inv := graded_algebra.left_inv,
-  right_inv := graded_algebra.right_inv,
+{ decompose' := direct_sum.decompose 𝓐,
+  left_inv := direct_sum.decomposition.left_inv,
+  right_inv := direct_sum.decomposition.right_inv,
   smul_mem := λ i j x y hi hj, set_like.graded_monoid.mul_mem hi hj }
-
-@[simp] lemma decompose_coe {i : ι} (x : 𝓜 i) :
-  graded_module.decompose 𝓐 𝓜 x = direct_sum.of _ i x :=
-by rw [← decompose_symm_of 𝓐 𝓜, add_equiv.apply_symm_apply]
-
-lemma decompose_of_mem {x : M} {i : ι} (hx : x ∈ 𝓜 i) :
-  graded_module.decompose 𝓐 𝓜 x = direct_sum.of _ i (⟨x, hx⟩ : 𝓜 i) :=
-graded_module.decompose_coe _ _ ⟨x, hx⟩
-
-lemma decompose_of_mem_ne {x : M} {i j : ι} (hx : x ∈ 𝓜 i) (hij : i ≠ j):
-  (graded_module.decompose 𝓐 𝓜 x j : M) = 0 :=
-by rw [decompose_of_mem _ _ hx, direct_sum.of_eq_of_ne _ _ _ _ hij, add_submonoid.coe_zero]
 
 /--
 `⨁ i, 𝓜 i` is also an `A`-module, via `a • z = decompose (a • redecompose z)` where `decompose` and
 `recompose` are the cannonical homomorphism `M → ⨁ i, 𝓜 i` and `⨁ i, 𝓜 i → M`.
 -/
-def is_module : module A (⨁ i, 𝓜 i) :=
+def is_module [graded_module 𝓐 𝓜] : module A (⨁ i, 𝓜 i) :=
 { smul := λ a z, graded_module.decompose 𝓐 𝓜 (a • (graded_module.decompose 𝓐 𝓜).symm z),
   one_smul := λ b, begin
     change graded_module.decompose 𝓐 𝓜 _ = _,

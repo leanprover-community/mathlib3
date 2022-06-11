6dc5ff1121752c76422df7457635242f265a3715
Anatole Dedecker <anatolededecker@gmail.com>
2022-06-12 00:39:36 +0200

    Finished

--- src/topology/algebra/uniform_convergence.lean
+++ src/topology/algebra/uniform_convergence.lean
@@ -9,46 +9,57 @@ import topology.algebra.uniform_group
 /-!
 # Algebraic facts about the topology of uniform convergence
 
-## Main definitions
-
-* `foo_bar`
+This file contains algrebraic compatibility results about the uniform structure of uniform
+convergence / `𝔖`-convergence. They will mostly be useful for defining strong topologies on the
+space of continuous linear maps between two topological vector spaces.
 
 ## Main statements
 
-* `foo_bar_unique`
+* `uniform_convergence.uniform_group` : if `G` is a uniform group, then the uniform structure of
+  uniform convergence makes `α → G` an uniform group
+* `uniform_convergence_on.uniform_group` : if `G` is a uniform group, then the uniform structure of
+  `𝔖`-convergence, for any `𝔖 : set (set α)`, makes `α → G` an uniform group
 
-## Notation
+## TODO
 
-
-
-## Implementation details
-
-
+* Let `E` be a TVS, `𝔖 : set (set α)` and `H` a submodule of `α → E`. If the image of any `S ∈ 𝔖`
+  by any `u ∈ H` is bounded (in the sense of `bornology.is_vonN_bounded`), then `H`, equipped with
+  the topology of `𝔖`-convergence, is a TVS.
 
 ## References
 
-* [F. Bar, *Quuxes*][bibkey]
+* [N. Bourbaki, *General Topology*][bourbaki1966]
 
 ## Tags
 
-Foobars, barfoos
+uniform convergence, strong dual
+
 -/
 
 section group
 
-variables {α G : Type*} [group G] [uniform_space G] [uniform_group G]
+variables {α G : Type*} [group G] [uniform_space G] [uniform_group G] {𝔖 : set $ set α}
 
 local attribute [-instance] Pi.uniform_space
 local attribute [-instance] Pi.topological_space
 local attribute [instance] uniform_convergence.uniform_space
 
-#check uniform_inducing.uniform_continuous
-
 @[to_additive]
 protected lemma uniform_convergence.uniform_group :
-uniform_group (α → G) :=
-⟨begin
-  suffices : uniform_continuous (uniform_convergence.uniform_equiv_Pi_comm (/))
-end⟩
+  uniform_group (α → G) :=
+⟨(uniform_convergence.postcomp_uniform_continuous uniform_continuous_div).comp
+  uniform_convergence.uniform_equiv_prod_arrow.symm.uniform_continuous⟩
+
+local attribute [-instance] uniform_convergence.uniform_space
+
+@[to_additive]
+protected lemma uniform_convergence_on.uniform_group :
+  @uniform_group (α → G) (uniform_convergence_on.uniform_space α G 𝔖) _ :=
+begin
+  letI : uniform_space (α → G) := uniform_convergence_on.uniform_space α G 𝔖,
+  letI : uniform_space (α → G × G) := uniform_convergence_on.uniform_space α (G × G) 𝔖,
+  exact ⟨(uniform_convergence_on.postcomp_uniform_continuous uniform_continuous_div).comp
+          uniform_convergence_on.uniform_equiv_prod_arrow.symm.uniform_continuous⟩
+end
 
 end group

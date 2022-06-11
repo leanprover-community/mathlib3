e1782263d40cb7114c7ef7d8eb5f25a72b0eb871
YaelDillies <yael.dillies@gmail.com>
2022-06-11 20:48:29 +0100

    remove auxiliary semiring

--- src/combinatorics/additive/ruzsa_covering.lean
+++ src/combinatorics/additive/ruzsa_covering.lean
@@ -16,12 +16,10 @@ with at most `(s + t).card/t.card` copies of `t - t`.
 Merge this file with other prerequisites to Freiman's theorem once we have them.
 -/
 
-open nat
-open_locale pointwise
-
+namespace finset
 variables {α β : Type*}
 
-namespace finset
+open_locale pointwise
 
 section
 variables [decidable_eq α] [left_cancel_semigroup α] {s t : finset α}
@@ -53,52 +51,24 @@ end mul_action
 
 end finset
 
+open_locale pointwise
+
+variables {α : Type*}
+
 namespace finset
-variables [decidable_eq α] [linear_ordered_semiring β]
+variables [decidable_eq α] [comm_group α] (s : finset α) {t : finset α}
 
 /-- **Ruzsa's covering lemma**. -/
-lemma exists_subset_add_sub [add_comm_group α] {s t : finset α} {r : β} (ht : t.nonempty)
-  (h : ↑((s + t).card) ≤ t.card • r) :
-  ∃ u : finset α, ↑(u.card) ≤ r ∧ s ⊆ u + t - t :=
-begin
-  haveI : Π u, decidable ((u : set α).pairwise_disjoint (+ᵥ t)) := λ u, classical.dec _,
-  set C := s.powerset.filter (λ u, (u : set α).pairwise_disjoint (+ᵥ t)),
-  obtain ⟨u, hu, hCmax⟩ := C.exists_maximal
-    (filter_nonempty_iff.2 ⟨∅, empty_mem_powerset _, set.pairwise_disjoint_empty⟩),
-  rw [mem_filter, mem_powerset] at hu,
-  refine ⟨u, _, λ a ha, _⟩,
-  { -- TODO: `smul_le_smul_iff_of_pos` ought to be useful here
-    refine le_of_mul_le_mul_left _ (cast_pos.2 ht.card_pos),
-    rw [←cast_mul, mul_comm, ←card_add hu.2, ←_root_.nsmul_eq_mul],
-    exact (cast_le.2 $ card_le_of_subset $ add_subset_add_right hu.1).trans h },
-  rw add_sub_assoc,
-  by_cases hau : a ∈ u,
-  { exact subset_add_left _ ht.zero_mem_sub hau },
-  by_cases H : ∀ b ∈ u, disjoint (a +ᵥ t) (b +ᵥ t),
-  { refine (hCmax _ _ $ ssubset_insert hau).elim,
-    rw [mem_filter, mem_powerset, insert_subset, coe_insert],
-    exact ⟨⟨ha, hu.1⟩, hu.2.insert $ λ b hb _, H _ hb⟩ },
-  push_neg at H,
-  simp_rw [not_disjoint_iff, ←neg_vadd_mem_iff] at H,
-  obtain ⟨b, hb, c, hc₁, hc₂⟩ := H,
-  exact mem_add.2 ⟨_, _, hb, mem_sub.2 ⟨_, _, hc₂, hc₁, by simp [sub_eq_add_neg a b]⟩, by simp⟩,
-end
-
-/-- **Ruzsa's covering lemma**. -/
-@[to_additive] lemma exists_subset_mul_div [comm_group α] {s t : finset α} {r : β} (ht : t.nonempty)
-  (h : ↑((s * t).card) ≤ t.card • r) :
-  ∃ u : finset α, ↑(u.card) ≤ r ∧ s ⊆ u * t / t :=
+@[to_additive "**Ruzsa's covering lemma**"]
+lemma exists_subset_mul_div (ht : t.nonempty) :
+  ∃ u : finset α, u.card * t.card ≤ (s * t).card ∧ s ⊆ u * t / t :=
 begin
   haveI : Π u, decidable ((u : set α).pairwise_disjoint (• t)) := λ u, classical.dec _,
   set C := s.powerset.filter (λ u, (u : set α).pairwise_disjoint (• t)),
   obtain ⟨u, hu, hCmax⟩ := C.exists_maximal
     (filter_nonempty_iff.2 ⟨∅, empty_mem_powerset _, set.pairwise_disjoint_empty⟩),
   rw [mem_filter, mem_powerset] at hu,
-  refine ⟨u, _, λ a ha, _⟩,
-  { -- TODO: `smul_le_smul_iff_of_pos` ought to be useful here
-    refine le_of_mul_le_mul_left _ (cast_pos.2 ht.card_pos),
-    rw [←cast_mul, mul_comm, ←card_mul hu.2, ←_root_.nsmul_eq_mul],
-    exact (cast_le.2 $ card_le_of_subset $ mul_subset_mul_right hu.1).trans h },
+  refine ⟨u, (card_mul hu.2).ge.trans (card_le_of_subset $ mul_subset_mul_right hu.1), λ a ha, _⟩,
   rw mul_div_assoc,
   by_cases hau : a ∈ u,
   { exact subset_mul_left _ ht.one_mem_div hau },

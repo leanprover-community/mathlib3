dbc65c9cd910f8a0bf1977ad1a1ea35993a7c0e7
Bart Michels <97157072+bartmichels@users.noreply.github.com>
2022-06-13 09:13:12 +0200

    Update src/linear_algebra/vandermonde.lean
    Co-authored-by: Eric Wieser <wieser.eric@gmail.com>

--- src/linear_algebra/vandermonde.lean
+++ src/linear_algebra/vandermonde.lean
@@ -129,8 +129,8 @@ begin
   { simp only [det_vandermonde v, finset.prod_eq_zero_iff, sub_eq_zero, forall_exists_index],
     exact λ i _ j h₁ h₂, ⟨j, i, h₂, (finset.mem_filter.mp h₁).2.ne'⟩ },
   { simp only [ne.def, forall_exists_index, and_imp],
-    exact λ i j h₁ h₂, matrix.det_zero_of_row_eq h₂
-      (funext (λ k, (by simp only [vandermonde_apply, h₁]))) }
+    refine λ i j h₁ h₂, matrix.det_zero_of_row_eq h₂ (funext $ λ k, _),
+    rw [vandermonde_apply, vandermonde_apply, h₁], }
 end
 
 lemma det_vandermonde_ne_zero_iff [is_domain R] {n : ℕ} (v : fin n → R) :

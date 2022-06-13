df2506713c24888ad71e9bc605a1189651c3bdfc
Jujian Zhang <jujian.zhang1998@outlook.com>
2022-06-13 21:05:52 +0100

    finished epi

--- src/algebra/category/Group/epi_mono.lean
+++ src/algebra/category/Group/epi_mono.lean
@@ -1,11 +1,41 @@
 import category_theory.epi_mono
+import group_theory.quotient_group
 import algebra.category.Group.basic
-
-namespace monoid_hom
 
 universes u
 
-variables {A B : Type u} [group A] [group B]
+namespace monoid_hom
+
+open quotient_group
+
+variables {A B : Type u}
+
+section
+
+variables [group A] [group B]
+
+@[to_additive add_monoid_hom.ker_eq_bot]
+lemma ker_eq_bot (f : A →* B) :
+  f.ker = ⊥ ↔ function.injective f :=
+{ mp := λ h1 x y h2, begin
+    rw [←div_eq_one, ←map_div, ←mem_ker] at h2,
+    rwa [h1, subgroup.mem_bot, div_eq_one] at h2,
+  end,
+  mpr := λ h, set_like.ext $ λ x, iff.trans (mem_ker _) $
+    iff.trans begin
+      rw ←(by rw map_one : f x = f 1 ↔ f x = 1),
+      exact ⟨λ h', h h', λ h, h ▸ rfl⟩,
+    end subgroup.mem_bot.symm }
+
+@[to_additive add_monoid_hom.range_eq_top]
+lemma range_eq_top (f : A →* B) :
+  f.range = ⊤ ↔ function.surjective f :=
+{ mp := λ h x, begin
+    rcases show x ∈ f.range, from h.symm ▸ subgroup.mem_top _ with ⟨a, h⟩,
+    exact ⟨a, h⟩,
+  end,
+  mpr := λ h, set_like.ext $ λ x, iff.trans mem_range
+    ⟨λ _, trivial, λ _, h x⟩ }
 
 @[to_additive add_monoid_hom.range_zero_eq_bot]
 lemma range_one_eq_bot :
@@ -13,6 +43,10 @@ lemma range_one_eq_bot :
 set_like.ext $ λ a, iff.trans monoid_hom.mem_range $
   iff.trans (by simp only [one_apply, exists_const]; split; intros h; subst h)
     subgroup.mem_bot.symm
+@[to_additive add_monoid_hom.ker_zero_eq_top]
+lemma ker_one_eq_top :
+  (1 : A →* B).ker = ⊤ :=
+set_like.ext $ λ a, iff.trans (monoid_hom.mem_ker _) $ by simp
 
 @[to_additive add_monoid_hom.ker_eq_bot_of_cancel]
 lemma ker_eq_bot_of_cancel {f : A →* B}
@@ -27,21 +61,75 @@ begin
   rw [←subgroup.subtype_range f.ker, ←h, range_one_eq_bot],
 end
 
+end
+
+section
+
+variables [comm_group A] [comm_group B]
+
+@[to_additive add_monoid_hom.range_eq_top_of_cancel]
+lemma range_eq_top_of_cancel {f : A →* B}
+  (h : ∀ (u v : B →* B ⧸ f.range), u.comp f = v.comp f → u = v) :
+  f.range = ⊤ :=
+begin
+  specialize h 1 (quotient_group.mk' _) begin
+    ext1,
+    simp only [one_apply, coe_comp, coe_mk', function.comp_app],
+    rw [show (1 : B ⧸ f.range) = (1 : B), from quotient_group.coe_one _, quotient_group.eq, inv_one,
+      one_mul],
+    exact ⟨x, rfl⟩,
+  end,
+  replace h : (quotient_group.mk' _).ker = (1 : B →* B ⧸ f.range).ker := by rw h,
+  rwa [ker_one_eq_top, quotient_group.ker_mk] at h,
+end
+
+end
 
 end monoid_hom
 
-namespace Group
+section
 
 open category_theory
 
-universe u
+namespace Group
 
 variables {A B : Group.{u}} (f : A ⟶ B)
 
 lemma ker_eq_bot_of_mono [mono f] : f.ker = ⊥ :=
-monoid_hom.ker_eq_bot_of_cancel $ λ u v h,
-monoid_hom.ext $ λ x, begin
+monoid_hom.ker_eq_bot_of_cancel $ λ u v,
+  (@cancel_mono _ _ _ _ _ f _
+    (show Group.of f.ker ⟶ A, from u) _).1
 
-end
+lemma mono_iff_ker_eq_bot :
+  mono f ↔ f.ker = ⊥ :=
+⟨λ h, @@ker_eq_bot_of_mono f h,
+λ h, concrete_category.mono_of_injective _ $ (monoid_hom.ker_eq_bot f).1 h⟩
+
+lemma mono_iff_injective :
+  mono f ↔ function.injective f :=
+iff.trans (mono_iff_ker_eq_bot f) $ monoid_hom.ker_eq_bot f
 
 end Group
+
+namespace CommGroup
+
+variables {A B : CommGroup.{u}} (f : A ⟶ B)
+
+@[to_additive]
+lemma range_eq_top_of_epi [epi f] : f.range = ⊤ :=
+monoid_hom.range_eq_top_of_cancel $ λ u v,
+  (@cancel_epi _ _ _ _ _ f _
+    (show CommGroup.of B ⟶ CommGroup.of (B ⧸ f.range), from u) _).1
+
+lemma epi_iff_range_eq_top :
+  epi f ↔ f.range = ⊤ :=
+⟨λ h, @@range_eq_top_of_epi f h,
+λ h, concrete_category.epi_of_surjective _ $ (monoid_hom.range_eq_top f).1 h⟩
+
+lemma epi_iff_surjective :
+  epi f ↔ function.surjective f :=
+iff.trans (epi_iff_range_eq_top f) $ monoid_hom.range_eq_top f
+
+end CommGroup
+
+end

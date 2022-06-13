071e47cd34c7d84ed35f0cb7eb3e0ae8c3a771ec
Bolton Bailey <bolton.bailey@gmail.com>
2022-06-13 11:51:00 -0400

    Update parity.lean

--- src/data/nat/parity.lean
+++ src/data/nat/parity.lean
@@ -183,9 +183,6 @@ begin
     apply even_mul_succ_self }
 end
 
-lemma even_prime_eq_two {p : ℕ} (he : even p) (hp : prime p) : p = 2 :=
-hp.even_iff.mp he
-
 lemma even_sub_one_of_prime_ne_two {p : ℕ} (hp : prime p) (hodd : p ≠ 2) : even (p - 1) :=
 odd.sub_odd (odd_iff.2 $ hp.eq_two_or_odd.resolve_left hodd) (odd_iff.2 rfl)
 

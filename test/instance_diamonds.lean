1a5bb84fdbab9c58e7b93d14d2157fbcde7073bf
Alex J. Best <alex.j.best@gmail.com>
2022-06-13 15:07:09 +0200

    add test case

--- test/instance_diamonds.lean
+++ test/instance_diamonds.lean
@@ -10,6 +10,7 @@ import group_theory.group_action.prod
 import group_theory.group_action.units
 import data.complex.module
 import ring_theory.algebraic
+import data.zmod.basic
 
 /-! # Tests that instances do not form diamonds -/
 
@@ -170,3 +171,13 @@ example [comm_semiring R] [nontrivial R] :
 rfl
 
 end polynomial
+
+/-! ## `zmod` instances -/
+section zmod
+
+variables {p : ℕ} [fact p.prime]
+example : @euclidean_domain.to_comm_ring _ (@field.to_euclidean_domain _ (zmod.field p)) =
+  zmod.comm_ring p :=
+rfl
+
+end zmod

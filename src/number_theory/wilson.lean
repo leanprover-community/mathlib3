ce6f252a22f6c8c421e442e57434259f0a111514
jn1z <jn1z@users.noreply.github.com>
2022-06-14 13:06:35 -0400

    Try fix for style build issues. Also simplify hypothesis names.

--- src/number_theory/wilson.lean
+++ src/number_theory/wilson.lean
@@ -26,25 +26,25 @@ open_locale nat
 namespace nat
 
 /-- For `n > 1`, `(n-1)!` is congruent to `-1` modulo `n` only if n is prime. --/
-private lemma wilsons_theorem_only_if_direction  (n : ℕ) [hyp: n > 1] :
+private lemma wilsons_theorem_only_if_direction  (n : ℕ) (h1: 1 < n) :
 (((n - 1)! : zmod n) = -1)  → (prime n):=
 begin
   intro h,
-  have hp2: ((n - 1)! + 1 : zmod n) = 0,
+  have hp: ((n - 1)! + 1 : zmod n) = 0,
   rw h,
   simp,
   have hn_divides: n ∣(n-1)! + 1,
   rw ← zmod.nat_coe_zmod_eq_zero_iff_dvd,
-  exact hp2,
+  exact hp,
 
   by_contradiction h2,
-  obtain ⟨m⟩  := exists_dvd_of_not_prime2 hyp h2,
+  obtain ⟨m⟩  := exists_dvd_of_not_prime2 h1 h2,
   have m_leq_n_minus_one : m ≤ (n-1),
   cases h_1.right,
   rw lt_iff_add_one_le at right,
   rw nat.add_le_to_le_sub at right,
   exact right,
-  exact le_of_lt hyp,
+  exact le_of_lt h1,
 
   have hm_divides_fact : (m ∣(n-1)!),
   refine nat.dvd_factorial _ m_leq_n_minus_one,
@@ -53,7 +53,7 @@ begin
   cases h_1.left,
   rw h_2 at hn_divides hm_divides_fact,
 
-  clear h h_2 h2 hp2 hyp m_leq_n_minus_one,
+  clear h h1 h2 h_2 hp m_leq_n_minus_one,
 
   have m_is_one: (m = 1), from
     nat.dvd_one.mp
@@ -64,12 +64,12 @@ begin
 end
 
 /-- **Wilson's Theorem**: For `n > 1`, `(n-1)!` is congruent to `-1` modulo `n` iff n is prime. --/
-theorem wilsons_theorem (n : ℕ) [hyp: n > 1] :
+theorem wilsons_theorem (n : ℕ) (h1: 1 < n) :
   (prime n) ↔ (((n - 1)! : zmod n) = -1) :=
 begin
   split,
-  { intro hp, rw ← zmod.wilsons_lemma _, exact fact_iff.mpr hp },
-  { apply wilsons_theorem_only_if_direction _, exact hyp }
+  { intro h2, rw ← zmod.wilsons_lemma _, exact fact_iff.mpr h2 },
+  { apply wilsons_theorem_only_if_direction _, exact h1 }
 end
 
 end nat

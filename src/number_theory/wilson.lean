521efa5cabb44c1a780ba99d4757312cb1a05672
jn1z <jn1z@users.noreply.github.com>
2022-06-14 16:55:54 -0400

    Style: use brackets to separate introduced goals.

--- src/number_theory/wilson.lean
+++ src/number_theory/wilson.lean
@@ -26,30 +26,28 @@ open_locale nat
 namespace nat
 
 /-- For `n > 1`, `(n-1)!` is congruent to `-1` modulo `n` only if n is prime. --/
-private lemma wilsons_theorem_only_if_direction (n : ℕ) (h1 : 1 < n) :
+private lemma wilsons_theorem_only_if_direction (n : ℕ) (h1 : 1 < n):
 (((n - 1)! : zmod n) = -1) → (prime n) :=
 begin
   intro h,
   have hp : ((n - 1)! + 1 : zmod n) = 0,
-  rw h,
-  simp,
+  { rw h, simp, },
   have hn_divides : n ∣(n-1)! + 1,
-  rw ← zmod.nat_coe_zmod_eq_zero_iff_dvd,
-  exact hp,
+  { rw ← zmod.nat_coe_zmod_eq_zero_iff_dvd, exact hp, },
 
   by_contradiction h2,
   obtain ⟨m⟩ := exists_dvd_of_not_prime2 h1 h2,
   have m_leq_n_minus_one : m ≤ (n-1),
-  cases h_1.right,
-  rw lt_iff_add_one_le at right,
-  rw nat.add_le_to_le_sub at right,
-  exact right,
-  exact le_of_lt h1,
+  { cases h_1.right,
+    rw lt_iff_add_one_le at right,
+    rw nat.add_le_to_le_sub at right,
+    exact right,
+    exact le_of_lt h1, },
 
   have hm_divides_fact : (m ∣(n-1)!),
-  refine nat.dvd_factorial _ m_leq_n_minus_one,
-  cases h_1.right,
-  exact pos_of_gt left,
+  { refine nat.dvd_factorial _ m_leq_n_minus_one,
+    cases h_1.right,
+    exact pos_of_gt left, },
   cases h_1.left,
   rw h_2 at hn_divides hm_divides_fact,
 

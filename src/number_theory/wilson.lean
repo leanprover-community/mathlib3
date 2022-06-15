6ec0a586576dc63b5ee2d866c62bee0ad34a5b8a
jn1z <jn1z@users.noreply.github.com>
2022-06-15 15:06:45 -0400

    Simplify more steps and use brackets for a type.

--- src/number_theory/wilson.lean
+++ src/number_theory/wilson.lean
@@ -26,28 +26,26 @@ open_locale nat
 namespace nat
 
 /-- For `n > 1`, `(n-1)!` is congruent to `-1` modulo `n` only if n is prime. --/
-private lemma wilsons_theorem_only_if_direction
-  (n : ℕ) (h : ((n - 1)! : zmod n) = -1) (h1 : 1 < n) : prime n :=
+lemma wilsons_theorem_only_if_direction
+  {n : ℕ} (h : ((n - 1)! : zmod n) = -1) (h1 : 1 < n) : prime n :=
 begin
   have hn_divides : n ∣ (n-1)! + 1,
   { rw [←zmod.nat_coe_zmod_eq_zero_iff_dvd, cast_add, cast_one, h, add_left_neg] },
 
   by_contradiction h2,
   obtain ⟨m, hm1, hm2, hm3⟩ := exists_dvd_of_not_prime2 h1 h2,
-  have m_leq_n_minus_one : m ≤ n-1,
-  { rwa [lt_iff_add_one_le, nat.add_le_to_le_sub m h1.le] at hm3 },
-  have hm_divides_fact : m ∣ (n-1)! :=
-  nat.dvd_factorial (pos_of_gt hm2) m_leq_n_minus_one ,
+  rw [lt_iff_add_one_le, nat.add_le_to_le_sub m h1.le] at hm3,
+  have hm_divides_fact : m ∣ (n-1)! := nat.dvd_factorial (pos_of_gt hm2) hm3,
   have m_is_one : m = 1 :=
   nat.dvd_one.mp ((nat.dvd_add_right hm_divides_fact).mp (hm1.trans hn_divides)),
   linarith,
 end
 
 /-- **Wilson's Theorem**: For `n > 1`, `(n-1)!` is congruent to `-1` modulo `n` iff n is prime. --/
-theorem wilsons_theorem (n : ℕ) (h : 1 < n) :
+theorem wilsons_theorem {n : ℕ} (h : 1 < n) :
   prime n ↔ ((n - 1)! : zmod n) = -1 :=
 begin
-  refine ⟨λ h1, _, λ h2, wilsons_theorem_only_if_direction n h2 h⟩,
+  refine ⟨λ h1, _, λ h2, wilsons_theorem_only_if_direction h2 h⟩,
   haveI := fact.mk h1,
   exact zmod.wilsons_lemma n,
 end

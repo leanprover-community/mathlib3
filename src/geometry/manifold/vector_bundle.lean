6d9a31b2106939ea46b7c815420e39898603cde4
Floris van Doorn <fpvdoorn@gmail.com>
2022-06-15 17:26:38 +0200

    fixes

--- src/geometry/manifold/vector_bundle.lean
+++ src/geometry/manifold/vector_bundle.lean
@@ -3,7 +3,7 @@ Copyright (c) 2022 Floris van Doorn. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Floris van Doorn
 -/
-import topology.vector_bundle
+import topology.vector_bundle.basic
 import geometry.manifold.fiber_bundle
 
 /-!

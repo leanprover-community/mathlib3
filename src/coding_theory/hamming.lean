eb4971b5b791e220109e36f41e7edab0d5455efc
Wrenna Robson <wren.robson@gmail.com>
2022-06-15 14:54:14 +0100

    Address EW comments

--- src/coding_theory/hamming.lean
+++ src/coding_theory/hamming.lean
@@ -45,11 +45,11 @@ variables {α ι : Type*} {β : ι → Type*}
 /-- `of_hamm` is the identity function from the `hamm` of a type.  -/
 @[pattern] def of_hamm : hamm β ≃ Π i, β i := equiv.refl _
 
-@[simp] lemma to_hamm_symm_eq                : (@to_hamm _ β).symm = of_hamm := rfl
-@[simp] lemma of_hamm_symm_eq                : (@of_hamm _ β).symm = to_hamm := rfl
-@[simp] lemma to_hamm_of_hamm (x : hamm β)     : to_hamm (of_hamm x) = x := rfl
+@[simp] lemma to_hamm_symm_eq                 : (@to_hamm _ β).symm = of_hamm := rfl
+@[simp] lemma of_hamm_symm_eq                 : (@of_hamm _ β).symm = to_hamm := rfl
+@[simp] lemma to_hamm_of_hamm (x : hamm β)    : to_hamm (of_hamm x) = x := rfl
 @[simp] lemma of_hamm_to_hamm (x : Π i, β i)  : of_hamm (to_hamm x) = x := rfl
-@[simp] lemma to_hamm_inj {x y : Π i, β i}   : to_hamm x = to_hamm y ↔ x = y := iff.rfl
+@[simp] lemma to_hamm_inj {x y : Π i, β i}    : to_hamm x = to_hamm y ↔ x = y := iff.rfl
 @[simp] lemma of_hamm_inj {x y : hamm β}      : of_hamm x = of_hamm y ↔ x = y := iff.rfl
 
 instance [Π i, has_zero (β i)] : has_zero (hamm β) := pi.has_zero
@@ -80,12 +80,12 @@ variables {α ι : Type*} {β : ι → Type*} [fintype ι] [Π i, decidable_eq (
 def hamm_dist (x y : Π i, β i) := card {i // x i ≠ y i}
 
 lemma hamm_dist_smul_le [Π i, has_scalar α (β i)] (k : α) (x y : Π i, β i) :
-hamm_dist (k • x) (k • y) ≤ hamm_dist x y :=
+  hamm_dist (k • x) (k • y) ≤ hamm_dist x y :=
 card_subtype_mono _ _ (λ i h H, h (by rw [pi.smul_apply, pi.smul_apply, H]))
 
 lemma hamm_dist_smul [Π i, has_scalar α (β i)] {k : α}
-(hk : ∀ i, is_smul_regular (β i) k) (x y : Π i, β i) :
-hamm_dist x y = hamm_dist (k • x) (k • y) :=
+  (hk : ∀ i, is_smul_regular (β i) k) (x y : Π i, β i) :
+  hamm_dist x y = hamm_dist (k • x) (k • y) :=
 le_antisymm (card_subtype_mono _ _ (λ _ h H, h (hk _ H))) (hamm_dist_smul_le _ _ _)
 
 lemma hamm_dist_eq (x y : Π i, β i) : hamm_dist x y = card {i // x i ≠ y i} := rfl
@@ -93,7 +93,8 @@ lemma hamm_dist_eq (x y : Π i, β i) : hamm_dist x y = card {i // x i ≠ y i} 
 lemma hamm_dist_comm (x y : Π i, β i) : hamm_dist x y = hamm_dist y x :=
 by simp_rw [hamm_dist_eq, ne_comm]
 
-lemma hamm_dist_triangle (x y z : Π i, β i) : hamm_dist x z ≤ hamm_dist x y + hamm_dist y z :=
+lemma hamm_dist_triangle (x y z : Π i, β i) :
+  hamm_dist x z ≤ hamm_dist x y + hamm_dist y z :=
 begin
   simp_rw hamm_dist_eq,
   refine le_trans (card_subtype_mono _ _ (λ _ h, _)) (card_subtype_or _ _),
@@ -109,8 +110,8 @@ end
 
 @[simp] lemma hamm_dist_self (x : Π i, β i) : hamm_dist x x = 0 := hamm_dist_eq_zero.mpr rfl
 
-lemma eq_of_hamm_dist_eq_zero (x y : Π i, β i) :
-hamm_dist x y = 0 → x = y := hamm_dist_eq_zero.mp
+lemma eq_of_hamm_dist_eq_zero (x y : Π i, β i) : hamm_dist x y = 0 → x = y :=
+hamm_dist_eq_zero.mp
 
 lemma hamm_dist_ne_zero {x y : Π i, β i} : hamm_dist x y ≠ 0 ↔ x ≠ y :=
 not_iff_not.mpr hamm_dist_eq_zero
@@ -119,10 +120,12 @@ lemma hamm_dist_pos {x y : Π i, β i} : 0 < hamm_dist x y ↔ x ≠ y :=
 by rw [←hamm_dist_ne_zero, iff_not_comm, not_lt, nat.le_zero_iff]
 
 @[simp] lemma hamm_dist_eq_zero_iff_forall_eq {x y : Π i, β i} :
-hamm_dist x y = 0 ↔ ∀ i, x i = y i := by rw [hamm_dist_eq_zero, function.funext_iff]
+  hamm_dist x y = 0 ↔ ∀ i, x i = y i :=
+by rw [hamm_dist_eq_zero, function.funext_iff]
 
 lemma hamm_dist_ne_zero_iff_exists_ne {x y : Π i, β i} :
-hamm_dist x y ≠ 0 ↔ ∃ i, x i ≠ y i := by rw [hamm_dist_ne_zero, function.ne_iff]
+  hamm_dist x y ≠ 0 ↔ ∃ i, x i ≠ y i :=
+by rw [hamm_dist_ne_zero, function.ne_iff]
 
 section has_zero
 
@@ -137,19 +140,16 @@ lemma hamm_wt_eq (x : Π i, β i) : hamm_wt x = card {i // x i ≠ 0} := rfl
 lemma hamm_wt_eq_hamm_dist_zero (x : Π i, β i) : hamm_wt x = hamm_dist x 0 := rfl
 
 lemma hamm_wt_smul_le [has_zero α] [Π i, smul_with_zero α (β i)] (k : α)
-(x : Π i, β i) : hamm_wt (k • x) ≤ hamm_wt x :=
+  (x : Π i, β i) : hamm_wt (k • x) ≤ hamm_wt x :=
 by convert hamm_dist_smul_le k x _; rw smul_zero'; refl
 
 lemma hamm_wt_smul [has_zero α] [Π i, smul_with_zero α (β i)] {k : α}
-(hk : ∀ i, is_smul_regular (β i) k) (x : Π i, β i) : hamm_wt x = hamm_wt (k • x) :=
+  (hk : ∀ i, is_smul_regular (β i) k) (x : Π i, β i) : hamm_wt x = hamm_wt (k • x) :=
 by convert hamm_dist_smul hk _ _; rw smul_zero'; refl
 
-lemma hamm_wt_eq_zero {x : Π i, β i} : hamm_wt x = 0 ↔ x = 0 := hamm_dist_eq_zero
+@[simp] lemma hamm_wt_eq_zero {x : Π i, β i} : hamm_wt x = 0 ↔ x = 0 := hamm_dist_eq_zero
 
 @[simp] lemma hamm_wt_zero : hamm_wt (0 : Π i, β i) = 0 := hamm_dist_self _
-
-lemma zero_of_hamm_wt_eq_zero (x : Π i, β i) :
-hamm_wt x = 0 → x = 0 := eq_of_hamm_dist_eq_zero _ _
 
 lemma hamm_wt_ne_zero {x : Π i, β i} : hamm_wt x ≠ 0 ↔ x ≠ 0 := hamm_dist_ne_zero
 
@@ -164,7 +164,7 @@ hamm_dist_ne_zero_iff_exists_ne
 end has_zero
 
 lemma hamm_dist_eq_hamm_wt_sub [Π i, add_group (β i)] (x y : Π i, β i) :
-hamm_dist x y = hamm_wt (x - y) :=
+  hamm_dist x y = hamm_wt (x - y) :=
 by simp_rw [hamm_wt_eq, hamm_dist_eq, pi.sub_apply, sub_ne_zero]
 
 end
@@ -175,8 +175,7 @@ variables {α ι : Type*} {β : ι → Type*} [fintype ι] [Π i, decidable_eq (
 
 instance : has_dist (hamm β) := ⟨λ x y, hamm_dist x y⟩
 
-@[simp, push_cast] lemma dist_eq_hamm_dist (x y : hamm β) :
-dist x y = hamm_dist x y := rfl
+@[simp, push_cast] lemma dist_eq_hamm_dist (x y : hamm β) : dist x y = hamm_dist x y := rfl
 
 instance : pseudo_metric_space (hamm β) :=
 { dist_self           := by push_cast; exact_mod_cast hamm_dist_self,
@@ -191,13 +190,12 @@ instance : metric_space (hamm β) :=
 instance [Π i, has_zero (β i)] : has_norm (hamm β) := ⟨λ x, hamm_wt x⟩
 
 @[simp, push_cast] lemma norm_eq_hamm_wt [Π i, has_zero (β i)] (x : hamm β) :
-∥x∥ = hamm_wt x := rfl
+  ∥x∥ = hamm_wt x := rfl
 
 instance [Π i, add_comm_group (β i)] : semi_normed_group (hamm β) :=
 { dist_eq := by push_cast; exact_mod_cast hamm_dist_eq_hamm_wt_sub, ..pi.add_comm_group }
 
-instance [Π i, add_comm_group (β i)] : normed_group (hamm β) :=
-{ ..hamm.semi_normed_group }
+instance [Π i, add_comm_group (β i)] : normed_group (hamm β) := { ..hamm.semi_normed_group }
 
 /-
 instance [Π i, add_comm_group (β i)] {α : Type*} [semiring α]

3e41b64bc123c5f9debbdafb469d7ecb00022ee4
Yury G. Kudryashov <urkud@urkud.name>
2022-06-15 14:26:22 -0400

    Snapshot

--- src/analysis/complex/unit_disc/basic.lean
+++ src/analysis/complex/unit_disc/basic.lean
@@ -4,16 +4,39 @@ Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Yury Kudryashov
 -/
 import analysis.complex.basic
+import analysis.normed.field.unit_ball
 
 /-!
 -/
+open set function
 
 namespace complex
 
-def unit_disc : Type := {p : ℂ | abs p < 1}
+@[derive [comm_semigroup, λ α, has_coe α ℂ]]
+def unit_disc : Type := metric.ball (0 : ℂ) 1
 
 localized "notation `𝔻` := complex.unit_disc" in unit_disc
 
+namespace unit_disc
 
+lemma coe_injective : injective (coe : 𝔻 → ℂ) := subtype.coe_injective
+
+lemma abs_lt_one (z : 𝔻) : abs (z : ℂ) < 1 := mem_ball_zero_iff.1 z.2
+
+def mk (z : ℂ) (hz : abs z < 1) : 𝔻 := ⟨z, mem_ball_zero_iff.2 hz⟩
+
+@[simp] lemma coe_mk (z : ℂ) (hz : abs z < 1) : (mk z hz : ℂ) = z := rfl
+
+@[simp] lemma mk_coe (z : 𝔻) (hz : abs (z : ℂ) < 1 := z.abs_lt_one) :
+  mk z hz = z :=
+subtype.eta _ _
+
+def re (z : 𝔻) : ℝ := re z
+
+def im (z : 𝔻) : ℝ := im z
+
+
+
+end unit_disc
 
 end complex
--- src/analysis/normed/field/unit_ball.lean
+++ src/analysis/normed/field/unit_ball.lean
@@ -18,6 +18,9 @@ def subsemigroup.unit_ball (𝕜 : Type*) [non_unital_semi_normed_ring 𝕜] :
 instance [non_unital_semi_normed_ring 𝕜] : semigroup (ball (0 : 𝕜) 1) :=
 mul_mem_class.to_semigroup (subsemigroup.unit_ball 𝕜)
 
+instance [semi_normed_comm_ring 𝕜] : comm_semigroup (ball (0 : 𝕜) 1) :=
+mul_mem_class.to_comm_semigroup (subsemigroup.unit_ball 𝕜)
+
 instance [non_unital_semi_normed_ring 𝕜] : has_distrib_neg (ball (0 : 𝕜) 1) :=
 subtype.coe_injective.has_distrib_neg (coe : ball (0 : 𝕜) 1 → 𝕜) (λ _, rfl) (λ _ _, rfl)
 
@@ -45,13 +48,19 @@ def submonoid.unit_closed_ball (𝕜 : Type*) [semi_normed_ring 𝕜] [norm_one_
 instance [semi_normed_ring 𝕜] [norm_one_class 𝕜] : monoid (closed_ball (0 : 𝕜) 1) :=
 submonoid_class.to_monoid (submonoid.unit_closed_ball 𝕜)
 
-def submonoid.unit_sphere (𝕜 : Type*) [normed_field 𝕜] : submonoid 𝕜 :=
+instance [semi_normed_comm_ring 𝕜] [norm_one_class 𝕜] : comm_monoid (closed_ball (0 : 𝕜) 1) :=
+submonoid_class.to_comm_monoid (submonoid.unit_closed_ball 𝕜)
+
+def submonoid.unit_sphere (𝕜 : Type*) [normed_division_ring 𝕜] : submonoid 𝕜 :=
 { carrier := sphere (0 : 𝕜) 1,
   mul_mem' := λ x y hx hy, by { rw [mem_sphere_zero_iff_norm] at *, simp * },
   one_mem' := mem_sphere_zero_iff_norm.2 norm_one }
 
-instance [normed_field 𝕜] : monoid (sphere (0 : 𝕜) 1) :=
+instance [normed_division_ring 𝕜] : monoid (sphere (0 : 𝕜) 1) :=
 submonoid_class.to_monoid (submonoid.unit_sphere 𝕜)
 
-instance [normed_field 𝕜] : has_distrib_neg (sphere (0 : 𝕜) 1) :=
+instance [normed_field 𝕜] : comm_monoid (sphere (0 : 𝕜) 1) :=
+submonoid_class.to_comm_monoid (submonoid.unit_sphere 𝕜)
+
+instance [normed_division_ring 𝕜] : has_distrib_neg (sphere (0 : 𝕜) 1) :=
 subtype.coe_injective.has_distrib_neg (coe : sphere (0 : 𝕜) 1 → 𝕜) (λ _, rfl) (λ _ _, rfl)

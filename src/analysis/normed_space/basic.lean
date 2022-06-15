c3e1730c713ef510368726d4da9d9deda96b1c51
Yury G. Kudryashov <urkud@urkud.name>
2022-06-14 22:55:12 -0400

    chore(analysis/normed): move `normed.normed_field` to `normed.field.basic`

--- src/analysis/normed_space/basic.lean
+++ src/analysis/normed_space/basic.lean
@@ -3,7 +3,7 @@ Copyright (c) 2018 Patrick Massot. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Patrick Massot, Johannes Hölzl
 -/
-import analysis.normed.normed_field
+import analysis.normed.field.basic
 import analysis.normed.group.infinite_sum
 import data.matrix.basic
 import topology.sequences

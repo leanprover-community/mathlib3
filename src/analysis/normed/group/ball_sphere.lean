5040016fb4f060900a48e9f7a9726c411563fda6
Yury G. Kudryashov <urkud@urkud.name>
2022-06-17 09:42:15 -0400

    Snapshot

--- src/analysis/normed/group/ball_sphere.lean
+++ src/analysis/normed/group/ball_sphere.lean
@@ -9,17 +9,20 @@ semi normed group.
 
 open metric set
 
-variables {E : Type*} [semi_normed_group E]
+variables {E : Type*} [semi_normed_group E] {r : ℝ}
 
 /-- We equip the sphere, in a seminormed group, with a formal operation of negation, namely the
 antipodal map. -/
-instance {r : ℝ} : has_involutive_neg (sphere (0 : E) r) :=
+instance : has_involutive_neg (sphere (0 : E) r) :=
 { neg := λ w, ⟨-↑w, by simp⟩,
   neg_neg := λ x, subtype.ext $ neg_neg x }
 
 @[simp] lemma coe_neg_sphere {r : ℝ} (v : sphere (0 : E) r) :
   (((-v) : sphere _ _) : E) = - (v:E) :=
 rfl
+
+instance : has_continuous_neg (sphere (0 : E) r) :=
+⟨continuous_subtype_mk _ continuous_subtype_coe.neg⟩
 
 /-- We equip the ball, in a seminormed group, with a formal operation of negation, namely the
 antipodal map. -/
@@ -31,6 +34,9 @@ instance {r : ℝ} : has_involutive_neg (ball (0 : E) r) :=
   (((-v) : ball _ _) : E) = - (v:E) :=
 rfl
 
+instance : has_continuous_neg (ball (0 : E) r) :=
+⟨continuous_subtype_mk _ continuous_subtype_coe.neg⟩
+
 /-- We equip the closed ball, in a seminormed group, with a formal operation of negation, namely the
 antipodal map. -/
 instance {r : ℝ} : has_involutive_neg (closed_ball (0 : E) r) :=
@@ -41,4 +47,5 @@ instance {r : ℝ} : has_involutive_neg (closed_ball (0 : E) r) :=
   (((-v) : closed_ball _ _) : E) = - (v:E) :=
 rfl
 
-
+instance : has_continuous_neg (closed_ball (0 : E) r) :=
+⟨continuous_subtype_mk _ continuous_subtype_coe.neg⟩

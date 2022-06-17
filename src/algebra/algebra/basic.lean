f136bcc1cb7014ae7c2a4bba99cd11e4cf88e7b8
Xavier Roblot <roblot@math.univ-lyon1.fr>
2022-06-17 16:04:00 +0200

    Fix Lint mathlib error

--- src/algebra/algebra/basic.lean
+++ src/algebra/algebra/basic.lean
@@ -1299,8 +1299,8 @@ def to_rat_alg_hom [ring R] [ring S] [algebra ℚ R] [algebra ℚ S] (f : R →+
 
 @[simp]
 lemma to_rat_alg_hom_to_ring_hom [ring R] [ring S] [algebra ℚ R] [algebra ℚ S]
-  (f : R →+* S) : ↑f.to_rat_alg_hom = f :=
-ring_hom.ext (by {intro x, refl, })
+  (f : R →+* S) : (f : R →+* S).to_rat_alg_hom = f :=
+ring_hom.ext $ λ x, rfl
 
 end ring_hom
 
@@ -1311,7 +1311,7 @@ variables {R S : Type*}
 @[simp]
 lemma alg_hom.to_ring_hom_to_rat_alg_hom [ring R] [ring S] [algebra ℚ R] [algebra ℚ S]
   (f : R →ₐ[ℚ] S) : (f : R →+* S).to_rat_alg_hom = f :=
-alg_hom.ext (by {intro x, refl, })
+alg_hom.ext $ λ x, rfl
 
 /-- The equivalence between `ring_hom` and `ℚ`-algebra homomorphisms. -/
 @[simps]

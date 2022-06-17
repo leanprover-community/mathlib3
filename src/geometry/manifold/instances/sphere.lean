870b4baf67b7bed154c2cc7410125f17c5c6a620
Yury G. Kudryashov <urkud@urkud.name>
2022-06-17 18:32:42 -0400

    Fix

--- src/geometry/manifold/instances/sphere.lean
+++ src/geometry/manifold/instances/sphere.lean
@@ -4,6 +4,7 @@ Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Heather Macbeth
 -/
 import analysis.complex.circle
+import analysis.normed_space.ball_action
 import analysis.inner_product_space.calculus
 import analysis.inner_product_space.pi_L2
 import geometry.manifold.algebra.lie_group

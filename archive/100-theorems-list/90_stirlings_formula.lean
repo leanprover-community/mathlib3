144b621fb21b3719467bfa1813d8b2b4f6710f37
Moritz Firsching <firsching@google.com>
2022-06-17 16:00:09 +0200

    no dots in the authors lines

--- archive/100-theorems-list/90_stirlings_formula.lean
+++ archive/100-theorems-list/90_stirlings_formula.lean
@@ -1,7 +1,7 @@
 /-
 Copyright (c) 2022. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
-Authors: Moritz Firsching, Fabian Kruse, Nikolas Kuhn.
+Authors: Moritz Firsching, Fabian Kruse, Nikolas Kuhn
 -/
 import tactic
 import analysis.special_functions.log

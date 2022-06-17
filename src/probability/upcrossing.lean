bbebe8dd90df0bb6decb5696bfa87da1d49ad70b
Kexing <kexing.ying19@imperial.ac.uk>
2022-06-17 17:59:10 +0300

    more api

--- src/probability/upcrossing.lean
+++ src/probability/upcrossing.lean
@@ -31,7 +31,7 @@ noncomputable
 def lower_crossing (a b : ℝ) (f : ℕ → α → ℝ) (N n : ℕ) : α → ℕ :=
 λ x, hitting f (set.Iic a) (upper_crossing a b f N n x) N x
 
-variables {a b : ℝ} {f : ℕ → α → ℝ} {N : ℕ} {n : ℕ} {x : α}
+variables {a b : ℝ} {f : ℕ → α → ℝ} {N : ℕ} {n m : ℕ} {x : α}
 
 @[simp]
 lemma upper_crossing_zero : upper_crossing a b f N 0 = 0 := rfl
@@ -66,32 +66,66 @@ begin
   exact le_hitting lower_crossing_le x,
 end
 
-lemma stopped_value_lower_crossing :
-  lower_crossing a b f N n x = N ∨
+lemma lower_crossing_mono (hnm : n ≤ m) :
+  lower_crossing a b f N n x ≤ lower_crossing a b f N m x :=
+begin
+  suffices : monotone (λ n, lower_crossing a b f N n x),
+  { exact this hnm },
+  exact monotone_nat_of_le_succ
+    (λ n, le_trans upper_crossing_le_lower_crossing_succ lower_crossing_le_upper_crossing)
+end
+
+lemma upper_crossing_mono (hnm : n ≤ m) :
+  upper_crossing a b f N n x ≤ upper_crossing a b f N m x :=
+begin
+  suffices : monotone (λ n, upper_crossing a b f N n x),
+  { exact this hnm },
+  exact monotone_nat_of_le_succ
+    (λ n, le_trans lower_crossing_le_upper_crossing upper_crossing_le_lower_crossing_succ),
+end
+
+lemma stopped_value_lower_crossing (h : lower_crossing a b f N n x ≠ N) :
   stopped_value f (lower_crossing a b f N n) x ≤ a :=
 begin
-  rw or_iff_not_imp_left,
-  intro h,
   obtain ⟨j, hj₁, hj₂⟩ :=
     (hitting_le_iff_of_lt _ (lt_of_le_of_ne lower_crossing_le h)).1 le_rfl,
   exact stopped_value_hitting_mem ⟨j, ⟨hj₁.1, le_trans hj₁.2 lower_crossing_le⟩, hj₂⟩,
 end
 
-lemma stopped_value_upper_crossing :
-  upper_crossing a b f N (n + 1) x = N ∨
+lemma stopped_value_upper_crossing (h : upper_crossing a b f N (n + 1) x ≠ N) :
   b ≤ stopped_value f (upper_crossing a b f N (n + 1)) x :=
 begin
-  rw or_iff_not_imp_left,
-  intro h,
   obtain ⟨j, hj₁, hj₂⟩ :=
     (hitting_le_iff_of_lt _ (lt_of_le_of_ne upper_crossing_le h)).1 le_rfl,
   exact stopped_value_hitting_mem ⟨j, ⟨hj₁.1, le_trans hj₁.2 (hitting_le _)⟩, hj₂⟩,
 end
 
+lemma lower_crossing_stabilize (hnm : n ≤ m) (hn : lower_crossing a b f N n x = N) :
+  lower_crossing a b f N m x = N :=
+le_antisymm lower_crossing_le (le_trans (le_of_eq hn.symm) (lower_crossing_mono hnm))
+
+lemma upper_crossing_stabilize (hnm : n ≤ m) (hn : upper_crossing a b f N n x = N) :
+  upper_crossing a b f N m x = N :=
+le_antisymm upper_crossing_le (le_trans (le_of_eq hn.symm) (upper_crossing_mono hnm))
+
 /-- The number of upcrossings (strictly) before time `N`. -/
 noncomputable
 def upcrossing (a b : ℝ) (f : ℕ → α → ℝ) (N : ℕ) (x : α) : ℕ :=
 Sup {n | upper_crossing a b f N n x < N}
+
+lemma upper_crossing_lt_bdd_above : bdd_above {n | upper_crossing a b f N n x < N} :=
+begin
+  sorry,
+end
+
+lemma le_sub_of_le_upcrossing (hn : n ≤ upcrossing a b f N x) :
+  b - a ≤
+  stopped_value f (upper_crossing a b f N (n + 1)) x -
+  stopped_value f (lower_crossing a b f N n) x :=
+begin
+  rw upcrossing at hn,
+  sorry
+end
 
 end upcrossing
 

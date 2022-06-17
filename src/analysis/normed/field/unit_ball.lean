d1dd181e3bd8ad3c7de457b313cd25055c61bb9e
Yury G. Kudryashov <urkud@urkud.name>
2022-06-17 09:53:04 -0400

    Add docs

--- src/analysis/normed/field/unit_ball.lean
+++ src/analysis/normed/field/unit_ball.lean
@@ -1,10 +1,24 @@
+/-
+Copyright (c) 2022 Yury Kudryashov. All rights reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
+Authors: Yury Kudryashov
+-/
 import analysis.normed.field.basic
 import analysis.normed.group.ball_sphere
+
+/-!
+# Algebraic structures on unit balls and spheres
+
+In this file we define algebraic structures (`semigroup`, `comm_semigroup`, `monoid`, `comm_monoid`,
+`group`, `comm_group`) on `metric.ball (0 : 𝕜) 1`, `metric.closed_ball (0 : 𝕜) 1`, and
+`metric.sphere (0 : 𝕜) 1`.
+-/
 
 open set metric
 
 variables {𝕜 : Type*}
 
+/-- Unit ball in a non unital semi normed ring as a bundled `subsemigroup`. -/
 def subsemigroup.unit_ball (𝕜 : Type*) [non_unital_semi_normed_ring 𝕜] :
   subsemigroup 𝕜 :=
 { carrier := ball (0 : 𝕜) 1,
@@ -28,6 +42,7 @@ mul_mem_class.to_comm_semigroup (subsemigroup.unit_ball 𝕜)
 instance [non_unital_semi_normed_ring 𝕜] : has_distrib_neg (ball (0 : 𝕜) 1) :=
 subtype.coe_injective.has_distrib_neg (coe : ball (0 : 𝕜) 1 → 𝕜) (λ _, rfl) (λ _ _, rfl)
 
+/-- Closed unit ball in a non unital semi normed ring as a bundled `subsemigroup`. -/
 def subsemigroup.unit_closed_ball (𝕜 : Type*) [non_unital_semi_normed_ring 𝕜] :
   subsemigroup 𝕜 :=
 { carrier := closed_ball 0 1,
@@ -47,6 +62,7 @@ instance [non_unital_semi_normed_ring 𝕜] : has_continuous_mul (closed_ball (0
 ⟨continuous_subtype_mk _ $ continuous.mul (continuous_subtype_val.comp continuous_fst)
   (continuous_subtype_val.comp continuous_snd)⟩
 
+/-- Closed unit ball in a semi normed ring as a bundled `submonoid`. -/
 def submonoid.unit_closed_ball (𝕜 : Type*) [semi_normed_ring 𝕜] [norm_one_class 𝕜] :
   submonoid 𝕜 :=
 { carrier := closed_ball 0 1,
@@ -59,6 +75,7 @@ submonoid_class.to_monoid (submonoid.unit_closed_ball 𝕜)
 instance [semi_normed_comm_ring 𝕜] [norm_one_class 𝕜] : comm_monoid (closed_ball (0 : 𝕜) 1) :=
 submonoid_class.to_comm_monoid (submonoid.unit_closed_ball 𝕜)
 
+/-- Unit sphere in a normed division ring as a bundled `submonoid`. -/
 def submonoid.unit_sphere (𝕜 : Type*) [normed_division_ring 𝕜] : submonoid 𝕜 :=
 { carrier := sphere (0 : 𝕜) 1,
   mul_mem' := λ x y hx hy, by { rw [mem_sphere_zero_iff_norm] at *, simp * },
@@ -73,6 +90,7 @@ instance [normed_division_ring 𝕜] : group (sphere (0 : 𝕜) 1) :=
 instance [normed_division_ring 𝕜] : has_distrib_neg (sphere (0 : 𝕜) 1) :=
 subtype.coe_injective.has_distrib_neg (coe : sphere (0 : 𝕜) 1 → 𝕜) (λ _, rfl) (λ _ _, rfl)
 
+/-- Monoid homomorphism from the unit sphere to the group of units. -/
 def unit_sphere_to_units (𝕜 : Type*) [normed_division_ring 𝕜] : sphere (0 : 𝕜) 1 →* units 𝕜 :=
 units.lift_right (submonoid.unit_sphere 𝕜).subtype (λ x, units.mk0 x $ ne_zero_of_mem_unit_sphere _)
   (λ x, rfl)

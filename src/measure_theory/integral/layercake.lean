5660c70b91087481fee047c18066ceda0653402f
RemyDegenne <Remydegenne@gmail.com>
2022-06-18 09:36:53 +0200

    golf

--- src/measure_theory/integral/layercake.lean
+++ src/measure_theory/integral/layercake.lean
@@ -28,75 +28,61 @@ layer cake representation, Cavalieri's principle
 -/
 
 noncomputable theory
-open_locale classical topological_space ennreal measure_theory
-open set function real ennreal
-open measure_theory measurable_space measure_theory.measure
-open topological_space
-open filter
+open_locale ennreal measure_theory
+open set measure_theory filter
 
 /-! ### Layercake theorem -/
 section layercake
 
 namespace measure_theory
 
-variables {α : Type*} [measurable_space α] {f g : α → ℝ} {s : set α}
+variables {α : Type*} [measurable_space α] {f : α → ℝ} {g : ℝ → ℝ} {s : set α}
 
 /-- An auxiliary version of the layer cake theorem (Cavalieri's principle), with a
 measurability assumption that would also essentially follow from the integrability assumptions.
 
 See `measure_theory.layercake` for the main formulation of the layer cake theorem. -/
-lemma layercake_of_measurable
-  (μ : measure α) [sigma_finite μ]
-  {f : α → ℝ} (f_nn : 0 ≤ f) (f_mble : measurable f)
-  {g : ℝ → ℝ} (g_intble : ∀ t > 0, interval_integrable g volume 0 t)
+lemma layercake_of_measurable (μ : measure α) [sigma_finite μ]
+  (f_nn : 0 ≤ f) (f_mble : measurable f)
+  (g_intble : ∀ t > 0, interval_integrable g volume 0 t)
   (g_mble : measurable g) (g_nn : ∀ t > 0, 0 ≤ g t) :
   ∫⁻ ω, ennreal.of_real (∫ t in 0 .. (f ω), g t) ∂μ
-    = ∫⁻ t, (μ {a : α | t ≤ f a}) * ennreal.of_real (g t) ∂(volume.restrict (Ioi 0)) :=
+    = ∫⁻ t in Ioi 0, (μ {a : α | t ≤ f a}) * ennreal.of_real (g t) :=
 begin
+  have g_intble' : ∀ (t : ℝ), 0 ≤ t → interval_integrable g volume 0 t,
+  { intros t ht,
+    cases eq_or_lt_of_le ht,
+    { simp [← h], },
+    { exact g_intble t h, }, },
   have integrand_eq : ∀ ω, ennreal.of_real (∫ t in 0 .. (f ω), g t)
-        = ∫⁻ t, ennreal.of_real (g t) ∂(volume.restrict (Ioc 0 (f ω))),
+        = ∫⁻ t in Ioc 0 (f ω), ennreal.of_real (g t),
   { intro ω,
-    by_cases f ω = 0, by simp [h],
-    have f_pos : 0 < f ω, from lt_of_le_of_ne (f_nn ω) (ne_comm.mp h),
-    have intble_now := g_intble (f ω) f_pos,
     have g_ae_nn : 0 ≤ᵐ[volume.restrict (Ioc 0 (f ω))] g,
-    { filter_upwards [self_mem_ae_restrict
-                      (show measurable_set (Ioc 0 (f ω)), from measurable_set_Ioc)],
-      exact λ x hx, g_nn x hx.1, },
-    rw ← of_real_integral_eq_lintegral_of_real intble_now.1 g_ae_nn,
-    apply congr_arg,
-    rw interval_integral.interval_integral_eq_integral_interval_oc,
-    simp only [interval_oc_of_le, (show (0 ≤ f ω), from f_nn ω), if_true,
-               algebra.id.smul_eq_mul, one_mul], },
-  simp_rw [integrand_eq, ← lintegral_indicator (λ t, ennreal.of_real (g t)) measurable_set_Ioc],
-  simp_rw [← lintegral_indicator _ measurable_set_Ioi],
+    { filter_upwards [self_mem_ae_restrict (measurable_set_Ioc : measurable_set (Ioc 0 (f ω)))]
+        with x hx using g_nn x hx.1, },
+    rw ← of_real_integral_eq_lintegral_of_real (g_intble' (f ω) (f_nn ω)).1 g_ae_nn,
+    congr,
+    exact interval_integral.integral_of_le (f_nn ω), },
+  simp_rw [integrand_eq, ← lintegral_indicator (λ t, ennreal.of_real (g t)) measurable_set_Ioc,
+    ← lintegral_indicator _ measurable_set_Ioi],
   rw lintegral_lintegral_swap,
   { apply congr_arg,
     funext s,
-    have aux₁ : (λ (x : α), (Ioc 0 (f x)).indicator (λ (t : ℝ), ennreal.of_real (g t)) s)
-               = (λ (x : α), (ennreal.of_real (g s) * (Ioi (0 : ℝ)).indicator (λ _, 1) s)
+    have aux₁ : (λ x, (Ioc 0 (f x)).indicator (λ (t : ℝ), ennreal.of_real (g t)) s)
+               = (λ x, (ennreal.of_real (g s) * (Ioi (0 : ℝ)).indicator (λ _, 1) s)
                              * (Ici s).indicator (λ (t : ℝ), (1 : ℝ≥0∞)) (f x)),
     { funext a,
       by_cases s ∈ Ioc (0 : ℝ) (f a),
       { simp only [h, (show s ∈ Ioi (0 : ℝ), from h.1),
                     (show f a ∈ Ici s, from h.2), indicator_of_mem, mul_one], },
-      { simp only [mem_Ioc, not_and, not_le] at h,
+      { have h_copy := h,
+        simp only [mem_Ioc, not_and, not_le] at h,
         by_cases h' : 0 < s,
-        { simp only [(show s ∉ Ioc 0 (f a), by simpa only [mem_Ioc, not_and, not_le] using h),
-                     indicator_of_not_mem, not_false_iff, zero_eq_mul, mul_eq_zero,
-                     ennreal.of_real_eq_zero, indicator_apply_eq_zero, mem_Ioi,
-                     one_ne_zero, mem_Ici],
-          right,
-          intro con,
-          specialize h h',
-          rw ← not_lt at con,
-          contradiction, },
-        { simp at h',
-          have : s ∉ Ioi (0 : ℝ), by simp only [h', mem_Ioi, not_lt],
-          simp only [this, indicator_of_not_mem, not_false_iff, mul_zero,
-                    zero_mul, indicator_apply_eq_zero, mem_Ioc, ennreal.of_real_eq_zero, and_imp],
-          intros con,
-          contradiction, }, }, },
+        { simp only [h_copy, h h', indicator_of_not_mem, not_false_iff, mem_Ici, not_le,
+            mul_zero], },
+        { have : s ∉ Ioi (0 : ℝ) := h',
+          simp only [this, h', indicator_of_not_mem, not_false_iff, mul_zero, zero_mul, mem_Ioc,
+            false_and], }, }, },
     simp_rw aux₁,
     rw lintegral_const_mul',
     swap, { apply ennreal.mul_ne_top ennreal.of_real_ne_top,
@@ -104,39 +90,57 @@ begin
     simp_rw [(show (λ a, (Ici s).indicator (λ (t : ℝ), (1 : ℝ≥0∞)) (f a))
                 = (λ a, {a : α | s ≤ f a}.indicator (λ _, 1) a),
               by { funext a, by_cases s ≤ f a; simp [h], })],
-    rw lintegral_indicator, swap, { exact f_mble measurable_set_Ici, },
-    rw lintegral_one,
-    simp only [measure.restrict_apply, measurable_set.univ, univ_inter],
-    rw indicator_mul_left,
-    rw mul_assoc,
-    rw [(show (Ioi 0).indicator (λ (_x : ℝ), (1 : ℝ≥0∞)) s * μ {a : α | s ≤ f a}
+    rw lintegral_indicator,
+    swap, { exact f_mble measurable_set_Ici, },
+    rw [lintegral_one, measure.restrict_apply measurable_set.univ, univ_inter, indicator_mul_left,
+      mul_assoc,
+      (show (Ioi 0).indicator (λ (_x : ℝ), (1 : ℝ≥0∞)) s * μ {a : α | s ≤ f a}
                 = (Ioi 0).indicator (λ (_x : ℝ), 1 * μ {a : α | s ≤ f a}) s,
          by { by_cases 0 < s; simp [h], })],
-    rw mul_comm _ (ennreal.of_real _),
-    simp_rw [one_mul],
+    simp_rw [mul_comm _ (ennreal.of_real _), one_mul],
     refl, },
   have aux₂ : function.uncurry
               (λ (x : α) (y : ℝ), (Ioc 0 (f x)).indicator (λ (t : ℝ), ennreal.of_real (g t)) y)
               = {p : α × ℝ | p.2 ∈ Ioc 0 (f p.1)}.indicator (λ p, ennreal.of_real (g p.2)),
   { funext p,
-    by_cases p.2 ∈ Ioc 0 (f p.1),
-    { have h' : p ∈ {p : α × ℝ | p.snd ∈ Ioc 0 (f p.fst)}, by simpa only using h,
-      rw set.indicator_of_mem h',
-      cases p,
-      dsimp,
-      simp only [h, indicator_of_mem], },
-    { have h' : p ∉ {p : α × ℝ | p.snd ∈ Ioc 0 (f p.fst)}, by simpa only using h,
-      rw set.indicator_of_not_mem h',
-      cases p,
-      dsimp,
-      rw set.indicator_of_not_mem h, }, },
+    cases p,
+    dsimp,
+    by_cases p_snd ∈ Ioc 0 (f p_fst),
+    { have h' : (p_fst, p_snd) ∈ {p : α × ℝ | p.snd ∈ Ioc 0 (f p.fst)} := h,
+      rw [set.indicator_of_mem h', set.indicator_of_mem h], },
+    { have h' : (p_fst, p_snd) ∉ {p : α × ℝ | p.snd ∈ Ioc 0 (f p.fst)} := h,
+      rw [set.indicator_of_not_mem h', set.indicator_of_not_mem h], }, },
   rw aux₂,
   have mble := measurable_set_region_between_oc measurable_zero f_mble measurable_set.univ,
-  simp only [mem_univ, pi.zero_apply, true_and] at mble,
-  apply (ae_measurable_indicator_iff mble).mpr,
-  apply measurable.ae_measurable,
-  apply ennreal.measurable_of_real.comp,
-  exact g_mble.comp measurable_snd,
+  simp_rw [mem_univ, pi.zero_apply, true_and] at mble,
+  exact (ennreal.measurable_of_real.comp (g_mble.comp measurable_snd)).ae_measurable.indicator mble,
+end
+
+lemma ae_measurable_Ioi_of_forall_Ioc
+  (g_meas : ∀ t > 0, ae_measurable g (volume.restrict (Ioc 0 t))) :
+  ae_measurable g (volume.restrict (Ioi 0)) :=
+begin
+  have Ioi_eq_Union : Ioi 0 = ⋃ n : ℕ, Ioc 0 (n : ℝ),
+  { rw Union_Ioc_eq_Ioi_self_iff.mpr _,
+    exact λ x _, exists_nat_ge x, },
+  rw [Ioi_eq_Union, ae_measurable_Union_iff],
+  intros n,
+  by_cases n = 0,
+  { simp only [h, nat.cast_zero, Ioc_self,
+                measure.restrict_empty, _root_.ae_measurable_zero_measure], },
+  { refine g_meas n _,
+    rw [gt_iff_lt, nat.cast_pos],
+    exact lt_of_le_of_ne (zero_le _) (ne.symm h), },
+end
+
+lemma _root_.ae_measurable.exists_measurable_nonneg {μ : measure ℝ}
+  (hg : ae_measurable g μ) (g_nn : ∀ᵐ t ∂μ, 0 ≤ g t) :
+  ∃ (G : ℝ → ℝ), measurable G ∧ g =ᵐ[μ] G ∧ 0 ≤ G :=
+begin
+  rcases hg with ⟨G₁, mble_G₁, g_eq_G₁⟩,
+  refine ⟨λ a, max 0 (G₁ a), measurable_const.max mble_G₁, _, λ a, le_max_left 0 (G₁ a)⟩,
+  filter_upwards [g_nn, g_eq_G₁] with a ga_nn ga_eq,
+  rw [← ga_eq, max_eq_right ga_nn],
 end
 
 /-- The layer cake theorem (Cavalieri's principle):
@@ -148,109 +152,78 @@ over the positive real line of the "complementary cumulative distribution functi
 (i.e., a function giving the measures of the sets on which `f` exceeds different positive real
 values) weighted by `g`. -/
 theorem layercake (μ : measure α) [sigma_finite μ]
-  {f : α → ℝ} (f_nn : 0 ≤ f) (f_mble : measurable f)
-  {g : ℝ → ℝ} (g_intble : ∀ t > 0, interval_integrable g volume 0 t)
-  (g_nn : ∀ᵐ t ∂(volume.restrict (Ioi (0 : ℝ))), 0 ≤ g t) :
-  ∫⁻ ω, ennreal.of_real (∫ t in 0 .. (f ω), g t) ∂μ
-    = ∫⁻ t, (μ {a : α | t ≤ f a}) * ennreal.of_real (g t) ∂(volume.restrict (Ioi 0)) :=
+  (f_nn : 0 ≤ f) (f_mble : measurable f)
+  (g_intble : ∀ t > 0, interval_integrable g volume 0 t)
+  (g_nn : ∀ᵐ t ∂(volume.restrict (Ioi 0)), 0 ≤ g t) :
+  ∫⁻ ω, ennreal.of_real (∫ t in 0 .. f ω, g t) ∂μ
+    = ∫⁻ t in Ioi 0, μ {a : α | t ≤ f a} * ennreal.of_real (g t) :=
 begin
-  have ex_G : ∃ (G : ℝ → ℝ),
-              measurable G ∧ g =ᵐ[volume.restrict (Ioi (0 : ℝ))] G ∧ 0 ≤ G,
-  { suffices : ∃ (G : ℝ → ℝ), measurable G ∧ g =ᵐ[volume.restrict (Ioi (0 : ℝ))] G,
-    { rcases this with ⟨G₁, mble_G₁, g_eq_G₁⟩,
-      use (λ a, max 0 (G₁ a)),
-      refine ⟨measurable.max measurable_const mble_G₁, _, _⟩,
-      { filter_upwards [g_nn, g_eq_G₁],
-        intros a ga_nn ga_eq,
-        simp only [←ga_eq, ga_nn, max_eq_right], },
-      { exact λ a, by simp only [pi.zero_apply, le_max_iff, le_refl, true_or], }, },
-    suffices : ae_measurable g (volume.restrict (Ioi (0 : ℝ))), from this,
-    have Ioi_eq_Union : Ioi (0 : ℝ) = ⋃ (n : ℕ), Ioc 0 (n : ℝ),
-    { rw Union_Ioc_eq_Ioi_self_iff.mpr _,
-      exact λ x _, exists_nat_ge x, },
-    rw [Ioi_eq_Union, ae_measurable_Union_iff],
-    intros n,
-    by_cases n = 0,
-    { simp only [h, nat.cast_zero, Ioc_self,
-                 measure.restrict_empty, _root_.ae_measurable_zero_measure], },
-    { exact (g_intble n (nat.cast_pos.mpr (zero_lt_iff.mpr h))).1.1.ae_measurable, }, },
-  rcases ex_G with ⟨G, ⟨G_mble, g_eq_G, G_nn⟩⟩,
-  have g_eq_G_on : ∀ (t : ℝ), g =ᵐ[volume.restrict (Ioc 0 t)] G,
-  from λ t, ae_mono (measure.restrict_mono Ioc_subset_Ioi_self le_rfl) g_eq_G,
-  have G_nn' : ∀ t > 0, 0 ≤ G t, from λ t t_pos, G_nn t,
+  have ex_G : ∃ (G : ℝ → ℝ), measurable G ∧ g =ᵐ[volume.restrict (Ioi 0)] G ∧ 0 ≤ G,
+  { refine ae_measurable.exists_measurable_nonneg _ g_nn,
+    exact ae_measurable_Ioi_of_forall_Ioc (λ t ht, (g_intble t ht).1.1.ae_measurable), },
+  rcases ex_G with ⟨G, G_mble, g_eq_G, G_nn⟩,
+  have g_eq_G_on : ∀ t, g =ᵐ[volume.restrict (Ioc 0 t)] G,
+    from λ t, ae_mono (measure.restrict_mono Ioc_subset_Ioi_self le_rfl) g_eq_G,
   have G_intble : ∀ t > 0, interval_integrable G volume 0 t,
-  { intros t t_pos,
-    refine ⟨integrable_on.congr_fun' (g_intble t t_pos).1 (g_eq_G_on t), _⟩,
-    convert integrable_on_empty,
-    exact Ioc_eq_empty_of_le (show 0 ≤ t, by linarith), },
-  have eq₁ : ∫⁻ (t : ℝ) in Ioi 0, μ {a : α | t ≤ f a} * ennreal.of_real (g t)
-             = ∫⁻ (t : ℝ) in Ioi 0, μ {a : α | t ≤ f a} * ennreal.of_real (G t),
+  { refine λ t t_pos, ⟨integrable_on.congr_fun' (g_intble t t_pos).1 (g_eq_G_on t), _⟩,
+    rw Ioc_eq_empty_of_le t_pos.lt.le,
+    exact integrable_on_empty, },
+  have eq₁ : ∫⁻ t in Ioi 0, μ {a : α | t ≤ f a} * ennreal.of_real (g t)
+             = ∫⁻ t in Ioi 0, μ {a : α | t ≤ f a} * ennreal.of_real (G t),
   { apply lintegral_congr_ae,
-    filter_upwards [g_eq_G],
-    intros a ha,
+    filter_upwards [g_eq_G] with a ha,
     rw [ha], },
-  have eq₂ : ∀ ω, ∫ (t : ℝ) in 0..f ω, g t = ∫ (t : ℝ) in 0..f ω, G t,
-  { intro ω,
-    have fω_nn : 0 ≤ f ω, by simpa only using f_nn ω,
-    rw [interval_integral.integral_of_le fω_nn, interval_integral.integral_of_le fω_nn],
-    exact integral_congr_ae (g_eq_G_on (f ω)), },
-  rw eq₁,
-  simp_rw eq₂,
+  have eq₂ : ∀ ω, ∫ t in 0..f ω, g t = ∫ t in 0..f ω, G t,
+  { refine λ ω, interval_integral.integral_congr_ae _,
+    have fω_nn : 0 ≤ f ω := f_nn ω,
+    rw [interval_oc_of_le fω_nn,
+      ← ae_restrict_iff' (measurable_set_Ioc : measurable_set (Ioc (0 : ℝ) (f ω)))],
+    exact g_eq_G_on (f ω), },
+  simp_rw [eq₁, eq₂],
   exact layercake_of_measurable μ f_nn f_mble G_intble G_mble (λ t t_pos, G_nn t),
 end
 
-theorem layercake_one
-  (μ : measure α) [sigma_finite μ]
-  {f : α → ℝ} (f_nn : 0 ≤ f) (f_mble : measurable f) :
+theorem layercake_one (μ : measure α) [sigma_finite μ] (f_nn : 0 ≤ f) (f_mble : measurable f) :
   ∫⁻ ω, ennreal.of_real (f ω) ∂μ = ∫⁻ t in Ioi 0, (μ {a : α | t ≤ f a}) :=
 begin
   set cst := λ (t : ℝ), (1 : ℝ) with def_cst,
   have cst_intble : ∀ t > 0, interval_integrable cst volume 0 t,
-  from λ _ _, interval_integrable_const,
+    from λ _ _, interval_integrable_const,
   have key := layercake μ f_nn f_mble cst_intble (eventually_of_forall (λ t, zero_le_one)),
   simp_rw [def_cst, ennreal.of_real_one, mul_one] at key,
   rw ← key,
-  apply congr_arg,
-  funext ω,
+  congr' with ω,
   simp only [interval_integral.integral_const, sub_zero, algebra.id.smul_eq_mul, mul_one],
 end
 
 -- TODO: The assumption `(p_large : 0 < p)` should suffice, but the assumptions of
 -- `interval_integral.interval_integrable_rpow` and `interval_integral.integral_rpow`
 -- are too stringent to apply here.
-theorem layercake_p
-  (μ : measure α) [sigma_finite μ]
-  {f : α → ℝ} (f_nn : 0 ≤ f) (f_mble : measurable f) {p : ℝ}
-  (p_large : 1 < p) :
+theorem layercake_p (μ : measure α) [sigma_finite μ]
+  (f_nn : 0 ≤ f) (f_mble : measurable f) {p : ℝ} (p_large : 1 < p) :
   ∫⁻ ω, ennreal.of_real ((f ω)^p) ∂μ
     = (ennreal.of_real p) * ∫⁻ t in Ioi 0, (μ {a : α | t ≤ f a}) * ennreal.of_real (t^(p-1)) :=
 begin
-  have p_pos : 0 < p := by linarith,
+  have p_pos : 0 < p := zero_le_one.trans_lt p_large,
+  have p_sub_one : 0 ≤ p - 1 := by linarith,
   have obs : ∀ (x : ℝ), (∫ (t : ℝ) in 0..x, t^(p-1)) = x^p / p,
   { intros x,
-    rw integral_rpow (or.inl (show 0 ≤ p - 1, by linarith)),
+    rw integral_rpow (or.inl p_sub_one),
     simp [real.zero_rpow p_pos.ne.symm], },
   set g := λ (t : ℝ), t^(p-1) with g_def,
   have g_nn : ∀ᵐ t ∂(volume.restrict (Ioi (0 : ℝ))), 0 ≤ g t,
-  { filter_upwards [self_mem_ae_restrict
-                    (show measurable_set (Ioi (0 : ℝ)), from measurable_set_Ioi)],
+  { filter_upwards [self_mem_ae_restrict (measurable_set_Ioi : measurable_set (Ioi (0 : ℝ)))],
     intros t t_pos,
     rw g_def,
-    apply real.rpow_nonneg_of_nonneg (mem_Ioi.mp t_pos).le, },
+    exact real.rpow_nonneg_of_nonneg (mem_Ioi.mp t_pos).le (p - 1), },
   have g_intble : ∀ t > 0, interval_integrable g volume 0 t,
-  from λ _ _, interval_integral.interval_integrable_rpow (or.inl (show 0 ≤ p - 1, by linarith)),
+    from λ _ _, interval_integral.interval_integrable_rpow (or.inl p_sub_one),
   have key := layercake μ f_nn f_mble g_intble g_nn,
   simp_rw [g_def] at key,
   rw [← key, ← lintegral_const_mul (ennreal.of_real p)]; simp_rw obs,
-  { apply congr_arg,
-    funext ω,
+  { congr' with ω,
     rw [← ennreal.of_real_mul p_pos.le, mul_div_cancel' ((f ω)^p) p_pos.ne.symm], },
-  { apply ennreal.measurable_of_real.comp,
-    apply (measurable_div_const' p).comp,
-    have : measurable (λ (z : ℝ), z^p),
-    { apply continuous.measurable,
-      exact continuous.rpow_const continuous_id (λ _, (or.inr p_pos.le)), },
-    apply this.comp f_mble, },
+  { exact ((f_mble.pow measurable_const).div_const p).ennreal_of_real, },
 end
 
 end measure_theory

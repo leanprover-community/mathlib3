f9ced04d0688485e52efe0705234d4f60d0f8c85
Yury G. Kudryashov <urkud@urkud.name>
2022-06-17 22:01:57 -0400

    Add Heather to authors

--- src/analysis/normed/field/unit_ball.lean
+++ src/analysis/normed/field/unit_ball.lean
@@ -1,7 +1,7 @@
 /-
 Copyright (c) 2022 Yury Kudryashov. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
-Authors: Yury Kudryashov
+Authors: Yury Kudryashov, Heather Macbeth
 -/
 import analysis.normed.field.basic
 import analysis.normed.group.ball_sphere

d08c6a14b7125f3c4464d8ce9e057c87e2af22de
Kexing <kexing.ying19@imperial.ac.uk>
2022-06-20 15:18:39 +0300

    upper crossing is bounded above

--- src/probability/upcrossing.lean
+++ src/probability/upcrossing.lean
@@ -55,11 +55,11 @@ end
 lemma lower_crossing_le : lower_crossing a b f N n x ≤ N :=
 by simp only [lower_crossing, hitting_le x]
 
-lemma lower_crossing_le_upper_crossing :
+lemma upper_crossing_le_lower_crossing :
   upper_crossing a b f N n x ≤ lower_crossing a b f N n x :=
 by simp only [lower_crossing, le_hitting upper_crossing_le x]
 
-lemma upper_crossing_le_lower_crossing_succ :
+lemma lower_crossing_le_upper_crossing_succ :
   lower_crossing a b f N n x ≤ upper_crossing a b f N (n + 1) x :=
 begin
   rw upper_crossing_succ,
@@ -72,7 +72,7 @@ begin
   suffices : monotone (λ n, lower_crossing a b f N n x),
   { exact this hnm },
   exact monotone_nat_of_le_succ
-    (λ n, le_trans upper_crossing_le_lower_crossing_succ lower_crossing_le_upper_crossing)
+    (λ n, le_trans lower_crossing_le_upper_crossing_succ upper_crossing_le_lower_crossing)
 end
 
 lemma upper_crossing_mono (hnm : n ≤ m) :
@@ -81,7 +81,7 @@ begin
   suffices : monotone (λ n, upper_crossing a b f N n x),
   { exact this hnm },
   exact monotone_nat_of_le_succ
-    (λ n, le_trans lower_crossing_le_upper_crossing upper_crossing_le_lower_crossing_succ),
+    (λ n, le_trans upper_crossing_le_lower_crossing lower_crossing_le_upper_crossing_succ),
 end
 
 lemma stopped_value_lower_crossing (h : lower_crossing a b f N n x ≠ N) :
@@ -100,6 +100,30 @@ begin
   exact stopped_value_hitting_mem ⟨j, ⟨hj₁.1, le_trans hj₁.2 (hitting_le _)⟩, hj₂⟩,
 end
 
+lemma upper_crossing_lt_lower_crossing (hab : a < b) (hn : lower_crossing a b f N (n + 1) x ≠ N) :
+  upper_crossing a b f N (n + 1) x < lower_crossing a b f N (n + 1) x :=
+begin
+  refine lt_of_le_of_ne upper_crossing_le_lower_crossing
+    (λ h, not_le.2 hab $ le_trans _ (stopped_value_lower_crossing hn)),
+  simp only [stopped_value],
+  rw ← h,
+  exact stopped_value_upper_crossing (h.symm ▸ hn),
+end
+
+lemma lower_crossing_lt_upper_crossing (hab : a < b) (hn : upper_crossing a b f N (n + 1) x ≠ N) :
+  lower_crossing a b f N n x < upper_crossing a b f N (n + 1) x :=
+begin
+  refine lt_of_le_of_ne lower_crossing_le_upper_crossing_succ
+    (λ h, not_le.2 hab $ le_trans (stopped_value_upper_crossing hn) _),
+  simp only [stopped_value],
+  rw ← h,
+  refine stopped_value_lower_crossing (h.symm ▸ hn),
+end
+
+lemma upper_crossing_lt_succ (hab : a < b) (hn : upper_crossing a b f N (n + 1) x ≠ N) :
+  upper_crossing a b f N n x < upper_crossing a b f N (n + 1) x :=
+lt_of_le_of_lt upper_crossing_le_lower_crossing (lower_crossing_lt_upper_crossing hab hn)
+
 lemma lower_crossing_stabilize (hnm : n ≤ m) (hn : lower_crossing a b f N n x = N) :
   lower_crossing a b f N m x = N :=
 le_antisymm lower_crossing_le (le_trans (le_of_eq hn.symm) (lower_crossing_mono hnm))
@@ -108,15 +132,44 @@ lemma upper_crossing_stabilize (hnm : n ≤ m) (hn : upper_crossing a b f N n x 
   upper_crossing a b f N m x = N :=
 le_antisymm upper_crossing_le (le_trans (le_of_eq hn.symm) (upper_crossing_mono hnm))
 
+lemma lower_crossing_stabilize' (hnm : n ≤ m) (hn : N ≤ lower_crossing a b f N n x) :
+  lower_crossing a b f N m x = N :=
+lower_crossing_stabilize hnm (le_antisymm lower_crossing_le hn)
+
+lemma upper_crossing_stabilize' (hnm : n ≤ m) (hn : N ≤ upper_crossing a b f N n x) :
+  upper_crossing a b f N m x = N :=
+upper_crossing_stabilize hnm (le_antisymm upper_crossing_le hn)
+
+-- from #12509
+lemma strict_mono.not_bdd_above_range {α β} [preorder α] [no_max_order α] [nonempty α] [preorder β]
+  [succ_order β] [is_succ_archimedean β] {f : α → β} (hf : strict_mono f) :
+  ¬ bdd_above (set.range f) :=
+begin
+  sorry
+end
+
+lemma exists_upper_crossing_eq (f : ℕ → α → ℝ) (N : ℕ) (x : α) (hab : a < b) :
+  ∃ n, upper_crossing a b f N n x = N :=
+begin
+  by_contra h, push_neg at h,
+  have : strict_mono (λ n, upper_crossing a b f N n x) :=
+    strict_mono_nat_of_lt_succ (λ n, upper_crossing_lt_succ hab (h _)),
+  obtain ⟨_, ⟨k, rfl⟩, hk⟩ := not_bdd_above_iff.1 (strict_mono.not_bdd_above_range this) N,
+  exact not_le.2 hk upper_crossing_le
+end
+
+lemma upper_crossing_lt_bdd_above (hab : a < b) : bdd_above {n | upper_crossing a b f N n x < N} :=
+begin
+  obtain ⟨k, hk⟩ := exists_upper_crossing_eq f N x hab,
+  refine ⟨k, λ n (hn : upper_crossing a b f N n x < N), _⟩,
+  by_contra hn',
+  exact hn.ne (upper_crossing_stabilize (not_le.1 hn').le hk)
+end
+
 /-- The number of upcrossings (strictly) before time `N`. -/
 noncomputable
 def upcrossing (a b : ℝ) (f : ℕ → α → ℝ) (N : ℕ) (x : α) : ℕ :=
 Sup {n | upper_crossing a b f N n x < N}
-
-lemma upper_crossing_lt_bdd_above : bdd_above {n | upper_crossing a b f N n x < N} :=
-begin
-  sorry,
-end
 
 lemma le_sub_of_le_upcrossing (hn : n ≤ upcrossing a b f N x) :
   b - a ≤

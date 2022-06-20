6b138e21fe925f35bcf594f58ffdd0c6edeb59f5
Stuart Presnell <stuart.presnell@gmail.com>
2022-06-20 10:27:38 +0100

    Fix long line

--- src/data/nat/totient.lean
+++ src/data/nat/totient.lean
@@ -336,7 +336,8 @@ begin
 end
 
 lemma gcd_prod_double_counting {β : Type*} [comm_monoid β] (m n : ℕ) (f : ℕ → β) :
-    (∏ (x : ℕ) in (m.gcd n).factors.to_finset, f x) * ∏ (x : ℕ) in (m * n).factors.to_finset, f x = (∏ (x : ℕ) in m.factors.to_finset, f x) * ∏ (x : ℕ) in n.factors.to_finset, f x :=
+    (∏ (x : ℕ) in (m.gcd n).factors.to_finset, f x) * ∏ (x : ℕ) in (m * n).factors.to_finset, f x
+     = (∏ (x : ℕ) in m.factors.to_finset, f x) * ∏ (x : ℕ) in n.factors.to_finset, f x :=
 begin
   rcases eq_or_ne n 0 with rfl | hm0, { simp },
   rcases eq_or_ne m 0 with rfl | hn0, { simp },
@@ -364,3 +365,4 @@ begin
 end
 
 end nat
+#lint

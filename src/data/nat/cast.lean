449e470657319555a1082091f0ab72b57af8e31e
Vierkantor <vierkantor@vierkantor.com>
2022-06-20 13:39:22 +0000

    Fix import

--- src/data/nat/cast.lean
+++ src/data/nat/cast.lean
@@ -3,6 +3,7 @@ Copyright (c) 2014 Mario Carneiro. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Mario Carneiro
 -/
+import algebra.order.field
 import data.nat.basic
 import data.nat.cast.defs
 import algebra.group.pi

f278f387000532ba92762e2c7f3bfb0c80e7522d
Jujian Zhang <jujian.zhang1998@outlook.com>
2022-06-22 16:36:52 +0100

    add doc

--- src/algebraic_geometry/closed_immersion.lean
+++ src/algebraic_geometry/closed_immersion.lean
@@ -1,3 +1,8 @@
+/-
+Copyright (c) 2022 Jujian Zhang. All rights reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
+Authors: Jujian Zhang
+-/
 import algebraic_geometry.presheafed_space
 
 noncomputable theory
@@ -10,20 +15,22 @@ open category_theory.limits
 universes v u
 
 variables {C : Type u} [category.{v} C]
-
-class PresheafedSpace.is_close_immersion {X Y : PresheafedSpace C} (f : X ⟶ Y) :=
-(base_close : closed_embedding f.base)
+/--
+Closed immersion between presheafed spaces.
+-/
+class PresheafedSpace.is_closed_immersion {X Y : PresheafedSpace C} (f : X ⟶ Y) :=
+(base_closed : closed_embedding f.base)
 (continuous_inverse : continuous $ set.range_splitting f.base)
 (c_epi : ∀ U : opens X, epi f.c)
 
-def PresheafedSpace.is_close_immersion.homeomorph {X Y : PresheafedSpace C} (f : X ⟶ Y)
-  [hf: PresheafedSpace.is_close_immersion f] :
+def PresheafedSpace.is_closed_immersion.homeomorph {X Y : PresheafedSpace C} (f : X ⟶ Y)
+  [hf: PresheafedSpace.is_closed_immersion f] :
   X ≃ₜ set.range f.base :=
 { to_fun := set.range_factorization f.base,
   inv_fun := set.range_splitting f.base,
-  left_inv := set.right_inverse_range_splitting hf.base_close.inj,
+  left_inv := set.right_inverse_range_splitting hf.base_closed.inj,
   right_inv := set.left_inverse_range_splitting f,
   continuous_to_fun := by simp only [set.range_factorization]; continuity,
-  continuous_inv_fun := PresheafedSpace.is_close_immersion.continuous_inverse }
+  continuous_inv_fun := hf.continuous_inverse }
 
 end algebraic_geometry

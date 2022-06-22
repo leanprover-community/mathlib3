bd4574ceaa9a0b1903904998684f81f7f2d441c0
Yury G. Kudryashov <urkud@urkud.name>
2022-06-22 10:17:28 -0400

    Golf

--- src/order/filter/basic.lean
+++ src/order/filter/basic.lean
@@ -1441,29 +1441,18 @@ h.mono $ λ x, mt
 h.inter h'.compl
 
 lemma eventually_le.mul_le_mul [ordered_semiring β] {l : filter α} {f₁ f₂ g₁ g₂ : α → β}
-  (hf : f₁ ≤ᶠ[l] f₂) (hg : g₁ ≤ᶠ[l] g₂) (hg' : 0 ≤ᶠ[l] g₁) (hf' : 0 ≤ᶠ[l] f₂) :
+  (hf : f₁ ≤ᶠ[l] f₂) (hg : g₁ ≤ᶠ[l] g₂) (hg₀ : 0 ≤ᶠ[l] g₁) (hf₀ : 0 ≤ᶠ[l] f₂) :
   f₁ * g₁ ≤ᶠ[l] f₂ * g₂ :=
-begin
-  filter_upwards [hf, hg, hf', hg'] with x hfle hgle hfzero hgzero,
-  exact mul_le_mul hfle hgle hgzero hfzero,
-end
+by filter_upwards [hf, hg, hg₀, hf₀] with x using mul_le_mul
 
 lemma eventually_le.mul_nonneg [ordered_semiring β] {l : filter α} {f g : α → β}
   (hf : 0 ≤ᶠ[l] f) (hg : 0 ≤ᶠ[l] g) :
   0 ≤ᶠ[l] f * g :=
-begin
-  rw ← zero_mul (0 : α → β),
-  exact eventually_le.mul_le_mul hf hg (eventually_le.refl _ _) hf,
-end
+by filter_upwards [hf, hg] with x using mul_nonneg
 
-lemma eventually_le_iff_sub_nonneg [ordered_ring β] {l : filter α} {f g : α → β} :
-  f ≤ᶠ[l] g ↔ 0 ≤ᶠ[l] g - f :=
-begin
-  refine ⟨λ h, _, λ h, _⟩;
-  filter_upwards [h] with x hx,
-  { exact sub_nonneg_of_le hx },
-  { exact sub_nonneg.1 hx }
-end
+lemma eventually_sub_nonneg [ordered_ring β] {l : filter α} {f g : α → β} :
+  0 ≤ᶠ[l] g - f ↔ f ≤ᶠ[l] g :=
+eventually_congr $ eventually_of_forall $ λ x, sub_nonneg
 
 lemma join_le {f : filter (filter α)} {l : filter α} (h : ∀ᶠ m in f, m ≤ l) : join f ≤ l :=
 λ s hs, h.mono $ λ m hm, hm hs

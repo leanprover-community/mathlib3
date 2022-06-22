dd77774cc25994ca0a2cb5a9549956224d0ba248
Vierkantor <vierkantor@vierkantor.com>
2022-06-22 10:38:43 +0000

    Add postponed import

--- src/data/rat/order.lean
+++ src/data/rat/order.lean
@@ -4,7 +4,7 @@ Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Johannes Hölzl, Mario Carneiro
 -/
 import algebra.order.field
-import data.rat.defs
+import data.rat.basic
 
 /-!
 # Order for Rational Numbers

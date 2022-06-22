7f2be2a122b42d57a2c66e987c7e34d2c2ec4fc3
Julian Külshammer <julian.kuelshammer@math.uu.se>
2022-06-22 10:01:41 +0200

    changed the proof a bit

--- src/combinatorics/catalan.lean
+++ src/combinatorics/catalan.lean
@@ -6,6 +6,7 @@ Authors: Julian Kuelshammer
 import data.nat.choose.central
 import algebra.big_operators.fin
 import tactic.field_simp
+import tactic.linear_combination
 
 /-!
 # Catalan numbers
@@ -18,12 +19,11 @@ triangulations of convex polygons.
 
 * `catalan n`: the `n`th Catalan number, defined recursively as
                `catalan (n + 1) = ∑ i : fin n.succ, catalan i * catalan (n - i)`.
-* `catalan' n`: the `n`th Catalan number, defined explicitly as
-               `catalan' n = 1 / (n + 1) * nat.central_binom n`.
 
 ## Main results
 
-* `catalan_defs_agree`: The fact that the two definitions both give the same sequence.
+* `catalan_eq_central_binom_div `: The explicit formula for the Catalan number using the central
+  binomial coefficient, `catalan n = nat.central_binom n / (n + 1)`.
 
 ## Implementation details
 
@@ -52,21 +52,14 @@ def catalan : ℕ → ℕ
 lemma catalan_succ (n : ℕ) : catalan (n + 1) = ∑ i : fin n.succ, catalan i * catalan (n - i) :=
 by rw catalan
 
-/-- The explicit definition of the sequence of Catalan numbers via the central binomial
-coefficient.-/
-def catalan' (n : ℕ) :  ℚ :=
-1 / (n + 1) * nat.central_binom n
-
-@[simp] lemma catalan'_zero : catalan' 0 = 1 :=
-begin rw catalan', simp, end
-
 /-- A helper sequence that can be used to prove the equality of the recursive and the explicit
 definition using a telescoping sum argument. -/
 private def gosper_catalan (n j : ℕ) : ℚ :=
 nat.central_binom j * nat.central_binom (n - j) * (2 * j - n) / (2 * n * (n + 1))
 
-lemma gosper_trick {n i : ℕ} (h : i ≤ n) :
-gosper_catalan (n+1) (i+1) - gosper_catalan (n+1) i = catalan' i * catalan' (n - i) :=
+private lemma gosper_trick {n i : ℕ} (h : i ≤ n) :
+gosper_catalan (n+1) (i+1) - gosper_catalan (n+1) i =
+nat.central_binom i / (i + 1) * nat.central_binom (n - i) / (n - i + 1) :=
 begin
   have : (n:ℚ) + 1 ≠ 0 := by exact_mod_cast n.succ_ne_zero,
   have : (n:ℚ) + 1 + 1 ≠ 0 := by exact_mod_cast (n + 1).succ_ne_zero,
@@ -77,49 +70,54 @@ begin
   have h₂ : ((n:ℚ) - i + 1) * (n - i + 1).central_binom
     = 2 * (2 * (n - i) + 1) * (n - i).central_binom,
   { exact_mod_cast nat.succ_mul_central_binom_succ (n - i) },
-  simp only [gosper_catalan, catalan'],
+  simp only [gosper_catalan],
   push_cast,
   field_simp,
-  have H : n + 1 - i = n - i + 1 := sorry,
-  rw H,
+  rw (nat.succ_sub h),
   linear_combination
     (2:ℚ) * (n - i).central_binom * (i + 1 - (n - i)) * (n + 1) * (n + 2) * ((n - i) + 1) * h₁
     - 2 * i.central_binom * (n + 1) * (n + 2) * (i - (n - i) - 1) * (i + 1) * h₂,
 end
 
 lemma gosper_catalan_sub_eq_catalan' (n : ℕ) :
-  gosper_catalan (n + 1) (n + 1) - gosper_catalan (n + 1) 0 = catalan' (n + 1) :=
+  gosper_catalan (n + 1) (n + 1) - gosper_catalan (n + 1) 0 = nat.central_binom (n + 1) / (n + 2) :=
 begin
-simp only [gosper_catalan, catalan', nat.cast_succ, one_div, tsub_self, nat.central_binom_zero,
-  nat.cast_one, mul_one, tsub_zero, one_mul, nat.cast_zero, mul_zero, zero_sub, neg_add_rev,
-  div_eq_mul_inv],
-rw [mul_assoc, mul_assoc, mul_assoc, ← mul_sub, mul_comm],
+simp only [gosper_catalan, tsub_self, nat.central_binom_zero, nat.cast_one, mul_one, nat.cast_add,
+  tsub_zero, one_mul, nat.cast_zero, mul_zero, zero_sub, neg_add_rev, div_sub_div_same, ← mul_sub,
+  ← mul_div],
 congr,
-nth_rewrite 1 ← one_mul (n + 1 : ℚ),
-rw [← neg_add_rev, neg_mul, sub_neg_eq_add, ← sub_mul],
-norm_num,
-rw [← mul_add, ← add_mul, ← two_mul, mul_comm _ (1/2 : ℚ), ← mul_assoc, mul_comm (n + 1 + 1 : ℚ)⁻¹],
-simp only [← mul_assoc],
-nth_rewrite 1 ← one_mul (n + 1 + 1 : ℚ)⁻¹,
-congr,
-simp only [one_div, inv_mul_cancel_right₀, ne.def, bit0_eq_zero, one_ne_zero, not_false_iff],
-apply mul_inv_cancel,
-exact_mod_cast n.succ_ne_zero,
+rw [← neg_add, sub_neg_eq_add, add_comm (1 : ℚ) _, sub_add_cancel, ← div_div, div_self,
+    inv_eq_one_div, add_assoc],
+{ refl },
+{ apply mul_ne_zero,
+  { exact two_ne_zero },
+  { exact_mod_cast n.succ_ne_zero } },
+end
+
+lemma s (a b c : ℕ) (h : (a / b : ℚ) = c) : a / b = c :=
+begin
+have h' : b ∣ a,
+{ use c,
+  rw ← h, sorry },
+exact_mod_cast h,
 end
 
 theorem catalan_eq_central_binom_div (n : ℕ) :
-  catalan n = nat.central_binom n / (n + 1) :=
+  (catalan n : ℚ) = nat.central_binom n / (n + 1) :=
 begin
   induction n using nat.case_strong_induction_on with d hd,
   { simp, },
   { simp_rw [catalan_succ, nat.cast_sum, nat.cast_mul],
-    transitivity ∑ i : fin d.succ, catalan' i * catalan' (d - i),
+    transitivity (∑ i : fin d.succ, (nat.central_binom i / (i + 1)) * (nat.central_binom (d - i) / (d - i + 1)) : ℚ),
+    refine sum_congr rfl (λ i _, _),
+    push_cast,
+    rw [←hd _ i.is_le],
+    rw [←hd _ tsub_le_self],
     { transitivity ∑ i : fin d.succ, (gosper_catalan (d + 1) (i + 1) - gosper_catalan (d + 1) i),
       { rw [← sum_range (λi, gosper_catalan (d + 1) (i + 1) - gosper_catalan (d + 1) i),
             sum_range_sub, nat.succ_eq_add_one],
         exact (gosper_catalan_sub_eq_catalan' d).symm, },
       refine sum_congr rfl (λ i _, _),
       exact gosper_trick d i (i.is_le), },
-    refine sum_congr rfl (λ i _, _),
-    rw [←hd _ i.is_le, ←hd _ tsub_le_self], },
+     },
 end

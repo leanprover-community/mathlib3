ce8ed831ad1e8ed1d97aee0f6fc5d3b5ddf12700
Markus Himmel <markus@himmel-villmar.de>
2022-06-24 20:08:09 +0200

    Quite possibly the worst proof I have ever written

--- src/category_theory/abelian/freyd_mitchell_special.lean
+++ src/category_theory/abelian/freyd_mitchell_special.lean
@@ -6,6 +6,9 @@ Authors: Markus Himmel
 import category_theory.abelian.fully_abelian
 import category_theory.abelian.projective
 import category_theory.preadditive.generator
+import category_theory.preadditive.yoneda
+import category_theory.limits.constructions.epi_mono
+import algebra.category.Module.projective
 
 /-!
 # A special case of the Freyd-Mitchell embedding theorem
@@ -20,7 +23,18 @@ open category_theory
 open category_theory.limits
 open opposite
 
-universes w v u
+universes w v u v₁ v₂ u₁ u₂
+
+namespace category_theory.functor
+variables {C : Type u₁} [category.{v₁} C] {D : Type u₂} [category.{v₂} D] (F : C ⥤ D)
+
+def full_of_surjective (h : ∀ (X Y : C) (f : F.obj X ⟶ F.obj Y), ∃ f', F.map f' = f) : full F :=
+begin
+  choose f h using h,
+  exact ⟨f, h⟩
+end
+
+end category_theory.functor
 
 namespace category_theory.abelian
 variables {C : Type u} [category.{v} C] [abelian C] [has_coproducts C]
@@ -61,18 +75,56 @@ begin
   apply epi_comp
 end
 
-instance : faithful (coyoneda.obj (op (refined_generator P hs F))) :=
+instance : faithful (preadditive_coyoneda_obj (op (refined_generator P hs F))) :=
 sorry
 
-instance : preserves_finite_limits (coyoneda.obj (op (refined_generator P hs F))) :=
+instance : preserves_finite_limits (preadditive_coyoneda_obj (op (refined_generator P hs F))) :=
 sorry
 
-instance : preserves_finite_colimits (coyoneda.obj (op (refined_generator P hs F))) :=
+instance : preserves_finite_colimits (preadditive_coyoneda_obj (op (refined_generator P hs F))) :=
 sorry
 
-instance : full (F ⋙ coyoneda.obj (op (refined_generator P hs F))) :=
+instance : full (F ⋙ preadditive_coyoneda_obj (op (refined_generator P hs F))) :=
 begin
-  suffices : ∀ (A B : D), function.surjective ((F ⋙ coyoneda.obj (op (refined_generator P hs F))).map : (A ⟶ B) → _)
+  let G := preadditive_coyoneda_obj (op (refined_generator P hs F)),
+  haveI : faithful G,
+  { dsimp [G], apply_instance },
+  let F' := F ⋙ G,
+  apply category_theory.functor.full_of_surjective,
+  rintros X Y (f : F'.obj X ⟶ F'.obj Y),
+  let l := from_refined P hs F X,
+  let R := End (op (refined_generator P hs F)),
+  haveI : projective (G.obj (refined_generator P hs F)),
+  { sorry, },
+  let t : End _ := projective.factor_thru
+    (G.map (from_refined P hs F X) ≫ f) (G.map (from_refined P hs F Y)),
+  dsimp [G] at t,
+  let t' : End (Module.of _ (End _)) := t,
+  let r : refined_generator P hs F ⟶ refined_generator P hs F := t'.to_fun (𝟙 _),
+  have h : G.map r = t',
+  { ext1,
+    dsimp,
+    have hx : x = ((x.op : End (op (refined_generator P hs F))) • (𝟙 _)) :=
+       (category.comp_id _).symm,
+    conv_rhs { rw [hx] },
+    simp only [linear_map.map_smulₛₗ, ring_hom.id_apply],
+    refl },
+  have hr : kernel.ι (from_refined P hs F X) ≫ r ≫ from_refined P hs F Y = 0,
+  { apply G.map_injective,
+    simp only [h, functor.map_comp, projective.factor_thru_comp, functor.map_zero],
+    rw [← category.assoc, ← G.map_comp, kernel.condition, G.map_zero, zero_comp] },
+  let hcoker : is_colimit (cokernel_cofork.of_π (from_refined P hs F X) (kernel.condition _) :
+    cokernel_cofork (kernel.ι (from_refined P hs F X))) :=
+    sorry,
+  let w := hcoker.desc (cokernel_cofork.of_π _ hr),
+  refine ⟨F.preimage w, _⟩,
+  rw ← cancel_epi (G.map (from_refined P hs F X)),
+  simp only [functor.comp_map, functor.image_preimage],
+  erw [←G.map_comp],
+  have hc := hcoker.fac (cokernel_cofork.of_π _ hr) walking_parallel_pair.one,
+  simp only [cofork.of_π_ι_app] at hc,
+  erw [hc, functor.map_comp, h],
+  exact projective.factor_thru_comp _ _
 end
 
 end category_theory.abelian

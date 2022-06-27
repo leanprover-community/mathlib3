f499f81b48ad3ac174df3963f986fb25d7dd58ce
Jujian Zhang <jujian.zhang1998@outlook.com>
2022-06-27 23:29:40 +0100

    changed to correct definition by Andrew Yang

--- src/algebraic_geometry/closed_immersion.lean
+++ src/algebraic_geometry/closed_immersion.lean
@@ -1,7 +1,7 @@
 /-
 Copyright (c) 2022 Jujian Zhang. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
-Authors: Jujian Zhang
+Authors: Andrew Yang, Jujian Zhang
 -/
 import algebraic_geometry.presheafed_space
 import algebraic_geometry.sheafed_space
@@ -28,51 +28,38 @@ universes v u
 variables {C : Type u} [category.{v} C]
 
 /--
-Closed immersion between presheafed spaces.
+Closed immersion between presheafed spaces are morphisms `f : X ⟶ Y`, such that
+* the underlying continuous map is a closed embedding;
+* the inverse `set.range f → X` is continuous and
+* `𝓞_Y ⟶ f_* 𝓞_X` is epi
 -/
-class PresheafedSpace.is_closed_immersion {X Y : PresheafedSpace C} (f : X ⟶ Y) : Prop :=
+class PresheafedSpace.is_closed_immersion [concrete_category C] [has_colimits C]
+  {X Y : PresheafedSpace C} (f : X ⟶ Y) : Prop :=
 (base_closed : closed_embedding f.base)
-(continuous_inverse : continuous $ set.range_splitting f.base)
-(c_epi : epi f.c)
+(c_epi : ∀ y : Y, function.surjective $ (forget C).map ((Top.presheaf.stalk_functor _ y).map f.c))
 
-/--
-If `f : X ⟶ Y` is a closed immersion between presheafed spaces, then `X` as a topological space is
-homeomorphic to the range of `f` endowed with the subspace topology.
--/
-def PresheafedSpace.is_closed_immersion.homeomorph {X Y : PresheafedSpace C} (f : X ⟶ Y)
-  [hf: PresheafedSpace.is_closed_immersion f] :
-  X ≃ₜ set.range f.base :=
-{ to_fun := set.range_factorization f.base,
-  inv_fun := set.range_splitting f.base,
-  left_inv := set.right_inverse_range_splitting hf.base_closed.inj,
-  right_inv := set.left_inverse_range_splitting f,
-  continuous_to_fun := by simp only [set.range_factorization]; continuity,
-  continuous_inv_fun := hf.continuous_inverse }
-
-instance PresheafedSpace.is_closed_immersion.id {X : PresheafedSpace C} :
+instance PresheafedSpace.is_closed_immersion.id [concrete_category C] [has_colimits C]
+  {X : PresheafedSpace C} :
   PresheafedSpace.is_closed_immersion (𝟙 X) :=
 { base_closed := closed_embedding_id,
-  continuous_inverse := (⟨λ s os, begin
-    rw is_open_induced_iff,
-    refine ⟨s, os, _⟩,
-    ext x,
-    split;
-    intros hx;
-    rw [set.mem_preimage] at hx ⊢,
-    { have := set.apply_range_splitting (𝟙 (X : Top.{v})) x,
-      simp only [Top.id_app] at this,
-      rwa this },
-    { have := set.apply_range_splitting (𝟙 (X : Top.{v})) x,
-      simp only [Top.id_app] at this,
-      rwa this at hx, },
-  end⟩ : continuous (set.range_splitting $ 𝟙 (X : Top.{v}))),
-  c_epi := infer_instance }
-
+  c_epi := λ x, begin
+    change function.surjective
+      ((forget C).map ((Top.presheaf.stalk_functor _ x).map (PresheafedSpace.id _).c)),
+    dunfold PresheafedSpace.id,
+    rw eq_to_hom_map,
+    generalize_proofs h1 h2,
+    intros y,
+    use (forget C).map (eq_to_hom h2.symm) y,
+    change ((forget C).map _ ≫ (forget C).map _) _ = y,
+    rw [←(forget C).map_comp, eq_to_hom_trans, eq_to_hom_refl, (forget C).map_id],
+    refl,
+  end }
 /--
 A morphism of sheafed spaces is a closed immersion iff it is a closed immersion as a morphism of
 presheafed spaces.
 -/
-abbreviation SheafedSpace.is_closed_immersion [has_products C]
+abbreviation SheafedSpace.is_closed_immersion [concrete_category C] [has_colimits C]
+  [has_products C]
   {X Y : SheafedSpace C} (f : X ⟶ Y) : Prop :=
 PresheafedSpace.is_closed_immersion f
 

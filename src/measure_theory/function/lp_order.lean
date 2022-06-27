0f557cd269fc458c7f7de788a44fb8f9c414714d
RemyDegenne <Remydegenne@gmail.com>
2022-06-27 12:37:57 +0200

    prove normed_lattice_add_comm_group for Lp

--- src/measure_theory/function/lp_order.lean
+++ src/measure_theory/function/lp_order.lean
@@ -17,8 +17,6 @@ import analysis.normed_space.lattice_ordered_group
 
 - move definitions of `Lp.pos_part` and `Lp.neg_part` to this file, and define them as
   `has_pos_part.pos` and `has_pos_part.neg` given by the lattice structure.
-- show that if `E` is a `normed_lattice_add_comm_group` then so is `Lp E p μ` for `1 ≤ p`. In
-  particular, this shows `order_closed_topology` for `Lp`.
 
 -/
 
@@ -55,8 +53,52 @@ begin
 end
 
 instance : ordered_add_comm_group (Lp E p μ) :=
-{ add_le_add_left := λ f g hfg f', add_le_add_left hfg f',
+{ add_le_add_left := λ f g, add_le_add_left,
   ..subtype.partial_order _, ..add_subgroup.to_add_comm_group _}
+
+instance [second_countable_topology E] [measurable_space E] [borel_space E]
+  [has_measurable_sup₂ E] [has_measurable_inf₂ E] :
+  lattice (Lp E p μ) :=
+subtype.lattice (λ f g hf hg, by { rw mem_Lp_iff_mem_ℒp at *,
+    exact (mem_ℒp_congr_ae (ae_eq_fun.coe_fn_sup _ _)).mpr (hf.sup hg), })
+  (λ f g hf hg, by { rw mem_Lp_iff_mem_ℒp at *,
+    exact (mem_ℒp_congr_ae (ae_eq_fun.coe_fn_inf _ _)).mpr (hf.inf hg),})
+
+lemma ae_eq_fun.coe_fn_abs [second_countable_topology E] [measurable_space E] [borel_space E]
+  [has_measurable_sup₂ E]
+  (f : α →ₘ[μ] E) :
+    ⇑|f| =ᵐ[μ] λ x, |f x| :=
+begin
+  simp_rw abs_eq_sup_neg,
+  refine (ae_eq_fun.coe_fn_sup _ _).trans _,
+  filter_upwards [ae_eq_fun.coe_fn_neg f] with x hx,
+  rw [hx, pi.neg_apply],
+end
+
+lemma coe_fn_sup [measurable_space E] [second_countable_topology E]
+  [borel_space E] [has_measurable_sup₂ E] (f g : Lp E p μ) : ⇑(f ⊔ g) =ᵐ[μ] ⇑f ⊔ ⇑g :=
+ae_eq_fun.coe_fn_sup _ _
+
+lemma coe_fn_inf [measurable_space E] [second_countable_topology E]
+  [borel_space E] [has_measurable_sup₂ E] (f g : Lp E p μ) : ⇑(f ⊓ g) =ᵐ[μ] ⇑f ⊓ ⇑g :=
+ae_eq_fun.coe_fn_inf _ _
+
+lemma coe_fn_abs [measurable_space E] [second_countable_topology E]
+  [borel_space E] [has_measurable_sup₂ E] (f : Lp E p μ) : ⇑|f| =ᵐ[μ] λ x, |f x| :=
+ae_eq_fun.coe_fn_abs _
+
+noncomputable
+instance [second_countable_topology E] [measurable_space E] [borel_space E]
+  [has_measurable_sup₂ E] [has_measurable_inf₂ E] [fact (1 ≤ p)] :
+  normed_lattice_add_comm_group (Lp E p μ) :=
+{ add_le_add_left := λ f g, add_le_add_left,
+  solid := λ f g hfg, by { rw ← coe_fn_le at hfg, simp_rw Lp.norm_def,
+    rw ennreal.to_real_le_to_real (Lp.snorm_ne_top f) (Lp.snorm_ne_top g),
+    refine snorm_mono_ae _,
+    filter_upwards [hfg, Lp.coe_fn_abs f, Lp.coe_fn_abs g] with x hx hxf hxg,
+    rw [hxf, hxg] at hx,
+    exact solid hx, },
+  ..Lp.lattice, ..Lp.normed_group, }
 
 end order
 

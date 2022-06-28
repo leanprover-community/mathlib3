55dca45618f6dd61fc690fde53255ea41e36e452
Eric Rodriguez <ericrboidi@gmail.com>
2022-06-28 02:24:55 +0100

    feat(topology/algebra/order): ⁻¹ continuous for linear ordered fields

--- src/topology/algebra/order/basic.lean
+++ src/topology/algebra/order/basic.lean
@@ -7,6 +7,7 @@ import algebra.group_with_zero.power
 import data.set.intervals.pi
 import order.filter.interval
 import topology.algebra.group
+import topology.algebra.group_with_zero
 import tactic.linarith
 import tactic.tfae
 
@@ -2070,6 +2071,43 @@ begin
       exact tendsto_const_nhds },
     { exact h.2.symm ▸ tendsto_const_mul_zpow_at_top_zero h.1} }
 end
+
+instance linear_ordered_field.has_continuous_inv₀ : has_continuous_inv₀ α :=
+{ continuous_at_inv₀ :=
+  begin
+    suffices : ∀ {x : K}, 0 < x → continuous_at has_inv.inv x,
+    { intros x hx,
+      cases hx.symm.lt_or_lt,
+      { exact this h },
+      convert (this $ neg_pos.mpr h).neg.comp continuous_neg.continuous_at,
+      ext,
+      simp [neg_inv] },
+    intros t ht,
+    rw [continuous_at,
+        (nhds_basis_Ioo_pos t).tendsto_iff $ nhds_basis_Ioo_pos_of_pos $ inv_pos.2 ht],
+    rintros ε ⟨hε : ε > 0, hεt : ε ≤ t⁻¹⟩,
+    refine ⟨min (t ^ 2 * ε / 2) (t / 2),
+            lt_min (half_pos $ mul_pos (by nlinarith) hε) $ by linarith, _⟩,
+    rintro x h,
+    obtain ⟨hδx, hxδ⟩ := id h,
+    rw [set.mem_Ioo, ←sub_lt_iff_lt_add', sub_lt, ←abs_sub_lt_iff] at h ⊢,
+    have hx : t / 2 < x,
+    { rw [sub_lt, lt_min_iff] at hδx,
+      nlinarith },
+    have hx' : 0 < x := (half_pos ht).trans hx,
+    have aux : 0 < 2 / t ^ 2 := div_pos zero_lt_two (sq_pos_of_pos ht),
+    rw [inv_sub_inv ht.ne' hx'.ne', abs_div, div_eq_mul_inv],
+    suffices : |t * x|⁻¹ < 2 / t ^ 2,
+    { rw [←abs_neg, neg_sub],
+      refine (mul_lt_mul'' h this (abs_nonneg _) $ inv_nonneg.mpr $ abs_nonneg _).trans_le _,
+      rw [mul_comm, mul_min_of_nonneg _ _ aux.le],
+      apply min_le_of_left_le,
+      rw [←mul_div, ←mul_assoc, div_mul_cancel _ (sq_pos_of_pos ht).ne',
+          mul_div_cancel' ε two_ne_zero] },
+      refine inv_lt_of_inv_lt aux _,
+      rw [inv_div, abs_of_pos $ mul_pos ht hx', sq, ←mul_div_assoc'],
+      exact mul_lt_mul_of_pos_left hx ht,
+  end }
 
 end linear_ordered_field
 

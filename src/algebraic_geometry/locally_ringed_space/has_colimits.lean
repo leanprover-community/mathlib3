bd5177941ed4a540f5d4862e5ceea0bde3d0ca99
Jakob von Raumer <jakob@von-raumer.de>
2022-06-29 16:43:27 +0200

    some more fixes

--- src/algebraic_geometry/locally_ringed_space/has_colimits.lean
+++ src/algebraic_geometry/locally_ringed_space/has_colimits.lean
@@ -120,7 +120,7 @@ begin
   rw ← preserves_coequalizer.iso_hom at this,
   erw SheafedSpace.congr_app this.symm (op U),
   rw [PresheafedSpace.comp_c_app,
-    ← PresheafedSpace.colimit_presheaf_obj_iso_componentwise_limit_hom_π.{0 0}],
+    ← PresheafedSpace.colimit_presheaf_obj_iso_componentwise_limit_hom_π],
   apply_instance
 end
 

55115eaa5ca9295753c62e4a75df3e6982599072
Violeta Hernández <vi.hdz.p@gmail.com>
2022-06-29 01:06:59 -0500

    fix

--- src/ring_theory/noetherian.lean
+++ src/ring_theory/noetherian.lean
@@ -517,7 +517,7 @@ variables {R M}
 theorem set_has_maximal_iff_noetherian :
   (∀ a : set $ submodule R M, a.nonempty → ∃ M' ∈ a, ∀ I ∈ a, M' ≤ I → I = M') ↔
   is_noetherian R M :=
-by rw [is_noetherian_iff_well_founded, well_founded.well_founded_iff_has_max']
+by rw [is_noetherian_iff_well_founded, is_well_founded.well_founded_iff_has_max']
 
 /-- A module is Noetherian iff every increasing chain of submodules stabilizes. -/
 theorem monotone_stabilizes_iff_noetherian :

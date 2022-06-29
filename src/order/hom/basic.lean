01a4097c1055f06da2026eafa037ea8bf235769f
Violeta Hernández <vi.hdz.p@gmail.com>
2022-06-29 11:45:39 -0500

    Update basic.lean

--- src/order/hom/basic.lean
+++ src/order/hom/basic.lean
@@ -414,7 +414,7 @@ f.lt_embedding.acc a
 
 protected theorem well_founded_lt {α β} [preorder α] [linear_order β] [well_founded_lt β]
   (f : α ↪o β) : well_founded_lt α :=
-⟨⟨f.lt_embedding.is_well_order.wf⟩⟩
+⟨by { haveI := well_founded_lt.is_well_order β, exact f.lt_embedding.is_well_order.wf }⟩
 
 /-- An order embedding is also an order embedding between dual orders. -/
 protected def dual : αᵒᵈ ↪o βᵒᵈ :=

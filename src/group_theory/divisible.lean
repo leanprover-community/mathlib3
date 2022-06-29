65e1b9485ff5e3ec758da3a7b21e6f3c8c2e4b48
Jujian Zhang <jujian.zhang1998@outlook.com>
2022-06-30 00:34:49 +0100

    apply changes from @eric-wieser

--- src/group_theory/divisible.lean
+++ src/group_theory/divisible.lean
@@ -9,15 +9,15 @@ import data.real.basic
 /-!
 # Divisible Group
 
-An abelian group `A` is said to be divisible iff `nA = A` for all `n ≠ 0`.
+An abelian group `A` is said to be divisible iff for all `n ≠ 0` `y ∈ A`, there is an `x ∈ A`
+such that `n • x = y`.
 
 ## Main results
 
-* `add_comm_group.divisbible_of_linear_solvable` : If in a group `A`, `n • x = y` is solveable
-  whenever `n ≠ 0`, then `A` is divisible.
+* `add_comm_group.divisible_iff_smul_top_eq_top` : `A` is divisible iff `n • A = A` for all `n ≠ 0`.
 * `add_comm_group.divisble_rat` : `ℚ` is a divisible group.
 * `add_comm_group.divisible_real` : `ℝ` is a divisible group.
-* `add_comm_group.divisible_of_product_divisible` : Any product of divisble group is divisible.
+* `add_comm_group.divisible_pi` : Any product of divisble group is divisible.
 
 TODO: Show that divisibility implies injectivity in the category of `AddCommGroup`.
 -/
@@ -33,44 +33,35 @@ variables (A : Type*) [add_comm_group A]
 A divisible group `A` is an abelian group such that `nA = A` for all `n ≠ 0`.
 -/
 class divisible : Prop :=
-(div : ∀ (n : ℤ), n ≠ 0 → n • (⊤ : add_subgroup A) = ⊤)
+(div : ∀ (n : ℤ), n ≠ 0 → function.surjective ((•) n : A → A))
 
-lemma divisible_of_elemement_divisible
-  (sol : ∀ (n : ℤ) (x : A), n ≠ 0 → ∃ (y : A), n • y = x) :
-  divisible A :=
-{ div := λ n hn, add_subgroup.ext $ λ q,
-  { mp := λ _, trivial,
-    mpr := λ _, ⟨(sol n q hn).some, ⟨trivial, (sol n q hn).some_spec⟩⟩ } }
 
-lemma divisible_iff_element_divisible :
-  divisible A ↔ ∀ (n : ℤ) (x : A), n ≠ 0 → ∃ (y : A), n • y = x :=
-⟨λ d n x hn, begin
-  rcases show x ∈ (n • ⊤ : add_subgroup A),
-  { convert (add_subgroup.mem_top _ : x ∈ ⊤),
-    rwa d.div, } with ⟨a', _, ha'⟩,
-  exact ⟨a', ha'⟩,
-end, add_comm_group.divisible_of_elemement_divisible _⟩
+lemma divisible_iff_smul_top_eq_top :
+  divisible A ↔ ∀ (n : ℤ), n ≠ 0 → n • (⊤ : add_subgroup A) = ⊤ :=
+{ mp := λ d n hn,  (add_subgroup.eq_top_iff' (n • ⊤)).mpr $
+     λ x, ⟨(@@divisible.div _ d n hn x).some, trivial, (@@divisible.div _ d n hn x).some_spec⟩,
+  mpr := λ h, ⟨λ n hn x, begin
+    rcases show x ∈ n • (⊤ : add_subgroup A), by rw h n hn; trivial with ⟨x', _, hx⟩,
+    exact ⟨x', hx⟩,
+  end⟩ }
 
 /-- ℚ is a divisible group. -/
 instance divisible_rat : divisible ℚ :=
-divisible_of_elemement_divisible _ $
-  λ n x hn, ⟨x/n, by rw [zsmul_eq_mul, mul_div_cancel']; exact_mod_cast hn⟩
+⟨λ n hn x, ⟨x/n, by rw [zsmul_eq_mul, mul_div_cancel']; exact_mod_cast hn⟩⟩
 
 /-- ℝ is a divisible group. -/
 instance divisible_real : divisible ℝ :=
-divisible_of_elemement_divisible _ $
-  λ n x hn, ⟨x/n, by rw [zsmul_eq_mul, mul_div_cancel']; exact_mod_cast hn⟩
+⟨λ n hn x, ⟨x/n, by rw [zsmul_eq_mul, mul_div_cancel']; exact_mod_cast hn⟩⟩
 
-section product
+section pi
 
 variables {ι : Type*} (B : ι → Type*) [Π i, add_comm_group (B i)] [Π i, divisible (B i)]
 
 /-- Any product of divisible group is divisible.-/
-instance divisible_of_product_divisible : divisible (Π i, B i) :=
-add_comm_group.divisible_of_elemement_divisible _ $
-  λ n x hn, ⟨λ i, ((divisible_iff_element_divisible (B i)).mp infer_instance n (x i) hn).some,
-    funext $ λ i, ((divisible_iff_element_divisible (B i)).mp infer_instance n (x i) hn).some_spec⟩
+instance divisible_pi : divisible (Π i, B i) :=
+⟨λ n hn x, ⟨λ i, (divisible.div n hn (x i)).some,
+  funext $ λ i, (divisible.div n hn (x i)).some_spec⟩⟩
 
-end product
+end pi
 
 end add_comm_group

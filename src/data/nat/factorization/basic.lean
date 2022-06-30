1f61030d908a94f64a4efecdc0610c41056befb3
Stuart Presnell <stuart.presnell@gmail.com>
2022-06-30 16:29:50 +0100

    Delete imports

--- src/data/nat/factorization/basic.lean
+++ src/data/nat/factorization/basic.lean
@@ -6,8 +6,6 @@ Authors: Stuart Presnell
 import data.nat.prime
 import data.finsupp.multiset
 import algebra.big_operators.finsupp
-import tactic.linarith
-import tactic.interval_cases
 import data.nat.log
 
 /-!

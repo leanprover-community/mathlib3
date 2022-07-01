0d1ec86769bc253d5458d2db919994e572b1755b
Heather Macbeth <25316162+hrmacbeth@users.noreply.github.com>
2022-07-01 15:55:13 -0400

    sorry-free

--- src/algebra/cubic_discriminant.lean
+++ src/algebra/cubic_discriminant.lean
@@ -38,79 +38,88 @@ open_locale polynomial big_operators
 
 variables {R S F K : Type*}
 
-variables {N : ℕ}
-
 namespace vec_polynomial
 
-/- Convert a vector to a polynomial. -/
-def to_poly {R : Type*} [semiring R] {N : ℕ} (P : fin N → R) : R[X] := ∑ i : fin N, monomial i (P i)
+section basic
+variables [semiring R] {N : ℕ}
 
-@[simp] lemma degree_lt_equiv_symm_apply [semiring R] (P : fin N → R) :
+/- Convert a vector to a polynomial. This construction allows one to work with the module of
+polynomials of a guaranteed maximal degree, known in mathlib as `degree_lt R`, while retaining easy
+access to the coefficients (which for an element `P : ↥(degree_lt R N)` would be accessed as
+`(P : R[X]).coeff`). -/
+def to_poly : (fin N → R) →+ R[X] :=
+(add_submonoid_class.subtype (degree_lt R N)).comp (degree_lt_equiv R N).symm
+
+lemma to_poly_apply (P : fin N → R) : to_poly P =  ∑ i : fin N, monomial i (P i) := rfl
+
+@[simp] lemma degree_lt_equiv_symm_apply (P : fin N → R) :
   ((degree_lt_equiv R N).to_equiv.symm P : R[X]) = to_poly P :=
 rfl
 
-lemma to_poly_mem_degree_lt [semiring R] (P : fin N → R) : to_poly P ∈ degree_lt R N :=
+lemma to_poly_mem_degree_lt (P : fin N → R) : to_poly P ∈ degree_lt R N :=
 ((degree_lt_equiv R N).symm P).prop
 
-section basic
-variables [semiring R]
+variables (R N)
+
+@[simp] lemma to_poly_injective : function.injective (to_poly : (fin N → R) → R[X]) :=
+(submodule.injective_subtype (degree_lt R N)).comp (degree_lt_equiv R N).symm.injective
+
+@[simp] lemma to_poly_zero : to_poly (0 : fin N → R) = 0 := (@to_poly R _ _).map_zero
+
+variables {R N}
+
+@[simp] lemma to_poly_eq_iff (P Q : fin N → R) : to_poly P = to_poly Q ↔ P = Q :=
+(to_poly_injective R N).eq_iff
+
+@[simp] lemma to_poly_eq_zero_iff (P : fin N → R) : to_poly P = 0 ↔ P = 0 :=
+by rw [← to_poly_zero, to_poly_eq_iff]
 
 /-! ### Coefficients -/
 
 section coeff
 variables {P : fin N → R}
 
-lemma coeff_gt_three (P : fin N.succ → R) (n : ℕ) (hn : N < n) :
-  (to_poly P).coeff n = 0 :=
+lemma coeff_ge_four (P : fin N → R) (n : ℕ) (hn : N ≤ n) : (to_poly P).coeff n = 0 :=
 (submodule.mem_infi _).mp ((submodule.mem_infi _).mp (to_poly_mem_degree_lt P) n : _ ∈ _) hn
 
-@[simp] lemma coeff_le_three (P : fin N → R) (n : fin N) :
-  (to_poly P).coeff n = P n :=
-begin
-  rw ← degree_lt_equiv_symm_apply,
-end
-
-@[simp] lemma to_poly_injective (P Q : fin N → R) : to_poly P = to_poly Q ↔ P = Q :=
-⟨λ h, by { ext i, rw [← coeff_le_three P, ← coeff_le_three Q, h] }, congr_arg _⟩
-
-@[simp] lemma of_zero (ha : P = 0) : to_poly P = 0 := by simp [ha, ← degree_lt_equiv_symm_apply]
-
-@[simp] lemma zero : to_poly (0 : fin N → R) = 0 := of_zero rfl
-
-@[simp] lemma eq_zero_iff : (to_poly P) = 0 ↔ P = 0 := by rw [← zero, to_poly_injective]
+@[simp] lemma coeff_le_three (P : fin N → R) (n : fin N) : (to_poly P).coeff n = P n :=
+by simp [to_poly_apply, coeff_monomial, set_coe.ext_iff, finset.sum_ite_eq']
 
 end coeff
 
 /-! ### Degrees -/
 
 section degree
+
+lemma degree_lt (P : fin N → R) : (to_poly P).degree < N :=
+by simpa [mem_degree_lt] using to_poly_mem_degree_lt P
+
 variables (P : fin N.succ → R)
 
 lemma degree_le : (to_poly P).degree ≤ N :=
-(degree_le_iff_coeff_zero _ _).mpr (λ m hm, coeff_gt_three P m (by simpa using hm))
+(degree_le_iff_coeff_zero _ _).mpr (λ m hm, coeff_ge_four P m (by simpa using hm))
 
 lemma nat_degree_le : (to_poly P).nat_degree ≤ N :=
 begin
   by_cases hP : to_poly P = 0,
-  { have hP' : P = 0 := by simpa [eq_zero_iff] using hP,
-    simp [hP'] },
+  { simp [hP] },
   simpa [degree_eq_nat_degree hP] using degree_le P,
 end
 
 variables {P}
 
 lemma degree (ha : P N ≠ 0) : (to_poly P).degree = N :=
-begin
-  refine le_antisymm (degree_le P) _,
-  rw ← coeff_le_three P at ha,
-  have := le_degree_of_ne_zero ha,
-  convert this,
-  -- library_search
-end
+le_antisymm (degree_le P) $
+calc (N : with_bot ℕ) = _ : congr_arg some (fin.coe_coe_of_lt (nat.lt.base N)).symm
+... ≤ (to_poly P).degree : le_degree_of_ne_zero (by rwa ← coeff_le_three P at ha)
 
-lemma degree_of_zero (hP : P = 0) : (to_poly P).degree = ⊥ := by rwa [of_zero, degree_zero]
+lemma nat_degree (ha : P N ≠ 0) : (to_poly P).nat_degree = N :=
+le_antisymm (nat_degree_le P) $
+calc N = _ : (fin.coe_coe_of_lt (nat.lt.base N)).symm
+... ≤ _ : le_nat_degree_of_ne_zero (by rwa ← coeff_le_three P at ha)
 
-lemma leading_coeff (ha : P N ≠ 0) : (to_poly P).leading_coeff = P N := sorry --leading_coeff_cubic ha
+lemma leading_coeff (ha : P N ≠ 0) : (to_poly P).leading_coeff = P N :=
+by rw [leading_coeff, nat_degree ha, ← coeff_le_three P, fin.coe_coe_of_lt (nat.lt.base N)]
 
 end degree
 
@@ -118,10 +127,10 @@ end degree
 
 section map
 
-variables {P : fin N → R} [semiring S] {φ : R →+* S}
+variables [semiring S] {P : fin N → R} {φ : R →+* S}
 
 lemma map_to_poly : to_poly (φ ∘ P) = polynomial.map φ (to_poly P) :=
-by simp [to_poly, polynomial.map_sum]
+by simp [to_poly_apply, polynomial.map_sum]
 
 end map
 
@@ -134,13 +143,12 @@ open multiset
 /-! ### Roots over an extension -/
 
 section extension
+variables [comm_ring R] [comm_ring S] {N: ℕ} {P : fin N → R} {φ : R →+* S}
 
-variables {P : fin N → R} [comm_ring R] [comm_ring S] {φ : R →+* S}
-
-theorem mem_roots_iff [is_domain R] (h0 : (to_poly P) ≠ 0) (x : R) :
+theorem mem_roots_iff [is_domain R] (h0 : to_poly P ≠ 0) (x : R) :
   x ∈ (to_poly P).roots ↔ ∑ i, P i * x ^ (i:ℕ) = 0 :=
 begin
-  rw [mem_roots h0, is_root, to_poly, eval_finset_sum],
+  rw [mem_roots h0, is_root, to_poly_apply, eval_finset_sum],
   simp only [eval_monomial],
 end
 
@@ -148,34 +156,43 @@ theorem card_roots_le {P : fin N.succ → R} [is_domain R] [decidable_eq R] :
   (to_poly P).roots.to_finset.card ≤ N :=
 begin
   apply (to_finset_card_le (to_poly P).roots).trans,
-  by_cases hP : (to_poly P) = 0,
-  { exact (card_roots' (to_poly P)).trans (by { rw [hP, nat_degree_zero], exact zero_le N }) },
-  { exact with_bot.coe_le_coe.1 ((card_roots hP).trans degree_le) },
+  by_cases hP : to_poly P = 0,
+  { simp [hP] },
+  { exact with_bot.coe_le_coe.1 ((card_roots hP).trans (degree_le P)) },
 end
 
 end extension
 
-variables {P : fin N.succ → F} [field F] [field K] {φ : F →+* K} {x y z : K}
-
 /-! ### Roots over a splitting field -/
 
 section split
+variables [field F] [field K] {N : ℕ} {P : fin N.succ → F} {φ : F →+* K} {x y z : K}
 
 theorem splits_iff_card_roots (ha : P N ≠ 0) :
   splits φ (to_poly P) ↔ (to_poly (φ ∘ P)).roots.card = N :=
 begin
   replace ha : (φ ∘ P) N ≠ 0 := (ring_hom.map_ne_zero φ).mpr ha,
   nth_rewrite_lhs 0 [← ring_hom.id_comp φ],
-  rw [← splits_map_iff, ← map_to_poly, splits_iff_card_roots,
-      ← ((degree_eq_iff_nat_degree_eq $ ne_zero_of_a_ne_zero ha).mp $ degree ha : _ = 3)]
+  rwa [← splits_map_iff, ← map_to_poly, splits_iff_card_roots, nat_degree],
 end
 
-lemma vieta {P : fin N.succ → F} (hP : splits φ (to_poly P)) (k : fin N.succ) :
-  φ (P k)
-  = (-1) ^ (N - k) * φ (P N) * (((to_poly (φ ∘ P)).roots.powerset_len (N - k)).map multiset.prod).sum :=
+-- in #14908
+theorem vieta {R} [comm_ring R] [is_domain R] (p : R[X])
+  (hroots : p.roots.card = p.nat_degree) (k : ℕ) (h : k ≤ p.nat_degree) :
+  p.coeff k = p.leading_coeff * (-1) ^ (p.nat_degree - k) *
+    ((p.roots.powerset_len (p.nat_degree - k)).map multiset.prod).sum :=
 sorry
-  -- (finset.univ.prod (λ (i : σ), ⇑polynomial.C (r i) + polynomial.X)).coeff k = (finset.powerset_len (fintype.card σ - k) finset.univ).sum (λ (t : finset σ), t.prod (λ (i : σ), r i))
 
+lemma to_poly_vieta {P : fin N.succ → F} (ha : P N ≠ 0) (hP : splits φ (to_poly P))
+  (k : fin N.succ) :
+  φ (P k)
+  = φ (P N) * (-1) ^ (N - k)
+    * (((to_poly (φ ∘ P)).roots.powerset_len (N - k)).map multiset.prod).sum :=
+begin
+  rw [splits_iff_card_roots ha, map_to_poly] at hP,
+  convert @vieta K _ _ (map φ (to_poly P)) _ k _;
+  simp [leading_coeff ha, nat_degree ha, fin.is_le k, hP, map_to_poly]
+end
 
 end split
 
@@ -188,7 +205,7 @@ namespace cubic
 
 open polynomial vec_polynomial multiset
 
-variables {P : fin 4 → F} [field F] [field K] {φ : F →+* K} {x y z : K}
+variables [field F] [field K] {P : fin 4 → F}  {φ : F →+* K} {x y z : K}
 
 section roots
 
@@ -213,22 +230,22 @@ begin
   rw [prod_cons, prod_cons, prod_singleton, mul_assoc, mul_assoc]
 end
 
--- move this
-@[simp] lemma foo {α : Type*} (a : α) : powerset_len 1 ({a} : multiset α) = {{a}} := sorry
-
 theorem b_eq_three_roots (ha : P 3 ≠ 0) (h3 : (to_poly (φ ∘ P)).roots = {x, y, z}) :
   φ (P 2) = φ (P 3) * -(x + y + z) :=
-calc φ (P 2) = - φ (P 3) * (z + y + x) : by { rw vieta (splits_of_three_roots ha h3), simp [h3] }
+calc φ (P 2)
+    = - φ (P 3) * (z + y + x) : by { rw to_poly_vieta ha (splits_of_three_roots ha h3), norm_num [h3] }
 ... = _ : by ring
 
 theorem c_eq_three_roots (ha : P 3 ≠ 0) (h3 : (to_poly (φ ∘ P)).roots = {x, y, z}) :
   φ (P 1) = φ (P 3) * (x * y + x * z + y * z) :=
-calc φ (P 1) = φ (P 3) * (y * z + (x * z + x * y)) : by { rw vieta (splits_of_three_roots ha h3), simp [h3] }
+calc φ (P 1)
+    = φ (P 3) * (y * z + (x * z + x * y)) : by { rw to_poly_vieta ha (splits_of_three_roots ha h3), norm_num [h3] }
 ... = _ : by ring
 
 theorem d_eq_three_roots (ha : P 3 ≠ 0) (h3 : (to_poly (φ ∘ P)).roots = {x, y, z}) :
   φ (P 0) = φ (P 3) * -(x * y * z) :=
-calc φ (P 0) = -(φ (P 3) * (x * (y * z))) : by { rw vieta (splits_of_three_roots ha h3), simp [h3] }
+calc φ (P 0)
+    = -(φ (P 3) * (x * (y * z))) : by { rw to_poly_vieta ha (splits_of_three_roots ha h3), norm_num [h3] }
 ... = _ : by ring
 
 end split
@@ -275,7 +292,7 @@ theorem card_roots_of_disc_ne_zero [decidable_eq K] (ha : P 3 ≠ 0)
   (to_poly (φ ∘ P)).roots.to_finset.card = 3 :=
 begin
   rw [to_finset_card_of_nodup $ (disc_ne_zero_iff_roots_nodup ha h3).mp hd,
-        ← @vec_polynomial.splits_iff_card_roots F K 3 P _ _ φ ha, splits_iff_roots_eq_three ha],
+        ← @vec_polynomial.splits_iff_card_roots F K _ _ 3 P φ ha, splits_iff_roots_eq_three ha],
   exact ⟨x, ⟨y, ⟨z, h3⟩⟩⟩
 end
 

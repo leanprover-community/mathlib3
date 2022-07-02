3a57527cd349f867ca79b9e5f1253ae62d949e6b
pbazin <pierre-alexandre.bazin@ens.psl.eu>
2022-07-02 13:03:39 +0200

    simp lemmas from companion def

--- src/data/matrix/companion.lean
+++ src/data/matrix/companion.lean
@@ -29,48 +29,92 @@ open_locale polynomial
 namespace matrix
 variables {R : Type*}
 
-/--The companion matrix of a polynomial.-/
+/-- The companion matrix of a polynomial. -/
 def companion [ring R] (P : R[X]) : matrix (fin P.nat_degree) (fin P.nat_degree) R :=
 if h : P.nat_degree = 0 then 0 else
   λ i j, if j = fin.last' h then -P.coeff i else if (j : ℕ) + 1 = i then 1 else 0
 
-open adjoin_root polynomial
+@[simp] lemma companion_apply_last [ring R] (P : R[X]) {i j : fin P.nat_degree}
+  (hj : (j : ℕ) + 1 = P.nat_degree) : companion P i j = -P.coeff i :=
+begin
+  dunfold companion,
+  split_ifs with h hj',
+  { refine (is_empty_iff.mp _ i).elim, rw h, apply_instance },
+  { refl },
+  { refine (hj' _).elim, ext, simp_rw [fin.coe_last', ← hj, nat.add_sub_cancel] },
+  { refine (hj' _).elim, ext, simp_rw [fin.coe_last', ← hj, nat.add_sub_cancel] }
+end
 
-/--The companion matrix of `P` is the matrix of the multiplication by `root P` in the canonical
-basis `1, root P, (root P)^2, ...` of `adjoin_root P`.-/
+@[simp] lemma companion_apply_eq_one [ring R] (P : R[X]) {i j : fin P.nat_degree}
+  (hj : (j : ℕ) + 1 = i) : companion P i j = 1 :=
+begin
+  dunfold companion,
+  split_ifs with h hj',
+  { refine (is_empty_iff.mp _ i).elim, rw h, apply_instance },
+  { refine (not_le.mpr i.is_lt _).elim,
+    rw [hj', fin.coe_last'] at hj,
+    rw ← hj, exact le_tsub_add },
+  { refl }
+end
+
+@[simp] lemma companion_apply_eq_zero [ring R] (P : R[X]) {i j : fin P.nat_degree}
+  (hj : (j : ℕ) + 1 ≠ P.nat_degree) (hji : (j : ℕ) + 1 ≠ i) : companion P i j = 0 :=
+begin
+  dunfold companion,
+  split_ifs with h hj',
+  { refine (is_empty_iff.mp _ i).elim, rw h, apply_instance },
+  { refine (hj _).elim, rw [hj', fin.coe_last', nat.sub_add_cancel $ nat.pos_of_ne_zero h] },
+  { refl }
+end
+
+open polynomial
+
+@[simp] lemma companion_monomial [ring R] {n : ℕ} {c : R} :
+  companion (monomial n c) = λ i j, if (j : ℕ) + 1 = i then 1 else 0 :=
+begin
+  ext i j,
+  split_ifs with h,
+  { exact companion_apply_eq_one _ h },
+  { by_cases hj : (j : ℕ) + 1 = (monomial n c).nat_degree,
+    { rw [companion_apply_last _ hj, coeff_monomial, neg_eq_zero],
+      by_cases hc : c = 0, { split_ifs, exact hc, refl },
+      simp_rw [hj, nat_degree_monomial_eq _ hc] at h, exact if_neg h },
+    { exact companion_apply_eq_zero _ hj h } }
+end
+
+open adjoin_root
+
+/-- The companion matrix of `P` is the matrix of the multiplication by `root P` in the canonical
+basis `1, root P, (root P)^2, ...` of `adjoin_root P`. -/
 lemma companion_eq_to_matrix_lmul_root [comm_ring R] {P : R[X]} (hP : P.monic) :
   companion P = alg_equiv_matrix (power_basis' hP).basis (algebra.lmul _ _ (root P)) :=
 begin
-  dunfold companion, split_ifs with h,
-  { haveI : subsingleton (matrix (fin P.nat_degree) (fin P.nat_degree) R),
-    { rw h, exact subsingleton_of_empty_left },
-    rw eq_iff_true_of_subsingleton, trivial },
   change _ = linear_map.to_matrix _ _ _,
   ext i j,
   rw [linear_map.to_matrix_apply, algebra.lmul_apply, power_basis.basis_eq_pow, power_basis'_gen,
     ← pow_succ, ← mk_X, ← map_pow, ← monomial_one_right_eq_X_pow],
   dsimp [power_basis'],
   rw [mod_by_monic_eq_sub_mul_div _ hP, coeff_sub, coeff_monomial],
-  by_cases hj : j = fin.last' h,
+  by_cases hj : (j : ℕ) + 1 = P.nat_degree,
   { suffices : monomial P.nat_degree 1 /ₘ P = 1,
-    from by rw [if_pos hj, hj, fin.coe_last', nat.sub_add_cancel $ nat.pos_of_ne_zero h,
-      if_neg $ ne_of_gt i.is_lt, zero_sub, this, mul_one],
+    from by rw [companion_apply_last P hj, hj, if_neg $ ne_of_gt i.is_lt, zero_sub, this, mul_one],
     nontriviality R,
     rw [← monic.nat_degree_eq_zero_iff_eq_one, nat_degree_div_by_monic _ hP,
       nat_degree_monomial_eq _ (@one_ne_zero R _ _), nat.sub_self],
     rw [monic.def, leading_coeff_div_by_monic_of_monic hP, leading_coeff_monomial],
     rw degree_monomial _ (@one_ne_zero R _ _), exact degree_le_nat_degree },
   { suffices : monomial (j + 1) 1 /ₘ P = 0,
-    from by rw [if_neg hj, this, mul_zero, coeff_zero, sub_zero],
+    { rw [this, mul_zero, coeff_zero, sub_zero],
+      split_ifs with hi,
+      { exact companion_apply_eq_one P hi },
+      { exact companion_apply_eq_zero P hj hi } },
     nontriviality R,
     rw [div_by_monic_eq_zero_iff hP, degree_monomial _ (@one_ne_zero R _ _),
       degree_eq_nat_degree hP.ne_zero, with_bot.coe_lt_coe],
-    apply lt_of_le_of_ne,
-    { rw nat.add_one_le_iff, exact j.is_lt },
-    { intro hj', apply hj, ext, simp_rw [fin.coe_last', ← hj', nat.add_sub_cancel] } }
+    exact lt_of_le_of_ne (nat.add_one_le_iff.mpr j.is_lt) hj }
 end
 
-/--The minimal polynomial of the companion matrix of `P` is `P`.-/
+/-- The minimal polynomial of the companion matrix of `P` is `P`. -/
 lemma minpoly_companion [field R] {P : R[X]} (hP : P.monic) : minpoly R (companion P) = P :=
 eq.symm $ minpoly.unique _ _ hP
 (by rw [companion_eq_to_matrix_lmul_root hP, aeval_alg_equiv_apply, aeval_alg_hom_apply, aeval_eq,

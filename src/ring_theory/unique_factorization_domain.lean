9a741737e6557e4a26afc9265f4eb354bd2d45d1
Paul-Lez <72892199+Paul-Lez@users.noreply.github.com>
2022-07-03 09:37:18 +0200

    Init

--- src/ring_theory/unique_factorization_domain.lean
+++ src/ring_theory/unique_factorization_domain.lean
@@ -627,6 +627,11 @@ begin
   rwa [multiset.eq_repeat_of_mem (λ b, h), multiset.prod_repeat] at this
 end
 
+lemma normalized_factors_prod_primes [nontrivial α] {m : multiset α} (h : ∀ p ∈ m, prime p) :
+  multiset.rel associated (normalized_factors m.prod) m :=
+prime_factors_unique (prime_of_normalized_factor) h
+  (normalized_factors_prod (m.prod_ne_zero_of_prime h))
+
 end unique_factorization_monoid
 
 namespace unique_factorization_monoid

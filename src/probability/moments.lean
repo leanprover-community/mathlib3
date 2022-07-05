a5a6793ec8955709d004e292aea4e604cb8127f4
RemyDegenne <Remydegenne@gmail.com>
2022-07-05 14:07:58 +0200

    change names

--- src/probability/moments.lean
+++ src/probability/moments.lean
@@ -195,7 +195,7 @@ begin
   exact real.log_mul (mgf_pos' hμ h_int_X).ne' (mgf_pos' hμ h_int_Y).ne',
 end
 
-lemma chernoff_bound_ge_mgf [is_finite_measure μ] (ε : ℝ) (ht : 0 ≤ t)
+lemma measure_ge_le_exp_mul_mgf [is_finite_measure μ] (ε : ℝ) (ht : 0 ≤ t)
   (h_int : integrable (λ ω, real.exp (t * X ω)) μ) :
   (μ {ω | ε ≤ X ω}).to_real ≤ real.exp(- t * ε) * mgf X μ t :=
 begin
@@ -203,10 +203,8 @@ begin
   cases ht with ht_zero_eq ht_pos,
   { rw ht_zero_eq.symm,
     simp only [neg_zero', zero_mul, real.exp_zero, mgf_zero', one_mul],
-    rw ennreal.to_real_le_to_real (measure_ne_top _ _) (measure_ne_top _ _),
-    { exact measure_mono (set.subset_univ _), },
-    { apply_instance, },
-    { apply_instance, }, },
+    rw ennreal.to_real_le_to_real (measure_ne_top μ _) (measure_ne_top μ _),
+    exact measure_mono (set.subset_univ _), },
   calc (μ {ω | ε ≤ X ω}).to_real
       = (μ {ω | real.exp (t * ε) ≤ real.exp (t * X ω)}).to_real :
     begin
@@ -224,32 +222,32 @@ begin
   ... = real.exp(- t * ε) * mgf X μ t : by { rw [neg_mul, real.exp_neg], refl, },
 end
 
-lemma chernoff_bound_le_mgf [is_finite_measure μ] (ε : ℝ) (ht : t ≤ 0)
+lemma measure_le_le_exp_mul_mgf [is_finite_measure μ] (ε : ℝ) (ht : t ≤ 0)
   (h_int : integrable (λ ω, real.exp (t * X ω)) μ) :
   (μ {ω | X ω ≤ ε}).to_real ≤ real.exp(- t * ε) * mgf X μ t :=
 begin
   rw [← neg_neg t, ← mgf_neg, neg_neg, ← neg_mul_neg (-t)],
-  refine eq.trans_le _ (chernoff_bound_ge_mgf (-ε) (neg_nonneg.mpr ht) _),
+  refine eq.trans_le _ (measure_ge_le_exp_mul_mgf (-ε) (neg_nonneg.mpr ht) _),
   { congr' with ω,
     simp only [pi.neg_apply, neg_le_neg_iff], },
   { simp_rw [pi.neg_apply, neg_mul_neg],
     exact h_int, },
 end
 
-lemma chernoff_bound_ge_cgf [is_finite_measure μ] (ε : ℝ) (ht : 0 ≤ t)
+lemma measure_ge_le_exp_cgf [is_finite_measure μ] (ε : ℝ) (ht : 0 ≤ t)
   (h_int : integrable (λ ω, real.exp (t * X ω)) μ) :
   (μ {ω | ε ≤ X ω}).to_real ≤ real.exp(- t * ε + cgf X μ t) :=
 begin
-  refine (chernoff_bound_ge_mgf ε ht h_int).trans _,
+  refine (measure_ge_le_exp_mul_mgf ε ht h_int).trans _,
   rw real.exp_add,
   exact mul_le_mul le_rfl (real.le_exp_log _) mgf_nonneg (real.exp_pos _).le,
 end
 
-lemma chernoff_bound_le_cgf [is_finite_measure μ] (ε : ℝ) (ht : t ≤ 0)
+lemma measure_le_le_exp_cgf [is_finite_measure μ] (ε : ℝ) (ht : t ≤ 0)
   (h_int : integrable (λ ω, real.exp (t * X ω)) μ) :
   (μ {ω | X ω ≤ ε}).to_real ≤ real.exp(- t * ε + cgf X μ t) :=
 begin
-  refine (chernoff_bound_le_mgf ε ht h_int).trans _,
+  refine (measure_le_le_exp_mul_mgf ε ht h_int).trans _,
   rw real.exp_add,
   exact mul_le_mul le_rfl (real.le_exp_log _) mgf_nonneg (real.exp_pos _).le,
 end

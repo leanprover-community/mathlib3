79b7bd6d071fffc0384cca85debfc24d7862bb0e
RemyDegenne <Remydegenne@gmail.com>
2022-07-05 10:47:52 +0200

    add left tail versions

--- src/probability/moments.lean
+++ src/probability/moments.lean
@@ -167,6 +167,11 @@ lemma mgf_pos [is_probability_measure μ] (h_int_X : integrable (λ ω, real.exp
   0 < mgf X μ t :=
 mgf_pos' (is_probability_measure.ne_zero μ) h_int_X
 
+lemma mgf_neg : mgf (-X) μ t = mgf X μ (-t) :=
+by simp_rw [mgf, pi.neg_apply, mul_neg, neg_mul]
+
+lemma cgf_neg : cgf (-X) μ t = cgf X μ (-t) := by simp_rw [cgf, mgf_neg]
+
 lemma indep_fun.mgf_add {X Y : Ω → ℝ} (h_indep : indep_fun X Y μ)
   (h_int_X : integrable (λ ω, real.exp (t * X ω)) μ)
   (h_int_Y : integrable (λ ω, real.exp (t * Y ω)) μ) :
@@ -190,13 +195,13 @@ begin
   exact real.log_mul (mgf_pos' hμ h_int_X).ne' (mgf_pos' hμ h_int_Y).ne',
 end
 
-lemma chernoff_bound [is_finite_measure μ] (ε : ℝ) (ht : 0 ≤ t)
+lemma chernoff_bound_ge_mgf [is_finite_measure μ] (ε : ℝ) (ht : 0 ≤ t)
   (h_int : integrable (λ ω, real.exp (t * X ω)) μ) :
   (μ {ω | ε ≤ X ω}).to_real ≤ real.exp(- t * ε) * mgf X μ t :=
 begin
   rw le_iff_eq_or_lt at ht,
-  cases ht with ht ht,
-  { rw ht.symm,
+  cases ht with ht_zero_eq ht_pos,
+  { rw ht_zero_eq.symm,
     simp only [neg_zero', zero_mul, real.exp_zero, mgf_zero', one_mul],
     rw ennreal.to_real_le_to_real (measure_ne_top _ _) (measure_ne_top _ _),
     { exact measure_mono (set.subset_univ _), },
@@ -207,7 +212,7 @@ begin
     begin
       congr' with ω,
       simp only [real.exp_le_exp, eq_iff_iff],
-      exact ⟨λ h, mul_le_mul_of_nonneg_left h ht.le, λ h, le_of_mul_le_mul_left h ht⟩,
+      exact ⟨λ h, mul_le_mul_of_nonneg_left h ht_pos.le, λ h, le_of_mul_le_mul_left h ht_pos⟩,
     end
   ... ≤ (real.exp (t * ε))⁻¹ * μ[λ ω, real.exp (t * X ω)] :
     begin
@@ -219,13 +224,34 @@ begin
   ... = real.exp(- t * ε) * mgf X μ t : by { rw [neg_mul, real.exp_neg], refl, },
 end
 
-lemma chernoff_bound_cgf [is_finite_measure μ] (ε : ℝ) (ht : 0 ≤ t)
+lemma chernoff_bound_le_mgf [is_finite_measure μ] (ε : ℝ) (ht : t ≤ 0)
+  (h_int : integrable (λ ω, real.exp (t * X ω)) μ) :
+  (μ {ω | X ω ≤ ε}).to_real ≤ real.exp(- t * ε) * mgf X μ t :=
+begin
+  rw [← neg_neg t, ← mgf_neg, neg_neg, ← neg_mul_neg (-t)],
+  refine eq.trans_le _ (chernoff_bound_ge_mgf (-ε) (neg_nonneg.mpr ht) _),
+  { congr' with ω,
+    simp only [pi.neg_apply, neg_le_neg_iff], },
+  { simp_rw [pi.neg_apply, neg_mul_neg],
+    exact h_int, },
+end
+
+lemma chernoff_bound_ge_cgf [is_finite_measure μ] (ε : ℝ) (ht : 0 ≤ t)
   (h_int : integrable (λ ω, real.exp (t * X ω)) μ) :
   (μ {ω | ε ≤ X ω}).to_real ≤ real.exp(- t * ε + cgf X μ t) :=
 begin
-  refine (chernoff_bound ε ht h_int).trans _,
+  refine (chernoff_bound_ge_mgf ε ht h_int).trans _,
   rw real.exp_add,
-  refine mul_le_mul le_rfl (real.le_exp_log _) mgf_nonneg (real.exp_pos _).le,
+  exact mul_le_mul le_rfl (real.le_exp_log _) mgf_nonneg (real.exp_pos _).le,
+end
+
+lemma chernoff_bound_le_cgf [is_finite_measure μ] (ε : ℝ) (ht : t ≤ 0)
+  (h_int : integrable (λ ω, real.exp (t * X ω)) μ) :
+  (μ {ω | X ω ≤ ε}).to_real ≤ real.exp(- t * ε + cgf X μ t) :=
+begin
+  refine (chernoff_bound_le_mgf ε ht h_int).trans _,
+  rw real.exp_add,
+  exact mul_le_mul le_rfl (real.le_exp_log _) mgf_nonneg (real.exp_pos _).le,
 end
 
 end moment_generating_function

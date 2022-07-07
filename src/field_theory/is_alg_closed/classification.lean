e139f5277912d5c8e50e4f039ca5599c640b1984
Yury G. Kudryashov <urkud@urkud.name>
2022-07-07 15:28:17 -0400

    Snapshot

--- src/field_theory/is_alg_closed/classification.lean
+++ src/field_theory/is_alg_closed/classification.lean
@@ -192,9 +192,9 @@ begin
       from ring_hom.injective _) with t ht,
   have : #s = #t,
   { rw [← cardinal_eq_cardinal_transcendence_basis_of_aleph_0_lt _ hs
-      (lt_aleph_0_of_fintype (zmod p)).le hK,
+      (lt_aleph_0_of_finite (zmod p)).le hK,
         ← cardinal_eq_cardinal_transcendence_basis_of_aleph_0_lt _ ht
-      (lt_aleph_0_of_fintype (zmod p)).le, hKL],
+      (lt_aleph_0_of_finite (zmod p)).le, hKL],
     rwa ← hKL },
   cases cardinal.eq.1 this with e,
   exact ⟨equiv_of_transcendence_basis _ _ e hs ht⟩

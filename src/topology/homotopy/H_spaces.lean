f3d260e6225a6ee08f4ccd3132a499003cec77bf
Filippo A. E. Nuccio <filippo.nuccio@univ-st-etienne.fr>
2022-07-08 15:03:07 +0200

    advances on H_spaces

--- src/topology/homotopy/H_spaces.lean
+++ src/topology/homotopy/H_spaces.lean
@@ -1,5 +1,5 @@
 /-
-Copyright (c) 2022 Shing Tak Lam. All rights reserved.
+Copyright (c) 2022 Filippo A. E. Nuccio Mortarino Majno di Capriglio. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Filippo A. E. Nuccio
 -/
@@ -7,30 +7,59 @@ Authors: Filippo A. E. Nuccio
 import topology.homotopy.basic
 import topology.homotopy.path
 
--- variables (X : Type*) [topological_space X]
+universe u
 
 noncomputable theory
 
-open path --continuous_map.homotopy_rel --homotopy--homotopic_with
+open path
 
 open_locale unit_interval
 
 namespace H_space
 
-class H_space (X : Type*) [topological_space X] :=
-(H_mul : X × X → X)
+class H_space (X : Type u) [topological_space X]  :=
+(Hmul : X × X → X)
 (e : X)
-(cont' : continuous H_mul)
-(self_e : H_mul (e, e) = e)
-(left_mul_e : ∀ x : X,
+(cont' : continuous Hmul)
+(Hmul_e_e : Hmul (e, e) = e)
+(left_Hmul_e : ∀ x : X,
   @continuous_map.homotopy_rel (X) (X) _ _
-  ⟨(λ x : X, H_mul (e, x)), (continuous.comp cont' (continuous_const.prod_mk continuous_id'))⟩
-  ⟨(λ x : X, H_mul (e, x)), (continuous.comp cont' (continuous_const.prod_mk continuous_id'))⟩
+  ⟨(λ x : X, Hmul (e, x)), (continuous.comp cont' (continuous_const.prod_mk continuous_id'))⟩
+  ⟨(λ x : X, Hmul (e, x)), (continuous.comp cont' (continuous_const.prod_mk continuous_id'))⟩
   {e})
-(right_mul_e : ∀ x : X,
+(right_Hmul_e : ∀ x : X,
   @continuous_map.homotopy_rel (X) (X) _ _
-  ⟨(λ x : X, H_mul (x, e)), (continuous.comp cont'(continuous_id'.prod_mk continuous_const))⟩
-  ⟨(λ x : X, H_mul (x, e)), (continuous.comp cont'(continuous_id'.prod_mk continuous_const))⟩
+  ⟨(λ x : X, Hmul (x, e)), (continuous.comp cont'(continuous_id'.prod_mk continuous_const))⟩
+  ⟨(λ x : X, Hmul (x, e)), (continuous.comp cont'(continuous_id'.prod_mk continuous_const))⟩
   {e})
+
+instance topological_group_H_space (G : Type u) [topological_space G] [group G]
+  [topological_group G]: H_space G :=
+{ Hmul := function.uncurry has_mul.mul,
+  e := 1,
+  cont' := continuous_mul,
+  Hmul_e_e := by {simp only [function.uncurry_apply_pair, mul_one]},
+  left_Hmul_e := λ _, continuous_map.homotopy_rel.refl _ _ ,
+  right_Hmul_e := λ _, continuous_map.homotopy_rel.refl _ _ }
+
+-- instance S0_H_space : H_space (metric.sphere (0 : ℝ × ℝ) 1) := sorry
+-- instance S1_H_space : H_space (metric.sphere (0 : ℝ × ℝ) 1) := sorry
+-- instance S3_H_space : H_space (metric.sphere (0 : ℝ × ℝ) 1) := sorry
+-- instance S7_H_space : H_space (metric.sphere (0 : ℝ × ℝ) 1) := sorry
+
+variables {X : Type u} [topological_space X]
+
+def loop_space (x : X) : Type u := {f : C(I, X) // f 0 = x ∧ f 1 = x}
+
+instance (x : X) : topological_space (loop_space x) := by {exact subtype.topological_space}
+
+instance loop_space_is_H_space (x : X) : H_space (loop_space x) :=
+{ Hmul := sorry,
+  e := sorry,
+  cont' := sorry,
+  Hmul_e_e := sorry,
+  left_Hmul_e := sorry,
+  right_Hmul_e := sorry}
+
 
 end H_space

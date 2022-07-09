767ec57a16b1cc83397dc5ce3ea1615d1268ca7f
Violeta Hernández <vi.hdz.p@gmail.com>
2022-07-08 19:29:17 -0500

    Update basic.lean

--- src/set_theory/cardinal/basic.lean
+++ src/set_theory/cardinal/basic.lean
@@ -848,7 +848,7 @@ lemma mk_finset_of_fintype [fintype α] : #(finset α) = 2 ^ℕ fintype.card α 
 
 @[simp] lemma mk_finsupp_of_fintype (α : Type u) (β : Type v) [fintype α] [has_zero β] :
   #(α →₀ β) = lift.{u} (#β) ^ℕ fintype.card α :=
-by simpa using finsupp.equiv_fun_on_fintype.cardinal_eq.trans (mk_arrow α β)
+by simpa using (@finsupp.equiv_fun_on_fintype α β _ _).cardinal_eq
 
 theorem card_le_of_finset {α} (s : finset α) : (s.card : cardinal) ≤ #α :=
 begin

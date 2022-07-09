ac51b38115a5e721f2d28e1b4626f591ff319879
Violeta Hernández <vi.hdz.p@gmail.com>
2022-07-09 18:04:20 -0500

    Update zfc.lean

--- src/set_theory/zfc.lean
+++ src/set_theory/zfc.lean
@@ -61,12 +61,22 @@ def arity (α : Type u) : ℕ → Type u
 | 0     := α
 | (n+1) := α → arity n
 
+@[simp] theorem arity_zero (α : Type u) : arity α 0 = α := rfl
+@[simp] theorem arity_succ (α : Type u) (n : ℕ) : arity α n.succ = (α → arity α n) := rfl
+
 namespace arity
 
 /-- Constant `n`-ary function with value `a`. -/
 def const {α : Type u} (a : α) : ∀ n, arity α n
 | 0     := a
 | (n+1) := λ _, const n
+
+@[simp] theorem const_zero {α : Type u} (a : α) : const a 0 = a := rfl
+@[simp] theorem const_succ {α : Type u} (a : α) (n : ℕ) : const a n.succ = λ _, const a n := rfl
+
+@[simp] theorem const_succ_apply {α : Type u} (a : α) (n : ℕ) (x : α) :
+  const a n.succ x = const a n :=
+rfl
 
 instance arity.inhabited {α n} [inhabited α] : inhabited (arity α n) :=
 ⟨const default _⟩

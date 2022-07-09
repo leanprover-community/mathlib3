24a7ad34da8e66d6c7a53355306cc7c57f79b12b
Stuart Presnell <stuart.presnell@gmail.com>
2022-07-09 14:23:50 +0100

    Remove import of `data.nat.log`

--- src/data/nat/factorization/basic.lean
+++ src/data/nat/factorization/basic.lean
@@ -6,7 +6,7 @@ Authors: Stuart Presnell
 import data.nat.prime
 import data.finsupp.multiset
 import algebra.big_operators.finsupp
-import data.nat.log
+-- import data.nat.log
 import data.nat.interval
 
 /-!

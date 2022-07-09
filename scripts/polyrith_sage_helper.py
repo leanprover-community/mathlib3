5dbc0f9a435c45295a26ba1745181c4cb31389c5
Eric Wieser <wieser.eric@gmail.com>
2022-07-09 12:41:12 +0000

    Do not go back and forth with string parsing

--- scripts/polyrith_sage_helper.py
+++ scripts/polyrith_sage_helper.py
@@ -1,61 +1,34 @@
 # this file will be run by the remote sage server, so should not import local files.
-def const_string(const):
-    num = str(const)
-    if "/" not in num:
-        num += "/1"
-    return "(poly.const " + num + ")"
+from functools import reduce
+from sage.rings.polynomial.polydict import ETuple
 
-def var_string(var):
-    if "var" in var:
-        var = var.replace("var", "")
-        var = "(poly.var " + var + ")"
-    return var
+def mk_app(*args: str) -> str:
+    return "(" + " ".join(args) + ")"
 
-def mul_vars_string(var_list):
-    if var_list == []:
-        return const_string(1)
-    elif len(var_list) == 1:
-        return var_list[0]
-    return f'(poly.mul {mul_vars_string(var_list[:-1])} {var_list[-1]})'
+def const_to_string(coeff: QQ) -> str:
+    return mk_app("poly.const", str(coeff.numerator()) + "/" + str(coeff.denominator()))
 
-# assumes a monomial is always (var^nat)*(var^nat)... true?
-def format_monom_component(e):
-    if "^" not in e:
-        return var_string(e)
-    base, exp = e.split("^")
-    return f'(poly.pow {var_string(base)} {int(exp)})'
+def power_to_string(var: int, pow: int) -> str:
+    assert pow != 0
+    var_s = mk_app("poly.var", str(var))
+    if pow == 1:
+        return var_s
+    return mk_app("poly.pow", var_s, str(pow))
 
-def monomial_to_string(m):
-    m = str(m)
-    m_list = m.replace(" ", "").split("*")
-    var_things = [format_monom_component(e) for e in m_list]
-    return mul_vars_string(var_things)
+def sum_to_string(terms: list[str]) -> str:
+    terms = iter(terms)
+    try:
+        first = next(terms)
+    except StopIteration:
+        return const_to_string(QQ(0))
+    else:
+        return reduce(lambda s, t: mk_app("poly.add", s, t), terms, first)
 
-def poly_terms_string(terms):
-    if terms == []:
-        return const_string(1)
-    elif len(terms) == 1:
-        if "-" in terms[0]:
-            return f'(poly.sub {const_string(0)} {terms[0].replace("-", "")})'
-        return terms[0]
-    elif "-" in terms[-1]:
-        return f'(poly.sub {poly_terms_string(terms[:-1])} {terms[-1].replace("-", "")})'
-    return f'(poly.add {poly_terms_string(terms[:-1])} {terms[-1]})'
+def monomial_to_string(etuple: ETuple, coeff: QQ) -> str:
+    return reduce(
+        lambda s, t: mk_app("poly.mul", s, power_to_string(t[0], t[1])),
+        etuple.sparse_iter(),
+        const_to_string(coeff))
 
-def polynomial_to_string(p):
-    monomials = p.monomials()
-    coeffs = p.coefficients()
-    if len(monomials) == 0:
-        return const_string(0)
-    out = []
-    for i in range(len(coeffs)):
-        if str(monomials[i]) == "1":
-            n = QQ(float(coeffs[i]))
-            out.append(const_string(n))
-        elif str(coeffs[i]) == "1":
-            out.append(monomial_to_string(monomials[i]))
-        elif str(coeffs[i]) == "-1":
-            out.append("-" + monomial_to_string(monomials[i]))
-        else:
-            out.append("(poly.mul " + const_string(coeffs[i]) + " " + monomial_to_string(monomials[i]) + ")")
-    return poly_terms_string(out)
+def polynomial_to_string(p) -> str:
+    return sum_to_string(monomial_to_string(pows, coeff) for pows, coeff in p.dict().items())

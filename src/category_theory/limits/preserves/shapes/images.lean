b384593d84ae1f998e1ba0ebf3ee7a525c5b75a1
Jujian Zhang <jujian.zhang1998@outlook.com>
2022-07-10 13:27:24 +0100

    apply suggestions

--- src/category_theory/limits/preserves/shapes/images.lean
+++ src/category_theory/limits/preserves/shapes/images.lean
@@ -34,38 +34,29 @@ variables [Π {X Y Z : A} (f : X ⟶ Y) (g : X ⟶ Z), preserves_colimit (span f
 /--
 If a functor preserves span and cospan, then it preserves images.
 -/
-def iso {X Y : A} (f : X ⟶ Y) : image (L.map f) ≅ L.obj (image f) :=
-have aux1 : strong_epi_mono_factorisation (L.map f) :=
+@[simps] def iso {X Y : A} (f : X ⟶ Y) : image (L.map f) ≅ L.obj (image f) :=
+let aux1 : strong_epi_mono_factorisation (L.map f) :=
 { I := L.obj (limits.image f),
   m := L.map $ limits.image.ι _,
   m_mono := infer_instance,
   e := L.map $ factor_thru_image _,
   e_strong_epi := @@strong_epi_of_epi _ _ _ $
     @@category_theory.preserves_epi _ _ L _ _ _,
-  fac' := by rw [←L.map_comp, limits.image.fac] },
+  fac' := by rw [←L.map_comp, limits.image.fac] } in
 is_image.iso_ext (image.is_image (L.map f)) aux1.to_mono_is_image
 
-lemma factor_thru_image_comp_hom {X Y : A} (f : X ⟶ Y) :
+@[reassoc] lemma factor_thru_image_comp_hom {X Y : A} (f : X ⟶ Y) :
   factor_thru_image (L.map f) ≫ (iso L f).hom =
   L.map (factor_thru_image f) :=
-begin
-  dunfold iso,
-  erw [is_image.iso_ext_hom, is_image.fac_lift],
-end
+by simp
 
-lemma hom_comp_map_image_ι {X Y : A} (f : X ⟶ Y) :
+@[reassoc] lemma hom_comp_map_image_ι {X Y : A} (f : X ⟶ Y) :
   (iso L f).hom ≫ L.map (image.ι f) = image.ι (L.map f) :=
-begin
-  dunfold iso,
-  simp
-end
+by simp
 
-lemma inv_comp_image_ι_map {X Y : A} (f : X ⟶ Y) :
+@[reassoc] lemma inv_comp_image_ι_map {X Y : A} (f : X ⟶ Y) :
   (iso L f).inv ≫ image.ι (L.map f) = L.map (image.ι f) :=
-begin
-  dunfold iso,
-  simp,
-end
+by simp
 
 end preserves_image
 

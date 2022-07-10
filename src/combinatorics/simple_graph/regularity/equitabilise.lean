78167af0469077aa41e7fd5ef92fdaef080ccbe0
YaelDillies <yael.dillies@gmail.com>
2022-07-10 23:00:32 +0100

    comment proof

--- src/combinatorics/simple_graph/regularity/equitabilise.lean
+++ src/combinatorics/simple_graph/regularity/equitabilise.lean
@@ -26,64 +26,72 @@ open finset nat
 namespace finpartition
 variables {α : Type*} [decidable_eq α] {s t : finset α} {m n a b : ℕ} {P : finpartition s}
 
+/-- Given a partition `P` of `s`, as well as a proof that `a * m + b * (m + 1) = s.card`, we can
+find a new partition `Q` of `s` where each part has size `m` or `m + 1`, every part of `P` is the
+union of parts of `Q` plus at most `m` extra elements, there are `b` parts of size `m + 1` and
+(provided `m > 0`, because a partition does not have parts of size `0`) there are `a` parts of size
+`m` and hence `a + b` parts in total. -/
 lemma equitabilise_aux (P : finpartition s) (hs : a * m + b * (m + 1) = s.card) :
   ∃ Q : finpartition s,
     (∀ x : finset α, x ∈ Q.parts → x.card = m ∨ x.card = m + 1) ∧
     (∀ x, x ∈ P.parts → (x \ (Q.parts.filter $ λ y, y ⊆ x).bUnion id).card ≤ m) ∧
     (Q.parts.filter $ λ i, card i = m + 1).card = b :=
 begin
+  -- Get rid of the easy case `m = 0`
   obtain rfl | m_pos := m.eq_zero_or_pos,
   { refine ⟨⊥, by simp, _, by simpa using hs.symm⟩,
     simp only [le_zero_iff, card_eq_zero, mem_bUnion, exists_prop, mem_filter, id.def, and_assoc,
       sdiff_eq_empty_iff_subset, subset_iff],
     exact λ x hx a ha, ⟨{a}, mem_map_of_mem _ (P.le hx ha), singleton_subset_iff.2 ha,
       mem_singleton_self _⟩ },
+  -- Prove the case `m > 0` by strong induction on `s`
   induction s using finset.strong_induction with s ih generalizing P a b,
+  -- If `a = b = 0`, then `s = ∅` and we can partition into zero parts
   by_cases hab : a = 0 ∧ b = 0,
   { simp only [hab.1, hab.2, add_zero, zero_mul, eq_comm, card_eq_zero] at hs,
     subst hs,
     exact ⟨finpartition.empty _, by simp, by simp [unique.eq_default P], by simp [hab.2]⟩ },
   simp_rw [not_and_distrib, ←ne.def, ←pos_iff_ne_zero] at hab,
-  set n := if 0 < a then m else m + 1 with hn, -- the size of the smallest part
-  obtain ⟨hn₀, hn₁, hn₂⟩ : 0 < n ∧ n ≤ m + 1 ∧ n ≤ a * m + b * (m + 1),
-  { rw hn,
-    split_ifs,
-    { exact ⟨m_pos, le_succ _, le_add_right (le_mul_of_pos_left ‹0 < a›)⟩ },
-    { exact ⟨succ_pos', le_rfl, le_add_left (le_mul_of_pos_left $ hab.resolve_left ‹¬0 < a›)⟩ } },
+  -- `n` will be the size of the smallest part
+  set n := if 0 < a then m else m + 1 with hn,
+  -- Some easy facts about it
+  obtain ⟨hn₀, hn₁, hn₂, hn₃⟩ : 0 < n ∧ n ≤ m + 1 ∧ n ≤ a * m + b * (m + 1) ∧
+    ite (0 < a) (a - 1) a * m + ite (0 < a) b (b - 1) * (m + 1) = s.card - n,
+  { rw [hn, ←hs],
+    split_ifs; rw [tsub_mul, one_mul],
+    { refine ⟨m_pos, le_succ _, le_add_right (le_mul_of_pos_left ‹0 < a›), _⟩,
+      rw tsub_add_eq_add_tsub (le_mul_of_pos_left h), },
+    { refine ⟨succ_pos', le_rfl, le_add_left (le_mul_of_pos_left $ hab.resolve_left ‹¬0 < a›), _⟩,
+      rw ←add_tsub_assoc_of_le (le_mul_of_pos_left $ hab.resolve_left ‹¬0 < a›) } },
+  /- We will call the inductive hypothesis on a partition of `s \ t` for a carefully chosen `t ⊆ s`.
+  To decide which, however, we must distinguish the case where all parts of `P` have size `m` (in
+  which case we take `t` to be an arbitrary subset of `s` of size `n`) from the case where at least
+  one part `u` of `P` has size `m + 1` (in which case we take `t` to be an arbitrary subset of `u`
+  of size `n`). The rest of each branch is just tedious calculations to satisfy the induction
+  hypothesis. -/
   by_cases ∀ u ∈ P.parts, card u < m + 1,
-  { obtain ⟨s', hs'₁, hs'₂⟩ := exists_smaller_set s n (hn₂.trans_eq hs),
-    have hs' : s'.nonempty := by rwa [←card_pos, hs'₂],
-    have : ite (0 < a) (a - 1) a * m + ite (0 < a) b (b - 1) * (m + 1) = (s \ s').card,
-    { rw [card_sdiff ‹s' ⊆ s›, hs'₂, hn, ←hs],
-      split_ifs,
-      { rw [tsub_mul, one_mul, tsub_add_eq_add_tsub (le_mul_of_pos_left ‹0 < a›)] },
-      { rw [tsub_mul, one_mul,
-          ←add_tsub_assoc_of_le (le_mul_of_pos_left (hab.resolve_left ‹¬0 < a›))] } },
-    obtain ⟨R, hR₁, hR₂, hR₃⟩ := @ih (s \ s') (sdiff_ssubset hs'₁ ‹s'.nonempty›) (P.avoid s')
-      (if 0 < a then a-1 else a) (if 0 < a then b else b-1) this,
-    refine ⟨R.extend hs'.ne_empty sdiff_disjoint (sdiff_sup_cancel hs'₁), _, _, _⟩,
-    { simp only [extend_parts, mem_insert, forall_eq_or_imp, and_iff_left hR₁, hs'₂, hn],
+  { obtain ⟨t, hts, htn⟩ := exists_smaller_set s n (hn₂.trans_eq hs),
+    have ht : t.nonempty := by rwa [←card_pos, htn],
+    have hcard : ite (0 < a) (a - 1) a * m + ite (0 < a) b (b - 1) * (m + 1) = (s \ t).card,
+    { rw [card_sdiff ‹t ⊆ s›, htn, hn₃] },
+    obtain ⟨R, hR₁, hR₂, hR₃⟩ := @ih (s \ t) (sdiff_ssubset hts ‹t.nonempty›) (P.avoid t)
+      (if 0 < a then a-1 else a) (if 0 < a then b else b-1) hcard,
+    refine ⟨R.extend ht.ne_empty sdiff_disjoint (sdiff_sup_cancel hts), _, _, _⟩,
+    { simp only [extend_parts, mem_insert, forall_eq_or_imp, and_iff_left hR₁, htn, hn],
       exact ite_eq_or_eq _ _ _ },
     { exact λ x hx, (card_le_of_subset $ sdiff_subset _ _).trans (lt_succ_iff.1 $ h _ hx) },
-    rw [extend_parts, filter_insert, hs'₂, hn],
-    simp only [one_ne_zero, ite_eq_right_iff, self_eq_add_right],
-    split_ifs,
-    { rw [card_insert_of_not_mem, hR₃, if_neg h_1, tsub_add_cancel_of_le],
-      { exact hab.resolve_left h_1 },
-      simp only [mem_filter, hs'₂, hn, if_neg h_1, eq_self_iff_true, and_true],
-      exact λ t, hs'.ne_empty (le_sdiff_iff.1 $ R.le t) },
-    { rw [hR₃, if_pos],
-      simpa using h_1 } },
+    simp_rw [extend_parts, filter_insert, htn, hn, m.succ_ne_self.symm.ite_eq_right_iff],
+    split_ifs with ha,
+    { rw [hR₃, if_pos ha] },
+    rw [card_insert_of_not_mem (λ H, _), hR₃, if_neg ha, tsub_add_cancel_of_le],
+    { exact hab.resolve_left ha },
+    { exact ht.ne_empty (le_sdiff_iff.1 $ R.le $ filter_subset _ _ H) } },
   push_neg at h,
   obtain ⟨u, hu₁, hu₂⟩ := h,
   obtain ⟨t, htu, htn⟩ := exists_smaller_set _ _ (hn₁.trans hu₂),
-  have hcard : ite (0 < a) (a - 1) a * m + ite (0 < a) b (b - 1) * (m + 1) = (s \ t).card,
-  { rw [card_sdiff (htu.trans $ P.le hu₁), ←hs, htn, hn],
-    split_ifs; rw [tsub_mul, one_mul],
-    { rw [tsub_add_eq_add_tsub (le_mul_of_pos_left h)] },
-    { rw [←add_tsub_assoc_of_le],
-      exact le_mul_of_pos_left (hab.resolve_left h) } },
   have ht : t.nonempty := by rwa [←card_pos, htn],
+  have hcard : ite (0 < a) (a - 1) a * m + ite (0 < a) b (b - 1) * (m + 1) = (s \ t).card,
+  { rw [card_sdiff (htu.trans $ P.le hu₁), htn, hn₃] },
   obtain ⟨R, hR₁, hR₂, hR₃⟩ := @ih (s \ t) (sdiff_ssubset (htu.trans $ P.le hu₁) ht) (P.avoid t)
     (if 0 < a then a-1 else a) (if 0 < a then b else b-1) hcard,
   refine ⟨R.extend ht.ne_empty sdiff_disjoint (sdiff_sup_cancel $ htu.trans $ P.le hu₁), _, _, _⟩,
@@ -103,10 +111,10 @@ begin
       exact λ hi₁ hi₂ hi₃, ⟨⟨hi₁, hi₂⟩, λ x hx hx', hi₃ _ hx $ hx'.trans $ sdiff_subset _ _⟩ },
     { apply sdiff_subset_sdiff subset.rfl (bUnion_subset_bUnion_of_subset_left _ _),
       exact filter_subset_filter _ (subset_insert _ _) },
-    { simp only [avoid, of_erase, mem_erase, mem_image, bot_eq_empty],
-      exact ⟨(nonempty_of_mem_parts _ $ mem_of_mem_erase hx).ne_empty, _, mem_of_mem_erase hx,
-        (disjoint_of_subset_right htu $
-          P.disjoint (mem_of_mem_erase hx) hu₁ $ ne_of_mem_erase hx).sdiff_eq_left⟩ } },
+    simp only [avoid, of_erase, mem_erase, mem_image, bot_eq_empty],
+    exact ⟨(nonempty_of_mem_parts _ $ mem_of_mem_erase hx).ne_empty, _, mem_of_mem_erase hx,
+      (disjoint_of_subset_right htu $ P.disjoint (mem_of_mem_erase hx) hu₁ $
+        ne_of_mem_erase hx).sdiff_eq_left⟩ },
   simp only [extend_parts, filter_insert, htn, hn, m.succ_ne_self.symm.ite_eq_right_iff],
   split_ifs,
   { rw [hR₃, if_pos h] },

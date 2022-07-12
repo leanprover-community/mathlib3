c13827b4b284bffea33511f2ef531af16f76f0a3
Eric Wieser <wieser.eric@gmail.com>
2022-07-12 12:39:57 +0000

    Swap argument order to golf

--- src/measure_theory/probability_mass_function/monad.lean
+++ src/measure_theory/probability_mass_function/monad.lean
@@ -97,7 +97,7 @@ by simp
 lemma coe_bind_apply (b : β) : (p.bind f b : ℝ≥0∞) = ∑'a, p a * f a b :=
 eq.trans (ennreal.coe_tsum $ bind.summable p f b) $ by simp
 
-@[simp] lemma pure_bind (a : α) : (pure a).bind f = f a :=
+@[simp] lemma pure_bind (a : α) (f : α → pmf β) : (pure a).bind f = f a :=
 have ∀ b a', ite (a' = a) 1 0 * f a' b = ite (a' = a) (f a b) 0, from
   assume b a', by split_ifs; simp; subst h; simp,
 by ext b; simp [this]
@@ -173,8 +173,8 @@ instance : is_lawful_functor pmf :=
 instance : is_lawful_monad pmf :=
 { bind_pure_comp_eq_map := λ α β f x, rfl,
   bind_map_eq_seq := λ α β f x, rfl,
-  pure_bind := λ α β f x, pmf.pure_bind x f,
-  bind_assoc := λ α β γ x f g, pmf.bind_bind x f g }
+  pure_bind := λ α β, pmf.pure_bind,
+  bind_assoc := λ α β γ, pmf.bind_bind }
 
 section bind_on_support
 

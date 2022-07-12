74cd828c305b24a902c88eccd835ebbe34c69915
Laurent Bartholdi <laurent.bartholdi@gmail.com>
2022-07-12 21:04:10 +0200

    New version, almost compiles :)

--- archive/normed_group.lean
+++ archive/normed_group.lean
@@ -14,32 +14,43 @@ namespace normed_group
 
 section
 
-variables {G S : Type*} [group G] [fintype S]
+variables {G S : Type*} [group G] [fintype S] [decidable_eq S]
 
 """a group with a norm and associated left-invariant metric"""
-class normed_group (G : Type*) [group G] extends has_norm G, metric_space G :=
+class normed_group extends has_norm G, metric_space G :=
 (dist_eq : ∀ x y : G, dist x y = ∥x⁻¹*y∥)
 
 """an S-generated group"""
-class generated_group (G S : Type*) [group G] :=
+class generated_group (G S : Type*) extends group G :=
 (marking : free_group S → G)
 (is_surjective : function.surjective marking)
 
 """an S-generated group, with additionally a norm on generators"""
-class norm_generated_group (G S : Type*) [group G] extends generated_group G S :=
+class norm_generated_group (G S : Type*) extends generated_group G S :=
 (Snorm : S → ℝ)
 (Spos : ∃ε > 0, ∀s : S, Snorm s ≥ ε)
 
---!! I forgot how to lift an element of F to a list (S × Prop)
-def free_group_norm (F : free_group S) (Snorm : S → ℝ) : F → ℝ := λf,
-| [] -> 0
-| [ (s,_) :: tail ] -> (Snorm s) + (free_group_norm F Snorm) tail
+def list_norm (Snorm : S → ℝ) : list (S × bool) → ℝ
+| [] := 0
+| (s :: tail) := (Snorm s.fst) + (list_norm tail)
 
-instance gg_to_ngg [group G] (GS : generated_group G S) : norm_generated_group G S := { Snorm := λ s, 1, Spos := begin use 1,
+--!! why do I have to repeat "decidable_eq S"?
+def free_group_norm (Snorm : S → ℝ) [decidable_eq S] : free_group S → ℝ := λf, list_norm Snorm (free_group.to_word f)
+
+instance gg_to_ngg (GS : generated_group G S) : norm_generated_group G S := { Snorm := λ s, 1, Spos := begin use 1,
 split, exact zero_lt_one, intro s, finish end, ..GS }
 
-instance ngg_to_ng [group G] (GS : norm_generated_group G S) : normed_group G := { norm := λ g, inf{ ∥w∥ | w : free_group S, marking w = g }, ..GS }
+--!! it's useful to have a norm and a metric. However, there will be lots of code duplication if we're not careful
+instance ngg_to_ng (GS : norm_generated_group G S) [decidable_eq S] : normed_group := { norm := λ g, Inf{ (free_group_norm GS.Snorm)'' (GS.marking⁻¹' g)},
+dist := sorry,
+dist_self := sorry,
+dist_comm := sorry,
+dist_triangle := sorry,
+eq_of_dist_eq_zero := sorry,
+dist_eq := sorry,
+..GS }
 
+--!! the syntax below is completely wrong
 lemma independence_of_generating_set [group G] {S₁ S₂} (GS₁ : norm_generated_group G S₁) (GS₂ : norm_generated_group G S₂) : ∃ K, lipschitz_with K ((λ (g : GS₁), (g : GS₂)) : (normed_group G) → (normed_group G)) := sorry
 
 end

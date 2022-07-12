91ad39bea61b0302279cc88a15a8696b7e9a05d5
Andrew Yang <36414270+erdOne@users.noreply.github.com>
2022-07-13 03:32:42 +0800

    Update image_sheaf.lean

--- src/category_theory/sites/image_sheaf.lean
+++ src/category_theory/sites/image_sheaf.lean
@@ -1,3 +1,8 @@
+/-
+Copyright (c) 2022 Andrew Yang. All rights reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
+Authors: Andrew Yang
+-/
 import category_theory.elementwise
 import category_theory.sites.compatible_sheafification
 

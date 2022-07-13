ede73b2571f6b40dba488d565a66c2bb1a91602f
Yury G. Kudryashov <urkud@urkud.name>
2022-07-13 00:00:41 +0000

    refactor(topology/separation): rename `regular_space` to `t3_space` (#15169)
    I'm going to add a version of `regular_space` without `t0_space` and prove, e.g., that any uniform space is a regular space in this sense. To do this, I need to rename the existing `regular_space`.

--- src/analysis/locally_convex/balanced_core_hull.lean
+++ src/analysis/locally_convex/balanced_core_hull.lean
@@ -247,7 +247,7 @@ end
 
 variables (𝕜 E)
 
-lemma nhds_basis_closed_balanced [regular_space E] : (𝓝 (0 : E)).has_basis
+lemma nhds_basis_closed_balanced [t3_space E] : (𝓝 (0 : E)).has_basis
   (λ (s : set E), s ∈ 𝓝 (0 : E) ∧ is_closed s ∧ balanced 𝕜 s) id :=
 begin
   refine (closed_nhds_basis 0).to_has_basis (λ s hs, _) (λ s hs, ⟨s, ⟨hs.1, hs.2.1⟩, rfl.subset⟩),

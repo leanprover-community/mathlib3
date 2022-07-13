3ebc4de50c3e124be1a23375b4ba0ec428308a5b
adomani <adomani@gmail.com>
2022-07-13 06:28:34 +0200

    order/monoid lemmas

--- src/algebra/order/monoid.lean
+++ src/algebra/order/monoid.lean
@@ -1175,6 +1175,28 @@ lemma add_eq_coe : a + b = x ↔ ∃ (a' b' : α), ↑a' = a ∧ ↑b' = b ∧ a
 @[simp] lemma add_coe_eq_bot_iff : a + y = ⊥ ↔ a = ⊥ := with_top.add_coe_eq_top_iff
 @[simp] lemma coe_add_eq_bot_iff : ↑x + b = ⊥ ↔ b = ⊥ := with_top.coe_add_eq_top_iff
 
+/-  It is not `to_additive` of `with_bot.map_mul_of_mul_hom`, since `with_bot` does not have
+a multiplication. -/
+lemma map_add_of_add_hom {β F} [has_add β] [add_hom_class F α β] (f : F)
+  (a b : with_bot α) :
+  (a + b).map f = a.map f + b.map f :=
+begin
+  rcases a.eq_bot_or_coe with rfl | ⟨a, rfl⟩,
+  { exact (b.map f).bot_add.symm },
+  { rcases b.eq_bot_or_coe with rfl | ⟨b, rfl⟩,
+    { exact (add_bot _).symm },
+    { rw [map_coe, map_coe, ← coe_add (f a), ← map_add],
+      refl } },
+end
+
+lemma exists_eq_add {A} [add_comm_group A] (a : with_bot A) (b : A) :
+  ∃ (a' : with_bot A), a = a' + b :=
+begin
+  rcases a.eq_bot_or_coe with rfl | ⟨a, rfl⟩,
+  { exact ⟨_, (bot_add _).symm⟩ },
+  { exact ⟨(a - b : A), coe_eq_coe.mpr (sub_add_cancel a b).symm⟩ }
+end
+
 variables [preorder α]
 
 instance covariant_class_add_le [covariant_class α α (+) (≤)] :

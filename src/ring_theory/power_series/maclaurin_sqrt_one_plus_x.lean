914b6b410e493af8c2b6dc167f46c84d955d78bf
Tyler Raven Billingsley <trbillin@gmail.com>
2022-07-13 16:45:22 -0500

    Cos identity, Start on Maclaurin

--- src/ring_theory/power_series/maclaurin_sqrt_one_plus_x.lean
+++ src/ring_theory/power_series/maclaurin_sqrt_one_plus_x.lean
@@ -8,73 +8,7 @@ open finset nat
 
 namespace power_series
 
-#check exp
-#check exp ℂ
-
-
-
-#check rescale
-#check I
-#check I^2
-#check two_mul
-
-
-example : 2 • exp ℂ = exp ℂ + exp ℂ := two_smul ℕ (exp ℂ)
-
 variables (A A' : Type*) [field A]  [algebra ℚ A]
-
-example : cos ℂ = (2:ℂ)⁻¹ • (rescale I (exp ℂ) + rescale (-I) (exp ℂ)) :=
-begin
-  ext1 n,
-  rw [map_smul, map_add, coeff_rescale, coeff_rescale, neg_pow I, mul_assoc, ← one_add_mul],
-  cases nat.even_or_odd n with neven nodd,
-  { --Even
-    unfold even at neven,
-    cases neven with r hr,
-    rw hr,
-    rw ← bit0,
-    rw I_pow_bit0,
-    rw neg_pow_bit0,
-    rw one_pow,
-    rw one_add_one_eq_two,
-    rw smul_eq_mul,
-    rw ← mul_assoc,
-    rw inv_mul_cancel,
-    rw one_mul,
-    rw cos,
-    rw exp,
-    rw coeff_mk,
-    rw if_pos,
-    rw coeff_mk,
-    rw bit0_div_two,
-    rw ← mul_one_div,
-    rw map_mul,
-    rw map_pow,
-    rw map_neg,
-    rw map_one,
-    apply even_bit0,
-    norm_num,
-  },
-  {
-    --Odd
-    cases nodd with r hr,
-    rw hr,
-    rw two_mul,
-    rw ← bit0,
-    rw ← bit1,
-    rw neg_pow_bit1,
-    rw one_pow,
-    rw add_neg_self,
-    rw zero_mul,
-    rw smul_zero,
-    rw cos,
-    rw coeff_mk,
-    rw if_neg,
-    apply not_even_bit1,
-    }
-end
-
-
 
 def maclaurin_sqrt_one_add_x : power_series A :=
   mk $ λ n, algebra_map ℚ A ((-1)^(n+1)*(n+1)*(catalan n)*(4^n * (2*n-1))⁻¹)
@@ -165,16 +99,5 @@ begin
     exact two_ne_zero,
   },
 end
-
-example  (A: Type*) [field A] (a : A) (ha : a ≠ 0) :  a*a⁻¹ = 1 :=
-begin
-  --suggest,--library_search,
-end
-
-example  (A: Type*) [field A] [algebra ℚ A] (a : A) :  (1: A) ≠ 0 :=
-begin
-  refine one_ne_zero,
-end
-
 
 end power_series

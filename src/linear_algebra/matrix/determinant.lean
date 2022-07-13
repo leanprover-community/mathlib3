50cf8d71d2c3522489a25afeef33619171b6b198
Eric Wieser <wieser.eric@gmail.com>
2022-07-13 15:55:11 +0000

    missing import

--- src/linear_algebra/matrix/determinant.lean
+++ src/linear_algebra/matrix/determinant.lean
@@ -5,6 +5,7 @@ Authors: Kenny Lau, Chris Hughes, Tim Baanen
 -/
 import data.matrix.pequiv
 import data.matrix.block
+import data.matrix.notation
 import data.fintype.card
 import group_theory.perm.fin
 import group_theory.perm.sign

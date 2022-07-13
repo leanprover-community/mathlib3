fea6a863e5020d75b393288a3df44167fafd1302
adomani <adomani@gmail.com>
2022-07-13 17:27:27 +0200

    degree_in

--- src/algebra/monoid_algebra/degree.lean
+++ src/algebra/monoid_algebra/degree.lean
@@ -28,9 +28,41 @@ Currently, the only results are
 namespace add_monoid_algebra
 open_locale classical
 
-variables {R A : Type*} [semiring R]
+variables {R A B : Type*} [semiring R]
+
+section degree_in
+variables [semilattice_sup B] [order_bot B]
+
+/--  Let `R` be a semiring, let `A` be a `semilattice_sup`, and let `f : R[A]` be an
+an element of `add_monoid_algebra R A`.  The degree of `f` takes values in `with_bot A` and
+it is the supremum of the support of `f` or `⊥`, depending on whether `f` is non-zero or not.
+
+If `A` has a linear order, then this notion coincides with the usual one, using the maximum of
+the exponents. -/
+def degree_in (D : A → B) (f : add_monoid_algebra R A) : B :=
+f.support.sup D
+
+variables [has_add A]
+variables [has_add B] [covariant_class B B (+) (≤)] [covariant_class B B (function.swap (+)) (≤)]
+
+lemma degree_in_mul_le (D : A → B) (Dm : ∀ {a b}, D (a + b) ≤ D a + D b)
+  (f g : add_monoid_algebra R A) :
+  (f * g).degree_in D ≤ f.degree_in D + g.degree_in D :=
+begin
+  refine (finset.sup_le (λ d ds, _)),
+  obtain ⟨a, af, b, bg, rfl⟩ : ∃ a, a ∈ f.support ∧ ∃ b, b ∈ g.support ∧ d = a + b,
+  { simpa only [finset.mem_bUnion, finset.mem_singleton, exists_prop] using f.support_mul g ds },
+  refine Dm.trans (add_le_add _ _);
+  exact finset.le_sup ‹_›,
+end
+
+end degree_in
+
+/-  This section is here only to show the relationship to the "standard" degree. -/
+section old_degrees
 
 section degree
+
 variables [semilattice_sup A]
 
 /--  Let `R` be a semiring, let `A` be a `semilattice_sup`, and let `f : R[A]` be an
@@ -54,6 +86,15 @@ begin
   exact finset.le_sup ‹_›,
 end
 
+variables [semilattice_sup A] [add_monoid A] [covariant_class A A (+) (≤)]
+  [covariant_class A A (function.swap (+)) (≤)] (f g : add_monoid_algebra R A)
+
+example : f.degree_in (coe : A → with_bot A) = f.degree :=
+rfl
+
+example : (f * g).degree ≤ f.degree + g.degree :=
+degree_in_mul_le (coe : A → with_bot A) (λ a b, (with_bot.coe_add _ _).le) f g
+
 end degree
 
 section trailing_degree
@@ -76,5 +117,7 @@ lemma le_trailing_degree_mul (f g : add_monoid_algebra R A) :
 @degree_mul_le _ Aᵒᵈ _ _ _ _ _ _ _
 
 end trailing_degree
+
+end old_degrees
 
 end add_monoid_algebra

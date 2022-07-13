9a7808bc816333720e93ceefe04926e7bcee53a5
Rob Lewis <rob.y.lewis@gmail.com>
2022-07-13 11:40:55 -0400

    helper update

--- scripts/polyrith_sage_helper.py
+++ scripts/polyrith_sage_helper.py
@@ -1,5 +1,9 @@
 # this file will be run by the remote sage server, so should not import local files.
 from functools import reduce
+from typing import Optional
+import dataclasses
+
+from attr import dataclass
 from sage.rings.polynomial.polydict import ETuple
 
 def mk_app(*args: str) -> str:
@@ -15,20 +19,47 @@ def power_to_string(var: int, pow: int) -> str:
         return var_s
     return mk_app("poly.pow", var_s, str(pow))
 
-def sum_to_string(terms: list[str]) -> str:
-    terms = iter(terms)
+def combine_terms(s: str, t: str) -> str:
+    print(s, t)
+    if t[0] == '-':
+        return mk_app("poly.sub", s, t[1:])
+    else:
+        return mk_app("poly.add", s, t)
+
+@dataclass
+class MonomForm:
+    pos_form: str
+    neg_form: Optional[str] = None
+
+def sum_to_string_aux(old: str, rest: list[MonomForm]) -> str:
+    try:
+        nxt = next(rest)
+    except StopIteration:
+        return old
+    s = mk_app("poly.sub" if nxt is not None else "poly.add", old, nxt.pos_form)
+    return sum_to_string_aux(s, rest)
+
+def sum_to_string(terms: list[MonomForm]) -> str:
     try:
         first = next(terms)
     except StopIteration:
         return const_to_string(QQ(0))
     else:
-        return reduce(lambda s, t: mk_app("poly.add", s, t), terms, first)
+        first_form = first.neg_form if first.neg_form is not None else first.pos_form
+        return sum_to_string_aux(first_form, terms)
 
-def monomial_to_string(etuple: ETuple, coeff: QQ) -> str:
-    return reduce(
+
+def monomial_to_string(etuple: ETuple, coeff: QQ) -> MonomForm:
+    pos_form = reduce(
         lambda s, t: mk_app("poly.mul", s, power_to_string(t[0], t[1])),
         etuple.sparse_iter(),
-        const_to_string(coeff))
+        const_to_string(abs(coeff)))
+    neg_form = reduce(
+        lambda s, t: mk_app("poly.mul", s, power_to_string(t[0], t[1])),
+        etuple.sparse_iter(),
+        const_to_string(coeff))if coeff < 0 else None
+    return MonomForm(pos_form, neg_form)
+
 
 def polynomial_to_string(p) -> str:
     return sum_to_string(monomial_to_string(pows, coeff) for pows, coeff in p.dict().items())

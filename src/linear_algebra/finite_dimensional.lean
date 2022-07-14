b2ad495e73e41af8ae859c338a6b9083daad381e
Daniel-Packer <62404584+Daniel-Packer@users.noreply.github.com>
2022-07-14 12:12:01 -0400

    fixed proof of linear_independent.finite

--- src/linear_algebra/finite_dimensional.lean
+++ src/linear_algebra/finite_dimensional.lean
@@ -317,7 +317,7 @@ end
 lemma _root_.linear_independent.finite {K : Type*} {V : Type*} [division_ring K] [add_comm_group V]
   [module K V] [finite_dimensional K V] {b : set V} (h : linear_independent K (λ (x:b), (x:V))) :
   b.finite :=
-cardinal.lt_omega_iff_finite.mp (finite_dimensional.lt_omega_of_linear_independent h)
+cardinal.lt_aleph_0_iff_set_finite.mp (finite_dimensional.lt_aleph_0_of_linear_independent h)
 
 lemma not_linear_independent_of_infinite {ι : Type w} [inf : infinite ι] [finite_dimensional K V]
   (v : ι → V) : ¬ linear_independent K v :=

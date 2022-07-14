1410c4d49bb23c00233d9c2ce7d5180e9e75c411
Jujian Zhang <jujian.zhang1998@outlook.com>
2022-07-14 19:56:12 +0100

    give me cache

--- src/group_theory/divisible.lean
+++ src/group_theory/divisible.lean
@@ -62,66 +62,66 @@ TODO: Show that divisibility implies injectivity in the category of `AddCommGrou
 
 open_locale pointwise
 
-namespace add_monoid
+-- namespace add_monoid
 
-variables (A α : Type*) [add_monoid A] [has_scalar α A] [has_zero α]
+-- variables (A α : Type*) [add_monoid A] [has_scalar α A] [has_zero α]
 
-/--
-An `add_monoid A` is `α`-divisible iff `n • x = a` has a solution for all `n ≠ 0 ∈ α` and `a ∈ A`.
-Here we adpot a constructive approach where we ask an explicit `div : A → α → A` function such that
-* `div a 0 = 0` for all `a ∈ A`
-* `n • div a n = a` for all `n ≠ 0 ∈ α` and `a ∈ A`.
--/
-class divisible_by :=
-(div : A → α → A)
-(div_zero : ∀ a, div a 0 = 0)
-(div_cancel : ∀ {n : α} (a : A), n ≠ 0 → n • (div a n) = a)
+-- /--
+-- An `add_monoid A` is `α`-divisible iff `n • x = a` has a solution for all `n ≠ 0 ∈ α` and `a ∈ A`.
+-- Here we adpot a constructive approach where we ask an explicit `div : A → α → A` function such that
+-- * `div a 0 = 0` for all `a ∈ A`
+-- * `n • div a n = a` for all `n ≠ 0 ∈ α` and `a ∈ A`.
+-- -/
+-- class divisible_by :=
+-- (div : A → α → A)
+-- (div_zero : ∀ a, div a 0 = 0)
+-- (div_cancel : ∀ {n : α} (a : A), n ≠ 0 → n • (div a n) = a)
 
-lemma smul_surj_of_divisible_by [divisible_by A α] {n : α} (hn : n ≠ 0) :
-  function.surjective ((•) n : A → A) :=
-λ x, ⟨divisible_by.div x n, divisible_by.div_cancel _ hn⟩
+-- lemma smul_surj_of_divisible_by [divisible_by A α] {n : α} (hn : n ≠ 0) :
+--   function.surjective ((•) n : A → A) :=
+-- λ x, ⟨divisible_by.div x n, divisible_by.div_cancel _ hn⟩
 
-/--
-An `add_monoid A` is `α`-divisible iff `n • _` is a surjective function, i.e. the constructive
-version implies the textbook approach.
--/
-noncomputable def divisible_by_of_smul_surj
-  [Π (n : α), decidable (n = 0)]
-  (H : ∀ {n : α}, n ≠ 0 → function.surjective ((•) n : A → A)) :
-  divisible_by A α :=
-{ div := λ a n, dite (n = 0) (λ _, 0) (λ hn, (H hn a).some),
-  div_zero := λ _, dif_pos rfl,
-  div_cancel := λ n a hn, by rw [dif_neg hn, (H hn a).some_spec] }
+-- /--
+-- An `add_monoid A` is `α`-divisible iff `n • _` is a surjective function, i.e. the constructive
+-- version implies the textbook approach.
+-- -/
+-- noncomputable def divisible_by_of_smul_surj
+--   [Π (n : α), decidable (n = 0)]
+--   (H : ∀ {n : α}, n ≠ 0 → function.surjective ((•) n : A → A)) :
+--   divisible_by A α :=
+-- { div := λ a n, dite (n = 0) (λ _, 0) (λ hn, (H hn a).some),
+--   div_zero := λ _, dif_pos rfl,
+--   div_cancel := λ n a hn, by rw [dif_neg hn, (H hn a).some_spec] }
 
-section pi
+-- section pi
 
-variables {ι β : Type*} (B : ι → Type*) [Π (i : ι), has_scalar β (B i)]
+-- variables {ι β : Type*} (B : ι → Type*) [Π (i : ι), has_scalar β (B i)]
 
-instance has_scalar_pi : has_scalar β (Π i, B i) :=
-{ smul := λ n x i, n • (x i) }
+-- instance has_scalar_pi : has_scalar β (Π i, B i) :=
+-- { smul := λ n x i, n • (x i) }
 
-variables [has_zero β] [Π (i : ι), add_monoid (B i)] [Π i, divisible_by (B i) β]
+-- variables [has_zero β] [Π (i : ι), add_monoid (B i)] [Π i, divisible_by (B i) β]
 
-instance divsible_by_pi : divisible_by (Π i, B i) β :=
-{ div := λ x n i, (divisible_by.div (x i) n),
-  div_zero := λ x, funext $ λ i, divisible_by.div_zero _,
-  div_cancel := λ n x hn, funext $ λ i, divisible_by.div_cancel _ hn }
+-- instance divsible_by_pi : divisible_by (Π i, B i) β :=
+-- { div := λ x n i, (divisible_by.div (x i) n),
+--   div_zero := λ x, funext $ λ i, divisible_by.div_zero _,
+--   div_cancel := λ n x hn, funext $ λ i, divisible_by.div_cancel _ hn }
 
-end pi
+-- end pi
 
-section prod
+-- section prod
 
-variables {β B B' : Type*} [has_zero β] [add_monoid B] [add_monoid B']
-variables [has_scalar β B] [has_scalar β B'] [divisible_by B β] [divisible_by B' β]
+-- variables {β B B' : Type*} [has_zero β] [add_monoid B] [add_monoid B']
+-- variables [has_scalar β B] [has_scalar β B'] [divisible_by B β] [divisible_by B' β]
 
-instance divisible_by_prod : divisible_by (B × B') β :=
-{ div := λ p n, (divisible_by.div p.1 n, divisible_by.div p.2 n),
-  div_zero := λ p, prod.ext (divisible_by.div_zero _) (divisible_by.div_zero _),
-  div_cancel := λ n p hn, prod.ext (divisible_by.div_cancel _ hn) (divisible_by.div_cancel _ hn) }
+-- instance divisible_by_prod : divisible_by (B × B') β :=
+-- { div := λ p n, (divisible_by.div p.1 n, divisible_by.div p.2 n),
+--   div_zero := λ p, prod.ext (divisible_by.div_zero _) (divisible_by.div_zero _),
+--   div_cancel := λ n p hn, prod.ext (divisible_by.div_cancel _ hn) (divisible_by.div_cancel _ hn) }
 
-end prod
+-- end prod
 
-end add_monoid
+-- end add_monoid
 
 namespace monoid
 
@@ -133,16 +133,16 @@ Here we adpot a constructive approach where we ask an explicit `root : A → α 
 * `root a 0 = 1` for all `a ∈ A`
 * `(root a n)ⁿ = a` for all `n ≠ 0 ∈ α` and `a ∈ A`.
 -/
-@[to_additive add_monoid.divisible_by]
+@[to_additive]
 class rootable_by :=
 (root : A → α → A)
 (root_zero : ∀ a, root a 0 = 1)
-(root_pow : ∀ {n : α} (a : A), n ≠ 0 → (root a n)^n = a)
+(root_cancel : ∀ {n : α} (a : A), n ≠ 0 → (root a n)^n = a)
 
 @[to_additive add_monoid.smul_surj_of_divisible_by]
 lemma pow_surj_of_rootable_by [rootable_by A α] {n : α} (hn : n ≠ 0) :
   function.surjective ((flip (^)) n : A → A) :=
-λ x, ⟨rootable_by.root x n, rootable_by.root_pow _ hn⟩
+λ x, ⟨rootable_by.root x n, rootable_by.root_cancel _ hn⟩
 
 /--
 A `monoid A` is `α`-rootable iff the `pow _ n` function is surjective, i.e. the constructive version
@@ -155,7 +155,7 @@ noncomputable def rootable_by_of_pow_surj
 rootable_by A α :=
 { root := λ a n, dite (n = 0) (λ _, 1) (λ hn, (H hn a).some),
   root_zero := λ _, dif_pos rfl,
-  root_pow := λ n a hn, by rw dif_neg hn; exact (H hn a).some_spec }
+  root_cancel := λ n a hn, by rw dif_neg hn; exact (H hn a).some_spec }
 
 section pi
 

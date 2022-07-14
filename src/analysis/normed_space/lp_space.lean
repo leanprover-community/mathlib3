18bdbcd45d6ca763ebe4a0703053a68b82d0d832
JonBannon <59937998+JonBannon@users.noreply.github.com>
2022-07-14 09:04:33 -0400

    Update src/analysis/normed_space/lp_space.lean
    style
    
    Co-authored-by: mcdoll <moritz.doll@googlemail.com>

--- src/analysis/normed_space/lp_space.lean
+++ src/analysis/normed_space/lp_space.lean
@@ -744,7 +744,7 @@ instance [∀ i, cstar_ring (B i)] : cstar_ring (lp B ∞) :=
     { rw [←sq, ←real.le_sqrt (norm_nonneg _) (norm_nonneg _)],
       refine lp.norm_le_of_forall_le (∥star f * f∥.sqrt_nonneg) (λ i, _),
       rw [real.le_sqrt (norm_nonneg _) (norm_nonneg _), sq, ←cstar_ring.norm_star_mul_self],
-      exact lp.norm_apply_le_norm ennreal.top_ne_zero ((star f) * f) i,}
+      exact lp.norm_apply_le_norm ennreal.top_ne_zero (star f * f) i, }
   end }
 
 end star_ring

4d343cba688bacd0ece236a1009524ff1623d32d
Michael Stoll <Michael.Stoll@uni-bayreuth.de>
2022-07-14 22:23:25 +0200

    feat(number_theory/legendre_symbol/*): redefine quadratic characters as mul_chars

--- src/number_theory/legendre_symbol/quadratic_reciprocity.lean
+++ src/number_theory/legendre_symbol/quadratic_reciprocity.lean
@@ -117,7 +117,7 @@ lemma legendre_sym_eq_pow (p : ℕ) (a : ℤ) [hp : fact p.prime] :
 begin
   rw legendre_sym,
   by_cases ha : (a : zmod p) = 0,
-  { simp only [ha, zero_pow (nat.div_pos (hp.1.two_le) (succ_pos 1)), quadratic_char_zero,
+  { simp only [ha, zero_pow (nat.div_pos (hp.1.two_le) (succ_pos 1)), mul_char.map_zero,
                int.cast_zero], },
   by_cases hp₁ : p = 2,
   { substI p,
@@ -146,10 +146,10 @@ lemma legendre_sym_eq_zero_iff (p : ℕ) [fact p.prime] (a : ℤ) :
 quadratic_char_eq_zero_iff a
 
 @[simp] lemma legendre_sym_zero (p : ℕ) [fact p.prime] : legendre_sym p 0 = 0 :=
-by rw [legendre_sym, int.cast_zero, quadratic_char_zero]
+by rw [legendre_sym, int.cast_zero, mul_char.map_zero]
 
 @[simp] lemma legendre_sym_one (p : ℕ) [fact p.prime] : legendre_sym p 1 = 1 :=
-by rw [legendre_sym, int.cast_one, quadratic_char_one]
+by rw [legendre_sym, int.cast_one, mul_char.map_one]
 
 /-- The Legendre symbol is multiplicative in `a` for `p` fixed. -/
 lemma legendre_sym_mul (p : ℕ) [fact p.prime] (a b : ℤ) :

cc5d22380d3962bc2559dec2d992820ed9c92f09
David Kurniadi Angdinata <dka31@cantab.ac.uk>
2022-07-16 00:18:20 +0100

    Add fintype_of_fg_torsion

--- src/group_theory/finite_abelian.lean
+++ src/group_theory/finite_abelian.lean
@@ -3,8 +3,8 @@ Copyright (c) 2022 Pierre-Alexandre Bazin. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Pierre-Alexandre Bazin
 -/
-import data.zmod.quotient
 import algebra.module.pid
+import data.zmod.quotient
 
 /-!
 # Structure of finite(ly generated) abelian groups
@@ -17,8 +17,28 @@ import algebra.module.pid
 
 -/
 
-
 open_locale direct_sum
+
+namespace module
+
+open_locale classical
+
+/-- A finitely generated torsion `ℤ`-module is finite. -/
+noncomputable def fintype_of_fg_torsion (G : Type*) [add_comm_group G] [module ℤ G]
+  [module.finite ℤ G] (hit : module.is_torsion ℤ G) : fintype G :=
+let hG := (module.equiv_direct_sum_of_is_torsion hit).some_spec in
+@fintype.of_equiv _ _
+  (@fintype.of_equiv _ _
+    (@pi.fintype _ _ _ hG.some $ λ i,
+      @fintype.of_equiv _ _
+        (@zmod.fintype _
+          ⟨int.nat_abs_pos_of_ne_zero $ pow_ne_zero (hG.some_spec.some_spec.some_spec.some i) $
+            irreducible.ne_zero $ hG.some_spec.some_spec.some i⟩)
+        (int.quotient_span_equiv_zmod _).symm.to_equiv)
+    (@dfinsupp.equiv_fun_on_fintype _ _ _ hG.some).symm)
+  hG.some_spec.some_spec.some_spec.some_spec.some.symm.to_equiv
+
+end module
 
 namespace add_comm_group
 
@@ -52,4 +72,21 @@ begin
       λ a, ⟨finsupp.single 0 a, finsupp.single_eq_same⟩).false.elim }
 end
 
+/-- A finitely generated torsion abelian additive group is finite. -/
+noncomputable def fintype_of_fg_torsion (G : Type*) [add_comm_group G] [hfg : add_group.fg G]
+  (hit : add_monoid.is_torsion G) : fintype G :=
+@module.fintype_of_fg_torsion _ _ _
+  (module.finite.iff_add_group_fg.mpr $ add_group.fg_iff_add_monoid.fg.mpr $
+    add_group.fg_iff_add_monoid.fg.mp hfg) $
+  add_monoid.is_torsion_iff_is_torsion_int.mp hit
+
 end add_comm_group
+
+namespace comm_group
+
+/-- A finitely generated torsion abelian multiplicative group is finite. -/
+noncomputable def fintype_of_fg_torsion (G : Type*) [comm_group G] [hfg : group.fg G]
+  (hit : monoid.is_torsion G) : fintype G :=
+@fintype.of_equiv _ _ (add_comm_group.fintype_of_fg_torsion (additive G) hit) multiplicative.of_add
+
+end comm_group

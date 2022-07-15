54786dffc15a11e75ef68c0ebc71f32208b8fa3a
Philip Wood <philipwood@Melanies-MacBook-Pro.local>
2022-07-14 21:38:07 -0400

    Catalan descent lemma proved

--- src/ring_theory/power_series/phil-catalan.lean
+++ src/ring_theory/power_series/phil-catalan.lean
@@ -225,11 +225,268 @@ begin
     --apply catalan_succ,
     rw [catalan],
     simp only [cast_sum, cast_mul, succ_sub_succ_eq_sub],
-    rw sum_range,--Holy cow, why did this work???
+    rw sum_range,--switiches between types of sums
   },
 end
-#check square_pow_series_catalan
+#check square_pow_series_catalan.
 
+lemma catalan_descent_lemma (n : ℕ ) :
+(((n.succ) + 1) * ((catalan n.succ) * (1 / (4 ^ n.succ * (2 * (n.succ) - 1)))):ℚ)
+=
+2 *(catalan n)/(4^n.succ) :=
+--old--  ((n.succ + 1) * (catalan n) / (2*n -1): ℚ) = 2 * (catalan (n-1)) :=
+--((n.succ) + 1) * (catalan n.succ)
+begin
+  have h : n.succ ≤ 2* n.succ,
+  {
+    rw two_mul,
+    linarith,
+  },
+   have two_ns_minus_ns: 2*n.succ-n.succ = n.succ,
+  {
+      simp only [two_mul,add_tsub_cancel_left],
+  },
+  have h2 : (2*n.succ -1:ℚ) = 2*n +1,
+  {
+    simp only [cast_succ],
+    rw two_mul,
+    linarith,
+  },
+  have h3 : (2*n.succ) = (2*n).succ.succ,
+  {
+    rw succ_eq_add_one,
+    rw succ_eq_add_one,
+    rw succ_eq_add_one,
+    rw two_mul,
+    rw two_mul,
+    linarith,
+  },
+  rw h2,
+
+  rw catalan_eq_central_binom_div,
+  rw nat.cast_div (succ n).succ_dvd_central_binom,
+  rw nat.cast_add,
+  rw nat.cast_one,
+  rw central_binom,
+  rw choose_eq_factorial_div_factorial h,
+  rw nat.cast_div (factorial_mul_factorial_dvd_factorial h),
+  rw two_ns_minus_ns,
+  rw nat.cast_mul,
+  rw h3,
+  rw factorial,
+  rw factorial,
+  rw factorial,
+/-  have two_ns_pos : 0< 2*n.succ,
+  {
+    rw two_mul,
+    rw add_succ,
+    exact succ_pos (n.succ+ n),
+  },
+  rw ← mul_factorial_pred (two_ns_pos),
+  rw nat.div_mul_cancel,-/
+
+  --push casts
+  rw nat.cast_mul,
+  rw nat.cast_mul,
+  rw nat.cast_mul,
+
+  --get rid of succs
+  rw succ_eq_add_one,
+  rw succ_eq_add_one,
+  rw succ_eq_add_one,
+
+  rw [one_div,mul_inv],
+  simp only [division_def],
+  --cancel the n+1+1 term
+  nth_rewrite 2 mul_comm,
+  rw [← mul_assoc, ← mul_assoc, ← mul_assoc],
+  rw mul_inv_cancel,
+  rw one_mul,
+  rw [mul_comm,←mul_assoc,← mul_assoc],
+  nth_rewrite 4 mul_comm,
+  rw [← mul_assoc,← mul_assoc],
+  nth_rewrite 4 mul_comm,
+  rw nat.cast_add,
+  rw nat.cast_mul,
+  rw nat.cast_two,
+  rw nat.cast_one,
+  rw mul_inv_cancel,
+  rw one_mul,
+  have h4 : 2* n +1+1 = 2*(n+1),
+  {
+    rw [two_mul, two_mul],
+    linarith,
+  },
+  rw h4,
+  rw nat.cast_mul,
+  rw mul_inv,
+  rw mul_inv,
+  nth_rewrite 1 mul_comm,
+  rw [← mul_assoc,← mul_assoc],
+  nth_rewrite 1 mul_comm,
+  rw [← mul_assoc,← mul_assoc,← mul_assoc,← mul_assoc,← mul_assoc],
+  rw mul_inv_cancel,
+  rw one_mul,
+  nth_rewrite 2 mul_comm,
+  nth_rewrite 4 mul_comm,
+  nth_rewrite 1 ← mul_assoc,
+  rw ← mul_assoc,
+  rw ← mul_inv,
+  nth_rewrite 2 ← division_def,
+  have h5 : n = 2*n - n,
+  {
+    simp only [two_mul, add_tsub_cancel_left],
+  },
+  nth_rewrite 2 h5,
+  have h6 : n ≤ 2*n,
+  {
+    simp only [two_mul, le_add_iff_nonneg_left, zero_le'],
+  },
+  rw ← nat.cast_mul,
+  rw ← nat.cast_div (factorial_mul_factorial_dvd_factorial h6),
+  rw ← choose_eq_factorial_div_factorial h6,
+  nth_rewrite 1 ← division_def,
+  rw ← central_binom,
+  rw ← nat.cast_div,--need to use that catalan is an integer; done later
+  rw ← catalan_eq_central_binom_div,
+  simp only [cast_bit0, cast_one, mul_eq_mul_right_iff, inv_eq_zero, pow_eq_zero_iff, succ_pos', bit0_eq_zero, one_ne_zero,
+  or_false],
+  left,
+  rw mul_comm,
+
+  ---Now entering the easy goals spawned by the above---
+  --showing n+1 divides 2n choose n; a fact about the Catalan Numbers!
+  {
+    rw ← succ_mul_catalan_eq_central_binom,
+    simp only [dvd_mul_right],
+  },
+  {
+    have h8 : 0≤ (n:ℚ), apply cast_nonneg,
+    linarith,
+  },
+  {
+    simp [mono_cast],
+    intro silly,
+    have h8 : 0< n.factorial, apply factorial_pos,
+    cases silly with wrong wronger,
+    linarith,
+    have h9 : 0 <  (2*n -n).factorial, apply factorial_pos,
+    linarith,
+  },
+  {
+    have h8 : 0≤ (n:ℚ), apply cast_nonneg,
+    linarith,
+  },
+  {
+    have h8 : 0≤ (n:ℚ), apply cast_nonneg,
+    linarith,
+  },
+  {
+    have h8 : 0≤ (n:ℚ), apply cast_nonneg,
+    linarith,
+  },
+  {
+    simp [mono_cast],
+    intro silly,
+    have h8 : 0< n.factorial, apply factorial_pos,
+    cases silly with wrong wronger,
+    cases wrong,
+    {
+      have h9 : 0≤ (n:ℚ), apply cast_nonneg,
+      linarith,
+    },
+    linarith,
+    have h9 : 0 <  (2*n.succ -n.succ).factorial, apply factorial_pos,
+    linarith,
+  },
+  {
+    have h8 : 0≤ (n:ℚ),
+    {
+      apply cast_nonneg,
+    },
+    rw nat.cast_add,
+    rw succ_eq_add_one,
+    rw nat.cast_add,
+    simp only [nat.cast_one],
+    intro itsfalse,
+    linarith,
+  },
+end
+--End of Catalan Descent.  Holy Cow!!
+
+example (a b : ℚ) (h: b≠ 0): a/b = a *b⁻¹:=
+begin
+exact division_def a b,
+end
+
+example (a b :ℕ) (h: 0 < b): b*a/b = a :=
+begin
+  exact mul_div_right a h,
+end
+
+--TODO: figure out whether we want the smul version like this, or the one below
+lemma before_squaring (A: Type*) [field A] [algebra ℚ A] :
+maclaurin_sqrt_one_add_x A = (1/(2:ℚ))•X*(rescale (-4⁻¹) (catalan_power_series A)) + 1 :=
+sorry
+
+lemma before_squaring (A: Type*) [field A] [algebra ℚ A] :
+maclaurin_sqrt_one_add_x A = X*2⁻¹*(rescale (-4⁻¹) (catalan_power_series A)) + 1 :=
+begin
+  ext,
+  cases n,
+  {--n equals zero case
+    sorry,
+  },
+  {-- n.succ case
+    rw succ_eq_add_one,
+    rw mul_assoc,
+    rw [maclaurin_sqrt_one_add_x, coeff_mk],
+    rw [mul_assoc, mul_assoc],
+    --rw mul_inv_rev,
+    --rw ← inv_div_left,
+    rw ← one_div,
+    rw catalan_descent_lemma,
+    rw map_add,
+    rw coeff_succ_X_mul,
+    --rw ← one_div,
+    --Wow--2⁻¹ is actually a _power_ series.  That is weird.
+    rw coeff_mul,
+    rw nat.sum_antidiagonal_eq_sum_range_succ_mk,
+
+
+    --rw map_mul,
+    --rw map_pow,
+    simp only [map_mul, map_pow, map_neg, map_one,
+          coeff_one, succ_ne_zero, if_false, add_zero],
+    --rw catalan_power_series,
+    --rw ← smul_eq_mul,
+    --rw ← map_smul,
+    rw coeff_rescale,
+    rw coeff_mk,
+    rw map_smul,
+
+
+    --rw ← eq_X_mul_shift_add_const,
+    rw coeff_rescale,
+
+
+    rw catalan_power_series,
+
+
+    --rw ← mul_assoc (↑(n.succ) + 1),--why did this not work?
+    --nth_rewrite 1 ← mul_assoc,
+   -- simp_rw catalan_eq_central_binom_div,
+
+
+
+  },
+
+  nth_rewrite 1 ← mul_assoc,
+  rw succ_mul_central_binom_succ,
+
+  --rw succ_mul_catalan_eq_central_binom,
+
+end
 
 example (n :ℕ ) (h: n ≥ 1): ∃ m, n = succ m :=
 begin
@@ -251,23 +508,6 @@ begin
   exact h,
 end
 
-/- Kaenchin Fodder for the proof above
-    rw coeff_mul,
-    rw nat.sum_antidiagonal_eq_sum_range_succ_mk,
-    rw sum_range_succ,
-    rw sum_range_one,
-    --rw coeff_mk,
-    --squeeze_simp [],
-    simp only [coeff_mk, maclaurin_sqrt_one_add_x, tsub_zero,
-      coeff_one, nat.one_ne_zero, if_false, coeff_one_X, zero_add],
-    simp only [map_add, coeff_one, coeff_X],
-    simp only [←map_mul, ←map_add],
-    rw [if_neg, if_pos, zero_add, ←map_one (algebra_map ℚ A),
-        (algebra_map ℚ A).injective.eq_iff],
-    norm_num,
-    refl,
-    exact one_ne_zero,
--/
 
 
 #check nat.eq_zero_or_pos.

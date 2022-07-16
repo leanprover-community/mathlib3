215db4447aedf86957432f6edf6199ccd2934e1b
Yaël Dillies <yael.dillies@gmail.com>
2022-07-16 20:02:19 +0000

    rename variables

--- src/group_theory/group_action/option.lean
+++ src/group_theory/group_action/option.lean
@@ -19,35 +19,35 @@ multiplication is defined by `a • some b = some (a • b)` and `a • none = n
 * `group_theory.group_action.sum`
 -/
 
-variables {M N P α β γ : Type*}
+variables {M N α : Type*}
 
 namespace option
 
 section has_smul
-variables [has_smul α β] [has_smul α γ] [has_smul β γ] (a : α) (b : β) (x : option β)
+variables [has_smul M α] [has_smul N α] (a : M) (b : α) (x : option α)
 
-@[to_additive option.has_vadd] instance : has_smul α (option β) := ⟨λ a, option.map $ (•) a⟩
+@[to_additive option.has_vadd] instance : has_smul M (option α) := ⟨λ a, option.map $ (•) a⟩
 
 @[to_additive] lemma smul_def : a • x = x.map ((•) a) := rfl
-@[simp, to_additive] lemma smul_none : a • (none : option β) = none := rfl
+@[simp, to_additive] lemma smul_none : a • (none : option α) = none := rfl
 @[simp, to_additive] lemma smul_some : a • some b = some (a • b) := rfl
 
-instance [is_scalar_tower α β γ] : is_scalar_tower α β (option γ) :=
+instance [has_smul M N] [is_scalar_tower M N α] : is_scalar_tower M N (option α) :=
 ⟨λ a b x, by { cases x, exacts [rfl, congr_arg some (smul_assoc _ _ _)] }⟩
 
-@[to_additive] instance {α β γ : Type*} [has_smul α γ] [has_smul β γ] [smul_comm_class α β γ] :
-  smul_comm_class α β (option γ) :=
+@[to_additive] instance {M N α : Type*} [has_smul M α] [has_smul N α] [smul_comm_class M N α] :
+  smul_comm_class M N (option α) :=
 ⟨λ a b, function.commute.option_map $ smul_comm _ _⟩
 
-instance [has_smul αᵐᵒᵖ β] [is_central_scalar α β] : is_central_scalar α (option β) :=
+instance [has_smul Mᵐᵒᵖ α] [is_central_scalar M α] : is_central_scalar M (option α) :=
 ⟨λ a x, by { cases x, exacts [rfl, congr_arg some (op_smul_eq_smul _ _)] }⟩
 
-@[to_additive] instance [has_faithful_smul α β] : has_faithful_smul α (option β) :=
-⟨λ x y h, eq_of_smul_eq_smul $ λ b : β, by injection h (some b)⟩
+@[to_additive] instance [has_faithful_smul M α] : has_faithful_smul M (option α) :=
+⟨λ x y h, eq_of_smul_eq_smul $ λ b : α, by injection h (some b)⟩
 
 end has_smul
 
-instance [monoid α] [mul_action α β] : mul_action α (option β) :=
+instance [monoid M] [mul_action M α] : mul_action M (option α) :=
 { smul := (•),
   one_smul := λ b, by { cases b, exacts [rfl, congr_arg some (one_smul _ _)] },
   mul_smul := λ a₁ a₂ b, by { cases b, exacts [rfl, congr_arg some (mul_smul _ _ _)] } }

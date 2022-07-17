a3c2b48b950859462b96a55370afe6bce115abbb
Yury G. Kudryashov <urkud@urkud.name>
2022-07-16 23:02:22 -0400

    Fix

--- src/analysis/convex/topology.lean
+++ src/analysis/convex/topology.lean
@@ -241,9 +241,9 @@ begin
   have hne : t ≠ 0, from (one_pos.trans ht).ne',
   refine ⟨homothety x t⁻¹ y, hs.open_segment_interior_closure_subset_interior hx hy _,
     (affine_equiv.homothety_units_mul_hom x (units.mk0 t hne)).apply_symm_apply y⟩,
-  rw [open_segment_eq_image_line_map, ← inv_one, ← inv_Ioi (@one_pos ℝ _ _), ← image_inv,
-    image_image, homothety_eq_line_map],
-  exact mem_image_of_mem _ ht
+  rw [open_segment_eq_image_line_map, ← inv_one, ← inv_Ioi, ← image_inv, image_image,
+    homothety_eq_line_map],
+  exacts [mem_image_of_mem _ ht, one_pos]
 end
 
 /-- If we dilate a convex set about a point in its interior by a scale `t > 1`, the interior of

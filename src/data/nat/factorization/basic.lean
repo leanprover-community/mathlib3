11bf3fe7be5a0bb3384cab5bdd2ed125f0316d24
Bolton Bailey <bolton.bailey@gmail.com>
2022-07-18 16:55:21 -0400

    Update src/data/nat/factorization/basic.lean
    Co-authored-by: Anne Baanen <Vierkantor@users.noreply.github.com>

--- src/data/nat/factorization/basic.lean
+++ src/data/nat/factorization/basic.lean
@@ -3,9 +3,9 @@ Copyright (c) 2021 Stuart Presnell. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Stuart Presnell
 -/
-import data.nat.prime
-import data.finsupp.multiset
 import algebra.big_operators.finsupp
+import data.finsupp.multiset
+import data.nat.prime
 import number_theory.padics.padic_val
 
 /-!

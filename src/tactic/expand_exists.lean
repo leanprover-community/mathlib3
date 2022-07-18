73fe41ebce2b549ed27aa23995822b4440c42cb3
0x182d4454fb211940 <ianwoodformal@gmail.com>
2022-07-18 23:48:55 +0100

    style(tactic/expand_exists): remove unnecessary imports

--- src/tactic/expand_exists.lean
+++ src/tactic/expand_exists.lean
@@ -3,9 +3,7 @@ Copyright (c) 2022 Ian Wood. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Ian Wood
 -/
-import init.meta.declaration
 import meta.expr
-import init.meta.lean.parser
 
 /-!
 # `expand_exists`

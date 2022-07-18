2a50e61888ed9b2eea451f6bab17f4ba11601b44
Eric Wieser <wieser.eric@gmail.com>
2022-07-18 20:14:26 +0100

    Update src/data/pi/algebra.lean

--- src/data/pi/algebra.lean
+++ src/data/pi/algebra.lean
@@ -84,7 +84,7 @@ instance has_mul [∀ i, has_mul $ f i] :
 @[simp, to_additive] lemma const_div [has_div β] (a b : β) :
   const α a / const α b = const α (a / b) := rfl
 
-@[to_additive] instance has_pow {β : Type*} [Π i, has_pow (f i) β] : has_pow (Π i, f i) β :=
+@[to_additive has_smul] instance has_pow {β : Type*} [Π i, has_pow (f i) β] : has_pow (Π i, f i) β :=
 { pow := λ x b i, (x i) ^ b }
 
 @[to_additive, simp] lemma pow_def {β : Type*} [Π i, has_pow (f i) β]

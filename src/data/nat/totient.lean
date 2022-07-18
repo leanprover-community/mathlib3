b5cc6f5b8ce9e70a065eeb855a9048d2128e2e66
Bolton Bailey <bolton.bailey@gmail.com>
2022-07-18 16:55:01 -0400

    Update src/data/nat/totient.lean
    Co-authored-by: Anne Baanen <Vierkantor@users.noreply.github.com>

--- src/data/nat/totient.lean
+++ src/data/nat/totient.lean
@@ -3,7 +3,6 @@ Copyright (c) 2018 Chris Hughes. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Chris Hughes
 -/
-import data.zmod.basic
 import algebra.char_p.two
 import data.nat.factorization.basic
 import data.nat.periodic

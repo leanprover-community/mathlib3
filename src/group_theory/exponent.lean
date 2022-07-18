6c4ccf864625eb671cd1aac8566463a7dfa236eb
Bolton Bailey <bolton.bailey@gmail.com>
2022-07-18 16:55:15 -0400

    Update src/group_theory/exponent.lean
    Co-authored-by: Anne Baanen <Vierkantor@users.noreply.github.com>

--- src/group_theory/exponent.lean
+++ src/group_theory/exponent.lean
@@ -4,8 +4,9 @@ Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Julian Kuelshammer
 -/
 import group_theory.order_of_element
-import algebra.punit_instances
 import algebra.gcd_monoid.finset
+import algebra.punit_instances
+import data.nat.factorization.basic
 import tactic.by_contra
 
 /-!

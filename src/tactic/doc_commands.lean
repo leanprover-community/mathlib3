4aa1ae67ae12cdbb84466312fcd7652979acf91c
Heather Macbeth <25316162+hrmacbeth@users.noreply.github.com>
2022-07-19 18:34:01 -0400

    delete file and try ci

--- src/tactic/doc_commands.lean
+++ src/tactic/doc_commands.lean
@@ -3,7 +3,6 @@ Copyright (c) 2020 Robert Y. Lewis. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Robert Y. Lewis
 -/
-import tactic.fix_reflect_string
 
 /-!
 # Documentation commands

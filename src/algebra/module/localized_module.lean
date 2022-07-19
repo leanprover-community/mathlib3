84f4bcc9af75a51e7725da68bc3581821454c89b
Jujian Zhang <jujian.zhang1998@outlook.com>
2022-07-19 02:58:24 +0100

    add author name

--- src/algebra/module/localized_module.lean
+++ src/algebra/module/localized_module.lean
@@ -1,7 +1,7 @@
 /-
 Copyright (c) 2022 Jujian Zhang. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
-Authors: Jujian Zhang
+Authors: Andrew Yang, Jujian Zhang
 -/
 
 import group_theory.monoid_localization

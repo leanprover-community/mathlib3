7c7fd91eb4f2be09234dd1ef1514adf0b737538b
Jujian Zhang <jujian.zhang1998@outlook.com>
2022-07-19 16:25:45 +0100

    fix?

--- src/data/pi/algebra.lean
+++ src/data/pi/algebra.lean
@@ -50,9 +50,9 @@ namespace pi
 instance has_smul {α : Type*} [Π i, has_smul α $ f i] : has_smul α (Π i : I, f i) :=
 ⟨λ s x, λ i, s • (x i)⟩
 
-@[to_additive]
+@[simp, to_additive]
 lemma smul_def {α : Type*} [Π i, has_smul α $ f i] (s : α) : s • x = λ i, s • x i := rfl
-@[to_additive]
+@[simp, to_additive]
 lemma smul_apply {α : Type*} [Π i, has_smul α $ f i] (s : α) : (s • x) i = s • x i := rfl
 
 @[to_additive pi.has_smul]

5e79405eb84688803a08bd500f078d554d44a9bf
Yury G. Kudryashov <urkud@urkud.name>
2022-07-19 07:36:23 -0400

    Fix authors line

--- src/analysis/convex/partition_of_unity.lean
+++ src/analysis/convex/partition_of_unity.lean
@@ -1,7 +1,7 @@
 /-
 Copyright (c) 2022 Yury Kudryashov. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
-Author: Yury Kudryashov
+Authors: Yury Kudryashov
 -/
 import topology.partition_of_unity
 import analysis.convex.combination

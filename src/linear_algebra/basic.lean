bec5ee2d2aaba9156ce38e283037748dd6572bbd
ADedecker <anatolededecker@gmail.com>
2022-07-19 22:03:04 -0400

    Suggestions

--- src/linear_algebra/basic.lean
+++ src/linear_algebra/basic.lean
@@ -1524,6 +1524,9 @@ variables (p q : submodule R M)
 
 variables {p q}
 
+@[simp] lemma of_eq_symm (h : p = q) : (of_eq p q h).symm = of_eq q p h.symm := rfl
+@[simp] lemma of_eq_rfl : of_eq p p rfl = linear_equiv.refl R p := by ext; refl
+
 include σ₂₁
 /-- A linear equivalence which maps a submodule of one module onto another, restricts to a linear
 equivalence of the two submodules. -/

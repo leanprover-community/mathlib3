e0fb404fd5b016ff39344d1a88da49f445518ac6
Violeta Hernández <vi.hdz.p@gmail.com>
2022-07-20 13:33:52 -0500

    Update nonempty.lean

--- src/logic/nonempty.lean
+++ src/logic/nonempty.lean
@@ -78,6 +78,9 @@ iff.intro (assume ⟨⟨a⟩, h⟩, ⟨a, h⟩) (assume ⟨a, h⟩, ⟨⟨a⟩, 
 lemma classical.nonempty_pi {α} {β : α → Sort*} : nonempty (Πa:α, β a) ↔ (∀a:α, nonempty (β a)) :=
 iff.intro (assume ⟨f⟩ a, ⟨f a⟩) (assume f, ⟨assume a, classical.choice $ f a⟩)
 
+instance pi.nonempty {α : Type*} {β : α → Type*} [h : Π a, nonempty (β a)] : nonempty (Π a, β a) :=
+classical.nonempty_pi.2 h
+
 /-- Using `classical.choice`, lifts a (`Prop`-valued) `nonempty` instance to a (`Type`-valued)
   `inhabited` instance. `classical.inhabited_of_nonempty` already exists, in
   `core/init/classical.lean`, but the assumption is not a type class argument,

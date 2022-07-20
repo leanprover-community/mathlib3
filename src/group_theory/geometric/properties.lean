e65a66b74b5924ef5c6d25353ab1fbfb9fc026b8
Jim Fowler <fowler@math.osu.edu>
2022-07-20 13:31:06 -0400

    added mul_equiv_respects so group properties are really about groups up to equiv

--- src/group_theory/geometric/properties.lean
+++ src/group_theory/geometric/properties.lean
@@ -3,10 +3,25 @@ import group_theory.nilpotent
 
 namespace geometric_group_theory
 
-/-- A property of a group is a `Prop` depending on a group `G` --/
+/-- A property of a group is a `Prop` depending on a group `G`. --/
 def group_property := Π (G : Type*) [group G], Prop
 
-def is_finite (G : Type*) [group G] : Prop := nonempty (fintype G)
+/-- In order to transport properties of groups across equivalences, a
+`group_property` should include data for facilitating that transport. --/
+class mul_equiv_respects (property : group_property) :=
+(eq_preserves: Π {G : Type*} [group G], Π {H : Type*} [group H], 
+(G ≃* H) → (property G) → (property H))
+
+def is_finite (G : Type*) [group G] : Prop := finite G
+
+instance : mul_equiv_respects is_finite :=
+⟨begin
+  intros G _ H _ e hg,
+  unfold is_finite at hg,
+  haveI hg := finite G,
+  apply finite.of_equiv G,
+  exact e.to_equiv,
+end⟩ 
 
 def is_trivial (G : Type*) [group G] : Prop := ∀ (g : G), g = 1
 
@@ -18,7 +33,20 @@ def virtually (property : group_property) (G : Type*) [group G] : Prop :=
 
 def is_virtually_nilpotent := virtually group.is_nilpotent
 
+def virtually_p_of_p (p : group_property) [ep : mul_equiv_respects p] (G : Type*) [gg : group G] : 
+(p G) → ((virtually p) G) :=
+begin
+  intro h,
+  use (⊤ : subgroup G),
+  split,
+  { simp, },
+  -- can this be cleaned up?
+  let k := ep.eq_preserves,
+  exact @k G gg _ _ subgroup.top_equiv.symm h,
+end
+
 -- none of these seem obvious :(
+-- worse, I think they're impossible without something like `mul_equiv_respects` ?
 example (G : Type*) [group G] : (virtually is_finite) G → is_finite G := begin
   rintros ⟨H,ind,prop⟩,
   let fintype_quo := subgroup.fintype_of_index_ne_zero (ne_of_gt ind),

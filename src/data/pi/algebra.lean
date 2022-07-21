c2197a419c17efe3fc765e11f63579ba6c560bd8
Alexander Bentkamp <bentkamp@gmail.com>
2022-07-21 16:38:17 +0200

    to_additive

--- src/data/pi/algebra.lean
+++ src/data/pi/algebra.lean
@@ -231,36 +231,24 @@ funext $ λ j, by rw [subsingleton.elim j i, pi.mul_single_eq_same]
 namespace sum
 variables (a a' : α → γ) (b b' : β → γ)
 
-lemma elim_zero_zero [has_zero γ] :
-  sum.elim (0 : α → γ) (0 : β → γ) = 0 :=
-by { ext x, cases x; simp }
-
+@[to_additive]
 lemma elim_one_one [has_one γ] :
   sum.elim (1 : α → γ) (1 : β → γ) = 1 :=
 by { ext x, cases x; simp }
 
+@[to_additive]
 lemma inv_elim [has_inv γ] :
   (sum.elim a b)⁻¹ = sum.elim a⁻¹ b⁻¹ :=
 by { ext x, cases x; simp }
 
-lemma neg_elim [has_neg γ] :
-  - (sum.elim a b) = sum.elim (- a) (- b) :=
-by { ext x, cases x; simp }
-
+@[to_additive]
 lemma mul_elim [has_mul γ] :
-  (sum.elim a b) * (sum.elim a' b') = sum.elim (a * a') (b * b') :=
+  sum.elim a b * sum.elim a' b' = sum.elim (a * a') (b * b') :=
 by { ext x, cases x; simp }
 
+@[to_additive]
 lemma div_elim [has_div γ] :
-  (sum.elim a b) / (sum.elim a' b') = sum.elim (a / a') (b / b') :=
-by { ext x, cases x; simp }
-
-lemma add_elim [has_add γ] :
-  (sum.elim a b) + (sum.elim a' b') = sum.elim (a + a') (b + b') :=
-by { ext x, cases x; simp }
-
-lemma sub_elim [has_sub γ] :
-  (sum.elim a b) - (sum.elim a' b') = sum.elim (a - a') (b - b') :=
+  sum.elim a b / sum.elim a' b' = sum.elim (a / a') (b / b') :=
 by { ext x, cases x; simp }
 
 end sum

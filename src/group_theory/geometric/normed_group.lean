5d454e340a8366afb1ed24ed83ab442262ae1759
Laurent Bartholdi <laurent.bartholdi@gmail.com>
2022-07-21 21:01:11 +0200

    Changed right-invariant to left-invariant norms (last line still broken)

--- src/group_theory/geometric/normed_group.lean
+++ src/group_theory/geometric/normed_group.lean
@@ -9,17 +9,17 @@ This file defines multiplicative normed groups.
 
 variables (E : Type*)
 
-/-- A semi-normed group is an additive group endowed with a norm for which `dist x y = ∥x - y∥`
+/-- A semi-normed group is a group endowed with a norm for which `dist x y = ∥x⁻¹*y∥`
 defines a metric space structure. -/
 class seminormed_mul_group extends has_norm E, group E, pseudo_metric_space E :=
-(dist := λ x y, ∥x / y∥)
-(dist_eq : ∀ x y, dist x y = ∥x / y∥ . obviously)
+(dist := λ x y, ∥x⁻¹*y∥)
+(dist_eq : ∀ x y, dist x y = ∥x⁻¹*y∥ . obviously)
 
 /-- A normed group is an additive group endowed with a norm for which `dist x y = ∥x - y∥` defines
 a metric space structure. -/
 class normed_mul_group extends has_norm E, group E, metric_space E :=
-(dist := λ x y, ∥x / y∥)
-(dist_eq : ∀ x y, dist x y = ∥x / y∥ . obviously)
+(dist := λ x y, ∥x⁻¹*y∥)
+(dist_eq : ∀ x y, dist x y = ∥x⁻¹*y∥ . obviously)
 
 @[priority 100] -- See note [lower instance priority]
 instance normed_mul_group.to_seminormed_mul_group [normed_mul_group E] : seminormed_mul_group E :=
@@ -32,18 +32,27 @@ is submultiplicative and such that `f x⁻¹ = f x` and `f x = 0 → x = 1` for 
 @[nolint has_inhabited_instance] structure group_norm extends group_seminorm E :=
 (eq_one_of_to_fun : ∀ ⦃x : E⦄, to_fun x = 0 → x = 1)
 
+lemma mul_mul_inv_cancel (x y z : E) [group E] : (x⁻¹*z) = (x⁻¹*y * (y⁻¹*z)) := by group
+
+lemma inv_of_inv_mul (x y : E) [group E] : (x⁻¹*y)⁻¹ = y⁻¹*x := by group
+
 /-- Constructing a seminormed group from a seminorm, i.e., registering the pseudodistance and the
 pseudometric space structure from the seminorm properties. Note that in most cases this instance
 creates bad definitional equalities (e.g., it does not take into account a possibly existing
 `uniform_space` instance on `E`). -/
 def group_seminorm.to_seminormed_mul_group (p : group_seminorm E) : seminormed_mul_group E :=
 { norm := p,
-  dist := λ x y, p (x / y),
+  dist := λ x y, p (x⁻¹*y),
   dist_self := λ x, by simp [p.map_one],
   dist_triangle := λ x y z,
-    calc p (x / z) = p (x / y * (y / z)) : by rw div_mul_div_cancel'
-            ... ≤ p (x / y) + p (y / z)  : p.mul_le _ _,
-  dist_comm := p.div_rev }
+    calc p (x⁻¹*z) = p (x⁻¹*y * (y⁻¹*z)) : by rw mul_mul_inv_cancel
+            ... ≤ p (x⁻¹*y) + p (y⁻¹*z)  : p.mul_le _ _,
+  dist_comm := begin
+    intros x y,
+    let pinv := p.inv (y⁻¹*x),
+    rw inv_of_inv_mul at pinv,
+    exact pinv
+  end }
 
 /-- Constructing a normed group from a norm, i.e., registering the distance and the metric space
 structure from the norm properties. Note that in most cases this instance creates bad definitional

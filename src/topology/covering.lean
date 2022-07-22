01be7e3ec465ead4775d8dc80f40ce4be11a96dd
Thomas Browning <tb65536@uw.edu>
2022-07-22 11:52:14 -0700

    changes

--- src/topology/covering.lean
+++ src/topology/covering.lean
@@ -1,8 +1,27 @@
+/-
+Copyright (c) 2021 Thomas Browning. All rights reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
+Authors: Jordan Brown, Thomas Browning
+-/
 import topology.is_locally_homeomorph
+import topology.fiber_bundle
+
+/-!
+# Local homeomorphisms
+
+This file defines covering maps.
+
+## Main definitions
+
+* `covering_map`: A covering map is a surjective continuous function `f : E → X` with discrete
+  fibers where each point of `X` has an evenly covered neighborhood.
+-/
 
 variables {E X : Type*} [topological_space E] [topological_space X] (f : E → X)
   (I : Type*) [topological_space I] {x y : X} (U V : set X)
 
+/-- `U` is evenly covered if `I × U` is homeomorphic to `f ⁻¹' U` -/
+@[nolint has_inhabited_instance] -- not all spaces are covering spaces
 structure evenly_covered_set extends homeomorph (I × U) (f ⁻¹' U) :=
 (commutes' : ∀ p, f (to_fun p) = p.2)
 
@@ -18,13 +37,14 @@ instance : has_coe_to_fun (evenly_covered_set f I U) (λ ι, I × U → f ⁻¹'
 lemma is_open_map : is_open_map ϕ :=
 ϕ.to_homeomorph.is_open_map
 
+/-- The homeomorphism from `f ⁻¹' U` to `I × U` -/
 def symm : f ⁻¹' U ≃ₜ I × U :=
 ϕ.to_homeomorph.symm
 
-@[simp] def apply_symm_apply (p : f ⁻¹' U) : ϕ (ϕ.symm p) = p :=
+@[simp] lemma apply_symm_apply (p : f ⁻¹' U) : ϕ (ϕ.symm p) = p :=
 ϕ.to_equiv.apply_symm_apply p
 
-@[simp] def symm_apply_apply (p : I × U) : ϕ.symm (ϕ p) = p :=
+@[simp] lemma symm_apply_apply (p : I × U) : ϕ.symm (ϕ p) = p :=
 ϕ.to_equiv.symm_apply_apply p
 
 lemma commutes (p : I × U) : f (ϕ p) = p.2 :=
@@ -33,6 +53,7 @@ lemma commutes (p : I × U) : f (ϕ p) = p.2 :=
 lemma commutes_inv (p : f ⁻¹' U) : f p = (ϕ.symm p).2 :=
 by rw [←ϕ.apply_symm_apply p, ϕ.commutes, ϕ.apply_symm_apply]
 
+/-- Subsets of an evenly covered set are evenly covered -/
 def mono (h : V ⊆ U) : evenly_covered_set f I V :=
 { to_fun := λ p, ⟨ϕ ⟨p.1, p.2, h p.2.2⟩, by rw [set.mem_preimage, ϕ.commutes]; exact p.2.2⟩,
   inv_fun := λ p, ⟨(ϕ.symm ⟨p, h p.2⟩).1,
@@ -42,6 +63,7 @@ def mono (h : V ⊆ U) : evenly_covered_set f I V :=
   commutes' := λ p, ϕ.commutes ⟨p.1, p.2, h p.2.2⟩,
   continuous_to_fun := by continuity! }
 
+/-- Each fiber is homeomorphic to the index set -/
 def fiber_homeomorph (hx : x ∈ U) : f ⁻¹' {x} ≃ₜ I :=
 { to_fun := λ p, (ϕ.symm ⟨p, (congr_arg (∈ U) p.2).mpr hx⟩).1,
   inv_fun := λ p, ⟨ϕ ⟨p, x, hx⟩, ϕ.commutes ⟨p, x, hx⟩⟩,
@@ -54,18 +76,24 @@ def fiber_homeomorph (hx : x ∈ U) : f ⁻¹' {x} ≃ₜ I :=
   right_inv := λ p, by simp only [subtype.coe_mk, subtype.coe_eta, symm_apply_apply],
   continuous_inv_fun := by continuity! }
 
-def translate (hx : x ∈ U) : evenly_covered_set f (f ⁻¹' {x}) U :=
-{ to_homeomorph := ((ϕ.fiber_homeomorph hx).prod_congr (homeomorph.refl U)).trans ϕ.to_homeomorph,
-  commutes' := λ p, ϕ.commutes ⟨ϕ.fiber_homeomorph hx p.1, p.2⟩ }
-
 end evenly_covered_set
 
 variables (f)
 
+/-- `U` is evenly covered if `f ⁻¹' {x} × U` is homeomorphic to `f ⁻¹' U` -/
+@[nolint has_inhabited_instance] -- not all spaces are covering spaces
 structure evenly_covered_pt (hx : x ∈ U) extends evenly_covered_set f (f ⁻¹' {x}) U :=
 (rigid' : ∀ p : f ⁻¹' {x}, to_fun ⟨p, x, hx⟩ = ⟨p, (congr_arg (∈ U) p.2).mpr hx⟩)
 
 variables {f}
+
+/-- Evenly covered with index set `I` implies evenly covered with index set `f ⁻¹' {x}` -/
+def evenly_covered_set.to_evenly_covered_pt (ϕ : evenly_covered_set f I U) (hx : x ∈ U) :
+  evenly_covered_pt f hx :=
+{ to_homeomorph := ((ϕ.fiber_homeomorph hx).prod_congr (homeomorph.refl U)).trans ϕ.to_homeomorph,
+  commutes' := λ p, ϕ.commutes ⟨ϕ.fiber_homeomorph hx p.1, p.2⟩,
+  rigid' := λ p, ϕ.to_equiv.apply_eq_iff_eq_symm_apply.mpr (prod.ext rfl (subtype.ext
+    (p.2.symm.trans (ϕ.commutes_inv ⟨p, (congr_arg (∈ U) p.2).mpr hx⟩)))) }
 
 namespace evenly_covered_pt
 
@@ -77,13 +105,14 @@ instance : has_coe_to_fun (evenly_covered_pt f hx) (λ ι, f ⁻¹' {x} × U →
 lemma is_open_map : is_open_map ϕ :=
 ϕ.to_evenly_covered_set.is_open_map
 
+/-- The homeomorphism from `f ⁻¹' U` to `I × U` -/
 def symm : f ⁻¹' U ≃ₜ f ⁻¹' {x} × U :=
 ϕ.to_evenly_covered_set.symm
 
-@[simp] def apply_symm_apply (p : f ⁻¹' U) : ϕ (ϕ.symm p) = p :=
+@[simp] lemma apply_symm_apply (p : f ⁻¹' U) : ϕ (ϕ.symm p) = p :=
 ϕ.to_evenly_covered_set.apply_symm_apply p
 
-@[simp] def symm_apply_apply (p : f ⁻¹' {x} × U) : ϕ.symm (ϕ p) = p :=
+@[simp] lemma symm_apply_apply (p : f ⁻¹' {x} × U) : ϕ.symm (ϕ p) = p :=
 ϕ.to_evenly_covered_set.symm_apply_apply p
 
 lemma commutes (p : f ⁻¹' {x} × U) : f (ϕ p) = p.2 :=
@@ -95,19 +124,19 @@ lemma commutes_inv (p : f ⁻¹' U) : f p = (ϕ.symm p).2 :=
 lemma rigid (p : f ⁻¹' {x}) : ϕ ⟨p, x, hx⟩ = ⟨p, (congr_arg (∈ U) p.2).mpr hx⟩ :=
 ϕ.rigid' p
 
-def mono {hx' : x ∈ V} (h : V ⊆ U) : evenly_covered_pt f hx' :=
-{ rigid' := λ p, subtype.ext (show _, from subtype.ext_iff.mp (ϕ.rigid p)),
-  .. ϕ.to_evenly_covered_set.mono h }
+/-- Subsets of an evenly covered set are evenly covered -/
+def mono (h : V ⊆ U) (hx' : x ∈ V) : evenly_covered_pt f hx' :=
+(ϕ.to_evenly_covered_set.mono h).to_evenly_covered_pt hx'
 
+/-- `evenly_covered_pt` does not depend on the choice of point -/
 def translate (hy : y ∈ U) : evenly_covered_pt f hy :=
-{ rigid' := λ p, ϕ.to_equiv.apply_eq_iff_eq_symm_apply.mpr (prod.ext rfl (subtype.ext
-    (p.2.symm.trans (ϕ.commutes_inv ⟨p, (congr_arg (∈ U) p.2).mpr hy⟩)))),
-  .. ϕ.to_evenly_covered_set.translate hy }
+ϕ.to_evenly_covered_set.to_evenly_covered_pt hy
 
 end evenly_covered_pt
 
 variables (f)
 
+/-- `U` is evenly covered if `f ⁻¹' {x} × U` is homeomorphic to `f ⁻¹' U` -/
 def evenly_covered (hx : x ∈ U) : Prop :=
 nonempty (evenly_covered_pt f hx)
 
@@ -117,7 +146,7 @@ namespace evenly_covered
 
 lemma mono {hx : x ∈ V} {hy : x ∈ U} (hf : evenly_covered f hy) (h : V ⊆ U) :
   evenly_covered f hx :=
-hf.elim (λ g, ⟨g.mono h⟩)
+hf.elim (λ g, ⟨g.mono h hx⟩)
 
 lemma translate {hx : x ∈ U} (hf : evenly_covered f hx) (hy : y ∈ U) : evenly_covered f hy :=
 hf.elim (λ g, ⟨g.translate hy⟩)
@@ -130,8 +159,10 @@ lemma evenly_covered_translate_iff (hx : x ∈ U) (hy : y ∈ U) :
 
 variables (E X)
 
+/-- A covering map is a surjective continuous function `f : E → X` with discrete
+  fibers where each point of `X` has an evenly covered neighborhood. -/
+@[nolint has_inhabited_instance] -- not all spaces are covering spaces
 structure covering_map extends continuous_map E X :=
-(surjective : function.surjective to_fun)
 (discrete_fibers : ∀ x, discrete_topology (to_fun ⁻¹' {x}))
 (evenly_covered : ∀ x, ∃ U, is_open U ∧ ∃ hU : x ∈ U, evenly_covered to_fun hU)
 
@@ -139,7 +170,7 @@ variables {E X}
 
 namespace covering_map
 
-infixr ` ↠ `:25 := covering_map -- shortcut: type `\rr-` or just type `\rr `
+infixr ` ↠ `:25 := covering_map
 
 instance : has_coe_to_fun (E ↠ X) (λ _, E → X) := ⟨λ q, q.to_fun⟩
 
@@ -173,24 +204,10 @@ begin
       continuity! } }, ⟨⟨q x, hx⟩, subtype.ext_iff.mp (ϕ.rigid ⟨x, rfl⟩)⟩, rfl⟩,
 end
 
-lemma is_open_map (q : E ↠ X) : is_open_map q :=
+lemma open_map (q : E ↠ X) : is_open_map q :=
 q.is_locally_homeomorph.is_open_map
 
-lemma covering_map_quotient_map (q : E ↠ X) : quotient_map q :=
-q.is_open_map.to_quotient_map q.continuous q.surjective
+lemma quotient_map_of_surjective (q : E ↠ X) (hq : function.surjective q) : quotient_map q :=
+q.open_map.to_quotient_map q.continuous hq
 
 end covering_map
-
--- *** FUNDAMENTAL GROUP *** --
-
--- def fundamental_group (x : X) :=
--- let y : fundamental_groupoid X := x in y ⟶ y
-
--- noncomputable instance (x : X) : group (fundamental_group x) :=
--- { mul := category_theory.category_struct.comp,
---   mul_assoc := category_theory.category.assoc,
---   one := category_theory.category_struct.id _,
---   one_mul := category_theory.category.id_comp,
---   mul_one := category_theory.category.comp_id,
---   inv := category_theory.groupoid.inv,
---   mul_left_inv := category_theory.groupoid.inv_comp }

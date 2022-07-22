f0da397887747876147115733f4d9f00d419f577
Kevin Buzzard <k.buzzard@imperial.ac.uk>
2022-07-21 23:11:11 -0400

    add first question

--- src/data/polynomial/partial_fractions.lean
+++ src/data/polynomial/partial_fractions.lean
@@ -5,6 +5,7 @@ Authors: Kevin Buzzard, [add your name if you want]
 -/
 import ring_theory.localization.fraction_ring -- field of fractions
 import data.polynomial.div -- theory of division and remainder for monic polynomials
+import tactic.field_simp
 /-
 
 # Partial fractions
@@ -26,14 +27,42 @@ https://en.wikipedia.org/wiki/Partial_fraction_decomposition#Statement
 ## TODO
 
 Everything
+
+## Strategy
+
+Here is my proposal.
+
+1) Build a general theory of partial fractions over an
+integral domain, as a "canonical" way of representing `f/g` when `g` is *monic*.
+Assuming monicity makes some divisions a lot less painful (it also guarantees nonzeroness).
+
+2) Deduce the theory of partial fractions over a field for `f/g` with `g` non-zero,
+simply by dividing by the appropriate scalars everywhere.
+
+I think that going from (1) to (2) is perhaps a bit fiddly (this is a nice Lean puzzle,
+you'll learn a lot about finite sums and units). But doing (1) properly will be some
+work. Here is my proposal for how to get through it.
+
+1a) First develop the theory corresponding to partial fractions with denominators
+`d₁, d₂, …, dₙ` where the `dᵢ` are monic and pairwise coprime. Do the theory
+for `n=1` and `n=2` first and then prove the general theorem by induction on `n`.
+Numerators must have degree less than denominators, and the decomposition is
+unique.
+
+1b) Now develop the theory for `f/g` with g a power of a monic polynomial.
+I don't see why irreducibility is even needed.
+
 -/
 
--- Let `K` be a field
+-- Let `R` be an integral domain
+variables (R : Type) [comm_ring R] [is_domain R]
 
-variables (K : Type) [field K]
-
--- Let's use the usual `K[X]` notation for polynomials
+-- Let's use the usual `R[X]` notation for polynomials
 open_locale polynomial
+
+-- I don't want to have to keep typing `polynomial.degree_mod_by_monic_lt`
+-- and `polynomial.mod_by_monic_add_div`, these names are long enough already
+open polynomial
 
 /-
 
@@ -44,12 +73,10 @@ for *monic* polynomials. Here's an example of it in action.
 
 -/
 
--- I don't want to have to keep typing `polynomial.degree_mod_by_monic_lt`
--- and `polynomial.mod_by_monic_add_div`, these names are long enough already
-open polynomial
+section worked_example
 
 -- let p and q be polynomials over `K`, and assume `q` is monic.
-variables (p q : K[X]) (hq : monic q)
+variables (p q : R[X]) (hq : monic q)
 
 -- I claim that `p /ₘ q` is the rounded-to-the-nearest-polynomial division of `p` by `q`,
 -- and that `p %ₘ q` is the remainder when you divide `p` by `q`.
@@ -66,3 +93,44 @@ example : (p %ₘ q).degree < q.degree := degree_mod_by_monic_lt p hq
 
 https://leanprover-community.github.io/mathlib_docs/data/polynomial/div.html
 https://leanprover-community.github.io/mathlib_docs/data/polynomial/ring_division.html
+
+-/
+
+end worked_example
+
+/-
+
+## One denominator, and two coprime denominators
+
+-/
+
+section one_denominator
+
+-- Let's show that we can write `f/g` as `q+r/g` with deg(q) < deg(g)
+
+namespace polynomial
+
+-- As always, R is an integral domain.
+-- Let f and g be polynomials
+variables (f : R[X]) {g : R[X]}
+
+-- Let K be the field of fractions of R[X].
+-- Examples of elements of `K` are `X^2+1`, `1/X`, `(X^2+2X+3)/(4X^3+5)` etc
+variables (K : Type) [field K] [algebra R[X] K]  [is_fraction_ring R[X] K]
+
+-- Internally, `R[X]` is not a subset of `K`, for foundational reasons.
+-- We set it up so that if `f : R[X]` then writing `(f : K)` will enable us to
+-- think of `f` as an element of `K`. Lean will denote this "invisible map"
+-- from R[X] to K with an `↑`.
+noncomputable instance : has_coe R[X] K := ⟨algebra_map R[X] K⟩
+
+-- If `g` is monic then `f/g` can be written as `q+r/g` with deg(r) < deg(g)
+lemma div_eq_div_add_mod_div (hg : g.monic) : ∃ q r : R[X], r.degree < g.degree ∧
+  (f : K) / g = q + r / g :=
+begin
+  sorry
+end
+
+end polynomial
+
+end one_denominator

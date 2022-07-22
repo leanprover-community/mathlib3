48fd677f3d18eec95bf810849a9c40d813638976
Kevin Buzzard <k.buzzard@imperial.ac.uk>
2022-07-22 11:20:35 -0400

    puzzle for UGs

--- src/data/polynomial/partial_fractions.lean
+++ src/data/polynomial/partial_fractions.lean
@@ -100,19 +100,15 @@ end worked_example
 
 /-
 
-## One denominator, and two coprime denominators
+## A trick: getting `norm_cast` to work
+
+`norm_cast` is a tactic which proves goals like `↑f + ↑g = ↑(f + g)`.
+Here the up-arrow is an "invisible map", which in this case will be the
+map from R[X] to its field of fractions. The point is that if we want to do division
+of polynomials we need to work in the field of fractions, but the moment we've
+cleared denominators we want to get back into the R[X] world.
 
 -/
-
-section one_denominator
-
--- Let's show that we can write `f/g` as `q+r/g` with deg(q) < deg(g)
-
-namespace polynomial
-
--- As always, R is an integral domain.
--- Let f and g be polynomials
-variables (f : R[X]) {g : R[X]}
 
 -- Let K be the field of fractions of R[X].
 -- Examples of elements of `K` are `X^2+1`, `1/X`, `(X^2+2X+3)/(4X^3+5)` etc
@@ -128,6 +124,8 @@ think of `f` as an element of `K`. Lean will denote this "invisible map"
 from R[X] to K with an `↑`.
 
 -/
+
+namespace polynomial
 
 noncomputable instance : has_coe R[X] K := ⟨algebra_map R[X] K⟩
 
@@ -158,10 +156,12 @@ map_zero (algebra_map R[X] K)
 @[norm_cast] lemma coe_algebra_map_pow (a : R[X]) (n : ℕ) : ((a ^ n : R[X]) : K) = a ^ n :=
 map_pow (algebra_map R[X] K) _ _
 
+end polynomial
+
 /-
 
-That's all the training I can think of right now. Here are examples
-of the cast tactic.
+That's all the training I can think of right now (although I'm open to the idea
+that there are more lemmas we'll need). Here are examples of `cast` tactics.
 
 -/
 
@@ -179,18 +179,84 @@ end
 
 end nice_trick
 
+/-
+
+## One denominator, and two coprime denominators
+
+-/
+
+section one_denominator
+
+-- Let's show that we can write `f/g` as `q+r/g` with deg(q) < deg(g)
+
+namespace polynomial
+
+-- As always, R is an integral domain.
+-- Let f and g be polynomials
+variables (f : R[X]) {g : R[X]}
+
 -- If `g` is monic then `f/g` can be written as `q+r/g` with deg(r) < deg(g)
-lemma div_eq_div_add_mod_div (hg : g.monic) : ∃ q r : R[X], r.degree < g.degree ∧
+lemma div_eq_quo_add_rem_div (hg : g.monic) : ∃ q r : R[X], r.degree < g.degree ∧
   (f : K) / g = q + r / g :=
 begin
+  -- let `q` be "polynomial division `f / g`" and let `r` be the remainder
   refine ⟨f /ₘ g, f %ₘ g, _, _⟩, -- same as `use, use, split`
+  -- The fact that the degree of the remainder is < degree of what we're dividing by is in the
+  -- library
   { exact degree_mod_by_monic_lt _ hg, },
+  -- For the other proof we first want to clear denominators.
+  -- Our goal is in `K` right now so to clear denominators we need (g : K) ≠ 0
+  -- Note that `monic.ne_zero hg` is a proof that (g : R[X]) ≠ 0, so a `cast` tactic can
+  -- finish the job.
   { have hg' : (g : K) ≠ 0 := by exact_mod_cast (monic.ne_zero hg),
-    field_simp [hg'], -- "clear denominators" tactic
-    norm_cast, -- now a question about R[X] not K
+     -- Now use the "clear denominators" tactic.
+    field_simp [hg'],
+    -- Now use `norm_cast` to get out of `K` and back into `R[X]`
+    norm_cast,
+    -- now it's nearly `mod_by_monic_add_div` except that things need some rearranging.
     rw [add_comm, mul_comm, mod_by_monic_add_div _ hg], },
 end
 
 end polynomial
 
 end one_denominator
+
+section two_denominators
+
+-- If `g₁` and `g₂` are coprime monics then `f/g₁g₂` can be written as `q+r₁/g₁+r₂/g₂`
+-- with deg(rᵢ) < deg(gᵢ)
+lemma div_eq_quo_add_rem_div_add_rem_div {f g₁ g₂ : R[X]} (hg₁ : g₁.monic) (hg₂ : g₂.monic) :
+  ∃ q r₁ r₂ : R[X], r₁.degree < g₁.degree ∧ r₂.degree < g₂.degree ∧
+  (f : K) / (g₁ * g₂) = q + r₁ / g₁ + r₂ / g₂ :=
+begin
+  sorry
+  -- definitely worth thinking about the maths proof first
+end
+
+end two_denominators
+
+section n_denominators
+
+-- need notation for finite products
+open_locale big_operators
+
+lemma div_eq_quo_add_sum_rem_div (f : R[X]) {ι : Type*} [fintype ι] {g : ι → R[X]}
+  (hg : ∀ i, (g i).monic) :
+∃ (q : R[X]) (r : ι → R[X]), (∀ i, (r i).degree < (g i).degree) ∧
+  (f : K) / ∏ i, g i = q + ∑ i, (r i) / (g i) :=
+begin
+  -- this might not be the best thing to do directly. Want to do "induction on ι"?
+  -- Haven't thought about it too hard
+  sorry
+end
+
+-- uniqueness
+lemma div_eq_quo_add_sum_rem_div_unique {f : R[X]} {ι : Type*} [fintype ι] {g : ι → R[X]}
+  (hg : ∀ i, (g i).monic) (q : R[X]) (r : ι → R[X]) (hr : ∀ i, (r i).degree < (g i.degree))
+  (hf : (f : K) / ∏ i, g i = q + ∑ i, (r i) / (g i)) :
+    q = (div_eq_quo_add_sum_rem_div R K f hg).some ∧ r = (div_eq_quo_add_sum_rem_div R K f hg).some_spec.some :=
+begin
+  sorry
+end
+
+end n_denominators

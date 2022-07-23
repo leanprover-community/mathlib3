5352f9f1e77f810922e12c979c6c8bdcfff37896
Stuart Presnell <stuart.presnell@gmail.com>
2022-07-23 11:53:37 +0100

    First working version of `totient_dvd_of_dvd`

--- src/data/nat/totient.lean
+++ src/data/nat/totient.lean
@@ -351,4 +351,49 @@ begin
   apply mul_le_mul_left' (nat.totient_le d),
 end
 
+lemma totient_dvd_of_dvd (a b : ℕ) (h : a ∣ b) : φ a ∣ φ b :=
+begin
+  rcases eq_or_ne a 0 with rfl | ha0, { simp [zero_dvd_iff.1 h] },
+  rcases eq_or_ne b 0 with rfl | hb0, { simp },
+  have hab : a.factors.to_finset ⊆ b.factors.to_finset, {
+    intro p,
+    simp only [list.mem_to_finset],
+    apply factors_subset_of_dvd h hb0,
+   },
+  have ha := prod_prime_factors_dvd a,
+  have hb := prod_prime_factors_dvd b,
+
+  rw [totient_eq_prod_factorization ha0, totient_eq_prod_factorization hb0],
+  simp,
+  unfold finsupp.prod,
+  simp only [support_factorization] at *,
+
+
+  refine mul_dvd_mul _ (prod_dvd_prod_of_subset _ _ _ hab),
+
+have := sdiff_union_of_subset hab,
+rw ←this,
+
+  rw prod_union sdiff_disjoint,
+  apply dvd_mul_of_dvd_right,
+  apply prod_dvd_prod_of_dvd,
+  rintro p hp,
+  apply pow_dvd_pow,
+  have := (factorization_le_iff_dvd ha0 hb0).2 h p,
+  simp only [tsub_le_iff_right],
+  apply this.trans,
+  apply le_of_eq,
+  rw nat.sub_add_cancel _,
+  rw succ_le_iff,
+  apply nat.pos_of_ne_zero,
+  intro H,
+  rw factorization_eq_zero_iff' b p at H,
+  contrapose! H,
+  have h1 : p ∈ b.factors.to_finset, {
+    apply hab hp },
+  simp at h1,
+  rw [mem_factors hb0] at h1,
+  refine ⟨h1.1, h1.2, hb0⟩,
+end
+
 end nat

789fef47cfd1f827937a67527800414791b640bb
Joël Riou <joel.riou@universite-paris-saclay.fr>
2022-07-23 12:54:08 +0530

    added comm_sq_lift.lean

---
+++ src/category_theory/limits/shapes/comm_sq_lift.lean
@@ -0,0 +1,7 @@
+/-
+Copyright (c) 2022 Joël Riou. All rights reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
+Authors: Joël Riou
+-/
+import category_theory.limits.shapes.comm_sq
+

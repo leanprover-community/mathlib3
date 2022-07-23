8df40ebdd8f4876dc8614e8fc1c87efbd51c54d8
Joël Riou <joel.riou@universite-paris-saclay.fr>
2022-07-23 14:17:51 +0530

    added basic lifting property for commutative squares

--- src/category_theory/limits/shapes/comm_sq_lift.lean
+++
@@ -1,7 +0,0 @@
-/-
-Copyright (c) 2022 Joël Riou. All rights reserved.
-Released under Apache 2.0 license as described in the file LICENSE.
-Authors: Joël Riou
--/
-import category_theory.limits.shapes.comm_sq
-

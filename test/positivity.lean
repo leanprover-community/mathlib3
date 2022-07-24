d16fa0289a6d1c053ca2dd4b38a1acad211bc5f6
Heather Macbeth <25316162+hrmacbeth@users.noreply.github.com>
2022-07-24 09:17:04 -0400

    add a test

--- test/positivity.lean
+++ test/positivity.lean
@@ -73,6 +73,13 @@ example {a : ℝ} (ha : 0 ≤ a) : 0 < real.sqrt (a + 3) := by positivity
 
 example {a b : ℤ} (ha : 3 < a) : 0 ≤ min a (b ^ 2) := by positivity
 
+-- test that the tactic can ignore arithmetic operations whose associated extension tactic requires
+-- more typeclass assumptions than are available
+example {R : Type*} [has_zero R] [has_div R] [linear_order R] {a b c : R} (h1 : 0 < a) (h2 : 0 < b)
+  (h3 : 0 < c) :
+  0 < max (a / b) c :=
+by positivity
+
 example : 0 ≤ max 3 4 := by positivity
 
 example {b : ℤ} : 0 ≤ max (-3) (b ^ 2) := by positivity

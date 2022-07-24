a53f859ba9de3c0dbbfda1a0eb266a5f531672db
Filippo A. E. Nuccio <filippo.nuccio@univ-st-etienne.fr>
2022-07-25 00:48:36 +0200

    advances

--- src/topology/homotopy/H_spaces.lean
+++ src/topology/homotopy/H_spaces.lean
@@ -102,9 +102,12 @@ end
 variable {x : X}
 
 @[simp]
+lemma continuous_to_Ω_if_to_C {Y : Type u} [topological_space Y] {g : Y → Ω(x)} :
+ continuous (↑g : Y → C(I,X)) → continuous g := λ h, continuous_induced_rng h
+
 lemma continuous_to_Ω_iff_to_C {Y : Type u} [topological_space Y] {g : Y → Ω(x) } :
  continuous g ↔ continuous (↑g : Y → C(I,X)) :=
- ⟨λ h, continuous.comp (continuous_coe x) h, λ h, continuous_induced_rng h⟩
+ ⟨λ h, continuous.comp (continuous_coe x) h, λ h, continuous_to_Ω_if_to_C h⟩
 
 lemma continuous_to_Ω_iff_uncurry {Y : Type u} [topological_space Y]
 [locally_compact_space Y] {g : Y → Ω(x)} : continuous g ↔ continuous (λ p : Y × I, g p.1 p.2) :=
@@ -112,67 +115,69 @@ lemma continuous_to_Ω_iff_uncurry {Y : Type u} [topological_space Y]
   (λ h, continuous_to_Ω_iff_to_C.mpr ((continuous_to_C_iff_uncurry Y I X ↑g).mpr h))
 
 
-lemma Hmul_Ω_cont (x : X) (γ γ' : Ω(x)) : continuous (λ t, γ.trans γ' t) :=
-begin
-  exact (trans γ γ').continuous,
-end
-
-lemma Hmul_Ω_cont' (x : X) : continuous (λ t, λ φ : Ω(x) × Ω(x), φ.1.trans φ.2 t) :=
-begin
-  exact continuous_pi (λ (i : path x x × path x x), (i.fst.trans i.snd).continuous),
-end
-
 lemma continuous_map.continuous_prod (X Y Z : Type*) [topological_space X] [topological_space Y] [locally_compact_space Y] [topological_space Z] : continuous (λ x : C(X, Y) × C(Y, Z), x.2.comp x.1) :=
 begin
   sorry,
 end
 
 
-lemma continuous_prod_first_half (x : X) : continuous (λ x : I × Ω(x) × Ω(x), x.2.1.extend (2 * x.1.1)) :=
+lemma continuous_prod_first_half (x : X) : continuous (λ x : (Ω(x) × Ω(x)) × I, x.1.1.extend (2 * x.2)) :=
 begin
-  set η := (λ p : Ω(x) × I, p.1.extend (2 * p.2)) with hη,
+  let η := (λ p : Ω(x) × I, p.1.extend (2 * p.2)),
   have H : continuous η,
-  { let Cproj : C(ℝ, I) := ⟨set.proj_Icc 0 1 zero_le_one, continuous_proj_Icc⟩,
-    have h_left := ((continuous_map.continuous_prod ℝ I X).comp (continuous.prod.mk Cproj)).comp continuous_induced_dom,
-    have h_right := (continuous_const.mul continuous_id').comp (@continuous_induced_dom _ _ (coe : I → ℝ) _),
+  { let Cproj : C(ℝ, I) := ⟨set.proj_Icc _ _ zero_le_one, continuous_proj_Icc⟩,
+    have h_left := ((continuous_map.continuous_prod _ _ _).comp (continuous.prod.mk Cproj)).comp continuous_induced_dom,
+    have h_right := (continuous_const.mul continuous_id').comp
+    (@continuous_induced_dom _ _ (coe : I → ℝ) _),
     exact (continuous_eval'.comp (continuous.prod_map h_left h_right)) },
-  replace H := (H.comp continuous_swap).comp (@continuous_fst _ Ω(x) _ _),
-  exact (homeomorph.comp_continuous_iff' (homeomorph.prod_assoc _ _ _).symm).mpr H,
+  replace H := (homeomorph.comp_continuous_iff' $ homeomorph.prod_assoc Ω(x) _ _).mpr (H.comp $ continuous_snd),
+  exact (H.comp $ continuous.prod_map continuous_swap continuous_id),
 end
 
-lemma continuous_prod_second_half (x : X) : continuous (λ x : I × Ω(x) × Ω(x), x.2.2.extend (2 * x.1.1 - 1)) :=
+lemma continuous_prod_second_half (x : X) : continuous (λ x : (Ω(x) × Ω(x)) × I, x.1.2.extend (2 * x.2 - 1)) :=
 begin
-  set η := (λ p : Ω(x) × I, p.1.extend (2 * p.2 -1 )) with hη,
+  let η := (λ p : Ω(x) × I, p.1.extend (2 * p.2 - 1)),
   have H : continuous η,
   { let Cproj : C(ℝ, I) := ⟨set.proj_Icc 0 1 zero_le_one, continuous_proj_Icc⟩,
-    have h_left := ((continuous_map.continuous_prod ℝ I X).comp (continuous.prod.mk Cproj)).comp continuous_induced_dom,
+    have h_left := ((continuous_map.continuous_prod _ _ _).comp (continuous.prod.mk Cproj)).comp continuous_induced_dom,
     have aux : continuous (λ x : ℝ, 2 * x -1 ),
       from (continuous_const.mul continuous_id').sub continuous_const,
-    have h_right := aux.comp (@continuous_induced_dom _ _ (coe : I → ℝ) _),
-    exact (continuous_eval'.comp (continuous.prod_map h_left h_right)),
-    },
-  replace H := (H.comp continuous_swap).comp (@continuous_fst _ Ω(x) _ _),
-  replace H := (homeomorph.comp_continuous_iff' (homeomorph.prod_assoc _ _ _).symm).mpr H,
-  exact H.comp (continuous.prod_map (continuous_id) continuous_swap),
+    have h_right := aux.comp continuous_induced_dom,
+    exact (continuous_eval'.comp (continuous.prod_map h_left h_right)) },
+  exact (homeomorph.comp_continuous_iff' (homeomorph.prod_assoc _ _ _).symm).mp (H.comp continuous_snd),
 end
 
-lemma Hmul_Ω_cont₁ (x : X) : continuous (λ x : I × Ω(x) × Ω(x), x.2.1.trans x.2.2 x.1) :=
+lemma Hmul_cont (x : X) : continuous (λ x : (Ω(x) × Ω(x)) × I, x.1.1.trans x.1.2 x.2) :=
 begin
   apply continuous.piecewise,
-  { sorry },
+  { rintros ⟨_, t⟩ ⟨h_left, h_right⟩,
+    have h_eq : (λ (i : (path x x × path x x) × I), (i.snd : ℝ) ≤ (1 / 2)) =  (set.univ) ×ˢ {s : I | (s : ℝ) ≤ (1 / 2)},
+    { ext p,
+      change (p.2 : ℝ) ≤ 1 / 2 ↔ p ∈ ((@set.univ (Ω(x) × Ω(x))) ×ˢ {s : I | (s : ℝ) ≤ 1 / 2}),
+      simp only [set.mem_prod, set.mem_univ, set.mem_set_of_eq, true_and] },
+    erw h_eq at h_left h_right,
+    simp only [closure_prod_eq, closure_univ, set.prod_mk_mem_set_prod_eq, set.mem_univ,
+      true_and] at h_left,
+    simp only [interior_prod_eq, interior_univ, set.prod_mk_mem_set_prod_eq, set.mem_univ, true_and] at h_right,
+
+    have H : (t : ℝ) = 1 / 2,
+    rw closure_induced at h_left,
+    -- haveI : order_closed_topology I, sorry,
+    have := @is_closed_le' ℝ _ _ _ (1 / 2),
+    unfold coe at h_left,
+    -- simp * at *,
+    sorry,
+
+    simp only [H, extend, mul_inv_cancel_of_invertible, set.Icc_extend_right, unit_interval.mk_one,
+      path.target, sub_self, set.Icc_extend_left, unit_interval.mk_zero, path.source, one_div] },
   exacts [continuous_prod_first_half x, continuous_prod_second_half x],
 end
 
 instance loop_space_is_H_space (x : X) : H_space Ω(x) :=
 { Hmul := λ ρ, ρ.1.trans ρ.2,
   e := refl _,
-  cont' :=
-  begin
-    sorry,
-    -- apply continuous_to_Ω_iff_uncurry.mpr,
-    -- apply (Hmul_Ω_cont₁ x),
-  end,
-  Hmul_e_e := sorry,
+  cont' := continuous_to_Ω_if_to_C $ continuous_of_continuous_uncurry _ $ Hmul_cont x,
+  Hmul_e_e := refl_trans_refl,
   left_Hmul_e := sorry,
   right_Hmul_e := sorry}
 

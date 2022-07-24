111953686b8a220a09aacb151d6ba2b77e2b06a4
Violeta Hernández <vi.hdz.p@gmail.com>
2022-07-24 12:06:41 -0500

    golf

--- src/order/antisymm_incomp.lean
+++ src/order/antisymm_incomp.lean
@@ -62,9 +62,9 @@ section incomp
 /-- The incomparability relation. -/
 def incomp_rel (a b : α) : Prop := ¬ r a b ∧ ¬ r b a
 
-@[simp] lemma antisymm_rel_compl : antisymm_rel (rᶜ) = incomp_rel r := rfl
+@[simp] lemma antisymm_rel_compl : antisymm_rel rᶜ = incomp_rel r := rfl
 
-@[simp] lemma incomp_rel_compl : incomp_rel (rᶜ) = antisymm_rel r :=
+@[simp] lemma incomp_rel_compl : incomp_rel rᶜ = antisymm_rel r :=
 by { ext, simp [incomp_rel, antisymm_rel, has_compl.compl] }
 
 lemma incomp_rel_swap : incomp_rel (swap r) = incomp_rel r := antisymm_rel_swap _

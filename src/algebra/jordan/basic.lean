3e8df8a9e586c978c2498aa551909a5e26b69a75
Christopher Hoskin <christopher.hoskin@gmail.com>
2022-07-25 15:37:58 +0100

    Motivation

--- src/algebra/jordan/basic.lean
+++ src/algebra/jordan/basic.lean
@@ -9,37 +9,19 @@ import data.real.basic
 import linear_algebra.basic
 
 /-!
-# Jordan algebras
+# Jordan rings
 
-Let `A` be a non-associative algebra (i.e. a module equipped with a bilinear multiplication
-operation). Then `A` is said to be a (commutative, linear) Jordan algebra if the multiplication is
-commutative and satisfies a weak associativity law known as the Jordan Identity: for all `a` and `b`
-in `A`,
+Let `A` be a non-unital, non-associative ring. Then `A` is said to be a (commutative, linear) Jordan
+ring if the multiplication is commutative and satisfies a weak associativity law known as the
+Jordan Identity: for all `a` and `b` in `A`,
 ```
 (a * b) * a^2 = a * (b * a^2)
 ```
-i.e. the operators of multiplication by `a` and `a^2` commute. Every associative algebra can be
-equipped with a symmetrized multiplication (characterized by `sym_alg.sym_mul_sym`) making it into a
-commutative Jordan algebra. Jordan algebras arising this way are said to be special.
+i.e. the operators of multiplication by `a` and `a^2` commute.
 
-There are also exceptional Jordan algebras which can be shown not to be the symmetrization of any
-associative algebra. The 3x3 matrices of octonions is the canonical example.
-
-Every Jordan algebra `A` has a triple product defined, for `a` `b` and `c` in `A` by
-$$
-{a\,b\,c} = (a * b) * c - (a * c) * b + a * (b * c).
-$$
-Via this triple product Jordan algebras are related to a number of other mathematical structures:
-Jordan triples, partial Jordan triples, Jordan pairs and quadratic Jordan algebras. In addition to
-their considerable algebraic interest ([mccrimmon2004]) these structures have been shown to have
-deep connections to mathematical physics, functional analysis and differential geometry. For more
-information about these connections the interested reader is referred to [alfsenshultz2003],
-[chu2012], [friedmanscarr2005], [iordanescu2003] and [upmeier1987].
-
-A more general concept of a (non-commutative) Jordan algebra can also be defined, as a
-(non-commutative, non-associative) algebra `A` where, for each `a` in `A`, the operators of left and
-right multiplication by `a` and `a^2` commute. Such algebras have connections to the Vidav-Palmer
-theorem [cabreragarciarodriguezpalacios2014].
+A more general concept of a (non-commutative) Jordan ring can also be defined, as a
+(non-commutative, non-associative) ring `A` where, for each `a` in `A`, the operators of left and
+right multiplication by `a` and `a^2` commute.
 
 A real Jordan algebra `A` can be introduced by
 ```lean
@@ -59,6 +41,29 @@ two) leaving quadratic algebras to those better versed in that theory.
 The conventional way to linearise the Jordan axiom is to equate coefficients (more formally, assume
 that the axiom holds in all field extensions). For simplicity we use brute force algebraic expansion
 and substitution instead.
+
+## Motivation
+
+Every Jordan algebra `A` has a triple product defined, for `a` `b` and `c` in `A` by
+$$
+{a\,b\,c} = (a * b) * c - (a * c) * b + a * (b * c).
+$$
+Via this triple product Jordan algebras are related to a number of other mathematical structures:
+Jordan triples, partial Jordan triples, Jordan pairs and quadratic Jordan algebras. In addition to
+their considerable algebraic interest ([mccrimmon2004]) these structures have been shown to have
+deep connections to mathematical physics, functional analysis and differential geometry. For more
+information about these connections the interested reader is referred to [alfsenshultz2003],
+[chu2012], [friedmanscarr2005], [iordanescu2003] and [upmeier1987].
+
+Every associative algebra can be equipped with a symmetrized multiplication (characterized by
+`sym_alg.sym_mul_sym`) making it into a commutative Jordan algebra. Jordan algebras arising this way
+are said to be special.
+
+There are also exceptional Jordan algebras which can be shown not to be the symmetrization of any
+associative algebra. The 3x3 matrices of octonions is the canonical example.
+
+Non-commutative Jordan algebras have connections to the Vidav-Palmer theorem
+[cabreragarciarodriguezpalacios2014].
 
 ## References
 

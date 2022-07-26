86346aed1f203e99ba37f4c67d86ef6e45be3f2f
Andrew Yang <36414270+erdOne@users.noreply.github.com>
2022-07-26 18:24:56 +0800

    Update arrow.lean

--- src/category_theory/arrow.lean
+++ src/category_theory/arrow.lean
@@ -99,7 +99,7 @@ comma.iso_mk l r h
 
 /-- A variant of `arrow.iso_mk` that creates an iso between two `arrow.mk`s with a better type
 signature. -/
-abbreviation iso_mk' {W X Y Z : C} (f : W ⟶ X) (g : Y ⟶ Z)
+abbreviation iso_mk' {W X Y Z : T} (f : W ⟶ X) (g : Y ⟶ Z)
   (e₁ : W ≅ Y) (e₂ : X ≅ Z) (h : e₁.hom ≫ g = f ≫ e₂.hom) : arrow.mk f ≅ arrow.mk g :=
 arrow.iso_mk e₁ e₂ h
 

5cd12413a88bddf5161b54790555465dc19e87b6
Thomas Browning <tb65536@uw.edu>
2022-07-27 15:15:36 -0700

    lint

--- src/group_theory/transfer.lean
+++ src/group_theory/transfer.lean
@@ -61,15 +61,13 @@ end subgroup
 
 namespace monoid_hom
 
-variables [fintype (G ⧸ H)]
-
 open mul_action subgroup subgroup.left_transversals
 
 /-- Given `ϕ : H →* A` from `H : subgroup G` to a commutative group `A`,
 the transfer homomorphism is `transfer ϕ : G →* A`. -/
 @[to_additive "Given `ϕ : H →+ A` from `H : add_subgroup G` to an additive commutative group `A`,
 the transfer homomorphism is `transfer ϕ : G →+ A`."]
-noncomputable def transfer : G →* A :=
+noncomputable def transfer [fintype (G ⧸ H)] : G →* A :=
 let T : left_transversals (H : set G) := inhabited.default in
 { to_fun := λ g, diff ϕ T (g • T),
   map_one' := by rw [one_smul, diff_self],
@@ -77,16 +75,16 @@ let T : left_transversals (H : set G) := inhabited.default in
 
 variables (T : left_transversals (H : set G))
 
-@[to_additive] lemma transfer_def (g : G) : transfer ϕ g = diff ϕ T (g • T) :=
+@[to_additive] lemma transfer_def [fintype (G ⧸ H)] (g : G) : transfer ϕ g = diff ϕ T (g • T) :=
 by rw [transfer, ←diff_mul_diff, ←smul_diff_smul, mul_comm, diff_mul_diff]; refl
 
-lemma quotient_group.out'_conj_pow_minimal_period_mem
+lemma _root_.quotient_group.out'_conj_pow_minimal_period_mem
   (g : G) (q : G ⧸ H) : q.out'⁻¹ * g ^ function.minimal_period ((•) g) q * q.out' ∈ H :=
 by rw [mul_assoc, ←quotient_group.eq', quotient_group.out_eq', ←smul_eq_mul, quotient.mk_smul_out',
   eq_comm, pow_smul_eq_iff_minimal_period_dvd]
 
 /-- Explicit computation of the transfer homomorphism. -/
-lemma transfer_eq_prod_quotient_orbit_rel_zpowers_quot
+lemma transfer_eq_prod_quotient_orbit_rel_zpowers_quot [fintype (G ⧸ H)]
   (g : G) [fintype (quotient (orbit_rel (zpowers g) (G ⧸ H)))] :
   transfer ϕ g = ∏ (q : quotient (orbit_rel (zpowers g) (G ⧸ H))),
     ϕ ⟨q.out'.out'⁻¹ * g ^ function.minimal_period ((•) g) q.out' * q.out'.out',
@@ -110,6 +108,10 @@ lemma transfer_eq_pow_aux (g : G)
   (key : ∀ (k : ℕ) (g₀ : G), g₀⁻¹ * g ^ k * g₀ ∈ H → g₀⁻¹ * g ^ k * g₀ = g ^ k) :
   g ^ H.index ∈ H :=
 begin
+  by_cases hH : H.index = 0,
+  { rw [hH, pow_zero],
+    exact H.one_mem },
+  haveI := fintype_of_index_ne_zero hH,
   classical,
   replace key : ∀ (k : ℕ) (g₀ : G), g₀⁻¹ * g ^ k * g₀ ∈ H → g ^ k ∈ H :=
   λ k g₀ hk, (_root_.congr_arg (∈ H) (key k g₀ hk)).mp hk,
@@ -124,7 +126,7 @@ begin
     fintype.card_congr (self_equiv_sigma_orbits (zpowers g) (G ⧸ H)), index_eq_card] using key,
 end
 
-lemma transfer_eq_pow (g : G)
+lemma transfer_eq_pow [fintype (G ⧸ H)] (g : G)
   (key : ∀ (k : ℕ) (g₀ : G), g₀⁻¹ * g ^ k * g₀ ∈ H → g₀⁻¹ * g ^ k * g₀ = g ^ k) :
   transfer ϕ g = ϕ ⟨g ^ H.index, transfer_eq_pow_aux g key⟩ :=
 begin

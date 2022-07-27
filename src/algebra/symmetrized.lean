b5cc9ae65ced6ffe58918c678a3a255fb266b52c
Christopher Hoskin <christopher.hoskin@gmail.com>
2022-07-27 05:39:15 +0100

    Add reference to module

--- src/algebra/symmetrized.lean
+++ src/algebra/symmetrized.lean
@@ -4,6 +4,7 @@ Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Christopher Hoskin
 -/
 import algebra.jordan.basic
+import algebra.module.basic
 
 /-!
 # Symmetrized algebra

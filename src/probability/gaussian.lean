e9b7a5243980aeedaf24caa1a20310cbb318410c
Kexing <kexing.ying19@imperial.ac.uk>
2022-07-28 12:55:26 +0300

    fix definition

--- src/probability/gaussian.lean
+++ src/probability/gaussian.lean
@@ -3,37 +3,43 @@ import probability.notation
 import analysis.convex.topology
 
 /-
-Given a topological vector space `α`, a Borel measure `μ` on `α` is a Gaussian if for
-all continuous linear functional `l : α → ℝ`, the push-forward measure `μ.map l` is a Gaussian on
-`ℝ`.
+We would like to define the Gaussian measure on ℝ.
+There are two ways of doing this.
 
-So we first need a Gaussian on `ℝ`.
+(1). Given `E` a topological vector space equipped with the measure `μ`, we say `f : E → ℝ` is the
+  standard Gaussian random variable with respect to `μ` if
+  ```
+    μ.with_density f = (volume : measure ℝ).with_density (λ x, √(2π)⁻¹ exp (-x² / 2)).
+  ```
+  Then, we say `μ` is a Gaussian measure if for all `l : E →L[ℝ] ℝ`, there exists some `m s : ℝ`
+  such that `l = sf + m` where `f` is the standard Gaussian.
+
+(2). Define the Gaussian measure on ℝ directly by saying `μ : measure ℝ` is a Gaussian measure
+  if there exists `m s : ℝ` such that
+  ```
+    μ = if s ≠ 0 then (volume : measure ℝ).with_density
+      (λ x, √(2πs²)⁻¹ exp (-(x - m)² / 2s²)) else δ(m)
+  ```
+  Then, we say a measure `μ` on the topological vector space `E` is Gaussian if for all
+  `l : E →L[ℝ] ℝ`, `μ.map l` is a Gaussian measure on `ℝ`.
+
+The first definition has the advantage of not having a if then else definition while it is
+easier to work with the second definition as we have the density readily. We will use the
+second definition.
 -/
 
-open_locale nnreal ennreal probability_theory measure_theory
+open_locale nnreal ennreal probability_theory measure_theory real
 
 namespace measure_theory
 
-noncomputable def gaussian_density (m : ℝ) : ℝ → ℝ≥0∞ :=
-λ x, ennreal.of_real $ (2 * real.pi)⁻¹ * real.exp (-(x - m)^2 / 2)
+open real measure
 
-def real_gaussian (μ : measure ℝ) (m : ℝ) (s : ℝ≥0∞) : Prop :=
-∂μ/∂(volume : measure ℝ) = s • gaussian_density m
+noncomputable def gaussian_density (m s : ℝ) : ℝ → ℝ≥0∞ :=
+λ x, ennreal.of_real $ (sqrt (2 * π * s^2))⁻¹ * exp (-(2 * s^2)⁻¹ * (x - m)^2)
 
-variables {E : Type*} [add_comm_group E] [module ℝ E] [topological_space E]
-  [topological_add_group E] [has_continuous_const_smul ℝ E]
+def real_gaussian (μ : measure ℝ) (m s : ℝ) : Prop :=
+if s ≠ 0 then μ = (volume : measure ℝ).with_density (gaussian_density m s) else μ = dirac m
 
-def gaussian {m0 : measurable_space E} (μ : measure E) : Prop :=
-∀ l : E →L[ℝ] ℝ, ∃ m s, real_gaussian (μ.map l) m s
 
-lemma gaussian.real_gaussian {μ : measure ℝ} (hμ : gaussian μ) : ∃ m s, real_gaussian μ m s :=
-begin
-  sorry
-end
-
-lemma absolutely_continuous_gaussian {μ : measure ℝ} (hμ : gaussian μ) : μ ≪ volume :=
-begin
-  sorry
-end
 
 end measure_theory

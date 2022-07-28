ec9a1576897daacd488b3cb467461cabcf651413
ADedecker <anatolededecker@gmail.com>
2022-07-28 00:53:16 -0400

    English

--- src/analysis/normed_space/compact_operator.lean
+++ src/analysis/normed_space/compact_operator.lean
@@ -346,7 +346,7 @@ begin
     apply_instance },
   -- Since `σ₁₂ c⁻¹` = `(σ₁₂ c)⁻¹`, we have to prove that `K ⊆ σ₁₂ c • U`.
   rw [σ₁₂.map_inv, ← subset_set_smul_iff₀ (σ₁₂.map_ne_zero.mpr hcnz)],
-  -- But `σ₁₂` is isometric, so `∥σ₁₂ c∥ = ∥c∥ > r`, which concludes since
+  -- But `σ₁₂` is isometric, so `∥σ₁₂ c∥ = ∥c∥ > r`, which concludes the argument since
   -- `∀ a : 𝕜₂, r ≤ ∥a∥ → K ⊆ a • U`.
   refine hrU (σ₁₂ c) _,
   rw ring_hom_isometric.is_iso,

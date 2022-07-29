477ac7ece5bc0524c743bfd212c15afd91a600fe
Joël Riou <joel.riou@universite-paris-saclay.fr>
2022-07-29 17:24:08 +0530

    tidied the refactor

--- src/category_theory/limits/shapes/strong_epi.lean
+++ src/category_theory/limits/shapes/strong_epi.lean
@@ -4,14 +4,15 @@ Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Markus Himmel
 -/
 import category_theory.balanced
+import category_theory.lifting_properties.basic
 
 /-!
 # Strong epimorphisms
 
-In this file, we define strong epimorphisms. A strong epimorphism is an epimorphism `f`, such
-that for every commutative square with `f` at the top and a monomorphism at the bottom, there is
-a diagonal morphism making the two triangles commute. This lift is necessarily unique (as shown in
-`comma.lean`).
+In this file, we define strong epimorphisms. A strong epimorphism is an epimorphism `f`
+which has the (unique) left lifting property with respect ot monomorphisms. Similarly,
+a strong monomorphisms in a monomorphism which has the (unique) right lifting property
+with respect to epimorphisms.
 
 ## Main results
 
@@ -39,22 +40,20 @@ variables {C : Type u} [category.{v} C]
 
 variables {P Q : C}
 
-/-- A strong epimorphism `f` is an epimorphism such that every commutative square with `f` at the
-    top and a monomorphism at the bottom has a lift. -/
+/-- A strong epimorphism `f` is an epimorphism which has the left lifting property
+with respect to monomorphisms. -/
 class strong_epi (f : P ⟶ Q) : Prop :=
 (epi : epi f)
-(has_lift : Π {X Y : C} {u : P ⟶ X} {v : Q ⟶ Y} {z : X ⟶ Y} [mono z] (h : u ≫ z = f ≫ v),
-  arrow.has_lift $ arrow.hom_mk' h)
+(llp : ∀ ⦃X Y : C⦄ (z : X ⟶ Y) [mono z], has_lifting_property f z)
 
-/-- A strong monomorphism `f` is a monomorphism such that every commutative square with `f` at the
-    bottom and an epimorphism at the top has a lift. -/
+/-- A strong monomorphism `f` is a monomorphism which has the right lifting property
+with respect to epimorphisms. -/
 class strong_mono (f : P ⟶ Q) : Prop :=
 (mono : mono f)
-(has_lift : Π {X Y : C} {u : X ⟶ P} {v : Y ⟶ Q} {z : X ⟶ Y} [epi z] (h : u ≫ f = z ≫ v),
-  arrow.has_lift $ arrow.hom_mk' h)
+(rlp : ∀ ⦃X Y : C⦄ (z : X ⟶ Y) [epi z], has_lifting_property z f)
 
-attribute [instance] strong_epi.has_lift
-attribute [instance] strong_mono.has_lift
+attribute [instance, priority 100] strong_epi.llp
+attribute [instance, priority 100] strong_mono.rlp
 
 @[priority 100]
 instance epi_of_strong_epi (f : P ⟶ Q) [strong_epi f] : epi f := strong_epi.epi
@@ -68,69 +67,55 @@ variables {R : C} (f : P ⟶ Q) (g : Q ⟶ R)
 /-- The composition of two strong epimorphisms is a strong epimorphism. -/
 lemma strong_epi_comp [strong_epi f] [strong_epi g] : strong_epi (f ≫ g) :=
 { epi := epi_comp _ _,
-  has_lift :=
-  begin
-    introsI,
-    have h₀ : u ≫ z = f ≫ g ≫ v, by simpa [category.assoc] using h,
-    let w : Q ⟶ X := arrow.lift (arrow.hom_mk' h₀),
-    have h₁ : w ≫ z = g ≫ v, by rw arrow.lift_mk'_right,
-    exact arrow.has_lift.mk ⟨(arrow.lift (arrow.hom_mk' h₁) : R ⟶ X), by simp, by simp⟩
-  end }
+  llp := by { introsI, apply_instance, }, }
 
 /-- The composition of two strong monomorphisms is a strong monomorphism. -/
 lemma strong_mono_comp [strong_mono f] [strong_mono g] : strong_mono (f ≫ g) :=
 { mono := mono_comp _ _,
-  has_lift :=
-  begin
-    introsI,
-    have h₀ : (u ≫ f) ≫ g = z ≫ v, by simpa [category.assoc] using h,
-    let w : Y ⟶ Q := arrow.lift (arrow.hom_mk' h₀),
-    have h₁ : u ≫ f = z ≫ w, by rw arrow.lift_mk'_left,
-    exact arrow.has_lift.mk ⟨(arrow.lift (arrow.hom_mk' h₁) : Y ⟶ P), by simp, by simp⟩
-  end }
+  rlp := by { introsI, apply_instance, }, }
 
 /-- If `f ≫ g` is a strong epimorphism, then so is `g`. -/
 lemma strong_epi_of_strong_epi [strong_epi (f ≫ g)] : strong_epi g :=
 { epi := epi_of_epi f g,
-  has_lift :=
-  begin
+  llp := begin
     introsI,
-    have h₀ : (f ≫ u) ≫ z = (f ≫ g) ≫ v, by simp only [category.assoc, h],
-    exact arrow.has_lift.mk
-      ⟨(arrow.lift (arrow.hom_mk' h₀) : R ⟶ X), (cancel_mono z).1 (by simp [h]), by simp⟩,
-  end }
+    constructor,
+    intros u v sq,
+    have h₀ : (f ≫ u) ≫ z = (f ≫ g) ≫ v, by simp only [category.assoc, sq.w],
+    exact comm_sq.has_lift.mk' ⟨(comm_sq.mk h₀).lift,
+      by simp only [← cancel_mono z, category.assoc, comm_sq.fac_right, sq.w], by simp⟩,
+  end, }
 
 /-- If `f ≫ g` is a strong monomorphism, then so is `f`. -/
 lemma strong_mono_of_strong_mono [strong_mono (f ≫ g)] : strong_mono f :=
 { mono := mono_of_mono f g,
-  has_lift :=
-  begin
+  rlp := begin
     introsI,
-    have h₀ : u ≫ f ≫ g = z ≫ v ≫ g, by rw reassoc_of h,
-    exact arrow.has_lift.mk
-      ⟨(arrow.lift (arrow.hom_mk' h₀) : Y ⟶ P), by simp, (cancel_epi z).1 (by simp [h])⟩
-  end }
+    constructor,
+    intros u v sq,
+    have h₀ : u ≫ f ≫ g = z ≫ v ≫ g, by rw reassoc_of sq.w,
+    exact comm_sq.has_lift.mk' ⟨(comm_sq.mk h₀).lift, by simp, by simp [← cancel_epi z, sq.w]⟩,
+  end, }
 
 /-- An isomorphism is in particular a strong epimorphism. -/
 @[priority 100] instance strong_epi_of_is_iso [is_iso f] : strong_epi f :=
 { epi := by apply_instance,
-  has_lift := λ X Y u v z _ h, arrow.has_lift.mk ⟨inv f ≫ u, by simp, by simp [h]⟩ }
+  llp := λ X Y z hz, has_lifting_property.of_left_iso _ _, }
 
 /-- An isomorphism is in particular a strong monomorphism. -/
 @[priority 100] instance strong_mono_of_is_iso [is_iso f] : strong_mono f :=
 { mono := by apply_instance,
-  has_lift := λ X Y u v z _ h, arrow.has_lift.mk
-    ⟨v ≫ inv f, by simp [← category.assoc, ← h], by simp⟩ }
+  rlp := λ X Y z hz, has_lifting_property.of_right_iso _ _, }
 
 end
 
 /-- A strong epimorphism that is a monomorphism is an isomorphism. -/
 lemma is_iso_of_mono_of_strong_epi (f : P ⟶ Q) [mono f] [strong_epi f] : is_iso f :=
-⟨⟨arrow.lift $ arrow.hom_mk' $ show 𝟙 P ≫ f = f ≫ 𝟙 Q, by simp, by tidy⟩⟩
+⟨⟨(comm_sq.mk (show 𝟙 P ≫ f = f ≫ 𝟙 Q, by simp)).lift, by tidy⟩⟩
 
 /-- A strong monomorphism that is an epimorphism is an isomorphism. -/
 lemma is_iso_of_epi_of_strong_mono (f : P ⟶ Q) [epi f] [strong_mono f] : is_iso f :=
-⟨⟨arrow.lift $ arrow.hom_mk' $ show 𝟙 P ≫ f = f ≫ 𝟙 Q, by simp, by tidy⟩⟩
+⟨⟨(comm_sq.mk (show 𝟙 P ≫ f = f ≫ 𝟙 Q, by simp)).lift, by tidy⟩⟩
 
 section
 variables (C)

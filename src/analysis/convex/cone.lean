42215b89d4c8b6283d44c1c52f75471ae54d5b3a
Apurva Nakade <apurvnakade@gmail.com>
2022-07-29 21:09:45 -0700

    fix import

--- src/analysis/convex/cone.lean
+++ src/analysis/convex/cone.lean
@@ -4,7 +4,7 @@ Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Yury Kudryashov, Frédéric Dupuis
 -/
 import analysis.convex.hull
-import analysis.inner_product_space.projection
+import analysis.inner_product_space.basic
 
 /-!
 # Convex cones

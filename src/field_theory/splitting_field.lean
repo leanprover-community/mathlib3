9e07903926a00113032ea15a0e483eb12f24998e
Thomas Browning <tb65536@uw.edu>
2022-08-01 12:47:23 -0700

    fix

--- src/field_theory/splitting_field.lean
+++ src/field_theory/splitting_field.lean
@@ -3,6 +3,7 @@ Copyright (c) 2018 Chris Hughes. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Chris Hughes
 -/
+import field_theory.intermediate_field
 import ring_theory.adjoin_root
 
 /-!

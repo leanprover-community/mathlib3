637be4cc51dc01400730a82bbc14afba07cb2b9e
Jake Levinson <levinson.jake@gmail.com>
2022-08-02 17:52:39 -0400

    linter edits

--- src/combinatorics/young_diagram.lean
+++ src/combinatorics/young_diagram.lean
@@ -3,10 +3,6 @@ Copyright (c) 2022 Jake Levinson. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Jake Levinson
 -/
-
-import tactic
-import data.set_like.basic
-import data.set.basic
 import order.upper_lower
 
 /-!
@@ -20,18 +16,22 @@ A Young diagram is a finite set of up-left justified boxes:
 □□□
 □
 
-Equivalently, a lower set in ℕ × ℕ in the product partial order.
+Equivalently, a lower set in `ℕ × ℕ` in the product partial order. We write `(i, j) ∈ μ`
+to say that `(i, j)` (in matrix coordinates) is in the Young diagram `μ`.
 
 ## Main functions:
 
-  - basic properties and definitions involving shape, size, row and column
-    lengths, and corners of a Young diagram
+  - `young_diagram` : basic definition
+  - `young_diagram.card` : the cardinality (number of boxes)
 
-  - [TODO] various constructors:
-      [young_diagram.has_empty]
-      [young_diagram.of_row_lens]
-      [young_diagram.outer_corner.add]
-      [young_diagram.inner_corner.del]
+  - various constructors:
+      `young_diagram.has_empty` : the empty Young diagram
+
+## Notation:
+
+In "English notation", a Young diagram is drawn so that (i1, j1) ≤ (i2, j2)
+means (i1, j1) is weakly up-and-left (northwest) of (i2, j2). This terminology is used
+below, e.g. in `young_diagram.nw_of`.
 
 ## Tags
 
@@ -45,37 +45,48 @@ Young diagram
 
 section young_diagram
 
+/-- A Young diagram is a lower set in ℕ × ℕ -/
 @[ext] structure young_diagram :=
-  (cells : finset (ℕ × ℕ))
-  (is_lower_set : is_lower_set (cells : set (ℕ × ℕ)))
+(cells : finset (ℕ × ℕ))
+(is_lower_set : is_lower_set (cells : set (ℕ × ℕ)))
 
 instance young_diagram.set_like : set_like young_diagram (ℕ × ℕ) :=
-{ coe := λ μ, (μ.cells : set (ℕ × ℕ)),
-  coe_injective' := λ μ ν h, by { rwa [young_diagram.ext_iff, ← finset.coe_inj],} }
+{ coe            := λ μ, (μ.cells : set (ℕ × ℕ)),
+  coe_injective' := λ μ ν h, by { rwa [young_diagram.ext_iff, ← finset.coe_inj] } }
 
 @[simp] lemma young_diagram.mem_cells {μ : young_diagram} (c : ℕ × ℕ) :
   c ∈ μ.cells ↔ c ∈ μ := iff.rfl
 
-def young_diagram.nw_of (μ : young_diagram)
-  {i1 i2 j1 j2 : ℕ} (hi : i1 ≤ i2) (hj : j1 ≤ j2) (hcell : (i2, j2) ∈ μ) :
-  (i1, j1) ∈ μ := μ.is_lower_set (prod.mk_le_mk.mpr ⟨hi, hj⟩) hcell
+/-- In "English notation", a Young diagram is drawn so that (i1, j1) ≤ (i2, j2)
+    means (i1, j1) is weakly up-and-left (northwest) of (i2, j2). -/
+lemma young_diagram.nw_of (μ : young_diagram) {i1 i2 j1 j2 : ℕ}
+  (hi : i1 ≤ i2) (hj : j1 ≤ j2) (hcell : (i2, j2) ∈ μ) : (i1, j1) ∈ μ :=
+μ.is_lower_set (prod.mk_le_mk.mpr ⟨hi, hj⟩) hcell
 
 instance young_diagram.has_subset : has_subset young_diagram :=
-  { subset := λ μ ν, μ.cells ⊆ ν.cells }
+{ subset := λ μ ν, μ.cells ⊆ ν.cells }
 
+/-- Cardinality of a Young diagram -/
 def young_diagram.card (μ : young_diagram) : ℕ := μ.cells.card
-lemma young_diagram.card_def (μ : young_diagram) {i : ℕ} : μ.card = μ.cells.card := rfl
+lemma young_diagram.card_def (μ : young_diagram) : μ.card = μ.cells.card := rfl
 
+/- (TODO) row and column lengths, and corners of a Young diagram.
+   (TODO: the transpose of a `young_diagram`)
+   (TODO: a `young_diagram` from a list of row or column lengths)
+   (TODO: adding or removing a corner box from a `young_diagram`) -/
 
 section μ_empty
 
-/- The empty Young diagram -/
+/-! The empty Young diagram -/
 
 instance young_diagram.has_emptyc : has_emptyc young_diagram :=
-{ emptyc := { cells := finset.empty, is_lower_set := λ _ _ _ h, h, } }
+{ emptyc := { cells := finset.empty, is_lower_set := λ _ _ _ h, h } }
 
+/-- The empty Young diagram -/
 def μ_empty := (∅ : young_diagram)
 @[simp] lemma μ_empty_card : (∅ : young_diagram).card = 0 := rfl
+
+instance : inhabited young_diagram := ⟨∅⟩
 
 end μ_empty
 

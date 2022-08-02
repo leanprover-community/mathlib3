f78fb66b502feb59eb437ee008c946e4cc27d683
erd1 <the.erd.one@gmail.com>
2022-08-02 10:52:50 +0800

    move stuff

--- src/ring_theory/ring_hom/integral.lean
+++ src/ring_theory/ring_hom/integral.lean
@@ -35,25 +35,12 @@ end
 lemma is_integral_stable_under_base_change :
   stable_under_base_change (λ R S _ _ f, by exactI f.is_integral) :=
 begin
-  classical,
   introv R h x,
   resetI,
   apply tensor_product.induction_on x,
   { apply is_integral_zero },
-  { intros x y,
-    obtain ⟨p, hp, hp'⟩ := h y,
-    refine ⟨(p.map (algebra_map R S)).scale_roots x, _, _⟩,
-    { rw polynomial.monic_scale_roots_iff, exact hp.map _ },
-    convert @polynomial.scale_roots_eval₂_mul (S ⊗[R] T) S _ _ _ include_left.to_ring_hom
-      (1 ⊗ₜ y) x using 2,
-    { simp only [alg_hom.to_ring_hom_eq_coe, alg_hom.coe_to_ring_hom, include_left_apply,
-        tmul_mul_tmul, _root_.mul_one, _root_.one_mul] },
-    convert (mul_zero _).symm,
-    rw [polynomial.eval₂_map, include_left_comp_algebra_map, ← polynomial.eval₂_map],
-    convert polynomial.eval₂_at_apply include_right.to_ring_hom y,
-    rw [polynomial.eval_map, hp', map_zero] },
-  { intros x y hx hy,
-    exact is_integral_add _ hx hy }
+  { intros x y, exact is_integral.tmul x (h y) },
+  { intros x y hx hy, exact is_integral_add _ hx hy }
 end
 
 end ring_hom

e2d927ec93ef8f932214dc0101c6cd25384c44a9
Jake Levinson <levinson.jake@gmail.com>
2022-08-02 17:29:07 -0400

    changed 'carrier' to 'cells'

--- src/combinatorics/young_diagram.lean
+++ src/combinatorics/young_diagram.lean
@@ -46,25 +46,25 @@ Young diagram
 section young_diagram
 
 @[ext] structure young_diagram :=
-  (carrier : finset (ℕ × ℕ))
-  (is_lower_set : is_lower_set (carrier : set (ℕ × ℕ)))
+  (cells : finset (ℕ × ℕ))
+  (is_lower_set : is_lower_set (cells : set (ℕ × ℕ)))
 
 instance young_diagram.set_like : set_like young_diagram (ℕ × ℕ) :=
-{ coe := λ μ, (μ.carrier : set (ℕ × ℕ)),
+{ coe := λ μ, (μ.cells : set (ℕ × ℕ)),
   coe_injective' := λ μ ν h, by { rwa [young_diagram.ext_iff, ← finset.coe_inj],} }
 
-@[simp] lemma young_diagram.mem_carrier {μ : young_diagram} (c : ℕ × ℕ) :
-  c ∈ μ.carrier ↔ c ∈ μ := iff.rfl
+@[simp] lemma young_diagram.mem_cells {μ : young_diagram} (c : ℕ × ℕ) :
+  c ∈ μ.cells ↔ c ∈ μ := iff.rfl
 
 def young_diagram.nw_of (μ : young_diagram)
   {i1 i2 j1 j2 : ℕ} (hi : i1 ≤ i2) (hj : j1 ≤ j2) (hcell : (i2, j2) ∈ μ) :
   (i1, j1) ∈ μ := μ.is_lower_set (prod.mk_le_mk.mpr ⟨hi, hj⟩) hcell
 
 instance young_diagram.has_subset : has_subset young_diagram :=
-  { subset := λ μ ν, μ.carrier ⊆ ν.carrier }
+  { subset := λ μ ν, μ.cells ⊆ ν.cells }
 
-def young_diagram.card (μ : young_diagram) : ℕ := μ.carrier.card
-lemma young_diagram.card_def (μ : young_diagram) {i : ℕ} : μ.card = μ.carrier.card := rfl
+def young_diagram.card (μ : young_diagram) : ℕ := μ.cells.card
+lemma young_diagram.card_def (μ : young_diagram) {i : ℕ} : μ.card = μ.cells.card := rfl
 
 
 section μ_empty
@@ -72,7 +72,7 @@ section μ_empty
 /- The empty Young diagram -/
 
 instance young_diagram.has_emptyc : has_emptyc young_diagram :=
-{ emptyc := { carrier := finset.empty, is_lower_set := λ _ _ _ h, h, } }
+{ emptyc := { cells := finset.empty, is_lower_set := λ _ _ _ h, h, } }
 
 def μ_empty := (∅ : young_diagram)
 @[simp] lemma μ_empty_card : (∅ : young_diagram).card = 0 := rfl

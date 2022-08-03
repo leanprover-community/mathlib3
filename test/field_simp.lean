1ee24418e29c78f27dfd1967eec0db72a0b1bd8c
Jon <jon.eugster@gmx.ch>
2022-08-03 14:41:59 +0100

    variable names

--- test/field_simp.lean
+++ test/field_simp.lean
@@ -8,44 +8,44 @@ import tactic.ring
 Check that `field_simp` works for units of a ring.
 -/
 
-variables {R : Type*} [comm_ring R]
+variables {R : Type*} [comm_ring R] (a b c d e f g : R) (u₁ u₂ : Rˣ)
 
 /--
 Check that `divp_add_divp_same` takes priority over `divp_add_divp`.
 -/
-example (a b : R) (u : Rˣ) : a /ₚ u + b /ₚ u = (a + b) /ₚ u :=
+example : a /ₚ u₁ + b /ₚ u₁ = (a + b) /ₚ u₁ :=
 by field_simp
 
 /--
 Check that `divp_sub_divp_same` takes priority over `divp_sub_divp`.
 -/
-example (a b : R) (u : Rˣ) : a /ₚ u - b /ₚ u = (a - b) /ₚ u :=
+example : a /ₚ u₁ - b /ₚ u₁ = (a - b) /ₚ u₁ :=
 by field_simp
 
 /--
 Combining `eq_divp_iff_mul_eq` and `divp_eq_iff_mul_eq`.
 -/
-example (a c : R) (b d : Rˣ)  : a /ₚ b = c /ₚ d ↔ a * d = c * b :=
+example : a /ₚ u₁ = b /ₚ u₂ ↔ a * u₂ = b * u₁ :=
 by field_simp
 
 /--
-Making sure inverses of units are rewritten propperly.
+Making sure inverses of units are rewritten properly.
 -/
-example (x : Rˣ) : ↑x⁻¹ = 1 /ₚ x :=
+example : ↑u₁⁻¹ = 1 /ₚ u₁ :=
 by field_simp
 
 /--
 Checking arithmetic expressions.
 -/
-example (a b c d e f g : R) (u : Rˣ) :
-(f - (e + c * -(a /ₚ u) * b + d) - g) = (f * u - (e * u + c * (-a) * b + d * u) - g * u) /ₚ u :=
+example : (f - (e + c * -(a /ₚ u₁) * b + d) - g) =
+  (f * u₁ - (e * u₁ + c * (-a) * b + d * u₁) - g * u₁) /ₚ u₁ :=
 by field_simp
 
 /--
 Division of units.
 -/
-example (a : R) (u₁ u₂ : Rˣ) : a /ₚ (u₁ / u₂) = a * u₂ /ₚ u₁ :=
+example : a /ₚ (u₁ / u₂) = a * u₂ /ₚ u₁ :=
 by field_simp
 
-example (a : R) (b c : Rˣ) : a /ₚ b /ₚ c = a /ₚ (c * b) :=
+example : a /ₚ u₁ /ₚ u₂ = a /ₚ (u₂ * u₁) :=
 by field_simp

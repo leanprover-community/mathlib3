3d02cfc49a7596f29b43f3d23582fa9ec1df56db
Yaël Dillies <yael.dillies@gmail.com>
2022-08-03 23:02:06 +0000

    fix analysis.specific_limits.normed

--- src/analysis/specific_limits/normed.lean
+++ src/analysis/specific_limits/normed.lean
@@ -558,14 +558,12 @@ begin
            nat.succ_sub_succ_eq_sub, tsub_zero],
   apply (normed_field.tendsto_zero_smul_of_tendsto_zero_of_bounded hf0
     ⟨b, eventually_map.mpr $ eventually_of_forall $ λ n, hgb $ n+1⟩).cauchy_seq.add,
-  apply (cauchy_seq_range_of_norm_bounded _ _ (_ : ∀ n, _ ≤ b * |f(n+1) - f(n)|)).neg,
-  { exact normed_uniform_group },
+  refine (cauchy_seq_range_of_norm_bounded _ _ (λ n, _ : ∀ n, _ ≤ b * |f(n+1) - f(n)|)).neg,
   { simp_rw [abs_of_nonneg (sub_nonneg_of_le (hfa (nat.le_succ _))), ← mul_sum],
     apply real.uniform_continuous_mul_const.comp_cauchy_seq,
     simp_rw [sum_range_sub, sub_eq_add_neg],
     exact (tendsto.cauchy_seq hf0).add_const },
-  { intro n,
-    rw [norm_smul, mul_comm],
+  { rw [norm_smul, mul_comm],
     exact mul_le_mul_of_nonneg_right (hgb _) (abs_nonneg _) },
 end
 

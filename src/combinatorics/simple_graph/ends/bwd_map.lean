63778fda4e8c5bad17a3bbf4371156e6394a53a8
Rémi Bottinelli <remi.bottinelli@bluewin.ch>
2022-08-04 13:42:18 +0200

    starting new ends

---
+++ src/combinatorics/simple_graph/ends/bwd_map.lean
@@ -0,0 +1,156 @@
+import data.set.finite
+import data.sym.sym2
+import combinatorics.simple_graph.basic
+import combinatorics.simple_graph.connectivity
+import topology.metric_space.basic
+import data.setoid.partition
+import set_theory.cardinal.basic
+
+import .mathlib
+import .reachable_outside
+
+open function
+open finset
+open set
+open classical
+open simple_graph.walk
+open relation
+open cardinal
+
+universes u v w
+
+
+
+noncomputable theory
+local attribute [instance] prop_decidable
+
+namespace simple_graph
+
+
+variables  {V : Type u}
+           (G : simple_graph V)
+
+
+
+namespace bwd_map
+
+open ro_component
+
+
+def bwd_map (Gpc : G.preconnected) {K L : finset V} (K_sub_L : K ⊆ L) : ro_components G L → ro_components G K :=
+λ D,
+let
+  itexists := ro_component.of_ro_set G
+              K D.val
+              (ro_component.nempty G L D.val D.prop)
+              (ro_component.ro_of_ro_component G Gpc K L K_sub_L D.val D.prop)
+, C := some itexists
+, C_prop := some_spec itexists
+in
+  ⟨C,C_prop.1⟩
+
+
+def bwd_map_def  (Gpc : G.preconnected) {K L : finset V} (K_sub_L : K ⊆ L)
+  (D : ro_components G L) (C : ro_components G K) :
+  bwd_map G Gpc K_sub_L D = C ↔ D.val ⊆ C.val :=
+let
+  itexists := ro_component.of_ro_set G K D (ro_component.nempty G L D.val D.prop) (ro_component.ro_of_ro_component G Gpc K L K_sub_L D.val D.prop),
+  C' := some itexists,
+  C_prop' := some_spec itexists
+in
+  begin
+    have eqdef : bwd_map G Gpc K_sub_L D =
+           ⟨C',C_prop'.1⟩, by
+    { unfold bwd_map, dsimp,simpa,},
+    split,
+    { intro eq, cases eq, exact C_prop'.2,},
+    { intro sub,
+      have lol := ro_component.unique_of_ro_set G K D (ro_component.nempty G L D.val D.prop) (ro_component.ro_of_ro_component G Gpc K L K_sub_L D.val D.prop), -- the fact that D is still connected wrt K … should be easy
+      rcases lol with ⟨uniqueC,uniqueC_is_good,unicity⟩,
+      rw eqdef,
+      apply subtype.ext_val, simp,
+      rw (unicity C' C_prop'),
+      rw (unicity C.val ⟨C.prop,sub⟩).symm,simp,
+    }
+  end
+
+def bwd_map_sub  (Gpc : G.preconnected) {K L : finset V} (K_sub_L : K ⊆ L)
+  (D : ro_components G L) : D.val ⊆ (bwd_map G Gpc K_sub_L D).val :=
+begin
+  apply (bwd_map_def G Gpc K_sub_L D (bwd_map G Gpc K_sub_L D)).mp,
+  reflexivity,
+end
+
+lemma bwd_map_refl'  (Gpc : G.preconnected) (K : finset V) (C : ro_components G K) : bwd_map G Gpc (set.subset.refl K) C = C :=
+by {rw bwd_map_def}
+
+lemma bwd_map_refl  (Gpc : G.preconnected) (K : finset V) : bwd_map G Gpc (subset.refl K) = id :=
+funext (bwd_map_refl' G Gpc K)
+
+
+/- TODO : A converse lemma to the one below: if some component is in the image of all bwd_maps, it is infinite-/
+
+lemma bwd_map_surjective_on_inf [locally_finite G]  (Gpc : G.preconnected) {K L : finset V} (K_sub_L : K ⊆ L)
+  (C : ro_components G K) (Cinf : C.val.infinite) : C ∈ set.range (bwd_map G Gpc K_sub_L) :=
+
+begin
+  rcases C with ⟨C,C_comp⟩,
+
+  let L_comps := ro_components G L,
+  let L_comps_in_C := { D : set V | D ∈ ro_components G L ∧ D ⊆ C},
+  have sub : L_comps_in_C ⊆ L_comps, from (λ D ⟨a,b⟩,  a),
+  have : L_comps_in_C.finite, from set.finite.subset (ro_component.finite G Gpc L) sub,
+  have : (⋃₀ L_comps_in_C).infinite, by {
+    rintro hfin,
+    have lol := set.infinite.mono (ro_component.sub_ro_components_cover G Gpc K L K_sub_L C C_comp) Cinf,
+    have := set.finite_union.mpr ⟨finset.finite_to_set L,hfin⟩,
+    exact lol this,
+  },
+
+  have : ∃ D : set V, D ∈ L_comps_in_C ∧ D.infinite, by {
+    by_contra' all_fin,
+    simp at all_fin,
+    exact this ( set.finite.sUnion
+                 ‹L_comps_in_C.finite›
+                 ( λ D ⟨D_comp,D_sub_C⟩, all_fin D D_comp D_sub_C) ),},
+  rcases this with ⟨D,⟨⟨D_comp_L,D_sub_C⟩,Dinf⟩⟩,
+  simp,
+  use [D,D_comp_L],
+  rw (bwd_map_def G Gpc K_sub_L ⟨D,D_comp_L⟩ ⟨C,C_comp⟩),
+  exact D_sub_C,
+end
+
+
+def bwd_map_comp  (Gpc : G.preconnected) {K L M : finset V} (K_sub_L : K ⊆ L) (L_sub_M : L ⊆ M) :
+  (bwd_map G Gpc K_sub_L) ∘ (bwd_map G Gpc L_sub_M) = (bwd_map G Gpc (K_sub_L.trans L_sub_M)) :=
+begin
+  apply funext,
+  rintro E,
+  let D := bwd_map G Gpc L_sub_M E,
+  let C := bwd_map G Gpc K_sub_L D,
+  apply eq.symm,
+  unfold function.comp,
+  apply (bwd_map_def G Gpc (K_sub_L.trans L_sub_M) E C).mpr,
+  exact (bwd_map_sub G Gpc L_sub_M E).trans (bwd_map_sub G Gpc K_sub_L D),
+end
+
+def bwd_map_comp'   (Gpc : G.preconnected) {K L M : finset V} (K_sub_L : K ⊆ L) (L_sub_M : L ⊆ M) (E : ro_components G M) :
+  bwd_map G Gpc K_sub_L (bwd_map G Gpc L_sub_M E) = bwd_map G Gpc (K_sub_L.trans L_sub_M) E :=
+begin
+  let D := bwd_map G Gpc L_sub_M E,
+  let C := bwd_map G Gpc K_sub_L D,
+  apply eq.symm,
+  apply (bwd_map_def G Gpc (K_sub_L.trans L_sub_M) E C).mpr,
+  exact (bwd_map_sub G Gpc L_sub_M E).trans (bwd_map_sub G Gpc K_sub_L D),
+end
+
+def bwd_map_diamond  (Gpc : G.preconnected) {K L L' M : finset V}
+  (K_sub_L : K ⊆ L) (L_sub_M : L ⊆ M)  (K_sub_L' : K ⊆ L') (L'_sub_M : L' ⊆ M)
+  (E : ro_components G M) :
+  bwd_map G Gpc K_sub_L (bwd_map G Gpc L_sub_M E) = bwd_map G Gpc K_sub_L' (bwd_map G Gpc L'_sub_M E) :=
+by rw [bwd_map_comp',bwd_map_comp']
+
+
+end bwd_map
+
+end simple_graph
--- src/combinatorics/simple_graph/ends/end_limit_construction.lean
+++ src/combinatorics/simple_graph/ends/end_limit_construction.lean
@@ -2,19 +2,22 @@ import category_theory.filtered
 import topology.category.Top.limits
 import data.finset.basic
 
-import .ends
+import .bwd_map
 
 open category_theory
 open opposite
 open simple_graph
+open classical
+open simple_graph.bwd_map
 
 /- Implementing Kyle Miller's suggestion:
 https://leanprover.zulipchat.com/#narrow/stream/116395-maths/topic/Geometric.20group.20theory/near/290624806 -/
 
 noncomputable theory
+local attribute [instance] prop_decidable
 
 universes u v w
-variables {V : Type u} [decidable_eq V] [Vinf : set.infinite (set.univ : set V)] (h : V ≃ ℕ)
+variables {V : Type u} [decidable_eq V] (h : V ≃ ℕ)
 variables (G : simple_graph V) (Gpc : G.preconnected) [locally_finite G]
 
 instance finset_preorder : preorder (finset V) := {
@@ -32,56 +35,23 @@ instance finset_directed : is_directed (finset V) (≤) := {
   directed := λ A B, ⟨A ∪ B, ⟨finset.subset_union_left A B, finset.subset_union_right A B⟩⟩ }
 
 /-The functor assigning a finite set in `V` to the set of infinite connected components in its complement-/
-def ComplInfComp : (finset V)ᵒᵖ ⥤ Type u := {
-  obj := λ A, inf_ro_components G (unop A),
-  map := λ A B f, ends.bwd_map G Gpc (le_of_hom f.unop),
-  map_id' := by {intro, funext, simp, apply ends.bwd_map_refl',}, -- tricky to get right
-  map_comp' := by {intros, funext, simp, apply eq.symm, apply ends.bwd_map_comp',},
+def ComplComp : (finset V)ᵒᵖ ⥤ Type u := {
+  obj := λ A, ro_components G (unop A),
+  map := λ A B f, bwd_map G Gpc (le_of_hom f.unop),
+  map_id' := by {intro, funext, simp, apply bwd_map_refl',}, -- tricky to get right
+  map_comp' := by {intros, funext, simp, apply eq.symm, apply bwd_map_comp',},
   }
 
-/-
-instance : is_cofiltered (finset V) := {
-  cocone_objs := _,
-  cocone_maps := _,
-  nonempty := _ }
--/
-
-instance obj_nonempty :  ∀ (j : (finset V)ᵒᵖ), nonempty ((ComplInfComp G Gpc).obj j) := by {
-  intro, show (nonempty (inf_ro_components G (unop _))), generalize : (unop _) = x,
-   apply ro_component.infinite_graph_to_inf_components_nonempty,
-  assumption, sorry, -- exact Vinf,
+instance obj_nonempty (Vinf : set.infinite (@set.univ V)) :  ∀ (j : (finset V)ᵒᵖ), nonempty ((ComplComp G Gpc).obj j) := by {
+  intro K,
+  obtain ⟨C,Ccomp,Cinf⟩ := ro_component.infinite_graph_to_inf_components_nonempty G Gpc K.unop Vinf,
+  use [C,Ccomp],
 }
 
-instance obj_fintype : Π (j : (finset V)ᵒᵖ), fintype ((ComplInfComp G Gpc).obj j) := by {
-  intro, show (fintype (inf_ro_components G (unop _))), generalize : unop _ = x,
-  apply ro_component.inf_components_finite, exact Gpc,
+instance obj_fintype : Π (j : (finset V)ᵒᵖ), fintype ((ComplComp G Gpc).obj j) := by {
+  intro K,
+  exact (ro_component.finite G Gpc K.unop).fintype,
 }
 
-theorem exists_end_inf_graph : (ComplInfComp G Gpc).sections.nonempty :=
-  nonempty_sections_of_fintype_inverse_system (ComplInfComp G Gpc)
-
--- a slightly modified definition of ends, meant to make proving equivalence with the sections definition easier
-def ends :=
-  {f : Π (K : finset V), inf_ro_components G K // ∀ A B : finset V, Π h: A ⊆ B, ends.bwd_map G Gpc h (f B) = f A}
-
-lemma ends_eq_sections :  (ComplInfComp G Gpc).sections ≃ (ends G Gpc)  :=
-begin
-  rw [functor.sections, ends, ComplInfComp], simp,
-  split, rotate 2,
-  { rintro ⟨Kfmly : Π (K : (finset V)ᵒᵖ), G.inf_ro_components (unop K), hbwd⟩,
-    let endK : Π (K : finset V), G.inf_ro_components K := by {
-      intro K, rw [← opposite.unop_op K], apply Kfmly, },
-    use endK,
-    intros _ _ hsub,
-    exact hbwd (quiver.hom.op (hom_of_le hsub)), },
-  { rintro ⟨Kfmly, hbwd⟩,
-    apply subtype.mk, rotate,
-    let endKop : Π (K : (finset V)ᵒᵖ), G.inf_ro_components (unop K) := by {intro Kop, apply Kfmly,},
-    use endKop,
-    intros _ _ f,
-    simp, apply hbwd, },
-  { simp [function.left_inverse],
-    intros, ext, refl, },
-  { simp [function.right_inverse, function.left_inverse],
-    intros, ext, refl, }
-end
+theorem exists_end_inf_graph (Vinf : set.infinite  (@set.univ V)) : (ComplComp G Gpc).sections.nonempty :=
+  @nonempty_sections_of_fintype_inverse_system _ _ _ (ComplComp G Gpc) _ (obj_nonempty G Gpc Vinf)

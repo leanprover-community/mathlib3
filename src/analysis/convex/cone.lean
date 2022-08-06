65ffd358f737f2f5973d522990b6e12ff729bf14
apurvnakade <apurvnakade@gmail.com>
2022-08-05 22:17:45 -0500

    Add `mem_map` rfl lemma.

--- src/analysis/convex/cone.lean
+++ src/analysis/convex/cone.lean
@@ -191,6 +191,9 @@ def map (f : E →ₗ[𝕜] F) (S : convex_cone 𝕜 E) : convex_cone 𝕜 F :=
   add_mem' := λ y₁ ⟨x₁, hx₁, hy₁⟩ y₂ ⟨x₂, hx₂, hy₂⟩, hy₁ ▸ hy₂ ▸ f.map_add x₁ x₂ ▸
     mem_image_of_mem f (S.add_mem hx₁ hx₂) }
 
+@[simp] lemma mem_map {f : E →ₗ[𝕜] F} {S : convex_cone 𝕜 E} {x : F} : x ∈ S.map f ↔ x ∈ f '' S :=
+iff.rfl
+
 lemma map_map (g : F →ₗ[𝕜] G) (f : E →ₗ[𝕜] F) (S : convex_cone 𝕜 E) :
   (S.map f).map g = S.map (g.comp f) :=
 set_like.coe_injective $ image_image g f S

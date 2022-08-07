3e322bd6748f528e48a96d2c1977fcc1b72f57f4
erd1 <the.erd.one@gmail.com>
2022-08-08 05:28:10 +0800

    attempt

--- src/linear_algebra/smodeq.lean
+++ src/linear_algebra/smodeq.lean
@@ -6,6 +6,7 @@ Authors: Kenny Lau
 
 import data.polynomial.eval
 import ring_theory.ideal.quotient
+import group_theory.subgroup.basic
 
 /-!
 # modular equivalence for submodule
@@ -14,63 +15,77 @@ import ring_theory.ideal.quotient
 open submodule
 open_locale polynomial
 
-variables {R : Type*} [ring R]
-variables {M : Type*} [add_comm_group M] [module R M] (U U₁ U₂ : submodule R M)
+variables {M X : Type*} [add_comm_group M] [set_like X M] [add_subgroup_class X M] (U U₁ U₂ : X)
+variables {R : Type*} [ring R] [module R M] (Ug : add_subgroup M) (Us : submodule R M)
 variables {x x₁ x₂ y y₁ y₂ z z₁ z₂ : M}
 variables {N : Type*} [add_comm_group N] [module R N] (V V₁ V₂ : submodule R N)
 
-/-- A predicate saying two elements of a module are equivalent modulo a submodule. -/
+/-- A predicate saying two elements of a additive group are equivalent modulo a subgroup. -/
 def smodeq (x y : M) : Prop :=
-(submodule.quotient.mk x : M ⧸ U) = submodule.quotient.mk y
+x - y ∈ U
 
 notation x ` ≡ `:50 y ` [SMOD `:50 N `]`:0 := smodeq N x y
 
-variables {U U₁ U₂}
+variables {U U₁ U₂ Us Ug}
 
-protected lemma smodeq.def : x ≡ y [SMOD U] ↔
-  (submodule.quotient.mk x : M ⧸ U) = submodule.quotient.mk y := iff.rfl
+protected lemma smodeq.def : x ≡ y [SMOD U] ↔ x - y ∈ U :=
+iff.rfl
 
 namespace smodeq
 
-lemma sub_mem : x ≡ y [SMOD U] ↔ x - y ∈ U :=
-by rw [smodeq.def, submodule.quotient.eq]
+protected lemma smodeq.iff_coe_eq : x ≡ y [SMOD Ug] ↔
+  (x : M ⧸ Ug) = y :=
+by { rw [quotient_add_group.eq, ← neg_mem_iff, smodeq.def, neg_add', neg_neg] }
 
-@[simp] theorem top : x ≡ y [SMOD (⊤ : submodule R M)] :=
-(submodule.quotient.eq ⊤).2 mem_top
+protected lemma smodeq.iff_mkq_eq : x ≡ y [SMOD Us] ↔
+  Us.mkq x = Us.mkq y :=
+@smodeq.iff_coe_eq _ _ Us.to_add_subgroup x y
 
-@[simp] theorem bot : x ≡ y [SMOD (⊥ : submodule R M)] ↔ x = y :=
-by rw [smodeq.def, submodule.quotient.eq, mem_bot, sub_eq_zero]
+@[simp] theorem top_submodule : x ≡ y [SMOD (⊤ : submodule R M)] :=
+trivial
+
+@[simp] theorem bot_submodule : x ≡ y [SMOD (⊥ : submodule R M)] ↔ x = y :=
+by rw [smodeq.def, mem_bot, sub_eq_zero]
 
 @[mono] theorem mono (HU : U₁ ≤ U₂) (hxy : x ≡ y [SMOD U₁]) : x ≡ y [SMOD U₂] :=
-(submodule.quotient.eq U₂).2 $ HU $ (submodule.quotient.eq U₁).1 hxy
+HU hxy
 
-@[refl] theorem refl : x ≡ x [SMOD U] := eq.refl _
+@[refl] theorem refl : x ≡ x [SMOD U] := by { rw [smodeq.def, sub_self], exact zero_mem _ }
 
-@[symm] theorem symm (hxy : x ≡ y [SMOD U]) : y ≡ x [SMOD U] := hxy.symm
+@[symm] theorem symm (hxy : x ≡ y [SMOD U]) : y ≡ x [SMOD U] :=
+by rwa [smodeq.def, ← neg_mem_iff, neg_sub]
 
 @[trans] theorem trans (hxy : x ≡ y [SMOD U]) (hyz : y ≡ z [SMOD U]) : x ≡ z [SMOD U] :=
-hxy.trans hyz
+by { rw smodeq.def, convert add_mem hxy hyz, rw [sub_add, sub_sub_cancel] }
+
+lemma comm : x ≡ y [SMOD U] ↔ y ≡ x [SMOD U] := ⟨symm, symm⟩
 
 theorem add (hxy₁ : x₁ ≡ y₁ [SMOD U]) (hxy₂ : x₂ ≡ y₂ [SMOD U]) : x₁ + x₂ ≡ y₁ + y₂ [SMOD U] :=
-by { rw smodeq.def at hxy₁ hxy₂ ⊢, simp_rw [quotient.mk_add, hxy₁, hxy₂] }
+by { rw smodeq.def, convert add_mem hxy₁ hxy₂, rw add_sub_add_comm }
 
-theorem smul (hxy : x ≡ y [SMOD U]) (c : R) : c • x ≡ c • y [SMOD U] :=
-by { rw smodeq.def at hxy ⊢, simp_rw [quotient.mk_smul, hxy] }
+theorem smul (hxy : x ≡ y [SMOD Us]) (c : R) : c • x ≡ c • y [SMOD Us] :=
+by { rw smodeq.iff_mkq_eq at hxy ⊢, simp_rw [Us.mkq.map_smul, hxy] }
 
 theorem zero : x ≡ 0 [SMOD U] ↔ x ∈ U :=
-by rw [smodeq.def, submodule.quotient.eq, sub_zero]
+by rw [smodeq.def, sub_zero]
 
-theorem map (hxy : x ≡ y [SMOD U]) (f : M →ₗ[R] N) : f x ≡ f y [SMOD U.map f] :=
-(submodule.quotient.eq _).2 $ f.map_sub x y ▸ mem_map_of_mem $ (submodule.quotient.eq _).1 hxy
+lemma congr (hxy₁ : x₁ ≡ y₁ [SMOD U]) (hxy₂ : x₂ ≡ y₂ [SMOD U]) :
+  x₁ ≡ x₂ [SMOD U] ↔ y₁ ≡ y₂ [SMOD U] :=
+⟨λ e, hxy₁.symm.trans (e.trans hxy₂), λ e, hxy₁.trans (e.trans hxy₂.symm)⟩
+
+lemma mem_iff (hxy : x ≡ y [SMOD U]) : x ∈ U ↔ y ∈ U :=
+by { rw [← zero, ← zero], exact congr hxy refl }
+
+theorem map (hxy : x ≡ y [SMOD Us]) (f : M →ₗ[R] N) : f x ≡ f y [SMOD Us.map f] :=
+by { rw [smodeq.def, ← map_sub], exact mem_map_of_mem hxy }
 
 theorem comap {f : M →ₗ[R] N} (hxy : f x ≡ f y [SMOD V]) : x ≡ y [SMOD V.comap f] :=
-(submodule.quotient.eq _).2 $ show f (x - y) ∈ V,
-from (f.map_sub x y).symm ▸ (submodule.quotient.eq _).1 hxy
+by rwa [smodeq.def, mem_comap, map_sub]
 
 lemma eval {R : Type*} [comm_ring R] {I : ideal R} {x y : R} (h : x ≡ y [SMOD I])
   (f : R[X]) : f.eval x ≡ f.eval y [SMOD I] :=
 begin
-  rw [smodeq.def] at h ⊢,
+  rw [smodeq.iff_mkq_eq] at h ⊢,
   show ideal.quotient.mk I (f.eval x) = ideal.quotient.mk I (f.eval y),
   change ideal.quotient.mk I x = ideal.quotient.mk I y at h,
   rw [← polynomial.eval₂_at_apply, ← polynomial.eval₂_at_apply, h],

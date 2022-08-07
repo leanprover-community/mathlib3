3693bc8800c8a7f005aa5d24958319374611e4a6
Eric Wieser <wieser.eric@gmail.com>
2022-08-07 21:41:23 +0000

    chore(data/polynomial/induction): cleanup
    This moves some lemmas to better homes, and replaces uses of `submodule.span R (s : set R)` with `ideal.span`.
    Some proofs have been golfed to enable the shuffling, but all lemma statements are defeq to the originals.

--- src/data/polynomial/induction.lean
+++ src/data/polynomial/induction.lean
@@ -4,11 +4,15 @@ Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Chris Hughes, Johannes Hölzl, Scott Morrison, Jens Wagemaker
 -/
 import data.polynomial.coeff
+import ring_theory.ideal.basic
 
 /-!
-# Theory of univariate polynomials
+# Induction on polynomials
 
-The main results are `induction_on` and `as_sum`.
+This file contains lemmas dealing with different flavours of induction on polynomials.
+See also `data/polynomial/inductions.lean` (with an `s`!).
+
+The main result is `polynomial.induction_on`.
 -/
 
 noncomputable theory
@@ -23,17 +27,6 @@ variables {R : Type u} {S : Type v} {T : Type w} {ι : Type x} {k : Type y} {A :
 
 section semiring
 variables [semiring R] {p q r : R[X]}
-
-lemma sum_C_mul_X_eq (p : R[X]) : p.sum (λn a, C a * X^n) = p :=
-begin
-  ext n,
-  simp only [polynomial.sum, X_pow_eq_monomial, coeff_monomial, mul_one, finset_sum_coeff,
-    C_mul_monomial, not_not, mem_support_iff, finset.sum_ite_eq', ite_eq_left_iff],
-  exact λ h, h.symm
-end
-
-lemma sum_monomial_eq (p : R[X]) : p.sum (λn a, monomial n a) = p :=
-by simp only [monomial_eq_C_mul_X, sum_C_mul_X_eq]
 
 @[elab_as_eliminator] protected lemma induction_on {M : R[X] → Prop} (p : R[X])
   (h_C : ∀a, M (C a))
@@ -66,45 +59,23 @@ and it holds for monomials.
 polynomial.induction_on p (h_monomial 0) h_add
 (λ n a h, by { rw ← monomial_eq_C_mul_X at ⊢, exact h_monomial _ _ })
 
-section coeff
-
-theorem coeff_mul_monomial (p : R[X]) (n d : ℕ) (r : R) :
-  coeff (p * monomial n r) (d + n) = coeff p d * r :=
-by rw [monomial_eq_C_mul_X, ←X_pow_mul, ←mul_assoc, coeff_mul_C, coeff_mul_X_pow]
-
-theorem coeff_monomial_mul (p : R[X]) (n d : ℕ) (r : R) :
-  coeff (monomial n r * p) (d + n) = r * coeff p d :=
-by rw [monomial_eq_C_mul_X, mul_assoc, coeff_C_mul, X_pow_mul, coeff_mul_X_pow]
-
--- This can already be proved by `simp`.
-theorem coeff_mul_monomial_zero (p : R[X]) (d : ℕ) (r : R) :
-  coeff (p * monomial 0 r) d = coeff p d * r :=
-coeff_mul_monomial p 0 d r
-
--- This can already be proved by `simp`.
-theorem coeff_monomial_zero_mul (p : R[X]) (d : ℕ) (r : R) :
-  coeff (monomial 0 r * p) d = r * coeff p d :=
-coeff_monomial_mul p 0 d r
-
-end coeff
 
 open submodule polynomial set
-variables {f : R[X]} {I : submodule R[X] R[X]}
+variables {f : R[X]} {I : ideal R[X]}
 
+-- TODO: fix whatever this typo from #5693 was supposed to say
 /--  If the coefficients of a polynomial belong to n ideal contains the submodule span of the
 coefficients of a polynomial. -/
-lemma span_le_of_coeff_mem_C_inverse (cf : ∀ (i : ℕ), f.coeff i ∈ (C ⁻¹' I.carrier)) :
-  (span R[X] {g | ∃ i, g = C (f.coeff i)}) ≤ I :=
+lemma span_le_of_C_coeff_mem (cf : ∀ (i : ℕ), C (f.coeff i) ∈ I) :
+  ideal.span {g | ∃ i, g = C (f.coeff i)} ≤ I :=
 begin
-  refine bInter_subset_of_mem _,
-  rintros _ ⟨i, rfl⟩,
-  exact set_like.mem_coe.mpr (cf i),
+  simp only [@eq_comm _ _ (C _)] {single_pass := tt},
+  exact (ideal.span_le.trans range_subset_iff).mpr cf,
 end
 
-lemma mem_span_C_coeff :
-  f ∈ span R[X] {g : R[X] | ∃ i : ℕ, g = (C (coeff f i))} :=
+lemma mem_span_C_coeff : f ∈ ideal.span {g : R[X] | ∃ i : ℕ, g = C (coeff f i)} :=
 begin
-  let p := span R[X] {g : R[X] | ∃ i : ℕ, g = (C (coeff f i))},
+  let p := ideal.span {g : R[X] | ∃ i : ℕ, g = C (coeff f i)},
   nth_rewrite 0 (sum_C_mul_X_eq f).symm,
   refine submodule.sum_mem _ (λ n hn, _),
   dsimp,
@@ -115,10 +86,8 @@ begin
   rw monomial_eq_C_mul_X,
 end
 
-lemma exists_coeff_not_mem_C_inverse :
-  f ∉ I → ∃ i : ℕ , coeff f i ∉ (C ⁻¹'  I.carrier) :=
-imp_of_not_imp_not _ _
-  (λ cf, not_not.mpr ((span_le_of_coeff_mem_C_inverse (not_exists_not.mp cf)) mem_span_C_coeff))
+lemma exists_C_coeff_not_mem : f ∉ I → ∃ i : ℕ, C (coeff f i) ∉ I :=
+not.imp_symm $ λ cf, span_le_of_C_coeff_mem (not_exists_not.mp cf) mem_span_C_coeff
 
 end semiring
 

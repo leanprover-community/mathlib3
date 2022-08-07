712a80a04f763b321cea6b89555ba9326b48af0b
Jujian Zhang <jujian.zhang1998@outlook.com>
2022-08-07 18:33:17 +0100

    apply suggetions, rename, universe level explicit

--- src/algebra/category/Module/change_of_rings.lean
+++ src/algebra/category/Module/change_of_rings.lean
@@ -10,21 +10,20 @@ import algebra.category.Module.basic
 
 ## Main definitions
 
-* `category_theory.Module.restrict_scalars.functor`: given rings `R, S` and a ring homomorphism
-  `R ⟶ S`, then `restrict_scalars.functor : Module S ⥤ Module R` is defined by `M ↦ M` where
-  `M : S-module` is seen as `R-module` by `r • m := f r • m` and `S`-linear map `l : M ⟶ M'` is
-  `R`-linear as well.
+* `category_theory.Module.restrict_scalars`: given rings `R, S` and a ring homomorphism `R ⟶ S`,
+  then `restrict_scalars : Module S ⥤ Module R` is defined by `M ↦ M` where `M : S-module` is seen
+  as `R-module` by `r • m := f r • m` and `S`-linear map `l : M ⟶ M'` is `R`-linear as well.
 -/
 
 
 namespace category_theory.Module
 
+universes u₁ u₂ v
+
 namespace restrict_scalars
 
-universes u₁ u₂
-
 variables {R : Type u₁} {S : Type u₂} [ring R] [ring S] (f : R →+* S)
-variable (M : Module S)
+variable (M : Module.{v} S)
 
 /-- Any `S`-module M is also an `R`-module via a ring homomorphism `f : R ⟶ S` by defining
     `r • m := f r • m` (`module.comp_hom`). This is called restriction of scalars. -/
@@ -37,8 +36,6 @@ def obj' : Module R :=
 protected def has_smul : has_smul R M :=
 (module.comp_hom M f).to_has_smul
 
-end
-
 localized "notation r ` r•[` f `] ` :=
   @@has_smul.smul (restrict_scalars.has_smul f _) r"
   in change_of_rings
@@ -49,22 +46,23 @@ localized "notation r ` r•[` f `] ` :=
 Given an `S`-linear map `g : M → M'` between `S`-modules, `g` is also `R`-linear between `M` and
 `M'` by means of restriction of scalars.
 -/
-@[simps] def map' {M M' : Module S} (g : M ⟶ M') :
+@[simps] def map' {M M' : Module.{v} S} (g : M ⟶ M') :
   obj' f M ⟶ obj' f M' :=
 { map_smul' := λ r (x : M), by simp,
   ..g }
+
+end restrict_scalars
 
 /--
 The restriction of scalars operation is functorial. For any `f : R →+* S` a ring homomorphism,
 * an `S`-module `M` can be considered as `R`-module by `r • m = f r • m`
 * an `S`-linear map is also `R`-linear
 -/
-@[simps, nolint check_univs] protected def functor : Module S ⥤ Module R :=
-{ obj := obj' f,
-  map := λ _ _, map' f,
+@[simps] def restrict_scalars {R : Type u₁} {S : Type u₂} [ring R] [ring S] (f : R →+* S) :
+  Module.{v} S ⥤ Module.{v} R :=
+{ obj := restrict_scalars.obj' f,
+  map := λ _ _, restrict_scalars.map' f,
   map_id' := λ _, linear_map.ext $ λ m, rfl,
   map_comp' := λ _ _ _ g h, linear_map.ext $ λ m, rfl }
 
-end restrict_scalars
-
-end category.Module
+end category_theory.Module

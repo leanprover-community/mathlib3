8f1c9a3a825af21f4dac05c435027d578a24f853
Kexing <kexing.ying19@imperial.ac.uk>
2022-08-09 15:31:06 +0300

    rename

--- src/measure_theory/function/conditional_expectation/indicator.lean
+++ src/measure_theory/function/conditional_expectation/indicator.lean
@@ -4,7 +4,7 @@ Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Rémy Degenne
 -/
 
-import measure_theory.function.condexp.basic
+import measure_theory.function.conditional_expectation.indicator.basic
 
 /-!
 

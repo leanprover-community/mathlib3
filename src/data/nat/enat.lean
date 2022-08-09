d31deaaf7d927c3e42b06a783059285bfec472d9
Yury G. Kudryashov <urkud@urkud.name>
2022-08-09 00:46:37 -0400

    Fix lint

--- src/data/nat/enat.lean
+++ src/data/nat/enat.lean
@@ -1,7 +1,7 @@
 /-
 Copyright (c) 2022 Yury Kudryashov. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
-Author: Yury Kudryashov
+Authors: Yury Kudryashov
 -/
 import data.nat.lattice
 import data.nat.succ_pred

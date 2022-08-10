b630a2d4cf0a581ff880f6471689217596958c50
Jujian Zhang <jujian.zhang1998@outlook.com>
2022-08-10 17:50:15 +0100

    write simp lemmas by hand

--- src/algebra/category/Module/change_of_rings.lean
+++ src/algebra/category/Module/change_of_rings.lean
@@ -29,27 +29,15 @@ variable (M : Module.{v} S)
     `r • m := f r • m` (`module.comp_hom`). This is called restriction of scalars. -/
 def obj' : Module R :=
 { carrier := M,
-  is_add_comm_group := infer_instance,
   is_module := module.comp_hom M f }
-
-/-- The `R`-scalar multiplication on `S`-module M defined by `r • m := f r • m` -/
-protected def has_smul : has_smul R M :=
-(module.comp_hom M f).to_has_smul
-
-localized "notation r ` r•[` f `] ` :=
-  @@has_smul.smul (restrict_scalars.has_smul f _) r"
-  in change_of_rings
-
-@[simp] lemma smul_def (r : R) (m : M) : r r•[f] m = f r • m := rfl
 
 /--
 Given an `S`-linear map `g : M → M'` between `S`-modules, `g` is also `R`-linear between `M` and
 `M'` by means of restriction of scalars.
 -/
-@[simps] def map' {M M' : Module.{v} S} (g : M ⟶ M') :
+def map' {M M' : Module.{v} S} (g : M ⟶ M') :
   obj' f M ⟶ obj' f M' :=
-{ map_smul' := λ r (x : M), by simp,
-  ..g }
+{ map_smul' := λ r, g.map_smul (f r), ..g }
 
 end restrict_scalars
 
@@ -58,11 +46,17 @@ The restriction of scalars operation is functorial. For any `f : R →+* S` a ri
 * an `S`-module `M` can be considered as `R`-module by `r • m = f r • m`
 * an `S`-linear map is also `R`-linear
 -/
-@[simps] def restrict_scalars {R : Type u₁} {S : Type u₂} [ring R] [ring S] (f : R →+* S) :
+def restrict_scalars {R : Type u₁} {S : Type u₂} [ring R] [ring S] (f : R →+* S) :
   Module.{v} S ⥤ Module.{v} R :=
 { obj := restrict_scalars.obj' f,
   map := λ _ _, restrict_scalars.map' f,
   map_id' := λ _, linear_map.ext $ λ m, rfl,
   map_comp' := λ _ _ _ g h, linear_map.ext $ λ m, rfl }
+
+@[simp] lemma restrict_scalars.map_apply {R : Type u₁} {S : Type u₂} [ring R] [ring S] (f : R →+* S)
+  {M M' : Module.{v} S} (g : M ⟶ M') (x) : (restrict_scalars f).map g x = g x := rfl
+
+@[simp] lemma restrict_scalars.smul_def {R : Type u₁} {S : Type u₂} [ring R] [ring S] (f : R →+* S)
+  {M : Module.{v} S} (r : R) (m : (restrict_scalars f).obj M) : r • m = (f r • m : M) := rfl
 
 end category_theory.Module

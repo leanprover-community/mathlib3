7e276593c3c70c0e0a6360ff8895ca70d50ce2ed
Joseph Myers <jsm@polyomino.org.uk>
2022-08-11 14:03:07 +0000

    feat(analysis/special_functions/trigonometric/angle): `to_real`
    Define `real.angle.to_real`, converting a `real.angle` to a real in
    the interval `Ioc (-π) π` (the same interval used by `complex.arg`),
    and prove some associated lemmas.  This is the inverse operation to
    coercing the result of `complex.arg` to `real.angle`, and is also
    useful for converting between oriented and unoriented angles.

--- src/analysis/special_functions/trigonometric/angle.lean
+++ src/analysis/special_functions/trigonometric/angle.lean
@@ -5,6 +5,7 @@ Authors: Calle Sönne
 -/
 import analysis.special_functions.trigonometric.basic
 import algebra.char_zero.quotient
+import algebra.order.to_interval_mod
 import data.sign
 
 /-!
@@ -271,6 +272,131 @@ cos_antiperiodic θ
 @[simp] lemma cos_sub_pi (θ : angle) : cos (θ - π) = -cos θ :=
 cos_antiperiodic.sub_eq θ
 
+@[simp] lemma coe_to_Ico_mod (θ ψ : ℝ) : ↑(to_Ico_mod ψ two_pi_pos θ) = (θ : angle) :=
+begin
+  rw angle_eq_iff_two_pi_dvd_sub,
+  refine ⟨to_Ico_div ψ two_pi_pos θ, _⟩,
+  rw [to_Ico_mod_sub_self, zsmul_eq_mul, mul_comm]
+end
+
+@[simp] lemma coe_to_Ioc_mod (θ ψ : ℝ) : ↑(to_Ioc_mod ψ two_pi_pos θ) = (θ : angle) :=
+begin
+  rw angle_eq_iff_two_pi_dvd_sub,
+  refine ⟨to_Ioc_div ψ two_pi_pos θ, _⟩,
+  rw [to_Ioc_mod_sub_self, zsmul_eq_mul, mul_comm]
+end
+
+/-- Convert a `real.angle` to a real number in the interval `Ioc (-π) π`. -/
+def to_real (θ : angle) : ℝ :=
+(to_Ioc_mod_periodic (-π) two_pi_pos).lift θ
+
+lemma to_real_coe (θ : ℝ) : (θ : angle).to_real = to_Ioc_mod (-π) two_pi_pos θ :=
+rfl
+
+lemma to_real_coe_eq_self_iff {θ : ℝ} : (θ : angle).to_real = θ ↔ -π < θ ∧ θ ≤ π :=
+begin
+  rw [to_real_coe, to_Ioc_mod_eq_self two_pi_pos],
+  ring_nf
+end
+
+lemma to_real_coe_eq_self_iff_mem_Ioc {θ : ℝ} : (θ : angle).to_real = θ ↔ θ ∈ set.Ioc (-π) π :=
+by rw [to_real_coe_eq_self_iff, ←set.mem_Ioc]
+
+lemma to_real_injective : function.injective to_real :=
+begin
+  intros θ ψ h,
+  induction θ using real.angle.induction_on,
+  induction ψ using real.angle.induction_on,
+  simpa [to_real_coe, to_Ioc_mod_eq_to_Ioc_mod, zsmul_eq_mul, mul_comm _ (2 * π),
+         ←angle_eq_iff_two_pi_dvd_sub, eq_comm] using h,
+end
+
+@[simp] lemma coe_to_real (θ : angle): (θ.to_real : angle) = θ :=
+begin
+  induction θ using real.angle.induction_on,
+  exact coe_to_Ioc_mod _ _
+end
+
+lemma neg_pi_lt_to_real (θ : angle) : -π < θ.to_real :=
+begin
+  induction θ using real.angle.induction_on,
+  exact left_lt_to_Ioc_mod _ two_pi_pos _
+end
+
+lemma to_real_le_pi (θ : angle) : θ.to_real ≤ π :=
+begin
+  induction θ using real.angle.induction_on,
+  convert to_Ioc_mod_le_right _ two_pi_pos _,
+  ring
+end
+
+lemma abs_to_real_le_pi (θ : angle) : |θ.to_real| ≤ π :=
+abs_le.2 ⟨(neg_pi_lt_to_real _).le, to_real_le_pi _⟩
+
+lemma to_real_mem_Ioc (θ : angle) : θ.to_real ∈ set.Ioc (-π) π :=
+begin
+  induction θ using real.angle.induction_on,
+  convert to_Ioc_mod_mem_Ioc _ two_pi_pos _,
+  ring
+end
+
+@[simp] lemma to_Ioc_mod_to_real (θ : angle): to_Ioc_mod (-π) two_pi_pos θ.to_real = θ.to_real :=
+begin
+  induction θ using real.angle.induction_on,
+  rw to_real_coe,
+  exact to_Ioc_mod_to_Ioc_mod _ _ _ _
+end
+
+@[simp] lemma to_real_zero : (0 : angle).to_real = 0 :=
+begin
+  rw [←coe_zero, to_real_coe_eq_self_iff],
+  exact ⟨(left.neg_neg_iff.2 real.pi_pos), real.pi_pos.le⟩
+end
+
+@[simp] lemma to_real_eq_zero_iff {θ : angle} : θ.to_real = 0 ↔ θ = 0 :=
+begin
+  nth_rewrite 0 ←to_real_zero,
+  exact to_real_injective.eq_iff
+end
+
+@[simp] lemma to_real_pi : (π : angle).to_real = π :=
+begin
+  rw [to_real_coe_eq_self_iff],
+  exact ⟨left.neg_lt_self real.pi_pos, le_refl _⟩
+end
+
+@[simp] lemma to_real_eq_pi_iff {θ : angle} : θ.to_real = π ↔ θ = π :=
+begin
+  nth_rewrite 0 ←to_real_pi,
+  exact to_real_injective.eq_iff
+end
+
+lemma abs_to_real_coe_eq_self_iff {θ : ℝ} : |(θ : angle).to_real| = θ ↔ 0 ≤ θ ∧ θ ≤ π :=
+⟨λ h, h ▸ ⟨abs_nonneg _, abs_to_real_le_pi _⟩, λ h,
+ (to_real_coe_eq_self_iff.2 ⟨(left.neg_neg_iff.2 real.pi_pos).trans_le h.1, h.2⟩).symm ▸
+    abs_eq_self.2 h.1⟩
+
+lemma abs_to_real_neg_coe_eq_self_iff {θ : ℝ} : |(-θ : angle).to_real| = θ ↔ 0 ≤ θ ∧ θ ≤ π :=
+begin
+  refine ⟨λ h, h ▸ ⟨abs_nonneg _, abs_to_real_le_pi _⟩, λ h, _⟩,
+  by_cases hnegpi : θ = π, { simp [hnegpi, real.pi_pos.le] },
+  rw [←coe_neg, to_real_coe_eq_self_iff.2 ⟨neg_lt_neg (lt_of_le_of_ne h.2 hnegpi),
+                                           (neg_nonpos.2 h.1).trans real.pi_pos.le⟩, abs_neg,
+      abs_eq_self.2 h.1]
+end
+
+@[simp] lemma sin_to_real (θ : angle) : real.sin θ.to_real = sin θ :=
+begin
+  conv_rhs { rw ←coe_to_real θ },
+  refl
+end
+
+@[simp] lemma cos_to_real (θ : angle) : real.cos θ.to_real = cos θ :=
+begin
+  conv_rhs { rw ←coe_to_real θ },
+  refl
+end
+
 /-- The sign of a `real.angle` is `0` if the angle is `0` or `π`, `1` if the angle is strictly
 between `0` and `π` and `-1` is the angle is strictly between `-π` and `0`. It is defined as the
 sign of the sine of the angle. -/
@@ -302,6 +428,75 @@ by simp [sign_antiperiodic.sub_eq']
 
 lemma sign_eq_zero_iff {θ : angle} : θ.sign = 0 ↔ θ = 0 ∨ θ = π :=
 by rw [sign, sign_eq_zero_iff, sin_eq_zero_iff]
+
+lemma to_real_neg_iff_sign_neg {θ : angle} : θ.to_real < 0 ↔ θ.sign = -1 :=
+begin
+  rw [sign, ←sin_to_real, sign_eq_neg_one_iff],
+  rcases lt_trichotomy θ.to_real 0 with (h|h|h),
+  { exact ⟨λ _, real.sin_neg_of_neg_of_neg_pi_lt h (neg_pi_lt_to_real θ), λ _, h⟩ },
+  { simp [h] },
+  { exact ⟨λ hn, false.elim (h.asymm hn),
+           λ hn, false.elim (hn.not_le (sin_nonneg_of_nonneg_of_le_pi h.le (to_real_le_pi θ)))⟩ }
+end
+
+lemma to_real_nonneg_iff_sign_nonneg {θ : angle} : 0 ≤ θ.to_real ↔ 0 ≤ θ.sign :=
+begin
+  rcases lt_trichotomy θ.to_real 0 with (h|h|h),
+  { refine ⟨λ hn, false.elim (h.not_le hn), λ hn, _⟩,
+    rw [to_real_neg_iff_sign_neg.1 h] at hn,
+    exact false.elim (hn.not_lt dec_trivial) },
+  { simp [h, sign, ←sin_to_real] },
+  { refine ⟨λ _, _, λ _, h.le⟩,
+    rw [sign, ←sin_to_real, sign_nonneg_iff],
+    exact sin_nonneg_of_nonneg_of_le_pi h.le (to_real_le_pi θ) }
+end
+
+lemma sign_to_real {θ : angle} (h : θ ≠ π) : _root_.sign θ.to_real = θ.sign :=
+begin
+  rcases lt_trichotomy θ.to_real 0 with (ht|ht|ht),
+  { simp [ht, to_real_neg_iff_sign_neg.1 ht] },
+  { simp [sign, ht, ←sin_to_real] },
+  { rw [sign, ←sin_to_real, sign_pos ht,
+        sign_pos (sin_pos_of_pos_of_lt_pi ht
+          ((to_real_le_pi θ).lt_of_ne (to_real_eq_pi_iff.not.2 h)))] }
+end
+
+lemma coe_abs_to_real_of_sign_nonneg {θ : angle} (h : 0 ≤ θ.sign) : ↑|θ.to_real| = θ :=
+by rw [abs_eq_self.2 (to_real_nonneg_iff_sign_nonneg.2 h), coe_to_real]
+
+lemma neg_coe_abs_to_real_of_sign_nonpos {θ : angle} (h : θ.sign ≤ 0) : -↑|θ.to_real| = θ :=
+begin
+  rw sign_type.nonpos_iff at h,
+  rcases h with h|h,
+  { rw [abs_of_neg (to_real_neg_iff_sign_neg.2 h), coe_neg, neg_neg, coe_to_real] },
+  { rw sign_eq_zero_iff at h,
+    rcases h with rfl|rfl;
+      simp [abs_of_pos real.pi_pos] }
+end
+
+lemma eq_iff_sign_eq_and_abs_to_real_eq {θ ψ : angle} :
+  θ = ψ ↔ θ.sign = ψ.sign ∧ |θ.to_real| = |ψ.to_real| :=
+begin
+  refine ⟨_, λ h, _⟩, { rintro rfl, exact ⟨rfl, rfl⟩ },
+  rcases h with ⟨hs, hr⟩,
+  rw abs_eq_abs at hr,
+  rcases hr with (hr|hr),
+  { exact to_real_injective hr },
+  { by_cases h : θ = π,
+    { rw [h, to_real_pi, eq_neg_iff_eq_neg] at hr,
+      exact false.elim ((neg_pi_lt_to_real ψ).ne hr.symm) },
+    { by_cases h' : ψ = π,
+      { rw [h', to_real_pi] at hr,
+        exact false.elim ((neg_pi_lt_to_real θ).ne hr.symm) },
+      { rw [←sign_to_real h, ←sign_to_real h', hr, left.sign_neg, sign_type.neg_eq_self_iff,
+            _root_.sign_eq_zero_iff, to_real_eq_zero_iff] at hs,
+        rw [hs, to_real_zero, neg_zero, to_real_eq_zero_iff] at hr,
+        rw [hr, hs] } } }
+end
+
+lemma eq_iff_abs_to_real_eq_of_sign_eq {θ ψ : angle} (h : θ.sign = ψ.sign) :
+  θ = ψ ↔ |θ.to_real| = |ψ.to_real| :=
+by simpa [h] using @eq_iff_sign_eq_and_abs_to_real_eq θ ψ
 
 end angle
 

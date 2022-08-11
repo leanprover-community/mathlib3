b25d4df915afa4d0c3868c8c9322075f1cefe1eb
Junyan Xu <junyanxumath@gmail.com>
2022-08-11 03:11:01 -0400

    copy docstring

--- src/ring_theory/algebraic.lean
+++ src/ring_theory/algebraic.lean
@@ -237,6 +237,7 @@ def is_algebraic.alg_equiv_equiv_alg_hom
   right_inv := λ _, by {ext, refl},
   map_mul' := λ _ _, rfl }
 
+/-- Bijection between algebra equivalences and algebra homomorphisms -/
 @[reducible] noncomputable def _root_.alg_equiv_equiv_alg_hom [finite_dimensional K L] :
   (L ≃ₐ[K] L) ≃* (L →ₐ[K] L) := (algebra.is_algebraic_of_finite K L).alg_equiv_equiv_alg_hom K L
 

50a3d3f962d8f092cee06da253c85687d6f28ada
adomani <adomani@gmail.com>
2022-08-12 14:10:39 +0200

    copyright

--- src/data/finsupp/witnesses.lean
+++ src/data/finsupp/witnesses.lean
@@ -1,4 +1,10 @@
+/-
+Copyright (c) 2022 Damiano Testa. All rights reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
+Authors: Damiano Testa
+-/
 import data.finsupp.lex
+
 /-!
 #  Witnesses, linear orders and covariant classes in  in finsupps
 -/

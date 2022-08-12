3816be59b836fa416ea1a7fa78983f0eda5de88a
Moritz Doll <moritz.doll@googlemail.com>
2022-08-13 01:24:58 +0200

    fixes v2

--- src/ring_theory/polynomial/cyclotomic/eval.lean
+++ src/ring_theory/polynomial/cyclotomic/eval.lean
@@ -200,7 +200,7 @@ begin
   { refine ⟨ζ, (mem_primitive_roots hn).mpr hζ, _⟩,
     suffices : ¬ same_ray ℝ (q : ℂ) ζ,
     { convert lt_norm_sub_of_not_same_ray this;
-      simp [real.norm_of_nonneg hq.le, hζ.norm'_eq_one hn.ne'] },
+      simp [abs_of_nonneg hq.le, hζ.norm'_eq_one hn.ne'] },
     rw complex.same_ray_iff,
     push_neg,
     refine ⟨by exact_mod_cast hq.ne', hζ.ne_zero hn.ne', _⟩,

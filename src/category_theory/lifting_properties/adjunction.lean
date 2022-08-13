9bbfd526bfcd32e59a09f6ba98eb134c03fbe034
Joël Riou <joel.riou@universite-paris-saclay.fr>
2022-08-13 18:27:43 +0530

    corrected the copyright line

--- src/category_theory/lifting_properties/adjunction.lean
+++ src/category_theory/lifting_properties/adjunction.lean
@@ -1,5 +1,5 @@
 /-
-Copyright (c) 2021 Jakob Scholbach. All rights reserved.
+Copyright (c) 2022 Joël Riou. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Joël Riou
 -/

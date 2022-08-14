9920797001de46188c4bbc497a7088f478cd5b0a
ADedecker <anatolededecker@gmail.com>
2022-08-14 13:58:48 +0200

    MInimize imports

--- src/topology/algebra/module/locally_convex.lean
+++ src/topology/algebra/module/locally_convex.lean
@@ -3,7 +3,8 @@ Copyright (c) 2022 Anatole Dedecker. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Anatole Dedecker
 -/
-import analysis.convex.topology
+import analysis.convex.basic
+import topology.algebra.module.basic
 /-!
 # Locally convex topological modules
 

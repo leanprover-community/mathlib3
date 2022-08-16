e406e1bcbbd5f01576f44861ad1ee859856c5986
madvorak <dvorakmartinbridge@seznam.cz>
2022-08-16 17:40:12 +0200

    refactor(data/list/forall2): empty lines fixed

--- src/data/list/forall2.lean
+++ src/data/list/forall2.lean
@@ -6,8 +6,8 @@ Authors: Mario Carneiro, Johannes Hölzl
 import data.list.infix
 
 /-!
-
 # Double universal quantification on a list
+
 This file provides an API for `list.forall₂` (definition in `data.list.defs`).
 `forall₂ R l₁ l₂` means that `l₁` and `l₂` have the same length, and whenever `a` is the nth element
 of `l₁`, and `b` is the nth element of `l₂`, then `R a b` is satisfied.

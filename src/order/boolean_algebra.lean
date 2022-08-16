2c7bf391fd193676a99d78a84d5375fc4aed3b43
YaelDillies <yael.dillies@gmail.com>
2022-08-16 14:27:15 +0200

    move to order.heyting.basic

--- src/order/boolean_algebra.lean
+++ src/order/boolean_algebra.lean
@@ -3,7 +3,7 @@ Copyright (c) 2017 Johannes Hölzl. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Johannes Hölzl, Bryan Gin-ge Chen
 -/
-import order.heyting_algebra
+import order.heyting.basic
 
 /-!
 # (Generalized) Boolean algebras

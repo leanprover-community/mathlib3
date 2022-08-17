caebae8366d1800d0edced53b294b4ff95c72791
Stuart Presnell <stuart.presnell@gmail.com>
2022-08-17 20:22:34 +0100

    Edit lemmas to use `set.Icc` counterparts
    Just to see what the `simp_nf` linter says about this.  Presumably the eventual solution will be to delete these lemmas and directly use the `set.Icc` versions wherever these were used?

--- src/topology/unit_interval.lean
+++ src/topology/unit_interval.lean
@@ -6,6 +6,7 @@ Authors: Patrick Massot, Scott Morrison
 import topology.instances.real
 import topology.algebra.field
 import data.set.intervals.proj_Icc
+import data.set.intervals.instances
 
 /-!
 # The unit interval, as a topological space
@@ -50,24 +51,22 @@ end
 
 instance has_zero : has_zero I := ⟨⟨0, zero_mem⟩⟩
 
-@[simp, norm_cast] lemma coe_zero : ((0 : I) : ℝ) = 0 := rfl
+@[simp, norm_cast] lemma coe_zero : ((0 : I) : ℝ) = 0 := Icc.coe_zero
 
-@[simp] lemma mk_zero (h : (0 : ℝ) ∈ Icc (0 : ℝ) 1) : (⟨0, h⟩ : I) = 0 := rfl
+@[simp] lemma mk_zero (h : (0 : ℝ) ∈ Icc (0 : ℝ) 1) : (⟨0, h⟩ : I) = 0 := Icc.mk_zero h
 
-@[simp, norm_cast] lemma coe_eq_zero {x : I} : (x : ℝ) = 0 ↔ x = 0 :=
-by { symmetry, exact subtype.ext_iff }
+@[simp, norm_cast] lemma coe_eq_zero {x : I} : (x : ℝ) = 0 ↔ x = 0 := Icc.coe_eq_zero
 
 instance has_one : has_one I := ⟨⟨1, by split ; norm_num⟩⟩
 
-@[simp, norm_cast] lemma coe_one : ((1 : I) : ℝ) = 1 := rfl
+@[simp, norm_cast] lemma coe_one : ((1 : I) : ℝ) = 1 := Icc.coe_one
 
 lemma coe_ne_zero {x : I} : (x : ℝ) ≠ 0 ↔ x ≠ 0 :=
 not_iff_not.mpr coe_eq_zero
 
-@[simp] lemma mk_one (h : (1 : ℝ) ∈ Icc (0 : ℝ) 1) : (⟨1, h⟩ : I) = 1 := rfl
+@[simp] lemma mk_one (h : (1 : ℝ) ∈ Icc (0 : ℝ) 1) : (⟨1, h⟩ : I) = 1 := Icc.mk_one h
 
-@[simp, norm_cast] lemma coe_eq_one {x : I} : (x : ℝ) = 1 ↔ x = 1 :=
-by { symmetry, exact subtype.ext_iff }
+@[simp, norm_cast] lemma coe_eq_one {x : I} : (x : ℝ) = 1 ↔ x = 1 := Icc.coe_eq_one
 
 lemma coe_ne_one {x : I} : (x : ℝ) ≠ 1 ↔ x ≠ 1 :=
 not_iff_not.mpr coe_eq_one
@@ -76,7 +75,7 @@ instance : nonempty I := ⟨0⟩
 
 instance : has_mul I := ⟨λ x y, ⟨x * y, mul_mem x.2 y.2⟩⟩
 
-@[simp, norm_cast] lemma coe_mul {x y : I} : ((x * y : I) : ℝ) = x * y := rfl
+@[simp, norm_cast] lemma coe_mul {x y : I} : ((x * y : I) : ℝ) = x * y := Icc.coe_mul x y
 
 -- todo: we could set up a `linear_ordered_comm_monoid_with_zero I` instance
 

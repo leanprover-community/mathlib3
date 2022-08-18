94a70b4ceed70485e03931cebb58ae7d425eec47
Eric Rodriguez <ericrboidi@gmail.com>
2022-08-18 13:23:59 +0100

    ne_zero improvements

--- src/algebra/ne_zero.lean
+++ src/algebra/ne_zero.lean
@@ -3,7 +3,10 @@ Copyright (c) 2021 Eric Rodriguez. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Eric Rodriguez
 -/
-import algebra.algebra.basic
+
+import data.nat.cast
+import data.pnat.basic
+import algebra.group_power.ring
 
 /-!
 # `ne_zero` typeclass
@@ -30,6 +33,9 @@ lemma ne_zero_iff {R : Type*} [has_zero R] {n : R} : ne_zero n ↔ n ≠ 0 :=
 lemma not_ne_zero {R : Type*} [has_zero R] {n : R} : ¬ ne_zero n ↔ n = 0 :=
 by simp [ne_zero_iff]
 
+lemma eq_zero_or_ne_zero {α} [has_zero α] (a : α) : a = 0 ∨ ne_zero a :=
+(eq_or_ne a 0).imp_right ne_zero.mk
+
 namespace ne_zero
 
 variables {R S M F : Type*} {r : R} {x y : M} {n p : ℕ} {a : ℕ+}
@@ -37,14 +43,28 @@ variables {R S M F : Type*} {r : R} {x y : M} {n p : ℕ} {a : ℕ+}
 instance pnat : ne_zero (a : ℕ) := ⟨a.ne_zero⟩
 instance succ : ne_zero (n + 1) := ⟨n.succ_ne_zero⟩
 
+instance one (R) [mul_zero_one_class R] [nontrivial R] : ne_zero (1 : R) := ⟨one_ne_zero⟩
+
+lemma pos (r : R) [canonically_ordered_add_monoid R] [ne_zero r] : 0 < r :=
+(zero_le r).lt_of_ne $ ne_zero.out.symm
+
 lemma of_pos [preorder M] [has_zero M] (h : 0 < x) : ne_zero x := ⟨h.ne'⟩
 lemma of_gt  [canonically_ordered_add_monoid M] (h : x < y) : ne_zero y := of_pos $ pos_of_gt h
 
-instance char_zero [ne_zero n] [add_monoid_with_one M] [char_zero M] : ne_zero (n : M) :=
-⟨nat.cast_ne_zero.mpr $ ne_zero.ne n⟩
+instance of_gt' [canonically_ordered_add_monoid M] [fact (x < y)] : ne_zero y :=
+of_gt $ fact.out $ x < y
 
-@[priority 100] instance invertible [mul_zero_one_class M] [nontrivial M] [invertible x] :
-  ne_zero x := ⟨nonzero_of_invertible x⟩
+instance bit0 [canonically_ordered_add_monoid M] [ne_zero x] : ne_zero (bit0 x) :=
+of_pos $ bit0_pos $ ne_zero.pos x
+
+instance bit1 [canonically_ordered_comm_semiring M] [nontrivial M] : ne_zero (bit1 x) :=
+⟨mt (λ h, le_iff_exists_add'.2 ⟨_, h.symm⟩) canonically_ordered_comm_semiring.zero_lt_one.not_le⟩
+
+instance pow [monoid_with_zero M] [no_zero_divisors M] [ne_zero x] : ne_zero (x ^ n) :=
+⟨pow_ne_zero n out⟩
+
+instance char_zero [ne_zero n] [add_monoid_with_one M] [char_zero M] : ne_zero (n : M) :=
+⟨nat.cast_ne_zero.mpr out⟩
 
 instance coe_trans [has_zero M] [has_coe R S] [has_coe_t S M] [h : ne_zero (r : M)] :
   ne_zero ((r : S) : M) := ⟨h.out⟩
@@ -70,14 +90,7 @@ lemma nat_of_injective [non_assoc_semiring M] [non_assoc_semiring R] [h : ne_zer
   [ring_hom_class F R M] {f : F} (hf : function.injective f) : ne_zero (n : M) :=
  ⟨λ h, (ne_zero.ne' n R) $ hf $ by simpa⟩
 
-lemma pos (r : R) [canonically_ordered_add_monoid R] [ne_zero r] : 0 < r :=
-(zero_le r).lt_of_ne $ ne_zero.out.symm
-
 variables (R M)
-
-lemma of_no_zero_smul_divisors (n : ℕ) [comm_ring R] [ne_zero (n : R)] [ring M] [nontrivial M]
-  [algebra R M] [no_zero_smul_divisors R M] : ne_zero (n : M) :=
-nat_of_injective $ no_zero_smul_divisors.algebra_map_injective R M
 
 lemma of_ne_zero_coe [add_monoid_with_one R] [h : ne_zero (n : R)] : ne_zero n :=
 ⟨by {casesI h, rintro rfl, by simpa using h}⟩
@@ -86,6 +99,3 @@ lemma pos_of_ne_zero_coe [add_monoid_with_one R] [ne_zero (n : R)] : 0 < n :=
 (ne_zero.of_ne_zero_coe R).out.bot_lt
 
 end ne_zero
-
-lemma eq_zero_or_ne_zero {α} [has_zero α] (a : α) : a = 0 ∨ ne_zero a :=
-(eq_or_ne a 0).imp_right ne_zero.mk

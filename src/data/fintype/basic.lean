4da82efe3a797320a1f4ca85b10a6c1f86478208
Alexander Bentkamp <bentkamp@gmail.com>
2022-08-18 15:59:32 +0200

    move lemmas

--- src/data/fintype/basic.lean
+++ src/data/fintype/basic.lean
@@ -1291,6 +1291,37 @@ instance Prop.fintype : fintype Prop :=
 instance subtype.fintype (p : α → Prop) [decidable_pred p] [fintype α] : fintype {x // p x} :=
 fintype.subtype (univ.filter p) (by simp)
 
+lemma image_subtype_ne_univ_eq_image_erase [fintype α] [decidable_eq α] (k : β) (b : α → β) :
+  image (λ i : {a // b a ≠ k}, b ↑i) univ = (image b univ).erase k :=
+begin
+  apply subset_antisymm,
+  { rw image_subset_iff,
+    intros i _,
+    apply mem_erase_of_ne_of_mem i.2 (mem_image_of_mem _ (mem_univ _)) },
+  { intros i hi,
+    rw mem_image,
+    rcases mem_image.1 (erase_subset _ _ hi) with ⟨a, _, ha⟩,
+    subst ha,
+    exact ⟨⟨a, ne_of_mem_erase hi⟩, mem_univ _, rfl⟩ }
+end
+
+lemma image_subtype_univ_ssubset_image_univ [fintype α] [decidable_eq α] (k : β) (b : α → β)
+  (hk : k ∈ image b univ) (p : β → Prop) [decidable_pred p] (hp : ¬ p k) :
+  image (λ i : {a // p (b a)}, b ↑i) univ ⊂ image b univ :=
+begin
+  split,
+  { intros x hx,
+    rcases mem_image.1 hx with ⟨y, _, hy⟩,
+    exact hy ▸ mem_image_of_mem b (mem_univ y) },
+  { intros h,
+    rw mem_image at hk,
+    rcases hk with ⟨k', _, hk'⟩, subst hk',
+    have := h (mem_image_of_mem b (mem_univ k')),
+    rw mem_image at this,
+    rcases this with ⟨j, hj, hj'⟩,
+    exact hp (hj' ▸ j.2) }
+end
+
 @[simp] lemma set.to_finset_eq_empty_iff {s : set α} [fintype s] : s.to_finset = ∅ ↔ s = ∅ :=
 by simp only [ext_iff, set.ext_iff, set.mem_to_finset, not_mem_empty, set.mem_empty_eq]
 
--- src/linear_algebra/matrix/block.lean
+++ src/linear_algebra/matrix/block.lean
@@ -217,21 +217,6 @@ begin
   simpa only [not_not] using h,
 end
 
--- TODO: move
-lemma xxx [decidable_eq α] (k : α):
-  image (λ i : {a // b a ≠ k}, b ↑i) univ = (image b univ).erase k :=
-begin
-  apply subset_antisymm,
-  { rw image_subset_iff,
-    intros i _,
-    apply mem_erase_of_ne_of_mem i.2 (mem_image_of_mem _ (mem_univ _)) },
-  { intros i hi,
-    rw mem_image,
-    rcases mem_image.1 (erase_subset _ _ hi) with ⟨a, _, ha⟩,
-    subst ha,
-    exact ⟨⟨a, ne_of_mem_erase hi⟩, mem_univ _, rfl⟩ }
-end
-
 protected lemma block_triangular.det [decidable_eq α] [linear_order α] (hM : block_triangular M b) :
   M.det = ∏ a in univ.image b, (M.to_square_block b a).det :=
 begin
@@ -280,23 +265,6 @@ begin
 end
 
 /-! ### Invertible -/
-
-
-lemma xxxx [decidable_eq α] (k : α) (hk : k ∈ image b univ) (p : α → Prop) [decidable_pred p] (hp : ¬ p k) :
-  image (λ i : {a // p (b a)}, b ↑i) univ ⊂ image b univ :=
-begin
-  split,
-  { intros x hx,
-    rcases mem_image.1 hx with ⟨y, _, hy⟩,
-    exact hy ▸ mem_image_of_mem b (mem_univ y) },
-  { intros h,
-    rw mem_image at hk,
-    rcases hk with ⟨k', _, hk'⟩, subst hk',
-    have := h (mem_image_of_mem b (mem_univ k')),
-    rw mem_image at this,
-    rcases this with ⟨j, hj, hj'⟩,
-    exact hp (hj' ▸ j.2) }
-end
 
 lemma to_block_mul {m n k : Type*} [fintype n] (p : m → Prop) (q : k → Prop)
     (A : matrix m n R) (B : matrix n k R) :

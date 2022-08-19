6cfe30f7f321b479b735444667b292c1fa2f1098
Jujian Zhang <jujian.zhang1998@outlook.com>
2022-08-19 04:01:22 +0100

    defined some auxilary isomorphism

--- src/topology/sheaves/godement.lean
+++ src/topology/sheaves/godement.lean
@@ -23,7 +23,7 @@ variables (𝓕 : presheaf C X) (𝓖 : sheaf C X)
 def godement_presheaf : presheaf C X :=
 ∏ (λ x, skyscraper_presheaf x (𝓕.stalk x) : X → presheaf C X)
 
-@[simps] def godement_presheaf_app (U : (opens X)ᵒᵖ) :
+@[simps] def godement_presheaf_obj (U : (opens X)ᵒᵖ) :
   (godement_presheaf 𝓕).obj U ≅ ∏ (λ x, (skyscraper_presheaf x (𝓕.stalk x)).obj U) :=
 limit_obj_iso_limit_comp_evaluation _ _ ≪≫
 { hom := lim_map
@@ -56,7 +56,7 @@ pi.lift (λ x, if m : x ∈ U.unop
   else terminal.from _ ≫ eq_to_hom (skyscraper_presheaf_obj_of_not_mem _ m).symm)
 
 @[simps] def to_godement_presheaf : 𝓕 ⟶ godement_presheaf 𝓕 :=
-{ app := λ U, to_godement_presheaf_aux 𝓕 U ≫ (godement_presheaf_app 𝓕 U).inv,
+{ app := λ U, to_godement_presheaf_aux 𝓕 U ≫ (godement_presheaf_obj 𝓕 U).inv,
   naturality' :=
   begin
     intros U V inc,
@@ -97,9 +97,9 @@ limit_is_sheaf _ $ λ ⟨x⟩, (skyscraper_sheaf x _).2
 def godement_sheaf : sheaf C X :=
 ⟨godement_presheaf 𝓖.1, godement_presheaf_is_sheaf _ 𝓖.2⟩
 
-def godement_sheaf_app (U : (opens X)ᵒᵖ) :
+def godement_sheaf_obj (U : (opens X)ᵒᵖ) :
   (godement_sheaf 𝓖).1.obj U ≅ ∏ (λ x, (skyscraper_presheaf x (𝓖.presheaf.stalk x)).obj U) :=
-godement_presheaf_app 𝓖.1 U
+godement_presheaf_obj 𝓖.1 U
 
 def to_godement_sheaf : 𝓖 ⟶ godement_sheaf 𝓖 :=
 ⟨to_godement_presheaf 𝓖.1⟩
@@ -107,14 +107,107 @@ def to_godement_sheaf : 𝓖 ⟶ godement_sheaf 𝓖 :=
 variables [concrete_category.{u} C] [preserves_limits (forget C)]
 variables [Π (U : opens X), preserves_colimits_of_shape
   ((opens.grothendieck_topology X).cover U)ᵒᵖ (forget C)]
-variables [reflects_isomorphisms (forget C)]
+variables [reflects_isomorphisms (forget C)] [preserves_filtered_colimits (forget C)]
 
-instance [preserves_filtered_colimits (forget C)] : mono $ to_godement_sheaf 𝓖 :=
+def sheaf_in_Type : sheaf C X ⥤ sheaf (Type u) X :=
+{ obj := λ F, ⟨F.1 ⋙ forget C, (presheaf.is_sheaf_iff_is_sheaf_comp (forget C) F.1).mp F.2⟩,
+  map := λ F G f, Sheaf.hom.mk
+  { app := λ U, (forget C).map (f.1.app U),
+    naturality' := λ U V inc, by erw [←(forget C).map_comp, ←(forget C).map_comp, f.1.naturality] },
+  map_id' := λ F, by { ext, dsimp, rw [id_apply] },
+  map_comp' := λ F G H f g, by { ext, dsimp, rw [comp_apply] } }
+
+example : true := trivial
+
+
+def godement_sheaf_in_Type : sheaf (Type u) X := sheaf_in_Type.obj (godement_sheaf 𝓖)
+
+def godement_sheaf_in_Type_obj_aux1 (U : (opens X)ᵒᵖ) :
+  (godement_sheaf_in_Type 𝓖).1.obj U ≅
+  (forget C).obj ∏ (λ x, (skyscraper_presheaf x (𝓖.presheaf.stalk x)).obj U) :=
+{ hom := (forget C).map $ (godement_sheaf_obj 𝓖 U).hom,
+  inv := (forget C).map $ (godement_sheaf_obj 𝓖 U).inv,
+  hom_inv_id' := by { rw [←(forget C).map_comp, iso.hom_inv_id, (forget C).map_id], refl, },
+  inv_hom_id' := by { rw [←(forget C).map_comp, iso.inv_hom_id, (forget C).map_id], } }
+
+example : true := trivial
+
+def godement_sheaf_in_Type_obj_aux2 (U : (opens X)ᵒᵖ) :
+  (forget C).obj ∏ (λ x, (skyscraper_presheaf x (𝓖.presheaf.stalk x)).obj U) ≅
+  ∏ (λ x, (forget C).obj ((skyscraper_presheaf x (𝓖.presheaf.stalk x)).obj U)) :=
+{ hom := pi.lift $ λ p, (forget C).map $ pi.π _ p,
+  inv :=
+  begin
+    refine lim_map _ ≫ (preserves_limit_iso (forget C) _).inv,
+    refine { app := λ p x, x, naturality' := _ },
+    rintros ⟨p⟩ ⟨q⟩ ⟨⟨(eq1 : p = q)⟩⟩,
+    dsimp,
+    induction eq1,
+    ext1,
+    dsimp,
+    simp only [discrete.functor_map_id, types_id_apply, id_apply],
+  end,
+  hom_inv_id' :=
+  begin
+    rw [←category.assoc, limit.lift_map, iso.comp_inv_eq, category.id_comp],
+    refine limit.hom_ext _,
+    rintros ⟨p⟩,
+    rw [preserves_limits_iso_hom_π, limit.lift_π],
+    simpa only [cones.postcompose_obj_π, nat_trans.comp_app, fan.mk_π_app, forget_map_eq_coe],
+  end,
+  inv_hom_id' := _ }
+
+def godement_sheaf_in_Type_obj (U : (opens X)ᵒᵖ) :
+  (godement_sheaf_in_Type 𝓖).1.obj U ≅
+  ∏ (λ x, (forget C).obj $ (skyscraper_presheaf x (𝓖.presheaf.stalk x)).obj U) :=
+godement_sheaf_in_Type_obj_aux1 𝓖 U ≪≫
+{ hom := _,
+  inv := _,
+  hom_inv_id' := _,
+  inv_hom_id' := _ }
+
+lemma to_godement_sheaf_app_section (U : (opens X)ᵒᵖ) (s : (sheaf_in_Type.obj 𝓖).1.obj U) (x : U.unop) :
+  -- (forget C).map (godement_sheaf_obj 𝓖 U).hom ((sheaf_in_Type.map $ to_godement_sheaf 𝓖).1.app U s) =
+  -- (forget C).obj (𝓖.1.stalk ) :=
+  sorry :=
+begin
+  have := (forget C).map (godement_sheaf_obj 𝓖 U).hom,
+end
+
+
+lemma to_godement_sheaf_app_injective (U : opens X) :
+  function.injective $ (forget C).map ((to_godement_sheaf 𝓖).1.app (opposite.op U)) :=
+λ x y eq1,
+begin
+  change (sheaf_in_Type.obj 𝓖).1.obj (opposite.op U) at x,
+  change (sheaf_in_Type.obj 𝓖).1.obj (opposite.op U) at y,
+  change (sheaf_in_Type.map (to_godement_sheaf 𝓖)).1.app (opposite.op U) x =
+    (sheaf_in_Type.map (to_godement_sheaf 𝓖)).1.app (opposite.op U) y at eq1,
+  apply presheaf.section_ext,
+  /-
+  U : opens ↥X,
+  x y : (sheaf_in_Type.obj 𝓖).val.obj (opposite.op U),
+  eq1 :
+    (sheaf_in_Type.map (to_godement_sheaf 𝓖)).val.app (opposite.op U) x =
+      (sheaf_in_Type.map (to_godement_sheaf 𝓖)).val.app (opposite.op U) y,
+  p : ↥U
+  ⊢ ⇑(𝓖.presheaf.germ p) x = ⇑(𝓖.presheaf.germ p) y
+  -/
+
+  intros p,
+  have := presheaf.germ_ext 𝓖.1 U p.2;
+  -- ext,
+  sorry
+end
+
+instance : mono $ to_godement_sheaf 𝓖 :=
 begin
   rw presheaf.mono_iff_stalk_mono,
   intros x,
   change mono ((presheaf.stalk_functor C x).map (to_godement_presheaf 𝓖.1)),
-  dsimp [presheaf.stalk_functor],
+  rw concrete_category.mono_iff_injective_of_preserves_pullback,
+  exact (presheaf.app_injective_iff_stalk_functor_map_injective (to_godement_presheaf 𝓖.1)).mpr
+    (to_godement_sheaf_app_injective 𝓖) x,
 end
 
 end presheaf

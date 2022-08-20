4f4491e84f55ba13dac63705458aace393150570
Yaël Dillies <yael.dillies@gmail.com>
2022-08-20 10:42:26 +0000

    fix analysis.complex.schwarz

--- src/analysis/complex/schwarz.lean
+++ src/analysis/complex/schwarz.lean
@@ -77,8 +77,8 @@ begin
   { rw frontier_ball c hr₀.ne',
     intros z hz,
     have hz' : z ≠ c, from ne_of_mem_sphere hz hr₀.ne',
-    rw [dslope_of_ne _ hz', slope_def_module, norm_smul, norm_inv,
-      (mem_sphere_iff_norm _ _ _).1 hz, ← div_eq_inv_mul, div_le_div_right hr₀, ← dist_eq_norm],
+    rw [dslope_of_ne _ hz', slope_def_module, norm_smul, norm_inv, mem_sphere_iff_norm.1 hz,
+      ← div_eq_inv_mul, div_le_div_right hr₀, ← dist_eq_norm],
     exact le_of_lt (h_maps (mem_ball.2 (by { rw mem_sphere.1 hz, exact hr.2 }))) },
   { rw [closure_ball c hr₀.ne', mem_closed_ball],
     exact hr.1.le }

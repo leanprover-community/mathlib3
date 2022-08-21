854ec4c68f5dcd5e86546be42065cd97422ad696
Pim Otte <otte.pim@gmail.com>
2022-08-21 10:04:43 +0200

    Apply suggestions from code review
    Co-authored-by: Junyan Xu <junyanxumath@gmail.com>

--- src/data/nat/choose/multinomial.lean
+++ src/data/nat/choose/multinomial.lean
@@ -33,31 +33,27 @@ variables {a b : α}
 /-- The multinomial coefficient. Gives the number of strings consisting of symbols
 from `s`, where `c ∈ s` appears with multiplicity `f c`.
 
-Defined as `(∑ i in s, f i)! / ∏ i in s, (factorial ∘ f) i`
+Defined as `(∑ i in s, f i)! / ∏ i in s, (f i)!`
 -/
-def multinomial : ℕ := (∑ i in s, f i)! / ∏ i in s, (factorial ∘ f) i
+def multinomial : ℕ := (∑ i in s, f i)! / ∏ i in s, (f i)!
 
-lemma prod_factorial_dvd_factorial_sum : s.prod (factorial ∘ f) ∣ (s.sum f)! :=
+lemma prod_factorial_dvd_factorial_sum : (∏ i in s, (f i)!) ∣ (∑ i in s, f i)! :=
 begin
   classical,
-  induction s using finset.induction with α' s' has ih,
+  induction s using finset.induction with a' s' has ih,
   { simp only [finset.sum_empty, finset.prod_empty, factorial], },
   { simp only [finset.prod_insert has, finset.sum_insert has],
-    refine dvd_trans (mul_dvd_mul_left ((f α')!) ih) _,
+    refine dvd_trans (mul_dvd_mul_left ((f a')!) ih) _,
     apply nat.factorial_mul_factorial_dvd_factorial_add, },
 end
 
-lemma prod_factorial_pos : 0 < s.prod (factorial ∘ f) :=
-begin
-  apply finset.prod_pos,
-  intros i hi,
-  exact factorial_pos (f i),
-end
+lemma prod_factorial_pos : 0 < ∏ i in s, (f i)! :=
+finset.prod_pos (λ i _, factorial_pos (f i))
 
-lemma multinomial_pos : 0 < multinomial s f := nat.div_pos (le_of_dvd (factorial_pos _)
-  (prod_factorial_dvd_factorial_sum s f)) (prod_factorial_pos s f)
+lemma multinomial_pos : 0 < multinomial s f := nat.div_pos
+  (le_of_dvd (factorial_pos _) (prod_factorial_dvd_factorial_sum s f)) (prod_factorial_pos s f)
 
-lemma multinomial_spec : s.prod (factorial ∘ f) * multinomial s f = (s.sum f)! :=
+lemma multinomial_spec : (∏ i in s, (f i)!) * multinomial s f = (∑ i in s, f i)! :=
 nat.mul_div_cancel' (prod_factorial_dvd_factorial_sum s f)
 
 @[simp] lemma multinomial_nil : multinomial ∅ f = 1 := rfl
@@ -65,7 +61,7 @@ nat.mul_div_cancel' (prod_factorial_dvd_factorial_sum s f)
 @[simp] lemma multinomial_singleton : multinomial {a} f = 1 :=
 by simp [multinomial, nat.div_self (factorial_pos (f a))]
 
-@[simp] lemma multinomial_one [decidable_eq α] (h : a ∉ s) (h₁ : f a = 1):
+@[simp] lemma multinomial_insert_one [decidable_eq α] (h : a ∉ s) (h₁ : f a = 1) :
   multinomial (insert a s) f = (s.sum f).succ * multinomial s f :=
 begin
   simp only [multinomial, one_mul, factorial],
@@ -74,14 +70,14 @@ begin
   rw nat.mul_div_assoc _ (prod_factorial_dvd_factorial_sum _ _),
 end
 
-lemma multinomial_add_n [decidable_eq α] (h : a ∉ s) {n : ℕ} (h₁ : f a = n) :
-  multinomial (insert a s) f = (n + s.sum f).choose n * multinomial s f :=
+lemma multinomial_insert [decidable_eq α] (h : a ∉ s) {n : ℕ} :
+  multinomial (insert a s) f = (f a + s.sum f).choose (f a) * multinomial s f :=
 begin
   rw choose_eq_factorial_div_factorial (le.intro rfl),
   simp only [multinomial, nat.add_sub_cancel_left, finset.sum_insert h, finset.prod_insert h,
     h₁, function.comp_app],
-  rw [div_mul_div_comm (nat.factorial_mul_factorial_dvd_factorial_add n (s.sum f))
-    (prod_factorial_dvd_factorial_sum _ _), mul_comm n! (s.sum f)!, mul_assoc,
+  rw [div_mul_div_comm (n.factorial_mul_factorial_dvd_factorial_add (s.sum f))
+    prod_factorial_dvd_factorial_sum _ _, mul_comm n! (s.sum f)!, mul_assoc,
     mul_comm _ (s.sum f)!, nat.mul_div_mul _ _ (factorial_pos _)],
 end
 
@@ -110,11 +106,12 @@ lemma binomial_succ_succ [decidable_eq α] (h : a ≠ b) :
 begin
   simp only [binomial_eq_choose, function.update_apply, function.update_noteq,
     succ_add, add_succ, choose_succ_succ, h, ne.def, not_false_iff, function.update_same],
-  rw if_neg (ne_comm.mp h),
+  rw if_neg h.symm,
   ring,
 end
 
-lemma succ_mul_binomial [decidable_eq α] (h : a ≠ b) : (f a + f b).succ * multinomial {a, b} f =
+lemma succ_mul_binomial [decidable_eq α] (h : a ≠ b) :
+  (f a + f b).succ * multinomial {a, b} f =
   (f a).succ * multinomial {a, b} (function.update f a (f a).succ) :=
 begin
   rw [binomial_eq_choose _ h, binomial_eq_choose _ h, mul_comm (f a).succ,

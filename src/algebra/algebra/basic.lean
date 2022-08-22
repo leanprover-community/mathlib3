6a5a2b134ccf036bb08984225757666380ac2938
Yury G. Kudryashov <urkud@urkud.name>
2022-08-22 08:40:46 +0000

    feat(data/rat/cast): drop an unneeded typeclass assumption (#15972)
    * drop `[char_zero _]` assumption in `map_rat_cast`;
    * golf `ring_hom.eq_rat_cast`, generalize to `ring_hom_class`.

--- src/algebra/algebra/basic.lean
+++ src/algebra/algebra/basic.lean
@@ -788,7 +788,7 @@ end alg_hom
 
 @[simp] lemma rat.smul_one_eq_coe {A : Type*} [division_ring A] [algebra ℚ A] (m : ℚ) :
   @@has_smul.smul algebra.to_has_smul m (1 : A) = ↑m :=
-by rw [algebra.smul_def, mul_one, ring_hom.eq_rat_cast]
+by rw [algebra.smul_def, mul_one, eq_rat_cast]
 
 set_option old_structure_cmd true
 /-- An equivalence of algebras is an equivalence of rings commuting with the actions of scalars. -/

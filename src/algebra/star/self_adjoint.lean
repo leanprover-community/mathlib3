0b97e43c2f42c2c22f74a79953c5f9245379f935
Jireh Loreaux <loreaujy@gmail.com>
2022-08-22 05:00:12 -0500

    make them simp lemmas

--- src/algebra/star/self_adjoint.lean
+++ src/algebra/star/self_adjoint.lean
@@ -58,9 +58,11 @@ lemma star_eq [has_star R] {x : R} (hx : is_self_adjoint x) : star x = x := hx
 
 lemma _root_.is_self_adjoint_iff [has_star R] {x : R} : is_self_adjoint x ↔ star x = x := iff.rfl
 
+@[simp]
 lemma star_mul_self [semigroup R] [star_semigroup R] (x : R) : is_self_adjoint (star x * x) :=
 by simp only [is_self_adjoint, star_mul, star_star]
 
+@[simp]
 lemma mul_star_self [semigroup R] [star_semigroup R] (x : R) : is_self_adjoint (x * star x) :=
 by simpa only [star_star] using star_mul_self (star x)
 

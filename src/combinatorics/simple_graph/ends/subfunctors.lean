41d3e176c426a17534a8d83685337557a13a0dbf
Rémi Bottinelli <remi.bottinelli@bluewin.ch>
2022-08-23 18:48:53 +0200

    subfunctor.ext by Adam Topaz

--- src/combinatorics/simple_graph/ends/subfunctors.lean
+++ src/combinatorics/simple_graph/ends/subfunctors.lean
@@ -17,18 +17,15 @@ def subfunctor {C : Type u} [category C] (F : C ⥤ Type v)
   map_id' := by {intro, ext, simp only [map_id, set.maps_to.coe_restrict_apply, types_id_apply], },
   map_comp' := by {intros, ext, simp only [map_comp, set.maps_to.coe_restrict_apply, types_comp_apply], },}
 
-lemma subfunctor.ext {C : Type u} [category C] (F : C ⥤ Type v)
+-- Thanks Andrew Yang and _Adam Topaz_ for this snippet!
+def subfunctor.ext {C : Type u} [category C] (F : C ⥤ Type v)
   (obj₁ : ∀ c, set $ F.obj c)
   (map₁ : ∀ (c d : C) (m : c ⟶ d), set.maps_to (F.map m) (obj₁ c) (obj₁ d))
   (obj₂ : ∀ c, set $ F.obj c)
   (map₂ : ∀ (c d : C) (m : c ⟶ d), set.maps_to (F.map m) (obj₂ c) (obj₂ d)) :
-  (∀ c, obj₁ c = obj₂ c) → F.subfunctor obj₁ map₁ = F.subfunctor obj₂ map₂ :=
-begin
-  rintro objeq,
-  have : obj₁ = obj₂ := funext objeq,
-  subst this,
-end
-
+  (∀ c, obj₁ c = obj₂ c) → (F.subfunctor obj₁ map₁ ≅ F.subfunctor obj₂ map₂) := λ objeq,
+nat_iso.of_components
+(λ X, equiv.to_iso $ equiv.subtype_equiv_prop $ objeq _ ) (by tidy)
 
 
 end category_theory.functor

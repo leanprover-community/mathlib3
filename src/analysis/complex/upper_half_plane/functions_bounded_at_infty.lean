035fb7a8f03c0aa2a04e9ed0b7b26710d66c6fca
CBirkbeck <c.birkbeck@ucl.ac.uk>
2022-08-24 09:47:21 +0100

    rev fixes/name changes

--- src/analysis/complex/upper_half_plane/functions_bounded_at_infty.lean
+++ src/analysis/complex/upper_half_plane/functions_bounded_at_infty.lean
@@ -11,7 +11,7 @@ import order.filter.zero_and_bounded_at_filter
 /-!
 # Bounded at infinity
 
-For complex valued functions on the upper half plane, this file defines the filter `at_I_infty`
+For complex valued functions on the upper half plane, this file defines the filter `at_im_infty`
 required for defining when functions are bounded at infinity and zero at infinity.
 Both of which are relevant for defining modular forms.
 
@@ -26,17 +26,16 @@ noncomputable theory
 namespace upper_half_plane
 
 /--Filter for approaching `i∞`-/
-def at_I_infty := filter.at_top.comap upper_half_plane.im
+def at_im_infty := filter.at_top.comap upper_half_plane.im
 
-lemma at_I_infty_basis : (at_I_infty).has_basis (λ _, true) (λ (i : ℝ), im ⁻¹' set.Ici i) :=
+lemma at_im_infty_basis : (at_im_infty).has_basis (λ _, true) (λ (i : ℝ), im ⁻¹' set.Ici i) :=
 filter.has_basis.comap upper_half_plane.im filter.at_top_basis
 
-lemma at_I_infty_mem (S : set ℍ) : S ∈ at_I_infty ↔ (∃ A : ℝ, ∀ z : ℍ, A ≤ im z → z ∈ S) :=
+lemma at_im_infty_mem (S : set ℍ) : S ∈ at_im_infty ↔ (∃ A : ℝ, ∀ z : ℍ, A ≤ im z → z ∈ S) :=
 begin
-  simp only [at_I_infty, filter.mem_comap', filter.mem_at_top_sets, ge_iff_le, set.mem_set_of_eq,
+  simp only [at_im_infty, filter.mem_comap', filter.mem_at_top_sets, ge_iff_le, set.mem_set_of_eq,
     upper_half_plane.coe_im],
-  split,
-  { intro h, cases h with a h, exact ⟨a, (λ z hz, h (im z) hz rfl)⟩ },
+   refine ⟨λ ⟨a, h⟩, ⟨a, (λ z hz, h (im z) hz rfl)⟩, _⟩,
   { rintro ⟨A, h⟩,
     refine ⟨A, λ b hb x hx, h x _⟩,
     rwa hx, }
@@ -45,33 +44,30 @@ end
 /--A function ` f : ℍ → ℂ` is bounded at infinity if there exist real numbers `M, A` such that
 for all `z ∈ ℍ` with `im z ≥ A` we have `abs(f (z)) ≤ M`,
  i.e. the function is bounded as you approach `i∞`. -/
-def is_bound_at_infty (f : ℍ → ℂ) : Prop := asymptotics.is_O at_I_infty f (1 : ℍ → ℂ)
+def is_bounded_at_im_infty (f : ℍ → ℂ) : Prop := asymptotics.is_O at_im_infty f (1 : ℍ → ℂ)
 
 /--A function ` f : ℍ → ℂ` is zero at infinity if for any `ε > 0` there exist a real
 number `A` such that for all `z ∈ ℍ` with `im z ≥ A` we have `abs(f (z)) ≤ ε`,
  i.e. the function tends to zero as you approach `i∞`. -/
-def is_zero_at_infty (f : ℍ → ℂ) : Prop := filter.tendsto f at_I_infty (𝓝 0)
+def is_zero_at_im_infty (f : ℍ → ℂ) : Prop := filter.tendsto f at_im_infty (𝓝 0)
 
-lemma zero_form_is_bounded_at_infty : is_bound_at_infty 0 :=
+lemma zero_form_is_bounded_at_im_infty : is_bounded_at_im_infty 0 :=
 zero_is_bounded_at_filter _
 
 /--Module of functions that are zero at infinity.-/
-def zero_at_infty_submodule : submodule ℂ (ℍ → ℂ) := zero_at_filter_submodule at_I_infty
-
-/--Module of functions that are bounded at infinity.-/
-def bounded_at_infty_submodule : submodule ℂ (ℍ → ℂ) := bounded_filter_submodule at_I_infty
+def zero_at_im_infty_submodule : submodule ℂ (ℍ → ℂ) := zero_at_filter_submodule at_im_infty
 
 /--Subalgebra of functions that are bounded at infinity.-/
-def bounded_at_infty_subalgebra : subalgebra ℂ (ℍ → ℂ) := bounded_filter_subalgebra at_I_infty
+def bounded_at_im_infty_subalgebra : subalgebra ℂ (ℍ → ℂ) := bounded_filter_subalgebra at_im_infty
 
-lemma prod_of_bound_is_bound {f g : ℍ → ℂ} (hf : is_bound_at_infty f) (hg : is_bound_at_infty g) :
-  is_bound_at_infty (f * g) :=
+lemma prod_of_bounded_is_bounded {f g : ℍ → ℂ} (hf : is_bounded_at_im_infty f) (hg : is_bounded_at_im_infty g) :
+  is_bounded_at_im_infty (f * g) :=
 by simpa only [pi.one_apply, mul_one, norm_eq_abs, complex.abs_mul] using hf.mul hg
 
-@[simp] lemma bound_mem (f : ℍ → ℂ) :
-  is_bound_at_infty f ↔ ∃ (M A : ℝ), ∀ z : ℍ, A ≤ im z → abs (f z) ≤ M :=
+@[simp] lemma bounded_mem (f : ℍ → ℂ) :
+  is_bounded_at_im_infty f ↔ ∃ (M A : ℝ), ∀ z : ℍ, A ≤ im z → abs (f z) ≤ M :=
 begin
-  simp [is_bound_at_infty, asymptotics.is_O_iff, filter.eventually, at_I_infty_mem],
+  simp [is_bounded_at_im_infty, asymptotics.is_O_iff, filter.eventually, at_im_infty_mem],
 end
 
 end upper_half_plane
--- src/order/filter/zero_and_bounded_at_filter.lean
+++ src/order/filter/zero_and_bounded_at_filter.lean
@@ -21,17 +21,19 @@ that are `bounded_at_filter`.
 
 namespace filter
 
+variables {α β : Type*}
+
 open_locale topological_space
 
 /--A function `f : α → β` is `zero_at_filter` if in the limit it is zero.-/
-def zero_at_filter {α : Type*} {β : Type*} [has_zero β] [topological_space β] (l : filter α)
+def zero_at_filter [has_zero β] [topological_space β] (l : filter α)
 (f : α → β) : Prop := filter.tendsto f l (𝓝 0)
 
-lemma zero_is_zero_at_filter {α : Type*} {β : Type*} [has_zero β] [topological_space β]
+lemma zero_is_zero_at_filter [has_zero β] [topological_space β]
 (l : filter α) : zero_at_filter l (0 : α → β) := tendsto_const_nhds
 
 /--The submodule of funtions that are `zero_at_filter`.-/
-def zero_at_filter_submodule {α : Type*} {β : Type*} [topological_space β] [semiring β]
+def zero_at_filter_submodule [topological_space β] [semiring β]
 [has_continuous_add β] [has_continuous_mul β] (l : filter α) : submodule β (α → β) :=
 { carrier := zero_at_filter l,
   zero_mem' := zero_is_zero_at_filter l,
@@ -39,28 +41,26 @@ def zero_at_filter_submodule {α : Type*} {β : Type*} [topological_space β] [s
   smul_mem' := by { intros c f hf, simpa using hf.const_mul c }, }
 
 /--The additive submonoid of funtions that are `zero_at_filter`.-/
-def zero_at_filter_add_submonoid {α : Type*} {β : Type*} [topological_space β]
+def zero_at_filter_add_submonoid [topological_space β]
 [add_zero_class β] [has_continuous_add β] (l : filter α) : add_submonoid (α → β) :=
 { carrier := zero_at_filter l,
   add_mem' := by { intros a b ha hb, simpa using ha.add hb },
   zero_mem' := zero_is_zero_at_filter l, }
 
 /--A function `f: α → β` is `bounded_at_filter` if `f =O[l] 1`. -/
-def bounded_at_filter {α : Type*} {β : Type*} [has_norm β] [has_one (α → β)] (l : filter α)
+def bounded_at_filter [has_norm β] [has_one (α → β)] (l : filter α)
 (f : α → β) : Prop := asymptotics.is_O l f (1 : α → β)
 
-lemma zero_at_filter_is_bounded_at_filter {α : Type*} {β : Type*} [normed_field β]
+lemma zero_at_filter_is_bounded_at_filter [normed_field β]
 (l : filter α) (f : α → β) (hf : zero_at_filter l f) : bounded_at_filter l f :=
-begin
-  apply asymptotics.is_O_of_div_tendsto_nhds, { simp, }, { convert hf, ext1, simp, },
-end
+asymptotics.is_O_of_div_tendsto_nhds (by simp) _ (by { convert hf, ext1, simp, })
 
-lemma zero_is_bounded_at_filter {α : Type*} {β : Type*} [normed_field β] (l : filter α) :
+lemma zero_is_bounded_at_filter [normed_field β] (l : filter α) :
   bounded_at_filter l (0 : α → β) :=
 (zero_at_filter_is_bounded_at_filter l _) (zero_is_zero_at_filter l)
 
 /--The submodule of funtions that are `bounded_at_filter`.-/
-def bounded_filter_submodule {α : Type*} {β : Type*} [normed_field β] (l : filter α) :
+def bounded_filter_submodule [normed_field β] (l : filter α) :
   submodule β (α → β) :=
 { carrier := bounded_at_filter l,
   zero_mem' := zero_is_bounded_at_filter l,
@@ -68,7 +68,7 @@ def bounded_filter_submodule {α : Type*} {β : Type*} [normed_field β] (l : fi
   smul_mem' := by { intros c f hf, simpa using hf.const_mul_left c }, }
 
 /--The subalgebra of funtions that are `bounded_at_filter`.-/
-def bounded_filter_subalgebra {α : Type*} {β : Type*} [normed_field β] (l : filter α) :
+def bounded_filter_subalgebra [normed_field β] (l : filter α) :
   subalgebra β (α → β) :=
 begin
   refine submodule.to_subalgebra (bounded_filter_submodule l) _ (λ f g hf hg, _),

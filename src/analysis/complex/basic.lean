fff98cd5b0b1488105a5a9fde7d0fdbe339e2315
Xavier-François Roblot <46200072+xroblot@users.noreply.github.com>
2022-08-24 10:15:07 +0200

    Move lemma

--- src/analysis/complex/basic.lean
+++ src/analysis/complex/basic.lean
@@ -236,6 +236,17 @@ instance : has_continuous_star ℂ := ⟨conj_lie.continuous⟩
 
 @[continuity] lemma continuous_conj : continuous (conj : ℂ → ℂ) := continuous_star
 
+/-- The only continuous ring homomorphisms from `ℂ` to `ℂ` are the identity and the complex
+conjugation. -/
+lemma ring_hom_eq_id_or_conj_of_continuous (f : ℂ →+* ℂ) (hf : continuous f) :
+  f = ring_hom.id ℂ ∨ f = conj :=
+begin
+  refine (real_alg_hom_eq_id_or_conj $ alg_hom.mk' f $ λ x z, congr_fun _ x).imp (λ h, _) (λ h, _),
+  { refine rat.dense_embedding_coe_real.dense.equalizer (by continuity) (by continuity) _,
+    ext1, simp only [real_smul, function.comp_app, map_rat_cast, of_real_rat_cast, map_mul], },
+  all_goals { convert congr_arg alg_hom.to_ring_hom h, ext1, refl, },
+end
+
 /-- Continuous linear equiv version of the conj function, from `ℂ` to `ℂ`. -/
 def conj_cle : ℂ ≃L[ℝ] ℂ := conj_lie
 
@@ -257,17 +268,6 @@ lemma isometry_of_real : isometry (coe : ℝ → ℂ) := of_real_li.isometry
 
 /-- Continuous linear map version of the canonical embedding of `ℝ` in `ℂ`. -/
 def of_real_clm : ℝ →L[ℝ] ℂ := of_real_li.to_continuous_linear_map
-
-/-- The only continuous ring homomorphisms from `ℂ` to `ℂ` are the identity and the complex
-conjugation. -/
-lemma ring_hom_eq_id_or_conj_of_continuous (f : ℂ →+* ℂ) (hf : continuous f) :
-  f = ring_hom.id ℂ ∨ f = conj :=
-begin
-  refine (real_alg_hom_eq_id_or_conj $ alg_hom.mk' f $ λ x z, congr_fun _ x).imp (λ h, _) (λ h, _),
-  { refine rat.dense_embedding_coe_real.dense.equalizer (by continuity) (by continuity) _,
-    ext1, simp only [real_smul, function.comp_app, map_rat_cast, of_real_rat_cast, map_mul], },
-  all_goals { convert congr_arg alg_hom.to_ring_hom h, ext1, refl, },
-end
 
 @[simp] lemma of_real_clm_coe : (of_real_clm : ℝ →ₗ[ℝ] ℂ) = of_real_am.to_linear_map := rfl
 

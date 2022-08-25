boboquack/mathlib

feat(category_theory/preadditive): projective iff variants of Yoneda preserve epimorphisms

src/category_theory/preadditive/projective.lean
@@ -6,6 +6,8 @@ Authors: Markus Himmel, Scott Morrison
 import algebra.homology.exact
 import category_theory.types
 import category_theory.limits.shapes.biproducts
+import category_theory.preadditive.yoneda
+import algebra.category.Module.epi_mono
 
 /-!
 # Projective objects and categories with enough projectives
@@ -133,6 +135,33 @@ lemma projective_iff_preserves_epimorphisms_coyoneda_obj (P : C) :
   by exactI ⟨factor_thru g f, factor_thru_comp _ _⟩⟩,
  λ h, ⟨λ E X f e he, by exactI (epi_iff_surjective _).1
   (infer_instance : epi ((coyoneda.obj (op P)).map e)) f⟩⟩
+
+section preadditive
+variables [preadditive C]
+
+lemma projective_iff_preserves_epimorphisms_preadditive_coyoneda_obj (P : C) :
+  projective P ↔ (preadditive_coyoneda.obj (op P)).preserves_epimorphisms :=
+begin
+  rw projective_iff_preserves_epimorphisms_coyoneda_obj,
+  refine ⟨λ (h : (preadditive_coyoneda.obj (op P) ⋙ (forget _)).preserves_epimorphisms), _, _⟩,
+  { exactI functor.preserves_epimorphisms_of_preserves_of_reflects (preadditive_coyoneda.obj (op P))
+      (forget _) },
+  { introI,
+    exact (infer_instance : (preadditive_coyoneda.obj (op P) ⋙ forget _).preserves_epimorphisms) }
+end
+
+lemma projective_iff_preserves_epimorphisms_preadditive_coyoneda_obj' (P : C) :
+  projective P ↔ (preadditive_coyoneda_obj (op P)).preserves_epimorphisms :=
+begin
+  rw projective_iff_preserves_epimorphisms_coyoneda_obj,
+  refine ⟨λ (h : (preadditive_coyoneda_obj (op P) ⋙ (forget _)).preserves_epimorphisms), _, _⟩,
+  { exactI functor.preserves_epimorphisms_of_preseeves_of_reflects (preadditive_coyoneda_obj (op P))
+      (forget _) },
+  { introI,
+    exact (infer_instance : (preadditive_coyoneda_obj (op P) ⋙ forget _).preserves_epimorphisms) }
+end
+
+end preadditive
 
 section enough_projectives
 variables [enough_projectives C]

leanprover-community/mathlib3

src/category_theory/preadditive/projective.lean
@@ -6,6 +6,8 @@ Authors: Markus Himmel, Scott Morrison
 import algebra.homology.exact
 import category_theory.types
 import category_theory.limits.shapes.biproducts
+import category_theory.preadditive.yoneda
+import algebra.category.Module.epi_mono
 
 /-!
 # Projective objects and categories with enough projectives
@@ -133,6 +135,33 @@ lemma projective_iff_preserves_epimorphisms_coyoneda_obj (P : C) :
   by exactI ⟨factor_thru g f, factor_thru_comp _ _⟩⟩,
  λ h, ⟨λ E X f e he, by exactI (epi_iff_surjective _).1
   (infer_instance : epi ((coyoneda.obj (op P)).map e)) f⟩⟩
+
+section preadditive
+variables [preadditive C]
+
+lemma projective_iff_preserves_epimorphisms_preadditive_coyoneda_obj (P : C) :
+  projective P ↔ (preadditive_coyoneda.obj (op P)).preserves_epimorphisms :=
+begin
+  rw projective_iff_preserves_epimorphisms_coyoneda_obj,
+  refine ⟨λ (h : (preadditive_coyoneda.obj (op P) ⋙ (forget _)).preserves_epimorphisms), _, _⟩,
+  { exactI functor.preserves_epimorphisms_of_preserves_of_reflects (preadditive_coyoneda.obj (op P))
+      (forget _) },
+  { introI,
+    exact (infer_instance : (preadditive_coyoneda.obj (op P) ⋙ forget _).preserves_epimorphisms) }
+end
+
+lemma projective_iff_preserves_epimorphisms_preadditive_coyoneda_obj' (P : C) :
+  projective P ↔ (preadditive_coyoneda_obj (op P)).preserves_epimorphisms :=
+begin
+  rw projective_iff_preserves_epimorphisms_coyoneda_obj,
+  refine ⟨λ (h : (preadditive_coyoneda_obj (op P) ⋙ (forget _)).preserves_epimorphisms), _, _⟩,
+  { exactI functor.preserves_epimorphisms_of_preserves_of_reflects (preadditive_coyoneda_obj (op P))
+      (forget _) },
+  { introI,
+    exact (infer_instance : (preadditive_coyoneda_obj (op P) ⋙ forget _).preserves_epimorphisms) }
+end
+
+end preadditive
 
 section enough_projectives
 variables [enough_projectives C]
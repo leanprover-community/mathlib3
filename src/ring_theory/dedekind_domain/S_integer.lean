556de2e96c72dfa48473766c402066fa9a0198a5
David Kurniadi Angdinata <dka31@cantab.ac.uk>
2022-08-26 23:50:42 +0100

    Show that the ring of S-integers is an intersection of valuation subrings

--- src/ring_theory/dedekind_domain/S_integer.lean
+++ src/ring_theory/dedekind_domain/S_integer.lean
@@ -21,12 +21,12 @@ TODO:
 
 ## Main definitions
 
- * `is_dedekind_domain.S_integer`: the ring of `S`-integers.
- * `is_dedekind_domain.S_unit`: the group of `S`-units.
+ * `set.integer`: the ring of `S`-integers.
+ * `set.unit`: the group of `S`-units.
 
 ## Main statements
 
- * `is_dedekind_domain.units_S_integer_equiv_S_unit`: units of `S`-integers are `S`-units.
+ * `set.units_integer_equiv_unit`: units of `S`-integers are `S`-units.
 
 ## References
 
@@ -39,19 +39,23 @@ TODO:
 S-integer, S-unit
 -/
 
+open is_dedekind_domain
+
 open_locale non_zero_divisors
 
-namespace is_dedekind_domain
+namespace set
 
 noncomputable theory
 
 universes u v
 
-variables {R : Type u} [comm_ring R] [is_domain R] [is_dedekind_domain R] (K : Type v) [field K]
-  [algebra R K] [is_fraction_ring R K] (S : set $ height_one_spectrum R)
+variables {R : Type u} [comm_ring R] [is_domain R] [is_dedekind_domain R]
+  (S : set $ height_one_spectrum R) (K : Type v) [field K] [algebra R K] [is_fraction_ring R K]
+
+/-! ## `S`-integers -/
 
 /-- The subring of `S`-integers of `K`. -/
-@[simps] def S_integer : subring K :=
+@[simps] def integer : subring K :=
 { carrier   := {x : K | ∀ v ∉ S, (v : height_one_spectrum R).valuation x ≤ 1},
   mul_mem'  := λ _ _ hx hy v hv, by simp only [map_mul, mul_le_one₀ (hx v hv) (hy v hv)],
   one_mem'  := λ _ _, by simp only [map_one, le_refl],
@@ -59,15 +63,12 @@ variables {R : Type u} [comm_ring R] [is_domain R] [is_dedekind_domain R] (K : T
   zero_mem' := λ _ _, by simp only [map_zero, zero_le_one],
   neg_mem'  := λ _ hx v hv, by simp only [valuation.map_neg, hx v hv] }
 
-/-- The subgroup of `S`-units of `Kˣ`. -/
-@[simps] def S_unit : subgroup Kˣ :=
-{ carrier  := {x : Kˣ | ∀ v ∉ S, (v : height_one_spectrum R).valuation (x : K) = 1},
-  mul_mem' := λ _ _ hx hy v hv, by rw [units.coe_mul, map_mul, hx v hv, hy v hv, one_mul],
-  one_mem' := λ _ _, map_one _,
-  inv_mem' := λ _ hx v hv, by rw [valuation.map_units_inv, hx v hv, inv_one] }
+lemma integer_eq :
+  S.integer K = ⨅ v ∉ S, (v : height_one_spectrum R).valuation.valuation_subring.to_subring :=
+subring.ext $ λ _, by simpa only [subring.mem_infi]
 
 /-- The subring of `S`-integers of `K` is an algebra over `R`. -/
-instance : algebra R (S_integer K S) :=
+instance : algebra R (S.integer K) :=
 { smul      := λ x y, ⟨algebra_map R K x, λ v _, v.valuation_le_one x⟩ * y,
   to_fun    := λ x, ⟨algebra_map R K x, λ v _, v.valuation_le_one x⟩,
   map_one'  := by simpa only [map_one],
@@ -77,8 +78,17 @@ instance : algebra R (S_integer K S) :=
   commutes' := λ _, mul_comm _,
   smul_def' := λ _ _, rfl }
 
+/-! ## `S`-units -/
+
+/-- The subgroup of `S`-units of `Kˣ`. -/
+@[simps] def unit : subgroup Kˣ :=
+{ carrier  := {x : Kˣ | ∀ v ∉ S, (v : height_one_spectrum R).valuation (x : K) = 1},
+  mul_mem' := λ _ _ hx hy v hv, by rw [units.coe_mul, map_mul, hx v hv, hy v hv, one_mul],
+  one_mem' := λ _ _, map_one _,
+  inv_mem' := λ _ hx v hv, by rw [map_units_inv, hx v hv, inv_one] }
+
 /-- The group of units of the ring of `S`-integers is the group of `S`-units. -/
-@[simps] def units_S_integer_equiv_S_unit : (S_integer K S)ˣ ≃* S_unit K S :=
+@[simps] def units_integer_equiv_unit : (S.integer K)ˣ ≃* S.unit K :=
 { to_fun    := λ x, ⟨units.mk0 x $ λ hx, x.ne_zero ((subring.coe_eq_zero_iff _).mp hx),
   λ v hv, eq_one_of_mul_eq_one_left (x.val.property v hv) (x.inv.property v hv) $
     by { rw [← map_mul, ← v.valuation.map_one], congr' 1, exact subtype.mk_eq_mk.mp x.val_inv }⟩,
@@ -90,4 +100,4 @@ instance : algebra R (S_integer K S) :=
   right_inv := λ _, by { ext, refl },
   map_mul'  := λ _ _, by { ext, refl } }
 
-end is_dedekind_domain
+end set

9f5a1d09d7898eff3ab0987cfe2be6d06fe7bc4d
Apurva Nakade <apurvnakade@gmail.com>
2022-08-27 15:46:08 +0530

    Fix merge errors

--- src/analysis/convex/cone/proper.lean
+++ src/analysis/convex/cone/proper.lean
@@ -15,39 +15,32 @@ import analysis.inner_product_space.adjoint
 open continuous_linear_map filter
 
 namespace convex_cone
-section ordered_semiring
-section sequential_space
 
-variables {𝕜 : Type*} [topological_space 𝕜] [ordered_semiring 𝕜]
-variables {E : Type*} [add_comm_monoid E] [topological_space E] [has_continuous_add E]
-  [sequential_space E]
-variables [has_smul 𝕜 E] [has_continuous_const_smul 𝕜 E]
+variables {E : Type*} [inner_product_space ℝ E]
 
 /-- The closure of a convex cone inside a sequential space is a convex cone. This construction is
 mainly used for defining maps between proper cones. -/
-def closure (K : convex_cone 𝕜 E) : convex_cone 𝕜 E :=
+def closure (K : convex_cone ℝ E) : convex_cone ℝ E :=
 { carrier := closure ↑K,
   smul_mem' :=
   begin
-    rw ← sequential_space.seq_closure_eq_closure,
+    rw ← seq_closure_eq_closure,
     rintros c hc x ⟨seq, mem, tends⟩,
     exact ⟨λ n, c • seq n, ⟨λ n, K.smul_mem hc (mem n), tendsto.const_smul tends c⟩⟩,
   end,
   add_mem' :=
   begin
-    rw ← sequential_space.seq_closure_eq_closure,
+    rw ← seq_closure_eq_closure,
     rintros x ⟨xseq, xmem, xtends⟩ y ⟨yseq, ymem, ytends⟩,
     exact ⟨λ n, xseq n + yseq n, ⟨λ n, K.add_mem (xmem n) (ymem n), tendsto.add xtends ytends⟩⟩,
   end }
 
-@[simp] lemma coe_closure {K : convex_cone 𝕜 E} : (K.closure : set E) = _root_.closure ↑K := rfl
+@[simp] lemma coe_closure {K : convex_cone ℝ E} : (K.closure : set E) = _root_.closure ↑K := rfl
 
-lemma mem_closure_iff_seq_limit {K : convex_cone 𝕜 E} {a : E} :
+lemma mem_closure_iff_seq_limit {K : convex_cone ℝ E} {a : E} :
   a ∈ K.closure ↔ ∃ x : ℕ → E, (∀ n : ℕ, x n ∈ K) ∧ tendsto x at_top (nhds a) :=
 by simp_rw [← set_like.mem_coe, coe_closure, mem_closure_iff_seq_limit]
 
-end sequential_space
-end ordered_semiring
 end convex_cone
 
 section definitions
@@ -80,7 +73,7 @@ lemma nonempty (K : proper_cone E) : (K : set E).nonempty := K.nonempty'
 lemma is_closed (K : proper_cone E) : is_closed (K : set E) := K.is_closed'
 
 lemma pointed (K : proper_cone E) : (K : convex_cone ℝ E).pointed :=
-K.pointed_of_nonempty_of_is_closed K.nonempty K.is_closed
+(K : convex_cone ℝ E).pointed_of_nonempty_of_is_closed K.nonempty K.is_closed
 
 @[ext] lemma ext {S T : proper_cone E} (h : (S : convex_cone ℝ E) = T) : S = T :=
 by cases S; cases T; congr'
@@ -91,7 +84,7 @@ instance : has_star (proper_cone E) := ⟨ λ K,
 instance : has_involutive_star (proper_cone E) :=
 { star := has_star.star,
   star_involutive := λ K, proper_cone.ext $
-    K.inner_dual_cone_of_inner_dual_cone_eq_self K.nonempty K.is_closed }
+    (K : convex_cone ℝ E).inner_dual_cone_of_inner_dual_cone_eq_self K.nonempty K.is_closed }
 
 @[simp] lemma coe_star (K : proper_cone E) : ↑(star K) = (K : set E).inner_dual_cone := rfl
 
@@ -126,21 +119,16 @@ begin
   simp_rw [proper_cone.mem_map, proper_cone.mem_star, adjoint_inner_right, proper_cone.mem_coe,
     exists_prop],
 
-  -- there is a sequence `seq` in `K` such that `f ∘ seq` converges to `b`
+  -- there is a sequence `seq : ℕ → F` in the image of `f` that converges to `b`
   rintro ⟨seq, hmem, htends⟩ y hinner,
-  have := continuous.seq_continuous (continuous.inner (@continuous_const _ _ _ _ y) continuous_id)
-    (λ n, seq n) htends, clear htends,
-  simp_rw [id.def] at this,
-  -- the inner products `λ n, ⟪y, f (seq n)⟫_ℝ` converge to `⟪y, b⟫_ℝ` as `f x` converges to `b`
 
-  -- to show that the limit is non-negative it suffices to show that each term is nonnegative
-  suffices h : ∀ n, 0 ≤ ⟪y, seq n⟫_ℝ, from ge_of_tendsto' this h,
+  suffices h : ∀ n, 0 ≤ ⟪y, seq n⟫_ℝ, from ge_of_tendsto' (continuous.seq_continuous
+    (continuous.inner (@continuous_const _ _ _ _ y) continuous_id) htends) h,
 
-  -- the rest of the proof is a straightforward simplification
   intro n,
-  rcases hmem n with ⟨x, hxK, hxseq⟩, clear hmem,
-  specialize hinner x hxK,
-  rwa [hxseq, real_inner_comm] at hinner,
+  obtain ⟨x, hx, hseq⟩ := hmem n,
+  rw [← hseq, real_inner_comm],
+  exact hinner x hx,
 end
 begin
   -- proof by contradiction
@@ -150,7 +138,7 @@ begin
   contrapose! h,
 
   -- as `b ∉ K.map f`, there is a hyperplane `y` separating `b` from `K.map f`
-  obtain ⟨y, hxy, hyb⟩ := K.hyperplane_separation_of_nonempty_of_is_closed_of_nmem
+  obtain ⟨y, hxy, hyb⟩ := convex_cone.hyperplane_separation_of_nonempty_of_is_closed_of_nmem _
     (K.map f).nonempty (K.map f).is_closed h,
   use y,
 

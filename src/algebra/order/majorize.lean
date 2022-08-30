917e7a170a53bfbae9a065fc5a337dbf32eb61bf
Yaël Dillies <yael.dillies@gmail.com>
2022-08-30 08:44:43 +0000

    switch to multisets

--- src/algebra/order/majorize.lean
+++ src/algebra/order/majorize.lean
@@ -1,36 +1,92 @@
 /-
-Copyright (c) 2022 Yaël Dillies. All rights reserved.
+Copyright (u) 2022 Yaël Dillies. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Yaël Dillies
 -/
-import algebra.big_operators.order
+import algebra.big_operators.basic
 import data.multiset.sort
 
 /-!
 # Majorization
 -/
 
+namespace multiset
+variables {α : Type*} (r : α → α → Prop) [decidable_rel r] [is_total α r] [is_antisymm α r]
+  [is_trans α r]
+
+open function
+
+lemma left_inverse_coe_sort : left_inverse coe (sort r) := λ a, by simp
+lemma sort_injective : injective (sort r) := (left_inverse_coe_sort _).injective
+
+end multiset
+
+open list
 open_locale big_operators
 
-variables {ι α : Type*} [fintype ι] [linear_ordered_add_comm_group α] {a b c : ι → α}
+variables {α : Type*}
+
+section linear_ordered_add_comm_monoid
+variables [linear_ordered_add_comm_monoid α] {s t u : multiset α}
 
 -- Why do I need this?
-instance linear_ordered_add_comm_group.to_decidable_le : @decidable_rel α (≤) :=
+instance linear_ordered_add_comm_monoid.to_decidable_le : @decidable_rel α (≤) :=
 linear_order.decidable_le
 
-def weak_majorize (a b : ι → α) : Prop := ∀ n,
-(((finset.univ.1.map a).sort (≤)).inits.inth n).sum ≤
-  (((finset.univ.1.map b).sort (≤)).inits.inth n).sum
+def weak_majorize (s t : multiset α) : Prop :=
+s.card = t.card ∧ ∀ n, ((s.sort (≥)).take n).sum ≤ ((t.sort (≥)).take n).sum
 
-infix ` ≺ʷ `:50 := weak_majorize
+infix ` ≼ʷ `:50 := weak_majorize
 
-def majorize (a b : ι → α) : Prop := a ≺ʷ b ∧ ∑ i, a i = ∑ i, b i
+def majorize (s t : multiset α) : Prop := s ≼ʷ t ∧ s.sum = t.sum
 
-infix ` ≺ᵐ `:50 := majorize
+infix ` ≼ᵐ `:50 := majorize
 
-protected lemma majorize.weak_majorize : a ≺ᵐ b → a ≺ʷ b := and.left
+def strict_majorize (s t : multiset α) : Prop := s ≼ᵐ t ∧ ¬ t ≼ᵐ s
 
-lemma weak_majorize.trans (hab : a ≺ʷ b) (hbc : b ≺ʷ c) : a ≺ʷ c := λ n, (hab _).trans $ hbc _
+infix ` ≺ᵐ `:50 := strict_majorize
 
-lemma majorize.trans (hab : a ≺ᵐ b) (hbc : b ≺ᵐ c) : a ≺ᵐ c :=
-⟨hab.1.trans hbc.1, hab.2.trans hbc.2⟩
+protected lemma majorize.weak_majorize : s ≼ᵐ t → s ≼ʷ t := and.left
+protected lemma majorize.card_eq_card (h : s ≼ᵐ t) : s.card = t.card := h.1.1
+
+@[refl] lemma weak_majorize.refl (s : multiset α) : s ≼ʷ s := ⟨rfl, λ n, le_rfl⟩
+lemma weak_majorize.rfl : s ≼ʷ s := weak_majorize.refl _
+
+@[refl] lemma majorize.refl (s : multiset α) : s ≼ᵐ s := ⟨weak_majorize.rfl, rfl⟩
+lemma majorize.rfl : s ≼ᵐ s := majorize.refl _
+
+lemma strict_majorize.irrefl (s : multiset α) : ¬ s ≺ᵐ s := λ h, h.2 h.1
+
+@[trans] lemma weak_majorize.trans (hst : s ≼ʷ t) (htu : t ≼ʷ u) : s ≼ʷ u :=
+⟨hst.1.trans htu.1, λ n, (hst.2 _).trans $ htu.2 _⟩
+
+@[trans] lemma majorize.trans (hst : s ≼ᵐ t) (htu : t ≼ᵐ u) : s ≼ᵐ u :=
+⟨hst.1.trans htu.1, hst.2.trans htu.2⟩
+
+end linear_ordered_add_comm_monoid
+
+variables [linear_ordered_cancel_add_comm_monoid α] {s t : multiset α}
+
+-- Why do I need this?
+instance linear_ordered_cancel_add_comm_monoid.to_decidable_le : @decidable_rel α (≤) :=
+linear_order.decidable_le
+
+lemma weak_majorize.antisymm (hst : s ≼ʷ t) (hts : t ≼ʷ s) : s = t :=
+begin
+  refine multiset.sort_injective (≥) (list.ext_le (by simp [hst.1]) $ λ n h₀ h₁, _),
+  have h := λ n, (hst.2 n).antisymm (hts.2 n),
+  simpa only [sum_take_succ _ _ h₀, sum_take_succ _ _ h₁, h n, add_right_inj] using h (n + 1),
+end
+
+lemma majorize.antisymm (hst : s ≼ᵐ t) (hts : t ≼ᵐ s) : s = t := hst.1.antisymm hts.1
+
+lemma weak_majorize_antisymm_iff : s = t ↔ s ≼ʷ t ∧ t ≼ʷ s :=
+⟨by { rintro rfl, exact ⟨weak_majorize.rfl, weak_majorize.rfl⟩ }, λ h, h.1.antisymm h.2⟩
+
+lemma majorize_antisymm_iff : s = t ↔ s ≼ᵐ t ∧ t ≼ᵐ s :=
+⟨by { rintro rfl, exact ⟨majorize.rfl, majorize.rfl⟩ }, λ h, h.1.antisymm h.2⟩
+
+lemma majorize_iff_strict_majorize_or_eq : s ≼ᵐ t ↔ s ≺ᵐ t ∨ s = t :=
+by rw [strict_majorize, majorize_antisymm_iff, ←and_or_distrib_left, and_iff_left (em' _)]
+
+alias majorize_iff_strict_majorize_or_eq ↔ majorize.strict_majorize_or_eq _
--- src/analysis/convex/karamata.lean
+++ src/analysis/convex/karamata.lean
@@ -4,14 +4,70 @@ Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Yaël Dillies
 -/
 import algebra.order.majorize
-import analysis.convex.jensen
+import analysis.convex.function
 
 /-!
 # Karamata's inequality
 -/
 
-variables {ι α β : Type*} [fintype ι] [linear_ordered_add_comm_group α] [preorder β]
+open order_dual set
 
-def schur_convex (f : (ι → α) → β) : Prop := ∀ ⦃a b⦄, a ≺ᵐ b → f a ≤ f b
+variables {𝕜 α β : Type*}
 
-def schur_concave (f : (ι → α) → β) : Prop := ∀ ⦃a b⦄, a ≺ᵐ b → f b ≤ f a
+section linear_ordered_add_comm_monoid
+variables [linear_ordered_add_comm_monoid α] [preorder β] {f : multiset α → β}
+
+def schur_convex (f : multiset α → β) : Prop := ∀ ⦃s t⦄, s ≼ᵐ t → f s ≤ f t
+
+def schur_concave (f : multiset α → β) : Prop := ∀ ⦃s t⦄, s ≼ᵐ t → f t ≤ f s
+
+def strict_schur_convex (f : multiset α → β) : Prop := ∀ ⦃s t⦄, s ≺ᵐ t → f s < f t
+
+def strict_schur_concave (f : multiset α → β) : Prop := ∀ ⦃s t⦄, s ≺ᵐ t → f t < f s
+
+lemma schur_convex.dual : schur_convex f → schur_concave (to_dual ∘ f) := id
+lemma schur_concave.dual : schur_concave f → schur_convex (to_dual ∘ f) := id
+lemma strict_schur_convex.dual : strict_schur_convex f → strict_schur_concave (to_dual ∘ f) := id
+lemma strict_schur_concave.dual : strict_schur_concave f → strict_schur_convex (to_dual ∘ f) := id
+
+end linear_ordered_add_comm_monoid
+
+section linear_ordered_cancel_add_comm_monoid
+variables [linear_ordered_cancel_add_comm_monoid α] [preorder β] {f : multiset α → β}
+
+protected lemma strict_schur_convex.schur_convex (hf : strict_schur_convex f) : schur_convex f :=
+begin
+  rintro s t h,
+  obtain h | rfl := h.strict_majorize_or_eq,
+  { exact (hf h).le },
+  { refl }
+end
+
+end linear_ordered_cancel_add_comm_monoid
+
+variables [linear_ordered_field 𝕜] [linear_ordered_add_comm_group α] [ordered_add_comm_group β]
+  [module 𝕜 α] [module 𝕜 β] [ordered_smul 𝕜 α] [ordered_smul 𝕜 β] {f : α → β}
+
+/-- **Karamata's inequality**: Convex functions are Schur-convex. -/
+lemma convex_on.schur_convex (hf : convex_on 𝕜 univ f) : schur_convex (λ s, (s.map f).sum) :=
+begin
+  rintro s t hst,
+  sorry
+end
+
+/-- **Karamata's inequality**: Concave functions are Schur-concave. -/
+lemma concave_on.schur_concave (hf : concave_on 𝕜 univ f) : schur_concave (λ s, (s.map f).sum) :=
+@convex_on.schur_convex 𝕜 _ βᵒᵈ _ _ _ _ _ _ _ _ hf.dual
+
+/-- Strict **Karamata's inequality**: Strictly convex functions are strictly Schur-convex. -/
+lemma strict_convex_on.strict_schur_convex (hf : strict_convex_on 𝕜 univ f) :
+  strict_schur_convex (λ s, (s.map f).sum) :=
+begin
+  rintro s t hst,
+  sorry
+end
+
+/-- Strict **Karamata's inequality**: Strictly concave functions are strictly Schur-concave. -/
+lemma strict_concave_on.strict_schur_concave (hf : strict_concave_on 𝕜 univ f) :
+  strict_schur_concave (λ s, (s.map f).sum) :=
+@strict_convex_on.strict_schur_convex 𝕜 _ βᵒᵈ _ _ _ _ _ _ _ _ hf.dual

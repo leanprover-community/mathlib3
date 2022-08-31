a3f235f022e4463e1553fae4c82c8c596df92991
Apurva Nakade <apurvnakade@gmail.com>
2022-08-31 23:00:31 +0530

    Fix copyright

--- src/analysis/convex/cone/proper.lean
+++ src/analysis/convex/cone/proper.lean
@@ -1,6 +1,6 @@
 
 /-
-Copyright (c) 2020 Apurva Nakade All rights reserved.
+Copyright (c) 2022 Apurva Nakade All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Apurva Nakade
 -/

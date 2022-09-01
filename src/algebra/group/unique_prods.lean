e2b1c8125c9aae6a64564c586416bdb48e0e449e
adomani <adomani@gmail.com>
2022-09-01 09:16:21 +0200

    shorter proofs

--- src/algebra/group/unique_prods.lean
+++ src/algebra/group/unique_prods.lean
@@ -5,7 +5,7 @@ Authors: Damiano Testa
 -/
 import data.finset.lattice
 import data.real.basic
-
+import data.zmod.basic
 /-!
 #  Unique products and related notions
 -/
@@ -28,21 +28,39 @@ lemma subsingleton {G} [has_mul G] (A B : finset G) (a0 b0 : G) (h : unique_mul 
   ((h ha hb ab).1.trans (h ha' hb' ab').1.symm) $ (h ha hb ab).2.trans (h ha' hb' ab').2.symm⟩
 
 @[to_additive]
+lemma exists_unique_iff {G} [has_mul G] {A B : finset G} {a0 b0 : G} (aA : a0 ∈ A) (bB : b0 ∈ B) :
+  unique_mul A B a0 b0 ↔ ∃! ab ∈ A ×ˢ B, ab.1 * ab.2 = a0 * b0 :=
+⟨λ _, ⟨(a0, b0), ⟨finset.mem_product.mpr ⟨aA, bB⟩, rfl, by simp⟩, by simpa⟩, λ h, begin
+  rcases h with ⟨⟨a, b⟩, -, J⟩,
+  refine λ x y xA yB H, _,
+  replace J : ∀ {r s : G}, r ∈ A → s ∈ B → r * s = a0 * b0 → r = a ∧ s = b, by simpa using J,
+  rcases J xA yB H with ⟨rfl, rfl⟩,
+  rcases J aA bB rfl with ⟨rfl, rfl⟩,
+  exact ⟨rfl, rfl⟩,
+end⟩
+
+@[to_additive]
 lemma exists_exists_unique {G} [has_mul G] {A B : finset G} {a0 b0 : G} (aA : a0 ∈ A) (bB : b0 ∈ B)
   (u : unique_mul A B a0 b0) :
   ∃ g : G, ∃! ab ∈ A ×ˢ B, ab.1 * ab.2 = g :=
-⟨a0 * b0, (a0, b0), ⟨finset.mem_product.mpr ⟨aA, bB⟩, rfl, by simp⟩, by simpa⟩
+⟨a0 * b0, (exists_unique_iff aA bB).mp u⟩
+
+@[to_additive]
+lemma of_exists_unique {G} [has_mul G] {A B : finset G} {g : G}
+  (h : ∃! ab ∈ A ×ˢ B, ab.1 * ab.2 = g) :
+  ∃ a0 b0 : G, a0 ∈ A ∧ b0 ∈ B ∧ unique_mul A B a0 b0 :=
+begin
+  have h' := h,
+  rcases h' with ⟨⟨a,b⟩, ⟨hab, rfl, -⟩, -⟩,
+  cases finset.mem_product.mp hab with ha hb,
+  exact ⟨a, b, ha, hb, (exists_unique_iff ha hb).mpr h⟩,
+end
 
 @[to_additive]
 lemma of_exists_exists_unique {G} [has_mul G] {A B : finset G}
   (h : ∃ g : G, ∃! ab ∈ A ×ˢ B, ab.1 * ab.2 = g) :
   ∃ a0 b0 : G, a0 ∈ A ∧ b0 ∈ B ∧ unique_mul A B a0 b0 :=
-begin
-  rcases h with ⟨g, ⟨a,b⟩, ⟨hab, rfl, lh⟩, J⟩,
-  cases finset.mem_product.mp hab with ha hb,
-  exact ⟨a, b, ha, hb, λ x y xA yB xy,
-    prod.mk.inj_iff.mp (J (x, y) ⟨finset.mem_product.mpr ⟨xA, yB⟩, xy, λ _ _, rfl⟩)⟩,
-end
+of_exists_unique h.some_spec
 
 end unique_mul
 
@@ -71,7 +89,7 @@ instance {M} [has_add M] [unique_sums M] : unique_prods (multiplicative M) :=
 
 end multiplicative
 
---  please, golf my proof and my name!
+--  please, golf me and rename me!
 @[to_additive] lemma eq_and_eq_of_le_of_le_of_mul_eq {A} [has_mul A] [linear_order A]
   [covariant_class A A (*) (≤)] [covariant_class A A (function.swap (*)) (<)]
   [contravariant_class A A (*) (≤)]
@@ -88,17 +106,17 @@ begin
   { exact (lt_irrefl _ ((mul_lt_mul_of_lt_of_le h hb).trans_le ab.le)).elim }
 end
 
-/--  This instance asserts that if `A` has an multiplication and a linear order and multiplication
+/--  This instance asserts that if `A` has a multiplication, a linear order, and multiplication
 is 'very monotone', then `A` also has `unique_prods`. -/
 @[priority 100,  -- see Note [lower instance priority]
-to_additive "This instance asserts that if `A` has an addition and a linear order and addition
+to_additive "This instance asserts that if `A` has an addition, a linear order, and addition
 is 'very monotone', then `A` also has `unique_sums`."]
 instance covariants.to_unique_prods {A} [has_mul A] [linear_order A]
   [covariant_class A A (*) (≤)] [covariant_class A A (function.swap (*)) (<)]
   [contravariant_class A A (*) (≤)] : unique_prods A :=
 { muls := λ A B hA hB, ⟨_, A.min'_mem ‹_›, _, B.min'_mem ‹_›, λ a b ha hb ab,
     eq_and_eq_of_le_of_le_of_mul_eq (finset.min'_le _ _ ‹_›) (finset.min'_le _ _ ‹_›) ‹_›⟩ }
-.
+
 /--  Here you can see several examples of Types that have `unique_sums/prods`. -/
 example : unique_sums ℕ   := by apply_instance
 example : unique_sums ℕ+  := by apply_instance
@@ -107,7 +125,7 @@ example : unique_sums ℚ   := by apply_instance
 example : unique_sums ℝ   := by apply_instance
 example : unique_prods ℕ+ := by apply_instance
 
-/--  And also a Type that does not have `unique_prods`. -/
+/--  A Type that does not have `unique_prods`. -/
 example : ¬ unique_prods ℕ :=
 begin
   rintros ⟨h⟩,
@@ -115,4 +133,16 @@ begin
   suffices : (∃ (x : ℕ), (x = 0 ∨ x = 1) ∧ ¬x = 0) ∧ ∃ (x : ℕ), (x = 0 ∨ x = 1) ∧ ¬x = 1,
   { simpa [unique_mul] },
   exact ⟨⟨1, by simp⟩, ⟨0, by simp⟩⟩,
+end
+
+/--  A Type that does not have `unique_sums`. -/
+example (n : ℕ) (n2 : 2 ≤ n): ¬ unique_sums (zmod n) :=
+begin
+  haveI : fintype (zmod n) := @zmod.fintype n ⟨(zero_lt_two.trans_le n2).ne'⟩,
+  haveI : nontrivial (zmod n) := char_p.nontrivial_of_char_ne_one (one_lt_two.trans_le n2).ne',
+  rintros ⟨h⟩,
+  refine not_not.mpr (h finset.univ_nonempty finset.univ_nonempty) _,
+  suffices : ∀ (x y : zmod n), ∃ (x' y' : zmod n), x' + y' = x + y ∧ (x' = x → ¬y' = y),
+  { simpa [unique_add] },
+  exact λ x y, ⟨x - 1, y + 1, sub_add_add_cancel _ _ _, by simp⟩,
 end

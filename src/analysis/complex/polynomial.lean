66a7b761a4a7a927cb0f7557abf18a503e5d007e
Eric Rodriguez <ericrboidi@gmail.com>
2022-09-02 00:32:53 +0100

    ftc

--- src/analysis/complex/polynomial.lean
+++ src/analysis/complex/polynomial.lean
@@ -37,10 +37,10 @@ have hg : g * (X - C z₀) ^ n = f - C (f.eval z₀),
   from div_by_monic_mul_pow_root_multiplicity_eq _ _,
 have hn0 : n ≠ 0, from λ hn0, by simpa [g, hn0] using hg0,
 let ⟨δ', hδ'₁, hδ'₂⟩ := continuous_iff.1 (polynomial.continuous g) z₀
-  ((g.eval z₀).abs) (complex.abs_pos.2 hg0) in
+  ((g.eval z₀).abs) (abs.pos hg0) in
 let δ := min (min (δ' / 2) 1) (((f.eval z₀).abs / (g.eval z₀).abs) / 2) in
-have hf0' : 0 < (f.eval z₀).abs, from complex.abs_pos.2 hf0,
-have hg0' : 0 < abs (eval z₀ g), from complex.abs_pos.2 hg0,
+have hf0' : 0 < (f.eval z₀).abs, from abs.pos hf0,
+have hg0' : 0 < (eval z₀ g).abs, from abs.pos hg0,
 have hfg0 : 0 < (f.eval z₀).abs / abs (eval z₀ g), from div_pos hf0' hg0',
 have hδ0 : 0 < δ, from lt_min (lt_min (half_pos hδ'₁) (by norm_num)) (half_pos hfg0),
 have hδ : ∀ z : ℂ, abs (z - z₀) = δ → abs (g.eval z - g.eval z₀) < (g.eval z₀).abs,
@@ -65,13 +65,13 @@ have hF₂ : (F.eval z').abs = (f.eval z₀).abs - (g.eval z₀).abs * δ ^ n,
   from calc (F.eval z').abs = (f.eval z₀ - f.eval z₀ * (g.eval z₀).abs
     * δ ^ n / (f.eval z₀).abs).abs : congr_arg abs hF₁
   ... = abs (f.eval z₀) * complex.abs (1 - (g.eval z₀).abs * δ ^ n /
-      (f.eval z₀).abs : ℝ) : by rw [← complex.abs_mul];
+      (f.eval z₀).abs : ℝ) : by rw [←map_mul];
         exact congr_arg complex.abs
           (by simp [mul_add, add_mul, mul_assoc, div_eq_mul_inv, sub_eq_add_neg])
   ... = _ : by rw [complex.abs_of_nonneg (sub_nonneg.2 (le_of_lt hδs)),
       mul_sub, mul_div_cancel' _ (ne.symm (ne_of_lt hf0')), mul_one],
 have hef0 : abs (eval z₀ g) * (eval z₀ f).abs ≠ 0,
-  from mul_ne_zero (mt complex.abs_eq_zero.1 hg0) (mt complex.abs_eq_zero.1 hf0),
+  from mul_ne_zero (abs.ne_zero hg0) (abs.ne_zero hf0),
 have hz'z₀ : abs (z' - z₀) = δ,
   by simp [z', mul_assoc, mul_left_comm _ (_ ^ n), mul_comm _ (_ ^ n),
     mul_comm (eval z₀ f).abs, _root_.mul_div_cancel _ hef0, of_real_mul,
@@ -89,7 +89,7 @@ have hF₃ : (f.eval z' - F.eval z').abs < (g.eval z₀).abs * δ ^ n,
 lt_irrefl (f.eval z₀).abs $
   calc (f.eval z₀).abs ≤ (f.eval z').abs : hz₀ _
     ... = (F.eval z' + (f.eval z' - F.eval z')).abs : by simp
-    ... ≤ (F.eval z').abs + (f.eval z' - F.eval z').abs : complex.abs_add _ _
+    ... ≤ (F.eval z').abs + (f.eval z' - F.eval z').abs : abs.add_le _ _
     ... < (f.eval z₀).abs - (g.eval z₀).abs * δ ^ n + (g.eval z₀).abs * δ ^ n :
       add_lt_add_of_le_of_lt (by rw hF₂) hF₃
     ... = (f.eval z₀).abs : sub_add_cancel _ _

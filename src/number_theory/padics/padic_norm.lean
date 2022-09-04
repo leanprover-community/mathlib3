74475986162e78fad542767df4857d15c0456677
Johan Commelin <johan@commelin.net>
2022-09-04 07:18:15 +0200

    wip

--- src/number_theory/padics/padic_norm.lean
+++ src/number_theory/padics/padic_norm.lean
@@ -207,7 +207,9 @@ the norm of `q`.
 protected theorem nonarchimedean {q r : ℚ} :
   padic_norm p (q + r) ≤ max (padic_norm p q) (padic_norm p r) :=
 begin
-    wlog hle := le_total (padic_val_rat p q) (padic_val_rat p r) using [q r],
+    wlog hle : (padic_val_rat p q) ≤ (padic_val_rat p r),
+    { cases le_total (padic_val_rat p q) (padic_val_rat p r) with H H,
+      { exact hle H }, { rw [add_comm, max_comm], exact hle H } },
     exact nonarchimedean_aux p hle
 end
 
@@ -234,8 +236,9 @@ the norms of `q` and `r`.
 lemma add_eq_max_of_ne {q r : ℚ} (hne : padic_norm p q ≠ padic_norm p r) :
   padic_norm p (q + r) = max (padic_norm p q) (padic_norm p r) :=
 begin
-  wlog hle := le_total (padic_norm p r) (padic_norm p q) using [q r],
-  have hlt : padic_norm p r < padic_norm p q, from lt_of_le_of_ne hle hne.symm,
+    wlog hlt : (padic_norm p r) < (padic_norm p q),
+    { cases hne.symm.lt_or_lt with H H,
+      { exact hlt hne H }, { rw [add_comm, max_comm], exact hlt  hne.symm H } },
   have : padic_norm p q ≤ max (padic_norm p (q + r)) (padic_norm p r), from calc
    padic_norm p q = padic_norm p (q + r - r) : by congr; ring
                ... ≤ max (padic_norm p (q + r)) (padic_norm p (-r)) : padic_norm.nonarchimedean p
--- src/topology/algebra/order/intermediate_value.lean
+++ src/topology/algebra/order/intermediate_value.lean
@@ -391,7 +391,10 @@ lemma is_preconnected_Icc : is_preconnected (Icc a b) :=
 is_preconnected_closed_iff.2
 begin
   rintros s t hs ht hab ⟨x, hx⟩ ⟨y, hy⟩,
-  wlog hxy : x ≤ y := le_total x y using [x y s t, y x t s],
+  wlog hxy : x ≤ y,
+  { cases le_total x y with H H,
+    { exact hxy s t hs ht hab x hx y hy H },
+    { rw union_comm at hab, rw inter_comm s, exact hxy t s ht hs hab y hy x hx H }, },
   have xyab : Icc x y ⊆ Icc a b := Icc_subset_Icc hx.1.1 hy.1.2,
   by_contradiction hst,
   suffices : Icc x y ⊆ s,

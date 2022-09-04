98137ffac5d26de6f500c97e24ff899b16b07c08
RemyDegenne <Remydegenne@gmail.com>
2022-09-04 12:41:00 +0200

    fix

--- src/probability/martingale/basic.lean
+++ src/probability/martingale/basic.lean
@@ -575,8 +575,8 @@ begin
   have h := set_integral_ge_of_const_le (measurable_set_le measurable_const
     (finset.measurable_range_sup'' (λ n _, (hsub.strongly_measurable n).measurable.le (𝒢.le n))))
     (measure_ne_top _ _) this
-    (integrable.integrable_on (integrable_stopped_value (hitting_is_stopping_time
-     hsub.adapted measurable_set_Ici) hsub.integrable hitting_le)),
+    (integrable.integrable_on (@integrable_stopped_value Ω ℕ _ _ _ _ _ _ _ _ _
+      (hitting_is_stopping_time hsub.adapted measurable_set_Ici) hsub.integrable _ hitting_le)),
   rw [ennreal.le_of_real_iff_to_real_le, ennreal.to_real_smul],
   { exact h },
   { exact ennreal.mul_ne_top (by simp) (measure_ne_top _ _) },
@@ -625,7 +625,7 @@ begin
     begin
       refine add_le_add (smul_le_stopped_value_hitting hsub _)
         (ennreal.of_real_le_of_real (set_integral_mono_on (hsub.integrable n).integrable_on
-        (integrable.integrable_on (integrable_stopped_value
+        (integrable.integrable_on (@integrable_stopped_value Ω ℕ _ _ _ _ _ _ _ _ _
           (hitting_is_stopping_time hsub.adapted measurable_set_Ici) hsub.integrable hitting_le))
         (measurable_set_lt (finset.measurable_range_sup''
           (λ n _, (hsub.strongly_measurable n).measurable.le (𝒢.le n))) measurable_const) _)),

dcf95b139ac4c23d3e77a4dc4b5b56f84cf93ed8
Damiano Testa <adomani@gmail.com>
2022-09-07 22:45:15 +0100

    tests

---
+++ test/remove_subs.lean
@@ -0,0 +1,8 @@
+import tactic.remove_subs
+
+example {i l : ℕ} (i1 : 1 ≤ i) (il : i + 1 ≤ l) :
+  i - 1 + (l - i - 1 + 1) + 1 = l :=
+by remove_subs!
+
+example (n m : ℕ) : nat.pred n - m = nat.pred (n - m) :=
+show n - 1 - m = n - m - 1, by remove_subs!

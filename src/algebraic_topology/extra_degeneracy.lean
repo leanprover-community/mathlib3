b6a041c522856fca8a53d0065ab9006f5e156123
Joël Riou <joel.riou@universite-paris-saclay.fr>
2022-09-07 08:54:40 +0200

    typo

--- src/algebraic_topology/extra_degeneracy.lean
+++ src/algebraic_topology/extra_degeneracy.lean
@@ -15,7 +15,7 @@ In simplicial homotopy theory, in order to prove that the connected components
 of a simplicial set `X` are contractible, it suffices to construct an extra
 degeneracy as it is defined in *Simplicial Homotopy Theory* by Goerrs-Jardine p. 190.
 It consists of a series of maps `π₀ X → X _[0]` and `X _[n] → X _[n+1]` which
-behaves formally like an extra degeneracy `σ (-1)`. It can be thought as a datum
+behave formally like an extra degeneracy `σ (-1)`. It can be thought as a datum
 associated to the augmented simplicial set `X → π₀ X`.
 
 In this file, we adapt this definition to the case of augmented

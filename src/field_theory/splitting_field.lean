e7316d7354d69c70c8535b38e43a6a93e0d41d69
negiizhao <egresf@gmail.com>
2022-09-07 16:32:48 +0800

    Update splitting_field.lean

--- src/field_theory/splitting_field.lean
+++ src/field_theory/splitting_field.lean
@@ -122,10 +122,10 @@ theorem splits_of_is_unit [is_domain K] {u : K[X]} (hu : is_unit u) : u.splits i
 (is_unit_iff.mp hu).some_spec.2 ▸ splits_C _ _
 
 theorem splits_X_sub_C {x : K} : (X - C x).splits i :=
-splits_of_degree_le_one _ degree_X_sub_C_le
+splits_of_degree_le_one _ $ degree_X_sub_C_le _
 
 theorem splits_X : X.splits i :=
-splits_of_degree_le_one _ degree_X_le
+splits_of_degree_le_one _ $ degree_X_le _
 
 theorem splits_prod {ι : Type u} {s : ι → K[X]} {t : finset ι} :
   (∀ j ∈ t, (s j).splits i) → (∏ x in t, s x).splits i :=

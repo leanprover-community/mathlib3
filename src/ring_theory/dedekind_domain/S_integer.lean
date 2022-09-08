882a08aea91a0ca3339ecb2b4e9f4696d4cfcbc9
David Kurniadi Angdinata <dka31@cantab.ac.uk>
2022-09-08 20:04:58 +0100

    Refactor with subalgebra

--- src/ring_theory/dedekind_domain/S_integer.lean
+++ src/ring_theory/dedekind_domain/S_integer.lean
@@ -14,14 +14,14 @@ the height one spectrum of `R`. An `S`-integer of `K` is defined to have `v`-adi
 one for all primes ideals `v` away from `S`, whereas an `S`-unit of `Kˣ` is defined to have `v`-adic
 valuation exactly one for all prime ideals `v` away from `S`.
 
-This file defines the subring of `S`-integers of `K` and the subgroup of `S`-units of `Kˣ`, which
-can be specialised to the case of a number field or a function field separately.
+This file defines the subalgebra of `S`-integers of `K` and the subgroup of `S`-units of `Kˣ`, where
+`K` can be specialised to the case of a number field or a function field separately.
 
 ## Main definitions
 
  * `set.integer`: `S`-integers.
  * `set.unit`: `S`-units.
- * TODO: notation for `S`-integers and `S`-units.
+ * TODO: localised notation for `S`-integers.
 
 ## Main statements
 
@@ -41,13 +41,13 @@ can be specialised to the case of a number field or a function field separately.
 S integer, S-integer, S unit, S-unit
 -/
 
-open is_dedekind_domain
-
-open_locale non_zero_divisors
-
 namespace set
 
 noncomputable theory
+
+open is_dedekind_domain
+
+open_locale non_zero_divisors
 
 universes u v
 
@@ -56,30 +56,21 @@ variables {R : Type u} [comm_ring R] [is_domain R] [is_dedekind_domain R]
 
 /-! ## `S`-integers -/
 
-/-- The subring of `S`-integers of `K`. -/
-@[simps] def integer : subring K :=
-(⨅ v ∉ S, (v : height_one_spectrum R).valuation.valuation_subring.to_subring).copy
-  {x : K | ∀ v ∉ S, (v : height_one_spectrum R).valuation x ≤ 1} $ set.ext $ λ _,
-  by simpa only [set_like.mem_coe, subring.mem_infi]
+/-- The `R`-subalgebra of `S`-integers of `K`. -/
+@[simps] def integer : subalgebra R K :=
+{ algebra_map_mem' := λ x v _, v.valuation_le_one x,
+  .. (⨅ v ∉ S, (v : height_one_spectrum R).valuation.valuation_subring.to_subring).copy
+      {x : K | ∀ v ∉ S, (v : height_one_spectrum R).valuation x ≤ 1} $ set.ext $ λ _,
+      by simpa only [set_like.mem_coe, subring.mem_infi] }
 
 lemma integer_eq :
-  S.integer K = ⨅ v ∉ S, (v : height_one_spectrum R).valuation.valuation_subring.to_subring :=
-subring.copy_eq _ _ _
+  (S.integer K).to_subring
+    = ⨅ v ∉ S, (v : height_one_spectrum R).valuation.valuation_subring.to_subring :=
+set_like.ext' $ by simpa only [integer, subring.copy_eq]
 
 lemma integer_valuation_le_one (x : S.integer K) {v : height_one_spectrum R} (hv : v ∉ S) :
   v.valuation (x : K) ≤ 1 :=
 x.property v hv
-
-/-- The subring of `S`-integers of `K` is an algebra over `R`. -/
-instance : algebra R (S.integer K) :=
-{ smul      := λ x y, ⟨algebra_map R K x, λ v _, v.valuation_le_one x⟩ * y,
-  to_fun    := λ x, ⟨algebra_map R K x, λ v _, v.valuation_le_one x⟩,
-  map_one'  := by simpa only [map_one],
-  map_mul'  := λ _ _, by simpa only [map_mul],
-  map_zero' := by simpa only [map_zero],
-  map_add'  := λ _ _, by simpa only [map_add],
-  commutes' := λ _, mul_comm _,
-  smul_def' := λ _ _, rfl }
 
 /-! ## `S`-units -/
 
@@ -99,13 +90,11 @@ x.property v hv
 
 /-- The group of `S`-units is the group of units of the ring of `S`-integers. -/
 @[simps] def unit_equiv_units_integer : S.unit K ≃* (S.integer K)ˣ :=
-{ to_fun    := λ x, ⟨⟨x.val, λ v hv, by { rw [x.property v hv], exact le_rfl }⟩,
-    ⟨x.val.inv, λ v hv, by { rw [← v.valuation.map_one, ← congr_arg v.valuation x.val.val_inv,
-      map_mul, units.val_eq_coe, x.property v hv, one_mul], exact le_rfl }⟩,
-    subtype.mk_eq_mk.mpr x.val.val_inv, subtype.mk_eq_mk.mpr x.val.inv_val⟩,
+{ to_fun    := λ x, ⟨⟨x, λ v hv, (x.property v hv).le⟩, ⟨↑x⁻¹, λ v hv, ((x⁻¹).property v hv).le⟩,
+    subtype.ext x.val.val_inv, subtype.ext x.val.inv_val⟩,
   inv_fun   := λ x, ⟨units.mk0 x $ λ hx, x.ne_zero ((subring.coe_eq_zero_iff _).mp hx),
-  λ v hv, eq_one_of_one_le_mul_left (x.val.property v hv) (x.inv.property v hv) $ eq.ge $
-    by { rw [← map_mul, ← v.valuation.map_one], congr' 1, exact subtype.mk_eq_mk.mp x.val_inv }⟩,
+    λ v hv, eq_one_of_one_le_mul_left (x.val.property v hv) (x.inv.property v hv) $ eq.ge $
+      by { rw [← map_mul], convert v.valuation.map_one, exact subtype.mk_eq_mk.mp x.val_inv }⟩,
   left_inv  := λ _, by { ext, refl },
   right_inv := λ _, by { ext, refl },
   map_mul'  := λ _ _, by { ext, refl } }

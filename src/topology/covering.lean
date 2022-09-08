33d41738abf9054371826b54cff08e378db9f43c
Jordan Brown <jlcbrown333@gmail.com>
2022-09-08 14:57:03 -0700

    aligned with covering2

--- src/topology/covering.lean
+++ src/topology/covering.lean
@@ -1,213 +1,109 @@
+
 /-
 Copyright (c) 2021 Thomas Browning. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
-Authors: Jordan Brown, Thomas Browning
+Authors: Thomas Browning
 -/
 import topology.is_locally_homeomorph
 import topology.fiber_bundle
 
 /-!
-# Local homeomorphisms
-
+# Covering Maps
 This file defines covering maps.
-
 ## Main definitions
-
-* `covering_map`: A covering map is a surjective continuous function `f : E → X` with discrete
-  fibers where each point of `X` has an evenly covered neighborhood.
+* `is_covering_map`: A covering map is a continuous function `f : E → X` with discrete
+  fibers such that each point of `X` has an evenly covered neighborhood.
 -/
 
 variables {E X : Type*} [topological_space E] [topological_space X] (f : E → X)
-  (I : Type*) [topological_space I] {x y : X} (U V : set X)
 
-/-- `U` is evenly covered if `I × U` is homeomorphic to `f ⁻¹' U` -/
-@[nolint has_inhabited_instance] -- not all spaces are covering spaces
-structure evenly_covered_set extends homeomorph (I × U) (f ⁻¹' U) :=
-(commutes' : ∀ p, f (to_fun p) = p.2)
+open topological_fiber_bundle
 
-variables {f I U V}
+/-- A point `x : X` is evenly covered by `f : E → X` if `x` has an evenly covered neighborhood. -/
+def is_evenly_covered (x : X) (I : Type*) [topological_space I] :=
+discrete_topology I ∧ ∃ t : trivialization I f, x ∈ t.base_set
 
-namespace evenly_covered_set
-
-variables (ϕ : evenly_covered_set f I U)
-
-instance : has_coe_to_fun (evenly_covered_set f I U) (λ ι, I × U → f ⁻¹' U) :=
-⟨λ ϕ, ϕ.to_fun⟩
-
-lemma is_open_map : is_open_map ϕ :=
-ϕ.to_homeomorph.is_open_map
-
-/-- The homeomorphism from `f ⁻¹' U` to `I × U` -/
-def symm : f ⁻¹' U ≃ₜ I × U :=
-ϕ.to_homeomorph.symm
-
-@[simp] lemma apply_symm_apply (p : f ⁻¹' U) : ϕ (ϕ.symm p) = p :=
-ϕ.to_equiv.apply_symm_apply p
-
-@[simp] lemma symm_apply_apply (p : I × U) : ϕ.symm (ϕ p) = p :=
-ϕ.to_equiv.symm_apply_apply p
-
-lemma commutes (p : I × U) : f (ϕ p) = p.2 :=
-ϕ.commutes' p
-
-lemma commutes_inv (p : f ⁻¹' U) : f p = (ϕ.symm p).2 :=
-by rw [←ϕ.apply_symm_apply p, ϕ.commutes, ϕ.apply_symm_apply]
-
-/-- Subsets of an evenly covered set are evenly covered -/
-def mono (h : V ⊆ U) : evenly_covered_set f I V :=
-{ to_fun := λ p, ⟨ϕ ⟨p.1, p.2, h p.2.2⟩, by rw [set.mem_preimage, ϕ.commutes]; exact p.2.2⟩,
-  inv_fun := λ p, ⟨(ϕ.symm ⟨p, h p.2⟩).1,
-    ⟨(ϕ.symm ⟨p, h p.2⟩).2, by rw [←ϕ.commutes, apply_symm_apply]; exact p.2⟩⟩,
-  left_inv := λ p, by simp only [subtype.coe_mk, subtype.coe_eta, prod.mk.eta, symm_apply_apply],
-  right_inv := λ p, by simp only [subtype.coe_mk, subtype.coe_eta, prod.mk.eta, apply_symm_apply],
-  commutes' := λ p, ϕ.commutes ⟨p.1, p.2, h p.2.2⟩,
-  continuous_to_fun := by continuity! }
-
-/-- Each fiber is homeomorphic to the index set -/
-def fiber_homeomorph (hx : x ∈ U) : f ⁻¹' {x} ≃ₜ I :=
-{ to_fun := λ p, (ϕ.symm ⟨p, (congr_arg (∈ U) p.2).mpr hx⟩).1,
-  inv_fun := λ p, ⟨ϕ ⟨p, x, hx⟩, ϕ.commutes ⟨p, x, hx⟩⟩,
-  left_inv := λ p, subtype.ext (by
-  { obtain ⟨p, rfl : f p = x⟩ := p,
-    have := ϕ.commutes_inv ⟨p, _⟩,
-    rw subtype.coe_mk at this,
-    simp only [this, subtype.coe_mk, subtype.coe_eta, prod.mk.eta, apply_symm_apply],
-    exact hx }),
-  right_inv := λ p, by simp only [subtype.coe_mk, subtype.coe_eta, symm_apply_apply],
-  continuous_inv_fun := by continuity! }
-
-end evenly_covered_set
-
-variables (f)
-
-/-- `U` is evenly covered if `f ⁻¹' {x} × U` is homeomorphic to `f ⁻¹' U` -/
-@[nolint has_inhabited_instance] -- not all spaces are covering spaces
-structure evenly_covered_pt (hx : x ∈ U) extends evenly_covered_set f (f ⁻¹' {x}) U :=
-(rigid' : ∀ p : f ⁻¹' {x}, to_fun ⟨p, x, hx⟩ = ⟨p, (congr_arg (∈ U) p.2).mpr hx⟩)
+namespace is_evenly_covered
 
 variables {f}
 
-/-- Evenly covered with index set `I` implies evenly covered with index set `f ⁻¹' {x}` -/
-def evenly_covered_set.to_evenly_covered_pt (ϕ : evenly_covered_set f I U) (hx : x ∈ U) :
-  evenly_covered_pt f hx :=
-{ to_homeomorph := ((ϕ.fiber_homeomorph hx).prod_congr (homeomorph.refl U)).trans ϕ.to_homeomorph,
-  commutes' := λ p, ϕ.commutes ⟨ϕ.fiber_homeomorph hx p.1, p.2⟩,
-  rigid' := λ p, ϕ.to_equiv.apply_eq_iff_eq_symm_apply.mpr (prod.ext rfl (subtype.ext
-    (p.2.symm.trans (ϕ.commutes_inv ⟨p, (congr_arg (∈ U) p.2).mpr hx⟩)))) }
+/-- If `x` is evenly covered by `f`, then we can construct a trivialization of `f` at `x`. -/
+noncomputable def to_trivialization {x : X} {I : Type*} [topological_space I]
+  (h : is_evenly_covered f x I) : trivialization (f ⁻¹' {x}) f :=
+(classical.some h.2).trans_fiber_homeomorph ((classical.some h.2).preimage_singleton_homeomorph
+  (classical.some_spec h.2)).symm
 
-namespace evenly_covered_pt
+lemma mem_to_trivialization_base_set {x : X} {I : Type*} [topological_space I]
+  (h : is_evenly_covered f x I) : x ∈ h.to_trivialization.base_set :=
+classical.some_spec h.2
 
-variables {hx : x ∈ U} (ϕ : evenly_covered_pt f hx)
+lemma to_trivialization_apply {x : E} {I : Type*} [topological_space I]
+  (h : is_evenly_covered f (f x) I) : (h.to_trivialization x).2 = ⟨x, rfl⟩ :=
+let e := classical.some h.2, h := classical.some_spec h.2, he := e.mk_proj_snd' h in
+  subtype.ext ((e.to_local_equiv.eq_symm_apply (e.mem_source.mpr h)
+    (by rwa [he, e.mem_target, e.coe_fst (e.mem_source.mpr h)])).mpr he.symm).symm
 
-instance : has_coe_to_fun (evenly_covered_pt f hx) (λ ι, f ⁻¹' {x} × U → f ⁻¹' U) :=
-⟨λ ϕ, ϕ.to_fun⟩
+lemma continuous_at {x : E} {I : Type*} [topological_space I]
+  (h : is_evenly_covered f (f x) I) : continuous_at f x :=
+let e := h.to_trivialization in
+  e.continuous_at_proj (e.mem_source.mpr (mem_to_trivialization_base_set h))
 
-lemma is_open_map : is_open_map ϕ :=
-ϕ.to_evenly_covered_set.is_open_map
+lemma to_is_evenly_covered_preimage {x : X} {I : Type*} [topological_space I]
+  (h : is_evenly_covered f x I) : is_evenly_covered f x (f ⁻¹' {x}) :=
+let ⟨h1, h2⟩ := h in by exactI ⟨((classical.some h2).preimage_singleton_homeomorph
+  (classical.some_spec h2)).embedding.discrete_topology, _, h.mem_to_trivialization_base_set⟩
 
-/-- The homeomorphism from `f ⁻¹' U` to `I × U` -/
-def symm : f ⁻¹' U ≃ₜ f ⁻¹' {x} × U :=
-ϕ.to_evenly_covered_set.symm
+end is_evenly_covered
 
-@[simp] lemma apply_symm_apply (p : f ⁻¹' U) : ϕ (ϕ.symm p) = p :=
-ϕ.to_evenly_covered_set.apply_symm_apply p
+/-- A covering map is a continuous function `f : E → X` with discrete fibers such that each point
+  of `X` has an evenly covered neighborhood. -/
+def is_covering_map :=
+∀ x, is_evenly_covered f x (f ⁻¹' {x})
 
-@[simp] lemma symm_apply_apply (p : f ⁻¹' {x} × U) : ϕ.symm (ϕ p) = p :=
-ϕ.to_evenly_covered_set.symm_apply_apply p
+namespace is_covering_map
 
-lemma commutes (p : f ⁻¹' {x} × U) : f (ϕ p) = p.2 :=
-ϕ.to_evenly_covered_set.commutes p
-
-lemma commutes_inv (p : f ⁻¹' U) : f p = (ϕ.symm p).2 :=
-ϕ.to_evenly_covered_set.commutes_inv p
-
-lemma rigid (p : f ⁻¹' {x}) : ϕ ⟨p, x, hx⟩ = ⟨p, (congr_arg (∈ U) p.2).mpr hx⟩ :=
-ϕ.rigid' p
-
-/-- Subsets of an evenly covered set are evenly covered -/
-def mono (h : V ⊆ U) (hx' : x ∈ V) : evenly_covered_pt f hx' :=
-(ϕ.to_evenly_covered_set.mono h).to_evenly_covered_pt hx'
-
-/-- `evenly_covered_pt` does not depend on the choice of point -/
-def translate (hy : y ∈ U) : evenly_covered_pt f hy :=
-ϕ.to_evenly_covered_set.to_evenly_covered_pt hy
-
-end evenly_covered_pt
-
-variables (f)
-
-/-- `U` is evenly covered if `f ⁻¹' {x} × U` is homeomorphic to `f ⁻¹' U` -/
-def evenly_covered (hx : x ∈ U) : Prop :=
-nonempty (evenly_covered_pt f hx)
+lemma mk (F : X → Type*) [Π x, topological_space (F x)]
+  [hF : Π x, discrete_topology (F x)] (e : Π x, trivialization (F x) f)
+  (h : ∀ x, x ∈ (e x).base_set) : is_covering_map f :=
+λ x, is_evenly_covered.to_is_evenly_covered_preimage ⟨hF x, e x, h x⟩
 
 variables {f}
 
-namespace evenly_covered
+lemma continuous (hf : is_covering_map f) : continuous f :=
+continuous_iff_continuous_at.mpr (λ x, (hf (f x)).continuous_at)
 
-lemma mono {hx : x ∈ V} {hy : x ∈ U} (hf : evenly_covered f hy) (h : V ⊆ U) :
-  evenly_covered f hx :=
-hf.elim (λ g, ⟨g.mono h hx⟩)
-
-lemma translate {hx : x ∈ U} (hf : evenly_covered f hx) (hy : y ∈ U) : evenly_covered f hy :=
-hf.elim (λ g, ⟨g.translate hy⟩)
-
-end evenly_covered
-
-lemma evenly_covered_translate_iff (hx : x ∈ U) (hy : y ∈ U) :
-  evenly_covered f hx ↔ evenly_covered f hy :=
-⟨λ hf, hf.translate hy, λ hf, hf.translate hx⟩
-
-variables (E X)
-
-/-- A covering map is a surjective continuous function `f : E → X` with discrete
-  fibers where each point of `X` has an evenly covered neighborhood. -/
-@[nolint has_inhabited_instance] -- not all spaces are covering spaces
-structure covering_map extends continuous_map E X :=
-(discrete_fibers : ∀ x, discrete_topology (to_fun ⁻¹' {x}))
-(evenly_covered : ∀ x, ∃ U, is_open U ∧ ∃ hU : x ∈ U, evenly_covered to_fun hU)
-
-variables {E X}
-
-namespace covering_map
-
-infixr ` ↠ `:25 := covering_map
-
-instance : has_coe_to_fun (E ↠ X) (λ _, E → X) := ⟨λ q, q.to_fun⟩
-
-lemma continuous (q : E ↠ X) : continuous q := q.continuous_to_fun
-
-lemma is_locally_homeomorph (q : E ↠ X) : is_locally_homeomorph q :=
+lemma is_locally_homeomorph (hf : is_covering_map f) : is_locally_homeomorph f :=
 begin
-  classical,
-  intro x,
-  obtain ⟨U, hU, hx, ⟨ϕ⟩⟩ := q.evenly_covered (q x),
-  let ψ : U → E := λ p, ϕ ⟨⟨x, rfl⟩, p⟩,
-  have hψ : ∀ p : U, q (ψ p) = p := λ p, ϕ.commutes ⟨⟨x, rfl⟩, p⟩,
-  refine ⟨{ to_fun := q,
-    inv_fun := λ p, if hp : p ∈ U then ψ ⟨p, hp⟩ else x,
-    source := set.range ψ,
-    target := U,
-    open_source := by
-    { haveI := q.discrete_fibers,
-      simp only,
-      rw [←set.image_univ, set.image_comp (coe ∘ ϕ), ←set.singleton_prod],
-      exact (hU.preimage q.continuous).is_open_map_subtype_coe.comp ϕ.is_open_map _
-        ((is_open_discrete _).prod is_open_univ) },
-    open_target := hU,
-    map_source' := λ _ ⟨⟨p, _⟩, hp⟩, by rwa [←hp, hψ],
-    map_target' := λ p hp, ⟨⟨p, hp⟩, by rw [dif_pos hp]⟩,
-    left_inv' := λ _ ⟨p, hp⟩, by rw [←hp, hψ, dif_pos p.prop, subtype.coe_eta],
-    right_inv' := λ p hp, by rw [dif_pos hp, hψ, subtype.coe_mk],
-    continuous_to_fun := q.continuous.continuous_on,
-    continuous_inv_fun := by
-    { rw [continuous_on_iff_continuous_restrict, set.restrict_dite],
-      continuity! } }, ⟨⟨q x, hx⟩, subtype.ext_iff.mp (ϕ.rigid ⟨x, rfl⟩)⟩, rfl⟩,
+  refine is_locally_homeomorph.mk f (λ x, _),
+  let e := (hf (f x)).to_trivialization,
+  have h := (hf (f x)).mem_to_trivialization_base_set,
+  refine ⟨e.to_local_homeomorph.trans
+  { to_fun := λ p, p.1,
+    inv_fun := λ p, ⟨p, x, rfl⟩,
+    source := e.base_set ×ˢ ({⟨x, rfl⟩} : set (f ⁻¹' {f x})),
+    target := e.base_set,
+    open_source := e.open_base_set.prod (singletons_open_iff_discrete.2 (hf (f x)).1 ⟨x, rfl⟩),
+    open_target := e.open_base_set,
+    map_source' := λ p, and.left,
+    map_target' := λ p hp, ⟨hp, rfl⟩,
+    left_inv' := λ p hp, prod.ext rfl hp.2.symm,
+    right_inv' := λ p hp, rfl,
+    continuous_to_fun := continuous_fst.continuous_on,
+    continuous_inv_fun := (continuous_id'.prod_mk continuous_const).continuous_on },
+    ⟨e.mem_source.2 h, _, (hf (f x)).to_trivialization_apply⟩, λ p h, (e.proj_to_fun p h.1).symm⟩,
+  rwa [e.to_local_homeomorph.symm_symm, e.proj_to_fun],
+  rwa e.mem_source,
 end
 
-lemma open_map (q : E ↠ X) : is_open_map q :=
-q.is_locally_homeomorph.is_open_map
+lemma is_open_map (hf : is_covering_map f) : is_open_map f :=
+hf.is_locally_homeomorph.is_open_map
 
-lemma quotient_map_of_surjective (q : E ↠ X) (hq : function.surjective q) : quotient_map q :=
-q.open_map.to_quotient_map q.continuous hq
+lemma quotient_map (hf : is_covering_map f) (hf' : function.surjective f) : quotient_map f :=
+hf.is_open_map.to_quotient_map hf.continuous hf'
 
-end covering_map
+end is_covering_map
+
+lemma is_topological_fiber_bundle.is_covering_map {B F Z : Type*} [topological_space B]
+  [topological_space F] [topological_space Z] [discrete_topology F] {f : Z → B}
+  (hf : is_topological_fiber_bundle F f) : is_covering_map f :=
+is_covering_map.mk f (λ x, F) (λ x, classical.some (hf x)) (λ x, classical.some_spec (hf x))

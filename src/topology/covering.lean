bb77572521d5d5d98a3882d81457aa670fe1748c
Thomas Browning <tb65536@uw.edu>
2022-09-09 11:11:31 -0700

    year

--- src/topology/covering.lean
+++ src/topology/covering.lean
@@ -1,5 +1,5 @@
 /-
-Copyright (c) 2021 Thomas Browning. All rights reserved.
+Copyright (c) 2022 Thomas Browning. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Thomas Browning
 -/

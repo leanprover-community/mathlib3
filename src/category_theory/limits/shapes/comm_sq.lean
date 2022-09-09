43d1c58dd58f0c98d2ca1d99ce8e609cbbd185b9
Joël Riou <joel.riou@universite-paris-saclay.fr>
2022-09-09 17:35:23 +0200

    feat(category_theory/morphism_property): miscellaneous basic results

--- src/category_theory/limits/shapes/comm_sq.lean
+++ src/category_theory/limits/shapes/comm_sq.lean
@@ -468,6 +468,16 @@ is_pullback.of_is_limit (is_limit.of_iso_limit
   (limits.pushout_cocone.is_colimit_equiv_is_limit_unop h.flip.cocone h.flip.is_colimit)
   h.to_comm_sq.flip.cocone_unop)
 
+lemma of_horiz_is_iso [is_iso f] [is_iso inr] (sq : comm_sq f g inl inr) :
+  is_pushout f g inl inr := of_is_colimit' sq
+begin
+  refine pushout_cocone.is_colimit.mk _ (λ s, inv inr ≫ s.inr) (λ s, _) (by tidy) (by tidy),
+  simp only [← cancel_epi f, s.condition, sq.w_assoc, is_iso.hom_inv_id_assoc],
+end
+
+lemma of_vert_is_iso [is_iso g] [is_iso inl] (sq : comm_sq f g inl inr) :
+  is_pushout f g inl inr := (of_horiz_is_iso sq.flip).flip
+
 end is_pushout
 
 namespace functor

6a220659bdadf64c8ef3b1f23544ef2876b09067
Heather Macbeth <25316162+hrmacbeth@users.noreply.github.com>
2022-09-12 00:44:40 -0400

    fix name

--- src/analysis/inner_product_space/gram_schmidt_ortho.lean
+++ src/analysis/inner_product_space/gram_schmidt_ortho.lean
@@ -337,31 +337,31 @@ include h
 size of the index set is the dimension of `E`, produce an orthonormal basis for `E` which agrees
 with the orthonormal set produced by the Gram-Schmidt orthonormalization process on the elements of
 `ι` for which this process gives a nonzero number. -/
-noncomputable def gram_schmidt_orthonormal_basis' : orthonormal_basis ι 𝕜 E :=
+noncomputable def gram_schmidt_orthonormal_basis : orthonormal_basis ι 𝕜 E :=
 ((gram_schmidt_orthonormal' f).exists_orthonormal_basis_extension_of_card_eq h).some
 
-lemma gram_schmidt_orthonormal_basis'_apply {f : ι → E} {i : ι}
+lemma gram_schmidt_orthonormal_basis_apply {f : ι → E} {i : ι}
   (hi : gram_schmidt_normed 𝕜 f i ≠ 0) :
-  gram_schmidt_orthonormal_basis' h f i = gram_schmidt_normed 𝕜 f i :=
+  gram_schmidt_orthonormal_basis h f i = gram_schmidt_normed 𝕜 f i :=
 ((gram_schmidt_orthonormal' f).exists_orthonormal_basis_extension_of_card_eq h).some_spec i hi
 
-lemma gram_schmidt_orthonormal_basis'_apply_of_orthogonal {f : ι → E}
+lemma gram_schmidt_orthonormal_basis_apply_of_orthogonal {f : ι → E}
   (hf : pairwise (λ i j, ⟪f i, f j⟫ = 0)) {i : ι} (hi : f i ≠ 0) :
-  gram_schmidt_orthonormal_basis' h f i = (∥f i∥⁻¹ : 𝕜) • f i :=
+  gram_schmidt_orthonormal_basis h f i = (∥f i∥⁻¹ : 𝕜) • f i :=
 begin
   have H : gram_schmidt_normed 𝕜 f i = (∥f i∥⁻¹ : 𝕜) • f i,
   { rw [gram_schmidt_normed, gram_schmidt_of_orthogonal 𝕜 hf] },
-  rw [gram_schmidt_orthonormal_basis'_apply h, H],
+  rw [gram_schmidt_orthonormal_basis_apply h, H],
   simpa [H] using hi,
 end
 
-lemma inner_gram_schmidt_orthonormal_basis'_eq_zero {f : ι → E} {i : ι}
+lemma inner_gram_schmidt_orthonormal_basis_eq_zero {f : ι → E} {i : ι}
   (hi : gram_schmidt_normed 𝕜 f i = 0) (j : ι) :
-  ⟪gram_schmidt_orthonormal_basis' h f i, f j⟫ = 0 :=
+  ⟪gram_schmidt_orthonormal_basis h f i, f j⟫ = 0 :=
 begin
   apply inner_right_of_mem_orthogonal_singleton,
   suffices : span 𝕜 (gram_schmidt_normed 𝕜 f '' Iic j)
-    ≤ (𝕜 ∙ gram_schmidt_orthonormal_basis' h f i)ᗮ,
+    ≤ (𝕜 ∙ gram_schmidt_orthonormal_basis h f i)ᗮ,
   { apply this,
     rw span_gram_schmidt_normed,
     simpa using mem_span_gram_schmidt 𝕜 f (le_refl j) },
@@ -370,41 +370,41 @@ begin
   apply mem_orthogonal_singleton_of_inner_left,
   by_cases hk : gram_schmidt_normed 𝕜 f k = 0,
   { simp [hk] },
-  rw ← gram_schmidt_orthonormal_basis'_apply h hk,
+  rw ← gram_schmidt_orthonormal_basis_apply h hk,
   have : k ≠ i,
   { rintros rfl,
     exact hk hi },
-  exact (gram_schmidt_orthonormal_basis' h f).orthonormal.2 this,
+  exact (gram_schmidt_orthonormal_basis h f).orthonormal.2 this,
 end
 
-lemma gram_schmidt_orthonormal_basis'_inv_triangular {i j : ι} (hij : i < j) :
-  ⟪gram_schmidt_orthonormal_basis' h f j, f i⟫ = 0 :=
+lemma gram_schmidt_orthonormal_basis_inv_triangular {i j : ι} (hij : i < j) :
+  ⟪gram_schmidt_orthonormal_basis h f j, f i⟫ = 0 :=
 begin
   by_cases hi : gram_schmidt_normed 𝕜 f j = 0,
-  { rw inner_gram_schmidt_orthonormal_basis'_eq_zero h hi },
-  { simp [gram_schmidt_orthonormal_basis'_apply h hi, gram_schmidt_normed, inner_smul_left,
+  { rw inner_gram_schmidt_orthonormal_basis_eq_zero h hi },
+  { simp [gram_schmidt_orthonormal_basis_apply h hi, gram_schmidt_normed, inner_smul_left,
       gram_schmidt_inv_triangular 𝕜 f hij] }
 end
 
-lemma gram_schmidt_orthonormal_basis'_inv_triangular' {i j : ι} (hij : i < j) :
-  (gram_schmidt_orthonormal_basis' h f).repr (f i) j = 0 :=
+lemma gram_schmidt_orthonormal_basis_inv_triangular' {i j : ι} (hij : i < j) :
+  (gram_schmidt_orthonormal_basis h f).repr (f i) j = 0 :=
 by simpa [orthonormal_basis.repr_apply_apply]
-  using gram_schmidt_orthonormal_basis'_inv_triangular h f hij
+  using gram_schmidt_orthonormal_basis_inv_triangular h f hij
 
 /-- Given an indexed family `f : ι → E` of vectors in an inner product space `E`, for which the
 size of the index set is the dimension of `E`, the matrix of coefficients of `f` with respect to the
-orthonormal basis `gram_schmidt_orthonormal_basis'` constructed from `f` is upper-triangular. -/
-lemma gram_schmidt_orthonormal_basis'_inv_block_triangular :
-  ((gram_schmidt_orthonormal_basis' h f).to_basis.to_matrix f).block_triangular id :=
-λ i j, gram_schmidt_orthonormal_basis'_inv_triangular' h f
+orthonormal basis `gram_schmidt_orthonormal_basis` constructed from `f` is upper-triangular. -/
+lemma gram_schmidt_orthonormal_basis_inv_block_triangular :
+  ((gram_schmidt_orthonormal_basis h f).to_basis.to_matrix f).block_triangular id :=
+λ i j, gram_schmidt_orthonormal_basis_inv_triangular' h f
 
-lemma gram_schmidt_orthonormal_basis'_det :
-  (gram_schmidt_orthonormal_basis' h f).to_basis.det f
-  = ∏ i, ⟪gram_schmidt_orthonormal_basis' h f i, f i⟫ :=
+lemma gram_schmidt_orthonormal_basis_det :
+  (gram_schmidt_orthonormal_basis h f).to_basis.det f
+  = ∏ i, ⟪gram_schmidt_orthonormal_basis h f i, f i⟫ :=
 begin
-  convert matrix.det_of_upper_triangular (gram_schmidt_orthonormal_basis'_inv_block_triangular h f),
+  convert matrix.det_of_upper_triangular (gram_schmidt_orthonormal_basis_inv_block_triangular h f),
   ext i,
-  exact ((gram_schmidt_orthonormal_basis' h f).repr_apply_apply (f i) i).symm,
+  exact ((gram_schmidt_orthonormal_basis h f).repr_apply_apply (f i) i).symm,
 end
 
 end orthonormal_basis

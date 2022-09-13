e6e270ba76e8aca07449412aa60f5333136412e3
Eric Wieser <wieser.eric@gmail.com>
2022-09-13 09:34:08 +0100

    remove more noncomputable

--- src/analysis/normed/group/seminorm.lean
+++ src/analysis/normed/group/seminorm.lean
@@ -206,7 +206,7 @@ variables (p q) (f : F →* E)
 
 -- TODO: define `has_Sup` too, from the skeleton at
 -- https://github.com/leanprover-community/mathlib/pull/11329#issuecomment-1008915345
-@[to_additive] noncomputable instance : has_sup (group_seminorm E) :=
+@[to_additive] instance : has_sup (group_seminorm E) :=
 ⟨λ p q,
   { to_fun := p ⊔ q,
     map_one' :=
@@ -219,7 +219,7 @@ variables (p q) (f : F →* E)
 @[simp, to_additive] lemma coe_sup : ⇑(p ⊔ q) = p ⊔ q := rfl
 @[simp, to_additive] lemma sup_apply (x : E) : (p ⊔ q) x = p x ⊔ q x := rfl
 
-@[to_additive] noncomputable instance : semilattice_sup (group_seminorm E) :=
+@[to_additive] instance : semilattice_sup (group_seminorm E) :=
 fun_like.coe_injective.semilattice_sup _ coe_sup
 
 /-- Composition of a group seminorm with a monoid homomorphism as a group seminorm. -/
@@ -430,7 +430,7 @@ variables (p q) (f : F →* E)
 @[simp, to_additive] lemma add_apply (x : E) : (p + q) x = p x + q x := rfl
 
 -- TODO: define `has_Sup`
-@[to_additive] noncomputable instance : has_sup (group_norm E) :=
+@[to_additive] instance : has_sup (group_norm E) :=
 ⟨λ p q,
   { eq_one_of_map_eq_zero' := λ x hx, of_not_not $ λ h, hx.not_gt $
       lt_sup_iff.2 $ or.inl $ map_pos_of_ne_one p h,
@@ -439,7 +439,7 @@ variables (p q) (f : F →* E)
 @[simp, to_additive] lemma coe_sup : ⇑(p ⊔ q) = p ⊔ q := rfl
 @[simp, to_additive] lemma sup_apply (x : E) : (p ⊔ q) x = p x ⊔ q x := rfl
 
-@[to_additive] noncomputable instance : semilattice_sup (group_norm E) :=
+@[to_additive] instance : semilattice_sup (group_norm E) :=
 fun_like.coe_injective.semilattice_sup _ coe_sup
 
 end group

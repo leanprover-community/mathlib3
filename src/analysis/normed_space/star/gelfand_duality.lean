ffa25da01d7adeead6774750f085b6b1d50eddc5
Jireh Loreaux <loreaujy@gmail.com>
2022-09-13 10:56:57 -0500

    further cleaning

--- src/analysis/normed_space/star/gelfand_duality.lean
+++ src/analysis/normed_space/star/gelfand_duality.lean
@@ -6,47 +6,46 @@ import topology.continuous_function.compact
 import topology.algebra.algebra
 import topology.continuous_function.stone_weierstrass
 
-section general
 open weak_dual
+open_locale nnreal
+
+section complex_banach_algebra
+open ideal
 
 variables {A : Type*} [normed_comm_ring A] [normed_algebra ℂ A] [complete_space A]
-  [norm_one_class A] (I : ideal A) [hI : I.is_maximal]
-
-include hI
+  [norm_one_class A] (I : ideal A) [ideal.is_maximal I]
 
 /-- Every maximal ideal in a commutative complex Banach algebra gives rise to a character on that
 algebra. In particular, the character, which may be identified as an algebra homomorphism due to
 `weak_dual.character_space.equiv_alg_hom`, is given by the composition of the quotient map and
 the Gelfand-Mazur isomorphism `normed_ring.alg_equiv_complex_of_complete`. -/
-noncomputable def ideal.is_maximal.character_space : character_space ℂ A :=
-weak_dual.character_space.equiv_alg_hom.symm $
-  ((@normed_ring.alg_equiv_complex_of_complete (A ⧸ I) _ _
-  (by { letI := ideal.quotient.field I, exact @is_unit_iff_ne_zero (A ⧸ I) _ }) _).symm :
+@[protected] noncomputable def ideal.character_space : character_space ℂ A :=
+character_space.equiv_alg_hom.symm $ ((@normed_ring.alg_equiv_complex_of_complete (A ⧸ I) _ _
+  (by { letI := quotient.field I, exact @is_unit_iff_ne_zero (A ⧸ I) _ }) _).symm :
   A ⧸ I →ₐ[ℂ] ℂ).comp
-  (ideal.quotient.mkₐ ℂ I)
+  (quotient.mkₐ ℂ I)
 
-lemma ideal.is_maximal.character_space_apply_zero_of_mem (a : A) (ha : a ∈ I) :
-  (ideal.is_maximal.character_space I) a = 0 :=
+lemma ideal.character_space_apply_zero_of_mem {a : A} (ha : a ∈ I) :
+  I.character_space a = 0 :=
 begin
-  unfold ideal.is_maximal.character_space,
-  simpa only [weak_dual.character_space.equiv_alg_hom_symm_coe, alg_hom.coe_comp,
-    alg_equiv.coe_alg_hom, ideal.quotient.mkₐ_eq_mk, function.comp_app,
-    ideal.quotient.eq_zero_iff_mem.mpr ha, spectrum.zero_eq,
-    normed_ring.alg_equiv_complex_of_complete_symm_apply]
-    using set.eq_of_mem_singleton (set.nonempty.some_mem (set.singleton_nonempty (0 : ℂ))),
+  unfold ideal.character_space,
+  simpa only [character_space.equiv_alg_hom_symm_coe, alg_hom.coe_comp,
+    alg_equiv.coe_alg_hom, quotient.mkₐ_eq_mk, function.comp_app, quotient.eq_zero_iff_mem.mpr ha,
+    spectrum.zero_eq, normed_ring.alg_equiv_complex_of_complete_symm_apply]
+    using set.eq_of_mem_singleton (set.singleton_nonempty (0 : ℂ)).some_mem,
 end
 
-omit hI
-
+/-- If `a : A` is not a unit, then some character takes the value zero at `a`. This is equivlaent
+to `gelfand_transform ℂ A a` takes the value zero at some character. -/
 lemma weak_dual.character_space.exists_apply_eq_zero {a : A} (ha : ¬ is_unit a) :
   ∃ f : character_space ℂ A, f a = 0 :=
 begin
-  obtain ⟨M, hM, haM⟩ := ideal.exists_le_maximal (ideal.span {a}) (ideal.span_singleton_ne_top ha),
-  haveI := hM,
-  exact ⟨ideal.is_maximal.character_space M, ideal.is_maximal.character_space_apply_zero_of_mem _ _
-    (haM (ideal.mem_span_singleton.mpr ⟨1, (mul_one a).symm⟩))⟩,
+  unfreezingI { obtain ⟨M, hM, haM⟩ := (span {a}).exists_le_maximal (span_singleton_ne_top ha) },
+  exact ⟨M.character_space, M.character_space_apply_zero_of_mem
+    (haM (mem_span_singleton.mpr ⟨1, (mul_one a).symm⟩))⟩,
 end
 
+/-- The Gelfand transform is spectrum-preserving. -/
 lemma spectrum.gelfand_transform_eq (a : A) : spectrum ℂ (gelfand_transform ℂ A a) = spectrum ℂ a :=
 begin
   refine set.subset.antisymm (alg_hom.spectrum_apply_subset (gelfand_transform ℂ A) a) (λ z hz, _),
@@ -56,21 +55,22 @@ begin
   exact (continuous_map.spectrum_eq_range (gelfand_transform ℂ A a)).symm ▸ ⟨f, hf.symm⟩,
 end
 
-instance [nontrivial A] : nonempty (character_space ℂ A) :=
-⟨classical.some $ weak_dual.character_space.exists_apply_eq_zero (zero_mem_nonunits.mpr zero_ne_one)⟩
+instance : nonempty (character_space ℂ A) :=
+begin
+  haveI := norm_one_class.nontrivial A,
+  exact ⟨classical.some $
+    weak_dual.character_space.exists_apply_eq_zero (zero_mem_nonunits.mpr zero_ne_one)⟩,
+end
 
-end general
+end complex_banach_algebra
 
-section cstar_ring
+section complex_cstar_algebra
 
-open weak_dual
 variables (A : Type*) [normed_comm_ring A] [normed_algebra ℂ A] [complete_space A]
 variables [star_ring A] [cstar_ring A] [star_module ℂ A] [nontrivial A]
 
 lemma coe_gelfand_star_transform : ⇑(gelfand_star_transform A) = gelfand_transform ℂ A :=
 funext $ λ a, continuous_map.ext $ λ φ, rfl
-
-open_locale nnreal
 
 /-- The Gelfand transform is an isometry when the algebra is a C⋆-algebra over `ℂ`. -/
 lemma gelfand_star_transform_isometry : isometry (gelfand_star_transform A) :=
@@ -89,7 +89,7 @@ begin
 end
 
 /-- The Gelfand transform is surjective when the algebra is a C⋆-algebra over `ℂ`. -/
-lemma gelfand_transform_surjective : function.surjective (gelfand_transform ℂ A) :=
+lemma gelfand_star_transform_surjective : function.surjective (gelfand_star_transform A) :=
 begin
   suffices : (gelfand_transform ℂ A).range = ⊤,
   { exact λ x, this.symm ▸ (gelfand_transform ℂ A).mem_range.mp (this.symm ▸ algebra.mem_top) },
@@ -108,7 +108,7 @@ begin
     exact λ h, subtype.ext (continuous_linear_map.ext $
       λ a, h (gelfand_transform ℂ A a) ⟨gelfand_transform ℂ A a, ⟨a, rfl⟩, rfl⟩), },
   /- If `f = gelfand_transform ℂ A a`, then `star f` is also in the range of `gelfand_transform ℂ A`
-  via the argument `star a`. The key lemma below may be hard to spot; it's `map_star` coming from
+  using the argument `star a`. The key lemma below may be hard to spot; it's `map_star` coming from
   `weak_dual.star_hom_class`, which is a nontrivial result. -/
   { obtain ⟨f, ⟨a, rfl⟩, rfl⟩ := subalgebra.mem_map.mp hf,
     refine ⟨star a, continuous_map.ext $ λ ψ, _⟩,
@@ -120,4 +120,4 @@ begin
       alg_hom.to_ring_hom_eq_coe, alg_hom.comp_left_continuous_apply] },
 end
 
-end cstar_ring
+end complex_cstar_algebra

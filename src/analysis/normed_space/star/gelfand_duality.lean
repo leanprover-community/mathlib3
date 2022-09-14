258d58bdf3b13fa98a613405402186d0c372ca3c
Jireh Loreaux <loreaujy@gmail.com>
2022-09-14 09:25:04 -0500

    remove references to `gelfand_star_transform` to save the name

--- src/analysis/normed_space/star/gelfand_duality.lean
+++ src/analysis/normed_space/star/gelfand_duality.lean
@@ -70,23 +70,25 @@ variables (A : Type*) [normed_comm_ring A] [normed_algebra ℂ A] [complete_spac
 variables [star_ring A] [cstar_ring A] [star_module ℂ A] [nontrivial A]
 
 /-- The Gelfand transform is an isometry when the algebra is a C⋆-algebra over `ℂ`. -/
-lemma gelfand_star_transform_isometry : isometry (gelfand_star_transform A) :=
+lemma gelfand_transform_isometry : isometry (gelfand_transform ℂ A) :=
 begin
-  refine add_monoid_hom_class.isometry_of_norm (gelfand_star_transform A) (λ a, _),
+  refine add_monoid_hom_class.isometry_of_norm (gelfand_transform ℂ A) (λ a, _),
+  have gt_map_star : gelfand_transform ℂ A (star a) = star (gelfand_transform ℂ A a),
+    from continuous_map.ext (λ φ, map_star φ a),
   /- by `spectrum.gelfand_transform_eq`, the spectra of `star a * a` and its
-  `gelfand_star_transform` coincide. Therefore, so do their spectral radii, and since they are
+  `gelfand_transform` coincide. Therefore, so do their spectral radii, and since they are
   self-adjoint, so also do their norms. Applying the C⋆-property of the norm and taking square
   roots shows that the norm is preserved. -/
-  have : spectral_radius ℂ (gelfand_star_transform A (star a * a)) = spectral_radius ℂ (star a * a),
-  { unfold spectral_radius, rw [coe_gelfand_star_transform, spectrum.gelfand_transform_eq], },
-  simp only [map_mul, map_star, (is_self_adjoint.star_mul_self _).spectral_radius_eq_nnnorm,
+  have : spectral_radius ℂ (gelfand_transform ℂ A (star a * a)) = spectral_radius ℂ (star a * a),
+  { unfold spectral_radius, rw spectrum.gelfand_transform_eq, },
+  simp only [map_mul, gt_map_star, (is_self_adjoint.star_mul_self _).spectral_radius_eq_nnnorm,
     ennreal.coe_eq_coe, cstar_ring.nnnorm_star_mul_self, ←sq] at this,
   simpa only [function.comp_app, nnreal.sqrt_sq]
     using congr_arg ((coe : ℝ≥0 → ℝ) ∘ ⇑nnreal.sqrt) this,
 end
 
 /-- The Gelfand transform is surjective when the algebra is a C⋆-algebra over `ℂ`. -/
-lemma gelfand_star_transform_surjective : function.surjective (gelfand_star_transform A) :=
+lemma gelfand_transform_surjective : function.surjective (gelfand_transform ℂ A) :=
 begin
   suffices : (gelfand_transform ℂ A).range = ⊤,
   { exact λ x, this.symm ▸ (gelfand_transform ℂ A).mem_range.mp (this.symm ▸ algebra.mem_top) },
@@ -95,7 +97,7 @@ begin
   points in `C(character_space ℂ A, ℂ)` and is closed under `star`. -/
   have h : (gelfand_transform ℂ A).range.topological_closure = (gelfand_transform ℂ A).range,
   from le_antisymm (subalgebra.topological_closure_minimal _ le_rfl
-    (gelfand_star_transform_isometry A).closed_embedding.closed_range)
+    (gelfand_transform_isometry A).closed_embedding.closed_range)
     (subalgebra.subalgebra_topological_closure _),
   refine h ▸ continuous_map.subalgebra_is_R_or_C_topological_closure_eq_top_of_separates_points
     _ (λ _ _, _) (λ f hf, _),

156aa4a99d201bddb9490e8b5238c3ca326c64fa
Jireh Loreaux <loreaujy@gmail.com>
2022-09-15 13:57:37 -0500

    fix algebra/monoid_algebra/basic

--- src/algebra/monoid_algebra/basic.lean
+++ src/algebra/monoid_algebra/basic.lean
@@ -1548,12 +1548,15 @@ variables [comm_semiring R] (k G)
 `multiplicative`. -/
 def add_monoid_algebra.to_multiplicative_alg_equiv [semiring k] [algebra R k] [add_monoid G] :
   add_monoid_algebra k G ≃ₐ[R] monoid_algebra k (multiplicative G) :=
-{ commutes' := λ r, by simp [add_monoid_algebra.to_multiplicative],
+{ map_smul' := alg_equiv.map_smul_of_map_mul_of_commutes
+    (map_mul $ add_monoid_algebra.to_multiplicative k G)
+    (λ r, by simp [add_monoid_algebra.to_multiplicative]),
   ..add_monoid_algebra.to_multiplicative k G }
 
 /-- The algebra equivalence between `monoid_algebra` and `add_monoid_algebra` in terms of
 `additive`. -/
 def monoid_algebra.to_additive_alg_equiv [semiring k] [algebra R k] [monoid G] :
   monoid_algebra k G ≃ₐ[R] add_monoid_algebra k (additive G) :=
-{ commutes' := λ r, by simp [monoid_algebra.to_additive],
+{ map_smul' := alg_equiv.map_smul_of_map_mul_of_commutes (map_mul $ monoid_algebra.to_additive k G)
+    (λ r, by simp [monoid_algebra.to_additive]),
   ..monoid_algebra.to_additive k G }

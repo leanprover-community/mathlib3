73e5682b218e82299c9b2a484257e418a61a196c
Michael Stoll <Michael.Stoll@uni-bayreuth.de>
2022-09-15 17:19:47 +0200

    move examples to test/norm_num_ext.lean

--- src/number_theory/legendre_symbol/norm_num.lean
+++ src/number_theory/legendre_symbol/norm_num.lean
@@ -438,23 +438,3 @@ end norm_num
 end tactic
 
 end tactic
-
-section examples
-
-open_locale number_theory_symbols
-
-example : J(123 | 335) = -1 := by norm_num
-example : J(-2345 | 6789) = -1 := by norm_num
-example : J(-1 | 1655801) = 1 := by norm_num
-example : J(-102334155 | 165580141) = -1 := by norm_num
-
-example : J(58378362899022564339483801989973056405585914719065 |
-            53974350278769849773003214636618718468638750007307) = -1 := by norm_num
-
-example : J(3 + 4 | 3 * 5) = -1 := by norm_num
-example : J(J(-1 | 7) | 11) = -1 := by norm_num
-
-instance prime_1000003 : fact (nat.prime 1000003) := ⟨by norm_num⟩
-example : legendre_sym 1000003 7 = -1 := by norm_num
-
-end examples

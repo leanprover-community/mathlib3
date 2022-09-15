c134b90716a48960519f2710bcfe16e5022c5be8
mcdoll <moritz.doll@googlemail.com>
2022-09-15 20:29:50 +0200

    Update src/measure_theory/integral/integrable_on.lean
    Co-authored-by: Rémy Degenne <remydegenne@gmail.com>

--- src/measure_theory/integral/integrable_on.lean
+++ src/measure_theory/integral/integrable_on.lean
@@ -255,11 +255,15 @@ begin
   exact le_abs_self (f x),
 end
 
-lemma integrable_on.lintegral_lt_top_ae' {f : α → ℝ} {s : set α} (hf : integrable_on f s μ) :
-  ∫⁻ x in s, ennreal.of_real (f x) ∂μ < ∞ :=
-calc ∫⁻ x in s, ennreal.of_real (f x) ∂μ
-    ≤ ∫⁻ x in s, ↑∥f x∥₊ ∂μ : lintegral_to_real_le_lintegral_nnnorm f
+lemma integrable.lintegral_lt_top {f : α → ℝ} (hf : integrable f μ) :
+  ∫⁻ x, ennreal.of_real (f x) ∂μ < ∞ :=
+calc ∫⁻ x, ennreal.of_real (f x) ∂μ
+    ≤ ∫⁻ x, ↑∥f x∥₊ ∂μ : lintegral_to_real_le_lintegral_nnnorm f
 ... < ∞ : hf.2
+
+lemma integrable_on.set_lintegral_lt_top {f : α → ℝ} {s : set α} (hf : integrable_on f s μ) :
+  ∫⁻ x in s, ennreal.of_real (f x) ∂μ < ∞ :=
+integrable.lintegral_lt_top hf
 
 /-- We say that a function `f` is *integrable at filter* `l` if it is integrable on some
 set `s ∈ l`. Equivalently, it is eventually integrable on `s` in `l.small_sets`. -/

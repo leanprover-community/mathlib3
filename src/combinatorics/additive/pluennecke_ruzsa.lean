4c220d2b283fa9115636c3f2f69cc485d13ee399
Yaël Dillies <yael.dillies@gmail.com>
2022-09-16 18:31:10 +0000

    use nnrat

--- src/combinatorics/additive/pluennecke_ruzsa.lean
+++ src/combinatorics/additive/pluennecke_ruzsa.lean
@@ -5,7 +5,7 @@ Authors: Yaël Dillies, George Shakan
 -/
 import combinatorics.double_counting
 import data.finset.pointwise
-import combinatorics.additive.positivity
+import data.rat.nnrat
 
 /-!
 # The Plünnecke-Ruzsa inequality
@@ -27,7 +27,7 @@ inequality.
 -/
 
 open nat
-open_locale pointwise
+open_locale nnrat pointwise
 
 namespace finset
 variables {α : Type*} [comm_group α] [decidable_eq α] {A B C : finset α}
@@ -109,14 +109,14 @@ end
 -- Auxiliary lemma for Ruzsa's triangle sum inequality, and the Plünnecke-Ruzsa inequality.
 @[to_additive]
 private lemma mul_aux (hA : A.nonempty) (hAB : A ⊆ B)
-  (h : ∀ A' ∈ B.powerset.erase ∅, ((A * C).card : ℚ) / ↑(A.card) ≤ ((A' * C).card) / ↑(A'.card)) :
+  (h : ∀ A' ∈ B.powerset.erase ∅, ((A * C).card : ℚ≥0) / ↑(A.card) ≤ ((A' * C).card) / ↑(A'.card)) :
   ∀ A' ⊆ A, (A * C).card * A'.card ≤ (A' * C).card * A.card :=
 begin
   rintro A' hAA',
   obtain rfl | hA' := A'.eq_empty_or_nonempty,
   { simp },
-  have hA₀ : (0 : ℚ) < A.card := cast_pos.2 hA.card_pos,
-  have hA₀' : (0 : ℚ) < A'.card := cast_pos.2 hA'.card_pos,
+  have hA₀ : (0 : ℚ≥0) < A.card := cast_pos.2 hA.card_pos,
+  have hA₀' : (0 : ℚ≥0) < A'.card := cast_pos.2 hA'.card_pos,
   exact_mod_cast (div_le_div_iff hA₀ hA₀').1 (h _ $ mem_erase_of_ne_of_mem hA'.ne_empty $
     mem_powerset.2 $ hAA'.trans hAB),
 end
@@ -130,16 +130,16 @@ begin
   obtain rfl | hB := B.eq_empty_or_nonempty,
   { simp },
   have hB' : B ∈ B.powerset.erase ∅ := mem_erase_of_ne_of_mem hB.ne_empty (mem_powerset_self _),
-  obtain ⟨U, hU, hUA⟩ := exists_min_image (B.powerset.erase ∅) (λ U, (U * A).card/U.card : _ → ℚ)
+  obtain ⟨U, hU, hUA⟩ := exists_min_image (B.powerset.erase ∅) (λ U, (U * A).card/U.card : _ → ℚ≥0)
     ⟨B, hB'⟩,
   rw [mem_erase, mem_powerset, ←nonempty_iff_ne_empty] at hU,
-  refine cast_le.1 (_ : (_ : ℚ) ≤ _),
+  refine cast_le.1 (_ : (_ : ℚ≥0) ≤ _),
   push_cast,
   refine (le_div_iff $ by exact cast_pos.2 hB.card_pos).1 _,
   rw [mul_div_right_comm, mul_comm _ B],
   refine (cast_le.2 $ card_le_card_mul_left _ hU.1).trans _,
   refine le_trans _ (mul_le_mul (hUA _ hB') (cast_le.2 $ card_le_of_subset $
-    mul_subset_mul_right hU.2) (cast_nonneg _) $ by positivity),
+    mul_subset_mul_right hU.2) (zero_le _) $ zero_le _),
   rw [←mul_div_right_comm, ←mul_assoc],
   refine (le_div_iff $ by exact cast_pos.2 hU.1.card_pos).2 _,
   exact_mod_cast mul_pluennecke_petridis C (mul_aux hU.1 hU.2 hUA),
@@ -166,7 +166,7 @@ by { rw [←div_inv_eq_mul, div_eq_mul_inv], exact card_mul_mul_le_card_div_mul_
 
 lemma card_add_nsmul_le {α : Type*} [add_comm_group α] [decidable_eq α] {A B : finset α}
   (hAB : ∀ A' ⊆ A, (A + B).card * A'.card ≤ (A' + B).card * A.card) (n : ℕ) :
-  ((A + n • B).card : ℚ) ≤ ((A + B).card / A.card) ^ n * A.card :=
+  ((A + n • B).card : ℚ≥0) ≤ ((A + B).card / A.card) ^ n * A.card :=
 begin
   obtain rfl | hA := A.eq_empty_or_nonempty,
   { simp },
@@ -176,22 +176,22 @@ begin
   swap, exact (cast_pos.2 hA.card_pos),
   refine (cast_le.2 $ add_pluennecke_petridis _ hAB).trans _,
   rw cast_mul,
-  exact mul_le_mul_of_nonneg_left ih (cast_nonneg _),
+  exact mul_le_mul_of_nonneg_left ih (zero_le _),
 end
 
 @[to_additive]
 lemma card_mul_pow_le (hAB : ∀ A' ⊆ A, (A * B).card * A'.card ≤ (A' * B).card * A.card) (n : ℕ) :
-  ((A * B ^ n).card : ℚ) ≤ ((A * B).card / A.card) ^ n * A.card :=
+  ((A * B ^ n).card : ℚ≥0) ≤ ((A * B).card / A.card) ^ n * A.card :=
 begin
   obtain rfl | hA := A.eq_empty_or_nonempty,
   { simp },
   induction n with n ih,
   { simp },
-  rw [pow_succ, ←mul_assoc, pow_succ, @mul_assoc ℚ, ←mul_div_right_comm, le_div_iff, ←cast_mul],
+  rw [pow_succ, ←mul_assoc, pow_succ, @mul_assoc ℚ≥0, ←mul_div_right_comm, le_div_iff, ←cast_mul],
   swap, exact (cast_pos.2 hA.card_pos),
   refine (cast_le.2 $ mul_pluennecke_petridis _ hAB).trans _,
   rw cast_mul,
-  exact mul_le_mul_of_nonneg_left ih (cast_nonneg _),
+  exact mul_le_mul_of_nonneg_left ih (zero_le _),
 end
 
 /-- The **Plünnecke-Ruzsa inequality**. Multiplication version. Note that this is genuinely harder
@@ -199,10 +199,10 @@ than the division version because we cannot use a double counting argument. -/
 @[to_additive "The **Plünnecke-Ruzsa inequality**. Addition version. Note that this is genuinely
 harder than the subtraction version because we cannot use a double counting argument."]
 lemma card_pow_div_pow_le (hA : A.nonempty) (B : finset α) (m n : ℕ) :
-  ((B ^ m / B ^ n).card : ℚ) ≤ ((A * B).card / A.card) ^ (m + n) * A.card :=
+  ((B ^ m / B ^ n).card : ℚ≥0) ≤ ((A * B).card / A.card) ^ (m + n) * A.card :=
 begin
   have hA' : A ∈ A.powerset.erase ∅ := mem_erase_of_ne_of_mem hA.ne_empty (mem_powerset_self _),
-  obtain ⟨C, hC, hCA⟩ := exists_min_image (A.powerset.erase ∅) (λ C, (C * B).card/C.card : _ → ℚ)
+  obtain ⟨C, hC, hCA⟩ := exists_min_image (A.powerset.erase ∅) (λ C, (C * B).card/C.card : _ → ℚ≥0)
     ⟨A, hA'⟩,
   rw [mem_erase, mem_powerset, ←nonempty_iff_ne_empty] at hC,
   refine (mul_le_mul_right $ cast_pos.2 hC.1.card_pos).1 _,
@@ -211,16 +211,16 @@ begin
   push_cast,
   rw mul_comm _ C,
   refine (mul_le_mul (card_mul_pow_le (mul_aux hC.1 hC.2 hCA) _)
-    (card_mul_pow_le (mul_aux hC.1 hC.2 hCA) _) (cast_nonneg _) $ by positivity).trans _,
+    (card_mul_pow_le (mul_aux hC.1 hC.2 hCA) _) (zero_le _) $ zero_le _).trans _,
   rw [mul_mul_mul_comm, ←pow_add, ←mul_assoc],
-  exact mul_le_mul_of_nonneg_right (mul_le_mul (pow_le_pow_of_le_left (by positivity) (hCA _ hA') _)
-    (cast_le.2 $ card_le_of_subset hC.2) (by positivity) $ by positivity) (cast_nonneg _),
+  exact mul_le_mul_of_nonneg_right (mul_le_mul (pow_le_pow_of_le_left (zero_le _) (hCA _ hA') _)
+    (cast_le.2 $ card_le_of_subset hC.2) (zero_le _) $ zero_le _) (zero_le _),
 end
 
 /-- The **Plünnecke-Ruzsa inequality**. Subtraction version. -/
 @[to_additive "The **Plünnecke-Ruzsa inequality**. Subtraction version."]
 lemma card_pow_div_pow_le' (hA : A.nonempty) (B : finset α) (m n : ℕ) :
-  ((B ^ m / B ^ n).card : ℚ) ≤ ((A / B).card / A.card) ^ (m + n) * A.card :=
+  ((B ^ m / B ^ n).card : ℚ≥0) ≤ ((A / B).card / A.card) ^ (m + n) * A.card :=
 begin
   rw [←card_inv, inv_div', ←inv_pow, ←inv_pow, div_eq_mul_inv A],
   exact card_pow_div_pow_le hA _ _ _,
@@ -229,13 +229,13 @@ end
 /-- Special case of the **Plünnecke-Ruzsa inequality**. Multiplication version. -/
 @[to_additive "Special case of the **Plünnecke-Ruzsa inequality**. Addition version."]
 lemma card_pow_le (hA : A.nonempty) (B : finset α) (n : ℕ) :
-  ((B ^ n).card : ℚ) ≤ ((A * B).card / A.card) ^ n * A.card :=
+  ((B ^ n).card : ℚ≥0) ≤ ((A * B).card / A.card) ^ n * A.card :=
 by simpa only [pow_zero, div_one] using card_pow_div_pow_le hA _ _ 0
 
 /-- Special case of the **Plünnecke-Ruzsa inequality**. Division version. -/
 @[to_additive "Special case of the **Plünnecke-Ruzsa inequality**. Subtraction version."]
 lemma card_pow_le' (hA : A.nonempty) (B : finset α) (n : ℕ) :
-  ((B ^ n).card : ℚ) ≤ ((A / B).card / A.card) ^ n * A.card :=
+  ((B ^ n).card : ℚ≥0) ≤ ((A / B).card / A.card) ^ n * A.card :=
 by simpa only [pow_zero, div_one] using card_pow_div_pow_le' hA _ _ 0
 
 end finset

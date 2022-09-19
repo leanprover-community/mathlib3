aea20d57400516b550c48c1fee01bbc2f8d363f0
Mark Lavrentyev <lavrema@outlook.com>
2022-09-19 16:55:01 -0400

    Add outline for adding topological retractions

---
+++ src/algebraic_topology/fundamental_groupoid/retraction.lean
@@ -0,0 +1,39 @@
+/-
+Copyright (c) 2022 Praneeth Kolichala. All rights reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
+Authors: Praneeth Kolichala
+-/
+import topology.homotopy.equiv
+import category_theory.equivalence
+import algebraic_topology.fundamental_groupoid.product
+
+/-!
+# Homotopic maps induce naturally isomorphic functors
+
+## Main definitions
+
+  - `retraction X A` A retraction from topological space X to a subspace A is a continuous
+    map `f : C(X, A)` such that the restriction of f to A is the identity
+
+  - `split_mono_of_retraction` The induced map on `fundamental_groupoid X` of the inclusion
+    `i : A → X` and the retraction form a `split_mono i`.
+-/
+
+noncomputable theory
+
+universe u
+
+open fundamental_groupoid
+open category_theory
+open fundamental_groupoid_functor
+
+open_locale fundamental_groupoid
+open_locale unit_interval
+
+section retraction
+
+-- We automatically get the subspace topology for A via `subtype.topological_space`
+variables {X : Type*} {p : X → Prop} {A : subtype p}
+variables [topological_space X]
+
+end retraction

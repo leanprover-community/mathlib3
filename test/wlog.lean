cb562911b6a14ffe98ba1cc3f65395b48bcb9a31
Johan Commelin <johan@commelin.net>
2022-09-20 13:40:31 +0000

    simplify tests

--- test/wlog.lean
+++ test/wlog.lean
@@ -9,41 +9,38 @@ section wlog
 
 example {x y : ℕ} : true :=
 begin
-  suffices : false, trivial,
   wlog h : x ≤ y,
   { guard_hyp h : ¬x ≤ y,
-    guard_hyp this : ∀ {x y : ℕ}, x ≤ y → false, -- `wlog` generalizes by default
-    guard_target false,
-    admit },
+    guard_hyp this : ∀ {x y : ℕ}, x ≤ y → true, -- `wlog` generalizes by default
+    guard_target true,
+    trivial },
   { guard_hyp h : x ≤ y,
-    guard_target false,
-    admit },
+    guard_target true,
+    trivial },
 end
 
 example {x y : ℕ} : true :=
 begin
-  suffices : false, trivial,
   wlog h : x ≤ y generalizing x,
   { guard_hyp h : ¬x ≤ y,
-    guard_hyp this : ∀ {x : ℕ}, x ≤ y → false, -- only `x` was generalized
-    guard_target false,
-    admit },
+    guard_hyp this : ∀ {x : ℕ}, x ≤ y → true, -- only `x` was generalized
+    guard_target true,
+    trivial },
   { guard_hyp h : x ≤ y,
-    guard_target false,
-    admit },
+    guard_target true,
+    trivial },
 end
 
 example {x y z : ℕ} : true :=
 begin
-  suffices : false, trivial,
   wlog h : x ≤ y + z using H,
   { guard_hyp h : ¬ x ≤ y + z,
-    guard_hyp H : ∀ {x y z : ℕ}, x ≤ y + z → false, -- wlog-claim is named `H` instead of `this`
-    guard_target false,
-    admit },
+    guard_hyp H : ∀ {x y z : ℕ}, x ≤ y + z → true, -- wlog-claim is named `H` instead of `this`
+    guard_target true,
+    trivial },
   { guard_hyp h : x ≤ y + z,
-    guard_target false,
-    admit },
+    guard_target true,
+    trivial },
 end
 
 end wlog

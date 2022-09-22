7fe139a4e25ffaa16b15f13a82d9cc4fb351ed1d
Yaël Dillies <yael.dillies@gmail.com>
2022-09-22 16:56:36 +0000

    fix merge

--- src/data/finset/basic.lean
+++ src/data/finset/basic.lean
@@ -585,7 +585,7 @@ by rw [disj_union_comm, singleton_disj_union]
 
 /-! ### insert -/
 
-section insert
+section decidable_eq
 variables [decidable_eq α] {s t u v : finset α} {a b : α}
 
 /-- `insert a s` is the set `{a} ∪ s` containing `a` and the elements of `s`. -/
@@ -756,12 +756,8 @@ begin
       subtype.coe_mk] },
 end
 
-end insert
 
 /-! ### Lattice structure -/
-
-section lattice
-variables [decidable_eq α] {s t u v : finset α} {a b : α}
 
 /-- `s ∪ t` is the set such that `a ∈ s ∪ t` iff `a ∈ s` or `a ∈ t`. -/
 instance : has_union (finset α) := ⟨λ s t, ⟨_, t.2.ndunion s.1⟩⟩
@@ -1138,11 +1134,7 @@ by { rw [finset.disjoint_left, set.disjoint_left], refl }
   s.pairwise_disjoint (λ i, f i : ι → set α) ↔ s.pairwise_disjoint f :=
 forall₅_congr $ λ _ _ _ _ _, disjoint_coe
 
-end lattice
-
 /-! ### erase -/
-section erase
-variables [decidable_eq α] {s t u v : finset α} {a b : α}
 
 /-- `erase s a` is the set `s - {a}`, that is, the elements of `s` which are
   not equal to `a`. -/
@@ -1264,12 +1256,7 @@ lemma erase_inj_on (s : finset α) : set.inj_on s.erase s := λ _ _ _ _, (erase_
 lemma erase_inj_on' (a : α) : {s : finset α | a ∈ s}.inj_on (λ s, erase s a) :=
 λ s hs t ht (h : s.erase a =  _), by rw [←insert_erase hs, ←insert_erase ht, h]
 
-end erase
-
 /-! ### sdiff -/
-
-section sdiff
-variables [decidable_eq α] {s t u v : finset α} {a b : α}
 
 /-- `s \ t` is the set consisting of the elements of `s` that are not in `t`. -/
 instance : has_sdiff (finset α) := ⟨λs₁ s₂, ⟨s₁.1 - s₂.1, nodup_of_le tsub_le_self s₁.2⟩⟩
@@ -1422,8 +1409,6 @@ disjoint_of_subset_right (inter_subset_right _ _) sdiff_disjoint
 
 lemma sdiff_eq_self_iff_disjoint : s \ t = s ↔ disjoint s t := sdiff_eq_self_iff_disjoint'
 lemma sdiff_eq_self_of_disjoint (h : disjoint s t) : s \ t = s := sdiff_eq_self_iff_disjoint.2 h
-
-end sdiff
 
 /-! ### Symmetric difference -/
 

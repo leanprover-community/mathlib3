d00de7412ca66f352350e7b067649460a471259f
Rob Lewis <rob.y.lewis@gmail.com>
2022-09-22 20:20:13 -0400

    update tests

--- test/polyrith.lean
+++ test/polyrith.lean
@@ -101,7 +101,7 @@ end tactic
 example (x y : ℚ) (h1 : x*y + 2*x = 1) (h2 : x = y) :
   x*y = -2*y + 1 :=
 begin
-  test_sage_output "{\"data\":[\"(poly.const 1/1)\",\"(poly.const -2/1)\"],\"success\":true}",
+  test_sage_output  "{\"data\":{\"coeffs\":[\"(poly.const 1/1)\",\"(poly.const -2/1)\"],\"power\":1},\"success\":true}",
   linear_combination h1 - 2 * h2
 end
 
@@ -109,7 +109,7 @@ example (w x y z : ℝ) (h1 : x + 2.1*y + 2*z = 2) (h2 : x + 8*z + 5*w = -6.5)
     (h3 : x + y + 5*z + 5*w = 3) :
   x + 2.2*y + 2*z - 5*w = -8.5 :=
 begin
-  test_sage_output "{\"data\":[\"(poly.const 2/1)\",\"(poly.const 1/1)\",\"(poly.const -2/1)\"],\"success\":true}",
+  test_sage_output  "{\"data\":{\"coeffs\":[\"(poly.const 2/1)\",\"(poly.const 1/1)\",\"(poly.const -2/1)\"],\"power\":1},\"success\":true}",
   linear_combination 2 * h1 + h2 - 2 * h3
 end
 
@@ -120,7 +120,7 @@ end
 example (x y : ℤ) (h1 : 3*x + 2*y = 10):
   3*x + 2*y = 10 :=
 by test_polyrith
-  "{\"data\":[\"(poly.const 1/1)\"],\"success\":true}"
+  "{\"data\":{\"coeffs\":[\"(poly.const 1/1)\"],\"power\":1},\"success\":true}"
   ["ff",
   "int",
   "2",
@@ -131,7 +131,7 @@ by test_polyrith
 example (x y : ℚ) (h1 : x*y + 2*x = 1) (h2 : x = y) :
   x*y = -2*y + 1 :=
 by test_polyrith
-  "{\"data\":[\"(poly.const 1/1)\",\"(poly.const -2/1)\"],\"success\":true}"
+  "{\"data\":{\"coeffs\":[\"(poly.const 1/1)\",\"(poly.const -2/1)\"],\"power\":1},\"success\":true}"
   ["ff",
   "rat",
   "2",
@@ -142,7 +142,7 @@ by test_polyrith
 example (x y : ℝ) (h1 : x + 2 = -3) (h2 : y = 10) :
   -y + 2*x + 4 = -16 :=
 by test_polyrith
-  "{\"data\":[\"(poly.const 2/1)\",\"(poly.const -1/1)\"],\"success\":true}"
+  "{\"data\":{\"coeffs\":[\"(poly.const 2/1)\",\"(poly.const -1/1)\"],\"power\":1},\"success\":true}"
   ["ff",
   "real",
   "2",
@@ -154,7 +154,7 @@ example (x y z : ℝ) (ha : x + 2*y - z = 4) (hb : 2*x + y + z = -2)
     (hc : x + 2*y + z = 2) :
   -3*x - 3*y - 4*z = 2 :=
 by test_polyrith
-  "{\"data\":[\"(poly.const 1/1)\",\"(poly.const -1/1)\",\"(poly.const -2/1)\"],\"success\":true}"
+  "{\"data\":{\"coeffs\":[\"(poly.const 1/1)\",\"(poly.const -1/1)\",\"(poly.const -2/1)\"],\"power\":1},\"success\":true}"
   ["ff",
   "real",
   "3",
@@ -166,7 +166,7 @@ example (w x y z : ℝ) (h1 : x + 2.1*y + 2*z = 2) (h2 : x + 8*z + 5*w = -6.5)
     (h3 : x + y + 5*z + 5*w = 3) :
   x + 2.2*y + 2*z - 5*w = -8.5 :=
 by test_polyrith
-  "{\"data\":[\"(poly.const 2/1)\",\"(poly.const 1/1)\",\"(poly.const -2/1)\"],\"success\":true}"
+  "{\"data\":{\"coeffs\":[\"(poly.const 2/1)\",\"(poly.const 1/1)\",\"(poly.const -2/1)\"],\"power\":1},\"success\":true}"
   ["ff",
   "real",
   "4",
@@ -177,7 +177,7 @@ by test_polyrith
 example (a b c d : ℚ) (h1 : a = 4) (h2 : 3 = b) (h3 : c*3 = d) (h4 : -d = a) :
   2*a - 3 + 9*c + 3*d = 8 - b + 3*d - 3*a :=
 by test_polyrith
-  "{\"data\":[\"(poly.const 2/1)\",\"(poly.const -1/1)\",\"(poly.const 3/1)\",\"(poly.const -3/1)\"],\"success\":true}"
+  "{\"data\":{\"coeffs\":[\"(poly.const 2/1)\",\"(poly.const -1/1)\",\"(poly.const 3/1)\",\"(poly.const -3/1)\"],\"power\":1},\"success\":true}"
   ["ff",
   "rat",
   "4",
@@ -190,7 +190,7 @@ by test_polyrith
 example («def evil» y : ℤ) (h1 : 3*«def evil» + 2*y = 10):
   3*«def evil» + 2*y = 10 :=
 by test_polyrith
-  "{\"data\":[\"(poly.const 1/1)\"],\"success\":true}"
+  "{\"data\":{\"coeffs\":[\"(poly.const 1/1)\"],\"power\":1},\"success\":true}"
   ["ff",
   "int",
   "2",
@@ -201,7 +201,7 @@ by test_polyrith
 example («¥» y : ℤ) (h1 : 3*«¥» + 2*y = 10):
   «¥» * (3*«¥» + 2*y) = 10 * «¥» :=
 by test_polyrith
-  "{\"data\":[\"(poly.var 0)\"],\"success\":true}"
+  "{\"data\":{\"coeffs\":[\"(poly.var 0)\"],\"power\":1},\"success\":true}"
   ["ff",
   "int",
   "2",
@@ -214,7 +214,7 @@ by test_polyrith
 example (a b : ℤ) (h : a = b) :
   a * a = a * b :=
 by test_polyrith
-  "{\"data\":[\"(poly.var 0)\"],\"success\":true}"
+  "{\"data\":{\"coeffs\":[\"(poly.var 0)\"],\"power\":1},\"success\":true}"
   ["ff",
   "int",
   "2",
@@ -225,7 +225,7 @@ by test_polyrith
 example (a b c : ℤ) (h : a = b) :
   a * c = b * c :=
 by test_polyrith
-  "{\"data\":[\"(poly.var 1)\"],\"success\":true}"
+  "{\"data\":{\"coeffs\":[\"(poly.var 1)\"],\"power\":1},\"success\":true}"
   ["ff",
   "int",
   "3",
@@ -236,7 +236,7 @@ by test_polyrith
 example (a b c : ℤ) (h1 : a = b) (h2 : b = 1) :
   c * a + b = c * b + 1 :=
 by test_polyrith
-  "{\"data\":[\"(poly.var 0)\",\"(poly.const 1/1)\"],\"success\":true}"
+  "{\"data\":{\"coeffs\":[\"(poly.var 0)\",\"(poly.const 1/1)\"],\"power\":1},\"success\":true}"
   ["ff",
   "int",
   "3",
@@ -247,7 +247,7 @@ by test_polyrith
 example (x y : ℚ) (h1 : x + y = 3) (h2 : 3*x = 7) :
   x*x*y + y*x*y + 6*x = 3*x*y + 14 :=
 by test_polyrith
-  "{\"data\":[\"(poly.mul (poly.var 0) (poly.var 1))\",\"(poly.const 2/1)\"],\"success\":true}"
+  "{\"data\":{\"coeffs\":[\"(poly.mul (poly.var 0) (poly.var 1))\",\"(poly.const 2/1)\"],\"power\":1},\"success\":true}"
   ["ff",
   "rat",
   "2",
@@ -257,7 +257,7 @@ by test_polyrith
 
 example (x y z w : ℚ) (hzw : z = w) : x*z + 2*y*z = x*w + 2*y*w :=
 by test_polyrith
-  "{\"data\":[\"(poly.add (poly.var 0) (poly.mul (poly.const 2/1) (poly.var 2)))\"],\"success\":true}"
+  "{\"data\":{\"coeffs\":[\"(poly.add (poly.var 0) (poly.mul (poly.const 2/1) (poly.var 2)))\"],\"power\":1},\"success\":true}"
   ["ff",
   "rat",
   "4",
@@ -270,7 +270,7 @@ by test_polyrith
 example (a b : ℝ) (ha : 2*a = 4) (hab : 2*b = a - b) (hignore : 3 = a + b) :
   b = 2 / 3 :=
 by test_polyrith only [ha, hab]
-  "{\"data\":[\"(poly.const 1/6)\",\"(poly.const 1/3)\"],\"success\":true}"
+  "{\"data\":{\"coeffs\":[\"(poly.const 1/6)\",\"(poly.const 1/3)\"],\"power\":1},\"success\":true}"
   ["ff",
   "real",
   "2",
@@ -282,7 +282,7 @@ constant term : ∀ a b : ℚ, a + b = 0
 
 example (a b c d : ℚ) (h : a + b = 0) (h2: b + c = 0): a + b + c + d = 0 :=
 by test_polyrith only [term c d, h]
-  "{\"data\":[\"(poly.const 1/1)\",\"(poly.const 1/1)\"],\"success\":true}"
+  "{\"data\":{\"coeffs\":[\"(poly.const 1/1)\",\"(poly.const 1/1)\"],\"power\":1},\"success\":true}"
   ["ff",
   "rat",
   "4",
@@ -294,7 +294,7 @@ constants (qc : ℚ) (hqc : qc = 2*qc)
 
 example (a b : ℚ) (h : ∀ p q : ℚ, p = q) : 3*a + qc = 3*b + 2*qc :=
 by test_polyrith [h a b, hqc]
-  "{\"data\":[\"(poly.const 3/1)\",\"(poly.const 1/1)\"],\"success\":true}"
+  "{\"data\":{\"coeffs\":[\"(poly.const 3/1)\",\"(poly.const 1/1)\"],\"power\":1},\"success\":true}"
   ["ff",
   "rat",
   "3",
@@ -306,7 +306,7 @@ constant bad (q : ℚ) : q = 0
 
 example (a b : ℚ) : a + b^3 = 0 :=
 by test_polyrith [bad a, bad (b^2)]
-  "{\"data\":[\"(poly.const 1/1)\",\"(poly.var 1)\"],\"success\":true}"
+  "{\"data\":{\"coeffs\":[\"(poly.const 1/1)\",\"(poly.var 1)\"],\"power\":1},\"success\":true}"
   ["ff",
   "rat",
   "2",
@@ -319,7 +319,7 @@ by test_polyrith [bad a, bad (b^2)]
 example {α} [h : comm_ring α] {a b c d e f : α} (h1 : a*d = b*c) (h2 : c*f = e*d) :
   c * (a*f - b*e) = 0 :=
 by test_polyrith
-  "{\"data\":[\"(poly.var 4)\",\"(poly.var 1)\"],\"success\":true}"
+  "{\"data\":{\"coeffs\":[\"(poly.var 4)\",\"(poly.var 1)\"],\"power\":1},\"success\":true}"
   ["ff",
   "α",
   "6",
@@ -336,7 +336,7 @@ begin
   have hs_nonzero : s ≠ 0,
   { contrapose! hp_nonzero with hs_nonzero,
     test_polyrith
-  "{\"data\":[\"(poly.const 0/1)\",\"(poly.const 0/1)\",\"(poly.const -1/1)\",\"(poly.const 0/1)\",\"(poly.var 4)\"],\"success\":true}"
+  "{\"data\":{\"coeffs\":[\"(poly.const 0/1)\",\"(poly.const 0/1)\",\"(poly.const -1/1)\",\"(poly.const 0/1)\",\"(poly.var 4)\"],\"power\":1},\"success\":true}"
   ["ff",
   "K",
   "6",
@@ -346,7 +346,7 @@ begin
   have H' : 2 * q = s ^ 3 - t ^ 3,
   { rw ← mul_left_inj' (pow_ne_zero 3 hs_nonzero),
     test_polyrith
-  "{\"data\":[\"(poly.const -1/1)\",\"(poly.sub (poly.add (poly.neg (poly.pow (poly.var 1) 3)) (poly.var 0)) (poly.var 3))\",\"(poly.add (poly.add (poly.mul (poly.pow (poly.var 1) 2) (poly.pow (poly.var 2) 2)) (poly.mul (poly.mul (poly.var 1) (poly.var 2)) (poly.var 4))) (poly.pow (poly.var 4) 2))\",\"(poly.const 0/1)\"],\"success\":true}"
+  "{\"data\":{\"coeffs\":[\"(poly.const -1/1)\",\"(poly.sub (poly.add (poly.neg (poly.pow (poly.var 1) 3)) (poly.var 0)) (poly.var 3))\",\"(poly.add (poly.add (poly.mul (poly.pow (poly.var 1) 2) (poly.pow (poly.var 2) 2)) (poly.mul (poly.mul (poly.var 1) (poly.var 2)) (poly.var 4))) (poly.pow (poly.var 4) 2))\",\"(poly.const 0/1)\"],\"power\":1},\"success\":true}"
   ["ff",
   "K",
   "6",
@@ -354,7 +354,7 @@ begin
   "(((2 * var0) * (var1 ^ 3)) - (((var1 ^ 3) - (var2 ^ 3)) * (var1 ^ 3)))"]
   "linear_combination -hr + (-s ^ 3 + q - r) * hs3 + (s ^ 2 * t ^ 2 + s * t * p + p ^ 2) * ht"},
   test_polyrith
-  "{\"data\":[\"(poly.const 0/1)\",\"(poly.const 0/1)\",\"(poly.add (poly.add (poly.sub (poly.add (poly.add (poly.sub (poly.add (poly.sub (poly.mul (poly.var 0) (poly.pow (poly.var 5) 4)) (poly.mul (poly.var 3) (poly.pow (poly.var 5) 4))) (poly.mul (poly.var 4) (poly.pow (poly.var 5) 4))) (poly.mul (poly.var 3) (poly.pow (poly.var 5) 3))) (poly.mul (poly.var 4) (poly.pow (poly.var 5) 3))) (poly.mul (poly.mul (poly.const 3/1) (poly.var 0)) (poly.pow (poly.var 5) 2))) (poly.mul (poly.var 3) (poly.pow (poly.var 5) 2))) (poly.mul (poly.var 4) (poly.pow (poly.var 5) 2))) (poly.mul (poly.mul (poly.const 2/1) (poly.var 0)) (poly.var 5)))\",\"(poly.add (poly.sub (poly.add (poly.sub (poly.sub (poly.add (poly.add (poly.sub (poly.add (poly.sub (poly.sub (poly.add (poly.neg (poly.mul (poly.mul (poly.var 0) (poly.pow (poly.var 3) 2)) (poly.var 5))) (poly.mul (poly.pow (poly.var 3) 3) (poly.var 5))) (poly.mul (poly.mul (poly.var 0) (poly.pow (poly.var 4) 2)) (poly.var 5))) (poly.mul (poly.pow (poly.var 4) 3) (poly.var 5))) (poly.mul (poly.mul (poly.var 0) (poly.var 1)) (poly.pow (poly.var 5) 2))) (poly.mul (poly.mul (poly.var 1) (poly.var 3)) (poly.pow (poly.var 5) 2))) (poly.mul (poly.mul (poly.var 1) (poly.var 4)) (poly.pow (poly.var 5) 2))) (poly.mul (poly.pow (poly.var 0) 2) (poly.var 3))) (poly.pow (poly.var 3) 3)) (poly.mul (poly.pow (poly.var 0) 2) (poly.var 4))) (poly.pow (poly.var 4) 3)) (poly.mul (poly.mul (poly.var 0) (poly.var 1)) (poly.var 5))) (poly.mul (poly.mul (poly.const 3/1) (poly.var 0)) (poly.var 1)))\",\"(poly.const -1/1)\"],\"success\":true}"
+  "{\"data\":{\"coeffs\":[\"(poly.const 0/1)\",\"(poly.const 0/1)\",\"(poly.add (poly.add (poly.sub (poly.add (poly.add (poly.sub (poly.add (poly.sub (poly.mul (poly.var 0) (poly.pow (poly.var 5) 4)) (poly.mul (poly.var 3) (poly.pow (poly.var 5) 4))) (poly.mul (poly.var 4) (poly.pow (poly.var 5) 4))) (poly.mul (poly.var 3) (poly.pow (poly.var 5) 3))) (poly.mul (poly.var 4) (poly.pow (poly.var 5) 3))) (poly.mul (poly.mul (poly.const 3/1) (poly.var 0)) (poly.pow (poly.var 5) 2))) (poly.mul (poly.var 3) (poly.pow (poly.var 5) 2))) (poly.mul (poly.var 4) (poly.pow (poly.var 5) 2))) (poly.mul (poly.mul (poly.const 2/1) (poly.var 0)) (poly.var 5)))\",\"(poly.sub (poly.add (poly.add (poly.sub (poly.sub (poly.add (poly.add (poly.sub (poly.add (poly.sub (poly.neg (poly.mul (poly.mul (poly.var 0) (poly.pow (poly.var 3) 2)) (poly.var 5))) (poly.mul (poly.mul (poly.var 0) (poly.pow (poly.var 4) 2)) (poly.var 5))) (poly.mul (poly.mul (poly.var 0) (poly.var 1)) (poly.pow (poly.var 5) 2))) (poly.mul (poly.mul (poly.var 1) (poly.var 3)) (poly.pow (poly.var 5) 2))) (poly.mul (poly.mul (poly.var 1) (poly.var 4)) (poly.pow (poly.var 5) 2))) (poly.mul (poly.pow (poly.var 0) 2) (poly.var 3))) (poly.mul (poly.pow (poly.var 0) 2) (poly.var 4))) (poly.mul (poly.mul (poly.var 0) (poly.var 1)) (poly.var 5))) (poly.mul (poly.mul (poly.const 3/1) (poly.var 0)) (poly.var 1))) (poly.mul (poly.mul (poly.const 2/1) (poly.var 2)) (poly.var 5))) (poly.mul (poly.const 2/1) (poly.var 2)))\",\"(poly.neg (poly.pow (poly.var 5) 3))\"],\"power\":1},\"success\":true}"
   ["ff",
   "K",
   "7",
@@ -362,14 +362,13 @@ begin
   "((((var0 ^ 3) + ((3 * var1) * var0)) - (2 * var2)) - (((var0 - (var3 - var4)) * (var0 - ((var3 * var5) - (var4 * (var5 ^ 2))))) * (var0 - ((var3 * (var5 ^ 2)) - (var4 * var5)))))"]
   "linear_combination (x * ω ^ 4 - s * ω ^ 4 + t * ω ^ 4 - s * ω ^ 3 + t * ω ^ 3 + 3 * x * ω ^ 2 - s * ω ^ 2 +
       t * ω ^ 2 +
-    2 * x * ω) * ht + (-(x * s ^ 2 * ω) + s ^ 3 * ω - x * t ^ 2 * ω - t ^ 3 * ω + x * p * ω ^ 2 - p * s * ω ^ 2 +
-                p * t * ω ^ 2 +
+    2 * x * ω) * ht + (-(x * s ^ 2 * ω) - x * t ^ 2 * ω + x * p * ω ^ 2 - p * s * ω ^ 2 + p * t * ω ^ 2 +
               x ^ 2 * s -
-            s ^ 3 -
-          x ^ 2 * t +
-        t ^ 3 -
-      x * p * ω +
-    3 * x * p) * H - H'"
+            x ^ 2 * t -
+          x * p * ω +
+        3 * x * p +
+      2 * q * ω -
+    2 * q) * H - ω ^ 3 * H'"
 end
 
 
@@ -377,7 +376,7 @@ end
 
 example {K : Type*} [field K] [char_zero K] {s : K} (hs : 3 * s + 1 = 4) : s = 1 :=
 by test_polyrith
-  "{\"data\":[\"(poly.const 1/3)\"],\"success\":true}"
+  "{\"data\":{\"coeffs\":[\"(poly.const 1/3)\"],\"power\":1},\"success\":true}"
   ["ff",
   "K",
   "1",
@@ -387,7 +386,7 @@ by test_polyrith
 
 example {x : ℤ} (h1 : x + 4 = 2) : x = -2 :=
 by test_polyrith
-  "{\"data\":[\"(poly.const 1/1)\"],\"success\":true}"
+  "{\"data\":{\"coeffs\":[\"(poly.const 1/1)\"],\"power\":1},\"success\":true}"
   ["ff",
   "int",
   "1",
@@ -397,7 +396,7 @@ by test_polyrith
 
 example {w : ℚ} (h1 : 3 * w + 1 = 4) : w = 1 :=
 by test_polyrith
-  "{\"data\":[\"(poly.const 1/3)\"],\"success\":true}"
+  "{\"data\":{\"coeffs\":[\"(poly.const 1/3)\"],\"power\":1},\"success\":true}"
   ["ff",
   "rat",
   "1",
@@ -407,7 +406,7 @@ by test_polyrith
 
 example {x : ℤ} (h1 : 2 * x + 3 = x) : x = -3 :=
 by test_polyrith
-  "{\"data\":[\"(poly.const 1/1)\"],\"success\":true}"
+  "{\"data\":{\"coeffs\":[\"(poly.const 1/1)\"],\"power\":1},\"success\":true}"
   ["ff",
   "int",
   "1",
@@ -417,7 +416,7 @@ by test_polyrith
 
 example {c : ℚ} (h1 : 4 * c + 1 = 3 * c - 2) : c = -3 :=
 by test_polyrith
-  "{\"data\":[\"(poly.const 1/1)\"],\"success\":true}"
+  "{\"data\":{\"coeffs\":[\"(poly.const 1/1)\"],\"power\":1},\"success\":true}"
   ["ff",
   "rat",
   "1",
@@ -427,13 +426,13 @@ by test_polyrith
 
 example (z : ℤ) (h1 : z + 1 = 2) (h2 : z + 2 = 2) : (1 : ℤ) = 2 :=
 by test_polyrith
-  "{\"data\":[\"(poly.const 1/1)\",\"(poly.const -1/1)\"],\"success\":true}"
+  "{\"data\":{\"coeffs\":[\"(poly.const -1/1)\",\"(poly.const 1/1)\"],\"power\":0},\"success\":true}"
   ["ff",
   "int",
   "1",
   "[((var0 + 1) - 2), ((var0 + 2) - 2)]",
   "(1 - 2)"]
-  "linear_combination h1 - h2"
+  "linear_combination -h1 + h2 with {exponent := 0}"
 
 
 /- ### Cases with powers -/
@@ -446,7 +445,7 @@ by test_polyrith
   "3",
   "[(var0 - var1), ((var0 * var1) - 0)]",
   "((var0 + (var1 * var2)) - 0)"]
-  "linear_combination (-(y * z ^ 2) + x) * h + (z ^ 2 + 2 * z + 1) * h2 with exponent 2"
+  "linear_combination (-(y * z ^ 2) + x) * h + (z ^ 2 + 2 * z + 1) * h2 with {exponent := 2}"
 
 .
 example (K : Type) (h_this : 3 - 1 = 2)
@@ -466,7 +465,10 @@ by test_polyrith
   "[(((var1 ^ 3) + (var0 * (3 * (var2 ^ 2)))) - 0), (((var0 ^ 3) + (var2 * (3 * (var1 ^ 2)))) - 0), (((var1 * (3 * (var0 ^ 2))) + (var2 ^ 3)) - 0), (((((var0 ^ 3) * var1) + ((var1 ^ 3) * var2)) + ((var2 ^ 3) * var0)) - 0)]",
   "(var0 - 0)"]
   "linear_combination 2 / 7 * y * z ^ 2 * h₂ + (x ^ 3 - 1 / 7 * y ^ 2 * z) * h₁ - x * y * z * h₀ + 1 / 7 * y *
-    z * h with exponent 6"
+    z * h with {exponent := 6}"
+
+
+
 
 -- We comment the following tests so that we don't overwhelm the SageCell API.
 
@@ -738,6 +740,24 @@ example {c : ℚ} (h1 : 4 * c + 1 = 3 * c - 2) : c = -3 :=
 by create_polyrith_test
 
 example (z : ℤ) (h1 : z + 1 = 2) (h2 : z + 2 = 2) : (1 : ℤ) = 2 :=
+by create_polyrith_test
+
+
+/- ### Cases with powers -/
+
+example (x y z : ℚ) (h : x = y) (h2 : x * y = 0) : x + y*z = 0 :=
+by create_polyrith_test
+
+.
+example (K : Type) (h_this : 3 - 1 = 2)
+  [field K]
+  [char_zero K]
+  {x y z : K}
+  (h₂ : y ^ 3 + x * (3 * z ^ 2) = 0)
+  (h₁ : x ^ 3 + z * (3 * y ^ 2) = 0)
+  (h₀ : y * (3 * x ^ 2) + z ^ 3 = 0)
+  (h : x ^ 3 * y + y ^ 3 * z + z ^ 3 * x = 0) :
+  x = 0 :=
 by create_polyrith_test
 
 

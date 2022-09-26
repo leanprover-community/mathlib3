282c3915b6d80193280f88ee0b84dd8bacb6c6c0
Jujian Zhang <jujian.zhang1998@outlook.com>
2022-09-26 07:11:55 +0100

    Apply suggestions from code review
    Co-authored-by: Junyan Xu <junyanxumath@gmail.com>

--- src/topology/sheaves/abelian.lean
+++ src/topology/sheaves/abelian.lean
@@ -30,12 +30,12 @@ variables {C : Type (max v u)} [category.{v} C]
 variables {D : Type w} [category.{max v u} D] [abelian D]
 variables {J : grothendieck_topology C}
 
--- This need to be specified manually because of universe level.
+-- This needs to be specified manually because of universe level.
 instance : abelian (Cᵒᵖ ⥤ D) := @abelian.functor_category_abelian.{v} Cᵒᵖ _ D _ _
 
--- This also need to be specified manually, but I don't know why.
+-- This also needs to be specified manually, but I don't know why.
 instance : has_finite_products (Sheaf J D) :=
-{ out := λ j ij, { has_limit := λ α, by { resetI, apply_instance } } }
+{ out := λ j ij, { has_limit := by { introI, apply_instance } } }
 
 -- sheafification assumptions
 variables [∀ (P : Cᵒᵖ ⥤ D) (X : C) (S : J.cover X), has_multiequalizer (S.index P)]
@@ -45,7 +45,7 @@ variables [∀ (X : C), preserves_colimits_of_shape (J.cover X)ᵒᵖ (forget D)
 variables [reflects_isomorphisms (forget D)]
 
 instance Sheaf_is_abelian [has_finite_limits D] : abelian (Sheaf J D) :=
-let adj := (sheafification_adjunction J D) in abelian_of_adjunction _ _ (as_iso adj.counit) $ adj
+let adj := sheafification_adjunction J D in abelian_of_adjunction _ _ (as_iso adj.counit) adj
 
 local attribute [instance] preserves_binary_biproducts_of_preserves_binary_products
 

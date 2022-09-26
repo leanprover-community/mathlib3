b47a6b1911ccb23e40038411bafc185065140fea
adomani <adomani@gmail.com>
2022-09-26 10:56:31 +0200

    reduce test and link

--- test/positivity.lean
+++ test/positivity.lean
@@ -15,12 +15,11 @@ This tactic proves goals of the form `0 ≤ a` and `0 < a`.
 -/
 
 /-  Test for instantiating meta-variables.  Reported on
-Stream: metaprogramming/tactics
-Topic: New tactic: `positivity`
-Date: Sept 25th, 2022.   -/
-example : 0 ≤ 1 :=
+https://leanprover.zulipchat.com/#narrow/stream/239415-metaprogramming-.2F-tactics/topic/New.20tactic.3A.20.60positivity.60/near/300639970
+-/
+example : 0 ≤ 0 :=
 begin
-  apply le_trans _ (le_rfl : 1 ≤ 1),
+  apply le_trans _ le_rfl,
   positivity,
 end
 

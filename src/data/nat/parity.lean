23528c409933f7e8d2421c70bb7deca13a5c6a9e
Mantas Bakšys <39908973+MantasBaksys@users.noreply.github.com>
2022-09-26 23:22:41 +0300

    Update src/data/nat/parity.lean
    Co-authored-by: Riccardo Brasca <riccardo.brasca@gmail.com>

--- src/data/nat/parity.lean
+++ src/data/nat/parity.lean
@@ -187,7 +187,7 @@ lemma two_mul_div_two_of_even : even n → 2 * (n / 2) = n :=
  λ h, nat.mul_div_cancel_left' (even_iff_two_dvd.mp h)
 
 lemma div_two_mul_two_of_even : even n → n / 2 * 2 = n := --nat.div_mul_cancel
- λ h, nat.div_mul_cancel (even_iff_two_dvd.mp h)
+λ h, nat.div_mul_cancel (even_iff_two_dvd.mp h)
 
 lemma two_mul_div_two_add_one_of_odd (h : odd n) : 2 * (n / 2) + 1 = n :=
 by { rw mul_comm, convert nat.div_add_mod' n 2, rw odd_iff.mp h }

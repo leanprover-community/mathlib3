253970ec97dc4a1ec6865cbc9c1772413024294a
Moritz Doll <moritz.doll@googlemail.com>
2022-09-30 01:05:47 +0200

    fixes

--- src/analysis/special_functions/japanese_bracket.lean
+++ src/analysis/special_functions/japanese_bracket.lean
@@ -46,85 +46,15 @@ open_locale big_operators nnreal filter topological_space ennreal
 
 open asymptotics filter set real measure_theory finite_dimensional
 
-section
-
-variables {α : Type*} [measurable_space α] [μ : measure α] {f g : α → ℝ≥0∞}
-  {s : set α}
-
-lemma lintegral_norm_eq_of_ae_nonneg {f : α → ℝ} {μ : measure α}
-  (h_nonneg : 0 ≤ᵐ[μ] f) :
-  ∫⁻ x, ennreal.of_real (∥f x∥) ∂μ = ∫⁻ x, ennreal.of_real (f x) ∂μ :=
-begin
-  apply lintegral_congr_ae,
-  filter_upwards [h_nonneg] with x hx,
-  rw norm_of_nonneg hx,
-end
-
-lemma lintegral_norm_eq_of_nonneg {f : α → ℝ} {μ : measure α}
-  (h_nonneg : 0 ≤ f) :
-  ∫⁻ x, ennreal.of_real (∥f x∥) ∂μ = ∫⁻ x, ennreal.of_real (f x) ∂μ :=
-lintegral_norm_eq_of_ae_nonneg (filter.eventually_of_forall h_nonneg)
-
-lemma integrable_on.lintegral_lt_top_ae' {f : α → ℝ} {s : set α} {μ : measure α}
-  (hf : integrable_on f s μ) (h_nonneg : 0 ≤ᵐ[μ.restrict s] f) :
-  ∫⁻ x in s, ennreal.of_real (f x) ∂μ < ⊤ :=
-begin
-  rw [←lintegral_norm_eq_of_ae_nonneg h_nonneg, ←has_finite_integral_iff_norm],
-  exact hf.2,
-end
-
-lemma integrable_on.lintegral_lt_top_ae {f : α → ℝ} {s : set α} {μ : measure α}
-  (hf : integrable_on f s μ) (hf' : ∀ᵐ x ∂μ, x ∈ s → 0 ≤ f x) :
-  ∫⁻ x in s, ennreal.of_real (f x) ∂μ < ⊤ :=
-begin
-  let g := hf.1.mk _,
-  have : ∫⁻ x in s, ennreal.of_real (f x) ∂μ = ∫⁻ x in s, ennreal.of_real (g x) ∂μ,
-  { apply lintegral_congr_ae,
-    filter_upwards [hf.1.ae_eq_mk] with x hx,
-    rw hx },
-  rw this,
-  apply integrable_on.lintegral_lt_top_ae' (hf.congr hf.1.ae_eq_mk),
-  apply (ae_restrict_iff (measurable_set_le measurable_zero (hf.1.measurable_mk))).2,
-  filter_upwards [hf', ae_imp_of_ae_restrict hf.1.ae_eq_mk] with x hx h'x,
-  assume xs,
-  rw ← h'x xs,
-  exact hx xs,
-end
-
-lemma integrable_on.lintegral_lt_top {f : ℝ → ℝ} {s : set ℝ} {μ : measure ℝ}
-  (hf : integrable_on f s μ) (hf' : ∀ x : ℝ, x ∈ s → 0 ≤ f x) :
-  ∫⁻ x in s, ennreal.of_real (f x) ∂μ < ⊤ :=
-integrable_on.lintegral_lt_top_ae hf (ae_of_all μ hf')
-
-end
-
 variables {E : Type*} [normed_add_comm_group E] [normed_space ℝ E]
-
-lemma rpow_two_sqrt {x : ℝ} (hx : 0 ≤ x): (real.sqrt x)^(2 : ℝ) = x :=
-begin
-  rw [sqrt_eq_rpow, ←rpow_mul hx],
-  norm_num,
-end
-
-lemma pow_two_sqrt {x : ℝ} (hx : 0 ≤ x): (real.sqrt x)^2 = x :=
-by rw [←rpow_two, rpow_two_sqrt hx]
-
-lemma rpow_div_two_eq_sqrt {x : ℝ} (r : ℝ) (hx : 0 ≤ x) : x^(r/2) = x.sqrt^r :=
-begin
-  rw [sqrt_eq_rpow, ←rpow_mul hx],
-  congr,
-  ring,
-end
 
 lemma jap_le_one_add_norm (x : E) : real.sqrt (1 + ∥x∥^2) ≤ 1 + ∥x∥ :=
 begin
-  have h1 : 0 ≤ real.sqrt (1 + ∥x∥^2) := by positivity,
-  have h1' : 0 ≤ 1 + ∥x∥^2 := by positivity,
+  have h1 : 0 ≤ 1 + ∥x∥^2 := by positivity,
   have h2 : 0 ≤ 1 + ∥x∥ := by positivity,
-  have h3 : 0 < (2 : ℝ) := by positivity,
-  rw ←rpow_le_rpow_iff h1 h2 h3,
-  rw rpow_two_sqrt h1',
-  rw rpow_two,
+  have h3 : 0 < 2 := by positivity,
+  refine le_of_pow_le_pow 2 h2 h3 _,
+  rw sq_sqrt h1,
   rw add_pow_two,
   simp,
 end
@@ -135,7 +65,7 @@ begin
   have h2 : 0 ≤ (real.sqrt 2) * real.sqrt (1 + ∥x∥^2) := by positivity,
   have : (sqrt 2 * sqrt (1 + ∥x∥ ^ 2)) ^ 2 - (1 + ∥x∥) ^ 2 = (1 - ∥x∥) ^2 :=
   begin
-    rw [mul_pow, pow_two_sqrt h1, add_pow_two, sub_pow_two],
+    rw [mul_pow, sq_sqrt h1, add_pow_two, sub_pow_two],
     norm_num,
     ring,
   end,
@@ -167,55 +97,8 @@ end
 
 variables [finite_dimensional ℝ E]
 
-lemma continuous_foo {ε : ℝ} (hε : 0 < ε) :
-  continuous (λ (x : E), (1 + ∥x∥)^(-((finrank ℝ E : ℝ) + ε))) :=
-begin
-  refine continuous.rpow_const (by continuity) _,
-  intro,
-  left,
-  refine ne_of_gt _,
-  positivity,
-end
-
 variables (E)
 
-lemma le_neg_rpow_iff {a b c : ℝ} (ha : 0 < a) (hb : 0 < b) (hc : 0 < c) :
-  a ≤ b^(-c) ↔ b ≤ a^(-c⁻¹) :=
-begin
-  have hc' : 0 < c⁻¹ := by positivity,
-  have hac : 0 < a^(c⁻¹) := rpow_pos_of_pos ha _,
-  have hbc : 0 < b^(-c) := rpow_pos_of_pos hb _,
-  rw [←rpow_le_rpow_iff ha.le hbc.le hc', ←rpow_mul hb.le],
-  simp only [ne_of_gt hc, rpow_neg_one, neg_mul, mul_inv_cancel, ne.def, not_false_iff],
-  rw [le_inv hac hb, ←rpow_neg_one, ←rpow_mul ha.le],
-  simp,
-end
-
-lemma lt_neg_rpow_iff {a b c : ℝ} (ha : 0 < a) (hb : 0 < b) (hc : 0 < c) :
-  a < b^(-c) ↔ b < a^(-c⁻¹) :=
-begin
-  have hc' : 0 < c⁻¹ := by positivity,
-  have hac : 0 < a^(c⁻¹) := rpow_pos_of_pos ha _,
-  have hbc : 0 < b^(-c) := rpow_pos_of_pos hb _,
-  rw [←rpow_lt_rpow_iff ha.le hbc.le hc', ←rpow_mul hb.le],
-  simp only [ne_of_gt hc, rpow_neg_one, neg_mul, mul_inv_cancel, ne.def, not_false_iff],
-  rw [lt_inv hac hb, ←rpow_neg_one, ←rpow_mul ha.le],
-  simp,
-end
-
-lemma neg_rpow_lt_iff {a b c : ℝ} (ha : 0 < a) (hb : 0 < b) (hc : 0 < c) :
-  a^(-c) < b ↔ b^(-c⁻¹) < a :=
-begin
-  convert lt_neg_rpow_iff (rpow_pos_of_pos ha _) (rpow_pos_of_pos hb _) hc;
-  simp [←rpow_mul ha.le, ←rpow_mul hb.le, ne_of_gt hc],
-end
-
-lemma neg_rpow_le_iff {a b c : ℝ} (ha : 0 < a) (hb : 0 < b) (hc : 0 < c) :
-  a^(-c) ≤ b ↔ b^(-c⁻¹) ≤ a :=
-begin
-  convert le_neg_rpow_iff (rpow_pos_of_pos ha _) (rpow_pos_of_pos hb _) hc;
-  simp [←rpow_mul ha.le, ←rpow_mul hb.le, ne_of_gt hc],
-end
 
 lemma set_as_closed_ball {r t : ℝ} (hr : 0 < r) (ht : 0 < t) :
   {a : E | t ≤ (1 + ∥a∥) ^ -r} = metric.closed_ball 0 (t^(-r⁻¹) - 1) :=
@@ -223,15 +106,15 @@ begin
   ext,
   simp only [norm_eq_abs, mem_set_of_eq, mem_closed_ball_zero_iff],
   rw le_sub_iff_add_le',
-  exact le_neg_rpow_iff ht (by positivity) hr,
+  rw neg_inv,
+  exact (real.le_rpow_inv_iff_of_neg (by positivity) ht (neg_lt_zero.mpr hr)).symm,
 end
 
 lemma closed_ball_empty {r t : ℝ} (hr : 0 < r) (ht : 1 < t) :
   metric.closed_ball (0 : E) (t^(-r⁻¹) - 1) = ∅ :=
 begin
-  rw [metric.closed_ball_eq_empty, sub_neg,
-    ←neg_rpow_lt_iff zero_lt_one (zero_lt_one.trans ht) hr],
-  simp [ht],
+  rw [metric.closed_ball_eq_empty, sub_neg],
+  exact real.rpow_lt_one_of_one_lt_of_neg ht (by simp only [hr, right.neg_neg_iff, inv_pos]),
 end
 
 variables {E}
@@ -246,11 +129,10 @@ begin
     intros x hx,
     have hxr : 0 ≤ x^ -r⁻¹ := rpow_nonneg_of_nonneg hx.1.le _,
     apply ennreal.of_real_le_of_real,
-    refine pow_le_pow_of_le_left _ _ n,
-    { rw [le_sub_iff_add_le', add_zero],
-      rw ← le_neg_rpow_iff hx.1 zero_lt_one hr,
-      simp [hx.2] },
-    simp,
+    refine pow_le_pow_of_le_left _ (by simp only [sub_le_self_iff, zero_le_one]) n,
+    rw [le_sub_iff_add_le', add_zero],
+    refine real.one_le_rpow_of_pos_of_le_one_of_nonpos hx.1 hx.2 _,
+    simp only [hr.le, right.neg_nonpos_iff, inv_nonneg],
   end,
   refine lt_of_le_of_lt (set_lintegral_mono _ _ h_int) _,
   measurability, -- all involved functions are measurable
@@ -262,21 +144,28 @@ begin
     rw [←rpow_nat_cast, ←rpow_mul hx.1.le, neg_mul],
   end,
   rw set_lintegral_congr_fun measurable_set_Ioc (ae_of_all volume h_int'),
-  refine integrable_on.lintegral_lt_top _ _,
-  { rw ←interval_integrable_iff_integrable_Ioc_of_le zero_le_one,
-    apply interval_integral.interval_integrable_rpow',
-    simp only [neg_lt_neg_iff],
-    rw inv_mul_lt_iff' hr,
-    simp [hnr], },
-  intros x hx,
-  exact rpow_nonneg_of_nonneg hx.1.le _,
+  refine integrable_on.set_lintegral_lt_top _,
+  rw ←interval_integrable_iff_integrable_Ioc_of_le zero_le_one,
+  apply interval_integral.interval_integrable_rpow',
+  simp only [neg_lt_neg_iff],
+  rw inv_mul_lt_iff' hr,
+  simp [hnr],
 end
 
-lemma finite_foo [measure_space E] [borel_space E] [(@volume E _).is_add_haar_measure]
-  {r : ℝ} (hnr : (finrank ℝ E : ℝ) < r) :
+lemma finite_integral_one_add_norm [measure_space E] [borel_space E]
+  [(@volume E _).is_add_haar_measure] {r : ℝ} (hnr : (finrank ℝ E : ℝ) < r) :
   ∫⁻ (x : E), ennreal.of_real ((1 + ∥x∥) ^ -r) < ⊤ :=
 begin
   have hr : 0 < r := lt_of_le_of_lt (finrank ℝ E).cast_nonneg hnr,
+
+  -- We start by applying the layer cake formula
+  have h_meas : measurable (λ (ω : E), (1 + ∥ω∥) ^ -r) := by measurability,
+  have h_pos : ∀ x : E, 0 ≤ (1 + ∥x∥) ^ -r :=
+  by { intros x, positivity },
+  rw lintegral_eq_lintegral_meas_le volume h_pos h_meas,
+
+  -- We use the first transformation of the integrant to show that we only have to integrate from
+  -- 0 to 1 and from 1 to ∞
   have h_int : ∀ (t : ℝ) (ht : t ∈ Ioi (0 : ℝ)),
     (volume {a : E | t ≤ (1 + ∥a∥) ^ -r} : ennreal) =
     volume (metric.closed_ball (0 : E) (t^(-r⁻¹) - 1)) :=
@@ -285,6 +174,12 @@ begin
     congr' 1,
     exact set_as_closed_ball E hr (mem_Ioi.mp ht),
   end,
+  rw set_lintegral_congr_fun measurable_set_Ioi (ae_of_all volume $ h_int),
+  have hIoi_eq : Ioi (0 : ℝ) = Ioc (0 : ℝ) 1 ∪ Ioi 1 :=
+    (set.Ioc_union_Ioi_eq_Ioi zero_le_one).symm,
+  have hdisjoint : disjoint (Ioc (0 : ℝ) 1) (Ioi 1) := by simp [disjoint_iff],
+  rw [hIoi_eq, lintegral_union measurable_set_Ioi hdisjoint, ennreal.add_lt_top],
+
   have h_int' : ∀ (t : ℝ) (ht : t ∈ Ioc (0 : ℝ) 1),
     (volume (metric.closed_ball (0 : E) (t^(-r⁻¹) - 1)) : ennreal) =
     ennreal.of_real ((t^(-r⁻¹) - 1) ^ finite_dimensional.finrank ℝ E)
@@ -292,9 +187,23 @@ begin
   begin
     intros t ht,
     rw measure.add_haar_closed_ball,
-    rw [le_sub_iff_add_le', add_zero, ← le_neg_rpow_iff ht.1 zero_lt_one hr],
-    simp [ht.2],
+    rw [le_sub_iff_add_le', add_zero],
+    exact real.one_le_rpow_of_pos_of_le_one_of_nonpos ht.1 ht.2 (by simp [hr.le]),
   end,
+  have h_meas' : measurable (λ (a : ℝ), ennreal.of_real ((a ^ -r⁻¹ - 1) ^ finrank ℝ E)) :=
+  by measurability,
+  split,
+  -- The integral from 0 to 1:
+  { rw [set_lintegral_congr_fun measurable_set_Ioc (ae_of_all volume $ h_int'),
+      lintegral_mul_const _ h_meas', ennreal.mul_lt_top_iff],
+    left,
+    -- We calculate the integral
+    use finite_foo' _ hnr,
+    -- The unit ball has finite measure
+    rw ←measure.add_haar_closed_unit_ball_eq_add_haar_unit_ball,
+    exact (is_compact_closed_ball _ _).measure_lt_top, },
+
+  -- The integral from 1 to ∞ is zero:
   have h_int'' : ∀ (t : ℝ) (ht : t ∈ Ioi (1 : ℝ)),
     (volume (metric.closed_ball (0 : E) (t^(-r⁻¹) - 1)) : ennreal) = 0 :=
   begin
@@ -302,63 +211,37 @@ begin
     rw closed_ball_empty E hr ht,
     simp,
   end,
-  rw lintegral_eq_lintegral_meas_le,
-  { -- We use the first transformation of the integrant to show that we only have to integrate from
-    -- 0 to 1 and from 1 to ∞
-    have hIoi_eq : Ioi (0 : ℝ) = Ioc (0 : ℝ) 1 ∪ Ioi 1 :=
-      (set.Ioc_union_Ioi_eq_Ioi zero_le_one).symm,
-    have hdisjoint : disjoint (Ioc (0 : ℝ) 1) (Ioi 1) := by simp [disjoint_iff],
-    rw set_lintegral_congr_fun measurable_set_Ioi (ae_of_all volume $ h_int),
-    rw [hIoi_eq, lintegral_union measurable_set_Ioi hdisjoint, ennreal.add_lt_top],
-    split,
-    -- The integral from 0 to 1:
-    { rw set_lintegral_congr_fun measurable_set_Ioc (ae_of_all volume $ h_int'),
-      rw lintegral_mul_const,
-      { rw ennreal.mul_lt_top_iff,
-        left,
-        -- We calculate the integral
-        use finite_foo' _ hnr,
-        -- The unit ball has finite measure
-        rw ←measure.add_haar_closed_unit_ball_eq_add_haar_unit_ball,
-        exact (is_compact_closed_ball _ _).measure_lt_top, },
-      measurability, },
-    -- The integral from 1 to ∞ is zero:
-    rw set_lintegral_congr_fun measurable_set_Ioi (ae_of_all volume $ h_int''),
-    simp, },
-  { intros x,
-    simp only [pi.zero_apply, norm_eq_abs, neg_add_rev],
-    refine rpow_nonneg_of_nonneg _ _,
-    positivity, },
-  measurability,
+  rw set_lintegral_congr_fun measurable_set_Ioi (ae_of_all volume $ h_int''),
+  simp,
 end
 
-lemma foo [measure_space E] [borel_space E] [(@volume E _).is_add_haar_measure]
+lemma integrable_one_add_norm [measure_space E] [borel_space E] [(@volume E _).is_add_haar_measure]
   {r : ℝ} (hnr : (finrank ℝ E : ℝ) < r) :
   integrable (λ (x : E), (1 + ∥x∥) ^ -r) :=
 begin
   refine ⟨by measurability, _⟩,
   -- Lower Lebesgue integral
-  have : ∫⁻ (a : E), ennreal.of_real (∥(1 + ∥a∥) ^ -r∥) =
+  have : ∫⁻ (a : E), ∥(1 + ∥a∥) ^ -r∥₊ =
     ∫⁻ (a : E), ennreal.of_real ((1 + ∥a∥) ^ -r) :=
   begin
-    refine lintegral_norm_eq_of_nonneg (λ x, _),
+    refine lintegral_nnnorm_eq_of_nonneg (λ x, _),
     have h : 0 ≤ (1 + ∥x∥) ^ -r := rpow_nonneg_of_nonneg (by positivity) _,
     simp [h],
   end,
-  rw [has_finite_integral_iff_norm, this],
-  exact finite_foo hnr,
+  rw [has_finite_integral, this],
+  exact finite_integral_one_add_norm hnr,
 end
 
 lemma integrable_jap [measure_space E] [borel_space E] [(@volume E _).is_add_haar_measure]
   {r : ℝ} (hnr : (finrank ℝ E : ℝ) < r) :
   integrable (λ (x : E), (1 + ∥x∥^2) ^ (-r/2)) :=
 begin
-  have hr : 0 < r := lt_of_le_of_lt (nat.cast_nonneg _) hnr,
-  refine ((foo hnr).const_mul (2^(r/2))).mono (by measurability) (eventually_of_forall _),
-  intros x,
-  have h1 : 0 ≤ (1 + ∥x∥ ^ 2) ^ (-r/2) := rpow_nonneg_of_nonneg (by positivity) _,
-  have h2 : 0 ≤ (1 + ∥x∥) ^ -r := rpow_nonneg_of_nonneg (by positivity) _,
-  have h3 : 0 ≤ (2 : ℝ)^(r/2) := rpow_nonneg_of_nonneg (by positivity) _,
+  have hr : 0 < r := lt_of_le_of_lt (finrank ℝ E).cast_nonneg hnr,
+  refine ((integrable_one_add_norm hnr).const_mul $ 2 ^ (r / 2)).mono (by measurability)
+    (eventually_of_forall $ λ x, _),
+  have h1 : 0 ≤ (1 + ∥x∥ ^ 2) ^ (-r/2) := by positivity,
+  have h2 : 0 ≤ (1 + ∥x∥) ^ -r := by positivity,
+  have h3 : 0 ≤ (2 : ℝ)^(r/2) := by positivity,
   simp_rw [norm_mul, norm_eq_abs, abs_of_nonneg h1, abs_of_nonneg h2, abs_of_nonneg h3],
   exact rpow_neg_jap_le_one_add_norm _ hr,
 end

eee3bf5117a4699ceb7e148b7eccaa1822f32482
Yaël Dillies <yael.dillies@gmail.com>
2022-09-29 22:29:14 +0000

    fix linear_algebra.affine_space.ordered

--- src/linear_algebra/affine_space/ordered.lean
+++ src/linear_algebra/affine_space/ordered.lean
@@ -203,7 +203,7 @@ begin
   vsub_eq_sub, vsub_eq_sub, vsub_eq_sub, vadd_eq_add, vadd_eq_add,
   smul_eq_mul, add_sub_cancel, smul_sub, smul_sub, smul_sub,
   sub_le_iff_le_add, mul_inv_rev, mul_smul, mul_smul, ←smul_sub, ←smul_sub, ←smul_add, smul_smul,
-  ← mul_inv_rev, smul_le_iff_of_pos (inv_pos.2 h), inv_inv, smul_smul,
+  ← mul_inv_rev, inv_smul_le_iff h, smul_smul,
   mul_inv_cancel_right₀ (right_ne_zero_of_mul h.ne'), smul_add,
   smul_inv_smul₀ (left_ne_zero_of_mul h.ne')],
   apply_instance

5ce5e8c6a90880ccd601cf202daa73e46fc1452d
Junyan Xu <junyanxumath@gmail.com>
2022-09-30 00:25:32 -0400

    match variable names

--- src/data/finsupp/lex.lean
+++ src/data/finsupp/lex.lean
@@ -147,30 +147,30 @@ end covariants
 
 section well_founded
 
-variables {ι α : Type*} [hz : has_zero α] (r : ι → ι → Prop) (s : α → α → Prop)
-  (hbot : ∀ ⦃a⦄, ¬ s a 0) (hs : well_founded s)
+variables [hz : has_zero N] (r : α → α → Prop) (s : N → N → Prop)
+  (hbot : ∀ ⦃n⦄, ¬ s n 0) (hs : well_founded s)
 include hbot hs
 
-lemma lex.acc (x : ι →₀ α) (h : ∀ i ∈ x.support, acc (rᶜ ⊓ (≠)) i) : acc (finsupp.lex r s) x :=
+lemma lex.acc (x : α →₀ N) (h : ∀ a ∈ x.support, acc (rᶜ ⊓ (≠)) a) : acc (finsupp.lex r s) x :=
 begin
   rw lex_eq_inv_image_dfinsupp_lex,
-  refine inv_image.accessible finsupp.to_dfinsupp (dfinsupp.lex.acc r _ (λ i, hbot) (λ i, hs) _ _),
+  refine inv_image.accessible finsupp.to_dfinsupp (dfinsupp.lex.acc r _ (λ a, hbot) (λ a, hs) _ _),
   simpa only [to_dfinsupp_support] using h,
 end
 
 theorem lex.well_founded (hr : well_founded $ rᶜ ⊓ (≠)) : well_founded (finsupp.lex r s) :=
-⟨λ x, lex.acc r s hbot hs x $ λ i _, hr.apply i⟩
+⟨λ x, lex.acc r s hbot hs x $ λ a _, hr.apply a⟩
 
-theorem lex.well_founded' [is_trichotomous ι r]
+theorem lex.well_founded' [is_trichotomous α r]
   (hr : well_founded r.swap) : well_founded (finsupp.lex r s) :=
 (lex_eq_inv_image_dfinsupp_lex r s).symm ▸
-  inv_image.wf _ (dfinsupp.lex.well_founded' r _ (λ i, hbot) (λ i, hs) hr)
+  inv_image.wf _ (dfinsupp.lex.well_founded' r _ (λ a, hbot) (λ a, hs) hr)
 
 omit hbot hs
 
-instance lex.well_founded_lt [has_lt ι] [is_trichotomous ι (<)] [hι : well_founded_gt ι]
-  [canonically_ordered_add_monoid α] [hα : well_founded_lt α] : well_founded_lt (lex (ι →₀ α)) :=
-⟨lex.well_founded' (<) (<) (λ a, (zero_le a).not_lt) hα.wf hι.wf⟩
+instance lex.well_founded_lt [has_lt α] [is_trichotomous α (<)] [hι : well_founded_gt ι]
+  [canonically_ordered_add_monoid N] [hN : well_founded_lt N] : well_founded_lt (lex (α →₀ N)) :=
+⟨lex.well_founded' (<) (<) (λ n, (zero_le n).not_lt) hN.wf hα.wf⟩
 
 /- product order -/
 

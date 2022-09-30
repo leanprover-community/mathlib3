7c6028979548d40cdad78b4f7da29718544eba85
Junyan Xu <junyanxumath@gmail.com>
2022-09-30 12:34:18 -0400

    port ne_locus, authors

--- src/data/dfinsupp/lex.lean
+++ src/data/dfinsupp/lex.lean
@@ -1,7 +1,7 @@
 /-
-Copyright (c) 2022 Junyan Xu. All rights reserved.
+Copyright (c) 2022 Damiano Testa and Junyan Xu. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
-Authors: Junyan Xu
+Authors: Damiano Testa, Junyan Xu
 -/
 import data.dfinsupp.order
 import order.game_add

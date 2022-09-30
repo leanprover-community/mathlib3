e61b5990c54dd512155556718094629340d8a210
Matthias Uschold <matthias.uschold@mathematik.uni-regensburg.de>
2022-09-30 15:16:14 +0200

    adapt to style guideline

--- src/group_theory/amenable/quotient.lean
+++ src/group_theory/amenable/quotient.lean
@@ -38,7 +38,6 @@ open function
 
 variables {G:Type*} [topological_space G] [group G] [topological_group G]
           {H:Type*} [topological_space H] [group H] [topological_group H]
-
 (π: G →* H)
 (pi_surj : surjective π)
 (pi_cont : continuous π)
@@ -50,27 +49,23 @@ open mean
 
 /--The pushforward mean is left-invariant if the
 map is surjective-/
-lemma mean_pushforward_leftinv
-  (pi_surj : surjective π)
-  (m : left_invariant_mean G)
-  : ∀(h:H), ∀(f: bounded_continuous_function H ℝ),
-        (mean_pushforward π pi_cont m) (left_translate h f)
-      = (mean_pushforward π pi_cont m) f
-:= begin
+lemma mean_pushforward_leftinv (pi_surj : surjective π) (m : left_invariant_mean G) :
+  ∀(h:H), ∀(f: bounded_continuous_function H ℝ),
+    (mean_pushforward π pi_cont m) (left_translate h f)
+  = (mean_pushforward π pi_cont m) f :=
+begin
   assume h : H,
   assume f : bounded_continuous_function H ℝ,
 
   -- h has a preimage under π
-  have : ∃ (g:G), π g = h
-        := by tauto,
+  have : ∃ (g:G), π g = h := by tauto,
   rcases this with ⟨g, pi_gh⟩,
 
   --main step: The pullback of (left_translate h f)
   -- is the left_translate of (via g) the pullback of f
-  have translate_pullback:
-      pull_bcont π pi_cont (left_translate h f) = left_translate g (pull_bcont π pi_cont f),
-  {
-    ext (x:G),
+  have translate_pullback:  pull_bcont π pi_cont (left_translate h f) =
+                            left_translate g (pull_bcont π pi_cont f),
+  { ext (x:G),
     calc  pull_bcont π pi_cont (left_translate h f) x
         = (left_translate h f) (π x)
           : by tauto
@@ -85,8 +80,7 @@ lemma mean_pushforward_leftinv
     ... = (pull_bcont π pi_cont f) (g⁻¹ * x)
           : by tauto
     ... = (left_translate g (pull_bcont π pi_cont f)) x
-          : by tauto,
-  },
+          : by tauto, },
 
   calc  (mean_pushforward π pi_cont m) (left_translate h f)
       = m (pull_bcont π pi_cont (left_translate h f))
@@ -101,71 +95,55 @@ end
 
 /-- pushforward invariant mean-/
 @[simp]
-noncomputable def inv_mean_pushforward
-  (pi_surj : surjective π)
-  (m : left_invariant_mean G)
-  : left_invariant_mean H
-:= left_invariant_mean.mk (mean_pushforward π pi_cont  m)
-                  (mean_pushforward_leftinv π pi_cont pi_surj m)
+noncomputable def inv_mean_pushforward (pi_surj : surjective π) (m : left_invariant_mean G) :
+  left_invariant_mean H :=
+{ to_mean         := mean_pushforward π pi_cont m,
+  left_invariance := mean_pushforward_leftinv π pi_cont pi_surj m}
 
 
 end amenable_quotient
 
 
 /--The target group is amenable if π is surjective -/
-theorem amenable_of_quotient'
-  (pi_surj : surjective π)
-  (G_am: amenable G)
-  : amenable H
-:= amenable_of_invmean (amenable_quotient.inv_mean_pushforward π pi_cont pi_surj(invmean_of_amenable G_am) )
+theorem amenable_of_quotient' (pi_surj : surjective π) (G_am: amenable G) : amenable H :=
+amenable_of_invmean
+  (amenable_quotient.inv_mean_pushforward π pi_cont pi_surj (invmean_of_amenable G_am))
 
 
 /--Formulation with quotients, using the quotient topology on G⧸N-/
-theorem amenable_of_quotient
-  {N : subgroup G} (nN : N.normal)
-  (G_am: amenable G)
-  : amenable (G ⧸ N)
-:= amenable_of_quotient' _ (by exact continuous_coinduced_rng) (quotient_group.mk'_surjective N) G_am
+theorem amenable_of_quotient {N : subgroup G} (nN : N.normal) (G_am: amenable G) :
+  amenable (G ⧸ N) := amenable_of_quotient' _ (by exact continuous_coinduced_rng)
+    (quotient_group.mk'_surjective N) G_am
 
 
 
 -- preparations for amenable of iso
 
-
-
-
 /--a multiplicative homomorphism between groups is a monoid homomorphism-/
-def monoidhom_of_mulhom
-  (f: mul_hom G H)
-  : G →* H
-:= monoid_hom.mk f.to_fun
-      (begin
-        have : f.to_fun 1 * f.to_fun 1 = f.to_fun 1,
-        {
-          calc  f.to_fun 1 * f.to_fun 1
-              = f.to_fun (1*1)
-                : by rw f.map_mul'
-          ... = f.to_fun 1
-                : by congr'; by group,
-        },
-        by finish,
-      end)
-    f.map_mul'
+def monoidhom_of_mulhom (f: mul_hom G H) : G →* H :=
+{ to_fun  := f.to_fun,
+  map_one' :=
+              (begin
+                have : f.to_fun 1 * f.to_fun 1 = f.to_fun 1,
+                {
+                  calc  f.to_fun 1 * f.to_fun 1
+                      = f.to_fun (1*1)
+                        : by rw f.map_mul'
+                  ... = f.to_fun 1
+                        : by congr'; by group,
+                },
+                by finish,
+              end),
+  map_mul' := f.map_mul' }
 
 @[simp]
-lemma monoidhom_of_mulhom_to_fun
-   {f: mul_hom G H}
-  : (monoidhom_of_mulhom f).to_fun = f.to_fun
-:= by refl
+lemma monoidhom_of_mulhom_to_fun {f: mul_hom G H} :
+  (monoidhom_of_mulhom f).to_fun = f.to_fun := by refl
 
 /--Amenability is preserved under (multiplicative) isomorphisms-/
-theorem amenable_of_iso
-  {H:Type*} [topological_space H] [group H] [topological_group H]
-  (i : G ≃* H)
-  (i_cont : continuous i.to_mul_hom)
-  (G_am : amenable G)
-  : amenable H
-:= begin
+theorem amenable_of_iso   {H:Type*} [topological_space H] [group H] [topological_group H]
+  (i : G ≃* H) (i_cont : continuous i.to_mul_hom) (G_am : amenable G) : amenable H :=
+begin
   -- we obtain a surjective group hom G →* H
   let p: G →* H := monoidhom_of_mulhom i.to_mul_hom,
   have p_surj : surjective p,

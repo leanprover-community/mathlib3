019c07e273ef94999c641a56a222239ebbe614c5
Junyan Xu <junyanxumath@gmail.com>
2022-09-30 21:06:26 -0400

    feat(data/pi/lex): generalize from partial_order to linear_order

--- src/data/pi/lex.lean
+++ src/data/pi/lex.lean
@@ -86,29 +86,31 @@ noncomputable instance [linear_order ι] [is_well_order ι (<)] [∀ a, linear_o
 @linear_order_of_STO (Πₗ i, β i) (<)
   { to_is_trichotomous := is_trichotomous_lex _ _ is_well_founded.wf } (classical.dec_rel _)
 
-lemma lex.le_of_forall_le [linear_order ι] [is_well_order ι (<)] [Π a, linear_order (β a)]
+lemma lex.le_of_forall_le [linear_order ι] [hwf : is_well_order ι (<)] [Π a, partial_order (β a)]
   {a b : lex (Π i, β i)} (h : ∀ i, a i ≤ b i) : a ≤ b :=
-le_of_not_lt (λ ⟨i, hi⟩, (h i).not_lt hi.2)
+or_iff_not_imp_left.2 $ λ he,
+  let ⟨i, hi, hl⟩ := hwf.to_is_well_founded.wf.has_min {i | a i ≠ b i} (function.ne_iff.1 he) in
+  ⟨i, λ j hj, by { contrapose! hl, exact ⟨j, hl, hj⟩ }, (h i).lt_of_ne hi⟩
 
-lemma lex.le_of_of_lex_le [linear_order ι] [is_well_order ι (<)] [Π a, linear_order (β a)]
+lemma lex.le_of_of_lex_le [linear_order ι] [is_well_order ι (<)] [Π a, partial_order (β a)]
   {a b : lex (Π i, β i)} (h : of_lex a ≤ of_lex b) : a ≤ b :=
 lex.le_of_forall_le h
 
-lemma to_lex_monotone [linear_order ι] [is_well_order ι (<)] [Π a, linear_order (β a)] :
+lemma to_lex_monotone [linear_order ι] [is_well_order ι (<)] [Π a, partial_order (β a)] :
   monotone (@to_lex (Π i, β i)) :=
 λ _ _, lex.le_of_forall_le
 
-instance [linear_order ι] [is_well_order ι (<)] [Π a, linear_order (β a)]
+instance [linear_order ι] [is_well_order ι (<)] [Π a, partial_order (β a)]
   [Π a, order_bot (β a)] : order_bot (lex (Π a, β a)) :=
 { bot := to_lex ⊥,
   bot_le := λ f, lex.le_of_of_lex_le bot_le }
 
-instance [linear_order ι] [is_well_order ι (<)] [Π a, linear_order (β a)]
+instance [linear_order ι] [is_well_order ι (<)] [Π a, partial_order (β a)]
   [Π a, order_top (β a)] : order_top (lex (Π a, β a)) :=
 { top := to_lex ⊤,
   le_top := λ f, lex.le_of_of_lex_le le_top }
 
-instance [linear_order ι] [is_well_order ι (<)] [Π a, linear_order (β a)]
+instance [linear_order ι] [is_well_order ι (<)] [Π a, partial_order (β a)]
   [Π a, bounded_order (β a)] : bounded_order (lex (Π a, β a)) :=
 { .. pi.lex.order_bot, .. pi.lex.order_top }
 

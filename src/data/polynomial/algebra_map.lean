13eb762874bfe8abffbd265d1a5aebb1bf6be71f
Riccardo Brasca <riccardo.brasca@gmail.com>
2022-10-02 19:16:18 +0200

    let's keep this for the moment

--- src/data/polynomial/algebra_map.lean
+++ src/data/polynomial/algebra_map.lean
@@ -228,6 +228,13 @@ theorem aeval_alg_hom_apply (f : A →ₐ[R] B) (x : A) (p : R[X]) :
   aeval (f x) p = f (aeval x p) :=
 aeval_alg_hom_class_apply _ _ _
 
+theorem aeval_alg_equiv (f : A ≃ₐ[R] B) (x : A) : aeval (f x) = (f : A →ₐ[R] B).comp (aeval x) :=
+aeval_alg_hom (f : A →ₐ[R] B) x
+
+theorem aeval_alg_equiv_apply (f : A ≃ₐ[R] B) (x : A) (p : R[X]) :
+  aeval (f x) p = f (aeval x p) :=
+aeval_alg_hom_class_apply _ _ _
+
 lemma aeval_algebra_map_apply_eq_algebra_map_eval (x : R) (p : R[X]) :
   aeval (algebra_map R A x) p = algebra_map R A (p.eval x) :=
 aeval_alg_hom_apply (algebra.of_id R A) x p

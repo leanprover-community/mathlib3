e9469c7c7a4bf305e9c84ebf9d0da743e58ca9f6
Riccardo Brasca <riccardo.brasca@gmail.com>
2022-10-02 19:00:04 +0200

    add is_integral_alg_hom_class

--- src/ring_theory/integral_closure.lean
+++ src/ring_theory/integral_closure.lean
@@ -114,15 +114,20 @@ variables {R A B S : Type*}
 variables [comm_ring R] [comm_ring A] [comm_ring B] [comm_ring S]
 variables [algebra R A] [algebra R B] (f : R →+* S)
 
-lemma is_integral_alg_hom {C : Type*} [comm_ring C] [algebra A B] [algebra R C]
-  [is_scalar_tower R A B] [algebra A C] [is_scalar_tower R A C] {b : B}
-  (f : B →ₐ[A] C) (hb : is_integral R b) : is_integral R (f b) :=
+lemma is_integral_alg_hom_class {C F : Type*} [comm_ring C] [algebra A B] [algebra R C]
+  [is_scalar_tower R A B] [algebra A C] [is_scalar_tower R A C] {b : B} [alg_hom_class F A B C]
+  (f : F) (hb : is_integral R b) : is_integral R (f b) :=
 begin
   obtain ⟨P, hP⟩ := hb,
   refine ⟨P, hP.1, _⟩,
   rw [← aeval_def, show (aeval (f b)) P = (aeval (f b)) (P.map (algebra_map R A)), by simp,
     aeval_alg_hom_apply, aeval_map_algebra_map, aeval_def, hP.2, _root_.map_zero]
 end
+
+lemma is_integral_alg_hom {C : Type*} [comm_ring C] [algebra A B] [algebra R C]
+  [is_scalar_tower R A B] [algebra A C] [is_scalar_tower R A C] {b : B} (f : B →ₐ[A] C)
+  (hb : is_integral R b) : is_integral R (f b) :=
+is_integral_alg_hom_class f hb
 
 @[simp]
 theorem is_integral_alg_equiv (f : A ≃ₐ[R] B) {x : A} : is_integral R (f x) ↔ is_integral R x :=
@@ -134,9 +139,10 @@ let ⟨p, hp, hpx⟩ := hx in
 ⟨p.map $ algebra_map R A, hp.map _,
   by rw [← aeval_def, aeval_map_algebra_map, aeval_def, hpx]⟩
 
-lemma is_integral_int_alg_hom [algebra A B] {b : B} (f : B →ₐ[A] B) (hb : is_integral ℤ b) :
+lemma is_integral_int_alg_hom_class {C F : Type*} [comm_ring C] [algebra A B] [algebra A C] {b : B}
+  [alg_hom_class F A B C] (f : F) (hb : is_integral ℤ b) :
   is_integral ℤ (f b) :=
-is_integral_alg_hom f hb
+is_integral_alg_hom_class f hb
 
 theorem is_integral_of_subring {x : A} (T : subring R)
   (hx : is_integral T x) : is_integral R x :=

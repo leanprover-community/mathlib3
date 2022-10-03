8cd0a47bfb69711be0da102085a800aeaacc3a8f
Kevin Buzzard <k.buzzard@imperial.ac.uk>
2022-10-03 19:26:30 +0100

    change joke priority 37 to the more sensible 900

--- src/algebra/algebra/basic.lean
+++ src/algebra/algebra/basic.lean
@@ -129,7 +129,7 @@ namespace algebra_map
 def has_lift_t (R A : Type*) [comm_semiring R] [semiring A] [algebra R A] :
   has_lift_t R A := ⟨λ r, algebra_map R A r⟩
 
-attribute [instance, priority 37] algebra_map.has_lift_t
+attribute [instance, priority 900] algebra_map.has_lift_t
 
 section comm_semiring_semiring
 

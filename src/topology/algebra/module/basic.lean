92f273c16c2200e99b8e83ec5bfb59cd86c508f5
ADedecker <anatolededecker@gmail.com>
2022-10-03 18:34:04 +0200

    No import

--- src/topology/algebra/module/basic.lean
+++ src/topology/algebra/module/basic.lean
@@ -12,7 +12,6 @@ import algebra.algebra.basic
 import linear_algebra.projection
 import linear_algebra.pi
 import linear_algebra.determinant
-import ring_theory.simple_module
 
 /-!
 # Theory of topological modules and continuous linear maps.

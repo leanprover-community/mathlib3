839a1be700014e9c7f27a3a00fe0682b60753ed1
Heather Macbeth <25316162+hrmacbeth@users.noreply.github.com>
2022-10-03 21:21:08 -0400

    fix bad merge

--- src/topology/algebra/const_mul_action.lean
+++ src/topology/algebra/const_mul_action.lean
@@ -356,12 +356,8 @@ export properly_discontinuous_vadd (finite_disjoint_inter_image)
 
 /-- The quotient map by a group action is open, i.e. the quotient by a group action is an open
   quotient. -/
-<<<<<<< HEAD
-@[to_additive "The quotient map by a group action is open."]
-=======
 @[to_additive "The quotient map by a group action is open, i.e. the quotient by a group
 action is an open quotient. "]
->>>>>>> RmodZ_topology
 lemma is_open_map_quotient_mk_mul [has_continuous_const_smul Γ T] :
   is_open_map (quotient.mk : T → quotient (mul_action.orbit_rel Γ T)) :=
 begin

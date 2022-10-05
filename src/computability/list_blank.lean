3e9d62d981355bd4e668b981064dcaa1b407d733
Bolton Bailey <bolton.bailey@gmail.com>
2022-10-05 11:16:04 -0700

    delete extra line

--- src/computability/list_blank.lean
+++ src/computability/list_blank.lean
@@ -4,7 +4,6 @@ Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Mario Carneiro, Bolton Bailey
 -/
 
-
 import data.fintype.basic
 import data.pfun
 import logic.function.iterate

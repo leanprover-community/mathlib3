d5799782578501e7e4a0df6abd0fe8ad3645b13d
Floris van Doorn <fpvdoorn@gmail.com>
2022-10-06 12:04:34 +0200

    undo

--- src/measure_theory/group/measure.lean
+++ src/measure_theory/group/measure.lean
@@ -315,7 +315,7 @@ lemma map_mul_right_inv_eq_self (μ : measure G) [is_inv_invariant μ] [is_mul_l
 @[to_additive]
 lemma map_div_left_ae (μ : measure G) [is_mul_left_invariant μ] [is_inv_invariant μ] (x : G) :
   filter.map (λ t, x / t) μ.ae = μ.ae :=
-(measure_preserving_div_left μ x).ae_eq
+((measurable_equiv.div_left x).map_ae μ).trans $ congr_arg ae $ map_div_left_eq_self μ x
 
 end mul_inv
 

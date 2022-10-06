5b0dd140dc3b854e9eb25e5124d5d5476e9c3d1c
Yaël Dillies <yael.dillies@gmail.com>
2022-10-06 17:50:20 +0000

    feat(analysis/normed/group/basic): Multiplicative, noncommutative normed groups (#15705)
    Define
    * `seminormed_group`
    * `seminormed_add_group`
    * `seminormed_comm_group`
    * `normed_group`
    * `normed_add_group`
    * `normed_comm_group`
    
    Multiplicativize all lemmas in `analysis.normed.group.basic`. To disambiguate names, I add one or two `'` to the multiplicative version where needed.

--- src/analysis/inner_product_space/basic.lean
+++ src/analysis/inner_product_space/basic.lean
@@ -1821,7 +1821,8 @@ end bessels_inequality
 
 /-- A field `𝕜` satisfying `is_R_or_C` is itself a `𝕜`-inner product space. -/
 instance is_R_or_C.inner_product_space : inner_product_space 𝕜 𝕜 :=
-{ inner := (λ x y, (conj x) * y),
+{ to_normed_add_comm_group := non_unital_normed_ring.to_normed_add_comm_group,
+  inner := λ x y, conj x * y,
   norm_sq_eq_inner := λ x,
     by { unfold inner, rw [mul_comm, mul_conj, of_real_re, norm_sq_eq_def'] },
   conj_sym := λ x y, by simp [mul_comm],
@@ -1834,7 +1835,8 @@ instance is_R_or_C.inner_product_space : inner_product_space 𝕜 𝕜 :=
 
 /-- Induced inner product on a submodule. -/
 instance submodule.inner_product_space (W : submodule 𝕜 E) : inner_product_space 𝕜 W :=
-{ inner             := λ x y, ⟪(x:E), (y:E)⟫,
+{ to_normed_add_comm_group := submodule.normed_add_comm_group _,
+  inner             := λ x y, ⟪(x:E), (y:E)⟫,
   conj_sym          := λ _ _, inner_conj_sym _ _ ,
   norm_sq_eq_inner  := λ _, norm_sq_eq_inner _,
   add_left          := λ _ _ _ , inner_add_left,
@@ -2088,7 +2090,8 @@ registered as an instance since it creates problems with the case `𝕜 = ℝ`, 
 proof to obtain a real inner product space structure from a given `𝕜`-inner product space
 structure. -/
 def inner_product_space.is_R_or_C_to_real : inner_product_space ℝ E :=
-{ norm_sq_eq_inner := norm_sq_eq_inner,
+{ to_normed_add_comm_group := inner_product_space.to_normed_add_comm_group 𝕜,
+  norm_sq_eq_inner := norm_sq_eq_inner,
   conj_sym := λ x y, inner_re_symm,
   add_left := λ x y z, by
   { change re ⟪x + y, z⟫ = re ⟪x, z⟫ + re ⟪y, z⟫,
@@ -2376,7 +2379,8 @@ protected lemma continuous.inner {α : Type*} [topological_space α]
 uniform_space.completion.continuous_inner.comp (hf.prod_mk hg : _)
 
 instance : inner_product_space 𝕜 (completion E) :=
-{ norm_sq_eq_inner := λ x, completion.induction_on x
+{ to_normed_add_comm_group := infer_instance,
+  norm_sq_eq_inner := λ x, completion.induction_on x
     (is_closed_eq
       (continuous_norm.pow 2)
       (continuous_re.comp (continuous.inner continuous_id' continuous_id')))

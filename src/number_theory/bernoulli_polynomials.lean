2d5ab30ee1821a14109c3ff693143c074ca2eb85
Vierkantor <vierkantor@vierkantor.com>
2022-10-06 09:37:05 +0000

    Fix missing names (apparently there's now a name conflict with `power_series`?)

--- src/number_theory/bernoulli_polynomials.lean
+++ src/number_theory/bernoulli_polynomials.lean
@@ -190,7 +190,7 @@ begin
   simp_rw [smul_eq_mul],
 end
 
-open power_series
+open _root_.power_series
 variables {A : Type*} [comm_ring A] [algebra ℚ A]
 
 -- TODO: define exponential generating functions, and use them here

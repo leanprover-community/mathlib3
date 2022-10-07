f0098dbd93d59ed89bef55ffcec6a1196dbadb67
Jireh Loreaux <loreaujy@gmail.com>
2022-10-07 09:56:20 -0500

    more progress

--- src/algebra/star/subalgebra.lean
+++ src/algebra/star/subalgebra.lean
@@ -3,7 +3,7 @@ Copyright (c) 2022 Scott Morrison. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Scott Morrison
 -/
-import algebra.star.basic
+import algebra.star.star_alg_hom
 import algebra.algebra.subalgebra.basic
 
 /-!
@@ -14,9 +14,28 @@ A *-subalgebra is a subalgebra of a *-algebra which is closed under *.
 The centralizer of a *-closed set is a *-subalgebra.
 -/
 
-universes u v
+universes u v w
 
 set_option old_structure_cmd true
+
+/-- `star_mem_class S G` states `S` is a type of subsets `s ⊆ G` closed under negation. -/
+class star_mem_class (S R : Type*) [has_star R] [set_like S R] :=
+(star_mem : ∀ {s : S} {r : R}, r ∈ s → star r ∈ s)
+
+export star_mem_class (star_mem)
+
+namespace star_mem_class
+
+variables {S R : Type u} [has_star R] [set_like S R] [hS : star_mem_class S R] (s : S)
+include hS
+
+instance : has_star s :=
+{ star := λ r, ⟨star (r : R), star_mem r.prop⟩ }
+
+end star_mem_class
+
+-- we really want a `star_subalgebra_class`, but we're several steps away from that yet,
+-- we don't even have `submodule_class`.
 
 /-- A *-subalgebra is a subalgebra of a *-algebra which is closed under *. -/
 structure star_subalgebra (R : Type u) (A : Type v) [comm_semiring R] [star_ring R]
@@ -30,8 +49,10 @@ Forgetting that a *-subalgebra is closed under *.
 -/
 add_decl_doc star_subalgebra.to_subalgebra
 
-variables (R : Type u) (A : Type v) [comm_semiring R] [star_ring R]
-  [semiring A] [star_ring A] [algebra R A] [star_module R A]
+variables {R A B C : Type*} [comm_semiring R] [star_ring R]
+variables [semiring A] [star_ring A] [algebra R A] [star_module R A]
+variables [semiring B] [star_ring B] [algebra R B] [star_module R B]
+variables [semiring C] [star_ring C] [algebra R C] [star_module R C]
 
 instance : set_like (star_subalgebra R A) A :=
 ⟨star_subalgebra.carrier, λ p q h, by cases p; cases q; congr'⟩
@@ -41,11 +62,26 @@ instance : has_top (star_subalgebra R A) :=
 
 instance : inhabited (star_subalgebra R A) := ⟨⊤⟩
 
+instance : star_mem_class (star_subalgebra R A) A :=
+{ star_mem := λ s a, s.star_mem' }
+
 instance : subsemiring_class (star_subalgebra R A) A :=
 { add_mem := add_mem',
   mul_mem := mul_mem',
   one_mem := one_mem',
   zero_mem := zero_mem' }
+
+-- this uses the `has_star` instance `s` inherits from `star_mem_class (star_subalgebra R A) A`
+instance (s : star_subalgebra R A) : star_ring s :=
+{ star := star,
+  star_involutive := λ r, subtype.ext (star_star r),
+  star_mul := λ r₁ r₂, subtype.ext (star_mul r₁ r₂),
+  star_add := λ r₁ r₂, subtype.ext (star_add r₁ r₂) }
+
+instance (s : star_subalgebra R A) : algebra R s := s.to_subalgebra.algebra'
+
+instance (s : star_subalgebra R A) : star_module R s :=
+{ star_smul := λ r a, subtype.ext (star_smul r a) }
 
 @[simp]
 lemma mem_carrier {s : star_subalgebra R A} {x : A} : x ∈ s.carrier ↔ x ∈ s := iff.rfl
@@ -60,12 +96,114 @@ iff.rfl
 
 theorem to_subalgebra_injective :
   function.injective (to_subalgebra : star_subalgebra R A → subalgebra R A) :=
-λ S T h, ext R S T _ --  by rw [← mem_to_subalgebra, ← mem_to_subalgebra, h]
+λ S T h, ext $ λ x, by rw [← mem_to_subalgebra, ← mem_to_subalgebra, h]
 
-theorem to_subsemiring_inj {S U : subalgebra R A} : S.to_subsemiring = U.to_subsemiring ↔ S = U :=
-to_subsemiring_injective.eq_iff
+theorem to_subalgebra_inj {S U : star_subalgebra R A} : S.to_subalgebra = U.to_subalgebra ↔ S = U :=
+to_subalgebra_injective.eq_iff
+
+/-- Copy of a star subalgebra with a new `carrier` equal to the old one. Useful to fix definitional
+equalities. -/
+protected def copy (S : star_subalgebra R A) (s : set A) (hs : s = ↑S) : star_subalgebra R A :=
+{ carrier := s,
+  add_mem' := λ _ _, hs.symm ▸ S.add_mem',
+  mul_mem' := λ _ _, hs.symm ▸ S.mul_mem',
+  algebra_map_mem' := hs.symm ▸ S.algebra_map_mem',
+  star_mem' := λ _, hs.symm ▸ S.star_mem' }
+
+@[simp] lemma coe_copy (S : star_subalgebra R A) (s : set A) (hs : s = ↑S) :
+  (S.copy s hs : set A) = s := rfl
+
+variables (S : star_subalgebra R A)
+
+theorem algebra_map_mem (r : R) : algebra_map R A r ∈ S :=
+S.algebra_map_mem' r
+
+theorem srange_le : (algebra_map R A).srange ≤ S.to_subalgebra.to_subsemiring :=
+λ x ⟨r, hr⟩, hr ▸ S.algebra_map_mem r
+
+theorem range_subset : set.range (algebra_map R A) ⊆ S :=
+λ x ⟨r, hr⟩, hr ▸ S.algebra_map_mem r
+
+theorem range_le : set.range (algebra_map R A) ≤ S :=
+S.range_subset
+
+protected theorem smul_mem {x : A} (hx : x ∈ S) (r : R) : r • x ∈ S :=
+(algebra.smul_def r x).symm ▸ mul_mem (S.algebra_map_mem r) hx
+
+/-- Embedding of a subalgebra into the algebra. -/
+def subtype : S →⋆ₐ[R] A :=
+by refine_struct { to_fun := (coe : S → A) }; intros; refl
+
+@[simp] lemma coe_subtype : (S.subtype : S → A) = coe := rfl
+
+lemma subtype_apply (x : S) : S.subtype x = (x : A) := rfl
+
+@[simp] lemma to_subalgebra_subtype : S.to_subalgebra.val = S.subtype.to_alg_hom :=
+rfl
+
+section map
+
+/-- Transport a star subalgebra via a star algebra homomorphism. -/
+def map (f : A →⋆ₐ[R] B) (S : star_subalgebra R A) : star_subalgebra R B :=
+{ star_mem' :=
+  begin
+    rintro _ ⟨a, ha, rfl⟩,
+    exact map_star f a ▸ set.mem_image_of_mem _ (S.star_mem' ha),
+  end,
+  .. S.to_subalgebra.map f.to_alg_hom }
+
+lemma map_mono {S₁ S₂ : star_subalgebra R A} {f : A →⋆ₐ[R] B} :
+  S₁ ≤ S₂ → S₁.map f ≤ S₂.map f :=
+set.image_subset f
+
+lemma map_injective {f : A →⋆ₐ[R] B} (hf : function.injective f) :
+  function.injective (map f) :=
+λ S₁ S₂ ih, ext $ set.ext_iff.1 $ set.image_injective.2 hf $ set.ext $ set_like.ext_iff.mp ih
+
+@[simp] lemma map_id (S : star_subalgebra R A) : S.map (star_alg_hom.id R A) = S :=
+set_like.coe_injective $ set.image_id _
+
+lemma map_map (S : star_subalgebra R A) (g : B →⋆ₐ[R] C) (f : A →⋆ₐ[R] B) :
+  (S.map f).map g = S.map (g.comp f) :=
+set_like.coe_injective $ set.image_image _ _ _
+
+lemma mem_map {S : star_subalgebra R A} {f : A →⋆ₐ[R] B} {y : B} :
+  y ∈ map f S ↔ ∃ x ∈ S, f x = y :=
+subsemiring.mem_map
+
+lemma map_to_subalgebra {S : star_subalgebra R A} {f : A →⋆ₐ[R] B} :
+  (S.map f).to_subalgebra = S.to_subalgebra.map f.to_alg_hom :=
+set_like.coe_injective rfl
+
+@[simp] lemma coe_map (S : star_subalgebra R A) (f : A →⋆ₐ[R] B) :
+  (S.map f : set B) = f '' S :=
+rfl
+
+/-- Preimage of a star subalgebra under an star algebra homomorphism. -/
+def comap (f : A →⋆ₐ[R] B) (S : star_subalgebra R B) : star_subalgebra R A :=
+{ star_mem' := λ a ha, show f (star a) ∈ S, from (map_star f a).symm ▸ star_mem ha,
+  .. S.to_subalgebra.comap f.to_alg_hom }
+
+theorem map_le {S : star_subalgebra R A} {f : A →⋆ₐ[R] B} {U : star_subalgebra R B} :
+  map f S ≤ U ↔ S ≤ comap f U :=
+set.image_subset_iff
+
+lemma gc_map_comap (f : A →⋆ₐ[R] B) : galois_connection (map f) (comap f) :=
+λ S U, map_le
+
+@[simp] lemma mem_comap (S : star_subalgebra R B) (f : A →⋆ₐ[R] B) (x : A) :
+  x ∈ S.comap f ↔ f x ∈ S :=
+iff.rfl
+
+@[simp, norm_cast] lemma coe_comap (S : star_subalgebra R B) (f : A →⋆ₐ[R] B) :
+  (S.comap f : set A) = f ⁻¹' (S : set B) :=
+rfl
+
+
+end map
+
 section centralizer
-variables {A}
+variables (R) {A}
 
 /-- The centralizer, or commutant, of a *-closed set as star subalgebra. -/
 def centralizer

384f93a594c69da97a775530a374b5338e53472a
Scott Morrison <scott.morrison@gmail.com>
2022-10-08 20:43:49 +1100

    chore(algebra/ring/basic): move results on dvd

--- src/algebra/hom/ring.lean
+++ src/algebra/hom/ring.lean
@@ -3,7 +3,7 @@ Copyright (c) 2019 Amelia Livingston. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Amelia Livingston, Jireh Loreaux
 -/
-import algebra.ring.basic
+import algebra.ring.divisibility
 
 /-!
 # Homomorphisms of semirings and rings

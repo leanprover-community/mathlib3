00852d763c9ab41ef2ebd4fad0c55cf617a3fa77
Scott Morrison <scott.morrison@gmail.com>
2022-10-08 20:37:12 +1100

    chore(algebra/ring/basic): postpone unneeded imports

--- src/algebra/hom/ring.lean
+++ src/algebra/hom/ring.lean
@@ -4,6 +4,7 @@ Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Amelia Livingston, Jireh Loreaux
 -/
 import algebra.ring.basic
+import data.pi.algebra
 
 /-!
 # Homomorphisms of semirings and rings

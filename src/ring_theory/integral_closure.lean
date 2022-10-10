ffe00cac8369f029259778c8444cc2e09e7076b3
riccardobrasca <riccardo.brasca@gmail.com>
2022-10-10 18:39:15 +0300

    fix build

--- src/ring_theory/integral_closure.lean
+++ src/ring_theory/integral_closure.lean
@@ -114,9 +114,9 @@ variables {R A B S : Type*}
 variables [comm_ring R] [comm_ring A] [comm_ring B] [comm_ring S]
 variables [algebra R A] [algebra R B] (f : R →+* S)
 
-lemma is_integral_alg_hom {C F : Type*} [comm_ring C] [algebra A B] [algebra R C]
-  [is_scalar_tower R A B] [algebra A C] [is_scalar_tower R A C] {b : B} [alg_hom_class F A B C]
-  (f : F) (hb : is_integral R b) : is_integral R (f b) :=
+lemma is_integral_alg_hom {B C F : Type*} [ring B] [ring C] [algebra R B] [algebra A B]
+  [algebra R C] [is_scalar_tower R A B] [algebra A C] [is_scalar_tower R A C] {b : B}
+  [alg_hom_class F A B C] (f : F) (hb : is_integral R b) : is_integral R (f b) :=
 begin
   obtain ⟨P, hP⟩ := hb,
   refine ⟨P, hP.1, _⟩,

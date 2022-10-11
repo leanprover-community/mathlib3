5de03a15ad6dd70fd11dc893ac1b2f613e9e0c84
Jujian Zhang <jujian.zhang1998@outlook.com>
2022-10-11 15:12:04 +0100

    add sheaf

--- src/topology/sheaves/construction/prod.lean
+++ src/topology/sheaves/construction/prod.lean
@@ -1,5 +1,15 @@
+/-
+Copyright (c) 2022 Jujian Zhang. All rights reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
+Authors: Jujian Zhang
+-/
 import topology.sheaves.limits
 import category_theory.limits.functor_category
+
+/-!
+# Products in category of (pre)sheaves
+-/
+
 
 noncomputable theory
 
@@ -15,10 +25,16 @@ variables {ι : Type u'} (f : ι → presheaf C X) [has_products.{u'} C]
 instance : has_products.{u'} (presheaf C X) :=
 λ J, category_theory.limits.functor_category_has_limits_of_shape
 
-def section_product_equiv_product_section
-  (U : (opens X)ᵒᵖ) : (∏ f).obj U ≅ ∏ (λ x, (f x).obj U) :=
+/--
+sections of product presheaf is product of sections of presheaves.
+-/
+def section_product_equiv_product_section (U : (opens X)ᵒᵖ) :
+  (∏ f).obj U ≅ ∏ (λ x, (f x).obj U) :=
 limit_obj_iso_limit_comp_evaluation _ _ ≪≫ limits.lim.map_iso (discrete.nat_iso $ λ X, iso.refl _)
 
+/--
+Let `f : ι → presheaf C X`, `product_presheaf'` is defined by `U ↦ ∏ᵢ (f i)(U)`
+-/
 @[simps] def product_presheaf' : presheaf C X :=
 { obj := λ U, ∏ (λ x, (f x).obj U),
   map := λ U V inc, pi.map $ λ i, (f i).map inc,
@@ -38,6 +54,9 @@ limit_obj_iso_limit_comp_evaluation _ _ ≪≫ limits.lim.map_iso (discrete.nat_
     rw [(f j.as).map_comp],
   end }
 
+/--
+The categorical product of presheaves agree with the concrete representation.
+-/
 def product_presheaf_iso_product_presheaf' :
   ∏ f ≅ product_presheaf' f :=
 nat_iso.of_components (λ U, section_product_equiv_product_section _ _) $
@@ -53,3 +72,63 @@ begin
 end
 
 end Top.presheaf
+
+namespace sheaf
+
+variables {X : Top.{w}} {C : Type v} [category.{w} C]
+variables {ι : Type w} (f : ι → sheaf C X) [has_limits.{w} C]
+
+/--
+let `f : ι → sheaf C X`, then the underlying presheaf of categorical product `∏ f` is the product
+of `∏ᵢ (f i).presheaf`
+-/
+def product_sheaf_iso_product_presheaf :
+  (∏ f) ≅ ⟨∏ (λ i, (f i).presheaf), Top.limit_is_sheaf _ $ λ ⟨j⟩, (f j).2⟩ :=
+let c : cone (discrete.functor f) :=
+{ X := ⟨∏ (λ i, (f i).presheaf), Top.limit_is_sheaf _ $ λ ⟨j⟩, (f j).2⟩,
+  π := discrete.nat_trans $ λ j, ⟨pi.π _ _⟩ },
+is_limit_c : is_limit c :=
+{ lift := λ s, ⟨pi.lift $ λ j, (s.π.app ⟨j⟩).1⟩,
+  fac' := λ s j,
+  begin
+    ext,
+    simp only [discrete.nat_trans_app, Sheaf.category_theory.category_comp_val, limit.lift_π,
+      fan.mk_π_app],
+    congr' 3,
+    ext,
+    refl,
+  end,
+  uniq' := λ s m h,
+  begin
+    ext,
+    dsimp only,
+    simp only [nat_trans.comp_app, limit.lift_π, fan.mk_π_app],
+    rw ←h,
+    simp only [discrete.nat_trans_app, Sheaf.category_theory.category_comp_val, nat_trans.comp_app],
+    congr,
+    ext,
+    refl,
+  end } in
+(is_limit.cone_point_unique_up_to_iso (product_is_product f) is_limit_c)
+
+/--
+sections of product sheaf is product of sections of sheaves
+-/
+def section_product_sheaf_equiv_product_section (U : (opens X)ᵒᵖ) :
+  (∏ f).presheaf.obj U ≅ ∏ (λ i, (f i).presheaf.obj U) :=
+{ hom := (product_sheaf_iso_product_presheaf f).hom.1.app U,
+  inv := (product_sheaf_iso_product_presheaf f).inv.1.app U,
+  hom_inv_id' :=
+  begin
+    change ((product_sheaf_iso_product_presheaf f).hom ≫
+      (product_sheaf_iso_product_presheaf f).inv).1.app U = _,
+    simp only [iso.hom_inv_id, Sheaf.category_theory.category_id_val, nat_trans.id_app],
+  end,
+  inv_hom_id' :=
+  begin
+    change ((product_sheaf_iso_product_presheaf f).inv ≫
+      (product_sheaf_iso_product_presheaf f).hom).1.app U = _,
+    simp only [iso.inv_hom_id, Sheaf.category_theory.category_id_val, nat_trans.id_app],
+  end } ≪≫ Top.presheaf.section_product_equiv_product_section _ _
+
+end sheaf

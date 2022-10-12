87516e7dda86026cf9f4ea6f954875b668f9995a
Yaël Dillies <yael.dillies@gmail.com>
2022-10-12 17:32:53 +0000

    some more

--- src/group_theory/geometric/marked_group.lean
+++ src/group_theory/geometric/marked_group.lean
@@ -1,9 +1,25 @@
 import .mathlib
-import .normed_group
 import .isom_action
 
 /-!
 # Marked groups
+
+## Comments by Sébastien Gouëzel
+
+If you want to register your group as a metric space (where the distance depends on `S`), you will
+need to embrace the type synonym trick. Instead of a class, define a structure `marking S G` as you
+did. And then given a group `G` and a marking `m`, define a new type `marked_group m G := G`. On
+this new type, you can register the same group structure as on `G`, but you can also register a
+distance as `m` is now available to the system when you consider `x y : marked_group m G`.
+
+First, work with normed groups, and prove whatever you like here. Possibly adding new typeclass
+assumptions that say that the distance is proper or hyperbolic or whatever.
+Then, to construct instances of such normed groups, do it on type synonyms. For instance, given two
+types `S` and `G` with `[group G]`, define `marking S G` as the space of markings of `G`
+parameterized by `S`. Then, given a group `G` and a marking `m`, define a type
+`marked_group G m := G` as a copy of `G`, then define on it the group structure coming from `G`
+(with `@[derive ...]`) and the norm associated to `m`. Then `marked_group G m` will be an instance
+of a normed group.
 -/
 
 set_option old_structure_cmd true
@@ -80,17 +96,15 @@ lemma aux [decidable_eq S] (x : marked m) :
   ∃ n (l : free_group S), m l = x ∧ l.to_word.length ≤ n :=
 by { classical, obtain ⟨l, rfl⟩ := m.map_surjective x, exact ⟨_, _, rfl, le_rfl⟩ }
 
-instance : normed_mul_group (marked m) :=
-group_norm.to_normed_mul_group _
+instance : normed_group (marked m) :=
+group_norm.to_normed_group
 { to_fun := λ x, by classical; exact nat.find (aux x),
   map_one' := cast_eq_zero.2 $ (find_eq_zero $ aux _).2 ⟨1, map_one _, le_rfl⟩,
-  nonneg' := λ _, cast_nonneg _,
   mul_le' := λ x y, begin
     norm_cast,
     obtain ⟨a, rfl, ha⟩ := nat.find_spec (aux x),
     obtain ⟨b, rfl, hb⟩ := nat.find_spec (aux y),
-    refine find_le ⟨a * b, map_mul _ _ _, _⟩,
-    refine (a.to_word_mul_sublist _).length.trans _,
+    refine find_le ⟨a * b, map_mul _ _ _, (a.to_word_mul_sublist _).length.trans _⟩,
     rw length_append,
     exact add_le_add ha hb,
   end,
@@ -99,105 +113,76 @@ group_norm.to_normed_mul_group _
     { refine λ _, congr_arg coe (this.antisymm _),
       convert this,
       simp_rw inv_inv },
-    refine λ x, find_mono (λ n, _),
+    refine λ x, find_mono (λ nI, _),
     rintro ⟨l, hl, h⟩,
     exact ⟨l⁻¹, by simp [hl], h.trans_eq' $ by simp⟩,
   end,
-  eq_one_of_to_fun := λ x hx, begin
+  eq_one_of_map_eq_zero' := λ x hx, begin
     obtain ⟨l, rfl, hl⟩ := (find_eq_zero $ aux _).1 (cast_eq_zero.1 hx),
-    rw [nat.le_zero_iff, length_eq_zero] at hl,
-    have hl2 : l.to_word = (1:free_group S).to_word := by tauto,
-    have hl3 := free_group.to_word.inj l 1 hl2,
-    finish
+    rw [le_zero_iff, length_eq_zero, ←free_group.to_word_one] at hl,
+    rw [free_group.to_word_injective hl, map_one],
   end }
 
 -- we need a lemma which translates between the general Type setting and the subset setting.
 
-
-lemma zero_norm_iff_one (g : marked m) : ∥g∥ = 0 ↔ g = 1 :=
-begin
-  sorry,
-end
-
-lemma dist_to_norm (g : marked m) : dist 1 g = ∥g∥ := sorry
-
-@[simp] lemma one_norm : ∥(1 : marked m)∥ = 0 := sorry
-
 lemma gen_norm_le_one (s : S) : 1 = ∥((to_marked (m (free_group.of s))) : marked m)∥ := sorry
 
-
-@[simp] lemma dist_inv (a g h : marked m) : dist (a⁻¹*g) h = dist g (a*h):= by
-{ rw [seminormed_mul_group.dist_eq, seminormed_mul_group.dist_eq],
-  apply congr_arg, group,}
-
-@[simp] lemma dist_inv' (a g h : marked m) : dist g (a⁻¹*h) = dist (a*g) h:= by
-{ rw [seminormed_mul_group.dist_eq, seminormed_mul_group.dist_eq],
-  apply congr_arg, group, }
-
-lemma gen_set_mul_right (x : marked m) (s : S)
-: ∥ (to_marked (of_marked x * m (free_group.of s)) : marked m) ∥ ≤ ∥x∥+1 :=
+lemma gen_set_mul_right (x : marked m) (s : S) :
+  ∥(to_marked (of_marked x * m (free_group.of s)) : marked m)∥ ≤ ∥x∥ + 1 :=
 sorry
 
-lemma gen_set_mul_right' (x : marked m) {n : ℝ} (hx : ∥x∥ ≤ n) (s : S)
-: ∥ (to_marked (of_marked x * m (free_group.of s)) : marked m) ∥ ≤ n+1 :=
+lemma gen_set_mul_right' (x : marked m) {n : ℝ} (hx : ∥x∥ ≤ n) (s : S) :
+  ∥(to_marked (of_marked x * m (free_group.of s)) : marked m)∥ ≤ n + 1 :=
 sorry
 
-
-lemma gen_set_mul_left (x : marked m) (s : S)
-: ∥ (to_marked ( m (free_group.of s) * of_marked x) : marked m) ∥ ≤ ∥x∥+1 :=
+lemma gen_set_mul_left (x : marked m) (s : S) :
+  ∥(to_marked ( m (free_group.of s) * of_marked x) : marked m)∥ ≤ ∥x∥ + 1 :=
 sorry
 
-lemma gen_set_mul_left' (x : marked m) {n : ℝ} (hx : ∥x∥ ≤ n) (s : S)
-: ∥ (to_marked ( m (free_group.of s) * of_marked x) : marked m) ∥ ≤ n+1 :=
+lemma gen_set_mul_left' (x : marked m) {n : ℝ} (hx : ∥x∥ ≤ n) (s : S) :
+  ∥(to_marked (m (free_group.of s) * of_marked x) : marked m) ∥ ≤ n + 1 :=
 sorry
 
 lemma dist_one_iff (x y : marked m) :
-dist x y = 1 ↔ (∃ s : S, x * m (free_group.of s) = y) ∨ ∃ s : S, y * m (free_group.of s) = x :=
+  dist x y = 1 ↔ (∃ s : S, x * m (free_group.of s) = y) ∨ ∃ s : S, y * m (free_group.of s) = x :=
 sorry
 
 lemma gen_set_div (x : marked m) (hx : x ≠ 1) : ∃ y : marked m, dist x y = 1 ∧ ∥y∥ = ∥x∥ - 1 :=
 sorry
 
 lemma gen_div_left (x : marked m) (hx : x ≠ 1) :
-  ∃ y : marked m, ((∃ s : S, (m (free_group.of s)) * y = x) ∨ (∃ s : S, m (free_group.of s)⁻¹ * y = x)) ∧ ∥y∥ = ∥x∥ - 1 :=
+  ∃ y : marked m,
+    ((∃ s : S, m (free_group.of s) * y = x) ∨ (∃ s : S, m (free_group.of s)⁻¹ * y = x)) ∧
+      ∥y∥ = ∥x∥ - 1 :=
 sorry
 
 -- same lemmas but for subsets
 
-lemma gen_norm_le_one_sub {H : set G}  {m' : marking G H} {s : marked m'} (sh : s ∈ H)
- : ∥s∥ ≤ 1 := sorry
+lemma gen_norm_le_one_sub {H : set G}  {m' : marking G H} {s : marked m'} (sh : s ∈ H) : ∥s∥ ≤ 1 :=
+sorry
 
-lemma gen_set_mul_right_sub {H : set G} {s : G} {m' : marking G H} (sh : s ∈ H) (g : marked m')
-: ∥g * s∥ ≤ ∥g∥+1 :=
-  sorry
+lemma gen_set_mul_right_sub {H : set G} {s : G} {m' : marking G H} (sh : s ∈ H) (g : marked m') :
+  ∥g * s∥ ≤ ∥g∥ + 1 :=
+sorry
 
-lemma gen_set_mul_right'_sub {H : set G} {s : G} {m' : marking G H} (sh : s ∈ H) (g : marked m') {n : ℝ}
-  (hg : ∥g∥ ≤ n) : ∥g * s∥ ≤ n+1 :=
-  sorry
+lemma gen_set_mul_right'_sub {H : set G} {s : G} {m' : marking G H} (sh : s ∈ H) (g : marked m')
+  {n : ℝ} (hg : ∥g∥ ≤ n) : ∥g * s∥ ≤ n + 1 :=
+sorry
 
-lemma gen_set_mul_left_sub {H : set G} {m' : marking G H} (g s: marked m') (sh : s ∈ H)
-: ∥s * g∥ ≤ ∥g∥+1 :=
-  sorry
+lemma gen_set_mul_left_sub {H : set G} {m' : marking G H} (g s: marked m') (sh : s ∈ H) :
+  ∥s * g∥ ≤ ∥g∥ + 1 :=
+sorry
 
 lemma gen_set_mul_left'_sub {H : set G} {m' : marking G H} (g s : marked m')  (sh : s ∈ H) {n : ℝ}
-  (hg : ∥g∥ ≤ n) : ∥s * g∥ ≤ n+1 :=
-  sorry
+  (hg : ∥g∥ ≤ n) : ∥s * g∥ ≤ n + 1 :=
+sorry
 
 lemma dist_one_iff_sub {H : set G} {m' : marking G H} (x y : marked m') :
-dist x y = 1 ↔ ((∃ s ∈ H, x * s = y) ∨ ∃ s ∈ H, y * s = x) :=
+  dist x y = 1 ↔ (∃ s ∈ H, x * s = y) ∨ ∃ s ∈ H, y * s = x :=
 sorry
 
 lemma gen_div_left_sub {H : set G} {m' : marking G H} (x : marked m') (hx : x ≠ 1) :
   ∃ y : marked m', ((∃ s ∈ H, s * y = x) ∨ (∃ s ∈ H, s⁻¹ * y = x)) ∧ ∥y∥ = ∥x∥ - 1 :=
 sorry
-
-/- comments by Sébastien Gouëzel:
-
-If you want to register your group as a metric space (where the distance depends on S), you will need to embrace the type synonym trick. Instead of a class, define a structure marking S G as you did. And then given a group G and a marking m, define a new type marked_group m G := G. On this new type, you can register the same group structure as on G, but you can also register a distance as m is now available to the system when you consider x y : marked_group m G.
-
-    First, work with normed groups, and prove whatever you like here. Possibly adding new typeclass assumptions that say that the distance is proper or hyperbolic or whatever.
-    Then, to construct instances of such normed groups, do it on type synonyms. For instance, given two types S and G with [group G], define marking S G as the space of markings of G parameterized by S. Then, given a group G and a marking m, define a typemarked_group G m := G as a copy of G, then define on it the group structure coming from G (with @[derive ...]) and the norm associated to m. Then marked_group G m will be an instance of a normed group.
-
--/
 
 end geometric_group_theory
--- src/group_theory/geometric/mathlib.lean
+++ src/group_theory/geometric/mathlib.lean
@@ -9,38 +9,24 @@ alias list.length_le_of_sublist ← list.sublist.length
 
 attribute [protected] list.sublist.length
 
+attribute [simp] free_group.to_word_inv
+
 namespace free_group
 variable {α : Type*}
 
 open list
 
-/-
-Similar to `free_group.of`, but includes generators as well as their inverses into the free group.
--/
-def of_gen (a : α) (sgn : bool) : free_group α := free_group.mk [(a, sgn)]
-
-@[simp] lemma of_gen_to_of (a : α) : free_group.of_gen a tt = free_group.of a := rfl
-
-@[simp] lemma of_gen_false_inv (a : α) : of_gen a ff = (of_gen a tt)⁻¹ :=
-begin
-  rw [of_gen, of_gen, inv_mk], refl
-end
-
-
-lemma cons_mk {a : α} {sgn : bool} (l : list (α × bool)): free_group.mk ((a, sgn) :: l) = (of_gen a sgn) * (free_group.mk l) :=
-begin
-  show mk ([(a, sgn)] ++ l) = _, rw [← mul_mk], refl,
-end
+@[simp] lemma mk_cons {a : α} {sgn : bool} (l : list (α × bool)) :
+  free_group.mk ((a, sgn) :: l) = mk [(a, sgn)] * free_group.mk l :=
+by rw [←singleton_append, ←mul_mk]
 
 variable [decidable_eq α]
-
-@[simp] lemma to_word_inv (x : free_group α) : x⁻¹.to_word = list.map (λ (x : α × bool), (x.fst, !x.snd)) x.to_word.reverse := sorry
 
 lemma to_word_mul_sublist (x y : free_group α) : (x * y).to_word <+ x.to_word ++ y.to_word :=
 begin
   refine red.sublist _,
   have : x * y = free_group.mk (x.to_word ++ y.to_word),
-  { rw [←free_group.mul_mk, free_group.to_word.mk, free_group.to_word.mk] },
+  { rw [←free_group.mul_mk, free_group.mk_to_word, free_group.mk_to_word] },
   rw this,
   exact free_group.reduce.red,
 end
@@ -48,22 +34,63 @@ end
 end free_group
 
 section
+variables {α : Type*} [pseudo_emetric_space α] {s : set α} {ε : ℝ}
+
+open emetric metric (hiding diam)
+open_locale ennreal nnreal
+
+lemma ediam_cthickening_le (ε : ℝ≥0) : diam (cthickening ε s) ≤ diam s + 2 * ε :=
+begin
+  refine diam_le (λ x hx y hy, _),
+  rw [mem_cthickening_iff, ennreal.of_real_coe_nnreal] at hx hy,
+  refine ennreal.le_of_forall_pos_le_add (λ δ hδ _, _),
+  have hε : (ε : ℝ≥0∞) < ε + ↑(δ / 2) := ennreal.coe_lt_coe.2 (lt_add_of_pos_right _ $ half_pos hδ),
+  rw [ennreal.coe_div two_ne_zero, ennreal.coe_two] at hε,
+  replace hx := hx.trans_lt hε,
+  replace hy := hy.trans_lt hε,
+  rw inf_edist_lt_iff at hx hy,
+  obtain ⟨x', hx', hxx'⟩ := hx,
+  obtain ⟨y', hy', hyy'⟩ := hy,
+  refine (edist_triangle_right _ _ _).trans ((add_le_add hxx'.le $ (edist_triangle _ _ _).trans $
+    add_le_add hyy'.le $ edist_le_diam_of_mem hy' hx').trans_eq _),
+  -- Now we're done, but `ring` won't do it :(
+  rw [←add_assoc, ←two_mul, mul_add,
+    ennreal.mul_div_cancel' ennreal.two_ne_zero ennreal.two_ne_top],
+  abel,
+end
+
+lemma ediam_thickening_le (ε : ℝ≥0) : diam (thickening ε s) ≤ diam s + 2 * ε :=
+(emetric.diam_mono $ thickening_subset_cthickening _ _).trans $ ediam_cthickening_le _
+
+end
+
+section
 variables {α : Type*} [pseudo_metric_space α] {s : set α} {ε : ℝ}
 
 open metric
+open_locale ennreal nnreal
 
-lemma diam_cthickening_le (hε : 0 ≤ ε) : diam (cthickening ε s) ≤ 2 * ε + diam s :=
+lemma diam_cthickening_le (hε : 0 ≤ ε) : diam (cthickening ε s) ≤ diam s + 2 * ε :=
 begin
-  apply metric.diam_le_of_forall_dist_le,
-  { have : 0 ≤ diam s := diam_nonneg, linarith },
-  intros x hx y hy,
-  rw [mem_cthickening_iff, emetric.inf_edist] at hx hy,
-  -- it seems like an additional hypothesis of `s` being closed might be required to finish the proof
-  -- without a strict inequality, one cannot pick elements from `s` sufficiently close to `x` or `y`
-  sorry,
+  refine diam_le_of_forall_dist_le (by positivity) (λ x hx y hy, _),
+  rw [mem_cthickening_iff, ennreal.of_real_coe_nnreal] at hx hy,
+  refine ennreal.le_of_forall_pos_le_add (λ δ hδ _, _),
+  have hε : (ε : ℝ≥0∞) < ε + ↑(δ / 2) := ennreal.coe_lt_coe.2 (lt_add_of_pos_right _ $ half_pos hδ),
+  rw [ennreal.coe_div two_ne_zero, ennreal.coe_two] at hε,
+  replace hx := hx.trans_lt hε,
+  replace hy := hy.trans_lt hε,
+  rw inf_edist_lt_iff at hx hy,
+  obtain ⟨x', hx', hxx'⟩ := hx,
+  obtain ⟨y', hy', hyy'⟩ := hy,
+  refine (edist_triangle_right _ _ _).trans ((add_le_add hxx'.le $ (edist_triangle _ _ _).trans $
+    add_le_add hyy'.le $ edist_le_diam_of_mem hy' hx').trans_eq _),
+  -- Now we're done, but `ring` won't do it :(
+  rw [←add_assoc, ←two_mul, mul_add,
+    ennreal.mul_div_cancel' ennreal.two_ne_zero ennreal.two_ne_top],
+  abel,
 end
 
-
-lemma diam_thickening_le (hε : 0 ≤ ε) : diam (thickening ε s) ≤ 2 * ε + diam s := sorry
+lemma diam_thickening_le (ε : ℝ≥0) : diam (thickening ε s) ≤ diam s + 2 * ε :=
+(emetric.diam_mono $ thickening_subset_cthickening _ _).trans $ ediam_cthickening_le _
 
 end

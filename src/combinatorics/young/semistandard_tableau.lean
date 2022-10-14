46861850ea2836f2bc9d5684e303d5a328f39711
Jake Levinson <levinson.jake@gmail.com>
2022-10-13 20:07:42 -0700

    shortened folder name

--- src/combinatorics/young/semistandard_tableau.lean
+++ src/combinatorics/young/semistandard_tableau.lean
@@ -3,7 +3,7 @@ Copyright (c) 2022 Jake Levinson. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Jake Levinson
 -/
-import combinatorics.young_tableau.young_diagram
+import combinatorics.young.young_diagram
 
 /-!
 # Semistandard Young tableaux

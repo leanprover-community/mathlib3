85965ab70ac30104c179328673b3a9f42ed53a98
mkaratarakis <40603357+mkaratarakis@users.noreply.github.com>
2022-10-15 11:12:26 +0200

    Update src/algebra/group_ring_action.lean
    Co-authored-by: Eric Wieser <wieser.eric@gmail.com>

--- src/algebra/group_ring_action.lean
+++ src/algebra/group_ring_action.lean
@@ -68,7 +68,7 @@ def mul_semiring_action.to_ring_equiv [mul_semiring_action G R] (x : G) : R ≃+
 /-- Compose a `mul_semiring_action` with a `monoid_hom`, with action `f r' • m`.
 See note [reducible non-instances]. -/
 @[reducible] def mul_semiring_action.comp_hom {N M : Type*} [monoid N] [monoid M] (f : N →* M)
-[mul_semiring_action M R] : mul_semiring_action N R :=
+  [mul_semiring_action M R] : mul_semiring_action N R :=
 { smul := has_smul.comp.smul f,
   ..distrib_mul_action.comp_hom R f,
   ..mul_distrib_mul_action.comp_hom R f }

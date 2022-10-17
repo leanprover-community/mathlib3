769be23fe6c06065aca15378250e9b70d58ea6de
Eric Wieser <wieser.eric@gmail.com>
2022-10-17 08:00:53 +0000

    wip

--- src/data/multiset/interval.lean
+++ src/data/multiset/interval.lean
@@ -20,21 +20,57 @@ open_locale big_operators pointwise
 
 variables {α : Type*} {β : α → Type*}
 
+@[ext]
+lemma multiset.add_hom_ext {A} [add_zero_class A] ⦃f g : multiset α →+ A⦄
+  (h : ∀ x, f {x} = g {x}) : f = g :=
+begin
+  ext s,
+  induction s using multiset.induction_on with a s ih,
+  { simp only [map_zero] },
+  { simp only [←multiset.singleton_add, map_add, ih, h] }
+end
+
 namespace dfinsupp
+
 
 /-- Non-dependent special case of `dfinsupp.add_zero_class` to help typeclass search. -/
 instance add_zero_class' {β} [add_zero_class β] : add_zero_class (Π₀ a : α, β) :=
 @dfinsupp.add_zero_class α (λ _, β) _
 
 /-- A computable version of `finsupp.to_multiset`. -/
-def to_multiset [decidable_eq α] : (Π₀ a : α, ℕ) ≃+ multiset α :=
+def to_multiset [decidable_eq α] : (Π₀ a : α, ℕ) →+ multiset α :=
+dfinsupp.sum_add_hom (λ a : α, multiset.repeat_add_monoid_hom a)
+
+
+-- { inv_fun := λ s, { to_fun := λ n, s.count n,
+--     support' := trunc.mk ⟨s, λ i, (em (i ∈ s)).imp_right multiset.count_eq_zero_of_not_mem⟩ },
+--   left_inv := λ f, dfinsupp.ext $ \lam by simp,
+--   .. dfinsupp.sum_add_hom (λ a : α, multiset.repeat_add_monoid_hom a) }
+
+end dfinsupp
+
+namespace multiset
+
+/-- A computable version of `multiset.to_finsupp` -/
+@[simps]
+def to_dfinsupp [decidable_eq α] : multiset α →+ Π₀ a : α, ℕ :=
+{ to_fun := λ s,
+  { to_fun := λ n, s.count n,
+    support' := trunc.mk ⟨s, λ i, (em (i ∈ s)).imp_right multiset.count_eq_zero_of_not_mem⟩ },
+  map_zero' := rfl,
+  map_add' := λ s t, dfinsupp.ext $ λ _, multiset.count_add _ _ _ }
+
+@[simp] lemma to_dfinsupp_apply [decidable_eq α] (s : multiset α) (a : α) :
+  s.to_dfinsupp a = s.count a := rfl
+
+@[simp] lemma to_dfinsupp_support [decidable_eq α] (s : multiset α) :
+  s.to_dfinsupp.support = s.to_finset :=
+(finset.filter_eq_self _).mpr (λ x hx, count_ne_zero.mpr $ multiset.mem_to_finset.1 hx)
+
+def equiv_dfinsupp [decidable_eq α] : multiset α ≃+ Π₀ a : α, ℕ :=
 add_monoid_hom.to_add_equiv
-  (dfinsupp.sum_add_hom (λ a : α, multiset.repeat_add_monoid_hom a))
-  { to_fun := λ s,
-    { to_fun := λ n, s.count n,
-      support' := trunc.mk ⟨s, λ i, (em (i ∈ s)).imp_right multiset.count_eq_zero_of_not_mem⟩ },
-    map_zero' := rfl,
-    map_add' := λ s t, dfinsupp.ext $ λ _, multiset.count_add _ _ _ }
+  multiset.to_dfinsupp
+  (dfinsupp.to_multiset)
   (by {
     classical,
     refine @dfinsupp.add_hom_ext α (λ _, ℕ) _ _ _ _ _ _ (λ i hi, _),
@@ -48,18 +84,6 @@ add_monoid_hom.to_add_equiv
     dsimp,
     sorry
   })
--- { inv_fun := λ s, { to_fun := λ n, s.count n,
---     support' := trunc.mk ⟨s, λ i, (em (i ∈ s)).imp_right multiset.count_eq_zero_of_not_mem⟩ },
---   left_inv := λ f, dfinsupp.ext $ \lam by simp,
---   .. dfinsupp.sum_add_hom (λ a : α, multiset.repeat_add_monoid_hom a) }
-
-end dfinsupp
-
-namespace multiset
-
-/-- A computable version of `multiset.to_finsupp` -/
-def to_dfinsupp [decidable_eq α] : multiset α →+ Π₀ a : α, ℕ :=
-add_equiv.symm (dfinsupp.to_multiset : (Π₀ a : α, ℕ) ≃+ multiset α)
 
 @[simp] lemma to_dfinsupp_to_multiset [decidable_eq α] (s : multiset α) : s.to_dfinsupp.to_multiset = s :=
 dfinsupp.to_multiset.apply_symm_apply s
@@ -83,20 +107,24 @@ locally_finite_order.of_Icc (multiset α)
   end)
 
 lemma Icc_eq [decidable_eq α] :
-  finset.Icc f g = (finset.Icc f.to_dfinsupp g.to_dfinsupp).map (dfinsupp.to_multiset.to_equiv.to_embedding) := rfl
+  finset.Icc f g =
+    (finset.Icc f.to_dfinsupp g.to_dfinsupp).map (dfinsupp.to_multiset.to_equiv.to_embedding) := rfl
 
 lemma card_Icc [decidable_eq α]  :
-  (finset.Icc f g).card = ∏ i in (f + g).to_finset, (g.count i - f.count i) :=
-sorry
+  (finset.Icc f g).card = ∏ i in f.to_finset ∪ g.to_finset, (g.count i + 1 - f.count i) :=
+by simp_rw [Icc_eq, finset.card_map, dfinsupp.card_Icc, nat.card_Icc, multiset.to_dfinsupp_apply,
+  to_dfinsupp_support]
 
 lemma card_Ico [decidable_eq α] :
-  (finset.Ico f g).card = ∏ i in (f + g).to_finset, (g.count i - f.count i) - 1 :=
+  (finset.Ico f g).card = ∏ i in f.to_finset ∪ g.to_finset, (g.count i + 1 - f.count i) - 1 :=
 by rw [card_Ico_eq_card_Icc_sub_one, card_Icc]
 
-lemma card_Ioc [decidable_eq α] : (finset.Ioc f g).card = ∏ i in (f + g).to_finset, (g.count i - f.count i) - 1 :=
+lemma card_Ioc [decidable_eq α] :
+  (finset.Ioc f g).card = ∏ i in f.to_finset ∪ g.to_finset, (g.count i + 1 - f.count i) - 1 :=
 by rw [card_Ioc_eq_card_Icc_sub_one, card_Icc]
 
-lemma card_Ioo [decidable_eq α] : (finset.Ioo f g).card = ∏ i in (f + g).to_finset, (g.count i - f.count i) - 2 :=
+lemma card_Ioo [decidable_eq α] :
+  (finset.Ioo f g).card = ∏ i in f.to_finset ∪ g.to_finset, (g.count i + 1 - f.count i) - 2 :=
 by rw [card_Ioo_eq_card_Icc_sub_two, card_Icc]
 
 end multiset

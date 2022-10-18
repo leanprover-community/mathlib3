d94f7d5776a32f096deb6f71147361054f85f6ca
Scott Morrison <scott.morrison@gmail.com>
2022-10-18 18:46:43 +1100

    fix imports

--- src/data/finset/mul_antidiagonal.lean
+++ src/data/finset/mul_antidiagonal.lean
@@ -3,7 +3,7 @@ Copyright (c) 2020 Floris van Doorn. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Floris van Doorn, Yaël Dillies
 -/
-import data.set.pointwise.smul
+import data.set.pointwise.basic
 import data.set.mul_antidiagonal
 
 /-! # Multiplication antidiagonal as a `finset`.

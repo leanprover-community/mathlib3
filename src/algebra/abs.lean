d9c0c09aee3e5f84a0c4dad9dfca107e27d54267
Scott Morrison <scott.morrison@gmail.com>
2022-10-19 09:43:07 +1100

    chore: mark algebra.abs as frozen, too

--- src/algebra/abs.lean
+++ src/algebra/abs.lean
@@ -7,6 +7,10 @@ Authors: Christopher Hoskin
 /-!
 # Absolute value
 
+> THIS FILE IS FROZEN, AS IT HAS BEEN PORTED TO MATHLIB4
+> https://github.com/leanprover-community/mathlib4/pull/477
+> No changes accepted without a corresponding PR to mathlib4.
+
 This file defines a notational class `has_abs` which adds the unary operator `abs` and the notation
 `|.|`. The concept of an absolute value occurs in lattice ordered groups and in GL and GM spaces.
 

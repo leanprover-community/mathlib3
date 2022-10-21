48bde1deec17994ef5ef880114f6632305406a2c
Yury Kudryashov <urkud@urkud.name>
2022-10-21 14:11:04 -0500

    refactor(data.fun_like,logic.equiv): generalize instances

--- src/data/fun_like/basic.lean
+++ src/data/fun_like/basic.lean
@@ -184,6 +184,9 @@ ext_iff.not.trans not_forall
 lemma exists_ne {f g : F} (h : f ≠ g) : ∃ x, f x ≠ g x :=
 ne_iff.mp h
 
+@[priority 100] instance subsingleton_cod [∀ a, subsingleton (β a)] : subsingleton F :=
+⟨λ f g, coe_injective $ subsingleton.elim _ _⟩
+
 end fun_like
 
 end dependent

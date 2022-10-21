33bbaa519416027cbf28086b7685e3fb2020b080
Joël Riou <joel.riou@universite-paris-saclay.fr>
2022-10-21 10:47:37 +0200

    Ring

--- src/algebra/category/Ring/internal.lean
+++ src/algebra/category/Ring/internal.lean
@@ -1,5 +1,6 @@
 import category_theory.concrete_category.operations
 import algebra.category.Ring.basic
+import algebra.category.Group.internal
 
 namespace category_theory
 
@@ -23,6 +24,27 @@ def Ring_mul : operation₂ Ring :=
 { app := λ R x, x.1 * x.2, }
 
 end operations
+
+namespace internal
+
+namespace Ring
+
+open concrete_category.operations
+
+variables {C : Type*} [category C]
+
+def mk (R : internal Ab C)
+  (yoneda_one : (functor.const Cᵒᵖ).obj punit ⟶ yoneda.obj R.obj)
+  (yoneda_mul : concat₂ (yoneda.obj R.obj) (yoneda.obj R.obj) ⟶ yoneda.obj R.obj)
+  (yoneda_mul_one : lift₂ (to_functor_const_punit ≫ yoneda_one) (𝟙 _) ≫ yoneda_mul = 𝟙 _)
+  (yoneda_mul_mul : lift₂ (pr₁₂_₃ ≫ yoneda_mul) pr₃_₃ ≫ yoneda_mul =
+    lift₂ pr₁_₃ (pr₂₃_₃ ≫ yoneda_mul) ≫ yoneda_mul) :
+  internal Ring C :=
+sorry
+
+end Ring
+
+end internal
 
 end concrete_category
 

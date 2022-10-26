0c24bc574ab4ac96bd1c57984067d998d65eae8b
Yaël Dillies <yael.dillies@gmail.com>
2022-10-26 07:39:20 +0000

    golf

--- src/data/set/intervals/basic.lean
+++ src/data/set/intervals/basic.lean
@@ -25,13 +25,11 @@ for some statements it should be `linear_order` or `densely_ordered`).
 TODO: This is just the beginning; a lot of rules are missing
 -/
 
+open function order_dual (to_dual of_dual)
+
 variables {α β : Type*}
 
 namespace set
-
-open set
-open order_dual (to_dual of_dual)
-
 section preorder
 variables [preorder α] {a a₁ a₂ b b₁ b₂ c x : α}
 
@@ -549,13 +547,11 @@ eq_singleton_iff_unique_mem.2 ⟨left_mem_Ici, λ b, h.eq_of_ge⟩
 
 lemma _root_.is_min.Iic_eq (h : is_min a) : Iic a = {a} := h.to_dual.Ici_eq
 
-lemma Ici_injective : function.injective (Ici : α → set α) :=
-λ a b h, le_antisymm (h.symm.subset le_rfl) (h.subset le_rfl)
+lemma Ici_injective : injective (Ici : α → set α) := λ a b, eq_of_forall_ge_iff ∘ set.ext_iff.1
+lemma Iic_injective : injective (Iic : α → set α) := λ a b, eq_of_forall_le_iff ∘ set.ext_iff.1
 
-lemma Iic_injective : function.injective (Iic : α → set α) := @Ici_injective αᵒᵈ _
-
-lemma Ici_eq_Ici_iff : Ici a = Ici b ↔ a = b := Ici_injective.eq_iff
-lemma Iic_eq_Iic_iff : Iic a = Iic b ↔ a = b := @Ici_eq_Ici_iff αᵒᵈ _ _ _
+lemma Ici_inj : Ici a = Ici b ↔ a = b := Ici_injective.eq_iff
+lemma Iic_inj : Iic a = Iic b ↔ a = b := Iic_injective.eq_iff
 
 end partial_order
 
@@ -651,6 +647,12 @@ by rw [diff_eq, compl_Iio, inter_comm, Ici_inter_Iic]
 
 @[simp] lemma Iio_diff_Iio : Iio b \ Iio a = Ico a b :=
 by rw [diff_eq, compl_Iio, inter_comm, Ici_inter_Iio]
+
+lemma Ioi_injective : injective (Ioi : α → set α) := λ a b, eq_of_forall_gt_iff ∘ set.ext_iff.1
+lemma Iio_injective : injective (Iio : α → set α) := λ a b, eq_of_forall_lt_iff ∘ set.ext_iff.1
+
+lemma Ioi_inj : Ioi a = Ioi b ↔ a = b := Ioi_injective.eq_iff
+lemma Iio_inj : Iio a = Iio b ↔ a = b := Iio_injective.eq_iff
 
 lemma Ico_subset_Ico_iff (h₁ : a₁ < b₁) :
   Ico a₁ b₁ ⊆ Ico a₂ b₂ ↔ a₂ ≤ a₁ ∧ b₁ ≤ b₂ :=

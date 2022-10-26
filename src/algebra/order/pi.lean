ed961efe41bba6668eac414e04bfcc706badf436
Scott Morrison <scott@tqft.net>
2022-10-27 10:17:38 +1100

    Update src/algebra/order/pi.lean
    Co-authored-by: Anne Baanen <Vierkantor@users.noreply.github.com>

--- src/algebra/order/pi.lean
+++ src/algebra/order/pi.lean
@@ -3,8 +3,8 @@ Copyright (c) 2018 Simon Hudon. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Simon Hudon, Patrick Massot
 -/
-import algebra.ring.pi
 import algebra.order.ring
+import algebra.ring.pi
 
 /-!
 # Pi instances for ordered groups and monoids

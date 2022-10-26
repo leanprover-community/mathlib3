dbe3abf813717fe6ac9443d72209aafbc5ea3383
Scott Morrison <scott.morrison@gmail.com>
2022-10-26 13:56:36 +1100

    chore(data/nat/basic): split into two files, one not depending on the order hierarchy

--- src/algebra/order/pi.lean
+++ src/algebra/order/pi.lean
@@ -4,6 +4,7 @@ Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Simon Hudon, Patrick Massot
 -/
 import algebra.ring.pi
+import algebra.order.ring
 
 /-!
 # Pi instances for ordered groups and monoids

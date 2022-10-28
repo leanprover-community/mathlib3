5e1d91139ea052b9dfb23edcab55f9c73a06cb58
Daan van Gent <daanvangent@gmail.com>
2022-10-28 11:57:11 +0200

    undo oopsie

--- src/algebra/module/composition_series.lean
+++ src/algebra/module/composition_series.lean
@@ -34,10 +34,7 @@ variables {R M : Type*} [ring R] [add_comm_group M] [module R M]
 @[simp]
 lemma comap_map_subtype (B : submodule R M) (A : submodule R B) :
   comap B.subtype (map B.subtype A) = A :=
-begin
-  apply comap_map_eq_of_injective,
-  exact subtype.coe_injective,
-end
+  comap_map_eq_of_injective subtype.coe_injective A
 
 lemma is_maximal_iff_quot_is_simple {A B : submodule R M} (hAB : A ≤ B) :
   lattice.is_maximal A B ↔ is_simple_module R (B ⧸ comap B.subtype A) :=
@@ -46,7 +43,7 @@ begin
   split; intro h, {
     split, {
       intro htop,
-      rw submodule.comap_subtype_eq_top at htop,
+      rw comap_subtype_eq_top at htop,
       rw le_antisymm hAB htop at h,
       exact lt_irrefl B h.lt,
     }, {
@@ -63,7 +60,7 @@ begin
         rw congr_arg (comap B.subtype) h.symm,
         exact comap_map_subtype B C',
       }, {
-        rw [←comap_map_subtype B C', h, submodule.comap_subtype_self],
+        rw [←comap_map_subtype B C', h, comap_subtype_self],
       }
     },
   }, {
@@ -72,12 +69,12 @@ begin
       intro hAB',
       apply h.1,
       rw hAB',
-      exact submodule.comap_subtype_self B,
+      exact comap_subtype_self B,
     }, {
       intros C hAC hCB,
       by_cases u : comap B.subtype A < comap B.subtype C, {
         right,
-        exact le_antisymm hCB (submodule.comap_subtype_eq_top.mp $ h.2 _ u),
+        exact le_antisymm hCB (comap_subtype_eq_top.mp $ h.2 _ u),
       }, {
         left,
         have eqAC := eq_of_le_of_not_lt (comap_mono hAC) u,
@@ -96,7 +93,7 @@ instance jordan_holder_module : jordan_holder_lattice (submodule R M) := {
     haveI h := (is_maximal_iff_quot_is_simple (le_sup_right : B ≤ A ⊔ B)).mp h₂,
     apply is_simple_module.congr (linear_map.quotient_inf_equiv_sup_quotient A B),
   end,
-  iso := λ X Y, nonempty $ ((X.2 ⧸ comap X.2.subtype X.1)) ≃ₗ[R] ((Y.2 ⧸ comap Y.2.subtype Y.1)),
+  iso := λ X Y, nonempty $ (X.2 ⧸ comap X.2.subtype X.1) ≃ₗ[R] (Y.2 ⧸ comap Y.2.subtype Y.1),
   iso_symm := λ {A B} ⟨f⟩, ⟨f.symm⟩,
   iso_trans := λ {A B C} ⟨f⟩ ⟨g⟩, ⟨f.trans g⟩,
   second_iso := λ {A B} h, ⟨begin

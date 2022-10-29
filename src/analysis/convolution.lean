3a1a91745763d9c74736fc3e692d35406a5c477f
sgouezel <sebastien.gouezel@univ-rennes1.fr>
2022-10-29 19:35:42 +0200

    rename, move

--- src/analysis/convolution.lean
+++ src/analysis/convolution.lean
@@ -6,7 +6,7 @@ Authors: Floris van Doorn
 import measure_theory.group.integration
 import measure_theory.group.prod
 import measure_theory.function.locally_integrable
-import analysis.calculus.specific_functions
+import analysis.calculus.bump_function_inner
 import analysis.calculus.parametric_integral
 import measure_theory.measure.haar_lebesgue
 

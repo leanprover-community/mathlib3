e0883e1f03165423fb6861932119544d9f4fbd08
Christopher Hoskin <mans0954@users.noreply.github.com>
2022-10-29 20:27:12 +0100

    Update src/topology/algebra/order/lower_topology.lean
    Co-authored-by: Junyan Xu <junyanxumath@gmail.com>

--- src/topology/algebra/order/lower_topology.lean
+++ src/topology/algebra/order/lower_topology.lean
@@ -108,8 +108,7 @@ The non-empty complements of the upper closures of finite subsets are a collecti
 which form a basis for the lower topology
 -/
 def lower_basis (α : Type u) [preorder α] :=
-{s : set α | ∃ (F : set α), F.finite ∧
-  ↑(upper_closure F).compl = s }
+{s : set α | ∃ (F : set α), F.finite ∧ ↑(upper_closure F).compl = s}
 
 lemma is_topological_basis : is_topological_basis (lower_basis (lower α)) :=
 begin

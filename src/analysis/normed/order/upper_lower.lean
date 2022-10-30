3218316dcb67abbb422a9dcd6dc3af71dbb9dc0a
Yaël Dillies <yael.dillies@gmail.com>
2022-10-30 11:18:11 +0000

    split norm results

---
+++ src/analysis/normed/order/upper_lower.lean
@@ -0,0 +1,185 @@
+/-
+Copyright (c) 2022 Yaël Dillies. All rights reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
+Authors: Yaël Dillies
+-/
+import analysis.normed.group.pointwise
+import analysis.normed.order.basic
+import topology.algebra.order.upper_lower
+import topology.metric_space.hausdorff_distance
+
+/-!
+# Upper/lower/order-connected sets in normed groups
+
+The topological closure and interior of an upper/lower/order-connected set is an
+upper/lower/order-connected set (with the notable exception of the closure of an order-connected
+set).
+
+We also prove lemmas specific to `ℝⁿ`. Those are helpful to prove that order-connected sets in `ℝⁿ`
+are measurable.
+-/
+
+section
+variables {E : Type*} [seminormed_group E]
+
+@[simp, to_additive] lemma dist_mul_self_right (a b : E) : dist b (a * b) = ∥a∥ :=
+by rw [←dist_one_left, ←dist_mul_right 1 a b, one_mul]
+
+@[simp, to_additive] lemma dist_mul_self_left (a b : E) : dist (a * b) b = ∥a∥ :=
+by rw [dist_comm, dist_mul_self_right]
+
+end
+
+section
+variables {α : Type*} [mul_one_class α] [has_lt α] [contravariant_class α α (*) (<)] {a b : α}
+
+@[to_additive] lemma one_lt_of_lt_mul_right : a < a * b → 1 < b :=
+by simpa only [mul_one] using (lt_of_mul_lt_mul_left' : a * 1 < a * b → 1 < b)
+
+end
+
+section
+variables {α : Type*} [mul_one_class α] [preorder α] [contravariant_class α α (*) (<)]
+  [has_exists_mul_of_le α] {a b : α}
+
+@[to_additive] lemma exists_one_lt_mul_of_lt' (h : a < b) : ∃ c, 1 < c ∧ a * c = b :=
+by { obtain ⟨c, rfl⟩ := exists_mul_of_le h.le, exact ⟨c, one_lt_of_lt_mul_right h, rfl⟩ }
+
+end
+
+section finite
+variables {α ι : Type*} [linear_ordered_semifield α] [has_exists_add_of_le α] [finite ι]
+  {x y : ι → α}
+
+lemma pi.exists_forall_pos_add_lt (h : ∀ i, x i < y i) : ∃ ε, 0 < ε ∧ ∀ i, x i + ε < y i :=
+begin
+  casesI nonempty_fintype ι,
+  casesI is_empty_or_nonempty ι,
+  { exact ⟨1, zero_lt_one, is_empty_elim⟩ },
+  choose ε hε hxε using λ i, exists_pos_add_of_lt' (h i),
+  obtain rfl : x + ε = y := funext hxε,
+  have hε : 0 < finset.univ.inf' finset.univ_nonempty ε := (finset.lt_inf'_iff _).2 (λ i _, hε _),
+  exact ⟨_, half_pos hε, λ i, add_lt_add_left ((half_lt_self hε).trans_le $ finset.inf'_le _ $
+    finset.mem_univ _) _⟩,
+end
+
+end finite
+
+open function metric set
+
+variables {α ι : Type*}
+
+section metric_space
+variables [normed_ordered_group α] {s : set α}
+
+@[to_additive is_upper_set.thickening]
+protected lemma is_upper_set.thickening' (hs : is_upper_set s) (ε : ℝ) :
+  is_upper_set (thickening ε s) :=
+by { rw ←ball_mul_one, exact hs.mul_left }
+
+@[to_additive is_lower_set.thickening]
+protected lemma is_lower_set.thickening' (hs : is_lower_set s) (ε : ℝ) :
+  is_lower_set (thickening ε s) :=
+by { rw ←ball_mul_one, exact hs.mul_left }
+
+@[to_additive is_upper_set.cthickening]
+protected lemma is_upper_set.cthickening' (hs : is_upper_set s) (ε : ℝ) :
+  is_upper_set (cthickening ε s) :=
+by { rw cthickening_eq_Inter_thickening'', exact is_upper_set_Inter₂ (λ δ hδ, hs.thickening' _) }
+
+@[to_additive is_lower_set.cthickening]
+protected lemma is_lower_set.cthickening' (hs : is_lower_set s) (ε : ℝ) :
+  is_lower_set (cthickening ε s) :=
+by { rw cthickening_eq_Inter_thickening'', exact is_lower_set_Inter₂ (λ δ hδ, hs.thickening' _) }
+
+end metric_space
+
+/-! ### `ℝⁿ` -/
+
+section finite
+variables [finite ι] {s : set (ι → ℝ)} {x y : ι → ℝ} {δ : ℝ}
+
+lemma is_upper_set.mem_interior_of_forall_lt (hs : is_upper_set s) (hx : x ∈ closure s)
+  (h : ∀ i, x i < y i) :
+  y ∈ interior s :=
+begin
+  casesI nonempty_fintype ι,
+  obtain ⟨ε, hε, hxy⟩ := pi.exists_forall_pos_add_lt h,
+  obtain ⟨z, hz, hxz⟩ := metric.mem_closure_iff.1 hx _ hε,
+  rw dist_pi_lt_iff hε at hxz,
+  have hyz : ∀ i, z i < y i,
+  { refine λ i, (hxy _).trans_le' (sub_le_iff_le_add'.1 $ (le_abs_self _).trans _),
+    rw [←real.norm_eq_abs, ←dist_eq_norm'],
+    exact (hxz _).le },
+  obtain ⟨δ, hδ, hyz⟩ := pi.exists_forall_pos_add_lt hyz,
+  refine mem_interior.2 ⟨ball y δ, _, is_open_ball, mem_ball_self hδ⟩,
+  rintro w hw,
+  refine hs (λ i, _) hz,
+  simp_rw [ball_pi _ hδ, real.ball_eq_Ioo] at hw,
+  exact ((lt_sub_iff_add_lt.2 $ hyz _).trans (hw _ $ mem_univ _).1).le,
+end
+
+lemma is_lower_set.mem_interior_of_forall_lt (hs : is_lower_set s) (hx : x ∈ closure s)
+  (h : ∀ i, y i < x i) :
+  y ∈ interior s :=
+begin
+  casesI nonempty_fintype ι,
+  obtain ⟨ε, hε, hxy⟩ := pi.exists_forall_pos_add_lt h,
+  obtain ⟨z, hz, hxz⟩ := metric.mem_closure_iff.1 hx _ hε,
+  rw dist_pi_lt_iff hε at hxz,
+  have hyz : ∀ i, y i < z i,
+  { refine λ i, (lt_sub_iff_add_lt.2 $ hxy _).trans_le (_root_.sub_le.1 $ (le_abs_self _).trans _),
+    rw [←real.norm_eq_abs, ←dist_eq_norm],
+    exact (hxz _).le },
+  obtain ⟨δ, hδ, hyz⟩ := pi.exists_forall_pos_add_lt hyz,
+  refine mem_interior.2 ⟨ball y δ, _, is_open_ball, mem_ball_self hδ⟩,
+  rintro w hw,
+  refine hs (λ i, _) hz,
+  simp_rw [ball_pi _ hδ, real.ball_eq_Ioo] at hw,
+  exact ((hw _ $ mem_univ _).2.trans $ hyz _).le,
+end
+
+end finite
+
+section fintype
+variables [fintype ι] {s : set (ι → ℝ)} {x y : ι → ℝ} {δ : ℝ}
+
+lemma is_upper_set.exists_subset_ball (hs : is_upper_set s) (hx : x ∈ closure s) (hδ : 0 < δ) :
+  ∃ y, closed_ball y (δ/4) ⊆ closed_ball x δ ∧ closed_ball y (δ/4) ⊆ interior s :=
+begin
+  refine ⟨x + const _ (3/4*δ), closed_ball_subset_closed_ball' _, _⟩,
+  { rw dist_self_add_left,
+    refine (add_le_add_left (pi_norm_const_le $ 3 / 4 * δ) _).trans_eq _,
+    simp [real.norm_of_nonneg, hδ.le, zero_le_three],
+    ring_nf },
+  obtain ⟨y, hy, hxy⟩ := metric.mem_closure_iff.1 hx _ (div_pos hδ zero_lt_four),
+  refine λ z hz, hs.mem_interior_of_forall_lt (subset_closure hy) (λ i, _),
+  rw [mem_closed_ball, dist_eq_norm'] at hz,
+  rw dist_eq_norm at hxy,
+  replace hxy := (norm_le_pi_norm _ i).trans hxy.le,
+  replace hz := (norm_le_pi_norm _ i).trans hz,
+  dsimp at hxy hz,
+  rw abs_sub_le_iff at hxy hz,
+  linarith,
+end
+
+lemma is_lower_set.exists_subset_ball (hs : is_lower_set s) (hx : x ∈ closure s) (hδ : 0 < δ) :
+  ∃ y, closed_ball y (δ/4) ⊆ closed_ball x δ ∧ closed_ball y (δ/4) ⊆ interior s :=
+begin
+  refine ⟨x - const _ (3/4*δ), closed_ball_subset_closed_ball' _, _⟩,
+  { rw dist_self_sub_left,
+    refine (add_le_add_left (pi_norm_const_le $ 3 / 4 * δ) _).trans_eq _,
+    simp [real.norm_of_nonneg, hδ.le, zero_le_three],
+    ring_nf },
+  obtain ⟨y, hy, hxy⟩ := metric.mem_closure_iff.1 hx _ (div_pos hδ zero_lt_four),
+  refine λ z hz, hs.mem_interior_of_forall_lt (subset_closure hy) (λ i, _),
+  rw [mem_closed_ball, dist_eq_norm'] at hz,
+  rw dist_eq_norm at hxy,
+  replace hxy := (norm_le_pi_norm _ i).trans hxy.le,
+  replace hz := (norm_le_pi_norm _ i).trans hz,
+  dsimp at hxy hz,
+  rw abs_sub_le_iff at hxy hz,
+  linarith,
+end
+
+end fintype

9f6dd28c46b80d549867cbd729b4be8a74612cbe
Yaël Dillies <yael.dillies@gmail.com>
2022-10-30 11:35:01 +0000

    revert analysis.normed.order.basic

--- src/analysis/normed/order/basic.lean
+++ src/analysis/normed/order/basic.lean
@@ -1,7 +1,7 @@
 /-
 Copyright (c) 2020 Anatole Dedecker. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
-Authors: Anatole Dedecker, Yaël Dillies
+Authors: Anatole Dedecker
 -/
 import analysis.normed_space.basic
 
@@ -15,53 +15,31 @@ These are mostly useful to avoid diamonds during type class inference.
 open filter set
 open_locale topological_space
 
-variables {α : Type*}
-
-/-- A `normed_ordered_add_group` is an additive group that is both a `normed_add_comm_group` and an
-`ordered_add_comm_group`. This class is necessary to avoid diamonds. -/
-class normed_ordered_add_group (α : Type*)
-  extends ordered_add_comm_group α, has_norm α, metric_space α :=
-(dist_eq : ∀ x y, dist x y = ∥x - y∥ . obviously)
-
-/-- A `normed_ordered_group` is a group that is both a `normed_comm_group` and an
-`ordered_comm_group`. This class is necessary to avoid diamonds. -/
-@[to_additive]
-class normed_ordered_group (α : Type*) extends ordered_comm_group α, has_norm α, metric_space α :=
-(dist_eq : ∀ x y, dist x y = ∥x / y∥ . obviously)
-
-/-- A `normed_linear_ordered_add_group` is an additive group that is both a `normed_add_comm_group`
-and a `linear_ordered_add_comm_group`. This class is necessary to avoid diamonds. -/
-class normed_linear_ordered_add_group (α : Type*)
-  extends linear_ordered_add_comm_group α, has_norm α, metric_space α :=
-(dist_eq : ∀ x y, dist x y = ∥x - y∥ . obviously)
-
-/-- A `normed_linear_ordered_group` is a group that is both a `normed_comm_group` and a
-`linear_ordered_comm_group`. This class is necessary to avoid diamonds. -/
-@[to_additive]
+/-- A `normed_linear_ordered_group` is an additive group that is both a `normed_add_comm_group` and
+    a `linear_ordered_add_comm_group`. This class is necessary to avoid diamonds. -/
 class normed_linear_ordered_group (α : Type*)
-  extends linear_ordered_comm_group α, has_norm α, metric_space α :=
-(dist_eq : ∀ x y, dist x y = ∥x / y∥ . obviously)
+extends linear_ordered_add_comm_group α, has_norm α, metric_space α :=
+(dist_eq : ∀ x y, dist x y = norm (x - y))
+
+@[priority 100] instance normed_linear_ordered_group.to_normed_add_comm_group (α : Type*)
+  [normed_linear_ordered_group α] : normed_add_comm_group α :=
+⟨normed_linear_ordered_group.dist_eq⟩
 
 /-- A `normed_linear_ordered_field` is a field that is both a `normed_field` and a
     `linear_ordered_field`. This class is necessary to avoid diamonds. -/
 class normed_linear_ordered_field (α : Type*)
 extends linear_ordered_field α, has_norm α, metric_space α :=
-(dist_eq : ∀ x y, dist x y = ∥x - y∥ . obviously)
-(norm_mul' : ∀ x y : α, ∥x * y∥ = ∥x∥ * ∥y∥)
-
-@[to_additive, priority 100]
-instance normed_ordered_group.to_normed_comm_group [normed_ordered_group α] : normed_comm_group α :=
-⟨normed_ordered_group.dist_eq⟩
-
-@[to_additive, priority 100]
-instance normed_linear_ordered_group.to_normed_comm_group [normed_linear_ordered_group α] :
-  normed_comm_group α :=
-⟨normed_linear_ordered_group.dist_eq⟩
+(dist_eq : ∀ x y, dist x y = norm (x - y))
+(norm_mul' : ∀ a b, norm (a * b) = norm a * norm b)
 
 @[priority 100] instance normed_linear_ordered_field.to_normed_field (α : Type*)
   [normed_linear_ordered_field α] : normed_field α :=
 { dist_eq := normed_linear_ordered_field.dist_eq,
   norm_mul' := normed_linear_ordered_field.norm_mul' }
+
+@[priority 100] instance normed_linear_ordered_field.to_normed_linear_ordered_group (α : Type*)
+[normed_linear_ordered_field α] : normed_linear_ordered_group α :=
+⟨normed_linear_ordered_field.dist_eq⟩
 
 instance : normed_linear_ordered_field ℚ :=
 ⟨dist_eq_norm, norm_mul⟩
@@ -69,9 +47,3 @@ instance : normed_linear_ordered_field ℚ :=
 noncomputable
 instance : normed_linear_ordered_field ℝ :=
 ⟨dist_eq_norm, norm_mul⟩
-
-@[to_additive] instance [normed_ordered_group α] : normed_ordered_group αᵒᵈ :=
-‹normed_ordered_group α›
-
-@[to_additive] instance [normed_linear_ordered_group α] : normed_linear_ordered_group αᵒᵈ :=
-‹normed_linear_ordered_group α›

6f5c5e026cab19b35d04ad1c2b62d41871099ec4
101damnations <101damnations@github.com>
2022-10-31 13:14:10 +0000

    generalise k

--- src/algebraic_geometry/GroupScheme.lean
+++ src/algebraic_geometry/GroupScheme.lean
@@ -3,19 +3,17 @@ import category_theory.limits.constructions.over.products
 
 universes v u w
 open algebraic_geometry category_theory category_theory.limits
-variables (k : CommRing.{u})
-
-abbreviation Over := over (Scheme.Spec.obj (opposite.op k))
+variables (S : Scheme.{u})
 
 abbreviation GroupScheme :=
-full_subcategory ({ G : (Over k)ᵒᵖ ⥤ Group.{u} | (G ⋙ forget _).representable })
+full_subcategory ({ G : (over S)ᵒᵖ ⥤ Group.{u} | (G ⋙ forget _).representable })
 
 #exit
 local attribute [instance] over.construct_products.over_binary_product_of_pullback
   over.over_has_terminal
 
 structure GroupScheme :=
-(G : Over k)
+(G : over S)
 (mul : G ⨯ G ⟶ G)
 (one : over.mk (𝟙 _) ⟶ G)
 (inv : G ⟶ G)
@@ -24,8 +22,8 @@ structure GroupScheme :=
 (one_mul : limits.prod.map one (𝟙 G) ≫ mul = limits.prod.snd)
 (mul_left_inv : limits.diag G ≫ limits.prod.map inv (𝟙 G) ≫ mul = terminal.from G ≫ one)
 
-variables {k}
+variables {S}
 
-structure GroupScheme.hom {k : CommRing.{u}} (G H : GroupScheme k) :=
+structure GroupScheme.hom (G H : GroupScheme S) :=
 (f : G.G ⟶ H.G)
 (comm : G.mul ≫ f = limits.prod.map f f ≫ H.mul)

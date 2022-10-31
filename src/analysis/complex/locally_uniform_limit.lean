9f9ad7a56b0fe65c9dd08109d032a97ac1ce784a
Vincent Beffara <vbeffara@gmail.com>
2022-10-31 17:20:08 +0100

    And again

--- src/analysis/complex/locally_uniform_limit.lean
+++ src/analysis/complex/locally_uniform_limit.lean
@@ -145,7 +145,7 @@ begin
   have h5 : tendsto_locally_uniformly_on (deriv ∘ F) (cderiv δ f) φ (interior K),
     from h1.tendsto_locally_uniformly_on.mono interior_subset,
   have h6 : ∀ x ∈ interior K, has_deriv_at f (cderiv δ f x) x,
-    from λ x h, has_deriv_at_of_tendsto_localy_uniformly_on'
+    from λ x h, has_deriv_at_of_tendsto_locally_uniformly_on'
       is_open_interior h5 h3 (λ _, h4.tendsto_at) h,
   have h7 : differentiable_on ℂ f (interior K),
     from λ x hx, (h6 x hx).differentiable_at.differentiable_within_at,

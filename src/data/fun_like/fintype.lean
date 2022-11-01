d17a539a66004f5f47f9810a2105036e2de059d6
Vierkantor <vierkantor@vierkantor.com>
2022-11-01 18:43:25 +0000

    These can't be instances because they cause loops

--- src/data/fun_like/fintype.lean
+++ src/data/fun_like/fintype.lean
@@ -12,18 +12,19 @@ import data.fun_like.basic
 # Finiteness of `fun_like` types
 
 We show a type `F` with a `fun_like F α β` is finite if both `α` and `β` are finite.
-This corresponds to the following two declarations:
+This corresponds to the following two pairs of declarations:
 
- * `fun_like.fintype` is a **definition** stating all `fun_like`s are finite if their domain and
+ * `fun_like.fintype` is a definition stating all `fun_like`s are finite if their domain and
    codomain are.
-   This is not an instance because specific `fun_like` types might have a better-suited definition.
- * `fun_like.finite` is an **instance** stating all `fun_like`s are finite if their domain and
+ * `fun_like.finite` is a lemma stating all `fun_like`s are finite if their domain and
    codomain are.
-   This can safely be an instance because `finite` is a proposition, so there is no risk of
-   non-defeq diamonds.
  * `fun_like.fintype'` is a non-dependent version of `fun_like.fintype` and
  * `fun_like.finite` is a non-dependent version of `fun_like.finite`, because dependent instances
    are harder to infer.
+
+You can use these to produce instances for specific `fun_like` types.
+(Although there might be options for `fintype` instances with better definitional behaviour.)
+They can't be instances themselves since they can cause loops.
 -/
 
 variables (F G : Type*) {α γ : Type*} {β : α → Type*} [fun_like F α β] [fun_like G α (λ _, γ)]
@@ -47,16 +48,15 @@ fun_like.fintype G
 
 /-- All `fun_like`s are finite if their domain and codomain are.
 
-Unlike `fun_like.fintype`, this can safely be an instance because `finite` is a proposition,
-so there is no risk of non-defeq diamonds.
+Can't be an instance because it can cause infinite loops.
 -/
-instance fun_like.finite [finite α] [Π i, finite (β i)] : finite F :=
+lemma fun_like.finite [finite α] [Π i, finite (β i)] : finite F :=
 finite.of_injective _ fun_like.coe_injective
 
 /-- All `fun_like`s are finite if their domain and codomain are.
 
 Non-dependent version of `fun_like.finite` that might be easier to infer.
-This is not an instance because specific `fun_like` types might have a better-suited definition.
+Can't be an instance because it can cause infinite loops.
 -/
-instance fun_like.finite' [decidable_eq α] [finite α] [finite γ] : finite G :=
+lemma fun_like.finite' [finite α] [finite γ] : finite G :=
 fun_like.finite G

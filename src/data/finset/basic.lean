f36ffdfb1ac70d8a913f22196ac457d20b3cdce3
Eric Wieser <wieser.eric@gmail.com>
2022-11-01 09:46:55 +0000

    reorder

--- src/data/finset/basic.lean
+++ src/data/finset/basic.lean
@@ -1777,28 +1777,6 @@ by { simp [subset.antisymm_iff],
        ... ⊇ s₁ \ ∅         : by mono using [(⊇)]
        ... ⊇ s₁             : by simp [(⊇)] } }
 
-theorem filter_union_filter_of_codisjoint (s : finset α) (h : codisjoint p q) :
-  s.filter p ∪ s.filter q = s :=
-(filter_or _ _ _).symm.trans $ filter_true_of_mem $ λ x hx, h x trivial
-
-theorem filter_inter_filter_of_disjoint (s t : finset α) (h : disjoint p q) :
-  s.filter p ∩ t.filter q = ∅ :=
-begin
-  ext,
-  simp only [mem_inter, mem_filter, not_mem_empty, iff_false],
-  rintros ⟨⟨ha, hp⟩, ⟨hb, hq⟩⟩,
-  exact h _ ⟨hp, hq⟩,
-end
-
-theorem filter_union_filter_neg_eq [decidable_pred (λ a, ¬ p a)] (s : finset α) :
-  s.filter p ∪ s.filter (λ a, ¬ p a) = s :=
-filter_union_filter_of_codisjoint _ _ _
-  (@disjoint_compl_right _ _ (order_dual.to_dual p)).dual
-
-theorem filter_inter_filter_neg_eq [decidable_pred (λ a, ¬ p a)] (s t : finset α) :
-  s.filter p ∩ t.filter (λ a, ¬ p a) = ∅ :=
-filter_inter_filter_of_disjoint _ _ _ _ disjoint_compl_right
-
 lemma subset_union_elim {s : finset α} {t₁ t₂ : set α} (h : ↑s ⊆ t₁ ∪ t₂) :
   ∃ s₁ s₂ : finset α, s₁ ∪ s₂ = s ∧ ↑s₁ ⊆ t₁ ∧ ↑s₂ ⊆ t₂ \ t₁ :=
 begin
@@ -1873,9 +1851,32 @@ lemma disjoint_filter_filter {s t : finset α} {p q : α → Prop}
   disjoint s t → disjoint (s.filter p) (t.filter q) :=
 disjoint.mono (filter_subset _ _) (filter_subset _ _)
 
-lemma disjoint_filter_filter_neg (s : finset α) (p : α → Prop) [decidable_pred p] :
-  disjoint (s.filter p) (s.filter $ λ a, ¬ p a) :=
-(disjoint_filter.2 $ λ a _, id).symm
+lemma disjoint_filter_filter' (s t : finset α) {p q : α → Prop}
+  [decidable_pred p] [decidable_pred q] (h : disjoint p q) :
+  disjoint (s.filter p) (t.filter q) :=
+begin
+  simp_rw [disjoint_left, mem_filter],
+  rintros a ⟨hs, hp⟩ ⟨ht, hq⟩,
+  exact h _ ⟨hp, hq⟩,
+end
+
+lemma disjoint_filter_filter_neg (s t : finset α) (p : α → Prop)
+  [decidable_pred p] [decidable_pred (λ a, ¬ p a)] :
+  disjoint (s.filter p) (t.filter $ λ a, ¬ p a) :=
+disjoint_filter_filter' s t disjoint_compl_right
+
+theorem filter_inter_filter_neg_eq [decidable_pred (λ a, ¬ p a)] (s t : finset α) :
+  s.filter p ∩ t.filter (λ a, ¬ p a) = ∅ :=
+(disjoint_filter_filter_neg s t p).eq_bot
+
+theorem filter_union_filter_of_codisjoint (s : finset α) (h : codisjoint p q) :
+  s.filter p ∪ s.filter q = s :=
+(filter_or _ _ _).symm.trans $ filter_true_of_mem $ λ x hx, h x trivial
+
+theorem filter_union_filter_neg_eq [decidable_pred (λ a, ¬ p a)] (s : finset α) :
+  s.filter p ∪ s.filter (λ a, ¬ p a) = s :=
+filter_union_filter_of_codisjoint _ _ _
+  (@disjoint_compl_right _ _ (order_dual.to_dual p)).dual
 
 end filter
 

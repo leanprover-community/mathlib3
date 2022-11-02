6310a4a2a63c58d882bca71468348945d1ad8628
Jujian Zhang <jujian.zhang1998@outlook.com>
2022-11-02 15:27:29 +0000

    defined sheaf of module as presheaf of module that is a sheaf

--- src/topology/sheaves/monoidal.lean
+++ src/topology/sheaves/monoidal.lean
@@ -821,6 +821,10 @@ lemma sections_smul_restriction {U V : (opens X)ᵒᵖ} (inc : U ⟶ V) (r : R.X
   M.X.map inc (r • m) = R.X.map inc r • M.X.map inc m :=
 eq.symm $ fun_like.congr_fun (M.act.naturality inc) $ r ⊗ₜ m
 
+structure sheaf_of_module (R : Mon_ (presheaf AddCommGroup.{u} X)) (h : is_sheaf R.X) :=
+(val : Mod R)
+(cond : is_sheaf val.X)
+
 end presheaf_of_module
 
 end monoidal

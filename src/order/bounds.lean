76ddc2911aad2597e28bbbf56a5a093f39f4731b
Ruben Van de Velde <65514131+Ruben-VandeVelde@users.noreply.github.com>
2022-11-04 23:11:07 +0100

    chore(order/bounds): move out group lemmas

--- src/order/bounds.lean
+++ src/order/bounds.lean
@@ -4,7 +4,6 @@ Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Johannes Hölzl, Yury Kudryashov
 -/
 
-import algebra.order.group.basic
 import data.set.intervals.basic
 
 /-!
@@ -805,31 +804,6 @@ let ⟨c, hcs, hac, hcb⟩ := h.exists_between hb
 in ⟨c, hcs, hac.lt_of_ne $ λ hac, h' $ hac.symm ▸ hcs, hcb⟩
 
 end linear_order
-
-/-!
-### Least upper bound and the greatest lower bound in linear ordered additive commutative groups
--/
-
-section linear_ordered_add_comm_group
-
-variables [linear_ordered_add_comm_group α] {s : set α} {a ε : α}
-
-lemma is_glb.exists_between_self_add (h : is_glb s a) (hε : 0 < ε) :
-  ∃ b ∈ s, a ≤ b ∧ b < a + ε :=
-h.exists_between $ lt_add_of_pos_right _ hε
-
-lemma is_glb.exists_between_self_add' (h : is_glb s a) (h₂ : a ∉ s) (hε : 0 < ε) :
-  ∃ b ∈ s, a < b ∧ b < a + ε :=
-h.exists_between' h₂ $ lt_add_of_pos_right _ hε
-
-lemma is_lub.exists_between_sub_self  (h : is_lub s a) (hε : 0 < ε) : ∃ b ∈ s, a - ε < b ∧ b ≤ a :=
-h.exists_between $ sub_lt_self _ hε
-
-lemma is_lub.exists_between_sub_self' (h : is_lub s a) (h₂ : a ∉ s) (hε : 0 < ε) :
-  ∃ b ∈ s, a - ε < b ∧ b < a :=
-h.exists_between' h₂ $ sub_lt_self _ hε
-
-end linear_ordered_add_comm_group
 
 /-!
 ### Images of upper/lower bounds under monotone functions

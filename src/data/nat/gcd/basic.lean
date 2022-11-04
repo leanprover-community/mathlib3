02812da5671ea3c58e48f39a358c0a1143de8c3e
Scott Morrison <scott@tqft.net>
2022-11-04 14:10:14 +0000

    chore(data/nat/gcd/basic): remove spurious imports (#17333)
    Co-authored-by: Scott Morrison <scott.morrison@gmail.com>

--- src/data/nat/gcd/basic.lean
+++ src/data/nat/gcd/basic.lean
@@ -3,7 +3,8 @@ Copyright (c) 2014 Jeremy Avigad. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Jeremy Avigad, Leonardo de Moura
 -/
-import algebra.group_power.order
+import algebra.group_power.basic
+import data.nat.order
 
 /-!
 # Definitions and properties of `gcd`, `lcm`, and `coprime`

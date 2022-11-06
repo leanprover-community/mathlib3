d6e5d9df35c47e0bf5f30b052e4e205ba1008a61
Scott Morrison <scott.morrison@gmail.com>
2022-11-06 12:16:03 +1100

    refactor(order/bounds): don't depend on ordered algebra hierarchy

--- src/data/set/intervals/unordered_interval.lean
+++ src/data/set/intervals/unordered_interval.lean
@@ -3,7 +3,7 @@ Copyright (c) 2020 Zhouhang Zhou. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Zhouhang Zhou
 -/
-import order.bounds
+import order.bounds.basic
 import data.set.intervals.image_preimage
 
 /-!

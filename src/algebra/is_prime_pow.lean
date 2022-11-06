aa45f68598a15a3b02b87be88cb7c4ad54ecee70
Yury G. Kudryashov <urkud@urkud.name>
2022-11-05 22:03:34 -0500

    Delete lines that were moved to another file

--- src/algebra/is_prime_pow.lean
+++ src/algebra/is_prime_pow.lean
@@ -59,20 +59,6 @@ theorem is_prime_pow.ne_zero [no_zero_divisors R] {n : R} (h : is_prime_pow n) :
 lemma is_prime_pow.ne_one {n : R} (h : is_prime_pow n) : n ≠ 1 :=
 λ t, eq.rec not_is_prime_pow_one t.symm h
 
-section unique_units
-
-lemma eq_of_prime_pow_eq {R : Type*} [cancel_comm_monoid_with_zero R] [unique Rˣ] {p₁ p₂ : R}
-  {k₁ k₂ : ℕ} (hp₁ : prime p₁) (hp₂ : prime p₂) (hk₁ : 0 < k₁) (h : p₁ ^ k₁ = p₂ ^ k₂) :
-  p₁ = p₂ :=
-by { rw [←associated_iff_eq] at h ⊢, apply h.of_pow_associated_of_prime hp₁ hp₂ hk₁ }
-
-lemma eq_of_prime_pow_eq' {R : Type*} [cancel_comm_monoid_with_zero R] [unique Rˣ] {p₁ p₂ : R}
-  {k₁ k₂ : ℕ} (hp₁ : prime p₁) (hp₂ : prime p₂) (hk₁ : 0 < k₂) (h : p₁ ^ k₁ = p₂ ^ k₂) :
-  p₁ = p₂ :=
-by { rw [←associated_iff_eq] at h ⊢, apply h.of_pow_associated_of_prime' hp₁ hp₂ hk₁ }
-
-end unique_units
-
 section nat
 
 lemma is_prime_pow_nat_iff (n : ℕ) :

e5ae584f490f07d657129771331ebbb5648e9c6c
Yaël Dillies <yael.dillies@gmail.com>
2022-11-06 20:59:05 +0000

    fix algebra.order.field.basic

--- src/algebra/order/field/basic.lean
+++ src/algebra/order/field/basic.lean
@@ -3,7 +3,7 @@ Copyright (c) 2014 Robert Lewis. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Robert Lewis, Leonardo de Moura, Mario Carneiro, Floris van Doorn
 -/
-import algebra.order.field_defs
+import algebra.order.field.defs
 import algebra.order.with_zero
 import algebra.parity
 import data.fintype.basic

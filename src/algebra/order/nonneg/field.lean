7ca9943893860245b521a6fa9161f6813f370d8e
Yaël Dillies <yael.dillies@gmail.com>
2022-11-06 20:53:58 +0000

    fix algebra.order.nonneg.field

--- src/algebra/order/nonneg/field.lean
+++ src/algebra/order/nonneg/field.lean
@@ -3,8 +3,8 @@ Copyright (c) 2021 Floris van Doorn. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Floris van Doorn
 -/
--- import algebra.order.archimedean
-import algebra.order.field
+import algebra.order.archimedean
+import algebra.order.field.basic
 import algebra.order.nonneg.ring
 
 /-!

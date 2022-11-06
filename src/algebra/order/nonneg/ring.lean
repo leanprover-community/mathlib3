15d90256e407fb2867e83cf86eaa3116e43fab97
Scott Morrison <scott.morrison@gmail.com>
2022-11-07 10:39:21 +1100

    fix import after merging master

--- src/algebra/order/nonneg/ring.lean
+++ src/algebra/order/nonneg/ring.lean
@@ -3,7 +3,7 @@ Copyright (c) 2021 Floris van Doorn. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Floris van Doorn
 -/
-import algebra.order.ring
+import algebra.order.ring.basic
 import order.complete_lattice_intervals
 import order.lattice_intervals
 

435816bcf24a6074f18a2468e60a96d512e7db72
Scott Morrison <scott.morrison@gmail.com>
2022-11-07 22:18:05 +1100

    .

--- src/data/int/cast/defs.lean
+++ src/data/int/cast/defs.lean
@@ -4,7 +4,6 @@ Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Mario Carneiro, Gabriel Ebner
 -/
 import data.nat.cast.defs
-import algebra.group.basic
 
 /-!
 # Cast of integers

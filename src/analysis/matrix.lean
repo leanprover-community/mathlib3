leanprover-community/mathlib3

commit 294159008a5ab2469cd4b69e5806fb20878ae6ba

feat(linear_algebra/matrix): Spectral theorem for matrices (#14231)
Co-authored-by: Eric Wieser <wieser.eric@gmail.com>

src/analysis/normed_space/pi_Lp.lean
@@ -83,8 +83,11 @@ to compare the `L^p` and `L^∞` distances through it. -/
 protected def equiv : pi_Lp p α ≃ Π (i : ι), α i :=
 equiv.refl _
 
-@[simp] lemma equiv_apply (x : pi_Lp p α) (i : ι) : pi_Lp.equiv p α x i = x i := rfl
-@[simp] lemma equiv_symm_apply (x : Π i, α i) (i : ι) : (pi_Lp.equiv p α).symm x i = x i := rfl
+lemma equiv_apply (x : pi_Lp p α) (i : ι) : pi_Lp.equiv p α x i = x i := rfl
+lemma equiv_symm_apply (x : Π i, α i) (i : ι) : (pi_Lp.equiv p α).symm x i = x i := rfl
+
+@[simp] lemma equiv_apply' (x : pi_Lp p α) : pi_Lp.equiv p α x = x := rfl
+@[simp] lemma equiv_symm_apply' (x : Π i, α i) : (pi_Lp.equiv p α).symm x = x := rfl
 
 section
 /-!

src/linear_algebra/matrix/spectrum.lean
@@ -1,0 +1,89 @@
+/-
+Copyright (c) 2022 Alexander Bentkamp. All rights reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
+Authors: Alexander Bentkamp
+-/
+import analysis.inner_product_space.spectrum
+import linear_algebra.matrix.hermitian
+
+/-! # Spectral theory of hermitian matrices
+
+This file proves the spectral theorem for matrices. The proof of the spectral theorem is based on
+the spectral theorem for linear maps (`diagonalization_basis_apply_self_apply`).
+
+## Tags
+
+spectral theorem, diagonalization theorem
+
+-/
+
+namespace matrix
+
+variables {𝕜 : Type*} [is_R_or_C 𝕜] [decidable_eq 𝕜] {n : Type*} [fintype n] [decidable_eq n]
+variables {A : matrix n n 𝕜}
+
+open_locale matrix
+
+namespace is_hermitian
+
+variables (hA : A.is_hermitian)
+
+/-- The eigenvalues of a hermitian matrix, indexed by `fin (fintype.card n)` where `n` is the index
+type of the matrix. -/
+noncomputable def eigenvalues₀ : fin (fintype.card n) → ℝ :=
+@inner_product_space.is_self_adjoint.eigenvalues 𝕜 _ _ (pi_Lp 2 (λ (_ : n), 𝕜)) _ A.to_lin'
+  (is_hermitian_iff_is_self_adjoint.1 hA) _ (fintype.card n) finrank_euclidean_space
+
+/-- The eigenvalues of a hermitian matrix, reusing the index `n` of the matrix entries. -/
+noncomputable def eigenvalues : n → ℝ :=
+λ i, hA.eigenvalues₀ $ (fintype.equiv_of_card_eq (fintype.card_fin _)).symm i
+
+/-- A choice of an orthonormal basis of eigenvectors of a hermitian matrix. -/
+noncomputable def eigenvector_basis : basis n 𝕜 (n → 𝕜) :=
+(@inner_product_space.is_self_adjoint.eigenvector_basis 𝕜 _ _
+    (pi_Lp 2 (λ (_ : n), 𝕜)) _ A.to_lin' (is_hermitian_iff_is_self_adjoint.1 hA) _
+    (fintype.card n) finrank_euclidean_space).reindex
+  (fintype.equiv_of_card_eq (fintype.card_fin _))
+
+/-- A matrix whose columns are an orthonormal basis of eigenvectors of a hermitian matrix. -/
+noncomputable def eigenvector_matrix : matrix n n 𝕜 :=
+(pi.basis_fun 𝕜 n).to_matrix (eigenvector_basis hA)
+
+/-- The inverse of `eigenvector_matrix` -/
+noncomputable def eigenvector_matrix_inv : matrix n n 𝕜 :=
+(eigenvector_basis hA).to_matrix (pi.basis_fun 𝕜 n)
+
+lemma eigenvector_matrix_mul_inv :
+  hA.eigenvector_matrix ⬝ hA.eigenvector_matrix_inv = 1 :=
+by apply basis.to_matrix_mul_to_matrix_flip
+
+/-- *Diagonalization theorem*, *spectral theorem* for matrices; A hermitian matrix can be
+diagonalized by a change of basis.
+
+For the spectral theorem on linear maps, see `diagonalization_basis_apply_self_apply`. -/
+theorem spectral_theorem :
+  hA.eigenvector_matrix_inv ⬝ A =
+    diagonal (coe ∘ hA.eigenvalues) ⬝ hA.eigenvector_matrix_inv :=
+begin
+  rw [eigenvector_matrix_inv, basis_to_matrix_basis_fun_mul],
+  ext i j,
+  convert @inner_product_space.is_self_adjoint.diagonalization_basis_apply_self_apply 𝕜 _ _
+    (pi_Lp 2 (λ (_ : n), 𝕜)) _ A.to_lin' (is_hermitian_iff_is_self_adjoint.1 hA) _ (fintype.card n)
+    finrank_euclidean_space (euclidean_space.single j 1)
+    ((fintype.equiv_of_card_eq (fintype.card_fin _)).symm i),
+  { rw [eigenvector_basis, inner_product_space.is_self_adjoint.diagonalization_basis,
+      to_lin'_apply],
+    simp only [basis.to_matrix, basis.coe_to_orthonormal_basis_repr, basis.equiv_fun_apply],
+    simp_rw [basis.reindex_repr, euclidean_space.single, pi_Lp.equiv_symm_apply', mul_vec_single,
+      mul_one],
+    refl },
+  { simp only [diagonal_mul, (∘), eigenvalues, eigenvector_basis,
+      inner_product_space.is_self_adjoint.diagonalization_basis],
+    rw [basis.to_matrix_apply, basis.coe_to_orthonormal_basis_repr, basis.reindex_repr,
+      basis.equiv_fun_apply, pi.basis_fun_apply, eigenvalues₀, linear_map.coe_std_basis,
+      euclidean_space.single, pi_Lp.equiv_symm_apply'] }
+end
+
+end is_hermitian
+
+end matrix

src/analysis/inner_product_space/pi_L2.lean
@@ -39,7 +39,7 @@ open_locale big_operators uniformity topological_space nnreal ennreal complex_co
 
 noncomputable theory
 
-variables {ι : Type*}
+variables {ι : Type*} {ι' : Type*}
 variables {𝕜 : Type*} [is_R_or_C 𝕜] {E : Type*} [inner_product_space 𝕜 E]
 variables {E' : Type*} [inner_product_space 𝕜 E']
 variables {F : Type*} [inner_product_space ℝ F]
@@ -111,6 +111,9 @@ instance : inner_product_space 𝕜 (euclidean_space 𝕜 ι) := by apply_instan
 
 lemma finrank_euclidean_space_fin {n : ℕ} :
   finite_dimensional.finrank 𝕜 (euclidean_space 𝕜 (fin n)) = n := by simp
+
+lemma euclidean_space.inner_eq_star_dot_product (x y : euclidean_space 𝕜 ι) :
+  ⟪x, y⟫ = matrix.dot_product (star $ pi_Lp.equiv _ _ x) (pi_Lp.equiv _ _ y) := rfl
 
 /-- A finite, mutually orthogonal family of subspaces of `E`, which span `E`, induce an isometry
 from `E` to `pi_Lp 2` of the subspaces equipped with the `L2` inner product. -/

src/linear_algebra/matrix/basis.lean
@@ -177,6 +177,32 @@ lemma basis_to_matrix_mul_linear_map_to_matrix_mul_basis_to_matrix
   c.to_matrix c' ⬝ linear_map.to_matrix b' c' f ⬝ b'.to_matrix b = linear_map.to_matrix b c f :=
 by rw [basis_to_matrix_mul_linear_map_to_matrix, linear_map_to_matrix_mul_basis_to_matrix]
 
+lemma basis_to_matrix_mul [decidable_eq κ]
+    (b₁ : basis ι R M) (b₂ : basis ι' R M) (b₃ : basis κ R N) (A : matrix ι' κ R) :
+  b₁.to_matrix b₂ ⬝ A = linear_map.to_matrix b₃ b₁ (to_lin b₃ b₂ A) :=
+begin
+  have := basis_to_matrix_mul_linear_map_to_matrix b₃ b₁ b₂ (matrix.to_lin b₃ b₂ A),
+  rwa [linear_map.to_matrix_to_lin] at this
+end
+
+lemma mul_basis_to_matrix [decidable_eq ι] [decidable_eq ι']
+    (b₁ : basis ι R M) (b₂ : basis ι' R M) (b₃ : basis κ R N) (A : matrix κ ι R) :
+  A ⬝ b₁.to_matrix b₂ = linear_map.to_matrix b₂ b₃ (to_lin b₁ b₃ A) :=
+begin
+  have := linear_map_to_matrix_mul_basis_to_matrix b₂ b₁ b₃ (matrix.to_lin b₁ b₃ A),
+  rwa [linear_map.to_matrix_to_lin] at this
+end
+
+lemma basis_to_matrix_basis_fun_mul (b : basis ι R (ι → R)) (A : matrix ι ι R) :
+  b.to_matrix (pi.basis_fun R ι) ⬝ A = (λ i j, b.repr (Aᵀ j) i) :=
+begin
+  classical,
+  simp only [basis_to_matrix_mul _ _ (pi.basis_fun R ι), matrix.to_lin_eq_to_lin'],
+  ext i j,
+  rw [linear_map.to_matrix_apply, matrix.to_lin'_apply, pi.basis_fun_apply,
+    matrix.mul_vec_std_basis_apply]
+end
+
 /-- A generalization of `linear_map.to_matrix_id`. -/
 @[simp] lemma linear_map.to_matrix_id_eq_basis_to_matrix [decidable_eq ι] :
   linear_map.to_matrix b b' id = b'.to_matrix b :=

src/linear_algebra/matrix/hermitian.lean
@@ -1,0 +1,176 @@
+/-
+Copyright (c) 2022 Alexander Bentkamp. All rights reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
+Authors: Alexander Bentkamp
+-/
+import analysis.inner_product_space.spectrum
+
+/-! # Hermitian matrices
+
+This file defines hermitian matrices and some basic results about them.
+
+## Main definition
+
+ * `matrix.is_hermitian `: a matrix `A : matrix n n α` is hermitian if `Aᴴ = A`.
+
+## Tags
+
+self-adjoint matrix, hermitian matrix
+
+-/
+
+namespace matrix
+
+variables {α β : Type*} {m n : Type*} {A : matrix n n α}
+
+open_locale matrix
+
+local notation `⟪`x`, `y`⟫` := @inner α (pi_Lp 2 (λ (_ : n), α)) _ x y
+
+section non_unital_semiring
+
+variables [non_unital_semiring α] [star_ring α] [non_unital_semiring β] [star_ring β]
+
+/-- A matrix is hermitian if it is equal to its conjugate transpose. On the reals, this definition
+captures symmetric matrices. -/
+def is_hermitian (A : matrix n n α) : Prop := Aᴴ = A
+
+lemma is_hermitian.eq {A : matrix n n α} (h : A.is_hermitian) : Aᴴ = A := h
+
+@[ext]
+lemma is_hermitian.ext {A : matrix n n α} : (∀ i j, star (A j i) = A i j) → A.is_hermitian :=
+by { intros h, ext i j, exact h i j }
+
+lemma is_hermitian.apply {A : matrix n n α} (h : A.is_hermitian) (i j : n) : star (A j i) = A i j :=
+by { unfold is_hermitian at h, rw [← h, conj_transpose_apply, star_star, h] }
+
+lemma is_hermitian.ext_iff {A : matrix n n α} : A.is_hermitian ↔ ∀ i j, star (A j i) = A i j :=
+⟨is_hermitian.apply, is_hermitian.ext⟩
+
+lemma is_hermitian_mul_conj_transpose_self [fintype n] (A : matrix n n α) :
+  (A ⬝ Aᴴ).is_hermitian :=
+by rw [is_hermitian, conj_transpose_mul, conj_transpose_conj_transpose]
+
+lemma is_hermitian_transpose_mul_self [fintype n] (A : matrix n n α) :
+  (Aᴴ ⬝ A).is_hermitian :=
+by rw [is_hermitian, conj_transpose_mul, conj_transpose_conj_transpose]
+
+lemma is_hermitian_add_transpose_self (A : matrix n n α) :
+  (A + Aᴴ).is_hermitian :=
+by simp [is_hermitian, add_comm]
+
+lemma is_hermitian_transpose_add_self (A : matrix n n α) :
+  (Aᴴ + A).is_hermitian :=
+by simp [is_hermitian, add_comm]
+
+@[simp] lemma is_hermitian_zero :
+  (0 : matrix n n α).is_hermitian :=
+conj_transpose_zero
+
+-- TODO: move
+lemma conj_transpose_map {A : matrix n n α} (f : α → β) (hf : f ∘ star = star ∘ f) :
+  Aᴴ.map f = (A.map f)ᴴ :=
+by rw [conj_transpose, conj_transpose, ←transpose_map, map_map, map_map, hf]
+
+@[simp] lemma is_hermitian.map {A : matrix n n α} (h : A.is_hermitian) (f : α → β)
+    (hf : f ∘ star = star ∘ f) :
+  (A.map f).is_hermitian :=
+by {refine (conj_transpose_map f hf).symm.trans _, rw h.eq }
+
+@[simp] lemma is_hermitian.transpose {A : matrix n n α} (h : A.is_hermitian) :
+  Aᵀ.is_hermitian :=
+by { rw [is_hermitian, conj_transpose, transpose_map], congr, exact h }
+
+@[simp] lemma is_hermitian.conj_transpose {A : matrix n n α} (h : A.is_hermitian) :
+  Aᴴ.is_hermitian :=
+h.transpose.map _ rfl
+
+@[simp] lemma is_hermitian.add {A B : matrix n n α} (hA : A.is_hermitian) (hB : B.is_hermitian) :
+  (A + B).is_hermitian :=
+(conj_transpose_add _ _).trans (hA.symm ▸ hB.symm ▸ rfl)
+
+@[simp] lemma is_hermitian.minor {A : matrix n n α} (h : A.is_hermitian) (f : m → n) :
+  (A.minor f f).is_hermitian :=
+(conj_transpose_minor _ _ _).trans (h.symm ▸ rfl)
+
+/-- The real diagonal matrix `diagonal v` is hermitian. -/
+@[simp] lemma is_hermitian_diagonal [decidable_eq n] (v : n → ℝ) :
+  (diagonal v).is_hermitian :=
+diagonal_conj_transpose _
+
+/-- A block matrix `A.from_blocks B C D` is hermitian,
+    if `A` and `D` are hermitian and `Bᴴ = C`. -/
+lemma is_hermitian.from_blocks
+  {A : matrix m m α} {B : matrix m n α} {C : matrix n m α} {D : matrix n n α}
+  (hA : A.is_hermitian) (hBC : Bᴴ = C) (hD : D.is_hermitian) :
+  (A.from_blocks B C D).is_hermitian :=
+begin
+  have hCB : Cᴴ = B, {rw ← hBC, simp},
+  unfold matrix.is_hermitian,
+  rw from_blocks_conj_transpose,
+  congr;
+  assumption
+end
+
+/-- This is the `iff` version of `matrix.is_hermitian.from_blocks`. -/
+lemma is_hermitian_from_blocks_iff
+  {A : matrix m m α} {B : matrix m n α} {C : matrix n m α} {D : matrix n n α} :
+  (A.from_blocks B C D).is_hermitian ↔ A.is_hermitian ∧ Bᴴ = C ∧ Cᴴ = B ∧ D.is_hermitian :=
+⟨λ h, ⟨congr_arg to_blocks₁₁ h, congr_arg to_blocks₂₁ h,
+       congr_arg to_blocks₁₂ h, congr_arg to_blocks₂₂ h⟩,
+ λ ⟨hA, hBC, hCB, hD⟩, is_hermitian.from_blocks hA hBC hD⟩
+
+end non_unital_semiring
+
+section semiring
+
+variables [semiring α] [star_ring α] [semiring β] [star_ring β]
+
+@[simp] lemma is_hermitian_one [decidable_eq n] :
+  (1 : matrix n n α).is_hermitian :=
+conj_transpose_one
+
+end semiring
+
+section ring
+
+variables [ring α] [star_ring α] [ring β] [star_ring β]
+
+@[simp] lemma is_hermitian.neg {A : matrix n n α} (h : A.is_hermitian) :
+  (-A).is_hermitian :=
+(conj_transpose_neg _).trans (congr_arg _ h)
+
+@[simp] lemma is_hermitian.sub {A B : matrix n n α} (hA : A.is_hermitian) (hB : B.is_hermitian) :
+  (A - B).is_hermitian :=
+(conj_transpose_sub _ _).trans (hA.symm ▸ hB.symm ▸ rfl)
+
+end ring
+
+section is_R_or_C
+
+variables [is_R_or_C α] [is_R_or_C β]
+
+/-- A matrix is hermitian iff the corresponding linear map is self adjoint. -/
+lemma is_hermitian_iff_is_self_adjoint [fintype n] [decidable_eq n] {A : matrix n n α} :
+  is_hermitian A ↔ inner_product_space.is_self_adjoint
+    ((pi_Lp.linear_equiv α (λ _ : n, α)).symm.conj A.to_lin' : module.End α (pi_Lp 2 _)) :=
+begin
+  rw [inner_product_space.is_self_adjoint, (pi_Lp.equiv 2 (λ _ : n, α)).symm.surjective.forall₂],
+  simp only [linear_equiv.conj_apply, linear_map.comp_apply, linear_equiv.coe_coe,
+    pi_Lp.linear_equiv_apply, pi_Lp.linear_equiv_symm_apply, linear_equiv.symm_symm],
+  simp_rw [euclidean_space.inner_eq_star_dot_product, equiv.apply_symm_apply, to_lin'_apply,
+    star_mul_vec, dot_product_mul_vec],
+  split,
+  { rintro (h : Aᴴ = A) x y,
+    rw h },
+  { intro h,
+    ext i j,
+    simpa only [(pi.single_star i 1).symm, ← star_mul_vec, mul_one, dot_product_single,
+      single_vec_mul, star_one, one_mul] using
+        h (@pi.single _ _ _ (λ i, add_zero_class.to_has_zero α) i 1)
+          (@pi.single _ _ _ (λ i, add_zero_class.to_has_zero α) j 1) }
+end
+
+end is_R_or_C
+
+end matrix

src/linear_algebra/matrix/to_lin.lean
@@ -172,9 +172,13 @@ variables {l m n : Type*}
 
 variables [fintype n] [decidable_eq n]
 
-@[simp] lemma matrix.mul_vec_std_basis (M : matrix m n R) (i j) :
+lemma matrix.mul_vec_std_basis (M : matrix m n R) (i j) :
   M.mul_vec (std_basis R (λ _, R) j 1) i = M i j :=
 (congr_fun (matrix.mul_vec_single _ _ (1 : R)) i).trans $ mul_one _
+
+@[simp] lemma matrix.mul_vec_std_basis_apply (M : matrix m n R) (j) :
+  M.mul_vec (std_basis R (λ _, R) j 1) = Mᵀ j :=
+funext $ λ i, matrix.mul_vec_std_basis M i j
 
 /-- Linear maps `(n → R) →ₗ[R] (m → R)` are linearly equivalent to `matrix m n R`. -/
 def linear_map.to_matrix' : ((n → R) →ₗ[R] (m → R)) ≃ₗ[R] matrix m n R :=

commit 9dffb788782da78d527f438cbc105150791fe920

fix(analysis/normed_space/pi_Lp): remove bad simp lemmas for `pi_Lp.equiv` (#17327)
This regression was introduced in #14231. These lemmas are a bad idea for the same reason that a lemma stating `multiplicative.of_add x = x` would be; it discards the type information that is the sole reason for the existance of the `def`!

This also adds a new `pi_Lp.basis_fun` which is the `pi_Lp` version of `pi.basis_fun`.

src/analysis/matrix.lean
@@ -399,7 +399,7 @@ instance frobenius_normed_star_group [star_add_monoid α] [normed_star_group α]
 @[simp] lemma frobenius_norm_row (v : m → α) : ∥row v∥ = ∥(pi_Lp.equiv 2 _).symm v∥ :=
 begin
   rw [frobenius_norm_def, fintype.sum_unique, pi_Lp.norm_eq_of_L2, real.sqrt_eq_rpow],
-  simp only [row_apply, real.rpow_two, pi_Lp.equiv_symm_apply'],
+  simp only [row_apply, real.rpow_two, pi_Lp.equiv_symm_apply],
 end
 @[simp] lemma frobenius_nnnorm_row (v : m → α) : ∥row v∥₊ = ∥(pi_Lp.equiv 2 _).symm v∥₊ :=
 subtype.ext $ frobenius_norm_row v
@@ -407,7 +407,7 @@ subtype.ext $ frobenius_norm_row v
 @[simp] lemma frobenius_norm_col (v : n → α) : ∥col v∥ = ∥(pi_Lp.equiv 2 _).symm v∥ :=
 begin
   simp_rw [frobenius_norm_def, fintype.sum_unique, pi_Lp.norm_eq_of_L2, real.sqrt_eq_rpow],
-  simp only [col_apply, real.rpow_two, pi_Lp.equiv_symm_apply']
+  simp only [col_apply, real.rpow_two, pi_Lp.equiv_symm_apply]
 end
 @[simp] lemma frobenius_nnnorm_col (v : n → α) : ∥col v∥₊ = ∥(pi_Lp.equiv 2 _).symm v∥₊ :=
 subtype.ext $ frobenius_norm_col v

src/analysis/normed_space/pi_Lp.lean
@@ -63,32 +63,30 @@ open_locale big_operators uniformity topological_space nnreal ennreal
 
 noncomputable theory
 
-variables {ι : Type*}
-
 /-- A copy of a Pi type, on which we will put the `L^p` distance. Since the Pi type itself is
 already endowed with the `L^∞` distance, we need the type synonym to avoid confusing typeclass
 resolution. Also, we let it depend on `p`, to get a whole family of type on which we can put
 different distances. -/
 @[nolint unused_arguments]
-def pi_Lp {ι : Type*} (p : ℝ≥0∞) (α : ι → Type*) : Type* := Π (i : ι), α i
+def pi_Lp (p : ℝ≥0∞) {ι : Type*} (α : ι → Type*) : Type* := Π (i : ι), α i
 
-instance {ι : Type*} (p : ℝ≥0∞) (α : ι → Type*) [Π i, inhabited (α i)] : inhabited (pi_Lp p α) :=
+instance (p : ℝ≥0∞) {ι : Type*} (α : ι → Type*) [Π i, inhabited (α i)] : inhabited (pi_Lp p α) :=
 ⟨λ i, default⟩
 
 namespace pi_Lp
 
-variables (p : ℝ≥0∞) (𝕜 : Type*) (α : ι → Type*) (β : ι → Type*)
+variables (p : ℝ≥0∞) (𝕜 : Type*) {ι : Type*} (α : ι → Type*) (β : ι → Type*)
 
 /-- Canonical bijection between `pi_Lp p α` and the original Pi type. We introduce it to be able
 to compare the `L^p` and `L^∞` distances through it. -/
 protected def equiv : pi_Lp p α ≃ Π (i : ι), α i :=
 equiv.refl _
 
-lemma equiv_apply (x : pi_Lp p α) (i : ι) : pi_Lp.equiv p α x i = x i := rfl
-lemma equiv_symm_apply (x : Π i, α i) (i : ι) : (pi_Lp.equiv p α).symm x i = x i := rfl
+/-! Note that the unapplied versions of these lemmas are deliberately omitted, as they break
+the use of the type synonym. -/
 
-@[simp] lemma equiv_apply' (x : pi_Lp p α) : pi_Lp.equiv p α x = x := rfl
-@[simp] lemma equiv_symm_apply' (x : Π i, α i) : (pi_Lp.equiv p α).symm x = x := rfl
+@[simp] lemma equiv_apply (x : pi_Lp p α) (i : ι) : pi_Lp.equiv p α x i = x i := rfl
+@[simp] lemma equiv_symm_apply (x : Π i, α i) (i : ι) : (pi_Lp.equiv p α).symm x i = x i := rfl
 
 section dist_norm
 variables [fintype ι]
@@ -320,7 +318,7 @@ lemma lipschitz_with_equiv_aux : lipschitz_with 1 (pi_Lp.equiv p β) :=
 begin
   intros x y,
   unfreezingI { rcases p.dichotomy with (rfl | h) },
-  { simpa only [equiv_apply', ennreal.coe_one, one_mul, edist_eq_supr, edist, finset.sup_le_iff,
+  { simpa only [ennreal.coe_one, one_mul, edist_eq_supr, edist, finset.sup_le_iff,
       finset.mem_univ, forall_true_left] using le_supr (λ i, edist (x i) (y i)), },
   { have cancel : p.to_real * (1/p.to_real) = 1 := mul_div_cancel' 1 (zero_lt_one.trans_le h).ne',
     rw edist_eq_sum (zero_lt_one.trans_le h),
@@ -343,13 +341,13 @@ begin
   intros x y,
   unfreezingI { rcases p.dichotomy with (rfl | h) },
   { simp only [edist_eq_supr, ennreal.div_top, ennreal.zero_to_real, nnreal.rpow_zero,
-      ennreal.coe_one, equiv_apply', one_mul, supr_le_iff],
+      ennreal.coe_one, one_mul, supr_le_iff],
     exact λ i, finset.le_sup (finset.mem_univ i), },
   { have pos : 0 < p.to_real := zero_lt_one.trans_le h,
     have nonneg : 0 ≤ 1 / p.to_real := one_div_nonneg.2 (le_of_lt pos),
     have cancel : p.to_real * (1/p.to_real) = 1 := mul_div_cancel' 1 (ne_of_gt pos),
     rw [edist_eq_sum pos, ennreal.to_real_div 1 p],
-    simp only [edist, equiv_apply', ←one_div, ennreal.one_to_real],
+    simp only [edist, ←one_div, ennreal.one_to_real],
     calc (∑ i, edist (x i) (y i) ^ p.to_real) ^ (1 / p.to_real) ≤
     (∑ i, edist (pi_Lp.equiv p β x) (pi_Lp.equiv p β y) ^ p.to_real) ^ (1 / p.to_real) :
     begin
@@ -650,7 +648,7 @@ lemma nnnorm_equiv_symm_const' {β} [seminormed_add_comm_group β] [nonempty ι]
   fintype.card ι ^ (1 / p).to_real * ∥b∥₊ :=
 begin
   unfreezingI { rcases (em $ p = ∞) with (rfl | hp) },
-  { simp only [equiv_symm_apply', ennreal.div_top, ennreal.zero_to_real, nnreal.rpow_zero, one_mul,
+  { simp only [equiv_symm_apply, ennreal.div_top, ennreal.zero_to_real, nnreal.rpow_zero, one_mul,
       nnnorm_eq_csupr, function.const_apply, csupr_const], },
   { exact nnnorm_equiv_symm_const hp b, },
 end
@@ -689,5 +687,45 @@ protected def linear_equiv : pi_Lp p β ≃ₗ[𝕜] Π i, β i :=
 { to_fun := pi_Lp.equiv _ _,
   inv_fun := (pi_Lp.equiv _ _).symm,
   ..linear_equiv.refl _ _}
+
+section basis
+
+variables (ι)
+
+/-- A version of `pi.basis_fun` for `pi_Lp`. -/
+def basis_fun : basis ι 𝕜 (pi_Lp p (λ _, 𝕜)) :=
+basis.of_equiv_fun (pi_Lp.linear_equiv p 𝕜 (λ _ : ι, 𝕜))
+
+@[simp] lemma basis_fun_apply [decidable_eq ι] (i) :
+  basis_fun p 𝕜 ι i = (pi_Lp.equiv p _).symm (pi.single i 1) :=
+by { simp_rw [basis_fun, basis.coe_of_equiv_fun, pi_Lp.linear_equiv_symm_apply, pi.single],
+     congr /- Get rid of a `decidable_eq` mismatch. -/ }
+
+@[simp] lemma basis_fun_repr (x : pi_Lp p (λ i : ι, 𝕜)) (i : ι) :
+  (basis_fun p 𝕜 ι).repr x i = x i :=
+rfl
+
+lemma basis_fun_eq_pi_basis_fun :
+  basis_fun p 𝕜 ι = (pi.basis_fun 𝕜 ι).map (pi_Lp.linear_equiv p 𝕜 (λ _ : ι, 𝕜)).symm :=
+rfl
+
+@[simp] lemma basis_fun_map :
+  (basis_fun p 𝕜 ι).map (pi_Lp.linear_equiv p 𝕜 (λ _ : ι, 𝕜)) = pi.basis_fun 𝕜 ι :=
+rfl
+
+open_locale matrix
+
+lemma basis_to_matrix_basis_fun_mul (b : basis ι 𝕜 (pi_Lp p (λ i : ι, 𝕜))) (A : matrix ι ι 𝕜) :
+  b.to_matrix (pi_Lp.basis_fun _ _ _) ⬝ A =
+    matrix.of (λ i j, b.repr ((pi_Lp.equiv _ _).symm (Aᵀ j)) i) :=
+begin
+  have := basis_to_matrix_basis_fun_mul (b.map (pi_Lp.linear_equiv _ 𝕜 _)) A,
+  simp_rw [←pi_Lp.basis_fun_map p, basis.map_repr, linear_equiv.trans_apply,
+    pi_Lp.linear_equiv_symm_apply, basis.to_matrix_map, function.comp, basis.map_apply,
+    linear_equiv.symm_apply_apply] at this,
+  exact this,
+end
+
+end basis
 
 end pi_Lp

src/linear_algebra/matrix/spectrum.lean
@@ -45,11 +45,11 @@ noncomputable def eigenvector_basis : orthonormal_basis n 𝕜 (euclidean_space 
 
 /-- A matrix whose columns are an orthonormal basis of eigenvectors of a hermitian matrix. -/
 noncomputable def eigenvector_matrix : matrix n n 𝕜 :=
-(pi.basis_fun 𝕜 n).to_matrix (eigenvector_basis hA).to_basis
+(pi_Lp.basis_fun _ 𝕜 n).to_matrix (eigenvector_basis hA).to_basis
 
 /-- The inverse of `eigenvector_matrix` -/
 noncomputable def eigenvector_matrix_inv : matrix n n 𝕜 :=
-(eigenvector_basis hA).to_basis.to_matrix (pi.basis_fun 𝕜 n)
+(eigenvector_basis hA).to_basis.to_matrix (pi_Lp.basis_fun _ 𝕜 n)
 
 lemma eigenvector_matrix_mul_inv :
   hA.eigenvector_matrix ⬝ hA.eigenvector_matrix_inv = 1 :=
@@ -62,17 +62,15 @@ noncomputable instance : invertible hA.eigenvector_matrix :=
 invertible_of_right_inverse _ _ hA.eigenvector_matrix_mul_inv
 
 lemma eigenvector_matrix_apply (i j : n) : hA.eigenvector_matrix i j = hA.eigenvector_basis j i :=
-by simp only [eigenvector_matrix, basis.to_matrix_apply, orthonormal_basis.coe_to_basis,
-  pi.basis_fun_repr]
+by simp_rw [eigenvector_matrix, basis.to_matrix_apply, orthonormal_basis.coe_to_basis,
+    pi_Lp.basis_fun_repr]
 
 lemma eigenvector_matrix_inv_apply (i j : n) :
   hA.eigenvector_matrix_inv i j = star (hA.eigenvector_basis i j) :=
 begin
   rw [eigenvector_matrix_inv, basis.to_matrix_apply, orthonormal_basis.coe_to_basis_repr_apply,
-    pi.basis_fun_apply, linear_map.coe_std_basis, orthonormal_basis.repr_apply_apply],
-  change inner (hA.eigenvector_basis i) (euclidean_space.single j 1) = _,
-  rw [euclidean_space.inner_single_right],
-  simp only [one_mul, conj_transpose_apply, is_R_or_C.star_def],
+    orthonormal_basis.repr_apply_apply, pi_Lp.basis_fun_apply, pi_Lp.equiv_symm_single,
+    euclidean_space.inner_single_right, one_mul, is_R_or_C.star_def],
 end
 
 lemma conj_transpose_eigenvector_matrix_inv : hA.eigenvector_matrix_invᴴ = hA.eigenvector_matrix :=
@@ -90,22 +88,25 @@ theorem spectral_theorem :
   hA.eigenvector_matrix_inv ⬝ A =
     diagonal (coe ∘ hA.eigenvalues) ⬝ hA.eigenvector_matrix_inv :=
 begin
-  rw [eigenvector_matrix_inv, basis_to_matrix_basis_fun_mul],
+  rw [eigenvector_matrix_inv, pi_Lp.basis_to_matrix_basis_fun_mul],
   ext i j,
-  convert @linear_map.is_symmetric.diagonalization_basis_apply_self_apply 𝕜 _ _
-    (pi_Lp 2 (λ (_ : n), 𝕜)) _ A.to_lin' (is_hermitian_iff_is_symmetric.1 hA) _ (fintype.card n)
-    finrank_euclidean_space (euclidean_space.single j 1)
+  have : linear_map.is_symmetric _ := is_hermitian_iff_is_symmetric.1 hA,
+  convert this.diagonalization_basis_apply_self_apply finrank_euclidean_space
+    (euclidean_space.single j 1)
     ((fintype.equiv_of_card_eq (fintype.card_fin _)).symm i),
-  { rw [eigenvector_basis, to_lin'_apply],
+  { dsimp only [linear_equiv.conj_apply_apply, pi_Lp.linear_equiv_apply,
+      pi_Lp.linear_equiv_symm_apply, pi_Lp.equiv_single, linear_map.std_basis,
+      linear_map.coe_single, pi_Lp.equiv_symm_single, linear_equiv.symm_symm,
+      eigenvector_basis, to_lin'_apply],
     simp only [basis.to_matrix, basis.coe_to_orthonormal_basis_repr, basis.equiv_fun_apply],
     simp_rw [orthonormal_basis.coe_to_basis_repr_apply, orthonormal_basis.reindex_repr,
-      euclidean_space.single, pi_Lp.equiv_symm_apply', mul_vec_single, mul_one],
+      linear_equiv.symm_symm, pi_Lp.linear_equiv_apply, pi_Lp.equiv_single, mul_vec_single,
+      mul_one],
     refl },
   { simp only [diagonal_mul, (∘), eigenvalues, eigenvector_basis],
     rw [basis.to_matrix_apply,
       orthonormal_basis.coe_to_basis_repr_apply, orthonormal_basis.reindex_repr,
-      pi.basis_fun_apply, eigenvalues₀, linear_map.coe_std_basis,
-      euclidean_space.single, pi_Lp.equiv_symm_apply'] }
+      eigenvalues₀, pi_Lp.basis_fun_apply, pi_Lp.equiv_symm_single] }
 end
 
 lemma eigenvalues_eq (i : n) :
2c9d8de7d49776ceb0b752b2a8337487d58c4bd0
Joël Riou <joel.riou@universite-paris-saclay.fr>
2022-11-10 13:10:26 +0100

    tidied the code

--- src/algebra/homology/single.lean
+++ src/algebra/homology/single.lean
@@ -219,11 +219,9 @@ noncomputable
 def homology_functor_0_single₀ [category_with_homology V] :
   single₀ V ⋙ homology_functor V _ 0 ≅ (𝟭 V) :=
 nat_iso.of_components homology_single₀_zero
-  (λ X Y f, begin
-    dsimp [homology_single₀_zero],
-    simp [(homology_map_data_single₀_map_zero f).homology_map_eq],
-end)
+  (λ X Y f, (homology_map_data_single₀_map_zero f).map_comm)
 
+#exit
 /--
 Sending objects to chain complexes supported at `0` then taking `(n+1)`-st homology
 is the same as the zero functor.

d284a9429c59f6349e15dc7438f83959f670f1be
Jireh Loreaux <loreaujy@gmail.com>
2022-11-10 23:05:41 -0600

    do a pre-clean before merging

--- src/analysis/normed_space/star/gelfand_duality.lean
+++ src/analysis/normed_space/star/gelfand_duality.lean
@@ -110,24 +110,28 @@ end complex_banach_algebra
 
 section complex_cstar_algebra
 
-variables (A : Type*) [normed_comm_ring A] [normed_algebra ℂ A] [complete_space A]
+variables {A : Type*} [normed_comm_ring A] [normed_algebra ℂ A] [complete_space A]
 variables [star_ring A] [cstar_ring A] [star_module ℂ A]
+
+lemma gelfand_transform_map_star (a : A) :
+  gelfand_transform ℂ A (star a) = star (gelfand_transform ℂ A a) :=
+continuous_map.ext $ λ φ, map_star φ a
+
+variable (A)
 
 /-- The Gelfand transform is an isometry when the algebra is a C⋆-algebra over `ℂ`. -/
 lemma gelfand_transform_isometry : isometry (gelfand_transform ℂ A) :=
 begin
   nontriviality A,
   refine add_monoid_hom_class.isometry_of_norm (gelfand_transform ℂ A) (λ a, _),
-  have gt_map_star : gelfand_transform ℂ A (star a) = star (gelfand_transform ℂ A a),
-    from continuous_map.ext (λ φ, map_star φ a),
   /- By `spectrum.gelfand_transform_eq`, the spectra of `star a * a` and its
   `gelfand_transform` coincide. Therefore, so do their spectral radii, and since they are
   self-adjoint, so also do their norms. Applying the C⋆-property of the norm and taking square
   roots shows that the norm is preserved. -/
   have : spectral_radius ℂ (gelfand_transform ℂ A (star a * a)) = spectral_radius ℂ (star a * a),
   { unfold spectral_radius, rw spectrum.gelfand_transform_eq, },
-  simp only [map_mul, gt_map_star, (is_self_adjoint.star_mul_self _).spectral_radius_eq_nnnorm,
-    ennreal.coe_eq_coe, cstar_ring.nnnorm_star_mul_self, ←sq] at this,
+  simp only [map_mul, (is_self_adjoint.star_mul_self _).spectral_radius_eq_nnnorm,
+    gelfand_transform_map_star a, ennreal.coe_eq_coe, cstar_ring.nnnorm_star_mul_self, ←sq] at this,
   simpa only [function.comp_app, nnreal.sqrt_sq]
     using congr_arg ((coe : ℝ≥0 → ℝ) ∘ ⇑nnreal.sqrt) this,
 end
@@ -157,26 +161,16 @@ begin
   `weak_dual.star_hom_class`, which is a nontrivial result. -/
   { obtain ⟨f, ⟨a, rfl⟩, rfl⟩ := subalgebra.mem_map.mp hf,
     refine ⟨star a, continuous_map.ext $ λ ψ, _⟩,
-    simpa only [gelfand_transform_apply_apply, map_star, ring_hom.coe_monoid_hom,
-      alg_equiv.coe_alg_hom, ring_hom.to_monoid_hom_eq_coe, alg_equiv.to_alg_hom_eq_coe,
-      ring_hom.to_fun_eq_coe, continuous_map.coe_mk, is_R_or_C.conj_ae_coe,
-      alg_hom.coe_to_ring_hom, monoid_hom.to_fun_eq_coe, ring_hom.comp_left_continuous_apply,
-      monoid_hom.comp_left_continuous_apply, continuous_map.comp_apply,
-      alg_hom.to_ring_hom_eq_coe, alg_hom.comp_left_continuous_apply] },
+    simpa only [gelfand_transform_map_star a, alg_hom.to_ring_hom_eq_coe, alg_hom.coe_to_ring_hom] }
 end
-
-.
 
 /-- The Gelfand transform as a `star_alg_equiv` between a commutative unital C⋆-algebra over `ℂ`
 and the continuous functions on its `character_space`. -/
 @[simps]
 noncomputable def gelfand_star_transform : A ≃⋆ₐ[ℂ] C(character_space ℂ A, ℂ) :=
 star_alg_equiv.of_bijective
-  (show A →⋆ₐ[ℂ] C(character_space ℂ A, ℂ), from
-  { map_star' := λ x, continuous_map.ext $ λ φ, by simp only [alg_hom.to_fun_eq_coe,
-      is_R_or_C.star_def, eq_self_iff_true, continuous_map.star_apply,
-      weak_dual.gelfand_transform_apply_apply, map_star φ],
-    .. gelfand_transform ℂ A })
+  (show A →⋆ₐ[ℂ] C(character_space ℂ A, ℂ),
+    from { map_star' := λ x, gelfand_transform_map_star x, .. gelfand_transform ℂ A })
   (gelfand_transform_bijective A)
 
 end complex_cstar_algebra

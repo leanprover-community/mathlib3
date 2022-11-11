cead93130da7100f8a9fe22ee210f7636a91168f
Scott Morrison <scott@tqft.net>
2022-11-11 04:41:49 +0000

    refactor(*): simplifying import hierarchy up to dat.rat.order (#17435)
    My apologies that this is essentially unreviewable. I promise not to have dropped anything. :-) I appreciate that big PRs are terrible, but if I can't get this stuff through CI in a timely manner I just can't work on this refactor.
    
    This PR introduces some technical debt: the lemmas in `data.nat.basic` / `data.nat.order.basic` / `data.nat.order.lemmas` / `data.nat.lemmas` (and similarly for `data.int.*`) are a total mess, organised mainly by trying to reduce import dependency for `linear_ordered_field ℚ`. We're going to have to go back and reorganise these later.
    
    Before:
    
    ![data rat order before](https://user-images.githubusercontent.com/477956/200711738-69cae67b-fdb7-4ce5-9a71-a871fb16acdd.png)
    
    After:
    
    ![data rat order after](https://user-images.githubusercontent.com/477956/200711761-7382a13f-bae7-4e2a-bb9a-56f0894ebdff.png)
    
    That's reduced the import depth for `data.rat.order` from 33 to 25.
    
    (For reference a week ago it had been at 59.)
    
    
    
    Co-authored-by: Yury G. Kudryashov <urkud@urkud.name>
    Co-authored-by: Scott Morrison <scott.morrison@gmail.com>
    Co-authored-by: Yaël Dillies <yael.dillies@gmail.com>

--- src/data/list/basic.lean
+++ src/data/list/basic.lean
@@ -3,7 +3,9 @@ Copyright (c) 2014 Parikshit Khanna. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Parikshit Khanna, Jeremy Avigad, Leonardo de Moura, Floris van Doorn, Mario Carneiro
 -/
-import data.nat.order
+import algebra.order.with_zero
+import data.nat.order.lemmas
+import data.set.function
 
 /-!
 # Basic properties of lists

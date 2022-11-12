60f93eb769ee42d6ac3ef484f0e2937e8b195514
Yaël Dillies <yael.dillies@gmail.com>
2022-11-12 14:36:45 +0000

    fix number_theory.padics.padic_number

--- src/number_theory/padics/padic_numbers.lean
+++ src/number_theory/padics/padic_numbers.lean
@@ -1025,9 +1025,8 @@ lemma norm_le_one_iff_val_nonneg (x : ℚ_[p]) : ∥ x ∥ ≤ 1 ↔ 0 ≤ x.val
 begin
   by_cases hx : x = 0,
   { simp only [hx, norm_zero, valuation_zero, zero_le_one, le_refl], },
-  { rw [norm_eq_pow_val hx, ← zpow_zero (p : ℝ), zpow_le_iff_le
-      (nat.one_lt_cast.mpr (nat.prime.one_lt' p).1), right.neg_nonpos_iff],
-    apply_instance, }
+  { rw [norm_eq_pow_val hx, ← zpow_zero (p : ℝ), zpow_le_iff_le, right.neg_nonpos_iff],
+    exact nat.one_lt_cast.2 (nat.prime.one_lt' p).1 }
 end
 
 end norm_le_iff

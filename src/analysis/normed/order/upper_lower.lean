d59930f5f85de25881089e62ffb77578d81c9139
Yaël Dillies <yael.dillies@gmail.com>
2022-11-15 23:51:48 +0000

    bump

--- src/analysis/normed/order/upper_lower.lean
+++ src/analysis/normed/order/upper_lower.lean
@@ -3,6 +3,7 @@ Copyright (c) 2022 Yaël Dillies. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Yaël Dillies
 -/
+import algebra.order.field.pi
 import analysis.normed.group.pointwise
 import analysis.normed.order.basic
 import topology.algebra.order.upper_lower
@@ -17,52 +18,6 @@ set).
 We also prove lemmas specific to `ℝⁿ`. Those are helpful to prove that order-connected sets in `ℝⁿ`
 are measurable.
 -/
-
-section
-variables {E : Type*} [seminormed_group E]
-
-@[simp, to_additive] lemma dist_mul_self_right (a b : E) : dist b (a * b) = ∥a∥ :=
-by rw [←dist_one_left, ←dist_mul_right 1 a b, one_mul]
-
-@[simp, to_additive] lemma dist_mul_self_left (a b : E) : dist (a * b) b = ∥a∥ :=
-by rw [dist_comm, dist_mul_self_right]
-
-end
-
-section
-variables {α : Type*} [mul_one_class α] [has_lt α] [contravariant_class α α (*) (<)] {a b : α}
-
-@[to_additive] lemma one_lt_of_lt_mul_right : a < a * b → 1 < b :=
-by simpa only [mul_one] using (lt_of_mul_lt_mul_left' : a * 1 < a * b → 1 < b)
-
-end
-
-section
-variables {α : Type*} [mul_one_class α] [preorder α] [contravariant_class α α (*) (<)]
-  [has_exists_mul_of_le α] {a b : α}
-
-@[to_additive] lemma exists_one_lt_mul_of_lt' (h : a < b) : ∃ c, 1 < c ∧ a * c = b :=
-by { obtain ⟨c, rfl⟩ := exists_mul_of_le h.le, exact ⟨c, one_lt_of_lt_mul_right h, rfl⟩ }
-
-end
-
-section finite
-variables {α ι : Type*} [linear_ordered_semifield α] [has_exists_add_of_le α] [finite ι]
-  {x y : ι → α}
-
-lemma pi.exists_forall_pos_add_lt (h : ∀ i, x i < y i) : ∃ ε, 0 < ε ∧ ∀ i, x i + ε < y i :=
-begin
-  casesI nonempty_fintype ι,
-  casesI is_empty_or_nonempty ι,
-  { exact ⟨1, zero_lt_one, is_empty_elim⟩ },
-  choose ε hε hxε using λ i, exists_pos_add_of_lt' (h i),
-  obtain rfl : x + ε = y := funext hxε,
-  have hε : 0 < finset.univ.inf' finset.univ_nonempty ε := (finset.lt_inf'_iff _).2 (λ i _, hε _),
-  exact ⟨_, half_pos hε, λ i, add_lt_add_left ((half_lt_self hε).trans_le $ finset.inf'_le _ $
-    finset.mem_univ _) _⟩,
-end
-
-end finite
 
 open function metric set
 
@@ -127,7 +82,7 @@ begin
   obtain ⟨z, hz, hxz⟩ := metric.mem_closure_iff.1 hx _ hε,
   rw dist_pi_lt_iff hε at hxz,
   have hyz : ∀ i, y i < z i,
-  { refine λ i, (lt_sub_iff_add_lt.2 $ hxy _).trans_le (_root_.sub_le.1 $ (le_abs_self _).trans _),
+  { refine λ i, (lt_sub_iff_add_lt.2 $ hxy _).trans_le (sub_le_comm.1 $ (le_abs_self _).trans _),
     rw [←real.norm_eq_abs, ←dist_eq_norm],
     exact (hxz _).le },
   obtain ⟨δ, hδ, hyz⟩ := pi.exists_forall_pos_add_lt hyz,

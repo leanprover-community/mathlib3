17a9f8f7a4ef32ee65d418103367cf30e84e631d
Yury G. Kudryashov <urkud@urkud.name>
2022-11-16 05:07:35 +0000

    chore(*): golf (#17558)
    Golfing part of #17553

--- src/set_theory/cardinal/basic.lean
+++ src/set_theory/cardinal/basic.lean
@@ -893,21 +893,14 @@ lemma mk_finsupp_of_fintype (α β : Type u) [fintype α] [has_zero β] :
 by simp
 
 theorem card_le_of_finset {α} (s : finset α) : (s.card : cardinal) ≤ #α :=
-begin
-  rw (_ : (s.card : cardinal) = #s),
-  { exact ⟨function.embedding.subtype _⟩ },
-  rw [cardinal.mk_fintype, fintype.card_coe]
-end
+@mk_coe_finset _ s ▸ mk_set_le _
 
 @[norm_cast] theorem nat_cast_pow {m n : ℕ} : (↑(pow m n) : cardinal) = m ^ n :=
 by simp only [cardinal.pow_cast_right, coe_pow]
 
 @[simp, norm_cast] theorem nat_cast_le {m n : ℕ} : (m : cardinal) ≤ n ↔ m ≤ n :=
-begin
-  rw [←lift_mk_fin, ←lift_mk_fin, lift_le],
-  exact ⟨λ ⟨⟨f, hf⟩⟩, by simpa only [fintype.card_fin] using fintype.card_le_of_injective f hf,
-    λ h, ⟨(fin.cast_le h).to_embedding⟩⟩
-end
+by rw [← lift_mk_fin, ← lift_mk_fin, lift_le, le_def, function.embedding.nonempty_iff_card_le,
+  fintype.card_fin, fintype.card_fin]
 
 @[simp, norm_cast] theorem nat_cast_lt {m n : ℕ} : (m : cardinal) < n ↔ m < n :=
 by simp [lt_iff_le_not_le, ←not_le]

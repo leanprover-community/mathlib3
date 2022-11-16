de330b052f92165290b275c2bf4a07542f8589f9
Moritz Doll <moritz.doll@googlemail.com>
2022-11-16 11:22:04 +1100

    more removal

--- src/topology/metric_space/basic.lean
+++ src/topology/metric_space/basic.lean
@@ -4,7 +4,6 @@ Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Jeremy Avigad, Robert Y. Lewis, Johannes Hölzl, Mario Carneiro, Sébastien Gouëzel
 -/
 
-import data.int.interval
 import tactic.positivity
 import topology.algebra.order.compact
 import topology.metric_space.emetric_space

9819a8f5050d400bf15abb182e69e16d01866ab8
Scott Morrison <scott.morrison@gmail.com>
2022-11-17 02:00:42 +1100

    cleanup

--- scripts/all_unused_imports.sh
+++
@@ -1,9 +0,0 @@
-# From the root directory of mathlib,
-# `scripts/all_unused_imports.sh` will attempt to find all unused imports in all files.
-# It is very slow, but could be made much faster if needed.
-scripts/mk_all.sh
-for file in $(cat src/all.lean | sed -e 's|import ||'); do
-  echo "# Unused imports in $file"
-  scripts/unused_imports.sh $file
-  echo
-done

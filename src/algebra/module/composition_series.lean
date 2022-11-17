a92093742dd1c32989fe2ade25b6d160c6595d9b
Daan van Gent <daanvangent@gmail.com>
2022-11-17 11:20:02 +0100

    Shortened proof

--- src/algebra/module/composition_series.lean
+++ src/algebra/module/composition_series.lean
@@ -4,54 +4,30 @@ import ring_theory.simple_module
 
 namespace composition_series
 
--- TODO
-instance temp {α : Type*} [lattice α] [is_modular_lattice α] : is_weak_lower_modular_lattice α := sorry
+open set
+
+lemma covby_iff_coatom_Iic {α : Type*} [partial_order α] {x y : α} (hxy : x ≤ y):
+  x ⋖ y ↔ is_coatom (⟨x, hxy⟩ : Iic y) :=
+begin
+  let f : Iic y ↪o α := order_embedding.subtype (λ z, z ≤ y),
+  rw ←covby_top_iff,
+  refine ⟨λ h, covby.of_image f h, λ h, covby.image f h _⟩,
+  change (range coe).ord_connected,
+  rw subtype.range_coe,
+  exact ord_connected_Iic,
+end
 
 open submodule
-open lattice
 variables {R M : Type*} [ring R] [add_comm_group M] [module R M]
-
-@[simp]
-lemma comap_map_subtype (B : submodule R M) (A : submodule R B) :
-  comap B.subtype (map B.subtype A) = A :=
-comap_map_eq_of_injective subtype.coe_injective A
 
 lemma covby_iff_quot_is_simple {A B : submodule R M} (hAB : A ≤ B) :
   A ⋖ B ↔ is_simple_module R (B ⧸ comap B.subtype A) :=
 begin
-  rw is_simple_module_iff_is_coatom,
-  split; intro h,
-  { split,
-    { intro htop,
-      rw comap_subtype_eq_top at htop,
-      rw le_antisymm hAB htop at h,
-      exact lt_irrefl B h.lt },
-    { intros C' hAC',
-      have hA : A = map B.subtype (comap B.subtype A),
-        rwa [map_comap_subtype,right_eq_inf],
-      have hAC : A ≤ map B.subtype C',
-      { rw hA,
-        exact map_mono (le_of_lt hAC') },
-      cases h.wcovby.eq_or_eq hAC (map_subtype_le B C') with h h,
-      { exfalso,
-        apply ne_of_lt hAC',
-        rw congr_arg (comap B.subtype) h.symm,
-        exact comap_map_subtype B C' },
-      { rw [←comap_map_subtype B C', h, comap_subtype_self] } } },
-  { rw covby_iff_lt_and_eq_or_eq,
-    split,
-    { apply lt_of_le_of_ne hAB,
-      intro hAB',
-      apply h.1,
-      rw hAB',
-      exact comap_subtype_self B },
-    { intros C hAC hCB,
-      by_cases u : comap B.subtype A < comap B.subtype C,
-      { right,
-        exact le_antisymm hCB (comap_subtype_eq_top.mp $ h.2 _ u) },
-      { left,
-        have eqAC := eq_of_le_of_not_lt (comap_mono hAC) u,
-        rw [right_eq_inf.mpr hAB, right_eq_inf.mpr hCB, ←map_comap_subtype, ←eqAC, map_comap_subtype] } } }
+  let f : submodule R B ≃o Iic B := map_subtype.rel_iso B,
+  rw [covby_iff_coatom_Iic hAB, is_simple_module_iff_is_coatom, ←order_iso.is_coatom_iff f _],
+  refine iff_of_eq (congr_arg _ _),
+  change (⟨A, _⟩ : Iic B) = ⟨map B.subtype (comap B.subtype A), _⟩,
+  rw [subtype.mk_eq_mk, map_comap_subtype, inf_eq_right.mpr hAB],
 end
 
 instance jordan_holder_module : jordan_holder_lattice (submodule R M) :=

d9877ec521c39551a79f94b133426518ff652c44
Michael Stoll <Michael.Stoll@uni-bayreuth.de>
2022-11-17 14:18:31 +0100

    adapt name to change

--- src/data/list/big_operators.lean
+++ src/data/list/big_operators.lean
@@ -471,7 +471,7 @@ begin
 end
 
 /-- If a product of integers is `-1`, then at least one factor must be `-1`. -/
-lemma exists_mem_eq_neg_one_of_prod_eq_neg_one {l : list ℤ} (h : l.prod = -1) : (-1 : ℤ) ∈ l :=
+lemma neg_one_mem_of_prod_eq_neg_one {l : list ℤ} (h : l.prod = -1) : (-1 : ℤ) ∈ l :=
 begin
   obtain ⟨x, h₁, h₂⟩ := exists_mem_ne_one_of_prod_ne_one (ne_of_eq_of_ne h dec_trivial),
   exact or.resolve_left (int.is_unit_iff.mp (prod_is_unit_iff.mp

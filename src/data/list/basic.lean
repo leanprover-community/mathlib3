2b97be1862eb88d833d8932a5c6ee2ce62a60c06
Eric Wieser <wieser.eric@gmail.com>
2022-11-17 11:50:37 +0000

    add the foldl version

--- src/data/list/basic.lean
+++ src/data/list/basic.lean
@@ -2271,9 +2271,25 @@ begin
     exact ⟨c :: m, rfl⟩ }
 end
 
+lemma foldl_range_subset_of_range_subset {f : γ → α → γ} {g : γ → β → γ}
+  (hfg : set.range (λ a c, f c a) ⊆ set.range (λ b c, g c b)) (a) :
+    set.range (foldl f a) ⊆ set.range (foldl g a) :=
+begin
+  change set.range (λ l, _) ⊆ set.range (λ l, _),
+  simp_rw ←foldr_reverse at hfg ⊢,
+  simp_rw [set.range_comp _ list.reverse, reverse_involutive.bijective.surjective.range_eq,
+    set.image_univ],
+  exact foldr_range_subset_of_range_subset hfg a,
+end
+
 lemma foldr_range_eq_of_range_eq {f : α → γ → γ} {g : β → γ → γ} (hfg : set.range f = set.range g)
   (a) : set.range (foldr f a) = set.range (foldr g a) :=
 (foldr_range_subset_of_range_subset hfg.le a).antisymm (foldr_range_subset_of_range_subset hfg.ge a)
+
+lemma foldl_range_eq_of_range_eq {f : γ → α → γ} {g : γ → β → γ}
+  (hfg : set.range (λ a c, f c a) = set.range (λ b c, g c b))
+  (a) : set.range (foldl f a) = set.range (foldl g a) :=
+(foldl_range_subset_of_range_subset hfg.le a).antisymm (foldl_range_subset_of_range_subset hfg.ge a)
 
 /-- Induction principle for values produced by a `foldr`: if a property holds
 for the seed element `b : β` and for all incremental `op : α → β → β`

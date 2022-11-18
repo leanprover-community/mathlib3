235537268b0fbbd220b17063de3884f40b53c4bd
Kalle-t30200-lr228 <kalle.kytola@aalto.fi>
2022-11-18 11:05:47 +0200

    Try to add two small lemmas in hopefully a suitable place (and get cache).

--- src/topology/algebra/order/liminf_limsup.lean
+++ src/topology/algebra/order/liminf_limsup.lean
@@ -320,6 +320,29 @@ f_incr.map_Liminf_of_continuous_at f_cont
 
 end monotone
 
+section infi_and_supr
+
+open_locale topological_space
+
+open filter
+
+variables {ι : Type*} {R : Type*} [complete_linear_order R] [topological_space R] [order_topology R]
+
+lemma infi_eq_of_forall_le_of_tendsto {x : R} {as : ι → R}
+  (x_le : ∀ i, x ≤ as i) {F : filter ι} [filter.ne_bot F] (as_lim : filter.tendsto as F (𝓝 x)) :
+  (⨅ i, as i) = x :=
+begin
+  refine infi_eq_of_forall_ge_of_forall_gt_exists_lt (λ i, x_le i) _,
+  apply λ w x_lt_w, ne_bot.nonempty_of_mem ‹filter.ne_bot F› (eventually_lt_of_tendsto_lt x_lt_w as_lim),
+end
+
+lemma supr_eq_of_forall_le_of_tendsto {x : R} {as : ι → R}
+  (le_x : ∀ i, as i ≤ x) {F : filter ι} [filter.ne_bot F] (as_lim : filter.tendsto as F (𝓝 x)) :
+  (⨆ i, as i) = x :=
+@infi_eq_of_forall_le_of_tendsto ι (order_dual R) _ _ _ x as le_x F _ as_lim
+
+end infi_and_supr
+
 section indicator
 
 open_locale big_operators

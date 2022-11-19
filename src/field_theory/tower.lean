7b981754d3d987f4f3fb4cb5b4dcb40b8275ab76
Junyan Xu <junyanxumath@gmail.com>
2022-11-19 01:39:33 -0500

    field_theory/tower import nat.prime

--- src/field_theory/tower.lean
+++ src/field_theory/tower.lean
@@ -4,6 +4,7 @@ Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Kenny Lau
 -/
 
+import data.nat.prime
 import ring_theory.algebra_tower
 import linear_algebra.matrix.finite_dimensional
 import linear_algebra.matrix.to_lin

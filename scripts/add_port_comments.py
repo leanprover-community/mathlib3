f67b2a7768c4ba16b0032ad7f8cef7e873202226
Eric Wieser <wieser.eric@gmail.com>
2022-11-19 08:56:48 +0000

    Use the new mathlibtools

--- scripts/add_port_comments.py
+++ scripts/add_port_comments.py
@@ -1,4 +1,4 @@
-from mathlibtools.lib import LeanProject
+from mathlibtools.file_status import PortStatus
 from pathlib import Path
 
 import re
@@ -9,71 +9,7 @@ import textwrap
 import requests
 import yaml
 
-# copied from https://github.com/leanprover-community/mathlib-tools/pull/137/files#
-
-@dataclass()
-class FileStatus:
-
-    ported: bool = False
-    mathlib4_pr: Optional[int] = None
-    mathlib3_hash: Optional[str] = None
-    comments: Optional[str] = None
-
-    @classmethod
-    def parse_old(cls, message: str) -> "FileStatus":
-        ported = False
-        mathlib4_pr: Optional[int] = None
-        mathlib3_hash: Optional[str] = None
-        if message.startswith("Yes"):
-            ported = True
-            if len(message.split()) > 2:
-                mathlib3_hash = message.split()[2]
-        if "mathlib4#" in message:
-            mathlib4_pr = int(re.findall(r"[0-9]+", message.replace("mathlib4#", ""))[0])
-        return cls(
-            ported=ported,
-            mathlib4_pr=mathlib4_pr,
-            mathlib3_hash=mathlib3_hash,
-        )
-
-    def asdict(self) -> Dict[str, Any]:
-        return {k: v for k, v in asdict(self).items() if v is not None}
-
-    def color(self) -> Optional[str]:
-        if self.ported:
-            return "green"
-        if self.mathlib4_pr:
-            return "lightskyblue"
-        if self.comments and "ready" in self.comments:
-            return "turquoise1"
-        return None
-
-
-@dataclass
-class PortStatus:
-
-    file_statuses: Dict[str, FileStatus]
-
-    @classmethod
-    def old_yaml(cls, url: Optional[str] = None) -> Dict[str, str]:
-        if url is None:
-            url = "https://raw.githubusercontent.com/wiki/leanprover-community/mathlib/mathlib4-port-status.md"
-        def yaml_md_load(wikicontent: bytes):
-            return yaml.safe_load(wikicontent.replace(b"```", b""))
-
-        return yaml_md_load(requests.get(url).content)
-
-    @classmethod
-    def deserialize_old(cls, yaml: Dict[str, str]) -> "PortStatus":
-        return cls(file_statuses={k: FileStatus.parse_old(v) for k, v in yaml.items()})
-
-    def serialize(self) -> Dict[str, Dict[str, Union[int, str, None]]]:
-        return yaml.dump({k: v.asdict() for k, v in self.file_statuses.items()})
-
-
-
-y = PortStatus.old_yaml()
-status = PortStatus.deserialize_old(y)
+status = PortStatus.deserialize_old()
 
 src_path = Path(__file__).parent.parent / 'src'
 
@@ -117,7 +53,6 @@ def add_port_status(fcontent, fstatus):
             fcontent = replace_range(fcontent, existing_label.start(), existing_label.end(), "")
         elif existing_header.end() <= existing_label.start():
             rest = fcontent[existing_label.end():module_comment_end]
-            print("!!!", repr(rest))
             trailing_whitespace = "\n" if rest.strip() else ""
             fcontent = replace_range(fcontent, existing_label.start(), existing_label.end(), "")
             fcontent = replace_range(fcontent, existing_header.end(), existing_header.end(),
@@ -133,7 +68,6 @@ def fname_for(import_path):
 for iname, f_status in status.file_statuses.items():
     if f_status.ported:
         fname = fname_for(iname)
-        print(iname, f_status)
         with open(fname) as f:
             fcontent = f.read()
         fcontent = add_port_status(fcontent, f_status)

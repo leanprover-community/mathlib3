b72002ac03e94453697634adb2d2215550c3be63
Riccardo Brasca <riccardo.brasca@gmail.com>
2022-11-23 08:05:09 +0100

    Update src/logic/basic.lean
    Co-authored-by: Scott Morrison <scott@tqft.net>

--- src/logic/basic.lean
+++ src/logic/basic.lean
@@ -490,7 +490,7 @@ iff.intro and.left (λ ha, ⟨ha, h ha⟩)
 theorem and_iff_right_of_imp {a b : Prop} (h : b → a) : (a ∧ b) ↔ b :=
 iff.intro and.right (λ hb, ⟨h hb, hb⟩)
 
-lemma ne_and_eq_iff_right {α : Type*} {a b c : α} (h : b ≠ c) : a ≠ b ∧ a = c ↔ a = c :=
+lemma ne_and_eq_iff_right {α : Sort*} {a b c : α} (h : b ≠ c) : a ≠ b ∧ a = c ↔ a = c :=
 and_iff_right_of_imp (λ h2, h2.symm ▸ h.symm)
 
 @[simp] theorem and_iff_left_iff_imp {a b : Prop} : ((a ∧ b) ↔ a) ↔ (a → b) :=

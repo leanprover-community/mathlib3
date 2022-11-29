7dd22fe7f64e6b9395341497ec415e972fdfa366
Heather Macbeth <25316162+hrmacbeth@users.noreply.github.com>
2022-11-29 19:43:53 +0100

    data.sym.sym2

--- src/data/sym/sym2.lean
+++ src/data/sym/sym2.lean
@@ -3,6 +3,8 @@ Copyright (c) 2020 Kyle Miller All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Kyle Miller
 -/
+import data.finset.prod
+import data.fintype.basic
 import data.sym.basic
 import tactic.linarith
 

9dcbd6841695fdcc40969e17e8f5e0b5a8b771ae
Heather Macbeth <25316162+hrmacbeth@users.noreply.github.com>
2022-11-29 19:04:37 +0100

    tactic.norm_fin

--- src/algebra/order/floor.lean
+++ src/algebra/order/floor.lean
@@ -4,9 +4,7 @@ Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Mario Carneiro, Kevin Kappelmann
 -/
 import data.int.lemmas
-import data.set.lattice
 import data.set.intervals.group
-import order.galois_connection
 import tactic.abel
 import tactic.linarith
 import tactic.positivity

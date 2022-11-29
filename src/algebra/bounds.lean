d42b9a1c94cc6e808ad818a686f5bc96be9ab2d7
Heather Macbeth <25316162+hrmacbeth@users.noreply.github.com>
2022-11-29 18:15:09 +0100

    possibly missing import

--- src/algebra/bounds.lean
+++ src/algebra/bounds.lean
@@ -4,6 +4,7 @@ Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Yury G. Kudryashov
 -/
 import data.set.pointwise.basic
+import order.bounds.order_iso
 import order.conditionally_complete_lattice.basic
 
 /-!

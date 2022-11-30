c8e53d550414cb3f8eba43ec8caf974069c3bcd3
Heather Macbeth <25316162+hrmacbeth@users.noreply.github.com>
2022-11-30 17:06:20 +0100

    fix test

--- test/semilinear.lean
+++ test/semilinear.lean
@@ -4,7 +4,7 @@ Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Frédéric Dupuis, Heather Macbeth
 -/
 import algebra.module.equiv
-import data.real.basic
+import data.real.star
 
 /-!
 # Test file for conjugate linear maps

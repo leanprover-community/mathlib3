e9de1dc6eebd108390f80906730a3924811c8d0f
Yaël Dillies <yael.dillies@gmail.com>
2022-12-01 00:17:53 +0000

    restore import

--- src/data/list/big_operators/basic.lean
+++ src/data/list/big_operators/basic.lean
@@ -3,6 +3,7 @@ Copyright (c) 2017 Johannes Hölzl. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Johannes Hölzl, Floris van Doorn, Sébastien Gouëzel, Alex J. Best
 -/
+import data.list.count
 import data.list.forall2
 
 /-!

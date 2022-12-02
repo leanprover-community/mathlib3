afad8e438d03f9d89da2914aa06cb4964ba87a18
Eric Wieser <wieser.eric@gmail.com>
2022-12-02 08:36:34 +0000

    refactor(algebra/group_ring_action): split out subobject actions (#17786)
    This moves:
    * Instances for `submonoid` and `subgroup` to a new `algebra.group_ring_action.subobjects` module
    * Instances for `subsemiring` and `subring` inline with the other smul instances
    * `invariant_subring` to its own file
    
    The argument goes that `mul_semiring_action` is just `mul_distrib_mul_action` + `ring`, so should be available as early as possible in the import hierarchy where both are already available. Cutting out subobject dependencies makes this easier.

--- src/ring_theory/subsemiring/pointwise.lean
+++ src/ring_theory/subsemiring/pointwise.lean
@@ -3,7 +3,7 @@ Copyright (c) 2021 Eric Wieser. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Eric Wieser
 -/
-import algebra.group_ring_action
+import algebra.group_ring_action.basic
 import ring_theory.subsemiring.basic
 import group_theory.submonoid.pointwise
 import data.set.pointwise.basic

c06ba14da749b6f53ad02307dc93800cf699ad3e
Heather Macbeth <25316162+hrmacbeth@users.noreply.github.com>
2022-12-02 15:03:04 +0100

    data.matrix.basic

--- src/linear_algebra/matrix/trace.lean
+++ src/linear_algebra/matrix/trace.lean
@@ -3,7 +3,6 @@ Copyright (c) 2019 Johannes Hölzl. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Johannes Hölzl, Patrick Massot, Casper Putz, Anne Baanen
 -/
-import algebra.star.big_operators
 import data.matrix.basic
 
 /-!

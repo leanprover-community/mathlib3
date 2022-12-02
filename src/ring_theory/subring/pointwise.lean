d631abbe66296143944b9d05856323738751023a
Eric Wieser <wieser.eric@gmail.com>
2022-12-02 00:54:27 +0000

    refactor(algebra/group_ring_action): split out subobject actions

--- src/ring_theory/subring/pointwise.lean
+++ src/ring_theory/subring/pointwise.lean
@@ -4,7 +4,6 @@ Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Eric Wieser
 -/
 import ring_theory.subring.basic
-import algebra.group_ring_action
 import group_theory.subgroup.pointwise
 import ring_theory.subsemiring.pointwise
 import data.set.pointwise.basic

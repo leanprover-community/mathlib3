fb39e2f9735e964a2438b1ec9b7a3a404df4f3b2
Yaël Dillies <yael.dillies@gmail.com>
2022-12-04 11:07:11 +0000

    chore(algebra/order/group): Restore basic instances

--- src/algebra/order/group/instances.lean
+++ src/algebra/order/group/instances.lean
@@ -13,21 +13,9 @@ import algebra.order.monoid.order_dual
 
 variables {α : Type*}
 
-@[priority 100, to_additive]    -- see Note [lower instance priority]
-instance ordered_comm_group.to_ordered_cancel_comm_monoid [s : ordered_comm_group α] :
-  ordered_cancel_comm_monoid α :=
-{ le_of_mul_le_mul_left := λ a b c, (mul_le_mul_iff_left a).mp,
-  ..s }
-
 @[to_additive] instance [ordered_comm_group α] : ordered_comm_group αᵒᵈ :=
 { .. order_dual.ordered_comm_monoid, .. order_dual.group }
 
 @[to_additive] instance [linear_ordered_comm_group α] :
   linear_ordered_comm_group αᵒᵈ :=
 { .. order_dual.ordered_comm_group, .. order_dual.linear_order α }
-
-@[priority 100, to_additive] -- see Note [lower instance priority]
-instance linear_ordered_comm_group.to_linear_ordered_cancel_comm_monoid
-  [linear_ordered_comm_group α] : linear_ordered_cancel_comm_monoid α :=
-{ le_of_mul_le_mul_left := λ x y z, le_of_mul_le_mul_left',
-  ..‹linear_ordered_comm_group α› }

861ad74bd8cdf4e1d4d235375d148909f5224247
Ruben Van de Velde <65514131+Ruben-VandeVelde@users.noreply.github.com>
2022-12-06 15:39:33 +0100

    fix?

--- src/data/prod/lex.lean
+++ src/data/prod/lex.lean
@@ -4,6 +4,7 @@ Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Scott Morrison, Minchao Wu
 -/
 import order.bounded_order
+import order.monotone
 
 /-!
 # Lexicographic order

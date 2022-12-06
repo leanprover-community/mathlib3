5f162794228f79d753bdbb3f61254c33b2122d80
Eric Wieser <wieser.eric@gmail.com>
2022-12-06 22:38:36 +0000

    fix

--- src/data/set/prod.lean
+++ src/data/set/prod.lean
@@ -4,6 +4,7 @@ Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Mario Carneiro, Johannes Hölzl, Patrick Massot
 -/
 import data.set.basic
+import data.set.n_ary
 
 /-!
 # Sets in product and pi types

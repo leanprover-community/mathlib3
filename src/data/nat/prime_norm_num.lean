fd659c8155bd4068ec723784b7b02911aaa43211
Ruben Van de Velde <65514131+Ruben-VandeVelde@users.noreply.github.com>
2022-12-07 11:40:44 +0100

    Move out factors.

--- src/data/nat/prime_norm_num.lean
+++ src/data/nat/prime_norm_num.lean
@@ -3,6 +3,7 @@ Copyright (c) 2015 Microsoft Corporation. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Leonardo de Moura, Jeremy Avigad, Mario Carneiro
 -/
+import data.nat.factors
 import data.nat.prime
 import tactic.norm_num
 

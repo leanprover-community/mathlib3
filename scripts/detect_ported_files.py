1b02b847a509f3de1e4c9ab65e0b5ccee6fdd23e
Eric Wieser <wieser.eric@gmail.com>
2022-12-08 22:30:24 +0000

    done

--- scripts/detect_ported_files.py
+++ scripts/detect_ported_files.py
@@ -1,28 +1,28 @@
+# this script is only intended to be run by CI
 import sys
-from mathlibtools.file_status import PortStatus, FileStatus
 from pathlib import Path
 
-import re
-from dataclasses import asdict, dataclass
-from typing import Any, Dict, Optional, Union
-import textwrap
-
-import requests
-import yaml
+from mathlibtools.file_status import PortStatus, FileStatus
 
 status = PortStatus.deserialize_old()
 
 src_path = Path(__file__).parent.parent / 'src'
+
+def encode_msg_text_for_github(msg):
+    # even though this is probably url quoting, we match the implementation at
+    # https://github.com/actions/toolkit/blob/af821474235d3c5e1f49cee7c6cf636abb0874c4/packages/core/src/command.ts#L36-L94
+    return msg.replace('%', '%25').replace('\r', '%0D').replace('\n', '%0A')
 
 def fname_for(import_path: str) -> Path:
     return src_path / Path(*import_path.split('.')).with_suffix('.lean')
 
 if __name__ == '__main__':
     files = sys.argv[1:]
-    print(files)
-    # Just print it on every file!
     for iname, f_status in status.file_statuses.items():
         if f_status.ported:
             fname = fname_for(iname)
-            print(fname, fname in files)
-            print(f"::warning file={fname},line=1,col=1::This file was ported in https://github.com/leanprover-community/mathlib4/pull/{f_status.mathlib4_pr}")
+            if fname in files:
+                msg = ("Changes to this file will need to be ported to mathlib 4!\n"
+                    "Please consider retracting the changes to this file unless you are willing "
+                    "immediately forward port them." )
+                print(f"::warning file={fname},line=1,col=1::{encode_msg_text_for_github(msg)}")

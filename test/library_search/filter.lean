a11815fec779c947e9f3e52358193249af9b19c5
Riccardo Brasca <riccardo.brasca@gmail.com>
2022-12-08 09:37:35 +0100

    remove test

--- test/library_search/filter.lean
+++ test/library_search/filter.lean
@@ -5,13 +5,10 @@ open filter
 /- Turn off trace messages so they don't pollute the test build: -/
 set_option trace.silence_library_search true
 
---The following timeouts without `set_option timeout 200000`.
-
-set_option timeout 200000
-
-example {α β γ : Type*} {A : filter α} {B : filter β} {C : filter γ} {f : α → β} {g : β → γ}
-  (hf : tendsto f A B) (hg : tendsto g B C) : map (g ∘ f) A = map g (map f A) :=
-by library_search
+-- The following fails with a deterministic timeout.
+-- example {α β γ : Type*} {A : filter α} {B : filter β} {C : filter γ} {f : α → β} {g : β → γ}
+--   (hf : tendsto f A B) (hg : tendsto g B C) : map (g ∘ f) A = map g (map f A) :=
+-- by library_search
 
 example {α β γ : Type*} {A : filter α} {B : filter β} {C : filter γ} {f : α → β} {g : β → γ}
   (hf : tendsto f A B) (hg : tendsto g B C) : map g (map f A) ≤ C :=

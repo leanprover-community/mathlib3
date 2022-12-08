30cb00db537a5e00f5a7e1d2ca8f8e4ee277e7c2
Eric Wieser <wieser.eric@gmail.com>
2022-12-08 22:21:21 +0000

    better?

--- scripts/detect_ported_files.py
+++ scripts/detect_ported_files.py
@@ -1,3 +1,4 @@
+import sys
 from mathlibtools.file_status import PortStatus, FileStatus
 from pathlib import Path
 
@@ -16,8 +17,12 @@ src_path = Path(__file__).parent.parent / 'src'
 def fname_for(import_path: str) -> Path:
     return src_path / Path(*import_path.split('.')).with_suffix('.lean')
 
-# Just print it on every file!
-for iname, f_status in status.file_statuses.items():
-    if f_status.ported:
-        fname = fname_for(iname)
-        print(f"::warning file={fname},line=1,col=1::This file was ported in https://github.com/leanprover-community/mathlib4/pull/{f_status.mathlib4_pr}")
+if __name__ == '__main__':
+    files = sys.argv[1:]
+    print(files)
+    # Just print it on every file!
+    for iname, f_status in status.file_statuses.items():
+        if f_status.ported:
+            fname = fname_for(iname)
+            print(fname, fname in files)
+            print(f"::warning file={fname},line=1,col=1::This file was ported in https://github.com/leanprover-community/mathlib4/pull/{f_status.mathlib4_pr}")

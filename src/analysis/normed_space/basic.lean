72c8e008201df83ef5fbfc4f2436d5c27df0a2d1
Eric Wieser <wieser.eric@gmail.com>
2022-12-08 13:54:44 +0000

    fix

--- src/analysis/normed_space/basic.lean
+++ src/analysis/normed_space/basic.lean
@@ -3,6 +3,7 @@ Copyright (c) 2018 Patrick Massot. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Patrick Massot, Johannes Hölzl
 -/
+import algebra.algebra.pi
 import algebra.algebra.restrict_scalars
 import analysis.normed.field.basic
 import data.real.sqrt

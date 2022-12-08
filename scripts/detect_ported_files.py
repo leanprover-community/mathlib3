dadc5d67427344b618185829d712946b6adc984d
Eric Wieser <wieser.eric@gmail.com>
2022-12-08 19:42:42 +0000

    feat(ci): emit warning annotations on ported files

---
+++ scripts/detect_ported_files.py
@@ -0,0 +1,20 @@
+from mathlibtools.file_status import PortStatus, FileStatus
+from pathlib import Path
+
+import re
+from dataclasses import asdict, dataclass
+from typing import Any, Dict, Optional, Union
+import textwrap
+
+import requests
+import yaml
+
+status = PortStatus.deserialize_old()
+
+src_path = Path(__file__).parent.parent / 'src'
+
+# Just print it on every file!
+for iname, f_status in status.file_statuses.items():
+    if f_status.ported:
+        fname = fname_for(iname)
+        print("::warning file={fname},line=1,col=1::This file has been ported!")

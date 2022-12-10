b88e403622b26163d77e740387f2b5c93c928b50
BillyMiao <bm221@ic.ac.uk>
2022-12-10 14:11:39 +0000

    Update src/analysis/normed/ring/seminorm.lean
    Co-authored-by: María Inés de Frutos-Fernández <88536493+mariainesdff@users.noreply.github.com>

--- src/analysis/normed/ring/seminorm.lean
+++ src/analysis/normed/ring/seminorm.lean
@@ -325,7 +325,8 @@ is_nonarchimedean f ↔ ∀ r s, f (r + s) ≤ max (f r) (f s) := iff.rfl
 
 namespace mul_ring_norm
 
-lemma is_nonarchimedean.map_nat_cast_le_one {R : Type*} [non_assoc_ring R] {f : mul_ring_norm R}
+lemma is_nonarchimedean.map_nat_cast_le_one {R F : Type*} [non_assoc_ring R]
+  [nonneg_hom_class F R ℝ] {f : F} (hf : is_nonarchimedean f) (n : ℕ) : f n ≤ 1 :=
   (hf : is_nonarchimedean f) (n : ℕ) : f n ≤ 1 :=
 begin
   induction n with c hc,

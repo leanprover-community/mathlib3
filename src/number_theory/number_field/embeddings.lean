39801978a8878bddc47b2c78660817c2ce60b5f2
Xavier-François Roblot <46200072+xroblot@users.noreply.github.com>
2022-12-10 22:50:15 +0100

    make phi implicit

--- src/number_theory/number_field/embeddings.lean
+++ src/number_theory/number_field/embeddings.lean
@@ -168,7 +168,7 @@ by { simp only [place, conjugate_coe_eq, absolute_value.coe_mk, mul_hom.coe_mk, 
 /-- A embedding into `ℂ` is real if it is fixed by complex conjugation. -/
 def is_real (φ : K →+* ℂ) : Prop := is_self_adjoint φ
 
-lemma is_real_iff (φ : K →+* ℂ) : is_real φ ↔ conjugate φ = φ :=
+lemma is_real_iff {φ : K →+* ℂ} : is_real φ ↔ conjugate φ = φ :=
 is_self_adjoint_iff
 
 /-- A real embedding as a ring homomorphism from `K` to `ℝ` . -/

3b07f8f90eeeccd1f4137b21d5485681e5948766
Yaël Dillies <yael.dillies@gmail.com>
2022-12-14 19:21:28 +0000

    move to fintype namespace

--- src/combinatorics/double_counting.lean
+++ src/combinatorics/double_counting.lean
@@ -29,11 +29,13 @@ and `t`.
 open finset function relator
 open_locale big_operators
 
+variables {α β : Type*}
+
 /-! ### Bipartite graph -/
 
 namespace finset
 section bipartite
-variables {α β : Type*} (r : α → β → Prop) (s : finset α) (t : finset β) (a a' : α) (b b' : β)
+variables (r : α → β → Prop) (s : finset α) (t : finset β) (a a' : α) (b b' : β)
   [decidable_pred (r a)] [Π a, decidable (r a b)] {m n : ℕ}
 
 /-- Elements of `s` which are "below" `b` according to relation `r`. -/
@@ -56,15 +58,13 @@ variables {s t a a' b b'}
 @[simp] lemma mem_bipartite_below {a : α} : a ∈ s.bipartite_below r b ↔ a ∈ s ∧ r a b := mem_filter
 @[simp] lemma mem_bipartite_above {b : β} : b ∈ t.bipartite_above r a ↔ b ∈ t ∧ r a b := mem_filter
 
-variables [Π a b, decidable (r a b)]
-
-lemma sum_card_bipartite_above_eq_sum_card_bipartite_below :
+lemma sum_card_bipartite_above_eq_sum_card_bipartite_below [Π a b, decidable (r a b)] :
   ∑ a in s, (t.bipartite_above r a).card = ∑ b in t, (s.bipartite_below r b).card :=
 by { simp_rw [card_eq_sum_ones, bipartite_above, bipartite_below, sum_filter], exact sum_comm }
 
 /-- Double counting argument. Considering `r` as a bipartite graph, the LHS is a lower bound on the
 number of edges while the RHS is an upper bound. -/
-lemma card_mul_le_card_mul
+lemma card_mul_le_card_mul [Π a b, decidable (r a b)]
   (hm : ∀ a ∈ s, m ≤ (t.bipartite_above r a).card)
   (hn : ∀ b ∈ t, (s.bipartite_below r b).card ≤ n) :
   s.card * m ≤ t.card * n :=
@@ -74,13 +74,13 @@ calc
       : sum_card_bipartite_above_eq_sum_card_bipartite_below _
   ... ≤ _ : t.sum_le_card_nsmul _ _ hn
 
-lemma card_mul_le_card_mul'
+lemma card_mul_le_card_mul' [Π a b, decidable (r a b)]
   (hn : ∀ b ∈ t, n ≤ (s.bipartite_below r b).card)
   (hm : ∀ a ∈ s, (t.bipartite_above r a).card ≤ m) :
   t.card * n ≤ s.card * m :=
 card_mul_le_card_mul (swap r) hn hm
 
-lemma card_mul_eq_card_mul
+lemma card_mul_eq_card_mul [Π a b, decidable (r a b)]
   (hm : ∀ a ∈ s, (t.bipartite_above r a).card = m)
   (hn : ∀ b ∈ t, (s.bipartite_below r b).card = n) :
   s.card * m = t.card * n :=
@@ -90,7 +90,7 @@ lemma card_mul_eq_card_mul
 lemma card_le_card_of_forall_subsingleton
   (hs : ∀ a ∈ s, ∃ b, b ∈ t ∧ r a b) (ht : ∀ b ∈ t, ({a ∈ s | r a b} : set α).subsingleton) :
   s.card ≤ t.card :=
-by simpa using card_mul_le_card_mul _ (λ a h, card_pos.2 $
+by classical; simpa using card_mul_le_card_mul _ (λ a h, card_pos.2 $
   (by { rw [←coe_nonempty, coe_bipartite_above], exact hs _ h } : (t.bipartite_above r a).nonempty))
   (λ b h, card_le_one.2 $ by { simp_rw mem_bipartite_below, exact ht _ h })
 
@@ -99,7 +99,13 @@ lemma card_le_card_of_forall_subsingleton'
   t.card ≤ s.card :=
 card_le_card_of_forall_subsingleton (swap r) ht hs
 
-variables [fintype α] [fintype β]
+end bipartite
+end finset
+
+open finset
+
+namespace fintype
+variables [fintype α] [fintype β] {r : α → β → Prop}
 
 lemma card_le_card_of_left_total_unique (h₁ : left_total r) (h₂ : left_unique r) :
   fintype.card α ≤ fintype.card β :=
@@ -109,5 +115,5 @@ lemma card_le_card_of_right_total_unique (h₁ : right_total r) (h₂ : right_un
   fintype.card β ≤ fintype.card α :=
 card_le_card_of_forall_subsingleton' r (by simpa using h₁) $ λ b _ a₁ ha₁ a₂ ha₂, h₂ ha₁.2 ha₂.2
 
-end bipartite
-end finset
+end fintype
+

dfa8c9b046185d07a7f27a73a04c01bbbc4f0819
Yaël Dillies <yael.dillies@gmail.com>
2022-12-15 10:06:37 +0000

    use commutative

--- src/order/basic.lean
+++ src/order/basic.lean
@@ -383,14 +383,14 @@ variables [partial_order α]
 
 /-- To prove commutativity of a binary operation `○`, we only to check `a ○ b ≤ b ○ a` for all `a`,
 `b`. -/
-lemma comm_of_le {f : β → β → α} (comm : ∀ a b, f a b ≤ f b a) : ∀ a b, f a b = f b a :=
+lemma commutative_of_le {f : β → β → α} (comm : ∀ a b, f a b ≤ f b a) : ∀ a b, f a b = f b a :=
 λ a b, (comm _ _).antisymm $ comm _ _
 
 /-- To prove associativity of a commutative binary operation `○`, we only to check
 `(a ○ b) ○ c ≤ a ○ (b ○ c)` for all `a`, `b`, `c`. -/
-lemma assoc_of_comm_of_le {f : α → α → α} (comm : ∀ a b, f a b = f b a)
+lemma associative_of_commutative_of_le {f : α → α → α} (comm : commutative f)
   (assoc : ∀ a b c, f (f a b) c ≤ f a (f b c)) :
-  ∀ a b c, f (f a b) c = f a (f b c) :=
+  associative f :=
 λ a b c, le_antisymm (assoc _ _ _) $ by { rw [comm, comm b, comm _ c, comm a], exact assoc _ _ _ }
 
 end partial_order

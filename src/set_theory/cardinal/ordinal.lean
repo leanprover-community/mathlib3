fc09f947976e73477a3dc7e303067ba751537f94
Pedro Sánchez Terraf <pedrost@gmail.com>
2022-12-17 16:19:02 -0300

    feat(set_theory): define `ω₁` and prove new lemma on cardinality of `Union` I define `ω₁` as an ordinal (not as an `out`, cf. `measure_theory.card_measurable_space`). I prove a pair of lemmas using its cofinality. I add a lemma on the cardinality of ordinal-indexed `Union`s in preparation for material on the Borel hierarchy.

--- src/set_theory/cardinal/ordinal.lean
+++ src/set_theory/cardinal/ordinal.lean
@@ -1183,3 +1183,89 @@ by simp [←bit1_zero]
 end bit
 
 end cardinal
+
+section omega_1
+
+namespace ordinal
+
+open cardinal
+open_locale ordinal cardinal
+
+/--
+The first uncountable ordinal in type universe `u`.
+You can use the notation `ω₁`.
+-/
+noncomputable def omega_1 : ordinal.{u} := (aleph 1).ord
+
+localized "notation (name := ordinal.omega_1) `ω₁` := ordinal.omega_1" in ordinal
+
+@[simp]
+theorem card_omega_1 : card ω₁ = aleph 1 := card_ord _
+
+lemma omega_lt_omega_1 :
+  ω < ω₁ :=
+begin
+  have := (ord_lt_ord.mpr (aleph_0_lt_aleph_one)),
+  rw ord_aleph_0 at this,
+  exact this
+end
+
+lemma is_limit_omega_1 : omega_1.is_limit := ord_is_limit (aleph_0_le_aleph 1)
+
+end ordinal
+
+end omega_1
+
+section ordinal_indices
+/-!
+### Cardinal operations with ordinal indices
+
+Results on cardinality of ordinal-indexed families of sets.
+-/
+namespace cardinal
+open_locale cardinal
+
+/--
+Bounding the cardinal of an ordinal-indexed union of sets.
+-/
+lemma mk_Union_ordinal_le_of_le {β : Type* } {κ : cardinal} {i : ordinal}
+  (hi : i ≤ κ.ord) (hκ : ℵ₀ ≤ κ) (A : ordinal → set β)
+  (hA : ∀ j < i, #↥(A j) ≤ κ) :
+  #(↥⋃ j < i, A j) ≤ κ :=
+begin
+  have : #(↥⋃ j < i, A j) = #(↥⋃ (j : Iio i), A j),
+  { have : (⋃ j < i, A j) = (⋃ (j : Iio i), A j) := by simp,
+    rw this },
+  rw this, clear this,
+  rw Union_congr_of_surjective i.enum_iso_out.to_fun
+    i.enum_iso_out.to_equiv.surjective,
+  rotate,
+  { have : ∀ (x : ↥(Iio i)), (λ y, A (i.enum_iso_out.inv_fun y)) (i.enum_iso_out.to_equiv.to_fun x) = A ↑x,
+    { intro x, simp_rw (i.enum_iso_out.to_equiv.left_inv x) },
+    exact this },
+  calc
+  # (↥⋃ (y : i.out.α), A ↑(i.enum_iso_out.to_equiv.inv_fun y)) ≤
+    # i.out.α * ⨆ j : i.out.α, # ↥(A ↑(i.enum_iso_out.to_equiv.inv_fun j)) :
+    mk_Union_le _
+  ... = i.card * ⨆ j : i.out.α, # ↥(A ↑(i.enum_iso_out.to_equiv.inv_fun j)) :
+    by { rw mk_ordinal_out }
+  ... ≤ κ * ⨆ j : i.out.α, # ↥(A ↑(i.enum_iso_out.to_equiv.inv_fun j)) :
+    mul_le_mul (card_le_of_le_ord hi) (le_refl _) (zero_le _) (zero_le κ)
+  ... ≤ κ * κ : by
+    { apply mul_le_mul (le_refl κ) _ (zero_le _) (zero_le κ),
+      rw ← lift_id (supr _),
+      apply lift_supr_le,
+      { use κ,
+      rintro x ⟨w, rfl⟩,
+      refine hA _ _,
+      exact (i.enum_iso_out.to_equiv.inv_fun w).property },
+      intro j,
+      rw lift_id,
+      apply hA,
+      exact (i.enum_iso_out.to_equiv.inv_fun j).property }
+  ... = κ : mul_eq_self hκ
+end
+
+end cardinal
+
+end ordinal_indices

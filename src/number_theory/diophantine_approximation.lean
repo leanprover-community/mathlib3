d7ef9c2ddcfd86e319c41d5e92a0dad6b945524c
Michael Stoll <Michael.Stoll@uni-bayreuth.de>
2022-12-27 17:33:34 +0100

    import tactic.basic instead of tactic

--- src/number_theory/diophantine_approximation.lean
+++ src/number_theory/diophantine_approximation.lean
@@ -3,7 +3,7 @@ Copyright (c) 2022 Michael Stoll. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Michael Geißer, Michael Stoll
 -/
-import tactic
+import tactic.basic
 import data.real.irrational
 import combinatorics.pigeonhole
 

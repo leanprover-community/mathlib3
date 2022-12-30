9d8f0defee520b84c370a014b3b71742f4cd9afc
Christopher Hoskin <christopher.hoskin@gmail.com>
2022-12-30 16:13:49 +0000

    Move inf_Inf_hom

--- src/topology/order/lower_topology.lean
+++ src/topology/order/lower_topology.lean
@@ -20,28 +20,6 @@ partial order is T₀ and the complements of the upper closures of finite subset
 lower topology, preorder
 -/
 
-/-! ### To move -/
-
-section
-variables (α : Type*) [complete_lattice α]
-
-open set
-
-/-- The map `(a, b) ↦ a ⊓ b` as an `Inf_hom`. -/
-def inf_Inf_hom : Inf_hom (α × α) α :=
-{ to_fun := λ x, x.1 ⊓ x.2,
-  map_Inf' := λ s, begin
-    refine le_antisymm (le_Inf _) (Inf_le_iff.2 $ λ a ha, le_inf (le_Inf _) $ le_Inf _),
-    { rintro _ ⟨x, hx, rfl⟩,
-      exact inf_le_inf (monotone_fst $ Inf_le hx) (monotone_snd $ Inf_le hx) },
-    { rintro _ ⟨x, hx, rfl⟩,
-      exact (ha $ mem_image_of_mem _ hx).trans inf_le_left },
-    { rintro _ ⟨x, hx, rfl⟩,
-      exact (ha $ mem_image_of_mem _ hx).trans inf_le_right }
-  end }
-
-end
-
 variables (α β : Type*)
 
 open set topological_space

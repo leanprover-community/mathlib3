e4f61350a7b30fe5cfbc5ee8d9868db50ae4765b
Michael Stoll <Michael.Stoll@uni-bayreuth.de>
2022-12-30 19:30:54 +0100

    change statement and fix proof of ex_better_approx

--- src/number_theory/diophantine_approximation.lean
+++ src/number_theory/diophantine_approximation.lean
@@ -233,24 +233,26 @@ We show that an irrational real number `ξ` has infinitely many "good rational a
 i.e., fractions `x/y` in lowest terms such that `|ξ - x/y| < 1/y^2`.
 -/
 
-/-- Given any rational approximation `x/y` to the irrational real number `ξ`, there is
-a good rational approximation `X/Y` such that `|ξ - X/Y| < |ξ - x/y|`. -/
-lemma ex_better_approx {ξ : ℝ} (hξ : irrational ξ) (x y : ℤ) :
-  ∃ X Y : ℤ, (X, Y) ∈ rat_approx ξ ∧ |ξ - X / Y| < |ξ - x / y| :=
+/-- Given any rational approximation `q` to the irrational real number `ξ`, there is
+a good rational approximation `q'` such that `|ξ - q'| < |ξ - q|`. -/
+lemma ex_better_approx {ξ : ℝ} (hξ : irrational ξ) (q : ℚ) :
+  ∃ q' : ℚ, |ξ - q'| < 1 / q'.denom ^ 2 ∧ |ξ - q'| < |ξ - q| :=
 begin
-  have h := abs_pos.mpr (sub_ne_zero.mpr $ (irrational_iff_ne_rational ξ).mp hξ x y),
-  obtain ⟨m, hm⟩ := exists_nat_gt (1 / |ξ - x / y|),
+  have h : 0 < |ξ - q|,
+  { refine abs_pos.mpr (sub_ne_zero.mpr _),
+    simp only [irrational, set.mem_range, not_exists, ← ne.def] at hξ,
+    exact (hξ q).symm, },
+  obtain ⟨m, hm⟩ := exists_nat_gt (1 / |ξ - q|),
   have m_pos : 0 < (m : ℝ) := (one_div_pos.mpr h).trans hm,
-  obtain ⟨X, Y, hcp, hbd, Y_pos, Y_le_m⟩ := ex_approx ξ (nat.cast_pos.mp m_pos),
-  have Y_pos_ℝ : (0 : ℝ) < Y := int.cast_pos.mpr Y_pos,
-  refine ⟨X, Y, ⟨Y_pos, hcp,
-                 lt_of_lt_of_le hbd (one_div_le_one_div_of_le (sq_pos_of_pos Y_pos_ℝ) _)⟩,
-                 (lt_of_lt_of_le hbd _).trans ((one_div_lt h m_pos).mp hm)⟩,
-  { rw [pow_two, mul_le_mul_right Y_pos_ℝ],
-    exact_mod_cast Y_le_m, },
-  { rw [mul_comm, ← div_div],
-    refine div_le_div_of_le m_pos.le ((div_le_one Y_pos_ℝ).mpr _),
-    exact_mod_cast (int.cast_le.mpr Y_pos : (_ : ℝ) ≤ Y), }
+  obtain ⟨q', hbd, hden⟩ := ex_approx ξ (nat.cast_pos.mp m_pos),
+  have den_pos : (0 : ℝ) < q'.denom := nat.cast_pos.mpr q'.pos,
+  have md_pos := mul_pos m_pos den_pos,
+  refine ⟨q', lt_of_lt_of_le hbd _, hbd.trans _⟩,
+  { rw [sq, one_div_le_one_div md_pos (mul_pos den_pos den_pos), mul_le_mul_right den_pos],
+    exact_mod_cast hden, },
+  { rw [one_div_lt md_pos h],
+    refine lt_of_lt_of_le hm ((le_mul_iff_one_le_right m_pos).mpr _),
+    exact_mod_cast (q'.pos : 1 ≤ q'.denom), }
 end
 
 /-- If `ξ` is an irrational real number, then there are infinitely many good

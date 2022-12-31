51d7f4de7b9a42672eddfac1a12b07bc43ea791d
Michael Stoll <Michael.Stoll@uni-bayreuth.de>
2022-12-31 20:55:26 +0100

    fix proof of ex_better_approx

--- src/number_theory/diophantine_approximation.lean
+++ src/number_theory/diophantine_approximation.lean
@@ -257,14 +257,15 @@ begin
     exact (hξ q).symm, },
   obtain ⟨m, hm⟩ := exists_nat_gt (1 / |ξ - q|),
   have m_pos : 0 < (m : ℝ) := (one_div_pos.mpr h).trans hm,
-  obtain ⟨q', hbd, hden⟩ := ex_approx ξ (nat.cast_pos.mp m_pos),
+  obtain ⟨q', hbd, hden⟩ := dirichlet_approx' ξ (nat.cast_pos.mp m_pos),
   have den_pos : (0 : ℝ) < q'.denom := nat.cast_pos.mpr q'.pos,
-  have md_pos := mul_pos m_pos den_pos,
-  refine ⟨q', lt_of_lt_of_le hbd _, hbd.trans _⟩,
-  { rw [sq, one_div_le_one_div md_pos (mul_pos den_pos den_pos), mul_le_mul_right den_pos],
-    exact_mod_cast hden, },
+  have md_pos := mul_pos (add_pos m_pos zero_lt_one) den_pos,
+  refine ⟨q', lt_of_le_of_lt hbd _, lt_of_le_of_lt hbd _⟩,
+  { rw [sq, one_div_lt_one_div md_pos (mul_pos den_pos den_pos), mul_lt_mul_right den_pos],
+    exact lt_add_of_le_of_pos (nat.cast_le.mpr hden) zero_lt_one, },
   { rw [one_div_lt md_pos h],
-    refine lt_of_lt_of_le hm ((le_mul_iff_one_le_right m_pos).mpr _),
+    refine hm.trans (lt_of_lt_of_le (lt_add_one _) $
+                      (le_mul_iff_one_le_right $ add_pos m_pos zero_lt_one).mpr _),
     exact_mod_cast (q'.pos : 1 ≤ q'.denom), }
 end
 

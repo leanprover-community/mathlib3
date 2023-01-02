448144f7ae193a8990cb7473c9e9a01990f64ac7
Eric Wieser <wieser.eric@gmail.com>
2023-01-02 11:27:23 +0000

    chore(*): remove references to porting PRs (#18031)
    These no longer reflect the yaml file (which doesn't really track PR numbers any more). Since the next version of the bot will remove them, let's remove them manually first so that the bot diffs are easier to follow.

--- src/order/directed.lean
+++ src/order/directed.lean
@@ -11,7 +11,6 @@ import order.max
 # Directed indexed families and sets
 
 > THIS FILE IS SYNCHRONIZED WITH MATHLIB4.
-> https://github.com/leanprover-community/mathlib4/pull/963
 > Any changes to this file require a corresponding PR to mathlib4.
 
 This file defines directed indexed families and directed sets. An indexed family/set is

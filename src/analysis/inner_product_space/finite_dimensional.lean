3a4a1789170e9285b848d1dc06eb9c5561341f28
themathqueen <2497250a@research.gla.ac.uk>
2023-01-03 12:54:31 +0000

    fix

--- src/analysis/inner_product_space/finite_dimensional.lean
+++ src/analysis/inner_product_space/finite_dimensional.lean
@@ -22,19 +22,19 @@ variables {V : Type*} [inner_product_space ℂ V]
 local notation `P`U := (orthogonal_projection U)
 local notation `P`U`ᗮ` := (orthogonal_projection Uᗮ)
 
-/-- `U` is `T` invariant is equivalent to `T(U) ⊆ U` -/
-lemma U_is_T_invariant_def (U : submodule ℂ V) (T : V →L[ℂ] V) :
- (∀ u : V, u ∈ U → T u ∈ U) ↔ (T '' U ⊆ U)
-  := by { simp only [set.image_subset_iff],
-          exact ⟨ λ h, λ x hx, by simp only [set.mem_preimage]; exact h x hx,
-                  λ h u h_1, by apply h h_1 ⟩, }
+/-- `U` is `T` invariant: `∀ u : V`, if `u ∈ U` then `T u ∈ U`-/
+def invariant_subspace (U : submodule ℂ V) (T : V →L[ℂ] V) :
+ Prop := (∀ u : V, u ∈ U → T u ∈ U)
+lemma invariant_subspace_def (U : submodule ℂ V) (T : V →L[ℂ] V) :
+ invariant_subspace U T ↔ (T '' U ⊆ U)
+  := by simp only [set.image_subset_iff]; refl
 
 /-- `U` is `T` invariant if and only if `(P U) * T * (P U) = T * (P U)` -/
-lemma U_is_T_invariant_iff_P_U_T_P_U_eq_T_P_U
+lemma subspace_is_invariant_iff_ortho_proj_mul_T_mul_ortho_proj_eq_T_mul_ortho_proj
  [finite_dimensional ℂ V] (U : submodule ℂ V) (T : V →L[ℂ] V) :
- (T '' U ⊆ U) ↔ ∀ x : V, ↑((P U) (T ↑((P U) x))) = (T ↑((P U) x))
-  := by rw ← U_is_T_invariant_def U T;
-        exact ⟨ λ h x, by obtain ⟨w,hw,v,hv,hvw⟩ := submodule.exists_sum_mem_mem_orthogonal U x;
+ (invariant_subspace U T) ↔ ∀ x : V, ↑((P U) (T ↑((P U) x))) = (T ↑((P U) x))
+  := by exact ⟨ λ h x, by
+	                     obtain ⟨w,hw,v,hv,hvw⟩ := submodule.exists_sum_mem_mem_orthogonal U x;
                        rw [ hvw,
                             map_add,
                             orthogonal_projection_mem_subspace_orthogonal_complement_eq_zero hv,
@@ -46,11 +46,11 @@ lemma U_is_T_invariant_iff_P_U_T_P_U_eq_T_P_U
                                    h] ⟩
 
 /-- `U,Uᗮ` are `T` invariant if and only if `(P U) * T = T * (P U)` -/
-lemma U_and_U_bot_are_T_inv_iff_P_UT_eq_TP_U
+lemma U_and_U_bot_are_T_invariant_iff_ortho_proj_mul_T_eq_T_mul_ortho_proj
  [finite_dimensional ℂ V] (U : submodule ℂ V) (T : V →L[ℂ] V) :
- (T '' U ⊆ U ∧ T '' Uᗮ ⊆ Uᗮ) ↔ ∀ x : V, ↑((P U) (T x)) = (T ↑((P U) x)) :=
+ (invariant_subspace U T ∧ invariant_subspace Uᗮ T) ↔ ∀ x : V, ↑((P U) (T x)) = (T ↑((P U) x)) :=
    begin
-     simp only [U_is_T_invariant_iff_P_U_T_P_U_eq_T_P_U],
+     simp only [subspace_is_invariant_iff_ortho_proj_mul_T_mul_ortho_proj_eq_T_mul_ortho_proj],
      have : ∀ x : V,
       ↑((P Uᗮ) x) = ((continuous_linear_map.id ℂ V) x) - ↑((P U) x)
         := λ x, by rw [ eq_sub_iff_add_eq,
@@ -64,8 +64,7 @@ lemma U_and_U_bot_are_T_inv_iff_P_UT_eq_TP_U
                  sub_eq_self,
                  add_subgroup_class.coe_sub,
                  sub_eq_zero,
-                 ← U_is_T_invariant_iff_P_U_T_P_U_eq_T_P_U],
-     simp only [← U_is_T_invariant_def],
+                 ← subspace_is_invariant_iff_ortho_proj_mul_T_mul_ortho_proj_eq_T_mul_ortho_proj],
      exact ⟨λ ⟨h1,h2⟩ x, by
             simp only [h2 x,
              orthogonal_projection_eq_self_iff.mpr
@@ -77,63 +76,58 @@ lemma U_and_U_bot_are_T_inv_iff_P_UT_eq_TP_U
    end
 
 -- Given an invertible operator, multiplying it by its inverse gives the identity.
-lemma Tinv_mul_T_eq_1 (T : V →L[ℂ] V) [invertible T] : T.inverse * T = 1 :=
+lemma mul_inv_operator (T : V →L[ℂ] V) [invertible T] :
+ T.inverse * T = 1 ∧ T * T.inverse = 1 :=
    by simp only [ ← continuous_linear_map.ring_inverse_eq_map_inverse,
                   ring.inverse_mul_cancel,
-                  is_unit_of_invertible T ]
-lemma T_mul_Tinv_eq_1 (T : V →L[ℂ] V) [invertible T] : T * T.inverse = 1 :=
-   by simp only [ ← continuous_linear_map.ring_inverse_eq_map_inverse,
-                  ring.mul_inverse_cancel,
-                  is_unit_of_invertible ]
+									ring.mul_inverse_cancel,
+                  is_unit_of_invertible T,
+									and_self ]
 
 -- `(P U) * T = T * (P U)` if and only if `T⁻¹ * (P U) * T = P U`
-lemma P_U_T_eq_T_P_U_iff_Tinv_P_U_T_eq_P_U
+lemma ortho_proj_mul_T_eq_T_mul_ortho_proj_iff_Tinv_mul_ortho_proj_mul_T_eq_ortho_proj
  [finite_dimensional ℂ V] (U : submodule ℂ V) (T : V →L[ℂ] V) [invertible T] :
  ∀ x : V, ↑((P U) (T x)) = T ↑((P U) x) ↔ T.inverse ↑((P U) (T x)) = ↑((P U) x)
   := λ x, ⟨ λ h, by rw h;
                     simp only [ ← continuous_linear_map.comp_apply,
                                 ← continuous_linear_map.mul_def ];
-                    rw Tinv_mul_T_eq_1;
+                    rw (mul_inv_operator T).1;
                     refl,
             λ h, by rw ← h;
                     simp only [ ← continuous_linear_map.comp_apply,
                                 ← continuous_linear_map.mul_def,
-                                T_mul_Tinv_eq_1 ];
+                                (mul_inv_operator T).2 ];
                     refl ⟩
 
 -- `T⁻¹(U) ⊆ U` is equivalent to `U ⊆ T(U)`
-lemma Tinv_image_subseteq_iff_subseteq_T_image
+-- or `U` is `T⁻¹` invariant if and only if `U ⊆ T(U)`
+lemma U_is_Tinv_invariant_iff_U_subseteq_T_image
  (U : submodule ℂ V) (T : V →L[ℂ] V) [invertible T] :
-  ((T.inverse) '' U ⊆ U) ↔ (↑U ⊆ T '' U)
+  (invariant_subspace U (T.inverse)) ↔ (↑U ⊆ T '' U)
   := ⟨ λ h x hx, by simp only [set.mem_image, set_like.mem_coe];
                     use T.inverse x;
                     rw [ ← continuous_linear_map.comp_apply,
                          ← continuous_linear_map.mul_def,
-                         T_mul_Tinv_eq_1 T,
+                         (mul_inv_operator T).2,
                          continuous_linear_map.one_apply ];
                     simp only [eq_self_iff_true, and_true];
                     apply h;
-                    rw set.mem_image;
-                    use x;
-                    simp only [eq_self_iff_true, and_true];
-                    exact hx,
-       λ h x hx, by simp only [set.mem_image, set_like.mem_coe] at hx;
-                    cases hx with y hy;
+										exact hx,
+       λ h x hx, by simp only [set.subset_def, set.mem_image] at h;
+                    cases h x hx with y hy;
                     rw ← hy.2;
-                    cases set.mem_of_subset_of_mem h hy.1 with z hz;
-                    rw [ ← hz.2,
-                         ← continuous_linear_map.comp_apply,
+                    rw [ ← continuous_linear_map.comp_apply,
                          ← continuous_linear_map.mul_def,
-                         Tinv_mul_T_eq_1 ];
-                    exact hz.1 ⟩
+                         (mul_inv_operator T).1 ];
+                    exact hy.1 ⟩
 
 /-- `T⁻¹ * (P U) * T = P U` if and only if `T(U) = U` and `T(Uᗮ) = Uᗮ` -/
 theorem T_inv_P_U_T_eq_P_U_iff_T''U_eq_U_and_T''U_bot_eq_U_bot
  [finite_dimensional ℂ V] (U : submodule ℂ V) (T : V →L[ℂ] V) [invertible T] :
  (∀ x : V, T.inverse ↑((P U) (T x)) = ↑((P U) x)) ↔ T '' U = U ∧ T '' Uᗮ = Uᗮ :=
   begin
-    simp only [ ← P_U_T_eq_T_P_U_iff_Tinv_P_U_T_eq_P_U,
-                ← U_and_U_bot_are_T_inv_iff_P_UT_eq_TP_U U T ],
+    simp only [ ← ortho_proj_mul_T_eq_T_mul_ortho_proj_iff_Tinv_mul_ortho_proj_mul_T_eq_ortho_proj,
+                ← U_and_U_bot_are_T_invariant_iff_ortho_proj_mul_T_eq_T_mul_ortho_proj U T ],
     simp only [set.subset.antisymm_iff],
     have Hu : ∀ p q r s, ((p ∧ q) ∧ r ∧ s) = ((p ∧ r) ∧ (q ∧ s)) := λ _ _ _ _, by
         { simp only [ and.assoc,
@@ -143,18 +137,17 @@ theorem T_inv_P_U_T_eq_P_U_iff_T''U_eq_U_and_T''U_bot_eq_U_bot
           simp only [and.comm],
           simp only [iff_self, implies_true_iff], },
     rw [ Hu,
-         ← U_is_T_invariant_def U T,
-         ← U_is_T_invariant_def Uᗮ T,
+         ← invariant_subspace_def U T,
+         ← invariant_subspace_def Uᗮ T,
          iff_self_and ],
     clear Hu,
-    simp only [← Tinv_image_subseteq_iff_subseteq_T_image],
-    simp only [U_is_T_invariant_def],
-    simp only [ U_and_U_bot_are_T_inv_iff_P_UT_eq_TP_U,
-                P_U_T_eq_T_P_U_iff_Tinv_P_U_T_eq_P_U ],
+    simp only [← U_is_Tinv_invariant_iff_U_subseteq_T_image],
+    simp only [ U_and_U_bot_are_T_invariant_iff_ortho_proj_mul_T_eq_T_mul_ortho_proj,
+                ortho_proj_mul_T_eq_T_mul_ortho_proj_iff_Tinv_mul_ortho_proj_mul_T_eq_ortho_proj ],
     intros h x,
     rw [← h],
     simp only [← continuous_linear_map.comp_apply],
     rw [ ← continuous_linear_map.mul_def,
-         T_mul_Tinv_eq_1 ],
+         (mul_inv_operator T).2 ],
     refl,
   end

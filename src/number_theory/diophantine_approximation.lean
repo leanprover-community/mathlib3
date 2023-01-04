0d5ab7ba7e163c4b4abbe84528639f184b20cb50
Michael Stoll <Michael.Stoll@uni-bayreuth.de>
2023-01-04 12:07:20 +0100

    fix documentation

--- src/number_theory/diophantine_approximation.lean
+++ src/number_theory/diophantine_approximation.lean
@@ -34,7 +34,7 @@ The main results are
   `j` and `k` with `0 < k ≤ n` and `|k*ξ - j| ≤ 1/(n+1)`,
 * `real.exists_int_abs_mul_sub_round_le`, which is a variant of this replacing `j`
   by `round(k*ξ)`,
-* A further variant `exists_rat_abs_sub_le_and_denom_le` in terms of rationals `q`
+* A further variant `real.exists_rat_abs_sub_le_and_denom_le` in terms of rationals `q`
   satisfying `|ξ - q| ≤ 1/((n+1)*q.denom)` and `q.denom ≤ n`,
 * `dioph_approx.rat_approx_infinite`, which states that for irrational `ξ`,
   `{q : ℚ | |ξ - q| < 1/q.denom^2}` is infinite,

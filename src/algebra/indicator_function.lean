2729af787c4ef40766b87065767df74bc26002e5
Yaël Dillies <yael.dillies@gmail.com>
2023-01-06 18:09:33 +0000

    fix algebra.indicator_function

--- src/algebra/indicator_function.lean
+++ src/algebra/indicator_function.lean
@@ -364,8 +364,8 @@ by rw [sub_eq_add_neg, indicator_compl']
 
 @[to_additive indicator_diff'] lemma mul_indicator_diff (h : s ⊆ t) (f : α → G) :
   mul_indicator (t \ s) f = mul_indicator t f * (mul_indicator s f)⁻¹ :=
-eq_mul_inv_of_mul_eq $ by rw [pi.mul_def, ←mul_indicator_union_of_disjoint disjoint_sdiff_self_left
-  f, diff_union_self, union_eq_self_of_subset_right h]
+eq_mul_inv_of_mul_eq $ by { rw [pi.mul_def, ←mul_indicator_union_of_disjoint, diff_union_self,
+  union_eq_self_of_subset_right h], exact disjoint_sdiff_self_left }
 
 lemma indicator_diff {G : Type*} [add_group G] {s t : set α} (h : s ⊆ t) (f : α → G) :
   indicator (t \ s) f = indicator t f - indicator s f :=

4b76a19f39285045de4c3a2c4af095e957a6ac75
Ruben Van de Velde <65514131+Ruben-VandeVelde@users.noreply.github.com>
2023-01-06 15:15:49 +0100

    Update scripts/lint-bib.sh
    Co-authored-by: Eric Wieser <wieser.eric@gmail.com>

--- scripts/lint-bib.sh
+++ scripts/lint-bib.sh
@@ -2,6 +2,7 @@
 
 set -exo pipefail
 # https://leanprover-community.github.io/contribute/doc.html#citing-other-works
+cp docs/references.bib docs/references.bib.old
 bibtool --preserve.key.case=on --preserve.keys=on --pass.comments=on --print.use.tab=off -s \
   -i docs/references.bib -o docs/references.bib
-git diff --exit-code
+diff docs/references.bib.old docs/references.bib 

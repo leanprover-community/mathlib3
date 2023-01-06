0d30a31f5c3c6e88a2f0424a2a55c51917ca92d1
RemyDegenne <Remydegenne@gmail.com>
2023-01-06 18:17:28 +0100

    simplify proofs

--- src/probability/kernel/basic.lean
+++ src/probability/kernel/basic.lean
@@ -522,13 +522,7 @@ end
 lemma measurable_lintegral' (κ : kernel α β) [is_s_finite_kernel κ]
   {f : β → ℝ≥0∞} (hf : measurable f) :
   measurable (λ a, ∫⁻ b, f b ∂κ a) :=
-begin
-  refine measurable_lintegral _ (λ t ht, _),
-  have : ((λ (p : α × β), f p.snd) ⁻¹' t) = set.univ ×ˢ (f ⁻¹' t),
-  { ext1 p, simp only [set.mem_preimage, set.mem_prod, set.mem_univ, true_and], },
-  simp_rw [function.uncurry_def, this],
-  exact measurable_set.univ.prod (hf ht),
-end
+measurable_lintegral κ (hf.comp measurable_snd)
 
 lemma measurable_set_lintegral (κ : kernel α β) [is_s_finite_kernel κ]
   {f : α → β → ℝ≥0∞} (hf : measurable (function.uncurry f)) {s : set β} (hs : measurable_set s) :
@@ -538,7 +532,7 @@ by { simp_rw ← lintegral_restrict κ hs, exact measurable_lintegral _ hf }
 lemma measurable_set_lintegral' (κ : kernel α β) [is_s_finite_kernel κ]
   {f : β → ℝ≥0∞} (hf : measurable f) {s : set β} (hs : measurable_set s) :
   measurable (λ a, ∫⁻ b in s, f b ∂κ a) :=
-by { simp_rw ← lintegral_restrict κ hs, exact measurable_lintegral' _ hf }
+measurable_set_lintegral κ (hf.comp measurable_snd) hs
 
 end measurable_lintegral
 

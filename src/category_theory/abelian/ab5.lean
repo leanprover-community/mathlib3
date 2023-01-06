9db5482faaac48cb3530b445b3a236a5e389c287
Jakob von Raumer <jakob@von-raumer.de>
2023-01-06 11:44:43 +0100

    preserves_colimits_colim has to be moved elsewhere

--- src/category_theory/abelian/ab5.lean
+++ src/category_theory/abelian/ab5.lean
@@ -2,20 +2,33 @@ import category_theory.abelian.basic
 import category_theory.abelian.exact
 import category_theory.limits.filtered
 
-universes w' w v u
+universes v u
 
 noncomputable theory
 
 open category_theory
 open category_theory.limits
 
-variables {C : Type u} [category.{v} C] [abelian C] [has_coproducts C]
+variables (C : Type u) [category.{v} C] [abelian C] [has_coproducts.{v} C]
 
 namespace category_theory
 
 class ab5 : Type (max u (v + 1)) :=
-(existence : ∀ (J : Type v) [small_category J] [is_filtered J], has_colimits_of_shape J C)
+(existence [] : ∀ (J : Type v) [small_category J] [is_filtered J], has_colimits_of_shape J C)
 (preserves_finite_limits [] : ∀ (J : Type v) [small_category J] [is_filtered J],
-  preserves_finite_limits (limits.colim : (J ⥤ C) ⥤ C))
+  preserves_finite_limits (colim : (J ⥤ C) ⥤ C))
+
+namespace ab5
+
+variables {C} [ab5 C]
+
+lemma has_colimits_of : has_colimits C :=
+has_colimits_of_has_coequalizers_and_coproducts
+
+instance preserves_colimits_colim (J : Type*) [category J] [has_colimits_of_shape J C] :
+  preserves_colimits (colim : (J ⥤ C) ⥤ C) :=
+adjunction.left_adjoint_preserves_colimits colim_const_adj
+
+end ab5
 
 end category_theory

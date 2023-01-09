4a990ac9355cadaeb79a8ee7cc4dd17a4b699155
David Loeffler <d.loeffler.01@cantab.net>
2023-01-09 23:11:41 +0000

    lint!

--- src/number_theory/zeta_values.lean
+++ src/number_theory/zeta_values.lean
@@ -3,11 +3,13 @@ Copyright (c) 2022 David Loeffler. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: David Loeffler
 -/
+
 import number_theory.bernoulli_polynomials
 import analysis.special_functions.integrals
 import measure_theory.integral.interval_integral
 import analysis.fourier.add_circle
-/-
+
+/-!
 # Critical values of the Riemann zeta function
 
 In this file we evaluate the Fourier coefficients of Bernoulli polynomials on the interval `[0, 1]`.

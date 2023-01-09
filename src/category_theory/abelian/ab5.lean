b8401f46493ff2741d5aa5a8c1506d6f13fab843
Jakob von Raumer <jakob@von-raumer.de>
2023-01-09 12:23:43 +0100

    doc

--- src/category_theory/abelian/ab5.lean
+++ src/category_theory/abelian/ab5.lean
@@ -1,6 +1,19 @@
+/-
+Copyright (c) 2023 Jakob von Raumer. All rights reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
+Authors: Jakob von Raumer
+-/
+
 import category_theory.abelian.basic
 import category_theory.abelian.exact
 import category_theory.limits.filtered
+
+/-!
+# AB5-categories
+
+We define AB5-categories as abelian categories which have all coproducts and all filtered colimits,
+and in which the the functor `colim : (J ⥤ C) ⥤ C` is right exact.
+-/
 
 universes v u
 
@@ -13,22 +26,13 @@ variables (C : Type u) [category.{v} C] [abelian C] [has_coproducts.{v} C]
 
 namespace category_theory
 
+/--
+AB5-categories are abelian categories which have all coproducts and all filtered colimits, and
+in which the the functor `colim : (J ⥤ C) ⥤ C` is right exact.
+-/
 class ab5 : Type (max u (v + 1)) :=
 (existence [] : ∀ (J : Type v) [small_category J] [is_filtered J], has_colimits_of_shape J C)
 (preserves_finite_limits [] : ∀ (J : Type v) [small_category J] [is_filtered J],
   preserves_finite_limits (colim : (J ⥤ C) ⥤ C))
-
-namespace ab5
-
-variables {C} [ab5 C]
-
-lemma has_colimits_of : has_colimits C :=
-has_colimits_of_has_coequalizers_and_coproducts
-
-instance preserves_colimits_colim (J : Type*) [category J] [has_colimits_of_shape J C] :
-  preserves_colimits (colim : (J ⥤ C) ⥤ C) :=
-adjunction.left_adjoint_preserves_colimits colim_const_adj
-
-end ab5
 
 end category_theory

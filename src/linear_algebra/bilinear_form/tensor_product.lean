150c930b40f611fc05442e2413d6f302a1ccfd4c
Eric Wieser <wieser.eric@gmail.com>
2023-01-09 16:31:07 +0000

    wip

---
+++ src/linear_algebra/bilinear_form/tensor_product.lean
@@ -0,0 +1,88 @@
+/-
+Copyright (c) 2023 Eric Wieser. All rights reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
+Authors: Eric Wieser
+-/
+import linear_algebra.bilinear_form
+import linear_algebra.tensor_product
+import linear_algebra.contraction
+
+/-! # Bilinear form on tensor product
+
+## Main definitions
+
+* `bilin_form.tmul Q₁ Q₂`: the bilinear form constructed elementwise on a tensor product
+
+-/
+
+universes u v w
+variables {ι : Type*} {R : Type*} {M₁ M₂ : Type*}
+
+open_locale tensor_product
+
+namespace bilin_form
+
+
+section comm_semiring
+variables [comm_semiring R]
+variables [add_comm_monoid M₁] [add_comm_monoid M₂]
+variables [module R M₁] [module R M₂]
+
+/-- The tensor product of two bilinear forms. -/
+protected def tmul (B₁ : bilin_form R M₁) (B₂ : bilin_form R M₂) : bilin_form R (M₁ ⊗[R] M₂) :=
+linear_map.to_bilin $ begin
+  refine (tensor_product.lift.equiv R (M₁ ⊗[R] M₂) (M₁ ⊗[R] M₂) R).symm _,
+  refine _ ∘ₗ (tensor_product.tensor_tensor_tensor_comm R _ _ _ _).to_linear_map,
+  refine (tensor_product.lid R R).to_linear_map ∘ₗ _,
+  exact tensor_product.map (tensor_product.lift B₁.to_lin) (tensor_product.lift B₂.to_lin),
+end
+
+@[simp] lemma tmul_tmul (B₁ : bilin_form R M₁) (B₂ : bilin_form R M₂)
+  (m₁ m₁' : M₁) (m₂ m₂' : M₂) :
+  B₁.tmul B₂ (m₁ ⊗ₜ m₂) (m₁' ⊗ₜ m₂') = B₁ m₁ m₁' * B₂ m₂ m₂' :=
+(tensor_product.lift.tmul _ _).trans $
+  congr_arg2 (*) (tensor_product.lift.tmul _ _) (tensor_product.lift.tmul _ _)
+
+end comm_semiring
+
+section comm_ring
+variables [comm_ring R]
+variables [add_comm_group M₁] [add_comm_group M₂]
+variables [module R M₁] [module R M₂]
+variables [module.free R M₁] [module.finite R M₁]
+variables [module.free R M₂] [module.finite R M₂]
+variables [nontrivial R]
+
+
+noncomputable def tmul_equiv :
+  bilin_form R M₁ ⊗[R] bilin_form R M₂ ≃ₗ[R] bilin_form R (M₁ ⊗[R] M₂) :=
+begin
+  -- convert to bilinear maps
+  refine (tensor_product.congr linear_map.to_bilin linear_map.to_bilin).symm
+    ≪≫ₗ _ ≪≫ₗ linear_map.to_bilin,
+  -- convert to tnsor products
+  refine (tensor_product.congr (tensor_product.lift.equiv R M₁ M₁ R)
+                               (tensor_product.lift.equiv R M₂ M₂ R))
+    ≪≫ₗ _ ≪≫ₗ (tensor_product.lift.equiv R (M₁ ⊗[R] M₂) (M₁ ⊗[R] M₂) R).symm,
+  -- reassociate
+  refine _ ≪≫ₗ
+    (tensor_product.tensor_tensor_tensor_comm R _ _ _ _).arrow_congr (tensor_product.lid R R),
+  exact hom_tensor_hom_equiv R _ _ R R
+end
+
+lemma tmul_equiv_tmul (B₁ : bilin_form R M₁) (B₂ : bilin_form R M₂) :
+  tmul_equiv (B₁ ⊗ₜ[R] B₂) = B₁.tmul B₂ :=
+begin
+  dsimp [tmul_equiv, bilin_form.tmul, tensor_product.lift.equiv_apply],
+  refine congr_arg _ _,
+  refine congr_arg _ _,
+  ext i : 1,
+  dsimp [tensor_product.lift.equiv_apply],
+  refine congr_arg _ _,
+  -- exact hom_tensor_hom_equiv_apply _,
+
+end
+
+end comm_ring
+
+end bilin_form
--- src/linear_algebra/quadratic_form/tensor_product.lean
+++ src/linear_algebra/quadratic_form/tensor_product.lean
@@ -3,8 +3,8 @@ Copyright (c) 2023 Eric Wieser. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Eric Wieser
 -/
+import linear_algebra.bilinear_form.tensor_product
 import linear_algebra.quadratic_form.isometry
-import linear_algebra.tensor_product
 
 /-! # Quadratic form on tensor product
 
@@ -12,39 +12,15 @@ import linear_algebra.tensor_product
 
 * `quadratic_form.tmul Q₁ Q₂`: the quadratic form constructed elementwise on a tensor product
 
-## Main results
-
-* `quadratic_form.equivalent.prod`, `quadratic_form.equivalent.pi`: quadratic forms are equivalent
-  if their components are equivalent
--- * `quadratic_form.nonneg_prod_iff`, `quadratic_form.nonneg_pi_iff`: quadratic forms are positive-
---   semidefinite if and only if their components are positive-semidefinite.
--- * `quadratic_form.pos_def_prod_iff`, `quadratic_form.pos_def_pi_iff`: quadratic forms are positive-
---   definite if and only if their components are positive-definite.
 -/
 
 universes u v w
-variables {ι : Type*} {R : Type*} {M₁ M₂ N₁ N₂ : Type*} {Mᵢ Nᵢ : ι → Type*}
+variables {R : Type*} {M₁ M₂ : Type*}
 variables [comm_semiring R]
-variables [add_comm_monoid M₁] [add_comm_monoid M₂] [add_comm_monoid N₁] [add_comm_monoid N₂]
-variables [module R M₁] [module R M₂] [module R N₁] [module R N₂]
-variables [Π i, add_comm_monoid (Mᵢ i)] [Π i, add_comm_monoid (Nᵢ i)]
-variables [Π i, module R (Mᵢ i)] [Π i, module R (Nᵢ i)]
+variables [add_comm_monoid M₁] [add_comm_monoid M₂]
+variables [module R M₁] [module R M₂]
 
 open_locale tensor_product
-
-def bilin_form.tmul (B₁ : bilin_form R M₁) (B₂ : bilin_form R M₂) : bilin_form R (M₁ ⊗[R] M₂) :=
-linear_map.to_bilin $ begin
-  refine (tensor_product.lift.equiv R (M₁ ⊗[R] M₂) (M₁ ⊗[R] M₂) R).symm _,
-  refine _ ∘ₗ (tensor_product.tensor_tensor_tensor_comm R _ _ _ _).to_linear_map,
-  refine (tensor_product.lid R R).to_linear_map ∘ₗ _,
-  exact tensor_product.map (tensor_product.lift B₁.to_lin) (tensor_product.lift B₂.to_lin),
-end
-
-lemma bilin_form.tmul_tmul (B₁ : bilin_form R M₁) (B₂ : bilin_form R M₂)
-  (m₁ m₁' : M₁) (m₂ m₂' : M₂) :
-  B₁.tmul B₂ (m₁ ⊗ₜ m₂) (m₁' ⊗ₜ m₂') = B₁ m₁ m₁' * B₂ m₂ m₂' :=
-(tensor_product.lift.tmul _ _).trans $
-  congr_arg2 (*) (tensor_product.lift.tmul _ _) (tensor_product.lift.tmul _ _)
 
 namespace quadratic_form
 
@@ -64,25 +40,35 @@ def to_linear_tensor_product (Q : quadratic_form R M₁) : M₁ →ₗ[R] (R ⊗
 -/
 def tmul (Q₁ : quadratic_form R M₁) (Q₂ : quadratic_form R M₂) : quadratic_form R (M₁ ⊗[R] M₂) :=
 { to_fun := λ x, begin
-    refine add_con.lift_on x _ _,
-    exact free_add_monoid.lift (λ m : _ × _, Q₁ m.1 * Q₂ m.2),
-    intros a b h,
-    induction h,
-    case of : a' b' hab' {
-      induction hab',
-      { simp_rw [free_add_monoid.lift_eval_of, map_zero, zero_mul, _root_.map_zero], },
-      { simp_rw [free_add_monoid.lift_eval_of, map_zero, mul_zero, _root_.map_zero], },
-      { simp_rw [map_add, free_add_monoid.lift_eval_of, free_add_monoid.lift_eval_of],
-        rw[map_zero, mul_zero, _root_.map_zero], },
-    },
-    case refl { refl },
-    case symm : a' b' h' ih { exact ih.symm },
-    case trans : a' b' c' hab' hbc' ihab ihbc { exact ihab.trans ihbc },
-    case add : a' b' c' d' hab' hcd' ihab ihcd { rw [map_add, map_add, ihab, ihcd] },
+    sorry
+    -- refine add_con.lift_on x _ _,
+    -- exact free_add_monoid.lift (λ m : _ × _, Q₁ m.1 * Q₂ m.2),
+    -- intros a b h,
+    -- induction h,
+    -- case of : a' b' hab' {
+    --   induction hab',
+    --   { simp_rw [free_add_monoid.lift_eval_of, map_zero, zero_mul, _root_.map_zero], },
+    --   { simp_rw [free_add_monoid.lift_eval_of, map_zero, mul_zero, _root_.map_zero], },
+    --   { simp_rw [map_add, free_add_monoid.lift_eval_of, free_add_monoid.lift_eval_of],
+    --     rw[map_zero, mul_zero, _root_.map_zero], },
+    -- },
+    -- case refl { refl },
+    -- case symm : a' b' h' ih { exact ih.symm },
+    -- case trans : a' b' c' hab' hbc' ihab ihbc { exact ihab.trans ihbc },
+    -- case add : a' b' c' d' hab' hcd' ihab ihcd { rw [map_add, map_add, ihab, ihcd] },
   end,
   to_fun_smul := sorry,
-  exists_companion' := sorry }
+  exists_companion' := begin
+    obtain ⟨B₁, hB₁⟩ := Q₁.exists_companion,
+    obtain ⟨B₂, hB₂⟩ := Q₂.exists_companion,
+    refine ⟨B₁.tmul B₂, λ x y, _⟩,
+    induction x using tensor_product.induction_on,
+    sorry
+  end }
 
+@[simp] lemma tmul_tmul (Q₁ : quadratic_form R M₁) (Q₂ : quadratic_form R M₂)
+  (m₁ m₁' : M₁) (m₂ m₂' : M₂) :
+  Q₁.tmul Q₂ (m₁ ⊗ₜ m₂) = Q₁ m₁ * Q₂ m₂ :=
 #check quadratic_form.add
 
 end quadratic_form

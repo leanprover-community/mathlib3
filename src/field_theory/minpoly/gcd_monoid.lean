141d46353b6e1931fabf8701ad5d136187164ccf
Paul-Lez <72892199+Paul-Lez@users.noreply.github.com>
2023-01-09 15:46:35 +0100

    Fixes and more cleanup

--- src/field_theory/minpoly/gcd_monoid.lean
+++ src/field_theory/minpoly/gcd_monoid.lean
@@ -31,20 +31,21 @@ open polynomial set function minpoly
 
 namespace minpoly
 
-variables {R S : Type*} [comm_ring R] [comm_ring S] [is_domain R] [is_domain S] [algebra R S]
+variables {R S : Type*} [comm_ring R] [comm_ring S] [is_domain R] [algebra R S]
 
 section gcd_domain
 
-variables (K L : Type*) [is_integrally_closed R] [field K] [algebra R K] [is_fraction_ring R K]
-  [field L] [algebra S L] [algebra K L] [algebra R L] [is_scalar_tower R K L]
-  [is_scalar_tower R S L] {s : S} (hs : is_integral R s)
+variables (K L : Type*) [field K] [algebra R K] [is_fraction_ring R K] [field L]  [algebra R L]
+ [algebra S L] [algebra K L] [is_scalar_tower R K L] [is_scalar_tower R S L]
 
-include hs
+section gcd_domain
+
+variable [normalized_gcd_monoid R]
 
 /-- For GCD domains, the minimal polynomial over the ring is the same as the minimal polynomial
 over the fraction field. See `minpoly.gcd_domain_eq_field_fractions'` if `S` is already a
 `K`-algebra. -/
-lemma gcd_domain_eq_field_fractions :
+lemma gcd_domain_eq_field_fractions [is_domain S] {s : S} (hs : is_integral R s) :
   minpoly K (algebra_map S L s) = (minpoly R s).map (algebra_map R K) :=
 begin
   refine (eq_of_irreducible_of_monic _ _ _).symm,
@@ -54,13 +55,25 @@ begin
   { exact (monic hs).map _ }
 end
 
-lemma test {α β : Type} (f : α → β) {a b : α} : a = b → f a = f b :=
+/-- For GCD domains, the minimal polynomial over the ring is the same as the minimal polynomial
+over the fraction field. Compared to `minpoly.gcd_domain_eq_field_fractions`, this version is useful
+if the element is in a ring that is already a `K`-algebra. -/
+lemma gcd_domain_eq_field_fractions' [is_domain S] [algebra K S] [is_scalar_tower R K S]
+  {s : S} (hs : is_integral R s) : minpoly K s = (minpoly R s).map (algebra_map R K) :=
 begin
-  exact congr_arg (λ {a : α}, f a)
+  let L := fraction_ring S,
+  rw [← gcd_domain_eq_field_fractions K L hs],
+  refine minpoly.eq_of_algebra_map_eq (is_fraction_ring.injective S L)
+    (is_integral_of_is_scalar_tower hs) rfl
 end
 
+end gcd_domain
+
+variables [is_integrally_closed R]
+
 /- TODO: see if the `no_zero_smul_divisors S L` assumption can be lifted -/
-theorem is_integrally_closed.map_minpoly_eq_minpoly_fraction_ring [no_zero_smul_divisors S L] :
+theorem is_integrally_closed_map_minpoly_eq_minpoly_fraction_ring [no_zero_smul_divisors S L]
+  {s : S} (hs : is_integral R s) :
   minpoly K (algebra_map S L s) = (map (algebra_map R K) (minpoly R s)) :=
 begin
   --the idea of the proof is the following: since the minpoly of `a` over `Frac(R)` divides the
@@ -70,7 +83,8 @@ begin
 
   --a few "trivial" preliminary results to set up the proof
   have lem0 : minpoly K (algebra_map S L s) ∣ (map (algebra_map R K) (minpoly R s)),
-  { exact (dvd_map_of_is_scalar_tower' K L hs) },
+  { apply dvd_map_of_is_scalar_tower' K L,
+    all_goals { assumption } },
 
   have lem1 : is_integral K (algebra_map S L s),
   { refine is_integral_map_of_comp_eq_of_is_integral (algebra_map R K) _ _ hs,
@@ -94,26 +108,13 @@ begin
   { exact nat_degree_le_nat_degree (minpoly.min R s lem3 lem2) },
 end
 
-/-- For GCD domains, the minimal polynomial over the ring is the same as the minimal polynomial
-over the fraction field. Compared to `minpoly.gcd_domain_eq_field_fractions`, this version is useful
-if the element is in a ring that is already a `K`-algebra. -/
-lemma gcd_domain_eq_field_fractions' [algebra K S] [is_scalar_tower R K S] :
-  minpoly K s = (minpoly R s).map (algebra_map R K) :=
-begin
-  let L := fraction_ring S,
-  rw [← gcd_domain_eq_field_fractions K L hs],
-  refine minpoly.eq_of_algebra_map_eq (is_fraction_ring.injective S L)
-    (is_integral_of_is_scalar_tower hs) rfl
-end
-
-variable [no_zero_smul_divisors R S]
-
+variables [is_domain S] [no_zero_smul_divisors R S]
 
 /-- For integrally closed rings, the minimal polynomial divides any polynomial that has the
   integral element as root. See also `minpoly.dvd` which relaxes the assumptions on `S`
   in exchange for stronger assumptions on `R`. -/
-theorem is_integrally_closed_dvd [nontrivial R] [is_integrally_closed R] (p : R[X])
-  (hs : is_integral R s) : polynomial.aeval s p = 0 ↔ minpoly R s ∣ p :=
+theorem is_integrally_closed_dvd [nontrivial R] (p : R[X]) {s : S} (hs : is_integral R s) :
+  polynomial.aeval s p = 0 ↔ minpoly R s ∣ p :=
 begin
   refine ⟨λ hp, _, λ hp, _⟩,
 
@@ -125,52 +126,51 @@ begin
       rw [← is_scalar_tower.algebra_map_eq, ← is_scalar_tower.algebra_map_eq],
 
       apply dvd_mul_of_dvd_left,
-      rw is_integrally_closed.map_minpoly_eq_minpoly_fraction_ring
+      rw is_integrally_closed_map_minpoly_eq_minpoly_fraction_ring
         (fraction_ring R) (fraction_ring S) hs,
 
       exact monic.map _ (minpoly.monic hs) },
-    rw [is_integrally_closed.map_minpoly_eq_minpoly_fraction_ring _ _ hs,  map_dvd_map
+    rw [is_integrally_closed_map_minpoly_eq_minpoly_fraction_ring _ _ hs, map_dvd_map
       (algebra_map R (fraction_ring R)) (is_fraction_ring.injective R (fraction_ring R))
       (minpoly.monic hs)] at this,
     rw [← dvd_iff_mod_by_monic_eq_zero (minpoly.monic hs)],
     refine polynomial.eq_zero_of_dvd_of_degree_lt this
-      (degree_mod_by_monic_lt p $ minpoly.monic hs) },
+      (degree_mod_by_monic_lt p $ minpoly.monic hs),
+      all_goals { apply_instance } },
 
   { simpa only [ring_hom.mem_ker, ring_hom.coe_comp, coe_eval_ring_hom,
       coe_map_ring_hom, function.comp_app, eval_map, ← aeval_def] using
       aeval_eq_zero_of_dvd_aeval_eq_zero hp (minpoly.aeval R s) }
 end
 
-lemma ker_eval [is_integrally_closed R] [nontrivial R] {a : S} (ha : is_integral R a) :
-    ((polynomial.aeval a).to_ring_hom : R[X] →+* S).ker = ideal.span ({ minpoly R a} : set R[X] ):=
+lemma ker_eval [nontrivial R] {s : S} (hs : is_integral R s) :
+    ((polynomial.aeval s).to_ring_hom : R[X] →+* S).ker = ideal.span ({ minpoly R s} : set R[X] ):=
 begin
   apply le_antisymm,
   { intros p hp,
     rwa [ring_hom.mem_ker, alg_hom.to_ring_hom_eq_coe, alg_hom.coe_to_ring_hom,
-      is_integrally_closed_dvd ha, ← ideal.mem_span_singleton] at hp },
+      is_integrally_closed_dvd p hs, ← ideal.mem_span_singleton] at hp },
   { intros p hp,
     rwa [ring_hom.mem_ker, alg_hom.to_ring_hom_eq_coe, alg_hom.coe_to_ring_hom,
-      is_integrally_closed_dvd ha, ← ideal.mem_span_singleton] }
+      is_integrally_closed_dvd p hs, ← ideal.mem_span_singleton] }
 end
 
 /-- If an element `x` is a root of a nonzero polynomial `p`, then the degree of `p` is at least the
 degree of the minimal polynomial of `x`. See also `minpoly.degree_le_of_ne_zero` which relaxes the
 assumptions on `S` in exchange for stronger assumptions on `R`. -/
-lemma is_integrally_closed.degree_le_of_ne_zero {p : R[X]} (hp0 : p ≠ 0)
-   (hp : polynomial.aeval s p = 0) : degree (minpoly R s) ≤ degree p :=
+lemma is_integrally_closed.degree_le_of_ne_zero {s : S} (hs : is_integral R s) {p : R[X]}
+  (hp0 : p ≠ 0) (hp : polynomial.aeval s p = 0) : degree (minpoly R s) ≤ degree p :=
 begin
   rw [degree_eq_nat_degree (minpoly.ne_zero hs), degree_eq_nat_degree hp0],
   norm_cast,
   exact nat_degree_le_of_dvd ((is_integrally_closed_dvd _ hs).mp hp) hp0
 end
 
-omit hs
-
 /-- The minimal polynomial of an element `x` is uniquely characterized by its defining property:
 if there is another monic polynomial of minimal degree that has `x` as a root, then this polynomial
 is equal to the minimal polynomial of `x`. See also `minpoly.unique` which relaxes the
 assumptions on `S` in exchange for stronger assumptions on `R`. -/
-lemma is_integrally_closed.minpoly.unique {P : R[X]} (hmo : P.monic)
+lemma is_integrally_closed.minpoly.unique {s : S} {P : R[X]} (hmo : P.monic)
   (hP : polynomial.aeval s P = 0)
   (Pmin : ∀ Q : R[X], Q.monic → polynomial.aeval s Q = 0 → degree P ≤ degree Q) :
   P = minpoly R s :=
@@ -194,7 +194,7 @@ noncomputable theory
 
 open algebra polynomial adjoin_root
 
-variables {R} {x : S} [is_integrally_closed R] [no_zero_smul_divisors R S]
+variables {R} [is_integrally_closed R] [no_zero_smul_divisors R S] [is_domain S] {x : S}
 
 lemma to_adjoin.injective (hx : is_integral R x) :
   function.injective (minpoly.to_adjoin R x) :=

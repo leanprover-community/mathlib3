7d317dd8c6c894c2b90708f050c7924302917767
Yaël Dillies <yael.dillies@gmail.com>
2023-01-10 18:21:27 +0000

    restore is_cycle_on_conj

--- src/group_theory/perm/cycle/basic.lean
+++ src/group_theory/perm/cycle/basic.lean
@@ -649,6 +649,10 @@ by simp [is_cycle_on, set.bij_on_perm_inv]
 
 alias is_cycle_on_inv ↔ is_cycle_on.of_inv is_cycle_on.inv
 
+lemma is_cycle_on.conj (h : f.is_cycle_on s) : (g * f * g⁻¹).is_cycle_on ((g : perm α) '' s) :=
+⟨(g.bij_on_image.comp h.1).comp g.bij_on_symm_image,
+  λ x hx y hy, by { rw ←preimage_inv at hx hy, convert (h.2 hx hy).conj; rw apply_inv_self }⟩
+
 lemma is_cycle_on_swap [decidable_eq α] (hab : a ≠ b) : (swap a b).is_cycle_on {a, b} :=
 ⟨bij_on_swap (by simp) (by simp), λ x hx y hy, begin
   rw [set.mem_insert_iff, set.mem_singleton_iff] at hx hy,

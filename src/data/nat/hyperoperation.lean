d5c51838b48915e2eb026476c745b41f4dd08771
Mark Andrew Gerads <nazgand@gmail.com>
2023-01-10 02:41:00 -0800

    Fix to match some suggestions

--- src/data/nat/hyperoperation.lean
+++ src/data/nat/hyperoperation.lean
@@ -26,6 +26,11 @@ hyperoperation
 
 /--
 Implementation of the hyperoperation sequence
+`hyperoperation 0 m k = k + 1`
+`hyperoperation 1 m k = m + k`
+`hyperoperation 2 m k = m * k`
+`hyperoperation 3 m k = m ^ k`
+`hyperoperation (n + 1) m (k + 1) = hyperoperation n m (hyperoperation (n + 1) m k)`
 -/
 def hyperoperation : ℕ → ℕ → ℕ → ℕ
 | 0 _ k := k + 1
@@ -40,21 +45,15 @@ lemma hyperoperation_zero_eq_succ (m k : ℕ) : hyperoperation 0 m k = k + 1 :=
 by rw hyperoperation
 
 lemma hyperoperation_one_eq_self (m : ℕ) : hyperoperation 1 m 0 = m :=
-begin
-  rw hyperoperation,
-end
+by rw hyperoperation
 
 lemma hyperoperation_two_eq_zero (m : ℕ) : hyperoperation 2 m 0 = 0 :=
-begin
-  rw hyperoperation,
-end
+by rw hyperoperation
 
-lemma hyperoperation_n3a0_1 (n m : ℕ) : hyperoperation (n + 3) m 0 = 1 :=
-begin
-  rw hyperoperation,
-end
+lemma hyperoperation_three_eq_one (n m : ℕ) : hyperoperation (n + 3) m 0 = 1 :=
+by rw hyperoperation
 
-lemma hyperoperation_n1ab1_recurse (n m k : ℕ) :
+lemma hyperoperation_recursion (n m k : ℕ) :
   hyperoperation (n + 1) m (k + 1) = hyperoperation n m (hyperoperation (n + 1) m k) :=
 by obtain (_|_|_) := n; rw hyperoperation
 
@@ -63,26 +62,26 @@ by obtain (_|_|_) := n; rw hyperoperation
 lemma hyperoperation_1_addition (m k : ℕ) : hyperoperation 1 m k = m + k :=
 begin
   induction k with bn bih,
-  rw [nat_add_zero m, hyperoperation_1a0_a],
-  rw [hyperoperation_n1ab1_recurse,bih,hyperoperation_0ab_b.succ],
+  rw [nat_add_zero m, hyperoperation_one_eq_self],
+  rw [hyperoperation_recursion,bih,hyperoperation_zero_eq_succ],
   exact nat.add_assoc m bn 1,
 end
 
 lemma hyperoperation_2_multiplication (m k : ℕ) : hyperoperation 2 m k = m * k :=
 begin
   induction k with bn bih,
-  rw hyperoperation_2a0_0,
+  rw hyperoperation_two_eq_zero,
   exact (nat.mul_zero m).symm,
-  rw [hyperoperation_n1ab1_recurse,hyperoperation_1_addition,bih],
+  rw [hyperoperation_recursion,hyperoperation_1_addition,bih],
   ring,
 end
 
 lemma hyperoperation_3_exponentiation (m k : ℕ) : hyperoperation 3 m k = m ^ k :=
 begin
   induction k with bn bih,
-  rw hyperoperation_n3a0_1,
+  rw hyperoperation_three_eq_one,
   exact (pow_zero m).symm,
-  rw [hyperoperation_n1ab1_recurse,hyperoperation_2_multiplication,bih],
+  rw [hyperoperation_recursion,hyperoperation_2_multiplication,bih],
   exact (pow_succ m bn).symm,
 end
 
@@ -91,12 +90,12 @@ begin
   induction n with nn nih,
   rw hyperoperation_2_multiplication,
   ring,
-  rw [hyperoperation_n1ab1_recurse,hyperoperation_n3a0_1,nih],
+  rw [hyperoperation_recursion,hyperoperation_three_eq_one,nih],
 end
 
 lemma hyperoperation_succn_2_2_eq_4 (n : ℕ) : hyperoperation (n + 1) 2 2 = 4 :=
 begin
   induction n with nn nih,
   rw hyperoperation_1_addition,
-  rw [hyperoperation_n1ab1_recurse,hyperoperation_n2a1_a,nih],
+  rw [hyperoperation_recursion,hyperoperation_n2a1_a,nih],
 end

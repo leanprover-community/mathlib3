3a307e57c210c57ce662855817c467b86e32adb7
Eric Wieser <wieser.eric@gmail.com>
2023-01-11 23:05:41 +0000

    feat(analysis/special_functions/pow): interaction between `inv` and `cpow`

--- src/analysis/special_functions/pow.lean
+++ src/analysis/special_functions/pow.lean
@@ -21,6 +21,7 @@ We also prove basic properties of these functions.
 noncomputable theory
 
 open_locale classical real topological_space nnreal ennreal filter big_operators asymptotics
+open_locale complex_conjugate
 open filter finset set
 
 namespace complex
@@ -491,6 +492,28 @@ begin
   { rw [of_real_zero, zero_cpow, map_zero, real.zero_rpow hy],
     exact ne_of_apply_ne re hy },
   { exact abs_cpow_eq_rpow_re_of_pos hlt y }
+end
+
+lemma inv_cpow_eq_ite (x : ℂ) (n : ℂ) :
+  x⁻¹ ^ n = if x.arg = π then conj (x ^ conj n)⁻¹ else (x ^ n)⁻¹ :=
+begin
+  simp_rw [complex.cpow_def, log_inv_eq_ite, inv_eq_zero, map_eq_zero, ite_mul, neg_mul,
+    is_R_or_C.conj_inv, apply_ite conj, apply_ite exp, apply_ite has_inv.inv, map_zero, map_one,
+    exp_neg, inv_one, inv_zero, ←exp_conj, map_mul, conj_conj],
+  split_ifs with hx hn ha ha; refl,
+end
+
+lemma inv_cpow (x : ℂ) (n : ℂ) (hx : x.arg ≠ π) : x⁻¹ ^ n = (x ^ n)⁻¹ :=
+by rw [inv_cpow_eq_ite, if_neg hx]
+
+/-- `complex.inv_cpow_eq_ite` with the `ite` on the other side. -/
+lemma inv_cpow_eq_ite' (x : ℂ) (n : ℂ) :
+  (x ^ n)⁻¹ = if x.arg = π then conj (x⁻¹ ^ conj n) else x⁻¹ ^ n :=
+begin
+  rw inv_cpow_eq_ite,
+  split_ifs,
+  { rw [conj_conj, conj_conj] },
+  { rw inv_cpow _ _ h }
 end
 
 end complex

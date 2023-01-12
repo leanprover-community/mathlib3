0f8c7d0c9e7c9cf22f4c8f8cb3ecf7c996217113
Sidharth Hariharan <fundamental.theor3m@gmail.com>
2023-01-12 18:55:19 +0000

    copyright

--- src/data/polynomial/partial_fractions.lean
+++ src/data/polynomial/partial_fractions.lean
@@ -1,5 +1,5 @@
 /-
-Copyright (c) 2022 The Xena Project. All rights reserved.
+Copyright (c) Sidharth Hariharan. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Kevin Buzzard, Sidharth Hariharan
 -/

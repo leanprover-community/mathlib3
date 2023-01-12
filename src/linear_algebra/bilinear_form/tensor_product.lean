74c3cfe4b3b11b996615d4f795e1c3b40a8671a7
Eric Wieser <wieser.eric@gmail.com>
2023-01-12 02:00:39 +0000

    and the equiv version, which is now easy

--- src/linear_algebra/bilinear_form/tensor_product.lean
+++ src/linear_algebra/bilinear_form/tensor_product.lean
@@ -11,7 +11,9 @@ import linear_algebra.contraction
 
 ## Main definitions
 
-* `bilin_form.tmul Q₁ Q₂`: the bilinear form constructed elementwise on a tensor product
+* `bilin_form.tensor_distrib (B₁ ⊗ₜ B₂)`: the bilinear form on `M₁ ⊗ₜ M₂`.
+* `bilin_form.tensor_distrib_equiv`: `bilin_form.tensor_distrib` as an equivalence on finite free
+  modules.
 
 -/
 
@@ -28,24 +30,25 @@ variables [comm_semiring R]
 variables [add_comm_monoid M₁] [add_comm_monoid M₂]
 variables [module R M₁] [module R M₂]
 
-/-- The tensor product of two bilinear forms. -/
+/-- The tensor product of two bilinear forms injects into bilinear forms on tensor products. -/
 def tensor_distrib : bilin_form R M₁ ⊗[R] bilin_form R M₂ →ₗ[R] bilin_form R (M₁ ⊗[R] M₂) :=
-linear_map.to_bilin.to_linear_map
-  ∘ₗ tensor_product.lcurry R _ _ _
-  ∘ₗ (tensor_product.tensor_tensor_tensor_comm R _ _ _ _).to_linear_map.dual_map
+((tensor_product.tensor_tensor_tensor_comm R _ _ _ _).dual_map
+  ≪≫ₗ (tensor_product.lift.equiv R _ _ _).symm
+  ≪≫ₗ linear_map.to_bilin).to_linear_map
   ∘ₗ tensor_product.dual_distrib R _ _
-  ∘ₗ tensor_product.map
-    (tensor_product.uncurry R _ _ _ ∘ₗ bilin_form.to_lin.to_linear_map)
-    (tensor_product.uncurry R _ _ _ ∘ₗ bilin_form.to_lin.to_linear_map)
+  ∘ₗ (tensor_product.congr
+    (bilin_form.to_lin ≪≫ₗ tensor_product.lift.equiv R _ _ _)
+    (bilin_form.to_lin ≪≫ₗ tensor_product.lift.equiv R _ _ _)).to_linear_map
 
-/-- The tensor product of two bilinear forms. -/
+@[simp] lemma tensor_distrib_tmul (B₁ : bilin_form R M₁) (B₂ : bilin_form R M₂)
+  (m₁ : M₁) (m₂ : M₂) (m₁' : M₁) (m₂' : M₂) :
+  tensor_distrib (B₁ ⊗ₜ B₂) (m₁ ⊗ₜ m₂) (m₁' ⊗ₜ m₂') = B₁ m₁ m₁' * B₂ m₂ m₂' :=
+rfl
+
+/-- The tensor product of two bilinear forms, a shorthand for dot notation. -/
+@[reducible]
 protected def tmul (B₁ : bilin_form R M₁) (B₂ : bilin_form R M₂) : bilin_form R (M₁ ⊗[R] M₂) :=
 tensor_distrib (B₁ ⊗ₜ[R] B₂)
-
-@[simp] lemma tmul_tmul (B₁ : bilin_form R M₁) (B₂ : bilin_form R M₂)
-  (m₁ m₁' : M₁) (m₂ m₂' : M₂) :
-  B₁.tmul B₂ (m₁ ⊗ₜ m₂) (m₁' ⊗ₜ m₂') = B₁ m₁ m₁' * B₂ m₂ m₂' :=
-rfl
 
 end comm_semiring
 
@@ -57,36 +60,21 @@ variables [module.free R M₁] [module.finite R M₁]
 variables [module.free R M₂] [module.finite R M₂]
 variables [nontrivial R]
 
-noncomputable def tmul_equiv :
+/-- `tensor_distrib` as an equivalence. -/
+noncomputable def tensor_distrib_equiv :
   bilin_form R M₁ ⊗[R] bilin_form R M₂ ≃ₗ[R] bilin_form R (M₁ ⊗[R] M₂) :=
-begin
-  -- convert to bilinear maps
-  refine (tensor_product.congr linear_map.to_bilin linear_map.to_bilin).symm
-    ≪≫ₗ _ ≪≫ₗ linear_map.to_bilin,
-  -- convert to tnsor products
-  refine (tensor_product.congr (tensor_product.lift.equiv R M₁ M₁ R)
-                               (tensor_product.lift.equiv R M₂ M₂ R))
-    ≪≫ₗ _ ≪≫ₗ (tensor_product.lift.equiv R (M₁ ⊗[R] M₂) (M₁ ⊗[R] M₂) R).symm,
-  -- reassociate
-  refine _ ≪≫ₗ
-    (tensor_product.tensor_tensor_tensor_comm R _ _ _ _).arrow_congr (tensor_product.lid R R),
-  exact hom_tensor_hom_equiv R _ _ R R
-end
+-- the same `linear_equiv`s as from `tensor_distrib`, but with the inner linear map also as an
+-- equiv
+tensor_product.congr
+    (bilin_form.to_lin ≪≫ₗ tensor_product.lift.equiv R _ _ _)
+    (bilin_form.to_lin ≪≫ₗ tensor_product.lift.equiv R _ _ _)
+  ≪≫ₗ tensor_product.dual_distrib_equiv R (M₁ ⊗ M₁) (M₂ ⊗ M₂)
+  ≪≫ₗ (tensor_product.tensor_tensor_tensor_comm R _ _ _ _).dual_map
+  ≪≫ₗ (tensor_product.lift.equiv R _ _ _).symm
+  ≪≫ₗ linear_map.to_bilin
 
-lemma tmul_equiv_tmul (B₁ : bilin_form R M₁) (B₂ : bilin_form R M₂) :
-  tmul_equiv (B₁ ⊗ₜ[R] B₂) = B₁.tmul B₂ :=
-begin
-  -- the definition of `hom_tensor_hom_equiv` means this can't just be rfl :(
-  dsimp [tmul_equiv, bilin_form.tmul, tensor_product.lift.equiv_apply,
-    linear_equiv.arrow_congr_apply],
-  refine congr_arg _ _,
-  refine congr_arg _ _,
-  apply tensor_product.ext_fourfold',
-  dsimp [tensor_product.lift.equiv_apply,
-    tensor_product.tensor_tensor_tensor_comm_symm],
-  rw hom_tensor_hom_equiv_apply _,
-  sorry
-end
+lemma tensor_distrib_equiv_tmul (B : bilin_form R M₁ ⊗ bilin_form R M₂) :
+  tensor_distrib_equiv B = tensor_distrib B := rfl
 
 end comm_ring
 

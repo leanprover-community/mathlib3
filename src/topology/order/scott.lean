9a412f7ae7852165952f6999b86035ade38383fb
Christopher Hoskin <christopher.hoskin@gmail.com>
2023-01-16 06:56:14 +0000

    complete lattice

--- src/topology/order/scott.lean
+++ src/topology/order/scott.lean
@@ -109,75 +109,6 @@ variable [preorder α]
 
 instance : preorder (with_scott_topology α) := ‹preorder α›
 
-/-- A subset `u` of a pre-order is Scott open if, for every (non-empty) directed set `d` with least
-upper bound `a`, if `a` lies in `u` then `u` and `d` intersect.
--/
-def is_scott_open (u : set α) : Prop := is_upper_set u ∧
-  ∀ (d : set α) (a : α), d.nonempty → directed_on (≤) d → is_lub d a → a ∈ u → d∩u ≠ ∅
-
-lemma is_open_univ : is_scott_open (univ : set α) :=
-begin
-  rw is_scott_open,
-  split,
-  { exact is_upper_set_univ, },
-  { intros d a hd₁ hd₂ hd₃ ha,
-    rw inter_univ,
-    exact nonempty.ne_empty hd₁ }
-end
-
-lemma is_open.inter (s t : set α) : is_scott_open s → is_scott_open t → is_scott_open (s ∩ t) :=
-begin
-  intros hs ht,
-  rw is_scott_open at *,
-  split,
-  { exact is_upper_set.inter hs.1 ht.1, },
-  { intros d a hd₁ hd₂ hd₃ ha,
-    have hs₁: d ∩ s ≠ ∅ := hs.2 d a hd₁ hd₂ hd₃ ha.1,
-    have ht₁: d ∩ t ≠ ∅ := ht.2 d a hd₁ hd₂ hd₃ ha.2,
-    rw [← nonempty_iff_ne_empty, inter_nonempty] at hs₁,
-    cases hs₁ with x hx,
-    rw [← nonempty_iff_ne_empty, inter_nonempty] at ht₁,
-    cases ht₁ with y hy,
-    rw directed_on at hd₂,
-    cases hd₂ x hx.1 y hy.1 with z hz,
-    cases hz,
-    rw [← nonempty_iff_ne_empty, inter_nonempty],
-    use z,
-    split,
-    { exact hz_w, },
-    { rw mem_inter_iff,
-      split,
-      { exact hs.1 hz_h.1 hx.2, },
-      { exact ht.1 hz_h.2 hy.2, } } }
-end
-
-lemma is_open_sUnion (s : set (set α)) (hs : ∀t∈s, is_scott_open t) : is_scott_open (⋃₀ s) :=
-begin
-  rw is_scott_open at *,
-  split,
-  { apply is_upper_set_sUnion,
-    intros s hs₁,
-    apply (hs s hs₁).1, },
-  { intros d a hd₁ hd₂ hd₃ ha,
-  cases ha with s₁,
-  cases ha_h,
-  have d1: d ∩ s₁ ≠ ∅ := begin
-   apply (hs s₁ ha_h_w).2,
-   exact hd₁,
-   exact hd₂,
-   exact hd₃,
-   exact ha_h_h,
-  end,
-  rw ← nonempty_iff_ne_empty,
-  rw ← nonempty_iff_ne_empty at d1,
-  cases d1 with x,
-  use x,
-  rw mem_inter_iff,
-  split,
-  { exact d1_h.1, },
-  { rw mem_sUnion, use s₁, split, exact ha_h_w, exact d1_h.2, } }
-end
-
 /--
 The set of upper sets forms a topology
 -/
@@ -249,6 +180,28 @@ end preorder
 
 section complete_lattice
 
+lemma complete_scott_open [complete_lattice α] (u : set (with_scott_topology α)) : is_open u =
+(is_upper_set u ∧
+  ∀ (d : set α), d.nonempty → directed_on (≤) d → Sup d ∈ u → ∃ b ∈ d, (Ici b) ∩ d ⊆ u) :=
+begin
+  rw scott_is_open,
+  refine eq_iff_iff.mpr _,
+  split,
+  { intro h,
+    split,
+    { exact h.1, },
+    { intros d hd₁ hd₂ hd₃,
+      exact h.2 d (Sup d) hd₁ hd₂ (is_lub_Sup d) hd₃, } },
+  { intro h,
+    split,
+    { exact h.1, },
+    { intros d a hd₁ hd₂ hd₃ ha,
+      apply h.2 d hd₁ hd₂,
+      { rw (is_lub.Sup_eq hd₃), exact ha, } } }
+  --refine let_value_eq (and (is_upper_set u)) _,
+end
+
+/-
 lemma complete_scott_open [complete_lattice α] :
   is_scott_open = λ u, is_upper_set u ∧
     ∀ (d : set α), d.nonempty → directed_on (≤) d → Sup d ∈ u → d∩u ≠ ∅ :=
@@ -268,6 +221,7 @@ begin
       apply h.2 d hd₁ hd₂,
       { rw (is_lub.Sup_eq hd₃), exact ha, } } }
 end
+-/
 
 end complete_lattice
 

7c135730b4cf58837efa7b72c8a1f74829a15a40
Heather Macbeth <25316162+hrmacbeth@users.noreply.github.com>
2023-01-18 10:47:23 -0500

    algebra.star.chsh

--- src/algebra/star/chsh.lean
+++ src/algebra/star/chsh.lean
@@ -3,7 +3,7 @@ Copyright (c) 2020 Scott Morrison. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Scott Morrison
 -/
-import data.is_R_or_C.basic
+import data.is_R_or_C.lemmas
 
 /-!
 # The Clauser-Horne-Shimony-Holt inequality and Tsirelson's inequality.

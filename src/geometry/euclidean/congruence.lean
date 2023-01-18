56613daf5696b69c2f27863f0c35e56a122e7c41
JovanGerb <jovan.gerbscheid@gmail.com>
2023-01-18 15:18:24 +0000

    fix copyright typo

--- src/geometry/euclidean/congruence.lean
+++ src/geometry/euclidean/congruence.lean
@@ -1,7 +1,7 @@
 /-
 Copyright (c) 2023 . All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
-Author: Jovan Gerbscheid
+Authors: Jovan Gerbscheid
 -/
 
 import topology.metric_space.isometry

efdb38976b5e1037386570d6e1f8b6dfca3c19fe
Paul-Lez <72892199+Paul-Lez@users.noreply.github.com>
2023-01-19 22:37:45 +0000

    Extra results

--- src/ring_theory/adjoin_root.lean
+++ src/ring_theory/adjoin_root.lean
@@ -168,6 +168,18 @@ by rw [is_root, eval_map, eval₂_root]
 lemma is_algebraic_root (hf : f ≠ 0) : is_algebraic R (root f) :=
 ⟨f, hf, eval₂_root f⟩
 
+lemma adjoin_root.mk_injective_of_degree_ne_zero [is_domain R] (hf : 0 < f.degree) :
+  function.injective (adjoin_root.of f) :=
+begin
+  rw ring_hom.injective_iff_ker_eq_bot,
+  refine le_antisymm (λ p hp, _) bot_le,
+  rw [ring_hom.mem_ker, adjoin_root.of, ring_hom.comp_apply, adjoin_root.mk_eq_zero] at hp,
+  by_contra h_contra,
+  refine not_le.mpr hf _,
+  replace hp := polynomial.degree_le_of_dvd hp (by rwa [ne.def, C_eq_zero, ← ideal.mem_bot]),
+  rwa degree_C h_contra at hp,
+end
+
 variables [comm_ring S]
 
 /-- Lift a ring homomorphism `i : R →+* S` to `adjoin_root f →+* S`. -/
@@ -243,6 +255,19 @@ lemma alg_hom_subsingleton {S : Type*} [comm_ring S] [algebra R S] {r : R} :
   (by rw [← g.commutes, ← g.map_mul, algebra_map_eq, root_is_inv, map_one]))⟩
 
 end adjoin_inv
+
+section prime
+
+instance [hf : fact (prime f)] : is_domain (adjoin_root f) :=
+(ideal.quotient.is_domain_iff_prime (span {f} : ideal R[X])).mpr $
+  (ideal.span_singleton_prime (fact_iff.mp hf).ne_zero).mpr (fact_iff.mp hf)
+
+instance [is_domain R] [hf : fact (prime f)] [hf' : fact (0 < f.degree)] :
+  no_zero_smul_divisors R (adjoin_root f) :=
+no_zero_smul_divisors.iff_algebra_map_injective.mpr
+  (adjoin_root.mk_injective_of_degree_ne_zero (fact_iff.mp hf'))
+
+end prime
 
 end comm_ring
 

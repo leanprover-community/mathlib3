a9892e18f5ccef75550eeb5d3b34c6ad74e77580
Monica Omar <2497250a@research.gla.ac.uk>
2023-01-19 17:39:48 +0000

    fix

--- src/linear_algebra/invariant_submodule.lean
+++ src/linear_algebra/invariant_submodule.lean
@@ -11,9 +11,9 @@ import linear_algebra.basic
 In this file, we define and prove some basic results on invaraint submodules.
 -/
 
-
 /-- `U` is `T` invariant: `∀ u : V`, if `u ∈ U` then `T u ∈ U`-/
-def submodule.invariant {V K : Type*} [semiring K] [add_comm_monoid V] [module K V] (U : submodule K V) (T : V →ₗ[K] V) : Prop := U ≤ U.comap T
-lemma submodule.invariant_iff {V K : Type*} [semiring K] [add_comm_monoid V] [module K V] (U : submodule K V) (T : V →ₗ[K] V) :
-  submodule.invariant U T ↔ T '' U ⊆ U :=
+def submodule.invariant {V K : Type*} [semiring K] [add_comm_monoid V] [module K V]
+  (U : submodule K V) (T : V →ₗ[K] V) : Prop := U ≤ U.comap T
+lemma submodule.invariant_iff {V K : Type*} [semiring K] [add_comm_monoid V] [module K V]
+  (U : submodule K V) (T : V →ₗ[K] V) : submodule.invariant U T ↔ T '' U ⊆ U :=
 by simp only [set.image_subset_iff]; refl

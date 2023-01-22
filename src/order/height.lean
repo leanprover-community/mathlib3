c3ca834abf86207055fce6a2b4530172c82fb4f9
Junyan Xu <junyanxumath@gmail.com>
2023-01-22 01:14:58 -0500

    formatting

--- src/order/height.lean
+++ src/order/height.lean
@@ -318,8 +318,7 @@ pairwise_iff_nth_le.2 $ λ i j hj hij, by { simp_rw nth_le_of_fn', apply hf, exa
 lemma _root_.rel_hom.pairwise_of_fn {α} {R : α → α → Prop} {n} (f : @has_lt.lt (fin n) _ →r R) :
   (of_fn f).pairwise R := list.pairwise_of_fn _ (λ i j, f.map_rel)
 
-lemma well_founded_gt_of_chain_height_ne_top
-  (s : set α) (hs : s.chain_height ≠ ⊤) :
+lemma well_founded_gt_of_chain_height_ne_top (s : set α) (hs : s.chain_height ≠ ⊤) :
   well_founded_gt s :=
 ⟨rel_embedding.well_founded_iff_no_descending_seq.2 ⟨λ f, begin
   obtain ⟨n, hn⟩ := with_top.ne_top_iff_exists.1 hs,

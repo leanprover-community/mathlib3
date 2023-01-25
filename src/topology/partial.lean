cbb0b0d4f7addd023282618dba1e0e3a33c8ce65
Yury G. Kudryashov <urkud@urkud.name>
2023-01-24 19:22:02 -0600

    Date

--- src/topology/partial.lean
+++ src/topology/partial.lean
@@ -1,5 +1,5 @@
 /-
-Copyright (c) 2023 Jeremy Avigad. All rights reserved.
+Copyright (c) 2018 Jeremy Avigad. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Jeremy Avigad
 -/

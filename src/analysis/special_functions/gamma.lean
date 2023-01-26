0ee3e6f52678a420c58b1072870fed5e0240c083
David Loeffler <d.loeffler.01@cantab.net>
2023-01-26 08:02:55 +0000

    feat(analysis/special_functions/gamma): monotonicity of the Gamma function (#18246)
    Show (using the log-convexity results proved in #18188) that Gamma is strictly increasing for `2 ≤ x`.

--- src/analysis/special_functions/gamma.lean
+++ src/analysis/special_functions/gamma.lean
@@ -52,7 +52,7 @@ Gamma
 
 noncomputable theory
 open filter interval_integral set real measure_theory asymptotics
-open_locale nat topological_space ennreal big_operators
+open_locale nat topological_space ennreal big_operators complex_conjugate
 
 lemma integral_exp_neg_Ioi : ∫ (x : ℝ) in Ioi 0, exp (-x) = 1 :=
 begin
@@ -131,6 +131,16 @@ end
 See `complex.Gamma_integral_convergent` for a proof of the convergence of the integral for
 `0 < re s`. -/
 def Gamma_integral (s : ℂ) : ℂ := ∫ x in Ioi (0:ℝ), ↑(-x).exp * ↑x ^ (s - 1)
+
+lemma Gamma_integral_conj (s : ℂ) : Gamma_integral (conj s) = conj (Gamma_integral s) :=
+begin
+  rw [Gamma_integral, Gamma_integral, ←integral_conj],
+  refine set_integral_congr measurable_set_Ioi (λ x hx, _),
+  dsimp only,
+  rw [ring_hom.map_mul, conj_of_real, cpow_def_of_ne_zero (of_real_ne_zero.mpr (ne_of_gt hx)),
+    cpow_def_of_ne_zero (of_real_ne_zero.mpr (ne_of_gt hx)), ←exp_conj, ring_hom.map_mul,
+    ←of_real_log (le_of_lt hx), conj_of_real, ring_hom.map_sub, ring_hom.map_one],
+end
 
 lemma Gamma_integral_of_real (s : ℝ) :
   Gamma_integral ↑s = ↑(∫ x:ℝ in Ioi 0, real.exp (-x) * x ^ (s - 1)) :=
@@ -307,7 +317,6 @@ begin
     rw this },
 end
 
-
 /-- The `Γ` function (of a complex variable `s`). -/
 @[pp_nodot] def Gamma (s : ℂ) : ℂ := Gamma_aux ⌊1 - s.re⌋₊ s
 
@@ -354,6 +363,20 @@ begin
   { simpa using Gamma_one },
   { rw (Gamma_add_one n.succ $ nat.cast_ne_zero.mpr $ nat.succ_ne_zero n),
     simp only [nat.cast_succ, nat.factorial_succ, nat.cast_mul], congr, exact hn },
+end
+
+lemma Gamma_conj (s : ℂ) : Gamma (conj s) = conj (Gamma s) :=
+begin
+  suffices : ∀ (n:ℕ) (s:ℂ) , Gamma_aux n (conj s) = conj (Gamma_aux n s), from this _ _,
+  intro n,
+  induction n with n IH,
+  { rw Gamma_aux, exact Gamma_integral_conj, },
+  { intro s,
+    rw Gamma_aux,
+    dsimp only,
+    rw [div_eq_mul_inv _ s, ring_hom.map_mul, conj_inv, ←div_eq_mul_inv],
+    suffices : conj s + 1 = conj (s + 1), by rw [this, IH],
+    rw [ring_hom.map_add, ring_hom.map_one] }
 end
 
 end Gamma_def
@@ -566,6 +589,9 @@ end
 lemma Gamma_one : Gamma 1 = 1 :=
 by rw [Gamma, complex.of_real_one, complex.Gamma_one, complex.one_re]
 
+lemma _root_.complex.Gamma_of_real (s : ℝ) : complex.Gamma (s : ℂ) = Gamma s :=
+by rw [Gamma, eq_comm, ←complex.eq_conj_iff_re, ←complex.Gamma_conj, complex.conj_of_real]
+
 theorem Gamma_nat_eq_factorial (n : ℕ) : Gamma (n + 1) = n! :=
 by rw [Gamma, complex.of_real_add, complex.of_real_nat_cast, complex.of_real_one,
   complex.Gamma_nat_eq_factorial, ←complex.of_real_nat_cast, complex.of_real_re]
@@ -695,6 +721,21 @@ begin
       (mul_pos
         (rpow_pos_of_pos (Gamma_pos_of_pos hx) _) (rpow_pos_of_pos (Gamma_pos_of_pos hy) _))],
   exact Gamma_mul_add_mul_le_rpow_Gamma_mul_rpow_Gamma hx hy ha hb hab,
+end
+
+lemma convex_on_Gamma : convex_on ℝ (Ioi 0) Gamma :=
+begin
+  refine ⟨convex_Ioi 0, λ x hx y hy a b ha hb hab, _⟩,
+  have := convex_on.comp (convex_on_exp.subset (subset_univ _) _) convex_on_log_Gamma
+    (λ u hu v hv huv, exp_le_exp.mpr huv),
+  convert this.2 hx hy ha hb hab,
+  { rw [function.comp_app, exp_log (Gamma_pos_of_pos $ this.1 hx hy ha hb hab)] },
+  { rw [function.comp_app, exp_log (Gamma_pos_of_pos hx)] },
+  { rw [function.comp_app, exp_log (Gamma_pos_of_pos hy)] },
+  { rw convex_iff_is_preconnected,
+    refine is_preconnected_Ioi.image _ (λ x hx, continuous_at.continuous_within_at _),
+    refine (differentiable_at_Gamma (λ m, _)).continuous_at.log (Gamma_pos_of_pos hx).ne',
+    exact (add_pos_of_pos_of_nonneg hx (nat.cast_nonneg m)).ne' },
 end
 
 section bohr_mollerup
@@ -927,5 +968,42 @@ begin
 end
 
 end bohr_mollerup
+
+section strict_mono
+
+lemma Gamma_two : Gamma 2 = 1 := by simpa using Gamma_nat_eq_factorial 1
+
+lemma Gamma_three_div_two_lt_one : Gamma (3 / 2) < 1 :=
+begin
+  -- This can also be proved using the closed-form evaluation of `Gamma (1 / 2)` in
+  -- `analysis.special_functions.gaussian`, but we give a self-contained proof using log-convexity
+  -- to avoid unnecessary imports.
+  have A : (0:ℝ) < 3/2, by norm_num,
+  have := bohr_mollerup.f_add_nat_le convex_on_log_Gamma (λ y hy, _) two_ne_zero one_half_pos
+    (by norm_num : 1/2 ≤ (1:ℝ)),
+  swap, { rw [function.comp_app, Gamma_add_one hy.ne', log_mul hy.ne' (Gamma_pos_of_pos hy).ne',
+    add_comm] },
+  rw [function.comp_app, function.comp_app, nat.cast_two, Gamma_two, log_one, zero_add,
+    (by norm_num : (2:ℝ) + 1/2 = 3/2 + 1), Gamma_add_one A.ne',
+    log_mul A.ne' (Gamma_pos_of_pos A).ne', ←le_sub_iff_add_le',
+    log_le_iff_le_exp (Gamma_pos_of_pos A)] at this,
+  refine this.trans_lt (exp_lt_one_iff.mpr _),
+  rw [mul_comm, ←mul_div_assoc, div_sub' _ _ (2:ℝ) two_ne_zero],
+  refine div_neg_of_neg_of_pos _ two_pos,
+  rw [sub_neg, mul_one, ←nat.cast_two, ←log_pow, ←exp_lt_exp, nat.cast_two, exp_log two_pos,
+    exp_log];
+  norm_num,
+end
+
+lemma Gamma_strict_mono_on_Ici : strict_mono_on Gamma (Ici 2) :=
+begin
+  convert convex_on_Gamma.strict_mono_of_lt (by norm_num : (0:ℝ) < 3/2)
+    (by norm_num : (3/2 : ℝ) < 2) (Gamma_two.symm ▸ Gamma_three_div_two_lt_one),
+  symmetry,
+  rw inter_eq_right_iff_subset,
+  exact λ x hx, two_pos.trans_le hx,
+end
+
+end strict_mono
 
 end real

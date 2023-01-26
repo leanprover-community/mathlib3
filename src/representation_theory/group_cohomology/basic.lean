4a773b91d4cc73d79202bf9a3017c83374fbeb08
101damnations <101damnations@github.com>
2023-01-26 11:57:33 +0000

    add import

--- src/representation_theory/group_cohomology/basic.lean
+++ src/representation_theory/group_cohomology/basic.lean
@@ -4,6 +4,7 @@ Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Amelia Livingston
 -/
 
+import algebra.homology.opposite
 import representation_theory.group_cohomology.resolution
 
 /-!

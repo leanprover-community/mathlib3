0c6232f9062fbd3e75e59f73f8d5af47883b863e
Eric Wieser <wieser.eric@gmail.com>
2023-02-01 17:26:36 +0000

    remove duplicates

--- src/topology/algebra/infinite_sum.lean
+++ src/topology/algebra/infinite_sum.lean
@@ -1065,26 +1065,6 @@ end tsum
 
 end topological_semiring
 
-section topological_division_ring
-variables [division_ring α] [topological_space α] [topological_division_ring α]
-variables {f g : β → α} {a a₁ a₂ : α}
-
-lemma has_sum.div_right (a₂) (hf : has_sum f a₁) : has_sum (λb, f b / a₂) (a₁ / a₂) :=
-by simpa only [div_eq_mul_inv] using hf.mul_right _
-
-lemma summable.div_right (a) (hf : summable f) : summable (λb, f b / a) :=
-(hf.has_sum.div_right _).summable
-
-section tsum
-variables [t2_space α]
-
-lemma summable.tsum_div_right (a) (hf : summable f) : (∑'b, f b / a) = (∑'b, f b) / a :=
-(hf.has_sum.div_right _).tsum_eq
-
-end tsum
-
-end topological_division_ring
-
 section const_smul
 variables {R : Type*}
 [monoid R]
@@ -1138,11 +1118,17 @@ lemma has_sum_mul_left_iff (h : a₂ ≠ 0) : has_sum f a₁ ↔ has_sum (λb, a
 lemma has_sum_mul_right_iff (h : a₂ ≠ 0) : has_sum f a₁ ↔ has_sum (λb, f b * a₂) (a₁ * a₂) :=
 ⟨has_sum.mul_right _, λ H, by simpa only [mul_inv_cancel_right₀ h] using H.mul_right a₂⁻¹⟩
 
+lemma has_sum_div_const_iff (h : a₂ ≠ 0) : has_sum f a₁ ↔ has_sum (λb, f b / a₂) (a₁ / a₂) :=
+by simpa only [div_eq_mul_inv] using has_sum_mul_right_iff (inv_ne_zero h)
+
 lemma summable_mul_left_iff (h : a ≠ 0) : summable f ↔ summable (λb, a * f b) :=
 ⟨λ H, H.mul_left _, λ H, by simpa only [inv_mul_cancel_left₀ h] using H.mul_left a⁻¹⟩
 
 lemma summable_mul_right_iff (h : a ≠ 0) : summable f ↔ summable (λb, f b * a) :=
 ⟨λ H, H.mul_right _, λ H, by simpa only [mul_inv_cancel_right₀ h] using H.mul_right a⁻¹⟩
+
+lemma summable_div_const_iff (h : a ≠ 0) : summable f ↔ summable (λb, f b / a) :=
+by simpa only [div_eq_mul_inv] using summable_mul_right_iff (inv_ne_zero h)
 
 lemma tsum_mul_left [t2_space α] : (∑' x, a * f x) = a * ∑' x, f x :=
 if hf : summable f then hf.tsum_mul_left a
@@ -1155,6 +1141,9 @@ if hf : summable f then hf.tsum_mul_right a
 else if ha : a = 0 then by simp [ha]
 else by rw [tsum_eq_zero_of_not_summable hf,
   tsum_eq_zero_of_not_summable (mt (summable_mul_right_iff ha).2 hf), zero_mul]
+
+lemma tsum_div_const [t2_space α] : (∑' x, f x / a) = (∑' x, f x) / a :=
+by simpa only [div_eq_mul_inv] using tsum_mul_right
 
 end division_ring
 

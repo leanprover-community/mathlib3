8e846e7fc7cf04379b14cc1739c9fb7385bcff24
Rémi Bottinelli <bottine@users.noreply.github.com>
2023-02-02 08:25:27 +0000

    Update src/combinatorics/simple_graph/ends/defs.lean
    Co-authored-by: Yaël Dillies <yael.dillies@gmail.com>

--- src/combinatorics/simple_graph/ends/defs.lean
+++ src/combinatorics/simple_graph/ends/defs.lean
@@ -35,7 +35,7 @@ connected_component_mk (G.induce Kᶜ) ⟨v, vK⟩
 def comp_out.supp (C : G.comp_out K) : set V :=
 {v : V | ∃ h : v ∉ K, G.comp_out_mk h = C}
 
-lemma comp_out.supp_injective : function.injective (comp_out.supp : G.comp_out K → set V) :=
+@[ext] lemma comp_out.supp_injective : function.injective (comp_out.supp : G.comp_out K → set V) :=
 begin
   refine connected_component.ind₂ _,
   rintros ⟨v, hv⟩ ⟨w, hw⟩ h,

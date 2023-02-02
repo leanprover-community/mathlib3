a835e4a146ab87eec6df254d64e49f6c449298f8
Ruben Van de Velde <65514131+Ruben-VandeVelde@users.noreply.github.com>
2023-02-02 21:43:49 +0100

    chore(analysis/inner_product_space/basic): remove redundant import

--- src/analysis/inner_product_space/basic.lean
+++ src/analysis/inner_product_space/basic.lean
@@ -3,7 +3,6 @@ Copyright (c) 2019 Zhouhang Zhou. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Zhouhang Zhou, Sébastien Gouëzel, Frédéric Dupuis
 -/
-import algebra.direct_sum.module
 import analysis.complex.basic
 import analysis.convex.uniform
 import analysis.normed_space.completion

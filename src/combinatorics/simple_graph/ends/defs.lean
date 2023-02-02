06da513f1fd78a6a3c2f76cd162ffeab3dccf50b
Rémi Bottinelli <bottine@users.noreply.github.com>
2023-02-02 08:25:10 +0000

    Update src/combinatorics/simple_graph/ends/defs.lean
    Co-authored-by: Yaël Dillies <yael.dillies@gmail.com>

--- src/combinatorics/simple_graph/ends/defs.lean
+++ src/combinatorics/simple_graph/ends/defs.lean
@@ -43,7 +43,7 @@ begin
   exact ((h v).mp ⟨hv, reachable.refl _⟩).some_spec,
 end
 
-@[ext] lemma comp_out.supp_inj {C D : G.comp_out K} : C.supp = D.supp ↔ C = D :=
+lemma comp_out.supp_inj {C D : G.comp_out K} : C.supp = D.supp ↔ C = D :=
 comp_out.supp_injective.eq_iff
 
 instance : set_like (G.comp_out K) V :=

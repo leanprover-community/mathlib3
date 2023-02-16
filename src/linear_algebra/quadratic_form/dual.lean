356b0ce039615cd5b2ade3bc44dc1fdd14672f2a
Eric Wieser <wieser.eric@gmail.com>
2023-02-16 02:41:40 +0000

    one more

--- src/linear_algebra/quadratic_form/dual.lean
+++ src/linear_algebra/quadratic_form/dual.lean
@@ -1,9 +1,10 @@
-import linear_algebra.quadratic_form.basic
+import linear_algebra.quadratic_form.isometry
+import linear_algebra.quadratic_form.prod
 .
 
 universes u v w
 variables {S : Type*}
-variables {R : Type*} {M : Type*}
+variables {R : Type*} {M N : Type*}
 
 variables (R M)
 
@@ -88,14 +89,87 @@ end ring
 
 
 end bilin_form
-variables [comm_semiring R] [add_comm_monoid M] [module R M]
+
+namespace quadratic_form
+
+section semiring
+variables [comm_semiring R] [add_comm_monoid M] [add_comm_monoid N] [module R M] [module R N]
 
 /-- The quadratic form defined as `Q (f, x) = f x`. -/
-def quadratic_form.dual_prod : quadratic_form R (module.dual R M × M) :=
+@[simps]
+def dual_prod : quadratic_form R (module.dual R M × M) :=
 { to_fun := λ p, p.1 p.2,
-  to_fun_smul := λ a p, by rw [prod.smul_fst, prod.smul_snd, linear_map.smul_apply, map_smul,
-                               smul_eq_mul, smul_eq_mul, mul_assoc],
+  to_fun_smul := λ a p, by rw [prod.smul_fst, prod.smul_snd, linear_map.smul_apply,
+                               linear_map.map_smul, smul_eq_mul, smul_eq_mul, mul_assoc],
   exists_companion' := ⟨bilin_form.dual_prod R M, λ p q, begin
     rw [bilin_form.dual_prod_apply, prod.fst_add, prod.snd_add, linear_map.add_apply, map_add,
       map_add, add_right_comm _ (q.1 q.2), add_comm (q.1 p.2) (p.1 q.2), ←add_assoc, ←add_assoc],
   end⟩ }
+
+variables {R M N}
+
+/-- Any module isomorphisms induces a quadratic isomorphism between the corresponding `dual_prod.`
+-/
+def dual_prod_isometry (f : M ≃ₗ[R] N) :
+  (dual_prod R M).isometry (dual_prod R N) :=
+{ to_linear_equiv :=  f.dual_map.symm.prod f,
+  map_app' := λ x, fun_like.congr_arg x.fst $ f.symm_apply_apply _ }
+
+/-- Four-way commutativity of `prod`.
+
+The name matches `mul_mul_mul_comm`. -/
+@[simps]
+def equiv.prod_prod_prod_comm (α β γ δ) : (α × β) × (γ × δ) ≃ (α × γ) × (β × δ) :=
+{ to_fun := λ abcd, ((abcd.1.1, abcd.2.1), (abcd.1.2, abcd.2.2)),
+  inv_fun := λ acbd, ((acbd.1.1, acbd.2.1), (acbd.1.2, acbd.2.2)),
+  left_inv := λ ⟨⟨a, b⟩, ⟨c, d⟩⟩, rfl,
+  right_inv := λ ⟨⟨a, c⟩, ⟨b, d⟩⟩, rfl, }
+
+/-- `equiv.prod_prod_prod_comm` as an isomorphism of modules. -/
+@[simps]
+def linear_equiv.prod_prod_prod_comm (α β γ δ)
+  [add_comm_monoid α] [add_comm_monoid β] [add_comm_monoid γ] [add_comm_monoid δ]
+  [module R α] [module R β] [module R γ] [module R δ] :
+  ((α × β) × (γ × δ)) ≃ₗ[R] ((α × γ) × (β × δ)) :=
+{ map_add' := λ x y, rfl,
+  map_smul' := λ c x, rfl,
+  ..equiv.prod_prod_prod_comm _ _ _ _ }
+
+/-- `quadratic_form.dual_prod` commutes with `quadratic_form.prod`. -/
+@[simps]
+def dual_prod_prod_isometry :
+  (dual_prod R (M × N)).isometry ((dual_prod R M).prod (dual_prod R N)) :=
+{ to_linear_equiv :=
+  ((module.dual_prod_dual_equiv_dual R M N).symm.prod (linear_equiv.refl R (M × N)))
+    ≪≫ₗ linear_equiv.prod_prod_prod_comm _ _ M N,
+  map_app' := λ m,
+    (m.fst.map_add _ _).symm.trans $ fun_like.congr_arg m.fst $ prod.ext (add_zero _) (zero_add _) }
+
+end semiring
+
+section ring
+variables [comm_ring R] [add_comm_group M] [module R M]
+
+variables {R M}
+-- page 84 of "Hermitian K-Theory and Geometric Applications"
+-- source neglects to mention characteristic two!
+@[simps]
+def foo (Q : quadratic_form R M) [invertible (2 : R)] :
+  M × M →ₗ[R] (module.dual R M × M) :=
+linear_map.prod
+  (Q.associated.to_lin.comp (linear_map.fst _ _ _)
+    + Q.associated.to_lin.comp (linear_map.snd _ _ _))
+  ((linear_map.fst _ _ _ - linear_map.snd _ _ _))
+
+lemma foo_app [invertible (2 : R)] (Q : quadratic_form R M) (x : M × M) :
+  quadratic_form.dual_prod R M (foo Q x) = (Q.prod $ -Q) x :=
+begin
+  dsimp only [foo, associated, associated_hom],
+  dsimp,
+  simp [polar_comm _ x.1 x.2, ←sub_add, mul_sub, sub_mul, smul_sub, submonoid.smul_def,
+    ←sub_eq_add_neg (Q x.1) (Q x.2)],
+end
+
+end ring
+
+end quadratic_form

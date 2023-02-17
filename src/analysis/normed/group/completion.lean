9bed7232093247b9618aaad74e099a5b5402e8c8
Eric Wieser <wieser.eric@gmail.com>
2023-02-17 21:54:59 +0000

    more

--- src/analysis/normed/group/completion.lean
+++ src/analysis/normed/group/completion.lean
@@ -28,11 +28,11 @@ instance [uniform_space E] [has_norm E] :
   has_norm (completion E) :=
 { norm := completion.extension has_norm.norm }
 
-@[simp] lemma norm_coe {E} [seminormed_add_comm_group E] (x : E) :
+@[simp] lemma norm_coe {E} [add_comm_group E] [seminormed_add_comm_group E] (x : E) :
   ‖(x : completion E)‖ = ‖x‖ :=
 completion.extension_coe uniform_continuous_norm x
 
-instance [seminormed_add_comm_group E] : normed_add_comm_group (completion E) :=
+instance [add_comm_group E] [seminormed_add_comm_group E] : normed_add_comm_group (completion E) :=
 { dist_eq :=
   begin
     intros x y,

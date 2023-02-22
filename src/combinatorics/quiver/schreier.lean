1f635744ab4d81ea71dd7f1a3f8f96d9b284af94
Rémi Bottinelli <remi.bottinelli@bluewin.ch>
2023-02-22 14:13:32 +0100

    wip

---
+++ src/combinatorics/quiver/schreier.lean
@@ -0,0 +1,61 @@
+import combinatorics.quiver.basic
+import combinatorics.quiver.single_obj
+import group_theory.group_action.basic
+import group_theory.group_action.group
+import combinatorics.quiver.covering
+
+universes u v w
+
+namespace quiver
+
+section basic
+
+def schreier_graph (V : Type*) {M : Type*} [has_smul M V] {S : Type*} (ι : S → M) := V
+
+@[simps] def equiv_schreier_graph {V : Type*} {M : Type*} [has_smul M V] {S : Type*} {ι : S → M} :
+  V ≃ schreier_graph V ι := equiv.refl V
+
+variables (V : Type*) {M : Type*} [has_smul M V] {S : Type*} (ι : S → M)
+
+instance : has_smul M (schreier_graph V ι) :=
+{ smul := λ x y, equiv_schreier_graph $ x • (equiv_schreier_graph.symm y)}
+
+instance schreier_graph.quiver : quiver (schreier_graph V ι) :=
+{ hom := λ x y, {s : S // (ι s) • x = y} }
+
+@[simps] def schreier_graph_covering : (schreier_graph V ι) ⥤q single_obj S :=
+{ obj := λ (x : schreier_graph V ι), single_obj.star S,
+  map := λ x y e, subtype.rec_on e (λ s h, s), }
+
+end basic
+
+section group_action
+
+variables (V : Type*) {M : Type*} [group M] [mul_action M V] {S : Type*} (ι : S → M)
+
+instance : mul_action M (schreier_graph V ι) :=
+{ smul := λ x y, equiv_schreier_graph $ x • (equiv_schreier_graph.symm y),
+  one_smul := mul_action.one_smul,
+  mul_smul := mul_action.mul_smul }
+
+lemma schreier_graph_covering_is_covering : (schreier_graph_covering V ι).is_covering :=
+begin
+  refine ⟨λ u, ⟨_, _⟩, λ u, ⟨_, _⟩⟩,
+  { rintro ⟨v,⟨x,hx⟩⟩ ⟨w,⟨y,hy⟩⟩ h,
+    simp only [prefunctor.star_apply, schreier_graph_covering_map, single_obj.to_hom_apply,
+               eq_iff_true_of_subsingleton, heq_iff_eq, true_and] at h,
+    subst_vars, },
+  { rintro ⟨⟨⟩,x⟩, exact ⟨⟨(ι x) • u, ⟨x, rfl⟩⟩, rfl⟩, },
+  { rintro ⟨v,⟨x,hx⟩⟩ ⟨w,⟨y,hy⟩⟩ h,
+    simp only [prefunctor.costar_apply, schreier_graph_covering_map, single_obj.to_hom_apply,
+               eq_iff_true_of_subsingleton, heq_iff_eq, true_and] at h,
+    subst_vars,
+    simp only [smul_left_cancel_iff] at hy,
+    subst hy, },
+  { rintro ⟨⟨⟩,x⟩,
+    exact ⟨⟨(ι x) ⁻¹ • u, ⟨x, by simp⟩⟩, by simp⟩, },
+end
+
+end group_action
+
+end quiver

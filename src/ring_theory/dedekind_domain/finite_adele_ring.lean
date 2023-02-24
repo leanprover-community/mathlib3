b5c121b4f8ada69f6f59128675fb91f7f6fdf4c1
Oliver Nash <github@olivernash.org>
2023-02-24 09:29:53 +0000

    Update src/ring_theory/dedekind_domain/finite_adele_ring.lean

--- src/ring_theory/dedekind_domain/finite_adele_ring.lean
+++ src/ring_theory/dedekind_domain/finite_adele_ring.lean
@@ -210,7 +210,7 @@ open prod_adic_completions.is_finite_adele
 
 variables (R K)
 /-- The finite adèle ring of `R` is the restricted product over all maximal ideals `v` of `R`
-of `adic_completion` with respect to `adic_completion_integers`.-/
+of `adic_completion` with respect to `adic_completion_integers`. -/
 noncomputable! def finite_adele_ring : subring (K_hat R K) :=
 { carrier   := { x : K_hat R K | x.is_finite_adele },
   mul_mem'  := λ _ _ hx hy, mul hx hy,

806f23a33d432e70228c776773b71c126fa02196
Aaron Anderson <65780815+awainverse@users.noreply.github.com>
2023-02-27 11:46:50 +0100

    Update src/model_theory/types.lean
    Co-authored-by: Yaël Dillies <yael.dillies@gmail.com>

--- src/model_theory/types.lean
+++ src/model_theory/types.lean
@@ -179,7 +179,7 @@ begin
 end
 
 lemma formula_mem_type_of {φ : L.formula α} :
-  (formula.equiv_sentence φ) ∈ T.type_of v ↔ φ.realize v :=
+  formula.equiv_sentence φ ∈ T.type_of v ↔ φ.realize v :=
 by simp
 
 /-- A complete type `p` is realized in a particular structure when there is some

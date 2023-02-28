543440333281830e0d25d1f450181179fdd7beae
Eric Wieser <wieser.eric@gmail.com>
2023-02-28 10:29:55 +0000

    Update src/data/set/semiring.lean
    Co-authored-by: Anne Baanen <Vierkantor@users.noreply.github.com>

--- src/data/set/semiring.lean
+++ src/data/set/semiring.lean
@@ -3,8 +3,8 @@ Copyright (c) 2020 Floris van Doorn. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Floris van Doorn
 -/
-import data.set.pointwise.smul
 import algebra.order.kleene
+import data.set.pointwise.smul
 
 /-!
 # Sets as a semiring under union

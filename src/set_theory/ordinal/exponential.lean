cc37371dd9c325a9e8c3e6784c33d604199b3400
Violeta Hernández <vi.hdz.p@gmail.com>
2023-03-01 03:31:28 -0600

    update doc

--- src/set_theory/ordinal/exponential.lean
+++ src/set_theory/ordinal/exponential.lean
@@ -10,7 +10,8 @@ import set_theory.ordinal.arithmetic
 > THIS FILE IS SYNCHRONIZED WITH MATHLIB4.
 > Any changes to this file require a corresponding PR to mathlib4.
 
-In this file we define the power function and the logarithm function on ordinals,
+In this file we define the power function and the logarithm function on ordinals. The latter is used
+to define the Cantor Normal Form.
 
 -/
 

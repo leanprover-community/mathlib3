68c439e610e0898be0d3180e30a028af101dc619
Rémi Bottinelli <bottine@users.noreply.github.com>
2023-03-01 15:47:28 +0000

    Update src/combinatorics/simple_graph/ends/properties.lean
    Co-authored-by: Yaël Dillies <yael.dillies@gmail.com>

--- src/combinatorics/simple_graph/ends/properties.lean
+++ src/combinatorics/simple_graph/ends/properties.lean
@@ -3,8 +3,8 @@ Copyright (c) 2022 Anand Rao, Rémi Bottinelli. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Anand Rao, Rémi Bottinelli
 -/
-import combinatorics.simple_graph.ends.defs
 import category_theory.cofiltered_system
+import combinatorics.simple_graph.ends.defs
 /-!
 # Properties of the ends of graphs
 

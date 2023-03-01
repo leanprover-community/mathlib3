51c5c0b6e9a22a643dd28c72cff79442c2c9c74c
Junyan Xu <junyanxumath@gmail.com>
2023-02-28 19:00:01 -0500

    fix import

--- src/combinatorics/simple_graph/finsubgraph.lean
+++ src/combinatorics/simple_graph/finsubgraph.lean
@@ -3,7 +3,7 @@ Copyright (c) 2022 Joanna Choules. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Joanna Choules
 -/
-import topology.category.Top.limits
+import category_theory.cofiltered_system
 import combinatorics.simple_graph.subgraph
 
 /-!

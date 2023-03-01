75fb113e4f13e634101678230c7cc918de4daaa1
Johan Commelin <johan@commelin.net>
2023-03-01 18:55:47 +0100

    Update src/ring_theory/quotient_noetherian.lean
    Co-authored-by: Eric Wieser <wieser.eric@gmail.com>

--- src/ring_theory/quotient_noetherian.lean
+++ src/ring_theory/quotient_noetherian.lean
@@ -1,7 +1,7 @@
 /-
-Copyright (c) 2018 Mario Carneiro, Kevin Buzzard. All rights reserved.
+Copyright (c) 2021 Anne Baanen. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
-Authors: Mario Carneiro, Kevin Buzzard
+Authors: Anne Baanen
 -/
 import ring_theory.noetherian
 import ring_theory.quotient_nilpotent

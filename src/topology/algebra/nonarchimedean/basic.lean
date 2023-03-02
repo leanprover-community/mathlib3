9a59dcb7a2d06bf55da57b9030169219980660cd
Yury G. Kudryashov <urkud@urkud.name>
2023-03-02 00:11:50 +0000

    chore(topology/algebra/ring): split into 2 files (#18532)

--- src/topology/algebra/nonarchimedean/basic.lean
+++ src/topology/algebra/nonarchimedean/basic.lean
@@ -5,7 +5,7 @@ Authors: Kevin Buzzard, Johan Commelin, Ashwin Iyengar, Patrick Massot
 -/
 import group_theory.subgroup.basic
 import topology.algebra.open_subgroup
-import topology.algebra.ring
+import topology.algebra.ring.basic
 
 /-!
 # Nonarchimedean Topology

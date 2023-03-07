07ebd9ff87e8d0c65bd85104278f1ac5b5a4ba66
Eric Wieser <wieser.eric@gmail.com>
2023-03-07 18:36:45 +0100

    Update module.lean

--- src/algebra/star/module.lean
+++ src/algebra/star/module.lean
@@ -72,7 +72,7 @@ variables (R : Type*) (A : Type*)
 
 /-- The self-adjoint elements of a star module, as a submodule. -/
 def self_adjoint.submodule : submodule R A :=
-{ smul_mem' := is_self_adjoint.smul,
+{ smul_mem' := λ r, (is_self_adjoint.all _).smul,
   ..self_adjoint A }
 
 /-- The skew-adjoint elements of a star module, as a submodule. -/

0abf854745b9ec13ba99970b7f14e5fad283b7f3
b-reinke <b-reinke@users.noreply.github.com>
2023-03-08 15:49:08 +0100

    add norm_cast attribute for coe_of_card
    Co-authored-by: Yaël Dillies <yael.dillies@gmail.com>

--- src/group_theory/sylow.lean
+++ src/group_theory/sylow.lean
@@ -615,7 +615,7 @@ def of_card [fintype G] {p : ℕ} [hp : fact p.prime] (H : subgroup G) [fintype 
       (P.card_eq_multiplicity.trans card_eq.symm).le).symm ▸ λ _, P.3,
   end }
 
-@[simp] lemma coe_of_card [fintype G] {p : ℕ} [hp : fact p.prime] (H : subgroup G) [fintype H]
+@[simp, norm_cast] lemma coe_of_card [fintype G] {p : ℕ} [hp : fact p.prime] (H : subgroup G) [fintype H]
   (card_eq: card H = p ^ (card G).factorization p) : ↑(of_card H card_eq) = H := rfl
 
 lemma subsingleton_of_normal {p : ℕ} [fact p.prime] [finite (sylow p G)] (P : sylow p G)

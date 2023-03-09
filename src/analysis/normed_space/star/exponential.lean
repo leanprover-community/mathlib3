a6c0429f20fda20cf89a3803c6ea5ae90e63147f
Eric Wieser <wieser.eric@gmail.com>
2023-03-09 16:56:56 +0000

    feat: more results

--- src/analysis/normed_space/star/exponential.lean
+++ src/analysis/normed_space/star/exponential.lean
@@ -26,21 +26,15 @@ variables {A : Type*}
 
 open complex
 
-lemma is_self_adjoint.exp_i_smul_unitary {a : A} (ha : is_self_adjoint a) :
-  exp ℂ (I • a) ∈ unitary A :=
-begin
-  rw [unitary.mem_iff, star_exp],
-  simp only [star_smul, is_R_or_C.star_def, self_adjoint.mem_iff.mp ha, conj_I, neg_smul],
-  rw ←@exp_add_of_commute ℂ A _ _ _ _ _ _ ((commute.refl (I • a)).neg_left),
-  rw ←@exp_add_of_commute ℂ A _ _ _ _ _ _ ((commute.refl (I • a)).neg_right),
-  simpa only [add_right_neg, add_left_neg, and_self] using (exp_zero : exp ℂ (0 : A) = 1),
-end
+lemma is_self_adjoint.I_smul_mem_skew_adjoint {a : A} (ha : is_self_adjoint a) :
+  (I • a) ∈ skew_adjoint A :=
+by rw [skew_adjoint.mem_iff, star_smul, is_R_or_C.star_def, conj_I, ha.star_eq, neg_smul]
 
 /-- The map from the selfadjoint real subspace to the unitary group. This map only makes sense
 over ℂ. -/
 @[simps]
 noncomputable def self_adjoint.exp_unitary (a : self_adjoint A) : unitary A :=
-⟨exp ℂ (I • a), a.prop.exp_i_smul_unitary⟩
+⟨exp ℂ (I • a), exp_mem_unitary_of_mem_skew_adjoint _ a.prop.I_smul_mem_skew_adjoint⟩
 
 open self_adjoint
 

af29f65b0a934632f1ef9428b2c96988df4f90be
Eric Wieser <wieser.eric@gmail.com>
2023-03-11 18:41:50 +0000

    more general lemma

--- src/algebra/star/self_adjoint.lean
+++ src/algebra/star/self_adjoint.lean
@@ -376,6 +376,14 @@ end has_smul
 
 end skew_adjoint
 
+/-- Scalar multiplication of a self-adjoint element by a skew-adjoint element produces a
+skew-adjoint element. -/
+lemma is_self_adjoint.smul_mem_skew_adjoint
+  [ring R] [add_comm_group A] [module R A] [star_add_monoid R] [star_add_monoid A] [star_module R A] {r : R}
+  (hr : r ∈ skew_adjoint R) {a : A} (ha : is_self_adjoint a) :
+  r • a ∈ skew_adjoint A :=
+(star_smul _ _).trans $ (congr_arg2 _ hr ha).trans $ neg_smul _ _
+
 instance is_star_normal_zero [semiring R] [star_ring R] : is_star_normal (0 : R) :=
 ⟨by simp only [star_comm_self, star_zero]⟩
 

79fcdfa27b3440995d8848b8ce8c350b7666e737
Michael Stoll <Michael.Stoll@uni-bayreuth.de>
2023-03-13 03:32:40 +0100

    simplify ext

--- src/number_theory/pell.lean
+++ src/number_theory/pell.lean
@@ -214,7 +214,7 @@ lemma rel_y (a : solution₁ d) : d * a.y ^ 2 = a.x ^ 2 - 1 := by {rw ← a.rel,
 /-- Two solutions are equal if their `x` and `y` components are equal. -/
 @[ext]
 lemma ext {a b : solution₁ d} (hx : a.x = b.x) (hy : a.y = b.y) : a = b :=
-by {ext, exact ext.mpr ⟨hx, hy⟩}
+subtype.ext $ ext.mpr ⟨hx, hy⟩
 
 /-- Construct a solution from `x`, `y` and a proof that the equation is satisfied. -/
 def mk (x y : ℤ) (rel : x ^ 2 - d * y ^ 2 = 1) : solution₁ d :=

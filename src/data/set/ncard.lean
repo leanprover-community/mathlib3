0cea80decd8a9948585ebf9bfb98505acd11aa4d
Peter Nelson <apnelson@uwaterloo.ca>
2023-03-13 13:56:44 -0400

    copyright header

--- src/data/set/ncard.lean
+++ src/data/set/ncard.lean
@@ -1,8 +1,7 @@
-
 /-
 Copyright (c) 2023 Peter Nelson. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
-Authors: Peter Nelson
+Author: Peter Nelson.
 -/
 import data.finite.card
 

524772a92da3137be7b26599a3ace532f3247c62
Eric Wieser <wieser.eric@gmail.com>
2023-03-14 14:54:34 +0000

    chore(order/with_bot): lemmas about unbot and untop

--- src/order/succ_pred/basic.lean
+++ src/order/succ_pred/basic.lean
@@ -825,6 +825,14 @@ instance : pred_order (with_top α) :=
 @[simp] lemma pred_top : pred (⊤ : with_top α) = ↑(⊤ : α) := rfl
 @[simp] lemma pred_coe (a : α) : pred (↑a : with_top α) = ↑(pred a) := rfl
 
+@[simp] lemma pred_unbot [nontrivial α] (a : with_top α) (ha : a ≠ ⊤) :
+  pred (a.untop ha) = (pred a).untop (by induction a using with_top.rec_top_coe; simp) :=
+begin
+  induction a using with_top.rec_top_coe,
+  cases ha rfl,
+  refl,
+end
+
 end pred
 
 /-! #### Adding a `⊤` to a `no_max_order` -/
@@ -921,6 +929,14 @@ instance : succ_order (with_bot α) :=
 
 @[simp] lemma succ_bot : succ (⊥ : with_bot α) = ↑(⊥ : α) := rfl
 @[simp] lemma succ_coe (a : α) : succ (↑a : with_bot α) = ↑(succ a) := rfl
+
+@[simp] lemma succ_unbot [nontrivial α] (a : with_bot α) (ha : a ≠ ⊥) :
+  succ (a.unbot ha) = (succ a).unbot (by induction a using with_bot.rec_bot_coe; simp) :=
+begin
+  induction a using with_bot.rec_bot_coe,
+  cases ha rfl,
+  refl,
+end
 
 end succ
 

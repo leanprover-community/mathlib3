f854a25177272633997fa1ae1a71f86efce4d258
Scott Morrison <scott@tqft.net>
2023-03-16 13:00:18 +1100

    Update src/linear_algebra/matrix/adjugate/det.lean
    Co-authored-by: Eric Wieser <wieser.eric@gmail.com>

--- src/linear_algebra/matrix/adjugate/det.lean
+++ src/linear_algebra/matrix/adjugate/det.lean
@@ -1,7 +1,7 @@
 /-
-Copyright (c) 2019 Anne Baanen. All rights reserved.
+Copyright (c) 2021 Eric Wieser. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
-Authors: Anne Baanen
+Authors: Eric Wieser
 -/
 import linear_algebra.matrix.adjugate.basic
 import linear_algebra.matrix.mv_polynomial

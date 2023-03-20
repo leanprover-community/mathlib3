5bd4cf32ffba639595a41d2cf9debd60d5032467
sgouezel <sebastien.gouezel@univ-rennes1.fr>
2023-03-20 08:56:42 +0100

    typo

--- src/analysis/calculus/cont_diff.lean
+++ src/analysis/calculus/cont_diff.lean
@@ -2783,7 +2783,7 @@ lemma cont_diff_on.add {s : set E} {f g : E → F}
 
 variables {i : ℕ}
 
-/-- The iterated derivative of the sum of two functions is the sum of the iterated derivative.
+/-- The iterated derivative of the sum of two functions is the sum of the iterated derivatives.
 See also `iterated_fderiv_within_add_apply'`, which uses the spelling `(λ x, f x + g x)`
 instead of `f + g`. -/
 lemma iterated_fderiv_within_add_apply {f g : E → F}
@@ -2818,7 +2818,7 @@ begin
     ... = (iterated_fderiv_within 𝕜 (i+1) f s + iterated_fderiv_within 𝕜 (i+1) g s) x h : rfl }
 end
 
-/-- The iterated derivative of the sum of two functions is the sum of the iterated derivative.
+/-- The iterated derivative of the sum of two functions is the sum of the iterated derivatives.
 This is the same as `iterated_fderiv_within_add_apply`, but using the spelling `(λ x, f x + g x)`
 instead of `f + g`, which can be handy for some rewrites.
 TODO: use one form consistently. -/

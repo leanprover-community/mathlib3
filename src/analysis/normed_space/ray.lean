1acf4fc9a5a798bd331d255877f6fb1e927bde18
Eric Wieser <wieser.eric@gmail.com>
2023-03-25 11:45:02 +0000

    fix

--- src/analysis/normed_space/ray.lean
+++ src/analysis/normed_space/ray.lean
@@ -4,7 +4,7 @@ Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Yury Kudryashov, Yaël Dillies
 -/
 import linear_algebra.ray
-import analysis.normed_space.basic
+import analysis.normed_space.real
 
 /-!
 # Rays in a real normed vector space

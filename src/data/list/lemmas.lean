78f1d2c0cefc9e68be52112aa866cfccd787bcef
Yury G. Kudryashov <urkud@urkud.name>
2023-03-25 14:20:04 -0500

    Move to a new file

--- src/data/list/lemmas.lean
+++ src/data/list/lemmas.lean
@@ -20,56 +20,6 @@ variables {α β γ : Type*}
 
 namespace list
 
-lemma range_map (f : α → β) : set.range (map f) = {l | ∀ x ∈ l, x ∈ set.range f} :=
-begin
-  refine set.subset.antisymm (set.range_subset_iff.2 $
-    λ l, forall_mem_map_iff.2 $ λ y _, set.mem_range_self _) (λ l hl, _),
-  induction l with a l ihl, { exact ⟨[], rfl⟩ },
-  rcases ihl (λ x hx, hl x $ subset_cons _ _ hx) with ⟨l, rfl⟩,
-  rcases hl a (mem_cons_self _ _) with ⟨a, rfl⟩,
-  exact ⟨a :: l, map_cons _ _ _⟩
-end
-
-lemma range_map_coe (s : set α) : set.range (map (coe : s → α)) = {l | ∀ x ∈ l, x ∈ s} :=
-by rw [range_map, subtype.range_coe]
-
-/-- If each element of a list can be lifted to some type, then the whole list can be lifted to this
-type. -/
-instance can_lift (c) (p) [can_lift α β c p] :
-  can_lift (list α) (list β) (list.map c) (λ l, ∀ x ∈ l, p x) :=
-{ prf  := λ l H,
-    begin
-      rw [← set.mem_range, range_map],
-      exact λ a ha, can_lift.prf a (H a ha),
-    end}
-
--- not a `simp` lemma because it uses `fin.val` instead of `coe` which is not available here
-lemma range_nth_le (l : list α) : set.range (λ k : fin l.length, l.nth_le k.1 k.2) = {x | x ∈ l} :=
-begin
-  ext x,
-  rw [set.mem_set_of_eq, mem_iff_nth_le],
-  exact ⟨λ ⟨⟨n, h₁⟩, h₂⟩, ⟨n, h₁, h₂⟩, λ ⟨n, h₁, h₂⟩, ⟨⟨n, h₁⟩, h₂⟩⟩
-end
-
-lemma range_nth (l : list α) : set.range l.nth = insert none (some '' {x | x ∈ l}) :=
-begin
-  rw [← range_nth_le, ← set.range_comp],
-  refine (set.range_subset_iff.2 $ λ n, _).antisymm (set.insert_subset.2 ⟨_, _⟩),
-  exacts [(le_or_lt l.length n).imp nth_eq_none_iff.2 (λ hlt, ⟨⟨_, _⟩, (nth_le_nth hlt).symm⟩),
-    ⟨_, nth_eq_none_iff.2 le_rfl⟩, set.range_subset_iff.2 $ λ k, ⟨_, nth_le_nth _⟩]
-end
-
-@[simp]
-lemma range_nthd (l : list α) (d : α) : set.range (λ n, l.nthd n d) = insert d {x | x ∈ l} :=
-calc set.range (λ n, l.nthd n d) = (λ o : option α, o.get_or_else d) '' set.range l.nth :
-  by simp only [← set.range_comp, (∘), nthd_eq_get_or_else_nth]
-... = insert d {x | x ∈ l} :
-  by simp only [range_nth, set.image_insert_eq, option.get_or_else, set.image_image, set.image_id']
-
-@[simp]
-lemma range_inth [inhabited α] (l : list α) : set.range l.inth = insert default {x | x ∈ l} :=
-range_nthd l default
-
 lemma inj_on_insert_nth_index_of_not_mem (l : list α) (x : α) (hx : x ∉ l) :
   set.inj_on (λ k, insert_nth k x l) {n | n ≤ l.length} :=
 begin

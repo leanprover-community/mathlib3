7bae83d891f50f2b0ca5d9506036ffb484ddcc11
Moritz Doll <moritz.doll@googlemail.com>
2023-03-27 01:53:52 +1100

    Update src/analysis/locally_convex/with_seminorms.lean

--- src/analysis/locally_convex/with_seminorms.lean
+++ src/analysis/locally_convex/with_seminorms.lean
@@ -381,7 +381,7 @@ variables {p : seminorm_family 𝕜 E ι}
 Variant with `finset.sup`. -/
 lemma with_seminorms.tendsto_nhds' (hp : with_seminorms p) (u : F → E) {f : filter F} (y₀ : E) :
   filter.tendsto u f (𝓝 y₀) ↔
-  ∀ (s : finset ι) (ε : ℝ) (hε : 0 < ε), ∀ᶠ x in f, s.sup p (u x - y₀) < ε :=
+    ∀ (s : finset ι) (ε : ℝ) (hε : 0 < ε), ∀ᶠ x in f, s.sup p (u x - y₀) < ε :=
 by simp [hp.has_basis_ball.tendsto_right_iff]
 
 /-- Convergence along filters for `with_seminorms`. -/

fe44cd36149e675eb5dec87acc7e8f1d6568e081
David Loeffler <d.loeffler.01@cantab.net>
2023-03-30 06:36:57 +0000

    feat (number_theory/modular_forms): holomorphy of the Jacobi theta function (#18631)
    Show that the Jacobi theta function is holomorphic on the upper half-plane, with exponential decay at infinity.

--- src/number_theory/modular_forms/basic.lean
+++ src/number_theory/modular_forms/basic.lean
@@ -4,7 +4,6 @@ Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Chris Birkbeck
 -/
 
-import geometry.manifold.mfderiv
 import analysis.complex.upper_half_plane.functions_bounded_at_infty
 import analysis.complex.upper_half_plane.topology
 import number_theory.modular_forms.slash_invariant_forms
@@ -23,12 +22,6 @@ open complex upper_half_plane
 open_locale topology manifold upper_half_plane
 
 noncomputable theory
-
-instance upper_half_plane.charted_space : charted_space ℂ ℍ :=
-upper_half_plane.open_embedding_coe.singleton_charted_space
-
-instance upper_half_plane.smooth_manifold_with_corners : smooth_manifold_with_corners 𝓘(ℂ) ℍ :=
-upper_half_plane.open_embedding_coe.singleton_smooth_manifold_with_corners 𝓘(ℂ)
 
 local prefix `↑ₘ`:1024 := @coe _ (matrix (fin 2) (fin 2) _) _
 

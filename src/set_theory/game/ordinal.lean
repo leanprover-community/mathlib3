011f981b06388126867acdc95a061f7a5f922095
Violeta Hernández <vi.hdz.p@gmail.com>
2023-03-30 03:32:01 -0600

    split off

--- src/set_theory/game/ordinal.lean
+++ src/set_theory/game/ordinal.lean
@@ -4,14 +4,15 @@ Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Violeta Hernández Palacios
 -/
 
+import algebra.order.hom.monoid
 import set_theory.game.basic
 import set_theory.ordinal.natural_ops
 
 /-!
 # Ordinals as games
 
-We define the canonical map `ordinal → pgame`, where every ordinal is mapped to the game whose left
-set consists of all previous ordinals.
+We define the canonical maps `ordinal → pgame` and `ordinal → game`, where every ordinal is mapped
+to the game whose left set consists of all previous ordinals.
 
 The map to surreals is defined in `ordinal.to_surreal`.
 
@@ -19,6 +20,11 @@ The map to surreals is defined in `ordinal.to_surreal`.
 
 - `ordinal.to_pgame`: The canonical map between ordinals and pre-games.
 - `ordinal.to_pgame_embedding`: The order embedding version of the previous map.
+- `ordinal.to_game`: The composition of `ordinal.to_pgame` with `quot.mk` as an order embedding.
+
+# Todo
+
+Prove that `nat_ordinal.to_game` preserves multiplication.
 -/
 
 universe u
@@ -28,6 +34,8 @@ open pgame
 open_locale natural_ops pgame
 
 namespace ordinal
+
+/-! ### Ordinals to `pgame` -/
 
 /-- Converts an ordinal into the corresponding pre-game. -/
 noncomputable! def to_pgame : ordinal.{u} → pgame.{u}
@@ -76,6 +84,9 @@ by simp
 noncomputable def zero_to_pgame_relabelling : to_pgame 0 ≡r 0 :=
 relabelling.is_empty _
 
+theorem zero_to_pgame_equiv : to_pgame 0 ≈ 0 :=
+zero_to_pgame_relabelling.equiv
+
 noncomputable instance unique_one_to_pgame_left_moves : unique (to_pgame 1).left_moves :=
 (equiv.cast $ to_pgame_left_moves 1).unique
 
@@ -95,12 +106,15 @@ noncomputable def one_to_pgame_relabelling : to_pgame 1 ≡r 1 :=
 ⟨equiv.equiv_of_unique _ _, equiv.equiv_of_is_empty _ _,
   λ i, by simpa using zero_to_pgame_relabelling, is_empty_elim⟩
 
+theorem one_to_pgame_equiv : to_pgame 1 ≈ 1 :=
+one_to_pgame_relabelling.equiv
+
 theorem to_pgame_lf {a b : ordinal} (h : a < b) : a.to_pgame ⧏ b.to_pgame :=
 by { convert move_left_lf (to_left_moves_to_pgame ⟨a, h⟩), rw to_pgame_move_left }
 
 theorem to_pgame_le {a b : ordinal} (h : a ≤ b) : a.to_pgame ≤ b.to_pgame :=
 begin
-  refine le_iff_forall_lf.2 ⟨λ i, _, is_empty_elim⟩,
+  refine le_of_forall_lf (λ i, _) is_empty_elim,
   rw to_pgame_move_left',
   exact to_pgame_lf ((to_left_moves_to_pgame_symm_lt i).trans_le h)
 end
@@ -110,6 +124,9 @@ theorem to_pgame_lt {a b : ordinal} (h : a < b) : a.to_pgame < b.to_pgame :=
 
 theorem to_pgame_nonneg (a : ordinal) : 0 ≤ a.to_pgame :=
 zero_to_pgame_relabelling.ge.trans $ to_pgame_le $ ordinal.zero_le a
+
+theorem to_pgame_strict_mono : strict_mono to_pgame :=
+@to_pgame_lt
 
 @[simp] theorem to_pgame_lf_iff {a b : ordinal} : a.to_pgame ⧏ b.to_pgame ↔ a < b :=
 ⟨by { contrapose, rw [not_lt, not_lf], exact to_pgame_le }, to_pgame_lf⟩
@@ -130,7 +147,7 @@ theorem to_pgame_injective : function.injective ordinal.to_pgame :=
 to_pgame_injective.eq_iff
 
 /-- The order embedding version of `to_pgame`. -/
-@[simps] noncomputable def to_pgame_embedding : ordinal.{u} ↪o pgame.{u} :=
+@[simps] noncomputable def to_pgame_embedding : ordinal ↪o pgame :=
 { to_fun := ordinal.to_pgame,
   inj' := to_pgame_injective,
   map_rel_iff' := @to_pgame_le_iff }
@@ -157,8 +174,67 @@ theorem to_pgame_add : ∀ a b : ordinal.{u}, a.to_pgame + b.to_pgame ≈ (a ♯
 end
 using_well_founded { dec_tac := `[solve_by_elim [psigma.lex.left, psigma.lex.right]] }
 
-@[simp] theorem to_pgame_add_mk (a b : ordinal) :
-  ⟦a.to_pgame⟧ + ⟦b.to_pgame⟧ = ⟦(a ♯ b).to_pgame⟧ :=
+/-! ### Ordinals to `game` -/
+
+/-- Converts an `ordinal` into a `pgame`. -/
+noncomputable def to_game : ordinal ↪o game :=
+{ to_fun := λ o, ⟦o.to_pgame⟧,
+  inj' := λ a b h, by rwa [←equiv_iff_game_eq, to_pgame_equiv_iff] at h,
+  map_rel_iff' := @to_pgame_le_iff }
+
+@[simp] theorem to_game_def (o : ordinal) : ⟦o.to_pgame⟧ = o.to_game := rfl
+
+theorem to_game_strict_mono : strict_mono to_game :=
+to_pgame_strict_mono
+
+@[simp] theorem zero_to_game : to_game 0 = 0 :=
+quot.sound zero_to_pgame_equiv
+
+@[simp] theorem one_to_game : to_game 1 = 1 :=
+quot.sound one_to_pgame_equiv
+
+@[simp] theorem to_game_add (a b : ordinal) : a.to_game + b.to_game = (a ♯ b).to_game :=
 quot.sound (to_pgame_add a b)
 
+@[simp] theorem to_game_add_one (a : ordinal) : a.to_game + 1 = (order.succ a).to_game :=
+by rw [←one_to_game, to_game_add, nadd_one]
+
+theorem to_pgame_add_one (a : ordinal) : a.to_pgame + 1 ≈ (order.succ a).to_pgame :=
+quotient.exact (to_game_add_one a)
+
+@[simp] theorem to_game_one_add (a : ordinal) : 1 + a.to_game = (order.succ a).to_game :=
+by rw [add_comm, to_game_add_one]
+
+theorem to_pgame_one_add (a : ordinal) : 1 + a.to_pgame ≈ (order.succ a).to_pgame :=
+quotient.exact (to_game_one_add a)
+
+@[simp] theorem nat_cast_to_game : ∀ n : ℕ, to_game n = n
+| 0       := by simp
+| (n + 1) := begin
+  rw [nat.cast_add, nat.cast_one, add_one_eq_succ, ←to_game_add_one, nat_cast_to_game],
+  refl
+end
+
+theorem nat_cast_to_pgame (n : ℕ) : to_pgame n ≈ n :=
+@quotient.exact pgame _ _ _ $ by simp
+
+@[simp] theorem to_game_add_nat (a : ordinal) (n : ℕ) : a.to_game + n = (a + n).to_game :=
+by rw [←nat_cast_to_game, to_game_add, nadd_nat]
+
+theorem to_pgame_add_nat (a : ordinal) (n : ℕ) : a.to_pgame + n ≈ (a + n).to_pgame :=
+@quotient.exact pgame _ _ _ $ by simp
+
+@[simp] theorem to_game_nat_add (a : ordinal) (n : ℕ) : ↑n + a.to_game = (a + n).to_game :=
+by rw [add_comm, to_game_add_nat]
+
+theorem to_pgame_nat_add (a : ordinal) (n : ℕ) : ↑n + a.to_pgame ≈ (a + n).to_pgame :=
+@quotient.exact pgame _ _ _ $ by simp
+
 end ordinal
+
+/-- The cast from `nat_ordinal` to `game` preserves addition. -/
+noncomputable def nat_ordinal.to_game : nat_ordinal →+o game :=
+{ to_fun := λ o, o.to_ordinal.to_game,
+  map_zero' := ordinal.zero_to_game,
+  map_add' := λ a b, (ordinal.to_game_add _ _).symm,
+  monotone' := ordinal.to_game_strict_mono.monotone }

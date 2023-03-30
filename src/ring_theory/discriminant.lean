a90a127250c5bfa7b66683dab35caef494155391
Eric Wieser <wieser.eric@gmail.com>
2023-03-30 09:44:56 +0000

    fix

--- src/ring_theory/discriminant.lean
+++ src/ring_theory/discriminant.lean
@@ -128,9 +128,9 @@ begin
   { have := span_eq_top_of_linear_independent_of_card_eq_finrank b.linear_independent
       (finrank_eq_card_basis b).symm,
     classical,
-    rw [discr_def, trace_matrix_def],
+    rw [discr_def, trace_matrix],
     simp_rw [← basis.mk_apply b.linear_independent this.ge],
-    rw [← trace_matrix_def, trace_matrix_of_basis, ← bilin_form.nondegenerate_iff_det_ne_zero],
+    rw [← trace_matrix, trace_matrix_of_basis, ← bilin_form.nondegenerate_iff_det_ne_zero],
     exact trace_form_nondegenerate _ _ },
 end
 

432560c76c16322801e553c43ad78bb40fe6cdfc
Eric Wieser <wieser.eric@gmail.com>
2023-03-30 16:34:23 +0100

    Update src/analysis/inner_product_space/basic.lean

--- src/analysis/inner_product_space/basic.lean
+++ src/analysis/inner_product_space/basic.lean
@@ -45,7 +45,6 @@ product structure on `n → 𝕜` for `𝕜 = ℝ` or `ℂ`, see `euclidean_spac
 We globally denote the real and complex inner products by `⟪·, ·⟫_ℝ` and `⟪·, ·⟫_ℂ` respectively.
 We also provide two notation namespaces: `real_inner_product_space`, `complex_inner_product_space`,
 which respectively introduce the plain notation `⟪·, ·⟫` for the real and complex inner product.
-\
 
 ## Implementation notes
 

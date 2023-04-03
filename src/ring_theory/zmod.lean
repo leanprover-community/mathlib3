56d2962bdd4d76a0c6b1c3670f42ea83e5fcc819
Yaël Dillies <yael.dillies@gmail.com>
2023-04-03 09:26:12 +0000

    use Junyan's lemmas

--- src/ring_theory/zmod.lean
+++ src/ring_theory/zmod.lean
@@ -10,27 +10,18 @@ import ring_theory.int.basic
 /-!
 # Ring theoretic facts about `zmod n`
 
+We collect a few facts about `zmod n` that need some ring theory to be proved/stated
+
 ## Main statements
 
-* `zmod.is_reduced` - `zmod n` is reduced for all squarefree `n`.
-
+* `is_reduced_zmod` - `zmod n` is reduced for all squarefree `n`.
 -/
 
+@[simp] lemma is_reduced_zmod {n : ℕ} : is_reduced (zmod n) ↔ squarefree n ∨ n = 0 :=
+by rw [← ring_hom.ker_is_radical_iff_reduced_of_surjective
+    (zmod.ring_hom_surjective $ int.cast_ring_hom $ zmod n),
+  zmod.ker_int_cast_ring_hom, ← is_radical_iff_span_singleton,
+  is_radical_iff_squarefree_or_zero, int.squarefree_coe_nat, nat.cast_eq_zero]
+
 instance {n : ℕ} [fact $ squarefree n] : is_reduced (zmod n) :=
-⟨begin
-  casesI n,
-  { exact (not_squarefree_zero (fact.out _ : squarefree 0)).elim, },
-  rintro ⟨x, hx⟩ ⟨_ | m, h⟩,
-  { rw [pow_zero, fin.one_eq_zero_iff] at h,
-    rw [h, nat.lt_one_iff] at hx,
-    simp only [hx, fin.mk_zero], },
-  { have : ((⟨x, hx⟩ : zmod n.succ) = (x : zmod n.succ)),
-    { ext,
-      simp only [fin.coe_mk, fin.coe_of_nat_eq_mod],
-      exact (nat.mod_eq_of_lt hx).symm, },
-    rw this at h ⊢,
-    norm_cast at h,
-    rw zmod.nat_coe_zmod_eq_zero_iff_dvd at h ⊢,
-    exact (unique_factorization_monoid.dvd_pow_iff_dvd_of_squarefree
-      (fact.out _) (ne_zero.ne _)).mp h, }
-end⟩
+is_reduced_zmod.2 $ or.inl $ fact.out _

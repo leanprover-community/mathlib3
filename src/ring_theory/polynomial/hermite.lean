7ef85a46cca59abfea606120e0bead29bae9a189
lukemantle <lukeomantle@gmail.com>
2023-04-04 14:57:12 -0700

    fix author line in hermite.lean

--- src/ring_theory/polynomial/hermite.lean
+++ src/ring_theory/polynomial/hermite.lean
@@ -1,7 +1,7 @@
 /-
 Copyright (c) 2023 Luke Mantle. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
-Author: Luke Mantle.
+Authors: Luke Mantle
 -/
 
 import data.polynomial.derivative

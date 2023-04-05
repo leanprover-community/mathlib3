4e17a2f866fe35e8dc82f89d3d559664ecff119e
Violeta Hernández <vi.hdz.p@gmail.com>
2023-04-05 00:36:51 -0600

    Update finite_dimensional.lean

--- src/linear_algebra/finite_dimensional.lean
+++ src/linear_algebra/finite_dimensional.lean
@@ -249,7 +249,7 @@ lemma cardinal_mk_le_finrank_of_linear_independent
   #ι ≤ finrank K V :=
 begin
   rw ← lift_le,
-  simpa [← finrank_eq_dim K V, -finrank_eq_rank] using cardinal_lift_le_dim_of_linear_independent h
+  simpa [← finrank_eq_dim, -finrank_eq_rank] using cardinal_lift_le_dim_of_linear_independent h
 end
 
 lemma fintype_card_le_finrank_of_linear_independent

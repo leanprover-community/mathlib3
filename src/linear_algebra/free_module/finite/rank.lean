601e1e447ba4b346f6d38d70362af963002c2367
Eric Wieser <wieser.eric@gmail.com>
2023-04-06 00:51:27 +0000

    fix

--- src/linear_algebra/free_module/finite/rank.lean
+++ src/linear_algebra/free_module/finite/rank.lean
@@ -57,7 +57,7 @@ end
 
 /-- The finrank of `(ι →₀ R)` is `fintype.card ι`. -/
 @[simp] lemma finrank_finsupp {ι : Type v} [fintype ι] : finrank R (ι →₀ R) = card ι :=
-by { rw [finrank, rank_finsupp'', ← mk_to_nat_eq_card, to_nat_lift] }
+by { rw [finrank, rank_finsupp_self, ← mk_to_nat_eq_card, to_nat_lift] }
 
 /-- The finrank of `(ι → R)` is `fintype.card ι`. -/
 lemma finrank_pi {ι : Type v} [fintype ι] : finrank R (ι → R) = card ι :=

7e00f85456dcbf4ebc5780e4058cb9248509bb51
lukemantle <lukeomantle@gmail.com>
2023-04-07 13:14:02 -0700

    change definition of hermite n, remove x_sub_dx

--- src/ring_theory/polynomial/hermite.lean
+++ src/ring_theory/polynomial/hermite.lean
@@ -5,6 +5,7 @@ Authors: Luke Mantle
 -/
 
 import data.polynomial.derivative
+open polynomial
 
 /-!
 # Hermite polynomials
@@ -24,44 +25,19 @@ This file defines `hermite n`, the nth probabilist's Hermite polynomial.
 -/
 
 noncomputable theory
-open polynomial
-
-section x_sub_dx
-
-variables {R : Type} [comm_ring R] (p : polynomial R)
-
-/-- the operation `(x - d/dx)` defined for `polynomial R` -/
-def x_sub_dx := X * p - p.derivative
-
-@[simp] lemma x_sub_dx_def : x_sub_dx p = X * p - p.derivative := rfl
-
-lemma x_sub_dx_apply {x : R} :
-  eval x (x_sub_dx p) = x * (eval x p) - (eval x (derivative p)) := by simp
-
-@[simp] lemma x_sub_dx_map {S : Type} [comm_ring S] {f : R →+* S} :
-  x_sub_dx (map f p) = map f (x_sub_dx p) := by simp
-
-end x_sub_dx
-
-section hermite
 
 /-- the nth probabilist's Hermite polynomial -/
-def hermite (n : ℕ) : polynomial ℤ := nat.iterate x_sub_dx n 1
+noncomputable def hermite : ℕ → polynomial ℤ
+| 0     := 1
+| (n+1) := X * (hermite n) - (hermite n).derivative
 
-lemma hermite_eq_iter {n : ℕ} : hermite n = nat.iterate x_sub_dx n 1 := rfl
-
-@[simp] lemma hermite_succ {n : ℕ} : hermite n.succ = x_sub_dx (hermite n) :=
-begin
-  rw [hermite_eq_iter, function.iterate_succ' x_sub_dx n],
-  refl,
-end
+@[simp] lemma hermite_succ {n : ℕ} : hermite (n+1) =  X * (hermite n) - (hermite n).derivative :=
+by rw hermite
 
 @[simp] lemma hermite_zero : hermite 0 = C 1 := rfl
 
 @[simp] lemma hermite_one : hermite 1 = X :=
 begin
-  rw [hermite_succ, x_sub_dx_def, hermite_zero],
-  simp only [map_one, mul_one, derivative_one, sub_zero],
+  rw [hermite_succ, hermite_zero],
+  simp only [map_one, mul_one, derivative_one, sub_zero]
 end
-
-end hermite

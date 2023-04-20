246f6f7989ff86bd07e1b014846f11304f33cf9e
Yury G. Kudryashov <urkud@urkud.name>
2023-04-20 16:22:55 +0000

    feat(algebra/group_with_zero): add lemmas about `f * pi.single i x` (#6418)
    Co-authored-by: Yaël Dillies <yael.dillies@gmail.com>

--- src/algebra/group/pi.lean
+++ src/algebra/group/pi.lean
@@ -22,7 +22,7 @@ universes u v w
 variables {ι α : Type*}
 variable {I : Type u}     -- The indexing type
 variable {f : I → Type v} -- The family of types already equipped with instances
-variables (x y : Π i, f i) (i : I)
+variables (x y : Π i, f i) (i j : I)
 
 @[to_additive]
 lemma set.preimage_one {α β : Type*} [has_one β] (s : set β) [decidable ((1 : β) ∈ s)] :
@@ -332,6 +332,22 @@ lemma pi.single_div [Π i, group $ f i] (i : I) (x y : f i) :
 lemma pi.single_mul [Π i, mul_zero_class $ f i] (i : I) (x y : f i) :
   single i (x * y) = single i x * single i y :=
 (mul_hom.single f i).map_mul x y
+
+lemma pi.single_mul_left_apply [Π i, mul_zero_class $ f i] (a : f i) :
+  pi.single i (a * x i) j = pi.single i a j * x j :=
+(pi.apply_single (λ i, (* x i)) (λ i, zero_mul _) _ _ _).symm
+
+lemma pi.single_mul_right_apply [Π i, mul_zero_class $ f i] (a : f i) :
+  pi.single i (x i * a) j = x j * pi.single i a j :=
+(pi.apply_single (λ i, ((*) (x i))) (λ i, mul_zero _) _ _ _).symm
+
+lemma pi.single_mul_left [Π i, mul_zero_class $ f i] (a : f i) :
+  pi.single i (a * x i) = pi.single i a * x :=
+funext $ λ j, pi.single_mul_left_apply _ _ _ _
+
+lemma pi.single_mul_right [Π i, mul_zero_class $ f i] (a : f i) :
+  pi.single i (x i * a) = x * pi.single i a :=
+funext $ λ j, pi.single_mul_right_apply _ _ _ _
 
 /-- The injection into a pi group at different indices commutes.
 

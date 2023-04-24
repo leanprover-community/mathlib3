30a3a5f1bfbbaa2e703bd048d1320cb12b413e48
Eric Rodriguez <37984851+ericrbg@users.noreply.github.com>
2023-04-24 09:08:57 -0400

    Update src/algebra/field/defs.lean
    Co-authored-by: Eric Wieser <wieser.eric@gmail.com>

--- src/algebra/field/defs.lean
+++ src/algebra/field/defs.lean
@@ -143,7 +143,7 @@ instance smul_division_ring : has_smul ℚ K :=
 
 lemma smul_def (a : ℚ) (x : K) : a • x = ↑a * x := division_ring.qsmul_eq_mul' a x
 
-@[simp] lemma smul_one_eq_coe {A : Type*} [division_ring A] (m : ℚ) :
+@[simp] lemma smul_one_eq_coe (A : Type*) [division_ring A] (m : ℚ) :
   m • (1 : A) = ↑m :=
 by rw [rat.smul_def, mul_one]
 

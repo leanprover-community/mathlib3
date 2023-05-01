46805b8283c58532591e03648dcd99d2717a9ce9
David Loeffler <d.loeffler.01@cantab.net>
2023-05-01 08:02:15 -0700

    formatting

--- src/analysis/special_functions/japanese_bracket.lean
+++ src/analysis/special_functions/japanese_bracket.lean
@@ -3,7 +3,6 @@ Copyright (c) 2022 Moritz Doll. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Moritz Doll
 -/
-
 import measure_theory.integral.layercake
 
 /-!

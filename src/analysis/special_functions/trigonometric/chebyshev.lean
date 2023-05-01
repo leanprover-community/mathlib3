3591f3a9659c3945e9dfd5b979121f0f97ce859d
David Loeffler <d.loeffler.01@cantab.net>
2023-05-01 08:36:41 -0700

    more transitive imports

--- src/analysis/special_functions/trigonometric/chebyshev.lean
+++ src/analysis/special_functions/trigonometric/chebyshev.lean
@@ -4,7 +4,6 @@ Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Johan Commelin
 -/
 import analysis.complex.basic
-import data.complex.exponential
 import data.polynomial.algebra_map
 import ring_theory.polynomial.chebyshev
 

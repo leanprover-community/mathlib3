91e162012683491db36fc38f0234280ef2b678fb
David Loeffler <d.loeffler.01@cantab.net>
2023-05-01 17:40:09 -0700

    ... and more

--- src/analysis/calculus/fderiv_analytic.lean
+++ src/analysis/calculus/fderiv_analytic.lean
@@ -4,7 +4,7 @@ Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Yury Kudryashov
 -/
 import analysis.analytic.basic
-import analysis.calculus.mean_value
+import analysis.calculus.local_extr
 import analysis.calculus.cont_diff_def
 
 /-!

04fafe53678166fb6855f93384b4b050bc1ebff3
Jake Levinson <levinson.jake@gmail.com>
2023-05-01 20:29:13 -0700

    modified proof to use division

--- src/ring_theory/polynomial/hermite.lean
+++ src/ring_theory/polynomial/hermite.lean
@@ -133,28 +133,32 @@ end coeff
 section gaussian
 
 /- `hermite n` is (up to sign) the factor appearing in `deriv^[n]` of a gaussian -/
-lemma hermite_eq_deriv_gaussian : ∀ (x : ℝ) (n : ℕ),
-  aeval x (hermite n) = (-1 : ℝ)^n * real.exp (x^2 / 2) * (deriv^[n] (λ y, real.exp (-(y^2 / 2))) x)
-| _ 0       := by simp [← real.exp_add]
-| _ (_ + 1) :=
+lemma hermite_eq_deriv_gaussian' (n : ℕ) (x : ℝ) :
+  aeval x (hermite n) =
+  (-1 : ℝ)^n * (deriv^[n] (λ y, real.exp (-(y^2 / 2))) x) / real.exp (-(x^2 / 2)) :=
 begin
-  have : ∀ n x,
-    (-1 : ℝ)^(n+1) * real.exp (x^2 / 2) * (deriv^[n+1] (λ y, real.exp (-(y^2 / 2))) x)
-    = x * ((-1 : ℝ)^n * real.exp (x^2 / 2) * (deriv^[n] (λ y, real.exp (-(y^2 / 2))) x)) -
-      deriv (λ x, (-1 : ℝ)^n * real.exp (x^2 / 2) * (deriv^[n] (λ y, real.exp (-(y^2 / 2))) x)) x,
-  { intros n x,
-    rw [function.iterate_succ_apply', deriv_mul, deriv_const_mul,
-        (by simp [mul_comm] : deriv (λ y, real.exp (y^2 / 2)) x = x * real.exp (x^2 / 2))],
-    ring_exp,
-    { simp },
-    { simp },
-    { apply (cont_diff_top_iff_deriv.mp (cont_diff.iterate_deriv _ _ _)).1,
-      exact ((cont_diff_id.pow 2).div_const 2).neg.exp } },
-  simp_rw [this, hermite_succ, ← hermite_eq_deriv_gaussian],
-  simp [aeval_def, eval₂_eq_eval_map],
+  induction n with n ih generalizing x,
+  { simp [← real.exp_sub] },
+  { let gaussian : ℝ → ℝ := λ x, real.exp (-(x^2 / 2)),
+    have gaussian_ne_zero : ∀ x, gaussian x ≠ 0 := by simp [gaussian, real.exp_ne_zero],
+    have cont_diff_gaussian : cont_diff ℝ ⊤ gaussian := ((cont_diff_id.pow 2).div_const 2).neg.exp,
+    have : ∀ x n,
+      (-1 : ℝ)^(n+1) * (deriv^[n+1] gaussian x) / gaussian x
+      = x * ((-1 : ℝ)^n * (deriv^[n] gaussian x)) / gaussian x -
+        deriv (λ z, (-1 : ℝ)^n * (deriv^[n] gaussian z) / gaussian z) x,
+    { intros x n,
+      rw [function.iterate_succ_apply', deriv_div _ _ (gaussian_ne_zero _), deriv_const_mul,
+          (by simp [gaussian, mul_comm] : deriv gaussian x = (-x) * gaussian x)],
+      field_simp [gaussian_ne_zero],
+      ring_exp,
+      { apply (cont_diff_top_iff_deriv.mp (cont_diff_gaussian.iterate_deriv _)).1, },
+      { apply (cont_diff_top_iff_deriv.mp _).1,
+        exact (cont_diff_gaussian.iterate_deriv _).const_smul ((-1 : ℝ)^n) },
+      { simp } },
+    simp_rw [this, hermite_succ, ← mul_div x, ← ih],
+    simp [aeval_def, eval₂_eq_eval_map] }
 end
 
 end gaussian
 
 end polynomial
--- ⁻¹

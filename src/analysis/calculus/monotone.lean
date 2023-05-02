2c1d8ca2812b64f88992a5294ea3dba144755cd1
David Loeffler <d.loeffler.01@cantab.net>
2023-05-02 02:56:29 +0000

    chore(analysis/special_functions): Reduce imports in special_functions (#18909)
    Remove a bunch of imports in `analysis/special_functions` and `analysis/calculus` which are unnecessary by transitivity, and a few more which are genuinely nontrivial (i.e. the import isn't in the transitive closure of the other imports) but nonetheless aren't needed for the file to compile.

--- src/analysis/calculus/monotone.lean
+++ src/analysis/calculus/monotone.lean
@@ -3,8 +3,6 @@ Copyright (c) 2022 Sébastien Gouëzel. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Sébastien Gouëzel
 -/
-import measure_theory.measure.lebesgue
-import analysis.calculus.deriv
 import measure_theory.covering.one_dim
 import order.monotone.extension
 

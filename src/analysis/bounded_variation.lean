7f751df3a3f35c119e12544c71f1e8895bd3b7f3
David Loeffler <d.loeffler.01@cantab.net>
2023-05-01 20:09:49 -0700

    unnecessary

--- src/analysis/bounded_variation.lean
+++ src/analysis/bounded_variation.lean
@@ -4,7 +4,6 @@ Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Sébastien Gouëzel
 -/
 import analysis.calculus.monotone
-import tactic.swap_var
 
 /-!
 # Functions of bounded variation

994e49ec325ea04dafba23522e2b62b3538604ad
Scott Morrison <scott.morrison@gmail.com>
2023-05-03 21:40:40 +1000

    Revert reorder

--- src/model_theory/graph.lean
+++ src/model_theory/graph.lean
@@ -3,8 +3,8 @@ Copyright (c) 2022 Aaron Anderson. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Aaron Anderson
 -/
-import combinatorics.simple_graph.basic
 import model_theory.satisfiability
+import combinatorics.simple_graph.basic
 
 /-!
 # First-Ordered Structures in Graph Theory

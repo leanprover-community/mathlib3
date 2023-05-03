6d169630978787b3d1c2e4a50324bebbabf1e1fd
David Loeffler <d.loeffler.01@cantab.net>
2023-05-02 20:24:13 -0700

    optimize

--- src/analysis/normed_space/star/spectrum.lean
+++ src/analysis/normed_space/star/spectrum.lean
@@ -4,7 +4,6 @@ Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Jireh Loreaux
 -/
 import analysis.normed_space.spectrum
-import analysis.normed_space.star.exponential
 import analysis.special_functions.exponential
 
 /-! # Spectral properties in C⋆-algebras

309493c80668b6651ed8f4bc735d10d089a372d3
Scott Morrison <scott.morrison@gmail.com>
2023-05-03 21:33:47 +1000

    fix

--- src/geometry/euclidean/angle/oriented/rotation.lean
+++ src/geometry/euclidean/angle/oriented/rotation.lean
@@ -3,7 +3,7 @@ Copyright (c) 2022 Joseph Myers. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Joseph Myers, Heather Macbeth
 -/
-import analysis.complex.circle
+import analysis.special_functions.complex.circle
 import geometry.euclidean.angle.oriented.basic
 
 /-!

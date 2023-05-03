99c00110e03f2d361fe78a5f804356b7334ccad4
Scott Morrison <scott.morrison@gmail.com>
2023-05-03 21:41:33 +1000

    Revert reorder

--- src/topology/category/UniformSpace.lean
+++ src/topology/category/UniformSpace.lean
@@ -4,8 +4,8 @@ Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Reid Barton, Patrick Massot, Scott Morrison
 -/
 import category_theory.adjunction.reflective
-import category_theory.monad.limits
 import category_theory.concrete_category.unbundled_hom
+import category_theory.monad.limits
 import topology.category.Top.basic
 import topology.uniform_space.completion
 

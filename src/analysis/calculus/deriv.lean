ec760503610cb5df231cca0481dfaa4a616bb391
Eric Wieser <wieser.eric@gmail.com>
2023-05-05 10:23:05 +0000

    alphabetize

--- src/analysis/calculus/deriv.lean
+++ src/analysis/calculus/deriv.lean
@@ -4,8 +4,8 @@ Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Gabriel Ebner, Sébastien Gouëzel
 -/
 import analysis.calculus.fderiv
-import data.polynomial.derivative
 import data.polynomial.algebra_map
+import data.polynomial.derivative
 import linear_algebra.affine_space.slope
 
 /-!

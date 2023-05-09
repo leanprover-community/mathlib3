34c5900808832a94c12f43eda354b8fafe61b3e2
Michael Stoll <Michael.Stoll@uni-bayreuth.de>
2023-05-09 13:41:22 +0200

    change pow to zpow in some names

--- src/number_theory/pell.lean
+++ src/number_theory/pell.lean
@@ -522,23 +522,23 @@ begin
 end
 
 /-- The `n`th power of a fundamental solution is trivial if and only if `n = 0`. -/
-lemma pow_eq_one_iff {a : solution₁ d} (h : is_fundamental a) (n : ℤ) : a ^ n = 1 ↔ n = 0 :=
+lemma zpow_eq_one_iff {a : solution₁ d} (h : is_fundamental a) (n : ℤ) : a ^ n = 1 ↔ n = 0 :=
 begin
   rw ← zpow_zero a,
   exact ⟨λ H, h.y_strict_mono.injective (congr_arg solution₁.y H), λ H, H ▸ rfl⟩,
 end
 
 /-- The `n`th power of a fundamental solution has positive `y` if and only if `n` is positive. -/
-lemma pow_y_pos_iff {a : solution₁ d} (h : is_fundamental a) (n : ℤ) : 0 < (a ^ n).y ↔ 0 < n :=
+lemma zpow_y_pos_iff {a : solution₁ d} (h : is_fundamental a) (n : ℤ) : 0 < (a ^ n).y ↔ 0 < n :=
 h.zpow_y_lt_iff_lt 0 n
 
 /-- The `n`th power of a fundamental solution has negative `y` if and only if `n` is negative. -/
-lemma pow_y_neg_iff {a : solution₁ d} (h : is_fundamental a) (n : ℤ) : (a ^ n).y < 0 ↔ n < 0 :=
+lemma zpow_y_neg_iff {a : solution₁ d} (h : is_fundamental a) (n : ℤ) : (a ^ n).y < 0 ↔ n < 0 :=
 h.zpow_y_lt_iff_lt n 0
 
 /-- A power of a fundamental solution is never equal to the negative of a power of this
 fundamental solution. -/
-lemma pow_ne_neg_pow {a : solution₁ d} (h : is_fundamental a) {n n' : ℤ} :
+lemma zpow_ne_neg_zpow {a : solution₁ d} (h : is_fundamental a) {n n' : ℤ} :
    a ^ n ≠ -a ^ n' :=
 begin
   intro hf,
@@ -688,12 +688,12 @@ begin
         ← zpow_neg_one, neg_mul, ← zpow_add, ← sub_eq_add_neg] at hn₁,
     cases hn₁,
     { rcases int.is_unit_iff.mp (is_unit_of_mul_eq_one _ _ $ sub_eq_zero.mp $
-                (ha₁.pow_eq_one_iff (n₂ * n₁ - 1)).mp hn₁) with rfl | rfl,
+                (ha₁.zpow_eq_one_iff (n₂ * n₁ - 1)).mp hn₁) with rfl | rfl,
       { rw zpow_one, },
       { rw [zpow_neg_one, y_inv, lt_neg, neg_zero] at Hy,
         exact false.elim (lt_irrefl _ $ ha₁.2.1.trans Hy), } },
     { rw [← zpow_zero a₁, eq_comm] at hn₁,
-      exact false.elim (ha₁.pow_ne_neg_pow hn₁), } },
+      exact false.elim (ha₁.zpow_ne_neg_zpow hn₁), } },
   { rw [x_neg, lt_neg] at Hx,
     have := (x_zpow_pos (zero_lt_one.trans ha₁.1) n₂).trans Hx,
     norm_num at this, }

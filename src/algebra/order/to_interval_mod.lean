0923ad588bde41364ea68c9459565f65213ad1d1
Mauricio Collares <mauricio@collares.org>
2023-05-10 20:46:26 +0200

    golf

--- src/algebra/order/to_interval_mod.lean
+++ src/algebra/order/to_interval_mod.lean
@@ -704,14 +704,12 @@ begin
   { suffices hIoc₂₁ : to_Ioc_mod hp x₂' x₁ = x₁ + p,
     { exact hIoc₂₁.symm.trans_ge h₃₁ },
     apply (to_Ioc_mod_eq_iff hp).2,
-    refine ⟨_, -1, by simp⟩,
-    exact ⟨h₂₁, (add_le_add_iff_right p).2 (left_le_to_Ico_mod hp x₁ x₂)⟩ },
+    exact ⟨⟨h₂₁, by simp [left_le_to_Ico_mod hp x₁ x₂]⟩, -1, by simp⟩ },
 
   have h₃₂ : x₃' - p < x₂' := sub_lt_iff_lt_add.2 (to_Ico_mod_lt_right hp x₂' x₃),
   have hIoc₁₃ : to_Ioc_mod hp x₁ x₃' = x₃' - p,
   { apply (to_Ioc_mod_eq_iff hp).2,
-    refine ⟨_, 1, by simp⟩,
-    exact ⟨lt_sub_iff_add_lt.2 h₁₃, le_of_lt (h₃₂.trans h₂₁)⟩ },
+    exact ⟨⟨lt_sub_iff_add_lt.2 h₁₃, le_of_lt (h₃₂.trans h₂₁)⟩, 1, by simp⟩ },
   have not_h₃₂ := (h.trans hIoc₁₃.le).not_lt,
   contradiction
 end

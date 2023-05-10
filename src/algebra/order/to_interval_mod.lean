55fd2840d62d179048a674160447af2621d4c7d7
Mauricio Collares <mauricio@collares.org>
2023-05-10 20:03:05 +0200

    golf

--- src/algebra/order/to_interval_mod.lean
+++ src/algebra/order/to_interval_mod.lean
@@ -698,24 +698,20 @@ begin
     simpa using hequiv },
 
   have h : x₂' ≤ to_Ioc_mod hp x₁ x₃' := by simpa,
+  have h₂₁ : x₂' < x₁ + p := to_Ico_mod_lt_right hp x₁ x₂,
 
   cases le_or_lt x₃' (x₁ + p) with h₃₁ h₁₃,
   { suffices hIoc₂₁ : to_Ioc_mod hp x₂' x₁ = x₁ + p,
     { exact hIoc₂₁.symm.trans_ge h₃₁ },
     apply (to_Ioc_mod_eq_iff hp).2,
-    refine ⟨_, -1, _⟩,
-    { simp only [set.mem_Ioc, add_le_add_iff_right],
-      exact ⟨to_Ico_mod_lt_right _ _ _, left_le_to_Ico_mod _ _ _⟩ },
-    { simp only [add_neg_cancel_right, one_smul, neg_smul] } },
+    refine ⟨_, -1, by simp⟩,
+    exact ⟨h₂₁, (add_le_add_iff_right p).2 (left_le_to_Ico_mod hp x₁ x₂)⟩ },
 
-  have h₃₂ : x₃' - p < x₂' := sub_lt_iff_lt_add.2 (to_Ico_mod_lt_right _ _ _),
-  have h₂₁ : x₂' < x₁ + p := to_Ico_mod_lt_right _ _ _,
+  have h₃₂ : x₃' - p < x₂' := sub_lt_iff_lt_add.2 (to_Ico_mod_lt_right hp x₂' x₃),
   have hIoc₁₃ : to_Ioc_mod hp x₁ x₃' = x₃' - p,
   { apply (to_Ioc_mod_eq_iff hp).2,
-    refine ⟨_, 1, _⟩,
-    { simp only [set.mem_Ioc, add_le_add_iff_right],
-      exact ⟨lt_sub_iff_add_lt.2 h₁₃, le_of_lt (h₃₂.trans h₂₁)⟩ },
-    { simp only [sub_add_cancel, one_smul] } },
+    refine ⟨_, 1, by simp⟩,
+    exact ⟨lt_sub_iff_add_lt.2 h₁₃, le_of_lt (h₃₂.trans h₂₁)⟩ },
   have not_h₃₂ := (h.trans hIoc₁₃.le).not_lt,
   contradiction
 end

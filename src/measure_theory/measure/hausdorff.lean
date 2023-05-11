00e0484043ca2d7ec90344056054813635d3eff0
Eric Wieser <wieser.eric@gmail.com>
2023-05-11 21:58:23 +0000

    move and lintfix

--- src/measure_theory/measure/hausdorff.lean
+++ src/measure_theory/measure/hausdorff.lean
@@ -763,6 +763,44 @@ begin
       simp only [zero_le, measure_singleton] } }
 end
 
+open_locale pointwise
+
+lemma hausdorff_measure_smul
+  {E : Type*} [normed_add_comm_group E] [normed_space ℝ E] [measurable_space E] [borel_space E]
+  (d : ℝ) (r : ℝ) (hr : r ≠ 0) (hd : 0 ≤ d) (s : set E) :
+  μH[d] (r • s) = ‖r‖₊ ^ d • μH[d] s :=
+begin
+  have hnr : ‖r‖₊ ≠ 0,
+  { simp [hr] },
+  have hnrc : (‖r‖₊ : ennreal) ≠ 0,
+  { simp [hr] },
+  have hrd : (↑(‖r‖₊ ^ d) : ℝ≥0∞) ≠ 0,
+  { simp [hr] },
+  -- simp_rw [hausdorff_measure, ← outer_measure.coe_mk_metric,
+  --   ←outer_measure.smul_apply, ennreal.smul_def],
+  -- rw ←outer_measure.mk_metric_smul _ ennreal.coe_ne_top,
+  -- simp_rw [pi.smul_def, ennreal.coe_rpow_of_ne_zero],
+  simp_rw [hausdorff_measure_apply, ennreal.smul_supr, ennreal.smul_def, smul_eq_mul,
+    ennreal.mul_infi_of_ne hrd ennreal.coe_ne_top, ←ennreal.tsum_mul_left, ennreal.mul_supr,
+    ←ennreal.coe_rpow_of_ne_zero hnr, ←ennreal.mul_rpow_of_nonneg _ _ hd],
+  -- refine (mul_left_surjective₀ hr).supr_congr _ (λ R, _),
+  have : function.surjective ((•) ‖r‖₊ : ℝ≥0∞ → ℝ≥0∞) :=
+    function.right_inverse.surjective (smul_inv_smul₀ hnr),
+  symmetry,
+  refine this.supr_congr _ (λ R, supr_congr_Prop _ $ λ hR, _),
+  { rw [ennreal.smul_def, smul_eq_mul, ennreal.mul_pos_iff, and_iff_right],
+    rw pos_iff_ne_zero,
+    exact hnrc, },
+  have : function.surjective ((•) r : (ℕ → set E) → (ℕ → set E)),
+  { intro x,
+    exact ⟨r⁻¹ • x, funext $ λ i, smul_inv_smul₀ hr _⟩ },
+  symmetry,
+  refine this.infi_congr _ (λ t, _),
+  simp_rw [pi.smul_apply, smul_set_nonempty, ←smul_set_Union, ediam_smul₀, ennreal.smul_def,
+    smul_eq_mul, ennreal.mul_le_mul_left hnrc ennreal.coe_ne_top,
+    set_smul_subset_set_smul_iff₀ hr],
+end
+
 end measure
 
 end measure_theory
@@ -935,46 +973,6 @@ e.isometry.hausdorff_measure_image (or.inr e.surjective) s
 by rw [← e.image_symm, e.symm.hausdorff_measure_image]
 
 end isometry_equiv
-
-open_locale pointwise
-
-lemma hausdorff_measure_smul
-  {E : Type*} [normed_add_comm_group E] [inner_product_space ℝ E]
-  [measurable_space E] [borel_space E]
-  (d : ℝ) (r : ℝ) (hr : r ≠ 0) (hd : 0 ≤ d) (s : set E) :
-  μH[d] (r • s) = ‖r‖₊ ^ d • μH[d] s :=
-begin
-  have hnr : ‖r‖₊ ≠ 0,
-  { simp [hr] },
-  have hnrc : (‖r‖₊ : ennreal) ≠ 0,
-  { simp [hr] },
-  have hrd : (↑(‖r‖₊ ^ d) : ℝ≥0∞) ≠ 0,
-  { simp [hr] },
-  -- simp_rw [hausdorff_measure, ← outer_measure.coe_mk_metric,
-  --   ←outer_measure.smul_apply, ennreal.smul_def],
-  -- rw ←outer_measure.mk_metric_smul _ ennreal.coe_ne_top,
-  -- simp_rw [pi.smul_def, ennreal.coe_rpow_of_ne_zero],
-  simp_rw [hausdorff_measure_apply, ennreal.smul_supr, ennreal.smul_def, smul_eq_mul,
-    ennreal.mul_infi_of_ne hrd ennreal.coe_ne_top, ←ennreal.tsum_mul_left, ennreal.mul_supr,
-    ←ennreal.coe_rpow_of_ne_zero hnr, ←ennreal.mul_rpow_of_nonneg _ _ hd],
-  -- refine (mul_left_surjective₀ hr).supr_congr _ (λ R, _),
-  have : function.surjective ((•) ‖r‖₊ : ℝ≥0∞ → ℝ≥0∞) :=
-    function.right_inverse.surjective (smul_inv_smul₀ hnr),
-  symmetry,
-  refine this.supr_congr _ (λ R, supr_congr_Prop _ $ λ hR, _),
-  { rw [ennreal.smul_def, smul_eq_mul, ennreal.mul_pos_iff, and_iff_right],
-    rw pos_iff_ne_zero,
-    exact hnrc, },
-  have : function.surjective ((•) r : (ℕ → set E) → (ℕ → set E)),
-  { intro x,
-    exact ⟨r⁻¹ • x, funext $ λ i, smul_inv_smul₀ hr _⟩ },
-  symmetry,
-  refine this.infi_congr _ (λ t, _),
-  simp_rw [pi.smul_apply, smul_set_nonempty, ←smul_set_Union, ediam_smul₀, ennreal.smul_def,
-    smul_eq_mul, ennreal.mul_le_mul_left hnrc ennreal.coe_ne_top,
-    set_smul_subset_set_smul_iff₀ hr],
-end
-
 
 namespace measure_theory
 

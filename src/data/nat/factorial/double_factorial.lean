117f06c1168570fc845c4e3d82b202ed768d173f
Jake Levinson <levinson.jake@gmail.com>
2023-05-12 10:03:24 -0700

    typesetting and name alignment

--- src/data/nat/factorial/double_factorial.lean
+++ src/data/nat/factorial/double_factorial.lean
@@ -10,7 +10,7 @@ import tactic.ring
 # Double factorials
 
 This file defines the double factorial,
-  `n ‼ := n * (n - 2) * (n - 4) * ...`.
+  `n‼ := n * (n - 2) * (n - 4) * ...`.
 
 ## Main declarations
 
@@ -29,42 +29,42 @@ namespace nat
 -- This notation is `\!!` not two !'s
 localized "notation (name := nat.double_factorial) n `‼`:10000 := nat.double_factorial n" in nat
 
-lemma double_factorial_add_two (n : ℕ) : (n + 2) ‼ = (n + 2) * n ‼ := rfl
+lemma double_factorial_add_two (n : ℕ) : (n + 2)‼ = (n + 2) * n‼ := rfl
 
-lemma double_factorial_def' (n : ℕ) : (n + 1) ‼ = (n + 1) * (n - 1) ‼ := by { cases n; refl }
+lemma double_factorial_def' (n : ℕ) : (n + 1)‼ = (n + 1) * (n - 1)‼ := by { cases n; refl }
 
-lemma factorial_eq_mul_double_factorial : ∀ (n : ℕ), (n + 1) ! = (n + 1) ‼ * n ‼
+lemma factorial_eq_mul_double_factorial : ∀ (n : ℕ), (n + 1)! = (n + 1)‼ * n‼
 | 0 := rfl
 | (k + 1) := begin
-  rw [double_factorial_def, factorial, factorial_eq_mul_double_factorial,
-      mul_comm _ (k ‼), mul_assoc]
+  rw [double_factorial_add_two, factorial, factorial_eq_mul_double_factorial,
+      mul_comm _ (k‼), mul_assoc]
 end
 
 lemma double_factorial_two_mul :
-  ∀ (n : ℕ), (2 * n) ‼ = 2^n * n!
+  ∀ (n : ℕ), (2 * n)‼ = 2^n * n!
 | 0       := rfl
 | (n + 1) := begin
-  rw [mul_add, mul_one, double_factorial_def, factorial, pow_succ,
-      factorial_eq_two_pow_mul_double_factorial, succ_eq_add_one],
+  rw [mul_add, mul_one, double_factorial_add_two, factorial, pow_succ,
+      double_factorial_two_mul, succ_eq_add_one],
   ring,
 end
 
 open_locale big_operators
 
 lemma double_factorial_eq_prod_even :
-  ∀ (n : ℕ), (2 * n) ‼ = ∏ i in finset.range n, (2 * (i + 1))
+  ∀ (n : ℕ), (2 * n)‼ = ∏ i in finset.range n, (2 * (i + 1))
 | 0       := rfl
 | (n + 1) := begin
-  rw [finset.prod_range_succ, ← factorial_eq_prod_even, mul_comm (2 * n) ‼,
+  rw [finset.prod_range_succ, ← double_factorial_eq_prod_even, mul_comm (2 * n)‼,
       (by ring : 2 * (n + 1) = 2 * n + 2)],
   refl,
 end
 
 lemma double_factorial_eq_prod_odd :
-  ∀ (n : ℕ), (2 * n + 1) ‼ = ∏ i in finset.range n, (2 * (i + 1) + 1)
+  ∀ (n : ℕ), (2 * n + 1)‼ = ∏ i in finset.range n, (2 * (i + 1) + 1)
 | 0       := rfl
 | (n + 1) := begin
-  rw [finset.prod_range_succ, ← factorial_eq_prod_odd, mul_comm (2 * n + 1) ‼,
+  rw [finset.prod_range_succ, ← double_factorial_eq_prod_odd, mul_comm (2 * n + 1)‼,
       (by ring : 2 * (n + 1) + 1 = (2 * n + 1) + 2)],
   refl,
 end

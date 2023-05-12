529b52fefe5cdb38de25875f2040435baeeda670
Jake Levinson <levinson.jake@gmail.com>
2023-05-11 23:07:47 -0700

    initial commit

---
+++ src/data/nat/factorial/double_factorial.lean
@@ -0,0 +1,71 @@
+/-
+Copyright (c) 2023 Jake Levinson. All rights reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
+Authors: Jake Levinson
+-/
+import data.nat.factorial.basic
+import algebra.big_operators.order
+import tactic.ring
+/-!
+# Double factorials
+
+This file defines the double factorial,
+  `n ‼ := n * (n - 2) * (n - 4) * ...`.
+
+## Main declarations
+
+* `nat.double_factorial`: The double factorial.
+-/
+
+namespace nat
+
+/-- `nat.double_factorial n` is the double factorial of `n`. -/
+@[simp] def double_factorial : ℕ → ℕ
+| 0       := 1
+| 1       := 1
+| (k + 2) := (k + 2) * double_factorial k
+
+-- This notation is `\!!` not two !'s
+notation n `‼`:10000 := double_factorial n
+
+lemma double_factorial_def (n : ℕ) : (n + 2) ‼ = (n + 2) * n ‼ := rfl
+
+lemma double_factorial_def' (n : ℕ) : (n + 1) ‼ = (n + 1) * (n - 1) ‼ := by { cases n; refl }
+
+lemma factorial_eq_mul_double_factorial : ∀ (n : ℕ), (n + 1).factorial = (n + 1) ‼ * n ‼
+| 0 := rfl
+| (k + 1) := begin
+  rw [double_factorial_def, factorial, factorial_eq_mul_double_factorial,
+      mul_comm _ (k ‼), mul_assoc]
+end
+
+lemma factorial_eq_two_pow_mul_double_factorial :
+  ∀ (n : ℕ), (2 * n) ‼ = 2^n * n.factorial
+| 0       := rfl
+| (n + 1) := begin
+  rw [mul_add, mul_one, double_factorial_def, factorial, pow_succ,
+      factorial_eq_two_pow_mul_double_factorial, succ_eq_add_one],
+  ring,
+end
+
+open_locale big_operators
+
+lemma factorial_eq_prod_even :
+  ∀ (n : ℕ), (2 * n) ‼ = ∏ i in finset.range n, (2 * (i + 1))
+| 0       := rfl
+| (n + 1) := begin
+  rw [finset.prod_range_succ, ← factorial_eq_prod_even, mul_comm (2 * n) ‼,
+      (by ring : 2 * (n + 1) = 2 * n + 2)],
+  refl,
+end
+
+lemma factorial_eq_prod_odd :
+  ∀ (n : ℕ), (2 * n + 1) ‼ = ∏ i in finset.range n, (2 * (i + 1) + 1)
+| 0       := rfl
+| (n + 1) := begin
+  rw [finset.prod_range_succ, ← factorial_eq_prod_odd, mul_comm (2 * n + 1) ‼,
+      (by ring : 2 * (n + 1) + 1 = (2 * n + 1) + 2)],
+  refl,
+end
+
+end nat

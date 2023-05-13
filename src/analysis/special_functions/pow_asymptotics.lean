baeff7ddcd5388d03103ecb56ae2f727ff702011
David Loeffler <d.loeffler.01@cantab.net>
2023-05-13 11:47:17 -0700

    fix header

--- src/analysis/special_functions/pow_asymptotics.lean
+++ src/analysis/special_functions/pow_asymptotics.lean
@@ -1,7 +1,8 @@
 /-
-Copyright (c) 2023 . All rights reserved.
+Copyright (c) 2018 Chris Hughes. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
-Authors:
+Authors: Chris Hughes, Abhimanyu Pallavi Sudhir, Jean Lo, Calle Sönne, Sébastien Gouëzel,
+  Rémy Degenne, David Loeffler
 -/
 import analysis.special_functions.pow_nnreal
 

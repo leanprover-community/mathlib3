4f49d409f9fda55fe12e08bdc23d07bedbf7e844
Heather Macbeth <25316162+hrmacbeth@users.noreply.github.com>
2023-05-14 19:46:33 -0400

    strongly_measurable.basic

--- src/measure_theory/function/strongly_measurable/basic.lean
+++ src/measure_theory/function/strongly_measurable/basic.lean
@@ -4,7 +4,7 @@ Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Rémy Degenne, Sébastien Gouëzel
 -/
 import analysis.normed_space.bounded_linear_maps
-import topology.metric_space.metrizable
+import measure_theory.constructions.borel_space.metrizable
 import measure_theory.integral.lebesgue
 import measure_theory.function.simple_func_dense
 

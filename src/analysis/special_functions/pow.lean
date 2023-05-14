68087966cc25c31a73fbd9f313e85ba43bc89945
David Loeffler <d.loeffler.01@cantab.net>
2023-05-13 17:58:07 -0700

    kill old file

--- src/analysis/special_functions/pow.lean
+++
@@ -1,8 +0,0 @@
-/-
-Copyright (c) 2018 Chris Hughes. All rights reserved.
-Released under Apache 2.0 license as described in the file LICENSE.
-Authors: Chris Hughes, Abhimanyu Pallavi Sudhir, Jean Lo, Calle Sönne, Sébastien Gouëzel,
-  Rémy Degenne, David Loeffler
--/
-import analysis.special_functions.pow_continuity
-import analysis.special_functions.pow_tactic

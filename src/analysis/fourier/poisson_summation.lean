4c49928cce7362eb8dcdb746596d28ace3cd1339
David Loeffler <d.loeffler.01@cantab.net>
2023-05-14 00:07:25 -0700

    final bug?

--- src/analysis/fourier/poisson_summation.lean
+++ src/analysis/fourier/poisson_summation.lean
@@ -7,6 +7,7 @@ import analysis.fourier.add_circle
 import analysis.fourier.fourier_transform
 import analysis.p_series
 import analysis.schwartz_space
+import analysis.special_functions.pow_tactic
 
 /-!
 # Poisson's summation formula

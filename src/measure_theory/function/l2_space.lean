cb8547c248552cc4e4b9b8d9f17dde0d878593b0
Heather Macbeth <25316162+hrmacbeth@users.noreply.github.com>
2023-05-15 11:13:28 -0400

    cleanup

--- src/measure_theory/function/l2_space.lean
+++ src/measure_theory/function/l2_space.lean
@@ -3,8 +3,8 @@ Copyright (c) 2021 Rémy Degenne. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Rémy Degenne
 -/
-import measure_theory.integral.set_integral
 import measure_theory.function.strongly_measurable.inner
+import measure_theory.integral.set_integral
 
 /-! # `L^2` space
 

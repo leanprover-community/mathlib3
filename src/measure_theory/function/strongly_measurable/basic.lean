bf6a01357ff5684b1ebcd0f1a13be314fc82c0bf
Heather Macbeth <25316162+hrmacbeth@users.noreply.github.com>
2023-05-15 04:39:37 +0000

    chore(measure_theory/{constructions/borel_space,integral/lebesgue}): split (#19015)
    Remove dependence on
    ```text
    import analysis.complex.basic
    import analysis.normed_space.basic
    import topology.instances.add_circle
    import topology.metric_space.metrizable
    ```
    from `measure_theory.constructions.borel_space` (in particular this gets rid of the `operator_norm` dependency, which is currently the big blocker) and `measure_theory.integral.lebesgue`.

--- src/measure_theory/function/strongly_measurable/basic.lean
+++ src/measure_theory/function/strongly_measurable/basic.lean
@@ -3,8 +3,9 @@ Copyright (c) 2021 Rémy Degenne. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Rémy Degenne, Sébastien Gouëzel
 -/
+import analysis.normed_space.finite_dimension
 import analysis.normed_space.bounded_linear_maps
-import topology.metric_space.metrizable
+import measure_theory.constructions.borel_space.metrizable
 import measure_theory.integral.lebesgue
 import measure_theory.function.simple_func_dense
 

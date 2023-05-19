63ad26746594e76f41e112e3a52a2efeecaf9798
Yaël Dillies <yael.dillies@gmail.com>
2023-05-19 19:45:45 +0100

    Update src/combinatorics/simple_graph/regularity/increment.lean

--- src/combinatorics/simple_graph/regularity/increment.lean
+++ src/combinatorics/simple_graph/regularity/increment.lean
@@ -3,8 +3,8 @@ Copyright (c) 2022 Yaël Dillies, Bhavik Mehta. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Yaël Dillies, Bhavik Mehta
 -/
-import .chunk
-import .energy
+import combinatorics.simple_graph.regularity.chunk
+import combinatorics.simple_graph.regularity.energy
 
 /-!
 # Increment partition for Szemerédi Regularity Lemma

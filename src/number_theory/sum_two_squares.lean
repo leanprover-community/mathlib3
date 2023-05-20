97e544d66bd5bb2e44c855eea6be7dea5f1f142e
Michael Stoll <Michael.Stoll@uni-bayreuth.de>
2023-05-20 22:22:39 +0200

    golf sq_add_sq

--- src/number_theory/sum_two_squares.lean
+++ src/number_theory/sum_two_squares.lean
@@ -30,10 +30,8 @@ of two squares. Also known as **Fermat's Christmas theorem**. -/
 theorem nat.prime.sq_add_sq {p : ℕ} [pp : fact (nat.prime p)] (hp : p % 4 ≠ 3) :
   ∃ (a b : ℕ), a ^ 2 + b ^ 2 = p :=
 begin
-  unfreezingI {rcases pp.1.eq_two_or_odd with rfl | h},
-  { exact ⟨1, 1, rfl⟩, },
-  { apply sq_add_sq_of_nat_prime_of_not_irreducible p,
-    rwa [principal_ideal_ring.irreducible_iff_prime, prime_iff_mod_four_eq_three_of_nat_prime p], }
+  apply sq_add_sq_of_nat_prime_of_not_irreducible p,
+  rwa [principal_ideal_ring.irreducible_iff_prime, prime_iff_mod_four_eq_three_of_nat_prime p],
 end
 
 end Fermat

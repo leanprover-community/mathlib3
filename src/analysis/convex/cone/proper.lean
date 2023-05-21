c6c27fd9249de90ba430b5b3bb9d8cbd1363c750
Apurva Nakade <apurvnakade@gmail.com>
2023-05-20 20:02:22 -0500

    Fix merge error

--- src/analysis/convex/cone/proper.lean
+++ src/analysis/convex/cone/proper.lean
@@ -3,8 +3,8 @@ Copyright (c) 2022 Apurva Nakade All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Apurva Nakade
 -/
-import analysis.convex.cone.basic
-import topology.algebra.monoid
+
+import analysis.convex.cone.dual
 
 /-!
 

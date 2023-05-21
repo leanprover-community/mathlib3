46863b21777771bb1f518fecc96a1dff2a0aacc5
Apurva Nakade <apurvnakade@gmail.com>
2023-05-20 20:03:25 -0500

    Whitespace

--- src/analysis/convex/cone/proper.lean
+++ src/analysis/convex/cone/proper.lean
@@ -3,7 +3,6 @@ Copyright (c) 2022 Apurva Nakade All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Apurva Nakade
 -/
-
 import analysis.convex.cone.dual
 
 /-!

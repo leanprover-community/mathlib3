612de6ab9fd103beeca56821dd75c30c59f213ff
Eric Wieser <wieser.eric@gmail.com>
2023-05-22 22:12:51 +0000

    add author

--- src/analysis/special_functions/exponential.lean
+++ src/analysis/special_functions/exponential.lean
@@ -1,7 +1,7 @@
 /-
 Copyright (c) 2021 Anatole Dedecker. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
-Authors: Anatole Dedecker
+Authors: Anatole Dedecker, Eric Wieser
 -/
 import analysis.normed_space.exponential
 import analysis.calculus.fderiv_analytic

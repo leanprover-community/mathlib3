b9956e0c88d317ff4db1006baeb5a9e1e9959d09
RemyDegenne <Remydegenne@gmail.com>
2023-05-22 21:03:35 +0200

    fix import

--- src/probability/kernel/with_density.lean
+++ src/probability/kernel/with_density.lean
@@ -4,6 +4,7 @@ Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Rémy Degenne
 -/
 import probability.kernel.measurable_integral
+import measure_theory.integral.set_integral
 
 /-!
 # With Density

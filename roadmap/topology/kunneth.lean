6b5ca9c267180198097c5bc7c48674fe4e0af1fc
Heather Macbeth <25316162+hrmacbeth@users.noreply.github.com>
2023-05-23 09:18:29 -0600

    topology on projective space

--- roadmap/topology/kunneth.lean
+++ roadmap/topology/kunneth.lean
@@ -55,12 +55,11 @@ abbreviation H := (λ n, singular_homology n ℤ)
 # Let's think about computing examples!
 -/
 
--- TODO this needs the correct instances!
-instance (K V : Type*) [division_ring K] [add_comm_group V] [module K V] :
+instance (K V : Type*) [division_ring K] [add_comm_group V] [module K V] [topological_space V] :
   topological_space (ℙ K V) :=
-sorry
+topological_space.coinduced (projectivization.mk' K) infer_instance
 
-def ℝℙ2 : Top.{0} := Top.of (ℙ ℝ (fin 2 → ℝ))
+def ℝℙ2 : Top.{0} := Top.of (ℙ ℝ (fin 3 → ℝ))
 
 namespace ℝℙ2
 

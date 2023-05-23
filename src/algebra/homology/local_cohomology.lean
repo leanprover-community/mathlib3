69a3a8bc42ebdbaa25d42f385fd7c920debd4dcd
Emily Witt <emwitt@gmail.com>
2023-05-23 11:08:57 -0600

    Clean-up, references, and doc-strings

--- src/algebra/homology/local_cohomology.lean
+++ src/algebra/homology/local_cohomology.lean
@@ -12,9 +12,38 @@ import category_theory.limits.final
 /-!
 # Local cohomology.
 
-We just give one definition, deferring for later the other equivalent definitions,
-and basic properties.
+This file defines the `i`-th local cohomology module of an `R`-module `M` with support in an
+ideal `I` of `R`, where `R` is a commutative ring, as the direct limit of Ext modules:
+
+Given a collection of ideals cofinal with the powers of `I`, consider the directed system of
+quotients of `R` by these ideals, and take the direct limit of the system induced on the `i`-th
+Ext into `M`.  One can, of course, take the collection to simply be the integral powers of `I`.
+
+## References
+
+* [M. Hochster, *Local cohomology*][hochsterunpublished]
+  <https://dept.math.lsa.umich.edu/~hochster/615W22/lcc.pdf>
+* [R. Hartshorne, *Local cohomology: A seminar given by A. Grothendieck*][hartshorne61]
+* [M. Brodmann and R. Sharp, *Local cohomology: An algebraic introduction with geometric
+  applications*][brodmannsharp13]
+* [S. Iyengar, G. Leuschke, A. Leykin, Anton, C. Miller, E. Miller, A. Singh, U. Walther,
+  *Twenty-four hours of local cohomology*][iyengaretal13]
+
+## Tags
+
+local cohomology, local cohomology modules
+
+## Future work
+
+* Prove that this definition is equivalent to:
+    * the right-derived functor definition
+    * the characterization as the limit of Koszul homology
+    * the characterization as the cohomology of a Cech-like complex
+* Prove that local cohomology depends only on the radical of the ideal
+* Establish long exact sequence(s) in local cohomology
 -/
+
+
 
 open opposite
 open category_theory
@@ -30,108 +59,38 @@ variables {D : Type} [category.{0} D] (I : Dᵒᵖ ⥤ ideal R)
 
 local attribute [ext] quiver.hom.unop_inj
 
+/--  The directed system of `R`-modules of the form `R/J`, where `J` is an ideal of `R`,
+determined by the functor `I`, represented as a functor  -/
 def ring_mod_ideals : D ⥤ (Module.{0} R)ᵒᵖ :=
-{
-  obj := λ t, op $ Module.of R $ R ⧸ (I.obj (op t)),
+{ obj := λ t, op $ Module.of R $ R ⧸ (I.obj (op t)),
   map := λ s t w, quiver.hom.op $
-    submodule.mapq (I.obj (op t)) (I.obj (op s)) (linear_map.id)
-    $ (I.map w.op).down.down
-}
+  submodule.mapq (I.obj (op t)) (I.obj (op s)) (linear_map.id)
+  $ (I.map w.op).down.down }
 
-/-- The diagram we will take the colimit of to define local cohomology -/
+/-- The diagram we will take the colimit of to define local cohomology, corresponding to the
+directed system determined by the functor `I` -/
 def local_cohomology_diagram (i : ℕ) :
    D ⥤ Module.{0} R ⥤ Module.{0} R :=
 ring_mod_ideals R I ⋙ Ext R (Module.{0} R) i
 
-def local_cohomology_ideals (i : ℕ) : Module.{0} R ⥤ Module.{0} R :=
+/-- local_cohomology `R` `I` `i` is `i`-th the local cohomology module of a module `M` over a
+commutative ring `R` with support in an ideal whose powers are cofinal with a collection of ideals
+of `R` that is represented as a functor `I` -/
+def local_cohomology (i : ℕ) : Module.{0} R ⥤ Module.{0} R :=
 colimit (local_cohomology_diagram R I i)
 
+/-- The functor sending a natural number `i` to the `i`-th power of the ideal `J` -/
 def ideal_powers (J : ideal R) : ℕᵒᵖ ⥤ ideal R := {
   obj := λ t, J^(unop t),
-  map := λ s t w, ⟨⟨ideal.pow_le_pow w.unop.down.down⟩⟩,
-}
+  map := λ s t w, ⟨⟨ideal.pow_le_pow w.unop.down.down⟩⟩, }
 
-def local_cohomology_powers (J : ideal R) :=
-  local_cohomology_ideals R (ideal_powers R J)
+/-- local_cohomology_powers `R` `J` `i` is `i`-th the local cohomology module of a module `M` over
+a commutative ring `R` with support in the ideal `J` of `R` -/
+def local_cohomology_powers (J : ideal R) (M : Module.{0} R) :=
+  local_cohomology R (ideal_powers R J)
 
 end local_cohomology
 
-section cofinal
-
-variables {E ι : Type} [category.{0} E]
-  {D : ι → Type} [∀ i, category.{0} (D i)] (I : Π (i : ι), D i ⥤ E)
-
-def train_tracks (I : Π (i : ι), D i ⥤ E) : Type :=
-  induced_category E (λ x : Σ i, D i, (I x.1).obj x.2)
-
-instance : category (train_tracks I) := begin
-  dsimp [train_tracks],
-  apply_instance,
-end
-
-namespace train_tracks
-
-def inclusion (i) : D i ⥤ train_tracks I := {
-  obj := λ x, ⟨i, x⟩,
-  map := λ x y f, (I i).map f,
-}
-
-end train_tracks
-end cofinal
-
-
-section
-
-variables {D E : Type} [category.{0} D] [category.{0} E]
-  (F : D ⥤ E) [is_filtered D] [quiver.is_thin E]
-
-lemma foo (w : ∀ e, ∃ d, nonempty (e ⟶ F.obj d)) : ∀ e, nonempty (structured_arrow e F) :=
-λ e, ⟨structured_arrow.mk (classical.some_spec (w e)).some⟩
-
-lemma zigzag_of_cofork {J : Type*} [category J] {j₁ j₂ j₃ : J} (f : j₁ ⟶ j₃) (g : j₂ ⟶ j₃) :
-  zigzag j₁ j₂ :=
-begin
-show_term {
-  apply relation.refl_trans_gen.tail,
-  apply relation.refl_trans_gen.tail,
-  apply relation.refl_trans_gen.refl,
-  left,
-  split, assumption,
-  right,
-  split, assumption,
-},
-end
-
-lemma final_of_filtered_of_thin_of_exists (w : ∀ e, ∃ d, nonempty(e ⟶ F.obj d)) : F.final :=
-{ out := λ e, @zigzag_is_connected _ _ (foo _  w e)
-  begin
-    intros j1 j2,
-    have f := is_filtered.left_to_max j1.right j2.right,
-    have g := is_filtered.right_to_max j1.right j2.right,
-    apply @zigzag_of_cofork _ _ _ _ (structured_arrow.mk _),
-    show D, exact (is_filtered.max j1.right j2.right),
-    show e ⟶ _, exact j1.hom ≫ F.map f,
-    { fapply structured_arrow.hom_mk,
-      exact f,
-      apply subsingleton.elim, },
-    { fapply structured_arrow.hom_mk,
-      exact g,
-      apply subsingleton.elim, },
-  end, }
 
 
 end
-
-
-
-
-
-
---   obj := λ t, I^(unop t),
---   map := λ s t w, begin
---     have := ideal.pow_le_pow w.unop.down.down,
---   end
--- }
-
-end
--- def local_cohomology_diagram' (I )

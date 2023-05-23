9e2d1f2ee7f64cf72f8ed78993d4bf5ddd76ad9e
Jireh Loreaux <loreaujy@gmail.com>
2023-05-23 13:34:16 -0500

    add copyright headers

--- src/algebra/star/order.lean
+++ src/algebra/star/order.lean
@@ -1,3 +1,9 @@
+/-
+Copyright (c) 2023 Scott Morrison. All rights reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
+Authors: Scott Morrison
+-/
+
 import algebra.star.basic
 import group_theory.submonoid.basic
 
--- src/data/rat/star.lean
+++ src/data/rat/star.lean
@@ -1,3 +1,9 @@
+/-
+Copyright (c) 2023 Jireh Loreaux. All rights reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
+Authors: Jireh Loreaux
+-/
+
 import algebra.star.order
 import data.rat.lemmas
 import group_theory.submonoid.membership

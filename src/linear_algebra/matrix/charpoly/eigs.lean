3d085b4bad81ecceac7f3e063540e56c95ffe6e4
Mohanad Ahmed <m.a.m.elhassan@gmail.com>
2023-05-24 19:46:55 +0300

    Name reversal and other comments by eric on the PR

--- src/linear_algebra/matrix/charpoly/eigs.lean
+++ src/linear_algebra/matrix/charpoly/eigs.lean
@@ -13,13 +13,19 @@ import algebra.char_p.two
 
 
 In algebraically closed fields, we show:
-* `matrix.det_eq_prod_roots_charpoly`: the determinant is the product of the roots of the characteristic
+* `matrix.prod_roots_charpoly_eq_det`: the determinant is the product of the roots of the characteristic
 polynomial.
-* `matrix.trace_eq_sum_roots_charpoly`: the trace is the sum of the roots of the characteristic
+* `matrix.sum_roots_charpoly_eq_trace`: the trace is the sum of the roots of the characteristic
 polynomial.
 
 Note that over other fields such as ` ℝ`, these results can be used by using `A.map (algebra_map ℝ ℂ)` as the matrix,
 and then applying `ring_hom.map_det`.
+
+The two lemmas `matrix.prod_roots_charpoly_eq_det` and `matrix.sum_roots_charpoly_eq_trace` are more commonly stated as
+trace is the sum of eigenvalues and determinant is the product of eigenvalues. Mathlib has already defined eigenvalues in
+`linear_algebra.eigenspace` as the roots of the minimal polynomial of a linear endomorphism. These do not have correct
+multiplicity and cannot be used in the theorems above. Hence we express these theorems in terms of the roots of the
+characteristic polynomial directly.
 -/
 variables {n: Type*}[fintype n][decidable_eq n]
 variables {R: Type*}[field R][is_alg_closed R]
@@ -32,23 +38,26 @@ lemma prod_roots_charpoly_eq_det (A : matrix n n R) :
   (matrix.charpoly A).roots.prod = A.det :=
 begin
   casesI is_empty_or_nonempty n,
+  { rw matrix.charpoly,
+    repeat {rw det_eq_one_of_card_eq_zero (fintype.card_eq_zero_iff.2 h)},
+    simp only [polynomial.roots_one, multiset.empty_eq_zero, multiset.prod_zero], },
   { have hdeg := charpoly_nat_degree_eq_dim A,
     rw [det_eq_sign_charpoly_coeff, ← hdeg,
       polynomial.prod_roots_eq_coeff_zero_of_monic_of_split A.charpoly_monic
         (is_alg_closed.splits _),
-      ← mul_assoc, ← pow_two, pow_right_comm, neg_one_sq, one_pow, one_mul] },
-  { rw not_nonempty_iff at hn,
-    rw matrix.charpoly,
-    repeat {rw det_eq_one_of_card_eq_zero (fintype.card_eq_zero_iff.2 hn)},
-    simp only [polynomial.roots_one, multiset.empty_eq_zero, multiset.prod_zero], },
+      ← mul_assoc, ← pow_two, pow_right_comm, neg_one_sq, one_pow, one_mul], },
 end
 
-lemma trace_eq_sum_roots_charpoly (A: matrix n n R) :
+lemma sum_roots_charpoly_eq_trace (A: matrix n n R) :
   A.trace = (matrix.charpoly A).roots.sum :=
 begin
   casesI is_empty_or_nonempty n,
-  { haveI hn1 := hn,
-    apply_fun (has_neg.neg),
+  { rw [matrix.trace, fintype.sum_empty],
+    rw matrix.charpoly,
+    rw det_eq_one_of_card_eq_zero (fintype.card_eq_zero_iff.2 h),
+    rw polynomial.roots_one,
+    simp only [multiset.empty_eq_zero, multiset.sum_zero],},
+  { apply_fun (has_neg.neg),
     rw ← polynomial.sum_roots_eq_next_coeff_of_monic_of_split ,
     rw trace_eq_neg_charpoly_coeff, rw next_coeff,
     rw neg_neg, rw charpoly_nat_degree_eq_dim,
@@ -60,11 +69,4 @@ begin
     apply matrix.charpoly_monic,
     exact is_alg_closed.splits A.charpoly,
     rintros a x hax, rwa neg_inj at hax, },
-  { rw not_nonempty_iff at hn,
-    rw [matrix.trace, fintype.sum_empty _],
-    rw matrix.charpoly,
-    rw det_eq_one_of_card_eq_zero (fintype.card_eq_zero_iff.2 hn),
-    rw polynomial.roots_one,
-    simp only [multiset.empty_eq_zero, multiset.sum_zero],
-    exact hn, },
 end

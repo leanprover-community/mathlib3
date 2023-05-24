ffea39ab303cd5d31ad3abc99481d93830aefdf7
dtumad <dtumad@gmail.com>
2023-05-24 01:12:55 -0500

    seperate from pmf stuff

--- src/probability/probability_mass_function/uniform.lean
+++ src/probability/probability_mass_function/uniform.lean
@@ -4,26 +4,22 @@ Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Devon Tuma
 -/
 import probability.probability_mass_function.constructions
-import data.multiset.dedup
 
 /-!
 # Uniform Probability Mass Functions
 
 This file defines a number of uniform `pmf` distributions from various inputs,
-uniformly drawing from the corresponding object.
+  uniformly drawing from the corresponding object.
 
 `pmf.uniform_of_finset` gives each element in the set equal probability,
-with `0` probability for elements not in the set.
+  with `0` probability for elements not in the set.
 
 `pmf.uniform_of_fintype` gives all elements equal probability,
-equal to the inverse of the size of the `fintype`.
+  equal to the inverse of the size of the `fintype`.
 
 `pmf.of_multiset` draws randomly from the given `multiset`, treating duplicate values as distinct.
-Each probability is given by the count of the element divided by the size of the `multiset`.
+  Each probability is given by the count of the element divided by the size of the `multiset`
 
-`pmf.of_list` draws randomly from the given `list`, treating duplicate values as distinct.
-Each probability is given by the count of the element divided by the size of the `list`.
-Requires a proof that the given `list` is nonempty to avoid division by `0`.
 -/
 
 namespace pmf
@@ -126,8 +122,6 @@ end uniform_of_fintype
 
 section of_multiset
 
-open multiset
-
 /-- Given a non-empty multiset `s` we construct the `pmf` which sends `a` to the fraction of
   elements in `s` that are `a`. -/
 def of_multiset (s : multiset α) (hs : s ≠ 0) : pmf α :=
@@ -136,9 +130,9 @@ def of_multiset (s : multiset α) (hs : s ≠ 0) : pmf α :=
       by simp_rw [ennreal.div_eq_inv_mul, ennreal.tsum_mul_left]
     ... = s.card⁻¹ * ∑ b in s.to_finset, (s.count b : ℝ≥0∞) :
       congr_arg (λ x, s.card⁻¹ * x) (tsum_eq_sum $ λ a ha, (nat.cast_eq_zero.2 $
-        by rwa [count_eq_zero, ← mem_to_finset]))
-    ... = 1 : by rw [← nat.cast_sum, to_finset_sum_count_eq s, ennreal.inv_mul_cancel
-      (nat.cast_ne_zero.2 (hs ∘ card_eq_zero.1)) (ennreal.nat_ne_top _)] ) ⟩
+        by rwa [multiset.count_eq_zero, ← multiset.mem_to_finset]))
+    ... = 1 : by rw [← nat.cast_sum, multiset.to_finset_sum_count_eq s, ennreal.inv_mul_cancel
+      (nat.cast_ne_zero.2 (hs ∘ multiset.card_eq_zero.1)) (ennreal.nat_ne_top _)] ) ⟩
 
 variables {s : multiset α} (hs : s ≠ 0)
 
@@ -152,21 +146,7 @@ by simp
 
 lemma of_multiset_apply_of_not_mem {a : α} (ha : a ∉ s) : of_multiset s hs a = 0 :=
 by simpa only [of_multiset_apply, ennreal.div_zero_iff, nat.cast_eq_zero,
-  count_eq_zero, ennreal.nat_ne_top, or_false] using ha
-
-/-- After removing duplicate elements from `s : multiset α`, the uniform distribution on `s`
-is the same as the uniform distribution on `s.to_finset`. -/
-@[simp] lemma of_multiset_dedup_eq_uniform_of_finset (hs : s.dedup ≠ 0) : of_multiset s.dedup hs =
-  uniform_of_finset s.to_finset ((to_finset_nonempty).2 (hs ∘ dedup_eq_zero.2)) :=
-pmf.ext (λ x, by simp only [of_multiset_apply, uniform_of_finset_apply, card_to_finset,
-  count_eq_of_nodup (s.nodup_dedup), nat.cast_ite, algebra_map.coe_one, div_eq_mul_inv,
-  algebra_map.coe_zero, ite_mul, one_mul, zero_mul, mem_dedup, mem_to_finset])
-
-lemma of_multiset_eq_uniform_of_finset_of_nodup (hs' : s.nodup) :
-  of_multiset s hs = uniform_of_finset s.to_finset (to_finset_nonempty.2 hs) :=
-calc of_multiset s hs = of_multiset s.dedup (hs ∘ dedup_eq_zero.1) : by simp [dedup_eq_self.2 hs']
-  ... = uniform_of_finset s.to_finset (to_finset_nonempty.2 hs) :
-    of_multiset_dedup_eq_uniform_of_finset _
+  multiset.count_eq_zero, ennreal.nat_ne_top, or_false] using ha
 
 section measure
 
@@ -189,62 +169,5 @@ end
 end measure
 
 end of_multiset
-
-section of_list
-
-/-- Given a non-empty list `l` construct the `pmf` that chooses an element in `l` uniformly.
-This requires a proof `h : ¬ l.empty` to ensure the total probability is `1`. -/
-def of_list (l : list α) (h : ¬ l.empty) : pmf α :=
-of_multiset (quotient.mk l) (h ∘ (multiset.coe_eq_zero_iff_empty l).1)
-
-variables (l : list α) (h : ¬ l.empty)
-
-@[simp] lemma support_of_list : (of_list l h).support = {x | x ∈ l} := by simp [of_list]
-
-lemma mem_support_of_list_iff (x : α) : x ∈ (of_list l h).support ↔ x ∈ l := by simp
-
-@[simp] lemma of_list_apply (x : α) : of_list l h x = l.count x / l.length := by simp [of_list]
-
-lemma of_list_apply_of_not_mem {x : α} (hx : x ∉ l) : of_list l h x = 0 := by simpa
-
-/-- After removing duplicate elements from `l : list α`, the uniform distribution on `l`
-is the same as the uniform distribution on `l.to_finset`. -/
-lemma of_list_dedup_eq_uniform_of_finset (hl : ¬ l.dedup.empty) : of_list l.dedup hl =
-  uniform_of_finset l.to_finset (l.to_finset_nonempty_iff.2 $ hl ∘ l.dedup_empty.2) :=
-begin
-  refine trans _ (of_multiset_dedup_eq_uniform_of_finset $ λ h, hl _),
-  { have : l = [] := by rwa [multiset.dedup_eq_zero, multiset.coe_eq_zero] at h,
-    simp only [this, list.dedup_nil, list.empty, coe_sort_tt] },
-  { exact pmf.ext (λ x, by simp only [of_list_apply, multiset.coe_dedup,
-      of_multiset_apply, multiset.coe_count, multiset.coe_card]) }
-end
-
-lemma of_list_eq_uniform_of_finset (hl : l.nodup) :
-  of_list l h = uniform_of_finset l.to_finset (l.to_finset_nonempty_iff.2 h) :=
-of_multiset_eq_uniform_of_finset_of_nodup _ hl
-
-section measure
-
-@[simp] lemma to_outer_measure_of_list_apply (t : set α) :
-  (of_list l h).to_outer_measure t = l.countp (∈ t) / l.length :=
-begin
-  suffices : ∑ x in l.to_finset, ((l.filter (∈ t)).count x : ℝ≥0∞) = l.countp (∈ t),
-  { rw [of_list, to_outer_measure_of_multiset_apply, multiset.quot_mk_to_coe, multiset.coe_card],
-    refine congr_arg (λ x, x / (l.length : ℝ≥0∞)) (trans (tsum_eq_sum (λ x hx, _)) this),
-    have : x ∉ l := λ h, hx (list.mem_to_finset.2 h),
-    simp only [nat.cast_eq_zero, list.count_eq_zero, list.mem_filter, not_and_distrib,
-      multiset.mem_coe, (λ h, hx (list.mem_to_finset.2 h) : x ∉ l), not_false_iff, true_or] },
-  rw [← finset.sum_filter_count_eq_countp, nat.cast_sum, finset.sum_filter],
-  exact finset.sum_congr rfl (λ x hx, by by_cases ht : x ∈ t; simp [ht]),
-end
-
-@[simp] lemma to_measure_of_list_apply [measurable_space α] (t : set α) (ht : measurable_set t) :
-  (of_list l h).to_measure t = l.countp (∈ t) / l.length :=
-(to_measure_apply_eq_to_outer_measure_apply _ t ht).trans
-  (to_outer_measure_of_list_apply l h t)
-
-end measure
-
-end of_list
 
 end pmf

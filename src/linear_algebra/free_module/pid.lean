131c9a88d294c85a0ccfb097e2416cfd95b1437f
David Kurniadi Angdinata <dka31@cantab.ac.uk>
2023-05-27 19:53:47 +0100

    Generalise instance
    Co-authored-by: Junyan Xu <junyanxumath@gmail.com>

--- src/linear_algebra/free_module/pid.lean
+++ src/linear_algebra/free_module/pid.lean
@@ -608,9 +608,9 @@ begin
   simp [hi]
 end
 
-instance (F : Type u) [comm_ring F] [algebra F R] (b : basis ι R S) {I : ideal S} (hI : I ≠ ⊥) :
-  Π i, module F (R ⧸ ideal.span ({I.smith_coeffs b hI i} : set R)) :=
-λ _, quotient.module' _
+instance (F : Type u) [comm_ring F] [algebra F R] (b : basis ι R S) {I : ideal S} (hI : I ≠ ⊥) (i) :
+  module F (R ⧸ ideal.span ({I.smith_coeffs b hI i} : set R)) :=
+by apply_instance -- quotient.module' _
 
 end ideal
 

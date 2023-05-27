d4cc593fa566a4c3e5ced688f294e420af05fbc3
Yury G. Kudryashov <urkud@urkud.name>
2023-05-27 12:13:14 -0500

    Fix some dates

--- src/analysis/calculus/deriv/linear.lean
+++ src/analysis/calculus/deriv/linear.lean
@@ -1,5 +1,5 @@
 /-
-Copyright (c) 2023 Gabriel Ebner. All rights reserved.
+Copyright (c) 2019 Gabriel Ebner. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Gabriel Ebner, Yury Kudryashov
 -/

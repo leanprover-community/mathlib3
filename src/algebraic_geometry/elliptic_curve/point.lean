dee7dcd7a3070c699e50ecb258539e964d07c773
David Kurniadi Angdinata <dka31@cantab.ac.uk>
2023-05-28 19:25:27 +0100

    Remove redundant auxiliary file

--- src/algebraic_geometry/elliptic_curve/point.lean
+++ src/algebraic_geometry/elliptic_curve/point.lean
@@ -4,7 +4,6 @@ Released under Apache 2.0 license as described in the file LICENSE.
 Authors: David Kurniadi Angdinata
 -/
 
-import algebraic_geometry.elliptic_curve.auxiliary
 import algebraic_geometry.elliptic_curve.weierstrass
 import ring_theory.class_group
 

849c26517ccd99fd60fdc9838a7fb2b10fc776d0
RemyDegenne <Remydegenne@gmail.com>
2023-05-29 11:58:26 +0200

    remove strongly_measurable assumption

--- src/probability/kernel/condexp.lean
+++ src/probability/kernel/condexp.lean
@@ -8,13 +8,15 @@ import probability.kernel.cond_distrib
 /-!
 # Kernel associated with a conditional expectation
 
-We define `condexp_kernel μ m`, a kernel from `Ω` to `Ω` such that for all integrable, strongly
-measurable `f`, `μ[f | m] =ᵐ[μ] λ ω, ∫ y, f y ∂(condexp_kernel μ m ω)`. This kernel is defined if
-`Ω` is a standard Borel space. In general, `μ⟦s | m⟧` maps a measurable set `s` to a function
-`Ω → ℝ≥0∞`, and for all `s` that map is unique up to a `μ`-null set. For all `a`, the map from sets
-to `ℝ≥0∞` that we obtain that way verifies some of the properties of a measure, but the fact that
-the `μ`-null set depends on `s` can prevent us from finding versions of the conditional expectation
-that combine into a true measure. The standard Borel space assumption on `Ω` allows us to do so.
+We define `condexp_kernel μ m`, a kernel from `Ω` to `Ω` such that for all integrable functions `f`,
+`μ[f | m] =ᵐ[μ] λ ω, ∫ y, f y ∂(condexp_kernel μ m ω)`.
+
+This kernel is defined if `Ω` is a standard Borel space. In general, `μ⟦s | m⟧` maps a measurable
+set `s` to a function `Ω → ℝ≥0∞`, and for all `s` that map is unique up to a `μ`-null set. For all
+`a`, the map from sets to `ℝ≥0∞` that we obtain that way verifies some of the properties of a
+measure, but the fact that the `μ`-null set depends on `s` can prevent us from finding versions of
+the conditional expectation that combine into a true measure. The standard Borel space assumption
+on `Ω` allows us to do so.
 
 ## Main definitions
 
@@ -48,16 +50,36 @@ def condexp_kernel (μ : measure Ω) [is_finite_measure μ] (m : measurable_spac
 
 /-- The conditional expectation of `f` with respect to a σ-algebra `m` is almost everywhere equal to
 the integral `∫ y, f y ∂(condexp_kernel μ m ω)`. -/
-lemma condexp_ae_eq_integral_condexp_kernel (hm : m ≤ mΩ)
-  {f : Ω → F} (hf : strongly_measurable f) (hf_int : integrable f μ) :
+lemma condexp_ae_eq_integral_condexp_kernel (hm : m ≤ mΩ) {f : Ω → F} (hf_int : integrable f μ) :
   μ[f | m] =ᵐ[μ] λ ω, ∫ y, f y ∂(condexp_kernel μ m ω) :=
 begin
   have hX : @measurable Ω Ω mΩ m id := measurable_id.mono le_rfl hm,
-  have hY : ae_measurable (id : Ω → Ω) μ := ae_measurable_id,
-  have hf' : @strongly_measurable (Ω × Ω) F _ (m.prod mΩ) (λ x : Ω × Ω, f x.2) :=
-    hf.comp_measurable measurable_id.snd,
+  have hY : measurable (id : Ω → Ω) := measurable_id,
+  have hXY : @ae_measurable Ω (Ω × Ω) (m.prod mΩ) mΩ (λ ω, (ω, ω)) μ :=
+    @measurable.ae_measurable Ω (Ω × Ω) mΩ (m.prod mΩ) _ μ (hX.prod_mk hY),
+  have hf' : @ae_strongly_measurable (Ω × Ω) F _ (m.prod mΩ) (λ x : Ω × Ω, f x.2)
+    (@measure.map _ _ (m.prod mΩ) _ (λ ω, (ω, ω)) μ),
+  { let f' := hf_int.1.mk f,
+    refine ⟨λ x, f' x.2, hf_int.1.strongly_measurable_mk.comp_measurable measurable_snd, _⟩,
+    suffices h : @measure.quasi_measure_preserving (Ω × Ω) Ω mΩ (m.prod mΩ) prod.snd
+      (@measure.map _ _ (m.prod mΩ) _ (λ ω, (ω, ω)) μ) μ,
+    { exact measure.quasi_measure_preserving.ae_eq h hf_int.1.ae_eq_mk, },
+    refine ⟨measurable_snd, measure.absolutely_continuous.mk (λ s hs hμs, _)⟩,
+    rw measure.map_apply _ hs,
+    swap, { exact measurable_snd, },
+    rw measure.map_apply,
+    { rw [← univ_prod, mk_preimage_prod, preimage_univ, univ_inter, preimage_id'],
+      exact hμs, },
+    { exact hX.prod_mk hY, },
+    { exact measurable_snd hs, }, },
+  have hf_int' : @integrable F _ (Ω × Ω) (m.prod mΩ) (λ x : Ω × Ω, f x.2)
+    (@measure.map _ _ (m.prod mΩ) _ (λ ω, (ω, ω)) μ),
+  { refine ⟨hf', _⟩,
+    rw [has_finite_integral, lintegral_map' hf'.ennnorm hXY],
+    exact hf_int.2, },
   rw condexp_kernel,
-  refine eventually_eq.trans _ (condexp_prod_ae_eq_integral_cond_distrib hX hY hf' hf_int),
+  refine eventually_eq.trans _
+    (condexp_prod_ae_eq_integral_cond_distrib' hX hY.ae_measurable hf_int'),
   simp only [measurable_space.comap_id, id.def],
 end
 

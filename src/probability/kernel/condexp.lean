808556ea504a46091e54ea7dfcce4b07cc55d7e9
RemyDegenne <Remydegenne@gmail.com>
2023-05-29 12:16:40 +0200

    extract lemmas

--- src/probability/kernel/condexp.lean
+++ src/probability/kernel/condexp.lean
@@ -48,38 +48,52 @@ def condexp_kernel (μ : measure Ω) [is_finite_measure μ] (m : measurable_spac
   @kernel Ω Ω m mΩ :=
 @cond_distrib Ω Ω Ω _ mΩ _ _ _ mΩ m id id μ _
 
+lemma ae_strongly_measurable_comp_snd_map_prod_mk (hm : m ≤ mΩ)
+  {f : Ω → F} (hf_int : integrable f μ) :
+  @ae_strongly_measurable (Ω × Ω) F _ (m.prod mΩ) (λ x : Ω × Ω, f x.2)
+    (@measure.map _ _ (m.prod mΩ) _ (λ ω, (ω, ω)) μ) :=
+begin
+  let f' := hf_int.1.mk f,
+  refine ⟨λ x, f' x.2, hf_int.1.strongly_measurable_mk.comp_measurable measurable_snd, _⟩,
+  suffices h : @measure.quasi_measure_preserving (Ω × Ω) Ω mΩ (m.prod mΩ) prod.snd
+    (@measure.map _ _ (m.prod mΩ) _ (λ ω, (ω, ω)) μ) μ,
+  { exact measure.quasi_measure_preserving.ae_eq h hf_int.1.ae_eq_mk, },
+  refine ⟨measurable_snd, measure.absolutely_continuous.mk (λ s hs hμs, _)⟩,
+  rw measure.map_apply _ hs,
+  swap, { exact measurable_snd, },
+  rw measure.map_apply,
+  { rw [← univ_prod, mk_preimage_prod, preimage_univ, univ_inter, preimage_id'],
+    exact hμs, },
+  { exact (measurable_id.mono hm le_rfl).prod_mk measurable_id, },
+  { exact measurable_snd hs, },
+end
+
+lemma integrable_comp_snd_map_prod_mk (hm : m ≤ mΩ) {f : Ω → F} (hf_int : integrable f μ) :
+  @integrable F _ (Ω × Ω) (m.prod mΩ) (λ x : Ω × Ω, f x.2)
+    (@measure.map _ _ (m.prod mΩ) _ (λ ω, (ω, ω)) μ) :=
+begin
+  have hX : @measurable Ω Ω mΩ m id := measurable_id.mono le_rfl hm,
+  have hY : measurable (id : Ω → Ω) := measurable_id,
+  have hXY : @ae_measurable Ω (Ω × Ω) (m.prod mΩ) mΩ (λ ω, (ω, ω)) μ :=
+    @measurable.ae_measurable Ω (Ω × Ω) mΩ (m.prod mΩ) _ μ (hX.prod_mk hY),
+  have hf := ae_strongly_measurable_comp_snd_map_prod_mk hm hf_int,
+  refine ⟨hf, _⟩,
+  rw [has_finite_integral, lintegral_map' hf.ennnorm hXY],
+  exact hf_int.2,
+end
+
 /-- The conditional expectation of `f` with respect to a σ-algebra `m` is almost everywhere equal to
 the integral `∫ y, f y ∂(condexp_kernel μ m ω)`. -/
 lemma condexp_ae_eq_integral_condexp_kernel (hm : m ≤ mΩ) {f : Ω → F} (hf_int : integrable f μ) :
   μ[f | m] =ᵐ[μ] λ ω, ∫ y, f y ∂(condexp_kernel μ m ω) :=
 begin
   have hX : @measurable Ω Ω mΩ m id := measurable_id.mono le_rfl hm,
-  have hY : measurable (id : Ω → Ω) := measurable_id,
-  have hXY : @ae_measurable Ω (Ω × Ω) (m.prod mΩ) mΩ (λ ω, (ω, ω)) μ :=
-    @measurable.ae_measurable Ω (Ω × Ω) mΩ (m.prod mΩ) _ μ (hX.prod_mk hY),
-  have hf' : @ae_strongly_measurable (Ω × Ω) F _ (m.prod mΩ) (λ x : Ω × Ω, f x.2)
-    (@measure.map _ _ (m.prod mΩ) _ (λ ω, (ω, ω)) μ),
-  { let f' := hf_int.1.mk f,
-    refine ⟨λ x, f' x.2, hf_int.1.strongly_measurable_mk.comp_measurable measurable_snd, _⟩,
-    suffices h : @measure.quasi_measure_preserving (Ω × Ω) Ω mΩ (m.prod mΩ) prod.snd
-      (@measure.map _ _ (m.prod mΩ) _ (λ ω, (ω, ω)) μ) μ,
-    { exact measure.quasi_measure_preserving.ae_eq h hf_int.1.ae_eq_mk, },
-    refine ⟨measurable_snd, measure.absolutely_continuous.mk (λ s hs hμs, _)⟩,
-    rw measure.map_apply _ hs,
-    swap, { exact measurable_snd, },
-    rw measure.map_apply,
-    { rw [← univ_prod, mk_preimage_prod, preimage_univ, univ_inter, preimage_id'],
-      exact hμs, },
-    { exact hX.prod_mk hY, },
-    { exact measurable_snd hs, }, },
+  have hY : ae_measurable (id : Ω → Ω) μ := ae_measurable_id,
   have hf_int' : @integrable F _ (Ω × Ω) (m.prod mΩ) (λ x : Ω × Ω, f x.2)
     (@measure.map _ _ (m.prod mΩ) _ (λ ω, (ω, ω)) μ),
-  { refine ⟨hf', _⟩,
-    rw [has_finite_integral, lintegral_map' hf'.ennnorm hXY],
-    exact hf_int.2, },
+  { exact integrable_comp_snd_map_prod_mk hm hf_int, },
   rw condexp_kernel,
-  refine eventually_eq.trans _
-    (condexp_prod_ae_eq_integral_cond_distrib' hX hY.ae_measurable hf_int'),
+  refine eventually_eq.trans _ (condexp_prod_ae_eq_integral_cond_distrib' hX hY hf_int'),
   simp only [measurable_space.comap_id, id.def],
 end
 

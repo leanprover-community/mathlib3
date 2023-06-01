8e95c837602c8e7154dbc4c3454d6057902b7efa
Michael Stoll <Michael.Stoll@uni-bayreuth.de>
2023-06-01 20:24:53 +0200

    add author

--- src/number_theory/sum_two_squares.lean
+++ src/number_theory/sum_two_squares.lean
@@ -1,7 +1,7 @@
 /-
 Copyright (c) 2019 Chris Hughes. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
-Authors: Chris Hughes
+Authors: Chris Hughes, Michael Stoll
 -/
 
 import number_theory.zsqrtd.gaussian_int

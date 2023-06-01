4605061f403fa0007d295cade27f50e249e1dcd5
Michael Stoll <Michael.Stoll@uni-bayreuth.de>
2023-06-01 19:39:48 +0200

    make suggested changes

--- src/number_theory/sum_two_squares.lean
+++ src/number_theory/sum_two_squares.lean
@@ -27,7 +27,7 @@ open gaussian_int
 
 /-- **Fermat's theorem on the sum of two squares**. Every prime not congruent to 3 mod 4 is the sum
 of two squares. Also known as **Fermat's Christmas theorem**. -/
-theorem nat.prime.sq_add_sq {p : ℕ} [fact (nat.prime p)] (hp : p % 4 ≠ 3) :
+theorem nat.prime.sq_add_sq {p : ℕ} [fact p.prime] (hp : p % 4 ≠ 3) :
   ∃ a b : ℕ, a ^ 2 + b ^ 2 = p :=
 begin
   apply sq_add_sq_of_nat_prime_of_not_irreducible p,
@@ -70,7 +70,7 @@ section neg_one_square
 -- This could be formulated for a general integer `a` in place of `-1`,
 -- but it would not directly specialize to `-1`,
 -- because `((-1 : ℤ) : zmod n)` is not the same as `(-1 : zmod n)`.
-lemma zmod.is_square_cast_neg_one {m n : ℕ} (hd : m ∣ n) (hs : is_square (-1 : zmod n)) :
+lemma zmod.is_square_neg_one_of_dvd {m n : ℕ} (hd : m ∣ n) (hs : is_square (-1 : zmod n)) :
   is_square (-1 : zmod m) :=
 begin
   let f : zmod n →+* zmod m := zmod.cast_hom hd _,
@@ -80,7 +80,6 @@ end
 
 /-- If `-1` is a square modulo coprime natural numbers `m` and `n`, then `-1` is also
 a square modulo `m*n`. -/
--- A similar comment applies here.
 lemma zmod.is_square_neg_one_mul {m n : ℕ} (hc : m.coprime n) (hm : is_square (-1 : zmod m))
   (hn : is_square (-1 : zmod n)) : is_square (-1 : zmod (m * n)) :=
 begin
@@ -94,10 +93,10 @@ begin
 end
 
 /-- If a prime `p` divides `n` such that `-1` is a square modulo `n`, then `p % 4 ≠ 3`. -/
-lemma nat.prime.mod_four_ne_three_of_dvd_is_square_neg_one {p n : ℕ} (hpp: p.prime) (hp : p ∣ n)
+lemma nat.prime.mod_four_ne_three_of_dvd_is_square_neg_one {p n : ℕ} (hpp : p.prime) (hp : p ∣ n)
   (hs : is_square (-1 : zmod n)) : p % 4 ≠ 3 :=
 begin
-  obtain ⟨y, h⟩ := zmod.is_square_cast_neg_one hp hs,
+  obtain ⟨y, h⟩ := zmod.is_square_neg_one_of_dvd hp hs,
   rw [← sq, eq_comm, show (-1 : zmod p) = -1 ^ 2, from by ring] at h,
   haveI : fact p.prime := ⟨hpp⟩,
   exact zmod.mod_four_ne_three_of_sq_eq_neg_sq' one_ne_zero h,
@@ -133,9 +132,9 @@ begin
   { exact ⟨0, 0, rfl⟩, },
   { exact ⟨0, 1, rfl⟩, },
   { haveI : fact p.prime := ⟨hpp⟩,
-    have hp : is_square (-1 : zmod p) := zmod.is_square_cast_neg_one ⟨n, rfl⟩ h,
+    have hp : is_square (-1 : zmod p) := zmod.is_square_neg_one_of_dvd ⟨n, rfl⟩ h,
     obtain ⟨u, v, huv⟩ := nat.prime.sq_add_sq (zmod.exists_sq_eq_neg_one_iff.mp hp),
-    obtain ⟨x, y, hxy⟩ := ih (zmod.is_square_cast_neg_one ⟨p, mul_comm _ _⟩ h),
+    obtain ⟨x, y, hxy⟩ := ih (zmod.is_square_neg_one_of_dvd ⟨p, mul_comm _ _⟩ h),
     exact nat.sq_add_sq_mul huv.symm hxy, }
 end
 
@@ -197,8 +196,7 @@ every prime `q` such that `q%4 = 3` in the prime factorization of `n` is even.
 (The assumption `0 < n` is not present, since for `n = 0`, both sides are satisfied;
 the right hand side holds, since `padic_val_nat q 0 = 0` by definition.) -/
 lemma nat.eq_sq_add_sq_iff {n : ℕ} :
-  (∃ x y : ℕ, n = x ^ 2 + y ^ 2) ↔
-    ∀ {q : ℕ} (hq : q.prime) (h : q % 4 = 3), even (padic_val_nat q n) :=
+  (∃ x y : ℕ, n = x ^ 2 + y ^ 2) ↔ ∀ {q : ℕ}, q.prime → q % 4 = 3 → even (padic_val_nat q n) :=
 begin
   rcases n.eq_zero_or_pos with rfl | hn₀,
   { exact ⟨λ H q hq h, (@padic_val_nat.zero q).symm ▸ even_zero, λ H, ⟨0, 0, rfl⟩⟩, },
@@ -213,7 +211,6 @@ begin
     have ha := mt sq_eq_zero_iff.mpr ha₂,
     have hb := right_ne_zero_of_mul hab,
     haveI hqi : fact q.prime := ⟨hq⟩,
-    -- Using `rw` necessitates giving either `q` or `hqi` to `padic_val_nat.mul` ...
     simp_rw [h₁, padic_val_nat.mul ha₂ hb, padic_val_nat.pow 2 ha, hqb, add_zero],
     exact even_two_mul _, },
   { obtain ⟨b, a, hb₀, ha₀, hab, hb⟩ := nat.sq_mul_squarefree_of_pos hn₀,

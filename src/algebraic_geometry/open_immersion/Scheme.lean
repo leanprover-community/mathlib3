49688991f274a074030bc262d224a96b9e18bba8
Heather Macbeth <25316162+hrmacbeth@users.noreply.github.com>
2023-06-04 19:59:59 -0400

    rename

--- src/algebraic_geometry/open_immersion/Scheme.lean
+++ src/algebraic_geometry/open_immersion/Scheme.lean
@@ -3,7 +3,7 @@ Copyright (c) 2021 Andrew Yang. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Andrew Yang
 -/
-import algebraic_geometry.open_immersion
+import algebraic_geometry.open_immersion.basic
 import algebraic_geometry.Scheme
 import category_theory.limits.shapes.comm_sq
 

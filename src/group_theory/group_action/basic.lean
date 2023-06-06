c3deac0e56eabcb0eb95b4f97dd78d71dd39e426
Eric Rodriguez <37984851+ericrbg@users.noreply.github.com>
2023-06-06 07:56:27 -0400

    add to_additive

--- src/group_theory/group_action/basic.lean
+++ src/group_theory/group_action/basic.lean
@@ -192,6 +192,7 @@ local attribute [instance] orbit_rel
 
 variables {α} {β}
 
+@[to_additive]
 lemma orbit_rel_apply {x y : β} : (orbit_rel α β).rel x y ↔ x ∈ orbit α y := iff.rfl
 
 /-- When you take a set `U` in `β`, push it down to the quotient, and pull back, you get the union

ad9d31f3643d85d80aae8700272089f57c23a542
Jireh Loreaux <loreaujy@gmail.com>
2023-06-06 00:27:34 -0500

    fix instances

--- src/data/rat/star.lean
+++ src/data/rat/star.lean
@@ -26,23 +26,20 @@ instance : has_trivial_star ℚ :=
 { star_trivial := λ _, rfl }
 
 instance : star_ordered_ring ℚ :=
-{ star := star,
-  add_le_add_left := λ _ _, add_le_add_left,
-  nonneg_iff := λ x,
-  begin
-    refine ⟨λ hx, _, λ hx, add_submonoid.closure_induction hx
-      (by { rintro - ⟨s, rfl⟩, exact mul_self_nonneg s }) le_rfl (λ _ _, add_nonneg)⟩,
-    suffices : (finset.range (x.num.nat_abs * x.denom)).sum
-      (function.const ℕ (rat.mk_pnat 1 ⟨x.denom, x.pos⟩ * rat.mk_pnat 1 ⟨x.denom, x.pos⟩)) = x,
-    { exact this ▸ sum_mem (λ n hn, add_submonoid.subset_closure ⟨_, rfl⟩) },
-    simp only [function.const_apply, finset.sum_const, finset.card_range, nsmul_eq_mul, mk_pnat_eq],
-    rw [←int.cast_coe_nat, int.coe_nat_mul, int.coe_nat_abs,
-      abs_of_nonneg (num_nonneg_iff_zero_le.mpr hx), int.cast_mul, int.cast_coe_nat],
-    simp only [int.cast_mul, int.cast_coe_nat, coe_int_eq_mk, coe_nat_eq_mk],
-    rw [mul_assoc, ←mul_assoc (mk (x.denom : ℤ) 1), mk_mul_mk_cancel one_ne_zero,
-      ←one_mul (x.denom : ℤ), div_mk_div_cancel_left (by simpa using x.pos.ne' : (x.denom : ℤ) ≠ 0),
-      one_mul, mk_one_one, one_mul, mk_mul_mk_cancel one_ne_zero, rat.num_denom],
-  end,
-  .. rat.star_ring }
+star_ordered_ring.of_nonneg_iff (λ _ _, add_le_add_left) $ λ x,
+begin
+  refine ⟨λ hx, _, λ hx, add_submonoid.closure_induction hx
+    (by { rintro - ⟨s, rfl⟩, exact mul_self_nonneg s }) le_rfl (λ _ _, add_nonneg)⟩,
+  suffices : (finset.range (x.num.nat_abs * x.denom)).sum
+    (function.const ℕ (rat.mk_pnat 1 ⟨x.denom, x.pos⟩ * rat.mk_pnat 1 ⟨x.denom, x.pos⟩)) = x,
+  { exact this ▸ sum_mem (λ n hn, add_submonoid.subset_closure ⟨_, rfl⟩) },
+  simp only [function.const_apply, finset.sum_const, finset.card_range, nsmul_eq_mul, mk_pnat_eq],
+  rw [←int.cast_coe_nat, int.coe_nat_mul, int.coe_nat_abs,
+    abs_of_nonneg (num_nonneg_iff_zero_le.mpr hx), int.cast_mul, int.cast_coe_nat],
+  simp only [int.cast_mul, int.cast_coe_nat, coe_int_eq_mk, coe_nat_eq_mk],
+  rw [mul_assoc, ←mul_assoc (mk (x.denom : ℤ) 1), mk_mul_mk_cancel one_ne_zero,
+    ←one_mul (x.denom : ℤ), div_mk_div_cancel_left (by simpa using x.pos.ne' : (x.denom : ℤ) ≠ 0),
+    one_mul, mk_one_one, one_mul, mk_mul_mk_cancel one_ne_zero, rat.num_denom],
+end
 
 end rat

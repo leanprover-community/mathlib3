5fa966a5b7cc7f97eaf52260c690bf9261cbc8f2
Yaël Dillies <yael.dillies@gmail.com>
2023-06-10 09:25:28 +0000

    sorry-free

--- src/order/limbo.lean
+++ src/order/limbo.lean
@@ -1,47 +1,89 @@
-import data.fintype.basic
+import data.fintype.card
 import order.atoms
+import data.nat.succ_pred
 
 variables {α : Type*}
 
+open order
+
+namespace list
+variables {l l₁ l₂ : list α} {a : α}
+
+lemma subperm_iff : l₁ <+~ l₂ ↔ ∃ l ~ l₂, l₁ <+ l :=
+begin
+  refine ⟨_, λ ⟨l, h₁, h₂⟩, h₂.subperm.trans h₁.subperm⟩,
+  rintro ⟨l, h₁, h₂⟩,
+  obtain ⟨l', h₂⟩ := h₂.exists_perm_append,
+  exact ⟨l₁ ++ l', (h₂.trans (h₁.append_right _)).symm, (prefix_append _ _).sublist⟩,
+end
+
+@[simp] lemma sublist_singleton : l <+ [a] ↔ l = [] ∨ l = [a] :=
+by split; rintro (_ | _); simp [*] at *
+
+@[simp] lemma subperm_singleton_iff' : l <+~ [a] ↔ l = [] ∨ l = [a] :=
+begin
+  split,
+  { rw subperm_iff,
+    rintro ⟨s, hla, h⟩,
+    rwa [perm_singleton.mp hla, sublist_singleton] at h },
+  { rintro (rfl | rfl),
+    exacts [nil_subperm, subperm.refl _] }
+end
+
+end list
 
 namespace multiset
 variables {s t : multiset α} {a : α}
 
-@[simp] lemma cons_zero (a : α) : a ::ₘ 0 = {a} := rfl
+instance : is_nonstrict_strict_order (multiset α) (⊆) (⊂) := ⟨λ _ _, iff.rfl⟩
 
 @[simp] lemma cons_lt_cons_iff : a ::ₘ s < a ::ₘ t ↔ s < t :=
 lt_iff_lt_of_le_iff_le' (cons_le_cons_iff _) (cons_le_cons_iff _)
 
 lemma cons_lt_cons (a : α) (h : s < t) : a ::ₘ s < a ::ₘ t := cons_lt_cons_iff.2 h
 
-lemma le_singleton_iff : s ≤ {a} ↔ s = 0 ∨ s = {a} :=
-quot.induction_on s $ λ l, by simp only [singleton_eq_cons, singleton_coe, quot_mk_to_coe'', coe_le,
+@[simp] lemma le_singleton : s ≤ {a} ↔ s = 0 ∨ s = {a} :=
+quot.induction_on s $ λ l, by simp only [cons_zero, ←coe_singleton, quot_mk_to_coe'', coe_le,
   coe_eq_zero, coe_eq_coe, list.perm_singleton, list.subperm_singleton_iff']
 
-lemma lt_singleton_iff : s < {a} ↔ s = 0 :=
+@[simp] lemma lt_singleton : s < {a} ↔ s = 0 :=
 begin
-  rw [lt_iff_le_and_ne, le_singleton_iff, or_and_distrib_right, or_iff_left (and_not_self _).1,
-    and_iff_left_of_imp],
+  simp only [lt_iff_le_and_ne, le_singleton, or_and_distrib_right, ne.def, and_not_self, or_false,
+    and_iff_left_iff_imp],
   rintro rfl,
-  rw singleton_eq_cons,
-  exact zero_ne_cons,
+  exact (singleton_ne_zero _).symm,
 end
 
 lemma covby_cons (m : multiset α) (a : α) : m ⋖ a ::ₘ m :=
 ⟨lt_cons_self _ _, begin
   simp_rw lt_iff_cons_le,
-  rintros m' ⟨b, hbm'⟩ ⟨c, hcm'⟩,
+  rintro m' ⟨b, hbm'⟩ ⟨c, hcm'⟩,
   apply @irrefl _ (<) _ m,
   have h := lt_of_le_of_lt hbm' (lt_cons_self _ c),
   replace h := lt_of_lt_of_le h hcm',
   clear hbm' hcm',
   induction m using multiset.induction with d m hm,
-  { rw [cons_zero a, lt_singleton_iff] at h,
+  { rw [cons_zero a, lt_singleton] at h,
     exact (cons_ne_zero h).elim },
   { simp_rw cons_swap _ d at h,
     rw cons_lt_cons_iff at h ⊢,
     exact hm h }
 end⟩
+
+attribute [simp] subset_zero
+
+@[simp] lemma zero_ssubset : 0 ⊂ s ↔ s ≠ 0 := by simp [ssubset_iff_subset_not_subset]
+
+@[simp] lemma singleton_subset : {a} ⊆ s ↔ a ∈ s := by simp [subset_iff]
+
+@[simp] lemma ssubset_singleton_iff : s ⊂ {a} ↔ s = 0 :=
+begin
+  refine ⟨λ hs, eq_zero_of_subset_zero $ λ b hb, hs.2 _, _⟩,
+  { obtain rfl := mem_singleton.1 (hs.1 hb),
+    rwa singleton_subset },
+  { rintro rfl,
+    simp }
+end
 
 lemma _root_.covby.exists_cons_multiset (h : s ⋖ t) : ∃ a, t = a ::ₘ s :=
 (lt_iff_cons_le.1 h.lt).imp $ λ a ha, ha.eq_of_not_gt $ h.2 $ lt_cons_self _ _
@@ -53,6 +95,10 @@ lemma _root_.covby.card_multiset (h : s ⋖ t) : s.card ⋖ t.card :=
 by { obtain ⟨a, rfl⟩ := h.exists_cons_multiset, rw card_cons, exact covby_succ _ }
 
 lemma card_strict_mono : strict_mono (card : multiset α → ℕ) := λ _ _, card_lt_of_lt
+
+lemma is_atom_iff : is_atom s ↔ ∃ a, s = {a} := bot_covby_iff.symm.trans covby_iff
+
+@[simp] lemma is_atom_singleton (a : α) : is_atom ({a} : multiset α) := is_atom_iff.2 ⟨_, rfl⟩
 
 end multiset
 
@@ -124,40 +170,3 @@ lemma is_coatom_iff [fintype α] [decidable_eq α] : is_coatom s ↔ ∃ a, s = 
 by simp_rw [←is_atom_compl, is_atom_iff, compl_eq_iff_is_compl, eq_compl_iff_is_compl]
 
 end finset
-
-namespace multiset
-variables {m : multiset α}
-
-lemma le_singleton_iff_eq {m : multiset α} {x : α} : m ≤ {x} ↔ m = 0 ∨ m = {x} :=
-begin
-  obtain rfl | hm := eq_or_ne m 0,
-  refine ⟨λ _, or.inl rfl, λ _, zero_le _⟩,
-  simp [eq_singleton_iff_nonempty_unique_mem, hs, ne_empty_iff_nonempty.2 hs],
-end
-
-@[simp] lemma lt_singleton_iff {m : multiset α} {a : α} :  m < {a} ↔ m = 0 :=
-begin
-  rw [lt_iff_le_and_ne, le_singleton_iff_eq, or_and_distrib_right, and_not_self, or_false,
-    and_iff_left_iff_imp],
-  rintro rfl,
-  exact (singleton_ne_zero _).symm,
-end
-
-lemma eq_zero_of_ssubset_singleton {s : multiset α} {x : α} (hs : s ⊂ {x}) : s = 0 :=
-ssubset_singleton_iff.1 hs
-
-lemma is_atom_singleton (a : α) : is_atom ({a} : multiset α) :=
-⟨singleton_ne_zero a, λ s, eq_zero_of_lt_singleton⟩
-
-lemma is_atom_iff : is_atom m ↔ ∃ a, m = {a} :=
-begin
-  refine ⟨λ hs, exists_eq_singleton_iff_nonempty_subsingleton.2 ⟨nonempty_of_ne_empty hs.1,
-    λ a ha b hb, _⟩, _⟩,
-  { rw [←singleton_subset_iff] at ha hb,
-    exact singleton_injective (((hs.le_iff_eq $ singleton_ne_empty _).1 ha).trans $
-      ((hs.le_iff_eq $ singleton_ne_empty _).1 hb).symm) },
-  { rintro ⟨a, rfl⟩,
-    exact is_atom_singleton _ }
-end
-
-end multiset

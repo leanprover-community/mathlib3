d91573f23b557488858a258bba1f6773c95e10e0
Riccardo Brasca <riccardo.brasca@gmail.com>
2023-06-12 23:07:15 +0200

    nicer

--- src/field_theory/finite/galois_field.lean
+++ src/field_theory/finite/galois_field.lean
@@ -156,14 +156,11 @@ local attribute [-instance] splitting_field.algebra'
 
 /-- A Galois field with exponent 1 is equivalent to `zmod` -/
 def equiv_zmod_p : galois_field p 1 ≃ₐ[zmod p] (zmod p) :=
-begin
-  have h : (X ^ p ^ 1 : (zmod p)[X]) = X ^ (fintype.card (zmod p)),
-    by rw [pow_one, zmod.card p],
-  haveI inst : is_splitting_field (zmod p) (zmod p) (X ^ p ^ 1 - X),
-    by { rw h, apply_instance },
-  let φ := is_splitting_field.alg_equiv (zmod p) (X ^ (p ^ 1) - X : (zmod p)[X]),
-  exact φ.symm,
-end
+let h : (X ^ p ^ 1 : (zmod p)[X]) = X ^ (fintype.card (zmod p)) :=
+  by rw [pow_one, zmod.card p] in
+let inst : is_splitting_field (zmod p) (zmod p) (X ^ p ^ 1 - X) :=
+  by { rw h, apply_instance } in
+(@is_splitting_field.alg_equiv _ (zmod p) _ _ _ (X ^ (p ^ 1) - X : (zmod p)[X]) inst).symm
 
 variables {K : Type*} [field K] [fintype K] [algebra (zmod p) K]
 

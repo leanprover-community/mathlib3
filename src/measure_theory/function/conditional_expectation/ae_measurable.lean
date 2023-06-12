b5e89eded450a29928bb23be8585e7be74848eaf
RemyDegenne <Remydegenne@gmail.com>
2023-06-12 10:12:20 +0200

    fix imports

--- src/measure_theory/function/conditional_expectation/ae_measurable.lean
+++ src/measure_theory/function/conditional_expectation/ae_measurable.lean
@@ -3,10 +3,8 @@ Copyright (c) 2021 Rémy Degenne. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Rémy Degenne
 -/
-
-import analysis.inner_product_space.projection
 import measure_theory.function.l2_space
-import measure_theory.function.ae_eq_of_integral
+import measure_theory.function.strongly_measurable.lp
 
 /-! # Functions a.e. measurable with respect to a sub-σ-algebra
 
--- src/measure_theory/function/conditional_expectation/condexp_L2.lean
+++ src/measure_theory/function/conditional_expectation/condexp_L2.lean
@@ -3,6 +3,7 @@ Copyright (c) 2021 Rémy Degenne. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Rémy Degenne
 -/
+import analysis.inner_product_space.projection
 import measure_theory.function.conditional_expectation.unique
 
 /-! # Conditional expectation in L2

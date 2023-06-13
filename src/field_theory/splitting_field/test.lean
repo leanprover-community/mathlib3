517bcb4ad4a89b396e241956560c23871730debe
riccardobrasca <riccardo.brasca@gmail.com>
2023-06-13 10:44:47 +0200

    erase

--- src/field_theory/splitting_field/test.lean
+++
@@ -1,6 +0,0 @@
-import data.mv_polynomial.basic
-import data.polynomial.ring_division
-
-structure test (K : Type) [inst : field K] :=
-(n : ℕ)
-(x : mv_polynomial ((n : polynomial K).root_set K) K)

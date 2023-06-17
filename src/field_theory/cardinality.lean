42e13bf3499b9ec49c9b35c5ad8ee10998d3cc70
Riccardo Brasca <riccardo.brasca@gmail.com>
2023-06-17 14:08:44 +0200

    another proof

--- src/field_theory/cardinality.lean
+++ src/field_theory/cardinality.lean
@@ -38,6 +38,7 @@ lemma fintype.is_prime_pow_card_of_field {α} [fintype α] [field α] : is_prime
 begin
   casesI char_p.exists α with p _,
   haveI hp := fact.mk (char_p.char_is_prime α p),
+  letI : algebra (zmod p) α := zmod.algebra _ _,
   let b := is_noetherian.finset_basis (zmod p) α,
   rw [module.card_fintype b, zmod.card, is_prime_pow_pow_iff],
   { exact hp.1.is_prime_pow },

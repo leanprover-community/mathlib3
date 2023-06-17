b96469da858af98d34c0a061483e917b143f74c6
Riccardo Brasca <riccardo.brasca@gmail.com>
2023-06-17 15:09:31 +0200

    Update src/ring_theory/polynomial/cyclotomic/expand.lean
    Co-authored-by: Anne Baanen <Vierkantor@users.noreply.github.com>

--- src/ring_theory/polynomial/cyclotomic/expand.lean
+++ src/ring_theory/polynomial/cyclotomic/expand.lean
@@ -124,9 +124,9 @@ section char_p
 /-- If `R` is of characteristic `p` and `¬p ∣ n`, then
 `cyclotomic (n * p) R = (cyclotomic n R) ^ (p - 1)`. -/
 lemma cyclotomic_mul_prime_eq_pow_of_not_dvd (R : Type*) {p n : ℕ} [hp : fact (nat.prime p)]
-  [ring R] [char_p R p] (hn : ¬p ∣ n) : cyclotomic (n * p) R = (cyclotomic n R) ^ (p - 1) :=
+  [ring R] [char_p R p] (hn : ¬p ∣ n) [algebra (zmod p) R]: 
+  cyclotomic (n * p) R = (cyclotomic n R) ^ (p - 1) :=
 begin
-  letI : algebra (zmod p) R := zmod.algebra _ _,
   suffices : cyclotomic (n * p) (zmod p) = (cyclotomic n (zmod p)) ^ (p - 1),
   { rw [← map_cyclotomic _ (algebra_map (zmod p) R), ← map_cyclotomic _ (algebra_map (zmod p) R),
       this, polynomial.map_pow] },

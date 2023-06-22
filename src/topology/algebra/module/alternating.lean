ceb0dfbc3f0f31788f6af7e92a0d11d1b139f1a1
Yury G. Kudryashov <urkud@urkud.name>
2023-06-21 20:46:49 -0500

    Add Sébastien to the authors
    I copied his file and edited it to make the first version of this one.

--- src/topology/algebra/module/alternating.lean
+++ src/topology/algebra/module/alternating.lean
@@ -1,7 +1,7 @@
 /-
 Copyright (c) 2023 Yury Kudryashov. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
-Authors: Yury Kudryashov, Heather Macbeth
+Authors: Yury Kudryashov, Heather Macbeth, Sébastien Gouëzel
 -/
 import linear_algebra.alternating
 import topology.algebra.module.multilinear

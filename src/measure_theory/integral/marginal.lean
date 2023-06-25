9f3e9cda78b026e52605257fbfbd13e55f26d3d8
Floris van Doorn <fpvdoorn@gmail.com>
2023-06-25 23:09:57 +0200

    Heather author

--- src/measure_theory/integral/marginal.lean
+++ src/measure_theory/integral/marginal.lean
@@ -1,7 +1,7 @@
 /-
 Copyright (c) 2022 Floris van Doorn. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
-Authors: Floris van Doorn
+Authors: Floris van Doorn, Heather Macbeth
 -/
 import measure_theory.constructions.pi
 import measure_theory.integral.interval_integral

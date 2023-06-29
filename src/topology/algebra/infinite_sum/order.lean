181e1172b30b61152984006c2c3359f7d92c95a8
Antoine Chambert-Loir <antoine.chambert-loir@math.univ-paris-diderot.fr>
2023-06-29 15:58:33 +0200

    feat(topology/algebra/infinite_sum) : infinite products

--- src/topology/algebra/infinite_sum/order.lean
+++ src/topology/algebra/infinite_sum/order.lean
@@ -4,7 +4,8 @@ Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Johannes Hölzl
 -/
 import algebra.order.archimedean
-import topology.algebra.infinite_sum.basic
+-- import topology.algebra.infinite_sum.basic
+import .basic
 import topology.algebra.order.field
 import topology.algebra.order.monotone_convergence
 
@@ -23,37 +24,46 @@ open_locale big_operators classical
 variables {ι κ α : Type*}
 
 section preorder
-variables [preorder α] [add_comm_monoid α] [topological_space α] [order_closed_topology α]
+variables [preorder α] [topological_space α] [order_closed_topology α]
   [t2_space α] {f : ℕ → α} {c : α}
 
-lemma tsum_le_of_sum_range_le (hf : summable f) (h : ∀ n, ∑ i in range n, f i ≤ c) :
-  ∑' n, f n ≤ c :=
-let ⟨l, hl⟩ := hf in hl.tsum_eq.symm ▸ le_of_tendsto' hl.tendsto_sum_nat h
+@[to_additive] variable [comm_monoid α] 
+
+@[to_additive tsum_le_of_sum_range_le] 
+lemma tprod_le_of_prod_range_le (hf : multipliable f) (h : ∀ n, ∏ i in range n, f i ≤ c) :
+  ∏' n, f n ≤ c :=
+let ⟨l, hl⟩ := hf in hl.tprod_eq.symm ▸ le_of_tendsto' hl.tendsto_sum_nat h
 
 end preorder
 
-section ordered_add_comm_monoid
-variables [ordered_add_comm_monoid α] [topological_space α] [order_closed_topology α] {f g : ι → α}
+section ordered_comm_monoid
+@[to_additive] variables [ordered_comm_monoid α] 
+variables [topological_space α] [order_closed_topology α] {f g : ι → α}
   {a a₁ a₂ : α}
 
-lemma has_sum_le (h : ∀ i, f i ≤ g i) (hf : has_sum f a₁) (hg : has_sum g a₂) : a₁ ≤ a₂ :=
-le_of_tendsto_of_tendsto' hf hg $ λ s, sum_le_sum $ λ i _, h i
+@[to_additive]
+lemma has_prod_le (h : ∀ i, f i ≤ g i) (hf : has_prod f a₁) (hg : has_prod g a₂) : a₁ ≤ a₂ :=
+le_of_tendsto_of_tendsto' hf hg $ λ s, prod_le_prod' $ λ i _, h i
 
-@[mono] lemma has_sum_mono (hf : has_sum f a₁) (hg : has_sum g a₂) (h : f ≤ g) : a₁ ≤ a₂ :=
-has_sum_le h hf hg
+@[to_additive, mono]
+lemma has_prod_mono (hf : has_prod f a₁) (hg : has_prod g a₂) (h : f ≤ g) : a₁ ≤ a₂ :=
+has_prod_le h hf hg
 
-lemma has_sum_le_of_sum_le (hf : has_sum f a) (h : ∀ s, ∑ i in s, f i ≤ a₂) : a ≤ a₂ :=
+@[to_additive]
+lemma has_prod_le_of_prod_le (hf : has_prod f a) (h : ∀ s, ∏ i in s, f i ≤ a₂) : a ≤ a₂ :=
 le_of_tendsto' hf h
 
-lemma le_has_sum_of_le_sum (hf : has_sum f a) (h : ∀ s, a₂ ≤ ∑ i in s, f i) : a₂ ≤ a :=
+@[to_additive]
+lemma le_has_prod_of_le_prod (hf : has_prod f a) (h : ∀ s, a₂ ≤ ∏ i in s, f i) : a₂ ≤ a :=
 ge_of_tendsto' hf h
 
-lemma has_sum_le_inj {g : κ → α} (e : ι → κ) (he : injective e) (hs : ∀ c ∉ set.range e, 0 ≤ g c)
-  (h : ∀ i, f i ≤ g (e i)) (hf : has_sum f a₁) (hg : has_sum g a₂) : a₁ ≤ a₂ :=
-have has_sum (λ c, (partial_inv e c).cases_on' 0 f) a₁,
+@[to_additive]
+lemma has_prod_le_inj {g : κ → α} (e : ι → κ) (he : injective e) (hs : ∀ c ∉ set.range e, 1 ≤ g c)
+  (h : ∀ i, f i ≤ g (e i)) (hf : has_prod f a₁) (hg : has_prod g a₂) : a₁ ≤ a₂ :=
+have has_prod (λ c, (partial_inv e c).cases_on' 1 f) a₁,
 begin
-  refine (has_sum_iff_has_sum_of_ne_zero_bij (e ∘ coe) (λ c₁ c₂ hc, he hc) (λ c hc, _) _).2 hf,
-  { rw mem_support at hc,
+  refine (has_prod_iff_has_prod_of_ne_one_bij (e ∘ coe) (λ c₁ c₂ hc, he hc) (λ c hc, _) _).2 hf,
+  { rw mem_mul_support at hc,
     cases eq : partial_inv e c with i; rw eq at hc,
     { contradiction },
     { rw [partial_inv_of_injective he] at eq,
@@ -62,7 +72,7 @@ begin
     simp [partial_inv_left he, option.cases_on'] }
 end,
 begin
-  refine has_sum_le (λ c, _) this hg,
+  refine has_prod_le (λ c, _) this hg,
   obtain ⟨i, rfl⟩ | h := em (c ∈ set.range e),
   { rw [partial_inv_left he, option.cases_on'],
     exact h _ },
@@ -71,153 +81,192 @@ begin
     exact hs _ h }
 end
 
-lemma tsum_le_tsum_of_inj {g : κ → α} (e : ι → κ) (he : injective e)
-  (hs : ∀ c ∉ set.range e, 0 ≤ g c) (h : ∀ i, f i ≤ g (e i)) (hf : summable f) (hg : summable g) :
-  tsum f ≤ tsum g :=
-has_sum_le_inj _ he hs h hf.has_sum hg.has_sum
+@[to_additive tsum_le_tsum_of_inj]
+lemma tprod_le_tprod_of_inj {g : κ → α} (e : ι → κ) (he : injective e)
+  (hs : ∀ c ∉ set.range e, 1 ≤ g c) (h : ∀ i, f i ≤ g (e i)) 
+  (hf : multipliable f) (hg : multipliable g) : tprod f ≤ tprod g :=
+has_prod_le_inj _ he hs h hf.has_prod hg.has_prod
 
-lemma sum_le_has_sum (s : finset ι) (hs : ∀ i ∉ s, 0 ≤ f i) (hf : has_sum f a) :
-  ∑ i in s, f i ≤ a :=
+@[to_additive]
+lemma prod_le_has_prod (s : finset ι) (hs : ∀ i ∉ s, 1 ≤ f i) (hf : has_prod f a) :
+  ∏ i in s, f i ≤ a :=
 ge_of_tendsto hf (eventually_at_top.2 ⟨s, λ t hst,
-  sum_le_sum_of_subset_of_nonneg hst $ λ i hbt hbs, hs i hbs⟩)
+  prod_le_prod_of_subset_of_one_le' hst $ λ i hbt hbs, hs i hbs⟩)
 
-lemma is_lub_has_sum (h : ∀ i, 0 ≤ f i) (hf : has_sum f a) :
-  is_lub (set.range $ λ s, ∑ i in s, f i) a :=
-is_lub_of_tendsto_at_top (finset.sum_mono_set_of_nonneg h) hf
+@[to_additive]
+lemma is_lub_has_prod (h : ∀ i, 1 ≤ f i) (hf : has_prod f a) :
+  is_lub (set.range $ λ s, ∏ i in s, f i) a :=
+is_lub_of_tendsto_at_top (finset.prod_mono_set_of_one_le' h) hf
 
-lemma le_has_sum (hf : has_sum f a) (i : ι) (hb : ∀ b' ≠ i, 0 ≤ f b') : f i ≤ a :=
-calc f i = ∑ i in {i}, f i : finset.sum_singleton.symm
-... ≤ a : sum_le_has_sum _ (by { convert hb, simp }) hf
+@[to_additive]
+lemma le_has_prod (hf : has_prod f a) (i : ι) (hb : ∀ b' ≠ i, 1 ≤ f b') : f i ≤ a :=
+calc f i = ∏ i in {i}, f i : finset.prod_singleton.symm
+... ≤ a : prod_le_has_prod _ (by { convert hb, simp }) hf
 
-lemma sum_le_tsum {f : ι → α} (s : finset ι) (hs : ∀ i ∉ s, 0 ≤ f i) (hf : summable f) :
-  ∑ i in s, f i ≤ ∑' i, f i :=
-sum_le_has_sum s hs hf.has_sum
+@[to_additive sum_le_tsum]
+lemma prod_le_tprod {f : ι → α} (s : finset ι) (hs : ∀ i ∉ s, 1 ≤ f i) (hf : multipliable f) :
+  ∏ i in s, f i ≤ ∏' i, f i :=
+prod_le_has_prod s hs hf.has_prod
 
-lemma le_tsum (hf : summable f) (i : ι) (hb : ∀ b' ≠ i, 0 ≤ f b') : f i ≤ ∑' i, f i :=
-le_has_sum (summable.has_sum hf) i hb
+@[to_additive le_tsum]
+lemma le_tprod (hf : multipliable f) (i : ι) (hb : ∀ b' ≠ i, 1 ≤ f b') : f i ≤ ∏' i, f i :=
+le_has_prod (multipliable.has_prod hf) i hb
 
-lemma tsum_le_tsum (h : ∀ i, f i ≤ g i) (hf : summable f) (hg : summable g) :
-  ∑' i, f i ≤ ∑' i, g i :=
-has_sum_le h hf.has_sum hg.has_sum
+@[to_additive tsum_le_tsum]
+lemma tprod_le_tprod (h : ∀ i, f i ≤ g i) (hf : multipliable f) (hg : multipliable g) :
+  ∏' i, f i ≤ ∏' i, g i :=
+has_prod_le h hf.has_prod hg.has_prod
 
-@[mono] lemma tsum_mono (hf : summable f) (hg : summable g) (h : f ≤ g) :
-  ∑' n, f n ≤ ∑' n, g n :=
-tsum_le_tsum h hf hg
+@[to_additive tsum_mono, mono] 
+lemma tprod_mono (hf : multipliable f) (hg : multipliable g) (h : f ≤ g) : 
+  ∏' n, f n ≤ ∏' n, g n :=
+tprod_le_tprod h hf hg
 
-lemma tsum_le_of_sum_le (hf : summable f) (h : ∀ s, ∑ i in s, f i ≤ a₂) : ∑' i, f i ≤ a₂ :=
-has_sum_le_of_sum_le hf.has_sum h
+@[to_additive tsum_le_of_sum_le]
+lemma tprod_le_of_prod_le (hf : multipliable f) (h : ∀ s, ∏ i in s, f i ≤ a₂) : ∏' i, f i ≤ a₂ :=
+has_prod_le_of_prod_le hf.has_prod h
 
-lemma tsum_le_of_sum_le' (ha₂ : 0 ≤ a₂) (h : ∀ s, ∑ i in s, f i ≤ a₂) : ∑' i, f i ≤ a₂ :=
+@[to_additive tsum_le_of_sum_le]
+lemma tprod_le_of_prod_le' (ha₂ : 1 ≤ a₂) (h : ∀ s, ∏ i in s, f i ≤ a₂) : ∏' i, f i ≤ a₂ :=
 begin
-  by_cases hf : summable f,
-  { exact tsum_le_of_sum_le hf h },
-  { rw tsum_eq_zero_of_not_summable hf,
+  by_cases hf : multipliable f,
+  { exact tprod_le_of_prod_le hf h },
+  { rw tprod_eq_one_of_not_multipliable hf,
     exact ha₂ }
 end
 
-lemma has_sum.nonneg (h : ∀ i, 0 ≤ g i) (ha : has_sum g a) : 0 ≤ a := has_sum_le h has_sum_zero ha
-lemma has_sum.nonpos (h : ∀ i, g i ≤ 0) (ha : has_sum g a) : a ≤ 0 := has_sum_le h ha has_sum_zero
+@[to_additive has_sum.nonneg]
+lemma has_prod.one_le (h : ∀ i, 1 ≤ g i) (ha : has_prod g a) : 1 ≤ a := 
+has_prod_le h has_prod_one ha
 
-lemma tsum_nonneg (h : ∀ i, 0 ≤ g i) : 0 ≤ ∑' i, g i :=
+@[to_additive has_sum.nonpos]
+lemma has_prod.le_one (h : ∀ i, g i ≤ 1) (ha : has_prod g a) : a ≤ 1 := 
+has_prod_le h ha has_prod_one
+
+@[to_additive]
+lemma tprod_one_le (h : ∀ i, 1 ≤ g i) : 1 ≤ ∏' i, g i :=
 begin
-  by_cases hg : summable g,
-  { exact hg.has_sum.nonneg h },
-  { simp [tsum_eq_zero_of_not_summable hg] }
+  by_cases hg : multipliable g,
+  { exact hg.has_prod.one_le h },
+  { simp [tprod_eq_one_of_not_multipliable hg] }
 end
 
-lemma tsum_nonpos (h : ∀ i, f i ≤ 0) : ∑' i, f i ≤ 0 :=
+@[to_additive tsum_nonpos]
+lemma tprod_le_one (h : ∀ i, f i ≤ 1) : ∏' i, f i ≤ 1 :=
 begin
-  by_cases hf : summable f,
-  { exact hf.has_sum.nonpos h },
-  { simp [tsum_eq_zero_of_not_summable hf] }
+  by_cases hf : multipliable f,
+  { exact hf.has_prod.le_one h },
+  { simp [tprod_eq_one_of_not_multipliable hf] }
 end
 
-end ordered_add_comm_monoid
+end ordered_comm_monoid
 
-section ordered_add_comm_group
-variables [ordered_add_comm_group α] [topological_space α] [topological_add_group α]
-  [order_closed_topology α] {f g : ι → α} {a₁ a₂ : α} {i : ι}
+section ordered_comm_group
 
-lemma has_sum_lt (h : f ≤ g) (hi : f i < g i) (hf : has_sum f a₁) (hg : has_sum g a₂) : a₁ < a₂ :=
-have update f i 0 ≤ update g i 0 := update_le_update_iff.mpr ⟨rfl.le, λ i _, h i⟩,
-have 0 - f i + a₁ ≤ 0 - g i + a₂ := has_sum_le this (hf.update i 0) (hg.update i 0),
-by simpa only [zero_sub, add_neg_cancel_left] using add_lt_add_of_lt_of_le hi this
 
-@[mono] lemma has_sum_strict_mono (hf : has_sum f a₁) (hg : has_sum g a₂) (h : f < g) : a₁ < a₂ :=
-let ⟨hle, i, hi⟩ := pi.lt_def.mp h in has_sum_lt hle hi hf hg
+variables   
+  
+@[to_additive] variables [ordered_comm_group α] [topological_space α] [topological_group α]  [order_closed_topology α] 
 
-lemma tsum_lt_tsum (h : f ≤ g) (hi : f i < g i) (hf : summable f) (hg : summable g) :
-  ∑' n, f n < ∑' n, g n :=
-has_sum_lt h hi hf.has_sum hg.has_sum
+variables {f g : ι → α} {a₁ a₂ : α} {i : ι}
 
-@[mono] lemma tsum_strict_mono (hf : summable f) (hg : summable g) (h : f < g) :
-  ∑' n, f n < ∑' n, g n :=
-let ⟨hle, i, hi⟩ := pi.lt_def.mp h in tsum_lt_tsum hle hi hf hg
+@[to_additive]
+lemma has_prod_lt (h : f ≤ g) (hi : f i < g i) (hf : has_prod f a₁) (hg : has_prod g a₂) : a₁ < a₂ :=
+have update f i 1 ≤ update g i 1 := update_le_update_iff.mpr ⟨rfl.le, λ i _, h i⟩,
+have 1 / f i * a₁ ≤ 1 / g i * a₂ := has_prod_le this (hf.update i 1) (hg.update i 1),
+by simpa only [one_div, mul_inv_cancel_left] using mul_lt_mul_of_lt_of_le hi this
 
-lemma tsum_pos (hsum : summable g) (hg : ∀ i, 0 ≤ g i) (i : ι) (hi : 0 < g i) : 0 < ∑' i, g i :=
-by { rw ←tsum_zero, exact tsum_lt_tsum hg hi summable_zero hsum }
+@[to_additive, mono] 
+lemma has_prod_strict_mono (hf : has_prod f a₁) (hg : has_prod g a₂) (h : f < g) : a₁ < a₂ :=
+let ⟨hle, i, hi⟩ := pi.lt_def.mp h in has_prod_lt hle hi hf hg
 
-lemma has_sum_zero_iff_of_nonneg (hf : ∀ i, 0 ≤ f i) : has_sum f 0 ↔ f = 0 :=
+@[to_additive tsum_lt_tsum]
+lemma tprod_lt_tprod (h : f ≤ g) (hi : f i < g i) (hf : multipliable f) (hg : multipliable g) :
+  ∏' n, f n < ∏' n, g n :=
+has_prod_lt h hi hf.has_prod hg.has_prod
+
+@[to_additive tsum_strict_mono, mono] 
+lemma tprod_strict_mono (hf : multipliable f) (hg : multipliable g) (h : f < g) :
+  ∏' n, f n < ∏' n, g n :=
+let ⟨hle, i, hi⟩ := pi.lt_def.mp h in tprod_lt_tprod hle hi hf hg
+
+@[to_additive tsum_one_lt]
+lemma tprod_pos (hprod : multipliable g) (hg : ∀ i, 1 ≤ g i) (i : ι) (hi : 1 < g i) : 
+  1 < ∏' i, g i :=
+by { rw ←tprod_one, exact tprod_lt_tprod hg hi multipliable_one hprod }
+
+@[to_additive]
+lemma has_prod_one_iff_of_one_le (hf : ∀ i, 1 ≤ f i) : has_prod f 1 ↔ f = 1 :=
 begin
   refine ⟨λ hf', _, _⟩,
   { ext i,
     refine (hf i).eq_of_not_gt (λ hi, _),
-    simpa using has_sum_lt hf hi has_sum_zero hf' },
+    simpa using has_prod_lt hf hi has_prod_one hf' },
   { rintro rfl,
-    exact has_sum_zero }
+    exact has_prod_one }
 end
 
-end ordered_add_comm_group
+end ordered_comm_group
 
-section canonically_ordered_add_monoid
-variables [canonically_ordered_add_monoid α] [topological_space α] [order_closed_topology α]
-  {f : ι → α} {a : α}
+section canonically_ordered_monoid
+@[to_additive] variables [canonically_ordered_monoid α] 
+variables [topological_space α] [order_closed_topology α] {f : ι → α} {a : α}
 
-lemma le_has_sum' (hf : has_sum f a) (i : ι) : f i ≤ a := le_has_sum hf i $ λ _ _, zero_le _
+@[to_additive]
+lemma le_has_prod' (hf : has_prod f a) (i : ι) : f i ≤ a := le_has_prod hf i $ λ _ _, one_le _
 
-lemma le_tsum' (hf : summable f) (i : ι) : f i ≤ ∑' i, f i := le_tsum hf i $ λ _ _, zero_le _
+@[to_additive le_tsum']
+lemma le_tprod' (hf : multipliable f) (i : ι) : f i ≤ ∏' i, f i := le_tprod hf i $ λ _ _, one_le _
 
-lemma has_sum_zero_iff : has_sum f 0 ↔ ∀ x, f x = 0 :=
+@[to_additive]
+lemma has_prod_one_iff : has_prod f 1 ↔ ∀ x, f x = 1 :=
 begin
   refine ⟨_, λ h, _⟩,
   { contrapose!,
-    exact λ ⟨x, hx⟩ h, hx (nonpos_iff_eq_zero.1$ le_has_sum' h x) },
-  { convert has_sum_zero,
+    exact λ ⟨x, hx⟩ h, hx (le_one_iff_eq_one.1$ le_has_prod' h x) },
+  { convert has_prod_one,
     exact funext h }
 end
 
-lemma tsum_eq_zero_iff (hf : summable f) : ∑' i, f i = 0 ↔ ∀ x, f x = 0 :=
-by rw [←has_sum_zero_iff, hf.has_sum_iff]
+@[to_additive tsum_eq_one_iff]
+lemma tprod_eq_one_iff (hf : multipliable f) : ∏' i, f i = 1 ↔ ∀ x, f x = 1 :=
+by rw [←has_prod_one_iff, hf.has_prod_iff]
 
-lemma tsum_ne_zero_iff (hf : summable f) : ∑' i, f i ≠ 0 ↔ ∃ x, f x ≠ 0 :=
-by rw [ne.def, tsum_eq_zero_iff hf, not_forall]
+@[to_additive tsum_ne_zero_iff]
+lemma tprod_ne_one_iff (hf : multipliable f) : ∏' i, f i ≠ 1 ↔ ∃ x, f x ≠ 1 :=
+by rw [ne.def, tprod_eq_one_iff hf, not_forall]
 
-lemma is_lub_has_sum' (hf : has_sum f a) : is_lub (set.range $ λ s, ∑ i in s, f i) a :=
-is_lub_of_tendsto_at_top (finset.sum_mono_set f) hf
+@[to_additive]
+lemma is_lub_has_prod' (hf : has_prod f a) : is_lub (set.range $ λ s, ∏ i in s, f i) a :=
+is_lub_of_tendsto_at_top (finset.prod_mono_set' f) hf
 
-end canonically_ordered_add_monoid
+end canonically_ordered_monoid
 
 section linear_order
 
 /-!
-For infinite sums taking values in a linearly ordered monoid, the existence of a least upper
-bound for the finite sums is a criterion for summability.
+For infinite prods taking values in a linearly ordered monoid, the existence of a least upper
+bound for the finite prods is a criterion for prodmability.
 
 This criterion is useful when applied in a linearly ordered monoid which is also a complete or
 conditionally complete linear order, such as `ℝ`, `ℝ≥0`, `ℝ≥0∞`, because it is then easy to check
 the existence of a least upper bound.
 -/
 
-lemma has_sum_of_is_lub_of_nonneg [linear_ordered_add_comm_monoid α] [topological_space α]
-  [order_topology α] {f : ι → α} (i : α) (h : ∀ i, 0 ≤ f i)
-  (hf : is_lub (set.range $ λ s, ∑ i in s, f i) i) :
-  has_sum f i :=
-tendsto_at_top_is_lub (finset.sum_mono_set_of_nonneg h) hf
+@[to_additive]
+lemma has_prod_of_is_lub_of_one_le [linear_ordered_comm_monoid α] [topological_space α]
+  [order_topology α] {f : ι → α} (i : α) (h : ∀ i, 1 ≤ f i)
+  (hf : is_lub (set.range $ λ s, ∏ i in s, f i) i) :
+  has_prod f i :=
+tendsto_at_top_is_lub (finset.prod_mono_set_of_one_le' h) hf
 
-lemma has_sum_of_is_lub [canonically_linear_ordered_add_monoid α] [topological_space α]
-   [order_topology α] {f : ι → α} (b : α) (hf : is_lub (set.range $ λ s, ∑ i in s, f i) b) :
-  has_sum f b :=
-tendsto_at_top_is_lub (finset.sum_mono_set f) hf
+@[to_additive]
+lemma has_prod_of_is_lub [canonically_linear_ordered_monoid α] [topological_space α]
+   [order_topology α] {f : ι → α} (b : α) (hf : is_lub (set.range $ λ s, ∏ i in s, f i) b) :
+  has_prod f b :=
+tendsto_at_top_is_lub (finset.prod_mono_set' f) hf
+
+-- No has_abs.abs in the mul case
 
 lemma summable_abs_iff [linear_ordered_add_comm_group α] [uniform_space α] [uniform_add_group α]
   [complete_space α] {f : ι → α} :
